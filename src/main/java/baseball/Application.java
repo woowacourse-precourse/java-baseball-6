bb29f1a01fb9721c2bf93c9d6d381fe7147f244a
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-10-19 21:56:10 +0900

    Feat : 프로그램 구현 MVC패턴을 이용해 프로그램 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
 
+import baseball.controller.GameController;
+import baseball.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameController gameController = new GameController();
+
+        OutputView.printStart();
+        do{
+            gameController.start();
+        } while (gameController.isReset());
     }
 }
