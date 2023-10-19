f0f20229ba5fbc42a8374b641f306c082a26738a
WeeYoungSeok <dndrldzk@gmail.com>
2023-10-19 22:31:00 +0900

    fix: GameValue 클래스 수정 변수들을 private으로 선언하여 외부 클래스에서 접근하지 못해 public으로 변경하였습니다.

--- src/main/java/resource/GameValue.java
+++ src/main/java/resource/GameValue.java
@@ -2,7 +2,7 @@ package resource;
 
 public final class GameValue {
 
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 9;
-    private static final int MAX_LENGTH = 3;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 9;
+    public static final int MAX_LENGTH = 3;
 }
