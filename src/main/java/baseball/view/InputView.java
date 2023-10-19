b1a92e2f6bdfc7a157ffc7a5717fcd8d231389c8
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-10-19 21:45:45 +0900

    chore : 메서드명 수정 retryNumber -> resetNumber

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -8,7 +8,7 @@ public class InputView {
         return Console.readLine();
     }
 
-    public static String retryNumber(){
+    public static String resetNumber(){
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         return Console.readLine();
     }
