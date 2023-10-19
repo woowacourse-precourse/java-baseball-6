2e4113c7ee25707228ad93534576870226e0d260
Sully <2dcoder@naver.com>
2023-10-19 21:31:17 +0900

    rename: GameInput to UserInput

--- src/main/java/baseball/user/UserInput.java
+++ src/main/java/baseball/user/UserInput.java
@@ -1,8 +1,8 @@
-package baseball.input;
+package baseball.user;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class GameInput {
+public class UserInput {
 
     private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String REQUEST_MESSAGE = "숫자를 입력해주세요 : ";
