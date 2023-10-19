e9f5c63bc6076e9796285a2b9d9eb277b72c2f71
sayya <djdj9876@naver.com>
2023-10-20 01:22:44 +0900

    feat: Controller에 해당하는 GameController 클래스 구현 -- 게임 프로세스 로직

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,4 +1,44 @@
 package baseball.controller;
 
+import baseball.model.Player;
+import baseball.model.Referee;
+import baseball.view.Message;
+
 public class GameController {
+
+    public Player player;
+    public Referee referee;
+
+    public void startGame() {
+        Message.getInstance().printStartMessage();
+        do {
+            setGame();
+            playGame();
+            Message.getInstance().printRestartMessage();
+        } while (referee.isRestart());
+    }
+
+    private void setGame() {
+        this.referee = new Referee();
+        this.player = new Player();
+        referee.setGame();
+    }
+
+    private void playGame() {
+        while (true) {
+            // 1. 공 던지기
+            Message.getInstance().printInputMessage();
+            String ball = player.throwBall();
+            // 2. 볼 판정
+            int[] ballCount = referee.callBallCount(ball);
+            Message.getInstance().printBallCount(ballCount[0], ballCount[1]);
+            // 3. 삼진 아웃 - 게임 종료
+            if (referee.isStrikeOut(ballCount)) {
+                Message.getInstance().printEndMessage();
+                break;
+            }
+        }
+    }
+
+
 }
