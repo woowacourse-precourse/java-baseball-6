3e7c379d6ce9f216acd3ac851f5077b4bc3cc510
HyunJoong Kim <80106238+hjk0761@users.noreply.github.com>
2023-10-20 01:10:19 +0900

    test: test generating answer

--- src/main/java/test/test.java
+++ src/main/java/test/test.java
@@ -1,10 +1,16 @@
 package test;
 import baseball.UserInput;
+import baseball.GenerateAnswer;
 
 public class test {
+    public static void main(String[] args){
+        UserInputTest();
+        GenerateAnswerTest();
+    }
+
     public static void UserInputTest(){
         while(true) {
-            UserInput userInput = new UserInput();
+            UserInput userInput = new UserInput(3);
             int temp = userInput.getInput();
             System.out.println(temp);
             if (temp == 123){
@@ -12,8 +18,12 @@ public class test {
             }
         }
     }
-    public static void main(String[] args){
-        UserInputTest();
+
+    public static void GenerateAnswerTest(){
+        for (int i = 0; i < 10; i ++){
+            GenerateAnswer generateAnswer = new GenerateAnswer(3);
+            System.out.println(generateAnswer.genAnswer());
+        }
     }
 
 }
