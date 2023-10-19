8d09e4ea84d57104ce950cf3bd463d05a0a7e56d
hongseokhodev <hongseokhodev@gmail.com>
2023-10-20 00:33:24 +0900

    feature: generate random balls

--- src/main/java/baseball/utils/Constants.java
+++ src/main/java/baseball/utils/Constants.java
@@ -3,4 +3,6 @@ package baseball.utils;
 public final class Constants {
     private Constants() {}
     public static final int NUMBER_OF_BALLS = 3;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 9;
 }
