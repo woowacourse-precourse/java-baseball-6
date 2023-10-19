265b23c1c790be2de61f953d4152f1d7462b9aaf
suihub <qkdcrt@gmail.com>
2023-10-19 20:04:15 +0900

    <feat>: "게임 시작 문구 출력 기능 구현"
    View에 출력을 담당하는 클래스를 추가.
    그에 따라 컨트롤러에서 해당 View를 의존하도록 변경.

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,6 +1,7 @@
 package baseball.controller;
 
 import baseball.domain.Ball;
+import baseball.view.GameOutput;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.LinkedHashSet;
@@ -8,12 +9,14 @@ import java.util.Set;
 
 public class GameController {
     private final Set<BallDto> computer = new LinkedHashSet<>();
+    private final GameOutput output = new GameOutput();
 
     public void start(){
         while (computer.size() < 3){
             Ball ball = new Ball(Randoms.pickNumberInRange(1, 9));
             computer.add(new BallDto(ball.getNumber()));
         }
+        output.printGameStart();
     }
 
     public Set<BallDto> getComputer() {
