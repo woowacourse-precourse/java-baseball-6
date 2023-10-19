678993fd9514917c3b2579efce351d73ccfa98c1
eunhee <dmsgml9903@gmail.com>
2023-10-19 21:07:38 +0900

    feat : 게임 시작시 문구 출력 기능
    게임이 시작되면 '숫자 야구 게임을 시작합니다.'라는 문구 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,13 +6,10 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
 
         Computer computer = new Computer();
         List<Integer> randomAnswer = computer.createRandomAnswer();
-
-
-
-
     }
 
 }
