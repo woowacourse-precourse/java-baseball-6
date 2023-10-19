373eec4306ce7019830e6bea3946a5e0561b571d
MSDN-SPECIAL\Administrator <jsw1767@gmail.com>
2023-10-19 20:17:55 +0900

    사용자가 잘못 입력한 경우 IllegalArgumentException 발생시키기

--- docs/README.md
+++ docs/README.md
@@ -1,7 +1,9 @@
 ## 게임 시작
 
-# 게임 시작 문구 출력
+## 게임 시작 문구 출력
 
-# 숫자 입력 문구와 함께 입력 받기
+## 숫자 입력 문구와 함께 입력 받기
 
 - camp.nextstep.edu.missionutils.Console의 readLine()을 활용
+- 입력된 숫자가 서로 다른 3자리 수인지 확인
+- 서로 다른 3자리 수 외의 입력은 IllegalArgumentException 발생시킨 후 종료
--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,6 @@
 package baseball;
 
-import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+import camp.nextstep.edu.missionutils.Console;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
@@ -8,8 +8,32 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
         System.out.print("숫자를 입력해주세요 : ");
-        String number = readLine();
-        System.out.println(number);
+        String numberStr = readLine();
+        System.out.println(numberStr);
+
+        boolean isThreeDifferentDigit = isThreeDifferentDigit(numberStr);
+        if (isThreeDifferentDigit) {
+            System.out.println(numberStr);
+        } else {
+            throw new IllegalArgumentException("서로 다른 3자리 숫자가 아닙니다.");
+        }
     }
 
+    public static boolean isThreeDifferentDigit(String numberStr) {
+        // 숫자가 아닐 때
+        try {
+            Integer.parseInt(numberStr);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자가 아닙니다.");
+        }
+        // 3자리가 아닐때
+        if (numberStr.length() != 3) {
+            throw new IllegalArgumentException("3자리 숫자가 아닙니다.");
+        }
+
+        char digit1 = numberStr.charAt(0);
+        char digit2 = numberStr.charAt(1);
+        char digit3 = numberStr.charAt(2);
+        return digit1 != digit2 && digit1 != digit3 && digit2 != digit3;
+    }
 }
