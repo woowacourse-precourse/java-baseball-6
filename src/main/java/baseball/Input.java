5ff2b661b2049bf5d3ed204d124581f9e391bdb7
Zeus6768 <akaps6768@gmail.com>
2023-10-19 16:00:33 +0900

    기능 구현: 재시작/종료 입력

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -4,9 +4,17 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
 
+    protected static final String RESTART = "1";
+    protected static final String EXIT = "2";
+
     public static String guess;
 
     public static void guessInput() {
         String input = Console.readLine();
+    }
+
+    public static boolean isRestart() {
+        String input = Console.readLine();
+        return input.equals(RESTART);
     }
 }
