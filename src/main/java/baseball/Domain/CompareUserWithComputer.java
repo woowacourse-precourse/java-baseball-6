eaaa2a86cd4d768c134ae22ce64f8afe2a303ccf
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-10-19 23:19:05 +0900

    feat: CompareUserWithComputer 구현

---
+++ src/main/java/baseball/Domain/CompareUserWithComputer.java
@@ -0,0 +1,38 @@
+package baseball.Domain;
+
+public class CompareUserWithComputer {
+    private String userNumber;
+    private String computerNumber;
+
+    private int strikeNum;
+    private int ballNum;
+
+    public CompareUserWithComputer(String userNumber, String computerNumber){
+        this.userNumber = userNumber;
+        this.computerNumber = computerNumber;
+    }
+
+    public int getStrikeNum(){
+        return strikeNum;
+    }
+
+    public int getBallNum(){
+        return ballNum;
+    }
+
+    private void countStrikeNum(){
+        for (int i = 0; i < userNumber.length(); i++) {
+            if(userNumber.charAt(i) == computerNumber.charAt(i)){
+                this.strikeNum++;
+            }
+        }
+    }
+
+    private void countBallNum(){
+        for (int i = 0; i < userNumber.length(); i++) {
+            if(computerNumber.contains(String.valueOf(userNumber.charAt(i)))){
+                this.ballNum++;
+            }
+        }
+    }
+}
