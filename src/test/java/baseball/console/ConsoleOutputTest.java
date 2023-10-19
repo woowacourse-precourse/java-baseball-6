873a4db7350fa95e23cf3a2b3414cd72ba727d5d
takealook97 <takealook97@naver.com>
2023-10-20 04:23:06 +0900

    refactor: ConsoleOutputTest 파라미터 타입 수정
    출력 테스트코드 또한 새로 생성한 BaseBallResult 클래스 타입으로 파라미터를 받도록 수정하였다.

--- src/test/java/baseball/console/ConsoleOutputTest.java
+++ src/test/java/baseball/console/ConsoleOutputTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import baseball.dto.BaseBallResult;
+
 class ConsoleOutputTest extends IOTest {
 	private final ConsoleOutput consoleOutput;
 
@@ -14,28 +16,28 @@ class ConsoleOutputTest extends IOTest {
 	@Test
 	void 볼() {
 		String expectedOutput = "1볼";
-		captureSystemOut(() -> consoleOutput.printResult(1, 0));
+		captureSystemOut(() -> consoleOutput.printResult(new BaseBallResult(1, 0)));
 		assertEquals(expectedOutput, getCapturedOutput());
 	}
 
 	@Test
 	void 스트라이크() {
 		String expectedOutput = "1스트라이크";
-		captureSystemOut(() -> consoleOutput.printResult(0, 1));
+		captureSystemOut(() -> consoleOutput.printResult(new BaseBallResult(0, 1)));
 		assertEquals(expectedOutput, getCapturedOutput());
 	}
 
 	@Test
 	void 볼스트라이크() {
 		String expectedOutput = "1볼 1스트라이크";
-		captureSystemOut(() -> consoleOutput.printResult(1, 1));
+		captureSystemOut(() -> consoleOutput.printResult(new BaseBallResult(1, 1)));
 		assertEquals(expectedOutput, getCapturedOutput());
 	}
 
 	@Test
 	void 낫싱() {
 		String expectedOutput = "낫싱";
-		captureSystemOut(() -> consoleOutput.printResult(0, 0));
+		captureSystemOut(() -> consoleOutput.printResult(new BaseBallResult(0, 0)));
 		assertEquals(expectedOutput, getCapturedOutput());
 	}
 }
