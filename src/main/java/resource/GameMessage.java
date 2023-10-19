f6e13b1adc3e7df31570cb9fc8cab2ba559d7062
WeeYoungSeok <dndrldzk@gmail.com>
2023-10-19 22:39:51 +0900

    feat: 게임 출력 메세지 구현 숫자 야구 게임을 진행할 때 필요한 출력 메세지 상수 클래스를 구현하였습니다.

---
+++ src/main/java/resource/GameMessage.java
@@ -0,0 +1,14 @@
+package resource;
+
+public final class GameMessage {
+
+    public static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public static final String END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    public static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    public static final String RESTART_QUESTION_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public static final String STRIKE_MESSAGE = "스트라이크";
+    public static final String BALL_MESSAGE = "볼";
+    public static final String NOTHING_MESSAGE = "낫싱";
+}
