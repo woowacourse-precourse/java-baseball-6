3fe0de1e7033c08019de8beb581b703c83a74354
yang <ywj9811@gmail.com>
2023-10-20 01:43:58 +0900

    refactor : 예외 처리 분리 및 메소드 분리

--- src/main/java/baseball/UserBehavior.java
+++ src/main/java/baseball/UserBehavior.java
@@ -5,23 +5,21 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class UserBehavior {
+    private CheckException checkException = new CheckException();
     private final List<Integer> computerNumbers;
     private int strike;
     private int ball;
 
     public UserBehavior(List<Integer> computerNumbers) {
         this.computerNumbers = computerNumbers;
+
     }
 
     public int input() {
         try {
-            strike = 0;
-            ball = 0;
-
-            System.out.print("숫자를 입력해주세요 : ");
-            String inputNumber = Console.readLine();
-            countResult(inputNumber);
-
+            String inputNumbers[] = setStart();
+            checkException.checkAll(inputNumbers);
+            countResult(inputNumbers);
             while (result()) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
@@ -33,22 +31,23 @@ public class UserBehavior {
         }
     }
 
-    private void countResult(String inputNumber) {
-        if (inputNumber.length() > 3)
-            throw new IllegalArgumentException();
+    private String[] setStart() {
+        strike = 0;
+        ball = 0;
+        System.out.print("숫자를 입력해주세요 : ");
+        return Console.readLine().split("");
+    }
 
-        int first = Integer.parseInt(inputNumber.substring(0, 1));
+    private void countResult(String[] inputNumbers) {
+        int first = Integer.parseInt(inputNumbers[0]);
         check(0, first);
-        int second = Integer.parseInt(inputNumber.substring(1, 2));
+        int second = Integer.parseInt(inputNumbers[1]);
         check(1, second);
-        int third = Integer.parseInt(inputNumber.substring(2, 3));
+        int third = Integer.parseInt(inputNumbers[2]);
         check(2, third);
-        checkError(first, second, third);
     }
 
     private void check(int index, int number) {
-        if (number == 0)
-            throw new IllegalArgumentException();
         if (computerNumbers.get(index) == number) {
             strike++;
             return;
@@ -56,6 +55,7 @@ public class UserBehavior {
         if (computerNumbers.contains(number))
             ball++;
     }
+
     private boolean result() {
         if (strike == 3) {
             System.out.println(strike+"스트라이크");
@@ -75,14 +75,5 @@ public class UserBehavior {
         }
         System.out.println(ball+"볼 "+strike+"스트라이크");
         return false;
-    }
-
-    private void checkError(int first, int second, int third) {
-        if (first == second)
-            throw new IllegalArgumentException();
-        if (second == third)
-            throw new IllegalArgumentException();
-        if (first == third)
-            throw new IllegalArgumentException();
     }
 }
