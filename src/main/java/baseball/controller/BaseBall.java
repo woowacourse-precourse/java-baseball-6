02c7757866c2a8dd325dce20604d171709e684dc
sinyebin <dpqls0625@naver.com>
2023-10-19 21:50:41 +0900

    게임 시작 메시치 출력

---
+++ src/main/java/baseball/controller/BaseBall.java
@@ -0,0 +1,13 @@
+package baseball.controller;
+
+import baseball.view.PrintMessage;
+
+public class BaseBall {
+
+    //게임 시작
+    public void startGame(){
+        boolean playGame=true;
+        PrintMessage.printGameStart();
+
+    }
+}
---
+++ src/main/java/baseball/view/PrintMessage.java
@@ -0,0 +1,9 @@
+package baseball.view;
+
+public class PrintMessage {
+
+    //게임 시작 메시지 출력
+    public static void printGameStart(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+}
