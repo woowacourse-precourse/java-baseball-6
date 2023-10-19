4fcc49ee0814f9a3f274169b169ba94a08694c77
takealook97 <takealook97@naver.com>
2023-10-20 04:21:46 +0900

    refactor: Scanner를 싱글톤 형태로 수정
    기존에는 인풋이 들어올때마다 선언을 하였지만, 필드에 놓고 지속적으로 사용가능하기에 싱글톤 형태로 수정하였다.

--- src/main/java/baseball/console/ConsoleInput.java
+++ src/main/java/baseball/console/ConsoleInput.java
@@ -6,29 +6,27 @@ import baseball.util.InputValidation;
 
 public class ConsoleInput {
 	private final InputValidation inputValidation;
+	private final Scanner sc;
 
 	public ConsoleInput() {
 		inputValidation = new InputValidation();
+		sc = new Scanner(System.in);
 	}
 
 	public String getGameInput() {
-		try (Scanner sc = new Scanner(System.in)) {
-			String input = sc.nextLine();
-			if (inputValidation.isGameInputValid(input)) {
-				return input;
-			}
-			throw new IllegalArgumentException("1 ~ 9 중 중복되지 않은 세자리 숫자만 입력 가능합니다.");
+		String input = sc.nextLine();
+		if (inputValidation.isGameInputValid(input)) {
+			return input;
 		}
+		throw new IllegalArgumentException("1 ~ 9 중 중복되지 않은 세자리 숫자만 입력 가능합니다.");
 	}
 
 	public String getEndInput() {
-		try (Scanner sc = new Scanner(System.in)) {
-			String input = sc.nextLine();
-			if (inputValidation.isEndInputValid(input)) {
-				return input;
-			}
-			throw new IllegalArgumentException("1 또는 2만 입력 가능합니다.");
+		String input = sc.nextLine();
+		if (inputValidation.isEndInputValid(input)) {
+			return input;
 		}
+		throw new IllegalArgumentException("1 또는 2만 입력 가능합니다.");
 	}
 }
 
