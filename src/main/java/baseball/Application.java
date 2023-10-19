ca71a836babb800bebc0430d3880d9aa9646c757
m5s3 <devmyoungsoo2@gmail.com>
2023-10-19 21:27:26 +0900

    feat: 입력값 예외처리 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Application {
     static final Scanner scanner = new Scanner(System.in);
@@ -10,13 +11,25 @@ public class Application {
     }
 
     public static void playGame() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        String number = requestNumber();
+        try {
+            System.out.println("숫자 야구 게임을 시작합니다.");
+            String number = requestNumber();
+            validateInput(number);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public static String requestNumber() {
         System.out.print("숫자를 입력해 주세요 : ");
         String input = scanner.nextLine();
         return input;
+    }
+
+    public static void validateInput(String input) {
+        // Pattern.matches("^[0-9]*$", str);
+        if (!Pattern.matches("^\\d{3}$", input)) {
+            throw new IllegalArgumentException("입력값이 잘못 되었습니다");
+        }
     }
 }
