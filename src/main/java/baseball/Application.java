25f905737b6612a07faf4d3d2b1b84ce4d15c6a8
koojawon <jawonkoo@naver.com>
2023-10-19 18:29:23 +0900

    refactor: 힌트 생성 시 공백 넣는 조건 변경 줄바꿈 추가 게임 종료 조건문 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,8 @@
 package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.Arrays;
 import java.util.ArrayList;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Application {
@@ -22,22 +20,26 @@ public class Application {
     }
 
     public static ArrayList<Integer> guessInputHandler() throws IllegalArgumentException{
-        ArrayList<Integer> result;
-        String s = Console.readLine();
+        ArrayList<Integer> rval;
+        String s;
 
-        if(s.isEmpty() || s.length() > 3)
+        s = Console.readLine();
+
+        if(s.isEmpty() || s.length() > 3) {
             throw new IllegalArgumentException();
+        }
+
         try{
-            result = (ArrayList<Integer>) Arrays
+            rval = (ArrayList<Integer>) Arrays
                     .stream(s.split(""))
                     .mapToInt(Integer::parseInt)
                     .boxed()
                     .collect(Collectors.toList());
-        } catch (Exception e){
+        } catch (NumberFormatException e){
             throw new IllegalArgumentException();
         }
 
-        return result;
+        return rval;
     }
 
     public static String restartInputHandler() throws  IllegalArgumentException{
@@ -52,10 +54,16 @@ public class Application {
     public static StringBuilder generateHint(Integer ball, Integer strike){
         StringBuilder resultStatement = new StringBuilder();
 
-        if (ball > 0)
-            resultStatement.append(ball).append("볼 ");
-        if (strike > 0)
+        if (ball > 0) {
+            resultStatement.append(ball).append("볼");
+        }
+
+        if (strike > 0) {
+            if(!resultStatement.isEmpty()){
+                resultStatement.append(" ");
+            }
             resultStatement.append(strike).append("스트라이크");
+        }
 
         if(strike == 0 && ball == 0)
             resultStatement.append("낫싱");
@@ -68,10 +76,10 @@ public class Application {
         ArrayList<Integer> answer;
         int ball;
         int strike;
+
         do {
             answer = generateAnswer();
             System.out.println("숫자 야구 게임을 시작합니다.");
-
             do {
                 ball = 0;
                 strike = 0;
@@ -89,9 +97,9 @@ public class Application {
                 }
 
                 System.out.println(generateHint(ball,strike));
-            }while(strike != 3);
 
+            } while(strike != 3);
             System.out.println("\n3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        } while (!Objects.equals(restartInputHandler(), "2"));
+        } while (!restartInputHandler().equals("2"));
     }
 }
