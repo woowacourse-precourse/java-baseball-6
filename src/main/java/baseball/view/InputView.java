bb6cc40efdd934b8b9333b9123ff465e3c46028c
pwrwpw <dlgustjr9912@gmail.com>
2023-10-20 01:35:11 +0900

    Fix(InputView): 의미상 메서드 클래스 위치 변경

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -9,17 +9,11 @@ import java.util.stream.Collectors;
 public class InputView {
 	private static final String INPUT_NUMBER = "숫자를 입력해주세요 : ";
 
-	public List<Integer> readNumber() {
+
+	public String readNumber() {
 		System.out.print(INPUT_NUMBER);
-		String input = InputValidation.validate(inputNumber());
-		return convertStringToInteger(input);
-	}
-	public static String inputNumber(){
 		return Console.readLine();
 	}
-	public static List<Integer> convertStringToInteger(String input){
-		return Arrays.stream(input.split(""))
-				.map(Integer::parseInt)
-				.collect(Collectors.toList());
-	}
+
+
 }
