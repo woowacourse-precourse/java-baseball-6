869d9d105fabe02cb45f3db6a9ca53820a9e1f9b
kes02 <hijin1220@naver.com>
2023-10-19 19:22:38 +0900

    Feat: Add Control Input & Exception
    readLine을 이용해 사용자가 입력한 값을 문자열로 읽고, checkInput을 통해 올바른 값인지 체크
    - 문자열 길이 체크
    - 동일한 문자가 있는지 체크
    - 문자열에 0이 포함되어 있는지 체크

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,23 +6,94 @@ import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        BaseBallGame rightAnswer = new BaseBallGame();
+        BaseBallGame computerAnswer = new BaseBallGame();
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        BaseBallGame.init(computerAnswer);
     }
 
     class Array() {
-
+        static char[] getStringToChar(String str) {
+            char[] charArr = new char[str.length()];
+            for (int i=0; i<str.length(); i++){
+                charArr[i] = str.charAt(i);
+            }
+            return charArr;
+        }
+        static boolean checkContains(char[] arr, char num) {
+            for (int value : arr) {
+                if (value == num) return true;
+            }
+            return false;
+        }
+        static boolean checkContains(int[] arr, int num) {
+            for (int value : arr) {
+                if (value == num) return true;
+            }
+            return false;
+        }
     }
 
 
     class BaseBallGame() {
         private static final int START_RANGE = 1;
         private static final int END_RANGE = 9;
-        final int NUMBER_IDX = 3;
+        static final int NUMBER_IDX = 3;
         private static final int NEW_GAME = 1;
         private static final int QUIT_GAME = 2;
 
         int[] computer = new int[NUMBER_IDX];
 
+        BaseBallGame() {
+            int nowRandomNum = 0, nowRandomNumIdx = 0;
+        }
+
+        public static void init(BaseBallGame rightAnswer) {
+            BaseBallGame userAnswer = BaseBallGame.getUserAnswer();
+            GameResult gameResult = new GameResult();
+
+        }
+
+        private static BaseBallGame getUserAnswer() {
+            System.out.println("숫자를 입력해주세요 : ");
+            String userInput = readLine();
+            BaseBallGame.checkInput(userInput);
+
+            return new BaseBallGame(userInput);
+        }
+
+        private static void checkInput(String str) { //IllegalException
+            if (!BaseBallGame.checkLen(str))
+                throw new IllegalArgumentException();
+            if (!BaseBallGame.checkEqual(str))
+                throw new IllegalArgumentException();
+            if (!BaseBallGame.checkNum(str))
+                throw new IllegalArgumentException();
+        }
+
+        static boolean checkLen(String str) {
+            return str.length() == BaseBallGame.NUMBER_IDX;
+        }
+        static boolean checkEqual(String str) {
+            char[] equalNum = new char[str.length()];
+            char[] word = Array.getStringToChar(str);
+
+            for(int i=0; i<str.length(); i++) {
+                if(Array.checkContains(equalNum, word[i]))
+                    return false;
+                equalNum[i] = word[i];
+            }
+            return true;
+        }
+        static boolean checkNum(String str) {
+            try {
+                int num = Integer.parseInt(str);
+            } catch (NumberFormatException e) {
+                return false;
+            }
+            char[] numArr = Array.getStringToChar(str);
+            return !Array.checkContains(numArr, '0');
+        }
     }
 
     class GameResult() {
@@ -39,7 +110,7 @@ public class Application {
             this.strike++;
         }
 
-        public void printResult() {
+        void printResult() {
             if(this.strike != 3) {
                 if (this.ball > 0 && this.strike > 0)
                     System.out.println(this.ball + BALL + " " + this.strike + STRIKE);
