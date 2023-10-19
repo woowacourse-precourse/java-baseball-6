3321a42626d3b4b69c44dab7d938ef7bb6a02a5f
BullChallenger <am23am45@naver.com>
2023-10-20 01:44:10 +0900

    [FIX] Computer 도메인과 Baseball 일급 컬렉션 간의 역할 재분배
    Baseball클래스 내부에서 자체적으로 현재 Computer 도메인에 할당 된 숫자의 수를 파악하고 생성된 숫자가 이미 포함되어 있는가를 판단하는 것은 적절한 책임의 분리가 아니라는 생각이 들었다
    
    현재 몇 개의 숫자가 할당되어 있고, 새로 생성된 숫자가 이미 포함되어 있는지를 확인하는 책임은 Baseball이 아닌 Computer가 가지는 것이 옳다고 보았기 때문에 책임을 이동시켰다
    
    숫자를 생성하는 책임 또한 Baseball에서 Computer로 이전시켰다
    
    책임을 이전하는 과정 속에서 기존 Computer 도메인이 지니고 있던 필드인 baseball의 타입이 List<Baseball>로 변경되었으며, 해당 배열 내부에서 Baseball 중복 체크를 위해 equals와 hashCode를 Baseball 클래스에 Override 했다

--- src/main/java/baseball/domain/Baseball.java
+++ src/main/java/baseball/domain/Baseball.java
@@ -1,47 +1,46 @@
 package baseball.domain;
 
-import baseball.util.RandomGenerator;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Baseball {
 
     private static final int MINIMUM_NUMBER = 1;
     private static final int MAXIMUM_NUMBER = 9;
-    private static final int CAPACITY = 3;
     private static final String ILLEGAL_RANGE = "시스템 내부에서 잘못된 값이 입력되었습니다.";
 
-    private final List<Integer> baseBallNumbers = new ArrayList<>();
+    private int number;
 
-    private boolean isContainNumber(int number) {
-        if (baseBallNumbers.contains(number)) return true;
-        else return false;
+    public Baseball(int number) {
+        validateRange(number);
+        this.number = number;
     }
 
-    public Baseball generateBaseballs() {
-        if (!baseBallNumbers.isEmpty()) baseBallNumbers.clear();
-
-        while (baseBallNumbers.size() != CAPACITY) {
-            int candidate = RandomGenerator.generateNumber();
-            validateRange(candidate);
-            validateDuplication(candidate);
-        }
-        return this;
-    }
-
-    private void validateRange(int candidate) {
-        if (MINIMUM_NUMBER > candidate && MAXIMUM_NUMBER < candidate)
+    private void validateRange(int number) {
+        if (MINIMUM_NUMBER > number && MAXIMUM_NUMBER < number)
             throw new IllegalArgumentException(ILLEGAL_RANGE);
     }
 
-    private void validateDuplication(int candidate) {
-        if (!isContainNumber(candidate))
-            baseBallNumbers.add(candidate);
+    public int getNumber() {
+        return number;
     }
 
-    public List<Integer> getBaseBallNumbers() {
-        return baseBallNumbers;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Baseball baseball = (Baseball) o;
+
+        return number == baseball.number;
     }
 
+    @Override
+    public int hashCode() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return "Baseball{" +
+                "number=" + number +
+                '}';
+    }
 }
--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,15 +1,36 @@
 package baseball.domain;
 
+import baseball.util.RandomGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Computer {
 
-    private Baseball baseball;
+    private static final int CAPACITY = 3;
 
-    public Computer(Baseball baseball) {
-        this.baseball = baseball;
+    private List<Baseball> baseballs = new ArrayList<>();
+
+    public List<Baseball> generateBaseball() {
+        if (!baseballs.isEmpty()) baseballs.clear();
+
+        while (this.baseballs.size() != CAPACITY) {
+            Baseball baseball = new Baseball(RandomGenerator.generateNumber());
+            if (!isDuplicatedBaseball(baseball))
+                this.baseballs.add(baseball);
+        }
+
+        return this.baseballs;
     }
 
-    public Baseball getBaseball() {
-        return baseball;
+    private boolean isDuplicatedBaseball(Baseball baseball) {
+        if (this.baseballs.contains(baseball)) return true;
+        return false;
+    }
+
+    public List<Integer> numbersOfBaseballs() {
+        return this.baseballs.stream().map(baseball -> baseball.getNumber()).collect(Collectors.toList());
     }
 
 }
