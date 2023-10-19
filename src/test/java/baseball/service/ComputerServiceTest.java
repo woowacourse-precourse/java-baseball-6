c073b7d6a0fbd209782697440dc030c112cdcc23
yeseong31 <yeseong31@naver.com>
2023-10-19 21:39:04 +0900

    test: 상대방(컴퓨터) 3자리 숫자 생성 테스트

---
+++ src/test/java/baseball/service/ComputerServiceTest.java
@@ -0,0 +1,34 @@
+package baseball.service;
+
+import static baseball.domain.Computer.BASEBALL_NUMBERS_SIZE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ComputerServiceTest {
+    
+    private final ComputerService computerService = new ComputerService();
+    
+    @Test
+    @DisplayName("상대방(컴퓨터) 3자리 숫자 생성 확인")
+    void createComputerNumber() {
+        
+        computerService.initComputer();
+        String computerNumber = computerService.getComputerNumber();
+        int computerNumberLength = computerNumber.length();
+        
+        assertThat(computerNumberLength).isEqualTo(3);
+    }
+    
+    @Test
+    @DisplayName("상대방(컴퓨터) 3자리 숫자 중복 확인")
+    void validateComputerNumber() {
+        
+        computerService.initComputer();
+        String computerNumber = computerService.getComputerNumber();
+        
+        assertEquals(BASEBALL_NUMBERS_SIZE, computerNumber.length());
+    }
+}
