81372c091e42f04b19f3d6680bc13d7e410441c4
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-10-19 22:04:32 +0900

    feat : inputView에서 게임 재시작 여부 입력 기능 구현 - docs : View -> InputView -> 모든 항목 체크

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,6 +6,6 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         InputView inputView = new InputView();
-        inputView.readBaseballNumber();
+        inputView.readRetryCommand();
     }
 }
--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -3,10 +3,16 @@ package baseball.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    
+
     public String readBaseballNumber(){
         String inputNumber = Console.readLine();
         System.out.println(inputNumber);
         return inputNumber;
+    }
+
+    public String readRetryCommand(){
+        String inputCommand = Console.readLine();
+        System.out.println(inputCommand);
+        return inputCommand;
     }
 }
