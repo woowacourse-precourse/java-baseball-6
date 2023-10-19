4b2fd174bad5029402c1d04065623f2c50514ca3
gosu <q63530@naver.com>
2023-10-19 16:40:58 +0900

    feat : 상수추가

---
+++ src/main/java/baseball/constant/GameConstant.java
@@ -0,0 +1,18 @@
+package baseball.constant;
+
+public class GameConstant {
+    public static String GAME_START = "숫자 야구 게임을 시작합니다.";
+
+    public static String INPUT_NUMBER = "숫자를 입력해주세요 : ";
+
+    public static String GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    public static String RESTART_OR_EXIT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+
+    public static String BALL = "볼";
+
+    public static String STRIKE = "스트라이크";
+
+    public static String NOTHING = "낫싱";
+}
---
+++ src/main/java/baseball/game/Game.java
@@ -0,0 +1,6 @@
+package baseball.game;
+
+public class Game {
+
+
+}
