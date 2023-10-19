84f5b59a0063350cc523f384e8882c82af9ed6ea
uhanuu <hwryu990927@gmail.com>
2023-10-20 01:38:34 +0900

    refactor: stream 개선

--- src/main/java/baseball/api/request/RequestChecker.java
+++ src/main/java/baseball/api/request/RequestChecker.java
@@ -6,13 +6,15 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class RequestChecker {
+    private static final int NUMBER_LENGTH = 3;
+
     public static String gameRequest() throws IllegalArgumentException{
         String input = Console.readLine();
-        inputCheck(input);
+        validateUserInput(input);
         return input;
     }
 
-    private static void inputCheck(String input) throws IllegalArgumentException{
+    private static void validateUserInput(String input) throws IllegalArgumentException{
         if (!isNumericAndThreeDigits(input)){
             throw new IllegalArgumentException("사용자의 입력값은 3자리 수이며 1-9까지의 값만 가능합니다.");
         } else if (!hasDuplicateDigits(input)) {
@@ -21,20 +23,19 @@ public class RequestChecker {
     }
 
     private static boolean isNumericAndThreeDigits(String input) {
-        if (!Pattern.matches("^[1-9]{3}$",input)){
-            return false;
+        if (Pattern.matches("^[1-9]{3}$",input)){
+            return true;
         }
-        return true;
+        return false;
     }
 
     private static boolean hasDuplicateDigits(String input){
         int length = input.chars()
-                .boxed()
-                .collect(Collectors.groupingBy(c -> c, Collectors.counting()))
-                .values()
+                .mapToObj(Integer::valueOf)
+                .collect(Collectors.toSet())
                 .size();
 
-        return length == 3 ? true : false;
+        return length == NUMBER_LENGTH ? true : false;
     }
 
     public static String retryRequest(){
