87fd7487f1939a2403bc395c03745f7f12f48b6b
heiler <threepebbles@naver.com>
2023-10-19 20:33:28 +0900

    feat(GameController): 3자리 난수 생성 메소드 추가

--- src/main/java/baseball/Controller/GameController.java
+++ src/main/java/baseball/Controller/GameController.java
@@ -1,11 +1,30 @@
 package baseball.Controller;
 
+import baseball.Model.Computer;
+import baseball.Model.NumberList;
 import baseball.View.OutputView;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameController {
     public void startGame() {
         OutputView.printStartGameMessage();
-        // TO DO: 게임 진행
+        Computer computer = new Computer();
+        computer.setAnswer(createRandomTarget());
+        // TO DO: 유저로부터 숫자 3자리를 받아 컴퓨터의 정답과 같을 때까지 비교
         OutputView.printEndGameMessage();
+    }
+
+    public NumberList createRandomTarget() {
+        List<Integer> answerList = new ArrayList<>();
+        while(answerList.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if(!answerList.contains(randomNumber)) {
+                answerList.add(randomNumber);
+            }
+        }
+        return new NumberList(answerList);
     }
 }
---
+++ src/main/java/baseball/Model/Computer.java
@@ -0,0 +1,13 @@
+package baseball.Model;
+
+public class Computer {
+    private NumberList answer;
+
+    public NumberList getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(NumberList answer) {
+        this.answer = answer;
+    }
+}
