5e30c8a91c136c597f880c053449e9aebaf60a0c
prao <pch5521@gmail.com>
2023-10-19 21:35:14 +0900

    feat: 심판 기능 구현
    - computerNumbers와 userNumbers를 전달받아 ball, strike, nothing을 판정
    - judgement를 형식에 맞게끔 getJudgement 구현

---
+++ src/main/java/baseball/domain/Referee.java
@@ -0,0 +1,55 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Referee {
+
+    private int ball = 0;
+    private int strike = 0;
+    private String judgement = "";
+
+    public Referee(List<Integer> computerNumbers, List<Integer> userNumbers) {
+        judge(computerNumbers, userNumbers);
+        judgement = result(ball, strike);
+    }
+
+    private String result(int ball, int strike) {
+        if (ball == 0 && strike == 0) {
+            return "낫싱";
+        }
+        return ball + "볼 " + strike + "스트라이크";
+    }
+
+    private void judge(List<Integer> computerNumbers, List<Integer> userNumbers) {
+        for (int i = 0; i < userNumbers.size(); i++) {
+            int computerNumber = computerNumbers.get(i);
+            int userNumber = userNumbers.get(i);
+            if (isStrike(computerNumber, userNumber)) {
+                strike += 1;
+            }
+            if (!isStrike(computerNumber, userNumber) && isBall(computerNumbers, userNumber)) {
+                ball += 1;
+            }
+        }
+    }
+
+    private boolean isStrike(int computerNumber, int userNumber) {
+        return computerNumber == userNumber;
+    }
+
+    private boolean isBall(List<Integer> computerNumbers, int userNumber) {
+        return computerNumbers.contains(userNumber);
+    }
+
+    public int getBall() {
+        return ball;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public String getJudgement() {
+        return judgement;
+    }
+}
