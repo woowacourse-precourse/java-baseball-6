4a24702919d51f99f0a8525e6377ff0ad1e1ec62
m5s3 <devmyoungsoo2@gmail.com>
2023-10-19 21:16:14 +0900

    feat: 입력 받은 수 출력 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
+import java.util.Scanner;
+
 public class Application {
+    static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        playGame();
+    }
+
+    public static void playGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        String number = requestNumber();
+    }
+
+    public static String requestNumber() {
+        System.out.print("숫자를 입력해 주세요 : ");
+        String input = scanner.nextLine();
+        return input;
     }
 }
