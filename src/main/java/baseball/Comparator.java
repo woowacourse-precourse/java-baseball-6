384fa8f5676da093418a41f3c0dbf0901d993374
Gukin Han <gukin.dev@gmail.com>
2023-10-19 18:57:27 +0900

    Feat: 유저값과 비교 기능

---
+++ src/main/java/baseball/Comparator.java
@@ -0,0 +1,45 @@
+package baseball;
+
+import java.util.List;
+
+public class Comparator {
+
+    private int ballCount;
+    private int strikeCount;
+    private boolean[] isStrike;
+
+    private void init(int digit) {
+        this.ballCount = 0;
+        this.strikeCount = 0;
+        this.isStrike = new boolean[digit];
+    }
+
+    public boolean compare(List<Integer> generatedNumbers, List<Integer> userInput) {
+        int digit = generatedNumbers.size();
+        init(digit);
+
+        // check strike position
+        for (int i = 0; i < digit; i++) {
+            if (generatedNumbers.get(i) == userInput.get(i)) {
+                strikeCount++;
+                isStrike[i] = true;
+            }
+        }
+
+        for (int i = 0; i < digit; i++) {
+            for (int j = 0; j < digit; j++) {
+                if (isStrike[i] || isStrike[j]) continue;
+                if (generatedNumbers.get(i) == userInput.get(j)) ballCount++;
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        if (ballCount != 0) result.append(ballCount).append("볼 ");
+        if (strikeCount != 0) result.append(strikeCount).append("스트라이크");
+        if (strikeCount == 0 && ballCount == 0) result.append("낫싱");
+
+        System.out.println(result.toString());
+
+        return strikeCount == 3;
+    }
+}
