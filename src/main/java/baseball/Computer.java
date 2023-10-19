126c8408125c6c4c2f1e5d0a81be4dc095c935e3
leeyeongjae <xingxing2001@cau.ac.kr>
2023-10-19 19:59:36 +0900

    numbers 및 setter메소드에 대한 주석 추가

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -4,13 +4,11 @@ import java.util.*;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Computer {
-    private ArrayList<Integer> numbers= new ArrayList<>(); //컴퓨터의 숫자 조합
+    private ArrayList<Integer> numbers= new ArrayList<>(); //컴퓨터가 선택한 서로 다른 숫자 조합
     Computer(){
-
     }
 
-    public void setNumbers(){
-        //3개의 난수 생성
+    public void setNumbers(){ //서로 다른 3개의 난수를 생성하는 setter 메소드
         while(this.numbers.size() <3){
             int randomNumber = Randoms.pickNumberInRange(1,9);
             if(!numbers.contains(randomNumber)){
