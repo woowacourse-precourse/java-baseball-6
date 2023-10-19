5c96b24fdbe7359b901117d909fe4f4030d47ca3
shinjaewon99 <tlswodnjs99@naver.com>
2023-10-19 19:45:08 +0900

    feat : 유저가 입력한 숫자 길이와 범위(타입)에 대한 예외 메시지상수 추가

---
+++ src/main/java/baseball/messageConstant/ErrorMessageConstant.java
@@ -0,0 +1,6 @@
+package baseball.messageConstant;
+
+public class ErrorMessageConstant {
+    public static final String PLAYER_INPUT_NUMBER_LENGTH_ERROR_MESSAGE = "3개의 숫자를 입력해야 합니다.";
+    public static final String PLAYER_INPUT_NUMBER_RANGE_ERROR_MESSAGE = "1 ~ 9 사이의 자연수를 입력해야 합니다.";
+}
