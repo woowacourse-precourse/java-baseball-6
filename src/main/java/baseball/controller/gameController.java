c7d1403797d25d03c4689e84dbc77af324d0fc10
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-10-19 21:37:31 +0900

    Feat : Controller 기능 추가 시작, 종료에 대한 기능 추가

---
+++ src/main/java/baseball/controller/gameController.java
@@ -0,0 +1,74 @@
+package baseball.controller;
+
+import baseball.model.CompareNumber;
+import baseball.model.ProgramNumber;
+import baseball.model.UserNumber;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+public class gameController {
+    CompareNumber compareNumber = new CompareNumber();
+    ProgramNumber program = new ProgramNumber();
+    private UserNumber user;
+
+
+    public void start(){
+        do{
+            checkUserNumber();
+            printAll(getResultList());
+        } while(!isEnd());
+    }
+
+    public boolean isEnd(){
+        if (compareNumber.isThreeStrike()) {
+            OutputView.printEnd();
+            return true;
+        }
+        return false;
+    }
+
+    public int[] getResultList(){
+        return compareNumber.getStrikeBall(user.getUserNumber(), program.getProgramNumber());
+    }
+
+    public void checkUserNumber(){
+        //생성자에서 숫자 검증
+        user = new UserNumber(InputView.inputUserNumber());
+    }
+
+    public void printBallStrike(int[] count){
+        if (count[0] != 0 & count[1] != 0) { //볼, 스트라이크 모두 있다면
+            OutputView.printCnt(count[0]);
+            OutputView.printBallWithStrike();
+            OutputView.printCnt(count[1]);
+            OutputView.printStrike();
+        }
+    }
+
+    public void printBall(int[] count){
+        if (count[0] != 0 & count[1] == 0) {
+            OutputView.printCnt(count[0]);
+            OutputView.printBall();
+        }
+    }
+
+    public void printStrike(int[] count){
+        if (count[0] == 0 & count[1] == 0) {
+            OutputView.printCnt(count[1]);
+            OutputView.printStrike();
+        }
+    }
+
+    public void printNothing(int[] count){
+        if (count[0] == 0 & count[1] == 0) {
+            OutputView.printNothing();
+        }
+    }
+
+    public void printAll(int[] count){
+        printBall(count);
+        printBallStrike(count);
+        printStrike(count);
+        printNothing(count);
+    }
+}
