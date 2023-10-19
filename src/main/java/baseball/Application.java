ec7b1ac28d2462ccfd61f9a65a24b88d527b184e
waveofmymind <sjun0913@gmail.com>
2023-10-19 21:14:16 +0900

    feat: 상대방의 숫자를 랜덤으로 생성하도록 RandomNumberGenerator 객체를 주입해서 게임을 시작한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,8 @@
 package baseball;
 
+import baseball.application.RandomNumberGenerator;
+import baseball.controller.BaseballGame;
+
 public class Application {
     public static void main(String[] args) {
         BaseballGame baseballGame = new BaseballGame(new RandomNumberGenerator());
