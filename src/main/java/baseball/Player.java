b10ed86851793bac95f7e54be99b9a74c24543f1
1000zoo <cjswldn99@gmail.com>
2023-10-19 18:37:56 +0900

    feat(Player): 플레이어 기능 구현 완료
    - 게임 시작 시, 입력을 받아 컴퓨터의 수들과 비교
    - 게임이 끝나면 재시작 여부를 물음, playAgain 메서드

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Player {
 
     private Computer computer;
@@ -13,15 +15,34 @@ public class Player {
         playAgain();
     }
 
-    public void play() {
-        while (true) {
+    private void reset() {
+        computer = new Computer();
+    }
 
+    private void play() {
+        while (true) {
+            String input = readBallInput();
+            if (computer.compareInput(input)) {
+                break;
+            }
         }
     }
 
     private void playAgain() {
-        if (true) {
+        System.out.println(Constants.RESTART_OR_FINISH_MESSAGE);
+        String option = readOptionInput();
+        if (option.equals("1")) {
+            reset();
             start();
         }
+    }
+
+    private String readBallInput() {
+        System.out.println(Constants.INPUT_MESSAGE);
+        return Console.readLine();
+    }
+
+    private String readOptionInput() {
+        return Console.readLine();
     }
 }
