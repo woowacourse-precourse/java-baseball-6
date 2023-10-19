e28295d5b93f862b58191ca4f9b09229a946d411
wooteco-daram <wooteco.daram@gmail.com>
2023-10-20 03:56:30 +0900

    feat: 게임 시작 메시지 출력하는 `printStartMessage` 함수 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
 public class Application {
+
+    private static final String STRING_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        printStartMessage();
+    }
+
+    public static void printStartMessage() {
+        System.out.println(STRING_START_MESSAGE);
     }
 }
