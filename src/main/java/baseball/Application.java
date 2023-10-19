5ca1d40bfa1929e9f2efbb8825a01199e277eb5f
dgh06175 <dgh06175@gmail.com>
2023-10-19 20:36:59 +0900

    feat: 플레이어 숫자 입력 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,6 +3,6 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         NumberBaseballGame numberBaseballGame = new NumberBaseballGame();
-        numberBaseballGame.play();
+        numberBaseballGame.start();
     }
 }
--- src/main/java/baseball/NumberBaseballGame.java
+++ src/main/java/baseball/NumberBaseballGame.java
@@ -1,15 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class NumberBaseballGame {
     List<Integer> computer = new ArrayList<>(3);
 
-    public void play() {
+    public void start() {
         System.out.println("숫자 야구 게임을 시작합니다.\n");
         computer = generateRandomNumbers();
+        boolean isContinue = true;
+        while (isContinue) {
+            playGame();
+        }
+    }
+
+    private void playGame() {
+        List<Integer> user = new ArrayList<>();
+        user = userNumberInput();
+        System.out.println(user);
     }
 
     private List<Integer> generateRandomNumbers() {
@@ -21,5 +33,22 @@ public class NumberBaseballGame {
             }
         }
         return generatedNumbers;
+    }
+
+    private List<Integer> userNumberInput() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String inputString = Console.readLine();
+
+        validateUserInput(inputString); // 예외처리 미구현
+
+        return inputString.chars()
+                .map(Character::getNumericValue)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
+    // 예외처리 미구현
+    private boolean validateUserInput(String inputString) {
+        return true;
     }
 }
