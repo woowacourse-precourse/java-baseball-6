fe36bd68c2c85f79ae8304e5e9069bf28dd2e9b6
pjaehyun <show6756@gmail.com>
2023-10-19 18:16:08 +0900

    컴퓨터의 수를 만들어주는 함수 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,7 +9,27 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // 프로그램 시작
         System.out.println("숫자 야구 게임을 시작합니다.");
 
+        while(true) {
+            List<Integer> computer = getRandomNumbers();
+        }
+    }
+
+    /**
+     * 컴퓨터의 수를 생성
+     * @return 1~9까지의 숫자 3개가 담긴 리스트
+     */
+    public static List<Integer> getRandomNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < 3){
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if(!numbers.contains(randomNumber)){
+                numbers.add(randomNumber);
+            }
+        }
+        return numbers;
     }
 }
