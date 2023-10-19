a6cf97365ae58b65afbb0e784a9f07e4b8b9f694
anneu <b.imnuj@gmail.com>
2023-10-19 19:41:33 +0900

    feat: 숫자 입력 문구를 출력하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,5 +4,6 @@ public class Application {
     public static void main(String[] args) {
         Output output = new Output();
         output.showStartMessage();
+        output.showInputMessage();
     }
 }
--- src/main/java/baseball/Output.java
+++ src/main/java/baseball/Output.java
@@ -3,8 +3,13 @@ package baseball;
 public class Output {
 
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String NUMBER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
 
     public void showStartMessage() {
         System.out.println(GAME_START_MESSAGE);
+    }
+
+    public void showInputMessage() {
+        System.out.print(NUMBER_INPUT_MESSAGE);
     }
 }
