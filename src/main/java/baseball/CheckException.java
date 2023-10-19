59bdb6975ee2bd099a14ca066afd93f30407d248
yang <ywj9811@gmail.com>
2023-10-20 01:42:49 +0900

    feat : 예외 처리 로직 분리

---
+++ src/main/java/baseball/CheckException.java
@@ -0,0 +1,30 @@
+package baseball;
+
+public class CheckException {
+    public void checkAll(String[] inputNumbers) {
+        checkSize(inputNumbers);
+        checkZero(inputNumbers);
+        checkDup(inputNumbers);
+    }
+
+    private void checkSize(String[] inputNumbers) {
+        if (inputNumbers.length > 3)
+            throw new IllegalArgumentException();
+    }
+
+    private void checkZero(String[] inputNumbers) {
+        for (String number : inputNumbers) {
+            if (number.equals("0"))
+                throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkDup(String[] inputNumbers) {
+        if (inputNumbers[0].equals(inputNumbers[1]))
+            throw new IllegalArgumentException();
+        if (inputNumbers[1].equals(inputNumbers[2]))
+            throw new IllegalArgumentException();
+        if (inputNumbers[0].equals(inputNumbers[2]))
+            throw new IllegalArgumentException();
+    }
+}
