c459b196dcd25ccd5d147d18d769b95eb7f177f1
cloudchamb3r <jizon0123@protonmail.com>
2023-10-20 00:26:24 +0900

    add implementation of utils

--- src/main/java/baseball/utils/IoHelper.java
+++ src/main/java/baseball/utils/IoHelper.java
@@ -1,2 +1,26 @@
-package baseball.utils;public class IoHelper {
+package baseball.utils;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class IoHelper {
+    public static void print(String message) {
+        System.out.print(message);
+    }
+
+    public static void println(String message) {
+        print(message + "\n");
+    }
+
+    public static String prompt(String message) {
+        print(message);
+        return Console.readLine();
+    }
+
+    public static String promptColon(String message) {
+        return prompt(message + " : ");
+    }
+
+    public static String promptln(String message) {
+        return prompt(message + "\n");
+    }
 }
--- src/main/java/baseball/utils/RandomGenerator.java
+++ src/main/java/baseball/utils/RandomGenerator.java
@@ -1,2 +1,22 @@
-package baseball.utils;public class RandomGenerator {
+package baseball.utils;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomGenerator {
+    public static List<Integer> generateUniqueRandomListOf(int size) {
+        if (size < 0 || size > 9){
+            throw new IllegalArgumentException();
+        }
+
+        final List<Integer> ret = new ArrayList<>();
+        while (ret.size() < size) {
+            var rnd = Randoms.pickNumberInRange(1, 9);
+            if (!ret.contains(rnd)) {
+                ret.add(rnd);
+            }
+        }
+        return ret;
+    }
 }
