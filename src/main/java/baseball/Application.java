9bec77e9a4d5b076c53090a0436cb56fcae27851
bitesnail <rkwoal216@gmail.com>
2023-10-19 22:25:32 +0900

    feat: implementation of id-2, id-7

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    static void gameStart(){
+        Computer.setRandomNumber();
+        do{
+            User.readNumber();
+            System.out.println(Computer.getHint(User.getUserNumber()));
+        }while(!Computer.isCorrect());
+    }
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
+        boolean isContinue = true;
+        while(isContinue){
+            gameStart();
+            String input = Console.readLine();
+            if(input.equals("2")){
+                isContinue = false;
+            }
+        }
     }
 }
