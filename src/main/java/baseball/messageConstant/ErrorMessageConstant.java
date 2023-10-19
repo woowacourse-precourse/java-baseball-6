51ca1312791ca3a5958707afd7e319319287c2db
shinjaewon99 <tlswodnjs99@naver.com>
2023-10-19 20:52:52 +0900

    feat : 유저가 입력한 숫자의 중복에 대한 예외 메시지상수 추가

--- src/main/java/baseball/messageConstant/ErrorMessageConstant.java
+++ src/main/java/baseball/messageConstant/ErrorMessageConstant.java
@@ -3,4 +3,5 @@ package baseball.messageConstant;
 public class ErrorMessageConstant {
     public static final String PLAYER_INPUT_NUMBER_LENGTH_ERROR_MESSAGE = "3개의 숫자를 입력해야 합니다.";
     public static final String PLAYER_INPUT_NUMBER_RANGE_ERROR_MESSAGE = "1 ~ 9 사이의 자연수를 입력해야 합니다.";
+    public static final String PLAYER_INPUT_NUMBER_DUPLICATE_ERROR_MESSAGE = "중복되지 않은 숫자를 입력해야 합니다.";
 }
