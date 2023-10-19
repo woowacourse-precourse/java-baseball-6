38eb5edbc61da6acf6b1a09b8ddb0870ce9ed25c
Hyeon0208 <dmsgktn0208@gmail.com>
2023-10-19 21:34:44 +0900

    feat : 정규식 값을 갖는 Enum 클래스 추가

---
+++ src/main/java/baseball/validation/RegexPattern.java
@@ -0,0 +1,17 @@
+package baseball.validation;
+
+public enum RegexPattern {
+    ONLY_NUMBER("^[0-9]*$"),
+    ONE_TO_NINE("[1-9]+");
+
+
+    private final String value;
+
+    RegexPattern(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
