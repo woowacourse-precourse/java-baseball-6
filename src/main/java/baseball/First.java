429f9f3bcd92922c6f56fd7b42b1f864bcb985a4
rlagudwls5518 <66249379+rlagudwls5518@users.noreply.github.com>
2023-10-20 00:07:18 +0900

    수정수정수정

--- src/main/java/baseball/First.java
+++ src/main/java/baseball/First.java
@@ -5,6 +5,8 @@ public class First {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("된거냐");
+		System.out.println("된거냐");
+		System.out.println("된거냐");
 
 	}
 
