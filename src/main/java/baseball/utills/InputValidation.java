c43d97f7e879184213cb3e63a8102c1707b1e93b
pwrwpw <dlgustjr9912@gmail.com>
2023-10-19 22:04:41 +0900

    feat(InputValidation): 입력 데이터가 1 ~ 9사이의 숫자인지 체크 숫자가 아니거나 범위에 해당하는 숫자가 아니라면 오류 발생후 애플리케이션을 종료한다. docs: 1-9까지의 수인지 검증 완료 체크

--- src/main/java/baseball/utills/InputValidation.java
+++ src/main/java/baseball/utills/InputValidation.java
@@ -4,15 +4,21 @@ import java.util.List;
 
 public class InputValidation {
 	private static final int NUMBER_LENGTH = 3;
-	private static final String ERROR_MESSAGE = "잘못된 입력입니다.";
-
+	private static final String WRONG_INPUT_LENGTH_MESSAGE = "세 자리만 입력 가능합니다.";
+	private static final String WRONG_INPUT_RANGE_MESSAGE = "1~9 사이의 숫자만 입력 가능합니다.";
 	public void validate(String inputNumber){
 		if(!hasExactLength(inputNumber)){
-			throw new IllegalArgumentException(ERROR_MESSAGE);
+			throw new IllegalArgumentException(WRONG_INPUT_LENGTH_MESSAGE);
+		}
+		if(!hasExactRange(inputNumber)){
+			throw new IllegalArgumentException(WRONG_INPUT_RANGE_MESSAGE);
 		}
 	}
 
 	public boolean hasExactLength(String inputNumber) {
 		return inputNumber.length() == NUMBER_LENGTH;
+	}
+	public boolean hasExactRange(String inputNumber) {
+		return inputNumber.chars().allMatch(Character::isDigit);
 	}
 }
