d2d399980b15427834b1c441a5af5c3b7dac0337
Hugh <lee2317884@gmail.com>
2023-10-19 20:28:16 +0900

    feat: '숫자를 입력해주세요 : ' 출력

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,4 +1,14 @@
 package baseball.view;
 
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+	private static final String INPUT_GAME_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+
+	public static String printInputGameNumberMessage() {
+		System.out.print(INPUT_GAME_NUMBER_MESSAGE);
+		return Console.readLine();
+	}
 }
