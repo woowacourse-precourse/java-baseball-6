23153369340038f05b5b53dc83cd23318e5a3946
sami355-24 <ddung355@gmail.com>
2023-10-20 00:11:57 +0900

    fix : 논리적 오류로 인해 프로그램의 시작점 수정
    프로그램의 시작할때 사용자로부터 값을 입력받고 게임을 시작하는것이 아닌 게임을 시작하고 입력을 받도록 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,7 @@
 package baseball;
 
-import baseball.Input.InputAnswer;
-import baseball.Input.InputAnswerImpl;
+import baseball.Game.BaseBallGame;
+import baseball.Game.BaseBallGameImpl;
 
 import static java.lang.System.*;
 
@@ -9,12 +9,12 @@ import static java.lang.System.*;
 public class Application {
 
     private static final String startupMessage = "숫자 야구 게임을 시작합니다.";
-    private static final InputAnswer inputAnswer = new InputAnswerImpl();
+    private static final BaseBallGame baseBallGame = new BaseBallGameImpl();
 
     public static void main(String[] args) {
         out.println(startupMessage);
         try{
-            inputAnswer.inputAnswer();
+            baseBallGame.initGame();
         } catch (IllegalArgumentException e) {
             out.println(e.getMessage());
             throw e;
