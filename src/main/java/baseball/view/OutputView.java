11cd33fb2cecbcab7ce560d1ec91cf2c3ddafd56
gusdn0108 <gusdn6671@naver.com>
2023-10-19 21:59:03 +0900

    feat(OutputView) : 숫자 야구게임 숫자 입력하기 출력

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -8,4 +8,8 @@ public class OutputView {
     public static void printStartGame(){
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
+    public static void printInputNumber(){
+        System.out.print("숫자를 입력해주세요 :");
+    }
 }
