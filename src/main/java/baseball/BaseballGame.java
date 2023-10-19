3c57947e0d542f9b64af64065237f02074a554a9
aka-nick <bechalrang@gmail.com>
2023-10-19 17:34:22 +0900

    추가: 야구숫자를 입력해달라는 메시지 출력 기능 구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -5,5 +5,10 @@ public class BaseballGame {
     public void play() {
         WinningNumber winningNumber = new WinningNumber();
 
+        while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
+
+        }
     }
+
 }
