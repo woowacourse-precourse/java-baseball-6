b19670562dd910798f51cd3b6f2604fbf31f7326
gyungchan Jo <namejgc@naver.com>
2023-10-20 00:27:24 +0900

    feat(OutputView): 입력한 수에 대한 결과를 출력하는 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,6 +3,10 @@ package baseball.view;
 public class OutputView {
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String NOTHING = "낫싱";
+    private static final String BALL = "볼";
+    private static final String STRIKE = "스트라이크";
+    private static final int ZERO = 0;
 
     public void printStartMessage() {
         System.out.println(GAME_START_MESSAGE);
@@ -10,5 +14,30 @@ public class OutputView {
 
     public void printEndMessage() {
         System.out.println(GAME_END_MESSAGE);
+    }
+
+    public void printResult(int ball, int strike) {
+        String resultMessage = generateResultMessage(ball, strike).trim();
+
+        System.out.println(resultMessage);
+    }
+
+    private String generateResultMessage(int ball, int strike) {
+        if (isNothing(ball, strike)) {
+            return NOTHING;
+        }
+        return String.format("%s %s", generateBallMessage(ball), generateStrikeMessage(strike));
+    }
+
+    private boolean isNothing(int ball, int strike) {
+        return ball == ZERO && strike == ZERO;
+    }
+
+    private String generateBallMessage(int ball) {
+        return ball + BALL;
+    }
+
+    private String generateStrikeMessage(int strike) {
+        return strike + STRIKE;
     }
 }
