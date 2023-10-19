fa1179f62bd13f209eb809c570f6f8031ab12328
ttalkisyrup <ttalkisyrup@gmail.com>
2023-10-19 23:37:23 +0900

    feat: 게임 한 번을 지속적으로 수행하도록 반복문 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,28 +7,31 @@ public class Application {
         BaseballGame baseballGame = new BaseballGame();
 
         System.out.println("숫자 야구 게임을 시작합니다.");
-        System.out.print("숫자를 입력해주세요 : ");
-        String userInput = readLine();
-        int ballCount = baseballGame.getBallCount(userInput);
-        int strikeCount = baseballGame.getStrikeCount(userInput);
+        boolean isDone = false;
+        while (!isDone) {
+            System.out.print("숫자를 입력해주세요 : ");
+            String userInput = readLine();
+            int ballCount = baseballGame.getBallCount(userInput);
+            int strikeCount = baseballGame.getStrikeCount(userInput);
 
-        StringBuilder countMessage = new StringBuilder();
-        if (ballCount == 0 && strikeCount == 0) {
-            System.out.println("낫싱");
-        }
+            StringBuilder countMessage = new StringBuilder();
+            if (ballCount == 0 && strikeCount == 0) {
+                System.out.println("낫싱");
+            }
 
-        if(ballCount != 0) {
-            countMessage.append(String.format("%d볼", ballCount));
-            if(strikeCount != 0) countMessage.append(" ");
-        }
-        if(strikeCount != 0) {
-            countMessage.append(String.format("%d스트라이크", strikeCount));
-        }
+            if(ballCount != 0) {
+                countMessage.append(String.format("%d볼", ballCount));
+                if(strikeCount != 0) countMessage.append(" ");
+            }
+            if(strikeCount != 0) {
+                countMessage.append(String.format("%d스트라이크", strikeCount));
+            }
 
-        System.out.printf(countMessage.toString());
-        if(strikeCount == 3){
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println(countMessage.toString());
+            if(strikeCount == 3){
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                isDone = true;
+            }
         }
-
     }
 }
