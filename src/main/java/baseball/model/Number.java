0d843f96817b222749f62351e51bc72e8c5f306f
koomchang <leahpar0401@gmail.com>
2023-10-19 23:42:48 +0900

    feat: 숫자 하나에 대한 Number 클래스를 값 객체로 생성

---
+++ src/main/java/baseball/model/Number.java
@@ -0,0 +1,29 @@
+package baseball.model;
+
+import java.util.Objects;
+
+public class Number {
+
+    private final int value;
+
+    public Number(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Number number = (Number) o;
+        return value == number.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
