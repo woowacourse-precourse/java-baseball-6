01ca5ed86d7cf666fc0c55a73cc2aa6f8c6db55f
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-10-19 21:57:02 +0900

    test : Computer 기능 테스트 구현 - Computer Strike, ball 개수 올바르게 오는지 테스트 - Computer Model 에서 Comupter로 오타 돼있는 부분 수정

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Comuputer {
+public class Computer {
     private List<Integer> computerNumberList;
-    public Comuputer(List<Integer> computerNumberList){
+
+    public Computer(List<Integer> computerNumberList){
         this.computerNumberList = computerNumberList;
     }
+
     public List<Integer> compareNumberList(List<Integer> userNumberList){
         int ballCount = 0;
         int strikeCount = 0;
@@ -21,12 +23,13 @@ public class Comuputer {
                 ballCount += 1;
             }
         }
-        return new ArrayList<>(Arrays.asList(strikeCount,ballCount));
+        return new ArrayList<>(Arrays.asList(ballCount,strikeCount));
     }
 
     private static boolean checkStrinkeNumber(int comuputerNumber, int userNumber){
         return comuputerNumber == userNumber;
     }
+
     private boolean checkBallNumber(int userNumber){
         return this.computerNumberList.contains(userNumber);
     }
