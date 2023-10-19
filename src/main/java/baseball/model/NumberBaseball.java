973bc5c32220a292f7e00a1e654e767d2b8301ff
박성춘 <songchon100@gmail.com>
2023-10-19 20:07:21 +0900

    faet : 컴퓨터의 랜덤 수 생성하는 generateComputerNum() 메서드 생성

--- src/main/java/baseball/model/NumberBaseball.java
+++ src/main/java/baseball/model/NumberBaseball.java
@@ -1,10 +1,24 @@
 package baseball.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class NumberBaseball {
     // 더 좋은 변수명 생각해보기
-    private List<Integer> randNum = new ArrayList<>();
+    private List<Integer> computerNum = new ArrayList<>();
 
+    public NumberBaseball() {
+        generateComputerNum();
+    }
+
+    private void generateComputerNum() {
+        while(computerNum.size() < 3) {
+            int randNumber = Randoms.pickNumberInRange(1, 9);
+            if(!computerNum.contains(randNumber)) {
+                computerNum.add(randNumber);
+            }
+        }
+    }
 }
