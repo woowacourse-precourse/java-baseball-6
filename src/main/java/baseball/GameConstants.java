a2484f715650517d17261225d7fd649dcd7a4b0a
hyunjin <guswlsdl0121@gmail.com>
2023-10-20 01:20:16 +0900

    feat: 상수 클래스 정의

---
+++ src/main/java/baseball/GameConstants.java
@@ -0,0 +1,8 @@
+package baseball;
+
+public class GameConstants {
+    public static final int RESTART_GAME = 1;
+    public static final int END_GAME = 2;
+    public static final int THREE_STRIKE = 3;
+    public static final int LIST_SIZE = 3;
+}
