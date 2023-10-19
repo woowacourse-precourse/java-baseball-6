aa6707960a35b982b200660a7a6e020122891083
bellring <starringbell@gmail.com>
2023-10-19 22:53:48 +0900

    Feat : 유니크한 랜덤 숫자 3개를 컴퓨터에 할당

---
+++ src/main/java/baseball/model/CreateComputerNumber.java
@@ -0,0 +1,26 @@
+package baseball.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateComputerNumber {
+
+    private List<Integer> computerNumber;
+
+    public List<Integer> getComputerNumber() {
+        return computerNumber;
+    }
+
+    public void generateRandomNumbers() {
+        List<Integer> numbers = new ArrayList<>(3);
+        while (numbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (numbers.contains(randomNumber)) {
+                continue;
+            }
+            numbers.add(randomNumber);
+        }
+        this.computerNumber = numbers;
+    }
+}
---
+++ src/test/java/baseball/model/CreateComputerNumberTest.java
@@ -0,0 +1,39 @@
+package baseball.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class CreateComputerNumberTest {
+    static List<Integer> computerNumbers;
+
+    @BeforeAll
+    static void init() {
+        CreateComputerNumber createComputerNumber = new CreateComputerNumber();
+        createComputerNumber.generateRandomNumbers();
+        computerNumbers = createComputerNumber.getComputerNumber();
+    }
+
+    @Test
+    void 총_3개의_숫자_생성() {
+        assertEquals(3, computerNumbers.size());
+    }
+
+    @Test
+    void 생성된_숫자가_모두_다른_숫자() {
+        Set<Integer> uniqueNumbers = new HashSet<>(computerNumbers);
+        assertEquals(3, uniqueNumbers.size());
+    }
+
+    @Test
+    void 숫자가_1과_9사이() {
+        for (int num : computerNumbers) {
+            assertTrue(num >= 1 && num <= 9);
+        }
+    }
+}
