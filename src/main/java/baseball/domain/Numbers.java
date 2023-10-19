c9d5a48122e0bfc697771a81cbe38f04d32ef234
Jeon HoGeon <jhg2819@naver.com>
2023-10-19 20:12:03 +0900

    feat: 입력 숫자 검증 기능 구현

--- src/main/java/baseball/domain/Numbers.java
+++ src/main/java/baseball/domain/Numbers.java
@@ -3,19 +3,34 @@ package baseball.domain;
 import java.util.List;
 
 public class Numbers {
-    private List<Integer> numberList;
 
-    private Numbers() {
-    }
+    public static final Integer MAX_NUMBER = 9;
+    public static final Integer MIN_NUMBER = 1;
+    public static final Integer NUMBER_COUNT = 3;
+    private final List<Integer> numberList;
 
-    public static Numbers create(List<Integer> numberList) {
-        Numbers numbers  = new Numbers();
-        numbers.setNumberList(numberList);
-
-        return  numbers;
-    }
-
-    private void setNumberList(List<Integer> numberList) {
+    public Numbers(List<Integer> numberList) {
+        validateNumberSize(numberList);
+        validateNumberRange(numberList);
         this.numberList = numberList;
     }
+
+    private void validateNumberSize(List<Integer> numberList) {
+        if (numberList.size() != NUMBER_COUNT) {
+            throw new IllegalArgumentException("[ERROR] 숫자 수량이 잘못되었습니다.");
+        }
+    }
+
+    private void validateNumberRange(List<Integer> numberList) {
+        for (Integer number : numberList) {
+            if (number < MIN_NUMBER || number > MAX_NUMBER) {
+                throw new IllegalArgumentException("[ERROR] 범위가 잘못되었습니다.");
+            }
+        }
+    }
+
+    public List<Integer> getNumberList() {
+        return numberList;
+    }
+
 }
