7836667a4707f567a2e28978bade15f2e5916e99
Taehyoun Kim <kth9903@gmail.com>
2023-10-20 01:18:09 +0900

    feat(input): add user input method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -7,14 +8,16 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
+        //(게임 시작 문구 출력)
         System.out.println("숫자 야구 게임을 시작합니다");
         //random number generation for computer
         List<Integer> computerNumber = generateComputerNumber();
         while(true){ //running game
-
+            List<Integer> unserGuess = getUserInput();
         }
 
     }
+
 
     private static List<Integer> generateComputerNumber() {
         List<Integer> computer = new ArrayList<>();
@@ -24,5 +27,16 @@ public class Application {
                 computer.add(randomNumber);
         }
         return computer;
+    }
+
+    //user input
+    private static List<Integer> getUserInput() {
+        System.out.print("숫자를 입력해주세요: ");
+        String input = Console.readLine();
+        List<Integer> parseInput = new ArrayList<>();
+        for(char ch : input.toCharArray()) {
+            parseInput.add(ch - '0'); //char to integer
+        }
+        return parseInput;
     }
 }
