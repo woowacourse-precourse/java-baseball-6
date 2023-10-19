7be1735cf8b22199d1265e6e575d258cb0bea255
donghoony <aru0504@naver.com>
2023-10-20 02:05:06 +0900

    feat: 출력을 포함한 숫자야구 게임 흐름 구현

--- src/main/java/baseball/BallCount.java
+++ src/main/java/baseball/BallCount.java
@@ -7,6 +7,10 @@ import static baseball.GameClient.BALL_LENGTH;
  */
 public class BallCount {
 
+    private final String MESSAGE_BALL = "볼";
+    private final String MESSAGE_STRIKE = "스트라이크";
+    private final String MESSAGE_NOTHING = "낫싱";
+
     private int strike;
     private int ball;
 
@@ -20,5 +24,26 @@ public class BallCount {
 
     public boolean isOut() {
         return this.strike == BALL_LENGTH;
+    }
+
+    private boolean isNothing() {
+        return this.strike == 0 && this.ball == 0;
+    }
+
+    @Override
+    public String toString() {
+        if (this.isNothing()) {
+            return MESSAGE_NOTHING;
+        }
+        if (this.ball > 0 && this.strike > 0) {
+            return String.format("%d%s %d%s", this.ball, MESSAGE_BALL, this.strike, MESSAGE_STRIKE);
+        }
+        if (this.ball > 0) {
+            return String.format("%d%s", this.ball, MESSAGE_BALL);
+        }
+        if (this.strike > 0) {
+            return String.format("%d%s", this.strike, MESSAGE_STRIKE);
+        }
+        return "";
     }
 }
--- src/main/java/baseball/GameClient.java
+++ src/main/java/baseball/GameClient.java
@@ -1,5 +1,10 @@
 package baseball;
 
+import static baseball.PlayerInput.NEW_GAME;
+import static baseball.PlayerInput.QUIT_GAME;
+
+import java.util.List;
+
 /**
  * 숫자야구 게임의 전반적인 진행을 담당하는 클래스.
  * <p>게임 진행을 담당한다. 전체적인 게임의 흐름을 구현하는 클래스이다.
@@ -8,4 +13,42 @@ public class GameClient {
     public static final int BALL_LENGTH = 3;
     public static final int RANGE_MINIMUM_VALUE = 1;
     public static final int RANGE_MAXIMUM_VALUE = 9;
+
+    private final String MESSAGE_INIT = "숫자 야구 게임을 시작합니다.";
+    private final String MESSAGE_PLAYER_INPUT = "숫자를 입력해주세요 : ";
+    private final String MESSAGE_GAME_FINISHED_FORMAT = "%d개의 숫자를 모두 맞히셨습니다! 게임 종료\n";
+    private final String MESSAGE_REPLAY_FORMAT = "게임을 새로 시작하려면 %d, 종료하려면 %d를 입력하세요.\n";
+
+    private final PlayerInput playerInput;
+    private final Computer computer;
+    private boolean isFirstTimePlaying = true;
+
+    public GameClient() {
+        this.playerInput = new PlayerInput();
+        this.computer = new Computer();
+    }
+
+    public void playBaseball() {
+        if (isFirstTimePlaying) {
+            System.out.println(MESSAGE_INIT);
+            isFirstTimePlaying = false;
+        }
+
+        BallCount ballCount;
+        computer.refreshAnswer();
+
+        do {
+            System.out.print(MESSAGE_PLAYER_INPUT);
+            List<Integer> playerNumbers = playerInput.getPlayerNumbers();
+            ballCount = computer.calculateBallCount(playerNumbers);
+            System.out.println(ballCount);
+        } while (!ballCount.isOut());
+
+        System.out.printf(MESSAGE_GAME_FINISHED_FORMAT, BALL_LENGTH);
+    }
+
+    public boolean askForReplay() {
+        System.out.printf(MESSAGE_REPLAY_FORMAT, NEW_GAME, QUIT_GAME);
+        return playerInput.getMenuNumber() == NEW_GAME;
+    }
 }
