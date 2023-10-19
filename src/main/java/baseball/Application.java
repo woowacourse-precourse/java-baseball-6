3b343ff7b766b276cbbb58be780b0530616992a2
wns312 <wns312@gmail.com>
2023-10-19 18:04:56 +0900

    fix: 입력 숫자 검증에 누락된 숫자 길이 검증 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -14,6 +14,9 @@ class GameNumber {
     }
 
     private void validate(List<Integer> numbers) {
+        if (numbers.size() != 3) {
+            throw new IllegalArgumentException();
+        }
         List<Integer> compare = new ArrayList<>();
         for (Integer number: numbers) {
             if (compare.contains(number)) {
