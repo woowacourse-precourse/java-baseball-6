a4608b38b38f963671a769b6c5dea89128a856c2
02ggang9 <02ggang9@gmail.com>
2023-10-19 21:55:40 +0900

    feat(NumberBaseball): 랜덤한 숫자들을 생성하는 기능 구현

--- src/main/java/entity/NumberBaseball.java
+++ src/main/java/entity/NumberBaseball.java
@@ -1,8 +1,29 @@
 package entity;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class NumberBaseball {
 
     private final String numbers;
+    private final String randomNumber = generateRandomNumber();
+
+    private String generateRandomNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        return computer.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+    }
 
     public NumberBaseball(String numbers) {
         this.numbers = numbers;
