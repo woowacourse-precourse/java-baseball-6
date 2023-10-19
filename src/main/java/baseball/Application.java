02aa1e28dcc0f85a8acba944dc07f23dbc3e42b2
kimminsu <xjfcnfw3@naver.com>
2023-10-19 22:41:59 +0900

    feat: 숫자 입력 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,12 @@
 package baseball;
 
 import baseball.game.BaseballGame;
+import baseball.view.InputView;
 import baseball.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        BaseballGame baseballGame = new BaseballGame(new OutputView());
+        BaseballGame baseballGame = new BaseballGame(new InputView(), new OutputView());
         baseballGame.start();
     }
 }
--- src/main/java/baseball/game/BaseballGame.java
+++ src/main/java/baseball/game/BaseballGame.java
@@ -1,12 +1,19 @@
 package baseball.game;
 
+import baseball.game.validate.NumberValidation;
+import baseball.view.InputView;
 import baseball.view.OutputView;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class BaseballGame {
 
     private OutputView outputView;
+    private InputView inputView;
 
-    public BaseballGame(OutputView outputView) {
+    public BaseballGame(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
         this.outputView = outputView;
     }
 
@@ -15,10 +22,22 @@ public class BaseballGame {
     }
 
     public void process() {
-        outputView.showInputNumberMessage();
+        boolean running = true;
+
+        while (running) {
+            outputView.showInputNumberMessage();
+            String number = inputView.inputNumber();
+            List<Integer> numbers = toNumbers(number);
+            running = false;
+        }
     }
 
     public void checkRestart() {
         outputView.showOptionMenu();
+    }
+
+    private List<Integer> toNumbers(String number) {
+        NumberValidation.validate(number);
+        return Arrays.stream(number.split("")).map(Integer::parseInt).collect(Collectors.toList());
     }
 }
