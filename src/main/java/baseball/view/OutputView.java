16e420f841fde69f55a84fba9d4d75096840c0fa
박승민 <tmddudals369@naver.com>
2023-10-20 02:53:56 +0900

    feat(outputView): 입력을 위한 메세지를 출력하는 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -2,8 +2,13 @@ package baseball.view;
 
 public class OutputView {
     private final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private final String USER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
 
     public void printGameStartMessage() {
         System.out.println(GAME_START_MESSAGE);
+    }
+
+    public void printUserInputMessage() {
+        System.out.print(USER_INPUT_MESSAGE);
     }
 }
