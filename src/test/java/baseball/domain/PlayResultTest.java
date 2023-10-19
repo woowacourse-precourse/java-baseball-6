c6ab688deed3c4aa4a0451641f94b10abac494eb
gyungchan Jo <namejgc@naver.com>
2023-10-19 18:39:29 +0900

    test(PlayResult): 공의 비교 결과를 기록하는 기능 테스트

---
+++ src/test/java/baseball/domain/PlayResultTest.java
@@ -0,0 +1,22 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class PlayResultTest {
+
+    @Test
+    @DisplayName("공의 비교 결과를 기록할 수 있다.")
+    void record() {
+        PlayResult playResult = new PlayResult();
+
+        playResult.record(BallStatus.STRIKE);
+        playResult.record(BallStatus.STRIKE);
+        playResult.record(BallStatus.NOTHING);
+        playResult.record(BallStatus.BALL);
+
+        assertThat(playResult).isEqualTo(new PlayResult(2, 1));
+    }
+}
