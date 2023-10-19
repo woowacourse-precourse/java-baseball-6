e93a5430c7e243ac5e3512f696e5b5240f74f78d
wns312 <wns312@gmail.com>
2023-10-19 16:37:29 +0900

    feat: 유저 입력 변환 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,7 +2,6 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,17 +18,34 @@ public class Application {
         return computer;
     }
 
-    static void readUserNumbers() {
+    static int parseInt(String target) {
+        try {
+            return Integer.parseInt(target);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
+    }
+
+    static List<Integer> convertUserInput(String input) {
+        List<Integer> user = new ArrayList<>();
+        String[] numbers = input.split("");
+        for (String element: numbers) {
+            int number = parseInt(element);
+            user.add(number);
+        }
+        return user;
+    }
+
+    static List<Integer> readUserNumbers() {
         System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
-
-
+        return convertUserInput(input);
     }
 
     static void play() {
         System.out.println("숫자 야구 게임을 시작합니다.");
         List<Integer> computer = pickRandomNumbers();
-        readUserNumbers();
+        List<Integer> user = readUserNumbers();
     }
 
 
