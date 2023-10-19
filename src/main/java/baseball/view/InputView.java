49f2adcce0fbf96584cb67327f3967097e0f1626
waveofmymind <sjun0913@gmail.com>
2023-10-19 21:16:51 +0900

    feat: 입출력 요구사항에 맞춰 게임 시작 문구를 출력한다.

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -6,6 +6,8 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     public static Numbers readUserNumbers() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
 
         return new Numbers(input);
