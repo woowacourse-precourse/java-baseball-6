62f825a9b616758b1cbae3faaa301fd62d7adc5d
donghyunkim <ghffu405@gmail.com>
2023-10-19 20:01:49 +0900

    feat: 실행로직 추가

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -1,18 +1,72 @@
 package baseball.controller;
 
 import baseball.constant.NumberConstant;
+import baseball.domain.AnswerNumbers;
+import baseball.validation.Validator;
+import baseball.view.InputView;
+import baseball.view.OutputView;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static baseball.constant.NumberConstant.*;
 
 public class BaseballController {
 
+    private static int RESTART = 1;
+    private static int FINISH = 2;
+
+
+    private InputView inputView;
+    private OutputView outputView;
+    private Validator validator;
+
+    public BaseballController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.validator = new Validator();
+    }
+
     public void run(){
+
+        inputView.start();
+        boolean isFirstTime = true;
+        AnswerNumbers answerNumbers = null;
+        while(true){
+
+
+            if(isFirstTime){
+                answerNumbers = new AnswerNumbers(getAnswerNumbers());
+            }
+
+            String input = inputView.input();
+
+            validator.validateThreeNumber(input);
+
+            List<Integer> inputNumbers = mapToInListInteger(input);
+
+            validator.validateDuplicate(inputNumbers);
+
+            Map<Integer, Integer> result = answerNumbers.getBallNStrkie(inputNumbers);
+
+            outputView.printResult(result.get(BALL), result.get(STRIKE));
+
+            if(result.get(STRIKE) == SUCCESS_NUMBER){
+                int finishNum = Integer.parseInt(inputView.finish());
+
+                if(finishNum == RESTART){
+                    isFirstTime = true;
+                    continue;
+                }
+                return;
+            }
+
+            isFirstTime = false;
+        }
 
     }
 
@@ -24,6 +78,11 @@ public class BaseballController {
                 numbers.add(randomNumber);
             }
         }
+
+        for(Integer i : numbers){
+            System.out.print(i + " ");
+        }
+
         return numbers;
     }
 
