f88bdf839c9c949828c5e4a2079e9c4c63bcc351
SeoJiWon <sjiwon4491@gmail.com>
2023-10-19 21:01:21 +0900

    feat: 사용자 Baseball 입력 흐름 구현

--- src/main/java/baseball/application/GameManager.java
+++ src/main/java/baseball/application/GameManager.java
@@ -3,15 +3,34 @@ package baseball.application;
 import baseball.domain.Computer;
 import baseball.domain.GameProcessStatus;
 import baseball.domain.User;
+import baseball.io.InputProcessor;
+import baseball.io.OutputProcessor;
+
+import java.util.List;
 
 public class GameManager {
-    private Computer computer;
-    private User user;
+    private final Computer computer;
+    private final User user;
     private GameProcessStatus gameProcessStatus;
 
     public GameManager() {
         computer = Computer.init();
         user = new User();
         gameProcessStatus = GameProcessStatus.GAME_RUNNING;
+    }
+
+    public void run() {
+        // 게임 시작
+        OutputProcessor.printGameStart();
+
+        while (gameProcessStatus.isGameInProgress()) {
+            // Baseball 입력
+            readUserBaseballs();
+        }
+    }
+
+    private void readUserBaseballs() {
+        final List<Integer> balls = InputProcessor.readUserBaseballs();
+        user.applyBaseball(balls);
     }
 }
