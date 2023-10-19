b64a163072310a57b02ade7026080489df323ed8
donghyunkim <ghffu405@gmail.com>
2023-10-19 18:09:03 +0900

    feat: 출력내용 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -12,10 +12,16 @@ public class OutputView {
 
     public void printStrike(int strike){
         System.out.println(strike + "스트라이크");
+
+        if(strike==3){
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
     }
 
     public void printNothing(){
         System.out.println("낫싱");
     }
+
+
 
 }
