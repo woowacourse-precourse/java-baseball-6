a569056c9862f17af7690d6037f242dd4f58fb8d
Seonghwan <aiya0422@gmail.com>
2023-10-19 18:30:38 +0900

    FEAT: 계산 기능 countBall 구현

--- src/main/java/baseball/Calculator.java
+++ src/main/java/baseball/Calculator.java
@@ -15,4 +15,21 @@ public class Calculator {
 
         return count;
     }
+
+    public static int countBall(List<Integer> input, List<Integer> answer) {
+        int count = 0;
+
+        for (int i = 0; i < 3; i++) {
+            if (!answer.contains(input.get(i))) {
+                continue;
+            }
+            if (input.get(i).equals(answer.get(i))) {
+                continue;
+            }
+
+            count++;
+        }
+
+        return count;
+    }
 }
