7f76ffa0c7be9c56a39e484a20079089d8a5da58
choimung <103022503+choimung@users.noreply.github.com>
2023-10-19 23:26:57 +0900

    사용자의 값을 입력받는 기능 추가

---
+++ src/main/java/baseball/UserNumber.java
@@ -0,0 +1,47 @@
+package baseball;
+import camp.nextstep.edu.missionutils.Console;
+
+public class UserNumber {
+    private String userNumber;
+
+    public UserNumber() {
+        String userNumber = setUserNumber();
+        validateLength(userNumber);
+        validateNumber(userNumber);
+        validateDuplication(userNumber);
+        this.userNumber = userNumber;
+    }
+
+    public String setUserNumber(){
+        String input = Console.readLine();
+        return input;
+    }
+
+    public String getUserNumber() {
+        return userNumber;
+    }
+
+    public void validateLength(String userNumber){
+        if(userNumber.length() != 3){
+            throw new IllegalArgumentException("숫자는 3자리 입니다.");
+        }
+    }
+
+    public void validateNumber(String userNumber){
+        if(!userNumber.matches("^[1-9]*$")){
+           throw new IllegalArgumentException("1 ~ 9 까지의 숫자를 입력 해주세요.");
+        }
+    }
+
+    public void validateDuplication(String userNumber){
+        for(int i = 0; i < userNumber.length(); i++){
+            for(int j = i+1; j < userNumber.length(); j++){
+                if(userNumber.charAt(i) == userNumber.charAt(j)){
+                    throw new IllegalArgumentException("숫자가 중복될 순 없습니다.");
+                }
+            }
+        }
+    }
+
+
+}
