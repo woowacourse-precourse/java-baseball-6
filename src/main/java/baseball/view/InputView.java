a84a19212e550ba1d73b23d3b1cde060772c8b27
donghyunkim <ghffu405@gmail.com>
2023-10-19 18:17:08 +0900

    feat: 게임종료 메소드 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -15,4 +15,9 @@ public class InputView {
         return Console.readLine();
     }
 
+    public String finish(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return Console.readLine();
+    }
+
 }
