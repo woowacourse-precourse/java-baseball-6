931a4123b4ae5b7138ceb452e906f866b7b89fb2
anneu <b.imnuj@gmail.com>
2023-10-19 22:55:01 +0900

    feat: 플레이어 수가 3자리가 아닌 경우 예외 처리

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -5,10 +5,20 @@ import java.util.List;
 
 public class Player {
 
+    private static final int NUMBER_SIZE = 3;
+    private static final String NUMBER_SIZE_EXCEPTION = "%d자리 수만 가능합니다.";
+
     private List<Integer> numbers;
 
     public Player(String number) {
+        validateNumberSize(number);
         this.numbers = convert(number);
+    }
+
+    private void validateNumberSize(String number) {
+        if (number.length() != NUMBER_SIZE) {
+            throw new IllegalArgumentException(String.format(NUMBER_SIZE_EXCEPTION, NUMBER_SIZE));
+        }
     }
 
     private List<Integer> convert(String number) {
