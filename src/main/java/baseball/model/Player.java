2bd909111e753ff830582dbee58e3da6ff842468
HongChaa <jun761_1@naver.com>
2023-10-19 18:28:39 +0900

    feat(Player): setter 및 getter 기능 추가

--- src/main/java/baseball/model/Player.java
+++ src/main/java/baseball/model/Player.java
@@ -1,4 +1,16 @@
 package baseball.model;
 
+import java.util.List;
+
 public class Player {
+
+    private List<Integer> playerNumberList;
+
+    public List<Integer> getPlayerNumberList() {
+        return playerNumberList;
+    }
+
+    public void setPlayerNumberList(List<Integer> playerNumberList) {
+        this.playerNumberList = playerNumberList;
+    }
 }
