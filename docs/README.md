1fa74e04ac7b4f6e37b1e9fe41f555441c3c9343
newh08 <newh08@gmail.com>
2023-10-20 08:03:54 +0900

    Feat: BallResult 에 비교 연산을 위한 메서드 구현
    BallResult- 결과값을 우선순위에 따라 정렬할 수 있도록 비교 연산 메서드 구현

--- docs/README.md
+++ docs/README.md
@@ -2,7 +2,7 @@
 - [x] 볼의 숫자 위치 정보 비교 - BallPosition
 - [x] 볼의 숫자 정보 비교 - BallNumber
 - [x] 숫자 정보, 위치 정보를 가지고 볼을 비교 - Ball
-- [ ] 볼을 비교한 결과를 저장해 가장 높은 값(스트라이크> 볼 > 낫띵) 선택 - BallCompareResult
+- [x] 볼을 비교한 결과를 (스트라이크> 볼 > 낫띵) 우선순위로 비교하기 - BallResult
 - [ ] Ball 3개를 가져 볼, 스트라이크 수 계산 - Balls
 - [ ] 게임 결과를 저장 - GameResult
 - [ ] 랜덤한 수를 Balls 로 변환 - BallsGenerator
--- src/main/java/baseball/domain/result/BallResult.java
+++ src/main/java/baseball/domain/result/BallResult.java
@@ -1,5 +1,15 @@
 package baseball.domain.result;
 
-public enum BallResult {
-    STRIKE, BALL, NOTHING
+public enum BallResult{
+    STRIKE(3), BALL(2), NOTHING(1);
+
+    private final int priority;
+
+    BallResult(int priority) {
+        this.priority = priority;
+    }
+
+    public int compare(BallResult result) {
+        return Integer.compare(this.priority, result.priority);
+    }
 }
