03c6d6cd25a1bc28eb1a49706cb8755d417790bd
spiky-haired-Pyramid-head <qns0147@gmail.com>
2023-10-19 21:20:27 +0900

    feat: 사용자 입력에 대한 예외처리 추가

--- src/main/java/baseball/domain/User.java
+++ src/main/java/baseball/domain/User.java
@@ -18,18 +18,27 @@ public class User {
         System.out.print("숫자를 입력해주세요 : ");
         String baseBallNumberStr = Console.readLine();
 
+        validateBaseballNumber(baseBallNumberStr);
         convertCharToInteger(baseBallNumberStr);
 
     }
 
     private void convertCharToInteger(String baseBallNumberStr) {
         ArrayList<Integer> baseballNumbers = new ArrayList<>();
-        for (char baseballNumber : baseBallNumberStr.toCharArray()) {
-            if (Character.isDigit(baseballNumber))
+
+        for (char baseballNumber : baseBallNumberStr.toCharArray())
+            if (Character.isDigit(baseballNumber) && Character.getNumericValue(baseballNumber) != 0)
                 baseballNumbers.add(Character.getNumericValue(baseballNumber));
 
-            this.baseballNumberList = baseballNumbers;
-        }
+        if (baseballNumbers.size() != 3)
+            throw new IllegalArgumentException();
+
+        this.baseballNumberList = baseballNumbers;
+    }
+
+    private void validateBaseballNumber(String baseBallNumberStr) {
+        if (baseBallNumberStr.chars().distinct().count() != 3)
+            throw new IllegalArgumentException();
     }
 
 
