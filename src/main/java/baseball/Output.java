eea459b80cf08ba62c418ae15fda437e2abf2668
anneu <b.imnuj@gmail.com>
2023-10-19 19:04:26 +0900

    feat: 게임 시작 문구를 출력하는 기능 구현

---
+++ src/main/java/baseball/Output.java
@@ -0,0 +1,10 @@
+package baseball;
+
+public class Output {
+
+    private static final String GAME_START_MASSAGE = "숫자 야구 게임을 시작합니다.";
+
+    public void showStartMessage() {
+        System.out.println(GAME_START_MASSAGE);
+    }
+}
