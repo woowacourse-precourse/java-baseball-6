38309b58d0a208804da612b87f59f66caf64458d
박승민 <tmddudals369@naver.com>
2023-10-20 03:08:57 +0900

    fix(outputView): 상수에 static 키워드가 누락된 문제 수정

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,8 +1,8 @@
 package baseball.view;
 
 public class OutputView {
-    private final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
-    private final String USER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String USER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
 
     public void printGameStartMessage() {
         System.out.println(GAME_START_MESSAGE);
