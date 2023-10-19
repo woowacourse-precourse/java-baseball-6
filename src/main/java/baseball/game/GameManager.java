21b9045207ee4ce827cce4dce4b7ea6d00287ec1
JunTaeINC <juntea3523@naver.com>
2023-10-19 21:25:16 +0900

    feat : 힌트 출력 구현

--- src/main/java/baseball/game/GameManager.java
+++ src/main/java/baseball/game/GameManager.java
@@ -1,8 +1,28 @@
 package baseball.game;
 
+import static baseball.game.GameMessages.BALL;
+import static baseball.game.GameMessages.BALL_STRIKE;
+import static baseball.game.GameMessages.NOTHING;
+import static baseball.game.GameMessages.STRIKE;
+
 import java.util.List;
 
 public class GameManager {
+
+	public String giveHint(List<Integer> player, List<Integer> answer) {
+		int balls = countBall(player, answer);
+		int strikes = countStrike(player, answer);
+
+		if (balls > 0 && strikes > 0) {
+			return BALL_STRIKE.getMessage().formatted(balls, strikes);
+		} else if (balls > 0) {
+			return BALL.getMessage().formatted(balls);
+		} else if (strikes > 0) {
+			return STRIKE.getMessage().formatted(strikes);
+		} else {
+			return NOTHING.getMessage();
+		}
+	}
 
 	public int countBall(List<Integer> player, List<Integer> answer) {
 		int ball = 0;
