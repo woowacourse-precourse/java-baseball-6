4c35bad81cee4593cb49e3fecff9091239c31aef
cmong0516 <cmong0516@gmail.com>
2023-10-19 20:44:38 +0900

    refactor(Input): 출력 메시지 기능 수정
    userNumberInput
    String -> List<Integer>

--- src/main/java/baseball/io/Input.java
+++ src/main/java/baseball/io/Input.java
@@ -2,15 +2,17 @@ package baseball.io;
 
 import baseball.validator.UserInputValidator;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 
 public class Input {
 
-    public String userNumberInput() {
+    public List<Integer> userNumberInput() {
         String userNumberInput = Console.readLine();
         UserInputValidator.validateIsNumberAndRange(userNumberInput);
         UserInputValidator.validateLength(userNumberInput);
 
-        return userNumberInput;
+        return Arrays.stream(userNumberInput.split("")).map(Integer::parseInt).toList();
     }
 
     public String finishOrRestartInput() {
