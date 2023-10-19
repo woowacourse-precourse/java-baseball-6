741cdb33324493eac1ee38c028b17b82698bd34d
JisooPyo <audwl326@gmail.com>
2023-10-19 21:10:21 +0900

    feat: 입력값 validate 메서드 구현

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -12,7 +12,31 @@ public class BaseBallGame {
 
 	private void start() {
 		this.computerNumber = generateRandomNumber();
+
 		System.out.println("숫자 야구 게임을 시작합니다.");
+
+		String judgment = "";
+		while (!judgment.equals("3 스트라이크")) {
+			System.out.print("숫자를 입력해주세요 : ");
+			String myNumber = Console.readLine();
+			validateMyNumber(myNumber);
+		}
+
+	}
+
+	private void validateMyNumber(String myNumber) {
+		char[] arr = myNumber.toCharArray();
+		if (arr.length != 3) {
+			throw new IllegalArgumentException("3자리 숫자를 입력해야 합니다.");
+		}
+		if (arr[0] == arr[1] || arr[1] == arr[2] || arr[2] == arr[0]) {
+			throw new IllegalArgumentException("서로 다른 임의의 수 3개를 입력해야 합니다.");
+		}
+		if (!('1' <= arr[0] && arr[0] <= '9') ||
+				!('1' <= arr[1] && arr[1] <= '9') ||
+				!('1' <= arr[2] && arr[2] <= '9')) {
+			throw new IllegalArgumentException("숫자를 입력해야 합니다.");
+		}
 	}
 
 	// 1에서 9까지 서로 다른 임의의 수 3개를 선택하여 문자열로 출력합니다.
@@ -27,5 +51,6 @@ public class BaseBallGame {
 		}
 		return str;
 	}
+
 
 }
