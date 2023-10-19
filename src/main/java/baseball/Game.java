8d9777695385c00bd48fed018ee3da593aba60fb
GiJung <weeds2577@gmail.com>
2023-10-20 08:11:19 +0900

    feat: 게임 시작 메세지 출력 기능 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,2 +1,25 @@
-package baseball;public class Game {
+package baseball;
+
+public class Game {
+
+    private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+
+    private boolean exit = false;
+
+    public void Game() {}
+
+    public void gameStart() {
+
+        while(!exit) {
+            startMessage();
+
+            return;
+        }
+    }
+
+    public void startMessage() {
+        System.out.println(START_MESSAGE);
+    }
+
+
 }
