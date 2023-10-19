0cc56c97427cde57e196b1ba085f932081a368ca
mainfn <_@mainfn.dev>
2023-10-19 15:10:38 +0900

    feat: BallNumbers 생성

---
+++ src/main/java/baseball/domain/BallNumbers.java
@@ -0,0 +1,24 @@
+package baseball.domain;
+
+import java.util.List;
+
+public final class BallNumbers {
+  private final List<Integer> numbers;
+
+  private BallNumbers(final List<Integer> numbers) {
+    this.numbers = numbers;
+  }
+
+  public static BallNumbers of(final List<Integer> numbers) {
+    validateNumbers(numbers);
+    return new BallNumbers(numbers);
+  }
+
+  private static void validateNumbers(final List<Integer> numbers) {
+    for (final Integer number : numbers) {
+      if (number < 1 || number > 9) {
+        throw new IllegalArgumentException("BallNumber는 1~9 사이의 3자리 수여야 합니다.");
+      }
+    }
+  }
+}
