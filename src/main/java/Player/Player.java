87e32f30641ef1b6c4598df09cd3f1e382a95730
kimwanyoung <dhks2869@gmail.com>
2023-10-19 17:41:28 +0900

    Feat : 사용자 입력 검증하는 validatePlayerInput() 구현

--- src/main/java/Player/Player.java
+++ src/main/java/Player/Player.java
@@ -2,7 +2,9 @@ package Player;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Player {
     private List<Integer> playerBaseballNumber;
@@ -10,12 +12,26 @@ public class Player {
     public List<Integer> inputPlayerNumber() {
         List<Integer> playerInputBaseballNumber = new ArrayList<>();
         String[] playerInput = Console.readLine().split("");
+
         for(int i = 0; i < 3; i++) {
             playerInputBaseballNumber.add(Integer.parseInt(playerInput[i]));
         }
-        //TODO: 사용자 입력 검증 validatePlayerInput()
+
+        validatePlayerInput(playerInputBaseballNumber);
         setPlayerBaseballNumber(playerInputBaseballNumber);
         return playerInputBaseballNumber;
+    }
+
+    private void validatePlayerInput(List<Integer> playerInput) {
+        Set<Integer> playerBaseballNumberSet = new HashSet<>(playerInput);
+
+        if(playerInput.size() > 3) {
+            throw new IllegalArgumentException("3자리의 숫자만 입력가능합니다.");
+        }
+
+        if(playerBaseballNumberSet.size() < 3) {
+            throw new IllegalArgumentException("중복된 숫자는 입력 불가능합니다.");
+        }
     }
 
     private void setPlayerBaseballNumber(List<Integer> playerBaseballNumber) {
