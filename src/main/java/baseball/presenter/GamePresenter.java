6f579f0257fe93e78cf9faa9c8b67a6ee18b485f
jmj005 <jmj00005@naver.com>
2023-10-20 02:56:29 +0900

    feat: 플레이어 숫자 중복 확인

--- src/main/java/baseball/presenter/GamePresenter.java
+++ src/main/java/baseball/presenter/GamePresenter.java
@@ -1,5 +1,8 @@
 package baseball.presenter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class GamePresenter {
     private boolean isValidNumeric(String numbers) {
         return numbers.matches("^[0-9]+$");
@@ -7,5 +10,13 @@ public class GamePresenter {
 
     private boolean isValidLength(String numbers) {
         return numbers.length() == 3;
+    }
+
+    private boolean isDuplicate(String numbers) {
+        Set<Character> uniqueDigits = new HashSet<Character>();
+        for (char element : numbers.toCharArray()) {
+            uniqueDigits.add(element);
+        }
+        return uniqueDigits.size() == 3;
     }
 }
