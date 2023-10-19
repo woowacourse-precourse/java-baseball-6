f2ef4055f5d8159b4e0112f899a431c8c0dc553e
MSDN-SPECIAL\Administrator <jsw1767@gmail.com>
2023-10-19 17:48:02 +0900

    게임 시작 문구 출력 및 숫자 입력 문구와 함께 입력받기

---
+++ docs/README.md
@@ -0,0 +1,7 @@
+## 게임 시작
+
+# 게임 시작 문구 출력
+
+# 숫자 입력 문구와 함께 입력 받기
+
+- camp.nextstep.edu.missionutils.Console의 readLine()을 활용
--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.print("숫자를 입력해주세요 : ");
+        String number = readLine();
+        System.out.println(number);
     }
+
 }
