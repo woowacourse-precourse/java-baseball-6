45b8c025fab82c695a818cd4ea3481d6f32a33c3
ttalkisyrup <ttalkisyrup@gmail.com>
2023-10-19 22:54:49 +0900

    feat: 유저에게 입력 받은 숫자에 대한 결과 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,5 +11,24 @@ public class Application {
         String userInput = readLine();
         int ballCount = baseballGame.getBallCount(userInput);
         int strikeCount = baseballGame.getStrikeCount(userInput);
+
+        StringBuilder countMessage = new StringBuilder();
+        if (ballCount == 0 && strikeCount == 0) {
+            System.out.println("낫싱");
+        }
+
+        if(ballCount != 0) {
+            countMessage.append(String.format("%d볼", ballCount));
+            if(strikeCount != 0) countMessage.append(" ");
+        }
+        if(strikeCount != 0) {
+            countMessage.append(String.format("%d스트라이크", strikeCount));
+        }
+
+        System.out.printf(countMessage.toString());
+        if(strikeCount == 3){
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
+
     }
 }
