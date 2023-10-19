b8330a6713132cff64ab6e70edb293e6d8914ca0
leeyeongjae <xingxing2001@cau.ac.kr>
2023-10-19 19:32:28 +0900

    코드 수정: 생성자에서 바로 난수 생성을 하는 것이 아닌 setter 메소드에서 난수 생성을 하도록 수정

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -3,12 +3,14 @@ package baseball;
 import java.util.*;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import javax.swing.*;
-
 public class Computer {
     private ArrayList<Integer> numbers= new ArrayList<>(); //컴퓨터의 숫자 조합
     Computer(){
-        //3개의 난수를 선택함
+
+    }
+
+    public void setNumbers(){
+        //3개의 난수 생성
         while(this.numbers.size() <3){
             int randomNumber = Randoms.pickNumberInRange(1,9);
             if(!numbers.contains(randomNumber)){
