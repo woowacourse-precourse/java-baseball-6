3bad49f66cafd00b054901e38ef6fd5f34c79b08
coenffl <>
2023-10-20 00:59:10 +0900

    Feat: Create computer numbers and modify input functions

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,25 +4,37 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 import model.ExceptionModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputView {
     ExceptionModel exceptionModel = new ExceptionModel();
     /**
      * 야구볼 서로 다른 3자리의 수 입력받기
      */
-    public String[] inputBaseBall() {
+    public List<Integer> inputBaseBall() {
         String number = readLine();
         exceptionModel.checkBaseBall(number);
-        return number.split("");
+        return changeInteger(number);
     }
 
+    private List<Integer> changeInteger (String input){
+        String[] str = input.split("");
+        List<Integer> numbers = new ArrayList<>();
+        for (String s :
+                str) {
+            numbers.add(Integer.parseInt(s));
+        }
+        return numbers;
+    }
 
     /**
      * 게임이 끝난 경우 재시작/종료를 구분하는 1과 2 중 하나의 수 입력받기
      */
-    public String inputRetryNumber() {
+    public int inputRetryNumber() {
         String number = readLine();
         exceptionModel.checkRetryNumber(number);
-        return number;
+        return Integer.parseInt(number);
     }
 
 }
