da0ff3f24a9c4198f2507b3b652ac5d9847437b0
Charm-Star <zxzx05050@naver.com>
2023-10-19 23:15:27 +0900

    feat: 컴퓨터 숫자 리스트 만들기 기능 구현

--- src/main/java/baseball/domain/BaseBallGame.java
+++ src/main/java/baseball/domain/BaseBallGame.java
@@ -2,19 +2,29 @@ package baseball.domain;
 
 import baseball.view.InputView;
 import baseball.view.OutputView;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BaseBallGame {
 
-    public void run(){
 
+    public void run() {
         OutputView.printStartMessage();
-
-
     }
-
-    private void startNewGame(){
+    private void startNewGame() {
+        List<Integer> computerNumbers = generateComputerNumber();
         InputView.readPlayerNumber();
 
     }
-
+    private List<Integer> generateComputerNumber(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
 }
