485af45d6f88791d7156fb122e7765f2138dbc95
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-10-20 06:14:37 +0900

    feat: Add Computer class

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,19 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+
+    List<Integer> com = new ArrayList<>();
+
+    public void getNumbers(List<Integer> array) {
+        while (com.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!com.contains(randomNumber)) {
+                com.add(randomNumber);
+            }
+        }
+    }
+}
