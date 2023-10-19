5239e6396578bc1aaf9ab8f1a986b28b07fce6aa
SeoJiWon <sjiwon4491@gmail.com>
2023-10-19 19:58:53 +0900

    feat: User - Computer 간 숫자야구 결과를 판단하는 Referee 도메인 생성

---
+++ src/main/java/baseball/domain/Referee.java
@@ -0,0 +1,15 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Referee {
+    private Referee() {
+    }
+
+    public static RoundResult judge(
+            final List<Integer> computerBaseballs,
+            final List<Integer> userBaseballs
+    ) {
+        return null;
+    }
+}
