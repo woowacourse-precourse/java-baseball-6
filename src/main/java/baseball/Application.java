af6a82ce2d7787674fa15c14583af669c8b88719
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-10-20 00:04:48 +0900

    랜덤 번호 생성 예제 추가
    참고용 랜덤 번호 생성 예제 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    // 랜덤번호 생성 예시 (참고용)
+    public List generateRandomNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 }
