50a8f40adf284541e0fad84a9a77debb9995ccfb
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-10-19 17:48:54 +0900

    Feat : 프로그램 숫자 생성 기능 추가 Set을 이용해 중복된 숫자가 없는 세자리 수 생성

---
+++ src/main/java/baseball/model/ProgramNumber.java
@@ -0,0 +1,36 @@
+package baseball.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ProgramNumber {
+
+    private String programNumber;
+
+    //생성된 프로그램 숫자 반환
+    public String getProgramNumber(){
+        return programNumber;
+    }
+
+    //생성 시 바로 프로그램 숫자를 만들 수 있도록 한다.
+    public ProgramNumber() {
+        setProgramNumber();
+    }
+
+    //1 ~ 9 범위 내의 랜덤한 숫자 하나 획득
+    public static String getOneNumber(){
+        return Integer.toString(Randoms.pickNumberInRange(1, 9));
+    }
+
+    //세자리 자연수 획득
+    public void setProgramNumber(){
+        //중복을 피하기 위해 HashSet 사용
+        Set<String> programNumberSet = new HashSet<>();
+        while (programNumberSet.size() < 3){
+            programNumberSet.add(getOneNumber());
+        }
+
+        programNumber = String.join("",programNumberSet);
+    }
+}
