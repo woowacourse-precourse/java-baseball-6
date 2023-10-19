1afc8541881b9b4046043e68fac85ef6d217fec8
1Bronze <khdkkhdd@gmail.com>
2023-10-19 18:36:15 +0900

    [feat] 사용자입력 검증로직 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,6 +5,10 @@ import baseball.controller.BaseballController;
 public class Application {
 
     public static void main(String[] args) {
-        new BaseballController().start();
+        try {
+            new BaseballController().start();
+        } catch (IllegalArgumentException e) {
+            System.out.println("잘못된 값을 입력하셨습니다. 프로그램을 종료합니다.");
+        }
     }
 }
--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -8,15 +8,15 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class BaseballController {
-    public void start() {
+    public void start() throws IllegalArgumentException {
         System.out.println("숫자 야구 게임을 시작합니다.");
         play();
     }
 
-    private void play() {
+    private void play() throws IllegalArgumentException {
         Integer answer = createAnswer();
         System.out.println("숫자를 입력해주세요 : ");
-        Integer userInput = Integer.parseInt(readLine());
+        Integer userInput = validate(readLine());
     }
 
     protected int createAnswer() {
@@ -30,5 +30,26 @@ public class BaseballController {
         StringBuilder answerStream = new StringBuilder();
         computer.stream().forEach(answerStream::append);
         return Integer.parseInt(answerStream.toString());
+    }
+
+    private int validate(String input) throws IllegalArgumentException {
+        try {
+            Integer intInput = Integer.parseInt(input);
+            if (intInput < 100 || intInput >= 999) {
+                throw new IllegalArgumentException();
+            }
+            Integer hundreds = intInput / 100;
+            Integer tens = (intInput / 10) % 10;
+            Integer ones = intInput % 10;
+            if (hundreds.equals(0) || tens.equals(0) || ones.equals(0)) {
+                throw new IllegalArgumentException();
+            }
+            if (hundreds.equals(tens) || hundreds.equals(ones) || tens.equals(ones)) {
+                throw new IllegalArgumentException();
+            }
+            return intInput;
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
     }
 }
