85e93de1d1e67a9948d718b576bd31a5e721b905
haroya01 <haroya01@naver.com>
2023-10-20 05:16:56 +0900

    feat: 플레이어가 입력한 값 검증 및 변환 로직 추가

--- src/main/java/baseball/BaseBallGamePlay.java
+++ src/main/java/baseball/BaseBallGamePlay.java
@@ -1,6 +1,8 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BaseBallGamePlay {
     //TODO: 게임진행
@@ -12,7 +14,9 @@ public class BaseBallGamePlay {
     private void play() {
         output();
         String playerBall = input();
-        System.out.println(playerBall);
+        PlayerNumbers playerNumbers = new PlayerNumbers(playerBall);
+
+        System.out.println(playerNumbers.getPlayerNumbers());
     }
 
     private void output() {
@@ -20,7 +24,39 @@ public class BaseBallGamePlay {
     }
 
     private String input() {
-        return Console.readLine();
+        String inputPlayerBall = Console.readLine();
+
+        // 입력값이 3자리가 아닐 경우 에러발생
+        if (inputPlayerBall.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+
+        return inputPlayerBall;
+
+    }
+
+    static class PlayerNumbers {
+        List<Integer> playerNumbers = new ArrayList<>();
+
+        public PlayerNumbers(String playerBalls) {
+
+            // 변환
+            for (int i = 0; i < playerBalls.length(); i++) {
+                char currentChar = playerBalls.charAt(i);
+
+                // 현재 문자가 숫자인지 확인 아니면 에러발생
+                if (Character.isDigit(currentChar)) {
+                    playerNumbers.add(currentChar - '0');
+                    continue;
+                }
+                throw new IllegalArgumentException();
+            }
+        }
+
+        public List<Integer> getPlayerNumbers() {
+            return playerNumbers;
+        }
+
     }
 
 }
