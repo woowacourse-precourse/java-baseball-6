70a66038dabd0f99803e8dcbf6492ab839983ca7
suhyun9764 <suhyun9764@naver.com>
2023-10-20 01:13:51 +0900

    refactor : player 객체 속성 및 getter, setter 추가

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -7,21 +7,57 @@ import java.util.ArrayList;
 public class Player {
     private ArrayList<Integer> playerValue;
     private final DataValidator dataValidator;
+    private int strike;
+    private int ball;
 
     public Player() {
         this.playerValue = new ArrayList<Integer>();
         this.dataValidator = new DataValidator();
+        strike = 0;
+        ball = 0;
+        continueDecision = 1;
     }
 
-    public ArrayList<Integer> getPlayerValue() {
+    public void setPlayerValue() {
         System.out.print("숫자를 입력해주세요 : ");
-        ArrayList<Integer> playerValue = new ArrayList<>();
         String[] stringValue  = Console.readLine().split("");
         for(String s : stringValue){
             playerValue.add(Integer.parseInt(s));
         }
         dataValidator.playerValueValidation(playerValue);
+    }
 
+    public ArrayList<Integer> getPlayerValue(){
         return playerValue;
     }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public void setStrike(int strike) {
+        this.strike = strike;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+
+    public void setBall(int ball) {
+        this.ball = ball;
+    }
+
+    public int getContinueDecision() {
+        return continueDecision;
+    }
+
+    public void setContinueDecision(int continueDecision) {
+        this.continueDecision = continueDecision;
+    }
+
+    private int continueDecision;
+
+
+
+
 }
