5c21011dc5e10dc26e63bb9c39d62c22a9e43475
Frod90 <142146516+Frod90@users.noreply.github.com>
2023-10-20 03:39:43 +0900

    Feat: createComputerNumber(컴퓨터 숫자 생성) 메서드 구현
    1부터 9까지 서로 다른 수로 이루어진 3자리의 컴퓨터 숫자를 생성하는 createComputerNumber 메서드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+
+	}
+
+	//1~9 사이의 서로 다른 3자리로 이루어진 컴퓨터 숫자를 생성하는 메서드
+	static Set<Integer> createComputerNumber() {
+		Set<Integer> computerNumber = new HashSet<>();
+		final int COMPUTER_NUMBER_SIZE = 3;
+		final int COMPUTER_MIN_NUMBER = 1;
+		final int COMPUTER_MAX_NUMBER = 9;
+
+		while (computerNumber.size() < COMPUTER_NUMBER_SIZE) {
+			int randomNumber = Randoms.pickNumberInRange(COMPUTER_MIN_NUMBER, COMPUTER_MAX_NUMBER);
+			computerNumber.add(randomNumber);
+		}
+
+		return computerNumber;
+	}
 }
