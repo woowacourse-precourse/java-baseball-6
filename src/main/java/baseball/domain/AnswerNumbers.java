b710507d48c071a32c046a5a391530cf4d48e028
donghyunkim <ghffu405@gmail.com>
2023-10-19 16:24:01 +0900

    feat: 중복된 숫자가 존재하는지 검증하는 메소드 추가

--- src/main/java/baseball/domain/AnswerNumbers.java
+++ src/main/java/baseball/domain/AnswerNumbers.java
@@ -2,7 +2,9 @@ package baseball.domain;
 
 import baseball.constant.NumberConstant;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static baseball.constant.NumberConstant.*;
 
@@ -11,7 +13,8 @@ public class AnswerNumbers {
     private List<Integer> numbers;
 
     public AnswerNumbers(List<Integer> numbers) {
-
+        validateSize(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
@@ -20,6 +23,15 @@ public class AnswerNumbers {
             throw new IllegalArgumentException("숫자는 3개만 가능합니다.");
         }
     }
+
+    public void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> nonDuplicateNumbers = new HashSet<>(numbers);
+
+        if(nonDuplicateNumbers.size() != VALID_NUMBER_SIZE){
+            throw new IllegalArgumentException("중복된 숫자가 존재합니다.");
+        }
+    }
+
 
 
 }
