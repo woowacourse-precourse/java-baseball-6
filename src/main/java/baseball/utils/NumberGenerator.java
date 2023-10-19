90f1263c009c307d3a5dc455a03f065996e9a93a
hlmg <devhlmg@gmail.com>
2023-10-19 22:24:46 +0900

    feat(NumberGenerator): 중복되지 않는 랜덤한 숫자를 생성하는 기능 구현

---
+++ src/main/java/baseball/utils/NumberGenerator.java
@@ -0,0 +1,30 @@
+package baseball.utils;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class NumberGenerator {
+    public static final String TO_SMALL_RANGE = "자릿수보다 범위가 더 커야 합니다";
+
+    public static List<Integer> generateUniqueNumbers(int startInclusive, int endInclusive, int digit) {
+        validate(startInclusive, endInclusive, digit);
+        Set<Integer> numbers = new HashSet<>();
+        while (numbers.size() < digit) {
+            int generatedRandomNumber = generateRandomNumber(startInclusive, endInclusive);
+            numbers.add(generatedRandomNumber);
+        }
+        return List.copyOf(numbers);
+    }
+
+    private static void validate(int startInclusive, int endInclusive, int digit) {
+        if (endInclusive - startInclusive + 1 < digit) {
+            throw new IllegalArgumentException(TO_SMALL_RANGE);
+        }
+    }
+
+    private static int generateRandomNumber(int startInclusive, int endInclusive) {
+        return Randoms.pickNumberInRange(startInclusive, endInclusive);
+    }
+}
