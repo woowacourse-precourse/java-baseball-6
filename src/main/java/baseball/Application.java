ee87e463c210eb7d0f7473bc0e2f4a40210e84bf
muyaaho <skanthsu715@gmail.com>
2023-10-19 20:29:58 +0900

    Fix: 한 숫자를 맞추도록 수정, 스트라이크 볼 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,22 +1,19 @@
 /*
 1. 3개의 랜덤한 수 고르기(만들어서 str로 만들어 두자)
-2. 반복을 어디서부터 어디까지 하는지 확인
-3. 한 숫자를 계속 맞추는 것이 아니라 숫자를 입력할 때마다 랜덤으로 숫자를 만들고 결과를 나타내야됨
+2. 반복: 한 게임이 끝날 때 까지(맞추거나 잘못된 값을 입력하는 동안)
+3. 한 숫자를 계속 맞춰야 됨
     - do{} while(조건);
-4. 오타 주의
-5. 숫자 맞치면 게임 종료(1, 2 선택하세요)
-6. 잘못된 값 => IllegalArgumentException
-7. 숫자 비교: for문을 돌면서 i를 둘 다 입력해서 서로 비교해서 같으면 스트라이크 다르면 볼?
-
-다시 생각
-1. 한 숫자에 대해 반복을 실행하고 맞추고 다른 게임을 시작할 때 랜덤 숫자가 바뀌어야됨
-2. 볼: 숫자만 맞는 것, 스트라이크: 숫자도 맞고 자리도 맞고
-
-
-추가해야 할 것
-1. 0으로 시작하게
-2. 이상한 숫자를 입력했ㅇㄹ 때 IllegalArgumentException
-*/
+4. 숫자 맞치면 게임 종료(1, 2 선택하세요)
+5. 잘못된 값 => IllegalArgumentException
+6. 숫자 비교:
+    - 한 게임이 시작될 때 random으로 각자 다른 수가 들어진 배열을 만들기
+    - 입력도 배열로 만들기
+    - 입력을 반복문을 돌면서 자리가 같은지 아닌지 확인하기(아니면 둘 다 스트링으로 하고 index 찾는 함수 있으면 쓰기)
+    - string은 indexOf를 사용할 수 있음. 그럼 입력을 반복문으로 보낸 i와 인덱스가 같은지 확인하거나 아님 i, j 이중 for문 쓰거나
+7. 볼: 숫자만 맞는 것, 스트라이크: 숫자도 맞고 자리도 맞고
+8. 1~9의 서로 다른 숫자
+9. https://github.com/woowacourse/woowacourse-docs/tree/main/styleguide/java 자바 스타일 가이드
+ */
 
 
 package baseball;
@@ -29,71 +26,81 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        Scanner sc = new Scanner(System.in);
+
+        // 초기화
+
         Random rand = new Random();
-        boolean isFinisth = false;
-        do {
+        int end = 1;
 
-            System.out.print("숫자를 입력해주세요 : ");
-            String num = sc.nextLine();
-            // 난수와 비교
 
-            //@@@@@@ 제거!
-            System.out.println("check num:"+num);
-            List<String> com = new ArrayList<String>();
-            int strike = 0;
-            int ball = 0;
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        boolean isFinish = false;
+        // 게임 진행
+        while (end == 1) {
+            // 한 게임당 초기화
+            // 랜덤한 수가 들어가는 com 리스트
+//            List<String> com = new ArrayList<String>();/
+            String com = "";
+            isFinish = false;
 
-            //@@@@@@ 제거!!
-            System.out.println("check size: "+com.size());
-            System.out.println("check charAt: "+num.charAt(0));
-            int size = 0;
-            while (size < 3) {
-                size = com.size();
-                // 서로 다른 3개의 숫자가 아님!
-                // 배열을 만들어서 집어넣고 안나오게 해야 됨
-                int r = rand.nextInt(9)+1;
+            while (com.length() < 3) {
+                int r = rand.nextInt(9) + 1;
                 String sr = Integer.toString(r);
-                if (com.contains(sr)) {     // 리스트에 포함된다면
-                    continue;
-                } else {     // 리스트에 포함되지 않는다면 추가하고 실행
-                    com.add(sr);
+                if (!com.contains(sr)) {     //리스트에 포함되지 않는다면 추가하고 실행
+                    com = com + sr;
+                }
+            }
+            //@@@@ 삭제해야 하는 코드
+            System.out.println("answer: "+com);
+            // 게임 시작
 
-                    // 추가를 하고 add 하기 때문에!
-                    if (sr.equals(num.charAt(size))) {
-                        strike += 1;
-                    } else {
-                        ball += 1;
+            // while (게임 진행되는 동안)
+            while (!isFinish) {
+                int strike = 0;
+                int ball = 0;
+                // 입력
+                System.out.print("숫자를 입력해주세요 : ");
+                Scanner sc = new Scanner(System.in);
+                String num = sc.nextLine();
+                // TODO: 이상한 입력 체크
+                // 잘못된 입력 1: 3자리가 아닌 수일 경우(1,2,4, ...)
+                // 잘못된 입력 2: 0이 들어올 경우
+                // 잘못된 입력 3: 숫자가 아닌 경우
+                // 비교
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        if (num.charAt(i) == com.charAt(j)) {
+                            if (i == j) {
+                                strike += 1;
+                            } else {
+                                ball += 1;
+                            }
+                        }
                     }
                 }
-            }
-            System.out.println("check com: "+com);
-            // 3가지 수를 비교하고 나서
-            if (strike == 3) {
-                System.out.println("3스트라이크");
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                // 새로 시작하는 boolean을 넣어 주기
-                int newgame = sc.nextInt();
-                if (newgame == 2){
-                    isFinisth = true;
+
+                // 결과
+                // 정답: 일단 나가기
+                if (strike == 3) {
+                    System.out.println("3스트라이크");
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                    end = sc.nextInt();
+                    // TODO: 잘못된 값 입력됬는지 확인!
+                    isFinish = true;
+
                 }
+                else if (strike == 0 && ball == 0) {
+                    System.out.println("낫싱");
+                }
+                else {
+                    System.out.println(ball+"볼 "+strike+"스트라이크");
+                }
+                // 계속
 
-            } else if (ball == 3) {
-                System.out.println("낫싱");
-            } else {
-                System.out.printf("%d볼 %d스트라이크\n", ball, strike);
             }
+
         }
-        while(!isFinisth);
-
-
-        sc.close();
-
-
-
-
 
     }
 }
