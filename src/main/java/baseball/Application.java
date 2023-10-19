2ceef49ff7d99dcb0b4c6dcd9a7ba6d22b369a4b
mainfn <_@mainfn.dev>
2023-10-19 15:52:52 +0900

    feat: create Application

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
-public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+import baseball.controller.GameController;
+import baseball.domain.AnswerCreator;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+public final class Application {
+
+  public static void main(final String[] args) {
+    final InputView inputView = new InputView();
+    final OutputView outputView = new OutputView();
+    final AnswerCreator answerCreator = new AnswerCreator();
+    final GameController gameController = new GameController(inputView, outputView, answerCreator);
+
+    gameController.run();
+  }
 }
