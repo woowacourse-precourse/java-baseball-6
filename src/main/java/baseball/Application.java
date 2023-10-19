57e4fb56ef18c2ddb235b5cbe46b9ba33f375dae
yang <ywj9811@gmail.com>
2023-10-20 01:10:47 +0900

    feat : 전체 게임 시작

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,7 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseBall baseBall = new BaseBall();
+        baseBall.start();
     }
 }
