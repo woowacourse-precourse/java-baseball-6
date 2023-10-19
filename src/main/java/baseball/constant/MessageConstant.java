cf4d0b62a4801d4bd1f355c2c459bfa42b671064
hwan2-99 <thh1233123@gmail.com>
2023-10-19 23:00:23 +0900

    feat: 유저 입력숫자 유효범위 검증기능 추가

--- src/main/java/baseball/constant/MessageConstant.java
+++ src/main/java/baseball/constant/MessageConstant.java
@@ -9,6 +9,8 @@ public class MessageConstant {
     public static final String RESTART_OR_END_MESSAGE =
             "게임을 새로 시작하려면 " + NumberConstant.RESTART_NUMBER + ", 종료하려면 " + NumberConstant.GAME_END_NUMBER + "를 입력하세요.";
     public static final String WINNER_MESSAGE = NumberConstant.GAME_WIN_NUMBER + "개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    public static final String VALIDATE_SIZE_MESSAGE = "크기가 "+NumberConstant.INPUT_SIZE + "이 아닙니다.";
+    public static final String VALIDATE_SIZE_MESSAGE = "크기가 " + NumberConstant.INPUT_SIZE + "이 아닙니다.";
+    public static final String VALIDATE_RANGE_MESSAGE =
+            NumberConstant.MIN_NUMBER + "에서 " + NumberConstant.MAX_NUMBER + "까지의 숫자만 입력할 수 있습니다,";
 
 }
--- src/main/java/baseball/validation/NumberValidation.java
+++ src/main/java/baseball/validation/NumberValidation.java
@@ -1,28 +1,40 @@
 package baseball.validation;
+
 import baseball.constant.MessageConstant;
+import baseball.constant.NumberConstant;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class NumberValidation {
 
-    public void validateInputNumber(String inputNumber){
-        List<Integer>userNumber = changeToList(inputNumber);
+    public void validateInputNumber(String inputNumber) {
+        List<Integer> userNumber = changeToList(inputNumber);
         validateNumberSize(userNumber);
+        validateNumberRange(userNumber);
 
     }
 
-    public List<Integer> changeToList(String inputNumber){
+    public List<Integer> changeToList(String inputNumber) {
         List<Integer> userNumber = Arrays.stream(inputNumber.split(""))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
         return userNumber;
     }
 
-    public void validateNumberSize(List<Integer> userNumber){
-        if(userNumber.size() != 3){
+    public void validateNumberSize(List<Integer> userNumber) {
+        if (userNumber.size() != 3) {
             throw new IllegalArgumentException(MessageConstant.VALIDATE_SIZE_MESSAGE);
         }
     }
+
+    public void validateNumberRange(List<Integer> userNumber) {
+        for (int i = 0; i < userNumber.size(); i++) {
+            if (userNumber.get(i) <= NumberConstant.MIN_NUMBER || userNumber.get(i) >= NumberConstant.MAX_NUMBER) {
+                throw new IllegalArgumentException(MessageConstant.VALIDATE_SIZE_MESSAGE);
+            }
+        }
+    }
+
 
 }
