6d3017cf2eab84840ecc2143bbfb47bc92de27b9
ddolboghi <rhdfyd128@gmail.com>
2023-10-19 21:00:25 +0900

    feat: 3자리수 입력 받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,5 +4,6 @@ public class Application {
     public static void main(String[] args) {
         BaseballGame baseballGame = new BaseballGame();
         System.out.println(baseballGame.createComputerNumbers());
+        System.out.println(baseballGame.createUserNumbers());
     }
 }
--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,12 +1,46 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInList;
 import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
 
 public class BaseballGame {
     private static final int BASEBALL_NUMBER_SIZE = 3;
     public List<Integer> createComputerNumbers() {
         return pickUniqueNumbersInRange(1,9, BASEBALL_NUMBER_SIZE);
+    }
+
+    //사용자로부터 입력 받은 숫자를 userNumber에 저장
+    public List<Integer> createUserNumbers() {
+        List<Integer> userNumbers = new ArrayList<>();
+        String userInput = readLine();
+        for (int i=0;i<userInput.length();i++) {
+            String str = userInput.substring(i, i+1);
+            userNumbers.add(Integer.parseInt(str));
+        }
+        validateUserNumber(userNumbers);
+        return userNumbers;
+    }
+
+
+    // userNumber 검증
+    private static void validateUserNumber(List<Integer> userNumbers) {
+        int randomUserNumber = pickNumberInList(userNumbers);
+        System.out.println(randomUserNumber);
+        if (randomUserNumber < 1) {
+            throw new IllegalArgumentException("각 숫자는 1부터 9까지만 입력해주세요.");
+        }
+        if (userNumbers.size() != BASEBALL_NUMBER_SIZE) {
+            throw new IllegalArgumentException("숫자 3개만 입력해주세요.");
+        }
+        Set<Integer> userNumbersSet = new HashSet<>(userNumbers);
+        if (userNumbersSet.size() != userNumbers.size()) {
+            throw new IllegalArgumentException("서로 다른 숫자 3개를 입력해주세요.");
+        }
     }
 }
