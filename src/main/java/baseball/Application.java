d3f044df4849b2c9966206eff8a908e89e94d22c
BellWin98 <jongseunghan@naver.com>
2023-10-19 18:01:45 +0900

    Docs: 기능 목록 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,31 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+
+//        System.out.println("숫자 야구 게임을 시작합니다.");
+//        System.out.print("숫자를 입력해주세요 : ");
+//        String str = Console.readLine();
+//        String[] strings = str.split("");
+//
+//        for (int i = 0; i < strings.length; i++){
+//
+//        }
+
+        List<Integer> computer = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            computer.add(Randoms.pickNumberInRange(1, 9));
+        }
+
+        System.out.println("컴퓨터 숫자 : "+computer);
+
+//        System.out.println("입력한 숫자: " + str);
     }
 }
