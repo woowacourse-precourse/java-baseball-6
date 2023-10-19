7027c880a1d213e8d4ed5a5b8836a73286cf26e9
Taehyoun Kim <kth9903@gmail.com>
2023-10-20 00:51:03 +0900

    chore(game): initialize game loop and welcome message

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다");
+
+        while(true){ //running game
+
+        }
+
     }
 }
