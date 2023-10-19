4928bd1775925e442bef15dcd4275ae9b10b1484
MyunghyunNero <mh1kim0910@gmail.com>
2023-10-19 21:41:07 +0900

    feat : 게임 설정 클래스 추가

---
+++ src/main/java/baseball/config/GameConfig.java
@@ -0,0 +1,10 @@
+package baseball.config;
+
+public class GameConfig {
+    //숫자 범위
+    public static final Integer START_NUMBER = 1;
+    public static final Integer END_NUMBER = 9;
+
+    //숫자 입력 길이
+    public static final Integer CORRECT_LENGTH = 3;
+}
