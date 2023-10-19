2e137c510105751dea1c6df28b909034a0654d46
minjun7410 <minjun7410@gmail.com>
2023-10-19 20:59:03 +0900

    feat: 유저에게 숫자 공백 제외한 입력 받는 기능 구현
    Display가 입력을 담당하기 떄문에 Display가 유저에게 입력을 받고 좌우 공백을 없앤 결과를 리턴한다.

--- src/main/java/baseball/Display.java
+++ src/main/java/baseball/Display.java
@@ -1,7 +1,13 @@
 package baseball;
-
+import camp.nextstep.edu.missionutils.Console;
 public class Display {
     public void printStartText(){
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+    public String getUserInput(){
+        System.out.print("숫자를 입력해주세요 : ");
+        int[] userInputs = new int[3];
+        String trimedUserInput = Console.readLine().trim();
+        return trimedUserInput;
     }
 }
