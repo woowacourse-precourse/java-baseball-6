0883bec07f42405ffc125f56d5834c97737cee80
Hugh <lee2317884@gmail.com>
2023-10-19 22:19:37 +0900

    feat: OutputView 출력 값 상수화

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,5 +1,11 @@
 package baseball.view;
 
 public class OutputView {
+	private static final String BASEBALL_GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+	private static final String BALL_MESSAGE = "볼 ";
+	private static final String STRIKE_MESSAGE = "스트라이크";
+	private static final String NOTHING_MESSAGE = "낫싱";
+	private static final String BASEBALL_GAME_CLEAR_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
 
 }
