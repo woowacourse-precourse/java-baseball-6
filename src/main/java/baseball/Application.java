5e3b093245ce808084ae1446895afa47a6e5cc66
devholic22 <hyunjoon.tech@gmail.com>
2023-10-19 22:54:49 +0900

    Feat: 숫자 입력 요청문 출력 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,11 @@
 package baseball;
 
+import baseball.view.AskView;
 import baseball.view.StartView;
 
 public class Application {
     public static void main(String[] args) {
         StartView.welcome();
+        AskView.askNumberInput();
     }
 }
---
+++ src/main/java/baseball/view/AskView.java
@@ -0,0 +1,8 @@
+package baseball.view;
+
+public class AskView {
+
+    public static void askNumberInput() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+}
