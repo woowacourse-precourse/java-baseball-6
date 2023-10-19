eb42685a71ec99977363ce5663d16f824c1573e0
1000zoo <cjswldn99@gmail.com>
2023-10-19 20:37:09 +0900

    feat(Output): 출력 담당 클래스 완성
    - ErrorDetector 에서 에러 출력 메서드 적용

--- src/main/java/baseball/ErrorDetector.java
+++ src/main/java/baseball/ErrorDetector.java
@@ -5,8 +5,8 @@ public class ErrorDetector {
     public static void throwIfNumbersInputInvalid(String numbers) {
         throwIfContainsNonNumber(numbers);
         if (numbers.length() != 3) {
-            // 에러 메시지 출력
-             throw new IllegalArgumentException();
+            Output.printErrorMessage(Constants.INVALID_NUMBERS_INPUT_LENGTH_ERROR_MESSAGE);
+            throw new IllegalArgumentException();
         }
     }
 
@@ -14,7 +14,7 @@ public class ErrorDetector {
         throwIfContainsNonNumber(option);
         int op = Integer.parseInt(option);
         if (op != 1 && op != 2) {
-            // 에러 메시지 출력
+            Output.printErrorMessage(Constants.INVALID_OPTION_INPUT_ERROR_MESSAGE);
             throw new IllegalArgumentException();
         }
     }
@@ -23,7 +23,7 @@ public class ErrorDetector {
         try {
             Integer.parseInt(input);
         } catch (Exception e) {
-            // 에러 메시지 출력
+            Output.printErrorMessage(Constants.INVALID_INPUT_ONLY_INTEGER_ERROR_MESSAGE);
             throw new IllegalArgumentException();
         }
     }
---
+++ src/main/java/baseball/Output.java
@@ -0,0 +1,12 @@
+package baseball;
+
+public class Output {
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static void printErrorMessage(String message) {
+        System.out.println(Constants.ERROR_MESSAGE + message);
+    }
+}
