245aefd142ee4f4e88c2a742acfa2c462072c80b
minjun7410 <minjun7410@gmail.com>
2023-10-19 21:52:15 +0900

    refactor: 유효성 검사 리펙토링
    유효성을 검사하는 로직 리펙토링
    - validateUserInputSize : 유저 입력의 사이즈를 검사하는 메서드
    - validateIsNumber : 유저 입력이 1~9 숫자인지 검사하는 메서드
    - validateDuplicatedNumber : 유저 입력에 중복이 없는지 검사하는 메서드

--- src/main/java/baseball/Display.java
+++ src/main/java/baseball/Display.java
@@ -11,15 +11,30 @@ public class Display {
     }
     public int[] splitUserInput(String input){
         int[] result = new int[3];
-        if(input.length() != 3) throw new IllegalArgumentException("3자리 숫자를 입력해주세요.");
+        validateUserInputSize(input);
         char[] userInputs = input.toCharArray();
-        boolean[] duplicateCheck = new boolean[10];
+        boolean[] numberCheckArray = new boolean[10];
         for(int i = 0; i < 3; i++){
-            if(userInputs[i] <= '0' || userInputs[i] > '9') throw new IllegalArgumentException("1에서 9사이 숫자를 입력해주세요.");
-            if(duplicateCheck[userInputs[i] - '0']) throw new IllegalArgumentException("중복되지 않는 숫자를 입력해주세요");
-            duplicateCheck[userInputs[i] - '0'] = true;
+            validateIsNumber(userInputs[i]);
+            validateDuplicatedNumber(numberCheckArray, userInputs[i]);
             result[i] = userInputs[i] - '0';
         }
         return result;
+    }
+    private void validateUserInputSize(String input){
+        if(input.length() != 3){
+            throw new IllegalArgumentException("3자리 숫자를 입력해주세요.");
+        }
+    }
+    private void validateIsNumber(char input){
+        if(input <= '0' || input > '9'){
+            throw new IllegalArgumentException("1에서 9사이 숫자를 입력해주세요.");
+        }
+    }
+    private void validateDuplicatedNumber(boolean[] numberCheckArray, char number){
+        if(numberCheckArray[number - '0']){
+            throw new IllegalArgumentException("중복되지 않는 숫자를 입력해주세요");
+        }
+        numberCheckArray[number - '0'] = true;
     }
 }
