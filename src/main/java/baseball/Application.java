1e57fa5c2875f0df948142fd28c3faa7cca1f20e
KimDabomi <cdabomi@nate.com>
2023-10-19 18:34:11 +0900

    사용자 입력값 리스트 추가, 예외클래스 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,15 @@
 package baseball;
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.*;
 
 public class Application {
+    private static final int MAX_LENGTH = 3;
+
     public static void main(String[] args) {
         getRandomNumber();
+        getUserInput();
     }
 
     public static void getRandomNumber() {
@@ -18,4 +22,18 @@ public class Application {
         }
         System.out.println(computer);
     }
+
+
+    public static void getUserInput() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+
+        List<Integer> numbers = new ArrayList<>();
+        for (char ch : input.toCharArray()) {
+            numbers.add(Character.getNumericValue(ch));
+        }
+
+        BaseballException.numListSize(numbers, MAX_LENGTH);
+    }
+
 }
