aec06fe88f9a8d299a64e35fb3871af00c276a74
pwrwpw <dlgustjr9912@gmail.com>
2023-10-19 22:12:03 +0900

    feat(InputValidation): 서로 다른 세자리 수 입력 검증 기능 서로 같은수가 입력될시 오류메시지 반환 후 애플리케이션 종료 docs: 서로 다른 수인지 검증 체크 완료

--- src/main/java/baseball/utills/InputValidation.java
+++ src/main/java/baseball/utills/InputValidation.java
@@ -1,17 +1,23 @@
 package baseball.utills;
 
-import java.util.List;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class InputValidation {
 	private static final int NUMBER_LENGTH = 3;
 	private static final String WRONG_INPUT_LENGTH_MESSAGE = "세 자리만 입력 가능합니다.";
 	private static final String WRONG_INPUT_RANGE_MESSAGE = "1~9 사이의 숫자만 입력 가능합니다.";
+	private static final String WRONG_INPUT_DUPLICATE_MESSAGE = "서로 다른 세자리 숫자만 입력 가능합니다.";
 	public void validate(String inputNumber){
 		if(!hasExactLength(inputNumber)){
 			throw new IllegalArgumentException(WRONG_INPUT_LENGTH_MESSAGE);
 		}
 		if(!hasExactRange(inputNumber)){
 			throw new IllegalArgumentException(WRONG_INPUT_RANGE_MESSAGE);
+		}
+		if(!hasExactDuplicate(inputNumber)){
+			throw new IllegalArgumentException(WRONG_INPUT_DUPLICATE_MESSAGE);
 		}
 	}
 
@@ -21,4 +27,11 @@ public class InputValidation {
 	public boolean hasExactRange(String inputNumber) {
 		return inputNumber.chars().allMatch(Character::isDigit);
 	}
+	public boolean hasExactDuplicate(String inputNumber) {
+		String[] inputNumbers = inputNumber.split("");
+		Set<String> setNumbers = Arrays.stream(inputNumbers).collect(Collectors.toSet());
+
+		return setNumbers.size() == NUMBER_LENGTH;
+	}
+
 }
