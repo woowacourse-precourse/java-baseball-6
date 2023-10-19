63690b9330b33899f4c1848fd7b9ed395904bb7f
yujeongkwon <87534067+yujeongkwon@users.noreply.github.com>
2023-10-20 04:26:29 +0900

    feat(view/InputView.java): 재시작 여부 메세지 출력

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -8,4 +8,8 @@ public class InputView {
         return Console.readLine();
     }
 
+    public static String readUserRestartStatus() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return Console.readLine();
+    }
 }
