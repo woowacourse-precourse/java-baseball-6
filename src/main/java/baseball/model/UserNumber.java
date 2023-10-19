24e9e9d02c37c654841c5b76fd7b41360a4400b2
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-10-19 19:07:12 +0900

    Feat : 세자리 숫자가 아닐 경우 예외 발생 세자리 숫자가 아닐 경우 예외 발생 코드 작성

--- src/main/java/baseball/model/UserNumber.java
+++ src/main/java/baseball/model/UserNumber.java
@@ -5,15 +5,24 @@ public class UserNumber {
     /**
      * 숫자가 아닐 경우 예외 발생
      */
-    public static void isDigitNumber(String inputWord){
+    public static void checkIsDigit(String inputWord){
         for (int i = 0; i < inputWord.length(); i++){
-            checkIsDigit(inputWord, i);
+            isDigitNumber(inputWord, i);
         }
     }
 
-    public static void checkIsDigit(String inputWord, int idx) throws IllegalArgumentException{
+    public static void isDigitNumber(String inputWord, int idx){
         if (!Character.isDigit(inputWord.charAt(idx))) {
             throw new IllegalArgumentException("숫자가 아닙니다.");
+        }
+    }
+
+    /**
+     * 세자리 숫자가 아닐 경우 예외 발생
+     */
+    public static void checkIsThree(String inputWord){
+        if (inputWord.length() != 3) {
+            throw new IllegalArgumentException("세자리 숫자가 아닙니다.");
         }
     }
 }
