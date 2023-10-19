1e879812297e7e0fe66a3928eb69d2604704f294
gyungchan Jo <namejgc@naver.com>
2023-10-20 00:04:38 +0900

    feat(Validator): 입력이 공백인지 검증하는 기능 구현

--- src/main/java/baseball/utils/Validator.java
+++ src/main/java/baseball/utils/Validator.java
@@ -1,10 +1,20 @@
 package baseball.utils;
 
+import org.junit.platform.commons.util.StringUtils;
+
 import java.util.regex.Pattern;
 
 public class Validator {
 
+    protected static void validateInput(String input) {
+        if (StringUtils.isBlank(input)) {
+            throw new IllegalArgumentException("입력은 공백일 수 없습니다.");
+        }
+    }
+
     public static void validateBallNumbers(String input) {
+        validateInput(input);
+
         if (isNotDigit(input)) {
             throw new IllegalArgumentException("숫자만 입력 가능합니다.");
         }
@@ -15,6 +25,8 @@ public class Validator {
     }
 
     public static void validateRestartOptionNumber(String input) {
+        validateInput(input);
+
         if (isIncorrectInput(input)) {
             throw new IllegalArgumentException("게임 재시작/종료 여부에 대한 입력은 1 또는 2로만 가능합니다.");
         }
--- src/test/java/baseball/utils/ValidatorTest.java
+++ src/test/java/baseball/utils/ValidatorTest.java
@@ -2,11 +2,21 @@ package baseball.utils;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class ValidatorTest {
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("입력이 null 이거나 공백이면 예외가 발생한다.")
+    void validateInput(String input) {
+        assertThatThrownBy(() -> Validator.validateInput(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("입력은 공백일 수 없습니다.");
+    }
 
     @ParameterizedTest
     @ValueSource(strings = {"a", "!", "12A"})
