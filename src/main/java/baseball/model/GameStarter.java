b1f651760ebb7d15672423267eaaa2f00b444e15
10Hyuna <kimha105@naver.com>
2023-10-19 17:53:19 +0900

    [add] readme, input, output

---
+++ src/main/java/baseball/model/GameStarter.java
@@ -0,0 +1,8 @@
+package baseball.model;
+
+public class GameStarter {
+
+    public void announceStarter() {
+
+    }
+}
---
+++ src/main/java/baseball/utility/Constant.java
@@ -0,0 +1,14 @@
+package baseball.utility;
+
+public class Constant {
+
+    public static final String GAME_START = "숫자 야구 게임을 시작합니다.";
+    public static final String ANNOUNCE_INSERT_NUMBER = "숫자를 입력해주세요 : ";
+    public static final String BALL = "%볼";
+    public static final String STRIKE = "%스트라이크";
+    public static final String NOTTING = "낫싱";
+    public static final String CLEAR = "3개의 숫자를 모두 맞히셨습니다!";
+    public static final String GAME_FINISH = "게임 종료";
+    public static final String ASK_RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+}
