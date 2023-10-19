ed9bd673328d358a43f2c174c1c8a77350f9901b
Jeon HoGeon <jhg2819@naver.com>
2023-10-19 20:17:35 +0900

    feat: 게임 반복 기능 구현

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -1,19 +1,50 @@
 package baseball;
 
-import baseball.domain.Opponent;
+import baseball.domain.Numbers;
+import baseball.domain.Computer;
+import baseball.domain.Result;
 import baseball.view.InputView;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class BaseBallGame {
 
-    private Opponent opponent;
+    private static final String CONTINUE_GAME_SIG = "1";
+    private static final String END_GAME_SIG = "2";
+    private final InputView view = new InputView();
+    private final Computer computer = new Computer();
+    private Numbers answerNumbers;
 
 
     public void run() {
-        start();
+        view.printStart();
+
+        Boolean continueGame = Boolean.TRUE;
+
+        while (continueGame) {
+            answerNumbers = computer.createNumbers();
+
+            startGame();
+
+            view.printFinish();
+            continueGame = isContinueGame(readLine());
+        }
     }
 
-    private void start() {
-        InputView.printStart();
-        opponent = Opponent.create();
+
+    private boolean isContinueGame(String continueGameInput) {
+        if (continueGameInput.equals(CONTINUE_GAME_SIG)) {
+            return true;
+        }
+        if (continueGameInput.equals(END_GAME_SIG)) {
+            return false;
+        }
+
+        throw new IllegalArgumentException("[ERROR] 잘못된 입력입니다.");
     }
+
 }
