a11ab4eca005ec0fd8c1a514c740d898107c547d
Hugh <lee2317884@gmail.com>
2023-10-19 21:26:32 +0900

    refactor: 사용자 숫자 입력 시 NULL 혹은 공백일 경우 예외 처리 하도록 리팩토링

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import baseball.controller.BaseballGameController;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+
+	public static void main(String[] args) {
+		BaseballGameController game = new BaseballGameController();
+		game.gamePlay();
+	}
 }
--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,20 +1,33 @@
 package baseball.view;
 
 
+import baseball.constant.Constant;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-	private static final String INPUT_GAME_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
-	private static final String INPUT_GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+	public static final String INPUT_GAME_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+	public static final String INPUT_GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
-	public static String printInputGameNumberMessage() {
-		System.out.print(INPUT_GAME_NUMBER_MESSAGE);
-		return Console.readLine();
+	public static String inputGameNumberMessage() {
+		String input = getInput();
+		validateBlankInputGameNumber(input);
+		return input;
 	}
+
+	private static void validateBlankInputGameNumber(String input) {
+		if (input == null || input.isBlank()) {
+			throw new IllegalArgumentException("공백 입력 예외 발생 테스트");
+		}
+	}
+
 
 	public static String printInputGameRestartMessage() {
 		System.out.println(INPUT_GAME_RESTART_MESSAGE);
+		return Console.readLine();
+	}
+
+	private static String getInput() {
 		return Console.readLine();
 	}
 }
