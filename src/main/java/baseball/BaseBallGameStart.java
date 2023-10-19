82413c3a1c486d0c1a7330a8fa22cd5d4df3e6ed
haroya01 <haroya01@naver.com>
2023-10-20 06:20:57 +0900

    style: BaseBallGameStart 주석 제거

--- src/main/java/baseball/BaseBallGameStart.java
+++ src/main/java/baseball/BaseBallGameStart.java
@@ -6,22 +6,16 @@ import java.util.List;
 
 public class BaseBallGameStart {
 
-    // 시작
     public BaseBallGameStart() {
         output();
         ComputerNumbers computerNumbers = ComputerNumbers.createRandomNumbers();
-        //TODO: 게임을 플레이 하는 로직 구현 할것
         new BaseBallGamePlay(computerNumbers);
-
-        System.out.println(computerNumbers.getNumbers());
     }
 
-    // 출력
     private void output() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    // 컴퓨터의 숫자를 생성
     public static class ComputerNumbers {
         private final List<Integer> numbers;
 
