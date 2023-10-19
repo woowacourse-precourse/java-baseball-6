fcf6d60b7fbed24003b4ff9118b7cd9090560bc4
devholic22 <hyunjoon.tech@gmail.com>
2023-10-19 23:09:41 +0900

    Feat: 자릿수 상수화 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,9 @@ import baseball.view.AskView;
 import baseball.view.StartView;
 
 public class Application {
+
+    private static final int PLAY_NUMBER_DIGIT = 3;
+
     public static void main(String[] args) {
         StartView.welcome();
         AskView.askNumberInput();
