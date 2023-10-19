f59823e6e2a37f01321e46e3dd97febe6b1638e9
shinjaewon99 <tlswodnjs99@naver.com>
2023-10-19 17:04:27 +0900

    feat : 게임 시작, 숫자 입력에 대한 출력메시지 기능 추가

---
+++ src/main/java/baseball/OutPutView.java
@@ -0,0 +1,11 @@
+package baseball;
+
+public class OutPutView {
+    public void printBaseBallGameStartMessage() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void printRequestInputNumberMessage() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+}
