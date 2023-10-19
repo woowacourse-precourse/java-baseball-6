3aa0b71b027c4dd5a97bfa987dbdc1ca4573ef56
yujeongkwon <87534067+yujeongkwon@users.noreply.github.com>
2023-10-20 08:49:32 +0900

    feat(domain/Hint.java): 같은 수가 다른 자리=> 볼

---
+++ src/main/java/baseball/domain/Hint.java
@@ -0,0 +1,34 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Hint {
+    private int ballCount;
+    private int strikeCount;
+
+    public int getBallCount(){
+        return ballCount;
+    }
+
+    public int getStrikeCount(){
+        return strikeCount;
+    }
+
+    public boolean isWin(){
+        if(strikeCount == 3)
+            return true;
+        else
+            return false;
+    }
+
+    private void updateBall(List<Integer> userNumbers, List<Integer> systemNumbers){
+        int count = 0;
+
+        for(int i=0; i<3; i++)
+            if (userNumbers.get(i) != systemNumbers.get(i) && systemNumbers.contains(userNumbers.get(i)))
+                count++;
+
+        ballCount = count;
+    }
+
+}
