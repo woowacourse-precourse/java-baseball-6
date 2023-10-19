bbb74c34cefbebe7a2e54a6f9e01e69675740d1e
heiler <threepebbles@naver.com>
2023-10-19 20:17:20 +0900

    feat: NumberList 객체에 대한 equals() 구현

---
+++ src/main/java/baseball/Model/NumberList.java
@@ -0,0 +1,31 @@
+package baseball.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberList {
+    private List<Integer> numberList;
+
+    public NumberList() {
+        numberList = new ArrayList<>();
+    }
+    public NumberList(List<Integer> numberList){
+        this.numberList = numberList;
+    };
+
+    public List<Integer> getNumberList() {
+        return numberList;
+    }
+
+    public void setNumberList(List<Integer> numberList) {
+        this.numberList = numberList;
+    }
+
+    public boolean equals(NumberList a) {
+        boolean isEqual = true;
+        for(int i = 0; i< numberList.size(); i++) {
+            isEqual &= (numberList.get(i).equals(a.numberList.get(i)));
+        }
+        return isEqual;
+    }
+}
