c0008b57e9689ee3ed0367ca04964b295c4aa98d
Hyeon0208 <dmsgktn0208@gmail.com>
2023-10-19 21:44:20 +0900

    feat : 중복 숫자 검증 클래스

---
+++ src/main/java/baseball/validation/NumberDuplicateValidator.java
@@ -0,0 +1,23 @@
+package baseball.validation;
+
+import baseball.exception.NumberDuplicateException;
+import baseball.view.ErrorMessage;
+import java.util.HashSet;
+import java.util.Set;
+
+public class NumberDuplicateValidator {
+
+    private NumberDuplicateValidator() {
+    }
+
+    public static void validate(String number) {
+        Set<Character> characterSet = new HashSet<>();
+        for (char c : number.toCharArray()) {
+            characterSet.add(c);
+        }
+
+        if (characterSet.size() != number.length()) {
+            throw new NumberDuplicateException(ErrorMessage.NUMBER_DUPLICATE.getMessage());
+        }
+    }
+}
