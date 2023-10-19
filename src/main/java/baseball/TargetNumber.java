20b98960a4dcc54e6fee11cc605d56d47c222e40
U-keun Song <sukeun319@gmail.com>
2023-10-19 19:24:37 +0900

    Comment : 주석 추가
    다른 클래스에서 사용할 수 있는 메서드에 주석을 추가했다.

--- src/main/java/baseball/TargetNumber.java
+++ src/main/java/baseball/TargetNumber.java
@@ -14,9 +14,18 @@ class TargetNumber {
             number[i] = value;
         }
     }
-
+    /*
+    TargetNumber 객체를 생성하는 정적 팩토리 메서드.
+     */
     public static TargetNumber generate() { return new TargetNumber(); }
 
+    /*
+    TargetNumber 객체가
+    특정 숫자 num을 특정 위치 idx에 갖고 있는지 물어보는 메서드.
+    반환 값은 길이가 2인 boolean 타입 배열이다.
+    0 번째 원소는 숫자가 있는지 여부를 말하고,
+    1 번째 원소는 숫자가 있다면 위치도 맞는지 여부를 말한다.
+     */
     public boolean[] has(int num, int idx) {
         boolean[] check = new boolean[2];
         for (int i = 0; i < 3; i++) {
