6440cdcca328cee0f69d8796fba1b31d9b22b6b4
haroya01 <haroya01@naver.com>
2023-10-20 06:00:47 +0900

    feat: '게임을 다시 시작 할지, 종료할지 선택지를 제시한다' 기능 구현

--- src/main/java/baseball/BaseBallGameEnd.java
+++ src/main/java/baseball/BaseBallGameEnd.java
@@ -6,9 +6,14 @@ public class BaseBallGameEnd {
     //TODO: 게임 종료 및 재시작
 
     public BaseBallGameEnd() {
+        BaseBallGameEnd();
+    }
+
+    public boolean BaseBallGameEnd() {
         output();
         String playerOrder = input();
         System.out.println("input : " + playerOrder);
+        return orderCheck(playerOrder);
     }
 
     private void output() {
@@ -18,6 +23,18 @@ public class BaseBallGameEnd {
 
     private String input(){
         return Console.readLine();
+    }
+
+    private boolean orderCheck(String playerOrder){
+        if(playerOrder.equals("1")){
+            return true;
+        }
+        else if(playerOrder.equals("2")){
+            return false;
+        }
+        else{
+            throw new IllegalArgumentException();
+        }
     }
 }
 
