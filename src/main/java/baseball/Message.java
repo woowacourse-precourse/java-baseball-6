7d0c1252d73a183b7dad43663512f6f027d0fdf3
IMWoo94 <leemeawoo94@gmail.com>
2023-10-19 17:53:13 +0900

    야구 게임 관련 문구 메시지 생성 2023.10.19

---
+++ src/main/java/baseball/Message.java
@@ -0,0 +1,12 @@
+package baseball;
+
+public class Message {
+
+    public static final String START_MSG = "숫자 야구 게임을 시작합니다.";
+    public static final String END_MSG = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String REGAME_MSG = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public static final String BALL_MSG = "볼";
+    public static final String STRIKE_MSG = "스트라이크";
+    public static final String NON_MSG = "낫싱";
+}
---
+++ src/test/java/baseball/MessageTest.java
@@ -0,0 +1,26 @@
+package baseball;
+
+import static baseball.Message.BALL_MSG;
+import static baseball.Message.END_MSG;
+import static baseball.Message.NON_MSG;
+import static baseball.Message.REGAME_MSG;
+import static baseball.Message.START_MSG;
+import static baseball.Message.STRIKE_MSG;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class MessageTest {
+
+    @Test
+    void msgCheck() {
+        assertThat(START_MSG).isEqualTo("숫자 야구 게임을 시작합니다.");
+        assertThat(END_MSG).isEqualTo("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        assertThat(REGAME_MSG).isEqualTo("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        assertThat(NON_MSG).isEqualTo("낫싱");
+        assertThat(BALL_MSG).isEqualTo("볼");
+        assertThat(STRIKE_MSG).isEqualTo("스트라이크");
+
+    }
+
+}
