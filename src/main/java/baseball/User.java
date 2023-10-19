0ecd18a4d0292f4b4a12231fa2884f167a1d3236
bitesnail <rkwoal216@gmail.com>
2023-10-19 22:22:33 +0900

    feat: implementation of id-1

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,6 +1,9 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class User {
+    private static int userNumber = 0;
 
     static boolean isParsableToInt(final String input){
         try{
@@ -21,6 +24,21 @@ public class User {
         if(input.length() != 3){
             throw new IllegalArgumentException("Input 3 digit integer.");
         }
+    }
+
+    static void setUserNumber(int userNumber){
+        User.userNumber = userNumber;
+    }
+
+    public static int getUserNumber(){
+        return userNumber;
+    }
+
+    public static void readNumber(){
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+        validateInput(input);
+        setUserNumber(Integer.parseInt(input));
     }
 
 }
