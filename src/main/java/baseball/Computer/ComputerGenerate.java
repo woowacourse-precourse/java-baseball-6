4cd4a8a13a084b851f435625ee9eb243fd32bee4
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2023-10-20 00:52:45 +0900

    ComputerGenerate 기능 구현

---
+++ src/main/java/baseball/Computer/ComputerGenerate.java
@@ -0,0 +1,19 @@
+package baseball.Computer;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComputerGenerate {
+    public List<Integer> getTarget(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+}
