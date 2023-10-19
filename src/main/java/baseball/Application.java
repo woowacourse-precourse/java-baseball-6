8a57972e4786f6489edcaa7b95548290bbf44ae6
aka-nick <bechalrang@gmail.com>
2023-10-19 22:46:47 +0900

    추가: 게임 재시작 종료메시지 출력 및 입력 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,15 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
 
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public static void main(String[] args) {
         System.out.println(GAME_START_MESSAGE);
 
-        BaseballGame baseballGame = new BaseballGame();
-        baseballGame.play();
+        boolean whetherPlay = true;
+        while (whetherPlay) {
+            BaseballGame baseballGame = new BaseballGame();
+            baseballGame.play();
 
+            System.out.println(GAME_RESTART_MESSAGE);
+            String inputRestartString = Console.readLine();
+            // TODO: 입력값에 대한 유효성 검증 필요
+            int inputRestart = Integer.parseInt(inputRestartString);
+
+        }
 
     }
 
