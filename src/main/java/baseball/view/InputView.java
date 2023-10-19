d536a866c966837b874956f25acdd122f91636d4
Jeon HoGeon <jhg2819@naver.com>
2023-10-19 17:15:19 +0900

    feat: 시작 메시지 출력 기능 구현

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,10 @@
+package baseball.view;
+
+public class InputView {
+
+    private static final String START_STRING = "숫자 야구 게임을 시작합니다.";
+
+    public static void printStart() {
+        System.out.println(START_STRING);
+    }
+}
