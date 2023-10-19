122953d26ac1328db1ce96d7fbcc3e9cee43262a
kongji <kongji4092@gmail.com>
2023-10-19 20:41:45 +0900

    refactor: 게임 시작 출력 문구 함수명 수정
    startGame() -> printStartGame()

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,7 +3,8 @@ package baseball.view;
 public class OutputView {
     private static final String START_GAME_MESSAGE = "숫자 야구 게임을 시작합니다.";
 
-    public void startGame() {
+    public void printStartGame() {
         System.out.println(START_GAME_MESSAGE);
     }
+
 }
