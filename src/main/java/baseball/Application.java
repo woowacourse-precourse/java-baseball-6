1d95b3efdec18cf91ba90969adb01f9f38120dd8
LeeJiWon <dlwldnjs1009@gmail.com>
2023-10-19 20:31:18 +0900

    feat: 잘못된 입력 검증 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -22,9 +22,25 @@ public class Application {
         List<Integer> player = new ArrayList<>();
         String playerInput = Console.readLine();
 
+        if (playerInput.length() != 3) {
+            throw new IllegalArgumentException("입력값은 3자리의 정수여야 합니다.");
+        }
+
         for (int i = 0; i < playerInput.length(); i++) {
             char c = playerInput.charAt(i);
-            int number = Integer.parseInt(String.valueOf(c));
+            int number;
+
+
+            try {
+                number = Integer.parseInt(String.valueOf(c));
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("입력값은 정수여야 합니다.");
+            }
+
+            if (number < 1 || number > 9) {
+                throw new IllegalArgumentException("입력값은 1부터 9까지 중 하나의 정수여야 합니다.");
+            }
+
             player.add(number);
         }
     }
