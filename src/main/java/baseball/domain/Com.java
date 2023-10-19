362cc28589846a2ca123bd9ad9e22a4c8049a41e
shin5774 <babo5774@naver.com>
2023-10-19 17:31:35 +0900

    Feat: "값 비교 테스트코드 기반 도메인 초안 구현"
    값 비교 기능의 테스트코드에 필요한 도메인들(User,Com)의 초안 구현.
    내부 함수는 차후 구현 예정.

---
+++ src/main/java/baseball/domain/Com.java
@@ -0,0 +1,8 @@
+package baseball.domain;
+
+public class Com {
+    public String value;
+    public Com(String value){
+
+    }
+}
---
+++ src/main/java/baseball/domain/User.java
@@ -0,0 +1,17 @@
+package baseball.domain;
+
+public class User {
+    public String value;
+    public int strike;
+    public int ball;
+
+
+    public User(String value){
+
+    }
+
+    public void compare(String value){
+
+    }
+
+}
