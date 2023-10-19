6522d5399ca7b3a950b2178f6c614fe545d77d62
rlagudwls5518 <66249379+rlagudwls5518@users.noreply.github.com>
2023-10-20 00:13:42 +0900

    Add files via upload

---
+++ src/main/java/baseball/First.java
@@ -0,0 +1,13 @@
+package baseball;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("된거냐");
+		System.out.println("된거냐");
+		System.out.println("된거냐");
+
+	}
+
+}
---
+++ src/main/java/baseball/Second.java
@@ -0,0 +1,12 @@
+package baseball;
+
+public class Second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("제발되라 제발");
+
+	}
+
+}
