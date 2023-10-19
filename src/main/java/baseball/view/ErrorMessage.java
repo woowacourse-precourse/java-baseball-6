1fb31e377fcf567cc93d5d8965d1abe8152c77f0
Hyeon0208 <dmsgktn0208@gmail.com>
2023-10-19 21:40:53 +0900

    feat : 에러 메시지를 갖는 Enum 클래스 추가

---
+++ src/main/java/baseball/view/ErrorMessage.java
@@ -0,0 +1,18 @@
+package baseball.view;
+
+public enum ErrorMessage {
+    NUMBER_RANGE("숫자는 1부터 9까지의 수로만 이뤄져야 합니다."),
+    NUMBER_TYPE("숫자로만 구성되어야 합니다."),
+    NUMBER_SIZE("숫자의 길이는 3이여야 합니다."),
+    NUMBER_DUPLICATE("중복되는 수가 존재할 수 없습니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
