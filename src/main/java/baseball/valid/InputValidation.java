c1bd65595a6f4129ee65b25c45fc9b871e3743c0
jongwonpc <c3392302@naver.com>
2023-10-20 02:09:59 +0900

    feat: 사용자로부터 숫자 입력 받는 기능 검증 구현

---
+++ src/main/java/baseball/valid/InputValidation.java
@@ -0,0 +1,35 @@
+package baseball.valid;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class InputValidation {
+
+    public static void validateInputNum(String input){
+        validateOnlyNum(input);
+        validateAllDifferentNumAndSize(input);
+        validateNumRange(input);
+    }
+
+    private static void validateOnlyNum(String input){
+        String reg = "^[0-9]*$";
+        if(!input.matches(reg)){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateAllDifferentNumAndSize(String input){
+        if(input.chars().count() == 3  && input.chars().distinct().count() != 3){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateNumRange(String input){
+        if(!input.chars().map(letter -> letter - '0').boxed().allMatch(num ->num >=1 && num <= 9)){
+            throw new IllegalArgumentException();
+        }
+    }
+
+}
