3e03f5d1099b3eef9a1dc182c642d7181e2ed11f
xogus3492 <wkdxogus3492@gmail.com>
2023-10-19 20:14:46 +0900

    feat(player): 수를 반환하는 기능 구현

---
+++ src/main/java/baseball/model/Player.java
@@ -0,0 +1,15 @@
+package baseball.model;
+
+import java.util.List;
+
+public class Player {
+    private final List<Integer> numbers;
+
+    public Player(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return this.numbers;
+    }
+}
