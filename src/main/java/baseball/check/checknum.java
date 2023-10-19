6bf67cf6a95dcbe0e7ddaffac98a617f40758ffb
Icarly2701 <dbtjdsla0127@gmail.com>
2023-10-19 22:04:26 +0900

    feat: 제대로된 입력값 여부 확인 추가

---
+++ src/main/java/baseball/check/checknum.java
@@ -0,0 +1,9 @@
+package baseball.check;
+
+public interface checknum {
+    public boolean checkIsNum(String num);
+
+    public boolean checkIsThreeDigit(String num);
+
+    public boolean checkIsDuplicate(String num);
+}
---
+++ src/main/java/baseball/check/checkthreenum.java
@@ -0,0 +1,47 @@
+package baseball.check;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class checkthreenum implements checknum{
+    @Override
+    public boolean checkIsNum(String num) {
+        try {
+            Integer.parseInt(num);
+            return true;
+        }catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public boolean checkIsThreeDigit(String num) {
+        if(num.length() == 3){
+            return true;
+        }
+        throw new IllegalArgumentException();
+    }
+
+    @Override
+    public boolean checkIsDuplicate(String num) {
+        char cc = 'a';
+        for(char c : num.toCharArray()){
+            if(cc == c){
+                throw new IllegalArgumentException();
+            }
+        }
+        return true;
+    }
+
+    public boolean checkIsOneToNine(String num){
+
+        List<Integer> digit = Arrays.asList(1,2,3,4,5,6,7,8,9);
+        for(char c : num.toCharArray()){
+            c -= '0';
+            if(!digit.contains(c))
+                throw new IllegalArgumentException();
+        }
+        return true;
+    }
+}
