04629640842d4dc96e4a332c550ceb0932492084
정성욱 <rkfka7849@gmail.com>
2023-10-19 20:46:02 +0900

    test(Result): 스트라이크/볼 개수를 저장하는 기능 구현

---
+++ src/main/java/baseball/domain/Result.java
@@ -0,0 +1,11 @@
+package baseball.domain;
+
+public class Result {
+    int numberOfStrikes;
+    int numberOfBalls;
+
+    Result(int newStrikes, int newBalls) {
+        numberOfStrikes = newStrikes;
+        numberOfBalls = newBalls;
+    }
+}
