085d765b0b01b56bcb54e6dafa0df04833033ffa
02ggang9 <02ggang9@gmail.com>
2023-10-19 20:20:52 +0900

    feat(InputView): 사용자의 입력 값을 받는 기능 구현

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,13 @@
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String getUserInput() {
+        return scanner.next();
+    }
+
+}
