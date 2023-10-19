31feb4b929118da016c556866bb494e8e0abc45f
HongChaa <jun761_1@naver.com>
2023-10-19 17:33:13 +0900

    feat(ComputerController): 컴퓨터가 임의의 세 자리 수 발생시키기 기능 추가

---
+++ src/main/java/baseball/controller/ComputerController.java
@@ -0,0 +1,30 @@
+package baseball.controller;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComputerController {
+
+    public static List<Integer> getComputerRandomNumber() {
+
+        return generateRandomNumber();
+    }
+
+    private static List<Integer> generateRandomNumber() {
+        List<Integer> randomNumberList = new ArrayList<>();
+
+        while(randomNumberList.size() < 3) {
+
+            int newRandomNumber = Randoms.pickNumberInRange(1, 9);
+
+            if (!randomNumberList.contains(newRandomNumber)) {
+                randomNumberList.add(newRandomNumber);
+            }
+
+        }
+
+        return randomNumberList;
+    }
+}
---
+++ src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,16 @@
+package baseball.controller;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class GameController {
+    public static void playGame() {
+
+        System.out.println(ComputerController.getComputerRandomNumber().toString());
+
+
+    }
+
+
+}
