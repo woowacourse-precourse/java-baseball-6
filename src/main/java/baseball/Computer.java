2d01e17b89a39aeae8fd090e065e70f2cae0ba78
donghoony <aru0504@naver.com>
2023-10-20 01:27:37 +0900

    refactor: 정답을 컴퓨터 객체가 가지도록 수정

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,8 +1,32 @@
 package baseball;
 
+import static baseball.GameClient.BALL_LENGTH;
+import static baseball.GameClient.RANGE_MAXIMUM_VALUE;
+import static baseball.GameClient.RANGE_MINIMUM_VALUE;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 /**
  * 컴퓨터의 계산을 처리하는 클래스.
  * <p>같은 수가 같은 자리에 있으면 스트라이크, 다른 자리에 있으면 볼, 같은 수가 전혀 없으면 낫싱이란 힌트를 제공한다.
  */
 public class Computer {
+
+    private List<Integer> answerNumbers;
+
+    public void refreshAnswer() {
+        this.answerNumbers = createAnswer();
+    }
+
+    private List<Integer> createAnswer() {
+        HashSet<Integer> answerNumbers = new HashSet<>();
+        while (answerNumbers.size() < BALL_LENGTH) {
+            int pickedNumber = Randoms.pickNumberInRange(RANGE_MINIMUM_VALUE, RANGE_MAXIMUM_VALUE);
+            answerNumbers.add(pickedNumber);
+        }
+        return new ArrayList<>(answerNumbers);
+    }
 }
