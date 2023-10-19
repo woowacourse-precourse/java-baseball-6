a9557123165bb1869ee832cc3502293fce0aa4ce
minseo  jo <liging12@naver.com>
2023-10-20 00:57:25 +0900

    feat: [view] OutputView 기능 추가
    게임 힌트에 대한 메시지도 출력해야하는데, GameResult 형식으로 반환할 지 아니면 String 형태로 반환 할 지 몰라서 추후 코드 추가할 예정

---
+++ src/main/java/baseball/view/console/OutputView.java
@@ -0,0 +1,35 @@
+package baseball.view.console;
+
+import baseball.domain.GameResult;
+
+public class OutputView {
+    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.\n";
+    private static final String INPUT_BALLS_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n";
+    private static final String INPUT_GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
+
+    public static void gameStartMessage() {
+        displayMessage(GAME_START_MESSAGE);
+    }
+
+    public static void inputBallsMessage() {
+        displayMessage(INPUT_BALLS_MESSAGE);
+    }
+
+    public static void gameEndMessage() {
+        displayMessage(GAME_END_MESSAGE);
+    }
+
+    public static void inputGameRestartMessage() {
+        displayMessage(INPUT_GAME_RESTART_MESSAGE);
+    }
+
+/*    public static void hintsMessage(GameResult message) {
+        displayMessage(message);
+    }*/
+
+
+    private static void displayMessage(String message) {
+        System.out.print(message);
+    }
+}
