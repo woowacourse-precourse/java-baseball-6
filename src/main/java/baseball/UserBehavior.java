c1a6ec971671ed53972878aef808fb38db9ecd86
yang <ywj9811@gmail.com>
2023-10-20 01:09:55 +0900

    fix : 정답시 반환형 수정

--- src/main/java/baseball/UserBehavior.java
+++ src/main/java/baseball/UserBehavior.java
@@ -13,7 +13,7 @@ public class UserBehavior {
         this.computerNumbers = computerNumbers;
     }
 
-    public void input() {
+    public int input() {
         strike = 0;
         ball = 0;
 
@@ -28,9 +28,10 @@ public class UserBehavior {
 
         while (result()) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            return;
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            return Integer.parseInt(Console.readLine());
         }
-        input();
+        return input();
     }
 
     public void check(int index, int number) {
