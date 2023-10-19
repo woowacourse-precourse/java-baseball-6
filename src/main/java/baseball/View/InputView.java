d961840f9ac4b5eb2c07248672e63f6c1465c43b
heiler <threepebbles@naver.com>
2023-10-19 20:54:03 +0900

    feat(InputView): 3자리수 입력 받는 화면 출력 기능 추가

---
+++ src/main/java/baseball/View/InputView.java
@@ -0,0 +1,9 @@
+package baseball.View;
+
+public class InputView {
+    private static final String ENTER_NUMBERS_MESSAGE = "숫자를 입력해주세요 : ";
+
+    public static void printEnterNumbers() {
+        System.out.println(ENTER_NUMBERS_MESSAGE);
+    }
+}
