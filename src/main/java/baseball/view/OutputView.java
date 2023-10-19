9e087759d578c919ce7d06bf694846d41c41815a
donghyunkim <ghffu405@gmail.com>
2023-10-19 18:04:50 +0900

    feat: 볼 개수 출력 메소드 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -5,4 +5,8 @@ public class OutputView {
     public void printBallNStrike(int ball, int strike){
         System.out.println(ball + "볼 " + strike + "스트라이크");
     }
+    
+    public void printBall(int ball){
+        System.out.println(ball + "볼");
+    }
 }
