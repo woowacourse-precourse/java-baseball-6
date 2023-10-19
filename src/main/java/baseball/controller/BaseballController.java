db8dcc37ddf481fdfc02d776c545e9f13b047162
donghyunkim <ghffu405@gmail.com>
2023-10-19 16:45:38 +0900

    feat: 1~9까지 서로다른3개의 수 제공 메소드추가

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -1,9 +1,28 @@
 package baseball.controller;
 
+import baseball.constant.NumberConstant;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static baseball.constant.NumberConstant.*;
+
 public class BaseballController {
 
     public void run(){
 
+    }
+
+    public List<Integer> getNumbers(){
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(START_NUMBER, END_NUMBER);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+        return numbers;
     }
 
 
