8d87240272467d508cad887dc24f02060e7d2637
sami355-24 <ddung355@gmail.com>
2023-10-19 22:48:56 +0900

    feat : 프로그램 시작 점 추가
    main에서 입력값 받는 클래스 추가하고 해당 클래스의 메소드 호출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import baseball.Input.InputAnswer;
+import baseball.Input.InputAnswerImpl;
+
+import static java.lang.System.*;
+
+
 public class Application {
+
+    private static final String startupMessage = "숫자 야구 게임을 시작합니다.";
+    private static final InputAnswer inputAnswer = new InputAnswerImpl();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        out.println(startupMessage);
+        try{
+            inputAnswer.inputAnswer();
+        } catch (IllegalArgumentException e) {
+            out.println(e.getMessage());
+            throw e;
+        }
     }
 }
