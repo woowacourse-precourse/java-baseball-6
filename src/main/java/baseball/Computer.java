285df98726b4035b8afe8d24656a8cd294eeefeb
Haebin <joker7011@naver.com>
2023-10-19 17:56:06 +0900

    feat: Couputer 랜덤 숫자 뽑기 기능 구현

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,20 @@
+package baseball;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+import static java.util.stream.Collectors.joining;
+
+import java.util.List;
+
+public class Computer {
+
+    public String pickTargetNumber(int numLength) {
+        List<Integer> pickedInteger = pickUniqueNumbersInRange(1, 9, numLength);
+        return convertIntegerListToString(pickedInteger);
+    }
+
+    private String convertIntegerListToString(List<Integer> pickedInteger) {
+        return pickedInteger.stream()
+            .map(Object::toString)
+            .collect(joining(""));
+    }
+}
