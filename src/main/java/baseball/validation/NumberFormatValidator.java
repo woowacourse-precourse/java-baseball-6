6c79b06d726e7471cfd7bfc887f182d1e7a12538
Hyeon0208 <dmsgktn0208@gmail.com>
2023-10-19 21:44:08 +0900

    feat : 숫자 값 형식 검증 클래스

---
+++ src/main/java/baseball/validation/NumberFormatValidator.java
@@ -0,0 +1,20 @@
+package baseball.validation;
+
+import baseball.view.ErrorMessage;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class NumberFormatValidator {
+
+    private NumberFormatValidator() {
+    }
+
+    public static void validate(String number) {
+        Pattern pattern = Pattern.compile(RegexPattern.ONLY_NUMBER.getValue());
+        Matcher matcher = pattern.matcher(number);
+
+        if (!matcher.matches()) {
+            throw new NumberFormatException(ErrorMessage.NUMBER_TYPE.getMessage());
+        }
+    }
+}
