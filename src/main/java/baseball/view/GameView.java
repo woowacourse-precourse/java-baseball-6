e451f27a9359ecf21cac25a43327a911f3c651b7
HongChaa <jun761_1@naver.com>
2023-10-19 18:22:27 +0900

    feat(GameView): 게임 내 메시지 출력 기능 구현

---
+++ src/main/java/baseball/view/GameView.java
@@ -0,0 +1,25 @@
+package baseball.view;
+
+public class GameView {
+
+    public void printStartGameMessage() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void printSuccessGameMessage() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료 ");
+    }
+
+    public void printOptionMessage() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    public void printInputNumberMessage() {
+        System.out.println("숫자를 입력해주세요 : ");
+    }
+
+    public void printAnswerHintMessage(String hintMessage) {
+        System.out.println(hintMessage);
+    }
+
+}
