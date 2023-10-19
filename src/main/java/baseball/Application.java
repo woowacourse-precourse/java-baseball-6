309e3c5cb17abbe07a9263869fc46435f0c48c38
zzawang <hobin0116@naver.com>
2023-10-19 21:29:38 +0900

    feat: 게임 시작 기능 구현
    - 게임 시작 문구 출력
    - 사용자에게 숫자를 입력받고 결과를 출력하는 함수 호출
    - 게임이 종료되면 사용자에게 1 또는 2를 입력받아 재시작 또는 게임을 완전히 종료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -11,6 +12,22 @@ public class Application {
     private final static int COMPUTER_NUM_SIZE = 3;
 
     public static void main(String[] args) {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        String flag;
+        do {
+            startGame();
+            flag = askReplayOrExit();
+        }while (flag.equals("1"));  // 1을 입력하면 게임 새로 시작
+    }
+
+    private static void startGame() {
+        // 숫자 야구 게임 시작
+
+        // 사용자에게 입력 받기
+
+        // 입력받은 숫자에 대한 결과 출력
+
+        // 사용자가 3개의 숫자를 모두 맞힌 경우 게임 종료
 
     }
 
@@ -23,5 +40,13 @@ public class Application {
             }
         }
         return computer;
+    }
+
+    private static String askReplayOrExit() {
+        // 사용자에게 숫자를 입력받아 return
+
+        // 1과 2가 아닌 다른 수를 입력할 경우 애플리케이션은 종료
+
+        return null;
     }
 }
