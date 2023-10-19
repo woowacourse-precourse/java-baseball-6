7672965d96d7989e8c86f5c915703391ff52d93b
박성춘 <songchon100@gmail.com>
2023-10-19 19:48:04 +0900

    faet : 프로젝트 셋업

---
+++ src/main/java/baseball/model/NumberBaseball.java
@@ -0,0 +1,10 @@
+package baseball.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberBaseball {
+    // 더 좋은 변수명 생각해보기
+    private List<Integer> randNum = new ArrayList<>();
+
+}
