20c2031c220a99cb7b80a3b19688efbb1998cc71
heiler <threepebbles@naver.com>
2023-10-19 22:53:15 +0900

    refactor(MainController): 일부 변수명 변경, 상수 위치 변경

--- src/main/java/baseball/Controller/MainController.java
+++ src/main/java/baseball/Controller/MainController.java
@@ -4,6 +4,8 @@ import baseball.View.InputView;
 import camp.nextstep.edu.missionutils.Console;
 
 public class MainController {
+    private static final char RESTART = '1';
+    private static final char END_GAME = '2';
     GameController gameController;
 
     public MainController() {
@@ -20,19 +22,17 @@ public class MainController {
 
     private static class InputController {
         private static final String WRONG_INPUT_WHETHER_RESTART_MESSAGE = "1 또는 2를 입력해주세요.";
-        private static final char RESTART = '1';
-        private static final char END_GAME = '2';
 
         public static boolean scanWhetherRestart() {
             InputView.printEnterWhetherRestart();
-            String inp = Console.readLine();
-            validateWhetherRestart(inp);
+            String inputString = Console.readLine();
+            validateWhetherRestart(inputString);
             return false;
         }
 
-        private static void validateWhetherRestart(String inp) {
-            if (inp.length() != 1) throw new IllegalArgumentException(WRONG_INPUT_WHETHER_RESTART_MESSAGE);
-            else if (inp.charAt(0) != RESTART || inp.charAt(0) != END_GAME)
+        private static void validateWhetherRestart(String inputString) {
+            if (inputString.length() != 1) throw new IllegalArgumentException(WRONG_INPUT_WHETHER_RESTART_MESSAGE);
+            else if (inputString.charAt(0) != RESTART || inputString.charAt(0) != END_GAME)
                 throw new IllegalArgumentException(WRONG_INPUT_WHETHER_RESTART_MESSAGE);
         }
     }
