873e357a1c62dbf3105cfc5401919ff70876c435
SeongIl <biuld1234@gmail.com>
2023-10-19 17:48:02 +0900

    1.selectNumber

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        //1. 선택
+        List<Integer> selectNumber = new ArrayList<>();
+        while (selectNumber.size() != 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!selectNumber.contains(randomNumber)) {
+                selectNumber.add(randomNumber);
+            }
+        }
+
     }
 }
