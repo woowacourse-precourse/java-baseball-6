08f9454565f654df01f40691f8f96aa8d94068e1
shin5774 <babo5774@naver.com>
2023-10-19 22:01:09 +0900

    Feat: "사용자 수 입력 예외처리 기능 구현"
    - 사용자 입력을 parameter로 받아 예외처리 대상인 경우(validate 하지않은 경우) false를 반환하게 함.

---
+++ src/main/java/baseball/domain/Validation.java
@@ -0,0 +1,13 @@
+package baseball.domain;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Validation {
+    public boolean playUserInput(String input){
+        String regex="[0-9]+";
+        HashSet<String> uniqueInput=new HashSet<>(Arrays.asList(input.split("")));
+
+        return input.length()==3 && input.matches(regex) && uniqueInput.size()==3;
+    }
+}
