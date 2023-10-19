0e5557f8f3e21fd362b6c3cf2522fc44f253832f
JunTaeINC <juntea3523@naver.com>
2023-10-19 21:10:46 +0900

    feat : 낫싱, 볼_스트라이크 메시지 추가

--- src/main/java/baseball/game/GameMessages.java
+++ src/main/java/baseball/game/GameMessages.java
@@ -5,6 +5,8 @@ public enum GameMessages {
 	INPUT_NUMBER("숫자를 입력해주세요 : "),
 	BALL("%d볼"),
 	STRIKE("%d스트라이크"),
+	BALL_STRIKE("%d볼 %d스트라이크"),
+	NOTHING("낫싱"),
 	GAME_OVER("3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
 	PLAY_AGAIN("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
