883d443d4d5a40ec3de3d6c2f4ecd42049607243
leeyeongjae <xingxing2001@cau.ac.kr>
2023-10-19 20:11:50 +0900

    1. checkNumber 메소드 구현 2. 스트라이크와 볼의 개수를 크기 2짜리 배열에 저장 후 리턴

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -16,9 +16,25 @@ public class Game {
         }
     }
 
-    /*
-    private int[] checkNumber(){ //컴퓨터의 숫자와 플레이어가 입력한 숫자를 비교하여 스트라이크와 볼의 개수를 리턴하는 메소드
 
+    private int[] checkNumber(){ //컴퓨터의 숫자와 플레이어가 입력한 숫자를 비교하여 스트라이크와 볼의 개수를 리턴하는 메소드
+         int [] strikeAndBall = {0,0}; // 0번째 인덱스: 스트라이크, 1번째 인덱스: 볼
+
+        ArrayList<Integer> comp = this.computer.getNumbers(); //컴퓨터의 숫자
+        ArrayList<Integer> player = this.user.getNumbers(); //유저가 입력한 숫자
+
+        for(int i=0;i<comp.size();i++){
+            for(int j=0;j<player.size();j++){
+                if(comp.get(i).equals(player.get(j))){
+                    if(i==j){ //컴퓨터와 유저의 숫자가 서로 같고 위치도 같다면 스트라이크
+                        strikeAndBall[0]++;
+                    } else{ //값만 서로 같으면 볼
+                        strikeAndBall[1]++;
+                    }
+                }
+            }
+        }
+        return strikeAndBall;
     }
-    */
+
 }
