49dfc5f061ecd8b02d691d5d2854e1f2d2ffdc44
m5s3 <devmyoungsoo2@gmail.com>
2023-10-20 00:07:14 +0900

    refactor: 게임을 지속 하는지에 대한 예외처리 리팩토링 진행함

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -16,7 +16,7 @@ public class Application {
             while (flag) {
                 System.out.println("숫자 야구 게임을 시작합니다.");
                 String number = requestNumber();
-                validateInput(number);
+                isThreeDigit(number);
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 flag = isRestartGame();
@@ -32,23 +32,28 @@ public class Application {
         return input;
     }
 
-    public static void validateInput(String input) {
+    public static void isThreeDigit(String input) {
         if (!Pattern.matches("^\\d{3}$", input)) {
             throw new IllegalArgumentException("입력값이 잘못 되었습니다.");
         }
     }
 
-    public static boolean isRestartGame() {
+    private static boolean isRestartGame() {
+        int input = readUserInput();
+        validateInput(input);
+        return input == 1;
+    }
+
+    private static int readUserInput() {
         try {
-            int input = Integer.parseInt(scanner.nextLine());
-            if (input != 1 && input != 2) {
-                throw new IllegalArgumentException("입력값이 잘못 되었습니다.");
-            }
-            if (input == 1) {
-                return true;
-            }
-            return false;
-        } catch (RuntimeException e) {
+            return Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("입력값이 잘못 되었습니다.");
+        }
+    }
+
+    private static void validateInput(int input) {
+        if (input != 1 && input != 2) {
             throw new IllegalArgumentException("입력값이 잘못 되었습니다.");
         }
     }
