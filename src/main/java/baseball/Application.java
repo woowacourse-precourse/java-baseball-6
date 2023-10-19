508bd5741f98e5a64365d111dc4018fb1100c343
LeeJiWon <dlwldnjs1009@gmail.com>
2023-10-19 21:03:46 +0900

    faet: 게임 재시작/종료 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -72,6 +72,14 @@ public class Application {
             if (strike == 3) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 gameRunning = false;
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                String newGame = Console.readLine();
+                if ("2".equals(newGame)) {
+                    gameRunning = false;
+                }
+                else {
+                    gameRunning = true;
+                }
             }
         }
     }
