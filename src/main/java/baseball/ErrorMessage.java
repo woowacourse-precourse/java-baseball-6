ece789e302a3c8fa885da7a8c7ff8eb994e63ea0
GaBaljaintheroom <junsu1222@naver.com>
2023-10-19 20:50:47 +0900

    feat: 예상된 숫자 입력 에러 메시지 작성

---
+++ src/main/java/baseball/ErrorMessage.java
@@ -0,0 +1,16 @@
+package baseball;
+
+public enum ErrorMessage {
+    INPUT_NUMERIC_ERROR("숫자를 입력해 주세요"),
+    INPUT_ONE_TO_NINE_DIFFERENT_THREE_NUMBERS("1부터 9까지 서로 다른 수로 이루어진 3자리의 수를 입력해 주세요");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
