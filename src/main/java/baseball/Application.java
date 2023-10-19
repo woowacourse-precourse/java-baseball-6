ff2900c5b7e2d7e3600c861b42cf37004952cecf
lynjun97 <lynjun97@naver.com>
2023-10-19 19:25:27 +0900

    Refactor : 예외 처리 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -34,6 +34,10 @@ public class Application {
                 System.out.print("숫자를 입력해주세요 : ");
                 String s = Console.readLine();
 
+                if (s.length()>3){
+                    throw new IllegalArgumentException();
+                }
+
                 for (int x = 0; x < s.length(); x++) {
                     int i1 = Integer.parseInt(String.valueOf(s.charAt(x)));
                     if (i1 < 1 || i1 > 9) {
