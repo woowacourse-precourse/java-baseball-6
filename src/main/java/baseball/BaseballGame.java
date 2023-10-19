fb3f2a9f8710239f73a12c23769f023025161be6
leesangjun <hubls@naver.com>
2023-10-19 22:31:06 +0900

    feat(BaseballGame): 게임 메시지 출력 기능

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,11 @@
+package baseball;
+
+public class BaseballGame {
+	private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+
+	public BaseballGame() {
+	}
+	public void run() {
+		System.out.println(START_MESSAGE);
+	}
+}
