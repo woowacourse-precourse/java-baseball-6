9fec5b011e1e05148b432b6b87626e27922b1159
seungheyon <fhzktm@naver.com>
2023-10-19 18:16:14 +0900

    사용자 입력 받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,6 +5,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -17,6 +19,8 @@ public class Application {
                 computer.add(randomNumber);
             }
         }
+
+        String inputNumber = readLine();
 
     }
 }
