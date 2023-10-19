6ba20361103fdb2774fe9ba19b799ecb22291506
HyungHoKim <hkim1109@naver.com>
2023-10-19 17:05:37 +0900

    BaseballGame 클래스 추가 및 생성자 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        BaseballGame baseballGame = new BaseballGame();
+    }
+}
 
+class BaseballGame{
+    List<Integer> computer = new ArrayList<>();
+    BaseballGame(){
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
     }
 }
