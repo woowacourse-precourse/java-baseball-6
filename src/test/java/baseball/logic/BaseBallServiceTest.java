13a0d200a802bd6b629548f7d98ff6853d2c02d8
bbbbooo <105257665+bbbbooo@users.noreply.github.com>
2023-10-19 21:29:31 +0900

    [Test] : 같은 수가 다른 자리에 있을 때 볼 확인 테스트

--- src/test/java/baseball/logic/BaseBallServiceTest.java
+++ src/test/java/baseball/logic/BaseBallServiceTest.java
@@ -18,7 +18,7 @@ class BaseBallServiceTest {
 
     @DisplayName("같은 수가 같은 자리에 있을 때 스트라이크 확인 테스트")
     @ParameterizedTest
-    @CsvSource({"123,1", "413,1", "135,1", "421,2", "415,2", "125,2", "425,3"})
+    @CsvSource({"123, 1", "413, 1", "135, 1", "421, 2", "415, 2", "125, 2", "425, 3"})
     void confirmSamePositionStrike(int userInput, int expectedStrikes) {
         // given
         int computerInput = 425;
@@ -28,5 +28,19 @@ class BaseBallServiceTest {
 
         // then
         assertTrue(result.contains(expectedStrikes + "스트라이크"));
+    }
+
+    @DisplayName("같은 수가 다른 자리에 있을 때 볼 확인 테스트")
+    @ParameterizedTest
+    @CsvSource({"542, 1", "387, 1", "217, 1", "321, 2", "359, 2", "953, 2","351, 3"})
+    void confirmOtherPositionBall(int userInput, int expectedBall) {
+        // given
+        int computerInput = 135;
+
+        // when
+        String result = baseBallService.getGameResult(computerInput, userInput);
+
+        // then
+        assertTrue(result.contains(expectedBall + "볼"));
     }
 }
