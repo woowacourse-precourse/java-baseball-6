7c964e8d481c8b3079a71ee534a1d947e1d65f00
HwangSeokJin <ghkd3703@gmail.com>
2023-10-19 17:19:47 +0900

    feat: 사용자가 입력한 숫자가 3개인지 확인

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -6,12 +6,19 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Player {
     public List<Integer> readNumber() {
-        String read = Console.readLine();
-        List<Integer> numbers = new ArrayList<>();
+        String read = Console.readLine().strip();
+        validateCount(read);
 
+        List<Integer> numbers = new ArrayList<>();
         for (char c: read.toCharArray()) {
             numbers.add(Character.getNumericValue(c));
         }
         return numbers;
+    }
+
+    private void validateCount(String numbers) {
+        if (numbers.length() != 3) {
+            throw new IllegalArgumentException("3개의 숫자만 입력해야 합니다.");
+        }
     }
 }
