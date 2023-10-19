a9fa84e6c30499b94f5e9eac2f361602e28b2505
mmihye <smilehye910@naver.com>
2023-10-19 22:22:00 +0900

    [FEAT]BaseBallGame 뼈대 구성

---
+++ src/main/java/baseball/BaseBallGame.java
@@ -0,0 +1,26 @@
+package baseball;
+
+public class BaseBallGame {
+    private int answerNum;
+    private Boolean isPlaying = false;
+
+    public void startGame(){
+
+    }
+
+    public void chooseNum(){
+
+    }
+
+    public String returnResult(){
+
+    }
+
+    public void exitGame(){
+
+    }
+
+    private void generateNum(){
+
+    }
+}
