809090bf6a8706a607d9669275e806a6430a37d0
Zeus6768 <akaps6768@gmail.com>
2023-10-19 15:42:49 +0900

    초기화 - 서로 다른 무작위 세 자리 숫자 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    public static List<Integer> computer = getRandomNumber();
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    private static List<Integer> getRandomNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 }
