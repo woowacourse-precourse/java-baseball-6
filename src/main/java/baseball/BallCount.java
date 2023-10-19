ddec10036b53d7c99f485a62e95c453c2dccb7c1
aka-nick <bechalrang@gmail.com>
2023-10-20 00:23:20 +0900

    수정: 볼카운팅 결과를 출력할 때, 포맷에 맞게 출력될 수 있도록 BallCount#toString()을 재정의.

--- src/main/java/baseball/BallCount.java
+++ src/main/java/baseball/BallCount.java
@@ -2,10 +2,31 @@ package baseball;
 
 public record BallCount(long strike, long ball) {
 
+    public static final long NO_COUNT = 0L;
     public static final long MAX_OF_STRIKE = 3L;
+
+    public static final String NOTHING = "낫싱";
+    public static final String BALL = "볼 ";
+    public static final String STRIKE = "스트라이크";
+
 
     public boolean isFullCount() {
         return strike == MAX_OF_STRIKE;
     }
 
+    @Override
+    public String toString() {
+        if (ball == NO_COUNT && strike == NO_COUNT) {
+            return NOTHING;
+        }
+
+        String result = "";
+        if (ball != NO_COUNT) {
+            result += ball + BALL;
+        }
+        if (strike != NO_COUNT) {
+            result += strike + STRIKE;
+        }
+        return result;
+    }
 }
