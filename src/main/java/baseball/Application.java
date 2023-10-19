00181bfc9cb75b9345d7ae50febbd5d4ab94c88d
Beconfident01 <hom0726@naver.com>
2023-10-20 00:46:14 +0900

    feat : 3자리의 수 입력 기능 및 예외처리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,16 +3,22 @@ package baseball;
 import java.util.ArrayList;
 import java.util.List;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
         List<Integer> computerNumbers = randomNums();
+
+        List<Integer> userNumbers = inputNums();
+
         System.out.println("숫자 야구 게임을 시작합니다.");
         System.out.println(computerNumbers);
+        System.out.println(userNumbers);
+
     }
 
-    public static List<Integer> randomNums(){
+    private static List<Integer> randomNums(){
         List<Integer> randomNums = new ArrayList<>();
         while (randomNums.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -22,4 +28,38 @@ public class Application {
         }
         return randomNums;
     }
+
+    private static List<Integer> inputNums() {
+        String inputNumber = Console.readLine();
+
+        String[] inputNumberArray = inputNumber.split("");
+
+        List<Integer> numbers = new ArrayList<>();
+
+        try {
+            if (inputNumberArray.length != 3) {
+                throw new IllegalArgumentException("3자리의 수가 아닙니다.");
+            }
+
+            for (String digit : inputNumberArray) {
+                int number = Integer.parseInt(digit);
+
+                if (number < 1 || number > 9) {
+                    throw new IllegalArgumentException("1부터 9까지의 수가 아닙니다.");
+                } else if (numbers.contains(number)) {
+                    throw new IllegalArgumentException("중복된 숫자를 입력했습니다.");
+                } else {
+                    numbers.add(number);
+                }
+
+            }
+        } catch (NumberFormatException e1) {
+            throw new IllegalArgumentException("숫자가 아닌 문자를 입력했습니다.");
+        } catch (IllegalArgumentException e2) {
+            throw new IllegalArgumentException(e2.getMessage());
+        }
+
+        return numbers;
+    }
+
 }
