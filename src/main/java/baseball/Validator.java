94b0d1796291d1b7f5f8e07c14159d5e9c688bc7
Jaehoon-So <jhspacelover@naver.com>
2023-10-19 19:49:56 +0900

    feat: 입력 검증로직

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -1,2 +1,37 @@
-package baseball;public class Validator {
+package baseball;
+
+public class Validator {
+
+    private static final Validator instance = new Validator();
+
+    private Validator() {
+    }
+
+    public static Validator getInstance() {
+        return instance;
+    }
+
+    public static boolean isValidNumber(String input) {
+        String[] digits = input.split("");
+        Character ch = '1';
+
+        return false;
+    }
+
+    public static boolean validateInput(String input) {
+        return validateInputRange(input) && validateInputDigit(input);
+    }
+
+    public static boolean validateInputRange(String input) {
+        return input.length() == Const.NUMBER_LENGTH;
+    }
+
+    public static boolean validateInputDigit(String input) {
+        try {
+            Integer.parseInt(input);
+            return true;
+        } catch (NumberFormatException e) {
+            throw new IllegalStateException();
+        }
+    }
 }
