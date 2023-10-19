44992bf875f19326f58b181bf6310c9da1311248
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-10-19 17:07:34 +0900

    feat:InputView Class 생성 : 유저넘버 입력 함수, 게임종료 및 재시작 입력 함수

---
+++ src/main/java/baseball/View/InputView.java
@@ -0,0 +1,16 @@
+package baseball.View;
+
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static String inputUserNumber(){
+        return Console.readLine();
+    }
+
+    public static String inputGameOverOrRestart(){
+        return Console.readLine();
+    }
+
+}
