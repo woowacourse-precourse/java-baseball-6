2e14660fce3484286aff17df1a485bf5b1dfc6f0
김영준 <rladudwns164@gmail.com>
2023-10-19 21:01:15 +0900

    feat : 컴퓨터(상대)가 3개의 무작위 숫자 생성 기능 구현

---
+++ src/main/java/Model/Computer.java
@@ -0,0 +1,25 @@
+package Model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.LinkedHashSet;
+
+public class Computer {
+    private String computerNumber;
+
+    public Computer(){
+        createComputerNumber();
+    }
+
+    private void createComputerNumber(){
+        LinkedHashSet<String> computerCheck = new LinkedHashSet<>();
+        while (computerCheck.size() < 3) {
+            computerCheck.add(randomNumberCreate());
+        }
+       this.computerNumber = String.join("",computerCheck);
+
+    }
+
+    private static String randomNumberCreate(){
+        return Integer.toString(Randoms.pickNumberInRange(1,9));
+    }
+}
