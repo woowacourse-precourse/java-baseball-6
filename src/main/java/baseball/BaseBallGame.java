a08654981ffb84cea2dcc232346f6cda888c3938
dlqudtjs <dlqudtjs922@gmail.com>
2023-10-19 18:04:16 +0900

    feat: 입력이 숫자로 이루어져 있는 지 확인하는 기능 구현

---
+++ src/main/java/baseball/BaseBallGame.java
@@ -0,0 +1,14 @@
+package baseball;
+
+public class BaseBallGame {
+
+    private boolean isNumeric(String input) {
+        for(char c : input.toCharArray()) {
+            if(!Character.isDigit(c)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
