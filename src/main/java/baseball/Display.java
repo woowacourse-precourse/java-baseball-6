1a7d7018b1b847e7ff923edae7891fbcbce65220
minjun7410 <minjun7410@gmail.com>
2023-10-19 21:37:02 +0900

    feat: 입력 문자 유효 체크 구현
    입력된 문자가 3자리인지, 모두 숫자인지, 숫자가 서로 다른지 검사하는 기능 구현.

--- src/main/java/baseball/Display.java
+++ src/main/java/baseball/Display.java
@@ -11,8 +11,13 @@ public class Display {
     }
     public int[] splitUserInput(String input){
         int[] result = new int[3];
+        if(input.length() != 3) throw new IllegalArgumentException("3자리 숫자를 입력해주세요.");
         char[] userInputs = input.toCharArray();
+        boolean[] duplicateCheck = new boolean[10];
         for(int i = 0; i < 3; i++){
+            if(userInputs[i] <= '0' || userInputs[i] > '9') throw new IllegalArgumentException("1에서 9사이 숫자를 입력해주세요.");
+            if(duplicateCheck[userInputs[i] - '0']) throw new IllegalArgumentException("중복되지 않는 숫자를 입력해주세요");
+            duplicateCheck[userInputs[i] - '0'] = true;
             result[i] = userInputs[i] - '0';
         }
         return result;
