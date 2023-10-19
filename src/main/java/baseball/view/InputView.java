33cff679ac6891f1a622b601525d9fa43c7afa13
donghyunkim <ghffu405@gmail.com>
2023-10-19 18:00:29 +0900

    feat: 입력메소드 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,10 +1,18 @@
 package baseball.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class InputView {
 
     public void start(){
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    
+    public String input(){
+        System.out.println("숫자를 입력해주세요 : ");
+        return Console.readLine();
+    }
+
 }
