23586d48afaa19ec94e7d6fe54a7d4db2fa17bb0
i3kae <snoger1234@gmail.com>
2023-10-19 18:37:15 +0900

    10/19 라이브러리 사용 체크

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,29 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // -- 프리코스 1주차 라이브러리 사용 예시 코드 --
+        // pickNumberInRange 테스트
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        System.out.println(computer);
+        
+        // readLine() 테스트
+        System.out.println(readLine());
+        // --------------------------------------------
     }
 }
