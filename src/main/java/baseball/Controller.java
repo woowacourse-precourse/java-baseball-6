0e6290c4b0270a7454e5fd9c9a88b9fab417dae0
freekyr7529 <freekyr7529@gmail.com>
2023-10-19 23:14:39 +0900

    refactor: 불필요한 import문 제거
    불필요한 import문 제거

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -1,9 +1,5 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Controller {
     private static Integer command = 1;
 
--- src/test/java/baseball/RandTest.java
+++ src/test/java/baseball/RandTest.java
@@ -1,9 +1,8 @@
 package baseball;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class RandTest {
