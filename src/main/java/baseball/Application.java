8f853112254070db9bd2cc25f4dcf1639905d1cc
leeyeongjae <xingxing2001@cau.ac.kr>
2023-10-19 20:50:44 +0900

    1. 숫자 야구를 진행하는 코드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,9 +8,14 @@ public class Application {
         // TODO: 프로그램 구현
         Game game = new Game(); //게임 인스턴스 생성
         game.printStartMessage();
-        /*
-        숫자 야구 진행
-         */
+        //숫자 야구 진행
+        while(true){
+            game.initGame(); //게임 상태 초기화
+            game.startGame(); //게임 시작
+            if(!game.isRestart()) //게임의 재시작 여부 결정
+                break;
+        }
+
 
     }
 }
