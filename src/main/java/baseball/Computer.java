6a8eed7ceb430afb23a3fa27aeb8ef9981f63cb8
leeyeongjae <xingxing2001@cau.ac.kr>
2023-10-19 19:09:40 +0900

    주석 추가

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -6,8 +6,9 @@ import camp.nextstep.edu.missionutils.Randoms;
 import javax.swing.*;
 
 public class Computer {
-    private ArrayList<Integer> numbers= new ArrayList<>(); //컴퓨터가 뽑은 숫자 조합
+    private ArrayList<Integer> numbers= new ArrayList<>(); //컴퓨터의 숫자 조합
     Computer(){
+        //3개의 난수를 선택함
         while(this.numbers.size() <3){
             int randomNumber = Randoms.pickNumberInRange(1,9);
             if(!numbers.contains(randomNumber)){
@@ -16,7 +17,7 @@ public class Computer {
         }
     }
 
-    ArrayList<Integer>getNumbers(){
+    public ArrayList<Integer>getNumbers(){
         return this.numbers;
     }
 }
