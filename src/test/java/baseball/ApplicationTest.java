98fb4030841e394996bd834720cd05b79344751b
sami355-24 <ddung355@gmail.com>
2023-10-19 23:54:21 +0900

    test : 야구 게임 기능에 대한 정상 시나리오를 테스트하는 코드 추가
    - 스트라이크, 볼, 낫싱확인하는 테스트 코드 추가
    - 무작위 수 생성하는 테스트 코드 추가
    - 사용자의 값을 입력받을때 이미 검증하였기에 정상 시나리오만 테스트

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,10 +1,14 @@
 package baseball;
 
+import baseball.Game.BaseBallGame;
+import baseball.Game.BaseBallGameImpl;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -28,6 +32,9 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    /**
+     * @Description: 유저가 입력 하는 값에 대한 예외 테스트
+     * */
     @Test
     void 예외_테스트_중복된_정수가_입력() {
         assertSimpleTest(() ->
@@ -66,6 +73,42 @@ class ApplicationTest extends NsTest {
                 assertThatThrownBy(() -> runException("test"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+
+    /**
+     * @Description: 야구 게임에 대한 테스트 코드(정상 시나리오)
+     * */
+    static Integer computerNumber = 123;
+    static BaseBallGame baseBallGame = new BaseBallGameImpl();
+    @Test
+    void 스트라이크_테스트() {
+        assertThat(baseBallGame.countStrike(123)).isEqualTo(3);
+        assertThat(baseBallGame.countStrike(124)).isEqualTo(2);
+        assertThat(baseBallGame.countStrike(134)).isEqualTo(1);
+        assertThat(baseBallGame.countStrike(234)).isZero();
+    }
+
+    @Test
+    void 볼_테스트() {
+        assertThat(baseBallGame.countBall(123)).isZero();
+        assertThat(baseBallGame.countBall(124)).isZero();
+        assertThat(baseBallGame.countBall(134)).isEqualTo(1);
+        assertThat(baseBallGame.countBall(234)).isEqualTo(2);
+        assertThat(baseBallGame.countBall(231)).isEqualTo(3);
+    }
+
+    @Test
+    void 낫싱_테스트(){
+        assertThat(baseBallGame.isNothing(123)).isFalse();
+        assertThat(baseBallGame.isNothing(999)).isTrue();
+    }
+
+    @Test
+    void 랜덤_수_생성_테스트(){
+        assertThat(baseBallGame.makeRandomNumber()).isBetween(100, 999);
+        assertThat(baseBallGame.makeRandomNumber()).isBetween(100, 999);
+        assertThat(baseBallGame.makeRandomNumber()).isBetween(100, 999);
     }
 
     @Override
