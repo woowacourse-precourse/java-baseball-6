acfec24f31afbd377d89031a5c82835b5476c4e0
JiEung2 <https://github.com/JiEung2>
2023-10-20 01:11:43 +0900

    스트라이크, 볼, 낫싱인지 계산하는 기능 구현

--- src/main/java/model/BallCounter.java
+++ src/main/java/model/BallCounter.java
@@ -6,7 +6,6 @@ public class BallCounter {
     private final int number = 3;
     public int[] ballCount(List<Integer> userNum, List<Integer> comNum){
         int[] answer = new int[2];
-
         for(int i=0; i<number; i++){
             if(comNum.contains(userNum.get(i))){
                 if(comNum.indexOf(userNum.get(i))==i) answer[0]++;
