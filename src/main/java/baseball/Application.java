135098f3bffa9928048661c567c5354710194909
j2noo <wlsdndml213@naver.com>
2023-10-19 16:13:00 +0900

    feat : 랜덤 숫자 추출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.print("@");
+
+
+
+
+
+      ArrayList<Integer> comArr = new ArrayList<Integer>(3);
+        for(int i=0;i<3;i++){
+          comArr.add(Randoms.pickNumberInRange(1,9));
+        }
+      System.out.print(comArr);
+        
     }
 }
