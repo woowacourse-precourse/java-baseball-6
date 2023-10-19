e19166d69df2047a61b2fe897001bbe33d766476
shinjaewon99 <tlswodnjs99@naver.com>
2023-10-19 17:16:30 +0900

    feat : 유저가 입력한 숫자, 재시작/종료 구분하는 숫자 입력 기능 추가

---
+++ src/main/java/baseball/InputView.java
@@ -0,0 +1,13 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String inputPlayerNumber() {
+        return Console.readLine();
+    }
+
+    public String inputPlayerRetryNumber() {
+        return Console.readLine();
+    }
+}
