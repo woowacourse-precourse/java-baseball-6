8080c73149be902d6d8383ce3dfe0ee220cb0ae0
kongji <kongji4092@gmail.com>
2023-10-19 20:04:46 +0900

    feat: 숫자 입력 기능

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,10 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String inputNumber() {
+        String number = Console.readLine();
+        return number;
+    }
+}
