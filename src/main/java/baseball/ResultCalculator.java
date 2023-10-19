6d671cedd6bb594dbaaa1cc662bc16a31ec2e8f4
정성욱 <rkfka7849@gmail.com>
2023-10-19 22:04:20 +0900

    feat(ResultCalculator): 상대방의 수를 저장하는 기능 구현

---
+++ src/main/java/baseball/ResultCalculator.java
@@ -0,0 +1,13 @@
+package baseball;
+
+import baseball.domain.BallNumber;
+import baseball.domain.Result;
+
+public class ResultCalculator {
+    BallNumber computerNumber;
+
+    ResultCalculator(BallNumber newComputerNumber) {
+        computerNumber = newComputerNumber;
+    }
+
+}
