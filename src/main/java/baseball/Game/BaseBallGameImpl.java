dc19e7fd4c633a625a353bbe5d55be174d5b1aeb
sami355-24 <ddung355@gmail.com>
2023-10-20 00:55:44 +0900

    feat : 정답 생성하는 기능 추가 및 야구 게임 클래스 시작하는 기능 추가
    정답을 랜덤으로 생성하는 기능 생성하고 이를 야구 게임 인스턴스의 메서드 호출시 동작하도록 구현

--- src/main/java/baseball/Game/BaseBallGameImpl.java
+++ src/main/java/baseball/Game/BaseBallGameImpl.java
@@ -14,7 +14,8 @@ public class BaseBallGameImpl implements BaseBallGame {
 
     @Override
     public void initGame() {
-
+        computerNumber = makeRandomNumber();
+        playGame();
     }
 
     @Override
@@ -23,7 +24,14 @@ public class BaseBallGameImpl implements BaseBallGame {
 
     @Override
     public List<Integer> makeRandomNumber() {
-        return null;
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 
     @Override
