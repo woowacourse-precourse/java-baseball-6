7868ccfd97a569e97c09afae8521f1a5b79a7348
Sully <2dcoder@naver.com>
2023-10-19 21:18:22 +0900

    feat: 숫자를 입력 받는 메서드 구현

--- src/main/java/baseball/input/GameInput.java
+++ src/main/java/baseball/input/GameInput.java
@@ -1,9 +1,16 @@
 package baseball.input;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class GameInput {
 
     private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String REQUEST_MESSAGE = "숫자를 입력해주세요 : ";
+
+    public String inputNumberString() {
+        printRequestMessage();
+        return Console.readLine();
+    }
 
     private void printRequestMessage() {
         System.out.print(REQUEST_MESSAGE);
