afddda7cc127ca88d80343a28cdc2510c38b72ad
JiEung2 <https://github.com/JiEung2>
2023-10-20 01:52:32 +0900

    사용자의 숫자 입력 기능 구현

--- src/main/java/controller/InputGameNumber.java
+++ src/main/java/controller/InputGameNumber.java
@@ -1,4 +1,18 @@
 package controller;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputGameNumber {
+    private int number = 3;
+    public List<Integer> inputNum(){
+        String tmp = Console.readLine();
+        List<Integer> userNum = new ArrayList<>();
+        for(int i=0; i<number; i++){
+            userNum.add(Character.getNumericValue(tmp.charAt(i)));
+        }
+        return userNum;
+    }
 }
