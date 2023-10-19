0e84e4d21dd605cb7bf159278d04d55bb1c7edea
zangsu <zangsu_@naver.com>
2023-10-19 16:18:53 +0900

    feat/Game : make skeleton code in Game

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,26 @@
+package baseball;
+
+public class Game {
+
+    public void startGame(){
+
+        boolean restart = false;
+        do{
+            play();
+
+            //사용자에게 재시작 여부를 확인
+            //restart = ...
+        }while(restart);
+        //TODO : 종료하기 전 Console.close() 호출
+
+    }
+    public void play(){
+        //사용자 객체와 컴퓨터 객체를 생성
+
+        //Computer.init();
+        /*
+        게임을 진행
+        결과 출력 ...
+         */
+    }
+}
