2520dfe4cc407fe45109a322ce654c9476541029
HanHyeJi <hjhan.0621@gmail.com>
2023-10-20 00:22:50 +0900

    feat: 1차 기능개발 완료
    TODO
    1. 예외처리
    2. 테스트코드 실행 시 Scanner로 입력값 받지 못하는 문제 발생
    3. 메서드 하나에서 모든 기능이 구현되어 있어, 기능 세분화 및 분리 필요

---
+++ src/main/java/baseball/BaseBallGame.java
@@ -0,0 +1,86 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class BaseBallGame {
+
+    private static BaseBallGame baseBallGame;
+    public static BaseBallGame getInstance() {
+        if (baseBallGame == null) {
+            baseBallGame = new BaseBallGame();
+        }
+        return baseBallGame;
+    }
+
+    public void run() {
+        //TODO 1. 예외처리
+        //TODO 2. 테스트코드 > Scanner로 입력값 받지 못함
+
+        while( true ) {
+
+            System.out.print("숫자 야구 게임을 시작합니다.");
+
+            List<Integer> answer = new ArrayList<>();
+            while (answer.size() < 3) {
+                int randomNumber = Randoms.pickNumberInRange(1, 9);
+                if (!answer.contains(randomNumber)) {
+                    answer.add(randomNumber);
+                }
+            }
+
+//            System.out.println(answer);
+
+            while ( true ) {
+
+                System.out.print("\n숫자를 입력해주세요 : ");
+
+                Scanner sc = new Scanner(System.in);
+                String st = sc.next();
+                String[] arr = st.split("");
+
+                List<Integer> input = new ArrayList<>();
+                for (int i = 0; i < 3; i++) {
+                    input.add(Integer.parseInt(arr[i]));
+                }
+
+                int numOfStrike = 0;
+                int numOfBall = 0;
+
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        if(answer.get(i).equals(input.get(j)))
+                            if(i==j) {
+                                numOfStrike++;
+                                break;
+                            } else {
+                                numOfBall++;
+                                break;
+                            }
+                    }
+                }
+
+                if (numOfBall + numOfStrike == 0)
+                    System.out.print("낫싱");
+                if (!(numOfBall==0))
+                    System.out.printf("%d볼 ", numOfBall);
+                if (!(numOfStrike==0))
+                    System.out.printf("%d스트라이크", numOfStrike);
+                if (numOfStrike == 3)
+                    break;
+
+            }
+            Scanner sc = new Scanner(System.in);
+            System.out.println(
+                    """
+                        \n3개의 숫자를 모두 맞히셨습니다! 게임 종료
+                        게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.""");
+
+            if (sc.nextInt() == 2)
+                break;
+        }
+    }
+}
