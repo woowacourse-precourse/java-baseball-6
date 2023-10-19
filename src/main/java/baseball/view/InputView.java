49bc3d60e346dbaf5e1d05a821ef6e4a2e93ee82
waveofmymind <sjun0913@gmail.com>
2023-10-19 17:34:36 +0900

    feat!: 입력에 대해 검증하는 기능을 구현한다.
    검증 조건은 3가지이다.
    - 입력에 숫자가 아닌 경우: getNumericValue()는 Unicode 값을 반환해서 오류를 발생시키지 않는다.
    - 중복되는 숫자가 있는 경우
    - 1~9 범위의 수 여부: 입력에 0이 포함될 수 있기 때문에 검증한다.

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -2,14 +2,37 @@ package baseball.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class InputView {
 
     public static List<Integer> readUserNumbers() {
         String input = Console.readLine();
-        return input.chars()
-                .mapToObj(Character::getNumericValue)
+
+        return parseToNumbers(input);
+    }
+
+    private static List<Integer> parseToNumbers(String input) {
+        if (!input.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
+        }
+
+        List<Integer> numbers = input.chars()
+                .map(Character::getNumericValue)
+                .boxed()
                 .toList();
+
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+
+        if (uniqueNumbers.size() != 3) {
+            throw new IllegalArgumentException("서로 다른 숫자 3개를 입력해주세요.");
+        }
+
+        if (numbers.stream().anyMatch(number -> number < 1 || number > 9)) {
+            throw new IllegalArgumentException("각 자릿수는 1~9 사이의 숫자만 가능합니다.");
+        }
+        return numbers;
     }
 }
