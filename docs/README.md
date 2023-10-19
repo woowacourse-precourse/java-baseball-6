a2a60a58921aaa4f6fe29a646ac222185195673f
unknown <winternrabbit@gmail.com>
2023-10-19 22:28:45 +0900

    ApplicationTest 실행 완료(오류X)

---
+++ docs/README.md
@@ -0,0 +1,35 @@
+<h3>구현 시나리오</h3>
+1. pinNumberInRange() 사용하여 compurter(리스트)에 임의의 서로 다른 숫자 3개 저장
+2. 사용자가 값을 입력하면 컴퓨터가 값 판별
+   -  예외처리 조건 (IllegalArgumentException 오류 반환)
+      * 입력 길이가 3이 아닐 경우
+      * 동일 숫자가 중복될 경우
+3. 받은 값에 따라 볼, 스트라이크, 낫싱 출력
+4. 3스트라이크가 될 때까지 값 입력 요청 반복
+5. 3스트라이크가 되면 계속(1)/종료(2) 입력하여 2일 경우 반복 중단, 1일 경우 1번부터 재진행
+   - 1, 2 이외의 값 입력할 경우 예외처리
+
+
+<h3>코드 작성 중 이슈</h3>
+1. 코드 실행 시 한글 깨짐 현상 발생   
+   1. [File]-[Settings..]-[File Encodings]의 Project Encoding 값을 "UTF-8"로 설정
+   2. [File]-[Settings..]-[File Encodings]의 Default encoding for properties files 값 "UTF-8"로 설정
+   3. 로컬의 '[설치경로]\bin'에 존재하는 idea64.exe.vmoptions 파일 메모장으로 열기
+   4. 맨 아래에 "-Dfile.encoding=UTF-8" 추가 후 저장
+   5. Intellij 재시작
+
+
+<h3>새로 알게 된 점</h3>
+1. 입력 받은 숫자를 하나씩 배열에 넣는 방법
+   1. 일단 String 형식으로 값을 받음
+   2. 받은 값을 새 배열 String에 split("")을 사용하여 하나씩 쪼개서 저장
+   3. Integer형 ArrayList 선언
+   4. for문 사용하여 배열 String 값들을 ArrayList에 저장
+      - 숫자 중복 방지를 위해 int로 변환한 contains(<i>str</i>)가 false인 것만 저장
+      - String > int로 형변환 불가능할 경우 자동으로 예외 발생
+2. 강제 예외 처리 방법
+    - throw를 통해 강제로 예외 발생 시킬 수 있음
+3. 기타 감상(나중에 삭제할 것)
+    - 예전에 게임 가챠충동 억제를 위해 랜덤함수 써서 몇 번 뽑아야 나오는지 돌려봤던 게 생각나서 즐거웠음🤯
+    - 결과 테스트를 게임처럼 할 수 있어서 재밌었다!
+    - 값 입력받는 부분을 많이 고민했는데 홀로 해결해내서 뿌듯함!👍
--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,70 @@
 package baseball;
 
+import net.bytebuddy.implementation.bind.MethodDelegationBinder;
+
+import java.util.List;
+import java.util.ArrayList;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        int retry = 1;  // 게임 종료 후 재시작 확인용
+         while (retry == 1) {
+            // 컴퓨터 숫자 값 설정
+            List<Integer> computer = new ArrayList<>();
+            while (computer.size() < 3) {
+                int randomNumber = Randoms.pickNumberInRange(1, 9);
+                if (!computer.contains(randomNumber)) {
+                    computer.add(randomNumber);
+                }
+            }
+            // 사용자 입력 부분: break 되기 전까지 무조건 반복이므로 true 사용
+            while (true) {
+                // 결과 출력 후 값 초기화 및 선언 일괄 처리 위해 반복문 하위에 작성
+                int strike = 0;
+                int ball = 0;
+                System.out.print("숫자를 입력해주세요 : ");
+                String user = Console.readLine();
+                String[] userArr = user.split("");
+                List<Integer> userNum = new ArrayList<>();
+                for(String str : userArr)
+                    if (!userNum.contains(Integer.parseInt(str)))
+                        userNum.add(Integer.parseInt(str));
+                // 예외 처리
+                if (userNum.size() != 3) {
+                    throw new IllegalArgumentException("올바른 값을 입력해 주세요");
+                }
+                // 스트라이크, 볼 계산
+                for (int i : userNum) {
+                    if (computer.contains(i)) {
+                        if (computer.indexOf(i) == userNum.indexOf(i))
+                            strike ++;
+                        if (computer.indexOf(i) != userNum.indexOf(i))
+                            ball++;
+                    }
+                }
+                // 결과 출력
+                if (ball > 0)
+                    System.out.print(ball + "볼 ");
+                if (strike > 0)
+                    System.out.print(strike + "스트라이크");
+                if (ball == 0 && strike == 0)
+                    System.out.print("낫싱");
+                System.out.println();
+                if (strike == 3) {
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                    retry = Integer.parseInt(Console.readLine());
+                    // 예외 처리
+                    if (retry < 1 || retry > 2) {
+                        throw new IllegalArgumentException("올바른 값을 입력해 주세요");
+                    }
+                    break;
+                }
+            }
+         }
     }
 }
