89203f8b3b23a574af0851eef0d7334a8906c658
coenffl <>
2023-10-20 01:07:15 +0900

    Refacor: modify class and package

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,10 +5,7 @@ import java.io.IOException;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-//        try {
-//            playBaseBallGame();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        BaseballGame baseballGame = new BaseballGame();
+        baseballGame.playBaseballGame();
     }
 }
--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,18 +1,8 @@
 package baseball;
 
-import java.util.*;
-import camp.nextstep.edu.missionutils.Randoms;
 public class BaseballGame {
-    public List<Integer> makeBaseballNumber() {
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
-        }
-        return computer;
+
+    public void playBaseballGame() {
+
     }
-
-
 }
