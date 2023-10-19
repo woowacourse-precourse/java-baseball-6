f1d16191750399d7c9cbafafccff95a3e510c763
JaeMin-1 <39516729+JaeMin-1@users.noreply.github.com>
2023-10-20 00:01:12 +0900

    조건에 맞게 정답 랜덤 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,30 @@
 package baseball;
 
+import java.io.*;
+import java.util.*;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        while(true) {
+            /*
+            * 정답 생성
+            * 중복되지 않은 세개의 숫자
+            * 1 ~ 9 범위에 해당되는 숫자
+            */
+            List<Integer> answer = new ArrayList<>();
+            while (answer.size() < 3) {
+                int randomNumber = Randoms.pickNumberInRange(1, 9);
+                if (!answer.contains(randomNumber)) {
+                    answer.add(randomNumber);
+                }
+            }
+
+
+
+        }
+
     }
 }
