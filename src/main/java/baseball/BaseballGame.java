fc5709af569d9e963325937d436cb5a3f3996f56
PgmJun <chltmdwns96@gmail.com>
2023-10-19 19:34:17 +0900

    fix : 컴퓨터 숫자값 입력 로직 버그 수정

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -70,8 +70,11 @@ public class BaseballGame {
 
 	private List<Integer> generateComputerNumber() {
 		List<Integer> computer = new ArrayList<>();
-		while(computer.size() < 3) {
-			computer.add(Randoms.pickNumberInRange(1, 9));
+		while (computer.size() < 3) {
+			int randomNumber = Randoms.pickNumberInRange(1, 9);
+			if (!computer.contains(randomNumber)) {
+				computer.add(randomNumber);
+			}
 		}
 
 		return computer;
