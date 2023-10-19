3c783b0e1d5231383682410d092245c1cc634621
SeoJiWon <sjiwon4491@gmail.com>
2023-10-19 20:01:27 +0900

    test: User - Computer RoundResult 판단 테스트 코드 작성

---
+++ src/test/java/baseball/domain/RefereeTest.java
@@ -0,0 +1,47 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class RefereeTest {
+    @ParameterizedTest
+    @MethodSource("eachPossibleCase")
+    @DisplayName("Referee가 Computer & User 간 숫자 야구 게임 결과(RoundResult)를 도출한다")
+    void judge(
+            List<Integer> computerBaseballs,
+            List<Integer> userBaseballs,
+            int strikeCount,
+            int ballCount
+    ) {
+        // when
+        final RoundResult result = Referee.judge(computerBaseballs, userBaseballs);
+
+        // then
+        assertAll(
+                () -> assertThat(result.strikeCount()).isEqualTo(strikeCount),
+                () -> assertThat(result.ballCount()).isEqualTo(ballCount)
+        );
+    }
+
+    private static Stream<Arguments> eachPossibleCase() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3), List.of(4, 5, 6), 0, 0), // nothing
+                Arguments.of(List.of(1, 2, 3), List.of(4, 5, 1), 0, 1), // 1 ball
+                Arguments.of(List.of(1, 2, 3), List.of(2, 1, 5), 0, 2), // 2 ball
+                Arguments.of(List.of(1, 2, 3), List.of(3, 1, 2), 0, 3), // 3 ball
+                Arguments.of(List.of(1, 2, 3), List.of(1, 4, 5), 1, 0), // 1 strike
+                Arguments.of(List.of(1, 2, 3), List.of(1, 2, 5), 2, 0), // 2 strike
+                Arguments.of(List.of(1, 2, 3), List.of(1, 2, 3), 3, 0), // 3 strike (clear)
+                Arguments.of(List.of(1, 2, 3), List.of(1, 3, 5), 1, 1), // 1 strike + 1 ball
+                Arguments.of(List.of(1, 2, 3), List.of(1, 3, 2), 1, 2) // 1 strike + 2 ball
+        );
+    }
+}
