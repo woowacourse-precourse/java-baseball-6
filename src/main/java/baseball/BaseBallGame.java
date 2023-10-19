31053db36713fd6de869ba85ff31b1d76823ad0b
HanHyeJi <hjhan.0621@gmail.com>
2023-10-20 01:52:47 +0900

    feat: 출력 메세지 클래스 별도 생성 (OutputMessage)

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -29,7 +29,7 @@ public class BaseBallGame {
     public void run() {
         while( true ) {
             initializeNum();
-            System.out.print("숫자 야구 게임을 시작합니다.");
+            System.out.print(OutputMessage.START_GAME);
             List<Integer> answer = setAnswer();
 
             // 정답 확인용 임시 코드
@@ -37,25 +37,26 @@ public class BaseBallGame {
 
             checkAnswer(answer);
 
-            System.out.println(
-                    """
-                        \n3개의 숫자를 모두 맞히셨습니다! 게임 종료
-                        게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.""");
+            System.out.println(OutputMessage.END_OR_NOT);
 
             if (askWhetherToEnd()) break;
         }
     }
 
-    private static boolean askWhetherToEnd() {
-        Scanner sc = new Scanner(System.in);
-        if (sc.nextInt() == 2)
-            return true;
-        return false;
+    private static List<Integer> setAnswer() {
+        List<Integer> answer = new ArrayList<>();
+        while (answer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!answer.contains(randomNumber)) {
+                answer.add(randomNumber);
+            }
+        }
+        return answer;
     }
 
     private void checkAnswer(List<Integer> answer) {
         while ( true ) {
-            System.out.print("\n숫자를 입력해주세요 : ");
+            System.out.print(OutputMessage.ENTER_NUMBER);
 
             List<Integer> input = getInput();
 
@@ -66,16 +67,16 @@ public class BaseBallGame {
         }
     }
 
-    private boolean getResult() {
-        if (numOfBall + numOfStrike == 0)
-            System.out.print("낫싱");
-        if (!(numOfBall==0))
-            System.out.printf("%d볼 ", numOfBall);
-        if (!(numOfStrike==0))
-            System.out.printf("%d스트라이크", numOfStrike);
-        if (numOfStrike == 3)
-            return true;
-        return false;
+    private static List<Integer> getInput() {
+        Scanner sc = new Scanner(System.in);
+        String st = sc.next();
+        String[] arr = st.split("");
+
+        List<Integer> input = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            input.add(Integer.parseInt(arr[i]));
+        }
+        return input;
     }
 
     private void compareAnswerToInput(List<Integer> answer, List<Integer> input) {
@@ -93,26 +94,22 @@ public class BaseBallGame {
         }
     }
 
-    private static List<Integer> getInput() {
-        Scanner sc = new Scanner(System.in);
-        String st = sc.next();
-        String[] arr = st.split("");
-
-        List<Integer> input = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            input.add(Integer.parseInt(arr[i]));
-        }
-        return input;
+    private boolean getResult() {
+        if (numOfBall + numOfStrike == 0)
+            System.out.print("낫싱");
+        if (!(numOfBall==0))
+            System.out.printf("%d볼 ", numOfBall);
+        if (!(numOfStrike==0))
+            System.out.printf("%d스트라이크", numOfStrike);
+        if (numOfStrike == 3)
+            return true;
+        return false;
     }
 
-    private static List<Integer> setAnswer() {
-        List<Integer> answer = new ArrayList<>();
-        while (answer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!answer.contains(randomNumber)) {
-                answer.add(randomNumber);
-            }
-        }
-        return answer;
+    private static boolean askWhetherToEnd() {
+        Scanner sc = new Scanner(System.in);
+        if (sc.nextInt() == 2)
+            return true;
+        return false;
     }
 }
