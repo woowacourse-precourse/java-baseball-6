6488e0e007fa6a165565cbff60123324438b7fee
yeseong31 <yeseong31@naver.com>
2023-10-19 19:03:12 +0900

    feat: 상대방(컴퓨터) 숫자 저장 기능 구현

--- src/main/java/baseball/service/BaseballService.java
+++ src/main/java/baseball/service/BaseballService.java
@@ -2,6 +2,7 @@ package baseball.service;
 
 import static baseball.domain.Computer.BASEBALL_NUMBERS_SIZE;
 
+import baseball.repository.ComputerRepository;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.LinkedHashSet;
 
@@ -10,8 +11,11 @@ public class BaseballService {
     private static final int BASEBALL_MIN_NUMBER = 1;
     private static final int BASEBALL_MAX_NUMBER = 9;
     
+    private final ComputerRepository computerRepository = new ComputerRepository();
+    
     public void initComputer() {
         String number = makeComputerNumber();
+        saveComputerNumber(number);
     }
     
     private String makeComputerNumber() {
@@ -27,5 +31,9 @@ public class BaseballService {
     private String makeRandomNumber() {
         int randomNumber = Randoms.pickNumberInRange(BASEBALL_MIN_NUMBER, BASEBALL_MAX_NUMBER);
         return Integer.toString(randomNumber);
+    }
+    
+    private void saveComputerNumber(String number) {
+        computerRepository.save(number);
     }
 }
