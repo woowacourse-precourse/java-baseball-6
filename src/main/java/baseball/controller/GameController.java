9cac0aa93c4ed8a1a6793713ec9918d6442f3c15
suihub <qkdcrt@gmail.com>
2023-10-19 23:49:14 +0900

    <feat>: "유저측 볼 생성 기능 구현"
    검증을 마친 숫자를 토대로 유저측 볼을 생성하는 기능을 구현.
    컨트롤러에 유저측 볼을 가리키기 위한 변수를 새로이 추가.

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -10,12 +10,14 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class GameController {
-    private final Set<BallDto> computer = new LinkedHashSet<>();
+    private Set<BallDto> computer;
+    private Set<BallDto> user;
     private final GameOutput output = new GameOutput();
     private final UserInput input = new UserInput();
     private final BallException ballException = new BallException();
 
     public void start(){
+        computer = new LinkedHashSet<>();
         while (computer.size() < 3){
             Ball ball = new Ball(Randoms.pickNumberInRange(1, 9));
             computer.add(new BallDto(ball.getNumber()));
@@ -24,11 +26,26 @@ public class GameController {
         input.inputUserNumber();
     }
 
-    public void gameProcess(String number){
+    public void gameProcess(String number) {
         ballException.validation(number);
+        createUserBall(number);
+    }
+
+    private void createUserBall(String number){
+        Set<BallDto> userBall = new LinkedHashSet<>();
+        char[] charArray = number.toCharArray();
+        for (char num : charArray) {
+            Ball ball = new Ball(num - '0');
+            userBall.add(new BallDto(ball.getNumber()));
+        }
+        this.user = userBall;
     }
 
     public Set<BallDto> getComputer() {
         return computer;
+    }
+
+    public Set<BallDto> getUser() {
+        return user;
     }
 }
