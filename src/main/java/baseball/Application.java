eee05c6d4395fe435d6b02b9b2576ae057a31eb1
june-777 <wlwhswnsrl96@gmail.com>
2023-10-19 23:50:48 +0900

    feat: 컴퓨터는 1에서 9까지 서로 다른 임의의 수 3개를 선택하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,9 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -32,6 +34,18 @@ public class Application {
                 .chars()
                 .mapToObj(Character::getNumericValue)
                 .collect(Collectors.toList());
+    }
+
+    // 기능: 컴퓨터는 1에서 9까지 서로 다른 임의의 수 3개를 선택
+    private static List<Integer> createComputerBalls() {
+        List<Integer> computerBalls = new ArrayList<>();
+        while (computerBalls.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerBalls.contains(randomNumber)) {
+                computerBalls.add(randomNumber);
+            }
+        }
+        return computerBalls;
     }
 
     // 기능: 게임 재시작 입력 검증
