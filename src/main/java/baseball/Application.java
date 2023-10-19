08a4f8577c0ef07c38cc179601389582b39e661c
1Bronze <khdkkhdd@gmail.com>
2023-10-19 16:59:51 +0900

    [refact] BaseballController 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,26 +1,11 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
-import java.util.List;
+import baseball.controller.BaseballController;
 
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("숫자 야구 게임을 시작합니다.");
-//        System.out.println("createAnswer() = " + createAnswer());
-    }
+    private static BaseballController baseballController;
 
-    private static int createAnswer() {
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
-        }
-        StringBuilder answerStream = new StringBuilder();
-        computer.stream().forEach(answerStream::append);
-        return Integer.parseInt(answerStream.toString());
+    public static void main(String[] args) {
+        baseballController.start();
     }
 }
---
+++ src/main/java/baseball/controller/BaseballController.java
@@ -0,0 +1,26 @@
+package baseball.controller;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseballController {
+    public void start() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        createAnswer();
+    }
+
+    private int createAnswer() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        StringBuilder answerStream = new StringBuilder();
+        computer.stream().forEach(answerStream::append);
+        return Integer.parseInt(answerStream.toString());
+    }
+}
