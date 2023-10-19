f111c36359dc6999e5c0226f5a23a70260098ecb
waveofmymind <sjun0913@gmail.com>
2023-10-19 20:50:52 +0900

    feat!: 파라미터의 길이에 따른 유효성 검증을 추가한다.
    3개 미만인 경우, 3개 초과인 경우 모두 예외를 발생시킨다.

--- src/main/java/baseball/domain/Numbers.java
+++ src/main/java/baseball/domain/Numbers.java
@@ -1,6 +1,6 @@
 package baseball.domain;
 
-import baseball.GameConstants;
+import baseball.controller.GameConstants;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,6 +16,10 @@ public class Numbers {
         List<Number> numbers = Arrays.stream(input.split(""))
                 .map(n -> new Number(Integer.parseInt(n)))
                 .toList();
+
+        if (numbers.size() != GameConstants.NUMBER_SIZE.getNumber()) {
+            throw new IllegalArgumentException("숫자는 3자리를 입력해야 합니다.");
+        }
 
         if (numbers.stream().distinct().count() < GameConstants.NUMBER_SIZE.getNumber()) {
             throw new IllegalArgumentException("서로 다른 숫자를 입력해야합니다.");
