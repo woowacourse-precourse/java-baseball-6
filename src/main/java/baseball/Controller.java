5bdccb6beafacde0ec2af5609e02aab7a3b90b14
jhon3242 <rldotnr2@gmail.com>
2023-10-19 20:11:02 +0900

    feat : 게임 재시작 기능 구현

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -8,6 +8,12 @@ import java.util.List;
 public class Controller {
 	public void run() {
 		System.out.println("숫자 야구 게임을 시작합니다.");
+		do {
+			proceed();
+		} while (InputView.readRestart() == 1);
+	}
+
+	private void proceed() {
 		Computer computer = new Computer();
 		List<Integer> trial;
 		while (true) {
@@ -18,5 +24,6 @@ public class Controller {
 				break;
 			}
 		}
+		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 	}
 }
--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -9,7 +9,7 @@ public class InputView {
 	public static List<Integer> readBaseballNumber() {
 		System.out.printf("숫자를 입력해주세요 : ");
 		String inputValue = Console.readLine();
-		validateLength(inputValue);
+		validateLength(inputValue, 3);
 		List<Integer> result = new ArrayList<>();
 		for (int i = 0; i < inputValue.length(); i++) {
 			String substring = inputValue.substring(i, i + 1);
@@ -20,8 +20,19 @@ public class InputView {
 		return result;
 	}
 
-	private static void validateLength(String value) {
-		if (value.length() != 3) {
+	public static int readRestart() {
+		System.out.printf("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+		String inputValue = Console.readLine();
+		validateLength(inputValue, 1);
+		validateNumber(inputValue);
+		if (!"12".contains(inputValue)) {
+			throw new IllegalArgumentException("1 과 2만 입력할 수 있습니다.");
+		}
+		return Integer.parseInt(inputValue);
+	}
+
+	private static void validateLength(String value, int size) {
+		if (value.length() != size) {
 			throw new IllegalArgumentException("3 자리 숫자를 입력해야 합니다.");
 		}
 	}
@@ -42,4 +53,6 @@ public class InputView {
 			throw new IllegalArgumentException("중복된 숫자가 포함되어 있습니다.");
 		}
 	}
+
+
 }
