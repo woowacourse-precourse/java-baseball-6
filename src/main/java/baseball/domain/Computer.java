67b9ccdec8261abb93dde9f1cf601790a0c78703
prao <pch5521@gmail.com>
2023-10-19 16:45:13 +0900

    feat: 컴퓨터 숫자 생성 기능 구현
    - LinkedHashSet을 활용하여 순서를 보장 및 중복 방지 기능 구현
    - Computer 생성자에 start, end, size를 입력하면 start 이상 end 이하의 수를 size 크기의 List에 저장하도록 구현

---
+++ src/main/java/baseball/domain/Computer.java
@@ -0,0 +1,29 @@
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Computer {
+
+    private final List<Integer> computerNumbers;
+
+    public Computer(int start, int end, int size) {
+        computerNumbers = generateUniqueNumbers(start, end, size);
+    }
+
+    private List<Integer> generateUniqueNumbers(int start, int end, int size) {
+        Set<Integer> uniqueNumbers = new LinkedHashSet<>();
+        while (uniqueNumbers.size() < size) {
+            int randomNumber = Randoms.pickNumberInRange(start, end);
+            uniqueNumbers.add(randomNumber);
+        }
+        return new ArrayList<>(uniqueNumbers);
+    }
+
+    public List<Integer> getComputerNumbers() {
+        return computerNumbers;
+    }
+}
