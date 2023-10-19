6ad84db8f98ddfaccab0597f6ebc8c233c7c6743
gosu <q63530@naver.com>
2023-10-19 18:57:58 +0900

    feat : StringToList<Integer>기능 추가

--- src/main/java/baseball/game/Game.java
+++ src/main/java/baseball/game/Game.java
@@ -9,32 +9,36 @@ import java.util.List;
 import static baseball.constant.GameConstant.*;
 
 public class Game {
-    List<Integer> randomNumberList = new ArrayList<>();
+    List<Integer> randomNumber = new ArrayList<>();
 
     private void createRandomNumber(){
-        randomNumberList.clear();
-        while (randomNumberList.size() < RANDOM_NUMBER_SIZE) {
-            int randomNumber = Randoms.pickNumberInRange(START_RANGE, LAST_RANGE);
-            if (!randomNumberList.contains(randomNumber)) {
-                randomNumberList.add(randomNumber);
+        randomNumber.clear();
+        while (randomNumber.size() < RANDOM_NUMBER_SIZE) {
+            int intRandomNumber = Randoms.pickNumberInRange(START_RANGE, LAST_RANGE);
+            if (!randomNumber.contains(intRandomNumber)) {
+                randomNumber.add(intRandomNumber);
             }
         }
     }
 
     private void userInput(){
         System.out.printf(INPUT_NUMBER);
-        String inputNumber = Console.readLine();
+        String inputStringNumber = Console.readLine();
     }
 
-    public void errorValidate(String string) throws IllegalAccessException {
-        if (!string.matches(REGULAR_EXPRESSION_NUMBER)){
+    private void errorValidate(String stringNumber) throws IllegalAccessException {
+        if (!stringNumber.matches(REGULAR_EXPRESSION_NUMBER)){
             throw new IllegalAccessException();
         }
     }
 
-//    private List<Integer> stringToIntegerList(String string){
-//
-//    }
+    private List<Integer> stringToIntegerList(String stringNumber)  {
+        List<Integer> inputNumber = new ArrayList<>();
+        for(char charNumber : stringNumber.toCharArray()){
+            inputNumber.add(Character.getNumericValue(charNumber));
+        }
+        return inputNumber;
+    }
 
     private void inputValidate(List<Integer> number){
 
