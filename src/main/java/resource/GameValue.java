51e58394e7061c8582af16c5c8ca1b9083313230
WeeYoungSeok <dndrldzk@gmail.com>
2023-10-19 22:43:10 +0900

    fix: GameValue 클래스 수정 게임 종료시 재시작, 종료에 대한 변수를 추가하였습니다.

--- src/main/java/resource/GameValue.java
+++ src/main/java/resource/GameValue.java
@@ -5,4 +5,7 @@ public final class GameValue {
     public static final int MIN_NUMBER = 1;
     public static final int MAX_NUMBER = 9;
     public static final int MAX_LENGTH = 3;
+
+    public static final int RESTART_GAME_VALUE = 1;
+    public static final int END_GAME_VALUE = 2;
 }
