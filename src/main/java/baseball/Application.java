8749e513843ce1bd46a325087ab805bd37586346
minsang-alt <mazinger97@naver.com>
2023-10-20 01:37:54 +0900

    feat: 서로 다른 임의의 세자리 숫자 정하는 기능 추가
    - 각 자리의 랜덤 숫자를 배열에 넣기 전 먼저 이 배열을 게임 재시작 할 때 다시 재사용할 수 있도록 초기화한다(힙(메모리) 낭비 방지)
    
    - startInclusive ~ endInclusive 사이의 랜덤 숫자를 중복되지 않게 배열에 포함시킨다

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,31 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    public static final int startInclusive = 1;
+    public static final int endInclusive = 9;
+
+    private static final int numLength = 3;
+
+    public static List<Integer> splitDigitsFromRandomNumber = new ArrayList<>();
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        initComputerNumber();
+    public static void initComputerNumber() {
+        splitDigitsFromRandomNumber.clear();
+        while (splitDigitsFromRandomNumber.size() < numLength) {
+            //여기서 IllegalArgumentException 고려해봐야 한다.
+            int randomNumber = Randoms.pickNumberInRange(startInclusive, endInclusive);
+            if (!splitDigitsFromRandomNumber.contains(randomNumber)) {
+                splitDigitsFromRandomNumber.add(randomNumber);
+            }
+        }
     }
 }
