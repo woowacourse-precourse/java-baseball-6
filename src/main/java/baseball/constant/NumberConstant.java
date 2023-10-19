4a73a219081ec657c85997a1a4be943765893c56
donghyunkim <ghffu405@gmail.com>
2023-10-19 16:43:32 +0900

    feat: 시작,끝 상수 추가

--- src/main/java/baseball/constant/NumberConstant.java
+++ src/main/java/baseball/constant/NumberConstant.java
@@ -3,5 +3,7 @@ package baseball.constant;
 public class NumberConstant {
 
     public static final int VALID_NUMBER_SIZE = 3;
-    
+    public static final int START_NUMBER = 1;
+    public static final int END_NUMBER = 9;
+
 }
