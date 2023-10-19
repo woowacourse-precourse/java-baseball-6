fe9e83e0c366a631b538c9c8e419e4b9437e0d90
wonchanjoo <wcj0911@naver.com>
2023-10-20 01:04:26 +0900

    fix: 게임 시작 및 종료 방식 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,27 +1,57 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class Application {
+
+    static boolean gameOver = false;
+    static int[] computer = new int[3];
+    static int[] user = new int[3];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (!gameOver) {
+            gameStart();
 
-        while (true) {
-            System.out.print("숫자를 입력해주세요 : ");
+            while (true) {
+                System.out.print("숫자를 입력해주세요 : ");
 
-            String input = br.readLine();
+                String input = br.readLine();
 
-            if (!isValidNumber(input)) {
-                try {
-                    inputError();
-                } catch (IllegalArgumentException e) {
-                    return;
+                if (!isValidNumber(input)) {
+                    try {
+                        inputError();
+                    } catch (IllegalArgumentException e) {
+                        gameOver = true;
+                        break;
+                    }
                 }
             }
+        }
+    }
+
+    private static void gameStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        // 상대방의 수 생성
+        Set<Integer> hashSet = new HashSet<>();
+
+        while (hashSet.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            hashSet.add(randomNumber);
+        }
+
+        Iterator<Integer> iterator = hashSet.iterator();
+        for (int i = 0; i < 3; i++) {
+            computer[i] = iterator.next();
         }
     }
 
