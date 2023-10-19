4d35eb1b2a6cba134b6bcde88f72433c1e2c9e26
shinjaewon99 <tlswodnjs99@naver.com>
2023-10-19 20:53:19 +0900

    feat : 유저가 입력한 숫자의 중복에 대한 예외메소드 추가

--- src/main/java/baseball/GameException.java
+++ src/main/java/baseball/GameException.java
@@ -2,8 +2,8 @@ package baseball;
 
 import java.util.regex.Pattern;
 
-import static baseball.messageConstant.ErrorMessageConstant.PLAYER_INPUT_NUMBER_LENGTH_ERROR_MESSAGE;
-import static baseball.messageConstant.ErrorMessageConstant.PLAYER_INPUT_NUMBER_RANGE_ERROR_MESSAGE;
+import static baseball.GameService.toInt;
+import static baseball.messageConstant.ErrorMessageConstant.*;
 
 public class GameException {
     private static final Pattern NUMBER_DIGIT_MATCH = Pattern.compile("^[1-9]*$");
@@ -16,5 +16,12 @@ public class GameException {
     public void playerNumberRangeException(String inputPlayerNumber) throws IllegalArgumentException {
         if (!NUMBER_DIGIT_MATCH.matcher(inputPlayerNumber).matches())
             throw new IllegalArgumentException(PLAYER_INPUT_NUMBER_RANGE_ERROR_MESSAGE);
+    }
+
+    public void playerNumberDuplicateException(int[] playerNumbers, char storeNumber) throws IllegalArgumentException {
+        for (int playerNumber : playerNumbers) {
+            if (playerNumber == toInt(storeNumber))
+                throw new IllegalArgumentException(PLAYER_INPUT_NUMBER_DUPLICATE_ERROR_MESSAGE);
+        }
     }
 }
