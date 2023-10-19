83d74b337988e6e89562e5ca41dff8157f2ee989
freekyr7529 <freekyr7529@gmail.com>
2023-10-20 01:29:59 +0900

    test: 잘못된 입력 예외 처리 로직 테스트
    userInputValidation 메서드의 유효성 테스트 케이스 작성 및 성공

--- src/test/java/baseball/InputTest.java
+++ src/test/java/baseball/InputTest.java
@@ -1,8 +1,10 @@
 package baseball;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import org.junit.jupiter.api.Test;
 
 public class InputTest {
@@ -10,9 +12,11 @@ public class InputTest {
 
     @Test
     void 입력_테스트() {
-        String input = "123";
-        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        String input = "123\n1234";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
 
         assertThat(inputView.inputGuideMsg()).isEqualTo(123);
+        assertThrows(IllegalArgumentException.class, () -> inputView.inputGuideMsg());
     }
 }
