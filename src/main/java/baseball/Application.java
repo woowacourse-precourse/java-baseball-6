7afead39e2c17da3c8d7df0e4d0a17c7f5f0eae2
june-777 <wlwhswnsrl96@gmail.com>
2023-10-19 23:13:18 +0900

    feat: 사용자 공 입력 검증 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,45 @@
 package baseball;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    // 기능: 사용자 공 입력 검증
+    private static void validatePlayerBallInput(String number) {
+        validateLength(number, 3);
+        validateNumeric(number);
+        validateUnique(number);
+    }
+
+    // 기능: 서로 다른 숫자로만 입력해야 한다
+    private static void validateUnique(String input) {
+        Set<Character> uniqueNums = new HashSet<>();
+        for (int numberIdx = 0; numberIdx < input.length(); numberIdx++) {
+            uniqueNums.add(input.charAt(numberIdx));
+        }
+        if (uniqueNums.size() != 3) {
+            throw new IllegalArgumentException("서로 다른 숫자만 입력해야 합니다.");
+        }
+    }
+
+    // 기능: 숫자만 입력해야 한다
+    private static void validateNumeric(String input) {
+        boolean onlyNumeric = Pattern.matches("^[\\d]*$", input);
+        if (!onlyNumeric) {
+            throw new IllegalArgumentException("숫자만 입력해야 합니다.");
+        }
+    }
+
+    // 기능: length 자릿수로 입력해야 한다
+    private static void validateLength(String input, int length) {
+        if (input.length() != length) {
+            throw new IllegalArgumentException(length + " 자릿수로 입력해야 합니다.");
+        }
     }
 
     private static void printInitialGameStartMessage() {
