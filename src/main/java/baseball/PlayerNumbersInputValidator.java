4ddc47c4ee6a35e097a7b9fbe8f34b84c5117a42
donghoony <aru0504@naver.com>
2023-10-20 00:07:30 +0900

    refactor: 사용자의 메뉴 입력과 수 입력의 검증 클래스 분리

--- src/main/java/baseball/PlayerNumbersInputValidator.java
+++ src/main/java/baseball/PlayerNumbersInputValidator.java
@@ -10,7 +10,7 @@ import java.util.List;
  * 사용자의 입력을 검증하는 클래스.
  * <p>단순한 입력 검증이 목표이며, 검증 성공 여부만을 알려준다. 추가적인 예외 발생 등은 PlayerInput에서 처리한다.
  */
-public class PlayerInputValidator {
+public class PlayerNumbersInputValidator {
 
     public boolean validatePlayerNumbers(List<Integer> playerNumbers) {
         return validateNumberCount(playerNumbers) &&
