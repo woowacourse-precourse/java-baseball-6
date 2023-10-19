221be6e017b868c3a6921a51056817f915b66d0b
prao <pch5521@gmail.com>
2023-10-19 16:53:00 +0900

    test: 컴퓨터 숫자 생성 테스트
    - 중복된 숫자가 없는지 확인
    - 입력받은 size의 개수만큼 리스트에 들어가 있는지 확인
    - 모든 숫자가 1 부터 9 사이에 있는지 확인

---
+++ src/test/java/baseball/domain/ComputerTest.java
@@ -0,0 +1,27 @@
+package baseball.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.LinkedHashSet;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class ComputerTest {
+
+    @Test
+    void testGenerateComputerNumbers() {
+        Computer computer = new Computer(1, 9, 3);
+
+        List<Integer> computerNumbers = computer.getComputerNumbers();
+
+        assertEquals(computerNumbers.size(), new LinkedHashSet<>(computerNumbers).size());
+
+        assertEquals(3, computerNumbers.size());
+
+        for (int number : computerNumbers) {
+            assertTrue(number >= 1 && number <= 9);
+        }
+    }
+
+}
