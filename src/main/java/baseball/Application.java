2c73daa2953ac24d99026f5d8378fdc425dcd791
1Bronze <khdkkhdd@gmail.com>
2023-10-19 16:37:18 +0900

    [feat] 정답 생성 로직 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
+//        System.out.println("createAnswer() = " + createAnswer());
+    }
+
+    private static int createAnswer() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        StringBuilder answerStream = new StringBuilder();
+        computer.stream().forEach(answerStream::append);
+        return Integer.parseInt(answerStream.toString());
     }
 }
