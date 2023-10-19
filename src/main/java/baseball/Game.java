911fc2497543c9253d6c36949e915f15c89048a9
zangsu <zangsu_@naver.com>
2023-10-19 20:44:02 +0900

    feat : implements play() to play game once

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,6 +1,11 @@
 package baseball;
 
+import java.util.List;
+
 public class Game {
+
+    Player player = new Player();
+    Computer computer = new Computer();
 
     public void startGame(){
 
@@ -15,8 +20,15 @@ public class Game {
 
     }
     public void play(){
-        //사용자 객체와 컴퓨터 객체를 생성
-
+        computer.init();
+        Result result;
+        do{
+            System.out.println("숫자를 입력해주세요 : ");
+            List<Integer> input = player.getInput();
+            result = computer.getResult(input);
+            System.out.println(result);
+        }while(result.getStrike() != 3);
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         //Computer.init();
         /*
         게임을 진행
