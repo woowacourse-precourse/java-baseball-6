4b59151e05a306691c6c16903e6fa8866f9aa2b4
KongSeungHwan <102781120+KongSeungHwan@users.noreply.github.com>
2023-10-19 15:37:45 +0900

    2023.10.19 우아한 테크 코스 첫 커밋

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        //1.컴퓨터 숫자 생성 메소드
+        //2.사용자 숫자 입력 받기
+        //3.비교 연산 메소드
+        //숫자, 자리가 모두 만족 시 스트라이크
+        //숫자가 맞지만 자리가 다른 경우 ball
+
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int input = Integer.parseInt(br.readLine());
+        }catch()
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
