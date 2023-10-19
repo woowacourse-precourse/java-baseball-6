9008a68977e6de2898f6ee89abe9f83b2d3ac921
dsa04156 <dsa04156@naver.com>
2023-10-19 19:50:10 +0900

    update README.md

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while(true){
+            int a = Randoms.pickNumberInRange(1,9);
+            System.out.println(a);
+
+        }
+
+//        while (true) {
+//            System.out.println("숫자를 입력해주세요 : ");
+//
+//
+//        }
+
+
     }
 }
