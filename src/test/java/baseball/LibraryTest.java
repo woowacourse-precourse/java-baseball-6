99ad7e8536fe32fd46773fb6ac47d16695347517
Donggeon Moon <nullsafer@naver.com>
2023-10-19 20:06:25 +0900

    test: missionutils 라이브러리 사용 연습

---
+++ src/test/java/baseball/LibraryTest.java
@@ -0,0 +1,39 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LibraryTest {
+
+    @Nested
+    class RandomLibraryTest {
+        private final List<Integer> integers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        @Test
+        void testRandomsLibrary() {
+            assertThat(Randoms.pickNumberInList(integers)).isIn(integers);
+            assertThat(Randoms.pickNumberInRange(1, 9)).isIn(integers);
+            assertThat(Randoms.shuffle(integers)).containsAll(integers);
+        }
+    }
+
+    @Nested
+    class ConsoleLibraryTest {
+        @Test
+        void testConsoleLibrary() {
+            String inputValue = "12345";
+            System.setIn(new ByteArrayInputStream(inputValue.getBytes()));
+            String readValue = Console.readLine();
+            Console.close();
+
+            assertThat(readValue).isEqualTo(inputValue);
+        }
+    }
+}
