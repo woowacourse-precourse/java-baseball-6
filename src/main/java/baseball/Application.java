5c8c5285dfbc7402e0c2f44d874bb9f8772e47c7
MyunghyunNero <mh1kim0910@gmail.com>
2023-10-19 20:55:55 +0900

    feat : Game 시작 로직 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,10 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        // 게임 시작!
+        Game game = new Game();
+        game.gameStart();
+
     }
 }
---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,30 @@
+package baseball;
+
+import java.io.Console;
+
+public class Game {
+
+    public void gameStart(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        while(playGame()) {
+
+        }
+
+
+    }
+    private boolean playGame(){
+        boolean retryCheck;
+        //컴퓨터의 랜덤 값 생성
+        while(true){
+            System.out.println("숫자를 입력해주세요 : ");
+            // 유저 입력 받기
+
+            // 딜러의 입력값 컴퓨터 입력값 비교
+
+            // 딜러의 결과값 출력
+
+            // 성공 여부 체크 후 return
+        }
+    }
+}
