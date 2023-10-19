7e5f2edc361e003e7c126e6415c0cab61f90adde
sinyebin <dpqls0625@naver.com>
2023-10-19 22:06:24 +0900

    랜덤 숫자 생성

--- src/main/java/baseball/controller/BaseBall.java
+++ src/main/java/baseball/controller/BaseBall.java
@@ -1,13 +1,23 @@
 package baseball.controller;
 
+import baseball.service.RandomNumbers;
 import baseball.view.PrintMessage;
 
 public class BaseBall {
 
+    private RandomNumbers randomNumbers;
     //게임 시작
     public void startGame(){
         boolean playGame=true;
         PrintMessage.printGameStart();
+        getRandomNumbers();
 
     }
+
+    public void getRandomNumbers(){
+        randomNumbers=new RandomNumbers();
+        System.out.println(randomNumbers);
+    }
+
+    
 }
