9b302926b2a9a5dc3814911079d306c58a7e77a7
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-10-20 02:26:39 +0900

    feat: 유저의 숫자와 컴퓨터의 숫자를 비교하여 결과값 계산

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -15,4 +15,42 @@ public class BaseballController {
     private static final int NO_HIT = 0;
     private static final int MAX_HIT = 3;
 
+
+    private final OutputView outputView = new OutputView();
+    private final InputView inputView = new InputView();
+    private User user;
+    private int[] result;
+    private Computer computer;
+
+    private User userNumber() {
+        outputView.inputNumber();
+        return new User(inputView.inputNumber());
+    }
+
+    private void resultCalculator() {
+        List<Integer> userNumbers = user.getUserNumbers();
+        List<Integer> computerNumbers = computer.getComputer();
+        result = new int[INDEX_SIZE];
+        for(int i=0;i<INDEX_SIZE;i++){
+            if(isCoincide(computerNumbers.get(i),userNumbers.get(i))) continue;
+            if(isContains(computerNumbers,userNumbers.get(i))) continue;
+            result[NOTHING]++;
+        }
+    }
+
+    private boolean isCoincide(int computerNumber, int userNumber) {
+        if(computerNumber == userNumber) {
+            result[STRIKE]++;
+            return true;
+        }
+        return false;
+    }
+    private boolean isContains(List<Integer> computerNumbers, int userNumber){
+        if(computerNumbers.contains(userNumber)) {
+            result[BALL]++;
+            return true;
+        }
+        return false;
+    }
+
 }
