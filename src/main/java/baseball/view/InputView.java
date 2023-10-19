d8af5fd290256d04c0c4152a14c8ebfd96bbaf5b
koomchang <leahpar0401@gmail.com>
2023-10-20 02:32:55 +0900

    feat: 게임이 끝난 경우 재시작/종료를 구분하는 1,2 중 하나의 수 Input 받도록 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -4,6 +4,8 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
 
+    public static final int NEW_GAME_CODE = 1;
+    public static final int END_GAME_CODE = 2;
     private static final int MIN_NUMERIC_RANGE_VALUE = 1;
     private static final int MAX_NUMERIC_RANGE_VALUE = 9;
     private static final int INPUT_LENGTH = 3;
@@ -48,6 +50,23 @@ public class InputView {
         char[] inputArray = input.toCharArray();
         if (inputArray[0] == inputArray[1] || inputArray[1] == inputArray[2] || inputArray[0] == inputArray[2]) {
             throw new IllegalArgumentException("중복된 값이 존재합니다");
+        }
+    }
+
+    public int askForNewGameOrEnd() {
+        String input = readLine();
+        validateNewGameOrEndInput(input);
+        return Integer.parseInt(input);
+    }
+
+    private void validateNewGameOrEndInput(String input) {
+        isNumeric(input);
+        isValueOneOrTwo(input);
+    }
+
+    private void isValueOneOrTwo(String input) {
+        if (Integer.parseInt(input) != NEW_GAME_CODE && Integer.parseInt(input) != END_GAME_CODE) {
+            throw new IllegalArgumentException("1과 2중 입력해야 합니다.");
         }
     }
 }
