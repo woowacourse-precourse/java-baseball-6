ed6507402a798c1c0462d60a001f76b74c0974e8
brightfanta <brightfanta@gmail.com>
2023-10-19 20:21:56 +0900

    feat: print game progress step by step

---
+++ src/main/java/baseball/OutputView.java
@@ -0,0 +1,25 @@
+package baseball;
+
+public class OutputView {
+    public static void printStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+    public static void printRequirePlayerInput() {
+        System.out.println("숫자를 입력해주세요 : ");
+    }
+
+    public static void printMiniResult(Integer strikeCnt, Integer ballCnt) {
+        System.out.println(ballCnt + "볼 " + strikeCnt + "스트라이크");
+        System.out.println(ballCnt + "볼 ");
+        System.out.println(strikeCnt + "스트라이크");
+        System.out.println("낫싱");
+    }
+
+    public static void printEndgame() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public static void printAskRestart() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+}
