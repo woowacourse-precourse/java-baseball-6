881fc484b8e5593db9744544173904285591b249
Jeon HoGeon <jhg2819@naver.com>
2023-10-19 20:13:30 +0900

    feat: 숫자 입력 메시지 출력 기능

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,10 +1,26 @@
 package baseball.view;
 
+import baseball.domain.Result;
+
+import static baseball.domain.Numbers.NUMBER_COUNT;
+
 public class InputView {
 
     private static final String START_STRING = "숫자 야구 게임을 시작합니다.";
+    private static final String NUMBER_INPUT_STRING = "숫자를 입력해주세요 : ";
+    private static final String FINISH_INPUT_STRING = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+            "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String STRIKE_STRING = "스트라이크";
+    private static final String BALL_STRING = "볼";
+    private static final String NON_STRING = "낫싱";
 
-    public static void printStart() {
+    public void printStart() {
         System.out.println(START_STRING);
     }
+
+    public void printNumberInput() {
+        System.out.print(NUMBER_INPUT_STRING);
+    }
+
+
 }
