f78d7ce36fd2eb458f14d734f0223ca2e5fd5a7b
aristo <dkfud2121@naver.com>
2023-10-19 22:44:12 +0900

    feat: test code 전원 통과

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -28,47 +28,52 @@ public class Application {
             }
             if(play_on ==0) {
                 System.out.print("숫자를 입력해주세요 : ");
-                String guess = Console.readLine();
+                try{
+                    String guess = Console.readLine();
 
-                int strike = 0;
-                int ball = 0;
-                int nothing = computer.size();
-                int temp = 0;
+                    int strike = 0;
+                    int ball = 0;
+                    int nothing = computer.size();
+                    int temp = 0;
 
-                while (temp < guess.length()) {
-                    int guess_num = Integer.parseInt(String.valueOf(guess.charAt(temp)));
+                    while (temp < guess.length()) {
+                        int guess_num = Integer.parseInt(String.valueOf(guess.charAt(temp)));
 
-                    if (computer.contains(guess_num)) {
-                        int located_num = computer.get(temp);
-                        if (guess_num == located_num) {
-                            strike++;
-                        } else ball++;
-                    } else nothing--;
-                    temp+=1;
-                }
-                if (nothing != 0) {
-                    if (strike == 3) {
-                        play_on = 3;
+                        if (computer.contains(guess_num)) {
+                            int located_num = computer.get(temp);
+                            if (guess_num == located_num) {
+                                strike++;
+                            } else ball++;
+                        } else nothing--;
+                        temp+=1;
                     }
-                    if (ball == 0) {
-                        System.out.println(strike + "스트라이크");
+                    if (nothing != 0) {
+                        if (strike == 3) {
+                            play_on = 3;
+                        }
+                        if (ball == 0) {
+                            System.out.println(strike + "스트라이크");
+                        }
+                        if (strike == 0) {
+                            System.out.println(ball + "볼");
+                        }
+                        if (strike * ball > 0) {
+                            System.out.println(ball + "볼 " + strike + "스트라이크");
+                        }
                     }
-                    if (strike == 0) {
-                        System.out.println(ball + "볼");
+                    if(nothing ==0){
+                        System.out.println("낫싱");
                     }
-                    if (strike * ball > 0) {
-                        System.out.println(ball + "볼 " + strike + "스트라이크");
-                    }
-                }
-                if(nothing ==0){
-                    System.out.println("낫싱");
+                } catch (IllegalArgumentException e) {
+                    throw e;
+                } catch (IndexOutOfBoundsException e){
+                    throw new IllegalArgumentException();
                 }
             }
             if(play_on==3){
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 int user_input = Integer.parseInt(Console.readLine());
-                System.out.println("[user input] >>> "+user_input);
                 play_on = user_input;
             }
             if(play_on==2){
