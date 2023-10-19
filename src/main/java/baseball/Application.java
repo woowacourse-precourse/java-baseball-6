8dc636587cd0698ef8bc4e8d7a9acae5b3dd9df2
happyhun <younghun.ai@gmail.com>
2023-10-19 23:12:16 +0900

    add: 주석 추가 및 예외 문구 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,14 +1,17 @@
 package baseball;
 
 public class Application {
+
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
+
         while (true) {
-            Game game = new Game();
-            game.start();
-            if (game.isEnd()) {
+            Game game = new Game(); // Game 로직을 담당할 객체를 생성한다.
+            game.start();   // 숫자 야구 게임을 시작한다.
+            if (game.isEnd()) { // 게임이 끝났는지 확인한다.
                 break;
             }
         }
+
     }
 }
--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -8,14 +8,14 @@ import java.util.List;
 
 public class Game {
 
-    private final String answer = generateAnswer();
+    private final String answer = generateAnswer(); // 1부터 9까지 서로 다른 수로 이루어진 3자리의 수
 
     public void start() {
         while (true) {
             System.out.print("숫자를 입력해주세요 : ");
-            String input = Console.readLine();
-            validateInput(input);
-            if (isCorrect(input)) {
+            String input = Console.readLine(); // 사용자로부터 3자리의 수를 입력받는다.
+            validateInput(input); // 입력값 검증
+            if (isCorrect(input)) { // 정답이면 게임 종료
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 break;
             }
@@ -23,33 +23,36 @@ public class Game {
     }
 
     private String generateAnswer() {
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
+        List<Integer> numbers = new ArrayList<>();
+
+        while (numbers.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int number : computer) {
+
+        for (int number : numbers) {
             sb.append(number);
         }
+
         return sb.toString();
     }
 
     private void validateInput(String input) {
         if (input.length() != 3) {
-            throw new IllegalArgumentException("3자리의 숫자를 입력해주세요.");
+            throw new IllegalArgumentException("3자리의 숫자를 입력해주세요!");
         }
         if (!input.matches("[0-9]+")) {
-            throw new IllegalArgumentException("숫자가 아닌 값을 입력하셨습니다.");
+            throw new IllegalArgumentException("숫자만 입력해주세요!");
         }
         if (input.contains("0")) {
-            throw new IllegalArgumentException("0을 포함한 숫자를 입력하셨습니다.");
+            throw new IllegalArgumentException("0은 넣으면 안돼요!");
         }
         if (input.chars().distinct().count() != 3) {
-            throw new IllegalArgumentException("중복되지 않은 숫자를 입력해주세요.");
+            throw new IllegalArgumentException("중복된 숫자는 안돼요!");
         }
     }
 
