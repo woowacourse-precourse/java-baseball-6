e6dc7444132acb55bf9ca6d0ec9445c0468d9a2e
nhlee98 <e5slnh9876@gmail.com>
2023-10-19 20:21:57 +0900

    refactor: GameNumber를 값객체로 변경

--- src/main/java/baseball/domain/GameNumber.java
+++ src/main/java/baseball/domain/GameNumber.java
@@ -1,6 +1,8 @@
 package baseball.domain;
 
-public class GameNumber {
+import java.util.Objects;
+
+public final class GameNumber {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 9;
     private static final String RANGE_EXCEPTION_MESSAGE =
@@ -17,5 +19,22 @@ public class GameNumber {
         if (number < MIN_NUMBER || number > MAX_NUMBER) {
             throw new IllegalArgumentException(RANGE_EXCEPTION_MESSAGE);
         }
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (!(object instanceof GameNumber gameNumber)) {
+            return false;
+        }
+
+        return number == gameNumber.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
