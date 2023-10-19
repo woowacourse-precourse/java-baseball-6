85abdf49fc26c6fe03e22f84f3960f0f7a6f2a80
sayya <djdj9876@naver.com>
2023-10-20 01:20:36 +0900

    feat: Model에 해당하는 Player와 Referee 클래스 구현 -- domain은 숫자를 제시하는 Player와 게임을 진행하는 Referee로 구성하였다 --> Player    1. 공 던지기(숫자 제시) --> Referee    1. goal number 생성하기    2. 볼 판정    3. 삼진 아웃 판정    4. 재시작 요청

--- src/main/java/baseball/model/Player.java
+++ src/main/java/baseball/model/Player.java
@@ -1,4 +1,28 @@
 package baseball.model;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Player {
+
+    public String throwBall() {
+        String ball = Console.readLine();
+        isInvalid(ball);
+
+        return ball;
+    }
+
+    private void isInvalid(String ball) {
+        if (ball.length() != 3 || !isNumberic(ball))
+            throw new IllegalArgumentException("Invalid Input");
+    }
+
+    private boolean isNumberic(String ball) {
+        for (char ch : ball.toCharArray()) {
+            if (!Character.isDigit(ch) || ch == '0')
+                throw new IllegalArgumentException("Invalid Input");
+        }
+        return true;
+    }
+
+
 }
--- src/main/java/baseball/model/Referee.java
+++ src/main/java/baseball/model/Referee.java
@@ -1,4 +1,67 @@
 package baseball.model;
 
+import baseball.view.Message;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Referee {
+
+    public String goalNumber;
+
+    public void setGame() {
+        this.goalNumber = generateGoalNumber();
+    }
+
+    private String generateGoalNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        return computer.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining());
+    }
+
+    public int[] callBallCount(String number) {
+        int ball = 0;
+        int strike = 0;
+        for (int i = 0; i < 3; i++) {
+            char player_number = number.charAt(i);
+            char goal_number = this.goalNumber.charAt(i);
+
+            if (this.goalNumber.contains(String.valueOf(player_number))) {
+                if (player_number != goal_number)
+                    ball++;
+                else
+                    strike++;
+            }
+        }
+        return new int[]{ball, strike};
+    }
+
+    public boolean isStrikeOut(int[] ballCount) {
+        return ballCount[1] == 3;
+    }
+
+    public boolean isRestart() {
+        String number = Console.readLine();
+        isValid(number);
+
+        return number.equals("1");
+    }
+
+    private void isValid(String number) {
+        if (!number.equals("1") && !number.equals("2"))
+            throw new IllegalArgumentException("Invalid Input");
+    }
+
+
 }
