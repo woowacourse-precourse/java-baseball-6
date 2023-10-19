8a71b1aac79998e9223631d8c4468debd22c382e
Jiyoung Choi <12201817@inha.edu>
2023-10-19 22:02:02 +0900

    feat(HintCalculator) : 사용자 수와 컴퓨터 수 비교 후 스트라이크, 볼 개수 저장 기능

---
+++ src/main/java/baseball/controller/HintCalculator.java
@@ -0,0 +1,29 @@
+package baseball.controller;
+
+import java.util.List;
+
+public class HintCalculator {
+    private final static int LIST_SIZE = 3;
+    private int strikeCnt = 0;
+    private int ballCnt = 0;
+
+
+    public HintCalculator(List<Integer> userNumber, List<Integer> computerNumber) {
+        for (int i = 0; i < LIST_SIZE; ++i) {
+            if (isStrike(userNumber.get(i), computerNumber.get(i))) {
+                ++strikeCnt;
+            } else if (isBall(userNumber.get(i), computerNumber.get(i))) {
+                ++ballCnt;
+            }
+        }
+    }
+
+    private boolean isStrike(Integer integer, Integer integer1) {
+        return false;
+    }
+
+    private boolean isBall(Integer integer, Integer integer1) {
+        return false;
+    }
+
+}
