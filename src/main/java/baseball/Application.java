9ceea79b2cf03018f63a51cd3157dae85a5739dc
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-10-19 18:57:23 +0900

    [Feat] #0 - 환경 설정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 public class Application {
+    public static final String GAME_START = "숫자 야구 게임을 시작합니다.";
+    public static final String INPUT_START = "숫자를 입력 해주세요 : ";
+    public static final String GAME_FINISH = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String NOTHING = "낫싱";
+    public static final String STRIKE = "스트라이크";
+    public static final String BALL = "볼";
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Boolean restart = true;
+        while (restart) {
+    }
     }
 }
