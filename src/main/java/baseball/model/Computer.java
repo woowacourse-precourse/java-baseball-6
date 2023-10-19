0f2fedcff70c306aecb1af83c472466337f7b8ae
HongChaa <jun761_1@naver.com>
2023-10-19 17:39:16 +0900

    feat(Computer): setter 및 getter 생성

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -1,4 +1,17 @@
 package baseball.model;
 
+import java.util.List;
+
 public class Computer {
+
+    private List<Integer> computerRandomNumber;
+
+    public List<Integer> getComputerRandomNumber() {
+        return computerRandomNumber;
+    }
+
+    public void setComputerRandomNumber(List<Integer> computerRandomNumber) {
+        this.computerRandomNumber = computerRandomNumber;
+    }
+
 }
