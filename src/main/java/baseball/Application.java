5f96ddae0b898482bee978a9ca5d3fa0554e982f
song-jae-wan <sjo9810@gmail.com>
2023-10-19 16:32:23 +0900

    Docs: 구현할 기능목록
    요구사항 정리 완료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        boolean[] temp = new boolean[10];
+        while(true) {
+            int val = Randoms.pickNumberInRange(0,8) + 1;
+            System.out.println(val);
+            temp[val] = true;
+            int cnt = 0;
+            for(int i = 1; i < 10; i++) {
+                if(temp[i]) { cnt++; }
+            }
+            if(cnt == 9) { break; }
+
+        }
+        System.out.println("done");
+
     }
 }
