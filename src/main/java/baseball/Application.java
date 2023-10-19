e7d40925291a7813280e52906fd0710d058a5914
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-10-19 17:03:18 +0900

    refactor:BaseballGame -> Controller/BaseBallGameController로 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,10 @@
 package baseball;
 
+import baseball.Controller.BaseBallGameController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        new BaseBallGame().gameStart();
+        new BaseBallGameController().gameStart();
     }
 }
--- src/main/java/baseball/Controller/BaseBallGameController.java
+++ src/main/java/baseball/Controller/BaseBallGameController.java
@@ -1,6 +1,6 @@
-package baseball;
+package baseball.Controller;
 
-public class BaseBallGame {
+public class BaseBallGameController {
     public void gameStart(){
         System.out.println("게임을 시작합니다.");
     }
