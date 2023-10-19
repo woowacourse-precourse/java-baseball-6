77d4d35ba237953074d20963d7c23299700615ee
mainfn <_@mainfn.dev>
2023-10-19 16:03:54 +0900

    refact: GameResult record class로 변경

--- src/main/java/baseball/domain/GameResult.java
+++ src/main/java/baseball/domain/GameResult.java
@@ -1,27 +1,15 @@
 package baseball.domain;
 
-public final class GameResult {
-
-  private final int ballCount;
-  private final int strikeCount;
-
-  public GameResult(
-      final int ballCount,
-      final int strikeCount
-  ) {
-    this.ballCount = ballCount;
-    this.strikeCount = strikeCount;
-  }
-
-  public int getBallCount() {
-    return ballCount;
-  }
-
-  public int getStrikeCount() {
-    return strikeCount;
-  }
+public record GameResult(
+    int ballCount,
+    int strikeCount
+) {
 
   public boolean isClear() {
     return strikeCount == BallNumbers.BALL_COUNT;
+  }
+
+  public boolean isNothing() {
+    return ballCount + strikeCount == 0;
   }
 }
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -5,14 +5,14 @@ import baseball.domain.GameResult;
 public final class OutputView {
 
   public void printResult(final GameResult result) {
-    if (result.getBallCount() + result.getStrikeCount() == 0) {
+    if (result.isNothing()) {
       System.out.print("낫싱");
     }
-    if (result.getBallCount() > 0) {
-      System.out.printf("%d볼 ", result.getBallCount());
+    if (result.ballCount() > 0) {
+      System.out.printf("%d볼 ", result.ballCount());
     }
-    if (result.getStrikeCount() > 0) {
-      System.out.printf("%d스트라이크", result.getStrikeCount());
+    if (result.strikeCount() > 0) {
+      System.out.printf("%d스트라이크", result.strikeCount());
     }
     if (result.isClear()) {
       System.out.print("\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
