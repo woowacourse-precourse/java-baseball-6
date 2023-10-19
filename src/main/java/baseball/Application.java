72c98890e7e82f9c35cecbae0edc6552f3464032
dyjeong365 <dyjeong365@gmail.com>
2023-10-19 21:42:26 +0900

    feat: add getComputerPicks()

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,33 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.*;
+
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        playBaseballGame();
+    }
+
+    private static void playBaseballGame() {
+        List<Integer> computer = getComputerPicks();
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        while (true) {
+            int balls = 0;
+            int strikes = 0;
+            System.out.print("숫자를 입력해주세요 : ");
+
+            String input = Console.readLine();
+    private static List<Integer> getComputerPicks() {
+        List<Integer> computer = new ArrayList<>();
+
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 }
