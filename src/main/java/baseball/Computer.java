829c35de01127e56298b4d8cb87188e18d748ea5
sohyun9527 <sohyun95277@gmail.com>
2023-10-20 00:04:18 +0900

    feat(Computer) ball, stirke의 개수를 세어 리스트 반환하는 메서드 작성, answer 저장 위치 변경 생성한 정답을 계속 메서드에서 메서드로 옮기는건 비효율적인 것같다는 생각 Game에서 한 번 생성한 후, Computer에서 값 저장 후 계산하는 방식으로 구현

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -6,8 +6,24 @@ public class Computer {
     final int NOTHING = 0;
     final int BALL = 1;
     final int STRIKE = 2;
+    List<Integer> answer;
 
-    int judgeScore(List<Integer> answer, int number, int index) {
+    List<Integer> countScore(List<Integer> userNumbers) {
+        int ball = 0;
+        int strike = 0;
+        for (int i = 0; i < userNumbers.size(); i++) {
+            int score = judgeScore(userNumbers.get(i), i);
+            if (score == STRIKE) {
+                strike++;
+            }
+            if (score == ball) {
+                ball++;
+            }
+        }
+        return List.of(ball, strike);
+    }
+
+    int judgeScore(int number, int index) {
         if (answer.get(index) == number) {
             return (STRIKE);
         }
