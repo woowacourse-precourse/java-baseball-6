5c44fbb78aaaa6e2bd4b5c7b351304b4fb8d01b8
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-10-20 02:22:15 +0900

    feat: 상수 정의

---
+++ src/main/java/baseball/controller/BaseballController.java
@@ -0,0 +1,18 @@
+package baseball.controller;
+
+import baseball.domain.Computer;
+import baseball.domain.User;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+import java.util.List;
+
+public class BaseballController {
+    private static final int INDEX_SIZE = 3;
+    private static final int BALL = 0;
+    private static final int STRIKE = 1;
+    private static final int NOTHING = 2;
+    private static final int NO_HIT = 0;
+    private static final int MAX_HIT = 3;
+
+}
