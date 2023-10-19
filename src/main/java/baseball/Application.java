3de1d5a0aa2031d365bf289d6693032c7caf96ea
kkw2758 <dtc3229@gmail.com>
2023-10-19 22:03:19 +0900

    Feat: "객체 추가"
    User, Computer 객체 추가
    .gitignore에 커밋 컨벤션 설정 파일 .gitmessage.txt 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String number;
+        User user;
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("숫자를 입력해주세요 : ");
+        number = sc.nextLine();
+        user = new User(number);
+        }
     }
-}
---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,47 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private final List<Integer> user = new ArrayList<>();
+
+    public User(String number){
+        checkInteger(number);
+        checkDuplicateNumber(number);
+        checkLength(number);
+        generateUser(number);
+    }
+
+    private void checkLength(String number) {
+        if (number.length() != 3) {
+            throw new IllegalArgumentException("길이가 3이 아닙니다.");
+        }
+    }
+
+    private void checkDuplicateNumber(String number) {
+        for (String stringNumber : number.split("")) {
+            int intNumber = Integer.parseInt(stringNumber);
+            if (user.contains(intNumber)) {
+                throw new IllegalArgumentException("중복된 숫자가 있습니다.");
+            }
+        }
+    }
+
+    private void checkInteger(String number) {
+        for (String stringNumber : number.split("")) {
+            try {
+                int intNumber = Integer.parseInt(stringNumber);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다.");
+            }
+        }
+    }
+
+    private void generateUser(String number) {
+        for (String stringNumber : number.split("")) {
+            int intNumber = Integer.parseInt(stringNumber);
+            user.add(intNumber);
+        }
+    }
+}
