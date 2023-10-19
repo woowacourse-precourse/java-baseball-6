98cfa080b1dae4dbcb6bebb849ca26d19b04562b
Seonghwan <aiya0422@gmail.com>
2023-10-19 17:53:50 +0900

    FEAT: 출력 기능 showResult 구현

--- src/main/java/baseball/Printing.java
+++ src/main/java/baseball/Printing.java
@@ -8,4 +8,16 @@ public class Printing {
     public static void closing() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
+
+    public static void showResult(int strike, int ball) {
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+        } else if (strike == 0) {
+            System.out.println(ball + "볼");
+        } else if (ball == 0) {
+            System.out.println(strike + "스트라이크");
+        } else {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
+    }
 }
