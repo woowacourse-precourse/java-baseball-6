4ceec50ee94d050116d6f4b8b02a2fefc6ba84f7
yyeennyy <kye3314@gmail.com>
2023-10-19 23:06:32 +0900

    feat: 사용자에게 숫자 입력 받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -43,8 +43,30 @@ public class Application {
     private static int inputNumber() {
         // 사용자로부터 숫자 입력 받기
         // checkNumber()으로 검증
-        return 0;
+        System.out.print("숫자를 입력해주세요 : ");
+        String inputString = readLine();
+        return checkNumber(inputString);
     }
+
+    private static int checkNumber(String inputString) {
+        int number;
+        // 사용자 인풋이 3자리가 아니면 예외 발생
+        if (inputString.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+        // 사용자 인풋에 "0"이 포함되면 예외 발생
+        if (inputString.contains("0")) {
+            throw new IllegalArgumentException();
+        }
+        // int로 파싱 불가능하면 예외 발생
+        try {
+            number = Integer.parseInt(inputString);
+        } catch (Exception e){
+            throw new IllegalArgumentException();
+        }
+        return number;
+    }
+
 
     private static boolean isContinue(int option) {
         // option값 유효성 검증 후,
