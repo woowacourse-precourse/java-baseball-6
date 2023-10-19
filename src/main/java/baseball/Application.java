d08f5ab426686a7a29d43ede1a9ba1ed5149f59f
devhoya97 <diajbh123@gmail.com>
2023-10-19 16:00:56 +0900

    feature : 메서드 getComputerNumbersList 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,10 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     static final String GET_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
@@ -10,8 +15,20 @@ public class Application {
     static final String GET_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        printStart();
+
     }
     public static void printStart() {
         System.out.println(START_MESSAGE);
+    }
+    public static List<Integer> getComputerNumbersList() {
+        List<Integer> computerNumbersList = new ArrayList<>();
+        while (computerNumbersList.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumbersList.contains(randomNumber)) {
+                computerNumbersList.add(randomNumber);
+            }
+        }
+        return computerNumbersList;
     }
 }
