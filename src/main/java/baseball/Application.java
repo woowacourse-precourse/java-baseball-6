1dce9566aab632cc50eeaaa369765665534a74c8
hackertaco <imsa1224@korea.ac.kr>
2023-10-19 19:26:03 +0900

    feat(2): 컴퓨터가 1-9까지의 숫자 중 임의의 수 3개를 선택한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,26 @@
 package baseball;
 
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    static final int DIGIT = 3;
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.\n");
+        List<Integer> pickedList = selectDigitNumbers(DIGIT);
+        assert(pickedList.stream().distinct().count() == DIGIT);
     }
 
-
+    public static List<Integer> selectDigitNumbers(int digit){
+        List<Integer> pickedList = new ArrayList<>();
+        while(pickedList.size() < digit){
+            int pickedRandomNum = Randoms.pickNumberInRange(1, 9);
+            if(pickedList.contains(pickedRandomNum)) continue;
+            pickedList.add(pickedRandomNum);
+        }
+        return pickedList;
+    }
 }
