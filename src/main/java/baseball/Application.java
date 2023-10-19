314068d8e375985521f3520898d104f752d37aaa
haroya01 <haroya01@naver.com>
2023-10-20 05:49:03 +0900

    feat: 게임 종료 문구를 출력 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,7 @@ public class Application {
     public static void main(String[] args) {
         // 숫자 야구 게임 시작
         new BaseBallGameStart();
+        new BaseBallGameEnd();
 
     }
 }
--- src/main/java/baseball/BaseBallGameEnd.java
+++ src/main/java/baseball/BaseBallGameEnd.java
@@ -1,6 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class BaseBallGameEnd {
     //TODO: 게임 종료 및 재시작
+
+    public BaseBallGameEnd() {
+        output();
+        String playerOrder = input();
+        System.out.println("input : " + playerOrder);
+    }
+
+    private void output() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    private String input(){
+        return Console.readLine();
+    }
 }
 
