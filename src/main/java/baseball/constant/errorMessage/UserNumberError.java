cf046f89c1525bf0723fdb31abafb6344f49f09c
Hugh <lee2317884@gmail.com>
2023-10-19 23:16:10 +0900

    feat: 입력값 길이 조건 에러 메시지 상수 처리

--- src/main/java/baseball/constant/errorMessage/UserNumberError.java
+++ src/main/java/baseball/constant/errorMessage/UserNumberError.java
@@ -3,5 +3,6 @@ package baseball.constant.errorMessage;
 public class UserNumberError {
 
 	public static final String USER_NUMBER_WRONG_INPUT_ERROR_MESSAGE = "[ERROR] 숫자가 아닌 값은 입력할 수 없습니다. 게임을 종료합니다.";
+	public static final String USER_NUMBER_WRONG_LENGTH_ERROR_MESSAGE = "[ERROR] 세 개의 숫자 입력이 필요합니다. 게임을 종료합니다.";
 
 }
