fc78ba5159a18a7545e2e15443794acb6dd3dca9
mmihye <smilehye910@naver.com>
2023-10-19 22:55:00 +0900

    [FIX]입력받는 숫자 타입 변경

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -1,23 +1,40 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BaseBallGame {
-    private int answerNum;
+    ArrayList<Integer> answerNum = new ArrayList<>();
+    ArrayList<Integer> userNum = new ArrayList<>();
+
     private Boolean isPlaying = false;
 
     public void startGame(){
         System.out.println("숫자 야구 게임을 시작합니다.");
+
+        while (answerNum.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!answerNum.contains(randomNumber)) {
+                answerNum.add(randomNumber);
+            }
+        }
     }
 
     public void chooseNum(){
         System.out.print("숫자를 입력해주세요 : ");
-        String userNum = Console.readLine();
-        returnResult(userNum);
+        String input = Console.readLine();
+        String[] userNumAsString = input.split("");
+        for(String str : userNumAsString){
+            int number = Integer.parseInt(str);
+            userNum.add(number);
+        }
     }
 
-    public String returnResult(String userNum){
-        return "ok";
+    public void returnResult(){
+
     }
 
     public void exitGame(){
