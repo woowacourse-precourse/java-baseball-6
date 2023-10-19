02ea2a829cfebaf7420216910646091dc3355042
freekyr7529 <freekyr7529@gmail.com>
2023-10-19 19:26:32 +0900

    feat: 난수 생성 기능 구현
    Randoms API를 통해 3자리의 난수를 생성하는 기능 구현

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -1,13 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
     private static Integer command = 1;
 
     public static void run() {
+        Controller controller = new Controller();
+
         InputView.greetingMsg();
+        Answer answer = new Answer(controller.createAnswer());
 
         while (command != 2) {
             Integer response = InputView.inputGuideMsg();
         }
+    }
+
+    private List<Integer> createAnswer() {
+        List<Integer> randNums = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            randNums.add(Randoms.pickNumberInRange(1, 9));
+        }
+
+        return randNums;
     }
 }
