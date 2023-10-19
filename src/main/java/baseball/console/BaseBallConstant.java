03b0122502221a683aef6b0a748195b4a368c81e
takealook97 <takealook97@naver.com>
2023-10-20 02:13:44 +0900

    feat: "낫싱" enum 추가
    실수로 빠뜨렸던 "낫싱" 또한 상수로 추가했다.

--- src/main/java/baseball/console/BaseBallConstant.java
+++ src/main/java/baseball/console/BaseBallConstant.java
@@ -5,6 +5,7 @@ public enum BaseBallConstant {
 	INPUT_REQ("숫자를 입력해주세요 : "),
 	BALL("볼"),
 	STRIKE("스트라이크"),
+	NONE("낫싱"),
 	SUCCESS("3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
 	GAME_END("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."),
 	RESTART("1"),
