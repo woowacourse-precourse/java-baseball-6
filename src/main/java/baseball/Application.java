c8b0b6f8fd9b40f8e30c0f91ecc97926a875de27
정민규 <fastdodge7@naver.com>
2023-10-19 18:23:43 +0900

    [Feat] 사용자 입력 및 입력값 검증 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,60 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
 public class Application {
+
+    private static final Pattern inputValidator = Pattern.compile("^([1-9]{3})$");
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        while(true) {
+            NumberBaseball game = new NumberBaseball();
+            System.out.println("숫자 야구 게임을 시작합니다.");
+            while(true) {
+                System.out.print("숫자를 입력해 주세요 : ");
+                String inputString = Console.readLine();
+
+                int[] userNumber = parseInput(inputString);
+
+
+
+
+
+
+            }
+        }
+    }
+
+
+
+    public static int[] parseInput(String inputString) throws IllegalArgumentException {
+        if(!inputValidator.matcher(inputString).matches()) {
+            throw new IllegalArgumentException();
+        }
+
+        int[] userNumber = (int[])Arrays.stream(inputString.split("")).mapToInt(
+                element -> Integer.parseInt(element)
+        ).toArray();
+
+        if(doContainDupNumber(userNumber)) {
+            throw new IllegalArgumentException();
+        }
+        return userNumber;
+    }
+
+    public static boolean doContainDupNumber(int[] input) {
+        boolean[] numberFlag = new boolean[10];
+        for(int i = 0; i < 3; i++) {
+            if(numberFlag[input[i]]) {
+                return true;
+            }
+            numberFlag[input[i]] = true;
+        }
+        return false;
     }
 }
