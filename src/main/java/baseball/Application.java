c13e57d6eb5b0f21d01a1153c806f750b500a77e
suhyun9764 <suhyun9764@naver.com>
2023-10-19 22:40:57 +0900

    컴퓨터 랜덤 값 생성 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,33 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+
 public class Application {
+    private static int playDecision = 0;
+    private static int strike =0;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        do{
+            play();
+        }while (playDecision==1);
+    }
+
+    private static void play() {
+        while (true){
+            ArrayList<Integer> computerAnswer = getComputerAnswer();
+        }
+    }
+
+    private static ArrayList<Integer> getComputerAnswer() {
+        ArrayList<Integer> computerAnswer = new ArrayList<>();
+        while (computerAnswer.size()<3){
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if(!computerAnswer.contains(randomNumber)){
+                computerAnswer.add(randomNumber);
+            }
+        }
+        return computerAnswer;
     }
 }
