6968af527b78f71c931249200b48bbe882aa37e2
hongseokhodev <hongseokhodev@gmail.com>
2023-10-19 21:32:47 +0900

    test: add test case for length over 3

--- src/test/java/baseball/domain/BallsTest.java
+++ src/test/java/baseball/domain/BallsTest.java
@@ -11,4 +11,10 @@ public class BallsTest {
         List<Integer> input = List.of(1, 1, 1);
         assertThatThrownBy(() -> Balls.from(input)).isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    void 길이가_3보다_크면_예외() {
+        List<Integer> input = List.of(1, 2, 3, 4);
+        assertThatThrownBy(() -> Balls.from(input)).isInstanceOf(IllegalArgumentException.class);
+    }
 }
