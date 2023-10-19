2e71c58622f27d06f39ffdb37590fc2d17578457
dy102 <132787602+dy102@users.noreply.github.com>
2023-10-19 19:59:06 +0900

    feature(Computer):랜덤한 세자리 숫자를 생성하는 컴퓨터

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,24 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private List<Integer> randomNumbers = new ArrayList<>();
+
+    public List<Integer> getRandomNumbers() {
+        return randomNumbers;
+    }
+
+    public Computer() {
+        while (randomNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!randomNumbers.contains(randomNumber)) {
+                randomNumbers.add(randomNumber);
+            }
+        }
+        System.out.println(randomNumbers);
+    }
+}
