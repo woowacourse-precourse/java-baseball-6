8b3c3bd75ee27a07579305820c42be14f556df31
gyungchan Jo <namejgc@naver.com>
2023-10-19 19:12:52 +0900

    refactor(BaseballGame): 현재 게임에 대한 상태를 가지도록 변경 게임이 진행중인가, 게임이 종료되었는가

--- src/main/java/baseball/domain/BaseballGame.java
+++ src/main/java/baseball/domain/BaseballGame.java
@@ -2,9 +2,11 @@ package baseball.domain;
 
 public class BaseballGame {
     private final Balls computerBalls;
+    private GameState gameState;
 
     public BaseballGame(Balls computerBalls) {
         this.computerBalls = computerBalls;
+        this.gameState = GameState.PLAYING;
     }
 
     public PlayResult play(Balls userBalls) {
