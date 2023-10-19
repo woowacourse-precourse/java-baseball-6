e38ca62d8c28973ec11b0365abb336ebca2c4595
jmj005 <jmj00005@naver.com>
2023-10-20 02:40:33 +0900

    feat: 플레이어 숫자 자릿수 확인

--- src/main/java/baseball/presenter/GamePresenter.java
+++ src/main/java/baseball/presenter/GamePresenter.java
@@ -4,4 +4,8 @@ public class GamePresenter {
     private boolean isValidNumeric(String numbers) {
         return numbers.matches("^[0-9]+$");
     }
+
+    private boolean isValidLength(String numbers) {
+        return numbers.length() == 3;
+    }
 }
