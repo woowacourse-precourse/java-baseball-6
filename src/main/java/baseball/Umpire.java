dc25c181fa44515ab21beaab08ae2c86bc8571bc
devholic22 <hyunjoon.tech@gmail.com>
2023-10-20 08:49:04 +0900

    Feat: 볼 판정 구현

--- src/main/java/baseball/Umpire.java
+++ src/main/java/baseball/Umpire.java
@@ -1,5 +1,8 @@
 package baseball;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class Umpire {
 
     public static int countStrike(final int originNumber, final int testNumber) {
@@ -26,5 +29,38 @@ public class Umpire {
 
     private static boolean isBothSame(final String origin, final String test) {
         return origin.equals(test);
+    }
+
+    public static int countBall(final int originNumber, final int testNumber) {
+        String[] origin = convertStringToArray(convertNumberToString(originNumber));
+        String[] test = convertStringToArray(convertNumberToString(testNumber));
+
+        boolean[] onlyOrigin = new boolean[origin.length];
+        boolean[] onlyTest = new boolean[test.length];
+        Arrays.fill(onlyOrigin, true);
+        Arrays.fill(onlyTest, true);
+
+        for (int i = 0; i < 3; i++) {
+            if (origin[i].equals(test[i])) {
+                onlyOrigin[i] = false;
+                onlyTest[i] = false;
+            }
+        }
+
+        HashSet<String> originSet = new HashSet<>();
+        HashSet<String> testSet = new HashSet<>();
+
+        for (int i = 0; i < origin.length; i++) {
+            if (onlyOrigin[i]) {
+                originSet.add(origin[i]);
+            }
+            if (onlyTest[i]) {
+                testSet.add(test[i]);
+            }
+        }
+
+        originSet.retainAll(testSet);
+
+        return originSet.size();
     }
 }
