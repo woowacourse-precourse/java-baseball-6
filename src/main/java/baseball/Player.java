f886d5b12c5ce579e570475bdf4d700ab3b28879
1000zoo <cjswldn99@gmail.com>
2023-10-19 17:29:18 +0900

    feat(Player): Player 클래스 생성 및 게임 실행 메서드
    - start 메서드로 게임 시작
    - play 메서드로 게임 루프 시작
    - playAgain 메서드로 재시작 및 종료 선택

---
+++ src/main/java/baseball/Player.java
@@ -0,0 +1,27 @@
+package baseball;
+
+public class Player {
+
+    private Computer computer;
+
+    public Player() {
+        computer = new Computer();
+    }
+
+    public void start() {
+        play();
+        playAgain();
+    }
+
+    public void play() {
+        while (true) {
+
+        }
+    }
+
+    private void playAgain() {
+        if (true) {
+            start();
+        }
+    }
+}
