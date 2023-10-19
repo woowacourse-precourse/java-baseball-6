aafccfe3212c48a202caf801c623b6979987690f
Yeriimii <seojs0511@naver.com>
2023-10-19 23:15:13 +0900

    1차 기능구현

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,12 +1,14 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -31,5 +33,65 @@ class ApplicationTest extends NsTest {
     @Override
     public void runMain() {
         Application.main(new String[]{});
+    }
+
+    @Test
+    public void 사용자_입력_테스트_올바른_값() {
+        // given
+        String[] userNumber = new String[] {"1", "2", "3"};
+
+        // when
+        boolean correctNumber = Application.isCorrectNumber(userNumber);
+
+        // then
+        Assertions.assertThat(correctNumber).isTrue();
+    }
+
+    @Test
+    public void 사용자_입력_테스트_제로_입력() throws IllegalArgumentException {
+        // given
+        String[] userNumber = new String[] {"0", "2", "2"};
+
+        // when
+
+
+        // then
+        assertThrows(IllegalArgumentException.class, () -> Application.isCorrectNumber(userNumber));
+    }
+
+    @Test
+    public void 사용자_입력_테스트_중복값() throws IllegalArgumentException {
+        // given
+        String[] userNumber = new String[] {"1", "2", "2"};
+
+        // when
+
+
+        // then
+        assertThrows(IllegalArgumentException.class, () -> Application.isCorrectNumber(userNumber));
+    }
+
+    @Test
+    public void 사용자_입력_테스트_문자() throws IllegalArgumentException {
+        // given
+        String[] userNumber = new String[] {"1", "2", "abc"};
+
+        // when
+
+
+        // then
+        assertThrows(IllegalArgumentException.class, () -> Application.isCorrectNumber(userNumber));
+    }
+
+    @Test
+    public void 사용자_입력_테스트_길이_초과() throws IllegalArgumentException {
+        // given
+        String[] userNumber = new String[] {"1", "2", "3", "4"};
+
+        // when
+
+
+        // then
+        assertThrows(IllegalArgumentException.class, () -> Application.isCorrectNumber(userNumber));
     }
 }
