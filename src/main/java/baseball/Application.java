5bf4a9ab9b01236f3867ebba63bd3320aba448cc
King's meow <mingee641@gmail.com>
2023-10-19 22:18:37 +0900

    feat: 컴퓨터 숫자 랜덤 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,25 @@
 package baseball;
 
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // 게임 시작을 사용자에게 알린다.
         System.out.println("숫자 야구 게임을 시작합니다.");
+
+        // 컴퓨터 숫자 랜덤 생성
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+                //System.out.println(randomNumber + " ");
+            }
+        }
 
     }
 }
