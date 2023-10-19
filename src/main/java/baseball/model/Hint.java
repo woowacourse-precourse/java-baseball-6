8c95c75063d61e1c9f222d07416020376fb1be06
heiler <threepebbles@naver.com>
2023-10-20 00:04:35 +0900

    feat(OutputView): 힌트 출력 기능 추가

---
+++ src/main/java/baseball/model/Hint.java
@@ -0,0 +1,29 @@
+package baseball.model;
+
+public class Hint {
+    int ballCount;
+    int strikeCount;
+
+    public Hint() {};
+
+    public Hint(int ballCount, int strikeCount) {
+        this.ballCount = ballCount;
+        this.strikeCount = strikeCount;
+    }
+
+    public int getBallCount() {
+        return ballCount;
+    }
+
+    public void setBallCount(int ballCount) {
+        this.ballCount = ballCount;
+    }
+
+    public int getStrikeCount() {
+        return strikeCount;
+    }
+
+    public void setStrikeCount(int strikeCount) {
+        this.strikeCount = strikeCount;
+    }
+}
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,9 +1,15 @@
-package baseball.View;
+package baseball.view;
+
+import baseball.model.Hint;
 
 public class OutputView {
     private static final String START_GAME_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String END_GAME_MESSAGE = "게임 종료";
     private static final String SUCCESS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다!";
+    private static final String NOTHING_MESSAGE = "낫싱";
+    private static final String BALL_MESSAGE = "볼";
+    private static final String STRIKE_MESSAGE = "스트라이크";
+    private static final String SPACE = " ";
 
     public static void printStartGameMessage() {
         System.out.println(START_GAME_MESSAGE);
@@ -14,5 +20,38 @@ public class OutputView {
 
     public static void printSuccessMessage() {
         System.out.println(SUCCESS_MESSAGE);
+    }
+
+    public static void printHintMessage(Hint hint) {
+        if(hint.getBallCount() == 0 && hint.getStrikeCount() == 0) {
+            printNothingMessage();
+        }
+        else if(hint.getBallCount() == 0) {
+            printStrikeMessage(hint.getStrikeCount());
+        }
+        else if(hint.getStrikeCount() == 0) {
+            printBallMessage(hint.getBallCount());
+        }
+        else {
+            printBallStrikeMessage(hint.getBallCount(), hint.getStrikeCount());
+        }
+    }
+
+    public static void printNothingMessage() {
+        System.out.println(NOTHING_MESSAGE);
+    }
+    public static void printBallMessage(Integer ballCount) {
+        System.out.println(ballCount.toString() + BALL_MESSAGE);
+    }
+
+    public static void printStrikeMessage(Integer strikeCount) {
+        System.out.println(strikeCount.toString() + STRIKE_MESSAGE);
+    }
+
+    public static void printBallStrikeMessage(Integer ballCount, Integer strikeCount) {
+        String Message = ballCount.toString() + BALL_MESSAGE +
+                SPACE +
+                strikeCount.toString() + STRIKE_MESSAGE;
+        System.out.println(Message);
     }
 }
