9234c7d3b666206462e5f8173b6ca65da95b66cd
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2023-10-19 22:10:22 +0900

    feat: 플레이어 숫자 자릿수 검증 기능

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -13,6 +13,7 @@ public class Player {
 
     private List<Integer> generatePlayerNumbers(String input) {
         validateNotZero(input);
+        validateNumberDigit(input);
         List<Integer> numbers = new ArrayList<>();
         for (char number : input.toCharArray()) {
             numbers.add(number - '0');
@@ -22,6 +23,12 @@ public class Player {
 
     private void validateNotZero(String input) {
         if (input.contains("0")) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNumberDigit(String input) {
+        if(input.length() != 3) {
             throw new IllegalArgumentException();
         }
     }
