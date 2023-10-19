3b4def4923288869c7d2f27b462230bbf9894295
sohyun9527 <sohyun95277@gmail.com>
2023-10-19 23:23:09 +0900

    feat(Validation): 유저가 입력한 내용에 대한 validation 기능을 수행하는 메서드 구현

--- src/main/java/baseball/Validation.java
+++ src/main/java/baseball/Validation.java
@@ -1,9 +1,31 @@
 package baseball;
 
-public class Validation {
-    char restart = '1';
-    char end = '2';
+import java.util.HashSet;
+import java.util.Set;
 
+public class Validation {
+    final char restart = '1';
+    final char end = '2';
+
+    void validateThreeNumbers(String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("3개의 숫자만 입력해주세요.");
+        }
+        Set<Character> charSet = new HashSet<>();
+        for (int i = 0; i < input.length(); i++) {
+            char num = input.charAt(i);
+            if (!Character.isDigit(num)) {
+                throw new IllegalArgumentException("숫자만 입력해 주세요");
+            }
+            if (num == '0') {
+                throw new IllegalArgumentException("1 ~ 9 숫자만 넣어주세요. 0은 입력할 수 없습니다.");
+            }
+            charSet.add(num);
+        }
+        if (charSet.size() != 3) {
+            throw new IllegalArgumentException("중복된 수는 입력할 수 없습니다");
+        }
+    }
     public void validateContinueSign(String input) {
         if (input.length() != 1) {
             throw new IllegalArgumentException("1 혹은 2만 입력해 주세요.");
