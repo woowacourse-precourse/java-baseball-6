5054152880e28664cb950ee122a65cde12ebffea
song-jae-wan <sjo9810@gmail.com>
2023-10-19 20:23:32 +0900

    Feat: main에 실행을 위한 객체 생성
    BaseballGame 객체 생성 후
    run 메소드 호출로 게임 실행하게 만듦

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,8 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-
+        BaseballGame game = new BaseballGame();
+        game.run();
     }
 }
