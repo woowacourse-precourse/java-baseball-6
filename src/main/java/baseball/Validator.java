bbd391e275c222e2eb1e4a82e88619c6f069428b
Jaehoon-So <jhspacelover@naver.com>
2023-10-20 00:09:37 +0900

    add: IllegalStateException 메세지 추가

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -25,21 +25,21 @@ public class Validator {
 
     private static void validateInputRange(final String input) {
         if (input.length() == Const.NUMBER_LENGTH) return;
-        throw new IllegalStateException();
+        throw new IllegalStateException("세개의 숫자만 입력받을 수 있습니다.");
     }
 
     private static void validateInputDigit(final String input) {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalStateException(e);
+            throw new IllegalStateException("1~9 이외의 문자가 입력되었습니다.");
         }
     }
 
     private static void validateInputContainsZero(final String input) {
         String[] split = input.split("");
         if (Arrays.asList(split).contains("0")) {
-            throw new IllegalStateException();
+            throw new IllegalStateException("입력에 0이 포함되었습니다.");
         }
     }
 
@@ -47,7 +47,7 @@ public class Validator {
         Set<Character> set = new HashSet<>();
         for (int i = 0; i < input.length(); i++) {
             if (set.contains(input.charAt(i))) {
-                throw new IllegalStateException();
+                throw new IllegalStateException("중복된 숫자가 입력되었습니다.");
             }
             set.add(input.charAt(i));
         }
