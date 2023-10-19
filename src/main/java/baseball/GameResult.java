e428a0643d646b227d90d58a3ba6d58c7f83e423
HwangSeokJin <ghkd3703@gmail.com>
2023-10-19 20:18:47 +0900

    feat: 결과를 계산하는 기능 구현

---
+++ src/main/java/baseball/GameResult.java
@@ -0,0 +1,42 @@
+package baseball;
+
+import java.util.List;
+import java.util.HashMap;
+
+public class GameResult {
+    private HashMap<String, Integer> result;
+    private List<Integer> computer;
+    private List<Integer> player;
+    private int strike;
+    private int ball;
+
+
+    public GameResult(List<Integer> computer, List<Integer> player) {
+        this.computer = computer;
+        this.player = player;
+        calculate();
+    }
+
+    private void calculate() {
+        calculateBallCount();
+        calculateStrikeCount();
+    }
+
+    private void calculateStrikeCount() {
+        strike = 0;
+        for (int i = 0; i < 3; i++) {
+            if (computer.get(i) == player.get(i)) {
+                strike += 1;
+            }
+        }
+    }
+
+    private void calculateBallCount() {
+        ball = 0;
+        for (int i = 0; i < 3; i++) {
+            if (computer.get(i) != player.get(i) && computer.contains(player.get(i))) {
+                ball += 1;
+            }
+        }
+    }
+}
