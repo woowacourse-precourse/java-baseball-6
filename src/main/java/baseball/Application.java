6960ec3a19a096b843baa5e53a76e5f4469d0d98
yuseok0215 <elwlahstmxjf@naver.com>
2023-10-19 18:34:27 +0900

    feat: 게임 시작을 알리는 문구를 출력하는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
 public class Application {
+
+    // 시작문구를 출력하는 기능
+    public void printGameStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+
     }
 }
