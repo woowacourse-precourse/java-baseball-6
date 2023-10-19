cf148131f65d20da95c929c7efb45750851e4e19
unknown <coguddlf1000@naver.com>
2023-10-19 22:51:43 +0900

    테스트 통과

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -30,7 +30,7 @@ public class Application {
     }
 
     private static int selectRestartOrEndGame(Computer computer) {
-        // 만약 player가 새로 시작한다고 하면, computer 는 다시 새로운 숫자를 뽑아 내야 한다.
+        // 만약 player 가 새로 시작한다고 하면, computer 는 다시 새로운 숫자를 뽑아 내야 한다.
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         int n = Integer.parseInt(Console.readLine());
