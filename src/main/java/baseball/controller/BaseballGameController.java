a0e2453193ac187fbeab14407907bd422181541a
jun <ggprgrkjh@naver.com>
2023-10-20 00:10:45 +0900

    feat: 시작 문구 출력

---
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -0,0 +1,11 @@
+package baseball.controller;
+
+import baseball.view.OutputView;
+
+public class BaseballGameController {
+
+    public BaseballGameController(){
+        OutputView.printGameInitMessage();
+    }
+
+}
