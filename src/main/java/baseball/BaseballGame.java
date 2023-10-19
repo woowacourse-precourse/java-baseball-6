663a1b2b25cedb0bd8c1ac1c3278a5a4cd1dd922
PgmJun <chltmdwns96@gmail.com>
2023-10-19 18:46:41 +0900

    feat : 숫자 비교 기능 구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,7 +1,9 @@
 package baseball;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import baseball.ui.InputView;
 import baseball.ui.OutputView;
@@ -23,7 +25,23 @@ public class BaseballGame {
 		List<Integer> computer = generateComputerNumber();
 
 		while(doGame) {
+			Map<String, Integer> gameResult = new HashMap<>();
+			gameResult.put("볼", 0);
+			gameResult.put("스트라이크", 0);
+
 			List<Integer> user = inputView.readNumber();
+
+			for(int i  = 0; i < 3; i++) {
+				int userNumber = user.get(i);
+
+				if (computer.contains(userNumber)) {
+					if(computer.get(i) == userNumber) {
+						gameResult.put("스트라이크", gameResult.get("스트라이크")+1);
+						continue;
+					}
+					gameResult.put("볼", gameResult.get("볼")+1);
+				}
+			}
 		}
 
 	}
