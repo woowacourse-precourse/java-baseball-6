5a4a9866eeb6349e7e5046f5e828c4eceaa7c5de
Zeus6768 <akaps6768@gmail.com>
2023-10-19 15:56:05 +0900

    기능 구현: 사용자 수 입력

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -1,6 +1,12 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Input {
 
+    public static String guess;
 
+    public static void guessInput() {
+        String input = Console.readLine();
+    }
 }
