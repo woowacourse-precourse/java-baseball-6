2b3ac1968dffd55e23a54747cad1eeb309754b78
KimDabomi <cdabomi@nate.com>
2023-10-19 20:16:01 +0900

    3스트라이크면 게임 끝 or 재시작 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,21 +8,25 @@ public class Application {
     private static final int MAX_LENGTH = 3;
 
     public static void main(String[] args) {
+        gameProcess();
+    }
+
+    public static void gameProcess() {
         List<Integer> computerNum = getRandomNumber();
         List<Integer> numbersNum = getUserInput();
-
-        checkBall(computerNum, numbersNum);
-        checkStrike(computerNum, numbersNum);
+        checkGame(computerNum, numbersNum);
     }
 
     public static List<Integer> getRandomNumber() {
         List<Integer> computer = new ArrayList<>();
+
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
         }
+
         System.out.println(computer);
         return computer;
     }
@@ -30,8 +34,8 @@ public class Application {
     public static List<Integer> getUserInput() {
         System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
-
         List<Integer> numbers = new ArrayList<>();
+
         for (char ch : input.toCharArray()) {
             numbers.add(Character.getNumericValue(ch));
         }
@@ -40,27 +44,46 @@ public class Application {
         return numbers;
     }
 
-    public static void checkBall(List<Integer> computer, List<Integer> user) {
+    public static void checkGame(List<Integer> computer, List<Integer> user) {
         int ballCount = 0;
+        int strikeCount = 0;
+
         for (int i = 0; i < MAX_LENGTH; i++) {
             if (computer.contains(user.get(i)) && !computer.get(i).equals(user.get(i))) {
                 ballCount++;
             }
-        }
-        System.out.print(ballCount + "볼 ");
-    }
-
-    public static void checkStrike(List<Integer> computer, List<Integer> user) {
-        int strikeCount = 0;
-        for (int i = 0; i < MAX_LENGTH; i++) {
             if (computer.get(i).equals(user.get(i))) {
                 strikeCount++;
             }
         }
-        System.out.print(strikeCount + "스트라이크 ");
 
         if (strikeCount == 3) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n");
+            if (selectExit())
+                gameProcess();
+            return;
         }
+
+        if (ballCount > 0) {
+            System.out.print(ballCount + "볼 ");
+        }
+
+        if (strikeCount > 0) {
+            System.out.println(strikeCount + " 스트라이크 ");
+        }
+
+        if (ballCount == 0 && strikeCount == 0) {
+            System.out.print("낫싱");
+        }
+
+        getUserInput();
+    }
+
+
+
+    public static boolean selectExit() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        int choice = Integer.parseInt(Console.readLine());
+        return choice == 1;
     }
 }
