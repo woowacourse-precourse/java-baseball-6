608c908db17472b44c20a84972bd1b45e26ec7e8
pjaehyun <show6756@gmail.com>
2023-10-19 18:08:33 +0900

    첫번째 커밋(숫자야구 시작)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
     }
 }
