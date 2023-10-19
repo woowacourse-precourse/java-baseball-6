63c1f5c8da49de6b5ab5558bf80e2e1330b045ed
Hugh <lee2317884@gmail.com>
2023-10-19 23:16:42 +0900

    feat: 입력값 길이 조건 예외 처리

--- src/main/java/baseball/domain/UserNumber.java
+++ src/main/java/baseball/domain/UserNumber.java
@@ -1,5 +1,6 @@
 package baseball.domain;
 
+import baseball.constant.Constant;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,6 +13,7 @@ public class UserNumber {
 
 	public UserNumber(String input) {
 		List<Integer> userNumberList = createUserNumber(input);
+		validateUserInputLength(userNumberList);
 		this.userNumber = userNumberList;
 	}
 
@@ -27,6 +29,13 @@ public class UserNumber {
 		} catch (NumberFormatException e) {
 			throw new IllegalArgumentException(
 					UserNumberError.USER_NUMBER_WRONG_INPUT_ERROR_MESSAGE);
+		}
+	}
+
+	private void validateUserInputLength(List<Integer> userNum) {
+		if (userNum.size() != Constant.NUMBER_LENGTH_LIMIT) {
+			throw new IllegalArgumentException(
+					UserNumberError.USER_NUMBER_WRONG_LENGTH_ERROR_MESSAGE);
 		}
 	}
 }
