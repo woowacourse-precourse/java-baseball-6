522ccf326f1c3d58eaa6f2f5bf792161dc5c9072
koomchang <leahpar0401@gmail.com>
2023-10-20 02:34:46 +0900

    feat: 숫자야구게임 컨트롤러 구현을 통해 전체적인 게임 진행을 순서대로 로직 구현

--- src/main/java/baseball/controller/BaseBallController.java
+++ src/main/java/baseball/controller/BaseBallController.java
@@ -1,2 +1,40 @@
-package baseball.controller;public class BaseBallController {
+package baseball.controller;
+
+import static baseball.view.InputView.END_GAME_CODE;
+
+import baseball.model.RandomNumbers;
+import baseball.model.UserInputNumbers;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+public class BaseBallController {
+
+    private static final int GAME_END_STRIKE_COUNTS = 3;
+
+    private final OutputView outputView = new OutputView();
+    private final InputView inputView = new InputView();
+
+    private RandomNumbers randomNumbers;
+    private UserInputNumbers userInputNumbers;
+
+    public void game() {
+        do {
+            play();
+        } while (inputView.askForNewGameOrEnd() != END_GAME_CODE);
+    }
+
+    public void play() {
+        int strikeCount, ballCount;
+        randomNumbers = new RandomNumbers();
+        outputView.printStartGame();
+        outputView.askForNumber();
+        do {
+            String userInput = inputView.inputNumber();
+            userInputNumbers = new UserInputNumbers(userInput);
+            strikeCount = userInputNumbers.countStrikes(randomNumbers);
+            ballCount = userInputNumbers.countBalls(randomNumbers);
+            outputView.printCountForBallsAndStrikes(ballCount, strikeCount);
+        } while (strikeCount != GAME_END_STRIKE_COUNTS);
+        outputView.printEndGame();
+    }
 }
