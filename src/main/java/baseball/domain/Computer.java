decf3cd87b8e9ffbbe7a4322b190391cb64cb1af
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-10-20 02:15:21 +0900

    feat: 상수 정의

---
+++ src/main/java/baseball/domain/Computer.java
@@ -0,0 +1,15 @@
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private static final int MAX_SIZE = 3;
+    private static final int LOWER_RANGE = 1;
+    private static final int UPPER_RANGE = 9;
+
+
+
+}
