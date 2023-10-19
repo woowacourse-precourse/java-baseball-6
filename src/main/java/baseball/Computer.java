2646f0db3a9433f57a69233f0d31dbfd831f9f50
bitesnail <rkwoal216@gmail.com>
2023-10-19 22:20:36 +0900

    feat: implementation of id-6

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -51,4 +51,13 @@ public class Computer {
 
         return result;
     }
+
+    public static boolean isCorrect() {
+        if(strike<3){
+            return false;
+        }
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return true;
+    }
 }
