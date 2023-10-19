81c4e83cfc44dd13a521ebf72886454a43875929
Hyeon0208 <dmsgktn0208@gmail.com>
2023-10-19 21:42:39 +0900

    feat : 숫자에 중복값 존재 시 발생하는 커스텀 예외

---
+++ src/main/java/baseball/exception/NumberDuplicateException.java
@@ -0,0 +1,8 @@
+package baseball.exception;
+
+public class NumberDuplicateException extends RuntimeException {
+
+    public NumberDuplicateException(String message) {
+        super(message);
+    }
+}
