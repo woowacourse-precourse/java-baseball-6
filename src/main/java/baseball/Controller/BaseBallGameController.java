a7e2a7e9dc562e013a5735b4be5d98abdda71676
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-10-19 23:12:02 +0900

    feat: ComputerNumber class 구현

--- src/main/java/baseball/Controller/BaseBallGameController.java
+++ src/main/java/baseball/Controller/BaseBallGameController.java
@@ -1,5 +1,6 @@
 package baseball.Controller;
 
+import baseball.Domain.ComputerNumber;
 import baseball.Domain.UserNumber;
 import baseball.View.InputView;
 import baseball.View.OutputView;
@@ -7,14 +8,20 @@ import baseball.View.OutputView;
 public class BaseBallGameController {
 
     private UserNumber userNumber;
+    private ComputerNumber computerNumber;
 
     public void gameStart(){
         OutputView.startBaseBallGame();
         inputUserNumber();
+        setComputerNumber();
     }
 
     public void inputUserNumber(){ // 유저 정보 넣기
         OutputView.inputUserNumber();
         userNumber = new UserNumber(InputView.inputUserNumber());
+    }
+
+    public void setComputerNumber(){
+        computerNumber = new ComputerNumber();
     }
 }
---
+++ src/main/java/baseball/Domain/ComputerNumber.java
@@ -0,0 +1,34 @@
+package baseball.Domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class ComputerNumber {
+
+    private static int NUMBER_RANGE_START = 1;
+    private static int NUMBER_RANGE_END = 9;
+
+    private String number;
+
+
+    public ComputerNumber(){
+        GeneratedRandomNumber();
+    }
+
+    private void GeneratedRandomNumber(){
+
+        StringBuilder sb = new StringBuilder();
+
+        while(sb.length() < 3){
+            String newNumber = String.valueOf(Randoms.pickNumberInRange(NUMBER_RANGE_START, NUMBER_RANGE_END));
+            if(!sb.toString().contains(newNumber)){
+                sb.append(newNumber);
+            }
+        }
+
+        this.number = sb.toString();
+    }
+
+    public String getNumber(){
+        return number;
+    }
+}
