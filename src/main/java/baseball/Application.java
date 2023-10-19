c084e0e86b53e66ca9225a64c6094c26ff93aa2f
devdynam0507 <wsnam0507@naver.com>
2023-10-19 19:23:03 +0900

    fix: 불필요한 코드 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,17 +3,13 @@ package baseball;
 import baseball.context.BaseballContext;
 import baseball.game.BaseballGame;
 import baseball.game.GameLoop;
-import baseball.game.dto.BaseballMatchResults;
-import baseball.game.transform.BaseballMatchResultsStringTransformer;
 
 import baseball.io.input.Input;
 
-import baseball.game.transform.Transformer;
 import baseball.validation.InputValidator;
 
 public class Application {
     public static void main(String[] args) {
-        Transformer<BaseballMatchResults, String> transformer = new BaseballMatchResultsStringTransformer();
         Input input = new Input("숫자를 입력해주세요 : ", new InputValidator());
         BaseballContext baseballContext = new BaseballContext();
         BaseballGame game = new BaseballGame();
