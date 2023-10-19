949810fbaf281a5838dc0883840152474eeaf01d
pjaehyun <show6756@gmail.com>
2023-10-19 18:21:13 +0900

    사용자가 입력한 값을 List로 변환 및 예외처리 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -15,6 +15,12 @@ public class Application {
 
         while(true) {
             List<Integer> computer = getRandomNumbers();
+
+            // 사용자가 번호 맞추기 시작
+            while(true) {
+                System.out.print("숫자를 입력해주세요 : ");
+                List<Integer> user = getUserNumbers();
+            }
         }
     }
 
@@ -28,6 +34,29 @@ public class Application {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if(!numbers.contains(randomNumber)){
                 numbers.add(randomNumber);
+            }
+        }
+        return numbers;
+    }
+
+    /**
+     * 사용자가 입력한 수를 예외처리와 동시에 List형태로 변환
+     * @return 1~9까지의 숫자 3개가 담긴 리스트
+     */
+    public static List<Integer> getUserNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+
+        String input = Console.readLine();
+        if (input.length() > 3){
+            throw new IllegalArgumentException();
+        }
+
+        for (int i = 0; i < input.length(); i++){
+            char c = input.charAt(i);
+            if (Character.isDigit(c) && 49 <= (int) c && 57 >= (int) c && !numbers.contains(Character.getNumericValue(c))){
+                numbers.add(Character.getNumericValue(c));
+            } else {
+                throw new IllegalArgumentException();
             }
         }
         return numbers;
