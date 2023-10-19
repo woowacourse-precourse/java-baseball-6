3f48483c943dbebf20a426d122837d949edee8d0
siyeonSon <siyeon0209@naver.com>
2023-10-19 22:47:26 +0900

    chore: 게임을 종료한 후 게임을 다시 시작하거나 완전히 종료할 수 있다

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,17 +9,29 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-        Game game = new Game();
-
         while (true) {
-            System.out.print("숫자를 입력해주세요 : ");
-            String input = readLine();
-            List<Integer> number = convertIntegerList(input);
-            validateThreeNums(number);
-            validateUnduplicated(number);
+            Game game = new Game();
 
-            boolean isAnswer = game.play(number);
-            if (isAnswer) break;
+            while (true) {
+                System.out.print("숫자를 입력해주세요 : ");
+                String input = readLine();
+                List<Integer> number = convertIntegerList(input);
+                validateThreeNums(number);
+                validateUnduplicated(number);
+
+                boolean isAnswer = game.play(number);
+                if (isAnswer) break;
+            }
+
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String input = readLine();
+
+            if (input.equals("1")) {
+                continue;
+            }
+            if (input.equals("2")) {
+                break;
+            }
         }
     }
 
