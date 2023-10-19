79bba410399523468df8f2812d68f6caf8d93827
Seotriever <skb0218@naver.com>
2023-10-20 06:45:45 +0900

    Revert "10.19-20_java"
    This reverts commit 4d0164c09d0e6fcfa461d74de4aa449fcb8ef33f.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,7 @@
 package baseball;
 
-import java.util.Random;
-import java.util.Scanner;
-
 public class Application {
-
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
     }
 }
