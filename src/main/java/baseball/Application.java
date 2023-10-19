146a9c966c9740d991b9f9cca831ca14cc9c081e
pjaehyun <show6756@gmail.com>
2023-10-19 18:35:09 +0900

    스트라이크, 볼, 낫싱 출력문 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -23,6 +23,20 @@ public class Application {
 
                 int strike = getStrike(computer, user);
                 int ball = getBall(computer, user);
+
+                if (strike == 3) {
+                    System.out.println("3스트라이크");
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    break;
+                } else if (strike > 0 && ball > 0) {
+                    System.out.println(ball + "볼 " + strike + "스트라이크");
+                } else if (strike > 0 && ball == 0) {
+                    System.out.println(strike + "스트라이크");
+                } else if (strike == 0 && ball > 0){
+                    System.out.println(ball + "볼");
+                } else if (strike == 0 && ball == 0) {
+                    System.out.println("낫싱");
+                }
             }
         }
     }
