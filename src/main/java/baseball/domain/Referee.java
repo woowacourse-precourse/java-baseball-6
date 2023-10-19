35f27e80918178f53c49d4ed59c785f6b8e73f5a
prao <pch5521@gmail.com>
2023-10-19 23:46:26 +0900

    refactor: 심판 기능 수정
    - strike == 0: ball + "볼" 출력
    - ball == 0: strike + "스트라이크" 출력

--- src/main/java/baseball/domain/Referee.java
+++ src/main/java/baseball/domain/Referee.java
@@ -17,6 +17,12 @@ public class Referee {
         if (ball == 0 && strike == 0) {
             return "낫싱";
         }
+        if (ball == 0) {
+            return strike + "스트라이크";
+        }
+        if (strike == 0) {
+            return ball + "볼";
+        }
         return ball + "볼 " + strike + "스트라이크";
     }
 
