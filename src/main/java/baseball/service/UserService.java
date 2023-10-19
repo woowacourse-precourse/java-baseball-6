408d8b28edb510f8b33ae60e7d0096209ba60545
hwan2-99 <thh1233123@gmail.com>
2023-10-19 22:29:15 +0900

    fix: 유저 숫자 입력 기능 수정

--- src/main/java/baseball/service/UserService.java
+++ src/main/java/baseball/service/UserService.java
@@ -3,11 +3,7 @@ package baseball.service;
 import camp.nextstep.edu.missionutils.Console;
 
 public class UserService {
-    public int inputUserNumber() {
-        String inputNumber = Console.readLine();
-        int userNumber = Integer.parseInt(inputNumber);
-
-        System.out.println(userNumber);
-        return userNumber;
+    public String inputUserNumber() {
+        return Console.readLine();
     }
 }
