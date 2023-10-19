3dcc46c034149fd2ddff764e96fb1cb4bf3693e5
siyeonSon <siyeon0209@naver.com>
2023-10-19 22:34:34 +0900

    chore: 입력 받은 숫자 형태를 String -> List<Integer> 로 변경한다

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,8 @@
 package baseball;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
@@ -8,5 +11,17 @@ public class Application {
 
         System.out.print("숫자를 입력해주세요 : ");
         String input = readLine();
+        List<Integer> number = convertIntegerList(input);
+    }
+
+    private static List<Integer> convertIntegerList(String number) {
+        try {
+            return Arrays.stream(number.split(""))
+                    .map(Integer::parseInt)
+                    .toList();
+        }
+        catch (NumberFormatException e) {
+            throw new IllegalArgumentException("문자가 아닌 숫자를 입력해주세요");
+        }
     }
 }
