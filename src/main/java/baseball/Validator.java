1a352e755ad706621fefd39dde7de0f823edae0d
Mr.Kim <joajy98@gmail.com>
2023-10-19 19:03:36 +0900

    feat: 입력받은 데이터가 1이상 9이하의 숫자로만 구성되어 있는가

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -2,9 +2,16 @@ package baseball;
 
 public class Validator {
 
-    public static void checkNumberLength(String number){
+    public static void checkNumberLength(String number) {
         // 입력받은 값의 길이가 3이 아닐 경우
-        if(number.length() != Constant.BALL_SIZE){
+        if (number.length() != Constant.BALL_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void checkNumberType(String number) {
+        // 1부터 9 사이의 "숫자"로 구성되지 않았을 경우
+        if (!number.matches(Constant.NUM_REGEX)) {
             throw new IllegalArgumentException();
         }
     }
