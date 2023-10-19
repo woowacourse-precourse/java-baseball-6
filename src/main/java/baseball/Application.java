736f215277be019e8bd51b03eda7946ee220fc1a
gimdaeyeon <rlaeodus1306@naver.com>
2023-10-19 20:32:04 +0900

    Docs: 필요한 기능목록 초기 작성 및 라이브러리 테스트

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,17 @@
 package baseball;
 
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Random;
 
 public class Application {
     public static void main(String[] args)  {
 
+        System.out.println(Console.readLine());
+
+//        System.out.println(Randoms.pickUniqueNumbersInRange(0,9,3));
 
     }
 }
