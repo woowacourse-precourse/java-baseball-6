237f8bc757e1b8bb755b14c0a75c14391c80ea4c
SeoJiWon <sjiwon4491@gmail.com>
2023-10-19 21:23:45 +0900

    refactor: Computer 랜덤 숫자 생성간 매직넘버 제거, basell mutable로 수정 후 reset() 메커니즘 추가

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -6,23 +6,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
-    private final Baseball baseball;
+    private Baseball baseball;
 
     private Computer(final Baseball baseball) {
         this.baseball = baseball;
     }
 
     public static Computer init() {
+        return new Computer(generateRandomBaseball());
+    }
+
+    public void reset() {
+        this.baseball = generateRandomBaseball();
+    }
+
+    private static Baseball generateRandomBaseball() {
         final List<Integer> balls = new ArrayList<>();
 
-        while (balls.size() < 3) {
-            final int randomNumber = Randoms.pickNumberInRange(1, 9);
+        while (balls.size() < Baseball.TOTAL_COUNT) {
+            final int randomNumber = Randoms.pickNumberInRange(Baseball.MIN_VALUE, Baseball.MAX_VALUE);
             if (!balls.contains(randomNumber)) {
                 balls.add(randomNumber);
             }
         }
 
-        return new Computer(Baseball.of(balls));
+        return Baseball.of(balls);
     }
 
     public List<Integer> getBaseball() {
--- src/test/java/baseball/domain/ComputerTest.java
+++ src/test/java/baseball/domain/ComputerTest.java
@@ -3,16 +3,51 @@ package baseball.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class ComputerTest {
     @Test
-    @DisplayName("Computer는 서로 다른 임의의 3자리 수를 관리하는 Baseball(Unmodifiable)을 가진다")
+    @DisplayName("Computer는 서로 다른 임의의 3자리 수를 관리하는 Baseball(Modifiable)을 가진다")
     void init() {
         // when
         final Computer computer = Computer.init();
 
         // then
-        assertThat(computer.getBaseball()).hasSize(3);
+        assertAll(
+                () -> assertThat(computer.getBaseball()).hasSize(3), // size
+                () -> assertThat(
+                        computer.getBaseball()
+                                .stream()
+                                .distinct()
+                                .toList()
+                ).hasSize(3) // has duplicate
+        );
+    }
+
+    @Test
+    @DisplayName("게임 재시작을 진행할 경우 Computer의 Baseball을 초기화된다")
+    void reset() {
+        // given
+        final Computer computer = Computer.init();
+        final List<Integer> previousBaseball = computer.getBaseball();
+        assertThat(computer.getBaseball()).isEqualTo(previousBaseball); // contains same elements as before
+
+        // when
+        computer.reset();
+
+        // then
+        assertAll(
+                () -> assertThat(computer.getBaseball()).hasSize(3), // size
+                () -> assertThat(
+                        computer.getBaseball()
+                                .stream()
+                                .distinct()
+                                .toList()
+                ).hasSize(3), // has duplicate,
+                () -> assertThat(computer.getBaseball()).isNotEqualTo(previousBaseball) // not contains same elements as before
+        );
     }
 }
