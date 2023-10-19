f6feef32e3b96426eec33555b5f4b97eaa573f0e
YongJOoOo <we072200@naver.com>
2023-10-19 19:25:17 +0900

    feat: 컴퓨터 랜덤 숫자 생성 함수 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,6 +6,20 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+       String randomTest = computerRandomNum();
+       System.out.println(randomTest);
+    }
 
+    //컴퓨터 랜덤 숫자 생성 함수
+    static String computerRandomNum(){
+        //중복 없이 3개의 숫자로 구성
+        String num = "";
+        while(num.length() <3){
+            String tmp = String.valueOf(Randoms.pickNumberInRange(1, 9));
+            if(!num.contains(tmp)){
+                num+=tmp;
+            }
+        }
+        return num;
     }
 }
