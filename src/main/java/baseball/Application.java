9470235ce63c90d400c46c9ef9b197f7287a58df
zzawang <hobin0116@naver.com>
2023-10-19 21:03:07 +0900

    feat: 컴퓨터의 수 생성 기능 구현
    - 1부터 9까지 서로 다른 수로 이루어진 3자리의 수 생성
    - 이미 나왔던 숫자가 뽑히면 다시 랜덤한 숫자 생성
    - 컴퓨터 숫자의 범위와 자릿수는 계속 일관된 값을 사용하기 위해 상수로 선언

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    private final static int MIN_RANDOM_NUMBER = 1;
+    private final static int MAX_RANDOM_NUMBER = 9;
+    private final static int COMPUTER_NUM_SIZE = 3;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    }
+
+    private static List<Integer> createComputerNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < COMPUTER_NUM_SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 }
