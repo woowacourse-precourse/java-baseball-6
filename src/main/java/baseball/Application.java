a855ed5bca7eba77f6c6211fcff15b4b8d6f1fef
yuseok0215 <elwlahstmxjf@naver.com>
2023-10-19 18:36:49 +0900

    feat: 사용자의 숫자입력을 알리는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,6 +7,9 @@ public class Application {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
+    public void printUserInput() {
+        System.out.println("숫자를 입력해주세요 : ");
+    }
 
     public static void main(String[] args) {
 
