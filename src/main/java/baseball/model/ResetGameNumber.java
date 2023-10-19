7d013810dec157e9d47c2212b7bbd916cd3eb3ab
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-10-19 21:55:42 +0900

    Feat : 게임 재시작 숫자 검증 로직 추가 1, 2가 아닌 다른 값을 입력했을 때 예외처리하고 입력받은 숫자에 대한 getter메서드 생성

---
+++ src/main/java/baseball/model/ResetGameNumber.java
@@ -0,0 +1,19 @@
+package baseball.model;
+
+public class ResetGameNumber {
+    private final String resetNumber;
+
+    public String getResetNumber(){
+        return resetNumber;
+    }
+
+    public ResetGameNumber(String number){
+        isCorrectNumber(number);
+        this.resetNumber = number;
+    }
+    public static void isCorrectNumber(String number){
+        if (!number.equals("1") && !number.equals("2")) {
+            throw new IllegalArgumentException("1, 2가 아닌 다른 값을 입력했습니다.");
+        }
+    }
+}
