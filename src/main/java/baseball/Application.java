96e1d89bb9e971a87267f6ef65cdd4cad1050f86
SeoJiWon <sjiwon4491@gmail.com>
2023-10-19 21:27:12 +0900

    feat: Application 게임 실행 메커니즘(GameManager) 적용

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,10 @@
 package baseball;
 
+import baseball.application.GameManager;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        final GameManager gameManager = new GameManager();
+        gameManager.run();
     }
 }
