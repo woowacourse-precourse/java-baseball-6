a84d2a19bc29d321f0b75fc0082ad8aef9c1f4ea
02ggang9 <02ggang9@gmail.com>
2023-10-19 22:39:05 +0900

    feat(OutputView): 게임 종료 문구 및 재시작 문구 출력 기능 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -24,4 +24,9 @@ public class OutputView {
         System.out.println("낫싱");
     }
 
+    public static void printEndGame() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
 }
