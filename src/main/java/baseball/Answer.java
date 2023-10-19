f82b8b480a2b5a5f85e098cf63896449dd0ad76a
freekyr7529 <freekyr7529@gmail.com>
2023-10-19 17:58:27 +0900

    feat: Model 생성
    Integer형의 ArrayList를 가진 정답 객체 Answer 생성

---
+++ src/main/java/baseball/Answer.java
@@ -0,0 +1,14 @@
+package baseball;
+
+import java.util.List;
+
+public class Answer {
+
+    private final List<Integer> answer;
+
+    public Answer(List<Integer> answer) {
+        this.answer = answer;
+    }
+
+    // 검증 로직
+}
