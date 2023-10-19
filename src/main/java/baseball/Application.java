44bbbcf268b1480985370c270c6fddb9c7f62f97
Beconfident01 <hom0726@naver.com>
2023-10-19 22:22:10 +0900

    feat : 랜덤 컴퓨터 수 생성 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Integer> computerNumbers = randomNums();
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println(computerNumbers);
+    }
+
+    public static List<Integer> randomNums(){
+        List<Integer> randomNums = new ArrayList<>();
+        while (randomNums.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!randomNums.contains(randomNumber)) {
+                randomNums.add(randomNumber);
+            }
+        }
+        return randomNums;
     }
 }
