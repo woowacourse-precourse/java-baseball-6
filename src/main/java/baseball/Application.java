41e4833bf65d778bc31e7d5040249e024160dc47
jy016011 <39334789+jy016011@users.noreply.github.com>
2023-10-19 17:51:22 +0900

    Application에서 게임 진행 구현Ver1.0

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,15 +1,35 @@
 package baseball;
 
-import baseball.RandomNumberGenerator;
-
 import java.util.List;
-
+import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        NumberChecker numberChecker = new NumberChecker();
+        ErrorChecker errorChecker = new ErrorChecker();
+
         List<Integer> computer = randomNumberGenerator.generateRandomNumber();
-        
+        boolean keepGoing = true;
+
+        while (keepGoing){
+            System.out.println("숫자를 입력해주세요 : ");
+            int user = Integer.parseInt(Console.readLine());
+            errorChecker.checkError(user);
+            String checkResult = numberChecker.checkNumber(user, computer);
+            if(checkResult.equals("3스트라이크")){
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                int retry = Integer.parseInt(Console.readLine());
+                errorChecker.checkError(retry);
+
+                if (retry == 1)
+                    computer = randomNumberGenerator.generateRandomNumber();
+                else if (retry == 2)
+                    keepGoing = false;
+
+            }
+        }
+
     }
 }
