0f79fe1e5d64fdd9fef11a788f3c7f3c50907a14
mmihye <smilehye910@naver.com>
2023-10-19 22:25:00 +0900

    [FEAT]startGame 메소드 구현

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -5,11 +5,10 @@ public class BaseBallGame {
     private Boolean isPlaying = false;
 
     public void startGame(){
-
+        System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
     public void chooseNum(){
-
     }
 
     public String returnResult(){
