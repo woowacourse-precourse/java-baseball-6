ecc60b53e1084b1298718213210553910bfda7fe
JunTaeINC <juntea3523@naver.com>
2023-10-19 23:07:50 +0900

    refactor : player -> user 변수명 변경(통일)

--- src/test/java/baseball/game/GameManagerTest.java
+++ src/test/java/baseball/game/GameManagerTest.java
@@ -11,50 +11,50 @@ class GameManagerTest {
 
 	@Test
 	void countBall() {
-		List<Integer> player = List.of(1, 2, 3);
+		List<Integer> user = List.of(1, 2, 3);
 		List<Integer> answer = List.of(7, 8, 2);
 
-		int balls = gameManager.countBall(player, answer);
+		int balls = gameManager.countBall(user, answer);
 
 		assertThat(balls).isEqualTo(1);
 	}
 
 	@Test
 	void countStrike() {
-		List<Integer> player = List.of(1, 2, 3);
+		List<Integer> user = List.of(1, 2, 3);
 		List<Integer> answer = List.of(1, 2, 4);
 
-		int balls = gameManager.countStrike(player, answer);
+		int balls = gameManager.countStrike(user, answer);
 
 		assertThat(balls).isEqualTo(2);
 	}
 
 	@Test
 	void giveHint_TowBalls() {
-		List<Integer> player = List.of(1, 2, 3);
+		List<Integer> user = List.of(1, 2, 3);
 		List<Integer> answer = List.of(3, 4, 1);
 
-		String hint = gameManager.giveHint(player, answer);
+		String hint = gameManager.giveHint(user, answer);
 
 		assertThat(hint).isEqualTo("2볼");
 	}
 
 	@Test
 	void giveHint_TowStrikes() {
-		List<Integer> player = List.of(1, 2, 3);
+		List<Integer> user = List.of(1, 2, 3);
 		List<Integer> answer = List.of(1, 2, 4);
 
-		String hint = gameManager.giveHint(player, answer);
+		String hint = gameManager.giveHint(user, answer);
 
 		assertThat(hint).isEqualTo("2스트라이크");
 	}
 
 	@Test
 	void giveHint_OneBall_OneStrike() {
-		List<Integer> player = List.of(1, 2, 3);
+		List<Integer> user = List.of(1, 2, 3);
 		List<Integer> answer = List.of(1, 7, 2);
 
-		String hint = gameManager.giveHint(player, answer);
+		String hint = gameManager.giveHint(user, answer);
 
 		assertThat(hint).isEqualTo("1볼 1스트라이크");
 	}
