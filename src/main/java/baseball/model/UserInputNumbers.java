ecb1a7e816676c1247cefce0e2b59df7e4b245f7
koomchang <leahpar0401@gmail.com>
2023-10-20 00:40:45 +0900

    feat: 유저가 입력한 정답을 UserInputNumbers 클래스로 구현

--- src/main/java/baseball/model/UserInputNumbers.java
+++ src/main/java/baseball/model/UserInputNumbers.java
@@ -1,2 +1,46 @@
-package baseball.model;public class UserInputNumbers {
+package baseball.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserInputNumbers {
+
+    private final int INPUT_NUMBER_LENGTH = 3;
+
+    private final List<Number> userInputNumbers;
+    private int strikeCount = 0;
+    private int ballCount = 0;
+
+    public UserInputNumbers(String userInput) {
+        this.userInputNumbers = userInputToNumbers(userInput);
+    }
+
+    private List<Number> userInputToNumbers(String userInput) {
+        List<Number> inputNumbers = new ArrayList<>();
+        for (char numberCharacter : userInput.toCharArray()) {
+            inputNumbers.add(new Number(Character.getNumericValue(numberCharacter)));
+        }
+        return inputNumbers;
+    }
+
+    public int countStrikes(RandomNumbers randomNumbers) {
+        List<Number> randomNumberList = randomNumbers.getRandomNumbers();
+        for (int i = 0; i < INPUT_NUMBER_LENGTH; i++) {
+            if (randomNumberList.get(i) == userInputNumbers.get(i)) {
+                strikeCount += 1;
+            }
+        }
+        return strikeCount;
+    }
+
+    public int countBalls(RandomNumbers randomNumbers) {
+        List<Number> randomNumberList = randomNumbers.getRandomNumbers();
+        for (int i = 0; i < INPUT_NUMBER_LENGTH; i++) {
+            Number userInputNumber = userInputNumbers.get(i);
+            if (randomNumberList.contains(userInputNumber) && !randomNumberList.get(i).equals(userInputNumber)) {
+                ballCount += 1;
+            }
+        }
+        return ballCount;
+    }
 }
