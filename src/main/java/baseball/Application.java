63e46e8da98151137f07856774b7c0d6ccdbb7c3
wonchanjoo <wcj0911@naver.com>
2023-10-20 00:46:06 +0900

    feat: 숫자 입력 받고 예외 처리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,47 @@
 package baseball;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         System.out.println("숫자 야구 게임을 시작합니다.");
 
         while (true) {
             System.out.print("숫자를 입력해주세요 : ");
+
+            String input = br.readLine();
+
+            if (!isValidNumber(input)) {
+                try {
+                    inputError();
+                } catch (IllegalArgumentException e) {
+                    return;
+                }
+            }
         }
+    }
+
+    private static boolean isValidNumber(String s) {
+        if (s.length() != 3) {
+            return false;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            int j = s.charAt(i) - '0';
+
+            if (!(j >= 1 && j <= 9)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static void inputError() {
+        throw new IllegalArgumentException();
     }
 }
