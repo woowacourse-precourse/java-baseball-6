6ac80195f909baccc39b2441fa52a6f11d992091
WeepingHeron <kimtaehoon_98@naver.com>
2023-10-19 21:19:06 +0900

    feat: combined with ParseNum, Judge, and GivenNum

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,40 @@
 package baseball;
 
+import baseball.givennum.GivenNum;
+import baseball.judge.Judge;
+
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GivenNum givenNum = new GivenNum();
+        Judge judge = new Judge(givenNum);
+
+        Scanner sc = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            System.out.println("숫자 야구 게임을 시작합니다");
+            int[] userGuess;
+            String result = "";
+
+            while (!result.equals("3스트라이크")) {
+                System.out.print("숫자를 입력해주세요 : ");
+                input = sc.nextLine();
+                userGuess = ParseNum.parseInput(input);
+                result = judge.strikesAndBalls(userGuess);
+                System.out.println(result);
+            }
+
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+            input = sc.nextLine();
+
+            if (!input.equals("1")) {
+                break; // 게임 종료
+            }
+        }
+
+        sc.close();
     }
 }
