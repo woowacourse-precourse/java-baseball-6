2fb4e5c9dca9ff46aeb4b98a96dedbeee80a04a3
zjacom <eephus625@icloud.com>
2023-10-19 23:04:45 +0900

    실행 시 터미널에 출력됐으면 하는 내용 정리

---
+++ src/main/java/baseball/BaseBallGameUI.java
@@ -0,0 +1,14 @@
+package baseball;
+
+public class BaseBallGameUI {
+
+    // 숫자 야구 게임을 시작합니다.
+    // 숫자를 입력해주세요 : readLine()
+    // 결과 출력
+        // 오답이면 오답 결과 출력
+        // 정답이면 다시 시작할지 출력
+            // 다시 시작하면
+                // 숫자를 입력해주세요 : readLine()
+            // 완전히 종료!
+
+}
