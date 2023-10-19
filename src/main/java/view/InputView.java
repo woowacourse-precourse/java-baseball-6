58528cd15add1f2ed2578a3ce17d2aaf4f947448
suhyun <qkrtngus116@naver,com>
2023-10-19 16:42:29 +0900

    feat: 숫자 입력 받기 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,7 @@
 package view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * packageName    : view
  * fileName       : InputView
@@ -12,4 +14,11 @@ package view;
  * 2023-10-19        qkrtn_ulqpbq2       최초 생성
  */
 public class InputView {
+
+    private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+
+    public String inputNumber(){
+        System.out.print(INPUT_NUMBER_MESSAGE);
+        return Console.readLine();
+    }
 }
