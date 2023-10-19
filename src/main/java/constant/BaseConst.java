055bbb17dad744e5f156e6f03994f3e032425947
김승우 <tmd8633@naver.com>
2023-10-19 21:48:41 +0900

    Feat: 상수 추가

--- src/main/java/constant/BaseConst.java
+++ src/main/java/constant/BaseConst.java
@@ -2,7 +2,10 @@ package constant;
 
 public abstract class BaseConst {
 
-    public static final int RESTART = 1;
-    public static final int EXIT = 2;
-    public static final int COMPLETE = 3;
+    public static final int RESTART = 1; // 재시작 여부
+    public static final int EXIT = 2; // 재시작 여부
+    public static final int COMPLETE = 3; // 스트라이크가 3개일 때 사용
+
+    public static final int STRIKE = 0;
+    public static final int BALL = 1;
 }
