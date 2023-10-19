4a021ca70be6ef37a6e6773124c46f8ea190144f
LeeJiWon <dlwldnjs1009@gmail.com>
2023-10-19 20:57:02 +0900

    feat: 게임 플레이 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,8 +8,9 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        boolean gameRunning = true;
 
+        System.out.println("숫자 야구 게임을 시작합니다.");
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -18,30 +19,60 @@ public class Application {
             }
         }
 
-        System.out.println("숫자를 입력해주세요 : ");
-        List<Integer> player = new ArrayList<>();
-        String playerInput = Console.readLine();
+        while (gameRunning) {
+            System.out.println("숫자를 입력해주세요 : ");
+            List<Integer> player = new ArrayList<>();
+            String playerInput = Console.readLine();
 
-        if (playerInput.length() != 3) {
-            throw new IllegalArgumentException("입력값은 3자리의 정수여야 합니다.");
-        }
-
-        for (int i = 0; i < playerInput.length(); i++) {
-            char c = playerInput.charAt(i);
-            int number;
-
-
-            try {
-                number = Integer.parseInt(String.valueOf(c));
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("입력값은 정수여야 합니다.");
+            if (playerInput.length() != 3) {
+                throw new IllegalArgumentException("입력값은 3자리의 정수여야 합니다.");
             }
 
-            if (number < 1 || number > 9) {
-                throw new IllegalArgumentException("입력값은 1부터 9까지 중 하나의 정수여야 합니다.");
+            for (int i = 0; i < playerInput.length(); i++) {
+                char c = playerInput.charAt(i);
+                int number;
+
+
+                try {
+                    number = Integer.parseInt(String.valueOf(c));
+                } catch (NumberFormatException e) {
+                    throw new IllegalArgumentException("입력값은 정수여야 합니다.");
+                }
+
+                if (number < 1 || number > 9) {
+                    throw new IllegalArgumentException("입력값은 1부터 9까지 중 하나의 정수여야 합니다.");
+                }
+
+                player.add(number);
             }
 
-            player.add(number);
+            int strike = 0;
+            int ball = 0;
+
+            for (int j = 0; j < 3; j++) {
+                if (player.get(j).equals(computer.get(j))) {
+                    strike++;
+                } else if (computer.contains(player.get(j))) {
+                    ball++;
+                }
+            }
+
+            if (ball == 0 && strike == 0) {
+                System.out.println("낫싱");
+            } else {
+                if (ball > 0) {
+                    System.out.print(ball + "볼 ");
+                }
+                if (strike > 0) {
+                    System.out.print(strike + "스트라이크");
+                }
+                System.out.println();
+            }
+
+            if (strike == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                gameRunning = false;
+            }
         }
     }
 }
