ae4bd202ef405a485d9f9d1a814f52faba5b135e
Taehyoun Kim <kth9903@gmail.com>
2023-10-20 00:58:23 +0900

    feat(random): random number generation for computer

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,14 +1,28 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다");
-
+        //random number generation for computer
+        List<Integer> computerNumber = generateComputerNumber();
         while(true){ //running game
 
         }
 
+    }
+
+    private static List<Integer> generateComputerNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while(computer.size()<3){
+            int randomNumber= Randoms.pickNumberInRange(1,9);
+            if(!computer.contains(randomNumber))
+                computer.add(randomNumber);
+        }
+        return computer;
     }
 }
