55934e706c696bb962df26e1950ebe76036e35b2
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-10-20 02:20:39 +0900

    feat: 출력에 대한 메소드 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -9,6 +9,29 @@ public class OutputView {
     private static final String FINISH_GAME = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String RETRY = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
-
+    public void startGame() {
+        System.out.println(START_GAME);
+    }
+    public void inputNumber() {
+        System.out.print(INPUT_NUMBER);
+    }
+    public void strike(int strike, StringBuilder sb){
+        sb.append(strike + STRIKE);
+    }
+    public void ball(int ball, StringBuilder sb) {
+        sb.append(ball + BALL);
+    }
+    public void printBuilder(StringBuilder sb){
+        System.out.println(sb);
+    }
+    public void nothing(StringBuilder sb) {
+        sb.append(NOTHING);
+    }
+    public void finishGame() {
+        System.out.println(FINISH_GAME);
+    }
+    public void retry() {
+        System.out.println(RETRY);
+    }
 
 }
