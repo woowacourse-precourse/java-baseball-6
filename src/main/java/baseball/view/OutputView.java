1d0807eea6afff1703f860e86acff39b53f692ff
mummhy0811 <mummhy0811@inu.ac.kr>
2023-10-19 18:36:34 +0900

    Feat: input/output view 작성

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,23 @@
+package baseball.view;
+
+public class OutputView {
+
+    public static void printStart(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void printBall(int ball){
+        System.out.println(ball + "볼");
+    }
+
+    public static void printStrike(int strike){ System.out.println(strike + "스트라이크");}
+
+    public static void printNothing(){
+        System.out.println("낫싱");
+    }
+
+    public static void printExit(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+}
