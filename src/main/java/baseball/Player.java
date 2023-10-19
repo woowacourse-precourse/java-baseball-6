f1f16997f463e355bd0a626fecdcaec4d5cc303b
anneu <b.imnuj@gmail.com>
2023-10-19 23:08:05 +0900

    feat: 플레이어 수에 같은 숫자가 있을 경우 예외 처리

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -7,17 +7,29 @@ public class Player {
 
     private static final int NUMBER_SIZE = 3;
     private static final String NUMBER_SIZE_EXCEPTION = "%d자리 수만 가능합니다.";
+    private static final String NUMBER_UNIQUE_EXCEPTION = "서로 다른 숫자만 가능합니다.";
 
     private List<Integer> numbers;
 
     public Player(String number) {
         validateNumberSize(number);
+        validateUniqueNumber(number);
         this.numbers = convert(number);
     }
 
     private void validateNumberSize(String number) {
         if (number.length() != NUMBER_SIZE) {
             throw new IllegalArgumentException(String.format(NUMBER_SIZE_EXCEPTION, NUMBER_SIZE));
+        }
+    }
+
+    private void validateUniqueNumber(String number) {
+        long count = Arrays.stream(number.split(""))
+                .distinct()
+                .count();
+
+        if (number.length() != count) {
+            throw new IllegalArgumentException(NUMBER_UNIQUE_EXCEPTION);
         }
     }
 
