f9ae43a720eb5c68c1f16c54dfb3d8659a9b47e4
SeoJiWon <sjiwon4491@gmail.com>
2023-10-19 20:37:07 +0900

    feat: InputProcessor Baseball 숫자 입력 흐름 추가

--- src/main/java/baseball/io/InputProcessor.java
+++ src/main/java/baseball/io/InputProcessor.java
@@ -1,4 +1,26 @@
 package baseball.io;
 
+import baseball.io.validator.BaseballInputValidator;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class InputProcessor {
+    private static final BaseballInputValidator BASEBALL_INPUT_VALIDATOR = new BaseballInputValidator();
+
+    public static List<Integer> readUserBaseballs() {
+        System.out.print("숫자를 입력해주세요 : ");
+
+        final String userInput = Console.readLine();
+        BASEBALL_INPUT_VALIDATOR.validateInputHasSpace(userInput);
+
+        return convertBaseballs(userInput);
+    }
+
+    private static List<Integer> convertBaseballs(final String userInput) {
+        return Arrays.stream(userInput.split(""))
+                .map(Integer::parseInt)
+                .toList();
+    }
 }
