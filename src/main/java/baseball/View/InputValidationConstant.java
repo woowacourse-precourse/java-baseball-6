da1ddeb271368887018551767ec8d13bcb8042a2
twkwon0417 <kwontony123@konkuk.ac.kr>
2023-10-19 22:12:29 +0900

    feat: 유저의 숫자 값을 검증하는 기능 구현 + 입력 받는 메서드 수정

---
+++ src/main/java/baseball/View/InputValidationConstant.java
@@ -0,0 +1,10 @@
+package baseball.View;
+
+public final class InputValidationConstant {
+    static final int MIN_COMBINATION = 111;
+    static final int MAX_COMBINATION = 999;
+
+    private InputValidationConstant() {
+
+    }
+}
--- src/main/java/baseball/View/InputView.java
+++ src/main/java/baseball/View/InputView.java
@@ -4,12 +4,21 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public static int inputUserGuess() {
-        String userInput = Console.readLine();
-        if(!validateUserGuess(userInput)) {
-            throw new IllegalArgumentException("유효하지 않은 입력입니다.");
+    public String inputUserGuess() {
+        String userInput = Console.readLine().trim();
+        try {
+            validateUserGuess(userInput);
+        } catch (IllegalArgumentException e) {
+            System.out.println("유효하지 않은 입력입니다.");
         }
-        return Integer.parseInt(userInput);
+
+        return userInput;
     }
 
+    private void validateUserGuess (String userInput) throws IllegalArgumentException {
+        int intUserInput = Integer.parseInt(userInput);
+        if(intUserInput < InputValidationConstant.MIN_COMBINATION || intUserInput > InputValidationConstant.MAX_COMBINATION) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
