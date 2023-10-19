2776639068ef472d1d8668cdeb217027dd6d53fb
jisu-om <lifyji@naver.com>
2023-10-19 22:27:28 +0900

    test: 테스트코드 작성

---
+++ src/test/java/baseball/BaseballGameTest.java
@@ -0,0 +1,112 @@
+package baseball;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BaseballGameTest {
+
+    @DisplayName("1~9의 서로 다른 숫자 3개를 생성한다.")
+    @Test
+    void generateComputerNumber(){
+
+    }
+
+
+    @DisplayName("입력 받은 숫자가 1~9의 서로 다른 숫자 3개인 경우 정상 처리 된다.")
+    @Test
+    void validatePlayerNumber_o(){
+
+    }
+
+    @DisplayName("입력 받은 값에 0이 포함된 경우 예외가 발생한다.")
+    @Test
+    void validatePlayerNumber_ex1(){
+
+    }
+
+    @DisplayName("입력 받은 값이 1~9의 서로 다른 숫자 3개가 아니면 예외가 발생한다.")
+    @Test
+    void validatePlayerNumber_ex2(){
+
+    }
+
+    //입력 받은 숫자와 컴퓨터 숫자 비교
+    @DisplayName("입력받은 숫자와 컴퓨터 숫자 비교 - 낫싱")
+    @Test
+    void generateResult1() {
+
+    }
+
+    @DisplayName("입력받은 숫자와 컴퓨터 숫자 비교 - 1볼")
+    @Test
+    void generateResult2() {
+
+    }
+
+    @DisplayName("입력받은 숫자와 컴퓨터 숫자 비교 - 2볼")
+    @Test
+    void generateResult3() {
+
+    }
+
+    @DisplayName("입력받은 숫자와 컴퓨터 숫자 비교 - 3볼")
+    @Test
+    void generateResult4() {
+
+    }
+
+    @DisplayName("입력받은 숫자와 컴퓨터 숫자 비교 - 1스트라이크")
+    @Test
+    void generateResult5() {
+
+    }
+
+    @DisplayName("입력받은 숫자와 컴퓨터 숫자 비교 - 2스트라이크")
+    @Test
+    void generateResult6() {
+
+    }
+
+    @DisplayName("입력받은 숫자와 컴퓨터 숫자 비교 - 3스트라이크")
+    @Test
+    void generateResult7() {
+
+    }
+
+    @DisplayName("입력받은 숫자와 컴퓨터 숫자 비교 - 1볼 1스트라이크")
+    @Test
+    void generateResult8() {
+
+    }
+
+    @DisplayName("입력받은 숫자와 컴퓨터 숫자 비교 - 2볼 1스트라이크")
+    @Test
+    void generateResult9() {
+
+    }
+
+    @DisplayName("결과 문자열 생성")
+    @Test
+    void printResult() {
+
+    }
+
+//    @DisplayName("3스트라이크인 경우, 게임 종료 문구 출력")
+//    @Test
+//    void printWinningResult() {
+//
+//    }
+//
+//    @DisplayName("게임 종료(3스트라이크)가 아닌 경우, 사용자로부터 숫자 입력 받기 진행")
+//    @Test
+//    void printNextRoundResult() {
+//
+//    }
+
+    @DisplayName("게임 종료 문구에 대한 입력값 검증 - 1 이나 2 가 아니면 예외가 발생한다.")
+    @Test
+    void validateInput_ex() {
+
+    }
+
+}
