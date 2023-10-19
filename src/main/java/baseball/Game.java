d60d867955c996ae4f97579face82aa25397e3a1
leeyeongjae <xingxing2001@cau.ac.kr>
2023-10-19 20:21:04 +0900

    1. 컴퓨터와 플레이어의 숫자 비교 2. 비교 결과에 따라 메세지 출력

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -13,9 +13,26 @@ public class Game {
         while(true){ //종료 조건 : 3스트라이크
             System.out.print("숫자를 입력해주세요: ");
             this.user.setGuessNumber(); //플레이어의 숫자 입력 받기
+            int[] strikeAndBall = this.checkNumber(); //strikeAndBall[0]: 스트라이크, strikeAndBall[1]: 볼
+            if(strikeAndBall[0]==3){ //3스트라이크: 반복문 종료 조건
+                //종료 멘트 출력
+                System.out.println("3스트라이크");
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            }
+            else if(strikeAndBall[0]+strikeAndBall[1]!=0){ //스트라이크와 볼이 둘다 0이 아닌 경우
+                if(strikeAndBall[1]!=0){//볼이 0이 아닐 때 개수 출력
+                    System.out.printf("%d볼 ", strikeAndBall[1]);
+                }
+                if(strikeAndBall[0]!=0){//스트라이크가 0이 아닐 시 개수 출력
+                    System.out.printf("%d스트라이크",strikeAndBall[0]);
+                }
+                System.out.println(); //줄바꿈
+            } else{ //둘 다 0인 경우
+                System.out.println("낫싱");
+            }
         }
     }
-
 
     private int[] checkNumber(){ //컴퓨터의 숫자와 플레이어가 입력한 숫자를 비교하여 스트라이크와 볼의 개수를 리턴하는 메소드
          int [] strikeAndBall = {0,0}; // 0번째 인덱스: 스트라이크, 1번째 인덱스: 볼
