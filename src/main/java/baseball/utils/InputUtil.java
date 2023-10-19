60f481bc2ab75bbc59d8971d556c100718ca3ed7
02ggang9 <02ggang9@gmail.com>
2023-10-20 04:21:08 +0900

    refactor(InputUtil): NumberBaseball Entity의 입력 값 검증 역할을 InputView가 하도록 수정

--- src/main/java/baseball/utils/InputUtil.java
+++ src/main/java/baseball/utils/InputUtil.java
@@ -12,7 +12,36 @@ public class InputUtil {
 
     public static String requireGuessNumber() {
         System.out.println(REQUIRE_NUMBER_MESSAGE.getMessage());
+        String guessNumber = Console.readLine();
+        isGuessNumberValid(guessNumber);
         return Console.readLine();
     }
+
+    private static void isGuessNumberValid(String guessNumber) {
+        if (isGuessNumberDigit(guessNumber) && isGuessNumberLengthThree(guessNumber) && isGuessNumberDuplicate(guessNumber)) {
+            return;
+        }
+
+        throw new IllegalArgumentException();
+    }
+
+    private static boolean isGuessNumberDuplicate(String guessNumber) {
+        char firstNumber = guessNumber.charAt(0);
+        char secondNumber = guessNumber.charAt(1);
+        char thirdNumber = guessNumber.charAt(2);
+
+        return firstNumber != secondNumber && secondNumber != thirdNumber && firstNumber != thirdNumber;
+    }
+
+    private static boolean isGuessNumberLengthThree(String guessNumber) {
+        return guessNumber.length() == 3;
+    }
+
+
+    private static boolean isGuessNumberDigit(String value) {
+        return value.chars()
+                .allMatch(Character::isDigit);
+    }
+
 
 }
