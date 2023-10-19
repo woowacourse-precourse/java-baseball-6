e6a637c732956eec0da464f19ac448afb607ee28
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2023-10-19 21:15:49 +0900

    feat: 컴퓨터 숫자 생성 기능

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,33 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private final List<Integer> computer;
+
+    public Computer() {
+        this.computer = generateComputerNumber();
+    }
+
+    private List<Integer> generateComputerNumber() {
+        List<Integer> randomNumbers = new ArrayList<>();
+        while (randomNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            addNumber(randomNumbers, randomNumber);
+        }
+        return randomNumbers;
+    }
+
+    private void addNumber(List<Integer> numbers, int number) {
+        if (!validateRedundantNumber(numbers, number)) {
+            numbers.add(number);
+        }
+    }
+
+    private boolean validateRedundantNumber(List<Integer> numbers, int number) {
+        return numbers.contains(number);
+    }
+}
