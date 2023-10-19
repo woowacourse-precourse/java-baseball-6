3bf87ee5150b564c5781e441000ccc3c194a3445
GaBaljaintheroom <junsu1222@naver.com>
2023-10-19 23:55:46 +0900

    refactor: validateNumeric Not추가

--- src/main/java/baseball/InputValidator.java
+++ src/main/java/baseball/InputValidator.java
@@ -4,7 +4,7 @@ public class InputValidator {
     private static final String NUMERIC_REGEX = "^[1-9]*$";
 
     public void validateNumeric(String number) {
-        if (number.matches(NUMERIC_REGEX)) {
+        if (!number.matches(NUMERIC_REGEX)) {
             throw new IllegalArgumentException(ErrorMessage.INPUT_NUMERIC_ERROR.getMessage());
         }
     }
