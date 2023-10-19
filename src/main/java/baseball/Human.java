b7b4824ce349087136ba8bfdd2ca045c744207ea
choheejin <hjcho9510@gmail.com>
2023-10-19 16:28:43 +0900

    feat: validate Human's numbers

--- src/main/java/baseball/Human.java
+++ src/main/java/baseball/Human.java
@@ -3,7 +3,9 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Human {
     private List<Integer> numbers;
@@ -11,6 +13,18 @@ public class Human {
     public void getNumbers() {
         System.out.print("숫자를 입력해주세요 : ");
         String numString = Console.readLine();
-        this.numbers = Arrays.stream(numString.split("")).map(Integer::parseInt).toList();
+        List<Integer> numbers = Arrays.stream(numString.split("")).map(Integer::parseInt).toList();
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<Integer>(numbers);
+        if(set.contains(0)) {
+            throw new IllegalArgumentException();
+        }
+        if(set.size() != 3) {
+            throw new IllegalArgumentException();
+        }
     }
 }
