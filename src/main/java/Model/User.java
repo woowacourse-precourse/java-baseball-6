910181446b96e3f3fc72d477f39374c3a4871590
김영준 <rladudwns164@gmail.com>
2023-10-19 21:27:13 +0900

    feat : 사용자 3개의 숫자 입력 기능 구현

---
+++ src/main/java/Model/User.java
@@ -0,0 +1,12 @@
+package Model;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class User {
+
+    private String userNumber;
+    public User(){
+        this.userNumber = Console.readLine();
+    }
+
+}
