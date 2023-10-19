372f53412d3ce6605f190ad0cdd773a370916d04
donghyunkim <ghffu405@gmail.com>
2023-10-19 19:25:53 +0900

    feat: 결과값 출력 메소드 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,6 +1,23 @@
 package baseball.view;
 
+import baseball.constant.NumberConstant;
+
 public class OutputView {
+
+    public void printResult(int ball, int strike){
+        if(ball != 0 && strike != 0){
+            printBallNStrike(ball, strike);
+        }
+        if(ball == 0 && strike != 0){
+            printStrike(strike);
+        }
+        if(ball != 0 && strike == 0){
+           printBall(ball);
+        }
+        if(ball == 0 && strike == 0){
+            printNothing();
+        }
+    }
 
     public void printBallNStrike(int ball, int strike){
         System.out.println(ball + "볼 " + strike + "스트라이크");
@@ -13,7 +30,7 @@ public class OutputView {
     public void printStrike(int strike){
         System.out.println(strike + "스트라이크");
 
-        if(strike==3){
+        if(strike== NumberConstant.SUCCESS_NUMBER){
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         }
     }
