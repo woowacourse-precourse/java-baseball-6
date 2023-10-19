32b2979b935eec27d40730aa3449c962cdce57a1
WonyuChoi <phillip07290@naver.com>
2023-10-20 00:38:06 +0900

    refactor: 이미 있는 상수로 대체

--- src/main/java/baseball/model/GuessResult.java
+++ src/main/java/baseball/model/GuessResult.java
@@ -1,9 +1,10 @@
 package baseball.model;
 
+import static baseball.Constants.NUMBER_COUNTS;
+
 public class GuessResult {
 
     private static final int INITIAL_ZERO_COUNT = 0;
-    private static final int END_STRIKE_COUNT = 3;
     private static final String NOTHING_RESULT = "낫싱";
     private static final String BALL_RESULT = "볼";
     private static final String STRIKE_RESULT = "스트라이크";
@@ -40,6 +41,6 @@ public class GuessResult {
     }
 
     public boolean isGameEnd() {
-        return strikeCount == END_STRIKE_COUNT;
+        return strikeCount == NUMBER_COUNTS;
     }
 }
