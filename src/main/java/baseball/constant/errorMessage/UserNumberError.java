7af7f5f2a80a6bbedd06fe703faa01842baac6b6
Hugh <lee2317884@gmail.com>
2023-10-19 23:01:31 +0900

    feat: 입력값 숫자인지 확인 에러 메시지 상수 처리

---
+++ src/main/java/baseball/constant/errorMessage/UserNumberError.java
@@ -0,0 +1,7 @@
+package baseball.constant.errorMessage;
+
+public class UserNumberError {
+
+	public static final String USER_NUMBER_WRONG_INPUT_ERROR_MESSAGE = "[ERROR] 숫자가 아닌 값은 입력할 수 없습니다. 게임을 종료합니다.";
+
+}
