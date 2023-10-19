9e137e62beba190f397bccbb1a2483b7ff9368c2
Minjoo <m0522inju@gmail.com>
2023-10-20 02:45:37 +0900

    refactor : 각 기능 별로 나누어 함수화

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,8 +3,6 @@ import java.util.Set;
 import java.util.HashSet;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
@@ -12,65 +10,93 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-        int isContinue = 1;
-        List<Integer> computer = new ArrayList<>();
+        while(true) {
 
-        while(isContinue == 1) {
+            playGame();
+
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String input = Console.readLine();
+            // TODO : 1, 2 이외의 숫자를 입력 시, 예외 처리
+            if ("2".equals(input)) {
+                break;
+            }
+        }
+    }
+
+    private static void playGame() {
+        List<Integer> computer = generateRandomNumber();
+
+        while (true) {
             System.out.print("숫자를 입력해주세요 : ");
             String input = Console.readLine();
 
-            // 사용자 입력 예외처리
-            String inputPattern = "^[1-9]{3}$";
-            Matcher matcher = Pattern.compile(inputPattern).matcher(input);
-            if (!matcher.matches()) {
+            if (!isValidInput(input)) {
                 throw new IllegalArgumentException();
             }
-            Set<Character> set = new HashSet<>();
-            for (char c : input.toCharArray()) {
-               if (!set.add(c)) {
-                   throw new IllegalArgumentException();
-               }
-            }
 
-            // 랜덤 숫자 생성 - 한 게임에 한 번만 생성!
-            while (computer.size() < 3) {
-                int randomNumber = Randoms.pickNumberInRange(1, 9);
-                if (!computer.contains(randomNumber)) {
-                    computer.add(randomNumber);
-                }
-            }
+            int[] result = getResult(computer, input);
 
-            int strike = 0;
-            int ball = 0;
-            for (int i=0; i < input.length(); i++) {
-                char numChar = input.charAt(i);
-                int num = Character.getNumericValue(numChar);
-                if (num == computer.get(i)) {
-                    strike++;
-                } else if (computer.contains(num)) {
-                    ball ++;
-                }
-            }
+            printResult(result);
 
-            if (strike == 3) {
-                System.out.println("3스트라이크");
+            if (result[0] == 3) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                // TODO : 변수명 바꾸기
-                String input2 = Console.readLine();
-                isContinue = Integer.parseInt(input2);
-                computer.clear();
-            } else if (strike == 0 && ball == 0) {
-                System.out.println("낫싱");
-            } else {
-                if (strike > 0 && ball > 0) {
-                    System.out.printf("%d볼 %d스트라이크\n", ball, strike);
-                } else if (ball != 0) {
-                    System.out.printf("%d볼\n", ball);
-                } else {
-                    System.out.printf("%d스트라이크\n", strike);
-                }
+                break;
             }
+
+        }
+    }
+
+    private static List<Integer> generateRandomNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
+    private static boolean isValidInput(String input) {
+        return input.matches("^[1-9]{3}$") && !checkDuplicate(input);
+    }
+
+    private static boolean checkDuplicate(String input) {
+        Set<Character> set = new HashSet<>();
+        for (char c : input.toCharArray()) {
+            if (!set.add(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static int[] getResult(List<Integer> computer, String input) {
+        int[] result = new int[2]; // [0] : strike, [1] : ball
+        for (int i=0; i < input.length(); i++) {
+            char numChar = input.charAt(i);
+            int num = Character.getNumericValue(numChar);
+            if (num == computer.get(i)) {
+                result[0]++;
+            } else if (computer.contains(num)) {
+                result[1]++;
+            }
+        }
+        return result;
+    }
+
+    private static void printResult(int[] result) {
+        if (result[0] == 0 && result[1] == 0) {
+            System.out.println("낫싱");
+        } else {
+            String resultMessage = new String("");
+            if (result[1] > 0) {
+                resultMessage = result[1] + "볼";
+            }
+            if (result[0] > 0) {
+                resultMessage += (resultMessage.isEmpty() ? "" : " ") + result[0] + "스트라이크";
+            }
+            System.out.println(resultMessage);
         }
     }
 }
