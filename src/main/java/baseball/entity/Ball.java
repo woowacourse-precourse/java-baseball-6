8916139bf582f62c2c87d1a111cf5b3a36a77316
le2sky <leehaneul990623@gmail.com>
2023-10-19 21:05:57 +0900

    feat : 공 위치 유효성 검사 기능 구현

--- src/main/java/baseball/entity/Ball.java
+++ src/main/java/baseball/entity/Ball.java
@@ -6,19 +6,27 @@ class Ball {
 
     private static final int MIN_NUMBER_RANGE = 1;
     private static final int MAX_NUMBER_RANGE = 9;
+    private static final int MIN_POSITION_RANGE = 1;
+    private static final int MAX_POSITION_RANGE = 3;
+
     private static final String INVALID_NUMBER_RANGE_MESSAGE =
             "공이 가질 수 있는 숫자는 " + MIN_NUMBER_RANGE + "~" + MAX_NUMBER_RANGE + " 중 하나입니다.";
+    private static final String INVALID_POSITION_RANGE_MESSAGE =
+            "공이 가질 수 있는 위치는 " + MIN_POSITION_RANGE + "~" + MAX_POSITION_RANGE + " 중 하나입니다.";
 
     private final int number;
+    private final int position;
 
-    private Ball(int number) {
+    private Ball(int number, int position) {
         this.number = number;
+        this.position = position;
     }
 
-    public static Ball from(int number) {
+    public static Ball of(int number, int position) {
         checkNumberInRange(number);
+        checkPositionInRange(position);
 
-        return new Ball(number);
+        return new Ball(number, position);
     }
 
     private static void checkNumberInRange(int number) {
@@ -27,16 +35,22 @@ class Ball {
         }
     }
 
+    private static void checkPositionInRange(int position) {
+        if (position < MIN_POSITION_RANGE || position > MAX_POSITION_RANGE) {
+            throw new IllegalArgumentException(INVALID_POSITION_RANGE_MESSAGE);
+        }
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Ball ball = (Ball) o;
-        return number == ball.number;
+        return number == ball.number && position == ball.position;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(number);
+        return Objects.hash(number, position);
     }
 }
--- src/test/java/baseball/entity/BallTest.java
+++ src/test/java/baseball/entity/BallTest.java
@@ -13,8 +13,8 @@ class BallTest {
     @DisplayName("공을 생성할 수 있다.")
     @Test
     void create() {
-        Ball ball = Ball.from(1);
-        Ball otherBall = Ball.from(1);
+        Ball ball = Ball.of(1, 1);
+        Ball otherBall = Ball.of(1, 1);
 
         assertThat(ball).isEqualTo(otherBall);
     }
@@ -22,11 +22,18 @@ class BallTest {
     @DisplayName("공이 가질 수 있는 숫자의 범위는 1부터 9사이의 수이다.")
     @ParameterizedTest
     @ValueSource(ints = {0, 10})
-    void checkNumberInRangeMin(int source) {
-        String expectedMessage = "공이 가질 수 있는 숫자는 1~9 중 하나입니다.";
-
-        assertThatThrownBy(() -> Ball.from(source))
+    void checkNumberInRange(int number) {
+        assertThatThrownBy(() -> Ball.of(number, 1))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(expectedMessage);
+                .hasMessage("공이 가질 수 있는 숫자는 1~9 중 하나입니다.");
+    }
+
+    @DisplayName("공이 가질 수 있는 위치의 범위는 1부터 3사이의 수이다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 4})
+    void checkPositionInRange(int position) {
+        assertThatThrownBy(() -> Ball.of(1, position))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("공이 가질 수 있는 위치는 1~3 중 하나입니다.");
     }
 }
