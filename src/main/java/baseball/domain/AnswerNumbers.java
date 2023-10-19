a97a634934684611bb6a374b8e2c5e7cfcfa093a
donghyunkim <ghffu405@gmail.com>
2023-10-19 17:37:44 +0900

    refactor: numbers -> answerNumbers로 변수명변경

--- src/main/java/baseball/domain/AnswerNumbers.java
+++ src/main/java/baseball/domain/AnswerNumbers.java
@@ -10,26 +10,31 @@ import static baseball.constant.NumberConstant.*;
 
 public class AnswerNumbers {
 
-    private List<Integer> numbers;
+    private List<Integer> answerNumbers;
 
-    public AnswerNumbers(List<Integer> numbers) {
-        validateSize(numbers);
-        validateDuplicate(numbers);
-        this.numbers = numbers;
+    public AnswerNumbers(List<Integer> answerNumbers) {
+        validateSize(answerNumbers);
+        validateDuplicate(answerNumbers);
+        this.answerNumbers = answerNumbers;
     }
 
-    private void validateSize(List<Integer> numbers) {
-        if (numbers.size() != VALID_NUMBER_SIZE) {
+    private void validateSize(List<Integer> answerNumbers) {
+        if (answerNumbers.size() != VALID_NUMBER_SIZE) {
             throw new IllegalArgumentException("숫자는 3개만 가능합니다.");
         }
     }
 
-    private void validateDuplicate(List<Integer> numbers) {
-        Set<Integer> nonDuplicateNumbers = new HashSet<>(numbers);
+    private void validateDuplicate(List<Integer> answerNumbers) {
+        Set<Integer> nonDuplicateNumbers = new HashSet<>(answerNumbers);
 
         if (nonDuplicateNumbers.size() != VALID_NUMBER_SIZE) {
             throw new IllegalArgumentException("중복된 숫자가 존재합니다.");
         }
     }
+
+
+
+
+
 
 }
