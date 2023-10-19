7cb2574e8a896bbd3f92f94eb3e346d0c4456016
김영준 <rladudwns164@gmail.com>
2023-10-19 22:04:51 +0900

    feat : 재시작 화면 출력 기능 구현

---
+++ src/main/java/View/RetryView.java
@@ -0,0 +1,28 @@
+package View;
+
+public class RetryView {
+
+    private static final RetryView retryview = new RetryView();
+
+    private RetryView(){};
+
+    public void RetryPrint(){
+        System.out.println(Message.GAME_END_RETRY.message);
+    }
+
+
+    public enum Message {
+        GAME_END_RETRY("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        private String message;
+
+        Message(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+
+}
