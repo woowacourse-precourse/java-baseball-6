d9be2e9c333140d149e334a71874449dd6d1ecb9
Zeus6768 <akaps6768@gmail.com>
2023-10-19 15:54:49 +0900

    기능 수정: 무작위 수를 리스트가 아닌 문자열로 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,25 +2,20 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Application {
 
-    public static List<Integer> computer = getRandomNumber();
+    public static String computer = getRandomNumber();
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
 
-    private static List<Integer> getRandomNumber() {
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
+    private static String getRandomNumber() {
+        StringBuilder computer = new StringBuilder();
+        for (int i = 0; i < 3; i++) {
+            int randomNUmber = Randoms.pickNumberInRange(1, 9);
+            computer.append(randomNUmber);
         }
-        return computer;
+        return computer.toString();
     }
 }
