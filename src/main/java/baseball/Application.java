8afb9b8d154eb6fea99cc28b5bc335e0f64e9bdf
choheejin <hjcho9510@gmail.com>
2023-10-19 21:32:46 +0900

    fix: changed to see where the strike is

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,13 +3,14 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Judger judger = new Judger();
         Human human = new Human();
         Computer computer = new Computer();
 
         computer.setNumbers();
 
         do {
+            Judger judger = new Judger();
+
             judger.startGame();
             human.setNumbers();
 
--- src/main/java/baseball/Judger.java
+++ src/main/java/baseball/Judger.java
@@ -1,25 +1,33 @@
 package baseball;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Judger {
+    private int[] score;
+
+    public Judger() {
+        this.score = new int[3];
+    }
+
     public void startGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
     public void compareNumbers(List<Integer> human, List<Integer> computer) {
-        List<Integer> result = new ArrayList<>(Arrays.asList(0, 0, 0));
-        for(int i = 0; i < 3; i ++){
-            if(isStrike(human.get(i), computer.get(i))) {
-                result.set(0, result.get(0) + 1);
-            }
-        }
-        System.out.println(result);
+        int[] strikeIdx = checkStrike(human, computer);
     }
 
-    private boolean isStrike(int human, int computer){
-        return human == computer;
+    private int[] checkStrike(List<Integer> human, List<Integer> computer){
+        int[] strikeIdx = new int[3];
+
+        for(int i = 0; i < 3; i++) {
+            if(human.get(i).equals(computer.get(i))){
+                this.score[0] += 1;
+                strikeIdx[i] = 1;
+            }
+        }
+
+        return strikeIdx;
     }
 }
