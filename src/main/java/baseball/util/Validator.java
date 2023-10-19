9fee635a2fa47c71d3f9ca9d42061d967374309d
prao <pch5521@gmail.com>
2023-10-19 19:51:14 +0900

    feat: 유효성 검사 도구 기능 구현
    - isValidSize(String input, int size): 입력 값이 3자리가 아닌 경우 예외 처리
    - isInteger(String input): 입력 값이 1 이상 9 이하의 숫자가 아닌 경우 예외 처리
    - isDuplicate(String input): 입력 값이 중복된 숫자를 포함하는 경우 예외 처리

---
+++ src/main/java/baseball/util/Validator.java
@@ -0,0 +1,39 @@
+package baseball.util;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class Validator {
+
+    public Validator(String input, int size) {
+        isValidSize(input, size);
+        isInteger(input);
+        isDuplicate(input);
+    }
+
+    public void isValidSize(String input, int size) throws IllegalArgumentException{
+        if (input.length() != size) {
+            throw new IllegalArgumentException("올바른 자리 수의 값을 입력하세요.");
+        }
+    }
+
+    private void isInteger(String input) throws IllegalArgumentException {
+        for (char c : input.toCharArray()) {
+            if (c <= '0' || c > '9') {
+                throw new IllegalArgumentException("1부터 9까지의 수를 입력하세요.");
+            }
+        }
+    }
+
+    private void isDuplicate(String input) throws IllegalArgumentException{
+        List<Integer> inputNumbers = new ArrayList<>();
+        char[] charNumbers = input.toCharArray();
+        for (char charNumber : charNumbers) {
+            inputNumbers.add((int) charNumber);
+        }
+        if (inputNumbers.size() != new HashSet<>(inputNumbers).size()) {
+            throw new IllegalArgumentException("중복된 숫자를 포함하고 있습니다.");
+        }
+    }
+}
