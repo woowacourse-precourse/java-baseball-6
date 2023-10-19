bac9101a09f338448e4dcaa535bb006dc029b587
koomchang <leahpar0401@gmail.com>
2023-10-20 02:33:48 +0900

    feat: 사용자 입력에 따른 볼, 스트라이크 갯수를 출력해주는 메소드 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -10,6 +10,27 @@ public class OutputView {
         System.out.print("숫자를 입력해주세요 : ");
     }
 
+    public void printCountForBallsAndStrikes(int ballCount, int strikeCount) {
+        if (ballCount == 0 && strikeCount == 0) {
+            askForNumber();
+            return;
+        }
+        if (ballCount == 0) {
+            System.out.println(strikeCount + "스트라이크");
+            if (strikeCount != 3) {
+                askForNumber();
+            }
+            return;
+        }
+        if (strikeCount == 0) {
+            System.out.println(ballCount + "볼");
+            askForNumber();
+            return;
+        }
+        System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+        askForNumber();
+    }
+
     public void printEndGame() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
