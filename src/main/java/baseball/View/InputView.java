7f554d2280732ed73789fd2421b00d23fa4c74bf
twkwon0417 <kwontony123@konkuk.ac.kr>
2023-10-19 23:39:42 +0900

    feat: 맞춘후 게임을 다시할껀지 입력 받는 부분 구현

--- src/main/java/baseball/View/InputView.java
+++ src/main/java/baseball/View/InputView.java
@@ -2,6 +2,8 @@ package baseball.View;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Objects;
+
 public class InputView {
 
     public String inputUserGuess() {
@@ -15,9 +17,25 @@ public class InputView {
         return userInput;
     }
 
+    public String inputQuitOrRestart() {
+        String userInput = Console.readLine().trim();
+        try {
+            validateQuitOrRestart(userInput);
+        } catch (IllegalArgumentException e) {
+            System.out.println("유효하지 않은 입력입니다.");
+        }
+        return userInput;
+    }
+
     private void validateUserGuess (String userInput) throws IllegalArgumentException {
         int intUserInput = Integer.parseInt(userInput);
         if(intUserInput < InputValidationConstant.MIN_COMBINATION || intUserInput > InputValidationConstant.MAX_COMBINATION) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void validateQuitOrRestart(String userInput) throws IllegalArgumentException {
+        if(!Objects.equals(userInput, InputValidationConstant.RESTART) && !Objects.equals(userInput, InputValidationConstant.QUIT)) {
             throw new IllegalArgumentException();
         }
     }
