84a14578bfa277a94c8af21e1515ff4e0e9eba25
leedohyun <lucky77795@naver.com>
2023-10-19 19:52:01 +0900

    refactor: 정답 숫자 값을 List로 받아 입력 숫자 값도 List로 받는 코드로 수정

--- src/main/java/baseball/domain/EnterNum.java
+++ src/main/java/baseball/domain/EnterNum.java
@@ -1,12 +1,16 @@
 package baseball.domain;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class EnterNum {
-    private String enterNum;
+    private List<Integer> number;
 
-    public void setEnterNum(String enterNum) {
-        this.enterNum = enterNum;
+    public void setNum(String enterNum) {
+        this.number = strToList(enterNum);
     }
 
     public void validation() {
@@ -16,25 +20,31 @@ public class EnterNum {
     }
 
     private void checkLength() {
-        if (enterNum.length() != 3) {
+        if (number.size() != 3) {
             throw new IllegalArgumentException();
         }
     }
 
     private void checkContainZero() {
-        if (enterNum.contains("0")) {
+        if (number.contains("0")) {
             throw new IllegalArgumentException();
         }
     }
 
     private void duplicateNumCheck() {
-        HashSet<Character> checkSet = new HashSet<>();
-        for (int i = 0; i < 3; i++) {
-            checkSet.add(enterNum.charAt(i));
-        }
+        HashSet<Integer> checkSet = new HashSet<>(number);
 
         if (checkSet.size() != 3) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private List<Integer> strToList(String enterNum) {
+        List<Integer> temp = new ArrayList<>();
+        for (int i = 0; i < enterNum.length(); i++) {
+            temp.add(enterNum.charAt(i) - '0');
+        }
+
+        return temp;
     }
 }
