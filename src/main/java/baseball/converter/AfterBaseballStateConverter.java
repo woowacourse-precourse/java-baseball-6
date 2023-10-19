f7dace480dfc8bec2aa69bddb5776649679138d5
cloudchamb3r <jizon0123@protonmail.com>
2023-10-20 00:24:01 +0900

    add : implementation of converter

--- src/main/java/baseball/converter/AfterBaseballStateConverter.java
+++ src/main/java/baseball/converter/AfterBaseballStateConverter.java
@@ -1,2 +1,15 @@
-package baseball.converter;public class AfterBaseballStateConverter {
+package baseball.converter;
+
+import baseball.state.AfterBaseballState;
+
+public class AfterBaseballStateConverter {
+    public static AfterBaseballState stringToAfterBaseballState(String s) throws IllegalArgumentException {
+        if (s.equals("1")) {
+            return AfterBaseballState.RESTART_GAME;
+        }
+        if (s.equals("2")) {
+            return AfterBaseballState.END_GAME;
+        }
+        throw new IllegalArgumentException();
+    }
 }
--- src/main/java/baseball/converter/BaseballStateConverter.java
+++ src/main/java/baseball/converter/BaseballStateConverter.java
@@ -1,2 +1,28 @@
-package baseball.converter;public class BaseballStateConverter {
+package baseball.converter;
+
+import static java.util.stream.Collectors.*;
+
+import baseball.state.BaseballState;
+import baseball.utils.IoHelper;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class BaseballStateConverter {
+    public static BaseballState stringToBaseballState(String s) throws IllegalArgumentException{
+        IoHelper.println("this is s => " + s);
+        if (s.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+        if (!s.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException();
+        }
+        var charSet = s.chars().mapToObj(c -> (char)c).collect(Collectors.toSet());
+        if (charSet.size() != 3) {
+            throw new IllegalArgumentException();
+        }
+        List<Integer> state = s.chars().mapToObj(Character::getNumericValue).collect(Collectors.toList());
+        return new BaseballState(state);
+    }
 }
