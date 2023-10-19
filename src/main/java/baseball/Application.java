7c89e7ab2a9932e434f4ed5ee462789ba2b4b02c
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-10-20 07:04:13 +0900

    feat: Update Application.java 사용자 숫자 입력 코드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,18 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
 
     public static void main(String[] args) {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
         Computer com = new Computer();
         com.getNumbers();
+        System.out.print("숫자를 입력해주세요 : ");
+        String player = Console.readLine();
+        
         com.getClear();
+        Console.close();
     }
 }
