daa854e699e1143e88dfdd3125b4a7502c527da4
Seonghwan <aiya0422@gmail.com>
2023-10-20 01:04:36 +0900

    REFACTOR: 게임 실행 부분을 메서드로 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,5 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class Application {
@@ -10,23 +9,27 @@ public class Application {
         boolean flag = true;
 
         while (flag) {
-            List<Integer> answer = Generator.createAnswer();
-            boolean gameFlag = true;
-
-            while (gameFlag) {
-                List<Integer> input = UserInput.inputList();
-
-                int strike = Calculator.countStrike(input, answer);
-                int ball = Calculator.countBall(input, answer);
-
-                Printing.showResult(strike, ball);
-
-                if (strike == 3) {
-                    gameFlag = false;
-                }
-            }
+            game();
 
             flag = UserInput.inputOneOrTwo();
+        }
+    }
+
+    private static void game() {
+        List<Integer> answer = Generator.createAnswer();
+        boolean gameFlag = true;
+
+        while (gameFlag) {
+            List<Integer> input = UserInput.inputList();
+
+            int strike = Calculator.countStrike(input, answer);
+            int ball = Calculator.countBall(input, answer);
+
+            Printing.showResult(strike, ball);
+
+            if (strike == 3) {
+                gameFlag = false;
+            }
         }
     }
 }
