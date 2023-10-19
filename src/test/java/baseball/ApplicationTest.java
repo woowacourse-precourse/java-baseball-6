a03dc54f3b1c2a928c923b8dd1b88c39b7fc47b4
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-19 17:30:14 +0900

    test(BaseballNumber):initializeComputerNumber 메서드에 테스트 코드 작성
    initializeComputerNumber_유효성 테스트

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,12 +1,18 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -32,4 +38,33 @@ class ApplicationTest extends NsTest {
     public void runMain() {
         Application.main(new String[]{});
     }
+    @Test
+    void initializeComputerNumber_유효성테스트() {
+        //given
+        //when
+        final Throwable thrown = catchThrowable(()->BaseballNumber.initializeComputerNumber());
+        //then
+        assertThat(thrown).as("initializeComputerNumber_테스트").doesNotThrowAnyException();
+    }
+    @Test
+    void 숫자의_개수가_3개이상_입력_예외() {
+        //given
+        List<Integer> testList = new ArrayList(Arrays.asList(1, 2, 3, 4));
+        //when
+        //then
+        assertThatThrownBy(() -> new BaseballNumber(testList))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("게임을 진행할 수는 항상 3개여야 합니다.");
+    }
+    @Test
+    void 중복된_숫자가_들어왔을때_예외() {
+        //given
+        List<Integer> testList = new ArrayList(Arrays.asList(2, 2, 3));
+        //when
+        //then
+        assertThatThrownBy(() -> new BaseballNumber(testList))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("서로 같은 숫자들은 입력될 수 없습니다.");
+    }
+
 }
