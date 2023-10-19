a3b4183db91e919742475ba9cde8659fa2650c20
tfer2442 <tfer2442@gmail.com>
2023-10-20 02:43:46 +0900

    feat(Computer): Computer가 가지고 있는 숫자의 개수를 반환하는 size 메서드 생성

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
+    // Computer가 어떤 숫자를 가지고 있는지 모르게 하기 위해
     private final List<Integer> threeNumbers = new ArrayList<>();
 
     public void add(int num){
@@ -14,6 +15,9 @@ public class Computer {
         return threeNumbers.contains(num);
     }
 
+    public int size(){
+        return threeNumbers.size();
+    }
     @Override
     public String toString() {
         return "Computer{" +
