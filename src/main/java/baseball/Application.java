f5aa07c7dadddba15df5647fc5e2804dcd7b755f
wns312 <wns312@gmail.com>
2023-10-19 15:54:28 +0900

    feat: 3자리의 난수 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    static List<Integer> pickRandomNumbers() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
+    static void play() {
+        List<Integer> computer = pickRandomNumbers();
+    }
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        play();
     }
 }
