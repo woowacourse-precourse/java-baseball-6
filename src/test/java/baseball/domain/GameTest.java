cb35489a4d3cba95918f0d47521d570434a9911b
hongseokhodev <hongseokhodev@gmail.com>
2023-10-20 00:49:10 +0900

    test: add argument

--- src/test/java/baseball/domain/GameTest.java
+++ src/test/java/baseball/domain/GameTest.java
@@ -7,13 +7,13 @@ import org.junit.jupiter.api.Test;
 public class GameTest {
     @Test
     void 게임_시작_전() {
-        Game game = new Game();
+        Game game = new Game(new RandomBallsGenerator());
         assertEquals(game.getGameState(), GameState.READY);
     }
 
     @Test
     void 게임_시작() {
-        Game game = new Game();
+        Game game = new Game(new RandomBallsGenerator());
         game.start();
         assertEquals(game.getGameState(), GameState.ON);
     }
