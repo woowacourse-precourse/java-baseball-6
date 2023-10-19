4945705c39b013b661ca846a41671587e288b69e
WeepingHeron <kimtaehoon_98@naver.com>
2023-10-19 21:17:59 +0900

    feat: judge whether it's strike or ball

---
+++ src/main/java/baseball/judge/Judge.java
@@ -0,0 +1,58 @@
+package baseball.judge;
+
+import baseball.givennum.GivenNum;
+
+public class Judge {
+    private int[] receivedNum;
+
+    public Judge(GivenNum givenNum) {
+        this.receivedNum = givenNum.getComputerNumbersAsArray();
+    }
+
+    public String strikesAndBalls(int[] userNum) {
+        String result = "";
+
+        if (userNum.length != 3) {
+            throw new IllegalArgumentException("3자리의 수를 입력해 주세요.");
+        }
+
+        int strikes = 0;
+        int balls = 0;
+        boolean[] countedAsStrike = new boolean[3];
+        boolean[] countedAsBall = new boolean[3];
+
+        for (int i = 0; i < 3; i++) {
+            if (userNum[i] == receivedNum[i]) {
+                strikes++;
+                countedAsStrike[i] = true;
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (!countedAsStrike[i] && !countedAsStrike[j] && userNum[i] == receivedNum[j]) {
+                    balls++;
+                    countedAsBall[i] = true;
+                }
+            }
+        }
+
+        if (strikes == 3) {
+            return "3스트라이크";
+        }
+
+        if (strikes > 0) {
+            result += strikes + "스트라이크";
+        }
+        if (balls > 0) {
+            if (!result.isEmpty()) {
+                result += " ";
+            }
+            result += balls + "볼";
+        }
+        if (strikes == 0 && balls == 0) {
+            result = "낫싱";
+        }
+        return result;
+    }
+}
