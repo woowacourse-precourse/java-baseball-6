a8a42937075a7c4c29f6df5936fca88d69180428
pwrwpw <dlgustjr9912@gmail.com>
2023-10-19 22:27:43 +0900

    refactor(InputValidation): 변수 스코프를 스태틱으로 변경, 반환값을 void에서 String으로 변경

--- src/main/java/baseball/utills/InputValidation.java
+++ src/main/java/baseball/utills/InputValidation.java
@@ -9,7 +9,7 @@ public class InputValidation {
 	private static final String WRONG_INPUT_LENGTH_MESSAGE = "세 자리만 입력 가능합니다.";
 	private static final String WRONG_INPUT_RANGE_MESSAGE = "1~9 사이의 숫자만 입력 가능합니다.";
 	private static final String WRONG_INPUT_DUPLICATE_MESSAGE = "서로 다른 세자리 숫자만 입력 가능합니다.";
-	public void validate(String inputNumber){
+	public static String validate(String inputNumber){
 		if(!hasExactLength(inputNumber)){
 			throw new IllegalArgumentException(WRONG_INPUT_LENGTH_MESSAGE);
 		}
@@ -19,19 +19,19 @@ public class InputValidation {
 		if(!hasExactDuplicate(inputNumber)){
 			throw new IllegalArgumentException(WRONG_INPUT_DUPLICATE_MESSAGE);
 		}
+		return inputNumber;
 	}
 
-	public boolean hasExactLength(String inputNumber) {
+	public static boolean hasExactLength(String inputNumber) {
 		return inputNumber.length() == NUMBER_LENGTH;
 	}
-	public boolean hasExactRange(String inputNumber) {
+	public static boolean hasExactRange(String inputNumber) {
 		return inputNumber.chars().allMatch(Character::isDigit);
 	}
-	public boolean hasExactDuplicate(String inputNumber) {
+	public static boolean hasExactDuplicate(String inputNumber) {
 		String[] inputNumbers = inputNumber.split("");
 		Set<String> setNumbers = Arrays.stream(inputNumbers).collect(Collectors.toSet());
 
 		return setNumbers.size() == NUMBER_LENGTH;
 	}
-
 }
