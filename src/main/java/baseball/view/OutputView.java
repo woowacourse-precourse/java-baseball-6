d518a0df9c3334e05d5e0b1a246587f0cdaca061
gyungchan Jo <namejgc@naver.com>
2023-10-20 01:17:19 +0900

    fix(OutputView): 볼 또는 스트라이크의 개수가 0일 경우 빈 문자열을 반환하도록 수정

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -6,6 +6,7 @@ public class OutputView {
     private static final String NOTHING = "낫싱";
     private static final String BALL = "볼";
     private static final String STRIKE = "스트라이크";
+    private static final String EMPTY = "";
     private static final int ZERO = 0;
 
     public void printStartMessage() {
@@ -34,10 +35,16 @@ public class OutputView {
     }
 
     private String generateBallMessage(int ball) {
+        if (ball == ZERO) {
+            return EMPTY;
+        }
         return ball + BALL;
     }
 
     private String generateStrikeMessage(int strike) {
+        if (strike == ZERO) {
+            return EMPTY;
+        }
         return strike + STRIKE;
     }
 }
