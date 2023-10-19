e3edd5e1d8337b10b17f9270af2ea7ff4c646b5f
Octoping <mooner925@gmail.com>
2023-10-20 00:42:48 +0900

    feat: 서로 다른 3개의 숫자를 무작위로 뽑는 RandomNumberGenerator 추가

---
+++ src/test/java/baseball/domain/RandomNumberGeneratorTest.java
@@ -0,0 +1,45 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RandomNumberGeneratorTest {
+    @DisplayName("RandomNumberGenerator는 3자리 숫자를 생성한다.")
+    @Test
+    void generate_3length_number_test() {
+        // given
+        RandomNumberGenerator generator = new RandomNumberGenerator();
+
+        // when
+        String number = generator.generate();
+        System.out.println("number = " + number);
+
+        // then
+        assertThat(number)
+                .hasSize(3)
+                .matches("[1-9]+");
+    }
+
+    @DisplayName("RandomNumberGenerator는 모든 자리의 숫자가 다르다")
+    @Test
+    void generate_different_each_digit_test() {
+        // given
+        RandomNumberGenerator generator = new RandomNumberGenerator();
+
+        // when
+        String number = generator.generate();
+
+        // then
+        Set<Character> characters = new HashSet<>();
+        for (char c : number.toCharArray()) {
+            assertThat(characters).doesNotContain(c);
+            characters.add(c);
+        }
+    }
+
+}
