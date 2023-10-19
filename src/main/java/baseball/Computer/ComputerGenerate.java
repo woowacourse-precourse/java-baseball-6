2f37ba6cb209b7362e03129c24bea668e1237269
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2023-10-20 01:13:46 +0900

    UserGenerate 기능, README 파일 수정(야구 로직을 따로 빼냄)

--- src/main/java/baseball/Computer/ComputerGenerate.java
+++ src/main/java/baseball/Computer/ComputerGenerate.java
@@ -8,12 +8,14 @@ import java.util.List;
 public class ComputerGenerate {
     public List<Integer> getTarget(){
         List<Integer> computer = new ArrayList<>();
+
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
         }
+
         return computer;
     }
 }
---
+++ src/main/java/baseball/User/UserGenerate.java
@@ -0,0 +1,22 @@
+package baseball.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserGenerate {
+    public List<Integer> getAttempt() {
+        UserInput userInputIsString = new UserInput();
+        List<Integer> result = new ArrayList<>();
+        String userInput;
+
+        // 사용자 입력 받기
+        userInput = userInputIsString.getGamingInput();
+
+        // String -> List<Integer>
+        for (char c : userInput.toCharArray()) {
+            result.add(c - '0');
+        }
+
+        return result;
+    }
+}
