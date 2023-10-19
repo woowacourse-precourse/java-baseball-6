ab1724d557106a6c6c0793ffd60d406b79059ad3
Chenowoo <cjsgusdn14@naver.com>
2023-10-20 01:02:33 +0900

    ComputerSelectNumber, PlayerSelectNumber 메서드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -7,7 +8,8 @@ import java.util.List;
 
 public class Application {
 
-    public static List<Integer> computerNumber;
+//    public static List<Integer> computerNumber;
+//    public static List<Integer> playerNumber;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다");
@@ -15,15 +17,42 @@ public class Application {
         GameStart();
     }
 
-    public static void GameStart(){
-        //컴퓨터가 서로 다른 3개의 숫자 생성
+    public static void GameStart() {
 
-        computerNumber = new ArrayList<>();
-        for(int i=0; i<3; i++) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            computerNumber.add(randomNumber);
+        String computerNumber = ComputerSelectNumber();
+
+        while(true) {
+            String playerNumber = PlayerSelectNumber();
+            CompareNumber(computerNumber, playerNumber);
         }
 
-
     }
+
+    public static String ComputerSelectNumber() {
+        //컴퓨터가 서로 다른 3개의 숫자 생성
+
+//        computerNumber = new ArrayList<>();
+        String computerNumber = "";
+        for(int i=0; i<3; i++) {
+//            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            computerNumber += String.valueOf(Randoms.pickNumberInRange(1, 9));
+//            computerNumber.add(randomNumber);
+        }
+
+        return computerNumber;
+    }
+
+    public static String PlayerSelectNumber() {
+//        playerNumber = new ArrayList<>();
+        System.out.print("숫자를 입력해주세요 : ");
+
+//        String playerInputNumber = Console.readLine();
+        return Console.readLine();
+
+//        for(int i=0; i<playerInputNumber.length(); i++) {
+//            playerNumber.add(playerInputNumber.charAt(i) - '0');
+//        }
+    }
+
+
 }
