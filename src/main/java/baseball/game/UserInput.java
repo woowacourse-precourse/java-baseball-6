63bb396f3855432b0817747f04fa5bb7013b1af7
JunTaeINC <juntea3523@naver.com>
2023-10-19 22:28:57 +0900

    feat : 재시작 입력받기 구현

--- src/main/java/baseball/game/UserInput.java
+++ src/main/java/baseball/game/UserInput.java
@@ -6,14 +6,24 @@ import java.util.List;
 
 public class UserInput {
 
-	public static List<Integer> getUserInput() {
+	public List<Integer> getNumbers() {
 		List<Integer> numbers = new ArrayList<>();
-		String input = Console.readLine();
+		String input = getUserInput();
 
 		for (int i = 0; i < input.length(); i++) {
 			numbers.add(Character.getNumericValue(input.charAt(i)));
 		}
 
 		return numbers;
+	}
+
+	public boolean askToPlayAgain() {
+		String input = getUserInput();
+
+		return input.equals("1");
+	}
+
+	public String getUserInput() {
+		return Console.readLine();
 	}
 }
