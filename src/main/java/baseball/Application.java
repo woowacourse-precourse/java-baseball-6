c6f45371663d4fd9ebc7feef8d9f483d7576a00d
wooteco-daram <wooteco.daram@gmail.com>
2023-10-20 04:22:52 +0900

    feat: 컴퓨터가 랜덤으로 숫자를 생성하는 `getRandomNumberList` 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,15 +1,36 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
     private static final String STRING_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
 
+    private static final int SIZE_RANDOM_NUMBER = 3;
+    private static final int MIN_PICK_NUMBER = 1;
+    private static final int MAX_PICK_NUMBER = 9;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         printStartMessage();
+
+        List<Integer> computer = getRandomNumberList();
     }
 
     public static void printStartMessage() {
         System.out.println(STRING_START_MESSAGE);
+    }
+
+    public static List<Integer> getRandomNumberList() {
+        List<Integer> randomNumberList = new ArrayList<>();
+        while (randomNumberList.size() < SIZE_RANDOM_NUMBER) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_PICK_NUMBER, MAX_PICK_NUMBER);
+            if (!randomNumberList.contains(randomNumber)) {
+                randomNumberList.add(randomNumber);
+            }
+        }
+        return randomNumberList;
     }
 }
