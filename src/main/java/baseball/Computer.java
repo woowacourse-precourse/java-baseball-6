d558ba9b01562f7b539ecfea98210feb0d52228c
leesangjun <hubls@naver.com>
2023-10-19 22:32:54 +0900

    feat(Computer): Computer 1 ~ 9 까지 서로 다른 수로 이루어진 3자리 수 생성 기능

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,23 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+	private final static int RANDOM_NUMBER_MINIMUM = 1;
+	private final static int RANDOM_NUMBER_MAXIMUM = 9;
+	public  List<Integer> randomNumber = new ArrayList<>();
+
+	public Computer() {
+	}
+
+	public void setRandomNumber() {
+		while (randomNumber.size() < 3) {
+			int number = Randoms.pickNumberInRange(RANDOM_NUMBER_MINIMUM, RANDOM_NUMBER_MAXIMUM);
+			if (!randomNumber.contains(number)) {
+				randomNumber.add(number);
+			}
+		}
+	}
+}
