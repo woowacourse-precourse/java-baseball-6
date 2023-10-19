9dce15b1dabd482bf6b2ba1e79113c15ac4552cf
Gukin Han <gukin.dev@gmail.com>
2023-10-19 19:10:21 +0900

    Feat: init 메서드로 변수 초기화

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,13 +1,31 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 public class BaseballGame {
-    private boolean running = true;
+    private boolean running;
+    private boolean found;
+    private List<Integer> randomNumbers = new ArrayList<>();
+    private Validator validator = new Validator();
+    private Comparator comparator = new Comparator();
+    private final int DIGIT = 3;
+
+    private void init() {
+        generateRandomNumbers();
+        this.running = true;
+        this.found = false;
+    }
 
     public BaseballGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
     public void run() {
+        init();
         while (running) {
             System.out.println("It starts running.");
             exit();
