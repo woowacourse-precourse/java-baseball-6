3595789f2bec92384a1c672571efa1eea41f82f5
jy016011 <39334789+jy016011@users.noreply.github.com>
2023-10-19 17:12:00 +0900

    랜덤숫자 생성기 Ver1.0  구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import baseball.RandomNumberGenerator;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        List<Integer> computer = randomNumberGenerator.generateRandomNumber();
+        
     }
 }
