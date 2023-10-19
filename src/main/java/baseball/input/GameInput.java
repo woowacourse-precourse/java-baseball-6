91d9e3885fd1443f55637133533492e89e882667
Sully <2dcoder@naver.com>
2023-10-19 21:17:52 +0900

    feat: 상수를 출력할 메서드 구현

--- src/main/java/baseball/input/GameInput.java
+++ src/main/java/baseball/input/GameInput.java
@@ -5,4 +5,12 @@ public class GameInput {
     private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String REQUEST_MESSAGE = "숫자를 입력해주세요 : ";
 
+    private void printRequestMessage() {
+        System.out.print(REQUEST_MESSAGE);
+    }
+
+    public void printlnStartMessage() {
+        System.out.println(START_MESSAGE);
+    }
+
 }
