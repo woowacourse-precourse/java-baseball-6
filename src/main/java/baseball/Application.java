2fbd92da11a29f95a34f31c7655027954c5b7b21
naraspc <wodnrtkfka@naver.com>
2023-10-20 04:07:14 +0900

    Feat: "숫자 야구 게임 구현"
    게임 메인 로직 구현 완료
    
    수정해야할 기능 :
    
    게임 로직 초기화시 컴퓨터의 랜덤값도 초기화 되야함
    
    지금 배열의 길이가 정해져있어서 그보다 짧거나 긴 값이 들어오면
    StringIndexOutOfBoundsException가 나옴.
    IllegalArgumentException 익셉션으로 수정해야함.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,103 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // 컴퓨터 랜덤 배열
+        List<Integer> computer = new ArrayList<>();
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+
+
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+            if (computer.size() == 3) {
+                System.out.println(computer);
+            }
+        }
+
+        while (true) try {
+            int[] insertNum;
+            int j;/*
+            System.out.println(computer.size() + "랜덤 컴퓨터 길이");*/
+            int ball = 0;
+            int strike = 0;
+
+            System.out.println("숫자를 입력해주세요");
+            String insertStringNum = Console.readLine();
+
+            insertNum = new int[insertStringNum.length()];
+
+            //z는
+            for (int z = 0; z < 3; z++) {
+                insertNum[z] = insertStringNum.charAt(z) - '0';
+            }
+            /*
+            System.out.println(insertNum[0] + " 첫 입력값");
+            System.out.println(insertNum[1] + " 두번째 입력값");
+            System.out.println(insertNum[2] + " 세번째 입력값");
+            System.out.println(insertNum.length + " 입력값 길이");*/
+
+            if (insertNum.length != 3) {
+                throw new IllegalArgumentException();
+            }
+
+
+            if (insertNum[0] == insertNum[1] || insertNum[0] == insertNum[2]) {
+                    /*    System.out.println(insertNum[0] + "첫번째 입력받은 값 입니다");
+                        System.out.println(insertNum[1] + "두번째 입력받은 값 입니다.");
+                        System.out.println(insertNum[2] + " 세번째 입력값");*/
+                throw new IllegalArgumentException();
+            }
+
+
+            // i는 컴퓨터랜덤값만큼 (3번) 돈다 -> 게임의 목적이 컴퓨터의 랜덤값 3자리를 전부 맞추는거니까 3번만 돎
+            // 실질적인 게임 로직 스타트
+            for (int i = 0; i < computer.size(); i++) {
+                for (int k = 0; k < insertNum.length; k++) {
+                    if (computer.get(i) == insertNum[k] && i == k) {
+                        strike++;
+                    } else if (computer.get(i) == insertNum[k] && i != k) {
+                        ball++;
+                    }
+                }
+
+            }
+            if (strike == 3) {
+                System.out.println("3스트라이크" + "\n" + "3개의 숫자를 모두 맞히셧습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+
+                int statusCode = Integer.parseInt(Console.readLine());
+                if (statusCode == 1) {
+                    continue;
+                } else if (statusCode == 2) {
+                    break;
+                } else throw new IllegalArgumentException();
+
+
+            } else if (ball != 0 && strike != 0) {
+                System.out.println(ball + "볼 "  + strike + "스트라이크");
+            } else if (ball == 0 && strike != 0) {
+                System.out.println(strike + "스트라이크");
+            } else if (ball != 0) {
+                System.out.println(ball + "볼");
+            } else {
+                System.out.println("낫싱");
+            }
+        } catch (IllegalArgumentException ILAE) {
+            break;
+        }
     }
 }
+
+
