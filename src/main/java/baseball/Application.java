aa0917cf0ae4964852ebad84d744e28d5b04ea32
leeyeongjae <xingxing2001@cau.ac.kr>
2023-10-19 20:04:18 +0900

    1. Game클래스를 통한 게임 진행 2.1번을 위해 코드 전체 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,46 +6,11 @@ import java.util.ArrayList;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Computer computer = new Computer();
-        System.out.println(computer.getNumbers());
+        Game game = new Game(); //게임 인스턴스 생성
         System.out.println("숫자 야구 게임을 시작합니다.");
-        while(true){
-            User user = new User();
-            System.out.print("숫자를 입력해주세요: ");
-            user.setGuessNumber();
-            System.out.println(user.getNumbers());
-            ArrayList<Integer>com = computer.getNumbers();
-            ArrayList<Integer>player = user.getNumbers();
-            int strike = 0;
-            int ball = 0;
-            for(int i=0 ;i<com.size();i++){
-                for(int j=0;j<player.size();j++){
-                    if(com.get(i).equals(player.get(j))){
-                        if(i==j){
-                            strike++;
-                        } else
-                            ball++;
-                    }
-                }
-            }
-            if(strike==3){
-                System.out.println("3스트라이크");
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            }
-            else if(strike!=0||ball!=0){
-                if(ball!=0){
-                    System.out.printf("%d볼 ",ball);
-                }
-                if(strike!=0){
-                    System.out.printf("%d스트라이크\n",strike);
-                } else{
-                    System.out.println();
-                }
-            } else {
-                System.out.println("낫싱");
-            }
-        }
+        /*
+        숫자 야구 진행
+         */
 
     }
 }
