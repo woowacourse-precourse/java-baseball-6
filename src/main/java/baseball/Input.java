567fd258d0269fff338b195a9ad7fa5344ba29ae
Zeus6768 <akaps6768@gmail.com>
2023-10-19 16:23:45 +0900

    기능 구현: 사용자 수 입력 검증, 재시작/종료 입력 검증

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -4,17 +4,55 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
 
-    protected static final String RESTART = "1";
-    protected static final String EXIT = "2";
+    public static final String RESTART = "1";
+    public static final String EXIT = "2";
 
     public static String guess;
 
-    public static void guessInput() {
-        String input = Console.readLine();
+    public static void askGuess() {
+        guess = Console.readLine();
+        validateGuessInput();
     }
 
-    public static boolean isRestart() {
+    public static boolean askRestart() {
         String input = Console.readLine();
+        validateRestartInput(input);
         return input.equals(RESTART);
+    }
+
+    private static void validateGuessInput() {
+        if (guess.length() != 3) {
+            throw new IllegalArgumentException(InputErrorMessage.GUESS);
+        }
+        if (!isNumeric()) {
+            throw new IllegalArgumentException(InputErrorMessage.GUESS);
+        }
+        if (guess.contains("0")) {
+            throw new IllegalArgumentException(InputErrorMessage.GUESS);
+        }
+        if (isDuplicate()) {
+            throw new IllegalArgumentException(InputErrorMessage.GUESS);
+        }
+    }
+
+    private static void validateRestartInput(String input) {
+        if (!(input.equals(RESTART) || input.equals(EXIT))) {
+            throw new IllegalArgumentException(InputErrorMessage.RESTART);
+        }
+    }
+
+    private static boolean isNumeric() {
+        for (char c : guess.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isDuplicate() {
+        return guess.charAt(0) == guess.charAt(1)
+                || guess.charAt(0) == guess.charAt(2)
+                || guess.charAt(1) == guess.charAt(2);
     }
 }
