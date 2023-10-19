3e968eaa8967d0e57464147c95c239cedfe17be5
WonyuChoi <phillip07290@naver.com>
2023-10-20 00:33:17 +0900

    feat: 상대 객체가 맞출 수들을 정하고 결과를 계산할 수 있도록 기능 추가

--- src/main/java/baseball/model/Opponent.java
+++ src/main/java/baseball/model/Opponent.java
@@ -5,6 +5,7 @@ import static baseball.Constants.MIN_NUM;
 import static baseball.Constants.NUMBER_COUNTS;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Opponent {
@@ -16,8 +17,33 @@ public class Opponent {
     }
 
     public static Opponent createOpponent() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_NUM, MAX_NUM,
-            NUMBER_COUNTS);
-        return new Opponent(numbers);
+        List<Integer> pickedNumbers = new ArrayList<>();
+        while (pickedNumbers.size() < NUMBER_COUNTS) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_NUM, MAX_NUM);
+            if (!pickedNumbers.contains(randomNumber)) {
+                pickedNumbers.add(randomNumber);
+            }
+        }
+        return new Opponent(pickedNumbers);
+    }
+
+    public GuessResult calculateResult(final UserNumbers userNumbers) {
+        GuessResult result = new GuessResult();
+        List<Integer> guessedNumbers = userNumbers.getNumbers();
+        for (int i = 0; i < guessedNumbers.size(); i++) {
+            int numberIndex = numbers.indexOf(guessedNumbers.get(i));
+            if (numberIndex == -1) {
+                continue;
+            }
+
+            if (numberIndex == i) {
+                result.addStrikeCount();
+                continue;
+            }
+
+            result.addBallCount();
+        }
+
+        return result;
     }
 }
