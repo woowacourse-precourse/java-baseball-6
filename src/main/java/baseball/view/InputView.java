ec332b90b7134b7cc364948b60188b3130285f73
Hugh <lee2317884@gmail.com>
2023-10-19 21:49:30 +0900

    refactor: 재시작 입력 1 혹은 2 아닐 경우 InputView 에서 입력 시 예외 처리 하도록 리팩토링

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -9,6 +9,9 @@ public class InputView {
 	public static final String INPUT_GAME_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
 	public static final String INPUT_GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
+	private static final String  GAME_RESTART_NUMBER = "1";
+	private static final String  GAME_END_NUMBER = "2";
+
 	public static String inputGameNumberMessage() {
 		String input = getInput();
 		validateBlankInputGameNumber(input);
@@ -22,9 +25,16 @@ public class InputView {
 	}
 
 
-	public static String printInputGameRestartMessage() {
-		System.out.println(INPUT_GAME_RESTART_MESSAGE);
-		return Console.readLine();
+	public static String inputGameRestartMessage() {
+		String input = getInput();
+		validateWrongGameRestartInput(input);
+		return input;
+	}
+
+	private static void validateWrongGameRestartInput(String input) {
+		if (!input.equals(GAME_RESTART_NUMBER) && !input.equals(GAME_END_NUMBER)) {
+			throw new IllegalArgumentException("잘못된 재시작 입력 예외 발생 테스트");
+		}
 	}
 
 	private static String getInput() {
