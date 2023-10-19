bdf21c9714ac1a0e4d37405b1a63a2aecd14cf47
Hugh <lee2317884@gmail.com>
2023-10-19 20:31:54 +0900

    feat: 클리어시 '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.' 출력

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -6,9 +6,15 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
 	private static final String INPUT_GAME_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+	private static final String INPUT_GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
 	public static String printInputGameNumberMessage() {
 		System.out.print(INPUT_GAME_NUMBER_MESSAGE);
+		return Console.readLine();
+	}
+
+	public static String printInputGameRestartMessage() {
+		System.out.println(INPUT_GAME_RESTART_MESSAGE);
 		return Console.readLine();
 	}
 }
