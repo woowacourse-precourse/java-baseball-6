91abeda22451f55aaba8b796fd3562df12abfe5c
Seonghwan <aiya0422@gmail.com>
2023-10-19 19:33:15 +0900

    FEAT: main 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,37 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Printing.opening();
+        boolean flag = true;
+
+        while (flag) {
+            List<Integer> answer = Generator.createAnswer();
+            System.out.println(answer);
+            boolean gameFlag = true;
+
+            while (gameFlag) {
+                System.out.print("숫자를 입력해주세요 : ");
+                List<Integer> input = UserInput.inputList();
+
+                int strike = Calculator.countStrike(input, answer);
+                int ball = Calculator.countBall(input, answer);
+
+                Printing.showResult(strike, ball);
+
+                if (strike == 3) {
+                    gameFlag = false;
+                }
+            }
+
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
+            Printing.closing();
+            flag = UserInput.inputOneOrTwo();
+        }
     }
 }
