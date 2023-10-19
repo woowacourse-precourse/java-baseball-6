84b360388ed379f13ae6fe18dbb6e3b1bd4acbab
WonyuChoi <phillip07290@naver.com>
2023-10-19 21:16:39 +0900

    feat: 출력에 관한 로직 구현

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,22 @@
+package baseball.view;
+
+public class OutputView {
+
+    private static final int NUMBER_COUNTS=3;
+
+    public static void showStartDescription() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void askNumbers() {
+        System.out.println("숫자를 입력해주세요 : ");
+    }
+
+    public static void informGameOver() {
+        System.out.println(NUMBER_COUNTS+"개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public static void askContinueGame() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+}
