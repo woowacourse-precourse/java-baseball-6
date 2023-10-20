e96174305642f9faefc9966dd26fe6abd03e5cae
kunsanglee <gosmdochee2@gmail.com>
2023-10-20 18:48:23 +0900

    refactor(RandomAnswerGenerator): 매직넘버 상수로 선언

--- src/main/java/baseball/RandomAnswerGenerator.java
+++ src/main/java/baseball/RandomAnswerGenerator.java
@@ -5,17 +5,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RandomAnswerGenerator implements AnswerGenerator {
+
+    public static final int ANSWER_SIZE = 3;
+    public static final int MIN_BASEBALL_NUMBER = 1;
+    public static final int MAX_BASEBALL_NUMBER = 9;
+
     @Override
     public List<Integer> generate() {
         List<Integer> answer = new ArrayList<>();
-        while (answer.size() < 3) {
+        while (answer.size() < ANSWER_SIZE) {
             addNonDuplicatedNumber(answer);
         }
         return answer;
     }
 
     private static void addNonDuplicatedNumber(List<Integer> answer) {
-        int number = Randoms.pickNumberInRange(1, 9);
+        int number = Randoms.pickNumberInRange(MIN_BASEBALL_NUMBER, MAX_BASEBALL_NUMBER);
         if (!answer.contains(number)) {
             answer.add(number);
         }
