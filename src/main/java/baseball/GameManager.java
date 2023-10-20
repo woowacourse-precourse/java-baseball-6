565afa1b54e257bab340b4e91dd9d5f17d626d06
Jonghyeok Lee <endb1228@gmail.com>
2023-10-20 19:50:28 +0900

    정답 비교 및 결과 출력 기능 구현

--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class GameManager {
     private static GameManager gameManager;
@@ -19,19 +20,51 @@ public class GameManager {
         while (true) {
             System.out.print("숫자를 입력해주세요 : ");
             String answer = Console.readLine();
-
             List<Integer> answerNumber = answerStringToNumber(answer);
-            break;
+
+            if (isMatched(answerNumber, correctNumber)) {
+                break;
+            }
         }
+    }
+
+    private boolean isMatched(List<Integer> answerNumber, List<Integer> correctNumber) {
+        int ball = 0;
+        int strike = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (Objects.equals(answerNumber.get(i), correctNumber.get(j))) {
+                    if (i == j) strike++;
+                    else ball++;
+                }
+            }
+        }
+
+        if (ball == 0 && strike == 0) {
+            System.out.println("낫싱");
+            return false;
+        }
+        if (ball > 0)
+            System.out.print(ball + "볼");
+        if (ball > 0 && strike > 0)
+            System.out.print(" ");
+        if (strike > 0)
+            System.out.print(strike + "스트라이크");
+        System.out.println();
+
+        return strike == 3;
     }
 
     private List<Integer> answerStringToNumber(String answer) {
         validateAnswerLength(answer);
+
         List<Integer> answerNumber = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
             char answerChar = answer.charAt(i);
             validateAnswerChar(answerChar);
-            answerNumber.add((int) answer.charAt(i));
+
+            answerNumber.add((int) answer.charAt(i) - 48);
         }
         return answerNumber;
     }
@@ -42,7 +75,7 @@ public class GameManager {
     }
 
     private void validateAnswerLength(String answer) {
-        if (answer.length() > 3)
+        if (answer.length() != 3)
             throw new IllegalArgumentException();
     }
 
