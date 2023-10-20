23db3106b00b1e355a4c3ad32d9e135a9caf6786
mizuirohoshi7 <mizuirohoshi7@gmail.com>
2023-10-20 17:27:08 +0900

    feat(GameResult): 스트라이크 온리, 볼 온리, 낫싱 여부 판단

--- src/main/java/baseball/dto/GameResult.java
+++ src/main/java/baseball/dto/GameResult.java
@@ -5,4 +5,16 @@ public record GameResult(int strike, int ball) {
     public boolean isClear() {
         return strike == 3;
     }
+
+    public boolean isNothing() {
+        return strike == 0 && ball == 0;
+    }
+
+    public boolean isStrikeOnly() {
+        return !isNothing() && ball == 0;
+    }
+
+    public boolean isBallOnly() {
+        return !isNothing() && strike == 0;
+    }
 }
