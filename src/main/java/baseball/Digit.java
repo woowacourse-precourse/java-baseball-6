59b94e90fe9a5a6bc77b50b257129cbbc9fc3198
yubin <dbqls2821@gmail.com>
2023-10-20 23:42:03 +0900

    refactor: Digit에 EqualsAndHashcode 추가

--- src/main/java/baseball/Digit.java
+++ src/main/java/baseball/Digit.java
@@ -2,11 +2,30 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Objects;
+
 class Digit {
 
-    final int digit;
+    private final int digit;
 
     Digit() {
         this.digit = Randoms.pickNumberInRange(1, 9);
+    }
+
+    Digit(char target) {
+        this.digit = target;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Digit target = (Digit) o;
+        return digit == target.digit;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(digit);
     }
 }
