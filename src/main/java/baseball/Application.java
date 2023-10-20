bca4b7ad0274424aba316e5c92bf0213279aa89e
yummygyudon <bang2brew@gmail.com>
2023-10-21 03:02:08 +0900

    FEAT : 게임 실행 어플리케이션 클래스 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,20 @@
 package baseball;
 
+import baseball.application.GameLauncher;
+import baseball.domain.Computer;
+import baseball.util.channel.Printer;
+import baseball.util.channel.Reader;
+import baseball.util.enums.Command;
+import baseball.util.enums.GameFlag;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        do {
+            Computer computer = new Computer();
+            GameLauncher launcher = new GameLauncher(computer);
+            Printer.print(Command.START_GAME.getCommand());
+            launcher.execute();
+        } while (Reader.read(Command.ASK_RESUME).equals(GameFlag.RETRY));
     }
 }
