999eb0243e76f5485ef6672b7e2d2d36bac226d4
cloudchamb3r <jizon0123@protonmail.com>
2023-10-20 10:52:06 +0900

    misc: remove dup in StringUtil

--- src/main/java/baseball/util/StringUtil.java
+++ src/main/java/baseball/util/StringUtil.java
@@ -1,22 +1,22 @@
 package baseball.util;
 
 import static java.util.stream.Collectors.toList;
-import static java.util.stream.Collectors.toSet;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class StringUtil {
-    public static Set<Character> stringToSetOfChar(String s) {
-        var chars = s.chars();
-        var objChars = chars.mapToObj(c -> (char) c);
-        return objChars.collect(toSet());
-    }
+
 
     public static List<Character> stringToListOfChar(String s) {
         var chars = s.chars();
         var objChars = chars.mapToObj(c -> (char) c);
         return objChars.collect(toList());
+    }
+
+    public static Set<Character> stringToSetOfChar(String s) {
+        return new HashSet<>(stringToListOfChar(s));
     }
 
     public static List<Integer> digitStringToListOfInt(String s) {
