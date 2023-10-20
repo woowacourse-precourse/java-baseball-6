01a9c56820c1b03a6950bdcffcbcc4286c683896
장기환 <jang-gihwan@jang-gihwan-ui-MacBookPro.local>
2023-10-20 13:38:36 +0900

    feat : RQ-002 Computer 정답 생성

--- src/main/java/baseball/Model/Computer.java
+++ src/main/java/baseball/Model/Computer.java
@@ -10,9 +10,16 @@ public class Computer {
     }
 
     private void makeAnswer() {
+        // 정답에 선택 된 값의 상태 Map 
+        boolean[] pickedMap = new boolean[10];
         answer = new int[3];
+
         for(int i = 0; i < 3; ++i) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if(!pickedMap[randomNumber]) {
+                pickedMap[randomNumber] = true;
+                answer[i] = randomNumber;
+            }
         }
     }
 
