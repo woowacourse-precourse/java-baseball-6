2c05f00bf9f7448ba6d60bc6ccf32eb1509fb43d
jy016011 <39334789+jy016011@users.noreply.github.com>
2023-10-20 19:08:16 +0900

    1볼, 2볼 테스트케이스 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -13,8 +13,8 @@ class ApplicationTest extends NsTest {
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
                 () -> {
-                    run("246", "135", "1", "597", "589", "2");
-                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+                    run("246", "135", "1", "597", "125", "925", "589", "2");
+                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "1볼", "2볼", "3스트라이크", "게임 종료");
                 },
                 1, 3, 5, 5, 8, 9
         );
