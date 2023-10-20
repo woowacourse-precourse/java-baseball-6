fe3f6fd476347e5cdfa574e06940020d3b137aa5
Seonghwan <aiya0422@gmail.com>
2023-10-20 15:32:59 +0900

    REFACTOR: showResult의 else 예약어 제거
    클린코드의 else 예약어를 사용하지 않는다는 규칙을 적용하기 위해
    else 예약어를 제거하고 return를 추가했습니다.

--- src/main/java/baseball/OutputView.java
+++ src/main/java/baseball/OutputView.java
@@ -15,12 +15,16 @@ public class OutputView {
     public static void showResult(int strike, int ball) {
         if (strike == 0 && ball == 0) {
             System.out.println("낫싱");
-        } else if (strike == 0) {
-            System.out.println(ball + "볼");
-        } else if (ball == 0) {
-            System.out.println(strike + "스트라이크");
-        } else {
-            System.out.println(ball + "볼 " + strike + "스트라이크");
+            return;
         }
+        if (strike == 0) {
+            System.out.println(ball + "볼");
+            return;
+        }
+        if (ball == 0) {
+            System.out.println(strike + "스트라이크");
+            return;
+        }
+        System.out.println(ball + "볼 " + strike + "스트라이크");
     }
 }
