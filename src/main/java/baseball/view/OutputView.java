ee374514c97b741197c3730d5e000aba8bd23690
YongHyeon <pyh4616@naver.com>
2023-10-21 05:08:06 +0900

    Feat: OutputView, 게임 종료 메시지, 게임을 추가 진행 여부를 확인하는 메시지를 출력하는 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,8 +3,15 @@ package baseball.view;
 public class OutputView {
 
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String GAME_OVER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String GAME_CONTINUE_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public void printGameStartMessage() {
         System.out.println(GAME_START_MESSAGE);
+    }
+
+    public void printGameOverAndContinueMessage() {
+        System.out.println(GAME_OVER_MESSAGE);
+        System.out.println(GAME_CONTINUE_MESSAGE);
     }
 }
