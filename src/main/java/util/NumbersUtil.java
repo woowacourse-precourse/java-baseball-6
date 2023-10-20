240518e25b58fca081967b21772f6053463ec809
song-jae-wan <sjo9810@gmail.com>
2023-10-20 10:47:32 +0900

    Feat: 수열을 관리하는 NumbersUtil 클래스 구현
    필드로 관리할 수열, 수열의 유효성 배열 선언
    생성자 : 메모리에 배열 공간 할당
    clear() : 수열 초기화
    setNumbers() : 수열 생성
    ballCount() : 인자로 받는 수열과 수열을 비교 후 카운트한 값 리턴

---
+++ src/main/java/util/NumbersUtil.java
@@ -0,0 +1,49 @@
+package util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class NumbersUtil {
+
+    private final int[] numbers;
+    private final boolean[] numberFlag;
+
+    NumbersUtil() {
+        numbers = new int[3];
+        numberFlag = new boolean[10];
+    }
+
+    public void clear() {
+        for(int i = 0; i < 3; i++) {
+            numberFlag[numbers[i]] = false;
+        }
+    }
+
+    public void setNumbers() {
+        setNumbers(0);
+    }
+
+    private void setNumbers(int idx) {
+        if(idx == 3) { return; }
+        int number = Randoms.pickNumberInRange(0, 8) + 1;
+        if(numberFlag[number]) {
+            setNumbers(idx);
+        } else {
+            numberFlag[number] = true;
+            numbers[idx] = number;
+            setNumbers(idx + 1);
+        }
+    }
+
+    public int[] ballCount(int[] numbers) {
+        int[] ballStrike = new int[2];
+        for(int i = 0; i < 3; i++) {
+            if(this.numbers[i] == numbers[i]) {
+                ballStrike[1]++;
+            }else if(numberFlag[numbers[i]]) {
+                ballStrike[0]++;
+            }
+        }
+        return ballStrike;
+    }
+
+}
