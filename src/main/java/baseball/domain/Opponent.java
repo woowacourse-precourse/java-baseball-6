0deb651588fbeb76344716b61a8bf5fad38f84be
SehyeonKang <gedfgrdt@naver.com>
2023-10-20 12:36:57 +0900

    feat(Opponent): 기본 생성자에 랜덤 정답값 생성 기능 추가

--- src/main/java/baseball/domain/Opponent.java
+++ src/main/java/baseball/domain/Opponent.java
@@ -1,5 +1,7 @@
 package baseball.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +11,13 @@ public class Opponent {
 
     public Opponent() {
         answerNumbers = new ArrayList<>();
+        while (answerNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+
+            if (!answerNumbers.contains(randomNumber)) {
+                answerNumbers.add(randomNumber);
+            }
+        }
     }
 
     public List<Integer> getAnswerNumbers() {
