59dfc5ca39253fbfbf8570b8d05b1732ee834d48
dy102 <132787602+dy102@users.noreply.github.com>
2023-10-21 00:23:01 +0900

    Refactor: 매직넘버를 상수로 교체

--- src/main/java/baseball/BallAndStrikeCount.java
+++ src/main/java/baseball/BallAndStrikeCount.java
@@ -4,12 +4,18 @@ import java.util.List;
 
 public class BallAndStrikeCount {
     private int ballCount;
+
     private int strikeCount;
+
+    private static final int THREE_NUMBER = 3;
+
+    private static final int THREE_STRIKE = 3;
 
     public void compare(List<Integer> randomNumber, List<Integer> userNumber) {
         ballCount = 0;
         strikeCount = 0;
-        for (int i = 0; i < 3; i++) {
+
+        for (int i = 0; i < THREE_NUMBER; i++) {
             if (randomNumber.contains(userNumber.get(i))) {
                 ballCount++;
             }
@@ -21,7 +27,9 @@ public class BallAndStrikeCount {
     }
 
     public boolean checkStrike() {
-        if (strikeCount == 3) {
+        if (strikeCount == THREE_STRIKE) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             return true;
         }
         return false;
@@ -29,13 +37,13 @@ public class BallAndStrikeCount {
 
     public void showStatus() {
         if (ballCount == 0 && strikeCount == 0) {
-            System.out.println("\n낫싱");
+            System.out.println("낫싱");
         } else if (ballCount == 0 && strikeCount != 0) {
-            System.out.println("\n" + strikeCount + "스트라이크");
+            System.out.println(strikeCount + "스트라이크");
         } else if (ballCount != 0 && strikeCount == 0) {
-            System.out.println("\n" + ballCount + "볼");
+            System.out.println(ballCount + "볼");
         } else if (ballCount != 0 && strikeCount != 0) {
-            System.out.println("\n" + ballCount + "볼" + " " + strikeCount + "스트라이크");
+            System.out.println(ballCount + "볼" + " " + strikeCount + "스트라이크");
         }
     }
 }
