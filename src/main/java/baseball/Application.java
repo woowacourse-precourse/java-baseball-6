0e45cb0b61b4331cb3f43a7a4a17edb614a319c0
Chris <hereokay88@gmail.com>
2023-10-20 19:39:40 +0900

    feat: 사용자 입력능 및 게임 재시작기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        int execInput = 1;
+        while (execInput == 1){
+            System.out.print("숫자를 입력해주세요 : ");
+            String number = Console.readLine();
+
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            execInput = Integer.parseInt(Console.readLine());
+
+            if (execInput == 1 || execInput == 2){
+                Exception IllegalArgumentException;
+            }
+
+        }
     }
 }
