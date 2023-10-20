128182aa4c36e97cd74af2026789bf9949434f8b
pwrwpw <dlgustjr9912@gmail.com>
2023-10-20 16:31:10 +0900

    refactor(InputView): 변수 위치를 파일명에 맞춰 변경 refactor(OutputView): 변수 위치를 파일명에 맞춰 변경

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -7,11 +7,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class InputView {
-	private static final String INPUT_NUMBER = "숫자를 입력해주세요 : ";
-
 
 	public String readNumber() {
-		System.out.print(INPUT_NUMBER);
+		OutputView.printInputNumber();
 		return Console.readLine();
 	}
 
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -2,6 +2,7 @@ package baseball.view;
 
 public class OutputView {
 	private static final String MESSAGE_GAME_START = "숫자 야구 게임을 시작합니다.";
+	private static final String MESSAGE_INPUT_NUMBER = "숫자를 입력해주세요 : ";
 	private static final String MESSAGE_GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 	public void printGameStart() {
 		System.out.println(MESSAGE_GAME_START);
@@ -9,4 +10,8 @@ public class OutputView {
 	public void printGameEnd() {
 		System.out.println(MESSAGE_GAME_END);
 	}
+	public static void printInputNumber() {
+		System.out.print(MESSAGE_INPUT_NUMBER);
+	}
+
 }
