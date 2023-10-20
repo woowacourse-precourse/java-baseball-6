59b2ae6252a47114b6bef92361d3a6593df10017
권도현 <tobeykwon@gmail.com>
2023-10-20 16:19:08 +0900

    feat : 프로그램 실행 로직 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import baseball.controller.GameController;
+import baseball.view.GameView;
+
 public class Application {
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameController client = new GameController(new GameView());
+        client.run();
     }
 }
