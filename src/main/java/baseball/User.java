73f1968aa7536025f899dbdc36735feee905daf3
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-10-20 17:17:34 +0900

    feat: Update User.java

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,5 +1,22 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
 
+    private final List<Integer> user = new ArrayList<>();
+
+    public void getIntArray(String input) {
+        String[] temp = input.split("");
+        for (String str : temp) {
+            if (!user.contains(Integer.parseInt(str))) {
+                user.add(Integer.parseInt(str));
+            }
+        }
+    }
+
+    public void clearList() {
+        user.clear();
+    }
 }
