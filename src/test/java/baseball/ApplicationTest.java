ec9ad08e9e030c6010429e55d285a4d2aa7bf6b0
gunwoong <gwj0421@gmail.com>
2023-10-20 21:31:44 +0900

    test : 랜덤한 숫자 생성 기능 테스트

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -3,7 +3,10 @@ package baseball;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
-import static baseball.Application.*;
+import java.util.HashSet;
+import java.util.List;
+
+import static baseball.Application.BaseBallGame;
 import static baseball.Application.main;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -11,11 +14,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
-
     @Test
     void makeBaseBallGameObject() {
         BaseBallGame baseBallGame = new BaseBallGame();
         assertThat(baseBallGame).isNotNull();
+    }
+
+    @Test
+    void makeRandomNumbers() {
+        // given
+        BaseBallGame baseBallGame = new BaseBallGame();
+
+        // when
+        List<Integer> computerNumbers = baseBallGame.getComputerNumbers();
+        HashSet<Integer> computerNumbersSet = new HashSet<>(computerNumbers);
+
+        // then
+        for (Integer computerNumber : computerNumbers) {
+            assertThat(computerNumber).isPositive()
+                    .isLessThanOrEqualTo(9);
+        }
+        assertThat(computerNumbersSet).hasSameSizeAs(computerNumbers);
     }
 
     @Test
