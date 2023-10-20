8b4c1deffd903e3a3c0e2fdb82064a4c98e7f478
Hyun Woo <hwsa10041@gmail.com>
2023-10-20 19:42:04 +0900

    test : inputRestartChoice() 테스트 작성

--- src/test/java/baseball/view/InputViewTest.java
+++ src/test/java/baseball/view/InputViewTest.java
@@ -19,10 +19,24 @@ class InputViewTest {
         System.setIn(inContent);
 
         // when
-        String s = inputView.inputNumber();
+        String input = inputView.inputNumber();
 
         // then
-        assertThat(s).isEqualTo("123");
+        assertThat(input).isEqualTo("123");
+    }
+
+    @DisplayName("재시작 여부를 입력받는다.")
+    @Test
+    void inputRestartChoice() {
+        // given
+        ByteArrayInputStream inContent = new ByteArrayInputStream("1".getBytes());
+        System.setIn(inContent);
+
+        // when
+        int input = inputView.inputRestartChoice();
+
+        // then
+        assertThat(input).isEqualTo(1);
     }
 
 }
