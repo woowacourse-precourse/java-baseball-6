cb312f5e68be805de53bd9b55b10667fbc506ee7
Jae-Young98 <ygy0102@gmail.com>
2023-10-21 03:07:45 +0900

    feat(InputValidator): 입력된 숫자를 리스트로 변환 하는 기능 추가

--- src/main/java/baseball/logic/InputValidator.java
+++ src/main/java/baseball/logic/InputValidator.java
@@ -1,5 +1,8 @@
 package baseball.logic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputValidator {
 
     public void validateLength(String inputNumbers) {
@@ -7,4 +10,13 @@ public class InputValidator {
             throw new IllegalArgumentException("입력한 숫자의 길이를 확인하세요.");
         }
     }
+
+    public List<Integer> convertStrToList(String inputNumbers) {
+        List<Integer> numberList = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            numberList.add((int) inputNumbers.charAt(i));
+        }
+        return numberList;
+    }
+    
 }
