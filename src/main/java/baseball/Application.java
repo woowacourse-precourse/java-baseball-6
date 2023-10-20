77c8f18a1aa5c882d9b23e8f8edacc2449e76075
LastCow9000 <sys19912002@hanmail.net>
2023-10-21 02:03:16 +0900

    feat: 숫자 입력 받는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,7 +10,7 @@ public class Application {
     public static void main(String[] args) {
         startBaseballGame();
     }
-    
+
     public static void startBaseballGame() {
         Queue<Integer> balls = new LinkedList<>();
         while (balls.size() < 3) {
@@ -24,5 +24,37 @@ public class Application {
         balls.forEach(System.out::println);
 
         System.out.println("숫자 야구 게임을 시작합니다.");
+
+        //입력 받기
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+        validateInput(input);
+    }
+
+    public static void validateInput(String input){
+        if (!is3Length(input) || isNan(input) || isLessThan1(input) || isDuplicate(input)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static boolean is3Length(String data) {
+        return data.length() == 3;
+    }
+    public static boolean isNan(String data) {
+        try {
+            Integer.parseInt(data);
+        } catch (NumberFormatException exception) {
+            return true;
+        }
+        return false;
+    }
+    public static boolean isLessThan1(String data) {
+        String[] splitedData = data.split("");
+        return Arrays.stream(splitedData).map(Integer::parseInt).anyMatch((number)-> number < 1);
+    }
+
+    public static boolean isDuplicate(String data) {
+        long count = data.chars().distinct().count();
+        return count != 3;
     }
 }
