4eb41af547995f6b0889df828067303093263163
TaeHye0n <thk001229@gmail.com>
2023-10-20 20:04:51 +0900

    feat: 컴퓨터가 1 ~ 9 사이 중복없이 3개의 숫자를 뽑는 기능  - 매직 넘버 대신 상수 사용  - 외부에서 컬렉션의 수정을 막기 위해 생성자에서 Wrapping  - 랜덤 숫자 리스트를 생성하는 private generateRandomNumbers 메서드 사용

---
+++ src/main/java/baseball/domain/ComputerRandomNumbers.java
@@ -0,0 +1,34 @@
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComputerRandomNumbers {
+
+    private static final int MAX_NUMBER_SIZE = 3;
+    private static final int BEGIN_NUMBER = 1;
+    private static final int END_NUMBER = 9;
+
+    private final List<Integer> randomNumbers;
+
+    // 컬렉션의 Wrapping을 생성자에서만 담당
+    public ComputerRandomNumbers() {
+        this.randomNumbers = generateRandomNumbers();
+    }
+
+    // 주어진 숫자 사이에서 중복을 허용하지 않고 MAX_NUMBER_SIZE 만큼 뽑음
+    private List<Integer> generateRandomNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < MAX_NUMBER_SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(BEGIN_NUMBER, END_NUMBER);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+        return numbers;
+    }
+
+
+}
