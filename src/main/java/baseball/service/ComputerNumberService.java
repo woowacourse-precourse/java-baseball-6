1c8b9a9c7bc3c459cbdf6da2fca47bf7b875c539
Hyeon0208 <dmsgktn0208@gmail.com>
2023-10-20 15:54:55 +0900

    refactor : convertSetToString() 메서드 ConversionService로 이동

--- src/main/java/baseball/service/ComputerNumberService.java
+++ src/main/java/baseball/service/ComputerNumberService.java
@@ -3,28 +3,18 @@ package baseball.service;
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 import baseball.domain.ComputerNumber;
+import baseball.view.GameValue;
 import java.util.HashSet;
 import java.util.Set;
 
 public class ComputerNumberService {
-    private static final int NUMBER_SIZE = 3;
-    private static final int MIN_RANGE = 1;
-    private static final int MAX_RANGE = 9;
 
     public ComputerNumber generateComputerNumber() {
         Set<Integer> numbers = new HashSet<>();
-        while (numbers.size() < NUMBER_SIZE) {
-            int number = pickNumberInRange(MIN_RANGE, MAX_RANGE);
+        while (numbers.size() < GameValue.NUMBER_SIZE.getValue()) {
+            int number = pickNumberInRange(GameValue.MIN_RANGE.getValue(), GameValue.MAX_RANGE.getValue());
             numbers.add(number);
         }
-        return new ComputerNumber(convertSetToString(numbers));
-    }
-
-    public String convertSetToString(Set<Integer> numbers) {
-        StringBuilder sb = new StringBuilder();
-        for (Integer number : numbers) {
-            sb.append(number);
-        }
-        return sb.toString();
+        return new ComputerNumber(ConversionService.convertSetToString(numbers));
     }
 }
