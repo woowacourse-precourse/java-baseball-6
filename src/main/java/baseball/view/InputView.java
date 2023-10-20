7a9c4f8bf75931a79c494c6802caa3c4c9d0ad4e
waveofmymind <sjun0913@gmail.com>
2023-10-20 23:52:11 +0900

    refactor: InputView에서는 입력을 받는 책임만 가지도록 변경한다.

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -5,9 +5,6 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    private static final String EXIT_COMMAND = "2";
-    private static final String RESTART_COMMAND = "1";
-
     public static Numbers readUserNumbers() {
         System.out.println("숫자 야구 게임을 시작합니다.");
         System.out.print("숫자를 입력해주세요 : ");
@@ -16,16 +13,8 @@ public class InputView {
         return new Numbers(input);
     }
 
-    public static boolean readContinueOrExit() {
+    public static String readRestartOrExit() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        String input = Console.readLine();
-
-        if (input.equals(EXIT_COMMAND)) {
-            return false;
-        } else if (input.equals(RESTART_COMMAND)) {
-            return true;
-        } else {
-            throw new IllegalArgumentException("1 또는 2를 입력해주세요.");
-        }
+        return Console.readLine();
     }
 }
