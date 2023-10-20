3edd20c2abc3491eb50437422d13ab96d1ef1f26
jy016011 <39334789+jy016011@users.noreply.github.com>
2023-10-20 13:05:05 +0900

    볼과 스트라이크 수를 기억하는 Score ver1.0 구현

---
+++ src/main/java/baseball/models/Score.java
@@ -0,0 +1,10 @@
+package baseball.models;
+
+public class Score {
+    public int ballCount;
+    public int strikeCount;
+    public Score(int ballCount, int strikeCount){
+        this.ballCount = ballCount;
+        this.strikeCount = strikeCount;
+    }
+}
