359bd1c2f75226ed3609dd4ff998612b67396364
mizuirohoshi7 <mizuirohoshi7@gmail.com>
2023-10-20 16:01:21 +0900

    test(GameNumber): 테스트 생성

--- src/main/java/baseball/domain/GameNumber.java
+++ src/main/java/baseball/domain/GameNumber.java
@@ -1,4 +1,15 @@
 package baseball.domain;
 
+import java.util.List;
+
 public class GameNumber {
+    private static final int NUMBER_LENGTH = 3;
+    private static final int MIN = 1;
+    private static final int MAX = 9;
+
+    private final List<Integer> numbers;
+
+    public GameNumber(String input) {
+        this.numbers = null;
+    }
 }
