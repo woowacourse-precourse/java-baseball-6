adad2895d55707b6e67d297f9487766a297f1244
kunsanglee <gosmdochee2@gmail.com>
2023-10-20 18:45:37 +0900

    refactor(FixedAnswerGenerator): generate() 메서드 오버라이딩 지정값 반환하게 변경

--- src/test/java/baseball/FixedAnswerGenerator.java
+++ src/test/java/baseball/FixedAnswerGenerator.java
@@ -1,18 +1,10 @@
 package baseball;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class FixedAnswerGenerator implements AnswerGenerator {
     @Override
     public List<Integer> generate() {
-        return List.of(1, 2, 3);
-    }
-
-    public List<Integer> generate(String input) {
-        String validatedInput = InputValidator.validateBaseBallNumber(input);
-        return validatedInput.chars()
-                .mapToObj(Character::getNumericValue)
-                .collect(Collectors.toList());
+        return List.of(7, 1, 3);
     }
 }
