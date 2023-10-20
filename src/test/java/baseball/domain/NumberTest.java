34e56f8138e1ba51e1f66c8a29a9ec86596f35df
elyudwo <dudqk9696@naver.com>
2023-10-20 18:23:09 +0900

    [Test] : 숫자가 1~9 사이가 아닌 경우 예외 처리 테스트 코드 작성

--- src/test/java/baseball/domain/NumberTest.java
+++ src/test/java/baseball/domain/NumberTest.java
@@ -27,8 +27,20 @@ class NumberTest {
     void validateDuplicateNumber() {
         // given
         List<Integer> numberList = List.of(1,1,2);
+
         //when, then
         assertThatThrownBy(() -> number.validateDuplicateNumber(numberList))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("숫자가 1~9 사이가 아닐 경우 예외가 발생한다.")
+    @Test
+    void validateNumberLength() {
+        // given
+        List<Integer> numberList = List.of(0,1,2);
+
+        //when, then
+        assertThatThrownBy(() -> number.validateNumberLength(numberList))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
