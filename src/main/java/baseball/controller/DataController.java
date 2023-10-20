b831df96cfe92b615a20fdd7d2e6b66cc9eff2dd
JunSeok Kim <skaksdl1238@gmail.com>
2023-10-20 15:54:37 +0900

    feat - DataController 구현

---
+++ src/main/java/baseball/controller/DataController.java
@@ -0,0 +1,33 @@
+package baseball.controller;
+
+import static camp.nextstep.edu.missionutils.Console.*;
+
+import baseball.domain.Balls;
+import baseball.exception.DataValidator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataController {
+
+    private static final int BALL_MAX_SIZE = 3;
+    private static final String EMPTY_STRING = "";
+
+    private List<String> readValues() {
+        String value = readLine();
+
+        DataValidator dataValidator = new DataValidator();
+        dataValidator.validateInputNumber(value);
+
+        return List.of(value.split(EMPTY_STRING));
+    }
+
+    public List<Balls> generatePlayerNumbers() {
+        List<String> values = readValues();
+        List<Balls> ballsList = new ArrayList<>();
+        for (int i = 0; i < BALL_MAX_SIZE; i++) {
+            ballsList.add(new Balls(Integer.parseInt(values.get(i)), i));
+        }
+        return ballsList;
+    }
+}
