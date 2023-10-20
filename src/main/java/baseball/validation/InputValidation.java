a5c17585b145422769f514ed81af44bf3a108e0e
acs0209 <annjae4676@naver.com>
2023-10-20 14:02:04 +0900

    feat: validate about input string consists solely of natural numbers

---
+++ src/main/java/baseball/validation/InputValidation.java
@@ -0,0 +1,22 @@
+package baseball.validation;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 입력값에 대한 검증을 진행하는 클래스
+ */
+public class InputValidation {
+
+    /**
+     * 사용자의 숫자 입력값이 자연수로만 이루어져 있는지 검증한다.
+     */
+    public static void validateNaturalNumber(String input) {
+        Pattern pattern = Pattern.compile("^[0-9]+$");
+        Matcher matcher = pattern.matcher(input);
+
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("입력된 문자열은 자연수로만 이루어져야 합니다.");
+        }
+    }
+}
