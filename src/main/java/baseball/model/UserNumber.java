dad7b8e4c9458c857300c52e2a65e1c41d5f2a0e
shAn-kor <53287605+shAn-kor@users.noreply.github.com>
2023-10-20 22:34:19 +0900

    userNUmber constructor & getIn

--- src/main/java/baseball/model/UserNumber.java
+++ src/main/java/baseball/model/UserNumber.java
@@ -1,4 +1,15 @@
 package baseball.model;
 
+import java.util.List;
+
 public class UserNumber {
+    private List<Integer> inputNumber;
+
+    public UserNumber(List<Integer> inputNumber) {
+        this.inputNumber = inputNumber;
+    }
+
+    /*public List<Integer> getInputNumber() {
+        return this.inputNumber;
+    }*/
 }
