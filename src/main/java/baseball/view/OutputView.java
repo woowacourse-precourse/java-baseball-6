f80913dbacf55ee605abd33c402c49ad208f58a4
soochan <tncksdl05@gmail.com>
2023-10-20 19:18:37 +0900

    refactor: 게임 결과 출력 로직 메서드 분리

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -9,6 +9,9 @@ public class OutputView {
     private static final String NOTHING_MESSAGE = "낫싱";
     private static final String BALL_COUNT_MESSAGE_FORMAT = "%d볼 ";
     private static final String STRIKE_COUNT_MESSAGE_FORMAT = "%d스트라이크";
+    private static final int ZERO_STRIKE_COUNT = 0;
+    private static final int ZERO_BALL_COUNT = 0;
+    private static final int THREE_STRIKE_COUNT = 3;
 
     private OutputView() {
     }
@@ -24,26 +27,45 @@ public class OutputView {
     public void printGameResult(GameResultDto gameResultDto) {
         int ballCount = gameResultDto.getBallCount();
         int strikeCount = gameResultDto.getStrikeCount();
-
-        if (ballCount == 0 && strikeCount == 0) {
+        if (isNothing(ballCount, strikeCount)) {
             System.out.println(NOTHING_MESSAGE);
             return;
         }
+        printResult(ballCount, strikeCount);
+    }
 
-        if (ballCount > 0) {
+    private void printResult(int ballCount, int strikeCount) {
+        printIfHasBall(ballCount);
+        printIfHasStrike(ballCount, strikeCount);
+    }
+
+    private void printIfHasStrike(int ballCount, int strikeCount) {
+        if (strikeCount > ZERO_STRIKE_COUNT) {
+            System.out.printf(STRIKE_COUNT_MESSAGE_FORMAT, strikeCount);
+            printIfEndingMessageIfAllStrike(ballCount, strikeCount);
+        }
+        printEmptyLine();
+    }
+
+    private void printIfEndingMessageIfAllStrike(int ballCount, int strikeCount) {
+        if (ballCount == ZERO_BALL_COUNT && strikeCount == THREE_STRIKE_COUNT) {
+            printEmptyLine();
+            System.out.print(GAME_END_MESSAGE);
+        }
+    }
+
+    private void printEmptyLine() {
+        System.out.println();
+    }
+
+    private void printIfHasBall(int ballCount) {
+        if (ballCount > ZERO_BALL_COUNT) {
             System.out.printf(BALL_COUNT_MESSAGE_FORMAT, ballCount);
         }
+    }
 
-        if (strikeCount > 0) {
-            System.out.printf(STRIKE_COUNT_MESSAGE_FORMAT, strikeCount);
-
-            if (ballCount == 0 && strikeCount == 3) {
-                System.out.println();
-                System.out.print(GAME_END_MESSAGE);
-            }
-        }
-
-        System.out.println();
+    private boolean isNothing(int ballCount, int strikeCount) {
+        return ballCount == ZERO_BALL_COUNT && strikeCount == ZERO_STRIKE_COUNT;
     }
 
     private static class LazyHolder {
