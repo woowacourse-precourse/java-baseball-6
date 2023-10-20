b4138565a689a99630a00adb4a1ec2732c3445b0
Octoping <mooner925@gmail.com>
2023-10-20 21:20:50 +0900

    feat: judgeResult에 3스트라이크인지 확인하는 함수 추가

--- src/main/java/baseball/domain/vo/JudgeResult.java
+++ src/main/java/baseball/domain/vo/JudgeResult.java
@@ -7,4 +7,8 @@ public record JudgeResult(
     public boolean isNothing() {
         return strike == 0 && ball == 0;
     }
+
+    public boolean isThreeStrike() {
+        return strike == 3;
+    }
 }
--- src/test/java/baseball/domain/vo/JudgeResultTest.java
+++ src/test/java/baseball/domain/vo/JudgeResultTest.java
@@ -34,4 +34,28 @@ class JudgeResultTest {
         assertThat(judgeResult.isNothing()).isFalse();
     }
 
+    @DisplayName("JudgeResult는 스트라이크가 3일 때 isThreeStrike가 true를 반환한다.")
+    @Test
+    void three_strike_test() {
+        // given
+        int strike = 3;
+        int ball = 0;
+
+        // when
+        JudgeResult judgeResult = new JudgeResult(strike, ball);
+
+        // then
+        assertThat(judgeResult.isThreeStrike()).isTrue();
+    }
+
+    @DisplayName("JudgeResult는 스트라이크가 3이 아닐 때 isThreeStrike가 false를 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1, 0", "0, 1", "1, 2"})
+    void not_three_strike_test(int strike, int ball) {
+        // when
+        JudgeResult judgeResult = new JudgeResult(strike, ball);
+
+        // then
+        assertThat(judgeResult.isThreeStrike()).isFalse();
+    }
 }
