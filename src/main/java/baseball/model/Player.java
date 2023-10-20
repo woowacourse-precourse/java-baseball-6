1afd1829d535d1a31b1923687b6b5980a2acdffd
junsoo <choix712@umn.edu>
2023-10-20 16:01:18 +0900

    feat: 입력값으로 중복된 숫자를 받는 경우, 예외 조건 발생 후 프로그램 종료

--- src/main/java/baseball/model/Player.java
+++ src/main/java/baseball/model/Player.java
@@ -1,6 +1,8 @@
 package baseball.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Player {
 
@@ -22,6 +24,13 @@ public class Player {
     private void validateSize(List<Integer> player) {
         if (player.size() != PLAYER_NUMBERS_SIZE) {
             throw new IllegalArgumentException("플레이어는 3개의 숫자만 선택할 수 있습니다.");
+        }
+    }
+
+    private void validateDuplicate(List<Integer> player) {
+        Set<Integer> duplicateNumbers = new HashSet<>(player);
+        if (duplicateNumbers.size() != PLAYER_NUMBERS_SIZE) {
+            throw new IllegalArgumentException("컴퓨터는 중복된 숫자를 가질 수 없습니다.");
         }
     }
 }
