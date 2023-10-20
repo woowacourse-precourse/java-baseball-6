a1dba28b4bde41b57c801315a2f6a79326002c9e
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-10-20 17:51:10 +0900

    feat(error) : 각 자리 숫자가 1~9 값인지 확인 기능 구현 - checkNumberRangeValidate : 각 자리 숫자가 1~9값 확인 - checkUserInputValidate에 검사 결과 올바르지 않으면 메시지와 함께 IlleagalArgumentException 후 종료 -docs : Error -> Input -> Game의 항목 모두 체크

--- src/main/java/baseball/error/GameInputErrorException.java
+++ src/main/java/baseball/error/GameInputErrorException.java
@@ -3,17 +3,44 @@ package baseball.error;
 import java.util.*;
 
 public class GameInputErrorException implements ErrorException{
+    private final static int VALID_NUMBER_LENGTH = 3;
+    private final static int VALID_NUMBER_MIN_RANGE = 1;
+    private final static int VALID_NUMBER_MAX_RANGE = 9;
+    private final static String IS_NOT_NUMERIC_MESSAGE = "숫자만 입력이 가능합니다. 애플리케이션을 종료합니다.";
+    private final static String IS_NOT_THREE_DIGITS_MESSAGE = "3자리 숫자만 입력 가능합니다. 애플리케이션을 종료합니다.";
+    private final static String IS_NOT_VALID_RANGE_MESSAGE = "각 자릿 숫자는 1~9의 숫자만 입력 가능합니다. 애플리케이션을 종료합니다.";
+    private final static String EXIST_DUPLICATE_NUMBER = "각 자리에 서로 다른 숫자를 입력해야 합니다. 애플리케이션을 종료합니다.";
     @Override
-    public void checkUserInputValidate(String message) {
-        throw new IllegalArgumentException(message);
+    public void checkUserInputValidate(String checkNumber) {
+        if(!isNumeric(checkNumber)){
+            throw new IllegalArgumentException(IS_NOT_NUMERIC_MESSAGE);
+        }
+        if(!checkThreeDigits(checkNumber)){
+            throw new IllegalArgumentException(IS_NOT_THREE_DIGITS_MESSAGE);
+        }
+        if(!checkNumberRangeValidate(checkNumber)){
+            throw new IllegalArgumentException(IS_NOT_VALID_RANGE_MESSAGE);
+        }
+        if(!hasDuplicateNumber(checkNumber)){
+            throw new IllegalArgumentException(EXIST_DUPLICATE_NUMBER);
+        }
     }
 
     private static boolean isNumeric(String checkNumber) {
         return checkNumber.matches("-?\\d+(\\.\\d+)?");
     }
 
-    private static boolean isNumberRange(String checkNumber) {
-        return checkNumber.length() == 3;
+    private static boolean checkThreeDigits(String checkNumber) {
+        return checkNumber.length() == VALID_NUMBER_LENGTH;
+    }
+    private static boolean checkNumberRangeValidate(String checkNumber){
+        for(char c : checkNumber.toCharArray()){
+            int cToInt = (int) c;
+            if (VALID_NUMBER_MIN_RANGE > cToInt || VALID_NUMBER_MAX_RANGE < cToInt){
+                return true;
+            }
+        }
+        return false;
     }
 
     private static boolean hasDuplicateNumber(String checkNumber) {
