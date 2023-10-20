b65a8e8489dfeb16295e1c3f5df16f67551706df
Sully <2dcoder@naver.com>
2023-10-20 12:36:31 +0900

    refactor(RandomComputerNumberGenerator): 매직 넘버와 판별 부분은 NumberValidator에서 가져오도록 리팩토링

--- src/main/java/baseball/computer/RandomComputerNumberGenerator.java
+++ src/main/java/baseball/computer/RandomComputerNumberGenerator.java
@@ -1,5 +1,9 @@
 package baseball.computer;
 
+import static baseball.validator.NumberValidator.MAX_VALUE;
+import static baseball.validator.NumberValidator.MIN_VALUE;
+import static baseball.validator.NumberValidator.needsMoreNumbers;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -7,14 +11,13 @@ import java.util.stream.Collectors;
 
 public class RandomComputerNumberGenerator {
 
-    private static final int LENGTH = 3;
     private final Set<Number> numbers;
 
     public RandomComputerNumberGenerator() {
         this.numbers = new LinkedHashSet<>();
 
-        while (numbers.size() < LENGTH) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+        while (needsMoreNumbers(numbers.size())) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_VALUE, MAX_VALUE);
             numbers.add(new Number(randomNumber));
         }
     }
--- src/main/java/baseball/validator/NumberValidator.java
+++ src/main/java/baseball/validator/NumberValidator.java
@@ -2,8 +2,9 @@ package baseball.validator;
 
 public class NumberValidator {
 
-    private static final int MIN_VALUE = 1;
-    private static final int MAX_VALUE = 9;
+    public static final int LENGTH = 3;
+    public static final int MIN_VALUE = 1;
+    public static final int MAX_VALUE = 9;
 
     private NumberValidator() {
         throw new IllegalArgumentException("NumberValidator(): 인스턴스 생성 불가능");
@@ -15,11 +16,8 @@ public class NumberValidator {
         }
     }
 
-    /**
-     * (예외 처리) 1부터 9까지 숫자로 구성 (0 불가능)
-     * (예외 처리) 입력 값은 3자리의 숫자 (양의 정수)
-     * (예외 처리) 0 이하의 정수 불가능 (양의 정수니)
-     * (예외 처리) 각 숫자는 중복 불가능 (겹칠 수 없음)
-     */
+    public static boolean needsMoreNumbers(int size) {
+        return size < LENGTH;
+    }
 
 }
