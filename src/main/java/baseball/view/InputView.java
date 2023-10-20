cf07d2bf259d3a3af3f620db26d4e1f06eb2c155
soozzang <soozzang@inha.edu>
2023-10-20 17:47:11 +0900

    Feat: readPlayerNum에 예외처리 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,9 +1,14 @@
 package baseball.view;
+
+import baseball.exception.BaseballGameException;
 import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     public int readPlayerNum() {
-        int player = Integer.parseInt(Console.readLine());
-        //예외처리 필요
-        return player;
+        BaseballGameException baseballGameException = new BaseballGameException();
+        String playerNumStr = Console.readLine();
+        baseballGameException.validPlayerNumIsInt(playerNumStr);
+        baseballGameException.validPlayerNumLenCount(playerNumStr);
+        baseballGameException.validPlayerNumIsDuplicate(playerNumStr);
+        return Integer.parseInt(playerNumStr);
     }
 }
