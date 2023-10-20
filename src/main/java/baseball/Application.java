7c298d64f350341f5c8d309463e70aea216dc118
linirini <2001yerin@naver.com>
2023-10-20 18:02:00 +0900

    feat : implement computer number generation feature

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,31 @@
 package baseball;
 
-public class Application {
-    public static void main(String[] args) {
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Application{
+
+    private static List<Integer> computer = new ArrayList<>();
+
+    public static void main(String[] args){
         startGame();
+        computer = createComputerRandomNumber();
     }
+
     private static void startGame(){
         System.out.println("숫자 야구 게임을 시작합니다.");
-        //TODO : 컴퓨터 랜덤 숫자 생성
     }
+
+    private static List<Integer> createComputerRandomNumber(){
+        List<Integer> randomNumbers = new ArrayList<>();
+        while (randomNumbers.size() < 3){
+            int number = Randoms.pickNumberInRange(1, 9);
+            if (!randomNumbers.contains(number)){
+                randomNumbers.add(number);
+            }
+        }
+        return randomNumbers;
+    }
+
 }
