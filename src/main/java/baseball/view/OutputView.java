824c1ff81c44cf767b88d41a556a480c792f5700
Jiyoung Choi <12201817@inha.edu>
2023-10-20 15:00:36 +0900

    refactor(OutputView) : 모든 함수들 static으로 선언

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,15 +4,15 @@ public class OutputView {
     private final static String startMessage = "숫자 야구 게임을 시작합니다.";
     private final static String endMessage = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
-    public void printStartMessage() {
+    public static void printStartMessage() {
         System.out.println(startMessage);
     }
 
-    public void printEndMessage() {
+    public static void printEndMessage() {
         System.out.println(endMessage);
     }
 
-    public void printHint(String hintMessage){
+    public static void printHint(String hintMessage){
         System.out.println(hintMessage);
     }
 }
