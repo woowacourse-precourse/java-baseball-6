adfb723bcce62be1f15a00ff9e096154604efd2a
kongji <kongji4092@gmail.com>
2023-10-20 16:13:27 +0900

    feat: 게임 시작시 3자리 랜덤 숫자 발생

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,10 +1,17 @@
 package baseball.controller;
 
+import baseball.view.InputView;
 import baseball.view.OutputView;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
 
 public class GameController {
     public void run(){
         OutputView.printStartGame();
         OutputView.printInputNumber();
+        List<Integer> numbers = InputView.inputNumber();
+        List<Integer> computerNumbers = Randoms.pickUniqueNumbersInRange(1, 9, 3);
+
     }
 }
