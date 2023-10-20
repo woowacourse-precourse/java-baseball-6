63b8909e59eca65dce4a524de145b218a09329c3
sihyunjojo <si4018@naver.com>
2023-10-20 16:55:59 +0900

    사용자 숫자 입력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,14 +2,18 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         List<Integer> computerNums;
+        List<Integer> userNums;
 
         computerNums = choiceComputerNums();
-        //commit test
+        printComputerNums(computerNums);
+
+        userNums = inputUserNums();
 
     }
 
@@ -18,5 +22,33 @@ public class Application {
         computerNums = Randoms.pickUniqueNumbersInRange(1, 9, 3);
         return computerNums;
     }
+
+
+    public static void printComputerNums(List<Integer> computerNums) {
+        System.out.print("컴퓨터 수 = ");
+        for (int computerNumber : computerNums) {
+            System.out.print(computerNumber);
+        }
+        System.out.println();
+    }
+
+    public static List<Integer> inputUserNums() {
+        List<Integer> userNums;
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+        userNums = splitNum(Integer.parseInt(input));
+        // 서로 다른 3개의 숫자가 아닌 예외 상황 구현
+
+        return userNums;
+    }
+
+    private static List<Integer> splitNum(int number) {
+        List<Integer> Nums = new ArrayList<>();
+        Nums.add(number / 100);
+        Nums.add((number / 10) % 10);
+        Nums.add(number % 10);
+        return Nums;
+    }
+
 }
 
