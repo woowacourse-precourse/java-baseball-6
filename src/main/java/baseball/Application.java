371868455698e09985048c911b037bf9201c9b79
joo-sang-ah <jsa@interiorteacher.com>
2023-10-20 15:50:02 +0900

    feat: 입력값에 따른 처리 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,14 +5,26 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
+
+    private static Integer LENGTH = 3;
 
     public static void main(String[] args) {
         // TODO: 1부터 9까지 서로 다른 수로 이루어진 3자리의 수 생성
-        List<Integer> computer = generateRandom(3);
+        List<Integer> computer = generateRandomNumber(LENGTH);
+
+        // TODO: 입력값에 따라 처리
+        while (true) {
+            String input = readLine();
+            List<Integer> user = convert(input, LENGTH);
+            int[] result = compare(computer, user);
+            if(result[0] == LENGTH) break;
+        }
     }
 
-    private static List<Integer> generateRandom(Integer length) {
+    private static List<Integer> generateRandomNumber(Integer length) {
         List<Integer> result = new ArrayList<>();
         while (result.size() < length) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -23,4 +35,42 @@ public class Application {
 
         return result;
     }
+
+    private static List<Integer> convert(String string, Integer limit) {
+        if (string.length() > limit || string.indexOf("0") < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < string.length(); i++) {
+            try {
+                Integer temp = Integer.parseInt(String.valueOf(string.charAt(i)));
+                result.add(temp);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        return result;
+    }
+
+    private static int[] compare(List<Integer> computer, List<Integer> user) {
+        int[] result = new int[3];
+
+        List<Integer> miss = new ArrayList<>();
+        for (int i = 0; i < computer.size(); i++) {
+            if (computer.get(i) == user.get(i)) {
+                result[0]++;
+            } else if (miss.indexOf(user.get(i)) > -1) {
+                result[1]++;
+                miss.remove(user.get(i));
+            } else {
+                miss.add(user.get(i));
+            }
+        }
+
+        result[2] = miss.size();
+        return result;
+    }
 }
+
