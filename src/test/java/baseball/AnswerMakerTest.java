4acd0dbb29f06bfa7deeb1d6b962b862704c0576
sohyun9527 <sohyun95277@gmail.com>
2023-10-20 16:39:20 +0900

    test(AnserMakerTest): 난수 생성 테스트 범위 내의 난수가 만들어지는지 테스트 중복수가 존재하는지 테스트

---
+++ src/test/java/baseball/AnswerMakerTest.java
@@ -0,0 +1,34 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnswerMakerTest {
+
+    AnswerMaker answerMaker = new AnswerMaker();
+
+    @Test
+    void 범위_내의_난수생성_테스트() {
+        int min = 1;
+        int max = 9;
+
+        for (int i = 0; i < 9999; i++) {
+            int randomNumber = answerMaker.makeNumbers(min, max);
+            assertThat(randomNumber).isBetween(min, max);
+        }
+    }
+
+    @Test
+    void 중복수_테스트() {
+        List<Integer> testList = answerMaker.makeThreeDifferentNumberList(1, 999);
+        for (int i = 0; i < testList.size(); i++) {
+            for (int j = i + 1; j < testList.size(); j++) {
+                assertNotEquals(testList.get(i), testList.get(j));
+            }
+        }
+    }
+}
