157f0a5a9b33f42f35fa95eb054e8cf1ded4cbdb
0Chord <seaweed.0chord@gmail.com>
2023-10-20 16:45:30 +0900

    feat : 추가 game 실행을 위한 while 문

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
 
+import java.util.ArrayList;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+
+	public static void main(String[] args) {
+		while (true) {
+			break;
+		}
+	}
 }
---
+++ src/main/java/baseball/game/BaseballGames.java
@@ -0,0 +1,12 @@
+package baseball.game;
+
+import java.util.List;
+
+public class BaseballGames {
+
+	public void playBaseball() {
+		while (true) {
+
+		}
+	}
+}
