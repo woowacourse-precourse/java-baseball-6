da3422eaf946dcb84ed6e7e62e53da5e0a7a5e6b
mizuirohoshi7 <mizuirohoshi7@gmail.com>
2023-10-20 16:28:35 +0900

    test(GameResult): 불필요한 테스트 제거

--- src/test/java/baseball/dto/GameResultTest.java
+++ src/test/java/baseball/dto/GameResultTest.java
@@ -1,10 +1,8 @@
 package baseball.dto;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class GameResultTest {
@@ -17,28 +15,6 @@ class GameResultTest {
         GameResult gameResult = new GameResult(strike, ball);
 
         assertThat(gameResult).isNotNull();
-    }
-
-    @Nested
-    @DisplayName("잘못된 개수의")
-    class createdByInvalidStrikeOrBall {
-        @Test
-        @DisplayName("스트라이크로 생성")
-        void createdByInvalidStrike() {
-            int strike = 4;
-            int ball = 1;
-
-            assertThrows(IllegalStateException.class, () -> new GameResult(strike, ball));
-        }
-
-        @Test
-        @DisplayName("볼로 생성")
-        void createdByInvalidBall() {
-            int strike = 1;
-            int ball = 4;
-
-            assertThrows(IllegalStateException.class, () -> new GameResult(strike, ball));
-        }
     }
 
     @Test
