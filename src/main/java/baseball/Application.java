9a4284aae53355603f0a6766e1d69cc61c964de0
Jonghyeok Lee <endb1228@gmail.com>
2023-10-20 19:04:57 +0900

    정답 입력 기능 및 길이 검증 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,10 +12,11 @@ public class Application {
         System.out.println("숫자 야구 게임을 시작합니다.");
         while (true) {
             getInstance().startGame();
+
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            String answer = Console.readLine();
 
+            String answer = Console.readLine();
             if (answer.equals("1"))
                 continue;
             if (answer.equals("2"))
--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -1,28 +1,41 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.*;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class GameManager {
     private static GameManager gameManager;
+
     public static GameManager getInstance() {
         if (gameManager == null) gameManager = new GameManager();
         return gameManager;
     }
 
     public void startGame() {
-        List<String> answerNumber = generateRandomNumber();
+        List<Integer> correctNumber = generateRandomNumber();
+
         while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
+            String answer = Console.readLine();
+
             break;
         }
     }
 
-    private List<String> generateRandomNumber() {
-        return Randoms.pickUniqueNumbersInRange(1, 9, 3)
-                .stream()
-                .map(String::valueOf)
-                .collect(Collectors.toList());
+    private List<Integer> answerStringToNumber(String answer) {
+        validateAnswerLength(answer);
+
+        return new ArrayList<>();
+    }
+
+    private void validateAnswerLength(String answer) {
+        if (answer.length() > 3)
+            throw new IllegalArgumentException();
+    }
+
+    private List<Integer> generateRandomNumber() {
+        return new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 9, 3));
     }
 }
