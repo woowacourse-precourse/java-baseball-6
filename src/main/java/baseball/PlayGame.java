b56cc6c3c588c54662f3535127e14129c84d55d7
alswp006 <alswp006@gmail.com>
2023-10-20 20:32:47 +0900

    1차 리팩토링

--- src/main/java/baseball/PlayGame.java
+++ src/main/java/baseball/PlayGame.java
@@ -5,7 +5,6 @@ import java.util.List;
 public class PlayGame {
     public void play(){
         CreateNumber createNumber = new CreateNumber();
-        //computer를 123보다 크거나 같고 987보다 작거나 같은 랜덤수로 초기화
         List<Integer> computer = createNumber.getComputer();
         boolean restart = true;
         while (restart){
