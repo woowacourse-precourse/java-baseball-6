ea26599b93956d5946ba2748d91db0e7f9466e90
Firedrago95 <yonghwa95@naver.com>
2023-10-20 17:12:17 +0900

    feat 컴퓨터 숫자와 유저 숫자를 비교하여 판정하는 기능 구현

--- src/main/java/baseball/Model/Numbers.java
+++ src/main/java/baseball/Model/Numbers.java
@@ -4,10 +4,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Numbers {
-
+    public static final int FIRST_INDEX = 0;
+    public static final int LAST_INDEX = 2;
     List<Integer> numbers;
+
+    public Numbers() {}
+    public Numbers(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
 
     public List<Integer> getNumbers() {
         return new ArrayList<Integer>(numbers);
+    }
+
+    public int compareAndCount(Numbers computer) {
+        int count = 0;
+        List<Integer> computerNumbers = computer.getNumbers();
+        for (int number : numbers) {
+            if (computerNumbers.contains(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    public int countSameIndexAndNumber(Numbers computer) {
+        int strike = 0;
+        List<Integer> computerNumbers = computer.getNumbers();
+        for (int index = FIRST_INDEX; index <= LAST_INDEX; index++) {
+            if (numbers.get(index) == computerNumbers.get(index)) {
+                strike++;
+            }
+        }
+        return strike;
     }
 }
--- src/main/java/baseball/Model/Referee.java
+++ src/main/java/baseball/Model/Referee.java
@@ -1,13 +1,34 @@
 package baseball.Model;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Referee {
+
     int strike;
     int ball;
 
-    public void judge (Numbers computer, Numbers user) {
-        int sameNumberCount = countSameNumber(computer, user);
-        int sameIndexAndNumberCount = countSameIndexAndNumber(computer, user);
+    public void judge(Numbers user, Numbers computer) {
+        int sameNumberCount = countSameNumber(user, computer);
+        int sameIndexAndNumberCount = countStrike(user, computer);
+
         strike = sameIndexAndNumberCount;
         ball = sameNumberCount - strike;
+    }
+
+    private int countSameNumber(Numbers user, Numbers computer) {
+        return user.compareAndCount(computer);
+    }
+
+    private int countStrike(Numbers user, Numbers computer) {
+        return user.countSameIndexAndNumber(computer);
+    }
+
+    public Map<String,Integer> getJudgement() {
+        Map<String,Integer> judgement = new HashMap<String,Integer>();
+        judgement.put("strike",strike);
+        judgement.put("ball",ball);
+        return judgement;
     }
 }
