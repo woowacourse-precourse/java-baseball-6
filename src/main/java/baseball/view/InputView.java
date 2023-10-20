c754c5f77cda17fa5f38a93b1e76bf6ed38f83c4
SehyeonKang <gedfgrdt@naver.com>
2023-10-20 12:50:32 +0900

    feat(InputView): 게임 재시작 여부를 입력받는 기능 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -5,8 +5,18 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     public String readGuessNumbers() {
+        System.out.print("숫자를 입력해주세요 : ");
         String inputString = Console.readLine();
         validateDigit(inputString);
+
+        return inputString;
+    }
+
+    public String readRestartOrShutDown() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String inputString = Console.readLine();
+        validateDigit(inputString);
+
         return inputString;
     }
 
