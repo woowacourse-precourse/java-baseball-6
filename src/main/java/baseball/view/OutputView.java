eb9221a0ae18342da7aa030602fd8de4e9ba4187
mizuirohoshi7 <mizuirohoshi7@gmail.com>
2023-10-20 17:30:43 +0900

    feat(OutputView): 스트라이크, 볼 개수 출력

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -6,6 +6,8 @@ public class OutputView {
     private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String STRIKE = "스트라이크";
     private static final String BALL = "볼";
+    private static final String NOTHING = "낫싱";
+    private static final String EMPTY = " ";
     private static final String CLEAR_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     public static void start() {
@@ -13,7 +15,20 @@ public class OutputView {
     }
 
     public static void result(GameResult gameResult) {
+        System.out.println(createResultMessage(gameResult));
+    }
 
+    private static String createResultMessage(GameResult gameResult) {
+        if (gameResult.isNothing()) {
+            return NOTHING;
+        }
+        if (gameResult.isStrikeOnly()) {
+            return gameResult.strike() + STRIKE;
+        }
+        if (gameResult.isBallOnly()) {
+            return gameResult.ball() + BALL;
+        }
+        return gameResult.ball() + BALL + EMPTY + gameResult.strike() + STRIKE;
     }
 
     public static void clear() {
