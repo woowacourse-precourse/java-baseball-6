d2963647b9ea0fa8a6002d2d06664009452b71ba
sohyun9527 <sohyun95277@gmail.com>
2023-10-20 20:24:47 +0900

    test(ComputerTest): 낫싱 판별 기능 테스트 작성 주어진 값이 answer 내부에 아예 포함되어 있지 않다면 NOTHING 반환

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.in;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,9 +25,39 @@ class ComputerTest {
     void judgeBall() {
         //given
         List<Integer> answer = List.of(3,9,6);
+        int number = 3;
+        int index = 3;
         //when
-        int result = computer.judgeScore(answer, 3,1);
+        int result = computer.judgeScore(answer, number,index);
         //then
         assertThat(result).isEqualTo(computer.BALL);
+    }
+
+
+    @DisplayName("스트라이크 판별 기능 테스트")
+    @Test
+    public void judgeStrike() throws Exception {
+        //given
+        List<Integer> answer = List.of(3,6,9);
+        int number = 3;
+        int index = 0;
+        //when
+        int result = computer.judgeScore(answer, number, index);
+        //then
+        assertThat(result).isEqualTo(computer.STRIKE);
+    }
+
+
+    @DisplayName("낫싱 판별 기능 테스트")
+    @Test
+    public void judgeNothing() throws Exception {
+        //given
+        List<Integer> answer = List.of(3,6,9);
+        int number = 8;
+        int index = 1;
+        //when
+        int result = computer.judgeScore(answer, number, index);
+        //then
+        assertThat(result).isEqualTo(computer.NOTHING);
     }
 }
