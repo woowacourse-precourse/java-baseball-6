425ca5642a1caa9d3a10ce1fa244e3cdd112646b
StrangeMin <mim0107@nate.com>
2023-10-20 17:03:51 +0900

    feat : 정답 확인하는 checkAnswer()과 strike, ball 변수를 초기화 하는 initStrikeAndBall() 정의

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -25,6 +25,9 @@ public class Application {
 
             // 사용자 수 입력
             getUserInputNumber();
+
+            // 정답 확인
+            boolean isAllCorrect = checkAnswer(userNumbers);
         }
     }
 
@@ -66,6 +69,29 @@ public class Application {
 
     public static boolean isValidUserNumber(String number){
         if(number.matches("[1-9]{3}"))
+            return true;
+
+        return false;
+    }
+
+    public static void initStrikeAndBall(){
+        strike =0;
+        ball =0;
+    }
+
+    public static boolean checkAnswer(String number){
+        initStrikeAndBall();
+
+        for(int i=0; i<number.length(); i++){
+            Integer curNumber = Integer.valueOf(number.charAt(i));
+            Integer order =orderOfNumbers[curNumber];
+            if(!order.equals(0) && order.equals(i+1))
+                strike++;
+            else if(!order.equals(0))
+                ball++;
+        }
+
+        if(strike.equals(3))
             return true;
 
         return false;
