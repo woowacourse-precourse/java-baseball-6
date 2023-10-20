d5e2f95a7a48dd11592e60a5f3449f5afde2d117
Haebin <joker7011@naver.com>
2023-10-20 20:27:42 +0900

    feat: 게임 breakPoint 설정

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,18 +1,28 @@
 package baseball;
 
+import static baseball.Config.NUMBER_LENGTH;
+
 public class Game {
-    private final static int numberLength = 3;
+
 
     public void start() {
         String computerNumber, playerNumber;
 
         Player player = new Player();
-        Computer computer = new Computer();
+        ComputerNumber computer = new ComputerNumber();
+        Hint hint = new Hint();
 
-        playerNumber = player.inputNumber(numberLength);
-        computerNumber = computer.pickTargetNumber(numberLength);
+        computerNumber = computer.pickTargetNumber(NUMBER_LENGTH);
 
-        System.out.println("computerNumber = " + computerNumber);
-        System.out.println("playerNumber = " + playerNumber);
+        do {
+            playerNumber = player.inputNumber(NUMBER_LENGTH);
+
+            hint.reset();
+            hint.updateHint(playerNumber, computerNumber);
+            hint.printHint();
+        } while (!hint.checkClear());
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
     }
 }
