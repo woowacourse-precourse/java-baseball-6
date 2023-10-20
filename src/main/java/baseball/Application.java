0817c684278b508394d0b01fe800655bf5e2e419
jaehyuuk <doogun97@gmail.com>
2023-10-20 16:20:39 +0900

    기능 목록 추가 및 매직넘버 제거
    기존 기능 목록을 구체화 하였고 매직넘버를 줄이기 위해 변수로 변경하였음
    볼과 스트라이크의 개수를 세는 기능에서 for문을 이용해 최적화를 진행하였음
    게임종료 후 1또는 2를 제외한 다른 것을 입력시 예외 처리 기능을 추가함

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -17,7 +17,7 @@ public class Application {
 
         System.out.println("숫자 야구 게임을 시작합니다.");
         while (startGame) {
-            // 컴퓨터 숫자 랜덤 설정
+
             List<Integer> computer = new ArrayList<>();
             while (computer.size() < numberSize) {
                 int randomNumber = Randoms.pickNumberInRange(randomMin, randomMax);
@@ -30,36 +30,34 @@ public class Application {
 
                 int strike = 0;
                 int ball = 0;
-                // 사용자 숫자 입력 받은 후 배열로 저장
+
                 System.out.print("숫자를 입력해주세요 : ");
                 int num = Integer.parseInt(Console.readLine());
                 int[] user = Stream.of(String.valueOf(num).split("")).mapToInt(Integer::parseInt).toArray();
-                if (user.length != numberSize) throw new IllegalArgumentException(); // 예외 처리
+                if (user.length != numberSize) throw new IllegalArgumentException();
 
-                // 볼, 스트라이크 개수 파악
                 for (int i = 0; i < numberSize; i++) {
+                    for (int j = 0; j < numberSize; j++) {
+                        if (j != i) {
+                            if (computer.get(i).equals(user[j])) ball++;
+                        }
+                    }
                     if (computer.get(i).equals(user[i])) strike++;
                 }
-                if (computer.get(0).equals(user[1]) || computer.get(0).equals(user[2])) ball++;
-                if (computer.get(1).equals(user[0]) || computer.get(1).equals(user[2])) ball++;
-                if (computer.get(2).equals(user[0]) || computer.get(2).equals(user[1])) ball++;
-                // 점수 계산
+
                 if      (ball > 0 && strike == 0) System.out.println(ball + "볼");
-                else if (ball == 0 && strike > 0) System.out.println(strike + "스트라이크");
+                else if (strike > 0 && ball == 0) System.out.println(strike + "스트라이크");
                 else if (ball > 0 && strike > 0)  System.out.println(ball + "볼 " + strike + "스트라이크");
                 else                              System.out.println("낫싱");
-                // 게임 종료 or 재시작
-                if (strike == 3) {
+
+                if (strike == numberSize) {
                     System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                     System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                     int restart = Integer.parseInt(Console.readLine());
                     if      (restart == 1) break;
-                    else if (restart == 2) {
-                        startGame = false; break;
-                    }
-                    else throw new IllegalArgumentException(); // 예외처리
+                    else if (restart == 2) { startGame = false; break; }
+                    else    throw new IllegalArgumentException();
                 }
-
             }
         }
     }
