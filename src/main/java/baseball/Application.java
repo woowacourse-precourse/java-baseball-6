85403a5646b595d75e090e9008687a579e8660b6
linirini <2001yerin@naver.com>
2023-10-20 17:52:13 +0900

    feat : implement game start feature

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,10 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        startGame();
+    }
+    private static void startGame(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        //TODO : 컴퓨터 랜덤 숫자 생성
     }
 }
