b842ce27a161d3b7914ca0e47f6f4521f7a9f590
dgh06175 <dgh06175@gmail.com>
2023-10-20 21:40:38 +0900

    feat: 사용자 숫자 입력 받고 컴퓨터와 비교 후 결과 출력

--- src/main/java/baseball/NumberBaseballGame.java
+++ src/main/java/baseball/NumberBaseballGame.java
@@ -8,19 +8,28 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class NumberBaseballGame {
+    private static final int NUMBER_LENGTH = 3;
     List<Integer> computer = new ArrayList<>(3);
 
     public void start() {
         System.out.println("숫자 야구 게임을 시작합니다.\n");
         computer = generateRandomNumbers();
         boolean isContinue = true;
-        playGame();
+        while (isContinue) {
+            playGame();
+            // isContinue = getUserContinueInput();
+        }
     }
 
     private void playGame() {
-        List<Integer> user = new ArrayList<>();
-        user = userNumberInput();
-        System.out.println(user);
+        boolean userWin = false;
+        while (!userWin) {
+            List<Integer> user = userNumberInput();
+            if (isUserWin(user)) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                userWin = true;
+            }
+        }
     }
 
     private List<Integer> generateRandomNumbers() {
@@ -31,6 +40,7 @@ public class NumberBaseballGame {
                 generatedNumbers.add(randomNumber);
             }
         }
+        System.out.println(generatedNumbers);
         return generatedNumbers;
     }
 
@@ -54,5 +64,42 @@ public class NumberBaseballGame {
             throw new IllegalArgumentException("잘못된 숫자 입력입니다: " + inputString);
         }
         return true;
+    }
+
+    private boolean isUserWin(List<Integer> user) {
+        int ball = countBall(user);
+        int strike = countStrike(user);
+        ball -= strike;
+        if (ball > 0) {
+            System.out.print(ball + "볼 ");
+        }
+        if (strike > 0) {
+            System.out.print(strike + "스트라이크");
+        }
+        if (ball == 0 && strike == 0) {
+            System.out.print("낫싱");
+        }
+        System.out.println();
+        return strike == 3;
+    }
+
+    private int countBall(List<Integer> user) {
+        int ball = 0;
+        for (int i = 0; i < NUMBER_LENGTH; i++) {
+            if (computer.contains(user.get(i))) {
+                ball++;
+            }
+        }
+        return ball;
+    }
+
+    private int countStrike(List<Integer> user) {
+        int strike = 0;
+        for (int i = 0; i < NUMBER_LENGTH; i++) {
+            if (computer.get(i).equals(user.get(i))) {
+                strike++;
+            }
+        }
+        return strike;
     }
 }
