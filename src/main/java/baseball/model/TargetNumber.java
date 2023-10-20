7af0f7f29560eb0aaae9e68294cfe3b4eed8cb05
eora21 <eora21@naver.com>
2023-10-20 15:27:04 +0900

    feat: 🎸 추가 게임의 결과를 계산하여 반환하는 메서드 작성

--- src/main/java/baseball/model/TargetNumber.java
+++ src/main/java/baseball/model/TargetNumber.java
@@ -2,7 +2,9 @@ package baseball.model;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class TargetNumber {
     private final List<Number> numbers;
@@ -25,5 +27,54 @@ public class TargetNumber {
         }
 
         return new TargetNumber(Collections.unmodifiableList(uniqueNumbers));
+    }
+
+    public GameResult calculateGameResult(final String inputNumberText) {
+        List<Number> userNumbers = convertInputNumberText(inputNumberText);
+
+        if (numbers.size() != userNumbers.size()) {
+            throw new IllegalArgumentException();
+        }
+
+        return createGameResult(userNumbers);
+    }
+
+    private List<Number> convertInputNumberText(final String inputNumberText) {
+        List<Number> inputNumbers = inputNumberText.chars()
+                .map(Character::getNumericValue)
+                .mapToObj(Number::new)
+                .toList();
+
+        verifyDuplicates(inputNumbers);
+        return inputNumbers;
+    }
+
+    private void verifyDuplicates(final List<Number> numbers) {
+        Set<Number> uniqueNumbers = new HashSet<>(numbers);
+
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private GameResult createGameResult(List<Number> userNumbers) {
+        int correctCount = 0;
+        int similarCount = 0;
+
+        for (int i = 0; i < numbers.size(); i++) {
+            Number computerNumber = numbers.get(i);
+            Number userNumber = userNumbers.get(i);
+
+            if (computerNumber.equals(userNumber)) {
+                correctCount++;
+                continue;
+            }
+
+            if (numbers.contains(userNumber)) {
+                similarCount++;
+            }
+        }
+
+        return new GameResult(correctCount, similarCount);
     }
 }
