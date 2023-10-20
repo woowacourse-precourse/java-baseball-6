73116de9a57e98d0bc92e50892703211f65b2aab
mummhy0811 <mummhy0811@inu.ac.kr>
2023-10-20 18:35:03 +0900

    Feat: ball, strike 동시 출력 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -16,6 +16,8 @@ public class OutputView {
         System.out.println("낫싱");
     }
 
+    public static void printBallAndStrike(int ball, int strike){ System.out.println(ball + "볼 "+strike + "스트라이크");}
+
     public static void printExit(){
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
