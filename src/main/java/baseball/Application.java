06de9ad09f8c1d7bdc7284207b2571633cd8405b
Domae-back-end <ty_ty123@naver.com>
2023-10-20 12:06:13 +0900

    공백 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -22,7 +22,7 @@ public class Application {
 
         List<String> gameSuccessKey = createRandomSuccessKey(gameFew); //정답 배열
         System.out.println("숫자 야구 게임을 시작합니다.");
-        
+
         //게임 시작
         while (true) {
             System.out.print("숫자를 입력해주세요 : ");
