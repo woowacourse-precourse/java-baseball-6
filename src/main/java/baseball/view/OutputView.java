5ad304ee8da6d3f4ac09df641e17563104ba3d03
5jeong <wjdgns4385@naver.com>
2023-10-21 02:25:21 +0900

    [Feat] : 볼, 스트라이크, 낫싱을 출력하는 기능 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -7,10 +7,29 @@ public class OutputView {
     private static final String BALL = "볼";
     private static final String NOTHING = "낫싱";
 
-    public static void gameStartOutput(){
+    public static void gameStartOutput() {
         System.out.println(START_MESSAGE);
     }
-    public static void gameEndOutput(){
+
+    public static void gameResult(int strikeCount, int ballCount) {
+        if (strikeCount == 0 && ballCount == 0) {
+            System.out.println(NOTHING);
+            return;
+        }
+        if(ballCount > 0 && strikeCount > 0){
+            System.out.println(ballCount+BALL+" "+strikeCount+STRIKE);
+            return;
+        }
+        if (strikeCount == 0) {
+            System.out.println(ballCount + BALL+" ");
+            return;
+        }
+        if(ballCount == 0){
+            System.out.println(strikeCount+STRIKE);
+        }
+    }
+
+    public static void gameEndOutput() {
         System.out.println(GAME_END_MESSAGE);
     }
 
