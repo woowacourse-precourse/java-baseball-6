018eb4bdca55c33b0f8548e42da9b78a9ebbb1ab
jy016011 <39334789+jy016011@users.noreply.github.com>
2023-10-20 19:10:04 +0900

    변수 낭비 개선

--- src/main/java/baseball/gameLogic/NumberBaseBall.java
+++ src/main/java/baseball/gameLogic/NumberBaseBall.java
@@ -42,7 +42,7 @@ public class NumberBaseBall {
             int strikeCount = score.strikeCount;
             messageViewer.printResultMsg(ballCount, strikeCount);
 
-            mind = checkKeepGoing(score.strikeCount);
+            mind = checkKeepGoing(strikeCount);
         }
     }
 
