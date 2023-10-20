cb87b611d211197c5a049fe872a8e45424d34f4d
김명준 <86913355+mjj111@users.noreply.github.com>
2023-10-20 14:47:38 +0900

    refactor(Computer): getRandomNumbers메서드 실제 동작하도록 수정
     getRandomNumbers메서드 실제 랜덤한 숫자를 가진 인스턴스 변수 randomNumbers 생성하고 반환하도록 수정

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,12 +1,28 @@
 package baseball.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
+    private static final int LENGTH_RANDOMNUMBERS = 3;
     private List<Integer> randomNumbers;
 
     public List<Integer> getRandomNumbers() {
-        return List.of(1, 2, 3);
+        randomNumbers = new ArrayList<>();
+
+        while (randomNumbers.size() < LENGTH_RANDOMNUMBERS) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+
+            if (!randomNumbers.contains(randomNumber)) {
+                randomNumbers.add(randomNumber);
+            }
+        }
+
+        return randomNumbers;
     }
+
+
 
 }
