1ee06f0ebc35ff1b94b37d10cd95a40f33974fb1
kunsanglee <gosmdochee2@gmail.com>
2023-10-20 12:51:14 +0900

    refactor(InputValidator): 선언된 상수의 접근제어자를 private로 변경

--- src/main/java/baseball/InputValidator.java
+++ src/main/java/baseball/InputValidator.java
@@ -3,17 +3,17 @@ package baseball;
 
 public class InputValidator {
 
-    public static final int INPUT_NUMBER_LENGTH = 3;
-    public static final int CONTINUE_OR_EXIT_NUMBER_LENGTH = 1;
-    public static final char MIN_NUMBER = '1';
-    public static final char MAX_NUMBER = '9';
-    public static final String CONTINUE_NUMBER = "1";
-    public static final String EXIT_NUMBER = "2";
-    public static final String INPUT_NUMBER_LENGTH_EXCEPTION_MESSAGE = "입력값은 3자리 수만 가능합니다.";
-    public static final String INPUT_NUMBER_RANGE_EXCEPTION_MESSAGE = "입력값은 1~9 범위의 숫자만 가능합니다.";
-    public static final String INPUT_NUMBER_DUPLICATED_EXCEPTION_MESSAGE = "입력값은 중복되지 않는 서로 다른 숫자만 가능합니다.";
-    public static final String CONTINUE_OR_EXIT_NUMBER_EXCEPTION_MESSAGE = "입력값은 1 또는 2 만 가능합니다.";
-    public static final String CONTINUE_OR_EXIT_NUMBER_LENGTH_EXCEPTION_MESSAGE = "입력값은 한 자리 수만 입력 가능합니다.";
+    private static final int INPUT_NUMBER_LENGTH = 3;
+    private static final int CONTINUE_OR_EXIT_NUMBER_LENGTH = 1;
+    private static final char MIN_NUMBER = '1';
+    private static final char MAX_NUMBER = '9';
+    private static final String CONTINUE_NUMBER = "1";
+    private static final String EXIT_NUMBER = "2";
+    private static final String INPUT_NUMBER_LENGTH_EXCEPTION_MESSAGE = "입력값은 3자리 수만 가능합니다.";
+    private static final String INPUT_NUMBER_RANGE_EXCEPTION_MESSAGE = "입력값은 1~9 범위의 숫자만 가능합니다.";
+    private static final String INPUT_NUMBER_DUPLICATED_EXCEPTION_MESSAGE = "입력값은 중복되지 않는 서로 다른 숫자만 가능합니다.";
+    private static final String CONTINUE_OR_EXIT_NUMBER_EXCEPTION_MESSAGE = "입력값은 1 또는 2 만 가능합니다.";
+    private static final String CONTINUE_OR_EXIT_NUMBER_LENGTH_EXCEPTION_MESSAGE = "입력값은 한 자리 수만 입력 가능합니다.";
 
     public static String validateBaseBallNumber(String input) {
         validInputLength(input);
