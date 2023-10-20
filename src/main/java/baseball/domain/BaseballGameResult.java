f1b0020d9c4be35f20643c3361176289b5e9ab1a
Hugh <lee2317884@gmail.com>
2023-10-20 16:21:38 +0900

    feat: 볼 개수 계산

--- src/main/java/baseball/domain/BaseballGameResult.java
+++ src/main/java/baseball/domain/BaseballGameResult.java
@@ -5,21 +5,23 @@ import java.util.stream.Stream;
 
 public class BaseballGameResult {
 
-    public final int strikeCount;
     public final int ballCount;
+    public final int strikeCount;
 
 
     public BaseballGameResult(List<Integer> computerNumbs, List<Integer> userNumbs) {
-        this.strikeCount = strikeCountProcess(computerNumbs, userNumbs);
         this.ballCount = ballCountProcess(computerNumbs, userNumbs);
+        this.strikeCount = strikeCountProcess(computerNumbs, userNumbs);
+    }
+
+    private int ballCountProcess(List<Integer> computerNumbs, List<Integer> userNumbs) {
+        return (int) Stream.iterate(0, i -> i + 1).limit(computerNumbs.size())
+                .filter(num -> !computerNumbs.get(num).equals(userNumbs.get(num))
+                        && computerNumbs.contains(userNumbs.get(num))).count();
     }
 
     private int strikeCountProcess(List<Integer> computerNumbs, List<Integer> userNumbs) {
         return (int) Stream.iterate(0, i -> i + 1).limit(computerNumbs.size())
                 .filter(num -> computerNumbs.get(num).equals(userNumbs.get(num))).count();
-    }
-
-    private int ballCountProcess(List<Integer> computerNumbs, List<Integer> userNumbs) {
-        return 0;
     }
 }
