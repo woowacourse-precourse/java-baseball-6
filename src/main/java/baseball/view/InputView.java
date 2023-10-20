280bd968d1d8b43b07ff8ae0384f3e122e5d35a6
5jeong <wjdgns4385@naver.com>
2023-10-21 02:24:53 +0900

    [Feat] : 입력숫자가 올바른지 판단하는 기능 추가
    사용자가 입력하는 숫자가 올바른지 검증
    - 3자리수인지 검증
    - 0이없는지 검증
    - 3개의 숫자가 모두 다른지 검증

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,6 +1,9 @@
 package baseball.view;
 import camp.nextstep.edu.missionutils.Console;
 
+import javax.xml.crypto.dom.DOMCryptoContext;
+import java.util.HashSet;
+
 public class InputView {
     private static final String INPUT_NUM_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
@@ -8,6 +11,7 @@ public class InputView {
     public static String inputNum(){
         System.out.print(INPUT_NUM_MESSAGE);
         String inputNum = Console.readLine();
+        judgeCorrectInputNum(inputNum);
         return inputNum;
     }
     public static int restart(){
@@ -16,7 +20,26 @@ public class InputView {
         return num;
     }
 
-    public static void correctInputnum(String num){
+    public static void judgeCorrectInputNum(String num){
+
+        // TODO: 길이가 3인지 검증
+        if(num.length() !=3){
+            throw new IllegalArgumentException("길이가 3이 아닙니다.");
+        }
+        // TODO: 0이 없는지 검증
+        if (num.contains("0")) {
+            throw new IllegalArgumentException("0은 입력할수 없습니다.");
+        }
+        // TODO: 3개의 숫자가 모두 다른지 검증
+        HashSet<Character> inputNumSet = new HashSet<>();
+        for(char x  :num.toCharArray()){
+            inputNumSet.add(x);
+        }
+        if(inputNumSet.size()<3){
+            throw new IllegalArgumentException("서로 다른 숫자를 입력해야합니다.");
+        }
+
+
 
     }
 
