6350ab8609f252e422356565ec4016a9b48506e3
yeseong31 <yeseong31@naver.com>
2023-10-20 11:48:37 +0900

    feat: 게임 진행 및 숫자 비교 기능 구현

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -1,5 +1,7 @@
 package baseball.controller;
 
+import static baseball.domain.Computer.BASEBALL_NUMBERS_SIZE;
+
 import baseball.service.ComputerService;
 import baseball.service.MessageService;
 import baseball.service.PlayerService;
@@ -10,24 +12,44 @@ public class BaseballController {
     private final ComputerService computerService = new ComputerService();
     private final PlayerService playerService = new PlayerService();
     private final MessageService messageService = new MessageService();
-
+    
     public void startGame() {
+        // TODO: 게임 재시작 및 종료
         initGame();
-        String playerNumber = getNumberFromPlayer();
+        playGame();
     }
     
     private void initGame() {
         messageService.startGame();
-        computerService.initComputer();
     }
     
-    private String getNumberFromPlayer() {
+    private void playGame() {
+        String computer = getComputerNumber();
+        
+        int balls, strikes = 0;
+        
+        while (strikes != BASEBALL_NUMBERS_SIZE) {
+            String player = getPlayerNumber();
+            
+            balls = computerService.countBalls(computer, player);
+            strikes = computerService.countStrikes(computer, player);
+            
+            messageService.printGameResult(balls, strikes);
+        }
     
+        messageService.announceThreeStrikes();
+    }
+    
+    private String getPlayerNumber() {
         messageService.inputPlayerNumber();
         String number = Console.readLine();
         
         playerService.validate(number);
-    
+        
         return number;
+    }
+    
+    private String getComputerNumber() {
+        return computerService.initComputer();
     }
 }
