1a7ab13741b97c6b31cf67fefd0ffe2e808fce04
kyeoungchan <wkc1002@naver.com>
2023-10-20 18:17:14 +0900

    feat: 서비스 calculateResult 기능 구현

--- src/main/java/baseball/service/Service.java
+++ src/main/java/baseball/service/Service.java
@@ -1,2 +1,38 @@
-package baseball.service;public class Service {
+package baseball.service;
+
+import baseball.domain.Computer;
+import baseball.domain.GamePlayer;
+import baseball.domain.Result;
+
+import java.util.List;
+import java.util.Objects;
+
+import static baseball.domain.Const.FIXED_THREE_CIPHERS;
+
+public class Service {
+
+    private final GamePlayer gamePlayer = new GamePlayer();
+    private final Computer computer = new Computer();
+    private final Result result = new Result();
+
+    public void initComputer() {
+        computer.generateThreeNumbers();
+    }
+
+    private void initData(String inputtedNumbers) {
+        gamePlayer.initThreeNumbers(inputtedNumbers);
+        result.initResult();
+    }
+
+    public Result calculateResult(String inputtedNumbers) {
+        initData(inputtedNumbers);
+        List<Integer> gamePlayerNumbers = gamePlayer.getThreeNumbers();
+        List<Integer> computerNumbers = computer.getThreeNumbers();
+
+        for (int i = 0; i < FIXED_THREE_CIPHERS; i++) {
+            if (Objects.equals(gamePlayerNumbers.get(i), computerNumbers.get(i))) result.addStrike();
+            else if(computerNumbers.contains(gamePlayerNumbers.get(i))) result.addBall();
+        }
+        return result;
+    }
 }
