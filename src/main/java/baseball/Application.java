ad8c017ce587d87f7cea6b46a54125226a81f2f0
shinel98 <75024473+shinel98@users.noreply.github.com>
2023-10-20 20:40:34 +0900

    feat: create user input feature

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,45 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        List<Integer> computer = new ArrayList<>();
+        List<Integer> user = new ArrayList<>();
+
+        while(computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if(!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+
+        while(true) {
+            System.out.print("숫자를 입력해주세요 : ");
+
+            String userInput = Console.readLine();
+            String[] userInputs = userInput.split("");
+
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+            int endGame = Integer.parseInt(Console.readLine());
+
+            if(endGame == 1) {
+                continue;
+            } else if (endGame == 2) {
+                break;
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 }
+
