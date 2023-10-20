67e55578a09abc59e7417caca26bcc5c87a342e8
JangHongJoon <wkdghdwns19969@gmail.com>
2023-10-21 06:14:24 +0900

    FEAT : 정답 공, 사용자 공 입력 받는 기능 추가

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -1,7 +1,29 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Baseball {
 
     public void play() {
+        Balls answerBalls, playerBalls;
+
+        // TODO : 정답 공 번호들 생성
+        answerBalls = new RandomBalls();
+        answerBalls.createBalls();
+        System.out.println(answerBalls.getBallsList());
+
+        // TODO : 플레이어 공 입력 받기
+        playerBalls = new PlayerBalls();
+        playerBalls.createBalls();
+        System.out.println(playerBalls.getBallsList());
+
+
+
+        // TODO : 비교
+
+        // TODO : 비교 결과 출력
+
+        // TODO : 다시 실행 or 정상 게임 종료
     }
 }
