f7f22d29eb5518b472dc838c0daf3e7918a14b97
leesangjun <hubls@naver.com>
2023-10-20 23:34:15 +0900

    fix: 게임 결과 출력 문자 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -10,6 +10,7 @@ public class BaseballGame {
 
 	private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
 	private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+	private static final String SUCCESS_STRIKE_MESSAGE = "3스트라이크";
 	private static final String SUCCESS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 	private static final String RESTART_OR_END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 	private boolean quit = false;
@@ -45,6 +46,7 @@ public class BaseballGame {
 	}
 
 	private void restartOrEnd() {
+		System.out.println(SUCCESS_STRIKE_MESSAGE);
 		System.out.println(SUCCESS_MESSAGE);
 		System.out.println(RESTART_OR_END_MESSAGE);
 		String playerInputNumber = Console.readLine();
