5bd8d48ac1c7d230af522ff636417b1320954f13
Identityless <rlandwnd555@naver.com>
2023-10-20 23:23:59 +0900

    feat : 공통 상수 관리용 enum 추가

---
+++ src/main/java/baseball/component/GeneralNumbers.java
@@ -0,0 +1,12 @@
+package baseball.component;
+
+public enum GeneralNumbers {
+
+    NUMBER_SIZE(3);
+
+    GeneralNumbers(int code) {
+        this.code = code;
+    }
+
+    private final int code;
+}
