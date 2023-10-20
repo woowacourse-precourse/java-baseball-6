dfbf80cc498f253e25958be5106e639efa3b2c76
박승찬 <seungchan141414@gmail.com>
2023-10-20 23:13:55 +0900

    [Refactor(baseball)] 심판의 게임 현황을 관찰하는 메서드 리펙토링

--- src/main/java/baseball/domain/Judge.java
+++ src/main/java/baseball/domain/Judge.java
@@ -13,6 +13,10 @@ public class Judge {
         gameStandard = new GameStandard();
     }
 
+    private static int getCntBall(int cntTotalSameUserAndComputer, int cntStrike) {
+        return cntTotalSameUserAndComputer - cntStrike;
+    }
+
     /**
      * computerList와 userList을
      *
@@ -24,25 +28,15 @@ public class Judge {
         int cntTotalSameUserAndComputer = 0; // computerList 중에 userList와 같은 것이 몇개인가.
         int cntStrike = 0; // strike의 갯수
         int cntBall = 0; // ball의 갯수
-        boolean isSamePlaceIndexUserAndCom = true; // 유저의 특정 index와 컴퓨터의 특정 index의 자리의 숫자가 같은가.
-        int numIndexOfUser = 0; // UserList의 특정 index의 값
 
         // 1. computerList 중에 userList와 같은 것이 몇개인지 구한다. - 숫자가 같은 갯수
-        cntTotalSameUserAndComputer = gameStandard.getSameNumber2Computer(computerList, userList);
+        cntTotalSameUserAndComputer = getSameTotalNumberAndComputer(computerList, userList);
 
         // 2. userList의 각각의 숫자와 computerList 중에 같은 것이 몇개인지 구한다 - 스트라이크 갯수
-        int sizeUserList = userList.size();
-        for (int indexUserList = 0; indexUserList < sizeUserList; indexUserList++) {
-            numIndexOfUser = userList.get(indexUserList); // index의 UserList의 값
-            isSamePlaceIndexUserAndCom = gameStandard.isSamePlaceNumUserAndComputer(computerList, indexUserList, numIndexOfUser); // user의 특정 index의 값과 computer의 특정 index의 값이 같은가.
-
-            if (isSamePlaceIndexUserAndCom) { // user의 특정 index의 값과 computer의 특정 index의 값이 같으면.
-                cntStrike = cntStrike + 1; // 스트라이크의 갯수 증가.
-            }
-        }
+        cntStrike = getCntStrike(computerList, userList);
 
         // 3. 숫자가 같은 갯수 - 스트라이크 갯수 는 볼의 갯수이다. - 볼의 갯수
-        cntBall = cntTotalSameUserAndComputer - cntStrike;
+        cntBall = getCntBall(cntTotalSameUserAndComputer, cntStrike);
 
         // 4. 숫자가 같은 갯수가 없다면 "낫싱"
         if (Utill.isSameInteger(cntTotalSameUserAndComputer, NOT_THING_RESULT)) {
@@ -61,5 +55,44 @@ public class Judge {
 
         // 7. 볼과 스트라이크이 있다면 "1볼 1스트라이크"
         return MyConstants.MSG_BALL_STRIKE(cntBall, cntStrike);
+    }
+
+    private int getSameTotalNumberAndComputer(List<Integer> computerList, List<Integer> userList) {
+        return gameStandard.getSameNumber2Computer(computerList, userList);
+    }
+
+    /**
+     * 스트라이크의 갯수를 얻는다.
+     *
+     * @param computerList
+     * @param userList
+     * @return
+     */
+    private int getCntStrike(List<Integer> computerList, List<Integer> userList) {
+        int cntStrike = 0;
+        int valueIndexOfUserList;
+        boolean isSamePlaceIndexUserAndCom;
+        int sizeUserList = userList.size();
+        for (int indexUserList = 0; indexUserList < sizeUserList; indexUserList++) {
+            valueIndexOfUserList = userList.get(indexUserList); // index의 UserList의 값
+            isSamePlaceIndexUserAndCom = isSamePlaceNumUserAndComputer(computerList, valueIndexOfUserList, indexUserList); // user의 특정 index의 값과 computer의 특정 index의 값이 같은가.
+
+            if (isSamePlaceIndexUserAndCom) { // user의 특정 index의 값과 computer의 특정 index의 값이 같으면.
+                cntStrike = cntStrike + 1; // 스트라이크의 갯수 증가.
+            }
+        }
+        return cntStrike;
+    }
+
+    /**
+     * 유저의 특정한 index의 값과 컴퓨터리스트의 특정한 index의 값이 같은지 확인한다.
+     *
+     * @param computerList         컴퓨터 3개의 랜덤 숫자가 있는 LIST
+     * @param valueIndexOfUserList 유저의 특정한 값의 index
+     * @param indexUserList        유저의 특정한 값이 있는 index
+     * @return
+     */
+    private boolean isSamePlaceNumUserAndComputer(List<Integer> computerList, int valueIndexOfUserList, int indexUserList) {
+        return gameStandard.isSamePlaceNumUserAndComputer(computerList, indexUserList, valueIndexOfUserList);
     }
 }
