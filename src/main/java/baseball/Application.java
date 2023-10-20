773f39323a7bc1dc10533ae499064abe27d2c218
honi20 <1594cyh@gmail.com>
2023-10-20 18:02:25 +0900

    [feat] 사용자가 입력한 값에 대한 결과 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,25 +1,45 @@
 package baseball;
 
-
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+    static class Result {
+        private int strike;
+        private int ball;
+
+        public Result(int strike, int ball) {
+            this.strike = strike;
+            this.ball = ball;
+        }
+
+        public int getStrike() {
+            return strike;
+        }
+
+        public int getBall() {
+            return ball;
+        }
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
+
         while (true) {
             List<Integer> computerNumbers = selectComputerNumbers();
 //            System.out.println(computerNumbers);
 
             while (true) {
                 List<Integer> userNumbers = inputUserNumbers();
-//                System.out.println(userNumbers);
+                Result result = getResultForUserNumbers(computerNumbers, userNumbers);
+                printResult(result);
 
-                break;
+                if (correctAllNumbers(result)){
+                    break;
+                }
             }
 
             if (selectUserExit()) {
@@ -52,6 +72,51 @@ public class Application {
             userNumbers.add(number);
         }
         return userNumbers;
+    }
+
+    private static Result getResultForUserNumbers(List<Integer> computerNumbers, List<Integer> userNumbers) {
+        int strike = 0;
+        int ball = 0;
+
+        for (int k = 0; k < userNumbers.size(); k++) {
+            int userNumber = userNumbers.get(k);
+
+            if (computerNumbers.get(k) == userNumber) {
+                strike++;
+            }
+            else if (computerNumbers.contains(userNumber)) {
+                ball++;
+            }
+        }
+        return new Result(strike, ball);
+    }
+
+    private static void printResult(Result result) {
+        int strikeCnt = result.getStrike();
+        int ballCnt = result.getBall();
+
+        if (strikeCnt == 0 && ballCnt == 0) {
+            System.out.println("낫싱");
+        }
+        else {
+            if (ballCnt > 0) {
+                System.out.print(ballCnt + "볼 ");
+            }
+
+            if (strikeCnt > 0) {
+                System.out.print(strikeCnt + "스트라이크");
+            }
+            System.out.println();
+        }
+        return;
+    }
+
+    private static boolean correctAllNumbers(Result result) {
+        if (result.getStrike() == 3){
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        }
+        return false;
     }
 
     private static boolean selectUserExit() {
