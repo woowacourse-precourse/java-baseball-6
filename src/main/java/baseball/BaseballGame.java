330af7dc714ff5fc832e6c4b62a40afaa47a4b9e
jungeun-choi <jungeun5.choi@gmail.com>
2023-10-20 23:45:49 +0900

    init: 필요한 class 생성

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,11 @@
+package baseball;
+
+public class BaseballGame {
+	/* 게임을 반복 진행 */
+
+	public void startBaseballGame() {
+		while (true) {
+
+		}
+	}
+}
---
+++ src/main/java/baseball/BaseballView.java
@@ -0,0 +1,9 @@
+package baseball;
+
+public class BaseballView {
+	/* input, output 화면(메세지) 출력 */
+	
+	public void printGameStart(){
+		System.out.println("숫자 야구 게임을 시작합니다.");
+	}
+}
