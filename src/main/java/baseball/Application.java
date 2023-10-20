770527c520cf5da9b2c258757aeace48ee0904a5
King's meow <mingee641@gmail.com>
2023-10-20 17:07:07 +0900

    refactor: 게임 종료 기능 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,9 +4,6 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,53 +13,64 @@ public class Application {
         // 게임 시작을 사용자에게 알린다.
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-        // 컴퓨터 숫자 랜덤 생성
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-                System.out.println(randomNumber);
+        while(true) {
+            // 컴퓨터 숫자 랜덤 생성
+            List<Integer> computer = new ArrayList<>();
+            while (computer.size() < 3) {
+                int randomNumber = Randoms.pickNumberInRange(1, 9);
+                if (!computer.contains(randomNumber)) {
+                    computer.add(randomNumber);
+                    //System.out.println(randomNumber);
+                }
             }
-        }
 
-        // 사용자 숫자 입력
-        List<Integer> user = new ArrayList<>();
+            while (true) {
+                // 사용자 숫자 입력
+                List<Integer> user = new ArrayList<>();
 
-        System.out.print("숫자를 입력해주세요 : ");
-        String input = Console.readLine();
+                System.out.print("숫자를 입력해주세요 : ");
+                String input = Console.readLine();
 
-        if (input.length() != 3) throw new IllegalArgumentException("세 자리 숫자가 아니다.");
+                if (input.length() != 3) throw new IllegalArgumentException("세 자리 숫자가 아니다.");
 
-        for (int i = 0; i < 3; i++) {
-            char splitInput = input.charAt(i);
-            if (!Character.isDigit(splitInput) || splitInput == '0')
-                throw new IllegalArgumentException("1~9 사이의 숫자를 입력하세요");
+                for (int i = 0; i < 3; i++) {
+                    char splitInput = input.charAt(i);
+                    if (!Character.isDigit(splitInput) || splitInput == '0')
+                        throw new IllegalArgumentException("1~9 사이의 숫자를 입력하세요");
 
-            int inputInt = Character.getNumericValue(splitInput);
-            if (user.contains(inputInt)) throw new IllegalArgumentException("같은 숫자가 존재한다");
-            user.add(inputInt);
-        }
+                    int inputInt = Character.getNumericValue(splitInput);
+                    if (user.contains(inputInt)) throw new IllegalArgumentException("같은 숫자가 존재한다");
+                    user.add(inputInt);
+                }
 
-        int s = 0, b = 0;
+                int s = 0, b = 0;
 
-        // 컴퓨터의 수와 사용자의 수 비교
-        for (int i = 0; i < 3; i++) {
-            int num = user.get(i);
-            if (computer.contains(num)) {
-                if (computer.get(i) == num) s++;
-                else b++;
+                // 컴퓨터의 수와 사용자의 수 비교
+                for (int i = 0; i < 3; i++) {
+                    int num = user.get(i);
+                    if (computer.contains(num)) {
+                        if (computer.get(i) == num) s++;
+                        else b++;
+                    }
+                }
+
+                if (s == 0 && b == 0) System.out.println("낫싱");
+                else if (s == 3) {
+                    System.out.println("3스트라이크");
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    break;
+                }
+                else if (b == 0) System.out.println(s + "스트라이크");
+                else if (s == 0) System.out.println(b + "볼");
+                else System.out.println(b + "볼 " + s + "스트라이크");
             }
+
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            int choice = Integer.parseInt(Console.readLine());
+            if(choice == 2) break;
+            else if(choice == 1) continue;
+            else throw new IllegalArgumentException("1 또는 2만 입력하세요");
         }
-
-        if (s == 0 && b == 0) System.out.println("낫싱");
-        else if (s != 0 && b != 0) System.out.println(b + "볼 " + s + "스트라이크");
-        else if (b == 0) System.out.println(s + "스트라이크");
-        else if (s == 0) System.out.println(b + "볼");
-        else System.out.println("낫싱");
-
-
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
 
     }
