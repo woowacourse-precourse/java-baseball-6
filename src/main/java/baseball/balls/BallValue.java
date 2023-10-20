ab49ab1acf3ded19c373a32283615f3b55466259
SJ70 <seojae70@gmail.com>
2023-10-21 01:40:53 +0900

    Feat & Test: BallValue의 값의 범위에 대한 검증

--- src/main/java/baseball/balls/BallValue.java
+++ src/main/java/baseball/balls/BallValue.java
@@ -2,10 +2,20 @@ package baseball.balls;
 
 public class BallValue {
 
+    public static final int BALL_MIN_VALUE = 1;
+    public static final int BALL_MAX_VALUE = 9;
+
     private final int value;
 
     public BallValue(int value) {
+        validateValueRange(value);
         this.value = value;
+    }
+
+    private void validateValueRange(int value) {
+        if (value < BALL_MIN_VALUE || value > BALL_MAX_VALUE) {
+            throw new IllegalArgumentException("공의 값이 유효하지 않은 값입니다.");
+        }
     }
 
 }
