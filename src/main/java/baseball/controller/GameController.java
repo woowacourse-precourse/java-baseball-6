997b72ed217818f0dac7e191c7b1756b06a0694e
choheejin <hjcho9510@gmail.com>
2023-10-20 21:49:14 +0900

    fix: use GameController to compare numbers

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -28,6 +28,7 @@ public class GameController {
         setComputers();
         do {
             setHumans();
+            compareNumbers();
         } while (gameModel.isRunning());
     }
     private void setHumans() {
@@ -44,4 +45,47 @@ public class GameController {
         }
         computerModel = new ComputerModel(computers);
     }
+
+    public void compareNumbers() {
+        List<Integer> human = humanModel.getHumans();
+        List<Integer> computer = computerModel.getComputers();
+        int[] strikeIdx = checkStrike(human, computer);
+        checkBall(human, computer, strikeIdx);
+        checkNothing();
+    }
+
+    private int[] checkStrike(List<Integer> human, List<Integer> computer){
+        int[] strikeIdx = new int[3];
+        int strike = 0;
+
+        for(int i = 0; i < 3; i++) {
+            if(human.get(i).equals(computer.get(i))){
+                strike += 1;
+                strikeIdx[i] = 1;
+            }
+        }
+
+        gameModel.setStrike(strike);
+
+        return strikeIdx;
+    }
+
+    private void checkBall(List<Integer> human, List<Integer> computer, int[] strikeIdx){
+        int ball = 0;
+        for(int i = 0; i < 3; i++) {
+            if(strikeIdx[i] == 1) continue;
+            if(computer.contains(human.get(i))) {
+                ball += 1;
+            }
+        }
+
+        gameModel.setBall(ball);
+    }
+
+    private void checkNothing() {
+        if(gameModel.getBall() == 0 && gameModel.getStrike() == 0){
+            gameModel.setNothing(1);
+        }
+    }
+
 }
--- src/main/java/baseball/model/GameModel.java
+++ src/main/java/baseball/model/GameModel.java
@@ -1,15 +1,37 @@
 package baseball.model;
 
 public class GameModel {
-    private int[] scores;
+    private int strike;
+    private int ball;
+    private int nothing;
     private boolean isRunning;
 
     public GameModel() {
         this.isRunning = true;
     }
 
-    public int[] getScores() {
-        return scores;
+    public int getStrike() {
+        return strike;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+
+    public int getNothing() {
+        return nothing;
+    }
+
+    public void setStrike(int strike) {
+        this.strike = strike;
+    }
+
+    public void setBall(int ball) {
+        this.ball = ball;
+    }
+
+    public void setNothing(int nothing) {
+        this.nothing = nothing;
     }
 
     public boolean isRunning() {
