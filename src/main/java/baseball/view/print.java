eeecb71e2c2b2cef9c9754a1738ff42bfcdede9a
Maljaaa <Maljaaa@github.com>
2023-10-20 16:44:00 +0900

    style: /view/print 코드 포맷 변경

--- src/main/java/baseball/view/print.java
+++ src/main/java/baseball/view/print.java
@@ -2,14 +2,19 @@ package baseball.view;
 
 public class print {
 
-    public void printInit(){
+    public static void printInit(){
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void printWin(){
+        System.out.println("3스트라이크");
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
     public void printResult(int ball, int strike){
         // 0볼 0스트라이크
         if(ball != 0 && strike != 0) {
-            System.out.println(ball + "볼" + strike + "스트라이크");
+            System.out.println(ball + "볼 " + strike + "스트라이크");
         }
         // 0볼
         else if(ball != 0 && strike == 0) {
@@ -23,9 +28,5 @@ public class print {
         else {
             System.out.println("낫싱");
         }
-    }
-
-    public void printWin(){
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 }
