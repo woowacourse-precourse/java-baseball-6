c58a25fb94330f726e75e99486b437e4ef7ab518
minseo  jo <liging12@naver.com>
2023-10-20 21:15:17 +0900

    feat: GameSetupService 기능 추가
    기능 목록
    1. 새로운 게임을 생성한다. (플레이어가 게임을 재 시작시 매번 새로운 게임(Game.java)을 만든다.)
    2. 컴퓨터 랜덤 Balls 를 생성한다.
    3. 플레이어가 추측한 String 입력을 컨트롤러가 넘겨주면 플레이어 Balls 를 만든다.

--- src/main/java/baseball/service/GameSetupService.java
+++ src/main/java/baseball/service/GameSetupService.java
@@ -1,4 +1,53 @@
 package baseball.service;
 
+import baseball.domain.Ball;
+import baseball.domain.Balls;
+import baseball.domain.Game;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static baseball.domain.Ball.*;
+
 public class GameSetupService {
+
+    public Game gameCreate() {
+        return new Game(generateComputerBalls());
+    }
+
+    public Balls generatePlayerBalls(String playerGuess) {
+        return new Balls(createPlayerBalls(playerGuess));
+    }
+
+    private Balls generateComputerBalls() {
+        return new Balls(createComputerBalls(generateRandomBallNumbers()));
+    }
+
+    private List<Integer> generateRandomBallNumbers() {
+        List<Integer> ballNumbers = new ArrayList<>();
+        while (ballNumbers.size() < BALL_SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_BALL, MAX_BALL);
+            if (!ballNumbers.contains(randomNumber)) {
+                ballNumbers.add(randomNumber);
+            }
+        }
+        return ballNumbers;
+    }
+
+    private List<Ball> createComputerBalls(List<Integer> ballNumbers) {
+        List<Ball> balls = new ArrayList<>();
+        for (Integer ballNumber : ballNumbers) {
+            balls.add(new Ball(ballNumber));
+        }
+        return balls;
+    }
+
+    private List<Ball> createPlayerBalls(String playerGuess) {
+        List<Ball> balls = new ArrayList<>();
+        for (char ballNumber : playerGuess.toCharArray()) {
+            balls.add(new Ball(ballNumber - '0'));
+        }
+        return balls;
+    }
 }
