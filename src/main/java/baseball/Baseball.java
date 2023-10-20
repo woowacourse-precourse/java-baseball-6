336ffe7f322635d662023f7f70ec6f58612e9e09
munis <aaaa6658@naver.com>
2023-10-20 17:55:14 +0900

    feat : 상대방 숫자 선택을 위한 Random 값 추출

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -1,7 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Baseball {
     public void startGame() {
         System.out.println("숫자 야구 게임을 시작합니다." );
+    }
+
+    public List<Integer> generateNumber() {
+        List<Integer> numberList = new ArrayList<>();
+        while (numberList.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!numberList.contains(randomNumber)) {
+                numberList.add(randomNumber);
+            }
+        }
+        return numberList;
     }
 }
