949477d8d87edaa484cbcf8a7d95d1d69ab011d9
joo-sang-ah <jsa@interiorteacher.com>
2023-10-20 16:33:49 +0900

    refactor: 결과 코드 enum으로 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,17 +11,41 @@ public class Application {
 
     private static Integer LENGTH = 3;
 
+    private enum RESULT_CODE {
+        STRIKE(0, "스트라이크"), BALL(1, "볼"), MISS(2, "낫싱");
+        private int value;
+        private String title;
+
+        RESULT_CODE(int value, String title) {
+            this.value = value;
+            this.title = title;
+        }
+
+        public String getTitle() {
+            return this.title;
+        }
+    }
+
     public static void main(String[] args) {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
         // TODO: 1부터 9까지 서로 다른 수로 이루어진 3자리의 수 생성
         List<Integer> computer = generateRandomNumber(LENGTH);
 
         // TODO: 입력값에 따라 처리
         while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
             String input = readLine();
             List<Integer> user = convert(input, LENGTH);
+
             int[] result = compare(computer, user);
-            if(result[0] == LENGTH) break;
+
+            if (result[0] == LENGTH) {
+                break;
+            }
         }
+
+        // TODO: 게임 종료
     }
 
     private static List<Integer> generateRandomNumber(Integer length) {
@@ -37,7 +61,7 @@ public class Application {
     }
 
     private static List<Integer> convert(String string, Integer limit) {
-        if (string.length() > limit || string.indexOf("0") < 0) {
+        if (string.length() > limit || string.indexOf("0") > -1) {
             throw new IllegalArgumentException();
         }
 
@@ -60,16 +84,16 @@ public class Application {
         List<Integer> miss = new ArrayList<>();
         for (int i = 0; i < computer.size(); i++) {
             if (computer.get(i) == user.get(i)) {
-                result[0]++;
+                result[RESULT_CODE.STRIKE.value]++;
             } else if (miss.indexOf(user.get(i)) > -1) {
-                result[1]++;
+                result[RESULT_CODE.BALL.value]++;
                 miss.remove(user.get(i));
             } else {
                 miss.add(user.get(i));
             }
         }
 
-        result[2] = miss.size();
+        result[RESULT_CODE.MISS.value] = miss.size();
         return result;
     }
 }
