fc9f4f9016e32d8a5c64b75afd2a29b91f461869
lee jihyeon <lejinhy@gmail.com>
2023-10-20 20:47:03 +0900

    feat: countStrike, countBall, isNothing 메소드 완성

--- src/main/java/baseball/GameTool.java
+++ src/main/java/baseball/GameTool.java
@@ -21,19 +21,37 @@ public class GameTool {
      * 스트라이크 개수를 반환
      */
     public int countStrike(List<Integer> computer, List<Integer> user) {
-
+        int count = 0;
+        for (int i = 0; i < computer.size(); i++) {
+            if (computer.get(i) == user.get(i)) {
+                count++;
+            }
+        }
+        return count;
     }
     /*
      * 볼 개수를 반환
      */
     public int countBall(List<Integer> computer, List<Integer> user) {
-
+        int count = 0;
+        for (int i = 0; i < computer.size(); i++) {
+            int find = user.get(i);
+            if (computer.contains(find) && computer.indexOf(find) != i) {
+                count++;
+            }
+        }
+        return count;
     }
     /*
      * 낫싱일 경우 true, 아닐 경우 false 반환
      */
     public boolean isNothing(List<Integer> computer, List<Integer> user) {
-
+        for (int i = 0; i < computer.size(); i++) {
+            if (computer.contains(user.get(i))) {
+                return false;
+            }
+        }
+        return true;
     }
     /*
      * 힌트 결과 출력
