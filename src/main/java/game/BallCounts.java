aac62c864e8712b78b5e57f989b0ffd4a340e960
1000zoo <cjswldn99@gmail.com>
2023-10-20 19:42:20 +0900

    feat(game): 두 개의 정수형 변수 Wrapper
    - Computer 내부에 있는 ballCounts 와 strikeCounts를 추출해내고,
    - 결과 출력을 용이하게 하기 위해 클래스화

---
+++ src/main/java/game/BallCounts.java
@@ -0,0 +1,15 @@
+package game;
+
+public class BallCounts {
+
+    private int ball;
+    private int strike;
+
+    public BallCounts() {
+    }
+
+    @Override
+    public String toString() {
+        return ball + ", " + strike;
+    }
+}
