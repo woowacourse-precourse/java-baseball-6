3308c6703bd6f4662ecbd9663d8cb321a55977de
hosung-222 <gik11kr@naver.com>
2023-10-20 19:45:09 +0900

    feat : 입력값과 랜던값 비교하기 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,9 +10,33 @@ public class Application {
         computer.setRandomNumbers();
 
         // 사용자 입력 받기
-        String n = Console.readLine();
-        if (n.length() != 3){
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+        if (input.length() != 3) {
             throw new IllegalArgumentException("입력 값 오류");
+        }
+
+        // 입력값 랜던값 비교하기
+        int strikeCnt = 0;
+        int ballCnt = 0;
+        String[] inputArr = input.split("");
+        for (int i = 0; i < 3; i++) {
+            if (computer.getNumber(i) == Integer.parseInt(inputArr[i])) {
+                strikeCnt++;
+            } else if (computer.contains(Integer.parseInt(inputArr[i]))) {
+                ballCnt++;
+            }
+        }
+
+        // 출력
+        if (strikeCnt + ballCnt == 0) {
+            System.out.println("낫싱");
+        } else if (strikeCnt == 0) {
+            System.out.println(ballCnt + "볼 ");
+        } else if (ballCnt == 0) {
+            System.out.println(strikeCnt + "스트라이크");
+        } else {
+            System.out.println(ballCnt + "볼 " + strikeCnt + "스트라이크");
         }
     }
 
