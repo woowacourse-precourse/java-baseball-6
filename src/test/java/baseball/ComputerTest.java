98b08b38fd9c40507a782393adf8601843234ced
minjun7410 <minjun7410@gmail.com>
2023-10-20 23:31:11 +0900

    test: Computer 판정 테스트 생성
    올바르게 판정되는지 테스트하기 위해 테스트 코드 생성.
    result1 = 3스트라이크, result2 = 3볼, result3 = 1볼, result4 = 1볼 1스트라이크, result5 = 2볼 1스트라이크

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -15,4 +15,21 @@ public class ComputerTest {
             }
         }
     }
+
+    @Test
+    void 판정테스트(){
+        Computer computer = new Computer();
+        computer.randomNumbers = new int[] {1, 2, 3};
+        int[] result1 = computer.judge(new int[] {1, 2, 3});
+        int[] result2 = computer.judge(new int[] {2, 3, 1});
+        int[] result3 = computer.judge(new int[] {4, 1, 9});
+        int[] result4 = computer.judge(new int[] {1, 9, 2});
+        int[] result5 = computer.judge(new int[] {1, 3, 2});
+
+        Assertions.assertEquals(result1[1], 3);
+        Assertions.assertEquals(result2[0], 3);
+        Assertions.assertEquals(result3[0], 1);
+        Assertions.assertTrue(result4[0] == 1 && result4[1] == 1);
+        Assertions.assertTrue(result5[0] == 2 && result5[1] == 1);
+    }
 }
