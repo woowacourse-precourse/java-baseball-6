e5f95b6422b3093ed9f93d81e6924b4e50547771
YongHyeon <pyh4616@naver.com>
2023-10-21 04:58:58 +0900

    Test: Convert, 문자를 입력받아 정수로 변환하는 기능 테스트

--- src/test/java/baseball/util/ConvertTest.java
+++ src/test/java/baseball/util/ConvertTest.java
@@ -1,12 +1,11 @@
 package baseball.util;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ConvertTest {
 
@@ -21,5 +20,15 @@ public class ConvertTest {
         assertThat(userNumber.get(0)).isEqualTo(1);
         assertThat(userNumber.get(1)).isEqualTo(2);
         assertThat(userNumber.get(2)).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("String을 Integer로 변환하는 기능을 테스트한다.")
+    void testConvertStringToInteger() {
+        String input = "1";
+
+        int restartOrExitNumber = Convert.convertStringToInteger(input);
+
+        assertThat(restartOrExitNumber).isEqualTo(1);
     }
 }
