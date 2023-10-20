124e935fc0d765efcc20d93a554672a62e36e00f
Hyun Woo <hwsa10041@gmail.com>
2023-10-20 19:42:47 +0900

    feat : inputRestartChoice() 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -11,4 +11,9 @@ public class InputView {
         return number;
     }
 
+    public int inputRestartChoice() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return Integer.parseInt(readLine());
+    }
+
 }
