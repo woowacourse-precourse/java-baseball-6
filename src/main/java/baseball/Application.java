4f4c5288ca6e2840b256c6c12667dedc23e8f345
gunwoong <gwj0421@gmail.com>
2023-10-20 20:55:01 +0900

    feat : BaseBallGame 객체 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,13 @@
 package baseball;
 
 public class Application {
+    public static class BaseBallGame {
+        public BaseBallGame() {
+
+        }
+    }
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
