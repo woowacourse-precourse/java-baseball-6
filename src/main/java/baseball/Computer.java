19055b7a1a3a1b670852b567dc8a71198bf34f53
wochae <officialwochae@gmail.com>
2023-10-20 17:10:43 +0900

    [FEAT] : Class Computer Implements constructor, attribution and getter

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,16 @@
+package baseball;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Computer {
+  List<Integer> randomNumbers = new ArrayList<>();
+
+  public Computer() {
+  }
+
+  public List<Integer> getRandomNumbers() {
+    return randomNumbers;
+  }
+
+}
