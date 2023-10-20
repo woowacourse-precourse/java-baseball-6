c3c19cf53ac7f0400dfccf11269d751ae0233088
gemstoneyang <bennyws99@gmail.com>
2023-10-20 18:04:49 +0900

    test: Add a toString test for the GameResult class - GameResult의 toString 결과를 확인하는 테스트 추가

--- src/test/java/baseball/ClassTest.java
+++ src/test/java/baseball/ClassTest.java
@@ -17,4 +17,19 @@ public class ClassTest {
         assertEquals(1, result.getStrike());
         assertEquals(2, result.getBall());
     }
+
+    @Test
+    void GameResult_클래스의_toString_결과가_올발라야_한다() {
+        GameResult result1 = new GameResult(1, 2);
+        assertEquals("2볼 1스트라이크", result1.toString());
+
+        GameResult result2 = new GameResult(0, 0);
+        assertEquals("낫싱", result2.toString());
+
+        GameResult result3 = new GameResult(2, 0);
+        assertEquals("2스트라이크", result3.toString());
+
+        GameResult result4 = new GameResult(0, 2);
+        assertEquals("2볼", result4.toString());
+    }
 }
