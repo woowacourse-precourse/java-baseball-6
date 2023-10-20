3ae53edb357870cfb12dbfb3462441245b504233
minisyu <iswater.kim@gmail.com>
2023-10-20 22:40:47 +0900

    feat(Application): 모든 기능 구현 완료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
+import baseball.controller.GameController;
+import baseball.domain.RandomComputerThreeNumbersGenerator;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        OutputView outputView = new OutputView();
+        InputView inputView = new InputView(outputView);
+        RandomComputerThreeNumbersGenerator randomComputerThreeNumbersGenerator = new RandomComputerThreeNumbersGenerator();
+        GameController gameController = new GameController(outputView, inputView, randomComputerThreeNumbersGenerator);
+        gameController.start();
     }
 }
