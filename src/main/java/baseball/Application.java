d4744520dcfc1d8b134681f10bd42ae9e72ae863
김승우 <tmd8633@naver.com>
2023-10-20 15:41:18 +0900

    Add: Application과 BaseController 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,28 +1,11 @@
 package baseball;
 
-import service.BaseService;
-import service.BaseServiceImpl;
+import controller.BaseController;
 
 public class Application {
 
-    private static final BaseService service = new BaseServiceImpl();
-
-    /**
-     * 전체 게임 기능
-     */
     public static void main(String[] args) {
-        System.out.println("숫자 야구 게임을 시작합니다.");
-
-        boolean start = true;
-
-        while (start){
-            // 게임 시작
-            service.game();
-
-            // 재시작 여부
-            start = service.isRestart();
-        }
-
+        BaseController.run();
     }
 
 
