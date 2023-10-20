29fff4807dffbf101495fcc96e048714593a7a3b
jiwoo <zbqmgldjfh@gmail.com>
2023-10-20 22:45:10 +0900

    feat(InputView): InputView 구현

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,10 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String readUserInput() {
+        String input = Console.readLine();
+        return input;
+    }
+}
