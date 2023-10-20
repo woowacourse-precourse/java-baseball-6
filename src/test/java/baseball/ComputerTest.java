fd0a8d4279d5cfaf8ae7001510a5b715ebdadfcf
sohyun9527 <sohyun95277@gmail.com>
2023-10-20 20:45:39 +0900

    test(ComputerTest): 단위별 테스트 작성 ball, strike, noting을 계산한 결과값이 올바르게 나왔는지 테스트 케이스 작성

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -13,9 +13,9 @@ class ComputerTest {
     Computer computer = new Computer();
 
 
-    @DisplayName("전체 스코어 기능 테스트")
+    @DisplayName("리스트 3볼 판별 기능 테스트")
     @Test
-    public void judgeScoreTest() throws Exception {
+    public void threeBallScoreTest() throws Exception {
         //given
         List<Integer> answer = List.of(3,6,9);
         List<Integer> userAnswer = List.of(6,9,3);
@@ -27,15 +27,57 @@ class ComputerTest {
         assertThat(score).isEqualTo(List.of(3,0));
     }
 
+    @DisplayName("리스트 낫싱 판별 기능 테스트")
+    @Test
+    public void nothingScoreTest() throws Exception {
+        //given
+        List<Integer> answer = List.of(3,6,9);
+        List<Integer> userAnswer = List.of(1,2,4);
+
+        //when
+        List<Integer> score = computer.countScore(answer, userAnswer);
+
+        //then
+        assertThat(score).isEqualTo(List.of(0,0));
+    }
+
+    @DisplayName("리스트 스트라이크 판별 기능 테스트")
+    @Test
+    public void strikeScoreTest() throws Exception {
+        //given
+        List<Integer> answer = List.of(3,6,9);
+        List<Integer> userAnswer = List.of(3,6,9);
+
+        //when
+        List<Integer> score = computer.countScore(answer, userAnswer);
+
+        //then
+        assertThat(score).isEqualTo(List.of(0,3));
+    }
+
+    @DisplayName("리스트 볼, 스트라이크 판별 기능 테스트")
+    @Test
+    public void ballStrikeScoreTest() throws Exception {
+        //given
+        List<Integer> answer = List.of(3,6,9);
+        List<Integer> userAnswer = List.of(9,6,3);
+
+        //when
+        List<Integer> score = computer.countScore(answer, userAnswer);
+
+        //then
+        assertThat(score).isEqualTo(List.of(2,1));
+    }
+
     @DisplayName("볼 판별 기능 테스트")
     @Test
     void judgeBall() {
         //given
         List<Integer> answer = List.of(3,9,6);
         int number = 3;
-        int index = 3;
+        int index = 2;
         //when
-        int result = computer.judgeScore(answer, number,index);
+        int result = computer.judgeScore(answer, number, index);
         //then
         assertThat(result).isEqualTo(computer.BALL);
     }
