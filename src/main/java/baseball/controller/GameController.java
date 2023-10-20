508cd31ef851a07e45e77bb9571f953daa6f7831
BullChallenger <am23am45@naver.com>
2023-10-20 18:55:54 +0900

    [FEAT] GameController 생성
    GameServiceImpl과 동일하게 싱글톤 패턴을 적용했다
    
    OCP와 DIP원칙을 최대한 지키기 위해 Computer와 GameService 객체를 외부로부터 주입 받는 방식을 사용했다
    
    start 메소드는 무작위 숫자 3개를 중복되지 않게 생성한다
    
    process 메소드는 사용자의 입력 값에 대한 유효성을 검사한다
    
    - 입력된 숫자의 개수가 3개인지 확인
    - 숫자만 입력됐는지 확인
    - 중복된 값을 입력했는지 확인
    
    유효성 검증 이후 값 판정을 위해 입력 값을 정수 배열로 변환하여 GameService로 넘긴다
    
    isRetry  메소드는 사용자 입력 값에 대한 유효성을 검사한다
    
    - 한 자리 수의 숫자만 입력됐는지 확인
    - 숫자만 입력됐는지 확인
    
    유효성 검증 이후 해당 값이 1이면 true를 반환, 2면 false를 반환하고 1과 2 이외의 숫자가 입려됐을 시 IllegalArgumentException을 호출한다

---
+++ src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,93 @@
+package baseball.controller;
+
+import baseball.domain.Computer;
+import baseball.dto.JudgeDTO;
+import baseball.service.GameService;
+
+import java.util.Arrays;
+
+public class GameController {
+
+    private static final String OUT_OF_BOUND = "입력된 숫자가 범위를 벗어났습니다.";
+    private static final String NON_NUMERIC_EXCEPTION = "숫자만 입력해주세요.";
+    private static final String DUPLICATED_ELEMENT_EXCEPTION = "중복된 값을 입력할 수 없습니다.";
+    private static final int PROCESS_CAPACITY = 3;
+    private static final int RETRY_CAPACITY = 1;
+    private static final int RETRY = 1;
+    private static final int EXIT = 2;
+
+    private final Computer computer;
+    private final GameService gameService;
+
+    private static GameController INSTANCE;
+    private GameController(Computer computer, GameService gameService) {
+        this.computer = computer;
+        this.gameService = gameService;
+    }
+    public static GameController getInstance(Computer computer, GameService gameService) {
+        if (INSTANCE == null)
+            INSTANCE = new GameController(computer, gameService);
+        return INSTANCE;
+    }
+
+    public void start() {
+        computer.generateBaseball();
+    }
+
+    public JudgeDTO process(String input) {
+        validateCapacity(input, PROCESS_CAPACITY);
+        char[] charArray = validateNumericOnly(input);
+        validateDuplication(charArray.clone());
+
+        int[] numbers = changeIntArray(charArray);
+
+        return gameService.process(computer.numbersOfBaseballs(), numbers);
+    }
+
+    public boolean isRetry(String input) {
+        validateCapacity(input, RETRY_CAPACITY);
+        validateNumericOnly(input);
+
+        int status = Integer.parseInt(input);
+
+        if (status == RETRY) return true;
+        if (status == EXIT) return false;
+        throw new IllegalArgumentException(OUT_OF_BOUND);
+    }
+
+    private void validateDuplication(char[] charArray) {
+        Arrays.sort(charArray);
+
+        for (int i = 0; i < charArray.length - 1; i++) {
+            if (charArray[i] == charArray[i + 1])
+                throw new IllegalArgumentException(DUPLICATED_ELEMENT_EXCEPTION);
+        }
+    }
+
+    private char[] validateNumericOnly(String input) {
+        char[] charArray = input.toCharArray();
+
+        for (char ch : charArray) {
+            if (!Character.isDigit(ch))
+                throw new IllegalArgumentException(NON_NUMERIC_EXCEPTION);
+        }
+
+        return charArray;
+    }
+
+    private void validateCapacity(String input ,int capacity) {
+        if (input.length() != capacity)
+            throw new IllegalArgumentException(OUT_OF_BOUND);
+    }
+
+    private int[] changeIntArray(char[] charArray) {
+        int[] numbers = new int[PROCESS_CAPACITY];
+
+        for (int i = 0; i < charArray.length; i++) {
+            numbers[i] = Character.getNumericValue(charArray[i]);
+        }
+
+        return numbers;
+    }
+
+}
