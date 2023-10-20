ed074236a1227adba3c1cfd346177ac62804104c
minisyu <iswater.kim@gmail.com>
2023-10-20 11:14:53 +0900

    feat(InputView): 플레이어가 서로 다른 3자리의 숫자 및 게임 실행과 관련된 입력 기능 구현

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,39 @@
+package baseball.view;
+
+import baseball.domain.Player;
+import baseball.util.Sentence;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+
+public class InputView {
+    private final OutputView outputView;
+
+    public InputView(OutputView outputView) {
+        this.outputView = outputView;
+    }
+
+    public Player inputThreeNumbers() {
+        outputView.printInputNumbersMessage();
+        List<Integer> numbers = Arrays.stream(readLine().split(""))
+                .map(Integer::parseInt)
+                .toList();
+        return Player.of(numbers);
+    }
+
+    public boolean inputGameAction() {
+        outputView.printGameActionMessage();
+        String gameActionAnswer = readLine();
+        if ("1".equals(gameActionAnswer)) {
+            return true;
+        }
+        if ("2".equals(gameActionAnswer)) {
+            return false;
+        }
+        throw new IllegalArgumentException(Sentence.ERROR_GAME_ACTION_INPUT_MESSAGE.getMessage());
+    }
+
+    private String readLine() {
+        return Console.readLine().trim();
+    }
+}
