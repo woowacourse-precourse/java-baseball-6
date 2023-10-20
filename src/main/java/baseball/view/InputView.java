33d35f7c70ff87d89dd546e47da933767d6efb38
BBOXEEEE <noeyhesx@naver.com>
2023-10-21 01:16:03 +0900

    feat: 사용자 입력 view 추가

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,13 @@
+package baseball.view;
+
+public class InputView {
+
+
+    public static void inputNumberMessage() {
+        System.out.println("숫자를 입력해주세요 : ");
+    }
+
+    public static void restartGameMessage() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+}
