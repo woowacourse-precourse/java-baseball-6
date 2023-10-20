1614a9a2c041f2303aa54b05bec53d56b10f24d5
JiEung2 <https://github.com/JiEung2>
2023-10-20 13:27:05 +0900

    예외처리 기능 구현

--- src/main/java/model/InputValidation.java
+++ src/main/java/model/InputValidation.java
@@ -1,4 +1,23 @@
 package model;
 
+import controller.InputGameNumber;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class InputValidation {
+
+    public void validation(List<Integer> inputNum){
+        Set<Integer> set = new HashSet<>(inputNum);
+
+        try{
+            if(inputNum.size()!=3) throw new IllegalArgumentException("3자리 수를 입력해야합니다.");
+
+            if(inputNum.size()!=set.size()) throw new IllegalArgumentException("각각 다른 숫자를 입력해주세요.");
+        } catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            throw e;
+        }
+    }
 }
