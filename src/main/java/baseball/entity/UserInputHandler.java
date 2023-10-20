86f6d932c4517f7171b72ee7ee840cfbd98ea2bb
taehoon <xogns1514@gmail.com>
2023-10-20 18:33:38 +0900

    refactor: 기존 Game 클래스 리팩토링
    - Game 클래스명을 UserInput 클래스명으로 변경
    - 예외처리 함수 UserInputHandler로 변경

--- src/main/java/baseball/entity/UserInputHandler.java
+++ src/main/java/baseball/entity/UserInputHandler.java
@@ -1,14 +1,8 @@
 package baseball.entity;
 
-import camp.nextstep.edu.missionutils.Console;
-
-public class Game {
-    public Game() {
-
-    }
-
+public class UserInputHandler {
     // 게임중, 사용자 입력 예외 처리를 위한 함수
-    private static void validInputNumber(String inputNumber) {
+    public static void validInputNumber(String inputNumber) {
         // 사용자 입력의 길이가 3이 아니면 예외 처리
         if(inputNumber.length() != 3) throw new IllegalArgumentException();
 
@@ -27,29 +21,8 @@ public class Game {
     }
 
     // 게임종료 후, 사용자 입력 예외 처리를 위한 함수
-    private static void validRestartNumber(String restartNumber) {
+    public static void validRestartNumber(String restartNumber) {
         // 사용자 입력이 1 또는 2가 아니면 예외 처리
         if(!restartNumber.equals("1") && !restartNumber.equals("2")) throw new IllegalArgumentException();
     }
-
-    // 게임중 입력을 받기 위한 함수
-    private static String receiveInputNumber() {
-        System.out.println("숫자를 입력해주세요 : ");
-        String inputNumber = Console.readLine();
-
-        validInputNumber(inputNumber);
-
-        return inputNumber;
-    }
-
-    // 게임 종료후, 입력을 받기 위한 함수
-    private static String receiveRestartNumber() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        String restartNumber = Console.readLine();
-
-        validRestartNumber(restartNumber);
-
-        return restartNumber;
-    }
-
 }
