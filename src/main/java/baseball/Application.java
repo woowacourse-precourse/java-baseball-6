11c1aad198d32878ed222ad2fd5461c14f8971d4
dukcode <duk9741@gmail.com>
2023-10-21 06:41:34 +0900

    refactor: main함수 내부 GameSystem 레퍼런스 타입 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,13 +3,14 @@ package baseball;
 import baseball.baseball.BaseballGameConsoleView;
 import baseball.baseball.BaseballGameService;
 import baseball.baseball.RandomBaseballNumberGenerator;
+import baseball.game.system.GameSystem;
 import baseball.game.system.ReplayableGameSystem;
 import baseball.game.system.ReplayableGameSystemConsoleView;
 import baseball.game.turn.TurnBasedGame;
 
 public class Application {
     public static void main(String[] args) {
-        ReplayableGameSystem gameSystem = new ReplayableGameSystem(new ReplayableGameSystemConsoleView(),
+        GameSystem gameSystem = new ReplayableGameSystem(new ReplayableGameSystemConsoleView(),
                 new TurnBasedGame(new BaseballGameConsoleView(),
                         new BaseballGameService(new RandomBaseballNumberGenerator())));
         gameSystem.run();
