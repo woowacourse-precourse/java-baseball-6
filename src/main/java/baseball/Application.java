1241ba20d85ca6239de79619482d0c3591493b55
dltjdgh0428 <dltjdgh0428@naver.com>
2023-10-21 00:43:04 +0900

    refactor: 매직넘버 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class Application {
     private final static int BALL = 0;
     private final static int STRIKE = 1;
+    private final static int COMPUTER_NUM_SIZE = 3;
     private static List<Integer> computerNum = new ArrayList<>();
 
     public static void main(String[] args) {
@@ -24,27 +25,11 @@ public class Application {
     }
 
     /**
-     * 게임을 실제로 플레이하는 구간 숫자를 계속 추측함.
-     */
-    private static void playingGame(){
-        String gameString = "";
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        while (!gameString.equals("3스트라이크")) {
-            System.out.println("숫자를 입력해주세요 : ");
-            String userInput = Console.readLine();
-
-            gameString = printBallCount(checkBallCount(userInput));
-            System.out.println(gameString);
-        }
-    }
-
-    /**
      * 컴퓨터의 3자리 숫자를 만드는 메소드
      */
     private static void makeComputerNum() {
         computerNum.clear();
-
-        while (computerNum.size() < 3) {
+        while (computerNum.size() < COMPUTER_NUM_SIZE) {
             int num = Randoms.pickNumberInRange(1, 9);
             if (!computerNum.contains(num)) {
                 computerNum.add(num);
@@ -53,20 +38,18 @@ public class Application {
     }
 
     /**
-     * 게임이 끝난 후 사용자에게 다시 시작할 것인지 물어보는 메소드
+     * 게임을 실제로 플레이하는 구간 숫자를 계속 추측함.
      */
-    public static boolean checkRestartGame() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-
-        String gameChoiceNum = Console.readLine();
-
-        if (gameChoiceNum.equals("1"))
-            return true;
-        else if (gameChoiceNum.equals("2"))
-            return false;
-        //else를 사용하지않고 잘못 입력시 예외처리 하기
-        throw new IllegalArgumentException("1과 2중에 입력하셔야 합니다. ");
+    private static void playingGame() {
+        String gameString = "";
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (!gameString.equals("3스트라이크")) {
+            System.out.println("숫자를 입력해주세요 : ");
+            String userInput = Console.readLine();
+            // 콜 카운트를 checkBallCount 메소드를 통하여 산출해 낸 다음 PrintBallCount를 통하여 적절한 String 으로 변환한다.
+            gameString = printBallCount(checkBallCount(userInput));
+            System.out.println(gameString);
+        }
     }
 
     /**
@@ -77,11 +60,11 @@ public class Application {
         int totalCount = 0;
         int strikeCount = 0;
 
-        for (int i = 0; i < computerNum.size(); i++) {
+        for (int i = 0; i < COMPUTER_NUM_SIZE; i++) {
             if (userInput.contains(computerNum.get(i).toString())) {
                 totalCount++;
             }
-            if (userInput.charAt(i) - 48 == computerNum.get(i)) {
+            if (Character.getNumericValue(userInput.charAt(i)) == computerNum.get(i)) {
                 strikeCount++;
             }
         }
@@ -102,6 +85,23 @@ public class Application {
         if (listBS.get(BALL) == 0 && listBS.get(STRIKE) == 0)
             returnString = "낫씽";
         return returnString;
+    }
+
+    /**
+     * 게임이 끝난 후 사용자에게 다시 시작할 것인지 물어보는 메소드
+     */
+    public static boolean checkRestartGame() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        String gameChoiceNum = Console.readLine();
+
+        if (gameChoiceNum.equals("1"))
+            return true;
+        else if (gameChoiceNum.equals("2"))
+            return false;
+        //else를 사용하지않고 잘못 입력시 예외처리 하기
+        throw new IllegalArgumentException("1과 2중에 입력하셔야 합니다. ");
     }
 
 }
