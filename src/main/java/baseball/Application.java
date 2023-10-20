4d60c25defcbc528cc7f4d79eb680c8ee8bd9b96
linirini <2001yerin@naver.com>
2023-10-20 18:33:12 +0900

    feat : implement user number input

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +13,8 @@ public class Application{
     public static void main(String[] args){
         startGame();
         computer = createComputerRandomNumber();
+        playGame();
+
     }
 
     private static void startGame(){
@@ -26,6 +30,26 @@ public class Application{
             }
         }
         return randomNumbers;
+    }
+
+    private static void playGame(){
+        List<Integer>userNumber = input();
+    }
+
+    private static List<Integer> input(){
+        List<Integer> inputNumbers;
+        String inputString = readLine();
+        //TODO : 입력값 유효성 검사
+        inputNumbers = stringIntoIntegerList(inputString);
+        return inputNumbers;
+    }
+
+    private static List<Integer> stringIntoIntegerList(String inputString){
+        List<Integer> integerList = new ArrayList<>();
+        for(int i=0;i<inputString.length();i++){
+            integerList.add(inputString.charAt(i)-'0');
+        }
+        return integerList;
     }
 
 }
