405e98f80fc6f60b68753568c71b1b139174e341
sun1234 <sdfjikl8787@gmail.com>
2023-10-20 19:09:11 +0900

    feat: OutputView객체의 기능 추가

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,44 @@
+package baseball.view;
+
+import java.util.StringJoiner;
+
+import baseball.util.message.ProgressMessage;
+
+public class OutputView {
+
+	public void printStartMessage() {
+		System.out.println(ProgressMessage.START.getMessage());
+	}
+
+	public void printInputDigitMessage() {
+		System.out.printf(ProgressMessage.INPUT_NUMBER.getMessage());
+	}
+
+	public void printClearMessage() {
+		System.out.println(ProgressMessage.CLEAR.getMessage());
+	}
+
+	public void printAskContinueMessage() {
+		System.out.println(ProgressMessage.ASK_CONTINUE.getMessage());
+	}
+
+	public void printResult(int ballCount, int StrikeCount) {
+		StringJoiner joiner = new StringJoiner(" ");
+
+		if (ballCount != 0) {
+			joiner.add(
+					String.format(ProgressMessage.BALL.getMessage(), ballCount)
+					);
+		}
+
+		if (StrikeCount != 0) {
+			joiner.add(
+					String.format(ProgressMessage.STRIKE.getMessage(), StrikeCount)
+					);
+		}
+
+		joiner.setEmptyValue(ProgressMessage.NOTHING.getMessage());
+
+		System.out.println(joiner.toString().trim());
+	}
+}
