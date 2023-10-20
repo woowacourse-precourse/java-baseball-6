02d4cd25980629a06450c7a28b841a72c8e08e3f
kongji <kongji4092@gmail.com>
2023-10-20 15:05:49 +0900

    feat: GameController 게임 시작 기능

---
+++ src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,10 @@
+package baseball.controller;
+
+import baseball.view.OutputView;
+
+public class GameController {
+    public void run(){
+        OutputView.printStartGame();
+        OutputView.printInputNumber();
+    }
+}
