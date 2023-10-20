2a8727116c4035628813dd92601d5bcce9342d01
olsohee <dlthgml0108@daum.net>
2023-10-20 12:10:48 +0900

    feat: Application 클래스 게임 시작과 끝을 제어하도록 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,34 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        boolean exitFlag = false;
+
+        while(!exitFlag) {
+
+            Game game = new Game();
+            game.play();
+
+            exitFlag = askExit();
+        }
+        System.out.println("프로그램을 종료합니다.");
+    }
+
+    public static boolean askExit() {
+
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        String userInput = Console.readLine();
+        if(userInput.equals("2")) {
+            return true;
+        }
+
+        return false;
     }
 }
