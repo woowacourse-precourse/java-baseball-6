8088ad7d3f597e67779cd6c3eb6dd02252746ae4
bbbbooo <105257665+bbbbooo@users.noreply.github.com>
2023-10-20 14:55:32 +0900

    [Refactor] : 랜덤 숫자 생성 클래스 pickNumberInRange() 활용한 리팩토링

--- src/main/java/baseball/logic/ComputerCreateRandomNumber.java
+++ src/main/java/baseball/logic/ComputerCreateRandomNumber.java
@@ -1,7 +1,8 @@
 package baseball.logic;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class ComputerCreateRandomNumber {
@@ -12,17 +13,19 @@ public class ComputerCreateRandomNumber {
     // 컴퓨터의 랜덤한 숫자 생성
     public int createRandomNumber() {
         List<Integer> availableNumbers = createNumberLists();
-        Collections.shuffle(availableNumbers);
 
-        return createRandomNumberByShuffleNumbers(availableNumbers);
+        return createRandomNumberByPickNumbers(availableNumbers);
     }
 
-    // 섞은 리스트를 기반으로 리스트 앞에 있는 3자리 숫자를 선택하여 3자리 숫자 생성
-    private int createRandomNumberByShuffleNumbers(List<Integer> availableNumbers) {
+    // Randoms.pickNumberInRange()을 이용하여 랜덤 인덱스 선택 후, 세 자리 수 생성
+    private int createRandomNumberByPickNumbers(List<Integer> availableNumbers) {
         int createdNumber = 0;
 
         for (int i = 0; i < NUMBER_COUNT; i++) {
-            int selectedNumber = availableNumbers.remove(0);
+            int selectedIdx = Randoms.pickNumberInRange(0, availableNumbers.size()-1);
+
+            Integer selectedNumber = availableNumbers.remove(selectedIdx);
+
             createdNumber = createdNumber * 10 + selectedNumber;
         }
 
@@ -33,7 +36,7 @@ public class ComputerCreateRandomNumber {
     private List<Integer> createNumberLists() {
         List<Integer> availableNumbers = new ArrayList<>();
 
-        for (int i = MIN_NUMBER; i <=MAX_NUMBER; i++) {
+        for (int i = MIN_NUMBER; i <= MAX_NUMBER; i++) {
             availableNumbers.add(i);
         }
 
