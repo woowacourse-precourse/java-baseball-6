a6da81f6de7ad514a9458230e5c5ca4022f88b9e
shinjaewon99 <tlswodnjs99@naver.com>
2023-10-20 17:26:04 +0900

    feat : 유저가 입력한 숫자에 대한 예외 기능 추가

--- src/main/java/baseball/InputView.java
+++ src/main/java/baseball/InputView.java
@@ -3,11 +3,20 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    GameException exception = new GameException();
+
     public String inputPlayerNumber() {
-        return Console.readLine();
+        String inputPlayerNumber = Console.readLine();
+        inputPlayerNumberException(inputPlayerNumber);
+        return inputPlayerNumber;
     }
 
-    public String inputPlayerRetryNumber() {
-        return Console.readLine();
+    public int inputPlayerRetryNumber() {
+        return Integer.parseInt(Console.readLine());
+    }
+
+    private void inputPlayerNumberException(String inputPlayerNumber) {
+        exception.playerNumberLengthException(inputPlayerNumber);
+        exception.playerNumberTypeException(inputPlayerNumber);
     }
 }
