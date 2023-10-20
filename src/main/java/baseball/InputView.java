83d74da1dc007f27a5eea24778b25ed12dda164c
devhoya97 <diajbh123@gmail.com>
2023-10-20 22:21:33 +0900

    refactor : InputView 클래스 생성 InputView가 해야하는 역할의 범위가 정확히 어느정도인지 모르겠다. 복잡한 프로그램이라고 가정하면, 다양한 입력 소스 중에 누구에게 입력값을 받을 것인지 정도만 구분해주고, 컨트롤러에서 모델에게 넘겨주면, 모델이 알아서 유효성 검사하면 될 것 같은데... 그럼 그냥 getConsoleInput 메서드 하나로 충분해 보인다.

---
+++ src/main/java/baseball/InputView.java
@@ -0,0 +1,9 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static String getConsoleInput() {
+        return Console.readLine();
+    }
+}
