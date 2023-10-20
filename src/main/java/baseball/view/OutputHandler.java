fd4440788a4a3ec67fe4f0aa4b5777ae10646c2a
정성욱 <rkfka7849@gmail.com>
2023-10-20 20:12:47 +0900

    feat(OutputHandler): 낫싱을 출력하는 기능 구현

--- src/main/java/baseball/view/OutputHandler.java
+++ src/main/java/baseball/view/OutputHandler.java
@@ -4,4 +4,8 @@ public class OutputHandler {
     public static void theGameStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
+    public static void printNothing() {
+        System.out.println("낫싱");
+    }
 }
