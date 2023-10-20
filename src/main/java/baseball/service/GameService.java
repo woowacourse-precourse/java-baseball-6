d5262a6b001aea993f2c8c9df6f89947f78a8c3e
jinny-l <jinny.l.dev@gmail.com>
2023-10-20 16:50:42 +0900

    ✨feat: 게임을 시작하는 서비스 기능 구현

---
+++ src/main/java/baseball/service/GameService.java
@@ -0,0 +1,21 @@
+package baseball.service;
+
+import baseball.domain.Baseball;
+import baseball.util.NumberGenerator;
+
+public class GameService {
+
+    private final NumberGenerator numberGenerator;
+
+    private Baseball computer;
+    private Baseball player;
+
+    public GameService(NumberGenerator numberGenerator) {
+        this.numberGenerator = numberGenerator;
+    }
+
+    public void startGame() {
+        this.computer = new Baseball(numberGenerator.generate());
+        this.player = new Baseball();
+    }
+}
