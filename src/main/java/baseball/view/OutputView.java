f7fa53b5a39e89dd81d662b53777e4ed53c402a6
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-10-20 16:10:30 +0900

    Style : 결과 출력 메서드 변경 볼, 스트라이크를 입력값으로 받아 조건에 맞는 결과값 출력하게 변경

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -6,30 +6,22 @@ public class OutputView {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    public static void printStrike(){
-        System.out.println("스트라이크");
-    }
-
-    public static void printBall(){
-        System.out.println("볼");
-    }
-
-    /**
-     * a볼 b스트라이크 -> "볼 " + "스트라이크"
-     */
-    public static void printBallWithStrike(){
-        System.out.print("볼 ");
-    }
-
-    public static void printNothing(){
-        System.out.println("낫싱");
-    }
-
     public static void printEnd(){
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
-    public static void printCnt(int count){
-        System.out.print(count);
+    public static void printScoreMessage(int ball, int strike){
+        if (ball != 0 && strike != 0) {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
+        if (ball != 0 && strike == 0) {
+            System.out.println(ball + "볼");
+        }
+        if (ball == 0 && strike != 0) {
+            System.out.println(strike + "스트라이크");
+        }
+        if (ball == 0 && strike == 0) {
+            System.out.println("낫싱");
+        }
     }
 }
