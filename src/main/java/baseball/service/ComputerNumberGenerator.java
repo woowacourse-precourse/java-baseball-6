7ef808998a2814687054f650b34255e146edb548
김효준 <khyojun1104@gmail.com>
2023-10-21 01:40:06 +0900

    [feat] ComputerNumberGenerator : 중복된 컴퓨터 번호가 나오지 않도록 로직 구현!

--- src/main/java/baseball/service/ComputerNumberGenerator.java
+++ src/main/java/baseball/service/ComputerNumberGenerator.java
@@ -1,6 +1,5 @@
 package baseball.service;
 
-import baseball.view.OutputView;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class ComputerNumberGenerator {
@@ -18,13 +17,24 @@ public class ComputerNumberGenerator {
 
     public String generate(){
         StringBuilder comNumberBuilder = new StringBuilder();
-        for(int i =0; i<NUMBER_LENGTH; i++){
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+        while(isShorterThanNumberLength(comNumberBuilder)){
+            int randomNumber = Randoms.pickNumberInRange(1, 9); // builder 에서 tostring 에서 중복되면 더하지 않으면 되잖아??
+            if(isDuplicated(comNumberBuilder, randomNumber)){
+                continue;
+            }
             comNumberBuilder.append(randomNumber);
         }
         return comNumberBuilder.toString();
     }
 
+    private boolean isDuplicated(StringBuilder comNumberBuilder, int randomNumber) {
+        return comNumberBuilder.toString().contains(String.valueOf(randomNumber));
+    }
+
+
+    private boolean isShorterThanNumberLength(StringBuilder comNumberBuilder) {
+        return comNumberBuilder.length() < NUMBER_LENGTH;
+    }
 
 
 }
