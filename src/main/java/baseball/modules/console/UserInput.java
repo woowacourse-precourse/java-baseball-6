cd46624dd867605cb98ebdfcff8744c02772b0e0
상원 최 <swchoi1@swm.ai>
2023-10-20 21:18:34 +0900

    feat: "사용자의 입력 정보를 갖고있는 클래스 추가"
    #3-user-input

---
+++ src/main/java/baseball/modules/console/UserInput.java
@@ -0,0 +1,14 @@
+package baseball.modules.console;
+
+public class UserInput {
+
+    private final String input;
+
+    public UserInput(final String input) {
+        this.input = input;
+    }
+
+    public String getInput() {
+        return this.input;
+    }
+}
