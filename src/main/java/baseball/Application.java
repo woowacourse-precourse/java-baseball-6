ec7f6adc8c3bb9bea62d596160ad4cab4e15b020
JGM <cnxw4570123@gmail.com>
2023-10-20 22:55:55 +0900

    Feat : Add Function getNumbers that receives String and returns List of Integer. 사용자로부터 입력을 받고 3개의 숫자를 가진 리스트를 반환하는 메소드를 추가함.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,43 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    static List<Integer> userGuess;
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        userGuess = getNumbers();
     }
+
+    /**
+     * 사용자의 입력을 받고 이를 숫자로 변환해 리스트에 저장하는 메소드
+     * @return 3개의 중복되지 않는 정수를 갖는 리스트
+     * @throws IllegalArgumentException
+     */
+    protected static List<Integer> getNumbers() throws IllegalArgumentException {
+        String input = Console.readLine();
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("input length should be exactly three.");
+        }
+
+        List<Integer> userGuess = new ArrayList<>();
+        final byte[] inputToBytes = input.getBytes();
+        for (byte b : inputToBytes) {
+            if (b > '9' || b < '1') {
+                throw new IllegalArgumentException("numbers should be in range 1 to 9.");
+            }
+            int number = b - '0';
+            if (userGuess.contains(number)) {
+                throw new IllegalArgumentException("numbers cannot be duplicated.");
+            } else {
+                userGuess.add(number);
+            }
+        }
+        return userGuess;
+    }
+
 }
