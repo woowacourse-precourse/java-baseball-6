4e26b84382f50d5abe55fb47c48466d3ffa54cbb
Dowon <3120kang@gmail.com>
2023-10-20 14:24:35 +0900

    feat: 예외 처리 구현

---
+++ src/main/java/baseball/Balls.java
@@ -0,0 +1,35 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
+public class Balls {
+
+    private static final Integer BALLS_SIZE=3;
+    private static List<Integer> balls;
+
+    public Balls(List<Integer> balls){
+
+        validateSize(balls);
+        validateDuplication(balls);
+        this.balls=balls;
+    }
+
+    private void validateDuplication(List<Integer> balls){
+        Set<Integer> nonDuplicatedNumber= new HashSet<>();
+        Stream<Integer> stream=balls.stream();
+        stream.forEach(ball->nonDuplicatedNumber.add(ball));
+        if(nonDuplicatedNumber.size()!=BALLS_SIZE){
+            throw new IllegalArgumentException("중복된 값을 입력하였습니다.");
+        }
+    }
+
+    public void validateSize(List<Integer> balls){
+        if(balls.size()!=BALLS_SIZE){
+            throw new IllegalArgumentException("3개의 숫자를 입력해야 합니다.");
+        }
+    }
+}
