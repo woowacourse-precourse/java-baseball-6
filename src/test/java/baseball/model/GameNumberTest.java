09a09cabbe6d2b09baa08c67075df95921d0ef51
reddevilmidzy <midzy0228@gmail.com>
2023-10-20 23:26:22 +0900

    test: GameNumber 생성자 검증 테스트 추가

--- src/test/java/baseball/model/GameNumberTest.java
+++ src/test/java/baseball/model/GameNumberTest.java
@@ -1,6 +1,6 @@
 package baseball.model;
 
-import baseball.constant.ErrorMessage;
+import baseball.util.Converter;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -15,19 +15,22 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class GameNumberTest {
 
+    //TODO: 왠지 한개의 테스트 코드에는 하나의 어설션만이 있어야 할 거 같다. 수정하기
     @DisplayName("잘못된 값에 대한 예외 처리 문자열 타입")
     @ParameterizedTest(name = "{index} {displayName} message={0}")
     @ValueSource(strings = {"1234", "5567", "-123", "ball", "wt6", "-0", "", "603", "999999999999999999999"})
     void checkInvalidValue(String value) {
         assertThatThrownBy(() -> new GameNumber(value))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.INVALID_GAME_NUMBER);
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new GameNumber(Converter.convertList(value)))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("올바른 값 입력 확인 문자열 타입")
     @ParameterizedTest(name = "{index} {displayName} message={0}")
     @MethodSource("checkValidGameNumberParametersProvider")
     void checkValidValue(String value, GameNumber gameNumber) {
+        assertThat(new GameNumber(Converter.convertList(value)).equals(gameNumber)).isTrue();
         assertThat(new GameNumber(value).equals(gameNumber)).isTrue();
     }
 
