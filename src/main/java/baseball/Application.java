429568eba9e8082c7e3012f9f6ce0927e3029d74
suhyun9764 <suhyun9764@naver.com>
2023-10-20 20:42:41 +0900

    refactor:필요없는 코드 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,8 +8,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Application {
-    private static int playDecision = 0;
-    private static int strike =0;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Game game = new Game();
