a07f5398c0a835f52c6373fb0bc9719240f9073f
jeonseong-gwon <wjs4518@naver.com>
2023-10-21 00:47:16 +0900

    기능구현에 초점을 잡고 작성한 초기 구성(Test코드 X)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,7 +7,6 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String numberStr = RandomNumberGenerator();
-        System.out.println(numberStr);
         System.out.println("숫자 야구 게임을 시작합니다.");
         playGame(numberStr);
     }
@@ -40,6 +39,7 @@ public class Application {
                 strikes++;
             }
         }
+
         return strikes;
     }
 
@@ -79,7 +79,6 @@ public class Application {
         int game = sc.nextInt();
         if (game == 1) {
             String ran_Num = RandomNumberGenerator();
-            System.out.println(ran_Num);
             playGame(ran_Num);
         }
     }
