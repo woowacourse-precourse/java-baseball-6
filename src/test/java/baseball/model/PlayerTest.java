5f1684b5a93c26929b678691e7d3f17e6215a7ce
anneu <b.imnuj@gmail.com>
2023-10-20 22:06:25 +0900

    test: 플레이어와 컴퓨터 간 같은 숫자 개수 및 같은 위치 개수를 구하는 기능 테스트 추가

--- src/test/java/baseball/model/PlayerTest.java
+++ src/test/java/baseball/model/PlayerTest.java
@@ -1,5 +1,6 @@
 package baseball.model;
 
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,5 +19,27 @@ class PlayerTest {
     void makePlayerUniqueEx() {
         Assertions.assertThatThrownBy(() -> new Player("333"))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("같은 숫자의 개수를 구한다.")
+    @Test
+    void countSameNumber() {
+        Computer computer = new Computer(List.of(1, 2, 3));
+        Player player = new Player("145");
+
+        int count = player.countSameNumber(computer);
+
+        Assertions.assertThat(count).isEqualTo(1);
+    }
+
+    @DisplayName("같은 위치에 있는 숫자의 개수를 구한다.")
+    @Test
+    void countSameIndex() {
+        Computer computer = new Computer(List.of(1, 2, 3));
+        Player player = new Player("123");
+
+        int count = player.countSameIndex(computer);
+
+        Assertions.assertThat(count).isEqualTo(3);
     }
 }
