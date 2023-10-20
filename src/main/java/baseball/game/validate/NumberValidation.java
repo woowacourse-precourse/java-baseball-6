e26ae15554b49749229ed4882dd218f7166ae526
kimminsu <xjfcnfw3@naver.com>
2023-10-20 15:24:51 +0900

    feat: 숫자 양수 검증 기능 구현

--- src/main/java/baseball/game/validate/NumberValidation.java
+++ src/main/java/baseball/game/validate/NumberValidation.java
@@ -3,12 +3,19 @@ package baseball.game.validate;
 public class NumberValidation {
 
     private static final String NONE_NUMBER = "숫자가 입력되지 않았습니다.";
+    private static final String NEGATIVE_NUMBER = "숫자는 양수로 입력해주세요.";
 
     public static void validate(String number) {
         try {
-            Integer.valueOf(number);
+            checkNegativeNumber(Integer.valueOf(number));
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(NONE_NUMBER);
+        }
+    }
+
+    public static void checkNegativeNumber(Integer number) {
+        if (number < 0) {
+            throw new IllegalArgumentException(NEGATIVE_NUMBER);
         }
     }
 }
--- src/test/java/baseball/game/validate/NumberValidationTest.java
+++ src/test/java/baseball/game/validate/NumberValidationTest.java
@@ -2,6 +2,7 @@ package baseball.game.validate;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -9,8 +10,16 @@ class NumberValidationTest {
 
     @ValueSource(strings = {"1a2", "11b", "sws"})
     @ParameterizedTest
-    void validate(String noneNumber) {
+    void validateInteger(String noneNumber) {
         assertThatThrownBy(() -> NumberValidation.validate(noneNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void checkNegativeNumber() {
+        String negativeNumber = "-234";
+
+        assertThatThrownBy(() -> NumberValidation.validate(negativeNumber))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
