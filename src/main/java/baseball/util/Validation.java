8d24eb6e1bfcbe32893f55008a436b147a2049dd
junOTL <naqp99@gmail.com>
2023-10-20 23:44:40 +0900

    feat: 세 자리 숫자 입력을 안한 경우 예외 발생 기능 구현

--- src/main/java/baseball/util/Validation.java
+++ src/main/java/baseball/util/Validation.java
@@ -7,4 +7,10 @@ public class Validation {
             throw new IllegalArgumentException("값을 입력해 주세요.");
         }
     }
+
+    public static void isLengthThree(String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("세 자리 숫자를 입력해 주세요.");
+        }
+    }
 }
