6c78f2ddcf9ae82fe42b47a394dc61d6cc505035
howudong <tjdvy963@naver.com>
2023-10-20 17:11:47 +0900

    feat : 숫자 야구 판정을 담당하는 referee 클래스 생성

---
+++ src/main/java/baseball/domain/Referee.java
@@ -0,0 +1,59 @@
+package baseball.domain;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public final class Referee {
+    private final AnswerNumbers answerNumbers;
+    private final Map<HintType, Integer> hintMap = new HashMap<>();
+
+    public Referee(AnswerNumbers answerNumbers) {
+        this.answerNumbers = answerNumbers;
+
+        hintMap.put(HintType.STRIKE, 0);
+        hintMap.put(HintType.BALL, 0);
+        hintMap.put(HintType.NOTHING, 0);
+    }
+
+    public Map<HintType, Integer> getHint(PlayerNumbers playerNumbers) {
+        List<Integer> numbers = playerNumbers.getNumbers();
+
+        for (int i = 0; i < numbers.size(); i++) {
+            HintType type = compareToAnswer(numbers.get(i), i);
+            int count = hintMap.get(type);
+            hintMap.put(type, count + 1);
+        }
+        trimHints();
+
+        return hintMap;
+    }
+
+    private HintType compareToAnswer(int number, int index) {
+        List<Integer> answer = answerNumbers.getAnswerNumbers();
+
+        if (answer.contains(number) && answer.get(index) == number) {
+            return HintType.STRIKE;
+        }
+        if (answer.contains(number)) {
+            return HintType.BALL;
+        }
+        return HintType.NOTHING;
+    }
+
+    private void trimHints() {
+        int numberSize = answerNumbers.getAnswerNumbers().size();
+
+        if (hintMap.get(HintType.NOTHING).equals(numberSize)) {
+            hasOnlyNOTHING();
+            return;
+        }
+        hintMap.remove(HintType.NOTHING);
+    }
+
+    private void hasOnlyNOTHING() {
+        hintMap.remove(HintType.STRIKE);
+        hintMap.remove(HintType.BALL);
+        hintMap.put(HintType.NOTHING, 1);
+    }
+}
