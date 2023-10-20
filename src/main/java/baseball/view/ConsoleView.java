ec31fa06136af28c6de2af1eb55587b21c2a7716
xogus3492 <wkdxogus3492@gmail.com>
2023-10-20 18:57:31 +0900

    fix(consoleview): 게임 결과 출력 기능 추가 볼 + 스트라이크 출력문 추가

--- src/main/java/baseball/view/ConsoleView.java
+++ src/main/java/baseball/view/ConsoleView.java
@@ -8,7 +8,7 @@ public class ConsoleView {
     private static final String THREE_DIGIT_NUMBER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String GAME_PROGRESS_NUMBER_INPUT_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static final String STRIKE_MESSAGE = "스트라이크";
-    private static final String BALL_MESSAGE = "볼 ";
+    private static final String BALL_MESSAGE = "볼";
     private static final String NOTHING_MESSAGE = "낫싱";
 
     public static void printGameStart() {
@@ -26,6 +26,10 @@ public class ConsoleView {
 
     public static void printBallCount(int count) {
         System.out.println(count + BALL_MESSAGE);
+    }
+
+    public static void printBallAndStrikeCount(int ball, int strike) {
+        System.out.println(ball + BALL_MESSAGE + " " + strike + STRIKE_MESSAGE);
     }
 
     public static void printNothing() {
