16ba888156086082373d36773a67766a149204fc
doha <dohaahi@naver.com>
2023-10-21 00:57:47 +0900

    feat: 숫자 야구 게임 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,20 @@
 package baseball;
 
+import baseball.controller.BaseballController;
+import baseball.domain.BallNumber;
+import baseball.service.BaseballService;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        BaseballService baseballService = new BaseballService();
+        BallNumber ballNumber = new BallNumber();
+        BaseballController baseballController = new BaseballController(inputView, outputView, baseballService,
+                ballNumber);
+
+        baseballController.start();
     }
 }
