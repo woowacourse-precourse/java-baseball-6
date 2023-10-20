dcee80ac37cb747bfc9316d6fe3f2d4b253386c4
Annjieun <dkslwdms2121@naver.com>
2023-10-20 22:16:50 +0900

    feat: 입력과 관련한 상수 추가

---
+++ src/main/java/baseball/constant/NumberConstant.java
@@ -0,0 +1,7 @@
+package baseball.constant;
+
+public class NumberConstant {
+    public static final Integer INPUT_LENGTH = 3;
+    public static final Integer MINIMUM_RANGE_NUMBER = 1;
+    public static final Integer MAXIMUM_RANGE_NUMBER = 9;
+}
