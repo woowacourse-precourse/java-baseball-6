fffcb497a0096d786e035e02073a7f791a29622b
Jieon <dlwldjs7544@naver.com>
2023-10-20 19:20:16 +0900

    feat: 컴퓨터의 3자리 난수 생성하고 저장하는 기능 구현
    난수로 생성된 컴퓨터의 3자리 수를 저장하기 위해 Model의 ComputerNumber class 생성

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,14 +1,33 @@
 package baseball.controller;
 
+import baseball.model.ComputerNumber;
 import baseball.view.InputView;
 import baseball.view.OutputView;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameController {
     OutputView outputView = new OutputView();
     InputView inputView = new InputView();
+    ComputerNumber computerNumber = new ComputerNumber();
 
     public void startGame() {
         outputView.printStartMessage();
+        computerNumber.setComputerNumbers(getRandomNumbers());
         String playerNumberStr = inputView.inputPlayerNumber();
+    }
+
+    public List<Integer> getRandomNumbers() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        return computer;
     }
 }
---
+++ src/main/java/baseball/model/ComputerNumber.java
@@ -0,0 +1,16 @@
+package baseball.model;
+
+import java.util.List;
+
+public class ComputerNumber {
+
+    private List<Integer> ComputerNumbers;
+
+    public void setComputerNumbers(List<Integer> computerNumbers) {
+        this.ComputerNumbers = computerNumbers;
+    }
+
+    public List<Integer> getComputerNumbers() {
+        return this.ComputerNumbers;
+    }
+}
