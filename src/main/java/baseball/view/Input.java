16fce555358e813deb2ca4b9f8e2e7958851a80a
redoyp <gsolu0302@gmail.com>
2023-10-20 23:24:56 +0900

    feat : 사용자 숫자 입력 및 유효성 검사 기능 추가

--- src/main/java/baseball/view/Input.java
+++ src/main/java/baseball/view/Input.java
@@ -1,6 +1,15 @@
 package baseball.view;
 
+import baseball.util.NumberValidation;
+
 import camp.nextstep.edu.missionutils.Console;
+
 public class Input {
+
+    public static String UserInputNumber() {
+        String UserInput = Console.readLine();
+        NumberValidation.InputValidate(UserInput);
+        return UserInput;
+    }
 
 }
--- src/main/java/baseball/view/Output.java
+++ src/main/java/baseball/view/Output.java
@@ -7,6 +7,6 @@ public class Output {
     }
 
     public static void Input_Message() {
-        System.out.println("숫자를 입력해주세요 : ");
+        System.out.print("숫자를 입력해주세요 : ");
     }
 }
