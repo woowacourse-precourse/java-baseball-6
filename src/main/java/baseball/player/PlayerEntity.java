63db8da01c731b0614885f558f3871ef04a84747
Yoo Hyunseok <brianyhs@naver.com>
2023-10-20 15:50:31 +0900

    feat: player의 base 객체가 되는 PlayerEntity

---
+++ src/main/java/baseball/player/PlayerEntity.java
@@ -0,0 +1,22 @@
+package baseball.player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class PlayerEntity {
+
+    protected List<Integer> numberList = new ArrayList<>();
+
+    public void emptyList(){
+        numberList.clear();
+    }
+
+    // list getter
+    public List<Integer> getNumberList() {
+        return numberList;
+    }
+
+    // list setter
+    public abstract void setNumberList();
+
+}
