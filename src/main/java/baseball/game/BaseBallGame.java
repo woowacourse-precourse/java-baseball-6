1ff02bbe2538cbd7df49d487803a3b37bcb92c6b
leesh <qwqw8019@naver.com>
2023-10-20 19:36:46 +0900

    feat: 상대방의 입력을 받는 기능 적용

--- src/main/java/baseball/game/BaseBallGame.java
+++ src/main/java/baseball/game/BaseBallGame.java
@@ -3,11 +3,16 @@ package baseball.game;
 
 public class BaseBallGame implements Game {
 
+    private final Computer computer;
     public BaseBallGame() {
-
+        this.computer = new Computer();
     }
 
     @Override
     public void play() {
+
+        computer.createRandomNumber();
+
+
     }
 }
