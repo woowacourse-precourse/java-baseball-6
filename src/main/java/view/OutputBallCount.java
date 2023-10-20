b328472ccd57bf6667b6b45cb6c2c0040ca43fc9
JiEung2 <https://github.com/JiEung2>
2023-10-20 13:42:56 +0900

    입력한 수에 대한 결과 출력 기능 구현

--- src/main/java/view/OutputBallCount.java
+++ src/main/java/view/OutputBallCount.java
@@ -1,4 +1,10 @@
 package view;
 
 public class OutputBallCount {
+    public void printCount(int[] ballCount){
+        if(ballCount[0] == 0 && ballCount[1] == 0) System.out.println("낫싱");
+        if(ballCount[0] == 0 && ballCount[1] != 0) System.out.println(ballCount[1] + "볼");
+        if(ballCount[0] != 0 && ballCount[1] == 0) System.out.println(ballCount[0] + "스트라이크");
+        if(ballCount[0] != 0 && ballCount[1] != 0) System.out.println(ballCount[1] + "볼 " + ballCount[0] + "스트라이크");
+    }
 }
