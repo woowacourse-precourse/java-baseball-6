d1f4586302bef48854b76bf8c249991f25f36443
Hyeon0208 <dmsgktn0208@gmail.com>
2023-10-20 15:51:32 +0900

    feat : 게임 출력 메세지 Enum 클래스 추가

---
+++ src/main/java/baseball/view/GameMessage.java
@@ -0,0 +1,22 @@
+package baseball.view;
+
+public enum GameMessage {
+    INPUT_NUMBER("숫자를 입력해주세요 : "),
+    INPUT_RETRY("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."),
+    START_GAME("숫자 야구 게임을 시작합니다."),
+    SUCCESS("3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
+    ASK_RETRY("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."),
+    BALL("볼"),
+    STRIKE("스트라이크"),
+    NOTHING("낫싱");
+
+    private final String message;
+
+    GameMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
