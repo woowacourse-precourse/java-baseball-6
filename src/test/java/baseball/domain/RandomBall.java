1f95af3f936dffafabbbde00b84d4fffe86bf1b2
sangwopark19 <sangwopark19@gmail.com>
2023-10-20 22:00:04 +0900

    매개변수없이 리스트를 초기화하는 RandomBall 생성자

--- src/test/java/baseball/domain/RandomBall.java
+++ src/test/java/baseball/domain/RandomBall.java
@@ -1,9 +1,14 @@
 package baseball.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RandomBall {
     private List<Integer> balls;
 
+    public RandomBall() {
+        balls = new ArrayList<>();
+    }
+    
 
 }
