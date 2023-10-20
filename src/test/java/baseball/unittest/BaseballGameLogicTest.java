7991990d5523428c1621164585d5280fe3274a48
SeYeol00 <dmot0126@gmail.com>
2023-10-20 10:32:08 +0900

    feat && refactor: 야구게임 로직 볼 관련 테스트 케이스 추가 및 스트라이크 로직 추가 후 리팩터링
    static 클래스와 같은 DTO를 만들어서 볼과 스트라이크를 관리하자 명심!

--- src/test/java/baseball/unittest/BaseballGameLogicTest.java
+++ src/test/java/baseball/unittest/BaseballGameLogicTest.java
@@ -28,4 +28,117 @@ public class BaseballGameLogicTest {
         }
         assert ballCount == 0;
     }
+
+    @Test
+    void one_ball(){
+        List<Integer> computer = new ArrayList<>();
+        computer.add(3);
+        computer.add(6);
+        computer.add(9);
+
+        int[] input = new int[3];
+        input[0] = 1;
+        input[1] = 3;
+        input[2] = 5;
+
+        int ballCount = 0;
+        for(int i = 0; i<3;i++){
+            if(computer.contains(input[i])){
+                ballCount++;
+            }
+        }
+        assert ballCount == 1;
+    }
+
+    @Test
+    void two_ball(){
+        List<Integer> computer = new ArrayList<>();
+        computer.add(3);
+        computer.add(6);
+        computer.add(9);
+
+        int[] input = new int[3];
+        input[0] = 1;
+        input[1] = 3;
+        input[2] = 6;
+
+        int ballCount = 0;
+        for(int i = 0; i<3;i++){
+            if(computer.contains(input[i])){
+                ballCount++;
+            }
+        }
+        assert ballCount == 2;
+    }
+
+    @Test
+    void three_ball(){
+        List<Integer> computer = new ArrayList<>();
+        computer.add(3);
+        computer.add(6);
+        computer.add(9);
+
+        int[] input = new int[3];
+        input[0] = 9;
+        input[1] = 3;
+        input[2] = 6;
+
+        int ballCount = 0;
+        for(int i = 0; i<3;i++){
+            if(computer.contains(input[i])){
+                ballCount++;
+            }
+        }
+        assert ballCount == 3;
+    }
+
+    @Test
+    void one_ball_one_strike(){
+        List<Integer> computer = new ArrayList<>();
+        computer.add(3);
+        computer.add(6);
+        computer.add(9);
+
+        int[] input = new int[3];
+        input[0] = 3;
+        input[1] = 8;
+        input[2] = 6;
+
+        Result result = calculateBallAndStrike(computer, input);
+
+        assert result.ballCount == 1;
+        assert result.strikeCount == 1;
+    }
+
+    private static Result calculateBallAndStrike(List<Integer> computer, int[] input) {
+        int ballCount = 0;
+        int strikeCount = 0;
+
+        for (int i = 0; i < input.length; i++) {
+            // 변수 할당
+            boolean ballCheck = computer.contains(input[i]);
+            boolean strikeCheck = computer.get(i) == input[i];
+
+            if (ballCheck && strikeCheck) {
+                strikeCount++;
+            }else if (ballCheck) {
+                ballCount++;
+            }
+        }
+
+        return new Result(ballCount, strikeCount);
+    }
+
+    // 결과 DTO를 하나 만들자
+    static class Result {
+        int ballCount;
+        int strikeCount;
+
+        Result(int ballCount, int strikeCount) {
+            this.ballCount = ballCount;
+            this.strikeCount = strikeCount;
+        }
+    }
+
+
 }
