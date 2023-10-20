d811b3f7a88661b4160afe9e2bea120b41b402c2
jongwonpc <c3392302@naver.com>
2023-10-20 15:43:01 +0900

    feat: 출력 기능 구현

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,31 @@
+package baseball.view;
+
+public class OutputView {
+
+    public static void printGameStart(){
+        System.out.print("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void printBallAndStrike(int ball, int strike){
+        if(ball == 0 && strike == 0){
+            System.out.println("낫싱");
+        }
+        if(ball != 0 && strike == 0){
+            System.out.println(ball + "볼");
+        }
+        if(ball == 0 && strike != 0){
+            System.out.println(strike + "스트라이크");
+        }
+        if(ball != 0 && strike != 0){
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
+    }
+
+    public static void printGameEnd(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public static void main(String[] args) {
+        printBallAndStrike(0,2);
+    }
+}
