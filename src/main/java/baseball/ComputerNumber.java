4e696cf7ddd66edfef644f58d28fdab8486ec5f4
aammddkkzxc <97333563+aammddkkzxc@users.noreply.github.com>
2023-10-20 20:02:33 +0900

    컴퓨터 번호 생성 기능 구현

---
+++ src/main/java/baseball/ComputerNumber.java
@@ -0,0 +1,19 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComputerNumber {
+
+    public List<Integer> randomNumberGenerator() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+}
