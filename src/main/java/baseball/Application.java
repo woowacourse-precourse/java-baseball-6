9a5cdd1cc99e28e88b8bfbfc8294fe6a6293d1b4
soozzang <soozzang@inha.edu>
2023-10-21 01:05:56 +0900

    Feat: Main 메소드 구성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
-
+import baseball.controller.BaseballController;
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseballController baseballController = new BaseballController();
+        try {
+            baseballController.game();
+        } catch(IllegalArgumentException e){
+            throw new IllegalArgumentException();
+        }
     }
 }
