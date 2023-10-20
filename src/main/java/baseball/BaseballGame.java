8ca6e3804b16946f637bb61620a7a1c63620d3e8
jungeun-choi <jungeun5.choi@gmail.com>
2023-10-21 00:26:25 +0900

    feat: 기본 입출력문을 BaseballGame 반복문에 적용

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,11 +1,34 @@
 package baseball;
 
+/* 게임을 반복 진행 */
 public class BaseballGame {
-	/* 게임을 반복 진행 */
+	private final BaseballView baseballView = new BaseballView();
+	private boolean isGameEnd = false; // 게임이 완전히 종료되는지 여부
 
 	public void startBaseballGame() {
-		while (true) {
+		do {
+			processBaseballGame();
+		} while (!isGameEnd);
+	}
 
-		}
+	private void processBaseballGame() {
+		// 게임 출력
+		baseballView.printGameStart();
+
+		// 유저가 숫자 입력
+		baseballView.printUserNumber();
+		String userNumber = baseballView.inputUserNumber();
+
+		// 판정
+		baseballView.printResultBall(1);
+		baseballView.printResultStrike(1);
+		baseballView.printResultNothing();
+
+		// 게임 종료
+		baseballView.printFinishGame();
+
+		// 유저가 게임 재시작, 종료 선택
+		baseballView.printSelectRestartOrEnd();
+		int selectNumber = baseballView.inputSelectRestartOrEnd();
 	}
 }
--- src/main/java/baseball/BaseballView.java
+++ src/main/java/baseball/BaseballView.java
@@ -15,9 +15,11 @@ public class BaseballView {
 	public void printResultBall(int count) {
 		System.out.printf("%d볼 ", count);
 	}
+
 	public void printResultStrike(int count) {
 		System.out.printf("%d스트라이크", count);
 	}
+
 	public void printResultNothing() {
 		System.out.print("낫싱");
 	}
@@ -25,13 +27,19 @@ public class BaseballView {
 	public void printFinishGame() {
 		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 	}
-	public void printSelectRestartOrEnd()	{
+
+	public void printSelectRestartOrEnd() {
 		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 	}
 
 	/* 값을 입력 받을 때 사용 */
-	public String inputNumber() {
+	public String inputUserNumber() {
 		Scanner sc = new Scanner(System.in);
 		return sc.nextLine();
+	}
+
+	public int inputSelectRestartOrEnd() {
+		Scanner sc = new Scanner(System.in);
+		return sc.nextInt();
 	}
 }
