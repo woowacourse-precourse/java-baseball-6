105a14c0cbd1a4d7fe5159c689022cab58b51f5c
acs0209 <annjae4676@naver.com>
2023-10-20 14:46:41 +0900

    test: refactor test code for validation

--- src/test/java/baseball/validation/InputValidationTest.java
+++ src/test/java/baseball/validation/InputValidationTest.java
@@ -3,14 +3,31 @@ package baseball.validation;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class InputValidationTest {
 
     @Test
     @DisplayName("입력된 문자열이 자연수로 이루어져 있는지 테스트")
     void validateNaturalNumberTest() {
-        assertThatThrownBy(() -> InputValidation.validateNaturalNumber("12ㅌ"))
+        // given
+        InputValidation inputValidation = new InputValidation();
+
+        // when
+        String invalidNaturalNumber = "12z";
+        String emptyString = "";
+        String blankSpaceString = "   ";
+
+        // then
+        assertThatThrownBy(() -> inputValidation.validateNaturalNumber(invalidNaturalNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("입력된 문자열은 자연수로만 이루어져야 합니다.");
+
+        assertThatThrownBy(() -> inputValidation.validateNaturalNumber(emptyString))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("입력된 문자열은 자연수로만 이루어져야 합니다.");
+
+        assertThatThrownBy(() -> inputValidation.validateNaturalNumber(blankSpaceString))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("입력된 문자열은 자연수로만 이루어져야 합니다.");
     }
