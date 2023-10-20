3977a309dfb7b43145d4de4fd307d7f1f54dec6e
chs98412 <chs98412@gmail.com>
2023-10-20 19:40:56 +0900

    feat: 숫자를 입력받았는지 체크 try-catch를 이용해서 숫자로 변환해보는 작업이 옳은 방법인지 생각해볼 필요

--- src/main/java/baseball/controller/InputController.java
+++ src/main/java/baseball/controller/InputController.java
@@ -6,7 +6,18 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputController {
 
     public String playerInput() {
-        return Console.readLine();
+        String inputString = Console.readLine();
+        validate(inputString);
+        return inputString;
     }
 
+
+    private void validate(String input) {
+        try {
+            int a = Integer.parseInt(input);
+            System.out.println(a);
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
