8cfafc5c7f780934ff4c57545006a274418fbcb1
ffolabear <ffolabear@gmail.com>
2023-10-21 01:23:51 +0900

    feat : 사용자가 숫자와 리플레이 명령어를 입력하는 기능구현

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,4 +1,15 @@
 package baseball;
 
 public class Player {
+
+    private final InputUtil inputUtil = new InputUtil();
+
+    public int inputNumber() {
+        return inputUtil.inputNumber();
+    }
+
+    public int inputReplay() {
+        return inputUtil.inputReplay();
+    }
+
 }
