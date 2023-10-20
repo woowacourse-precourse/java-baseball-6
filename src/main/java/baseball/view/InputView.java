657b3bb407dc10e430ca14974f75bce33a90fdbc
SehyeonKang <gedfgrdt@naver.com>
2023-10-20 12:45:56 +0900

    feat(InputView): 입력 뷰 클래스 추가, 정답 추측 숫자를 입력받는 기능 추가

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,11 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String readGuessNumbers() {
+        String inputString = Console.readLine();
+        return inputString;
+    }
+}
