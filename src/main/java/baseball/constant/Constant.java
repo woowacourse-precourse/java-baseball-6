e0c38e2951413410e8d3d60d11f19b1223bafbd6
Hugh <lee2317884@gmail.com>
2023-10-20 16:42:39 +0900

    feat: 숫자 0 상수 추가

--- src/main/java/baseball/constant/Constant.java
+++ src/main/java/baseball/constant/Constant.java
@@ -4,5 +4,6 @@ public class Constant {
     public static final int NUMBER_LENGTH_LIMIT = 3;
     public static final int START_NUMBER = 1;
     public static final int END_NUMBER = 9;
+    public static final int ZERO_NUMBER = 0;
     public static final String BLANK_VALUE = "";
 }
