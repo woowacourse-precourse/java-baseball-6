802ac436493b4368f5c7f9e05bd049ef67b20456
wooteco-daram <wooteco.daram@gmail.com>
2023-10-20 13:14:39 +0900

    feat: 볼과 스트라이크 수를 담은 `BaseBallResult` 객체 추가

---
+++ src/main/java/baseball/BaseBallResult.java
@@ -0,0 +1,9 @@
+package baseball;
+
+public class BaseBallResult {
+    private int ball = 0;
+    private int strike = 0;
+
+    public BaseBallResult(BaseBallNumberGroup answer, BaseBallNumberGroup user) {
+    }
+}
