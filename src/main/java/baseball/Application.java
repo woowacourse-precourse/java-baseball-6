ed604af5892ed82cd110ea8ed1d958088da18e6c
devhoya97 <diajbh123@gmail.com>
2023-10-20 18:01:16 +0900

    style : 상수명, 메서드명, 매개변수명 변경 구글 컨벤션에 맞도록 각종 이름 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,25 +9,25 @@ public class Application {
     static final int MIN_NUMBER = 1;
     static final int MAX_NUMBER = 9;
     static final int NUMBERS_LENGTH = 3;
-    static final String MORE_GAME = "1";
-    static final String QUIT_GAME = "2";
-    static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
-    static final String GET_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
-    static final String BALL_MESSAGE = "볼";
-    static final String STRIKE_MESSAGE = "스트라이크";
-    static final String NOTHING_MESSAGE = "낫싱";
+    static final String GAME_RESTART = "1";
+    static final String GAME_OVER = "2";
+    static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    static final String ENTER_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    static final String BALL = "볼";
+    static final String STRIKE = "스트라이크";
+    static final String NOTHING = "낫싱";
     static final String ANSWER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    static final String GET_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    static final String GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         playGames();
     }
     public static void playGames() {
-        printStart();
+        printGameStart();
         do {
             playGameOnce();
             printAnswer();
-        } while(getMoreGame().equals(MORE_GAME));
+        } while(getGameRestart().equals(GAME_RESTART));
     }
     public static void playGameOnce() {
         List<Integer> computerNumbersList = getComputerNumbersList();
@@ -37,17 +37,17 @@ public class Application {
             List<Integer> userNumbersList = getUserNumbersList();
             ballCount = getBallCount(computerNumbersList, userNumbersList);
             strikeCount = getStrikeCount(computerNumbersList, userNumbersList);
-            printBallStrike(ballCount, strikeCount);
+            printBallStrikeCount(ballCount, strikeCount);
         } while(!isAnswer(strikeCount));
     }
-    public static void printStart() {
-        System.out.println(START_MESSAGE);
+    public static void printGameStart() {
+        System.out.println(GAME_START_MESSAGE);
     }
     public static void printAnswer() {
         System.out.println(ANSWER_MESSAGE);
-        System.out.println(GET_RESTART_MESSAGE);
+        System.out.println(GAME_RESTART_MESSAGE);
     }
-    public static void printBallStrike(int ballCount, int strikeCount) {
+    public static void printBallStrikeCount(int ballCount, int strikeCount) {
         printBallCount(ballCount);
         printStrikeCount(strikeCount);
         printNothing(ballCount, strikeCount);
@@ -55,17 +55,17 @@ public class Application {
     }
     public static void printBallCount(int ballCount) {
         if (ballCount != 0) {
-            System.out.print(ballCount+BALL_MESSAGE+" ");
+            System.out.print(ballCount+ BALL +" ");
         }
     }
     public static void printStrikeCount(int strikeCount) {
         if (strikeCount != 0) {
-            System.out.print(strikeCount+STRIKE_MESSAGE);
+            System.out.print(strikeCount+ STRIKE);
         }
     }
     public static void printNothing(int ballCount, int strikeCount) {
         if (ballCount == 0 && strikeCount == 0) {
-            System.out.print(NOTHING_MESSAGE);
+            System.out.print(NOTHING);
         }
     }
     public static boolean isAnswer(int strikeCount) {
@@ -105,7 +105,7 @@ public class Application {
     }
     public static List<Integer> getUserNumbersList() {
         List<Integer> userNumbersList = new ArrayList<>();
-        System.out.print(GET_NUMBER_MESSAGE);
+        System.out.print(ENTER_NUMBER_MESSAGE);
         String userNumbersString = Console.readLine();
         validateUserNumbersString(userNumbersString);
         for(String userNumberString : userNumbersString.split("")) {
@@ -113,41 +113,41 @@ public class Application {
         }
         return userNumbersList;
     }
-    public static String getMoreGame() {
+    public static String getGameRestart() {
         String moreGame = Console.readLine();
-        validateMoreGameString(moreGame);
+        validateGameRestartString(moreGame);
         return moreGame;
     }
     public static void validateUserNumbersString(String userNumbers) {
         validateEmptyOrNullOfInputString(userNumbers);
-        validateStringLength(userNumbers, NUMBERS_LENGTH);
-        validateStringDistinct(userNumbers);
-        validateStringDigit(userNumbers);
+        validateInputStringLength(userNumbers, NUMBERS_LENGTH);
+        validateInputStringDistinct(userNumbers);
+        validateInputStringDigit(userNumbers);
     }
-    public static void validateMoreGameString(String moreGame) {
-        validateEmptyOrNullOfInputString(moreGame);
-        if (!moreGame.equals(MORE_GAME) && !moreGame.equals(QUIT_GAME)) {
-            throw new IllegalArgumentException("[ERROR] 입력 값이 " + MORE_GAME + " 또는 " + QUIT_GAME + " 가 아닙니다.");
+    public static void validateGameRestartString(String gameRestart) {
+        validateEmptyOrNullOfInputString(gameRestart);
+        if (!gameRestart.equals(GAME_RESTART) && !gameRestart.equals(GAME_OVER)) {
+            throw new IllegalArgumentException("[ERROR] 입력 값이 " + GAME_RESTART + " 또는 " + GAME_OVER + " 가 아닙니다.");
         }
     }
-    public static void validateEmptyOrNullOfInputString(String str) {
-        if (str == null || str.isEmpty()) {
+    public static void validateEmptyOrNullOfInputString(String inputString) {
+        if (inputString == null || inputString.isEmpty()) {
             throw new IllegalArgumentException("[ERROR] 입력 값이 null이거나 비어있습니다.");
         }
     }
-    public static void validateStringLength(String str, int length) {
-        if (str.length() != length) {
+    public static void validateInputStringLength(String inputString, int length) {
+        if (inputString.length() != length) {
             throw new IllegalArgumentException("[ERROR] 입력 값의 길이가 " + length + "(이)가 아닙니다.");
         }
     }
-    public static void validateStringDistinct(String str) {
-        Set<String> inputSet = new HashSet<>(Arrays.asList(str.split("")));
-        if (str.length() != inputSet.size()) {
+    public static void validateInputStringDistinct(String inputString) {
+        Set<String> inputStringSet = new HashSet<>(Arrays.asList(inputString.split("")));
+        if (inputString.length() != inputStringSet.size()) {
             throw new IllegalArgumentException("[ERROR] 입력 값에 중복이 있습니다.");
         }
     }
-    public static void validateStringDigit(String str) {
-        for (char ch : str.toCharArray()) {
+    public static void validateInputStringDigit(String inputString) {
+        for (char ch : inputString.toCharArray()) {
             if (!Character.isDigit(ch)) {
                 throw new IllegalArgumentException("[ERROR] 입력 값이 숫자로만 이루어져 있지 않습니다.");
             }
