b9c45ab7e878a6b2ccbb5ad71d07019377ad7127
ga0nii <kgw0124@kakao.com>
2023-10-20 21:57:36 +0900

    [feat/사용자] 1. 사용자에게 수를 입력 받는 함수 생성 - 함수 내에서 예외 처리를 수행하도록 함 2. 사용자의 재실행 여부를 판단할 변수 생성

---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,28 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private boolean isContinueGame = true;
+
+    public List<Integer> getNumbers(){
+        String input = Console.readLine();
+
+        List<Integer> numbers = new ArrayList<>();
+        for(int i=0; i<input.length(); i++){
+            Integer number = Character.getNumericValue(input.charAt(i));
+            numbers.add(number);
+        }
+
+        Exception exception = new Exception();
+        exception.validateNumbers(numbers);
+        return numbers;
+    }
+
+    public boolean getIsContinueGame(){
+        return isContinueGame;
+    }
+}
