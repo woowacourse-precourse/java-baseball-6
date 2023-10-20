ba40124510a66d27f3f893b897b29db93922a866
HeeHeeg <qmffndoden@naver.com>
2023-10-20 20:51:42 +0900

    6-1, 6-2. 게임을 종료한 후 게임을 다시 시작하거나 완전히 종료할 수 있다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,8 +9,6 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        System.out.print("숫자를 입력해주세요 : ");
 
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
@@ -19,12 +17,13 @@ public class Application {
                 computer.add(randomNumber);
             }
         }
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.print("숫자를 입력해주세요 : ");
         while (true) {
             String input = Console.readLine();
             char[] player = input.toCharArray();
             int strike = 0, ball = 0;
             String nothing = "낫싱";
-
             for (int i = 0; i < 3; i++) {
                 int playerNum = Character.getNumericValue(player[i]);
                 if (computer.get(i) == playerNum) {
@@ -37,7 +36,16 @@ public class Application {
                 if (strike == 3 && ball == 0) {
                     System.out.println(strike + "스트라이크");
                     System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                    return;
+                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                    String choice = Console.readLine();
+                    int pick = Integer.parseInt(choice);
+                    if (pick == 1) {
+                        main(args);
+                    } else if (pick == 2) {
+                        break;
+                    } else {
+                        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                    }
                 } else if (strike == 0 && ball != 0) {
                     System.out.println(ball + "볼");
                 } else if (strike != 0 && ball == 0) {
@@ -45,10 +53,10 @@ public class Application {
                 } else {
                     System.out.println(strike + "스트라이크, " + ball + "볼");
                 }
-
             } else {
                 System.out.println(nothing);
             }
+            //숫자 야구 게임 test를 위해 임시 사용중인 코드.
             System.out.println(computer);
         }
     }
