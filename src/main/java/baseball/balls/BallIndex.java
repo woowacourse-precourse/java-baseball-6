da5e74a422518d901d0facc091badd1277c64b02
SJ70 <seojae70@gmail.com>
2023-10-21 02:02:25 +0900

    Feat & Test: BallIndex equals 오버라이딩

--- src/main/java/baseball/balls/BallIndex.java
+++ src/main/java/baseball/balls/BallIndex.java
@@ -1,11 +1,28 @@
 package baseball.balls;
 
+import java.util.Objects;
+
 public class BallIndex {
 
     private final int index;
 
     public BallIndex(int index) {
         this.index = index;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        BallIndex otherBallIndex = (BallIndex) obj;
+        return index == otherBallIndex.index;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(index);
     }
 
 }
