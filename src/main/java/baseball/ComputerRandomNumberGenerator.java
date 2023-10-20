a637159aca6796c04413c7b668bfe29563dd7ac8
tlarbals824 <simkm0824@naver.com>
2023-10-20 14:50:15 +0900

    feat : 랜덤 숫자 생성 인터페이스 구현 및 랜덤 숫자 배열 생성 및 사용자 입력과 랜덤 숫자를 통해 게임 승리 여부 반환 메소드 추가

---
+++ src/main/java/baseball/ComputerRandomNumberGenerator.java
@@ -0,0 +1,41 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComputerRandomNumberGenerator implements RandomNumberGenerator<List<Integer>, ComputerRandomNumber> {
+    @Override
+    public ComputerRandomNumber generate(Integer randomNumberSize) {
+        List<Integer> computer = new ArrayList<>();
+        while(computer.size() < randomNumberSize){
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if(!computer.contains(randomNumber)){
+                computer.add(randomNumber);
+            }
+        }
+        return new ComputerRandomNumber(computer);
+    }
+
+    @Override
+    public Boolean match(final List<Integer> input, final ComputerRandomNumber generatedNumber) {
+        final BaseBallScore baseBallScore = calculateBaseBallScore(generatedNumber, input);
+        System.out.println(baseBallScore);
+        return baseBallScore.verifyingWinGame();
+    }
+
+    private BaseBallScore calculateBaseBallScore(final ComputerRandomNumber randomNumber, final List<Integer> input) {
+        final BaseBallScore baseBallScore = new BaseBallScore(randomNumber.size());
+        for(int i=0;i<input.size();i++){
+            final Integer findNumberIndex = randomNumber.indexOf(input.get(i));
+            if(findNumberIndex.equals(ComputerRandomNumber.NOT_FOUND)){
+                baseBallScore.increaseNothing();
+            }else{
+                if(findNumberIndex==i) baseBallScore.increaseStrike();
+                else baseBallScore.increaseBall();
+            }
+        }
+        return baseBallScore;
+    }
+}
