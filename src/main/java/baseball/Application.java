d4da74e0a6a16e83fcafcf308b49b455b17109f1
leeyeongjae <xingxing2001@cau.ac.kr>
2023-10-20 14:41:58 +0900

    1. 시작문구 출력 방식 변경 2. 게임을 재시작할 때마다 Game인스턴스를 새로 생성하는 방식으로 게임 상태 초기화 진행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,16 +1,12 @@
 package baseball;
 public class Application {
     public static void main(String[] args) {
-        Game game = new Game(); //게임 인스턴스 생성
-        game.printStartMessage(); //게임 시작 문구 출력
-        //숫자 야구 진행
-        while(true){
-            game.initGame(); //게임 상태 초기화
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while(true){ //숫자 야구 진행
+            Game game = new Game(); //게임 인스턴스 생성 및 게임 상태 초기화
             game.startGame(); //게임 시작
             if(!game.isRestart()) //게임의 재시작 여부 결정
                 break;
         }
-
-
     }
 }
