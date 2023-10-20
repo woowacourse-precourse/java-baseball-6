71aa80e07f23007c65936eea596f9f751e03321b
LeeJaeHoon <dlwogns3413@naver.com>
2023-10-20 22:58:55 +0900

    feat: RandomNumberPicker class generateDistinctNumbers 메서드 구현

--- src/main/java/baseball/domain/RandomNumberPicker.java
+++ src/main/java/baseball/domain/RandomNumberPicker.java
@@ -1,12 +1,22 @@
 package baseball.domain;
 
-import java.util.Collections;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class RandomNumberPicker {
 
     public List<Integer> generateDistinctNumbers(int numberLength) {
-        return Collections.emptyList();
+
+        List<Integer> distinctNumberList = new ArrayList<>();
+        while (distinctNumberList.size() < numberLength) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!distinctNumberList.contains(randomNumber)) {
+                distinctNumberList.add(randomNumber);
+            }
+        }
+        return distinctNumberList;
     }
 
 }
