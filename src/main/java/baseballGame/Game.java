9df4a186f5f93ed5754a5f2f471eba61aa8ff79f
dltmddus <tmddus4671@gmail.com>
2023-10-20 13:11:36 +0900

    feat: gameStart 메소드 구현

--- src/main/java/baseballGame/Game.java
+++ src/main/java/baseballGame/Game.java
@@ -9,7 +9,7 @@ import static constant.Constants.*;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 public class Game {
-    List<Integer> answer = new ArrayList<>(); // 실제 정답
+    List<Integer> answer = new ArrayList<>();
 
     public Game() {
         for (int i = 0; i < 3; i++) {
@@ -26,7 +26,31 @@ public class Game {
         do {
             RESULT = false;
             game = new Game();
+            game.gameStart();
         }while(game.restart());
+    }
+    private void gameStart(){
+
+        do {
+            List<Integer> myAnswer = new ArrayList<>(); // 내가 유추한 정답
+            System.out.print(INPUT_NUMBER);
+            String inputString = Console.readLine();
+
+            // 입력한 3자리 문자열(사실 숫자) 하나씩 순회하면서 myAnswer 배열에 추가해줌
+            for (int i = 0; i < inputString.length(); i++) {
+                if(inputString.charAt(i) < '0' || inputString.charAt(i) > '9'){
+                    throw new IllegalArgumentException(WRONG_INPUT_TYPE);
+                }
+                char charAtIndex = inputString.charAt(i);
+                int intAtIndex = Character.getNumericValue(charAtIndex);
+                if (!myAnswer.contains(intAtIndex)) { // 중복 예외 처리
+                    myAnswer.add(intAtIndex);
+                }
+            }
+            // todo
+
+        }while(!RESULT);
+
     }
 
     private boolean restart() {
