478b6e95d367e286733476e4353161240f764ddd
sun1234 <sdfjikl8787@gmail.com>
2023-10-20 19:09:33 +0900

    feat: BaseballGame객체의 기능 추가

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,69 @@
+package baseball;
+
+import baseball.domain.BaseballNumber;
+import baseball.service.BaseballCheckAnswerService;
+import baseball.service.BaseballCreateService;
+import baseball.service.BaseballGameFlowService;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+public class BaseballGame {
+
+	private BaseballCreateService baseballCreateService;
+	private BaseballCheckAnswerService baseballCheckAnswerService;
+	private BaseballGameFlowService baseballGameFlowService;
+	private InputView inputView;
+	private OutputView outputView;
+
+	public BaseballGame() {
+		this.baseballCreateService = new BaseballCreateService();
+		this.baseballCheckAnswerService = new BaseballCheckAnswerService();
+		this.baseballGameFlowService = new BaseballGameFlowService();
+		this.inputView = new InputView();
+		this.outputView = new OutputView();
+	}
+
+	public void play() {
+		try {
+			init();
+			progress();
+			retryOrExit();
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			throw e;
+		}
+	}
+
+	public void init() {
+		baseballGameFlowService.reset();
+		outputView.printStartMessage();
+	}
+
+	public void progress() {
+		BaseballNumber baseballNumber = baseballCreateService.createBaseballNumber();
+
+		do {
+			outputView.printInputDigitMessage();
+
+			int inputDigit = inputView.inputDigit();
+			int ballCount = baseballCheckAnswerService.getBallCount(baseballNumber, inputDigit);
+			int StrikeCount = baseballCheckAnswerService.getStrikeCount(baseballNumber, inputDigit);
+
+			outputView.printResult(ballCount, StrikeCount);
+			baseballGameFlowService.checkClear(StrikeCount);
+		} while (!baseballGameFlowService.isClear());
+
+		outputView.printClearMessage();
+	}
+
+	public void retryOrExit() {
+		outputView.printAskContinueMessage();
+		int command = inputView.inputCommand();
+
+		baseballGameFlowService.checkContinue(command);
+
+		if (baseballGameFlowService.isContinue()) {
+			play();
+		}
+	}
+}
