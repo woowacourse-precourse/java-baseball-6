9e87710e74dacffeef2cf4895b18863252ce26cc
mingdu9 <min035759@gmail.com>
2023-10-21 03:42:53 +0900

    feat: main 로직 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,20 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        int[] com = Creator.getComputer();
+        int[] user;
+        while (true) {
+            Counter counter = new Counter();
+            user = Creator.getUser();
+            counter.checkCounts(com, user, counter);
+            if (counter.isCorrect()) {
+                if (Manager.restart()) {
+                    com = Creator.getComputer();
+                } else {
+                    break;
+                }
+            }
+        }
     }
 }
