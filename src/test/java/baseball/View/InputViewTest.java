c0c445f1142a4900efea996c52b50d94c0235798
PlutoWooSeok <plutoxxx1014@gmail.com>
2023-10-20 19:44:59 +0900

    create test InputView

---
+++ src/test/java/baseball/View/InputViewTest.java
@@ -0,0 +1,14 @@
+package baseball.View;
+
+import org.junit.jupiter.api.Test;
+
+class InputViewTest {
+
+    @Test
+    void enterAnswer() {
+    }
+
+    @Test
+    void printAskNumber() {
+    }
+}
