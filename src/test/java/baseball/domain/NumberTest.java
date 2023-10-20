d1b73c34fa471124e40f67a3cf4e1abe9572113e
elyudwo <dudqk9696@naver.com>
2023-10-20 18:16:56 +0900

    [Test] : 숫자가 3개가 아닐 경우 예외 처리 테스트 코드 작성

---
+++ src/test/java/baseball/domain/NumberTest.java
@@ -0,0 +1,25 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NumberTest {
+
+    private final Number number = new Number();
+
+    @DisplayName("숫자가 3개가 아닐경우 예외가 발생한다.")
+    @Test
+    void validateNumberSize() {
+        // given
+        List<Integer> numberList = List.of(1, 2, 3, 4);
+
+        // when, then
+        assertThatThrownBy(() -> number.validateNumberSize(numberList))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
