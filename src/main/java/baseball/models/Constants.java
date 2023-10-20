995f6a732a9cf09c8575a6f6b41af2a233c87fa5
jy016011 <39334789+jy016011@users.noreply.github.com>
2023-10-20 13:40:28 +0900

    Constant 클래스 수정

--- src/main/java/baseball/models/Constants.java
+++ src/main/java/baseball/models/Constants.java
@@ -1,2 +1,10 @@
-package baseball.models;public class Constants {
+package baseball.models;
+
+public class Constants {
+    public static final int ANSWER_LENGTH = 3;
+    public static final int USER_ANSWER_LOWER_LIMIT = 1;
+    public static final int USER_ANSWER_UPPER_LIMIT = 9;
+    public static final int RESTART_NUMBER = 1;
+    public static final int END_NUMBER = 2;
+    public static final int MAX_STRIKES = 3;
 }
