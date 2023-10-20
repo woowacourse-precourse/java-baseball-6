d35e15e5987b4cb91d83007c7d9de05e9939501f
liarreez <liarreez@gmail.com>
2023-10-20 15:21:24 +0900

    Feature(Input): feature that allows client to re-input when input in wrong format.
    If the application crashes every time input incorrectly, it will be inconvenient to use. Therefore, instead of stopping if the wrong format is inputted, guides client to the correct format and allows client to re-input it.

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -4,29 +4,36 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 
 public class Input {
+    private static final int gameNumberDigits = BaseballGame.gameNumberDigits;
     private static final Output output = new Output();
     private ArrayList<Integer> userNumbers = new ArrayList<>();
 
     public ArrayList<Integer> inputUserNumbers() {
         userNumbers.clear();
-        output.printInputMessage();
         String inputtedNumber = inputStringNumber();
-        checkInputtedNumber(inputtedNumber);
         convertStringNumberToIntegerList(inputtedNumber);
         return userNumbers;
     }
 
     private String inputStringNumber() {
-        return Console.readLine();
+        try {
+            output.printInputMessage();
+            String inputtedNumber =  Console.readLine();
+            checkInputtedNumber(inputtedNumber);
+            return inputtedNumber;
+        } catch (IllegalArgumentException e) {
+            System.out.printf("%d자리의 중복 되지 않는 적절한 수를 입력해주세요.\n", gameNumberDigits);
+            return inputStringNumber();
+        }
     }
 
     private void checkInputtedNumber(String inputtedNumber) {
-        checkNumbersDigit(inputtedNumber);
-        checkNumbersDuplicate(inputtedNumber);
+            checkNumbersDigit(inputtedNumber);
+            checkNumbersDuplicate(inputtedNumber);
     }
 
     private void checkNumbersDigit(String inputtedNumber) {
-        if(inputtedNumber.length() != BaseballGame.gameNumberDigits) {
+        if(inputtedNumber.length() != gameNumberDigits) {
             throw new IllegalArgumentException();
         }
     }
@@ -52,10 +59,15 @@ public class Input {
     }
 
     public String inputRestartOption() {
-        output.printSelectRestartMessage();
-        String inputtedOption = Console.readLine();
-        checkAppropriateOption(inputtedOption);
-        return inputtedOption;
+        try{
+            output.printSelectRestartMessage();
+            String inputtedOption = Console.readLine();
+            checkAppropriateOption(inputtedOption);
+            return inputtedOption;
+        } catch (IllegalArgumentException e) {
+            System.out.println("1 또는 2를 입력해주세요.");
+            return inputRestartOption();
+        }
     }
 
     private void checkAppropriateOption(String inputtedOption) {
