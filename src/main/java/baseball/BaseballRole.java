20589822cd993f0944ed141cc56458f33a056aed
GaBaljaintheroom <junsu1222@naver.com>
2023-10-20 13:56:13 +0900

    feat: 재시작 여부 validate 메서드 작성

--- src/main/java/baseball/BaseballRole.java
+++ src/main/java/baseball/BaseballRole.java
@@ -3,7 +3,9 @@ package baseball;
 public enum BaseballRole {
     MAX_BASEBALL_NUMBER_SIZE(3),
     MIN_BASEBALL_NUMBER(1),
-    MAX_BASEBALL_NUMBER(9);
+    MAX_BASEBALL_NUMBER(9),
+    GAME_RESTART(1),
+    GAME_EXIT(2);
 
     private final int value;
 
@@ -13,5 +15,13 @@ public enum BaseballRole {
 
     public int getValue() {
         return value;
+    }
+
+
+
+    private static void validateRestartMessage(int value){
+        if(value != GAME_RESTART.value && value != GAME_EXIT.value){
+            throw new IllegalArgumentException(ErrorMessage.INPUT_RESTART_ERROR.getMessage());
+        }
     }
 }
