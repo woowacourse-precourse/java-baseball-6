baf28141203353d31e939c34956770017e02657f
Octoping <mooner925@gmail.com>
2023-10-20 18:14:02 +0900

    feat: 두 숫자를 비교한 스트라이크 정보를 출력하는 기능 추가

--- src/main/java/baseball/presentation/BaseballView.java
+++ src/main/java/baseball/presentation/BaseballView.java
@@ -1,7 +1,31 @@
 package baseball.presentation;
 
+import baseball.domain.vo.JudgeResult;
+
 public class BaseballView {
     public void printGameStartMessage() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void printJudgeResult(JudgeResult judgeResult) {
+        System.out.println(getJudgeResultMessage(judgeResult));
+    }
+
+    private String getJudgeResultMessage(JudgeResult judgeResult) {
+        if(judgeResult.isNothing()) {
+            return "낫싱";
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        if(judgeResult.ball() > 0) {
+            sb.append(judgeResult.ball()).append("볼 ");
+        }
+
+        if(judgeResult.strike() > 0) {
+            sb.append(judgeResult.strike()).append("스트라이크");
+        }
+
+        return sb.toString();
     }
 }
