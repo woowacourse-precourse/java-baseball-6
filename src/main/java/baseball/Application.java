d459bc8bf3d8992a9e10686e521afe69ad19806a
seona-moon <fhflwhwl5@gmail.com>
2023-10-20 15:01:55 +0900

    게임 로직 기초 세팅

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,12 @@
 package baseball;
 
-public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
 
-    }
+public class Application {
+
+  public static void main(String[] args) {
+    // TODO: 프로그램 구현
+
+    //게임 시작
+    new GameStart();
+  }
 }
--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,2 +1,28 @@
-package baseball;public class Computer {
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+
+  private List<Integer> Num = new ArrayList<>();
+
+  public Computer() {
+    GenerateRandomNum();
+  }
+
+  public void GenerateRandomNum() {
+    while (Num.size() < 3) {
+      int randomNumber = Randoms.pickNumberInRange(1, 9);
+      if (!Num.contains(randomNumber)) {
+        Num.add(randomNumber);
+      }
+    }
+    System.out.println(Num);
+  }
+
+  public List<Integer> getNum() {
+    return Num;
+  }
 }
