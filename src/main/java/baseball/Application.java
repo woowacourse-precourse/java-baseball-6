5a3740304f411fb251ad51272fa70c0322415904
U-keun Song <sukeun319@gmail.com>
2023-10-20 14:52:48 +0900

    Feat : 재시작/종료 기능 추가
    `1`과 `2`를 입력하는 것으로 재시작/종료 기능을 구분하도록 구현했다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,16 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
+        boolean restart;
+        do {
+
+            System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ");
+            restart = Console.readLine().equals("1");
+        } while (restart);
     }
 }
