8a47f8e36142ef7eed0557b7c7108903467fe346
jy016011 <39334789+jy016011@users.noreply.github.com>
2023-10-20 12:33:16 +0900

    NumberChecker Ver1.2: 메시지 출력부분 MessageViewer에 위임.

--- src/main/java/baseball/NumberChecker.java
+++ src/main/java/baseball/NumberChecker.java
@@ -3,23 +3,8 @@ package baseball;
 import java.util.List;
 
 public class NumberChecker {
-    public StringBuilder checkNumber(List<Integer> user, List<Integer> computer){
-        StringBuilder checkResult = new StringBuilder();
-        int[] ballAndStrikeCounts = countBallAndStrike(user, computer);
-        int ballCount = ballAndStrikeCounts[0];
-        int strikeCount = ballAndStrikeCounts[1];
-        if (ballCount == 0 && strikeCount == 0)
-            checkResult.append("낫싱");
-        else if (ballCount > 0 && strikeCount > 0) {
-            checkResult.append(ballCount).append("볼 ");
-            checkResult.append(strikeCount).append("스트라이크");
-        }
-        else if (ballCount > 0) {
-            checkResult.append(ballCount).append("볼");
-        } else if (strikeCount > 0) {
-            checkResult.append(strikeCount).append("스트라이크");
-        }
-        return checkResult;
+    public int[] checkNumber(List<Integer> user, List<Integer> computer){
+        return countBallAndStrike(user, computer);
     }
 
     public int[] countBallAndStrike(List<Integer> user, List<Integer> computer){
