2061ae0f62d9a5eb8e2730f2778ffff00ab1b8e9
김명준 <86913355+mjj111@users.noreply.github.com>
2023-10-20 14:38:37 +0900

    feat(Computer): 테스트 코드 성공을 위한 최소한의 getRandomNumbers, randomNumbers 구현
    Computer 클레스를 선언하고 인스턴스 변수 randmoNumbers과 원하는 형식의 값을 반환하는 getRandomNumbers를 작성합니다.

---
+++ src/main/java/baseball/domain/Computer.java
@@ -0,0 +1,12 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Computer {
+    private List<Integer> randomNumbers;
+
+    public List<Integer> getRandomNumbers() {
+        return List.of(1, 2, 3);
+    }
+
+}
