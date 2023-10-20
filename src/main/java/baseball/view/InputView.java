56e2c6adc6560a2ba99498b60c0716c35e19d1e0
김효준 <khyojun1104@gmail.com>
2023-10-20 12:42:02 +0900

    [feat] InputView: 싱글톤 패턴 적용!

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -2,4 +2,18 @@ package baseball.view;
 
 public class InputView {
 
+    private static InputView instance = new InputView();
+
+    private InputView() {
+    }
+
+
+    public static InputView getInstance(){
+        return instance;
+    }
+
+
+
+
+
 }
