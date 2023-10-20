53274105f03a40589c342a52b51227c98a3cab25
useu <lakingsung@gmail.com>
2023-10-21 02:50:56 +0900

    feat: 메인 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        NumberBaseballGame game = new NumberBaseballGame();
+        boolean startGame = true;
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (startGame) {
+            game.start();
+            String input = Console.readLine();
+            if (!NumberBaseballUtility.isValidChoice(input)) {
+                throw new IllegalArgumentException();
+            }
+            if (input.equals("2")) {
+                startGame = false;
+            }
+        }
     }
 }
