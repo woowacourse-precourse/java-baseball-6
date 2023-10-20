5696ffb27583980ee54b00256ca027a346de86bf
leemiyeon <lemingul7@gmail.com>
2023-10-21 00:10:28 +0900

    feat: 사용자 입력 기능 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -8,14 +8,17 @@ public class Game {
 
     public void startGame() {
 
-        System.out.println("숫자 야구 게임을 시작합니다.");
-
         Number computer = new Number();
         Number player = new Number();
 
         List<Integer> computerNumber = computer.setRandomNumber();
         // log
         System.out.println("컴퓨터: " + computerNumber);
+
+        List<Integer> playerNumber = player.inputNumber();
+        // log
+        System.out.println("플레이어: " + playerNumber);
+
 
     }
 
--- src/main/java/baseball/Number.java
+++ src/main/java/baseball/Number.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,6 +9,10 @@ public class Number {
 
     public Number() {
     }
+
+    private Console console;
+    private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String ASK_FOR_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
 
     /**
      * 컴퓨터가 랜덤 3자리 숫자를 생성하는 메서드
@@ -21,6 +26,49 @@ public class Number {
         }
 
         return computer;
+    }
+
+    /**
+     * 사용자의 입력을 받아 list<integer>로 변환하는 함수
+     */
+    public List<Integer> inputNumber() {
+
+        System.out.println(START_MESSAGE);
+        System.out.print(ASK_FOR_NUMBER_MESSAGE);
+        String input = console.readLine();
+        int number = parseInt(input);
+
+        return getDigit(number);
+    }
+
+    /**
+     * int -> list<integer> 변환 함수
+     */
+    private List<Integer> getDigit(int number) {
+
+        List<Integer> digits = new ArrayList<>();
+
+        int digit1 = number / 100;
+        int digit2 = (number % 100) / 10;
+        int digit3 = number % 10;
+
+        digits.add(digit1);
+        digits.add(digit2);
+        digits.add(digit3);
+
+        return digits;
+    }
+
+    /**
+     * string -> int 변환 함수
+     */
+    private int parseInt(String input) {
+        try {
+            int number = Integer.parseInt(input);
+            return number;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("잘못된 값을 입력했습니다. 정수를 입력해주세요.)");
+        }
     }
 
 }
