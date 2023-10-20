112337f2708e56cf39325158c7822bb17a6f37d7
useu <lakingsung@gmail.com>
2023-10-20 21:48:00 +0900

    refactor: 클래스 이름 변경

--- src/main/java/baseball/NumberBaseballGame.java
+++ src/main/java/baseball/NumberBaseballGame.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BaseballGame {
+public class NumberBaseballGame {
     public static List<Integer> generateAnswer() {
         List<Integer> answer = new ArrayList<>();
         while (answer.size() < 3) {
