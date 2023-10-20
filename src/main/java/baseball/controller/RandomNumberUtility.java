facc8d30da5e81c232e5a0cbae41d15f69084d8a
JH-TT <leejongho9803@gmail.com>
2023-10-21 02:25:35 +0900

    refactor: 랜덤숫자뽑기를 static으로 변경
    - 기능이 한 개밖에 없으니 생성자를 이용하는것 보다는 정적으로 처리하는게 좋다고 판단

--- src/main/java/baseball/controller/RandomNumberUtility.java
+++ src/main/java/baseball/controller/RandomNumberUtility.java
@@ -4,14 +4,13 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class RandomNumberUtility {
     private static final int START_NUMBER = 1;
     private static final int END_NUMBER = 9;
     private static final int NUMBER_LENGTH = 3;
 
-    public String generateRandomNumbers() {
+    public static String generateRandomNumbers() {
         Set<Integer> randomNumbers = new LinkedHashSet<>();
         while(randomNumbers.size() != NUMBER_LENGTH) {
             randomNumbers.add(Randoms.pickNumberInRange(START_NUMBER, END_NUMBER));
