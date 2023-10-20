b53ea3f405db69a630e0f4b1a8455520dda43eb1
kimdohyung <pdohyung@gmail.com>
2023-10-20 18:39:50 +0900

    feat: 사용자의 문자 입력을 숫자 리스트로 변환하는 기능 구현

--- src/main/java/baseball/model/UserNumber.java
+++ src/main/java/baseball/model/UserNumber.java
@@ -1,4 +1,27 @@
 package baseball.model;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class UserNumber {
+	private List<Integer> userNumber;
+
+	public UserNumber() {
+		this.userNumber = List.of(0, 0, 0);
+	}
+
+	public List<Integer> getUserNumber() {
+		return userNumber;
+	}
+
+	public void newUserNumber(String input){
+		this.userNumber = stringToIntegerList(input);
+	}
+
+	private List<Integer> stringToIntegerList(String input){
+		return input.chars()
+			.map(Character::getNumericValue)
+			.boxed()
+			.collect(Collectors.toList());
+	}
 }
