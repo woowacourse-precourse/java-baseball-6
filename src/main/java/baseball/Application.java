ed854fbfcc78609a995832937203fc24ee1ce2f9
이병수 <qudtn0315@naver.com>
2023-10-20 18:26:51 +0900

    게임 재시작 메서드 예외처리 만듬 (코드 리팩토리 하기 전!)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,13 +1,12 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Application {
 
     static ArrayList<Integer> baseball = new ArrayList<>(3);
-    static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -39,24 +38,17 @@ public class Application {
         while(gameStart != 2)
         {
             randomNumberList();
-            System.out.println(baseball.get(0) + " " + baseball.get(1) + " " + baseball.get(2));
             while(result != 3)
             {
                 System.out.print("숫자를 입력해주세요 : ");
-                try
-                {
-                    String number = input.nextLine();
-                    doSomething(number);
-                    result = gameResult(number);
-                } catch(IllegalArgumentException e)
-                {
-                    System.out.println(e.getMessage());
-                    return;
-                }
+                String number = Console.readLine();
+                doSomething(number);
+                result = gameResult(number);
+
             }
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
-            gameStart = input.nextInt();
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            gameStart = restartGame(Console.readLine());
             result = -1;
         }
     }
@@ -68,7 +60,7 @@ public class Application {
 
         for(int i = 0; i < 3; i++)
         {
-            int count = number.charAt(i);
+            int count = number.charAt(i) - 48;
             if(baseball.contains(count))
             {
                 if(baseball.get(i) == count)
@@ -98,9 +90,30 @@ public class Application {
 
     public static void doSomething(String something) throws IllegalArgumentException
     {
-        int score = Integer.parseInt(something);
+        if(something.length() != 3)
+            throw new IllegalArgumentException("3개의 숫자를 입력하지 않으셨습니다.");
 
-        if(score < 100 || score > 999)
-            throw new IllegalArgumentException("숫자 범위를 초과하였습니다.");
+        for(int i = 0; i < something.length()-1; i++)
+        {
+            if(something.charAt(i) == something.charAt(i+1))
+            {
+                throw new IllegalArgumentException("값이 중복되었습니다.");
+            }
+        }
+
+        for(int i = 0; i < something.length(); i++) {
+            if(something.charAt(i) < '1' || something.charAt(i) > '9') {
+                throw new IllegalArgumentException("해당 숫자가 아닙니다.");
+            }
+        }
+    }
+
+
+    public static int restartGame(String something) throws IllegalArgumentException
+    {
+        if(!something.equals("1") && !something.equals("2"))
+            throw new IllegalArgumentException();
+
+        return Integer.parseInt(something);
     }
 }
