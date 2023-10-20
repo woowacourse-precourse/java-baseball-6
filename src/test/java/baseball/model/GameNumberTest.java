eed34569a424ef4175059722ef90c874c53c027a
WonyuChoi <phillip07290@naver.com>
2023-10-20 14:59:21 +0900

    test: 게임을 지속할 것인지를 판단하는 객체 테스트

---
+++ src/test/java/baseball/model/GameNumberTest.java
@@ -0,0 +1,51 @@
+package baseball.model;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class GameNumberTest {
+
+    private static final String END_NUMBER = "2";
+
+    @Test
+    @DisplayName("게임 넘버가 1일 때 게임을 지속할 수 있다고 알려준다")
+    void when_game_number_1_can_contin트ue_game() {
+        // given
+        GameNumber gameNumber = GameNumber.createDefault();
+
+        // when
+        boolean canContinueGame = gameNumber.canContinueGame();
+
+        // then
+        assertTrue(canContinueGame);
+    }
+
+    @Test
+    @DisplayName("게임 넘버가 2일 때 게임을 지속할 수 없다고 알려준다.")
+    void when_game_number_2_can_not_continue_game() {
+        // given
+        GameNumber gameNumber = GameNumber.createDefault();
+
+        // when
+        gameNumber.changeNumber(END_NUMBER);
+        boolean canContinueGame = gameNumber.canContinueGame();
+        // then
+        assertFalse(canContinueGame);
+    }
+
+    @Test
+    @DisplayName("지정된 입력 외 다른 입력이 들어오면 예외가 발생한다.")
+    void when_() {
+        // given
+        GameNumber gameNumber = GameNumber.createDefault();
+
+        // when & then
+        assertThrows(IllegalArgumentException.class, () ->
+            gameNumber.changeNumber("12")
+        );
+    }
+}
