1f53baecaedd669a51cf0e5e9588b3a01194ac32
PARKHWANYEONG <welcome0828@naver.com>
2023-10-20 14:05:14 +0900

    feat(controller): 컴퓨터 숫자 초기화 컨트롤러 구현

---
+++ src/main/java/baseball/service/GameService.java
@@ -0,0 +1,16 @@
+package baseball.service;
+
+import baseball.domain.GameNumbers;
+
+public class GameService {
+
+    private final GameNumbers gameNumbers;
+
+    public GameService(GameNumbers gameNumbers) {
+        this.gameNumbers = gameNumbers;
+    }
+
+    public void numbersInit() {
+        gameNumbers.init();
+    }
+}
