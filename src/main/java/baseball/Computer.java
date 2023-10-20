fd8ed21813f8f96d4a179600caf572f52fc97943
InGyu Moon <ansdlsrb1121@naver.com>
2023-10-20 22:51:07 +0900

    feat: 사용자가 입력한 수와 정답을 비교하는 함수 구현

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -16,4 +16,22 @@ public class Computer {
         }
         return answer;
     }
+
+    public BallAndStrikeData compareUserInputAndAnswer(List<Integer> userInput, List<Integer> answer){
+        int ball = 0;
+        int strike = 0;
+
+        for (int i = 0; i < userInput.size(); i++) {
+            if (userInput.get(i).equals(answer.get(i))) {
+                strike++;
+            }
+            else if (answer.contains(userInput.get(i))) {
+                ball++;
+            }
+        }
+        BallAndStrikeData data = new BallAndStrikeData(ball,strike);
+        return data;
+    }
+
+
 }
