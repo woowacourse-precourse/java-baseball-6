9dd49de2caaebd3e9ba3c14f2e0db8b9daa9276d
jeonseong-gwon <wjs4518@naver.com>
2023-10-21 03:34:07 +0900

    여러가지 예외처리 구현 및 값을 String -> List<Integer> 형태로 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,41 +1,74 @@
 package baseball;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        String numberStr = RandomNumberGenerator();
+        List<Integer> computerNumber = RandomNumberGenerator();
         System.out.println("숫자 야구 게임을 시작합니다.");
-        playGame(numberStr);
+        playGame(computerNumber);
     }
 
-    public static void playGame(String numberStr) {
+    public static void playGame(List<Integer> computerNumber) {
         Scanner sc = new Scanner(System.in);
         while (true) {
             System.out.print("숫자를 입력해주세요 : ");
-            String N = sc.nextLine();
-            int strikes = countStrikes(numberStr, N);
-            int balls = countBalls(numberStr, N);
+            String N = Console.readLine();
+            List<Integer> li = changeStrToList(N);
+            runException(N);
+            duplicate_Num(li);
+            int strikes = countStrikes(computerNumber, li);
+            int balls = countBalls(computerNumber, li);
             printInfo(strikes, balls);
             if (strikes == 3) break;
         }
         restartGame();
     }
 
-    public static String RandomNumberGenerator() {
-        int min = 100;
-        int max = 999;
-        int randomNumber = Randoms.pickNumberInRange(min, max);
-        String numberStr = String.valueOf(randomNumber);
-        return numberStr;
+    public static void duplicate_Num(List<Integer> li){
+        Set<Integer> set = new HashSet<>();
+
+        for(Integer item : li){
+            if(!set.add(item)){
+                throw new IllegalStateException("같은 숫자 입력은 안됩니다.");
+            }
+        }
     }
 
-    public static int countStrikes(String key, String guess_num) {
+    public static List<Integer> changeStrToList(String input){
+        List<Integer> resultList = Arrays.stream(input.split(""))
+                .map(str -> {
+                    try {
+                        return Integer.parseInt(str);
+                    } catch (NumberFormatException e) {
+                        // 숫자로 변환할 수 없는 경우, 예외 던지기
+                        throw new IllegalArgumentException("숫자만 사용 가능합니다.");
+                    }
+                })
+                .collect(Collectors.toList());
+        return resultList;
+    }
+
+    public static List<Integer> RandomNumberGenerator() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
+    public static int countStrikes(List<Integer> computerNumber, List<Integer> guess_Num) {
         int strikes = 0;
-        for (int i = 0; i < key.length(); i++) {
-            if (key.charAt(i) == guess_num.charAt(i)) {
+        for (int i = 0; i < computerNumber.size(); i++) {
+            if (computerNumber.get(i) == guess_Num.get(i)) {
                 strikes++;
             }
         }
@@ -43,11 +76,11 @@ public class Application {
         return strikes;
     }
 
-    public static int countBalls(String key, String guess_num) {
+    public static int countBalls(List<Integer> computerNumber, List<Integer> guess_Num) {
         int balls = 0;
-        for (int i = 0; i < key.length(); i++) {
-            char targetDigit = guess_num.charAt(i);
-            if (targetDigit != key.charAt(i) && key.contains(String.valueOf(targetDigit))) {
+        for (int i = 0; i < computerNumber.size(); i++) {
+            int targetDigit = guess_Num.get(i);
+            if (targetDigit != computerNumber.get(i) && computerNumber.contains(targetDigit)){
                 balls++;
             }
         }
@@ -55,11 +88,11 @@ public class Application {
     }
 
     public static void printInfo(int strikes, int balls) {
-        if (strikes > 0) {
-            System.out.print(strikes + "스트라이크 ");
-        }
         if (balls > 0) {
             System.out.print(balls + "볼 ");
+        }
+        if (strikes > 0) {
+            System.out.print(strikes + "스트라이크");
         }
 
         if (strikes == 0 && balls == 0) {
@@ -75,12 +108,17 @@ public class Application {
 
     public static void restartGame() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        Scanner sc = new Scanner(System.in);
-        int game = sc.nextInt();
+        int game = Integer.parseInt(Console.readLine());
         if (game == 1) {
-            String ran_Num = RandomNumberGenerator();
+            List<Integer> ran_Num = RandomNumberGenerator();
             playGame(ran_Num);
         }
+    }
+    public static void runException(String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("숫자는 3자리로 입력해야합니다.");
+        }
+
     }
 }
 
