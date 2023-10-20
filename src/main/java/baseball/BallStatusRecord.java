8437e4b13b30ed54800f6fe9483d567eec27f0b9
mythpoy <okmlnsunok@gmail.com>
2023-10-20 16:57:32 +0900

    feat : add BallStatusRecord to record ballStatus

---
+++ src/main/java/baseball/BallStatusRecord.java
@@ -0,0 +1,24 @@
+package baseball;
+
+public class BallStatusRecord {
+
+    private int ballCount;
+    private int strikeCount;
+
+    public BallStatusRecord() {
+        this.ballCount = 0;
+        this.strikeCount = 0;
+    }
+
+    public void record(BallStatus status) {
+        if(status == BallStatus.STRIKE) this.strikeCount += 1;
+        else if(status == BallStatus.BALL) this.ballCount += 1;
+    }
+
+    public String getResult() {
+        if(ballCount != 0 && strikeCount != 0) return ballCount + "볼 " + strikeCount + "스트라이크";
+        else if(ballCount != 0) return ballCount + "볼";
+        else if(strikeCount != 0) return strikeCount + "스트라이크";
+        return "낫싱";
+    }
+}
---
+++ src/main/java/baseball/Balls.java
@@ -0,0 +1,31 @@
+package baseball;
+
+import java.util.Arrays;
+import java.util.List;
+
+// 일급 컬렉션
+public class Balls {
+
+    private final List<Ball> ballList;
+
+    public Balls(List<Integer> integerList) {
+
+        this.ballList = Arrays.asList(
+                new Ball(integerList.get(0), 1),
+                new Ball(integerList.get(1), 2),
+                new Ball(integerList.get(2), 3)
+        );
+    }
+
+    public String compare(Balls otherBalls) {
+        BallStatusRecord statusRecord = new BallStatusRecord();
+
+        for(int i = 0; i < 3; i++) {
+            for (int k = 0; k < 3; k++) {
+                statusRecord.record(ballList.get(i).compare(otherBalls.ballList.get(k)));
+            }
+        }
+
+        return statusRecord.getResult();
+    }
+}
