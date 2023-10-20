30eae2d850d28ecc89721533bdd0f0b592d74bc0
JH-TT <leejongho9803@gmail.com>
2023-10-21 00:41:17 +0900

    feat: 출력관련 객체 생성
    - 게임시작 메시지
    - 입력 메시지
    - 성공 메시지
    - 의사결정 메시지 (게임 재게할지 끝낼지)
    - 결과 메시지

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,42 @@
+package baseball.view;
+
+public class OutputView {
+    private static final String START_GAME = "숫자 야구 겡임을 시작합니다.";
+    private static final String INPUT_NUMBER = "숫자를 입력해주세요.";
+    private static final String BALL = "볼 ";
+    private static final String STRIKE = "스트라이크";
+    private static final String NOTHING = "낫싱";
+    private static final String SUCCESS = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String CONTINUE_OR_END = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요";
+
+    public void startMessage() {
+        System.out.println(START_GAME);
+    }
+
+    public void inputNumberMessage() {
+        System.out.print(INPUT_NUMBER);
+    }
+
+    public void gameSuccessMessage() {
+        System.out.println(SUCCESS);
+    }
+
+    public void contiOrEndMessage() {
+        System.out.println(CONTINUE_OR_END);
+    }
+
+    public void printResult(int ball, int strike, boolean isNothing) {
+        if (isNothing) {
+            System.out.println(NOTHING);
+            return;
+        }
+        StringBuilder hint = new StringBuilder();
+        if (ball > 0) {
+            hint.append(ball + BALL);
+        }
+        if (strike > 0) {
+            hint.append(strike + STRIKE);
+        }
+        System.out.println(hint);
+    }
+}
