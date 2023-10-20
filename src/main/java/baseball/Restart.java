625a00f139c21bbf5b7a1838e1a568a64d88b721
dy102 <132787602+dy102@users.noreply.github.com>
2023-10-20 22:36:23 +0900

    Refactor: 매직넘버를 상수로 교체

--- src/main/java/baseball/Restart.java
+++ src/main/java/baseball/Restart.java
@@ -3,12 +3,19 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Restart {
-    public boolean inputRestartNumber() {
-        String number = Console.readLine();
-        if (number.equals("1")) {
+    private static final String RESTART_NUMBER = "1";
+
+    private static final String END_NUMBER = "2";
+
+    public String inputRestartNumber() {
+        return Console.readLine();
+    }
+
+    public boolean checkRestartNumber(String number) {
+        if (number.equals(RESTART_NUMBER)) {
             return true;
-        } else if (number.equals("2")) {
+        } else if (number.equals(END_NUMBER)) {
             return false;
-        } else throw new IllegalArgumentException();
+        } else throw new IllegalArgumentException("1 또는 2를 입력하세요.");
     }
 }
