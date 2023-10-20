9aae072b70133fd21b4469824b2e3018b19610d5
kunsanglee <gosmdochee2@gmail.com>
2023-10-21 00:19:43 +0900

    feat(InputView): continueOrExit() 메서드 기능 구현 및 상수 선언

--- src/main/java/baseball/InputView.java
+++ src/main/java/baseball/InputView.java
@@ -5,8 +5,8 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     public static final String BASEBALL_NUMBER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
-    public static final String CONTINUE_NUMBER = "1";
     public static final String CONTINUE_OR_EXIT_INPUT_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String CONTINUE_NUMBER = "1";
 
     public static String baseBallNumber() {
         System.out.print(BASEBALL_NUMBER_INPUT_MESSAGE);
