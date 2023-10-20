deae092ab40667a717e6837d9b87405eea3684f0
MoonJaeGyeong <bblbblan69@gmail.com>
2023-10-20 21:27:47 +0900

    <RunGame> : <RunGame 리팩토링>
    Judge 클래스를 삭제하고 RunGame에 있는 기능을 여러 function 으로 기능을 나누었음

--- src/main/java/runGame/RunGame.java
+++ src/main/java/runGame/RunGame.java
@@ -15,30 +15,97 @@ public class RunGame {
     }
 
     public void run() {
-        ErrorManage errorManage = new ErrorManage();
-        Judge judge = new Judge();
-        String exit; // 종료
-        String number; // 유저가 입력한 숫자
-        String result; // 유저가 입력한 숫자에 대한 결과
-
         System.out.println("숫자 야구 게임을 시작합니다.");
         do {
-            do {
-                System.out.print("숫자를 입력해주세요 : ");
-                number = Console.readLine();
-                errorManage.inputNumberErrorManage(number);
-                result = judge.isStrike(number, computer_num);
-                System.out.println(result);
-            } while (!result.equals("3스트라이크"));
-
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-
-            exit = Console.readLine();
-            errorManage.exitNumberErrorManage(exit);
-        } while (!exit.equals("2"));
+            RunGame runGame = new RunGame();
+            runGame.gaemStart();
+        } while (gameOver());
 
         Console.close();
+    }
+
+    private void gaemStart() {
+        ErrorManage errorManage = new ErrorManage();
+        String number; // 유저가 입력한 숫자
+
+        do {
+            System.out.print("숫자를 입력해주세요 : ");
+            number = Console.readLine();
+            errorManage.inputNumberErrorManage(number);
+        } while (!gameResult(number).equals("3스트라이크"));
+    }
+
+    private String gameResult(String number) {
+        int strike = isStrike(number);
+        int ball = isBall(number);
+        String result = NumOfStrikeAndBall(strike, ball);
+        System.out.println(result);
+
+        return result;
+    }
+
+    private boolean gameOver() {
+        ErrorManage errorManage = new ErrorManage();
+        String exit;
+
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        exit = Console.readLine();
+        errorManage.exitNumberErrorManage(exit);
+        return exit.equals("1");
+    }
+
+    private int isStrike(String numbers) {
+        int strike = 0;
+
+        for (int i = 0; i < computer_num.length(); i++) {
+            if (numbers.charAt(i) == computer_num.charAt(i)) {
+                strike++;
+            }
+        }
+        return strike;
+    }
+
+    private int isBall(String numbers) {
+        int ball = 0;
+
+        for (int i = 0; i < numbers.length(); i++) {
+            String number = numbers.substring(i, i + 1);
+            if (computer_num.contains(number)) {
+                ball++;
+            }
+        }
+        return ball;
+    }
+
+    private String NumOfStrikeAndBall(int strike, int ball) {
+        String answer = "";
+
+        if (strike == 0 && ball == 0) {
+            answer = "낫싱";
+        }
+
+        answer += NumOfBall(ball - strike);
+        answer += NumOfStrike(strike);
+
+        return answer;
+    }
+
+    private String NumOfStrike(int strike) {
+        if (strike > 0) {
+            return strike + "스트라이크";
+        }
+
+        return "";
+    }
+
+    private String NumOfBall(int ball) {
+        if (ball > 0) {
+            return ball + "볼 ";
+        }
+
+        return "";
     }
 
 
