04a117d8b2ad9e73f12aedadc205f992e48c355c
맹순영 <symaeng98@naver.com>
2023-10-20 21:39:39 +0900

    feat: 사용자 숫자 입력에 따른 결과 계산

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,17 +9,17 @@ import java.util.List;
 public class Application {
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String USER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
-    private static final List<Integer> computerNumber = new ArrayList<>();
-    private static final List<Integer> userNumber = new ArrayList<>();
+    private static final List<Integer> computerNumbers = new ArrayList<>();
+    private static final List<Integer> userNumbers = new ArrayList<>();
     public static void main(String[] args) {
         // 게임 시작 메시지 출력
         System.out.println(GAME_START_MESSAGE);
 
         // 랜덤 숫자 생성
-        while (computerNumber.size() < 3) {
+        while (computerNumbers.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computerNumber.contains(randomNumber)) {
-                computerNumber.add(randomNumber);
+            if (!computerNumbers.contains(randomNumber)) {
+                computerNumbers.add(randomNumber);
             }
         }
 
@@ -36,7 +36,21 @@ public class Application {
         // 사용자 숫자(문자열)를 List<Integer> 타입으로 변경
         for (char c : userInput.toCharArray()) {
             int number = Character.getNumericValue(c);
-            userNumber.add(number);
+            userNumbers.add(number);
+        }
+
+        // 사용자 숫자 입력에 따른 결과 계산
+        int strike = 0;
+        int ball = 0;
+
+        for (int i = 0; i < userNumbers.size(); i++) {
+            Integer currentUserNumber = userNumbers.get(i);
+            Integer currentComputerNumber = computerNumbers.get(i);
+            if (currentUserNumber.equals(currentComputerNumber)) {
+                strike++;
+            } else if (computerNumbers.contains(currentUserNumber)) {
+                ball++;
+            }
         }
     }
 }
