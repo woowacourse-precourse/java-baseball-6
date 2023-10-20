fe5a9995dabd981080f0ccf431c6ffdd0b0e8d2b
jinny-l <jinny.l.dev@gmail.com>
2023-10-20 20:20:53 +0900

    ✨feat: 게임 결과를 판단하는 서비스 기능 구현

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -1,9 +1,13 @@
 package baseball.service;
 
+import baseball.config.GameConfig;
 import baseball.domain.Baseball;
+import baseball.domain.GameResult;
 import baseball.util.NumberGenerator;
 
 public class GameService {
+
+    private static final int WINNING_COUNT = GameConfig.BASEBALL_LENGTH;
 
     private final NumberGenerator numberGenerator;
 
@@ -19,7 +23,21 @@ public class GameService {
         this.player = new Baseball();
     }
 
-    public boolean isStrike() {
+    public GameResult getGameResult() {
+        int strikeCount = computer.countSameNumbersByIndex(player);
+        if (strikeCount == WINNING_COUNT) {
+            return GameResult.allStrike();
+        }
+
+        int sameCount = computer.countSameNumbers(player);
+        if (sameCount == 0) {
+            return GameResult.nothing();
+        }
+
+        return GameResult.ballsAndStrikes(sameCount - strikeCount, strikeCount);
+    }
+
+    public boolean isGameWon() {
         return this.computer.equals(player);
     }
 }
