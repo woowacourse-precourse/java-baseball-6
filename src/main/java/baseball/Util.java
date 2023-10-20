9a3925ea9cc99dd2aec4a385a391ccb518125136
olsohee <dlthgml0108@daum.net>
2023-10-20 11:50:47 +0900

    feat: 점수 측정하는 getScore 메소드 구현

--- src/main/java/baseball/Util.java
+++ src/main/java/baseball/Util.java
@@ -15,4 +15,26 @@ public class Util {
         }
         return numbers;
     }
+
+    public Score getScore(List<Integer> answerNumbers, List<Integer> userNumbers) {
+
+        Score score = new Score();
+        int strike = 0;
+        int ball = 0;
+
+        for(int i = 0; i < answerNumbers.size(); i++) {
+            for(int j = 0; j < userNumbers.size(); j++) {
+                if(i == j && answerNumbers.get(i) == userNumbers.get(j)) {
+                    strike++;
+                }
+                if(answerNumbers.get(i) == userNumbers.get(j)) {
+                    ball++;
+                }
+            }
+        }
+
+        score.setStrike(strike);
+        score.setBall(ball);
+        return score;
+    }
 }
