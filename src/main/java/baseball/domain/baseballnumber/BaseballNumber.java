9f6dd3db57f87158c2e4cbe4f30bc49c69a2a915
wwan13 <wwan13@naver.com>
2023-10-21 00:43:54 +0900

    feat : `BaseballNumber` 구현 - 숫자 야구 게임에 필요한 숫자(정수) 하나를 포함. - 정수로 생성 가능하게 구현 - 1~9 이외에 값이 들어오는 경우 `IllegalArgumentException`발생 - 같은 정수 값이 들어오면 true를 반환하도록 equals 오버라이딩

---
+++ src/main/java/baseball/domain/baseballnumber/BaseballNumber.java
@@ -0,0 +1,42 @@
+package baseball.domain.baseballnumber;
+
+public class BaseballNumber {
+
+    private static final int MIN_VALUE = 1;
+    private static final int MAX_VALUE = 9;
+    private static final String ERROR_MESSAGE_INVALID_RANGE = "1에서 9 사이의 수만 입력할 수 있습니다.";
+
+    private final int value;
+
+    public BaseballNumber(int value) {
+        validateIsInRange(value);
+        this.value = value;
+    }
+
+    private void validateIsInRange(int value) {
+        if (!isInRange(value)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_INVALID_RANGE);
+        }
+    }
+
+    private boolean isInRange(int value) {
+        return value >= MIN_VALUE && value <= MAX_VALUE;
+    }
+
+    public int value() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (! (object instanceof BaseballNumber)) {
+            return false;
+        }
+
+        BaseballNumber baseballNumber = (BaseballNumber) object;
+        return value == baseballNumber.value;
+    }
+}
