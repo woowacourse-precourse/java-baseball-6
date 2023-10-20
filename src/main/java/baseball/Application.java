b69d99aa3f37db8949bda4572721198a50b21901
minseo  jo <liging12@naver.com>
2023-10-20 10:38:59 +0900

    feat: InputView, OutputView 주입 [콘솔]

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import baseball.view.console.InputView;
+import baseball.view.console.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    private OutputView outputView() {
+        return new OutputView();
+    }
+
+    private InputView inputView() {
+        return new InputView();
     }
 }
--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,4 +1,17 @@
 package baseball.controller;
 
+import baseball.view.console.InputView;
+import baseball.view.console.OutputView;
+
 public class GameController {
+    private final OutputView outputView;
+    private final InputView inputView;
+    GameController(OutputView outputView, InputView inputView) {
+        this.outputView = outputView;
+        this.inputView = inputView;
+    }
+
+    private void run() {
+        outputView.gameStartMessage();
+    }
 }
