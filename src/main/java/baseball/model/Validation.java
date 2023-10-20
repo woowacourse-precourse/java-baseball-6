cf9ed8ec4a2e80883b349751292f23bcd58fcb8c
leeedohyun <ldh19738@gmail.com>
2023-10-20 17:53:55 +0900

    feat: 입력한 문자열이 숫자가 아닌 경우 예외처리 기능 구현

--- src/main/java/baseball/model/Validation.java
+++ src/main/java/baseball/model/Validation.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Validation {
 
     private static final String SAME_NUMBER_EXCEPTION_MESSAGE = "같은 숫자를 입력할 수 없습니다.";
+    private static final String NON_NUMERIC_EXCEPTION_MESSAGE = "숫자만 입력할 수 있습니다.";
 
     public static void validateSameNumber(String numbers) {
         List<Character> checkList = new ArrayList<>();
@@ -15,6 +16,21 @@ public class Validation {
                 throw new IllegalArgumentException(SAME_NUMBER_EXCEPTION_MESSAGE);
             }
             checkList.add(singleNumber);
+        }
+    }
+
+    public static void validateThatInputIsNumeric(String numbers) {
+        if (!isNumeric(numbers)) {
+            throw new IllegalArgumentException(NON_NUMERIC_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private static boolean isNumeric(String numbers) {
+        try {
+            Integer.parseInt(numbers);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
         }
     }
 }
