fdddcf3730ab4828bdfd55fa71078a2543c66bfd
sohyun9527 <sohyun95277@gmail.com>
2023-10-20 19:37:51 +0900

    test(AnswerMakerTest): 생성자로 인스턴스 생성 시 정답 저장 테스트 인스턴스 호출 시 지정된 범위 내로 서로 다른 세자리 수 설정 테스트 코드는 이전과 동일

--- src/test/java/baseball/AnswerMakerTest.java
+++ src/test/java/baseball/AnswerMakerTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AnswerMakerTest {
 
-    AnswerMaker answerMaker = new AnswerMaker();
+    AnswerMaker answerMaker = new AnswerMaker(1, 9);
 
     @Test
     void 범위_내의_난수생성_테스트() {
@@ -24,7 +24,8 @@ class AnswerMakerTest {
 
     @Test
     void 중복수_테스트() {
-        List<Integer> testList = answerMaker.makeThreeDifferentNumberList(1, 999);
+        AnswerMaker answer = new AnswerMaker(1, 9);
+        List<Integer> testList = answer.getAnswer();
         for (int i = 0; i < testList.size(); i++) {
             for (int j = i + 1; j < testList.size(); j++) {
                 assertNotEquals(testList.get(i), testList.get(j));
@@ -35,7 +36,8 @@ class AnswerMakerTest {
     @Test
     void 세_개의_숫자_테스트() {
         for (int i = 0; i < 100; i++) {
-            List<Integer> testList = answerMaker.makeThreeDifferentNumberList(1, 9);
+            AnswerMaker answer = new AnswerMaker(1, 9);
+            List<Integer> testList = answer.getAnswer();
             assertEquals(3, testList.size());
         }
 
