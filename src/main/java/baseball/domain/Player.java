da6f88e5cfc5c13a90e5655bf87ec054af5daef2
gimdaeyeon <rlaeodus1306@naver.com>
2023-10-20 14:10:10 +0900

    Refactor: 사용자가 올바른 값을 입력했는지 검증부분 수정
    입력한 값이 3자리 수인지 검증

--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -5,15 +5,19 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.IllformedLocaleException;
 import java.util.List;
 
 public class Player {
+
+    private final int NUMBER_OF_DIGITS = 3;
+    private final String ERROR_MESSAGE ="입력값이 잘못되었습니다";
     private final PlayerType playerType;
     private final List<Integer> numberList;
 
     public Player(PlayerType playerType) {
         this.playerType = playerType;
-        this.numberList = initNumberList(this.playerType);
+        this.numberList = initNumberList(playerType);
     }
 
     private List<Integer> initNumberList(PlayerType playerType){
@@ -28,24 +32,38 @@ public class Player {
     private List<Integer> createHumansNumberList(){
         List<Integer> list = new ArrayList<>();
         String numberStr = Console.readLine();
-        int number = verifyEnteredValuesAndReturnForNumber(numberStr);
 
-        return numberToList(number);
+        if(!verifyInputValue(numberStr)){
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+        return numberToList(Integer.parseInt(numberStr));
     }
 
-//    사용자가 입력한 값이 숫자이고 3자리인지 확인
-    private int verifyEnteredValuesAndReturnForNumber(String numberStr){
-        if(numberStr.length()!=3){
-           throw new IllegalArgumentException("입력하신 숫자의 형식이 잘못되었습니다");
-        }
-        int number = 0;
-        try {
-            number = Integer.parseInt(numberStr);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("잘못된 값을 입력하셨습니다");
-        }
-        return number;
+
+    private boolean verifyInputValue(String numberStr){
+        return isThreeDigits(numberStr);
     }
+
+//    사용자가 입력한 수가 3자리 수인지 검증
+    private boolean isThreeDigits(String numberStr){
+        return numberStr.length() == NUMBER_OF_DIGITS;
+    }
+
+
+
+////    사용자가 입력한 값이 정상적인 입력값인지 검증 (숫자이고 3자리인지, 중복된 수는 없는지 확인)
+//    private int verifyEnteredValuesAndReturnForNumber(String numberStr){
+//        if(numberStr.length()!=3){
+//           throw new IllegalArgumentException("입력하신 숫자의 형식이 잘못되었습니다");
+//        }
+//        int number = 0;
+//        try {
+//            number = Integer.parseInt(numberStr);
+//        } catch (NumberFormatException e) {
+//            throw new IllegalArgumentException("잘못된 값을 입력하셨습니다");
+//        }
+//        return number;
+//    }
 
 //    입력받은 숫자를 리스트로 반환
     private List<Integer> numberToList(int number){
@@ -62,7 +80,7 @@ public class Player {
 //    컴퓨터의 숫자 셋팅
     private List<Integer> createComputersNumberList(){
         List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
+        while (computer.size() < NUMBER_OF_DIGITS) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
