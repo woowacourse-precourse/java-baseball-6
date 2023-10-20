4b3e5d98cbc88b70c496789d3ba72fb37ddb216d
gemstoneyang <bennyws99@gmail.com>
2023-10-20 15:17:55 +0900

    feat : Implement function to get user trial - 사용자의 입력값을 받아 리스트로 반환하는 함수 구현 - 항상 올바른 입력값만 들어온다고 가정 - 이후 유저의 입력값을 검사하는 로직 추가 예정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -16,9 +16,25 @@ public class Application {
         return computer;
     }
 
+    public static List<Integer> getUserTrialInput() {
+        String userInput = camp.nextstep.edu.missionutils.Console.readLine(); // 항상 올바른 입력값이라고 가정함
+        // todo: 유저 입력값 검사
+
+        List<Integer> userInputList = new ArrayList<>();
+        for (char c : userInput.toCharArray()) {
+            userInputList.add(c - '0');
+        }
+
+        camp.nextstep.edu.missionutils.Console.close();
+
+        return userInputList;
+    }
+
     public static void main(String[] args) {
         List<Integer> answerList = makeRandomAnswer();
+        List<Integer> userInputList = getUserTrialInput();
 
         System.out.println(answerList);
+        System.out.println(userInputList);
     }
 }
