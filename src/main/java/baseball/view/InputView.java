8481ba5916739640b11eed8466d2e2cfd680c013
hongseokhodev <hongseokhodev@gmail.com>
2023-10-20 17:25:49 +0900

    feature: insert numbers

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,29 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
+public class InputView {
+    public List<Integer> insertNumbers() {
+        String input = Console.readLine();
+        validateNumber(input);
+        return toIntegerList(input);
+    }
+
+    private void validateNumber(String input) {
+        if (!isAllDigit(input)) {
+            throw new IllegalArgumentException("숫자를 입력하세요.");
+        }
+    }
+
+    private boolean isAllDigit(String input) {
+        return input.matches("^[0-9]*$");
+    }
+
+    private List<Integer> toIntegerList(String input) {
+        return input.chars()
+                .map(integer -> integer - '0')
+                .boxed()
+                .toList();
+    }
+}
