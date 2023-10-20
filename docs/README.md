22eb521bdce8248c14369530c762ac9bb360e972
hyun-hyang <jimin3060@naver.com>
2023-10-20 19:43:07 +0900

    docs : add detail game template

--- docs/README.md
+++ docs/README.md
@@ -3,7 +3,13 @@
 < Recursive >
 
 1. Game Start
+    1. 출력 숫자 야구 게임을 시작합니다.
 2. Game ing
 3. if Success Goal , Game Finish
+    1. 출력 3스트라이크
+       3개의 숫자를 모두 맞히셨습니다! 게임 종료
 4. choose Restart/Exit
-    1. if wrong input , IllegalArgumentException & Exit
+    1. ask 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.
+    2. user input
+        1. 1 restart(recursive) 2 exit
+        2. if wrong input , IllegalArgumentException & Exit
--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,18 +1,7 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
-        }
+        
     }
 }
