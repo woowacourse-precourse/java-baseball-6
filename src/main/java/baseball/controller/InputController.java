a2e1921b521ecb0f02c79d54feb8152b60b675d3
chs98412 <chs98412@gmail.com>
2023-10-20 16:45:41 +0900

    feat: inputController 사용자 입력 기능 구현

---
+++ src/main/java/baseball/controller/InputController.java
@@ -0,0 +1,12 @@
+package baseball.controller;
+
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputController {
+
+    public String playerInput() {
+        return Console.readLine();
+    }
+
+}
