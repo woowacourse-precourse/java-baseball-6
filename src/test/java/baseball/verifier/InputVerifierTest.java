299ebebf72495ae5f07f258b1a5242d2ab86f026
정성욱 <rkfka7849@gmail.com>
2023-10-20 18:30:14 +0900

    test(InputVerifier): 입력값이 123~ 987 사이의 수인지 확인하는 기능의 테스트

--- src/test/java/baseball/verifier/InputVerifierTest.java
+++ src/test/java/baseball/verifier/InputVerifierTest.java
@@ -1,15 +1,22 @@
 package baseball.verifier;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class InputVerifierTest {
     @DisplayName("숫자값이 입력되지 않았을 때 예외 처리하는지 테스트")
     @Test
-    void throwNumricException(){
+    void throwNumricException() {
         InputVerifier inputVerifier = new InputVerifier();
-        assertThrows(IllegalArgumentException.class,() -> inputVerifier.checkBallNumber("숫자아님"));
+        assertThrows(IllegalArgumentException.class, () -> inputVerifier.checkBallNumber("숫자아님"));
+    }
+
+    @DisplayName("입력값이 123 ~ 987 사이의 수인지 확인하는 테스트")
+    @Test
+    void throwRangeException() {
+        InputVerifier inputVerifier = new InputVerifier();
+        assertThrows(IllegalArgumentException.class, () -> inputVerifier.checkBallNumber("1000"));
     }
 }
