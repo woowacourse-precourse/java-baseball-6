05d15b37136cf02847400a3beceef8252ce10e37
JunSeok Kim <skaksdl1238@gmail.com>
2023-10-20 15:34:22 +0900

    feat - BallGenerator 구현

---
+++ src/main/java/baseball/service/BallsGenerator.java
@@ -0,0 +1,41 @@
+package baseball.service;
+
+import baseball.domain.Balls;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BallsGenerator {
+
+    private static final int MAX_BALL_SIZE = 3;
+    private static final int MINIMUM_RANDOM_NUMBER = 1;
+    private static final int MAXIMUM_RANDOM_NUMBER = 9;
+
+    public int generateRandomBall() {
+        return Randoms.pickNumberInRange(MINIMUM_RANDOM_NUMBER, MAXIMUM_RANDOM_NUMBER);
+    }
+
+    private List<Integer> generateUniqueNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+
+        while (numbers.size() < MAX_BALL_SIZE) {
+            int randomNumber = generateRandomBall();
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+        return numbers;
+    }
+
+    public List<Balls> generateComputerNumbers() {
+        List<Integer> numbers = generateUniqueNumbers();
+        List<Balls> computers = new ArrayList<>();
+
+        for (int i = 0; i < numbers.size(); i++) {
+            int value = numbers.get(i);
+            computers.add(new Balls(value, i));
+        }
+        return computers;
+    }
+}
