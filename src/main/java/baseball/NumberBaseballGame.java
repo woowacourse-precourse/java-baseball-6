c627015e932d88f8fcdcb334e7a5be3b6b471623
useu <lakingsung@gmail.com>
2023-10-21 02:54:57 +0900

    fix: 게임 재시작시 게임 시작 메시지가 다시 나오던 문제 해결

--- src/main/java/baseball/NumberBaseballGame.java
+++ src/main/java/baseball/NumberBaseballGame.java
@@ -10,7 +10,6 @@ public class NumberBaseballGame {
         List<Integer> answer = NumberBaseballUtility.generateAnswer();
         int strike = 0;
         int ball = 0;
-        System.out.println("숫자 야구 게임을 시작합니다.");
         while (strike != 3) {
             System.out.print("숫자를 입력해주세요 : ");
             String input = Console.readLine();
