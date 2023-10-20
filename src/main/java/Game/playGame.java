112820218a6d62185bddaeb07cc2e57452493c05
coli-geonwoo <kkwoo001021@naver.com>
2023-10-21 05:08:08 +0900

    feat(checkInputString) : check User's Input valid or not

--- src/main/java/Game/playGame.java
+++ src/main/java/Game/playGame.java
@@ -26,6 +26,27 @@ public class playGame {
         }while(true);
     }
 
-  
+    private void checkInputString(String inputString)
+    {
+        String tmp="";
+        if (inputString.length() !=3)
+        {   //예외처리 : 문자열 길이 판단
+            throw new IllegalArgumentException("잘못된 길이의 문자를 입력하셨습니다.");
+        }
+        for(char c: inputString.toCharArray()) {
+            //예외처리 : 음수 입력 및 문자입력
+            if (c < '0' || c > '9') {
+                throw new IllegalArgumentException("잘못된 타입의 문자를 입력하셨습니다.");
+            }
+            // 예외처리 : 중복된 숫자 입력
+            if(tmp.contains(String.valueOf(c))){
+                throw new IllegalArgumentException("중복된 숫자를 입력하셨습니다");
+            }
+            tmp+=String.valueOf(c);
+
+        }
+    }
+
+
 
 }
