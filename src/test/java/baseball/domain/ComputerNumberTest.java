eb6fc5668c2b7c8dd89015f8530561145c1a9eb4
Hugh <lee2317884@gmail.com>
2023-10-20 15:33:41 +0900

    test: 컴퓨터 3자리 숫자 랜덤 생성 테스트

---
+++ src/test/java/baseball/domain/ComputerNumberTest.java
@@ -0,0 +1,29 @@
+package baseball.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class ComputerNumberTest {
+
+    @Test
+    void 컴퓨터_랜덤_숫자_길이_확인() {
+        List<Integer> computerNumber = new ComputerNumber().getComputerNumber();
+        assertEquals(3, computerNumber.size());
+    }
+
+    @Test
+    void 컴퓨터_랜덤_숫자_중복_확인() {
+        int result = (int) new ComputerNumber().getComputerNumber().stream().distinct().count();
+        assertEquals(3, result);
+    }
+
+    @Test
+    void 컴퓨터_랜덤_숫자_범위_확인() {
+        List<Integer> computerNumber = new ComputerNumber().getComputerNumber();
+        assertTrue(computerNumber.stream().allMatch(num -> 1 <= num && num <= 9));
+    }
+
+}
