efd80cf77875955291017edd690963da4095c099
waveofmymind <sjun0913@gmail.com>
2023-10-20 23:53:30 +0900

    refactor: 게임 재시작 및 종료를 BaseballGame 클래스에서 결정한다.

--- src/main/java/baseball/controller/BaseballGame.java
+++ src/main/java/baseball/controller/BaseballGame.java
@@ -6,6 +6,9 @@ import baseball.domain.Numbers;
 import baseball.view.InputView;
 import baseball.view.ResultView;
 
+import static baseball.controller.InputCommand.EXIT_COMMAND;
+import static baseball.controller.InputCommand.RESTART_COMMAND;
+
 public class BaseballGame {
 
     private final NumberGenerator numberGenerator;
@@ -16,7 +19,7 @@ public class BaseballGame {
 
             playRound(otherNumbers);
         }
-        while (checkRestart());
+        while (checkNextGame());
     }
 
     private static void playRound(Numbers otherNumbers) {
@@ -33,8 +36,16 @@ public class BaseballGame {
         }
     }
 
-    private boolean checkRestart() {
-        return InputView.readContinueOrExit();
+    private boolean checkNextGame() {
+        String command = InputView.readRestartOrExit();
+
+        if (command.equals(EXIT_COMMAND.getValue())) {
+            return false;
+        } else if (command.equals(RESTART_COMMAND.getValue())) {
+            return true;
+        } else {
+            throw new IllegalArgumentException("1 또는 2를 입력해주세요.");
+        }
     }
 
     public BaseballGame(NumberGenerator numberGenerator) {
