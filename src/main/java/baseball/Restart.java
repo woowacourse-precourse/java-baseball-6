2bb6b1eac9deab4fb45f5b620bcd8cc3bfe45f40
Kim, JeongHwan <jhkim980214@gmail.com>
2023-10-21 01:47:29 +0900

    재시작 예외처리 추가

--- src/main/java/baseball/Restart.java
+++ src/main/java/baseball/Restart.java
@@ -4,12 +4,13 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Restart {
 	public boolean restart() {
-		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-		String choice = Console.readLine();
 
-		if (choice != "1" && choice != "2") {
-			throw new IllegalArgumentException();
+		String choice = "";
+		while (!choice.equals("1") && !choice.equals("2")) {
+			System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+			choice = Console.readLine();
 		}
+		System.out.println("1, 2만 입력 가능합니다. 다시 입력해주세요.");
 
 		if ("2".equals(choice)) {
 			System.out.println("게임 종료");
