4360eef44032c11640e50df007379dd27f8d536f
metishonora <metishonora@icloud.com>
2023-10-20 20:48:09 +0900

    feat: 재시작 선택하기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -31,7 +31,7 @@ public class Application {
         int ball = 0;
         for (int i = 1; i <= 3; i++) {
             int here = input[i - 1];
-            int index = exist[here];
+            int index = exist[here - 1];
             if (index == i) {
                 strike++;
             } else if (index != 0) {
@@ -41,30 +41,54 @@ public class Application {
         return new int[] {strike, ball};
     }
 
+    static boolean readSelection() {
+        String input = Console.readLine();
+        if (input.equals("1")) {
+            return true;
+        } else if (input.equals("2")) {
+            return false;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        List<Integer> numbers = new ArrayList<>();
-        for (int i = 1; i <= 9; i++) {
-            numbers.add(i);
-        }
-        int[] answer = new int[3];
-        int[] exist = new int[9];
-        for (int i = 1; i <= 3; i++) {
-            int hereNum = numbers.remove(Randoms.pickNumberInRange(0, 8 - i));
-            answer[i-1] = hereNum;
-            exist[hereNum] = i;
-        }
-        System.out.printf("*** 정답: %s ***\n", Arrays.toString(answer));
-        int[] userInput = readNumber();
-        int[] result = checkAnswer(exist, userInput);
-        if (result[1] > 0) {
-            System.out.printf("%d볼 ", result[1]);
-        }
-        if (result[0] > 0) {
-            System.out.printf("%d스트라이크\n", result[0]);
-        }
-        if (result[0] == 0 && result[1] == 0) {
-            System.out.println("낫싱");
+        APP: while (true) {
+            List<Integer> numbers = new ArrayList<>();
+            for (int i = 1; i <= 9; i++) {
+                numbers.add(i);
+            }
+            int[] answer = new int[3];
+            int[] exist = new int[9];
+            for (int i = 1; i <= 3; i++) {
+                int hereNum = numbers.remove(Randoms.pickNumberInRange(0, 8 - i));
+                answer[i - 1] = hereNum;
+                exist[hereNum - 1] = i;
+            }
+            // System.out.printf("*** 정답: %s ***\n", Arrays.toString(answer));
+            GAME: while (true) {
+                int[] userInput = readNumber();
+                int[] result = checkAnswer(exist, userInput);
+                if (result[1] > 0 && result[0] > 0) {
+                    System.out.printf("%d볼 %d스트라이크\n", result[1], result[0]);
+                } else if (result[0] > 0) {
+                    System.out.printf("%d스트라이크\n", result[0]);
+                } else if (result[1] > 0) {
+                    System.out.printf("%d볼\n", result[1]);
+                } else {
+                    System.out.println("낫싱");
+                }
+                if (result[0] == 3) {
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                    boolean again = readSelection();
+                    if (again) {
+                        break GAME;
+                    } else {
+                        return;
+                    }
+                }
+            }
         }
     }
 }
