ab99c3e5a1cf50271016bcc08eabcf00c72a164f
slowcloud <sju9417@gmail.com>
2023-10-20 21:28:52 +0900

    chore: import 최적화

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,7 +2,6 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
 
         GameView gameView = new GameView();
         GameService gameService = new GameService();
--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -1,6 +1,9 @@
 package baseball;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Baseball {
 
