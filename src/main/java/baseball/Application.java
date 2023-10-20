8bbf50329805c18ff69f6a4fb612c1f85ef5ee60
jjaegii <hn06038@gmail.com>
2023-10-20 18:52:40 +0900

    refactor: encapsulation UserInput

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -41,36 +41,45 @@ class ComputerNumbers {
     }
 }
 
+class UserInput {
+    private final List<Integer> numbers;
+
+    public UserInput() {
+        this.numbers = new ArrayList<>();
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("세 자리의 숫자를 입력해주세요.");
+        }
+
+        for (char c : input.toCharArray()) {
+            if (!Character.isDigit(c) || c == '0') {
+                throw new IllegalArgumentException("1-9 사이의 숫자만 입력해주세요.");
+            }
+
+            int number = Character.getNumericValue(c);
+            if (numbers.contains(number)) {
+                throw new IllegalArgumentException("서로 다른 숫자를 입력해주세요.");
+            }
+            numbers.add(number);
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+}
+
 public class Application {
     public static void main(String[] args) {
         while (true) {
             ComputerNumbers computerNumbers = new ComputerNumbers();
             System.out.println("숫자 야구 게임을 시작합니다.");
             while (true) {
-                System.out.print("숫자를 입력해주세요 : ");
-                String userInput = Console.readLine();
+                UserInput userInput = new UserInput();
+                int[] result = computerNumbers.compare(userInput.getNumbers());
 
-                if (userInput.length() != 3) {
-                    throw new IllegalArgumentException("세 자리의 숫자를 입력해주세요.");
-                }
-
-                List<Integer> userNumbers = new ArrayList<>();
-
-                for (int i = 0; i < 3; i++) {
-                    char splitedChar = userInput.charAt(i);
-                    if (!Character.isDigit(splitedChar) || splitedChar == '0') {
-                        throw new IllegalArgumentException("1-9 사이의 숫자만 입력해주세요.");
-                    }
-
-                    int numericChar = Character.getNumericValue(splitedChar);
-                    if (userNumbers.contains(numericChar)) {
-                        throw new IllegalArgumentException("서로 다른 숫자를 입력해주세요.");
-                    }
-
-                    userNumbers.add(numericChar);
-                }
-
-                int[] result = computerNumbers.compare(userNumbers);
                 int strikeCount = result[0];
                 int ballCount = result[1];
 
