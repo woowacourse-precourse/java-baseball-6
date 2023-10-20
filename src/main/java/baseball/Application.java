e8fff41d434264becf0f9ca00cd1f5f1509f433f
JAY-Winter <rubat0@naver.com>
2023-10-20 17:24:45 +0900

    feat: 사용자 입력 유효성 검증

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -21,17 +21,22 @@ public class Application {
 
         while (true) {
             diplayUserInput();
-            String userNumber = getUserNumber();
+            try {
+                String userNumber = getUserNumber();
+                String hint = inferHint(computerNumber, userNumber);
+                System.out.println(hint);
 
-            String hint = inferHint(computerNumber, userNumber);
-            System.out.println(hint);
-
-            if (isAnswer(hint)) {
-                displayCorrectAnswerMessage();
-                if (isWantToQuitGame()) {
-                    break;
+                if (isAnswer(hint)) {
+                    displayCorrectAnswerMessage();
+                    if (isWantToQuitGame()) {
+                        break;
+                    }
+                    computerNumber = makeRandomComputerNumber();
                 }
-                computerNumber = makeRandomComputerNumber();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e);
+                return;
+//                System.exit(1); -> 프로그램 종료 시 System.exit()를 호출하지 않는다.
             }
         }
     }
@@ -58,11 +63,12 @@ public class Application {
         // 사용자에게 3자리 숫자 입력받기
         String userNumber = Console.readLine();
 
-        // 사용자가 입력한 3자리 숫자가 유효한지 확인
+
         // 1. 3자리가 아닌경우
         if (userNumber.length() != 3) {
-            System.out.println("올바른 숫자 3가지를 입력해주세요.");
+            throw new IllegalArgumentException("올바른 숫자 3가지를 입력해주세요.");
         }
+
         // 2. 중복된 수가 있을 경우
         HashSet<Integer> set = new HashSet<>();
         for (int i = 0; i < userNumber.length(); i++) {
@@ -70,7 +76,15 @@ public class Application {
             set.add(number);
         }
         if (set.size() != 3) {
-            System.out.println("올바른 숫자 3가지를 입력해주세요");
+            throw new IllegalArgumentException("올바른 숫자 3가지를 입력해주세요");
+        }
+
+        // 3. 1 ~ 9 사이의 숫자만 입력
+        for (int i = 0; i < userNumber.length(); i++) {
+            String currentUserNumber = String.valueOf(userNumber.charAt(i) - 48);
+            if (!currentUserNumber.matches("^[1-9$]")) {
+                throw new IllegalArgumentException("1부터 9사이의 숫자만 입력해주세요");
+            }
         }
 
         // 3. 숫자를 입력하지 않은 경우
