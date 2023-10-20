af969c21c8c97f4781686287ce7dbacfb0720f1a
GiJung <weeds2577@gmail.com>
2023-10-20 10:25:21 +0900

    test: 입력받은 사용자의 숫자를 검증하는 기능 테스트

---
+++ src/test/java/baseball/ValidatorTest.java
@@ -0,0 +1,41 @@
+package baseball;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ValidatorTest {
+
+    private static Validator validator;
+
+    @BeforeAll
+    static void beforeAll() {
+        validator = new Validator();
+    }
+
+    @Test
+    void 숫자가_아닌_값을_입력한_경우() {
+        assertThat(validator.isValidNumber("12r")).isFalse();
+    }
+
+    @Test
+    void 각_자리수가_중복되게_입력한_경우() {
+        assertThat(validator.isValidNumber("112")).isFalse();
+    }
+
+    @Test
+    void 범위가_아닌_숫자를_입력한_경우() {
+        assertThat(validator.isValidNumber("120")).isFalse();
+    }
+
+    @Test
+    void 숫자_자리수를_초과해서_입력한_경우() {
+        assertThat(validator.isValidNumber("1234")).isFalse();
+    }
+
+    @Test
+    void 숫자_자리수를_미만으로_입력한_경우() {
+        assertThat(validator.isValidNumber("12")).isFalse();
+    }
+}
