4aa915f4e159df37fa145c5f37b8d26373c4ced9
xogus3492 <wkdxogus3492@gmail.com>
2023-10-20 14:36:52 +0900

    feat(result): 게임 종료 여부를 반환하는 기능 구현

--- src/main/java/baseball/model/Result.java
+++ src/main/java/baseball/model/Result.java
@@ -1,11 +1,19 @@
 package baseball.model;
 
 public class Result {
+    private static final int GAME_FINISH_COUNT = 3;
     private final int strike;
     private final int ball;
 
     public Result(int strike, int ball) {
         this.strike = strike;
         this.ball = ball;
+    }
+
+    public boolean isGameFinish() {
+        if (strike == GAME_FINISH_COUNT) {
+            return true;
+        }
+        return false;
     }
 }
