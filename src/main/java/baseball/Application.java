67404de570b81b461b50e3d932f0d25680cbf91a
이희수 <hslee9234@naver.com>
2023-10-21 05:43:47 +0900

    게임 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import baseball.controller.GameController;
+import baseball.model.BaseballGame;
+import baseball.view.GameView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        BaseballGame game = new BaseballGame();
+        GameView view = new GameView();
+        GameController controller = new GameController(game, view);
+        controller.startGame();
+
     }
 }
