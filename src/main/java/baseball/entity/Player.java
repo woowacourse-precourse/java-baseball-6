2eb842a1a79540f309bf8f7b0c742fa038b8413c
chs98412 <chs98412@gmail.com>
2023-10-20 16:39:36 +0900

    feat: player 엔티티 설계

---
+++ src/main/java/baseball/entity/Player.java
@@ -0,0 +1,10 @@
+package baseball.entity;
+
+public class Player {
+    private String inputString;
+    private int strikeCount=0;
+    private int ballCount=0;
+    private boolean gameWin = false;
+
+
+}
