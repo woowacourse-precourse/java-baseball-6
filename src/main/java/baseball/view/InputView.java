5b94167d9b9d7e0aa6f0fccd1711c3ed35ef0ab6
scarlet0o0 <ghd7673@naver.com>
2023-10-21 03:32:24 +0900

    Refactor : InputView 클래스 비슷한 메소드 병합

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -3,11 +3,7 @@ package baseball.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static int promptForThreeNumbers() {
-        return Integer.parseInt(Console.readLine());
-    }
-
-    public static int promptForOneNumbers() {
+    public static int promptForNumbers() {
         return Integer.parseInt(Console.readLine());
     }
 }
