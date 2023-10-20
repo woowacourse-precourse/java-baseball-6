9b504ef3a2a3cf1414b823f11aa97cae57ef2abc
JiEung2 <https://github.com/JiEung2>
2023-10-20 15:41:49 +0900

    게임 MC를 생성해서 게임을 시작하는 코드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import model.GameMC;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameMC gameMC = new GameMC();
+        gameMC.gameStart();
     }
 }
