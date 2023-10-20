cd0f5bcc81223f5cc7ba6402848418353114615f
elyudwo <dudqk9696@naver.com>
2023-10-20 19:31:33 +0900

    [Test] : UserNumber 객체 올바른 숫자 반환 검증 테스트 코드 작성
    UserNumber 클래스의 getNumber 메소드 검증

--- src/test/java/baseball/domain/UserNumberTest.java
+++ src/test/java/baseball/domain/UserNumberTest.java
@@ -1,5 +1,6 @@
 package baseball.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,6 +10,22 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserNumberTest {
+
+    @DisplayName("올바른 숫자 리스트를 반환한다.")
+    @Test
+    void getNumber() {
+        // given
+        List<Integer> numberList = List.of(1,2,3);
+        UserNumber userNumber = new UserNumber();
+        userNumber.setNumber(numberList);
+
+        // when
+        List<Integer> answerNumberList = userNumber.getNumber();
+
+        // then
+        Assertions.assertThat(answerNumberList).containsExactlyInAnyOrderElementsOf(numberList);
+    }
+
 
     @DisplayName("올바르지 않은 숫자가 인자로 들어올 경우 예외가 발생한다.")
     @Test
