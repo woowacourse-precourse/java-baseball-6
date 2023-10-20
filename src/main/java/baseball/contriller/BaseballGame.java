8bfbe4ed95477cb1ae5777d01e1dd124620e6b6a
shAn-kor <53287605+shAn-kor@users.noreply.github.com>
2023-10-20 23:34:24 +0900

    gameStart 작성

--- src/main/java/baseball/contriller/BaseballGame.java
+++ src/main/java/baseball/contriller/BaseballGame.java
@@ -1,16 +1,26 @@
 package baseball.contriller;
 
+import static baseball.view.InputView.userInput;
+import static baseball.view.OutputView.gameStartMessage;
+import static baseball.view.OutputView.userInputMessage;
+
 import baseball.model.ComputerNumber;
 import baseball.model.UserNumber;
+
 
 public class BaseballGame {
     private UserNumber userNumber;
     private ComputerNumber computerNumber;
 
-    public BaseballGame(UserNumber userNumber, ComputerNumber computerNumber) {
-        this.userNumber = userNumber;
-        this.computerNumber = computerNumber;
-    }
+    /*public BaseballGame() {
+        this.computerNumber = new ComputerNumber();
+    }*/
 
+    public void gameStart() {
+        this.computerNumber = new ComputerNumber();
+        gameStartMessage();
+        userInputMessage();
+        this.userNumber = new UserNumber(userInput());
+    }
 
 }
