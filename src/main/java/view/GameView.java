edbd20a28252aaf25bbb4e873eb151c466250704
song-jae-wan <sjo9810@gmail.com>
2023-10-20 16:14:29 +0900

    Fix: enum 적용
    기존의 상수들 제거후
    enum으로 처리함

--- src/main/java/view/GameView.java
+++ src/main/java/view/GameView.java
@@ -2,34 +2,25 @@ package view;
 
 public class GameView {
 
-    private final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
-    private final String INPUT_NUMBERS_MESSAGE = "숫자를 입력해주세요 : ";
-    private final String BALL = "볼";
-    private final String STRIKE = "스트라이크";
-    private final String NOTHING = "낫싱";
-    private final String END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    private final String REGANE_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-
-    public GameView() {
-
-    }
+    public GameView() {}
 
     public void printStartMessage() {
-        System.out.println(START_MESSAGE);
+        System.out.println(ComputerMessage.START_MESSAGE.message());
     }
 
     public void printInputNumbersMessage() {
-        System.out.print(INPUT_NUMBERS_MESSAGE);
+        System.out.print(ComputerMessage.INPUT_NUMBERS_MESSAGE.message());
     }
 
     public void printEndMessage() {
-        String message = String.join("\n",END_MESSAGE,REGANE_MESSAGE);
+        String message = String.join(
+                "\n",ComputerMessage.END_MESSAGE.message(), ComputerMessage.REGANE_MESSAGE.message());
         System.out.println(message);
     }
 
     public void printCountMessage(int[] ballStrike) {
-        String ball = ballStrike[0] + BALL;
-        String strike = ballStrike[1] + STRIKE;
+        String ball = ballStrike[0] + ComputerMessage.BALL.message();
+        String strike = ballStrike[1] + ComputerMessage.STRIKE.message();
         if(ballStrike[0] > 0 && ballStrike[1] > 0) {
             System.out.println(String.join(" ",ball,strike));
         }else if(ballStrike[0] == 0 && ballStrike[1] > 0) {
@@ -37,7 +28,7 @@ public class GameView {
         }else if(ballStrike[0] > 0 && ballStrike[1] == 0) {
             System.out.println(ball);
         }else if(ballStrike[0] == 0 && ballStrike[1] == 0) {
-            System.out.println(NOTHING);
+            System.out.println(ComputerMessage.NOTHING.message());
         }
     }
 
