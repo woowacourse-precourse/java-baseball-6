8114d3e925ab00f868f3e6bd8f4dfbbaac3954b6
nhn1030 <shgusska12@gmail.com>
2023-10-20 18:07:57 +0900

    feat: 랜덤한 숫자 생성과 플레이어 입력 처리를 구현한 커밋입니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,28 @@
 package baseball;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Scanner;
+
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        int[] playerNumbers = new int[3];
+        int[] randomNumbers = new int[3];
+
+        //: 3개의 난수 생성(COM)
+        for (int i = 0; i < 3; i++) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            randomNumbers[i] = randomNumber;
+        }
+
+        Scanner scan = new Scanner(System.in);
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("1 ~ 9 사이의 숫자를 입력하세요");
+            int playerNumber = scan.nextInt();
+            playerNumbers[i] = playerNumber;
+        }
     }
 }
