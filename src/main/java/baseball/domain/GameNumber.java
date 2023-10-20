4aa7d4e61c1654d911c24b61fc96e51d503ea10d
mizuirohoshi7 <mizuirohoshi7@gmail.com>
2023-10-20 16:06:58 +0900

    feat(GameNumber): 입력 데이터 유효성 검사(1부터 9까지의 숫자로만 이루어져 있는지)

--- src/main/java/baseball/domain/GameNumber.java
+++ src/main/java/baseball/domain/GameNumber.java
@@ -6,6 +6,7 @@ public class GameNumber {
     private static final int NUMBER_LENGTH = 3;
     private static final int MIN = 1;
     private static final int MAX = 9;
+    private static final String ZERO = "0";
 
     private final List<Integer> numbers;
 
@@ -16,10 +17,30 @@ public class GameNumber {
 
     private void validate(String input) {
         validateLength(input);
+        validateValidNumber(input);
     }
 
     private void validateLength(String input) {
         if (input.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateValidNumber(String input) {
+        isNumber(input);
+        isContainingZero(input);
+    }
+
+    private void isNumber(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void isContainingZero(String input) {
+        if (input.contains(ZERO)) {
             throw new IllegalArgumentException();
         }
     }
