ce773265a9f735e42ac231c1b5d2ebbc9a40a116
suihub <qkdcrt@gmail.com>
2023-10-20 13:45:57 +0900

    <feat>: "스트라이크 결과, 볼 결과 반환 메서드 구현"
    Dto로부터 숫자를 받아 각각 스트라이크와 볼의 갯수를 반환하는 메서드 2개를 구현.
    
    배열을 사용하면 하나의 반복문을 사용해 둘을 한꺼번에 반환시키는 것도 가능하지만,
    둘 중 어떤 게 스트라이크이고 볼인지 읽는 이가 헷갈릴 거라 생각해서 각각 한 개씩만 반환하도록 메서드를 둘로 나누었다.

---
+++ src/main/java/baseball/service/GameService.java
@@ -0,0 +1,36 @@
+package baseball.service;
+
+import baseball.controller.BallDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameService {
+    public int strikeResult(List<BallDto> computer, List<BallDto> user){
+        int result = 0;
+        for (int i=0; i<computer.size(); i++){
+            BallDto computerBall = computer.get(i);
+            BallDto userBall = user.get(i);
+            if (computerBall.getNumber().equals(userBall.getNumber())) result += 1;
+        }
+        return  result;
+    }
+
+    public int ballResult(List<BallDto> computer, List<BallDto> user){
+        int strike = 0;
+        List<Integer> computerList = new ArrayList<>();
+        List<Integer> userList = new ArrayList<>();
+
+        for (int i=0; i<computer.size(); i++){
+            Integer computerNum = computer.get(i).getNumber();
+            Integer userNum = user.get(i).getNumber();
+            computerList.add(computerNum);
+            userList.add(userNum);
+            if (computerNum.equals(userNum)) strike += 1;
+        }
+
+        computerList.retainAll(userList);
+
+        return computerList.size() - strike;
+    }
+}
