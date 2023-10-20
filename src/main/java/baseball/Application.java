e6276ea843660f8faecfad2ece7e6904b59568f4
kymin <park48664866@gmail.com>
2023-10-20 17:25:41 +0900

    기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,21 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Game game = new Game();
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        game.init();
+        while(true){
+            game.inputVal();
+            if(!game.determine()){
+                continue;
+            }else{
+                if(!game.endGame()){
+                    game.init();
+                    continue;
+                }else{
+                    break;
+                }
+            }
+        }
     }
 }
