9ab8a6a04858c34860e3b9589daf87932eb5077a
JH-TT <leejongho9803@gmail.com>
2023-10-21 00:54:10 +0900

    refactor: 랜덤숫자뽑기를 String으로 리턴하도록 변경
    변경한 이유
    - 플레이어가 입력하는 타입과 통일시키기 위함
    - 통일시킴으로써 기능을 구현할때 편하고 단순해짐

--- src/main/java/baseball/controller/RandomNumberUtility.java
+++ src/main/java/baseball/controller/RandomNumberUtility.java
@@ -4,17 +4,20 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class RandomNumberUtility {
     private static final int START_NUMBER = 1;
     private static final int END_NUMBER = 9;
     private static final int NUMBER_LENGTH = 3;
 
-    public List<Integer> generateRandomNumbers() {
+    public String generateRandomNumbers() {
         Set<Integer> randomNumbers = new LinkedHashSet<>();
         while(randomNumbers.size() != NUMBER_LENGTH) {
             randomNumbers.add(Randoms.pickNumberInRange(START_NUMBER, END_NUMBER));
         }
-        return randomNumbers.stream().toList();
+        return randomNumbers.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining());
     }
 }
