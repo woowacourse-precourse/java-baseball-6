fd1d66ca76407a3d8bc84b87d16f61e3b84d67c0
aka-nick <bechalrang@gmail.com>
2023-10-20 14:26:05 +0900

    추가: 게임 재시작 입력값에 대해 유효성검증 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,17 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Objects;
+import java.util.Set;
 
 public class Application {
 
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-    private static final int GAME_FINISH = 2;
+
+    private static final String GAME_RESTART = "1";
+    private static final String GAME_FINISH = "2";
+    private static final Set<String> INPUT_RESTARTS = Set.of(GAME_RESTART, GAME_FINISH);
 
     public static void main(String[] args) {
         System.out.println(GAME_START_MESSAGE);
@@ -17,10 +22,13 @@ public class Application {
             baseballGame.play();
 
             System.out.println(GAME_RESTART_MESSAGE);
-            String inputRestartString = Console.readLine();
-            // TODO: 입력값에 대한 유효성 검증 필요
-            int inputRestart = Integer.parseInt(inputRestartString);
-            if (inputRestart == GAME_FINISH) {
+            String inputRestart = Console.readLine();
+
+            if (!INPUT_RESTARTS.contains(inputRestart)) {
+                throw new IllegalArgumentException("1 또는 2를 입력해야 합니다.");
+            }
+
+            if (Objects.equals(inputRestart, GAME_FINISH)) {
                 whetherPlay = false;
             }
         }
