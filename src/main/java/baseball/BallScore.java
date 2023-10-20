bcbfb2ecc578c4f006672c28e1ea24473e5a4c65
K-KY <kyuyoungk@naver.com>
2023-10-21 03:14:29 +0900

    Feat(BallScore) : 스트라이크 또는 볼인지 확인하고 boolean 리턴

--- src/main/java/baseball/BallScore.java
+++ src/main/java/baseball/BallScore.java
@@ -1,5 +1,13 @@
 package baseball;
 
 public enum BallScore {
-    NOTHING, BALL, STRIKE
+    NOTHING, BALL, STRIKE;
+
+    public boolean isStrike() {
+        return this.equals(STRIKE);
+    }
+
+    public boolean isBall() {
+        return this.equals(BALL);
+    }
 }
