486bf050b1c66958fefad8795cdc55bfe772dbac
kmh0601 <pongkmh@naver.com>
2023-10-20 23:04:38 +0900

    feat: Ball 클래스 수정
    ball 필드의 타입을 List<Integer>으로 바꾸면서 메서드 수정
    createRandBall() 에서 각 자릿수가 다를 수 있도록 리스트로부터 중복되지 않는 숫자를 생성하는 기능 추가

--- src/main/java/baseball/Ball.java
+++ src/main/java/baseball/Ball.java
@@ -2,36 +2,36 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Ball {
     /* fields */
-    private String ball; // int 보다 String이 게임의 결과를 판단하기 편하다고 생각함
-
-    /* constructors */
-    Ball(){
-        this.ball = "";
-    }
-    Ball(String num){
-        this.ball = num;
-    }
+    private List<Integer> ball;
 
     /* functions */
     // 컴퓨터의 3자리 난수를 생성해서 updateBall() 호출
-    public void createRandBall(final int MAXIMUM_NUM, final  int MINIMUM_NUM, final int MAXIMUM_BALL_SIZE){
-        String result = "";
+    public void createRandBall(final int MINIMUM_NUM, final  int MAXIMUM_NUM, final int MAXIMUM_BALL_SIZE){
+        List<Integer> newBall = new ArrayList<Integer>(MAXIMUM_BALL_SIZE);
         for(int i = 0 ; i < MAXIMUM_BALL_SIZE ; i++){
-            result = result + Integer.toString(Randoms.pickNumberInRange(MINIMUM_NUM, MAXIMUM_NUM));
+            newBall.add(createDiffDigit(newBall, MINIMUM_NUM, MAXIMUM_NUM));
         }
-        updateBall(result);
+        updateBall(newBall);
     }
     // updateball 분리한 이유 -> Ball의 필드에 새로운게 추가 될 경우에 업데이트를 전담하게 하기 위해서
-    public void createUserBall(String userInput){
+    public void createUserBall(List<Integer> userInput){
         updateBall(userInput);
     }
-    public void updateBall(String newBall){
+    private int createDiffDigit(List<Integer> target, final int MAXIMUM_NUM, final int MINIMUM_NUM){
+        int newDigit;
+        do {
+             newDigit = Randoms.pickNumberInRange(MINIMUM_NUM, MAXIMUM_NUM);
+        }while(target.contains(newDigit));
+        return newDigit;
+    }
+    private void updateBall(List<Integer> newBall){
         ball = newBall;
     }
 }
