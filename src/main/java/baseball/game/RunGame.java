bfc87c5851b764b2ee4490ffeb7d0795c8bcbe20
ahma0 <dsd932@naver.com>
2023-10-20 16:05:55 +0900

    feat: 컴퓨터가 생성한 수를 검증하는 기능 및 사용자가 수를 입력하는 기능 구현

--- src/main/java/baseball/game/RunGame.java
+++ src/main/java/baseball/game/RunGame.java
@@ -1,9 +1,40 @@
 package baseball.game;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class RunGame {
 
     public static void runBaseBallGame() {
+        String number = generateNumber();
 
+        while (true) {
+
+        }
+    }
+
+    private static String generateNumber() {
+        List<Integer> list = new ArrayList<>();
+
+        int cnt = 3;
+        while(cnt-- > 0) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if(!list.contains(randomNumber))
+                list.add(randomNumber);
+        }
+
+        return list.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+    }
+
+    private static String getUserAnswer() {
+        System.out.print("숫자를 입력해주세요 : ");
+        return Console.readLine();
     }
 
 }
