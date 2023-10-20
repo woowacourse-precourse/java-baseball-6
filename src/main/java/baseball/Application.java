9316bfa428083f3f03ef750ca723540e81546809
StrangeMin <mim0107@nate.com>
2023-10-20 18:26:56 +0900

    fix : 3 strike 시 3스트라이크를 출력하도록 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -106,6 +106,7 @@ public class Application {
 
     public static void printResult(boolean isAllCorrect){
         if(isAllCorrect) {
+            System.out.println("3스트라이크\n");
             System.out.printf("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             return;
         }
