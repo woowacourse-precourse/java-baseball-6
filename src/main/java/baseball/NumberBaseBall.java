77a85b2b51a8c4b204bc9052e201846ed64e1179
jy016011 <39334789+jy016011@users.noreply.github.com>
2023-10-20 12:34:14 +0900

    MessageViewer로 출력 위임에 따른 코드 수정

--- src/main/java/baseball/NumberBaseBall.java
+++ src/main/java/baseball/NumberBaseBall.java
@@ -1,7 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
-
+import baseball.userInterface.MessageViewer;
 import java.util.List;
 
 public class NumberBaseBall {
@@ -9,26 +9,30 @@ public class NumberBaseBall {
     NumberChecker numberChecker;
     InputValidator inputValidator;
     List<Integer> computer;
+    MessageViewer messageViewer;
     public void init(){
         randomNumberGenerator = new RandomNumberGenerator();
         numberChecker = new NumberChecker();
         inputValidator = new InputValidator();
         computer = randomNumberGenerator.generateRandomNumber();
+        messageViewer = new MessageViewer();
     }
 
     public void run(){
         boolean keepGoing = true;
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        messageViewer.printStartMsg();
         while (keepGoing){
-            System.out.println("숫자를 입력해주세요 : ");
+            messageViewer.printInputRequestMsg();
             System.out.println(computer.get(0) + " " + computer.get(1) + " " + computer.get(2));
             List<Integer> user = inputValidator.validateUserAnswer(Console.readLine());
             System.out.println(user.get(0) + " " + user.get(1) + " " + user.get(2));
-            StringBuilder checkResult = numberChecker.checkNumber(user, computer);
-            System.out.println(checkResult);
-            if(checkResult.toString().equals("3스트라이크")){
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            int[] results = numberChecker.checkNumber(user, computer);
+            int ballCounts = results[0];
+            int strikeCounts = results[1];
+            messageViewer.printResultMsg(ballCounts, strikeCounts);
+            if(strikeCounts == 3){
+                messageViewer.printGameEndMsg();
+                messageViewer.printRestartMsg();
                 int restart = inputValidator.validateRestartInput(Console.readLine());
 
                 if (restart == 1)
