b8f8de2a88cca0ddfa93b54b7a2169649768635e
YongHyeon <pyh4616@naver.com>
2023-10-21 04:30:40 +0900

    Test: GameService, 컴퓨터 숫자와 사용자 숫자를 비교했을 때, 결과로 3볼이 나오는 케이스를 테스트

--- src/test/java/baseball/service/GameServiceTest.java
+++ src/test/java/baseball/service/GameServiceTest.java
@@ -43,4 +43,16 @@ public class GameServiceTest {
 
         Assertions.assertThat(compareResult).isEqualTo("2볼");
     }
+
+    @Test
+    @DisplayName("컴퓨터 숫자와 사용자 숫자를 비교하여, 3볼이 나오는 케이스를 테스트한다.")
+    void testThreeBallCase() {
+        List<Integer> computerNumber = List.of(1, 2, 3);
+        List<Integer> userNumber = List.of(3, 1, 2);
+
+        GameService gameService = new GameService();
+        String compareResult = gameService.getCompareResult(computerNumber, userNumber);
+
+        Assertions.assertThat(compareResult).isEqualTo("3볼");
+    }
 }
