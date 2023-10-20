a39fa4a593cf4b0fd37ee1ee6351291b141f38df
tlarbals824 <simkm0824@naver.com>
2023-10-20 14:51:43 +0900

    feat : 랜덤 숫자 배열 결괏값 저장하는 클래스 추가

---
+++ src/main/java/baseball/ComputerRandomNumber.java
@@ -0,0 +1,29 @@
+package baseball;
+
+import java.util.List;
+
+public class ComputerRandomNumber {
+    private final List<Integer> randomNumberList;
+
+    public static final Integer NOT_FOUND = -1;
+
+    public ComputerRandomNumber(List<Integer> randomNumberList) {
+        this.randomNumberList = randomNumberList;
+    }
+
+    public Integer indexOf(Integer number){
+        if(randomNumberList.contains(number)){
+            return randomNumberList.indexOf(number);
+        }
+        return NOT_FOUND;
+    }
+
+    public Integer size(){
+        return randomNumberList.size();
+    }
+
+    public List<Integer> getRandomNumberList(){
+        return randomNumberList;
+    }
+
+}
