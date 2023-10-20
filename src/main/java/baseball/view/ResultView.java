b91f0a8393ffb94b5d3ecf24192759d7e43eefab
waveofmymind <sjun0913@gmail.com>
2023-10-20 21:38:33 +0900

    fix: 스트라이크만 존재할 경우 출력되지 않는 문제를 해결한다.

--- src/main/java/baseball/view/ResultView.java
+++ src/main/java/baseball/view/ResultView.java
@@ -8,7 +8,7 @@ public class ResultView {
         if (gameResult.isStrikeOut()) {
             System.out.println("3스트라이크");
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        } else if (gameResult.hasBall()) {
+        } else if (gameResult.hasBall() || gameResult.hasStrike()) {
             System.out.println(gameResult.getBallCount() + "볼 " + gameResult.getStrikeCount() + "스트라이크");
         } else {
             System.out.println("낫싱");
