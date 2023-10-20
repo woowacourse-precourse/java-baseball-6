8a52967c0857c61bbdea589754e193fda308cb78
flown222 <jangu3384@gmail.com>
2023-10-20 22:07:16 +0900

    feat: add game start

---
+++ src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,11 @@
+package baseball.controller;
+
+import baseball.view.View;
+
+public class GameController {
+
+    public void runGame() {
+        View.gameStart();
+        //initGame();
+    }
+}
---
+++ src/main/java/baseball/view/View.java
@@ -0,0 +1,8 @@
+package baseball.view;
+
+public class View {
+
+    public static void gameStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+}
