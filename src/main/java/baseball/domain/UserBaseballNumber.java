b1103dd2115c397dca1e5473f06d71a7fd9d3f34
noxknow <chris2769@naver.com>
2023-10-20 14:17:35 +0900

    update(UserBaseballNumber) : user가 입력한 값에 대한 유효성 검사 추가

--- src/main/java/baseball/domain/UserBaseballNumber.java
+++ src/main/java/baseball/domain/UserBaseballNumber.java
@@ -1,18 +1,34 @@
 package baseball.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import static baseball.handler.ErrorHandler.DUPLICATE_NUMBER;
+import static baseball.handler.ErrorHandler.INVALID_COUNT;
 
 public class UserBaseballNumber {
 
-    private List<String> userValue;
+    private List<String> userValues;
 
-    public UserBaseballNumber(List<String> userValue) {
-        this.userValue = userValue;
+    public UserBaseballNumber(List<String> userValues) {
+        validate(userValues);
+        validateDuplicate(userValues);
+
+        this.userValues = userValues;
     }
 
-    private void validate(List<String> userValue) {
-        if (userValue.size() != 3) {
+    private void validate(List<String> userValues) {
+        if (userValues.size() != 3) {
+            throw INVALID_COUNT.getException();
+        }
+    }
 
+    private void validateDuplicate(List<String> userValues) {
+        Set<String> uniqueValues = new HashSet<>(userValues);
+
+        if (userValues.size() != uniqueValues.size()) {
+            throw DUPLICATE_NUMBER.getException();
         }
     }
 }
