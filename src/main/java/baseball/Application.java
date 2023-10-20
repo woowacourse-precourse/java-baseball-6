d6ab2212706e183ac5d038633cd06562b3fed824
SungHoon Ko <sunghoon_ko@tmax.co.kr>
2023-10-20 14:21:50 +0900

    Add createComputerNum method to make three random numbers in List

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    static void createComputerNum(List<Integer> computerNum){
+        while(computerNum.size() < 3){
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if(!computerNum.contains(randomNumber)){
+                computerNum.add(randomNumber);
+            }
+        }
+    }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("Start Number Baseball Game!");
+
+        List<Integer> computerNum = new ArrayList<>();
+        String personNum;
+
+        createComputerNum(computerNum);
+
     }
 }
