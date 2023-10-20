0562f3075b7a68be976ac5f9ca905e0e66bc8203
Sully <2dcoder@naver.com>
2023-10-20 15:38:48 +0900

    feat: 각 숫자가 중복되었는지 확인하는 메서드 구현
    HashSet으로 만들었을 때 그 길이가 줄어들었다면 숫자가 중복되었다는 의미이다.
    만약 그렇게 되었을 시에는 예외를 발생시킨다.

--- src/main/java/baseball/validator/NumberValidator.java
+++ src/main/java/baseball/validator/NumberValidator.java
@@ -1,5 +1,8 @@
 package baseball.validator;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class NumberValidator {
 
     public static final int LENGTH = 3;
@@ -14,6 +17,23 @@ public class NumberValidator {
         if (number < MIN_VALUE || number > MAX_VALUE) {
             throw new IllegalArgumentException("숫자는 " + MIN_VALUE + "에서 " + MAX_VALUE + "까지여야 합니다.");
         }
+    }
+
+    public static void validateDuplicateNumber(int number) {
+        String numberString = String.valueOf(number);
+        Set<Character> characters = convertStringToSet(numberString);
+        if (characters.size() < numberString.length()) {
+            throw new IllegalArgumentException(number + ": 중복된 숫자가 존재합니다.");
+        }
+    }
+
+    private static Set<Character> convertStringToSet(String numberString) {
+        Set<Character> resultSet = new HashSet<>();
+        for (char c : numberString.toCharArray()) {
+            resultSet.add(c);
+        }
+
+        return resultSet;
     }
 
     public static boolean isBelowRequiredLength(int size) {
