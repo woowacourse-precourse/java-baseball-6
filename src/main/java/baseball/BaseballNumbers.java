74752e5e9d5ec5fde1596b3d2a00460e1c788b6f
aka-nick <bechalrang@gmail.com>
2023-10-20 14:04:49 +0900

    추가: 야구숫자 입력 시 유효성검증 추가

--- src/main/java/baseball/BaseballNumbers.java
+++ src/main/java/baseball/BaseballNumbers.java
@@ -5,12 +5,27 @@ import java.util.List;
 
 public class BaseballNumbers {
 
+    private static final int MAX_LENGTH_OF_NUMBERS = 3;
     List<Integer> baseballNumbers;
 
     public BaseballNumbers() {
         System.out.print("숫자를 입력해주세요 : ");
         String inputNumbersString = Console.readLine();
-        // TODO: 야구숫자 입력값에 대한 예외처리 필요
+
+        if (inputNumbersString.length() != MAX_LENGTH_OF_NUMBERS) {
+            throw new IllegalArgumentException("야구숫자는 세 자리여야 합니다.");
+        }
+        if (inputNumbersString.replaceAll("[0-9]", "").length() != 0) {
+            throw new IllegalArgumentException("숫자만 입력 가능합니다.");
+        }
+        if (inputNumbersString.length() != inputNumbersString.chars().distinct().count()) {
+            throw new IllegalArgumentException("입력값은 중복되지 않아야 합니다.");
+        }
+        if (inputNumbersString.chars().mapToObj(Character::getNumericValue)
+                .anyMatch(number -> number.compareTo(0) < 0)) {
+            throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+        }
+
         this.baseballNumbers = inputNumbersString.chars()
                 .mapToObj(Character::getNumericValue)
                 .toList();
