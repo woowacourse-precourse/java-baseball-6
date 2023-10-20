91a2e689807023aa959f2b3c9af07f5f0df9a533
혜민 <alddlv81@gmail.com>
2023-10-20 14:33:51 +0900

    Chore: 코드 정리 - 주석 삭제, 알아보기 쉽게 정리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -52,7 +52,6 @@ class Game{
                 computer_answer.add(randomNumber);
             }
         }
-        System.out.println("computer: " + computer_answer);
     }
 
     void user() {
@@ -60,21 +59,25 @@ class Game{
         String input = Console.readLine();
 
         try {
-            if (input.length() != 3) {
-                throw new IllegalArgumentException("3자리 숫자가 아닙니다.");
-            }
-            for (int i = 0; i < input.length(); i++) {
-                int num = input.charAt(i) - '0';
-                if (num < 1 || num > 9) {
-                    throw new IllegalArgumentException("1부터 9까지의 숫자를 입력해주세요.");
-                }
-                if (answer.contains(num)) {
-                    throw new IllegalArgumentException("중복된 숫자가 입력되었습니다.");
-                }
-                answer.add(num);
-            }
+            validateInput(input);
         } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("게임 종료");
+            throw new IllegalArgumentException("숫자가 아닙니다.");
+        }
+    }
+    private void validateInput(String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("3자리 숫자가 아닙니다.");
+        }
+
+        for (int i = 0; i < input.length(); i++) {
+            int num = input.charAt(i) - '0';
+            if (num < 1 || num > 9) {
+                throw new IllegalArgumentException("1부터 9까지의 숫자를 입력해주세요.");
+            }
+            if (answer.contains(num)) {
+                throw new IllegalArgumentException("중복된 숫자가 입력되었습니다.");
+            }
+            answer.add(num);
         }
     }
 }
