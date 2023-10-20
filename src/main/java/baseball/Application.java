f5184b6488b49b33e75e945abca3a201a141fd94
chs98412 <chs98412@gmail.com>
2023-10-20 16:52:04 +0900

    feat: 메인 함수에 흐름 생성 게임의 전반적인 흐름을 어디에 둘지 고민해봐야 할듯

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import baseball.controller.InputController;
+import baseball.entity.Player;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        Player player = new Player();
+        InputController inputController = new InputController();
+        player.setInputString(inputController.playerInput());
+        System.out.println(player.getInputString());
     }
 }
