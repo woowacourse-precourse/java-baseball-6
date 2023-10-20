f0239422581d1565bc0e736a869d28cefa0c178e
haruday97 <starharu97@naver.com>
2023-10-20 22:46:26 +0900

    feat: 구현 결과출력 기능

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -12,11 +12,13 @@ public class GameController {
 
     public void start() {
         List<Integer> computerNumbers = service.generateNumbers();
+        System.out.println("치트: " + computerNumbers.get(0) + computerNumbers.get(1) + computerNumbers.get(2));
         System.out.println("숫자 야구 게임을 시작합니다.");
         System.out.println("숫자를 입력해주세요.");
         String input = Console.readLine();
         List<Integer> userNumbers = service.parseInputToList(input);
         if(validation.isInvalid(userNumbers)) throw new IllegalArgumentException("올바르지 않은 입력값입니다.");
         int[] result = service.compare(computerNumbers, userNumbers);
+        service.printResult(result);
     }
 }
--- src/main/java/baseball/domain/GameService.java
+++ src/main/java/baseball/domain/GameService.java
@@ -48,4 +48,29 @@ public class GameService {
     private boolean isStrike(List<Integer> computerNumbers, List<Integer> userNumbers, int index) {
         return computerNumbers.get(index) ==  userNumbers.get(index);
     }
+
+    public void printResult(int[] result){
+        int strikes = result[0];
+        int balls = result[1];
+
+        String ballResult = printBalls(balls);
+        String strikeResult = printStrikes(strikes);
+
+        if(ballResult.isEmpty() && strikeResult.isEmpty()) {
+            System.out.println("낫싱");
+            return;
+        }
+        System.out.println(ballResult + strikeResult);
+    }
+
+    private String printBalls(int balls){
+        if(balls >= 1) return balls + "볼 ";
+        return "";
+    }
+
+    private String printStrikes(int strikes){
+        if(strikes >= 1) return strikes + "스트라이크";
+        return "";
+    }
+
 }
