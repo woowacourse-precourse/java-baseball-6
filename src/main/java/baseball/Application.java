c7940d631b422fe94e00c93bcff2ed76f624db3e
김효준 <khyojun1104@gmail.com>
2023-10-20 13:09:48 +0900

    [feat] Application : main에서 Controller로 넘기는 기본작업 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,12 +5,19 @@ import baseball.view.InputView;
 import baseball.view.OutputView;
 
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         InputView inputView = InputView.getInstance();
         OutputView outputView = OutputView.getInstance();
 
+        GameController game = new GameController(inputView, outputView);
+        outputView.startMessage();
+        while(true){
+            game.playball();
 
+
+        }
 
     }
 }
