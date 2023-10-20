c7ef80cea2b8da0beb365eb504fa6c1b5aa47d8d
kyu-wooon <catdog0831@gmail.com>
2023-10-20 15:03:59 +0900

    main 함수의 while문 기능 작성 완료 1. 스트라이크 / 볼 판정 및 출력 추가 2. 숫자 입력 시 중복 예외 추가 3. 3 스트라이크 시 게임 재시작 / 종료 여부 체크 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -39,7 +39,7 @@ public class Application {
             //while문 돌 때마다 ball과 strike는 초기화
             ball = 0;
             strike = 0;
-
+            Set<Character> dup_check = new HashSet<>();//중복 체크용 set
 
             //숫자 입력
             System.out.println("숫자를 입력해주세요 : ");
@@ -52,12 +52,60 @@ public class Application {
                 throw new IllegalArgumentException();
             }
 
+            for(int i = 0; i<input.length(); i++){
+                dup_check.add(input.charAt(i));
+            }
+            if(dup_check.size() != input.length()){ //예외 3: 입력 숫자를 중복해서 입력하였을 경우
+                throw new IllegalArgumentException();
+            }
 
 
+            //입력한 문자열을 담을 int 배열
+            int[] input_split = new int[input.length()];
+            //입력한 문자열을 input_split에 담는다
+            for(int i = 0; i<input.length(); i++){
+                input_split[i] = Character.getNumericValue(input.charAt(i));
+            }
+
+            //배열 길이(3) 만큼 실행
+            for(int i = 0 ; i<input_split.length; i++){
+                if(answer_list.get(i) == input_split[i]){ // 자릿수와 숫자 모두 같을  떄 : strike 증가
+                    strike++;
+                }else{
+                    if(answer_list.contains(input_split[i])){ // 숫자만 포함하고 있을 때 : ball 증가
+                        ball++;
+                    }
+                }
+
+            }
+
+            //스트라이크나 볼이 하나도 없을 때
+            if(strike == 0  && ball == 0){
+                System.out.println("낫싱");
+            }else{//있을 시,  각자 값 출력
+                System.out.println(ball + "볼 " + strike + "스트라이크");
+            }
+
+            if(strike == 3){// 모두 맞추었을 경우
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
 
+              //1이나 2를 입력해서 재시작 or 종료
+                input = Console.readLine();
+                if(input.length() != 1){ //예외 1 : 길이가 1이 아닐 경우
+                    throw new IllegalArgumentException();
+                }
+                if(input.matches("[1-2]") == false){//예외 2 : 1이나 2 가 아닌 다른 숫자/문자일 경우
+                    throw new IllegalArgumentException();
+                }
 
+                if(input.equals("1")){//1 : 게임 재시작
+                    answer_list = random_pick(); // 다시 answer 값을 만든다.
+                }else if(input.equals("2")){//2 : 게임 종료
+                    end_check = true;}
 
+            }
 
         }
 
