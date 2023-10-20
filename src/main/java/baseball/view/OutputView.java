8ed256428b62d79e1e5d6468442d994e34b55aaa
pwrwpw <dlgustjr9912@gmail.com>
2023-10-21 01:50:57 +0900

    fix(OutputView): 상수값 출력시 getMessage()메서드를 통해 출력

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -7,13 +7,13 @@ import static baseball.model.Constant.MESSAGE_INPUT_NUMBER;
 public class OutputView {
 
 	public void printGameStart() {
-		System.out.println(MESSAGE_GAME_START);
+		System.out.println(MESSAGE_GAME_START.getMessage());
 	}
 	public void printGameEnd() {
-		System.out.println(MESSAGE_GAME_END);
+		System.out.println(MESSAGE_GAME_END.getMessage());
 	}
 	public static void printInputNumber() {
-		System.out.print(MESSAGE_INPUT_NUMBER);
+		System.out.print(MESSAGE_INPUT_NUMBER.getMessage());
 	}
 
 }
