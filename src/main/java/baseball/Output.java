ca7cf01d5b773be4337ab9f3872f4cc59ba1a663
aammddkkzxc <97333563+aammddkkzxc@users.noreply.github.com>
2023-10-20 19:53:45 +0900

    출력 기능 추가

---
+++ src/main/java/baseball/Output.java
@@ -0,0 +1,16 @@
+package baseball;
+
+public class Output {
+
+    public void startMessage() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void resultMessage() {
+
+    }
+
+    public void endMessage() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+}
