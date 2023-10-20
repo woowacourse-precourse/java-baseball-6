6222061bd58f37554bd8cb589691325d6a1407ae
김승우 <tmd8633@naver.com>
2023-10-20 17:40:59 +0900

    Feat: Template try 문 추가

--- src/main/java/template/MessageTemplate.java
+++ src/main/java/template/MessageTemplate.java
@@ -2,17 +2,23 @@ package template;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.NoSuchElementException;
+
 public class MessageTemplate {
 
     public String execute(String message, Callback callback){
 
-        System.out.print(message); // 메세지 출력
+        try {
+            System.out.print(message); // 메세지 출력
 
-        String result = Console.readLine(); // 값을 받아옴
+            String result = Console.readLine(); // 값을 받아옴
 
-        callback.valid(result); // result 값 검증
+            callback.valid(result); // result 값 검증
 
-        return result;
+            return result;
+        } catch (NoSuchElementException e) {
+            throw new IllegalArgumentException("값을 찾을 수 없음");
+        }
 
     }
 }
