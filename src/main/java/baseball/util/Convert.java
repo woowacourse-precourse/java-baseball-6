033f247f6370d4e942c61dde96320af8d8af393f
YongHyeon <pyh4616@naver.com>
2023-10-20 15:27:43 +0900

    Feat: Convert, 문자를 입력받아 정수 리스트로 변환하는 기능 구현

---
+++ src/main/java/baseball/util/Convert.java
@@ -0,0 +1,16 @@
+package baseball.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Convert {
+
+    public static List<Integer> convertStringToIntegerList(String input) {
+        List<Integer> userNumber = new ArrayList<>();
+        for (int i = 0; i < input.length(); i++) {
+            int inputNumber = Character.getNumericValue(input.charAt(i));
+            userNumber.add(inputNumber);
+        }
+        return userNumber;
+    }
+}
