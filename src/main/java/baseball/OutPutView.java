68a0460d08ef68dff700b9199410bc409ca7b3d9
shinjaewon99 <tlswodnjs99@naver.com>
2023-10-20 16:50:47 +0900

    feat : 스트라이크, 볼 갯수 출력 기능 추가

--- src/main/java/baseball/OutPutView.java
+++ src/main/java/baseball/OutPutView.java
@@ -6,4 +6,11 @@ public class OutPutView {
     public void printRequestInputNumberMessage() {
         System.out.print(REQUEST_INPUT_NUMBER_MESSAGE);
     }
+
+    public void printStrikeBallCountMessage(int strike, int ball) {
+        if (strike > 0 && ball > 0) System.out.println(ball + BALL + " " + strike + STRIKE);
+        if (strike == 0 && ball > 0) System.out.println(ball + BALL);
+        if (strike > 0 && ball == 0) System.out.println(strike + STRIKE);
+        if (strike == 0 && ball == 0) System.out.println(NOTHING);
+    }
 }
