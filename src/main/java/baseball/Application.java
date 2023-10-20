c6b38277ba58cac3d01208f7389b87a3a2b24bd7
Jonghyeok Lee <endb1228@gmail.com>
2023-10-20 14:51:11 +0900

    정답 숫자 생성 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        startGame();
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (true) {
+            List<String> numberAnswer = generateRandomNumber();
+        }
     }
 
-    public static void startGame() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+    public static List<String> generateRandomNumber() {
+        return Randoms.pickUniqueNumbersInRange(1, 9, 3)
+                .stream()
+                .map(String::valueOf)
+                .collect(Collectors.toList());
     }
 }
