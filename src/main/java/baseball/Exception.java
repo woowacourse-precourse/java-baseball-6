061142d8437a31a5dd96c73b79925ae26a010ae5
ga0nii <kgw0124@kakao.com>
2023-10-21 00:51:06 +0900

    [fact] 사용자가 입력한 각 자릿수의 범위에 대한 예외 처리 추가

--- src/main/java/baseball/Exception.java
+++ src/main/java/baseball/Exception.java
@@ -7,6 +7,7 @@ import java.util.Set;
 public class Exception {
     private static final String LENGTH_EXCEPTION_MSG = "3자리의 수를 입력해주세요.";
     private static final String DUPLICATE_EXCEPTION_MSG = "각 자리 수는 중복이 불가합니다.";
+    private static final String RANGE_EXCEPTION_MSG = "각 자리 수는 1~9 사이의 숫자만을 사용 가능합니다.";
     private static final String OPTION_EXCEPTION_MSG = "재실행을 하려면 1을, 종료하려면 2를 입력해주세요.";
 
     //사용자가 입력한 3개의 수에 대한 입력 처리
@@ -17,6 +18,11 @@ public class Exception {
             Set<Integer> removeDuplicate = new HashSet<>(input);
             if(removeDuplicate.size() != 3){
                 throw new IllegalArgumentException(DUPLICATE_EXCEPTION_MSG);
+            }
+        }
+        for(int i=0; i<input.size(); i++){
+            if(input.get(i) == 0){
+                throw new IllegalArgumentException(RANGE_EXCEPTION_MSG);
             }
         }
     }
