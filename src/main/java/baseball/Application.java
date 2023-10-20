4706bb5b395d25c48b969088439e4db831778eaa
akdntmrkdjqtsp <gytjd9516@naver.com>
2023-10-20 15:11:23 +0900

    게임 시작 후 숫자뽑기, 입력받기, 결과 생성까지

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import java.io.*;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO: 프로그램 구현
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        writer.write("숫자 야구 게임을 시작합니다.");
+        writer.flush();
+        
+        Baseball baseball = new Baseball();
+        baseball.start();
+        while(true) {
+            if (!baseball.play()) {
+                break;
+            }
+            
+        }
+        writer.close();
     }
 }
