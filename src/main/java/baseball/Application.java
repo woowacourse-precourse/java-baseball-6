534899edb8fba85e0ad6edecb4b39513616d5a65
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-20 17:20:33 +0900

    refactor(Application):재사용성을 위한 기능 분할로 인한 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,6 @@ public class Application {
     public static void main(String[] args){
         // TODO: 프로그램 구현
         BaseballService baseballService = new BaseballService();
-        baseballService.startBaseballGame();
+        baseballService.playBaseballGame();
     }
 }
