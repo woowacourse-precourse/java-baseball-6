e1633326753a514b177b4c3b950af54d3241a8cd
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-10-20 16:18:19 +0900

    Refactor : start()메서드 변경 do while 조건문을 gameController 내부의 별도 메서드로 추가하여 코드 간결화

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,16 +1,11 @@
 package baseball;
 
 import baseball.controller.GameController;
-import baseball.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         GameController gameController = new GameController();
-
-        OutputView.printStart();
-        do{
-            gameController.start();
-        } while (gameController.isReset());
+        gameController.run();
     }
 }
