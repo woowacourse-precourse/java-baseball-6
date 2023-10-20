fa9b5b38589b5f59a48149937388320b40edc859
prao <pch5521@gmail.com>
2023-10-20 15:06:13 +0900

    Refactor: 컨트롤러 구조 변경(테스트 성공)
    - 기존의 run()에 모두 포함되어 있던 메서드 분리
    - while문을 통해 게임 재시작 및 게임 종료 기능 구현
    - indent(들여쓰기)가 3이므로 2 이하로 되게끔 리팩토링 예정

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -1,42 +1,95 @@
 package baseball;
 
 import baseball.domain.Computer;
-import baseball.domain.Manager;
 import baseball.domain.Referee;
 import baseball.util.Converter;
 import baseball.util.Validator;
+import baseball.view.InputView;
+import baseball.view.OutputView;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Controller {
 
     public static final int SIZE = 3;
-    Validator validator = new Validator();
+    private int ball;
+    private int strike;
+    List<Integer> computerNumbers;
+    List<Integer> userNumbers;
+    OutputView outputView = new OutputView();
+    Validator validator;
     Converter converter;
-    Computer computer = new Computer(SIZE);
     Referee referee;
-    Manager manager;
-
 
     public void run() {
-        System.out.print("숫자를 입력해주세요 : ");
-        String input = Console.readLine();
-        validator.checkValid(input, SIZE);
-        converter = new Converter(input);
-        List<Integer> computerNumbers = computer.getComputerNumbers();
-        List<Integer> userNumbers = converter.getSeperatedNumbers();
-        referee = new Referee(computerNumbers, userNumbers);
-        System.out.println(referee.getJudgement());
-        if (referee.getStrike() != 3) {
-            run();
-        }
-        if (referee.getStrike() == 3) {
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            manager = new Manager(Console.readLine());
-            if (manager.getResult().equals("재시작")) {
-                run();
+        resetGame();
+        generateComputerNumbers();
+        while (true) {
+            String input = requestUserInput();
+            initGame();
+            checkInputData(input);
+            generateUserNumbers(input);
+            requestJudge(computerNumbers, userNumbers);
+            requestResult(ball, strike);
+            if (strike == 3) {
+                String retryMessage = requestRetryMessage();
+                if (retryMessage.equals("1")) {
+                    initGame();
+                    generateComputerNumbers();
+                }
+                if (retryMessage.equals("2")) {
+                    break;
+                }
             }
         }
+    }
+
+    public void initGame() {
+        userNumbers = new ArrayList<>();
+        validator = new Validator();
+        referee = new Referee();
+    }
+
+    public void resetGame() {
+        computerNumbers = new ArrayList<>();
+    }
+
+    private String requestUserInput() {
+        InputView.requestInputData();
+        String input = Console.readLine();
+        System.out.print(input);
+        System.out.println();
+        return input;
+    }
+
+    private void checkInputData(String input) {
+        validator.checkValid(input, SIZE);
+    }
+
+    private void generateUserNumbers(String input) {
+        converter = new Converter(input);
+        userNumbers = converter.getSeperatedNumbers();
+    }
+
+    private void generateComputerNumbers() {
+        Computer computer = new Computer(SIZE);
+        computerNumbers = computer.getComputerNumbers();
+    }
+
+    private void requestJudge(List<Integer> computerNumbers, List<Integer> userNumbers) {
+        referee.judge(computerNumbers, userNumbers);
+        ball = referee.getBall();
+        strike = referee.getStrike();
+    }
+
+    private void requestResult(int ball, int strike) {
+        outputView.printJudgement(ball, strike);
+    }
+
+    private String requestRetryMessage() {
+        OutputView.printEndMessage();
+        InputView.printRetryMessage();
+        return Console.readLine();
     }
 }
