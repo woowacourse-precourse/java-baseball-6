722820ce41eec47cb50fd4d19416042d60125213
StrangeMin <mim0107@nate.com>
2023-10-20 15:44:53 +0900

    feat : orderOfNumbers, computerNumbers, userNumbers 변수 선언

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,13 @@
 package baseball;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Application {
+
+    private static Integer[] orderOfNumbers = new Integer[10];
+    private static List<Integer> computerNumbers = new ArrayList<>();
+    private static String userNumbers;
     public static void main(String[] args) {
         // 시작
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -12,4 +19,5 @@ public class Application {
         START,
         END
     }
+
 }
