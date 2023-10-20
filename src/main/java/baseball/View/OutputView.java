ace52a9d79d4fcb5bf62df4032ccf0fd61f59ecf
sungwon2598 <tjddnjs2598@gmail.com>
2023-10-20 18:41:58 +0900

    refactor(OutputView): View가 Model의 데이터를 컨트롤러에서 가져오게 수정
    기존의 코드는 view가 직접 model의 데이터를 가져왔다면
    수정된 코드는 controller에서 model의 데이터에 접근하여 view에 넘겨주려 한다

--- src/main/java/baseball/View/OutputView.java
+++ src/main/java/baseball/View/OutputView.java
@@ -1,24 +1,17 @@
 package baseball.View;
 
-import baseball.Model.GameData;
-
 public class OutputView {
 
-    private final GameData gameData;
-
-    public OutputView(GameData gameData) {
-        this.gameData = gameData;
-    }
     public void printGameStart(){
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
-    public void printHint() {
+    public void printHint(int ballCount, int strikeCount) {
         String hint = "";
-        if (gameData.getBallCount() > 0) {
-            hint += gameData.getBallCount() + "볼 ";
+        if (ballCount > 0) {
+            hint += ballCount + "볼 ";
         }
-        if (gameData.getStrikeCount() > 0) {
-            hint += gameData.getStrikeCount() + "스트라이크";
+        if (strikeCount > 0) {
+            hint += strikeCount + "스트라이크";
         }
         if (hint.isEmpty()) {
             hint = "낫싱";
