621c70684dba77e877db1ff418157cfaa2f10dde
jyajoo <jyajoo1020@gmail.com>
2023-10-20 21:13:05 +0900

    test: 잘못된 입력(3자리 수보다 적거나 많은 경우)

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -1,6 +1,8 @@
 package baseball;
 
+import static baseball.status.ErrorCode.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -15,4 +17,31 @@ class GameTest {
         List<Integer> inputNum = game.getIntegerInput("123");
         assertThat(inputNum).containsExactly(1, 2, 3);
     }
+
+    @Test
+    @DisplayName("잘못된 입력_3자리 수보다 적은 경우")
+    void validationInputTest2() {
+        Game game = new Game();
+        IllegalArgumentException exception = assertThrows(
+            IllegalArgumentException.class, ()->game.getIntegerInput("12"));
+        IllegalArgumentException exception2 = assertThrows(
+            IllegalArgumentException.class, ()->game.getIntegerInput("1"));
+        IllegalArgumentException exception3 = assertThrows(
+            IllegalArgumentException.class, ()->game.getIntegerInput(""));
+
+        assertThat(INVALID_LENGTH_INPUT.getMsg()).isEqualTo(exception.getMessage());
+        assertThat(INVALID_LENGTH_INPUT.getMsg()).isEqualTo(exception2.getMessage());
+        assertThat(INVALID_LENGTH_INPUT.getMsg()).isEqualTo(exception3.getMessage());
+    }
+
+    @Test
+    @DisplayName("잘못된 입력_3자리 수보다 많은 경우")
+    void validationInputTest3() {
+        Game game = new Game();
+        IllegalArgumentException exception = assertThrows(
+            IllegalArgumentException.class, ()->game.getIntegerInput("1234"));
+
+        assertThat(INVALID_LENGTH_INPUT.getMsg()).isEqualTo(exception.getMessage());
+    }
+
 }
