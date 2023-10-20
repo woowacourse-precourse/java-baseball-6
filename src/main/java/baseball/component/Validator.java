f6efd85dfb8a551bef9e76e2463a41e1596684e0
Identityless <rlandwnd555@naver.com>
2023-10-20 22:42:03 +0900

    feat : 스트라이크 갯수 판별 기능 추가

--- src/main/java/baseball/component/Validator.java
+++ src/main/java/baseball/component/Validator.java
@@ -5,10 +5,23 @@ public class Validator {
 
     private int strike, ball;
     private boolean noting;
+    private List<Integer> target;
+    private final int NUMBER_SIZE = 3;
 
-    public Validator() {
+    public Validator(List<Integer> target) {
         strike = 0;
         ball = 0;
         noting = false;
+        this.target = target;
+    }
+
+    private void getStrike(List<Integer> numbers) {
+        int strike = 0;
+        for (int i = 0 ; i < NUMBER_SIZE ; i++) {
+            if (numbers.get(i) == target.get(i)) {
+                strike++;
+            }
+        }
+        this.strike = strike;
     }
 }
