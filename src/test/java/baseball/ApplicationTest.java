e987e9d4fe4192cfd0708fa070e6140750c85d6f
xogus3492 <wkdxogus3492@gmail.com>
2023-10-20 19:51:21 +0900

    test(input): 숫자가 중복된 입력인지에 대한 예외 테스트 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -36,6 +36,14 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 중복_숫자_입력_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("112"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
