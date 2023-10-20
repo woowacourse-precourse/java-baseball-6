c051fdac06c504b74fbd60602c047e042bbb431a
jinny-l <jinny.l.dev@gmail.com>
2023-10-20 20:02:19 +0900

    ✨feat: 게임 결과를 저장하는 `GameResult` 도메인 생성

---
+++ src/main/java/baseball/domain/GameResult.java
@@ -0,0 +1,20 @@
+package baseball.domain;
+
+public class GameResult {
+
+    private final int ballCount;
+    private final int strikeCount;
+
+    public GameResult(int ballCount, int strikeCount) {
+        this.ballCount = ballCount;
+        this.strikeCount = strikeCount;
+    }
+
+    public int getBallCount() {
+        return ballCount;
+    }
+
+    public int getStrikeCount() {
+        return strikeCount;
+    }
+}
