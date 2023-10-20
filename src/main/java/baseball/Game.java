f2d0e5583d3e7c5a9b6cb827998382c29316a616
leemiyeon <lemingul7@gmail.com>
2023-10-21 01:44:04 +0900

    feat: 결과 계산 기능 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -6,10 +6,15 @@ public class Game {
     public Game() {
     }
 
+    private int strike = 0;
+    private int ball = 0;
+
     public void startGame() {
 
         Number computer = new Number();
         Number player = new Number();
+
+        resetScore();
 
         List<Integer> computerNumber = computer.setRandomNumber();
         // log
@@ -19,8 +24,32 @@ public class Game {
         // log
         System.out.println("플레이어: " + playerNumber);
 
-        
+        resetScore();
+        calculateScore(computerNumber, playerNumber);
+
     }
 
+    private void calculateScore(List<Integer> computerNumber, List<Integer> playerNumber) {
 
+        for (int i = 0; i < computerNumber.size(); i++) {
+            if (computerNumber.get(i) == playerNumber.get(i)) {
+                strike++;
+            } else if (computerNumber.contains(playerNumber.get(i))) {
+                ball++;
+            }
+        }
+
+        if (strike == 0 && ball == 0) {
+            System.out.println("나싱");
+        } else {
+            System.out.println("스트라이크: " + strike);
+            System.out.println("볼: " + ball);
+        }
+    }
+
+    private void resetScore() {
+        strike = 0;
+        ball = 0;
+    }
+    
 }
