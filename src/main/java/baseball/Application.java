2be4fef5d5be863c7413e42deb8efbedf5368982
SeongIl <biuld1234@gmail.com>
2023-10-20 21:15:04 +0900

    Style: Modify if문{}

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,7 +13,7 @@ public class Application {
         // TODO: 프로그램 구현
         String inputNumber;
         Integer[] intArr = new Integer[3];
-        int strikeCount=0, ballCount=0;
+        int strikeCount = 0, ballCount = 0;
         //1. 선택
         List<Integer> selectNumber = new ArrayList<>();
         while (selectNumber.size() != 3) {
@@ -36,21 +36,24 @@ public class Application {
                 inputNumber = Console.readLine();
 
                 //입력값의 크기가 3개가 아닐 때 => 예외 발생
-                if (inputNumber.length() != 3)
+                if (inputNumber.length() != 3) {
                     throw new IllegalArgumentException("3개의 숫자만 입력하세요.");
+                }
 
                 //숫자배열 만들기
                 strArr = inputNumber.split("");
                 for (int i = 0; i < 3; i++) {
 
                     //0입력시 => 예외 발생
-                    if (Integer.parseInt(strArr[i]) == 0)
+                    if (Integer.parseInt(strArr[i]) == 0) {
                         throw new IllegalArgumentException("1~9 사이의 숫자를 입력하세요.");
+                    }
                     intArr[i] = Integer.parseInt(strArr[i]);
                     //같은 숫자를 입력했을 때 => 예외발생
                     for (int j = i + 1; j < 3; j++)
-                        if (intArr[i] == Integer.parseInt(strArr[j]))
+                        if (intArr[i] == Integer.parseInt(strArr[j])) {
                             throw new IllegalArgumentException("서로 다른 숫자를 입력하세요.");
+                        }
 
                 }
 
@@ -65,9 +68,9 @@ public class Application {
             //4. 규칙에 따라 출력
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
-                    if (selectNumber.get(i) == intArr[j] && i == j)
+                    if (selectNumber.get(i) == intArr[j] && i == j) {
                         strikeCount++;
-                    else if (selectNumber.get(i) == intArr[j]) {
+                    } else if (selectNumber.get(i) == intArr[j]) {
                         ballCount++;
                     }
                 }
@@ -78,21 +81,23 @@ public class Application {
             if (strikeCount != 0) {
                 System.out.print(strikeCount + "스트라이크");
             }
-            if(ballCount + strikeCount ==0)
+            if (ballCount + strikeCount == 0) {
                 System.out.print("낫싱");
+            }
             System.out.println();
-            if(strikeCount ==3){
+            if (strikeCount == 3) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 inputNumber = Console.readLine();
                 try {
-                    if (Integer.parseInt(inputNumber) != 1 && Integer.parseInt(inputNumber) != 2)
+                    if (Integer.parseInt(inputNumber) != 1 && Integer.parseInt(inputNumber) != 2) {
                         throw new IllegalArgumentException("1과 2중에 입력하세요.");
-                }catch (NumberFormatException ex){
+                    }
+                } catch (NumberFormatException ex) {
                     throw new IllegalArgumentException("숫자만 입력하세요.");
                 }
 
-                if(Integer.parseInt(inputNumber) == 1){
+                if (Integer.parseInt(inputNumber) == 1) {
                     selectNumber.clear();
                     while (selectNumber.size() != 3) {
                         int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -101,8 +106,7 @@ public class Application {
                         }
                     }
                     System.out.println("selectNumber = " + selectNumber);
-                }
-                else {
+                } else {
                     break;
                 }
             }
