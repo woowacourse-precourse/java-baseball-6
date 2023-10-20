f4a24a6d325ba2c6bd99346e96f827f9aaca115c
uhanuu <hwryu990927@gmail.com>
2023-10-20 16:56:56 +0900

    test: 객체 생성 test

--- src/test/java/baseball/api/ComputerTest.java
+++ src/test/java/baseball/api/ComputerTest.java
@@ -3,7 +3,6 @@ package baseball.api;
 import org.junit.jupiter.api.*;
 
 import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.Collection;
 import java.util.List;
--- src/test/java/baseball/api/GameHelperTest.java
+++ src/test/java/baseball/api/GameHelperTest.java
@@ -1,7 +1,30 @@
 package baseball.api;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class GameHelperTest {
 
+    @Test
+    @DisplayName("startGame 메서드를 통해서 GameStarter 객체를 얻어올 수 있다.")
+    public void startGame() {
+        // given
+        GameHelper gameHelper = new GameHelper();
+        // when
+        GameStarter gameStarter = gameHelper.startGame();
+        // then
+        Assertions.assertThat(gameStarter).isNotNull();
+    }
+
+    @Test
+    @DisplayName("reStartGame 메서드를 통해서 GameStarter 객체를 얻어올 수 있다.")
+    public void reStartGame() {
+        // given
+        GameHelper gameHelper = new GameHelper();
+        // when
+        GameStarter gameStarter = gameHelper.reStartGame();
+        // then
+        Assertions.assertThat(gameStarter).isNotNull();
+    }
 }
