78065a32cb6f55aa5a5620d47e8bc566b5fa56dc
cloudchamb3r <jizon0123@protonmail.com>
2023-10-20 10:49:48 +0900

    misc: remove .  in StringUtil

--- src/main/java/baseball/util/StringUtil.java
+++ src/main/java/baseball/util/StringUtil.java
@@ -1,26 +1,27 @@
 package baseball.util;
 
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
+
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class StringUtil {
     public static Set<Character> stringToSetOfChar(String s) {
         var chars = s.chars();
         var objChars = chars.mapToObj(c -> (char) c);
-        return objChars.collect(Collectors.toSet());
+        return objChars.collect(toSet());
     }
 
     public static List<Character> stringToListOfChar(String s) {
         var chars = s.chars();
         var objChars = chars.mapToObj(c -> (char) c);
-        return objChars.collect(Collectors.toList());
+        return objChars.collect(toList());
     }
 
     public static List<Integer> digitStringToListOfInt(String s) {
-        var listOfChars = stringToListOfChar(s);
-        var stream = listOfChars.stream();
-        var mapToInt = stream.map(Character::getNumericValue);
-        return mapToInt.collect(Collectors.toList());
+        var charStream = stringToListOfChar(s).stream();
+        var charStreamIntMapping = charStream.map(Character::getNumericValue);
+        return charStreamIntMapping.collect(toList());
     }
 }
