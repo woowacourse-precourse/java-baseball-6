2cb5c9f3c86705705c4bb4151e9130ffb6b6a5b1
정성욱 <rkfka7849@gmail.com>
2023-10-20 21:47:46 +0900

    feat(InputHandler): 게임의 시작/종료를 입력받는 기능

--- src/main/java/baseball/view/InputHandler.java
+++ src/main/java/baseball/view/InputHandler.java
@@ -4,9 +4,12 @@ package baseball.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputHandler {
-    public String insertBallNum(){
+    public String insertBallNum() {
         System.out.print("숫자를 입력해주세요 : ");
         return Console.readLine();
     }
 
+    public String insertNewGameOrGameOver() {
+        return Console.readLine();
+    }
 }
