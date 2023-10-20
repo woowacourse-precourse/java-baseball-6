52f7f5ddb4c3a4f8c6b79bdf41adeac79705b0d6
hyeonwoo <ziczin7176@gmail.com>
2023-10-21 05:57:03 +0900

    코드 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,27 +3,23 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
-        final List<String> computerNumbers = new ArrayList<>();
-        List<String> playerNumbers;
+        final String[] computerNumbers = new String[3];
+        String[] playerNumbers;
         boolean isAnswer = false;
+
         // [기능 1] 컴퓨터가 1 - 9 서로 다른 임의의 수 3개 선택하는 기능
         for (int i = 0; i < 3; i++) {
-            computerNumbers.add(String.valueOf(Randoms.pickNumberInRange(1, 9)));
+            computerNumbers[i] = String.valueOf(Randoms.pickNumberInRange(1, 9));
         }
 
         // [기능 4] 1 - 3 과정을 계속 반복하고 컴퓨터가 선택한 숫자 3개를 모두 맞히면 게임이 종료되는 기능
-        while(isAnswer) {
-            playerNumbers = Arrays.stream(getValidatedInput()).toList();
-            isAnswer = playGame();
+        while(!isAnswer) {
+            playerNumbers = getValidatedInput();
+            isAnswer = playGame(computerNumbers, playerNumbers);
         }
         // [기능 5] 게임 종료 후 플레이어가 게임을 다시 시작하거나 완전히 종료시킬 수 있는 기능 (1: 재시작, 2: 종료)
     }
@@ -45,15 +41,15 @@ public class Application {
         }
         // 중복된 숫자를 입력한 경우
         String[] letters = input.split("");
-        if(Objects.equals(letters[0], letters[1]) || Objects.equals(letters[0], letters[2]) || Objects.equals(letters[1], letters[2])){
+        if(letters[0].equals(letters[1]) || letters[0].equals(letters[2]) || letters[1].equals(letters[2])){
             throw new IllegalArgumentException("중복된 숫자를 입력했습니다.");
         }
         return letters;
     }
 
     // [기능 3] 컴퓨터가 플레이어가 입력한 숫자에 대한 숫자야구 게임 결과를 출력하는 기능
-    private static boolean playGame(){
-
+    private static boolean playGame(String[] computerNumbers, String[] playerNumbers){
+        
         return false;
     }
 }
