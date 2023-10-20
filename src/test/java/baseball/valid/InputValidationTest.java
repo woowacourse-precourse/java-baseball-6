6ccae4b1790f37c427e17749b0d9e124aba91b65
jongwonpc <c3392302@naver.com>
2023-10-20 15:32:39 +0900

    test: 재시작 여부 입력받는 기능의 검증 테스트

--- src/test/java/baseball/valid/InputValidationTest.java
+++ src/test/java/baseball/valid/InputValidationTest.java
@@ -2,6 +2,7 @@ package baseball.valid;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -26,5 +27,23 @@ class InputValidationTest {
     void 사용자_Num_검증3() throws Exception{
         String test = "1a2";
         assertThrows(IllegalArgumentException.class, () -> InputValidation.validateInputNum(test));
+    }
+
+    @Test
+    @DisplayName("1이나 2가 아닌 다른 것이 들어가면 에러를 터트린다")
+    void restart_Num_검증에러() throws Exception{
+        List<String> testList = List.of("3", "35", "12", "bg");
+        for (String test : testList) {
+            assertThrows(IllegalArgumentException.class,() -> InputValidation.validateRestartNum(test));
+        }
+    }
+
+    @Test
+    @DisplayName("1이나 2가 들어갈 시 에러가 발생하지 않는다")
+    void restart_Num_검증성공() throws Exception{
+        List<String> testList = List.of("1","2");
+        for (String test : testList) {
+            assertDoesNotThrow(() -> InputValidation.validateRestartNum(test));
+        }
     }
 }
