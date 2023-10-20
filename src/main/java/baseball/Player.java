5d0df5963a9f2a7c9498dedc1154251e6995fb9d
suhyun9764 <suhyun9764@naver.com>
2023-10-20 13:43:11 +0900

    refactor:player 객체 구조 변경

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -5,59 +5,26 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 
 public class Player {
-    private ArrayList<Integer> playerValue;
     private final DataValidator dataValidator;
-    private int strike;
-    private int ball;
 
     public Player() {
-        this.playerValue = new ArrayList<Integer>();
         this.dataValidator = new DataValidator();
-        strike = 0;
-        ball = 0;
-        continueDecision = 1;
     }
 
-    public void setPlayerValue() {
-        System.out.print("숫자를 입력해주세요 : ");
+    public ArrayList<Integer> getPlayerAnswer() {
         String[] stringValue  = Console.readLine().split("");
+        ArrayList<Integer> playerAnswer = new ArrayList<>();
         for(String s : stringValue){
-            playerValue.add(Integer.parseInt(s));
+            playerAnswer.add(Integer.parseInt(s));
         }
-        dataValidator.playerValueValidation(playerValue);
+        dataValidator.playerAnswerValidation(playerAnswer);
+        return playerAnswer;
     }
 
-    public ArrayList<Integer> getPlayerValue(){
-        return playerValue;
-    }
-
-    public int getStrike() {
-        return strike;
-    }
-
-    public void setStrike(int strike) {
-        this.strike = strike;
-    }
-
-    public int getBall() {
-        return ball;
-    }
-
-    public void setBall(int ball) {
-        this.ball = ball;
-    }
 
     public int getContinueDecision() {
+        int continueDecision = Integer.parseInt(Console.readLine());
+        dataValidator.continueDecisionValidate(continueDecision);
         return continueDecision;
     }
-
-    public void setContinueDecision(int continueDecision) {
-        this.continueDecision = continueDecision;
-    }
-
-    private int continueDecision;
-
-
-
-
 }
