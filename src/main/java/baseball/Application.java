f93ab1a4e5fe399aada5845ecbcd577a24a7f887
FriedHamm <kwh2716@gmail.com>
2023-10-20 11:00:10 +0900

    스타일 약간 수정 커밋 컨벤션 적용해야 함 예외처리 하나 빠짐(중복처리)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,12 @@
 package baseball;
-import camp.nextstep.edu.missionutils. *;
+
+import camp.nextstep.edu.missionutils.*;
 
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-       BaseballApplication.turnOn();
+        BaseballApplication.turnOn();
     }
 }
