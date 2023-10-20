99642258029e466dd1448ce71742698ffe26a152
LeeJaeHoon <dlwogns3413@naver.com>
2023-10-20 23:36:10 +0900

    feat: InputOutputHandler class 구현

--- src/main/java/baseball/domain/InputOutputHandler.java
+++ src/main/java/baseball/domain/InputOutputHandler.java
@@ -1,12 +1,19 @@
 package baseball.domain;
 
-import java.util.Collections;
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputOutputHandler {
 
     public List<Integer> inputUniqueNumbers(int numberLength) {
-        return Collections.emptyList();
+        String input = Console.readLine();
+        if(!isNumeric(input) || hasDuplicateCharacters(input)) throw new IllegalStateException("오류발생");
+
+        return input.chars()
+                .mapToObj(Character::getNumericValue)
+                .collect(Collectors.toList());
     }
 
     public int inputNumber() {
@@ -14,6 +21,20 @@ public class InputOutputHandler {
     }
 
     public void printText(String text) {
-
+        System.out.print(text);
     }
+
+    public void printlnText(String text) {
+        System.out.println(text);
+    }
+
+    private boolean isNumeric(String str) {
+        return str.matches("\\d+");
+    }
+
+    private boolean hasDuplicateCharacters(String str) {
+        return str.length() != str.chars().distinct().count();
+    }
+
+
 }
