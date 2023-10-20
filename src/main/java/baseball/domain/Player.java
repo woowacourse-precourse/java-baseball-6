16e97777d15022b68c4cdd7a061401bb87a1bcd2
SehyeonKang <gedfgrdt@naver.com>
2023-10-20 11:32:24 +0900

    feat(Player): 사용자 클래스 추가

---
+++ src/main/java/baseball/domain/Player.java
@@ -0,0 +1,12 @@
+package baseball.domain;
+
+public class Player {
+
+    private int ballCount;
+    private int strikeCount;
+
+    public Player() {
+        ballCount = 0;
+        strikeCount = 0;
+    }
+}
