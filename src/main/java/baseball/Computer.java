44bec1986b6b74d28a3b282854fa6a9f26146e91
ga0nii <kgw0124@kakao.com>
2023-10-20 21:39:13 +0900

    [refact/컴퓨터] - 1에서 9까지 서로 다른 임의의 수 생성하는 함수를 생성자가 아닌 getNumbers()에서 수행하도록 수정

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -6,20 +6,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
-    private List<Integer> numbers = new ArrayList<>();
-
-    //생성자 : 1부터 9까지 서로 다른 수로 이루어진 3자리의 수 생성
-    Computer(){
+    //1에서 9까지 서로 다른 임의의 수 3개 생성 및 반환
+    public List<Integer> getNumbers(){
+        List<Integer> numbers = new ArrayList<>();
         while(numbers.size() < 3){
             int randomNumb = Randoms.pickNumberInRange(1,9);
             if(!numbers.contains(randomNumb)){
                 numbers.add(randomNumb);
             }
         }
-    }
-
-    //생성한 수 반환 함수
-    public List<Integer> getNumbers(){
         return numbers;
     }
 }
