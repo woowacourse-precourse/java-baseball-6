f685007a7fb4d861e867d9a7d7ce79e59dcdf2cf
김효준 <khyojun1104@gmail.com>
2023-10-20 19:36:38 +0900

    [feat] Validation : 숫자 관련 검증 로직 작성 : 숫자인지? 길이가 3인지?

--- src/main/java/baseball/util/Validation.java
+++ src/main/java/baseball/util/Validation.java
@@ -1,5 +1,20 @@
 package baseball.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Validation {
 
+    private static final String NUMBER_REGEX = "^[1-9]$";
+    private static final Integer INPUT_LENGTH = 3;
+
+    public boolean validateNumber(String number) {
+        if(isRightNumber(number))
+            return true;
+        throw new IllegalArgumentException("Error");
+    }
+
+    private boolean isRightNumber(String number) {
+        return number.matches(NUMBER_REGEX) && number.length() == INPUT_LENGTH;
+    }
 }
