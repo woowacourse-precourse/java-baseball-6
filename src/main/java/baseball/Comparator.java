ebbd771fc348cf5a6c76b0f9b9afe541b5651b47
Gukin Han <gukin.dev@gmail.com>
2023-10-20 18:48:03 +0900

    Refactor: 메서드명 descriptive하게 변경

--- src/main/java/baseball/Comparator.java
+++ src/main/java/baseball/Comparator.java
@@ -6,40 +6,47 @@ public class Comparator {
 
     private int ballCount;
     private int strikeCount;
+    private Map<Integer, Integer> generatedNumbers;
+    private Map<Integer, Integer> guessedNumbers;
 
-    private void init() {
+    private void init(Map<Integer, Integer> generatedNumbers, Map<Integer, Integer> guessedNumbers) {
         this.ballCount = 0;
         this.strikeCount = 0;
+        this.generatedNumbers = generatedNumbers;
+        this.guessedNumbers = guessedNumbers;
     }
 
-    public boolean compare(Map<Integer, Integer> generatedNumbers, Map<Integer, Integer> guessedNumbers) {
-        init();
+    public boolean checkThreeStrikeAndPrintResult(Map<Integer, Integer> generatedNumbers, Map<Integer, Integer> guessedNumbers) {
+        init(generatedNumbers, guessedNumbers);
+        countStrikeAndBall();
+        printResult();
+        return strikeCount == 3;
+    }
 
-        countStrikeAndBall(generatedNumbers, guessedNumbers);
-
+    private void printResult() {
         StringBuilder result = new StringBuilder();
         if (ballCount != 0) result.append(ballCount).append("볼 ");
         if (strikeCount != 0) result.append(strikeCount).append("스트라이크");
         if (strikeCount == 0 && ballCount == 0) result.append("낫싱");
-
         System.out.println(result);
-
-        return strikeCount == 3;
     }
 
-    private void countStrikeAndBall(Map<Integer, Integer> generatedNumbers, Map<Integer, Integer> guessedNumbers) {
+    private void countStrikeAndBall() {
         for (int guessedNumber : guessedNumbers.keySet()) {
             if(!generatedNumbers.containsKey(guessedNumber)) {
                 continue;
             }
-
-            int generatedPosition = generatedNumbers.get(guessedNumber);
-            int guessedPosition = guessedNumbers.get(guessedNumber);
-            if (generatedPosition == guessedPosition) {
-                strikeCount++;
-                continue;
-            }
-            ballCount++;
+            countStrikeAndBall(guessedNumber);
         }
+    }
+
+    private void countStrikeAndBall(int guessedNumber) {
+        int generatedPosition = generatedNumbers.get(guessedNumber);
+        int guessedPosition = guessedNumbers.get(guessedNumber);
+        if (generatedPosition == guessedPosition) {
+            strikeCount++;
+            return;
+        }
+        ballCount++;
     }
 }
