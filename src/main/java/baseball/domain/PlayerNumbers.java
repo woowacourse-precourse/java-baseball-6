ceaff0d09219d022809f2b15d3e5270f00e34193
howudong <tjdvy963@naver.com>
2023-10-20 15:53:49 +0900

    feat : 플레이어의 입력을 담당하는 PlayerNumbers 클래스 추가

---
+++ src/main/java/baseball/domain/PlayerNumbers.java
@@ -0,0 +1,32 @@
+package baseball.domain;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class PlayerNumbers {
+    private final List<Integer> numbers;
+
+    public PlayerNumbers(List<Integer> numbers) {
+        validateNumberSize(numbers);
+        validateDuplicate(numbers);
+        this.numbers = numbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        int distinctSize = new HashSet<>(numbers).size();
+
+        if (numbers.size() != distinctSize) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNumberSize(List<Integer> numbers) {
+        if (numbers.size() != AnswerNumbers.NUMBER_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
