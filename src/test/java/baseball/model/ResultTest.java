ea6d03531823edf741e6743808715ce95ff94f4b
shin5774 <babo5774@naver.com>
2023-10-21 02:15:11 +0900

    Test: "결과 비교 기능 테스트 코드 작성"
    - 두 Numbers를 비교하여 game의 결과를 가지는 Result라는 클래스.
    -해당 클래스가 정상적으로 작동해 올바른 결과값을 가지는지 확인하는 테스트코드

---
+++ src/test/java/baseball/model/ResultTest.java
@@ -0,0 +1,20 @@
+package baseball.model;
+
+import baseball.domain.Numbers;
+import baseball.domain.Result;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class ResultTest {
+    @DisplayName("Numbers의 비교롤 통한 result 생성 기능")
+    @Test
+    void 결과_생성(){
+        //둘다 1스트라이크 1볼이 나온다.
+        Result result1=new Result(new Numbers("123"),new Numbers("134"));
+        Result result2=new Result(new Numbers("123"),new Numbers("243"));
+
+        assertThat(result1.equals(result2)).isTrue();
+    }
+}
