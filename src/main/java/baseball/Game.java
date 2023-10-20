da3fb0b526b6db8c8c9b007a031ccbc3a675a487
kimnoca <kimnoca@naver.com>
2023-10-20 22:08:26 +0900

    Feat: 사용자의 입력받기 기능 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,17 +1,37 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class Game {
     Player user;
     Player computer;
 
+    public void startGame() {
+        this.user = new Player();
+        this.computer = new Player();
+        initComputerNumbers();
+        inputUser();
+        System.out.println(computer.getPlayerNumbers());
+        System.out.println(user.getPlayerNumbers());
+    }
+
+    public void inputUser() {
+        String inputData = Console.readLine();
+        String[] inputNumbers = inputData.split("");
+        List<Integer> userNumbers = new ArrayList<>(3);
+        for (int i = 0; i < 3; i++) {
+            int tempNumber = Integer.parseInt(inputNumbers[i]); //
+            userNumbers.add(tempNumber);
+        }
+        this.user.setPlayerNumbers(userNumbers);
+    }
+
     public void initComputerNumbers() {
-        List<Integer> computerNumbers = new ArrayList<Integer>(3);
+        List<Integer> computerNumbers = new ArrayList<>(3);
         while (computerNumbers.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computerNumbers.contains(randomNumber)) {
