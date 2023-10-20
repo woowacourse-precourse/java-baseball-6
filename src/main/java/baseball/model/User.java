76cfc09a8a3896def23f7baeab322f8f993634f1
jihwankim128 <jihwan1535@nate.com>
2023-10-20 17:36:28 +0900

    feat: 사용자 모델 기본구조 생성

---
+++ src/main/java/baseball/model/User.java
@@ -0,0 +1,25 @@
+package baseball.model;
+
+import java.util.List;
+
+public class User {
+    private List<Integer> numbers;
+    public User() {
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public void setNumbers(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "numbers=" + numbers +
+                '}';
+    }
+}
