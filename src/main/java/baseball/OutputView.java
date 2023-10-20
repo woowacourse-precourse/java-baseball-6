ce5feec96b1e2acd44ad327b8c4a0e38a8337e68
hwanhee <brightfanta@gmail.com>
2023-10-20 18:00:53 +0900

    refactor: separate print-result-function according to 4 cases

--- src/main/java/baseball/OutputView.java
+++ src/main/java/baseball/OutputView.java
@@ -1,34 +1,34 @@
 package baseball;
 
 public class OutputView {
-    public static void printStart() {
+    public void printStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
-    public static void printRequirePlayerInput() {
+    public void printRequirePlayerInput() {
         System.out.println("숫자를 입력해주세요 : ");
     }
 
-    public static void printBallAndStrikeResult(Integer strikeCnt, Integer ballCnt) {
+    public void printBallAndStrikeResult(Integer strikeCnt, Integer ballCnt) {
         System.out.println(ballCnt + "볼 " + strikeCnt + "스트라이크");
     }
 
-    public static void printBallOnlyResult(Integer ballCnt) {
+    public void printBallOnlyResult(Integer ballCnt) {
         System.out.println(ballCnt + "볼 ");
     }
 
-    public static void printStrikeOnlyResult(Integer strikeCnt) {
+    public void printStrikeOnlyResult(Integer strikeCnt) {
         System.out.println(strikeCnt + "스트라이크");
     }
 
-    public static void printNothingResult() {
+    public void printNothingResult() {
         System.out.println("낫싱");
     }
 
-    public static void printEndgame() {
+    public void printEndgame() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
-    public static void printAskRestart() {
+    public void printAskRestart() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 }
