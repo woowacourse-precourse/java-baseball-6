c8daa5b5741cca061f33d3b72c90822999a01b2f
Sully <2dcoder@naver.com>
2023-10-20 18:28:59 +0900

    feat: 입력한 문자의 형식을 판단하는 NumberValidator의 메서드 추가
    validateDuplicateNumber는  validateAllDigits가 먼저 검증되었기 때문에 Integer.parseInt()로 넣을 수 있다.

--- src/main/java/baseball/user/UserInput.java
+++ src/main/java/baseball/user/UserInput.java
@@ -1,6 +1,9 @@
 package baseball.user;
 
 import static baseball.output.GameOutput.printRequestMessage;
+import static baseball.validator.NumberValidator.validateAllDigits;
+import static baseball.validator.NumberValidator.validateDuplicateNumber;
+import static baseball.validator.NumberValidator.validateRequiredLength;
 
 import camp.nextstep.edu.missionutils.Console;
 
@@ -8,7 +11,13 @@ public class UserInput {
 
     public String inputNumberString() {
         printRequestMessage();
-        return Console.readLine();
+        String numberString = Console.readLine();
+
+        validateRequiredLength(numberString);
+        validateAllDigits(numberString);
+        validateDuplicateNumber(Integer.parseInt(numberString));
+
+        return numberString;
     }
 
 }
