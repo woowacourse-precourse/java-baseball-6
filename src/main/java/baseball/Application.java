18fa8e0b914b423a8c5cb150ddabaafc106beed4
WooJJam <jaemin5548@naver.com>
2023-10-20 18:27:31 +0900

    [Feat] 중복을 제외한 랜덤숫자 생성 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
 
+import baseball.utils.RandomUtils;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        int[] answer = RandomUtils.getRandomNumbers();
+        for (int i =0; i<3; i++) {
+            System.out.print(answer[i]);
+        }
     }
 }
--- src/main/java/baseball/utils/RandomUtils.java
+++ src/main/java/baseball/utils/RandomUtils.java
@@ -1,2 +1,28 @@
-package baseball.utils;public class RandomUtils {
+package baseball.utils;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomUtils {
+
+    public static int[] getRandomNumbers() {
+        int[] numbers = new int[3];
+        boolean flag = false;
+
+        for(int i=0; i<3;) {
+            int randNum = Randoms.pickNumberInRange(1,9);
+            if(checkDuplicated(randNum,numbers)) {
+                numbers[i++]=randNum;
+            }
+        }
+        return numbers;
+    }
+
+    public static boolean checkDuplicated(int randNum, int[] numbers) {
+        for(int j=0; j<numbers.length; j++) {
+            if(numbers[j] == randNum) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
