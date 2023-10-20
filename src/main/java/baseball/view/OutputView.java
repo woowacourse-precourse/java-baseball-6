dc9f9fee310cafdec68c5ed5f97e11d8dd8fb4b9
soozzang <soozzang@inha.edu>
2023-10-20 16:54:54 +0900

    Feat: printInputMessage 생성

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,4 +4,8 @@ public class OutputView {
     public void printGameStartMessage() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
+    public void printInputMessage() {
+        System.out.println("숫자를 입력해주세요 : ");
+    }
 }
