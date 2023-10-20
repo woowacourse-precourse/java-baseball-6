236f894aa16d1a19012c0a1e69fb098f42f31ce6
kwon <kckc0608@naver.com>
2023-10-21 00:01:40 +0900

    feat: computer 클래스  추측에 대한 결과 판단 메소드 추가

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -20,4 +20,21 @@ public class Computer {
             }
         }
     }
+
+    public List<Integer> getResultOfGuess(List<Integer> guess) {
+        List<Integer> result = new ArrayList<>();
+        int strike = 0, ball = 0;
+        for (int i = 0; i < 3; i++) {
+            if (guess.get(i).equals(answer.get(i))) {
+                strike++;
+            } else if (answer.contains(guess.get(i))) {
+                ball++;
+            }
+        }
+
+        result.add(strike);
+        result.add(ball);
+
+        return result;
+    }
 }
