eb651c6fb7b1624e01ec1334198b9f1277d93e8e
jooys130 <wndustn130@gmail.com>
2023-10-20 22:38:54 +0900

    feat: 게임 시작 문구를 출력한다

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,9 @@
 package baseball;
 
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        PrintMessage printMessage = new PrintMessage();
+        printMessage.printGameStartMessage();
     }
 }
---
+++ src/main/java/baseball/PrintMessage.java
@@ -0,0 +1,9 @@
+package baseball;
+
+public class PrintMessage {
+
+    public void printGameStartMessage() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+}
