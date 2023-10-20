4e4bb0eff977b689d6633224f6b656a119544bfa
suddiyo <suddiyo@naver.com>
2023-10-20 16:46:23 +0900

    feat: 1에서 9까지 서로 다른 임의의 수 3개 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import baseball.controller.BaseballController;
+import baseball.service.BaseballNumberGenerator;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 
@@ -8,7 +9,10 @@ public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
-        BaseballController baseballController = new BaseballController(inputView, outputView);
+        BaseballNumberGenerator baseballNumberGenerator = new BaseballNumberGenerator();
+        BaseballController baseballController = new BaseballController(inputView, outputView, baseballNumberGenerator);
+
         baseballController.startBaseballGame();
+
     }
 }
--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -1,18 +1,24 @@
 package baseball.controller;
 
+import baseball.service.BaseballNumberGenerator;
 import baseball.view.InputView;
 import baseball.view.OutputView;
+
+import java.util.List;
 
 public class BaseballController {
     private final InputView inputView;
     private final OutputView outputView;
+    private final BaseballNumberGenerator baseballNumberGenerator;
 
-    public BaseballController(InputView inputView, OutputView outputView) {
+    public BaseballController(InputView inputView, OutputView outputView, BaseballNumberGenerator baseballNumberGenerator) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.baseballNumberGenerator = baseballNumberGenerator;
     }
 
     public void startBaseballGame() {
         outputView.printStartGameMessage();
+        List<Integer> numbers = baseballNumberGenerator.generateNumbers();
     }
 }
