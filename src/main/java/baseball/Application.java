3c158cda9957a64eab103261bda8a6372423c1dd
HeeHeeg <qmffndoden@naver.com>
2023-10-20 14:09:05 +0900

    컴퓨터가 숫자에 대한 결과를 출력부분 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -35,15 +35,21 @@ public class Application {
             }
             if (strike != 0 || ball != 0) {
                 if (strike == 3 && ball == 0) {
-                    System.out.println(strike + "스트라이크" + "3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    System.out.println(strike + "스트라이크");
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                     return;
-                }else {
+                } else if (strike == 0 && ball != 0) {
+                    System.out.println(ball + "볼");
+                } else if (strike != 0 && ball == 0) {
+                    System.out.println(strike + "스트라이크");
+                } else {
                     System.out.println(strike + "스트라이크, " + ball + "볼");
                 }
 
             } else {
                 System.out.println(nothing);
             }
+            System.out.println(computer);
         }
     }
 }
