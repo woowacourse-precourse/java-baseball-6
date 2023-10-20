f65861bef67c3db152cde93b1321fa9e1004770b
Haebin <joker7011@naver.com>
2023-10-20 21:10:05 +0900

    feat: 시스템 설정(Config) 제약조건 추가

--- src/main/java/baseball/config/Config.java
+++ src/main/java/baseball/config/Config.java
@@ -1,6 +1,7 @@
 package baseball.config;
 
 public class Config {
-
+    public static final int NUMBER_MINIMUM_VALUE = 1;
+    public static final int NUMBER_MAXIMUM_VALUE = 9;
     public static final int NUMBER_LENGTH = 3;
 }
