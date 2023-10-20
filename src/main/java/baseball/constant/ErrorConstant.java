d1d1da5e837dd27d507b57f1b3ba9abfa07804c5
gosu <q63530@naver.com>
2023-10-20 14:23:30 +0900

    refactor : 코드 컨벤션 및 상수 리팩토링

--- src/main/java/baseball/constant/ErrorConstant.java
+++ src/main/java/baseball/constant/ErrorConstant.java
@@ -2,5 +2,5 @@ package baseball.constant;
 
 public class ErrorConstant {
 
-    public static String INCORRECT_ERROR = "올바르지 않은 값을 입력 받았습니다 ";
+    public static final String INCORRECT_ERROR = "올바르지 않은 값을 입력 받았습니다 ";
 }
--- src/main/java/baseball/game/Game.java
+++ src/main/java/baseball/game/Game.java
@@ -1,27 +1,45 @@
 package baseball.game;
 
-import baseball.constant.ErrorConstant;
+import static baseball.constant.ErrorConstant.INCORRECT_ERROR;
+import static baseball.constant.GameConstant.BALL;
+import static baseball.constant.GameConstant.COMPLETE_CHECK_NUM;
+import static baseball.constant.GameConstant.END_VALUE;
+import static baseball.constant.GameConstant.GAME_END;
+import static baseball.constant.GameConstant.GAME_START;
+import static baseball.constant.GameConstant.INPUT_NUMBER;
+import static baseball.constant.GameConstant.INPUT_NUMBER_SIZE;
+import static baseball.constant.GameConstant.LAST_RANGE;
+import static baseball.constant.GameConstant.NOTHING;
+import static baseball.constant.GameConstant.NOTHING_CHECK_NUM;
+import static baseball.constant.GameConstant.RANDOM_NUMBER_SIZE;
+import static baseball.constant.GameConstant.REGULAR_EXPRESSION_INPUT_NUMBER;
+import static baseball.constant.GameConstant.REGULAR_EXPRESSION_RESTART_OR_END_NUMBER;
+import static baseball.constant.GameConstant.RESTART_OR_EXIT;
+import static baseball.constant.GameConstant.RESTART_VALUE;
+import static baseball.constant.GameConstant.START_RANGE;
+import static baseball.constant.GameConstant.STRIKE;
+import static baseball.constant.GameConstant.THREE_STRIKE;
+
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-
-import static baseball.constant.ErrorConstant.*;
-import static baseball.constant.GameConstant.*;
 
 public class Game {
-    List<Integer> randomNumber = new ArrayList<>();
+    final List<Integer> randomNumber;
 
-    public void run()  {
+    public Game() {
+        this.randomNumber = new ArrayList<>();
+    }
+
+    public void run() {
         do {
-            oneGame();
+            oneGamePlay();
         } while (restartOrExit());
     }
 
     //한 게임
-    private void oneGame() {
+    private void oneGamePlay() {
         boolean iterCheck;
         System.out.println(GAME_START);
         createRandomNumber();
@@ -35,7 +53,7 @@ public class Game {
     }
 
     //난수 생성
-    private void createRandomNumber(){
+    private void createRandomNumber() {
         randomNumber.clear();
         while (randomNumber.size() < RANDOM_NUMBER_SIZE) {
             int intRandomNumber = Randoms.pickNumberInRange(START_RANGE, LAST_RANGE);
@@ -46,7 +64,7 @@ public class Game {
     }
 
     //유저 입력
-    private String userInput()  {
+    private String userInput() {
         System.out.print(INPUT_NUMBER);
         return Console.readLine();
     }
@@ -59,19 +77,19 @@ public class Game {
     }
 
     //String -> List<Integer>
-    private List<Integer> stringToIntegerList(String stringNumber)  {
+    private List<Integer> stringToIntegerList(String stringNumber) {
         List<Integer> inputNumber = new ArrayList<>();
-        for(char charNumber : stringNumber.toCharArray()) {
+        for (char charNumber : stringNumber.toCharArray()) {
             inputNumber.add(Character.getNumericValue(charNumber));
         }
         return inputNumber;
     }
 
     //입력값과 정해진 난수와 비교하여 볼 , 스트라이크 , 낫싱 결정
-    private boolean inputCheck(List<Integer> number){
+    private boolean inputCheck(List<Integer> number) {
         int strike = strikeCheck(number);
         int ball = ballCheck(number);
-        boolean nothing = nothingCheck(strike,ball);
+        boolean nothing = nothingCheck(strike, ball);
         if (nothing) {
             printNothing();
             return true;
@@ -87,71 +105,71 @@ public class Game {
     }
 
     //입력값과 난수를 비교하여 스트라이크 결정
-    private int strikeCheck(List<Integer> number){
+    private int strikeCheck(List<Integer> number) {
         int count = 0;
-        for (int i = 0; i<INPUT_NUMBER_SIZE; i++) {
+        for (int i = 0; i < INPUT_NUMBER_SIZE; i++) {
             if (number.get(i).equals(randomNumber.get(i))) {
-                count+=1;
-                number.set(i,COMPLETE_CHECK_NUM);
+                count += 1;
+                number.set(i, COMPLETE_CHECK_NUM);
             }
         }
         return count;
     }
 
     //입력값과 난수를 비교하여 볼 결정
-    private int ballCheck(List<Integer> number){
+    private int ballCheck(List<Integer> number) {
         int count = 0;
-        for (int i = 0; i<INPUT_NUMBER_SIZE; i++) {
+        for (int i = 0; i < INPUT_NUMBER_SIZE; i++) {
             if (randomNumber.contains(number.get(i))) {
-                count+=1;
-                number.set(i,COMPLETE_CHECK_NUM);
+                count += 1;
+                number.set(i, COMPLETE_CHECK_NUM);
             }
         }
         return count;
     }
 
     //입력값과 난수를 비교하여 낫싱 결정
-    private boolean nothingCheck(int strike, int ball){
+    private boolean nothingCheck(int strike, int ball) {
         return ball == NOTHING_CHECK_NUM && strike == NOTHING_CHECK_NUM;
     }
 
     //낫싱 프린트
-    private void printNothing(){
+    private void printNothing() {
         System.out.println(NOTHING);
     }
 
     //스트라이크 프린트
-    private void printStrike(int strike){
+    private void printStrike(int strike) {
         if (strike != 0) {
-            System.out.print(strike+STRIKE);
+            System.out.print(strike + STRIKE);
         }
     }
 
     //볼 프린트
-    private void printBall(int ball){
+    private void printBall(int ball) {
         if (ball != 0) {
-            System.out.print(ball+BALL);
+            System.out.print(ball + BALL);
             System.out.print(" ");
         }
     }
 
     //게임 끝 프린트
-    private void gameEnd(){
+    private void gameEnd() {
         System.out.println(GAME_END);
     }
 
     //게임이 끝나고 입력값에 따라서 재시작할지(1) , 게임을 종료할지(2) 결정
-    private boolean restartOrExit()  {
+    private boolean restartOrExit() {
         gameEnd();
         System.out.println(RESTART_OR_EXIT);
         String restartOrExit = Console.readLine();
-        restartOrExitValueValidate(restartOrExit);
         return restartOrExitCheck(restartOrExit);
 
     }
 
     //입력값이 올바른지 , 재시작인지 , 종료인지를 확인
-    private boolean restartOrExitCheck(String restartOrExit)  {
+    private boolean restartOrExitCheck(String restartOrExit) {
+        restartOrExitValueValidate(restartOrExit);
         if (restartOrExit.equals(RESTART_VALUE)) {
             return true;
         } else if (restartOrExit.equals(END_VALUE)) {
