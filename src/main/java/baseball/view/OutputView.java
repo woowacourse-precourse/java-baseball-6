e8e279d3851018eff3de06bad400ac5327f93af2
soozzang <soozzang@inha.edu>
2023-10-21 01:07:54 +0900

    Refactor:printInputMessage 출력형식 맞춰 print로 변경

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -6,12 +6,12 @@ public class OutputView {
     }
 
     public void printInputMessage() {
-        System.out.println("숫자를 입력해주세요 : ");
+        System.out.print("숫자를 입력해주세요 : ");
     }
 
     public void printRoundResultMessage(int strike, int ball) {
         if(strike!=0 && ball!=0){
-            System.out.println(ball+"볼" +strike+"스트라이크");
+            System.out.println(ball+"볼 "+strike+"스트라이크");
         }
         if(strike!=0 && ball==0){
             System.out.println(strike+"스트라이크");
