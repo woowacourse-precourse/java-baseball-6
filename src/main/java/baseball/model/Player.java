e5efe6e8b789359c933e43752765e890a99771bc
junsoo <choix712@umn.edu>
2023-10-20 15:59:13 +0900

    feat: 3개 미만의 숫자나 4개 이상의 숫자를 받는 경우, 예외조건 발생 후 프로그램 종료

--- src/main/java/baseball/model/Player.java
+++ src/main/java/baseball/model/Player.java
@@ -9,12 +9,19 @@ public class Player {
 
     public Player(List<Integer> player) {
         validateCheckZero(player);
+        validateSize(player);
         this.player = player;
     }
 
     private void validateCheckZero(List<Integer> player) {
         if (player.contains(0)) {
             throw new IllegalArgumentException("플레이어는 1 ~ 9 사이의 숫자만 선택해야 합니다.");
+        }
+    }
+
+    private void validateSize(List<Integer> player) {
+        if (player.size() != PLAYER_NUMBERS_SIZE) {
+            throw new IllegalArgumentException("플레이어는 3개의 숫자만 선택할 수 있습니다.");
         }
     }
 }
