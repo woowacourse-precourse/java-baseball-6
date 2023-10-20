b071c50f8acbd736fcc1be6971d1dc19ed670e58
김승우 <tmd8633@naver.com>
2023-10-20 15:44:00 +0900

    Add: BaseRepository 인터페이스 추가

--- src/main/java/service/BaseServiceImpl.java
+++ src/main/java/service/BaseServiceImpl.java
@@ -2,6 +2,7 @@ package service;
 
 import domain.Baseball;
 import repository.BaseRepository;
+import repository.BaseRepositoryImpl;
 import repository.InputRepository;
 import repository.InputRepositoryImpl;
 
@@ -12,7 +13,7 @@ import static constant.BaseConst.*;
 public class BaseServiceImpl implements BaseService{
 
     private final InputRepository inputRepository = new InputRepositoryImpl();
-    private final BaseRepository baseRepository = new BaseRepository();
+    private final BaseRepository baseRepository = new BaseRepositoryImpl();
 
     /**
      * 게임 기능
@@ -23,25 +24,35 @@ public class BaseServiceImpl implements BaseService{
 
         boolean flag = false;
         while (!flag) {
+            // 사용자로부터 입력을 받고 검증한 값을 String으로 반환
             String myNumberStr = inputRepository.getMyNumber();
+
+            // 검증된 문자열을 List<Integer>로 반환
             List<Integer> myNumber = baseRepository.getMyNumber(myNumberStr);
 
+            // 정답과 사용자가 입력한 값을 비교한 결과를 int[] result에 반환
             int[] result = baseRepository.confirmNumber(number.getBaseNumber(), myNumber);
 
+            // 결과 출력
             baseRepository.printMessage(result);
-            flag = result[STRIKE] == COMPLETE;
+
+            // 스트라이크가 3개라면 flag = true 변경
+            flag = ( result[STRIKE] == MAX_LENGTH );
         }
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
     /**
      * 재시작 여부확인
-     * @return 선택결과가 RESTART(1) 이면 true, EXIT(2)이면 false
+     * @return 선택결과가 RESTART("1") 이면 true, EXIT("2")이면 false
      */
     @Override
     public boolean isRestart() {
+        // 사용자로부터 입력을 받고 검증한 값을 String으로 반환
         String restartSelect = inputRepository.getRestartSelect();
-        return Integer.parseInt(restartSelect) == RESTART;
+
+        // 반환된 값은 "1" 또는 "2"인 상태에서 RESTART("1") 와 일치하는지 확인
+        return restartSelect.equals(RESTART);
     }
 
 
