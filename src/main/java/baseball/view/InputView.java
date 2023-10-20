1a85d682fdc2218945fea07dedb382b472e662f4
Jiyoung Choi <12201817@inha.edu>
2023-10-20 15:58:35 +0900

    refactor(InputView) : 사용자 수 입력 함수 수정  println -> print

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -9,7 +9,7 @@ public class InputView {
     private final static String inputRestartValueMessage = "게임을 새로 시작하려면 %s, 종료하려면 %s를 입력하세요.\n";
 
     public static String inputUserNumber(){
-        System.out.println(inputUserNumberMessage);
+        System.out.print(inputUserNumberMessage);
         return Console.readLine();
     }
 
