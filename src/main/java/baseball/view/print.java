baf144aadd0999dcb6ea48f1f058f5b857b2174e
Maljaaa <Maljaaa@github.com>
2023-10-20 15:52:32 +0900

    feat: 출력 기능 구현

--- src/main/java/baseball/view/print.java
+++ src/main/java/baseball/view/print.java
@@ -1,2 +1,31 @@
-package baseball.view;public class print {
+package baseball.view;
+
+public class print {
+
+    public void printInit(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void printResult(int ball, int strike){
+        // 0볼 0스트라이크
+        if(ball != 0 && strike != 0) {
+            System.out.println(ball + "볼" + strike + "스트라이크");
+        }
+        // 0볼
+        else if(ball != 0 && strike == 0) {
+            System.out.println(ball + "볼");
+        }
+        // 0스트라이크
+        else if(ball == 0 && strike != 0) {
+            System.out.println(strike + "스트라이크");
+        }
+        // 낫싱
+        else {
+            System.out.println("낫싱");
+        }
+    }
+
+    public void printWin(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
 }
