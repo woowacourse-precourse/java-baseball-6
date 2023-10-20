c2b09f11946cf21e89b96aefe18ca3ee8b593da7
leesh <qwqw8019@naver.com>
2023-10-20 19:00:14 +0900

    feat: 게임 실행 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import baseball.game.BaseBallGame;
+import baseball.game.Game;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        Game game = new BaseBallGame();
+
+        Machine machine = new Machine(game);
+
+        User.obtainMachine(machine);
+
+        User.startGame();
+
     }
 }
