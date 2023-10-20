6eb42a76f78b4b98a7316c15900c2b4073a6e8e5
GaBaljaintheroom <junsu1222@naver.com>
2023-10-20 12:27:32 +0900

    refactor: 볼, 스트라이크 비교 오류 수정

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -2,6 +2,7 @@ package baseball;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -52,14 +53,15 @@ public class Player {
     }
 
     public long compareBallCount(List<Integer> baseballNumbers) {
-        return expectedNumbers.stream()
-                .filter(baseballNumbers::contains)
+        return IntStream.range(0, BaseballRole.MAX_BASEBALL_NUMBER_SIZE.getValue())
+                .filter(i -> !Objects.equals(expectedNumbers.get(i), baseballNumbers.get(i)))
+                .filter(i -> baseballNumbers.contains(expectedNumbers.get(i)))
                 .count();
     }
 
     public long compareStrikeCount(List<Integer> baseballNumbers) {
         return IntStream.range(0, BaseballRole.MAX_BASEBALL_NUMBER_SIZE.getValue())
-                .filter(i -> expectedNumbers.get(i).equals(baseballNumbers.get(i)))
+                .filter(i -> Objects.equals(expectedNumbers.get(i), baseballNumbers.get(i)))
                 .count();
 
     }
