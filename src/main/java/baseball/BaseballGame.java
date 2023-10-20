7dea9c355b93ab7927ddd4dc45324eb751f59cc6
scarlet0o0 <ghd7673@naver.com>
2023-10-21 01:08:10 +0900

    Refactor : BallResult 클래스 proses 메소드 수정

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -8,37 +8,31 @@ public class BaseballGame {
     public void start() {
         do{
             OutView.gameStartPrint();
-            Computer computer = new Computer();
-
-            //반복
-            proses(computer);
-
-            //게임 종료
+            proses(new Computer());
             OutView.endGamePrint();
-
         }while (confirmGameRestart());
     }
 
     //게임 진행
     private void proses(Computer computer) {
+        int strikeCount;
         do {
             OutView.numberInputPrint();
             int number = InputView.promptForThreeNumbers();
             Ball ball = new Ball(number);
             BallResult userBallResult = computer.getUserBallResult(ball);
-
-            //결과 출력
             OutView.ballScoringResultsPrint(userBallResult);
-
-            if (userBallResult.getStrike() >= 3) {
-                break;
-            }
-        }while (true); //3스트라이크 일 경우 종료
+            strikeCount = userBallResult.getStrike();
+        } while (isGameOverWithStrikeCount(strikeCount)); //3스트라이크 일 경우 종료
     }
 
     //게임 재시작 여부
     private boolean confirmGameRestart() {
         OutView.restartGamePrint();
         return InputView.promptForOneNumbers() == 1;
+    }
+
+    private boolean isGameOverWithStrikeCount(int strikeCount) {
+        return strikeCount < 3;
     }
 }
