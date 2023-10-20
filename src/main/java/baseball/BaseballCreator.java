a67326481aa87a78f6e3ea2f93dc4cfc0e774725
june-777 <wlwhswnsrl96@gmail.com>
2023-10-20 15:05:37 +0900

    refactor: 야구게임 공을 생성하는 BaseballCreator 클래스로 분리

---
+++ src/main/java/baseball/BaseballCreator.java
@@ -0,0 +1,34 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BaseballCreator {
+    private Validator validator = new Validator();
+
+    // 기능: 사용자의 공을 입력 받고 생성한다
+    public List<Integer> createPlayerBalls() {
+        String number = Console.readLine();
+        validator.validatePlayerBallInput(number);
+        return number
+                .chars()
+                .mapToObj(Character::getNumericValue)
+                .collect(Collectors.toList());
+    }
+
+    // 기능: 컴퓨터는 1에서 9까지 서로 다른 임의의 수 3개를 선택
+    public List<Integer> createComputerBalls() {
+        List<Integer> computerBalls = new ArrayList<>();
+        while (computerBalls.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerBalls.contains(randomNumber)) {
+                computerBalls.add(randomNumber);
+            }
+        }
+        return computerBalls;
+    }
+}
