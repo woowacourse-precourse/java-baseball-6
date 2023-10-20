41db26fe0839d4c69daa4c62a96c17e742c65bb8
minseo  jo <liging12@naver.com>
2023-10-20 21:25:42 +0900

    feat: GameResult 기능 추가
    기능 목록
    1. GameLogicService 로 부터 받은 GameResult 를 토대로 게임 결과를 만들어주는 기능 (?볼 ?스트라이크, 낫싱, ?볼, ?스트라이크)
    2. 게임 재 시작 여부에 대한 플레이어 입력을 컨트롤러부터 가져오면 게임 재시작여부에 대해 판별하는 기능

--- src/main/java/baseball/service/GameResultService.java
+++ src/main/java/baseball/service/GameResultService.java
@@ -1,4 +1,34 @@
 package baseball.service;
 
+import baseball.domain.GameResult;
+
 public class GameResultService {
+    private static final String BALL = "볼";
+    private static final String STRIKE = "스트라이크";
+    private static final String NOTHING = "낫싱";
+    private static final String RESTART = "1";
+    private static final String EXIT = "2";
+
+    public String showResult(GameResult gameResult) {
+        if (gameResult.isNothing()) {
+            return NOTHING;
+        }
+        if (gameResult.isAllBall()) {
+            return gameResult.getBalls() + BALL;
+        }
+        else if (gameResult.isAllStrike()) {
+            return gameResult.getStrikes() + STRIKE;
+        }
+        return gameResult.toString();
+    }
+
+    public boolean restartGame(String choice) {
+        if (choice.equals(RESTART)) {
+            return true;
+        } else if (choice.equals(EXIT)) {
+            return false;
+        }
+
+        throw new IllegalArgumentException("1 또는 2 중 하나의 유효한 입력이 필요합니다.");
+    }
 }
