eeacee126af642b88ccbdf5c5ca9994acceb7ffd
waveofmymind <sjun0913@gmail.com>
2023-10-20 21:21:06 +0900

    feat: 게임 진행 결과에 따른 출력 클래스를 구현한다.

---
+++ src/main/java/baseball/view/ResultView.java
@@ -0,0 +1,17 @@
+package baseball.view;
+
+import baseball.domain.GameResult;
+
+public class ResultView {
+
+    public static void showResult(GameResult gameResult) {
+        if (gameResult.isStrikeOut()) {
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        } else if (gameResult.hasBall()) {
+            System.out.println(gameResult.getBallCount() + "볼 " + gameResult.getStrikeCount() + "스트라이크");
+        } else {
+            System.out.println("낫싱");
+        }
+    }
+}
