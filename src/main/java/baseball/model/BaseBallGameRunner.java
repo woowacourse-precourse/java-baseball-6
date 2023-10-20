213dc44a68fedf556b3094f90afbdfad6589ea3e
hoyun06 <hoyun06@gmail.com>
2023-10-20 11:07:46 +0900

    feat: 사용자 input 받는 기능 구현

--- src/main/java/baseball/model/BaseBallGameRunner.java
+++ src/main/java/baseball/model/BaseBallGameRunner.java
@@ -1,9 +1,21 @@
 package baseball.model;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class BaseBallGameRunner {
     private Computer computer;
 
     public BaseBallGameRunner() {
         this.computer = new Computer();
+    }
+
+    public void run() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        String inputString = getPlayerInput();
+    }
+
+    private String getPlayerInput() {
+        System.out.print("숫자를 입력해주세요 : ");
+        return Console.readLine();
     }
 }
