69f69bc3e6c23fbb10063fdf17105937ca85e9c9
Seotriever <122240520+Seotriever@users.noreply.github.com>
2023-10-20 21:25:40 +0900

    21:24 메모추가
    입력값을 배열로 나타내고 선언하는게 좋을지 반대일지 다시한번 생각해볼 메모추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,8 +9,8 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-
-        List<Integer> computer = new ArrayList<>(); // 랜덤숫자3자리 생성
+        // 랜덤숫자3자리 생성 메소드  =============================================================
+        List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computer.contains(randomNumber)) {
@@ -18,24 +18,18 @@ public class Application {
             }
         }   System.out.println(computer);   //난수확인값
 
-
-        String user = Console.readLine(); // 사용자가 입력할 3자리
+        // 입력값 메소드 =======================================================================
+        String user = Console.readLine(); // 사용자가 입력할 3자리 Console.readLine()
         int[] arr = new int[user.length()];
         for( int i = 0; i < user.length(); i++ ) {
             arr[i] = user.charAt(i)- '0';
         }
         for( int j = 0; j < user.length(); j++ ) {
             }System.out.println(Arrays.toString(user.toCharArray()));  // 배열로 입력되는 확인값
+        // 배열로 나타낸값을  가독성있게 선언해줄필요가 있을듯
 
-
-
-
-//
-//
-//        int numArr[] = new int[3]; //랜덤숫자를 넣기위한 length 3인 빈 배열
-//        int inputArr[] = new int[3]; // 사용자가 입력한 숫자를 넣기위한 length 3인 빈 배열
-//        int strike = 0; //입력한 값의 strike를 저장받을 변수
-//        int ball = 0; //입력한 값의 ball을 입력받을 변수
+        // 비교 메소드
+        //for문으로 computer의 난수 3개와 user
 
 
     }
