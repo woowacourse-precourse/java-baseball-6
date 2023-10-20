37a72eb059b465bc91c70404c4f9b347e9d76072
xogus3492 <wkdxogus3492@gmail.com>
2023-10-20 14:49:05 +0900

    feat(consoleview): 게임 결과 출력 기능 구현

--- src/main/java/baseball/view/ConsoleView.java
+++ src/main/java/baseball/view/ConsoleView.java
@@ -5,6 +5,9 @@ import camp.nextstep.edu.missionutils.Console;
 public class ConsoleView {
     private static final String GAME_START_OUTPUT_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String THREE_DIGIT_NUMBER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String STRIKE_MESSAGE = "스트라이크";
+    private static final String BALL_MESSAGE = "볼 ";
+    private static final String NOTHING_MESSAGE = "낫싱";
 
     public static void printGameStart() {
         System.out.println(GAME_START_OUTPUT_MESSAGE);
@@ -13,5 +16,17 @@ public class ConsoleView {
     public static String inputThreeDigitNumber() {
         System.out.print(THREE_DIGIT_NUMBER_INPUT_MESSAGE);
         return Console.readLine();
+    }
+
+    public static void printStrikeCount(int count) {
+        System.out.println(count + STRIKE_MESSAGE);
+    }
+
+    public static void printBallCount(int count) {
+        System.out.println(count + BALL_MESSAGE);
+    }
+
+    public static void printNothing() {
+        System.out.println(NOTHING_MESSAGE);
     }
 }
