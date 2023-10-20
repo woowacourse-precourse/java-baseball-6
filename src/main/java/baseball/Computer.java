7c837580a37f7dc5b9e30a036adf10c8f5f0df0d
Jmmk <jmk525@gmail.com>
2023-10-20 15:57:03 +0900

    feat: 타겟 넘버를 저장할 컴퓨터 클래스 생성

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,14 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private static final int NUM_DIGITS = 3; // 타겟 넘버 자릿 수
+    private List<Integer> targetNumber;
+
+    public Computer() {
+        targetNumber = new ArrayList<>();
+    }
+
+}
