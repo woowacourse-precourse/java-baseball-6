70ee453cf313393b979888cd57a4db6964ef91c8
ehtjsv2 <ehtjsv2@naver.com>
2023-10-20 17:13:29 +0900

    feat: 숫자 입력받기, 예외처리 구현

--- src/main/java/baseball/NumberManager.java
+++ src/main/java/baseball/NumberManager.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class NumberManager {
+    private final static int LENGTH = 3;
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public List<Integer> createRandomNumber() {
@@ -19,6 +21,35 @@ public class NumberManager {
             }
         }
         return number;
+    }
+
+    public List<Integer> receiveNumber() throws IOException{
+        String number = br.readLine();
+        if(!isVaildNumber(number)){
+            throw new IllegalArgumentException();
+        }
+        return convertToList(number);
+    }
+
+    public boolean isVaildNumber(String number){
+        if(number.length()!=LENGTH)
+            return false;
+        if(number.contains("0"))
+            return false;
+        for(int i=0;i<3;i++){
+            if(number.charAt(i)<'1' || number.charAt(i)>'9')
+                return false;
+        }
+        return true;
+
+    }
+
+    public List<Integer> convertToList(String number){
+        List<Integer> numberList = new ArrayList<Integer>();
+        for(int i=0;i<number.length();i++){
+            numberList.add(number.charAt(0)-'0');
+        }
+        return numberList;
     }
 
     
