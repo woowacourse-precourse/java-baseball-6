bfb5006de30dc2f8024fda5e33ec04f08dc58041
inkyu-yoon <tryingmybest24h@gmail.com>
2023-10-21 08:57:52 +0900

    [feat] : 기본 게임 시작 문구 출력 및 사용자 입력 받기 기능 추가
    - BaseballGame는 Answer, Rule 객체를 의존

--- src/main/java/baseball/domain/BaseballGame.java
+++ src/main/java/baseball/domain/BaseballGame.java
@@ -1,4 +1,31 @@
 package baseball.domain;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import static baseball.constants.GameMessage.GET_USER_INPUT;
+import static baseball.constants.GameMessage.START_MESSAGE;
+
 public class BaseballGame {
+    private Answer answer;
+    private Rule rule;
+
+    public BaseballGame() {
+        rule = new Rule();
+    }
+
+    public void run() {
+        System.out.println(START_MESSAGE);
+        start();
+    }
+
+    private void start() {
+        while (true) {
+            String userInput = getUserInput();
+        }
+    }
+
+    private static String getUserInput() {
+        System.out.print(GET_USER_INPUT);
+        return Console.readLine();
+    }
 }
