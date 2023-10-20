b78a45e7a62bcf442aa29055a08a36d1135375aa
jy016011 <39334789+jy016011@users.noreply.github.com>
2023-10-20 13:41:23 +0900

    User의 행동을 정의하는 User클래스 구현 Ver1.0

--- src/main/java/baseball/gameLogic/User.java
+++ src/main/java/baseball/gameLogic/User.java
@@ -1,2 +1,14 @@
-package baseball.gameLogic;public class User {
+package baseball.gameLogic;
+
+import java.util.ArrayList;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
+
+public class User {
+    public List<Integer> numberList;
+
+    public void inputAnswer(InputValidator inputValidator){
+        numberList = inputValidator.validateUserAnswer(Console.readLine());
+    }
+
 }
