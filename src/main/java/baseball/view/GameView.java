e56bbdf31b00a40d00fdc492a033633ccd98d45f
이희수 <hslee9234@naver.com>
2023-10-21 05:47:57 +0900

    재시작 뷰 생성

--- src/main/java/baseball/view/GameView.java
+++ src/main/java/baseball/view/GameView.java
@@ -19,4 +19,9 @@ public class GameView {
     public void displayGameExitMessage() {
         System.out.println("게임 종료");
     }
+    public static String setRetryNumber(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return Console.readLine();
+    }
+
 }
