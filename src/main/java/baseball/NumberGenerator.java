66fd18ef775d85dd4b9af3a781f3c0b480cb09da
jiwoo <zbqmgldjfh@gmail.com>
2023-10-20 16:27:08 +0900

    feat(NumberGenerator): 기본 사이즈 3의 길이로 랜덤번호를 생성하는 generateDefaultSizeComputerNumber 메서드 구현

--- src/main/java/baseball/NumberGenerator.java
+++ src/main/java/baseball/NumberGenerator.java
@@ -7,6 +7,10 @@ import java.util.List;
 
 public class NumberGenerator {
 
+    private static final int START_INCLUSIVE = 1;
+    private static final int END_INCLUSIVE = 9;
+    private static final int DEFAULT_NUMBER_SIZE = 3;
+
     public static List<Integer> generateComputerNumber(int size) {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < size) {
@@ -18,7 +22,11 @@ public class NumberGenerator {
         return computer;
     }
 
+    public static List<Integer> generateDefaultSizeComputerNumber() {
+        return generateComputerNumber(DEFAULT_NUMBER_SIZE);
+    }
+
     private static int generateRandomNumber() {
-        return Randoms.pickNumberInRange(1, 9);
+        return Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
     }
 }
