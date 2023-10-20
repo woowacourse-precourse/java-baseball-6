de013e5074a25fa50b96966a921c2b8f51e847df
Jiyoung Choi <12201817@inha.edu>
2023-10-20 15:57:09 +0900

    feat(Application) : 게임 실행 테스트

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,10 @@
 package baseball;
 
+import baseball.controller.GameController;
 import baseball.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-        inputView.inputRestartValue();
+        GameController.proceedGame();
     }
 }
