2ad81cf76c58f4380477e01916b36ea237751893
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2023-10-20 18:38:48 +0900

    feat: 재시작/종료 입력값 검증 기능

--- src/main/java/baseball/GameRestartOrQuit.java
+++ src/main/java/baseball/GameRestartOrQuit.java
@@ -2,10 +2,18 @@ package baseball;
 
 public class GameRestartOrQuit {
 
+    private static final String GAME_RESTART = "1";
+    private static final String GAME_QUIT = "2";
     private String restartOrQuitNumber;
 
     public GameRestartOrQuit(String restartOrQuitNumber) {
+        validateRestartOrQuitNumber(restartOrQuitNumber);
         this.restartOrQuitNumber = restartOrQuitNumber;
     }
 
+    private void validateRestartOrQuitNumber(String number) {
+        if (!number.equals(GAME_RESTART) && !number.equals(GAME_QUIT)) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
