bea18954612198e2af28ac3950c36e9b461b31e0
mummhy0811 <mummhy0811@inu.ac.kr>
2023-10-20 16:23:15 +0900

    Feat: 컴퓨터 랜덤 수 생성

--- src/main/java/baseball/controller/BaseballGame.java
+++ src/main/java/baseball/controller/BaseballGame.java
@@ -1,6 +1,13 @@
 package baseball.controller;
 
+import baseball.model.ComputerNumber;
+
 public class BaseballGame {
+    private ComputerNumber computerNumber;
+
+    public BaseballGame(){
+        computerNumber = new ComputerNumber();
+    }
 
     public void start(){
 
---
+++ src/main/java/baseball/model/ComputerNumber.java
@@ -0,0 +1,28 @@
+package baseball.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComputerNumber {
+    private final List<Integer> computer = new ArrayList<>();
+
+    public ComputerNumber(){
+        makeRandomNumber();
+    }
+
+    public List<Integer> getComputer(){
+        return computer;
+    }
+
+    public void makeRandomNumber(){
+
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+    }
+}
