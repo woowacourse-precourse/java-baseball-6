e5a2acd34ada25dd17055e783a8ad98b198e5889
JHyun0302 <wogustkfkd11@gmail.com>
2023-10-20 11:49:56 +0900

    :art: [Refactor] result() 메서드 개선

--- src/main/java/baseball/service/PrintResult.java
+++ src/main/java/baseball/service/PrintResult.java
@@ -18,19 +18,29 @@ public class PrintResult {
 
     // 0 : 계속 진행, 1 : 재시작, 2 : 종료
     public int result(User user) {
-        if (user.getStrike() == 0 && user.getBall() == 0) {
-            System.out.println("낫싱");
-        } else if (user.getStrike() != 0 && user.getBall() != 0) {
-            System.out.println(user.getBall() + "볼 " + user.getStrike() + "스트라이크");
-        } else if (user.getStrike() == 0 && user.getBall() != 0) {
-            System.out.println(user.getBall() + "볼 ");
-        } else if (user.getStrike() != 0 && user.getBall() == 0) {
-            System.out.println(user.getStrike() + "스트라이크");
-            if (user.getStrike() == 3) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                return restart.restartOrExit();
-            }
+        int strike = user.getStrike();
+        int ball = user.getBall();
+
+        String message = generateMessage(strike, ball);
+        System.out.println(message);
+
+        if (strike == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return restart.restartOrExit();
         }
-        return 3;
+
+        return 0;
+    }
+
+    private String generateMessage(int strike, int ball) {
+        if (strike == 0 && ball == 0) {
+            return "낫싱";
+        } else if (strike != 0 && ball != 0) {
+            return ball + "볼 " + strike + "스트라이크";
+        } else if (strike == 0) {
+            return ball + "볼 ";
+        } else {
+            return strike + "스트라이크";
+        }
     }
 }
