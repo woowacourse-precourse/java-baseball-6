a9f6e069d9f192e58a49e60c873f2434c36d193f
Potato-Y <projectpotatoy@gmail.com>
2023-10-20 16:37:23 +0900

    refact : 불필요한 import 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,10 +3,6 @@ package baseball;
 import baseball.model.Computer;
 import baseball.model.User;
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Application {
     public static void main(String[] args) {
         Computer computer = new Computer(3);
