d331821f03e7fdbee31c72c59bbc77e0adf9a582
김효준 <khyojun1104@gmail.com>
2023-10-20 21:57:11 +0900

    [feat] GameController : computerNumber 를 위한 validate 재활용하기!

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,5 +1,7 @@
 package baseball.controller;
 
+import baseball.service.ComputerNumberGenerator;
+import baseball.util.Validation;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 
@@ -7,6 +9,7 @@ public class GameController {
 
     InputView inputView;
     OutputView outputView;
+    Validation validation = new Validation();
 
     public GameController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
@@ -15,6 +18,22 @@ public class GameController {
 
 
     public void playball() {
+        String nowComNumber = getComputerNumber();
+
+        String userNumber = getUserNumber();
+
 
     }
+
+    private String getComputerNumber() {
+        ComputerNumberGenerator generator = ComputerNumberGenerator.getInstance();
+        return validation.validateNumber(generator.generate());
+    }
+
+    private String getUserNumber() {
+        String inputUserNumber = inputView.readUserNumber();
+        return validation.validateNumber(inputUserNumber);
+    }
+
+
 }
