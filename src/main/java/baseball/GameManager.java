c5f0df4f69d16df65e09a0c63dd74c4b72ef492d
jiwoo <zbqmgldjfh@gmail.com>
2023-10-20 16:45:24 +0900

    feat(GameManager): GameManager의 전체 흐름 잡기

---
+++ src/main/java/baseball/GameManager.java
@@ -0,0 +1,18 @@
+package baseball;
+
+import java.util.List;
+
+public class GameManager {
+
+    public void play() {
+        List<Integer> computerNumbers = NumberGenerator.generateDefaultSizeComputerNumber();
+
+        // TODO : 사용자 입력 받기
+
+        // TODO : 입력으로 게임 진행
+
+        // TODO : 결과 출력하기
+
+        // TODO : 다음게임 실행 확인
+    }
+}
