68d81e3a90a7f4d08e4bb642386f92007322ae13
WeeYoungSeok <dndrldzk@gmail.com>
2023-10-20 17:02:11 +0900

    feat: 입력 겁증 추가 플레이어의 입력 검증을 위해 InputValidation 클래스에 기능을 추가하였고, 플레이어의 숫자를 관리하기 위해 PlayerNumbers 클래스를 추가와 해당 클래스에서 검증을 진행했습니다.

---
+++ src/main/java/baseball/domain/PlayerNumbers.java
@@ -0,0 +1,31 @@
+package baseball.domain;
+
+import baseball.validator.InputValidation;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayerNumbers {
+
+    private List<Integer> numbers;
+
+    public PlayerNumbers() {
+        String inputNumber = Console.readLine();
+        InputValidation.isNull(inputNumber);
+        InputValidation.isNumber(inputNumber);
+        InputValidation.isThreeLetter(inputNumber);
+        InputValidation.isNumberScope(inputNumber);
+        InputValidation.isDuplicateNumber(inputNumber);
+
+        this.numbers = changeInputStringToNumber(inputNumber);
+    }
+
+    private List<Integer> changeInputStringToNumber(String inputNumber) {
+        List<Integer> changeList = new ArrayList<>();
+        for (int i = 0; i < inputNumber.length(); i++) {
+            changeList.add(Character.getNumericValue(inputNumber.charAt(i)));
+        }
+        return changeList;
+    }
+}
--- src/main/java/baseball/validator/InputValidation.java
+++ src/main/java/baseball/validator/InputValidation.java
@@ -20,6 +20,35 @@ public class InputValidation {
         }
     }
 
+    public static void isThreeLetter(String playerNumber) {
+        if (playerNumber.length() != GameValue.MAX_LENGTH) {
+            throw new IllegalArgumentException(GameValue.MAX_LENGTH + ErrorMessage.LENGTH_ERROR_MESSAGE);
+        }
+    }
+
+    public static void isNumberScope(String playerNumber) {
+        for (int i = 0; i < playerNumber.length(); i++) {
+            numberScopeCheck(Character.getNumericValue(playerNumber.charAt(i)));
+        }
+    }
+
+    public static void numberScopeCheck(int number) {
+        if ((number < GameValue.MIN_NUMBER)
+                || (number > GameValue.MAX_NUMBER)) {
+            throw new IllegalArgumentException(ErrorMessage.SCOPE_NUMBER_ERROR_MESSAGE);
+        }
+    }
+
+    public static void isDuplicateNumber(String playerNumber) {
+        Set<Integer> duplicateCheckSet = new HashSet<>();
+        for (int i = 0; i < playerNumber.length(); i++) {
+            duplicateCheckSet.add(Character.getNumericValue(playerNumber.charAt(i)));
+        }
+        if (duplicateCheckSet.size() != 3) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_ERROR_MESSAGE);
+        }
+    }
+
     public static void reStartNumberCheck(String reStartPlayerNumber) {
         if (!reStartPlayerNumber.equals("1") && !reStartPlayerNumber.equals("2")) {
             throw new IllegalArgumentException(ErrorMessage.RESTART_ERROR_MESSAGE);
