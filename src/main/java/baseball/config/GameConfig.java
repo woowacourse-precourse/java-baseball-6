8ec901c38d0e11992971c4e8683e0c0f0c7f8a18
jinny-l <jinny.l.dev@gmail.com>
2023-10-20 14:53:46 +0900

    ✨feat: 숫자 야구 게임 설정 관련 변수를 상수화하여 GameConfig 클래스 생성

---
+++ src/main/java/baseball/config/GameConfig.java
@@ -0,0 +1,9 @@
+package baseball.config;
+
+public abstract class GameConfig {
+
+    public static final int BASEBALL_LENGTH = 3;
+    public static final int BASEBALL_START_NUMBER = 1;
+    public static final int BASEBALL_END_NUMBER = 9;
+
+}
