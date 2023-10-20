0d0de24fd54777a17aabe3c713ac387c61ab31ab
gusdn0108 <gusdn6671@naver.com>
2023-10-20 10:20:50 +0900

    feat(Controller) : userInput 값 생성 메서드

--- src/main/java/baseball/controller/Controller.java
+++ src/main/java/baseball/controller/Controller.java
@@ -1,21 +1,37 @@
 package baseball.controller;
 
-import baseball.domain.Computer;
+import baseball.domain.Baseball;
+import baseball.utils.Utils;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class Controller {
-    Computer computer = new Computer();
-    public void run(){
-        computer.generateRandomNumber();
+    Baseball baseball = new Baseball();
+
+    public void run() {
+        List<Integer> generateRandomNumber = baseball.generateRandomNumber();
         OutputView.printStartGame();
         OutputView.printInputNumber();
-        InputView.userInput();
+        String input = InputView.userInput();
+        List<Integer> userInput = parsedArrayUserNumber(input);
+        baseball.findStrike(generateRandomNumber,userInput);
     }
+
+    private List<Integer> parsedArrayUserNumber(String input) {
+        List<Integer> paredArrayNumbers = new ArrayList<>();
+        Utils.convertArray(input,paredArrayNumbers);
+        return paredArrayNumbers;
+    }
+
+
+
+
+
+
 
 
 
