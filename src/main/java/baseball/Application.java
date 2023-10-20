797988d9fa3e18a162a78c0f9eea804a3a9befda
Uniaut <kunwoo0927@gmail.com>
2023-10-20 23:37:11 +0900

    feat: added Game.judge

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -17,6 +17,46 @@ class Game {
         int query = Integer.parseInt(Console.readLine());
         return query;
     }
+
+    int[] judge(int query) {
+        int[] result = {0, 0};
+
+        int[] queryDigit = {
+                this.answer % 10,
+                (this.answer / 10) % 10,
+                (this.answer / 100) % 10};
+        int[] answerDigit = {
+                this.answer % 10,
+                (this.answer / 10) % 10,
+                (this.answer / 100) % 10};
+
+        // Strike judgement
+        for (int i = 0; i < 3; i++) {
+            if (queryDigit[i] == answerDigit[i]) {
+                result[0] += 1;
+                queryDigit[i] = 0;
+                answerDigit[i] = 0;
+            }
+        }
+
+        // Ball judgement
+        boolean[] queryDigitDistribution = new boolean[10];
+        boolean[] answerDigitDistribution = new boolean[10];
+        for (int i = 0; i < 10; i++) {
+            queryDigitDistribution[i] = false;
+            answerDigitDistribution[i] = false;
+        }
+        for (int i = 0; i < 3; i++) {
+            queryDigitDistribution[queryDigit[i]] = true;
+            answerDigitDistribution[answerDigit[i]] = true;
+        }
+        for (int i = 1; i < 10; i++) {
+            if (queryDigitDistribution[i] & answerDigitDistribution[i])
+                result[1]++;
+        }
+
+        return result;
+    }
 }
 
 
