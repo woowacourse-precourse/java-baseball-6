a6d7983e03d3ff4003d365f159af98e43f9c5336
StrangeMin <mim0107@nate.com>
2023-10-20 14:41:17 +0900

    feat : GameStatus enum 정의 및 초기화

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,5 +4,12 @@ public class Application {
     public static void main(String[] args) {
         // 시작
         System.out.println("숫자 야구 게임을 시작합니다.");
+        GameStatus gameStatus = GameStatus.START;
+    }
+
+    public enum GameStatus{
+        NONE,
+        START,
+        END
     }
 }
