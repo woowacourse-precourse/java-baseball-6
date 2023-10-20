db841760f07817aba9974db31ee7294177aa857f
watermelon3737 <pshrina1@naver.com>
2023-10-21 02:18:09 +0900

    [test] : 유저 inputdp ','가 들어가 있는 경우의 테스트 구현

--- src/test/java/baseball/InputValidationTest.java
+++ src/test/java/baseball/InputValidationTest.java
@@ -3,8 +3,7 @@ package baseball;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class InputValidationTest {
@@ -20,5 +19,18 @@ class InputValidationTest {
         assertThatThrownBy(() -> inputValidation.validateUserInput(containSpaceUserInput))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("공백 없이 오직 숫자만 입력해주시기 바랍니다.");
+    }
+
+    @DisplayName("유저 input에 ','가 들어가 있는 경우의 테스트")
+    @Test
+    void containCommaUserInputTest() {
+        //given
+        InputValidation inputValidation = new InputValidation();
+        //when
+        final String containCommaUserInput = "2,87";
+        //then
+        assertThatThrownBy(() -> inputValidation.validateUserInput(containCommaUserInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("콤마 없이 오직 숫자만 입력해주시기 바랍니다.");
     }
 }
