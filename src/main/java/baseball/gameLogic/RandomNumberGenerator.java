51170b28b08b3d157e3ae078e24f176403d613f6
jy016011 <39334789+jy016011@users.noreply.github.com>
2023-10-20 13:45:49 +0900

    상수를 Constatns클래스의 상수로 변경

--- src/main/java/baseball/gameLogic/RandomNumberGenerator.java
+++ src/main/java/baseball/gameLogic/RandomNumberGenerator.java
@@ -5,11 +5,13 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
+import static baseball.models.Constants.*;
+
 public class RandomNumberGenerator {
     public List<Integer> generateRandomNumber(){
         List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3){
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+        while (computer.size() < ANSWER_LENGTH){
+            int randomNumber = Randoms.pickNumberInRange(USER_ANSWER_LOWER_LIMIT, USER_ANSWER_UPPER_LIMIT);
             if (!computer.contains(randomNumber)){
                 computer.add(randomNumber);
             }
