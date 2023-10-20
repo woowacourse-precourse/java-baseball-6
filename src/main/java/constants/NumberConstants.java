00ae20e12ce655c7fda354e55c94e06c47d87056
1000zoo <cjswldn99@gmail.com>
2023-10-20 17:59:39 +0900

    feat(constants): 숫자 및 문자열 상수 클래스 생성
    - 매직넘버를 제거하기 위한 상수 클래스

---
+++ src/main/java/constants/NumberConstants.java
@@ -0,0 +1,9 @@
+package constants;
+
+public class NumberConstants {
+    public static final int ZERO = 0;
+    public static final int MAX_STRIKE = 3;
+    public static final int MAX_BALL_SIZE = 3;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 9;
+}
---
+++ src/main/java/constants/StringConstants.java
@@ -0,0 +1,7 @@
+package constants;
+
+public class StringConstants {
+    public static final String BLANK = " ";
+    public static final String CONTINUE = "1";
+    public static final String FINISH = "2";
+}
