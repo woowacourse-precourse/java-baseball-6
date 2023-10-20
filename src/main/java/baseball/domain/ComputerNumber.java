23e796f36937375e3754d0c233d827a0f12dbe5a
Hugh <lee2317884@gmail.com>
2023-10-20 15:19:26 +0900

    feat: 컴퓨터 랜덤 숫자 중복 값이 있는지 확인

--- src/main/java/baseball/domain/ComputerNumber.java
+++ src/main/java/baseball/domain/ComputerNumber.java
@@ -1,17 +1,31 @@
 package baseball.domain;
 
 import baseball.constant.Constant;
+import baseball.constant.errorMessage.UserNumberError;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class ComputerNumber {
 
+    private List<Integer> createRandomNumber() {
+        List<Integer> randomNumbers = pickRandomNumber();
+        if (validateRandomNumberDuplicate(randomNumbers)) {
+            return randomNumbers;
+        }
+        return createRandomNumber();
+    }
+
     private static List<Integer> pickRandomNumber() {
         return Arrays.stream(new List[Constant.NUMBER_LENGTH_LIMIT])
                 .map(num -> Randoms.pickNumberInRange(Constant.START_NUMBER, Constant.END_NUMBER))
                 .collect(Collectors.toList());
+    }
+
+    private boolean validateRandomNumberDuplicate(List<Integer> randomNumbs) {
+        return randomNumbs.stream().distinct().count() == Constant.NUMBER_LENGTH_LIMIT;
     }
 
 }
