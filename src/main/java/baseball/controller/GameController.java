f92852c6eb5d5df66a2d0ee4c0b2e73570a8eca9
mizuirohoshi7 <mizuirohoshi7@gmail.com>
2023-10-20 17:39:55 +0900

    feat(GameController): 게임 시작

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,9 +1,13 @@
 package baseball.controller;
 
 import baseball.service.GameService;
+import baseball.view.InputView;
+import baseball.view.OutputView;
 
 public class GameController {
     private static GameController gameController;
+
+    private static final String RESTART = "1";
 
     private final GameService gameService;
 
@@ -16,5 +20,18 @@ public class GameController {
 
     private GameController() {
         gameService = GameService.getInstance();
+    }
+
+    public void start() {
+        OutputView.start();
+        do {
+            play();
+        } while (shouldRestart());
+    }
+
+    private void play() {}
+
+    private boolean shouldRestart() {
+        return InputView.restart().equals(RESTART);
     }
 }
