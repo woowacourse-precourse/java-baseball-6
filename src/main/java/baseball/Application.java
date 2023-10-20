60b0aa94daaf7930b7395304e4c1c1e619f9252d
Jonghyeok Lee <endb1228@gmail.com>
2023-10-20 16:09:45 +0900

    게임 진행에 관한 로직을 GameManager에 위임

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,29 +1,16 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.*;
-
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
+import static baseball.GameManager.getInstance;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
         while (true) {
-            List<String> numberAnswer = generateRandomNumber();
+            getInstance().startGame();
+            break;
         }
-    }
-
-    public static List<String> generateRandomNumber() {
-        return Randoms.pickUniqueNumbersInRange(1, 9, 3)
-                .stream()
-                .map(String::valueOf)
-                .collect(Collectors.toList());
-    }
-
-    public static void startGame() {
-        System.out.print("숫자를 입력해주세요 : ");
-        String number = Console.readLine();
     }
 }
--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -1,4 +1,28 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class GameManager {
+    private static GameManager gameManager;
+    public static GameManager getInstance() {
+        if (gameManager == null) gameManager = new GameManager();
+        return gameManager;
+    }
+
+    public void startGame() {
+        List<String> answerNumber = generateRandomNumber();
+        while (true) {
+            break;
+        }
+    }
+
+    private List<String> generateRandomNumber() {
+        return Randoms.pickUniqueNumbersInRange(1, 9, 3)
+                .stream()
+                .map(String::valueOf)
+                .collect(Collectors.toList());
+    }
 }
