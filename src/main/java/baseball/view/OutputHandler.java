4dd37eefcc6203eb781471156c8fd43e85c0e133
정성욱 <rkfka7849@gmail.com>
2023-10-20 20:26:29 +0900

    feat(OutputHandler): 볼, 스트라이크 개수를 출력하는 기능 구현

--- src/main/java/baseball/view/OutputHandler.java
+++ src/main/java/baseball/view/OutputHandler.java
@@ -1,5 +1,7 @@
 package baseball.view;
 
+import baseball.domain.Result;
+
 public class OutputHandler {
     public static void theGameStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -7,5 +9,16 @@ public class OutputHandler {
 
     public static void printNothing() {
         System.out.println("낫싱");
+    }
+
+    public static void printStrikesAndBalls(Result forPrint) {
+        String stringForPrint = "";
+        if (forPrint.numberOfBalls > 0) {
+            stringForPrint += forPrint.numberOfBalls + "볼 ";
+        }
+        if (forPrint.numberOfStrikes > 0) {
+            stringForPrint += forPrint.numberOfStrikes + "스트라이크";
+        }
+        System.out.println(stringForPrint);
     }
 }
