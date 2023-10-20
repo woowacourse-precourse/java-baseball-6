326a31bf505dfea8db1e0b91fba5e5750a7337af
june-777 <wlwhswnsrl96@gmail.com>
2023-10-20 15:02:51 +0900

    refactor: 게임결과를 판단하는 Judgement 클래스로 분리

---
+++ src/main/java/baseball/Judgment.java
@@ -0,0 +1,58 @@
+package baseball;
+
+import java.util.List;
+
+public class Judgment {
+    // 기능: 힌트 얻기
+    public String getHint(List<Integer> computerBalls, List<Integer> playerBalls) {
+        if (isNothing(computerBalls, playerBalls)) {
+            return "낫싱";
+        }
+        int strikeCount = getStrikeCount(computerBalls, playerBalls);
+        int ballCount = getBallCount(computerBalls, playerBalls);
+        return getResult(ballCount, strikeCount);
+    }
+
+    // 기능: 같은 수가 전혀 없으면 낫싱
+    private boolean isNothing(List<Integer> computerBalls, List<Integer> playerBalls) {
+        return computerBalls.stream()
+                .noneMatch(playerBalls::contains);
+    }
+
+    // 기능: 같은 수가 같은 자리에 있는 스트라이크의 개수 세기
+    private int getStrikeCount(List<Integer> computerBalls, List<Integer> playerBalls) {
+        int strikeCount = 0;
+        for (int ballPosition = 0; ballPosition < computerBalls.size(); ballPosition++) {
+            int playerBall = playerBalls.get(ballPosition);
+            int computerBall = computerBalls.get(ballPosition);
+            if (computerBalls.contains(playerBall) && playerBall == computerBall) {
+                strikeCount++;
+            }
+        }
+        return strikeCount;
+    }
+
+    // 기능: 같은 수가 다른 자리에 있는 볼의 개수 세기
+    private int getBallCount(List<Integer> computerBalls, List<Integer> playerBalls) {
+        int ballCount = 0;
+        for (int ballPosition = 0; ballPosition < computerBalls.size(); ballPosition++) {
+            int playerBall = playerBalls.get(ballPosition);
+            int computerBall = computerBalls.get(ballPosition);
+            if (computerBalls.contains(playerBall) && playerBall != computerBall) {
+                ballCount++;
+            }
+        }
+        return ballCount;
+    }
+
+    private String getResult(int ballCount, int strikeCount) {
+        StringBuilder result = new StringBuilder();
+        if (ballCount > 0) {
+            result.append(String.format("%d볼 ", ballCount));
+        }
+        if (strikeCount > 0) {
+            result.append(String.format("%d스트라이크", strikeCount));
+        }
+        return result.toString().trim();
+    }
+}
