4f9c1e33cd40fdf8c19f2394398fcdb36c1d17f1
kyeoungchan <wkc1002@naver.com>
2023-10-20 17:34:38 +0900

    feat: Computer(상대방 역할) 객체 생성

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,2 +1,25 @@
-package baseball.domain;public class Computer {
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static baseball.domain.Const.FIXED_THREE_CIPHERS;
+
+public class Computer {
+    private final List<Integer> threeNumbers = new ArrayList<>();
+
+    public void generateThreeNumbers() {
+        while (threeNumbers.size() < FIXED_THREE_CIPHERS) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!threeNumbers.contains(randomNumber)) {
+                threeNumbers.add(randomNumber);
+            }
+        }
+    }
+
+    public List<Integer> getThreeNumbers() {
+        return threeNumbers;
+    }
 }
