2d273f9daf4be0f44c217ce76cc19f57b4dd3ddf
sunghun <nokdonwithkj211@gmail.com>
2023-10-20 10:18:56 +0900

    feat(IOtest, InputViewTest): IOTest 구현 및 InputView 단, 복수 인자 전달 테스트 코드 작성

--- src/test/java/baseball/IOTest.java
+++ src/test/java/baseball/IOTest.java
@@ -1,5 +1,58 @@
 package baseball;
 
+import baseball.view.InputView;
+import camp.nextstep.edu.missionutils.Console;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class IOTest {
     // Input, Output 확인하기
+    private PrintStream standardOut;
+    private OutputStream captor;
+    private InputStream userInputStream;
+
+    @BeforeEach
+    void init() {
+        standardOut = System.out;
+        captor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(captor));
+    }
+
+    @AfterEach
+    void printOutput() {
+        System.setOut(standardOut);
+        System.out.println(output());
+    }
+
+    protected String output() {
+        return captor.toString();
+    }
+
+    protected static InputStream generateMultipleInputsStream(String input, String result) {
+        List<InputStream> inputStreams = Arrays.asList(
+                generateUserInputStream(input),
+                generateUserInputStream(result));
+        return new SequenceInputStream(Collections.enumeration(inputStreams));
+    }
+
+    protected static InputStream generateUserInputStream(String userInput) {
+        return new ByteArrayInputStream(userInput.getBytes());
+    }
+
+    protected static void setInUserInput(InputStream userInputStream) {
+        System.setIn(userInputStream);
+    }
+
+    protected static String generateMultipleInputString(String... values) {
+        return String.join("\n", values);
+    }
+
 }
---
+++ src/test/java/baseball/InputViewTest.java
@@ -0,0 +1,64 @@
+package baseball;
+
+import baseball.view.InputView;
+import camp.nextstep.edu.missionutils.Console;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.InputStream;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class InputViewTest extends IOTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"checking my IoTest"})
+    void generateMultiInputString(String input) {
+        String[] inputArray = input.split(" ");
+        String sentence = generateMultipleInputString(inputArray);
+
+        setInUserInput(generateUserInputStream(sentence));
+        String result = Console.readLine();
+        String resultB = Console.readLine();
+        String resultC = Console.readLine();
+
+        System.out.println("result = " + result);
+        System.out.println("resultB = " + resultB);
+        System.out.println("resultC = " + resultC);
+    }
+
+    @ParameterizedTest(name = "user :{0} result : {1}")
+    @CsvSource(value = {"checking:checking"}, delimiterString = ":")
+    void InputView_newPlayer_Test(String userInput, String result) {
+        System.out.println(userInput);
+        System.out.println(result);
+
+        setInUserInput(generateUserInputStream(userInput));
+        String playerInput = InputView.getPlayerInput();
+
+        Assertions.assertThat(playerInput).isEqualTo(result);
+    }
+
+    private static Stream<Arguments> generateInputArguments() {
+        return Stream.of(
+                Arguments.of("checking\n", "checking"),
+                Arguments.of("My\n", "My"),
+                Arguments.of("IOTest\n", "IOTest")
+        );
+    }
+
+    @ParameterizedTest(name = "input : {0} result : {1}")
+    @MethodSource("generateInputArguments")
+    void userMultipleUserInputs(String input, String result) {
+        InputStream inputStream = generateUserInputStream(input);
+        setInUserInput(inputStream);
+        String sentence = new Scanner(System.in).nextLine();
+
+        Assertions.assertThat(sentence).isEqualTo(result);
+    }
+
+}
