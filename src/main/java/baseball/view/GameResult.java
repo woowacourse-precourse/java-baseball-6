fe8938238aebee0bc2f2caef444ab74a96e194ca
kongji <kongji4092@gmail.com>
2023-10-20 23:31:42 +0900

    feat: OutputView 결과 반환 기능

---
+++ src/main/java/baseball/view/GameResult.java
@@ -0,0 +1,21 @@
+package baseball.view;
+
+public class GameResult {
+    private final int strike;
+    private final int ball;
+
+
+    public GameResult(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+
+}
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,6 +3,10 @@ package baseball.view;
 public class OutputView {
     private static final String START_GAME_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String GET_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String NOTING_MESSAGE = "낫싱";
+    private static final String STRIKE_MESSAGE = "%d스트라이크";
+    private static final String BALL_MESSAGE = "%d볼";
+    private static final String BALL_STRIKE_MESSAGE = "%d볼 %d스트라이크";
 
 
     public static void printStartGame() {
@@ -13,4 +17,20 @@ public class OutputView {
         System.out.println(GET_NUMBER_MESSAGE);
     }
 
+    public static void printNoting(GameResult gameResult){
+        System.out.println(NOTING_MESSAGE);
+    }
+
+    public static void printStrike(GameResult gameResult){
+        System.out.printf(STRIKE_MESSAGE, gameResult.getStrike());
+    }
+
+    public static void printBall(GameResult gameResult){
+        System.out.printf(BALL_MESSAGE, gameResult.getBall());
+
+    }
+
+    public static void printStrikeAndBall(GameResult gameResult){
+        System.out.printf(BALL_STRIKE_MESSAGE, gameResult.getBall(), gameResult.getStrike());
+    }
 }
