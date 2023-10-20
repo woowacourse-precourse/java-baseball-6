840663241d92b1b69bf0ffdb901b35ce7434bfc7
JH-TT <leejongho9803@gmail.com>
2023-10-21 02:39:48 +0900

    feat: 야구숫자객체 생성
    - 정적 팩토리 메서드 사용
        - 컴퓨터에게 주는 숫자인지, 플레이어의 숫자인지 의도를 파악할 수 있다.
    - 주어진 숫자길이가 맞는지 판단하는 기능
    - 겹치는 숫자가 있는지 판단하는 기능
    - 숫자로 이루어져있는지 판단하는 기능
    - 해당 문자가 1 ~ 9 인 숫자인지 판단하는 기능
    - 볼의 개수를 세는 기능
        - 볼인지 확인하는 기능
    - 스트라이크의 개수를 세는 기능
        - 스트라이크인지 확인하는 기능

---
+++ src/main/java/baseball/model/BaseballNumbers.java
@@ -0,0 +1,92 @@
+package baseball.model;
+
+import baseball.controller.RandomNumberUtility;
+
+public class BaseballNumbers {
+    private static final String IS_NOT_NUMBER = "숫자로 이루어져 있어야 합니다.";
+    private static final String SIZE_EXCEPTION = "3개의 숫자로 이루어져야 합니다";
+    private static final String IS_NOT_DISTINCT = "서로다른 숫자로 이루어져야 합니다.";
+    private static final char ONE = '1';
+    private static final char NINE = '9';
+    private final String numbers;
+
+    public BaseballNumbers(String numbers) {
+        correctSize(numbers);
+        isNumber(numbers);
+        isDistinct(numbers);
+        this.numbers = numbers;
+    }
+    public static BaseballNumbers computerNumbers() {
+        return new BaseballNumbers(RandomNumberUtility.generateRandomNumbers());
+    }
+
+    public static BaseballNumbers playerNumbers(String numbers) {
+        return new BaseballNumbers(numbers);
+    }
+
+    private void isDistinct(String numbers) {
+        int[] check = new int[10];
+        for (char c : numbers.toCharArray()) {
+            if (check[c-ONE] == 1) {
+                throw new IllegalArgumentException(IS_NOT_DISTINCT);
+            }
+            check[c-ONE]++;
+        }
+    }
+
+    private void correctSize(String num) {
+        if (num.length() != 3) {
+            throw new IllegalArgumentException(SIZE_EXCEPTION);
+        }
+    }
+    private void isNumber(String numbers) {
+        for (int i = 0; i < numbers.length(); i++) {
+            char num = numbers.charAt(i);
+            isBetween1And9(num);
+        }
+    }
+
+    private void isBetween1And9(char num) {
+        if (num < ONE || NINE < num) {
+            throw new IllegalArgumentException(IS_NOT_NUMBER);
+        }
+    }
+
+    public int countBall(BaseballNumbers other) {
+        int cnt = 0;
+        for (int i = 0; i < numbers.length(); i++) {
+            if (other.checkBall(numbers, i)) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    public int countStrike(BaseballNumbers other) {
+        int cnt = 0;
+        for (int i = 0; i < numbers.length(); i++) {
+            if (other.checkStrike(numbers, i)) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    public boolean checkBall(String com, int idx) {
+        char p = numbers.charAt(idx);
+        char c = com.charAt(idx);
+        if (c != p && com.contains(String.valueOf(p))) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkStrike(String com, int idx) {
+        char p = numbers.charAt(idx);
+        char c = com.charAt(idx);
+        if (c == p) {
+            return true;
+        }
+        return false;
+    }
+}
