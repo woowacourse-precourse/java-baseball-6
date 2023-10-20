301a41cf7b31bc93341a5fab63bfc71ee87666c3
seokmyungham <hellotjraud@gmail.com>
2023-10-21 01:20:35 +0900

    feat(BaseballNumber): BaseballNumber 클래스 생성

---
+++ src/main/java/baseball/domain/BaseballNumber.java
@@ -0,0 +1,9 @@
+package baseball.domain;
+
+public class BaseballNumber {
+    private int ballNumber;
+
+    public BaseballNumber(int baseballNumber) {
+        this.ballNumber = baseballNumber;
+    }
+}
