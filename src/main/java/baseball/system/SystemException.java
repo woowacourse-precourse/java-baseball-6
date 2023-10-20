943ba318dd27f0164822d9e47a7787f2bf0f95d3
정성욱 <rkfka7849@gmail.com>
2023-10-20 19:16:30 +0900

    feat(InputVerifier): 중복값 예외처리 기능 구현

--- src/main/java/baseball/system/SystemException.java
+++ src/main/java/baseball/system/SystemException.java
@@ -3,4 +3,5 @@ package baseball.system;
 public class SystemException {
     static public final String EXCEPTION_NUMERIC = "숫자가 아닌 값이 입력되었습니다.";
     static public final String EXCEPTION_RANGE = "범위(123 ~ 987)를 벗어난 수가 입력되었습니다.";
+    static public final String EXCEPTION_DISTINCT = "중복된 숫자가 입력되었습니다.";
 }
--- src/main/java/baseball/verifier/InputVerifier.java
+++ src/main/java/baseball/verifier/InputVerifier.java
@@ -1,11 +1,13 @@
 package baseball.verifier;
 
+import baseball.system.SystemConstant;
 import baseball.system.SystemException;
 
 public class InputVerifier {
     public void checkBallNumber(String isBallNumber) throws IllegalArgumentException {
         checkNumeric(isBallNumber);
         checkRange(isBallNumber);
+        checkDistinct(isBallNumber);
     }
 
     private void checkNumeric(String isNumeric) {
@@ -19,6 +21,14 @@ public class InputVerifier {
     private void checkRange(String inRange) {
         if (Integer.parseInt(inRange) < 123 || 987 < Integer.parseInt(inRange)) {
             throw new IllegalArgumentException(SystemException.EXCEPTION_RANGE);
+        }
+    }
+
+    private void checkDistinct(String isDistinct) {
+        if (isDistinct.chars()
+                .distinct()
+                .count() != SystemConstant.MAXIMUM_DIGIT) {
+            throw new IllegalArgumentException(SystemException.EXCEPTION_DISTINCT);
         }
     }
 }
