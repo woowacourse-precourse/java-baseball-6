de1d85bd41e7841521cec64c803bf449b504cac8
hojang <109591135+26solitude@users.noreply.github.com>
2023-10-20 22:04:28 +0900

    게임 시작 문구 출력 및 컴퓨터에게 랜덤 숫자 생성하기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,21 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");     // 프로그램 시작
+
+        List<Integer> computer = new ArrayList<>();     // 컴퓨터에게 랜덤 숫자 생성하기
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        System.out.println(computer);   // test
     }
 }
