5769f6e1e9a6c50866b96252d3b29261973f8ea7
Juhyorim <joohr1234@gmail.com>
2023-10-20 16:58:07 +0900

    feat: 컴퓨터의 랜덤한 세 자리 수 만들기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,16 +1,35 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
+        String computerNum = makeRandomNumber();
 
         while (true) {
             System.out.print("숫자를 입력해주세요 : ");
             String input = Console.readLine().strip();
             checkInput(input);
+
+            System.out.println(computerNum);
         }
+    }
+
+    private static String makeRandomNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        return "" + computer.get(0) + computer.get(1) + computer.get(2);
     }
 
     private static void checkInput(String input) {
