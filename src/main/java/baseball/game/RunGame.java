88008f4623285d79630e0394db4dc92de4e4db13
ahma0 <dsd932@naver.com>
2023-10-20 17:57:07 +0900

    fix: 3스트라이크를 출력하지 않는 문제 해결

--- src/main/java/baseball/game/RunGame.java
+++ src/main/java/baseball/game/RunGame.java
@@ -67,6 +67,7 @@ public class RunGame {
 
     private static boolean printResult(int ball, int strike) {
         if(strike == 3) {
+            System.out.println(strike + "스트라이크");
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             return true;
         }
