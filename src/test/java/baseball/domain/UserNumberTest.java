bea2a3b35ff076d31d081d4238e7f2f3ed2140e2
Hugh <lee2317884@gmail.com>
2023-10-20 15:36:32 +0900

    style: 사용자 3자리 숫자 생성 테스트 import CodeStyle 변경

--- src/test/java/baseball/domain/UserNumberTest.java
+++ src/test/java/baseball/domain/UserNumberTest.java
@@ -1,6 +1,7 @@
 package baseball.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
