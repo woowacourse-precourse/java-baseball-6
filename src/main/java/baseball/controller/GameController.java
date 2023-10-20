2acb4ecec4d8d7bd4f0f89632b6a46d04937ab60
shin5774 <babo5774@naver.com>
2023-10-20 21:20:03 +0900

    Feat: "사용자의 재시작 입력값의 예외처리 기능 구현"
    - 사용자의 재시작 입력값의 길이와 1 또는 2 인지 확인하는 기능.
    - 적합하지 않을시 IllegalArgumentException 발생.

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -3,12 +3,28 @@ package baseball.controller;
 import baseball.domain.Numbers;
 
 public class GameController {
-
     public GameController(){
-
     }
 
     public Numbers convertToNumbers(String values){
         return new Numbers(values);
     }
+
+    public void validateOption(String input){
+        validateSize(input);
+        validateType(input);
+    }
+
+    private void validateSize(String input){
+        if(input.length()!=1){
+            throw new IllegalArgumentException("값이 1자리여야 합니다.");
+        }
+    }
+
+    private void validateType(String input){
+        if (!input.matches("[1-2]+")){
+            throw new IllegalArgumentException("값은 1 또는 2이어야 합니다.");
+        }
+    }
+
 }
