26f64d957862755ca7abb44917b5b407b4319fcf
nhlee98 <e5slnh9876@gmail.com>
2023-10-20 13:56:53 +0900

    refactor: GameNumbers 생성자의 파리미터 타입을 String으로 변경
    만약 요구사항이 숫자에 0을 허용하고 "012"와 같은 입력을 허용하는 것으로 변경되면, 문자열을 숫자로 변환할 때 앞의 0이
    잘리게 된다. 따라서 GameNumbers에서 String으로 게임 숫자를 넘겨받고 검증하도록 변경한다.

--- src/main/java/baseball/domain/GameNumbers.java
+++ src/main/java/baseball/domain/GameNumbers.java
@@ -7,23 +7,23 @@ public class GameNumbers {
 
     private List<GameNumber> gameNumbers;
 
-    public GameNumbers(int gameNumbers) {
+    public GameNumbers(String gameNumbers) {
         validate(gameNumbers);
     }
 
-    private void validate(int gameNumbers) {
+    private void validate(String gameNumbers) {
         validateIsNegative(gameNumbers);
         validateLength(gameNumbers);
     }
 
-    private void validateIsNegative(int gameNumbers) {
-        if (gameNumbers < 0) {
+    private void validateIsNegative(String gameNumbers) {
+        if (Integer.parseInt(gameNumbers) < 0) {
             throw new IllegalArgumentException("Error : 게임 숫자는 음수일 수 없습니다.");
         }
     }
 
-    private void validateLength(int gameNumbers) {
-        if (Integer.toString(gameNumbers).length() != LENGTH) {
+    private void validateLength(String gameNumbers) {
+        if (gameNumbers.length() != LENGTH) {
             throw new IllegalArgumentException(String.format("Error : 게임 숫자는 %d 자리 수이어야 합니다.", LENGTH));
         }
     }
