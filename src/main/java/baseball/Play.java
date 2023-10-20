4ce1f5ab5e86c768e724a287628e370ab8611e79
aristo <dkfud2121@naver.com>
2023-10-20 22:35:59 +0900

    refactor: 게임 시작 문구 추가

--- src/main/java/baseball/Play.java
+++ src/main/java/baseball/Play.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Play {
     public Balls start_game() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
         Balls goal = new Balls();
         while (goal.size < goal.limit_size) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
