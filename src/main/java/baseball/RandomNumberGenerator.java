1e8cb97eb3e7b9d5dafed0adfb67e4afcd19ebb7
tlarbals824 <simkm0824@naver.com>
2023-10-20 14:49:06 +0900

    feat : 랜덤 숫자 생성 인터페이스 추가

---
+++ src/main/java/baseball/RandomNumberGenerator.java
@@ -0,0 +1,9 @@
+package baseball;
+
+public interface RandomNumberGenerator<T,R> {
+    R generate(Integer size);
+
+    default Boolean match(T input, R generatedNumber){
+        return false;
+    }
+}
