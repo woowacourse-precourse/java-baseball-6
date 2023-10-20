dbf66f45bb0d32ccc9d380f8e833b07e59a9a7c7
sami355-24 <ddung355@gmail.com>
2023-10-20 12:31:13 +0900

    fix : 출력 형식을 문제의 요구에 맞게 수정, 정상 종료를 위한 코드 추가
    게임 종료시 플래그를 통해서 종료가능하게끔 변경

--- src/main/java/baseball/ProcessGame/BaseBallGameImpl.java
+++ src/main/java/baseball/ProcessGame/BaseBallGameImpl.java
@@ -9,6 +9,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
+import static baseball.EndGame.EndProcessImpl.isEnd;
+
 public class BaseBallGameImpl implements BaseBallGame {
 
     private static InputAnswer inputAnswer = new InputAnswerImpl();
@@ -68,12 +70,16 @@ public class BaseBallGameImpl implements BaseBallGame {
         if (strike == 0 && ball == 0)
             System.out.println("낫싱");
         else if (strike == 3) {
+            System.out.println("3스트라이크");
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             endProcess.userChoice();
         } else{
             if (ball > 0) System.out.print(ball + "볼" + " ");
-            if (strike > 0) System.out.println(strike + "스트라이크");
+            if (strike > 0) System.out.print(strike + "스트라이크");
+            System.out.println();
         }
-        playGame();
+        if (!isEnd)
+            playGame();
+
     }
 }
