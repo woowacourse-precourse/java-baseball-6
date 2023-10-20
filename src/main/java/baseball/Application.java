b77600a64e1c4a61402b693e0175545e32b9f589
honi20 <1594cyh@gmail.com>
2023-10-20 16:58:23 +0900

    [feat] 사용자 3개의 숫자 입력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,7 @@
 package baseball;
 
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -11,21 +12,39 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
         while (true) {
-            List<Integer> computerNumber = selectComputerNumber();
-//            System.out.println(computerNumber);
+            List<Integer> computerNumbers = selectComputerNumbers();
+            List<Integer> userNumbers = inputUserNumbers();
+            System.out.println(computerNumbers);
+            System.out.println(userNumbers);
 
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            break;
         }
     }
 
-    private static List<Integer> selectComputerNumber() {
-        List<Integer> computerNumber = new ArrayList<>();
-        while (computerNumber.size() < 3) {
+    private static List<Integer> selectComputerNumbers() {
+        List<Integer> computerNumbers = new ArrayList<>();
+
+        while (computerNumbers.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computerNumber.contains(randomNumber)) {
-                computerNumber.add(randomNumber);
+            if (!computerNumbers.contains(randomNumber)) {
+                computerNumbers.add(randomNumber);
             }
         }
-        return computerNumber;
+        return computerNumbers;
+    }
+
+    private static List<Integer> inputUserNumbers() {
+        List<Integer> userNumbers = new ArrayList<>();
+        String userInput;
+
+        System.out.print("숫자를 입력해주세요 : ");
+        userInput = Console.readLine();
+
+        for (int i = 0; i < userInput.length(); i++) {
+            int number = userInput.charAt(i) - '0';
+            userNumbers.add(number);
+        }
+        return userNumbers;
     }
 }
