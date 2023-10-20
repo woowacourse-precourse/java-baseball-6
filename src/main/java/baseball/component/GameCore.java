9c6ec9013b09b962ae953a613d7e81530cfcb318
Identityless <rlandwnd555@naver.com>
2023-10-20 23:38:45 +0900

    feat : 게임 수행 로직 및 볼/스트라이크 문자열 포메팅 메서드 구현

--- src/main/java/baseball/component/GameCore.java
+++ src/main/java/baseball/component/GameCore.java
@@ -15,8 +15,33 @@ public class GameCore {
         this.validator = validator;
     }
 
+    private void doGame() {
+        boolean flag = true;
+        while (flag) {
+            validator.validate(inputter.input());
+            if (validator.getStrike() == GeneralNumbers.NUMBER_SIZE.getCode()) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                flag = false;
+            } else if (validator.getNoting()) {
+                System.out.println("낫싱");
+            } else {
+                System.out.println(getFormatedString(validator.getStrike(), validator.getBall()));
+            }
+        }
+    }
+
+    private String getFormatedString(int strike, int ball) {
+        StringBuilder sb = new StringBuilder();
+        if (ball > 0) {
+            sb.append(ball).append("볼 ");
+        }
+        if (strike > 0) {
+            sb.append(strike).append("스트라이크");
+        }
+        return sb.toString();
+    }
+
     private void initGame() {
         validator.setTarget(initializer.initGame());
     }
-
 }
