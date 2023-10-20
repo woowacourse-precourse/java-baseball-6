84f0543e31d42e3ff171c5d14199911f4166b842
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2023-10-20 13:12:06 +0900

    UserInput checkDuplicationNumber 추가

--- src/main/java/baseball/User/Input/UserInput.java
+++ src/main/java/baseball/User/Input/UserInput.java
@@ -2,6 +2,9 @@ package baseball.User.Input;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class UserInput {
     public String getGamingInput() {
         String userInput;
@@ -32,6 +35,7 @@ public class UserInput {
     private void validateGetGamingInput(String input) {
         checkThreeNumber(input);
         checkOnlyCompositionNumber(input);
+        checkDuplicationNumber(input);
     }
 
     private void validateGetEndGameInput(String input) {
@@ -49,6 +53,17 @@ public class UserInput {
             if (c < '1' || c > '9') {
                 throw new IllegalArgumentException("입력된 값은 1~9 사이의 숫자만 가능합니다.");
             }
+        }
+    }
+
+    private void checkDuplicationNumber(String input){
+        Set<Character> uniqueNumbers = new HashSet<>();
+
+        for (char c : input.toCharArray()) {
+            if(uniqueNumbers.contains(c)){
+                throw new IllegalArgumentException("입력된 숫자에 중복된 값이 있습니다.");
+            }
+            uniqueNumbers.add(c);
         }
     }
 
