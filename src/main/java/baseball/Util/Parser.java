e0739fb56364f566f7f38a30a6e452e1a293d9fd
Firedrago95 <yonghwa95@naver.com>
2023-10-20 14:22:28 +0900

    feat 입력된 문자열을 List 로 변환하는 기능 구현

---
+++ src/main/java/baseball/Util/Parser.java
@@ -0,0 +1,17 @@
+package baseball.Util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parser {
+
+    public List<Integer> convertStringToArrayList(String input) {
+        List<Integer> convertedInput = new ArrayList<Integer>();
+        String[] inputElements = input.split("");
+        for (String inputElement : inputElements) {
+            int number = Integer.parseInt(inputElement);
+            convertedInput.add(number);
+        }
+        return convertedInput;
+    }
+}
