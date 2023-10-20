8dc68e8b1fc2092bec24a26a41aa185cf6a3f04d
chs98412 <chs98412@gmail.com>
2023-10-20 16:51:10 +0900

    feat: inputString 게터세터 함수 생성 임시 방편으로 게터 세터 만들어두었으나, 후에 고민해 볼 필요가 있음

--- src/main/java/baseball/entity/Player.java
+++ src/main/java/baseball/entity/Player.java
@@ -6,5 +6,12 @@ public class Player {
     private int ballCount=0;
     private boolean gameWin = false;
 
+    public void setInputString(String inputString) {
+        this.inputString = inputString;
+    }
+
+    public String getInputString() {
+        return inputString;
+    }
 
 }
