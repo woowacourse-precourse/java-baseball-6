71372cffe29639608fb497011fed3337a7030ff4
freekyr7529 <freekyr7529@gmail.com>
2023-10-20 20:16:04 +0900

    refactor: 테스트 방식 수정
    NumberGenerator클래스의 반환형이 List<Integer>에서 Answer 객체로 바뀐 데 따라 기존의 방식으로 코드의 유효성을 검증할 수 없었다.
    
    이에 따라 mock데이터를 생성한 후 이것과 Answer를 비교하게 함으로써 간접적으로 난수가 정상적으로 생성된다는 것을 검증했다.

--- src/test/java/baseball/RandTest.java
+++ src/test/java/baseball/RandTest.java
@@ -1,8 +1,8 @@
 package baseball;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+import java.util.ArrayList;
 import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class RandTest {
@@ -10,9 +10,18 @@ public class RandTest {
     @Test
     void 난수_생성_여부_확인() {
         NumberGenerator generator = new NumberGenerator();
-        List<Integer> answer = generator.createAnswer();
-        assertThat(answer.size()).isEqualTo(3);
-        assertThat(answer.size()).isNotEqualTo(4);
-        assertThat(answer.size()).isNotEqualTo(10);
+        Answer answer = generator.createAnswer();
+
+        List<Integer> mockInput = new ArrayList<>(){{
+            add(3);
+            add(1);
+            add(2);
+        }};
+
+        GameScore gameScore = answer.calcScore(mockInput);
+        List<Integer> result = gameScore.getResult();
+
+        Assertions.assertThat(result.get(0)).isNotNull();
+        Assertions.assertThat(result.get(1)).isNotNull();
     }
 }
