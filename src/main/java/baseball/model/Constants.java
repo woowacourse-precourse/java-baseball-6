ae708dc16434f290f6d7f57ced25aa73494a7449
leeedohyun <ldh19738@gmail.com>
2023-10-20 22:10:02 +0900

    feat: 재시작/종료에 대한 예외 기능 구현

--- src/main/java/baseball/model/Constants.java
+++ src/main/java/baseball/model/Constants.java
@@ -5,4 +5,6 @@ public class Constants {
     public static final int START_NUMBER = 1;
     public static final int END_NUMBER = 9;
     public static final int NUMBER_OF_NUMBERS = 3;
+    public static final String GAME_RESTART_NUMBER = "1";
+    public static final String GAME_END_NUMBER = "2";
 }
--- src/main/java/baseball/model/Validation.java
+++ src/main/java/baseball/model/Validation.java
@@ -8,6 +8,7 @@ public class Validation {
     private static final String SAME_NUMBER_EXCEPTION_MESSAGE = "같은 숫자를 입력할 수 없습니다.";
     private static final String NON_NUMERIC_EXCEPTION_MESSAGE = "숫자만 입력할 수 있습니다.";
     private static final String NUMBER_OF_NUMBERS_EXCEPTION_MESSAGE = "입력한 숫자는 3자리여야 합니다.";
+    private static final String RESTART_OR_END_NUMBER_EXCEPTION_MESSAGE = "1과 2만 입력해야 합니다.";
 
     public static void validateSameNumber(String numbers) {
         List<Character> checkList = new ArrayList<>();
@@ -29,6 +30,12 @@ public class Validation {
     public static void validateNumberOfNumbers(String numbers) {
         if (numbers.length() != Constants.NUMBER_OF_NUMBERS) {
             throw new IllegalArgumentException(NUMBER_OF_NUMBERS_EXCEPTION_MESSAGE);
+        }
+    }
+
+    public static void validateRestartOrEnd(String number) {
+        if (!(Constants.GAME_RESTART_NUMBER.equals(number) || Constants.GAME_END_NUMBER.equals(number))) {
+            throw new IllegalArgumentException(RESTART_OR_END_NUMBER_EXCEPTION_MESSAGE);
         }
     }
 
