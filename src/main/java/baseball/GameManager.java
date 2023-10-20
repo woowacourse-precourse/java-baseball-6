a5d3b2ce54c571dd79539272f2e8216fe38b1790
mythpoy <okmlnsunok@gmail.com>
2023-10-20 17:20:37 +0900

    refactor : change Scanner to readLine in nextstep library

--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -1,16 +1,15 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class GameManager {
 
 
     public static void start() {
-        Scanner sc = new Scanner(System.in);
         Balls computerBalls = createComputerBalls();
 
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -18,14 +17,14 @@ public class GameManager {
 
         do {
             System.out.print("숫자를 입력해주세요 : ");
-            String input = sc.nextLine();
+            String input = Console.readLine();
 
             List<Integer> integerList = GameUtil.inputParse(input);
             Balls myBalls = new Balls(integerList);
             String statusResult = myBalls.compare(computerBalls);
             System.out.println(statusResult);
 
-            result = checkResult(statusResult, sc);
+            result = checkResult(statusResult);
             if(result.equals("1")) computerBalls = createComputerBalls(); // 새로 시작할 경우 computer의 Balls 변경
 
         } while(!result.equals("2"));
@@ -44,12 +43,12 @@ public class GameManager {
         return new Balls(computer);
     }
 
-    public static String checkResult(String result, Scanner sc) {
+    public static String checkResult(String result) {
         if(result.equals("3스트라이크")) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
                                 "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
-            return sc.nextLine();
+            return Console.readLine();
         }
 
         return "0";
