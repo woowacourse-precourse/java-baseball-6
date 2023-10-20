239b970b859565b326dc6fa206efda05129a4515
김승우 <tmd8633@naver.com>
2023-10-20 15:48:17 +0900

    Feat: 상수 반환타입 int -> String 으로 변경

--- src/main/java/constant/BaseConst.java
+++ src/main/java/constant/BaseConst.java
@@ -2,9 +2,9 @@ package constant;
 
 public abstract class BaseConst {
 
-    public static final int RESTART = 1; // 재시작 여부
-    public static final int EXIT = 2; // 재시작 여부
-    public static final int COMPLETE = 3; // 스트라이크가 3개일 때 사용
+    public static final String RESTART = "1"; // 재시작 여부
+    public static final String EXIT = "2"; // 재시작 여부
+    public static final int MAX_LENGTH = 3; // 스트라이크가 3개일 때 사용
 
     public static final int STRIKE = 0;
     public static final int BALL = 1;
