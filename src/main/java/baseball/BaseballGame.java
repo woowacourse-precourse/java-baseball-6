31ecbb834c282b35e76c45d913ae448f086620f1
useu <lakingsung@gmail.com>
2023-10-20 21:36:23 +0900

    feat: generateAnswer 메서드 작성

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,19 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseballGame {
+    public static List<Integer> generateAnswer() {
+        List<Integer> answer = new ArrayList<>();
+        while (answer.size() < 3) {
+            int randomNum = Randoms.pickNumberInRange(1, 9);
+            if (!answer.contains(randomNum)) {
+                answer.add(randomNum);
+            }
+        }
+        return answer;
+    }
+}
