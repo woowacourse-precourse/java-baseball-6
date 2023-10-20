37ebab20ca378b9ddb4e67760f891ff57b1588bb
minseo  jo <liging12@naver.com>
2023-10-20 21:29:55 +0900

    feat: Application 기능 추가
    기능 목록
    1. 서비스, 뷰 를 컨트롤러에 DI 시킨다.
    2. 컨트롤러를 통해 DI로 설정된 게임을 실행시킨다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,18 +1,35 @@
 package baseball;
 
+import baseball.controller.GameController;
+import baseball.service.GameLogicService;
+import baseball.service.GameResultService;
+import baseball.service.GameSetupService;
 import baseball.view.console.InputView;
 import baseball.view.console.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameController gameController = new GameController(outputView(), inputView(), gameSetupService(), gameLogicService(), gameResultService());
+        gameController.run();
     }
 
-    private OutputView outputView() {
+    private static OutputView outputView() {
         return new OutputView();
     }
 
-    private InputView inputView() {
+    private static InputView inputView() {
         return new InputView();
+    }
+
+    private static GameSetupService gameSetupService() {
+        return new GameSetupService();
+    }
+
+    private static GameLogicService gameLogicService() {
+        return new GameLogicService();
+    }
+
+    private static GameResultService gameResultService() {
+        return new GameResultService();
     }
 }
