d07bc15f6ce1e49ab21c8cd7f17a0735ebf71efb
Heojamong <wlaud0823@naver.com>
2023-10-20 22:01:11 +0900

    [기능추가]사용자가 입력한 문자열의 유효성 검사 메소드 생성

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,10 +1,26 @@
 package baseball;
 
+import java.util.HashSet;
+
 public class User {
     private String userSelectNum;
 
     public void userSelect() {
         System.out.println("숫자를 입력해주세요");
         userSelectNum = camp.nextstep.edu.missionutils.Console.readLine();
+        userInputVaildationCheck(userSelectNum);
+    }
+
+    // 조건문을 하나로 합치면 안되는건가? 확인해보자
+    public void userInputVaildationCheck(String userSelectNum) {
+        HashSet<Character> hashUserSelectNum = new HashSet<>();
+        if (userSelectNum.length() != 3) {
+            throw new IllegalArgumentException("잘못된 값을 입력하였습니다.");
+        }
+        for (char num : userSelectNum.toCharArray()) {
+            if (!hashUserSelectNum.add(num) && !Character.isDigit(num)) {
+                throw new IllegalArgumentException("잘못된 값을 입력하였습니다.");
+            }
+        }
     }
 }
