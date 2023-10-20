35dce559fff7f9e3bda6eb7d149eded1378b6833
zangsu <zangsu_@naver.com>
2023-10-20 13:53:15 +0900

    feat : implements Game context

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -8,19 +8,18 @@ public class Game {
     Computer computer = new Computer();
 
     public void startGame(){
-
         boolean restart = false;
+        System.out.println("숫자 야구 게임을 시작합니다.");
         do{
+            computer.init();
             play();
-
-            //사용자에게 재시작 여부를 확인
-            //restart = ...
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            restart = player.restart();
         }while(restart);
         //TODO : 종료하기 전 Console.close() 호출
 
     }
     public void play(){
-        computer.init();
         Result result;
         do{
             System.out.println("숫자를 입력해주세요 : ");
@@ -29,10 +28,5 @@ public class Game {
             System.out.println(result);
         }while(result.getStrike() != 3);
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        //Computer.init();
-        /*
-        게임을 진행
-        결과 출력 ...
-         */
     }
 }
