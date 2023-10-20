7cc29805429c12b19fea27ec73090c6950413822
박형균 <phk@baghyeong-gyun-ui-MacBookPro.local>
2023-10-20 15:40:03 +0900

    feat: create and implement

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,29 @@
+package baseball.view;
+
+public class OutputView {
+
+
+    public static void ballAndStrike(int ball, int strike) {
+        String result = String.format("%s볼 %s스트라이크", ball, strike);
+        System.out.println(result);
+    }
+
+    public static void onlyBall(int ball) {
+        String result = String.format("%s볼", ball);
+        System.out.println(result);
+    }
+
+    public static void onlyStrike(int strike) {
+        String result = String.format("%s스트라이크", strike);
+        System.out.println(result);
+    }
+
+    public static void nothing() {
+        System.out.println("낫싱");
+    }
+
+    public static void endGame() {
+        String result = String.format("%s개의 숫자를 모두 맞히셨습니다! 게임 종료", 3);
+        System.out.println(result);
+    }
+}
