ecf7ede2ce28e36c7cf0ecb52597156f0317afea
jihwankim128 <jihwan1535@nate.com>
2023-10-20 17:48:26 +0900

    feat: 입력검증 실패시 예외처리

---
+++ src/main/java/baseball/utility/InputDataExceptionHandler.java
@@ -0,0 +1,23 @@
+package baseball.utility;
+
+public class InputDataExceptionHandler {
+    public static void validateNumbers(String inputNumbers) {
+        int numbersLength = inputNumbers.length();
+
+        if(numbersLength != 3) {
+            throw new IllegalArgumentException();
+        }
+
+        for(int i = 0; i < numbersLength; i++) {
+            char number = inputNumbers.charAt(i);
+            if(number < '1' || number > '9') {
+                throw new IllegalArgumentException();
+            }
+            for(int j = 0; j < i; j++) {
+                if(number == inputNumbers.charAt(j)) {
+                    throw new IllegalArgumentException();
+                }
+            }
+        }
+    }
+}
