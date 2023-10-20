e48a87fa1f0384e716342d4f1daa42d617e442f1
Dowon <3120kang@gmail.com>
2023-10-20 20:03:58 +0900

    feat: 수를 비교하는 기능 추가
    컴퓨터의 수와 사용자가 입력한 수를 비교

--- src/main/java/computer/Comparator.java
+++ src/main/java/computer/Comparator.java
@@ -1,2 +1,53 @@
-package computer;public class Comparator {
+package computer;
+
+import baseball.Balls;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Comparator {
+
+
+    private static final int BALLS_SIZE=3;
+    private static final int STRIKE=0;
+    private static final int BALL=1;
+    private List<Integer> computerNumber = new ArrayList<>();
+    private List<Integer> userNumber = new ArrayList<>();
+    List<Integer> hint=new ArrayList<>(Arrays.asList(0,0));
+    private int strike;
+    private int ball;
+
+    public void compare(Balls computer, Balls user){
+        receiveBalls(computer,user);
+        initScore();
+        int current=0;
+        while(current<BALLS_SIZE){
+            if(computerNumber.get(current).equals(userNumber.get(current))){
+                strike++;
+                current++;
+                continue;
+            }
+            if(computerNumber.contains(userNumber.get(current))){
+                ball++;
+                current++;
+            }
+        }
+    }
+
+    private void receiveBalls(Balls computer,Balls user){
+        this.computerNumber=computer.getBalls();
+        this.userNumber=user.getBalls();
+    }
+
+    private void initScore(){
+        strike=0;
+        ball=0;
+    }
+
+    public List<Integer> getHint(){
+        hint.set(STRIKE,strike);
+        hint.set(BALL,ball);
+        return hint;
+    }
 }
