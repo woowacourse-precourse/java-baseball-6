41f4f7e729c762f818904523a33b4068f87d42ad
dukcode <duk9741@gmail.com>
2023-10-21 05:25:21 +0900

    fix: 재시작 입력 검증안되는 버그 수정
    - 재시작 또는 종료 커맨드가 아니면 예외를 던지도록 수정

--- src/main/java/baseball/game/system/ReplayableGameSystemConsoleView.java
+++ src/main/java/baseball/game/system/ReplayableGameSystemConsoleView.java
@@ -4,14 +4,23 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class ReplayableGameSystemConsoleView implements ReplayableGameSystemView {
 
-    private static final String REQUEST_REPLAY_PROMPT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static final String RESTART = "1";
     private static final String EXIT = "2";
+    private static final String REQUEST_REPLAY_PROMPT = "게임을 새로 시작하려면 " + RESTART +
+            ", 종료하려면 " + EXIT + "를 입력하세요.";
 
     @Override
     public boolean requestReplayInput() {
         System.out.println(REQUEST_REPLAY_PROMPT);
-        return parseInput(Console.readLine());
+        String input = Console.readLine();
+        validInput(input);
+        return parseInput(input);
+    }
+
+    private void validInput(String input) {
+        if (!input.equals(RESTART) && !input.equals(EXIT)) {
+            throw new IllegalArgumentException("입력은 " + RESTART + " 이거나 " + EXIT + " 여야 합니다.");
+        }
     }
 
     private boolean parseInput(String input) {
