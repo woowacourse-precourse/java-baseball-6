6f10a73fd2ca9b84a032710ac73d8026edd940f4
seminss <2007ksm@naver.com>
2023-10-20 13:01:06 +0900

    feat : create output view

---
+++ src/main/java/baseball/OutputView.java
@@ -0,0 +1,36 @@
+package baseball;
+
+public class OutputView {
+    public void printStart(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void printInputMessage(){
+        System.out.println("숫자를 입력해주세요 : ");
+    }
+
+    public void printEnd(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    public void printResult(int strike, int ball){
+        if (strike==3){
+            System.out.println("3스트라이크\n" +
+                    "3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return;
+        }
+        if (strike==0 && ball>0){
+            System.out.println(ball+"볼");
+            return;
+        }
+        if (strike>0 && ball==0){
+            System.out.println(strike+"스트라이크");
+            return;
+        }
+        if (strike>0 && ball>0){
+            System.out.println(ball+"볼 "+strike+"스트라이크");
+            return;
+        }
+        System.out.println("낫싱");
+    }
+}
---
+++ src/test/java/baseball/OutputViewTest.java
@@ -0,0 +1,21 @@
+package baseball;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class OutputViewTest {
+    OutputView script = new OutputView();
+    @Test
+    @DisplayName("안내 문구 테스트")
+    public void basicScriptTest(){
+        script.printStart();
+        script.printInputMessage();
+        script.printResult(1,1);
+        script.printResult(0,1);
+        script.printResult(0,2);
+        script.printResult(1,0);
+        script.printResult(3,0);
+        script.printEnd();
+    }
+
+}
