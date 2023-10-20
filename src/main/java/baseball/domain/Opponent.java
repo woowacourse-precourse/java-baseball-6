a53ae1c9fa8ceab527a12a41d6793ac7ceb26298
SehyeonKang <gedfgrdt@naver.com>
2023-10-20 11:48:00 +0900

    feat(Opponent): 상대방 클래스 추가

---
+++ src/main/java/baseball/domain/Opponent.java
@@ -0,0 +1,16 @@
+package baseball.domain;
+
+import java.util.ArrayList;
+
+public class Opponent {
+
+    private ArrayList<Integer> answerNumbers;
+
+    public Opponent() {
+        answerNumbers = new ArrayList<>();
+    }
+
+    public ArrayList<Integer> getAnswerNumbers() {
+        return answerNumbers;
+    }
+}
