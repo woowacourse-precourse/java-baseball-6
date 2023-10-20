47813304f284bfe1c57a0a0ba7a20d526ffcf3f3
Heojamong <wlaud0823@naver.com>
2023-10-20 21:44:21 +0900

    [기능추가] 사용자로부터 숫자를 입력받는 메소드 생성

---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,10 @@
+package baseball;
+
+public class User {
+    private String userSelectNum;
+
+    public void userSelect() {
+        System.out.println("숫자를 입력해주세요");
+        userSelectNum = camp.nextstep.edu.missionutils.Console.readLine();
+    }
+}
