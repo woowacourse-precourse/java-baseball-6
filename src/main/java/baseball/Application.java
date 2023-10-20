a2f179d48e186f4d2441139835deaafe96ef3988
8x15yz <8x15yz@gmail.com>
2023-10-20 23:16:16 +0900

    refactor : RunGame 클래스를 생성해 Application과 분리함

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,64 +1,8 @@
 package baseball;
-
-import java.util.Scanner;
+import baseball.RunGame;
 
 public class Application {
-    static int[] answer = {2, 2, 4};
-    static int[] keyNum = new int[3];
-
-    public void startGame(){
-        System.out.println("숫자 야구 게임을 시작합니다.");
-    }
-
-    public boolean inputNumbers(){
-        System.out.print("숫자를 입력해주세요 : ");
-        Scanner input = new Scanner(System.in);
-        int num = input.nextInt();
-        int j = 100;
-        for (int i = 0; i < 3; i++) {
-            if (i == 0 && num/j == 0) return false;
-
-            keyNum[i] = (int) num/j;
-            num -= (num/j)*j;
-            j /= 10;
-        }
-
-        return true;
-    }
-
-    public boolean returnResult(){
-        Application finishGame = new Application();
-        int isSame = 0;
-        for (int i = 0; i < 3; i++) {
-            if (answer[i] == keyNum[i]) { isSame += 1; }
-        }
-        if (isSame == 3) {
-            finishGame.finishGame();
-            return true;
-        }
-        else {
-            System.out.println("낫싱");
-            return false;
-        }
-    }
-
-    public void finishGame() {
-        System.out.println("3스트라이크");
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-    }
-
     public static void main(String[] args) {
-        Application startGame = new Application();
-        Application inputNumbers = new Application();
-        Application returnResult = new Application();
-        startGame.startGame();
-
-        while(true){
-            boolean alert = inputNumbers.inputNumbers();
-            if (!alert) break;
-            boolean isEnd = returnResult.returnResult();
-            if (isEnd) break;
-        }
-
+        RunGame.run();
     }
 }
---
+++ src/main/java/baseball/IsMatch.java
@@ -0,0 +1,7 @@
+package baseball;
+
+public class IsMatch {
+    public static void accurateTest(){
+        System.out.println("나오나용?");
+    }
+}
