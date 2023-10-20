5091da13033767c7cf9fa12b913251f860a4dbd3
freekyr7529 <freekyr7529@gmail.com>
2023-10-20 22:12:05 +0900

    feat: OutputView 클래스 작성
    사용자의 입력에 따른 결과를 출력하는 OutputView 클래스 생성

---
+++ src/main/java/baseball/OutputView.java
@@ -0,0 +1,38 @@
+package baseball;
+
+import java.util.List;
+
+public class OutputView {
+
+    public void printResult(GameScore score) {
+        List<Integer> result = score.getResult();
+        int strike = result.get(0);
+        int ball = result.get(1);
+
+        if (strike > 0 && ball == 0) {
+            printStrike(strike);
+        } else if (strike == 0 && ball > 0) {
+            printBall(ball);
+        } else if (strike > 0 && ball > 0) {
+            printStrikeAndBall(strike, ball);
+        }
+
+        printNothing();
+    }
+
+    private void printStrike(int strike) {
+        System.out.println(strike + "스트라이크");
+    }
+
+    private void printBall(int ball) {
+        System.out.println(ball + "볼");
+    }
+
+    private void printStrikeAndBall(int strike, int ball) {
+        System.out.println(ball + "볼 " + strike + "스트라이크");
+    }
+
+    private void printNothing() {
+        System.out.println("낫싱");
+    }
+}
