e8aef3b0cd1bce2a9f945c1100d513b1310b2b04
howudong <tjdvy963@naver.com>
2023-10-20 14:53:46 +0900

    feat : 정답 번호를 가지고 있고, 이를 반환하는 AnswerNumbers 클래스 생성

---
+++ src/main/java/baseball/domain/AnswerNumbers.java
@@ -0,0 +1,22 @@
+package baseball.domain;
+
+import java.util.List;
+
+public final class AnswerNumbers {
+    private static final int NUMBER_SIZE = 3;
+
+    private final List<Integer> answerNumbers;
+
+    public List<Integer> getAnswerNumbers() {
+        return answerNumbers;
+    }
+
+    private AnswerNumbers(List<Integer> answerNumbers) {
+        this.answerNumbers = answerNumbers;
+    }
+
+    public static AnswerNumbers createAnswerNumbers(AnswerNumberGenerator numberGenerator) {
+        List<Integer> generateNumbers = numberGenerator.generate(NUMBER_SIZE);
+        return new AnswerNumbers(generateNumbers);
+    }
+}
