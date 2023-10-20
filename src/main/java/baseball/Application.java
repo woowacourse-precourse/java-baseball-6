08ad1087665f2bb87ae6ad25edda9824b4b6901d
shlihs95 <fhzptvh@gmail.com>
2023-10-20 17:08:11 +0900

    feat: 1. 게임 준비 - 상대방(컴퓨터)이 숫자 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,28 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    public List<Integer> makeComputerNumber() {
+        List<Integer> computer = new ArrayList<>();
+
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        return computer;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application application = new Application();
+        List<Integer> computer = application.makeComputerNumber();
+
     }
 }
