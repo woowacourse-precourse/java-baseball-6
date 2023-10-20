2e5b9b1e7a21b034d067ed7cd675c2c940fb7213
8x15yz <8x15yz@gmail.com>
2023-10-20 22:09:55 +0900

    feat : startGame() 기능구현 완료 ✅

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
 public class Application {
+    static int[] answer = {2, 2, 4};
+    static int[] keyNum = new int[3];
+
+    public void startGame(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application startGame = new Application();
+        startGame.startGame();
     }
 }
