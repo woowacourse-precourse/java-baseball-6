e75e5b650d15ec67bedd5f7009dd51665c1b66c5
jinkshower <askthem@naver.com>
2023-10-20 11:27:14 +0900

    feat: 랜덤숫자 생성기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,8 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+//        Computer computer = new Computer();
+//        computer.generateRandomNum();
+//        System.out.println(computer.getRandomNum());
     }
 }
---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,27 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private List<Integer> randomNum;
+
+    Computer() {
+        this.randomNum =  new ArrayList<>();
+    }
+
+    public List<Integer> getRandomNum() {
+        return List.copyOf(randomNum);
+    }
+
+    public void generateRandomNum() {
+        while (randomNum.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!randomNum.contains(randomNumber)) {
+                randomNum.add(randomNumber);
+            }
+        }
+    }
+}
