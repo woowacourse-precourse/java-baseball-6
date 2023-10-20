40372827bd17b6bbf9c4a1b8d6424b3d307d5a5d
shAn-kor <53287605+shAn-kor@users.noreply.github.com>
2023-10-20 23:48:37 +0900

    ComputerNumber 생성 시 랜덤 값 생성

--- src/main/java/baseball/model/ComputerNumber.java
+++ src/main/java/baseball/model/ComputerNumber.java
@@ -1,4 +1,22 @@
 package baseball.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ComputerNumber {
+    private final List<Integer> computer = new ArrayList<>();
+
+    public ComputerNumber() {
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+    }
+
+    public List<Integer> getComputer() {
+        return computer;
+    }
 }
