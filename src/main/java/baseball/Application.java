fdd9c359a22a89f599d051eb89cde001c208d9c6
muyaaho <skanthsu715@gmail.com>
2023-10-20 13:47:00 +0900

    feat: add exception handling

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,27 +1,15 @@
 /*
-1. 3개의 랜덤한 수 고르기(만들어서 str로 만들어 두자)
-2. 반복: 한 게임이 끝날 때 까지(맞추거나 잘못된 값을 입력하는 동안)
-3. 한 숫자를 계속 맞춰야 됨
-    - do{} while(조건);
-4. 숫자 맞치면 게임 종료(1, 2 선택하세요)
-5. 잘못된 값 => IllegalArgumentException
-6. 숫자 비교:
-    - 한 게임이 시작될 때 random으로 각자 다른 수가 들어진 배열을 만들기
-    - 입력도 배열로 만들기
-    - 입력을 반복문을 돌면서 자리가 같은지 아닌지 확인하기(아니면 둘 다 스트링으로 하고 index 찾는 함수 있으면 쓰기)
-    - string은 indexOf를 사용할 수 있음. 그럼 입력을 반복문으로 보낸 i와 인덱스가 같은지 확인하거나 아님 i, j 이중 for문 쓰거나
-7. 볼: 숫자만 맞는 것, 스트라이크: 숫자도 맞고 자리도 맞고
-8. 1~9의 서로 다른 숫자
-9. https://github.com/woowacourse/woowacourse-docs/tree/main/styleguide/java 자바 스타일 가이드
- */
-
+* 잘못된 값이 입력되었을 때
+*
+* 0을 입력해도 종료하나? 일단 예외로 하고 종료시킴. 테스트 해볼 때 0을 어떻게 처리할지 체크하기
+* */
 
 package baseball;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
 
 public class Application {
     public static void main(String[] args) {
@@ -38,8 +26,6 @@ public class Application {
         // 게임 진행
         while (end == 1) {
             // 한 게임당 초기화
-            // 랜덤한 수가 들어가는 com 리스트
-//            List<String> com = new ArrayList<String>();/
             String com = "";
             isFinish = false;
 
@@ -51,20 +37,38 @@ public class Application {
                 }
             }
             //@@@@ 삭제해야 하는 코드
-            System.out.println("answer: "+com);
+            //System.out.println("answer: "+com);
             // 게임 시작
 
             // while (게임 진행되는 동안)
             while (!isFinish) {
                 int strike = 0;
                 int ball = 0;
+                int canInt = 0;
                 // 입력
                 System.out.print("숫자를 입력해주세요 : ");
                 Scanner sc = new Scanner(System.in);
                 String num = sc.nextLine();
                 // TODO: 이상한 입력 체크
-                // 잘못된 입력 1: 3자리가 아닌 수일 경우(1,2,4, ...)
-                // 잘못된 입력 2: 0이 들어올 경우
+                try {
+                    //0이 들어올 경우
+                    if (num.contains("0")) {
+                        throw new Exception();
+                    }
+                    // 3자리 수가 아닌 경우
+                    if (num.length() != 3) {
+                        throw new Exception();
+                    }
+
+                    // 숫자가 아닌 경우
+                    canInt = parseInt(num);
+                }
+                catch(Exception e){
+                    throw new IllegalArgumentException("잘못된 값을 입력했습니다.");
+
+                }
+
+
                 // 잘못된 입력 3: 숫자가 아닌 경우
                 // 비교
                 for (int i = 0; i < 3; i++) {
@@ -86,7 +90,17 @@ public class Application {
                     System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                     System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                     end = sc.nextInt();
+                    String one = "1";
                     // TODO: 잘못된 값 입력됬는지 확인!
+                    try {
+                        if (!(end == 1 || end == 2)) {
+                            throw new Exception();
+                        }
+
+                    }catch (Exception e){
+                        throw new IllegalArgumentException("잘못된 값을 입력했습니다.");
+                    }
+
                     isFinish = true;
 
                 }
