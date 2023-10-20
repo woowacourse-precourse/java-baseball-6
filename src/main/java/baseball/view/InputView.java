10e31dd5f65a3aa6d0452c40dad58c711c803f76
YongHyeon <pyh4616@naver.com>
2023-10-20 15:50:41 +0900

    Feat: InputView, 사용자 숫자를 입력하는 기능 구현

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,19 @@
+package baseball.view;
+
+import baseball.util.Convert;
+
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputView {
+
+    private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+
+    public List<Integer> inputUserNumber() {
+        System.out.print(INPUT_NUMBER_MESSAGE);
+        String input = readLine();
+        System.out.println();
+        return Convert.convertStringToIntegerList(input);
+    }
+}
