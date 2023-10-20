c32d53239b916ae1e664d99b6ce2b5d261d2e9eb
kg9585 <kg9585@naver.com>
2023-10-20 15:23:55 +0900

    feat: 세 자리 자연수가 아닌 입력값 예외 처리

--- src/main/java/baseball/UserNumber.java
+++ src/main/java/baseball/UserNumber.java
@@ -1,26 +1,44 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
 
 public class UserNumber {
 
-  int userNumber;
+  private static final String REGEX = "[0-9]{3}";
+  private static final int VALID_LENGTH = 3;
+  private static final String REQUEST_MESSAGE = "숫자를 입력해주세요 : ";
+  private static final String CHECK_TYPE = "CheckType :";
 
-  public UserNumber(int userNumber) {
-    this.userNumber = userNumber;
+  private int userNumber;
 
+  public UserNumber() {
   }
 
   public int getUserNumber() {
     return userNumber;
   }
 
-  public void readUserNumberFromConsole() {
-    this.userNumber = parseUserNumber(Console.readLine());
+  public void setUserNumber(String input) {
+    validateInput(input);
+    this.userNumber = parseUserNumber(input);
   }
 
-  public void setUserNumberForTest(String input) {
+  public void readUserNumberFromConsole() {
+    System.out.println(REQUEST_MESSAGE);
+    String input = Console.readLine();
+    validateInput(input);
     this.userNumber = parseUserNumber(input);
+  }
+
+  private void validateInput(String input) {
+    boolean isValidLength = input.length() == VALID_LENGTH;
+    boolean isValidType = Pattern.matches(REGEX, input);
+    System.out.println(CHECK_TYPE + isValidType);
+
+    if (!(isValidLength && isValidType)) {
+      throw new IllegalArgumentException();
+    }
   }
 
   private int parseUserNumber(String input) {
--- src/test/java/baseball/UserNumberTest.java
+++ src/test/java/baseball/UserNumberTest.java
@@ -1,13 +1,57 @@
 package baseball;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import camp.nextstep.edu.missionutils.Console;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
 class UserNumberTest {
 
-  @Test
-  void isUserNumberSetByTest() {
-    UserNumber user = new UserNumber(123);
-    System.out.println(user.getUserNumber());
+  private UserNumber userNumber;
+
+  @BeforeEach
+  void setUp() {
+    userNumber = new UserNumber();
   }
 
+  @Test
+  void readValidUserNumberFromConsole() {
+    try (MockedStatic<Console> theMock = Mockito.mockStatic(Console.class)) {
+      theMock.when(Console::readLine).thenReturn("123");
+      userNumber.readUserNumberFromConsole();
+      assertEquals(123, userNumber.getUserNumber());
+    }
+  }
+
+  @Test
+  void readInvalidUserNumberThrowsException() {
+    try (MockedStatic<Console> theMock = Mockito.mockStatic(Console.class)) {
+      theMock.when(Console::readLine).thenReturn("12a");
+      assertThrows(IllegalArgumentException.class, userNumber::readUserNumberFromConsole);
+    }
+  }
+
+  @Test
+  void isUserNumberSetByTest() {
+    userNumber.setUserNumber("123");
+    assertEquals(123, userNumber.getUserNumber());
+  }
+
+  @Test
+  void isUserNumberLengthEqualsThree() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      userNumber.setUserNumber("12");
+    });
+  }
+
+  @Test
+  void isUserNumberNotString() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      userNumber.setUserNumber("abc");
+    });
+  }
 }
