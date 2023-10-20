484d1d3188021ec541738fb048b80ce832ba0267
junOTL <naqp99@gmail.com>
2023-10-20 23:50:14 +0900

    feat: 1부터 9까지 숫자외 입력한 경우 예외 발생 기능 구현

--- src/main/java/baseball/util/Validation.java
+++ src/main/java/baseball/util/Validation.java
@@ -1,5 +1,7 @@
 package baseball.util;
 
+import java.util.regex.Pattern;
+
 public class Validation {
 
     public static void isEmpty(String input) {
@@ -11,6 +13,12 @@ public class Validation {
     public static void isLengthThree(String input) {
         if (input.length() != 3) {
             throw new IllegalArgumentException("세 자리 숫자를 입력해 주세요.");
+        }
+    }
+
+    public static void isInRange(String input) {
+        if (!Pattern.matches("^[1-9]+$", input)) {
+            throw new IllegalArgumentException("1부터 9까지의 숫자만 입력 가능 합니다.");
         }
     }
 }
