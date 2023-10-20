22f40869929f40a1a880ba4d7fd0a66b610ffa05
minjun7410 <minjun7410@gmail.com>
2023-10-20 22:45:55 +0900

    feat: Computer 클래스 initiate 메서드 구현
    게임을 시작할 때 컴퓨터가 중복되지 않는 3개의 수를 랜덤으로 뽑는 기능 구현

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,21 @@
+package baseball;
+import camp.nextstep.edu.missionutils.Randoms;
+public class Computer {
+    int[] randomNumbers;
+    Computer(){
+        randomNumbers = new int[3];
+    }
+    public int[] initiate(){
+        boolean[] duplicateCheck = new boolean[10];
+        for(int i = 0; i < 3; i++){
+            int num = Randoms.pickNumberInRange(1, 10);
+            if(duplicateCheck[num]){
+                i--;
+                continue;
+            }
+            duplicateCheck[num] = true;
+            randomNumbers[i] = num;
+        }
+        return randomNumbers;
+    }
+}
--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -2,20 +2,23 @@ package baseball;
 
 public class Game {
     Display display;
+    Computer computer;
     public Game(){
         display = new Display();
+        computer = new Computer();
         display.printStartText();
     }
     public void playGame(){
-        boolean restartFlag = true;
-        while(restartFlag) {
-            int[] userInputs = display.getUserInput();
+        do{
+            computer.initiate();
             boolean gameOver = true;
-            if(gameOver){
-                display.printGameOverText();
-                restartFlag = display.getRestartInput();
+            do{
+                int[] userInputs = display.getUserInput();
+                gameOver = false;
             }
+            while(gameOver);
+            display.printGameOverText();
         }
-
+        while(display.getRestartInput());
     }
 }
