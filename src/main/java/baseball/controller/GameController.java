666a2fb0abca2d5ea693d8696d31982eadb9cef9
mizuirohoshi7 <mizuirohoshi7@gmail.com>
2023-10-20 17:35:51 +0900

    feat: GameController, GameService 싱글톤 패턴 적용

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,4 +1,20 @@
 package baseball.controller;
 
+import baseball.service.GameService;
+
 public class GameController {
+    private static GameController gameController;
+
+    private final GameService gameService;
+
+    public static GameController getInstance() {
+        if (gameController == null) {
+            gameController = new GameController();
+        }
+        return gameController;
+    }
+
+    private GameController() {
+        gameService = GameService.getInstance();
+    }
 }
--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -1,4 +1,14 @@
 package baseball.service;
 
 public class GameService {
+    private static GameService gameService;
+
+    private GameService() {}
+
+    public static GameService getInstance() {
+        if (gameService == null) {
+            gameService = new GameService();
+        }
+        return gameService;
+    }
 }
