c0d03d75f36657b28200d273b26a26737ca4e8a0
박승찬 <seungchan141414@gmail.com>
2023-10-20 15:21:32 +0900

    [Style(baseball)] 주석 추가

--- src/main/java/baseball/domain/GameStandard.java
+++ src/main/java/baseball/domain/GameStandard.java
@@ -4,24 +4,26 @@ import java.util.List;
 
 public class GameStandard {
     /**
+     * 유저의 입력 숫자중에 컴퓨터와 같은 숫자가 몇개 인지 cnt을 반환
      *
      * @param computerList 컴퓨터의 3개의 랜덤 LIST [1, 2, 3]
-     * @param userList 유저의 3개의 랜덤 LIST [2, 3, 4]
+     * @param userList     유저의 3개의 랜덤 LIST [2, 3, 4]
      * @return
      */
-    public int getSameNumber2Computer(List<Integer> computerList, List<Integer> userList){
+    public int getSameNumber2Computer(List<Integer> computerList, List<Integer> userList) {
 
         return 0;
     }
 
     /**
      * List 숫자중 해당 index의 숫자와 numIndexPlaceOfUser의 숫자가 동일한지 확인한다.
-     * @param computerList 컴퓨터의 3개의 랜덤 숫자 리스트 [ 1, 2, 3]
-     * @param placeIndexOfUser 유저가 확인을 원하는 숫자의 index
+     *
+     * @param computerList        컴퓨터의 3개의 랜덤 숫자 리스트 [ 1, 2, 3]
+     * @param placeIndexOfUser    유저가 확인을 원하는 숫자의 index
      * @param numIndexPlaceOfUser 유저가 확인을 원하는 index의 num값
      * @return 만약에 List의 index의 숫자가 numIndexPlaceOfUser와 동일하다면 true
      */
-    public boolean isSamePlaceNumUserAndComputer(List<Integer> computerList, int placeIndexOfUser, int numIndexPlaceOfUser){
+    public boolean isSamePlaceNumUserAndComputer(List<Integer> computerList, int placeIndexOfUser, int numIndexPlaceOfUser) {
 
         return true;
     }
