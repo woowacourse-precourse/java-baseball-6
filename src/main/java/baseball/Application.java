295ca339e072e4f2b181cb0f76b452fee3020811
stone <109290247+astrataraxia@users.noreply.github.com>
2023-10-20 16:57:41 +0900

    input valid method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -15,12 +15,11 @@ public class Application {
         while(true) {
             List<Integer> randomNumber = Randoms.pickUniqueNumbersInRange(1, 9, 3);
 
-            int attempts = 0;
-            while (attempts == 0) {
+            while (true) {
                 System.out.print("숫자를 입력해주세요 : ");
                 String inputNumber = Console.readLine();
                 List<Integer> answer = parseInput(inputNumber);
-                attempts = checkAnswer(randomNumber, answer);
+                break;
             }
 
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
@@ -34,22 +33,30 @@ public class Application {
 
 
     private static List<Integer> parseInput(String input) {
+        validInput(input);
         List<Integer> parse = new ArrayList<>();
         input.split("");
-
-        validInputNumber(parse);
-
         return parse;
-    }
-
-    private static void validInputNumber(List<Integer> parseNumber) {
-        if (parseNumber.size() > 3) {
-            throw new IllegalArgumentException();
-        }
     }
 
     private static int checkAnswer(List<Integer> randomNumber, List<Integer> answer) {
 
         return 1;
     }
+
+    private static void validInput(String input) {
+        if (!isThreeDigitNumber(input) || hasDuplicateDigits(input)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static boolean isThreeDigitNumber(String input) {
+        return input.length() == 3 && input.chars().allMatch(Character::isDigit);
+    }
+
+    private static boolean hasDuplicateDigits(String input) {
+        char[] digits = input.toCharArray();
+        return digits[0] == digits[1] || digits[0] == digits[2] || digits[1] == digits[2];
+    }
+
 }
