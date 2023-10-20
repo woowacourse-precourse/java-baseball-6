969e022045c98f3926c0fbbc6cdbf20f0187b1c7
Uniaut <kunwoo0927@gmail.com>
2023-10-21 00:12:19 +0900

    feat: implemented main function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -70,7 +70,18 @@ class Game {
 
 
 public class Application {
+    static boolean askRestart() {
+        String inputString = Console.readLine();
+        return inputString.equals("1");
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        while (true) {
+            System.out.println("숫자야구 게임을 시작합니다.");
+            Game game = new Game(123);
+            game.run();
+            if (!askRestart())
+                break;
+        }
     }
 }
