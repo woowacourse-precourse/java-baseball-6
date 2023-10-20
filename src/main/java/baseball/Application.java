9e93cfdf66980867234bde2e4aed409c36099784
HungKungE <jonsun59@gmail.com>
2023-10-20 22:38:18 +0900

    [src/main] 랜덤 숫자 문자열 생성 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,37 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    public static List<Integer> get_random_numbers(){
+        List<Integer> computer = new ArrayList<>();
+
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        return computer;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        // 중복 없는 3자리 수 만들기
+        List<Integer> computer = get_random_numbers();
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        System.out.print("숫자를 입력해주세요 : ");
+        String userInput = Console.readLine();
+        System.out.println();
+        System.out.println("사용자 입력: " + userInput);
+
     }
 }
