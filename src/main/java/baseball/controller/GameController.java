32a190af4c77a45ada1820ae2acf6583d599747e
acs0209 <annjae4676@naver.com>
2023-10-20 21:15:20 +0900

    feat: add user input prompt messages

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -9,6 +9,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class GameController {
 
     public String getUserInput() {
+        System.out.print("숫자를 입력해주세요 : ");
         return Console.readLine();
     }
 
