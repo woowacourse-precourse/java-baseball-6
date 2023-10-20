9e6337e6beedc6fe3ff38541843bb4e7d5c15d6f
이희수 <hslee9234@naver.com>
2023-10-21 05:49:31 +0900

    게임 재실행 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,7 @@ package baseball;
 
 import baseball.controller.GameController;
 import baseball.model.BaseballGame;
+import baseball.model.RetryNumber;
 import baseball.view.GameView;
 
 public class Application {
@@ -9,10 +10,19 @@ public class Application {
 
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-        BaseballGame game = new BaseballGame();
-        GameView view = new GameView();
-        GameController controller = new GameController(game, view);
-        controller.startGame();
+        do {
+            BaseballGame game = new BaseballGame();
+            GameView view = new GameView();
+            GameController controller = new GameController(game, view);
+            controller.startGame();
+        }while(isRetry());
+    }
 
+    public static boolean isRetry(){
+        RetryNumber retryNumber = new RetryNumber(GameView.setRetryNumber());
+        if(retryNumber.getRetryNumber().equals("1")){
+            return true;
+        }
+        return false;
     }
 }
