b407d01cfe086be4d67a76dcc9514881b8199072
Juhyorim <joohr1234@gmail.com>
2023-10-20 23:29:35 +0900

    refactor: BaseballGame 클래스 생성, 함수들을 클래스 안으로 이동
    main함수에서는 BaseballGame 객체를 만들고 start만 하면 게임을 할 수 있음

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,27 +3,23 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-public class Application {
-    public static void main(String[] args) {
-        boolean isFinish = false;
-        System.out.println("숫자 야구 게임을 시작합니다.");
+class BaseballGame {
+    private String computerNum;
+    private int strikeCnt;
+    private int ballCnt;
 
-        while (!isFinish) {
-            isFinish = startGame();
-        }
+    public BaseballGame() {
+        this.computerNum = makeRandomNumber();
     }
 
-    private static boolean startGame() {
-        String computerNum = makeRandomNumber();
+    public boolean start() {
         boolean isFinish = false;
 
         while (!isFinish) {
             String input = getInput(InputType.FIND);
-            isFinish = checkFinish(input, computerNum);
+            isFinish = checkFinish(input);
         }
 
         boolean isRestart = askRestart();
@@ -34,23 +30,37 @@ public class Application {
         }
     }
 
-    private static String getInput(InputType type) {
-        String input = Console.readLine().strip();
+    private enum InputType { FIND, RESTART }
+
+    private String makeRandomNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        return "" + computer.get(0) + computer.get(1) + computer.get(2);
+    }
+
+    private String getInput(InputType type) {
+        String input;
 
         if (type == InputType.FIND) {
             System.out.print("숫자를 입력해주세요 : ");
+            input = Console.readLine().strip();
             checkFindInput(input);
         } else {
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            input = Console.readLine().strip();
             checkRestartInput(input);
         }
 
         return input;
     }
 
-    private enum InputType { FIND, RESTART }
-
-    private static boolean askRestart() {
+    private boolean askRestart() {
         String restartInput = getInput(InputType.RESTART);
 
         if (restartInput.equals("1")) {
@@ -60,42 +70,42 @@ public class Application {
         return false;
     }
 
-    private static boolean checkFinish(String input, String computerNum) {
-        Map<String, Integer> cnt = compare(input, computerNum);
+    private boolean checkFinish(String input) {
+        compare(input);
 
-        if (cnt.get("strike") == 3) {
+        if (strikeCnt == 3) {
             printFinish();
             return true;
         }
 
-        printResult(cnt);
+        printResult();
         System.out.println(computerNum); //TODO 확인용, 나중에 삭제
         return false;
     }
 
-    private static void printResult(Map<String, Integer> cnt) {
+    private void printResult() {
         StringBuilder sb = new StringBuilder();
-        if (cnt.get("ball")==0 && cnt.get("strike")==0) {
+        if (ballCnt==0 && strikeCnt==0) {
             sb.append("낫싱");
         } else {
-            if (cnt.get("ball")!=0) {
-                sb.append(cnt.get("ball") + "볼 ");
+            if (ballCnt!=0) {
+                sb.append(ballCnt + "볼 ");
             }
 
-            if (cnt.get("strike")!=0) {
-                sb.append(cnt.get("strike") + "스트라이크");
+            if (strikeCnt!=0) {
+                sb.append(strikeCnt + "스트라이크");
             }
         }
 
         System.out.println(sb);
     }
 
-    private static void printFinish() {
+    private void printFinish() {
         System.out.println("3스트라이크");
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
-    private static Map<String, Integer> compare(String input, String computerNum) {
+    private void compare(String input) {
         int strikeCnt = 0;
         int ballCnt = 0;
 
@@ -111,26 +121,11 @@ public class Application {
             }
         }
 
-        Map<String, Integer> map = new HashMap<>();
-        map.put("strike", strikeCnt);
-        map.put("ball", ballCnt);
-
-        return map;
+        this.strikeCnt = strikeCnt;
+        this.ballCnt = ballCnt;
     }
 
-    private static String makeRandomNumber() {
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
-        }
-
-        return "" + computer.get(0) + computer.get(1) + computer.get(2);
-    }
-
-    private static void checkRestartInput(String input) {
+    private void checkRestartInput(String input) {
         //한 글자인지 확인
         if (input.length() != 1) {
             throw new IllegalArgumentException("한 글자가 아닙니다");
@@ -142,7 +137,7 @@ public class Application {
         }
     }
 
-    private static void checkFindInput(String input) {
+    private void checkFindInput(String input) {
         //세 글자인지 확인
         if (input.length() != 3) {
             throw new IllegalArgumentException("세 글자가 아닙니다");
@@ -152,18 +147,34 @@ public class Application {
         if (!input.matches("[1-9]+")) {
             throw new IllegalArgumentException("1에서 9사이 숫자가 아닙니다");
         }
-        
+
         //셋 다 다른 숫자인지 확인
-        if (hasSameNumber(input.charAt(0), input.charAt(1), input.charAt(2))) {
+        if (hasSameNumber(input)) {
             throw new IllegalArgumentException("중복되는 숫자가 존재합니다");
         }
     }
 
-    private static boolean hasSameNumber(char c1, char c2, char c3) {
+    private boolean hasSameNumber(String input) {
+        int c1 = input.charAt(0);
+        int c2 = input.charAt(1);
+        int c3 = input.charAt(2);
+
         if (c1==c2 || c2==c3 || c3==c1) {
             return true;
         }
 
         return false;
+    }
+}
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        boolean isFinish = false;
+
+        while (!isFinish) {
+            BaseballGame game = new BaseballGame();
+            isFinish = game.start();
+        }
     }
 }
