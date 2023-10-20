7ae7c12eddc09d23f925d3cc60df42c07ef52002
혜민 <alddlv81@gmail.com>
2023-10-20 14:00:06 +0900

    Feat: 기능 1. 컴퓨터에서 임의의 수 3개 출력 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        Game game = new Game();
+        game.computer();
+    }
+}
 
+class Game{
+    List<Integer> computer_answer = new ArrayList<>();
+
+    void computer(){
+        while (computer_answer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer_answer.contains(randomNumber)) {
+                computer_answer.add(randomNumber);
+            }
+        }
+        System.out.println("computer: " + computer_answer);
     }
 }
