30933ea8891eeecef4772c48714353d501dfb973
junsoo <choix712@umn.edu>
2023-10-20 23:58:25 +0900

    feat: 컨트롤러에 Computer와 Player 객체 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
 
+import baseball.Controller.BaseballGameController;
+
 public class Application {
     public static void main(String[] args) {
+
+        BaseballGameController baseballGameController = new BaseballGameController();
+        baseballGameController.gameStart();
         // TODO: 프로그램 구현
     }
 }
---
+++ src/main/java/baseball/Controller/BaseballGameController.java
@@ -0,0 +1,31 @@
+package baseball.Controller;
+
+import baseball.model.Computer;
+import baseball.model.Player;
+import baseball.service.CompareNumber;
+import baseball.service.ComputerService;
+import baseball.service.PlayerService;
+import baseball.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseballGameController {
+
+    private static ComputerService computerService = new ComputerService();
+    private static List<Integer> computer = computerService.createComputerList();
+
+    private static PlayerService playerService = new PlayerService();
+    private static List<Integer> player;
+
+    public static void gameStart() {
+        View.printStart();
+        playerInput();
+    }
+
+    public static void playerInput() {
+        String input = View.printInput();
+        player = playerService.createPlayerList(input);
+    }
+
+}
