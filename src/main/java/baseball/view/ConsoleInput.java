91d4f6ef8008c913e6914dae88e0e4a920c0a930
noxknow <chris2769@naver.com>
2023-10-20 13:31:02 +0900

    update(ConsoleInput) : 게임이 끝난 경우 상태를 선택하는 메서드 구현

--- src/main/java/baseball/view/ConsoleInput.java
+++ src/main/java/baseball/view/ConsoleInput.java
@@ -14,6 +14,7 @@ public class ConsoleInput implements InputHandler {
 
     @Override
     public int inputGameResult() {
-        return 0;
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return Integer.parseInt(readLine());
     }
 }
