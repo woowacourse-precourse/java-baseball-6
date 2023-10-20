bc9384864d98f095555fd112ae3bffa3c1f75fdd
jiwoo <zbqmgldjfh@gmail.com>
2023-10-20 23:06:37 +0900

    feat(InputView): InputValidator 구현

---
+++ src/main/java/baseball/util/InputValidator.java
@@ -0,0 +1,30 @@
+package baseball.util;
+
+public class InputValidator {
+
+    public static boolean isValidUserInput(String input) {
+        if(isNotNumeric(input)) return false;
+        if(isInvalidLength(input)) return false;
+        if(isDuplicated(input)) return false;
+        return true;
+    }
+
+    private static boolean isDuplicated(String input) {
+        for(int i = 0; i < input.length() - 1; i++) {
+            for(int j = i + 1; j < input.length(); j++) {
+                if(input.charAt(i) == input.charAt(j)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean isInvalidLength(String input) {
+        return input.length() != 3;
+    }
+
+    private static boolean isNotNumeric(String s) {
+        return !s.chars().allMatch(Character::isDigit);
+    }
+}
