7a678eaabd029c42d37746b5d4f19eabeb856025
rbgusdlza <rbgusdlza@gmail.com>
2023-10-21 03:35:41 +0900

    user input 처리 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,5 +5,19 @@ public class Application {
 
     }
     
+    static void checkUserInput(String input) throws IllegalArgumentException{
+        String errorMessage = "[error] 올바르지 않은 입력입니다.";
+        // check the input size is 3
+        if(input.length() != 3) throw new IllegalArgumentException(errorMessage);
+        // check each character is number
+        for(int i = 0; i < input.length(); i++){
+            if(!(input.charAt(i) >= '0' && input.charAt(i) <= '9')) throw new IllegalArgumentException(errorMessage);
+        }
+        // check number are different each other
+        char firstNum = input.charAt(0);
+        char secondNum = input.charAt(1);
+        char thirdNum = input.charAt(2);
+        if(firstNum == secondNum || firstNum == thirdNum || secondNum == thirdNum) throw new IllegalArgumentException(errorMessage);
+    }
 }
 
