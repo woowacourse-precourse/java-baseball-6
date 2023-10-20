a7d4360a116ec54ca2742f8f4be965b71b21dc73
Firedrago95 <yonghwa95@naver.com>
2023-10-20 12:21:26 +0900

    test InputView 의 유효성검사 테스트코드 작성

---
+++ src/test/java/baseball/View/InputViewTest.java
@@ -0,0 +1,42 @@
+package baseball.View;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class InputViewTest {
+
+    @Test
+    @DisplayName("null 유효성검사 테스트")
+    public void isNullTest() {
+        Throwable exception = assertThrows(IllegalArgumentException.class,
+                () -> InputView.validateInput(null));
+        assertEquals(InputView.NULL_OR_EMPTY_MESSAGE, exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("빈 문자열 유효성검사 테스트")
+    public void isEmptyTest() {
+        Throwable exception = assertThrows(IllegalArgumentException.class,
+                () -> InputView.validateInput(InputView.EMPTY));
+        assertEquals(InputView.NULL_OR_EMPTY_MESSAGE, exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("세자리 숫자 유효성검사 테스트")
+    public void isThreeDigitNumberTest() {
+        Throwable exception = assertThrows(IllegalArgumentException.class,
+                () -> InputView.validateInput("2222"));
+        assertEquals(InputView.NOT_THREE_DIGIT_NUMBER_MESSAGE, exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("숫자 중복 유효성검사 테스트")
+    public void isUniqueNumberTest() {
+        Throwable exception = assertThrows(IllegalArgumentException.class,
+                () -> InputView.validateInput("222"));
+        assertEquals(InputView.NOT_UNIQUE_NUMBER_MESSAGE, exception.getMessage());
+    }
+}
