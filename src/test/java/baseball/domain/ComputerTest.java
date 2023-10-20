9ec136943377c07415898ad338df4a0a9cafb647
김명준 <86913355+mjj111@users.noreply.github.com>
2023-10-20 15:22:20 +0900

    style(ComputerTest): given when then 구조로 구분할 수 있도록 줄바꿈 추가
    given when then 구조로 테스트코드가 동작함을 시각적으로 더 편히 확인할 수 있게 줄바꿈을 추가하였습니다.

--- src/test/java/baseball/domain/ComputerTest.java
+++ src/test/java/baseball/domain/ComputerTest.java
@@ -11,6 +11,7 @@ public class ComputerTest {
     @Test
     void Computer_랜덤숫자생성() {
         Computer computer = new Computer();
+
         List<Integer> randomNumbers =  computer.getRandomNumbers();
         int createdResultSize = randomNumbers.size();
 
@@ -21,8 +22,8 @@ public class ComputerTest {
     void Computer_서로다른_랜덤숫자생성() {
         Computer computer = new Computer();
         List<Integer> randomNumbers =  computer.getRandomNumbers();
-        HashSet<Integer> randomNumbersSet = new HashSet<>(randomNumbers);
 
+        HashSet<Integer> randomNumbersSet = new HashSet<>(randomNumbers);
         int createdResultSize = randomNumbersSet.size();
 
         Assertions.assertEquals(3, createdResultSize);
