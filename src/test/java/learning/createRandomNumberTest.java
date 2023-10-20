2f303d68d0e01adcd1246ef8f9c5071359f5ba42
Mingeun Park <mindal99546@gmail.com>
2023-10-20 17:00:16 +0900

    test(learning): missionutils 라이브러리에 대한 학습테스트 작성

---
+++ src/test/java/learning/createRandomNumberTest.java
@@ -0,0 +1,36 @@
+package learning;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class createRandomNumberTest {
+
+    @Test
+    void 랜덤으로_숫자_세_개_생성() {
+        // given
+        List<Integer> computer = new ArrayList<>();
+        // when
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        // then
+        Assertions.assertTrue(isThreeDifferentNumbers(computer));
+    }
+
+    boolean isThreeDifferentNumbers(List<Integer> numbers) {
+        int[] countOf = new int[10];
+        for (Integer n: numbers) {
+            countOf[n] += 1;
+            if (countOf[n] > 1)
+                return false;
+        }
+        return true;
+    }
+}
