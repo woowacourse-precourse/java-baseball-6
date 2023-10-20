b04db7cd7e5ff8cf3f8bcc86ec9f53c8f47cf54e
dltjdgh0428 <dltjdgh0428@naver.com>
2023-10-20 13:25:48 +0900

    feat: 랜덤한 수를 뽑는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner input = new Scanner(System.in);
+        boolean gameChoice = true;
+        List<Integer> computerNum = new ArrayList<>();
+        String gameString = "";
+        // 컴퓨터 숫자를 생성함. 차후 메소드로 바꿀것
+        while (computerNum.size() < 3) {
+            int num = Randoms.pickNumberInRange(1, 9);
+            if (!computerNum.contains(num)) {
+                computerNum.add(num);
+            }
+        }
     }
 }
