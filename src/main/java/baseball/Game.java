b15c026a4b5694857b61c3e90a7e34d562f5bf7c
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-10-20 14:45:02 +0900

    feat: 게임 시작 문구 출력 구현

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,7 @@
+package baseball;
+
+public class Game {
+    public void startGame(){
+        Output.startCommand();
+    }
+}
---
+++ src/main/java/baseball/Output.java
@@ -0,0 +1,7 @@
+package baseball;
+
+public class Output {
+    public static void startCommand() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+}
