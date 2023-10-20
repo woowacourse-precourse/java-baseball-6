c189064ff7b5df2a7789993e17cbcf78934a4aac
jinny-l <jinny.l.dev@gmail.com>
2023-10-20 14:21:56 +0900

    ✨feat: 게임 종료 메시지 출력 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,10 +1,17 @@
 package baseball.view;
 
+import baseball.config.GameConfig;
+
 public class OutputView {
 
     private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String END_MESSAGE = "%s개의 숫자를 모두 맞히셨습니다! 게임 종료%n";
 
     public static void printGameStartMessage() {
         System.out.println(START_MESSAGE);
+    }
+
+    public static void printGameEndMessage() {
+        System.out.printf(END_MESSAGE, GameConfig.BASEBALL_LENGTH);
     }
 }
