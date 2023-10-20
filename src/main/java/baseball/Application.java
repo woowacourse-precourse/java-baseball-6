93b78c207f1df8c92d0bf3bcc804981d398c7ee8
tfer2442 <tfer2442@gmail.com>
2023-10-21 00:33:08 +0900

    feat(Application): 메인 메서드 로직 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,31 @@
 package baseball;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        camp.nextstep.edu.missionutils.
+        boolean endFlag;
+        printHandler.gameStartPrint();
+
+        do {
+            Computer computer = new Computer();
+            InputHandler inputHandler = new InputHandler();
+
+            while (true) {
+                List<Integer> inputNumberList = inputHandler.inputNumber();
+
+                if (computer.compareNumber(inputNumberList)) {
+                    printHandler.gameEndPrint();
+                    printHandler.choiceEndOrRePrint();
+                    endFlag = inputHandler.choiceEndOrReInput();
+                    break;
+                }
+            }
+        } while (!endFlag);
+
+       printHandler.programEnd();
     }
 }
