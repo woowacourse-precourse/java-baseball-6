dd4d542aaf0806b22f9de75c20e4fe2da692ca5d
김승우 <tmd8633@naver.com>
2023-10-20 17:40:36 +0900

    Feat: Valid Pattern, Matcher 기능 추가

--- src/main/java/validation/Valid.java
+++ src/main/java/validation/Valid.java
@@ -2,16 +2,23 @@ package validation;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static constant.BaseConst.*;
 
 public class Valid {
 
+    private static Pattern p;
+    private Matcher m;
+
 
     public void myNumberValid(String number) {
+        if (number == null || number.length() != MAX_LENGTH) throw new IllegalArgumentException(MAX_LENGTH + "개의 숫자를 입력해야함");
 
-        if (number.matches("[^1-9]")) throw new IllegalArgumentException("문자와 0은 사용할 수 없음");
-        if (number.length() != MAX_LENGTH) throw new IllegalArgumentException(MAX_LENGTH + "개의 숫자를 입력할 수 있음");
+        p = Pattern.compile("[^1-9]");
+        m = p.matcher(number);
+        if (m.find()) throw new IllegalArgumentException("문자와 0은 사용할 수 없음");
 
         Set<Character> set = new HashSet<>();
         for (int i = 0; i < number.length(); i++) {
@@ -22,11 +29,13 @@ public class Valid {
     }
 
     public void restartValid(String text) {
-        if (text.matches("[^12]")){
-            throw new IllegalArgumentException("게임을 재시작하려면 1, 종료하려면 2를 입력해야함");
+        if (text == null || text.length() != 1) {
+            throw new IllegalArgumentException("숫자 하나를 입력해야함");
         }
-        if (text.length() != 1) {
-            throw new IllegalArgumentException("숫자 하나만 입력해야함");
+        p = Pattern.compile("[^12]");
+        m = p.matcher(text);
+        if (m.find()){
+            throw new IllegalArgumentException("게임을 재시작하려면 1, 종료하려면 2를 입력해야함");
         }
 
     }
