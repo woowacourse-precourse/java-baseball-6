e404157cbb6a2159b860214a306d997d11ff396d
hyun-hyang <jimin3060@naver.com>
2023-10-20 22:50:56 +0900

    feature : add comp&user num ready

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,18 +1,42 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
     boolean gameClear = false;
 
     public Game() {
         startAnnounce();
-        gameIng();
+        computerPickNum();
+        inputGuess();
+        gameClearAnnounce();
     }
 
-    private void gameIng() {
-//        while (!gameClear) {
-//
-//        }
-        gameClearAnnounce();
+    private void computerPickNum() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+    }
+
+    private void inputGuess() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+
+        List<Integer> userGuess = new ArrayList<>();
+        input.chars().forEach(i -> userGuess.add(i - 48));
+
+        for (Integer data : userGuess) {
+            System.out.println(data);
+        }
+
+
     }
 
     private void startAnnounce() {
