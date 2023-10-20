d122bc375393d9a8bf850e70cd9a9aed768c0d42
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-10-20 19:00:10 +0900

    fix: Add 누락된 문자열 출력용 메소드 게임 종료 직후 문자열 출력 관련 메소드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -27,6 +27,7 @@ public class Application {
             }
             // 게임 재시작 여부 결정
             System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
             input = Console.readLine();
             if (input.equals("1")) {
