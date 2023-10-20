54397a4707badf42497c9b1ac274faea76d79c89
김영준 <rladudwns164@gmail.com>
2023-10-20 14:30:18 +0900

    feat : 사용자 입력 전 유효성 검사 추가

--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -1,12 +1,25 @@
 package Model;
 
+import Util.Validator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class User {
 
     private String userNumber;
-    public User(){
-        this.userNumber = Console.readLine();
+
+    public User() throws IllegalArgumentException{
+        try{
+            String userInput = Console.readLine();
+            Validator.validateUserInput(userInput);
+            Validator.validateInputLength(userInput, 3);
+            this.userNumber = userInput;
+        } catch (IllegalArgumentException e){
+            throw e;
+        }
+    }
+
+    public String getuserNumber(){
+        return userNumber;
     }
 
 }
