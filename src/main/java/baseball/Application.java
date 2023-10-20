0fca8df9e2a638d8caf18a1e75fa3a31a149c4e1
dy102 <132787602+dy102@users.noreply.github.com>
2023-10-21 01:04:11 +0900

    Add: domain 패키지 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,10 @@
 package baseball;
 
+import baseball.domain.BallAndStrikeCount;
+import baseball.domain.Computer;
+import baseball.domain.Restart;
+import baseball.domain.User;
+
 public class Application {
     public static void main(String[] args) {
         User user = new User();
