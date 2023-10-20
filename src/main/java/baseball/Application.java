e224cf5b67d1861e1989e929eb697f9be5de7444
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2023-10-20 18:18:52 +0900

    feat:  3개의 숫자를 모두 맞힐 때까지 게임 로직 반복

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,12 +7,19 @@ public class Application {
         Computer computerNumbers = new Computer();
 
         Print.printGameStart();
-        Print.printInputNumber();
-        String input = Console.readLine();
+        boolean threeStrike;
+        do {
+            Print.printInputNumber();
+            String input = Console.readLine();
+            Player playerNumbers = new Player(input);
 
-        Player playerNumbers = new Player(input);
+            Hint hint = new Hint(computerNumbers.getComputer(), playerNumbers.getPlayerNumbers());
+            Print.printHint(hint);
+            threeStrike = isThreeStrike(hint.getStrike());
+        } while(!threeStrike);
+    }
 
-        Hint hint = new Hint(computerNumbers.getComputer(), playerNumbers.getPlayerNumbers());
-        Print.printHint(hint);
+    private static boolean isThreeStrike(int strike) {
+        return strike == 3;
     }
 }
