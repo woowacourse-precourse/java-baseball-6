bd98d0da9072426a53e9e7a142b101bd04ade1ec
lee jihyeon <lejinhy@gmail.com>
2023-10-20 19:18:08 +0900

    feat: application에서 IllegalArgumentException 예외 잡을 수 있게 try-catch 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,12 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameController gameController = new GameController();
+        try {
+            gameController.start();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
