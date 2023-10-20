9e252e0684c77aea29a786ec6865769c95558e93
leesh <qwqw8019@naver.com>
2023-10-20 19:46:08 +0900

    feat: BaseBallGameMessage 클래스 변경
    - enum 타입에서 class로 변경

--- src/main/java/baseball/game/BaseBallGameMessage.java
+++ src/main/java/baseball/game/BaseBallGameMessage.java
@@ -1,16 +1,5 @@
 package baseball.game;
 
-public enum BaseBallGameMessage {
-    START_GAME("숫자 야구 게임을 시작합니다.")
-
-    ;
-
-    private final String message;
-    BaseBallGameMessage(String message){
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
+public class BaseBallGameMessage {
+    public static final String GAME_START = "숫자 야구 게임을 시작합니다.";
 }
