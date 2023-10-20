712e3948c99d9ea5dcf054749b699ddc6e8fb241
leeedohyun <ldh19738@gmail.com>
2023-10-20 23:00:25 +0900

    feat: 스트라이크와 볼의 개수 그리고 낫싱 출력하는 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,6 +4,10 @@ public class OutputView {
 
     private static final String BASEBALL_GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String INPUTTING_NUMBERS_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String NOTHING = "낫싱";
+    public static final String STRIKE = "스트라이크";
+    public static final String BALL = "볼";
+    public static final String BLANK_SPACE = " ";
 
     public static void printBaseballGameStartMessage() {
         System.out.println(BASEBALL_GAME_START_MESSAGE);
@@ -11,5 +15,17 @@ public class OutputView {
 
     public static void printMessageToInputNumbers() {
         System.out.print(INPUTTING_NUMBERS_MESSAGE);
+    }
+
+    public static void printNothing() {
+        System.out.println(NOTHING);
+    }
+
+    public static void printOnlyStrike(int strikeNumbers) {
+        System.out.println(strikeNumbers + STRIKE);
+    }
+
+    public static void printBallAndStrike(int ballNumbers, int strikeNumbers) {
+        System.out.println(ballNumbers + BALL + BLANK_SPACE + strikeNumbers + STRIKE);
     }
 }
