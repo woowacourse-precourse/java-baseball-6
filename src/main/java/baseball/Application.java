baeff290703eed7f65f0e456d0b2d95f5d4ef82d
Uniaut <kunwoo0927@gmail.com>
2023-10-21 00:17:39 +0900

    feat: added prints on main, askRestart, askQuery

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -15,7 +15,7 @@ class Game {
             int[] result = this.judge(query);
             System.out.println(result[0] + "스트라이크 " + result[1] + "볼");
             if (result[0] == 3) {
-                System.out.println("게임 종료");
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 break;
             }
         }
@@ -23,6 +23,7 @@ class Game {
 
     int askQuery() {
         // TODO: Add Query Validation
+        System.out.print("숫자를 입력해주세요 : ");
         int query = Integer.parseInt(Console.readLine());
         return query;
     }
@@ -71,13 +72,13 @@ class Game {
 
 public class Application {
     static boolean askRestart() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String inputString = Console.readLine();
         return inputString.equals("1");
     }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
         while (true) {
-            System.out.println("숫자야구 게임을 시작합니다.");
             Game game = new Game(123);
             game.run();
             if (!askRestart())
