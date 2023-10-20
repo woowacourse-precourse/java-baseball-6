fc7551c0058791acfb0a60791d881d9f7922081a
Jieon <dlwldjs7544@naver.com>
2023-10-20 20:51:02 +0900

    feat: 게임 종료 메시지 출력 기능 구현

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -47,6 +47,8 @@ public class GameController {
 
             printHint();
         }
+
+        outputView.printEndMessage();
     }
 
     public List<Integer> getRandomNumbers() {
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -2,6 +2,7 @@ package baseball.view;
 
 public class OutputView {
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     public void printStartMessage() {
         System.out.println(GAME_START_MESSAGE);
@@ -9,5 +10,9 @@ public class OutputView {
 
     public void printHindMessage(String hintMessage) {
         System.out.println(hintMessage);
+    }
+
+    public void printEndMessage() {
+        System.out.println(GAME_END_MESSAGE);
     }
 }
