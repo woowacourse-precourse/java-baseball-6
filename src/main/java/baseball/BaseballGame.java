5429dd2945f037b23495a6275eea50c0334e7c85
Heojamong <wlaud0823@naver.com>
2023-10-20 22:50:21 +0900

    [기능추가]게임이 종료된 이후 게임을 새로 시작할지 종료할지 입력받는 selectNewGame 메소드 생성, 입력받은 값에따라 boolean 값을 리턴해주는 gameOver 메소드 생성

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class BaseballGame {
     private int pointer, strike, ball;
+    private String isPlaying;
     List<Integer> result = new ArrayList<>();
 
     public void strikeZone(User user, Computer computer) {
@@ -21,33 +22,43 @@ public class BaseballGame {
             if (computer.getRandomComputerPickNum().contains(value)) {
                 ball++;
                 pointer++;
-                continue;
             }
         }
         result.add(ball);
         result.add(strike);
     }
 
-    public boolean printGameResult(List<Integer> result) {
+    public void printGameResult(List<Integer> result) {
         if (result.get(1) == 3) {
             System.out.println(result.get(1) + "스트라이크");
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            return true;
+            selectNewGame();
         }
         if (result.get(0) != 0 && result.get(1) != 0) {
             System.out.println(result.get(0) + "볼 " + result.get(1) + "스트라이크");
-            return false;
         }
         if (result.get(0) != 0 && result.get(1) == 0) {
             System.out.println(result.get(0) + "볼");
-            return false;
         }
         if (result.get(0) == 0 && result.get(1) != 0) {
             System.out.println(result.get(1) + "스트라이크");
-            return false;
         }
         if (result.get(0) == 0 && result.get(1) == 0) {
             System.out.println("낫싱");
+        }
+    }
+
+    public void selectNewGame() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+        isPlaying = camp.nextstep.edu.missionutils.Console.readLine();
+        gameOver();
+    }
+
+    public boolean gameOver() {
+        if (isPlaying.equals("1")) {
+            return true;
+        }
+        if (isPlaying.equals("2")) {
             return false;
         }
         return false;
