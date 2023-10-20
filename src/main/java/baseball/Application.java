ead0cd42cbd58aaa0de44a94c96fe82356a61b8d
PJH <parkjh0858@gmail.com>
2023-10-20 13:52:30 +0900

    docs: 기능목록 구현 완료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static void main(String[] args){
+        String test = Console.readLine();
+        System.out.println(test);
+
+    }
+
+    public int makeNumber() {
+        StringBuilder sb = new StringBuilder();
+        Set<Integer> set = new HashSet<>();
+        Console.readLine();
+
+        return 0;
+
     }
 }
