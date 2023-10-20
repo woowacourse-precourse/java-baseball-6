c5167567b646bf9031ecefbbfabfa58e42e91189
TaeHye0n <thk001229@gmail.com>
2023-10-20 21:12:58 +0900

    feat: 게임 시작 후 숫자 입력을 요구하는 메시지 출력

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,8 +3,13 @@ package baseball.view;
 public class OutputView {
 
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String GAME_INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
 
     public void printStartMessage() {
         System.out.println(GAME_START_MESSAGE);
+    }
+
+    public void printInputNumberMessage() {
+        System.out.println(GAME_INPUT_NUMBER_MESSAGE);
     }
 }
