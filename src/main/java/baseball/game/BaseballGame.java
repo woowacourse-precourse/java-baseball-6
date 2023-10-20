88ac1059827a9641bb4b5e67cf73d1c181273ae3
wwwaas0 <bjm040364@gmail.com>
2023-10-20 21:52:43 +0900

    [feat] 컴퓨터 숫자와 사용자 숫자를 비교하고 결과 출력

--- src/main/java/baseball/game/BaseballGame.java
+++ src/main/java/baseball/game/BaseballGame.java
@@ -1,6 +1,47 @@
 package baseball.game;
 
-public class BaseballGame {
-    //입력 값 받았을 때 computer, player의 값 비교, 출력
+import java.util.List;
 
+public class BaseballGame {
+    /**
+     * 컴퓨터 숫자와 사용자 입력 숫자를 비교한다.
+     * @param computers 컴퓨터 랜덤 숫자
+     * @param players 사용자 입력 숫자
+     */
+    public static void compare(List<Integer> computers, List<Integer> players) {
+        int strike = 0;
+        int duplicateCount = 0;
+        int ball;
+
+        //각 자리수별로 일치한지
+        for (int i = 0; i < 3; i++) {
+            if (computers.get(i) == players.get(i)) {
+                strike++;
+            }
+            if (players.contains(computers.get(i))) {
+                duplicateCount++;
+            }
+        }
+
+        ball = duplicateCount - strike;
+        printCompare(strike, ball);
+    }
+
+    /**
+     * 비교한 결과를 출력한다.
+     * @param strike 스크라이크의 개수
+     * @param ball 볼의 개수
+     */
+    public static void printCompare(int strike, int ball) {
+        if (strike == 0 && ball == 0) {
+            System.out.print("낫싱");
+        }
+        if (ball != 0) {
+            System.out.print(ball + "볼 ");
+        }
+        if (strike != 0) {
+            System.out.print(strike + "스트라이크");
+        }
+        System.out.println("");
+    }
 }
--- src/main/java/baseball/game/GameManager.java
+++ src/main/java/baseball/game/GameManager.java
@@ -1,7 +1,15 @@
 package baseball.game;
 
+import baseball.computer.Computer;
+import baseball.player.Player;
+
 public class GameManager {
+    Computer computer = new Computer();
+    Player player = new Player();
+    BaseballGame baseballGame = new BaseballGame();
+
     //게임 시작
     //3스트라이크 될때까지 반복 시키는 애
     //종료, 다시 시작 결정
+
 }
