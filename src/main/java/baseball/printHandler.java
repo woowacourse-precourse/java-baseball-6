c19aa049479d7d57c58a1082e44eda0be747b50e
tfer2442 <tfer2442@gmail.com>
2023-10-21 00:32:16 +0900

    feat(printHandler): programEnd 메서드, 프로그램 종료 메시지 출력 구현

--- src/main/java/baseball/printHandler.java
+++ src/main/java/baseball/printHandler.java
@@ -9,6 +9,10 @@ public class printHandler {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
+    public static void programEnd() {
+        System.out.println("프로그램 종료");
+    }
+
     public static void choiceEndOrRePrint(){
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
@@ -16,7 +20,6 @@ public class printHandler {
     public static void baseballPrint(int ball, int strike) {
         if (strike == 3) {
             System.out.println(strike+"스트라이크");
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         } else if ((ball > 0) && (strike > 0)) {
             System.out.println(ball + "볼 " + strike + "스트라이크");
         } else if ((ball > 0) && (strike == 0)) {
