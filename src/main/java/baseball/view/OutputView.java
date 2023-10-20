cad85e919e5876c770443852cf9cd4eadb7f7280
kyeoungchan <wkc1002@naver.com>
2023-10-20 18:32:59 +0900

    feat: 게임 시작 멘트 및 결과에 따른 출력 기능 구현

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,20 @@
+package baseball.view;
+
+import baseball.domain.Result;
+
+public class OutputView {
+    public void startMention() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void printResult(Result result) {
+        if (result.isNothing()) {
+            System.out.println("낫싱");
+            return;
+        } else if (result.isAllStrike()) {
+            System.out.println("3스트라이크");
+            return;
+        }
+        System.out.printf("%d볼 %d스트라이크%n", result.getBallCount(), result.getStrikeCount());
+    }
+}
