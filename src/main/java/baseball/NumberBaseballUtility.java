d84bf4872acd0039c0e905442fac80f9b0c4d2d9
useu <lakingsung@gmail.com>
2023-10-20 23:19:46 +0900

    feat: isUniqueThreeDigitNumber 메서드 작성

--- src/main/java/baseball/NumberBaseballUtility.java
+++ src/main/java/baseball/NumberBaseballUtility.java
@@ -3,7 +3,9 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class NumberBaseballUtility {
     public static List<Integer> generateAnswer() {
@@ -15,5 +17,22 @@ public class NumberBaseballUtility {
             }
         }
         return answer;
+    }
+
+    public static boolean isUniqueThreeDigitNumber(String input) {
+        if (input.length() != 3) {
+            return false;
+        }
+        Set<Character> seen = new HashSet<>();
+        for (char c : input.toCharArray()) {
+            if (c < '1' || c > '9') {
+                return false;
+            }
+            if (seen.contains(c)) {
+                return false;
+            }
+            seen.add(c);
+        }
+        return true;
     }
 }
