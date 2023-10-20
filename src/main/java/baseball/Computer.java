b2e46154421add92f55b66453c488a6be65caf51
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2023-10-20 21:29:02 +0900

    Init: Implemented Random number generator

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,20 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    public static List<Integer> computer(){
+        List<Integer> answer = new ArrayList<>();
+        while (answer.size() < 3){
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!answer.contains(randomNumber)){
+                answer.add(randomNumber);
+            }
+        }
+        return answer;
+    }
+}
+
