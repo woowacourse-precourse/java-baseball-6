eac4cdf74d60995c11852bb2c2a0d156e7fc8f2e
junOTL <naqp99@gmail.com>
2023-10-20 23:42:58 +0900

    test: 빈값 입력 예외 테스트

---
+++ src/test/java/baseball/view/InputViewTest.java
@@ -0,0 +1,18 @@
+package baseball.view;
+
+import baseball.util.Validation;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class InputViewTest {
+    @ParameterizedTest
+    @DisplayName("빈값 입력 예외 테스트")
+    @ValueSource(strings = {"", " ", "  ", "   "})
+    void invalidInputsTest1(String input) {
+        Assertions.assertThatThrownBy(() -> Validation.isEmpty(input)).
+                isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("값을 입력해 주세요.");
+    }
+}
