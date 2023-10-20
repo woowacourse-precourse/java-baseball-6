4ad037cbb57196e10b0721b55668761ff17c301c
hosung-222 <gik11kr@naver.com>
2023-10-20 20:32:44 +0900

    feat : 종료 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,13 +1,25 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
 
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        GameController game = new GameController();
-        game.startGame();
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        while (true) {
+            System.out.println("숫자 야구 게임을 시작합니다.");
+            GameController game = new GameController();
+            game.startGame();
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
+            // 종료 기능
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            int nextGame = Integer.parseInt(Console.readLine());
+            if (nextGame == 2) {
+                break;
+            } else if (nextGame != 1) {
+                throw new IllegalArgumentException("입력 오류");
+            }
+        }
     }
 
 }
