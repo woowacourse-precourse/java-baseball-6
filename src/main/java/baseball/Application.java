ad588b9784ed391f5b35e115e5fe3094d3dfa2ad
Uniaut <kunwoo0927@gmail.com>
2023-10-20 22:46:42 +0900

    feat: Added class Game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,17 @@
 package baseball;
 
+class Game {
+    public int answer;
+    Game(int answer) {
+        this.answer = answer;
+    }
+
+    void run() {
+        // TODO: implement
+    }
+}
+
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
