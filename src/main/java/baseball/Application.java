51746e9b67ed23ca80623fc4e3c166fd3a0af561
joo-sang-ah <jsa@interiorteacher.com>
2023-10-20 11:55:54 +0900

    feat: 랜덤 숫자 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO: 1부터 9까지 서로 다른 수로 이루어진 3자리의 수 생성
+        List<Integer> computer = generateRandom(3);
+    }
+
+    private static List<Integer> generateRandom(Integer length) {
+        List<Integer> result = new ArrayList<>();
+        while (result.size() < length) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!result.contains(randomNumber)) {
+                result.add(randomNumber);
+            }
+        }
+
+        return result;
     }
 }
