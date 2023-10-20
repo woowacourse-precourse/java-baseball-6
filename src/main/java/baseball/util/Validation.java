faaf0eec912d79ed9f7ca5e3cc34323bb43387ef
김효준 <khyojun1104@gmail.com>
2023-10-20 21:55:39 +0900

    [fix] Validation : 정규식 올바르게 수정!

--- src/main/java/baseball/util/Validation.java
+++ src/main/java/baseball/util/Validation.java
@@ -2,29 +2,27 @@ package baseball.util;
 
 public class Validation {
 
-    private static final String NUMBER_REGEX = "^[1-9]$";
+    private static final String NUMBER_REGEX = "^[1-9]*$";
     private static final Integer INPUT_LENGTH = 3;
 
-    public boolean validateNumber(String number) {
-        if(isRightNumber(number))
-            return true;
-        throw new IllegalArgumentException("Error");
+    public String validateNumber(String number) {
+        checkRightNumber(number);
+        return number;
     }
 
-    private boolean isRightNumber(String number) {
-        return validateIsNumber(number) && validateIsRightLength(number);
+    private void checkRightNumber(String number) {
+        validateIsNumber(number);
+        validateIsRightLength(number);
     }
 
-    private boolean validateIsNumber(String number) {
-        if(number.matches(NUMBER_REGEX))
-           return true;
-        throw new IllegalArgumentException("숫자로 입력이 되어야 합니다!");
+    private void validateIsNumber(String number) {
+        if(!number.matches(NUMBER_REGEX))
+           throw new IllegalArgumentException("숫자로 입력이 되어야 합니다!");
     }
 
-    private boolean validateIsRightLength(String number) {
-        if(number.length() == INPUT_LENGTH)
-            return true;
-        throw new IllegalArgumentException("입력 숫자의 길이는 3입니다!");
+    private void validateIsRightLength(String number) {
+        if(number.length() != INPUT_LENGTH)
+            throw new IllegalArgumentException("입력 숫자의 길이는 3입니다!");
     }
 
 
