dcff0eb17fa8305b7d97250811ddf46256765cbe
yummygyudon <bang2brew@gmail.com>
2023-10-20 23:33:13 +0900

    FEAT : Command 메시지 관리 Enum 구현

---
+++ src/main/java/baseball/util/view/Command.java
@@ -0,0 +1,18 @@
+package baseball.util.view;
+
+public enum Command {
+    START_GAME("숫자 야구 게임을 시작합니다.\n"),
+    ASK_PLAYER_NUMBER("숫자를 입력해주세요 : "),
+    END_GAME("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"),
+    ASK_RESUME("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n")
+    ;
+    private final String command;
+
+    Command(String command) {
+        this.command = command;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+}
