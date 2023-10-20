8613ff1db0bb845c72999ba02de15128fab7477d
박승찬 <seungchan141414@gmail.com>
2023-10-20 14:09:40 +0900

    [Test(baseball)] Judge#baseballGameInspectionTest TDD 작성
    유저가 입력한 3개의 숫자와 컴퓨터가 랜덤으로 저장한 3개의 숫자를 확인하여 결과를 알려준다.
    
    유저가 입력한 숫자 3개중 컴퓨터에 같은 수가 다른 자리에있으면 볼
    유저가 입력한 숫자 3개중 컴퓨터에 같은 수가 같은 자리에있으면 스트라이크
    같은 수가 전혀 없으면 낫싱이다.

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import baseball.domain.GameStandard;
+import baseball.domain.Judge;
+import baseball.domain.NumbersGenerate;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -9,6 +17,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+    GameStandard gameStandard;
+    Judge judge;
+
+
+    @BeforeEach
+    void setUp() {
+        gameStandard = new GameStandard();
+        judge = new Judge();
+    }
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
@@ -31,5 +49,57 @@ class ApplicationTest extends NsTest {
     @Override
     public void runMain() {
         Application.main(new String[]{});
+    }
+
+    @DisplayName("심판의 유저의 게임 진행 상황 확인, 볼, 스트라이크, 낫싱")
+    @Test
+    public void baseballGameInspectionTest() throws Exception {
+        // given
+        List<Integer> computerList = new ArrayList<>(List.of(1, 2, 3));
+        List<List<Integer>> numInputUser = new ArrayList<>(List.of(
+                // 3스트라이크
+                List.of(1, 2, 3),
+                // 2스트라이크
+                List.of(1, 2, 4),
+                // 2볼 1스트라이크
+                List.of(1, 3, 2),
+                // 1볼 1스트라이크
+                List.of(1, 8, 2),
+                // 1스트라이크
+                List.of(1, 8, 4),
+                // 1볼
+                List.of(4, 1, 6),
+                // 2볼
+                List.of(7, 1, 2),
+                // 3볼
+                List.of(3, 1, 2),
+                // 낫싱
+                List.of(7, 9, 8)
+        ));
+
+        List<String> hints = new ArrayList<>(
+                List.of(
+                        "3스트라이크",
+                        "2스트라이크",
+                        "2볼 1스트라이크",
+                        "1볼 1스트라이크",
+                        "1스트라이크",
+                        "1볼 ",
+                        "2볼 ",
+                        "3볼 ",
+                        "낫싱"
+                )
+        );
+        String resultHint = "";
+        List<String> hintResult = new ArrayList<>();
+
+        // when
+        for (List<Integer> numList : numInputUser) {
+            resultHint = judge.baseballGameInspection(computerList, numList);
+            hintResult.add(resultHint);
+        }
+
+        // then
+        assertThat(hintResult).containsAll(hints);
     }
 }
