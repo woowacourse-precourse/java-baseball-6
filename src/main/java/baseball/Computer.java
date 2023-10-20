92d86098f4c09fb63aa5a61dcb50216145cb9856
sohyun9527 <sohyun95277@gmail.com>
2023-10-20 15:33:28 +0900

    fix(Computer): 변수명 수정 ball이라는 변수로 들어가서 ball이 2개씩 증가.. 상수로 선언해둔 BALL로 변수명 수정

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -11,12 +11,13 @@ public class Computer {
     public List<Integer> countScore(List<Integer> userNumbers) {
         int ball = 0;
         int strike = 0;
+
         for (int i = 0; i < userNumbers.size(); i++) {
             int score = judgeScore(userNumbers.get(i), i);
             if (score == STRIKE) {
                 strike++;
             }
-            if (score == ball) {
+            if (score == BALL) {
                 ball++;
             }
         }
@@ -25,9 +26,17 @@ public class Computer {
 
     public int judgeScore(int number, int index) {
         if (answer.get(index) == number) {
+            System.out.println("-------------in strike");
+            System.out.println("answer.get(index) = " + answer.get(index));
+            System.out.println("index = " + index);
+            System.out.println("number = " + number);
             return (STRIKE);
         }
         if (answer.contains(number)) {
+            System.out.println("-------------in ball");
+            System.out.println("answer.get(index) = " + answer.get(index));
+            System.out.println("index = " + index);
+            System.out.println("number = " + number);
             return (BALL);
         }
         return (NOTHING);
@@ -37,4 +46,7 @@ public class Computer {
         this.answer = answer;
     }
 
+    public List<Integer> getAnswer() {
+        return answer;
+    }
 }
