0fc35c569dc374391d53923ea5ef85f3ce295306
gunwoong <gwj0421@gmail.com>
2023-10-20 21:35:14 +0900

    feat : 핵심 알고리즘 및 구현할 기능 순서 1("숫자 야구 게임을 시작합니다." 출력) & 2번(컴퓨터가 서로 다른 임의의 수 3개를 선택) 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,14 +1,42 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static class BaseBallGame {
-        public BaseBallGame() {
+        private List<Integer> computerNumbers;
 
+        public BaseBallGame() {
+            this.computerNumbers = makeRandomNumbers();
         }
+
+        public List<Integer> getComputerNumbers() {
+            return computerNumbers;
+        }
+
+        private List<Integer> makeRandomNumbers() {
+            List<Integer> computer = new ArrayList<>();
+            while (computer.size() < 3) {
+                int randomNumber = Randoms.pickNumberInRange(1, 9);
+                if (!computer.contains(randomNumber)) {
+                    computer.add(randomNumber);
+                }
+            }
+            return computer;
+        }
+
+        public void playGame() {
+            System.out.println("숫자 야구 게임을 시작합니다.");
+        }
+
     }
 
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseBallGame baseBallGame = new BaseBallGame();
+        baseBallGame.playGame();
     }
 }
