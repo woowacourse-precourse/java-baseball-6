be311d1a3cc4cbf86e4470c91f0f65dec1723b6c
noxknow <chris2769@naver.com>
2023-10-20 22:49:58 +0900

    feat(ConsoleOutput) : 게임 시작 문구를 출력하는 메서드 구현

---
+++ src/main/java/baseball/view/ConsoleOutput.java
@@ -0,0 +1,16 @@
+package baseball.view;
+
+import baseball.handler.OutputHandler;
+
+public class ConsoleOutput implements OutputHandler {
+
+    @Override
+    public void printGameStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    @Override
+    public void printGameResult() {
+
+    }
+}
