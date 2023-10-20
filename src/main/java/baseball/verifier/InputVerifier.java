bc4b4ec38deae4e6574aeaa734671700dcfa39fe
정성욱 <rkfka7849@gmail.com>
2023-10-20 18:46:55 +0900

    feat(InputVerifier): 입력값이 123~ 987 사이의 수인지 확인하는 기능 구현

--- src/main/java/baseball/verifier/InputVerifier.java
+++ src/main/java/baseball/verifier/InputVerifier.java
@@ -5,8 +5,9 @@ import baseball.system.SystemException;
 public class InputVerifier {
     public void checkBallNumber(String isBallNumber) throws IllegalArgumentException {
         checkNumeric(isBallNumber);
-
+        checkRange(isBallNumber);
     }
+
     private void checkNumeric(String isNumeric) {
         try {
             Integer.parseInt(isNumeric);
@@ -15,4 +16,9 @@ public class InputVerifier {
         }
     }
 
+    private void checkRange(String inRange) {
+        if (Integer.parseInt(inRange) < 123 || 987 < Integer.parseInt(inRange)) {
+            throw new IllegalArgumentException(SystemException.EXCEPTION_RANGE);
+        }
+    }
 }
