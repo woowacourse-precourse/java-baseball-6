518c4944805c090489046e2e635405bf777b4181
WonyuChoi <phillip07290@naver.com>
2023-10-20 19:14:11 +0900

    refactor: 필요 없어진 상수 삭제

--- src/main/java/baseball/model/GuessResult.java
+++ src/main/java/baseball/model/GuessResult.java
@@ -5,9 +5,6 @@ import static baseball.Constants.NUMBER_COUNTS;
 public class GuessResult {
 
     private static final int INITIAL_ZERO_COUNT = 0;
-    private static final String NOTHING_RESULT = "낫싱";
-    private static final String BALL_RESULT = "볼";
-    private static final String STRIKE_RESULT = "스트라이크";
 
     private int strikeCount = INITIAL_ZERO_COUNT;
     private int ballCount = INITIAL_ZERO_COUNT;
