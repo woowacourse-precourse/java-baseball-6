28d861d74cd27d8a92cab3773694b33a94e7798d
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2023-10-20 13:23:28 +0900

    feat: create user input function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,18 +1,38 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Application {
+    static final int NUM_OF_DIGITS = 3;
+
     public static void main(String[] args) {
         List<Integer> computer = createRandomNumbers();
-        System.out.println(computer);
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        int[] userNumberDigits = new int[NUM_OF_DIGITS];
+        getUserInput(userNumberDigits);
+        System.out.println(Arrays.toString(userNumberDigits));
     }
 
-    private static List<Integer> createRandomNumbers() {
+    private static void getUserInput(int[] userNumberDigits) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("숫자를 입력해주세요 : ");
+        int userNumber = scanner.nextInt();
+        for (int i = NUM_OF_DIGITS - 1; i >= 0; i--) {
+            int filteredNum = (int) (userNumber % Math.pow(10, i + 1));
+            int digit = filteredNum / (int) Math.pow(10, i);
+            int index = NUM_OF_DIGITS - 1 - i;
+            userNumberDigits[index] = digit;
+        }
+    }
+
+    public static List<Integer> createRandomNumbers() {
         List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
+        while (computer.size() < NUM_OF_DIGITS) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
