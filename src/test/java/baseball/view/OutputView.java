bf4504d389a7148d24fe3dad84aea64f87fdedfe
leeedohyun <ldh19738@gmail.com>
2023-10-20 10:31:15 +0900

    feat: 숫자를 입력받는 문구 출력하는 기능 구현

--- src/test/java/baseball/view/OutputView.java
+++ src/test/java/baseball/view/OutputView.java
@@ -3,8 +3,13 @@ package baseball.view;
 public class OutputView {
 
     private static final String BASEBALL_GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String INPUTTING_NUMBERS_MESSAGE = "숫자를 입력해주세요 : ";
 
     public static void printBaseballGameStartMessage() {
         System.out.println(BASEBALL_GAME_START_MESSAGE);
+    }
+
+    public static void printMessageToInputNumbers() {
+        System.out.print(INPUTTING_NUMBERS_MESSAGE);
     }
 }
