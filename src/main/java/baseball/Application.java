3c13325e07273da4d923883a70c3b10745f47679
DohunHyun <dohun.hyun@gmail.com>
2023-10-20 13:09:33 +0900

    Fix : 입력 방식 변경
    입력 방식을 제공된 라이브러리인 Console의 readline() 사용.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -23,23 +26,20 @@ public class Application {
                 // 입력 : 1~9 숫자 중 3가지로 이루어진 세자리 숫자.
                 // 입력 시 같은 숫자는 사용 금지.
                 // 입력 시 숫자만 입력 가능.
-                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                StringTokenizer st;
-                try {
-                    st = new StringTokenizer(br.readLine(), "");
-                    int[] inputArray = new int[3];
-                    boolean[] checkList = new boolean[9];
-                    for (int i=0; i<inputArray.length; i++) {
-                        inputArray[i] = Integer.parseInt(st.nextToken()); // 입력 값이 숫자인지 체크
-                        if(!checkList[inputArray[i]-1]) {
-                            checkList[inputArray[i]-1] = true;
-                        } else { // 중복된 숫자 체크.
-                            throw new IllegalArgumentException();
-                        }
+                String input = Console.readLine();
+                String[] inputArray = input.split("");
+                int[] inputToIntArray = new int[3];
+                boolean[] checkList = new boolean[9];
+
+                for (int i=0; i<inputArray.length; i++) {
+                    inputToIntArray[i] = Integer.parseInt(inputArray[i]); // 입력 값이 숫자인지 체크
+                    if(!checkList[inputToIntArray[i]-1]) {
+                        checkList[inputToIntArray[i]-1] = true;
+                    } else { // 중복된 숫자 체크.
+                        throw new IllegalArgumentException();
                     }
-                } catch (IOException e) {
-                    throw new IllegalArgumentException(e);
                 }
+
 
                 // 입력 후 판단.
                 // 구현 필요.
@@ -47,18 +47,13 @@ public class Application {
                 // 정답이라면 while문 break.
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                try {
-                    st = new StringTokenizer(br.readLine());
-                    if(Integer.parseInt(st.nextToken()) == 2) { // 게임 종료
-                        endSign = 2;
-					} else { // 이번 게임은 끝. 다음 게임 다시 시작.
-                        endSign = 1;
-					}
-					isCorrect = true;
-				} catch (IOException e) {
-                    throw new IllegalArgumentException(e);
+                int inputInt = Integer.parseInt(Console.readLine());
+                if(inputInt == 2) { // 게임 종료
+                    endSign = 2;
+                } else { // 이번 게임은 끝. 다음 게임 다시 시작.
+                    endSign = 1;
                 }
-
+                isCorrect = true;
             }
 
         }
