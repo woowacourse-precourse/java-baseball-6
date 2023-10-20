11daaf18a38ca4a3af424d6193bd43a2051d83db
dy102 <132787602+dy102@users.noreply.github.com>
2023-10-20 22:36:12 +0900

    Refactor: 매직넘버를 상수로 교체

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -8,13 +8,19 @@ import java.util.List;
 public class Computer {
     private List<Integer> randomNumbers = new ArrayList<>();
 
+    private static final int THREE_NUMBER = 3;
+
+    private static final int MINIMUM_NUMBER = 1;
+
+    private static final int MAXIMUM_NUMBER = 9;
+
     public List<Integer> getRandomNumbers() {
         return randomNumbers;
     }
 
     public Computer() {
-        while (randomNumbers.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+        while (randomNumbers.size() < THREE_NUMBER) {
+            int randomNumber = Randoms.pickNumberInRange(MINIMUM_NUMBER, MAXIMUM_NUMBER);
             if (!randomNumbers.contains(randomNumber)) {
                 randomNumbers.add(randomNumber);
             }
