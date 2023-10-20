8a96f89333bc749013fb88b68076b9330d7bb27f
Arachneee <jhg2819@naver.com>
2023-10-20 16:09:57 +0900

    refactor: Result 결과 문자 toString 으로 리펙토링

--- src/main/java/baseball/domain/Result.java
+++ src/main/java/baseball/domain/Result.java
@@ -6,6 +6,10 @@ public class Result {
     private Integer ball;
     private Integer strike;
 
+    private static final String STRIKE_STRING = "스트라이크";
+    private static final String BALL_STRING = "볼";
+    private static final String NON_STRING = "낫싱";
+
     private Result() {
     }
 
@@ -17,31 +21,68 @@ public class Result {
         return result;
     }
 
-    public boolean isFinish() {
-        return strike.equals(NUMBER_COUNT);
-    }
-
-    public boolean isContinue() {
-        return !strike.equals(NUMBER_COUNT);
-    }
-
-    public boolean isNon() {
-        return ball.equals(0) && strike.equals(0);
-    }
-
-    public Integer getBall() {
-        return ball;
-    }
-
-    public Integer getStrike() {
-        return strike;
-    }
-
     private void setBall(Integer ball) {
         this.ball = ball;
     }
 
     private void setStrike(Integer strike) {
         this.strike = strike;
+    }
+
+    /*
+     * 출력 예시 :
+     *  3스트라이크
+     *  1볼 2스트라이크
+     *  1볼
+     *  낫싱
+     */
+    @Override
+    public String toString() {
+        if (isNon()) {
+            return NON_STRING;
+        }
+
+        return getBallStrikeString();
+    }
+
+    private String getBallStrikeString() {
+        String resultString = "";
+
+        if (isBallPresent()) {
+            resultString += getBallString();
+        }
+
+        if (isStrikePresent()) {
+            if (isBallPresent()) {
+                resultString += " ";
+            }
+            resultString += getStrikeString();
+        }
+
+        return resultString;
+    }
+
+    private boolean isNon() {
+        return !isStrikePresent() && !isBallPresent();
+    }
+
+    private String getStrikeString() {
+        return strike + STRIKE_STRING;
+    }
+
+    private String getBallString() {
+        return ball + BALL_STRING;
+    }
+
+    private boolean isStrikePresent() {
+        return !strike.equals(0);
+    }
+
+    private boolean isBallPresent() {
+        return !ball.equals(0);
+    }
+
+    public boolean isContinue() {
+        return !strike.equals(NUMBER_COUNT);
     }
 }
--- src/main/java/baseball/view/ConsolePrint.java
+++ src/main/java/baseball/view/ConsolePrint.java
@@ -9,9 +9,6 @@ public class ConsolePrint {
     private static final String NUMBER_INPUT_STRING = "숫자를 입력해주세요 : ";
     private static final String FINISH_INPUT_STRING = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
             "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-    private static final String STRIKE_STRING = "스트라이크";
-    private static final String BALL_STRING = "볼";
-    private static final String NON_STRING = "낫싱";
 
     private ConsolePrint() {
     }
@@ -29,22 +26,6 @@ public class ConsolePrint {
     }
 
     public static void printResult(Result result) {
-        if (result.isFinish()) {
-            System.out.println(getNoBallString(result));
-            return;
-        }
-        if (result.isNon()) {
-            System.out.println(NON_STRING);
-            return;
-        }
-        System.out.println(getBallStrikeString(result));
-    }
-
-    private static String getBallStrikeString(Result result) {
-        return result.getBall() + BALL_STRING + " " + result.getStrike() + STRIKE_STRING;
-    }
-
-    private static String getNoBallString(Result result) {
-        return result.getStrike() + STRIKE_STRING;
+        System.out.println(result);
     }
 }
