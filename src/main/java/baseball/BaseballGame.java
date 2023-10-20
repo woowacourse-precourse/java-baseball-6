5933b5f72ce499a1d70046d9abc086de9f923d7d
scarlet0o0 <ghd7673@naver.com>
2023-10-21 03:35:09 +0900

    Refactor : proses 메소드 간결화

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -4,35 +4,36 @@ import baseball.view.InputView;
 import baseball.view.OutView;
 
 public class BaseballGame {
+    private final Computer computer = new Computer();
 
     public void start() {
         do{
             OutView.gameStartPrint();
-            proses(new Computer());
+            proses();
             OutView.endGamePrint();
         }while (confirmGameRestart());
     }
 
     //게임 진행
-    private void proses(Computer computer) {
-        int strikeCount;
+    private void proses() {
+        BallArray ballArray;
+        computer.setRandomBallArray();
         do {
             OutView.numberInputPrint();
-            int number = InputView.promptForThreeNumbers();
-            Ball ball = new Ball(number);
-            BallResult userBallResult = computer.getUserBallResult(ball);
-            OutView.ballScoringResultsPrint(userBallResult);
-            strikeCount = userBallResult.getStrike();
-        } while (isGameOverWithStrikeCount(strikeCount)); //3스트라이크 일 경우 종료
+            int number = InputView.promptForNumbers();
+            ballArray = BaseballBallFactory.createManualBall(number);
+        } while (isGameOverWithStrikeCount(ballArray)); //3스트라이크 일 경우 종료
     }
 
     //게임 재시작 여부
     private boolean confirmGameRestart() {
         OutView.restartGamePrint();
-        return InputView.promptForOneNumbers() == 1;
+        return InputView.promptForNumbers() == 1;
     }
 
-    private boolean isGameOverWithStrikeCount(int strikeCount) {
-        return strikeCount < 3;
+    private boolean isGameOverWithStrikeCount(BallArray ballArray) {
+        BallResult userBallResult = computer.getUserBallResult(ballArray);
+        OutView.ballScoringResultsPrint(userBallResult);
+        return userBallResult.getStrike() < 3;
     }
 }
