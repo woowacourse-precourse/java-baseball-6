3ca2ea9a177fb9a93e280253c3f12537f5972f9b
BullChallenger <am23am45@naver.com>
2023-10-20 12:08:04 +0900

    [FEAT] View 클래스 생성
    View 클래스의 책임은 게임 진행 중 발생하는 메세지를 화면에 표시해주는 것과 플레이어가 입력한 값을 Controller 에 전달해주는 것, 두 가지의 책임을 가진다

---
+++ src/main/java/baseball/view/View.java
@@ -0,0 +1,30 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class View {
+
+    private static final String START_MESSAGE = "숫자야구를 시작합니다.";
+    private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String ALL_STRIKE_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String RETRY_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public static void printStartMessage() {
+        System.out.println(START_MESSAGE);
+    }
+
+    public static String input() {
+        System.out.print(INPUT_MESSAGE);
+        return Console.readLine();
+    }
+
+    public static String retry() {
+        System.out.println(ALL_STRIKE_MESSAGE + "\n" + RETRY_MESSAGE);
+        return Console.readLine();
+    }
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+
+}
