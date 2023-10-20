47702c685440d1354eade549d98a4d42ffc7fa29
acs0209 <annjae4676@naver.com>
2023-10-20 19:41:47 +0900

    feat: implement game control logic
    게임을 제어하는 로직을 구현

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,20 @@
+package baseball;
+
+import baseball.controller.GameController;
+import baseball.domain.GameInfo;
+import baseball.service.GameService;
+
+public class BaseballGame {
+
+    private final GameController gameController = new GameController();
+    private final GameService gameService = new GameService();
+
+    public void run() {
+        GameInfo gameInfo = new GameInfo();
+        gameInfo.createRandomNumber(gameService.generateRandomDistinctThreeDigit());
+        while (gameInfo.getStrike() < 3) {
+            String userInput = gameController.getUserInput();
+            gameService.countStrikeOrBall(gameInfo, userInput);
+        }
+    }
+}
