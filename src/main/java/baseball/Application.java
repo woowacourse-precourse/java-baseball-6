3c040c93e5e416467f5e4de973b3638ba74cc46f
박형균 <phk@baghyeong-gyun-ui-MacBookPro.local>
2023-10-20 16:12:03 +0900

    feat: implement

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import baseball.controller.BaseballController;
+
 public class Application {
     public static void main(String[] args) {
+        BaseballController baseballController = new BaseballController();
+        baseballController.run();
         // TODO: 프로그램 구현
     }
 }
