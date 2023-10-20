8c3b15b694ce605b93901ddc9cf5abca83a668c1
junOTL <naqp99@gmail.com>
2023-10-20 20:49:40 +0900

    feat: 컴퓨터는 1에서 9까지 서로 다른 임의의 수 3개를 생성 기능 구현

---
+++ src/main/java/baseball/domain/Computer.java
@@ -0,0 +1,19 @@
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+
+    public List<Integer> getRandomNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+}
