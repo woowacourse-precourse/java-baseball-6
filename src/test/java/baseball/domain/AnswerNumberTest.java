39f60e0d9a4be4b7c8004913be7c8e870f923886
elyudwo <dudqk9696@naver.com>
2023-10-20 19:23:45 +0900

    [Test] : 생성자 예외 처리 테스트 코드 작성
    AnswerNumber 클래스의 생성자로 올바르지않은 숫자 리스트가 들어올 경우 예외를 발생시킨다.

--- src/test/java/baseball/domain/AnswerNumberTest.java
+++ src/test/java/baseball/domain/AnswerNumberTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class AnswerNumberTest {
 
     @DisplayName("올바른 숫자 리스트를 반환한다.")
@@ -20,5 +22,16 @@ class AnswerNumberTest {
 
         // then
         Assertions.assertThat(answerNumberList).containsExactlyInAnyOrderElementsOf(numberList);
+    }
+
+    @DisplayName("생성자로 올바르지 않은 숫자가 들어올 경우 예외가 발생한다.")
+    @Test
+    void answerNumber() {
+        // given
+        List<Integer> numberList = List.of(0,1,2);
+
+        // when, then
+        assertThatThrownBy(() -> new AnswerNumber(numberList))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
