4be2d896a312c427337d98ee1116c64cce7e2639
nhn1030 <shgusska12@gmail.com>
2023-10-20 21:23:58 +0900

    feat: 스트라이크와 볼을 계산하기 위한 메서드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,7 +4,8 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application app = new Application();
+        // 난수 생성 및 플레이어 입력 처리
         int[] playerNumbers = new int[3];
         int[] randomNumbers = new int[3];
 
@@ -18,5 +19,34 @@ public class Application {
             int playerNumber = Integer.parseInt(Console.readLine());
             playerNumbers[i] = playerNumber;
         }
+
+        String result = app.defineStrikesOrBall(randomNumbers, playerNumbers);
     }
+
+    // 스트라이크 & 볼 계산 로직
+    public String defineStrikesOrBall(int[] randomNumbers, int[] playerNumbers) {
+        int strikes = 0;
+        int balls = 0;
+
+        for (int i = 0; i < playerNumbers.length; i++) {
+            if (playerNumbers[i] == randomNumbers[i]) {
+                strikes++;
+            } else if (containsNumber(randomNumbers, playerNumbers[i])) {
+                balls++;
+            }
+        }
+
+        return String.format("%d스트라이크 %d볼", strikes, balls);
+    }
+
+    //
+    private boolean containsNumber(int[] numbers, int target) {
+        for (int number : numbers) {
+            if (number == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
