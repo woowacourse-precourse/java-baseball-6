096cea62534c98669b35d6efc45287bc75741444
LastCow9000 <sys19912002@hanmail.net>
2023-10-20 20:36:15 +0900

    feat: 컴퓨터가 랜덤한 3개의 숫자를 선택하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,28 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Application {
     public static void main(String[] args) {
         startBaseballGame();
     }
     
     public static void startBaseballGame() {
+        Queue<Integer> balls = new LinkedList<>();
+        while (balls.size() < 3) {
+            int ball = Randoms.pickNumberInRange(1, 9);
+            if (balls.contains(ball)) {
+                continue;
+            }
+            balls.add(ball);
+        }
+
+        balls.forEach(System.out::println);
+
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 }
