b0c118b2b2667a4973ea463e04a06567963e2882
ybkim <ybkim37@gmail.com>
2023-10-20 14:52:27 +0900

    v5

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,5 @@
 package baseball;
 
-
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
@@ -11,31 +10,23 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
         while (true) {
-            List<Integer> computerNumbers = RandNum();
+            List<Integer> computerNumbers = generateComputerNumbers();
 
             boolean gameWon = false;
-            int attempts = 0;
 
             while (!gameWon) {
                 int[] userNumbers = getUserGuess();
                 int[] result = checkGuess(userNumbers, computerNumbers);
                 displayResult(result);
 
-                if (result[0] == 3) {
+                if (result[1] == 3) {
                     System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                     gameWon = true;
                 }
-                attempts++;
             }
 
             System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요: ");
-            int choice;
-            try {
-                choice = Integer.parseInt(Console.readLine());
-            } catch (NumberFormatException e) {
-                System.out.println("잘못된 입력입니다. 게임을 종료합니다.");
-                continue;
-            }
+            int choice = getIntegerInput();
 
             if (choice == 2) {
                 return; // 프로그램 종료
@@ -43,7 +34,7 @@ public class Application {
         }
     }
 
-    private static List<Integer> RandNum() {
+    private static List<Integer> generateComputerNumbers() {
         List<Integer> computerNumbers = new ArrayList<>();
         while (computerNumbers.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -92,6 +83,22 @@ public class Application {
             System.out.println(result[0] + "볼");
         } else {
             System.out.println(result[0] + "볼 " + result[1] + "스트라이크");
+        }
+    }
+
+    private static int getIntegerInput() {
+        while (true) {
+            try {
+                String input = Console.readLine();
+                int choice = Integer.parseInt(input);
+                if (choice == 1 || choice == 2) {
+                    return choice;
+                } else {
+                    System.out.println("1 또는 2를 입력하세요.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("잘못된 입력입니다. 1 또는 2를 입력하세요.");
+            }
         }
     }
 }
