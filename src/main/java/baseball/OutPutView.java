3f0e0d40fa3284e00dea9387a6892b3d1921abb3
shinjaewon99 <tlswodnjs99@naver.com>
2023-10-20 16:46:19 +0900

    refactor : 숫자 입력 요청에 대한 메시지 상수 변환

--- src/main/java/baseball/OutPutView.java
+++ src/main/java/baseball/OutPutView.java
@@ -1,11 +1,9 @@
 package baseball;
 
-public class OutPutView {
-    public void printBaseBallGameStartMessage() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
-    }
+import static baseball.messageConstant.MessageConstant.*;
 
+public class OutPutView {
     public void printRequestInputNumberMessage() {
-        System.out.print("숫자를 입력해주세요 : ");
+        System.out.print(REQUEST_INPUT_NUMBER_MESSAGE);
     }
 }
