7aedfb63aad2e20f3ff558b24fd767c360135e48
whxogus215 <whxogus215@naver.com>
2023-10-20 11:43:58 +0900

    feat : 입력 값 유효성 검사 객체 구현

---
+++ src/main/java/baseball/utils/NumbersValidator.java
@@ -0,0 +1,25 @@
+package baseball.utils;
+
+public class NumbersValidator {
+    /**
+     * 사용자 입력 값의 유효성 검사
+     */
+    public static void inputValidate(String input) {
+        if (input.length() != 3 || isNotInteger(input)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * 문자열이 숫자형태인지 판별
+     * @return true : 숫자 X, false : 숫자
+     */
+    public static boolean isNotInteger(String input) {
+        try {
+            Integer.parseInt(input);
+            return false;
+        } catch (NumberFormatException numberFormatException) {
+            return true;
+        }
+    }
+}
