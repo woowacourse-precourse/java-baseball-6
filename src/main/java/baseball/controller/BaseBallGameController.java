2202428caaa984399b8009bbff3a8c02dd2dbea3
SungWoo <sungwoo166@gmail.com>
2023-10-20 18:22:42 +0900

    feat: Add game start logic in controller

---
+++ src/main/java/baseball/controller/BaseBallGameController.java
@@ -0,0 +1,11 @@
+package baseball.controller;
+
+import static baseball.BaseballGameMessage.*;
+
+public class BaseBallGameController {
+    public void startGame() {
+        System.out.println(START_GAME);
+    }
+}
+
+
