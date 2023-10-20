4c59a2d2531d56d9254b235afa91c7fa9d134f54
Firedrago95 <yonghwa95@naver.com>
2023-10-20 09:44:03 +0900

    feat 1~9까지 중복없는 세자리 숫자를 가지는 컴퓨터 객체생성

---
+++ src/main/java/baseball/Model/Computer.java
@@ -0,0 +1,27 @@
+package baseball.Model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Computer {
+    public static final int MIN_NUMBER_RANGE = 1;
+    public static final int MAX_NUMBER_RANGE = 9;
+    public static final int THREE_DIGIT = 3;
+    private List<Integer> numbers;
+
+    public Computer(List<Integer> numbers) {
+        generateRandomThreeDigitNumber();
+    }
+
+    private void generateRandomThreeDigitNumber() {
+        List<Integer> allNumbers = new ArrayList<Integer>();
+
+        for (int i = MIN_NUMBER_RANGE; i <= MAX_NUMBER_RANGE; i++) {
+            allNumbers.add(i);
+        }
+
+        Collections.shuffle(numbers);
+        this.numbers = new ArrayList<Integer>(allNumbers.subList(0, THREE_DIGIT));
+    }
+}
