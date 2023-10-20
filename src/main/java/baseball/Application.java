e67116e7e7f621743a9890846771a6a4c2f37a25
parkchangeon <jg9870@naver.com>
2023-10-20 20:54:52 +0900

    컴퓨터용 숫자 생성, 사용자에게 숫자 입력 받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Set<Integer> computerSet = new HashSet<>();
+        while (computerSet.size() < 3) {
+            computerSet.add(Randoms.pickNumberInRange(1, 9));
+        }
+        List<Integer> computer = new ArrayList<>(computerSet);
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.print("숫자를 입력해주세요 : ");
+        String readNumber = Console.readLine();
+        Integer.parseInt(readNumber);
     }
 }
