0a2396df8f03a3b32b4e102051a51ca670af00f7
rbgusdlza <rbgusdlza@gmail.com>
2023-10-21 04:32:44 +0900

    user input을 int arr로 변환 및 메인 메서드 추가 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,23 +1,43 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-
+        int userflag = 1;
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while(userflag == 1){
+            int[] userInputNum = new int[3];
+            System.out.println("숫자를 입력해주세요 : ");
+            String userInputString = Console.readLine();
+            checkUserInput(userInputString);
+            convertUserInput(userInputString, userInputNum);
+        }
     }
-    
+
     static void checkUserInput(String input) throws IllegalArgumentException{
         String errorMessage = "[error] 올바르지 않은 입력입니다.";
-        // check the input size is 3
+        // Check the input size is 3.
         if(input.length() != 3) throw new IllegalArgumentException(errorMessage);
-        // check each character is number
+        // Check each character is number.
         for(int i = 0; i < input.length(); i++){
             if(!(input.charAt(i) >= '0' && input.charAt(i) <= '9')) throw new IllegalArgumentException(errorMessage);
         }
-        // check number are different each other
+        // Check number are different each other.
         char firstNum = input.charAt(0);
         char secondNum = input.charAt(1);
         char thirdNum = input.charAt(2);
         if(firstNum == secondNum || firstNum == thirdNum || secondNum == thirdNum) throw new IllegalArgumentException(errorMessage);
     }
+
+    static void convertUserInput(String userInputString, int[] userInputNum){
+        // Convert user input from a string to an integer array.
+        for(int i = 0; i < userInputString.length(); i++){
+            userInputNum[i] = (int)(userInputString.charAt(i) - '0');
+        }
+    }
+
+    
 }
 
