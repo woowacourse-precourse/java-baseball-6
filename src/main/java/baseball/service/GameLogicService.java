0dbac56242ff97b78dd0987657cc3b34721d168c
minseo  jo <liging12@naver.com>
2023-10-20 21:18:26 +0900

    feat: GameLogicService 기능 추가
    기능 목록
    1. Game 에 있는 (컴퓨터 Balls), (플레이어 Balls)를 비교한다.
    2. 비교하는 로직 (스트라이크 개수, 볼 개수 세기)
    3. 비교한 값이 3스트라이크 인 경우, 게임이 끝나므로 그 게임상태를 변경한다.

--- src/main/java/baseball/service/GameLogicService.java
+++ src/main/java/baseball/service/GameLogicService.java
@@ -1,4 +1,53 @@
 package baseball.service;
 
+import baseball.domain.Balls;
+import baseball.domain.Game;
+import baseball.domain.GameResult;
+
+import static baseball.domain.Ball.BALL_SIZE;
+
 public class GameLogicService {
+    public GameResult compareBalls(Game game, Balls playerBalls) {
+        Balls computerBalls = game.getComputerBalls();
+        int balls = countBalls(computerBalls, playerBalls);
+        int strikes = countStrikes(computerBalls, playerBalls);
+
+        return new GameResult(balls, strikes);
+    }
+
+    public Game updateGameStatusOnStrike(Game game, GameResult gameResult) {
+        if (gameResult.getStrikes() == BALL_SIZE) {
+            game.setGameFinished(true);
+        }
+        return game;
+    }
+
+    private int countStrikes(Balls computerBalls, Balls playerBalls) {
+        int strikes = 0;
+        for (int index = 0; index < BALL_SIZE; index++) {
+            if (isStrike(index, computerBalls, playerBalls)) {
+                strikes++;
+            }
+        }
+        return strikes;
+    }
+
+    private boolean isStrike(int index, Balls computerBalls, Balls playerBalls) {
+        return computerBalls.getBall(index).getNumber().equals(playerBalls.getBall(index).getNumber());
+    }
+
+    private int countBalls(Balls computerBalls, Balls playerBalls) {
+        int balls = 0;
+        for (int index = 0; index < BALL_SIZE; index++) {
+            if (isBall(index, computerBalls, playerBalls)) {
+                balls++;
+            }
+        }
+        return balls;
+    }
+
+    private boolean isBall(int index, Balls computerBalls, Balls playerBalls) {
+        return !isStrike(index, computerBalls, playerBalls) && computerBalls.containsBall(playerBalls.getBall(index));
+    }
+
 }
