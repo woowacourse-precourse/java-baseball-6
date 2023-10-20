82a995f9ed9b18b36f32fca722290ddfb2e723cb
eora21 <eora21@naver.com>
2023-10-20 15:32:21 +0900

    feat: 🎸 추가 컨트롤러와 view를 결합하고 게임을 진행시킬 수 있도록 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import baseball.controller.BaseballGameController;
+import baseball.view.ConsoleView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseballGameController baseballGameController = new BaseballGameController(new ConsoleView());
+        baseballGameController.play();
     }
 }
