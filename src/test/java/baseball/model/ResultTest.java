f89b2deefa3cf736056ce5a169737694070a28da
shin5774 <babo5774@naver.com>
2023-10-21 03:18:38 +0900

    Test: "결과 값 문자열 반환 기능 테스트 코드 작성"
    - 내부의 필드값 확인을 통해 결과를 정해진 형식으로 반환해주는 기능의 테스트 코드.

--- src/test/java/baseball/model/ResultTest.java
+++ src/test/java/baseball/model/ResultTest.java
@@ -4,6 +4,8 @@ import baseball.domain.Numbers;
 import baseball.domain.Result;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -24,5 +26,14 @@ public class ResultTest {
         Result nothing=new Result(new Numbers("123"),new Numbers("456"));
 
         assertThat(nothing.isNothing()).isTrue();
+    }
+
+    @DisplayName("결과 값 문자열 반환 기능")
+    @ParameterizedTest
+    @CsvSource(value={"134:1볼 1스트라이크","456:낫싱"},delimiter = ':')
+    void 결과_문자열_반환(String numbers,String expected){
+        Result result=new Result(new Numbers("123"),new Numbers(numbers));
+
+        assertThat(result.getResult()).isEqualTo(expected);
     }
 }
