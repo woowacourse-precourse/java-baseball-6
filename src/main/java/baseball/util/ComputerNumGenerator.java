7b9ab72d4d74a28d441b86d14aff2253737ee6a4
soozzang <soozzang@inha.edu>
2023-10-20 14:38:55 +0900

    Feat: ComputerNumGenerator 추가

---
+++ src/main/java/baseball/util/ComputerNumGenerator.java
@@ -0,0 +1,17 @@
+package baseball.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+
+public class ComputerNumGenerator {
+    ArrayList<Integer> computer = new ArrayList<>();
+    public ArrayList<Integer> computerNumGenerator() {
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+}
