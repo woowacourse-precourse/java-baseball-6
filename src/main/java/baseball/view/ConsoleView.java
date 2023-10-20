ab06e32671fb21c5429d880aa8b5ec4a33e7e858
xogus3492 <wkdxogus3492@gmail.com>
2023-10-20 12:34:11 +0900

    feat(consoleview): 서로 다른 3자리 수 입력 기능 구현 + 게임 시작 메시지 상수명 수정

--- src/main/java/baseball/view/ConsoleView.java
+++ src/main/java/baseball/view/ConsoleView.java
@@ -1,9 +1,17 @@
 package baseball.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class ConsoleView {
-    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String GAME_START_OUTPUT_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String THREE_DIGIT_NUMBER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
 
     public static void printGameStart() {
-        System.out.println(GAME_START_MESSAGE);
+        System.out.println(GAME_START_OUTPUT_MESSAGE);
+    }
+
+    public static String inputThreeDigitNumber() {
+        System.out.print(THREE_DIGIT_NUMBER_INPUT_MESSAGE);
+        return Console.readLine();
     }
 }
