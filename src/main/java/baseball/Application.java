4d1c4aa165f72326f9378f30e96406b65dbb4afc
Heojamong <wlaud0823@naver.com>
2023-10-20 21:29:47 +0900

    [기능추가]무작위 숫자 3개 생성 메소드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,5 @@
 package baseball;
 
-class BaseballGame {
-
-}
-
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,18 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private List<Integer> randomComputerPickNum = new ArrayList<>();
+
+    public void randomComputerPick() {
+        while (randomComputerPickNum.size() < 3) {
+            int randomNum = Randoms.pickNumberInRange(1, 9);
+            if (!randomComputerPickNum.contains(randomNum)) {
+                randomComputerPickNum.add(randomNum);
+            }
+        }
+    }
+}
