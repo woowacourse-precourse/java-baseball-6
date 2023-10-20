e9f78b2a40a5f2ef2a5f26ca6fca517a15fd9946
tkdtn4657 <tkdtn101226@gmail.com>
2023-10-20 21:24:48 +0900

    feat : initRandomNumber() 메서드를 통해 랜덤한 3개의 값을 answerNumberList에 초기화

--- docs/README.md
+++ docs/README.md
@@ -1,5 +1,7 @@
 # 구현할 기능 Todo
 - [x] 입출력 시뮬레이션
-- [ ] 랜덤으로 값 입력받기(camp.nextstep.edu.missionutils 패키지활용)
+  - PrintMessage 클래스를 통해 출력문 사용
+- [x] 랜덤으로 값 입력받기(camp.nextstep.edu.missionutils 패키지활용)
+  - initRandomNumber() 메서드를 통해 랜덤한 3개의 값을 answerNumberList에 초기화
 - [ ] 입력받은 값을 볼인지 스트라이크인지 판단하는 checkGameScore()메서드 구현
 - [ ] 예외값 처리
--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -1,18 +1,58 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+
 public class Baseball {
 
     private final PrintMessage printMessage = new PrintMessage();
-    void run(){
+    private ArrayList<Integer> answerNumberList;
+
+    public void run(){
         printMessage.gameStartMessage();
         startGame();
     }
 
-    void startGame(){
-        printMessage.inputNumberMessage();
-        printMessage.gameResult(1,1);
-        printMessage.inputNumberMessage();
-        printMessage.gameResult(0,3);
-        printMessage.gameCommand();
+    public void startGame(){
+        int commandNumber;
+        do {
+            initRandomNumber();
+            for( int num : answerNumberList){
+                System.out.println(num);
+            }
+            printMessage.inputNumberMessage();
+            printMessage.gameResult(1, 1);
+            printMessage.inputNumberMessage();
+            printMessage.gameResult(0, 3);
+            printMessage.gameCommand();
+            commandNumber = Integer.parseInt(Console.readLine());
+        }while (commandNumber == 1);
+    }
+
+    public void initRandomNumber(){
+
+        answerNumberList = new ArrayList<>();
+        answerNumberList.add(Randoms.pickNumberInRange(1, 9));
+        boolean equalPrevNumber = false;
+
+        while(answerNumberList.size() < 3){
+
+            equalPrevNumber = false;
+
+            int nowNumber = Randoms.pickNumberInRange(1, 9);
+            for(int prevNumber : answerNumberList){
+                if(prevNumber == nowNumber) {
+                    equalPrevNumber = true;
+                    break;
+                }
+            }
+
+            if(!equalPrevNumber) {
+                answerNumberList.add(nowNumber);
+            }
+
+        }
     }
 }
