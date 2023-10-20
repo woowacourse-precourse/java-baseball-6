0c5ef547934f7ee86beeafa5a0bda7ce6c36d169
ga0nii <kgw0124@kakao.com>
2023-10-20 21:52:44 +0900

    [feat/게임] 1. 사용자의 게임 재실행 여부에 대응하여 게임을 시작하도록 구현 2. 컴퓨터의 수와 사용자의 수를 비교한 결과에 대응하여 사용자에게 입력값을 받도록 수현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -3,10 +3,34 @@ package baseball;
 import java.util.List;
 
 public class BaseballGame {
+    private static final String START_MSG = "숫자를 야구 게임을 시작합니다.";
+    private static final String INPUT_NUMBERS_MSG = "숫자를 입력해주세요 : ";
+    private static final String END_MSG = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    private User user;
+    private Computer computer;
+    private Result result;
+
+    protected BaseballGame() {
+        this.user = new User();
+        this.computer = new Computer();
+        this.result = new Result();
+    }
+
     public void startGame(){
-        //(1) 컴퓨터 : 1에서 9까지 서로 다른 임의의 수 3개 생성
-        Computer computer = new Computer();
-        List<Integer> computerNumbs = computer.getNumbers();
-        System.out.println("컴퓨터 생성 수 : " + computerNumbs);
+        System.out.println(START_MSG);
+
+        while(user.getIsContinueGame()){
+            //(1) 컴퓨터 : 1에서 9까지 서로 다른 임의의 수 3개 생성
+            List<Integer> computerNumbs = computer.getNumbers();
+
+            //(2) 사용자가 추측하는 3개의 수 입력 받기
+            while(!result.getIsAnswer()){
+                System.out.println(INPUT_NUMBERS_MSG);
+                List<Integer> userNumbs = user.getNumbers();
+
+            }
+
+        }
     }
 }
