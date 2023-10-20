4b4f76158b27144fa57baf2f9134a8fe7c5efad9
Roista57 <Roista57@gmail.com>
2023-10-20 17:14:59 +0900

    기능 목록과 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,56 +7,74 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        try{
-            while(true){
-                List<Integer> computer = new ArrayList<>();
-                while (computer.size() < 3) {
-                    int randomNumber = Randoms.pickNumberInRange(1, 9);
-                    if (!computer.contains(randomNumber)) {
-                        computer.add(randomNumber);
-                    }
-                }
-                while(true){
-                    System.out.print("숫자를 입력해주세요 :");
-                    String input = Console.readLine();
-                    if(isNumber(computer, input) == 3){
-                        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                        break;
-                    }
-                }
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                int s = Integer.parseInt(Console.readLine());
-                if(s == 1) continue;
-                else if(s == 2) break;
+    static List<Integer> randNum(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
             }
-        }catch(IllegalArgumentException e) {
-            System.out.println(e.toString());
         }
+        return computer;
     }
-    static int isNumber(List<Integer> computer, String input) throws IllegalArgumentException{
-        if(input.length() > 3) throw new IllegalArgumentException("입력된 값이 3개보다 많습니다. : "+input);
-        if(input.length() < 3) throw new IllegalArgumentException("입력된 값이 3개보다 적습니다. : "+input);
-        for(int i=0;i<input.length();i++){
-            if(input.charAt(i) < '0' || input.charAt(i) > '9') throw new IllegalArgumentException("입력된 값이 숫자가 아닙니다. : "+input);
+
+    static int isNumber(List<Integer> computerNumber, String userNumber) throws IllegalArgumentException{
+        if (userNumber.length() != 3){
+            // 입력된 숫자가 3개가 아닌 경우
+            throw new IllegalArgumentException("사용자가 입력한 숫자의 개수가 3개가 아닙니다.");
         }
-        int number = Integer.parseInt(input);
+
+        List<Integer> userNumList = new ArrayList<>();
+        for (int i=0;i<3;i++){
+            int num = userNumber.charAt(i) - '0';
+            if(num >= 1 && num <= 9) {
+                if (!userNumList.contains(num)){
+                    userNumList.add(num);
+                }
+                else{
+                    // 중복인 경우
+                    throw new IllegalArgumentException("사용자가 입력한 숫자 중 중복된 숫자가 있습니다.");
+                }
+            }else{
+                // 숫자가 아닌 경우
+                throw new IllegalArgumentException("사용자가 입력한 값에 숫자가 아닌 값이 있습니다.");
+            }
+        }
+
         int strike = 0, ball = 0;
-        Integer[] list = {number/100, number%100/10, number%10};
-        for(int i = 0;i<computer.size();i++){
-            for(int j=0;j<list.length;j++){
-                if(computer.get(i).equals(list[j])){
-                    if(i == j) strike++;
+        for (int i=0;i<3;i++){
+            for (int j=0;j<3;j++){
+                if (userNumList.get(i).equals(computerNumber.get(j))){
+                    if (i == j) strike++;
                     else ball++;
                 }
             }
         }
-        if(ball != 0) System.out.print(ball+"볼 ");
-        if(strike != 0) System.out.println(strike+"스트라이크");
-        if(ball == 0 && strike == 0) System.out.println("낫싱");
-        System.out.println();
+        if (ball != 0) System.out.print(ball+"볼 ");
+        if (strike != 0) System.out.println(strike+"스트라이크");
+        if (ball ==0 && strike == 0) System.out.println("낫싱");
         return strike;
+    }
+
+    public static void main(String[] args)  throws IllegalArgumentException{
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        boolean game;
+        do {
+            List<Integer> computerNumber = randNum();
+            int strike;
+            do{
+                System.out.print("숫자를 입력해주세요 : ");
+                String userInput = Console.readLine();
+                strike = isNumber(computerNumber, userInput);
+            }while(strike != 3);
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String userInput = Console.readLine();
+            if (userInput.equals("1")) game = true;
+            else if (userInput.equals("2")) game = false;
+            else{
+                throw new IllegalArgumentException("사용자가 1, 2 이외의 값을 입력했습니다.");
+            }
+        }while(game);
     }
 }
