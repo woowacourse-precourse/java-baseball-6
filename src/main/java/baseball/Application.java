d59085d0ba5c0afaf8df4d7cd660e6e2d0039550
noxknow <chris2769@naver.com>
2023-10-20 22:54:14 +0900

    update(BaseballGameController) : 게임 시작 문구 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,14 +2,17 @@ package baseball;
 
 import baseball.controller.BaseballGameController;
 import baseball.handler.InputHandler;
+import baseball.handler.OutputHandler;
 import baseball.view.ConsoleInput;
+import baseball.view.ConsoleOutput;
 
 public class Application {
 
     public static void main(String[] args) {
 
         final InputHandler inputHandler = new ConsoleInput();
+        final OutputHandler outputHandler = new ConsoleOutput();
 
-        new BaseballGameController(inputHandler).run();
+        new BaseballGameController(inputHandler, outputHandler).run();
     }
 }
--- src/main/java/baseball/controller/BaseballGameController.java
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -2,6 +2,7 @@ package baseball.controller;
 
 import baseball.domain.UserBaseballNumber;
 import baseball.handler.InputHandler;
+import baseball.handler.OutputHandler;
 
 import java.util.Arrays;
 import java.util.List;
@@ -9,12 +10,16 @@ import java.util.List;
 public class BaseballGameController {
 
     private final InputHandler inputHandler;
+    private final OutputHandler outputHandler;
 
-    public BaseballGameController(InputHandler inputHandler) {
+    public BaseballGameController(InputHandler inputHandler, OutputHandler outputHandler) {
         this.inputHandler = inputHandler;
+        this.outputHandler = outputHandler;
     }
 
     public void run() {
+        outputHandler.printGameStart();
+
         UserBaseballNumber userBaseballNumber = loadUserValues();
     }
 
