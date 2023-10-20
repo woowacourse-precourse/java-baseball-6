6cfa2a6a91d367ce20fb02f64767de70a19d0f84
김효준 <khyojun1104@gmail.com>
2023-10-20 13:08:07 +0900

    [feat] GameController : inputView, outputView 컨트롤러에 넘기고 main에서 넘기기 위한 초기 셋팅

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,5 +1,20 @@
 package baseball.controller;
 
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class GameController {
 
+    InputView inputView;
+    OutputView outputView;
+
+    public GameController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+
+    public void playball() {
+
+    }
 }
