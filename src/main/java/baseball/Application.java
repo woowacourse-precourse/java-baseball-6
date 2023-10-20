6b85c561df42cdcd490321cd256868f400bb897d
jhkim <wellsy001012@gmail.com>
2023-10-21 07:23:47 +0900

    feat: 게임 플레이

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Computer computer = new Computer();
+        Player player = new Player();
+        player.playGame(computer);
     }
 }
