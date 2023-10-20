bd21a34e9b5b8d6893a8484d9955103a980f4ba4
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2023-10-20 17:17:45 +0900

    feat: 게임 결과(스트라이크, 볼)을 반환하는 GameResult 구현

---
+++ src/main/java/baseball/domain/GameResult.java
@@ -0,0 +1,44 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class GameResult {
+
+    private int strike;
+    private int ball;
+    private final String NOTHING_STR = "낫씽";
+    private final String BALL_STR = "볼";
+    private final String STRIKE_STR = "스트라이크";
+
+    public void strikeOrBall(List<Integer> baseballNumbers, List<Integer> userInputNumbers) {
+        this.strike = 0;
+        this.ball = 0;
+
+        for (int i = 0; i < userInputNumbers.size(); i++) {
+            if (baseballNumbers.get(i) == userInputNumbers.get(i)) this.strike++;
+            else if (baseballNumbers.contains(userInputNumbers.get(i))) this.ball++;
+        }
+    }
+
+    @Override
+    public String toString() {
+        int strike = this.strike;
+        int ball = this.ball;
+
+        if (strike == 0 && ball == 0) {
+            return NOTHING_STR;
+        } else if (strike == 0) {
+            return ball + BALL_STR;
+        } else if (ball == 0) {
+            return strike + STRIKE_STR;
+        }
+
+        return strike + STRIKE_STR + " " + ball + BALL_STR;
+    }
+
+    public boolean isWin() {
+        if (this.strike == 3) return true;
+
+        return false;
+    }
+}
