efd820428c06b131baf81a69dc7ac8ce0548d03c
shAn-kor <53287605+shAn-kor@users.noreply.github.com>
2023-10-20 22:26:56 +0900

    controller constructor

--- src/main/java/baseball/contriller/BaseballGame.java
+++ src/main/java/baseball/contriller/BaseballGame.java
@@ -1,4 +1,16 @@
 package baseball.contriller;
 
+import baseball.model.ComputerNumber;
+import baseball.model.UserNumber;
+
 public class BaseballGame {
+    private UserNumber userNumber;
+    private ComputerNumber computerNumber;
+
+    public BaseballGame(UserNumber userNumber, ComputerNumber computerNumber) {
+        this.userNumber = userNumber;
+        this.computerNumber = computerNumber;
+    }
+
+
 }
