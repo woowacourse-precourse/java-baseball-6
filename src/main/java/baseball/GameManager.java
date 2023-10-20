5eeb2b283db51dc5c249216cd4d2cb9b781c1bb3
june-777 <wlwhswnsrl96@gmail.com>
2023-10-20 15:07:44 +0900

    refactor: 게임 흐름을 담당하는 GameManager 클래스로 분리

---
+++ src/main/java/baseball/GameManager.java
@@ -0,0 +1,53 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
+public class GameManager {
+
+    private Validator validator = new Validator();
+    private BaseballCreator baseballCreator = new BaseballCreator();
+    private Judgment judgment = new Judgment();
+
+    // 기능: 게임을 최초로 시작한다
+    public void initialGameStart() {
+        Output.printInitialGameStartMessage();
+        while (true) {
+            progressGame();
+            if (isNoMoreGame()) break;
+        }
+    }
+
+    // 기능: 게임 순서에 맞게 게임을 진행한다
+    private void progressGame() {
+        List<Integer> computerBalls = baseballCreator.createComputerBalls();
+        while (true) {
+            Output.printNumberInputMessage();
+            List<Integer> playerBalls = baseballCreator.createPlayerBalls();
+            String hint = judgment.getHint(computerBalls, playerBalls);
+            Output.printHint(hint);
+            if (isGameEnd(hint)) break;
+        }
+    }
+
+    // 기능: 힌트가 3스트라이크면 게임 종료
+    private boolean isGameEnd(String hint) {
+        if (hint.equals("3스트라이크")) {
+            Output.printGameEndMessage();
+            return true;
+        }
+        return false;
+    }
+
+    // 기능: 게임 종료 후, 게임 재시작 여부
+    private boolean isNoMoreGame() {
+        Output.printRestartGameMessage();
+        String restartCommand = Console.readLine();
+        validator.validateRestartCommandInput(restartCommand);
+        if (restartCommand.equals("2")) {
+            return true;
+        }
+        return false;
+    }
+}
