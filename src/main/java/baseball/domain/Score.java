c6792902d8060706279b252a00262bc031c8ed48
WOONBE <kepull2918@naver.com>
2023-10-20 12:13:37 +0900

    [feat] Score 도메인 작성

---
+++ src/main/java/baseball/domain/Score.java
@@ -0,0 +1,25 @@
+package baseball.domain;
+
+public class Score {
+
+    private int strike;
+    private int ball;
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+
+    public void setScore(int strike, int ball){
+        this.strike = strike;
+        this.ball = ball;
+    }
+
+    public Score(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+}
---
+++ src/main/java/baseball/service/PrintService.java
@@ -0,0 +1,43 @@
+package baseball.service;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class PrintService {
+
+    public void printBeforeStart(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void printGameResult(int strike, int ball){
+        if(strike == 0 && ball == 0){
+            System.out.println("낫싱");
+            return;
+
+        } if (strike > 0 && ball > 0) {
+            System.out.printf("%d볼 %d스트라이크\n", ball, strike);
+            return;
+        }
+        if (strike > 0) {
+            System.out.printf("%d스트라이크\n", strike);
+            return;
+        }
+        if (ball > 0) {
+            System.out.printf("%d볼\n", ball);
+        }
+
+    }
+    public boolean printAfterFinish(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String userInput = readLine();
+        //유효한 숫자인지 검증하는 로직 필요
+
+        if (userInput.equals("2")) {
+            return true;
+        }
+        return false;
+    }
+
+
+
+
+}
