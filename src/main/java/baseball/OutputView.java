ff34c2d890bbfa3e8bd5861ded3a0cb7bd3c3279
hwanhee <brightfanta@gmail.com>
2023-10-20 20:40:27 +0900

    refactor: change method(println -> print) not to display line break on console

--- src/main/java/baseball/OutputView.java
+++ src/main/java/baseball/OutputView.java
@@ -5,7 +5,7 @@ public class OutputView {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
     public void printRequirePlayerInput() {
-        System.out.println("숫자를 입력해주세요 : ");
+        System.out.print("숫자를 입력해주세요 : ");
     }
 
     public void printBallAndStrikeResult(Integer strikeCnt, Integer ballCnt) {
