b6b12201cf1027e79599250e07ad31e36276e94f
sosojean <thwlsdl0989@gmail.com>
2023-10-20 13:58:50 +0900

    구현할 기능 목록 작성 및 사용자 입력 기능 추가

--- src/main/java/baseball/BaseballFunction.java
+++ src/main/java/baseball/BaseballFunction.java
@@ -1,2 +1,28 @@
-package baseball;public class BaseballFunction {
+package baseball;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BaseballFunction {
+    public List<Integer> genNum(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
+    public static char[] getNum(){
+        char[] player;
+        String input = Console.readLine();
+        player = input.toCharArray();
+
+        return player;
+    }
 }
