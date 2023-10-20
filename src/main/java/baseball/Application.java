a8d233119128096e87c7dc65a5c9cee40cccfa43
inkyu-yoon <tryingmybest24h@gmail.com>
2023-10-21 08:52:14 +0900

    [feat] : 애플리케이션을 구성하는 기본 도메인 정의

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,9 @@
 package baseball;
 
+import baseball.domain.BaseballGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseballGame baseballGame = new BaseballGame();
     }
 }
---
+++ src/main/java/baseball/domain/Answer.java
@@ -0,0 +1,4 @@
+package baseball.domain;
+
+public class Answer {
+}
