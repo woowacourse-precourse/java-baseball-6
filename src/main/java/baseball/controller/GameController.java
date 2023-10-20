698a6d52f74115a67dfb6a5f7d86046b823978c9
Jieon <dlwldjs7544@naver.com>
2023-10-20 19:54:05 +0900

    feat: 사용자의 입력값을 저장하는 기능 구현
    - 문자열로 받은 사용자의 입력 값을 Integer List 로 변환하기 위한 기능 추가
    - 사용자의 입력값을 저장하기 위한 Model의 playerNumber class 생성

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,6 +1,7 @@
 package baseball.controller;
 
 import baseball.model.ComputerNumber;
+import baseball.model.PlayerNumber;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 import camp.nextstep.edu.missionutils.Randoms;
@@ -9,19 +10,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameController {
+    private static final int NUMBER_LENGTH = 3;
+    private static final char ZERO_CHAR = '0';
+
     OutputView outputView = new OutputView();
     InputView inputView = new InputView();
     ComputerNumber computerNumber = new ComputerNumber();
+    PlayerNumber playerNumber = new PlayerNumber();
 
     public void startGame() {
         outputView.printStartMessage();
         computerNumber.setComputerNumbers(getRandomNumbers());
+
         String playerNumberStr = inputView.inputPlayerNumber();
+        playerNumber.setPlayerNumbers(convertPlayerNumberToList(playerNumberStr));
     }
 
     public List<Integer> getRandomNumbers() {
         List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
+        while (computer.size() < NUMBER_LENGTH) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
@@ -29,5 +36,15 @@ public class GameController {
         }
 
         return computer;
+    }
+
+    public List<Integer> convertPlayerNumberToList(String playerNumberStr) {
+        List<Integer> player = new ArrayList<>();
+        for (int index = 0; index < NUMBER_LENGTH; index++) {
+            int number = playerNumberStr.charAt(index) - ZERO_CHAR;
+            player.add(number);
+        }
+
+        return player;
     }
 }
---
+++ src/main/java/baseball/model/PlayerNumber.java
@@ -0,0 +1,15 @@
+package baseball.model;
+
+import java.util.List;
+
+public class PlayerNumber {
+    private List<Integer> playerNumbers;
+
+    public void setPlayerNumbers(List<Integer> playerNumbers) {
+        this.playerNumbers = playerNumbers;
+    }
+
+    public List<Integer> getPlayerNumbers() {
+        return playerNumbers;
+    }
+}
