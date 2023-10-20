644e225a4559ff7e7702ccc0454437aacd2dcd6d
sj <hera4224@naver.com>
2023-10-20 19:45:46 +0900

    [Feat]view구현 완료

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -2,5 +2,13 @@ package baseball.view;
 //사용자로부터 숫자입력받기, 게임 종료/재시작 여부
 import camp.nextstep.edu.missionutils.Console;
 public class InputView {
+    public static String GetNum(){
+        System.out.println("숫자를 입력해주세요 : ");
+        return Console.readLine();
+    }
 
+    public static String AskForRestart(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return Console.readLine();
+    }
 }
--- src/main/java/baseball/view/OutputVIew.java
+++ src/main/java/baseball/view/OutputVIew.java
@@ -1,5 +1,20 @@
 package baseball.view;
 //모든 결과 출력
 public class OutputVIew {
+    public static void printGameStart(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+    public static void printBallMessage(){
+        System.out.println("볼");
+    }
+    public static void printStrikeMessage(){
+        System.out.println("스트라이크");
+    }
+    public static void printNothingMessage(){
+        System.out.println("낫싱");
+    }
+    public static void printGameEnd() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
 
 }
