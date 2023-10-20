74f0fb6b79a5df6cdafb6f841f1bc5d5b25935be
Chenowoo <cjsgusdn14@naver.com>
2023-10-20 18:32:43 +0900

    Fix: ComputerSelectNumber 메소드 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -50,10 +50,7 @@ public class Application {
         List<Integer> computer = new ArrayList<>();
 
         while(computer.size()<3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if(!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
+            computer = Randoms.pickUniqueNumbersInRange(1, 9, 3);
         }
         return computer;
     }
