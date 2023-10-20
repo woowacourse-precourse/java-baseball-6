843dc4ffba732dfd5218886bcc8dbb171c3fdb1c
hyun-hyang <jimin3060@naver.com>
2023-10-20 23:30:00 +0900

    Feat : main feature finish

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -9,31 +9,73 @@ public class Game {
     boolean gameClear = false;
     List<Integer> computer = null;
     List<Integer> userGuess = null;
+    int strike = 0;
+    int ball = 0;
 
 
     public Game() {
         startAnnounce();
         computerPickNum();
-        inputGuess();
+
+        do {
+            inputGuess();
+            this.strike = 0;
+            this.ball = 0;
+            checkResult();
+            printResult();
+        } while (strike != 3);
         gameClearAnnounce();
     }
 
+    private void printResult() {
+        if (ball == 0 && strike == 0) {
+            System.out.println("낫싱");
+        }
+
+        if (ball > 0) {
+            System.out.print(ball + "볼 ");
+        }
+        if (strike > 0) {
+            System.out.println(strike + "스트라이크");
+        }
+    }
+
     private void computerPickNum() {
-        this.computer = new ArrayList<>();
+        ArrayList<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
         }
+        this.computer = computer;
     }
 
     private void inputGuess() {
         System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
+        if (input.length() != 3) {
+            throw new IllegalArgumentException();
+        }
 
-        this.userGuess = new ArrayList<>();
+        ArrayList<Integer> userGuess = new ArrayList<>();
         input.chars().forEach(i -> userGuess.add(i - 48));
+
+        this.userGuess = userGuess;
+    }
+
+    private void checkResult() {
+        for (int i = 0; i < 3; i++) {
+            if (userGuess.get(i).equals(computer.get(i))) {
+                strike++;
+            } else {
+                for (int j = 0; j < 3; j++) {
+                    if (userGuess.get(i).equals(computer.get(j))) {
+                        ball++;
+                    }
+                }
+            }
+        }
     }
 
     private void startAnnounce() {
