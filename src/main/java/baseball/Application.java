73eff0fa7eb0f0639e90bbc3bb11d0827ea78b44
kyu-wooon <catdog0831@gmail.com>
2023-10-20 14:21:42 +0900

    main 함수의 while문 기능 작성 진행 1. 랜덤 숫자 3개를 담은 answer_list 생성 2. 숫자 야구 게임 시작 알림 3.while문으로 숫자야구 진행(진행중) - 숫자 입력 부분의 예외 2가지 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -21,19 +21,41 @@ public class Application {
     }
     //메인 숫자야구 함수
     public static void main(String[] args) {
-        List<Integer> answer_list = new ArrayList<>(); // 정답을 담는 리스트
-        int ball =0; // ball 갯수
-        int strike = 0; // strike 갯수
+        List<Integer> answer_list = new ArrayList<>();
+        int ball =0;
+        int strike = 0;
+        String input = "";
+        boolean end_check = false;
 
-        //1. 랜덤 함수 실행
-        random_pick();
-
+        //1. 랜덤 숫자 3개를 담은 answer_list 생성
+        answer_list = random_pick();
 
         //2. 숫자 야구 게임 시작 알림
         System.out.println("숫자 야구 게임을 시작합니다.");
 
         //3.while문으로 숫자야구 진행
-        while (){
+        //while 문 조건 - strike 갯수가 3개가 아니거나 end_check 가 false 상태일 것
+        while (strike != 3 || end_check == false){
+            //while문 돌 때마다 ball과 strike는 초기화
+            ball = 0;
+            strike = 0;
+
+
+            //숫자 입력
+            System.out.println("숫자를 입력해주세요 : ");
+            input = Console.readLine();
+
+            if(input.length() != 3){//예외 1 : 길이가 3이 아닌 경우
+                throw new IllegalArgumentException();
+            }
+            if(input.matches("[+-]?\\d*(\\.\\d+)?") == false){//예외 2 : 숫자 이외의 문자가 포함되어 있을 경우
+                throw new IllegalArgumentException();
+            }
+
+
+
+
+
 
 
 
