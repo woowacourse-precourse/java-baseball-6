8796f9ee165ee0fc93973af6d98cfd7e2fa8b04a
JiEung2 <https://github.com/JiEung2>
2023-10-20 15:38:05 +0900

    유저에게 입력하라는 글씨를 보여주는 출력 기능 구현

---
+++ src/main/java/view/InputText.java
@@ -0,0 +1,11 @@
+package view;
+
+public class InputText {
+    public void inputNum(){
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
+    public void coinNum(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+}
