62e39e4941c1285f8f1bf345c6018a090107d42e
watermelon3737 <pshrina1@naver.com>
2023-10-21 02:27:31 +0900

    [feat] : 유저 input String의 개별적 요소에 대한 중복 및 유효성 검사

--- src/main/java/baseball/InputValidation.java
+++ src/main/java/baseball/InputValidation.java
@@ -1,5 +1,8 @@
 package baseball;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class InputValidation {
     public void validateUserInput(String input) throws IllegalArgumentException{
         if (input.contains(" ")) {
@@ -10,6 +13,26 @@ public class InputValidation {
         }
         if (input.length() != 3) {
             throw new IllegalArgumentException("3개의 숫자만을 입력해주시기 바랍니다.");
+        }
+    }
+
+    private void validateInputNumber(String input) {
+        Set<Character> charSet = new HashSet<Character>();
+        charSet.add(input.charAt(0));
+        charSet.add(input.charAt(1));
+        charSet.add(input.charAt(2));
+
+        if (charSet.size() != 3) {
+            throw new IllegalArgumentException("숫자를 중복하지 않고 입력해주시기 바랍니다.");
+        }
+
+        for (Character c : charSet) {
+            if (!Character.isDigit(c)) {
+                throw new IllegalArgumentException("숫자만을 입력해주시기 바랍니다.");
+            }
+            if (c.equals('0')) {
+                throw new IllegalArgumentException("1 ~ 9 사이의 숫자만을 입력해주시기 바랍니다.");
+            }
         }
     }
 }
