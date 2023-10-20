e6795dd1919d003c8f3b1af9675fc9d697e64e9e
pwrwpw <dlgustjr9912@gmail.com>
2023-10-21 01:55:57 +0900

    feat(OutputView): 볼,스트라이크 카운트에 따른 메시지 출력 기능 추가
    docs: 볼,스트라이크 카운트 에 따른 기능 추가 체크 완료

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,8 +1,13 @@
 package baseball.view;
 
+import static baseball.model.Constant.MESSAGE_BALL;
 import static baseball.model.Constant.MESSAGE_GAME_END;
 import static baseball.model.Constant.MESSAGE_GAME_START;
 import static baseball.model.Constant.MESSAGE_INPUT_NUMBER;
+import static baseball.model.Constant.MESSAGE_NOTHING;
+import static baseball.model.Constant.MESSAGE_STRIKE;
+
+import java.text.MessageFormat;
 
 public class OutputView {
 
@@ -14,6 +19,21 @@ public class OutputView {
 	}
 	public static void printInputNumber() {
 		System.out.print(MESSAGE_INPUT_NUMBER.getMessage());
+	}
+	public void printResult(int strike, int ball) {
+		if(strike == 0 && ball == 0){
+			System.out.println(MESSAGE_NOTHING.getMessage());
+			return;
+		}
+		if(strike > 0){
+			System.out.print(
+				MessageFormat.format("{0}{1}", strike, MESSAGE_STRIKE.getMessage()));
+		}
+		if(ball > 0){
+			System.out.print(
+				MessageFormat.format(" {0}{1}", ball, MESSAGE_BALL.getMessage()));
+		}
+		System.out.println();
 	}
 
 }
