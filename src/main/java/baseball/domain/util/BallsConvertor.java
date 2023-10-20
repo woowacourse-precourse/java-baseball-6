05deadb33c299e0e048ab683c11ba98f58d5adc5
newh08 <newh08@gmail.com>
2023-10-20 14:06:25 +0900

    Refactor: BallsConvertor 상수 import 변경

--- src/main/java/baseball/domain/util/BallsConvertor.java
+++ src/main/java/baseball/domain/util/BallsConvertor.java
@@ -4,18 +4,18 @@ import baseball.domain.ball.Ball;
 import java.util.ArrayList;
 import java.util.List;
 
+import static baseball.domain.util.RandomBallGenerator.SIZE_OF_NUMBER;
+import static baseball.domain.util.RandomBallGenerator.ZERO;
+
 public class BallsConvertor {
     private static final int HUNDRED = 100;
     private static final int TEN = 10;
-    private static final int THREE = 3;
     private static final int ONE = 1;
-    private static final int ZERO = 0;
-
 
     public static List<Ball> convertNumberToBalls(int number) {
         List<Ball> balls = new ArrayList<>();
         List<Integer> cutNumbers = cutNumber(number);
-        for (int i = ZERO; i < THREE; i++) {
+        for (int i = ZERO; i < SIZE_OF_NUMBER; i++) {
             balls.add(new Ball(cutNumbers.get(i), i));
         }
         return balls;
