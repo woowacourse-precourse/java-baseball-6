ebda735383e6b370ff7cd763480b887cc2c67da6
박성춘 <songchon100@gmail.com>
2023-10-20 17:50:44 +0900

    feat : 게임 결과 출력 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,4 +1,22 @@
 package baseball.view;
 
+import baseball.model.NumberBaseballResult;
+import camp.nextstep.edu.missionutils.Console;
+
 public class OutputView {
+
+    public void printGameResult(NumberBaseballResult numberBaseballResult) {
+        System.out.println(numberBaseballResult.generateResultString());
+    }
+
+    public int printEndGame() {
+        System.out.println("3스트라이크");
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return Integer.parseInt(Console.readLine());
+    }
+
+    public void endOfGame() {
+        System.out.println("숫자야구 게임이 종료되었습니다.");
+    }
 }
