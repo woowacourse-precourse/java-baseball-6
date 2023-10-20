96f302e14be4a90cd82c86647e7e07f90aba005e
Hanjaemo <hjm2530@gmail.com>
2023-10-20 22:33:13 +0900

    feat(ComputerNumbers): 플레이어의 수 체크 기능 구현

--- src/main/java/baseball/domain/ComputerNumbers.java
+++ src/main/java/baseball/domain/ComputerNumbers.java
@@ -9,4 +9,19 @@ public class ComputerNumbers {
     public ComputerNumbers(List<Integer> numbers) {
         this.numbers = numbers;
     }
+
+    public void checkPlayerNumbers(List<Integer> playerNumbers, int digitNumber) {
+        for (int pIndex = 0; pIndex < digitNumber; pIndex++) {
+            int playerNumberOfIndex = playerNumbers.get(pIndex);
+            for (int cIndex = 0; cIndex < digitNumber; cIndex++) {
+                if (playerNumberOfIndex == numbers.get(cIndex)) {
+                    if (pIndex == cIndex) {
+                        BallCounter.countStrike();
+                        continue;
+                    }
+                    BallCounter.countBall();
+                }
+            }
+        }
+    }
 }
