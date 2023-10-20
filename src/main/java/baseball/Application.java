eb7868b24492545fe9ae3c7d3f4337cf920691db
kyu-wooon <catdog0831@gmail.com>
2023-10-20 14:01:32 +0900

    최초 기능 설계 // ramdom_pick 함수 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,37 @@
 package baseball;
 
+
+import java.util.*;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    //랜덤 숫자 list 삽입 함수
+    public static List<Integer> random_pick(){
+        Set<Integer> set_answer = new HashSet<>();
+        List<Integer> answer = new ArrayList<>(set_answer);
+    return answer;
+    }
+    //메인 숫자야구 함수
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Integer> answer_list = new ArrayList<>(); // 정답을 담는 리스트
+        int ball =0; // ball 갯수
+        int strike = 0; // strike 갯수
+
+        //1. 랜덤 함수 실행
+        random_pick();
+
+
+        //2. 숫자 야구 게임 시작 알림
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        //3.while문으로 숫자야구 진행
+        while (){
+
+
+
+        }
+
     }
 }
