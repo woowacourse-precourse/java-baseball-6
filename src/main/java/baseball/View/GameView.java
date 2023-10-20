9530a3dd7271073884dd1a566e92ed130f577c85
dltjdgh0428 <dltjdgh0428@naver.com>
2023-10-21 05:03:59 +0900

    refactor: GameView 클래스 작성완료

--- src/main/java/baseball/View/GameView.java
+++ src/main/java/baseball/View/GameView.java
@@ -1,4 +1,27 @@
 package baseball.View;
 
 public class GameView {
+    public void displayWelcomeMessage() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void displayResult(int ball, int strike) {
+        String returnString = "";
+        if (ball != 0)
+            returnString = ball + "볼 ";
+        if (strike != 0)
+            returnString += strike + "스트라이크";
+        if (ball == 0 && strike == 0)
+            returnString = "낫싱";
+        System.out.println(returnString);
+    }
+
+    public void displayGameOverMessage() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    public void displayPrompt() {
+        System.out.println("숫자를 입력해주세요 : ");
+    }
+
 }
