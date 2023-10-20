ef3dca488879b467e78b1361672b6600124935cb
jjminsuh <minsuh3203@gmail.com>
2023-10-21 00:55:22 +0900

    add: 볼, 스트라이크 확인 기능 추가

--- src/main/java/baseball/PlayBaseballGame.java
+++ src/main/java/baseball/PlayBaseballGame.java
@@ -8,6 +8,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class PlayBaseballGame {
     static List<Integer> getRandomNum() {
@@ -59,13 +60,37 @@ public class PlayBaseballGame {
         return List.of(firstNum, secondNum, thirdNum);
 
     }
+
+    static boolean checkStrike(List<Integer> computerNum, List<Integer> playerNum) {
+        int ballCnt = 0;
+        int strikeCnt = 0;
+
+        for(int i = 0; i < 3; i++) {
+            if(Objects.equals(playerNum.get(i), computerNum.get(i))) {
+                strikeCnt ++;
+            } else if(computerNum.contains(playerNum.get(i))) {
+                ballCnt ++;
+            }
+        }
+
+        if(ballCnt == 0 && strikeCnt == 0) {
+            System.out.println("낫싱");
+            return false;
+        } else if(strikeCnt == 3) {
+            System.out.printf("%d스트라이크\n", strikeCnt);
+            return true;
+        } else {
+            System.out.printf("%d볼 %d스트라이크\n", ballCnt, strikeCnt);
+            return false;
+        }
+    }
     public static void playGame() {
 
         boolean isCorrect = false;
 
         // set the number of the computer
         List<Integer> computerNumberList = getRandomNum();
-        List<Integer> playerGuessNumberList = getRandomNum();
+        List<Integer> playerGuessNumberList;
 
         while(!isCorrect) {
             System.out.print("숫자를 입력해주세요 : ");
@@ -76,9 +101,9 @@ public class PlayBaseballGame {
                 return;
             }
 
-            System.out.println(playerGuessNumberList);
-
-            isCorrect = true;
+            isCorrect = checkStrike(computerNumberList, playerGuessNumberList);
         }
+
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 }
