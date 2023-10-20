2096a152a55a80e8d1edb82b8bd50af475c3a6c4
JiEung2 <https://github.com/JiEung2>
2023-10-20 14:36:52 +0900

    게임을 다시 시작할 것인가에 대한 입력 구현

--- src/main/java/controller/InputReOrEnd.java
+++ src/main/java/controller/InputReOrEnd.java
@@ -1,4 +1,15 @@
 package controller;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputReOrEnd {
+    public int inputCoin(){
+        String tmp = Console.readLine();
+        int coin = Integer.parseInt(tmp);
+
+        return coin;
+    }
 }
