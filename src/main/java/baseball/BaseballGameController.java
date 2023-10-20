680dd351610986e822fa1ee6edccd622602f2e77
ParkSoyun <soyp@naver.com>
2023-10-20 10:06:23 +0900

    Feat: 게임 시작 메시지 출력 기능

---
+++ src/main/java/baseball/BaseballGameController.java
@@ -0,0 +1,7 @@
+package baseball;
+
+public class BaseballGameController {
+    public void run() {
+        ConsoleIO.printStartMessage();
+    }
+}
---
+++ src/main/java/baseball/ConsoleIO.java
@@ -0,0 +1,7 @@
+package baseball;
+
+public class ConsoleIO {
+    public static void printStartMessage() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+}
