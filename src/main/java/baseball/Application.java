48f2804a5d42b92837da4a5c57f96705e5897cce
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2023-10-20 15:42:45 +0900

    feat : 콘솔에서 숫자 입력받기 기능 구현
    - nextstep 에서 제공하는 readLine() 사용함
    - 입력받은 문자열을 char 으로 바꾼뒤 Character.getNumericValue() 으로 변경뒤 List 에 넣어줌
    - 입력받은 문자 길이가 3개가 아니거나 숫자로 못바꾸면 IllegalArgumentException 발생시키는 방향으로 넣어줌

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +20,27 @@ public class Application {
         }
 
         System.out.println(computer);
+
+
+
+        List<Integer> my = new ArrayList<>();
+        String inputNumbers = readLine();
+
+        if (inputNumbers.length() != 3){
+            throw new IllegalArgumentException("입력한 숫자 길이가 맞지 않습니다.");
+        }
+
+        for (int i = 0; i < inputNumbers.length(); i++){
+            try {
+                int number = Character.getNumericValue(inputNumbers.charAt(i));
+                my.add(number);
+            } catch (Exception e) {
+                throw new IllegalArgumentException("입력한 문자가 숫자가 아닙니다.");
+            }
+
+        }
+
+        System.out.println(my);
 
     }
 }
