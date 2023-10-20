ee265bb24b04461ce988c46e9088b51330d23159
leemiyeon <lemingul7@gmail.com>
2023-10-21 00:05:53 +0900

    feat: 컴퓨터 랜덤 숫자 생성 기능 구현

--- src/main/java/baseball/Number.java
+++ src/main/java/baseball/Number.java
@@ -1,8 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Number {
 
     public Number() {
+    }
+
+    /**
+     * 컴퓨터가 랜덤 3자리 숫자를 생성하는 메서드
+     */
+    public List<Integer> setRandomNumber() {
+
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            computer.add(randomNumber);
+        }
+
+        return computer;
     }
 
 }
