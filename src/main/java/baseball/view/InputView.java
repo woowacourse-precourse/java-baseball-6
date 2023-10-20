d42519b54bcb60268c3be1c3d9c863341310aec0
hellomatia <shuaigejp80@gmail.com>
2023-10-20 11:54:17 +0900

    feat(view): InputView 클래스 추가
    - InputView 클래스를 새로 추가하였습니다.
    - 이 클래스는 플레이어로부터 숫자 입력과 게임 재시작 또는 종료 선택을 입력받는 데 사용됩니다.
    - `readPlayerNumbers` 메서드는 숫자를 입력 받아 List로 반환하고, `readPlayerChoice` 메서드는 게임 재시작 또는 종료 선택을 입력받아 문자열로 반환합니다.

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,24 @@
+package baseball.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class InputView {
+    private static final String INPUT_BASEBALL_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String INPUT_RETRY_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public List<Integer> readPlayerNumbers() {
+        System.out.print(INPUT_BASEBALL_NUMBER_MESSAGE);
+        return Arrays.stream(readLine().split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    public String readPlayerChoice() {
+        System.out.println(INPUT_RETRY_MESSAGE);
+        return readLine();
+    }
+}
