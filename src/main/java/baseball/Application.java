93c2bf9558c3b8cfc1f15cc872b5ac0e9d33875b
muyaaho <skanthsu715@gmail.com>
2023-10-20 17:12:25 +0900

    refactor: add a class to manage strike and ball field

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,8 @@
 /*
-* 잘못된 값이 입력되었을 때
-*
-* 0을 입력해도 종료하나? 일단 예외로 하고 종료시킴. 테스트 해볼 때 0을 어떻게 처리할지 체크하기
-* */
+ * 잘못된 값이 입력되었을 때
+ *
+ * 0을 입력해도 종료하나? 일단 예외로 하고 종료시킴. 테스트 해볼 때 0을 어떻게 처리할지 체크하기
+ * */
 
 package baseball;
 
@@ -11,13 +11,80 @@ import camp.nextstep.edu.missionutils.Console;
 
 import static java.lang.Integer.parseInt;
 
+class Baseball {
+    private int strike;
+    private int ball;
+//    private boolean isFinish = false;
+
+    /* 새로운 입력이 반복 될 때마다 strike와 ball을 초기화 하는 함수*/
+    void clear() {
+        strike = 0;
+        ball = 0;
+    }
+
+    /* strike와 ball을 count 하는 함수 */
+    void count(String com, String gamer) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (gamer.charAt(i) == com.charAt(j)) {
+                    if (i == j) {
+                        strike += 1;
+                    } else {
+                        ball += 1;
+                    }
+                }
+            }
+        }
+
+    }
+
+    /* strike, ball 결과 따라 결과를 출력하고 또 실행 할 건지 T/F를 리턴하는 함수 */
+    boolean check() {
+        if (strike == 3) {
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+
+        } else if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+
+        } else {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
+        return false;
+
+    }
+
+    void inputException(String num) {
+        try {
+            //0이 들어올 경우
+            if (num.contains("0")) {
+                throw new Exception();
+            }
+            // 3자리 수가 아닌 경우
+            if (num.length() != 3) {
+                throw new Exception();
+            }
+
+            // 숫자가 아닌 경우
+            int canInt = parseInt(num);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("잘못된 값을 입력했습니다.");
+
+        }
+    }
+
+
+}
 
 
 public class Application {
 
     private static final boolean IS_TEST = true;        // 정답 출력 여부 설정, 제출 시에는 false
+    static int end = 1;
 
-    public static String makeRandomNum(){
+
+    public static String makeRandomNum() {
         String com = "";
         // to method
         while (com.length() < 3) {
@@ -33,76 +100,36 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        // 초기화
-
-        int end = 1;
         System.out.println("숫자 야구 게임을 시작합니다.");
         boolean isFinish = false;
         // 게임 진행
         while (end == 1) {
+
             // 한 게임당 초기화
             String com = makeRandomNum();
             isFinish = false;
+            Baseball b = new Baseball();
 
-
-            if (IS_TEST){
-                System.out.println("answer: "+com);
+            if (IS_TEST) {
+                System.out.println("answer: " + com);
             }
             // 게임 시작
-
-            // to method -> return을 end로 해서 계속 반복하거나 아니거나, input
             // while (게임 진행되는 동안)
             while (!isFinish) {
-                int strike = 0;
-                int ball = 0;
+                b.clear();
                 // 입력
                 System.out.print("숫자를 입력해주세요 : ");
                 String num = Console.readLine();
-                // TODO: 이상한 입력 체크
-                try {
-                    //0이 들어올 경우
-                    if (num.contains("0")) {
-                        throw new Exception();
-                    }
-                    // 3자리 수가 아닌 경우
-                    if (num.length() != 3) {
-                        throw new Exception();
-                    }
+                b.inputException(num);
 
-                    // 숫자가 아닌 경우
-                    int canInt = parseInt(num);
-                }
-                catch(Exception e){
-                    throw new IllegalArgumentException("잘못된 값을 입력했습니다.");
+                b.count(num, com);
 
-                }
+                // 종료할 지 말지
+                boolean ischeck = b.check();
 
-
-                // 잘못된 입력 3: 숫자가 아닌 경우
-                // 비교
-                // to method: input - (num, com), output - (strike, ball)
-                for (int i = 0; i < 3; i++) {
-                    for (int j = 0; j < 3; j++) {
-                        if (num.charAt(i) == com.charAt(j)) {
-                            if (i == j) {
-                                strike += 1;
-                            } else {
-                                ball += 1;
-                            }
-                        }
-                    }
-                }
-
-                // 결과
-                // 정답: 일단 나가기
-
-                // to method: input (strike, ball), output (isFinish)
-                if (strike == 3) {
-                    System.out.println("3스트라이크");
-                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                if (ischeck) {
                     System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                     String s_end = Console.readLine();
-
                     // TODO: 잘못된 값 입력됬는지 확인!
                     try {
                         // 정수로 입력되지 않았을 때
@@ -113,20 +140,13 @@ public class Application {
                             throw new Exception();
                         }
 
-                    }catch (Exception e){
+                    } catch (Exception e) {
                         throw new IllegalArgumentException("잘못된 값을 입력했습니다.");
                     }
 
                     isFinish = true;
-
+                    // 계속
                 }
-                else if (strike == 0 && ball == 0) {
-                    System.out.println("낫싱");
-                }
-                else {
-                    System.out.println(ball+"볼 "+strike+"스트라이크");
-                }
-                // 계속
 
             }
 
