a5f837e745d8360bad71de0e6c0f484848b39a2f
Big-Cir97 <eodnjs01477@gmail.com>
2023-10-20 17:52:24 +0900

    feat: OutputView 게임 시작, 사용자 입력, 종료 출력 메세지 method 구현

--- src/main/java/baseball/OutputView.java
+++ src/main/java/baseball/OutputView.java
@@ -8,4 +8,20 @@ public class OutputView {
   private static final String HIT_NOTHING_MESSAGE = "";
   private static final String GAME_SUCCESS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
   private static final String GAME_RESTART_OR_END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+  public void printStartGameMessage() {
+    System.out.println(START_GAME_MESSAGE);
+  }
+
+  public void printInputMessage() {
+    System.out.println(INPUT_NUMBER_MESSAGE);
+  }
+
+  public void printGameSuccessMessage() {
+    System.out.println(GAME_SUCCESS_MESSAGE);
+  }
+
+  public void printRestartOrEndMeesage() {
+    System.out.println(GAME_RESTART_OR_END_MESSAGE);
+  }
 }
