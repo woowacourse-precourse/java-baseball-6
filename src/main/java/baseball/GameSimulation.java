1b2102a073f7100e372ec09a214fe5551b87ee7c
kms0946 <gghuly67@gmail.com>
2023-10-20 10:45:40 +0900

    사용자의 값이 오류인지 판단하는 메서드 구현

--- src/main/java/baseball/GameSimulation.java
+++ src/main/java/baseball/GameSimulation.java
@@ -38,6 +38,21 @@ public class GameSimulation {
         return userNumber;
     }
 
+    // 입력된 유저의 값이 오류인 지 판단 하는 메서드
+    public void judgeUser() {
+        if (user.size() != 3){
+            throw new IllegalArgumentException("3자리 숫자가 아닙니다.");
+        }
+        if (user.contains(0)){
+            throw new IllegalArgumentException("0이 포함 되어 있습니다.");
+        }
+        for (int i = 0; i < user.size(); i++){
+            if (Collections.frequency(user,user.get(i)) != 1){
+                throw new IllegalArgumentException("서로 다른 3자리 숫자가 아닙니다.");
+            }
+        }
+    }
+
 
 
 }
