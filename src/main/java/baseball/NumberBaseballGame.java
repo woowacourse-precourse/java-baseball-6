bf27fb3dcfed77922c140fd80820aee08a8ddfd2
useu <lakingsung@gmail.com>
2023-10-21 02:41:13 +0900

    refactor: isAnswer() 메서드 제거

--- src/main/java/baseball/NumberBaseballGame.java
+++ src/main/java/baseball/NumberBaseballGame.java
@@ -5,27 +5,23 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class NumberBaseballGame {
-    private int strike;
-    private int ball;
 
     public void start() {
         List<Integer> answer = NumberBaseballUtility.generateAnswer();
+        int strike = 0;
+        int ball = 0;
         System.out.println("숫자 야구 게임을 시작합니다.");
-        while (!isAnswer()) {
+        while (strike != 3) {
             System.out.print("숫자를 입력해주세요 : ");
             String input = Console.readLine();
             if (!NumberBaseballUtility.isValidInput(input)) {
                 throw new IllegalArgumentException();
             }
             List<Integer> playerAnswer = NumberBaseballUtility.convertStringToNumberList(input);
-            this.strike = NumberBaseballUtility.calculateStrike(answer, playerAnswer);
-            this.ball = NumberBaseballUtility.calculateBall(answer, playerAnswer);
+            strike = NumberBaseballUtility.calculateStrike(answer, playerAnswer);
+            ball = NumberBaseballUtility.calculateBall(answer, playerAnswer);
             NumberBaseballUtility.printGameResult(strike, ball);
         }
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-    }
-
-    private boolean isAnswer() {
-        return (strike == 3);
     }
 }
