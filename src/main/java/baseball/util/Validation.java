57ceb4a73337c35cb2d6106ec648042c629953b4
junOTL <naqp99@gmail.com>
2023-10-21 00:41:43 +0900

    feat: 중복된 숫자를 입력한 경우 예외 발생 기능 구현

--- src/main/java/baseball/util/Validation.java
+++ src/main/java/baseball/util/Validation.java
@@ -1,5 +1,7 @@
 package baseball.util;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 public class Validation {
@@ -19,6 +21,16 @@ public class Validation {
     public static void isInRange(String input) {
         if (!Pattern.matches("^[1-9]+$", input)) {
             throw new IllegalArgumentException("1부터 9까지의 숫자만 입력 가능 합니다.");
+        }
+    }
+
+    public static void isDuplicate(String input) {
+        Set<Character> number = new HashSet<>();
+        for (char digit : input.toCharArray()) {
+            number.add(digit);
+        }
+        if (number.size() != 3) {
+            throw new IllegalArgumentException("서로 다른 숫자를 입력해 주세요.");
         }
     }
 }
