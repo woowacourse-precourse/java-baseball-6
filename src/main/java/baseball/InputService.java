8ee830e288d2237ed91a53bceb409cfb13920481
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2023-10-20 22:07:00 +0900

    feat: 사용자 입력값 검증 로직 구현

--- src/main/java/baseball/InputService.java
+++ src/main/java/baseball/InputService.java
@@ -10,11 +10,44 @@ public class InputService {
         System.out.print("숫자를 입력해주세요 : ");
         String inputValue = Console.readLine();
 
+        // 입력값 검증
+        if(!validateLength(inputValue) || !validateType(inputValue)) return null;
+
         // 입력값 List<Integer>로 매핑
         List<Integer> inputValueList = new ArrayList<>();
         mapping(inputValue, inputValueList);
 
         return inputValueList;
+    }
+
+    // 입력값의 길이를 검증하는 로직
+    private Boolean validateLength(String inputValue) {
+        try {
+            if (inputValue.length() != 3) {
+                throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.");
+            }
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    // 입력값의 타입을 검증하는 로직
+    private Boolean validateType(String inputValue) {
+        for (int i = 0; i < 3; i++) {
+            int intValue = inputValue.charAt(i) - 48;
+            try {
+                if (!(1 <= intValue && intValue <= 9)) {
+                    throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.");
+                }
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return true;
     }
 
     // 입력값을 List<Integer>로 매핑하는 로직
