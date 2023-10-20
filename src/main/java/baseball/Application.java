338efef2e24c1b3ede32b5abcac39f78170ecc30
devholic22 <hyunjoon.tech@gmail.com>
2023-10-20 09:29:24 +0900

    Feat: 수 모두 맞히면 게임 종료 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,6 +3,8 @@ package baseball;
 import baseball.factory.NumberFactory;
 import baseball.validator.NumberValidator;
 import baseball.view.AskView;
+import baseball.view.EndView;
+import baseball.view.ResultView;
 import baseball.view.StartView;
 import camp.nextstep.edu.missionutils.Console;
 
@@ -18,5 +20,15 @@ public class Application {
         NumberValidator.assertInputNumberWithLength(userInput, PLAY_NUMBER_DIGIT);
 
         int computerNumber = NumberFactory.pickNumberWithLength(PLAY_NUMBER_DIGIT);
+        int userNumber = Integer.parseInt(userInput);
+
+        int ball = Umpire.countBall(computerNumber, userNumber);
+        int strike = Umpire.countStrike(computerNumber, userNumber);
+
+        ResultView.printResult(ball, strike);
+
+        if (strike == PLAY_NUMBER_DIGIT) {
+            EndView.end(PLAY_NUMBER_DIGIT);
+        }
     }
 }
