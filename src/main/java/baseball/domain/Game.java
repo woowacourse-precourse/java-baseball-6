112d841a80ae2bd5ea4a5e315aab1a81812fe22f
mizuirohoshi7 <mizuirohoshi7@gmail.com>
2023-10-20 16:47:39 +0900

    test(Game): 테스트 생성

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -1,4 +1,20 @@
 package baseball.domain;
 
 public class Game {
+    private static final int START = 1;
+    private static final int END = 9;
+
+    private final GameNumber answerNumber;
+
+    public Game() {
+        answerNumber = null;
+    }
+
+    public int countStrike(GameNumber inputNumber) {
+        return 0;
+    }
+
+    public int countBall(GameNumber inputNumber) {
+        return 0;
+    }
 }
