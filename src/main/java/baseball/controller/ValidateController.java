d81033e515fd1c68b132f0b0ffeb80cdb412b195
ch1hyun <och2510@gmail.com>
2023-10-20 13:45:34 +0900

    ValidateController 클래스 내의 문자열 상수 분리 - ValidateMessage 클래스의 모델 생성

--- src/main/java/baseball/controller/ValidateController.java
+++ src/main/java/baseball/controller/ValidateController.java
@@ -1,39 +1,34 @@
 package baseball.controller;
 
+import baseball.model.ValidateMessage;
+
 import java.util.List;
 
 public class ValidateController {
-    private static final String numbers = "123456789";
-    private static final String validEndOrRestartInput = "12";
-    private static final String ERROR_INPUT_NUMBER_RANGE = "1에서 9 사이의 숫자만 가능합니다.";
-    private static final String ERROR_INPUT_NUMBER_LENGTH = "길이 %d의 숫자이어야 합니다.";
-    private static final String ERROR_INPUT_END_OR_RESTART_RANGE = "1 또는 2의 숫자만 가능합니다.";
-    private static final String ERROR_INPUT_END_OR_RESTART_LENGTH = "한 개의 숫자만 입력 가능합니다.";
-    private static final String ERROR_INPUT_NUMBER_HAS_DUPLICATE = "입력 값에 중복된 숫자가 있습니다.";
     public void validateInputString(String input, Integer length) {
         if (input.length() != length) { // 숫자 길이 검사
-            throw new IllegalArgumentException(String.format(ERROR_INPUT_NUMBER_LENGTH, length));
+            throw new IllegalArgumentException(String.format(ValidateMessage.ERROR_INPUT_NUMBER_LENGTH, length));
         }
 
         for (String inputAtOne : input.split("")) { // 문자가 아닌 숫자를 입력했는지 검사
-            if (!numbers.contains(inputAtOne)) {
-                throw new IllegalArgumentException(ERROR_INPUT_NUMBER_RANGE);
+            if (!ValidateMessage.numbers.contains(inputAtOne)) {
+                throw new IllegalArgumentException(ValidateMessage.ERROR_INPUT_NUMBER_RANGE);
             }
         }
     }
 
     public void validateInputString(String input) throws IllegalArgumentException{
         if (input.length() != 1) { // 길이 검사
-            throw new IllegalArgumentException(ERROR_INPUT_END_OR_RESTART_LENGTH);
+            throw new IllegalArgumentException(ValidateMessage.ERROR_INPUT_END_OR_RESTART_LENGTH);
         }
-        if (!validEndOrRestartInput.contains(input)) { // 1, 2 이외의 숫자나 문자를 입력했는지 검사
-            throw new IllegalArgumentException(ERROR_INPUT_END_OR_RESTART_RANGE);
+        if (!ValidateMessage.validEndOrRestartInput.contains(input)) { // 1, 2 이외의 숫자나 문자를 입력했는지 검사
+            throw new IllegalArgumentException(ValidateMessage.ERROR_INPUT_END_OR_RESTART_RANGE);
         }
     }
 
     public void validateInputIntegerList(List<Integer> input, Integer length) {
         if (input.size() != length) {
-            throw new IllegalArgumentException(ERROR_INPUT_NUMBER_HAS_DUPLICATE);
+            throw new IllegalArgumentException(ValidateMessage.ERROR_INPUT_NUMBER_HAS_DUPLICATE);
         }
     }
 }
---
+++ src/main/java/baseball/model/ValidateMessage.java
@@ -0,0 +1,11 @@
+package baseball.model;
+
+public class ValidateMessage {
+    public static final String numbers = "123456789";
+    public static final String validEndOrRestartInput = "12";
+    public static final String ERROR_INPUT_NUMBER_RANGE = "1에서 9 사이의 숫자만 가능합니다.";
+    public static final String ERROR_INPUT_NUMBER_LENGTH = "길이 %d의 숫자이어야 합니다.";
+    public static final String ERROR_INPUT_END_OR_RESTART_RANGE = "1 또는 2의 숫자만 가능합니다.";
+    public static final String ERROR_INPUT_END_OR_RESTART_LENGTH = "한 개의 숫자만 입력 가능합니다.";
+    public static final String ERROR_INPUT_NUMBER_HAS_DUPLICATE = "입력 값에 중복된 숫자가 있습니다.";
+}
