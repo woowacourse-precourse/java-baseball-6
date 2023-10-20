d1f1f78e9a4326029dfe354f4f20de052d0012c9
whxogus215 <whxogus215@naver.com>
2023-10-20 11:45:52 +0900

    feat : 메인 메서드 및 테스트 코드 완료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import baseball.domain.BaseballCalculator;
+import baseball.domain.BaseBall;
+import baseball.domain.GameManager;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BaseBall baseBall = new BaseBall(new BaseballCalculator());
+        GameManager gameManager = new GameManager(baseBall);
+
+        gameManager.game();
     }
 }
