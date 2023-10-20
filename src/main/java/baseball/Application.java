0ce5d38a81032d1529e7991ad801e1b111b3c596
zzawang <hobin0116@naver.com>
2023-10-20 15:40:48 +0900

    feat: 게임 시작 기능에 예외 처리 추가
    - 사용자가 잘못된 입력을 할 때 발생하는 예외를 처리하기 위한 try-catch 문 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,11 +13,14 @@ public class Application {
 
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        String flag;
+        String flag = "";
         do {
-            startGame();
-            flag = askReplayOrExit();
-        }while (flag.equals("1"));  // 1을 입력하면 게임 새로 시작
+            try{
+                startGame();
+            }catch (IllegalArgumentException e){
+                flag = askReplayOrExit();
+            }
+        }while(flag.equals("1"));  // 1을 입력하면 게임 새로 시작
     }
 
     private static void startGame() {
