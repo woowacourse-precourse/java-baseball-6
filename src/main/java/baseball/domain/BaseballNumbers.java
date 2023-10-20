9906f30929aadf2a71ca4bdabc9cfe7c14f0f011
seokmyungham <hellotjraud@gmail.com>
2023-10-21 01:22:36 +0900

    feat(BaseballNumbers): BaseballNumbers 클래스 생성

---
+++ src/main/java/baseball/domain/BaseballNumbers.java
@@ -0,0 +1,11 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class BaseballNumbers {
+    private List<BaseballNumber> numbers;
+
+    public BaseballNumbers(List<BaseballNumber> numbers) {
+        this.numbers = numbers;
+    }
+}
