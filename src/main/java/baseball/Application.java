e1583560c0295cfe269b4f5eba43733b36d25937
Taaate <an981022@naver.com>
2023-10-20 20:49:55 +0900

    chore: 컨트롤러와 뷰 연결 세팅
    게임 실행시 컨트롤러는 뷰와 연결되어 동작한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,20 @@
 package baseball;
 
+import baseball.controller.GameController;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameController controller = new GameController(inputView(), outputView());
+        controller.run();
+    }
+
+    private static InputView inputView() {
+        return new InputView();
+    }
+
+    private static OutputView outputView() {
+        return new OutputView();
     }
 }
---
+++ src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,19 @@
+package baseball.controller;
+
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+public class GameController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public GameController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+
+    }
+}
