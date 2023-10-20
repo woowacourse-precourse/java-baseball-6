b6c57569c7dc5602c974b0c3429cae0725c46dee
Seonghwan <aiya0422@gmail.com>
2023-10-20 11:29:29 +0900

    STYLE: calculator 클래스의 매직넘버를 수정

--- src/main/java/baseball/Calculator.java
+++ src/main/java/baseball/Calculator.java
@@ -4,10 +4,19 @@ import java.util.List;
 
 public class Calculator {
 
-    public static int countStrike(List<Integer> input, List<Integer> answer) {
-        int count = 0;
+    private static int STRIKE_NUMBER = 3;
+    private static int LIST_TO = 3;
+    private static int LIST_FROM = 0;
+    private static int COUNTER_ZERO = 0;
 
-        for (int i = 0; i < 3; i++) {
+    public static boolean isNotOver(int strike) {
+        return strike != STRIKE_NUMBER;
+    }
+
+    public static int countStrike(List<Integer> input, List<Integer> answer) {
+        int count = COUNTER_ZERO;
+
+        for (int i = LIST_FROM; i < LIST_TO; i++) {
             if (input.get(i).equals(answer.get(i))) {
                 count++;
             }
@@ -17,9 +26,9 @@ public class Calculator {
     }
 
     public static int countBall(List<Integer> input, List<Integer> answer) {
-        int count = 0;
+        int count = COUNTER_ZERO;
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = LIST_FROM; i < LIST_TO; i++) {
             if (!answer.contains(input.get(i))) {
                 continue;
             }
