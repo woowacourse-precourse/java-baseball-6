114b2491d306b18ddb29c787e5b4147056989f15
cloudchamb3r <jizon0123@protonmail.com>
2023-10-21 08:53:41 +0900

    misc: remove TODO

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,7 +5,6 @@ import baseball.game.GameManager;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         var game = new BaseballGame();
         GameManager.start(game);
     }
