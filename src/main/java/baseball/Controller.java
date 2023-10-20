2762432f7cf1d708b1d60de5ed9cae4bcad3bff3
jhon3242 <rldotnr2@gmail.com>
2023-10-20 12:29:50 +0900

    docs : 확장 가능한 요구 사항 작성

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -2,55 +2,54 @@ package baseball;
 
 import baseball.view.InputView;
 import baseball.view.OutputView;
-
 import java.util.List;
 
 public class Controller {
 
-	private Validator validator;
+    private Validator validator;
 
-	public Controller(Validator validator) {
-		this.validator = validator;
-	}
+    public Controller(Validator validator) {
+        this.validator = validator;
+    }
 
-	public void run() {
-		OutputView.printMessage("숫자 야구 게임을 시작합니다.");
-		do {
-			proceed();
-		} while (isRestartGame());
-	}
+    public void run() {
+        OutputView.printMessage("숫자 야구 게임을 시작합니다.");
+        do {
+            proceed();
+        } while (isRestartGame());
+    }
 
-	private void proceed() {
-		GameData gameData = new GameData();
-		initGame(gameData);         // 게임 초기화
-		proceedGame(gameData);      // 게임 진행
-		endGame();                  // 게임 종료
-	}
+    private void proceed() {
+        GameData gameData = new GameData();
+        initGame(gameData);         // 게임 초기화
+        proceedGame(gameData);      // 게임 진행
+        endGame();                  // 게임 종료
+    }
 
-	private void initGame(GameData gameData) {
-		gameData.initComputer();
-	}
+    private void initGame(GameData gameData) {
+        gameData.initComputer();
+    }
 
-	private void proceedGame(GameData gameData) {
-		while (true) {
-			List<Integer> trial = validator.validateBaseballNumber(InputView.readBaseballNumber());
-			List<Integer> result = gameData.calculateResult(trial);
-			OutputView.printResult(result);
-			if (isThreeStrike(result)) {
-				return;
-			}
-		}
-	}
+    private void proceedGame(GameData gameData) {
+        while (true) {
+            List<Integer> trial = validator.validateBaseballNumber(InputView.readBaseballNumber());
+            List<Integer> result = gameData.calculateResult(trial);
+            OutputView.printResult(result);
+            if (isThreeStrike(result)) {
+                return;
+            }
+        }
+    }
 
-	private boolean isThreeStrike(List<Integer> result) {
-		return result.get(1) == 3;
-	}
+    private boolean isThreeStrike(List<Integer> result) {
+        return result.get(1) == 3;
+    }
 
-	private void endGame() {
-		OutputView.printMessage("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-	}
+    private void endGame() {
+        OutputView.printMessage("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
 
-	private boolean isRestartGame() {
-		return validator.validatedRestart(InputView.readRestart()) == 1;
-	}
+    private boolean isRestartGame() {
+        return validator.validatedRestart(InputView.readRestart()) == 1;
+    }
 }
