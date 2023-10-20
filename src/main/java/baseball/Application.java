1eb34b5a99f6dd889e6eaa638e6e93221b06dd1b
Chenowoo <cjsgusdn14@naver.com>
2023-10-20 19:10:34 +0900

    Fix: main 메소드 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,7 +13,12 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다");
 
-        GameStart();
+        try {
+            GameStart();
+        } catch(IllegalArgumentException e) {
+            return;
+        }
+
 
     }
 
