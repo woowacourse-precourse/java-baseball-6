18e3493dbeefec24d80a7244594d18808efe6306
wochae <officialwochae@gmail.com>
2023-10-20 21:43:14 +0900

    [FEAT] : determine ball or strike or nothing

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int restartStatus;
+        do {
+            Play play = new Play();
+            restartStatus = play.play();
+        } while (restartStatus == 1);
     }
 }
