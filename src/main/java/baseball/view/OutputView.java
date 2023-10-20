9cbe7af3068019161748a9c09a4fb8528ce9ec6a
Hanjaemo <hjm2530@gmail.com>
2023-10-21 02:31:38 +0900

    feat(OutputView): 종료 문구 출력 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -5,4 +5,9 @@ public class OutputView {
     public static void printBeginningMessage() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
+    public static void printEndingMessageForAllStrike(int digitNumber) {
+        System.out.println(digitNumber + "스트라이크\n" +
+                digitNumber + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
 }
