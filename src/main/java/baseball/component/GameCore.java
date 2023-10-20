a109c56af8cab93dc12b62cb7e519c8f99578b83
Identityless <rlandwnd555@naver.com>
2023-10-20 23:10:17 +0900

    feat : 게임 코어 클래스 추가

---
+++ src/main/java/baseball/component/GameCore.java
@@ -0,0 +1,18 @@
+package baseball.component;
+
+public class GameCore {
+    private Initializer initializer;
+    private Inputter inputter;
+    private Validator validator;
+
+    public GameCore(
+            Initializer initializer,
+            Inputter inputter,
+            Validator validator
+            ) {
+        this.initializer = initializer;
+        this.inputter = inputter;
+        this.validator = validator;
+    }
+
+}
