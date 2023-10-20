ee505d2de11eba33b4301383f392cbf938efa6c2
Jonghyeok Lee <endb1228@gmail.com>
2023-10-20 18:02:17 +0900

    게임이 끝난 후 재시작/종료 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.List;
 
 import static baseball.GameManager.getInstance;
@@ -10,7 +12,16 @@ public class Application {
         System.out.println("숫자 야구 게임을 시작합니다.");
         while (true) {
             getInstance().startGame();
-            break;
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String answer = Console.readLine();
+
+            if (answer.equals("1"))
+                continue;
+            if (answer.equals("2"))
+                break;
+            else
+                throw new IllegalArgumentException();
         }
     }
 }
