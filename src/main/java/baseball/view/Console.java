888f9202e6ad871cf34895e15120489623e96825
leesh <qwqw8019@naver.com>
2023-10-20 18:45:43 +0900

    feat: 입력을 위한 메소드 및 검증 메소드 구현
    - 입력
      - 3자리 랜덤 숫자 입력 메소드(input3DigitRandomNumber)
      - 3자리 숫자 입력 메소드 (input3DigitNumber)
      - 게임 종료 커맨드 입력 메소드 (inputRestartOrTerminate)
    
    - 검증
      - validateInputNotNull()
      - validateInputIsNumber()
      - validateInput3DigitNumber()
      - validateInputNotDuplicate()
      - validateRestartOrTerminate()

---
+++ src/main/java/baseball/view/Console.java
@@ -0,0 +1,94 @@
+package baseball.view;
+
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Console {
+
+    private static final Exception ILLEGAL_ARGUMENT_EXCEPTION = new IllegalArgumentException();
+    private static final String NUMBER_REGEX = "[1-9]+";
+    private static final List<String> RESTART_OR_TERMINATE = List.of("1", "2");
+
+    public static List<Integer> input3DigitRandomNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
+    private static String readLine() throws Exception {
+        String inputString = camp.nextstep.edu.missionutils.Console.readLine();
+
+        validateInputNotNull(inputString);
+        validateInputIsNumber(inputString);
+
+        return inputString;
+    }
+
+    public static String input3DigitNumber() throws Exception {
+        String inputString = readLine();
+
+        validateInput3DigitNumber(inputString);
+        validateInputNotDuplicate(inputString);
+
+        return inputString;
+    }
+
+    public static String inputRestartOrTerminate() throws Exception {
+
+        String inputString = readLine();
+
+        validateRestartOrTerminate(inputString);
+
+        return inputString;
+    }
+
+    private static void validateInputNotNull(String inputString) throws Exception {
+        if (inputString == null) {
+            throw ILLEGAL_ARGUMENT_EXCEPTION;
+        }
+    }
+
+    private static void validateInputIsNumber(String inputString) throws Exception {
+
+        if (!inputString.matches(NUMBER_REGEX)) {
+            throw ILLEGAL_ARGUMENT_EXCEPTION;
+        }
+    }
+
+    private static void validateInput3DigitNumber(String inputString) throws Exception {
+
+        if (inputString.length() != 3) {
+            throw ILLEGAL_ARGUMENT_EXCEPTION;
+        }
+    }
+
+    private static void validateInputNotDuplicate(String inputString) throws Exception {
+        int[] appearance = new int[10];
+
+        for (char inputChar : inputString.toCharArray()) {
+            appearance[inputChar-'0'] ++;
+        }
+
+        for (int appeared : appearance) {
+            if (appeared>=2) {
+                throw ILLEGAL_ARGUMENT_EXCEPTION;
+            }
+        }
+
+    }
+
+    private static void validateRestartOrTerminate(String inputString) throws Exception {
+        if (!RESTART_OR_TERMINATE.contains(inputString)) {
+            throw ILLEGAL_ARGUMENT_EXCEPTION;
+        }
+    }
+
+}
