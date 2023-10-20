159bbe3dd58b6cad5b9ee396c69d75626266aa91
SehyeonKang <gedfgrdt@naver.com>
2023-10-20 12:48:00 +0900

    feat(InputView): 입력받은 문자열이 숫자로 변환가능한지 확인하는 기능 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -6,6 +6,15 @@ public class InputView {
 
     public String readGuessNumbers() {
         String inputString = Console.readLine();
+        validateDigit(inputString);
         return inputString;
+    }
+
+    private void validateDigit(String inputString) {
+        try {
+            Integer.parseInt(inputString);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
     }
 }
