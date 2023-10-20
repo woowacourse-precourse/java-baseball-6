c1a1152add15e482ff41a1bef43388bd5643e182
hongseokhodev <hongseokhodev@gmail.com>
2023-10-20 17:40:16 +0900

    feature: print game start message

---
+++ src/main/java/baseball/view/Messages.java
@@ -0,0 +1,8 @@
+package baseball.view;
+
+import static baseball.utils.Constants.NUMBER_OF_BALLS;
+
+public final class Messages {
+    public static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+
+}
---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,9 @@
+package baseball.view;
+
+import static baseball.view.Messages.GAME_START_MESSAGE;
+
+public class OutputView {
+    public void printGameStart() {
+        System.out.println(GAME_START_MESSAGE);
+    }
+}
