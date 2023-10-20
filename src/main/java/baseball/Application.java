0348c48156b16374ad74c00864f216410949a15a
Gyumin Sim <gyu2781@gmail.com>
2023-10-20 17:19:04 +0900

    [Feat] 야구게임 메인 메서드, 게임 실행 메서드 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,33 @@
 package baseball;
 
+
+import java.util.List;
+
 public class Application {
+    private static final Utils utils = new Utils();
+    private static final Logic logic = new Logic();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        playGame();
+    }
+
+    private static void playGame() {
+        List<Integer> computer = utils.generateRandomNumbers();
+        boolean isContinue = true;
+
+        while (isContinue) {
+            int[] userGuess = logic.getUserGuess();
+            int[] result = logic.calculateResult(userGuess, computer);
+
+            utils.displayResult(result);
+
+            if (result[0] == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                isContinue = logic.askForRestart();
+                if(!isContinue) break;
+                computer = utils.generateRandomNumbers();
+            }
+        }
     }
 }
