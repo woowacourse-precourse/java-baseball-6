3e8ba53e6f1c55a217b1c5ee270d048c2689bc34
dlqudtjs <dlqudtjs922@gmail.com>
2023-10-20 15:30:26 +0900

    feat: 컴퓨터의 난수 생성 기능 구현

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -1,6 +1,31 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class BaseBallGame {
+    private static final int MIN_RANDOM_NUMBER = 1;
+    private static final int MAX_RANDOM_NUMBER = 9;
+    private static final int RANDOM_NUMBER_LENGTH = 3;
+
+    private List<Integer> getRandomNumber() {
+        Set<Integer> computerNumber = new HashSet<>();
+
+        while(computerNumber.size() < RANDOM_NUMBER_LENGTH) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
+
+            computerNumber.add(randomNumber);
+        }
+
+        return computerNumber.stream().toList();
+    }
+
+    private boolean isInvalidInput(String input) {
+        return isNumeric(input) && isUniqueDigits(input) && isThreeDigit(input);
+    }
 
     private boolean isNumeric(String input) {
         for(char c : input.toCharArray()) {
