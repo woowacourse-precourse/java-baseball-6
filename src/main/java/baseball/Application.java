0dd1600694a01512a2a36ba2176eacbc3393aea3
tfer2442 <tfer2442@gmail.com>
2023-10-21 01:13:59 +0900

    refactor(all): import 줄 바꿈 수정(Google Java Style Guide)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,4 @@
 package baseball;
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.List;
 
--- src/main/java/baseball/InputHandler.java
+++ src/main/java/baseball/InputHandler.java
@@ -1,8 +1,6 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class InputHandler {
