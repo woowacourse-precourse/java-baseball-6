384f3441298675aa443e6e5b5f5d6ba5710dbff6
doha <dohaahi@naver.com>
2023-10-21 00:12:46 +0900

    refactor: 기능 요구 사항에 따른 터미널 출력 문구 수정

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class OutputView {
     private static final String START_GAME_MESSAGE = "숫자 야구 게임을 시작합니다.";
-    private static final String INPUT_NUMBERS_MESSAGE = "숫자를 입력해주세요 :";
+    private static final String INPUT_NUMBERS_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String THREE_STRIKE_AND_END_GAME_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String START_NEW_GAME_OR_END_GAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
@@ -13,7 +13,7 @@ public class OutputView {
     }
 
     public void printInputNumbers() {
-        System.out.println(INPUT_NUMBERS_MESSAGE);
+        System.out.print(INPUT_NUMBERS_MESSAGE);
     }
 
     public void printGameResult(final List<Integer> result) {
@@ -26,21 +26,23 @@ public class OutputView {
         }
 
         if (strike == 0 && ball == 0) {
-            System.out.println("낫싱");
+            System.out.println("낫싱\n");
             return;
         }
 
         if (strike > 0 && ball == 0) {
-            System.out.printf("%d스트라이크", strike);
+            System.out.printf("%d스트라이크\n", strike);
             return;
         }
 
         if (strike == 0 && ball > 0) {
-            System.out.printf("%d볼", ball);
+            System.out.printf("%d볼\n", ball);
             return;
         }
 
-        System.out.printf("%d볼 %d스트라이크", ball, strike);
+        System.out.printf("%d볼 %d스트라이크\n", ball, strike);
+    }
+
     public void printStartNewGameOrEndGame() {
         System.out.println(START_NEW_GAME_OR_END_GAME_MESSAGE);
     }
