57f04fae91276ebdb909b7174ae1fab7e4435b6a
elyudwo <dudqk9696@naver.com>
2023-10-20 14:27:00 +0900

    [Feat] : 1~9 사이의 숫자가 아닌 경우 예외 처리 기능 구현

--- src/main/java/baseball/domain/Number.java
+++ src/main/java/baseball/domain/Number.java
@@ -6,7 +6,16 @@ public class Number {
 
     public void validateNumberSize(List<Integer> number) {
         if(number.size() != 3) {
-            throw new IllegalArgumentException("숫자의 개수가 올바르지 않습니다! (숫자는 반드시 세개로 구성되어야합니다).");
+            throw new IllegalArgumentException("숫자의 개수가 올바르지 않습니다. (숫자는 반드시 세개로 구성되어야합니다)");
+        }
+    }
+
+    public void validateNumberLength(List<Integer> number) {
+        boolean isValid = number.stream()
+                .allMatch(numbers -> numbers >= 1 && numbers <= 9);
+
+        if (!isValid) {
+            throw new IllegalArgumentException("숫자는 1에서 9 사이여야 합니다.");
         }
     }
 
