8e49ea611cb19d3e8d478889d502696aa127c95c
plastichong <zakkslash18@naver.com>
2023-10-20 19:25:19 +0900

    feat: 메시지 출력
    GameManager 생성

---
+++ src/main/java/baseball/GameManager.java
@@ -0,0 +1,11 @@
+package baseball;
+
+public class GameManager {
+
+    private final static String  GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+
+    public static void printGameStartMessage() {
+        System.out.println(GAME_START_MESSAGE);
+    }
+
+}
