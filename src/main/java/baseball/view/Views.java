33b3ee5db7b5bbfbec02e74fb0e15b732f87c3eb
Ho-Da-Dak <godqhr721@gmail.com>
2023-10-20 17:49:34 +0900

    게임중 발생하는 출력 메서드 작성

---
+++ src/main/java/baseball/view/Views.java
@@ -0,0 +1,29 @@
+package baseball.view;
+
+public class Views {
+
+    private static final int OUT_COUNT = 3;
+    public static void printIntro() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void printNothing() {
+        System.out.println("낫싱");
+    }
+
+    public static void printStrikeBallCount(int strike,int ball) {
+        System.out.printf("%d볼 %d스트라이크\n",ball,strike);
+    }
+
+    public static void printStrike(int strike) {
+        System.out.printf("%d스트라이크\n" , strike);
+    }
+
+    public static void printBall(int ball) {
+        System.out.printf("%d볼\n" , ball);
+    }
+
+    public static void printOut() {
+        System.out.printf("%d개의 숫자를 모두 맞히셨습니다! 게임 종료",OUT_COUNT);
+    }
+}
