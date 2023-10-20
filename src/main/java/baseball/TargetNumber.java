53b2ccf73ced6003b9bbe9a1c07bab82c03244f8
U-keun Song <sukeun319@gmail.com>
2023-10-20 13:58:32 +0900

    Refactor : 매개 변수 유효성 확인 코드 추가
    generate, has 메서드에 유효한 값이 들어가는지 확인하는 코드를 추가했다. 예외를 던지도록 수정하는 것이 좋을까?

--- src/main/java/baseball/TargetNumber.java
+++ src/main/java/baseball/TargetNumber.java
@@ -17,8 +17,8 @@ class TargetNumber {
     TargetNumber 객체를 생성하는 정적 팩토리 메서드.
      */
     public static TargetNumber generate(int a, int b, int c) {
-        if (a == 0 || b == 0 || c == 0) return new TargetNumber();
-        return new TargetNumber(a, b, c);
+        if (isValidNumber(a) && isValidNumber(b) && isValidNumber(c)) return new TargetNumber(a, b, c);
+        return new TargetNumber(0, 0, 0);
     }
 
     /*
@@ -30,6 +30,9 @@ class TargetNumber {
      */
     public boolean[] has(int num, int idx) {
         boolean[] check = new boolean[2];
+
+        if (!isValidNumber(num) || !isValidIndex(idx)) return check;
+
         for (int i = 0; i < 3; i++) {
             if (number[i] == num) {
                 if (i == idx) check[1] = true;
@@ -50,5 +53,13 @@ class TargetNumber {
             answer[i] = value;
         }
         return answer;
+    }
+
+    private static boolean isValidNumber(int i) {
+        return i >= 1 && i <= 9;
+    }
+
+    private static boolean isValidIndex(int i) {
+        return i >= 0 && i <= 2;
     }
 }
