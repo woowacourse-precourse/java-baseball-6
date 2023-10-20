fc8f920002e088b7b7f3c864695bdac5074256ee
1000zoo <cjswldn99@gmail.com>
2023-10-20 18:02:02 +0900

    test(ErrorDetector): 통과하는 테스트도 추가

--- src/test/java/baseball/ErrorDetectorTest.java
+++ src/test/java/baseball/ErrorDetectorTest.java
@@ -1,13 +1,24 @@
 package baseball;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 class ErrorDetectorTest {
 
     @Test
+    void 숫자입력이_올바른_경우() {
+        ErrorDetector.throwIfNumbersInputInvalid("123");
+    }
+
+    @Test
+    void 옵션입력이_올바른_경우() {
+        ErrorDetector.throwIfOptionInputInvalid("1");
+    }
+
+    @Test
     void 입력에영문이있을경우() {
-        Assertions.assertThrows(
+        assertThrows(
                 IllegalArgumentException.class,
                 () -> ErrorDetector.throwIfNumbersInputInvalid("11as")
         );
@@ -15,7 +26,7 @@ class ErrorDetectorTest {
 
     @Test
     void 입력길이가3이아닌경우() {
-        Assertions.assertThrows(
+        assertThrows(
                 IllegalArgumentException.class,
                 () -> ErrorDetector.throwIfNumbersInputInvalid("1234")
         );
@@ -23,7 +34,7 @@ class ErrorDetectorTest {
 
     @Test
     void 옵션입력에영문이있을경우() {
-        Assertions.assertThrows(
+        assertThrows(
                 IllegalArgumentException.class,
                 () -> ErrorDetector.throwIfOptionInputInvalid("av")
         );
@@ -31,7 +42,7 @@ class ErrorDetectorTest {
 
     @Test
     void 옵션입력이잘못되었을경우() {
-        Assertions.assertThrows(
+        assertThrows(
                 IllegalArgumentException.class,
                 () -> ErrorDetector.throwIfOptionInputInvalid("5")
         );
