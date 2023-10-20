1183ff14a710484318c46f5d1d23a21a8080a533
watermelon3737 <pshrina1@naver.com>
2023-10-20 18:10:14 +0900

    [feat] : 유저 input에 대한 결과를 출력하는 메시지 메서드 구현

--- src/main/java/baseball/Message.java
+++ src/main/java/baseball/Message.java
@@ -16,4 +16,17 @@ public class Message {
     public String requestRetryOrEnd() {
         return "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     }
+
+    public String result(int ball, int strike) {
+        if (ball == 0 && strike == 0) {
+            return "낫싱";
+        }
+        if (ball > 0 && strike == 0) {
+            return ball + "볼";
+        }
+        if (ball == 0 && strike > 0) {
+            return strike + "스트라이크";
+        }
+        return ball + "볼 " + strike + "스트라이크";
+    }
 }
