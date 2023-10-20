09c0d77fa3a9ac0363313daa12231d73e61ca7b8
xogus3492 <wkdxogus3492@gmail.com>
2023-10-20 14:52:15 +0900

    feat(consoleview): 게임 종료 문구 출력 기능 구현

--- src/main/java/baseball/view/ConsoleView.java
+++ src/main/java/baseball/view/ConsoleView.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class ConsoleView {
     private static final String GAME_START_OUTPUT_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String THREE_DIGIT_NUMBER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String GAME_FINISH_OUTPUT_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String STRIKE_MESSAGE = "스트라이크";
     private static final String BALL_MESSAGE = "볼 ";
     private static final String NOTHING_MESSAGE = "낫싱";
@@ -28,5 +29,9 @@ public class ConsoleView {
 
     public static void printNothing() {
         System.out.println(NOTHING_MESSAGE);
+    }
+
+    public static void printGameFinsh() {
+        System.out.println(GAME_FINISH_OUTPUT_MESSAGE);
     }
 }
