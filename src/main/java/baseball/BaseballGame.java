3d48ee5c6d97facd7e8b58f3f393cf7a91e6ac96
Heojamong <wlaud0823@naver.com>
2023-10-20 22:36:21 +0900

    [기능추가]게임결과를 출력하고 3스트라이크인지 판별하는 blooean메소드 생성

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -27,4 +27,29 @@ public class BaseballGame {
         result.add(ball);
         result.add(strike);
     }
+
+    public boolean printGameResult(List<Integer> result) {
+        if (result.get(1) == 3) {
+            System.out.println(result.get(1) + "스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        }
+        if (result.get(0) != 0 && result.get(1) != 0) {
+            System.out.println(result.get(0) + "볼 " + result.get(1) + "스트라이크");
+            return false;
+        }
+        if (result.get(0) != 0 && result.get(1) == 0) {
+            System.out.println(result.get(0) + "볼");
+            return false;
+        }
+        if (result.get(0) == 0 && result.get(1) != 0) {
+            System.out.println(result.get(1) + "스트라이크");
+            return false;
+        }
+        if (result.get(0) == 0 && result.get(1) == 0) {
+            System.out.println("낫싱");
+            return false;
+        }
+        return false;
+    }
 }
