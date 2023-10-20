e00ebe96e0ce6b6bf3b280632ab3be5d381bea55
Identityless <rlandwnd555@naver.com>
2023-10-20 22:35:52 +0900

    feat : 스트라이크/볼 판별을 위한 Validator 클래스 추가

---
+++ src/main/java/baseball/component/Validator.java
@@ -0,0 +1,14 @@
+package baseball.component;
+
+import java.util.*;
+public class Validator {
+
+    private int strike, ball;
+    private boolean noting;
+
+    public Validator() {
+        strike = 0;
+        ball = 0;
+        noting = false;
+    }
+}
