d06c16b95f0ddebcc0c4ff209799db451360c50b
Joohui <joohuii96@gmail.com>
2023-10-20 20:43:48 +0900

    프로젝트 초기화

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,6 @@
 package baseball;
 
-import baseball.controller.GameController;
-
 public class Application {
     public static void main(String[] args) {
-        GameController gameController = new GameController();
-        gameController.gameStart();
     }
 }
--- src/main/java/baseball/view/StartView.java
+++
@@ -1,8 +0,0 @@
-package baseball.view;
-
-public class StartView {
-    private static final String START_GAME_MESSAGE = "숫자 야구 게임을 시작합니다.";
-    public void printStartGame() {
-        System.out.println(START_GAME_MESSAGE);
-    }
-}
