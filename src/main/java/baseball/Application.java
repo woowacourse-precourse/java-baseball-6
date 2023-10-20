f50b3bdfc6d9b4e32ee7e7f1d7e14695b77f685f
dltjdgh0428 <dltjdgh0428@naver.com>
2023-10-20 14:46:31 +0900

    feat: 스트라이크와 볼 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -21,21 +21,54 @@ public class Application {
         }
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-        while(gameChoice){
+        while (gameChoice) {
             System.out.println("숫자를 입력해주세요 : ");
             String userInput = Console.readLine();
-            gameString = check(userInput,computerNum);
+            gameString = check(userInput, computerNum);
 
             System.out.println(gameString); // 진행상황 표시
 
-            if(gameString.equals("3스트라이크")){
+            if (gameString.equals("3스트라이크")) {
                 //이후 게임이 다시 시작할 것 인지 말것인지 구현
             }
         }
 
     }
-    static String check(String userInput, List<Integer> ComputerNum){
-        // Switch case 로 상황에 따른 String 출력하기
-        return null;
+
+    static String check(String userInput, List<Integer> ComputerNum) {
+        int totalCount = 0;
+        int strikeCount = 0;
+        int ballCount = 0;
+        String returnString = "";
+
+        for (int i = 0; i < 3; i++) {
+            if (userInput.contains(ComputerNum.get(i).toString())) {
+                totalCount++;
+            }
+            if (userInput.charAt(i) - 48 == ComputerNum.get(i)) {
+                strikeCount++;
+            }
+        }
+
+        ballCount = totalCount - strikeCount;
+
+        switch (totalCount) {
+            case 0:
+                returnString = "낫싱";
+                break;
+            case 1:
+            case 2:
+                returnString = ballCount + "볼 " + strikeCount + "스트라이크";
+                break;
+            case 3:
+                if (strikeCount == 3)
+                    returnString = strikeCount + "스트라이크";
+                else if (strikeCount == 0 || strikeCount == 1 || strikeCount == 2)
+                    returnString = ballCount + "볼 " + strikeCount + "스트라이크";
+                break;
+            default:
+                break;
+        }
+        return returnString;
     }
 }
