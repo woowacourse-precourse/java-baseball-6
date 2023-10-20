8a041483b88105a2f3b55c17df1a04299479c7ab
HungKungE <jonsun59@gmail.com>
2023-10-20 23:02:53 +0900

    [src/main] 사용자 입력값의 형 변환 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -38,22 +38,39 @@ public class Application {
 
         return true;
     }
-    
+
+    public static List<Integer> convert_user_input(String userInputStr){
+        List<Integer> userInput = new ArrayList<>();
+
+        for (char ch : userInputStr.toCharArray()){
+            userInput.add(Character.getNumericValue(ch));
+        }
+
+        return userInput;
+    }
+
     public static void main(String[] args) {
 
         // 중복 없는 3자리 수 만들기
         List<Integer> computer = get_random_numbers();
 
+        // 사용자 입력 받기
         System.out.println("숫자 야구 게임을 시작합니다.");
-
         System.out.print("숫자를 입력해주세요 : ");
-        String userInput = Console.readLine();
+        String userInputStr = Console.readLine();
         System.out.println();
-        System.out.println("사용자 입력: " + userInput);
+        //System.out.println("사용자 입력: " + userInputStr);
 
-        if (!check_user_input(userInput)){
+        // 사용자 입력 유효성 확인
+        if (!check_user_input(userInputStr)){
             // TODO : IllegalArgumentException 발생 후 종료
             System.out.println("입력 잘못 했네!");
         }
+
+        // 사용자 입력을 String -> List<Integer>로 변환
+        List<Integer> userInput = convert_user_input(userInputStr);
+        System.out.println("사용자 입력: " + userInput);
+
+
     }
 }
