1cf71d4a09d91ddc7d8883df26c65c01c2cbcc2b
kimdohyung <pdohyung@gmail.com>
2023-10-20 19:07:54 +0900

    feat: 컴퓨터 숫자와 사용자의 숫자를 한 자리씩 비교하는 결과 계산 기능 구현

--- src/main/java/baseball/model/Score.java
+++ src/main/java/baseball/model/Score.java
@@ -1,4 +1,27 @@
 package baseball.model;
 
+import java.util.List;
+
 public class Score {
+	private int ball;
+	private int strike;
+
+	public Score() {
+		this.ball = 0;
+		this.strike = 0;
+	}
+
+	public void countScore(List<Integer> userNumber, List<Integer> computerNumber) {
+		for (int i = 0; i < 3; i++) {
+
+			if (userNumber.get(i).equals(computerNumber.get(i))) {
+				this.strike++;
+				continue;
+			}
+
+			if (computerNumber.contains(userNumber.get(i))) {
+				this.ball++;
+			}
+		}
+	}
 }
