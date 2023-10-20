d65a93e8e0d507c7cd8ac95f3c8b97858bb71c74
mummhy0811 <mummhy0811@inu.ac.kr>
2023-10-20 17:23:09 +0900

    Fix: 사용자 숫자 입력 중복 예외처리 추가

--- src/main/java/baseball/model/UserNumber.java
+++ src/main/java/baseball/model/UserNumber.java
@@ -17,6 +17,7 @@ public class UserNumber {
         validateIsNumber(input);
         validateSize(input);
         validateHasZero(input);
+        validateDuplicate(input);
     }
 
     private void validateIsNumber(String input){
@@ -34,6 +35,12 @@ public class UserNumber {
     private void validateHasZero(String input){
         if(input.contains("0"))
             throw new IllegalArgumentException("1-9 사이의 숫자만 입력 가능합니다.");
+    }
+
+    private void validateDuplicate(String input){
+        char[] inputToArray = input.toCharArray();
+        if(inputToArray[0]==inputToArray[1] || inputToArray[0]==inputToArray[2] || inputToArray[1]==inputToArray[2])
+            throw new IllegalArgumentException("세 자리 수는 모두 달라야합니다.");
     }
 
 }
