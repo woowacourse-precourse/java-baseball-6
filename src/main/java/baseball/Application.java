77999a08e6aaaafde3699e1eee03d5c311503b6d
pwrwpw <dlgustjr9912@gmail.com>
2023-10-21 02:00:41 +0900

    feat(Application): 테스트 코드 삭제 및 gameStart 메서드 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,11 @@
 package baseball;
 
+import baseball.controller.GameController;
 import baseball.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-        inputView.readNumber();
+        GameController gameController = new GameController();
+        gameController.gameStart();
     }
 }
