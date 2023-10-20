85569cfa8eb547b4dbfcdcafaad0a9049506444f
noxknow <chris2769@naver.com>
2023-10-20 14:03:34 +0900

    feat(ErrorHandler) : 중복된 숫자와 3개가 아닌 숫자를 입력한 경우에 대한 예외처리 메서드 구현

---
+++ src/main/java/baseball/handler/ErrorHandler.java
@@ -0,0 +1,17 @@
+package baseball.handler;
+
+public enum ErrorHandler {
+
+    INVALID_COUNT("3개의 숫자만 입력해야 합니다."),
+    DUPLICATE_NUMBER("입력한 값은 중복될 수 없습니다.");
+
+    private final String errorMessage;
+
+    ErrorHandler(String message) {
+        errorMessage = "[ERROR] " + message;
+    }
+
+    public RuntimeException getException() {
+        return new IllegalArgumentException(errorMessage);
+    }
+}
