81f319f0e4d0f543521d33cac378f92fcc6e95fd
freekyr7529 <freekyr7529@gmail.com>
2023-10-20 19:59:25 +0900

    refactor: 반환형 변경
    InputView클래스의 inputGuideMsg 메서드의 반환형을 Integer형에서 List<Integer>로 변경
    
    이에 따라 InputView 클래스 내부에 Integer형 변수를 List<Integer>로 변경하는 inputConverter 메서드 추가

--- src/main/java/baseball/InputView.java
+++ src/main/java/baseball/InputView.java
@@ -1,6 +1,8 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InputView {
 
@@ -8,18 +10,29 @@ public class InputView {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    public Integer inputGuideMsg() throws IllegalArgumentException {
+    public List<Integer> inputGuideMsg() throws IllegalArgumentException {
         System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
 
         userInputValidation(input);
 
-        return Integer.parseInt(input);
+        return inputConverter(Integer.parseInt(input));
     }
 
     private void userInputValidation(String input) throws IllegalArgumentException {
         if (input.length() != 3) {
             throw new IllegalArgumentException("입력값이 잘못되었습니다.");
         }
+    }
+
+    private List<Integer> inputConverter(Integer input) {
+        List<Integer> convertedInput = new ArrayList<>();
+
+        for (int i = 100; i >= 1; i /= 10) {
+            convertedInput.add(input / i);
+            input %= i;
+        }
+
+        return convertedInput;
     }
 }
