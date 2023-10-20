4b5fd9790feb913e05faef75202ddec93ea57003
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-10-20 20:03:55 +0900

    feat: Update Play class

--- src/main/java/baseball/Play.java
+++ src/main/java/baseball/Play.java
@@ -1,5 +1,35 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Play {
 
+    private final User user;
+    private final Computer com;
+    private final Rules rules;
+    private String input;
+
+    public Play() {
+        user = new User();
+        com = new Computer();
+        rules = new Rules();
+    }
+
+    public void gameStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        com.getIntArray();
+    }
+
+    public void getInput(String msg) {
+        System.out.print("숫자를 입력해주세요 : ");
+        input = Console.readLine();
+    }
+
+    public void RestartOrFinishGame() {
+
+    }
+
+    public void runGame() {
+
+    }
 }
