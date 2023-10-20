b30e792ba0902698c2e068ae86ea169c29d16274
김효준 <khyojun1104@gmail.com>
2023-10-20 22:36:42 +0900

    [feat] BaseballService : Strike 연산 로직 기능 구현!

--- src/main/java/baseball/service/BaseballService.java
+++ src/main/java/baseball/service/BaseballService.java
@@ -1,5 +1,24 @@
 package baseball.service;
 
+import baseball.dto.GameStateDto;
+
 public class BaseballService {
 
+
+
+    public void caculate(String userNumber, String nowComNumber) {
+        GameStateDto gameStateDto = new GameStateDto();
+        calculateStrike(userNumber, nowComNumber, gameStateDto);
+
+
+    }
+
+    private void calculateStrike(String userNumber, String nowComNumber,
+        GameStateDto gameStateDto) {
+        for(int i=0; i<3; i++){
+            if(userNumber.charAt(i) == nowComNumber.charAt(i)) {
+                gameStateDto.addStrike();
+             }
+        }
+    }
 }
