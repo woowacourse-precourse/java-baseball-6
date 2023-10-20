e481a1975dd4a34e5f2deb1ab3699fb9307def20
TaeHye0n <thk001229@gmail.com>
2023-10-20 20:57:04 +0900

    feat: 게임 시작 메시지 출력  - 출력 메시지를 담당하는 OutputView 클래스  - 게임 시작 메시지를 출력하는 public 메서드 printStartMessage

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,10 @@
+package baseball.view;
+
+public class OutputView {
+
+    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+
+    public void printStartMessage() {
+        System.out.println(GAME_START_MESSAGE);
+    }
+}
