b83d45dfb65621b444e2a4034c034c7b101e0655
Jiyoon Kim <rlawldbs0911@naver.com>
2023-10-20 17:47:24 +0900

    feat(Computer): 컴퓨터가 1에서 9까지 서로 다른 임의의 수를 선택하는 메서드 구현

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,26 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private static final int COMPUTER_NUMBER_COUNT = 3;
+    private static final int MINIMUM_NUMBER = 1;
+    private static final int MAXIMUM_NUMBER = 9;
+    private final List<Integer> numbers = new ArrayList<>();
+
+    public void selectNumbers() {
+        while (numbers.size() < COMPUTER_NUMBER_COUNT) {
+            int randomNumber = Randoms.pickNumberInRange(MINIMUM_NUMBER, MAXIMUM_NUMBER);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+    }
+
+    public void getNumbers() {
+        numbers.forEach(System.out::println);
+    }
+}
---
+++ src/main/java/baseball/GameController.java
@@ -0,0 +1,9 @@
+package baseball;
+
+public class GameController {
+    Computer computer = new Computer();
+
+    public void start() {
+        computer.selectNumbers();
+    }
+}
