f9087e4a77a9e14713d3879a03982eaaa0f6e9fd
황중섭 <jshwang9709@gmail.com>
2023-10-20 23:53:22 +0900

    feat: Computer 클래스 생성

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,26 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+
+    private final List<Integer> answer;
+
+    public Computer() {
+        this.answer = new ArrayList<>();
+        while (answer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!answer.contains(randomNumber)) {
+                answer.add(randomNumber);
+            }
+        }
+    }
+
+    public List<Integer> getAnswer() {
+        return this.answer;
+    }
+
+}
