7635311d69929479c5a69309ac343b4ff654229d
PARKHWANYEONG <welcome0828@naver.com>
2023-10-20 14:30:16 +0900

    feat(validator): 입력받는 값이 유효한지 검증

---
+++ src/main/java/baseball/validator/Validator.java
@@ -0,0 +1,52 @@
+package baseball.validator;
+
+import java.util.HashSet;
+
+public class Validator {
+    public static void isValidNumbers(String numbers) {
+        if(!isValidDigit(numbers)){
+            throw new IllegalArgumentException("3자리가 아닙니다");
+        }
+        if (!isInteger(numbers)) {
+            throw new IllegalArgumentException("입력한 값이 정수가 아닙니다.");
+        }
+        if (hasDuplicate(numbers)) {
+            throw new IllegalArgumentException("입력한 값에 중복된 값이 있습니다.");
+        }
+        if (hasZero(numbers)) {
+            throw new IllegalArgumentException("입력한 값에 0이 있습니다. 1~9의 숫자를 입력하세요.");
+        }
+    }
+
+    /**
+     *  입력된 값에 0이 있을 경우 isInteger()로 예외 처리가 안되므로
+     *  hasZero()로 예외 처리를 해준다.
+     */
+    public static boolean hasZero(String numbers) {
+        return numbers.contains("0");
+    }
+    public static boolean isValidDigit(String numbers) {
+        return numbers.length() == 3;
+    }
+
+    public static boolean hasDuplicate(String numbers) {
+        HashSet<Character> set = new HashSet<>();
+        for (char ch : numbers.toCharArray()) {
+            set.add(ch);
+        }
+        if (set.size() != numbers.length()) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isInteger(String numbers) {
+        try {
+            Integer.parseInt(numbers);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+}
