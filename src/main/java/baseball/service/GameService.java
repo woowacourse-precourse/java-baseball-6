84b9baf1b938aa866c30ee53ae8a92d0fae42179
BullChallenger <am23am45@naver.com>
2023-10-20 16:25:11 +0900

    [REFACTOR] GameService 인터페이스 이름 오타 수정

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -2,7 +2,7 @@ package baseball.service;
 
 import java.util.List;
 
-public interface GamsService {
+public interface GameService {
 
     boolean process(List<Integer> numbers, int[] inputNumbers);
 
