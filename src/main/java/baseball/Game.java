e0f69e077d506b0ef76eac9e5a555b3a97c8eeea
GiJung <weeds2577@gmail.com>
2023-10-21 00:47:14 +0900

    feat: 랜덤 3자리 숫자와 사용자 숫자 비교 기능 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -7,28 +7,57 @@ public class Game {
     private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
 
     private InputValue inputValue = new InputValue();
+    private List<Integer> computerNumber;
+    private List<Integer> userNumber;
+
     private boolean exit = false;
+    private int strike = 0;
+    private int ball = 0;
 
     public void Game() {}
 
     public void gameStart() {
 
-        Number computerNumber = new Number();
-        computerNumber.randomNumberGenerator();
-        Number userNumber = new Number();
+        Number computer = new Number();
+        computer.randomNumberGenerator();
+        computerNumber = computer.getNumber();
+
+        Number user = new Number();
 
         startMessage();
 
         while(!exit) {
             String inputNumber = inputValue.inputAnswer();
-            userNumber.userNumberGenerator(inputNumber);
+            user.userNumberGenerator(inputNumber);
+            userNumber = user.getNumber();
+
+            resetScore();
+            calculateScore();
 
             return;
         }
     }
 
-    public void startMessage() {
+    private void startMessage() {
         System.out.println(START_MESSAGE);
     }
 
+    private void resetScore() {
+        strike = 0;
+        ball = 0;
+    }
+
+    private void calculateScore() {
+        for(int i = 0; i < 3; i++) {
+            int selectNumber = userNumber.get(i);
+            if(computerNumber.get(i) == selectNumber){
+                strike++;
+                continue;
+            }
+
+            if(computerNumber.contains(selectNumber)){
+                ball++;
+            }
+        }
+    }
 }
