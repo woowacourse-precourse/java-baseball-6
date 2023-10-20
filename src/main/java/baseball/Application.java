eeb2f08dcd5df5c4ab2337aae33cb891310a3125
Yehyeok Bang <qkddpgur318@gmail.com>
2023-10-20 23:15:40 +0900

    feat: 사용자 입력 검증

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -25,6 +25,18 @@ public class Application {
             for (String number : input.split("")) {
                 user.add(Integer.parseInt(number));
             }
+
+            if (user.size() != 3) {
+                throw new IllegalArgumentException("3자리 숫자를 입력해주세요.");
+            }
+
+            if (user.get(0) < 1 || user.get(0) > 9 || user.get(1) < 1 || user.get(1) > 9 || user.get(2) < 1 || user.get(2) > 9) {
+                throw new IllegalArgumentException("1부터 9까지의 숫자만 입력해주세요.");
+            }
+
+            if (user.get(0).equals(user.get(1)) || user.get(0).equals(user.get(2)) || user.get(1).equals(user.get(2))) {
+                throw new IllegalArgumentException("중복된 숫자가 있습니다.");
+            }
         }
     }
 }
