c10badea610042bc41888c4974de92e6b9b01b73
Juhyorim <joohr1234@gmail.com>
2023-10-20 16:51:14 +0900

    feat: 반복적으로 사용자의 입력을 받아 검증
    사용자의 입력이 세 글자인지, 1-9사이 글자인지, 모두 다른 숫자인지 검증함

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,48 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
+            String input = Console.readLine().strip();
+            checkInput(input);
+        }
+    }
+
+    private static void checkInput(String input) {
+        //세 글자인지 확인
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("세 글자가 아닙니다");
+        }
+
+        //1-9 사이 숫자인지 확인
+        if (!input.matches("[1-9]+")) {
+            throw new IllegalArgumentException("1에서 9사이 숫자가 아닙니다");
+        }
+        
+        //셋 다 다른 숫자인지 확인
+        if (hasSameNumber(input.charAt(0), input.charAt(1), input.charAt(2))) {
+            throw new IllegalArgumentException("중복되는 숫자가 존재합니다");
+        }
+    }
+
+    private static boolean hasSameNumber(char c1, char c2, char c3) {
+        if (c1==c2) {
+            return true;
+        }
+
+        if (c2==c3) {
+            return true;
+        }
+
+        if (c3==c1) {
+            return true;
+        }
+
+        return false;
     }
 }
