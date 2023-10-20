8ebc6c82e4cd77c037f4a5fcf8763c47e22fe6a2
pgd <rudeh1253@gmail.com>
2023-10-20 09:35:49 +0900

    feat: define Validator for validation of input

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -1,2 +1,39 @@
-package baseball;public class Validator {
+package baseball;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * For validating the user input.
+ * Validation rule:
+ *     1. The input should 3 integers, each of which ranges from 1 to 9.
+ *     2. Length of input should be 3. Not longer and not shorter.
+ *     3. The input is a string, which isn't yet parsed into an integer.
+ *     4. No duplicate is allowed.
+ */
+public class Validator {
+
+    /**
+     * Validate input whether it follows the rule specified above.
+     * If input isn't valid, throws an exception.
+     * @param input of the user to validate.
+     * @throws IllegalStateException if the input doesn't follow the rule, this exception is thrown.
+     */
+    public void validate(String input) {
+        Pattern pattern = Pattern.compile("[1-9][1-9][1-9]");
+        Matcher matcher = pattern.matcher(input);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("입력이 잘못됐습니다.");
+        }
+        String[] split = input.split("");
+        if (hasDuplicate(split)) {
+            throw new IllegalArgumentException("입력이 잘못됐습니다.");
+        }
+    }
+
+    private boolean hasDuplicate(String[] chars) {
+        return chars[0].equals(chars[1])
+                || chars[0].equals(chars[2])
+                || chars[1].equals(chars[2]);
+    }
 }
