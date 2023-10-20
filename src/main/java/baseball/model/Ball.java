a83f2992c0ec116b9cb29795fdaaa88f668f20da
Dreaming-J <kimjjong321@naver.com>
2023-10-21 00:29:09 +0900

    :white_check_mark: test. 야구공 1개 판별

--- src/main/java/baseball/model/Ball.java
+++ src/main/java/baseball/model/Ball.java
@@ -1,5 +1,7 @@
 package baseball.model;
 
+import java.util.Objects;
+
 public class Ball {
     private final int position;
     private final int number;
@@ -10,6 +12,27 @@ public class Ball {
     }
 
     public BallStatus play(Ball ball) {
-        return BallStatus.BALL;
+        if (this.equals(ball)) return BallStatus.STRIKE;
+
+        if (ball.matchBallNo(this.number)) return BallStatus.BALL;
+
+        return BallStatus.NOTHING;
+    }
+
+    private boolean matchBallNo(int number) {
+        return this.number == number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ball ball = (Ball) o;
+        return position == ball.position && number == ball.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position, number);
     }
 }
--- src/test/java/baseball/model/BallTest.java
+++ src/test/java/baseball/model/BallTest.java
@@ -18,4 +18,14 @@ public class BallTest {
     void strike() {
         assertThat(com.play(new Ball(0, 5))).isEqualTo(BallStatus.STRIKE);
     }
+
+    @Test
+    void ball() {
+        assertThat(com.play(new Ball(2, 5))).isEqualTo(BallStatus.BALL);
+    }
+
+    @Test
+    void nothing() {
+        assertThat(com.play(new Ball(0, 4))).isEqualTo(BallStatus.NOTHING);
+    }
 }
