d5bdb4fe59aa349fa19404ad8a603e5f58510f1b
San Kim <rlatks1217@gmail.com>
2023-10-20 11:52:03 +0900

    feat AnswerCreator 클래스 생성, 정답 생성 기능 구현

---
+++ src/main/java/Service/AnswerCreator.java
@@ -0,0 +1,24 @@
+package Service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class AnswerCreator {
+	private final Validator validator;
+	public AnswerCreator() {
+		this.validator = new Validator();
+	}
+
+	public int createAnswer() {
+		int answerNumber = 0;
+		int multiplyNumber = 1;
+		for (int i = 0; i < 3; i++) {
+			answerNumber += Randoms.pickNumberInRange(1, 9)*multiplyNumber;
+			multiplyNumber *= 10;
+		}
+		if (!validator.checkDuplication(answerNumber)) {
+			answerNumber = createAnswer();
+		}
+		return answerNumber;
+	}
+
+}
--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,15 +1,19 @@
 package controller;
 
+import Service.AnswerCreator;
 import views.InputView;
 
 public class Controller {
+	private final AnswerCreator answerCreator;
 	private final InputView inputView;
 	public Controller() {
+		this.answerCreator = new AnswerCreator();
 		this.inputView = new InputView();
 	}
 
 	public void playGame() {
 		inputView.printIntro();
+		int answerNumber = answerCreator.createAnswer();
 		int inputNumber = inputView.getInputNumber();
 	}
 }
