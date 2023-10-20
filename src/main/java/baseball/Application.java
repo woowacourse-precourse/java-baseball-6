99ba48f966b6be3fc5e96d8329e36b1451be8a38
dltjdgh0428 <dltjdgh0428@naver.com>
2023-10-20 14:11:56 +0900

    feat: 숫자 입력의 반복기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,23 +1,41 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+
         boolean gameChoice = true;
         List<Integer> computerNum = new ArrayList<>();
         String gameString = "";
-        // 컴퓨터 숫자를 생성함. 차후 메소드로 바꿀것
+        // 컴퓨터 숫자를 생성함. 차후 메소드로 분리
         while (computerNum.size() < 3) {
             int num = Randoms.pickNumberInRange(1, 9);
             if (!computerNum.contains(num)) {
                 computerNum.add(num);
             }
         }
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        while(gameChoice){
+            System.out.println("숫자를 입력해주세요 : ");
+            String userInput = Console.readLine();
+            gameString = check(userInput,computerNum);
+
+            System.out.println(gameString); // 진행상황 표시
+
+            if(gameString.equals("3스트라이크")){
+                //이후 게임이 다시 시작할 것 인지 말것인지 구현
+            }
+        }
+
+    }
+    static String check(String userInput, List<Integer> ComputerNum){
+        // Switch case 로 상황에 따른 String 출력하기
+        return null;
     }
 }
