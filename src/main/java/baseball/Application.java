d87e9118cefc59b4ca76eb54ebe9d9b1244a405b
SCY <helenason@naver.com>
2023-10-20 16:43:46 +0900

    feat: 사용자의 수와 컴퓨터의 수 비교 후 해당 결과 출력 반복

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -18,13 +18,55 @@ public class Application {
         }
 
         System.out.println("숫자 야구 게임을 시작합니다.");
-        System.out.print("숫자를 입력해주세요 : ");
 
-        String input = Console.readLine();
+        boolean isAnswer = false;
+        while (!isAnswer) {
+            System.out.print("숫자를 입력해주세요 : ");
+            String input = Console.readLine();
 
-        if (!input.matches("[1-9]{3}")) {
-            throw new IllegalArgumentException();
+            if (!input.matches("[1-9]{3}")) {
+                throw new IllegalArgumentException();
+            }
+            isAnswer = compare(computer, input);
         }
+    }
 
+    private static boolean compare(List<Integer> computer, String input) {
+
+        int ball = 0;
+        int strike = 0;
+
+        for (int i = 0; i < 3; i++) {
+            String computerNumber = Integer.toString(computer.get(i));
+            if (!input.contains(computerNumber)) {
+                continue;
+            }
+            int indexOfInput = input.indexOf(computerNumber);
+            if (indexOfInput == i) {
+                strike += 1;
+            } else {
+                ball += 1;
+            }
+        }
+        return printResult(ball, strike);
+    }
+
+    private static boolean printResult(int ball, int strike) {
+
+        if (ball == 0 && strike == 0) {
+            System.out.println("낫싱");
+        } else if (strike == 3) {
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            return true;
+        } else if (strike == 0) {
+            System.out.println(ball + "볼");
+        } else if (ball == 0) {
+            System.out.println(strike + "스트라이크");
+        } else {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
+        return false;
     }
 }
