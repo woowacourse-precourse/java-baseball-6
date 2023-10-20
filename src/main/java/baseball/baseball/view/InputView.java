e64334b4b931afb19e20cb9481d2b01a996f8962
Hajin-Lee0406 <glgkwls1@naver.com>
2023-10-20 23:18:00 +0900

    feat: view 싱글톤 패턴 적용

--- src/main/java/baseball/baseball/view/InputView.java
+++ src/main/java/baseball/baseball/view/InputView.java
@@ -1,4 +1,13 @@
 package baseball.baseball.view;
 
 public class InputView {
+
+    private static final InputView instance = new InputView();
+
+    public static InputView getInstance() {
+        return instance;
+    }
+
+    private InputView() {
+    }
 }
--- src/main/java/baseball/baseball/view/OutputView.java
+++ src/main/java/baseball/baseball/view/OutputView.java
@@ -1,4 +1,12 @@
 package baseball.baseball.view;
 
 public class OutputView {
+    private static final OutputView instance = new OutputView();
+
+    public static OutputView getInstance() {
+        return instance;
+    }
+
+    private OutputView() {
+    }
 }
