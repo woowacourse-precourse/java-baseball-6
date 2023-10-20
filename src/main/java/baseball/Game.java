4b452ec1c1481f89782e41887b198e86aa5422b2
yhcho0405 <yhcho0405@kakao.com>
2023-10-20 15:36:55 +0900

    feat: 정답일 경우 3스트라이크 출력

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -3,13 +3,28 @@ package baseball;
 import baseball.handler.UserInputHandler;
 
 public class Game {
+
     private final UserInputHandler userInputHandler = new UserInputHandler();
+    private boolean isCorrect = false;
 
     public void play() {
-        System.out.println("숫자를 입력해주세요 : ");
+        String computerNumber = "425";
+        while (!isCorrect) {
+            String userInputNumber = userInputHandler.askUserInputNumber();
+            isCorrect = getCorrect(userInputNumber, computerNumber);
+        }
     }
 
     public boolean checkRestart() {
         return userInputHandler.askRestart();
+    }
+
+    private boolean getCorrect(String userInput, String computerNumber) {
+        if (userInput.equals(computerNumber)) {
+            System.out.println("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        }
+        // TODO: 게임 결과 계산
+        return false;
     }
 }
--- src/main/java/baseball/handler/UserInputHandler.java
+++ src/main/java/baseball/handler/UserInputHandler.java
@@ -4,6 +4,11 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class UserInputHandler {
 
+    public String askUserInputNumber() {
+        System.out.println("숫자를 입력해주세요 : ");
+        return Console.readLine();
+    }
+
     public boolean askRestart() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String input = Console.readLine();
