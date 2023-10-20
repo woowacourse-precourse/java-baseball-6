dc3628b1d6ad52ad564dbf6d54b81d0dc7804268
haruday97 <starharu97@naver.com>
2023-10-21 03:37:06 +0900

    refactor: 수정 가독성 향상을 위한 메서드 및 상수 추가

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -15,32 +15,45 @@ public class GameController {
     Validator validator = new Validator();
     List<Integer> computerNumbers = new ArrayList<>();
 
-    private static final String RESTART = "1";
     private static final String EXIT = "2";
+    private static final String MSG_START = "숫자 야구 게임을 시작합니다.";
+    private static final String MSG_QUERY = "숫자를 입력해주세요.";
+    private static final String MSG_GAMEOVER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String MSG_ASK_IF_CONTINUES = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String MSG_EXCEPTION_INVALID_INPUT = "올바르지 않은 입력값입니다.";
+    private static boolean playing = true;
 
     private void init() {
         computerNumbers.clear();
     }
 
     public void start() {
-        while (true) {
+        System.out.println(MSG_START);
+        while (playing) {
             if (computerNumbers.isEmpty()) computerNumbers = service.generateNumbers();
-            System.out.println("숫자를 입력해주세요.");
-            System.out.println("치트: " + computerNumbers.get(0) + computerNumbers.get(1) + computerNumbers.get(2));
-            String input = Console.readLine();
-            List<Integer> userNumbers = parser.parseInputToList(input);
-            if (validator.isInvalid(userNumbers)) throw new IllegalArgumentException("올바르지 않은 입력값입니다.");
+            System.out.println(MSG_QUERY);
+            List<Integer> userNumbers = receiveInput();
             int[] result = service.compare(computerNumbers, userNumbers);
             service.printResult(result);
             if(service.isThreeStrikes(result)){
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                System.out.println(MSG_GAMEOVER);
+                System.out.println(MSG_ASK_IF_CONTINUES);
                 String restartInput = Console.readLine();
-                if(validator.isInvalid(restartInput)) throw new IllegalArgumentException("올바르지 않은 입력값입니다.");
-                if(restartInput.equals(RESTART)) init();
-                if(restartInput.equals(EXIT)) return;
+                determineRestart(restartInput);
             }
-
         }
+    }
+
+    private List<Integer> receiveInput(){
+        String input = Console.readLine();
+        List<Integer> userNumbers = parser.parseInputToList(input);
+        if (validator.isInvalid(userNumbers)) throw new IllegalArgumentException(MSG_EXCEPTION_INVALID_INPUT);
+        return userNumbers;
+    }
+
+    private void determineRestart(String restartInput) {
+        if (validator.isInvalid(restartInput)) throw new IllegalArgumentException(MSG_EXCEPTION_INVALID_INPUT);
+        init();
+        if(restartInput.equals(EXIT)) playing = false;
     }
 }
