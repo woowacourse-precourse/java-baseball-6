38790567c9d5e203b08b8ece6aff45ef9d4b0e2f
kimdohyung <pdohyung@gmail.com>
2023-10-20 17:35:33 +0900

    feat: 컴퓨터 숫자 리스트를 생성하는 기능과 getter 구현

--- src/main/java/baseball/model/ComputerNumber.java
+++ src/main/java/baseball/model/ComputerNumber.java
@@ -1,4 +1,29 @@
 package baseball.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class ComputerNumber {
+	private List<Integer> computerNumber;
+
+	public ComputerNumber() {
+		this.computerNumber = computerNumber();
+	}
+
+	public List<Integer> getComputerNumber() {
+		return computerNumber;
+	}
+
+	private List<Integer> computerNumber() {
+		List<Integer> computer = new ArrayList<>();
+		while (computer.size() < 3) {
+			int randomNumber = Randoms.pickNumberInRange(1, 9);
+			if (!computer.contains(randomNumber)) {
+				computer.add(randomNumber);
+			}
+		}
+		return computer;
+	}
 }
