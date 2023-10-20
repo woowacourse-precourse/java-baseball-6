473139a16e46b7bc6afd64dc08ac41ce9e9404f2
Haaae <101785628+Haaae@users.noreply.github.com>
2023-10-20 22:10:29 +0900

    fix: pickNumberInRange()를 사용하도록 수정

--- src/main/java/baseball/domain/BaseballNumberMaker.java
+++ src/main/java/baseball/domain/BaseballNumberMaker.java
@@ -1,22 +1,27 @@
 package baseball.domain;
 
-import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
-
 import baseball.domain.gamedata.BaseballNumber;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class BaseballNumberMaker {
 
-    public static final int START_INCLUSIVE = 0;
+    public static final int START_INCLUSIVE = 1;
     public static final int END_INCLUSIVE = 9;
     public static final int COUNT = 3;
 
     public BaseballNumber make() {
-        return new BaseballNumber(
-                Collections.unmodifiableList(
-                        pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, COUNT)
-                )
-        );
+
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < COUNT) {
+            int randomNumber = Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+        return new BaseballNumber(Collections.unmodifiableList(numbers));
     }
 
 
