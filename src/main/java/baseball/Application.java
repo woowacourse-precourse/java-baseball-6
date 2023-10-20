aeb65b6a03a9791194234db5f7e4a617cbd768d0
noxknow <chris2769@naver.com>
2023-10-20 14:48:03 +0900

    update(Application) : ConsoleInput객체에 대한 의존성 주입 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import baseball.controller.BaseballGameController;
+import baseball.handler.InputHandler;
+import baseball.view.ConsoleInput;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        final InputHandler inputHandler = new ConsoleInput();
+
+        new BaseballGameController(inputHandler).run();
     }
 }
