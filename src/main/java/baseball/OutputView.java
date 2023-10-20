944e0ee87709bbb956b0d420332c5b34c893ae45
Big-Cir97 <eodnjs01477@gmail.com>
2023-10-20 17:44:11 +0900

    feat: OutputView 정적 상수 정의

---
+++ src/main/java/baseball/OutputView.java
@@ -0,0 +1,11 @@
+package baseball;
+
+public class OutputView {
+  private static final String START_GAME_MESSAGE = "숫자 야구 게임을 시작합니다.";
+  private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+  private static final String HIT_BALL_MESSAGE = "";
+  private static final String HIT_STRIKE_MESSAGE = "";
+  private static final String HIT_NOTHING_MESSAGE = "";
+  private static final String GAME_SUCCESS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+  private static final String GAME_RESTART_OR_END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+}
