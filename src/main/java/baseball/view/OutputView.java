fe4dc4e0da591ff81b1b69184532f33f8753ed40
Hanjaemo <hjm2530@gmail.com>
2023-10-21 02:36:06 +0900

    feat(OutputView): 컴퓨터의 수와 플레이어의 수를 비교한 결과 출력 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -6,6 +6,22 @@ public class OutputView {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
+    public static void printBallCountResult(int countOfBall, int countOfStrike) {
+        if (countOfBall == 0 && countOfStrike == 0) {
+            System.out.println("낫싱");
+            return;
+        }
+        if (countOfStrike == 0) {
+            System.out.println(countOfBall + "볼");
+            return;
+        }
+        if (countOfBall == 0) {
+            System.out.println(countOfStrike + "스트라이크");
+            return;
+        }
+        System.out.println(countOfBall + "볼 " + countOfStrike + "스트라이크");
+    }
+
     public static void printEndingMessageForAllStrike(int digitNumber) {
         System.out.println(digitNumber + "스트라이크\n" +
                 digitNumber + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
