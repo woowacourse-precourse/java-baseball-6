6a7203eb3c4033459a06d12493c708f422ac6123
shin5774 <babo5774@naver.com>
2023-10-20 13:37:13 +0900

    Refactor: "Numbers 값 검증 기능 개선"
    - validate에서 3개로 뻗어나가는것이 아닌 각 메서드에서 throw를 발생시킬수 있게 함으로 예외처리를 구현. 메서드 이름 변환을 통해 가시성을 증가시킴

--- src/main/java/baseball/domain/Numbers.java
+++ src/main/java/baseball/domain/Numbers.java
@@ -7,29 +7,29 @@ public class Numbers {
     private final String numbers;
 
     public Numbers(String numbers){
-        if(!validate(numbers)){
-            throw new IllegalArgumentException();
-        }
+        validateSize(numbers);
+        validateType(numbers);
+        validateUnique(numbers);
+
         this.numbers=numbers;
     }
 
-    private boolean validate(String numbers){
-        return isCorrectSize(numbers) && isConsistofNumbers(numbers)
-                && isNotDuplicate(numbers);
+    private void validateSize(String numbers){
+        if (numbers.length()!=3){
+            throw new IllegalArgumentException("값은 3자리 수여야 합니다.");
+        }
     }
 
-    private boolean isCorrectSize(String numbers){
-        return numbers.length()==3;
+    private void validateType(String numbers){
+        if (!numbers.matches("[0-9]+")){
+            throw new IllegalArgumentException("값은 숫자로만 구성 되어야 합니다.");
+        }
     }
 
-    private boolean isConsistofNumbers(String numbers){
-        return numbers.matches("[0-9]+");
-    }
-
-    private boolean isNotDuplicate(String numbers){
-        HashSet<String> uniqueNumbers=new HashSet<>(Arrays.asList(numbers.split("")));
-
-        return uniqueNumbers.size()==3;
+    private void validateUnique(String numbers){
+        if(new HashSet<>(Arrays.asList(numbers.split(""))).size()!=3){
+            throw new IllegalArgumentException("값의 중복은 없어야 합니다.");
+        }
     }
 
     public String getNumbers(){
