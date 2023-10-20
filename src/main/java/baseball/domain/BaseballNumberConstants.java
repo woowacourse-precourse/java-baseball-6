09a5983270070017e2e8dd9edf43c098133a50b8
hellomatia <shuaigejp80@gmail.com>
2023-10-20 12:01:59 +0900

    refactor(domain): BaseballNumberConstants 클래스의 상수 접근 제한자 수정
    - BaseballNumberConstants 클래스의 BASEBALL_NUMBER_LEN 상수의 접근 제한자를 `public`으로 수정하였습니다.
    - BASEBALL_NUMBER_LEN 상수를 외부에서 더 쉽게 접근할 수 있도록 허용하였습니다.

--- src/main/java/baseball/domain/BaseballNumberConstants.java
+++ src/main/java/baseball/domain/BaseballNumberConstants.java
@@ -3,5 +3,5 @@ package baseball.domain;
 public class BaseballNumberConstants {
     static final int BASEBALL_MIN_NUMBER = 1;
     static final int BASEBALL_MAX_NUMBER = 9;
-    static final int BASEBALL_NUMBER_LEN = 3;
+    public static final int BASEBALL_NUMBER_LEN = 3;
 }
