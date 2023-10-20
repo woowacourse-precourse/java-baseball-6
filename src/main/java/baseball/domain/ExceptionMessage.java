b022da918c3827c5654ec586ac950e4769180764
soochan <tncksdl05@gmail.com>
2023-10-20 19:40:40 +0900

    refactor: 객체 생성을 제한하기 위해 private 생성자 추가

--- src/main/java/baseball/domain/ExceptionMessage.java
+++ src/main/java/baseball/domain/ExceptionMessage.java
@@ -7,4 +7,7 @@ public final class ExceptionMessage {
     public static final String BASEBALL_NUMBER_DUPLICATE_EXCEPTION_MESSAGE = "야구 숫자 목록은 중복되지 않아야 합니다.";
     public static final String RESTART_EXCEPTION_MESSAGE = "게임 재시작 번호가 잘못되었습니다.";
 
+    private ExceptionMessage() {
+    }
+
 }
