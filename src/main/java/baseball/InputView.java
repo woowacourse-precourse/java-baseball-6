9b0c37ac27b7249b4b9d6c761161378b6263b988
kunsanglee <gosmdochee2@gmail.com>
2023-10-20 12:56:04 +0900

    refactor(InputView): 숫자를 입력해주세요 : 문자열을 상수로 선언

--- src/main/java/baseball/InputView.java
+++ src/main/java/baseball/InputView.java
@@ -4,8 +4,10 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    public static final String BASEBALL_NUMBER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+
     public static String baseBallNumber() {
-        System.out.print("숫자를 입력해주세요 : ");
+        System.out.print(BASEBALL_NUMBER_INPUT_MESSAGE);
         return InputValidator.validateBaseBallNumber(Console.readLine());
     }
 }
