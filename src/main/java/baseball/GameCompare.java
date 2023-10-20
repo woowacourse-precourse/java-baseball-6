54a6378d75f789b99418ea268c95ddf27e3695c5
Jung Yunho <a01049048063@gmail.com>
2023-10-20 15:40:12 +0900

    fix(GameCompare): ball이 strike까지 중복해서 계산하는 로직 수정

--- src/main/java/baseball/GameCompare.java
+++ src/main/java/baseball/GameCompare.java
@@ -13,15 +13,15 @@ public class GameCompare {
     }
 
     public void compareStrike(List<Integer> randomNum, List<Integer> inputNum) {
-        List<Integer> strike = IntStream.range(0, 3)
-                .filter(i -> randomNum.get(i).equals(inputNum.get(i)))
+        List<Integer> strike = IntStream.range(0, 3).filter(i -> randomNum.get(i).equals(inputNum.get(i)))
                 .mapToObj(randomNum::get).toList();
         this.strike = strike.size();
     }
 
     public void compareBall(List<Integer> randomNum, List<Integer> inputNum) {
-        List<Integer> ball = randomNum.stream()
-                .filter(inputNum::contains).toList();
+        List<Integer> ball = IntStream.range(0, randomNum.size())
+                .filter(i -> inputNum.contains(randomNum.get(i)) && inputNum.indexOf(randomNum.get(i)) != i)
+                .mapToObj(randomNum::get).toList();
         this.ball = ball.size();
     }
 
