1b923cd47dd6c1336621cb9958934e549a10f5c3
BH kang <rkdqudgjs1127@gmail.com>
2023-10-20 21:18:42 +0900

    Feat : 사용자에게 수 입력 받기
    1) 매 턴을 진행하는 메소드이다 사용자에게 입력받고, 3스트라이크가 되기 전 까지 반복한다(아직 미구현).
    
    2) 사용자에게 입력을 받고 입력받은 내용은 Integer 제너릭 타입 ArrayList에 저장한다.

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,52 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+    List<Integer> randomNum = new ArrayList<>();
+    List<Integer> answerNum = new ArrayList<>();
+
+    public void playGame(){
+        System.out.println("숫자 야구 게임을 시작합니다");
+
+        do{
+            initGame();
+            playTurn();
+        }while ();
+
+    }
+    public void initGame(){
+        randomNum.clear();
+        while (randomNum.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!randomNum.contains(randomNumber)) {
+                randomNum.add(randomNumber);
+            }
+        }
+    }
+    public void playTurn(){
+        String answer;
+        do{
+            answer = getNum();
+            changeIntArray(answer);
+        }while ();
+    }
+    public void changeIntArray(String answer) {
+        answerNum.clear();
+        for (int i = 0; i < 3; i++) {
+            answerNum.add(answer.charAt(i) - '0');
+        }
+    }
+    public String getNum(){
+        System.out.print("\n숫자를 입력해주세요 : ");
+        String answer = Console.readLine();
+
+        return answer;
+    }
+
+}
+
