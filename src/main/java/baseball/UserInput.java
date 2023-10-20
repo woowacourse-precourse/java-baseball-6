7972bab7ed65202a75a540b64daff7c0d33342cd
jinkshower <askthem@naver.com>
2023-10-20 12:43:44 +0900

    refactor: 입력받기와 배열화 분리

--- src/main/java/baseball/UserInput.java
+++ src/main/java/baseball/UserInput.java
@@ -15,10 +15,13 @@ public class UserInput {
     public List<Integer> getUserNum() {
         return List.copyOf(userNum);
     }
-    public void storeUserInput() {
+    public void promptUserInput() {
         System.out.println("Enter 3 integer");
-        String a = Console.readLine();
-        char[] tmp = a.toCharArray();
+        String s = Console.readLine();
+        stringToInt(s);
+    }
+    public void stringToInt(String string) {
+        char[] tmp = string.toCharArray();
         for(char c: tmp) {
             userNum.add(Character.getNumericValue(c));
         }
