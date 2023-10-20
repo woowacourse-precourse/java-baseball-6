934fbb05617075f8367fa957586174d1ff849cef
sami355-24 <ddung355@gmail.com>
2023-10-21 01:12:34 +0900

    refactor : 게임 재시작/종료 기능 클래스 수정
    try-catch문을 중복되어 있는 것을 한번으로 수정

--- src/main/java/baseball/EndGame/EndProcessImpl.java
+++ src/main/java/baseball/EndGame/EndProcessImpl.java
@@ -11,22 +11,19 @@ public class EndProcessImpl implements EndProcess {
     @Override
     public void userChoice() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        try {
-            ValidateUserChoice(Console.readLine());
-        } catch (IllegalArgumentException e) {
-            throw e;
-        }
+        ValidateUserChoice(Console.readLine());
     }
 
     @Override
     public void ValidateUserChoice(String userChoice) {
-        try {
-            if (userChoice.equals("1"))
-                baseBallGame.initGame();
-            else if (userChoice.equals("2"))
-                System.out.println("게임을 종료합니다.");
-        } catch (Exception e) {
-            throw new IllegalStateException("1 또는 2를 입력해주세요.");
+        if (userChoice.equals("1")) {
+            baseBallGame.initGame();
+            return;
         }
+        else if (userChoice.equals("2")) {
+            System.out.println("게임을 종료합니다.");
+            return;
+        }
+        throw new IllegalStateException("1 또는 2를 입력해주세요.");
     }
 }
