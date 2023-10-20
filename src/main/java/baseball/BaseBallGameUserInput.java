1417b5af80a6904edac78de18e7472d1f7dcdb0a
tlarbals824 <simkm0824@naver.com>
2023-10-20 14:51:12 +0900

    feat : 사용자 입력 인터페이스 구현 및 입력 및 close(사용자 입력 닫는 메소드) 메소드 추가

---
+++ src/main/java/baseball/BaseBallGameUserInput.java
@@ -0,0 +1,59 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class BaseBallGameUserInput implements UserInput<List<Integer>> {
+    private static final String INPUT_FORMAT = "숫자를 입력해주세요 :";
+    @Override
+    public List<Integer> input(Integer inputSize) {
+        System.out.println(INPUT_FORMAT);
+        final String inputString = Console.readLine();
+        final List<Character> inputCharacterList = inputString.chars()
+            .mapToObj(c -> (char) c)
+            .filter(c -> !Objects.equals(c, ' '))
+            .toList();
+        validateInput(inputCharacterList, inputSize);
+        return inputCharacterList.stream()
+            .map(c -> c-'0')
+            .collect(Collectors.toList());
+    }
+
+    @Override
+    public void close() {
+        Console.close();
+    }
+
+    private void validateInput(List<Character> validateTarget, Integer inputSize) {
+        validateInputCharacterLength(validateTarget, inputSize);
+        validateInputCharacterComposedWithNumber(validateTarget);
+        validateInputCharacterComposeWithUniqueValue(validateTarget);
+    }
+
+    private void validateInputCharacterLength(List<Character> validateTarget, Integer inputSize) {
+        if (validateTarget.size() != inputSize) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateInputCharacterComposedWithNumber(List<Character> validateTarget) {
+        for (char c : validateTarget) {
+            final int validateCharValue = c - '0';
+            if (validateCharValue < 1 || validateCharValue > 9) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private void validateInputCharacterComposeWithUniqueValue(List<Character> validateTarget) {
+        Set<Character> uniqueCharacterSet = new HashSet<>(validateTarget);
+        if(uniqueCharacterSet.size()!=validateTarget.size()){
+            throw new IllegalArgumentException();
+        }
+    }
+}
