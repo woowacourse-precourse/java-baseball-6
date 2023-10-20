84120f34ee9446160bf5f8d330f04b692e56fb6c
reddevilmidzy <midzy0228@gmail.com>
2023-10-20 18:38:55 +0900

    refactor: 입출력 문자 상수로 변경

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,16 +1,19 @@
 package baseball.view;
 
+import baseball.model.Command;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private static final String PLEASE_ENTER_NUMBER = "숫자를 입력해주세요 : ";
+    private static final String PLEASE_ENTER_COMMAND = "게임을 새로 시작하려면 %s, 종료하려면 %s를 입력하세요.\n";
 
     public String readGuessNumber() {
-        System.out.print("숫자를 입력해주세요 : ");
+        System.out.print(PLEASE_ENTER_NUMBER);
         return readLine();
     }
 
     public String readGameCommand() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.printf(PLEASE_ENTER_COMMAND, Command.RESTART, Command.QUIT);
         return readLine();
     }
 
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,27 +1,37 @@
 package baseball.view;
 
 public class OutputView {
+
+    private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String BALL = "볼";
+    private static final String STRIKE = "스트라이크";
+    private static final String NOT_THING = "낫싱";
+    private static final String BLANK = " ";
+
     public void printStartMessage() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println(START_MESSAGE);
     }
 
-    public void printResult(int strike, int ball) {
+    // TODO: 과연 여기서 낫싱 여부를 체크하는게 맞는지 고민
+    public void printResult(int strikeCount, int ballCount) {
         StringBuilder builder = new StringBuilder();
-        if (ball == 0 && strike == 0) {
-            builder.append("낫싱");
+        if (ballCount == 0 && strikeCount == 0) {
+            builder.append(NOT_THING);
         }
-        if (ball > 0) {
-            builder.append(ball);
-            builder.append("볼 ");
+        if (ballCount > 0) {
+            builder.append(ballCount);
+            builder.append(BALL);
+            builder.append(BLANK);
         }
-        if (strike > 0) {
-            builder.append(strike);
-            builder.append("스트라이크");
+        if (strikeCount > 0) {
+            builder.append(strikeCount);
+            builder.append(STRIKE);
         }
         System.out.println(builder);
     }
 
     public void printGameEndMessage() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println(GAME_END_MESSAGE);
     }
 }
