97185830e86eec0a7a4405a5d0f1db9e827c9538
hwan2-99 <thh1233123@gmail.com>
2023-10-20 16:52:02 +0900

    style: todo 주석 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,7 +4,6 @@ import baseball.service.ComputerService;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         BaseBallGame baseBallGame = new BaseBallGame();
         baseBallGame.startGame();
     }
