cb961b178ceccb6abc7374df63199446fe222132
minseo  jo <liging12@naver.com>
2023-10-20 21:01:37 +0900

    feat: Ball 기능 추가
    - Ball 최솟값, 최대값, 개수
    - 사용자의 입력이 NULL 을 예상해서 int 가 아닌 볼을 Integer 선언

--- src/main/java/baseball/domain/Ball.java
+++ src/main/java/baseball/domain/Ball.java
@@ -1,4 +1,14 @@
 package baseball.domain;
 
 public class Ball {
+    public static final int BALL_SIZE = 3;
+    public static final int MIN_BALL = 1;
+    public static final int MAX_BALL = 9;
+    private final Integer number;
+    public Ball(Integer number) {
+        this.number = number;
+    }
+    public Integer getNumber() {
+        return number;
+    }
 }
