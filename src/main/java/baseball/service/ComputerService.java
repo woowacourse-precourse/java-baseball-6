34aa13e68318707074c50049fef0b7833d2713eb
junsoo <choix712@umn.edu>
2023-10-20 15:04:55 +0900

    feat: 비즈니스 로직을 수행하는 ComputerService 클래스 생성

---
+++ src/main/java/baseball/service/ComputerService.java
@@ -0,0 +1,25 @@
+package baseball.service;
+
+import baseball.model.Computer;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComputerService {
+
+    public void createComputerList() {
+        Computer computer = new Computer(generateRandom());
+    }
+
+    private List<Integer> generateRandom() {
+        List<Integer> randomList = new ArrayList<>();
+        while(randomList.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!randomList.contains(randomNumber)) {
+                randomList.add(randomNumber);
+            }
+        }
+        return new ArrayList<>(randomList);
+    }
+}
