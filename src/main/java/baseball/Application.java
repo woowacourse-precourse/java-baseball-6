4ef67fdfadf35a054209c9ad0e5d39214fe25ac4
Infikei <hckhong@naver.com>
2023-10-20 14:29:24 +0900

    feat: computerNumber를 랜덤으로 생성하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,32 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
         displayGameStartMessage();
+
+        List<Integer> computerNumber = generateComputerNumber();
     }
 
     public static void displayGameStartMessage() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static List<Integer> generateComputerNumber() {
+        List<Integer> computerNumber = new ArrayList<>();
+
+        while (computerNumber.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+
+            if (!computerNumber.contains(randomNumber)) {
+                computerNumber.add(randomNumber);
+            }
+        }
+
+        return computerNumber;
     }
 }
