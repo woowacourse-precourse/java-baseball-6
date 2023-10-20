35b1a0e325c90e313dd7fd8359ee2913c273326d
Juhyorim <joohr1234@gmail.com>
2023-10-20 20:01:08 +0900

    feat: 게임이 종료되었을 경우 사용자에게 다시시작/종료 여부를 받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -16,11 +16,32 @@ public class Application {
             String input = Console.readLine().strip();
             checkInput(input);
 
-            compare(input, computerNum);
+            boolean isFinish = compare(input, computerNum);
+
+            if (isFinish) {
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                boolean isRestart = askRestart();
+                if (isRestart) {
+                    computerNum = makeRandomNumber();
+                } else {
+                    break;
+                }
+            }
         }
     }
 
-    private static void compare(String input, String computerNum) {
+    private static boolean askRestart() {
+        String restartInput = Console.readLine().strip();
+        checkRestartInput(restartInput);
+
+        if (restartInput.equals("1")) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static boolean compare(String input, String computerNum) {
         int strikeCnt = 0;
         int ballCnt = 0;
         StringBuilder sb = new StringBuilder();
@@ -40,6 +61,8 @@ public class Application {
         if (strikeCnt==3) {
             System.out.println("3스트라이크");
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
+            return true;
         } else {
             if (ballCnt==0 && strikeCnt==0) {
                 sb.append("낫싱");
@@ -56,6 +79,8 @@ public class Application {
             System.out.println(sb);
             System.out.println(computerNum);
         }
+
+        return false;
     }
 
     private static String makeRandomNumber() {
@@ -68,6 +93,18 @@ public class Application {
         }
 
         return "" + computer.get(0) + computer.get(1) + computer.get(2);
+    }
+
+    private static void checkRestartInput(String input) {
+        //한 글자인지 확인
+        if (input.length() != 1) {
+            throw new IllegalArgumentException("한 글자가 아닙니다");
+        }
+
+        //1-2 사이 숫자인지 확인
+        if (!input.matches("[1-2]+")) {
+            throw new IllegalArgumentException("1이나 2가 아닙니다");
+        }
     }
 
     private static void checkInput(String input) {
