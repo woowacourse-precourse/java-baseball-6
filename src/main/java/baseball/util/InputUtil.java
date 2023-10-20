165159964f950d9db76760325b2935df5b72e586
jinny-l <jinny.l.dev@gmail.com>
2023-10-20 14:13:26 +0900

    ✨feat: 숫자 입력 기능 구현
    - 유효성 검증:
        - 숫자가 아닐 경우 예외 처리

--- src/main/java/baseball/util/InputUtil.java
+++ src/main/java/baseball/util/InputUtil.java
@@ -1,8 +1,27 @@
 package baseball.util;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class InputUtil {
+
+    public static List<Integer> readIntegerList() {
+        try {
+            return readLine().chars()
+                    .mapToObj(Character::getNumericValue)
+                    .toList();
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(); // TODO 커스텀 예외 만들기
+        }
+    }
+
+    public static int readInt() {
+        try {
+            return Integer.parseInt(readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(); // TODO 커스텀 예외 만들기
+        }
+    }
 
     private static String readLine() {
         String input = Console.readLine().trim();
