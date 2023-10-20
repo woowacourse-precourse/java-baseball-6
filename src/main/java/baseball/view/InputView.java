98e10d9829c518fe251149d2cc113ed3ab8d3b01
shin5774 <babo5774@naver.com>
2023-10-20 20:26:01 +0900

    Refactor: "InputView 클래스 공통기능 정리"
    - 두 메서드는 콘솔로부터 입력을 받고 이를 String으로 반환하는 기능이 있기에 해당 기능을 가진 메서드를 만들어 공통된 기능의 중복작성을 제거

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -5,15 +5,17 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class InputView {
     public String inputNumbers(){
         System.out.print("숫자를 입력해주세요 : ");
-        String numbers=readLine();
 
-        return numbers;
+        return input();
     }
 
     public String inputContinue(){
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        String option=readLine();
 
-        return option;
+        return input();
+    }
+
+    private String input(){
+        return readLine();
     }
 }
