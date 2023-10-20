45d27181b5cd8282814960c4a5cbfcf29f323b0b
WeepingHeron <kimtaehoon_98@naver.com>
2023-10-20 18:36:56 +0900

    feat: now throws IllegalArgumentException

--- src/main/java/baseball/ParseNum.java
+++ src/main/java/baseball/ParseNum.java
@@ -2,13 +2,24 @@ package baseball;
 
 public class ParseNum {
     public static int[] parseInput(String input) {
+        if (input.equals("낫싱") || input.equals("3스트라이크")) {
+            throw new IllegalArgumentException("올바른 숫자를 입력해 주세요.");
+        }
+
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("3자리의 수를 입력해 주세요.");
+        }
+
         int[] parsedNum = new int[3];
         for (int i = 0; i < 3; i++) {
             char c = input.charAt(i);
             if (Character.isDigit(c)) {
                 parsedNum[i] = Character.getNumericValue(c);
+            } else {
+                throw new IllegalArgumentException("숫자 이외의 문자를 입력하셨습니다.");
             }
         }
         return parsedNum;
     }
 }
+
