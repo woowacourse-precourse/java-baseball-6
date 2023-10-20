c4bcaee7c81dc6991c3474ac5af6ea0586c5d0ad
GaBaljaintheroom <junsu1222@naver.com>
2023-10-20 12:24:38 +0900

    feat: main 메서드 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,8 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Player player = new Player();
+        BaseballController baseballController = new BaseballController(player);
+        baseballController.run();
     }
 }
