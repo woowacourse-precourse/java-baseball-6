29c7dfafb150bbbb36cae2e251abe4e2c121bd06
박승찬 <seungchan141414@gmail.com>
2023-10-20 15:35:12 +0900

    [Feat(baseball)] 3개의 랜덤 숫자의 리스트를 반환하는 기능 구현

--- src/main/java/baseball/domain/NumbersGenerate.java
+++ src/main/java/baseball/domain/NumbersGenerate.java
@@ -1,14 +1,24 @@
 package baseball.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class NumbersGenerate {
     /**
      * 3개의 랜덤 숫자의 리스트를 반환한다.
+     *
      * @return
      */
     public List<Integer> createRandomNumList() {
-
-        return null;
+        List<Integer> ranNumTreeList = new ArrayList<>();
+        while (ranNumTreeList.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!ranNumTreeList.contains(randomNumber)) {
+                ranNumTreeList.add(randomNumber);
+            }
+        }
+        return ranNumTreeList;
     }
 }
