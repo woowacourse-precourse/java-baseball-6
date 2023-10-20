84e3f36cd95236af884a1d0227a2f4ad9d4fa731
haiseong <wjdgotjd9908@gmail.com>
2023-10-21 00:57:30 +0900

    test: NumberInputTest 생성: validate 테스트

---
+++ src/test/java/baseball/model/NumberInputTest.java
@@ -0,0 +1,79 @@
+package baseball.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NumberInputTest {
+
+    @Test
+    void validate_정상적인_경우() {
+        // given
+        String input = "123";
+
+        // when
+        NumberInput numberInput = new NumberInput(input);
+
+        // then
+    }
+
+    @Test
+    void validate_3자리가_아닌_경우() {
+        // given
+        String input = "1234";
+
+        // when
+
+        // then
+        assertThatThrownBy(() -> new NumberInput(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void validate_숫자가_아닌_문자가_있는_경우() {
+        // given
+        String input = "a12";
+
+        // when
+
+        // then
+        assertThatThrownBy(() -> new NumberInput(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void validate_0이_섞여있는_경우() {
+        // given
+        String input = "120";
+
+        // when
+
+        // then
+        assertThatThrownBy(() -> new NumberInput(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void validate_중복된_수가_있는_경우() {
+        // given
+        String input = "122";
+
+        // when
+
+        // then
+        assertThatThrownBy(() -> new NumberInput(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void validate_빈_문자열의_경우() {
+        // given
+        String input = "";
+
+        // when
+
+        // then
+        assertThatThrownBy(() -> new NumberInput(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
