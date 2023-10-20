f9c950ca8e448bb7dd2efd2978f5ea70fc9646a5
mizuirohoshi7 <mizuirohoshi7@gmail.com>
2023-10-20 16:25:45 +0900

    test(GameResult): 테스트 생성

--- src/main/java/baseball/dto/GameResult.java
+++ src/main/java/baseball/dto/GameResult.java
@@ -1,4 +1,15 @@
 package baseball.dto;
 
 public class GameResult {
+    private final int strike;
+    private final int ball;
+
+    public GameResult(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+
+    public boolean isClear() {
+        return true;
+    }
 }
