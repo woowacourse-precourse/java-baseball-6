a3438e8e176bb41cd7f54bfc75b9afd2c962db61
plastichong <zakkslash18@naver.com>
2023-10-20 22:24:19 +0900

    feat: Game answer validate
    게임 답안 중복 숫자 검증 로직 추가

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -3,15 +3,38 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Computer {
-    private final int firstPositionAnswer;
-    private final int secondPositionAnswer;
-    private final int thirdPositionAnswer;
+    private int firstPositionAnswer;
+    private int secondPositionAnswer;
+    private int thirdPositionAnswer;
 
     public Computer() {
-        firstPositionAnswer = Randoms.pickNumberInRange(1, 9);
-        secondPositionAnswer = Randoms.pickNumberInRange(1, 9);
-        thirdPositionAnswer = Randoms.pickNumberInRange(1, 9);
-        System.out.println("new game 생성 : " +firstPositionAnswer+""+secondPositionAnswer+""+thirdPositionAnswer);
+
+        while (true) {
+
+            generateAnswer();
+            System.out.println("new game 생성 : " + this.firstPositionAnswer + "" + this.secondPositionAnswer + "" + this.thirdPositionAnswer);
+
+            boolean isCanUse = isCanUseThisAnswer();
+
+            if (isCanUse) {
+                break;
+            }
+        }
+
+    }
+
+    private boolean isCanUseThisAnswer() {
+        return !(
+                this.firstPositionAnswer == this.secondPositionAnswer ||
+                this.firstPositionAnswer == this.thirdPositionAnswer ||
+                this.secondPositionAnswer == this.thirdPositionAnswer
+        );
+    }
+
+    private void generateAnswer() {
+        this.firstPositionAnswer = Randoms.pickNumberInRange(1, 9);
+        this.secondPositionAnswer = Randoms.pickNumberInRange(1, 9);
+        this.thirdPositionAnswer = Randoms.pickNumberInRange(1, 9);
     }
 
     public int getBallCount(int userInput) {
@@ -58,7 +81,7 @@ public class Computer {
         return getBallCount(userInput) + getStrikeCount(userInput) == 0;
     }
 
-    public boolean isPlayerWin(int userInput){
+    public boolean isPlayerWin(int userInput) {
         return getStrikeCount(userInput) == 3;
     }
 
