513127152371b4b6fda33ec378713f6e9f34e045
cloudchamb3r <jizon0123@protonmail.com>
2023-10-20 09:49:55 +0900

    refactor: extract string to list conversion

--- src/main/java/baseball/converter/BaseballStateConverter.java
+++ src/main/java/baseball/converter/BaseballStateConverter.java
@@ -1,30 +1,26 @@
 package baseball.converter;
 
-import static java.util.stream.Collectors.*;
-
 import baseball.state.BaseballState;
-import baseball.utils.IoHelper;
 import baseball.utils.StringUtils;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class BaseballStateConverter {
-    public static BaseballState stringToBaseballState(String s) throws IllegalArgumentException{
-        IoHelper.println("this is s => " + s);
+    public static BaseballState stringToBaseballState(String s) throws IllegalArgumentException {
         if (s.length() != 3) {
             throw new IllegalArgumentException();
         }
+
         if (!s.chars().allMatch(Character::isDigit)) {
             throw new IllegalArgumentException();
         }
+
         var setOfChar = StringUtils.stringToSetOfChar(s);
         if (setOfChar.size() != 3) {
             throw new IllegalArgumentException();
         }
-        List<Integer> state = s.chars().mapToObj(Character::getNumericValue).collect(Collectors.toList());
+
+        List<Integer> state = StringUtils.digitStringToListOfInt(s);
+
         return new BaseballState(state);
     }
 }
--- src/main/java/baseball/utils/StringUtils.java
+++ src/main/java/baseball/utils/StringUtils.java
@@ -1,11 +1,26 @@
 package baseball.utils;
+
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class StringUtils {
     public static Set<Character> stringToSetOfChar(String s) {
         var chars = s.chars();
-        var objChars = chars.mapToObj(c -> (char)c);
+        var objChars = chars.mapToObj(c -> (char) c);
         return objChars.collect(Collectors.toSet());
+    }
+
+    public static List<Character> stringToListOfChar(String s) {
+        var chars = s.chars();
+        var objChars = chars.mapToObj(c -> (char) c);
+        return objChars.collect(Collectors.toList());
+    }
+
+    public static List<Integer> digitStringToListOfInt(String s) {
+        var listOfChars = stringToListOfChar(s);
+        var stream = listOfChars.stream();
+        var mapToInt = stream.map(Character::getNumericValue);
+        return mapToInt.collect(Collectors.toList());
     }
 }
