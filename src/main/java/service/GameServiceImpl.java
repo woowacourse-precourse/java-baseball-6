1b48a6e8a573184942958a7c5502c9c5d95dfe47
song-jae-wan <sjo9810@gmail.com>
2023-10-20 10:19:51 +0900

    Feat: 서비스 인터페이스 구현
    인터페이스로 선언한 서비스단
    GameServiceImpl 클래스에서
    구현 받아 오버라이드

---
+++ src/main/java/service/GameServiceImpl.java
@@ -0,0 +1,19 @@
+package service;
+
+public class GameServiceImpl implements GameService{
+
+    @Override
+    public void gameStart() {
+
+    }
+
+    @Override
+    public void restartOrQuit(String order) {
+
+    }
+
+    @Override
+    public String checkCount(String numbers) {
+        return null;
+    }
+}
