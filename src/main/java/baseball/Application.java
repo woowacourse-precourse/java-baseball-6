e369ba9e6e2cecda12945110c3de6d6530af5d0e
ldw <leehan02@hanyang.ac.kr>
2023-10-20 14:29:07 +0900

    Baseball : Add countBallAndStrike() for counting number of ball and strike and printing them

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -19,12 +19,13 @@ public class Application {
 
     public static void runGame() {
         List<Integer> answer = generateRandomNum();
+        System.out.println("answer = " + answer);
         while (true) {
             System.out.println("숫자 야구 게임을 시작합니다.");
             System.out.print("숫자를 입력해주세요 : ");
             String inputString = Console.readLine();
             List<Integer> inputNum = convertStringToList(inputString);
-            System.out.println("inputNum = " + inputNum);
+            countBallAndStrike(inputNum, answer);
         }
     }
 
@@ -44,8 +45,7 @@ public class Application {
         for (int i = 0; i < inputString.length(); i++) {
             char c = inputString.charAt(i);
             if (Character.isDigit(c)) {
-                int digitValue = Character.getNumericValue(c);
-                inputList.add(digitValue);
+                inputList.add(Character.getNumericValue(c));
             } else {
                 throw new IllegalArgumentException("잘못된 입력입니다.");
             }
@@ -53,10 +53,29 @@ public class Application {
         return inputList;
     }
 
-//    public static void isAnswer(String inputNum) {
+//    public static void isAnswer(List<Integer> inputNum, List<Integer> answer) {
 //
 //    }
-//    public static boolean isStrike() {
+//    public static int countStrike(List<Integer> inputNum, List<Integer> answer) {
 //
 //    }
+
+    public static void countBallAndStrike(List<Integer> inputNum, List<Integer> answer) {
+        int ball = 0;
+        int strike = 0;
+        for (int i = 0; i < inputNum.size(); i++) {
+            for (int j = 0; j < inputNum.size(); j++) {
+                if (i != j && inputNum.get(i).equals(answer.get(j)))
+                    ball++;
+                else if (i == j && inputNum.get(i).equals(answer.get(j)))
+                    strike++;
+            }
+        }
+        if (ball > 0 && strike > 0)
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        else if (ball > 0)
+            System.out.println(ball + "볼");
+        else if (strike > 0)
+            System.out.println(strike + "스트라이크");
+    }
 }
