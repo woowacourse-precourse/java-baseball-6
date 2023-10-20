19263dd757ae80a51bc2d98d6f291b4ded3cc16d
plan11plan <tjtfy7028@gmail.com>
2023-10-20 23:16:34 +0900

    V2

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,7 +7,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
-    public static void go(List<Integer> target, String input) {
+    public static List<Integer> setCPU() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
+    public static void play(List<Integer> target, String input) {
         List<Integer> reTarget = target;
         int strike = 0;
         int ball = 0;
@@ -26,23 +37,19 @@ public class Application {
             }
         }
         if (strike == 3) {
-            System.out.println(strike + "스트라이크");
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            String number1 = readLine();
-            if (number1.equals("1")) {
-                List<Integer> computer = new ArrayList<>();
-                while (computer.size() < 3) {
-                    int randomNumber = Randoms.pickNumberInRange(1, 9);
-                    if (!computer.contains(randomNumber)) {
-                        computer.add(randomNumber);
-                    }
-                }
-                System.out.println("컴퓨터 숫자:" + computer);
+            System.out.println(
+                    strike + "스트라이크\n"
+                            + "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"
+                            + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+            String userDecide = readLine();
+            if (userDecide.equals("1")) {
+                List<Integer> cpuNewInput = setCPU();
+                System.out.println("컴퓨터 숫자:" + cpuNewInput);
                 System.out.print("숫자를 입력해주세요 : ");
-                String s = readLine();
-                go(computer, s);
-            } else if (number1 == "2") {
+                String userNewInput = readLine();
+                play(cpuNewInput, userNewInput);
+            } else if (userDecide.equals("2")) {
 
             }
         } else {
@@ -56,9 +63,6 @@ public class Application {
                     }
                 }
             }
-//
-//            System.out.println("strike : " + strike);
-//            System.out.println("ball : " + ball);
             if (strike == 0 && ball == 0) {
                 message = "낫싱";
             } else {
@@ -76,28 +80,25 @@ public class Application {
 
             System.out.println(message);
             System.out.print("숫자를 입력해주세요 : ");
-            String s = readLine();
-            go(reTarget, s);
+            String userInput = readLine();
+            play(reTarget, userInput);
         }
     }
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
-        }
-        System.out.println("컴퓨터 숫자:" + computer);
+        List<Integer> cpuInput = setCPU();
+        System.out.println("컴퓨터 숫자:" + cpuInput);
         System.out.print("숫자를 입력해주세요 : ");
-        String s = readLine();
-        if (s.length() != 3) {
-            throw new IllegalArgumentException("3자리 숫자만 입력해주세요");
+        String userInput = readLine();
+        if (userInput.length() != 3 ||
+                !Character.isDigit(userInput.charAt(0)) ||
+                !Character.isDigit(userInput.charAt(1)) ||
+                !Character.isDigit(userInput.charAt(2))) {
+            throw new IllegalArgumentException("입력한 값이 세 자리 숫자가 아닙니다.");
         }
-
-        go(computer, s);
+        play(cpuInput, userInput);
     }
+
 }
