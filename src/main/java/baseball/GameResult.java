e2bd1ef5851d8e236d984921a117c6ab0c56afe7
gemstoneyang <bennyws99@gmail.com>
2023-10-20 18:08:21 +0900

    fix: Fixed a toString error in GameResult class - 스트라이크와 볼이 모두 0인 경우 아무 출력이 나오지 않는 현상 수정 - 볼과 스트라이크를 모두 출력하는 경우 사이 공백이 없는 현상 수정

--- src/main/java/baseball/GameResult.java
+++ src/main/java/baseball/GameResult.java
@@ -19,10 +19,14 @@ public class GameResult {
 
     @Override
     public String toString() {
+        if (strike == 0 && ball == 0) {
+            return "낫싱";
+        }
+
         StringBuilder result = new StringBuilder();
 
         if (ball > 0) {
-            result.append(ball).append("볼");
+            result.append(ball).append("볼 ");
         }
 
         if (strike > 0) {
