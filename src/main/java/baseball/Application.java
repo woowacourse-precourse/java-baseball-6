16d95ea6ab72e9635f789628a351036701ad9789
MoonJaeGyeong <bblbblan69@gmail.com>
2023-10-20 19:23:19 +0900

    <Error> : <잘못입력된 값을 판별하여 예외처리>
    숫자의 길이, 서로 다르지 않은 세 수, 숫자가 아닌 문자열을 처리하여 에러로 throw IllegalArgumentException 해주는 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,10 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.*;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashSet;
+import java.util.List;
+
 
 public class Application {
     public static void main(String[] args) {
@@ -10,25 +14,63 @@ public class Application {
         String result; // 유저가 입력한 숫자에 대한 결과
 
         System.out.println("숫자 야구 게임을 시작합니다.");
-        do{
-            String computer_num = Randoms.pickUniqueNumbersInRange(1,9,3)
-                    .toString().replaceAll("[^0-9]",""); // 컴퓨터가 생성한 1~9 사이 Random 한 수
+        do {
+            List<Integer> c_num = Randoms.pickUniqueNumbersInRange(1, 9, 3);
+            String List_to_String = c_num.toString();
+            String computer_num = List_to_String.replaceAll("[^0-9]", "");
             do {
-                 System.out.print("숫자를 입력해주세요 : ");
-                 number = Console.readLine();
-                 result = judge.isStrike(number,computer_num);
-                 System.out.println(result);
-            } while(!result.equals("3스트라이크"));
+                System.out.print("숫자를 입력해주세요 : ");
+                number = Console.readLine();
+                ErrorManage(number);
+                result = judge.isStrike(number, computer_num);
+                System.out.println(result);
+            } while (!result.equals("3스트라이크"));
 
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
             exit = Console.readLine();
-
-            if(!exit.equals("1") && !exit.equals("2")){
-                throw new IllegalArgumentException();
-            }
+            ErrorManage(exit);
         } while (!exit.equals("2"));
+
+        Console.close();
+    }
+
+    private static void ErrorManage(String input) {
+        if (input.length() != 3 && input.length() != 1) {
+            throw new IllegalArgumentException("input의 길이가 이상합니다.");
+        }
+
+        if (input.length() == 3) {
+            if (!isNumeric(input)) {
+                throw new IllegalArgumentException("숫자가 아닌 문자가 포함되어 있습니다.");
+            }
+
+            if (!hasUniqueDigits(input)) {
+                throw new IllegalArgumentException("숫자가 서로 다르지 않습니다.");
+            }
+        }
+
+        if (input.length() == 1) {
+            if (!input.equals("1") && !input.equals("2")) {
+                throw new IllegalArgumentException("제대로 된 input값이 아닙니다.");
+            }
+        }
+    }
+
+    private static boolean isNumeric(String str) {
+        return str.matches("\\d+");
+    }
+
+    private static boolean hasUniqueDigits(String str) {
+        HashSet<Character> uniqueDigits = new HashSet<>();
+        for (char c : str.toCharArray()) {
+            if (uniqueDigits.contains(c)) {
+                return false;
+            }
+            uniqueDigits.add(c);
+        }
+        return true;
     }
 }
 
--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,19 +1,19 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
                 () -> {
-                    run("246", "135", "1", "597", "589", "2");
+                    run("236", "125", "1", "597", "589", "2");
                     assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
                 },
                 1, 3, 5, 5, 8, 9
