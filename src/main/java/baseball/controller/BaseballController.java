4612c3b8cd4c522f4fbc62e3fe55c4e3fa9961c6
1Bronze <khdkkhdd@gmail.com>
2023-10-20 14:21:56 +0900

    [refact] Game, Baseball 간 결합도 낮추도록 리팩토링

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -1,11 +1,43 @@
 package baseball.controller;
 
+import java.util.List;
+import java.util.Objects;
+import baseball.domain.Baseball;
 import baseball.domain.Game;
+import baseball.domain.Result;
 
 public class BaseballController {
     public void start() throws IllegalArgumentException {
         System.out.println("숫자 야구 게임을 시작합니다.");
+
         Game game = new Game();
-        game.start();
+        Baseball gameAnswer = game.getAnswer();
+        Baseball userGuess = game.input();
+        Result result = compare(userGuess, gameAnswer);
+    }
+
+    private Result compare(Baseball guess, Baseball answer) {
+        Integer ballCount = 0;
+        Integer strikeCount = 0;
+        List<Integer> guessDigits = guess.getDigits();
+        List<Integer> answerDigits = answer.getDigits();
+
+        for (int index = 0; index < 3; index++) {
+            if (isStrike(guessDigits, answerDigits, index)) {
+                strikeCount++;
+            }
+            if (isBall(guessDigits, answerDigits, index)) {
+                ballCount++;
+            }
+        }
+        return new Result(ballCount, strikeCount);
+    }
+
+    private boolean isStrike(List<Integer> guessDigits, List<Integer> answerDigits, int index) {
+        return Objects.equals(guessDigits.get(index), answerDigits.get(index));
+    }
+
+    private boolean isBall(List<Integer> guessDigits, List<Integer> answerDigits, int index) {
+        return answerDigits.contains(guessDigits.get(index)) && !isStrike(guessDigits, answerDigits, index);
     }
 }
--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -1,8 +1,5 @@
 package baseball.domain;
 
-import java.util.List;
-import java.util.Objects;
-
 import static baseball.domain.Baseball.createAnswer;
 import static baseball.validator.InputValidator.validateGuess;
 import static camp.nextstep.edu.missionutils.Console.readLine;
@@ -14,34 +11,14 @@ public class Game {
         answer = createAnswer();
     }
 
-    public void start() {
-        System.out.println("숫자를 입력해주세요 : ");
+    public Baseball input() {
+        System.out.print("숫자를 입력해주세요 : ");
         Integer userInput = validateGuess(readLine());
         Baseball userGuess = new Baseball(userInput);
+        return userGuess;
     }
 
-    public Result compareWithAnswer(Baseball guess) {
-        Integer ballCount = 0;
-        Integer strikeCount = 0;
-        List<Integer> guessDigits = guess.getDigits();
-        List<Integer> answerDigits = answer.getDigits();
-
-        for (int index = 0; index < 3; index++) {
-            if (isStrike(guessDigits, answerDigits, index)) {
-                strikeCount++;
-            }
-            if (isBall(guessDigits, answerDigits, index)) {
-                ballCount++;
-            }
-        }
-        return new Result(ballCount, strikeCount);
-    }
-
-    private boolean isStrike(List<Integer> guessDigits, List<Integer> answerDigits, int index) {
-        return Objects.equals(guessDigits.get(index), answerDigits.get(index));
-    }
-
-    private boolean isBall(List<Integer> guessDigits, List<Integer> answerDigits, int index) {
-        return answerDigits.contains(guessDigits.get(index)) && !isStrike(guessDigits, answerDigits, index);
+    public Baseball getAnswer() {
+        return answer;
     }
 }
