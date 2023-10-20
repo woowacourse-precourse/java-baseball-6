e8423c9f7837f827a32c64508b00365277b30a81
Austin-Choi <austinsupra@gmail.com>
2023-10-21 00:19:06 +0900

    Feat: 데이터 유효성 검사용 유틸리티 클래스 추가

---
+++ src/main/java/utility/BaseballNumberChecker.java
@@ -0,0 +1,30 @@
+package utility;
+
+import java.util.Collections;
+import java.util.List;
+
+public final class BaseballNumberChecker {
+    private BaseballNumberChecker(){}
+    public static void validateBaseballNumberSize(final String input)
+            throws IllegalArgumentException{
+        if(input.length() != 3)
+            throw new IllegalArgumentException();
+    }
+    public static void validateBaseballNumberType(final String input)
+            throws IllegalArgumentException{
+        char[] guessCharArr = input.toCharArray();
+        for(int i = 0; i<input.length(); i++){
+            if(!Character.isDigit(guessCharArr[i]))
+                throw new IllegalArgumentException();
+        }
+    }
+    public static void isBaseballNumberSequence(final List<Integer> numbers,final int maxSize)
+            throws IllegalArgumentException{
+        int currentNumber;
+        for(int i = 0; i<maxSize; i++){
+            currentNumber = numbers.get(i);
+            if(Collections.frequency(numbers, currentNumber) > 1)
+                throw new IllegalArgumentException();
+        }
+    }
+}
