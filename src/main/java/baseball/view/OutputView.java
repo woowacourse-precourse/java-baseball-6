e7ab3d1617079ef9fa705fad07cd1c570ba2320c
PARKHWANYEONG <welcome0828@naver.com>
2023-10-20 12:55:18 +0900

    feat(view): 게임 시작 문구 출력

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,9 @@
+package baseball.view;
+
+public class OutputView {
+
+    private final String GAME_START = "숫자 야구 게임을 시작합니다.";
+    public void gameStart() {
+        System.out.println(GAME_START);
+    }
+}
