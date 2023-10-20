72f21f79b7a77d26762bb90a4b5b6bc65873dbc9
Annjieun <dkslwdms2121@naver.com>
2023-10-20 22:16:28 +0900

    feat: 예외 메시지 상수 추가

---
+++ src/main/java/baseball/constant/MessageConstant.java
@@ -0,0 +1,11 @@
+package baseball.constant;
+
+import static baseball.constant.NumberConstant.*;
+
+public class MessageConstant {
+    public static final String BLANK_EXCEPTION_MSG = "공백을 입력하였습니다.";
+    public static final String NON_DIGIT_EXCEPTION_MSG = "숫자를 입력해주세요.";
+    public static final String INPUT_LENGTH_EXCEPTION_MSG = INPUT_LENGTH + "자리 숫자를 입력해주세요.";
+    public static final String NUMBER_RANGE_EXCEPTION_MSG = MINIMUM_RANGE_NUMBER + "에서 " + MAXIMUM_RANGE_NUMBER + "사이의 숫자를 입력해주세요.";
+    public static final String NUMBER_DUPLICATE_EXCEPTION_MSG = "서로 다른 숫자를 입력해주세요.";
+}
