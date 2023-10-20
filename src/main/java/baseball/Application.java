ad9e0d79ef87b12feb03a1c2a1e23ff13a6f18d5
Lee SeungHyeon <kocan9803@naver.com>
2023-10-21 00:35:38 +0900

    임시

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,20 @@
 package baseball;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 시작합니다.");
+        while(true) {
+            System.out.print("숫자를 입력해주세요 : ");
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
+            for(int i = 0;i<input.length();i++)
+            {
+
+            }
+        }
+
     }
 }
