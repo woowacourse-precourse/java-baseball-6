07a6d5c1dfec5d81f6765a32670515e83443a7df
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-10-21 01:48:35 +0900

    refactor: static import 삭제
    Oracle Java 공식 문서에서 static import를 지양하는 것이 좋다고 명시되어 있기 때문에 static import를 import로 변경하였음.
    
    Ref: https://docs.oracle.com/javase/1.5.0/docs/guide/language/static-import.html

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,12 +1,11 @@
 package baseball.view;
 
 import baseball.util.Messages;
-
-import static camp.nextstep.edu.missionutils.Console.readLine;
+import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public String getUserNumber() {
         System.out.print(Messages.GET_USER_NUMBER_MESSAGE.getMessage());
-        return readLine();
+        return Console.readLine();
     }
 }
