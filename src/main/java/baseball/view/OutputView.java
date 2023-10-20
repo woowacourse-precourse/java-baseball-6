96304b2360aac9baa6ec0b1b4ad096584966b035
shAn-kor <53287605+shAn-kor@users.noreply.github.com>
2023-10-20 23:12:49 +0900

    printBallStrike 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,4 +4,22 @@ public class OutputView {
     public void gameStartMessage() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
+    public void printBallStrike(Integer ball, Integer strike) {
+        if (ball == 0 && strike == 0) {
+            System.out.println("낫싱");
+        }
+
+        if (ball > 0 && strike == 0) {
+            System.out.println(ball + "볼");
+        }
+
+        if (strike > 0 && ball == 0) {
+            System.out.println(strike + "스트라이크");
+        }
+
+        if (strike > 0 && ball > 0) {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
+    }
 }
