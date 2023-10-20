df5457da7fa124a1289b1057d08dcfd70bec3c31
Hajin-Lee0406 <glgkwls1@naver.com>
2023-10-21 00:30:22 +0900

    feat: view - 시작 출력문 추가

--- src/main/java/baseball/baseball/controller/BaseballController.java
+++ src/main/java/baseball/baseball/controller/BaseballController.java
@@ -1,18 +1,24 @@
 package baseball.baseball.controller;
 
 import baseball.baseball.model.BaseballService;
+import baseball.baseball.util.StringConvertUtil;
+import baseball.baseball.view.InputView;
+import baseball.baseball.view.OutputView;
 
 import java.util.List;
 
 public class BaseballController {
     private static final BaseballService baseballService = new BaseballService();
+    private static final OutputView outputView = OutputView.getInstance();
+    private static final InputView inputView = InputView.getInstance();
     private static List<Integer> goalNumber;
 
-    public void run(){
+    public void run() {
         start();
     }
 
-    private static void start(){
+    private void start() {
+        outputView.printStartGame();
         goalNumber = baseballService.createGoalNumber();
     }
 }
--- src/main/java/baseball/baseball/view/OutputView.java
+++ src/main/java/baseball/baseball/view/OutputView.java
@@ -9,4 +9,8 @@ public class OutputView {
 
     private OutputView() {
     }
+
+    public void printStartGame() {
+        System.out.println("숫자 야구 게임을 시작합니다");
+    }
 }
