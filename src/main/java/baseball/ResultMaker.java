f40c620ee1a9cf1b1f26a195bfe61cd2ac84d94a
seminss <2007ksm@naver.com>
2023-10-20 15:44:07 +0900

    feat : create result maker

---
+++ src/main/java/baseball/ResultMaker.java
@@ -0,0 +1,21 @@
+package baseball;
+
+import java.util.List;
+
+public class ResultMaker {
+    public List<Integer> makeResult(List<Integer> userNumbers, List<Integer> computerNumbers) {
+        int strike =0;
+        int ball =0;
+        for (int i = 0; i < userNumbers.size(); i++) {
+            int thisUserNumber = userNumbers.get(i);
+            int thisComputerNumber = computerNumbers.get(i);
+            if (thisUserNumber==thisComputerNumber){
+                strike++;
+                continue;
+            }
+            if(computerNumbers.contains(thisUserNumber))
+                ball++;
+        }
+        return List.of(strike,ball);
+    }
+}
---
+++ src/test/java/baseball/ResultMakerTest.java
@@ -0,0 +1,38 @@
+package baseball;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+
+class ResultMakerTest {
+    ResultMaker resultMaker = new ResultMaker();
+
+    @Test
+    @DisplayName("볼, 스트라이크, 낫싱을 판단 테스트")
+    void makeResult() {
+
+        List<Integer> computerNumbers = List.of(1,2,3);
+
+        List<Integer> strikeBall = //3스트라이크
+                resultMaker.makeResult(List.of(1,2,3),computerNumbers);
+        Assertions.assertEquals(strikeBall,List.of(3,0));
+
+        strikeBall = //낫싱
+                resultMaker.makeResult(List.of(6,4,7),computerNumbers);
+        Assertions.assertEquals(strikeBall,List.of(0,0));
+
+
+        strikeBall = //2볼
+                resultMaker.makeResult(List.of(5,1,2),computerNumbers);
+        Assertions.assertEquals(strikeBall,List.of(0,2));
+
+
+        strikeBall = //1스트라이크 1볼
+                resultMaker.makeResult(List.of(1,3,0),computerNumbers);
+        Assertions.assertEquals(strikeBall,List.of(1,1));
+
+    }
+}
