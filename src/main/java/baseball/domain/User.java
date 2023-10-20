9ae2e53dec4b73485efbdd5b0395852f0c449b4a
spiky-haired-Pyramid-head <qns0147@gmail.com>
2023-10-20 14:21:27 +0900

    refactor: 변수 타입 변경

--- src/main/java/baseball/domain/User.java
+++ src/main/java/baseball/domain/User.java
@@ -7,23 +7,20 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class User {
 
-    private List<Integer> baseballNumberList;
+    private String baseBallNumberString;
 
 
-    public List<Integer> getBaseballNumberList() {
-        return baseballNumberList;
+    public String getBaseballNumberList() {
+        return baseBallNumberString;
     }
 
     public void readBaseballNumber() {
         System.out.print("숫자를 입력해주세요 : ");
-        String baseBallNumberStr = Console.readLine();
-
-        validateBaseballNumber(baseBallNumberStr);
-        convertCharToInteger(baseBallNumberStr);
+        baseBallNumberString = Console.readLine();
 
     }
 
-    private void convertCharToInteger(String baseBallString) {
+    public List<Integer> convertCharToInteger(String baseBallString) {
         ArrayList<Integer> baseballNumbers = new ArrayList<>();
 
         for (char baseballChar : baseBallString.toCharArray())
@@ -33,10 +30,10 @@ public class User {
         if (baseballNumbers.size() != 3)
             throw new IllegalArgumentException();
 
-        this.baseballNumberList = baseballNumbers;
+        return baseballNumbers;
     }
 
-    private void validateBaseballNumber(String baseBallNumberStr) {
+    public void validateBaseballNumber(String baseBallNumberStr) {
         if (baseBallNumberStr.chars().distinct().count() != 3)
             throw new IllegalArgumentException();
     }
