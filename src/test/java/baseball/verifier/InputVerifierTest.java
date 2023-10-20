102e87f8fd40f8ce9cfcfc9a0a1b665d8e895b86
정성욱 <rkfka7849@gmail.com>
2023-10-20 18:51:19 +0900

    test(InputVerifier): 중복값 예외 테스트

--- src/test/java/baseball/verifier/InputVerifierTest.java
+++ src/test/java/baseball/verifier/InputVerifierTest.java
@@ -6,17 +6,24 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class InputVerifierTest {
-    @DisplayName("숫자값이 입력되지 않았을 때 예외 처리하는지 테스트")
+    @DisplayName("숫자값이 입력되지 않았을 때 예외처리 하는지 테스트")
     @Test
     void throwNumricException() {
         InputVerifier inputVerifier = new InputVerifier();
         assertThrows(IllegalArgumentException.class, () -> inputVerifier.checkBallNumber("숫자아님"));
     }
 
-    @DisplayName("입력값이 123 ~ 987 사이의 수인지 확인하는 테스트")
+    @DisplayName("입력값이 123 ~ 987 사이의 수가 아닐 때 예외처리 하는지 테스트")
     @Test
     void throwRangeException() {
         InputVerifier inputVerifier = new InputVerifier();
         assertThrows(IllegalArgumentException.class, () -> inputVerifier.checkBallNumber("1000"));
+    }
+
+    @DisplayName("중복값이 있을 때 예외처리 하는지 테스트")
+    @Test
+    void throwNotDistinct() {
+        InputVerifier inputVerifier = new InputVerifier();
+        assertThrows(IllegalArgumentException.class, () -> inputVerifier.checkBallNumber("155"));
     }
 }
