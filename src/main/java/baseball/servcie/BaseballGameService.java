79f57d581548330951ae30cb4c05dd6be8289a47
spiky-haired-Pyramid-head <qns0147@gmail.com>
2023-10-20 16:17:55 +0900

    feat: 사용자 입력 값과 컴퓨터의 값 비교 기능 구현

--- src/main/java/baseball/servcie/BaseballGameService.java
+++ src/main/java/baseball/servcie/BaseballGameService.java
@@ -4,12 +4,15 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class BaseballGameService {
 
+    private final int NO_HIT = 0;
+    private final int ALL_STRIKE = 3;
+
 
     public List<Integer> initComputerBaseBallNumber() {
-
         List<Integer> computerNumberList = new ArrayList<>();
 
         while (computerNumberList.size() < 3) {
@@ -21,4 +24,38 @@ public class BaseballGameService {
 
         return computerNumberList;
     }
+
+    public boolean compareNumber(List<Integer> userNumber, List<Integer> computerNumber) {
+        int strike = calculateStrike(userNumber, computerNumber);
+        int ball = calculateBall(userNumber, computerNumber);
+
+        if (strike == NO_HIT && ball == NO_HIT)
+            System.out.println("낫싱");
+
+        if (ball > NO_HIT)
+            System.out.println(ball + "볼 ");
+
+        if (strike > NO_HIT)
+            System.out.println(strike + "스트라이크 ");
+
+        if (strike == ALL_STRIKE) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
+
+        return (strike == ALL_STRIKE);
+    }
+
+    private static int calculateStrike(List<Integer> userNumber, List<Integer> computerNumber) {
+        return (int) IntStream.range(0, computerNumber.size())
+                .filter(index -> computerNumber.get(index).equals(userNumber.get(index)))
+                .count();
+    }
+
+    private static int calculateBall(List<Integer> userNumber, List<Integer> computerNumber) {
+        return (int) IntStream.range(0, computerNumber.size())
+                .filter(index -> computerNumber.contains(userNumber.get(index))
+                        && !computerNumber.get(index).equals(userNumber.get(index)))
+                .count();
+    }
+
 }
