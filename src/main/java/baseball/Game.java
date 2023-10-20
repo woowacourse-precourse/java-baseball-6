2c20050602ebf25c00b15333af4c42159eeb19c3
kg9585 <kg9585@naver.com>
2023-10-20 18:46:07 +0900

    feat: 불과 스트라이크가 같이 있는 수가 입력되면 불 계산이 추가되어야한다

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,10 +1,13 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.stream.IntStream;
 
 public class Game {
 
   private static final int NUMBER_LENGTH = 3;
+  private static final String STRIKE = "스트라이크";
+  private static final String BALL = "볼";
   private final RandomNumber computer = new RandomNumber();
   private final UserNumber user = new UserNumber();
 
@@ -19,36 +22,48 @@ public class Game {
       var input = Console.readLine();
       user.setUserNumber(input);
       var result = calculateResult(randomNumber, user.getUserNumber());
-      if (gameOver(result)) {
+      if (gameOver(Integer.parseInt(result))) {
         break;
       }
     }
   }
 
   private boolean gameOver(int strike) {
-    System.out.println(strike + " 스트라이크");
+    System.out.println(strike + STRIKE);
     return strike == NUMBER_LENGTH;
   }
 
-  private int calculateResult(int randomNumber, int userNumber) {
-    return strike(String.valueOf(randomNumber), String.valueOf(userNumber));
+  private String calculateResult(int randomNumber, int userNumber) {
+    return strikeAndBall(String.valueOf(randomNumber), String.valueOf(userNumber));
   }
 
-  private int strike(String randomNumber, String userNumber) {
-    var strike = 0;
-    var randomNumberArr = randomNumber.split("");
-    var userNumberArr = userNumber.split("");
+  private String strikeAndBall(String randomNumber, String userNumber) {
 
-    for (var i = 0; i < NUMBER_LENGTH; i++) {
-      if (randomNumberArr[i].equals(userNumberArr[i])) {
-        strike++;
-      }
-    }
-    return strike;
+    int strikeCount = getStrikeCount(randomNumber, userNumber);
+    int ballCount = getBallCount(randomNumber, userNumber);
+
+    return formatResult(strikeCount, ballCount);
+  }
+
+  private String formatResult(int strike, int ball) {
+    return String.format("%d%s %d%s", ball, BALL, strike, STRIKE);
+  }
+
+  public int getStrikeCount(String randomNumber, String userNumber) {
+    return (int) IntStream.range(0, NUMBER_LENGTH)
+        .filter(i -> randomNumber.charAt(i) == userNumber.charAt(i))
+        .count();
+  }
+
+  public int getBallCount(String randomNumber, String userNumber) {
+    return (int) IntStream.range(0, randomNumber.length())
+        .filter(i -> IntStream.range(0, userNumber.length())
+            .anyMatch(k -> randomNumber.charAt(i) == userNumber.charAt(k) && i != k))
+        .count();
   }
 
   // strike 메소드를 public으로 노출시키기 위해 추가한 메소드
-  public int publicCalculateResult(String randomNumber, String userNumber) {
-    return strike(randomNumber, userNumber);
+  public String publicCalculateResult(String randomNumber, String userNumber) {
+    return strikeAndBall(randomNumber, userNumber);
   }
 }
--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -9,7 +9,7 @@ class GameTest {
   @Test
   void returnStrike() {
     Game game = new Game();
-    assertEquals(1, game.publicCalculateResult("135", "123"));
+    assertEquals("1볼 1스트라이크", game.publicCalculateResult("589", "597"));
   }
 }
 
