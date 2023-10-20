4459e017cb1c269b624f271ed097f8876395dcf9
acs0209 <annjae4676@naver.com>
2023-10-20 21:14:22 +0900

    feat: implement program with TODOs

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,8 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        BaseballGame baseballGame = new BaseballGame();
+        baseballGame.run();
     }
 }
