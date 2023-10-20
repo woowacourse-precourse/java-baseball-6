179c9c1d3f607e037686c600919aeb58b2d500f7
leeedohyun <ldh19738@gmail.com>
2023-10-20 22:41:13 +0900

    feat: 볼 개수 세는 기능 구현

--- src/main/java/baseball/model/Referee.java
+++ src/main/java/baseball/model/Referee.java
@@ -11,4 +11,24 @@ public class Referee {
         }
         return strikeNumbers;
     }
+
+    public int calculateBallNumbers(String randomNumbers, String inputNumbers) {
+        int ballNumbers = 0;
+        for (int i = 0; i < Constants.NUMBER_OF_NUMBERS; i++) {
+            if (isRandomNumbersContainInputNumber(randomNumbers, inputNumbers.charAt(i)) &&
+                    randomNumbers.charAt(i) != inputNumbers.charAt(i)) {
+                ballNumbers++;
+            }
+        }
+        return ballNumbers;
+    }
+
+    private boolean isRandomNumbersContainInputNumber(String randomNumbers, char inputNumber) {
+        for (int i = 0; i < Constants.NUMBER_OF_NUMBERS; i++) {
+            if (randomNumbers.charAt(i) == inputNumber) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
