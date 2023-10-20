e2c26fc8e259bdc23d5ba1754ef0de06489f9be7
Gukin Han <gukin.dev@gmail.com>
2023-10-20 18:47:25 +0900

    Refactor: 스트라이크 판정 로직 간결화

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,26 +1,21 @@
 package baseball;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class BaseballGame {
-    private boolean running;
-    private boolean isThreeStrike;
+    private boolean isThreeStrikes;
 
     private final Map<Integer, Integer> randomNumbers = new HashMap<>();
     private final Validator validator = new Validator();
     private final Comparator comparator = new Comparator();
 
-    private final int DIGIT = 3;
-
     private void init() {
         generateRandomNumbers();
-        this.running = true;
+        this.isThreeStrikes = false;
     }
 
     public BaseballGame() {
@@ -29,11 +24,10 @@ public class BaseballGame {
 
     public void run() {
         init();
-        while (running) {
+        while (!isThreeStrikes) {
             String userInput = getUserInput();
             Map<Integer, Integer> guessedNumbers = validator.validateAndParseGuessedNumbers(userInput);
-            isThreeStrike = comparator.compare(randomNumbers, guessedNumbers);
-            checkThreeStrike();
+            isThreeStrikes = comparator.checkThreeStrikeAndPrintResult(randomNumbers, guessedNumbers);
         }
         askRerunOrExit();
     }
@@ -43,35 +37,24 @@ public class BaseballGame {
         return readLine();
     }
 
-    private void checkThreeStrike() {
-        if (isThreeStrike) {
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            exit();
-        }
-    }
-
     private void askRerunOrExit() {
-        if (isThreeStrike) {
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            int userInput = Integer.parseInt(readLine());
-            if (userInput == 2) return;
-            run();
-        }
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        int userInput = Integer.parseInt(readLine());
+        if (userInput == 2) return;
+        run();
     }
 
     private void generateRandomNumbers() {
         int index = 0;
+        int digitLimit = 3;
 
         if (!randomNumbers.isEmpty()) randomNumbers.clear();
-        while (index < DIGIT) {
+        while (index < digitLimit) {
             int randomNumber = pickNumberInRange(1, 9);
             if (!randomNumbers.containsKey(randomNumber)) {
                 randomNumbers.put(randomNumber, index++);
             }
         }
-    }
 
-    public void exit() {
-        this.running = false;
     }
 }
