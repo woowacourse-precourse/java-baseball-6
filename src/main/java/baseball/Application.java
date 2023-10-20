466c2f304e4d4e20122aefad68f600c4c89cf0fc
Sunhee Kim <sunni372@icloud.com>
2023-10-21 01:03:24 +0900

    feat: 게임 시작, 실행, 종료 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,8 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 숫자 야구 게임을 시작합니다.
+        BaseballGame baseballGame = new BaseballGame();
+        baseballGame.start();
     }
 }
---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,23 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class BaseballGame {
+    public void start() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        run();
+    }
+
+    public void run() {
+        exist();
+    }
+
+    public void exist() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        int inputMode = Integer.parseInt(Console.readLine());
+        if (inputMode == 1) {
+            run();
+        } else if (inputMode == 2) {
+        }
+    }
+}
