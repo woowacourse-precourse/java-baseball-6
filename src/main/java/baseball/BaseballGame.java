db0c773c830a9f8749e283497d82d2e4447bee5c
voyager003 <miraclex000@gmail.com>
2023-10-20 20:51:59 +0900

    Feat: 심판이 판단한 결과를 출력하고, 게임 종료조건이 충족하는 지 판단하는 기능 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,9 +1,21 @@
 package baseball;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BaseballGame {
 
     public void start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+        GameStatus gameStatus = GameStatus.CONTINUE;
+        BotPlayer botPlayer = new BotPlayer(new ArrayList<>());
+        List<Integer> botNumbers = botPlayer.generateNumbers();
+
+        do {
+            List<Integer> integers = Player.inputPlayerNumbers();
+            GameStatus status = BaseballUmpire.judge(botNumbers, integers);
+            gameStatus = status;
+        } while (gameStatus == GameStatus.CONTINUE);
     }
 }
