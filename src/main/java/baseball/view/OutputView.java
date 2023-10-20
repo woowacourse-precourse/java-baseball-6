b1824a644dd27a491ede7294c5bd8183529ba02a
leeedohyun <ldh19738@gmail.com>
2023-10-21 01:03:45 +0900

    feat: 볼 개수와 정답 문구 출력하는 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -9,6 +9,7 @@ public class OutputView {
     public static final String BALL = "볼";
     public static final String BLANK_SPACE = " ";
     public static final String RESTART_OR_END_INPUT_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String ANSWER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     public static void printBaseballGameStartMessage() {
         System.out.println(BASEBALL_GAME_START_MESSAGE);
@@ -26,12 +27,19 @@ public class OutputView {
         System.out.println(strikeNumbers + STRIKE);
     }
 
+    public static void printOnlyBall(int ballNumbers) {
+        System.out.println(ballNumbers + BALL);
+    }
+
     public static void printBallAndStrike(int ballNumbers, int strikeNumbers) {
         System.out.println(ballNumbers + BALL + BLANK_SPACE + strikeNumbers + STRIKE);
     }
 
-    public static void printRestartOrEndInpuMessage() {
-        System.out.println(RESTART_OR_END_INPUT_MESSAGE);
+    public static void printAnswerMessage() {
+        System.out.println(ANSWER_MESSAGE);
     }
 
+    public static void printRestartOrEndInputMessage() {
+        System.out.println(RESTART_OR_END_INPUT_MESSAGE);
+    }
 }
