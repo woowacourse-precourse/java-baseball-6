2bd8d97d8f0fcd1490d863494e13e904e0aa9107
Haebin <joker7011@naver.com>
2023-10-21 01:08:48 +0900

    fix: 숫자 입력 간 개행 오류 수정

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -7,7 +7,7 @@ public class OutputView {
     }
 
     public static void printRequestNumber() {
-        System.out.println("숫자를 입력해주세요 : ");
+        System.out.print("숫자를 입력해주세요 : ");
     }
 
     public static void printRequestRestartOrExit() {
