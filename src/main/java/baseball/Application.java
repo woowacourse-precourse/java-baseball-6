1b15362c8d557b2b473be8867ad5970a0e6bb194
chaeshee0908 <chaeshee0908@naver.com>
2023-10-21 19:27:27 +0900

    [feat] 게임 시작 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
 public class Application {
+    public void startGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application app = new Application();
+        app.startGame();    // 게임 시작
+
     }
 }
