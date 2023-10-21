602811bff1697cc761b76a071106cdc6fb7f3796
hil679 <hil679922@gmail.com>
2023-10-21 13:34:25 +0900

    Feat(inputUserNum): Add inputUserNum founction
    - 사용자 입력을 받은 후 랜덤 숫자와 비교를 위해 리스트로 사용자 입력 반환

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BaseballGame {
     public void run(){
-
     }
+
+    private List<Integer> inputUserNum(){
+        int userInput = Integer.valueOf(Console.readLine());
+        List<Integer> userNum = new ArrayList<>();
+
+        while(userInput != 0){
+            userNum.add(0, userInput % 10);
+            userInput /= 10;
+        }
+        return userNum;
+    }
+
 }
