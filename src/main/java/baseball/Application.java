553226db977e1259bbfd526bacdef809d719aace
gengminy <kls1998@naver.com>
2023-10-22 01:16:09 +0900

    fix: Scanner 제거 및 Console 로 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,27 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.List;
-import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다");
 
         final BaseBall baseBall = new BaseBall(3);
-        try (Scanner scanner = new Scanner(System.in)) {
-            while (true) {
-                System.out.println("숫자를 입력해주세요 : ");
-                List<Integer> player = NumberParser.parseIntegerList(scanner.next());
-                baseBall.doGame(player);
+        while (true) {
+            System.out.println("숫자를 입력해주세요 : ");
+            List<Integer> player = NumberParser.parseIntegerList(Console.readLine());
+            baseBall.doGame(player);
 
-                if (baseBall.isOver()) {
-                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                    int operation = NumberParser.parseSingleDigitNumber(scanner.next());
-                    if (operation == 1) {
-                        baseBall.newGame();
-                    } else {
-                        break;
-                    }
+            if (baseBall.isOver()) {
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                int operation = NumberParser.parseSingleDigitNumber(Console.readLine());
+                if (operation == 1) {
+                    baseBall.newGame();
+                } else {
+                    break;
                 }
             }
         }
--- src/main/java/baseball/BaseBall.java
+++ src/main/java/baseball/BaseBall.java
@@ -1,8 +1,5 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class BaseBall {
