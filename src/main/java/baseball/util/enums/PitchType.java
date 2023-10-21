8f3f43ba16821616d303ea31f10da7256967e133
yummygyudon <bang2brew@gmail.com>
2023-10-22 06:16:37 +0900

    STYLE : 판정 구질에 대해 직관적인 변수명으로 수정

--- src/main/java/baseball/util/enums/PitchType.java
+++ src/main/java/baseball/util/enums/PitchType.java
@@ -3,13 +3,13 @@ package baseball.util.enums;
 public enum PitchType {
     NOTHING("낫싱"), STRIKE("스트라이크 "), BALL("볼 ")
     ;
-    private final String value;
+    private final String type;
 
     PitchType(String value) {
-        this.value = value;
+        this.type = value;
     }
 
-    public String getValue() {
-        return value;
+    public String getType() {
+        return type;
     }
 }
