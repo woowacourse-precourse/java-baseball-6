7f7fdfb82a33edb74813ceb5cfefb9ddea097327
깃SeoMoonK <sunghoon1216@naver.com>
2023-10-21 17:14:15 +0900

    Feat: 컴퓨터의 난수 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,31 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        
+        // 1. 난수 생성하기
+        List<Integer> computer = new ArrayList<>();
+
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+
+            //1부터 9까지의 난수 중 중복되지 않게 3개의 난수를 computer라는 리스트에 집어넣음.
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        sb.append(computer);
+
+        System.out.println(sb.toString());
+
     }
 }
