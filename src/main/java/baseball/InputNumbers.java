65ec2ea045ffef2a6befff217e2186b8656472ce
hongkikii <hyangmeeh@icloud.com>
2023-10-22 00:35:22 +0900

    feat: 숫자 입력 문구 출력하기, 숫자 입력받기

---
+++ src/main/java/baseball/InputNumbers.java
@@ -0,0 +1,67 @@
+package com.baseball;
+
+import static baseball.Constants.COUNT;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class InputNumbers extends Input {
+
+    private List<Integer> inputNumbers;
+
+    public List<Integer> getInputNumbers() {
+        return this.inputNumbers;
+    }
+
+    @Override
+    public void readInput() {
+        String readLine = Console.readLine();
+
+        if (isNotValidLength(readLine)) {
+            throwIllegalArgumentException();
+        }
+        if (isNotNumber(readLine)) {
+            throwIllegalArgumentException();
+        }
+        if (isNotDifferentNumber(readLine)) {
+            throwIllegalArgumentException();
+        }
+        StringToIntegerList(readLine);
+    }
+
+    private boolean isNotValidLength(String readLine) {
+        return readLine.length() != COUNT;
+    }
+
+    private boolean isNotNumber(String readLine) {
+        for (int i = 0; i < readLine.length(); i++) {
+            char letter = readLine.charAt(i);
+            if (letter < '1' || letter > '9') {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isNotDifferentNumber(String readLine) {
+        HashSet<Character> characterSet = new HashSet<>();
+        for (char character : readLine.toCharArray()) {
+            if (characterSet.contains(character)) {
+                return true;
+            }
+            characterSet.add(character);
+        }
+        return false;
+    }
+
+    private void StringToIntegerList(String readLine) {
+        inputNumbers = new ArrayList<>();
+        for (int i = 0; i < readLine.length(); i++) {
+            char character = readLine.charAt(i);
+            String letter = String.valueOf(character);
+            inputNumbers.add(Integer.parseInt(letter));
+        }
+    }
+}
