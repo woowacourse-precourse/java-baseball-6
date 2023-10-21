74b75300861e18ef3cf3dfc195fcb191f9d6fdc1
jae woo Jung <awfjol2008@naver.com>
2023-10-21 21:24:06 +0900

    feat: 입력된 문자열이 비어있지 않은지 검증하는 메소드 구현

--- src/main/java/baseball/util/NumberUtil.java
+++ src/main/java/baseball/util/NumberUtil.java
@@ -1,4 +1,9 @@
 package baseball.util;
 
 public class NumberUtil {
+    protected static void validateEmpty(String number) {
+        if (number.isEmpty()) {
+            throw new IllegalArgumentException("입력값은 비어있지 않아야 합니다.");
+        }
+    }
 }
