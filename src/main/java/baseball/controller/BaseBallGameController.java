d02dc96e8e42fee0e9c521655f0577bd2c4bc3ce
SungWoo <sungwoo166@gmail.com>
2023-10-21 16:44:04 +0900

    feat: Implement baseball game loop

--- src/main/java/baseball/controller/BaseBallGameController.java
+++ src/main/java/baseball/controller/BaseBallGameController.java
@@ -2,22 +2,36 @@ package baseball.controller;
 
 import baseball.BaseballGameMessage;
 import baseball.service.BaseBallService;
+import baseball.view.BaseballInputView;
+import baseball.view.BaseballOutputView;
 
 import java.util.List;
 
 public class BaseBallGameController {
     private final BaseBallService baseBallService;
+    private final BaseballInputView baseballInputView;
+    private final BaseballOutputView baseballOutputView;
 
     public BaseBallGameController() {
         baseBallService = new BaseBallService();
+        baseballInputView = new BaseballInputView();
+        baseballOutputView = new BaseballOutputView();
     }
 
     public void startGame() {
-        System.out.println(BaseballGameMessage.START_GAME);
+        baseballOutputView.printMessage(BaseballGameMessage.START_GAME);
 
         List<Integer> targetNumber = baseBallService.generateRandomNumbers();
+
+        while (true) {
+            baseballOutputView.printMessage(BaseballGameMessage.REQUEST_USER_INPUT);
+            List<Integer> userInput = baseballInputView.userGuessInput();
+            String gameResult = baseBallService.getGameResult(targetNumber, userInput).toString();
+            baseballOutputView.printGameResult(gameResult);
+            if (gameResult.equals("3스트라이크")) {
+                System.out.println(BaseballGameMessage.GAME_OVER);
+                break;
+            }
+        }
     }
-
 }
-
-
