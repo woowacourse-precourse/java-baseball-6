7fe1648edc0400b82569e13e139a0a511db543e1
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-10-21 21:51:39 +0900

    feat(InputView) : 플레이어에게 입력받는 기능
    플레이어에게 입력을 받는 기능을 완성

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,9 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String enterPlayerInput() {
+        return Console.readLine();
+    }
+}
