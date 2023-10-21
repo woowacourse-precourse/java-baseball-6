db4dc12904a311f0623be29771c4545c8a78ccc9
HiiWee <wpdlzhf159@gmail.com>
2023-10-21 20:43:22 +0900

    refactor: Numbers 검증 부분 내부 validator 클래스로 변경

--- src/main/java/baseball/domain/Numbers.java
+++ src/main/java/baseball/domain/Numbers.java
@@ -16,9 +16,9 @@ public class Numbers {
         this.numbers = numbers;
     }
 
-    public static Numbers createUserNumbers(final String inputUserNumber) {
-        validate(inputUserNumber);
-        List<Integer> userNumbers = Arrays.stream(inputUserNumber.split(""))
+    public static Numbers createUserNumbers(final String userInputNumber) {
+        NumbersValidator.validate(userInputNumber);
+        List<Integer> userNumbers = Arrays.stream(userInputNumber.split(""))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
         return new Numbers(userNumbers);
@@ -47,39 +47,44 @@ public class Numbers {
         return Objects.equals(computerNumber, userNumber);
     }
 
-    private static void validate(final String inputUserNumber) {
-        validateLength(inputUserNumber);
-        validateInteger(inputUserNumber);
-        validateRandomNumber(inputUserNumber);
-    }
 
-    private static void validateInteger(final String inputUserNumber) {
-        try {
-            Integer.parseInt(inputUserNumber);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자만 입력해야 합니다.");
-        }
-    }
+    static private class NumbersValidator {
 
-    private static void validateLength(final String inputUserNumber) {
-        if (inputUserNumber.length() != NUMBER_LENGTH) {
-            throw new IllegalArgumentException("3개의 숫자를 입력해야 합니다.");
+        private static void validate(final String userInputNumber) {
+            validateLength(userInputNumber);
+            validateInteger(userInputNumber);
+            validateRandomNumber(userInputNumber);
         }
-    }
 
-    private static void validateRandomNumber(final String inputUserNumber) {
-        String[] userNumbers = inputUserNumber.split("");
-        if (userNumbers[0].equals(userNumbers[1])
-                || userNumbers[1].equals(userNumbers[2])
-                || userNumbers[0].equals(userNumbers[2])) {
-            throw new IllegalArgumentException("중복된 숫자를 입력할 수 없습니다.");
+        private static void validateInteger(final String userInputNumber) {
+            try {
+                Integer.parseInt(userInputNumber);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("숫자만 입력해야 합니다.");
+            }
         }
-        validateZeroNumber(userNumbers);
-    }
 
-    private static void validateZeroNumber(final String[] userNumbers) {
-        if (userNumbers[0].equals("0") || userNumbers[1].equals("0") || userNumbers[2].equals("0")) {
-            throw new IllegalArgumentException("숫자는 1~9 사이의 숫자만 입력할 수 있습니다.");
+        private static void validateLength(final String userInputNumber) {
+            if (userInputNumber.length() != NUMBER_LENGTH) {
+                throw new IllegalArgumentException("3개의 숫자를 입력해야 합니다.");
+            }
         }
+
+        private static void validateRandomNumber(final String userInputNumber) {
+            String[] userNumbers = userInputNumber.split("");
+            if (userNumbers[0].equals(userNumbers[1])
+                    || userNumbers[1].equals(userNumbers[2])
+                    || userNumbers[0].equals(userNumbers[2])) {
+                throw new IllegalArgumentException("중복된 숫자를 입력할 수 없습니다.");
+            }
+            validateZeroNumber(userNumbers);
+        }
+
+        private static void validateZeroNumber(final String[] userNumbers) {
+            if (userNumbers[0].equals("0") || userNumbers[1].equals("0") || userNumbers[2].equals("0")) {
+                throw new IllegalArgumentException("숫자는 1~9 사이의 숫자만 입력할 수 있습니다.");
+            }
+        }
+
     }
 }
