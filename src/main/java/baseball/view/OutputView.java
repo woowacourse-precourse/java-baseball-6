9da2ddcc9230c8a3187d0c9051fd9e7780fa7666
siwol25 <cyj5695@naver.com>
2023-10-22 01:00:02 +0900

    feat: add function to display game results

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,36 @@
+package baseball.view;
+
+import baseball.constant.GameConstants;
+
+public class OutputView {
+    private final String GUESS_SUCCESS_MESSAGE = GameConstants.FIXED_DIGIT_NUMBER + "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private final String STRIKE_MESSAGE = "스트라이크";
+    private final String BALL_MESSAGE = "볼";
+    private final String NOTTING_MESSAGE = "낫싱";
+
+    public void printGameResult(int strikeCount, int ballCount) {
+        if (strikeCount == GameConstants.FIXED_DIGIT_NUMBER) {
+            System.out.println(GUESS_SUCCESS_MESSAGE);
+
+            return;
+        }
+
+        if (strikeCount == 0 && ballCount == 0) {
+            System.out.println(NOTTING_MESSAGE);
+
+            return;
+        }
+
+        String resultMessage = "";
+
+        if (ballCount != 0) {
+            resultMessage = ballCount + BALL_MESSAGE;
+        }
+
+        if (strikeCount != 0) {
+            resultMessage += " " + strikeCount + STRIKE_MESSAGE;
+        }
+
+        System.out.println(resultMessage);
+    }
+}
