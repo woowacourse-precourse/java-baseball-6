8a005115d7d70be8701ff2e47f9973474b2a331e
kimdohyung <pdohyung@gmail.com>
2023-10-22 01:12:03 +0900

    refactor: 객체 생성 시 list를 초기화 하도록 수정

--- src/main/java/baseball/model/ComputerNumber.java
+++ src/main/java/baseball/model/ComputerNumber.java
@@ -1,33 +1,33 @@
 package baseball.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
 public class ComputerNumber {
-	private List<Integer> computerNumber;
+    private List<Integer> computerNumber;
 
-	public ComputerNumber() {
-		this.computerNumber = computerNumber();
-	}
+    public ComputerNumber() {
+        this.computerNumber = List.of(0, 0, 0);
+    }
 
-	public List<Integer> getComputerNumber() {
-		return computerNumber;
-	}
+    public List<Integer> getComputerNumber() {
+        return computerNumber;
+    }
 
-	public void resetComputerNumber(){
-		this.computerNumber = computerNumber();
-	}
+    public void newComputerNumber() {
+        this.computerNumber = computerNumber();
+    }
 
-	private List<Integer> computerNumber() {
-		List<Integer> computer = new ArrayList<>();
-		while (computer.size() < 3) {
-			int randomNumber = Randoms.pickNumberInRange(1, 9);
-			if (!computer.contains(randomNumber)) {
-				computer.add(randomNumber);
-			}
-		}
-		return computer;
-	}
+    private List<Integer> computerNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
 }
