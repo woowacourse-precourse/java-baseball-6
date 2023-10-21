2f18ce9b175ecec2fdf91951108adabc9df90a1b
sungwon2598 <tjddnjs2598@gmail.com>
2023-10-21 17:33:23 +0900

    feat(MainController) : 게임시작 기능추가
    processInputAndCompare미완성

---
+++ src/main/java/baseball/Controller/MainController.java
@@ -0,0 +1,33 @@
+package baseball.Controller;
+
+import baseball.Model.GameData;
+import baseball.View.InputView;
+import baseball.View.OutputView;
+
+public class MainController {
+    OutputView outputView = new OutputView();
+    InputView inputView = new InputView();
+    GameData gameData = new GameData();
+    RandomNumbersGenerator randomNumbersGenerator = new RandomNumbersGenerator();
+    PlayerInputController playerInputController = new PlayerInputController();
+    Comparator comparator = new Comparator();
+    Validator validator = new Validator();
+
+    private static int state = 1;
+    private static int count = 0;
+    public MainController() {
+        state = 1;
+        count = 0;
+    }
+
+    public void playGame(){
+        outputView.printGameStart();
+        while (state == 1){
+            if(count == 0){
+                gameData.setComputerNumber(randomNumbersGenerator.generateNumbers());
+            }
+            //processInputAndCompare();
+        }
+    }
+
+}
