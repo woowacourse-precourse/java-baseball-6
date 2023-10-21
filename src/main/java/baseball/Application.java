1e3f05030cc7104a3e2c226301e17b5ec6adf24f
yu-teacher <yusm1231@gmail.com>
2023-10-21 11:01:58 +0900

    test_commit 2

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,27 +4,28 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        Computer computer = new Computer();
-        InputAndOutput text = new InputAndOutput();
-        NumberBaseball baseball = new NumberBaseball();
+        Judge judge = new Judge();
 
-        text.gameStart();
+        System.out.println("숫자 야구 게임을 시작합니다.");
 
         boolean isGame = true;
 
         while (isGame) {
-            int[] computerNumber = computer.computerCreateNumber();
+            int[] computerNumber = judge.createComputerNumber();
+            int[] userNumber;
+            int[] gameResult;
 
-            boolean isThreeStrike;
             do {
-                text.scanInputNumber();
+                System.out.print("숫자를 입력해주세요 : ");
 
                 String inputNum = Console.readLine();
-                int[] userNumber = baseball.create(inputNum);
-                isThreeStrike = baseball.playGame(computerNumber, userNumber);
-            } while (isThreeStrike);
+                userNumber = judge.createUserNumber(inputNum);
+                gameResult = judge.playGame(computerNumber, userNumber);
+                System.out.println(judge.checkStrikeAndBall(gameResult));
+            } while (judge.isGameEnd(gameResult));
 
-            text.gameEnd();
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n "
+                    + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
             String isNum = Console.readLine();
             isGame = Integer.parseInt(isNum) == 1;
---
+++ src/main/java/baseball/Judge.java
@@ -0,0 +1,82 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Judge {
+    public int[] createComputerNumber() {
+        int[] numbers = new int[3];
+        for (int i = 0; i < 3; i++) {
+            int tempNum;
+            do {
+                tempNum = Randoms.pickNumberInRange(1, 9);
+            } while (containsNumber(numbers, tempNum));
+            numbers[i] = tempNum;
+        }
+        return numbers;
+    }
+
+    public int[] createUserNumber(String userInputStr){
+        checkError(userInputStr);
+        int userInputNum = Integer.parseInt(userInputStr);
+        int[] userInputArr = new int[3];
+        userInputArr[0] = userInputNum / 100;
+        userInputArr[1] = (userInputNum / 10) % 10;
+        userInputArr[2] = userInputNum % 10;
+        return userInputArr;
+    }
+
+    public int[] playGame(int[] computer, int[] user) {
+        int[] strikeAndBall = new int[2];
+        for (int i = 0; i < 3; i++) {
+            if (computer[i] == user[i])
+                strikeAndBall[1]++; // strikes
+            else if (containsNumber(computer, user[i]))
+                strikeAndBall[0]++; // balls
+        }
+        return strikeAndBall;
+    }
+
+    public String checkStrikeAndBall(int[] strikeAndBall) {
+        String message = switch (strikeAndBall[0]) {
+            case 1 -> "1볼 ";
+            case 2 -> "2볼 ";
+            case 3 -> "3볼 ";
+            default -> "";
+        };
+
+        message += switch (strikeAndBall[1]) {
+            case 1 -> "1스트라이크";
+            case 2 -> "2스트라이크";
+            case 3 -> "3스트라이크";
+            default -> "낫싱";
+        };
+
+        return message;
+    }
+
+    public boolean isGameEnd(int[] gameResult){
+        return gameResult[1] == 3 ? false : true;
+    }
+
+    private void checkError(String inputStr){
+        if (inputStr.length() != 3)
+            throw new IllegalArgumentException();
+
+        int[] inputNumArray = new int[inputStr.length()];
+
+        for (int i = 0; i < 3; i++) {
+            int tempIndex = Character.getNumericValue(inputStr.charAt(i));
+            if(tempIndex < 1 || tempIndex > 9 || containsNumber(inputNumArray,tempIndex))
+                throw new IllegalArgumentException();
+            inputNumArray[i] = tempIndex;
+        }
+    }
+
+    private boolean containsNumber(int[] array, int target) {
+        for (int number : array)
+            if (number == target)
+                return true;
+        return false;
+    }
+
+}
