d5d51934b5a291303b3b8fc88b312ee7675b3e47
yumble <hnjn1101@gmail.com>
2023-10-21 12:49:39 +0900

    feat: 게임 시작 초기 세팅
    - 게임 시작 출력문 출력
    - 랜덤 숫자 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,29 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+    public void gameStart() {
+        printGameStartMessage();
+        extractRandomNumber();
+    }
+
+    public static void printGameStartMessage() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+    private static void extractRandomNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
     }
 }
