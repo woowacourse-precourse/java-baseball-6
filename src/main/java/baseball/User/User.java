482076d60fd936848199713a3d2f94a2d308aeec
seaniiio <cnc4750@naver.com>
2023-10-21 22:21:03 +0900

    feat: 사용자로부터 숫자 입력받는 기능 구현

---
+++ src/main/java/baseball/User/User.java
@@ -0,0 +1,12 @@
+package baseball.User;
+
+public class User {
+    public void main() {
+
+    }
+    public void UserInput() {
+        UserInput.main();
+    }
+
+
+}
---
+++ src/main/java/baseball/User/UserInput.java
@@ -0,0 +1,8 @@
+package baseball.User;
+import camp.nextstep.edu.missionutils.Console;
+public class UserInput {
+    public static void main() {
+        String userInput = Console.readLine();
+        System.out.printf(userInput);
+    }
+}
