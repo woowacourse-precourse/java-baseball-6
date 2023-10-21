b25bace696302e1f5703a6676bbacdfa6291165c
jeondui <jeondui@gmail.com>
2023-10-21 16:10:29 +0900

    feat(NumericValidator): 숫자 범위 검사 기능

--- src/main/java/baseball/util/NumericValidator.java
+++ src/main/java/baseball/util/NumericValidator.java
@@ -15,4 +15,13 @@ public class NumericValidator {
         return set.size() == 3;
     }
 
+    // 1 ~ 9 사이의 숫자인지 검사
+    private boolean isBetween1And9(List<Integer> numbers) {
+        boolean status = true;
+        for (int i = 0; i < numbers.size(); i++) {
+            status = numbers.get(i) >= 1 && numbers.get(i) <= 9;
+        }
+        return status;
+    }
+
 }
