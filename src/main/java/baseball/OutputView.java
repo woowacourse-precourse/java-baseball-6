d9a57b5a818af1fcc979c6e3c376de5e28e0803a
kunsanglee <gosmdochee2@gmail.com>
2023-10-21 17:28:31 +0900

    refactor(OutputView): 상수 선언 및 matchResult() 메서드 수정

--- src/main/java/baseball/OutputView.java
+++ src/main/java/baseball/OutputView.java
@@ -2,8 +2,8 @@ package baseball;
 
 public class OutputView {
 
-    public static final String START_BASEBALL_GAME_MESSAGE = "숫자 야구 게임을 시작합니다.";
-    public static final String END_BASEBALL_GAME_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임종료";
+    private static final String START_BASEBALL_GAME_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String END_BASEBALL_GAME_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임종료";
 
     public static void startGame() {
         System.out.println(START_BASEBALL_GAME_MESSAGE);
@@ -14,7 +14,7 @@ public class OutputView {
     }
 
     public static boolean matchResult(Score score) {
-        System.out.print(score.toString());
+        System.out.println(score.toString());
         return score.isStrikeOut();
     }
 }
