586240daea49a6076385f3e86231780a08ef3cbb
nk kim <trait99@daum.net>
2023-10-21 18:34:29 +0900

    getUserInput 요구사항 예외처리 수정 Application.java

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,10 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
+
 
 public class Application {
 
@@ -44,54 +44,50 @@ public class Application {
     }
 
     private static List<Integer> generateRandomNumbers() {
-        List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
-        Collections.shuffle(numbers);
-        return numbers.subList(0, 3);
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+        return numbers;
     }
 
     private static List<Integer> getUserInput() {
-        Scanner scanner = new Scanner(System.in);
 
         while (true) {
             System.out.print("3자리 숫자를 입력해주세요 : ");
-            String input = scanner.nextLine();
+            String input = Console.readLine();
 
             // 길이 검증
             if (input.length() != 3) {
-                System.out.println("입력한 값이 3자리 수가 아닙니다. 다시 입력해주세요.");
-                continue;
+                throw new IllegalArgumentException("입력한 값이 3자리 수가 아닙니다.");
             }
 
             // 문자나 기호 포함 검증
             if (!input.matches("\\d{3}")) {
-                System.out.println("숫자가 아닌 문자나 기호가 포함되어 있습니다. 다시 입력해주세요.");
-                continue;
+                throw new IllegalArgumentException("숫자가 아닌 문자나 기호가 포함되어 있습니다.");
             }
 
             List<Integer> numbers = new ArrayList<>();
-            boolean isValid = true;
             for (char c : input.toCharArray()) {
                 int num = c - '0';
                 // 범위 검증
                 if (num < 1 || num > 9) {
-                    System.out.println("입력된 숫자가 1부터 9의 범위를 벗어났습니다. 다시 입력해주세요.");
-                    isValid = false;
-                    break;
+                    throw new IllegalArgumentException("입력된 숫자가 1부터 9의 범위를 벗어났습니다.");
                 }
                 // 중복 검증
                 if (numbers.contains(num)) {
-                    System.out.println("중복된 숫자가 있습니다. 다시 입력해주세요.");
-                    isValid = false;
-                    break;
+                    throw new IllegalArgumentException("중복된 숫자가 있습니다.");
                 }
                 numbers.add(num);
             }
 
-            if (isValid) {
-                return numbers;
-            }
+            return numbers;
         }
     }
+
 
 
     private static Result compareNumbers(List<Integer> userInput, List<Integer> computerNumbers) {
@@ -113,6 +109,7 @@ public class Application {
         if (result.getBalls() == 0 && result.getStrikes() == 0) {
             System.out.println("낫싱");
         } else if (result.getStrikes() == 3) {
+            System.out.println("3스트라이크");
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         } else {
             System.out.println(result.getBalls() + "볼 " + result.getStrikes() + "스트라이크");
@@ -120,11 +117,23 @@ public class Application {
     }
 
     private static boolean isGameContinued() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        int choice = scanner.nextInt();
+        String input = Console.readLine();
 
-        return choice == 1;
+        try {
+            int choice = Integer.parseInt(input);
+            if (choice == 1) {
+                return true;
+            } else if (choice == 2) {
+                return false;
+            } else {
+                throw new IllegalArgumentException("잘못된 입력 값입니다.");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("올바르지 않은 입력 값입니다.");
+        }
     }
+
+
 }
 
