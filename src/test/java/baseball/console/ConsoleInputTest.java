f7c0056c96aa3c94a68ba2cda3e7150d426a22fb
takealook97 <takealook97@naver.com>
2023-10-21 23:46:17 +0900

    test: 입력 테스트 케이스 예외 추가
    입력 시 IllegalArgumentException에 대한 테스트를 추가적으로 작성하였다. 추가적으로, 기존에는 입력값과 입력된값만 비교했다면, 현재는 유효성 검사 로직까지 포함하여 테스트를 하도록 수정했다.

--- src/test/java/baseball/console/ConsoleInputTest.java
+++ src/test/java/baseball/console/ConsoleInputTest.java
@@ -3,15 +3,19 @@ package baseball.console;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import baseball.console.util.InputValidation;
 import camp.nextstep.edu.missionutils.Console;
 
 class ConsoleInputTest extends IOTest {
 	private final ConsoleInput consoleInput;
+	private final InputValidation inputValidation;
 
 	public ConsoleInputTest() {
 		consoleInput = new ConsoleInput();
+		inputValidation = new InputValidation();
 	}
 
 	@AfterEach
@@ -21,23 +25,71 @@ class ConsoleInputTest extends IOTest {
 	}
 
 	@Test
-	void 세자리입력테스트() {
-		String input = "123";
-		systemIn(input);
-		assertEquals(input, consoleInput.getGameInput());
+	void 세자리_정상입력테스트() {
+		String testInput = "123";
+		systemIn(testInput);
+		String actualInput = consoleInput.getGameInput();
+		assertTrue(inputValidation.isValidGameInput(actualInput));
+		assertEquals(testInput, actualInput);
 	}
 
 	@Test
-	void 게임재시작입력테스트() {
-		String input = "1";
-		systemIn(input);
-		assertEquals(input, consoleInput.getEndInput());
+	void 세자리_긴입력테스트() {
+		String testInput = "1234";
+		systemIn(testInput);
+		Assertions.assertThrows(IllegalArgumentException.class, consoleInput::getGameInput);
 	}
 
 	@Test
-	void 게임종료입력테스트() {
-		String input = "2";
-		systemIn(input);
-		assertEquals(input, consoleInput.getEndInput());
+	void 세자리_짧은입력테스트() {
+		String testInput = "12";
+		systemIn(testInput);
+		Assertions.assertThrows(IllegalArgumentException.class, consoleInput::getGameInput);
+	}
+
+	@Test
+	void 세자리_문자입력테스트() {
+		String testInput = "a12";
+		systemIn(testInput);
+		Assertions.assertThrows(IllegalArgumentException.class, consoleInput::getGameInput);
+	}
+
+	@Test
+	void 세자리_음수입력테스트() {
+		String testInput = "-12";
+		systemIn(testInput);
+		Assertions.assertThrows(IllegalArgumentException.class, consoleInput::getGameInput);
+	}
+
+	@Test
+	void 세자리_0입력테스트() {
+		String testInput = "120";
+		systemIn(testInput);
+		Assertions.assertThrows(IllegalArgumentException.class, consoleInput::getGameInput);
+	}
+
+	@Test
+	void 한자리_재시작입력테스트() {
+		String testInput = "1";
+		systemIn(testInput);
+		String actualInput = consoleInput.getEndInput();
+		assertTrue(inputValidation.isValidEndInput(actualInput));
+		assertEquals(testInput, actualInput);
+	}
+
+	@Test
+	void 한자리_종료입력테스트() {
+		String testInput = "2";
+		systemIn(testInput);
+		String actualInput = consoleInput.getEndInput();
+		assertTrue(inputValidation.isValidEndInput(actualInput));
+		assertEquals(testInput, actualInput);
+	}
+
+	@Test
+	void 한자리_범위밖입력테스트() {
+		String testInput = "3";
+		systemIn(testInput);
+		Assertions.assertThrows(IllegalArgumentException.class, consoleInput::getGameInput);
 	}
 }
