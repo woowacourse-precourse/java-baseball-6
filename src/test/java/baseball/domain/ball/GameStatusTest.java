a70dee0398c8a18498a8ca1d7e131ea94ff56b99
mainfn <_@mainfn.dev>
2023-10-22 02:38:29 +0900

    fixed: test changed to verify matched enum constant when input is invalid

--- src/test/java/baseball/domain/ball/GameStatusTest.java
+++ src/test/java/baseball/domain/ball/GameStatusTest.java
@@ -1,34 +1,35 @@
 package baseball.domain.ball;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public final class GameStatusTest {
 
-  @DisplayName("String 1 또는 2로 GameStatus enum constant를 가져올 수 있다")
-  @Test
-  void gameStatusSuccess() {
-    // given
-    // when
-    final GameStatus status = GameStatus.of("1");
-    final GameStatus status2 = GameStatus.of("2");
+    @DisplayName("String 1 또는 2로 GameStatus enum constant를 가져올 수 있다")
+    @Test
+    void gameStatusSuccess() {
+        // given
+        // when
+        final GameStatus status = GameStatus.of("1");
+        final GameStatus status2 = GameStatus.of("2");
 
-    // then
-    assertThat(status).isEqualTo(GameStatus.REPLAY);
-    assertThat(status2).isEqualTo(GameStatus.EXIT);
-  }
+        // then
+        assertThat(status).isEqualTo(GameStatus.REPLAY);
+        assertThat(status2).isEqualTo(GameStatus.EXIT);
+    }
 
-  @DisplayName("String 1 또는 2 외의 값으로 GameStatus enum constant를 생성하면 예외 발생")
-  @Test
-  void gameStatusFailure() {
-    // given
-    // when
-    // then
-    assertThatThrownBy(() -> GameStatus.of("3"))
-        .isInstanceOf(IllegalArgumentException.class)
-        .hasMessage("GameStatus는 반드시 1, 2 중 하나를 입력해서 만들 수 있습니다.");
-  }
+    @DisplayName("String 1 또는 2 외의 값으로 GameStatus enum constant를 생성하면 예외 발생")
+    @Test
+    void gameStatusFailure() {
+        // given
+        final String input = "3";
+
+        // when
+        final GameStatus gameStatus = GameStatus.of(input);
+
+        // then
+        assertThat(gameStatus).isEqualTo(GameStatus.UNKNOWN);
+    }
 }
