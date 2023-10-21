b661e7dd33cbaa46bf7ef7fff82bed77a1ccdce4
jeondui <jeondui@gmail.com>
2023-10-21 22:25:51 +0900

    feat(OutputView): 힌트 메시지 출력 기능

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -2,15 +2,27 @@ package baseball.view;
 
 public class OutputView {
 
-    public void printStartGameMessage() {
+    public static void printStartGameMessage() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    public void printEndGameMessage() {
+    public static void printEndGameMessage() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
-    public void printHintMessage() {
-        System.out.println("힌트입니다.");
+    public static void printStrikeHintMessage(int strikeCount) {
+        System.out.println(strikeCount + " 스트라이크");
+    }
+
+    public static void printBallHintMessage(int ballCount) {
+        System.out.println(ballCount + " 볼");
+    }
+
+    public static void printStrikeAndBallMessage(int strikeCount, int ballCount) {
+        System.out.println(strikeCount + " 스트라이크 " + ballCount + " 볼");
+    }
+
+    public static void printNothingMessage() {
+        System.out.println("낫싱");
     }
 }
