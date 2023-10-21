2d33513b0d98d8677090a86cd6458783c018945e
김명준 <86913355+mjj111@users.noreply.github.com>
2023-10-21 18:08:33 +0900

    style(InputView): 문자열 상수를 통해 긴 한국어를 간단한 영어로 치환
    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요"와 같이 긴 문장에 대해 INSERT_CONTINUE와 같은 문자열 상수를 통해 길이를 줄여 가독성을 높입니다.

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -3,14 +3,16 @@ package baseball.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private final static String INSERT_ANSWER = "숫자를 입력해주세요 : ";
+    private final static String INSERT_CONTINUE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public String readPlayersAnswer() {
-        System.out.print("숫자를 입력해주세요 : ");
+        System.out.print(INSERT_ANSWER);
         return Console.readLine();
     }
 
     public String readPlayersContinue() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println(INSERT_CONTINUE);
         return Console.readLine();
     }
 }
