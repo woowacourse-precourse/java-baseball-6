c52ef169a82c98178cce67896643b0d65edee6a0
yubin <dbqls2821@gmail.com>
2023-10-21 15:02:33 +0900

    feat: 입력값으로 들어오는 숫자를 Digits 인스턴스로 만들 생성자 추가

--- src/main/java/baseball/Digits.java
+++ src/main/java/baseball/Digits.java
@@ -12,4 +12,14 @@ final class Digits {
             if (!digits.contains(randomDigit)) digits.add(randomDigit);
         }
     }
+
+    //TODO : 정적팩터리로?
+    Digits(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            Digit digit = new Digit(input.charAt(i));
+            if(digits.contains(digit)) throw new IllegalArgumentException();
+            digits.add(digit);
+        }
+    }
+
 }
