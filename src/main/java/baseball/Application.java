701580fadc76a28881291272cf7b97e24aebb887
Kim Mi Ju <76895039+miju0515@users.noreply.github.com>
2023-10-21 23:46:18 +0900

    feat: 게임 종료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -27,6 +27,9 @@ public class Application {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 int reset = Integer.valueOf(readLine());
+                if(reset == 2){
+                    break;
+                }
             }
         }
     }
