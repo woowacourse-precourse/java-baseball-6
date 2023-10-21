411c5a2dc77e5122733d5d737cd3a3d6312b5eb2
U-keun Song <sukeun319@gmail.com>
2023-10-21 12:34:07 +0900

    Feat : judge 메서드 추가
    사용자가 입력한 숫자가 정수 배열로 주어질 때, targetNumber와 비교해서 볼, 스트라이크의 개수를 반환하는 메서드를 구현했다. 메서드 안에서 결과를 출력할 수 있게 구현해 두었다.

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -2,11 +2,36 @@ package baseball;
 
 public class Referee {
     private TargetNumber targetNumber;
-    private Referee() { }
 
-    public static Referee enter() { return new Referee(); }
+    private Referee() {
+    }
+
+    public static Referee enter() {
+        return new Referee();
+    }
 
     public void playBall(int a, int b, int c) {
         targetNumber = TargetNumber.generate(a, b, c);
+    }
+
+    public int[] judge(int[] numbers) {
+        int[] judgment = new int[2];
+        for (int i = 0; i < 3; i++) {
+            boolean[] check = targetNumber.has(numbers[i], i);
+            if (check[0]) judgment[0]++;
+            if (check[1]) judgment[1]++;
+        }
+
+        StringBuilder print = new StringBuilder();
+        if (judgment[1] == 3) {
+            print.append("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        } else if (judgment[0] == 0) {
+            print.append("낫싱");
+        } else {
+            print.append(String.format("%d볼 %d스트라이크", judgment[0], judgment[1]));
+        }
+        System.out.println(print);
+
+        return judgment;
     }
 }
