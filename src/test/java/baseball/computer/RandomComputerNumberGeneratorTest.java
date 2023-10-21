7e93de5f50dea514ee072145548da7ba2edceb5e
Sully <2dcoder@naver.com>
2023-10-21 22:55:36 +0900

    refactor: reformat code

--- src/test/java/baseball/computer/RandomComputerNumberGeneratorTest.java
+++ src/test/java/baseball/computer/RandomComputerNumberGeneratorTest.java
@@ -1,6 +1,6 @@
 package baseball.computer;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
