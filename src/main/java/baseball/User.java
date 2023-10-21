24f4a8555508b659b58c8abbd2adc293d03771ce
pjm2476 <pjm2476@gmail.com>
2023-10-21 22:50:19 +0900

    feat: 정상적인 입력이 들어온 경우 입력한 숫자 저장

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,13 +1,29 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class User {
 
     private static final String ENTER_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
 
+    private static final int NUMBER_LEN = 3;
+
+    public List<Integer> userAnswer = new ArrayList<Integer>();
+
     public void enterNumber() {
-        System.out.println(ENTER_NUMBER_MESSAGE);
+        System.out.print(ENTER_NUMBER_MESSAGE);
         String number = Console.readLine();
+        setUserAnswer(number);
+
     }
+
+    public void setUserAnswer(String number) {
+        for (int i = 0; i < NUMBER_LEN; i++) {
+            userAnswer.add(Character.getNumericValue(number.charAt(i)));
+        }
+    }
+
+
 }
