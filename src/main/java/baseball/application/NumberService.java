18a85ad4cfd90f84eec0a764c1e5e4bc69470fd1
jae woo Jung <awfjol2008@naver.com>
2023-10-21 23:29:59 +0900

    feat: 숫자 비교에 따라 힌트를 결정하는 메소드 구현

--- src/main/java/baseball/application/NumberService.java
+++ src/main/java/baseball/application/NumberService.java
@@ -1,9 +1,11 @@
 package baseball.application;
 
+import baseball.domain.Hint;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 public class NumberService {
@@ -15,5 +17,16 @@ public class NumberService {
         }
 
         return new ArrayList<>(numberSet);
+    }
+
+    private static void setHint(List<Integer> computerNumber, List<Integer> playerNumber, Hint hint,
+                                int i, int j) {
+        if (Objects.equals(playerNumber.get(i), computerNumber.get(j))) {
+            if (i == j) {
+                hint.increaseStrike();
+            } else {
+                hint.increaseBall();
+            }
+        }
     }
 }
