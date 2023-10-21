62195fc426705ed4d36588a22b21366885eec857
tam31 <wxogud3377@naver.com>
2023-10-22 04:24:32 +0900

    Feat(Referee): Referee의 스트라이크와 볼의 개수를 구하기 위해 기본 뼈대 구현

--- src/main/java/baseball/model/Referee.java
+++ src/main/java/baseball/model/Referee.java
@@ -1,4 +1,18 @@
 package baseball.model;
 
+import java.util.List;
+
 public class Referee {
+
+    private Integer strike;
+    private Integer ball;
+
+    public void duplicateNumbers(List<Integer> computerNumber, List<Integer> playerNumber){
+        int strikeCount = 0;
+        int ballCount = 0;
+        for(int i=0; i<3; i++){
+            strikeCount += duplicateStrike(computerNumber.get(i), playerNumber.get(i));
+            ballCount += duplicateBall(computerNumber.get(i), playerNumber.get(i), playerNumber);
+        }
+    }
 }
