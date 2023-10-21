972cf79d27b2950f8bf47b6165f8567f246c1612
Jung Yunho <a01049048063@gmail.com>
2023-10-21 14:34:22 +0900

    refactor(Validation): InputView에서 validation 분리

--- src/main/java/baseball/Inputview.java
+++ src/main/java/baseball/Inputview.java
@@ -1,38 +1,31 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Inputview {
 
-    public static void gameStart() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+    private static final String START_GAME = "숫자 야구 게임을 시작합니다.";
+    private static final String PLEASE_INPUT_NUMBER = "숫자를 입력해주세요 : ";
+    private static final String RESTART_GAME = "게임을 새로 시작할면 1, 종료하려면 2를 입력하세요.";
+
+    public static void startGame() {
+        System.out.println(START_GAME);
     }
 
-    public static List<Integer> input() {
-        System.out.print("숫자를 입력해주세요: ");
-        String inputString = Console.readLine();
+    public static String inputNumber() {
+        System.out.print(PLEASE_INPUT_NUMBER);
+        String inputNumber = Console.readLine();
 
-        if (inputString.length() != 3) {
-            throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.");
-        }
+        Validation.checkLengthThree(inputNumber);
+        Validation.checkStringRegex(inputNumber);
 
-        List<Integer> numberList = new ArrayList<>();
-
-        for (int i = 0; i < inputString.length(); i++) {
-            char charDigit = inputString.charAt(i);
-            int intDigit = Character.getNumericValue(charDigit);
-            numberList.add(intDigit);
-        }
-
-        return numberList;
+        return inputNumber;
     }
 
     public static String reStart() {
-        System.out.println("게임을 새로 시작할면 1, 종료하려면 2를 입력하세요.");
-        String inputString = Console.readLine();
-        return inputString;
-    }
+        System.out.println(RESTART_GAME);
+        String inputOption = Console.readLine();
 
+        return inputOption;
+    }
 }
---
+++ src/main/java/baseball/Validation.java
@@ -0,0 +1,21 @@
+package baseball;
+
+public class Validation {
+
+    private static final int THREE_DIGIT_NUMBER = 3;
+    private static final String STRING_REGEX = "\\d";
+    private static final String OUT_OF_NUMBER_RANGE = "[ERROR] 3자리 숫자를 입력하셔야 합니다.";
+    private static final String NOT_AVAILABLE_STRING ="[ERROR] 숫자만 입력해주세요.";
+
+    public static void checkLengthThree(String number) {
+        if (number.length() != THREE_DIGIT_NUMBER) {
+            throw new IllegalArgumentException(OUT_OF_NUMBER_RANGE);
+        }
+    }
+
+    public static void checkStringRegex(String number) {
+        if (!number.matches(STRING_REGEX)) {
+            throw new IllegalArgumentException(NOT_AVAILABLE_STRING);
+        }
+    }
+}
