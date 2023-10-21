d509ce1af36183a6aaba0201e7a5657784b953e3
inpink <inpink@kakao.com>
2023-10-21 17:26:27 +0900

    feat: 사용자는 서로 다른 숫자로 이루어진 3자리 숫자 1개를 입력
    Console.readLine() 이용

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,17 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
         //시작 문구
         System.out.println("숫자 야구 게임을 시작합니다.");
+
+        //사용자는 서로 다른 숫자로 이루어진 3자리 숫자 1개를 입력　
+        System.out.print("숫자를 입력해주세요 : ");
+        String inputNum = Console.readLine();
 
 
     }
