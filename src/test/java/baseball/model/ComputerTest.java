4e426d13df41a22377b6c74e297cde6b9a433495
IToriginal <gitshineit@gmail.com>
2023-10-22 02:35:36 +0900

    :white_check_mark: [test] #2 랜덤으로 초기화된 숫자는 중복된 값을 가지지 않고 유니크하다.

--- src/test/java/baseball/model/ComputerTest.java
+++ src/test/java/baseball/model/ComputerTest.java
@@ -6,11 +6,13 @@ import static baseball.util.NumberConstants.MIN_RANGE_NUMBER;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ComputerTest {
+
     private Computer computer;
 
     @BeforeEach
@@ -47,4 +49,25 @@ class ComputerTest {
         }
     }
 
+    @DisplayName("랜덤으로 초기화된 숫자는 중복된 값을 가지지 않고 유니크하다.")
+    @Test
+    public void checkUniquNumber() {
+        //when
+        computer.generateRandomNumber();
+        List<Integer> randomNumberList = computer.getRandomNumberList();
+
+        //then
+        assertTrue(isUniqueNumber(randomNumberList));
+    }
+
+    private boolean isUniqueNumber(List<Integer> randomNumberList) {
+        for (int i = 0; i < randomNumberList.size(); i++) {
+            for (int j = i + 1; j < randomNumberList.size(); j++) {
+                if (randomNumberList.get(i).equals(randomNumberList.get(j))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
