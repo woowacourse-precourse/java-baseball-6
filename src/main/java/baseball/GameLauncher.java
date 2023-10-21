9222423a339403cdb304433ae2b6ec2a3b4f9180
Hanjaemo <hjm2530@gmail.com>
2023-10-21 22:12:00 +0900

    feat(Application, GameLauncher): 게임 실행 기능 구현

---
+++ src/main/java/baseball/GameLauncher.java
@@ -0,0 +1,52 @@
+package baseball;
+
+import static baseball.utils.GameStatusCommand.QUIT;
+
+import baseball.domain.BallCounter;
+import baseball.domain.ComputerNumbers;
+import baseball.domain.ComputerNumbersGenerator;
+import baseball.domain.NumberGenerator;
+import baseball.domain.PlayerNumbers;
+import baseball.utils.GameStatusCommand;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+public class GameLauncher {
+
+    private static final int DIGIT_NUMBER = 3;
+
+    private final NumberGenerator numberGenerator;
+
+    public GameLauncher(NumberGenerator numberGenerator) {
+        this.numberGenerator = numberGenerator;
+    }
+
+    public void launch() {
+        OutputView.printBeginningMessage();
+        while (true) {
+            playBaseball();
+            GameStatusCommand gameStatusCommand = GameStatusCommand.of(InputView.readNumberOfGameStatusCommand());
+            if (gameStatusCommand == QUIT) {
+                return;
+            }
+        }
+    }
+
+    private void playBaseball() {
+        ComputerNumbers computerNumbers = ComputerNumbersGenerator.generate(numberGenerator, DIGIT_NUMBER);
+        repeatBallCount(computerNumbers);
+        OutputView.printEndingMessageForAllStrike(DIGIT_NUMBER);
+    }
+
+    private void repeatBallCount(ComputerNumbers computerNumbers) {
+        while (true) {
+            BallCounter.reset();
+            PlayerNumbers playerNumbers = new PlayerNumbers(InputView.readNumbers(), DIGIT_NUMBER);
+            computerNumbers.checkPlayerNumbers(playerNumbers, DIGIT_NUMBER);
+            if (BallCounter.isAllStrike(DIGIT_NUMBER)) {
+                return;
+            }
+            OutputView.printBallCountResult(BallCounter.getCountOfBall(), BallCounter.getCountOfStrike());
+        }
+    }
+}
