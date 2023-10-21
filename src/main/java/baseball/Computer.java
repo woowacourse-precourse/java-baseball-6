fcf95c31a20906f43e06453bca295c5a45c2c4df
wonjun <wonjon0930@daum.net>
2023-10-21 21:31:44 +0900

    feat: add function to calculate strike/ball count using user input

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -3,7 +3,9 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Computer {
     private List<Integer> randomNumbers;
@@ -18,6 +20,23 @@ public class Computer {
     private void addNumberInList(List<Integer> randomNumbers, int value) {
         if(!randomNumbers.contains(value)) {
             randomNumbers.add(value);
+        }
+    }
+    public Map<String, Integer> getBaseballResult(List<Integer> userInputNumbers){
+        Map<String, Integer> baseballResult = new HashMap<>();
+        baseballResult.put("strike", 0);
+        baseballResult.put("ball", 0);
+        for(int i = 0; i < userInputNumbers.size(); i++){
+            checkIfInputIsStrikeOrBall(userInputNumbers, i, baseballResult);
+        }
+        return baseballResult;
+    }
+    private void checkIfInputIsStrikeOrBall(List<Integer> userInputNumbers, int digit, Map<String, Integer> baseballResult){
+        int userInput = userInputNumbers.get(digit);
+        if(userInput == randomNumbers.get(digit)) {
+            baseballResult.put("strike", baseballResult.get("strike") + 1);
+        } else if(randomNumbers.contains(userInput)){
+            baseballResult.put("ball", baseballResult.get("ball") + 1);
         }
     }
 }
