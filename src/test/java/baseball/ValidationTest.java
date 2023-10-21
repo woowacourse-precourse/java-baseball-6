c6a74205a66e9428e2baaa5256ccde64b87de0f0
Donggeon Moon <nullsafer@naver.com>
2023-10-21 15:48:26 +0900

    refactor: 람다식으로 변경

--- src/test/java/baseball/ValidationTest.java
+++ src/test/java/baseball/ValidationTest.java
@@ -13,36 +13,28 @@ public class ValidationTest {
     void throwExceptionIfInputIsAlphabet() {
         String alphabetInput = "abc";
         command(alphabetInput);
-        assertThatThrownBy(() -> {
-            Application.main(new String[]{});
-        }).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Application.main(new String[]{})).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void throwExceptionIfInputIsKorean() {
         final String koreanInput = "가나다";
         command(koreanInput);
-        assertThatThrownBy(() -> {
-            Application.main(new String[]{});
-        }).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Application.main(new String[]{})).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void throwExceptionIfInputIsShorterThanThree() {
         final String shortNumbers = "23";
         command(shortNumbers);
-        assertThatThrownBy(() -> {
-            Application.main(new String[]{});
-        }).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Application.main(new String[]{})).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void throwExceptionIfInputIsLongerThanThree() {
         final String longNumbers = "23553";
         command(longNumbers);
-        assertThatThrownBy(() -> {
-            Application.main(new String[]{});
-        }).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Application.main(new String[]{})).isInstanceOf(IllegalArgumentException.class);
     }
 
     private static void command(String... args) {
