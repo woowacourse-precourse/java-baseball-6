24368dd2760d36f4c71e5aee61db80cdaeaff883
Dongwoongkim <rlaehddnd0422@naver.com>
2023-10-22 00:04:49 +0900

    feat : 숫자게임의 정답을 생성하는 기능 구현

---
+++ src/main/java/baseball/model/NumberGenerator.java
@@ -0,0 +1,27 @@
+package baseball.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberGenerator {
+    private static final Integer BALL_SIZE = 3;
+    private final static Integer FIRST_NUMBER = 1;
+    private final static Integer LAST_NUMBER = 9;
+
+    public List<Integer> getGeneratedNumbers() {
+        List<Integer> answer = new ArrayList<>();
+        while (answer.size() < BALL_SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(FIRST_NUMBER, LAST_NUMBER);
+            addIfNotInList(answer, randomNumber);
+        }
+        return answer;
+    }
+
+    private void addIfNotInList(List<Integer> answer, int randomNumber) {
+        if (!answer.contains(randomNumber)) {
+            answer.add(randomNumber);
+        }
+    }
+
+}
