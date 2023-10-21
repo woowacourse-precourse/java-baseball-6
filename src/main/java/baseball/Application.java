1bc6e4e02594bc5e84f71e87dc281ebd2adddd9a
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-10-21 17:09:42 +0900

    feat(view) : OutputView에게임 시작 메시지 출력 구현 - docs : View -> OutputView -> 게임 시작 메시지 항목 체크

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,10 +2,13 @@ package baseball;
 
 import baseball.util.MessageUtil;
 import baseball.view.InputView;
+import baseball.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        outputView.printGameStartMessage();
         inputView.readBaseballNumber();
         inputView.readRetryCommand();
     }
---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,9 @@
+package baseball.view;
+
+import baseball.util.MessageUtil;
+
+public class OutputView {
+    public void printGameStartMessage(){
+        MessageUtil.GAME_START_MESSAGE.messagePrintln();
+    }
+}
