9fe979da86189f718fd4aff663ab86747ba6a924
김승진 <ohksj77@gmail.com>
2023-10-21 22:09:06 +0900

    feat(Number): 같은 값을 갖고 있는지 확인 기능 구현

--- src/main/java/baseball/domain/Number.java
+++ src/main/java/baseball/domain/Number.java
@@ -1,5 +1,7 @@
 package baseball.domain;
 
+import java.util.Objects;
+
 public final class Number {
 
     private static final int MIN_VALUE = 1;
@@ -19,5 +21,18 @@ public final class Number {
 
     private boolean isNotProperValue(final Integer value) {
         return value < MIN_VALUE || value > MAX_VALUE;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Number number = (Number) o;
+        return Objects.equals(value, number.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
