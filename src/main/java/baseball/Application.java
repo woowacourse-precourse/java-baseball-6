b4e7daaeb406e5b978d74a25d1452fffb42f6f01
cookingTorch <jhw7181@naver.com>
2023-10-21 22:03:35 +0900

    make generateRandomAnswer

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    private static int[] generateRandomAnswer() {
+        int[] answer;
+
+        answer = new int[3];
+        do {
+            for (int i = 0; i < 2; i++) {
+                answer[i] = Randoms.pickNumberInRange(1, 9);
+            }
+        } while(!duplicateNumber(answer));
+        return answer;
+    }
+
+    private static boolean duplicateNumber(int[] number) {
+        return ((number[0] == number[1])
+                || (number[1] == number[2])
+                || (number[2] == number[0]));
     }
 }
