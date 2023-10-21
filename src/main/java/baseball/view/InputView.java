019253db1735fe253e46aaf7a7ef338327857ea2
jeondui <jeondui@gmail.com>
2023-10-21 20:37:48 +0900

    refactor(InputNumber): 플레이어 입력 메서드명 가독성 있도록 수정

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -4,12 +4,12 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public String getNumber() {
+    public static String inputNumber() {
         System.out.print("숫자를 입력해주세요 : ");
         return Console.readLine();
     }
 
-    public String getRetryNumber() {
+    public static String getRetryNumber() {
         System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         return Console.readLine();
     }
