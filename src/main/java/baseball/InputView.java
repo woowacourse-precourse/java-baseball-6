23d7a930e75ecc930b1ae0ebce6f86e460ffef4f
freekyr7529 <freekyr7529@gmail.com>
2023-10-21 21:41:21 +0900

    feat: 게임 재시작 여부를 묻는 메세지 출력 메서드 구현
    게임 재시작 여부를 묻는 askRestartMsg 구현

--- src/main/java/baseball/InputView.java
+++ src/main/java/baseball/InputView.java
@@ -14,6 +14,10 @@ public class InputView {
         System.out.print("숫자를 입력해주세요 : ");
     }
 
+    public void askRestartMsg() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
     public List<Integer> inputByConsole() {
         String input = Console.readLine();
 
