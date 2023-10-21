36b2aaab13c263280bb424101363d83a53697aa8
Firedrago95 <yonghwa95@naver.com>
2023-10-21 12:52:17 +0900

    refactor 판정기능중 같은 숫자와 같은 위치의 같은 숫자 구하는 기능 Numbers 책임으로 변경

--- src/main/java/baseball/Model/Numbers.java
+++ src/main/java/baseball/Model/Numbers.java
@@ -4,10 +4,35 @@ import java.util.List;
 
 public class Numbers {
 
-    private List<Integer> numbers;
+    private List<Integer> balls;
 
     public Numbers (List<Integer> numbers) {
-        this.numbers = numbers;
+        this.balls = numbers;
     }
 
+    public int countSameNumber(Numbers computerNumbers) {
+        int count = 0;
+        List<Integer> computerBalls = computerNumbers.getBalls();
+        for (int ball : balls) {
+            if (computerBalls.contains(ball)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int countSameIndexAndNumber(Numbers computerNumbers) {
+        int count = 0;
+        List<Integer> computerBalls = computerNumbers.getBalls();
+        for (int index = 0; index <= 2; index++) {
+            if (balls.get(index) == computerBalls.get(index)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public List<Integer> getBalls() {
+        return balls;
+    }
 }
--- src/main/java/baseball/Model/User.java
+++ src/main/java/baseball/Model/User.java
@@ -8,28 +8,12 @@ public class User {
     public static final int LAST_INDEX = 2;
     private Numbers numbers;
 
-    public User(Numbers numbers) {
-        this.numbers = numbers;
+    public User(List<Integer> convertedInput) {
+        this.numbers = new Numbers(convertedInput);
     }
 
     public Numbers getNumbers() {
         return numbers;
     }
 
-    public int compareAndCount(Computer computer) {
-        Numbers computerNumbers = computer.getNumbers();
-        return numbers.
-    }
-
-
-    public int countSameIndexAndNumber(Computer computer) {
-        int strike = 0;
-        List<Integer> computerNumbers = computer.getNumbers();
-        for (int index = FIRST_INDEX; index <= LAST_INDEX; index++) {
-            if (numbers.get(index) == computerNumbers.get(index)) {
-                strike++;
-            }
-        }
-        return strike;
-    }
 }
