c3020772232190219302b4d844ee8445711fc6d7
suhyun9764 <suhyun9764@naver.com>
2023-10-21 21:30:13 +0900

    feat:구조 새로 설계

---
+++ src/main/java/baseball/controller/controller.java
@@ -0,0 +1,19 @@
+package baseball.controller;
+
+public class controller {
+
+    public void run(){  // 게임 실행
+        // 새로운 게임 생성(컴퓨터 랜덤 값 생성 및 저장) - settingGame();
+
+        /* 게임 시작 - startGame();
+        // 사용자 입력 받기
+        // 두 값 비교
+        //결과 출력
+        */
+
+        //if(strike==3)
+        //selectContinueOrNot();
+    }
+
+
+}
