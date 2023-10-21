b252a285572a2e356fe41d7588b7e99b1b8f31b2
seokmyungham <hellotjraud@gmail.com>
2023-10-22 02:50:54 +0900

    feat(GameView): 클래스 생성 및 startGame 메서드 추가
    startGame: 게임 시작 메시지를 출력하는 메서드 입니다.

--- src/main/java/baseball/view/GameView.java
+++ src/main/java/baseball/view/GameView.java
@@ -1,4 +1,9 @@
 package baseball.view;
 
+import static baseball.domain.Constants.START_GAME_MESSAGE;
+
 public class GameView {
+    public void startGame() {
+        System.out.println(START_GAME_MESSAGE);
+    }
 }
