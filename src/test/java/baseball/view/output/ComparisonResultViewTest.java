e1f698ba90bc13d2568935924e8d64d3033ab0e6
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-10-21 12:55:45 +0900

    test (ComparisonResultView) : 테스트 코드 중 중복되는 코드 리팩토링 실시
    - 같은 기능을 다른 케이스로만 하는 테스트 코드들을 @ParameterizedTest를 이용해 인자를 받아 하나의 메서드로 구현
    - 중복 코드가 줄어 코드 관리가 용이해짐

--- src/test/java/baseball/view/output/ComparisonResultViewTest.java
+++ src/test/java/baseball/view/output/ComparisonResultViewTest.java
@@ -4,52 +4,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import baseball.dto.ComparisonResult;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class ComparisonResultViewTest {
 
-    private static final ComparisonResultView VIEW = new ComparisonResultView();
+    @ParameterizedTest(name = "strike : {0}, ball : {1}, except : {2}")
+    @CsvSource({"2,0,2스트라이크", "0,3,3볼", "1,2,2볼 1스트라이크", "0,0,낫싱"})
+    @DisplayName("비교 결과를 출력 메세지로 바꿀 수 있다")
+    void changeToOutputMessageTest(int strike, int ball, String expected) {
+        ComparisonResult result = new ComparisonResult(strike, ball, false);
 
-    @Test
-    @DisplayName("스트라이크만 있을 때 결과를 출력 메세지로 바꿀 수 있다")
-    void changeToOutputMessageTest_whenOnlyStrike() {
-        ComparisonResult result = new ComparisonResult(2, 0, false);
-        String expected = "2스트라이크";
-
-        String actual = VIEW.changeToOutputMessage(result);
-
-        assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
-    @DisplayName("볼만 있을 때 결과를 출력 메세지로 바꿀 수 있다")
-    void changeToOutputMessageTest_whenOnlyBall() {
-        ComparisonResult result = new ComparisonResult(0, 3, false);
-        String expected = "3볼";
-
-        String actual = VIEW.changeToOutputMessage(result);
-
-        assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
-    @DisplayName("볼, 스트라이크 모두 있을 때 결과를 출력 메세지로 바꿀 수 있다")
-    void changeToOutputMessageTest_whenStrikeAndBall() {
-        ComparisonResult result = new ComparisonResult(2, 1, false);
-        String expected = "1볼 2스트라이크";
-
-        String actual = VIEW.changeToOutputMessage(result);
-
-        assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
-    @DisplayName("전부 일치하지 않을 때 결과를 출력 메세지로 바꿀 수 있다")
-    void changeToOutputMessageTest_whenNothing() {
-        ComparisonResult result = new ComparisonResult(0, 0, false);
-        String expected = "낫싱";
-
-        String actual = VIEW.changeToOutputMessage(result);
+        String actual = new ComparisonResultView().changeToOutputMessage(result);
 
         assertThat(actual).isEqualTo(expected);
     }
