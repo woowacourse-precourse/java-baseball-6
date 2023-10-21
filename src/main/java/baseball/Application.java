8a26f0c988e30617e6f637bed90fb2a522cc73ad
육예진 <yngbao@yug-yejin-ui-MacBookAir.local>
2023-10-21 15:03:23 +0900

    1~9 중 서로 다른 숫자 3개로 배열 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,25 @@
 package baseball;
 
-public class Application {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.*;
+
+public class Application 
+{
+    public static void main(String[] args) 
+    {
         
     	System.out.println("숫자 야구 게임을 시작합니다.");
     	
+    	List<Integer> computer = new ArrayList<>();
+    	while (computer.size() < 3) 
+    	{
+    	    int randomNumber = Randoms.pickNumberInRange(1, 9);
+    	    if (!computer.contains(randomNumber)) 
+    	    {
+    	        computer.add(randomNumber);
+    	    }
+    	}
     }
 }
