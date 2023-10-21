5d7d0d24556390df5c507540e16f021482a892e6
dltjdgh0428 <dltjdgh0428@naver.com>
2023-10-21 14:46:15 +0900

    feat: View 유저입력 메소드 추가

--- src/main/java/baseball/View/GameView.java
+++ src/main/java/baseball/View/GameView.java
@@ -29,13 +29,11 @@ public class GameView {
 
     public String printGameOverMessage() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-
         return Console.readLine();
     }
 
     public String printInputMessage() {
         System.out.println("숫자를 입력해주세요 : ");
-
         return Console.readLine();
     }
 
