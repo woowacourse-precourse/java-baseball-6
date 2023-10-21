26cd03b92d3497982160118c36407d6ba37b3518
gumgu <bienlee@naver.com>
2023-10-21 18:18:32 +0900

    [구현] 출력 문구 추가

--- src/main/java/baseball/PrintManager.java
+++ src/main/java/baseball/PrintManager.java
@@ -35,4 +35,7 @@ public class PrintManager {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
+    public void printRestart() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
