959ba44c67c075409d2e5ce5c934647de5935ebf
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-10-22 03:35:51 +0900

    fix: if condition of baseball referee scoring method

--- src/main/java/baseball/BaseballReferee.java
+++ src/main/java/baseball/BaseballReferee.java
@@ -8,7 +8,7 @@ public class BaseballReferee {
         int strike = 0;
 
         for(int i = 0; i < expect.size(); i++){
-            if(expect.get(i) == expect.get(i)) strike++;
+            if(expect.get(i) == actual.get(i)) strike++;
             else if(expect.contains(actual.get(i))) ball++;
         }
 
