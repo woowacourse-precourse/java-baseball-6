f43b5246a6ba5a4c6731a78a271323947c927bb8
eunseob <seobseob0810@naver.com>
2023-10-21 12:54:59 +0900

    feat: 컴퓨터 랜덤 숫자 생성 클래스 추가

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,22 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    // 정답 숫자 설정
+    public List<Integer> createAnswerNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
+
+}
