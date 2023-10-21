6dce63eb119198137653205c126e1344ab0ef160
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-21 22:19:36 +0900

    refactor(Util):정규식을 사용한 검증으로 수정

--- src/main/java/baseball/util/Util.java
+++ src/main/java/baseball/util/Util.java
@@ -4,47 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Util {
+    public static final String NATURAL_NUMBER_REGULAR_EXPRESSION = "^[0-9]*$";
 
     //완성시 모든 메서드 테스트
-
-
-    //try-catch 필요없나 테스트 해보기
-    public static void validateStringIsNaturalNumber(String string, String exceptionMessage) {
-        int numberByString = Integer.parseInt(string);
-        if (numberByString <= 0) {
-            throw new IllegalArgumentException(exceptionMessage);
-        }
-    }
-
-    public static void validateStringHasNot(String notContainsString, String string, String exceptionMessage) {
-        if (string.contains(notContainsString)) {
-            throw new IllegalArgumentException(exceptionMessage);
-        }
-    }
-
-    public static void validateStringLength(int length, String string, String exceptionMessage) {
-        if (string.length() != length) {
-            throw new IllegalArgumentException(exceptionMessage);
-        }
-    }
-
-    public static void validateStringAllDifferent(String string, String exceptionMessage) {
-        for (int i = 0; i < string.length(); i++) {
-            String stringByIdx = String.valueOf(string.charAt(i));
-            String stringRemoveByIdx = string.substring(0, i) + string.substring(i + 1);
-            if (stringRemoveByIdx.contains(stringByIdx)) {
-                throw new IllegalArgumentException(exceptionMessage);
-            }
-
-        }
-    }
-
-    public static String reverseString(String string) {
-        StringBuilder stringBuffer = new StringBuilder(string);
-        return stringBuffer.reverse().toString();
-    }
-
-    //(int)를 써도 괜찮을까 테스트해보기
     public static List<Integer> stringToIntegerList(String string) {
         List<Integer> integerList = new ArrayList<>();
         for (int i = 0; i < string.length(); i++) {
@@ -89,11 +51,5 @@ public class Util {
             }
         }
         return count;
-    }
-
-    public static void validateStringIsOneOrTwo(String string) {
-        if (!string.equals("1") && !string.equals("2")) {
-            throw new IllegalArgumentException("1, 2 중 하나를 입력해주세요");
-        }
     }
 }
