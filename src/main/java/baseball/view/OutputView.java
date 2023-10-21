ef63aa52975cbe0872e004babbf15c1609206b7d
kimtaesoo <kimtaesoo7@naver.com>
2023-10-22 00:53:52 +0900

    feat: OutputView 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,4 +1,24 @@
 package baseball.view;
 
 public class OutputView {
+
+    private static final String START = "야구 게임을 시작합니다.";
+    private static final String END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public void printStart() {
+        System.out.println(START);
+    }
+
+    public void printResult(final String result) {
+        System.out.println(result);
+    }
+
+    public void printEnd() {
+        System.out.println(END);
+    }
+
+    public void printRestart() {
+        System.out.println(RESTART);
+    }
 }
