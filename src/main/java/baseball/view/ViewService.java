4557577f437ac9d2c3b4da131c3135d6f9b03738
jae woo Jung <awfjol2008@naver.com>
2023-10-21 20:15:16 +0900

    feat: 사용자 숫자 입력 메소드 구현

--- src/main/java/baseball/view/ViewService.java
+++ src/main/java/baseball/view/ViewService.java
@@ -1,8 +1,14 @@
 package baseball.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class ViewService {
 
     public void printReadMessage() {
         System.out.print("숫자를 입력해주세요 : ");
+    }
+
+    public String readNumber() {
+        return readLine();
     }
 }
