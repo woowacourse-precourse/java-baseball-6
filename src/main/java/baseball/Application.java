f5f28769896311775d120bc1e2546c02b903df04
KIVSH <kihongkim3@hotmail.com>
2023-10-21 10:58:30 +0900

    구현할 메소드 및 클래스 선언 및 정의

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,47 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    public static class Result {
+        String message;
+        boolean isContinue;
+
+        Result(String message, boolean isContinue) {
+            this.message = message;
+            this.isContinue = isContinue;
+        }
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public static void repeatGame() {
+
+    }
+
+    public static void repeatRound(List<Integer> computer) {
+
+    }
+
+    public static List<Integer> generateRandomNumber() {
+        List<Integer> computer = new ArrayList<>();
+        return computer;
+    }
+
+    public static Result compareTwoNumber(List<Integer> computer, List<Integer> user) {
+        Result result = null;
+        return result;
+    }
+
+    public static List<Integer> input(int inputLength) {
+        List<Integer> user = new ArrayList<>();
+        return user;
+    }
+
+    public static void output(String outputMessage) {
+
     }
 }
