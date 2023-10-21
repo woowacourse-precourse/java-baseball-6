3a78bcddb492ab8f20322b90bc24188dcd9592d1
dukcode <duk9741@gmail.com>
2023-10-21 15:11:31 +0900

    refactor: while문 안의 break문 return으로 대체

--- src/main/java/baseball/game/turn/TurnBasedGame.java
+++ src/main/java/baseball/game/turn/TurnBasedGame.java
@@ -24,7 +24,7 @@ public class TurnBasedGame implements Game {
             turnBasedGameView.printResult(turnResult);
             if (turnResult.isGameCleared()) {
                 turnBasedGameView.printClear();
-                break;
+                return;
             }
         }
     }
