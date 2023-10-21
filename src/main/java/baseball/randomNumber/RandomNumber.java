064350a9217f0d8bc443457484a11f269edd83f9
chaeshee0908 <chaeshee0908@naver.com>
2023-10-22 01:54:04 +0900

    [fix] 랜덤 숫자 반환형 리스트로 변경

--- src/main/java/baseball/randomNumber/RandomNumber.java
+++ src/main/java/baseball/randomNumber/RandomNumber.java
@@ -1,15 +1,13 @@
-package baseball.RandomNumber;
+package baseball.randomNumber;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.util.stream.Collectors.joining;
-
 public class RandomNumber {
 
-    public String createRandomNumber() {
+    public static List<Integer> createRandomNumber() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -17,9 +15,6 @@ public class RandomNumber {
                 computer.add(randomNumber);
             }
         }
-        String randomNumber = computer.stream()
-                .map(Object::toString)
-                .collect(joining(""));
-        return randomNumber;
+        return computer;
     }
 }
