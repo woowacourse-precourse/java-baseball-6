1d4229fcfccb4c015e0689b28759c577a094bc4e
lee jihyeon <lejinhy@gmail.com>
2023-10-21 16:58:22 +0900

    Feat: 숫자 범위, 중복, 개수를 체크하는 예외처리 메소드 완성

--- src/main/java/baseball/Exception.java
+++ src/main/java/baseball/Exception.java
@@ -1,7 +1,9 @@
 package baseball;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * 예외처리 클래스
@@ -13,9 +15,10 @@ public class Exception {
      * 예외가 발생하지 않았다면 문자열을 int list로 반환해줌
      */
     public List<Integer> isValid(String number) {
-
-            inRange(number);
-            numberList = convertStringToIntList(number);
+        inRange(number);
+        numberList = convertStringToIntList(number);
+        checkDuplicate(numberList);
+        checkCount(numberList);
 
         return numberList;
     }
@@ -25,20 +28,25 @@ public class Exception {
      */
     private void inRange(String number) {
         if (!number.matches("^[1-9]*$")) {
-            throw new IllegalArgumentException("[ERROR] 1~9 사이의 숫자를 입력해주세요.");
+            throw new IllegalArgumentException("[ERROR] 1~9 사이의 숫자를 입력해 주세요.");
         }
     }
     /*
      * 숫자 중 중복이 없는지 확인
      */
-    private void checkDuplicate() {
-
+    private void checkDuplicate(List<Integer> numbers) {
+        Set<Integer> noDuplicate = new HashSet<>(numbers);
+        if (numbers.size() != noDuplicate.size()) {
+            throw new IllegalArgumentException("[ERROR] 중복이 아닌 숫자를 입력해 주세요.");
+        }
     }
     /*
      * 숫자 개수가 3개인지 확인
      */
-    private void checkCount() {
-
+    private void checkCount(List<Integer> numbers) {
+        if (numbers.size() != 3) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 세개 입력해 주세요.");
+        }
     }
     /*
      * 문자열 입력을 int list로 변환해주는 메소드
