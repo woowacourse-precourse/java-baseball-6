3121eec2f8eae1e7da6090ce454a5647e31bcde6
Jae-Young98 <ygy0102@gmail.com>
2023-10-21 18:49:00 +0900

    feat(User): 사용자의 재시작/종료를 입력 받는 기능 추가

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,10 +1,20 @@
 package baseball;
 
+import baseball.logic.InputValidator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class User {
 
+    private final InputValidator validator = new InputValidator();
+
     public String inputNumbers() {
         return Console.readLine();
+    }
+
+    public int commandInput() {
+        String input = Console.readLine();
+        int command = validator.validateCommand(input);
+
+        return command;
     }
 }
