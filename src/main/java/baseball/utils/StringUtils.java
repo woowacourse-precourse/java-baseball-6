f274673f5d79c5cc02c55d03a29fe858390b91d5
상원 최 <swchoi1@swm.ai>
2023-10-21 10:46:07 +0900

    feat: "사용자 입력값 검증에 도움을 주는 StringUtils 추가" 모든 문자열이 숫자인지, 해당 문자열이 내가 입력한 길이와 같은지, 문자열에 중복이 있는지 확인하는 기능 추가
    #3-user-input

---
+++ src/main/java/baseball/utils/StringUtils.java
@@ -0,0 +1,32 @@
+package baseball.utils;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class StringUtils {
+
+    private StringUtils() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static boolean isNumeric(final String value) {
+        if (StringUtils.isLengthEqual(value, 0)) {
+            return false;
+        }
+
+        return value.chars().allMatch(Character::isDigit);
+    }
+
+    public static boolean isLengthEqual(final String value, final int length) {
+        if (length < 0) {
+            throw new IllegalArgumentException();
+        }
+        return value.length() == length;
+    }
+
+    public static boolean hasDuplicate(final String value) {
+        return value.length() == Arrays.stream(value.split("")).collect(Collectors.toSet()).size();
+    }
+
+
+}
