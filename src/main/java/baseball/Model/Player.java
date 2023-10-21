747fc1b6e65bf5617013819b2453bd4ca9692bc9
stopmin <geemin0@naver.com>
2023-10-22 02:48:01 +0900

    feat: computer, player 클래스 추가

---
+++ src/main/java/baseball/Model/Player.java
@@ -0,0 +1,12 @@
+package baseball.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    private List<Integer> player;
+
+    public void setPlayer(List<Integer> player) {
+        this.player = player;
+    }
+}
