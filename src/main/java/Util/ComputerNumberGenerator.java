42df78b72f8b64c760d78c4521ab2e44cb0d7ff8
chunghyeon-kim <chunghyeonkim90@gmail.com>
2023-10-22 05:16:34 +0900

    기능 구현 - 1부터 9까지 서로 다른 수로 이루어진 3자리의 수를 생성한다

---
+++ src/main/java/Util/ComputerNumberGenerator.java
@@ -0,0 +1,19 @@
+package Util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComputerNumberGenerator {
+    public static List<Integer> generateComputerNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        return computer;
+    }
+}
