fdacf20cb1fcc14c6ed767911e721884a1a4935d
김연진 <love_homebody@naver.com>
2023-10-21 16:57:04 +0900

    feat(Player): 중복된 숫자를 입력한 경우 예외 발생

--- src/main/java/baseball/constant/ExceptionMessage.java
+++ src/main/java/baseball/constant/ExceptionMessage.java
@@ -3,7 +3,8 @@ package baseball.constant;
 public enum ExceptionMessage {
 
     NOT_INTEGER("숫자만 입력할 수 있습니다."),
-    INVALID_LENGTH("3개의 숫자를 입력해주세요.");
+    INVALID_LENGTH("3개의 숫자를 입력해주세요."),
+    DUPLICATE_NUMBER("중복된 숫자는 입력할 수 없습니다.");
 
     private final String message;
 
--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -4,7 +4,9 @@ import baseball.constant.BallConstant;
 import baseball.constant.ExceptionMessage;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Player {
 
@@ -17,12 +19,21 @@ public class Player {
 
     private void validateElements(List<Integer> elements) {
         validateLength(elements);
+        validateDuplicate(elements);
     }
 
     private void validateLength(List<Integer> elements) {
         if (elements.size() != BallConstant.BALL_LENGTH.getValue()) {
-            ExceptionMessage exceptionMessage = ExceptionMessage.INVALID_LENGTH;
-            throw new IllegalArgumentException(exceptionMessage.getMessage());
+            ExceptionMessage message = ExceptionMessage.INVALID_LENGTH;
+            throw new IllegalArgumentException(message.getMessage());
+        }
+    }
+
+    private void validateDuplicate(List<Integer> elements) {
+        Set<Integer> notDuplicateElements = new HashSet<>(elements);
+        if (notDuplicateElements.size() != BallConstant.BALL_LENGTH.getValue()) {
+            ExceptionMessage message = ExceptionMessage.DUPLICATE_NUMBER;
+            throw new IllegalArgumentException(message.getMessage());
         }
     }
 
