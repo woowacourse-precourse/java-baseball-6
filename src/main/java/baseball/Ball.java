09b3bac8a33d1ada18edc85d7f7b00700af9963e
sangdol <priceelees@gmail.com>
2023-10-22 00:29:43 +0900

    feat: 하나의 숫자 정보를 저장하는 Ball 클래스

---
+++ src/main/java/baseball/Ball.java
@@ -0,0 +1,32 @@
+package baseball;
+
+import java.util.Objects;
+
+public class Ball {
+    private final int number;
+
+    public Ball(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Ball ball = (Ball) o;
+        return getNumber() == ball.getNumber();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNumber());
+    }
+}
