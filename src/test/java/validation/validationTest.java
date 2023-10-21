be3fa3b9497147b184dc282b465cd3dba4a61bac
jaegyeongkim <dongji_11@naver.com>
2023-10-21 11:25:04 +0900

    test(ValidationTest): 사용자 입력을 검증하는 기능 테스트

---
+++ src/test/java/validation/validationTest.java
@@ -0,0 +1,27 @@
+package validation;
+
+import Validation.Validation;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class validationTest {
+    Validation validation= new Validation();
+    @Test
+    public void 사용자입력이_공백인_경우() {
+        String input = "";
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                validation.inputLengthIsZero(input));
+    }
+    @Test
+    public void 사용자입력이_숫자가_아닌경우(){
+        String input="asdf";
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                validation.inputIsNotNumber(input));
+    }
+    @Test
+    public void 사용자입력이_중복된_경우(){
+        String input="113";
+       Assertions.assertThrows(IllegalArgumentException.class, ()->
+               validation.duplicateNumber(input));
+    }
+}
