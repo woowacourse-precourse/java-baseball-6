12c1ebc44d4d509963fd90b1216d16fb1ab7f9a0
kmss6905 <kmss69052@naver.com>
2023-10-21 11:17:20 +0900

    ♻️ refactor : 화면 출력하는 부분을 메서드로 추출
    printResultScore 내부에서 score 에 따라 출력하는 부분을 알아서 처리하고 있기 때문에 외부에서 score 에 따라 분기하던 부분(if ~ else) 을 없앨 수 있었습니다.
    게임에서 이기던 어떤 점수를 받던 상관없이 print 를 해주고 있기 때문에 점수에 따라 게임 재시작, 종료 로직할 지 결정하는 로직을 타기 전에 화면을 출력하도록 했습니다.
    
    게임 시작 시 출력하는 부분, 입력 값을 받기 위해 화면에 메시지 출력하는 부분 역시 동일하게 printXXX 라는 이름으로 메서드를 추출하였습니다.
    printResultScore 와는 다르게 외부 인수가 필요 없기 때문에 인자없는 별도의 메서드로 만들었습니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,60 +13,36 @@ public class Application {
         List<Integer> computer = new ArrayList<>();
         setRandomNumberToComputer(computer);
 
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        printOpening();
         while (true) {
-            System.out.print("숫자를 입력해주세요 : ");
-            // 사용자 숫자
+            printInput();
             final String inputNumberStr = Console.readLine();
-
             // 입력을 제대로 못받을 경우 예외 던진다. ( 현재는 무조건 3글자만 )
             if (inputNumberStr.length() != 3) {
                 throw new IllegalArgumentException("잘못된 입력값 입니다.(입력:3자리 숫자)");
             }
-
-            // 입력받은 숫자와 컴퓨터의 숫자를 비교한다.
             Score score = getCompareResultWithInputNumberAndComputerNumber(
                     inputNumberStr, computer);
-
-            // 비교 결과를 출력한다.
+            printScoreResult(score);
             if (score.hasWinScore()) {
-                System.out.println(String.format("%s스트라이크", score.getStrike()));
-                System.out.println("3개의 숫자를 모두 맞히셨습니다.! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-
                 String input = Console.readLine();
                 if (!List.of("1", "2").contains(input)) {
                     throw new IllegalArgumentException("1 또는 2를 입력해야합니다.");
                 }
-
                 if (Integer.parseInt(input) == 1) {
                     // 컴퓨터 번호 리셋
                     computer = new ArrayList<>();
                     setRandomNumberToComputer(computer);
                 }
-
                 if (Integer.parseInt(input) == 2) {
                     break;
                 }
-
-            } else {
-                if (score.isNoScore()) {
-                    System.out.println("낫싱");
-                }
-
-                if (score.isOnlyBall()) {
-                    System.out.println(String.format("%s볼", score.getBall()));
-                }
-
-                if (score.isOnlyStrike()) {
-                    System.out.println(String.format("%s스트라이크", score.getStrike()));
-                }
-
-                if(score.isStrikeWithBall()) {
-                    System.out.println(String.format("%s볼 %s스트라이크", score.getBall(), score.getStrike()));
-                }
             }
         }
+    }
+
+    private static void printInput() {
+        System.out.print("숫자를 입력해주세요 : ");
     }
 
     private static Score getCompareResultWithInputNumberAndComputerNumber(String inputNumberStr,
@@ -90,5 +66,33 @@ public class Application {
                 computer.add(randomNumber);
             }
         }
+    }
+
+    private static void printScoreResult(Score score) {
+        if (score.hasWinScore()) {
+            System.out.println(String.format("%s스트라이크", score.getStrike()));
+            System.out.println("3개의 숫자를 모두 맞히셨습니다.! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        }
+
+        if (score.isNoScore()) {
+            System.out.println("낫싱");
+        }
+
+        if (score.isOnlyBall()) {
+            System.out.println(String.format("%s볼", score.getBall()));
+        }
+
+        if (score.isOnlyStrike()) {
+            System.out.println(String.format("%s스트라이크", score.getStrike()));
+        }
+
+        if(score.isStrikeWithBall()) {
+            System.out.println(String.format("%s볼 %s스트라이크", score.getBall(), score.getStrike()));
+        }
+    }
+
+    private static void printOpening() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
     }
 }
