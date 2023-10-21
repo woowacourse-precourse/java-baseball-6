4eb2bdf470191f50877edee0beabca560bb5d6e5
Sully <2dcoder@naver.com>
2023-10-21 22:32:08 +0900

    feat: 낫싱 출력 메서드 추가

--- src/main/java/baseball/output/GameOutput.java
+++ src/main/java/baseball/output/GameOutput.java
@@ -9,6 +9,10 @@ public class GameOutput {
     private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String REQUEST_MESSAGE = "숫자를 입력해주세요 : ";
 
+    public static void printlnNothing() {
+        System.out.println(NOTHING);
+    }
+
     public static void printRequestMessage() {
         System.out.print(REQUEST_MESSAGE);
     }
