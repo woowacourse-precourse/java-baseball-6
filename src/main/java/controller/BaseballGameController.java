07f1590e44d91f55a741a2f779b7699a7d37fa84
chunghyeon-kim <chunghyeonkim90@gmail.com>
2023-10-22 06:06:06 +0900

    기능 구현 - 사용자가 숫자를 입력하면 정답과 비교해서 적절한 메세지를 출력한다

--- src/main/java/controller/BaseballGameController.java
+++ src/main/java/controller/BaseballGameController.java
@@ -6,7 +6,7 @@ import java.util.List;
 import view.BaseballGameView;
 
 public class BaseballGameController {
-    private BaseballGameView baseballGameView = new BaseballGameView();
+    private final BaseballGameView baseballGameView = new BaseballGameView();
 
     public void doBaseballGame() {
         List<Integer> computer = ComputerNumberGenerator.generate();
@@ -15,6 +15,34 @@ public class BaseballGameController {
 
         while (!computer.equals(userNumbers)) {
             userNumbers = baseballGameView.getUserNumberList();
+
+            int strikeCount = 0;
+            int includedNumberCount = 0;
+
+            for (int i = 0; i < 3; i++) {
+                if (computer.get(i).equals(userNumbers.get(i))) {
+                    strikeCount ++;
+                }
+            }
+
+            for (int i = 0; i < 3; i++) {
+                if (computer.contains(userNumbers.get(i))) {
+                    includedNumberCount ++;
+                }
+            }
+
+            int ballCount = includedNumberCount - strikeCount;
+
+            if (includedNumberCount == 0) {
+                System.out.println("낫싱");
+            } else if (strikeCount == 0) {
+                System.out.println(ballCount + "볼");
+            } else if (strikeCount == includedNumberCount) {
+                System.out.println(strikeCount + "스트라이크");
+            } else if (strikeCount > 0) {
+                System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+            }
         }
     }
+
 }
