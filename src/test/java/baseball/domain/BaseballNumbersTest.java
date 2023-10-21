88bc1be718ce0c0bb03f9b0e632c8bfb50aa7e7a
seokmyungham <hellotjraud@gmail.com>
2023-10-21 22:15:10 +0900

    test(BaseballNumbersTest): getStrikeCount, getBallCount 단위 테스트 추가

---
+++ src/test/java/baseball/domain/BaseballNumbersTest.java
@@ -0,0 +1,28 @@
+package baseball.domain;
+
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class BaseballNumbersTest {
+
+    @Test
+    void 스트라이크_개수_테스트() {
+        BaseballNumbers computerNumber = new BaseballNumbers(List.of(1, 2, 3));
+        BaseballNumbers playerNumber = new BaseballNumbers(List.of(4, 2, 5));
+
+        int strikeCount = computerNumber.getStrikeCount(playerNumber);
+
+        Assertions.assertEquals(1, strikeCount);
+    }
+
+    @Test
+    void 볼_개수_테스트() {
+        BaseballNumbers computerNumber = new BaseballNumbers(List.of(1, 2, 3));
+        BaseballNumbers playerNumber = new BaseballNumbers(List.of(2, 3, 4));
+
+        int ballCount = computerNumber.getBallCount(playerNumber);
+
+        Assertions.assertEquals(2, ballCount);
+    }
+}
