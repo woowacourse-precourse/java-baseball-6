261ba8b3cb15b8ecf6d1e0543f616e5fb8652115
chansoo <chansoo38@gmail.com>
2023-10-21 16:50:43 +0900

    <Feat> : InputHandler - 입력에 대해 검증하는 기능을 추가한다.

--- src/main/java/baseball/handler/InputHandler.java
+++ src/main/java/baseball/handler/InputHandler.java
@@ -1,20 +1,28 @@
 package baseball.handler;
 
+import baseball.Exception.InputValidator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputHandler {
     private final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
     private final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
+    private final InputValidator inputValidator;
+    public InputHandler() {
+        inputValidator = new InputValidator();
+    }
+
     public String getNumbers() {
-        System.out.println(INPUT_MESSAGE);
+        System.out.print(INPUT_MESSAGE);
         String input = Console.readLine();
+        inputValidator.doValidate(input);
         return input;
     }
 
     public String getCommand() {
         System.out.println(RESTART_MESSAGE);
         String command = Console.readLine();
+        inputValidator.validateCommand(command);
         return command;
     }
 }
