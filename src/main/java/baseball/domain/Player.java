60fec19009b4cd1b2b3d7966b0ea40fbf695058c
hayannn <dlgkdis801@naver.com>
2023-10-21 17:48:00 +0900

    chore: 숫자 야구 게임 시작 시의 문구를 Player.java에서 실행되도록 수정

--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -6,6 +6,10 @@ import java.util.List;
 public class Player {
     private List<Integer> playerNumbers = new ArrayList<>();
 
+    public Player() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
     public List<Integer> getPlayerNumbers() {
         return playerNumbers;
     }
