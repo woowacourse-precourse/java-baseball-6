8c20b66130019344cc7989e9a48b33bc860b169a
dyjeong365 <dyjeong365@gmail.com>
2023-10-21 12:37:15 +0900

    refactor: rename variable for clarity

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -37,7 +37,7 @@ public class Application {
         if (result.strikes() == computer.size()) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            String oneOrTwo = Console.readLine();
+            String input = Console.readLine();
 
             validateOneOrTwo(input);
 
