1371377383912c22f8015ae54c220b4fc83b2f5d
박성춘 <songchon100@gmail.com>
2023-10-21 17:40:11 +0900

    feat : 게임 재시작 입력값 validator 기능 구현

--- src/main/java/baseball/util/ReStartInputValidator.java
+++ src/main/java/baseball/util/ReStartInputValidator.java
@@ -1,4 +1,20 @@
 package baseball.util;
 
-public class ReStartInputValidator {
+import java.util.regex.Pattern;
+
+public class ReStartInputValidator extends Validator{
+    private static final Pattern RESTART_NUMBER_REGEX = Pattern.compile("^[1-2]{1}$");
+
+    @Override
+    public void validate(String inputRestartNum) throws IllegalArgumentException {
+        String afterNum = removeSpace(inputRestartNum);
+        validateEmptyInput(afterNum);
+        validateReStartInputNum(afterNum);
+    }
+
+    private void validateReStartInputNum(String reStartNum) throws IllegalArgumentException{
+        if(!RESTART_NUMBER_REGEX.matcher(reStartNum).matches()) {
+            throw new IllegalArgumentException("Exception Message : 재시작 입력값의 형식에 맞춰주세요.");
+        }
+    }
 }
