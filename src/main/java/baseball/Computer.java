b305b64cb0e2328afc3b389e2b521902f2d26a8a
HiiWee <wpdlzhf159@gmail.com>
2023-10-21 18:10:31 +0900

    refactor: 사용자 입력 숫자 검증을 Numbers 객체 내부로 이동

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,13 +1,11 @@
 package baseball;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class Computer {
 
-    private static final int COUNT_TO_GENERATE = 3;
+    private static final int GENERATE_COUNT = 3;
     private static final int STRIKE_INDEX = 0;
     private static final int BALL_INDEX = 1;
 
@@ -15,62 +13,16 @@ public class Computer {
     private Numbers userNumbers;
 
     public void generateNumber(final Function<Integer, List<Integer>> computerNumberGenerator) {
-        List<Integer> randomNumbers = computerNumberGenerator.apply(COUNT_TO_GENERATE);
-        computerNumbers = Numbers.from(randomNumbers);
+        List<Integer> randomNumbers = computerNumberGenerator.apply(GENERATE_COUNT);
+        computerNumbers = new Numbers(randomNumbers);
     }
 
     public void addUserNumber(final String inputUserNumber) {
-        this.userNumbers = convertToNumbers(inputUserNumber);
+        this.userNumbers = Numbers.createUserNumbers(inputUserNumber);
     }
 
     public GameResult createResult() {
         List<Integer> compareResults = computerNumbers.createCompareResult(userNumbers);
         return new GameResult(compareResults.get(STRIKE_INDEX), compareResults.get(BALL_INDEX));
-    }
-
-    private Numbers convertToNumbers(final String inputUserNumber) {
-        validate(inputUserNumber);
-        List<Integer> userNumbers = Arrays.stream(inputUserNumber.split(""))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-
-        return Numbers.from(userNumbers);
-    }
-
-    // TODO 별도의 validater로 분리해야 할까?
-    private void validate(final String inputUserNumber) {
-        validateLength(inputUserNumber);
-        validateInteger(inputUserNumber);
-        validateRandomNumber(inputUserNumber);
-    }
-
-    private static void validateInteger(final String inputUserNumber) {
-        try {
-            Integer.parseInt(inputUserNumber);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자만 입력해야 합니다.");
-        }
-    }
-
-    private static void validateLength(final String inputUserNumber) {
-        if (inputUserNumber.length() != COUNT_TO_GENERATE) {
-            throw new IllegalArgumentException("3개의 숫자를 입력해야 합니다.");
-        }
-    }
-
-    private void validateRandomNumber(final String inputUserNumber) {
-        String[] userNumbers = inputUserNumber.split("");
-        if (userNumbers[0].equals(userNumbers[1])
-                || userNumbers[1].equals(userNumbers[2])
-                || userNumbers[0].equals(userNumbers[2])) {
-            throw new IllegalArgumentException("중복된 숫자를 입력할 수 없습니다.");
-        }
-        validateZeroNumber(userNumbers);
-    }
-
-    private static void validateZeroNumber(final String[] userNumbers) {
-        if (userNumbers[0].equals("0") || userNumbers[1].equals("0") || userNumbers[2].equals("0")) {
-            throw new IllegalArgumentException("숫자는 1~9 사이의 숫자만 입력할 수 있습니다.");
-        }
     }
 }
--- src/main/java/baseball/Numbers.java
+++ src/main/java/baseball/Numbers.java
@@ -1,10 +1,14 @@
 package baseball;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Numbers {
+
+    private static final int NUMBER_LENGTH = 3;
 
     private final List<Integer> numbers;
 
@@ -12,8 +16,12 @@ public class Numbers {
         this.numbers = numbers;
     }
 
-    public static Numbers from(final List<Integer> numbers) {
-        return new Numbers(numbers);
+    public static Numbers createUserNumbers(final String inputUserNumber) {
+        validate(inputUserNumber);
+        List<Integer> userNumbers = Arrays.stream(inputUserNumber.split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        return new Numbers(userNumbers);
     }
 
     public List<Integer> createCompareResult(final Numbers userNumbers) {
@@ -37,5 +45,41 @@ public class Numbers {
 
     private boolean isSameNumber(final int computerNumber, final int userNumber) {
         return Objects.equals(computerNumber, userNumber);
+    }
+
+    private static void validate(final String inputUserNumber) {
+        validateLength(inputUserNumber);
+        validateInteger(inputUserNumber);
+        validateRandomNumber(inputUserNumber);
+    }
+
+    private static void validateInteger(final String inputUserNumber) {
+        try {
+            Integer.parseInt(inputUserNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자만 입력해야 합니다.");
+        }
+    }
+
+    private static void validateLength(final String inputUserNumber) {
+        if (inputUserNumber.length() != NUMBER_LENGTH) {
+            throw new IllegalArgumentException("3개의 숫자를 입력해야 합니다.");
+        }
+    }
+
+    private static void validateRandomNumber(final String inputUserNumber) {
+        String[] userNumbers = inputUserNumber.split("");
+        if (userNumbers[0].equals(userNumbers[1])
+                || userNumbers[1].equals(userNumbers[2])
+                || userNumbers[0].equals(userNumbers[2])) {
+            throw new IllegalArgumentException("중복된 숫자를 입력할 수 없습니다.");
+        }
+        validateZeroNumber(userNumbers);
+    }
+
+    private static void validateZeroNumber(final String[] userNumbers) {
+        if (userNumbers[0].equals("0") || userNumbers[1].equals("0") || userNumbers[2].equals("0")) {
+            throw new IllegalArgumentException("숫자는 1~9 사이의 숫자만 입력할 수 있습니다.");
+        }
     }
 }
