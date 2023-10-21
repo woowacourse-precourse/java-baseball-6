305bbfdaeaae7aaa8c0e2f00c2763a8b7f3b55a5
CHANG MIN PARK <98qkrckdals@naver.com>
2023-10-22 02:00:06 +0900

    commit6 : CheckFinal/ball, strike, out의 유무

--- src/main/java/baseball/CheckFinal.java
+++ src/main/java/baseball/CheckFinal.java
@@ -18,4 +18,23 @@ public class CheckFinal {
         return total - strikeCount;
     }
 
+    /**
+     * total, strike, ball의 갯수를 다 확인한 메서드입 니다.
+     * 낫싱, 볼, 스트라이크의 유무를 알려주는 확인 메서드 입니다.
+     * */
+    public String checkAll(List<Integer> computer, List<Integer> player){
+        int total = startCompare.countDigit(computer, player);
+        int stikeCount = strike.checkStrike(computer, player);
+        int ball = ball(computer, player);
+
+        if (total == 0){
+            return "낫싱";
+        } else if (stikeCount == 0){
+            return ball + "볼";
+        } else if (ball == 0){
+            return stikeCount + "스트라이크";
+        }
+
+        return ball + "볼" + " " + stikeCount + "스트라이크";
+    }
 }
