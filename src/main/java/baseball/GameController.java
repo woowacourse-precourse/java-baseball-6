0052c9369c0a1217e4fde375b1c9c2eff0536bdb
JUNG GYUWON <milestogo0905@naver.com>
2023-10-21 09:51:12 +0900

    feat(Application, GameController): Add Controller function

---
+++ src/main/java/baseball/GameController.java
@@ -0,0 +1,32 @@
+package baseball;
+
+public class GameController {
+
+    private static final String GAME_START = "숫자 야구 게임을 시작합니다.";
+    private static final String INPUT_NUMBER = "숫자를 입력해주세요 : ";
+    private static final String ANSWER_RESULT = "3스트라이크";
+    private static final String GAME_OVER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String ASK_TO_CONTINUE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public void run(){
+        Player player = new Player();
+        System.out.println(GAME_START);
+        do {
+            Computer computer = new Computer();
+            computer.generateAnswer();
+
+            while(true){
+                System.out.println(INPUT_NUMBER);
+                String playerInput = player.inputNumbers();
+                String result = computer.generateResult(playerInput);
+
+                System.out.println(result);
+                if (result.equals(ANSWER_RESULT)){
+                    break;
+                }
+            }
+            System.out.println(GAME_OVER);
+            System.out.println(ASK_TO_CONTINUE);
+        }while (player.conformGameRestart());
+    }
+}
