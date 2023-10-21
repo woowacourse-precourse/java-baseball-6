dded66443133bf40efaa7d56bb815e407034ee70
김명준 <86913355+mjj111@users.noreply.github.com>
2023-10-21 17:05:15 +0900

    test(PlayerTest): 생성자 메서드에서 들어오는 값을 String으로 받아 List<Integer>로 변환하여 초기화하도록 변경
    Player의 인스턴스 변수 inputAnswer은 List<Integer> 이기 때문에, 이전의 경우에는 같은 타입을 넣어줘야 했습니다. 그러나 String 값을 List<Integer>로 변환하는 기능에 대한 책임을 어디에 둘까 고민하다가 Player 안에서만 이뤄지기 때문에 Player에게 넣기로 결정했습니다.

--- src/test/java/baseball/domain/PlayerTest.java
+++ src/test/java/baseball/domain/PlayerTest.java
@@ -9,11 +9,12 @@ public class PlayerTest {
 
     @Test
     void 사용자_입력_초기화() {
-        List<Integer> inputNumbers = List.of(1, 2, 3);
+        String inputNumbers = "123";
+        List<Integer> expectedResult = List.of(1, 2, 3);
 
         Player player = new Player(inputNumbers);
 
         List<Integer> playersInputAnswer = player.getInputAnswer();
-        Assertions.assertEquals(inputNumbers,playersInputAnswer);
+        Assertions.assertEquals(expectedResult,playersInputAnswer);
     }
 }
