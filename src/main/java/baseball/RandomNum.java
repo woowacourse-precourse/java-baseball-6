f2abc9fef34099b8c389db40e2194fc2dbd20fd5
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-10-21 23:23:52 +0900

    FEAT / make random number list by Randoms

--- src/main/java/baseball/RandomNum.java
+++ src/main/java/baseball/RandomNum.java
@@ -1,4 +1,20 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+
 public class RandomNum {
+    private static final int LENGTH = 3;
+    private static final int MIN_NUM = 1;
+    private static final int MAX_NUM = 9;
+    private ArrayList<Integer> nums = new ArrayList<>();
+
+    public RandomNum() {
+        while (nums.size() < LENGTH) {
+            int randomNum = Randoms.pickNumberInRange(MIN_NUM, MAX_NUM);
+            if (!nums.contains(randomNum)) {
+                nums.add(randomNum);
+            }
+        }
+    }
 }
