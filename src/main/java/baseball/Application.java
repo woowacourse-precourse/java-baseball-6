f7673f854b19549601d668a7ef4714831211a10e
gumgu <bienlee@naver.com>
2023-10-21 20:26:52 +0900

    [구현] 게임 반복진행 기능구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,16 +4,25 @@ import java.util.List;
 
 public class Application {
 
-
-
     public static void main(String[] args) {
 
         BaseBallGame baseBallGame = new BaseBallGame();
         PrintManager printManager = new PrintManager();
+        InputManager inputManager = new InputManager();
 
         printManager.printGameStart();
 
-        baseBallGame.doBaseBallGame();
+        int restart = 0;
+
+        while (restart != 2) {
+
+            baseBallGame.doBaseBallGame();
+
+            printManager.printRestart();
+            restart = inputManager.getRestart();
+            System.out.println("restart = " + restart);
+
+        }
 
     }
 
