71b8261552888506ed4d55c9451f900a60476a5f
Minjae-An <mj3242@naver.com>
2023-10-21 17:47:05 +0900

    feat : 게임 끝 문구 출력 메서드 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -3,6 +3,7 @@ package baseball;
 public class Game {
     private Computer computer;
     private final String GAME_START="숫자 야구 게임을 시작합니다.";
+    private final String GAME_END="3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     private Game(Computer computer) {
         this.computer = computer;
@@ -18,5 +19,9 @@ public class Game {
 
     private void printGameStart(){
         System.out.println(GAME_START);
+    }
+
+    private void printGameEnd(){
+        System.out.println(GAME_END);
     }
 }
