1a593e967f9d0fa6736026e5ae2abdd5bc700fce
munis <aaaa6658@naver.com>
2023-10-21 17:30:00 +0900

    feat : BaseBallNumber 클래스에서 스트라이크 개수 구하기 구현

--- src/main/java/baseball/BaseBallNumber.java
+++ src/main/java/baseball/BaseBallNumber.java
@@ -1,11 +1,22 @@
 package baseball;
 
 import java.util.List;
+import java.util.Objects;
 
 public class BaseBallNumber {
     private List<Integer> baseballNumber;
 
     public BaseBallNumber(List<Integer> baseballNumber) {
         this.baseballNumber = baseballNumber;
+    }
+
+    public Integer getStrikeCount(BaseBallNumber userNumber) {
+        Integer strikeCount = 0;
+        for (int i = 0; i < 3; ++i) {
+            if (Objects.equals(baseballNumber.get(i), userNumber.baseballNumber.get(i))) {
+                strikeCount++;
+            }
+        }
+        return strikeCount;
     }
 }
