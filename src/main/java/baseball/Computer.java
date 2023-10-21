12f7a55daf96e77605be3c56b37758079b636c94
Moly <102847513+jminkkk@users.noreply.github.com>
2023-10-21 15:54:36 +0900

    feat: Random값 추출 메서드 구현
    요구사항: Random 값 추출은 camp.nextstep.edu.missionutils.Randoms의 pickNumberInRange()를 활용한다.

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -6,5 +6,15 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class Computer {
+    private static final Integer RANDOM_NUMBER_MIN_RANGE = 1;
+    private static final Integer RANDOM_NUMBER_MAX_RANGE = 9;
+    private List<Integer> RandomNumber;
 
+    public Computer(List<Integer> randomNumber) {
+        RandomNumber = randomNumber;
+    }
+
+    private int chooseRandomNumber() {
+        return pickNumberInRange(RANDOM_NUMBER_MIN_RANGE, RANDOM_NUMBER_MAX_RANGE);
+    }
 }
