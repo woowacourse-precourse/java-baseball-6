ad84dc9decb0a6bf2eedd4eaf56acd554ad0f20e
Firedrago95 <yonghwa95@naver.com>
2023-10-21 16:28:37 +0900

    feat 게임시작 , 정답맞춤, 게임종료 메시지 출력 기능 구현

--- src/main/java/baseball/View/OutputView.java
+++ src/main/java/baseball/View/OutputView.java
@@ -9,6 +9,11 @@ public class OutputView {
     public static final String NOTHING = "낫싱";
     public static final String BALL = "볼";
     public static final String STRIKE = "스트라이크";
+    public static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+
+    public static void printStartMessage() {
+        System.out.println(START_MESSAGE);
+    }
 
     public static void printJudgement(Map<String,Integer> judgement) {
         String judgementMessage = makeJudgementMessage(judgement);
@@ -29,5 +34,13 @@ public class OutputView {
             return strike+STRIKE;
         }
         return ball+BALL+" "+strike+STRIKE;
+    }
+
+    public static void printCorrectMessage() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다!");
+    }
+
+    public static void printEndMessage() {
+        System.out.println("게임 종료");
     }
 }
