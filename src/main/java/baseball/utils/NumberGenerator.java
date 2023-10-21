a33da6060433b3357eeb7d0258aaf85e9ef266b7
hlmg <devhlmg@gmail.com>
2023-10-21 17:52:55 +0900

    refactor(NumberGenerator): 스트림 적용해서 코드 개선

--- src/main/java/baseball/utils/NumberGenerator.java
+++ src/main/java/baseball/utils/NumberGenerator.java
@@ -1,9 +1,8 @@
 package baseball.utils;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.stream.IntStream;
 
 public class NumberGenerator {
     public static final String TO_SMALL_RANGE = "자릿수보다 범위가 더 커야 합니다";
@@ -12,18 +11,22 @@ public class NumberGenerator {
 
     public static List<Integer> generateUniqueNumbers(int startInclusive, int endInclusive, int digit) {
         validate(startInclusive, endInclusive, digit);
-        Set<Integer> numbers = new LinkedHashSet<>();
-        while (numbers.size() < digit) {
-            int generatedRandomNumber = generateRandomNumber(startInclusive, endInclusive);
-            numbers.add(generatedRandomNumber);
-        }
-        return List.copyOf(numbers);
+
+        return IntStream.generate(() -> generateRandomNumber(startInclusive, endInclusive))
+                .distinct()
+                .limit(digit)
+                .boxed()
+                .toList();
     }
 
     private static void validate(int startInclusive, int endInclusive, int digit) {
-        if (endInclusive - startInclusive + 1 < digit) {
+        if (getRange(startInclusive, endInclusive) < digit) {
             throw new IllegalArgumentException(TO_SMALL_RANGE);
         }
+    }
+
+    private static int getRange(int startInclusive, int endInclusive) {
+        return endInclusive - startInclusive + 1;
     }
 
     private static int generateRandomNumber(int startInclusive, int endInclusive) {
