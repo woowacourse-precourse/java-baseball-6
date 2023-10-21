620a9c131af600249126759516b8034f3720c0f5
SJ70 <seojae70@gmail.com>
2023-10-21 19:50:20 +0900

    Rename: ResultsUtilsTest -> ResultsUtilDetermineTest

--- src/test/java/baseball/results/ResultsUtilDetermineTest.java
+++ src/test/java/baseball/results/ResultsUtilDetermineTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class ResultsUtilsTest {
+public class ResultsUtilDetermineTest {
 
     private Balls balls;
 
