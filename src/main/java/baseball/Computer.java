29d72ee5b419add22feafa7e89822bb2216f024e
Minjae-An <mj3242@naver.com>
2023-10-21 17:29:33 +0900

    feat : Computer 클래스 문제 숫자 생성 로직 추가
    고유한 세 자리수를 나타내는 List를 생성하는 메서드 구현

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -9,11 +9,19 @@ public class Computer {
     private final int END_INCLUSIVE=9;
     private final int LENGTH=3;
 
+    private List<Integer> computerNumbers;
+
     private static class LazyHolder{
         public static final Computer INSTANCE=new Computer();
     }
 
     public static Computer getInstance(){
         return LazyHolder.INSTANCE;
+    }
+
+    public List<Integer> generateRandomNumber(){
+        List<Integer> uniqueNumbers = Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, LENGTH);
+        computerNumbers=uniqueNumbers;
+        return uniqueNumbers;
     }
 }
