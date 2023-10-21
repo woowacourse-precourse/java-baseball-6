329cdcf891e4bb7263129a1445c5aabbdeef384d
YongJOoOo <we072200@naver.com>
2023-10-21 18:55:40 +0900

    style: Applecation의 main() 코드 정리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,9 +11,9 @@ public class Application {
         do {
             ComputerGame game = new ComputerGame();
             game.gameStart();
+
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             gameRepeat = Console.readLine();
         } while (gameRepeat.equals("1"));
     }
-
 }
