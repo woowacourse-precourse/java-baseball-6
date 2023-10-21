0d3b2466cc1e7a9a44b18d76f5a75307ad9baf7b
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-10-22 05:24:01 +0900

    feat: add input reader when game finish

--- src/main/java/baseball/InputReader.java
+++ src/main/java/baseball/InputReader.java
@@ -2,25 +2,59 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class InputReader {
-    public void readUserInput(){
+    public List<Integer> readUserGameInput(){
         String input = Console.readLine();
 
+        validateGameInputSize(input);
+        validateGameInputType(input);
+        validateGameInputDuplicationOfNumber(input);
 
+        return toIntegerList(input);
     }
 
-    public void validateInputSize(String input){
+    public int readUserGameFinishInput(){
+        String input = Console.readLine();
+
+        validateGameFinishInputSize(input);
+        validateGameFinishInputType(input);
+
+        return toInt(input);
+    }
+
+    public int toInt(String input){
+        return Integer.parseInt(input);
+    }
+
+    public List<Integer> toIntegerList(String input){
+        return Arrays.stream(input.split(""))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .toList();
+    }
+
+    public void validateGameFinishInputSize(String input){
+        if (input.length() != 1) throw new IllegalArgumentException("input's size have to be 1");
+    }
+
+    public void validateGameFinishInputType(String input){
+        if(!input.matches("[1-2]+")) throw new IllegalArgumentException("input have to type only one or two.");
+    }
+
+    public void validateGameInputSize(String input){
         if(input.length() != 3) throw new IllegalArgumentException("input's size have to be 3");
     }
 
-    public void validateInputType(String input){
+    public void validateGameInputType(String input){
         if(!input.matches("[0-9]+")) throw new IllegalArgumentException("input have to type only number.");
     }
 
-    public void validateInputDuplicationOfNumber(String input){
+    public void validateGameInputDuplicationOfNumber(String input){
         Set<Character> set = new HashSet<>();
 
         for(char c : input.toCharArray()){
