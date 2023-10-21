048f7a2eb824a9d4f0b33a72ffe143125cc00a25
ChaeAg <dydkglel123@gmail.com>
2023-10-21 19:07:27 +0900

    refactor: 변경된 컨트롤러의 게임 시작 메서드를 호출하도록 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,10 @@
 package baseball;
 
-import baseball.controller.BaseballGame;
+import baseball.controller.BaseballGameController;
 
 public class Application {
     public static void main(String[] args) {
-        BaseballGame baseballGame = new BaseballGame();
-        baseballGame.GameStart();
+        BaseballGameController baseballGameController = new BaseballGameController();
+        baseballGameController.run();
     }
 }
