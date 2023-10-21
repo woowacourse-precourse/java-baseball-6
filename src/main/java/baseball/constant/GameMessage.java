5e007c59ac92c4196ca6f3373f057e56a4ed923d
김승진 <ohksj77@gmail.com>
2023-10-21 18:59:18 +0900

    feat(GameMessage): 게임 진행 메시지에 대한 열거형 추가

---
+++ src/main/java/baseball/constant/GameMessage.java
@@ -0,0 +1,11 @@
+package baseball.constant;
+
+public enum GameMessage {
+    GAME_START("숫자 야구 게임을 시작합니다.");
+
+    private final String message;
+
+    GameMessage(final String message) {
+        this.message = message;
+    }
+}
