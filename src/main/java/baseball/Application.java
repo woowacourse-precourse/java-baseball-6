a85b8dbd887d79d9068a559d1f0617402ab06a36
wonandonly <wanny416@sookmyung.ac.kr>
2023-10-22 05:50:29 +0900

    feat: 입력, 출력 로직 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,14 +1,45 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import camp.nextstep.edu.missionutils.Console;
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
+    static int inputNumber;
     public static void main(String[] args) {
-        //사용자 입력 받기
-        Game game = new Game();
+        startNewGame();
+    }
+
+    private static void startNewGame() {
+        System.out.println("숫자 야구 게임을 시작합니다."); //이거 외부라이브러리면.. 스트링으로 저장한 후 리턴해야겠다
+
+        while (true) {
+            Game game = new Game();
+            boolean isGameOver = false;
+
+            while (!isGameOver) {
+                System.out.print("숫자를 입력해주세요: ");
+                String inputString = readLine();
+                inputNumber = Integer.parseInt(inputString);
+
+                String result = game.evaluation(inputNumber);
+                System.out.println(result);
+
+                if (result.equals("3개의 숫자를 모두 맞히셨습니다! 게임 종료")) {
+                    isGameOver = true;
+                }
+            }
+
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String inputString = readLine();
+            inputNumber = Integer.parseInt(inputString);
+            if (inputNumber == 2) {
+                break;
+            }
+        }
+
     }
 }
+
 
 class RandomNumberGenerator{
     public static int generate() {
@@ -31,12 +62,49 @@ class RandomNumberGenerator{
 class Game{
     int answer;
     int[] answerDigits;
+
     Game(){
         answer = RandomNumberGenerator.generate();
+        answerDigits = new int[3];
+        answerDigits[0] = answer / 100;
+        answerDigits[1] = (answer / 10) % 10;
+        answerDigits[2] = answer % 10;
     }
 
     // 사용자가 제시한 숫자를 평가하는 함수
     public String evaluation(int inputNumber) {
-        return "";
+        int[] inputDigits = new int[3];
+        inputDigits[0] = inputNumber / 100;
+        inputDigits[1] = (inputNumber / 10) % 10;
+        inputDigits[2] = inputNumber % 10;
+
+        int strikes = 0;
+        int balls = 0;
+
+        for (int i = 0; i < 3; i++) {
+            if (inputDigits[i] == answerDigits[i]) {
+                strikes++;
+            } else if (containsDigit(inputDigits[i])) {
+                balls++;
+            }
+        }
+
+        if (strikes == 3) {
+            return "3개의 숫자를 모두 맞히셨습니다! 게임 종료"; //정답
+        } else if (strikes > 0 || balls > 0) {
+            return balls + "볼 " + strikes + "스트라이크";
+        } else {
+            return "낫싱";
+        }
+    }
+
+    // 정답에 특정 숫자가 있는지 확인하는 함수 (스트라이크가 아닌 경우에)
+    private boolean containsDigit(int digit) {
+        for (int i = 0 ; i < 3 ; i++) {
+            if (answerDigits[i] == digit) {
+                return true;
+            }
+        }
+        return false;
     }
 }
