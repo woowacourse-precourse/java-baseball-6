27e1715a570988806e7bc73650a36ea1277564d6
JoonKyo <joonkyolee@gmail.com>
2023-10-22 04:13:10 +0900

    refactor(Game): 생성자 내부에 computerNumber와 playerNumber 클래스 생성

--- src/main/java/baseball/controller/Game.java
+++ src/main/java/baseball/controller/Game.java
@@ -1,18 +1,27 @@
 package baseball.controller;
 
+import baseball.model.ComputerNumber;
+import baseball.model.PlayerNumber;
 import baseball.view.OutputView;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
+    private static ComputerNumber computerNumber;
+    private static PlayerNumber playerNumber;
     private final OutputView outputView = new OutputView();
     private List<Integer> computer;
     private List<Integer> player;
 
     public Game() {
+        computerNumber = new ComputerNumber();
+        playerNumber = new PlayerNumber();
+        
         computer = new ArrayList<>();
         player = new ArrayList<>();
 
         outputView.printStartGame();
     }
+
+
 }
