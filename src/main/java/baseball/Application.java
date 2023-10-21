3bef4971c8bbd688b683a4c4de4c83880760e44b
정성욱 <rkfka7849@gmail.com>
2023-10-21 18:58:02 +0900

    refactor(SystemManager): 숫자 야구를 처음 실행하는 기능을 숫자 야구를 시작하는 기능과 통합

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,11 @@
 package baseball;
 
 import baseball.controller.SystemManager;
+import baseball.view.OutputHandler;
 
 public class Application {
     public static void main(String[] args) {
-        SystemManager.firstGame();
+        OutputHandler.theGameStart();
+        SystemManager.newGame();
     }
 }
--- src/main/java/baseball/controller/SystemManager.java
+++ src/main/java/baseball/controller/SystemManager.java
@@ -1,14 +1,6 @@
 package baseball.controller;
 
-import baseball.view.OutputHandler;
-
 public class SystemManager {
-    static public void firstGame() {
-        GameManager gameManager = new GameManager();
-        OutputHandler.theGameStart();
-        gameManager.playRound();
-    }
-
     static public void newGame() {
         GameManager gameManager = new GameManager();
         gameManager.playRound();
