4381a06b82ce9202f7987fd89a90e495660860d9
acecic82 <102938al@naver.com>
2023-10-22 02:12:28 +0900

    [rilla] output 분석 시 println 이 맞는것 같아 추가합니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -30,7 +30,7 @@ public class Application {
                     break;
                 }
             }
-            System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
             flag = Integer.parseInt(Console.readLine());
 
