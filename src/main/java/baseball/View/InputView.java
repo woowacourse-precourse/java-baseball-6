651b8735750b3fa3a74528b03ec39ba162113a38
kim <hwiyul_g@daum.net>
2023-10-21 20:20:07 +0900

    feat(InputView) : 게임을 새로할지 종료할지 선택하는 함수 추가

--- src/main/java/baseball/View/InputView.java
+++ src/main/java/baseball/View/InputView.java
@@ -4,9 +4,20 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public String requestUserNumber(){
+    public String requestUserNumber() {
         System.out.print("숫자를 입력해주세요 : ");
         return Console.readLine();
+    }
+
+    public int requestGameRestart() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String userInput = Console.readLine();
+
+        if (!"1".equals(userInput) && !"2".equals(userInput)) {
+            throw new IllegalArgumentException("1또는 2만 입력하세요.");
+        }
+
+        return Integer.parseInt(userInput);
     }
 
 
