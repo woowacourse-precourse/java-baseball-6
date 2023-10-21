14ba020b83df90b8f4e522bf1496957fab62360f
kimdohyung <pdohyung@gmail.com>
2023-10-22 01:53:36 +0900

    feat: 3스트라이크가 나오기 전까지 게임을 반복하는 기능 구현

--- src/main/java/baseball/controller/Play.java
+++ src/main/java/baseball/controller/Play.java
@@ -1,4 +1,28 @@
 package baseball.controller;
 
+import baseball.model.ComputerNumber;
+import baseball.model.Score;
+import baseball.model.UserNumber;
+import baseball.view.InputView;
+
 public class Play {
+    private static final int STRIKE_THREE = 3;
+    private final Score score;
+    private final UserNumber userNumber;
+
+    public Play(Score score, UserNumber userNumber) {
+        this.score = score;
+        this.userNumber = userNumber;
+    }
+
+    public void play(ComputerNumber computerNumber) {
+        while (true) {
+            userNumber.newUserNumber(InputView.userNumberInput());
+            score.countScore(userNumber.getUserNumber(), computerNumber.getComputerNumber());
+            if (score.getStrike() == STRIKE_THREE) {
+                break;
+            }
+            score.resetScore();
+        }
+    }
 }
