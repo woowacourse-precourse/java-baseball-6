6509d36084734f3210c7b4cbb720382cc759b74e
yunju-m <dbswnfnal@naver.com>
2023-10-21 22:11:34 +0900

    feat(NumberValidator): 추가 입력한 데이터가 숫자인지 확인하는 함수

--- src/main/java/baseball/controller/NumberValidator.java
+++ src/main/java/baseball/controller/NumberValidator.java
@@ -3,14 +3,21 @@ package baseball.controller;
 public class NumberValidator {
     public static final int BASEBALL_NUMBER_LENGTH = 3;
     private static final String WRONG_LEGNTH_NUMBER_MESSAGE = "세 자리 숫자가 아닙니다."; 
+    private static final String WRONG_RANGE_NUMBER_MESSAGE = "1~9 사이의 숫자만 입력해주세요.";
 
     public void toValidateNumber(String inputNumber) {
-        if (!isSameLength(inputNumber)){
+        if (!isSameLength(inputNumber)) {
             throw new IllegalArgumentException(WRONG_LEGNTH_NUMBER_MESSAGE);
+        } else if (!isSameRange(inputNumber)) {
+            throw new IllegalArgumentException(WRONG_RANGE_NUMBER_MESSAGE);
         }
     }
 
     public boolean isSameLength(String inputNumber) {
         return inputNumber.length() == BASEBALL_NUMBER_LENGTH;
+    }
+
+    public boolean isSameRange(String inputNumber) {
+        return inputNumber.chars().allMatch(Character::isDigit);
     }
 }
