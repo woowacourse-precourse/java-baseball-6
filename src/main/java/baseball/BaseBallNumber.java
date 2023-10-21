b33011acaf9eaa9bacae1233a53f8b55c7442c5a
munis <aaaa6658@naver.com>
2023-10-21 18:47:08 +0900

    refactor : BaseBallNumber 클래스, baseballNumber 변수 final로 수정

--- src/main/java/baseball/BaseBallNumber.java
+++ src/main/java/baseball/BaseBallNumber.java
@@ -3,7 +3,7 @@ package baseball;
 import java.util.List;
 
 public class BaseBallNumber {
-    private List<Integer> baseballNumber;
+    private final List<Integer> baseballNumber;
 
     public BaseBallNumber(List<Integer> baseballNumber) {
         this.baseballNumber = baseballNumber;
