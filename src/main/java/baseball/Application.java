5e5f8d9ac087878418382ad3d0f5d1ea699462a5
KIVSH <kihongkim3@hotmail.com>
2023-10-21 11:55:02 +0900

    게임 반복 메소드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -16,10 +16,34 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        repeatGame();
     }
 
     public static void repeatGame() {
+        boolean isContinue = true;
+        int inputLength = 1;
 
+        List<Integer> computer;
+        List<Integer> user;
+
+        String gameStartMessage = "숫자 야구 게임을 시작합니다.\n";
+        String continueOrEndMessage = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
+
+        while (isContinue) {
+            output(gameStartMessage);
+
+            computer = generateRandomNumber();
+
+            repeatRound(computer);
+
+            output(continueOrEndMessage);
+
+            user = input(inputLength);
+
+            if (user.get(0) == 2) {
+                isContinue = false;
+            }
+        }
     }
 
     public static void repeatRound(List<Integer> computer) {
