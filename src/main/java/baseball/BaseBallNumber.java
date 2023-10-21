48ead51779587c7224aadde22af6e68c15ef659c
munis <aaaa6658@naver.com>
2023-10-21 18:40:10 +0900

    refactor : BaseBallNumber 클래스에 볼 비교 로직 checkBall 함수로 분리

--- src/main/java/baseball/BaseBallNumber.java
+++ src/main/java/baseball/BaseBallNumber.java
@@ -14,6 +14,13 @@ public class BaseBallNumber {
         return answerNumber.equals(userNumber);
     }
 
+    public boolean checkBall(Integer userNumber, int index) {
+        if (checkStrike(userNumber, index)) {
+            return false;
+        }
+        return baseballNumber.contains(userNumber);
+    }
+
     public Integer getStrikeCount(BaseBallNumber userNumber) {
         Integer strikeCount = 0;
         for (int i = 0; i < 3; ++i) {
@@ -29,7 +36,7 @@ public class BaseBallNumber {
         Integer ballCount = 0;
         for (int i = 0; i < 3; ++i) {
             Integer number = userNumber.baseballNumber.get(i);
-            if (baseballNumber.contains(number) && !checkStrike(number, i)) {
+            if (checkBall(number, i)) {
                 ballCount++;
             }
         }
