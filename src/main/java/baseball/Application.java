70a940063cba684f9a2e9434640dcebefe092a8a
tentenball <lth8905@gmail.com>
2023-10-21 23:18:52 +0900

    Feat, Docs: 기능구현 완료, 테스트x

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -32,12 +32,11 @@ class Game {
         }
     }
     public static boolean restart() {
-        System.out.println("게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String reNum = Console.readLine();
         if(reNum.equals("1")){ return true;}
         else if(reNum.equals("2")){return false;}
-        else throw new IllegalArgumentException("잘못된 입력으로 인해 게임을 종료합니다.");
+        else throw new IllegalArgumentException("1또는 2중 하나를 입력하세요.");
     }
     public boolean isCorrect() {
         int strikeNum = 0;
@@ -58,7 +57,7 @@ class Game {
             return false;
         } else if (strikeNum == 3) {
             System.out.println("3스트라이크");
-            System.out.println("3개의 숫자를 모두 맞히셨습니다!");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
             return true;
         } else {
             System.out.println(ballNum + "볼 " + strikeNum + "스트라이크");
@@ -72,32 +71,51 @@ class Game {
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
+            System.out.println(computer);
         }
-        System.out.println(computer);
     }
     public boolean input() {
         System.out.print("숫자를 입력하세요.");
         String inputStr = Console.readLine();
         int num;
             if (inputStr.length() == 3) {
-                if (userInput.size() < 3) {
-                    for (int i = 0; i < inputStr.length(); i++) {
-                        num = Integer.parseInt(inputStr.substring(i, i + 1));
-                        if (num == 0)
-                            throw new IllegalArgumentException("잘못된 입력으로 인해 게임을 종료합니다.");
-                        userInput.add(num);
+                try{
+                    if (userInput.size() < 3) {
+                    
+                        for (int i = 0; i < inputStr.length(); i++) {
+                            num = Integer.parseInt(inputStr.substring(i, i + 1));
+                            if (num == 0)
+                            throw new IllegalArgumentException("0은 입력불가능합니다.");
+                            userInput.add(num);
+                        }
+                        for(int i = 0; i< userInput.size(); i++){
+                            for (int j = 0; j < i; j++) {
+                                if (userInput.get(i).equals(userInput.get(j))) {
+                                    throw new IllegalArgumentException("중복된 요소가 있습니다.");
+                                }
+                            }
+                        }
+                    } else {
+                        for (int i = 0; i < inputStr.length(); i++) {
+                            num = Integer.parseInt(inputStr.substring(i, i + 1));
+                            if (num == 0)
+                            throw new IllegalArgumentException("0은 입력불가능합니다.");
+                            userInput.set(i, num);
+                        }
+                        for(int i = 0; i<userInput.size(); i++){
+                            for (int j = 0; j < i; j++) {
+                                if (userInput.get(i).equals(userInput.get(j))) {
+                                    throw new IllegalArgumentException("중복된 요소가 있습니다.");
+                                }
+                            }
+                        }          
                     }
-                } else {
-                    for (int i = 0; i < inputStr.length(); i++) {
-                        num = Integer.parseInt(inputStr.substring(i, i + 1));
-                        if (num == 0)
-                            throw new IllegalArgumentException("잘못된 입력으로 인해 게임을 종료합니다.");
-                        userInput.set(i, num);
+                }catch(NumberFormatException e){
+                        throw new IllegalArgumentException("숫자만 입력해주세요.");
                     }
-                }
                 return true;
             } else {
-                throw new IllegalArgumentException("잘못된 입력으로 인해 게임을 종료합니다.");
+                throw new IllegalArgumentException("3글자를 입력해주세요.");
             }
     }
 }
