dfe2b1b6342df1c4c076ddde40dc4edb5518f0e4
leesangjun <hubls@naver.com>
2023-10-22 02:57:57 +0900

    refactor(OutputView): 메서드 접근자 수정

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -9,7 +9,7 @@ public class OutputView {
 	private static final String SUCCESS_STRIKE_MESSAGE = "3스트라이크";
 	private static final String SUCCESS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
-	public void printStrikeAndBall(int ball, int strike) {
+	public static void printStrikeAndBall(int ball, int strike) {
 
 		if (ball != 0) {
 			System.out.print(ball + "볼 ");
