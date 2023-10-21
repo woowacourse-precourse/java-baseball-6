20cfa4532d821ddb6b3bb5bc54266864c36d6d9e
sungwon2598 <tjddnjs2598@gmail.com>
2023-10-21 17:47:27 +0900

    feat(Application) : 미션 목적에 맞게 Application에서 게임이 진행되도록 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,10 @@
 package baseball;
 
+import baseball.Controller.MainController;
+
 public class Application {
     public static void main(String[] args) {
-
+                MainController mainController = new MainController();
+                mainController.playGame();
     }
 }
