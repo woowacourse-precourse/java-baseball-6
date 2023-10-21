739c8d9062e2dd43b5a53b6e0564bb4ad2671fe0
HongChaa <jun761_1@naver.com>
2023-10-21 19:14:05 +0900

    refactor(GameView): 정답시 사용자가 게임을 새로시작/종료 선택지 문구 출력메서드 접근제한자 private으로 변경

--- src/main/java/baseball/view/GameView.java
+++ src/main/java/baseball/view/GameView.java
@@ -9,10 +9,13 @@ public class GameView {
     }
 
     public void printSuccessGameMessage() {
+
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료 ");
+        printOptionMessage();
+
     }
 
-    public void printOptionMessage() {
+    private void printOptionMessage() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
