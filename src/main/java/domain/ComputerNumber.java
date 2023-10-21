667d1ce32a72aa621ee0d8c4f0bb0f1df7dcf387
jum0624 <jum0624@naver.com>
2023-10-22 04:49:49 +0900

    feat: 랜덤 수 생성

--- src/main/java/domain/ComputerNumber.java
+++ src/main/java/domain/ComputerNumber.java
@@ -1,4 +1,31 @@
 package domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import validation.ValidationNumber;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ComputerNumber {
+    private List<Integer> computerNumber;
+
+    private ComputerNumber(List<Integer> computerNumber) {
+        this.computerNumber = computerNumber;
+    }
+
+    public static ComputerNumber getInstance(List<Integer> computerNumber) {
+        return new ComputerNumber(computerNumber);
+    }
+
+    public List<Integer> getRandomNumber() {
+        computerNumber = new ArrayList<>();
+        while (computerNumber.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumber.contains(randomNumber)) {
+                computerNumber.add(randomNumber);
+            }
+        }
+        ValidationNumber.validateNumbers(computerNumber);
+        return computerNumber;
+    }
 }
