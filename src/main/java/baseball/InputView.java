c3a0854a067b934cc504940b4fa2a381e6c55323
freekyr7529 <freekyr7529@gmail.com>
2023-10-21 21:41:46 +0900

    refactor: 불필요한 throws 제거

--- src/main/java/baseball/InputView.java
+++ src/main/java/baseball/InputView.java
@@ -10,7 +10,7 @@ public class InputView {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    public void inputGuideMsg() throws IllegalArgumentException {
+    public void inputGuideMsg() {
         System.out.print("숫자를 입력해주세요 : ");
     }
 
