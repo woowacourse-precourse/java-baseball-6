eefec7d4fb6266f4f0caa79ef2398b3d869a6033
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-10-21 23:01:04 +0900

    update: add makeResult

--- src/main/java/baseball/model/Result.java
+++ src/main/java/baseball/model/Result.java
@@ -1,9 +1,31 @@
 package baseball.model;
 
+import java.util.List;
+
 public class Result {
 
-    private final int strike;
-    private final int ball;
+    private int strike;
+    private int ball;
 
 
+    public Result(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+
+    public Result makeResult(List<Integer> computer, List<Integer> user) {
+
+        strike = 0;
+        ball = 0;
+
+        for (int i = 0; i < 3; i++) {
+            if (user.indexOf(i) == computer.indexOf(i)) {
+                strike += 1;
+            } else if (computer.contains(user.indexOf(i))) {
+                ball += 1;
+            }
+        }
+
+        return new Result(strike, ball);
+    }
 }
