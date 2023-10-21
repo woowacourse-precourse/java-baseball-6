04e2ed06cd1527a38cd93b8460f028fc239a7bef
dabin <db021129@naver.com>
2023-10-21 22:11:03 +0900

    CheckBall : 입력한 수에 대한 결과 제공드하는 메소

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println(RandomBall.makeRandomBall());
+        int click = 0;
+        while (click != 2) {
+            List<Integer> computerBall = RandomBall.makeRandomBall();
+            System.out.println(computerBall);
+            boolean loop = false;
+            while (!loop) {
+                String tempBall = Console.readLine();
+                loop = CheckBall.giveBallResult(tempBall, computerBall);
+            }
+        }
 
     }
 }
---
+++ src/main/java/baseball/CheckBall.java
@@ -0,0 +1,53 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CheckBall {
+
+    protected static boolean giveBallResult(String tempBall, List<Integer> rightBall) {
+
+        List<Integer> integerTempBall = Arrays.stream(tempBall.split(""))
+                .map(Integer::valueOf)
+                .collect(Collectors.toList());
+
+        int strike = 0;
+        int ball = 0;
+        boolean status = false;
+
+        for (int i = 0; i < rightBall.size(); i++) {
+            if (rightBall.get(i) == integerTempBall.get(i)) {
+                strike++;
+            }
+            if (rightBall.contains(integerTempBall.get(i))) {
+                ball++;
+            }
+        }
+
+        ball -= strike;
+
+        if (strike == 3) {
+            System.out.println("3스트라이크");
+            return true;
+        }
+
+        if (ball == 0 && strike == 0) {
+            System.out.println("낫싱");
+            return false;
+        }
+
+        if (ball != 0) {
+            System.out.format("%d볼 ", ball);
+        }
+
+        if (strike != 0) {
+            System.out.format("%d스트라이크", strike);
+        }
+
+        System.out.println();
+        return false;
+    }
+}
