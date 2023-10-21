166307299357b10167035488ce1f8f5d5e180d8f
yuseonjun <seonjun0906@gmail.com>
2023-10-21 19:10:23 +0900

    feat: Computer의 숫자를 가지고 있는 Computer 클래스 생성

---
+++ src/main/java/baseball/domain/Computer.java
@@ -0,0 +1,12 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Computer {
+
+	private final List<Integer> computerNumbers;
+
+	public Computer(List<Integer> computerNumbers) {
+		this.computerNumbers = computerNumbers;
+	}
+}
