c75fb39421808426e345ebce3f772faf1de8a9a4
민주 <hinote444@gmail.com>
2023-10-21 19:40:03 +0900

    refactor: domain 저장 기능을 controller로 이동

--- src/main/java/baseball/controller/controller.java
+++ src/main/java/baseball/controller/controller.java
@@ -1,4 +1,24 @@
 package baseball.controller;
 
+import baseball.domain.Computer;
+import baseball.domain.Player;
+import baseball.service.BaseballGame;
+import baseball.utils.Utils;
+import baseball.view.InputView;
+import java.util.List;
+
 public class controller {
+    private Computer computer = new Computer();
+    private Player player = new Player();
+    private BaseballGame baseballGame = new BaseballGame();
+
+    public void startGame() {
+        setUpBaseballGame();
+    }
+
+    public void setUpBaseballGame() {
+        computer.setBaseballNumbers(baseballGame.createComputerNumbers());
+        List<Integer> playerNumbers = Utils.parseIntegerList(InputView.inputBaseballNumbers());
+        player.setBaseballNumbers(playerNumbers);
+    }
 }
--- src/main/java/baseball/service/BaseballGame.java
+++ src/main/java/baseball/service/BaseballGame.java
@@ -1,28 +1,12 @@
 package baseball.service;
 
-import baseball.domain.Computer;
-import baseball.domain.Player;
-import baseball.utils.Utils;
-import baseball.view.InputView;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BaseballGame {
-    private Computer computer = new Computer();
-    private Player player = new Player();
 
-    public void startGame() {
-        setUpBaseballGame();
-    }
-
-    public void setUpBaseballGame() {
-        computer.setBaseballNumbers(createComputerNumbers());
-        List<Integer> playerNumbers = Utils.parseIntegerList(InputView.inputBaseballNumbers());
-        player.setBaseballNumbers(playerNumbers);
-    }
-
-    private List<Integer> createComputerNumbers() {
+    public List<Integer> createComputerNumbers() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
