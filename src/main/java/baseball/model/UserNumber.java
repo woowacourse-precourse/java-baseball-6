c471fa9eea88e16ae24e60845b848caf73b333ef
YongHyeon <pyh4616@naver.com>
2023-10-21 16:01:37 +0900

    Feat: UserNumber, 사용자 숫자 3자리가 서로 중복되지 않는지 검사하는 기능 구현

--- src/main/java/baseball/model/UserNumber.java
+++ src/main/java/baseball/model/UserNumber.java
@@ -1,6 +1,8 @@
 package baseball.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class UserNumber {
 
@@ -9,6 +11,7 @@ public class UserNumber {
     public UserNumber(List<Integer> userNumber) {
         validateSize(userNumber);
         validateContainZero(userNumber);
+        validateDuplicate(userNumber);
         this.userNumber = userNumber;
     }
 
@@ -20,6 +23,16 @@ public class UserNumber {
 
     private void validateContainZero(List<Integer> userNumber) {
         if (userNumber.contains(0)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplicate(List<Integer> userNumber) {
+        Set checkDuplication = new HashSet();
+        for (int i = 0; i < userNumber.size(); i++) {
+            checkDuplication.add(userNumber.get(i));
+        }
+        if (checkDuplication.size() != 3) {
             throw new IllegalArgumentException();
         }
     }
