647eefd000b8513174736561b357750511b463b3
GS_song <20003204@sju.ac.kr>
2023-10-22 01:17:04 +0900

    feat: 기본적인 Game을 관리하는 GameManager와 예측값과 정답을 비교환 결과를 받는 ResultScoreBoard 생성

---
+++ src/main/java/baseball/manager/GameManager.java
@@ -0,0 +1,19 @@
+package baseball.manager;
+
+import static baseball.constant.Constants.GAME_START_MESSAGE;
+
+public class GameManager {
+
+    public void play() {
+        // 게임 시작 메시지 출력
+        System.out.println(GAME_START_MESSAGE);
+
+        // Game 실행? init? -> random 값 생성
+
+        // 예측값 입력 메시지 출력, 입력 받기
+
+        // 입력받은 에측값을 사용해 Game에 있는 정답과 비교
+
+        // 비교받은 결과값 출력
+    }
+}
---
+++ src/main/java/baseball/model/ResultScoreBoard.java
@@ -0,0 +1,11 @@
+package baseball.model;
+
+public class ResultScoreBoard {
+    private final int strike;
+    private final int ball;
+
+    public ResultScoreBoard(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+}
