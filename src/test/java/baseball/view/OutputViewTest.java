ca91af0fe5f1ae162dfbd56770dcafaa07e1a63a
Hyun Woo <hwsa10041@gmail.com>
2023-10-21 21:08:48 +0900

    test : printResult() 테스트 추가 작성

--- src/test/java/baseball/view/OutputViewTest.java
+++ src/test/java/baseball/view/OutputViewTest.java
@@ -42,6 +42,32 @@ class OutputViewTest {
 
     @DisplayName("게임 한판 당 결과 메시지를 출력한다.")
     @Test
+    void printResultWithBall() {
+        // given
+
+        // when
+        outputView.printResult(2,0);
+
+        // then
+        String expectedOutput = "2볼\n";
+        assertThat(outContent.toString()).isEqualTo(expectedOutput);
+    }
+    @DisplayName("게임 한판 당 결과 메시지를 출력한다.")
+    @Test
+    void printResultWithStrike() {
+        // given
+
+        // when
+        outputView.printResult(0,2);
+
+        // then
+        String expectedOutput = "2스트라이크\n";
+        assertThat(outContent.toString()).isEqualTo(expectedOutput);
+    }
+
+
+    @DisplayName("게임 한판 당 결과 메시지를 출력한다.")
+    @Test
     void printResult() {
         // given
 
@@ -50,6 +76,19 @@ class OutputViewTest {
 
         // then
         String expectedOutput = "1볼 2스트라이크\n";
+        assertThat(outContent.toString()).isEqualTo(expectedOutput);
+    }
+
+    @DisplayName("게임 한판 당 결과 메시지를 출력한다.")
+    @Test
+    void printResultWithNothing() {
+        // given
+
+        // when
+        outputView.printResult(0,0);
+
+        // then
+        String expectedOutput = "낫싱\n";
         assertThat(outContent.toString()).isEqualTo(expectedOutput);
     }
 }
