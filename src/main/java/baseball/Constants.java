1b3a698fc52c6d39fb55a8252902abfd5b4750b4
suhyun9764 <suhyun9764@naver.com>
2023-10-21 20:29:18 +0900

    refactor:상수 클래스 생성

---
+++ src/main/java/baseball/Constants.java
@@ -0,0 +1,18 @@
+package baseball;
+
+public class Constants {
+
+    public static final int CONTINUE_NUMBER = 1;
+    public static final int STOP_NUMBER = 2;
+
+    public static final int THREE_STRIKE = 3;
+
+    public static final int BALL_NUMBER = 0;
+    public static final int STRIKE_NUMBER = 0;
+
+    public static final int DIGIT_NUMBER = 3;
+    public static final int RANGE_START_NUMBER = 1;
+    public static final int RANGE_END_NUMBER = 9;
+
+
+}
