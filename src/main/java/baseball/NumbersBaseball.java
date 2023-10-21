c38be663195e19dbed20e4307794df4a65cc9f82
alscjf1329 <alscjf1329@gmail.com>
2023-10-21 16:32:17 +0900

    Feat) finish 메소드 생성

--- src/main/java/baseball/NumbersBaseball.java
+++ src/main/java/baseball/NumbersBaseball.java
@@ -22,13 +22,20 @@ public class NumbersBaseball {
             String input = Console.readLine();
             validateInput(input);
             baseballInfo = getCompareResultWith(convertToIntArr(input));
+            System.out.println(baseballInfo);
             if (baseballInfo.isThreeStrike()) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                //finish();
+                finish();
                 return;
             }
-            System.out.println(baseballInfo);
+        }
+    }
+
+    public void finish() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = Console.readLine();
+        if (input.equals("1")) {
+            play();
         }
     }
 
