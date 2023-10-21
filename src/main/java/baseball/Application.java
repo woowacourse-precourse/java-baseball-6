c04adff6956630f3bf5618a8e64182295297ea10
jongmee <ra123221@gmail.com>
2023-10-21 21:47:43 +0900

    feat: 프로그램의 application 구현하기
    - 입, 출력을 담당한다.
    - 게임 상태에 따라 프로그램을 종료하거나 다시 시작한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,50 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameController gameController = new GameController();
+
+        System.out.print("숫자 야구 게임을 시작합니다.\n");
+        Long computerCodeId = gameController.gameStart();
+
+        boolean pass = false;
+        boolean restart = false;
+        Map<Grade, Integer> result;
+        while(!pass) {
+            System.out.print("숫자를 입력해주세요 : ");
+            String input = Console.readLine();
+            result = gameController.guessCode(input, computerCodeId);
+            pass = gameController.determineGameStatus(result);
+
+            printResult(result);
+
+            if(pass) {
+                System.out.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n");
+                System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+                String command = Console.readLine();
+                restart = gameController.determineRestarting(command);
+
+                gameController.initializeSetting(computerCodeId);
+            }
+
+            if(restart) {
+                computerCodeId = gameController.gameStart();
+                pass = false;
+                restart = false;
+            }
+        }
+    }
+
+    private static void printResult(Map<Grade, Integer> resultMap) {
+        if(resultMap.get(Grade.BALL) != 0)
+            System.out.print(resultMap.get(Grade.BALL)+"볼 ");
+        if(resultMap.get(Grade.STRIKE) != 0)
+            System.out.print(resultMap.get(Grade.STRIKE)+"스트라이크");
+        if(resultMap.get(Grade.BALL) == 0 && resultMap.get(Grade.STRIKE) == 0)
+            System.out.print("낫싱");
+        System.out.print("\n");
     }
 }
