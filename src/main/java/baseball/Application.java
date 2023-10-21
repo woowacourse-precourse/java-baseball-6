f09c0ae7ea005a47c94e83bd7778c140427211ca
U-keun Song <sukeun319@gmail.com>
2023-10-21 15:38:56 +0900

    Fix : 예외 테스트를 통과하지 않던 문제 수정
    기존 코드는 예외 테스트를 통과하지 못했었다. main 함수에서 예외를 던지도록 수정했는데, 테스트 코드에 대해서 공부를 좀 해봐야할 것 같다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,14 +1,9 @@
 package baseball;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalArgumentException {
         // TODO: 프로그램 구현
         Game game = Game.generate();
-        try {
-            game.start();
-        } catch (IllegalArgumentException e) {
-            System.out.println("잘못된 입력입니다. 애플리케이션을 종료합니다.");
-        }
-
+        game.start();
     }
 }
