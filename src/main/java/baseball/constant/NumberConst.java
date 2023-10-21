1ecdf97bc54b69db195558ee2aaf0e1eff37817a
배정현 <bae1004kin@gmail.com>
2023-10-21 19:53:57 +0900

    commit : 숫자 상수 클래스 생성 및 구현

---
+++ src/main/java/baseball/constant/NumberConst.java
@@ -0,0 +1,19 @@
+package baseball.constant;
+
+/**
+ * 숫자 상수 클래스
+ */
+public final class NumberConst {
+
+    public static final Integer RESTART_NUM = 1;
+
+    public static final Integer EXIT_NUM = 2;
+
+    public static final Integer INPUT_LENGTH = 3;
+
+    public static final Integer FIRST_RANGE = 1;
+
+    public static final Integer LAST_RANGE = 9;
+
+    public static final Integer MAX_STRIKE = 3;
+}
