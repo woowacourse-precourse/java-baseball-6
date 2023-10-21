11af0f3f2e3ffe0a7792df69ec1a4f8fabb94838
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-10-21 23:07:01 +0900

    update: add printNothing

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,4 +1,12 @@
 package baseball.view;
 
 public class OutputView {
+
+    private final static String NOTHING = "낫싱";
+
+    public void printNothing() {
+        System.out.println(NOTHING);
+    }
+
+
 }
