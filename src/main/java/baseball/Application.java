0f0f5f07f880026b2e1e6e5ca8090471445cfc93
shinel98 <75024473+shinel98@users.noreply.github.com>
2023-10-21 09:45:25 +0900

    feat: implemented the game repetition check feature

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,45 +10,46 @@ import java.util.Set;
 
 public class Application {
     public static void main(String[] args) {
-
-        List<Integer> computer = new ArrayList<>();
-        List<Integer> user = new ArrayList<>();
-
-        while(computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1,9);
-            if(!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
-        }
-
         System.out.println("숫자 야구 게임을 시작합니다.");
 
         while(true) {
-            System.out.print("숫자를 입력해주세요 : ");
-            String userInput = Console.readLine();
+            List<Integer> computer = new ArrayList<>();
+            List<Integer> user = new ArrayList<>();
 
-            if(!checkIsNumber(userInput)
-                    || checkIsDuplicated(userInput)
-                    || !checkInputLength(userInput)
-                    || checkIsZero(userInput)) {
-                throw new IllegalArgumentException();
+            while(computer.size() < 3) {
+                int randomNumber = Randoms.pickNumberInRange(1,9);
+                if(!computer.contains(randomNumber)) {
+                    computer.add(randomNumber);
+                }
             }
 
-            user = makeUserNumberList(userInput);
-            int strikeCount = checkStrike(user, computer);
-            int ballCount = checkBall(user, computer);
+            while(true) {
+                System.out.print("숫자를 입력해주세요 : ");
+                String userInput = Console.readLine();
 
-            if(strikeCount == 3) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                int endGame = Integer.parseInt(Console.readLine());
-
-                if(endGame == 1) {
-                    continue;
-                } else if (endGame == 2) {
-                    break;
-                } else {
+                if(!checkIsNumber(userInput)
+                        || checkIsDuplicated(userInput)
+                        || !checkInputLength(userInput)
+                        || checkIsZero(userInput)) {
                     throw new IllegalArgumentException();
+                }
+
+                user = makeUserNumberList(userInput);
+                int strikeCount = checkStrike(user, computer);
+                int ballCount = checkBall(user, computer);
+
+                if(strikeCount == 3) {
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                    int endGame = Integer.parseInt(Console.readLine());
+
+                    if(endGame == 1) {
+                        break;
+                    } else if (endGame == 2) {
+                        return;
+                    } else {
+                        throw new IllegalArgumentException();
+                    }
                 }
             }
         }
