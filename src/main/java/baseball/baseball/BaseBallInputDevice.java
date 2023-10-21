5c57e1d397f1140c39aea21c1237aef56e925061
BonSik <rnqhstlr2297@gmail.com>
2023-10-21 19:16:16 +0900

    feat: 야구 게임 입력기 종료 입력 메서드 구현

--- src/main/java/baseball/baseball/BaseBallInputDevice.java
+++ src/main/java/baseball/baseball/BaseBallInputDevice.java
@@ -9,8 +9,9 @@ public class BaseBallInputDevice {
   private static final int INPUT_SIZE = 3;
 
   public List<Integer> startInput() {
-    List<Integer> inputNumbers = new ArrayList<>();
+    System.out.print("숫자를 입력해주세요 : ");
 
+    List<Integer> inputNumbers = new ArrayList<>();
     String inputStr = Console.readLine();
     validateLength(inputStr);
 
@@ -25,6 +26,16 @@ public class BaseBallInputDevice {
     }
 
     return inputNumbers;
+  }
+
+  public int endInput() {
+    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+    String inputStr = Console.readLine();
+    int inputNumber = Integer.parseInt(inputStr);
+    validateEndNumber(inputNumber);
+
+    return inputNumber;
   }
 
   private void validateLength(String inputStr) {
@@ -42,6 +53,12 @@ public class BaseBallInputDevice {
   private void validateExistsNumber(int inputNumber, List<Integer> inputNumbers) {
     if (inputNumbers.contains(inputNumber)) {
       throw new IllegalArgumentException("서로 다른 숫자만 입력이 가능 합니다.");
+    }
+  }
+
+  private void validateEndNumber(int inputNumber) {
+    if (inputNumber != 1 && inputNumber != 2) {
+      throw new IllegalArgumentException("1 또는 2만 입력이 가능합니다.");
     }
   }
 
