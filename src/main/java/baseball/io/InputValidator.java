6ac56c365d22a61bd19f0eea5cc5b0c51dba8de9
김승진 <ohksj77@gmail.com>
2023-10-21 23:02:40 +0900

    feat(InputValidator): 재시작 입력값 검증 기능 구현

--- src/main/java/baseball/io/InputValidator.java
+++ src/main/java/baseball/io/InputValidator.java
@@ -4,6 +4,7 @@ public class InputValidator {
 
     private static final char MIN_NUMBER = '0';
     private static final char MAX_NUMBER = '9';
+    private static final int COMMAND_LENGTH = 1;
 
     public void validateNumber(final String input) {
         if (hasNonNumeric(input)) {
@@ -17,5 +18,15 @@ public class InputValidator {
 
     private boolean isNotNumeric(final int c) {
         return MIN_NUMBER > c || c > MAX_NUMBER;
+    }
+
+    public void validateRetryCommand(final String input) {
+        if (isProperCommand(input)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isProperCommand(final String input) {
+        return input.length() != COMMAND_LENGTH || hasNonNumeric(input);
     }
 }
