ab42597f8d8a4ceeee305160f0eaeffeb22c4218
KIVSH <kihongkim3@hotmail.com>
2023-10-21 20:45:19 +0900

    임의의 수 3개 결정 메소드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,7 @@ package baseball;
 
 import java.util.ArrayList;
 import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static class Result {
@@ -52,6 +53,12 @@ public class Application {
 
     public static List<Integer> generateRandomNumber() {
         List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
         return computer;
     }
 
