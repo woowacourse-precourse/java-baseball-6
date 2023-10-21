8dfda0681c1bad640ccd0208b712878639ddba0e
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-10-22 02:41:41 +0900

    update: add input not num exception

--- src/main/java/baseball/exception/InputException.java
+++ src/main/java/baseball/exception/InputException.java
@@ -1,4 +1,15 @@
 package baseball.exception;
 
-public class InputException {
+public enum InputException {
+    NOT_INTEGER("숫자가 아닌 값이 입력되었습니다.");
+
+    private final String exceptionMessage;
+
+    InputException(String exceptionMessage) {
+        this.exceptionMessage = exceptionMessage;
+    }
+
+    public String getExceptionMessage() {
+        return this.exceptionMessage;
+    }
 }
--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,4 +1,20 @@
 package baseball.view;
 
+import baseball.exception.InputException;
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    private final static String GET_GAME_NUM = "숫자를 입력해 주세요 : ";
+
+    public void getGameNum() {
+        System.out.println(GET_GAME_NUM);
+        String gameNum = Console.readLine();
+    }
+
+    public void validateNum(String line) {
+        if (!line.matches("\\d+")) {
+            throw new IllegalArgumentException(InputException.NOT_INTEGER.getExceptionMessage());
+        }
+    }
 }
