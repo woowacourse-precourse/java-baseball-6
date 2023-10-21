8b23693deec1372c2cb5839c11c94663a43d30d8
leesangjun <hubls@naver.com>
2023-10-21 16:53:55 +0900

    refactor(InputView): 재시작, 끝내기를 물어보는 기능 분리

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -6,9 +6,16 @@ import java.util.List;
 
 public class InputView {
 	private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+	private static final String RESTART_OR_END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
 	public static String inputNumber() {
 		System.out.print(INPUT_NUMBER_MESSAGE);
+		String number = Console.readLine();
+		return number;
+	}
+
+	public static String restartOrEndNumber() {
+		System.out.print(RESTART_OR_END_MESSAGE);
 		String number = Console.readLine();
 		return number;
 	}
