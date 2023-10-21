c826b936b88c5440aae9a7db6bebd236aa87baf3
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-10-21 18:02:06 +0900

    refactor: Revise User.java User 클래스의 메소드 리팩토링

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -5,22 +5,59 @@ import java.util.List;
 
 public class User {
 
-    private final List<Integer> user = new ArrayList<>();
+    private final List<Integer> user;
+    private String tokens[];
 
-    public void getIntArray(String input) {
-        String[] temp = input.split("");
-        for (String str : temp) {
-            if (!user.contains(Integer.parseInt(str))) {
-                user.add(Integer.parseInt(str));
-            }
+    public User() {
+        this.user = new ArrayList<>();
+    }
+
+    private void checkInputLength(String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("Invalid input length.");
         }
     }
 
-    public void clearList() {
+    private void checkInputHasZero(String input) {
+        if (input.contains("0")) {
+            throw new IllegalArgumentException("This input includes zero.");
+        }
+    }
+
+    private void checkInputHasChar(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("This input is not Integer: " + input);
+        }
+    }
+
+    private void checkInputString(String input) {
+        checkInputLength(input);
+        checkInputHasZero(input);
+        checkInputHasChar(input);
+    }
+
+    private void addNumber(int num) {
+        if (user.contains(num) || num < 1 || num > 9) {
+            throw new IllegalArgumentException("This is already contained: " + num);
+        }
+        user.add(num);
+    }
+
+    public void clearUser() {
         user.clear();
     }
 
-    public List<Integer> getList() {
+    public List<Integer> getUser() {
         return user;
+    }
+
+    public void setUser(String input) {
+        checkInputString(input);
+        tokens = input.split("");
+        for (String str : tokens) {
+            addNumber(Integer.parseInt(str));
+        }
     }
 }
