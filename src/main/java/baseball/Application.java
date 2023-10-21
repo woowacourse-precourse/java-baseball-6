ed33c40f6aa1c6c067bd9751bf34c94831132201
rbgusdlza <rbgusdlza@gmail.com>
2023-10-21 19:18:45 +0900

    전체 구현 완료 및 기타 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,22 +7,28 @@ import java.util.List;
 import java.util.Objects;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalArgumentException {
         int try_again = 1;
-        boolean is_it_correct = false;
+        boolean is_it_correct;
         System.out.println("숫자 야구 게임을 시작합니다.");
         while (try_again == 1) {
             List<Integer> computerNum = new ArrayList<>();
             getComputerNum(computerNum);
-            while (!is_it_correct) {
+            while (true) {
                 List<Integer> userInputNum = new ArrayList<>();
                 System.out.print("숫자를 입력해주세요 : ");
                 String userInputString = Console.readLine();
                 checkUserInput(userInputString);
                 getUserInput(userInputString, userInputNum);
                 is_it_correct = compareUserWithCom(userInputNum, computerNum);
+                if (is_it_correct) {
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    break;
+                }
             }
-            try_again = 2;
+            // Check if user wants to play the game again.
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            try_again = Integer.parseInt(Console.readLine());
         }
     }
 
@@ -72,18 +78,21 @@ public class Application {
         for (int i = 0; i < 3; i++) {
             if (Objects.equals(userInputNum.get(i), computerNum.get(i))) {
                 strike_count++;
-            } else {
+            } else if (computerNum.contains(userInputNum.get(i))) {
                 ball_count++;
             }
         }
         // Print out the game result.
-        if (ball_count == 0) {
-            System.out.printf("%d스트라이크\n", strike_count);
-        } else if (strike_count == 0) {
+        if (ball_count == 0 && strike_count == 0) {
+            System.out.println("낫싱");
+        } else if (ball_count != 0 && strike_count == 0) {
             System.out.printf("%d볼\n", ball_count);
+        } else if (ball_count == 0 && strike_count != 0) {
+            System.out.printf("%d스트라이크\n", strike_count);
         } else {
             System.out.printf("%d볼 %d스트라이크\n", ball_count, strike_count);
         }
+
         // Check if strike count is 3
         if (strike_count == 3) {
             is_it_correct = true;
