2c1896851d195962c404064075e284407bee3957
jongmyeongbak <withpleasure@gmail.com>
2023-10-21 16:22:20 +0900

    feat: Add generateNumbers method

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,4 +1,18 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Computer {
+    public static List<Integer> generateNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+        return numbers;
+    }
 }
