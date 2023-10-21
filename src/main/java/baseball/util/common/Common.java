087d8899302528c03609bdbc3b8017e4c51f670a
minsang-alt <mazinger97@naver.com>
2023-10-21 22:33:46 +0900

    feat: 공통으로 쓰이는 상수를 클래스로 분리
    - 무작위의 숫자의 범위를 결정할 시작과 끝 상수
    - 게임의 자리수 상수

---
+++ src/main/java/baseball/util/common/Common.java
@@ -0,0 +1,10 @@
+package baseball.util.common;
+
+public class Common {
+
+    public static final int START_INCLUSIVE = 1;
+    public static final int END_INCLUSIVE = 9;
+    public static final int NUM_LENGTH = 3;
+
+
+}
