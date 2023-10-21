03356eec523151ea79b18e498efe514f0fffdf64
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-10-21 21:18:44 +0900

    10/21 (토) commit test

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,7 +3,7 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("hello");
+        System.out.println("helloo");
 
     }
 }
