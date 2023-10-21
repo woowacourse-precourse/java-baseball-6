dfef02ec7bd76ba75f92e4272548609b9aee83a4
Mr.Kim <joajy98@gmail.com>
2023-10-21 22:40:22 +0900

    feat: 모든 예외사항 한 번에 처리

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -16,6 +16,12 @@ public class Validator {
         }
     }
 
+    public static void checkAllException(String number){
+        checkNumberLength(number);
+        checkDuplicatedNumber(number);
+        checkNumberType(number);
+    }
+
     public static void checkNumberLength(String number) {
         // 입력받은 값의 길이가 3이 아닐 경우
         if (number.length() != Constant.BALL_SIZE) {
