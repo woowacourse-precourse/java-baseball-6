c117b05325cfd537e8b3621031660a67060e7610
chaeshee0908 <chaeshee0908@naver.com>
2023-10-22 01:56:02 +0900

    [feat] 사용자 숫자 리스트 형변환 기능 구현

--- src/main/java/baseball/UserNumber/UserNumber.java
+++ src/main/java/baseball/UserNumber/UserNumber.java
@@ -1,20 +1,37 @@
-package baseball.UserNumber;
+package baseball.userNumber;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserNumber {
 
-    public void validateInputNumber(String input) {
+    public static void validateInputNumber(String input) {
         if (input.length() != 3 || !input.matches("\\d+")) {
             throw new IllegalArgumentException("유효한 숫자가 아닙니다.");
         }
-
     }
 
-    public String getUserNumber() {
+    private static List<Integer> toIntegerList(String input) {
+        List<Integer> randomNumber = new ArrayList<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (Character.isDigit(c)) {
+                randomNumber.add(Integer.valueOf(c));
+            }
+        }
+
+        return randomNumber;
+    }
+
+    public static List<Integer> getUserNumber() {
         System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
+        validateInputNumber(input);
 
-        return input;
+        return toIntegerList(input);
     }
+
 }
