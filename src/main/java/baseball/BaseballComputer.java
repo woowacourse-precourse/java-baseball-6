d3f770d4a62991941b6de69b255bde8c884cd5a1
jungeun-choi <jungeun5.choi@gmail.com>
2023-10-22 00:23:13 +0900

    comment: 주석 수정

--- src/main/java/baseball/BaseballComputer.java
+++ src/main/java/baseball/BaseballComputer.java
@@ -7,7 +7,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 /* 1에서 9까지 서로 다른 임의의 수 3개의 숫자 생성 */
 public class BaseballComputer {
-	// 컴퓨터 숫자 세자리 생성
+	// HashSet<Integer> 타입을 String 타입으로 변환 (숫자 게임에 사용)
 	public String createComputerNumbers() {
 		HashSet<Integer> computerNumbers = pickComputerNumbers();
 		StringBuilder str = new StringBuilder();
@@ -17,7 +17,7 @@ public class BaseballComputer {
 		return str.toString();
 	}
 
-	// 랜덤으로 1~9까지의 숫자 생성
+	// 랜덤으로 1~9 사이의 임의의 숫자 생성 (중복 x)
 	private HashSet<Integer> pickComputerNumbers() {
 		HashSet<Integer> computerNumbers = new LinkedHashSet<>(); // 중복 방지용 hashset
 		while (computerNumbers.size() < 3) {
