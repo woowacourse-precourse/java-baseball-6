bf28a09c5974040a3acd2fd2320768b323a73c46
dlqudtjs <dlqudtjs922@gmail.com>
2023-10-22 03:15:49 +0900

    feat: Player 모델에 플레이어 숫자 설정 기능 추가

--- src/main/java/baseball/model/Player.java
+++ src/main/java/baseball/model/Player.java
@@ -1,12 +1,18 @@
 package baseball.model;
 
+import baseball.view.InputView;
+
 import java.util.List;
 
 public class Player {
 
+    private final InputView inputView;
+    private final Util util;
     private List<Integer> numbers;
 
     public Player() {
+        this.inputView = new InputView();
+        this.util = new Util();
     }
 
     public void setNumbers(List<Integer> numbers) {
@@ -15,5 +21,12 @@ public class Player {
 
     public List<Integer> getNumbers() {
         return numbers;
+    }
+
+    public void setPlayerNumbers() {
+        String input = inputView.inputPlayerNumber();
+        List<Integer> inputNumbers = util.parseToNumberList(input);
+
+       setNumbers(inputNumbers);
     }
 }
