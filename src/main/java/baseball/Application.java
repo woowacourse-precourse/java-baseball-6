9b25d851952743c7f81071c1806e3a286c39b7d0
BangMingGing <rnrmfbmk802@gmail.com>
2023-10-22 01:13:34 +0900

    add game start function and setNewAnswer function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,31 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        Game game = new Game();
+        game.startGame();
+    }
+}
+
+class Game {
+    private List<Integer> answer;
+    public void startGame() {
+        System.out.println(this.answer);
+    }
+
+    private void setNewAnswer() {
+        this.answer = new ArrayList<>();
+        while (this.answer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!this.answer.contains(randomNumber)) {
+                this.answer.add(randomNumber);
+            }
+        }
     }
 }
