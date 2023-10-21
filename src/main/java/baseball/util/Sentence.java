88b0fd5aa890fb3c2ec68f0a0f2db69ceaa64cd6
minisyu <iswater.kim@gmail.com>
2023-10-21 18:29:23 +0900

    refactor(Sentence): 출력 메시지와 에러 메시지 분리

--- src/main/java/baseball/util/Sentence.java
+++ src/main/java/baseball/util/Sentence.java
@@ -4,11 +4,7 @@ public enum Sentence {
     GAME_START_MESSAGE("숫자 야구 게임을 시작합니다."),
     INPUT_NUMBER_MESSAGE("숫자를 입력해주세요 : "),
     GAME_SUCCESS_MESSAGE("3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
-    GAME_RESTART_EXIT_MESSAGE("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."),
-    ERROR_NUMBERS_DUPLICATION_MESSAGE("[ERROR] 서로 다른 숫자를 입력해주세요."),
-    ERROR_NOT_THREE_SIZE_MESSAGE("[ERROR] 3자리의 숫자를 입력해주세요."),
-    ERROR_GAME_ACTION_INPUT_MESSAGE("[ERROR] 게임 재실행 또는 종료에 대한 입력 값은 1 또는 2만 가능합니다."),
-    ERROR_INPUT_NOT_NUMBERS_MESSAGE("[ERROR] 숫자만 입력할 수 있습니다.");
+    GAME_RESTART_EXIT_MESSAGE("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
     private final String message;
 
