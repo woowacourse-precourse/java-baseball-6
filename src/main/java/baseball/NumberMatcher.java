4bfea8693a222fd7db3aee2eb819adcaece869eb
kim <hwiyul_g@daum.net>
2023-10-21 19:38:15 +0900

    feat(NumberMatcher) : 정답과 입력을 비교하고 결과를 표하는 함수 추가

---
+++ src/main/java/baseball/NumberMatcher.java
@@ -0,0 +1,23 @@
+package baseball;
+
+import baseball.Model.GameResult;
+import java.util.List;
+
+public class NumberMatcher {
+    private static final int NUMBER_SIZE = 3;
+
+    public GameResult match(List<Integer> userNumber, List<Integer> answerNumber){
+        GameResult result = new GameResult();
+
+        for(int i = 0; i < NUMBER_SIZE; i++){
+            if(userNumber.get(i).equals(answerNumber.get(i))){
+                result.incrementStrike();
+            }else if(answerNumber.contains(userNumber.get(i))){
+                result.incrementBall();
+            }
+        }
+
+        return result;
+    }
+
+}
