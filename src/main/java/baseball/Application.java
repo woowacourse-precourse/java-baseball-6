c9514cdc585b4b6d6ff490e1c53d2c6da12702bc
육예진 <yngbao@yug-yejin-ui-MacBookAir.local>
2023-10-21 22:21:13 +0900

    feat:정답과 인풋의 값/위치 일치여부에 따라 볼/스트라이크 개수 확인

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -14,31 +14,40 @@ public class Application
         
     	System.out.println("숫자 야구 게임을 시작합니다.");
     	
-    	List<Integer> computer = new ArrayList<>();
-    	while (computer.size() < 3) 
-    	{
-    	    int randomNumber = Randoms.pickNumberInRange(1, 9);
-    	    if (!computer.contains(randomNumber)) 
-    	    {
-    	        computer.add(randomNumber);
-    	    }
-    	}
+    	
+    	List<Integer> computer = makeThreeRandomNum();
+    	System.out.println(computer);
     	
     	System.out.print("숫자를 입력해주세요 : ");
     	String userInput = Console.readLine();
     	Console.close();
+    	inputLengthThree(userInput);
+    	inputNumberOnly(userInput);
+    	inputAllDistinct(userInput);
     	
-    	if (userInput.length() != 3)
+		int strike = 0;
+    	int ball = 0;
+    	for (int i = 0; i < 3; i++) 
     	{
-            throw new IllegalArgumentException();
-        }
+    		char currentNum = userInput.charAt(i);
+    		int num = Character.getNumericValue(currentNum);
+    		if(computer.contains(num)) 
+    		{
+    			if(num == computer.get(i)) 
+    			{
+    				strike = strike + 1;
+    			} 
+    			else 
+    			{
+    				ball = ball + 1;
+    			}
+    		}
+    	}
     	
-    	if (!userInput.matches("\\d{3}")) 
-    	{
-            throw new IllegalArgumentException();
-        }
-    	
-    	Set<Character> set = new HashSet<>();
+    }
+
+	public static void inputAllDistinct(String userInput) {
+		Set<Character> set = new HashSet<>();
 
         for (int i = 0; i < userInput.length(); i++) // 중복된 값을 제외하고 입력값 전부를 배열에 추가 
         {
@@ -48,7 +57,36 @@ public class Application
         {
             throw new IllegalArgumentException();
         }
-    	
-    }
+	}
+
+	public static void inputNumberOnly(String userInput) 
+	{
+		if (!userInput.matches("\\d{3}")) // 입력값에 숫자가 아닌 문자가 포함된 경우
+    	{
+            throw new IllegalArgumentException();
+        }
+	}
+
+	public static void inputLengthThree(String userInput) 
+	{
+		if (userInput.length() != 3)
+    	{
+            throw new IllegalArgumentException();
+        }
+	}
+
+	public static List<Integer> makeThreeRandomNum() 
+	{
+		List<Integer> RandomNumbers = new ArrayList<>();
+    	while (RandomNumbers.size() < 3) 
+    	{
+    	    int Num = Randoms.pickNumberInRange(1, 9);
+    	    if (!RandomNumbers.contains(Num)) 
+    	    {
+    	    	RandomNumbers.add(Num);
+    	    }
+    	}
+    	return RandomNumbers;
+	}
 
 }
