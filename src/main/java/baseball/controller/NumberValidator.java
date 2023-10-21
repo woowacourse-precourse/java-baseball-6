b038c89001323df83fbcd3c524203680a61ce858
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-10-21 19:31:51 +0900

    feat(NumberValidator) : isValidNumber 구현
    3자리 수인지? 1-9사이 정수인지? 중복되지 않는지 3가지를 검사하고, 실패할 경우 IllegalArgumentException와 적절한 에러메세지를 출력하는 메소드

--- src/main/java/baseball/controller/NumberValidator.java
+++ src/main/java/baseball/controller/NumberValidator.java
@@ -8,6 +8,9 @@ public class NumberValidator {
     private static final int BASEBALL_GAME_NUMBER_LENGTH = 3;
     private static final char MIN_DIGIT = '1';
     private static final char MAX_DIGIT = '9';
+    private static final String WRONG_LENGTH_THROW_MESSAGE = "세 자리 숫자를 입력하세요.";
+    private static final String WRONG_RANGE_THROW_MESSAGE = "1-9 사이의 정수를 입력하세요.";
+    private static final String DUPLICATE_INPUT_MESSAGE = "중복되지 않는 서로 다른 숫자를 입력해주세요.";
 
     public boolean isTreeLengthLetter(String playerInput) {
         return playerInput.length() == BASEBALL_GAME_NUMBER_LENGTH;
@@ -31,5 +34,16 @@ public class NumberValidator {
             seenChar.add(playerInput.charAt(i));
         }
         return false;
+    }
+
+    public boolean isValidNumber(String playerInput) {
+        if (!isTreeLengthLetter(playerInput)) {
+            throw new IllegalArgumentException(WRONG_LENGTH_THROW_MESSAGE);
+        } else if (!isAllDigit(playerInput)) {
+            throw new IllegalArgumentException(WRONG_RANGE_THROW_MESSAGE);
+        } else if (hasDuplicateChars(playerInput)) {
+            throw new IllegalArgumentException(DUPLICATE_INPUT_MESSAGE);
+        }
+        return true;
     }
 }
