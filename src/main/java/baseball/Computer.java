641ce95b3d2fd34167980a2571886ca30b306158
H-server <hani103201@naver.com>
2023-10-21 11:45:56 +0900

    refactor: 컴퓨터 랜덤 숫자 생성 메소드 변경

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,2 +1,23 @@
-package baseball;public class Computer {
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private List<Integer> computerNum = new ArrayList<>();
+
+    public void setComputerNum() {
+        while (computerNum.size() < 3) {
+            int randomNum = Randoms.pickNumberInRange(1, 9);
+            if (!computerNum.contains(randomNum)) {
+                computerNum.add(randomNum);
+            }
+        }
+        System.out.println(computerNum);
+    }
+
+    public List<Integer> getComputerNum() {
+        return computerNum;
+    }
 }
--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -1,4 +1,23 @@
-import static org.junit.jupiter.api.Assertions.*;
+package baseball;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
 class ComputerTest {
-  
+
+    @Test
+    void testSetComputerNumTest() {
+        Computer computer = new Computer();
+        computer.setComputerNum();
+        List<Integer> computerNumbers = computer.getComputerNum();
+
+        assertEquals(3, computerNumbers.size());
+
+        assertTrue(computerNumbers.get(0) != computerNumbers.get(1));
+        assertTrue(computerNumbers.get(0) != computerNumbers.get(2));
+        assertTrue(computerNumbers.get(1) != computerNumbers.get(2));
+    }
 }
