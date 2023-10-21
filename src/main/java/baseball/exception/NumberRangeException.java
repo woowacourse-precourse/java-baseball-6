9d9d3ac30d94aa7ddb1e70a69de82774fd2766c9
Lieslot <rlaxodud1999@gmail.com>
2023-10-21 18:10:15 +0900

    [feat] Number에 예외처리 로직 기능 구현

--- src/main/java/baseball/exception/NumberRangeException.java
+++ src/main/java/baseball/exception/NumberRangeException.java
@@ -9,7 +9,8 @@ public class NumberRangeException extends IllegalArgumentException {
 
     private static final String ERROR_MESSAGE = String.format("숫자는 %d에서 %d사이여야 합니다.", MIN_VALUE.showValue(), MAX_VALUE.showValue());
 
-    public NumberRangeException(String s) {
-        super(s);
+    public NumberRangeException() {
+
+        super(ERROR_MESSAGE);
     }
 }
--- src/main/java/baseball/vo/Number.java
+++ src/main/java/baseball/vo/Number.java
@@ -1,5 +1,11 @@
 package baseball.vo;
 
+import static baseball.enums.NumbersEnum.MAX_VALUE;
+import static baseball.enums.NumbersEnum.MIN_VALUE;
+
+import baseball.enums.NumbersEnum;
+import baseball.exception.NumberRangeException;
+
 public class Number {
 
     private int number;
@@ -11,7 +17,9 @@ public class Number {
 
 
     private void checkRange(int number) {
-
+        if (number < MIN_VALUE.showValue() || number > MAX_VALUE.showValue()) {
+            throw new NumberRangeException();
+        }
     }
 
 
