62499781f2465e062be5255b3044709a50c9129e
kimdohyung <pdohyung@gmail.com>
2023-10-22 02:06:18 +0900

    feat: 사용자의 입력마다 해당하는 결과 출력하는 기능 구현

--- src/main/java/baseball/controller/Play.java
+++ src/main/java/baseball/controller/Play.java
@@ -4,6 +4,7 @@ import baseball.model.ComputerNumber;
 import baseball.model.Score;
 import baseball.model.UserNumber;
 import baseball.view.InputView;
+import baseball.view.OutputView;
 
 public class Play {
     private static final int STRIKE_THREE = 3;
@@ -19,10 +20,36 @@ public class Play {
         while (true) {
             userNumber.newUserNumber(InputView.userNumberInput());
             score.countScore(userNumber.getUserNumber(), computerNumber.getComputerNumber());
+            printResult(score);
             if (score.getStrike() == STRIKE_THREE) {
                 break;
             }
             score.resetScore();
+        }
+    }
+
+    private void printResult(Score score) {
+        int strike = score.getStrike();
+        int ball = score.getBall();
+
+        if (strike == 0 && ball == 0) {
+            OutputView.nothing();
+            return;
+        }
+
+        if (strike == 0) {
+            OutputView.onlyBall(ball);
+            return;
+        }
+
+        if (ball == 0) {
+            OutputView.onlyStrike(strike);
+            return;
+        }
+
+        if (strike > 0 && ball > 0) {
+            OutputView.strikeAndBall(strike, ball);
+            return;
         }
     }
 }
