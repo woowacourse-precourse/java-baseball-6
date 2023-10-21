91425e83e4ba8dad41d8b10723867c4ef1e51f71
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-10-22 03:41:57 +0900

    test: add addition test of baseball referee

--- src/test/java/baseball/BaseballRefereeTest.java
+++ src/test/java/baseball/BaseballRefereeTest.java
@@ -28,8 +28,37 @@ class BaseballRefereeTest {
     }
 
     @Test
-    @DisplayName("")
+    @DisplayName("스트라이크 개수 테스트")
     void countStrikeTest(){
+        List<Integer> actual = List.of(1, 2, 3);
+        List<Integer> expect = List.of(1, 4, 3);
 
+        int[] result = baseballReferee.scoreInput(expect, actual);
+
+        assertEquals(2, result[1]);
+    }
+
+    @Test
+    @DisplayName("볼과 스트라이크 개수 테스트")
+    void countBallAndStrikeTest(){
+        List<Integer> actual = List.of(1, 2, 3);
+        List<Integer> expect = List.of(1, 3, 4);
+
+        int[] result = baseballReferee.scoreInput(expect, actual);
+
+        assertEquals(1, result[0]);
+        assertEquals(1, result[1]);
+    }
+
+    @Test
+    @DisplayName("낫싱 테스트")
+    void countNoBallAndStrikeTest(){
+        List<Integer> actual = List.of(1, 2, 3);
+        List<Integer> expect = List.of(4, 5, 6);
+
+        int[] result = baseballReferee.scoreInput(expect, actual);
+
+        assertEquals(0, result[0]);
+        assertEquals(0, result[1]);
     }
 }
