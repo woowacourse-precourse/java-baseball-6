d04300c3af2bb2e2adce2cf31b02fa24b7314443
hyuna <monicajo0@naver.com>
2023-10-22 00:46:50 +0900

    clean: Application 코드 정리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,13 +1,6 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import camp.nextstep.edu.missionutils.Console;
-
-import java.util.ArrayList;
-
 public class Application {
-    private static ArrayList<Integer> answer;
-
     public static void main(String[] args) {
         BaseballGame baseballGame = new BaseballGame();
         baseballGame.start();
