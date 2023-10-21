e35a9372e32e38e910f679680b5aa5d8bfa478aa
Dendrog <goodshs335@gmail.com>
2023-10-22 04:17:49 +0900

    Feat: 숫자 입력 기능 추가(상세 처리 미구현)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
+        Player a = new Player();
+        System.out.println(a.getNumbers());
         // TODO: 프로그램 구현
     }
 }
--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,4 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
+
+    List<Integer> saveThreeNumbers = new ArrayList<>(3);
+
+    public List<Integer> getNumbers() {
+        String inputText = Console.readLine();
+
+        for (int i = 0; i < 3; i++) {
+            char convertChar = inputText.charAt(i);
+            int convertInt = Character.getNumericValue(convertChar);
+            saveThreeNumbers.add(convertInt);
+        }
+
+        return saveThreeNumbers;
+    }
 }
