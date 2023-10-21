d2772f0e68091bfc10d8799aa088bd46c2151061
Jung Yunho <a01049048063@gmail.com>
2023-10-21 20:38:48 +0900

    refactor(Controller): Controller 메서드 분리

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -1,20 +1,46 @@
 package baseball;
 
 public class Controller {
+
+    private static final String GAME_RESTART = "1";
+    private static final String GAME_END = "2";
+
     private boolean isGameFinished = false;
+
     public void play() {
         Player player = new Player();
-
         Inputview.startGame();
-        while(!isGameFinished) {
+        while (true) {
+            GameCompare gameCompare = new GameCompare();
+            playGame(player, gameCompare);
+            String inputNumber = Inputview.reStart();
+            if (isReStart(inputNumber)) {
+                break;
+            }
+        }
+    }
+
+    private boolean isReStart(String inputNumber) {
+        if (inputNumber.equals(GAME_RESTART)) {
+            isGameFinished = false;
+        }
+        if (inputNumber.equals(GAME_END) ) {
+            return true;
+        }
+        return false;
+    }
+
+    private void playGame(Player player, GameCompare gameCompare) {
+        while (!isGameFinished) {
             String inputNumber = Inputview.inputNumber();
             player.updateNumbers(inputNumber);
-            GameCompare gameCompare = new GameCompare();
             boolean endGameSign = gameCompare.updateGameCompare(player);
             String result = gameCompare.showResult();
             OutputView.showResult(result);
             isGameFinished = endGameSign;
-            Inputview.reStart();
+            if (Boolean.TRUE.equals(endGameSign)) {
+                break;
+            }
         }
     }
 }
