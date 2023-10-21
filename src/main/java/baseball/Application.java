ddc82c5eaf40a80aa4dbb30551841d07c29386cb
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-10-21 17:23:09 +0900

    feat : Update main application
    - make sharedScanner for userInput, userDecision
    - continue playing game if user decision is 1
    - if user decision is 2, end game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import java.util.Scanner;
+
+import static baseball.EndGame.endGame;
+import static baseball.PlayGame.playGame;
+import static baseball.StartGame.startGame;
+import static baseball.UserDecision.userDecision;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Scanner sharedScanner = new Scanner(System.in);
+
+        startGame();
+        String userDecision;
+
+        do {
+            playGame(sharedScanner);
+            userDecision = userDecision(sharedScanner);
+        } while (userDecision.equals("1"));
+
+        endGame();
     }
 }
