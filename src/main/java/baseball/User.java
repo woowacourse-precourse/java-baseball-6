188c68146d19c63f104dc6e28abbd1ae50992447
unifolio0 <boho82ha@gmail.com>
2023-10-21 19:15:03 +0900

    숫자 입력 기능 구현

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,9 +1,18 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 import java.util.List;
 
 public class User {
     List<Integer> user;
 
-    
+    public void inputNumber() {
+        user = new ArrayList<>();
+        System.out.print("숫자를 입력해주세요 : ");
+        String[] numbers = Console.readLine().split("");
+        for(int i=0;i<numbers.length;i++) {
+            user.add(Integer.parseInt(numbers[i]));
+        }
+    }
 }
