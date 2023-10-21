c35d0a838e8a6d86cb9985903b4eb0e90b66a3e0
박성춘 <songchon100@gmail.com>
2023-10-21 17:40:44 +0900

    feat : validator 구현

--- src/main/java/baseball/util/Validator.java
+++ src/main/java/baseball/util/Validator.java
@@ -1,4 +1,18 @@
 package baseball.util;
 
 public abstract class Validator {
+
+    public abstract void validate(String inputNum) throws IllegalArgumentException;
+
+    // 공백 제거
+    static String removeSpace(String inputNum) {
+        return inputNum.replace(" ", "");
+    }
+
+    static void validateEmptyInput(String inputNum) {
+        if(inputNum.isEmpty()) {
+            throw new IllegalArgumentException("Exception Message : 공백을 입력하지 마세요.");
+        }
+    }
+
 }
