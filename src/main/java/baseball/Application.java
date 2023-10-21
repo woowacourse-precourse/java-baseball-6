a25cc7aeccb7494209604e968d8dd867ede7fe0b
Uniaut <kunwoo0927@gmail.com>
2023-10-22 00:50:52 +0900

    refactor: optimize imports

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,10 +3,7 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Application {
     static boolean askRestart() {
