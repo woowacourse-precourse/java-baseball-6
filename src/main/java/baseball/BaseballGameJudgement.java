e67cc93f60b2b0a762e6e5d97152a0071ad46ad3
chanhoim <cksgh201@naver.com>
2023-10-21 19:22:46 +0900

    Feat : 채점 결과 출력
    결과에 볼이 k(1~3)개라면 k볼 을 출력하고 스트라이크가 k(1~3)개라면 k스트라이크를 출력한다.
    볼과 스트라이크가 모두 0개보다 많다면 볼을 먼저 출력하고 한칸을 띄워 스트라이크를 출력한다.
    볼과 스트라이크가 모두 0개라면 낫싱을 출력한다.

--- src/main/java/baseball/BaseballGameJudgement.java
+++ src/main/java/baseball/BaseballGameJudgement.java
@@ -13,4 +13,18 @@ public class BaseballGameJudgement {
         this.strike++;
     }
 
+    public void printJudgement() {
+        StringBuilder sb = new StringBuilder();
+        if(ball != 0) {
+            sb.append(ball).append("볼 ");
+        }
+        if(strike != 0) {
+            sb.append(strike).append("스트라이크 ");
+        }
+        if(sb.isEmpty()) {
+            sb.append("낫싱");
+        }
+        System.out.println(sb);
+    }
+
 }
