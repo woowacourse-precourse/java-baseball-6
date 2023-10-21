4bfecab9a97f54bad541863286585f42c51548c2
ssuzyn <asj7090@gmail.com>
2023-10-21 15:44:53 +0900

    feat: 숫자야구 게임 초기화 및 사용자 입력값 검증 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,70 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    static List<Integer> computer = new ArrayList<>();
+    static List<Integer> user = new ArrayList<>();
+    private static void setGame(){
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+                System.out.println("컴퓨터 숫자 : " + randomNumber);
+            }
+        }
+    }
+
+    private static boolean isValidInput(String input){
+        if (input.length() != 3) {
+            return false;
+        }
+
+        char[] chars = input.toCharArray();
+        if (chars[0] == chars[1] || chars[1] == chars[2] || chars[0] == chars[2]) {
+            return false;
+        }
+
+        for (char c : chars) {
+            if (c < '1' || c > '9') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
+        setGame();
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(true){
+            try{
+                System.out.print("숫자를 입력해주세요 : ");
+                String input = br.readLine();
+
+                if(isValidInput(input)){
+                    for(char c : input.toCharArray()){
+                        user.add(Integer.parseInt(String.valueOf(c)));
+                    }
+                }
+                else{
+                    throw new IllegalArgumentException("잘못된 입력입니다. 게임 종료");
+                }
+
+            }catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                break;
+            }
+
+
+
+        }
     }
 }
