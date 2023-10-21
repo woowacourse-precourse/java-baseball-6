16c1e684931cf1a489f533a38e3c19c5f60efbb4
sangwopark19 <sangwopark19@gmail.com>
2023-10-21 17:14:16 +0900

    스트라이크와 볼을 리턴하는 메서드

--- src/main/java/baseball/domain/BallHint.java
+++ src/main/java/baseball/domain/BallHint.java
@@ -8,4 +8,12 @@ public class BallHint {
     public boolean isNothing() {
         return strike + ball == 0;
     }
+
+    public int getStrike() {
+        return this.strike;
+    }
+
+    public int getBall() {
+        return this.ball;
+    }
 }
