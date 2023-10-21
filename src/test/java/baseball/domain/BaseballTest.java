426a726bf3e77e3820879b291d726a91aa6bebe9
jinny-l <jinny.l.dev@gmail.com>
2023-10-21 19:11:44 +0900

    💡comment: given, when, then 주석 추가

--- src/test/java/baseball/domain/BaseballTest.java
+++ src/test/java/baseball/domain/BaseballTest.java
@@ -13,15 +13,23 @@ class BaseballTest {
     @DisplayName("정상 상황일 때 숫자 야구가 생성된다.")
     @Test
     void create_baseball() {
+        // given
         List<Integer> numbers = List.of(1, 2, 3);
+
+        // when
         Baseball baseball = new Baseball(numbers);
+
+        // then
         Assertions.assertThat(baseball).isNotNull();
     }
 
     @DisplayName("길이가 3이상일 때 예외가 발생한다.")
     @Test
     void throw_exception_when_length_exceeds_than_expected() {
+        // given
         List<Integer> numbers = List.of(1, 2, 3, 4);
+
+        // when & then
         assertThatThrownBy(() ->
                 new Baseball(numbers)).isInstanceOf(IllegalArgumentException.class);
     }
@@ -29,7 +37,10 @@ class BaseballTest {
     @DisplayName("길이가 3미만일 때 예외가 발생한다.")
     @Test
     void throw_exception_when_length_less_than_expected() {
+        // given
         List<Integer> numbers = List.of(1, 2);
+
+        // when & then
         assertThatThrownBy(() ->
                 new Baseball(numbers)).isInstanceOf(IllegalArgumentException.class);
     }
@@ -37,7 +48,10 @@ class BaseballTest {
     @DisplayName("0이 포함된 경우 예외가 발생한다.")
     @Test
     void throw_exception_when_zero_contained() {
+        // given
         List<Integer> numbers = List.of(0, 1, 2);
+
+        // when & then
         assertThatThrownBy(() ->
                 new Baseball(numbers)).isInstanceOf(IllegalArgumentException.class);
     }
@@ -45,7 +59,10 @@ class BaseballTest {
     @DisplayName("중복 숫자가 있을 경우 예외가 발생한다.")
     @Test
     void throw_exception_when_numbers_duplicated() {
+        // given
         List<Integer> numbers = List.of(1, 2, 2);
+
+        // when & then
         assertThatThrownBy(() ->
                 new Baseball(numbers)).isInstanceOf(IllegalArgumentException.class);
     }
