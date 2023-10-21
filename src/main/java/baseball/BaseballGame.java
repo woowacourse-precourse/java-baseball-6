10e01be76f8a2abc9a02940d20916953e188f03b
HaLin Kim <halin130@tukorea.ac.kr>
2023-10-21 15:36:51 +0900

    fix: 컴퓨터 숫자 추출 시 서로 다른 숫자인지 검사 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,20 +1,27 @@
 package baseball;
 
-import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BaseballGame {
     private static final int START_NUMBER = 1;
     private static final int END_NUMBER = 9;
     private static final int NUMBER_SIZE = 3;
-    private int[] numbers = new int[NUMBER_SIZE];
+    private final List<Integer> numbers;
 
     public BaseballGame() {
+        numbers = new ArrayList<>();
         setNumber();
     }
 
     private void setNumber(){
-        for (int i = 0; i < NUMBER_SIZE; i++) {
-            numbers[i] = pickNumberInRange(START_NUMBER, END_NUMBER);
+        while (numbers.size() < NUMBER_SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(START_NUMBER, END_NUMBER);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
         }
     }
 }
