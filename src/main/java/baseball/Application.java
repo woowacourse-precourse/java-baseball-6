82600ab03032799238742b0e1be394c5af94cedf
King's meow <mingee641@gmail.com>
2023-10-21 20:51:12 +0900

    refactor: 각 기능들 메서드화

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -16,53 +16,23 @@ public class Application {
         while(true) {
             // 컴퓨터 숫자 랜덤 생성
             List<Integer> computer = new ArrayList<>();
-            while (computer.size() < 3) {
-                int randomNumber = Randoms.pickNumberInRange(1, 9);
-                if (!computer.contains(randomNumber)) {
-                    computer.add(randomNumber);
-                    //System.out.println(randomNumber);
-                }
-            }
+            makeRandomNumber(computer);
 
             while (true) {
                 // 사용자 숫자 입력
                 List<Integer> user = new ArrayList<>();
+                int [] result = new int[2];
 
                 System.out.print("숫자를 입력해주세요 : ");
                 String input = Console.readLine();
 
-                if (input.length() != 3) throw new IllegalArgumentException("세 자리 숫자가 아니다.");
-
-                for (int i = 0; i < 3; i++) {
-                    char splitInput = input.charAt(i);
-                    if (!Character.isDigit(splitInput) || splitInput == '0')
-                        throw new IllegalArgumentException("1~9 사이의 숫자를 입력하세요");
-
-                    int inputInt = Character.getNumericValue(splitInput);
-                    if (user.contains(inputInt)) throw new IllegalArgumentException("같은 숫자가 존재한다");
-                    user.add(inputInt);
-                }
-
-                int s = 0, b = 0;
+                inputValidation(input, user);
 
                 // 컴퓨터의 수와 사용자의 수 비교
-                for (int i = 0; i < 3; i++) {
-                    int num = user.get(i);
-                    if (computer.contains(num)) {
-                        if (computer.get(i) == num) s++;
-                        else b++;
-                    }
-                }
+                compare(computer, user, result);
 
-                if (s == 0 && b == 0) System.out.println("낫싱");
-                else if (s == 3) {
-                    System.out.println("3스트라이크");
-                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                    break;
-                }
-                else if (b == 0) System.out.println(s + "스트라이크");
-                else if (s == 0) System.out.println(b + "볼");
-                else System.out.println(b + "볼 " + s + "스트라이크");
+                if(viewResult(result)) break;
+
             }
 
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
@@ -73,6 +43,54 @@ public class Application {
         }
 
 
+    }
+
+    public static boolean viewResult(int[] result) {
+        if (result[0] == 0 && result[1] == 0) System.out.println("낫싱");
+        else if (result[0] == 3) {
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        }
+        else if (result[1] == 0) System.out.println(result[0] + "스트라이크");
+        else if (result[0] == 0) System.out.println(result[1] + "볼");
+        else System.out.println(result[1] + "볼 " + result[0] + "스트라이크");
+
+        return false;
+    }
+
+    public static void compare(List<Integer> computer, List<Integer> user, int[] result) {
+        for (int i = 0; i < 3; i++) {
+            int num = user.get(i);
+            if (computer.contains(num)) {
+                if (computer.get(i) == num) result[0]++;
+                else result[1]++;
+            }
+        }
+    }
+
+    public static void inputValidation(String input, List<Integer> user) {
+        if (input.length() != 3) throw new IllegalArgumentException("세 자리 숫자가 아니다.");
+
+        for (int i = 0; i < 3; i++) {
+            char splitInput = input.charAt(i);
+            if (!Character.isDigit(splitInput) || splitInput == '0')
+                throw new IllegalArgumentException("1~9 사이의 숫자를 입력하세요");
+
+            int inputInt = Character.getNumericValue(splitInput);
+            if (user.contains(inputInt)) throw new IllegalArgumentException("같은 숫자가 존재한다");
+            user.add(inputInt);
+        }
+    }
+
+    public static void makeRandomNumber(List<Integer> computer) {
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+                //System.out.println(randomNumber);
+            }
+        }
     }
 
 }
