09bf182de2f73279ad7fb8503b4a708d8836edd3
junsoo <choix712@umn.edu>
2023-10-21 15:49:06 +0900

    feat: 3개의 숫자를 모두 맞힐 경우 게임 종료 문구 출력

--- src/main/java/baseball/view/View.java
+++ src/main/java/baseball/view/View.java
@@ -5,7 +5,8 @@ public class View {
 
     private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
-
+    private static final String END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     public static void printStart() {
         System.out.println(START_MESSAGE);
     }
@@ -15,4 +16,12 @@ public class View {
         String input = Console.readLine();
         return input;
     }
+
+    public static int printRestart() {
+        System.out.println(END_MESSAGE);
+        System.out.println(RESTART_MESSAGE);
+        String input = Console.readLine();
+        return Integer.parseInt(input);
+    }
+
 }
