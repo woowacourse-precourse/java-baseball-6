3bae85e1390c13542a670934cf09463f6f7d058d
takealook97 <takealook97@naver.com>
2023-10-21 20:38:46 +0900

    style: ErrorMessage 패키지 분리
    기존에 console 패키지 하위에 있던 ErrorMessage를 확장성을 고려하여 exception 패키지로 이동시켰다.

--- src/main/java/baseball/console/ConsoleInput.java
+++ src/main/java/baseball/console/ConsoleInput.java
@@ -1,6 +1,6 @@
 package baseball.console;
 
-import baseball.console.constant.ErrorMessage;
+import baseball.exception.ErrorMessage;
 import camp.nextstep.edu.missionutils.Console;
 
 import baseball.console.util.InputValidation;
