0cf820278c3e1da818b16b337f688d4e0d5611d1
takealook97 <takealook97@naver.com>
2023-10-21 20:29:31 +0900

    feat: RandomNumberGenerator 클래스 생성 및 구현
    랜덤 숫자 생성 메서드를 기존의 Service 레이어에서 Utility 패키지로 분리하였다.

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -1,24 +1,22 @@
 package baseball.service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import baseball.console.ConsoleInput;
 import baseball.console.ConsoleOutput;
-import baseball.service.constant.NumberConstant;
 import baseball.dto.GameResult;
+import baseball.service.constant.NumberConstant;
 import baseball.service.constant.PlayConstant;
-import camp.nextstep.edu.missionutils.Randoms;
+import baseball.service.util.RandomNumberGenerator;
 
 public class GameService {
 	private final ConsoleInput consoleInput;
 	private final ConsoleOutput consoleOutput;
+	private final RandomNumberGenerator randomNumberGenerator;
 	private String targetNumber;
 
 	public GameService() {
 		consoleInput = new ConsoleInput();
 		consoleOutput = new ConsoleOutput();
+		randomNumberGenerator = new RandomNumberGenerator();
 	}
 
 	public void run() {
@@ -32,7 +30,7 @@ public class GameService {
 
 	private void playGame() {
 		consoleOutput.printInit();
-		generateTargetNumber();
+		targetNumber = randomNumberGenerator.getRandomNumber();
 		while (true) {
 			consoleOutput.printReq();
 			GameResult result = getResult(consoleInput.getGameInput());
@@ -42,17 +40,6 @@ public class GameService {
 			}
 			consoleOutput.printResult(result);
 		}
-	}
-
-	private void generateTargetNumber() {
-		List<Integer> numbers = new ArrayList<>();
-		while (numbers.size() < NumberConstant.PICK_COUNT.getValue()) {
-			int randomNumber = Randoms.pickNumberInRange(NumberConstant.MIN_NUMBER.getValue(), NumberConstant.MAX_NUMBER.getValue());
-			if (!numbers.contains(randomNumber)) {
-				numbers.add(randomNumber);
-			}
-		}
-		targetNumber = numbers.stream().map(String::valueOf).collect(Collectors.joining());
 	}
 
 	private GameResult getResult(String input) {
