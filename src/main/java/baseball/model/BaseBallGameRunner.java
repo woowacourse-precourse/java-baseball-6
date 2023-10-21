af7e3d883f87292daac1e96522e00a9f56a8101d
hoyun06 <hoyun06@gmail.com>
2023-10-21 09:59:01 +0900

    feat: 사용자 input validation하는 내용 구현

--- src/main/java/baseball/model/BaseBallGameRunner.java
+++ src/main/java/baseball/model/BaseBallGameRunner.java
@@ -2,6 +2,9 @@ package baseball.model;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BaseBallGameRunner {
     private Computer computer;
 
@@ -12,10 +15,27 @@ public class BaseBallGameRunner {
     public void run() {
         System.out.println("숫자 야구 게임을 시작합니다.");
         String inputString = getPlayerInput();
+        List<Integer> guessNumber = new ArrayList<>();
+
+        validateInputAndAddToList(inputString, guessNumber);
     }
 
     private String getPlayerInput() {
         System.out.print("숫자를 입력해주세요 : ");
         return Console.readLine();
+    }
+
+    private void validateInputAndAddToList(String inputString, List<Integer> guessNumber) {
+        if (inputString.length() != 3) {
+            throw new IllegalArgumentException("잘못된 입력값입니다.");
+        }
+        for (int i = 0; i < 3; i++) {
+            char c = inputString.charAt(i);
+            if (c >= '1' && c <= '9') {
+                guessNumber.add(c - '0');
+                continue;
+            }
+            throw new IllegalArgumentException("잘못된 입력값입니다.");
+        }
     }
 }
