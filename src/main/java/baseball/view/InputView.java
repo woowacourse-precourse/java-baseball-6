622fe3ffc3677c77df6f58518dcda931977972d5
minisyu <iswater.kim@gmail.com>
2023-10-21 18:28:41 +0900

    refactor(InputView): inputOtherThreeNumbers()의 readLine() 수정

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,37 +1,53 @@
 package baseball.view;
 
-import baseball.domain.Player;
-import baseball.util.Sentence;
+import baseball.util.Err;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
 
 public class InputView {
+    private static final int INPUT_SIZE = 3;
     private final OutputView outputView;
 
     public InputView(OutputView outputView) {
         this.outputView = outputView;
     }
 
-    public Player inputThreeNumbers() {
+    private static String validateInput(String input, int inputSize) {
+        InputValidator.validateInputLength(input, inputSize);
+        InputValidator.validateInputScope(input, inputSize);
+        InputValidator.validateInputDuplicated(input);
+        return input;
+    }
+
+    public static String inputByTest(String testInput) {
+        return validateInput(testInput, INPUT_SIZE);
+    }
+
+    public static String inputGameActionByTest(String input) {
+        return input;
+    }
+
+    public List<Integer> inputOtherThreeNumbers() {
         outputView.printInputNumbersMessage();
-        List<Integer> numbers = Arrays.stream(readLine().split(""))
+        String validatedInput = validateInput(readLine(), INPUT_SIZE);
+
+        return Arrays.stream(validatedInput.split(""))
                 .map(Integer::parseInt)
                 .toList();
-        return Player.of(numbers);
     }
 
     public boolean inputGameAction() {
         outputView.printGameSuccessMessage();
         outputView.printGameActionMessage();
-        String gameActionAnswer = readLine();
+        String gameActionAnswer = inputGameActionByTest(readLine());
         if ("1".equals(gameActionAnswer)) {
             return true;
         }
         if ("2".equals(gameActionAnswer)) {
             return false;
         }
-        throw new IllegalArgumentException(Sentence.ERROR_GAME_ACTION_INPUT_MESSAGE.getMessage());
+        throw new IllegalArgumentException(Err.ERROR_GAME_ACTION_INPUT_MESSAGE.getMessage());
     }
 
     private String readLine() {
