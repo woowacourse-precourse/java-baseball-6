8b3af9b98f2f5acdedbf295f94e6d49463e696d9
kimtaesoo <kimtaesoo7@naver.com>
2023-10-22 01:26:42 +0900

    test: 랜덤값을 원하는대로 나오도록 NumberGenerator 구현체 생성

---
+++ src/test/java/baseball/util/StubNumberGenerator.java
@@ -0,0 +1,20 @@
+package baseball.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StubNumberGenerator implements NumberGenerator {
+
+    private final String numbers;
+
+    public StubNumberGenerator(final String numbers) {
+        this.numbers = numbers;
+    }
+
+    @Override
+    public List<Integer> generate() {
+        return Arrays.stream(numbers.split(""))
+            .map(Integer::parseInt)
+            .toList();
+    }
+}
