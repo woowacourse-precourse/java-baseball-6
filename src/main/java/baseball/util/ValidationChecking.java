ba24d67430e184b2c5d6f8cc5f8ff8ef3985e4b4
shAn-kor <kjn0406@naver.com>
2023-10-21 15:53:57 +0900

    readme 겹치는 내용 삭제
    입력값 예외 체크 함수 생성

--- src/main/java/baseball/util/ValidationChecking.java
+++ src/main/java/baseball/util/ValidationChecking.java
@@ -1,5 +1,17 @@
 package baseball.util;
 
+import java.util.List;
+
 public class ValidationChecking {
-    //public void checkUserInputValidate()
+    public void checkUserInputValidate(List<Integer> userNumber) {
+        if (userNumber.size() != 3) {
+            throw new IllegalArgumentException();
+        }
+
+        for (Integer number : userNumber) {
+            if (number < 1 || number > 9) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 }
