1275e59d2d6c884acf51bcd7a4384ebd90af2ca0
JoonKyo <joonkyolee@gmail.com>
2023-10-22 01:31:38 +0900

    feat(NumbersValidator): 문자열을 초기화하는 생성자 추가

---
+++ src/main/java/baseball/model/NumbersValidator.java
@@ -0,0 +1,9 @@
+package baseball.model;
+
+public class NumbersValidator {
+    private String playerNumber;
+
+    public NumbersValidator(String playerNumber) {
+        this.playerNumber = playerNumber;
+    }
+}
