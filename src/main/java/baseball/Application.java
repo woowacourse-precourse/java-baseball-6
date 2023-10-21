5051858db745db1ecececd434de613cdfb630f8b
qkrckstjq <117289923+qkrckstjq@users.noreply.github.com>
2023-10-21 21:30:33 +0900

    feat 난수 비교결과 출력 함수

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,7 +12,7 @@ public class Application {
 
         System.out.println("숫자 야구 게임을 시작합니다.");
         System.out.println(Arrays.toString(computerNum));
-        System.out.println(Arrays.toString(checkNumbers(computerNum,strikeBallNums)));
+        System.out.println(printResult(checkNumbers(computerNum,strikeBallNums)));
     }
 
     public static int[] makeRandom() {
@@ -35,21 +35,38 @@ public class Application {
     }
 
     public static int[] checkNumbers (int[] computerNum, int[] inputNum) {
-        //resultArr[0] = 스트라이크수, resultArr[1] = 볼 수
+        //resultArr[0] = 볼 수, resultArr[1] = 스트라이크 수
         int[] resultArr = new int[2];
         for(int i = 0; i < computerNum.length; i++) {
             for(int j = 0; j < inputNum.length; j++) {
                 if(computerNum[i] == inputNum[j]) {
                     if(i == j) {
-                        resultArr[0] += 1;
-                    } else {
                         resultArr[1] += 1;
+                    } else {
+                        resultArr[0] += 1;
                     }
                 }
             }
         }
 
         return resultArr;
+    }
+
+    public static String printResult (int[] checkNumbers) {
+        String result = "";
+        if(checkNumbers[0] != 0 && checkNumbers[1] != 0) {
+            result = checkNumbers[1]+"볼 " + checkNumbers[0]+"스트라이크\n";
+        } else if (checkNumbers[0] != 0 && checkNumbers[1] == 0) {
+            result = checkNumbers[0]+"볼\n";
+        } else if (checkNumbers[0] == 0 && checkNumbers[1] != 0) {
+            result = checkNumbers[1]+"스트라이크\n";
+        } else {
+            result = "낫싱\n";
+        }
+        if(checkNumbers[0] == 3) {
+            result+="3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+        }
+        return result;
     }
 
 }
