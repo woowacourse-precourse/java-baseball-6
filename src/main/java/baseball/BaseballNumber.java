db75426dc2d77d2a6cbc0a5ed64eb404153babdf
devhoya97 <diajbh123@gmail.com>
2023-10-21 12:03:00 +0900

    refactor : validateBaseballNumber 메서드 위치 변경 해당 메서드는 BaseballNumber에서만 사용할 것이 확실하므로, 여러 곳에서 사용할 가능성이 있는 메서드들을 모아둔  Validation 클래스보다는 BaseballNumber 클래스에 배치하는게 옳다고 판단하여 위치를 변경함

--- src/main/java/baseball/BaseballNumber.java
+++ src/main/java/baseball/BaseballNumber.java
@@ -1,9 +1,21 @@
 package baseball;
 
 public class BaseballNumber {
-    int baseballNumber;
+    private static final int MIN_BASEBALL_NUMBER = 1;
+    private static final int MAX_BASEBALL_NUMBER = 9;
+
+    private int baseballNumber;
 
     public BaseballNumber(int baseballNumber) {
+        validateBaseballNumber(baseballNumber);
         this.baseballNumber = baseballNumber;
+    }
+
+    public static void validateBaseballNumber(int baseballNumber) {
+        if (baseballNumber < MIN_BASEBALL_NUMBER || baseballNumber > MAX_BASEBALL_NUMBER) {
+            throw new IllegalArgumentException(
+                    "[ERROR] 숫자가 " + MIN_BASEBALL_NUMBER + "~" + MAX_BASEBALL_NUMBER + " 사이가 아닙니다."
+            );
+        }
     }
 }
