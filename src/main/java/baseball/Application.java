74835a24b4032d06ba4ea45b279cb17c3d103935
박은지 <qkrdmswl1018@naver.com>
2023-10-21 22:58:14 +0900

    feat: 메인 게임 루프 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+  public static void main(String[] args) {
+    while (true) {
+      Printer.start();
+
+      if (!Game.play()) {
+        break;
+      }
     }
+  }
 }
