15aa7ff42b53baf869c9f966acc55370c5de85a7
mk3058 <kmk3058@icloud.com>
2023-10-21 13:48:47 +0900

    feat: 게임 진행 로직 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String isEnd;
+
+        do {
+            Game game = new Game(new User(), new Computer());
+            
+            game.play();
+            GameMessage.GAME_RESTART_PROMPT.print();
+            isEnd = readLine();
+            if (isEnd.length() != 1 || !Character.isDigit(isEnd.charAt(0)) || !(1 <= Integer.parseInt(isEnd)
+                    && Integer.parseInt(isEnd) <= 2)) {
+                throw new IllegalArgumentException("잘못된 입력입니다.");
+            }
+        } while (Integer.parseInt(isEnd) == 1);
+
     }
 }
