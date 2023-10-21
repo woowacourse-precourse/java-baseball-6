6d09c0f61253ea769510f83c9e6cd9938a6c8d7d
seokmyungham <hellotjraud@gmail.com>
2023-10-22 03:03:42 +0900

    feat(GameView): endGame 메서드 추가
    endGame: 게임 종료 메시지와, 재시작 메시지를 출력한 후 사용자의 입력을 받는 메서드입니다.

--- src/main/java/baseball/view/GameView.java
+++ src/main/java/baseball/view/GameView.java
@@ -1,5 +1,7 @@
 package baseball.view;
 
+import static baseball.domain.Constants.END_GAME_MESSAGE;
+import static baseball.domain.Constants.RESTART_GAME_MESSAGE;
 import static baseball.domain.Constants.START_GAME_MESSAGE;
 import static baseball.domain.Constants.USER_INPUT_MESSAGE;
 
@@ -18,5 +20,13 @@ public class GameView {
 
     public void printHintMessage(String hintMessage) {
         System.out.println(hintMessage);
+    }
+
+    public int endGame() {
+        System.out.println(END_GAME_MESSAGE);
+        System.out.println(RESTART_GAME_MESSAGE);
+        String userInput = Console.readLine();
+        int restartType = Integer.parseInt(userInput);
+        return restartType;
     }
 }
