43d8a8828daf22eb91cb73fef13599578ce71044
Jiyoung Choi <12201817@inha.edu>
2023-10-21 23:01:04 +0900

    test : RandomListMaker에서 생성된 리스트의 길이가 3인지 확인

---
+++ src/test/java/baseball/RandomListMakerTest.java
@@ -0,0 +1,20 @@
+package baseball;
+
+import baseball.controller.RandomListMaker;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RandomListMakerTest {
+    private final int LIST_SIZE = 3;
+
+    @DisplayName("RandomListMaker에서 생성된 리스트의 길이가 3인지 확인")
+    @Test
+    public void isThreeComponent(){
+        List<Integer> integerList = RandomListMaker.generateRandomNumbers();
+        assertThat(integerList.size()).isEqualTo(LIST_SIZE);
+    }
+}
