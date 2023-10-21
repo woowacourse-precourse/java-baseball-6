a1b9ac0c419c19c980dc0bd075ebf7b6017b60bc
chaeshee0908 <chaeshee0908@naver.com>
2023-10-22 01:53:03 +0900

    [feat] 게임 완료 시 완료 메세지 및 게임 시작 기능 구현

--- src/main/java/baseball/game/Game.java
+++ src/main/java/baseball/game/Game.java
@@ -2,14 +2,21 @@ package baseball.game;
 
 public class Game {
 
-    public void printStartGame() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String NEW_GAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public static void startGame() {
+        System.out.println(GAME_START_MESSAGE);
     }
 
-    public void startGame() {
-        printStartGame();
-
+    public static void successGame() {
+        System.out.println(GAME_END_MESSAGE);
+        newGame();
     }
 
+    public static void newGame() {
+        System.out.println(NEW_GAME_MESSAGE);
 
+    }
 }
