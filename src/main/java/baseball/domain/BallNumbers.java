4317ac3e9421f1cdcb3dfd969b20b64925641904
Jiihyun <cwlgusc@gmail.com>
2023-10-21 15:11:12 +0900

    feat(BallNumbers): 세자리 수 일급컬렉션적용 및 숫자 요구 사항 검증한 정적 팩토리 메서드 생성

--- src/main/java/baseball/domain/BallNumbers.java
+++ src/main/java/baseball/domain/BallNumbers.java
@@ -1,4 +1,43 @@
 package baseball.domain;
 
+import java.util.List;
+
+import static baseball.util.MessageConst.*;
+import static baseball.util.NumberConst.*;
+
 public class BallNumbers {
+    private final List<Integer> numberList;
+
+    public BallNumbers(List<Integer> numberList) {
+        validateNumberRange(numberList);
+        validateNumberSize(numberList);
+        validateDuplicateNumber(numberList);
+        this.numberList = numberList;
+    }
+
+    public static BallNumbers from(List<Integer> numberList) {
+        return new BallNumbers(numberList);
+    }
+
+    private void validateNumberSize(List<Integer> numberList) {
+        if (numberList.size() != NUMBER_SIZE) {
+            throw new IllegalArgumentException(NUMBER_SIZE_EXCEPTION);
+        }
+    }
+
+    private void validateDuplicateNumber(List<Integer> numberList) {
+        if (numberList.stream()
+                .distinct()
+                .count() != NUMBER_SIZE) {
+            throw new IllegalArgumentException(NUMBER_DUPLICATION_EXCEPTION);
+        }
+    }
+
+    private void validateNumberRange(List<Integer> numberList) {
+        for (Integer num : numberList) {
+            if (num < START_INCLUSIVE || num > END_INCLUSIVE) {
+                throw new IllegalArgumentException(NUMBER_RANGE_EXCEPTION);
+            }
+        }
+    }
 }
--- src/main/java/baseball/util/MessageConst.java
+++ src/main/java/baseball/util/MessageConst.java
@@ -4,4 +4,7 @@ public class MessageConst {
     public final static String GAME_START_MSG = "숫자 야구 게임을 시작합니다.\n";
     public final static String NUMBER_INPUT_FORMAT_EXCEPTION = "[ERROR] 숫자만 입력해주세요.\n";
     public final static String STRING_INPUT_FORMAT_EXCEPTION = "[ERROR] 입력값이 빈 문자열 또는 공백입니다.\n";
+    public final static String NUMBER_SIZE_EXCEPTION = "[ERROR] 세자리수를 입력해주세요.\n";
+    public final static String NUMBER_DUPLICATION_EXCEPTION = "[ERROR] 숫자가 중복되었습니다.\n";
+    public final static String NUMBER_RANGE_EXCEPTION = "[ERROR] 1이상 9이하 사이의 숫자를 입력해주세요.\n";
 }
