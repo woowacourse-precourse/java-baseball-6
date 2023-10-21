5cec0a840feae73430b3646618f70cf1420c5c13
민주 <hinote444@gmail.com>
2023-10-22 01:43:52 +0900

    refactor(OutputView): 메소드명 수정

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,12 +1,12 @@
 package baseball.view;
 
 public class OutputView {
-    
+
     public static void printStartGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    public static void printInput() {
+    public static void printInputNumbers() {
         System.out.print("숫자를 입력해주세요 : ");
     }
 
@@ -14,7 +14,7 @@ public class OutputView {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
-    public static void printRestartOrEnd() {
+    public static void printRestartOrQuit() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
