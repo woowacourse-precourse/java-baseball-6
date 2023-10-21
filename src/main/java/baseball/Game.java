d2d751fcc72fd9c7b0259efce408d154ae5df323
Moly <102847513+jminkkk@users.noreply.github.com>
2023-10-21 16:57:25 +0900

    feat: 게임을 진행하는 메서드, 볼, 스트라이크를 체크하는 메서드 생성
    요구사항: 컴퓨터는 같은 수가 같은 자리에 있으면 스트라이크, 다른 자리에 있으면 볼, 같은 수가 전혀 없으면 낫싱이란 힌트(결과)를 출력한다.

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Game {
     private final String START_GAME_COMMENT = "숫자 야구 게임을 시작합니다.";
-    private final String PLAY_GAME_COMMENT = "숫자를 입력해주세요 : ";
+    private final String INPUT_NUMBER_COMMENT = "숫자를 입력해주세요 : ";
 
     private final Player player;
     private final List<Integer> answer;
@@ -16,5 +16,38 @@ public class Game {
 
     public void start() {
         System.out.println(START_GAME_COMMENT);
+        play();
+
     }
+
+    public void play() {
+        System.out.println(INPUT_NUMBER_COMMENT);
+        List<Integer> playerNumbers = player.loadListOf3Number();
+
+        int duplicateCount = countDuplicateIntegers(playerNumbers, answer);
+        int strike = countStrike(playerNumbers, answer);
+        int ball = duplicateCount - strike;
+    }
+
+
+    private int countDuplicateIntegers(List<Integer> playerNumbers, List<Integer> answer) {
+        int ball = 0;
+        for (int i = 0; i < playerNumbers.size(); i++) {
+            if (answer.contains(playerNumbers.get(i))) {
+                ball++;
+            }
+        }
+        return ball;
+    }
+
+    private int countStrike(List<Integer> playerNumbers, List<Integer> answer) {
+        int strike = 0;
+        for (int i = 0; i < playerNumbers.size(); i++) {
+            if (answer.get(i) == playerNumbers.get(i)) {
+                strike++;
+            }
+        }
+        return strike;
+    }
+
 }
