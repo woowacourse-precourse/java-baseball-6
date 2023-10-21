5c2ac91a552b66c667f57698de17984a6cd4a7cc
unifolio0 <boho82ha@gmail.com>
2023-10-21 20:03:57 +0900

    User 수정

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -5,14 +5,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class User {
-    List<Integer> user;
-
     public List<Integer> inputNumber() {
-        this.user = new ArrayList<>();
+        List<Integer> user = new ArrayList<>();
         System.out.print("숫자를 입력해주세요 : ");
         String[] numbers = Console.readLine().split("");
         for(int i=0;i<numbers.length;i++) {
-            this.user.add(Integer.parseInt(numbers[i]));
+            user.add(Integer.parseInt(numbers[i]));
         }
         return user;
     }
