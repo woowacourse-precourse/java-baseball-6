386296ffdd9dee9b2118c064aaff09dc6d87d27e
HongChaa <jun761_1@naver.com>
2023-10-21 17:06:27 +0900

    feat(PlayerAnswer): setter 및 getter 기능 추가

---
+++ src/main/java/baseball/model/PlayerAnswer.java
@@ -0,0 +1,25 @@
+package baseball.model;
+
+public class PlayerAnswer {
+
+    private int ball = 0;
+    private int strike = 0;
+
+    public int getBall() {
+        return ball;
+    }
+
+    public void setBall(int ball) {
+        this.ball = ball;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public void setStrike(int strike) {
+        this.strike = strike;
+    }
+
+
+}
