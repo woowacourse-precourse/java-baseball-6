6c8f5a8d05a5216bcbf422ba5c9dcd0bab98cedd
watermelon3737 <pshrina1@naver.com>
2023-10-21 18:44:55 +0900

    [feat] : display 메세지를 출력하는 메서드 구현

--- src/main/java/baseball/BaseballGameMachine.java
+++ src/main/java/baseball/BaseballGameMachine.java
@@ -14,22 +14,22 @@ public class BaseballGameMachine {
         boolean gameEnd = false;
         while (!gameEnd) {
             NumberGenerator generator = new NumberGenerator();
-            startMessage();
+            display(message.start() + '\n');
             Referee referee = new Referee(generator.generateRandomAnswerList());
-            System.out.println(referee.getAnswerList());
+//            System.out.println(referee.getAnswerList());
             InputValidation inputValidation = new InputValidation();
             boolean success = false;
             while (!success) {
-                System.out.print(message.requestInput());
+                display(message.requestInput());
                 List<Integer> userInput = inputValidation.validateUserInput(input());
                 List<Integer> result = referee.judgeUserInput(userInput);
-//                System.out.println(message.result(result));
+                display(message.result(result) + '\n');
                 if (result != null && result.get(0) == 0 && result.get(1) == 3) {
-                    System.out.println(message.success());
+                    display(message.success());
                     success = true;
                 }
             }
-            System.out.println(message.requestRetryOrEnd());
+            display(message.requestRetryOrEnd() + '\n');
             int newOrEnd = newGameOrEnd(input());
             if (newOrEnd == 2) {
                 gameEnd = true;
@@ -37,8 +37,8 @@ public class BaseballGameMachine {
         }
     }
 
-    public void startMessage() {
-        System.out.println(message.start());
+    private void display(String message) {
+        System.out.print(message);
     }
 
     public int newGameOrEnd(String input) {
