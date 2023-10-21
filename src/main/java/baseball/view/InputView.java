ee73b12463e200e8375e8f4c626f00f76e37f1de
haiseong <wjdgotjd9908@gmail.com>
2023-10-22 02:12:17 +0900

    feat: InputView 클래스 생성

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,10 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+     public static String readLine() {
+        String line = Console.readLine();
+        return line;
+    }
+}
