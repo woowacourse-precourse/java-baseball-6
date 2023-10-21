1aca7d8640d7142127408b73d41cdf52c6eaf005
alscjf1329 <alscjf1329@gmail.com>
2023-10-21 14:59:41 +0900

    Feat) start 생성

---
+++ src/main/java/baseball/NumbersBaseball.java
@@ -0,0 +1,15 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class NumbersBaseball {
+    private int[] answer = new int[3];
+
+    public NumbersBaseball() {
+    }
+
+    public void start() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+}
