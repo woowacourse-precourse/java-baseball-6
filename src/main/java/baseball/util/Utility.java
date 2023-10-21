21aad5f377b40a09aa57d3f0be5aace30afa2496
whereami2048 <whereami2048@gmail.com>
2023-10-21 21:02:33 +0900

    [feat]: String -> ArrayList<Integer> 변환 함수 구현

--- src/main/java/baseball/util/Utility.java
+++ src/main/java/baseball/util/Utility.java
@@ -9,5 +9,16 @@ public class Utility {
     private static List<Integer> assignComputerRandomNumber() {
         return Randoms.pickUniqueNumbersInRange(1, 9, 3);
     }
+
+    private static List<Integer> string2IntegerArrayList(String userInput) {
+        List<Integer> arrayListUserInput = new ArrayList<>();
+        String[] splitUserInput = userInput.split("");
+
+        for (String splitWord : splitUserInput) {
+            arrayListUserInput.add(Integer.parseInt(splitWord));
+        }
+
+        return arrayListUserInput;
+    }
 }
 
