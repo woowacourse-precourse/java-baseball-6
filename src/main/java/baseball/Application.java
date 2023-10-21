cbd7250aa13a2474632c9786f6553e69f21a53dc
Dcool <90443691+Leesiwon12171260@users.noreply.github.com>
2023-10-21 13:44:59 +0900

    computer add randomNumber 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        List<Integer> comArrayList = new ArrayList<>();
+        List<Integer> humanArrayList = new ArrayList<>();
+        while (comArrayList.size() < 3){
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if(!comArrayList.contains(randomNumber)){
+                comArrayList.add(randomNumber);
+            }
+        }
+
+
     }
-}
