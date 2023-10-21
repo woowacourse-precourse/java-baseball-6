46e29e2d25af05917661db5dc962cf97582a075c
inpink <inpink@kakao.com>
2023-10-21 17:19:52 +0900

    feat: main method에 "게임 시작 문구" 출력
    "숫자 야구 게임을 시작합니다." 출력
    우선 절차지향으로 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,10 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        //시작 문구
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+
     }
 }
