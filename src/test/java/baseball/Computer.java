0f07a5b7ea6426d9b8eb65049ec4f5915e8a8805
baksakcci <pshsh910@gmail.com>
2023-10-21 18:07:36 +0900

    refactor($Computer): generateRandomNumber() 내부 로직 메서드 분리

--- src/test/java/baseball/Computer.java
+++ src/test/java/baseball/Computer.java
@@ -7,15 +7,21 @@ public class Computer {
     private String number;
 
     public String generateRandomNumber() {
-        String number = String.valueOf(Randoms.pickNumberInRange(100, 999));
-
-        while (number.charAt(0) == number.charAt(1) ||
-                number.charAt(1) == number.charAt(2) ||
-                number.charAt(0) == number.charAt(2)) {
-            number = String.valueOf(Randoms.pickNumberInRange(100, 999));
+        String number = numberToString();
+        while (validationDifferentNumber(number)) {
+            number = numberToString();
         }
-
         this.number = number;
         return this.number;
+    }
+
+    private boolean validationDifferentNumber(String number) {
+        return number.charAt(0) == number.charAt(1) ||
+                number.charAt(1) == number.charAt(2) ||
+                number.charAt(0) == number.charAt(2);
+    }
+
+    private String numberToString() {
+        return String.valueOf(Randoms.pickNumberInRange(100, 999));
     }
 }
