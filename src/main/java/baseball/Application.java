73011764a211686742eba7fcc06922f20cc39975
cookingTorch <jhw7181@naver.com>
2023-10-21 22:25:26 +0900

    make getInputNumber

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
@@ -23,5 +24,50 @@ public class Application {
         return ((number[0] == number[1])
                 || (number[1] == number[2])
                 || (number[2] == number[0]));
+    }
+
+    private static int[] getInputNumber() {
+        String input;
+
+        input = Console.readLine();
+        if (!isValidInput(input)) {
+            throw new IllegalArgumentException();
+        };
+        return (stringToIntArray(input));
+    }
+
+    private static boolean isValidInput(String input) {
+        return (isLengthThree(input)
+                && isInRange(input)
+                && !duplicateInput(input));
+    }
+
+    private static boolean isLengthThree(String input) {
+        return (input.length() == 3);
+    }
+
+    private static boolean isInRange(String input) {
+        for (char ch : input.toCharArray()) {
+            if (!('1' <= ch && ch <= '9')) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean duplicateInput(String input) {
+        return ((input.charAt(0) == input.charAt(1))
+                || (input.charAt(1) == input.charAt(2))
+                || (input.charAt(2) == input.charAt(0)));
+    }
+
+    private static int[] stringToIntArray(String input) {
+        int[] inputNumber;
+
+        inputNumber = new int[input.length()];
+        for (int i = 0; i < input.length(); i++) {
+            inputNumber[i] = (int) input.charAt(i);
+        }
+        return inputNumber;
     }
 }
