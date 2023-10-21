8119462af724309cadaf6984ff8f07d36080e757
jeondui <jeondui@gmail.com>
2023-10-21 15:50:26 +0900

    feat(Player): 플레이어 모델에 숫자 저장 기능

--- src/main/java/baseball/model/Player.java
+++ src/main/java/baseball/model/Player.java
@@ -9,6 +9,18 @@ public class Player {
 
     private List<Integer> playerNumbers;
 
+    public Player(String input) {
+        setNumbers(input);
+    }
+
+    public void setNumbers(String input) {
+        this.playerNumbers = generateInputNumbers(input);
+    }
+
+    public List<Integer> getNumbers() {
+        return playerNumbers;
+    }
+
     public List<Integer> generateInputNumbers(String input) {
         int[] intArray = Arrays.stream(input.split(""))
                 .mapToInt(Integer::parseInt)
