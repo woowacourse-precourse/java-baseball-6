1213cd38a870ed3bb6d5385df5df508b92f5f99a
Sunhee Kim <sunni372@icloud.com>
2023-10-22 01:05:28 +0900

    feat: 입력된 Numbers의 예외 처리
    - 숫자 이외의 문자가 포함된 경우
    - 3자리의 수가 아닌 경우
    - 0이 포함된 경우
    - 중복된 값이 있는 경우

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,29 +1,67 @@
 package baseball;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class User {
-    private List<Integer> numbers;
+    private final List<Integer> numbers;
 
     public User() {
-        // TODO: 입력된 Numbers 예외 처리
+        String numbers = Input.readUserNumbers();
+        validateInput(numbers);
 
-        setNumbers(Input.readUserNumbers());
+        this.numbers = toList(numbers);
     }
 
-    private void setNumbers(String inputNumbers) {
+    private void validateInput(String numbers) {
+        isNumber(numbers);
+        isThreeDigitNumber(numbers);
+        hasZero(numbers);
+        hasDuplicateNumber(numbers);
+    }
+
+    private List<Integer> toList(String numbersStr) {
         List<Integer> numbers = new ArrayList<>();
 
-        for (int i = 0; i < inputNumbers.length(); i++) {
-            int number = inputNumbers.charAt(i) - '0';
+        for (int i = 0; i < numbersStr.length(); i++) {
+            int number = numbersStr.charAt(i) - '0';
             numbers.add(number);
         }
 
-        this.numbers = numbers;
+        return numbers;
     }
 
     public List<Integer> getNumbers() {
         return this.numbers;
+    }
+
+    private void isNumber(String numbers) {
+        try {
+            Integer.parseInt(numbers);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("숫자 이외의 문자가 포함된 값을 입력했습니다.");
+        }
+    }
+
+    private void isThreeDigitNumber(String numbers) throws IllegalArgumentException {
+        if (numbers.length() != Constants.NUMBER_COUNT) {
+            throw new IllegalArgumentException("3자리의 수가 아닌 값을 입력했습니다.");
+        }
+    }
+
+    private void hasZero(String numbers) throws IllegalArgumentException {
+        if (toList(numbers).contains(0)) {
+            throw new IllegalArgumentException("0이 포함된 값을 입력했습니다.");
+        }
+    }
+
+    private void hasDuplicateNumber(String numbers) {
+        Set<Integer> numbersSet = new HashSet<>(toList(numbers));
+
+        if (numbersSet.size() != Constants.NUMBER_COUNT) {
+            throw new IllegalArgumentException("중복된 값을 입력했습니다.");
+        }
     }
 }
