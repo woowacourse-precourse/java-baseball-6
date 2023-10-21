cd8ff46a934bd3d9fcc8f36e97788a43a6bf847a
kylim99 <zxzx05050@naver.com>
2023-10-21 11:47:07 +0900

    refator: result class 필드 제한자 수정 및 공배 문자열 추가

--- src/main/java/baseball/domain/Result.java
+++ src/main/java/baseball/domain/Result.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 public class Result {
 
-    public static final String STRIKE_MESSAGE = "%d스트라이크";
-    public static final String BALL_MESSAGE = "%d볼";
-    public static final String NOTHING_MESSAGE = "낫싱";
+    private static final String STRIKE_MESSAGE = "%d스트라이크";
+    private static final String BALL_MESSAGE = "%d볼";
+    private static final String NOTHING_MESSAGE = "낫싱";
+    private static final String SPACE = " ";
     private int strikeCount;
     private int ballCount;
 
@@ -34,7 +35,7 @@ public class Result {
         } else if (strikeCount == 0) {
             return String.format(BALL_MESSAGE, ballCount);
         }
-        return String.format(BALL_MESSAGE + " " +STRIKE_MESSAGE, ballCount, strikeCount);
+        return String.format(BALL_MESSAGE + SPACE +STRIKE_MESSAGE, ballCount, strikeCount);
     }
 
     public int getStrikeCount() {
