40a11232a2587243b35c34f022a8d7835849885d
hlmg <devhlmg@gmail.com>
2023-10-21 15:07:55 +0900

    refactor(GameCommand): cache 적용

--- src/main/java/baseball/domain/GameCommand.java
+++ src/main/java/baseball/domain/GameCommand.java
@@ -1,22 +1,29 @@
 package baseball.domain;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public enum GameCommand {
     RESTART(1), END(2);
 
-    private static final String INVALID_GAME_STATUS_MESSAGE = "1 또는 2를 입력해주세요";
     private final int command;
+    private static final String INVALID_GAME_STATUS_MESSAGE = "1 또는 2를 입력해주세요";
+    private static final Map<Integer, GameCommand> cache;
+
+    static {
+        cache = Arrays.stream(GameCommand.values())
+                .collect(Collectors.toMap((gameCommand -> gameCommand.command), gameCommand -> gameCommand));
+    }
 
     GameCommand(int command) {
         this.command = command;
     }
 
     public static GameCommand from(int command) {
-        for (GameCommand gameCommand : GameCommand.values()) {
-            if (gameCommand.command == command) {
-                return gameCommand;
-            }
-        }
-        throw new IllegalArgumentException(INVALID_GAME_STATUS_MESSAGE);
+        return Optional.ofNullable(cache.get(command))
+                .orElseThrow(() -> new IllegalArgumentException(INVALID_GAME_STATUS_MESSAGE));
     }
 
     public boolean isEnd() {
