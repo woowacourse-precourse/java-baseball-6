9cb23313112f453b8ab14921af5d3b904d9887cc
takealook97 <takealook97@naver.com>
2023-10-21 15:23:06 +0900

    feat: 매직넘버 final처리 Enum클래스 생성 후 이동
    기존에는 필드에 min_num, max_num, pick_count를 final로 상수처리를 하였는데 전반적으로 다른 클래스들에서도 쓰이는 경우가 있어서 Enum클래스로 이동하여 글로벌하게 사용할 수 있도록 수정하였다.

---
+++ src/main/java/baseball/console/NumberConstant.java
@@ -0,0 +1,17 @@
+package baseball.console;
+
+public enum NumberConstant {
+	MIN_NUMBER(1),
+	MAX_NUMBER(9),
+	PICK_COUNT(3);
+
+	private final int value;
+
+	NumberConstant(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+}
