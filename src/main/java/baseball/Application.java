213686cb15665619cbd59b43e78440258f67f0af
minsang-alt <mazinger97@naver.com>
2023-10-21 23:23:42 +0900

    feat: 애플리케이션 시작 클래스 구현
    - 애플리케이션의 시작 (추후에 컨트롤러 생성시 많은 인자를 줄이는 방법으로 바꿔야 할 필요가 있어보임)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,117 +1,26 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.List;
+import baseball.controller.BaseballController;
+import baseball.model.ComputerModel;
+import baseball.model.UserModel;
+import baseball.util.exception.TerminateApplicationException;
+import baseball.view.UserInputView;
+import baseball.view.UserOutputView;
 
 public class Application {
 
-    public static final int START_INCLUSIVE = 1;
-    public static final int END_INCLUSIVE = 9;
 
-    private static final int NUM_LENGTH = 3;
 
-    public static List<Integer> splitDigitsFromRandomNumber = new ArrayList<>();
+
 
     public static void main(String[] args) {
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        initComputerNumber();
-
-        while (true) {
-            List<Integer> splitDigitsFromUserInput = inputUserNumber();
-            boolean resultByCompare = compareUserInputByComputerNumber(splitDigitsFromUserInput);
-            if (resultByCompare) {
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                String isContinueByUserInput = Console.readLine();
-                if (isContinueByUserInput.equals("1")) {
-                    initComputerNumber();
-                } else if (isContinueByUserInput.equals("2")) {
-                    break;
-                } else {
-                    throw new IllegalArgumentException(
-                            "잘못된 값을 입력했습니다" + " you input = " + isContinueByUserInput);
-                }
-            }
-        }
-
-    }
-
-
-    public static boolean compareUserInputByComputerNumber(List<Integer> splitDigitsFromUserInput) {
-        int strikeNumber = 0;
-        int ballNumber = 0;
-        int nothingNumber = 0;
-        int trackIdx = 0;
-
-        for (Integer userDigit : splitDigitsFromUserInput) {
-            if (userDigit.equals(splitDigitsFromRandomNumber.get(trackIdx))) {
-                strikeNumber++;
-            } else if (splitDigitsFromRandomNumber.contains(userDigit)) {
-                ballNumber++;
-            } else {
-                nothingNumber++;
-            }
-            trackIdx++;
-        }
-
-        return evaluateByResult(strikeNumber, ballNumber, nothingNumber);
-    }
-
-    public static boolean evaluateByResult(int strikeNumber, int ballNumber, int nothingNumber) {
-        if (strikeNumber == NUM_LENGTH) {
-            System.out.println(NUM_LENGTH + "스트라이크");
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            return true;
-        } else if (nothingNumber == NUM_LENGTH) {
-            System.out.println("낫싱");
-        } else if (strikeNumber != 0 || ballNumber != 0) {
-            if (ballNumber != 0) {
-                System.out.print(ballNumber + "볼 ");
-            }
-            if (strikeNumber != 0) {
-                System.out.println(strikeNumber + "스트라이크");
-            }
-            System.out.println();
-        }
-        return false;
-    }
-
-    public static void initComputerNumber() {
-        splitDigitsFromRandomNumber.clear();
-        while (splitDigitsFromRandomNumber.size() < NUM_LENGTH) {
-            //여기서 IllegalArgumentException 고려해봐야 한다.
-            int randomNumber = Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
-            if (!splitDigitsFromRandomNumber.contains(randomNumber)) {
-                splitDigitsFromRandomNumber.add(randomNumber);
-            }
+        try{
+            BaseballController baseballController = new BaseballController(new UserInputView(),new UserOutputView(),
+                    new ComputerModel(),new UserModel());
+            baseballController.startGame();
+        } catch (TerminateApplicationException e) {
+            System.out.println(e.getMessage());
         }
     }
 
-    public static List<Integer> inputUserNumber() {
-        System.out.print("숫자를 입력해주세요 : ");
-        String stringDigitsFromUserInput = Console.readLine();
-        validateUserInputLength(stringDigitsFromUserInput);
-        return splitDigits(stringDigitsFromUserInput);
-    }
-
-    private static void validateUserInputLength(String stringDigitsFromUserInput) {
-        if (stringDigitsFromUserInput.length() != NUM_LENGTH) {
-            throw new IllegalArgumentException("숫자의 길이가 " + NUM_LENGTH + "와 다릅니다.");
-        }
-    }
-
-    private static List<Integer> splitDigits(String stringDigitsFromUserInput) {
-        List<Integer> splitDigitsFromUserInput = new ArrayList<>();
-        for (int i = 0; i < stringDigitsFromUserInput.length(); i++) {
-            int number = Character.getNumericValue(stringDigitsFromUserInput.charAt(i));
-            if (!splitDigitsFromUserInput.contains(number)) {
-                splitDigitsFromUserInput.add(number);
-            }
-        }
-        if (splitDigitsFromUserInput.size() != NUM_LENGTH) {
-            throw new IllegalArgumentException("중복된 숫자 존재");
-        }
-        return splitDigitsFromUserInput;
-    }
 }
