f0b4a446951124d259298ade975563f945aeaf8d
민주 <hinote444@gmail.com>
2023-10-21 18:02:23 +0900

    feat(Outputview): 게임 진행 출력 메시지 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,4 +1,19 @@
 package baseball.view;
 
 public class OutputView {
+    public static void printStartGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void printInput() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
+    public static void printSuccess() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public static void printRestartOrEnd() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
