4c4ae3c85857b7986f62360809385d9c979fd3af
민주 <hinote444@gmail.com>
2023-10-21 20:03:55 +0900

    feat(BaseBallGame): 상대방 숫자와 플레이어의 숫자를 비교후 스트라이크와 볼 개수를 구하는 기능 구현

--- src/main/java/baseball/service/BaseballGame.java
+++ src/main/java/baseball/service/BaseballGame.java
@@ -6,6 +6,21 @@ import java.util.List;
 
 public class BaseballGame {
 
+    public void calculateNumbers(List<Integer> computerNumbers, List<Integer> playerNumbers) {
+        int strikeCount = 0;
+        int ballCount = 0;
+
+        for (int i = 0; i < playerNumbers.size(); i++) {
+            if (computerNumbers.get(i).equals(playerNumbers.get(i))) {
+                strikeCount++;
+            }
+            if (computerNumbers.contains(playerNumbers.get(i)) && !computerNumbers.get(i)
+                    .equals(playerNumbers.get(i))) {
+                ballCount++;
+            }
+        }
+    }
+
     public List<Integer> createComputerNumbers() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
