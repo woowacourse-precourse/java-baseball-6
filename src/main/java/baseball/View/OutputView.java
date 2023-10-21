4e9e26965f46e6c6e773bc0554f8d1a945579c1b
kim <hwiyul_g@daum.net>
2023-10-21 20:23:15 +0900

    feat(OutputView) : 게임 종료 안내 함수 추가

--- src/main/java/baseball/View/OutputView.java
+++ src/main/java/baseball/View/OutputView.java
@@ -6,5 +6,9 @@ public class OutputView {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
+    public void displayWinningMessage() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
 
 }
