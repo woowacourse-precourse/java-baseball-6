3411e69ab09de4bf6713ac789e72426bdcf01b84
TaeHye0n <thk001229@gmail.com>
2023-10-21 20:00:19 +0900

    feat: 숫자야구 게임 동작 기능  - 컨트롤러로 부터 사용자 입력과 랜덤 숫자를 받아 처리  - 띄어쓰기 구분문자를 사용하기 위해 StringJoiner 사용

---
+++ src/main/java/baseball/domain/BaseBallGameService.java
@@ -0,0 +1,53 @@
+package baseball.domain;
+
+import java.util.List;
+import java.util.StringJoiner;
+
+public class BaseBallGameService {
+
+    private static final int MAX_NUMBER_SIZE = 3;
+
+    public String startGame(ComputerRandomNumbers computerRandomNumbers, List<Integer> userInputs) {
+        int strikeCount = getStrikeCount(computerRandomNumbers, userInputs);
+        int ballCount = getBallCount(computerRandomNumbers, userInputs);
+
+        StringJoiner stringJoiner = new StringJoiner(" ");
+
+        if (ballCount != 0) {
+            stringJoiner.add(String.format("%d볼", ballCount));
+        }
+
+        if (strikeCount != 0) {
+            stringJoiner.add(String.format("%d스트라이크", strikeCount));
+        }
+
+        if (strikeCount == 0 && ballCount == 0) {
+            stringJoiner.add("낫싱");
+        }
+
+        return stringJoiner.toString();
+    }
+
+    private int getStrikeCount(ComputerRandomNumbers computerRandomNumbers, List<Integer> userInputs) {
+        int count = 0;
+        for (int i = 0; i < MAX_NUMBER_SIZE; i++) {
+            if (computerRandomNumbers.get(i) == userInputs.get(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private int getBallCount(ComputerRandomNumbers computerRandomNumbers, List<Integer> userInputs) {
+        int count = 0;
+        for (int i = 0; i < MAX_NUMBER_SIZE; i++) {
+            int userNumber = userInputs.get(i);
+            int computerNumberIdx = computerRandomNumbers.indexOf(userNumber);
+            if (computerNumberIdx != i) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
