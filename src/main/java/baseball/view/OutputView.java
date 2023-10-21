b27745ad945ee18b6aabff0e1098fb3093254710
JoonKyo <joonkyolee@gmail.com>
2023-10-22 05:41:12 +0900

    style(OutputView): 띄어쓰기 분리

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 public class OutputView {
     private static final String START_GAME_MESSAGE = "숫자 야구 게임을 시작합니다.\n";
-    private static final String BALL_MESSAGE = "볼 ";
+    private static final String BALL_MESSAGE = "볼";
     private static final String STRIKE_MESSAGE = "스트라이크";
     private static final String NOTHING_MESSAGE = "낫싱";
-    private static final String MATHCES_ALL_NUMBERS = "3개의 숫자를 모두 맞히셨습니다.\n";
+    private static final String SPACE = " ";
+    private static final String MATHCES_ALL_NUMBERS = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n";
     private static final int BALL_INDEX_IN_LIST = 0;
     private static final int STRIKE_INDEX_IN_LIST = 1;
     private static final int ZERO = 0;
@@ -24,7 +25,7 @@ public class OutputView {
             System.out.println(NOTHING_MESSAGE);
         }
         if (ball > ZERO) {
-            System.out.println(ball + BALL_MESSAGE);
+            System.out.println(ball + BALL_MESSAGE + SPACE);
         }
         if (strike > ZERO) {
             System.out.println(strike + STRIKE_MESSAGE);
