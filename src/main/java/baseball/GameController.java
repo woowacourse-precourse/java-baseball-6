29e310c90523655920a35fa9c0efac5fa1cd1c93
junest66 <chlwndks333@gmail.com>
2023-10-21 17:38:28 +0900

    refactor: 비교 함수, 결과출력함수 컴퓨터 기능으로 변경

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -1,7 +1,5 @@
 package baseball;
 
-import java.util.List;
-
 public class GameController {
 
     private Computer computer;
@@ -30,40 +28,4 @@ public class GameController {
 
     }
 
-    public boolean compareNumbers(List<Integer> userNumbers, List<Integer> computerNumbers) {
-        int ballNum = 0;
-        int strikeNum = 0;
-
-        for (int i = 0; i < userNumbers.size(); i++) {
-            if (userNumbers.get(i) == computerNumbers.get(i)) {
-                strikeNum++;
-                continue;
-            }
-            if (computerNumbers.contains(userNumbers.get(i))) {
-                ballNum++;
-            }
-        }
-        printResult(ballNum, strikeNum);
-        if (strikeNum == 3) {
-            return true;
-        }
-        return false;
-    }
-
-    public void printResult(int ballNum, int strikeNum) {
-        if (ballNum != 0) {
-            System.out.print(ballNum + "볼 ");
-        }
-        if (strikeNum != 0) {
-            System.out.print(strikeNum + "스트라이크");
-        }
-        if (strikeNum == 0) {
-            System.out.print("낫싱");
-        }
-        System.out.println();
-        if (strikeNum == 3) {
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        }
-
-    }
 }
