c27134f7a1738c6832816aef5ee0bbf942ef3c75
parkchaerin <alicx991117@naver.com>
2023-10-21 17:41:05 +0900

    게임 진행 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,29 @@
 package baseball;
 
+import baseball.controller.BaseballGame;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String s;
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while(true) {
+            BaseballGame baseballGame = new BaseballGame();
+            baseballGame.baseballGame();
+
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            s=Console.readLine();
+
+            if(s.equals("1")){
+                continue;
+            }
+            else if(s.equals("2")){
+                break;
+            }
+            else{
+                throw new IllegalArgumentException();
+            }
+        }
     }
 }
--- src/main/java/baseball/controller/BaseballGame.java
+++ src/main/java/baseball/controller/BaseballGame.java
@@ -1,7 +1,45 @@
 package baseball.controller;
 
+import baseball.model.CountNum;
+import baseball.model.TargetNum;
+import baseball.model.UserNum;
+import camp.nextstep.edu.missionutils.Console;
 public class BaseballGame {
 
+    public void baseballGame(){
+        GenerateTargetNum generateTargetNum = new GenerateTargetNum();
+        TargetNum targetNum = generateTargetNum.getTargetNum();
+        String s;
+        while(true){
+            s=Console.readLine();
+            ValidUserNum validUserNum = new ValidUserNum(s);
+            UserNum userNum=validUserNum.getUserNum();
+
+            JudgeCountNum judgeCountNum = new JudgeCountNum(userNum,targetNum);
+            CountNum countNum = judgeCountNum.getCountNum();
+            int strike = countNum.getStrikeNum();
+            int ball = countNum.getBallNum();
+
+            if(strike==3){
+                System.out.println("3스트라이크");
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            }
+            else if(strike==0 && ball==0){
+                System.out.println("낫싱");
+            }
+            else if(strike==0){
+                System.out.println(ball+"볼");
+            }
+            else if(ball==0){
+                System.out.println(strike+"스트라이크");
+            }
+            else{
+                System.out.println(ball+"볼 "+strike+"스트라이크");
+            }
+
+        }
+    }
 
 
 }
