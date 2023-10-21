4537d2de6bda35f8bea323d799d209936b4ad405
HaLoomin <103578343+HaLoomin@users.noreply.github.com>
2023-10-22 04:05:25 +0900

    HitterTest 테스트 완료
    HitterTest의 테스트 내용을 입력하여 테스팅 하였고 성공하였습니다.

--- src/test/java/baseball/hitter/HitterTest.java
+++ src/test/java/baseball/hitter/HitterTest.java
@@ -1,20 +1,63 @@
 package baseball.hitter;
 
+import baseball.AppConfig;
+import baseball.game.hitter.Hitter;
+import baseball.game.util.IllegalArgumentCheck;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 public class HitterTest {
 
+    Hitter hitter;
+
     @BeforeEach
+    public void BeforeEach(){
+        AppConfig appConfig = new AppConfig();
+        hitter = appConfig.hitter();
+    }
 
     @Test
     void hitterSwingTest(){
-
+        //given
+        String bat = "";
+        String input = "123";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        //when
+        bat = hitter.swing();
+        //then
+        Assertions.assertThat(bat).isEqualTo(input);
     }
-//        PlayerHitterImpl hitter = new PlayerHitterImpl();
-//
-//        String bat = "";
-//
-//        System.out.println("입력:");
-//        bat = hitter.swing();
+
+    @Test
+    void hitterSwingIllegalArgumentTest(){
+        //given
+        String bat = "";
+        String input = "1233";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        //when
+        Assertions.assertThatThrownBy(() -> hitter.swing())
+                .isInstanceOf(IllegalArgumentException.class);
+        //then
+        //sucess
+    }
+
+    @Test
+    void hitterSwingIllegalArgumentTest2(){
+        //given
+        String bat = "";
+        String input = "12";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        //when
+        Assertions.assertThatThrownBy(() -> hitter.swing())
+                .isInstanceOf(IllegalArgumentException.class);
+        //then
+        //sucess
+    }
 }
