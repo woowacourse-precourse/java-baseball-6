fc3bda2e0ee7320496bd0e209bb666e27db0eb20
jaepyo99 <jaepyo99@naver.com>
2023-10-22 03:28:59 +0900

    [Feat] 컴퓨터가 생성한 번호 검증 메서드 추가
    컴퓨터가 생성한 숫자를 이전까지 검증된 번호리스트와 비교하여 중복되지 않은 번호임을 검증하는 메서드

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -1,4 +1,12 @@
 package baseball;
 
+
+import java.util.List;
+
 public class Validator {
+
+    // 컴퓨터를 통한 랜덤 야구번호를 검증해주는 메서드
+    public Boolean validNumNotInList(Integer invalidNum, List<Integer>validNumList){
+        return validNumList.contains(invalidNum);
+    }
 }
