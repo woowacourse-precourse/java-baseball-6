641caf7a07b969af9f2c23e5cfac653d49e8ec3f
wwan13 <wwan13@naver.com>
2023-10-21 15:18:28 +0900

    fix : `BaseballNumbers` 중복 검증 로직 변경, 컬렉션 LinkedHashSet에서 ArrayList로 변경 - 순서 보장과 중복값 제거 및 검증을 위해 `LinkedHashSet`을 사용하였으나 이전과 같은 검증 로직으로는 [1,1,2,3] 과 같이 중복 제거 후에도 크기가 3인 리스트가 입력되면 적절한 오류를 발생하지 못하였음. - `Set`을 중복 검증 용으로만 사용하도록 변경 - 위와 같은 변경으로 인해 필드에서 꼭 `LinkedHashSet` 사용할 이유가 없다고 판단, number() 메서드에서 반환시 `ArrayList`로 변환할 때 발생하는 오버헤드를 줄이기 위한 이유로 필드 에서 `LinkedHashSet`이 아닌 `ArrayList`를 사용하도록 변경 - number() 메서드가 즉시 `ArrayList`를 반환 - 오류 메세지 '크기', '중복' 구분

--- src/main/java/baseball/domain/baseballnumber/BaseballNumbers.java
+++ src/main/java/baseball/domain/baseballnumber/BaseballNumbers.java
@@ -1,33 +1,41 @@
 package baseball.domain.baseballnumber;
 
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class BaseballNumbers {
 
     private static final int NUMBERS_SIZE = 3;
-    private static final String ERROR_MESSAGE_WRONG_SIZE = "세 개의 숫자를 중복 없이 입력해 주세요.";
+    private static final String ERROR_MESSAGE_WRONG_SIZE = "세 개의 숫자를 입력해 주세요.";
+    private static final String ERROR_MESSAGE_DUPLICATED = "중복 없이 입력해 주세요.";
 
-    private Set<BaseballNumber> numbers = new LinkedHashSet<>();
+    private List<BaseballNumber> numbers = new ArrayList<>();
 
     public BaseballNumbers(List<Integer> input) {
+        validateSizeAndDuplicated(input);
         input.forEach((number) -> numbers.add(new BaseballNumber(number)));
-        validateSize();
     }
 
     public List<BaseballNumber> numbers() {
-        return new ArrayList<>(numbers);
+        return numbers;
     }
 
-    private void validateSize() {
-        if (!isRightSize()) {
+    private void validateSizeAndDuplicated(List<Integer> input) {
+        if (!isRightSizeInput(input)) {
             throw new IllegalArgumentException(ERROR_MESSAGE_WRONG_SIZE);
+        } else if (!isDuplicated(input)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_DUPLICATED);
         }
     }
 
-    private boolean isRightSize() {
-        return numbers.size() == NUMBERS_SIZE;
+    private boolean isRightSizeInput(List<Integer> input) {
+        return input.size() == NUMBERS_SIZE;
+    }
+
+    private boolean isDuplicated(List<Integer> input) {
+        Set<Integer> set = new HashSet<>(input);
+        return set.size() == NUMBERS_SIZE;
     }
 }
