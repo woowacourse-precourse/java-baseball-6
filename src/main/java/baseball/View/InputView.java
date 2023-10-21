68c2beb77143eb878e7fe594bf8f847e3dff0fd0
sungwon2598 <tjddnjs2598@gmail.com>
2023-10-21 20:25:41 +0900

    refactor(InputView) : 문자열 메시지를 상수로 변경

--- src/main/java/baseball/View/InputView.java
+++ src/main/java/baseball/View/InputView.java
@@ -1,12 +1,16 @@
 package baseball.View;
 
 public class InputView {
+    private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String CHECK_RESTART_GAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
     public void printNumberRequest(){
-        System.out.print("숫자를 입력해주세요 : ");
+        System.out.print(INPUT_NUMBER_MESSAGE);
     }
 
     public void printGameEnd(){
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println(GAME_END_MESSAGE);
+        System.out.println(CHECK_RESTART_GAME_MESSAGE);
     }
 }
