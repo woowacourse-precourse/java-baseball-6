e3f6085b5d7b27b3d9e46509bf79d28df5f4a687
rdyjun <rdyjun00@gmail.com>
2023-10-21 11:16:33 +0900

    refactor: change the way user data received
    remove Scanner and using missionutils.Console

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,18 +1,17 @@
 package baseball;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
 
 public class BaseballGame {
     /* 게임 시작 */
     public static void startGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
         final int numberLength = 3;
-        Scanner inputValue = new Scanner(System.in);
         Answer answer = new Answer(numberLength);
         String message;
         while (true) {
             System.out.print("숫자를 입력해주세요 : ");
-            String userAnswer = inputValue.nextLine();
+            String userAnswer = Console.readLine();
             checkInputValue(userAnswer, numberLength);
             int[] result = answer.getStatusArray(userAnswer);
             Status status = new Status(result);
