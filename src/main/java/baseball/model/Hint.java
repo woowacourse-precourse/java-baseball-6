9fa1b26e258314cb37bf840ddd7727241b859e91
heiler <threepebbles@naver.com>
2023-10-22 05:14:30 +0900

    refactor: 힌트 출력 기능 리팩토링

--- src/main/java/baseball/model/Hint.java
+++ src/main/java/baseball/model/Hint.java
@@ -4,27 +4,19 @@ public class Hint {
     int ballCount;
     int strikeCount;
 
-    public Hint() {
-    }
-
     public Hint(int ballCount, int strikeCount) {
         this.ballCount = ballCount;
         this.strikeCount = strikeCount;
     }
 
-    public int getBallCount() {
-        return ballCount;
-    }
-
-    public void setBallCount(int ballCount) {
-        this.ballCount = ballCount;
-    }
-
-    public int getStrikeCount() {
-        return strikeCount;
-    }
-
-    public void setStrikeCount(int strikeCount) {
-        this.strikeCount = strikeCount;
+    public String toString() {
+        if (ballCount == 0 && strikeCount == 0) {
+            return "낫싱";
+        } else if (ballCount == 0) {
+            return strikeCount + "스트라이크";
+        } else if (strikeCount == 0) {
+            return ballCount + "볼";
+        }
+        return ballCount + "볼 " + strikeCount + "스트라이크";
     }
 }
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,49 +3,15 @@ package baseball.view;
 import baseball.model.Hint;
 
 public class OutputView {
-    private static final String START_PROGRAM_MESSAGE = "숫자 야구 게임을 시작합니다.";
-    private static final String SUCCESS_AND_END__GAME_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    private static final String NOTHING_MESSAGE = "낫싱";
-    private static final String BALL_MESSAGE = "볼";
-    private static final String STRIKE_MESSAGE = "스트라이크";
-    private static final String SPACE = " ";
-
     public static void printStartProgramMessage() {
-        System.out.println(START_PROGRAM_MESSAGE);
+        System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
     public static void printSuccessAndEndGameMessage() {
-        System.out.println(SUCCESS_AND_END__GAME_MESSAGE);
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
     public static void printHintMessage(Hint hint) {
-        if (hint.getBallCount() == 0 && hint.getStrikeCount() == 0) {
-            printNothingMessage();
-        } else if (hint.getBallCount() == 0) {
-            printStrikeMessage(hint.getStrikeCount());
-        } else if (hint.getStrikeCount() == 0) {
-            printBallMessage(hint.getBallCount());
-        } else {
-            printBallStrikeMessage(hint.getBallCount(), hint.getStrikeCount());
-        }
-    }
-
-    public static void printNothingMessage() {
-        System.out.println(NOTHING_MESSAGE);
-    }
-
-    public static void printBallMessage(Integer ballCount) {
-        System.out.println(ballCount.toString() + BALL_MESSAGE);
-    }
-
-    public static void printStrikeMessage(Integer strikeCount) {
-        System.out.println(strikeCount.toString() + STRIKE_MESSAGE);
-    }
-
-    public static void printBallStrikeMessage(Integer ballCount, Integer strikeCount) {
-        String Message = ballCount.toString() + BALL_MESSAGE +
-                SPACE +
-                strikeCount.toString() + STRIKE_MESSAGE;
-        System.out.println(Message);
+        System.out.println(hint.toString());
     }
 }
