189ea24c1cf18aa51472eff00e1d8b2a703c8c40
김성현 <aruesin2@gmail.com>
2023-10-21 23:45:17 +0900

    feat: 3자리 여부 확인 isThreeDigits() 구현

--- src/main/java/baseball/Count.java
+++ src/main/java/baseball/Count.java
@@ -1,4 +1,26 @@
 package baseball;
 
+import java.util.Scanner;
+
 public class Count {
+    private String number;
+
+    public Count(){
+        System.out.println("숫자를 입력해주세요 : ");
+        Scanner sc = new Scanner(System.in);
+        String number = sc.next();
+
+    }
+
+    private void isThreeDigits(String number){
+        if (number.length() != 3) {
+            throw new IllegalArgumentException("3자리의 정수를 입력해야 합니다.");
+        }
+
+        try {
+            Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("3자리의 정수를 입력해야 합니다");
+        }
+    }
 }
