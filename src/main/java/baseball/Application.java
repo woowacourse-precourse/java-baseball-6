59ffd9948710cbb8653af8c34a316a96c016796a
star-candy <el0710@naver.com>
2023-10-21 20:53:14 +0900

    <feat> : <볼, 스트라이크 판단 기능 추가>

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,13 +13,21 @@ public class Application {
         int randomFirstNumber; //(100의 자리로 인식할 랜덤한 숫자)
         int randomSecondNumber; //(10의 자리로 인식할 랜덤한 숫자)
         int randomThirdNumber; //(1의 자리로 인식할 랜덤한 숫자)
-        int strikeCount, ballCount; //(스트라이크, 볼 횟수)
+        int strikeCount = 0; //(스트라이크 횟수)
+        int ballCount = 0; //(볼 횟수)
 
       randomNumber = generateRandomNumber();
       System.out.println("숫자 야구 게임을 시작합니다.");
+
+      while(strikeCount != 3){
       System.out.print("숫자를 입력해주세요 :");
       inputNumber = Integer.parseInt(Console.readLine());
+      strikeCount = generateGameGuess(inputNumber, randomNumber);
+      }
+      System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
+
     public static int generateRandomNumber() {
        int tempRandomNum = Randoms.pickNumberInRange(100, 999);
 
@@ -27,5 +35,36 @@ public class Application {
             tempRandomNum = Randoms.pickNumberInRange(100, 999);
         }
         return tempRandomNum;
+    }
+
+    public static int generateGameGuess(int inputNumber, int randomNumber) {
+        int strikeCount = 0; int ballCount = 0;
+        int[] inputArray = {inputNumber / 100, (inputNumber / 10) % 10, inputNumber % 10};
+        int[] randomArray = {randomNumber / 100, (randomNumber / 10) % 10, randomNumber % 10};
+        for(int i : randomArray){
+            if(inputArray[i] == randomArray[i]) {strikeCount++;}
+            else if (ballCounter(randomArray, inputArray[i])) {ballCount++;}
+        }
+        if(strikeCount == 0 && ballCount == 0) {
+            System.out.println("낫싱");
+        }
+        else if(strikeCount == 0 && ballCount > 0){
+            System.out.printf("%d볼", ballCount);
+        }
+        else if(strikeCount > 0 && ballCount == 0){
+            System.out.printf("%d스트라이크", strikeCount);
+        }
+        else {
+            System.out.printf("%d볼 %d스트라이크", ballCount, strikeCount);
+        }
+        return strikeCount;
+    }
+    public static boolean ballCounter(int[] randomArray, int inputValue) {
+        for (int i : randomArray) {
+            if (i == inputValue) {
+                return true;
+            }
+        }
+        return false;
     }
 }
