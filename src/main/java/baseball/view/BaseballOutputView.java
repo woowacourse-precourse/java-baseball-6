1d4d229057f7e86712bfc521679938cb0b783c00
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-22 00:14:26 +0900

    refactor(BaseballOutputView):패키지 구조 세분화로인한 수정

--- src/main/java/baseball/view/BaseballOutputView.java
+++ src/main/java/baseball/view/BaseballOutputView.java
@@ -1,19 +1,9 @@
 package baseball.view;
 
-import baseball.domain.BaseballScore;
-
-public class BaseballPrint extends BaseballScore {
-
-    public BaseballPrint(int ballCount, int strikeCount) {
-        super(ballCount, strikeCount);
-    }
+public class BaseballOutputView {
 
     public static void printStartMessage() {
         System.out.println("숫자 야구 게임을 시작합니다");
-    }
-
-    public static void printInputNumberMessage() {
-        System.out.print("숫자를 입력해주세요 : ");
     }
 
     public static void printBallAndStrikeCount(int ballCount, int strikeCount) {
@@ -36,7 +26,4 @@ public class BaseballPrint extends BaseballScore {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
-    public static void printRestartMessage() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
-    }
 }
