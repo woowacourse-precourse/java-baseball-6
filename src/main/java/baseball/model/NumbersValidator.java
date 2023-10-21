2fbe3abc960f827a70eef428968468fe6fd835eb
JoonKyo <joonkyolee@gmail.com>
2023-10-22 02:31:19 +0900

    feat(NumbersValidator): 입력한 값에 대한 유효성을 검증하는 메서드 추가

--- src/main/java/baseball/model/NumbersValidator.java
+++ src/main/java/baseball/model/NumbersValidator.java
@@ -1,5 +1,6 @@
 package baseball.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class NumbersValidator {
@@ -10,7 +11,57 @@ public class NumbersValidator {
     private static final int VALID_LENGTH = 3;
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 9;
-    private String playerNumber;
-    private List<Integer> playerNum;
+    private static List<Integer> playerNum;
 
+    // 입력한 값에 대한 유효성을 검증하고 IllegalArgumentException을 발생함
+    public static boolean validate(String numbers) {
+        if (!hasValidLength(numbers)) {
+            throw new IllegalArgumentException(INPUT_LENGTH_IS_NOT_VALID);
+        }
+        if (!hasOnlyNumbers(numbers)) {
+            throw new IllegalArgumentException(INPUT_IS_NOT_CONSIST_OF_NUMBERS);
+        }
+        convert(numbers); // 숫자로 이루어져 있다는 검증이 위의 두 개의 메서드를 통해 확인이 되었기 때문에 리스트로 변환
+        if (!isInValidRange()) {
+            throw new IllegalArgumentException(INPUT_NUMBER_IS_NOT_IN_RANGE);
+        }
+        if (hasDuplicatedNumbers()) {
+            throw new IllegalArgumentException(INPUT_HAS_DUPLICATED_NUMBER);
+        }
+        return true;
+    }
+
+    // 입력한 값의 길이가 3인지 확인하는 메서드
+    private static boolean hasValidLength(String numbers) {
+        return numbers.length() == VALID_LENGTH;
+    }
+
+    // 입력한 값이 숫자로만 이루어져 있는지 확인하는 메서드
+    private static boolean hasOnlyNumbers(String numbers) {
+        return numbers.chars()
+                .allMatch(Character::isDigit);
+    }
+
+    // 입력한 문자열을 정수형 리스트로 변환하는 메서드
+    private static void convert(String numbers) {
+        playerNum = new ArrayList<>();
+
+        for (String s : numbers.split("")) {
+            playerNum.add(Integer.parseInt(s));
+        }
+    }
+
+    // 입력한 값이 1 ~ 9 사이에 존재하는 숫자들로 구성되어 있는지 확인하는 메서드
+    private static boolean isInValidRange() {
+        return playerNum.stream()
+                .filter(number -> MIN_NUMBER <= number && number <= MAX_NUMBER)
+                .count() == VALID_LENGTH;
+    }
+
+    // 입력한 값에 중복되는 숫자가 존재하는지 확인하는 메서드
+    private static boolean hasDuplicatedNumbers() {
+        return playerNum.stream()
+                .distinct()
+                .count() != VALID_LENGTH;
+    }
 }
