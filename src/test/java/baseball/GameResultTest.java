909f7e366901d52ebf5741ce955cf1e0791a92b7
gemstoneyang <bennyws99@gmail.com>
2023-10-21 13:44:09 +0900

    test: Added isAllStrike() method test - GameResultTest의 isAllStrike() 메소드 테스트 추가

--- src/test/java/baseball/GameResultTest.java
+++ src/test/java/baseball/GameResultTest.java
@@ -1,6 +1,8 @@
 package baseball;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -23,5 +25,14 @@ public class GameResultTest {
 
         GameResult result4 = new GameResult(0, 2);
         assertEquals("2볼", result4.toString());
+    }
+
+    @Test
+    void 모든_숫자가_스트라이크가_맞는지_확인한다() {
+        GameResult result1 = new GameResult(1, 2);
+        assertFalse(result1.isAllStrike());
+
+        GameResult result2 = new GameResult(3, 0);
+        assertTrue(result2.isAllStrike());
     }
 }
