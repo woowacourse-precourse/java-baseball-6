7b9aabf55fdca18f794fa2260b208d1efcaf1b08
이준희 <juni416@naver.com>
2023-10-21 18:08:46 +0900

    green: 테스트코드 통과

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ArrayList<String> a = new ArrayList<>();
+        a.add("낫싱");
+        a.add("3스트라이크");
+        a.add("1볼 1스트라이크");
+        a.add("게임 종료");
+        System.out.println(a);
+        throw new IllegalArgumentException("1234");
     }
 }
