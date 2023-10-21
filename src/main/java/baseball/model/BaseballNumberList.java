99c0bdc143624ac9b1acf042b614a17f7fd8db41
devhoya97 <diajbh123@gmail.com>
2023-10-21 21:16:09 +0900

    refactor : BaseballNumberList 클래스 추가 BaseballNumber 객체를 담고 있을 BaseballNumberList 객체가 해야할 행동들이 많기 때문에 객체로 따로 선언함

--- src/main/java/baseball/model/BaseballNumberList.java
+++ src/main/java/baseball/model/BaseballNumberList.java
@@ -1,2 +1,37 @@
-package baseball.model;public class BaseballNumbers {
+package baseball.model;
+
+import baseball.utils.BaseballStringUtils;
+import baseball.utils.Validator;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseballNumberList {
+    private static final int BASEBALL_NUMBER_LIST_LENGTH = 3;
+    private List<BaseballNumber> baseballNumberList = new ArrayList<>();
+
+    public void setRandomBaseballNumberList() {
+        while (baseballNumberList.size() != BASEBALL_NUMBER_LIST_LENGTH) {
+            BaseballNumber randomBaseballNumber = BaseballNumber.getRandomBaseballNumber();
+            if (!baseballNumberList.contains(randomBaseballNumber)) {
+                baseballNumberList.add(randomBaseballNumber);
+            }
+        }
+    }
+
+    public void setBaseballNumberList(String baseballNumberListString) {
+        validateBaseballNumberListString(baseballNumberListString);
+        List<BaseballNumber> baseballNumbers = new ArrayList<>();
+        for (String baseballNumberString : baseballNumberListString.split(BaseballStringUtils.EMPTY_STRING)) {
+            int baseballNumberInt = Integer.parseInt(baseballNumberString);
+            BaseballNumber baseballNumber = new BaseballNumber(baseballNumberInt);
+            baseballNumbers.add(baseballNumber);
+        }
+    }
+
+    public static void validateBaseballNumberListString(String baseballNumberListString) {
+        Validator.validateInputEmptyOrNull(baseballNumberListString);
+        Validator.validateInputLength(baseballNumberListString, BASEBALL_NUMBER_LIST_LENGTH);
+        Validator.validateInputDistinct(baseballNumberListString);
+        Validator.validateInputDigit(baseballNumberListString);
+    }
 }
