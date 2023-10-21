5413f7ca6107f3f79aed87a5327c55a1fd2da8b5
민주 <hinote444@gmail.com>
2023-10-21 21:08:40 +0900

    feat(Controller): 사용자 입력에 대한 결과를 반환하는 기능 구현

--- src/main/java/baseball/controller/Controller.java
+++ src/main/java/baseball/controller/Controller.java
@@ -5,20 +5,29 @@ import baseball.domain.Player;
 import baseball.service.BaseballGame;
 import baseball.utils.Utils;
 import baseball.view.InputView;
+import baseball.view.OutputView;
 import java.util.List;
 
-public class controller {
+public class Controller {
     private Computer computer = new Computer();
     private Player player = new Player();
     private BaseballGame baseballGame = new BaseballGame();
 
-    public void startGame() {
-        setUpBaseballGame();
+    public void run() {
+        startGame();
+        PlayGame();
     }
 
-    public void setUpBaseballGame() {
+    public void startGame() {
+        OutputView.printStartGame();
         computer.setBaseballNumbers(baseballGame.createComputerNumbers());
+        OutputView.printInput();
         List<Integer> playerNumbers = Utils.parseIntegerList(InputView.inputBaseballNumbers());
         player.setBaseballNumbers(playerNumbers);
+    }
+
+    public void PlayGame() {
+        baseballGame.calculateNumbers(computer.getBaseballNumbers(), player.getBaseballNumbers());
+        OutputView.printCalculateResult(baseballGame.ballCount, baseballGame.strikeCount);
     }
 }
