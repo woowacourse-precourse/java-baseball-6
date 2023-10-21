6cab4306b097949d00f4b30c1cd373c181d21524
Minjae-An <mj3242@naver.com>
2023-10-21 17:34:37 +0900

    refactor : 코드 정렬

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -5,54 +5,54 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class Computer {
-    private final int START_INCLUSIVE=1;
-    private final int END_INCLUSIVE=9;
-    private final int LENGTH=3;
+    private final int START_INCLUSIVE = 1;
+    private final int END_INCLUSIVE = 9;
+    private final int LENGTH = 3;
 
     private List<Integer> computerNumbers;
 
-    private static class LazyHolder{
-        public static final Computer INSTANCE=new Computer();
+    private static class LazyHolder {
+        public static final Computer INSTANCE = new Computer();
     }
 
-    public static Computer getInstance(){
+    public static Computer getInstance() {
         return LazyHolder.INSTANCE;
     }
 
-    public List<Integer> generateRandomNumber(){
+    public List<Integer> generateRandomNumber() {
         List<Integer> uniqueNumbers = Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, LENGTH);
-        computerNumbers=uniqueNumbers;
+        computerNumbers = uniqueNumbers;
         return uniqueNumbers;
     }
 
-    public int getBallCount(List<Integer> userNumbers){
-        int count=0;
+    public int getBallCount(List<Integer> userNumbers) {
+        int count = 0;
         int userNumber, computerNumber;
 
-        for(int i=0; i<userNumbers.size(); i++){
-            userNumber=userNumbers.get(i);
+        for (int i = 0; i < userNumbers.size(); i++) {
+            userNumber = userNumbers.get(i);
 
-            for(int j=0; j<computerNumbers.size(); j++){
-                computerNumber=computerNumbers.get(j);
+            for (int j = 0; j < computerNumbers.size(); j++) {
+                computerNumber = computerNumbers.get(j);
 
-                if(userNumber==computerNumber && i!=j) count++;
+                if (userNumber == computerNumber && i != j) count++;
             }
         }
 
         return count;
     }
 
-    public int getStrikeCount(List<Integer> userNumbers){
-        int count=0;
+    public int getStrikeCount(List<Integer> userNumbers) {
+        int count = 0;
         int userNumber, computerNumber;
 
-        for(int i=0; i<userNumbers.size(); i++){
-            userNumber=userNumbers.get(i);
+        for (int i = 0; i < userNumbers.size(); i++) {
+            userNumber = userNumbers.get(i);
 
-            for(int j=0; j<computerNumbers.size(); j++){
-                computerNumber=computerNumbers.get(j);
+            for (int j = 0; j < computerNumbers.size(); j++) {
+                computerNumber = computerNumbers.get(j);
 
-                if(userNumber==computerNumber && i==j) count++;
+                if (userNumber == computerNumber && i == j) count++;
             }
         }
 
