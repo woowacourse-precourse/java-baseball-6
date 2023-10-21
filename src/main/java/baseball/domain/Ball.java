180e17c979ba2b06a1cfa2d5eb782290028a9ece
kunsanglee <gosmdochee2@gmail.com>
2023-10-21 18:41:36 +0900

    refactor(Ball): Ball 객체 생성시 유효범위 검증 메서드 추가

--- src/main/java/baseball/domain/Ball.java
+++ src/main/java/baseball/domain/Ball.java
@@ -4,12 +4,21 @@ import java.util.Objects;
 
 public class Ball {
     private static final int ZERO_COUNT = 0;
+    public static final int MAX_COUNT = 3;
     private static final String NO_BALL_MESSAGE = "";
     private static final String BALL_MESSAGE = "볼";
+    public static final String BALL_RANGE_EXCEPTION_MESSAGE = "볼의 유효 범위는 0~3 입니다.";
     private final int ball;
 
     public Ball(int ball) {
+        validateBallRange(ball);
         this.ball = ball;
+    }
+
+    private static void validateBallRange(int ball) {
+        if (ball < ZERO_COUNT || MAX_COUNT < ball) {
+            throw new IllegalArgumentException(BALL_RANGE_EXCEPTION_MESSAGE);
+        }
     }
 
     public boolean isEmpty() {
