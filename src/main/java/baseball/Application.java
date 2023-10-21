d90ec65b993ace44c221c01b99a96bbf48e0b2ea
yumble <hnjn1101@gmail.com>
2023-10-21 13:01:16 +0900

    feat: user input 함수 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,29 +1,59 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static baseball.Application.InputClass.extractRandomNumber;
+import static baseball.Application.InputClass.inputUserNumber;
+import static baseball.Application.PrintPackage.printGameStartMessage;
+import static baseball.Application.PrintPackage.printRequestNumber;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        gameStart();
     }
-    public void gameStart() {
+    public static void gameStart() {
         printGameStartMessage();
-        extractRandomNumber();
+        List<Integer> computer = extractRandomNumber();
+        while(compare(computer, inputUserNumber())){
+
+        }
     }
 
-    public static void printGameStartMessage() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+    private static Boolean compare(List<Integer> computer, List<Integer> user) {
+        return Boolean.TRUE;
     }
-    private static void extractRandomNumber() {
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
+    public class PrintPackage {
+        public static void printGameStartMessage() {
+            System.out.println("숫자 야구 게임을 시작합니다.");
+        }
+        public static void printRequestNumber() {
+            System.out.print("숫자를 입력해주세요 : ");
+        }
+    }
+    public class InputClass {
+        static List<Integer> extractRandomNumber() {
+            List<Integer> computer = new ArrayList<>();
+            while (computer.size() < 3) {
+                int randomNumber = Randoms.pickNumberInRange(1, 9);
+                if (!computer.contains(randomNumber)) {
+                    computer.add(randomNumber);
+                }
             }
+            return computer;
+        }
+        public static List<Integer> inputUserNumber() {
+            printRequestNumber();
+            String readLine = Console.readLine();
+            List<Integer> user = new ArrayList<>();
+            for( int i = 0 ; i < readLine.length(); i++) {
+                user.add(readLine.indexOf(i));
+            }
+            return user;
         }
     }
 }
