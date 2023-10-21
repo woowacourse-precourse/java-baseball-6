7df4ea0598204aa6a3a30c7c0be501325aa63508
jmj005 <jmj00005@naver.com>
2023-10-21 16:36:27 +0900

    feat: 스트라이크 확인 기능 추가

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -1,2 +1,17 @@
-package baseball;public class Referee {
+package baseball;
+
+public class Referee {
+    public int getStrikeCount(String computer, String player) {
+        int strikeCount = 0;
+        for (int index = 0; index < computer.length(); index++) {
+            if (isStrike(computer, player, index)) {
+                strikeCount++;
+            }
+        }
+        return strikeCount;
+    }
+
+    private boolean isStrike(String computer, String player, int index) {
+        return computer.charAt(index) == player.charAt(index);
+    }
 }
