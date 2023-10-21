0602ac6870f4ca0092c4af2f2ffb1d4bcaac3668
Kimhyunjin4455 <dp4455@naver.com>
2023-10-22 01:03:39 +0900

    RewriteAll

--- src/main/java/baseball/PlayRestart.java
+++
@@ -1,8 +0,0 @@
-package baseball;
-
-public class PlayRestart {
-    public boolean restart(){
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-    }
-}
