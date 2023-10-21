9816817409b7c744f42772923861367ff4570919
Sasha <106591445+kchaeys2@users.noreply.github.com>
2023-10-21 19:51:52 +0900

    feat(InputJudgmentService) : 스트라이크 판별 함수 생성

--- src/main/java/baseball/Service/InputJudgmentService.java
+++ src/main/java/baseball/Service/InputJudgmentService.java
@@ -1,11 +1,30 @@
 package baseball.Service;
 
+import baseball.DTO.Computer;
 import baseball.DTO.User;
+
+import java.util.List;
+import java.util.Objects;
 
 public class InputJudgmentService {
     private final User user;
+    private final Computer computer;
+    private Integer strike = 0;
+    private Integer ball;
 
-    public InputJudgmentService(User user){
+    public InputJudgmentService(User user,Computer computer){
         this.user = user;
+        this.computer = computer;
+    }
+
+    public Integer judgeStrike(List<Integer> userNumbers,List<Integer> computerNumbers){
+        for (int i=0; i <userNumbers.size(); i++){
+            if(Objects.equals(userNumbers.get(i), computerNumbers.get(i))){
+                computerNumbers.remove(i);
+                userNumbers.remove(i);
+                strike++;
+            }
+        }
+        return strike;
     }
 }
