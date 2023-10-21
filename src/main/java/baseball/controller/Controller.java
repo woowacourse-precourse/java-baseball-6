6ae485c7d3fb1c1b8b82793cb2f7bf9e9270acce
suhyun9764 <suhyun9764@naver.com>
2023-10-21 21:47:33 +0900

    style:controller->Controller 클래스명 수정

---
+++ src/main/java/baseball/controller/Controller.java
@@ -0,0 +1,49 @@
+package baseball.controller;
+
+
+import java.util.ArrayList;
+
+public class Controller {
+
+    private final Service service;
+    private static int continueDecision = 1;
+    public Controller() {
+        this.service = new Service();
+    }
+
+    public void run() {  // 게임 실행
+        while (continueDecision==1){
+            settingGame();
+            while (service.getStrike()<3){
+                startGame();
+            }
+        }
+        // 새로운 게임 생성(컴퓨터 랜덤 값 생성 및 저장) - settingGame();
+
+        /* 게임 시작 - startGame();
+        // 사용자 입력 받기
+        // 두 값 비교
+        // 점수 저장
+        // 점수 출력
+        */
+
+        //if(strike==3)
+        //selectContinueOrNot();
+    }
+
+    private void settingGame() {
+        ArrayList<Integer> computerAnswer = service.getComputerAnswer();
+        service.saveComputerAnswer(computerAnswer);
+    }
+
+    private void startGame() {
+        ArrayList<Integer> playerAnswer = service.getPlayerAnswer();
+        int[] scoreTable = service.compareTwoAnswer(playerAnswer);
+        service.printResult(scoreTable);
+    }
+
+    private void selectContinueOrNot(){
+        if(service.getPlayerDecision()==2)
+    }
+
+}
