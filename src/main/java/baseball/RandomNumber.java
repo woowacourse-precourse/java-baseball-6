ae68427335a5115b0f3cc03d6cf6326364ca0bdb
임강호 <55732952+Icln@users.noreply.github.com>
2023-10-21 22:15:16 +0900

    feat: 난수 생성 메서드 구현

--- src/main/java/baseball/RandomNumber.java
+++ src/main/java/baseball/RandomNumber.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,5 +14,17 @@ public class RandomNumber {
     public List<Integer> getRandomNumber() {
         return this.randomNumber;
     }
-    
+
+    public void randomNumberGenerate() {
+        if (randomNumber.size() >= 3) {
+            randomNumber = new ArrayList<>();
+        }
+
+        while (randomNumber.size() < 3) {
+            int num = Randoms.pickNumberInRange(1, 9);
+            if (!randomNumber.contains(num)) {
+                randomNumber.add(num);
+            }
+        }
+    }
 }
