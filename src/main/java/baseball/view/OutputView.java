a08b79dad9650084cf1c7860348db5fd8c068e37
dokkisan <dokkisan.wang@gmail.com>
2023-10-22 02:42:14 +0900

    add: getMassage() 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,18 +3,18 @@ package baseball.view;
 public class OutputView {
 
     private void printGameStart() {
-        System.out.println(GameInstructions.GUIDE_GAME_START);
+        System.out.println(GameInstructions.GUIDE_GAME_START.getMessage());
     }
 
     private void printGameSuccess() {
-        System.out.println(GameInstructions.GUIDE_GAME_SUCCESS);
+        System.out.println(GameInstructions.GUIDE_GAME_SUCCESS.getMessage());
     }
 
     private void printForInputThreeDigitNumbers() {
-        System.out.println(GameInstructions.ASK_INPUT_THREE_DIGIT_NUMBERS);
+        System.out.println(GameInstructions.ASK_INPUT_THREE_DIGIT_NUMBERS.getMessage());
     }
 
     private void printForInputRestartOrExit() {
-        System.out.println(GameInstructions.ASK_RESTART_OR_EXIT);
+        System.out.println(GameInstructions.ASK_RESTART_OR_EXIT.getMessage());
     }
 }
