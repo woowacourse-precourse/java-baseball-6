dbda57518b2d4eecd2639c6dd0063422e5ef89b8
devhoya97 <diajbh123@gmail.com>
2023-10-21 15:52:23 +0900

    refactor : 기존 코드를 BaseballGameController로 이전

--- src/main/java/baseball/BaseballGameController.java
+++ src/main/java/baseball/BaseballGameController.java
@@ -1,11 +1,72 @@
 package baseball;
 
-public class BaseballGameController {
-    public void playGames() {
+import java.util.List;
 
+public class BaseballGameController {
+
+    private static final String GAME_RESTART = "1";
+    private static final String GAME_OVER = "2";
+    private static final int NUMBERS_LENGTH = 3;
+
+    public static void playGames() {
+        OutputView.printGameStart();
+        do {
+            playGameOnce();
+            OutputView.printAnswer();
+        } while (getGameRestart().equals(GAME_RESTART));
     }
 
-    private void playGame() {
+    public static void playGameOnce() {
+        List<BaseballNumber> randomBaseballNumbers = BaseballNumbersFactory.getRandomBaseballNumbers();
+        int ballCount;
+        int strikeCount;
+        do {
+            List<BaseballNumber> userBaseballNumbers = getUserBaseballNumbers();
+            ballCount = getBallCount(randomBaseballNumbers, userBaseballNumbers);
+            strikeCount = getStrikeCount(randomBaseballNumbers, userBaseballNumbers);
+            OutputView.printBallStrikeCount(ballCount, strikeCount);
+        } while (!isAnswer(strikeCount));
+    }
 
+    public static List<BaseballNumber> getUserBaseballNumbers() {
+        String baseballNumbersString = InputView.getConsoleInput();
+        return BaseballNumbersFactory.getBaseballNumbers(baseballNumbersString);
+    }
+
+    public static int getBallCount(List<BaseballNumber> randomBaseballNumbers,
+                                   List<BaseballNumber> userBaseballNumbers) {
+        int ballCount = 0;
+        for (int i = 0; i < userBaseballNumbers.size(); i++) {
+            BaseballNumber userBaseballNumber = userBaseballNumbers.get(i);
+            BaseballNumber randomBaseballNumber = randomBaseballNumbers.get(i);
+            if (!userBaseballNumber.equals(randomBaseballNumber) &&
+                    randomBaseballNumbers.contains(userBaseballNumber)) {
+                ballCount++;
+            }
+        }
+        return ballCount;
+    }
+
+    public static int getStrikeCount(List<BaseballNumber> randomBaseballNumbers,
+                                     List<BaseballNumber> userBaseballNumbers) {
+        int strikeCount = 0;
+        for (int i = 0; i < userBaseballNumbers.size(); i++) {
+            BaseballNumber userBaseballNumber = userBaseballNumbers.get(i);
+            BaseballNumber randomBaseballNumber = randomBaseballNumbers.get(i);
+            if (userBaseballNumber.equals(randomBaseballNumber)) {
+                strikeCount++;
+            }
+        }
+        return strikeCount;
+    }
+
+    public static boolean isAnswer(int strikeCount) {
+        return strikeCount == NUMBERS_LENGTH;
+    }
+
+    public static String getGameRestart() {
+        String gameRestartOrNot = InputView.getConsoleInput();
+        Validation.validateGameRestartOrNot(gameRestartOrNot, GAME_RESTART, GAME_OVER);
+        return gameRestartOrNot;
     }
 }
