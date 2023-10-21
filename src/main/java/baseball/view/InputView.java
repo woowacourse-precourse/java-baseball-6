54752005cbbf614fc3b7e9a3fccbbfc412e245e0
elyudwo <dudqk9696@naver.com>
2023-10-21 21:55:30 +0900

    [Refactor] : InputView 객체 Enum 값 연결 문자열 출력하도록 수정

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -9,20 +9,20 @@ import java.util.List;
 public class InputView {
 
     public void printStart() {
-        System.out.println(InputMessage.START);
+        System.out.println(InputMessage.START.getMessage());
     }
 
     public List<Integer> printInsertNumber() {
-        System.out.println(InputMessage.INSERT_NUMBER);
+        System.out.println(InputMessage.INSERT_NUMBER.getMessage());
         return Utils.stringToList(Console.readLine());
     }
 
     public void printCorrect() {
-        System.out.println(InputMessage.CORRECT);
+        System.out.println(InputMessage.CORRECT.getMessage());
     }
 
     public int printEnd() {
-        System.out.println(InputMessage.END);
+        System.out.println(InputMessage.END.getMessage());
         return Utils.stringToInt(Console.readLine());
     }
 
