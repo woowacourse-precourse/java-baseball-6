adffb7d938a2cb8771b435e7f682d89fa681fb5a
김명준 <86913355+mjj111@users.noreply.github.com>
2023-10-21 17:05:32 +0900

    refactor(Player): 생성자 메서드에서 들어오는 값을 String으로 받아 List<Integer>로 변환하여 초기화하도록 변경
    Player의 인스턴스 변수 inputAnswer은 List<Integer> 이기 때문에, 이전의 경우에는 같은 타입을 넣어줘야 했습니다. 그러나 String 값을 List<Integer>로 변환하는 기능에 대한 책임을 어디에 둘까 고민하다가 Player 안에서만 이뤄지기 때문에 Player에게 넣기로 결정했습니다.

--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -1,15 +1,26 @@
 package baseball.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
     private final List<Integer> inputAnswer;
 
-    public Player(List<Integer> inputAnswer) {
-        this.inputAnswer = inputAnswer;
+    public Player(String stringInputAnswer) {
+        this.inputAnswer = convertStringInputToIntegerList(stringInputAnswer);
     }
 
     public List<Integer> getInputAnswer() {
         return inputAnswer;
+    }
+
+    private List<Integer> convertStringInputToIntegerList(String stringInput) {
+        List<Integer> integerList = new ArrayList<>();
+
+        for (char c : stringInput.toCharArray()) {
+            integerList.add(Character.getNumericValue(c));
+        }
+
+        return integerList;
     }
 }
