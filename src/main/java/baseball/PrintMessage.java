e92f4608bd6da70e10c04470d28e1e65bf426847
lee jihyeon <lejinhy@gmail.com>
2023-10-21 17:53:26 +0900

    "style: 에디터를 사용해서 커밋 테스트"

--- src/main/java/baseball/PrintMessage.java
+++ src/main/java/baseball/PrintMessage.java
@@ -24,7 +24,6 @@ public class PrintMessage {
         }
         System.out.println();
     }
-
     public void gameEnd() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
