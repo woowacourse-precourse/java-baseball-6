d6c22f21c57a31af1a54d0a597f9b98380b62363
watermelon3737 <pshrina1@naver.com>
2023-10-21 17:33:25 +0900

    [feat] : 콘솔로 inputLine을 받는 메소드 구현 [test] : console 테스트 구현

---
+++ src/main/java/baseball/BaseballGameMachine.java
@@ -0,0 +1,9 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class BaseballGameMachine {
+    public String input() {
+        return Console.readLine();
+    }
+}
---
+++ src/test/java/baseball/BaseballGameMachineTest.java
@@ -0,0 +1,30 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.in;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BaseballGameMachineTest {
+
+    @DisplayName("console 테스트")
+    @Test
+    void input() {
+        //given
+        String inputLine = "콘솔로 입력합니다.";
+        InputStream inputStream = new ByteArrayInputStream(inputLine.getBytes());
+        System.setIn(inputStream);
+
+        BaseballGameMachine baseballGameMachine = new BaseballGameMachine();
+        //when
+        String input = baseballGameMachine.input();
+        //then
+        assertThat(inputLine).isEqualTo(input);
+    }
+}
