e3212bd66879f4a386e2eb2476b1253c57bdc8cf
JaeHunJang <wuddlaa@gmail.com>
2023-10-21 17:33:46 +0900

    feat(InputView): Create InputView

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,15 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static String inputThreeNumbers() {
+        System.out.print("숫자를 입력해주세요 : ");
+        return Console.readLine();
+    }
+
+    public static String inputRestart() {
+        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return Console.readLine();
+    }
+}
