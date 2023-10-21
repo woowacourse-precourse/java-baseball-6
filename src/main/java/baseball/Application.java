bfb7ef21a11a786547d60aa74db1b94907091784
Kim Daehyeon <daehyeon3351@gmail.com>
2023-10-21 20:15:26 +0900

    feat: 숫자를 입력받고, 잘못된 입력이면 예외를 발생기키는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+    public static boolean isStringAllDigits(String input) {
+        if (input.isEmpty()) {
+            return false;
+        }
+        for (char c : input.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        // TODO: 사용자에게 숫자를 입력받고, 적절하지 않은 입력이면 IllegalArgumentException을 발생기키는 기능 구현
+        // TODO: 사용자가 입력한 수를 컴퓨터가 입력한 수와 비교하며 ball 갯수와 strike 갯수를 세는 기능 구현
+        boolean continueGame = true;
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -14,5 +28,20 @@ public class Application {
                 computer.add(randomNumber);
             }
         }
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        do {
+            System.out.print("숫자를 입력해주세요 : ");
+            List<Integer> user = new ArrayList<>();
+            String userInput;
+            userInput = Console.readLine();
+            if (userInput.length() != 3 || !isStringAllDigits(userInput)) {
+                throw new IllegalArgumentException(userInput);
+            }
+
+            for (char c : userInput.toCharArray()) {
+                user.add(Character.getNumericValue(c));
+            }
+        } while (continueGame);
+        System.out.println("게임 종료");
     }
 }
