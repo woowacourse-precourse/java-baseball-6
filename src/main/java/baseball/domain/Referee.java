947f8a144b55abc73bce4344453c6045b29ca5ac
706com <ehdgusjdbswl@gmail.com>
2023-10-22 03:36:58 +0900

    feat : 심판(관리자) 도메인 객체 생성

---
+++ src/main/java/baseball/domain/Referee.java
@@ -0,0 +1,57 @@
+package baseball.domain;
+
+import baseball.view.OutputView;
+
+public class Referee {
+
+    private Computer computer;
+
+    private Player player;
+
+    private int strike;
+
+    private int ball;
+
+    public Referee(Computer computer,Player player) {
+        this.computer = computer;
+        this.player = player;
+        this.strike = 0;
+        this.ball = 0;
+    }
+
+    public void checkResult(){
+        init();
+        int strike = checkStrike();
+        int ball = checkBall();
+
+        OutputView outputView = new OutputView();
+        outputView.printResultMessage(strike,ball);
+    }
+
+    void init(){
+        this.strike = 0;
+        this.ball = 0;
+    }
+
+    int checkStrike(){
+        for(int i=0; i<computer.getNumber().size(); i++){
+            for(int j=0; j<player.getNumber().size(); j++){
+                if(computer.getNumber().get(i) == player.getNumber().get(j) && i==j){
+                    this.strike++;
+                }
+            }
+        }
+        return strike;
+    }
+
+    int checkBall(){
+        for(int i=0; i<computer.getNumber().size(); i++){
+            for(int j=0; j<player.getNumber().size(); j++){
+                if(computer.getNumber().get(i) == player.getNumber().get(j) && i!=j){
+                    this.ball++;
+                }
+            }
+        }
+        return ball;
+    }
+}
