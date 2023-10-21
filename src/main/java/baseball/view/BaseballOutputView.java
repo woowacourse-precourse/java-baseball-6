25b8285b4e1fe34c95f6c4d52389db5914732ad7
kunsanglee <gosmdochee2@gmail.com>
2023-10-22 00:34:16 +0900

    refactor(BaseballOutputView): OutputView 인터페이스를 구현하도록 변경

--- src/main/java/baseball/view/BaseballOutputView.java
+++ src/main/java/baseball/view/BaseballOutputView.java
@@ -2,20 +2,20 @@ package baseball.view;
 
 import baseball.domain.Score;
 
-public class BaseballOutputView {
+public class BaseballOutputView implements OutputView {
 
     private static final String START_BASEBALL_GAME_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String END_BASEBALL_GAME_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
-    public static void startGame() {
+    public void startGame() {
         System.out.println(START_BASEBALL_GAME_MESSAGE);
     }
 
-    public static void endGame() {
+    public void endGame() {
         System.out.println(END_BASEBALL_GAME_MESSAGE);
     }
 
-    public static void matchResult(Score score) {
+    public void matchResult(Score score) {
         System.out.println(score.toString());
     }
 }
