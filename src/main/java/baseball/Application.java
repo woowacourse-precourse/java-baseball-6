81b4d5127c50917d5d6fba8a90d2e417781fb2c7
임강호 <55732952+Icln@users.noreply.github.com>
2023-10-21 22:23:33 +0900

    feat: 결과 출력 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -31,6 +31,20 @@ public class Application {
                     ball += 1;
                 }
             }
+
+            if (strike == 0 && ball == 0) {
+                System.out.println("낫싱");
+            } else if (strike == 3) {
+                System.out.println("3스트라이크");
+                System.out.println("3개의 숫자를 모두 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하시면 2를 입력하세요.");
+            } else if (ball == 0) {
+                System.out.println(strike + "스트라이크");
+            } else if (strike == 0) {
+                System.out.println(ball + "볼");
+            } else {
+                System.out.println(ball + "볼 " + strike + "스트라이크");
+            }
         }
     }
 }
