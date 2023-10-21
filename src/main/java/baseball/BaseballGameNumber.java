dfed663f0337d90dd425fc03b20560d1d0ad1725
chanhoim <cksgh201@naver.com>
2023-10-21 18:10:54 +0900

    Feat : 채점 구현
    입력된 숫자는 길이다 3, 각 자릿수의 숫자는 1~9 중 하나이며 중복이 있어선 안된다.

--- src/main/java/baseball/BaseballGameNumber.java
+++ src/main/java/baseball/BaseballGameNumber.java
@@ -3,6 +3,7 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 class BaseballGameNumber {
 
@@ -16,5 +17,38 @@ class BaseballGameNumber {
                 numbers.add(randomNumber);
             }
         }
+    }
+
+    public BaseballGameNumber(String input) {
+        numbers = new ArrayList<>();
+        int n = Integer.parseInt(input);
+        int divisor = 100;
+        while (numbers.size() < 3) {
+            int quotient = n/divisor;
+            numbers.add(quotient);
+            n-=quotient*divisor;
+            divisor/=10;
+        }
+    }
+
+    public static boolean validateBaseballGameNumber (String input) throws IllegalStateException {
+        if(!validateFormat(input) || !validateDuplication(input)) {
+            throw new IllegalArgumentException();
+        }
+        return true;
+    }
+
+    static private boolean validateFormat (String input) {
+        return Pattern.matches("^[1-9]{3}",input);
+    }
+
+    static private boolean validateDuplication (String input) {
+        for (int i = 0; i < 3; i++) {
+            char c = input.charAt(i);
+            for (int j = 0; j < 3; j++) {
+                if(i != j && c == input.charAt(j)) return false;
+            }
+        }
+        return true;
     }
 }
