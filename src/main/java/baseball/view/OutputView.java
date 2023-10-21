85a59c881ab5eebc8433bbbfeb29db45110be8fa
shin5774 <babo5774@naver.com>
2023-10-21 18:40:26 +0900

    Feat: "사용자에게 보여지는 Outputview 추가"
    - 사용자에게 보여지는 문구들과 결과를 출력하는 메서드를 가진 클래스 추가

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,15 @@
+package baseball.view;
+
+public class OutputView {
+    public void printProgramStartMessage(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void printFinishGameMessage(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public void printResult(String result){
+        System.out.println(result);
+    }
+}
