5e676f4beb24fa79c3b4f0fb1cb2d2635f58ecd0
pjm2476 <pjm2476@gmail.com>
2023-10-21 23:47:14 +0900

    feat: 유저의 입력이 올바른 길이를 가지는지 확인

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -5,25 +5,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class User {
-
     private static final String ENTER_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
-
     private static final int NUMBER_LEN = 3;
-
     public List<Integer> userAnswer = new ArrayList<Integer>();
 
     public void enterNumber() {
         System.out.print(ENTER_NUMBER_MESSAGE);
         String number = Console.readLine();
         setUserAnswer(number);
-
     }
 
-    public void setUserAnswer(String number) {
-        for (int i = 0; i < NUMBER_LEN; i++) {
-            userAnswer.add(Character.getNumericValue(number.charAt(i)));
+    private void setUserAnswer(String number) {
+        if (isValidInput(number)) {
+            for (int i = 0; i < NUMBER_LEN; i++) {
+                userAnswer.add(Character.getNumericValue(number.charAt(i)));
+            }
+        } else {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
         }
     }
 
+    private boolean isValidInput(String number) {
+        if (!checkValidLength(number)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
+    // 올바른 길이로 이루어져 있는지 검사하는 함수
+    private boolean checkValidLength(String number) {
+        if (number.length() != NUMBER_LEN) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
