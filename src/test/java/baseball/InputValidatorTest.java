6017522e2852275fd8ea96c4f043fa1ffb402a1f
Jae-Young98 <ygy0102@gmail.com>
2023-10-22 02:52:13 +0900

    test(InputValidatorTest): 통합 검증 테스트를 제외한 나머지 테스트 삭제

--- src/test/java/baseball/InputValidatorTest.java
+++ src/test/java/baseball/InputValidatorTest.java
@@ -1,13 +1,10 @@
 package baseball;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import baseball.logic.InputValidator;
+import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
-import baseball.logic.InputValidator;
-import java.util.ArrayList;
-import java.util.List;
-import org.junit.jupiter.api.Test;
 
 public class InputValidatorTest {
 
@@ -16,102 +13,13 @@ public class InputValidatorTest {
     @Test
     void 사용자_입력_통합_검증() {
         // given
-        String input = "369";
-
-        // when
-        List<Integer> userInput = validator.validateInput(input);
-
-        // then
-        assertThat(userInput.size()).isEqualTo(3);
-        assertThat(userInput.getClass().getSimpleName()).isEqualTo("ArrayList");
-        assertThat(userInput).contains(3, 6, 9);
-    }
-
-    @Test
-    void 숫자_길이_예외_발생() {
-        // given
-        String input = "3478";
+        String CorrectInput = "369";
+        String ErrorInput = "1234";
 
         // when, then
-        assertThatThrownBy(() -> validator.validateLength(input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("입력한 숫자의 길이를 확인하세요.");
-    }
+        assertThatNoException().isThrownBy(() -> validator.validateInput(CorrectInput));
+        assertThatThrownBy(() -> validator.validateInput(ErrorInput))
+                .isInstanceOf(IllegalArgumentException.class);
 
-    @Test
-    void 사용자_입력_리스트_변환() {
-        // given
-        String input = "234";
-
-        // when
-        List<Integer> userInput = validator.convertStrToList(input);
-
-        // then
-        assertThat(userInput.size()).isEqualTo(3);
-        assertThat(userInput.getClass().getSimpleName()).isEqualTo("ArrayList");
-    }
-
-    @Test
-    void 사용자_입력_범위_검증() {
-        // given
-        List<Integer> correctInput = new ArrayList<>();
-        List<Integer> errorInput = new ArrayList<>();
-
-        // when
-        correctInput.add(1);
-        correctInput.add(7);
-        correctInput.add(6);
-        errorInput.add(0);
-        errorInput.add(1);
-        errorInput.add(3);
-
-        // then
-        assertThatNoException().isThrownBy(() ->
-                validator.validateRange(correctInput));
-
-        assertThatThrownBy(() ->
-                validator.validateRange(errorInput))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("입력 값의 범위(1~9)를 확인하세요.");
-    }
-
-    @Test
-    void 사용자_입력_중복_검증() {
-        // given
-        List<Integer> correctInput = new ArrayList<>();
-        List<Integer> errorInput = new ArrayList<>();
-
-        // when
-        correctInput.add(1);
-        correctInput.add(7);
-        correctInput.add(6);
-        errorInput.add(2);
-        errorInput.add(3);
-        errorInput.add(3);
-
-        // then
-        assertThatNoException().isThrownBy(() ->
-                validator.validateDuplicate(correctInput));
-
-        assertThatThrownBy(() ->
-                validator.validateDuplicate(errorInput))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("중복된 숫자가 존재합니다.");
-    }
-
-    @Test
-    void 사용자_입력_명령어_검증() {
-        // given
-        String correctInput = "1";
-        String wrongInput = "3";
-
-        // when, then
-        assertThatNoException().isThrownBy(() ->
-                validator.validateCommand(correctInput));
-
-        assertThatThrownBy(() ->
-                validator.validateCommand(wrongInput))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("명령어를 확인하세요. 1 (재시작), 2 (종료)");
     }
 }
