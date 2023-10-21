e89847c64182be5712e35d98771c3dc98d268e55
EunKyo Chae <dmsry1022@gmail.com>
2023-10-21 22:56:49 +0900

    README 임시 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) {  // 프로그램 실행의 시작점
         // TODO: 프로그램 구현
+
+
     }
 }
