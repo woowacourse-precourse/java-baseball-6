c84d3376385d85024d69555ab16ffcaa0c367ad6
heiler <threepebbles@naver.com>
2023-10-22 07:32:32 +0900

    refactor: 컨트롤러 영역에서 getter/setter 없애기

--- src/main/java/baseball/controller/BaseballGameController.java
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -1,73 +1,29 @@
 package baseball.controller;
 
-import static baseball.utils.Convert.booleanToInt;
-
 import baseball.model.Computer;
-import baseball.model.Hint;
 import baseball.model.NumberList;
-import baseball.model.User;
 import baseball.utils.Convert;
 import baseball.validator.Validator;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.List;
 
 public class BaseballGameController {
     public void startGame() {
         Computer computer = new Computer();
-        User user = new User();
+        NumberList guess;
 
-        createRandomAnswer(computer);
+        computer.createRandomAnswer();
         do {
-            guessing(user);
-            OutputView.printHintMessage(calculateHint(computer.getAnswer(), user.getGuess()));
-        } while (!computer.getAnswer().equals(user.getGuess()));
+            guess = guessing();
+            OutputView.printHintMessage(computer.calculateHint(guess));
+        } while (!computer.isAnswer(guess));
         OutputView.printSuccessAndEndGameMessage();
     }
 
-    private void guessing(User user) {
+    private NumberList guessing() {
         String guessString = InputController.scanUsersGuess();
-        NumberList guess = Convert.stringToNumberList(guessString);
-        user.setGuess(guess);
-    }
-
-    public void createRandomAnswer(Computer computer) {
-        List<Integer> answerList = new ArrayList<>() {{
-            while (size() < 3) {
-                int randomNumber = Randoms.pickNumberInRange(1, 9);
-                if (!contains(randomNumber)) {
-                    add(randomNumber);
-                }
-            }
-        }};
-        computer.setAnswer(new NumberList(answerList));
-    }
-
-    private Hint calculateHint(NumberList answer, NumberList guess) {
-        int ballCount = calculateBallCount(answer, guess);
-        int strikeCount = calculateStrikeCount(answer, guess);
-        return new Hint(ballCount, strikeCount);
-    }
-
-    private int calculateBallCount(NumberList answer, NumberList guess) {
-        int ballCount = 0;
-        for (int i = 0; i < answer.size(); i++) {
-            for (int j = 0; i != j && j < guess.size(); j++) {
-                ballCount += booleanToInt(answer.get(i).equals(guess.get(j)));
-            }
-        }
-        return ballCount;
-    }
-
-    private int calculateStrikeCount(NumberList answer, NumberList guess) {
-        int strikeCount = 0;
-        for (int i = 0; i < answer.size(); i++) {
-            strikeCount += booleanToInt(answer.get(i).equals(guess.get(i)));
-        }
-        return strikeCount;
+        return Convert.stringToNumberList(guessString);
     }
 
     private static class InputController {
--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -1,5 +1,11 @@
 package baseball.model;
 
+import static baseball.utils.Convert.booleanToInt;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Computer {
     private NumberList answer;
 
@@ -7,7 +13,42 @@ public class Computer {
         return answer;
     }
 
-    public void setAnswer(NumberList answer) {
-        this.answer = answer;
+    public boolean isAnswer(NumberList guess) {
+        return answer.equals(guess);
+    }
+
+    public void createRandomAnswer() {
+        List<Integer> answerList = new ArrayList<>() {{
+            while (size() < 3) {
+                int randomNumber = Randoms.pickNumberInRange(1, 9);
+                if (!contains(randomNumber)) {
+                    add(randomNumber);
+                }
+            }
+        }};
+        answer = new NumberList(answerList);
+    }
+
+    public Hint calculateHint(NumberList guess) {
+        return new Hint(calculateBallCount(answer, guess),
+                calculateStrikeCount(answer, guess));
+    }
+
+    private int calculateBallCount(NumberList answer, NumberList guess) {
+        int ballCount = 0;
+        for (int i = 0; i < answer.size(); i++) {
+            for (int j = 0; i != j && j < guess.size(); j++) {
+                ballCount += booleanToInt(answer.get(i).equals(guess.get(j)));
+            }
+        }
+        return ballCount;
+    }
+
+    private int calculateStrikeCount(NumberList answer, NumberList guess) {
+        int strikeCount = 0;
+        for (int i = 0; i < answer.size(); i++) {
+            strikeCount += booleanToInt(answer.get(i).equals(guess.get(i)));
+        }
+        return strikeCount;
     }
 }
