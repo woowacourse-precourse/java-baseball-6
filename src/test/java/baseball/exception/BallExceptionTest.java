e800e5ff674b74c5889b1cf3de21fd9bd27ecefd
suihub <qkdcrt@gmail.com>
2023-10-21 14:48:43 +0900

    <refactor>: "볼 숫자 입력 예외 테스트 상수 추출"
    각 메서드에서 개별적으로 테스트할 컨트롤러를 생성하고 있는 걸 확인.
    상수로 만들어서 공유하도록 수정

--- src/test/java/baseball/exception/BallExceptionTest.java
+++ src/test/java/baseball/exception/BallExceptionTest.java
@@ -5,64 +5,60 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 class BallExceptionTest {
+    private final BallException exception = new BallException();
 
     @Test
     void 숫자_0_입력시_예외_발생(){
-        BallException ballException = new BallException();
         String number = "025";
         assertThatThrownBy(() ->
-                ballException.validation(number))
+                exception.validation(number))
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() ->
-                ballException.validation(number))
+                exception.validation(number))
                 .hasMessage("1~9까지의 숫자를 입력해주세요.");
     }
 
     @Test
     void 글자_입력시_예외_발생(){
-        BallException ballException = new BallException();
         String number = "가나다";
         assertThatThrownBy(() ->
-                ballException.validation(number))
+                exception.validation(number))
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() ->
-                ballException.validation(number))
+                exception.validation(number))
                 .hasMessage("1~9까지의 숫자를 입력해주세요.");
     }
 
     @Test
     void 숫자_2개_이하일시_예외_발생(){
-        BallException ballException = new BallException();
         String number = "15";
         assertThatThrownBy(() ->
-                ballException.validation(number))
+                exception.validation(number))
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() ->
-                ballException.validation(number))
+                exception.validation(number))
                 .hasMessage("숫자는 3개만 입력해주세요.");
     }
 
     @Test
     void 숫자_4개_이상일시_예외_발생(){
-        BallException ballException = new BallException();
         String number = "1598";
         assertThatThrownBy(() ->
-                ballException.validation(number))
+                exception.validation(number))
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() ->
-                ballException.validation(number))
+                exception.validation(number))
                 .hasMessage("숫자는 3개만 입력해주세요.");
     }
 
     @Test
     void 숫자_중복일시_예외_발생(){
-        BallException ballException = new BallException();
         String number = "155";
         assertThatThrownBy(() ->
-                ballException.validation(number))
+                exception.validation(number))
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() ->
-                ballException.validation(number))
+                exception.validation(number))
                 .hasMessage("숫자는 중복없이 입력해주세요.");
     }
 }
