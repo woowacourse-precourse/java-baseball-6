67bfbf3dc3983f436d4092c8b7ecd8a959d38ef4
Austin-Choi <austinsupra@gmail.com>
2023-10-21 10:44:07 +0900

    Feat: strike counting 기능 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -29,4 +29,22 @@ public class BaseballGame {
         }
         return totalBall;
     }
+    private int countStrike(final int computerNumber,final int playerNumber){
+        if(computerNumber == playerNumber)
+            return 1;
+        return 0;
+    }
+
+    private int sumStrike(final ComputerNumber computer, final GuessNumber guess){
+        int totalStrike = 0;
+        List<Integer> computerNumbers = computer.getNumbers();
+        List<Integer> guessNumbers = guess.getNumbers();
+        for(int index = 0; index < 3; index++){
+            int currentPlayerNum = guessNumbers.get(index);
+            int currentComputerNum = computerNumbers.get(index);
+            totalStrike += countStrike(currentComputerNum, currentPlayerNum);
+        }
+        return totalStrike;
+    }
+
 }
