e70954689910874578351d2028e172ad9e37659f
dukcode <duk9741@gmail.com>
2023-10-21 16:42:16 +0900

    docs: ReplayableGameSystemView 자바독 추가

--- src/main/java/baseball/game/system/ReplayableGameSystemView.java
+++ src/main/java/baseball/game/system/ReplayableGameSystemView.java
@@ -2,5 +2,10 @@ package baseball.game.system;
 
 public interface ReplayableGameSystemView {
 
+    /**
+     * 게임 재시작 여부를 입력 받아 boolean으로 반환한다.
+     *
+     * @return 게임 재시작 입력을 받으면 true, 게임 종료 입력을 받으면 false를 반환한다.
+     */
     boolean requestReplayInput();
 }
