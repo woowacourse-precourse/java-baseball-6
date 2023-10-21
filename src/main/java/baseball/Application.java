d67f195da334ce7f5fc9e80f6289518d39054d90
limzzum <chfhddl1234@naver.com>
2023-10-22 02:40:20 +0900

    :sparkles: feat : 사용자에게 숫자 입력 받기.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,18 +1,39 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) {
 
-        baseballGame();
+        try {
+            baseballGame();
+        }catch (IOException ioException){
+
+        }
 
     }
 
-    private static void baseballGame() {
+    private static void baseballGame() throws IOException {
         List<Integer> answerNumber = createAnswerNumber();
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        String input;
+        while (true){
+            System.out.println("숫자를 입력해주세요 : ");
+            input = br.readLine();
+            isValidNumber();
+        }
+    }
+
+    private static boolean isValidNumber() {
+        //TODO : 세자리 숫자인지 체크
+        return false;
     }
 
     private static List<Integer> createAnswerNumber() {
