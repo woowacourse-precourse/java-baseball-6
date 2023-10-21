70373c29902a61af1a8611fdcc83213a5e94a3db
i3kae <snoger1234@gmail.com>
2023-10-21 15:46:23 +0900

    숫자 야구 기능 프로토타입 구현
    모듈화, 오류, 코드 가독성, 로직에 대해 좀 더 생각해 볼 예정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,7 +12,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class Application {
     public static void main(String[] args) {
         // -- 프리코스 1주차 라이브러리 사용 예시 코드 --
-        // pickNumberInRange 테스트
+        /* pickNumberInRange 테스트
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -21,9 +21,72 @@ public class Application {
             }
         }
         System.out.println(computer);
-        
-        // readLine() 테스트
+        */
+
+        /* readLine() 테스트
         System.out.println(readLine());
-        // --------------------------------------------
+        */
+
+        boolean contiGame = true;
+        // 프로그램 전체 동작 반복 제어
+        while(contiGame){
+            // ---- 게임 초기화 과정 (추후 모듈화)
+            // 컴퓨터 숫자 생성
+            String user = new String();
+            List<Integer> computer = new ArrayList<>();
+            while (computer.size() < 3) {
+                int randomNumber = Randoms.pickNumberInRange(1, 9);
+                if (!computer.contains(randomNumber)) {
+                    computer.add(randomNumber);
+                }
+            }
+            // ----
+
+            System.out.println("숫자 야구 게임을 시작합니다.");
+            // 사용자 입력, 숫자 야구 규칙 적용 반복 제어
+            while(true){
+                int ball = 0, strike = 0;
+                // 사용자 입력
+                System.out.print("숫자를 입력해주세요 : ");
+                user = readLine();
+                
+                // 사용자 입력 무결성 검사 (추가 예정)
+
+                // 컴퓨터, 사용자 숫자 검증 (추후 모듈화)
+                for (int i = 0; i < 3; i++){
+                    for (int j = 0; j < 3; j++){
+                        if (computer.get(i) == (user.charAt(j) - '0')){
+                            if (i == j)
+                                strike++;
+                            else
+                                ball++;
+                        }
+                    }
+                }
+
+                // 볼, 스트라이크 결과 출력 (추후 모듈화?)
+                if (ball != 0)
+                    System.out.print(ball + "볼 ");
+                if (strike != 0)
+                    System.out.print(strike + "스트라이크");
+                if (ball == 0 && strike == 0)
+                    System.out.print("낫싱");
+                System.out.println();    
+            
+                // 사용자가 숫자를 맞췄을 경우 (추후 모듈화?)
+                if (strike == 3){
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                    user = readLine();
+
+                    // 사용자 입력 무결성 검사 (추가 예정) 
+
+                    // 사용자의 입력이 2인 경우 프로그램 전체 반복 제어 변수를 변경
+                    if (user.charAt(0) == '2')
+                        contiGame = false;
+                    break;
+                }
+            }
+        }
     }
 }
