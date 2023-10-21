6f575ed07676e7bf2ffc82d8d70db3e837e4bcaf
haiseong <wjdgotjd9908@gmail.com>
2023-10-22 01:57:52 +0900

    feat: AnswerGenerator 클래스 생성

---
+++ src/main/java/baseball/controller/AnswerGenerator.java
@@ -0,0 +1,20 @@
+package baseball.controller;
+
+import static baseball.model.GameNumbers.GAME_NUMBERS_SIZE;
+
+import baseball.model.GameNumbers;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class AnswerGenerator {
+    public GameNumbers generateAnser() {
+        Set<Integer> set = new HashSet<>(3);
+        while (set.size() < GAME_NUMBERS_SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            set.add(randomNumber);
+        }
+        return new GameNumbers(new ArrayList<>(set));
+    }
+}
