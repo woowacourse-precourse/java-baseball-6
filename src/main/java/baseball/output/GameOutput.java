d9ac3b6ee30d5bfa5a4fbd7134cd93983cbd9c05
Sully <2dcoder@naver.com>
2023-10-21 22:31:50 +0900

    feat: 스트라이크, 볼, 낫싱 필드 추가

--- src/main/java/baseball/output/GameOutput.java
+++ src/main/java/baseball/output/GameOutput.java
@@ -2,6 +2,10 @@ package baseball.output;
 
 public class GameOutput {
 
+    public static final String STRIKE = "스트라이크";
+    public static final String BALL = "볼";
+    public static final String NOTHING = "낫싱";
+
     private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String REQUEST_MESSAGE = "숫자를 입력해주세요 : ";
 
@@ -12,5 +16,4 @@ public class GameOutput {
     public static void printlnStartMessage() {
         System.out.println(START_MESSAGE);
     }
-
 }
