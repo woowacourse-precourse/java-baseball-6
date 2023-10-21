3266653b3e782ac65eef4d5ce3c118ed8a09aa4c
elyudwo <dudqk9696@naver.com>
2023-10-21 16:30:09 +0900

    [Feat] : 출력 메시지 열거형으로 구현

---
+++ src/main/java/baseball/util/OutputMessage.java
@@ -0,0 +1,22 @@
+package baseball.util;
+
+public enum OutputMessage {
+
+    BALL("%d볼\n"),
+    STRIKE("%d스트라이크\n"),
+    BALL_STRIKE("%d볼 %d스트라이크\n");
+
+    private String message;
+
+    OutputMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage(int value1, int value2) {
+        return String.format(message, value1, value2);
+    }
+
+    public String getMessage(int value) {
+        return String.format(message, value);
+    }
+}
