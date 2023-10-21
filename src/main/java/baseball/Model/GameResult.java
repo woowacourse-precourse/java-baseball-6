2226b71502ab33beed269916aa1b4029f669b90d
kim <hwiyul_g@daum.net>
2023-10-21 17:48:20 +0900

    feat(GameResult) : ball과 strike가 몇 개인지 게임 결과를 관리하는 클래스 추가

---
+++ src/main/java/baseball/Model/GameResult.java
@@ -0,0 +1,13 @@
+package baseball.Model;
+
+public class GameResult {
+    private int ballCount;
+    private int strikeCount;
+
+    public GameResult(){
+        ballCount = 0;
+        strikeCount = 0;
+    }
+
+
+}
