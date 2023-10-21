d86bcd710a166300d0205ab1e1e8948eeb5c113a
haiseong <wjdgotjd9908@gmail.com>
2023-10-22 01:58:07 +0900

    test: AnswerGeneratorTest 테스트 코드 작성

---
+++ src/test/java/baseball/controller/AnswerGeneratorTest.java
@@ -0,0 +1,21 @@
+package baseball.controller;
+
+import baseball.model.GameNumbers;
+import org.junit.jupiter.api.Test;
+
+class AnswerGeneratorTest {
+    AnswerGenerator answerGenerator = new AnswerGenerator();
+
+    @Test
+    void generateAnser() {
+        // given
+
+        for (int i = 0; i < 10; i++) {
+            // when
+            GameNumbers gameNumbers = answerGenerator.generateAnser();
+            System.out.printf("%2d%2d%2d\n", gameNumbers.get(0), gameNumbers.get(1), gameNumbers.get(2));
+            System.out.println("-------------------");
+        }
+        // then
+    }
+}
