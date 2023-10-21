a77d95061bbef93b9b7db6758e7903ed5975829d
aammddkkzxc <97333563+aammddkkzxc@users.noreply.github.com>
2023-10-22 00:12:57 +0900

    실행 로직 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,19 +1,24 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Input input = new Input();
         Output output = new Output();
         GameState gameState = new GameState(output.gameStart());
+        NumberController numberController = new NumberController();
+        List<Integer> computer = new ArrayList<>(numberController.randomComputerNumberGenerator());
         while (gameState.gameSwitch != GameState.END_STATE) {
-            NumberController numberController = new NumberController();
             GameScore gameScore = new GameScore(numberController.suggestedNumberConverter(input.numberSuggestion()),
-                    numberController.randomComputerNumberGenerator());
+                    computer);
             output.drawOutResult(gameScore.getBall(), gameScore.getStrike());
             if (gameScore.getStrike() == 3) {
                 output.gameEnd();
                 gameState.gameSwitch = input.retryOrEnd();
+                computer = numberController.randomComputerNumberGenerator();
             }
         }
     }
