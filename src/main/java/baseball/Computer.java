3f7623f3b1a072dc5f4a92687586ae3477762770
Minjae-An <mj3242@naver.com>
2023-10-21 20:35:56 +0900

    feat : 난수 배열 생성 메서드 수정
    Radom.pickNumberInRange 를 사용하도록 로직 수정.
    동시성 문제를 방지하도록 synchronized 키워드 추가.

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -2,6 +2,7 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
@@ -9,7 +10,7 @@ public class Computer {
     private final int END_INCLUSIVE = 9;
     private final int LENGTH = 3;
 
-    private List<Integer> computerNumbers;
+    private List<Integer> computerNumbers=new ArrayList<>(LENGTH);
 
     private static class LazyHolder {
         public static final Computer INSTANCE = new Computer();
@@ -19,8 +20,16 @@ public class Computer {
         return LazyHolder.INSTANCE;
     }
 
-    public void generateRandomNumber() {
-        computerNumbers = Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, LENGTH);
+    public synchronized void generateRandomNumber() {
+        if(computerNumbers.size()>0)
+            computerNumbers.clear();
+
+        int randomNumber;
+        while(computerNumbers.size()<LENGTH){
+            randomNumber = Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
+            if(!computerNumbers.contains(randomNumber))
+                computerNumbers.add(randomNumber);
+        }
     }
 
     public int getBallCount(List<Integer> userNumbers) {
