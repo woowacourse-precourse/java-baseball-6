513f476ac6243ccfb1db8fe6172887be3fcb1528
handwoong <handwoong@gmail.com>
2023-10-21 21:58:27 +0900

    feat: input whether player's retry
    Baseball game ask whether to retry after the game ends
    - print input line
    - input player's select

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public static final String PLEASE_INPUT_NUMBER = "숫자를 입력해주세요 : ";
+    public static final String RETRY_GAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static final InputView instance = new InputView();
 
     private InputView() {
@@ -15,6 +16,11 @@ public class InputView {
 
     public String readPlayerBall() {
         System.out.print(PLEASE_INPUT_NUMBER);
+        return Console.readLine();
+    }
+
+    public String readRetryGame() {
+        System.out.println(RETRY_GAME);
         return Console.readLine();
     }
 }
