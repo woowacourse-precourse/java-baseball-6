de74c0e20231f164f9fe3b4776ebcd01b504b68c
0Chord <seaweed.0chord@gmail.com>
2023-10-21 15:48:25 +0900

    test : 테스트 예외테스트 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -13,20 +13,29 @@ class ApplicationTest extends NsTest {
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
                 () -> {
-                    run("246", "135", "1", "597", "589", "2");
-                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+                    run("246", "135", "1", "597", "895", "589", "2");
+                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3볼", "3스트라이크", "게임 종료");
                 },
                 1, 3, 5, 5, 8, 9
         );
     }
 
     @Test
-    void 예외_테스트() {
+    void 예외_테스트_숫자가_3개가_아닐떼() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+    @Test
+    void 예외_테스트_숫자가_3개인데_같은수_중복() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("332"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    
 
     @Override
     public void runMain() {
