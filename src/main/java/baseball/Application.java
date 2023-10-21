d096d7575d206021317095e3a58737aec4567096
BangMingGing <rnrmfbmk802@gmail.com>
2023-10-22 01:25:25 +0900

    add checkRestart function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -31,6 +31,10 @@ class Game {
             checkResult(input);
             showResult();
         }
+
+        if (checkRestart()) {
+            startGame();
+        }
     }
 
     private void setNewAnswer() {
@@ -119,6 +123,18 @@ class Game {
 
         if (this.strikes == this.answer.size()) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
+    }
+
+    private boolean checkRestart() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = Console.readLine();
+        if (input.equals("1")) {
+            return true;
+        } else if (input.equals("2")) {
+            return false;
+        } else {
+            throw new IllegalArgumentException();
         }
     }
 }
