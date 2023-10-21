d85b1691bfca38b2ac645cf5b8fbc8d71acedff2
wonandonly <wanny416@sookmyung.ac.kr>
2023-10-22 05:17:07 +0900

    feat: 서로 다른 숫자로 구성된 세자리 수 생성기 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,42 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        //사용자 입력 받기
+        Game game = new Game();
+    }
+}
+
+class RandomNumberGenerator{
+    public static int generate() {
+        int randomNumber;
+        do {
+            randomNumber = Randoms.pickNumberInRange(100, 1000);
+        } while (!isDistinct(randomNumber));
+        return randomNumber;
+    }
+
+    //세 자리가 모두 다른 숫자인지 확인하는 함수
+    public static boolean isDistinct(int randomNumber){
+        int hundred = randomNumber / 100;
+        int ten = (randomNumber / 10) % 10;
+        int one = randomNumber % 10;
+        return (hundred != ten) && (hundred != one) && (one != ten);
+    }
+}
+
+class Game{
+    int answer;
+    int[] answerDigits;
+    Game(){
+        answer = RandomNumberGenerator.generate();
+    }
+
+    // 사용자가 제시한 숫자를 평가하는 함수
+    public String evaluation(int inputNumber) {
+        return "";
     }
 }
