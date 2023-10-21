071af0aa1d076d51087334aaf69293a045ed5f78
woong <hiidy115@gmail.com>
2023-10-22 03:03:13 +0900

    :sparkles: feat: Console API를 이용해서 유저의 숫자를 입력받는 기능 구현

---
+++ src/main/java/baseball/PlayGame.java
@@ -0,0 +1,14 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class PlayGame {
+
+    private int ball;
+    private int strike;
+
+    public void userInput() {
+        System.out.println("숫자를 입력해주세요 : ");
+        String originalInput = Console.readLine();
+    }
+}
