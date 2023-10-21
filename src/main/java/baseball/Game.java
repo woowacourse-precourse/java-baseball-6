57711b8cb0255a1d905fe22ab393d316b8938637
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-10-21 23:28:31 +0900

    FEAT / Add hint

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -4,6 +4,8 @@ public class Game {
     private Player player = new Player();
     private Player computer = new Player();
 
+    private Hint hint = new Hint();
+
     public Game() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
