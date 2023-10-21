3ab8825b93a6f95b827b608e682f648e0dc0c3cf
kunsanglee <gosmdochee2@gmail.com>
2023-10-22 00:10:46 +0900

    refactor(BaseballController): AnswerGenerator를 외부에서 주입받도록 변경 Application에서 BaseballController 객체를 생성할 때 AnswerGenerator 인터페이스를 구현한 RandomAnswerGenerator를 매개변수로 넣어 외부에서 주입받도록 변경. 또 Computer 객체를 불변객체로 변경했기 때문에 run() 메서드 내에서 매번 새로운 Computer객체를 생성하여 사용하도록 함.

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -1,29 +1,30 @@
 package baseball.controller;
 
+import baseball.domain.AnswerGenerator;
 import baseball.domain.Computer;
-import baseball.domain.RandomAnswerGenerator;
 import baseball.domain.Score;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 
 public class BaseballController {
-    public static void run() {
+    private final AnswerGenerator answerGenerator;
+
+    public BaseballController(AnswerGenerator answerGenerator) {
+        this.answerGenerator = answerGenerator;
+    }
+
+    public void run() {
         OutputView.startGame();
         boolean isContinue = true;
         while (isContinue) {
-            Computer computer = getComputer();
+            Computer computer = new Computer(answerGenerator);
             play(computer);
             OutputView.endGame();
             isContinue = InputView.continueOrExit();
         }
     }
 
-    private static Computer getComputer() {
-        RandomAnswerGenerator answerGenerator = new RandomAnswerGenerator();
-        return new Computer(answerGenerator);
-    }
-
-    private static void play(Computer computer) {
+    private void play(Computer computer) {
         boolean isStrikeOut = false;
         while (!isStrikeOut) {
             Score score = computer.getScore(InputView.baseballNumber());
