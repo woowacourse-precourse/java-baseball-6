8bb9f9ce438a05743db657b8df185cd68ba88b5c
WooJJam <jaemin5548@naver.com>
2023-10-21 21:00:59 +0900

    [Refactor] 출력문 view 에서 처리

---
+++ src/main/java/baseball/view/PrintView.java
@@ -0,0 +1,12 @@
+package baseball.view;
+
+public class PrintView {
+
+    public void printGameStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void printUserInput() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+}
