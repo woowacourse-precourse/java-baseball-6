b81ea536348d714138a00c053a232620859bb6fe
scarlet0o0 <ghd7673@naver.com>
2023-10-21 11:16:12 +0900

    Refactor : OutView 오타 수정

--- src/main/java/baseball/view/OutView.java
+++ src/main/java/baseball/view/OutView.java
@@ -4,7 +4,7 @@ import baseball.BallResult;
 
 public class OutView {
     public static void gameStartPrint() {
-        System.out.println("[컴퓨터] : 게임을 시작합니다.");
+        System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
     public static void numberInputPrint() {
