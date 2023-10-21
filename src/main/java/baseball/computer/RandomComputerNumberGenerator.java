718ed265a5ef7a0124537f2cd9263bc692160ca9
Sully <2dcoder@naver.com>
2023-10-21 22:29:33 +0900

    fix: 컴퓨터 난수 생성 시 중복값 생성되던 것 해결
    Number 클래스를 제거 후 Integer형 LinkedHashSet으로 관리하도록 하여 해결했다.
    그 후 좀 더 명확한 예외 처리를 위해 validateDuplicateNumber()를 호출했다.

--- src/main/java/baseball/computer/RandomComputerNumberGenerator.java
+++ src/main/java/baseball/computer/RandomComputerNumberGenerator.java
@@ -2,7 +2,7 @@ package baseball.computer;
 
 import static baseball.validator.NumberValidator.MAX_VALUE;
 import static baseball.validator.NumberValidator.MIN_VALUE;
-import static baseball.validator.NumberValidator.isBelowRequiredLength;
+import static baseball.validator.NumberValidator.isBelowThreeLength;
 import static baseball.validator.NumberValidator.validateDuplicateNumber;
 
 import camp.nextstep.edu.missionutils.Randoms;
@@ -12,23 +12,26 @@ import java.util.stream.Collectors;
 
 public class RandomComputerNumberGenerator {
 
-    private final Set<Number> numbers;
+    private final Set<Integer> numbers;
 
     public RandomComputerNumberGenerator() {
         this.numbers = new LinkedHashSet<>();
 
-        while (isBelowRequiredLength(numbers.size())) {
+        while (isBelowThreeLength(numbers.size())) {
             int randomNumber = Randoms.pickNumberInRange(MIN_VALUE, MAX_VALUE);
-            validateDuplicateNumber(randomNumber);
-            numbers.add(new Number(randomNumber));
+            numbers.add(randomNumber);
         }
     }
 
     @Override
     public String toString() {
-        return numbers.stream()
-                .map(Number::toString)
+        String numberString = numbers.stream()
+                .map(String::valueOf)
                 .collect(Collectors.joining());
+
+        validateDuplicateNumber(Integer.parseInt(numberString));
+
+        return numberString;
     }
 
 }
