59ee440b439eced7345a84d5b433aeca5ea407fd
kim <hwiyul_g@daum.net>
2023-10-21 21:38:23 +0900

    feat(GameController) : 사용자와 상호작용하며 게임을 시작하는 함수 추가

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,6 +1,8 @@
 package baseball.controller;
 
 import baseball.model.AnswerNumber;
+import baseball.model.GameResult;
+import baseball.model.UserNumber;
 import baseball.service.NumberMatcher;
 import baseball.view.InputView;
 import baseball.view.OutputView;
@@ -17,5 +19,25 @@ public class GameController {
         this.inputView = new InputView();
         this.outputView = new OutputView();
     }
+
+    public void gameStart(){
+        outputView.displayStartGame();
+        while(true) {
+            String userInput = inputView.requestUserNumber();
+            UserNumber userNumber = new UserNumber(userInput);
+
+            GameResult result = numberMatcher.match(userNumber.getUserNumber(), answerNumber.getAnswerNumber());
+
+            if(result.isCorrect()){
+                outputView.displayWinningMessage();
+                break;
+            }
+
+            outputView.displayResult(result.getBallCount(), result.getStrikeCount());
+
+        }
+
+    }
+
 
 }
