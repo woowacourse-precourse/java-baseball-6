7e83dac7ff778d738c7a107e1d240e4b83220dc5
ImGrooott <20184456@chosun.ac.kr>
2023-10-22 00:28:08 +0900

    feat: 3자리 여부 확인 isThreeDigits() 구현, classInvariant() 추가

--- src/main/java/baseball/Count.java
+++ src/main/java/baseball/Count.java
@@ -1,26 +1,27 @@
 package baseball;
 
-import java.util.Scanner;
-
 public class Count {
     private String number;
 
-    public Count(){
-        System.out.println("숫자를 입력해주세요 : ");
-        Scanner sc = new Scanner(System.in);
-        String number = sc.next();
-
+    public Count(String number) {
+        classInvariant(number);
+        this.number = number;
     }
 
-    private void isThreeDigits(String number){
+    private void classInvariant(String number) {
+        isThreeDigits(number);
+    }
+
+    private void isThreeDigits(String number) {
         if (number.length() != 3) {
             throw new IllegalArgumentException("3자리의 정수를 입력해야 합니다.");
         }
 
-        try {
-            Integer.parseInt(number);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("3자리의 정수를 입력해야 합니다");
+        for (int i = 0; i < number.length(); i++) {
+            char c = number.charAt(i);
+            if (c < '1' || c > '9') {
+                throw new IllegalArgumentException("3자리의 정수를 입력해야 합니다");
+            }
         }
     }
 }
