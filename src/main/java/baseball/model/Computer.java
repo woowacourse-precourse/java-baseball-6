95a9da860126aec36f4865cd9e577f8c1496152e
kimtaesoo <kimtaesoo7@naver.com>
2023-10-21 22:48:31 +0900

    feat: Computer 생성자 구현

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -1,4 +1,19 @@
 package baseball.model;
 
+import baseball.NumberGenerator;
+
+import java.util.List;
+
 public class Computer {
+
+    private final List<Integer> numbers;
+
+    private Computer(final List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public static Computer createWithGenerator(final NumberGenerator generator) {
+        return new Computer(generator.generate());
+    }
 }
+
