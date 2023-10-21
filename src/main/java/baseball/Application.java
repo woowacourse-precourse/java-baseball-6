67adcd1d6db6707ccff62db063149495a8dea473
shAn-kor <kjn0406@naver.com>
2023-10-21 17:53:43 +0900

    Baseball 객체 구현
    main에서 전체 로직 시작

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import baseball.controller.BaseballGame;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BaseballGame baseballGame = new BaseballGame();
+        baseballGame.gameStart();
     }
 }
--- src/main/java/baseball/controller/BaseballGame.java
+++ src/main/java/baseball/controller/BaseballGame.java
@@ -2,35 +2,84 @@ package baseball.controller;
 
 import static baseball.view.InputView.userInput;
 import static baseball.view.OutputView.gameStartMessage;
+import static baseball.view.OutputView.printCelebrate;
 import static baseball.view.OutputView.userInputMessage;
 
 import baseball.model.ComputerNumber;
+import baseball.model.RetryCheck;
 import baseball.model.UserNumber;
+import baseball.view.OutputView;
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class BaseballGame {
+    private List<Integer> computerNumber;
+    private int[] cntBallStrike;
 
-    public void gameStart() {
-        List<Integer> computerNumber = ComputerNumber.makeComputerNumber();
-        gameStartMessage();
-        userInputMessage();
-        UserNumber userNumber = new UserNumber(userInput());
+    public BaseballGame() {
+        this.cntBallStrike = new int[2];
+        this.computerNumber = new ArrayList<>();
     }
 
-    public int[] countBallStrike(List<Integer> userNumber, List<Integer> computerNumber) {
-        int[] cntBallStrike = {0, 0};
+    public void gameStart() {
+        this.cntBallStrike = new int[2];
+
+        gameStartMessage();
+
+        //랜덥 값 생성
+        computerNumber = ComputerNumber.makeComputerNumber();
+
+        //게임 시작
+        inGame();
+
+        //재시작 여부 입력
+        retryCheck();
+    }
+
+    public void inGame() {
+        while (cntBallStrike[1] != 3) {
+            //사용자 값 입력
+            userInputMessage();
+            UserNumber userNumber = new UserNumber(userInput());
+
+            //볼, 스트라이크 카운트
+            this.countBallStrike(userNumber.getInputNumber(), computerNumber);
+
+            //볼, 스트라이크 값 출력
+            OutputView.printBallStrike(cntBallStrike[0], cntBallStrike[1]);
+
+            //볼, 스트라이크 카운트 초기화
+            cntBallStrike[0] = 0;
+            cntBallStrike[1] = 0;
+        }
+
+        printCelebrate();
+    }
+
+    public void countBallStrike(List<Integer> userNumber, List<Integer> computerNumber) {
 
         for (int i = 0; i < userNumber.size(); i++) {
+
             if (userNumber.get(i).equals(computerNumber.get(i))) {
                 cntBallStrike[1]++;
             } else if (computerNumber.contains(userNumber.get(i))) {
                 cntBallStrike[0]++;
             }
+
         }
 
-        return cntBallStrike;
     }
 
-    //public static
+    public void retryCheck() {
+        RetryCheck retryCheck = new RetryCheck(userInput());
+
+        if (retryCheck.getNum().equals("1")) {
+            this.gameStart();
+        }
+
+        /*if (retryCheck.getNum().equals("2")) {
+
+        }*/
+    }
 }
