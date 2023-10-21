7bd9077cf34bb4e23a014df2cb6d7985565c9219
devhoya97 <diajbh123@gmail.com>
2023-10-21 11:49:07 +0900

    refactor : Model 클래스 추가 기존엔 List<Integer> 형태로 유저넘버와 컴퓨터넘버를 관리했는데, Integer형태가 아니라 BaseballNumber 객체 형태로 원시타입을 감싸기 위해 필요한 BaseballNumber 클래스를 추가함. 이를 위한 validation 메서드 추가 필요

---
+++ src/main/java/baseball/BaseballNumber.java
@@ -0,0 +1,9 @@
+package baseball;
+
+public class BaseballNumber {
+    int baseballNumber;
+
+    public BaseballNumber(int baseballNumber) {
+        this.baseballNumber = baseballNumber;
+    }
+}
