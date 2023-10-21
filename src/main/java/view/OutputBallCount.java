a3f2cf8445950127206196c75d85d7bf2b71f403
JiEung2 <https://github.com/JiEung2>
2023-10-21 16:52:30 +0900

    if문이 중첩으로 쓰여있던 부분 리팩토링

--- src/main/java/view/OutputBallCount.java
+++ src/main/java/view/OutputBallCount.java
@@ -4,13 +4,13 @@ public class OutputBallCount {
     public void printCount(int[] ballCount){
         if(ballCount[0] == 0 && ballCount[1] == 0) System.out.println("낫싱");
         if(ballCount[0] == 0 && ballCount[1] != 0) System.out.println(ballCount[1] + "볼");
-        if(ballCount[0] != 0 && ballCount[1] == 0){
-            if(ballCount[0] == 3){
-                System.out.println(ballCount[0] + "스트라이크");
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                return;
-            }
+        if(ballCount[0] != 0 && ballCount[1] == 0 && ballCount[0] != 3){
             System.out.println(ballCount[0] + "스트라이크");
+        }
+        if(ballCount[0] == 3){
+            System.out.println(ballCount[0] + "스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
         }
         if(ballCount[0] != 0 && ballCount[1] != 0) System.out.println(ballCount[1] + "볼 " + ballCount[0] + "스트라이크");
     }
