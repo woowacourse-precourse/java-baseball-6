d3dea69440e9f1c3d018df1bc15c5b718c1cd2cb
dokkisan <dokkisan.wang@gmail.com>
2023-10-22 02:25:53 +0900

    feat: 플레이어 입력 기능 구현

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,16 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private void inputThreeDigitNumber() {
+        String numbers = Console.readLine();
+    }
+
+    private void inputRestartOrExit() {
+        String playerChoice = Console.readLine();
+    }
+
+    // TODO: 사용자 입력값 예외 처리
+}
