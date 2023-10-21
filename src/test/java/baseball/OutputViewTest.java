de47ece9ebbd65f4e18b7cb8f393ffeed23fce6d
kunsanglee <gosmdochee2@gmail.com>
2023-10-21 17:29:45 +0900

    test(OutputViewTest): matchResult() 메서드 테스트코드 작성

--- src/test/java/baseball/OutputViewTest.java
+++ src/test/java/baseball/OutputViewTest.java
@@ -1,12 +1,17 @@
 package baseball;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class OutputViewTest {
 
@@ -29,5 +34,25 @@ public class OutputViewTest {
     void endGameTest() {
         OutputView.endGame();
         assertThat(output.toString()).isEqualTo("3개의 숫자를 모두 맞히셨습니다! 게임종료\n");
+    }
+
+    @DisplayName("정답과 입력값을 비교하여 Score를 출력한다.")
+    @ParameterizedTest
+    @MethodSource("provideMatchResultTestArguments")
+    void matchResultTest(Score score, String matchResult, boolean isStrikeOut) {
+        boolean isFullCount = OutputView.matchResult(score);
+        assertThat(output.toString()).isEqualTo(matchResult);
+        assertThat(isFullCount).isEqualTo(isStrikeOut);
+    }
+
+    static Stream<Arguments> provideMatchResultTestArguments() {
+        return Stream.of(
+                arguments(new Score(1, 1), "1볼 1스트라이크\n", false),
+                arguments(new Score(1, 0), "1볼\n", false),
+                arguments(new Score(2, 0), "2볼\n", false),
+                arguments(new Score(0, 1), "1스트라이크\n", false),
+                arguments(new Score(0, 0), "낫싱\n", false),
+                arguments(new Score(0, 3), "3스트라이크\n", true)
+        );
     }
 }
