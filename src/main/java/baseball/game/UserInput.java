a64b81cf5c541abb7904958a94b909482245a5d8
YongJOoOo <we072200@naver.com>
2023-10-21 17:23:12 +0900

    refactor: UserInput 클래스 리팩토링

--- src/main/java/baseball/game/UserInput.java
+++ src/main/java/baseball/game/UserInput.java
@@ -3,36 +3,57 @@ package baseball.game;
 import camp.nextstep.edu.missionutils.Console;
 
 public class UserInput {
-    String userAnswer;
+    private static final int INPUT_LEN = 3;
+
+    public String userAnswer;
 
     public UserInput() {
         System.out.print("숫자를 입력해주세요 : ");
-        String tmp = Console.readLine();
-        chkInputException(tmp);
-        userAnswer = tmp;
+        userAnswer = Console.readLine();
+        validate(userAnswer);
     }
 
-    //사용자 입력값에 대한 예외처리
-    private void chkInputException(String answer) throws IllegalArgumentException {
-        //1) 길이3 아닌 경우
-        if (answer.length() != 3) {
-            throw new IllegalArgumentException("입력 길이가 잘못되었습니다. " + answer.length());
+    /**
+     * 사용자 입력값에 대한 예외처리
+     */
+    private void validate(String input) throws IllegalArgumentException {
+        validateLength(input);
+        validateCharacter(input);
+        validateDuplicate(input);
+    }
+
+    /**
+     * 길이 3 아닌 경우
+     */
+    private void validateLength(String input) throws IllegalArgumentException {
+        if (input.length() != INPUT_LEN) {
+            throw new IllegalArgumentException("입력 길이가 잘못되었습니다.");
         }
-        //2) 입력값 각 숫자 범위가 1-9 외 구성인 경우
-        for (int i = 0; i < 3; i++) {
-            int num = Character.getNumericValue(answer.charAt(i));
+    }
+
+    /**
+     * 입력값 각 숫자 범위가 1-9 외 구성인 경우
+     */
+    private void validateCharacter(String input) throws IllegalArgumentException {
+        for (int i = 0; i < INPUT_LEN; i++) {
+            int num = Character.getNumericValue(input.charAt(i));
             if (num < 1 || num > 9) {
                 throw new IllegalArgumentException("1-9 범위를 벗어난 입력입니다.");
             }
         }
-        //3) 입력값 중복된 경우-서로 다른 수가 아닌 경우
+    }
+
+    /**
+     * 입력값 중복된 경우-서로 다른 수가 아닌 경우
+     */
+    private void validateDuplicate(String input) throws IllegalArgumentException {
         String tmp = "";
-        for (int i = 0; i < 3; i++) {
-            String num = String.valueOf(answer.charAt(i));
+        for (int i = 0; i < INPUT_LEN; i++) {
+            String num = String.valueOf(input.charAt(i));
             if (tmp.contains(num)) {
                 throw new IllegalArgumentException("중복된 입력입니다. 서로 다른 세가지 숫자가 입력되어야 합니다.");
             }
-            tmp += num;
+            tmp = tmp.concat(num);
         }
     }
 }
