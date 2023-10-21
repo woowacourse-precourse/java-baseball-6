d7362b6152806d5852b4c626d7795ec568b6c2e0
4444ki <qkrwogh423@gmail.com>
2023-10-21 15:01:44 +0900

    3차 커밋 IllegalArgumentException처리 추가

--- src/main/java/baseball/Readline.java
+++ src/main/java/baseball/Readline.java
@@ -7,11 +7,19 @@ import camp.nextstep.edu.missionutils.Console;
 public class Readline {
     public List<Integer> ReadPlayerNumber(){
         System.out.print("숫자를 입력해주세요 : ");
+
         List<Integer> playNumber = new ArrayList<>();
         String input = Console.readLine();
 
+        int checkinput = Integer.parseInt(input);
+        if(checkinput < 100 || checkinput > 999){
+            throw new IllegalArgumentException();
+        }
         for(String sliceInput: input.split("")){
             playNumber.add(Integer.parseInt(sliceInput));
+        }
+        if(playNumber.get(0) == playNumber.get(1) || playNumber.get(1) == playNumber.get(2) || playNumber.get(0) == playNumber.get(2)){
+            throw new IllegalArgumentException();
         }
         return playNumber;
     }
