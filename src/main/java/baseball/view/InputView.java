8f097e00411f790ccb1c6bc5edc461426197903c
Seokhyun Lee <lseokhyun99@gmail.com>
2023-10-22 00:46:20 +0900

    feat(InputView): 게임 재시작 입력 기능 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -5,9 +5,17 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     private static final String NUMBER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String RESTART_OPTION_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요";
 
     public static String getUserNumber() {
         System.out.print(NUMBER_INPUT_MESSAGE);
+        return Console.readLine();
+    }
+
+    public static String getRestartCommand() {
+        System.out.println(GAME_END_MESSAGE);
+        System.out.println(RESTART_OPTION_MESSAGE);
         return Console.readLine();
     }
 
