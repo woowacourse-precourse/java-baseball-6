1786a4a7f70a59ea2093ec01b69e8e8f66deed6f
5jeong <wjdgns4385@naver.com>
2023-10-21 20:44:27 +0900

    [Refactor] : 불필요한 코드 제거

--- src/main/java/baseball/util/Randomutil.java
+++ src/main/java/baseball/util/Randomutil.java
@@ -1,11 +1,7 @@
 package baseball.util;
 
-import baseball.view.InputView;
 import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.Random;
 
 public class Randomutil {
 
@@ -13,7 +9,7 @@ public class Randomutil {
         LinkedList<Integer> randomNumComputer = new LinkedList<>();
         while (randomNumComputer.size() < 3) {
             int randomNum = Randoms.pickNumberInRange(1, 9);
-            if(!randomNumComputer.contains(randomNum)){
+            if (!randomNumComputer.contains(randomNum)) {
                 randomNumComputer.add(randomNum);
             }
         }
