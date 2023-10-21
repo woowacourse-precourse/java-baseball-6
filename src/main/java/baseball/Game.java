87327fa08de7029dd3af2a0ab58e030924cd53a1
jimmy0006 <jinmin645@gmail.com>
2023-10-21 13:35:32 +0900

    add:3. 입력값과 비교하여 결과를 나타내는 함수

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -31,6 +31,32 @@ public class Game {
         }
     }
 
+    private List<Integer> intToArray(int a){
+        List<Integer> res = new ArrayList<>();
+        res.add(a/100);
+        res.add((a%100)/10);
+        res.add(a%10);
+        return res;
+    }
 
+    //[0] for strike, [1] for ball
+    public List<Integer> compareNumbers(int a){
+        List<Integer> res = new ArrayList<>();
+        res.add(0);
+        res.add(0);
+        List<Integer> integers = intToArray(a);
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if(target.get(i).equals(integers.get(j))){
+                    if(i==j){
+                        res.set(0,res.get(0)+1);
+                    }else{
+                        res.set(1,res.get(1)+1);
+                    }
+                }
+            }
+        }
+        return res;
+    }
 
 }
