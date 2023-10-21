ef131c70de3ec709616e8adfcd8a3d6e665901e1
gengminy <kls1998@naver.com>
2023-10-21 21:11:37 +0900

    feat: String to Int 유틸 함수 구현

--- src/main/java/baseball/NumberParser.java
+++ src/main/java/baseball/NumberParser.java
@@ -1,24 +1,39 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class RandomNumber {
-    private RandomNumber() {}
+public class NumberParser {
+    private NumberParser() {}
 
-    public static List<Integer> generate(int length) {
-        List<Integer> randomNumbers = new ArrayList<>();
-        if (length <= 0) {
-            throw new IllegalArgumentException("길이는 1 이상이어야 합니다.");
+    public static List<Integer> parseIntegerList(String numberString) {
+        List<Integer> parsedNumbers = new ArrayList<>();
+        validateIntString(numberString);
+        for (int i = 0; i < numberString.length(); i++) {
+            char ch = numberString.charAt(i);
+            parsedNumbers.add(parseIntCharacter(ch));
         }
-        while (randomNumbers.size() < length) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!randomNumbers.contains(randomNumber)) {
-                randomNumbers.add(randomNumber);
-            }
+        return parsedNumbers;
+    }
+
+    public static int parseSingleDigitNumber(String numberString) {
+        validateIntString(numberString);
+        if (numberString.length() != 1) {
+            throw new IllegalArgumentException("문자열 길이가 올바르지 않습니다.");
         }
-        return randomNumbers;
+        return Integer.parseInt(numberString);
+    }
+
+    private static int parseIntCharacter(char ch) {
+        if (!('0' <= ch && ch <= '9')) {
+            throw new IllegalArgumentException("숫자만 허용됩니다.");
+        }
+        return Character.getNumericValue(ch);
+    }
+
+    private static void validateIntString(String numberString) {
+        if (numberString == null || numberString.isEmpty()) {
+            throw new IllegalArgumentException("비어있지 않은 문자열이 입력되어야 합니다.");
+        }
     }
 }
