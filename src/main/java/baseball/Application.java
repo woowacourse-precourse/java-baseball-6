9ff8064a40a644f6a83968f4f14293646a889fae
eora21 <eora21@naver.com>
2023-10-21 16:51:22 +0900

    refactor: 💡 수정 컨트롤러가 InputView, OutputView 사용할 수 있도록 main 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,13 @@
 package baseball;
 
 import baseball.controller.BaseballGameController;
-import baseball.view.ConsoleView;
+import baseball.view.ConsoleInputView;
+import baseball.view.ConsoleOutputView;
 
 public class Application {
     public static void main(String[] args) {
-        BaseballGameController baseballGameController = new BaseballGameController(new ConsoleView());
+        BaseballGameController baseballGameController =
+                new BaseballGameController(new ConsoleInputView(), new ConsoleOutputView());
         baseballGameController.play();
     }
 }
