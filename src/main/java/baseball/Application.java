9eae96393ca849154f946bbb0f1516afd89ca79c
임강호 <55732952+Icln@users.noreply.github.com>
2023-10-21 22:18:14 +0900

    feat: 사용자 입력 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RandomNumber randomNumber = new RandomNumber();
+        randomNumber.randomNumberGenerate();
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        while (true) {
+            System.out.printf("숫자를 입력하세요 : ");
+            String inputString = Console.readLine();
+        }
     }
 }
