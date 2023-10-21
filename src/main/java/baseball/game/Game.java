577b923b476c7dc0ae26d91f5226cb9e5a76df2b
ahma0 <dsd932@naver.com>
2023-10-21 23:31:42 +0900

    refactor: 수 생성, 비교, 결과 출력 로직 분리

---
+++ src/main/java/baseball/game/Game.java
@@ -0,0 +1,62 @@
+package baseball.game;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Game {
+
+    private static String number;
+    private static int ball = 0;
+    private static int strike = 0;
+
+    public static void generateNumber() {
+        List<Integer> list = new ArrayList<>();
+
+        int cnt = 3;
+        while(cnt-- > 0) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if(!list.contains(randomNumber))
+                list.add(randomNumber);
+        }
+
+        number = list.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+    }
+
+    public static void compareNumber(String answer) {
+        ball = 0;
+        strike = 0;
+
+        for(int i = 0; i < number.length(); i++) {
+            if(answer.contains(String.valueOf(number.charAt(i)))) {
+                if(answer.charAt(i) == number.charAt(i))
+                    strike++;
+                else
+                    ball++;
+            }
+        }
+    }
+
+    public static boolean printResult() {
+        if(strike == 3) {
+            System.out.println(strike + "스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        }
+
+        if(ball == 0 && strike == 0)
+            System.out.println("낫싱");
+        else if(ball == 0)
+            System.out.println(strike + "스트라이크");
+        else if(strike == 0)
+            System.out.println(ball + "볼");
+        else
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+
+        return false;
+    }
+}
--- src/main/java/baseball/game/RunGame.java
+++ src/main/java/baseball/game/RunGame.java
@@ -1,56 +1,19 @@
 package baseball.game;
 
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class RunGame {
 
     public static void runBaseBallGame() {
-        String number = generateNumber();
+        Game.generateNumber();
 
         while (true) {
             String userAnswer = getUserAnswer();
             checkUserAnswer(userAnswer);
-
-            int[] results = compareNumber(userAnswer, number);
-
-            if(printResult(results[0], results[1]))
+            Game.compareNumber(userAnswer);
+            if(Game.printResult())
                 break;
         }
-    }
-
-    private static int[] compareNumber(String answer, String number) {
-        int arr[] = new int[2]; // 0: ball, 1: strike
-
-        for(int i = 0; i < number.length(); i++) {
-            if(answer.contains(String.valueOf(number.charAt(i)))) {
-                if(answer.charAt(i) == number.charAt(i))
-                    arr[1]++;
-                else
-                    arr[0]++;
-            }
-        }
-
-        return arr;
-    }
-
-    private static String generateNumber() {
-        List<Integer> list = new ArrayList<>();
-
-        int cnt = 3;
-        while(cnt-- > 0) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if(!list.contains(randomNumber))
-                list.add(randomNumber);
-        }
-
-        return list.stream()
-                .map(String::valueOf)
-                .collect(Collectors.joining());
     }
 
     private static String getUserAnswer() {
@@ -63,25 +26,6 @@ public class RunGame {
                 || !isAnswerDigit(answer)
                 || isAnswerContainZero(answer))
             throw new IllegalArgumentException("잘못된 입력입니다.");
-    }
-
-    private static boolean printResult(int ball, int strike) {
-        if(strike == 3) {
-            System.out.println(strike + "스트라이크");
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            return true;
-        }
-
-        if(ball == 0 && strike == 0)
-            System.out.println("낫싱");
-        else if(ball == 0)
-            System.out.println(strike + "스트라이크");
-        else if(strike == 0)
-            System.out.println(ball + "볼");
-        else
-            System.out.println(ball + "볼 " + strike + "스트라이크");
-
-        return false;
     }
 
     private static boolean isAnswerLengthRight(String answer) {
