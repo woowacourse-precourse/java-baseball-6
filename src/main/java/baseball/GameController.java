49977f2879a55c98ddcb08a632349fc5e2cb4824
lee jihyeon <lejinhy@gmail.com>
2023-10-21 17:38:58 +0900

    feat: 사용자 숫자 입력 - 힌트 결과 출력 - 3스트라이크이면 status 입력받기 과정 모두 완성

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -7,13 +7,14 @@ import java.util.List;
 
 public class GameController {
 
+    public PrintMessage printMessage = new PrintMessage();
     public List<Integer> computer = new ArrayList<>();  // 컴퓨터(정답) 번호
     public List<Integer> user = new ArrayList<>();  // 유저가 입력한 번호
-    public boolean status = true;   // 야구 게임의 상태가 새로 시작(1) 인지, 종료(2) 인지 구분
+    public int status = 1;   // 야구 게임의 상태가 새로 시작(1) 인지, 종료(2) 인지 구분
     public void start() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        printMessage.start();
 
-        while(status) {
+        while(status == 1) {
             gaming();
         }
     }
@@ -22,12 +23,26 @@ public class GameController {
         GameTool gameTool = new GameTool();
         Exception exception = new Exception();
         computer = gameTool.generatedRandomNumbers(3); // 3개 랜덤 숫자 생성
+        // 테스트
+        System.out.println(computer);
 
         while(true) {
-            System.out.print("숫자를 입력해주세요 : ");
+            printMessage.requestNumber();
             String number = Console.readLine();
+
             user = exception.isValid(number);    // 예외 발생하지 않으면 List<Integer> 형태로 숫자 반환
 
+            int strike = gameTool.countStrike(computer, user);
+            int ball = gameTool.countBall(computer, user);
+            boolean nothing = gameTool.isNothing(computer, user);
+
+            printMessage.hint(strike, ball, nothing);
+
+            if (strike == 3) {
+                printMessage.gameEnd();
+                status = Integer.parseInt(Console.readLine());
+                break;
+            }
        }
     }
 }
