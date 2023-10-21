06a9d9191b7a79772c826870ed03bbe8a11e5515
oxdjww <oxdjww@gmail.com>
2023-10-21 23:25:27 +0900

    feat : 새게임 진행 기능 추가 및 안내문 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,31 @@
 package baseball;
 
 import baseball.model.BaseballGame;
+import camp.nextstep.edu.missionutils.Console;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        BaseballGame game = BaseballGame.createBaseballGame();
-        game.run();
 
+        do {
+            BaseballGame game = BaseballGame.createBaseballGame();
+            game.run();
+        } while (gotoNextGame());
+
+    }
+
+    private static boolean gotoNextGame() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String flag = readLine();
+        if (flag.equals("1")) {
+            return true;
+        } else if (flag.equals("2")) {
+            System.out.println("게임 종료");
+            return false;
+        } else {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
     }
 }
