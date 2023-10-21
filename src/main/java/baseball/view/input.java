d2048554419522b9656abbc856a446631d944ccf
Maljaaa <Maljaaa@github.com>
2023-10-21 21:18:33 +0900

    style : 변수와 메서드에 static 추가

--- src/main/java/baseball/view/input.java
+++ src/main/java/baseball/view/input.java
@@ -5,12 +5,12 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class input {
 
     // player가 선택한 서로 다른 3가지 숫자
-    private String userNumber;
+    private static String userNumber;
     // 게임 상태 번호
-    private String gameMode;
+    private static String gameMode;
 
     // player가 서로 다른 숫자 3가지 입력
-    public void inputUserNumber() {
+    public static void inputUserNumber() {
         System.out.print("숫자를 입력해주세요 : ");
         userNumber = readLine();
         // 검증 코드
@@ -22,7 +22,7 @@ public class input {
     }
 
     // player가 게임 종료 후 재시작/종료 선택
-    public void endGame() {
+    public static void endGame() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         gameMode = readLine();
         // 검증 코드
--- src/main/java/baseball/view/print.java
+++ src/main/java/baseball/view/print.java
@@ -11,7 +11,7 @@ public class print {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
-    public void printResult(int ball, int strike){
+    public static void printResult(int ball, int strike){
         // 0볼 0스트라이크
         if(ball != 0 && strike != 0) {
             System.out.println(ball + "볼 " + strike + "스트라이크");
