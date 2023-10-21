0ea7ba9157f03f883c72e1b42aea4846fc3b6b93
김효준 <khyojun1104@gmail.com>
2023-10-21 17:25:06 +0900

    [feat] GameController : 재시작 관련한 기능 구현 ! (재시작에 따른 computerNumber 재발급, 재시작에 따른 do-while 구성)

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -9,9 +9,11 @@ import baseball.view.OutputView;
 
 public class GameController {
 
-    InputView inputView;
-    OutputView outputView;
-    Validation validation = new Validation();
+    private static String computerNumber;
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final Validation validation = new Validation();
+
 
     public GameController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
@@ -20,36 +22,66 @@ public class GameController {
 
 
     public void playball() {
-        String nowComNumber = getComputerNumber();
-        while(true){
+        GameStateDto gamestate;
+        computerNumber = getComputerNumber();
+        do {
             outputView.printInputInitMessage();
             String userNumber = getUserNumber();
-            BaseballService baseballService = new BaseballService(userNumber,nowComNumber);
-            GameStateDto gamestate = baseballService.caculate();
+            BaseballService baseballService = new BaseballService(userNumber, computerNumber);
+            gamestate = baseballService.caculate();
             outputResult(gamestate);
-
-        }
-
-
+        } while (isGameReStart(gamestate));
     }
 
+    private boolean isGameReStart(GameStateDto gameState) {
+        if (gameState.isEndGame()) {
+            checkRestart(gameState);
+            checkComputerNumberChange(gameState);
+            return gameState.isRestart();
+        }
+        return true;
+    }
+
+    private void checkComputerNumberChange(GameStateDto gameState) {
+        if (gameState.isRestart()) {
+            computerNumber = changeComputerNumber();
+        }
+    }
+
+    private String changeComputerNumber() {
+        String computerNumber;
+        computerNumber = getComputerNumber();
+        return computerNumber;
+    }
+
+    private void checkRestart(GameStateDto gamestate) {
+        String restartNumber = inputView.readRestart();
+        validation.validateRestart(restartNumber);
+        if (restartNumber.equals("1")) {
+            gamestate.activateRestart();
+        }
+    }
 
 
     private void outputResult(GameStateDto gamestate) {
         int strike = gamestate.getStrike();
         int ball = gamestate.getBall();
-        if(strike == 3) {
-            threeThrikeOutput(strike);
-            gamestate.activateEndGame();
-        }
-        else if(strike >0 && ball >0)
+        if (strike == 3) {
+            threeStrikeProcess(gamestate, strike);
+        } else if (strike > 0 && ball > 0) {
             outputView.strikeBall(strike, ball);
-        else if(strike>0)
+        } else if (strike > 0) {
             outputView.onlyStrike(strike);
-        else if(ball>0)
+        } else if (ball > 0) {
             outputView.onlyBall(ball);
-        else if(strike==0 && ball==0)
+        } else if (strike == 0 && ball == 0) {
             outputView.nothing();
+        }
+    }
+
+    private void threeStrikeProcess(GameStateDto gamestate, int strike) {
+        threeThrikeOutput(strike);
+        gamestate.activateEndGame();
     }
 
     private void threeThrikeOutput(int strike) {
@@ -57,7 +89,6 @@ public class GameController {
         outputView.endGame();
         outputView.restart();
     }
-
 
 
     private String getComputerNumber() {
