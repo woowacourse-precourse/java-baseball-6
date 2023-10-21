fd6fb0f97f06ec437390b75659c07914f745df12
sohyun9527 <sohyun95277@gmail.com>
2023-10-21 15:38:26 +0900

    test(AnswerMakerTest): 단위별 기능 테스트 작성 객체 생성 후 요구사항대로 정답이 저장되는지 테스트 객체 호출 시 각기 다른 답을 생성하는지 테스트

--- src/test/java/baseball/AnswerMakerTest.java
+++ src/test/java/baseball/AnswerMakerTest.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -40,6 +41,35 @@ class AnswerMakerTest {
             List<Integer> testList = answer.getAnswer();
             assertEquals(3, testList.size());
         }
+    }
+
+    @DisplayName("객체 생성 후 올바른 정답이 저장되는지 테스트")
+    @Test
+    public void testSaveAnswer() throws Exception {
+        //given
+        int min = 1;
+        int max = 9;
+
+        //when
+        AnswerMaker answerMaker1 = new AnswerMaker(1, 9);
+        List<Integer> answer = answerMaker1.getAnswer();
+        //then
+        assertEquals(3, answer.size(), "세 자리 숫자입니다!");
+        assertTrue(answer.stream().allMatch(num -> num >= min && num <= max),
+                "범위 내의 숫자입니다!");
+
+    }
+
+    @DisplayName("다른 답을 생성하는지 테스트")
+    @Test
+    public void saveAnswerTest() throws Exception {
+        int min = 1;
+        int max = 9;
+
+        AnswerMaker answer1 = new AnswerMaker(1, 9);
+        AnswerMaker answer2 = new AnswerMaker(1, 9);
+
+        assertNotEquals(answer1, answer2);
 
     }
 }
