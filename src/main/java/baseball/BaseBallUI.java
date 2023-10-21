4efc1b844e5f1c68296fee013537286e2498e180
hyunsu15 <48500411+hyunsu15@users.noreply.github.com>
2023-10-21 16:29:24 +0900

    feat: 게임 시작 문구를 출력한다.

---
+++ src/main/java/baseball/BaseBallUI.java
@@ -0,0 +1,8 @@
+package baseball;
+
+public class BaseBallUI {
+    private static final String START_MESSAGE ="숫자 야구 게임을 시작합니다.";
+    void gameStart(){
+        System.out.println(START_MESSAGE);
+    }
+}
