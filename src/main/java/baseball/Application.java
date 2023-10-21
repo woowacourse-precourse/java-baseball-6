52904c74a3a5ce18ad001ae726e8bff2574dca55
kimtaesoo <kimtaesoo7@naver.com>
2023-10-21 22:37:07 +0900

    feat: 기본 뼈대 구축

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import baseball.controller.Controller;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Controller controller = new Controller(new OutputView(), new InputView());
+        controller.start();
     }
 }
---
+++ src/main/java/baseball/controller/Controller.java
@@ -0,0 +1,19 @@
+package baseball.controller;
+
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+public class Controller {
+
+    private final OutputView outputView;
+    private final InputView inputView;
+
+    public Controller(OutputView outputView, InputView inputView) {
+        this.outputView = outputView;
+        this.inputView = inputView;
+    }
+
+    public void start() {
+        // TODO: 프로그램 구현
+    }
+}
