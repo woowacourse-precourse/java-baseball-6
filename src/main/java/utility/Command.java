fc9feee0e6d9fb6314db610793fdb3227557d3e5
Austin-Choi <austinsupra@gmail.com>
2023-10-21 10:40:22 +0900

    Feat:진행 명령어 상수 클래스 추가

---
+++ src/main/java/utility/Command.java
@@ -0,0 +1,9 @@
+package utility;
+
+public class Command {
+    public static final int INITIALIZE = 0;
+    public static final int RESTARTED = 1;
+    public static final int EXIT = 2;
+
+    Command(){}
+}
