b95072e4687ad5f0ecb94832a32545461663e4e7
noxknow <chris2769@naver.com>
2023-10-21 14:35:02 +0900

    update(BaseballGameService) : 숫자를 비교하는 compareNumber 메서드 추가

--- src/main/java/baseball/service/BaseballGameService.java
+++ src/main/java/baseball/service/BaseballGameService.java
@@ -9,7 +9,24 @@ import java.util.List;
 public class BaseballGameService {
 
     public void compareNumber(UserBaseballNumber userBaseballNumber) {
+        List<Integer> computer = getComputerNumbers();
+        int strike = 0, ball = 0;
 
+        while (true) {
+            List<Integer> userNumbers = userBaseballNumber.getUserValues();
+
+            for (int i = 0; i < userNumbers.size(); i++) {
+                if (userNumbers.get(i) == computer.get(i)) {
+                    strike += 1;
+                } else if (computer.contains(userNumbers.get(i))) {
+                    ball += 1;
+                }
+            }
+
+            if (strike == 3) {
+                break;
+            }
+        }
     }
 
     private List<Integer> getComputerNumbers() {
