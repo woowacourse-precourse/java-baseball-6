de92f7fd6be0a71ae8dc1f1f8b58ddf7fc880c3f
JuSeong1130 <jsmoon1130@naver.com>
2023-10-21 22:34:01 +0900

    feat(GameResult) : 게임 결과 담을 값 객체 구현

---
+++ src/main/java/baseball/GameResult.java
@@ -0,0 +1,20 @@
+package baseball;
+
+public class GameResult {
+
+    private int strike;
+    private int ball;
+
+    public GameResult(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+}
