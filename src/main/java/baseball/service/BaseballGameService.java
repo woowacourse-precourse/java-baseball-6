630ba308d3d3ebfe2e6f824158390a1096ac96a7
5jeong <wjdgns4385@naver.com>
2023-10-21 20:59:36 +0900

    [Feat] : 숫자 입력값이 올바른지 검증하는 기능 구현

--- src/main/java/baseball/service/BaseballGameService.java
+++ src/main/java/baseball/service/BaseballGameService.java
@@ -2,12 +2,7 @@ package baseball.service;
 
 import baseball.model.Computer;
 import baseball.util.Randomutil;
-import baseball.view.InputView;
-
-import javax.print.DocFlavor;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class BaseballGameService {
 
@@ -26,12 +21,17 @@ public class BaseballGameService {
         computer = new Computer(Randomutil.CreateRandomNum());
     }
 
-    public HashMap<String, Integer> judgeResult(String num) {
-        System.out.println("컴퓨터 숫자 : "+computer.getComputerNum());
+    public ArrayList<Integer> changeStirngToList(String num) {
         ArrayList<Integer> nums = new ArrayList<>();
         for (char x : num.toCharArray()) {
             nums.add(Integer.parseInt(String.valueOf(x)));
         }
+        return nums;
+    }
+
+    public HashMap<String, Integer> judgeResult(String num) {
+        ArrayList<Integer> nums = changeStirngToList(num);
+
         for (int i = 0; i < nums.size(); i++) {
             if (computer.getComputerNum().contains(nums.get(i)) && computer.getComputerNum().get(i) == nums.get(i)) {
                 userResult.put(STRIKE_KEY, userResult.get(STRIKE_KEY) + 1);
@@ -43,6 +43,27 @@ public class BaseballGameService {
         return userResult;
     }
 
+    public void judgeCorrectInputNum(String num) {
+        if (num.length() != 3) {
+            throw new IllegalArgumentException("입력한 숫자가 3자리의 수가 아닙니다.");
+        }
+        if (num.contains("0")) {
+            throw new IllegalArgumentException("1~9사이의 숫자만 입력해야 합니다.");
+        }
+        HashSet<Character> inputNumSet = new HashSet<>();
+        for (char x : num.toCharArray()) {
+            inputNumSet.add(x);
+        }
+        if (inputNumSet.size() < 3) {
+            throw new IllegalArgumentException("서로 다른 숫자를 입력해야 합니다.");
+        }
+    }
+
+    public void judgeCorrectRestartNum(int num) {
+        if (num != 1 && num != 2) {
+            throw new IllegalArgumentException("1,2 둘중 하나의 수만 입력해야 합니다.");
+        }
+    }
 
     public int StrikeCount() {
         return userResult.get("STRIKE");
