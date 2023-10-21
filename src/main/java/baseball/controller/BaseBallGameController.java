ef3fc61fe6f45fa5d9789a917fdbc5d0137e9b6d
TaeHye0n <thk001229@gmail.com>
2023-10-21 21:19:03 +0900

    feat: 게임 진행 플로우 제어  - 한번의 게임의 플로우를 담당하는 startGame() 메서드  - 정답을 맞출 경우 startGame() 메서드 종  - 전체게임의 플로우를 담당하며, Main에서 접근 가능한 run() 메서드  - 한 게임 종료후 유저 입력으로 재시작 또는 종료 제어

--- src/main/java/baseball/controller/BaseBallGameController.java
+++ src/main/java/baseball/controller/BaseBallGameController.java
@@ -2,19 +2,24 @@ package baseball.controller;
 
 import baseball.domain.BaseBallGameService;
 import baseball.domain.ComputerRandomNumbers;
+import baseball.utils.Utils;
 import baseball.validator.InputValidate;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class BaseBallGameController {
+
+    private static final String CORRECT_RESULT = "3스트라이크";
+    private static final String RESTART_COMMAND = "1";
+    private static final String END_COMMAND = "2";
 
     private final InputView inputView;
     private final OutputView outputView;
     private final BaseBallGameService baseBallGameService;
     private final InputValidate inputValidate;
+    private ComputerRandomNumbers computerRandomNumbers;
 
     public BaseBallGameController(
             InputView inputView,
@@ -30,19 +35,52 @@ public class BaseBallGameController {
 
     public void run() {
         outputView.printStartMessage();
-        
-        String userInput = inputView.getUserInput();
+        boolean isRestart = false;
+
+        do {
+            startGame();
+            outputView.printFinishMessage();
+            outputView.printRestartOrEndMessage();
+            String userGameCommand = inputView.getUserGameCommand();
+            inputValidate.validateGameCommand(userGameCommand);
+            if (userGameCommand.equals(RESTART_COMMAND)) {
+                isRestart = true;
+            }
+
+            if (userGameCommand.equals(END_COMMAND)) {
+                isRestart = false;
+            }
+
+        } while (isRestart);
+
+    }
+
+    private void startGame() {
+        updateRandomNumber();
+
+        while (true) {
+            outputView.printInputNumberMessage();
+
+            String userInput = inputView.getUserInput();
+            validateInput(userInput);
+            List<Integer> userInputs = Utils.stringToIntList(userInput);
+
+            String result = baseBallGameService.startGame(computerRandomNumbers, userInputs);
+            outputView.printResult(result);
+            if (result.equals(CORRECT_RESULT)) {
+                return;
+            }
+        }
+    }
+
+    private void updateRandomNumber() {
+        computerRandomNumbers = new ComputerRandomNumbers();
+    }
+
+    private void validateInput(String userInput) {
         inputValidate.validateNumeric(userInput);
         inputValidate.validateLength(userInput);
         inputValidate.validateDuplicateNumber(userInput);
-
-        List<Integer> userInputs = userInput.chars()
-                .mapToObj(Character::getNumericValue)
-                .collect(Collectors.toList());
-
-        ComputerRandomNumbers computerRandomNumbers = new ComputerRandomNumbers();
-
-        baseBallGameService.startGame(computerRandomNumbers, userInputs);
     }
 
 }
