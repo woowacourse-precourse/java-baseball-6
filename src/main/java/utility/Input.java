aab0e1050eefd1ea65c48cb674d00c93eaf834b0
Austin-Choi <austinsupra@gmail.com>
2023-10-21 11:35:27 +0900

    Add: 컴퓨터 숫자 입력을 위한 유틸리티 클래스 추가

---
+++ src/main/java/utility/Input.java
@@ -0,0 +1,19 @@
+package utility;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Input {
+    private Input(){}
+    public static List<Integer> sliceToThree(final int[] input, int count) throws IllegalArgumentException{
+        List<Integer> chunk = new ArrayList<>();
+        int currentNumber = 0;
+        if(input.length % 3 != 0)
+            throw new IllegalArgumentException();
+        for(int index = count*3; index < count*3+3; index++){
+            currentNumber = input[index];
+            chunk.add(currentNumber);
+        }
+        return chunk;
+    }
+}
