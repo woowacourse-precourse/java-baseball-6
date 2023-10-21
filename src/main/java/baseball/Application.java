0beff17f45e86e18f056d824211c92e483704d19
Donggeon Moon <nullsafer@naver.com>
2023-10-21 15:53:38 +0900

    feat: 서로 다른 숫자인지 검증하는 로직 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -68,6 +68,10 @@ public class Application {
         if (!inputValue.matches("^[0-9]{3}$")) {
             throw new IllegalArgumentException("숫자 세 자리만 입력해주세요.");
         }
+
+        if (inputValue.charAt(0) == inputValue.charAt(1) || (inputValue.charAt(1) == inputValue.charAt(2))) {
+            throw new IllegalArgumentException("서로 다른 3개의 숫자를 입력해주세요.");
+        }
     }
 
     private static List<Integer> getRandomIntegers() {
