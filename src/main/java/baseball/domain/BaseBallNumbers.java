50fa2ae7681293ed8581def60b822e37ee549c1b
jinsu4755 <parkjinsu4755@gmail.com>
2023-10-22 03:50:40 +0900

    feat: 숫자 야구 수 추가 함수 작성

--- src/main/java/baseball/domain/BaseBallNumbers.java
+++ src/main/java/baseball/domain/BaseBallNumbers.java
@@ -1,5 +1,7 @@
 package baseball.domain;
 
+import exception.DuplicateBaseBallNumber;
+import exception.OutOfBaseBallNumbersSize;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,4 +25,21 @@ public final class BaseBallNumbers {
         return baseballNumbers.size();
     }
 
+    public void add(int number) {
+        validateDuplicate(number);
+        validateNumbersSize();
+        baseballNumbers.add(number);
+    }
+
+    public void validateDuplicate(int number) {
+        if (baseballNumbers.contains(number)) {
+            throw new DuplicateBaseBallNumber(String.format("Number %d already exists", number));
+        }
+    }
+
+    public void validateNumbersSize() {
+        if (this.size() >= MAX_BASE_BALL_SIZE) {
+            throw new OutOfBaseBallNumbersSize(String.format("max size of baseball numbers is %d", MAX_BASE_BALL_SIZE));
+        }
+    }
 }
