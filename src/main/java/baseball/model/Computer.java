4d32a359d935492878ce2eecf9765b81286a4fb1
chosunghyun18 <krismon@naver.com>
2023-10-21 17:06:07 +0900

    feat :add getHint by player choosen Balls

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -1,21 +1,23 @@
 package baseball.model;
 
+import baseball.view.OutputView;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.LinkedHashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class Computer {
     private Set<Ball> balls;
+    private final OutputView outputView;
     private final static Integer MIN_VALUE = 1;
     private final static Integer MAX_VALUE = 9;
 
     public Computer() {
-        this.balls = new LinkedHashSet<>();
+        outputView = new OutputView() ;
         initComputerNumbers();
     }
 
     public void initComputerNumbers() {
+        this.balls = new LinkedHashSet<>();
         while (balls.size() < 3) {
             int randomNumber = pickNumberFromThreadLocalRandomForBalls(MIN_VALUE, MAX_VALUE);
             balls.add(new Ball(randomNumber));
@@ -24,5 +26,46 @@ public class Computer {
 
     public Integer pickNumberFromThreadLocalRandomForBalls(int minValue, int maxValue) {
         return Randoms.pickNumberInRange(minValue, maxValue);
+    }
+    public boolean getHintByPlayer(Set<Ball> playerBalls) {
+        int ballCount = 0 ;
+        int strikeCount = 0 ;
+        int computeIndex = 0 ;
+        int playerIndex  = 0 ;
+        for(Ball computeBall : balls) {
+            computeIndex++ ;
+            for(Ball playerBall : playerBalls) {
+                playerIndex++ ;
+                if(playerBall.equals(computeBall)){
+                    if(computeIndex == playerIndex){
+                        strikeCount++;
+                    }else{
+                        ballCount++;
+                    }
+                }
+            }
+
+        }
+
+        if(strikeCount == 3) {
+            System.out.println();
+            outputView.showGameClearMessage();
+            return true;
+        }
+        if(strikeCount != 0 && ballCount != 0) {
+            outputView.showBallAndStrikeHint(ballCount,strikeCount);
+            System.out.println();
+            return false;
+        }
+        if(strikeCount == 0 && ballCount != 0) {
+            outputView.showBallOnlyHint(ballCount);
+            return false;
+        }
+        if(strikeCount != 0) {
+            outputView.showStrikeOnlyHint(strikeCount);
+            return false;
+        }
+        outputView.showNothingHint();
+        return false;
     }
 }
