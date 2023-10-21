04140299da30dc8aaa128b141f66c9100a5ce51d
GS_song <20003204@sju.ac.kr>
2023-10-21 16:12:12 +0900

    feat: 매직 리터럴 / 매직 넘버 사용을 자제하기 위한 상수 정의

---
+++ src/main/java/baseball/constant/Constants.java
@@ -0,0 +1,18 @@
+package baseball.constant;
+
+public final class Constants {
+
+    //input
+    public final static int INPUT_MAX_SIZE = 3;
+    public final static int INPUT_NUM_MIN = 1;
+    public final static int INPUT_NUM_MAX = 9;
+    public final static String INPUT_PREDICTED_MESSAGE = "숫자를 입력해주세요 : ";
+    public final static int INPUT_RESTART = 1;
+    public final static int INPUT_STOP = 2;
+    public final static String INPUT_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+
+    //output
+    public final static String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public final static String GAME_STOP_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+}
