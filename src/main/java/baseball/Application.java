4ebc24991ff9df32b5db3e829ed60b9e89762d03
ru-kka <ruka4739@gmail.com>
2023-10-21 23:07:41 +0900

    feat: 입력 자리수 에러 처리 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            System.out.print("숫자를 입력해주세요 : ");
+            String player = Console.readLine();
+            if(player.length() != 3){
+                throw new IllegalArgumentException("입력은 세자리로 이루어져야합니다.");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("예외 발생 - " + e.getMessage());
+        }
     }
 }
