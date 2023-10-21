2749fc798a8a414ec15a07f094535fa4c304b436
NamJihyun99 <namjihyun9936@gmail.com>
2023-10-22 05:10:14 +0900

    feat:> main method 구조

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,10 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int choice;
+        do {
+            playBaseBall();
+            choice = getChoice();
+        } while(choice == 1);
     }
 }
