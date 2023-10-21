6c9920eb822cff0d325e9e84b1db473dcf853b4f
Haeun Jeong <gkdms4248@gmail.com>
2023-10-21 16:01:02 +0900

    fix: 프린트 메서드 분리

--- src/main/java/baseball/PrintMessage.java
+++ src/main/java/baseball/PrintMessage.java
@@ -29,6 +29,9 @@ public class PrintMessage {
 
     public void correctMessage() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public void menuMessage() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 }
