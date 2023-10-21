1f504e7859494d66b9102ac41ff21c2e93027487
kim <hwiyul_g@daum.net>
2023-10-21 16:39:48 +0900

    feat(UserNumber) : 입력값의 길이 검사하는 함수 추가

--- src/main/java/baseball/UserNumber.java
+++ src/main/java/baseball/UserNumber.java
@@ -3,11 +3,19 @@ package baseball;
 import java.util.List;
 
 public class UserNumber {
+    private static final int NUMBER_SIZE = 3;
     private final List<Integer> userNumber;
 
     public UserNumber(List<Integer> userNumber) {
         this.userNumber = userNumber;
     }
+
+    private void validateLength(List<Integer> userInputNumber){
+        if(userInputNumber.size() != NUMBER_SIZE){
+            throw new IllegalArgumentException();
+        }
+    }
+
 
     public List<Integer> getUserNumber(){
         return userNumber;
