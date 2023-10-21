b5396951e478eeef49698992597acd14c4421c68
임강호 <55732952+Icln@users.noreply.github.com>
2023-10-21 22:18:41 +0900

    feat: 사용자 입력 예외처리 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,8 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Application {
     public static void main(String[] args) {
@@ -11,6 +13,13 @@ public class Application {
         while (true) {
             System.out.printf("숫자를 입력하세요 : ");
             String inputString = Console.readLine();
+            Set<Character> inputArr = new HashSet<>();
+            for (char c : inputString.toCharArray()) {
+                inputArr.add(c);
+            }
+            if (inputArr.size() != 3 || inputString.contains("0")) {
+                throw new IllegalArgumentException();
+            }
         }
     }
 }
