5cf976d2b79b45d5edaff623aee970c2b851079d
kim <hwiyul_g@daum.net>
2023-10-21 16:53:38 +0900

    feat(UserNumber) : Exception message 추가

--- src/main/java/baseball/UserNumber.java
+++ src/main/java/baseball/UserNumber.java
@@ -18,14 +18,14 @@ public class UserNumber {
 
     private void validateLength(String userInputNumber){
         if(userInputNumber.length() != NUMBER_SIZE){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("입력 숫자의 길이가 3이 아닙니다.");
         }
     }
 
     private void validateDigit(String userInputNumber){
         for(char ch : userInputNumber.toCharArray()){
             if(!Character.isDigit(ch) || ch <'1' || ch >'9'){
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("입력 값이 숫자가 아닙니다.");
             }
         }
     }
@@ -34,7 +34,7 @@ public class UserNumber {
         Set<Character> set = new HashSet<>();
         for(char ch : userInputNumber.toCharArray()){
             if(set.contains(ch)){
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("입력값에 중복된 값이 있습니다.");
             }
             set.add(ch);
         }
