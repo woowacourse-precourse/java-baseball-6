3269e3b3c899834a53a9ee62c3efba9b4703f0f4
YongHyeon <pyh4616@naver.com>
2023-10-21 21:26:01 +0900

    Fix: InputView, 잘못된 줄바꿈 제거

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -10,13 +10,11 @@ public class InputView {
 
     public List<Integer> inputUserNumber() {
         String input = readLine();
-        System.out.println();
         return Convert.convertStringToIntegerList(input);
     }
 
     public int inputRestartOrExitNumber() {
         String input = readLine();
-        System.out.println();
         return Convert.convertStringToInteger(input);
     }
 }
