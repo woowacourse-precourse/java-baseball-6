fe90560b86e0b91baa9ef3b323032dcde64718d5
hoyeon <highoption@naver.com>
2023-10-21 16:07:31 +0900

    feat : RandomNumberGenerator 클래스 생성 및 구현

---
+++ src/main/java/baseball/model/RandomNumberGenerator.java
@@ -0,0 +1,16 @@
+package baseball.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomNumberGenerator {
+    public static List<Integer> makeRandomNumber() {
+        List<Integer> answerNumbers = new ArrayList<>();
+        while (answerNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            answerNumbers.add(randomNumber);
+        }
+        return answerNumbers;
+    }
+}
