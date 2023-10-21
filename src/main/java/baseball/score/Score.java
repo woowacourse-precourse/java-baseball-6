6f87ff87d84f1d94e099a7560240c0185f202a26
chaeshee0908 <chaeshee0908@naver.com>
2023-10-22 01:55:05 +0900

    [feat] 숫자 판별 기능 구현

---
+++ src/main/java/baseball/score/Score.java
@@ -0,0 +1,44 @@
+package baseball.score;
+
+import baseball.game.Game;
+
+import java.util.List;
+
+public class Score {
+
+    private static final String BALL = "볼";
+    private static final String STRIKE = "스트라이크";
+    private static final String NOTHING = "낫싱";
+
+    public static void checkScore(List<Integer> input, List<Integer> randomNumber) {
+        int ballCount = 0;
+        int strikeCount = 0;
+
+        for (int i = 0; i < 3; i++) {
+            if (input.get(i) == randomNumber.get(i)) {
+                strikeCount++;
+            } else if (randomNumber.contains(input.get(i))) {
+                ballCount++;
+            }
+        }
+
+        printScore(ballCount, strikeCount);
+
+        if (strikeCount == 3) {
+            Game.successGame();
+        }
+    }
+
+    private static void printScore(int ball, int strike) {
+        if (ball != 0 && strike != 0) {
+            System.out.println(ball + BALL + " " + strike + STRIKE);
+        } else if (ball != 0) {
+            System.out.println(ball + BALL);
+        } else if (strike != 0) {
+            System.out.println(strike + STRIKE);
+        } else {
+            System.out.println(NOTHING);
+        }
+    }
+
+}
