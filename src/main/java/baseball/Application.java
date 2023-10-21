c62a15c909c9c2fbf4c79c542c964ec7e867ff32
mmihye <smilehye910@naver.com>
2023-10-21 20:18:00 +0900

    [CLN]내부에서만 사용하는 메소드들 private로 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,5 +4,8 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
+        BaseBallGame baseBallGame = new BaseBallGame();
+        baseBallGame.startGame();
+        baseBallGame.play();
     }
 }
--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -42,7 +42,7 @@ public class BaseBallGame {
         }
     }
 
-    public String getResult(){
+    private String getResult(){
         String msg = "";
 
         int ballNum = Calculater.getBall(answerNum, userNum);
@@ -58,7 +58,7 @@ public class BaseBallGame {
         return msg;
     }
 
-    public void exitGame(){
+    private void exitGame(){
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String input = Console.readLine();
