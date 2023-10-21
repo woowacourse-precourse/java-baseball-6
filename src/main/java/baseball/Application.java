6543ed94b94e6de46a14742b6dff1f0433486c49
eom-tae-in <eti0728@daum.net>
2023-10-21 20:06:04 +0900

    refactor: Controller의 인스턴스 변수의 수를 줄이기 위해 코드 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,6 @@
 package baseball;
 
 import baseball.controller.Controller;
-import baseball.domain.Computer;
-import baseball.domain.GuessNumber;
-import baseball.domain.State;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 
@@ -11,8 +8,7 @@ public class Application {
 
     public static void main(String[] args) {
         OutputView outputView = new OutputView();
-        Controller controller = new Controller(new Computer(), new GuessNumber(), new InputView(outputView), outputView,
-                new State());
+        Controller controller = new Controller(new InputView(outputView), outputView);
         controller.start();
     }
 }
--- src/main/java/baseball/controller/Controller.java
+++ src/main/java/baseball/controller/Controller.java
@@ -12,59 +12,54 @@ import baseball.view.OutputView;
 
 public class Controller {
 
-    private final Computer computer;
-    private final GuessNumber guessNumber;
     private final InputView inputView;
     private final OutputView outputView;
-    private final State state;
 
-    public Controller(final Computer computer,
-                      final GuessNumber guessNumber,
-                      final InputView inputView,
-                      final OutputView outputView,
-                      final State state) {
-        this.computer = computer;
-        this.guessNumber = guessNumber;
+    public Controller(final InputView inputView,
+                      final OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
-        this.state = state;
     }
 
     public void start() {
-        play();
-        askMore();
-        if (state.isMoreGame()) {
-            start();
+        Computer computer = new Computer();
+        State state = new State();
+        GuessNumber guessNumber = new GuessNumber();
+        play(computer, guessNumber, state);
+        askMore(state);
+        while (state.isMoreGame()) {
+            play(computer, guessNumber, state);
+            askMore(state);
         }
     }
 
-    private void play() {
+    private void play(final Computer computer, final GuessNumber guessNumber, final State state) {
         outputView.printStartMessage();
         while (!computer.isGameOver()) {
-            guess();
-            calculate();
+            guess(guessNumber);
+            calculate(computer, guessNumber);
         }
-        end();
+        end(computer, state);
     }
 
-    private void guess() {
+    private void guess(final GuessNumber guessNumber) {
         outputView.printGuessNumberInputMessage();
         String guessNumbers = inputView.readGuessNumbers();
         guessNumber.changeGuessNumbers(convertStringToIntegerList(guessNumbers));
     }
 
-    private void calculate() {
+    private void calculate(final Computer computer, final GuessNumber guessNumber) {
         GuessNumberResultResponse resultResponse = computer.calculateGuessNumberResult(guessNumber.getGuessNumbers());
         outputView.printGuessNumberResult(resultResponse.getResult());
     }
 
-    private void askMore() {
+    private void askMore(final State state) {
         outputView.printRestartOrFinishMessage();
         String stateNumber = inputView.readGameStateNumber();
         state.changeState(convertStringToInt(stateNumber));
     }
 
-    private void end() {
+    private void end(final Computer computer, final State state) {
         computer.end();
         state.end();
         outputView.printWinningMessage();
