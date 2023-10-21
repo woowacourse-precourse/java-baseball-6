7c60e4e40ffe6e4b1c217ac0aa71d4f66dce57e0
Relaxed-Mind <dlwlstn0709@uos.ac.kr>
2023-10-21 18:17:50 +0900

    Refactor: Add constant value 재시작과 종료를 입력받는 상수 추가 Constants #2

--- src/main/java/baseball/common/Constants.java
+++ src/main/java/baseball/common/Constants.java
@@ -1,5 +1,7 @@
 package baseball.common;
 
 public class Constants {
+    public static final int RESTART = 1;
+    public static final int END_GAME = 2;
     public static final int NUMBER_LENGTH = 3;
 }
