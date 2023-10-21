df2c76e22efd57dce49b209843dc82aa5ef0b8fc
watermelon3737 <pshrina1@naver.com>
2023-10-21 16:11:31 +0900

    [feat] : 1~9까지의 랜덤한 정답을 만들어주는 메서드, 정답을 클래스 외부로 보내주는 getter 메서드

---
+++ src/main/java/baseball/NumberGenerator.java
@@ -0,0 +1,26 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class NumberGenerator {
+    private final int maxNumber = 9;
+    private final int minNumber = 1;
+    private List<Integer> answer = new ArrayList<Integer>();
+
+    public NumberGenerator() {
+        while (answer.size() < 3) {
+            final int randomNumber = Randoms.pickNumberInRange(minNumber, maxNumber);
+            if (!answer.contains(randomNumber)) {
+                answer.add(randomNumber);
+            }
+        }
+    }
+
+    public List<Integer> getAnswer() {
+        return this.answer;
+    }
+}
