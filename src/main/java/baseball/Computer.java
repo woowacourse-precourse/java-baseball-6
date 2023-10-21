31278993003d377b4ced797f7da5aad199151c74
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-10-22 01:39:09 +0900

    feat : 상대방 기능 요구사항 2번 완성
    "상대방은 사용자가 입력한 값에 대한 결과를 보내준다." 기능 구현
    해당 기능 수행 후 나올수 있는 case Unit Test 작성
    docs/README.md 이모티콘 수정

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -9,15 +9,18 @@ public class Computer {
     private static final int minimumCanCreatedValue = 1;
     private static final int maximumCanCreatedValue = 9;
     private final ArrayList<Integer> numberList;
+    private final int digitCount;
     public ArrayList<Integer> getNumberList(){
         return this.numberList;
     }
 
     public Computer() {
         this.numberList = generateRandomNumberListWithNotRepeating();
+        this.digitCount = defaultDigitalCount;
     }
     public Computer(int digitCount) {
         this.numberList = generateRandomNumberListWithNotRepeating(digitCount);
+        this.digitCount = digitCount;
     }
     private ArrayList<Integer> generateRandomNumberListWithNotRepeating(){
         return generateRandomNumberListWithNotRepeating(defaultDigitalCount);
@@ -35,4 +38,65 @@ public class Computer {
 
         return randomNumberList;
     }
+
+    public Boolean checkUserNumberAndResponse(int playerNumber){
+
+        int ballCount = 0;
+        int strikeCount = 0;
+
+        var compareNumberList = parsingNumberToNumberList(playerNumber);
+        for (int i =0 ; i< digitCount;i++){
+            if (compareNumberList[i]==numberList.get(i)){
+                strikeCount++;
+                compareNumberList[i]=-1;
+            }
+        }
+        for (int i =0; i<digitCount;i++){
+            if (compareNumberList[i]==-1){
+                continue;
+            }
+            if (numberList.contains(compareNumberList[i])){
+                ballCount++;
+                compareNumberList[i] = -1;
+            }
+        }
+
+        printResultWithCount(ballCount,strikeCount);
+
+        if (strikeCount==3){
+            return true;
+        }
+        return false;
+    }
+    private int[] parsingNumberToNumberList(int parsingNumber) {
+        int sliceUnit = (int) Math.pow(10, digitCount - 1);
+        var numberList = new int[digitCount];
+        for (int i = 0; i < digitCount; i++) {
+            int number = parsingNumber / sliceUnit;
+            numberList[i]=number;
+            parsingNumber %= sliceUnit;
+            sliceUnit /= 10;
+        }
+        return numberList;
+    }
+
+    protected void printResultWithCount(int strikeCount,int ballCount){
+        StringBuilder resultBuilder = new StringBuilder();
+        if (strikeCount==0 && ballCount == 0){
+            resultBuilder.append("낫싱");
+        }
+        if (ballCount!=0) {
+            resultBuilder.append(ballCount);
+            resultBuilder.append("볼");
+        }
+        if (strikeCount!=0){
+            resultBuilder.append(" ");
+            resultBuilder.append(strikeCount);
+            resultBuilder.append("스트라이크");
+        }
+        String result = resultBuilder.toString();
+        System.out.println(result);
+        return;
+    }
+
 }
--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -4,12 +4,18 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.HashSet;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ComputerTest {
     private Computer computer;
+    @BeforeEach
+    void setUp(){
+        computer = new Computer();
+    }
     @Test
     void 중복되지않는_세자리_숫자를_생성한다(){
 
@@ -23,8 +29,53 @@ public class ComputerTest {
         // 중복된 숫자가 없어야 한다.
         HashSet<Integer> numberSet = new HashSet<>(numberList);
         assertEquals(digitCount,numberSet.size());
+    }
+    @Test
+    void testCheckUserNumberAndResponse_숫자가_전부_일치하면_참을_리턴한다(){
+        var numberList = computer.getNumberList();
 
+        int correctAnswer = 0;
+        for (int number : numberList){
+            correctAnswer = correctAnswer*10 + number;
+        }
+
+        // computer 가 가지는 숫자를 넣었으므로 참이 나와야 한다.
+        assertEquals(true,computer.checkUserNumberAndResponse(correctAnswer));
+    }
+    @Test
+    void testCheckUserNumberAndResponse_숫자가_하나라도_일치하지_않으면_거짓을_리턴한다(){
+        var numberList = computer.getNumberList();
+
+        StringBuilder wrongAnswerBuilder = new StringBuilder();
+        for (int i =0; i<numberList.size();i++){
+            wrongAnswerBuilder.append(0);
+        }
+
+        // 절대 나올수 없는 숫자를 넣었으므로 거짓이 나와야 한다.
+        int wrongAnswer = Integer.parseInt(wrongAnswerBuilder.toString());
+        assertEquals(false,computer.checkUserNumberAndResponse(wrongAnswer));
+    }
+    @Test
+    void testPrintResultWithCount_숫자가_하나라도_일치하지_않으면_낫싱을_출력한다(){
+        ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStreamCaptor));
+
+        computer.printResultWithCount(0,0);
+
+        String expectedAnswer = "낫싱";
+        Assertions.assertEquals(expectedAnswer,outputStreamCaptor.toString().trim());
+    }
+    @Test
+    void testPrintResultWithCount_볼카운트_또는_스트라이크카운트_가_있으면_출력한다(){
+        ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStreamCaptor));
+        int strikeCount = 3;
+        int ballCount = 1;
+
+        computer.printResultWithCount(strikeCount,ballCount);
+
+        String expectedAnswer = ballCount+"볼 "+strikeCount+"스트라이크";
+        Assertions.assertEquals(expectedAnswer,outputStreamCaptor.toString().trim());
 
     }
-
 }
