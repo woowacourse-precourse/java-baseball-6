6afb1dc8f95357da72034b2f31c7870e24c2eefa
Firedrago95 <yonghwa95@naver.com>
2023-10-21 12:52:55 +0900

    test Numbers 객체 생성으로 인한 Computer 테스트코드 수정

--- src/test/java/baseball/Model/ComputerTest.java
+++ src/test/java/baseball/Model/ComputerTest.java
@@ -11,27 +11,29 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class ComputerTest {
 
-    private List<Integer> computerNumbers;
+    private Numbers computerNumbers;
+    private List<Integer> computerBalls;
 
     @BeforeEach
     @DisplayName("객체생성")
     public void getInstance() {
         Computer computer = new Computer();
         computerNumbers = computer.getNumbers();
+        computerBalls = computerNumbers.getBalls();
     }
 
     @Test
     @DisplayName("Computer객체가 3자리 숫자를 생성하는지 확인")
     public void generateThreeDigitNumberTest() {
-        assertThat(computerNumbers.size()).isEqualTo(3);
+        assertThat(computerBalls.size()).isEqualTo(3);
     }
 
     @Test
     @DisplayName("Computer객체가 중복된 숫자를 가지지 않는지 확인")
     public void generateUniqueNumberTest() {
-        for (int i = 0; i < computerNumbers.size(); i++) {
-            for (int j = i + 1; j < computerNumbers.size(); j++) {
-                assertNotEquals(computerNumbers.get(i), computerNumbers.get(j));
+        for (int i = 0; i < computerBalls.size(); i++) {
+            for (int j = i + 1; j < computerBalls.size(); j++) {
+                assertNotEquals(computerBalls.get(i), computerBalls.get(j));
             }
         }
     }
