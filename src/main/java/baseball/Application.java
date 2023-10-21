1e4a3c680dbd38ccc1460bc8c3c70f31e78395ea
chaeshee0908 <chaeshee0908@naver.com>
2023-10-22 00:11:33 +0900

    [feat] 숫자 유효성 검사 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -27,6 +27,13 @@ public class Application {
         return randomNumber;
     }
 
+    public void validateInputNumber(String input) {
+        if (input.length() != 3 || !input.matches("\\d+")) {
+            throw new IllegalArgumentException("유효한 숫자가 아닙니다.");
+        }
+
+    }
+
     public String getUserNumber() {
         System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
