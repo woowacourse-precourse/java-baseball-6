80e9dd751192dffd9fee9600dbf7d81224af6d98
dltjdgh0428 <dltjdgh0428@naver.com>
2023-10-21 14:09:05 +0900

    refactor: Veiw 페이지 리펙터링

--- src/main/java/baseball/View/GameView.java
+++ src/main/java/baseball/View/GameView.java
@@ -6,26 +6,30 @@ public class GameView {
      * Model로 부터 데이터를 받을때는, 사용자마다 다르게 보여주어야하는 데이터에 대해서만 받아야한다.
      * 우리는근데 유저마다 다를게 없기에 의존하지 않는다는 원칙을 따른다.
      */
-    public void displayWelcomeMessage() {
+    public void printStartMessage() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    public void displayResult(int ball, int strike) {
-        String returnString = "";
-        if (ball != 0)
-            returnString = ball + "볼 ";
-        if (strike != 0)
-            returnString += strike + "스트라이크";
-        if (ball == 0 && strike == 0)
-            returnString = "낫싱";
-        System.out.println(returnString);
+    public void printBallStrikeMessage(){
+        System.out.print("볼 ");
+    }
+    public void printBallMessage(){
+        System.out.println("볼");
     }
 
-    public void displayGameOverMessage() {
+    public void printStrikeMessage(){
+        System.out.println("스트라이크");
+    }
+
+    public void printNothingMessage(){
+        System.out.println("낫싱");
+    }
+
+    public void printGameOverMessage() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
-    public void displayPrompt() {
+    public void printInputMessage() {
         System.out.println("숫자를 입력해주세요 : ");
     }
 
