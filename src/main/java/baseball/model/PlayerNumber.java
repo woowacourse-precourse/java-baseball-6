a1b29e1ad0b50805928ca70fb176813787b93935
JoonKyo <joonkyolee@gmail.com>
2023-10-22 02:40:55 +0900

    feat(PlayerNumber): 플레이어의 수에 대한 세터, 게터 생성 및 문자열을 리스트로 변환하는 메서드 추가

---
+++ src/main/java/baseball/model/PlayerNumber.java
@@ -0,0 +1,25 @@
+package baseball.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayerNumber {
+    private static List<Integer> playerNumber;
+
+    public List<Integer> getPlayerNumber() {
+        return playerNumber;
+    }
+
+    public void setPlayerNumber(String numbers) {
+        playerNumber = new ArrayList<>();
+        convert(numbers);
+    }
+
+    private static void convert(String numbers) {
+        playerNumber = new ArrayList<>();
+
+        for (String s : numbers.split("")) {
+            playerNumber.add(Integer.parseInt(s));
+        }
+    }
+}
