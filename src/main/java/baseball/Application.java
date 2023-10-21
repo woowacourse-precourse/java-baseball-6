401135617a10dcba003bcda9006cba27c927ffcd
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2023-10-21 15:32:41 +0900

    feat: create number checking function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,23 +3,29 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Application {
     static final int NUM_OF_DIGITS = 3;
+    static boolean GAME = true;
 
     public static void main(String[] args) {
-        List<Integer> computer = createRandomNumbers();
+        List<Integer> computerNumber = createRandomNumbers();
         System.out.println("숫자 야구 게임을 시작합니다.");
-        int[] userNumberDigits = new int[NUM_OF_DIGITS];
-        getUserInput(userNumberDigits);
-        System.out.println(Arrays.toString(userNumberDigits));
+        while (GAME){
+            int[] userNumber = new int[NUM_OF_DIGITS];
+            getUserInput(userNumber);
+            String ballStrikeCheck = checkNumber(userNumber, computerNumber);
+            System.out.println(ballStrikeCheck);
+            if (!GAME)
+                break;
+        }
     }
 
     /**
      * 컴퓨터가 랜덤한 3 자리 정수를 생성하는 함수
+     *
      * @return ArrayList[3]
      */
     public static List<Integer> createRandomNumbers() {
@@ -35,6 +41,7 @@ public class Application {
 
     /**
      * 사용자가 입력한 숫자를 3 자리로 나눠 배열에 저장하는 함수
+     *
      * @param userNumberDigits
      */
     private static void getUserInput(int[] userNumberDigits) {
@@ -44,7 +51,7 @@ public class Application {
         try {
             String userNumberString = scanner.next();
             userNumber = Integer.parseInt(userNumberString);
-            if (userNumberString.length() != 3)
+            if (userNumberString.length() != NUM_OF_DIGITS)
                 throw new IllegalAccessException();
         } catch (IllegalAccessException e) {
             e.printStackTrace();
@@ -55,5 +62,27 @@ public class Application {
             int index = NUM_OF_DIGITS - 1 - i;
             userNumberDigits[index] = digit;
         }
+    }
+
+    public static String checkNumber(int[] userNumbers, List<Integer> computerNumbers) {
+        int ballCount = 0;
+        int strikeCount = 0;
+        for (int i = 0; i < NUM_OF_DIGITS; i++) {
+            for (int j = 0; j < NUM_OF_DIGITS; j++) {
+                if (userNumbers[i] == computerNumbers.get(j) && i != j)
+                    ballCount++;
+                if (userNumbers[i] == computerNumbers.get(j) && i == j)
+                    strikeCount++;
+            }
+        }
+        String result = "아웃";
+        if (ballCount != 0 && strikeCount == 0) result = String.format("%d볼", ballCount);
+        if (ballCount == 0 && strikeCount != 0) result = String.format("%d스트라이크", strikeCount);
+        if (ballCount != 0 && strikeCount != 0) result = String.format("%d볼 %d스트라이크", ballCount, strikeCount);
+        if (strikeCount == NUM_OF_DIGITS) {
+            result = String.format("%d개의 숫자를 모두 맞히셨습니다! 게임종료", NUM_OF_DIGITS);
+            GAME = false;
+        }
+        return result;
     }
 }
