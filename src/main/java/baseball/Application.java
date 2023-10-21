be2801c710e00bf072164f68d621919db0fbb872
JunSeok Kim <skaksdl1238@gmail.com>
2023-10-21 21:58:03 +0900

    refactor - GameController -> SystemController변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,12 @@
 package baseball;
 
-import baseball.controller.GameController;
+import baseball.controller.SystemController;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        GameController gameController = new GameController();
+        SystemController systemController = new SystemController();
 
-        gameController.run();
+        systemController.run();
     }
 }
