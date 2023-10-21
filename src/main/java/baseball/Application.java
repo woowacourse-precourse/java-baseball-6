b5bc2d0ebef43ed90ee5d21ce28b6bb514b7f03f
whxogus215 <whxogus215@naver.com>
2023-10-21 09:06:25 +0900

    refactor : BaseBall 구현 클래스 -> Game 인터페이스로 추상화

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,14 +1,15 @@
 package baseball;
 
-import baseball.domain.BaseballCalculator;
 import baseball.domain.BaseBall;
+import baseball.domain.BaseballCalculator;
+import baseball.domain.Game;
 import baseball.domain.GameManager;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        BaseBall baseBall = new BaseBall(new BaseballCalculator());
-        GameManager gameManager = new GameManager(baseBall);
+        Game game = new BaseBall(new BaseballCalculator());
+        GameManager gameManager = new GameManager(game);
 
         gameManager.game();
     }
