fd25f95b6ee6a698666145cf693e8e265069a5c3
김효준 <khyojun1104@gmail.com>
2023-10-21 15:13:34 +0900

    [feat] BaseballService : ball 연산 기능 구현 !

--- src/main/java/baseball/service/BaseballService.java
+++ src/main/java/baseball/service/BaseballService.java
@@ -4,13 +4,36 @@ import baseball.dto.GameStateDto;
 
 public class BaseballService {
 
+    private final String userNumber;
+    private final String computerNumber;
 
 
-    public void caculate(String userNumber, String nowComNumber) {
+    public BaseballService(String userNumber, String computerNumber) {
+        this.userNumber = userNumber;
+        this.computerNumber = computerNumber;
+    }
+
+    public GameStateDto caculate() {
         GameStateDto gameStateDto = new GameStateDto();
-        calculateStrike(userNumber, nowComNumber, gameStateDto);
+        calculateStrike(userNumber, computerNumber, gameStateDto);
+        calculateBall(gameStateDto);
+        return gameStateDto;
 
+    }
 
+    private void calculateBall(
+        GameStateDto gameStateDto) {
+        for(int i=0; i<3; i++)
+        {
+            findOtherIndexSameNumberAndAddBall(gameStateDto, i);
+        }
+    }
+
+    private void findOtherIndexSameNumberAndAddBall(GameStateDto gameStateDto, int i) {
+        for (int j = 0; j < 3; j++) {
+            if (i != j && userNumber.charAt(i) == computerNumber.charAt(j))
+                gameStateDto.addBall();
+        }
     }
 
     private void calculateStrike(String userNumber, String nowComNumber,
