9f803c6b8beef1aa8630efbcab1aee024928738c
kim <hwiyul_g@daum.net>
2023-10-21 16:46:24 +0900

    !feat(UserNumber) : 사용자가 입력하는 값 String 변경 + 숫자인지 판별하는 함수 추가

--- src/main/java/baseball/UserNumber.java
+++ src/main/java/baseball/UserNumber.java
@@ -1,21 +1,29 @@
 package baseball;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserNumber {
     private static final int NUMBER_SIZE = 3;
-    private final List<Integer> userNumber;
+    private final List<Integer> userNumber = new ArrayList<>();
 
-    public UserNumber(List<Integer> userNumber) {
-        this.userNumber = userNumber;
+    public UserNumber(String userNumber) {
+        // this.userNumber = userNumber;
     }
 
-    private void validateLength(List<Integer> userInputNumber){
-        if(userInputNumber.size() != NUMBER_SIZE){
+    private void validateLength(String userInputNumber){
+        if(userInputNumber.length() != NUMBER_SIZE){
             throw new IllegalArgumentException();
         }
     }
 
+    private void validateDigit(String userInputNumber){
+        for(char ch : userInputNumber.toCharArray()){
+            if(!Character.isDigit(ch) || ch <'1' || ch >'9'){
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 
     public List<Integer> getUserNumber(){
         return userNumber;
