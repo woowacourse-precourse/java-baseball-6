5c838b497a82e38e1c331e9326fb6c7ab695d779
JHyun0302 <wogustkfkd11@gmail.com>
2023-10-21 15:08:22 +0900

    :recycle: [Refactor] 게임 재실행 or 종료 로직 리팩토링
    출력문을 PrintResult 클래스로 이동
    - 클래스의 역할과 책임 나누기

--- src/main/java/baseball/service/Restart.java
+++ src/main/java/baseball/service/Restart.java
@@ -13,7 +13,6 @@ public class Restart {
     }
 
     public int restartOrExit() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String restart = Console.readLine();
         int userInput = userValidation.restartValid(restart);
 
