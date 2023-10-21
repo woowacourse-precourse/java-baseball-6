019e2eb8b58d80db7ae7f298fff16a10525d963b
eeekeee <ekrtionl@gmail.com>
2023-10-21 21:51:11 +0900

    Feat: 같은 숫자 예외처리

--- src/main/java/validation/Validation.java
+++ src/main/java/validation/Validation.java
@@ -3,11 +3,14 @@ package validation;
 import static validation.Constant.*;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Validation {
     public static void inputValidationCheck(String inputString) {
         verifyLength(inputString);
         isNumericString(inputString);
+        hasDuplicateInteger(inputString);
     }
 
     private static void verifyLength(String input) {
@@ -19,8 +22,20 @@ public class Validation {
     private static void isNumericString(String input) {
         for (char c : input.toCharArray()) {
             if (!Character.isDigit(c)) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException(HAS_CHAR_INPUT);
             }
+        }
+    }
+
+    private static void hasDuplicateInteger(String input) {
+        Set<String> seen = new HashSet<>();
+
+        for (char c : input.toCharArray()) {
+            String currentChar = String.valueOf(c);
+            if (seen.contains(currentChar)) {
+                throw new IllegalArgumentException(DUPLICATED_STRING);
+            }
+            seen.add(currentChar);
         }
     }
 
@@ -28,7 +43,7 @@ public class Validation {
         int inputNumber = Integer.parseInt(Console.readLine());
 
         if (inputNumber > QUIT || inputNumber < RESTART) {
-            throw new IllegalArgumentException(WRONG_INPUT_STRING);
+            throw new IllegalArgumentException(WRONG_RESTART_INPUT);
         }
         return inputNumber;
     }
