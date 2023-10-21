b4bd52a0a02e27e71956fc5267bfc68131372d31
LDY <leedy3838@naver.com>
2023-10-21 17:42:50 +0900

    feat: warning correct

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,7 +11,7 @@ public class Application {
     public static void main(String[] args) {
 
         System.out.println("숫자 야구 게임을 시작합니다.");
-        while (true) {
+        do {
             List<Integer> randomNum = makeRandomNum();
 
             while (true) {
@@ -23,10 +23,7 @@ public class Application {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
-            if (!isContinue()) {
-                break;
-            }
-        }
+        } while (isContinue());
     }
 
     private static List<Integer> makeRandomNum() {
