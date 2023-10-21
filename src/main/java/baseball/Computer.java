a55d0be82f9e9a78d6e40a73340e9d6f49096f16
Moly <102847513+jminkkk@users.noreply.github.com>
2023-10-21 15:58:37 +0900

    feat: 3개 숫자를 선택하는 컴퓨터의 메서드 생성
    요구사항: 컴퓨터는 1에서 9까지 서로 다른 임의의 수 3개를 선택한다.

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -6,15 +6,40 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class Computer {
+    private static final Integer RANDOM_NUMBER_SIZE = 3;
     private static final Integer RANDOM_NUMBER_MIN_RANGE = 1;
     private static final Integer RANDOM_NUMBER_MAX_RANGE = 9;
     private List<Integer> RandomNumber;
 
-    public Computer(List<Integer> randomNumber) {
-        RandomNumber = randomNumber;
+    public Computer() {
+        RandomNumber = generateListOf3RandomNumber();
     }
 
     private int chooseRandomNumber() {
         return pickNumberInRange(RANDOM_NUMBER_MIN_RANGE, RANDOM_NUMBER_MAX_RANGE);
+    }
+
+    private List<Integer> generateListOf3RandomNumber() {
+        List<Integer> randomNumberList = new ArrayList<>();
+
+        for (int i = 0; i < RANDOM_NUMBER_SIZE; i++) {
+            int number = chooseRandomNumber();
+
+            if (!isDuplicatedNumber(randomNumberList, number)) {
+                i--;
+                continue;
+            }
+            randomNumberList.add(number);
+        }
+
+        return randomNumberList;
+    }
+
+    public List<Integer> loadRandomNumber() {
+        return RandomNumber;
+    }
+
+    private boolean isDuplicatedNumber(final List<Integer> randomNumberList, final int number) {
+        return randomNumberList.contains(number);
     }
 }
