86a842f22f5795d2400fc3b9504c88fedf54ba33
U-keun Song <sukeun319@gmail.com>
2023-10-21 15:26:29 +0900

    Feat : 어플리케이션 조립
    지금까지 만든 클래스를 이용해서 Game 클래스에서 게임이 진행될 수 있도록 matchStart 메서드를 작성했다.

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -2,7 +2,7 @@ package baseball;
 
 public class Game {
     private final Pitcher input = new Pitcher();
-    private Referee referee = Referee.enter();
+    private final Referee referee = Referee.enter();
     private Game() {}
 
     public static Game generate() { return new Game(); }
@@ -10,9 +10,16 @@ public class Game {
     public void start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
         do {
-            int[] numbers = input.pitch();
-
-
+            matchStart(0, 0, 0);
         } while (input.restart());
+    }
+
+    private void matchStart(int a, int b, int c) {
+        referee.playBall(a, b, c);
+        int[] pitch, judge;
+        do {
+            pitch = input.pitch();
+            judge = referee.judge(pitch);
+        } while (judge[1] != 3);
     }
 }
