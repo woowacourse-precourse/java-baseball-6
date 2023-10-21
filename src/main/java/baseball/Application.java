1b58b36f99f654cafb7ca539ca979ae377331382
육예진 <yngbao@yug-yejin-ui-MacBookAir.local>
2023-10-21 22:29:52 +0900

    feat:볼/스트라이크 각 개수에 따른 힌트 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -44,6 +44,22 @@ public class Application
     		}
     	}
     	
+    	if ((strike == 0) && (ball == 0)) 
+    	{
+    		System.out.println("낫싱");
+    	} 
+    	else if ((strike == 0) && (ball > 0))
+    	{
+    		System.out.println(ball+"볼");
+    	}
+    	else if ((strike > 0) && (ball == 0))
+    	{
+    		System.out.println(strike+"스트라이크");
+    	}
+    	else
+    	{
+    		System.out.println(ball+"볼 "+strike+"스트라이크");
+    	}
     }
 
 	public static void inputAllDistinct(String userInput) {
