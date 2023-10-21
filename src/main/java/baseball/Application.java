e60f354b4e128836e496b01476e78bec0bd1f2f7
daeun Kong <daeun113@ajou.ac.kr>
2023-10-22 00:11:53 +0900

    set up

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,16 @@
 package baseball;
 
+ //countStrike countBall printResult
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("hello world");
+        BaseballGame game = new BaseballGame();
+        game.play();
+    }
+}
+
+class BaseballGame {
+    void play() {
+        System.out.println("hello");
     }
 }
