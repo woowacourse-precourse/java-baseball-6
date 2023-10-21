0b990e90098dcbcaf1ac68a14b150c8264ba09ab
정재웅 <jweidon0821@gmail.com>
2023-10-21 23:19:42 +0900

    feat: 게임 시작 문구 출력

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,9 @@
+package baseball;
+
+public class Game {
+    private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+
+    public void startGame() {
+        System.out.println(START_MESSAGE);
+    }
+}
