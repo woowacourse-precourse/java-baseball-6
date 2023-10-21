e5ed8ce199b7048d9148a73f00f67cefb6a1811d
rizingblare <rizingblare@gmail.com>
2023-10-21 23:29:31 +0900

    refactor: separate console status
    - 콘솔의 상태 데이터를 console/util 패키지로 분리함

--- src/main/java/baseball/console/GameConsole.java
+++ src/main/java/baseball/console/GameConsole.java
@@ -2,12 +2,12 @@ package baseball.console;
 
 import baseball.console.game.GameList;
 import baseball.console.game.numberbaseball.NumberBaseballGame;
+import baseball.console.util.Status;
 import camp.nextstep.edu.missionutils.Console;
 
 public class GameConsole {
     NumberBaseballGame numberBaseballGame;
-    enum Mode {CONTINUE, EXIT}
-    Mode mode;
+    Status status;
     GameList target;
 
     public GameConsole() {
@@ -16,18 +16,18 @@ public class GameConsole {
 
     private void init() {
         target = GameList.NUMBER_BASEBALL;
-        mode = Mode.CONTINUE;
+        status = Status.CONTINUE;
     }
 
     public void start() {
-        while (mode == Mode.CONTINUE) {
+        while (status == Status.CONTINUE) {
             if (target == GameList.NUMBER_BASEBALL) {
                 System.out.println("숫자 야구 게임을 시작합니다.");
                 launchNumberBaseball();
             }
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             int choice = Integer.parseInt(Console.readLine());
-            mode = Mode.values()[choice-1];
+            status = Status.values()[choice];
         }
 
     }
---
+++ src/main/java/baseball/console/util/Status.java
@@ -0,0 +1,5 @@
+package baseball.console.util;
+
+public enum Status {
+    NONE, CONTINUE, EXIT
+}
