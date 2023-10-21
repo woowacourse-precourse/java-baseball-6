69b467e3f5a9b002ba84606c056cdc67e14baf29
박찬건 <jg9870@naver.com>
2023-10-21 18:17:17 +0900

    클래스 분리, 야구 게임 진행 로직 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,25 +1,40 @@
 package baseball;
 
+import baseball.number.NumberMatcher;
+import baseball.number.NumberProducer;
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        Set<Integer> computerSet = new HashSet<>();
-        while (computerSet.size() < 3) {
-            computerSet.add(Randoms.pickNumberInRange(1, 9));
-        }
-        List<Integer> computer = new ArrayList<>(computerSet);
+        int reGame = 1;
+        List<Integer> computer = new ArrayList<>();
 
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        System.out.print("숫자를 입력해주세요 : ");
-        String readNumber = Console.readLine();
-        Integer.parseInt(readNumber);
+        try {
+            System.out.println("숫자 야구 게임을 시작합니다.");
+            while (reGame != 2) {
+                if (reGame == 1) {
+                    computer = NumberProducer.makeNumberInRange(1, 9, 3);
+                    reGame = 0; // reGame 초기화
+                    // test
+                    System.out.println("computer = " + computer);
+                }
+                System.out.print("숫자를 입력해주세요 : ");
+                String result = NumberMatcher.numberMatcherResult(computer);
+                // todo 결과를 어떤 식으로 줘서 판별할지 고민
+                if (result.equals("3스트라이크")) {
+                    System.out.println(result);
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                    reGame = Integer.parseInt(Console.readLine());
+                } else {
+                    System.out.println(result);
+                }
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("잘못된 입력입니다. 1부터 9 사이의 숫자만 입력할 수 있습니다.");
+        }
     }
 }
