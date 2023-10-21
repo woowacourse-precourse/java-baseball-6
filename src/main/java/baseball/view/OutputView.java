556a9819119f35f7c32bf4f06e85c391e9c64af4
shAn-kor <kjn0406@naver.com>
2023-10-21 17:40:09 +0900

    printCelebrate 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -26,4 +26,9 @@ public class OutputView {
             System.out.println(ball + "볼 " + strike + "스트라이크");
         }
     }
+
+    public static void printCelebrate() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
