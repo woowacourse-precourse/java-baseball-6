c481a063e25230d50cd61838c1541e9d3e886c02
JuSeong1130 <jsmoon1130@naver.com>
2023-10-21 19:33:19 +0900

    feat(Ball) : Ball 생성 기능 구현

---
+++ src/main/java/baseball/domain/Ball.java
@@ -0,0 +1,12 @@
+package baseball.domain;
+
+public class Ball {
+
+    private Number number;
+    private int position;
+
+    public Ball(Number number, int position) {
+        this.number = number;
+        this.position = position;
+    }
+}
