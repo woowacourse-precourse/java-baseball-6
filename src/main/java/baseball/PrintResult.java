21e9e026969ed396adf65132ea26d79172d5345e
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2023-10-21 11:58:48 +0900

    Refactor: move 3Strike printing to Application.java

--- src/main/java/baseball/PrintResult.java
+++ src/main/java/baseball/PrintResult.java
@@ -12,16 +12,13 @@ public class PrintResult {
         if(strike == 0 && ball == 0){
             return "낫싱";
         }
-        else if (strike != 0 && strike < 3 && ball == 0) {
-            return strike + " 스트라이크";
-        }
-        else if (strike == 3) {
-            return strike + " 스트라이크 \n3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+        else if (strike != 0 && ball == 0) {
+            return strike + "스트라이크";
         }
         else if (strike == 0 && ball != 0) {
-            return ball + " 볼";
+            return ball + "볼";
         }
 
-        return strike + " 스트라이크 " + ball + " 볼";
+        return ball + "볼 "+ strike + "스트라이크";
     }
 }
