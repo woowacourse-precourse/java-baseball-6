56771581dd1c4110604cc022b73bc4b78903f072
howudong <tjdvy963@naver.com>
2023-10-21 14:42:09 +0900

    refactor : Controller에 service 의존성 주입 추가

--- src/main/java/baseball/controller/Controller.java
+++ src/main/java/baseball/controller/Controller.java
@@ -6,10 +6,12 @@ import baseball.view.OutputView;
 public final class Controller {
     private final InputView inputView;
     private final OutputView outputView;
+    private final Service service;
 
-    public Controller(InputView inputView, OutputView outputView) {
+    public Controller(InputView inputView, OutputView outputView, Service service) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.service = service;
     }
 
     public void run() {
