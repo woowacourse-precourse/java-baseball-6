a2229f85eb067a290ec68351dab7a4df19b4c6cf
tangba484 <tangba484@naver.com>
2023-10-22 00:47:58 +0900

    사용자 입력의 유효성 검증기능 구현

--- src/main/java/baseball/service/NumberService.java
+++ src/main/java/baseball/service/NumberService.java
@@ -2,6 +2,7 @@ package baseball.service;
 
 import baseball.model.Number;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashSet;
 
 public class NumberService {
 
@@ -14,5 +15,34 @@ public class NumberService {
             }
         }
         return computer;
+    }
+
+    public Number toNumber(String userInput){
+        Number user = new Number();
+        if (validateInput(userInput)) {
+            userInput.chars()
+                    .map(Character::getNumericValue).forEach(user::add);
+            return user;
+        } else{
+            throw new IllegalArgumentException("서로 다른 정수 3개를 입력해주세요 1<= N <= 9");
+        }
+    }
+
+    public boolean validateInput(String userInput){
+        HashSet<Character> set = new HashSet<>();
+
+        if (userInput.length() != Number.getGameSize()) {
+            return false;
+        }
+
+        for (int i=0;i<userInput.length();i++) {
+            char ch = userInput.charAt(i);
+            if (ch < '1' || ch > '9') {
+                return false;
+            }
+            set.add(ch);
+        }
+
+        return set.size() == Number.getGameSize();
     }
 }
