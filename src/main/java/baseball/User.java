9823898190c2edfbec51030f865c333bb2820dda
junest66 <chlwndks333@gmail.com>
2023-10-21 18:06:15 +0900

    refactor: 예외 메세지 추가

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -14,16 +14,16 @@ public class User {
 
     public void validateNumbersFromUser(String input) {
         if (input.length() != 3) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("입력한 숫자의 길이가 올바르지 않습니다.");
         }
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("유효한 숫자를 입력하세요.");
         }
         if (input.charAt(0) == input.charAt(1) || input.charAt(0) == input.charAt(2) || input.charAt(1) == input.charAt(
                 2)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("중복된 숫자가 있습니다.");
         }
     }
 
@@ -40,7 +40,7 @@ public class User {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String input = Console.readLine();
         if (!(input.equals("1") || input.equals("2"))) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("1 또는 2를 입력하세요.");
         }
         if (input.equals("2")) {
             return false;
