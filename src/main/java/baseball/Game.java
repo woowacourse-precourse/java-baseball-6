9c0fcad70c3eaf8d745d5231fb65678872bfba44
Sully <2dcoder@naver.com>
2023-10-21 22:34:52 +0900

    feat: 숫자 야구 게임 로직 구현
    테스트도 정상적으로 전부 통과한다.
    gameStatus를 이용하여 "1"이라는 재시작 명령어라면 계속 반복하게끔 만들었다.
    strikeCount와 ballCount의 값이 몇이냐에 따라 원하는 값을 출력하게끔 한다.
    TODO: 한번에 너무 많은 것들을 처리하고 일단 테스트 코드만 돌아가게끔 하게 한 코드이기 때문에 클래스와 메서드를 적절히 분리해야 한다.

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,5 +1,66 @@
 package baseball;
 
+import static baseball.validator.NumberValidator.THREE_LENGTH;
+
+import baseball.computer.RandomComputerNumberGenerator;
+import baseball.output.GameOutput;
+import baseball.user.UserInput;
+import java.util.Objects;
+
 public class Game {
+
+    public static void start() {
+        GameOutput.printlnStartMessage();
+        String gameStatus = "1";
+
+        String computerNumberString = new RandomComputerNumberGenerator().toString();
+
+        while (Objects.equals(gameStatus, "1")) {
+            UserInput userInput = new UserInput();
+            String userNumberString = userInput.inputNumberString();
+
+            int strikeCount = 0;
+            int ballCount = 0;
+            for (int i = 0; i < THREE_LENGTH; i++) {
+                if (userNumberString.charAt(i) == computerNumberString.charAt(i)) {
+                    strikeCount += 1;
+                    continue;
+                }
+
+                if (computerNumberString.contains(Character.toString(userNumberString.charAt(i)))) {
+                    ballCount += 1;
+                }
+            }
+
+            if (strikeCount == 0 && ballCount == 0) {
+                GameOutput.printlnNothing();
+                continue;
+            }
+
+            if (strikeCount == 3) {
+                System.out.println(strikeCount + GameOutput.STRIKE);
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                gameStatus = userInput.inputOneOrTwo();
+                if (Objects.equals(gameStatus, "1")) {
+                    computerNumberString = new RandomComputerNumberGenerator().toString();
+                    continue;
+                }
+
+                System.out.println("게임 종료");
+                break;
+            }
+
+            if (ballCount > 0) {
+                System.out.print(ballCount + GameOutput.BALL + " ");
+            }
+
+            if (strikeCount > 0) {
+                System.out.print(strikeCount + GameOutput.STRIKE);
+            }
+
+            System.out.println();
+        }
+    }
 
 }
