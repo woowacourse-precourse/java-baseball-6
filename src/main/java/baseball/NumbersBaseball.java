3f0f3342b248ebde74a257c0851c1468f018fa88
alscjf1329 <alscjf1329@gmail.com>
2023-10-21 16:20:15 +0900

    Feat) play메소드 생성

--- src/main/java/baseball/NumbersBaseball.java
+++ src/main/java/baseball/NumbersBaseball.java
@@ -11,5 +11,72 @@ public class NumbersBaseball {
 
     public void start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+        play();
+    }
+
+    private void play() {
+        BaseballInfo baseballInfo = new BaseballInfo(0, 0);
+        generateAnswer();
+        while (!baseballInfo.isThreeStrike()) {
+            System.out.print("숫자를 입력해주세요 : ");
+            String input = Console.readLine();
+            validateInput(input);
+            baseballInfo = getCompareResultWith(convertToIntArr(input));
+            if (baseballInfo.isThreeStrike()) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                //finish();
+                return;
+            }
+            System.out.println(baseballInfo);
+        }
+    }
+
+    private int[] convertToIntArr(String input) {
+        int[] inputArr = new int[3];
+        for (int i = 0; i < 3; i++) {
+            inputArr[i] = Integer.parseInt(input.charAt(i) + "");
+        }
+        return inputArr;
+    }
+
+    private boolean contains(int[] input, int num) {
+        for (int i = 0; i < 3; i++) {
+            if (input[i] == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private BaseballInfo getCompareResultWith(int[] input) {
+        BaseballInfo baseballInfo = new BaseballInfo(0, 0);
+        for (int i = 0; i < 3; i++) {
+            if (answer[i] == input[i]) {
+                baseballInfo.addStrike();
+            }
+            if (!(answer[i] == input[i]) && contains(answer, input[i])) {
+                baseballInfo.addball();
+            }
+        }
+        return baseballInfo;
+    }
+
+    private void generateAnswer() {
+        for (int i = 0; i < 3; i++) {
+            answer[i] = Randoms.pickNumberInRange(1, 9);
+        }
+    }
+
+    private boolean validateInput(String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("입력값은 3자리여야 합니다.");
+        }
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("입력값은 숫자여야 합니다.");
+        }
+        return true;
     }
 }
