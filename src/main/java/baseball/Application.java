3a9a0634a0c3179c4141205614bffd3427437bc3
lsj5657 <lsj56575@naver.com>
2023-10-21 19:01:28 +0900

    3스트라이크 시에 출력하는 기능 구현 기능 구현 목록에 게임 종료 시 입력받는 수에 대한 예외처리 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -69,6 +69,11 @@ public class Application {
                 System.out.println();
             }
 
+            if(strike==3){
+                System.out.println("3스트라이크");
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            }
         }
 
     }
