e69d04f0e2a008893a5b018aff9ee92438f3326f
정성욱 <rkfka7849@gmail.com>
2023-10-21 22:03:09 +0900

    refactor(BalNumber): 생성자 단순화

--- src/main/java/baseball/domain/BallNumber.java
+++ src/main/java/baseball/domain/BallNumber.java
@@ -1,16 +1,18 @@
 package baseball.domain;
 
+import static java.util.stream.Collectors.toList;
+
 import java.util.ArrayList;
 
 public class BallNumber {
     public ArrayList<Integer> ballNumber = new ArrayList<>();
 
     public BallNumber(String newBallNumber) {
-        ballNumber = newBallNumber
+        ballNumber = (ArrayList<Integer>) newBallNumber
                 .chars()
                 .map(Character::getNumericValue)
                 .boxed()
-                .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+                .collect(toList());
     }
 
     public BallNumber(ArrayList<Integer> newBallNumber) {
