a5b561d28b68c323d9bb4894174ecb0f4eaa466d
jae woo Jung <awfjol2008@naver.com>
2023-10-22 00:35:55 +0900

    feat: GameController 클래스 의존성 주입 로직 구현
    NumberService와 ViewService의 의존성을 주입받는다.

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,4 +1,15 @@
 package baseball.controller;
 
+import baseball.application.NumberService;
+import baseball.view.ViewService;
+
 public class GameController {
+
+    private final NumberService numberService;
+    private final ViewService viewService;
+
+    public GameController(NumberService numberService, ViewService viewService) {
+        this.numberService = numberService;
+        this.viewService = viewService;
+    }
 }
