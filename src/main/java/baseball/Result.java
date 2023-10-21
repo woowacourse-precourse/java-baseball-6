47715f5937d7edc52eb6a6b6d5954c8e29e056d7
yubin <dbqls2821@gmail.com>
2023-10-21 21:20:00 +0900

    feat: Result객체에서 볼, 스트라이크 결과를 반환하는 메서드 추가

--- src/main/java/baseball/Result.java
+++ src/main/java/baseball/Result.java
@@ -6,4 +6,13 @@ record Result(int ballCount, int strikeCount) {
         return strikeCount == Size.THREE.num;
     }
 
+    String getResultInKorean() {
+        if(ballCount > 0 && strikeCount > 0)
+            return ballCount + OutputMessage.BALL.message + " " + strikeCount + OutputMessage.STRIKE.message;
+        else if (ballCount > 0)
+            return ballCount + OutputMessage.BALL.message;
+        else if (strikeCount > 0)
+            return strikeCount + OutputMessage.STRIKE.message;
+        else return OutputMessage.NOTHING.message;
+    }
 }
