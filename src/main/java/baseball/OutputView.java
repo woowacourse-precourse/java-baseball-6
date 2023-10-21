1be04a73052bb84d9e122461670cf9748abdf703
Jiyoon Kim <rlawldbs0911@naver.com>
2023-10-21 19:29:18 +0900

    feat(ProcessMessage): 게임 진행 메시지 enum 및 출력 메서드 구현

--- src/main/java/baseball/OutputView.java
+++ src/main/java/baseball/OutputView.java
@@ -1,18 +1,34 @@
 package baseball;
 
+import java.util.List;
+import java.util.Map;
+
 public class OutputView {
+    private static final String BLANK = " ";
+
     public void printStartMessage() {
-        System.out.println(ProcessMessage.START_MESSAGE);
-    public void printResult(GameResult gameResult) {
-        if (gameResult.hasThreeStrike()) {
-            printThreeStrikeResult();
-            return;
-        }
-        printBallAndStrikeResult(gameResult);
+        System.out.println(ProcessMessage.START);
     }
 
-    private void printThreeStrikeResult() {
+    public void newLine() {
+        System.out.println();
+    }
+
+    public void printResult(GameResult gameResult) {
+        if (gameResult.hasThreeStrike()) {
+            printThreeStrikeResult(gameResult.getStrikeResult());
+            return;
+        }
+        printBallAndStrikeResult(gameResult.getBallAndStrikeResult());
+    }
+
+    private void printThreeStrikeResult(Integer result) {
+        System.out.println(result + CompareStatus.STRIKE.toString());
         System.out.println(ProcessMessage.THREE_STRIKE);
     }
 
+    public void printBallAndStrikeResult(List<String> results) {
+        results.forEach(s -> System.out.print(s + BLANK));
+        newLine();
+    }
 }
