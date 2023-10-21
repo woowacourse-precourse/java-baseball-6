d6fbb7c6d8db290e03d5d6bd612142f582a66d42
YongHyeon <pyh4616@naver.com>
2023-10-21 19:00:00 +0900

    Feat: ComputerNumber, 컴퓨터 숫자가 3자리인지 검사하는 기능 구현

--- src/main/java/baseball/model/ComputerNumber.java
+++ src/main/java/baseball/model/ComputerNumber.java
@@ -7,11 +7,13 @@ public class ComputerNumber {
     private final List<Integer> computerNumber;
 
     public ComputerNumber(List<Integer> computerNumber) {
-        validate();
+        validateSize(computerNumber);
         this.computerNumber = computerNumber;
     }
 
-    private void validate() {
-
+    private void validateSize(List<Integer> computerNumber) {
+        if (computerNumber.size() != 3) {
+            throw new IllegalArgumentException();
+        }
     }
 }
