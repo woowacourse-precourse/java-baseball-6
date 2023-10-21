46731e76646c55e0fff0391688f19f8fb7204d4a
chosunghyun18 <krismon@naver.com>
2023-10-21 18:35:48 +0900

    feat : add InputController - view and validate input data

--- src/main/java/baseball/controller/InputController.java
+++ src/main/java/baseball/controller/InputController.java
@@ -1,2 +1,30 @@
-package baseball.controller;public class InputController {
+package baseball.controller;
+
+import baseball.validate.GameValidate;
+import baseball.validate.InputValidate;
+import baseball.view.InputView;
+
+public class InputController {
+    private final InputView inputView;
+    private final InputValidate inputValidate;
+    private final GameValidate gameValidate;
+
+    public InputController() {
+        this.inputView = new InputView();
+        this.inputValidate = new InputValidate();
+        this.gameValidate = new GameValidate();
+    }
+
+    public boolean isRestartGame() {
+        String input  = inputView.getPlayerRestart();
+        int restartNumber = inputValidate.checkNumber(input);
+        gameValidate.checkGameRestartInput(restartNumber);
+        return restartNumber == 1;
+    }
+
+    public String getPlayerGuessNumber() {
+        String input = inputView.getPlayerGuessNumbers();
+        inputValidate.checkNumbers(input);
+        return input;
+    }
 }
