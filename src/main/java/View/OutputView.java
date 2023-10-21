85544d9ea487d29f711f972522b533abf0ca03a3
김영준 <rladudwns164@gmail.com>
2023-10-21 23:20:43 +0900

    feat : 게임 결과 출력 기능 추가
    -볼과 스트라이크 갯수에 따라 맞는 결과 기능 추가
    -스트라이크가 게임종료 조건에 부합하면 true 리턴
    -enum클래스 낫싱,스트라이크,볼 문자열 추가
    -enum클래스 게임 종료 넘버 추가

--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -1,5 +1,7 @@
 package View;
 
+import java.util.Map;
+
 public class OutputView {
 
     private static final OutputView INSTANCE = new OutputView();
@@ -15,17 +17,60 @@ public class OutputView {
         System.out.println(Message.GAME_START.getMessage());
     }
 
+    public boolean printGamerResult(Map<String, Integer> comparisonResult) {
+        int balls = comparisonResult.get("BALL");
+        int strikes = comparisonResult.get("STRIKE");
+
+        if (balls == 0 && strikes == 0) {
+            System.out.println(Message.NOTHING.getMessage());
+            return false;
+        }
+        if (balls == 0) {
+            System.out.println(strikes + Message.STRIKE.getMessage());
+
+            if (strikes == Message.GAME_END.getEndCount()) {
+                return true;
+            }
+
+            return false;
+        }
+        if (strikes == 0) {
+            System.out.println(balls + Message.BALL.getMessage());
+            return false;
+        }
+        System.out.println(
+                balls + Message.BALL.getMessage() +" " +strikes + Message.STRIKE.getMessage());
+        return false;
+
+    }
+
+
     private enum Message {
-        GAME_START("숫자 야구 게임을 시작합니다");
+        GAME_START("숫자 야구 게임을 시작합니다"),
+        NOTHING("낫싱"),
+        BALL("볼"),
+        STRIKE("스트라이크"),
+        GAME_END(3);
 
         private String message;
+        private int strikeEndCount;
 
         Message(String message) {
             this.message = message;
         }
 
+        Message(int strikeEndCount) {
+            this.strikeEndCount = strikeEndCount;
+        }
+
         public String getMessage() {
             return message;
         }
+
+        public int getEndCount() {
+            return strikeEndCount;
+        }
     }
+
+
 }
