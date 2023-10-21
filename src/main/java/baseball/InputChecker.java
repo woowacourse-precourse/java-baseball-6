33ea7359a3b85d4c0e492fd15cd95e67bf850722
enduf7686 <enduf768640@gmail.com>
2023-10-21 17:11:34 +0900

    예외 설명 추가

--- src/main/java/baseball/InputChecker.java
+++ src/main/java/baseball/InputChecker.java
@@ -38,34 +38,34 @@ public class InputChecker {
 
     public void checkMyNumber(String input) {
         if (input.length() != MY_NUMBER_INPUT_LENGTH) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("3개의 다른 수를 입력하세요.");
         }
 
         if (Arrays.stream(input.split("")).collect(Collectors.toSet()).size() != 3) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("숫자를 입력해주세요.");
         }
 
         for (char c : input.toCharArray()) {
             if (c < ASCII_CODE_ONE || c > ASCII_CODE_NINE) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("1부터 9사이의 숫자를 입력해주세요.");
             }
         }
     }
 
     public void checkRestartNumber(String input) {
         if (input.length() != RESTART_OR_END_INPUT_LENGTH) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("1 또는 2를 입력해주세요.");
         }
 
         int num;
         try {
             num = Integer.valueOf(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("숫자를 입력해주세요.");
         }
 
         if (num != RESTART_NUMBER && num != END_NUMBER) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("1 또는 2를 입력해주세요.");
         }
     }
 }
