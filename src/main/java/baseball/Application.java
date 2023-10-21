a0639e5eab1ed803adae87644889b014bc980069
kitewater <97283971+kitewater@users.noreply.github.com>
2023-10-22 04:00:30 +0900

    Feat: 랜덤 숫자생성, 입력, 판정 기능 추가
    중복 상황에 편하게 대처하기 위하여 Set을 사용함.
    1. 다만 테스트케이스 진행 시. 게임이 종료되지 않았는데도 불구하고 1이 입력되어 오류가 발생하는 경우가 존재한다. 이 부분에 대한 오류 수정이 필요하고
    2. 출력 부분 리팩토링 필요
    3. mvc 패턴을 공부하여 적용해 볼 필요가 있다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,89 @@
 package baseball;
 
+import java.util.*;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<Integer> randomnumber = new ArrayList<>();
+        List<Integer> inputnumber = new ArrayList<>();
+        int[] count = new int[2]; // 첫번째는 스트라이크 카운트, 두번째는 볼카운트
+        int quit = 0;
+
+        randomnumber = makecomputernumber();
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while(quit != 2){
+            System.out.printf("숫자를 입력해주세요 : ");
+            inputnumber = saveinput(Integer.parseInt(Console.readLine()));
+            count = judgecount(inputnumber, randomnumber);
+            if(count[0] == 3){
+                System.out.println("3스트라이크");
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                quit = Integer.parseInt(Console.readLine());
+                if (quit == 1){
+                    randomnumber = makecomputernumber();
+                }
+            }
+            else if(count[0]>0 && count[1] == 0){
+                System.out.printf("%d스트라이크\n", count[0]);
+            }
+            else if(count[0] == 0 && count[1] > 0){
+                System.out.printf("%d볼\n", count[1]);
+            }
+            else if(count[0] > 0 && count[1] > 0){
+                System.out.printf("%d볼 %d스트라이크\n", count[1], count[0]);
+            }
+            else if(count[0] == 0 && count[1]  == 0){
+                System.out.printf("낫싱\n");
+            }
+        }
+        return;
+    }
+    public static List<Integer> makecomputernumber(){
+        HashSet<Integer> set = new HashSet<Integer>();
+        List<Integer> numbers = new ArrayList<>();
+        int i = 0;
+        while(set.size()<3){
+            set.add(Randoms.pickNumberInRange(1,9));
+        }
+        Iterator<Integer> iter = set.iterator();
+        while (iter.hasNext()){
+            numbers.add(iter.next());
+        }
+        return numbers;
+    }
+
+    public static List<Integer> saveinput(int input){
+        LinkedHashSet<Integer> set = new LinkedHashSet<Integer>();
+        List<Integer> inputnumber = new ArrayList<>();
+        while(input >0){
+            set.add(input%10);
+            input = input /10;
+        }
+        if(set.size() != 3){
+            throw new IllegalArgumentException();
+        }
+        Iterator<Integer> iter = set.iterator();
+        while (iter.hasNext()){
+            inputnumber.add(iter.next());
+        }
+        return inputnumber;
+    }
+    public static int[] judgecount(List<Integer> input, List<Integer> computer){
+        int[] count = {0, 0}; // 첫번째는 스트라이크 카운트, 두번째는 볼카운트
+        for(int i=0; i<3;i++){
+            if(input.get(i) == computer.get(i)){
+                count[0] ++;
+            }
+            else if(computer.contains(input.get(i))){
+                count[1] ++;
+            }
+        }
+        return count;
     }
 }
