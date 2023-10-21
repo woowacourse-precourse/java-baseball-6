5cd0a072b6ceab5c3e1073f986969eab6329e40b
username <57055730+Jake-huen@users.noreply.github.com>
2023-10-21 17:46:42 +0900

    feat: 야구 게임 매니저로 게임 순환 구현
    BaseballGameManager
    - playBaseballGame()
    - menuChoice()

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,17 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BaseballGameManger baseballGameManger = new BaseballGameManger();
+        baseballGameManger.playBaseballGame();
+
+        while (true) {
+            int choice = baseballGameManger.menuChoice();
+            if (choice == 1) {
+                baseballGameManger.playBaseballGame();
+            } else if (choice == 2) {
+                System.out.println("게임을 종료합니다. 감사합니다!");
+                break;
+            }
+        }
     }
 }
