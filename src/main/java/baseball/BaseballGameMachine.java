7d7dbfcd12f22e77b8ba5a49989c31cb1ae2a0b9
watermelon3737 <pshrina1@naver.com>
2023-10-21 19:16:17 +0900

    [refactor] : 에러 메시지 수정

--- src/main/java/baseball/BaseballGameMachine.java
+++ src/main/java/baseball/BaseballGameMachine.java
@@ -5,30 +5,23 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class BaseballGameMachine {
-    public Message message = new Message();
+    private Message message = new Message();
+    private NumberGenerator numberGenerator = new NumberGenerator();
+
+    private InputValidation inputValidation = new InputValidation();
+
     public String input() {
         return Console.readLine();
+    }
+    private void display(String message) {
+        System.out.print(message);
     }
 
     public void play() {
         boolean gameEnd = false;
         while (!gameEnd) {
-            NumberGenerator generator = new NumberGenerator();
             display(message.start() + '\n');
-            Referee referee = new Referee(generator.generateRandomAnswerList());
-//            System.out.println(referee.getAnswerList());
-            InputValidation inputValidation = new InputValidation();
-            boolean success = false;
-            while (!success) {
-                display(message.requestInput());
-                List<Integer> userInput = inputValidation.validateUserInput(input());
-                List<Integer> result = referee.judgeUserInput(userInput);
-                display(message.result(result) + '\n');
-                if (result != null && result.get(0) == 0 && result.get(1) == 3) {
-                    display(message.success());
-                    success = true;
-                }
-            }
+            gameProcess();
             display(message.requestRetryOrEnd() + '\n');
             int newOrEnd = newGameOrEnd(input());
             if (newOrEnd == 2) {
@@ -37,12 +30,24 @@ public class BaseballGameMachine {
         }
     }
 
-    private void display(String message) {
-        System.out.print(message);
+
+    public void gameProcess() {
+        Referee referee = new Referee(numberGenerator.generateRandomAnswerList());
+        System.out.println(referee.getAnswerList());
+        boolean success = false;
+        while (!success) {
+            display(message.requestInput());
+            List<Integer> userInput = inputValidation.validateUserInput(input());
+            List<Integer> result = referee.judgeUserInput(userInput);
+            display(message.result(result) + '\n');
+            if (result != null && result.get(0) == 0 && result.get(1) == 3) {
+                display(message.success());
+                success = true;
+            }
+        }
     }
 
     public int newGameOrEnd(String input) {
-        InputValidation inputValidation = new InputValidation();
         return inputValidation.validateNewGameRequest(input);
     }
 }
