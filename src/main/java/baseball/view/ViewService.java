9c2f5d4a65aa40a67b33a142354e061e035f7eba
jae woo Jung <awfjol2008@naver.com>
2023-10-22 00:27:50 +0900

    feat: 게임 종료 메시지를 출력하는 메소드 구현

--- src/main/java/baseball/view/ViewService.java
+++ src/main/java/baseball/view/ViewService.java
@@ -12,6 +12,11 @@ public class ViewService {
         return readLine();
     }
 
+    private void printGameEndMessage() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        printRetryMessage();
+    }
+
     public void printRetryMessage() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
