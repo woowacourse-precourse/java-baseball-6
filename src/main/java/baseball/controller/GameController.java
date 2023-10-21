d077c6146a5d1943473b538498e6339681cc5c96
seonghoo1217 <seonghoo1217@naver.com>
2023-10-21 23:06:14 +0900

    refactor : modify variable name for fit purpose

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -11,12 +11,12 @@ import java.util.List;
 public class GameController {
 
     public static void playingBaseballGame(GameCondition game_coin){
-        List<Integer> computer_numbers = GenerateNumberList.generateRandomNumberToComputer(3);
+        List<Integer> computer = GenerateNumberList.generateRandomNumberToComputer(3);
         while (game_coin.equals(GameCondition.CONTINUE)){
             OutputView.outputForGameValueInput();
             String user_input_value = InputView.readLineByConsole();
             if (GameValidation.verifyForGameValue(user_input_value)){
-                List<Integer> integers = GenerateNumberList.generateInputValueToUser(user_input_value);
+                List<Integer> user = GenerateNumberList.generateInputValueToUser(user_input_value);
 
             }
         }
