1b6560cc1302193d558d47d3aac2aedd8482356a
pjm2476 <pjm2476@gmail.com>
2023-10-21 20:53:08 +0900

    feat: 컴퓨터가 3자리의 난수 생성

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,24 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Computer {
+    private static final int NUMBER_LEN = 3;
+    private static final int MIN_VAL = 1;
+    private static final int MAX_VAL = 9;
+    private int[] answer = new int[NUMBER_LEN];
+
+    public Computer() {
+        boolean[] isUsed = new boolean[MAX_VAL];
+
+        for (int i = 0; i < NUMBER_LEN; ) {
+            int tmp = Randoms.pickNumberInRange(MIN_VAL, MAX_VAL);
+            if (isUsed[tmp] != true) {
+                isUsed[tmp] = true;
+                answer[i++] = tmp;
+            }
+        }
+    }
+
+
+}
