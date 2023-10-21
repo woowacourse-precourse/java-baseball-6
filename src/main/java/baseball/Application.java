c846e6bcfcbf0f225ca13c1d2887e3888a80cbb9
Kim Daehyeon <daehyeon3351@gmail.com>
2023-10-21 20:14:04 +0900

    feat: 랜덤 숫자를 생성하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO: 사용자에게 숫자를 입력받고, 적절하지 않은 입력이면 IllegalArgumentException을 발생기키는 기능 구현
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
     }
 }
