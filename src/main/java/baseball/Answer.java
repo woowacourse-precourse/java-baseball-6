39e91fa97ba752d3cf8ab12a0f9881bd16fb5700
DoohyunHwang97 <ghkdengus97@gmail.com>
2023-10-21 20:16:22 +0900

    feat(Answer): add data class Answer

---
+++ src/main/java/baseball/Answer.java
@@ -0,0 +1,19 @@
+package baseball;
+
+public class Answer {
+    private int firstNumber;
+    private int secondNumber;
+    private int thirdNumber;
+
+    public int getFirstNumber() {
+        return firstNumber;
+    }
+
+    public int getSecondNumber() {
+        return secondNumber;
+    }
+
+    public int getThirdNumber() {
+        return thirdNumber;
+    }
+}
