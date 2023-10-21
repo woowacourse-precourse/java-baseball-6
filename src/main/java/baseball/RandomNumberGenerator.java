c86f9805442ddb6dd7803f605dd9761fe8fd57ee
dokkisan <dokkisan.wang@gmail.com>
2023-10-22 01:24:14 +0900

    feat: 1~9까지 랜덤한 3자리 수 생성 기능 구현

---
+++ src/main/java/baseball/RandomNumberGenerator.java
@@ -0,0 +1,19 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class RandomNumberGenerator {
+    public static int MIN_RANDOM_NUMBER = 1;
+    public static int MAX_RANDOM_NUMBER = 9;
+    public static int THREE_DIGIT_NUMBER_RANGE = 3;
+
+    public List<Integer> generateRandomNumbers() {
+        return generateUniqueThreeDigitNumbers();
+    }
+
+    private List<Integer> generateUniqueThreeDigitNumbers() {
+        return Randoms.pickUniqueNumbersInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER, THREE_DIGIT_NUMBER_RANGE);
+    }
+}
