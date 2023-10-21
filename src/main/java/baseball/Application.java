133afed4861f0e0c5126a046de46276536c204e4
m5s3 <devmyoungsoo2@gmail.com>
2023-10-21 14:07:30 +0900

    feat: 서로다른 숫자를 체크하는 로직 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,8 +3,9 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Scanner;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 public class Application {
@@ -22,11 +23,9 @@ public class Application {
             while (flag) {
                 System.out.println("숫자 야구 게임을 시작합니다.");
                 String number = requestNumber();
-                isThreeDigit(number);
                 List<Integer> computerNumbers = generateComputerNumbers();
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-
                 flag = isRestartGame();
             }
         } catch (IllegalArgumentException e) {
@@ -39,13 +38,38 @@ public class Application {
     private static String requestNumber() {
         System.out.print("숫자를 입력해 주세요 : ");
         String input = Console.readLine();
+        validateInput(input);
         return input;
+    }
+
+    private static void validateInput(String input) {
+        isThreeDigit(input);
+        isUniqueNumbers(input);
     }
 
     private static void isThreeDigit(String input) {
         if (!Pattern.matches("^\\d{3}$", input)) {
             throw new IllegalArgumentException("입력값이 잘못 되었습니다.");
         }
+    }
+
+    private static void isUniqueNumbers(String input) {
+        Set<Character> numbers = new HashSet<>();
+        for (char number : input.toCharArray()) {
+            numbers.add(number);
+        }
+
+        if (numbers.size() != 3) {
+            throw new IllegalArgumentException("입력값이 잘못 되었습니다.");
+        }
+    }
+
+    private static List<Integer> convertStringToList(String input) {
+        List<Integer> list = new ArrayList<>();
+        for (char number : input.toCharArray()) {
+            list.add((int) number);
+        }
+        return list;
     }
 
     private static List<Integer> generateComputerNumbers() {
