bd837e517d1fb0d3af896ab53a7df565be389f29
xogns4909 <xogns4909@naver.com>
2023-10-21 19:50:13 +0900

    Refactor : printResult 메소드 매개변수 변경

--- src/main/java/io/OutPut.java
+++ src/main/java/io/OutPut.java
@@ -1,12 +1,14 @@
 package io;
 
+import domain.GameResult;
+
 public interface OutPut {
 
     void printStartGame();
 
     void printEnterNumberMsg();
 
-    void printResult(String string);
+    void printResult(GameResult gameResult);
 
     void printEndMessage();
 
