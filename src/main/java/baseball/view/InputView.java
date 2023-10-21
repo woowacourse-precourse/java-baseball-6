1ed095695c0f9d78689f56d330c200e78c9aef84
jeongjihun <wlgns2234@naver.com>
2023-10-21 18:32:46 +0900

    feat: 사용자로부터 재시작 여부를 입력받는 기능 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -11,6 +11,8 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class InputView {
 
     private static final String INPUT_MESSAGE = "숫자를 입력해 주세요 : ";
+    private static final String END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n";
+    private static final String RESTART_OR_END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
 
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 9;
@@ -36,6 +38,12 @@ public class InputView {
         if (digit < MIN_NUMBER || MAX_NUMBER < digit) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public static int RestartOrEnd() {
+        System.out.println(END_MESSAGE);
+        System.out.println(RESTART_OR_END_MESSAGE);
+        return Integer.parseInt(readLine());
     }
 
 }
