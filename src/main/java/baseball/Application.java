ac3d73353a961d529dc2648a1a6bb0dd1d93f2e1
hojang <109591135+26solitude@users.noreply.github.com>
2023-10-21 22:17:12 +0900

    재경기 여부를 묻고 재경기 및 종료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,53 +10,63 @@ import java.util.Objects;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("숫자 야구 게임을 시작합니다.");     // 프로그램 시작
-
-        List<Integer> computer = new ArrayList<>();     // 컴퓨터에게 랜덤 숫자 생성하기
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
-        }
-
         while(true) {
-            System.out.print("숫자를 입력해주세요 : ");
-            String num = Console.readLine();        // 사용자에게 숫자를 입력받기
+            System.out.println("숫자 야구 게임을 시작합니다.");     // 프로그램 시작
 
-            if (num.length() != 3) throw new IllegalArgumentException();    // 예외처리
-
-            char[] checkDup = num.toCharArray();        // 예외처리
-            HashMap<Character, Integer> hm = new HashMap<>();
-            for (char c : checkDup) {
-                if (!hm.containsKey(c)) {
-                    hm.put(c, 0);
-                } else {
-                    throw new IllegalArgumentException();
+            List<Integer> computer = new ArrayList<>();     // 컴퓨터에게 랜덤 숫자 생성하기
+            while (computer.size() < 3) {
+                int randomNumber = Randoms.pickNumberInRange(1, 9);
+                if (!computer.contains(randomNumber)) {
+                    computer.add(randomNumber);
                 }
             }
+            boolean playGame = true;
+            while (playGame) {
+                System.out.print("숫자를 입력해주세요 : ");
+                String num = Console.readLine();        // 사용자에게 숫자를 입력받기
 
-            List<Integer> userNum = new ArrayList<>();
-            for (int i = 0; i < 3; i++) {
-                userNum.add(Character.getNumericValue(num.charAt(i)));
+                if (num.length() != 3) throw new IllegalArgumentException();    // 예외처리
+
+                char[] checkDup = num.toCharArray();        // 예외처리
+                HashMap<Character, Integer> hm = new HashMap<>();
+                for (char c : checkDup) {
+                    if (!hm.containsKey(c)) {
+                        hm.put(c, 0);
+                    } else {
+                        throw new IllegalArgumentException();
+                    }
+                }
+
+                List<Integer> userNum = new ArrayList<>();
+                for (int i = 0; i < 3; i++) {
+                    userNum.add(Character.getNumericValue(num.charAt(i)));
+                }
+
+                int ball = 0;
+                int strike = 0;
+
+                for (int i = 0; i < 3; i++) {
+                    if (Objects.equals(userNum.get(i), computer.get(i))) strike++;
+                    else if (!Objects.equals(userNum.get(i), computer.get(i)) && computer.contains(userNum.get(i)))
+                        ball++;
+                }
+
+
+                System.out.println("computer = " + computer + " user = " + userNum);
+                if (ball == 0 && strike == 0) System.out.println("낫싱");
+                else if (strike == 3) {
+                    System.out.println("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    playGame = false;
+                } else System.out.println(ball + "볼 " + strike + "스트라이크");
             }
 
-            int ball = 0;
-            int strike = 0;
-
-            for(int i=0; i<3; i++){
-                if(Objects.equals(userNum.get(i), computer.get(i))) strike++;
-                else if(!Objects.equals(userNum.get(i), computer.get(i)) && computer.contains(userNum.get(i))) ball++;
-            }
-
-
-            System.out.println("computer = " + computer + " user = " + userNum);
-            if(ball == 0 && strike == 0) System.out.println("낫싱");
-            else if(strike == 3) {
-                System.out.println("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String againOrExit = Console.readLine();
+            if (Objects.equals(againOrExit, "1")) System.out.println("게임을 새로 시작합니다.");
+            else if (Objects.equals(againOrExit, "2")) {
+                System.out.println("종료");
                 break;
             }
-            else System.out.println(ball + "볼 " + strike + "스트라이크");
         }
     }
 }
