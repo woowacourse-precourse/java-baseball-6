683c7b2eb12ec629247f0b7d28670352709c4a21
munis <aaaa6658@naver.com>
2023-10-21 16:48:07 +0900

    feat : 3자리 숫자 야구의 클래스 BaseBallNumber 생성

---
+++ src/main/java/baseball/BaseBallNumber.java
@@ -0,0 +1,11 @@
+package baseball;
+
+import java.util.List;
+
+public class BaseBallNumber {
+    private List<Integer> baseballNumber;
+
+    public BaseBallNumber(List<Integer> baseballNumber) {
+        this.baseballNumber = baseballNumber;
+    }
+}
