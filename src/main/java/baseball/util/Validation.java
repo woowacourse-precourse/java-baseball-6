0538547f7de4a68849e8ecc0d92705f57bacb4a3
김효준 <khyojun1104@gmail.com>
2023-10-21 16:37:36 +0900

    [feat] Validation : 재시작 번호 입력 관련 validation 기능 구현 !

--- src/main/java/baseball/util/Validation.java
+++ src/main/java/baseball/util/Validation.java
@@ -3,7 +3,10 @@ package baseball.util;
 public class Validation {
 
     private static final String NUMBER_REGEX = "^[1-9]+$";
+    private static final String RESTART_NUMBER_REGEX= "^[1-2]+$";
     private static final Integer INPUT_LENGTH = 3;
+
+    private static final Integer RESTART_LENGTH = 1;
 
     public String validateNumber(String number) {
         checkRightNumber(number);
@@ -11,6 +14,23 @@ public class Validation {
     }
 
 
+
+    public void validateRestart(String number){
+        checkRightRestartNumber(number);
+        validateRestartNumberLength(number);
+    }
+
+    private static void checkRightRestartNumber(String number) {
+        if(!number.matches(RESTART_NUMBER_REGEX)){
+            throw new IllegalArgumentException("재시작 관련 입력은 1 또는 2의 숫자여야 합니다! ");
+        }
+    }
+
+    private static void validateRestartNumberLength(String number) {
+        if(number.length() != RESTART_LENGTH){
+            throw new IllegalArgumentException("1 또는 2 의 숫자로 한 번만 입력해주세요!");
+        }
+    }
 
     private void checkRightNumber(String number) {
         validateIsNumber(number);
