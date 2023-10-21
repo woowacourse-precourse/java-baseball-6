54c416a070dbc884382ddc8227a8a80d55ec8778
jay <wlguswhd0809@naver.com>
2023-10-21 23:52:22 +0900

    feat : 랜덤 숫자 뽑기 메서드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    private static List<Integer> randomComputerNumber() { // 랜덤 숫자를 뽑는 메서드
+        List<Integer> computer = new ArrayList<>();
+
+        while (computer.size() < 3) { // 3자리 숫자를 뽑기 위함
+            int randomNumber = Randoms.pickNumberInRange(1, 9); // 1부터 9까지의 랜덤 수
+
+            if (!computer.contains(randomNumber)) { // 서로 같은 수의 중복을 피하기 위함
+                computer.add(randomNumber);
+            }
+        }
+
+        return computer;
     }
 }
