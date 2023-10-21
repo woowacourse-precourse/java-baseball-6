ad5587df2236fa2679576bc41b2d9e48786ace69
Jinwoo Lee <binaryWooo@gmail.com>
2023-10-22 00:49:38 +0900

    feat: 게임 결과 검증 로직 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,7 @@ import static camp.nextstep.edu.missionutils.Console.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
@@ -20,7 +21,11 @@ public class Application {
 
 				while (true) {
 					System.out.print("숫자를 입력해주세요 : ");
-					List<Integer> user = setUserNumber(readLine());
+					List<Integer> user = setUserNumber(readLine());     // 사용자 숫자 중복 체크 필요
+
+					if (checkResult(computer, user)) {
+						break;
+					}
 				}
 
 				System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
@@ -32,6 +37,8 @@ public class Application {
 		} catch (IllegalArgumentException e) {
 			System.out.println("숫자만 입력가능합니다.");
 			System.out.println("프로그램을 종료합니다.");
+		} catch (NoSuchElementException e) {
+			throw new IllegalArgumentException(e);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -53,5 +60,41 @@ public class Application {
 			user.add(Integer.parseInt(s));
 		}
 		return user;
+	}
+
+	public static boolean checkResult(List<Integer> computer, List<Integer> user) {
+		int ball = 0;
+		int strike = 0;
+
+		for (int i = 0; i < 3; i++) {
+			if (computer.get(i).equals(user.get(i))) {
+				strike++;
+			} else {
+				if (computer.contains(user.get(i))) {
+					ball++;
+				}
+			}
+		}
+
+		if (strike == 3) {
+			System.out.println("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
+			return true;
+		} else {
+			if (ball == 0 && strike == 0) {
+				System.out.println("낫싱");
+			} else {
+				if (ball > 0) {
+					System.out.print(ball + "볼 ");
+				}
+
+				if (strike > 0) {
+					System.out.print(strike + "스트라이크");
+				}
+				System.out.println();
+			}
+
+		}
+		return false;
 	}
 }
