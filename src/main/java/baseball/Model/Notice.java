b31198756d03ab8d68530837c8fef32f7daceb43
SEOWONLEEE <9952sun@jj.ac.kr>
2023-10-21 11:49:54 +0900

    feat: 기본 입출력 및 컴퓨터 숫자 조건

---
+++ src/main/java/baseball/Model/Notice.java
@@ -0,0 +1,28 @@
+package baseball.Model;
+
+public class Notice {
+    public static final String Game_Start = "숫자 야구 게임을 시작합니다.";
+    public static final String Input_Number = "숫자를 입력해주세요 : ";
+    public static final String Game_Over = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String Restart_or_Exit = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public static final int Minimum_Range = 1;
+    public static final int Maximum_Range = 9;
+    public static final int fixed_Number_Size = 3;
+
+    public static final String Status_Ball = "볼";
+    public static final String Status_Strike = "스트라이크";
+    public static final String Status_None = "낫싱";
+
+    public static final int Input_Restart_Number = 1;
+    public static final int Input_Exit_Number = 2;
+    public static final String Input_Restart_Game = "1";
+    public static final String Input_Exit_Game = "2";
+
+    public static final String Number_Size_Exception = "3자리의 숫자로 구성해 주세요.";
+    public static final String Number_Overlap_Exception = "각 숫자는 중복이 안 됩니다.";
+    public static final String Input_Type_Exception = "숫자(양수)를 입력해 주세요.";
+    public static final String Input_Nothing = "입력된 내용이 없습니다.";
+    public static final String Input_Wrong = "잘못된 입력. 다시 입력해 주세요.";
+
+}
