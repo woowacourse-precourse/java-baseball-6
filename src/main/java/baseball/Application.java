00fe9bcfc3969ded88114036c0275f582eb5095b
Hayoon <hy97@sju.ac.kr>
2023-10-21 21:48:59 +0900

    feat(Application): Application과 BaseBallGame 모듈 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import baseball.console.BaseBallGame;
+
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO: 프로그램 구현
+        BaseBallGame baseBallGame = new BaseBallGame();
+        baseBallGame.playGame();
     }
 }
