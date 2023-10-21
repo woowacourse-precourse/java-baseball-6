542331c05b02ecf36774a9b0dfc180f9f3d22246
mainfn <_@mainfn.dev>
2023-10-22 02:34:14 +0900

    refact: handle input without throwing exception

--- src/main/java/baseball/domain/ball/GameStatus.java
+++ src/main/java/baseball/domain/ball/GameStatus.java
@@ -1,22 +1,23 @@
 package baseball.domain.ball;
 
 public enum GameStatus {
-  REPLAY("1"),
-  EXIT("2");
+    REPLAY("1"),
+    EXIT("2"),
+    UNKNOWN("_");
 
-  private final String input;
+    private final String input;
 
-  GameStatus(final String input) {
-    this.input = input;
-  }
-
-  public static GameStatus of(final String input) {
-    if ("1".equals(input)) {
-      return REPLAY;
+    GameStatus(final String input) {
+        this.input = input;
     }
-    if ("2".equals(input)) {
-      return EXIT;
+
+    public static GameStatus of(final String input) {
+        if ("1".equals(input)) {
+            return REPLAY;
+        }
+        if ("2".equals(input)) {
+            return EXIT;
+        }
+        return UNKNOWN;
     }
-    throw new IllegalArgumentException("GameStatus는 반드시 1, 2 중 하나를 입력해서 만들 수 있습니다.");
-  }
 }
--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -7,27 +7,34 @@ import java.util.List;
 
 public final class InputView {
 
-  private int executionCount = 0;
+    private int executionCount = 0;
 
-  public List<Integer> inputBallNumbers() {
-    if (executionCount++ == 0) {
-      System.out.println("숫자 야구 게임을 시작합니다.");
+    public List<Integer> inputBallNumbers() {
+        if (executionCount++ == 0) {
+            System.out.println("숫자 야구 게임을 시작합니다.");
+        }
+        System.out.print("숫자를 입력해주세요 : ");
+
+        return Arrays.stream(readLine().split(""))
+                .map(Integer::parseInt)
+                .toList();
     }
-    System.out.print("숫자를 입력해주세요 : ");
 
-    return Arrays.stream(readLine().split(""))
-        .map(Integer::parseInt)
-        .toList();
-  }
+    private String readLine() {
+        return Console.readLine().trim();
+    }
 
-  private String readLine() {
-    return Console.readLine().trim();
-  }
+    public GameStatus inputNextGameStatus() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
-  public GameStatus inputWillReplay() {
-    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-    return GameStatus.of(readLine());
-  }
+        final GameStatus status = GameStatus.of(readLine());
+
+        if (GameStatus.UNKNOWN == status) {
+            System.out.println("잘못된 입력입니다. 반드시 1, 2 중 하나를 입력해야 합니다.");
+            return inputNextGameStatus();
+        }
+
+        return status;
+    }
 
 }
