f56b173786226b04541d24a3c14889e1d714eead
jeondui <jeondui@gmail.com>
2023-10-22 02:35:29 +0900

    style(NumericValidator): 중복 검사 부분 간결한 코드로 수정

--- src/main/java/baseball/util/NumericValidator.java
+++ src/main/java/baseball/util/NumericValidator.java
@@ -27,10 +27,7 @@ public class NumericValidator {
 
     // 서로 다른 숫자인지 검사
     private boolean isDifferentNumber(List<Integer> numbers) {
-        Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < numbers.size(); i++) {
-            set.add(numbers.get(i));
-        }
+        Set<Integer> set = new HashSet<>(numbers);
         return set.size() == BALL_NUMBER_LENGTH;
     }
 
