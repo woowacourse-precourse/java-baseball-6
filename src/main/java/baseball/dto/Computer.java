ac6e868f700874ca99f0adfd2b1a90bfdb2448b9
tackyu97 <tack970913@gmail.com>
2023-10-21 22:52:55 +0900

    Feat: 컴퓨터 클래스 생성

--- src/main/java/baseball/dto/Computer.java
+++ src/main/java/baseball/dto/Computer.java
@@ -1,5 +1,25 @@
 package baseball.dto;
 
 public class Computer {
-    privat
+    private String input = "";
+    private int[] computerNumberBall;
+
+    Computer() {
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+
+    public int[] getComputerNumberBall() {
+        return computerNumberBall;
+    }
+
+    public void setComputerNumberBall(int[] computerNumberBall) {
+        this.computerNumberBall = computerNumberBall;
+    }
 }
