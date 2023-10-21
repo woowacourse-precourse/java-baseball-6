9674c275069bfddd0207e3569a603d85198dc440
jeondui <jeondui@gmail.com>
2023-10-21 20:34:00 +0900

    feat(NumericValidator): 입력값을 리스트로 변환하는 기능

--- src/main/java/baseball/util/NumericValidator.java
+++ src/main/java/baseball/util/NumericValidator.java
@@ -1,8 +1,10 @@
 package baseball.util;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class NumericValidator {
 
@@ -33,5 +35,12 @@ public class NumericValidator {
             }
         }
         return true;
+    }
+
+    public List<Integer> converStringToList(String inputNumber) {
+        int[] intArray = Arrays.stream(inputNumber.split(""))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        return Arrays.stream(intArray).boxed().collect(Collectors.toList());
     }
 }
