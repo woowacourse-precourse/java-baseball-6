31eb58ea3a2060ee9f85abea50c322f5ba46d1fd
tam31 <wxogud3377@naver.com>
2023-10-22 02:01:15 +0900

    Feat(Computer): 컴퓨터의 3가지 숫자뽑기 기능구현 및 Controller 연결 Docs: 컴퓨터의 3가지 숫자뽑기 완료 체크

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -1,10 +1,13 @@
 package baseball.controller;
 
+import baseball.model.Computer;
 import baseball.view.OutputView;
 
 public class BaseballController {
 
+    Computer computer = new Computer();
     public void startGame(){
         OutputView.baseballGameStart();
+        System.out.println(computer.getComputerNumber());
     }
 }
--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -1,4 +1,31 @@
 package baseball.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Computer {
+
+    private List<Integer> computerNumber;
+
+    public Computer() {
+        this.computerNumber = drawComputerNumber();
+    }
+
+    public List<Integer> getComputerNumber() {
+        return computerNumber;
+    }
+
+    private List<Integer> drawComputerNumber(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
 }
