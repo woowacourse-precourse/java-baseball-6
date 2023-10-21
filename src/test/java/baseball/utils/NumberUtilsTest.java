6f971148faadad7f4764e3a2b36cd4630c8f46e8
상원 최 <swchoi1@swm.ai>
2023-10-21 10:44:11 +0900

    fix: "오타 수정"
    #3-user-input

--- src/test/java/baseball/utils/NumberUtilsTest.java
+++ src/test/java/baseball/utils/NumberUtilsTest.java
@@ -37,7 +37,7 @@ class NumberUtilsTest {
 
     @ParameterizedTest
     @MethodSource("withInFailureTest")
-    @DisplayName("숫자가 범위 내에 있는지 확인 - 성공 시 true 리턴")
+    @DisplayName("숫자가 범위 내에 있는지 확인 - 실패 시 false 리턴")
     public void checkTargetNumberWithInRange_Failure(final Integer target,
                                                      final Integer start,
                                                      final Integer end
