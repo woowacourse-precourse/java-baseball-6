fa7c98a034dd554acb323e2d49ab4b74db9b6528
jsj <tkdlek4186@naver.com>
2023-10-21 20:12:07 +0900

    feat: 모든 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,23 +1,17 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
-
-
 import java.util.ArrayList;
 import java.util.List;
-
-
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
-
 public class Application {
-    public static boolean check(List<Integer> input_list, List<Integer> com_list){
+    public static boolean game_check(List<Integer> input_list, List<Integer> com_list){
         int strick_cnt=0;
         int ball_cnt=0;
 
         for(int i=0;i<input_list.size();i++){
-            if(input_list.get(i)==com_list.get(i)) strick_cnt++;
-            System.out.println(input_list+" "+com_list);
+            if(input_list.get(i).equals(com_list.get(i))) strick_cnt++;
         }
 
         if (strick_cnt==3) {
@@ -26,7 +20,6 @@ public class Application {
         }
         else {
             for(int i : com_list){
-
                 if(input_list.contains(i)) ball_cnt++;
             }
             ball_cnt=ball_cnt-strick_cnt;
@@ -40,12 +33,7 @@ public class Application {
             }
             return false;
         }
-
-
-
     }
-
-
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
@@ -53,26 +41,33 @@ public class Application {
         List<Integer> input_list=new ArrayList<>();
         List<Integer> com_list = new ArrayList<>();
 
-        com_list.clear();
-        while (com_list.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!com_list.contains(randomNumber)) {
-                com_list.add(randomNumber);
-            }
-        }
-
-        for(int i : com_list){
-            System.out.print(i);
-        }
-
+        String game;
         do{
-            input_list.clear();
-            System.out.print("숫자를 입력해주세요. : ");
-            String num = readLine();
-            for(int i=0;i<num.length();i++){
-                input_list.add((int)num.charAt(i)-'0');
+            com_list.clear();
+            while (com_list.size() < 3) {
+                int randomNumber = Randoms.pickNumberInRange(1, 9);
+                if (!com_list.contains(randomNumber)) {
+                    com_list.add(randomNumber);
+                }
             }
-        }
-        while(!check(input_list,com_list)) ;
+
+            do{
+                input_list.clear();
+                System.out.print("숫자를 입력해주세요. : ");
+                String num = readLine();
+
+                if(num.length() != 3){
+                    throw new IllegalArgumentException("Invalid size ");
+                }
+                for (int i = 0; i < num.length(); i++) {
+                    input_list.add((int) num.charAt(i) - '0');
+                }
+            }
+            while(!game_check(input_list,com_list)) ;
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            game=readLine();
+        }while(game.contains("1"));
+
     }
 }
