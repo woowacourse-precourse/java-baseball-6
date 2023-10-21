96a131d5e6bc6ab1bbcff9f44d1fe5e9f6417ee4
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-10-21 23:28:10 +0900

    FEAT / Add player, computer

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,11 @@
+package baseball;
+
+public class Game {
+    private Player player = new Player();
+    private Player computer = new Player();
+
+    public Game() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+}
