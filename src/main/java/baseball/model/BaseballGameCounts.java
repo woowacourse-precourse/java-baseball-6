b091949a0072b97789d3d604c8c9ef0595f8ae5d
oxdjww <oxdjww@gmail.com>
2023-10-21 21:27:13 +0900

    add : BaseballGameCounts 생성

---
+++ src/main/java/baseball/model/BaseballGameCounts.java
@@ -0,0 +1,21 @@
+package baseball.model;
+
+public class BaseballGameCounts {
+    private int strikes;
+    private int balls;
+    private String message;
+
+    protected BaseballGameCounts() {}
+
+    public static BaseballGameCounts createBaseballGameCounts() {
+        return new BaseballGameCounts();
+    }
+
+    public boolean isWinCondition() {
+        return this.strikes == 3;
+    }
+
+    public void showCounts() {
+
+    }
+}
