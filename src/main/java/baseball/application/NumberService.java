e1e02854888ced4ca8caf96c9fecd04249488ac4
jae woo Jung <awfjol2008@naver.com>
2023-10-21 18:39:23 +0900

    feat: 컴퓨터의 숫자를 랜덤하게 정하는 메소드 구현

--- src/main/java/baseball/application/NumberService.java
+++ src/main/java/baseball/application/NumberService.java
@@ -1,4 +1,19 @@
 package baseball.application;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class NumberService {
+
+    public List<Integer> getRandomNumber() {
+        Set<Integer> numberSet = new HashSet<>();
+        while (numberSet.size() < 3) {
+            numberSet.add(Randoms.pickNumberInRange(1, 9));
+        }
+
+        return new ArrayList<>(numberSet);
+    }
 }
