161692360507f3d7161826e07a86504a1dd0519f
chansoo <chansoo38@gmail.com>
2023-10-21 17:35:08 +0900

    <Feat> : Application - main 메서드를 작성한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,10 @@
 package baseball;
 
 public class Application {
+    static final GameStarter gameStater = new GameStarter();
     public static void main(String[] args) {
+        gameStater.initGame();
+        gameStater.startGame();
         // TODO: 프로그램 구현
     }
 }
