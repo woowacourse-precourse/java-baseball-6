1f945dccbb31abee4e3a61f5850862d955785f2d
깃wooqqq <sge0225@naver.com>
2023-10-21 15:43:37 +0900

    feat: 볼 개수 세어주는 countBall 메서드 구현

--- src/main/java/baseball/GameProcess.java
+++ src/main/java/baseball/GameProcess.java
@@ -19,4 +19,16 @@ public class GameProcess {
 
         return strike;
     }
+
+    public static Integer countBall(List<Integer> playerNumber, List<Integer> computerNumber) {
+        int ball = 0;
+
+        for (int i = 0; i < 3; i++) {
+            if (!Objects.equals(playerNumber.get(i), computerNumber.get(i)) && computerNumber.contains(playerNumber.get(i))) {
+                ball += 1;
+            }
+        }
+
+        return ball;
+    }
 }
