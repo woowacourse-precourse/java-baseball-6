afc35bc12d995a8e0884b8323a3f7c1ef4e9a276
이준희 <juni416@naver.com>
2023-10-21 21:15:14 +0900

    feature: 컴퓨터가 생성되면 랜덤 숫자가 함께 생성

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -6,14 +6,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
-    static List<Integer> createRandomNumber(){
-        List<Integer> number = new ArrayList<>();
-        while (number.size() < 3) {
+    List<Integer> number = new ArrayList<>();
+    Computer() {
+        createRandomNumber();
+    }
+
+    public List<Integer> createRandomNumber() {
+        List<Integer> result = new ArrayList<>();
+        while (result.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!number.contains(randomNumber)) {
-                number.add(randomNumber);
+            if (!result.contains(randomNumber)) {
+                result.add(randomNumber);
             }
         }
-        return number;
+        number = result;
+        System.out.println(number);
+        return result;
+    }
+
+    public void resetNumber() {
+        number.clear();
+        createRandomNumber();
     }
 }
