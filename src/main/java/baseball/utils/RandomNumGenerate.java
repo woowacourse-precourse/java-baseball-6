7fb2d6381b13b2e5c1e120df7eed550bfbbacced
다운 <j_d_woon@naver.com>
2023-10-22 07:17:46 +0900

    refactor: create 메서드 static으로 변경

--- src/main/java/baseball/utils/RandomNumGenerate.java
+++ src/main/java/baseball/utils/RandomNumGenerate.java
@@ -1,11 +1,11 @@
-package baseball;
+package baseball.utils;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
 public class RandomNumGenerate {
-    public List<Integer> create() {
+    public static List<Integer> create() {
         List<Integer> randomNumber = new ArrayList<>();
         while (randomNumber.size() < 3) {
             int number = Randoms.pickNumberInRange(1, 9);
