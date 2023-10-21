8ca623832536a110f83a3fa243819470e5abcef6
kimdohyung <pdohyung@gmail.com>
2023-10-22 00:07:19 +0900

    feat: 게임 시작과 종료 메세지 출력 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,4 +1,11 @@
 package baseball.view;
 
 public class OutputView {
+	public void gameStart(){
+		System.out.println("숫자 야구 게임을 시작합니다.");
+	}
+
+	public void gameFinish(){
+		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+	}
 }
