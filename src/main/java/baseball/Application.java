cc881eb62dd8106629bb89301735090acbc75e71
LEM <ppoppo9802@naver.com>
2023-10-21 22:13:28 +0900

    컴퓨터에 랜덤한 숫자 할당하는 클래스 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,18 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
-    public static class Computer {
-        public List<Integer> computerNumber() {
-            List<Integer> number = new ArrayList<>();
-            while (number.size() < 3) {
-                int randomNumber = Randoms.pickNumberInRange(1, 9);
-                if (!number.contains(randomNumber)) {
-                    number.add(randomNumber);
-                }
-            }
-            return number;
-        }
-    }
     public static void main(String[] args) {
         Computer com = new Computer();
         System.out.println(com.computerNumber().toString());
