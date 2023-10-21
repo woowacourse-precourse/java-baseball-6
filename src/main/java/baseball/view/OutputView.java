8b4dec2d223a0346e1317c592df882ff52fba4b8
haiseong <wjdgotjd9908@gmail.com>
2023-10-22 02:53:54 +0900

    refactor: OutputView: print, println 분리

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -2,6 +2,10 @@ package baseball.view;
 
 public class OutputView {
     public static void print(String string) {
+        System.out.print(string);
+    }
+
+    public static void println(String string) {
         System.out.println(string);
     }
 }
