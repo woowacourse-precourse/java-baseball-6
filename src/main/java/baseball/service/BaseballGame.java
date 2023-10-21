6be66e6f24504163dab4ce7f0e6d74f25208abc4
민주 <hinote444@gmail.com>
2023-10-21 18:57:58 +0900

    feat(BaseballGame): 플레이어가 입력한 값을 domain에 저장하는 기능 구현

--- src/main/java/baseball/service/BaseballGame.java
+++ src/main/java/baseball/service/BaseballGame.java
@@ -1,15 +1,25 @@
 package baseball.service;
 
 import baseball.domain.Computer;
+import baseball.domain.Player;
+import baseball.utils.Utils;
+import baseball.view.InputView;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BaseballGame {
     private Computer computer = new Computer();
+    private Player player = new Player();
 
     public void startGame() {
+        setUpBaseballGame();
+    }
+
+    public void setUpBaseballGame() {
         computer.setBaseballNumbers(createComputerNumbers());
+        List<Integer> playerNumbers = Utils.parseIntegerList(InputView.inputBaseballNumbers());
+        player.setBaseballNumbers(playerNumbers);
     }
 
     private List<Integer> createComputerNumbers() {
