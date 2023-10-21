6fea28bed4336631e6d48121c75453342d4aa405
jeondui <jeondui@gmail.com>
2023-10-21 22:21:52 +0900

    refactor(InputView): 게임 재시작 여부 입력 메서드 수정

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -10,7 +10,7 @@ public class InputView {
     }
 
     public static String getRetryNumber() {
-        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         return Console.readLine();
     }
 }
