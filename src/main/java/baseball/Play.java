bd6175c1c5173051d54233b385122e0fb7772ffd
wochae <officialwochae@gmail.com>
2023-10-21 23:04:06 +0900

    [FEAT] : play implements, test with ApplicationTest

--- src/main/java/baseball/Play.java
+++ src/main/java/baseball/Play.java
@@ -9,9 +9,10 @@ public class Play {
   List<Integer> randomNumbers = new ArrayList<>();
 
 
-  public int play() {
+  public int gameSet() {
     Computer computer = new Computer();
     Referee referee = new Referee();
+    Player player = new Player();
     int gameStatus;
 
     referee.startGameMessage();
@@ -20,8 +21,14 @@ public class Play {
     do {
       System.out.print("숫자를 입력해주세요 : ");
       String input = Console.readLine();
+      if (!player.validateInput(input)) {
+        throw new IllegalArgumentException("입력 형식이 올바르지 않습니다.");
+      }
       gameStatus = referee.determine(input);
-    } while (gameStatus == 1);
+
+    }
+    while (gameStatus == 1);
+
     System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     String restart = Console.readLine();
     return Integer.parseInt(restart);
