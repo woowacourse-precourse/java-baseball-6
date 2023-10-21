e6a1b0c086b1869894a008832760f7b102d8974f
starcat37 <starcat37@korea.ac.kr>
2023-10-22 01:04:30 +0900

    refactor: 예외 메시지 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -64,7 +64,7 @@ public class Application {
         }
         for (char c : userInput.toCharArray()) {
             if (Character.getNumericValue(c) < 1 || Character.getNumericValue(c) > 9) {
-                throw new IllegalArgumentException("사용자 입력 범위가 잘못되었습니다.");
+                throw new IllegalArgumentException("사용자 입력 범위는 1-9 사이 값이어야 합니다.");
             }
             if (userInput.indexOf(c) != userInput.lastIndexOf(c)) {
                 throw new IllegalArgumentException("사용자 입력이 중복되었습니다.");
