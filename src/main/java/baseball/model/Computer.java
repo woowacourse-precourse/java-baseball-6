90984ffa2868886d1cf50e06fb9908bbf4eb779a
kimtaesoo <kimtaesoo7@naver.com>
2023-10-21 22:55:12 +0900

    feat: 스트라이크 갯수 비교 구현

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -3,8 +3,11 @@ package baseball.model;
 import baseball.NumberGenerator;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Computer {
+
+    private static final int FIRST_NUMBER = 0;
 
     private final List<Integer> numbers;
 
@@ -14,6 +17,16 @@ public class Computer {
 
     public static Computer createWithGenerator(final NumberGenerator generator) {
         return new Computer(generator.generate());
+    }
+
+    public int getStrikeCount(final List<Integer> inputNumbers) {
+        return (int) IntStream.range(FIRST_NUMBER, inputNumbers.size())
+            .filter(index -> isStrike(index, inputNumbers))
+            .count();
+    }
+
+    private boolean isStrike(int index, List<Integer> inputNumbers) {
+        return numbers.get(index).equals(inputNumbers.get(index));
     }
 }
 
