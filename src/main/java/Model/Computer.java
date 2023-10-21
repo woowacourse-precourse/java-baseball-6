22a683a9c84990ff24f881192d48085d2bdc0cf9
김영준 <rladudwns164@gmail.com>
2023-10-21 18:46:54 +0900

    refactor : 자바 컨벤션에 맞게 코드 수정

--- src/main/java/Model/Computer.java
+++ src/main/java/Model/Computer.java
@@ -4,27 +4,26 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.LinkedHashSet;
 
 public class Computer {
+
     private String computerNumber;
 
-    public Computer(){
+    public Computer() {
         createComputerNumber();
     }
 
-    private void createComputerNumber(){
+    private void createComputerNumber() {
         LinkedHashSet<String> computerCheck = new LinkedHashSet<>();
         while (computerCheck.size() < 3) {
             computerCheck.add(randomNumberCreate());
         }
-       this.computerNumber = String.join("",computerCheck);
-
+        this.computerNumber = String.join("", computerCheck);
     }
 
-    private static String randomNumberCreate(){
-        return Integer.toString(Randoms.pickNumberInRange(1,9));
+    private static String randomNumberCreate() {
+        return Integer.toString(Randoms.pickNumberInRange(1, 9));
     }
 
-    public String getComputerNumber(){
-
+    public String getComputerNumber() {
         return computerNumber;
     }
 }
