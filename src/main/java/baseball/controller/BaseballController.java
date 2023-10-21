97c419bc22c8d8b11ad94070e7575244e381cd88
김승진 <ohksj77@gmail.com>
2023-10-21 19:45:54 +0900

    feat(BaseballController): 랜덤 숫자 생성 기능 컨트롤러 연결

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -1,16 +1,24 @@
 package baseball.controller;
 
 import baseball.io.OutputView;
+import baseball.service.BaseballService;
 
 public class BaseballController {
 
     private final OutputView outputView;
+    private final BaseballService baseballService;
 
-    public BaseballController(final OutputView outputView) {
+    public BaseballController(final OutputView outputView, final BaseballService baseballService) {
         this.outputView = outputView;
+        this.baseballService = baseballService;
     }
 
     public void run() {
         outputView.printStartMessage();
+        play();
+    }
+
+    private void play() {
+        baseballService.saveRandomNumbers();
     }
 }
--- src/main/java/baseball/factory/ComponentFactory.java
+++ src/main/java/baseball/factory/ComponentFactory.java
@@ -2,11 +2,21 @@ package baseball.factory;
 
 import baseball.controller.BaseballController;
 import baseball.io.OutputView;
+import baseball.service.BaseballService;
+import baseball.utils.RandomNumberGenerator;
 
 public class ComponentFactory {
 
     public BaseballController baseballController() {
-        return new BaseballController(outputView());
+        return new BaseballController(outputView(), baseballService());
+    }
+
+    private BaseballService baseballService() {
+        return new BaseballService(randomNumberGenerator());
+    }
+
+    private RandomNumberGenerator randomNumberGenerator() {
+        return new RandomNumberGenerator();
     }
 
     private OutputView outputView() {
