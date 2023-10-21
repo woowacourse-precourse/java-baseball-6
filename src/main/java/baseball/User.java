fdec9d400066be0092922b561bb707c58bda8c6c
choimung <103022503+choimung@users.noreply.github.com>
2023-10-21 14:58:28 +0900

    refactor : SetUserNumber() Method Refactoring 객체에서 스스로 상태값을 변경할 수 있도록 Console.readLine 을 추가하였습니다.

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,10 +1,13 @@
 package baseball;
-
+import camp.nextstep.edu.missionutils.Console;
 public class User {
     private UserNumber userNumber;
+    private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
 
-    public void setUserNumber(String number){
-        this.userNumber = new UserNumber(number);
+    public void setUserNumber(){
+        System.out.print(INPUT_MESSAGE);
+        String input = Console.readLine();
+        this.userNumber = new UserNumber(input);
     }
 
     public String getUserNumber() {
