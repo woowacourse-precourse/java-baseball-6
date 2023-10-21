07528575a3db7b7259c7950f32ade5b4a043e390
Kim Mi Ju <76895039+miju0515@users.noreply.github.com>
2023-10-21 13:23:48 +0900

    feat: 게임 시작하기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+
+
     }
 }
