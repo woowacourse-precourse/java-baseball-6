c627f4f679493d14eda818bf3f8e1336941e8fcd
dlqudtjs <dlqudtjs922@gmail.com>
2023-10-22 00:09:20 +0900

    feat: Computer 모델에 관련 상수 추가

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -1,6 +1,9 @@
 package baseball.model;
 
 public class Computer {
+    private static final int MIN_RANDOM_NUMBER = 1;
+    private static final int MAX_RANDOM_NUMBER = 9;
+    private static final int RANDOM_NUMBER_LENGTH = 3;
 
     public Computer() {
 
