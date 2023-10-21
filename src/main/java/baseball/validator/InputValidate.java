a61bb70bd93677afc42e76f37b347938b9b1f9ad
TaeHye0n <thk001229@gmail.com>
2023-10-21 18:33:06 +0900

    feat: 사용자 입력에 중복된 숫자가 있는지 검증 기능  - 중복을 체크하기 위해 Set 자료구조 사용

--- src/main/java/baseball/validator/InputValidate.java
+++ src/main/java/baseball/validator/InputValidate.java
@@ -1,12 +1,16 @@
 package baseball.validator;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class InputValidate {
 
     private static final int BEGIN_ASCII_CODE = 48; // 숫자 0
     private static final int END_ASCII_CODE = 57; // 숫자 9
     private static final int MAX_NUMBER_LENGTH = 3;
-    private static final String NON_NUMERIC_ERROR_MESSAGE = "숫자만 입력해주세요.";
-    private static final String LENGTH_ERROR_MESSAGE = "입력 길이가 일치하지 않습니다. 입력 길이는 : ";
+    private static final String NON_NUMERIC_ERROR_MESSAGE = "숫자만 입력해주세요";
+    private static final String LENGTH_ERROR_MESSAGE = "입력 길이가 일치하지 않습니다 입력 길이는 : ";
+    private static final String DUPLICATE_ERROR_MESSAGE = "중복된 숫자가 포함되어 있습니다";
 
     public void validateNumeric(String input) {
         String stringValue = String.valueOf(input);
@@ -24,5 +28,15 @@ public class InputValidate {
         }
     }
 
+    public void validateDuplicateNumber(String input) {
+        String stringValue = String.valueOf(input);
+        Set<Character> characterSet = new HashSet<>();
+        for (char c : stringValue.toCharArray()) {
+            characterSet.add(c);
+        }
+        if (characterSet.size() != MAX_NUMBER_LENGTH) {
+            throw new IllegalArgumentException(DUPLICATE_ERROR_MESSAGE);
+        }
+    }
 
 }
