da6c36da9c4edb05c84d771a96628db95bcc9aa4
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-10-21 22:10:46 +0900

    feat: 사용자 입력에 대한 볼, 스트라이크/낫싱 출력 기능 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -3,5 +3,7 @@ package baseball;
 public class Game {
     public void startGame(){
         Output.startCommand();
+        BaseballGame baseballGame = new BaseballGame();
+        baseballGame.play();
     }
 }
