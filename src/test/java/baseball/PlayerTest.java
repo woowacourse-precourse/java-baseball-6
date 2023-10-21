34feec3304105f4ec03723da15e343acdfb09325
김성현 <aruesin2@gmail.com>
2023-10-21 23:21:26 +0900

    feat: 객체별 테스트 코드 작성

---
+++ src/test/java/baseball/PlayerTest.java
@@ -0,0 +1,19 @@
+package baseball;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PlayerTest {
+
+    Player player = new Player(Count.provideCount("456"));
+    @Test
+    void Counter_리턴(){
+        Assertions.assertEquals("456", player.getCount());
+    }
+    @Test
+    void String_추측값_입력받기(){
+        player.enterGuess(Count.makeCount("123"));
+        Assertions.assertEquals("123", player.getCount());
+    }
+
+}
