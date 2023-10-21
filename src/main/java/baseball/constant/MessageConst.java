af084d7ad08fe5e950de1fc6807fcfba73c35399
배정현 <bae1004kin@gmail.com>
2023-10-21 20:36:12 +0900

    commit : 예외 처리 출력 메시지 추가

--- src/main/java/baseball/constant/MessageConst.java
+++ src/main/java/baseball/constant/MessageConst.java
@@ -20,4 +20,13 @@ public final class MessageConst {
     public static final String GAME_CONTROL_MESSAGE = "게임을 새로 시작하려면 " + NumberConst.RESTART_NUMBER +
             ", 종료하려면 " + NumberConst.EXIT_NUMBER + "를 입력하세요.";
 
+    public static final String INPUT_LENGTH_EXCEPTION_MESSAGE = NumberConst.INPUT_LENGTH + "글자를 입력해 주세요.";
+
+    public static final String INPUT_RANGE_EXCEPTION_MESSAGE = NumberConst.FIRST_RANGE + "부터 " +
+            NumberConst.LAST_RANGE + "까지의 숫자를 입력해 주세요.";
+
+    public static final String INPUT_DUPLICATE_EXCEPTION_MSG = "중복된 숫자는 입력할 수 없습니다.";
+
+    public static final String GAME_CONTROL_EXCEPTION_MSG = NumberConst.RESTART_NUMBER + "(재시작) 혹은 " +
+            NumberConst.EXIT_NUMBER + "(종료)를 입력해 주세요.";
 }
