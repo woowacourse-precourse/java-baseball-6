0d16dedcd886d30f695132ac269baff8a731a814
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-10-21 23:32:46 +0900

    FEAT / add guessNum variable

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -4,9 +4,18 @@ import java.util.ArrayList;
 
 public class Player {
     private ArrayList<Integer> nums = new ArrayList<>();
+    private ArrayList<Integer> guessNums = new ArrayList<>();
 
     public Player() {
         RandomNum randomNum = new RandomNum();
         nums = randomNum.getNums();
+    }
+
+    public void setGuessNums(ArrayList<Integer> guessNums) {
+        this.guessNums = guessNums;
+    }
+
+    public ArrayList<Integer> getGuessNums() {
+        return guessNums;
     }
 }
