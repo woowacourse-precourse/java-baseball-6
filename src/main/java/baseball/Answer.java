f0ccb2eec62c8835896f29c8221d5aa95c3df708
hongkikii <hyangmeeh@icloud.com>
2023-10-22 00:15:58 +0900

    feat : 랜덤값(정답 값)정하기, 게임 시작 문구 출력하기

---
+++ src/main/java/baseball/Answer.java
@@ -0,0 +1,23 @@
+package baseball;
+
+import static baseball.Constants.COUNT;
+import static baseball.Constants.END_INCLUSIVE;
+import static baseball.Constants.START_INCLUSIVE;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Answer {
+
+    private List<Integer> answer;
+
+    public Answer() {
+        answer = new ArrayList<>();
+    }
+
+    public List<Integer> pickAnswerNumbers() {
+        answer = Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, COUNT);
+        return answer;
+    }
+}
---
+++ src/main/java/baseball/Constants.java
@@ -0,0 +1,10 @@
+package baseball;
+
+public class Constants {
+    // 클래스 별로 구분해야 하나?
+    public static final Integer START_INCLUSIVE = 1;
+    public static final Integer END_INCLUSIVE = 9;
+    public static final Integer COUNT = 3;
+
+    public static final String ALERT_START = "숫자 야구 게임을 시작합니다.";
+}
