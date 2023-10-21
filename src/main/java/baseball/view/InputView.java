37283e42de3008715ddd6599a11ba9d62c69e3c4
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2023-10-22 00:47:17 +0900

    feat: Input 숫자 받기 & Output 출력 기능 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,4 +1,15 @@
 package baseball.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    public String inputUserNum() {
+        System.out.println("숫자를 입력해주세요 : ");
+        return Console.readLine();
+    }
+
+    public String inputUserGame() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return Console.readLine();
+    }
 }
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,4 +1,15 @@
 package baseball.view;
 
 public class OutputView {
+    public void printStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void printHint(String hintMessage) {
+        System.out.println(hintMessage);
+    }
+
+    public void printEnd() {
+        System.out.println("개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
 }
