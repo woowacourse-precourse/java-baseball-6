c416bf750b130549a4180b55e50a2c3889922704
Hanjaemo <hjm2530@gmail.com>
2023-10-22 03:43:52 +0900

    refactor(OutputView): Java 컨벤션에 따른 줄바꿈 위치 개선

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -38,7 +38,7 @@ public class OutputView {
     }
 
     public static void printEndingMessageForAllStrike(int digitNumber) {
-        System.out.println(digitNumber + "스트라이크\n" +
-                digitNumber + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println(digitNumber + "스트라이크\n"
+                + digitNumber + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 }
