99669d149ab088991513563d13f08d2a8efd7259
Moly <102847513+jminkkk@users.noreply.github.com>
2023-10-21 16:02:23 +0900

    feat: Game 클래스 생성

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,16 @@
+package baseball;
+
+import java.util.List;
+
+public class Game {
+    private final String START_GAME_COMMENT = "숫자 야구 게임을 시작합니다.";
+    private final String PLAY_GAME_COMMENT = "숫자를 입력해주세요 : ";
+
+    private final Player player;
+    private final List<Integer> answer;
+
+    public Game(Player player, List<Integer> answer) {
+        this.player = player;
+        this.answer = answer;
+    }
+}
