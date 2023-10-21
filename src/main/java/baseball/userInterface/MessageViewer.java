ed52a7cd26d0f0468ec3a45da7f3bc174937a0af
jy016011 <39334789+jy016011@users.noreply.github.com>
2023-10-21 23:56:07 +0900

    style: 가독성위해 공백 추가

--- src/main/java/baseball/userInterface/MessageViewer.java
+++ src/main/java/baseball/userInterface/MessageViewer.java
@@ -33,7 +33,8 @@ public class MessageViewer {
     }
 
     public void printGameEndMsg(){
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료" +
+                "\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
 }
