bf9bfb7a2564fe4e9d63d242f307b60c1e1dfd5c
sangwopark19 <sangwopark19@gmail.com>
2023-10-21 17:02:08 +0900

    리스트에 중복되지않는 3개의 난수를 추가하는 메서드

---
+++ src/main/java/baseball/domain/RandomBall.java
@@ -0,0 +1,24 @@
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomBall {
+    private List<Integer> balls;
+
+    public RandomBall() {
+        this.balls = new ArrayList<>();
+    }
+
+    public void ballGenerator() {
+        balls.clear();
+        while (balls.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!balls.contains(randomNumber)) {
+                balls.add(randomNumber);
+            }
+        }
+    }
+
+}
