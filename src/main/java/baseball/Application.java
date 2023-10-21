45f3439eec8989f66245a780bf5dbcde5f572ea8
oxdjww <oxdjww@gmail.com>
2023-10-21 21:27:38 +0900

    feat : game 생성 및 run() 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
 
+import baseball.model.BaseballGame;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BaseballGame game = BaseballGame.createBaseballGame();
+        game.run();
+
     }
 }
