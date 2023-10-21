795f1a20803196b302b3449144a688a422bf7d5a
임강호 <55732952+Icln@users.noreply.github.com>
2023-10-21 22:14:17 +0900

    feat: 난수 객체 생성

---
+++ src/main/java/baseball/RandomNumber.java
@@ -0,0 +1,17 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomNumber {
+    private List<Integer> randomNumber;
+
+    public RandomNumber() {
+        this.randomNumber = new ArrayList<>();
+    }
+
+    public List<Integer> getRandomNumber() {
+        return this.randomNumber;
+    }
+    
+}
