43dbe895b536a25de5b8010dd038ec5dbcf90259
Firedrago95 <yonghwa95@naver.com>
2023-10-21 11:08:30 +0900

    refactor Computer 클래스와 User 객체의 인스턴스변수 Numbers 클래스 생성

---
+++ src/main/java/baseball/Model/Numbers.java
@@ -0,0 +1,15 @@
+package baseball.Model;
+
+import java.util.List;
+
+public class Numbers {
+
+    private List<Integer> numbers;
+
+    public Numbers (List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public void generateRandomThreeDigit() {
+    }
+}
