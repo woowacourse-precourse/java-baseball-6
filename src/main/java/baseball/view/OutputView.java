13aa5253deaf76175e19a6db7352351a9212e59a
hYe807n <rlagkdus55@gmail.com>
2023-10-21 21:53:45 +0900

    feat: 게임 시작 문구 출력 기능

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,11 @@
+package baseball.view;
+
+public class OutputView {
+  public static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+  public static final String END_MASSAGE = "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+  public static final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+  public void printStartMessage() {
+    System.out.println(START_MESSAGE);
+  }
+}
