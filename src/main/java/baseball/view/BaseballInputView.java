b9e92e2502687c1e575f450a0f7a7aaa9090c4cb
kunsanglee <gosmdochee2@gmail.com>
2023-10-22 00:30:51 +0900

    refactor(BaseballInputView): InputView 인터페이스를 구현하도록 변경

--- src/main/java/baseball/view/BaseballInputView.java
+++ src/main/java/baseball/view/BaseballInputView.java
@@ -2,20 +2,20 @@ package baseball.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class BaseballInputView {
+public class BaseballInputView implements InputView {
 
     private static final String BASEBALL_NUMBER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String CONTINUE_OR_EXIT_INPUT_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static final String CONTINUE_NUMBER = "1";
 
-    public static String inputBaseballNumber() {
+    public String inputBaseballNumber() {
         System.out.print(BASEBALL_NUMBER_INPUT_MESSAGE);
         String input = Console.readLine();
         InputValidator.validateBaseballNumber(input);
         return input;
     }
 
-    public static boolean continueOrExit() {
+    public boolean continueOrExit() {
         System.out.println(CONTINUE_OR_EXIT_INPUT_MESSAGE);
         String input = Console.readLine();
         InputValidator.validateContinueOrExitNumber(input);
