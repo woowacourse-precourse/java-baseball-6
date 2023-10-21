67e61b1121f5388e0404465d7bb3c86f2b3141b6
elyudwo <dudqk9696@naver.com>
2023-10-21 21:35:39 +0900

    [Feat] : 게임 실행 기능 구현
    유저가 입력한 숫자를 정답 숫자와 검증해 스트라이크, 볼을 출력한다.

---
+++ src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,23 @@
+package baseball.controller;
+
+import baseball.domain.ComputerNumber;
+import baseball.domain.UserNumber;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+public class GameController {
+
+    private InputView inputView = new InputView();
+    private OutputView outputView = new OutputView();
+
+    public void startInspection() {
+        ComputerNumber computerNumber = new ComputerNumber();
+        UserNumber userNumber = new UserNumber();
+
+        while(computerNumber.verifyEnd()) {
+            userNumber.setNumber(inputView.printInsertNumber());
+            computerNumber.verifyNumber(userNumber.getNumber());
+            outputView.printResult(computerNumber.getStrike(), computerNumber.getBall());
+        }
+    }
+}
