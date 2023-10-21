505b0510f14953a554f6447a36157582d0b8ee49
junOTL <naqp99@gmail.com>
2023-10-21 15:40:41 +0900

    feat: 같은 수가 전혀 없으면 낫싱 기능 구현

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -4,6 +4,27 @@ import java.util.List;
 
 public class Game {
 
+    private int strike;
+    private int ball;
+
+    public Game() {
+        this.strike = 0;
+        this.ball = 0;
+    }
+
+    public void compareNumbers(List<Integer> computerNumber, List<Integer> playerNumber) {
+        strike = countStrikes(computerNumber, playerNumber);
+        ball = countBalls(computerNumber, playerNumber);
+    }
+
+    public int getStrikes() {
+        return strike;
+    }
+
+    public int getBalls() {
+        return ball;
+    }
+
     public int countStrikes(List<Integer> computerNumber, List<Integer> playerNumber) {
         int strikes = 0;
         for (int i = 0; i < 3; i++) {
