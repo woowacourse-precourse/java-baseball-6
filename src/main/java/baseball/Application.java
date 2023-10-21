87198a462a81e5a39c0e4b3c4b27c673d0a32d1d
euics <pjh575157@gmail.com>
2023-10-21 23:26:36 +0900

    feat: askForNewGame 메서드 구현
    게임 종료 및 재시작 기능 메서드.
    - 'askForNewGame' 메서드를 통해 게임의 종료 또는 재시작 여부 결정.
      - "게임을 새로 시작하려면 1, 종료하려면 2를 입력해주세요." 문구 출력.
      - 사용자의 선택에 따라 게임 재시작 또는 종료.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -98,4 +98,10 @@ public class Application {
 
         return false;
     }
+
+    public static boolean askForNewGame() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        int choice = Integer.parseInt(Console.readLine());
+        return choice == 1;
+    }
 }
