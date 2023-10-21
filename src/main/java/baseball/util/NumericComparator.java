1ae3047c44744ca7bec07d7c4fa3fc13941b2fb8
jeondui <jeondui@gmail.com>
2023-10-21 16:27:40 +0900

    refactor(NumericComparator): 스트라이크, 볼 조건 로직 분리

--- src/main/java/baseball/util/NumericComparator.java
+++ src/main/java/baseball/util/NumericComparator.java
@@ -19,12 +19,20 @@ public class NumericComparator {
             int computerNumber = computerNumbers.get(i);
 
             // 같은 위치에 있는 원소가 똑같을 때 -> 스트라이크 ++
-            if (playerNumber == computerNumber) {
+            if (isStrike(playerNumber, computerNumber)) {
                 strikeCount++;
                 // 같은 위치에 없고, 리스트에 존재는 할 때 -> 볼++
-            } else if (computerNumbers.contains(playerNumber)) {
+            } else if (isBall(playerNumber, computerNumbers)) {
                 ballCount++;
             }
         }
+    }
+
+    private boolean isStrike(int playerNumber, int computerNumber) {
+        return playerNumber == computerNumber;
+    }
+
+    private boolean isBall(int playerNumber, List<Integer> computerNumbers) {
+        return computerNumbers.contains(playerNumber);
     }
 }
