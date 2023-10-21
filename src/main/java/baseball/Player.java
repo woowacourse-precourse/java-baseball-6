3db37cb355d5e624ea3c1fcc2c24047c486cbdf7
Jung Yunho <a01049048063@gmail.com>
2023-10-21 15:54:09 +0900

    feat(Player): 중복된 값이 있는지 검증하는 로직 추가

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,18 +1,22 @@
 package baseball;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.regex.Pattern;
 
 public class Player {
 
-    private static final String REGEX_ONE_TO_NINE ="^[1-9]+$";
+    private static final String REGEX_ONE_TO_NINE = "^[1-9]+$";
     private static final String OUT_OF_ONE_TO_NIME = "[ERROR] 1 ~ 9 사이의 값만 입력해주세요";
+    private static final String HAVE_DUPLICATE_NUMBER = "[ERROR] 중복된 값이 있습니다.";
+
     private List<Integer> inputNumbers;
 
     public Player(String inputNumber) {
         List<Integer> inputNumbers = parsingNumber(inputNumber);
         validateOneToNine(inputNumbers);
+        validateDuplicateNumber(inputNumbers);
         this.inputNumbers = inputNumbers;
     }
 
@@ -34,6 +38,16 @@ public class Player {
             boolean isValid = Pattern.matches(REGEX_ONE_TO_NINE, numberStr);
             if (!isValid) {
                 throw new IllegalArgumentException(OUT_OF_ONE_TO_NIME);
+            }
+        }
+    }
+
+    private void validateDuplicateNumber(List<Integer> Numbers) {
+        HashSet<Integer> uniqueNumbers = new HashSet<>();
+
+        for (Integer number : Numbers) {
+            if (uniqueNumbers.contains(number)) {
+                throw new IllegalArgumentException(HAVE_DUPLICATE_NUMBER);
             }
         }
     }
