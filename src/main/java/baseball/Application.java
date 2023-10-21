117626b8c79be71d037df16691e1236d2eb61a4e
MIRO <regina2504@naver.com>
2023-10-21 13:18:12 +0900

    숫자 야구 게임

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,8 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
+        Game game = new Game();
+        game.gameStart();
         // TODO: 프로그램 구현
     }
 }
