321d1478f5fa37bd2eb47798607a859cf818de33
jae woo Jung <awfjol2008@naver.com>
2023-10-22 03:56:02 +0900

    feat: Player 클래스 getter, setter 메소드 구현

--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -6,4 +6,12 @@ import java.util.List;
 public class Player {
 
     private List<Integer> numbers = new ArrayList<>();
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public void setPlayer(List<Integer> number) {
+        numbers = number; // 컴퓨터의 숫자를 정한다.
+    }
 }
