6806121de9d72af1d0c8aaaabb9ca921d2553e19
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-22 00:13:14 +0900

    refactor(BaseballService):구현 기능 목록 수정

--- src/main/java/baseball/service/BaseballService.java
+++ src/main/java/baseball/service/BaseballService.java
@@ -2,34 +2,13 @@ package baseball.service;
 
 import baseball.domain.BaseballNumber;
 import baseball.domain.BaseballScore;
-import baseball.util.Util;
-import baseball.view.BaseballPrint;
-import camp.nextstep.edu.missionutils.Console;
+import baseball.validator.Validator;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
 // 숫자야구게임의 진행을 담당하는 클래스
 public class BaseballService {
-    public BaseballService() {
-        BaseballPrint.printStartMessage();
-    }
-
-    public void playBaseballGame() {
-        BaseballNumber computerNumber = initializeComputerNumber();
-        while (true) {
-            BaseballNumber userNumber = initializeUserNumber();
-            BaseballScore resultScore = compareBaseballNumber(computerNumber, userNumber);
-            if (resultScore.isStrikeThree()) {
-                BaseballPrint.printEndMessage();
-                break;
-            }
-        }
-        if (isContinueGame()) {
-            playBaseballGame();
-        }
-    }
-
     public BaseballNumber initializeComputerNumber() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
@@ -41,21 +20,25 @@ public class BaseballService {
         return new BaseballNumber(computer);
     }
 
-    private boolean isContinueGame() {
-        BaseballPrint.printRestartMessage();
-        String restartResult = Console.readLine();
-        Util.validateStringIsOneOrTwo(restartResult);
-        return restartResult.equals("1");
+    public boolean isContinueGame(String inputRestartOrEnd) {
+        Validator.validateStringIsOneOrTwo(inputRestartOrEnd);
+        return inputRestartOrEnd.equals("1");
     }
 
     public BaseballScore compareBaseballNumber(BaseballNumber computerNumber, BaseballNumber userNumber) {
         BaseballScore baseballScore = BaseballNumber.compareNumber(computerNumber, userNumber);
-        baseballScore.printBaseballScore();
         return baseballScore;
     }
 
-    public BaseballNumber initializeUserNumber() {
-        BaseballPrint.printInputNumberMessage();
-        return new BaseballNumber(Console.readLine());
+    public BaseballNumber initializeUserNumber(String userNumber) {
+        validateUserNumber(userNumber);
+        return new BaseballNumber(userNumber);
+    }
+
+    private void validateUserNumber(String userNumber) {
+        Validator.validateStringIsNaturalNumber(userNumber, "자연수로만 입력되어야합니다.");
+        Validator.validateStringLength(3, userNumber, "입력의 길이는 3이어야합니다.");
+        Validator.validateStringHasNot("0", userNumber, "입력은 0을 포함할 수 없습니다.");
+        Validator.validateStringAllDifferent(userNumber, "모든 입력된 값은 달라야합니다.");
     }
 }
