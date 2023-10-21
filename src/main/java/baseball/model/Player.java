6217a3500f163cce88353c9ff8266475b0a3462f
dlqudtjs <dlqudtjs922@gmail.com>
2023-10-22 00:38:24 +0900

    feat: player 모델에 setNumbers 기능 추가

--- src/main/java/baseball/model/Player.java
+++ src/main/java/baseball/model/Player.java
@@ -1,8 +1,15 @@
 package baseball.model;
 
+import java.util.List;
+
 public class Player {
 
-    public Player() {
+    private List<Integer> numbers;
 
+    public Player() {
+    }
+
+    public void setNumbers(List<Integer> numbers) {
+        this.numbers = numbers;
     }
 }
