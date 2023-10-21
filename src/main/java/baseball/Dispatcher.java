8981f8f2a5b4b0e20ac94fad801a7dd327cb3407
BullChallenger <am23am45@naver.com>
2023-10-21 12:20:47 +0900

    [FEAT] Dispatcher 클래스 생성
    View와 Controller를 연결하는 역할을 하며 실질적인 게임 진행을 담당하는 클래스이다
    
    Controller에서 데이터 유효성 검증을 진행하기 때문에 Controller를 노출시키는 것 보다는 Controller에 데이터 전달을 담당하는 클래스를 하나 생성하는게 좋을 것 같아서 생성했다

---
+++ src/main/java/baseball/Dispatcher.java
@@ -0,0 +1,32 @@
+package baseball;
+
+import baseball.controller.GameController;
+import baseball.dto.JudgeDTO;
+
+public class Dispatcher {
+
+    private static Dispatcher INSTANCE;
+    private Dispatcher(GameController gameController) {
+        this.gameController = gameController;
+    }
+    public static Dispatcher getInstance(GameController gameController) {
+        if (INSTANCE == null)
+            INSTANCE = new Dispatcher(gameController);
+        return INSTANCE;
+    }
+
+    private final GameController gameController;
+
+    public void dispatchGameStart() {
+        gameController.start();
+    }
+
+    public JudgeDTO dispatchProcess(String input) {
+        return gameController.process(input);
+    }
+
+    public boolean dispatchRetry(String input) {
+        return gameController.isRetry(input);
+    }
+
+}
