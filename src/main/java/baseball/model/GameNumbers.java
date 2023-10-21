7e10c4ca2df9e7118f50f3c8769b20d90ad46b86
haiseong <wjdgotjd9908@gmail.com>
2023-10-22 00:24:34 +0900

    refactor: GameNumber: equals 메서드 삭제

--- src/main/java/baseball/model/GameNumbers.java
+++ src/main/java/baseball/model/GameNumbers.java
@@ -3,22 +3,11 @@ package baseball.model;
 import java.util.List;
 
 public class GameNumbers {
-    private static final int GAME_NUMBERS_SIZE = 3;
+    public static final int GAME_NUMBERS_SIZE = 3;
     private final List<Integer> numbers;
 
     public GameNumbers(List<Integer> list) {
         numbers = list;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        } else if (obj == null || getClass() != obj.getClass()) {
-            return false;
-        }
-        GameNumbers gameNumbers = (GameNumbers) obj;
-        return numbers.equals(gameNumbers.numbers);
     }
 
 }
