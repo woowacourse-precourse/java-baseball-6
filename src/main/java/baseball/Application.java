fc7aabdac7ce7480babdc4cad5095d5e3ac4402c
Chenowoo <cjsgusdn14@naver.com>
2023-10-21 17:15:27 +0900

    Fix: Application 클래스 내 PrintResult 메소드 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -109,6 +109,6 @@ public class Application {
         if(strike==0) {
             return ball + "볼";
         }
-        return ball + "볼" + strike + "스트라이크";
+        return ball + "볼 " + strike + "스트라이크";
     }
 }
