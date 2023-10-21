4cbceb716cdf30ed7c747e5a5644c8713842b9a5
chosunghyun18 <krismon@naver.com>
2023-10-21 17:15:34 +0900

    feat : add check number size in 3

--- src/main/java/baseball/validate/InputValidate.java
+++ src/main/java/baseball/validate/InputValidate.java
@@ -3,6 +3,9 @@ package baseball.validate;
 public class InputValidate {
     public void checkNumbers(String givenNumbers) {
         String[] numbers = givenNumbers.split("");
+        if(numbers.length > 3 || numbers.length < 0){
+            throw new NumberFormatException("[ERROR] Wrong Size");
+        }
         for(String number : numbers) {
             checkNumber(number);
         }
