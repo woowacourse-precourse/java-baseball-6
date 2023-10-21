c9bb41436b71ce702f2006482afb80bab22510e4
euics <pjh575157@gmail.com>
2023-10-21 23:24:53 +0900

    feat: displayGameResult 메서드 구현
    플레이 결과 출력 메서드.
    - 'displayGameResult' 메서드를 통해 게임의 결과를 화면에 출력.
      - 3스트라이크인 경우 게임 종료 문구 출력.
      - 스트라이크 및 볼 수에 따라 해당 결과를 화면에 출력.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -77,4 +77,25 @@ public class Application {
 
         return displayGameResult(balls, strikes);
     }
+
+    public static boolean displayGameResult(int balls, int strikes) {
+        if (strikes == 3) {
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        } else if (strikes == 0 && balls == 0) {
+            System.out.println("낫싱");
+        } else {
+            if (balls > 0) {
+                System.out.print(balls + "볼 ");
+            }
+            if (strikes > 0) {
+                System.out.println(strikes + "스트라이크");
+            } else {
+                System.out.println();
+            }
+        }
+
+        return false;
+    }
 }
