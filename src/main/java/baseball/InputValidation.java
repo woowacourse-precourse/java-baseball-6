8844fb537acf25f782f0ff60e26cdeb6922488c0
sohyun9527 <sohyun95277@gmail.com>
2023-10-21 17:06:03 +0900

    refactor(InputValidation): 접근제어자 private 추가, 테스트 코드 수정 validation은 내부에서만 검증을 마치기 때문에 BaseballGame 클래스에서 사용하는 메서드를 제외하고 모두 접근제어자 private를 붙임. 이에 따라 테스트 코드에도 Method 객체를 이용해 임의로 접근 권한을 열고, invoke를 사용해 메소드 사용, 에러 발생 시 invokeException이 발생하므로 내부에 getCause를 사용해 원본 exception으로 검증

--- src/main/java/baseball/InputValidation.java
+++ src/main/java/baseball/InputValidation.java
@@ -24,18 +24,18 @@ public class InputValidation {
     }
 
     // 길이에 대한 검증
-    public void validateLength(String input) {
+    private void validateLength(String input) {
         if (input.length() != 3) {
             throw new IllegalArgumentException("3개의 숫자만 입력해주세요.");
         }
     }
 
-    public String deleteSpace(String input) {
+    private String deleteSpace(String input) {
         return input.replace(" ", "");
     }
 
     // 숫자로만 이루어져있는지 검증
-    public void validateOnlyDigit(String input) {
+    private void validateOnlyDigit(String input) {
         boolean checkOnlyDigit = input.chars().allMatch(Character::isDigit);
 
         if (!checkOnlyDigit) {
@@ -44,40 +44,35 @@ public class InputValidation {
     }
 
     // 0을 포함했는지 검증
-    public void validateContainZero(String input) {
+    private void validateContainZero(String input) {
         if (input.contains("0")) {
             throw new IllegalArgumentException("1 ~ 9 사이의 숫자만 입력해주세요.");
         }
     }
 
     // 중복수에 대한 검증
-    public void validateDuplicateNumber(String input) {
+    private void validateDuplicateNumber(String input) {
         if (input.chars().distinct().count() < 3) {
             throw new IllegalArgumentException("중복된 수는 입력할 수 없습니다");
         }
     }
 
     // 문자열 -> 리스트 변환
-    /*
-    * 드디어 stream 써보는군아~~
-    * 타입 변환할 때, for문을 돌면서 작업할 때. 공부하던거 드디어 쓴당!!
-    * 머가 더 가독성 좋을까??
-    * */
-    public List<Integer> convertStrToIntegerList(String input) {
+    private List<Integer> convertStrToIntegerList(String input) {
         return input.chars()
                 .mapToObj(Character::getNumericValue)
                 .collect(Collectors.toList());
     }
 
-    public void validateSign(String input) {
+    private void validateSign(String input) {
         if (input.charAt(0) != '1' && input.charAt(0) != '2') {
             throw new IllegalArgumentException("재시작은 1, 게임 종료는 2입니다.");
         }
     }
 
-    public void validateSignLength(String input) {
+    private void validateSignLength(String input) {
         if (input.length() != 1) {
-            throw new IllegalArgumentException("재시작은 1, 게임 종료는 2입니다.");
+            throw new IllegalArgumentException("하나의 숫자만 입력해 주세요");
         }
     }
 }
