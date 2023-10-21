42a9d6e4bdf6d0a7c1a63617b2b23ecd1e232de3
InGyu Moon <ansdlsrb1121@naver.com>
2023-10-21 22:54:55 +0900

    feat: main문 프로그램 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,11 @@
 package baseball;
 
+import baseball.controller.NumberBaseBallGame;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
+        NumberBaseBallGame game = new NumberBaseBallGame();
+        game.game();
     }
 }
