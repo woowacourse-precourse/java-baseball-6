22bbe377ab82af674e5b8a1da383101112186121
oxdjww <oxdjww@gmail.com>
2023-10-21 23:20:42 +0900

    feat : checkStrikes(), checkBalls() 로직 수정 및 안내말 추가

--- src/main/java/baseball/model/BaseballGame.java
+++ src/main/java/baseball/model/BaseballGame.java
@@ -12,58 +12,81 @@ public class BaseballGame {
     private final List<Integer> user = new ArrayList<>();
     private final BaseballGameCounts baseballGameCounts = BaseballGameCounts.createBaseballGameCounts();
 
-    protected BaseballGame() {}
+    protected BaseballGame() {
+    }
 
     public static BaseballGame createBaseballGame() {
         return new BaseballGame();
     }
 
     public void run() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
         // 랜덤 수 생성
         this.generateRandomNum();
 
         while (true) {
             this.getUserInput();
 
-            this.checkBalls();
-
             this.checkStrikes();
+
+            this.checkBalls();
 
             this.baseballGameCounts.showCounts();
 
             if (this.baseballGameCounts.isWinCondition()) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
                 return;
             }
+
+            this.baseballGameCounts.resetCounts();
+
+            this.resetUserInput();
         }
     }
 
     private void getUserInput() {
+        System.out.print("숫자를 입력해주세요 : ");
         String userInput = readLine();
+        if (userInput.length() != 3) throw new IllegalArgumentException("잘못된 입력입니다.");
         for (int i = 0; i < userInput.length(); i++) {
-            user.add(Character.getNumericValue(userInput.charAt(i)));
+            int num = Character.getNumericValue(userInput.charAt(i));
+            if (!user.contains(num)) {
+                user.add(num);
+            } else {
+                throw new IllegalArgumentException("잘못된 입력입니다.");
+            }
         }
+    }
+
+    private void resetUserInput() {
+        user.clear();
     }
 
     private void checkBalls() {
         int counts = 0;
         for (Integer idx : user) {
-            if (computer.contains(user.get(idx)) && !Objects.equals(computer.get(idx), user.get(idx))) {
+            if (computer.contains(idx)) {
                 counts++;
             }
         }
-        if(counts > 0)
-            baseballGameCounts.editMessage(String.format("%볼 ",counts));
+        counts -= this.baseballGameCounts.getStrikes();
+        if (counts > 0) {
+            baseballGameCounts.editMessage(String.format("%d볼 ", counts));
+            baseballGameCounts.setBalls(counts);
+        }
     }
 
     private void checkStrikes() {
         int counts = 0;
-        for (Integer idx : user) {
+        for (int idx = 0; idx < user.size(); idx++) {
             if (user.get(idx).equals(computer.get(idx))) {
                 counts++;
             }
         }
-        if(counts > 0)
-            baseballGameCounts.editMessage(String.format("%d스트라이크",counts));
+        if (counts > 0) {
+            baseballGameCounts.editMessage(String.format("%d스트라이크", counts));
+            baseballGameCounts.setStrikes(counts);
+        }
     }
 
     private void generateRandomNum() {
