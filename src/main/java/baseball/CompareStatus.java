17fe31dee8fcb67826ea585207a4a614c9c5b9dc
Jiyoon Kim <rlawldbs0911@naver.com>
2023-10-21 19:32:02 +0900

    feat(CompareStatus): 입력값 결과에 대한 결과값을 갖는 enum 생성

--- src/main/java/baseball/CompareStatus.java
+++ src/main/java/baseball/CompareStatus.java
@@ -1,7 +1,18 @@
 package baseball;
 
 public enum CompareStatus {
-    STRIKE,
-    BALL,
-    NOTHING
+    BALL("볼"),
+    STRIKE("스트라이크"),
+    NOTHING("낫싱");
+
+    private final String value;
+
+    CompareStatus(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
 }
