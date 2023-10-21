a4eec136c6ab304578fcec0c0d22847052776aee
kim <hwiyul_g@daum.net>
2023-10-21 16:59:44 +0900

    test(UserNumber) : UserNumber클래스의 테스트 추가

---
+++ src/test/java/study/UserNumberTest.java
@@ -0,0 +1,38 @@
+package study;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import baseball.UserNumber;
+import org.junit.jupiter.api.Test;
+
+class UserNumberTest {
+
+    @Test
+    void invalidLengthTest(){
+        assertThrows(IllegalArgumentException.class, () -> new UserNumber("1234"));
+    }
+
+    @Test
+    void duplicateNumberTest(){
+        assertThrows(IllegalArgumentException.class, () -> new UserNumber("112"));
+    }
+
+    @Test
+    void nonDigitInputTest() {
+        assertThrows(IllegalArgumentException.class, () -> new UserNumber("1a2"));
+    }
+
+    @Test
+    void outOfRangeTest() {
+        assertThrows(IllegalArgumentException.class, () -> new UserNumber("012"));
+    }
+
+    @Test
+    void validInputTest() {
+        UserNumber userNumber = new UserNumber("123");
+        assertEquals(3, userNumber.getUserNumber().size());
+        assertEquals(1, userNumber.getUserNumber().get(0));
+        assertEquals(2, userNumber.getUserNumber().get(1));
+        assertEquals(3, userNumber.getUserNumber().get(2));
+    }
+}
