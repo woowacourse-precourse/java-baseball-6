63a1c794da4615ed0239d763f6d96183226b2d3e
mainfn <_@mainfn.dev>
2023-10-22 02:36:52 +0900

    fixed: add omitted output message

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,7 +4,8 @@ import baseball.domain.dto.GuessResult;
 
 public final class OutputView {
 
-  public void printResult(final GuessResult result) {
-    System.out.println(result.renderToString());
-  }
+    public void printResult(final GuessResult result) {
+        System.out.println(result.renderToString());
+        System.out.println("게임 종료");
+    }
 }
