3cf3366c7511f0509e79b78dd82d3d014144f6a4
ParkDH0809 <znxn1237@gmail.com>
2023-10-21 20:57:03 +0900

    feat(출력): 게임 시작 문구 출력 구현

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,9 @@
+package baseball.view;
+
+public class OutputView {
+    private static final String OUTPUT_START_PROGRAM = "숫자 야구 게임을 시작합니다.";
+
+    public void outputStartProgram() {
+        System.out.println(OUTPUT_START_PROGRAM);
+    }
+}
