5fef9a9f1b2e4e2de4f88b0ad9c7484770a873ea
TaeHye0n <thk001229@gmail.com>
2023-10-21 20:16:37 +0900

    feat: 정답을 맞췄을 경우 게임 종료 메시지 출력 기능  - 종료 메시지와 동시에 재시작할지 종료할지 요구하는 메시지도 출력

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,6 +4,9 @@ public class OutputView {
 
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String GAME_INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String GAME_FINISHED_MESSAGE = "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String GAME_RESTART_OR_END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려 2를 입력하세요.";
+    private static final int MAX_NUMBER_LENGTH = 3;
 
     public void printStartMessage() {
         System.out.println(GAME_START_MESSAGE);
@@ -11,5 +14,13 @@ public class OutputView {
 
     public void printInputNumberMessage() {
         System.out.print(GAME_INPUT_NUMBER_MESSAGE);
+    }
+
+    public void printFinishMessage() {
+        System.out.println(MAX_NUMBER_LENGTH + GAME_FINISHED_MESSAGE);
+    }
+
+    public void printRestartOrEndMessage() {
+        System.out.println(GAME_RESTART_OR_END_MESSAGE);
     }
 }
