a3b5cc9a1c7694a2ae63bd5026902738c70b6cf3
kimdohyung <pdohyung@gmail.com>
2023-10-22 01:05:20 +0900

    refactor: 기존 view 메서드를 static 메서드로 수정

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,27 +1,27 @@
 package baseball.view;
 
 public class OutputView {
-    public void gameStart() {
+    public static void gameStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    public void gameFinish() {
+    public static void gameFinish() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
-    public void onlyBall(int ball) {
+    public static void onlyBall(int ball) {
         System.out.println(ball + "볼");
     }
 
-    public void onlyStrike(int strike) {
+    public static void onlyStrike(int strike) {
         System.out.println(strike + "스트라이크");
     }
 
-    public void strikeAndBall(int strike, int ball) {
+    public static void strikeAndBall(int strike, int ball) {
         System.out.println(ball + "볼 " + strike + "스트라이크");
     }
 
-    public void nothing() {
+    public static void nothing() {
         System.out.println("낫싱");
     }
 }
