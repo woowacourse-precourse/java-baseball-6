56435f1eb8af1ab8aa3199107970c970786b75d2
sungwon2598 <tjddnjs2598@gmail.com>
2023-10-21 20:14:44 +0900

    refactor(RandomNumbersGenerator) : 매직넘버 제거

--- src/main/java/baseball/Controller/RandomNumbersGenerator.java
+++ src/main/java/baseball/Controller/RandomNumbersGenerator.java
@@ -5,11 +5,14 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 
 public class RandomNumbersGenerator {
+    private static final int COMPUTERNUMBERS_LENGTH = 3;
+    private static final int NUMBER_RANGE_START = 1;
+    private static final int NUMBER_RANGE_END = 9;
 
     public ArrayList<Integer> generateNumbers() {
         ArrayList<Integer> computernumbers = new ArrayList<>();
-        while (computernumbers.size() < 3) {
-            int randomnumber = Randoms.pickNumberInRange(1, 9);
+        while (computernumbers.size() < COMPUTERNUMBERS_LENGTH) {
+            int randomnumber = Randoms.pickNumberInRange(NUMBER_RANGE_START, NUMBER_RANGE_END);
                 if (!computernumbers.contains(randomnumber))
                      computernumbers.add(randomnumber);
                 }
