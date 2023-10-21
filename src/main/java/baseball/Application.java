6dd084728f0132b3442a6ea925c627fd57872a2e
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2023-10-21 18:34:20 +0900

    feat : start game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
+import view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        boolean startGame = true;
+        while (startGame) {
+            startGame();
+        }
+    }
+
+    static void startGame() {
+        OutputView.printStartMessage();
     }
 }
---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,9 @@
+package view;
+
+public class OutputView {
+    static String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+
+    public static void printStartMessage() {
+        System.out.println(GAME_START_MESSAGE);
+    }
+}
