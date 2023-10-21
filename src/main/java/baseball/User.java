1540a63ca336c91b635f33c472a25e18b2d452bc
이준희 <juni416@naver.com>
2023-10-21 21:17:21 +0900

    feature: 유저가 숫자를 입력시 메모리에 리스트형식으로 저장

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -3,14 +3,33 @@ package baseball;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class User {
-    static String readLine() throws IOException {
+    List<Integer> number = new ArrayList<>();
+    public String readLine() {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        String result = in.readLine();
+        String result;
+        try {
+            result = in.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         if(result.length() != 3){
             throw new IllegalArgumentException("3자리 숫자를 입력해주세요.");
         }
+        number = convertStringToIntegerArray(result);
         return result;
+    }
+
+    private ArrayList<Integer> convertStringToIntegerArray(String result) {
+        ArrayList<Integer> userNumber = new ArrayList<>();
+        String[] splitResult = result.split("");
+        Stream<String> stream = Arrays.stream(splitResult);
+        stream.forEach(s -> userNumber.add(Integer.parseInt(s)));
+        return userNumber;
     }
 }
