a8b61dd64692b82dbc5a0c8e1604f44fe4061dd7
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2023-10-21 19:13:28 +0900

    feat : count number of balls

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -18,12 +18,25 @@ public class Application {
         List<Integer> computer = BaseballNumberGenerator.generateThreeDigits();
         List<Integer> userNumbers = InputView.getThreeDigits();
         int strikeCounts = getStrikeCounts(computer, userNumbers);
+        int ballCounts = getBallCounts(computer, userNumbers);
     }
 
     static int getStrikeCounts(List<Integer> computer, List<Integer> userNumbers) {
         int count = 0;
         for (int i = 0; i < computer.size(); ++i) {
             if (computer.get(i) == userNumbers.get(i)) {
+                ++count;
+            }
+        }
+        return count;
+    }
+
+    static int getBallCounts(List<Integer> computer, List<Integer> userNumbers) {
+        int count = 0;
+        for (int i = 0; i < computer.size(); ++i) {
+            int computerNumber = computer.get(i);
+            int userNumber = userNumbers.get(i);
+            if (computerNumber != userNumber && computer.contains(userNumber)) {
                 ++count;
             }
         }
