aa4c188b7952b13a7129f9443d5a2be9d7f170d8
hojang <109591135+26solitude@users.noreply.github.com>
2023-10-21 22:07:04 +0900

    입력받은 숫자와 컴퓨터의 숫자를 비교 및 조건에 따른 결과를 출력(2)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -51,7 +51,12 @@ public class Application {
 
 
             System.out.println("computer = " + computer + " user = " + userNum);
-            System.out.println(ball + "볼 " + strike + "스트라이크");
+            if(ball == 0 && strike == 0) System.out.println("낫싱");
+            else if(strike == 3) {
+                System.out.println("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            }
+            else System.out.println(ball + "볼 " + strike + "스트라이크");
         }
     }
 }
