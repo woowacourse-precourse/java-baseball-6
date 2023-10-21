8a934d6640d502af2e0e1ebe2cd83c57c7324ef3
Sasha <106591445+kchaeys2@users.noreply.github.com>
2023-10-21 17:28:06 +0900

    feat(InputJudgmentService) : 입력값 판정 클래스 생성

---
+++ src/main/java/baseball/Service/InputJudgmentService.java
@@ -0,0 +1,11 @@
+package baseball.Service;
+
+import baseball.DTO.User;
+
+public class InputJudgmentService {
+    private final User user;
+
+    public InputJudgmentService(User user){
+        this.user = user;
+    }
+}
