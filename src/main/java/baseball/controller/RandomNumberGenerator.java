b4abef94bb0906ce8d137d79f1f47b984b3dfcca
정성욱 <rkfka7849@gmail.com>
2023-10-21 16:54:23 +0900

    fix(RandomNumberGenerator): 중복되는 난수 발생 시 자릿수가 줄어드는 현상 수정

--- src/main/java/baseball/controller/RandomNumberGenerator.java
+++ src/main/java/baseball/controller/RandomNumberGenerator.java
@@ -1,19 +1,19 @@
 package baseball.controller;
 
-import baseball.system.SystemConstant;
+import static java.util.stream.Collectors.toList;
+
 import baseball.domain.BallNumber;
 import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.ArrayList;
 import java.util.stream.IntStream;
 
 public class RandomNumberGenerator {
     static public BallNumber generate() {
-        ArrayList<Integer> computer = new ArrayList<>();
-        IntStream.rangeClosed(1, SystemConstant.MAXIMUM_DIGIT)
-                 .mapToObj(i -> Randoms.pickNumberInRange(1, 9))
-                 .distinct()
-                 .forEach(computer::add);
+        ArrayList<Integer> computer = (ArrayList<Integer>) IntStream.generate(() -> Randoms.pickNumberInRange(1, 9))
+                .distinct()
+                .limit(3)
+                .boxed()
+                .collect(toList());
         return new BallNumber(computer);
     }
 }
