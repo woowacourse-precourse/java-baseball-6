688ee6b2bc273ea6f9575fa862d1befdbbe74290
chosunghyun18 <krismon@naver.com>
2023-10-21 17:16:16 +0900

    feat : add start poin in application main

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,10 @@
 package baseball;
 
+import baseball.controller.GameController;
+
 public class Application {
     public static void main(String[] args) {
-
+        GameController game = GameController.StartGame();
+        game.playGame();
     }
 }
