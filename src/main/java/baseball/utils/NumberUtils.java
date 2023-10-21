828c3750519a8cde469b232b23885291fde99488
LeeJaeHoon <dlwogns3413@naver.com>
2023-10-21 18:02:18 +0900

    refactor: Utils class들 생성자 private로 변경

--- src/main/java/baseball/utils/NumberUtils.java
+++ src/main/java/baseball/utils/NumberUtils.java
@@ -2,6 +2,9 @@ package baseball.utils;
 
 
 public class NumberUtils {
+
+    private NumberUtils() {}
+
     public static boolean isNumeric(String str) {
         return str.matches("\\d+");
     }
--- src/main/java/baseball/utils/StringUtils.java
+++ src/main/java/baseball/utils/StringUtils.java
@@ -1,7 +1,10 @@
 package baseball.utils;
 
 public class StringUtils {
+
+    private StringUtils() {}
     public static boolean hasDuplicateCharacters(String str) {
         return str.length() != str.chars().distinct().count();
     }
+
 }
