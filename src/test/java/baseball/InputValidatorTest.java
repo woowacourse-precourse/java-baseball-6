1c5fac883fc7ba5adb7050e0d32c55baa93e8fce
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-10-21 17:03:47 +0900

    test(InputValidatorTest) : isAllDigit 메소드 테스트 작성
    인자의 문자열이 1-9의 정수로 구성되어있을 경우와 아닌 경우 2가지 테스트 작성

--- src/test/java/baseball/InputValidatorTest.java
+++ src/test/java/baseball/InputValidatorTest.java
@@ -20,8 +20,24 @@ public class InputValidatorTest {
     void isTreeLengthLetterTest_caseFalse() {
         String playerInput1 = "가나다라";
         String playerInput2 = "12";
-        
+
         assertThat(inputValidator.isTreeLengthLetter(playerInput1)).isFalse();
         assertThat(inputValidator.isTreeLengthLetter(playerInput2)).isFalse();
+    }
+
+    @Test
+    void isAllDigitTest_caseTrue() {
+        String playerInput = "4424";
+
+        assertThat(inputValidator.isAllDigit(playerInput)).isTrue();
+    }
+
+    @Test
+    void isAllDigitTest_caseFalse() {
+        String playerInput1 = "english";
+        String playerInput2 = "한글";
+
+        assertThat(inputValidator.isAllDigit(playerInput1)).isFalse();
+        assertThat(inputValidator.isAllDigit(playerInput2)).isFalse();
     }
 }
