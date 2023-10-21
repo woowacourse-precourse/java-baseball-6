bec927068a520f8fd44c2fe024a1a026f23368b2
koomchang <leahpar0401@gmail.com>
2023-10-21 16:20:58 +0900

    feat: Number 값 객체에 범위와 관련된 validation 추가

--- src/main/java/baseball/model/Number.java
+++ src/main/java/baseball/model/Number.java
@@ -4,10 +4,20 @@ import java.util.Objects;
 
 public class Number {
 
+    private final int MIN_VALUE = 1;
+    private final int MAX_VALUE = 9;
+
     private final int value;
 
     public Number(int value) {
         this.value = value;
+        validateValueRange(value);
+    }
+
+    private void validateValueRange(int value) {
+        if (value < MIN_VALUE || value > MAX_VALUE) {
+            throw new IllegalArgumentException("1 에서 9 사이의 값이 아닙니다");
+        }
     }
 
     @Override
