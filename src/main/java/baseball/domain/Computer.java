2f8b4b5225277c61127b84f9f64e7342d5e5bf3c
민주 <hinote444@gmail.com>
2023-10-21 17:23:22 +0900

    feat(domain): 컴퓨터와 플레이어의 숫자 3개를 저장하는 기능 구현

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,4 +1,15 @@
 package baseball.domain;
 
+import java.util.List;
+
 public class Computer {
+    private List<Integer> baseballNumber;
+
+    public void setBaseballNumber(List<Integer> baseballNumber) {
+        this.baseballNumber = baseballNumber;
+    }
+
+    public List<Integer> getBaseballNumber() {
+        return baseballNumber;
+    }
 }
