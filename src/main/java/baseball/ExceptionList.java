3aaad40192077105044f2d35c23f77947f414a73
Chenowoo <cjsgusdn14@naver.com>
2023-10-21 14:37:54 +0900

    Feat: checkDuplicateNumber 메소드 구현

--- src/main/java/baseball/ExceptionList.java
+++ src/main/java/baseball/ExceptionList.java
@@ -5,12 +5,32 @@ import java.util.List;
 public class ExceptionList {
     public static void validNumbers(List<Integer> numbers) {
         if(numbers.size() != 3) {
-            throw new IllegalArgumentException("예외 발생");
+            throw new IllegalArgumentException("numbers사이즈 예외 발생");
         }
         if(checkDuplicateNumber(numbers)) {
 
         }
     }
 
+    public static boolean checkDuplicateNumber(List<Integer> numbers) {
+        int numbersIdx = 0;
+        int checkNumberIdx = 0;
+
+        while(numbersIdx != numbers.size()) {
+            int searchNumber = numbers.get(numbersIdx);
+            int checkNumber = numbers.get(checkNumberIdx);
+
+            if(numbersIdx == checkNumberIdx) {
+                numbersIdx++;
+            }
+            if(checkNumberIdx == numbers.size()-1) {
+                checkNumberIdx = 0;
+                numbersIdx++;
+            }
+            if(searchNumber == checkNumber) return true;
+            else checkNumberIdx++;
+        }
+        return false;
+    }
 
 }
