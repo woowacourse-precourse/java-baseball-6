e9315a2c0280fa0d24ffa3101d0716cb24f3471c
Jiwon Kwon <kjiwon411@gmail.com>
2023-10-22 00:50:53 +0900

    랜덤 수 생성, 예외 처리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,48 @@
 package baseball;
 
+import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    public static String targetGenerator() {
+        String target = "";
+        for (int i=0;i<3;i++){
+            int t = Randoms.pickNumberInRange(1,9);
+            String.valueOf(t);
+            target = target + t;
+        }
+        return target;
+    }
+
+    public static void game (String target) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            String player = Console.readLine();
+            IllegalArgumentTest(player);
+        }
+        catch (IllegalArgumentException e){
+            System.out.println("비정상적인 숫자가 입력되었으므로 종료 ~~~");
+
+        }
+        sc.close();
+    }
+    public static void IllegalArgumentTest(String playerNumber ) throws IllegalArgumentException{
+
+        if (playerNumber.length() != 3) {
+            throw new IllegalArgumentException();
+    }
+    public static void init() {
+        System.out.println("숫자 야구 게임을 시작합니다");
+        String target = targetGenerator();
+        game(target);
+        // baseballgame
+
+
+
+    }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        init();
     }
 }
