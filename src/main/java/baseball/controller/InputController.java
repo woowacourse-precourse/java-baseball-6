1546d9ae1a795f45c33094f37cd20cbdb6293c3a
devholic22 <hyunjoon.tech@gmail.com>
2023-10-22 01:07:33 +0900

    Refactor: 단순 호출 함수 없애기
    기능과 단순 호출을 구분하기 위해 단순 호출용 함수는 제거하도록 수정

--- src/main/java/baseball/controller/InputController.java
+++ src/main/java/baseball/controller/InputController.java
@@ -12,30 +12,16 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputController {
 
     public static int receiveUserNumber() {
-        String userInput = receivePlayNumberWithLength(PLAY_NUMBER_DIGIT);
+        String userInput = Console.readLine();
+        NumberValidator.assertInputNumberWithLength(userInput, PLAY_NUMBER_DIGIT);
 
         return StringInputConverter.toInt(userInput);
     }
 
-    private static String receivePlayNumberWithLength(final int length) {
-        String userInput = Console.readLine();
-        assertNumberValueWithLength(userInput, length);
-
-        return userInput;
-    }
-
-    private static void assertNumberValueWithLength(final String inputNumber, final int expectedLength) {
-        NumberValidator.assertInputNumberWithLength(inputNumber, expectedLength);
-    }
-
     public static int receiveResumeNumber() {
         String resumeInput = Console.readLine();
-        assertResumeValue(resumeInput, PLAY_WANT, END_WANT);
+        ResumeValidator.assertResumeInput(resumeInput, PLAY_WANT, END_WANT);
 
         return StringInputConverter.toInt(resumeInput);
-    }
-
-    private static void assertResumeValue(final String resume, final int restart, final int end) {
-        ResumeValidator.assertResumeInput(resume, restart, end);
     }
 }
