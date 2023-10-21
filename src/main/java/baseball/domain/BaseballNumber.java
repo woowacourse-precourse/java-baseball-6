321f4c482a9a80bb7dd3b76e73135616d09de56f
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-22 00:12:46 +0900

    refactor(BaseballNumber):구현 기능 목록 수정

--- src/main/java/baseball/domain/BaseballNumber.java
+++ src/main/java/baseball/domain/BaseballNumber.java
@@ -8,28 +8,14 @@ public class BaseballNumber {
     private List<Integer> number;
 
     public BaseballNumber(String number) {
-        validateInputString(number);
+
         this.number = Util.stringToIntegerList(number);
     }
 
     public BaseballNumber(List<Integer> computerNumber) {
         this.number = computerNumber;
     }
-
-    @Override
-    public String toString() {
-        return "BaseballNumber{" +
-                "number=" + number +
-                '}';
-    }
-
-    private static void validateInputString(String input) {
-        Util.validateStringLength(3, input, "입력의 길이는 3이어야합니다.");
-        Util.validateStringIsNaturalNumber(input, "자연수로만 입력되어야합니다.");
-        Util.validateStringHasNot("0", input, "입력은 0을 포함할 수 없습니다.");
-        Util.validateStringAllDifferent(input, "모든 입력된 값은 달라야합니다.");
-    }
-
+     
     public static BaseballScore compareNumber(BaseballNumber computerNumber, BaseballNumber userNumber) {
         BaseballScore baseballScore = new BaseballScore(0, 0);
         List<Integer> computerNumberList = computerNumber.number;
