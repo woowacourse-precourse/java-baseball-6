8b1e1f4940a70a41f56ed54fa195c834b61f822c
ykmxxi <ykmxxi97@gmail.com>
2023-10-21 14:53:31 +0900

    feat(Player): 숫자인지 검증하는 기능 구현

--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -2,14 +2,19 @@ package baseball.domain;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Player {
 
 	private static final String DELIMITER = "";
+	public static final String NUMBER_FORMAT = "\\d+";
+	public static final String NOT_NUMBER_ERROR_MESSAGE = "숫자가 아닙니다.";
 
 	private Balls balls;
 
 	public Player(final String numbers) {
+		validate(numbers);
 		this.balls = new Balls(mapToList(numbers));
 	}
 
@@ -17,6 +22,18 @@ public class Player {
 		return Arrays.stream(numbers.split(DELIMITER))
 					 .map(Integer::parseInt)
 					 .toList();
+	}
+
+	private void validate(final String numbers) {
+		validateNumber(numbers);
+	}
+
+	private void validateNumber(final String numbers) {
+		Pattern pattern = Pattern.compile(NUMBER_FORMAT);
+		Matcher matcher = pattern.matcher(numbers);
+		if (!matcher.matches()) {
+			throw new IllegalArgumentException(NOT_NUMBER_ERROR_MESSAGE);
+		}
 	}
 
 }
--- src/test/java/baseball/domain/PlayerTest.java
+++ src/test/java/baseball/domain/PlayerTest.java
@@ -1,6 +1,7 @@
 package baseball.domain;
 
 import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -11,6 +12,14 @@ class PlayerTest {
 	@ValueSource(strings = {"123", "456", "789"})
 	void givenNumbers_whenCreatePlayer_thenNoException(String numbers) {
 		assertThatNoException().isThrownBy(() -> new Player(numbers));
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"12삼", "1@3", "12e"})
+	void givenInput_whenValidateNumber_thenThrowIllegalArgumentException(String input) {
+		assertThatThrownBy(() -> new Player(input))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("숫자가 아닙니다.");
 	}
 
 }
