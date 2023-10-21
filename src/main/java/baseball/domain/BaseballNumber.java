d7142d9e836342396cd5276886fadcc88b4fa7fd
yuseonjun <seonjun0906@gmail.com>
2023-10-21 22:38:37 +0900

    refactor: 접근자와 상수화 위치 및 static 제거

--- src/main/java/baseball/domain/BaseballNumber.java
+++ src/main/java/baseball/domain/BaseballNumber.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 public class BaseballNumber {
 
+	private static final int MAX_NUMBER_SIZE = 3;
+	private static final int MIN_NUMBER_RANGE = 1;
+	private static final int MAX_NUMBER_RANGE = 9;
+
 	private final List<Integer> numbers;
 
 	public BaseballNumber(List<Integer> numbers) {
@@ -19,28 +23,13 @@ public class BaseballNumber {
 		return numbers.contains(playerNumber);
 	}
 
-	private static final int MAX_NUMBER_SIZE = 3;
-	private static final int MIN_NUMBER_RANGE = 1;
-	private static final int MAX_NUMBER_RANGE = 9;
-
-	public static void validateNumber(List<Integer> numbers) {
-		validateComputerNumberSize(numbers);
+	private void validateNumber(List<Integer> numbers) {
+		validateNumberSize(numbers);
 		validateDuplicateNumber(numbers);
 		validateNumberRange(numbers);
 	}
 
-	private static void validateNumberRange(List<Integer> numbers) {
-		if (isOverRange(numbers)) {
-			throw new IllegalArgumentException("숫자가 범위에 벗어났습니다.");
-		}
-	}
-
-	private static boolean isOverRange(List<Integer> numbers) {
-		return numbers.stream()
-			.anyMatch(number -> number < MIN_NUMBER_RANGE || number > MAX_NUMBER_RANGE);
-	}
-
-	private static void validateComputerNumberSize(List<Integer> numbers) {
+	private void validateNumberSize(List<Integer> numbers) {
 		if (numbers.size() != MAX_NUMBER_SIZE) {
 			throw new IllegalArgumentException("숫자 개수의 오류가 발생하였습니다.");
 		}
@@ -57,5 +46,16 @@ public class BaseballNumber {
 			.distinct()
 			.count();
 		return distinctCount != numbers.size();
+	}
+
+	private void validateNumberRange(List<Integer> numbers) {
+		if (isOverRange(numbers)) {
+			throw new IllegalArgumentException("숫자가 범위에 벗어났습니다.");
+		}
+	}
+
+	private boolean isOverRange(List<Integer> numbers) {
+		return numbers.stream()
+			.anyMatch(number -> number < MIN_NUMBER_RANGE || number > MAX_NUMBER_RANGE);
 	}
 }
