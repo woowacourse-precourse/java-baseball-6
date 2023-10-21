f9d66aa16a775427036d604f867a2674c3d8a7f9
jinsu4755 <parkjinsu4755@gmail.com>
2023-10-22 03:20:31 +0900

    feat: 숫자 야구 숫자 배열 Wrapper 작성

---
+++ src/main/java/baseball/domain/BaseBallNumbers.java
@@ -0,0 +1,13 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class BaseBallNumbers {
+
+    private final List<Integer> baseballNumbers;
+
+    private BaseBallNumbers(List<Integer> numbers) {
+        this.baseballNumbers = numbers;
+    }
+
+}
