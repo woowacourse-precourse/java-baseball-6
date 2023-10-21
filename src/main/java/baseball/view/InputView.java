84f29ad8eb8b77d353b25b9040708faad458925b
kimdohyung <pdohyung@gmail.com>
2023-10-21 23:58:28 +0900

    feat: 사용자가 종료 및 재시작을 입력하는 기능 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -3,9 +3,13 @@ package baseball.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-
-	public String userNumberInput(){
+	public String userNumberInput() {
 		System.out.print("숫자를 입력해주세요. : ");
+		return Console.readLine();
+	}
+
+	public String againOrStopInput() {
+		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 		return Console.readLine();
 	}
 }
