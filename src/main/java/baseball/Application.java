eb30074bf69810cab4801934f14cb4550fd35ae0
이승렬 <ibban9810@icloud.com>
2023-10-21 17:58:41 +0900

    Feat: MVP Feature 작성
    1. 먼저 개발하기 전에 어떻게 돌아가는지 보깅 위한 간단한 MVP 작성
    2. 어떠한 동작들이 어떻게 이루어지는지, 그리고 이 코드들을 어떻게 분리해서 작성할 것인지 생각.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,68 @@
 package baseball;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Scanner scanner = new Scanner(System.in);
+        int[] player = new int[3];
+        int[] computer = new int[3];
+        int flag = 1;
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        for (int i = 0; i < 3; i++) {
+            computer[i] = (int) (Math.random() * 10);
+        }
+
+        System.out.println(Arrays.toString(computer));
+
+        // 1. 게임시작
+        while (flag == 1) {
+
+            System.out.print("숫자를 입력해주세요 : ");
+
+            String tmp = scanner.next();
+            for (int i = 0; i < 3; i++) {
+                player[i] = tmp.charAt(i) - '0';
+            }
+
+            int strikeCount = 0;
+            int ballCount = 0;
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (computer[i] == player[j] && i == j) strikeCount++;
+                    else if (computer[i] == player[j]) ballCount++;
+                }
+            }
+
+            if (strikeCount != 0 && ballCount == 0) System.out.println(strikeCount + "스트라이크");
+            else if (strikeCount == 0 && ballCount != 0) System.out.println(ballCount + "볼");
+            else if (strikeCount == 0) System.out.println("낫싱");
+            else {
+                System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+            }
+
+            if (strikeCount == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                int n = scanner.nextInt();
+                if (n == 1) {
+                    System.out.println("숫자 야구 게임을 시작합니다.");
+
+                    for (int i = 0; i < 3; i++) {
+                        computer[i] = (int) (Math.random() * 10);
+                    }
+
+                    System.out.println(Arrays.toString(computer));
+                }
+                if (n == 2) flag = 2;
+            }
+        }
+
+
     }
 }
