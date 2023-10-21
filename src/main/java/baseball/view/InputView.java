46836b785670ab2771c1d28c6615d45a87753746
tam31 <wxogud3377@naver.com>
2023-10-22 01:25:09 +0900

    Feat(InputView): 플레이어 숫자입력 기능 구현 Docs: 숫자를 입력해주세요 메시지 완료 체크

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,4 +1,13 @@
 package baseball.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+
+    public static String playerNumber(){
+        System.out.println(INPUT_NUMBER_MESSAGE);
+        return Console.readLine();
+    }
 }
