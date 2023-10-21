ac43638284dd306870dc734c999a83167a648d42
euics <pjh575157@gmail.com>
2023-10-21 23:12:10 +0900

    feat: playGame() 메서드 구현
    게임 라운드 실행 메서드.
    - generateUniqueRandomNumbers를 통해 컴퓨터가 숫자를 선택
    - inputAndValidateUserNumbers를 통해 사용자의 예측 정답값을 입력
    - evaluateAndDisplayGameResult 메서드를 통해 사용자의 입력값과 컴퓨터의 숫자를 비교
    - displayGameResult로 게임 결과를 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -8,5 +10,17 @@ public class Application {
 
         while (continueGame)
             continueGame = playGame();
+    }
+
+    public static boolean playGame() {
+        List<Integer> computerNumbers = generateUniqueRandomNumbers();
+        boolean gameFinished = false;
+
+        while (!gameFinished) {
+            List<Integer> userNumbers = inputAndValidateUserNumbers();
+            gameFinished = evaluateAndDisplayGameResult(computerNumbers, userNumbers);
+        }
+
+        return askForNewGame();
     }
 }
