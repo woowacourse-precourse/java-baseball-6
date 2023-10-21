090adc85917ea3ed3064922f6091e3d7ad45df56
lolmageap <ekxk1234@naver.com>
2023-10-21 21:41:49 +0900

    complete validation

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,32 +3,40 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 public class Application {
-
     public static void main(String[] args) {
-        int goal = Randoms.pickNumberInRange(0, 999);
-        System.out.println("goal = " + goal);
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        playGame();
+    }
 
+    private static void playGame() {
+        System.out.println("숫자를 입력해주세요");
         String input = Console.readLine();
         validation(input);
     }
 
-    public static void validation(String input){
-        String pattern = "^[1-9]*$";
+    private static void validation(String input){
+        String pattern = "[1-9]{1,3}";
 
         boolean regex = Pattern.matches(pattern, input);
-        boolean length = input.length() == 3;
+        if ( !regex ) throw new IllegalArgumentException();
 
-        if (!regex && !length) throw new IllegalArgumentException();
+        boolean isDuplicated = duplicateCheck(input);
+        if ( isDuplicated ) throw new IllegalArgumentException();
     }
 
-    public static void strike(String goal, String input){
-        char first = input.charAt(0);
-        char second = input.charAt(1);
-        char third = input.charAt(3);
-
+    private static boolean duplicateCheck(String input) {
+        Set<Character> characters = new HashSet<>();
+        for (char c : input.toCharArray()) {
+            characters.add(c);
+        }
+        return characters.size() != input.length();
     }
 
 }
