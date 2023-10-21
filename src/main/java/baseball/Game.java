9389645d255f9b9f5df8d6773a2c6d69d2266e92
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-10-21 23:34:18 +0900

    FEAT / add function enter guess num from player

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,5 +1,8 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+
 public class Game {
     private Player player = new Player();
     private Player computer = new Player();
@@ -10,4 +13,14 @@ public class Game {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
+    public void getGuessNums() {
+        System.out.println("숫자를 입력해주세요 : ");
+        String guessNumsStr = Console.readLine();
+        ArrayList guessNums = new ArrayList<>();
+        for (int i = 0; i < guessNumsStr.length(); i++) {
+            guessNums.add(guessNumsStr.charAt(i));
+        }
+
+        player.setGuessNums(guessNums);
+    }
 }
