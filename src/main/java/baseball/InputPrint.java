8435b6ce128b714db15e63cf41b578aa7d11eb3b
kmh0601 <pongkmh@naver.com>
2023-10-21 21:43:15 +0900

    chore: ErrorCatcher를 이용해 올바른 입력인지 확인하는 기능 추가

--- src/main/java/baseball/InputPrint.java
+++ src/main/java/baseball/InputPrint.java
@@ -6,12 +6,13 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputPrint{
-    public static List<Integer> ballInput(int MaxBallSize){
+    public static List<Integer> ballInput(int maxBallSize, int minimumNum, int maximumNum){
         String[] userInput;
-        List<Integer> returnInput = new ArrayList<>(MaxBallSize);
+        List<Integer> returnInput = new ArrayList<>(maxBallSize);
 
         System.out.print("숫자를 입력해주세요 : ");
         userInput = readLine().split("");
+        ErrorCatcher.errorCheck(userInput, maxBallSize, minimumNum, maximumNum);
         for(String s : userInput) returnInput.add(Integer.parseInt(s));
         return returnInput;
     }
