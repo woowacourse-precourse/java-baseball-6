69c9512b18563be46d7f0761ab755aa5263830ab
김영준 <rladudwns164@gmail.com>
2023-10-21 18:45:09 +0900

    refactor : 상수 인스턴스 대문자 변경
    -자바 컨벤션에 맞게 코드 수정

--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -2,27 +2,30 @@ package View;
 
 public class OutputView {
 
-    private static final OutputView instance = new OutputView();
+    private static final OutputView INSTANCE = new OutputView();
 
-    public static OutputView getInstance(){
-        return instance;
-    }
-    private OutputView(){
-    }
-    public void printGameStart(){
-        System.out.println(Message.GAME_START.message);
+    public static OutputView getInstance() {
+        return INSTANCE;
     }
 
-    private enum Message{
+    private OutputView() {
+    }
+
+    public void printGameStart() {
+        System.out.println(Message.GAME_START.getMessage());
+    }
+
+    private enum Message {
         GAME_START("숫자 야구 게임을 시작합니다");
 
         private String message;
-        private Message(String message){
+
+        Message(String message) {
             this.message = message;
         }
 
-       public String getMessage(){
+        public String getMessage() {
             return message;
-       }
+        }
     }
 }
