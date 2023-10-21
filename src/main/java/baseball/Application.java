b352a83f8877698038c31e202f5ea4501060a5af
qkrckstjq <117289923+qkrckstjq@users.noreply.github.com>
2023-10-22 00:47:26 +0900

    난수와 입력값을 비교 후 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,26 +10,8 @@ import baseball.controller.GamePlay;
 public class Application {
     public static void main(String[] args) {
         Output.printGameStart();
-        boolean isEnd = false;
-        while(!isEnd) {
-            GamePlay.playing();
-        }
 
+        GamePlay.playing();
     }
 
-
-    //유틸
-    public static String printResult (int[] checkNumbers) {
-        String result = "";
-        if(checkNumbers[0] != 0 && checkNumbers[1] != 0) {
-            result = checkNumbers[1]+"볼 " + checkNumbers[0]+"스트라이크\n";
-        } else if (checkNumbers[0] != 0 && checkNumbers[1] == 0) {
-            result = checkNumbers[0]+"볼\n";
-        } else if (checkNumbers[0] == 0 && checkNumbers[1] != 0) {
-            result = checkNumbers[1]+"스트라이크\n";
-        } else {
-            result = "낫싱\n";
-        }
-        return result;
-    }
 }
--- src/main/java/baseball/controller/GamePlay.java
+++ src/main/java/baseball/controller/GamePlay.java
@@ -3,6 +3,7 @@ package baseball.controller;
 import baseball.util.Array;
 import baseball.util.ThrowError;
 import baseball.view.Input;
+import baseball.view.Output;
 import java.util.Arrays;
 
 
@@ -14,6 +15,28 @@ public class GamePlay {
             String inputValue = Input.printNoticeInputNum();
             ThrowError.throwError(inputValue);
             int[] inputNumbers = Array.strToArr(inputValue);
+            int[] compareResult = Array.checkNumbers(randomNumbers, inputNumbers);
+            printResult(compareResult);
+            if(compareResult[0] == 3) {
+                validationResult = false;
+            }
         }
     }
+
+    public static void printResult (int[] compareResult) {
+        if(compareResult[0] != 0 && compareResult[1] != 0) {
+            Output.printBall(compareResult[0]);
+            Output.printStrike(compareResult[1]);
+        } else if (compareResult[0] != 0) {
+            Output.printBall(compareResult[0]);
+            System.out.println();
+        } else if (compareResult[1] != 0) {
+            Output.printStrike(compareResult[1]);
+            System.out.println();
+        } else {
+            Output.printWhenGameEnd();
+        }
+    }
+
+
 }
