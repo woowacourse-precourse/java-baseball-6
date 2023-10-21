3c61728c03e4b13ef1f512850e07d38afe058497
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-10-21 19:04:18 +0900

    refactor: remove unnecessary classes
    Input, Output

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,9 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        MessageManager messageManager = new MessageManager();
+
+        messageManager.showIntro();
+        messageManager.getInput();
     }
 }
--- src/main/java/baseball/Messages.java
+++ src/main/java/baseball/Messages.java
@@ -1,18 +1,18 @@
 package baseball;
 
 public class Messages {
-    private String introMessage;
-    private String inputMessage;
-    private String sameDigitMessage;
-    private String sameNumberMessage;
-    private String outroMessage;
+    private String introMessage = "숫자 야구 게임을 시작합니다.";
+    private String inputMessage = "숫자를 입력해주세요 : ";;
+    private String outroMessage = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private String sameDigitMessage = "스트라이크";
+    private String sameNumberMessage = "볼";
 
     public Messages() {
-        inputMessage = "숫자 야구 게임을 시작합니다.";
-        inputMessage = "숫자를 입력해주세요 : ";
-        outroMessage = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-        sameDigitMessage = "스트라이크";
-        sameNumberMessage = "볼";
+        this.introMessage = introMessage;
+        this.inputMessage = inputMessage;
+        this.outroMessage = outroMessage;
+        this.sameDigitMessage = sameDigitMessage;
+        this.sameNumberMessage = sameNumberMessage;
     }
 
     public void setIntroMessage(String introMessage) {
