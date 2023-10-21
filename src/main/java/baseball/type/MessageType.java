4cb0c2c105898c119f8916e60a55ad71b1c24c67
YoonbeomSo <ybum1224@naver.com>
2023-10-21 23:47:39 +0900

    fix: 성공 메세지 띄어쓰기 오탈자 수정

--- src/main/java/baseball/type/MessageType.java
+++ src/main/java/baseball/type/MessageType.java
@@ -9,7 +9,7 @@ public enum MessageType {
 
     START("숫자 야구 게임을 시작합니다."),
     ASK("숫자를 입력해주세요 : "),
-    SUCCESS("3개의 숫자를 모두 맞히셨습니다! 게임종료"),
+    SUCCESS("3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
     REPLAY_CHECK("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."),
     BALL("볼"),
     STRIKE("스트라이크"),
