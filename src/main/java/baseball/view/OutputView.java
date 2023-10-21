5753a51fc3c0e60492313ff6f614f1bc22700204
yuseonjun <seonjun0906@gmail.com>
2023-10-21 23:22:58 +0900

    feat: 힌트를 계산한 후 출력하는 출력문 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,5 +1,7 @@
 package baseball.view;
 
+import baseball.domain.HintResult;
+
 public class OutputView {
 
 	private OutputView() {
@@ -7,5 +9,9 @@ public class OutputView {
 
 	public static void startMessage() {
 		System.out.println("숫자 야구 게임을 시작합니다.");
+	}
+
+	public static void printHint(HintResult hintResult) {
+		System.out.println(hintResult);
 	}
 }
