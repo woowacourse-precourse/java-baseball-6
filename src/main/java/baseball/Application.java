34364fb120ed32ae7eefdf3adf5cc29b8862df0d
hoyun06 <hoyun06@gmail.com>
2023-10-21 21:34:40 +0900

    feat: Application 클래스의 main 함수에 게임 실행 로직 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,10 @@
 package baseball;
 
+import baseball.model.BaseBallGameRunner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseBallGameRunner runner = new BaseBallGameRunner();
+        runner.run();
     }
 }
