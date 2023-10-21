eaa6b49257cbc0185ed788347bb9dbc37dcfe04a
shAn-kor <kjn0406@naver.com>
2023-10-21 17:40:57 +0900

    RetryCheck 객체 구현

--- src/main/java/baseball/model/RetryCheck.java
+++ src/main/java/baseball/model/RetryCheck.java
@@ -1,4 +1,16 @@
 package baseball.model;
 
+import static baseball.util.ValidationChecking.checkRetryInput;
+
 public class RetryCheck {
+    private String num;
+
+    public RetryCheck(String num) {
+        checkRetryInput(num);
+        this.num = num;
+    }
+
+    public String getNum() {
+        return num;
+    }
 }
