053129cb61e7877077729cac56e7e212395f6599
fishman1123 <cho.y.j1997@gmail.com>
2023-10-21 10:06:52 +0900

    인터페이스 어느정도 구현완료..?

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,43 @@
 package baseball;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Application {
+    static class BaseballProgram {
+        private String inputValue;
+        private int step = 1;
+        private boolean run = true;
+        BaseballProgram() {
+            display(step);
+        }
+
+
+        public void display(int inputStep) {
+            while (run) {
+                switch (inputStep) {
+                    case 1:
+                        System.out.println("========@숫자야구입니다@========");
+                        System.out.println("============================");
+                        System.out.println("숫자 3자리를 이어서 작성하시오 :");
+                        Scanner scanner = new Scanner(System.in);
+                        int input = scanner.nextInt();
+                        getValue(input);
+                }
+            }
+
+
+
+        }
+        //getter
+        public String getValue(int value) {
+            this.inputValue = Integer.toString(value);
+            return inputValue;
+        }
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BaseballProgram test = new BaseballProgram();
+
+
     }
 }
