d932a0687e7901bed0fbbcc04c8353e830af20fc
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-10-21 23:53:59 +0900

    FEAT / add get player's guess num

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,16 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
+        Game game = new Game();
+        int roundResult;
+        boolean running = true;
 
+        while (true) {
+            if (!running) {
+                break;
+            }
+
+            game.getGuessNums();
+        }
     }
 }
