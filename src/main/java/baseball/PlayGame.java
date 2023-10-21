71625d6962bd9b6e4bf17c39dd3b66c09802b7f2
woong <hiidy115@gmail.com>
2023-10-22 03:08:46 +0900

    :sparkles: feat: String으로 입력받은 유저 숫자를 ArrayList로 변환하는 기능 구현

--- src/main/java/baseball/PlayGame.java
+++ src/main/java/baseball/PlayGame.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 
 public class PlayGame {
 
@@ -10,5 +11,16 @@ public class PlayGame {
     public void userInput() {
         System.out.println("숫자를 입력해주세요 : ");
         String originalInput = Console.readLine();
+        ArrayList<Integer> userNumberList = inputToArrayList(originalInput);
+    }
+
+    public ArrayList<Integer> inputToArrayList(String numInput) {
+        ArrayList<Integer> userNumbers = new ArrayList<>();
+
+        for (char c : numInput.toCharArray()) {
+            userNumbers.add(c - '0');
+        }
+
+        return userNumbers;
     }
 }
