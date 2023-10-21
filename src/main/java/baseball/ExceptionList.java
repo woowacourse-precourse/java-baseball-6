9dbb7ab5a2e61783443d6f489cfa1e3c271b574e
Chenowoo <cjsgusdn14@naver.com>
2023-10-21 16:01:29 +0900

    Fix: ExceptionList 클래스 수정 모든 예외 발생 시 메세지 삭제

--- src/main/java/baseball/ExceptionList.java
+++ src/main/java/baseball/ExceptionList.java
@@ -5,16 +5,16 @@ import java.util.List;
 public class ExceptionList {
     public static void validNumbers(List<Integer> numbers) {
         if(numbers.size() != 3) {
-            throw new IllegalArgumentException("numbers사이즈 예외 발생");
+            throw new IllegalArgumentException();
         }
         if(checkDuplicateNumber(numbers, 0, 0)) {
-            throw new IllegalArgumentException("중복수 예외 발생");
+            throw new IllegalArgumentException();
         }
     }
 
     public static void validReStartInputNumber(String InputNumber) {
         if(!InputNumber.equals("1") && !InputNumber.equals("2")) {
-            throw new IllegalArgumentException("종료시 입력한 수 예외 발생");
+            throw new IllegalArgumentException();
         }
 //        if(InputNumber == null) {
 //            throw new IllegalArgumentException("어떤 수도 입력하지 않음");
