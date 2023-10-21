8a055d0b65ac52a72f72b33d17ad6279b347e831
SungWoo <sungwoo166@gmail.com>
2023-10-21 15:27:06 +0900

    feat: Implement method to calculate game result

--- src/main/java/baseball/service/BaseBallService.java
+++ src/main/java/baseball/service/BaseBallService.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BaseBallService {
-
     public List<Integer> generateRandomNumbers() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
@@ -17,5 +16,20 @@ public class BaseBallService {
             }
         }
         return computer;
+    }
+
+    public GameResult getGameResult(List<Integer> targetNumber, List<Integer> userInput) {
+        int strike = 0;
+        int ball = 0;
+        for (int i = 0; i < 3; i++) {
+            if (userInput.contains(targetNumber.get(i))) {
+                ball++;
+            }
+            if (userInput.get(i).equals(targetNumber.get(i))) {
+                strike++;
+                ball--;
+            }
+        }
+        return new GameResult(strike, ball);
     }
 }
