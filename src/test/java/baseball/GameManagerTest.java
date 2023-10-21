135f46a0d70fcba3d21de03bed28fedb4ac498af
이준희 <juni416@naver.com>
2023-10-21 20:14:36 +0900

    test: 스트라이크와 볼이 나왔을 때의 경우 테스트코드 작성

---
+++ src/test/java/baseball/GameManagerTest.java
@@ -0,0 +1,34 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+
+class GameManagerTest {
+    @Test
+    void 스트라이크가_3개인_경우() {
+        // Arrange
+        // Action
+        // Assert
+    }
+
+    @Test
+    void 스트라이크가_1개_이상인_경우() {
+        // Arrange
+        // Action
+        // Assert
+    }
+
+    @Test
+    void 볼이_1개_이상인_경우() {
+        // Arrange
+        // Action
+        // Assert
+    }
+
+    @Test
+    void 스트라이크와_볼이_없는_경우() {
+        // Arrange
+        // Action
+        // Assert
+    }
+}
