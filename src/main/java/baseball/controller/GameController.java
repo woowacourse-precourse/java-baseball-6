879bc4a501890bfa34da7ff81b0d29b2be501e49
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-10-21 15:49:53 +0900

    style : Change field, method name 가독성을 위해 메서드와 필드의 이름을 변경하고, 외부에서 접근하지 못하도록 private로 변경

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -11,36 +11,40 @@ public class GameController {
     CompareNumber compareNumber = new CompareNumber();
     private ProgramNumber program;
     private UserNumber user;
+    private final CompareNumber numberComparator = new CompareNumber();
+    private ProgramNumber programNumber;
+    private UserNumber userNumber;
+
 
     public void run() {
         OutputView.printStart();
         do {
-            start();
-        } while (isReset());
+            playGame();
+        } while (isResetRequested());
     }
 
-    public void start() {
-        program = new ProgramNumber();
+    private void playGame() {
+        programNumber = new ProgramNumber();
         do {
-            user = new UserNumber(InputView.inputUserNumber());
+            userNumber = new UserNumber(InputView.inputUserNumber());
             int[] result = getResultList();
             OutputView.printScoreMessage(result[0], result[1]);
-        } while (!isEnd());
+        } while (!isGameEnd());
     }
 
-    public boolean isEnd() {
-        if (compareNumber.isThreeStrike()) {
+    private boolean isGameEnd() {
+        if (numberComparator.isThreeStrike()) {
             OutputView.printEnd();
             return true;
         }
         return false;
     }
 
-    public int[] getResultList() {
-        return compareNumber.getStrikeBall(user.getUserNumber(), program.getProgramNumber());
+    private int[] getResultList() {
+        return numberComparator.getStrikeBall(userNumber.getUserNumber(), programNumber.getProgramNumber());
     }
 
-    public boolean isReset() {
+    private boolean isResetRequested() {
         ResetGameNumber resetGameNumber = new ResetGameNumber(InputView.resetNumber());
         return resetGameNumber.getResetNumber().equals("1");
     }
