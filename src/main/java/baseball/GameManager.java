69b78ff7d6bd23438ecc49c7b7e208195d7efb0c
jiwoo <zbqmgldjfh@gmail.com>
2023-10-21 13:54:32 +0900

    refactor(GameManager): 리팩토링

--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -5,42 +5,54 @@ import baseball.util.Parser;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 
+import java.util.Collections;
 import java.util.List;
 
 public class GameManager {
 
+    private static final String GAME_RETRY_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String GAME_USER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String GAME_INTRO_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String RETRY_COMMAND = "1";
     private final InputView inputView;
     private final OutputView outputView;
+    private List<Integer> computerNumbers;
+    private List<Integer> userInputNumbers;
 
     public GameManager(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.computerNumbers = Collections.emptyList();
+        this.userInputNumbers = Collections.emptyList();
     }
 
     public void play() {
-        outputView.writeNewlineMessage("숫자 야구 게임을 시작합니다.");
+        outputView.writeNewlineMessage(GAME_INTRO_MESSAGE);
         do {
-            List<Integer> computerNumbers = NumberGenerator.generateDefaultSizeComputerNumber();
-            List<Integer> userInputNumbers;
-
-            do {
-                outputView.writeInlineMessage("숫자를 입력해주세요 : ");
-                String userInput = inputView.readUserInput();
-                userInputNumbers = Parser.parseToIntegerList(userInput);
-            } while (calculateGameResult(computerNumbers, userInputNumbers));
+            playOneRoundGame();
         } while (isContinue());
     }
 
-    private boolean calculateGameResult(List<Integer> computerNumbers, List<Integer> userInputNumbers) {
+    private void playOneRoundGame() {
+        this.computerNumbers = NumberGenerator.generateDefaultSizeComputerNumber();
+
+        do {
+            outputView.writeInlineMessage(GAME_USER_INPUT_MESSAGE);
+            String userInput = inputView.readUserInput();
+            this.userInputNumbers = Parser.parseToIntegerList(userInput);
+        } while (calculateGameResult());
+    }
+
+    private boolean calculateGameResult() {
         // TODO : 게임 결과 계산
         return false;
     }
 
     private boolean isContinue() {
-        outputView.writeNewlineMessage("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        String userInput = inputView.readUserRetryInput();
+        outputView.writeNewlineMessage(GAME_RETRY_MESSAGE);
 
-        if(userInput.equals("1")) {
+        String userInput = inputView.readUserRetryInput();
+        if(userInput.equals(RETRY_COMMAND)) {
             return true;
         }
         return false;
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,7 +3,7 @@ package baseball.view;
 public class OutputView {
 
     public void writeInlineMessage(String message) {
-        System.out.println(message);
+        System.out.print(message);
     }
 
     public void writeNewlineMessage(String message) {
