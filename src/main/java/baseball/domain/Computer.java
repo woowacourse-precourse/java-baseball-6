23beca80f8033b954c92b40506cc7dda04cb1f30
zzun73 <736300@naver.com>
2023-10-21 21:31:49 +0900

    feat: 상대방(컴퓨터) 역할 구현

---
+++ src/main/java/baseball/domain/Computer.java
@@ -0,0 +1,15 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Computer {
+    List<Integer> computer;
+
+    public List<Integer> getComputer() {
+        return computer;
+    }
+
+    public void setComputer(List<Integer> computer) {
+        this.computer = computer;
+    }
+}
