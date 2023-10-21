8c88196b814100faaa4653e3e801d916266f6c31
yubin <dbqls2821@gmail.com>
2023-10-21 21:23:05 +0900

    fix: Computer 객체가 Digits의 정적 팩터리 메서드를 사용하도록 수정

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -2,11 +2,15 @@ package baseball;
 
 final class Computer {
 
-    final Digits digits = new Digits();
+    private Digits digits = Digits.generateRandomDigits();
 
     Result calculateScore(Digits target){
         int strikeCount = digits.countStrike(target);
         int ballCount = digits.countBall(target) - strikeCount;
         return new Result(ballCount, strikeCount);
+    }
+
+    void prepareGameRetry() {
+        digits = Digits.generateRandomDigits();
     }
 }
