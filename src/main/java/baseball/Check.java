1b4c49bc54a66e70304c3678c198e4aa0d788df3
unifolio0 <boho82ha@gmail.com>
2023-10-21 19:36:15 +0900

    Check기능 추가 구현

--- src/main/java/baseball/Check.java
+++ src/main/java/baseball/Check.java
@@ -1,12 +1,16 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Check {
     Computer computer;
     User user;
+    int strike;
+    int ball;
 
     public void resultCheck() {
-        int strike = 0;
-        int ball = 0;
+        strike = 0;
+        ball = 0;
         for(int i=0;i<3;i++) {
             int idx = user.user.indexOf(computer.computer.get(i));
             if(idx==i) {
@@ -27,5 +31,26 @@ public class Check {
             result+="낫싱";
         }
         System.out.println(result);
+    }
+    
+    public boolean endingCheck() {
+        if(strike==3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        }
+        return false;
+    }
+
+    public boolean replayCheck() {
+        String choice = Console.readLine();
+        if(choice.equals("1")) {
+            return true;
+        }
+        else if(choice.equals("2")) {
+            return false;
+        }
+        else {
+            return false;
+        }
     }
 }
