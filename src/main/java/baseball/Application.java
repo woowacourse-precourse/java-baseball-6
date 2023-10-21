423df338176c5a5fcc237acd818cfd44e28cc162
깃SeoMoonK <sunghoon1216@naver.com>
2023-10-21 17:58:30 +0900

    Feat: 사용자의 숫자 입력과 예외 처리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -9,10 +10,34 @@ public class Application {
 
     static List<Integer> computer;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
+        System.out.println("숫자 야구 게임을 시작합니다.");
         genNumbers();
-        System.out.println(computer);
+
+        while(true) {
+
+            System.out.print("숫자를 입력해주세요 : ");
+            String iNumbers = Console.readLine();
+
+            /*
+            입력된 내용에 대해 검사해 봐야 하는 것
+            1. 숫자는 맞는가?
+            2. 3자리의 숫자인가?
+             */
+
+            if(iNumbers.length() != 3) {
+                throw new IllegalArgumentException("3자리의 숫자가 입력되어야 합니다.");
+            }
+
+            try {
+                Integer.parseInt(iNumbers);
+            } catch(NumberFormatException e) {
+                //NumberFormatException은 IllegalArgumentException의 하위 예외이다.
+                throw new IllegalArgumentException("숫자가 아닌 값이 입력되었습니다.");
+            }
+
+        }
 
     }
 
