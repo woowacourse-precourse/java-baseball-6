009b8edff61ea8e54d34ce1d1c76cec47501ac3e
watermelon3737 <pshrina1@naver.com>
2023-10-21 16:30:39 +0900

    [test] : 1~9까지의 지정 범위에 숫자 생성 테스트 구현

--- src/test/java/baseball/NumberGeneratorTest.java
+++ src/test/java/baseball/NumberGeneratorTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class NumberGeneratorTest {
@@ -12,10 +13,12 @@ class NumberGeneratorTest {
     @DisplayName("1000개의 랜덤한 숫자를 생성하는 테스트")
     @Test
     void makeRandomInRangeTest() {
+        //given
         NumberGenerator numberGenerator = new NumberGenerator();
+        //when
         for (int i = 0; i < 1000 ; i++) {
             int randomNumber = numberGenerator.generateRandomNumber();
-            //확인차 출력
+            //then
             System.out.println(randomNumber);
         }
     }
@@ -23,15 +26,25 @@ class NumberGeneratorTest {
     @DisplayName("1000개의 랜덤한 숫자 리스트를 생성하는 테스트")
     @Test
     void generateRandomList() {
+        //give
         NumberGenerator numberGenerator = new NumberGenerator();
+        //when
         for (int i = 0; i < 1000; i++) {
             List<Integer> randomList = numberGenerator.makeRandomAnswerList();
-            //확인차 출력
+            //then
             System.out.println(randomList);
         }
     }
 
+    @DisplayName("1 ~ 9까지 지정 범위에 숫자 생성 테스트")
     @Test
-    void getAnswer() {
+    void generateRandomNumberInCustomRange() {
+        //given
+        NumberGenerator numberGenerator = new NumberGenerator();
+        //when
+        int randomNumber = numberGenerator.generateRandomNumber();
+        //then
+        assertThat(randomNumber).isBetween(1, 9);
     }
+
 }
