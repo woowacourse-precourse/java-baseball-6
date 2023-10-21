3fca415c1984d845a4e55a9d68004cd3cc412e2f
tangba484 <tangba484@naver.com>
2023-10-22 00:40:17 +0900

    게임에 필요한 숫자배열객체 Number구현

---
+++ src/main/java/baseball/model/Number.java
@@ -0,0 +1,34 @@
+package baseball.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Number {
+    private final List<Integer> number;
+    private static final int gameSize = 3;
+
+    public Number() {
+        number = new ArrayList<>();
+    }
+
+    public void add(int num) {
+        number.add(num);
+    }
+
+    public int size(){
+        return number.size();
+    }
+
+    public static int getGameSize(){
+        return gameSize;
+    }
+
+    public boolean contains(int num) {
+        return number.contains(num);
+    }
+
+    @Override
+    public String toString() {
+        return number.toString();
+    }
+}
