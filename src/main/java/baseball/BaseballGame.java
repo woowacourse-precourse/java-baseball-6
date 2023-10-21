a7286fbc35784fe621d2261cfd5239aba1820bb6
mmalfoy <stpmalfoy@gmail.com>
2023-10-21 19:15:18 +0900

    FEAT : 구현 기능 Class 의 목록 1,2,5 구현 BODY : 기능목록의 구현 기능 Class에 해당하는 1, 2, 5 구현. 3,4 구현중. 1. setNumofComputer 메소드는 3자리수의 서로다른 1~9까지의 숫자를 랜덤    생성하여 numOfComputer 멤버 리스트에 저장. 2. 인풋의 에러 판단과    저장함수는 입력기능Class의 setUserInput 메소드 호출. 5. 게임이 끝난 후 재시작    입력은 입력 기능 Class의 setRestartNum  메소드 호출

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,2 +1,48 @@
-package baseball;public class BaseballGame {
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseballGame {
+    private static List<Integer> numOfComputer;
+
+    private static void setNumOfComputer(){
+        numOfComputer = new ArrayList<>();
+        while (numOfComputer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!numOfComputer.contains(randomNumber)) {
+                numOfComputer.add(randomNumber);
+            }
+        }
+    }
+
+    public static void startGame(){
+        Baseball_output.printStart();
+        setNumOfComputer();
+        try{
+            do{
+                Baseball_Input.setUserInput();
+
+            }while(true);
+
+        }catch (IllegalArgumentException e){
+            throw e;
+        }
+
+    }
+
+    public static void run(){
+        try{
+            do{
+                startGame();
+            }while(Baseball_Input.setRestartNum());
+
+        }catch (IllegalArgumentException e){
+            throw e;
+        }
+
+    }
+
 }
