6ea5eac47bc0e8e1f5b016cae32b21c96d7ad998
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2023-10-22 01:01:19 +0900

    feat: Computer의 랜덤값 생성 기능

--- src/main/java/baseball/model/ComputerRandomInt.java
+++ src/main/java/baseball/model/ComputerRandomInt.java
@@ -1,4 +1,17 @@
 package baseball.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashSet;
+
 public class ComputerRandomInt {
+    public void randomNum() {
+        HashSet computer = new HashSet();
+
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+    }
 }
