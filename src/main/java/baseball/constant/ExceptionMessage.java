c0fc1918bc0f6da5bd15ba881ef6431af7f408b0
김연진 <love_homebody@naver.com>
2023-10-21 16:51:38 +0900

    feat(Player): 3개 이상의 숫자 입력시 예외 발생

--- src/main/java/baseball/constant/ExceptionMessage.java
+++ src/main/java/baseball/constant/ExceptionMessage.java
@@ -2,7 +2,8 @@ package baseball.constant;
 
 public enum ExceptionMessage {
 
-    NOT_INTEGER("숫자만 입력할 수 있습니다.");
+    NOT_INTEGER("숫자만 입력할 수 있습니다."),
+    INVALID_LENGTH("3개의 숫자를 입력해주세요.");
 
     private final String message;
 
--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -1,5 +1,8 @@
 package baseball.domain;
 
+import baseball.constant.BallConstant;
+import baseball.constant.ExceptionMessage;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,7 +11,19 @@ public class Player {
     private final List<Integer> elements;
 
     public Player(List<Integer> elements) {
+        validateElements(elements);
         this.elements = new ArrayList<>(elements);
+    }
+
+    private void validateElements(List<Integer> elements) {
+        validateLength(elements);
+    }
+
+    private void validateLength(List<Integer> elements) {
+        if (elements.size() != BallConstant.BALL_LENGTH.getValue()) {
+            ExceptionMessage exceptionMessage = ExceptionMessage.INVALID_LENGTH;
+            throw new IllegalArgumentException(exceptionMessage.getMessage());
+        }
     }
 
     public int getByPosition(int position) {
