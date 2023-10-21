84846292c553dc8099abe7badab75122026bd92a
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-10-21 17:14:25 +0900

    chore : Add DisplayName

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -9,27 +10,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
-    @Test
-    void 게임종료_후_재시작() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("246", "135", "1", "597", "589", "2");
-                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
-                },
-                1, 3, 5, 5, 8, 9
-        );
-    }
 
-    @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("1234"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
+  @Test
+  @DisplayName("게임종료 후 재시작")
+  void 게임종료_후_재시작() {
+    assertRandomNumberInRangeTest(
+        () -> {
+          run("246", "135", "1", "597", "589", "2");
+          assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+        },
+        1, 3, 5, 5, 8, 9
+    );
+  }
 
-    @Override
-    public void runMain() {
-        Application.main(new String[]{});
-    }
+  @Test
+  @DisplayName("예외 테스트")
+  void 예외_테스트() {
+    assertSimpleTest(() ->
+        assertThatThrownBy(() -> runException("1234"))
+            .isInstanceOf(IllegalArgumentException.class)
+    );
+  }
+
+  @Override
+  public void runMain() {
+    Application.main(new String[]{});
+  }
 }
