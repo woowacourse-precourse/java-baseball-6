a963a4006d83c2782de98c480c0ecac8cac1400d
takealook97 <takealook97@naver.com>
2023-10-21 20:16:14 +0900

    refactor: Application의 run 메서드 실행 수정
    생성과 동시에 실행을 하도록 하였다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,7 +4,6 @@ import baseball.service.GameService;
 
 public class Application {
 	public static void main(String[] args) {
-		GameService gameService = new GameService();
-		gameService.run();
+		new GameService().run();
 	}
 }
