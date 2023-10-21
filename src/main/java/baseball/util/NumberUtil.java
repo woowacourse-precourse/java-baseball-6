f854531b22879e9c8013612be834b831beb5ec83
jae woo Jung <awfjol2008@naver.com>
2023-10-21 21:25:45 +0900

    feat: 입력된 문자열의 길이가 3인지 검증하는 메소드 구현

--- src/main/java/baseball/util/NumberUtil.java
+++ src/main/java/baseball/util/NumberUtil.java
@@ -6,4 +6,10 @@ public class NumberUtil {
             throw new IllegalArgumentException("입력값은 비어있지 않아야 합니다.");
         }
     }
+
+    protected static void validateLength(String number) {
+        if (number.length() != 3) {
+            throw new IllegalArgumentException("입력값은 3자리이어야 합니다.");
+        }
+    }
 }
