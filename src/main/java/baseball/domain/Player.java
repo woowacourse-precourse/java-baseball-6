4f5561fa93b324b2856fdb35931e3e6d2a354927
zzun73 <736300@naver.com>
2023-10-21 21:31:18 +0900

    feat: 사용자 역할 구현

---
+++ src/main/java/baseball/domain/Player.java
@@ -0,0 +1,15 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Player {
+    List<Integer> player;
+
+    public List<Integer> getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(List<Integer> player) {
+        this.player = player;
+    }
+}
