6e938fd1f8985970e7593c041232572957ba0e18
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-10-21 22:46:46 +0900

    add: baseball number generate

--- src/main/java/baseball/model/Baseball.java
+++ src/main/java/baseball/model/Baseball.java
@@ -1,4 +1,26 @@
 package baseball.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Baseball {
+    
+    List<Integer> ballNum = new ArrayList<>();
+
+    public Baseball(List<Integer> ballNum) {
+        this.ballNum = ballNum;
+    }
+
+    public void makeBaseball() {
+
+        while (ballNum.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!ballNum.contains(randomNumber)) {
+                ballNum.add(randomNumber);
+            }
+        }
+
+    }
 }
