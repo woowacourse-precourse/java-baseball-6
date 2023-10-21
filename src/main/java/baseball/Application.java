95ade1ac828c1c5d9a66eb28be69d2f073e1f2eb
Seon1134 <jseon382@gmail.com>
2023-10-22 01:01:42 +0900

    feat: Add a response when ball and strike exist
    볼과 스트라이크 모두 존재 시의 응답 추가,
    맞춘 숫자 응답 if문과 무작위 숫자 생성 함수 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,12 +3,9 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.List;
-
 
 public class Application {
 
-    // TODO 예외처리 만들기 (잘못된 입력 처리)
     // TODO 코드 깔끔하게 만들기 (가독성을 높이고 좀 더 효율적으로)
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -16,11 +13,16 @@ public class Application {
         int strikeCount = 0;
         boolean isRun = true;
 
-        List<Integer> computer = createRandomNumList();
+        // 랜덤 숫자 생성
+        ArrayList<Integer> computer = createRandomNumList(new ArrayList<>());
 
         while (isRun) {
-            // 유저가 입력한 3개의 숫자를 조회
+            // 유저가 입력한 숫자를 조회
             String[] userStr = Console.readLine().trim().split("");
+
+            //TODO 유저가 잘못된 값을 입력했을 때, 예외처리 제작 (IllegalArgumentException 발생)
+
+            // 숫자 정답여부 확인
             for (int i = 0; i < 3; i++) {
                 int nowNum = Integer.parseInt(userStr[i]);
                 if (computer.contains(nowNum)) {
@@ -32,29 +34,30 @@ public class Application {
                 }
             }
 
-            // 조회 결과값 출력
-            if (ballCount == 0 && strikeCount == 0) {
-                System.out.println("낫싱");
-            }
-            if (ballCount != 0) {
-                System.out.println(ballCount + "볼 ");
-            }
-            if (strikeCount != 0) {
+            // 맞춘 숫자의 종류와 개수 출력
+            if (strikeCount == 3) {
                 System.out.println(strikeCount + "스트라이크");
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 
-                // 숫자 맞추기 성공시
-                if (strikeCount == 3) {
-                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-
-                    // 게임 종료 이후 재시작 질문
-                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                    if (Console.readLine().trim().equals("1")) {
-                        computer = createRandomNumList();
-                        System.out.print("숫자를 입력해주세요 : ");
-                    } else {
-                        break;
-                    }
+                // 게임 종료 이후 재시작 질문
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                // 재시작을 선택했다면
+                if (Console.readLine().trim().equals("1")) {
+                    computer.clear();
+                    computer = createRandomNumList(computer);
+                    System.out.print("숫자를 입력해주세요 : ");
+                    // 종료를 선택했다면
+                } else {
+                    isRun = false;
                 }
+            } else if (ballCount != 0 && strikeCount != 0) {
+                System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+            } else if (ballCount != 0) {
+                System.out.println(ballCount + "볼 ");
+            } else if (strikeCount != 0) {
+                System.out.println(strikeCount + "스트라이크");
+            } else {
+                System.out.println("낫싱");
             }
 
             ballCount = 0;
@@ -64,16 +67,14 @@ public class Application {
     }
 
     // 랜덤으로 3개의 숫자를 생성해주는 로직.
-    public static List<Integer> createRandomNumList() {
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
+    public static ArrayList<Integer> createRandomNumList(ArrayList<Integer> numList) {
+        while (numList.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
+            if (!numList.contains(randomNumber)) {
+                numList.add(randomNumber);
             }
         }
-        return computer;
+        return numList;
     }
-
 
 }
