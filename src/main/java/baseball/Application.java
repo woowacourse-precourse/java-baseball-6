32c6f9425335cd8ae428ce34b2bce33c701683f0
hyuna <monicajo0@naver.com>
2023-10-21 11:21:03 +0900

    feat: 게임 시작

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+
 public class Application {
+    private static ArrayList<Integer> answer;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseballGame baseballGame = new BaseballGame();
+        baseballGame.start();
     }
 }
