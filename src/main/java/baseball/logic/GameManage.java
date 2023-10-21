0f52d82eee42b3bca26d23319b3579ddaa529a0c
Jae-Young98 <ygy0102@gmail.com>
2023-10-21 22:14:14 +0900

    feat(GameManage): 숫자야구 결과를 출력하는 기능 추가

--- src/main/java/baseball/logic/GameManage.java
+++ src/main/java/baseball/logic/GameManage.java
@@ -12,9 +12,36 @@ public class GameManage {
         ball = 0;
     }
 
+    public int getStrikeCount() {
+        return this.strike;
+    }
+
+    public int getBallCount() {
+        return this.ball;
+    }
+
     public void getResult(List<Integer> answerNumbers, List<Integer> userNumbers) {
         checkStrike(answerNumbers, userNumbers);
         checkBall(answerNumbers, userNumbers);
+    }
+
+    public void showResult() {
+        message(strike, ball);
+    }
+
+    private void message(int strike, int ball) {
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+        }
+        if (strike > 0 && ball > 0) {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
+        if (strike > 0 && ball == 0) {
+            System.out.println(strike + "스트라이크");
+        }
+        if (strike == 0 && ball > 0) {
+            System.out.println(ball + "볼");
+        }
     }
 
     private void checkStrike(List<Integer> answerNumbers, List<Integer> userNumbers) {
