4e24884598fac6745fdd8f6854f2d3467c96cd3f
giljin <68088970+giljin@users.noreply.github.com>
2023-10-21 21:17:19 +0900

    feat: 사용자에게 3자리 숫자를 입력 받고 잘못된 입력이 들어올 시 IllegalArgumentException을 발생 시킨다.

--- src/main/java/baseball/Balls.java
+++ src/main/java/baseball/Balls.java
@@ -11,6 +11,34 @@ public class Balls {
         createRandomNumbers();
     }
 
+    public Balls(String ballsStr) {
+        isValidation(ballsStr);
+
+        balls = new ArrayList<>();
+        for (int i = 0; i < ballsStr.length(); i++) {
+            balls.add(ballsStr.charAt(i) - '0');
+        }
+    }
+
+    private void isValidation(String ballsStr) {
+        //사용자 숫자 유효성 검사
+        boolean isNumbers = ballsStr.matches("[1-9]+");
+        if (!isNumbers) {
+            throw new IllegalArgumentException("1~9 숫자가 아닙니다.");
+        }
+        if (ballsStr.length() != 3) {
+            throw new IllegalArgumentException("3자리가 아닙니다.");
+        }
+
+        for (int i = 0; i < ballsStr.length() - 1; i++) {
+            char searchChar = ballsStr.charAt(i);
+            int searchIndex = ballsStr.indexOf(searchChar, i + 1);
+            if (searchIndex > -1) {
+                throw new IllegalArgumentException("중복된 숫자가 있습니다.");
+            }
+        }
+    }
+
     private void createRandomNumbers() {
         balls = new ArrayList<>();
 
@@ -21,4 +49,6 @@ public class Balls {
             }
         }
     }
+
+
 }
