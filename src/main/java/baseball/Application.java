d96affbfa478997169734c04799e905232a9e13e
cookingTorch <jhw7181@naver.com>
2023-10-22 00:36:20 +0900

    modified getContinueGame

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -157,6 +157,7 @@ public class Application {
     private static int getContinueGame() {
         String choice;
 
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         choice = Console.readLine();
         if (!isValidChoice(choice)) {
             throw (new IllegalArgumentException());
