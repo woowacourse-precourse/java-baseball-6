82b4c8a25053c9a9e1a4ab82e5f9fe21e606f92a
Austin-Choi <austinsupra@gmail.com>
2023-10-21 23:30:03 +0900

    Refactor: exitCommand -> command 변수명 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,24 +13,22 @@ public class Application {
     public static void main(String[] args){
         int[] computerInput = {1,3,5,5,8,9};
         BaseballGame game = BaseballGame.getInstance();
-        //TODO: run내용을 BaseballGame 클래스 메서드로 따로 분리하고 1234 넣었을때 exception안뜨는거 해결하기
-        //game.run(computerInput);
 
         ComputerNumber computerNumber;
         GuessNumber guessNumber;
-        List<Integer> countResult;
-        int exitCommand = INITIALIZE;
         List<Integer> chunk;
+        List<Integer> countResult;
+        int command = INITIALIZE;
 
         int chunkIndex = 0;
 
-        chunk = Input.sliceToThree(computerInput, 0);
         System.out.println("숫자 야구 게임을 시작합니다.");
+        chunk = Input.sliceToThree(computerInput, chunkIndex);
         computerNumber = new ComputerNumber(chunk);
         chunkIndex++;
 
-        while(exitCommand == INITIALIZE || exitCommand == RESTARTED) {
-            if(exitCommand == RESTARTED){
+        while(command == INITIALIZE || command == RESTARTED) {
+            if(command == RESTARTED) {
                 chunk = Input.sliceToThree(computerInput, chunkIndex);
                 computerNumber = new ComputerNumber(chunk);
                 chunkIndex++;
@@ -41,7 +39,7 @@ public class Application {
             Printer.printBaseballNumber(guessNumber);
             countResult = game.count(computerNumber, guessNumber);
             Printer.printResult(countResult);
-            exitCommand = game.checkRestart(countResult);
+            command = game.checkRestart(countResult);
         }
     }
 }
