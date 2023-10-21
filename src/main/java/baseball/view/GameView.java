333758c940f9cff7822b0ba5b6af00201ae3fbc6
seokmyungham <hellotjraud@gmail.com>
2023-10-22 02:52:06 +0900

    feat(GameView): getUserInput 메서드 추가
    getUserInput: 메시지를 출력하고 사용자의 입력을 받는 메서드입니다.

--- src/main/java/baseball/view/GameView.java
+++ src/main/java/baseball/view/GameView.java
@@ -1,9 +1,18 @@
 package baseball.view;
 
 import static baseball.domain.Constants.START_GAME_MESSAGE;
+import static baseball.domain.Constants.USER_INPUT_MESSAGE;
+
+import camp.nextstep.edu.missionutils.Console;
 
 public class GameView {
     public void startGame() {
         System.out.println(START_GAME_MESSAGE);
+    }
+
+    public String getUserInput() {
+        System.out.println(USER_INPUT_MESSAGE);
+        String userInput = Console.readLine();
+        return userInput;
     }
 }
