f388437ba9e2cc3f78795320097b977321001069
김명준 <86913355+mjj111@users.noreply.github.com>
2023-10-21 15:48:51 +0900

    style(InputViewTest): 사용하지 않는 import문, 줄 바꿈 제거

--- src/test/java/baseball/view/InputViewTest.java
+++ src/test/java/baseball/view/InputViewTest.java
@@ -1,7 +1,6 @@
 package baseball.view;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -31,6 +30,4 @@ public class InputViewTest {
 
         Assertions.assertEquals(correctInput, inputPlayersContinue);
     }
-
-
 }
