d3f37d75e9a336b6b6fdf3edc5babbf7860cf38e
jungeun-choi <jungeun5.choi@gmail.com>
2023-10-22 00:15:35 +0900

    init: 숫자 생성, 판정에 필요한 class 생성

---
+++ src/main/java/baseball/BaseballComputer.java
@@ -0,0 +1,6 @@
+package baseball;
+
+/* 1에서 9까지 서로 다른 임의의 수 3개의 숫자 생성 */
+public class BaseballComputer {
+
+}
---
+++ src/main/java/baseball/BaseballUmpire.java
@@ -0,0 +1,6 @@
+package baseball;
+
+/* 플레이어가 제시한 숫자를 판정하고 결과를 반환 */
+public class BaseballUmpire {
+
+}
