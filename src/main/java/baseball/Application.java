8e5479fc5a4c0fd07ead473be87de90334f0c1db
howudong <tjdvy963@naver.com>
2023-10-21 15:23:23 +0900

    feat : 컨트롤러 추가 및 Application에 연동

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import baseball.controller.Controller;
+import baseball.service.Service;
+import baseball.view.InputValidator;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        new Controller(new InputView(new InputValidator()), new OutputView(), new Service()).run();
     }
 }
--- src/main/java/baseball/controller/Controller.java
+++ src/main/java/baseball/controller/Controller.java
@@ -1,7 +1,14 @@
 package baseball.controller;
 
+import baseball.domain.*;
+import baseball.dto.HintDto;
+import baseball.service.Service;
 import baseball.view.InputView;
 import baseball.view.OutputView;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public final class Controller {
     private final InputView inputView;
@@ -15,11 +22,36 @@ public final class Controller {
     }
 
     public void run() {
-        createAnswerNumber();
+        Referee referee = initGame();
+        PlayerNumbers playerNumbers = null;
+        while (playerNumbers == null || !referee.isSameWithAnswer(playerNumbers)) {
+            playerNumbers = viewPlayerNumbersInput();
+            System.out.println("playerNumbers = " + playerNumbers.getNumbers());
+            viewHint(referee, playerNumbers);
+        }
+        outputView.displayCorrectAnswer();
+        outputView.viewEndGame();
+        inputView.readEndNumber();
     }
 
-    private void createAnswerNumber() {
-        createAnswerNumber();
+    private Referee initGame() {
+        AnswerNumbers answerNumbers = AnswerNumbers.createAnswerNumbers(new AnswerNumberGenerator());
+        return new Referee(answerNumbers);
+
     }
 
+    private PlayerNumbers viewPlayerNumbersInput() {
+        outputView.displayInputNumbers();
+        String readNumbers = inputView.readNumbers();
+        List<Integer> numbers = readNumbers.chars().mapToObj(num -> num - '0').toList();
+        return new PlayerNumbers(numbers);
+    }
+
+    private void viewHint(Referee referee, PlayerNumbers playerNumbers) {
+        int numberSize = playerNumbers.getNumbers().size();
+
+        Map<HintType, Integer> hintMap = referee.getHint(playerNumbers);
+        HintDto hintDto = service.getHintDto(new HashMap<>(hintMap), numberSize);
+        outputView.displayHint(hintDto.getHint());
+    }
 }
