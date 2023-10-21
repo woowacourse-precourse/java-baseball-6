4594c74fff703c30752314a8352316fc74bdb06a
cloudchamb3r <jizon0123@protonmail.com>
2023-10-21 09:08:58 +0900

    removing dot!

--- src/main/java/baseball/util/Assertions.java
+++ src/main/java/baseball/util/Assertions.java
@@ -5,33 +5,32 @@ import java.util.Collection;
 public class Assertions {
     public static <E extends Comparable<? super E>>
     void assertInRange(E value, E startInclusive, E endInclusive) throws IllegalArgumentException {
-        if (0 <= value.compareTo(startInclusive) && value.compareTo(endInclusive) <= 0) {
-            return;
+        if (0 > value.compareTo(startInclusive)) {
+            throw new IllegalArgumentException();
         }
-        throw new IllegalArgumentException();
+        if (0 < value.compareTo(endInclusive)) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public static <E extends Comparable<? super E>>
     void assertEqual(E lhs, E rhs) throws IllegalArgumentException {
-        if (lhs.compareTo(rhs) == 0) {
-            return;
+        if (lhs.compareTo(rhs) != 0) {
+            throw new IllegalArgumentException();
         }
-        throw new IllegalArgumentException();
     }
 
     public static <E>
     void assertExistsIn(E needle, Collection<E> hay) {
-        if (hay.contains(needle)) {
-            return;
+        if (!hay.contains(needle)) {
+            throw new IllegalArgumentException();
         }
-        throw new IllegalArgumentException();
     }
 
     public static <E extends Object>
     void assertNotNull(E value) {
-        if (value != null) {
-            return;
+        if (value == null) {
+            throw new NullPointerException();
         }
-        throw new NullPointerException();
     }
 }
