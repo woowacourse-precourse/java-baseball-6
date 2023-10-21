41ffe7d801b7d4a7aa28ca96770601ae5f7cca50
이준희 <juni416@naver.com>
2023-10-21 20:22:05 +0900

    blue: 상황별 게임 결과 반환 테스트코드 구현

---
+++ src/main/java/baseball/GameManager.java
@@ -0,0 +1,46 @@
+package baseball;
+
+import java.util.List;
+
+public class GameManager {
+    public String isEndGame(List<Integer> computerNumber, List<Integer> userNumber) {
+        // 같은 수가 같은 자리에 있으면 스트라이크
+        Integer strikeCount = getStrikeCount(computerNumber, userNumber);
+        if (strikeCount == 3) {
+            return strikeCount + "스트라이크";
+        }
+
+        Integer ballCount = getBallCount(computerNumber, userNumber);
+
+        if (strikeCount > 0) {
+            return +ballCount + "볼" + " " + strikeCount + "스트라이크";
+        }
+
+        if (ballCount > 0) {
+            return ballCount + "볼";
+        }
+
+        return "낫싱";
+    }
+
+    private Integer getStrikeCount(List<Integer> computerNumber, List<Integer> userNumber) {
+        int strikeCount = 0;
+        for (int i = 0; i < computerNumber.size(); i++) {
+            if (computerNumber.get(i) == userNumber.get(i)) {
+                strikeCount++;
+            }
+        }
+        return strikeCount;
+    }
+
+    private Integer getBallCount(List<Integer> computerNumber, List<Integer> userNumber) {
+        // 숫자가 같은 자리에 없고 다른 자리에 있으면 볼
+        int ballCount = 0;
+        for (int i = 0; i < computerNumber.size(); i++) {
+            if (computerNumber.get(i) != userNumber.get(i) && computerNumber.contains(userNumber.get(i))) {
+                ballCount++;
+            }
+        }
+        return ballCount;
+    }
+}
--- src/test/java/baseball/GameManagerTest.java
+++ src/test/java/baseball/GameManagerTest.java
@@ -2,33 +2,57 @@ package baseball;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 
 class GameManagerTest {
     @Test
     void 스트라이크가_3개인_경우() {
         // Arrange
+        List<Integer> computerNumber = List.of(1, 2, 3);
+        List<Integer> userNumber = List.of(1, 2, 3);
+        GameManager gameManager = new GameManager();
         // Action
+        String result = gameManager.isEndGame(computerNumber, userNumber);
         // Assert
+        assertThat(result).isEqualTo("3스트라이크");
     }
 
     @Test
     void 스트라이크가_1개_이상인_경우() {
         // Arrange
+        List<Integer> computerNumber = List.of(1, 3, 2);
+        List<Integer> userNumber = List.of(1, 2, 4);
+        GameManager gameManager = new GameManager();
         // Action
+        String result = gameManager.isEndGame(computerNumber, userNumber);
         // Assert
+        assertThat(result).isEqualTo("1볼 1스트라이크");
     }
 
     @Test
-    void 볼이_1개_이상인_경우() {
+    void 스트라이크가_0개_이고_볼이_1개_이상인_경우() {
         // Arrange
+        List<Integer> computerNumber = List.of(1, 2, 3);
+        List<Integer> userNumber = List.of(2, 3, 4);
+        GameManager gameManager = new GameManager();
         // Action
+        String result = gameManager.isEndGame(computerNumber, userNumber);
         // Assert
+        assertThat(result).isEqualTo("2볼");
     }
 
     @Test
     void 스트라이크와_볼이_없는_경우() {
         // Arrange
+        List<Integer> computerNumber = List.of(1, 2, 3);
+        List<Integer> userNumber = List.of(4, 5, 6);
+        GameManager gameManager = new GameManager();
         // Action
+        String result = gameManager.isEndGame(computerNumber, userNumber);
         // Assert
+        assertThat(result).isEqualTo("낫싱");
     }
 }
