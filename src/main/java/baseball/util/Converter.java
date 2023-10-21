4d7ecd4e2ac615050bda1a0f78c45115b335c6dc
kimtaesoo <kimtaesoo7@naver.com>
2023-10-21 23:26:17 +0900

    feat: 문자열을 정수형 리스트로 변환 구현

---
+++ src/main/java/baseball/util/Converter.java
@@ -0,0 +1,18 @@
+package baseball.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Converter {
+
+    private static final String SEPARATOR = "";
+
+    private Converter() {
+    }
+
+    public static List<Integer> convertStringToIntegerList(final String string) {
+        return Arrays.stream(string.split(SEPARATOR))
+            .map(Integer::parseInt)
+            .toList();
+    }
+}
