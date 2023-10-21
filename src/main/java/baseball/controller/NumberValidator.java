834cf8ddb0e773d420fed08ae6c088400347a62c
yunju-m <dbswnfnal@naver.com>
2023-10-21 22:23:50 +0900

    feat(NumberValidator): 추가 입력한 데이터가 서로 다른 수인지 확인하는 함수

--- src/main/java/baseball/controller/NumberValidator.java
+++ src/main/java/baseball/controller/NumberValidator.java
@@ -1,15 +1,22 @@
 package baseball.controller;
 
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class NumberValidator {
     public static final int BASEBALL_NUMBER_LENGTH = 3;
     private static final String WRONG_LEGNTH_NUMBER_MESSAGE = "세 자리 숫자가 아닙니다."; 
     private static final String WRONG_RANGE_NUMBER_MESSAGE = "1~9 사이의 숫자만 입력해주세요.";
+    private static final String WRONG_SAME_NUMBER_MESSAGE = "서로 다른 세 자리 숫자만 입력해주세요.";
 
     public void toValidateNumber(String inputNumber) {
         if (!isSameLength(inputNumber)) {
             throw new IllegalArgumentException(WRONG_LEGNTH_NUMBER_MESSAGE);
         } else if (!isSameRange(inputNumber)) {
             throw new IllegalArgumentException(WRONG_RANGE_NUMBER_MESSAGE);
+        } else if (!isDiffNumber(inputNumber)) {
+            throw new IllegalArgumentException(WRONG_SAME_NUMBER_MESSAGE);
         }
     }
 
@@ -19,5 +26,12 @@ public class NumberValidator {
 
     public boolean isSameRange(String inputNumber) {
         return inputNumber.chars().allMatch(Character::isDigit);
+    }
+
+    public boolean isDiffNumber(String inputNumber) {
+        String[] numbers = inputNumber.split("");
+        Set<String> setNumbers = Arrays.stream(numbers).collect(Collectors.toSet());
+
+        return setNumbers.size() == BASEBALL_NUMBER_LENGTH;
     }
 }
