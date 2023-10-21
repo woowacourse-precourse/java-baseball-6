b780468a30079c8dd4825307d0094bf6c6577d77
koomchang <leahpar0401@gmail.com>
2023-10-21 19:35:16 +0900

    refactor: 랜덤 숫자 생성을 스트림으로 처리

--- src/main/java/baseball/model/RandomNumbers.java
+++ src/main/java/baseball/model/RandomNumbers.java
@@ -1,9 +1,9 @@
 package baseball.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class RandomNumbers {
 
@@ -13,26 +13,15 @@ public class RandomNumbers {
 
     private final List<Number> randomNumbers;
 
-
     public RandomNumbers() {
         this.randomNumbers = generateNewRandomNumbers();
     }
 
     private List<Number> generateNewRandomNumbers() {
-        List<Number> newRandomNumbers = new ArrayList<>();
-        do {
-            int randomValue = Randoms.pickNumberInRange(MIN_NUMERIC_VALUE, MAX_NUMERIC_VALUE);
-            Number randomNumber = new Number(randomValue);
-            newRandomNumbers.add(randomNumber);
-            newRandomNumbers = deduplicateNumbers(newRandomNumbers);
-        } while (newRandomNumbers.size() < RANDOM_NUMBER_LENGTH);
-        return newRandomNumbers;
-    }
-
-    private List<Number> deduplicateNumbers(List<Number> randomNumbers) {
-        return randomNumbers
-                .stream()
+        return Stream.generate(() -> Randoms.pickNumberInRange(MIN_NUMERIC_VALUE, MAX_NUMERIC_VALUE))
                 .distinct()
+                .limit(RANDOM_NUMBER_LENGTH)
+                .map(Number::new)
                 .collect(Collectors.toList());
     }
 
