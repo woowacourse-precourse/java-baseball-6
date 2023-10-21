27fb3dc0c3e733d73436fff50d842dd6f1656229
tam31 <wxogud3377@naver.com>
2023-10-22 04:02:38 +0900

    Feat(Player): Player의 뼈대 구현 및 어플리케이션 연결

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -1,13 +1,19 @@
 package baseball.controller;
 
 import baseball.model.Computer;
+import baseball.model.Player;
+import baseball.view.InputView;
 import baseball.view.OutputView;
 
 public class BaseballController {
 
     Computer computer = new Computer();
+    Player player = new Player();
     public void startGame(){
         OutputView.baseballGameStart();
         System.out.println(computer.getComputerNumber());
+        player.inputPlayerNumber(InputView.playerNumber());
+        System.out.println(player.getPlayerNumber());
+
     }
 }
--- src/main/java/baseball/model/Player.java
+++ src/main/java/baseball/model/Player.java
@@ -1,4 +1,18 @@
 package baseball.model;
 
+import baseball.validation.PlayerValidation;
+
+import java.util.List;
+
 public class Player {
+
+    private List<Integer> playerNumber;
+
+    public List<Integer> getPlayerNumber() {
+        return playerNumber;
+    }
+
+    public void inputPlayerNumber(String numbers){
+        playerNumber = PlayerValidation.checkNumber(numbers);
+    }
 }
