e1c265a072e4c285b9ff7a22c7ae5648c5a2c591
Kim Daehyeon <daehyeon3351@gmail.com>
2023-10-22 05:15:59 +0900

    refactor: BaseballGame 리팩터링에 따른 코드 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,13 +1,11 @@
 package baseball;
 
-import baseball.game.BaseballGame;
-
 public class Application {
     public static void main(String[] args) {
         BaseballGame baseballGame = new BaseballGame(3);
         do {
             baseballGame.start();
-        } while (baseballGame.restart());
+        } while (baseballGame.isRestart());
         baseballGame.end();
     }
 }
