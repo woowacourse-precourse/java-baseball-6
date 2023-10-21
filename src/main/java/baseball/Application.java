50372ca38295b37acae52cfcba4528df13b84ebf
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-10-21 21:44:04 +0900

    feat: add a class, GameManager

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,15 +2,9 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        MessageManager messageManager = new MessageManager();
-        Data data = new Data();
-        GameProcessor gameProcessor = new GameProcessor();
+        GameManager gameManager = new GameManager();
 
-        messageManager.showIntro();
-        data.setInputData(messageManager.getInputResponse());
-
-        gameProcessor.IllegalArgumentException(data);
+        gameManager.proceedIntro();
 
     }
 }
---
+++ src/main/java/baseball/GameManager.java
@@ -0,0 +1,19 @@
+package baseball;
+
+public class GameManager {
+    private MessageManager messageManager = new MessageManager();
+    private Data data = new Data();
+    private GameProcessor gameProcessor = new GameProcessor();
+
+    public void proceedIntro() {
+        messageManager.showIntro();
+        data.setInputData(messageManager.getInputResponse());
+
+        gameProcessor.IllegalArgumentException(data);
+    }
+
+    public void proceedMainGame() {
+
+    }
+
+}
