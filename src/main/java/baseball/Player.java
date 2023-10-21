e54cd33b89f95b928ebeaf0d890eae952e6108cb
Moly <102847513+jminkkk@users.noreply.github.com>
2023-10-21 16:15:57 +0900

    feat: 사용자의 숫자를 입력받는 메서드 작성
    요구사항1: 사용자가 입력하는 값은 camp.nextstep.edu.missionutils.Console의 readLine()을 활용한다.
    요구사항2: 게임 플레이어는 컴퓨터가 생각하고 있는 서로 다른 3개의 숫자를 입력한다.

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,12 +1,25 @@
 package baseball;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 
 public class Player {
     private static final Integer USER_NUMBER_SIZE = 3;
     private final List<Integer> userNumberList;
+
     public Player(List<Integer> userNumberList) {
         this.userNumberList = userNumberList;
     }
+
+    public List<Integer> loadListOf3Number() {
+        return userNumberList;
+    }
+
+    public int input3Number() {
+        return Integer.parseInt(readLine());
+    }
+
 }
