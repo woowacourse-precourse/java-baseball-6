b0483d2af1c78d196607e3022804cac7de8d33cd
배정현 <bae1004kin@gmail.com>
2023-10-21 19:53:39 +0900

    commit : 메시지 상수 클래스 생성 및 구현

---
+++ src/main/java/baseball/constant/MessageConst.java
@@ -0,0 +1,24 @@
+package baseball.constant;
+
+/**
+ * 메시지 상수 클래스
+ */
+public final class MessageConst {
+
+    public static final String START_GAME_MSG = "숫자 야구 게임을 시작합니다.";
+
+    public static final String INPUT_MSG = "숫자를 입력해주세요 : ";
+
+    public static final String BALL = "볼";
+
+    public static final String STRIKE = "스트라이크";
+
+    public static final String NOTHING = "낫싱";
+
+    public static final String SUCCESS_MSG = NumberConst.INPUT_LENGTH + "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    public static final String GAME_CONTROL_MSG = "게임을 새로 시작하려면 " + NumberConst.RESTART_NUM +
+            ", 종료하려면 " + NumberConst.EXIT_NUM + "를 입력하세요.";
+
+
+}
