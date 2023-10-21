6d2d5196ef2068c102f99dbc30b4456aab479d99
민주 <hinote444@gmail.com>
2023-10-21 18:03:02 +0900

    feat(Inputview): 플레이어에게 숫자 입력 받는 기능 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,4 +1,13 @@
 package baseball.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class InputView {
+    public static String inputBaseballNumber() {
+        return readLine();
+    }
+
+    public static String inputGameStateNumber() {
+        return readLine();
+    }
 }
