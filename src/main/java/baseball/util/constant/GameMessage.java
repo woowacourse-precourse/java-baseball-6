30781d7bb45f15b141b0c59ab5669d4e4a3e2863
khabh1 <kshababe1@gmail.com>
2023-10-21 23:55:46 +0900

    refactor(GameMessage): message를 final로 수정

--- src/main/java/baseball/util/constant/GameMessage.java
+++ src/main/java/baseball/util/constant/GameMessage.java
@@ -6,7 +6,7 @@ public enum GameMessage {
     END("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"),
     REQUEST_COMMAND("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
 
-    String message;
+    final String message;
 
     GameMessage(String message) {
         this.message = message;
