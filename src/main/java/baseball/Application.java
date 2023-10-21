9c84ce321a7f833536e7f715e6440a1601e86243
Nbowow <nbo5548@naver.com>
2023-10-21 22:43:53 +0900

    Refactor: 코드 리펙토링

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -23,8 +23,6 @@ public class Application {
                 }
             }
 
-//            System.out.println("computer = " + computer);
-
             Boolean isCorrect = Boolean.FALSE;
             while (!isCorrect){
                 int ball = 0;
@@ -33,10 +31,7 @@ public class Application {
                 System.out.print("숫자를 입력해주세요 : ");
                 int inputNum;
                 try {
-//                    Scanner num = new Scanner(System.in);
                     inputNum = Integer.parseInt(Console.readLine());
-//                    inputNum = num.nextInt();
-//                    System.out.println("inputNum = " + inputNum);
                 } catch (InputMismatchException e) {
                     throw new IllegalArgumentException("[ERROR] 숫자를 입력해주세요.");
                 }
@@ -51,7 +46,6 @@ public class Application {
                 }
                 for (int i = 0; i < 3; i++) {
 
-//                    System.out.println("c = " + intArray[i]);
                     if (computer.contains(intArray[i])) {
                         if (computer.get(i) == intArray[i]) {
                             //위치가 같으면
@@ -66,9 +60,7 @@ public class Application {
                     System.out.println("3스트라이크");
                     System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                     System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-//                    Scanner num2 = new Scanner(System.in);
                     int isReplaying = Integer.parseInt(Console.readLine());
-//                    int isReplaying = Integer.parseInt(num2.next());
                     if (isReplaying == 2) isPlaying = Boolean.FALSE;
                 }
                 else if (ball > 0 && strike > 0) System.out.println(ball + "볼 " + strike +"스트라이크");
