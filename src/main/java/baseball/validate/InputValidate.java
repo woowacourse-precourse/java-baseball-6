cea58f9503e1969e6c467acf18446281d2cd8b9f
chosunghyun18 <krismon@naver.com>
2023-10-21 14:15:20 +0900

    feat : add input validate check number

--- src/main/java/baseball/validate/InputValidate.java
+++ src/main/java/baseball/validate/InputValidate.java
@@ -1,4 +1,18 @@
 package baseball.validate;
 
 public class InputValidate {
+    public void checkNumbers(String givenNumbers) {
+        String[] numbers = givenNumbers.split("");
+        for(String number : numbers) {
+            checkNumber(number);
+        }
+    }
+
+    public void checkNumber(String givenNumber) {
+        try {
+            Integer.parseInt(givenNumber);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("[ERROR]" + givenNumber);
+        }
+    }
 }
--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,13 +1,24 @@
 package baseball.view;
+
 import baseball.validate.InputValidate;
 import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
     private final InputValidate inputValidate;
 
-    public InputView(){
+    public InputView() {
         inputValidate = new InputValidate();
     }
-    public String getPlayerGuessNumbers(){
-        return Console.readLine();
+
+    public String getPlayerGuessNumbers() {
+        String givenInput = Console.readLine();
+        inputValidate.checkNumbers(givenInput);
+        return givenInput;
+    }
+
+    public String getPlayerRestart() {
+        String givenInput = Console.readLine();
+        inputValidate.checkNumber(givenInput);
+        return givenInput;
     }
 }
