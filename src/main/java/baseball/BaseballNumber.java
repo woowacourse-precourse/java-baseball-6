00fa1ba475e94e90713509b306c92197fed4d0de
devhoya97 <diajbh123@gmail.com>
2023-10-21 12:47:56 +0900

    refactor : BaseballNumber 클래스 기능 추가 Baseball 객체의 기준에 맞는 randomBaseballNumber를 생성하는 스태틱 메서드를 추가함. Baseball 객체만으로는 연산이 어려우므로 Baseball 객체를 줬을 때, 대응하는 int 값을 알려주는 스태틱 메서드를 추가함.

--- src/main/java/baseball/BaseballNumber.java
+++ src/main/java/baseball/BaseballNumber.java
@@ -1,21 +1,32 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class BaseballNumber {
     private static final int MIN_BASEBALL_NUMBER = 1;
     private static final int MAX_BASEBALL_NUMBER = 9;
-
     private int baseballNumber;
+
 
     public BaseballNumber(int baseballNumber) {
         validateBaseballNumber(baseballNumber);
         this.baseballNumber = baseballNumber;
     }
 
-    public static void validateBaseballNumber(int baseballNumber) {
+    private static void validateBaseballNumber(int baseballNumber) {
         if (baseballNumber < MIN_BASEBALL_NUMBER || baseballNumber > MAX_BASEBALL_NUMBER) {
             throw new IllegalArgumentException(
                     "[ERROR] 숫자가 " + MIN_BASEBALL_NUMBER + "~" + MAX_BASEBALL_NUMBER + " 사이가 아닙니다."
             );
         }
+    }
+
+    public static int getBaseballNumberValue(BaseballNumber baseballNumber) {
+        return baseballNumber.baseballNumber;
+    }
+
+    public static BaseballNumber getRandomBaseballNumber() {
+        int randomNumber = Randoms.pickNumberInRange(MIN_BASEBALL_NUMBER, MAX_BASEBALL_NUMBER);
+        return new BaseballNumber(randomNumber);
     }
 }
