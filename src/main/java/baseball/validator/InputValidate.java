a9b857e1c6390341f3246801adea2dc8c7bbedf4
TaeHye0n <thk001229@gmail.com>
2023-10-21 21:09:20 +0900

    feat: 게임 재시작 또는 종료 커맨드 검증

--- src/main/java/baseball/validator/InputValidate.java
+++ src/main/java/baseball/validator/InputValidate.java
@@ -8,9 +8,13 @@ public class InputValidate {
     private static final int BEGIN_ASCII_CODE = 48; // 숫자 0
     private static final int END_ASCII_CODE = 57; // 숫자 9
     private static final int MAX_NUMBER_LENGTH = 3;
+    private static final int GAME_RESTART_COMMAND = 49;
+    private static final int GAME_END_COMMAND = 50;
+    private static final int GAME_COMMAND_LENGTH = 1;
     private static final String NON_NUMERIC_ERROR_MESSAGE = "숫자만 입력해주세요";
     private static final String LENGTH_ERROR_MESSAGE = "입력 길이가 일치하지 않습니다 입력 길이는 : ";
     private static final String DUPLICATE_ERROR_MESSAGE = "중복된 숫자가 포함되어 있습니다";
+    private static final String GAME_COMMAND_ERROR_MESSAGE = "올바른 명령이 아닙니다";
 
     public void validateNumeric(String input) {
         String stringValue = String.valueOf(input);
@@ -36,6 +40,18 @@ public class InputValidate {
         }
         if (characterSet.size() != MAX_NUMBER_LENGTH) {
             throw new IllegalArgumentException(DUPLICATE_ERROR_MESSAGE);
+        }
+    }
+
+    public void validateGameCommand(String input) {
+        String stringValue = String.valueOf(input);
+        int inputLength = input.length();
+        if (inputLength != GAME_COMMAND_LENGTH) {
+            throw new IllegalArgumentException(GAME_COMMAND_ERROR_MESSAGE);
+        }
+
+        if (!(stringValue.charAt(0) == GAME_RESTART_COMMAND || stringValue.charAt(0) == GAME_END_COMMAND)) {
+            throw new IllegalArgumentException(GAME_COMMAND_ERROR_MESSAGE);
         }
     }
 
