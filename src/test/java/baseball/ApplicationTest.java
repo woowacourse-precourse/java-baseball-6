4202fbec273f5f077071d4f120be7d08c3fdacc5
HyungUk Ryu <hw0603@naver.com>
2023-10-21 17:08:12 +0900

    Test: RandomUtil 테스트 코드 추가 난수 리스트의 길이 테스트 난수 범위 테스트

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -8,6 +8,8 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import baseball.utils.RandomUtil;
+
 class ApplicationTest extends NsTest {
     @Test
     void 게임종료_후_재시작() {
@@ -25,6 +27,20 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
                         .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 난수_생성후_개수_테스트() {
+        RandomUtil randomUtil = new RandomUtil();
+        assertThat(randomUtil.createRandomList().size()).isEqualTo(3);
+    }
+
+    @Test
+    void 난수_생성후_범위_테스트() {
+        RandomUtil randomUtil = new RandomUtil();
+        randomUtil.createRandomList().forEach(number ->
+                assertThat(number).isBetween(1, 9)
         );
     }
 
