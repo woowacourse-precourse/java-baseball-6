046ad29c5d7c9bb854640ffbd7ff8cc69c7fde0a
mangwonKing <aud1035@naver.com>
2023-10-22 00:06:55 +0900

    initCimputerNumber 추가

--- src/main/java/baseball/Data.java
+++ src/main/java/baseball/Data.java
@@ -1,2 +1,13 @@
-package baseball;public class Data {
+package baseball;
+
+import java.util.ArrayList;
+
+public class Data {
+    boolean isCreated = false;
+    public static ArrayList<Integer> computerNumber;
+    public static ArrayList<Integer> playerNumber;
+    public final int DO_RESTART = 1;
+
+    public int strike;
+    public int ball;
 }
--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,2 +1,24 @@
-package baseball;public class Game {
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+
+public class Game {
+
+    public static void startGame(){
+        System.out.println("게임시작");
+        initComputerNumber();
+
+    }
+    static void initComputerNumber(){
+        Data.computerNumber = new ArrayList<>();
+        while (Data.computerNumber.size() < 3){
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if(!Data.computerNumber.contains(randomNumber)){
+                Data.computerNumber.add(randomNumber);
+            }
+        }
+    }
+
 }
