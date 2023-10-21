a927bc35b0f32987f027c05e51b71575e60e3819
jcw1031 <jcw001031@gmail.com>
2023-10-21 22:58:36 +0900

    Feat: 입력 검증 + 주석 추가 + numbers 초기화

--- src/main/java/baseball/BaseballController.java
+++ src/main/java/baseball/BaseballController.java
@@ -2,6 +2,8 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.HashSet;
+
 public class BaseballController {
 
     private final BaseballView view;
@@ -14,21 +16,65 @@ public class BaseballController {
         this.model = model;
     }
 
+    /**
+     * 게임 시작
+     */
     public void startGame() {
         view.outputStartMessage();
     }
 
+    /**
+     * 게임 진행
+     */
     public void playGame() {
+        isPlaying = true;
         model.chooseRandomNumbers();
         while (isPlaying) {
             view.outputPlayMessage();
             String input = Console.readLine();
+            validateInput(input);
 
             HintResult hintResult = model.getHint(input);
             view.outputHintResultMessage(hintResult);
-
             if (hintResult.isThreeStrike()) {
                 isPlaying = false;
+            }
+        }
+    }
+
+    private void validateInput(String input) {
+        validateLength(input);
+        validateContainsZero(input);
+        validateContainsAlphabetic(input);
+        validateDuplicateNumber(input);
+    }
+
+    private void validateLength(String input) {
+        if (input.length() != BaseballModel.NUMBERS_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateContainsZero(String input) {
+        if (input.contains("0")) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateContainsAlphabetic(String input) {
+        for (char bit : input.toCharArray()) {
+            if (Character.isAlphabetic(bit)) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private void validateDuplicateNumber(String input) {
+        HashSet<String> set = new HashSet<>();
+        String[] inputNumbers = input.split("");
+        for (String inputNumber : inputNumbers) {
+            if (!set.add(inputNumber)) {
+                throw new IllegalArgumentException();
             }
         }
     }
--- src/main/java/baseball/BaseballModel.java
+++ src/main/java/baseball/BaseballModel.java
@@ -8,13 +8,17 @@ import java.util.List;
 
 public class BaseballModel {
 
-    private static final int NUMBERS_SIZE = 3;
+    public static final int NUMBERS_SIZE = 3;
     private static final int START_NUMBER = 1;
     private static final int END_NUMBER = 9;
 
     private final List<Integer> numbers = new ArrayList<>();
 
+    /**
+     * 랜덤한 수 3개 선택
+     */
     public void chooseRandomNumbers() {
+        numbers.clear();
         while (numbers.size() < NUMBERS_SIZE) {
             int randomNumber = Randoms.pickNumberInRange(START_NUMBER, END_NUMBER);
             if (!numbers.contains(randomNumber)) {
@@ -23,6 +27,12 @@ public class BaseballModel {
         }
     }
 
+    /**
+     * 힌트(볼, 스트라이크) 조회
+     *
+     * @param input 사용자 입력
+     * @return 힌트 결과
+     */
     public HintResult getHint(String input) {
         HintResult hintResult = new HintResult();
         List<Integer> inputNumbers = convertInputToList(input);
