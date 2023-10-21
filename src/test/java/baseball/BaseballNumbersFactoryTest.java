99926c6911b4f9f4f26dc9fb4099c99a64162c39
devhoya97 <diajbh123@gmail.com>
2023-10-21 13:50:55 +0900

    test : BaseballNumbersFactoryTest BaseballNumbersFactory 클래스의 스태틱 메서드들이 의도한 대로 동작하는지 테스트 코드 생성. 메서드들 중 getRandomBaseballNumbers 메서드 먼저 테스트함

--- src/test/java/baseball/BaseballNumbersFactoryTest.java
+++ src/test/java/baseball/BaseballNumbersFactoryTest.java
@@ -1,2 +1,29 @@
-package baseball;public class BaseballNumbersFactoryTest {
+package baseball;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class BaseballNumbersFactoryTest extends NsTest {
+    @Test
+    void getRandomBaseballNumbers_테스트() {
+        //given
+        //when
+        List<BaseballNumber> randomBaseballNumbers = BaseballNumbersFactory.getRandomBaseballNumbers();
+        List<Integer> result = new ArrayList<>();
+        for (BaseballNumber baseballNumber : randomBaseballNumbers) {
+            int baseballNumberValue = BaseballNumber.getBaseballNumberValue(baseballNumber);
+            result.add(baseballNumberValue);
+        }
+        //then
+        assertThat(result).doesNotHaveDuplicates().hasSize(3).allMatch(i -> i >= 1 && i <= 9);
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
 }
