19c5ecdaba5e24ed7cfa35c30b7513d3fcdf98b4
박성춘 <songchon100@gmail.com>
2023-10-21 17:39:47 +0900

    feat : 사용자 입력값 validator 기능 구현

--- src/main/java/baseball/util/GameInputValidator.java
+++ src/main/java/baseball/util/GameInputValidator.java
@@ -1,4 +1,21 @@
 package baseball.util;
 
-public class GameInputValidator {
+import java.util.regex.Pattern;
+
+public class GameInputValidator extends Validator{
+    private static final Pattern GAME_NUMBER_REGEX = Pattern.compile("^[1-9]{3}$");
+
+    @Override
+    public void validate(String inputNum) throws IllegalArgumentException {
+        // 공백 제거
+        String gameInputNum = removeSpace(inputNum);
+        validateEmptyInput(gameInputNum);
+        validateGameInputNum(gameInputNum);
+    }
+
+    private void validateGameInputNum(String gameInputNum) throws IllegalArgumentException{
+        if(!GAME_NUMBER_REGEX.matcher(gameInputNum).matches()) {
+            throw new IllegalArgumentException("Exception Message : 숫자야구 입력 형식에 맞춰주세요.");
+        }
+    }
 }
