5301f27fc3ffc8ec2ea053cfff56d09484428a63
yunju-m <dbswnfnal@naver.com>
2023-10-21 19:20:43 +0900

    feat(OutputView): 추가 게임 시작 메시지

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,10 @@
 package baseball;
 
+import baseball.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OutputView outputView = new OutputView();
+        outputView.printGameStart();
     }
 }
---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,9 @@
+package baseball.view;
+
+public class OutputView {
+    private static final String GameStartMessage = "숫자 야구 게임을 시작합니다.";
+
+    public void printGameStart() {
+        System.out.println(GameStartMessage);
+    }
+}
