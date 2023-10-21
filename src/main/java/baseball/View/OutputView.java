e356d82c0c8766f2c7a756d088e5fe0315ca9f93
kim <hwiyul_g@daum.net>
2023-10-21 20:26:28 +0900

    feat(OutputView) : 게임 중간 결과 안내 함수 추가

--- src/main/java/baseball/View/OutputView.java
+++ src/main/java/baseball/View/OutputView.java
@@ -6,6 +6,19 @@ public class OutputView {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
+    public void displayResult(int balls, int strikes) {
+
+        if (balls > 0 && strikes > 0) {
+            System.out.printf("%d볼 %d스트라이크\n", balls, strikes);
+        }else if (balls > 0) {
+            System.out.printf("%d볼\n", balls);
+        }else if (strikes > 0) {
+            System.out.printf("%d스트라이크\n", strikes);
+        }else if(balls == 0 && strikes == 0){
+            System.out.println("낫싱");
+        }
+    }
+
     public void displayWinningMessage() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
