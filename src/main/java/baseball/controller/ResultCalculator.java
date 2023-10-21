515da2feb53952fbc0d4ec55d7d533066a829e54
정성욱 <rkfka7849@gmail.com>
2023-10-21 19:05:28 +0900

    refactor(ResultCalculator): 스트라이크의 개수를 세는 기능과 볼의 개수를 세는 기능을 분화

--- src/main/java/baseball/controller/ResultCalculator.java
+++ src/main/java/baseball/controller/ResultCalculator.java
@@ -13,21 +13,29 @@ public class ResultCalculator {
     }
 
     public Result calculateResult(BallNumber comparisonNumber) {
-        int newStrikes = (int) IntStream
-                .range(0, SystemConstant.MAXIMUM_DIGIT)
-                .filter(i ->
-                        isStrike(comparisonNumber.getBallNumberByDigit(i), i))
-                .count();
-        int newBalls = (int) comparisonNumber.ballNumber
-                .stream()
-                .filter(this::ballInThisArea)
-                .count() - newStrikes;
+        int newStrikes = countStrikes(comparisonNumber);
+        int newBalls = countBalls(comparisonNumber, newStrikes);
 
         return new Result(newStrikes, newBalls);
     }
 
+    private int countStrikes(BallNumber comparisonNumber) {
+        return (int) IntStream
+                .range(0, SystemConstant.MAXIMUM_DIGIT)
+                .filter(i ->
+                        isStrike(comparisonNumber.getBallNumberByDigit(i), i))
+                .count();
+    }
+
     private boolean isStrike(int eachNumber, int digit) {
         return NumberComparator.isEqual(eachNumber, computerNumber.getBallNumberByDigit(digit));
+    }
+
+    private int countBalls(BallNumber comparisonNumber, int numberOfStrikes) {
+        return (int) comparisonNumber.ballNumber
+                .stream()
+                .filter(this::ballInThisArea)
+                .count() - numberOfStrikes;
     }
 
     private boolean ballInThisArea(int eachNumber) {
