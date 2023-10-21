7df7050538522c0ad503a4f17d805f74168bdcef
GaBaljaintheroom <junsu1222@naver.com>
2023-10-21 19:41:10 +0900

    refactor: 에러 출력 메시지 생성 추가

--- src/main/java/baseball/BaseballRole.java
+++ src/main/java/baseball/BaseballRole.java
@@ -2,6 +2,7 @@ package baseball;
 
 public enum BaseballRole {
     MAX_BASEBALL_NUMBER_SIZE(3),
+    THREE_STRIKE(3),
     MIN_BASEBALL_NUMBER(1),
     MAX_BASEBALL_NUMBER(9),
     GAME_RESTART(1),
@@ -24,7 +25,11 @@ public enum BaseballRole {
 
     private static void validateRestartMessage(int value){
         if(value != GAME_RESTART.value && value != GAME_EXIT.value){
-            throw new IllegalArgumentException(ErrorMessage.INPUT_RESTART_ERROR.getMessage());
+            throw new IllegalArgumentException(getRestartErrorMessage());
         }
+    }
+
+    private static String getRestartErrorMessage() {
+        return String.format(ErrorMessage.INPUT_RESTART_ERROR.getMessage(), GAME_RESTART.value, GAME_RESTART.value);
     }
 }
--- src/main/java/baseball/ErrorMessage.java
+++ src/main/java/baseball/ErrorMessage.java
@@ -3,7 +3,7 @@ package baseball;
 public enum ErrorMessage {
     INPUT_NUMERIC_ERROR("1부터 9사이의 숫자를 입력해 주세요"),
     INPUT_DIFFERENT_THREE_NUMBERS("서로 다른 수로 이루어진 %d자리의 수를 입력해 주세요"),
-    INPUT_RESTART_ERROR("숫자 1 혹은 2를 입력해 주세요");
+    INPUT_RESTART_ERROR("숫자 %d 혹은 %d를 입력해 주세요");
 
     private final String message;
 
