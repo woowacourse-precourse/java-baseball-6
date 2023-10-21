0a53d9eff678bf21e847f7719fd30929642dea82
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-10-22 04:39:41 +0900

    style(OutPutView) : 게임힌트제작에 관한걸 컨트롤러에 위임

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -6,9 +6,7 @@ public class OutputView {
     private static final String GAME_CLEAR_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String PLAYER_NUMBER_INPUT_PROMPT = "숫자를 입력해주세요 : ";
     private static final String CONTINUE_GAME_CHOICE_PROMPT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-    private static final String BALL = "볼";
-    private static final String STRIKE = "스트라이크";
-
+    
     public void printGameStartMessage() {
         System.out.println(GAME_START_MESSAGE);
     }
@@ -25,7 +23,7 @@ public class OutputView {
         System.out.println(CONTINUE_GAME_CHOICE_PROMPT);
     }
 
-    public void printBaseBallHint(int ballCount, int strikeCount) {
-        System.out.println(ballCount + BALL + " " + strikeCount + STRIKE);
+    public void printBaseBallHint(String hintMessage) {
+        System.out.println(hintMessage);
     }
 }
