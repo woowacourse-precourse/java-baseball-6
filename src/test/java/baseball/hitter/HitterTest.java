d98ffac8e31a15dd4482a247259691e2ce0c791c
HaLoomin <103578343+HaLoomin@users.noreply.github.com>
2023-10-22 02:59:49 +0900

    test 구성 및 IllegalACheckTest 일부 구성
    -

---
+++ src/test/java/baseball/hitter/HitterTest.java
@@ -0,0 +1,20 @@
+package baseball.hitter;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class HitterTest {
+
+    @BeforeEach
+
+    @Test
+    void hitterSwingTest(){
+
+    }
+//        PlayerHitterImpl hitter = new PlayerHitterImpl();
+//
+//        String bat = "";
+//
+//        System.out.println("입력:");
+//        bat = hitter.swing();
+}
---
+++ src/test/java/baseball/util/IllegalArgumentCheckTest.java
@@ -0,0 +1,21 @@
+package baseball.util;
+
+import baseball.AppConfig;
+import baseball.game.util.IllegalArgumentCheck;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class IllegalArgumentCheckTest {
+
+    IllegalArgumentCheck illegalArgumentCheck;
+
+    @BeforeEach
+    public void BeforeEach(){
+        AppConfig appConfig = new AppConfig();
+    }
+
+    @Test
+    void lengthCheck(){
+
+    }
+}
