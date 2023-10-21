32d46637205359c69cd058d92d7ba45e08a89b1f
shin5774 <babo5774@naver.com>
2023-10-21 21:24:03 +0900

    Feat: "게임 플레이 기능 구현"
    - 구현했던 기능들을 통합해서 게임 플레이 기능을 구현함.

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,10 +1,20 @@
 package baseball.controller;
 
+import baseball.domain.Game;
 import baseball.domain.Numbers;
+import baseball.domain.Result;
 import baseball.domain.Validator;
+import baseball.view.InputView;
+import baseball.view.OutputView;
 
 public class GameController {
-    public GameController() {}
+    private InputView inputView;
+    private OutputView outputView;
+
+    public GameController() {
+        inputView=new InputView();
+        outputView=new OutputView();
+    }
 
     public Numbers convertToNumbers(String values){
         return new Numbers(values);
@@ -18,5 +28,34 @@ public class GameController {
 
     private boolean isOne(String input){
         return input.equals("1");
+    }
+
+    public void start(){
+        while(true) {
+            Game game = new Game();
+            outputView.printProgramStartMessage();
+
+            while (true) {//입력과 결과확인
+                String userInput = inputView.inputNumbers();
+                Numbers user = new Numbers(userInput);
+                Result result = game.calculateResult(user);
+
+                outputView.printResult(result.getResult());
+
+                if (result.isThreeStrike()) {
+                    outputView.printFinishGameMessage();
+                    break;
+                }
+            }
+
+            //재시작 여부
+            String input = inputView.inputContinue();
+            Validator validator = new Validator();
+            validator.validateContinue(input);
+
+            if (!isOne(input)) {
+                break;
+            }
+        }
     }
 }
