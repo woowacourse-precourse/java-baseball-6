e4fe042a10d27c4104b8960b730eb4da73f5f5aa
kmh0601 <pongkmh@naver.com>
2023-10-21 22:46:07 +0900

    chore: 게임 재시작 여부 확인 메서드 콜 추가

--- src/main/java/baseball/InputPrint.java
+++ src/main/java/baseball/InputPrint.java
@@ -12,7 +12,7 @@ public class InputPrint{
 
         System.out.print("숫자를 입력해주세요 : ");
         userInput = readLine().split("");
-        ErrorCatcher.errorCheck(userInput, maxBallSize, minimumNum, maximumNum);
+        ErrorCatcher.ballErrorCheck(userInput, maxBallSize, minimumNum, maximumNum);
         for(String s : userInput) returnInput.add(Integer.parseInt(s));
         return returnInput;
     }
@@ -20,7 +20,8 @@ public class InputPrint{
         String userInput;
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         userInput = readLine();
-        if (userInput == "1") return true;
-        else return false;
+        ErrorCatcher.reGameErrorCheck(userInput);
+        if (userInput.equals("1")) return false;
+        else return true;
     }
 }
