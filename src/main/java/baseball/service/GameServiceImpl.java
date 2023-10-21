b90f154328af4b0013210e6957e67fa0cccc29a0
BullChallenger <am23am45@naver.com>
2023-10-22 00:00:29 +0900

    [REFACTOR] GameServiceImpl 내부의 코드 리팩토링
    GameServiceImpl의 책무를 분배받은 Referee 클래스가 생성됨에 따라 Referee 객체를 주입 받을 수 있도록 싱글톤 객체 생성 코드를 수정했다
    
    checkStatus 메소드에서 가독성을 해치던 if-else 문을 지우고 메소드 분리를 활용한 삼항 연산자로 대체했다
    
    기존에 사용했던 StringBuilder로 인하여 코드의 가독성이 떨어지는 것 같아서 StringBuilder가 아닌 String의 + 연산자를 활용하기로 했다

--- src/main/java/baseball/service/GameServiceImpl.java
+++ src/main/java/baseball/service/GameServiceImpl.java
@@ -1,5 +1,6 @@
 package baseball.service;
 
+import baseball.domain.Referee;
 import baseball.dto.JudgeDTO;
 import java.util.List;
 
@@ -10,69 +11,52 @@ public class GameServiceImpl implements GameService {
     private static final String NOTHING_STATUS_MESSAGE = "낫싱";
 
     private static final int CAPACITY = 3;
-    private static final int STRIKE = 0;
-    private static final int BALL = 1;
-    private static final int NOTHING = 2;
 
-    private static final GameService INSTANCE = new GameServiceImpl();
+    private final Referee referee;
 
-    private GameServiceImpl() {
+    private static GameService INSTANCE;
+
+    private GameServiceImpl(Referee referee) {
+        this.referee = referee;
     }
 
-    public static GameService getInstance() {
+    public static GameService getInstance(Referee referee) {
+        if (INSTANCE == null) {
+            INSTANCE = new GameServiceImpl(referee);
+        }
         return INSTANCE;
     }
 
     @Override
     public JudgeDTO process(List<Integer> numbers, int[] inputNumbers) {
-        boolean flag = false;
-        return judge(numbers, inputNumbers, flag);
+        return judge(numbers, inputNumbers);
     }
 
-    private JudgeDTO judge(List<Integer> numbers, int[] inputNumbers, boolean flag) {
-        int[] referee = new int[CAPACITY];
-        checkStatus(numbers, inputNumbers, referee);
-        return buildStatusAndMessage(referee, flag);
+    private JudgeDTO judge(List<Integer> numbers, int[] inputNumbers) {
+        referee.judge(numbers, inputNumbers);
+        return build();
     }
 
-    private void checkStatus(List<Integer> numbers, int[] inputNumbers, int[] referee) {
-        for (int i = 0; i < numbers.size(); i++) {
-            int currentTarget = inputNumbers[i];
-            if (numbers.get(i) == currentTarget) {
-                referee[STRIKE]++;
-            } else {
-                if (numbers.contains(currentTarget)) {
-                    referee[BALL]++;
-                } else {
-                    referee[NOTHING]++;
-                }
-            }
-        }
+    private JudgeDTO build() {
+        JudgeDTO judge = referee.getMiss() == CAPACITY ? buildNothing() : buildJudge();
+        referee.clearCount();
+        return judge;
     }
 
-    private JudgeDTO buildStatusAndMessage(int[] referee, boolean flag) {
-        StringBuilder builder = new StringBuilder();
-        if (referee[NOTHING] == CAPACITY) {
-            builder.append(NOTHING_STATUS_MESSAGE);
-        } else {
-            if (referee[BALL] != 0) {
-                builder.append(referee[BALL] + BALL_STATUS_MESSAGE);
-            }
-
-            if (referee[STRIKE] != 0) {
-                builder.append(referee[STRIKE] + STRIKE_STATUS_MESSAGE);
-                flag = allStrikeCheck(referee);
-            }
-        }
-
-        return new JudgeDTO(flag, builder.toString());
+    private JudgeDTO buildNothing() {
+        return new JudgeDTO(false, NOTHING_STATUS_MESSAGE);
     }
 
-    private boolean allStrikeCheck(int[] referee) {
-        if (referee[STRIKE] == CAPACITY) {
-            return true;
-        }
-        return false;
+    private JudgeDTO buildJudge() {
+        return new JudgeDTO(referee.isAllStrike(), buildBallMsg() + buildStrikeMsg());
+    }
+
+    private String buildStrikeMsg() {
+        return referee.getStrike() != 0 ? referee.getStrike() + STRIKE_STATUS_MESSAGE : "";
+    }
+
+    private String buildBallMsg() {
+        return referee.getBall() != 0 ? referee.getBall() + BALL_STATUS_MESSAGE : "";
     }
 
 }
