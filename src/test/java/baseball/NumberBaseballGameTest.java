fbc6e0a15018ce150ee25e72f662842301c3dd0d
sohyun9527 <sohyun95277@gmail.com>
2023-10-21 15:17:44 +0900

    test(NumberBaseballGameTest): 재시작 / 종료 기능에 대한 테스트 코드 작성

--- src/test/java/baseball/NumberBaseballGameTest.java
+++ src/test/java/baseball/NumberBaseballGameTest.java
@@ -3,9 +3,14 @@ package baseball;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class NumberBaseballGameTest {
+
+    NumberBaseballGame numberBaseballGame = new NumberBaseballGame();
 
     @DisplayName("게임 종료를 입력받기 전까지 진행되는 과정")
     @Test
@@ -16,4 +21,19 @@ class NumberBaseballGameTest {
     @Test
     void oneRound() {
     }
+
+    @DisplayName("재시작 입력에 대한 true 반환 테스트")
+    @Test
+    public void restartOrStopTest() throws Exception {
+        //given
+        String sign = "1";
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(sign.getBytes());
+        System.setIn(byteArrayInputStream);
+        //when
+        boolean result = numberBaseballGame.restartOrStop();
+        //then
+        assertThat(result).isEqualTo(true);
+
+    }
+
 }
