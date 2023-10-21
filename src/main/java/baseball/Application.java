106574b850abab95189c9e06aa2d833dc6284d56
limzzum <chfhddl1234@naver.com>
2023-10-22 02:26:31 +0900

    :sparkles: feat : 베이스볼 게임 정답 랜덤값 생성 메서드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,29 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        baseballGame();
+
     }
+
+    private static void baseballGame() {
+        List<Integer> answerNumber = createAnswerNumber();
+    }
+
+    private static List<Integer> createAnswerNumber() {
+        List<Integer> answer = new ArrayList<>();
+        while (answer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!answer.contains(randomNumber)) {
+                answer.add(randomNumber);
+            }
+        }
+        return answer;
+    }
+
 }
