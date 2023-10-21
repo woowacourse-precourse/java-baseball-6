e92093db33b1f6ddccbc8ff2d6867d2a3da1050f
jmj005 <jmj00005@naver.com>
2023-10-21 16:35:19 +0900

    refactor: ComputerTest 구조 변경

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -2,24 +2,17 @@ package baseball;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
-import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class ComputerTest {
     @Test
     public void testGenerateRandomNumbers() {
         Computer computer = new Computer();
-        List<Integer> randomNumbers = computer.generate();
-
-        StringBuilder sb = new StringBuilder();
-        for (Integer number : randomNumbers) {
-            sb.append(number);
-        }
-        String validNumbers = sb.toString();
+        String randomNumbers = computer.generate();
 
         BaseballGame baseballGame = new BaseballGame();
         try {
-            baseballGame.validateNumbers(validNumbers);
+            baseballGame.validateNumbers(randomNumbers);
         } catch (IllegalArgumentException e) {
             fail("예외가 발생했습니다: " + e.getMessage());
         }
