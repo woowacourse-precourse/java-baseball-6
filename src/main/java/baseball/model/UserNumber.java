942924979b34a30b0f27245e9242319c2dda3e88
shAn-kor <kjn0406@naver.com>
2023-10-21 16:07:34 +0900

    changeStringtoList UserNumber로 이동, StringtoList.java 삭제

--- src/main/java/baseball/model/UserNumber.java
+++ src/main/java/baseball/model/UserNumber.java
@@ -1,17 +1,34 @@
 package baseball.model;
 
-import static baseball.util.StringtoList.changeStringtoList;
+import static baseball.util.ValidationChecking.checkUserInputValidate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserNumber {
-    private List<Integer> inputNumber;
+    private List<Integer> userNumber;
 
     public UserNumber(String inputNumber) {
-        this.inputNumber = changeStringtoList(inputNumber);
+        this.userNumber = changeStringtoList(inputNumber);
+        checkUserInputValidate(this.userNumber);
     }
 
     public List<Integer> getInputNumber() {
-        return this.inputNumber;
+        return this.userNumber;
+    }
+
+    public static List<Integer> changeStringtoList(String string) {
+        return getIntegers(string);
+    }
+
+    public static List<Integer> getIntegers(String string) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < string.length(); i++) {
+            int currentInteger = (int) (string.charAt(i)) - '0';
+            list.add(currentInteger);
+        }
+
+        return list;
     }
 }
