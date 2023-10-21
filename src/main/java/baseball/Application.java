e1d1820d2f375a27abb5e7357404d5d7699196d3
Kim Mi Ju <76895039+miju0515@users.noreply.github.com>
2023-10-21 13:24:04 +0900

    feat: 숫자 입력 받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,7 +6,10 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
-
+        while(true){
+            System.out.print("숫자를 입력해주세요 : ");
+            String input = readLine();
+        }
 
 
     }
