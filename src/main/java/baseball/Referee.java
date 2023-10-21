18ce64e591ffb9839490d59874026394c6487a41
jmj005 <jmj00005@naver.com>
2023-10-22 01:18:09 +0900

    feat: 볼 확인 기능 추가

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -14,4 +14,19 @@ public class Referee {
     private boolean isStrike(String computer, String player, int index) {
         return computer.charAt(index) == player.charAt(index);
     }
+
+    public int getBallCount(String computer, String player) {
+        int ballCount = 0;
+        for (int index = 0; index < computer.length(); index++) {
+            if (isBall(computer, player, index)) {
+                ballCount++;
+            }
+        }
+        return ballCount;
+    }
+
+    private boolean isBall(String computer, String player, int index) {
+        return (computer.charAt(index) != player.charAt(index)) && computer.contains(
+                String.valueOf(player.charAt(index)));
+    }
 }
