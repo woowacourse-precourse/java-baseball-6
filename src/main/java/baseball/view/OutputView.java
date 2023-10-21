a1e6541c6295a0ba6fbb3514b023514010b30d91
tam31 <wxogud3377@naver.com>
2023-10-22 04:51:38 +0900

    Feat(OutputView): 스트라이크와 볼의 개수 메시지 뼈대 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -2,8 +2,19 @@ package baseball.view;
 
 public class OutputView {
     private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String BALL_MESSAGE = "볼";
+    private static final String STRIKE_MESSAGE = "스트라이크";
+    private static final String NONE_MESSAGE = "낫싱";
+
 
     public static void baseballGameStart(){
         System.out.println(START_MESSAGE);
+    }
+
+    public static void detailMessage(int strike, int ball){
+        isZeroStrikeAndBall(strike, ball);
+        isZeroStrike(strike, ball);
+        isZeroball(strike, ball);
+        isNotZeroStrikeAndBall(strike,ball);
     }
 }
