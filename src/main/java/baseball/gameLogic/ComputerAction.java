3a5199cb418d3ed3e445b81db82752af772762b2
jy016011 <39334789+jy016011@users.noreply.github.com>
2023-10-21 23:53:53 +0900

    refactor: RandomNumberGenerator 클래스 유틸화에 따른 코드 수정

--- src/main/java/baseball/gameLogic/ComputerAction.java
+++ src/main/java/baseball/gameLogic/ComputerAction.java
@@ -4,14 +4,17 @@ import baseball.models.Computer;
 
 import java.util.List;
 
+import static baseball.utils.RandomNumberGenerator.generateRandomNumberList;
+
 /*
   Computer의 행동들을 수행하는 클래스
 */
 
 public class ComputerAction {
-    public Computer computer = new Computer();
+    private final Computer computer = new Computer();
+
     public void generateNumber(){
-        computer.numberList = computer.randomNumberGenerator.generateRandomNumber();
+        computer.numberList = generateRandomNumberList();
     }
 
     public List<Integer> getNumberList(){
