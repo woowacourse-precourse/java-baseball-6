82dfabaeff2d75b22ca08c67779b7db8e2ec112e
yuseonjun <seonjun0906@gmail.com>
2023-10-21 20:47:30 +0900

    feat: Hint 클래스에 낫싱과 3스트라이크 메서드 구현

--- src/main/java/baseball/domain/Hint.java
+++ src/main/java/baseball/domain/Hint.java
@@ -2,12 +2,31 @@ package baseball.domain;
 
 public class Hint {
 
-	private int strikeCount;
-	private int ballCount;
+	private static final int ZERO = 0;
+	private static final int THREE_COUNT = 0;
+
+	private final int strikeCount;
+	private final int ballCount;
 
 	public Hint(int strikeCount, int ballCount) {
 		this.strikeCount = strikeCount;
 		this.ballCount = ballCount;
+	}
+
+	public boolean isStrikeCountZero() {
+		return strikeCount == ZERO;
+	}
+
+	public boolean isBallCountZero() {
+		return ballCount == ZERO;
+	}
+
+	public boolean isThreeStrike() {
+		return strikeCount == THREE_COUNT;
+	}
+
+	public boolean isNothing() {
+		return isBallCountZero() && isStrikeCountZero();
 	}
 
 	public int getStrikeCount() {
