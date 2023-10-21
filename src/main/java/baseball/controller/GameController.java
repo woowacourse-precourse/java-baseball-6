919d1ab3f199e0850b8ef1508ac3979b41fa2cff
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-10-21 18:43:24 +0900

    feat(controller) : ball,strike 횟수에 따라서 출력할 String 생성 기능 구현 - provideStrikeAndBallCount() : ball , strike 리스트를 받아 String으로 전환기능

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -9,6 +9,12 @@ import baseball.view.OutputView;
 import java.util.List;
 
 public class GameController {
+    private final static String NOTHING_STRING = "낫싱";
+    private final static String BALL_STRING = "볼";
+    private final static String STRIKE_STRING = "스트라이크";
+    private final static int COUNT_ZERO_VALUE = 0;
+    private final static int LIST_BALL_INDEX = 0;
+    private final static int LIST_STRIKE_INDEX = 1;
     private final static Computer computer = new Computer(new BaseballRandomNumberGenerator().generate());
     private final static InputView inputView = new InputView();
     private final static OutputView outputView = new OutputView();
@@ -16,5 +22,17 @@ public class GameController {
         outputView.printGameStartMessage();
     }
 
+    private static String provideStrikeAndBallCount(List<Integer> ballAndStrikeCount){
+        int ballCount = ballAndStrikeCount.get(LIST_BALL_INDEX);
+        int strikeCount = ballAndStrikeCount.get(LIST_STRIKE_INDEX);
+        if(ballCount == COUNT_ZERO_VALUE && strikeCount == COUNT_ZERO_VALUE){
+            return NOTHING_STRING;
+        } else if (ballCount != COUNT_ZERO_VALUE && strikeCount == COUNT_ZERO_VALUE) {
+            return String.valueOf(ballCount) + BALL_STRING;
+        } else if (ballCount == COUNT_ZERO_VALUE && strikeCount != COUNT_ZERO_VALUE) {
+            return String.valueOf(ballCount) + STRIKE_STRING;
+        }
+        return String.valueOf(ballCount) + BALL_STRING + " " + String.valueOf(ballCount) + STRIKE_STRING;
+    }
 
 }
