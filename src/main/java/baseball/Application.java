12f29b017cda044f2b536ff3669a201d8b7a0781
lsj5657 <lsj56575@naver.com>
2023-10-21 19:06:09 +0900

    3스트라이크 시에 입력을 받고 예외처리 하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -73,6 +73,20 @@ public class Application {
                 System.out.println("3스트라이크");
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                String continueOrEnd=Console.readLine();
+                if(continueOrEnd.length()!=1) error=true;
+
+                for (int i=0; i<continueOrEnd.length(); i++){
+                    if(continueOrEnd.charAt(i)!='1' || continueOrEnd.charAt(i)!=2) {
+                        error=true;
+                        break;
+                    }
+                }
+                try {
+                    if (error) throw new IllegalArgumentException();
+                }catch(IllegalArgumentException e){
+                    break;
+                }
             }
         }
 
