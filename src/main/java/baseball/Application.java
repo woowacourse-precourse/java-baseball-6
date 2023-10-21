628e4f2e082f31904ad701db7f7812e9a3267327
깃wooqqq <sge0225@naver.com>
2023-10-21 17:11:48 +0900

    style: 필요없는 import 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,5 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
