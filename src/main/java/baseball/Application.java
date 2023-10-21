faca42d17530fb149500514ee044d500223c8b3a
junho <ssmm0205@naver.com>
2023-10-22 04:40:07 +0900

    feat: 숫자 야구 게임 application 기능 구현 완료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import baseball.controller.BaseBallGame;
+import baseball.strategy.ForbidDuplicationPolicy;
+import baseball.strategy.RandomNumberGeneratePolicy;
+import baseball.view.ConsoleInput;
+import baseball.view.ConsoleOutput;
+
 public class Application {
+
+    private final static int NUMBER_OF_DIGITS = 3;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        new BaseBallGame(
+                new ConsoleInput(),
+                new ConsoleOutput()
+        ).run(
+                new RandomNumberGeneratePolicy(),
+                new ForbidDuplicationPolicy(),
+                NUMBER_OF_DIGITS
+        );
     }
 }
