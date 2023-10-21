4d6dab9c3af755dd124134c860e556e0a71eaf77
oxdjww <oxdjww@gmail.com>
2023-10-21 21:02:30 +0900

    refactor : computer data, user data 자료형 변경, generateRandom(), getUserInput() 함수 재구현

--- src/main/java/baseball/model/BaseballGame.java
+++ src/main/java/baseball/model/BaseballGame.java
@@ -1,17 +1,21 @@
 package baseball.model;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class BaseballGame {
-
-    private int randomNum;
+    private final List<Integer> computer = new ArrayList<>();
     private BaseballGameCounts baseballGameCounts;
-    private String userInput;
+    private final List<Integer> user = new ArrayList<>();
 
-    protected BaseballGame() {}
+    protected BaseballGame() {
+    }
 
     public static BaseballGame createBaseballGame() {
         return new BaseballGame();
@@ -21,8 +25,7 @@ public class BaseballGame {
         // 랜덤 수 생성
         this.generateRandomNum();
 
-        while(true)
-        {
+        while (true) {
             this.getUserInput();
 
             this.checkStrikes();
@@ -38,6 +41,10 @@ public class BaseballGame {
     }
 
     private void getUserInput() {
+        String userInput = readLine();
+        for (int i = 0; i < userInput.length(); i++) {
+            user.add(Character.getNumericValue(userInput.charAt(i)));
+        }
     }
 
     private void showCounts() {
@@ -52,31 +59,11 @@ public class BaseballGame {
     }
 
     private void generateRandomNum() {
-        while (true){
-            int randomNum = pickNumberInRange(100, 999);
-            // 유효한 RandomNum인지 검증 후 저장
-            if (isValidRandomNum(randomNum)) {
-                this.randomNum = randomNum;
-                break;
+        while (computer.size() < 3) {
+            int randomNumber = pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
             }
         }
-    }
-
-    private static boolean isValidRandomNum(int randomNum) {
-        // 0 ~ 9 숫자들의 등장 여부를 저장할 배열
-        boolean[] existingDigits = new boolean[10];
-        // 파라미터로 받은 랜덤 수가 0보다 클 때까지 루프를 돌며 검사
-        while (randomNum > 0) {
-            // 가장 우측 자리의 수를 추출하고, 등장했던 적이 있는지 if문으로 검사
-            int digit = randomNum % 10;
-            if (existingDigits[digit]) {
-                // 한번이라도 등장했다면 false
-                return false;
-            }
-            // 처음 등장하는 경우,
-            existingDigits[digit] = true;
-            randomNum /= 10;
-        }
-        return true;
     }
 }
