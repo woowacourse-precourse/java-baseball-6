6d8d3c1fb9f87059c69afc5099be768d72502e1b
junest66 <chlwndks333@gmail.com>
2023-10-21 17:33:59 +0900

    refactor: User restartGame 함수 타입 void -> boolean 수정

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -52,14 +52,15 @@ public class User {
         return numberList;
     }
 
-    public void restartGame() {
+    public boolean restartGame() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String input = Console.readLine();
         if (!(input.equals("1") || input.equals("2"))) {
             throw new IllegalArgumentException();
         }
         if (input.equals("2")) {
-            this.gameStatus = false;
+            return false;
         }
+        return true;
     }
 }
