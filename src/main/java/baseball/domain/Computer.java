5ef1fd28b07cde231811b138255830d63a9911ab
DaeYoungK <hanguk830@naver.com>
2023-10-21 16:06:32 +0900

    feat(Computer): 플레이어가 맞춰야하는 숫자 생성 기능 추가

---
+++ src/main/java/baseball/domain/Computer.java
@@ -0,0 +1,33 @@
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+
+    private int computerNumber;
+    private List<Integer> computerNumbers;
+
+    public void setComputerNumber() {
+        computerNumber = makeNumber();
+    }
+
+    public int getComputerNumber() {
+        return computerNumber;
+    }
+
+    private int makeNumber() {
+        computerNumbers = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        while (computerNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumbers.contains(randomNumber)) {
+                sb.append(randomNumber);
+                computerNumbers.add(randomNumber);
+            }
+        }
+        return Integer.parseInt(String.valueOf(sb));
+    }
+}
