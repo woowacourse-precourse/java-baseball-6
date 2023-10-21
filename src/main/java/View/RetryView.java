337b2b72b1358c1f9e91c1e951c0d97bc1761ab3
김영준 <rladudwns164@gmail.com>
2023-10-21 23:18:22 +0900

    feat : 사용자 재시작 번호 입력 기능 추가
    -입력 후 유효성 검사 기능 추가
    -재시작 숫자 입력시 참 반환
    -enum클래스에 RETRY/ENDGAME 조건 숫자 추가

--- src/main/java/View/RetryView.java
+++ src/main/java/View/RetryView.java
@@ -1,5 +1,9 @@
 package View;
 
+import static Util.Validator.validateMenuChoice;
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class RetryView {
 
     private static final RetryView INSTANCE = new RetryView();
@@ -11,12 +15,24 @@ public class RetryView {
         return INSTANCE;
     }
 
-    public void printRetryMessage() {
+    public boolean printRetryMessage() {
         System.out.println(Message.GAME_END_RETRY.getMessage());
+        String retryNumber = Console.readLine();
+        validateMenuChoice(retryNumber);
+        if(retryNumber.equals(Message.RETRYGAME.getMessage())){
+            return true;
+        }
+        if(retryNumber.equals(Message.ENDGAME.getMessage())){
+            return false;
+        }
+
+        return false;
     }
 
     public enum Message {
-        GAME_END_RETRY("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        GAME_END_RETRY("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."),
+        RETRYGAME("1"),
+        ENDGAME("2");
 
         private String message;
 
