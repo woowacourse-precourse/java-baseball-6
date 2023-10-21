140cfc4e49cbc59a1a6b5843f9a971814635453a
tangba484 <tangba484@naver.com>
2023-10-22 05:17:31 +0900

    게임 종료 후 status갱신 기능

--- src/main/java/baseball/io/Input.java
+++ src/main/java/baseball/io/Input.java
@@ -1,13 +1,26 @@
 package baseball.io;
+import baseball.model.Status;
 import camp.nextstep.edu.missionutils.Console;
 
 
 public class Input {
 
-    private static final String Input_Message = "숫자를 입력해주세요 : ";
+    private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private final Status status;
+
+    public Input(Status status) {
+        this.status = status;
+
+    }
+
     public String readLine(){
-        System.out.print(Input_Message);
+        System.out.print(INPUT_MESSAGE);
         return Console.readLine();
+    }
+
+    public void userChoice(){
+        int userChoice = Integer.parseInt(Console.readLine());
+        status.setStatus(userChoice);
     }
 }
 
