22c72e87f3d034f75aabcd9d433c53cb38a61b1a
eom-tae-in <eti0728@daum.net>
2023-10-21 19:31:12 +0900

    feat: 자주 사용되는 상수를 재사용하기 위한 Constant 클래스 추가

---
+++ src/main/java/baseball/constant/Constant.java
@@ -0,0 +1,11 @@
+package baseball.constant;
+
+public class Constant {
+
+    public static final int START_INCLUSIVE = 1;
+    public static final int END_INCLUSIVE = 9;
+    public static final int BASEBALL_GAME_NUMBER_DIGIT = 3;
+
+    private Constant() {
+    }
+}
