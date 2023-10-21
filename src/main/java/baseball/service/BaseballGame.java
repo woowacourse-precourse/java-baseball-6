82d21fd35fa75195d7dc0238a8de23984e579c1f
민주 <hinote444@gmail.com>
2023-10-21 18:37:24 +0900

    feat(BaseballGame): 생성된 상대방 숫자를 domain에 저장하는 기능 구현

--- src/main/java/baseball/service/BaseballGame.java
+++ src/main/java/baseball/service/BaseballGame.java
@@ -1,10 +1,16 @@
 package baseball.service;
 
+import baseball.domain.Computer;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BaseballGame {
+    private Computer computer = new Computer();
+
+    public void startGame() {
+        computer.setBaseballNumbers(createComputerNumbers());
+    }
 
     private List<Integer> createComputerNumbers() {
         List<Integer> computer = new ArrayList<>();
