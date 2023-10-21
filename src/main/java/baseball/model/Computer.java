c51fc15cd8e3fcdc75e32439d909219932f273ca
jeondui <jeondui@gmail.com>
2023-10-22 01:57:16 +0900

    refactor(Computer): 상수 저장부분 추가

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -6,12 +6,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
+    private static final int BALL_NUMBER_LENGTH = 3;
+    private static final int BALL_MIN_NUMBER = 1;
+    private static final int BALL_MAX_NUMBER = 9;
     private List<Integer> computerNumbers;
 
     public List<Integer> generateComputerNumbers() {
         List<Integer> computerNumbers = new ArrayList<>();
 
-        while (computerNumbers.size() < 3) {
+        while (computerNumbers.size() < BALL_NUMBER_LENGTH) {
             int randomNumber = randomNumberGenerator();
             if (isDifferentNumber(computerNumbers, randomNumber)) {
                 computerNumbers.add(randomNumber);
@@ -22,7 +25,7 @@ public class Computer {
 
     // 난수 생성기
     private int randomNumberGenerator() {
-        return Randoms.pickNumberInRange(1, 9);
+        return Randoms.pickNumberInRange(BALL_MIN_NUMBER, BALL_MAX_NUMBER);
     }
 
     // 새로 생성한 숫자가 이미 존재하는지 중복을 체크한다.
