0807dc73f85c4e6373096ebde149f941fd838b92
김승진 <ohksj77@gmail.com>
2023-10-21 22:08:01 +0900

    feat(AnswerNumbers): 스트라이크 볼 여부 확인 기능 추가

--- src/main/java/baseball/domain/AnswerNumbers.java
+++ src/main/java/baseball/domain/AnswerNumbers.java
@@ -1,5 +1,7 @@
 package baseball.domain;
 
+import baseball.constant.StrikeBall;
+
 import java.util.List;
 
 public final class AnswerNumbers {
@@ -8,5 +10,19 @@ public final class AnswerNumbers {
 
     public AnswerNumbers(final List<Integer> numbers) {
         this.numbers = new Numbers(numbers);
+    }
+
+    public StrikeBall checkStrikeBall(final Number number, final int index) {
+        if (numbers.contains(number)) {
+            return checkIfStrike(number, index);
+        }
+        return StrikeBall.NONE;
+    }
+
+    private StrikeBall checkIfStrike(final Number number, final int index) {
+        if (numbers.hasSameIndexAndValue(number, index)) {
+            return StrikeBall.STRIKE;
+        }
+        return StrikeBall.BALL;
     }
 }
