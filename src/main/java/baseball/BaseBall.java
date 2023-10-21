3cd30e8bb9db744c86ce102428de8bf0b1faa0ad
gengminy <kls1998@naver.com>
2023-10-21 21:50:03 +0900

    feat: 야구 게임 클래스 구현

--- src/main/java/baseball/BaseBall.java
+++ src/main/java/baseball/BaseBall.java
@@ -5,20 +5,94 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RandomNumber {
-    private RandomNumber() {}
+public class BaseBall {
 
-    public static List<Integer> generate(int length) {
-        List<Integer> randomNumbers = new ArrayList<>();
+    private static final String ONLY_STRIKE = "%d스트라이크";
+    private static final String ONLY_BALL = "%d볼";
+    private static final String BALL_AND_STRIKE = "%d볼 %d스트라이크";
+    private static final String DONE = "%d개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String NONE = "낫싱";
+
+    private final int length;
+    private List<Integer> computer;
+    private boolean isOver;
+
+    public BaseBall(int length) {
         if (length <= 0) {
-            throw new IllegalArgumentException("길이는 1 이상이어야 합니다.");
+            throw new IllegalArgumentException("입력 길이는 양수이어야 합니다.");
         }
-        while (randomNumbers.size() < length) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!randomNumbers.contains(randomNumber)) {
-                randomNumbers.add(randomNumber);
+        this.length = length;
+        this.computer = RandomNumber.generate(length);
+        this.isOver = false;
+    }
+
+    public void newGame() {
+        computer = RandomNumber.generate(length);
+        isOver = false;
+    }
+
+    public void doGame(List<Integer> player) {
+        validateInput(player);
+        int ball = getBallCount(player);
+        int strikes = getStrikeCount(player);
+        printResult(ball, strikes);
+        if (strikes == length) {
+            finishGame();
+        }
+    }
+
+    private void printResult(int balls, int strikes) {
+        if (balls > 0 && strikes > 0) {
+            System.out.println(String.format(BALL_AND_STRIKE, balls, strikes));
+        } else if (balls > 0) {
+            System.out.println(String.format(ONLY_BALL, balls));
+        } else if (strikes > 0) {
+            System.out.println(String.format(ONLY_STRIKE, strikes));
+        } else {
+            System.out.println(String.format(NONE, strikes));
+        }
+    }
+
+    public void finishGame() {
+        System.out.println(String.format(DONE, length));
+        isOver = true;
+    }
+
+    public boolean isOver() {
+        return isOver;
+    }
+
+    private int getBallCount(List<Integer> player) {
+        int balls = 0;
+        for (int i = 0; i < player.size(); i++) {
+            final int playerNumber = player.get(i);
+            int computerNumberIndex = computer.indexOf(playerNumber);
+            if (computerNumberIndex > 0 && computerNumberIndex != i) {
+                balls++;
             }
         }
-        return randomNumbers;
+        return balls;
+    }
+
+    private int getStrikeCount(List<Integer> player) {
+        int strikes = 0;
+        for (int i = 0; i < player.size(); i++) {
+            final int playerNumber = player.get(i);
+            int computerNumberIndex = computer.indexOf(playerNumber);
+            if (computerNumberIndex == i) {
+                strikes++;
+            }
+        }
+        return strikes;
+    }
+
+    private void validateInput(List<Integer> player) {
+        if (computer == null || player == null) {
+            throw new IllegalArgumentException("올바르지 않은 입력값입니다.");
+        } else if (computer.isEmpty() || player.isEmpty()) {
+            throw new IllegalArgumentException("입력값은 빈 배열일 수 없습니다.");
+        } else if (computer.size() != player.size()) {
+            throw new IllegalArgumentException("입력 길이가 일치하지 않습니다.");
+        }
     }
 }
