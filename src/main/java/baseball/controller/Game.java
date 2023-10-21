0604dfbb401133b0ddbb08684df81d73ff92a5a2
kimdohyung <pdohyung@gmail.com>
2023-10-22 02:46:34 +0900

    feat: 게임을 시작하고 종료하면 재시작 여부를 물어보는 기능 구현

--- src/main/java/baseball/controller/Game.java
+++ src/main/java/baseball/controller/Game.java
@@ -1,4 +1,41 @@
 package baseball.controller;
 
+import baseball.model.ComputerNumber;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class Game {
+    private static boolean gameFlag = true;
+
+    private static final String AGAIN = "1";
+    private static final String STOP = "2";
+
+    private final ComputerNumber computerNumber;
+
+    private final Play play;
+
+    public Game() {
+        this.computerNumber = new ComputerNumber();
+        this.play = new Play();
+    }
+
+    public void game() {
+        while (gameFlag) {
+            OutputView.gameStart();
+            computerNumber.newComputerNumber();
+            play.play(computerNumber);
+            OutputView.gameFinish();
+            checkAgainOrStop(InputView.againOrStopInput());
+        }
+    }
+
+    private void checkAgainOrStop(String input) {
+        if (input.equals(AGAIN)) {
+            gameFlag = true;
+        }
+
+        if (input.equals(STOP)) {
+            gameFlag = false;
+        }
+    }
 }
