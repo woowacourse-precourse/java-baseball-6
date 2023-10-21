84074c61815fcaa56a84015ef02f244e8825f4ac
JoonKyo <joonkyolee@gmail.com>
2023-10-22 03:13:09 +0900

    style(RestartNumberValidator): 세미콜론 추가

--- src/main/java/baseball/model/RestartNumberValidator.java
+++ src/main/java/baseball/model/RestartNumberValidator.java
@@ -7,21 +7,21 @@ public class RestartNumberValidator {
     private static final String FINISH_NUMBER = "2";
     private static final int RESTART_FINISH_NUMBER_LENGTH = 1;
 
-    public static boolean validate(String restartNumber){
-        if(!hasValidLength(restartNumber)){
+    public static boolean validate(String restartNumber) {
+        if (!hasValidLength(restartNumber)) {
             throw new IllegalArgumentException(INPUT_LENGTH_IS_NOT_ONE);
         }
-        if(!isInValidRange(restartNumber)){
+        if (!isInValidRange(restartNumber)) {
             throw new IllegalArgumentException(INPUT_IS_NOT_VALID_NUMBER);
         }
         return RESTART_NUMBER.equals(restartNumber);
     }
 
-    private static boolean hasValidLength(String restartNumber){
-        return restartNumber.length() == RESTART_FINISH_NUMBER_LENGTH
+    private static boolean hasValidLength(String restartNumber) {
+        return restartNumber.length() == RESTART_FINISH_NUMBER_LENGTH;
     }
 
-    private static boolean isInValidRange(String restartNumber){
+    private static boolean isInValidRange(String restartNumber) {
         return RESTART_NUMBER.equals(restartNumber) || FINISH_NUMBER.equals(restartNumber);
     }
 }
