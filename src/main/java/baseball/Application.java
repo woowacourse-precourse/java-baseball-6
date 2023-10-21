ee1e823c4f9a55d0d557aac3fbaaa2d0adb04534
LEM <ppoppo9802@naver.com>
2023-10-21 21:56:29 +0900

    컴퓨터가 랜덤한 숫자 3개를 고르는 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    public static class Computer {
+        public List<Integer> computerNumber() {
+            List<Integer> number = new ArrayList<>();
+            while (number.size() < 3) {
+                int randomNumber = Randoms.pickNumberInRange(1, 9);
+                if (!number.contains(randomNumber)) {
+                    number.add(randomNumber);
+                }
+            }
+            return number;
+        }
+    }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Computer com = new Computer();
+        System.out.println(com.computerNumber().toString());
     }
 }
