154dd3e01c057dc1060040b394a0ccc6edc90fd5
m5s3 <devmyoungsoo2@gmail.com>
2023-10-21 16:26:54 +0900

    feat: 상대방(컴퓨터)의 수와 입력 받은 수 비교 로직 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,28 +3,46 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.regex.Pattern;
 
 public class Application {
+
     public Application() {
 
     }
 
     public static void main(String[] args) {
-        playGame();
+        startGame();
     }
 
-    public static void playGame() {
+    enum PlayResult {
+        BALL("볼"), STRIKE("스트라이크"), NOTHING("낫싱");
+
+        PlayResult(String text) {
+            this.label = text;
+        }
+
+        private final String label;
+
+        public String getLabel() {
+            return this.label;
+        }
+    }
+
+    public static void startGame() {
         try {
             boolean flag = true;
             while (flag) {
                 System.out.println("숫자 야구 게임을 시작합니다.");
-                String number = requestNumber();
-                List<Integer> computerNumbers = generateComputerNumbers();
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                playGame();
+//                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 flag = isRestartGame();
             }
@@ -35,11 +53,38 @@ public class Application {
         }
     }
 
-    private static String requestNumber() {
+    private static void playGame() {
+        List<Integer> computerNumbers = generateComputerNumbers();
+
+        System.out.println(computerNumbers);
+
+        boolean isAnswerRight = false;
+        while (!isAnswerRight) {
+            List<Integer> userNumbers = requestUserNumbers();
+            Map<PlayResult, Integer> gameResult = new HashMap<>();
+
+            System.out.println(userNumbers);
+
+            for (int index = 0; index < computerNumbers.size(); index++) {
+                int computerNumber = computerNumbers.get(index);
+                PlayResult playResult = compareComputerAndUserNumbers(computerNumber, userNumbers, index);
+                gameResult.put(playResult, gameResult.getOrDefault(playResult, 0) + 1);
+            }
+
+            printGameResultMessage(gameResult);
+            isAnswerRight = checkAnswer(gameResult);
+        }
+    }
+
+    private static List<Integer> generateComputerNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 9, 3);
+    }
+
+    private static List<Integer> requestUserNumbers() {
         System.out.print("숫자를 입력해 주세요 : ");
         String input = Console.readLine();
         validateInput(input);
-        return input;
+        return convertStringToList(input);
     }
 
     private static void validateInput(String input) {
@@ -49,7 +94,7 @@ public class Application {
 
     private static void isThreeDigit(String input) {
         if (!Pattern.matches("^\\d{3}$", input)) {
-            throw new IllegalArgumentException("입력값이 잘못 되었습니다.");
+            throw new IllegalArgumentException("3자리의 수가 아닙니다.");
         }
     }
 
@@ -60,21 +105,87 @@ public class Application {
         }
 
         if (numbers.size() != 3) {
-            throw new IllegalArgumentException("입력값이 잘못 되었습니다.");
+            throw new IllegalArgumentException("서로 다른 3자리의 수가 아닙니다.");
         }
     }
 
     private static List<Integer> convertStringToList(String input) {
         List<Integer> list = new ArrayList<>();
-        for (char number : input.toCharArray()) {
-            list.add((int) number);
+        for (Character character : input.toCharArray()) {
+            list.add(convertCharToInt(character));
         }
         return list;
     }
 
-    private static List<Integer> generateComputerNumbers() {
-        return Randoms.pickUniqueNumbersInRange(1, 9, 3);
+    private static int convertCharToInt(Character character) {
+        return character - '0';
     }
+
+    private static PlayResult compareComputerAndUserNumbers(int computerNumber, List<Integer> userNumbers, int index) {
+        if (isNothing(computerNumber, userNumbers)) {
+            return PlayResult.NOTHING;
+        }
+
+        if (isStrike(userNumbers.get(index), computerNumber)) {
+            return PlayResult.STRIKE;
+        }
+
+        return PlayResult.BALL;
+    }
+
+    private static boolean isNothing(int computerNumber, List<Integer> userNumbers) {
+        return !userNumbers.contains(computerNumber);
+    }
+
+    private static boolean isStrike(int userNumber, int computerNumber) {
+        return userNumber == computerNumber;
+    }
+
+    private static boolean checkAnswer(Map<PlayResult, Integer> gameResult) {
+        if (isRight(gameResult)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isRight(Map<PlayResult, Integer> gameResult) {
+        if (!gameResult.containsKey(PlayResult.STRIKE)) {
+            return false;
+        }
+        return gameResult.get(PlayResult.STRIKE) == 3;
+    }
+
+    private static void printGameResultMessage(Map<PlayResult, Integer> gameResult) {
+        StringBuilder gameResultMessage = generateGameResultMessage(gameResult);
+        System.out.println(gameResultMessage);
+    }
+
+
+    private static StringBuilder generateGameResultMessage(Map<PlayResult, Integer> gameResult) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        if (
+            gameResult.containsKey(PlayResult.NOTHING) &&
+            gameResult.get(PlayResult.NOTHING) == 3
+        ) {
+            stringBuilder.append(PlayResult.NOTHING.getLabel());
+            return stringBuilder;
+        }
+
+        Arrays.sort(gameResult.keySet().toArray());
+
+        for (Entry<PlayResult, Integer> playResultIntegerEntry : gameResult.entrySet()) {
+            if (playResultIntegerEntry.getKey() == PlayResult.NOTHING) {
+                continue;
+            }
+            stringBuilder
+                    .append(playResultIntegerEntry.getValue())
+                    .append(playResultIntegerEntry.getKey().getLabel());
+        }
+
+        return stringBuilder;
+    }
+
 
     private static boolean isRestartGame() {
         int input = readUserInput();
