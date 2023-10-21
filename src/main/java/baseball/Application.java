7ada01e31c5edbb28d757f177ab8039bf97d8e1a
rlagudwls5518 <66249379+rlagudwls5518@users.noreply.github.com>
2023-10-21 22:02:25 +0900

    dfdf

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,6 +7,7 @@ public class Application {
     public static void main(String[] args) 
     {
     	System.out.println("숫자 야구 게임을 시작합니다.");
+    	System.out.println("숫자 야구 게임을 시작합니다.");
         // TODO: 프로그램 구현
         startGame();
     }
