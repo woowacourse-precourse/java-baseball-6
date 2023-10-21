689cbc3cee8ee541bdf823d92ba9804b93ba4b0f
answer <answer3dw@gmail.com>
2023-10-21 18:04:38 +0900

    feat : 게임 재시작 입력 뷰 생성

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -8,4 +8,9 @@ public class InputView {
         return Console.readLine();
     }
 
+    public static String setRetryNumber(){
+        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력해주세요 : ");
+        return Console.readLine();
+    }
+
 }
