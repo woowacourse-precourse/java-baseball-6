7f25d31c56e7e7fa725b48cab725386ae757c0ae
yubin <dbqls2821@gmail.com>
2023-10-21 21:17:11 +0900

    refactor: Digits의 생성자를 정적 팩터리 메소드로 변경

--- src/main/java/baseball/Digits.java
+++ src/main/java/baseball/Digits.java
@@ -4,20 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 final class Digits {
-    private final List<Digit> digits = new ArrayList<>();
+    private List<Digit> digits;
 
-    Digits() {
-        while (digits.size() < Size.THREE.num) {
+//    Digits() {
+//        while (digits.size() < Size.THREE.num) {
+//            Digit randomDigit = new Digit();
+//            if (!digits.contains(randomDigit)) digits.add(randomDigit);
+//        }
+//    }
+
+    private Digits() {
+    }
+
+    static Digits generateRandomDigits() {
+        Digits random = new Digits();
+        random.digits = new ArrayList<>();
+        while (random.digits.size() < Size.THREE.num) {
             Digit randomDigit = new Digit();
-            if (!digits.contains(randomDigit)) digits.add(randomDigit);
+            if (!random.digits.contains(randomDigit)) random.digits.add(randomDigit);
         }
+//        System.out.println(random.digits.get(0).digit + " " + random.digits.get(1).digit + " " + random.digits.get(2).digit);
+        return random;
     }
 
     //TODO : 정적팩터리로?
     Digits(String input) {
         for (int i = 0; i < input.length(); i++) {
             Digit digit = new Digit(input.charAt(i));
-            if(digits.contains(digit)) throw new IllegalArgumentException();
+            if (digits.contains(digit)) throw new IllegalArgumentException();
             digits.add(digit);
         }
     }
