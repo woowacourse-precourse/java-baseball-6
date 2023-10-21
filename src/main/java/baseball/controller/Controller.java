fb746a2cf200199826bd4e5eaeb4a2400aff1ee6
kimtaesoo <kimtaesoo7@naver.com>
2023-10-22 00:55:32 +0900

    feat: 컨트롤러 흐름 정리

--- src/main/java/baseball/controller/Controller.java
+++ src/main/java/baseball/controller/Controller.java
@@ -2,9 +2,13 @@ package baseball.controller;
 
 import baseball.model.Computer;
 import baseball.model.GameCommand;
+import baseball.model.UserNumbers;
 import baseball.util.RandomNumberGenerator;
 import baseball.view.InputView;
 import baseball.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Controller {
 
@@ -26,8 +30,28 @@ public class Controller {
     }
 
     private void playGame(final Computer computer) {
+        List<Integer> inputNumbers = new ArrayList<>();
 
+        while (!isGameOver(computer, inputNumbers)) {
+            inputNumbers = getInputNumbers();
+            int ballCount = computer.getBallCount(inputNumbers);
+            int strikeCount = computer.getStrikeCount(inputNumbers);
+            // todo 계산로직
+
+        }
+        outputView.printEnd();
     }
+
+    private boolean isGameOver(final Computer computer, final List<Integer> inputNumbers) {
+        return computer.isThreeStrikes(inputNumbers);
+    }
+
+    private List<Integer> getInputNumbers() {
+        String numbers = inputView.readLine();
+        UserNumbers user = UserNumbers.createWithInput(numbers);
+        return user.getNumbers();
+    }
+
 
     private GameCommand askForRestart() {
         String choice = inputView.readLine();
