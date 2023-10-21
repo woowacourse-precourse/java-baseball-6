5633343b86d03a7893941867cb3d654809e27358
김영준 <rladudwns164@gmail.com>
2023-10-21 23:28:14 +0900

    refactor : 기존의 메소드 삭제 및 코드 간결화
    -숫자 비교후 결과 출력 기능 삭제
    -게임 종료 조건에 부합한지 체크하는 기능 삭제
    -재시작 화면 입력 및 출력 기능 삭제

--- src/main/java/Controller/MainController.java
+++ src/main/java/Controller/MainController.java
@@ -1,16 +1,15 @@
 package Controller;
 
-import static Util.Validator.validateMenuChoice;
-
 import Model.Computer;
 import Model.User;
 import Util.NumberCompare;
 import View.InputView;
 import View.OutputView;
 import View.RetryView;
-import camp.nextstep.edu.missionutils.Console;
+import java.util.Map;
 
 public class MainController {
+
     private final InputView inputView;
     private final OutputView outputView;
 
@@ -18,67 +17,34 @@ public class MainController {
 
     private final NumberCompare numberCompare = new NumberCompare();
 
-    public MainController(InputView inputView , OutputView outputView, RetryView retryView){
+    public MainController(InputView inputView, OutputView outputView, RetryView retryView) {
         this.inputView = inputView;
         this.outputView = outputView;
         this.retryView = retryView;
     }
 
-    public void run() throws IllegalArgumentException{
+    public void run() throws IllegalArgumentException {
         try {
-
             outputView.printGameStart();
             Computer computer = new Computer();
 
             while (true) {
-                inputView.printUserInput();
                 User user = new User();
-                int[] result = numberCompare.getNumberCount(computer.getComputerNumber(),
+                user.setUserNumber(inputView.printUserInput());
+                Map<String, Integer> comparisonResult = numberCompare.getNumberCount(
+                        computer.getComputerNumber(),
                         user.getuserNumber());
-                resultPrint(result);
-                if (correctNumber(result)) {
-                    if (!retryGame()) {
-                        break;
-                    } else {
+                if (outputView.printGamerResult(comparisonResult)) {
+                    if (retryView.printRetryMessage()) {
                         run();
                         break;
                     }
+                    break;
                 }
-
             }
-        } catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             throw e;
         }
 
-    }
-
-
-    private void resultPrint(int[] result) {
-        if (result[0] == 0 && result[1] == 0) {
-            System.out.println("낫싱");
-        } else if (result[0] != 0 && result[1] == 0) {
-            System.out.printf("%d 볼\n", result[0]);
-        } else if (result[0] == 0) {
-            System.out.printf("%d 스트라이크\n", result[1]);
-        } else {
-            System.out.printf("%d볼 %d스트라이크\n", result[0], result[1]);
-        }
-
-    }
-
-    private boolean correctNumber(int[] result){
-
-        if(result[1]==3){
-            return true;
-        }
-
-        return false;
-    }
-
-    private boolean retryGame() {
-        retryView.RetryPrint();
-        String retryNumber = Console.readLine();
-        validateMenuChoice(retryNumber);
-        return retryNumber.equals("1");
     }
 }
