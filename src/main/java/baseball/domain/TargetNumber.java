1784dc5403ecb2ccc949f8d6ff3942059a39aff1
noxknow <chris2769@naver.com>
2023-10-21 15:47:00 +0900

    feat(TargetNumber) : 대상이 되는 컴퓨터의 숫자를 저장하는 클래스

---
+++ src/main/java/baseball/domain/TargetNumber.java
@@ -0,0 +1,29 @@
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TargetNumber {
+
+    private List<Integer> targetNumber;
+
+    public TargetNumber() {
+        this.targetNumber = getComputerNumbers();
+    }
+
+    private List<Integer> getComputerNumbers() {
+        List<Integer> computer = new ArrayList<>();
+
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        return computer;
+    }
+}
--- src/main/java/baseball/service/BaseballGameService.java
+++ src/main/java/baseball/service/BaseballGameService.java
@@ -9,37 +9,23 @@ import java.util.List;
 public class BaseballGameService {
 
     public void compareNumber(UserBaseballNumber userBaseballNumber) {
-        List<Integer> computer = getComputerNumbers();
-        int strike = 0, ball = 0;
-
-        while (true) {
-            List<Integer> userNumbers = userBaseballNumber.getUserValues();
-
-            for (int i = 0; i < userNumbers.size(); i++) {
-                if (userNumbers.get(i) == computer.get(i)) {
-                    strike += 1;
-                } else if (computer.contains(userNumbers.get(i))) {
-                    ball += 1;
-                }
-            }
-
-            if (strike == 3) {
-                break;
-            }
-        }
-    }
-
-    private List<Integer> getComputerNumbers() {
-        List<Integer> computer = new ArrayList<>();
-
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
-        }
-
-        return computer;
+//        List<Integer> computer = getComputerNumbers();
+//        int strike = 0, ball = 0;
+//
+//        while (true) {
+//            List<Integer> userNumbers = userBaseballNumber.getUserValues();
+//
+//            for (int i = 0; i < userNumbers.size(); i++) {
+//                if (userNumbers.get(i) == computer.get(i)) {
+//                    strike += 1;
+//                } else if (computer.contains(userNumbers.get(i))) {
+//                    ball += 1;
+//                }
+//            }
+//
+//            if (strike == 3) {
+//                break;
+//            }
+//        }
     }
 }
