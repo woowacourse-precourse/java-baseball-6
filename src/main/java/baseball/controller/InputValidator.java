e27e1ed226cc3d584b203ac397edb16128c834fc
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-10-21 17:05:39 +0900

    feat(InputValidator) : isAllDigit() 메소드 작성
    인자의 문자열이 전부 정수로 구성되어있는지 확인하는 메소드 작성

--- src/main/java/baseball/controller/InputValidator.java
+++ src/main/java/baseball/controller/InputValidator.java
@@ -1,10 +1,21 @@
 package baseball.controller;
 
 public class InputValidator {
-    
-    private static final int BASEBALL_GAME_NUMBER_LENGTH = 3;
 
-    public Boolean isTreeLengthLetter(String playerInput) {
+    private static final int BASEBALL_GAME_NUMBER_LENGTH = 3;
+    private static final char MIN_DIGIT = '1';
+    private static final char MAX_DIGIT = '9';
+
+    public boolean isTreeLengthLetter(String playerInput) {
         return playerInput.length() == BASEBALL_GAME_NUMBER_LENGTH;
+    }
+
+    public boolean isAllDigit(String playerInput) {
+        for (int i = 0; i < playerInput.length(); i++) {
+            if (playerInput.charAt(i) < MIN_DIGIT || playerInput.charAt(i) > MAX_DIGIT) {
+                return false;
+            }
+        }
+        return true;
     }
 }
