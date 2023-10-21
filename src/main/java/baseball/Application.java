1d50e0d955fd4021888360be1d9104828d72f535
lsj5657 <lsj56575@naver.com>
2023-10-21 18:57:06 +0900

    스트라이크, 볼 개수에 따른 출력 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -55,6 +55,19 @@ public class Application {
                 break;
             }
 
+            int strike=0,ball=0;
+            for (int i=0; i<userInput.length(); i++){
+                int digit=Character.getNumericValue(userInput.charAt(i));
+                if(digit==computerNumber.get(i)) strike++;
+                else if (computerNumber.contains(digit)) ball++;
+            }
+
+            if(strike+ball==0)System.out.println("낫싱");
+            else {
+                if(ball>0) System.out.print(ball+"볼 ");
+                if(strike>0)System.out.print(strike+"스트라이크");
+                System.out.println();
+            }
 
         }
 
