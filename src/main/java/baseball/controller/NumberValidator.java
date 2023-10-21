6178524295fb5b8c5c653361ff9baa2cf4e281bf
yunju-m <dbswnfnal@naver.com>
2023-10-21 21:50:56 +0900

    rename(NumberValidator): wrongNumberMessage to WRONG_LEGNTH_NUMBER_MESSAGE

--- src/main/java/baseball/controller/NumberValidator.java
+++ src/main/java/baseball/controller/NumberValidator.java
@@ -2,11 +2,11 @@ package baseball.controller;
 
 public class NumberValidator {
     public static final int BASEBALL_NUMBER_LENGTH = 3;
-    private static final String wrongNumberMessage = "세 자리 숫자가 아닙니다.";
+    private static final String WRONG_LEGNTH_NUMBER_MESSAGE = "세 자리 숫자가 아닙니다."; 
 
     public void toValidateNumber(String inputNumber) {
         if (!isSameLength(inputNumber)){
-            throw new IllegalArgumentException(wrongNumberMessage);
+            throw new IllegalArgumentException(WRONG_LEGNTH_NUMBER_MESSAGE);
         }
     }
 
