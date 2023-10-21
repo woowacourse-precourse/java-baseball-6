bb6d031fba192908f35285c86934f7929eba2dea
hsyoodev <hsyoodev@gmail.com>
2023-10-22 02:16:53 +0900

    feat: validate player number

--- src/main/java/baseball/BaseballNumber.java
+++ src/main/java/baseball/BaseballNumber.java
@@ -3,6 +3,8 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class BaseballNumber {
 
@@ -22,9 +24,26 @@ public class BaseballNumber {
     }
 
     public static List<Integer> generatePlayerNumber(String inputNumber) {
+        validatePlayerNumber(inputNumber);
         return inputNumber.chars()
                 .map(e -> e - '0')
                 .boxed()
                 .toList();
+    }
+
+    private static void validatePlayerNumber(String inputNumber) {
+        if (inputNumber.length() != 3) {
+            throw new IllegalArgumentException("INVALID NUMBER LENGTH");
+        }
+        Set<Integer> playerNumber = inputNumber.chars()
+                .map(e -> e - '0')
+                .boxed()
+                .collect(Collectors.toSet());
+        if (playerNumber.size() < NUMBER_LENGTH) {
+            throw new IllegalArgumentException("DUPLICATE NUMBER");
+        }
+        if (playerNumber.stream().anyMatch(e -> e < 1 || e > 9)) {
+            throw new IllegalArgumentException("INVALID NUMBER");
+        }
     }
 }
