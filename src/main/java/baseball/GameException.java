bdf11a2db69ecf2fa1ba12c23f4648b2fd1ce6f5
shinjaewon99 <tlswodnjs99@naver.com>
2023-10-21 22:35:13 +0900

    refactor : 메소드 시그니처 throws 키워드 제거

--- src/main/java/baseball/GameException.java
+++ src/main/java/baseball/GameException.java
@@ -8,24 +8,24 @@ import static baseball.messageConstant.ErrorMessageConstant.*;
 public class GameException {
     private static final Pattern NUMBER_DIGIT_MATCH = Pattern.compile("^[1-9]*$");
 
-    public void playerNumberLengthException(String inputPlayerNumber) throws IllegalArgumentException {
+    public void playerNumberLengthException(String inputPlayerNumber) {
         if (inputPlayerNumber.length() != 3)
             throw new IllegalArgumentException(PLAYER_INPUT_NUMBER_LENGTH_ERROR_MESSAGE);
     }
 
-    public void playerNumberTypeException(String inputPlayerNumber) throws IllegalArgumentException {
+    public void playerNumberTypeException(String inputPlayerNumber) {
         if (!NUMBER_DIGIT_MATCH.matcher(inputPlayerNumber).matches())
             throw new IllegalArgumentException(PLAYER_INPUT_NUMBER_RANGE_ERROR_MESSAGE);
     }
 
-    public void playerNumberDuplicateException(int[] playerNumbers, char storeNumber) throws IllegalArgumentException {
+    public void playerNumberDuplicateException(int[] playerNumbers, char storeNumber) {
         for (int playerNumber : playerNumbers) {
             if (playerNumber == toInt(storeNumber))
                 throw new IllegalArgumentException(PLAYER_INPUT_NUMBER_DUPLICATE_ERROR_MESSAGE);
         }
     }
 
-    public void playerRetryNumberMissMatchException() throws IllegalArgumentException {
+    public void playerRetryNumberMissMatchException() {
         throw new IllegalArgumentException(PLAYER_INPUT_RETRY_NUMBER_ERROR_MESSAGE);
     }
 }
