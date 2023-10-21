34c627b8544d7d48bcbd90a4af1ed438732fdb14
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-10-21 23:26:41 +0900

    FEAT / add nums by RandomNum in constructor

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,4 +1,12 @@
 package baseball;
 
+import java.util.ArrayList;
+
 public class Player {
+    private ArrayList<Integer> nums = new ArrayList<>();
+
+    public Player() {
+        RandomNum randomNum = new RandomNum();
+        nums = randomNum.getNums();
+    }
 }
