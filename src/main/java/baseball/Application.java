9513b88a68cbbb90a5f4b21ba0447f997b481d68
jay <wlguswhd0809@naver.com>
2023-10-22 01:07:58 +0900

    feat : 입력 값 스트라이크, 볼 판정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,6 +9,39 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
+
+        List<Integer> numbers = randomComputerNumber();
+
+        System.out.println("randomComputerNumber() = " + numbers);
+
+        int cnt_strike = 0;
+        int cnt_ball = 0;
+
+        while (cnt_strike != 3) {
+            cnt_strike = 0;
+            cnt_ball = 0;
+
+            System.out.print("숫자를 입력해주세요 : ");
+//            String input = Console.readLine();
+            int input = Integer.parseInt(Console.readLine());
+
+            int[] input_arr = new int[3];
+
+            input_arr[0] = input / 100;  // 100의 자리
+            input_arr[1] = (input % 100) / 10;  // 10의 자리
+            input_arr[2] = input % 10;  // 1의 자리
+
+            // 스트라이크, 볼 판정
+            for (int i = 0; i < numbers.size(); i++) {
+                if (numbers.contains(input_arr[i])) {
+                    cnt_ball++;
+                    if (numbers.get(i) == input_arr[i]) {
+                        cnt_strike++;
+                        cnt_ball--;
+                    }
+                }
+            }
+        }
     }
 
     private static List<Integer> randomComputerNumber() { // 랜덤 숫자를 뽑는 메서드
