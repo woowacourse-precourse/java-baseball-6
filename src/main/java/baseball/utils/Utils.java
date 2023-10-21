9d91cbf5d1afc9cfe7b636c8be88e9a2c17ec442
민주 <hinote444@gmail.com>
2023-10-21 18:55:28 +0900

    feat(Utils): 문자열로 입력된 숫자를 List<Integer>로 변환하는 기능 구현

--- src/main/java/baseball/utils/Utils.java
+++ src/main/java/baseball/utils/Utils.java
@@ -1,4 +1,17 @@
 package baseball.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Utils {
+    public static List<Integer> parseIntegerList(String string) {
+        List<Integer> separatedString = new ArrayList<>();
+
+        for (int i = 0; i < string.length(); i++) {
+            int number = string.charAt(i) - '0';
+            separatedString.add(number);
+        }
+
+        return null;
+    }
 }
