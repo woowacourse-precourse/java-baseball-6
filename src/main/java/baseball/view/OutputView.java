f26591a67de9e0bc04f4b4e721192e759042a006
Joohui <joohuii96@gmail.com>
2023-10-21 19:54:03 +0900

    feat: 게임 시작 문구 출력

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,10 @@
+package baseball.view;
+
+public class OutputView {
+
+    public static final String START_GAME_MESSAGE = "숫자 야구 게임을 시작합니다.";
+
+    public void printStartGame() {
+        System.out.println(START_GAME_MESSAGE);
+    }
+}
