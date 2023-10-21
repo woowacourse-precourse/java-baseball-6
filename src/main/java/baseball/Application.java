4983c0a07561d43bdc1ddebf97776e466e4df90a
kunsanglee <gosmdochee2@gmail.com>
2023-10-22 00:46:06 +0900

    refactor(Application): BaseballController 생성시 매개변수 추가 InputView, OutputView 인터페이스를 구현하는 BaseballInputView와 BaseballOutputView를 생성자에 주입시키도록 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,10 +2,14 @@ package baseball;
 
 import baseball.controller.BaseballController;
 import baseball.domain.RandomAnswerGenerator;
+import baseball.view.BaseballInputView;
+import baseball.view.BaseballOutputView;
 
 public class Application {
     public static void main(String[] args) {
-        BaseballController baseballController = new BaseballController(new RandomAnswerGenerator());
+        BaseballController baseballController = new BaseballController(new RandomAnswerGenerator(),
+                new BaseballInputView(),
+                new BaseballOutputView());
         baseballController.run();
     }
 }
