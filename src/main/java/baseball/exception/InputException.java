dc8544c1a8ddf6c53200e07d8e19f5fae06dc34e
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-10-22 02:50:22 +0900

    update: add input mismatch count exception

--- src/main/java/baseball/exception/InputException.java
+++ src/main/java/baseball/exception/InputException.java
@@ -1,7 +1,8 @@
 package baseball.exception;
 
 public enum InputException {
-    NOT_INTEGER("숫자가 아닌 값이 입력되었습니다.");
+    NOT_INTEGER("숫자가 아닌 값이 입력되었습니다."),
+    MISMATCH_COUNT("세 개의 숫자를 입력해야 합니다.");
 
     private final String exceptionMessage;
 
--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -10,11 +10,20 @@ public class InputView {
     public void getGameNum() {
         System.out.println(GET_GAME_NUM);
         String gameNum = Console.readLine();
+        validateNum(gameNum);
+        validateCount(gameNum);
+
     }
 
     public void validateNum(String line) {
         if (!line.matches("\\d+")) {
             throw new IllegalArgumentException(InputException.NOT_INTEGER.getExceptionMessage());
+        }
+    }
+
+    public void validateCount(String line) {
+        if (line.length() != 3) {
+            throw new IllegalArgumentException(InputException.MISMATCH_COUNT.getExceptionMessage());
         }
     }
 }
