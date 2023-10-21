e178975b0a0a71c6a375aa70457be8ea2075f475
hYe807n <rlagkdus55@gmail.com>
2023-10-22 03:00:57 +0900

    feat: 플레이어 - 컴퓨터 점수 계산

--- src/main/java/baseball/model/ComputerModel.java
+++ src/main/java/baseball/model/ComputerModel.java
@@ -1,11 +1,34 @@
 package baseball.model;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ComputerModel {
-    private List<Integer> computerNumber;
+    private String computerNumber;
 
     public ComputerModel(List<Integer> computerNumber) {
-        this.computerNumber = computerNumber;
+        this.computerNumber = computerNumber.toString();
+    }
+
+    public Map<String, Integer> scoreCalculator(String playerNumber) {
+        Map<String, Integer> scoreCounter = new HashMap<String, Integer>();
+        String computerNumber = this.computerNumber;
+        scoreCounter.put("ballScore", 0);
+        scoreCounter.put("strikeScore", 0);
+
+        for (int i = 0; i < computerNumber.length(); i++) {
+            if (computerNumber.charAt(i) == playerNumber.charAt(i)) {
+                String sameNumber = String.valueOf(computerNumber.charAt(i));
+                scoreCounter.put("strikeScore",(scoreCounter.get("strikeScore") + 1));
+                computerNumber = computerNumber.replace(sameNumber, "");
+                playerNumber = playerNumber.replace(sameNumber, "");
+                i--;
+            }
+            if (computerNumber.contains(Character.toString(playerNumber.charAt(i)))) {
+                scoreCounter.put("ballScore",(scoreCounter.get("ballScore") + 1));
+            }
+        }
+        return scoreCounter;
     }
 }
