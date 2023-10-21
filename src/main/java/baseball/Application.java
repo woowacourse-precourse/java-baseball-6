4a20250ae73e2b133137f20fbba98e598e31ede0
lsj5657 <lsj56575@naver.com>
2023-10-21 17:56:02 +0900

    컴퓨터 숫자 생성 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,20 @@
 package baseball;
+import java.util.List;
+import java.util.ArrayList;
+import camp.nextstep.edu.missionutils.Randoms;
+
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        List<Integer> computer = new ArrayList<>();
+        while(computer.size() < 3){
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if(!computer.contains(randomNumber)){
+                computer.add(randomNumber);
+            }
+        }
+
     }
 }
