3376812d2b336dc39db0b564955cb65b33bbbebc
changhyun9 <ckdgus1713@naver.com>
2023-10-21 16:05:53 +0900

    Refactoring - Computer 객체 생성 및 리팩토링

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,2 +1,47 @@
-package baseball;public class Computer {
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+
+public class Computer {
+    private String randomNumber;
+    private int size;
+
+    Computer() {
+        randomNumber = "";
+        size = 0;
+    }
+
+    public String generate() {
+
+        while (size < 3) {
+            String number = createOne(); // 숫자 하나 생성
+
+            boolean check = checkDuplication(randomNumber, number); // 중복체크
+            if (!check) {
+                randomNumber += number; // 중복이 아니므로 이어 붙이기
+                addSize();
+            }
+        }
+        return randomNumber;
+    }
+
+    private String createOne() {
+        return String.valueOf(Randoms.pickNumberInRange(1, 9)); // 1~9 사이의 번호 하나 생성
+    }
+
+    private boolean checkDuplication(String number, String num) { // 중복 체크
+        if(number.contains(num)) return true;
+        return false;
+    }
+
+    private void addSize() { // 사이즈 증가
+        size++;
+    }
+
+    public void reset() { // 초기화
+        randomNumber = "";
+        size = 0;
+    }
+
 }
