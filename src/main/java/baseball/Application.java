b85a46255a10e37567dd711996f244d207bc87a8
gumgu <bienlee@naver.com>
2023-10-21 16:32:24 +0900

    [구현] 게임종료 안내 문구 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -33,6 +33,8 @@ public class Application {
 
         }
 
+        printManager.printGameOver();
+
 
     }
 }
--- src/main/java/baseball/PrintManager.java
+++ src/main/java/baseball/PrintManager.java
@@ -30,4 +30,8 @@ public class PrintManager {
 
     }
 
+    public void printGameOver() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
 }
