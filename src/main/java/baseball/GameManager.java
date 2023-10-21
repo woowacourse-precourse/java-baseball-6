b0b25d9a79c7fc4fa6fd3872be3a5fc22317f60e
jiwoo <zbqmgldjfh@gmail.com>
2023-10-21 11:20:47 +0900

    feat(OutputView): OutputView를 사용하여 결과를 출력하도록 구현

--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -3,23 +3,28 @@ package baseball;
 import baseball.util.NumberGenerator;
 import baseball.util.Parser;
 import baseball.view.InputView;
+import baseball.view.OutputView;
 
 import java.util.List;
 
 public class GameManager {
 
     private final InputView inputView;
+    private final OutputView outputView;
 
-    public GameManager(InputView inputView) {
+    public GameManager(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
+        this.outputView = outputView;
     }
 
     public void play() {
+        outputView.writeNewlineMessage("숫자 야구 게임을 시작합니다.");
         do {
             List<Integer> computerNumbers = NumberGenerator.generateDefaultSizeComputerNumber();
             List<Integer> userInputNumbers;
 
             do {
+                outputView.writeInlineMessage("숫자를 입력해주세요 : ");
                 String userInput = inputView.readUserInput();
                 userInputNumbers = Parser.parseToIntegerList(userInput);
             } while (calculateGameResult(computerNumbers, userInputNumbers));
@@ -32,17 +37,12 @@ public class GameManager {
     }
 
     private boolean isContinue() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        outputView.writeNewlineMessage("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String userInput = inputView.readUserRetryInput();
-
-        if(!(userInput.equals("1") || userInput.equals("2"))) {
-            throw new IllegalArgumentException();
-        }
 
         if(userInput.equals("1")) {
             return true;
         }
-
         return false;
     }
 }
---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,12 @@
+package baseball.view;
+
+public class OutputView {
+
+    public void writeInlineMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void writeNewlineMessage(String message) {
+        System.out.println(message);
+    }
+}
