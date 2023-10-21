4fe4dca88475c20bed3dfca4b7a6ec606508a832
김연진 <love_homebody@naver.com>
2023-10-21 16:19:06 +0900

    feat(GameResult): 볼, 스트라이크 수 반환

--- src/main/java/baseball/constant/OutputMessage.java
+++ src/main/java/baseball/constant/OutputMessage.java
@@ -3,7 +3,10 @@ package baseball.constant;
 public enum OutputMessage {
 
     GAME_START("숫자 야구 게임을 시작합니다."),
-    PLAYER_INPUT_GUIDE("숫자를 입력해주세요 : ");
+    PLAYER_INPUT_GUIDE("숫자를 입력해주세요 : "),
+    BALL("%d볼 "),
+    STRIKE("%d스트라이크"),
+    NOTHING("낫싱");
 
     private final String message;
 
--- src/main/java/baseball/domain/GameResult.java
+++ src/main/java/baseball/domain/GameResult.java
@@ -1,6 +1,8 @@
 package baseball.domain;
 
+import baseball.constant.BallConstant;
 import baseball.constant.BallStatus;
+import baseball.constant.OutputMessage;
 
 import java.util.Arrays;
 import java.util.EnumMap;
@@ -20,5 +22,32 @@ public class GameResult {
 
     public void increaseCount(BallStatus ballStatus) {
         elements.put(ballStatus, elements.get(ballStatus) + 1);
+    }
+
+    public String getResultScreen() {
+        StringBuilder sb = new StringBuilder();
+        if (isNothing()) {
+            sb.append(OutputMessage.NOTHING.getMessage());
+            return sb.toString();
+        }
+        appendBall(sb);
+        appendStrike(sb);
+        return sb.toString();
+    }
+
+    private boolean isNothing() {
+        return elements.get(BallStatus.NOTHING) == BallConstant.BALL_LENGTH.getValue();
+    }
+
+    private void appendBall(StringBuilder sb) {
+        if (elements.get(BallStatus.BALL) != DEFAULT) {
+            sb.append(String.format(OutputMessage.BALL.getMessage(), elements.get(BallStatus.BALL)));
+        }
+    }
+
+    private void appendStrike(StringBuilder sb) {
+        if (elements.get(BallStatus.STRIKE) != DEFAULT) {
+            sb.append(String.format(OutputMessage.STRIKE.getMessage(), elements.get(BallStatus.STRIKE)));
+        }
     }
 }
