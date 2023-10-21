23063a42892ecc9900c758b76d661dac49c2890c
inpink <inpink@kakao.com>
2023-10-21 22:13:13 +0900

    refactor: 게임 재시작/애플리케이션 종료 번호를 상수화
    GameConstants class에 int형으로 추가

--- src/main/java/baseball/domain/GameConstants.java
+++ src/main/java/baseball/domain/GameConstants.java
@@ -4,6 +4,8 @@ public class GameConstants {
     public static final int NUMBER_LENGTH = 3;
     public static final int MIN_DIGIT = 1;
     public static final int MAX_DIGIT = 9;
+    public static final int RESTART_GAME = 1;
+    public static final int EXIT_GAME = 2;
 
     public static final String STRIKE_TEXT = "스트라이크";
     public static final String BALL_TEXT = "볼";
