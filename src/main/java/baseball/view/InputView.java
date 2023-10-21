4cf507386193ea790a148acaaead3826086491de
JoonKyo <joonkyolee@gmail.com>
2023-10-22 01:03:50 +0900

    style: 메서드 이름 수정

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -3,10 +3,10 @@ package baseball.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private static final String USER_GAME_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String PLAYER_GAME_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
 
-    public String inputUserGameNumber() {
-        System.out.println(USER_GAME_NUMBER_MESSAGE);
+    public String inputPlayerGameNumber() {
+        System.out.println(PLAYER_GAME_NUMBER_MESSAGE);
         return Console.readLine();
     }
 }
