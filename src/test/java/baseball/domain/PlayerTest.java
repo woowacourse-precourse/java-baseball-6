931add7d106f609f7a7de8b96a6420fe7805774b
김명준 <86913355+mjj111@users.noreply.github.com>
2023-10-21 15:40:47 +0900

    refacotor(Player,PlayTest): Player클레스에서 입력값에 대한 책임을 제거
    플레이어는 입력된 값을 갖고 보여줄 수 있으며 선행되는 검증 절차에 대해 책임을 갖지 않도록 합니다.

--- src/test/java/baseball/domain/PlayerTest.java
+++ src/test/java/baseball/domain/PlayerTest.java
@@ -1,59 +1,19 @@
 package baseball.domain;
 
-import baseball.exception.InputException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 public class PlayerTest {
 
     @Test
-    void 사용자_입력_검증_길이초과_예외() {
-        Player player = new Player();
-        String testSentence = "1234";
+    void 사용자_입력_초기화() {
+        List<Integer> inputNumbers = List.of(1, 2, 3);
 
-        InputException exception = Assertions.assertThrows(InputException.class, () -> {
-            player.validateAnswer(testSentence);
-        });
+        Player player = new Player(inputNumbers);
 
-        String message = exception.getMessage();
-        Assertions.assertEquals("입력값의 길이가 4이상입니다.", message);
-    }
-
-    @Test
-    void 사용자_입력_검증_길이부족_예외() {
-        Player player = new Player();
-        String testSentence = "34";
-
-        InputException exception = Assertions.assertThrows(InputException.class, () -> {
-            player.validateAnswer(testSentence);
-        });
-
-        String message = exception.getMessage();
-        Assertions.assertEquals("입력값의 길이가 2이하입니다.", message);
-    }
-
-    @Test
-    void 사용자_입력_검증_문자_예외() {
-        Player player = new Player();
-        String testSentence = "테스트";
-
-        InputException exception = Assertions.assertThrows(InputException.class, () -> {
-            player.validateAnswer(testSentence);
-        });
-
-        String message = exception.getMessage();
-        Assertions.assertEquals("입력값이 숫자가 아닙니다.", message);
-    }
-
-    @Test
-    void 사용자_입력_검증_NULL_예외() {
-        Player player = new Player();
-
-        InputException exception = Assertions.assertThrows(InputException.class, () -> {
-            player.validateAnswer(null);
-        });
-
-        String message = exception.getMessage();
-        Assertions.assertEquals("입력값에 Null 값이 확인되었습니다.", message);
+        List<Integer> playersInputAnswer = player.getInputAnswer();
+        Assertions.assertEquals(inputNumbers,playersInputAnswer);
     }
 }
