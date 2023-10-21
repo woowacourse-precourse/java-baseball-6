c5b2c4ccde334806239e1f2e697640abebf068a6
3juhwan <13selfesteem91@naver.com>
2023-10-21 14:25:08 +0900

    feat(computer): 인스턴스 생성과 동시에 새로운 정답 생성

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,25 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private final List<Integer> answer;
+
+    public Computer() {
+        this.answer = generateAnswer();
+    }
+
+    private List<Integer> generateAnswer() {
+        List<Integer> answer = new ArrayList<>();
+        while (answer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!answer.contains(randomNumber)) {
+                answer.add(randomNumber);
+            }
+        }
+        return answer;
+    }
+}
