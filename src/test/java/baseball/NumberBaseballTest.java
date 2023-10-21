102feda599ae61dea8c7bd20ad2075b3edf59812
baksakcci <pshsh910@gmail.com>
2023-10-21 19:25:29 +0900

    refactor($NumberBaseballTest): Game, Computer 객체를 필드로 변경

--- src/test/java/baseball/NumberBaseballTest.java
+++ src/test/java/baseball/NumberBaseballTest.java
@@ -3,15 +3,24 @@ package baseball;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class NumberBaseballTest {
 
+    private Game game;
+    private Computer computer;
+
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+        computer = new Computer();
+    }
+
     @Test
     @DisplayName("컴퓨터가 생성한 숫자가_3자리 서로 다른 임의의 숫자이면_성공한다")
     void generateNumber_is3DifferentRandomNumber_Success() {
-        Computer computer = new Computer();
         String number = computer.generateRandomNumber();
         System.out.println(number);
         assertThat(number.charAt(0))
@@ -24,13 +33,13 @@ public class NumberBaseballTest {
     @Test
     @DisplayName("사용자로부터 받은 숫자가_3자리 서로다른 숫자가 아닐경우_예외를 발생한다.")
     void userNumber_isNot3DifferentNumber_throwException() {
-        Assertions.assertThatThrownBy(() -> new Game().validateUserNumber("21a"))
+        Assertions.assertThatThrownBy(() -> game.validateUserNumber("21a"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("숫자가 아니다");
-        Assertions.assertThatThrownBy(() -> new Game().validateUserNumber("224"))
+        Assertions.assertThatThrownBy(() -> game.validateUserNumber("224"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("중복 숫자가 존재한다");
-        assertThat(new Game().validateUserNumber("234"))
+        assertThat(game.validateUserNumber("234"))
                 .isEqualTo("234");
     }
 }
