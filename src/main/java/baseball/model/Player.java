0aff9c2075a3296c6b573f93bbf9e1efce4bf06b
leesangjun <hubls@naver.com>
2023-10-22 13:52:51 +0900

    style(Player): tab을 space로 수정

--- src/main/java/baseball/model/Player.java
+++ src/main/java/baseball/model/Player.java
@@ -4,18 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
-	List<Integer> playerNumber = new ArrayList<>();
+    List<Integer> playerNumber = new ArrayList<>();
 
-	public void setPlayerNumber(String number) {
-		playerNumber.clear();
-		for (int i = 0; i < number.length(); i++) {
-			char charDigit = number.charAt(i);
-			int digit = Character.getNumericValue(charDigit);
-			playerNumber.add(digit);
-		}
-	}
+    public void setPlayerNumber(String number) {
+        playerNumber.clear();
+        for (int i = 0; i < number.length(); i++) {
+            char charDigit = number.charAt(i);
+            int digit = Character.getNumericValue(charDigit);
+            playerNumber.add(digit);
+        }
+    }
 
-	public List<Integer> getPlayerNumber() {
-		return playerNumber;
-	}
+    public List<Integer> getPlayerNumber() {
+        return playerNumber;
+    }
 }
