c6d94c676b9da32ad58ef1fe95c953e7948388b9
yubin <dbqls2821@gmail.com>
2023-10-22 17:14:58 +0900

    test: 게임을 시작할때 요구사항에 맞는 문구를 출력하는 메서드에 대한 테스트코드 추가

---
+++ src/test/java/baseball/OutputCommanderTest.java
@@ -0,0 +1,40 @@
+package baseball;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OutputCommanderTest {
+
+    private ByteArrayOutputStream outputStream;
+    private PrintStream printStream;
+
+    @BeforeEach
+    void setUp() {
+        printStream = System.out;
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(printStream);
+    }
+
+    @Test
+    void 게임을_시작할때_요구사항에_맞는_문구를_출력한다() {
+        OutputCommander outputCommander = new OutputCommander();
+        String expected = OutputMessage.START.message + "\n";
+
+        outputCommander.printGameStart();
+
+        String output = outputStream.toString();
+        assertEquals(expected, output);
+    }
+}
