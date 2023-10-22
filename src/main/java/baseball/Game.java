5343bf4f4379a0a0851dc25ad4b46db93468213f
Happeeee <cmg118@naver.com>
2023-10-23 00:39:27 +0900

    Feat: Add numberGenerate
    컴퓨터 번호 생성, 사용자 번호 입력 및 검증 기능 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 public class Game {
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
-    private static final String NUMBER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String GAME_OVER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private List<Integer> computerNumber;
     private List<Integer> playerNumber;
--- src/main/java/baseball/NumberGenerator.java
+++ src/main/java/baseball/NumberGenerator.java
@@ -1,18 +1,60 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class NumberGenerator {
     private static final int SIZE = 3;
+    private static final String NUMBER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
 
     public List<Integer> setComputerNumber() {
-        List<Integer> dummy = new ArrayList<>();
-        return dummy;
+        List<Integer> computerNumber;
+        Set<Integer> set = new HashSet<>(); // 중복제거를 위해 Set 사용
+        while (set.size() < SIZE) {
+            set.add(Randoms.pickNumberInRange(1, 9));
+        }
+        computerNumber = new ArrayList<>(set);
+        return computerNumber;
     }
 
     public List<Integer> setPlayerNumber() {
-        List<Integer> dummy = new ArrayList<>();
-        return dummy;
+        System.out.print(NUMBER_INPUT_MESSAGE);
+        List<Integer> playerNumber = stringToList(inputNumber());
+        return playerNumber;
+    }
+
+    private String inputNumber() { // 사용자로부터 번호를 입력받고 예외 케이스 검사
+        String num = Console.readLine();
+        if (!isValidNumber(num)) {
+            throw new IllegalArgumentException();
+        }
+        return num;
+    }
+
+    private boolean isValidNumber(String num) { // 유효한 숫자인지 검사
+        if (num.length() != 3) {
+            return false;
+        }
+        for (char c : num.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        if (num.charAt(0) == num.charAt(1) || num.charAt(0) == num.charAt(2) || num.charAt(1) == num.charAt(2)) {
+            return false;
+        }
+        return true;
+    }
+
+    public List<Integer> stringToList(String num) { // 검증된 숫자를 list 타입으로 변환
+        List<Integer> list = new ArrayList<>();
+        for (char c : num.toCharArray()) {
+            list.add(c - '0');
+        }
+        return list;
     }
 }
