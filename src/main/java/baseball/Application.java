2c839d8d8dbfc9e9ec31547fa2fb6d70e029d731
cyberjam <jamsince1994@naver.com>
2023-10-22 17:00:22 +0900

    refactor: split to getRandomNumbers method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,15 +7,18 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        // input computer
+        List<Integer> randomNumbers = getRandomNumbers();
+        System.out.println(randomNumbers);
+    }
+    private static List<Integer> getRandomNumbers() {
+        // get random numbers from 1 to 9
         List<Integer> randomNumbers = new ArrayList<>();
         while (randomNumbers.size() < 3) {
-            int pickNumber = pickNumberInRange(1,9);;
+            int pickNumber = pickNumberInRange(1,9);
             if (!randomNumbers.contains(pickNumber)) {
                 randomNumbers.add(pickNumber);
             }
         }
-
-        System.out.println(randomNumbers);
+        return randomNumbers;
     }
 }
