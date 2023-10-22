1c19200aa46121a7003b3e380dd57e57150f1a60
rkf06 <rkf06@DESKTOP-DUIQUNE>
2023-10-23 04:56:05 +0900

    랜덤 숫자가 잘 생성되는 지 확인

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,19 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
-    public static void main(String[] args) {
-    }
+	public static void main(String[] args) {
+		List<Integer> answerNum = new ArrayList<>();
+		while (answerNum.size() < 3) {
+			int randomNumber = Randoms.pickNumberInRange(1, 9);
+			if (!answerNum.contains(randomNumber)) {
+				answerNum.add(randomNumber);
+			}
+		}
+		System.out.println(answerNum);
+	}
 }
