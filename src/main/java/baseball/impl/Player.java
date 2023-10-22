6f7857447d6a1ee3064ae8000ca166993fd5e25e
ajou20658 <kwy1379@ajou.ac.kr>
2023-10-22 18:35:43 +0900

    PlayerImpl 객체 구현, Player interface 메서드 반환 타입 수정

--- src/main/java/baseball/impl/Player.java
+++ src/main/java/baseball/impl/Player.java
@@ -2,5 +2,5 @@ package baseball.impl;
 
 public interface Player {
     void gameInputCheck(String input);
-    void continueInputCheck(String input);
+    boolean continueInputCheck(String input);
 }
