2bfc172d483a34c0804c4554baf4337bdc1a98e0
Jieon <dlwldjs7544@naver.com>
2023-10-22 14:30:47 +0900

    feat: ComputerNumber, PlayerNumber 클래스 이름 변경
    ComputerNumber, PlayerNumber 클래스 이름을 각각 Computer, Player 로 변경

---
+++ src/main/java/baseball/model/Computer.java
@@ -0,0 +1,16 @@
+package baseball.model;
+
+import java.util.List;
+
+public class Computer {
+
+    private List<Integer> computerNumbers;
+
+    public List<Integer> getComputerNumbers() {
+        return computerNumbers;
+    }
+
+    public void setComputerNumbers(List<Integer> computerNumbers) {
+        this.computerNumbers = computerNumbers;
+    }
+}
--- src/main/java/baseball/model/Player.java
+++ src/main/java/baseball/model/Player.java
@@ -2,14 +2,14 @@ package baseball.model;
 
 import java.util.List;
 
-public class PlayerNumber {
+public class Player {
     private List<Integer> playerNumbers;
-
-    public void setPlayerNumbers(List<Integer> playerNumbers) {
-        this.playerNumbers = playerNumbers;
-    }
 
     public List<Integer> getPlayerNumbers() {
         return playerNumbers;
+    }
+
+    public void setPlayerNumbers(List<Integer> playerNumbers) {
+        this.playerNumbers = playerNumbers;
     }
 }
