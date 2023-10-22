fe3af5d6ee72b8bb64c3d24182258ee8d3b7ffc6
Jihyun3478 <leejihyun3478@gmail.com>
2023-10-23 05:53:28 +0900

    Feat(NumberValidation): 컴퓨터 랜덤 숫자 생성 검증 구현

--- src/main/java/baseball/validation/NumberValidation.java
+++ src/main/java/baseball/validation/NumberValidation.java
@@ -1,15 +1,25 @@
 package baseball.validation;
 
+import static baseball.constant.Constant.NUMBER_LENGTH;
+
 import java.util.Arrays;
 
 public class NumberValidation {
+
     public void validationAll(int[] input) {
         validateDuplicate(input);
         validateRangeAndType(input);
+        validateLength(input);
     }
 
     public static void validateDuplicate(int[] input) {
         if (!(input.length == Arrays.stream(input).distinct().count())) {
+            throw new IllegalArgumentException("숫자의 형식이 올바르지 않습니다.");
+        }
+    }
+
+    private void validateLength(int[] input) {
+        if (!(input.length == NUMBER_LENGTH)) {
             throw new IllegalArgumentException("숫자의 형식이 올바르지 않습니다.");
         }
     }
@@ -24,5 +34,13 @@ public class NumberValidation {
         if (!(input.equals("1") || input.equals("2"))) {
             throw new IllegalArgumentException("숫자의 형식이 올바르지 않습니다.");
         }
+    }
+
+    public static boolean validateComputerDuplicate(int[] input) {
+        return input.length == Arrays.stream(input).distinct().count();
+    }
+
+    public static boolean isInArray(int[] computerNumber, int num) {
+        return Arrays.stream(computerNumber).anyMatch(array -> array == num);
     }
 }
