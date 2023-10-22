e3aaa67017e051f6d26d7699a4275cedecd45ead
cousim46 <wjdghldns24@naver.com>
2023-10-22 16:01:25 +0900

    Add : 게임 실행 기능 구현

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -1,11 +1,33 @@
 package baseball;
 
+import baseball.ballcount.BallCount;
+import baseball.ballcount.BallCountCalculate;
 import baseball.game.Game;
+import baseball.game.GameProcess;
+import baseball.input.InputService;
+import baseball.output.PrintService;
 
 public class BaseBallGame implements Game {
 
     @Override
-    public void execute(String value) {
-
+    public void execute(GameProcess gameProcess) {
+        String value = Computer.createValue();
+        PrintService.start();
+        while (true) {
+            PrintService.input();
+            String input = InputService.readLine();
+            PrintService.inputValue(input);
+            Validate.validateInputValue(input);
+            int strike  = BallCountCalculate.getStrike(value, input);
+            int ball  = BallCountCalculate.getBall(value, input);
+            BallCount ballCount = new BallCount(strike, ball);
+            PrintService.result(ballCount);
+            if(strike == 3) {
+                PrintService.end();
+                break;
+            }
+        }
+        gameProcess.checkGameContinue(InputService.readLine(), new BaseBallGame(), gameProcess);
+        InputService.close();
     }
 }
