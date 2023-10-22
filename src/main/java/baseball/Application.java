64c4f5d99c3b6cb69fc5f5efcf6cafac5d8fa696
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-10-23 03:33:57 +0900

    feat: 컴퓨터 난수생성기 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,10 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -14,5 +19,17 @@ public class Application {
     }
 
     private static void play() {
+        List<Integer> computer = createRandomNumbers();
+    }
+
+    private static List<Integer> createRandomNumbers() {
+        List<Integer> randomNumbers = new ArrayList<>();
+        while (randomNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!randomNumbers.contains(randomNumber)) {
+                randomNumbers.add(randomNumber);
+            }
+        }
+        return randomNumbers;
     }
 }
