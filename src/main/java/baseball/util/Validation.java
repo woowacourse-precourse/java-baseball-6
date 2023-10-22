9045a2ebf4457babcdec14e8a7f5d1559159e29d
junseoks-99 <angec5645@naver.com>
2023-10-23 00:28:28 +0900

    feat: "입력값 0 포함여부 확인"

--- src/main/java/baseball/util/Validation.java
+++ src/main/java/baseball/util/Validation.java
@@ -5,4 +5,7 @@ public class Validation {
     private final int MAX_INPUT_LENGTH = 3;
     private final int MAX_EXIT_INPUT_LENGTH = 1;
 
+    private boolean containsZero(String number) {
+        return number.contains("0");
+    }
 }
