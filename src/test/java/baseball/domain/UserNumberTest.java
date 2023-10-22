f06eaba1997213d36a73e2a845caae3a2ea0e7f6
YongHyeon <pyh4616@naver.com>
2023-10-23 01:05:15 +0900

    Test: UserNumber, 사용자 숫자 3자리가 각각 범위 1~9에 속하지 않을 때, 예외가 발생하는지 확인하는 테스트

--- src/test/java/baseball/domain/UserNumberTest.java
+++ src/test/java/baseball/domain/UserNumberTest.java
@@ -1,13 +1,11 @@
 package baseball.domain;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import baseball.model.UserNumber;
-import org.assertj.core.api.Assertions;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.*;
 
 public class UserNumberTest {
 
@@ -19,9 +17,23 @@ public class UserNumberTest {
     }
 
     @Test
-    @DisplayName("사용자 숫자가 0을 포함하고 있으면 예외가 발생한다.")
-    void testValidateUserNumberContainZero() {
+    @DisplayName("사용자 숫자 3자리가 각각 범위 1~9에 속하지 않는다면 예외가 발생한다.")
+    void testValidateUserNumberRange1() {
         assertThatThrownBy(() -> new UserNumber(List.of(0, 1, 2)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("사용자 숫자 3자리가 각각 범위 1~9에 속하지 않는다면 예외가 발생한다.")
+    void testValidateUserNumberRange2() {
+        assertThatThrownBy(() -> new UserNumber(List.of(8, 9, 10)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("사용자 숫자 3자리가 각각 범위 1~9에 속하지 않는다면 예외가 발생한다.")
+    void testValidateUserNumberRange3() {
+        assertThatThrownBy(() -> new UserNumber(List.of(-1, -2, -3)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
