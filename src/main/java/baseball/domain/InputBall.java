1a76afabd00cef5dd5e0522fd9dcf99039f75cc3
sangwopark19 <sangwopark19@gmail.com>
2023-10-22 22:24:22 +0900

    사용자의 입력을 제한하는 int형 상수 추가, 입력값을 저장하는 saveInput메서드 작성

--- src/main/java/baseball/domain/InputBall.java
+++ src/main/java/baseball/domain/InputBall.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class InputBall {
     private final List<Integer> balls;
+    private final int MAX_LENGTH = 3;
 
     public InputBall() {
         this.balls = new ArrayList<>();
@@ -12,8 +13,10 @@ public class InputBall {
 
     public void saveInput(String input) {
         validateInput(input);
-        
-
+        String[] parts = input.split("");
+        for (int i = 0; i < MAX_LENGTH; i++) {
+            balls.add(Integer.parseInt(parts[i]));
+        }
     }
 
     private void validateInput(String input) {
@@ -22,8 +25,8 @@ public class InputBall {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자가 아닙니다");
         }
-        if (input.length() > 3) {
-            throw new IllegalArgumentException("3자리 이상 입력했습니다.");
+        if (input.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException(MAX_LENGTH + "자리 이상 입력했습니다.");
         }
         if (input.contains("0")) {
             throw new IllegalArgumentException("0이 포함되어 있습니다");
