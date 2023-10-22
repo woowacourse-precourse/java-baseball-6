ec3f085367203a5ce8a1cd024fef8b42fb606ffd
pjm2476 <pjm2476@gmail.com>
2023-10-22 19:50:13 +0900

    feat: 유저가 입력한 값과 컴퓨터가 생성한 난수를 비교하여 힌트 계산

--- src/main/java/baseball/Hint.java
+++ src/main/java/baseball/Hint.java
@@ -1,11 +1,28 @@
 package baseball;
 
+import java.util.List;
+
 public class Hint {
     public int strike;
     public int ball;
 
+    private static final int NUMBER_LEN = 3;
+
     public Hint() {
         this.strike = 0;
         this.ball = 0;
+    }
+
+    public void calculateHint(List<Integer> userAnswer, List<Integer> computerAnswer) {
+        countStrike(userAnswer, computerAnswer);
+        countBall(userAnswer, computerAnswer);
+    }
+
+    private void countStrike(List<Integer> userAnswer, List<Integer> computerAnswer) {
+
+    }
+
+    private void countBall(List<Integer> userAnswer, List<Integer> computerAnswer) {
+ 
     }
 }
