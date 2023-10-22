2ef09e7c2ff023058f8c309801e1e750a406ae32
Joohui <joohuii96@gmail.com>
2023-10-23 02:05:19 +0900

    test: 입력 검증 테스트

---
+++ src/test/java/baseball/common/InputNumberValidatorTest.java
@@ -0,0 +1,34 @@
+package baseball.common;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class InputNumberValidatorTest {
+
+    @Test
+    @DisplayName("숫자가 아닌 문자 입력")
+    void validateNotNumber() {
+        //given
+        String notNumberInput = "aaa";
+        assertThrows(IllegalArgumentException.class, () -> InputNumberValidator.validate(notNumberInput));
+    }
+
+    @Test
+    @DisplayName("4자리 숫자 입력")
+    void validateThreeLetters() {
+        //given
+        String notNumberInput = "1234";
+        assertThrows(IllegalArgumentException.class, () -> InputNumberValidator.validate(notNumberInput));
+    }
+
+    @Test
+    @DisplayName("같은 숫자 입력")
+    void validateUniqueCharacters() {
+        //given
+        String notNumberInput = "555";
+        assertThrows(IllegalArgumentException.class, () -> InputNumberValidator.validate(notNumberInput));
+    }
+}
