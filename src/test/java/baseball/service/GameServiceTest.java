145f9ddd27f8a3de8de0981ac634ae38a5beffda
SSung023 <50323157+SSung023@users.noreply.github.com>
2023-10-22 15:35:38 +0900

    test: validate answer consist of distinct numbers
    - 생성한 정답이 1부터 9로 이루어져 있으며 중복되지 않은 수로 이루어져있는지 검증하는 테스트 코드 추가

---
+++ src/test/java/baseball/service/GameServiceTest.java
@@ -0,0 +1,29 @@
+package baseball.service;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GameServiceTest {
+    private final GameService gameService = new GameService();
+
+    @Test
+    @DisplayName("생성한 정답은 1부터 9까지 중복되지 않는 수로 이루어져 있다.")
+    public void createAnswer(){
+        //given
+        List<Integer> answer = gameService.createAnswer();
+
+        //when
+        List<Integer> filtered = answer.stream()
+                .filter(a -> 1 <= a && a <= 9)
+                .distinct()
+                .toList();
+
+        //then
+        assertThat(answer).isEqualTo(filtered);
+    }
+}
