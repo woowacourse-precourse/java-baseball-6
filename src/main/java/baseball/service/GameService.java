9adc9ecf332e8be878efbb4a47a287dc44865ce7
YongHyeon <pyh4616@naver.com>
2023-10-23 04:51:55 +0900

    Refactor: GameService, 매직 넘버를 상수로변경, 논리 연산자의 양쪽에 있는 비교 연산에 소괄호를 씌움

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -1,18 +1,26 @@
 package baseball.service;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
-
 public class GameService {
+
+    private static final int COMPUTER_NUMBER_SIZE = 3;
+    private static final int COMPUTER_NUMBER_START_RANGE = 1;
+    private static final int COMPUTER_NUMBER_END_RANGE = 9;
+    private static final int BALL_COUNT_INITIAL_VALUE = 0;
+    private static final int STRIKE_COUNT_INITIAL_VALUE = 0;
+    private static final int BALL_COUNT_INCREASE_VALUE = 1;
+    private static final int STRIKE_COUNT_INCREASE_VALUE = 1;
 
     public List<Integer> createComputerNumber() {
         List<Integer> computerNumber = new ArrayList<>();
-        while (computerNumber.size() < 3) {
-            int number = pickNumberInRange(1, 9);
+        while (computerNumber.size() < COMPUTER_NUMBER_SIZE) {
+            int number = pickNumberInRange(COMPUTER_NUMBER_START_RANGE, COMPUTER_NUMBER_END_RANGE);
             if (!computerNumber.contains(number)) {
                 computerNumber.add(number);
             }
@@ -27,14 +35,15 @@ public class GameService {
 
     private Map<String, Integer> getBallAndStrikeCount(List<Integer> computerNumber, List<Integer> userNumber) {
         Map<String, Integer> ballAndStrikeCount = new HashMap<>();
-        ballAndStrikeCount.put("ballCount", 0);
-        ballAndStrikeCount.put("strikeCount", 0);
+        ballAndStrikeCount.put("ballCount", BALL_COUNT_INITIAL_VALUE);
+        ballAndStrikeCount.put("strikeCount", STRIKE_COUNT_INITIAL_VALUE);
         for (int i = 0; i < userNumber.size(); i++) {
             if (computerNumber.contains(userNumber.get(i)) && computerNumber.get(i) != userNumber.get(i)) {
-                ballAndStrikeCount.put("ballCount", ballAndStrikeCount.get("ballCount") + 1);
+                ballAndStrikeCount.put("ballCount", ballAndStrikeCount.get("ballCount") + BALL_COUNT_INCREASE_VALUE);
             }
             if (computerNumber.get(i) == userNumber.get(i)) {
-                ballAndStrikeCount.put("strikeCount", ballAndStrikeCount.get("strikeCount") + 1);
+                ballAndStrikeCount.put("strikeCount",
+                        ballAndStrikeCount.get("strikeCount") + STRIKE_COUNT_INCREASE_VALUE);
             }
         }
         return ballAndStrikeCount;
@@ -42,17 +51,22 @@ public class GameService {
 
     private String getResultMessage(Map<String, Integer> ballAndStrikeCount) {
         String resultMessage = new String();
-        if (ballAndStrikeCount.get("ballCount") == 0 && ballAndStrikeCount.get("strikeCount") == 0) {
+        if ((ballAndStrikeCount.get("ballCount") == BALL_COUNT_INITIAL_VALUE) &&
+                (ballAndStrikeCount.get("strikeCount") == STRIKE_COUNT_INITIAL_VALUE)) {
             resultMessage = "낫싱";
         }
-        if (ballAndStrikeCount.get("ballCount") == 0 && ballAndStrikeCount.get("strikeCount") != 0) {
+        if ((ballAndStrikeCount.get("ballCount") == BALL_COUNT_INITIAL_VALUE) &&
+                (ballAndStrikeCount.get("strikeCount") != STRIKE_COUNT_INITIAL_VALUE)) {
             resultMessage = ballAndStrikeCount.get("strikeCount") + "스트라이크";
         }
-        if (ballAndStrikeCount.get("ballCount") != 0 && ballAndStrikeCount.get("strikeCount") == 0) {
+        if ((ballAndStrikeCount.get("ballCount") != BALL_COUNT_INITIAL_VALUE) &&
+                (ballAndStrikeCount.get("strikeCount") == STRIKE_COUNT_INITIAL_VALUE)) {
             resultMessage = ballAndStrikeCount.get("ballCount") + "볼";
         }
-        if (ballAndStrikeCount.get("ballCount") != 0 && ballAndStrikeCount.get("strikeCount") != 0) {
-            resultMessage = ballAndStrikeCount.get("ballCount") + "볼 " + ballAndStrikeCount.get("strikeCount") + "스트라이크";
+        if ((ballAndStrikeCount.get("ballCount") != BALL_COUNT_INITIAL_VALUE) &&
+                (ballAndStrikeCount.get("strikeCount") != STRIKE_COUNT_INITIAL_VALUE)) {
+            resultMessage = ballAndStrikeCount.get("ballCount") + "볼 " +
+                    ballAndStrikeCount.get("strikeCount") + "스트라이크";
         }
         return resultMessage;
     }
