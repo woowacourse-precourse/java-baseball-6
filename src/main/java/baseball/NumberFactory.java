a5ec900be61ab0e74c5cc5da7183c2961240c024
JINU-CHANG <jenny010925@naver.com>
2023-10-22 21:21:57 +0900

    [feat] 랜덤값 생성하는 객체 구현

--- src/main/java/baseball/NumberFactory.java
+++ src/main/java/baseball/NumberFactory.java
@@ -1,5 +1,12 @@
 package baseball;
 
-public class NumberFactory {
+import camp.nextstep.edu.missionutils.Randoms;
 
+public class NumberFactory {
+	private static final int START_NUMBER = 0;
+	private static final int END_NUMBER = 0;
+
+	public static String createRandomNumbers(int size) {
+		return Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, size).toString();
+	}
 }
