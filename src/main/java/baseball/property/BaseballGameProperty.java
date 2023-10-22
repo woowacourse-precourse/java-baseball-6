fa017e2d0a667e0899aeaec76d0e5222dd583f49
irerin07 <counterchord89@gmail.com>
2023-10-23 01:38:27 +0900

    #8 게임 설정 파일 구현 - 자릿수 - 사용 가능한 단일 숫자 최소 값 - 사용 가능한 단일 숫자 최대 값 - 설정한 자릿수에서 사용 가능한 최소 값 계산 메서드 - 설정한 자릿수에서 사용 가능한 최대 값 계산 메서드

--- src/main/java/baseball/property/BaseballGameProperty.java
+++ src/main/java/baseball/property/BaseballGameProperty.java
@@ -6,4 +6,41 @@ package baseball.property;
  * @since 2023.10.23
  **********************************************************************************************************************/
 public class BaseBallGameProperty {
+
+  private Integer digit;
+  private Integer usableSingleNumberFrom;
+  private Integer usableSingleNumberTo;
+
+  public BaseBallGameProperty(Integer digit, Integer usableNumberFrom, Integer usableSingleNumberTo) {
+    this.digit = digit;
+    this.usableSingleNumberFrom = usableNumberFrom;
+    this.usableSingleNumberTo = usableSingleNumberTo;
+  }
+
+  public Integer digit() {
+    return digit;
+  }
+
+  public Integer usableNumberFrom() {
+    return usableSingleNumberFrom;
+  }
+
+  public Integer usableSingleNumberTo() {
+    return usableSingleNumberTo;
+  }
+
+  public Integer minimumRange() {
+    StringBuilder builder = new StringBuilder();
+    builder.append(1);
+    for (int i = 1; i < digit; ++i) {
+      builder.append(0);
+    }
+    String num = builder.toString();
+    return Integer.parseInt(num);
+  }
+
+  public Integer maximumRange() {
+    return this.minimumRange() * 10 - 1;
+  }
+
 }
