c5d8ffe09e7e0a9e17fa8e562ef5c2e63c6ee3d8
Jihyun3478 <leejihyun3478@gmail.com>
2023-10-23 06:29:44 +0900

    Fix(NumberValidation): 예외 메시지 수정

--- src/main/java/baseball/validation/NumberValidation.java
+++ src/main/java/baseball/validation/NumberValidation.java
@@ -12,27 +12,27 @@ public class NumberValidation {
         validateLength(input);
     }
 
-    public static void validateDuplicate(int[] input) {
+    private static void validateDuplicate(int[] input) {
         if (!(input.length == Arrays.stream(input).distinct().count())) {
-            throw new IllegalArgumentException("숫자의 형식이 올바르지 않습니다.");
+            throw new IllegalArgumentException("중복되지 않는 숫자를 입력해주세요.");
         }
     }
 
     private void validateLength(int[] input) {
         if (!(input.length == NUMBER_LENGTH)) {
-            throw new IllegalArgumentException("숫자의 형식이 올바르지 않습니다.");
+            throw new IllegalArgumentException("숫자 3개를 입력해주세요.");
         }
     }
 
     private void validateRangeAndType(int[] input) {
         if (!(input.length == Arrays.stream(input).filter(i -> i >= 1 && i <= 9).count())) {
-            throw new IllegalArgumentException("숫자의 형식이 올바르지 않습니다.");
+            throw new IllegalArgumentException("1과 9 사이의 숫자를 입력해주세요.");
         }
     }
 
     public static void validateGameStatus(String input) {
         if (!(input.equals("1") || input.equals("2"))) {
-            throw new IllegalArgumentException("숫자의 형식이 올바르지 않습니다.");
+            throw new IllegalArgumentException("1 또는 2를 입력해주세요.");
         }
     }
 
