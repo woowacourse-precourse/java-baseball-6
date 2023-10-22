4ae06dd62b64a0e46acb2e667a57f331770f0a44
munis <aaaa6658@naver.com>
2023-10-22 23:52:18 +0900

    feat : UserInputNumber 클래스 내에 parseToIntList 함수 구현

--- src/main/java/baseball/UserInputNumber.java
+++ src/main/java/baseball/UserInputNumber.java
@@ -1,9 +1,20 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserInputNumber {
     private final UserInputValidation userInputValidation;
 
     public UserInputNumber() {
         userInputValidation = new UserInputValidation();
+    }
+
+    public List<Integer> parseToIntList(String userInput) {
+        List<Integer> integerList = new ArrayList<>();
+        for (char c : userInput.toCharArray()) {
+            integerList.add(Character.getNumericValue(c));
+        }
+        return integerList;
     }
 }
