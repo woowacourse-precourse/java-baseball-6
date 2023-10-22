826c4d7cc5d2a85a03245b28b2b2bf35f6dc0a1a
minisyu <iswater.kim@gmail.com>
2023-10-22 22:59:42 +0900

    refactor(Player): Player 클래스 리팩토링
    컴퓨터와 플레이어의 숫자를 비교하는 역할을 제거,
    BallNumbersValidator의 검증 로직으로 변경

--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -1,18 +1,15 @@
 package baseball.domain;
 
-import baseball.util.Err;
+import baseball.util.BallNumbersValidator;
 import java.util.List;
 
 public class Player {
-    private static final int START_INCLUSIVE = 1;
-    private static final int END_INCLUSIVE = 9;
-    private static final int INPUT_SIZE = 3;
     private final List<Integer> numbers;
 
     private Player(List<Integer> numbers) {
-        validateInputSize(numbers);
-        validateInputRange(numbers);
-        validateInputUniqueness(numbers);
+        BallNumbersValidator.validateInputSize(numbers);
+        BallNumbersValidator.validateInputRange(numbers);
+        BallNumbersValidator.validateInputUniqueness(numbers);
         this.numbers = numbers;
     }
 
@@ -20,60 +17,7 @@ public class Player {
         return new Player(numbers);
     }
 
-    private void validateInputSize(List<Integer> numbers) {
-        if (numbers.size() != INPUT_SIZE) {
-            throw new IllegalArgumentException(Err.ERROR_NOT_THREE_SIZE_MESSAGE.getMessage());
-        }
-    }
-
-    private void validateInputRange(List<Integer> numbers) {
-        for (Integer number : numbers) {
-            if (number < START_INCLUSIVE || number > END_INCLUSIVE) {
-                throw new IllegalArgumentException(Err.ERROR_INPUT_SCOPE_MESSAGE.getMessage());
-            }
-        }
-    }
-
-    private void validateInputUniqueness(List<Integer> numbers) {
-        int uniqueCount = (int) numbers.stream()
-                .distinct()
-                .count();
-
-        if (uniqueCount != numbers.size()) {
-            throw new IllegalArgumentException(Err.ERROR_NUMBERS_DUPLICATION_MESSAGE.getMessage());
-        }
-    }
-
-    public GameResult calculateAndGetGameResult(List<Integer> computer) {
-        int ball = 0;
-        int strike = 0;
-        GameStatus gameStatus = GameStatus.ONGOING;
-
-        for (Integer number : numbers) {
-            int playerNumberPosition = numbers.indexOf(number);
-
-            if (isSameAtPosition(computer, playerNumberPosition)) {
-                strike += 1;
-                continue;
-            }
-
-            if (isContainedAtPosition(computer, playerNumberPosition)) {
-                ball += 1;
-            }
-        }
-
-        if (strike == 3) {
-            gameStatus = GameStatus.SUCCESS;
-        }
-
-        return new GameResult(ball, strike, gameStatus);
-    }
-
-    private boolean isSameAtPosition(List<Integer> computer, int index) {
-        return numbers.get(index).equals(computer.get(index));
-    }
-
-    private boolean isContainedAtPosition(List<Integer> computer, int index) {
-        return computer.contains(numbers.get(index));
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 }
