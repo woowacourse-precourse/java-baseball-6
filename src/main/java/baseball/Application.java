dcb86bf1fa80828c7dd83740ed52a4c272c0ad96
soyoungxxx <otherplet@gmail.com>
2023-10-23 03:25:10 +0900

    feat: Application.java 작성 - gamecontoller 변수 추가 - gameStart() 메소드 실행문 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import baseball.game.GameControl;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameControl gamecontoller = new GameControl();
+        gamecontoller.gameStart();
     }
 }
