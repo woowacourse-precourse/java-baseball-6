4294a0287e758093d914526320d5a973a2f06a82
ksw13 <ksw48483@gmail.com>
2023-10-22 18:58:09 +0900

    feat: 스트라이크, 볼 판별 기능 구현

---
+++ src/main/java/baseball/model/Balls.java
@@ -0,0 +1,37 @@
+package baseball.model;
+
+import baseball.util.NumberConst;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Balls {
+    List<Ball> balls = new ArrayList<>();
+
+    public Balls(String numbers) {
+        for (int index = 0; index < NumberConst.BALL_LENGTH; index++) {
+            balls.add(new Ball(index, getIndexNumber(numbers, index)));
+        }
+    }
+
+    private static int getIndexNumber(String numbers, int index) {
+        return numbers.charAt(index) - '0';
+    }
+
+    public boolean isStrike(Ball ball) {
+        for (Ball computerBall : balls) {
+            if (computerBall.equals(ball)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isBall(Ball ball) {
+        for (Ball computerBall : balls) {
+            if (computerBall.isBall(ball)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
