af0a49fa21a328e514e998b6bbdc24a3759f80c9
koomchang <leahpar0401@gmail.com>
2023-10-22 21:07:57 +0900

    refactor: NumberTest에서 값 범위 테스트 메소드 네이밍 변경

--- src/test/java/baseball/model/NumberTest.java
+++ src/test/java/baseball/model/NumberTest.java
@@ -9,7 +9,7 @@ class NumberTest {
 
     @Test
     @DisplayName("Number 객체 안에 저장되는 value 값의 범위는 1부터 9사이어야 한다.")
-    public void numberValueRange() {
+    public void is_Number_Value_Range_1to9() {
         assertThrows(IllegalArgumentException.class, () -> new Number(0));
         assertThrows(IllegalArgumentException.class, () -> new Number(10));
     }
