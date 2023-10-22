2675d4015bfed9f685090d90e3d5a69c587582d4
bboodd <92453279+bboodd@users.noreply.github.com>
2023-10-23 02:43:27 +0900

    refactor:컴퓨터 숫자 랜덤 생성기능 클래스->메서드로 리펙토링

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,18 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class BaseballGame {
 
     String inputNumbers;
-    int strike = 0;
-    int ball = 0;
+    int strike;
+    int ball;
+    List<Integer> computer;
 
-    private Computer computer;
 
     public void start(){
-        computer = new Computer();
-        computer.createNumbers();
+        computer = createComputerNumbers();
         System.out.println("숫자 야구 게임을 시작합니다.");
         while (true){
             inputNumbers();
@@ -23,8 +27,18 @@ public class BaseballGame {
         // check point
         // null인지, size가 3인지, 숫자인지, 서로다른 숫자인지
         System.out.print("숫자를 입력해주세요 : ");
-        String temp = readLine();
-        inputNumbers = temp;
+        this.inputNumbers = readLine();
+    }
+
+    public List<Integer> createComputerNumbers(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 
 }
