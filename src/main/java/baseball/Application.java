adbe039cacd07c472ddee2df74b32c4c30921406
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-10-22 15:00:42 +0900

    feat: 컴퓨터 랜덤 숫자 생성 추가
    - Computer Class에 랜덤으로 숫자 3개를 생성하는 함수 추가
    - Application Class에 랜덤으로 생성한 숫자 받는 변수 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,14 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         boolean progress = true;
         Computer computer = new Computer();
         while (progress) {
             computer.gameStartPhrase();
+            List<Integer> computerNums = computer.generateNumber();
         }
 
     }
--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Computer {
     public void gameStartPhrase() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public List<Integer> generateNumber() {
+        List<Integer> computerNums = new ArrayList<>();
+
+        while (computerNums.size() < 3) {
+            int randomNum = Randoms.pickNumberInRange(1, 9);
+            if (!computerNums.contains(randomNum)) {
+                computerNums.add(randomNum);
+            }
+        }
+
+        return computerNums;
     }
 }
