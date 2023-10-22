108982184f8836405f278083f780756ecec93ac5
leesh <qwqw8019@naver.com>
2023-10-22 14:21:20 +0900

    test: 사용자의 임의의 3자리 수 입력에 대한 테스트
    - 정상 입력 테스트
    - 예외
      - null 입력
      - 숫자가 아닌 입력
      - 범위를 벗어난 숫자 입력
      - 정해진 길이를 벗어난 수 입력
      - 중복된 수 입력

---
+++ src/test/java/baseball/ConsoleTest.java
@@ -0,0 +1,95 @@
+package baseball;
+
+import baseball.view.Console;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ConsoleTest {
+    private final String nullString = null;
+    private final String notNumber = "ABC";
+    private final String outOfRangeNumber = "012";
+    private final String shorterThanLimitNumber = "1";
+    private final String longerThanLimitNumber = "1234";
+    private final String duplicateNumber = "112";
+
+    @Nested
+    @DisplayName("사용자의 3자리 수 입력을 받는 기능")
+    class input3DigitNumberTest extends NsTest {
+        List<Integer> successList = new ArrayList<>(List.of(1, 2, 3));
+
+        @Test
+        @DisplayName("정상적으로 입력한다.")
+        void success() {
+            assertSimpleTest(
+                    () -> {
+                        run("123");
+                        assertThat(output()).isEqualTo(successList.toString());
+                    }
+            );
+        }
+
+        @Test
+        @DisplayName("null을 입력하면 예외가 발생한다.")
+        void inputNull() {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException(nullString))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+
+        @Test
+        @DisplayName("숫자가 아닌 입력을 하면 예외가 발생한다.")
+        void inputNotNumber() {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException(notNumber))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+
+        @Test
+        @DisplayName("범위를 벗어난 수를 입력하면 예외가 발생한다.")
+        void inputOutOfRange() {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException(outOfRangeNumber))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+
+        @Test
+        @DisplayName("정해진 길이를 벗어난 숫자를 입력하면 예외가 발생한다.")
+        void inputInvalidLengthNumber() {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException(shorterThanLimitNumber))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException(longerThanLimitNumber))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+
+        @Test
+        @DisplayName("중복된 수를 입력하면 예외가 발생한다.")
+        void inputDuplicateNumber() {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException(duplicateNumber))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+
+
+        @Override
+        protected void runMain() {
+            System.out.println(Console.input3DigitNumber().toString());
+        }
+    }
+}
