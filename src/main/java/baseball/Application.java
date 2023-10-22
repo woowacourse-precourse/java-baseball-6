57963efbf5027cbe7d85b7f5c3f321f0aaa34820
saera <yook.saera@gmail.com>
2023-10-22 21:09:38 +0900

    feat: PlayerNumber가 사용자로부터 받은 입력을 정수 리스트로 변환하라

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,13 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+//        CompareNumbers compareNumbers = new CompareNumbers();
+//        String actual = compareNumbers.compare(comNumber, playerNumber);
+//        if ("3스트라이크".equals(actual)){
+//            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+//        } else {
+//
+//        }
     }
 }
--- src/main/java/baseball/PlayerNumber.java
+++ src/main/java/baseball/PlayerNumber.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PlayerNumber {
     public List<Integer> inputNumbers;
+
+    public void input() {
+        List<Integer> player = new ArrayList<>();
+        String playerInput = Console.readLine();
+        for (char number : playerInput.toCharArray()) {
+            player.add(number - '0');
+        }
+        inputNumbers = player;
+    }
 }
