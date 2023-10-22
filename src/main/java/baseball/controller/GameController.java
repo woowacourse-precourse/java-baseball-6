667ca538265de3aacd8675e52ab21fb166e81b07
jaehee <y2hjjh@naver.com>
2023-10-23 01:55:17 +0900

    feat: 게임 진행 메서드 작성

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -5,17 +5,26 @@ import baseball.model.InputNumber;
 
 public class GameController {
     static final AnswerNumber answerNumber = new AnswerNumber();
-    static InputNumber inputNumber = new InputNumber();
+    private PrintController printController = new PrintController();
+    private CountController countController = new CountController();
 
     public void createAnswer() {
         answerNumber.createRandomNumberList();
         System.out.println("정답: " + answerNumber.getRandomNumber());
     }
 
-    public void 시작입력() { //없애야함
-        inputNumber.setInputNumber("352");
-        System.out.println("사용자 입력: " + inputNumber.getInputNumber());
-    }
+    public void proceedGame() { //임시로 테스트 케이스 작성
+        int i = 0;
+        do {
+            //뷰로 입력 받기
+            InputNumber inputNumber = new InputNumber();
+            inputNumber.setInputNumber("516");
+            System.out.println("사용자 입력: " + inputNumber.getInputNumber());
 
+            System.out.println(printController.printCount(inputNumber));
+
+            i++;
+        } while (i < 3); //!isDone 검증 메서드
+    }
 }
 
