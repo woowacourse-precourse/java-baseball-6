1a468e356f43db1dbd052f41a56712c4e13b7843
Hyeon0208 <dmsgktn0208@gmail.com>
2023-10-22 23:33:48 +0900

    refactor : View 의 기능만 담당하도록 String 반환

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,6 +1,9 @@
 package baseball.view;
 
-import baseball.domain.PlayerNumber;
+import baseball.validation.NumberDuplicateValidator;
+import baseball.validation.NumberFormatValidator;
+import baseball.validation.NumberRangeValidator;
+import baseball.validation.NumberSizeValidator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
@@ -8,14 +11,16 @@ public class InputView {
     private InputView() {
     }
 
-    public static PlayerNumber inputPlayerNumber() {
-        String number = Console.readLine();
-        PlayerNumber playerNumber = new PlayerNumber(number);
-        return playerNumber;
+    public static String inputPlayerNumber() {
+        String input = Console.readLine();
+        NumberFormatValidator.validate(input);
+        NumberRangeValidator.validate(input);
+        NumberDuplicateValidator.validate(input);
+        NumberSizeValidator.validate(input);
+        return input;
     }
 
     public static String inputRetryCommand() {
-        String command = Console.readLine();
-        return command;
+        return Console.readLine();
     }
 }
