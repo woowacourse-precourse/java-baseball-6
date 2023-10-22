1ccaacf989491e1f08ac536b77c5b4867380f3d6
Lee JaeHyuk <80966413+JH22222@users.noreply.github.com>
2023-10-22 19:42:29 +0900

    feat: 낫싱 메시지 추가

--- src/main/java/output/OutputModule.java
+++ src/main/java/output/OutputModule.java
@@ -6,6 +6,7 @@ public class OutputModule {
     private final String inputMessage = "숫자를 입력해주세요 : ";
     private final String ballMessage = "볼";
     private final String strikeMessage = "스트라이크";
+    private final String nothingMessage = "낫싱";
     private final String out = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private final String oneMoreTime = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
@@ -28,6 +29,9 @@ public class OutputModule {
         System.out.print(strike + strikeMessage);
     }
 
+    public void printNothing(){
+        System.out.println(nothingMessage);
+    }
     public void printOut() {
         System.out.println(out);
     }
