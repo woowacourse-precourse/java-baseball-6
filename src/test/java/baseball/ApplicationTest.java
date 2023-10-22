861ad22f2ba8df68e3dc32f210a95a37170bbf71
ppusda <ppusda1234@gmail.com>
2023-10-22 14:48:48 +0900

    fix: 테스트 케이스 추가 및 정상 구동 여부 확인

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -28,6 +28,28 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 게임종료_후_예외_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> runException("246", "135", "abc"))
+                            .isInstanceOf(IllegalArgumentException.class);
+                },
+                1, 3, 5
+        );
+    }
+
+    @Test
+    void 게임_진행문구_확인() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("123", "456", "789", "125", "126", "153", "351", "135", "2");
+                    assertThat(output()).contains("1볼 1스트라이크", "1볼", "낫싱", "2스트라이크", "1스트라이크", "2볼 1스트라이크", "3볼", "3스트라이크", "게임 종료");
+                },
+                1, 3, 5
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
