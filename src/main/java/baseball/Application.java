93e1e6a18d30c0a975cd2edc9752ed75a49582e7
Jiihyun <cwlgusc@gmail.com>
2023-10-22 16:09:19 +0900

    feat(Application): 필요한 객체 생성 후 게임 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
+import baseball.controller.GameController;
+import baseball.domain.RandomBallNumberGenerator;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        OutputView outputView = new OutputView();
+        InputView inputView = new InputView();
+        RandomBallNumberGenerator randomBallNumberGenerator = new RandomBallNumberGenerator();
+        GameController gameController = new GameController(outputView, inputView, randomBallNumberGenerator);
+        gameController.start();
     }
 }
