3bc446710ffdfe348e3b10e9d82ea27e20b0edf7
Brian <bwook9908@gmail.com>
2023-10-22 12:42:12 +0900

    feat: 사용자 입력 기능 추가

---
+++ src/main/java/baseball/exception/InputException.java
@@ -0,0 +1,47 @@
+package baseball.exception;
+
+public class InputException {
+    public static void validateInput(String input) {
+        checkNumberLength(input);
+        isNumeric(input);
+        checkInputNumberOverlap(input);
+    }
+
+    public static void validateRestartInput(String input) {
+        checkRestartNumberLength(input);
+        checkRestartNumberType(input);
+    }
+
+    private static void checkNumberLength(String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("입력된 숫자는 3자리여야 합니다.");
+        }
+    }
+    private static void checkRestartNumberLength(String input) {
+        if (input.length() != 1) {
+            throw new IllegalArgumentException("입력된 숫자는 1자리여야 합니다.");
+        }
+    }
+
+    private static void checkRestartNumberType(String input) {
+        if (input.equals("1") || input.equals("2")) {
+            throw new IllegalArgumentException("1 또는 2를 입력해야 합니다.");
+        }
+    }
+
+    private static void checkInputNumberOverlap(String input) {
+        char firstChar = input.charAt(0);
+        char secondChar = input.charAt(1);
+        char thirdChar = input.charAt(2);
+        if (firstChar == secondChar || secondChar == thirdChar || firstChar == thirdChar) {
+            throw new IllegalArgumentException("서로 다른 3자리를 입력해주세요");
+        }
+    }
+    private static void isNumeric(String str) {
+        try {
+            int number = Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("입력된 값은 숫자여야 합니다.");
+        }
+    }
+}
---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,23 @@
+package baseball.view;
+
+import baseball.util.Convertion;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import baseball.exception.InputException;
+
+public class InputView {
+    public ArrayList<Integer> numberInput() {
+        System.out.print("숫자를 입력해주세요 :");
+        String input = Console.readLine();
+        InputException.validateInput(input);
+        return Convertion.stringToArrayList(input);
+    }
+
+    public Integer restartInput() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = Console.readLine();
+        InputException.validateRestartInput(input);
+        return Integer.parseInt(input);
+    }
+}
