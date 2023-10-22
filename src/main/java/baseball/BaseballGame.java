a7b8256d1ddd2af34c0a197a3eac5c6752b1e4ba
mmalfoy <stpmalfoy@gmail.com>
2023-10-22 16:50:51 +0900

    Refactor : 구현 기능Class 내부, 입출력 기능 이름 변경에 따른  메소드 코드 (스네이크 -> upperSnakeCase) 변경 적용

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -33,6 +33,7 @@ public class BaseballGame {
                 countBallandStrike();
                 printBallandStrike();
 
+
             }while(!isThreeStrike);
 
         }catch (IllegalArgumentException e){
