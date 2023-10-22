fdccdf045e595c229f67455acf88f9f810ca5be7
devhoya97 <diajbh123@gmail.com>
2023-10-22 16:45:26 +0900

    refactor : 메서드들의 반환타입 변경 BaseballNumbers라는 객체에게 역할을 위임함에 따라 BaseballNumbersGenerator도 List<BaseballNumber> 형태가 아닌 BaseballNumbers 객체를 반환하는 것이 자연스러워 졌습니다. 따라서 이를 반환하도록 메서드들을 수정했고, 특히 getUserBaseballNumbers 메서드는, 숫자로만 이루어진 StringNumbers 객체를 받아서, 이를 IntegerList로 변환하는 메서드도 가지고 있기에 스트림을 활용하여 손쉽게 List<BaseballNumber> 형태로 변환할 수 있었습니다. 마지막으로 이를 BaseballNumbers 생성자에 전달해줌으로써, 해당 BaseballNumber들의 길이가 올바른지, 중복값은 없는지에 대한 검사는 BaseballNumbers객체가 수행하도록 역할을 분담하였습니다

--- src/main/java/baseball/model/BaseballNumbersGenerator.java
+++ src/main/java/baseball/model/BaseballNumbersGenerator.java
@@ -1,9 +1,7 @@
 package baseball.model;
 
-import baseball.utils.Validator;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,32 +9,23 @@ public class BaseballNumbersGenerator {
     private static final int BASEBALL_NUMBERS_LENGTH = 3;
     private static final int MIN_BASEBALL_NUMBER = 1;
     private static final int MAX_BASEBALL_NUMBER = 9;
-    private static final String EMPTY_STRING = "";
 
-    public static List<BaseballNumber> getComputerBaseballNumbers() {
-        List<BaseballNumber> computerBaseballNumbers = new ArrayList<>();
-        while (computerBaseballNumbers.size() != BASEBALL_NUMBERS_LENGTH) {
+    public static BaseballNumbers getComputerBaseballNumbers() {
+        List<BaseballNumber> computerBaseballNumberList = new ArrayList<>();
+        while (computerBaseballNumberList.size() != BASEBALL_NUMBERS_LENGTH) {
             int randomNumber = Randoms.pickNumberInRange(MIN_BASEBALL_NUMBER, MAX_BASEBALL_NUMBER);
             BaseballNumber randomBaseballNumber = new BaseballNumber(randomNumber);
-            if (!computerBaseballNumbers.contains(randomBaseballNumber)) {
-                computerBaseballNumbers.add(randomBaseballNumber);
+            if (!computerBaseballNumberList.contains(randomBaseballNumber)) {
+                computerBaseballNumberList.add(randomBaseballNumber);
             }
         }
-        return computerBaseballNumbers;
+        return new BaseballNumbers(computerBaseballNumberList);
     }
 
-    public static List<BaseballNumber> getUserBaseballNumbers(String baseballNumbersString) {
-        validateBaseballNumbersString(baseballNumbersString);
-        return Arrays.stream(baseballNumbersString.split(EMPTY_STRING))
-                .map(Integer::parseInt)
+    public static BaseballNumbers getUserBaseballNumbers(StringNumbers baseballStringNumbers) {
+        return new BaseballNumbers(baseballStringNumbers.translateToIntegerList()
+                .stream()
                 .map(BaseballNumber::new)
-                .collect(Collectors.toList());
-    }
-
-    private static void validateBaseballNumbersString(String baseballNumbersString) {
-        Validator.validateInputEmptyOrNull(baseballNumbersString);
-        Validator.validateInputLength(baseballNumbersString, BASEBALL_NUMBERS_LENGTH);
-        Validator.validateInputDigit(baseballNumbersString);
-        Validator.validateInputDistinct(baseballNumbersString);
+                .collect(Collectors.toList()));
     }
 }
