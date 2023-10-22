a0edb8285a06e88ac399199fc2508308e896cb1a
gmu1026 <gmu1026@gmail.com>
2023-10-22 13:59:43 +0900

    STYLE: 코드 가독성 향상

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,6 +9,7 @@ public class Application {
     public static void main(String[] args) {
         System.out.print("숫자를 입력해주세요 : ");
         String playerInputBall = Console.readLine();
+
         String[] inputBallSplit = playerInputBall.split("");
         List<Integer> playerBalls = new ArrayList<>();
         for (String ball:inputBallSplit) {
