21c389352daacf3a297e29b552ff6d1aafd5af94
Woongbae Yun <devbelly@naver.com>
2023-10-22 15:49:56 +0900

    feat : 숫자 야구 게임 전체 로직 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameStatus gameStatus = new GameStatus();
+        while (!gameStatus.isEnd()) {
+            Information.printStartGame();
+            Computer computer = new Computer();
+            GameService gameService = new GameService();
+            while (!gameService.isAllNumberMatched()) {
+                Player player = new Player();
+
+                Information.printInputNumber();
+                player.guessNumber(Console.readLine());
+
+                gameService.comparePlayerAndComputer(player, computer);
+                Information.printBallAndStrike(gameService.getBall(), gameService.getStrike());
+            }
+            Information.printGuessAll();
+            Information.printEndGame();
+            gameStatus.updateGameStatus(Console.readLine());
+        }
     }
 }
