f7982923dd70bcd5713e288141870772eab1237a
songsunkook <songsunkook@gmail.com>
2023-10-22 22:11:33 +0900

    feat: 중복되지 않는 숫자 생성

--- src/main/java/domain/Number.java
+++ src/main/java/domain/Number.java
@@ -1,8 +1,34 @@
 package domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Number {
     private final int num;
+    public static final int numberCount = 3;
+
+    private static final int minimumNumber = 1;
+    private static final int maximumNumber = 9;
+    private static final List<Integer> goalNumbers = new ArrayList<>(numberCount);
+
+    static {
+        setGoalNumbers();
+    }
+
     public Number(int num) {
         this.num = num;
+    }
+
+    private static void setGoalNumbers() {
+        for (int i = 0; i < numberCount; i++) {
+            int randomNumber = Randoms.pickNumberInRange(minimumNumber, maximumNumber);
+            if (!goalNumbers.contains(randomNumber)) {
+                goalNumbers.set(i, randomNumber);
+            }
+        }
+    }
+
     }
 }
