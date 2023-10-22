d2061030537d19de0692de74940ecd304bd88f37
woo <leindey@gmail.com>
2023-10-22 16:07:01 +0900

    feat: 게임 시작 및 간단한 입력 받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Scanner scanner = new Scanner(System.in);
+
+        // 게임 시작
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.print("숫자를 입력해주세요 : ");
+        int num = Integer.parseInt(scanner.nextLine());
+
+
     }
 }
