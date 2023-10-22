5c35e52b12b9f347a28c0f4b6c1bb2931fd892d7
leesangjun <hubls@naver.com>
2023-10-22 16:25:38 +0900

    refactor(Computer): 하드코딩 상수로 변경

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -6,12 +6,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
+    final static int COMPUTER_NUMBER_LENGTH = 3;
+    final static int COMPUTER_NUMBER_MINIMUM = 1;
+    final static int COMPUTER_NUMBER_MAXIMUM = 9;
     List<Integer> computerNumber = new ArrayList<>();
 
     public void createComputerNumber() {
         computerNumber.clear();
-        while (computerNumber.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+        while (computerNumber.size() < COMPUTER_NUMBER_LENGTH) {
+            int randomNumber = Randoms.pickNumberInRange(COMPUTER_NUMBER_MINIMUM, COMPUTER_NUMBER_MAXIMUM);
             if (!computerNumber.contains(randomNumber)) {
                 computerNumber.add(randomNumber);
             }
