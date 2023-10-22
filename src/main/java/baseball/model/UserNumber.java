a661e28ac036736e0e47b085662f0471277f5422
박형균 <phk@baghyeong-gyun-ui-MacBookPro.local>
2023-10-22 13:44:16 +0900

    refactor(UserNumber, UserNumberGenerateService): modify UserNumber and delete UserNumberGenerateService
    *UserNumber에 사용자가 입력한 문자열의 유효성을 검증하는 로직추가
    *UserNumber에 문자열을 숫자배열로 만드는 로직추가
    *UserNumberGenerateService는 더이상 필요하지 않으므로 삭제

--- src/main/java/baseball/model/UserNumber.java
+++ src/main/java/baseball/model/UserNumber.java
@@ -1,16 +1,65 @@
 package baseball.model;
 
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class UserNumber {
 
-    private final int[] NUMBER;
+    private final List<Integer> number;
 
-    public UserNumber(int[] NUMBER) {
-        this.NUMBER = NUMBER;
+    public UserNumber(String input) {
+        this.number = generateUserNumber(input);
     }
 
-    public int[] getNumber() {
-        return this.NUMBER;
+    private List<Integer> generateUserNumber(String input) {
+        isRightFormatNumber(input);
+        isOverLengthNumber(input);
+        isDuplicatedNumber(input);
+        isContainedZero(input);
+        int[] userNumber = convertStringToList(input);
+        return Arrays.stream(userNumber).boxed().collect(Collectors.toList());
+
+    }
+
+    public List<Integer> getNumber() {
+        return number;
+    }
+
+    private int[] convertStringToList(String input) {
+        return Arrays.stream(input.split("")).mapToInt(Integer::parseInt).toArray();
+    }
+
+    private void isDuplicatedNumber(String input) {
+        long count = input.chars().distinct().count();
+        if (count != 3) {
+            System.out.println("중복된 숫자가 있습니다.");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void isRightFormatNumber(String input) {
+        if (!input.matches("\\d+")) {
+            System.out.println("숫자가 아닌 값이 포함 되어 있습니다.");
+            throw new IllegalArgumentException();
+        }
+
+    }
+
+    private void isContainedZero(String input) {
+        if (input.contains("0")) {
+            System.out.println("0이 포함되어 있습니다.");
+            throw new IllegalArgumentException();
+        }
+
+    }
+
+    private void isOverLengthNumber(String input) {
+        if (input.length() != 3) {
+            System.out.println("숫자는 3개여야 합니다.");
+            throw new IllegalArgumentException();
+        }
     }
 
 }
