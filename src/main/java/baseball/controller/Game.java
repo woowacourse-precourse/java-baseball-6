dbe3a3fe49723f884ce5244fd97757beb83e196a
yoonjuho <dbswn990@naver.com>
2023-10-23 04:00:27 +0900

    feat(Game): 컴퓨터가 선택한 숫자를 모두 맞히면 종료된다

---
+++ src/main/java/baseball/controller/Game.java
@@ -0,0 +1,46 @@
+package baseball.controller;
+
+import baseball.constant.GameResult;
+import baseball.model.Computer;
+import baseball.model.GameNumber;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+import java.util.List;
+import java.util.Map;
+
+public class Game {
+    private final int GAME_OVER_CONDITION = 3;
+    private final OutputView outputView;
+    private final Computer computer;
+
+
+    public Game() {
+        outputView = new OutputView();
+        computer = generateComputer();
+    }
+
+    public void play(List<Integer> userNumbers) {
+        GameNumber userGameNumber = new GameNumber(userNumbers);
+        Map<GameResult, Long> result = computer.calculate(userGameNumber);
+        if (!checkGameOver(result)) {
+            play(InputView.inputNumbers());
+        }
+    }
+
+
+    private Computer generateComputer() {
+        List<Integer> computerNumber = Computer.createNumbers();
+        return new Computer(computerNumber);
+    }
+
+
+    private boolean checkGameOver(Map<GameResult, Long> result) {
+        if (result.containsKey(GameResult.STRIKE)
+                && result.get(GameResult.STRIKE) == GAME_OVER_CONDITION) {
+            return true;
+        }
+        return false;
+    }
+
+}
