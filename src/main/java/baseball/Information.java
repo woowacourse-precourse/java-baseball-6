adc1c0417f84c6dc95631a0a8406a1024f4a8d0a
Woongbae Yun <devbelly@naver.com>
2023-10-22 15:02:29 +0900

    feat : 숫자 입력 안내 문구를 출력하는 기능 추가

--- src/main/java/baseball/Information.java
+++ src/main/java/baseball/Information.java
@@ -3,8 +3,13 @@ package baseball;
 public class Information {
 
     private static final String START_GAME = "숫자 야구 게임을 시작합니다.";
+    private static final String INPUT_NUMBER = "숫자를 입력해주세요 : ";
 
     public static void printStartGame() {
         System.out.println(START_GAME);
+    }
+
+    public static void printInputNumber() {
+        System.out.println(INPUT_NUMBER);
     }
 }
