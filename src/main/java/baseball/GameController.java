ff5e763b51f4cfe70adb5e392fdf3a364183ffa6
DaeyeonCho <daecho8@gmail.com>
2023-10-22 16:09:20 +0900

    feat: 게임 재시작 기능 구현
    3개 숫자를 모두 맞히면 해당 게임을 종류하고 새로운 게임을 시작할지 종류할지 사용자에게 선택권이 주어집니다.
    스트라이크 카운트 값이 3 (모두 정답) 일때만 게임을 종류하도록 합니다.

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -7,6 +7,7 @@ public class GameController {
     static String guessNum;
     static boolean canPlay;
     static String theAnswer;
+    static boolean isAnswer;
 
     public static void gameInitializer() {
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -16,7 +17,10 @@ public class GameController {
             theAnswer = AnswerController.makeAnswer();
             System.out.println("answer: " + theAnswer);    // 테스트용
 
-            gamePlay();
+            isAnswer = false;
+            while (!isAnswer) {
+                gamePlay();
+            }
         }
     }
 
@@ -28,7 +32,21 @@ public class GameController {
         int resultCount = Counter.hitBallCounter(guessNum, theAnswer);
         System.out.println("resultCount = " + resultCount);     // for test
 
+        playAgain(resultCount);
     }
 
+    private static void playAgain(int resultCount) {
+        if (resultCount == 3) {
+            isAnswer = true;
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종류하려면 2를 입력하세요.");
+            int playOrStop = Integer.parseInt(Console.readLine());
+            if (playOrStop == 1) {
+                canPlay = true;
+            } else if (playOrStop == 2) {
+                canPlay = false;
+            }
+        }
+    }
 
 }
