ba43522646d87f03d9b0de95598e0164de8ead3a
insu3160 <is23193160@gmail.com>
2023-10-22 15:08:50 +0900

    feat : 게임을 시작하면 Judge는 랜덤한 숫자 3자리 리스트 저장하는 기능 구현
    run이 종료되면 재시작이나 종료를 통해 while문을 종료할 예정입니다.

--- src/main/java/baseball/controller/BaseBallController.java
+++ src/main/java/baseball/controller/BaseBallController.java
@@ -1,10 +1,21 @@
 package baseball.controller;
 
+import baseball.Judge;
+import baseball.RandomNumberGenerator;
 import baseball.view.OutputView;
 
 public class BaseBallController {
+    private Judge judge;
 
     public void playGame() {
         OutputView.startGameMessage();
+        do {
+            judge = new Judge(RandomNumberGenerator.createRandomNumber());
+            run();
+        } while (true);
+    }
+
+    private void run() {
+
     }
 }
