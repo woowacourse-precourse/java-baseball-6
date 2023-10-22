25acb41475d846676a93769d4026e20ddc2fe07f
HyunShik <hshong0907@gmail.com>
2023-10-22 21:44:39 +0900

    feat: 하드 모드 카운트 7 이상 시 실패 로직 추가

---
+++ src/main/java/baseball/hard/service/HardService.java
@@ -0,0 +1,18 @@
+package baseball.hard.service;
+
+import baseball.common.service.CommonNumberService;
+
+public class HardService extends CommonNumberService {
+
+    private static final int COUNT_LIMIT = 7;
+
+    //카운트 제한을 넘었는지 체크
+    public boolean isCountOver(int count){
+        if(count>COUNT_LIMIT) return true;
+        return false;
+    }
+
+
+    //카운트 캡슐화
+    public int getCountLimit(){return COUNT_LIMIT;}
+}
