62d2b761b0d4117e38185000aa0291802133aa7d
LeeSeungUk <tmddnr5410@gmail.com>
2023-10-23 00:35:51 +0900

    feat : 정답 처리 기능 구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -8,10 +8,16 @@ public class BaseballGame {
         System.out.println("숫자 야구 게임을 시작합니다.");
 
         boolean isPlaying = true;
-        int answer = Randoms.pickNumberInRange(111, 999);
+        int answerPlayer = -1;
+        int answerComputer = Randoms.pickNumberInRange(111, 999);
+        System.out.print("생성된 숫자는 " + answerComputer + "\n");
         do {
             System.out.print("숫자를 입력해주세요 : ");
-            Console.readLine();
+            answerPlayer = Integer.parseInt(Console.readLine());
+            if (answerPlayer == answerComputer) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                isPlaying = false;
+            }
         } while (isPlaying);
     }
 }
