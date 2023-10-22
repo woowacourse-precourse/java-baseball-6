dc899d9bb56b4e19110c937d240d2613ae6d2ef1
NAM GIBEOM <rlqja0523@naver.com>
2023-10-22 21:40:00 +0900

    refactor(Status): 게임 종료 코드 검증 분리

--- src/main/java/baseball/enums/GameStatus.java
+++ src/main/java/baseball/enums/GameStatus.java
@@ -1,20 +1,24 @@
 package baseball.enums;
 
+import java.util.Arrays;
+
 public enum GameStatus {
     PLAY("1"),
     EXIT("2");
 
+    private static final GameStatus[] gameStatuses = GameStatus.values();
     private final String status;
 
     GameStatus(String status) {
         this.status = status;
     }
 
-    public String getStatus() {
-        return status;
-    }
-
-    public boolean equals(String status) {
-        return this.status.equals(status);
+    public static GameStatus of(String status) {
+        return Arrays.stream(gameStatuses)
+                .filter(gameStatus -> gameStatus.status.equals(status))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(
+                        String.format("게임 재시작은 %s, 종료는 %s이어야 합니다.", GameStatus.PLAY.status, GameStatus.EXIT.status))
+                );
     }
 }
