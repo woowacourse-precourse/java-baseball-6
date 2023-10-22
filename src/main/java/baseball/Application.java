573fd6bc87ca66284d523c0045c3731e6fb97423
jjaegii <hn06038@gmail.com>
2023-10-22 17:39:03 +0900

    refactor: separating UserInput class into files

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,38 +1,6 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.ArrayList;
-import java.util.List;
-
-class UserInput {
-    private final List<Integer> numbers;
-
-    public UserInput() {
-        this.numbers = new ArrayList<>();
-        System.out.print("숫자를 입력해주세요 : ");
-        String input = Console.readLine();
-
-        if (input.length() != 3) {
-            throw new IllegalArgumentException("세 자리의 숫자를 입력해주세요.");
-        }
-
-        for (char c : input.toCharArray()) {
-            if (!Character.isDigit(c) || c == '0') {
-                throw new IllegalArgumentException("1-9 사이의 숫자만 입력해주세요.");
-            }
-
-            int number = Character.getNumericValue(c);
-            if (numbers.contains(number)) {
-                throw new IllegalArgumentException("서로 다른 숫자를 입력해주세요.");
-            }
-            numbers.add(number);
-        }
-    }
-
-    public List<Integer> getNumbers() {
-        return numbers;
-    }
-}
 
 class Game {
     private ComputerNumbers computerNumbers;
