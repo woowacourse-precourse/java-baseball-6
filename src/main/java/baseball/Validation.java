739eb02dbe82b97191f7b7a7a880559eb6741fbf
임강호 <55732952+Icln@users.noreply.github.com>
2023-10-22 19:43:45 +0900

    refactor: 입력 검증 메소드 분리

--- src/main/java/baseball/Validation.java
+++ src/main/java/baseball/Validation.java
@@ -1,9 +1,26 @@
 package baseball;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Validation {
     private static final int ZERO = 0;
     private static final int RESTART_NUMBER = 1;
     private static final int QUIT_NUMBER = 2;
     private static final int CORRECT_INPUT_RANGE = 3;
+
+    public void inputValidation(String inputString) {
+        Set<Character> inputArr = new HashSet<>();
+        for (char c : inputString.toCharArray()) {
+            if (!Character.isDigit(c) || Character.getNumericValue(c) == ZERO) {
+                throw new IllegalArgumentException();
+            }
+            inputArr.add(c);
+        }
+
+        if (inputArr.size() != CORRECT_INPUT_RANGE) {
+            throw new IllegalArgumentException();
+        }
+    }
 
 }
