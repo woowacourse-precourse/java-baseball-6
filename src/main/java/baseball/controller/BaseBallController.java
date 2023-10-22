da922ac1f5ea656fd73e1f96b054ead7798f4eaa
Relaxed-Mind <dlwlstn0709@uos.ac.kr>
2023-10-22 13:11:51 +0900

    Feat: judge Strike And Ball 스트라이크 볼 확인하는 함수 구현 judge Strike And Ball #1

--- src/main/java/baseball/controller/BaseBallController.java
+++ src/main/java/baseball/controller/BaseBallController.java
@@ -10,8 +10,10 @@ public class BaseBallController {
 
     public void start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        List<Integer> computerNum = systemService.generateComputerNum();
-        baseBallService.init(computerNum);
-        baseBallService.start();
+        
+        do {
+            List<Integer> computerNum = systemService.generateComputerNum();
+            baseBallService.init(computerNum);
+        } while (baseBallService.start());
     }
 }
--- src/main/java/baseball/service/BaseBallService.java
+++ src/main/java/baseball/service/BaseBallService.java
@@ -1,5 +1,7 @@
 package baseball.service;
 
+import static baseball.common.Constants.NUMBER_LENGTH;
+
 import baseball.validation.InputValidator;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
@@ -8,23 +10,47 @@ import java.util.List;
 public class BaseBallService {
     private static List<Integer> computerNum;
     private static List<Integer> userNum;
-    private static int strike = 0;
-    private static int ball = 0;
+    private static int strike;
+    private static int ball;
 
     private static InputValidator inputValidator = new InputValidator();
 
     public void init(List<Integer> generatedComputerNum) {
         computerNum = generatedComputerNum;
+        initStrikeAndBall();
     }
 
-    public void start() {
-        boolean haveToGuess = true;
-        
-        while (haveToGuess) {
+    public void initStrikeAndBall() {
+        strike = 0;
+        ball = 0;
+    }
+
+    public boolean start() {
+
+        do {
+            initStrikeAndBall();
             receiveUserInput();
             compareUserAndComputer();
-        }
+        } while (judgeStrikeAndBall());
 
+        return true;
+    }
+
+    public boolean judgeStrikeAndBall() {
+        if (strike == NUMBER_LENGTH) {
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return false;
+        } else if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+        } else if (strike == 0 && ball != 0) {
+            System.out.println(ball + "볼");
+        } else if (strike != 0 && ball == 0) {
+            System.out.println(strike + "스트라이크");
+        } else {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
+        return true;
     }
 
     public void compareUserAndComputer() {
