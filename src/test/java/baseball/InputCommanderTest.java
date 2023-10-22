28bb5f7d654218e7cd305f2f6c9001da1f96d240
yubin <dbqls2821@gmail.com>
2023-10-22 16:57:52 +0900

    fix: InputCommanderTest의 메서드명 변경

--- src/test/java/baseball/InputCommanderTest.java
+++ src/test/java/baseball/InputCommanderTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class InputCommanderTest {
 
     @Test
-    void receiveInput(){
+    void 사용자의_입력을_받아온다(){
         String expected = "wooteco";
         System.setIn(new ByteArrayInputStream(expected.getBytes()));
         InputCommander inputCommander = new InputCommander();
