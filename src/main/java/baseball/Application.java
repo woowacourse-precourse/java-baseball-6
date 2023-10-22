1920d53b9859bc90a620ba1d75c016127c71eaba
Su-daa <subin.kk019@gmail.com>
2023-10-23 00:08:55 +0900

    # 본문은 아래에 작성 Feat: 사용자에게 3개 수 입력 받기 기능 구현 --- 후에 입력값에 대한 유효성 확인 로직 작성 필요 현재는 서로다른 숫자 3개만 입력 된다고 가정하고 작성함 # 꼬릿말은 아래에 작성: ex) Github issue #23
    # --- COMMIT END ---
    #   <타입> 리스트
    #   feat        : 기능 (새로운 기능)
    #   fix         : 버그 (버그 수정)
    #   refactor    : 리팩토링
    #   design      : CSS 등 사용자 UI 디자인 변경
    #   comment     : 필요한 주석 추가 및 변경
    #   style       : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs        : 문서 수정 (문서 추가, 수정, 삭제, README)
    #   test        : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore       : 기타 변경사항 (빌드 스크립트 수정, assets, 패키지 매니저 등)
    #   init        : 초기 생성
    #   rename      : 파일 혹은 폴더명을 수정하거나 옮기는 작업만 한 경우
    #   remove      : 파일을 삭제하는 작업만 수행한 경우
    # ------------------
    #   제목 첫 글자를 대문자로
    #   제목은 명령문으로
    #   제목 끝에 마침표(.) 금지
    #   제목과 본문을 한 줄 띄워 분리하기
    #   본문은 "어떻게" 보다 "무엇을", "왜"를 설명한다.
    #   본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    # ------------------
    #   <꼬리말>
    #   필수가 아닌 optioanl
    #   Fixes        :이슈 수정중 (아직 해결되지 않은 경우)
    #   Resolves     : 이슈 해결했을 때 사용
    #   Ref          : 참고할 이슈가 있을 때 사용
    #   Related to   : 해당 커밋에 관련된 이슈번호 (아직 해결되지 않은 경우)
    #   ex) Fixes: #47 Related to: #32, #21

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,16 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+
+
+    public static final int BALLS_SIZE = 3;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
@@ -15,9 +20,10 @@ public class Application {
      * 랜덤 볼 3개 만들기
      */
     public List<Integer> makeRandomBalls() {
+
         List<Integer> randomBalls = new ArrayList<>();
 
-        while (randomBalls.size() < 3) {
+        while (randomBalls.size() < BALLS_SIZE) {
             int randomBall = Randoms.pickNumberInRange(1, 9);
             if (!randomBalls.contains(randomBall)) {
                 randomBalls.add(randomBall);
@@ -27,4 +33,21 @@ public class Application {
         return randomBalls;
     }
 
+    /**
+     * 사용자에게 3개 숫자 입력받기
+     */
+    public List<Integer> inputUserBalls() {
+
+        List<Integer> userBalls = new ArrayList<>();
+
+        System.out.print("숫자를 입력해주세요 : ");
+        String userInput = Console.readLine();
+
+        //TODO: 입력값 유효성 확인 로직 필요
+        for (char c : userInput.toCharArray()) {
+            userBalls.add(Character.getNumericValue(c));
+        }
+
+        return userBalls;
+    }
 }
