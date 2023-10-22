698f9b550b6900b114f7ae2d61004e2ec97f70a0
Hoo-sung <gntjd135@gmail.com>
2023-10-22 16:11:03 +0900

    refactor: ControlNumVerifier.check 함수 2개로 세분화

--- src/main/java/baseball/verifier/ControlNumVerifier.java
+++ src/main/java/baseball/verifier/ControlNumVerifier.java
@@ -3,8 +3,22 @@ package baseball.verifier;
 public class ControlNumVerifier implements Verifier {
     @Override
     public void check(String input) throws IllegalArgumentException {
-        if(input.equals("1")|| input.equals("2"))
-            return;
-        throw new IllegalArgumentException("1 또는 2를 입력하지 않았습니다.");
+        checkNumeric(input);
+        checkRange(input);
+    }
+
+    private void checkNumeric(String input){
+        try{
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("1~9사이 수가 아닌 값을 입력하였습니다.");
+        }
+    }
+
+    private void checkRange(String input){//1 또는 2가 아닌 모둔 숫자는 예외처리.
+        int control = Integer.parseInt(input);
+        if(control != 1 && control != 2){
+            throw new IllegalArgumentException("알 수 없는 컨트롤 값입니다.");
+        }
     }
 }
