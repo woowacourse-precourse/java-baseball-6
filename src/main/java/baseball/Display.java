81bbf654590709976d030f3a815a151bcf4c6827
minjun7410 <minjun7410@gmail.com>
2023-10-22 22:58:15 +0900

    refactor(Display): printJudgeResult 메서드 리팩토링
    * else를 없애고 조기 반환하여 코드가 명확해지도록 리팩토링했다.

--- src/main/java/baseball/Display.java
+++ src/main/java/baseball/Display.java
@@ -14,16 +14,19 @@ public class Display {
     public void printJudgeResult(int ballCount, int strikeCount){
         if(ballCount == 0 && strikeCount == 0){
             System.out.println("낫싱");
+            return;
         }
-
-        else if(ballCount == 0){
-            System.out.printf("%d스트라이크\n", strikeCount);
-        }
-        else if(strikeCount == 0){
-            System.out.printf("%d볼\n", ballCount);
-        }
-        else{
+        if(ballCount > 0 && strikeCount > 0){
             System.out.printf("%d볼 %d스트라이크\n", ballCount, strikeCount);
+            return;
+        }
+        if(ballCount == 0){
+            System.out.printf("%d스트라이크\n", strikeCount);
+            return;
+        }
+        if(strikeCount == 0){
+            System.out.printf("%d볼\n", ballCount);
+            return;
         }
     }
     public int[] getUserInput(){
