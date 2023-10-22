7b757325e089a8c35e10cdc2c9b28b2d6c2853b5
minwoo <mark42son@gmail.com>
2023-10-22 19:28:46 +0900

    docs : 필요없는 주석 제거

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -22,7 +22,7 @@ public class User {
     }
 
     private static String inputUserNum() {
-        return Console.readLine();  // java의 readLine()에 대하여 아는대로 설명하면?
+        return Console.readLine();
     }
 
     private void validateInput(String input) {
@@ -35,12 +35,11 @@ public class User {
             throw new IllegalArgumentException("입력 숫자의 자릿수는 3자리로 입력해주세요.");
         }
     }
+
     private static int changeToInteger(String input) {
-        try {  // java의 try-catch에 대한 개념과 예제 코드는?
+        try {
             return Integer.parseInt(input);
-        }
-        // java의 IllegalArgumentException, NumberFormatException을 설명?
-        catch (NumberFormatException e) {
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("서로다른 세 자리 자연수를 입력해주세요.");
         }
     }
