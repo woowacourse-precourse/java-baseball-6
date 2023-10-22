b750c4cf593a71826794dda746dbeab8b0a845d9
shAn-kor <kjn0406@naver.com>
2023-10-22 18:49:51 +0900

    볼, 스트라이크 카운터 객체 생성해 객체가 볼, 스트라이크 카운트
    OutputView 에서는 컨트롤러가 전달해준 BallStrikeCounter 객체의 toString 값 출력만 하도록 변경
    
    BallStrikeCounter 테스트 코드 작성

--- src/main/java/baseball/controller/BaseballGame.java
+++ src/main/java/baseball/controller/BaseballGame.java
@@ -5,6 +5,7 @@ import static baseball.view.OutputView.gameStartMessage;
 import static baseball.view.OutputView.printCelebrate;
 import static baseball.view.OutputView.userInputMessage;
 
+import baseball.model.BallStrikeCounter;
 import baseball.model.ComputerNumber;
 import baseball.model.RetryCheck;
 import baseball.model.UserNumber;
@@ -14,16 +15,16 @@ import java.util.List;
 
 
 public class BaseballGame {
+    private final BallStrikeCounter ballStrikeCounter = new BallStrikeCounter();
     private List<Integer> computerNumber;
-    private int[] cntBallStrike;
 
     public BaseballGame() {
-        this.cntBallStrike = new int[2];
         this.computerNumber = new ArrayList<>();
     }
 
     public void gameStart() {
-        this.cntBallStrike = new int[2];
+
+        this.ballStrikeCounter.resetBallStrike();
 
         gameStartMessage();
 
@@ -38,39 +39,27 @@ public class BaseballGame {
     }
 
     public void inGame() {
-        while (cntBallStrike[1] != 3) {
+        while (true) {
             //사용자 값 입력
             userInputMessage();
             UserNumber userNumber = new UserNumber(userInput());
 
             //볼, 스트라이크 카운트
-            this.countBallStrike(userNumber.getInputNumber(), computerNumber);
+            ballStrikeCounter.countBallStrike(userNumber.getInputNumber(), computerNumber);
 
             //볼, 스트라이크 값 출력
-            OutputView.printBallStrike(cntBallStrike[0], cntBallStrike[1]);
+            OutputView.printBallStrike(ballStrikeCounter.toString());
+
+            //3스트라이크 시 while 종료
+            if (ballStrikeCounter.toString().equals("3스트라이크")) {
+                break;
+            }
 
             //볼, 스트라이크 카운트 초기화
-            if (cntBallStrike[1] != 3) {
-                cntBallStrike[0] = 0;
-                cntBallStrike[1] = 0;
-            }
+            ballStrikeCounter.resetBallStrike();
         }
 
         printCelebrate();
-    }
-
-    public void countBallStrike(List<Integer> userNumber, List<Integer> computerNumber) {
-
-        for (int i = 0; i < userNumber.size(); i++) {
-
-            if (userNumber.get(i).equals(computerNumber.get(i))) {
-                cntBallStrike[1]++;
-            } else if (computerNumber.contains(userNumber.get(i))) {
-                cntBallStrike[0]++;
-            }
-
-        }
-
     }
 
     public void retryCheck() {
@@ -79,9 +68,5 @@ public class BaseballGame {
         if (retryCheck.getNum().equals("1")) {
             this.gameStart();
         }
-
-        /*if (retryCheck.getNum().equals("2")) {
-
-        }*/
     }
 }
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -9,22 +9,8 @@ public class OutputView {
         System.out.print("숫자를 입력해주세요 : ");
     }
 
-    public static void printBallStrike(Integer ball, Integer strike) {
-        if (ball == 0 && strike == 0) {
-            System.out.println("낫싱");
-        }
-
-        if (ball > 0 && strike == 0) {
-            System.out.println(ball + "볼");
-        }
-
-        if (strike > 0 && ball == 0) {
-            System.out.println(strike + "스트라이크");
-        }
-
-        if (strike > 0 && ball > 0) {
-            System.out.println(ball + "볼 " + strike + "스트라이크");
-        }
+    public static void printBallStrike(String result) {
+        System.out.println(result);
     }
 
     public static void printCelebrate() {
