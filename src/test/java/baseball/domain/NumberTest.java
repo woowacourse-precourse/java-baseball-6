f7594afd6011132744f266f266b3014131608ed5
JuSeong1130 <jsmoon1130@naver.com>
2023-10-22 19:55:29 +0900

    test(Number) : 다른 Number와 같은지 체크 기능 테스트
    변수
    1. 1과 1은 true
    2. 1과 1이아닌 다른값 false

--- src/test/java/baseball/domain/NumberTest.java
+++ src/test/java/baseball/domain/NumberTest.java
@@ -2,10 +2,15 @@ package baseball.domain;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class NumberTest {
@@ -25,4 +30,22 @@ class NumberTest {
             Number number = new Number(inputNumber);
         });
     }
+
+
+    @DisplayName("Number 값 에따른 true false 테스트")
+    @ParameterizedTest
+    @MethodSource("createIsSameNumberData")
+    void Number_숫자_같은지에따른_결과_테스트(Number number, Number otherNumber, boolean expect) {
+        assertThat(number.isSameNumber(otherNumber)).isEqualTo(expect);
+    }
+
+    private static Stream<Arguments> createIsSameNumberData() {
+        return Stream.of(
+                Arguments.of(new Number(1), new Number(1),true),
+                Arguments.of(new Number(1), new Number(8),false)
+        );
+    }
+
+
+
 }
