f23c35289eba80621c54122b4a7daaf81e8e409a
chaehuichan <eqholic1125@gmail.com>
2023-10-23 08:00:26 +0900

    feat: ConsoleGameReceiver 생성자 구현

--- src/main/java/baseball/receiver/ConsoleGameReceiver.java
+++ src/main/java/baseball/receiver/ConsoleGameReceiver.java
@@ -1,4 +1,13 @@
 package baseball.receiver;
 
+import java.io.BufferedReader;
+
 public class ConsoleGameReceiver implements GameReceiver {
+
+    private final BufferedReader receiver;
+
+    public ConsoleGameReceiver(BufferedReader receiver) {
+        this.receiver = receiver;
+    }
+
 }
