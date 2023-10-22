90c216c63e273d5657aade78f4b38752b8710a67
준써크 <kjs8960@inha.edu>
2023-10-22 16:10:04 +0900

    feat: 컴퓨터가 선택한 랜덤 수 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,23 +1,36 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) throws IOException {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String userInput;
-        String answer = "123";
+        List<Integer> computer = new ArrayList<>();
 
         System.out.println("숫자 야구 게임을 시작합니다.");
         while(true) {
             while (true) {
+                // 서로 다른 수로 이루어진 3자리 수 랜덤 생성
+                while(computer.size() < 3) {
+                    int randomNumber = Randoms.pickNumberInRange(1, 9);
+                    if(!computer.contains(randomNumber)) {
+                        computer.add(randomNumber);
+                    }
+                }
                 System.out.print("숫자를 입력해주세요 : ");
                 userInput = reader.readLine();
-                System.out.println(userInput);
-                if (userInput.equals(answer)) {
+
+                // TODO 사용자 입력에 따른 결과 판단 코드 작성
+
+                if (true) {
                     System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                     break;
                 }
