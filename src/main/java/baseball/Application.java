22dc44a07d7443239a98fb20285d74f922bd509a
alswp006 <alswp006@gmail.com>
2023-10-22 21:15:03 +0900

    .

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,7 +2,7 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        //게임 시작
+        //게임 시작.
         System.out.println("숫자 야구 게임을 시작합니다.");
         PlayGame game = new PlayGame();
         game.playGame();
