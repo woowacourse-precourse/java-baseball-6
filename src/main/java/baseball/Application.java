20f3516e0dbcd78690335bf75ff470873da48146
soozzang <soozzang@inha.edu>
2023-10-22 17:12:59 +0900

    Refactor: 예외처리 중복코드 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,10 +3,6 @@ import baseball.controller.BaseballController;
 public class Application {
     public static void main(String[] args) {
         BaseballController baseballController = new BaseballController();
-        try {
-            baseballController.game();
-        } catch(IllegalArgumentException e){
-            throw new IllegalArgumentException();
-        }
+        baseballController.game();
     }
 }
