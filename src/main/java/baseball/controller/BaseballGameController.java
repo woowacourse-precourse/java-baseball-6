df124f0a36044d21b7372e5a43c5514abe78c5b7
WooYoungDoo <duden5000@naver.com>
2023-10-23 00:53:55 +0900

    feat(BaseballGameController) : 게임 초기화, 진행, 재시작 기능

---
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -0,0 +1,60 @@
+package baseball.controller;
+
+import baseball.model.GameStatus;
+import baseball.service.BaseballGameService;
+import baseball.service.BaseballGameValidateService;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+public class BaseballGameController {
+
+    private final BaseballGameService baseballGameService = new BaseballGameService();
+    private final BaseballGameValidateService baseballGameValidateService = new BaseballGameValidateService();
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
+    public void playGame() {
+            initGame();
+            printBaseballGameStartMessage();
+            inputNumber();
+            retryOrEnd();
+    }
+
+    private void initGame() {
+        baseballGameService.initGame();
+    }
+
+    private void printBaseballGameStartMessage() {
+        outputView.printBaseballGameStartMessage();
+    }
+
+    private void inputNumber() {
+        while (baseballGameService.getStrikeCount() != 3) {
+            outputView.printInputNumberMessage();
+            String inputNumber = inputView.inputNumber();
+            baseballGameValidateService.validateInputNumber(inputNumber);
+            String countMessage = baseballGameService.resolveBallOrStrikeCount(inputNumber);
+            outputView.printCountMessage(countMessage);
+        }
+    }
+
+    private void retryOrEnd() {
+        inputRetryOrEndCommandProcess();
+        if (baseballGameService.getGameStatus().equals(GameStatus.START)) {
+            retryProcess();
+        }
+    }
+
+    private void retryProcess() {
+        baseballGameService.initGame();
+        inputNumber();
+        retryOrEnd();
+    }
+
+    private void inputRetryOrEndCommandProcess() {
+        outputView.printRetryOrEndMessageProcess();
+        String inputRetryOrEndCommand = inputView.inputRetryOrEndCommand();
+        baseballGameValidateService.validateGameRetryOrEndCommand(inputRetryOrEndCommand);
+        baseballGameService.changeGameStatusByCommand(inputRetryOrEndCommand);
+    }
+}
