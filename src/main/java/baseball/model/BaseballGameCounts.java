db58e714c1ca0f3d0e8e375f50e664e4df31c875
oxdjww <oxdjww@gmail.com>
2023-10-23 02:39:17 +0900

    refactore : define STRIKE = 1, BALL = 0로 순서 바꿈, 정적 팩토리메서드 삭제 후 생성자로 구현

--- src/main/java/baseball/model/BaseballGameCounts.java
+++ src/main/java/baseball/model/BaseballGameCounts.java
@@ -1,56 +1,44 @@
 package baseball.model;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class BaseballGameCounts {
 
-    private static final int STRIKE = 0;
-    private static final int BALL = 1;
+    private static final int BALL = 0;
+    private static final int STRIKE = 1;
 
-    private final List<Integer> counts = new ArrayList<>();
+    private final List<Integer> counts = Arrays.asList(0, 0);
 
-    protected BaseballGameCounts() {
-    }
+    public BaseballGameCounts(ComputerNumber computer, UserNumber user) {
+        for (int idx = 0; idx < user.getUserNumber().size(); idx++) {
+            if (user.getUserNumber().get(idx).equals(computer.getComputerNumber().get(idx))) {
+                counts.set(STRIKE, counts.get(STRIKE) + 1);
+            }
+        }
 
-    public static BaseballGameCounts createBaseballGameCounts(ComputerNumber computer, UserNumber user) {
-
-        BaseballGameCounts baseballGameCounts = new BaseballGameCounts();
         int ballCounts = 0;
         for (Integer idx : user.getUserNumber()) {
             if (computer.getComputerNumber().contains(idx)) {
                 ballCounts++;
             }
         }
-        ballCounts -= baseballGameCounts.getCounts().get(STRIKE);
-        if (ballCounts > 0) {
-            baseballGameCounts.counts.set(BALL, ballCounts);
-        }
-
-        int strickCounts = 0;
-        for (int idx = 0; idx < user.getUserNumber().size(); idx++) {
-            if (user.getUserNumber().get(idx).equals(computer.getComputerNumber().get(idx))) {
-                strickCounts++;
-            }
-        }
-        if (strickCounts > 0) {
-
-            baseballGameCounts.counts.set(STRIKE, strickCounts);
-        }
-        return baseballGameCounts;
+        ballCounts -= counts.get(STRIKE);
+        counts.set(BALL, ballCounts);
     }
 
     public boolean isWinCondition() {
-        return this.counts.get(STRIKE) == 3;
+        return counts.get(STRIKE) == 3;
     }
 
     public void resetCounts() {
+        counts.set(BALL, 0);
         counts.set(STRIKE, 0);
-        counts.set(BALL, 1);
     }
 
     public List<Integer> getCounts() {
-        return this.counts;
+        return counts;
     }
 
 }
