d7f83c7d40754713cf819457b55ac33c3010d3ef
BBOXEEEE <noeyhesx@naver.com>
2023-10-22 13:32:27 +0900

    feat: 1차 기능 구현 완료 (테스트, 리펙토링 필요)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import baseball.controller.Controller;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Controller controller = new Controller();
+        controller.run();
     }
 }
