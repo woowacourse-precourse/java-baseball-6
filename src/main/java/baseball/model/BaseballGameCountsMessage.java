e5d0d45cfe9ab2427be8e11b4e27887016a67b55
oxdjww <oxdjww@gmail.com>
2023-10-23 00:34:45 +0900

    refactor : BaseballGameCounts 일급객체화에 따른 BaseballCountsMessage 구현

--- src/main/java/baseball/model/BaseballGameCountsMessage.java
+++ src/main/java/baseball/model/BaseballGameCountsMessage.java
@@ -5,8 +5,22 @@ import java.util.Objects;
 public class BaseballGameCountsMessage {
     private String message;
 
+    private static final int STRIKE = 0;
+    private static final int BALL = 0;
 
-    public void showCounts() {
+    public BaseballGameCountsMessage(BaseballGameCounts baseballGameCounts) {
+        Integer strikeCounts = baseballGameCounts.getCounts().get(STRIKE);
+        Integer ballCounts = baseballGameCounts.getCounts().get(BALL);
+
+        if (ballCounts > 0) {
+            editMessage(String.format("%d볼 ", ballCounts));
+        }
+        if (strikeCounts > 0) {
+            editMessage(String.format("%d스트라이크", strikeCounts));
+        }
+    }
+
+    public void printMessage() {
         System.out.println(Objects.requireNonNullElse(message, "낫싱"));
     }
 
