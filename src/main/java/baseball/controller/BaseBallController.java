1c973529308377c109efdf91dfef78a8f4cfb0d3
Hyeon0208 <dmsgktn0208@gmail.com>
2023-10-22 23:35:04 +0900

    refactor : 변경된 클래스에 맞춰 코드 수정

--- src/main/java/baseball/controller/BaseBallController.java
+++ src/main/java/baseball/controller/BaseBallController.java
@@ -1,46 +1,45 @@
 package baseball.controller;
 
-import baseball.domain.ComputerNumber;
+import baseball.domain.Computer;
+import baseball.domain.Player;
 import baseball.dto.CountResultDto;
-import baseball.domain.PlayerNumber;
-import baseball.service.ComputerNumberService;
 import baseball.service.CountService;
 import baseball.view.ErrorMessage;
+import baseball.view.GameCommand;
 import baseball.view.GameValue;
 import baseball.view.InputView;
 import baseball.view.GameMessage;
 import baseball.view.OutputView;
 
 public class BaseBallController {
-    private final static String RESTART = "1";
-    private final static String END = "2";
 
     public void startGame() {
         System.out.println(GameMessage.START_GAME.getMessage());
-        ComputerNumber computerNumber = ComputerNumberService.generateComputerNumber();
-        runGame(computerNumber);
+        Computer computer = new Computer();
+        runGame(computer);
     }
 
     public boolean restartGame() {
         System.out.println(GameMessage.ASK_RETRY.getMessage());
         String command = InputView.inputRetryCommand();
-        if (command.equals(RESTART)) {
+        if (command.equals(GameCommand.RESTART.getString())) {
             return true;
         }
-        if (command.equals(END)) {
+        if (command.equals(GameCommand.END.getString())) {
             System.out.println(GameMessage.END_GAME.getMessage());
             return false;
         }
         throw new IllegalArgumentException(ErrorMessage.RESTART_COMMAND.getMessage());
     }
 
-    private void runGame(ComputerNumber computerNumber) {
+    private void runGame(Computer computer) {
         boolean gameStatus = true;
         while (gameStatus) {
             System.out.print(GameMessage.INPUT_NUMBER.getMessage());
-            PlayerNumber playerNumber = InputView.inputPlayerNumber();
-            CountResultDto countResultDto = CountService.calculateCount(computerNumber.getNumber(),
-                    playerNumber.getNumber());
+            String input = InputView.inputPlayerNumber();
+            Player player = new Player(input);
+            CountResultDto countResultDto = CountService.calculateCount(computer.getNumbers(),
+                    player.getNumbers());
             System.out.println(OutputView.printResult(countResultDto));
             gameStatus = isWinGame(countResultDto.getStrikeCount());
         }
