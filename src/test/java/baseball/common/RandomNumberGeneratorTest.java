c6f5b852505e0ebb159088a5bf59bc1dcd89dcc9
킹받드라슈 <soccer_by@naver.com>
2023-10-22 20:44:33 +0900

    test(unit): 고유한 3자리 난수 생성하기

---
+++ src/test/java/baseball/common/RandomNumberGeneratorTest.java
@@ -0,0 +1,25 @@
+package baseball.common;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+
+class RandomNumberGeneratorTest {
+
+    static final int DIGIT = 3;
+    static final int REPEAT = 3;
+    RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+
+    @DisplayName("3자리 고유한 숫자를 생성한다.")
+    @RepeatedTest(REPEAT)
+    void createNumber() {
+        List<Integer> createdNumbers = randomNumberGenerator.createNumber(DIGIT);
+        Set<Integer> uniqueNumbers = new HashSet<>(createdNumbers);
+
+        assertThat(uniqueNumbers).hasSize(DIGIT);
+    }
+}
