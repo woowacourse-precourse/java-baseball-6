1f7564ee1707f6327f292407c5550dbd73b34d7e
hyunjin <guswlsdl0121@gmail.com>
2023-10-22 22:21:00 +0900

    Test: ballStrikeCount에 대한 Test코드 작성

---
+++ src/test/java/baseball/BallStrikeCountTest.java
@@ -0,0 +1,31 @@
+package baseball;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BallStrikeCountTest {
+
+    private BallStrikeCount ballStrikeCount;
+
+    @BeforeEach
+    public void setUp() {
+        ballStrikeCount = new BallStrikeCount();
+    }
+
+    @Test
+    @DisplayName("볼 카운트 증가 테스트")
+    public void testIncreaseBall() {
+        ballStrikeCount.increaseBall();
+        assertEquals(1, ballStrikeCount.getBallCount());
+    }
+
+    @Test
+    @DisplayName("스트라이크 카운트 증가 테스트")
+    public void testIncreaseStrike() {
+        ballStrikeCount.increaseStrike();
+        assertEquals(1, ballStrikeCount.getStrikeCount());
+    }
+}
