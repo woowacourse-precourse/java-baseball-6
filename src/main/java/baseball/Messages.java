ee1f3404f1eb4bc59b56951a161baba951ec52ff
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-10-22 11:26:30 +0900

    feat: add a message to repeat the entire game

--- src/main/java/baseball/Messages.java
+++ src/main/java/baseball/Messages.java
@@ -4,6 +4,7 @@ public class Messages {
     private String introMessage = "숫자 야구 게임을 시작합니다.";
     private String inputMessage = "숫자를 입력해주세요 : ";;
     private String outroMessage = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private String willRestartMessage = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private String sameDigitMessage = "스트라이크";
     private String sameNumberMessage = "볼";
     private String wrongMessage = "낫싱";
@@ -32,6 +33,10 @@ public class Messages {
         return wrongMessage;
     }
 
+    public String getWillRestartMessage() {
+        return willRestartMessage;
+    }
+
     public void setIntroMessage(String introMessage) {
         this.introMessage = introMessage;
     }
@@ -54,5 +59,9 @@ public class Messages {
 
     public void setWrongMessage(String wrongMessage) {
         this.wrongMessage = wrongMessage;
+    }
+
+    public void setWillRestartMessage(String willRestartMessage) {
+        this.willRestartMessage = willRestartMessage;
     }
 }
