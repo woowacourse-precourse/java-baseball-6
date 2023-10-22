50729891ac1cf43a269d722707043d9b16906930
benjaminuj <chosj1526@gmail.com>
2023-10-22 22:13:22 +0900

    feat(Application): 컴퓨터의 숫자 3개 선택 기능 구현
    - camp.nextstep.edu.missionutils.Randoms의 pickNumberInRange()를 활용해 1이상 9이하 정수 랜덤값 추출
    - 서로 다른 숫자 3개로 구성되도록, 추가전 중복값 검사 진행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,13 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    static List<Integer> computerNumbers = new ArrayList<>();
     public static void main(String[] args) {
         Application application = new Application();
-        application.printGameStartText();
 
+        application.printGameStartText();
+        application.setComputerNumber();
     }
 
     public void printGameStartText() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
+    public void setComputerNumber() {
+        while (computerNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumbers.contains(randomNumber)) {
+                computerNumbers.add(randomNumber);
+            }
+        }
+    }
+
 }
