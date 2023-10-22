5b83e28af0fd1f7d6890806a78152fa125a06f2c
oxdjww <oxdjww@gmail.com>
2023-10-23 02:40:48 +0900

    refactor : controller 패키지 추가에 따른 import 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,7 +2,7 @@ package baseball;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
-import baseball.model.BaseballGame;
+import baseball.controller.BaseballGame;
 
 public class Application {
     public static void main(String[] args) {
