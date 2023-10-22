b1d0ec9e6947754b0e5357acbb8ffc31012ca1c0
jinsu4755 <parkjinsu4755@gmail.com>
2023-10-22 16:17:59 +0900

    feat: 콘솔 입력을 받을 객체 생성

---
+++ src/main/java/baseball/view/InputConsole.java
@@ -0,0 +1,10 @@
+package baseball.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputConsole {
+
+    public static String readConsole() {
+        return readLine();
+    }
+}
