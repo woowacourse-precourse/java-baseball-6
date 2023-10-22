73221c6e522ec1d8e67510f911c0f5f05c4e46c2
wook <dong930112@gmail.com>
2023-10-22 22:58:41 +0900

    refactor(NumberGenerator): 클래스 이름을 'NumberController'로 변경 숫자만 생성뿐 아니라 숫자와 관련된 다양한 기능을 포함하기 위해 이름 변경

--- src/main/java/baseball/NumberController.java
+++ src/main/java/baseball/NumberController.java
@@ -6,9 +6,9 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class NumberGenerator {
+public class NumberController {
 
-  public List generate() {
+  public List<Integer> generate() {
     List<Integer> numList = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9));
     Collections.shuffle(numList);
 
