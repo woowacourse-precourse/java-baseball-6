180acaf1885bc9fa750c5a782801603dad50aaa8
tam31 <wxogud3377@naver.com>
2023-10-22 16:21:12 +0900

    Refactor(PlayerValidation): 에러메시지 정의

--- src/main/java/baseball/validation/PlayerValidation.java
+++ src/main/java/baseball/validation/PlayerValidation.java
@@ -5,6 +5,12 @@ import java.util.List;
 
 public class PlayerValidation {
 
+    private static final String NUMBER_ERROR_MESSAGE = "숫자가 아닙니다";
+    private static final String SIZE_ERROR_MESSAGE = "자리 수가 아닙니다";
+    private static final String DUPLICATION_ERROR_MESSAGE = "중복된 값입니다";
+    private static final String IS_NOT_INPUT_ERROR_MESSAGE = "잘못된 입력 방식입니다";
+
+
     public static List<Integer> checkNumber(String numbers){
         isCheckNumber(numbers);
         isCheckSize(numbers,3);
@@ -14,14 +20,14 @@ public class PlayerValidation {
     public static void isCheckNumber(String numbers) {
         for(int i=0; i<numbers.length(); i++){
             if(!Character.isDigit(numbers.charAt(i))){
-                throw new IllegalArgumentException("숫자가 아닙니다");
+                throw new IllegalArgumentException(NUMBER_ERROR_MESSAGE);
             }
         }
     }
 
     public static void isCheckSize(String numbers, int size) {
         if(numbers.length() != size){
-            throw new IllegalArgumentException(size+"자리 수가 아닙니다");
+            throw new IllegalArgumentException(size+SIZE_ERROR_MESSAGE);
         }
     }
 
@@ -29,7 +35,7 @@ public class PlayerValidation {
         List<Integer> number = new ArrayList<>();
         for(int i=0; i<numbers.length(); i++){
             if(number.contains((numbers.charAt(i)-'0'))){
-                throw new IllegalArgumentException("중복된 값입니다");
+                throw new IllegalArgumentException(DUPLICATION_ERROR_MESSAGE);
             }
             number.add(numbers.charAt(i)-'0');
         }
@@ -45,7 +51,7 @@ public class PlayerValidation {
 
     private static void ischeckOneOrTwo(String number) {
         if(!(number.equals("1") || number.equals("2"))){
-            throw new IllegalArgumentException("잘못된 입력 방식입니다");
+            throw new IllegalArgumentException(IS_NOT_INPUT_ERROR_MESSAGE);
         }
     }
 }
