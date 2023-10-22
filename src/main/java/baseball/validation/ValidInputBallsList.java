bdbe84a7cbdcf60532bd28ba6cda797385010b8e
JangHongJoon <wkdghdwns19969@gmail.com>
2023-10-23 03:48:02 +0900

    Refactor : 입력받은 값에 대한 검증 package vaildation 에 옮김

--- src/main/java/baseball/validation/ValidInputBallsList.java
+++ src/main/java/baseball/validation/ValidInputBallsList.java
@@ -1,4 +1,4 @@
-package baseball;
+package baseball.validation;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,7 +9,7 @@ public class ValidInputBallsList {
     public List<Integer> createInputBallsList() {
         List<Integer> validBallsList = new ArrayList<>();
         String numbersString = readLine();
-        String[] numberStringArray = numbersString.split(" ");
+        String[] numberStringArray = numbersString.split("");
         for(String numberString : numberStringArray){
             validBallsList.add(Integer.parseInt(numberString));
         }
--- src/main/java/baseball/validation/ValidRandomBallsList.java
+++ src/main/java/baseball/validation/ValidRandomBallsList.java
@@ -1,6 +1,5 @@
-package baseball;
+package baseball.validation;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 public class ValidRandomBallsList {
