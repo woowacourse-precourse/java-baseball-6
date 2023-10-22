40cd09b8dee8134e6bde859bcc0167e542c1cc68
Maljaaa <Maljaaa@github.com>
2023-10-22 18:14:50 +0900

    feat: 숫자 야구 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,27 @@
 package baseball;
 
 
-import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
-import static camp.nextstep.edu.missionutils.Console.readLine;
+import baseball.controller.GameController;
+import baseball.model.Computer;
+import baseball.model.GameManager;
+import baseball.model.User;
+import baseball.service.MatchService;
+import baseball.utils.RandomNumberGenerator;
+import baseball.view.Input;
+import baseball.view.Print;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
+        Computer computer = new Computer(new RandomNumberGenerator());
+        User user = new User();
+        GameManager gameManager = new GameManager();
+        Input input = new Input();
+        Print print = new Print();
+        MatchService matchService = new MatchService();
+        GameController gameController = new GameController(computer, user, gameManager, input, print, matchService);
+
+        gameController.run();
     }
 }
