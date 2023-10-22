3667f287da4ba836071ca7ff397a0a6ecbcf3c5c
mayy24th <86235986+mayy24th@users.noreply.github.com>
2023-10-22 18:00:51 +0900

    feat: 다시 시작 입력 검사 기능 구현

---
+++ src/main/java/baseball/Error/CheckRestartNumber.java
@@ -0,0 +1,21 @@
+package baseball.Error;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CheckRestartNumber {
+
+    public static void checkLength(String input){
+        if(input.length() != 1){
+            throw new IllegalArgumentException("잘못된 길이의 값이 입력되었습니다.");
+        }
+    }
+
+    public static void checkNumber(String input){
+        char inputChar = input.charAt(0);
+        if(!('1' <= inputChar && inputChar <= '2')){
+            throw new IllegalArgumentException("숫자 외의 다른 값이 입력되었습니다.");
+        }
+    }
+}
+
