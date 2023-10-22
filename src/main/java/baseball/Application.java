988c7e1384b361eb77d3743e9fd4ef0c1bc0d28d
bananananaa <crown9924@gmail.com>
2023-10-22 17:33:34 +0900

    최종 완성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,7 @@ package baseball;
 
 public class Application {
     public static void main(String[] args){
+
         System.out.println("숫자 야구 게임을 시작합니다.");
 
         Baseball baseball = new Baseball();
