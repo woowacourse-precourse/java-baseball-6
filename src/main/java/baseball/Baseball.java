2a0c78b0a632d5fab8ee429954a30cf6854d5b39
bbbang105 <2018111366@dgu.ac.kr>
2023-10-23 02:22:56 +0900

    Feat : showResult 메소드 구현 스트라이크, 볼 개수에 따라 결과를 출력해주는 메소드

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -37,4 +37,19 @@ public class Baseball {
 
         return strikeBallNums;
     }
+
+    public void showResult(int strike, int ball) {
+        if (strike > 0 && ball > 0) {
+            System.out.println(String.format("%d볼 %d스트라이크", ball, strike));
+        }
+        else if (strike > 0 && ball == 0) {
+            System.out.println(String.format("%d스트라이크", strike));
+        }
+        else if (strike == 0 && ball > 0) {
+            System.out.println(String.format("%d볼", ball));
+        }
+        else if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+        }
+    }
 }
