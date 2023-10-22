a5c095024d3f50f00c0a85f0dfbd2620d519a16a
eunjin3481 <eunjin3481@gmail.com>
2023-10-22 21:42:17 +0900

    Feat: Exception class 구조

---
+++ src/main/java/baseball/Exception.java
@@ -0,0 +1,26 @@
+package baseball;
+
+public class Exception {
+
+    public void checkOneOrTwo(){
+
+    }
+
+    public void checkUserInput(){
+        checkNumbers();
+        checkDigits();
+        checkDuplicateNumbers();
+    }
+
+    public void checkNumbers(){
+
+    }
+
+    public void  checkDigits(){
+
+    }
+
+    public void checkDuplicateNumbers(){
+
+    }
+}
