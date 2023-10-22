5535c832080668a8880daaf11a2848ad7f77bed8
soyoungxxx <otherplet@gmail.com>
2023-10-23 05:15:02 +0900

    feat(GameControl): 게임 시작과 종료 조건 작성

--- src/main/java/baseball/game/GameControl.java
+++ src/main/java/baseball/game/GameControl.java
@@ -1,6 +1,7 @@
 package baseball.game;
 
 import baseball.print.PrintMessage;
+import camp.nextstep.edu.missionutils.Console;
 
 public class GameControl {
     boolean gameState = false;
@@ -13,6 +14,22 @@ public class GameControl {
 
         while(gameState) {
             gameState = newGame.playGame();
+            isGameOver();
+        }
+    }
+
+    private void isGameOver() {
+        if (!gameState) {
+            printer.printEndMessage();
+            String str = Console.readLine();
+            int n = Integer.parseInt(str);
+            decideGame(n);
+        }
+    }
+
+    private void decideGame(int n) {
+        if (n == 1) {
+            gameState = true;
         }
     }
 }
