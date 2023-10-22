cddc081d08e55d10b7535170e25a90bc759fe86b
Nara Park <skfk6352@naver.com>
2023-10-23 02:27:34 +0900

    <feat>: create random number method

---
+++ src/test/java/baseball/util/ComputerUtilTest.java
@@ -0,0 +1,17 @@
+package baseball.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ComputerUtilTest {
+
+    @Test
+    void 컴퓨터_숫자_생성() {
+        List<Integer> computerNumber = ComputerUtil.generateComputerNumber();
+        System.out.println(computerNumber);
+    }
+}
