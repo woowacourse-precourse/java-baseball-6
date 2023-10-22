91424098599082cf919882a83eed594c3a6c310a
Jisu Lee <wltn12047323@gmail.com>
2023-10-22 18:42:37 +0900

    사용자 값 입력받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.sql.Array;
@@ -11,10 +12,13 @@ public class Application {
         // TODO: 프로그램 구현
         List<Integer> computer = new ArrayList<>();
         List<Integer> user = new ArrayList<>();
+        String input;
         int i, j;
         int n;
 
-        //1. 랜덤한 3개의 수 생성하기
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        //1. 랜덤한 3개의 수 생성 하기
         while(computer.size() < 3){
             n = Randoms.pickNumberInRange(1, 9);
             if(!computer.contains(n))
@@ -22,6 +26,14 @@ public class Application {
         }
         //System.out.println(computer);
 
-        //2. 사용자 값 입력받기
+        //2. 사용자 값 입력 받기
+        while(!computer.equals(user)) {
+            System.out.println("숫자를 입력해주세요 : ");
+            input = Console.readLine();
+            for(i = 0; i < 3; i++)
+                user.add((int) input.charAt(i));
+
+
+        }
     }
 }
