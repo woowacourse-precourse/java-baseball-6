4fe3846146d0844903a061075c823c72e500582a
dlrjs2360 <lhg3670@naver.com>
2023-10-22 20:49:42 +0900

    :sparkles: feat: 랜덤 정답 생성 기능 개발
    - Randoms의 `pickNumberInRange()`메서드를 통해 랜덤한 중복되지 않는 랜덤 숫자 3개를 생성합니다.
    - 무한루프에 대한 고민을 정리하여 README.md에 작성했습니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,11 +3,23 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // 게임 시작
         System.out.println("숫자 야구 게임을 시작합니다.");
+
+        // 정답 생성
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) computer.add(randomNumber);
+        }
+
+
+
+
     }
 }
