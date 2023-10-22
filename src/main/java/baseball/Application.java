75e102ae9cd5489cd7a4d683f3b331e0d0be014f
Uniaut <kunwoo0927@gmail.com>
2023-10-23 00:48:19 +0900

    refactor: separate generation of randomAnswer, changed variable name randomList to randomAnswer

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,6 +6,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+    static List<Integer> generateRandomAnswer() {
+        List<Integer> randomAnswer = new ArrayList<>();
+        while (randomAnswer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!randomAnswer.contains(randomNumber)) {
+                randomAnswer.add(randomNumber);
+            }
+        }
+        return randomAnswer;
+    }
+
     static boolean askRestart() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String inputString = Console.readLine();
@@ -16,20 +27,13 @@ public class Application {
 
         throw new IllegalArgumentException("Input must be 1 or 2");
     }
+
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
         while (true) {
-            // Create random answer
-            List<Integer> randomList = new ArrayList<>();
-            while (randomList.size() < 3) {
-                int randomNumber = Randoms.pickNumberInRange(1, 9);
-                if (!randomList.contains(randomNumber)) {
-                    randomList.add(randomNumber);
-                }
-            }
+            List<Integer> randomAnswer = generateRandomAnswer();
 
-            // Run game
-            Game game = new Game(randomList);
+            Game game = new Game(randomAnswer);
             game.run();
 
             // Ask and restart or break.
