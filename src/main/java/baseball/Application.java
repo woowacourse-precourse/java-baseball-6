0e8a7529c0fdc7779a9d015af3fcf4e0dea2edc8
IronD414 <kcy1760@gmail.com>
2023-10-22 17:08:29 +0900

    Add baseballStartingMessage

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,10 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO: 숫자야구 시작 메시지 출력
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        // TODO: computer 의 임의 숫자 생성
+
+        // TODO: while 로 exception 또는 게임 종료시까지 guess 진행
     }
 }
