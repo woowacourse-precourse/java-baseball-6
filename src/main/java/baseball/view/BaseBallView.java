19b2e73a81ca9f16c2c14e3ae1c4531a65c2d462
Arachneee <jhg2819@naver.com>
2023-10-22 11:26:31 +0900

    refactor: ConsolePrint 제거

--- src/main/java/baseball/view/BaseBallView.java
+++ src/main/java/baseball/view/BaseBallView.java
@@ -5,6 +5,10 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class BaseBallView {
 
+    private static final String START_STRING = "숫자 야구 게임을 시작합니다.";
+    private static final String NUMBER_INPUT_STRING = "숫자를 입력해주세요 : ";
+    private static final String FINISH_INPUT_STRING = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+            "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     private BaseBallView() {
     }
@@ -14,22 +18,22 @@ public class BaseBallView {
     }
 
     public void startGameView() {
-        ConsolePrint.printStart();
+        System.out.println(START_STRING);
     }
 
     public void resultGameView(String result) {
-        ConsolePrint.printResult(result);
+        System.out.println(result);
     }
 
 
     public String numbersInputView() {
-        ConsolePrint.printNumberInput();
+        System.out.print(NUMBER_INPUT_STRING);
 
         return readLine();
     }
 
     public String continueInputView() {
-        ConsolePrint.printFinish();
+        System.out.println(FINISH_INPUT_STRING);
 
         return readLine();
     }
