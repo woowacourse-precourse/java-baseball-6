f4b51172788aea0039b550c35a3d95ce34074339
aammddkkzxc <97333563+aammddkkzxc@users.noreply.github.com>
2023-10-22 19:15:18 +0900

    Application 리팩토링

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,9 +3,7 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        GameState gameState = new GameState(Output.gameStart());
-        GameNumberController gameNumberController = new GameNumberController();
-        GamePlayer gamePlayer = new GamePlayer(gameState, gameNumberController);
+        GamePlayer gamePlayer = new GamePlayer(new GameState(Output.gameStart()), new GameNumberController());
         gamePlayer.playGame();
     }
 }
