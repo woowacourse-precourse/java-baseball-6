d967a23dbf53c557c335425d37c328e37a40b32a
Seyeon <wjdtpdus828@naver.com>
2023-10-22 23:24:24 +0900

    feat(View): startMassage와 inputText 메소드 구현

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -3,5 +3,14 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class View{
+    private static final String START_SENTENCE = "숫자 야구 게임을 시작합니다.";
+    private static final String INPUT_SENTENCE = "숫자를 입력해주세요 : ";
+
+    public void startMassage(){
+        System.out.println(START_SENTENCE);
+    }
+    public void inputText(){
+        System.out.print(INPUT_SENTENCE);
+    }
 
 }
