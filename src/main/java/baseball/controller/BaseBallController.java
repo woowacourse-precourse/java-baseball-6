38194b1893f18174bd21137b3b6b7900a274f20c
Relaxed-Mind <dlwlstn0709@uos.ac.kr>
2023-10-22 11:33:50 +0900

    Feat: Receive User Input 유저에게 숫자 입력받는 기능 추가 User Input #1

--- src/main/java/baseball/controller/BaseBallController.java
+++ src/main/java/baseball/controller/BaseBallController.java
@@ -12,7 +12,6 @@ public class BaseBallController {
         System.out.println("숫자 야구 게임을 시작합니다.");
         List<Integer> computerNum = systemService.generateComputerNum();
         baseBallService.init(computerNum);
-        
-
+        baseBallService.start();
     }
 }
--- src/main/java/baseball/service/BaseBallService.java
+++ src/main/java/baseball/service/BaseBallService.java
@@ -1,13 +1,47 @@
 package baseball.service;
 
+import baseball.validation.InputValidator;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BaseBallService {
     private static List<Integer> computerNum;
+    private static List<Integer> userNum;
     private static int strike = 0;
     private static int ball = 0;
+
+    private static InputValidator inputValidator = new InputValidator();
 
     public void init(List<Integer> generatedComputerNum) {
         computerNum = generatedComputerNum;
     }
+
+    public void start() {
+        boolean restart = true;
+
+        while (restart) {
+            receiveUserInput();
+        }
+
+    }
+
+    public void checkValid(String strUserNum) {
+        if (!inputValidator.isValidLength(strUserNum) || !inputValidator.isValidNumber(strUserNum)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void receiveUserInput() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String strUserNum = Console.readLine();
+        checkValid(strUserNum);
+
+        String[] strArrUserNum = strUserNum.split("");
+        userNum = new ArrayList<>();
+        for (int i = 0; i < strArrUserNum.length; i++) {
+            userNum.add(Integer.parseInt(strArrUserNum[i]));
+        }
+    }
+
 }
