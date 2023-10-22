b27f9e2da7da21e99f2caf0f59c61edd86ae1e2c
jinchiim <nellroll@syuin.ac.kr>
2023-10-23 05:00:22 +0900

    feature : Application 실행을 위한 코드 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,20 @@
 package baseball;
 
+import baseball.domain.computer.Computer;
+import baseball.domain.game.Game;
+import baseball.domain.input.Input;
+import baseball.domain.output.Output;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        Output output = new Output();
+        Computer computer = new Computer();
+        Input input = new Input(computer);
+        Game game = new Game(input, computer, output);
+
+        output.printStartGameMessage();
+        game.startGame();
+
     }
 }
