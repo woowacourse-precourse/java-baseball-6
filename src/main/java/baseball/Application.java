5cff8dff096f0d6b9a96a644ccc019a6e28af2ad
woo <leindey@gmail.com>
2023-10-22 16:44:09 +0900

    feat: 입력방법 Scanner -> Console로 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,17 +1,14 @@
 package baseball;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Scanner scanner = new Scanner(System.in);
 
         // 게임 시작
         System.out.println("숫자 야구 게임을 시작합니다.");
         System.out.print("숫자를 입력해주세요 : ");
-        int num = Integer.parseInt(scanner.nextLine());
-
-
+        int num = Integer.parseInt(Console.readLine());
     }
 }
