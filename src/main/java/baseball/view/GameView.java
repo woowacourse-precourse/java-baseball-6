5d9c2ccf047e4c784b2cdc326b48093c884bd1d2
mksystem98 <mksystem98@gmail.com>
2023-10-23 00:53:47 +0900

    feat : requestNumberInput 메서드 생성

--- src/main/java/baseball/view/GameView.java
+++ src/main/java/baseball/view/GameView.java
@@ -5,4 +5,8 @@ public class GameView {
     public void startGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
+    public void requestNumberInput(){
+        System.out.println("숫자를 입력해주세요 : ");
+    }
 }
