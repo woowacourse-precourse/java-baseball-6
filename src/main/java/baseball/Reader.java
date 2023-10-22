50ac31e34f8e045d9065403008ac3103d081f685
Moly <102847513+jminkkk@users.noreply.github.com>
2023-10-22 20:30:09 +0900

    feat: 입력을 처리하는 Reader 클래스 생성

---
+++ src/main/java/baseball/Reader.java
@@ -0,0 +1,14 @@
+package baseball;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class Reader {
+    public static int readNumber() {
+        return Integer.parseInt(readLine());
+    }
+
+    public static String readString() {
+        return readLine();
+    }
+
+}
