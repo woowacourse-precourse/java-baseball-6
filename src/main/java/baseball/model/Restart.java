0c29de846b2bed21c3a70cf0b9d3c6eff8804a75
junsoo <choix712@umn.edu>
2023-10-22 18:13:24 +0900

    feat: 재시작 유저 메세지가 1이나 2가 아닐때 유효성 검사

--- src/main/java/baseball/model/Restart.java
+++ src/main/java/baseball/model/Restart.java
@@ -7,12 +7,19 @@ public class Restart {
 
     public Restart(String input) {
         validateBlank(input);
+        validateOptions(input);
         this.input = input;
     }
 
-    private static void validateBlank(String input) {
+    private void validateBlank(String input) {
         if (input.length() != RESTART_MESSAGE_SIZE) {
             throw new IllegalArgumentException("입력값이 공백일 수 없습니다.");
+        }
+    }
+
+    private void validateOptions(String input) {
+        if (!input.equals("1") && !input.equals("2")) {
+            throw new IllegalArgumentException("플레이어는 1이나 2 이외의 값을 입력할 수 없습니다.");
         }
     }
 }
