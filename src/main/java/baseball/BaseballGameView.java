34d3b7b68587126786197a62ca69b3d5ca3690c8
dgh06175 <dgh06175@gmail.com>
2023-10-22 17:44:16 +0900

    refactor: 추가한 상수 전체 코드에 적용

--- src/main/java/baseball/BaseballGameView.java
+++ src/main/java/baseball/BaseballGameView.java
@@ -1,12 +1,16 @@
 package baseball;
 
+import static baseball.constant.GameConstants.MAX_NUMBER_LENGTH;
+
+import baseball.constant.GameStatus;
+
 public class BaseballGameView {
     static void showStartMessage() {
-        System.out.println("숫자 야구 게임을 시작합니다.\n");
+        System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
     static void showWinMessage() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println(MAX_NUMBER_LENGTH + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
     public static void printLoopResult(int ball, int strike) {
@@ -30,6 +34,7 @@ public class BaseballGameView {
     }
 
     static void showGameContinueInputMessage() {
-        System.out.print("숫자를 입력해주세요 : ");
+        System.out.println("게임을 새로 시작하려면 " + GameStatus.CONTINUE.getStatus() + ", 종료하려면 " + GameStatus.STOP.getStatus()
+                + "를 입력하세요.");
     }
 }
--- src/main/java/baseball/constant/GameConstants.java
+++ src/main/java/baseball/constant/GameConstants.java
@@ -2,11 +2,12 @@ package baseball.constant;
 
 public final class GameConstants {
     public static final String ERROR_INVALID_INPUT = "[ERROR] 잘못된 숫자 입력입니다: ";
-    public static final String ERROR_INVALID_CONTINUE_INPUT = "[ERROR] 1 또는 2 를 입력하세요.";
-    public static final String INPUT_REGEX = "^[1-9]{3}$";
+    public static final String ERROR_INVALID_CONTINUE_INPUT =
+            "[ERROR] " + GameStatus.CONTINUE.getStatus() + " 또는 " + GameStatus.STOP.getStatus() + " 를 입력하세요.";
     public static final int MIN_NUMBER = 1;
     public static final int MAX_NUMBER = 9;
     public static final int MAX_NUMBER_LENGTH = 3;
+    public static final String INPUT_REGEX = "^[" + MIN_NUMBER + "-" + MAX_NUMBER + "]{" + MAX_NUMBER_LENGTH + "}$";
 
     private GameConstants() {
     }
