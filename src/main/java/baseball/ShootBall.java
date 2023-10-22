61eaefb731211ce88164e26e9eff896ae69aa328
서창덕 <44630705+Garodden@users.noreply.github.com>
2023-10-22 22:59:11 +0900

    ShootBall 클래스에서(4)번 기능 구현 23.10.22 -서창덕

--- src/main/java/baseball/ShootBall.java
+++ src/main/java/baseball/ShootBall.java
@@ -10,7 +10,7 @@ public class ShootBall {
         numbers = new int[10];
     }
 
-    //TODO:(2)번 구현 -23.10.22
+    //TODO:(2)번 기능 구현 -23.10.22
     private void randomNumGenerator(){
         int randNum;
         for(int i=1; i<=3;i++) {
@@ -19,6 +19,28 @@ public class ShootBall {
         }
     }
 
-    //TODO:(3)번 구현 -23.10.22
+    //TODO:(3)번 기능 구현 -23.10.22
 
+    //TODO:(4)번 기능 구현 -23.10.22
+    private void validNumInspect(String input){
+        char tmp;
+        //3개보다 많은 문자가 입력됐을 시
+        if(input.length() != 3)
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+
+        for(int i =0;i<3;i++){
+            tmp = input.charAt(i);
+
+            //정수형 외의 문자가 입력됐을 때
+            if( tmp>'9'||tmp<'0')
+                throw new IllegalArgumentException("잘못된 입력입니다.");
+
+            //중복된 입력이 있을 때
+            for(int j=i+1; j<3;j++){
+                if(tmp==input.charAt(j))
+                    throw new IllegalArgumentException("잘못된 입력입니다.");
+            }
+        }
+
+    }
 }
