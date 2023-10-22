7c8d62b5226e91f7d7a1946f47189199a12ce571
cousim46 <wjdghldns24@naver.com>
2023-10-22 16:01:39 +0900

    Add : 프로그램 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import baseball.ballcount.BaseballGameProcess;
+import baseball.game.Game;
+import baseball.game.GameProcess;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Game baseBallGame = new BaseBallGame();
+        GameProcess baseBallGameProcess = new BaseballGameProcess();
+        baseBallGame.execute(baseBallGameProcess);
     }
 }
