e762f3a538e1be46d1ce0d00e1a9a4bb977eacf0
hoyun06 <hoyun06@gmail.com>
2023-10-22 13:41:06 +0900

    feat: Computer 클래스 내부에 doesExist 초기화 하는 메소드 및 hiddenNumber 재생성하는 메소드 추가

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -10,20 +10,36 @@ public class Computer {
     private final boolean[] doesExist = new boolean[10];
 
     public Computer() {
-        while (hiddenNumber.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            checkDuplicateAndAddToList(randomNumber);
-        }
+        initializeHiddenNumber();
     }
 
     public List<Integer> getHiddenNumber() {
         return hiddenNumber;
     }
 
+    public void resetForNewGame() {
+        resetDoesExist();
+        hiddenNumber.clear();
+        initializeHiddenNumber();
+    }
+
     private void checkDuplicateAndAddToList(int randomNumber) {
         if (!doesExist[randomNumber]) {
             hiddenNumber.add(randomNumber);
             doesExist[randomNumber] = true;
+        }
+    }
+
+    private void initializeHiddenNumber() {
+        while (hiddenNumber.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            checkDuplicateAndAddToList(randomNumber);
+        }
+    }
+
+    private void resetDoesExist() {
+        for (int i = 0; i < 10; i++) {
+            doesExist[i] = false;
         }
     }
 }
