50e0edaed1f800e98742cf88e277f2fb994a2703
PINGPINGYEE <owb7994@nate.com>
2023-10-22 23:33:26 +0900

    2

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,54 +3,67 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.Arrays;
-import java.util.Scanner;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
+    public static int[] randomValue() {
+        int[] numberOfAnswer = new int[3]; // 컴퓨터의 정답 출력
+        for (int i = 0; i < numberOfAnswer.length; i++) {
+            numberOfAnswer[i] = Randoms.pickNumberInRange(1, 9);
+            for (int j = 0; j < i; j++) if (numberOfAnswer[i] == numberOfAnswer[j]) i--;
+        }
+        return numberOfAnswer;
+    }
+
+    public static int[] userInput() {
+        int[] userInput = new int[3]; // 사용자의 정답 입력
+        String tempUserInput = ""; // 사용자 입력을 String으로 나눈 후 int형으로 바꾸기 위해 임시 저장
+        // System.out.println(Arrays.toString(answerArray));
+
+
+        // 사용자 input
+        tempUserInput = readLine();
+        if (tempUserInput.length() != 3) {
+            throw new IllegalArgumentException("올바르지 않은 입력입니다. 3자리 숫자를 입력하세요.");
+        }
+
+
+        for (int l = 0; l < tempUserInput.length(); l++) {
+            if (tempUserInput.charAt(l) < '1' || tempUserInput.charAt(l) > '9') {
+                throw new IllegalArgumentException("올바르지 않은 입력입니다.숫자를 입력하세요.");
+            }
+            userInput[l] = (tempUserInput.charAt(l) - '0');
+
+        }
+
+
+        for (int l = 0; l < tempUserInput.length(); l++) {
+            userInput[l] = (tempUserInput.charAt(l) - '0');
+        }
+        return userInput;
+    }
+    
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구를 시작합니다."); // 게임 시작 문구
-        int[] numberOfAnswer = new int[3]; // 컴퓨터의 정답 출력
-        int[] userInput = new int[3]; // 사용자의 정답 입력
-        String tempUserInput = ""; // 사용자 입력을 String으로 나눈 후 int형으로 바꾸기 위해 임시 저장
+
+
         int[] answerArray = {0, 0, 0}; // Strike, Ball, Out 개수 체크
-        String gameEndUserChoice = "-1"; // 정답 맞춘 후 게임 종료 체크
+        String gameEndUserChoice; // 정답 맞춘 후 게임 종료 체크
 
         while (true) {
             //if (countGame > 1) scanner.nextLine();
             // 숫자 생성
-            for (int i = 0; i < numberOfAnswer.length; i++) {
-                numberOfAnswer[i] = Randoms.pickNumberInRange(1, 9);
-                for (int j = 0; j < i; j++) if (numberOfAnswer[i] == numberOfAnswer[j]) i--;
-            }
-            System.out.println(Arrays.toString(numberOfAnswer));
+            int []numberOfAnswer = randomValue();
+            // System.out.println(Arrays.toString(numberOfAnswer));
 
             while (true) {
-
-                // System.out.println(Arrays.toString(answerArray));
                 Arrays.fill(answerArray, 0);
 
-                // 사용자 input
-                tempUserInput = readLine();
-                try {
-                    if (tempUserInput.length() > 3) {
-                        throw new IllegalArgumentException("Out of Index");
-                    }
-                    for (int l = 0; l < tempUserInput.length(); l++) {
-                        if ((int) (tempUserInput.charAt(l) - '0') < 0
-                                || (int) (tempUserInput.charAt(l) - '0') > 9) {
-                            throw new IllegalArgumentException("Wrong Number");
-                        }
-                        userInput[l] = (int) (tempUserInput.charAt(l) - '0');
-                    }
-                } catch (IllegalArgumentException e) {
-                    System.err.println("올바르지 않은 입력입니다. 3자리 숫자를 입력하세요.");
-                    return;
-                }
-                for (int l = 0; l < tempUserInput.length(); l++) {
-                    userInput[l] = (int) (tempUserInput.charAt(l) - '0');
-                }
+                int userInput[] = userInput();
 
                 for (int k = 0; k < numberOfAnswer.length; k++) {
                     if (userInput[k] == numberOfAnswer[k]) {
@@ -93,5 +106,3 @@ public class Application {
         }
     }
 }
-
-
