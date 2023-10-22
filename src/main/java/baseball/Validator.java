ee8ec68f01e18e5d0614e421a236b448840ae04e
dgh06175 <dgh06175@gmail.com>
2023-10-22 20:41:04 +0900

    feat: Validator 클래스 메소드 public 으로 변경

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -8,13 +8,13 @@ import java.util.Set;
 import java.util.regex.Pattern;
 
 public class Validator {
-    void numberInput(String inputString) throws IllegalArgumentException {
+    public void numberInput(String inputString) throws IllegalArgumentException {
         if (!Pattern.matches(INPUT_REGEX, inputString) || !hasUniqueNumber(inputString)) {
             throw new IllegalArgumentException(ERROR_INVALID_INPUT + inputString);
         }
     }
 
-    void continueInput(String inputContinue) throws IllegalArgumentException {
+    public void continueInput(String inputContinue) throws IllegalArgumentException {
         if (!inputContinue.equals(GameStatus.CONTINUE.getStatus()) && !inputContinue.equals(
                 GameStatus.STOP.getStatus())) {
             throw new IllegalArgumentException(ERROR_INVALID_CONTINUE_INPUT);
