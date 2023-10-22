1174ca6ab270973a0085d10e9efdf87ce9526492
sangdol <priceelees@gmail.com>
2023-10-22 14:54:28 +0900

    refactor: Ball이 숫자 정보 뿐만 아니라 위치 정보도 가지도록 수정
    < description >
    - 저장된 숫자와 위치값을 이용하여, 다른 공과 비교 후 스트라이크, 볼 여부를 판단

--- src/main/java/baseball/Ball.java
+++ src/main/java/baseball/Ball.java
@@ -4,13 +4,28 @@ import java.util.Objects;
 
 public class Ball {
     private final int number;
+    private final int location;
 
-    public Ball(int number) {
+    public Ball(int number, int location) {
         this.number = number;
+        this.location = location;
+    }
+
+    public boolean isStrike(Ball other) {
+        return this.equals(other);
+    }
+
+    public boolean isBall(Ball other) {
+        return number == other.getNumber() &&
+                location != other.getLocation();
     }
 
     public int getNumber() {
         return number;
+    }
+
+    public int getLocation() {
+        return location;
     }
 
     @Override
@@ -22,11 +37,11 @@ public class Ball {
             return false;
         }
         Ball ball = (Ball) o;
-        return getNumber() == ball.getNumber();
+        return getNumber() == ball.getNumber() && getLocation() == ball.getLocation();
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getNumber());
+        return Objects.hash(getNumber(), getLocation());
     }
 }
