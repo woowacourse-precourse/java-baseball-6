01d0567092988116d96cee9ea5f118eeb4361672
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-10-23 02:15:48 +0900

    refactor : playGame 메서드 쪼개기
    - getUserInput
    - displayEndMessage

--- src/main/java/baseball/PlayGame.java
+++ src/main/java/baseball/PlayGame.java
@@ -7,18 +7,30 @@ import static baseball.StartGame.createThreeNums;
 import static baseball.UserInput.userInput;
 
 public class PlayGame {
-    public static void playGame(Scanner sc) {
-        String answer = createThreeNums();
-        System.out.println("answer : " + answer);
 
-        String message = "";
-        while (!message.equals("3스트라이크") ){
-            String input = userInput(sc);
-            message = baseballGame(input,answer);
-            System.out.println(message);
-        }
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+  public static void playGame(Scanner sc) {
+    String answer = createThreeNums();
 
+    // 정답 확인용 출력 코드
+    System.out.println("answer : " + answer);
 
+    String message = "";
+    do {
+      String input = getUserInput(sc);
+      message = baseballGame(input, answer);
+      System.out.println(message);
     }
+    while (!message.equals("3스트라이크"));
+    displayEndMessage();
+  }
+
+  private static String getUserInput(Scanner sc) {
+    return userInput(sc);
+  }
+
+  private static void displayEndMessage() {
+    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+  }
+
+
 }
