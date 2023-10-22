9d1980eb3188318a8e4888085830ac65498dfe3b
jinnxyoung <wlsdud7722@naver.com>
2023-10-22 18:42:26 +0900

    feat : 게임 진행 로직 GameService.java 구현

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -9,6 +9,7 @@ import baseball.view.SystemInputMessage;
 import camp.nextstep.edu.missionutils.Console;
 
 public class GameService {
+    
     int size;
     Game game;
     User user = new User();
@@ -66,4 +67,5 @@ public class GameService {
             game.addStrikeCount();
         }
     }
+
 }
