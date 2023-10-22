2104988bcbe70d0b7a4aaa8c3d2ef733f95ba4cd
Knight <kangrack333@gmail.com>
2023-10-22 20:53:04 +0900

    재시작 및 종료 선택 추가(플레이어)

--- src/main/java/player/Start.java
+++ src/main/java/player/Start.java
@@ -1,23 +1,30 @@
 package player;
 
+import camp.nextstep.edu.missionutils.Console;
 import computer.CompareTwoValues;
 import computer.RandomPickNumber;
 import constant.GuideMessageConstant;
+import constant.StringConstant;
 
 public class Start {
+    String restarting_Game = "1";
 
     public Start() {
-        System.out.println(GuideMessageConstant.START_MESSAGE);
-        RandomPickNumber.randomPickNumber();
-        CompareTwoValues compareTwoValues = new CompareTwoValues();
+        while (StringConstant.CONTINUE.equals(restarting_Game)) {
+            System.out.println(GuideMessageConstant.START_MESSAGE);
+            RandomPickNumber.randomPickNumber();
+            CompareTwoValues compareTwoValues = new CompareTwoValues();
 
-        while (compareTwoValues.strikeCount != 3) {
-            Input.inputNumber();
+            while (compareTwoValues.strikeCount != 3) {
+                Input.inputNumber();
 
-            compareTwoValues.compareTwoValues(Input.inputNumber);
+                compareTwoValues.compareTwoValues(Input.inputNumber);
+            }
+
+            System.out.println(GuideMessageConstant.END_MESSAGE);
+            System.out.println(GuideMessageConstant.RESTART_END_MESSAGE);
+            restarting_Game = Console.readLine();
         }
-
-        System.out.println(GuideMessageConstant.END_MESSAGE);
     }
 
 
