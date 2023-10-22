305178217505ae5db7f13849ecd24edeeeb4892e
Jae-Young98 <ygy0102@gmail.com>
2023-10-22 22:00:36 +0900

    feat(BaseballGame): 재시작, 종료를 체크 하는 기능, 컴퓨터 번호 재생성 추가, 기존 로직 삭제
    - while 문 내부에 indent가 깊어짐에 따라 이를 개선하기 위해 3스트라이크일시 재시작, 종료 명령을 확인하는 함수로 분리 하였습니다.
    
    - 명령어가 재시작 (1) 일시 컴퓨터가 새로운 번호를 만들어내도록 함수를 분리 하였습니다.

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -4,6 +4,8 @@ import baseball.logic.GameManage;
 import baseball.logic.InputValidator;
 import java.util.List;
 
+import static baseball.constant.Constant.*;
+
 public class BaseballGame {
     private final Computer computer = new Computer();
 
@@ -15,32 +17,49 @@ public class BaseballGame {
 
     private final GameMessage gameMessage = new GameMessage();
 
+    private List<Integer> answerNumbers;
+
 
     public BaseballGame() {
         gameMessage.initGame();
     }
 
     public void start() {
-        int command = 1;
-        List<Integer> answerNumbers = computer.makeNumbers();
+        int command = COMMAND_RESTART;
+        answerNumbers = computer.makeNumbers();
 
-        while (command == 1) {
+        while (command == COMMAND_RESTART) {
             gameManage.init();
             gameMessage.getInputNumber();
             String userInput = user.inputNumbers();
             List<Integer> userNumbers = validator.validateInput(userInput);
 
             gameManage.getResult(answerNumbers, userNumbers);
-            gameManage.showResult();
-            if (gameManage.getStrikeCount() == 3) {
-                gameMessage.endGame();
-                gameMessage.getInputCommand();
-                command = user.commandInput();
 
-                if (command == 1) {
-                    answerNumbers = computer.makeNumbers();
-                }
-            }
+            int strike = gameManage.getStrikeCount();
+            int ball = gameManage.getBallCount();
+            gameMessage.showResult(strike, ball);
+
+            command = checkEndGame(strike);
+        }
+    }
+
+    private int checkEndGame(int strike) {
+        int command = COMMAND_RESTART;
+
+        if (strike == ALL_STRIKE) {
+            gameMessage.endGame();
+            gameMessage.getInputCommand();
+            command = user.commandInput();
+            checkCommandRestart(command);
+        }
+
+        return command;
+    }
+
+    private void checkCommandRestart(int command) {
+        if (command == COMMAND_RESTART) {
+            answerNumbers = computer.makeNumbers();
         }
     }
 }
