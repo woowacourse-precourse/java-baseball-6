7c34030f75c54b0f26606034d1c3de0613a092c1
dgh06175 <dgh06175@gmail.com>
2023-10-22 21:21:55 +0900

    refactor: 정적 메소드 호출만 하도록 인스턴스 생성 제거

--- src/test/java/baseball/GameRunnerTest.java
+++ src/test/java/baseball/GameRunnerTest.java
@@ -10,16 +10,12 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class GameRunnerTest {
-    private final GameRunner gameRunner = new GameRunner();
-    private final Validator validator = new Validator();
-    private final GameRules gameRules = new GameRules();
-
     @Test
     public void 숫자_정답시_true_반환_테스트() {
         List<Integer> computer = Arrays.asList(1, 2, 3);
         List<Integer> user = Arrays.asList(1, 2, 3);
 
-        boolean isWin = gameRules.isUserWin(gameRules.countStrike(user, computer));
+        boolean isWin = GameRules.isUserWin(GameRules.countStrike(user, computer));
 
         assertThat(isWin).isTrue();
     }
@@ -29,7 +25,7 @@ public class GameRunnerTest {
         List<Integer> computer = Arrays.asList(1, 2, 3);
         List<Integer> user = Arrays.asList(3, 1, 2);
 
-        int ballCount = gameRules.countBall(user, computer);
+        int ballCount = GameRules.countBall(user, computer);
 
         assertThat(ballCount).isEqualTo(3);
     }
@@ -39,7 +35,7 @@ public class GameRunnerTest {
         List<Integer> computer = Arrays.asList(1, 2, 3);
         List<Integer> user = Arrays.asList(1, 0, 0);
 
-        int strikeCount = gameRules.countStrike(user, computer);
+        int strikeCount = GameRules.countStrike(user, computer);
 
         assertThat(strikeCount).isEqualTo(1);
     }
@@ -48,7 +44,7 @@ public class GameRunnerTest {
     void 숫자_입력_예외발생_테스트_0입력() {
         String invalidInput = "012";
         assertThatThrownBy(() -> {
-            validator.numberInput(invalidInput);
+            Validator.numberInput(invalidInput);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -56,7 +52,7 @@ public class GameRunnerTest {
     void 숫자_입력_예외발생_테스트_문자() {
         String invalidInput = "dqe";
         assertThatThrownBy(() -> {
-            validator.numberInput(invalidInput);
+            Validator.numberInput(invalidInput);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -64,7 +60,7 @@ public class GameRunnerTest {
     void 숫자_입력_예외발생_테스트_3자리_이상() {
         String invalidInput = "3125";
         assertThatThrownBy(() -> {
-            validator.numberInput(invalidInput);
+            Validator.numberInput(invalidInput);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -72,7 +68,7 @@ public class GameRunnerTest {
     void 숫자_입력_예외발생_테스트_중복() {
         String invalidInput = "114";
         assertThatThrownBy(() -> {
-            validator.numberInput(invalidInput);
+            Validator.numberInput(invalidInput);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -80,7 +76,7 @@ public class GameRunnerTest {
     void 재시작_입력_예외발생_테스트_3입력() {
         String invalidInput = "3";
         assertThatThrownBy(() -> {
-            validator.continueInput(invalidInput);
+            Validator.continueInput(invalidInput);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -88,7 +84,7 @@ public class GameRunnerTest {
     void 재시작_입력_예외발생_테스트_빈칸입력() {
         String invalidInput = "";
         assertThatThrownBy(() -> {
-            validator.continueInput(invalidInput);
+            Validator.continueInput(invalidInput);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -96,7 +92,7 @@ public class GameRunnerTest {
     void 재시작_입력_예외발생_테스트_문자() {
         String invalidInput = "ㅇ";
         assertThatThrownBy(() -> {
-            validator.continueInput(invalidInput);
+            Validator.continueInput(invalidInput);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 }
