30152f636abd0bb5f3f86e258ba7d7a5a4858cf8
woo <leindey@gmail.com>
2023-10-22 16:54:36 +0900

    feat: 생성 조건에 따라 무작위 수 정하기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,22 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // 정답(3자리 숫자) 생성
+        List<Integer> target = new ArrayList<>();
+        while (target.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!target.contains(randomNumber)) {
+                target.add(randomNumber);
+            }
+        }
 
         // 게임 시작
         System.out.println("숫자 야구 게임을 시작합니다.");
