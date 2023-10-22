a8b377395f4e64a6f95bc20731f263dfec4bbdc0
김승진 <ohksj77@gmail.com>
2023-10-22 13:02:12 +0900

    refactor(BallCountFormatter): 포맷팅할 문자열 공백 더하는 위치 수정

--- src/main/java/baseball/formatter/BallCountFormatter.java
+++ src/main/java/baseball/formatter/BallCountFormatter.java
@@ -24,16 +24,16 @@ public class BallCountFormatter {
     private String toCountResult(final long ballNum, final long strikeNum) {
         final StringBuilder stringBuilder = new StringBuilder();
         if (ballNum != NONE) {
-            stringBuilder
-                    .append(String.format(BallCountMessage.BALL.toValue(), ballNum))
-                    .append(BLANK_DELIMITER);
+            stringBuilder.append(String.format(BallCountMessage.BALL.toValue(), ballNum));
         }
         return appendStrikeIfExists(stringBuilder, strikeNum);
     }
 
     private String appendStrikeIfExists(final StringBuilder stringBuilder, final long strikeNum) {
         if (strikeNum != NONE) {
-            stringBuilder.append(String.format(BallCountMessage.STRIKE.toValue(), strikeNum));
+            stringBuilder
+                    .append(BLANK_DELIMITER)
+                    .append(String.format(BallCountMessage.STRIKE.toValue(), strikeNum));
         }
         return stringBuilder.toString();
     }
