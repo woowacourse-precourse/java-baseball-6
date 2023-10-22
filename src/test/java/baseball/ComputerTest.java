f33998e47c39713e90781451f5bd286ca7346e1f
jeondui <jeondui@gmail.com>
2023-10-22 15:47:22 +0900

    feat(ComputerTest): 낫싱 힌트 테스트

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -1,27 +1,38 @@
 package baseball;
 
 import baseball.model.Computer;
+import baseball.util.NumericComparator;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ComputerTest extends NsTest {
+    private Computer computer = new Computer();
+    private NumericComparator comparator = new NumericComparator();
+
     @Test
     void 컴퓨터_숫자랜덤생성() {
-        Computer computer = new Computer();
         List<Integer> computerNumbers = computer.generateComputerNumbers();
         assertThat(computerNumbers.size()).isEqualTo(3);
     }
 
     @Test
     void 컴퓨터_숫자중복값() {
-        Computer computer = new Computer();
         List<Integer> computerNumbers = computer.generateComputerNumbers();
         assertThat(computerNumbers.get(0)).isNotEqualTo(computerNumbers.get(1)).isNotEqualTo(computerNumbers.get(2));
+    }
 
+    @Test
+    void 숫자_비교_힌트_낫싱() {
+        List<Integer> computerNumbers = Arrays.asList(7, 8, 9);
+        List<Integer> playerNumbers = Arrays.asList(1, 2, 3);
+        int result = comparator.getResult(playerNumbers, computerNumbers);
+
+        assertThat(result).isEqualTo(5);
     }
 
     @Override
