d0b08cbe74e2d472c93868ddd64be2163c0193f4
minwoo <mark42son@gmail.com>
2023-10-22 15:20:05 +0900

    feat :상대방(컴퓨터)의 수를 생성하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import java.util.ArrayList;
 public class Application {
+
+    private final static int GAME_RESTART = 1;
+    private final static int GAME_EXIT = 2;
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+//        System.out.println(computer);  // 3자리 숫자 정상 생성 확인
+
+
     }
+
+//    private static List<Integer> generateComputerNumbers(int randomNumber) {
+//
+//    }
 }
