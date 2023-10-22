bdd2f56b5e85782df3c22a84fb97c71fefd33699
yunju-m <dbswnfnal@naver.com>
2023-10-22 15:09:27 +0900

    rename(OutputView): 함수 및 변수명 변경

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,14 +1,14 @@
 package baseball.view;
 
 public class OutputView {
-    private static final String GameStartMessage = "숫자 야구 게임을 시작합니다.";
-    private static final String GameEndMessage = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String START_GAME_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String END_GAME_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
-    public void printGameStart() {
-        System.out.println(GameStartMessage);
+    public void printStartGame() {
+        System.out.println(START_GAME_MESSAGE);
     }
 
-    public void printGameEnd() {
-        System.out.println(GameEndMessage);
+    public void printEndGame() {
+        System.out.println(END_GAME_MESSAGE);
     }
 }
