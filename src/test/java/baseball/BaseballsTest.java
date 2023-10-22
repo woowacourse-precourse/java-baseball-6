7225e62ffbe2138b2bbb92b130ee42ca2f4f5a89
seung-won-kim <hesjs5@skuniv.ac.kr>
2023-10-23 00:06:58 +0900

    Test: 스트라이크의 갯수를 구하는 기능 테스트 코드 작성

--- src/test/java/baseball/BaseballsTest.java
+++ src/test/java/baseball/BaseballsTest.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
@@ -38,6 +39,20 @@ class BaseballsTest {
         );
     }
 
+    private static Stream<Arguments> compareParameters() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3), 3),
+                Arguments.of(List.of(1, 2, 4), 2),
+                Arguments.of(List.of(1, 4, 5), 1),
+                Arguments.of(List.of(3,1, 2), 0),
+                Arguments.of(List.of(3,1, 4), 0),
+                Arguments.of(List.of(3,4, 5), 0),
+                Arguments.of(List.of(1,3, 2), 1),
+                Arguments.of(List.of(1,3, 4), 1),
+                Arguments.of(List.of(4, 5, 6), 0)
+        );
+    }
+
     @Test
     @DisplayName("1 ~ 9 사이 서로 다른 3자리 수인 경우 Baseballs 객체를 생성하는 테스트")
     void constructorTest() {
@@ -70,5 +85,20 @@ class BaseballsTest {
     void invalidValueRangeExceptionTest(List<Integer> numbers) {
         // expected
         assertThrows(IllegalArgumentException.class, () -> new Baseballs(numbers));
+    }
+
+    @ParameterizedTest
+    @MethodSource("compareParameters")
+    @DisplayName("사용자 수와 컴퓨터 수를 비교하는 테스트")
+    void compareUserNumberAndComputerNumberTest(List<Integer> otherNumbers, int expected) {
+        // given
+        List<Integer> userNumbers = List.of(1, 2, 3);
+        Baseballs userNumber = new Baseballs(userNumbers);
+        Baseballs computerNumber = new Baseballs(otherNumbers);
+
+        int actual = userNumber.compare(computerNumber);
+
+        // then
+        assertEquals(expected, actual);
     }
 }
