1143210b0d8fae53ec73a9db1d13fc04aeb5c36c
sunghyun <shlim0287@naver.com>
2023-10-22 20:07:14 +0900

    feat : add user domain

---
+++ src/main/java/baseball/domain/User.java
@@ -0,0 +1,13 @@
+package baseball.domain;
+
+public class User {
+    private int[] userNums;
+
+    public int[] getUserNums() {
+        return userNums;
+    }
+
+    public void setUserNums(int[] userNums) {
+        this.userNums = userNums;
+    }
+}
