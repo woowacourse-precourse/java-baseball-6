2ecd53c46963610e4b9941efea6e8b0e1d9b5d65
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-10-22 12:16:46 +0900

    feat: 숫자 비교 결과를 출력하는 기능 구현
    `ball`과 `strike`의 개수에 따라 출력 메세지가 달라질 수 있도록 구현함.

--- src/main/java/baseball/util/Messages.java
+++ src/main/java/baseball/util/Messages.java
@@ -2,7 +2,10 @@ package baseball.util;
 
 public enum Messages {
     GAME_START_MESSAGE("숫자 야구 게임을 시작합니다."),
-    GET_USER_NUMBER_MESSAGE("숫자를 입력해주세요: ");
+    GET_USER_NUMBER_MESSAGE("숫자를 입력해주세요: "),
+    BALL_COUNT_MESSAGE("볼"),
+    STRIKE_COUNT_MESSAGE("스트라이크"),
+    NOTHING_MESSAGE("낫싱");
 
     private final String message;
 
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -6,4 +6,21 @@ public class OutputView {
     public void printGameStartMessage() {
         System.out.println(Messages.GAME_START_MESSAGE.getMessage());
     }
+
+    public void printGameResultBallAndStrike(int ballCount, int strikeCount) {
+        StringBuilder sb = new StringBuilder();
+        if (ballCount != 0) {
+            sb.append(ballCount).append(Messages.BALL_COUNT_MESSAGE.getMessage());
+        }
+        if (strikeCount != 0) {
+            if (ballCount != 0) {
+                sb.append(" ");
+            }
+            sb.append(strikeCount).append(Messages.STRIKE_COUNT_MESSAGE.getMessage());
+        }
+        if (ballCount == 0 && strikeCount == 0) {
+            sb.append(Messages.NOTHING_MESSAGE.getMessage());
+        }
+        System.out.println(sb);
+    }
 }
