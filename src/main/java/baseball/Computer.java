3f5141eef0f8c81a248259871e78689be1cbd29f
kg9585 <kg9585@naver.com>
2023-10-22 19:34:05 +0900

    refactor: Computer 클래스에서 무작위 숫자 생성 리팩토링한다

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,43 +1,38 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.function.IntSupplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Computer {
 
   static final int NUMBER_LENGTH = 3;
-  static final int MAX_DIGIT = 9;
-  static final int MIN_DIGIT = 1;
-
-  private int randomNumber;
+  static final int DIGIT_MAX = 9;
+  static final int DIGIT_MIN = 1;
+  int randomNumber;
 
   public Computer() {
-    this.randomNumber = generateUniqueRandomNumber();
   }
-
 
   public int getRandomNumber() {
     return randomNumber;
   }
 
-
-  private int generateUniqueRandomNumber() {
-    Set<Integer> randomDigitSet = generateRandomDigits();
-    return convertSetToNumber(randomDigitSet);
+  public void setRandomNumber() {
+    this.randomNumber = generateRandomNumber();
   }
 
-  private Set<Integer> generateRandomDigits() {
-    Set<Integer> randomDigitSet = new HashSet<>();
-    while (randomDigitSet.size() < NUMBER_LENGTH) {
-      int number = (int) (Math.random() * MAX_DIGIT) + MIN_DIGIT;
-      randomDigitSet.add(number);
-    }
-    return randomDigitSet;
-  }
-
-  private int convertSetToNumber(Set<Integer> digits) {
-    var randomNumberString = new StringBuilder();
-    digits.forEach(randomNumberString::append);
-    return Integer.parseInt(randomNumberString.toString());
+  private int generateRandomNumber() {
+    String number = Stream.generate(() -> Randoms.pickNumberInRange(DIGIT_MIN, DIGIT_MAX))
+        .map(String::valueOf)
+        .distinct()
+        .limit(NUMBER_LENGTH)
+        .collect(Collectors.joining(""));
+    return Integer.parseInt(number);
   }
 }
