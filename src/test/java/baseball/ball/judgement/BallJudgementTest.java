c8ef40209d695e25e783d983faf375d97073cdbf
hongyoung <qwe916@naver.com>
2023-10-23 03:41:05 +0900

    feat: 사용자의 Ball을 판정하는 BallJudgement 구현

---
+++ src/test/java/baseball/ball/judgement/BallJudgementTest.java
@@ -0,0 +1,43 @@
+package baseball.ball.judgement;
+
+import baseball.ball.Ball;
+import baseball.ball.status.BallStatus;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class BallJudgementTest {
+    BallJudgement ballJudgement;
+
+    @BeforeEach
+    void setUp() {
+        ballJudgement = new BallJudgement(List.of(1, 2, 3));
+    }
+
+    @Test
+    void strike(){
+        Ball ball = new Ball(1, 1);
+        BallStatus ballStatus = ballJudgement.judgeBall(ball);
+
+        assertThat(ballStatus).isEqualTo(BallStatus.STRIKE);
+    }
+
+    @Test
+    void ball(){
+        Ball ball = new Ball(2, 1);
+        BallStatus ballStatus = ballJudgement.judgeBall(ball);
+
+        assertThat(ballStatus).isEqualTo(BallStatus.BALL);
+    }
+
+    @Test
+    void nothing(){
+        Ball ball = new Ball(4, 1);
+        BallStatus ballStatus = ballJudgement.judgeBall(ball);
+
+        assertThat(ballStatus).isEqualTo(BallStatus.NOTHING);
+    }
+}
