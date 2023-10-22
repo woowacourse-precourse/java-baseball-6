3f54ddf53e7fbb311bccc9f301ac3500fc2eb424
JINU-CHANG <jenny010925@naver.com>
2023-10-22 20:28:27 +0900

    [feat] 숫자객체 생성 및 유효성 확인 메서드 구현

---
+++ src/main/java/baseball/domain/Number.java
@@ -0,0 +1,45 @@
+package baseball.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Number {
+	private final int SIZE = 3;
+	private String number;
+
+	public Number(String number) {
+		validateSize(number);
+		validateDuplication(number);
+		this.number = number;
+	}
+
+	public void validateSize(String number) {
+		if(number.length()!=SIZE) {
+			throw new IllegalArgumentException(SIZE+"자리수의 숫자를 입력해주세요.");
+		}
+	}
+
+	public void validateDuplication(String number) {
+		Map<Character, Integer> checkMap = new HashMap<>();
+
+		for(char checkNumber : number.toCharArray()) {
+			initCheckMap(checkMap, checkNumber);
+			checkDuplication(checkMap, checkNumber);
+		}
+	}
+
+	private void initCheckMap(Map<Character, Integer> checkMap, char checkNumber) {
+		int checkNumberCount = checkMap.getOrDefault(checkNumber, 0);
+		checkMap.put(checkNumber, checkNumberCount + 1);
+	}
+
+	private void checkDuplication(Map<Character, Integer> checkMap, char checkNumber) {
+		if(isDuplicated(checkMap, checkNumber)) {
+			throw new IllegalArgumentException("중복된 숫자가 존재합니다.");
+		}
+	}
+
+	private boolean isDuplicated(Map<Character, Integer> checkMap, char checkNumber) {
+		return checkMap.get(checkNumber) > 1;
+	}
+}
