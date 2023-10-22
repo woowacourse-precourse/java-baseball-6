5c5b3687224234ca4177093d5c2278f13b398b42
hoyeon <highoption@naver.com>
2023-10-22 11:54:39 +0900

    add : InputView 에 검증 로직 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,5 +1,7 @@
 package baseball.view;
 
+import baseball.validation.InputValidation;
+
 public class InputView {
     public static void firstStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -7,7 +9,9 @@ public class InputView {
 
     public static String startInput() {
         System.out.println("숫자를 입력해주세요 : ");
-        return System.console().readLine();
+        String inputNum = System.console().readLine();
+        InputValidation.verifyInputNum(inputNum);
+        return inputNum;
     }
 
     public static String endInput() {
