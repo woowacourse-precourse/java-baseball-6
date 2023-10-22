8774f3528899061b962b7b94cfe440168c22829d
yunju-m <dbswnfnal@naver.com>
2023-10-22 16:58:10 +0900

    refactor(GameController): 수정 게임종료 문구 함수를 이용하여 출력

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -20,7 +20,7 @@ public class GameController {
         computerController.provideHint();
 
         if (computerController.isSuccessGame()) {
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            outputView.printEndGame();
         }
     }
 }
