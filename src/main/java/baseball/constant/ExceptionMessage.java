06730b8f99731b758c55c3dcf2b7c4439bcb9087
itsmo1031 <itsmo1031@gmail.com>
2023-10-22 18:09:37 +0900

    :sparkles: feat(constant): add Exception message
    숫자가 아닌 문자가 입력되었을 경우에 출력할 Exception 메세지 추가.

--- src/main/java/baseball/constant/ExceptionMessage.java
+++ src/main/java/baseball/constant/ExceptionMessage.java
@@ -8,7 +8,8 @@ import static baseball.constant.GameConstant.TOTAL_BALL_CNT;
 public enum ExceptionMessage {
     LENGTH("입력한 수의 길이가 길거나 짧습니다.", TOTAL_BALL_CNT.getValue()),
     TYPE("잘못된 입력입니다.", "1(재시작) 또는 2(종료)"),
-    DUPLICATE("서로 다른 세 숫자를 입력해야 합니다.", 123);
+    DUPLICATE("서로 다른 세 숫자를 입력해야 합니다.", 123),
+    NUMBER("서로 다른 세 숫자만 입력되어야 합니다.", 123);
 
     private final String message;
     private final Object expect;
