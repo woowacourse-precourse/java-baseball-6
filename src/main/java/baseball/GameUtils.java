5b0d0884085972c3fe91caf07975e465bd6ec947
Youth <boysoeng@naver.com>
2023-10-23 00:47:54 +0900

    fix: Randoms number 생성 API 수정

--- src/main/java/baseball/GameUtils.java
+++ src/main/java/baseball/GameUtils.java
@@ -2,11 +2,20 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameUtils {
+    private static final int RANDOM_NUMBERS = 3;
     public static List<Integer> generateRandomNumber(){
-        return Randoms.pickUniqueNumbersInRange(1,9,3);
+        List<Integer> answer = new ArrayList<>();
+        while(answer.size()<RANDOM_NUMBERS){
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if (!answer.contains(randomNumber)){
+                answer.add(randomNumber);
+            }
+        }
+        return answer;
     }
 
 }
