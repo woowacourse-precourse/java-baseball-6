a027c28a5f974a7f6ee8a94c4a8bac28c7aeaa8f
Brian <bwook9908@gmail.com>
2023-10-23 01:18:49 +0900

    feat: gameController 구현

---
+++ src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,41 @@
+package baseball.controller;
+
+import baseball.model.Computer;
+import baseball.model.Inning;
+
+import java.util.ArrayList;
+
+public class GameController {
+    private final IOController ioController;
+
+    public GameController() {
+        this.ioController = new IOController();
+    }
+
+    public void gameStart() {
+        ioController.gameStartNotify();
+        ArrayList<Integer> answer = Computer.getNumbers(3);
+        inGame(answer);
+    }
+
+    private void inGame(ArrayList<Integer> answer) {
+        while (true) {
+            ArrayList<Integer> input = ioController.inningNumberInput();
+            Inning inning = new Inning(answer, input);
+            ioController.showResult(inning.getResult());
+            if (inning.getResult().equals("3스트라이크")) {
+                restart();
+                break;
+            }
+        }
+    }
+
+    private void restart() {
+        ioController.gameEndNotify();
+        int restart = ioController.restartNumberInput();
+        if (restart == 1) {
+            ArrayList<Integer> answer = Computer.getNumbers(3);
+            inGame(answer);
+        }
+    }
+}
