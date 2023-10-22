4596d9e239f20d74889bcfd7fa05c44cad6f9175
snaag <snaag.dev@gmail.com>
2023-10-23 02:37:38 +0900

    feat: (Constants) 상수 값 작성

---
+++ src/main/java/baseball/utils/Constants.java
@@ -0,0 +1,13 @@
+package baseball.utils;
+
+public class Constants {
+    public static Integer INIT_VALUE = -1;
+    public static Integer ZERO = 0;
+    public static Integer BALL_MAX = 3;
+    public static Integer RANGE_END = 10;
+
+    public static String GAME_FLAG_START = "1";
+    public static String GAME_FLAG_END = "2";
+
+    public static String NUMBER_REGEX = "^[1-9]{3}$";
+}
