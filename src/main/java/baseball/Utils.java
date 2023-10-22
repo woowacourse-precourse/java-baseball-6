8bcbf81f04e02ad6d54c4cede493599d5d90a287
aaa <cnj991279@gmail.com>
2023-10-22 16:09:13 +0900

    feat: 유효성 검사 구현
    1~9 사이의 자연수인지 검사
    정해진 길이가 맞는지 검사
    중복된 문자가 없는지 검사

--- src/main/java/baseball/Utils.java
+++ src/main/java/baseball/Utils.java
@@ -2,8 +2,7 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Utils {
     public static List<Integer> generateComputerRandomNumber(int length) {
@@ -15,5 +14,32 @@ public class Utils {
             }
         }
         return computerRandomNumber;
+    }
+
+    public static void validateStringNaturalNumberLengthDuplicate(String input) {
+        validateStringNaturalNumber(input);
+        validateStringLength(input, 3);
+        validateStringDuplicate(input);
+    }
+
+    public static void validateStringNaturalNumber(String input) {
+        for (char c : input.toCharArray()) {
+            if (!Character.isDigit(c) || c == '0') {
+                throw new IllegalArgumentException("1 ~ 9사이의 자연수를 입력해주세요.");
+            }
+        }
+    }
+
+    public static void validateStringLength(String input, int length) {
+        if (input.length() != length) {
+            throw new IllegalArgumentException("정해진 길이의 문자열을 입력해주세요.");
+        }
+    }
+
+    public static void validateStringDuplicate(String input) {
+        Set<String> set = new HashSet<>(Arrays.stream(input.split("")).toList());
+        if (set.size() != input.length()) {
+            throw new IllegalArgumentException("중복되지 않은 문자열을 입력해주세요.");
+        }
     }
 }
