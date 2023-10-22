2081725bf65a43c722933526989b227b5bc9c992
chosunghyun18 <krismon@naver.com>
2023-10-22 20:15:48 +0900

    refact : change computer count method name

--- src/main/java/baseball/model/BaseballGamePlayer.java
+++ src/main/java/baseball/model/BaseballGamePlayer.java
@@ -5,15 +5,11 @@ import java.util.List;
 import java.util.Set;
 
 public class BaseballGamePlayer {
-    public BaseballGamePlayer() {
-
-    }
-
-    public static Hint swingBat(BaseBallGameComputer computer, List<Integer> playerNumbers) {
+    public static Hint swingBat(BaseballGameComputer computer, List<Integer> playerNumbers) {
         Set<Ball> balls = new LinkedHashSet<>();
         for (Integer number : playerNumbers) {
             balls.add(new Ball(number));
         }
-        return computer.getHintByPlayer(balls);
+        return computer.getHintByPlayerBalls(balls);
     }
 }
