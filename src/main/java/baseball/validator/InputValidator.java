918321f87d0d3d0cf477a58752f8791d257eacd0
NAM GIBEOM <rlqja0523@naver.com>
2023-10-22 15:21:32 +0900

    feat(Validator): 중복된 값이 있을 때 예외를 발생시키는 기능 추가

--- src/main/java/baseball/validator/InputValidator.java
+++ src/main/java/baseball/validator/InputValidator.java
@@ -31,4 +31,15 @@ public class InputValidator {
             throw new IllegalArgumentException(String.format("입력값이 %d~%d 사이의 값이 아닙니다.", minNumber, maxNumber));
         }
     }
+
+    private void validateDuplicate(String input) {
+        int ballCount = GameConfig.BALL_COUNT.getValue();
+        for (int i = 0; i < ballCount; i++) {
+            for (int j = i + 1; j < ballCount; j++) {
+                if (input.charAt(i) == input.charAt(j)) {
+                    throw new IllegalArgumentException("중복된 숫자가 있습니다.");
+                }
+            }
+        }
+    }
 }
