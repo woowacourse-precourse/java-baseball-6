661227fee86f426e22313065ae07146fd0b872a6
ma-an-jong <alswhd1113@gmail.com>
2023-10-23 01:38:00 +0900

    comment(Application) : 구현 계획 주석으로 정리
    사용자 입력과 컴퓨터가 생성하는 '숫자'는 모두 공통사항이다.
    검증 로직조차 동일하기 때문에 Number 도메인 객체를 만든다.
    
    Controller -> Service -> Domain으로 가는 흐름을 만든다
    컴퓨터 난수만 저장하면 되기 때문에 Repository는 필요없다는 결론이 나왔다.
    Service 클래스에서 컴퓨터가 생성한 Number를 가지고 init() 메소드로 초기화하는 생각을 했다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,16 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
+        /***
+         * 사용자 입력과 컴퓨터가 생성하는 '숫자'는 모두 공통사항이라고 생각
+         * 검증 로직조차 동일하기 때문에 Number 도메인 객체를 정의
+         *
+         * Controller > Service > Domain으로 가는 흐름
+         *
+         * I/O 과정은 InputView , OutputView 를 통해서 Controller에서 사용자 입출력 받기
+         * 예외 메시지, 입출력 메시지는 utils 패키지의 message 패키지에서 관리
+         */
+
         // TODO: 프로그램 구현
     }
 }
