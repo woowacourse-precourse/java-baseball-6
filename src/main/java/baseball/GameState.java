f1f3ca82d34d316af82b2f0c4d59ba961fa29a42
aammddkkzxc <97333563+aammddkkzxc@users.noreply.github.com>
2023-10-22 15:33:54 +0900

    실행 로직 수정 및 게임 성공 상태 추가

--- src/main/java/baseball/GameState.java
+++ src/main/java/baseball/GameState.java
@@ -1,6 +1,8 @@
 package baseball;
 
 public class GameState {
+
+    public static final int GAME_SUCCEED_STATE = 0;
     public static final int PROGRESS_STATE = 1;
     public static final int END_STATE = 2;
     public int gameSwitch;
@@ -9,4 +11,9 @@ public class GameState {
         this.gameSwitch = gameSwitch;
     }
 
+    public void checkGameSucceeded(int strike) {
+        if (strike == 3) {
+            gameSwitch = GAME_SUCCEED_STATE;
+        }
+    }
 }
