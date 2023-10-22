ff12ad79b0c886e2ec7ebc7fba0acffd3491ba49
eom-tae-in <eti0728@daum.net>
2023-10-22 15:34:49 +0900

    feat: 계산한 볼과 스트라이크로 게임의 결과를 만들어주는 ResultMapper 생성

---
+++ src/main/java/baseball/controller/mapper/ResultMapper.java
@@ -0,0 +1,21 @@
+package baseball.controller.mapper;
+
+public class ResultMapper {
+    private static final int NO_STRIKE = 0;
+    private static final int NO_BALL = 0;
+    private static final String NOTHING = "낫싱";
+    private static final String STRIKE = "스트라이크";
+    private static final String BALL = "볼";
+    private static final String BLANK = " ";
+
+    public String makeResult(final int ballCount, final int strikeCount) {
+        if (isNothing(ballCount, strikeCount)) {
+            return NOTHING;
+        }
+        return ballCount + BALL + BLANK + strikeCount + STRIKE;
+    }
+
+    private boolean isNothing(final int ballCount, final int strikeCount) {
+        return ballCount == NO_BALL && strikeCount == NO_STRIKE;
+    }
+}
