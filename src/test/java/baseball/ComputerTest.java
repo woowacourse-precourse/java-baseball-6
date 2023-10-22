81203dcc0184fffa19f484a452cda84355695840
yechop <channetattoo@gmail.com>
2023-10-22 17:38:55 +0900

    Test: given when then 추가

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -16,13 +16,21 @@ class ComputerTest {
 
     @Test
     void computerAnswer의_자리수_테스트() {
+        //given
+
+        //when
+
+        //then
         assertThat(computerAnswer.size()).isEqualTo(3);
     }
 
     @Test
     void computerAnswer의_중복숫자_없는지_테스트() {
+        //given
         int cnt = 0;
         List<Integer> answerToArr = new ArrayList<>();
+
+        //when
         for (int i = 0; i < 3; i++) {
             int num = computerAnswer.get(i);
             if(answerToArr.contains(num)) {
@@ -30,12 +38,17 @@ class ComputerTest {
             }
             answerToArr.add(num);
         }
+
+        //then
         assertThat(cnt).isEqualTo(0);
     }
 
     @Test
     void computerAnswer의숫자가_1부터9사이에있는지_테스트() {
+        //given
         boolean test = true;
+
+        //when
         for (int i = 0; i < 3; i++) {
             int num = computerAnswer.get(i);
             if(!(num >= 1 && num <= 9)) {
@@ -43,6 +56,8 @@ class ComputerTest {
                 break;
             }
         }
+
+        //then
         assertThat(test).isEqualTo(true);
     }
 }
