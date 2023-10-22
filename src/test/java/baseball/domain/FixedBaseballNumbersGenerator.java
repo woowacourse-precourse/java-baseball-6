45a8c1b20b668acf69c92349c408729ee88a2493
kunsanglee <gosmdochee2@gmail.com>
2023-10-22 19:02:52 +0900

    refactor(FixedBaseballNumbersGenerator): 클래스명 변경

--- src/test/java/baseball/domain/FixedBaseballNumbersGenerator.java
+++ src/test/java/baseball/domain/FixedBaseballNumbersGenerator.java
@@ -2,7 +2,7 @@ package baseball.domain;
 
 import java.util.stream.Stream;
 
-public class FixedNumbersGenerator implements NumbersGenerator {
+public class FixedBaseballNumbersGenerator implements BaseballNumbersGenerator {
     @Override
     public BaseballNumbers generate() {
         return new BaseballNumbers(Stream.of(7, 1, 3).map(BaseballNumber::new).toList());
