f1cdccac091a1a395d5b7b960fd1bd82a7816229
Dowon <3120kang@gmail.com>
2023-10-22 15:41:03 +0900

    Refactor: 인스턴스 변수 수정
    Comparator 내부에 리스트 변수를 없애고 Balls 변수를 선언해서 Balls 변수로 값을 전달받고 비교한다

--- src/main/java/computer/Comparator.java
+++ src/main/java/computer/Comparator.java
@@ -8,19 +8,18 @@ import java.util.List;
 public class Comparator {
 
     private static final int BALLS_SIZE=3;
-    private List<Integer> computerNumber=new ArrayList<>();
-    private List<Integer> userNumber=new ArrayList<>();
+    private Balls computer;
+    private Balls user;
     private int strike;
     private int ball;
 
     public void compare(Balls computer, Balls user){
 
         initScore();
-        initNumber();
         receiveBalls(computer, user);
 
-        for(int current=0; current<3;current++) {
-            if(isSameValueAtSameLocation(current)){
+        for (int current = 0; current < BALLS_SIZE; current++) {
+            if (isSameValueAtSameLocation(current)) {
                 continue;
             }
             isContainInComputerNumber(current);
@@ -30,7 +29,7 @@ public class Comparator {
     }
 
     private boolean isSameValueAtSameLocation(int location){
-        if (computerNumber.get(location).equals(userNumber.get(location))) {
+        if (computer.getBalls().get(location).equals(user.getBalls().get(location))) {
             strike++;
             return true;
         }
@@ -38,7 +37,7 @@ public class Comparator {
     }
 
     private boolean isContainInComputerNumber(int location){
-        if (computerNumber.contains(userNumber.get(location))) {
+        if (computer.getBalls().contains(user.getBalls().get(location))) {
             ball++;
             return true;
         }
@@ -46,20 +45,13 @@ public class Comparator {
     }
 
     private void receiveBalls(Balls computer,Balls user){
-
-        computer.getBalls().stream().forEach(n -> this.computerNumber.add(n));
-        //this.userNumber=user.getBalls();
-        user.getBalls().stream().forEach(n -> this.userNumber.add(n));
+        this.computer=computer;
+        this.user=user;
     }
 
     private void initScore(){
         strike=0;
         ball=0;
-    }
-
-    private void initNumber(){
-        computerNumber.clear();
-        userNumber.clear();
     }
 
     public void storeToHint(int strike,int ball){
