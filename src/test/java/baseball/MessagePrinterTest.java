2f9b4235a4b85b61da039cd18caf2f670eed63ca
MinseokGo <rhalstjr1999@naver.com>
2023-10-22 15:46:11 +0900

    test: 볼 출력 테스트

--- src/test/java/baseball/MessagePrinterTest.java
+++ src/test/java/baseball/MessagePrinterTest.java
@@ -28,4 +28,28 @@ class MessagePrinterTest {
             System.out.println(strikeCount + "스트라이크");
         }
     }
+
+    @Test
+    @DisplayName("볼만 존재할 경우 볼만 출력한다.")
+    void test2() {
+        int strikeCount = 0;
+        int ballCount = 0;
+        List<Integer> computerNumbers = List.of(3, 5, 6);
+        String playerNumbers = "134";
+
+        for (int i=0; i<playerNumbers.length(); i++) {
+            int playerNumber = playerNumbers.charAt(i) - 48;
+            int computerNumber = computerNumbers.get(i);
+
+            if (playerNumber == computerNumber) {
+                strikeCount += 1;
+            } else if (computerNumbers.contains(playerNumber)) {
+                ballCount += 1;
+            }
+        }
+
+        if (strikeCount == 0 && ballCount != 0) {
+            System.out.println(ballCount + "볼");
+        }
+    }
 }
