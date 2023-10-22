bd6552ba11c90476e159d6488f381dfa8ff0cd70
Dongwoongkim <rlaehddnd0422@naver.com>
2023-10-22 15:19:51 +0900

    feat : (InputView) 입력 요청 메시지 출력 기능 구현

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,19 @@
+package baseball.view;
+
+public class InputView {
+    private static final String START_MSG = "숫자 야구 게임을 시작합니다.";
+    private static final String REQUEST_RESTART_OR_EXIT_MSG = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String REQUEST_NUMBER_MSG = "숫자를 입력해주세요 : ";
+
+    public void printStartMessage() {
+        System.out.println(START_MSG);
+    }
+
+    public void printSuggestNumberMessage() {
+        System.out.print(REQUEST_NUMBER_MSG);
+    }
+
+    public void printRestartOrExitMessage() {
+        System.out.print(REQUEST_RESTART_OR_EXIT_MSG);
+    }
+}
