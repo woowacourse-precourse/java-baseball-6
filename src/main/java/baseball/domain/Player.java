28c0da7fbffaaeb0f03e115e85bfc5f556073447
Hyeon0208 <dmsgktn0208@gmail.com>
2023-10-22 23:23:41 +0900

    feat : PlayerNumber -> Player, 숫자 생성 로직 추가

---
+++ src/main/java/baseball/domain/Player.java
@@ -0,0 +1,24 @@
+package baseball.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    private final Numbers numbers;
+
+    public Player(String input) {
+        this.numbers = createPlayerNumbers(input);
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers.getNumbers();
+    }
+
+    private static Numbers createPlayerNumbers(String input) {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < input.length(); i++) {
+            numbers.add(input.charAt(i) - '0');
+        }
+        return new Numbers(numbers);
+    }
+}
