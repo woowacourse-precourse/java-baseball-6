617d8825f11d35f07d5737a3aa30fb5c9d8b86eb
권용하 <yongha.kwon12@gmail.com>
2023-10-22 18:05:09 +0900

    refactor : add throw Exception

--- src/main/java/baseball/domain/BaseballNumber.java
+++ src/main/java/baseball/domain/BaseballNumber.java
@@ -8,12 +8,12 @@ public class BaseballNumber {
     private final BaseballNumberGenerator baseballNumberGenerator;
     private static final int BASEBALL_NUMBER_LEN = 3;
 
-    public BaseballNumber(BaseballNumberGenerator baseballNumberGenerator) {
+    public BaseballNumber(BaseballNumberGenerator baseballNumberGenerator) throws IllegalArgumentException{
         this.baseballNumberGenerator = baseballNumberGenerator;
         initBaseballNumber();
     }
 
-    public void initBaseballNumber() {
+    public void initBaseballNumber() throws IllegalArgumentException{
         baseballNumber = baseballNumberGenerator.generateBaseballNumber(BASEBALL_NUMBER_LEN);
     }
 
