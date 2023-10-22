dd36e2e25f6848ae302045ac84c6a62deeafd0d9
Lee JaeHyuk <80966413+JH22222@users.noreply.github.com>
2023-10-22 15:55:54 +0900

    feat: 난수 생성 모듈 추가

---
+++ src/main/java/controller/RandomNumber.java
@@ -0,0 +1,32 @@
+package controller;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomNumber {
+    private final List<Integer> randomNumbers;
+
+    public RandomNumber() {
+        this.randomNumbers = new ArrayList<>();
+    }
+
+    public List<Integer> getRandomNumbers() {
+        return new ArrayList<>(randomNumbers);
+    }
+
+    public void generateNumbers(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+    }
+
+    public void clear(){
+        randomNumbers.clear();
+    }
+}
