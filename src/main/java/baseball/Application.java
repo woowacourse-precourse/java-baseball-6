a61fab19dcc44af758e7828420775c1a7ff4f89f
LeeSeungUk <tmddnr5410@gmail.com>
2023-10-22 21:23:28 +0900

    feat : 게임 시작시 문구 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,6 +3,7 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
+        BaseballGame gameBaseball = new BaseballGame();
+        gameBaseball.startGame();
     }
 }
---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,7 @@
+package baseball;
+
+public class BaseballGame {
+    public void startGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+}
