506630fade272cf6acb33e3ff762151d28bfae61
MinseokGo <rhalstjr1999@naver.com>
2023-10-22 15:40:47 +0900

    test: 플레이어의 입력 자릿수 실패 테스트
    플레이어가 입력한 숫자의 자릿수가 3자리가 아닐 경우를 테스트 합니다.

--- src/test/java/baseball/PlayerInputTest.java
+++ src/test/java/baseball/PlayerInputTest.java
@@ -30,4 +30,12 @@ class PlayerInputTest {
 
         assertThat(input.length()).isEqualTo(3);
     }
+
+    @Test
+    @DisplayName("실패 테스트 - 플레이어가 입력한 숫자는 3자리가 아니다.")
+    void test4() {
+        String input = "1234";
+
+        assertThat(input.length()).isNotSameAs(3);
+    }
 }
