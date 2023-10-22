3fddd10d5bce3cadf71055632accc389d1cb1c5e
haiseong <wjdgotjd9908@gmail.com>
2023-10-23 02:03:58 +0900

    refactor: AnswerGenerator: import 정리

--- src/main/java/baseball/controller/AnswerGenerator.java
+++ src/main/java/baseball/controller/AnswerGenerator.java
@@ -2,10 +2,8 @@ package baseball.controller;
 
 import static baseball.model.GameNumbers.GAME_NUMBERS_SIZE;
 
-import baseball.model.Game;
 import baseball.model.GameNumbers;
 import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.ArrayList;
 import java.util.List;
 
