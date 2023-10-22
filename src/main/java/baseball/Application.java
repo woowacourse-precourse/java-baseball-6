3371a98b96004518f96bd31ec7addaab516a5957
이진모 <jinmlee210@naver.com>
2023-10-23 00:09:17 +0900

    기능 1차구현 완료 기능테스트 완료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -89,6 +89,7 @@ public class Application {
     }
 
     static boolean replayInputCheck() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String input = readLine();
         if (input.equals("1")) {
             return true;
@@ -101,6 +102,11 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        playGame();
+        boolean replay = true;
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (replay) {
+            playGame();
+            replay = replayInputCheck();
+        }
     }
 }
