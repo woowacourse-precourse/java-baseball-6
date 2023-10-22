5925803e1dd4f95e3c77cc656f6a487cc7a7a24f
hayannn <dlgkdis801@naver.com>
2023-10-22 16:27:41 +0900

    feat: 스트라이크 개수를 인덱싱하는 변수와 3스트라이크의 값을 가지고 있는 변수를 생성

--- src/main/java/baseball/Controller/BaseballGameController.java
+++ src/main/java/baseball/Controller/BaseballGameController.java
@@ -13,9 +13,13 @@ public class BaseballGameController {
     private Computer computer = new Computer();
     private Player player = new Player();
 
-    private final GameFlowManagementService gameFlowManagementService = new GameFlowManagementService(); //숫자 야구 게임의 흐름 및 다음 게임의 'Replay or Quit' question 담당
-    private final GameNumberCheckService gameNumberCheckService = new GameNumberCheckService(); //컴퓨터와 플레이어의 숫자 비교-'스트라이크, 볼'을 카운트
-    private final FirstGamePlayerInputView firstGamePlayerInputView = new FirstGamePlayerInputView(); //플레이어의 숫자 입력 처리 및 저장
-    private final GameScoreView gameScoreView = new GameScoreView(); //숫자 야구 게임의 플레이 상황에 따른 적절한 결과 출력
-    private List<Integer> countStrikeBall = new ArrayList<>(); //스트라이크와 볼의 개수 카운트
+    /* 숫자의 비교를 통한 게임 진행을 위해 변수 생성 */
+    private static final int STRIKE_COUNT_INDEX = 0; //현재 스트라이크 개수 -> 인덱스로 저장
+    private static final int STRIKE_COUNT_THREE = 3; //현재의 스트라이크 개수와 비교하기 위한 3스트라이크의 값 생성
+
+    private final GameFlowManagementService gameFlowManagementService = new GameFlowManagementService(); //숫자 야구 게임의 흐름 및 다음 게임의 'Replay or Quit' question 담당 서비스를 연결
+    private final GameNumberCheckService gameNumberCheckService = new GameNumberCheckService(); //컴퓨터와 플레이어의 숫자 비교-'스트라이크, 볼'을 카운트하는 서비스를 연결
+    private final FirstGamePlayerInputView firstGamePlayerInputView = new FirstGamePlayerInputView(); //플레이어의 숫자 입력 실제 처리 및 저장하는 뷰를 연결
+    private final GameScoreView gameScoreView = new GameScoreView(); //숫자 야구 게임의 플레이 상황에 따른 적절한 결과를 출력하는 뷰를 연결
+    private List<Integer> countStrikeBall = new ArrayList<>(); //스트라이크와 볼의 개수 카운트하는 리스트 생성
 }
