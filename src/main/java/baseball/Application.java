fc199ec6960825071cbda3e1bb3eb68ddcd6a310
unknown <kimji1026@gmail.com>
2023-10-22 15:19:21 +0900

    docs : Application에 주석 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,10 @@
 package baseball;
 
+/**
+ * 숫자 야구 게임의 실행을 담당하는 애플리케이션 클래스입니다.
+ * 
+ * @author 김지환
+ */
 public class Application {
     public static void main(String[] args) {
         BaseballGameImpl baseball = new BaseballGameImpl();
