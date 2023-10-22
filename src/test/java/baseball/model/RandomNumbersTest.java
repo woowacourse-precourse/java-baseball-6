53747df8517ecdc0b53a30011c92e1637c691342
koomchang <leahpar0401@gmail.com>
2023-10-22 21:08:29 +0900

    test: 랜덤 숫자는 3개가 만들어져야 한다

--- src/test/java/baseball/model/RandomNumbersTest.java
+++ src/test/java/baseball/model/RandomNumbersTest.java
@@ -1,4 +1,20 @@
+package baseball.model;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class RandomNumbersTest {
-  
+
+    RandomNumbers randomNumbers = new RandomNumbers();
+    List<Number> generatedNumbers = randomNumbers.getRandomNumbers();
+
+    @Test
+    @DisplayName("3개의 랜덤 숫자가 만들어져야 한다.")
+    public void is_RandumNumber_Size_3() {
+        assertEquals(3, generatedNumbers.size());
+    }
+
 }
