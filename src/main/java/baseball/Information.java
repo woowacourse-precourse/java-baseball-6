f8772a9057a07c3cd5a974d6571a9babe87aa650
Woongbae Yun <devbelly@naver.com>
2023-10-22 15:09:29 +0900

    feat : ball, strike에 따른 안내 문구를 출력하는 기능 추가

--- src/main/java/baseball/Information.java
+++ src/main/java/baseball/Information.java
@@ -12,4 +12,16 @@ public class Information {
     public static void printInputNumber() {
         System.out.println(INPUT_NUMBER);
     }
+
+    public static void printBallAndStrike(int ball, int strike) {
+        if (ball == 0 && strike == 0) {
+            System.out.println("낫싱");
+        }
+        if (ball != 0) {
+            System.out.println(ball + "볼");
+        }
+        if (strike != 0) {
+            System.out.println(strike + "스트라이크");
+        }
+    }
 }
