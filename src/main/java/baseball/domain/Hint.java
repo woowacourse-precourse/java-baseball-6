83501de55f4578a402d7751a02846123c9172505
inpink <inpink@kakao.com>
2023-10-22 23:04:20 +0900

    refactor: 힌트를 담는 Model Hint 생성
    ball 개수, strike 개수, nothing 여부를 담는 Model.
    Getter Setter가 있음.

---
+++ src/main/java/baseball/domain/Hint.java
@@ -0,0 +1,32 @@
+package baseball.domain;
+
+public class Hint {
+
+    private int ball;
+    private int strike;
+    private boolean nothing;
+
+    public int getBall() {
+        return ball;
+    }
+
+    public void setBall(int ball) {
+        this.ball = ball;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public void setStrike(int strike) {
+        this.strike = strike;
+    }
+
+    public boolean isNothing() {
+        return nothing;
+    }
+
+    public void setNothing(boolean nothing) {
+        this.nothing = nothing;
+    }
+}
