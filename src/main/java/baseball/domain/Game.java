29e7c3e238294beac111d0ad55617d7175f7522d
JuSeong1130 <jsmoon1130@naver.com>
2023-10-22 19:23:11 +0900

    feat(Game) : 게임 생성(초기화) 기능 구현

---
+++ src/main/java/baseball/domain/Game.java
@@ -0,0 +1,24 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Game {
+
+    private Balls balls;
+
+    GameStatus gameStatus;
+
+    public Game() {
+        init();
+    }
+
+    private void init() {
+        this.balls = createBalls();
+        gameStatus = GameStatus.RUN;
+    }
+
+    private Balls createBalls() {
+        List<Integer> randomNumbers = RandomNumberGenerator.createRandomNumbers(3, 1, 9);
+        return new Balls(randomNumbers);
+    }
+}
