e84189f82746f57cffc3701561e5bae03a23f808
soyoungxxx <otherplet@gmail.com>
2023-10-23 04:15:21 +0900

    fix: PlayerNumber.java 입력 문구 추가

--- src/main/java/baseball/variable/PlayerNumber.java
+++ src/main/java/baseball/variable/PlayerNumber.java
@@ -7,14 +7,16 @@ import java.util.List;
 import java.lang.Integer;
 
 public class PlayerNumber {
+    private static final String INPUT = "숫자를 입력해주세요 : ";
     public List<Integer> playerNumber = new ArrayList<>();
     Validator validator = new Validator();
 
     public void getPlayerNumber() {
+        System.out.print(INPUT);
         String tempString = Console.readLine();
         String[] str = splitString(tempString);
-        for (int i = 0; i < str.length; i++) {
-            int n = Integer.parseInt(str[i]);
+        for (String s : str) {
+            int n = Integer.parseInt(s);
             playerNumber.add(n);
         }
         validator.isValid(playerNumber);
