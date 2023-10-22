1816dd308aeda10873a7acf71d202b35be0e3e90
GO-TE <alsrudxo9810@naver.com>
2023-10-22 16:09:34 +0900

    Feat: 랜덤 번호 생성 호출 기능 구현

---
+++ src/main/java/baseball/Validator.java
@@ -0,0 +1,22 @@
+package baseball;
+
+public class Validator {
+    private static final String STRIKE_MESSAGE = "스트라이크";
+    private static final String BALL_MESSAGE = "볼";
+    private static final String NOTHING_MESSAGE = "낫싱";
+    private static final String WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final int INPUT_LENGTH = 3;
+
+
+    Computer computer = new Computer();
+    User user = new User();
+    private int ballCount = 0;
+    private int strikeCount = 0;
+
+    private void initializeRandomNumber() {
+        this.computer.setComputerNumber();
+    }
+
+
+}
