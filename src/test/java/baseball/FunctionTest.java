532f68eee836ed16d4b154ee6f3b5cbe0102e3cb
wook <dong930112@gmail.com>
2023-10-23 01:15:29 +0900

    test(NumberController): 'compareNumberList' 메서드 기능 테스트 테스트 통과

--- src/test/java/baseball/FunctionTest.java
+++ src/test/java/baseball/FunctionTest.java
@@ -1,5 +1,8 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 
@@ -10,11 +13,19 @@ public class FunctionTest {
     NumberController numberController = new NumberController();
     System.out.println(numberController.generate());
   }
-
   @Test
   void NumberSplitterTest() {
     NumberController numberController = new NumberController();
     System.out.println(numberController.splitter(125));
+  }
+  @Test
+  void CompareNumberListTest() {
+    NumberController numberController = new NumberController();
+    List<Integer> numList1 = new ArrayList<Integer>(Arrays.asList(1,2,3));
+    List<Integer> numList2 = new ArrayList<Integer>(Arrays.asList(7,1,1));
+
+    System.out.println(numberController.compareNumberList(numList1, numList2).get("strike"));
+    System.out.println(numberController.compareNumberList(numList1, numList2).get("ball"));
   }
 
 }
