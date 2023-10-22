ee57102ab311c551224dbe39c11910069571a6e1
GaBaljaintheroom <junsu1222@naver.com>
2023-10-22 23:50:06 +0900

    test: 사용자가 0이 포함한 숫자를 입력하면 에러가 발생한다.

--- src/test/java/baseball/view/InputValidatorTest.java
+++ src/test/java/baseball/view/InputValidatorTest.java
@@ -13,4 +13,11 @@ class InputValidatorTest {
         assertThatThrownBy(() -> inputValidator.validateNumeric("박준수"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    @DisplayName("사용자가 0이 포함한 숫자를 입력하면 에러가 발생한다.")
+    void input_number_zero_error(){
+        assertThatThrownBy(() -> inputValidator.validateNumeric("304"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
