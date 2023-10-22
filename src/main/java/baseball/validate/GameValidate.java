1c3a2e27b5527f9db30e18093c8c8ed712aad3e7
chosunghyun18 <krismon@naver.com>
2023-10-22 19:39:59 +0900

    fix : change to throw IllegalArgument Exception

--- src/main/java/baseball/validate/GameValidate.java
+++ src/main/java/baseball/validate/GameValidate.java
@@ -15,7 +15,7 @@ public class GameValidate {
 
     public void checkPlayerGuessNumbers(List<Integer> numbers) {
         if (numbers.size() != 3) {
-            throw new RuntimeException("[ERROR] : Wrong Size ");
+            throw new IllegalArgumentException("[ERROR] : Wrong Size ");
         }
         for (Integer number : numbers) {
             if (number < 0 || number > 10) {
