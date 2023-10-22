e37560fb1213efeacea76410fad053092b825591
minnim1010 <pms1010123@daum.net>
2023-10-23 05:09:53 +0900

    feat: CLI 출력 기능 구현

---
+++ src/main/java/baseball/io/OutputWriter.java
@@ -0,0 +1,12 @@
+package baseball.io;
+
+public class OutputWriter {
+
+    public void write(String message) {
+        System.out.print(message);
+    }
+
+    public void writeLine(String message) {
+        System.out.println(message);
+    }
+}
