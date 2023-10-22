7a482691b21c7b134c415d750dfc64ffb0c05e03
twoosky <lhjae3@naver.com>
2023-10-23 01:58:45 +0900

    fix: pickNumberInRange 사용해 NumberGenerator 구현

--- src/main/java/baseball/utils/RandomNumberGenerator.java
+++ src/main/java/baseball/utils/RandomNumberGenerator.java
@@ -1,14 +1,21 @@
 package baseball.utils;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
-public class RandomNumberGenerator {
-    public static final int NUMBER_SIZE = 3;
+public class RandomNumberGenerator implements NumberGenerator {
+    public static final int NUMBER_LENGTH = 3;
     public static final int MIN_RANGE = 1;
     public static final int MAX_RANGE = 9;
 
-    public static List<Integer> generate() {
-        return Randoms.pickUniqueNumbersInRange(MIN_RANGE, MAX_RANGE, NUMBER_SIZE);
+    @Override
+    public List<Integer> generate() {
+        HashSet<Integer> picks = new HashSet<>();
+        while (picks.size() < NUMBER_LENGTH) {
+            picks.add(Randoms.pickNumberInRange(MIN_RANGE, MAX_RANGE));
+        }
+        return new ArrayList<>(picks);
     }
 }
