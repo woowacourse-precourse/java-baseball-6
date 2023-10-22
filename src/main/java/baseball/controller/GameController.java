600e53e0f438b6937d59ffd2077a7e4c9ac4d63f
NAM GIBEOM <rlqja0523@naver.com>
2023-10-22 23:59:23 +0900

    feat(Game): 3스트라이크일 때 게임 재시작 또는 종료하는 기능 추가

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,20 +1,42 @@
 package baseball.controller;
 
+import baseball.domain.GameButton;
 import baseball.domain.Pitch;
+import baseball.dto.Inning;
 import baseball.factory.PitchFactory;
 import baseball.ui.InputView;
+import baseball.ui.OutputView;
 
 public class GameController {
 	private final InputView inputView;
 	private final PitchFactory pitchFactory;
+	private final OutputView outputView;
 
-	public GameController(InputView inputView, PitchFactory pitchFactory) {
+	public GameController(InputView inputView, PitchFactory pitchFactory, OutputView outputView) {
 		this.inputView = inputView;
 		this.pitchFactory = pitchFactory;
+		this.outputView = outputView;
 	}
 
 	private Pitch getPlayer() {
 		String input = inputView.scanNumbers();
 		return pitchFactory.create(input);
+	}
+
+	private Pitch playNewGame(Inning inning, GameButton gameButton, Pitch computer) {
+		if (inning.isThreeStrike()) {
+			outputView.printGameEndMessage();
+			String input = inputView.scanRestartNumber();
+			gameButton.changeButton(input);
+			computer = createNewComputer(gameButton, computer);
+		}
+		return computer;
+	}
+
+	private Pitch createNewComputer(GameButton gameButton, Pitch computer) {
+		if (gameButton.isPlay()) {
+			computer = pitchFactory.create();
+		}
+		return computer;
 	}
 }
