dd5bddff0d66546698547574c6c69d2d33a5c92c
박정현 <ros28081@naver.com>
2023-10-22 19:09:32 +0900

    feat: 랜덤 숫자 3개 생성 기능 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,10 +1,31 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
 
     // generateNum
     public static final int SIZE = 3;
     public static final int MIN_NUM = 3;
     public static final int MAX_NUM = 3;
-    
+
+    List<Integer> answerNum;
+
+    public void startGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        answerNum = generateAnswer();
+    }
+
+    public List<Integer> generateAnswer() {
+        List<Integer> answer = new ArrayList<>();
+        while (answer.size() < SIZE) {
+            int randomNum = Randoms.pickNumberInRange(MIN_NUM, MAX_NUM);
+            if (!answer.contains(randomNum)) {
+                answer.add(randomNum);
+            }
+        }
+        return answer;
+    }
 }
