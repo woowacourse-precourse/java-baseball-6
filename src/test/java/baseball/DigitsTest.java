8528d7f80553f401c9e21f47ca14ddda10b3ac53
yubin <dbqls2821@gmail.com>
2023-10-22 15:50:00 +0900

    test: 랜덤으로 생성된 Digits는 유니크한 세자리 숫자리를 가지도록 하는 테스트코드 추가

--- src/test/java/baseball/DigitsTest.java
+++ src/test/java/baseball/DigitsTest.java
@@ -1,10 +1,40 @@
 package baseball;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mockStatic;
 
 class DigitsTest {
+
+    @Test
+    void 랜덤으로_생성된_Digits는_유니크한_세자리_숫자를_가진다() {
+
+        // given
+        Digits comparisonDigits = Digits.generateFixedDigits("123");
+        Digit digit1 = Digit.generateFixedDigit(1);
+        Digit digit2 = Digit.generateFixedDigit(2);
+        Digit digit3 = Digit.generateFixedDigit(3);
+        try (MockedStatic<Digit> mDigit = mockStatic(Digit.class)) {
+            mDigit.when(Digit::generateRandomDigit)
+                    .thenReturn(digit1)
+                    .thenReturn(digit1)
+                    .thenReturn(digit1)
+                    .thenReturn(digit2)
+                    .thenReturn(digit3);
+
+            // when
+            Digits digits = Digits.generateRandomDigits();
+
+            // then
+            assertEquals(digits, comparisonDigits);
+        }
+    }
 
     @Test
     void 입력값의_ball_점수를_count_한다() {
