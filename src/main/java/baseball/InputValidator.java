850a436b8f55a05a8d36016380d6b0ae53a8c015
gyulim5 <ohrbfla1004@khu.ac.kr>
2023-10-22 20:41:26 +0900

    ✨ Feat: input 유효성 검사 로직 수정

--- src/main/java/baseball/InputValidator.java
+++ src/main/java/baseball/InputValidator.java
@@ -1,31 +1,33 @@
 package baseball;
 
 public class InputValidator {
-    public boolean checkInput(String input){
-        int userInput = getIntegerValue(input);
-
-        return isInputThreeNumbers(input) && isDuplicated(input) && userInput != -1;
+    public boolean isValidInput(String input){
+        return isInputThreeNumbers(input) && isDuplicated(input) && isDigitStringInRange(input);
     }
 
     private boolean isInputThreeNumbers(String input) {
         return input.length() == 3;
     }
 
-    private int getIntegerValue(String input) {
+    private boolean isDigitStringInRange(String input) {
         try {
-            return Integer.parseInt(input);
+            if (input.contains("0")) {
+                return false;
+            }
+            Integer.parseInt(input);
+            return true;
         }
         catch (Exception ex) {
-            return -1;
+            return false;
         }
     }
 
     private boolean isDuplicated(String input) {
-        boolean[] numberArray = new boolean[10];
+        boolean[] numberArray = new boolean[9];
 
         for(int index = 0; index < input.length(); index++)
         {
-            int number = input.charAt(index)-'0';
+            int number = input.charAt(index)-'1';
             if (numberArray[number]) {
                 return false;
             }
