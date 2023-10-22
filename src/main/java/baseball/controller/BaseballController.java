f0988e73ef680eb741703e54b342d6f2beb40963
hong <kevin0928@naver.com>
2023-10-22 21:43:41 +0900

    feat: Controller에 service,view 객체 생성

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -1,4 +1,11 @@
 package baseball.controller;
 
+import baseball.service.BaseballService;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class BaseballController {
+    private final BaseballService baseballService = new BaseballService();
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
 }
