a77e2e8aeccabe40ff6ac2cdc7858d8f32813fad
DaeYoungK <hanguk830@naver.com>
2023-10-22 16:26:10 +0900

    fix(InputView): 플레이어가 입력하는 예상 숫자를 String타입으로 반환하도록 변경

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -6,9 +6,9 @@ public class InputView {
     private final String INPUT_NUMBER = "숫자를 입력해주세요 : ";
     private final String RETRY_NUMBER = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
-    public int print_input_message() {
+    public String print_input_message() {
         System.out.print(INPUT_NUMBER);
-        return Integer.parseInt(Console.readLine());
+        return Console.readLine();
     }
 
     public int print_retry_message() {
