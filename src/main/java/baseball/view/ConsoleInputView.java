81e25e0dc362cfdcc569454e939a491a3bb2528a
eom-tae-in <eti0728@daum.net>
2023-10-22 22:32:22 +0900

    refactor: ConsoleInputView에서 부모의 메서드를 구현하여 사용하도록 변경

--- src/main/java/baseball/view/ConsoleInputView.java
+++ src/main/java/baseball/view/ConsoleInputView.java
@@ -1,31 +1,20 @@
 package baseball.view;
 
-import static java.util.regex.Pattern.matches;
-
 import baseball.exception.consoleView.ConsoleInputBlankException;
-import baseball.exception.consoleView.ConsoleInputCanNotChangeToIntException;
 import camp.nextstep.edu.missionutils.Console;
 
 public class ConsoleInputView implements InputView {
 
-    private static final String REGEXP_PATTERN_NUMBER = "^[\\d]*$";
-
-    public String readStringCanConvertInt() {
-        String guessNumbers = Console.readLine();
-        checkIsBlank(guessNumbers);
-        checkCanChangeToInteger(guessNumbers);
-        return guessNumbers;
+    @Override
+    public String readLine() {
+        String text = Console.readLine();
+        checkIsBlank(text);
+        return text;
     }
 
     private void checkIsBlank(final String string) {
         if (string.isBlank()) {
             throw new ConsoleInputBlankException();
-        }
-    }
-
-    private void checkCanChangeToInteger(final String string) {
-        if (!matches(REGEXP_PATTERN_NUMBER, string)) {
-            throw new ConsoleInputCanNotChangeToIntException(string);
         }
     }
 }
--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,4 +1,5 @@
 package baseball.view;
 
 public interface InputView {
+    String readLine();
 }
