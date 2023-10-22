e506754e0456325cbdf5c1b5b5817a7f6cbcf213
masonkimseoul <masonkimseoul@gmail.com>
2023-10-22 23:05:52 +0900

    feat : (Application.java) Add chooseRandomNumbers function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    }
+
+    static List<Integer> chooseRandomNumbers() {
+        List<Integer> computerNumbers = new ArrayList<>();
+
+        while (computerNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumbers.contains(randomNumber)) {
+                computerNumbers.add(randomNumber);
+            }
+        }
+
+        return computerNumbers;
     }
 }
