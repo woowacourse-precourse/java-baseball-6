a55326c4f92f05762f506a2b7dd8b2417eb5555c
haiseong <wjdgotjd9908@gmail.com>
2023-10-22 21:27:56 +0900

    refactor: generateAnswer: Set을 이용 -> List를 이용

--- src/main/java/baseball/controller/AnswerGenerator.java
+++ src/main/java/baseball/controller/AnswerGenerator.java
@@ -2,19 +2,32 @@ package baseball.controller;
 
 import static baseball.model.GameNumbers.GAME_NUMBERS_SIZE;
 
+import baseball.model.Game;
 import baseball.model.GameNumbers;
 import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 public class AnswerGenerator {
+    private static final int START_INCLUSIVE = 1;
+    private static final int END_INCLUSIVE = 9;
+
     public GameNumbers generateAnswer() {
-        Set<Integer> set = new HashSet<>(3);
-        while (set.size() < GAME_NUMBERS_SIZE) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            set.add(randomNumber);
+        List<Integer> list = new ArrayList<>(GAME_NUMBERS_SIZE);
+        for (int i = 0; i < GAME_NUMBERS_SIZE; i++) {
+            int randomNumber = getRandomValueNotInList(list, START_INCLUSIVE, END_INCLUSIVE);
+            list.add(randomNumber);
         }
-        return new GameNumbers(new ArrayList<>(set));
+        return new GameNumbers(list);
+    }
+
+    private static int getRandomValueNotInList(List<Integer> list, final int startInclusive,
+            final int endInclusive) {
+        int randomNumber = Randoms.pickNumberInRange(startInclusive, endInclusive);
+        while (list.contains(randomNumber)) {
+            randomNumber = Randoms.pickNumberInRange(startInclusive, endInclusive);
+        }
+        return randomNumber;
     }
 }
