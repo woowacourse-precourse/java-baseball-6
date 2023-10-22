70368c2e01520b484cf4225db9e4d4536ecfd73c
wlgh1553 <112055561+wlgh1553@users.noreply.github.com>
2023-10-22 21:47:23 +0900

    [FEAT] 메시지 출력 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,7 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        
+        GameManager baseballGame = new GameManager();
+        baseballGame.gameStart();
     }
 }
