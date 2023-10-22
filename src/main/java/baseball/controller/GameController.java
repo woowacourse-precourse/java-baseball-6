c038335b1a0d766aa55dad67924e4b600218dc15
changhyun9 <ckdgus1713@naver.com>
2023-10-22 23:19:07 +0900

    Feat - GameController 애플리케이션 진행 기능 구현

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,4 +1,58 @@
 package baseball.controller;
 
-public class gameController {
+import baseball.service.ComputerService;
+import baseball.service.JudgeService;
+import baseball.service.UserService;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
+public class GameController {
+    private ComputerService computerService;
+    private JudgeService judgeService;
+    private UserService userService;
+
+    public GameController() {
+        computerService = new ComputerService();
+        judgeService = new JudgeService();
+        userService = new UserService();
+    }
+
+    public void playGame() {
+        List<Integer> computerNumber = computerService.generateRandom();
+        boolean threeStrike = false;
+
+        while (!threeStrike) {
+            System.out.print("숫자를 입력해주세요 : ");
+            List<Integer> userNumber = userService.generateInput();
+            judgeService.judgeResult(computerNumber, userNumber);
+            judgeService.printResult();
+
+            threeStrike = judgeService.checkThree();
+            statusReset();
+        }
+    }
+
+    private void statusReset() {
+        judgeService.resetResult();
+        userService.resetInput();
+    }
+
+    public boolean check() {
+        String s = Console.readLine();
+        switch (s) {
+            case "1":
+                randomReset();
+                return false;
+            case "2":
+                return true;
+            default:
+                throw new IllegalArgumentException("Invalid input");
+        }
+    }
+
+    private void randomReset() {
+        computerService.resetRandom();
+    }
+
+
 }
