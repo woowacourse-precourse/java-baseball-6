fc66b427f3a8f2f2a4e2666a85bff3be9d908c8e
wonjun <wonjon0930@daum.net>
2023-10-22 12:35:01 +0900

    feat: add frame to implement feature flow

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,7 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+       Game game = new Game();
+//       game.init(); // Test 중이라 실행 안됨. ( 무한 루프 )
     }
 }
--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -6,5 +6,29 @@ public class Game {
     }
     private Status status;
 
+    public void init(){
+        this.status = Status.START;
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while(status != Status.TERMINATED){
+            switch (status) {
+                case START -> start();
+                case PROGRESS -> progress();
+                case FINISH -> finish();
+            }
+        }
+    }
+
+    private void start(){
+
+    }
+    private void progress(){
+
+    }
+    private void finish(){
+
+    }
+    private void terminate(){
+        this.status = Status.TERMINATED;
+    }
 
 }
