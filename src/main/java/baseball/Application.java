c3abbdba0621ff893606410ff953fa5a434f5000
kunsanglee <gosmdochee2@gmail.com>
2023-10-22 19:13:05 +0900

    refactor(Application): 의존 클래스명 변경 BaseballController 클래스를 생성할 때 매개변수로 넣어주던 RandomNumbersGenerator의 클래스명을 변경하여 수정사항 반영.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,13 +1,13 @@
 package baseball;
 
 import baseball.controller.BaseballController;
-import baseball.domain.RandomNumbersGenerator;
+import baseball.domain.RandomBaseballNumbersGenerator;
 import baseball.view.BaseballInputView;
 import baseball.view.BaseballOutputView;
 
 public class Application {
     public static void main(String[] args) {
-        BaseballController baseballController = new BaseballController(new RandomNumbersGenerator(),
+        BaseballController baseballController = new BaseballController(new RandomBaseballNumbersGenerator(),
                 new BaseballInputView(),
                 new BaseballOutputView());
         baseballController.run();
