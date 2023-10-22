2cbd026a3e78dde92659ab24471fa5b12fa0c289
minnim1010 <pms1010123@daum.net>
2023-10-23 06:55:10 +0900

    feat(model): 숫자 야구의 숫자 클래스 구현

---
+++ src/main/java/baseball/model/BaseballNumber.java
@@ -0,0 +1,50 @@
+package baseball.model;
+
+public class BaseballNumber {
+
+    public static final int MIN_VALUE = 1;
+    public static final int MAX_VALUE = 9;
+
+    private static final String ERROR_MESSAGE = "1 이상 9 이하 값을 가져야 합니다.";
+
+    private final int number;
+
+    public BaseballNumber(int number) {
+        validateRange(number);
+
+        this.number = number;
+    }
+
+    private static boolean isValidRange(int number) {
+        return MIN_VALUE <= number && number <= MAX_VALUE;
+    }
+
+    private void validateRange(int number) {
+        if (!isValidRange(number)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        BaseballNumber that = (BaseballNumber) o;
+
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return number;
+    }
+}
