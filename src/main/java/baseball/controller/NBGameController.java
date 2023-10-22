ae7de950c90b551b4413d5175efe8e013e649a61
baksakcci <pshsh910@gmail.com>
2023-10-23 00:30:00 +0900

    refactor($NBGameController): 타입 변화에 따른 출력 메서드 변경

--- src/main/java/baseball/controller/NBGameController.java
+++ src/main/java/baseball/controller/NBGameController.java
@@ -2,14 +2,15 @@ package baseball.controller;
 
 import baseball.model.Computer;
 import baseball.view.OutputView;
+import java.util.List;
 
 public class NBGameController {
     public void start() {
         OutputView out = new OutputView();
-        out.print("숫자 야구 게임을 시작합니다.");
+        out.displayString("숫자 야구 게임을 시작합니다.");
 
         Computer computer = Computer.generateRandomNumber();
 
-        out.print(computer.getComputerNumber());
+        out.displayList(computer.getComputerNumber());
     }
 }
