dba103ba8c4741bbc1aa3dc066dde8be78d32805
eom-tae-in <eti0728@daum.net>
2023-10-22 18:05:20 +0900

    test: ResultMapper의 테스트 구현

---
+++ src/test/java/baseball/contoller/mapper/ResultMapperTest.java
@@ -0,0 +1,28 @@
+package baseball.contoller.mapper;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import baseball.controller.mapper.ResultMapper;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+@DisplayNameGeneration(ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+class ResultMapperTest {
+
+    @CsvSource({"1, 1, 1볼 1스트라이크", "2, 1, 2볼 1스트라이크", "0, 0, 낫싱"})
+    @ParameterizedTest
+    void 스트라이크와_볼으로_문자열_결과를_만든다(int ballCont, int strikeCount, String expected) {
+        // given
+        ResultMapper resultMapper = new ResultMapper();
+
+        // when
+        String result = resultMapper.makeResult(ballCont, strikeCount);
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+}
+
