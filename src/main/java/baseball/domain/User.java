2ff9438782a9f6e382bf9eb4c6d1b8540e5e835e
junseoks-99 <angec5645@naver.com>
2023-10-23 00:11:43 +0900

    feat: "User 변수, 생성자 추가"

---
+++ src/main/java/baseball/domain/User.java
@@ -0,0 +1,13 @@
+package baseball.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+
+    private List<Integer> number;
+
+    public User() {
+        this.number = new ArrayList<Integer>();
+    }
+}
