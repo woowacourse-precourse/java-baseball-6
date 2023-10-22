799e0742493a2792ee80f88f1b739c2ced9871b5
kyeoungchan <wkc1002@naver.com>
2023-10-22 15:12:21 +0900

    fix: 컨트롤러를 메인 메서드 내부에서 생성하는 것으로 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,10 +3,9 @@ package baseball;
 import baseball.controller.Controller;
 
 public class Application {
-    private static final Controller controller = new Controller();
-
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        final Controller controller = new Controller();
         controller.run();
     }
 }
