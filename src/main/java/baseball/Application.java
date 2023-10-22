d844224d7f22b1ba3a2f491a021b5c163264f9be
eunjin3481 <eunjin3481@gmail.com>
2023-10-22 21:29:36 +0900

    fix: main 메서드 불필요한 문구 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,7 +4,6 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
         boolean runFlag = true;
 
@@ -21,5 +20,4 @@ public class Application {
         // 예외처리
         return userInput == 1;
     }
-}
 }
