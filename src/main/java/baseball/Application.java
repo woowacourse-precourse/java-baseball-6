182e9fed1ffc661bd43fa0af20f7f2dad83ffad4
Jisu Lee <wltn12047323@gmail.com>
2023-10-22 19:16:36 +0900

    게임시작, 종료 숫자 입력받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,6 +12,7 @@ public class Application {
         // TODO: 프로그램 구현
         List<Integer> computer = new ArrayList<>();
         List<Integer> user = new ArrayList<>();
+        String isContinue = "1";
         String input;
         String output;
         int ball, strike;
@@ -20,51 +21,56 @@ public class Application {
 
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-        //1. 랜덤한 3개의 수 생성 하기
-        while(computer.size() < 3){
-            n = Randoms.pickNumberInRange(1, 9);
-            if(!computer.contains(n))
-                computer.add(n);
-        }
-        //System.out.println("컴퓨터 숫자 확인용: " + computer);
-
-        //2. 사용자 값 입력 받기
-        while(!computer.equals(user)) {
-            user.clear();
-
-            System.out.print("숫자를 입력해주세요 : ");
-            input = Console.readLine();
-            for(i = 0; i < 3; i++) {
-                user.add((Integer) (input.charAt(i) - '0'));
+        //4. 게임 시작, 종료를 위한 숫자 입력 받기
+        while(isContinue.equals("1")) {
+            //1. 랜덤한 3개의 수 생성 하기
+            computer.clear();
+            while (computer.size() < 3) {
+                n = Randoms.pickNumberInRange(1, 9);
+                if (!computer.contains(n))
+                    computer.add(n);
             }
+            System.out.println("컴퓨터 숫자 확인용: " + computer);
+            //2. 사용자 값 입력 받기
+            while (!computer.equals(user)) {
+                user.clear();
 
-            //3. 볼의 개수, 스트라이크 개수 출력 하기
-            output = "";
-            ball = 0;
-            strike = 0;
-            for(i = 0; i < 3; i++){
-                if(computer.contains(user.get(i))) {
-                    if(computer.get(i).equals(user.get(i)))
-                        strike++;
-                    else
-                        ball++;
+                System.out.print("숫자를 입력해주세요 : ");
+                input = Console.readLine();
+                for (i = 0; i < 3; i++) {
+                    user.add((Integer) (input.charAt(i) - '0'));
                 }
-            }
-            if (ball == 0 && strike == 0)
-                output += "낫싱";
-            else {
-                if (ball >= 1)
-                    output += ball + "볼";
-                if (ball >= 1 && strike >= 1)
-                    output += " ";
-                if (strike >= 1)
-                    output += strike + "스트라이크";
 
-                if (strike == 3)
-                    output += "\n3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+                //3. 볼의 개수, 스트라이크 개수 출력 하기
+                output = "";
+                ball = 0;
+                strike = 0;
+                for (i = 0; i < 3; i++) {
+                    if (computer.contains(user.get(i))) {
+                        if (computer.get(i).equals(user.get(i)))
+                            strike++;
+                        else
+                            ball++;
+                    }
+                }
+                if (ball == 0 && strike == 0)
+                    output += "낫싱";
+                else {
+                    if (ball >= 1)
+                        output += ball + "볼";
+                    if (ball >= 1 && strike >= 1)
+                        output += " ";
+                    if (strike >= 1)
+                        output += strike + "스트라이크";
+
+                    if (strike == 3)
+                        output += "\n3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+                }
+                //System.out.println("볼/스트라이크 확인용: " + ball + " / " + strike);
+                System.out.println(output);
             }
-            //System.out.println("볼/스트라이크 확인용: " + ball + " / " + strike);
-            System.out.println(output);
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            isContinue = Console.readLine();
         }
     }
 }
