dd0cd8821df2dcb6813823e37c7eda00dd2c592f
windfall0369 <tlsehdrl4340@naver.com>
2023-10-23 02:33:32 +0900

    코드 리펙토링

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,23 +1,30 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.logging.Logger;
 
 
 public class Application {
+
+    private static final String RESTART_GAME = "1";
+    private static final String CLOSE_GAME = "2";
+    public static final Logger LOG = Logger.getGlobal();
+
+    //변수명은 리펙토링(관점의 차이) 남이 내 코드를 알 수 있는가? 1이 뭐고 2가 뭔데?
+
+
     public static void main(String[] args) {
-        Baseball baseball = new Baseball();
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        while (true) {
-            baseball.playBaseball();
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            String play = Console.readLine();
-            if (play.equals("1")) {
+        Baseball.playBaseball();
+        LOG.info("숫자 야구 게임을 시작합니다.");
+        String replayChoose = "1";
+        while (RESTART_GAME.equals(replayChoose)) {
+            Baseball.playBaseball();
+            LOG.info("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            replayChoose = Console.readLine();
+            if (replayChoose.equals(RESTART_GAME)) {
                 continue;
             }
-            if (play.equals("2")) {
+            if (replayChoose.equals(CLOSE_GAME)) {
                 return;
             }
         }
--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -6,8 +6,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Baseball {
-    //랜덤값 생성
-    private static List<Integer> generateValue() {
+
+    //랜덤값 생성 ex. 외부참조 하는가? private or 그 외
+    //변수명을 명확하게, 변수명을 고민했는가? -> 디테일
+
+
+    private static List<Integer> generateRandom() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -18,52 +22,62 @@ public class Baseball {
         return computer;
     }
 
-    //사용자 입력 받음
-    void playBaseball() {
-        List<Integer> computer = generateValue();
-        int strike = 0;
-        int ball = 0;
-        while (strike != 3) {
-            System.out.println("숫자를 입력해주세요 : ");
-            String user = Console.readLine();
-            handleException(user);
-            strike = 0;
-            ball = 0;
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    if (computer.get(i) == Integer.parseInt(String.valueOf(user.charAt(j)))) {
-                        if (i == j) {
-                            strike++;
+
+    //메서드명이 중위적, 명확한 기능
+    //이름을 고치든, 함수를 쪼개든
+    //하나의 기능만 구현
+    public static void playBaseball() {
+        List<Integer> computer = generateRandom();
+        compareNumber(computer);
+    }
+
+    private static void compareNumber(List<Integer> computer) {
+        int strikeCount = 0;
+        int ballCount = 0;
+        while (strikeCount != 3) {
+            String user = getUserNum();
+            handleException(user); //여기서 catch 처리, logger로 처리
+            strikeCount = 0;
+            ballCount = 0;
+            for (int computerNum = 0; computerNum < 3; computerNum++) {
+                for (int userNum = 0; userNum < 3; userNum++) {
+                    if (computer.get(computerNum) == Integer.parseInt(String.valueOf(user.charAt(userNum)))) {
+                        if (computerNum == userNum) {
+                            strikeCount++;
                         } else {
-                            ball++;
+                            ballCount++;
                         }
                     }
                 }
             }
-            if (countingBall(strike, ball)) {
+            if (countingBall(strikeCount, ballCount)) {
                 break;
             }
         }
     }
 
-    //스트라이크, 볼 처리
-    boolean countingBall(int strike, int ball) {
+    private static String getUserNum() {
+        Application.LOG.info("숫자를 입력해주세요 : ");
+        return Console.readLine();
+    }
+
+    private static boolean countingBall(int strike, int ball) {
         if (strike == 3) {
-            System.out.println("3스트라이크");
-            System.out.println("3개의 숫자를 모두 맞추셨습니다! 게임 종료");
+            Application.LOG.info("3스트라이크");
+            Application.LOG.info("3개의 숫자를 모두 맞추셨습니다! 게임 종료");
             return true;
         }
         if (strike == 0 && ball == 0) {
-            System.out.println("낫싱");
+            Application.LOG.info("낫싱");
         }
         if (strike != 0 && ball != 0) {
-            System.out.println(ball + "볼 " + strike + "스트라이크");
+            Application.LOG.info(ball + "볼 " + strike + "스트라이크");
         }
         return false;
     }
 
-    //사용자의 잘못된 입력 IllegalArgumentException 처리
-    void handleException(String user) {
+    //try-catch, catch를 했는지, 에러 처리 handleException에서 처리
+    private static void handleException(String user) {
         if (user.length() != 3) {
             throw new IllegalArgumentException();
         }
@@ -82,3 +96,5 @@ public class Baseball {
         }
     }
 }
+
+
