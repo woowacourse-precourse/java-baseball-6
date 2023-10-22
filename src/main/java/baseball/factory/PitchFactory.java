5fec9d5b1418cdc856cf10f47365039c8f3d3c50
NAM GIBEOM <rlqja0523@naver.com>
2023-10-22 23:51:26 +0900

    feat(PitchFactory): string으로 pitch 생성하는 기능 추가

--- src/main/java/baseball/factory/PitchFactory.java
+++ src/main/java/baseball/factory/PitchFactory.java
@@ -1,16 +1,26 @@
 package baseball.factory;
 
+import java.util.List;
+
+import baseball.converter.ValueConverter;
 import baseball.domain.Pitch;
 import baseball.generator.NumberGenerator;
 
 public class PitchFactory {
     private final NumberGenerator numberGenerator;
+    private final ValueConverter converter;
 
-    public PitchFactory(NumberGenerator numberGenerator) {
+    public PitchFactory(NumberGenerator numberGenerator, ValueConverter converter) {
         this.numberGenerator = numberGenerator;
+        this.converter = converter;
     }
 
     public Pitch create() {
         return new Pitch(numberGenerator.generate());
+    }
+
+    public Pitch create(String input) {
+        List<Integer> numbers = converter.convertToList(input);
+        return new Pitch(numbers);
     }
 }
