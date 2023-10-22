6ef22842c1ff720601f18715a03eb87910b0adfb
yunju-m <dbswnfnal@naver.com>
2023-10-22 15:48:19 +0900

    feat(ComputerController): 추가 세 자리 난수와 플레이어 수 비교하여 힌트 함수

--- src/main/java/baseball/controller/ComputerController.java
+++ src/main/java/baseball/controller/ComputerController.java
@@ -1,17 +1,49 @@
 package baseball.controller;
 
+import java.util.Arrays;
+
 import baseball.model.GameNumber;
 import baseball.view.InputView;
 import baseball.view.OutputView;
+import camp.nextstep.edu.missionutils.Console;
 
 public class ComputerController {
     GameNumber gameNumber = new GameNumber();
     OutputView outputView = new OutputView();
     InputView inputView = new InputView();
+    
+    private static final int BASEBALL_NUMBER_LENGTH = 3;
+    private int strikeCount;
+    private int ballCount;
 
     public void startGame() {
         outputView.printStartGame();
         gameNumber.setRandomNumbers();
-        inputView.enterGameNumber();
+        
+        String inputNumber = Console.readLine();
+        gameNumber.setInputNumber(inputNumber);
+        int[] playerNumbers = gameNumber.getPlayerNumbers();
+        System.out.println("playerNumbers: " + Arrays.toString(playerNumbers));
+
+        for (int i = 0; i < BASEBALL_NUMBER_LENGTH; i++) {
+            countStrike(playerNumbers[i], i);
+            countBall(playerNumbers[i], i);
+        }
+
+        System.out.println("strikeCount: " + strikeCount);
+        System.out.println("ballCount: " + ballCount);
+    }
+
+    private void countStrike(int playerNumber, int numberIndex) {
+        if (gameNumber.getRandomrNumbers()[numberIndex] == playerNumber) {
+            strikeCount++;
+        }
+    }
+
+    private void countBall(int playerNumber, int numberIndex) {
+        int[] randomNumbers = gameNumber.getRandomrNumbers();
+        if ((randomNumbers[numberIndex] != playerNumber) && Arrays.stream(randomNumbers).anyMatch(num -> num == playerNumber)) {
+            ballCount++;
+        } 
     }
 }
