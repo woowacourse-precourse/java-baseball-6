cc52adcd8226adb83c112243fd0b515273a37832
twoosky <lhjae3@naver.com>
2023-10-23 01:57:58 +0900

    feat: integerConvertor 구현

---
+++ src/main/java/baseball/utils/IntegerConvertor.java
@@ -0,0 +1,14 @@
+package baseball.utils;
+
+public class IntegerConvertor {
+    private IntegerConvertor() {
+    }
+
+    public static int toInteger(String value) {
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessages.NOT_INTEGER);
+        }
+    }
+}
