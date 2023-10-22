d9ab01e8d2da52cf3f4e713bf43a7e9e421ce4c2
Dongwoongkim <rlaehddnd0422@naver.com>
2023-10-22 17:10:35 +0900

    refactor : (BaseballGameController) playOneRound while 조건문 수정

--- src/main/java/baseball/controller/BaseballGameController.java
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -10,6 +10,8 @@ import java.util.List;
 public class BaseballGameController {
     private static final String START_MSG = "숫자 야구 게임을 시작합니다.";
     private static final Integer EXIT = 2;
+    private static final Integer INIT_STRIKE = 0;
+    private static final Integer INIT_BALL = 0;
     private final Computer computer = new Computer();
     private final Player player = new Player();
     private final InputView inputView = new InputView();
@@ -18,20 +20,17 @@ public class BaseballGameController {
     public void start() {
         printStartMessage();
         while (player.getRestartOrExit() != EXIT) {
-            playOneRound(computer.generateNumber());
+            playOneRound(computer.generateNumber(), new Score(INIT_STRIKE, INIT_BALL));
             player.inputRestartOrExit();
         }
     }
 
-    private void playOneRound(List<Integer> answer) {
-        while (true) {
+    private void playOneRound(List<Integer> answer, Score score) {
+        while (!score.isAllStrike()) {
             inputView.printSuggestNumberMessage();
             player.inputNumbers();
-            Score score = computer.calculateScore(player.getNumbers(), answer);
+            score = computer.calculateScore(player.getNumbers(), answer);
             outputView.printResult(score);
-            if (score.isAllStrike()) {
-                break;
-            }
         }
         inputView.printRestartOrExitMessage();
     }
