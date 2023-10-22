6d03e79644987adde5804271d2692b92507afcb3
Junobee25 <wnsdhqo@naver.com>
2023-10-22 16:52:28 +0900

    feat: compareMyInputNumberAndComputerNumber 메서드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,43 @@
 package baseball;
 
+
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+
+    public void compareMyInputNumberAndComputerNumber(String myNumber, List<Integer> computerNumber) {
+        int strike = 0;
+        int ball = 0;
+
+        for (int i =0; i<myNumber.length(); i++){
+            if (myNumber.charAt(i) == computerNumber.get(i)){
+                strike++;
+            }
+            else if (computerNumber.contains(myNumber.charAt(i)) && computerNumber.indexOf(myNumber.charAt(i)) != i) {
+                ball++;
+            }
+        }
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        String myNumber = Console.readLine();
+
+        List<Integer> computerNumber = new ArrayList<>();
+
+        while (computerNumber.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if(!computerNumber.contains(randomNumber)){
+                computerNumber.add(randomNumber);
+            }
+        }
+
+
     }
 }
