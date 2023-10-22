589a90a8c4440e1766f36999e81fac93e765f64f
박형균 <phk@baghyeong-gyun-ui-MacBookPro.local>
2023-10-22 14:44:58 +0900

    refactor: modify method and variable
    *BaseballGameService를 참조하도록 로직 변경
    *변수 소문자로 변경

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -1,9 +1,8 @@
 package baseball.controller;
 
-import baseball.service.BallStrikeCheckService;
+import baseball.model.BaseballGameResult;
+import baseball.service.BaseballGameService;
 import baseball.view.InputView;
-import baseball.service.ComNumberGenerateService;
-import baseball.service.UserNumberGenerateService;
 import baseball.view.OutputView;
 
 import java.util.List;
@@ -11,57 +10,48 @@ import java.util.Map;
 
 public class BaseballController {
 
-    private final ComNumberGenerateService comNumberGenerateService = new ComNumberGenerateService();
-    private final UserNumberGenerateService userNumberGenerateService = new UserNumberGenerateService();
-    private final BallStrikeCheckService ballStrikeCheckService = new BallStrikeCheckService();
-    private int BALL;
-    private int STRIKE;
-
+    private final BaseballGameService baseballGameService = new BaseballGameService();
+    private int ball;
+    private int strike;
 
     public void run() {
         do {
+            OutputView.startGame();
             start();
 
-        } while (!isEnd());
+        } while (isRestart());
     }
 
     private void start() {
-        List<Integer> comNumber = comNumberGenerateService.generateComNumber();
-        System.out.println(comNumber);
+        baseballGameService.setComputerNumber();
         do {
             String input = InputView.requestUserNumber();
-            int[] userNumber = userNumberGenerateService.generateUserNumber(input);
-            Map<String, Integer> resultGame = ballStrikeCheckService.resultBallStrike(userNumber, comNumber);
-            BALL = resultGame.get("BALL");
-            STRIKE = resultGame.get("STRIKE");
+            BaseballGameResult gameResult = baseballGameService.getGameResult(input);
+            ball = gameResult.getBall();
+            strike = gameResult.getStrike();
             resultGame();
-
-        } while (STRIKE != 3);
-
-
+        } while (strike != 3);
     }
 
     private void resultGame() {
-        if (BALL != 0 && STRIKE == 0) {
-            OutputView.onlyBall(BALL);
+        if (ball != 0 && strike == 0) {
+            OutputView.onlyBall(ball);
         }
-        if (BALL == 0 && STRIKE != 0) {
-            OutputView.onlyStrike(STRIKE);
+        if (ball == 0 && strike != 0) {
+            OutputView.onlyStrike(strike);
         }
-        if (BALL != 0 && STRIKE != 0) {
-            OutputView.ballAndStrike(BALL, STRIKE);
+        if (ball != 0 && strike != 0) {
+            OutputView.ballAndStrike(ball, strike);
         }
-        if (BALL == 0 && STRIKE == 0) {
+        if (ball == 0 && strike == 0) {
             OutputView.nothing();
         }
     }
 
-    private boolean isEnd() {
+    private boolean isRestart() {
         OutputView.endGame();
-        String result = InputView.requestRestartGame();
-        if (result.equals("1")) return false;
-        if (result.equals("2")) return true;
-        throw new IllegalArgumentException();
+        String input = InputView.requestRestartGame();
+        return baseballGameService.wantRestart(input);
     }
 
 }
