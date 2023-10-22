9d789d454d4475c594992c0ffac4744d0a9bee56
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-10-22 21:21:05 +0900

    style(User): Change methods' names

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -13,18 +13,18 @@ public class User {
 
     // 문자열 형식의 입력값을 정수 배열에 저장
     public void setUser(String input) {
-        checkValidInputNumber(input);   // 문자열에 대한 유효성검사 실시
+        checkIfValidInputNumber(input);   // 문자열에 대한 유효성검사 실시
         String[] tokens = input.split("");
         for (String str : tokens) {
             addNumberToArray(Integer.parseInt(str));
         }
     }
 
-    // 숫자로만 이루어진 문자열에 관한 유효성 검사
+    // 숫자로만 이루어진 문자열을 대상으로 하는 유효성 검사
     // 입력값은 Play 클래스의 checkInputHasChar()에서의 유효성 검사를 통과한 문자열
-    private void checkValidInputNumber(String input) {
+    private void checkIfValidInputNumber(String input) {
         checkInputLength(input);    // 문자열 길이 검사
-        checkInputHasZero(input);   // 0 포함 여부 검사
+        checkIfInputHasZero(input);   // 0 포함 여부 검사
     }
 
     // 입력된 문자열의 길이가 3인지 검사
@@ -35,9 +35,9 @@ public class User {
     }
 
     // 입력된 문자열에 0이 포함되었는지 검사
-    // 이 유효성검사는 입력값은 3자리 숫자인 문자열을 대상으로 수행
+    // 이 유효성검사는 3자리 숫자인 입력값을 대상으로 수행
     // 따라서 이 검사를 통과하는 문자열은 1~9 사이 숫자로만 구성된 3자리 수
-    private void checkInputHasZero(String input) {
+    private void checkIfInputHasZero(String input) {
         if (input.contains("0")) {
             throw new IllegalArgumentException("This input includes zero.");
         }
