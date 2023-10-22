0d8893d57b57f3460bdfdca882d4e4731c9394ae
Dongwoongkim <rlaehddnd0422@naver.com>
2023-10-22 15:34:10 +0900

    refactor : (Computer) 난수 생성 기능 추가

--- src/main/java/baseball/controller/BaseballGameController.java
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -1,5 +1,4 @@
 package baseball.controller;
 
 public class BaseballGameController {
-    
 }
--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -2,9 +2,14 @@ package baseball.model;
 
 import baseball.util.NumberGenerator;
 import baseball.util.ScoreCalculator;
+import java.util.List;
 
 public class Computer {
 
     private NumberGenerator numberGenerator = new NumberGenerator();
     private ScoreCalculator scoreCalculator = new ScoreCalculator();
+
+    public List<Integer> getGeneratedNumber() {
+        return numberGenerator.getGeneratedNumbers();
+    }
 }
