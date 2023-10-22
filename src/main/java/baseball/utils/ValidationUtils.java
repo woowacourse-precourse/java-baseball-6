e681107c501c3726f1bfe319b817327867ebf2e4
soyeonii <dingdingcrong@naver.com>
2023-10-23 01:17:12 +0900

    refactor(ValidationUtils): 불필요한 메서드 제거

--- src/main/java/baseball/utils/ValidationUtils.java
+++ src/main/java/baseball/utils/ValidationUtils.java
@@ -5,15 +5,11 @@ public class ValidationUtils {
         return input.matches("^\\d+$");
     }
 
-    private static boolean isThreeDigitNumber(String input) {
-        return input.length() == 3;
-    }
-
     private static boolean hasNoDuplicateDigits(String input) {
         return input.chars().distinct().count() == input.length();
     }
 
-    private static boolean isWithinRange(String input) {
+    private static boolean isNonZeroWithinRange(String input) {
         int number = Integer.parseInt(input);
         return number >= 111 && number <= 999 && !input.contains("0");
     }
@@ -29,8 +25,7 @@ public class ValidationUtils {
 
     public static boolean isValidGuess(String input) {
         return containsOnlyDigits(input)
-                && isThreeDigitNumber(input)
                 && hasNoDuplicateDigits(input)
-                && isWithinRange(input);
+                && isNonZeroWithinRange(input);
     }
 }
