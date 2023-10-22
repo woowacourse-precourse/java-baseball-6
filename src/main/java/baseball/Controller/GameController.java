0efeeda7c36e7be321a1cf720f750e9f80330ae5
Sasha <106591445+kchaeys2@users.noreply.github.com>
2023-10-22 15:48:57 +0900

    feat(GameController) : 판별한 값 출력하는 함수

--- src/main/java/baseball/Controller/GameController.java
+++ src/main/java/baseball/Controller/GameController.java
@@ -9,5 +9,17 @@ public class GameController {
         computer = new Computer();
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
-
+    public String judgeUserInput(Integer strike,Integer ball){
+        String judgement = null;
+        if (strike == 0 && ball == 0) {
+            judgement = "낫싱";
+        }else if(strike > 0 && ball == 0) {
+            judgement = strike + "스트라이크";
+        } else if (strike == 0 && ball > 0) {
+            judgement = ball+"볼";
+        } else if (strike > 0 && ball >0) {
+            judgement = ball+"볼"+" "+strike+"스트라이크";
+        }
+        return judgement;
+    }
 }
