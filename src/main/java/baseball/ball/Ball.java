b0ca638bff16b90e2f97d13dc80613b272bbe41e
hongyoung <qwe916@naver.com>
2023-10-23 03:32:22 +0900

    feat: Ball 클래스에 getter 추가

--- src/main/java/baseball/ball/Ball.java
+++ src/main/java/baseball/ball/Ball.java
@@ -11,6 +11,10 @@ public class Ball {
         this.position = position;
     }
 
+    public int getNumber() {
+        return number;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
