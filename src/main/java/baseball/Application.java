f6329bbd58f547ffd778c6587b5dfc0a271eb0e5
이승렬 <ibban9810@icloud.com>
2023-10-22 16:32:54 +0900

    Feat: Script 구현.
    1. 주어진 스크립트 내용을 그냥 적는것 보다 ENUM 타입으로 묶어서 사용하는것이 용이하다고 판단.
    2. 후에 주어진 내용들이 더 많아질 경우, 관리하기 용이하고, 문제가 생길 경우에 (오타) -> 이런경우 컴파일 에러로 체크할 수 있을것으로 생각.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,44 +1,49 @@
 package baseball;
 
-import java.util.Arrays;
+import baseball.domain.Player.Player;
+import baseball.domain.computer.Computer;
+
+import static baseball.domain.script.Script.*;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         Scanner scanner = new Scanner(System.in);
-        int[] player = new int[3];
-        int[] computer = new int[3];
         int flag = 1;
 
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        // 1. Computer/Player Entity 생성.
+        Computer computer = new Computer();
 
-        for (int i = 0; i < 3; i++) {
-            computer[i] = (int) (Math.random() * 10);
-        }
+        // 2. 게임시작
+        System.out.println(GAME_START.getMessage());
 
-        System.out.println(Arrays.toString(computer));
-
-        // 1. 게임시작
         while (flag == 1) {
 
-            System.out.print("숫자를 입력해주세요 : ");
+            // 2- 1. Player 숫자를 입력받음.
+            System.out.print(NUMS_INPUT.getMessage());
 
+            List<Integer> list = new ArrayList<>();
             String tmp = scanner.next();
             for (int i = 0; i < 3; i++) {
-                player[i] = tmp.charAt(i) - '0';
+                list.add(tmp.charAt(i) - '0');
             }
+            Player player = new Player(list);
 
+            // 2-2. Player 숫자랑 Computer 숫자와 비교해보기.
             int strikeCount = 0;
             int ballCount = 0;
-
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
-                    if (computer[i] == player[j] && i == j) strikeCount++;
-                    else if (computer[i] == player[j]) ballCount++;
+                    if (computer.getIndexNumbers(i) == player.getIndexNumbers(j) && i == j) strikeCount++;
+                    else if (computer.getIndexNumbers(i) == player.getIndexNumbers(j)) ballCount++;
                 }
             }
 
+            // 2-3. Player 숫자에 따른 카운트 출력.
             if (strikeCount != 0 && ballCount == 0) System.out.println(strikeCount + "스트라이크");
             else if (strikeCount == 0 && ballCount != 0) System.out.println(ballCount + "볼");
             else if (strikeCount == 0) System.out.println("낫싱");
@@ -46,18 +51,15 @@ public class Application {
                 System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
             }
 
+            // 2-4. 만약 전부 완료하게 된다면, 게임을 다시 시작할지, 아니면 종료할지 선택.
             if (strikeCount == 3) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                System.out.println(GAME_END.getMessage());
+                System.out.println(GAME_RESTART.getMessage());
+
                 int n = scanner.nextInt();
                 if (n == 1) {
-                    System.out.println("숫자 야구 게임을 시작합니다.");
-
-                    for (int i = 0; i < 3; i++) {
-                        computer[i] = (int) (Math.random() * 10);
-                    }
-
-                    System.out.println(Arrays.toString(computer));
+                    System.out.println(GAME_START.getMessage());
+                    computer.resetNumbers();
                 }
                 if (n == 2) flag = 2;
             }
