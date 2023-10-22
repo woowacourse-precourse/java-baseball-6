2e856f68a0222de6d8d35ace530b77c877bc333c
jeongjihun <wlgns2234@naver.com>
2023-10-22 11:32:19 +0900

    refactor: position 매직넘버를 상수로 대체

--- src/main/java/baseball/domain/NumberGenerator.java
+++ src/main/java/baseball/domain/NumberGenerator.java
@@ -10,12 +10,13 @@ public class NumberGenerator {
 
     private static final int MIN_RANGE = 1;
     private static final int MAX_RANGE = 9;
+    private static final int MIN_POSITION = 0;
     private static final int NUMBER = 3;
 
     public Balls generate() {
 
         List<Ball> balls = new ArrayList<>();
-        for (int position = 0; position < NUMBER; position++) {
+        for (int position = MIN_POSITION; position < NUMBER; position++) {
             int number = pickNumberInRange(MIN_RANGE, MAX_RANGE);
             balls.add(new Ball(number, position));
         }
