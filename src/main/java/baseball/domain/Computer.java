2c5a47b03b96272724191c205e29bf74667a855e
junseoks-99 <angec5645@naver.com>
2023-10-22 23:58:55 +0900

    feat: "Computer 변수, 생성자 추가"

---
+++ src/main/java/baseball/domain/Computer.java
@@ -0,0 +1,13 @@
+package baseball.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+
+    List<Integer> number;
+
+    public Computer() {
+        this.number = new ArrayList<>();
+    }
+}
