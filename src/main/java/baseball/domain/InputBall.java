917a5b6ed4727cbaaa5b26a5c4f1ed496cf2e50b
sangwopark19 <sangwopark19@gmail.com>
2023-10-22 21:25:51 +0900

    InputBall클래스에 리스트를 초기화하는 매개변수 없는 생성자 추가

--- src/main/java/baseball/domain/InputBall.java
+++ src/main/java/baseball/domain/InputBall.java
@@ -1,7 +1,12 @@
 package baseball.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class InputBall {
-    private List<Integer> balls;
+    private final List<Integer> balls;
+
+    public InputBall() {
+        this.balls = new ArrayList<>();
+    }
 }
