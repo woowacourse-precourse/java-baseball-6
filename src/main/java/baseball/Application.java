906f95d40a5b3c89ca20325f59222d6fd3dd9297
kanghuijin <rkd8527@naver.com>
2023-10-22 16:59:09 +0900

    1. 컴퓨터 번호 생성 기능 메소드로 따로 분리 2. 예외처리 수정 후 테스트 완료 3. 메인 메소드에서 while문으로 반복하여 프로그램이 지속되게 하였는데 Play클래스 내부에서만 반복되게 구현 4. 모든 테스트 통과

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,9 +4,9 @@ public class Application {
     public static void main(String[] args) {
         Play play = new Play();
         System.out.println("숫자 야구 게임을 시작합니다.");
-        while (!play.play()) {
+        /*while (!play.play()) {
             play.play();
-        }
-
+        }*/
+        play.play();
     }
 }
