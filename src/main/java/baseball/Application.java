bffae884000ecd4bc55711d6cdd302e2c9dea630
yooooonshine <hl3470@naver.com>
2023-10-22 14:02:16 +0900

    Feat: 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,37 @@
 package baseball;
 
+import java.util.List;
+
+
+import static baseball.service.GameService.*;
+import static baseball.service.UserService.receiveUserNumber;
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        List<Integer> computerNumList;
+        List<Integer> userNumList;
+        int[] resultArray;
+
+        do {
+            computerNumList = generateComputerNumber();
+            do {
+                userNumList = receiveUserNumber();
+                resultArray = compare(userNumList, computerNumList);
+                printResult(resultArray);
+
+            } while (!checkResult(resultArray));
+
+        } while (askForRegame());
+
+
     }
+
+
+
+
 }
+
+
---
+++ src/main/java/baseball/exceptionHandler/ExecptionHandler.java
@@ -0,0 +1,27 @@
+package baseball.exceptionHandler;
+
+public class ExecptionHandler {
+    public static void exceptionHandlingForUserNumber(String userInput) {
+        //정규표현식
+        String regularExpForUserNumber = "^(?!.*(.).*\\1)[1-9]{3}$";
+
+        //예외처리
+        if (!userInput.matches(regularExpForUserNumber)) {
+            throw new IllegalArgumentException();
+
+
+        }
+    }
+
+    public static void exceptionHandlingForUserInput(String userInput) {
+        //정규표현식
+        String regularExpForUserInput = "^[1-2]{1}$";
+
+        //예외처리
+        if (!userInput.matches(regularExpForUserInput)) {
+            throw new IllegalArgumentException();
+
+        }
+
+    }
+}
