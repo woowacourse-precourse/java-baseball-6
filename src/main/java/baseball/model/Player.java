4cbf32addc231fb37edf453f988353dcbc8e2513
handwoong <handwoong@gmail.com>
2023-10-23 01:26:06 +0900

    feat: initialize player balls

--- src/main/java/baseball/model/Player.java
+++ src/main/java/baseball/model/Player.java
@@ -1,12 +1,19 @@
 package baseball.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import baseball.validation.PlayerBallValidator;
 
 public class Player {
+    private final List<Integer> playerBalls = new ArrayList<>();
+
     public Player(final String playerInput) {
         validatePlayerBalls(playerInput);
+        for (char c : playerInput.toCharArray()) {
+            int playerBall = Character.getNumericValue(c);
+            playerBalls.add(playerBall);
+        }
     }
 
     private void validatePlayerBalls(final String playerInput) {
