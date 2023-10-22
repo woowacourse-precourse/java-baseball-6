9e0ada26128f4e24219e2d7f8d1cc9244df5c1df
cousim46 <wjdghldns24@naver.com>
2023-10-22 14:54:44 +0900

    Add : 사용자 요청값이 3글자 이하, 1 ~ 9 사이인지에 대한 유효성 검증 메서드 추가

--- src/main/java/baseball/Validate.java
+++ src/main/java/baseball/Validate.java
@@ -1,5 +1,16 @@
 package baseball;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
+
 public class Validate {
+    private static void validateNumber(String number) {
+        String regex = "[1-9]{1,3}";
+        if (!Pattern.matches(regex, number)) {
+            throw new IllegalArgumentException();
+        }
+    }
 
 }
