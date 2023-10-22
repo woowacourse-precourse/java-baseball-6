a16a79b341e2967bdf000e2b86aee0c88d4c00c6
jinchiim <nellroll@syuin.ac.kr>
2023-10-23 04:59:05 +0900

    feature : 전체적인 Game 로직 구현

--- src/main/java/baseball/domain/game/Game.java
+++ src/main/java/baseball/domain/game/Game.java
@@ -13,6 +13,101 @@ public class Game {
     private final Computer computer;
     private final Output output;
 
+    private static Integer STRIKE;
+    private static Integer BALL;
+
+    private static Integer BALL_COUNT;
+    private static Integer STRIKE_COUNT;
+
+
+    public void startGame(){
+        STRIKE = 0;
+        BALL = 0;
+        List<String> computerNumber = computer.makeRandomNumbers();
+        baseBallGame(computerNumber);
+    }
+
+    public void baseBallGame(List<String> computerNumber){
+        while (STRIKE != 3){
+            output.printGetNumberMessage(); // "숫자를 입력해주세요 :"
+            String guessNumber = input.getExpectationNumber();
+            checkInputNumbers(guessNumber);
+            setBallsAndStrikes(computerNumber, guessNumber);
+            checkBallsAndStrikes(BALL, STRIKE);
+        }
+        output.printAllStrikeMessage(STRIKE);
+        checkRestartGame();
+    }
+
+    public void checkInputNumbers(String numbers){
+        if (!(numbers.length() == 3)){
+            throw new InputException(InputError.NEED_THREE_DIGIT);
+        }
+        if (!numbers.matches("\\d+")){
+            throw new InputException(InputError.MUST_BE_DIGIT);
+        }
+    }
+
+    public void countBallAndStrike(List<String> computerNumbers, String computerNumber, String guessNumber){
+        if(isGuessNumberContainsAnswer(computerNumbers, guessNumber)){
+            BALL_COUNT += 1;
+            checkNumberEquals(computerNumber, guessNumber);
+        }
+    }
+
+    public void checkNumberEquals(String computerNumber, String guessNumber){
+        if (computerNumber.equals(guessNumber)){
+            STRIKE_COUNT += 1;
+        }
+    }
+
+    public Boolean isGuessNumberContainsAnswer(List<String> computerNumbers, String guessNumber){
+        return computerNumbers.contains(guessNumber);
+    }
+
+
+    public void setBallsAndStrikes(List<String> computerNumbers, String guessNumber){
+        BALL_COUNT = 0;
+        STRIKE_COUNT = 0;
+
+        for (int i = 0; i < computerNumbers.size() ; i++){
+            countBallAndStrike(computerNumbers, computerNumbers.get(i), String.valueOf(guessNumber.charAt(i)));
+        }
+        BALL = BALL_COUNT - STRIKE_COUNT;
+        STRIKE = STRIKE_COUNT;
+    }
+
+    public void checkBallsAndStrikes(Integer ball, Integer strike){
+        if (ball != 0 & strike != 0){
+            output.printTotalMessage(ball, strike);
+        }
+        if (ball == 0 & strike != 0){
+            output.printStrikeMessage(strike);
+        }
+        if (ball != 0 & strike == 0){
+            output.printBallMessage(ball);
+        }
+        if (ball == 0 & strike == 0){
+            output.printNothingMessage();
+        }
+    }
+
+    public void checkRestartGame(){
+        output.printRestartOrNotMessage();
+
+        int opinion = Integer.parseInt(input.getRestartOrNot());
+
+        switch (opinion){
+            case 1:
+                startGame();
+                break;
+            case 2:
+                return;
+            default:
+                output.printNumberIsNoneMessage();
+        }
+    }
+
     public Game(Input input, Computer computer, Output output) {
         this.input = input;
         this.computer = computer;
