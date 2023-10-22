8a79bf0fa98db81f9cd8b7b7db5f4fc1876cd088
hyuna <monicajo0@naver.com>
2023-10-23 00:52:58 +0900

    fix: 1-9 3자리 판단

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -8,6 +8,7 @@ public class Player {
     private static final int INPUT_LENGTH = 3;
     private static final String RESTART_NUM = "1";
     private static final String END_NUM = "2";
+    private static final String VALID_DIGIT_REGEX = "^[1-9]{3}$";
 
     private String input;
     private String endInput;
@@ -51,12 +52,8 @@ public class Player {
     }
 
     private void validateOnlyNumber(){
-        for(int i=0;i<INPUT_LENGTH;i++){
-            char ch = input.charAt(i);
-
-            if(!Character.isDigit(ch)){
-                throw new IllegalArgumentException();
-            }
+        if(!input.matches(VALID_DIGIT_REGEX)){
+            throw new IllegalArgumentException();
         }
     }
 
