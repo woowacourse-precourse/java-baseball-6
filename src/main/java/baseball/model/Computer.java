c646986116628659ce476cdc4f61a61542cccaf6
snaag <snaag.dev@gmail.com>
2023-10-23 02:41:50 +0900

    feat: (Computer) Computer class 구현

---
+++ src/main/java/baseball/model/Computer.java
@@ -0,0 +1,52 @@
+package baseball.model;
+
+import baseball.utils.NumberUtils;
+import java.util.Arrays;
+import static baseball.utils.Constants.*;
+
+public class Computer {
+    private int[] digitPositions;
+    private final int[] answerSequence;
+
+    public Computer() {
+        this.answerSequence = new int[BALL_MAX];
+    }
+
+    public void setAnswer() {
+        this.setAnswerSequence();
+        this.setDigitPositions();
+    }
+
+    private void setAnswerSequence() {
+        boolean[] exist = new boolean[RANGE_END];
+        Arrays.fill(exist, false);
+
+        int times = 0;
+
+        while(times < BALL_MAX) {
+            Integer v = NumberUtils.random();
+            if(v > ZERO && v < RANGE_END) {
+                if(!exist[v]) {
+                    this.answerSequence[times] = v;
+                    exist[v] = true;
+                    times += 1;
+                }
+            }
+        }
+    }
+
+    private void setDigitPositions() {
+        this.digitPositions = new int[RANGE_END];
+        Arrays.fill(digitPositions, INIT_VALUE);
+
+        // [-1, 2, -1, 0, -1, -1, -1, -1, -1, 1]
+        for(int i=0; i<BALL_MAX; i++) {
+            int position = answerSequence[i];
+            digitPositions[position] = i;
+        }
+    }
+
+    public int[] getDigitPositions() {
+        return digitPositions;
+    }
+}
