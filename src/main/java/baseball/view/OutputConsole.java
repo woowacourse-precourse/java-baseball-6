6f1b1b8ce9f658f8eeef293f19804347d5604c05
jinsu4755 <parkjinsu4755@gmail.com>
2023-10-22 16:20:55 +0900

    feat: 콘솔 출력 객체 생성

---
+++ src/main/java/baseball/view/OutputConsole.java
@@ -0,0 +1,11 @@
+package baseball.view;
+
+public class OutputConsole {
+    public static void printStartMessage() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void requestInputMessage() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+}
