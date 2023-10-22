b90cff4371a539d036b4e77067232c57cc0785ff
Hoo-sung <gntjd135@gmail.com>
2023-10-22 15:58:59 +0900

    refactor: BaseBallNumVerifier.check() 함수 3개로 세부 분리(함수당 하나의 기능)

--- src/main/java/baseball/verifier/BaseBallNumVerifier.java
+++ src/main/java/baseball/verifier/BaseBallNumVerifier.java
@@ -8,17 +8,29 @@ import static baseball.Application.NUM_DIGITS;
 public class BaseBallNumVerifier implements Verifier {
     @Override
     public void check(String input) throws IllegalArgumentException {
-        if(input.length()!=NUM_DIGITS)//3개 미만, 또는 초과해서 입력된 경우에 대한 예외처리
+        checkThreeDigits(input);
+        checkNumeric(input);
+        checkDistinct(input);
+    }
+
+    private void checkThreeDigits(String input){
+        if(input.length() != NUM_DIGITS){
             throw new IllegalArgumentException("3개의 수를 입력하지 않았습니다.");
-
+        }
+    }
+    private void checkNumeric(String input){
+        try{
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("1~9사이 수가 아닌 값을 입력하였습니다.");
+        }
+    }
+    private void checkDistinct(String input){
         Set<Character> uniqueChars = new HashSet<>();
-
         for(char char_num : input.toCharArray()){
-            if(char_num<'1' || char_num>'9')//1~9가 아닌 값에 대한 예외처리
-                throw new IllegalArgumentException("1~9사이 수가 아닌 값을 입력하였습니다.");
-
             if(!uniqueChars.add(char_num))//중복된 수를 넣으려고 하는 경우 예외처리
                 throw new IllegalArgumentException("중복된 수를 입력하였습니다.");
         }
     }
+
 }
