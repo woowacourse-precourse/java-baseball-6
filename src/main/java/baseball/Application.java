e47dd6a421ea875714a10c87e610f7730ca4adec
dy102 <132787602+dy102@users.noreply.github.com>
2023-10-22 18:27:13 +0900

    Refactor: if문 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,6 @@
 package baseball;
 
-import baseball.domain.BallAndStrikeCount;
+import baseball.domain.Count;
 import baseball.domain.Computer;
 import baseball.domain.Restart;
 import baseball.domain.User;
@@ -9,7 +9,7 @@ public class Application {
     public static void main(String[] args) {
         User user = new User();
         Computer computer = new Computer();
-        BallAndStrikeCount ballAndStrikeCount = new BallAndStrikeCount();
+        Count count = new Count();
         Restart restart = new Restart();
 
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -17,11 +17,12 @@ public class Application {
             System.out.print("숫자를 입력해주세요 : ");
             user.inputUserNumber();
 
-            ballAndStrikeCount.compare(computer.getRandomNumbers(), user.getUserNumber());
-            ballAndStrikeCount.showStatus();
+            count.compare(computer.getRandomNumbers(), user.getUserNumber());
+            count.showStatus();
 
-            if (ballAndStrikeCount.checkStrike()) {
-                if (!restart.checkRestartNumber(restart.inputRestartNumber())) break;
+            if (count.checkStrike() && !restart.checkRestartNumber(restart.inputRestartNumber())) {
+                break;
+            } else if (count.checkStrike()) {
                 computer = new Computer();
             }
         }
