2e051466439b71d5d78b85981b323001548b20d8
Jay <injaesong0@gmail.com>
2023-10-23 02:05:49 +0900

    feat: add instruction to input numbers

--- src/main/java/baseball/Communicator.java
+++ src/main/java/baseball/Communicator.java
@@ -1,10 +1,26 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Communicator {
     private final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     public void printStart() {
         System.out.println(START_MESSAGE);
+    }
+
+    public List<Integer> instructInputNumbers() {
+        System.out.print("숫자를 입력해주세요 : ");
+
+        List<Integer> input = new ArrayList<>();
+
+        // TODO: Validate Input
+        String inputString = Console.readLine();
+        for (int i=0; i<inputString.length(); i++) {
+            input.add(inputString.charAt(i) - '0');
+        }
+
+        return input;
     }
 }
