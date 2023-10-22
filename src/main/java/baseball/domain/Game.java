448aed57183be28cb3f68108fc5ca023a70ce158
JuSeong1130 <jsmoon1130@naver.com>
2023-10-22 22:39:03 +0900

    feat(Game) : 숫자비교 기능 구현

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -19,10 +19,34 @@ public class Game {
 
     private Balls createBalls() {
         List<Integer> randomNumbers = RandomNumberGenerator.createRandomNumbers(3, 1, 9);
+        //System.out.println(randomNumbers);
         return new Balls(randomNumbers);
+    }
+
+    public GameResult compareBalls(Balls otherBalls) {
+        GameResult gameResult = createGameResult(otherBalls);
+        checkResult(gameResult);
+        return gameResult;
+    }
+    private GameResult createGameResult(Balls otherBalls) {
+        int strike = this.balls.getSamePositionAndSameNumberCount(otherBalls);
+        int ball = this.balls.getOtherPositionAndSameNumberCount(otherBalls);
+        return new GameResult(strike, ball);
+    }
+    private void checkResult(GameResult gameResult) {
+        if (gameResult.getStrike() == 3) {
+            gameStatus = GameStatus.STOP;
+        }
     }
 
     public boolean isRun() {
         return GameStatus.isRun(gameStatus);
+    }
+
+    public void performCommand(String inputCommand) {
+        GameHandlerCommand command = GameHandlerCommand.findCommand(inputCommand);
+        if(command == GameHandlerCommand.RESTART) {
+            init();
+        }
     }
 }
