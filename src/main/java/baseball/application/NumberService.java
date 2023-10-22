5407aee1c85fbf6300ea6d7b0c2c639233296886
jae woo Jung <awfjol2008@naver.com>
2023-10-23 00:09:53 +0900

    refector: 숫자 비교에 따른 힌트 설정 로직 최적화

--- src/main/java/baseball/application/NumberService.java
+++ src/main/java/baseball/application/NumberService.java
@@ -30,21 +30,12 @@ public class NumberService {
         return hint;
     }
 
-    private static void compareNumber(List<Integer> computerNumber, List<Integer> playerNumber,
-                                      Hint hint) {
+    private static void compareNumber(List<Integer> computerNumber, List<Integer> playerNumber, Hint hint) {
         for (int i = 0; i < playerNumber.size(); i++) {
-            for (int j = 0; j < computerNumber.size(); j++) {
-                setHint(computerNumber, playerNumber, hint, i, j);
-            }
-        }
-    }
-
-    private static void setHint(List<Integer> computerNumber, List<Integer> playerNumber, Hint hint,
-                                int i, int j) {
-        if (Objects.equals(playerNumber.get(i), computerNumber.get(j))) {
-            if (i == j) {
+            Integer myNumber = playerNumber.get(i);
+            if (Objects.equals(myNumber, computerNumber.get(i))) {
                 hint.increaseStrike();
-            } else {
+            } else if (computerNumber.contains(myNumber)) {
                 hint.increaseBall();
             }
         }
