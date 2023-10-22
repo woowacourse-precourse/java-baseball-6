39630fe972626d2b6a8795a396f81f64f5895b8f
sangwopark19 <sangwopark19@gmail.com>
2023-10-22 21:55:26 +0900

    사용자의 입력값을 검증하는 메서드

--- src/main/java/baseball/domain/InputBall.java
+++ src/main/java/baseball/domain/InputBall.java
@@ -9,4 +9,19 @@ public class InputBall {
     public InputBall() {
         this.balls = new ArrayList<>();
     }
+
+    public boolean validateInput(String input) {
+        try {
+            int num = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자가 아닙니다");
+        }
+        if (input.length() > 3) {
+            throw new IllegalArgumentException("3자리 이상 입력했습니다.");
+        }
+        if (input.contains("0")) {
+            throw new IllegalArgumentException("0이 포함되어 있습니다");
+        }
+        return true;
+    }
 }
