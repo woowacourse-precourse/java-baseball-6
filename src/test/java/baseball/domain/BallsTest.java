3d1d3d53085e1ab3c2c8821eb6f17b80b42d71da
JuSeong1130 <jsmoon1130@naver.com>
2023-10-22 21:05:08 +0900

    test(Balls) : strike 개수 기능 테스트

--- src/test/java/baseball/domain/BallsTest.java
+++ src/test/java/baseball/domain/BallsTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class BallsTest {
@@ -22,6 +23,7 @@ class BallsTest {
                 Arguments.of(Arrays.asList(3, 9, 7))
         );
     }
+
     static Stream<Arguments> createBallsExceptionData() {
         return Stream.of(
                 Arguments.of(Arrays.asList(1, 2, 3)),
@@ -29,6 +31,7 @@ class BallsTest {
                 Arguments.of(Arrays.asList(3, 9, 7))
         );
     }
+
     @DisplayName("balls 생성 시 null 값이 아닌지 테스트.")
     @ParameterizedTest
     @MethodSource("createBallsData")
@@ -43,4 +46,25 @@ class BallsTest {
     void balls_생성_예외_테스트(List<Integer> values) {
         assertThrows(IllegalArgumentException.class, () -> new Balls(values));
     }
+
+
+    @DisplayName("Balls끼리 가지고 있는 Ball중에서 같은 숫자,같은 위치의 개수를 구한다.")
+    @ParameterizedTest
+    @MethodSource("createBallsWithSameNumberAndSamePositionData")
+    void 같은숫자_같은위치_개수(List<Integer> numbers, List<Integer> otherNumbers, int expected) {
+        Balls balls = new Balls(numbers);
+        Balls otherBalls = new Balls(otherNumbers);
+        assertThat(balls.getSamePositionAndSameNumberCount(otherBalls)).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> createBallsWithSameNumberAndSamePositionData() {
+        return Stream.of(
+                Arguments.of(List.of(2, 5, 9), List.of(1, 2, 3), 0),
+                Arguments.of(List.of(1, 5, 9), List.of(1, 2, 3), 1),
+                Arguments.of(List.of(1, 2, 9), List.of(1, 2, 3), 2),
+                Arguments.of(List.of(1, 2, 3), List.of(1, 2, 3), 3)
+        );
+    }
+
+
 }
