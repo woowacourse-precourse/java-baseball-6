2bbc0aa71fa9f74c03f2d6346ee4d8c27ef2122f
LeeSeungUk <tmddnr5410@gmail.com>
2023-10-23 00:13:33 +0900

    feat : 숫자 입력 기능 구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,7 +1,16 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class BaseballGame {
     public void startGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+
+        boolean isPlaying = true;
+
+        do {
+            System.out.print("숫자를 입력해주세요 : ");
+            Console.readLine();
+        } while (isPlaying);
     }
 }
