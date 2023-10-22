b0edac8c04f4dca590dcfac38e4db47b07c84722
장기환 <jang-gihwan@jang-gihwan-ui-MacBookPro.local>
2023-10-22 13:42:53 +0900

    refactor : RQ-005 -> cleanCode

--- src/main/java/baseball/Controller/GameController.java
+++ src/main/java/baseball/Controller/GameController.java
@@ -29,8 +29,7 @@ public class GameController {
             while(!game.getRefereeController().judge(game.getPlayerController().query(), game.getComputer())){}
             gameView.display("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             
-            if(!isReStart())
-                return;
+            if(!isReStart()) {return;}
             game = new Game();
         }
     }
