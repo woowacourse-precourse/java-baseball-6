0b11a776584bda898c8e6a8c122ce137573327e4
Seotriever <122240520+Seotriever@users.noreply.github.com>
2023-10-22 16:18:28 +0900

    10/22 16:15 입력및 출력
    입,출력 메소드 구현
    예외처리 해야함

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -19,31 +19,43 @@ public class Application {
                 System.out.println(computer);   //난수확인값
 
                 // 입력값 메소드 =======================================================================
-                System.out.println("숫자 야구 게임을 시작합니다.");
-                System.out.print("숫자 세자리를 입력하세요 : ");
+                System.out.println("생각하는 숫자 한개씩 입력(enter)해주세요.");       //todo 예외처리 (중복포함)
                 List<Integer> user = new ArrayList<>();
-                for (int i = 0; user.size() < 3; i++) {
-                        String userInput = Console.readLine(); // 사용자가 입력할 3자리 Console.readLine()
+//                for (int i=0; user.size()<3;i++){
+                while (user.size() < 3) { // while문 test
+                        String userInput = Console.readLine();
                         user.add(Integer.parseInt(userInput));
                 }
-                System.out.println();
+                System.out.println(user); // 입력된 숫자 확인값
 
+                // 판정 메소드 =====================================================================================
+                int ballCount = 0;
+                int strikeCount = 0;
+                for (int i = 0; i < user.size(); i++) {
+                        if (computer.contains(user.get(i))) {
+                                if (computer.indexOf(user.get(i)) == i) {
+                                        strikeCount++;
+                                } else {
+                                        ballCount++;
+                                }
+                        }
+                }
+                // 출력 메소드 ===================================================================================
+                if (strikeCount == 3) {
+                        System.out.println("3스트라이크!");
+                        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+                                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                }else if (strikeCount + ballCount == 0) {
+                        System.out.println("낫싱!");
+                }else {
+                        System.out.println(ballCount + "볼, " + strikeCount + "스트라이크");        // 중간확인값
+                }
 
-//        String arrUser = Arrays.toString(user.toCharArray());
-//        if(arrUser.length()!=3){
-//                System.out.println("세자리의 숫자만 입력해주세요");
-//        for( int j = 0; j < user.length(); j++ ) {
-//            }try{
-//        }catch (NumberFormatException e){
-//            e.printStackTrace();
-//        }}        // 예외처리
-//        System.out.println(Arrays.toString(user.toCharArray()));  // 배열로 입력되는 확인값
-
-//        System.out.println(arrUser);
-                // 배열로 나타낸값을  가독성있게 선언해줄필요가 있을듯
-
-                // 비교 메소드 =========================================================================
-                //for문으로 computer의 난수 3개와 user
+//                int[] test = new int[0];              // 이형식은 왜 안되는지 찾아보기
+//                for (int x = 0; test.length < 3; x++) {
+//                        String testInut = Console.readLine();
+//                        test.
+//                }
         }
 
 }
