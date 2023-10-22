44ac2426a6bbdc798a2fd22175712ddd52e0fa9a
JINU-CHANG <jenny010925@naver.com>
2023-10-23 03:52:13 +0900

    [fix] pickUniqueNumbersInRange()에서 pickNumberInRange()로 변환

--- src/main/java/baseball/domain/NumberFactory.java
+++ src/main/java/baseball/domain/NumberFactory.java
@@ -1,5 +1,6 @@
 package baseball.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Randoms;
@@ -9,6 +10,14 @@ public class NumberFactory {
 	private static final int END_NUMBER = 9;
 
 	public static List<Integer> createRandomNumbers(int size) {
-		return Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, size);
+		List<Integer> randomNumbers = new ArrayList<>();
+
+		while(randomNumbers.size()<size) {
+			Integer randomNumber = Randoms.pickNumberInRange(START_NUMBER, END_NUMBER);
+			if(!randomNumbers.contains(randomNumber)) {
+				randomNumbers.add(randomNumber);
+			}
+		}
+		return randomNumbers;
 	}
 }
