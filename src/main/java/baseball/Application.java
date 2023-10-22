cae607e9ba4628268842e0430339b44ee5d0e2ab
ohhamma <judyham10634@naver.com>
2023-10-22 16:33:52 +0900

    :art: refactor: 자바 코드 컨벤션 적용

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,7 +4,6 @@ public class Application {
     public static void main(String[] args) {
         Game game = new Game();
 
-        // 1. 게임 시작
-        game.PrintStartPhrase();
+        game.printStartPhrase();
     }
 }
--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,10 +1,10 @@
 package baseball;
 
 public class Game {
-
     private String startPhrase;
 
-    public void PrintStartPhrase() {
+    /* Starts the game */
+    public void printStartPhrase() {
         startPhrase = "숫자 야구 게임을 시작합니다.";
         System.out.println(startPhrase);
     }
