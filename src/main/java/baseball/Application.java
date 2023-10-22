f37df0dfa1395f692ab207a4c8b41f52b4c84add
KongSeungHwan <102781120+KongSeungHwan@users.noreply.github.com>
2023-10-23 01:11:09 +0900

    사용자 입력값, 컴퓨터 숫자 할당

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,10 @@
 package baseball;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+import java.util.stream.IntStream;
 
 public class Application {
     public static void main(String[] args) {
@@ -12,14 +14,29 @@ public class Application {
         //3.비교 연산 메소드
         //숫자, 자리가 모두 만족 시 스트라이크
         //숫자가 맞지만 자리가 다른 경우 ball
-
         try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            int input = Integer.parseInt(br.readLine());
-        }catch()
-        catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+            System.out.printf("숫자를 입력해주세요 :");
+            String input = Console.readLine();//입력 받기
+            List<Integer> user = input.chars().mapToObj(e->Character.getNumericValue(e)).toList();
 
+            user.stream().forEach(System.out::println);//출력
+
+            System.out.println("컴퓨터 숫자 추출");
+            List<Integer> computer = IntStream.generate(() -> Randoms.pickNumberInRange(1, 9))
+                    .distinct()
+                    .limit(3)
+                    .boxed()
+                    .toList();//서로 다른 숫자 3개가 나올때까지 생성(스트림 연산)
+            
+            computer.stream().forEach(System.out::print);//출력
+
+            //비교연산 메소드
+
+
+
+            //유효성 검사
+        }catch(IllegalArgumentException e){
+            throw new IllegalArgumentException(e);
+        }
     }
 }
