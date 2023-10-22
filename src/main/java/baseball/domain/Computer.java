8bfa385511e5fda96d43e421e06b541163724910
junseoks-99 <angec5645@naver.com>
2023-10-23 00:00:38 +0900

    feat: "서로 다른 3자리 수 생성"

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,5 +1,6 @@
 package baseball.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,5 +10,17 @@ public class Computer {
 
     public Computer() {
         this.number = new ArrayList<>();
+    }
+
+    public void makeNumber() {
+
+        number.clear();
+
+        while (number.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!number.contains(randomNumber)) {
+                number.add(randomNumber);
+            }
+        }
     }
 }
