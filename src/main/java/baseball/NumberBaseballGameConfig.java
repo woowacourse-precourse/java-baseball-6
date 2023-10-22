f00821d6acdca3fba3eb549c772e4ba6faf10bac
Kuen Hee Lee <Leekh10195@gmail.com>
2023-10-22 16:55:00 +0900

    feat: 게임 설정 상수 정의

---
+++ src/main/java/baseball/NumberBaseballGameConfig.java
@@ -0,0 +1,12 @@
+package baseball;
+
+public class NumberBaseballGameConfig {
+
+    // 게임 설정 상수 정의
+    public static final int BASEBALL_NUMBER_LENGTH = 3;
+    public static final int MIN_PLACE_VALUE = 1;
+    public static final int MAX_PLACE_VALUE = 9;
+    public static final int CONTINUE_GAME = 1;
+    public static final int EXIT_GAME = 2;
+
+}
