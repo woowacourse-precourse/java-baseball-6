f4e39b5881a9f0c07927790d10280061d9d44123
민주 <hinote444@gmail.com>
2023-10-22 16:54:39 +0900

    feat(Validator): 플레이어의 입력 값에서 중복된 값이 있는지 검사하는 기능 구현

--- src/main/java/baseball/validator/Validator.java
+++ src/main/java/baseball/validator/Validator.java
@@ -1,4 +1,14 @@
 package baseball.validator;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Validator {
+    public static void validateOverlap(List<Integer> playerNumbers) {
+        Set<Integer> noOverlapNumbers = new HashSet<>(playerNumbers);
+        if (noOverlapNumbers.size() != playerNumbers.size()) {
+            throw new IllegalArgumentException("서로 다른 수를 입력해야 합니다.");
+        }
+    }
 }
