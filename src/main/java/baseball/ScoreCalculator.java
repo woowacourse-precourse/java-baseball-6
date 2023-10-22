901d577ccd834f3348ba0daaa98498fef6ce4b5f
bumstead-bumstead <dldyghks951@gmail.com>
2023-10-22 16:53:38 +0900

    refactor: ScoreCalculator - calculater()의 매개변수와 반환 타입을 List로 수정

--- src/main/java/baseball/ScoreCalculator.java
+++ src/main/java/baseball/ScoreCalculator.java
@@ -1,22 +1,24 @@
 package baseball;
 
+import java.util.List;
+
 public class ScoreCalculator {
-    public static Integer[] calculate(Integer[] pitchInput, Integer[] numbers) {
+    public static List<Integer> calculate(List<Integer> pitchInput, List<Integer> numbers) {
         Integer balls = 0;
         Integer strikes = 0;
 
-        for (int index = 0; index < numbers.length; index++) {
-            if (numbers[index].equals(pitchInput[index])) {
+        for (int index = 0; index < numbers.size(); index++) {
+            if (numbers.get(index).equals(pitchInput.get(index))) {
                 strikes++;
             }
 
             for (int index2 = 1; index2 < 3; index2++) {
-                if (numbers[(index + index2) % 3].equals(pitchInput[index])) {
+                if (numbers.get((index + index2) % 3).equals(pitchInput.get(index))) {
                     balls++;
                 }
             }
         }
 
-        return new Integer[]{strikes, balls};
+        return List.of(strikes, balls);
     }
 }
