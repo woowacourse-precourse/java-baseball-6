eaebedaeedd8546d8b74c32b29efb7e296a2b530
jinkshower <askthem@naver.com>
2023-10-22 14:45:26 +0900

    refactor: 상수 클래스 생성

---
+++ src/main/java/baseball/Constant.java
@@ -0,0 +1,9 @@
+package baseball;
+
+public class Constant {
+    public static final int MAX_DIGIT = 3;
+    public static final int MIN_NUM = 1;
+    public static final int MAX_NUM = 9;
+    public static final int THREE_STRIKE = 3;
+    public static final int RESTART = 1;
+}
