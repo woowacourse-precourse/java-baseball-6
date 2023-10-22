9e7ee793f65077c6170c516d240f425580473b64
junseoks-99 <angec5645@naver.com>
2023-10-23 01:39:30 +0900

    feat: "user 숫자 입력값 저장"

--- src/main/java/baseball/controller/BaseballGameController.java
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -3,6 +3,7 @@ package baseball.controller;
 import baseball.domain.Computer;
 import baseball.domain.User;
 import baseball.util.Validation;
+import camp.nextstep.edu.missionutils.Console;
 
 public class BaseballGameController {
 
@@ -11,7 +12,6 @@ public class BaseballGameController {
     private Computer computer;
     private User user;
     private Validation validation;
-    private final int MAX_LENGTH = 3;
 
     public BaseballGameController() {
         this.computer = new Computer();
@@ -23,5 +23,17 @@ public class BaseballGameController {
         this.ball = 0;
         this.strike = 0;
         this.user = new User();
+    }
+
+    public void inputNumber() {
+
+        System.out.print("숫자를 입력해주세요 : ");
+        String inputNumber = Console.readLine();
+
+        validation.validNumber(inputNumber);
+
+        for (int i = 0; i < Validation.MAX_INPUT_LENGTH; i++) {
+            user.addNumber(Character.getNumericValue(inputNumber.charAt(i)));
+        }
     }
 }
