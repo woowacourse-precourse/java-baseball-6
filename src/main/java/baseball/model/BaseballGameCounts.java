fa83ea4f3a9ddf442dd87370f656649dabae726e
oxdjww <oxdjww@gmail.com>
2023-10-23 00:17:17 +0900

    refactor : BaseballGameCounts 일급컬렉션화

--- src/main/java/baseball/model/BaseballGameCounts.java
+++ src/main/java/baseball/model/BaseballGameCounts.java
@@ -1,41 +1,56 @@
 package baseball.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BaseballGameCounts {
-    private int strikes;
-    private int balls;
+
+    private static final int STRIKE = 0;
+    private static final int BALL = 0;
+
+    private final List<Integer> counts = new ArrayList<>(2);
 
     protected BaseballGameCounts() {
     }
 
-    public static BaseballGameCounts createBaseballGameCounts() {
-        return new BaseballGameCounts();
+    public static BaseballGameCounts createBaseballGameCounts(ComputerNumber computer, UserNumber user) {
+
+            BaseballGameCounts baseballGameCounts = new BaseballGameCounts();
+            int ballCounts = 0;
+            for (Integer idx : user.getUserNumber()) {
+                if (computer.getComputerNumber().contains(idx)) {
+                    ballCounts++;
+                }
+            }
+            ballCounts -= baseballGameCounts.getCounts().get(STRIKE);
+            if (ballCounts > 0) {
+                baseballGameCounts.counts.set(BALL, ballCounts);
+            }
+
+            int strickCounts = 0;
+            for (int idx = 0; idx < user.getUserNumber().size(); idx++) {
+                if (user.getUserNumber().get(idx).equals(computer.getComputerNumber().get(idx))) {
+                    strickCounts++;
+                }
+            }
+            if (strickCounts > 0) {
+
+                baseballGameCounts.counts.set(STRIKE,strickCounts);
+            }
+        return baseballGameCounts;
     }
 
     public boolean isWinCondition() {
-        return this.strikes == 3;
+        return this.counts.get(STRIKE) == 3;
     }
-
-
 
     public void resetCounts() {
-        this.strikes = 0;
-        this.balls = 0;
+        counts.set(STRIKE, 0);
+        counts.set(BALL, 1);
     }
 
-    public void setStrikes(int strikes) {
-        this.strikes = strikes;
-    }
-
-    public void setBalls(int balls) {
-        this.balls = balls;
-    }
-
-    public int getStrikes() {
-        return strikes;
-    }
-
-    public int getBalls() {
-        return balls;
+    public List<Integer> getCounts() {
+        return this.counts;
     }
 
 }
