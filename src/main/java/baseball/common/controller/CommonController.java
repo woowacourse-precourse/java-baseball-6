8de2b16980e3e7a8920d291db8724590bc198fd1
HyunShik <hshong0907@gmail.com>
2023-10-22 20:34:09 +0900

    feat: 야구 종료 시 발생 method 작성
    새로 시작 1, 종료 2
    1 혹은 2 입력받을 때까지 재귀

--- src/main/java/baseball/common/controller/CommonController.java
+++ src/main/java/baseball/common/controller/CommonController.java
@@ -1,9 +1,21 @@
 package baseball.common.controller;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class CommonController {
 
     //야구 시작 -> 정답 생성
     private void baseballStart(){
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    //야구 종료 -> 재시작 or 종료
+    private int baseballEnd(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = Console.readLine();
+        //1,2번이 입력될 때까지 재귀
+        if(!input.equals("1")&&!input.equals("2")) return baseballEnd();
+
+        return Integer.parseInt(input);
     }
 }
