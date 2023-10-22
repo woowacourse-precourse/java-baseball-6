bd16a0a40bb06c39f3601df56b1858e241554a1b
gyulim5 <ohrbfla1004@khu.ac.kr>
2023-10-22 21:22:34 +0900

    ✨ Feat: 랜덤 숫자 생성 로직 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,4 +1,29 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Game{
+
+        Input input = new Input();
+        Output output = new Output();
+        public void init()
+        {
+
         }
+
+        private int generateNumber()
+        {
+                int[] randomNumber = new int[3];
+
+                for (int index = 0; index < 3;) {
+                        int randomPickNumber = Randoms.pickNumberInRange(1, 9);
+                        if (randomNumber[0] == randomPickNumber || randomNumber[1] == randomPickNumber) {
+                                continue;
+                        }
+                        randomNumber[index] = randomPickNumber;
+                        index++;
+                }
+
+                return randomNumber[0]*100 + randomNumber[1]*10 + randomNumber[2];
+        }
+}
