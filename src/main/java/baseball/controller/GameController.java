446a97af382ee58d54ef2f8340787b9650f984f7
ykmxxi <ykmxxi97@gmail.com>
2023-10-22 15:49:39 +0900

    feat(GameController): 게임이 끝날 때 까지 객체들에게 요청을 보내는 기능 구현

---
+++ src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,54 @@
+package baseball.controller;
+
+import baseball.domain.Computer;
+import baseball.domain.Player;
+import baseball.service.GameService;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+public class GameController {
+
+	private final GameService gameService;
+
+	public GameController() {
+		this.gameService = new GameService();
+	}
+
+	public void run() {
+		OutputView.printStartGuide();
+		playBaseBallGame();
+		replayOrExitGame();
+	}
+
+	private void playBaseBallGame() {
+		final Computer computer = gameService.createComputer();
+		repeatBaseBallGamePlaying(computer);
+	}
+
+	private void repeatBaseBallGamePlaying(final Computer computer) {
+		String gameResultMessage = "";
+		while (!gameService.isEnded(gameResultMessage)) {
+			final Player player = new Player(getNumbersInput());
+			gameResultMessage = gameService.requestJudgment(computer, player);
+			OutputView.printGameResultMessage(gameResultMessage);
+		}
+	}
+
+	private String getNumbersInput() {
+		OutputView.printNumbersInputGuide();
+		return InputView.getInput();
+	}
+
+	private void replayOrExitGame() {
+		String replayCommandInput = getReplayCommandInput();
+		if (gameService.isReplay(replayCommandInput)) {
+			playBaseBallGame();
+		}
+	}
+
+	private String getReplayCommandInput() {
+		OutputView.printGameEndGuide();
+		return InputView.getInput();
+	}
+
+}
