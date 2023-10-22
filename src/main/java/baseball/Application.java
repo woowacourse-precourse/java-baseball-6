ce56d68c0372fbe44fffd26b97a954ea3d857cb0
sdsr <dlxodnr1212@gmail.com>
2023-10-22 22:20:46 +0900

    "feat: add welcomeMessage"

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,11 @@
 package baseball;
 
 import baseball.controller.BaseballController;
+import baseball.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
+        InputView.welcomeMessage();
         BaseballController.StartBaseballGame();
     }
 }
