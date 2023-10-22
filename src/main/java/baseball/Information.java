28ee48b4e35c3636b315ecc87994bc36b93e6501
Woongbae Yun <devbelly@naver.com>
2023-10-22 15:01:45 +0900

    feat : 게임 시작 안내 문구를 출력하는 기능 추가

---
+++ src/main/java/baseball/Information.java
@@ -0,0 +1,10 @@
+package baseball;
+
+public class Information {
+
+    private static final String START_GAME = "숫자 야구 게임을 시작합니다.";
+
+    public static void printStartGame() {
+        System.out.println(START_GAME);
+    }
+}
