661e259de9aeafa1f883b4a194bfcbfd0ef63ccb
Kimhyunjin4455 <dp4455@naver.com>
2023-10-22 22:45:15 +0900

    inputUserAndCoputerBalls

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,14 +1,25 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.SplittableRandom;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
         UserBalls userBalls = new UserBalls();
+        List<Integer> userBallList = userBalls.inputBalls();
         ComputerBalls computerBalls = new ComputerBalls();
-        PlayGame playGame = new PlayGame();
+        List<Integer> computerBallList = computerBalls.inputComputerBalls();
 
-
+        System.out.println(userBallList);
+        System.out.println(computerBallList);
     }
 }
