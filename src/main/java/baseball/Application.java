9228756cd6a2b4da419510c339a93e9228800969
권용하 <yongha.kwon12@gmail.com>
2023-10-22 18:07:02 +0900

    feat : add BaseballGameController

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import baseball.application.BaseballGameService;
+import baseball.application.BaseballGeneratorFactory;
+import baseball.domain.BaseBallReferee;
+import baseball.presentation.BaseballGameController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseballGameService baseballGameService = new BaseballGameService(new BaseBallReferee(), new BaseballGeneratorFactory());
+        BaseballGameController baseballGameController = new BaseballGameController(baseballGameService);
+
+        baseballGameController.startGame();
     }
 }
