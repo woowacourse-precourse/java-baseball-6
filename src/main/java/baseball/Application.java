10fef5949dcd987cdb20b79abe7a81f3852c4851
osukhyun00 <osh123chm@gmail.com>
2023-10-23 00:13:11 +0900

    feat: add user input

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,10 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
@@ -13,6 +15,24 @@ public class Application {
             if (!answer.contains(randomNumber)) {
                 answer.add(randomNumber);
             }
+        }
+
+        // 2. 숫자 입력 받고 검증하기
+
+        System.out.println("입력하세요: ");
+        String input = Console.readLine();
+
+        if (input.length() != 3 || !input.matches("[1-9]+")) {
+            throw new IllegalArgumentException();
+        }
+
+        List<Integer> inputList = new ArrayList<>(3);
+        for (int i = 0; i < 3; i++) {
+            inputList.add(input.charAt(i) - '0');
+        }
+
+        if (inputList.stream().distinct().collect(Collectors.toList()).size() != inputList.size()) {
+            throw new IllegalArgumentException();
         }
     }
 }
