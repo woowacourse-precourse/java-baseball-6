2e4e9a1921b3a7e4b92205bdcbc7622693edc91e
ssook <60427387+ssook1222@users.noreply.github.com>
2023-10-22 13:20:43 +0000

    UPDATE : 컴퓨터 중복으로 나오는 숫자 부분 처리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,15 +5,27 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        BaseballGame game = new BaseballGame();
 
         System.out.println("숫자 야구 게임을 시작합니다.");
+        boolean first_check = true; // 처음에는 반복문을 돌리지 않기 위해 true로 설정
 
         do {
-            game.playGame();
-            System.out.print("게임을 다시 시작하시겠습니까? (Y/N): ");
-        } while (scanner.next().equalsIgnoreCase("Y"));
+            if (!first_check) {
+                System.out.print("게임을 다시 시작하시겠습니까? (Y/N): ");
+                String restartInput = scanner.next();
 
-        scanner.close();
+                if (!restartInput.equalsIgnoreCase("Y") && !restartInput.equalsIgnoreCase("N")) {
+                    System.out.println("잘못 입력하셨습니다.");
+                } else if (restartInput.equalsIgnoreCase("N")) {
+                    break; // 게임 종료
+                }
+            }
+
+            first_check = false; // 처음 실행 이후에는 사용자에게 물어보도록 false로 변경
+
+            BaseballGame game = new BaseballGame();
+            game.playGame();
+
+        } while (true);
     }
 }
