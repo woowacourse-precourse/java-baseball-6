487536ff26197209bb11444b94f18ebadde3d300
0Chord <seaweed.0chord@gmail.com>
2023-10-23 01:59:12 +0900

    test : 테스트 0입력시 예외

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -76,6 +76,14 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 예외_테스트_0이_입력될때() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("012"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
 
     @Override
     public void runMain() {
