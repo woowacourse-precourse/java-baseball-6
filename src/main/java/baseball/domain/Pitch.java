2ecc864b6b722078489a7e75444db58a9295c3b6
yummygyudon <bang2brew@gmail.com>
2023-10-23 00:01:52 +0900

    DOCS : Pitch 도메인 주석 작성

--- src/main/java/baseball/domain/Pitch.java
+++ src/main/java/baseball/domain/Pitch.java
@@ -3,6 +3,11 @@ package baseball.domain;
 import java.util.List;
 
 public class Pitch extends DefaultData{
+    /**
+     * 사용자 입력 값을 기반으로 사용자의 투구 정보를 담는 객체
+     * @see DefaultData
+     * @throws IllegalArgumentException : MAX_LENGTH 가 아닌 리스트가 들어올 경우
+     */
     public Pitch(List<Integer> inputNumberList) {
         super();
         if (!(inputNumberList.size() == MAX_LENGTH)) {
