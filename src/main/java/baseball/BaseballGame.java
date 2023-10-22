34b1486a13c0e364dc1ea7e38d44f5aec3350b7b
LeeSeungUk <tmddnr5410@gmail.com>
2023-10-23 00:21:53 +0900

    feat : 랜덤한 숫자 생성 기능 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,13 +1,14 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class BaseballGame {
     public void startGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
 
         boolean isPlaying = true;
-
+        int answer = Randoms.pickNumberInRange(111, 999);
         do {
             System.out.print("숫자를 입력해주세요 : ");
             Console.readLine();
