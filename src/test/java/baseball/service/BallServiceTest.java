d3b4b3a779421b66ef069613638e57e07db8a4c2
inpink <inpink@kakao.com>
2023-10-23 04:28:15 +0900

    test: BallServiceTest 생성
    숫자가 완전 다른 경우, 같은 자리에 숫자가 있는 경우는 0Ball.
    다른 자리에 같은 숫자 있는 경우 1Ball.
    테스트 잘 됨.

---
+++ src/test/java/baseball/service/BallServiceTest.java
@@ -0,0 +1,60 @@
+package baseball.service;
+
+import baseball.domain.Baseball;
+import baseball.util.IntegerListUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BallServiceTest {
+
+    private final BallService ballService = BallService.getInstance();
+    private Baseball computerBaseball;
+    private Baseball inputBaseball;
+
+    @BeforeEach
+    void setup() {
+        computerBaseball = new Baseball();
+        inputBaseball = new Baseball();
+    }
+
+    @Test
+    void count_zeroBall() {
+        //given
+        computerBaseball.setValues(IntegerListUtil.parseIntegerList("123"));
+        inputBaseball.setValues(IntegerListUtil.parseIntegerList("456"));
+
+        //when
+        int count = ballService.count(computerBaseball, inputBaseball);
+
+        //then
+        assertThat(count).isZero();
+    }
+
+    @Test
+    void count_zeroBall_samePlace() {
+        //given
+        computerBaseball.setValues(IntegerListUtil.parseIntegerList("123"));
+        inputBaseball.setValues(IntegerListUtil.parseIntegerList("156"));
+
+        //when
+        int count = ballService.count(computerBaseball, inputBaseball);
+
+        //then
+        assertThat(count).isZero();
+    }
+
+    @Test
+    void count_oneBall() {
+        //given
+        computerBaseball.setValues(IntegerListUtil.parseIntegerList("123"));
+        inputBaseball.setValues(IntegerListUtil.parseIntegerList("256"));
+
+        //when
+        int count = ballService.count(computerBaseball, inputBaseball);
+
+        //then
+        assertThat(count).isOne();
+    }
+}
