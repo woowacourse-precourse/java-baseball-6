27cbb579892084c0fe4931d32cec2d888511133b
혜민 <alddlv81@gmail.com>
2023-10-22 18:56:53 +0900

    refactor: 결과 출력 함수, 정답 출력 함수 분리 및 결과 출력 간결화

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -100,21 +100,27 @@ public class Game {
     }
 
     boolean result(){
-        if(this.ball == 0 && this.strike == 0){
-            System.out.println("낫싱");
+        String result = "";
+        if (this.ball == 0 && this.strike == 0) {
+            result = "낫싱";
         }
-        else if(ball > 0 && strike == 0){
-            System.out.println(ball+"볼");
+        if (ball > 0) {
+            result += ball + "볼";
         }
-        else if(ball == 0 && strike > 0){
-            System.out.println(strike+"스트라이크");
-            if(strike == COUNT){
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                return true;
+        if (strike > 0) {
+            if (ball > 0) {
+                result += " ";
             }
+            result += strike + "스트라이크";
         }
-        else if(ball > 0 && strike > 0){
-            System.out.println(ball+"볼 "+strike+"스트라이크");
+        System.out.println(result);
+        return isGameEnded();
+    }
+
+    boolean isGameEnded() {
+        if (strike == COUNT) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
         }
         return false;
     }
