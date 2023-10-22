ed288f8fe4b93dc6c000e7bffb6af586eedd266e
Juhyorim <joohr1234@gmail.com>
2023-10-22 22:23:51 +0900

    refactor: 사용자 입력 검증 InputValidation클래스 구현, 객체 추가

--- src/main/java/baseball/ConsoleInput.java
+++ src/main/java/baseball/ConsoleInput.java
@@ -3,18 +3,29 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 public class ConsoleInput implements Input {
+    private InputValidation inputValidation;
+
+    public ConsoleInput() {
+        this.inputValidation = new InputValidation();
+    }
 
     private String getInput() {
         return Console.readLine().strip();
     }
 
     @Override
-    public String getRestartInput() {
-        return getInput();
+    public String getRestartInput() throws IllegalArgumentException {
+        String input = getInput();
+        inputValidation.validateRestartInput(input);
+
+        return input;
     }
 
     @Override
-    public String getGameInput() {
-        return getInput();
+    public String getGameInput() throws IllegalArgumentException {
+        String input = getInput();
+        inputValidation.validateGameInput(input);
+
+        return input;
     }
 }
