e6886d199433eb18d51936880ce40cf7749b13d2
cousim46 <wjdghldns24@naver.com>
2023-10-22 15:05:44 +0900

    Add : 랜덤한 값과 입력값을 비교하여 스트라이크 개수 계산해주는 메서드 추가

--- src/main/java/baseball/BallCountCalculate.java
+++ src/main/java/baseball/BallCountCalculate.java
@@ -1,5 +1,17 @@
 package baseball;
 
+import java.util.Arrays;
+
 public class BallCountCalculate {
 
+
+    public static int getStrike(String randomNum, String inputNum) {
+        int strikeCount = 0;
+        for (int i = 0; i < inputNum.length(); i++) {
+            if (randomNum.charAt(i) == inputNum.charAt(i)) {
+                strikeCount++;
+            }
+        }
+        return strikeCount;
+    }
 }
