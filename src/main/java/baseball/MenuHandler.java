5d85f025578b9e34bb1a84ff7976ab93aad37efc
Jay <injaesong0@gmail.com>
2023-10-23 01:53:04 +0900

    feat: call printStart in initialHandle of MenuHandler

--- src/main/java/baseball/MenuHandler.java
+++ src/main/java/baseball/MenuHandler.java
@@ -2,13 +2,16 @@ package baseball;
 
 public class MenuHandler {
     private final Computer computer;
+    private final Communicator communicator;
 
-    public MenuHandler(Computer computer) {
+    public MenuHandler(Computer computer, Communicator communicator) {
         this.computer = computer;
+        this.communicator = communicator;
         initialHandle();
     }
 
     private void initialHandle() {
+        communicator.printStart();
         computer.decideAnswer();
     }
 }
