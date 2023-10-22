95d4820b55bab736571d7c731c3db3db4fd54620
gmu1026 <gmu1026@gmail.com>
2023-10-22 13:55:07 +0900

    FEAT: 플레이어 공 입력 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,20 @@
 package baseball;
 
+import baseball.player.Player;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.print("숫자를 입력해주세요 : ");
+        String playerInputBall = Console.readLine();
+        String[] inputBallSplit = playerInputBall.split("");
+        List<Integer> playerBalls = new ArrayList<>();
+        for (String ball:inputBallSplit) {
+            playerBalls.add(Integer.valueOf(ball));
+        }
+
+        Player player = new Player(playerBalls);
     }
 }
