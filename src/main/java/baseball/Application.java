99cde70ae184a93c9ec87a31275a87fc579abcb9
saera <yook.saera@gmail.com>
2023-10-22 21:59:53 +0900

    feat: Application이 게임을 반복해서 진행하라

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,19 +1,33 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        ComNumber comNumber = new ComNumber();
-        comNumber.pickNumbers();
-        PlayerNumber playerNumber = new PlayerNumber();
-        CompareNumbers compareNumbers = new CompareNumbers();
-        while (true) {
-            System.out.print("숫자를 입력해주세요 : ");
-            playerNumber.input();
+        System.out.println("숫자 야구 게임을 시작합니다.");
 
-            String result = compareNumbers.compare(comNumber, playerNumber);
-            System.out.println(result);
-            if ("3스트라이크".equals(result)) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        while (true) {
+            ComNumber comNumber = new ComNumber();
+            comNumber.pickNumbers();
+
+            while (true) {
+                System.out.print("숫자를 입력해주세요 : ");
+
+                PlayerNumber playerNumber = new PlayerNumber();
+                playerNumber.input();
+
+                CompareNumbers compareNumbers = new CompareNumbers();
+                String result = compareNumbers.compare(comNumber, playerNumber);
+                System.out.println(result);
+
+                if ("3스트라이크".equals(result)) {
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    break;
+                }
+            }
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String playerInput = Console.readLine();
+            if ("2".equals(playerInput)) {
                 break;
             }
         }
