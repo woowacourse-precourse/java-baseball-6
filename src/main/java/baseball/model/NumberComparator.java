4788c7b62cbc0663abfa939ad47feb57ad52c9ca
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-10-22 20:46:24 +0900

    refactor : 접근제한자 변경 public -> private calculateResult메서드 내에 존재하는 메서드들은 private 접근제한자로 변경

--- src/main/java/baseball/model/NumberComparator.java
+++ src/main/java/baseball/model/NumberComparator.java
@@ -8,7 +8,7 @@ public class NumberComparator {
     /**
      * 스트라이크 개수 체크
      */
-    public void checkStrike(String userNumber, String programNumber) {
+    private void checkStrike(String userNumber, String programNumber) {
         for (int i = 0; i < userNumber.length(); i++) {
             if (userNumber.charAt(i) == programNumber.charAt(i)) {
                 strikes++;
@@ -19,7 +19,7 @@ public class NumberComparator {
     /**
      * 볼 개수 체크
      */
-    public void checkBall(String userNumber, String programNumber) {
+    private void checkBall(String userNumber, String programNumber) {
         for (int i = 0; i < userNumber.length(); i++) {
             if (programNumber.contains(String.valueOf(userNumber.charAt(i)))) {
                 balls++;
@@ -30,7 +30,7 @@ public class NumberComparator {
     /**
      * 스트라이크, 볼 개수 조정 위 로직에는 볼 개수를 셀 때, 스트라이크인 숫자를 제외하는 기능이 없다.
      */
-    public void updateBall() {
+    private void updateBall() {
         balls -= strikes;
         if (balls < 0) {
             balls = 0;
@@ -40,7 +40,7 @@ public class NumberComparator {
     /**
      * 새로운 게임이 시작되었을 때, 기존 게임의 결과 초기화
      */
-    public void resetResult() {
+    private void resetResult() {
         strikes = 0;
         balls = 0;
     }
