98e5e6c4fcd102d3ac4356bba16a66ab00ef68a1
Seol_JY <wlsdud5654@gmail.com>
2023-10-22 23:52:51 +0900

    refactor: 번호에 대한 유효성 검사를 추상클래스로 끌어올림
    - AbstractBaseballs 자체에 대한 무결성을 강화하기 위함
    - verify 접두어를 validate 접두어로 수정

--- src/main/java/baseball/domain/AbstractBaseballs.java
+++ src/main/java/baseball/domain/AbstractBaseballs.java
@@ -5,10 +5,23 @@ import java.util.List;
 abstract class AbstractBaseballs {
     protected static final int BASEBALLS_SIZE = 3;
 
-    protected List<Baseball> baseballs;
+    protected final List<Baseball> baseballs;
 
     protected AbstractBaseballs(List<Baseball> baseballs) {
+        validateSizeExact(baseballs);
+        validateNoDuplicate(baseballs);
         this.baseballs = baseballs;
     }
 
+    private void validateSizeExact(List<Baseball> baseballs) {
+        if (baseballs.size() != BASEBALLS_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNoDuplicate(List<Baseball> baseballs) {
+        if (baseballs.stream().distinct().count() != BASEBALLS_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
--- src/main/java/baseball/domain/UserBaseballs.java
+++ src/main/java/baseball/domain/UserBaseballs.java
@@ -1,24 +1,15 @@
 package baseball.domain;
 
+import java.util.Collections;
 import java.util.List;
 
 public class UserBaseballs extends AbstractBaseballs {
-
-    public UserBaseballs(List<Baseball> baseballs) {
+    protected UserBaseballs(List<Baseball> baseballs) {
         super(baseballs);
-        verifySizeExact(this.baseballs);
-        verifyNoDuplicate(this.baseballs);
     }
 
-    private void verifySizeExact(List<Baseball> baseballs) {
-        if (baseballs.size() != BASEBALLS_SIZE) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private void verifyNoDuplicate(List<Baseball> baseballs) {
-        if (baseballs.stream().distinct().count() != BASEBALLS_SIZE) {
-            throw new IllegalArgumentException();
-        }
+    public List<Baseball> getBaseballs() {
+        // 불변
+        return Collections.unmodifiableList(baseballs);
     }
 }
