ecde2c641e4dd00f8ccf5269505a2ae422bfe5bc
eunjin3481 <eunjin3481@gmail.com>
2023-10-22 21:27:42 +0900

    Feat: main 메서드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,18 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        boolean runFlag = true;
+
+        while(runFlag){
+            BaseballGame baseballGame = new BaseballGame();
+            baseballGame.play();
+            runFlag = askExit();
+        }
     }
     public static boolean askExit(){
     }
