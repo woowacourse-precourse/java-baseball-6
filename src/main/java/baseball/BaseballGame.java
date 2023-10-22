352eb1a507b64251648b6e2446851c6bd2d6be4a
김관진 <cozyschooll@gmail.com>
2023-10-22 23:11:58 +0900

    docs: readme 기능 목록 작성, generateAnswer 기능 구현

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,18 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class BaseballGame {
+
+    private int answer;
+
+    //게임을 생성합니다.
+    public BaseballGame() {
+
+    }
+
+    public int generateAnswer() {
+        return Randoms.pickNumberInRange(1, 9)*100 + Randoms.pickNumberInRange(1, 9)*10
+                + Randoms.pickNumberInRange(1, 9);
+    }
+}
