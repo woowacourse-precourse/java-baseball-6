9ee7b1a997aca5f75eb071cfc3bbdc4447c907bb
soyoungxxx <otherplet@gmail.com>
2023-10-23 04:11:14 +0900

    feat: GamePlaying.java 변수 추가

--- src/main/java/baseball/game/GamePlaying.java
+++ src/main/java/baseball/game/GamePlaying.java
@@ -1,4 +1,15 @@
 package baseball.game;
 
+import baseball.variable.ComputerNumber;
+import baseball.variable.PlayerNumber;
+
 public class GamePlaying {
+    private static final String STRIKE = "스트라이크";
+    private static final String BALL = "볼";
+    private static final String NOTHING = "낫싱";
+
+    ComputerNumber computerNumber = new ComputerNumber();
+    PlayerNumber playerNumber = new PlayerNumber();
+
+
 }
