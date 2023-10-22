92416e75e749864b009851d448fca8a6007d3ec3
ddolboghi <rhdfyd128@gmail.com>
2023-10-22 21:27:27 +0900

    refactor: User 숫자 검증 로직 분리

--- src/main/java/baseball/GameResult.java
+++ src/main/java/baseball/GameResult.java
@@ -12,16 +12,16 @@ public class GameResult {
         if (ball == 0 & strike == 0) {
             System.out.println("낫싱");
         }
-        if (ball != 0 & strike == 0) {
+        else if (ball != 0 & strike == 0) {
             System.out.println(ball+"볼");
         }
-        if (ball == 0 & strike > 0 & strike < 3) {
+        else if (ball == 0 & strike > 0 & strike < 3) {
             System.out.println(strike+"스트라이크");
         }
-        if (ball != 0 & strike > 0 & strike < 3) {
+        else if (ball != 0 & strike > 0 & strike < 3) {
             System.out.println(ball+"볼 "+strike+"스트라이크");
         }
-        if (strike == 3) {
+        else if (strike == 3) {
             System.out.println(strike+"스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         }
     }
--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -12,19 +12,25 @@ public class User {
         List<Integer> userNumbers = new ArrayList<>();
         System.out.print("숫자를 입력해주세요 : ");
         String userInput = readLine();
-
+        validateUserInput(userInput);
         for (int i=0;i<userInput.length();i++) {
             String str = userInput.substring(i, i+1);
             userNumbers.add(Integer.parseInt(str));
         }
-        validateUserNumber(userNumbers);
+        validateDuplication(userNumbers);
         return userNumbers;
     }
 
-    private static void validateUserNumber(List<Integer> userNumbers) {
-        if (userNumbers.size() != Constants.BASEBALL_NUMBER_SIZE) {
-            throw new IllegalArgumentException("숫자 3개만 입력해주세요.");
+    private static void validateUserInput(String userInput) {
+        if (userInput.length() != Constants.BASEBALL_NUMBER_SIZE) {
+            throw new IllegalArgumentException("숫자 3개를 입력해주세요.");
         }
+        if (userInput.contains("0")) {
+            throw new IllegalArgumentException("각 자리의 숫자는 1부터 9까지만 입력해주세요.");
+        }
+    }
+
+    private static void validateDuplication(List<Integer> userNumbers) {
         Set<Integer> userNumbersSet = new HashSet<>(userNumbers);
         if (userNumbersSet.size() != userNumbers.size()) {
             throw new IllegalArgumentException("서로 다른 숫자 3개를 입력해주세요.");
