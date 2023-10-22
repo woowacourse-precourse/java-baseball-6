bbdfc1a5c9c63bc6d1d9035de93dbebfbc09dd45
mdgome <mdgome3@gmail.com>
2023-10-22 21:14:40 +0900

    Feat: 유효성 검사 추가
    입력 받은 값 유효성 검사
    
    예외 처리 정리:
    - 잘못된 값을 입력 시 `IllegalArgumentException`를 발생시킨 후 애플리케이션은 종료되어야 함.
      - 0부터 9까지의 숫자를 입력하지 않은 경우
      - 3자리 숫자가 아닌 경우
      - 숫자가 아닌 문자를 입력한 경우
      - 중복된 숫자가 존재하는 경우
        - 예시) 121, 277, 883 등

--- src/main/java/baseball/validation/InputValidator.java
+++ src/main/java/baseball/validation/InputValidator.java
@@ -1,9 +1,11 @@
 package baseball.validation;
 
-    import baseball.constant.NumberConst;
-    import baseball.util.MessageUtil;
+import baseball.constant.NumberConst;
+import baseball.util.MessageUtil;
 
-    import java.util.List;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputValidator {
     // TODO: 입력받은 값의 유효성 검사
@@ -19,6 +21,10 @@ public class InputValidator {
      * 반환 값 : 리스트(숫자)
      */
     private List<Integer> convertStringToList(String input) {
+        // TODO: String 타입을 List 타입으로 변환하여 반환
+        return Arrays.stream(input.split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 
     /*
@@ -32,6 +38,21 @@ public class InputValidator {
      */
     public List<Integer> validateInputValueAndReturnList(String inputValue) {
         // TODO: 입력받은 값 유효성 검사
+        List<Integer> result;
+
+        if (inputValue == null) {
+            throw new IllegalArgumentException();
+        }
+
+        validateInputIsNumeric(inputValue); // 입력받은 값이 숫자만 있는지 확인
+
+        result = convertStringToList(inputValue); // 숫자만 있을 경우 String 타입을 List<Integer> 타입으로 변경
+        validateInputIsOneToNine(result); // 입력받은 값 중 1부터 9사이만 존재하는지 확인
+
+        validateInputDuplicated(result); // 입력받은 값 중 중복 숫자가 있는지 확인
+        validateInputLength(result); // 입력받은 값이 3글자 초과 혹은 미만인지 확인
+
+        return result;
     }
 
     /*
@@ -41,6 +62,10 @@ public class InputValidator {
      */
     public static void validationRestartAndStopValue(int inputValue) {
         // TODO: 게임 종료 이후 1,2 값만 입력하였는지 유효성 검사
+        if (inputValue != 1 && inputValue != 2) {
+            MESSAGE_UTIL.printInvalidGameControlInput();
+            throw new IllegalArgumentException();
+        }
     }
 
     /* *
@@ -50,6 +75,10 @@ public class InputValidator {
      */
     private static void validateInputDuplicated(List<Integer> inputValue) {
         // TODO: 입력받은 값 중복 숫자 유효성 검사
+        if (inputValue.size() != inputValue.stream().distinct().count()) {
+            MESSAGE_UTIL.printDuplicatedInputNumber();
+            throw new IllegalArgumentException();
+        }
     }
 
     /* *
@@ -59,6 +88,10 @@ public class InputValidator {
      */
     private static void validateInputLength(List<Integer> inputValue) {
         // TODO: 입력받은 값이 inputNumLength 와 같은지 유효성 검사
+        if (inputValue.size() != INPUT_NUM_LENGTH) {
+            MESSAGE_UTIL.printInvalidNumber();
+            throw new IllegalArgumentException();
+        }
     }
 
 
@@ -69,14 +102,24 @@ public class InputValidator {
      */
     private static void validateInputIsNumeric(String inputValue) {
         // TODO: 입력받은 값이 숫자인지 유효성 검사
+        try {
+            Integer.parseInt(inputValue);
+        } catch (NumberFormatException exception) {
+            MESSAGE_UTIL.printInvalidNumeric();
+            throw new IllegalArgumentException();
+        }
     }
 
     /*
-     * 입력받은 값이 VALIDATION_MIN_VALUE부터 VALIDATION_MAX_VALUE사이만 존재하는지 유효성 검사
+     * 입력받은 값이 1부터 9사이만 존재하는지 유효성 검사
      * 매개변수 : 유저에게 입력받은 값
-     * VALIDATION_MIN_VALUE부터부터 VALIDATION_MAX_VALUE사이 값이 아닌 경우 IllegalArgumentException 발생
+     * 1부터 9사이 값이 아닌 경우 IllegalArgumentException 발생
      */
     private static void validateInputIsOneToNine(List<Integer> inputValue) {
-        // TODO: 입력받은 값이 VALIDATION_MIN_VALUE부터 ALIDATION_MAX_VALUE사이 값인지 유효성 검사
+        // TODO: 입력받은 값이 VALIDATION_MIN_VALUE 부터 VALIDATION_MAX_VALUE 사이 존재 하는지 유효성 검사
+        if (!inputValue.stream().allMatch(element -> element >= VALIDATION_MIN_VALUE && element <= VALIDATION_MAX_VALUE)) {
+            MESSAGE_UTIL.printInvalidRange();
+            throw new IllegalArgumentException();
+        }
     }
 }
