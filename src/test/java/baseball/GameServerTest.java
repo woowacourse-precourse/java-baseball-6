0f0048589e1e3b2000b61bcfa4e5b45c9397e11a
yubin <dbqls2821@gmail.com>
2023-10-22 20:57:49 +0900

    test: 게임 클리어 후 계속해서 재시작하는 시나리오에 대한 테스트코드

--- src/test/java/baseball/GameServerTest.java
+++ src/test/java/baseball/GameServerTest.java
@@ -21,6 +21,17 @@ class GameServerTest extends NsTest {
         );
     }
 
+    @Test
+    void 클리어_후_계속해서_재시작할_수_있는가() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("246", "135", "1", "264", "246", "1", "573", "357", "2");
+                    assertThat(output()).contains("시작", "숫자를 입력", "낫싱", "3스트라이크", "3개의 숫자를 모두 맞히셨습니다! 게임 종료", "새로 시작하려면 1, 종료하려면 2를", "2볼 1스트라이크", "3볼");
+                },
+                1, 3, 5, 2, 4, 6, 3, 5, 7
+        );
+    }
+
 
     @Override
     protected void runMain() {
