505b6fa0b5d995018eb0700263f92d7182bceea3
toychip <dlawnsgud427@naver.com>
2023-10-22 21:50:04 +0900

    refactor: ComputerNumber static factory 메서드 적용

--- src/main/java/baseball/model/ComputerNumber.java
+++ src/main/java/baseball/model/ComputerNumber.java
@@ -5,12 +5,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ComputerNumber {
-
+    private final List<Integer> numberList;
     private final static int SIZE = 3;
     private final static int START_INCLUSIVE = 1;
     private final static int END_INCLUSIVE = 9;
 
-    public static List<Integer> getComputerNumber() {
+    private ComputerNumber(List<Integer> numberList) {
+        this.numberList = numberList;
+    }
+    public static ComputerNumber getComputerNumber() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < SIZE) {
             int randomNumber = Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
@@ -18,6 +21,10 @@ public class ComputerNumber {
                 computer.add(randomNumber);
             }
         }
-        return computer;
+        return new ComputerNumber(computer);
+    }
+
+    public List<Integer> getNumberList() {
+        return numberList;
     }
 }
