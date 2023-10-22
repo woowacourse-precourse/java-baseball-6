46850a28e9a8efa2b840a47b4e864bec42403d4d
seungheyon <fhzktm@naver.com>
2023-10-22 23:31:15 +0900

    기능 구현 완료 : 테스트 코드 통과

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,34 +10,85 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        baseballStart();
+
+    }
+
+    public static void baseballStart(){
         System.out.println("숫자 야구 게임을 시작합니다.");
 
+        int gameSize = 3;
+
         List<Integer> computer = new ArrayList<>();
-        while(computer.size() < 3){
+        while (computer.size() < gameSize) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if(!computer.contains(randomNumber)){
+            if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
         }
 
-        String inputString = readLine();
+        int strike = 0;
+        int ball = 0;
+        while (true) {
+            System.out.print("숫자를 입력해 주세요 : ");
+            String inputString = readLine();
 
-        List<Integer> inputNumber = new ArrayList<>();
-        if(inputString.length() != 3){
-            throw new IllegalArgumentException();
-        }
-        for(int i=0;i<3;i++){
-            try{
-                int number = Integer.parseInt(inputString.substring(i,i+1));
-                if(inputNumber.contains(number)){
-                    throw new IllegalArgumentException();
-                }
-                inputNumber.add(number);
-            }
-            catch (NumberFormatException e){
+            List<Integer> inputNumber = new ArrayList<>();
+            if (inputString.length() != gameSize) {
                 throw new IllegalArgumentException();
             }
-        }
+            for (int i = 0; i < gameSize; i++) {
+                try {
+                    int number = Integer.parseInt(inputString.substring(i, i + 1));
+                    if (inputNumber.contains(number)) {
+                        throw new IllegalArgumentException();
+                    }
+                    inputNumber.add(number);
+                } catch (NumberFormatException e) {
+                    throw new IllegalArgumentException();
+                }
+            }
 
+            strike = 0;
+            ball = 0;
+            for (int i = 0; i < gameSize; i++) {
+                if (computer.get(i).equals(inputNumber.get(i))) {
+                    strike++;
+                } else if (computer.contains(inputNumber.get(i))) {
+                    ball++;
+                }
+            }
+
+            if (ball > 0) {
+                System.out.print(ball + "볼 ");
+            }
+            if (strike > 0) {
+                System.out.print(strike + "스트라이크");
+            }
+            if (strike == 0 && ball == 0) {
+                System.out.print("낫싱");
+            }
+            System.out.println();
+
+            if (strike == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            }
+        }
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+        String num = readLine();
+        if (num.equals("1")) {
+            System.out.println("게임을 다시 시작합니다");
+            baseballStart();
+        }
+        else if(num.equals("2")){
+            System.out.println("프로그램을 종료합니다.");
+            return;
+        }
+        else{
+            throw new IllegalArgumentException();
+        }
     }
 }
+
