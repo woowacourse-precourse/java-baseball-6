59366cebb5280a34330b9f538025ab238063413e
Sasha <106591445+kchaeys2@users.noreply.github.com>
2023-10-22 15:54:33 +0900

    refactor (User) : 예외처리 IllegalStateException -> IllegalArgumentException 로 수정

--- src/main/java/baseball/DTO/User.java
+++ src/main/java/baseball/DTO/User.java
@@ -1,6 +1,5 @@
 package baseball.DTO;
 
-import javax.swing.text.TableView;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -18,18 +17,18 @@ public class User {
     }
     public void checkSize(String inputNumbers){
         if (inputNumbers.length() != 3){
-            throw new IllegalStateException("3자리 숫자여야 합니다.");
+            throw new IllegalArgumentException("3자리 숫자여야 합니다.");
         }
     }
     public void checkRange(List<Integer> userNumbers){
         if(!userNumbers.stream().allMatch(c -> c > 0 && c < 10)){
-            throw new IllegalStateException("1~9사이 숫자여야 합니다.");
+            throw new IllegalArgumentException("1~9사이 숫자여야 합니다.");
         }
     }
     public void checkDuplicate(List<Integer> userNumbers){
         Set<Integer> userNumberSet = new HashSet<>(userNumbers);
         if (userNumbers.size() != userNumberSet.size()){
-            throw new IllegalStateException("중복된 숫자가 있습니다.");
+            throw new IllegalArgumentException("중복된 숫자가 있습니다.");
         }
     }
     public List<Integer> parseStringToInteger(String inputNumbers){
