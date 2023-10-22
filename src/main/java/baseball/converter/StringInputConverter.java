bbe858b37e9c8651c57dcf1e63eabcebdc1c9aa3
devholic22 <hyunjoon.tech@gmail.com>
2023-10-22 14:56:51 +0900

    Refactor: StringInputConverter 필요없는 메서드 삭제

--- src/main/java/baseball/converter/StringInputConverter.java
+++ src/main/java/baseball/converter/StringInputConverter.java
@@ -1,8 +1,5 @@
 package baseball.converter;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class StringInputConverter {
 
     public static int toInt(final String input) {
@@ -11,15 +8,5 @@ public class StringInputConverter {
 
     public static String[] toArray(final String input) {
         return input.split("");
-    }
-
-    public static Set<Character> toCharSet(final String input) {
-        HashSet<Character> inputSet = new HashSet<>();
-
-        for (char character : input.toCharArray()) {
-            inputSet.add(character);
-        }
-
-        return inputSet;
     }
 }
