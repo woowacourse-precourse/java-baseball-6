1d09875c61b8f73ffb1e945ecad986a75a171857
mmalfoy <stpmalfoy@gmail.com>
2023-10-22 13:13:36 +0900

    FEAT : 구현기능 Clss 구현(기능목록 1~11)

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -7,6 +7,10 @@ import java.util.List;
 
 public class BaseballGame {
     private static List<Integer> numOfComputer;
+    private static List<Integer> userInput;
+    private static boolean isThreeStrike;
+    private static int ball;
+    private static int strike;
 
     private static void setNumOfComputer(){
         numOfComputer = new ArrayList<>();
@@ -23,9 +27,13 @@ public class BaseballGame {
         setNumOfComputer();
         try{
             do{
+                initGameMembers();
                 Baseball_Input.setUserInput();
+                userInput = Baseball_Input.getUserInput();
+                countBallandStrike();
+                printBallandStrike();
 
-            }while(true);
+            }while(!isThreeStrike);
 
         }catch (IllegalArgumentException e){
             throw e;
@@ -33,11 +41,64 @@ public class BaseballGame {
 
     }
 
+    public static void countBallandStrike(){
+
+        for (int userIndex = 0; userIndex  < 3; ++userIndex) {
+            int tmpUser = userInput.get(userIndex);
+            for(int comIndex = 0; comIndex < 3; comIndex++){
+                int tmpCom = numOfComputer.get(comIndex);
+                if(tmpUser == tmpCom && userIndex == comIndex){
+                    strike +=1;
+                    continue;
+                }
+                if(tmpUser == tmpCom){
+                    ball +=1;
+                }
+            }
+        }
+
+    }
+
+    public static void printBallandStrike(){
+
+        if(ball == 0 && strike == 0){
+            Baseball_output.printNothing();
+            return;
+        }
+
+        if(ball == 0 && strike != 0){
+            Baseball_output.printStrike(strike);
+            if(strike == 3) {
+                Baseball_output.printGameEnd();
+                isThreeStrike = true;
+                return;
+            }
+            return;
+        }
+
+        if(ball != 0 && strike == 0){
+            Baseball_output.printBall(ball);
+            return;
+        }
+
+        Baseball_output.printBallAndStrike(ball,strike);
+
+    }
+
+    public static void initGameMembers(){
+        isThreeStrike = false;
+        ball = 0;
+        strike = 0;
+        return;
+    }
+
     public static void run(){
         try{
             do{
                 startGame();
-            }while(Baseball_Input.setRestartNum());
+                Baseball_output.printRestart();
+                Baseball_Input.setRestartNum();
+            }while(Baseball_Input.getRestartNum() == 1);
 
         }catch (IllegalArgumentException e){
             throw e;
