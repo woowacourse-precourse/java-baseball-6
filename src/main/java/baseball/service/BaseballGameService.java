ac105c6698eda9c37aecb7dc00192c83e037c123
박형균 <phk@baghyeong-gyun-ui-MacBookPro.local>
2023-10-22 13:57:08 +0900

    feat: create and implement
    *컴퓨터숫자를 셋팅하는 기능 구현
    *게임의 결과(볼,스트라이크의 갯수)를 가져오는 기능 구현
    *게임 재시작 여부를 체크하는 기능 구현

---
+++ src/main/java/baseball/service/BaseballGameService.java
@@ -0,0 +1,30 @@
+package baseball.service;
+
+import baseball.model.BaseballGameResult;
+import baseball.model.ComputerNumber;
+import baseball.model.UserNumber;
+
+import java.util.List;
+
+public class BaseballGameService {
+
+    private List<Integer> computerNumber;
+
+    public void setComputerNumber() {
+        this.computerNumber = new ComputerNumber().getNumber();
+        System.out.println(computerNumber);
+    }
+
+    public BaseballGameResult getGameResult(String input) {
+        List<Integer> userNumber = new UserNumber(input).getNumber();
+        return new BaseballGameResult(userNumber, computerNumber);
+    }
+
+    public boolean wantRestart(String input) {
+        if (input.equals("1")) return true;
+        if (input.equals("2")) return false;
+        throw new IllegalArgumentException();
+    }
+
+
+}
