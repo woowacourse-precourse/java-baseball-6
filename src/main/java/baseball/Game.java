5923b2f6bf8143f292c4d99364b1c2720f2e9ff5
chowoni <rnjschdnjs@naver.com>
2023-10-22 11:50:27 +0900

    Feat(재시작 기능)

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -7,139 +7,145 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 public class Game {
-	List<Integer> comNum = new ArrayList<>(); //컴퓨터 생성 랜덤 숫자
-	List<Integer> inputNum = new ArrayList<>(); //입력값
+	List<Integer> comNum = new ArrayList<>(); 
+	List<Integer> inputNum = new ArrayList<>();
 
-	 //게임 시작
 	public static void init() {
 		Game game = new Game();
 		System.out.println(Constants.START_MSG);
 		game.ready();
 	}
-	
-	//랜덤 수 생성
+
 	private void ready() {
-		while(comNum.size() < Constants.DIGIT_SIZE) { //서로 다른 세자리 수 생성
-	         int randomNum = Randoms.pickNumberInRange(Constants.START_RANGE, Constants.END_RANGE);
-	         if(!comNum.contains(randomNum)) {
-	            comNum.add(randomNum);
-	         }
-	      } 
-		
+		while (comNum.size() < Constants.DIGIT_SIZE) {
+			int randomNum = Randoms.pickNumberInRange(Constants.START_RANGE, Constants.END_RANGE);
+			if (!comNum.contains(randomNum)) {
+				comNum.add(randomNum);
+			}
+		}
 		getInputNum();
 	}
-	
+
 	private void getInputNum() {
-		while(true) {
+		while (true) {
 			System.out.print(Constants.INPUT_MSG);
 			String input_str = Console.readLine();
 			checkCondition(input_str);
 			inputNum = setInputNum(input_str);
-		
-			if(checkResult() == true) //3스트라이크, 게임 종료
+
+			if (checkResult() == true) //3스트라이크, 게임 종료
 				break;
 		}
-		printStrike();
 	}
-	
+
 	private void checkCondition(String str) {
 		isRightLength(str);
 		isRightDigit(str);
 		isNotDuplicate(str);
 	}
-	
-	private void isRightLength(String str) { //입력된 값이 3자리 수인지
-		if(str.length() != Constants.DIGIT_SIZE)
+
+	private void isRightLength(String str) {
+		if (str.length() != Constants.DIGIT_SIZE)
 			throw new IllegalArgumentException(Constants.WRONG_LENGTH_ERROR);
 	}
-	
-	private void isRightDigit(String str) { //입력된 값이 0을 제외한 정수인지
+
+	private void isRightDigit(String str) {
 		String tmp = "^[1-9]*$";
-		if(Pattern.matches(tmp, str) != true)
+		if (Pattern.matches(tmp, str) != true)
 			throw new IllegalArgumentException(Constants.WRONG_INPUT_ERROR);
 	}
-	
-	private void isNotDuplicate(String str) { //중복되는 수가 있는지
-		for(int i = 0; i < str.length(); i++) {
+
+	private void isNotDuplicate(String str) {
+		for (int i = 0; i < str.length(); i++) {
 			int firstInd = str.indexOf(str.charAt(i));
-			if(firstInd != i)
+			if (firstInd != i)
 				throw new IllegalArgumentException(Constants.WRONG_INPUT_ERROR);
 		}
 	}
-	
-	private List<Integer> setInputNum(String str){ //입력값 타입 변경
+
+	private List<Integer> setInputNum(String str) {
 		List<Integer> tmp = new ArrayList<>();
-		for(int j = 0; j < str.length(); j++) {
+		for (int j = 0; j < str.length(); j++) {
 			int num = Character.getNumericValue(str.charAt(j));
 			tmp.add(num);
 		}
-		
 		return tmp;
 	}
-	
-	//결과
+
 	private boolean checkResult() {
-	      int strike = getStrike();
-	      int ball = StrikeOrBall() - strike;
+		int strike = getStrike();
+		int ball = StrikeOrBall() - strike;
 
-	      if(strike == Constants.DIGIT_SIZE)
-	         return true;
-	      else {
-	         if(strike == 0 && ball == 0)//낫싱
-	            printNothing();
-	         else
-	            printBall(strike, ball);
-	      }
-	      return false;
-	   }
-
-	
-	private void printStrike() { //3스트라이크 결과 출력
-	      System.out.println(Constants.SUCCESS_STR);
-	      System.out.println(Constants.END_MSG); //success + restart 고려
-	        
-	        //restart() 재시작여부 확인 함수
+		if (strike == Constants.DIGIT_SIZE)
+			printStrike();
+		else {
+			if (strike == 0 && ball == 0)
+				printNothing();
+			else
+				printBall(strike, ball);
+		}
+		return true;
 	}
-	
+
+	private void printStrike() {
+		System.out.println(Constants.SUCCESS_STR);
+		System.out.println(Constants.END_MSG);
+		
+		if (restart() == true)
+			init();
+	}
+
 	private void printNothing() {
-	      System.out.println(Constants.NOTHING_STR);
+		System.out.println(Constants.NOTHING_STR);
+		getInputNum();
 	}
-	
+
 	private void printBall(int str, int b) {
-	      if(b != 0) {
-	         if(str > 0) //볼 + 스트라이크
-	            System.out.print(b + "볼 ");
-	         else //3볼
-	            System.out.println(b + "볼");
-	      }
-	      printResult(str);
+		if (b != 0) {
+			if (str > 0) //볼 + 스트라이크
+				System.out.print(b + "볼 ");
+			else //3볼
+				System.out.println(b + "볼");
+		}
+		printResult(str);
 	}
-	
-	private void printResult(int str) { //스트라이크, 스트라이크 + 볼, 볼
-	      if(str != 0)
-	         System.out.println(str + "스트라이크");
-	      
-	      getInputNum();
+
+	private void printResult(int str) {
+		if (str != 0)
+			System.out.println(str + "스트라이크");
+
+		getInputNum();
 	}
 
 	private int StrikeOrBall() {
 		int cnt = 0;
-		for(int k = 0; k <Constants.DIGIT_SIZE; k++)
-			if(inputNum.contains(comNum.get(k)) == true)
+		for (int k = 0; k < Constants.DIGIT_SIZE; k++)
+			if (inputNum.contains(comNum.get(k)) == true)
 				cnt++;
-		
+
 		return cnt;
 	}
 
 	private int getStrike() {
 		int cnt = 0;
-		for(int k = 0; k <Constants.DIGIT_SIZE; k++) {
-			if(inputNum.contains(comNum.get(k)) == true) {//랜덤 수와 입력값에 중복 존재(스트라이크 or ball)
-				if(inputNum.get(k) == comNum.get(k)) //같은 자리
+		for (int k = 0; k < Constants.DIGIT_SIZE; k++) {
+			if (inputNum.contains(comNum.get(k)) == true) {
+				if (inputNum.get(k) == comNum.get(k))
 					cnt++;
 			}
 		}
 		return cnt;
 	}
+
+	private boolean restart() {
+		System.out.println(Constants.RESTART_MSG);
+		String reply = Console.readLine();
+				
+		if (reply.equals(Constants.INPUT_RESTART_STR))
+			return true;
+		else if (reply.equals(Constants.INPUT_TERMINATE_STR))
+			return false;
+		else
+			throw new IllegalArgumentException(Constants.WRONG_INPUT_ERROR);
+	}
 }
-	
