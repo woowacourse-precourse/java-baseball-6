fc9a5630bf1ec42b8d90df24c57d26bdcd1b8447
Sanghyeon Sim <5338095@gmail.com>
2023-10-22 23:45:56 +0900

    feat: 컴퓨터가 선택한 숫자를 고르는 함수 구현
    - randomNumber를 pickNumberInRange 함수를 사용하여 뽑는다.
    - 기존에 값이 있다면 다시 뽑고 아니라면 추가한다.
    - size가 3이 되면 종료한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,5 +8,15 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+    private static void pickComputerNumber(List<Integer> computerNumber) {
+        int randomNumber;
+        while (computerNumber.size() < 3) {
+            randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumber.contains(randomNumber)) {
+                computerNumber.add(randomNumber);
+            }
+        }
+    }
     }
 }
