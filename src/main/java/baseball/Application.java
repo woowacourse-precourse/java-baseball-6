8bb79d8828d84ad4b972561f88987244effde217
KyungsooLee <jmtkd9196@gmail.com>
2023-10-23 03:58:04 +0900

    feat: 유효성 검사함수 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,6 +5,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+    // 입력된 숫자 유효성 검사
+    public static boolean checkValidation(String inputString) {
+        // 입력된 숫자가 세 자리인지 체크
+        if (inputString.length() != 3) {
+            throw new IllegalArgumentException("세 자리 숫자를 입력해주세요. 프로그램을 종료합니다.");
+        }
+
+        // 입력된 숫자가 (0~9)사이의 문자인지 체크
+        if (!checkNumber(inputString)) {
+            throw new IllegalArgumentException("숫자가 아닙니다. 프로그램을 종료합니다.");
+        }
+
+        // 입력된 숫자의 중복 체크
+        if (!checkDuplication(inputString)) {
+            throw new IllegalArgumentException("중복된 값을 입력했습니다. 프로그램을 종료합니다.");
+        }
+
+        return true;
+    }
+
     // 컴퓨터가 랜덤 넘버를 생성
     public static List<Integer> createRandomNumber() {
         List<Integer> computer = new ArrayList<>();
