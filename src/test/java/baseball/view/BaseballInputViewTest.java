aa8f72ea628c294f6b75c621258094c3913b004d
kunsanglee <gosmdochee2@gmail.com>
2023-10-22 21:14:46 +0900

    refactor(BaseballInputViewTest): 테스트 코드 수정 BaseballInputView의 continueOrExit() 메서드와 InputValidator의 검증로직을 분리.

--- src/test/java/baseball/view/BaseballInputViewTest.java
+++ src/test/java/baseball/view/BaseballInputViewTest.java
@@ -53,7 +53,8 @@ public class BaseballInputViewTest {
     void baseballNumberWithInputValidatorFailTest(String input, String message) {
         System.setIn(generateUserInput(input));
         BaseballInputView baseballInputView = new BaseballInputView();
-        assertThatCode(baseballInputView::inputBaseballNumber)
+        String inputBaseballNumbers = baseballInputView.inputBaseballNumber();
+        assertThatCode(() -> InputValidator.validateBaseballNumber(inputBaseballNumbers))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(message);
         assertThat(output.toString()).isEqualTo("숫자를 입력해주세요 : ");
@@ -75,7 +76,9 @@ public class BaseballInputViewTest {
     void continueOrExitSuccessTest(String input, boolean expected) {
         System.setIn(generateUserInput(input));
         BaseballInputView baseballInputView = new BaseballInputView();
-        boolean isContinue = baseballInputView.continueOrExit();
+        baseballInputView.continueOrExit();
+        assertThat(output.toString()).isEqualTo("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+        boolean isContinue = baseballInputView.isContinue(input);
         assertThat(isContinue).isEqualTo(expected);
     }
 
@@ -86,13 +89,14 @@ public class BaseballInputViewTest {
         );
     }
 
-    @DisplayName("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. 출력하고 사용자의 입력을 받는다.")
+    @DisplayName("사용자의 입력이 1 또는 2가 아니라면 IllegalArgumentException 에러가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"-1", "0", "3", "가나다", "abc"})
     void continueOrExitFailTest(String input) {
         System.setIn(generateUserInput(input));
         BaseballInputView baseballInputView = new BaseballInputView();
-        assertThatCode(baseballInputView::continueOrExit)
+        String inputContinueOrExit = baseballInputView.continueOrExit();
+        assertThatCode(() -> InputValidator.validateContinueOrExitNumber(inputContinueOrExit))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
