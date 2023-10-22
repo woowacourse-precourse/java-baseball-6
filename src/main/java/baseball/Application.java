532d238fc014865298bdb12fdebb8178f8c39e36
Moly <102847513+jminkkk@users.noreply.github.com>
2023-10-22 20:43:07 +0900

    fix: 게임 재시작 시 Computer, Game 초기화가 안되는 버그 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,14 +2,12 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // 게임 시작 전
-        Computer computer = new Computer();
-        Player player = new Player();
-        Judge judge = new Judge();
-
-        Game game = new Game(computer, player, judge);
-
         while (true) {
+            Computer computer = new Computer();
+            Player player = new Player();
+            Judge judge = new Judge();
+
+            Game game = new Game(computer, player, judge);
             game.start();
 
             if (!game.isRestart()) break;
