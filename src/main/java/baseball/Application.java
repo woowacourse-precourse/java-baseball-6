d990396ed8a671626bab3e58da0edf27cc337ec5
gahyeon <1000ji1@naver.com>
2023-10-22 14:36:35 +0900

    FEAT/Create while frame

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,19 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (true) {
+
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String userInput = readLine();
+            if (userInput.equals("1")) {
+                continue;
+            } else if (userInput.equals("2")) {
+                break;
+            }
+        }
     }
 }
