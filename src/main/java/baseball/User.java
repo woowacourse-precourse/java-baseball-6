445ad0422f7bf4d58c79ed9a5e8869bc767735b4
minwoo <mark42son@gmail.com>
2023-10-22 21:31:21 +0900

    feat : 사용자 숫자 입력 검증 메서드 분리 및 에러메세지 수정

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,46 +1,51 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
 
 public class User {
+    private static final int NUMBER_LENGTH = 3;
+    private static final String OUTPUT_ENTER_NUMBER = "숫자를 입력해주세요 : ";
+    private static final Pattern INPUT_REGEX = Pattern.compile("[1-9]{3,3}");
+    private static final String EXCEPTION_MESSAGE = "Please enter a three-digit natural number consisting of 1 to 9.";
+    int userNumber;
 
-    int userNum;
+    public User() {}
 
-    public User() {
-
-    }
-
-    public void setUserNum() {
-        System.out.println("숫자를 입력해주세요 : ");
-        String input = inputUserNum();
+    public void setUserNumber() {
+        System.out.print(OUTPUT_ENTER_NUMBER);
+        String input = inputUserNumber();
         validateInput(input);
-        this.userNum = changeToInteger(input);
+        this.userNumber = Integer.parseInt(input);
+    }
+    // method only for test code
+    public void setUserNumber(String input) {
+        validateInput(input);
+        this.userNumber = Integer.parseInt(input);
     }
 
-    public int getUserNum() {
-        return userNum;
+    public int getUserNumber() {
+        return userNumber;
     }
 
-    private static String inputUserNum() {
+    private static String inputUserNumber() {
         return Console.readLine();
     }
 
     private void validateInput(String input) {
-        int number = changeToInteger(input);
-        if (number < 1 || number > 9) {
-            throw new IllegalArgumentException("서로다른 세 자리 자연수를 입력해주세요.");
-        }
+        validateInputLength(input);
+        validateInputRegex(input);
+    }
 
-        if (input.length() != 3) {
-            throw new IllegalArgumentException("입력 숫자의 자릿수는 3자리로 입력해주세요.");
+    private void validateInputRegex(String input) {
+        if(!INPUT_REGEX.matcher(input).matches()) {
+            throw new IllegalArgumentException(EXCEPTION_MESSAGE);
         }
     }
 
-    private static int changeToInteger(String input) {
-        try {
-            return Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("서로다른 세 자리 자연수를 입력해주세요.");
+    private static void validateInputLength(String input) {
+        if(input.length() != NUMBER_LENGTH) {
+            throw new IllegalArgumentException(EXCEPTION_MESSAGE);
         }
     }
 }
