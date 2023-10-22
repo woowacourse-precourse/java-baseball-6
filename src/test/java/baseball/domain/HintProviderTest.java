9e13c60f44b1f3b41c4a39cea78523357d2e10eb
howudong <tjdvy963@naver.com>
2023-10-22 18:19:17 +0900

    test : 힌트 관련 테스트 HintProvider로 옮김

--- src/test/java/baseball/domain/HintProviderTest.java
+++ src/test/java/baseball/domain/HintProviderTest.java
@@ -1,5 +1,10 @@
 package baseball.domain;
 
+import baseball.domain.hints.HintProvider;
+import baseball.domain.hints.HintType;
+import baseball.domain.numbers.AnswerNumbers;
+import baseball.domain.numbers.NumberGenerator;
+import baseball.domain.numbers.PlayerNumbers;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,17 +12,17 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.Map;
 
-class RefereeTest {
-    private final AnswerNumbers answerNumbers = AnswerNumbers.createAnswerNumbers(new TestGenerator());
-    private final Referee referee = new Referee(answerNumbers);
+class HintProviderTest {
+    private final List<Integer> answerNumbers = AnswerNumbers.createAnswerNumbers(new TestGenerator()).getAnswerNumbers();
+    private final HintProvider hintProvider = new HintProvider();
 
     @Test
     @DisplayName("플레이어의 숫자가 1,2,3 일 때 STRIKE가 3개가 나오면 성공한다.")
     void STRIKE_3() {
         //given
-        PlayerNumbers playerNumbers = new PlayerNumbers(List.of(1, 2, 3));
+        List<Integer> playerNumbers = new PlayerNumbers(List.of(1, 2, 3)).getNumbers();
         //when
-        Map<HintType, Integer> hints = referee.getHint(playerNumbers);
+        Map<HintType, Integer> hints = hintProvider.compareNumbers(answerNumbers, playerNumbers);
         //then
         Assertions.assertThat(hints.get(HintType.STRIKE)).isEqualTo(3);
     }
@@ -26,9 +31,9 @@ class RefereeTest {
     @DisplayName("플레이어의 숫자가 3,1,2 일 때 BALL이 3개가 나오면 성공한다.")
     void BALL_3() {
         //given
-        PlayerNumbers playerNumbers = new PlayerNumbers(List.of(3, 1, 2));
+        List<Integer> playerNumbers = new PlayerNumbers(List.of(3, 1, 2)).getNumbers();
         //when
-        Map<HintType, Integer> hints = referee.getHint(playerNumbers);
+        Map<HintType, Integer> hints = hintProvider.compareNumbers(answerNumbers, playerNumbers);
         //then
         Assertions.assertThat(hints.get(HintType.BALL)).isEqualTo(3);
     }
@@ -37,9 +42,9 @@ class RefereeTest {
     @DisplayName("플레이어의 숫자가 3,2,1 일 때 2BALL, 1 STRIKE이면 성공한다.")
     void STRIKE_1_AND_BALL_2() {
         //given
-        PlayerNumbers playerNumbers = new PlayerNumbers(List.of(3, 2, 1));
+        List<Integer> playerNumbers = new PlayerNumbers(List.of(3, 2, 1)).getNumbers();
         //when
-        Map<HintType, Integer> hints = referee.getHint(playerNumbers);
+        Map<HintType, Integer> hints = hintProvider.compareNumbers(answerNumbers, playerNumbers);
         //then
         Assertions.assertThat(hints.get(HintType.BALL)).isEqualTo(2);
         Assertions.assertThat(hints.get(HintType.STRIKE)).isEqualTo(1);
@@ -49,9 +54,9 @@ class RefereeTest {
     @DisplayName("플레이어의 숫자가 4,5,6 일 때 1 NOTHING 이면 성공한다.")
     void ONLY_1_NOTHING() {
         //given
-        PlayerNumbers playerNumbers = new PlayerNumbers(List.of(4, 5, 6));
+        List<Integer> playerNumbers = new PlayerNumbers(List.of(4, 5, 6)).getNumbers();
         //when
-        Map<HintType, Integer> hints = referee.getHint(playerNumbers);
+        Map<HintType, Integer> hints = hintProvider.compareNumbers(answerNumbers, playerNumbers);
         //then
         Assertions.assertThat(hints.get(HintType.NOTHING)).isEqualTo(1);
     }
