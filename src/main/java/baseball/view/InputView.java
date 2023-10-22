bccd33daf11cb7074f3f121ba374ffe9fe2f883b
박성춘 <songchon100@gmail.com>
2023-10-22 17:17:28 +0900

    refactor : 상수를 이용하여 출력 메시지 관리

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -5,20 +5,24 @@ import baseball.util.ReStartInputValidator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String INPUT_NUM_MESSAGE = "숫자를 입력해주세요 : ";
+    public static final String RE_START_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
     // 메서드 네임 리팩토링 필요
     public void showGameStartMent() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println(GAME_START_MESSAGE);
     }
 
     public int readNum() throws IllegalArgumentException {
-        System.out.print("숫자를 입력해주세요 : ");
+        System.out.print(INPUT_NUM_MESSAGE);
         String gameInputNum = Console.readLine();
         new GameInputValidator().validate(gameInputNum);
         return Integer.parseInt(gameInputNum);
     }
 
     public int readNumOfReGame() throws IllegalArgumentException {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println(RE_START_MESSAGE);
         String reStartInputNum = Console.readLine();
         new ReStartInputValidator().validate(reStartInputNum);
         return Integer.parseInt(reStartInputNum);
