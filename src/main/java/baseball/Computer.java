c87c489bd371b3f90070116008bf6bad4966665b
yelm <21yrshin@naver.com>
2023-10-22 19:23:37 +0900

    refactor : deleted unnecessary field & methods in Computer class

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -2,30 +2,14 @@ package baseball;
 
 import java.util.List;
 
+// TODO : Computer Service, Controller Implementation
 public class Computer {
-    // Todo : make this weird string as N length List or Array
+    // Randomly Generated 1-9 numbers (N length)
     private final List<Integer> computerList;
 
-    private int strike;
-    private int ball;
-
     public Computer(List<Integer> computerList) {
-
         this.computerList = computerList;
-        this.strike = 0;
-        this.ball = 0;
     }
 
-    public boolean keepGame(){
-        // Todo: N개 숫자를 모두 맞췄는지 확인해 리턴함
-        // TODO : Computer Service, Controller Implementation
-
-//        computeGame();
-
-//        if (strike == N) return true;
-
-        return false;
-    }
-
-
+    // Todo: N개 숫자를 모두 맞췄는지 확인해 리턴함 -> Game Class로 옮길 예정
 }
