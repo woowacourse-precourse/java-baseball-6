ff7d3938f35159a28528276cd90311aff70a0fd6
jum0624 <jum0624@naver.com>
2023-10-23 05:26:28 +0900

    refactor: 숫자 검증 메스드명 수정

--- src/main/java/validation/ValidationNumber.java
+++ src/main/java/validation/ValidationNumber.java
@@ -8,7 +8,7 @@ public class ValidationNumber {
     private final static int MIN_NUMBER = 1;
     private final static int MAX_NUMBER = 9;
     private final static String RANGE_ERROR_MESSAGE = "1-9사이의 값을 입력해주세요.";
-    private final static String INDEX_ERROR_MESSAGE = "세자리 수를 입력해주세요.";
+    private final static String SIZE_ERROR_MESSAGE = "세자리 수를 입력해주세요.";
     private final static String DUPLICATE_ERROR_MESSAGE = "서로 다른 숫자를 입력해주세요.";
     private final static String NUMBER_TYPE_ERROR_MESSAGE = "숫자만 입력해주세요.";
 
@@ -21,9 +21,9 @@ public class ValidationNumber {
         }
     }
 
-    public static void validationIndex(List<Integer> numbers) {
+    public static void validationSize(List<Integer> numbers) {
         if (numbers.size() != 3) {
-            throw new IllegalArgumentException(INDEX_ERROR_MESSAGE);
+            throw new IllegalArgumentException(SIZE_ERROR_MESSAGE);
         }
     }
 
@@ -43,7 +43,7 @@ public class ValidationNumber {
     }
 
     public static void validateNumbers(List<Integer> numbers) {
-        validationIndex(numbers);
+        validationSize(numbers);
         validationRange(numbers);
         validationDuplicateNumber(numbers);
     }
