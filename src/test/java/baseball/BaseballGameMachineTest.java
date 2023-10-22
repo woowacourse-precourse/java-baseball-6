0a5edbf648484ab3e2ca9ac3b422410416e7cab8
watermelon3737 <pshrina1@naver.com>
2023-10-22 16:57:37 +0900

    [test] : 유저 input이 낫싱이거나, 볼이 들어있는경우 게임 성공을 판단하는 테스트 구현

--- src/test/java/baseball/BaseballGameMachineTest.java
+++ src/test/java/baseball/BaseballGameMachineTest.java
@@ -26,14 +26,36 @@ class BaseballGameMachineTest {
         assertThat(inputLine).isEqualTo(input);
     }
 
-    @DisplayName("유저 input이 3스트라이크 일때, 게임이 성공 했는지 팓단하는 테스트")
+    @DisplayName("유저 input이 3스트라이크 일때, 게임이 성공 했는지 판단하는 테스트")
     @Test
-    void isGameSuccess() {
+    void isSuccessThreeStrikeTest() {
         //given
         BaseballGameMachine baseballGameMachine = new BaseballGameMachine();
         //when
         final List<Integer> userInputResult = List.of(0, 3);
         //then
         assertThat(baseballGameMachine.isSuccess(userInputResult)).isEqualTo(true);
+    }
+
+    @DisplayName("유저 input이 낫싱 일때, 게임 성공을 판단하는 테스트")
+    @Test
+    void isSuccessNothingTest() {
+        //given
+        BaseballGameMachine baseballGameMachine = new BaseballGameMachine();
+        //when
+        final List<Integer> userInputResult = null;
+        //then
+        assertThat(baseballGameMachine.isSuccess(userInputResult)).isEqualTo(false);
+    }
+
+    @DisplayName("유저 input이 3 스트라이크 아닐 경우, 게임 성공을 판단한느 테스트")
+    @Test
+    void isSuccessBallStrikeTest() {
+        //given
+        BaseballGameMachine baseballGameMachine = new BaseballGameMachine();
+        //when
+        final List<Integer> userInputResult = List.of(1, 3);
+        //then
+        assertThat(baseballGameMachine.isSuccess(userInputResult)).isEqualTo(false);
     }
 }
