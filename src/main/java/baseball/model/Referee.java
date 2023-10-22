e1850d5ef5478de84a78394e66f3ec1510499d91
leesangjun <hubls@naver.com>
2023-10-22 13:52:32 +0900

    style(Referee): tab을 space로 수정

--- src/main/java/baseball/model/Referee.java
+++ src/main/java/baseball/model/Referee.java
@@ -4,36 +4,36 @@ import java.util.List;
 
 public class Referee {
 
-	public boolean compareComputerPlayer(List computerNumber, List playerNumber) {
-		if (computerNumber.equals(playerNumber)) {
-			return true;
-		}
-		return false;
-	}
+    public boolean compareComputerPlayer(List computerNumber, List playerNumber) {
+        if (computerNumber.equals(playerNumber)) {
+            return true;
+        }
+        return false;
+    }
 
-	public int calculateBall(List<Integer> computerNumber, List<Integer> playerNumber) {
-		int ball = 0;
+    public int calculateBall(List<Integer> computerNumber, List<Integer> playerNumber) {
+        int ball = 0;
 
-		for (int i = 0; i < computerNumber.size(); i++) {
-			for (int j = 0; j < playerNumber.size(); j++) {
-				if (i != j && computerNumber.get(i).equals(playerNumber.get(j))) {
-					ball++;
-				}
-			}
-		}
+        for (int i = 0; i < computerNumber.size(); i++) {
+            for (int j = 0; j < playerNumber.size(); j++) {
+                if (i != j && computerNumber.get(i).equals(playerNumber.get(j))) {
+                    ball++;
+                }
+            }
+        }
 
-		return ball;
-	}
+        return ball;
+    }
 
-	public int calculateStrike(List<Integer> computerNumber, List<Integer> playerNumber) {
-		int strike = 0;
+    public int calculateStrike(List<Integer> computerNumber, List<Integer> playerNumber) {
+        int strike = 0;
 
-		for (int i = 0; i < computerNumber.size(); i++) {
-			if (computerNumber.get(i).equals(playerNumber.get(i))) {
-				strike++;
-			}
-		}
+        for (int i = 0; i < computerNumber.size(); i++) {
+            if (computerNumber.get(i).equals(playerNumber.get(i))) {
+                strike++;
+            }
+        }
 
-		return strike;
-	}
+        return strike;
+    }
 }
