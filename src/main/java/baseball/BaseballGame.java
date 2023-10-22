1d7f30387b782a485d2fe5af6e3e9a61a076504b
jmj005 <jmj00005@naver.com>
2023-10-23 02:21:22 +0900

    refactor: 플레이어 숫자 유효성 로직을 Player 클래스로 이동

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -2,9 +2,6 @@ package baseball;
 
 import static baseball.PlayerView.*;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class BaseballGame {
     private PlayerView playerView = new PlayerView();
     private Computer computer = new Computer();
@@ -28,34 +25,6 @@ public class BaseballGame {
             gameResult = referee.judge(computerNumbers, playerNumbers);
             playerView.output(gameResult.result());
         } while (!gameResult.isWin());
-    }
-
-    public void validateNumbers(String numbers) {
-        if (!isValidNumeric(numbers)) {
-            throw new IllegalArgumentException("잘못된 형식입니다.");
-        }
-        if (!isValidLength(numbers)) {
-            throw new IllegalArgumentException("자릿수가 올바르지 않습니다.");
-        }
-        if (!isDuplicate(numbers)) {
-            throw new IllegalArgumentException("중복된 수가 존재합니다.");
-        }
-    }
-
-    private boolean isValidNumeric(String numbers) {
-        return numbers.matches("^[1-9]+$");
-    }
-
-    private boolean isValidLength(String numbers) {
-        return numbers.length() == 3;
-    }
-
-    private boolean isDuplicate(String numbers) {
-        Set<Character> uniqueDigits = new HashSet<Character>();
-        for (char element : numbers.toCharArray()) {
-            uniqueDigits.add(element);
-        }
-        return uniqueDigits.size() == 3;
     }
 
     private boolean isRetry() {
---
+++ src/main/java/baseball/Player.java
@@ -0,0 +1,41 @@
+package baseball;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Player {
+    private String playerNumbers;
+
+    public Player(String playerNumbers) {
+        validateNumbers(playerNumbers);
+        this.playerNumbers = playerNumbers;
+    }
+
+    private void validateNumbers(String numbers) {
+        if (!isValidNumeric(numbers)) {
+            throw new IllegalArgumentException("잘못된 형식입니다.");
+        }
+        if (!isValidLength(numbers)) {
+            throw new IllegalArgumentException("자릿수가 올바르지 않습니다.");
+        }
+        if (!isDuplicate(numbers)) {
+            throw new IllegalArgumentException("중복된 수가 존재합니다.");
+        }
+    }
+
+    private boolean isValidNumeric(String numbers) {
+        return numbers.matches("^[1-9]+$");
+    }
+
+    private boolean isValidLength(String numbers) {
+        return numbers.length() == 3;
+    }
+
+    private boolean isDuplicate(String numbers) {
+        Set<Character> uniqueDigits = new HashSet<Character>();
+        for (char element : numbers.toCharArray()) {
+            uniqueDigits.add(element);
+        }
+        return uniqueDigits.size() == 3;
+    }
+}
