e61b628519170cc42aff298d2adad10d1748bb08
belljun3395 <195850@jnu.ac.kr>
2023-10-23 03:41:18 +0900

    feat: SoleConsoleGame 구현

---
+++ src/main/java/baseball/domain/game/SoleConsoleGame.java
@@ -0,0 +1,46 @@
+package baseball.domain.game;
+
+import baseball.domain.player.attacker.Attacker;
+import baseball.domain.player.defender.Defender;
+import baseball.domain.player.defender.value.Results;
+import baseball.domain.player.value.Ball;
+import baseball.initializer.GameFactory;
+import baseball.support.generator.BallGenerator;
+import baseball.support.generator.RandomBallsGenerator;
+import baseball.utils.input.Selector;
+import baseball.utils.viewer.Viewer;
+import java.util.List;
+
+public class SoleConsoleGame extends SoleGame {
+
+	private final Defender defender;
+	private final Attacker attacker;
+
+	public SoleConsoleGame(Selector selector, Viewer viewer, Defender defender, Attacker attacker) {
+		super(selector, viewer);
+		this.defender = defender;
+		this.attacker = attacker;
+	}
+
+	public SoleConsoleGame(GameFactory factory, Defender defender, Attacker attacker) {
+		this(factory.getSelector(), factory.getViewer(), defender, attacker);
+	}
+
+	@Override
+	protected void initializeDefender() {
+		Long ballSize = getSelectorSize();
+		defender.initialize(RandomBallsGenerator.generate(ballSize));
+	}
+
+	@Override
+	protected List<Ball> getBalls() {
+		List<Long> numbers = getSelector().selectNumbers();
+		attacker.initialize(BallGenerator.generate(numbers));
+		return attacker.getBalls();
+	}
+
+	@Override
+	protected Results play(List<Ball> balls) {
+		return defender.compareBalls(balls);
+	}
+}
