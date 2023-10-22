6c8e471ea40ecf7962e803de1ee5c25a523758e5
acycoco <pink10257@naver.com>
2023-10-22 18:34:14 +0900

    fix(Computer): 랜덤 넘버를 int[]로 타입 변경, Set으로 중복 검사로 수정

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -2,24 +2,27 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Computer {
 
-    private final List<Integer> numbers = new ArrayList<>();
+    private final int[] secretNumbers = new int[3];
 
     public Computer() {
     }
 
-    public List<Integer> pickNumbers() {
-        while (numbers.size() < 3) {
-            Integer newNumber = Randoms.pickNumberInRange(1, 9);
-            if (!numbers.contains(newNumber)) {
-                numbers.add(newNumber);
-            }
+    public int[] pickNumbers() {
+        Set<Integer> numberSet = new HashSet<>();
+        while (numberSet.size() < 3) {
+            int newNumber = Randoms.pickNumberInRange(1, 9);
+            if (numberSet.contains(newNumber))
+                continue;
+            secretNumbers[numberSet.size()] = newNumber;
+            numberSet.add(newNumber);
         }
-        return numbers;
+        return secretNumbers;
     }
+
 
 }
