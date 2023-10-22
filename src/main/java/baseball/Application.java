f8976acf7fb38df7ac23b574590b19b1777f5b71
D0ri123 <lsh81550@gmail.com>
2023-10-23 01:00:51 +0900

    chore: main 메서드에서 baseball 클래스 분리 - 2차 구현
    1차 구현 코드에서 클래스를 분리했습니다. 협력에 필요한 클래스를 설계하고, 이를 위해 분리함으로써 2차 구현을 시작합니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,29 +7,6 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
-class Baseball {
-
-    // 야구공 타입 - None, 스트라이크, 볼
-    Type type;
-
-    // 야구공 숫자
-    int number;
-
-    // 야구공의 위치
-    int index;
-
-    public Baseball(Type type, int number, int index) {
-        this.type = type;
-        this.number = number;
-        this.index = index;
-    }
-
-    public void changeType(Type type) {
-        this.type = type;
-    }
-}
-
-
 public class Application {
     public static void main(String[] args) {
 
