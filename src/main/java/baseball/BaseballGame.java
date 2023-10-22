78ffd2c2b0cb38fca22d531eca2a6315b816cdf6
minjumost <95904729+minjumost@users.noreply.github.com>
2023-10-22 21:10:42 +0900

    feat: add caculate answer
    - 정답을 계산해서 출력하는 메소드 작성완료
    - 1번테스트 통과 확인

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -8,17 +8,25 @@ import java.util.List;
 public class BaseballGame {
     List<Integer> answer = new ArrayList<>();
     List<Integer> userAnswer = new ArrayList<>();
-
+    Boolean wantToStop = false;
+    Boolean correct = false;
 
     public void start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        initGame();
-        waitInput();
+        while (!wantToStop) {
+            initGame();
+            while (!correct) {
+                waitInput();
+                calculate();
+            }
+        }
+
     }
 
     public void initGame() {
         int randomNumber;
         int length = 0;
+        correct = false;
 
         answer.clear();
         userAnswer.clear();
@@ -46,6 +54,51 @@ public class BaseballGame {
         for (int i = 0; i < userInputArray.length; i++) {
             userInputInteger = Integer.valueOf(userInputArray[i]);
             userAnswer.add(userInputInteger);
+        }
+    }
+
+    public void calculate() {
+        String select;
+        int strike = 0;
+        int ball = 0;
+
+        if (answer.equals(userAnswer)) {
+            correct = true;
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            select = Console.readLine();
+
+            if (select.equals("1")) {
+                wantToStop = false;
+            } else if (select.equals("2")) {
+                System.out.println("게임 종료");
+                wantToStop = true;
+            } else {
+                throw new IllegalArgumentException();
+            }
+        } else {
+            for (int i = 0; i < answer.size(); i++) {
+                int num = userAnswer.get(i);
+                if (answer.contains(num)) {
+                    if (answer.indexOf(num) == i) {
+                        strike++;
+                    } else {
+                        ball++;
+                    }
+                }
+            }
+
+            if (strike == 0 && ball == 0) {
+                System.out.println("낫싱");
+            } else if (strike != 0 && ball == 0) {
+                System.out.println(strike + "스트라이크");
+            } else if (strike == 0 && ball != 0) {
+                System.out.println(ball + "볼");
+            } else if (strike != 0 && ball != 0) {
+                System.out.println(ball + "볼 " + strike + "스트라이크");
+            }
+            userAnswer.clear();
         }
     }
 }
