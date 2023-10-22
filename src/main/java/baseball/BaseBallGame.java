44f5ff729bdea44b166720562abb5c3a35531f19
kang yeon joo <rkdduswndi@naver.com>
2023-10-23 01:34:14 +0900

    feat: 예외 처리 기능 구현

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -13,6 +13,7 @@ public class BaseBallGame {
         while(true){
             System.out.print("숫자를 입력해주세요 : ");
             String input_str = Console.readLine();
+            ValidateNum.validatePlayerInput(input_str);
             List<Integer> user_nums = StringToIntegerList.turnToIntList(input_str);
 
             if(CompareWithAnswer.compareWithAnswer(randomNums,user_nums)){
@@ -24,6 +25,7 @@ public class BaseBallGame {
     public boolean isRestart(){
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String input_num = Console.readLine();
+        ValidateNum.validateRestartInput(input_num);
         if(input_num.equals("1"))
             return true;
         else
