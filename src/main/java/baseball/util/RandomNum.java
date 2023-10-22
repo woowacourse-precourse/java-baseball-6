e9f98b35ad7ebd13a03c7f792df795014bcf9114
daheeParkk <qkrekgml7414@naver.com>
2023-10-22 22:10:09 +0900

    feat : 숫자 랜덤 생성 기능 구현

---
+++ src/main/java/baseball/util/RandomNum.java
@@ -0,0 +1,10 @@
+package baseball.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNum {
+
+    public int create() {
+        return Randoms.pickNumberInRange(1,9);
+    }
+}
