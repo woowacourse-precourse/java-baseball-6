eca202b2d32da884acaaa2a1d9298c70e2abf6e3
shAn-kor <kjn0406@naver.com>
2023-10-22 17:14:18 +0900

    printCelebrate 내부 출력문 한줄로 변경

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -28,7 +28,6 @@ public class OutputView {
     }
 
     public static void printCelebrate() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 }
