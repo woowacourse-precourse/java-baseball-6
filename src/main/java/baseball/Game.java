cc78395d7c5c862946c02c14250e0774a4223b78
minwoo <mark42son@gmail.com>
2023-10-22 20:10:08 +0900

    feat : 스트라이크, 볼 개수 세는 기능 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -7,4 +7,50 @@ import java.util.List;
 
 public class Game {
 
+    private Computer computer = new Computer();
+    private User user = new User();
+
+
+    public void play() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (true) {
+            computer.setRandomNum();
+            int randNum = computer.getRandomNum();
+            int userNum = user.getUserNum();
+
+            List<Integer> randomDigits = getDigits(randNum);
+            List<Integer> userDigits = getDigits(userNum);
+
+            int strike = countStrike(randomDigits, userDigits);
+            int ball = countBall(randomDigits, userDigits);
+
+            printResult(strike, ball);
+
+            if (strike == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                String quitInput = Console.readLine();
+                if (quitInput.equals(2)) {  // java의 equals()에 대해서?
+                    break;
+                }
+            }
+        }
+    }
+
+    private List<Integer> getDigits(int number) {
+        List<Integer> digits = new ArrayList<>();
+        while (number > 0) {
+            digits.add(number % 10);
+            number /= 10;
+        }
+        return Arrays.asList(new Integer[3 - digits.size()]);  // Arrays의 asList()는 무엇인가?
+    }
+
+    private int ca
+
+    private void initRandomNum() {
+        user.setUserNum();
+
+    }
 }
+
