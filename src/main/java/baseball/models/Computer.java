4448cac61493292d08c835039d87e1e67b5c473b
jy016011 <39334789+jy016011@users.noreply.github.com>
2023-10-22 17:24:14 +0900

    refactor: Computer 클래스 캡슐화
    외부에서 멤버변수 접근 못하도록 변경
    멤버변수 변경을 위해서는 함수들을 거치도록 변경하였음.
    또한, 멤버변수의 내용을 넘겨줄때는 복사본을 넘겨 예상치 못한 내용 수정이 일어나지 않도록 방지함.
    멤버변수에 직접 난수를 저장하면서, 게임 재시작시마다 멤버변수를 비워줘야하므로 clearAnswerList도 새로 만들어줬다.

--- src/main/java/baseball/models/Computer.java
+++ src/main/java/baseball/models/Computer.java
@@ -1,11 +1,31 @@
 package baseball.models;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static baseball.utils.InputValidator.*;
 /*
  Computer의 변수들을 기억하는 클래스
  */
 
 public class Computer {
-    public List<Integer> numberList;
+    private final List<Integer> numberList = new ArrayList<>();
+
+    public void saveNumber(int randomNumber){
+        validateDigitRange(randomNumber);
+        numberList.add(randomNumber);
+    }
+
+    public int getListSize(){
+        return numberList.size();
+    }
+
+    public List<Integer> showNumberList() {
+        return new ArrayList<>(numberList);
+    }
+
+    public void clearAnswerList(){
+        numberList.clear();
+    }
+
 }
