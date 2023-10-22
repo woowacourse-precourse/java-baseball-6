911e4320095b93d1efc4bf8923f689505bc5d942
tam31 <wxogud3377@naver.com>
2023-10-22 15:44:56 +0900

    Feat(Player): 재시작 혹은 종료 번호를 위한 뼈대구현

--- src/main/java/baseball/model/Player.java
+++ src/main/java/baseball/model/Player.java
@@ -7,12 +7,21 @@ import java.util.List;
 public class Player {
 
     private List<Integer> playerNumber;
+    private Integer answerNumber;
 
     public List<Integer> getPlayerNumber() {
         return playerNumber;
     }
 
+    public Integer getAnswerNumber() {
+        return answerNumber;
+    }
+
     public void inputPlayerNumber(String numbers){
         playerNumber = PlayerValidation.checkNumber(numbers);
+    }
+
+    public void answerRestartOrEnd(String number){
+        answerNumber = PlayerValidation.checkAnswerNumber(number);
     }
 }
