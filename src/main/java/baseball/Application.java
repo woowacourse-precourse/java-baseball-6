0d745da11a716f72a8a00abb74c5214bf10c0ce0
LooanCheong <jjyejun@naver.com>
2023-10-22 14:56:08 +0900

    최종 결과 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -51,12 +51,24 @@ public class Application {
                 strike += 1;
             }
         }
-
-        System.out.println(cnt);
-        System.out.println(strike);
-
-
+        
+        //볼 개수 확인
         int ball = cnt - strike;
+        
+        //최종 출력
+        
+        if(cnt == 0){
+            System.out.println("낫싱");
+        }else if(strike == 0){
+            System.out.println(ball + "볼");
+        }else if(strike == 3){
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }else if(ball == 0){
+            System.out.println(strike + "스트라이크");
+        }else {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
 
     }
 }
