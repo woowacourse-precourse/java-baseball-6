deeef1a010d09955495e7be2bd2340ea20e1ca54
minwoo <mark42son@gmail.com>
2023-10-22 21:40:47 +0900

    fix : 정규식 처리 대신 조건문으로 변경

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,13 +1,11 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.regex.Pattern;
 
 public class User {
     private static final int NUMBER_LENGTH = 3;
     private static final String OUTPUT_ENTER_NUMBER = "숫자를 입력해주세요 : ";
-    private static final Pattern INPUT_REGEX = Pattern.compile("[1-9]{3,3}");
-    private static final String EXCEPTION_MESSAGE = "Please enter a three-digit natural number consisting of 1 to 9.";
+    private static final String EXCEPTION_MESSAGE = "1부터 9까지 서로 다른 숫자를 입력해주세요.";
     int userNumber;
 
     public User() {}
@@ -32,16 +30,21 @@ public class User {
         return Console.readLine();
     }
 
+
     private void validateInput(String input) {
         validateInputLength(input);
-        validateInputRegex(input);
+        validateInputDigits(input);
     }
 
-    private void validateInputRegex(String input) {
-        if(!INPUT_REGEX.matcher(input).matches()) {
-            throw new IllegalArgumentException(EXCEPTION_MESSAGE);
+    private void validateInputDigits(String input) {
+        for (char c : input.toCharArray()) {
+            if (c < '1' || c > '9') {
+                throw new IllegalArgumentException(EXCEPTION_MESSAGE);
+            }
         }
     }
+
+
 
     private static void validateInputLength(String input) {
         if(input.length() != NUMBER_LENGTH) {
