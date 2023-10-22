ee4730ac1b3842e994058463d3e1b0e45e34783a
devhoya97 <diajbh123@gmail.com>
2023-10-22 19:06:22 +0900

    refactor : InputView의 사용하지 않는 메서드 삭제 GameRestartOrNot 객체를 활용한 메서드를 추가하게 되면서, 기존의 필요 없어진 메서드들을 제거했습니다

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -5,10 +5,6 @@ import baseball.model.StringNumbers;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static String getConsoleInput() {
-        return Console.readLine();
-    }
-
     public static StringNumbers getUserBaseballNumbersString() {
         OutputView.printEnterNumber();
         return new StringNumbers(Console.readLine());
@@ -17,10 +13,5 @@ public class InputView {
     public static GameRestartOrNot getGameRestartOrNot() {
         OutputView.printAnswer();
         return new GameRestartOrNot(Console.readLine());
-    }
-
-    public static String getRestartOrNot() {
-        OutputView.printAnswer();
-        return Console.readLine();
     }
 }
