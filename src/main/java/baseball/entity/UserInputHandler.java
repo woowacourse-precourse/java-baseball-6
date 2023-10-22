ea6125bef6219af22fe4150ec05d278b8df946c8
taehoon <xogns1514@gmail.com>
2023-10-22 15:33:03 +0900

    refactor: 게임중, 사용자 입력 예외 처리 로직 분리.
    1~9 까지 숫자 검증 로직과 서로 다른 숫자 로직을 분리했다.

--- src/main/java/baseball/entity/UserInputHandler.java
+++ src/main/java/baseball/entity/UserInputHandler.java
@@ -6,18 +6,19 @@ public class UserInputHandler {
         // 사용자 입력의 길이가 3이 아니면 예외 처리
         if(inputNumber.length() != 3) throw new IllegalArgumentException();
 
-        // 사용자의 입력이 1~9까지 다른 숫자가 아니라면 예외 처리
+        // 사용자의 입력이 1~9까지 숫자가 아니라면 예외 처리
         for(int i = 0; i < 3; i++) {
             char currentNumber = inputNumber.charAt(i);
             if (currentNumber < '1' || currentNumber > '9') {
                 throw new IllegalArgumentException();
             }
-            for(int j = i + 1; j < 3; j++) {
-                if (currentNumber == inputNumber.charAt(j)) {
-                    throw new IllegalArgumentException();
-                }
-            }
         }
+
+        // 사용자의 입력이 서로 다른 숫자가 아니라면 예외 처리
+        if(inputNumber.charAt(0) == inputNumber.charAt(1)) throw new IllegalArgumentException();
+        if(inputNumber.charAt(0) == inputNumber.charAt(2)) throw new IllegalArgumentException();
+        if(inputNumber.charAt(1) == inputNumber.charAt(2)) throw new IllegalArgumentException();
+
     }
 
     // 게임종료 후, 사용자 입력 예외 처리를 위한 함수
