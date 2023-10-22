a15daf4c1db1aa78dcc974136f697b6cff7a07df
sujnkim <blackout3210@naver.com>
2023-10-22 22:31:53 +0900

    refactor: 낫싱, 볼, 스트라이크 문자열 하드코딩 상수로 변경

--- src/main/java/baseball/CountResult.java
+++ src/main/java/baseball/CountResult.java
@@ -3,6 +3,9 @@ package baseball;
 import java.util.List;
 
 public class CountResult {
+    private final static String NOTHING_SENTENCE = "낫싱";
+    private final static String BALL_SENTENCE = "볼";
+    private final static String STRIKE_SENTENCE = "스트라이크";
 
     private final int ballCount;
     private final int strikeCount;
@@ -37,15 +40,15 @@ public class CountResult {
     public String makeHintString(){
 
         if (ballCount==0 && strikeCount==0){
-            return "낫싱";
+            return NOTHING_SENTENCE;
         }
 
         StringBuilder stringBuilder = new StringBuilder();
         if(ballCount!=0){
-            stringBuilder.append(ballCount).append("볼 ");
+            stringBuilder.append(ballCount).append(BALL_SENTENCE).append(" ");
         }
         if(strikeCount!=0){
-            stringBuilder.append(strikeCount).append("스트라이크");
+            stringBuilder.append(strikeCount).append(STRIKE_SENTENCE);
         }
         return stringBuilder.toString();
     }
