9de0ef83c951a046cd4ca89ebdff023a2639fba5
NamJihyun99 <namjihyun9936@gmail.com>
2023-10-22 21:21:42 +0900

    fix:> 게임 시작 메시지 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -88,6 +88,7 @@ public class Application {
 
     public static void main(String[] args) {
         int choice;
+        System.out.println("숫자 야구 게임을 시작합니다.");
         do {
             playBaseball();
             choice = askToContinue();
