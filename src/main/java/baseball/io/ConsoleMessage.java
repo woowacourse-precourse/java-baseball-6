de5ccc161368cc9d8994e66bfafedd5bd0677083
beomsic <qjatjr29@khu.ac.kr>
2023-10-23 00:59:31 +0900

    :sparkles: feat: 출력값 형식 요구사항에 맞는 출력값 enum 추가

---
+++ src/main/java/baseball/io/ConsoleMessage.java
@@ -0,0 +1,22 @@
+package baseball.io;
+
+public enum ConsoleMessage {
+
+    GAME_START("숫자 야구 게임을 시작합니다. \n"),
+    ENTER_NUMBER("숫자를 입력해주세요 : "),
+    STRIKE("스트라이크 "),
+    BALL("볼 "),
+    NOTHING("낫싱"),
+    CORRECT_ANSWER("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"),
+    RESTART("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+
+    private String message;
+
+    ConsoleMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
