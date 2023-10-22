f5ecd4c22ec3781b26ac1db9c6ca4e91f2e89af7
hoyeon <highoption@naver.com>
2023-10-22 14:46:24 +0900

    refactor : BaseballGame 클래스의 generateUserNumbers 메서드 리펙토링

--- src/main/java/baseball/controller/BaseballGame.java
+++ src/main/java/baseball/controller/BaseballGame.java
@@ -1,8 +1,8 @@
 package baseball.controller;
 
 import baseball.model.RandomNumberGenerator;
+import baseball.model.UserNumberGenerator;
 import baseball.view.InputView;
-import java.util.ArrayList;
 import java.util.List;
 
 public class BaseballGame {
@@ -17,16 +17,7 @@ public class BaseballGame {
 
     private static List<Integer> generateUserNumbers() {
         String userNumbersToString = InputView.startInput();
-        return convertStringtoList(userNumbersToString);
+        return UserNumberGenerator.makeUserNumberToList(userNumbersToString);
     }
 
-    private static List<Integer> convertStringtoList(String str) {
-        List<Integer> numbers = new ArrayList<>();
-        String[] splitStr = str.split("");
-        for (String splitUserNumber : splitStr) {
-            int parseInt = Integer.parseInt(splitUserNumber);
-            numbers.add(parseInt);
-        }
-        return numbers;
-    }
 }
