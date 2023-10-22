2fa17720ac43974574884c376b16c58d4d2b2435
Flash-KIM142 <topqr123q@gmail.com>
2023-10-22 20:44:45 +0900

    refactor: 게임 관련 상수 모음 클래스명 변경 및 속성 추가

--- src/main/java/baseball/constant/GameConstants.java
+++
@@ -1,9 +0,0 @@
-package baseball.constant;
-
-public class GameConstants {
-
-    public static final int MAX_LENGTH = 3;
-    public static final int MAX_VALUE = 9;
-    public static final int MIN_VALUE = 1;
-    public static final String ONLY_NUMBER_REGEX = "[1-9]+";
-}
