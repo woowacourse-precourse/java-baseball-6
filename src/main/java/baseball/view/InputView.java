4a55a939fc15bce193ec5066be3371b2b966f0df
devhoya97 <diajbh123@gmail.com>
2023-10-22 18:57:37 +0900

    refactor : InputView에 getGameRestartOrNot 메서드 추가 입력받은 String을 생성자 매개변수로 사용해서 새롭게 정의한 GameRestartOrNot 객체를 만들어 반환하도록 메서드를 추가했습니다.

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,5 +1,6 @@
 package baseball.view;
 
+import baseball.model.GameRestartOrNot;
 import baseball.model.StringNumbers;
 import camp.nextstep.edu.missionutils.Console;
 
@@ -11,6 +12,11 @@ public class InputView {
     public static StringNumbers getUserBaseballNumbersString() {
         OutputView.printEnterNumber();
         return new StringNumbers(Console.readLine());
+    }
+
+    public static GameRestartOrNot getGameRestartOrNot() {
+        OutputView.printAnswer();
+        return new GameRestartOrNot(Console.readLine());
     }
 
     public static String getRestartOrNot() {
