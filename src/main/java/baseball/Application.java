faf8a9a38274f843885565d6f129521dbf62980b
GyyymShark <sangwoo0795@naver.com>
2023-10-22 22:49:02 +0900

    feat: 비교에 따른 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -45,6 +45,27 @@ public class Application {
                 }
             }
 
+            //출력
+            if(strike==0 && ball==0){
+                System.out.println("낫싱");
+            }
+            else{
+                if(strike==3){
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                    int exit = sc.nextInt();
+                    if(exit==2) break;
+                }
+                else{
+                    if(ball>0 && strike>0) System.out.println(ball+"볼 "+strike+"스트라이크");
+                    else if(ball>0) System.out.println(ball+"볼");
+                    else if(strike>0) System.out.println(strike+"스트라이크");
+                }
+            }
+
+            user.clear();
+
+
 
         }
 
