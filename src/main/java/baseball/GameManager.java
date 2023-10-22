616ac6c3672c0efdd26a6be38d2413f259c75445
wlgh1553 <112055561+wlgh1553@users.noreply.github.com>
2023-10-23 00:46:43 +0900

    [FEAT] 게임 흐름 관리용 게임 매니저 기능

--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -1,7 +1,50 @@
 package baseball;
 
+import baseball.domain.Computer;
+import baseball.domain.Player;
+import baseball.service.JudgeResult;
+import baseball.view.OutputMessage;
+import baseball.view.PlayerInput;
+
 public class GameManager {
-    void gameStart() {
-        
+    private Computer computer;
+    private Player player;
+    private boolean isGameClear;
+
+    public GameManager() {
+        isGameClear = false;
+        OutputMessage.printGameStartMsg();
+    }
+
+    void playGame() {
+        boolean goGame = true;
+        while (goGame) {
+            oneRoundOfGame();
+            OutputMessage.printGameExitMsg();
+            goGame = PlayerInput.isGameRestartInput();
+        }
+    }
+
+    private void settingGame() {
+
+        isGameClear = false;
+        computer = new Computer();
+    }
+
+    private void oneInputCycleOfGame() {
+        OutputMessage.printInputNumMsg();
+        player = new Player(PlayerInput.getInput());
+
+        JudgeResult judge = new JudgeResult(computer, player);
+        OutputMessage.printGameResult(judge.getResult());
+        isGameClear = judge.isGameClear();
+    }
+
+    private void oneRoundOfGame() {
+        settingGame();
+        while (!isGameClear) {
+            oneInputCycleOfGame();
+        }
+        OutputMessage.printGameClearMsg();
     }
 }
