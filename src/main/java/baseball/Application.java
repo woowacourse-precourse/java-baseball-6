02c286fd5b5fd58a49c6eb3a5689bb318d564a1d
ssuzyn <asj7090@gmail.com>
2023-10-22 16:35:43 +0900

    feat: 비교 결과 출력 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -95,6 +95,24 @@ class Game {
         while(true){
             User user = new User();
             int[] result = computer.compareNum(user.getUserNum());
+
+            if (result[0] == 3) {
+                System.out.println("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                return true;
+            }
+            printGameResult(result);
+        }
+    }
+
+    private void printGameResult(int[] result) {
+        if (result[0] > 0 && result[1] > 0) {
+            System.out.println(result[1] + "볼 " + result[0] + "스트라이크");
+        } else if (result[0] > 0 && result[1] == 0) {
+            System.out.println(result[0] + "스트라이크");
+        } else if (result[0] == 0 && result[1] > 0) {
+            System.out.println(result[1] + "볼");
+        } else if (result[0] == 0 && result[1] == 0) {
+            System.out.println("낫싱");
         }
     }
 
