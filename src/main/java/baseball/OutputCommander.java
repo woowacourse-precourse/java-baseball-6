305cb8b7e433bde3979f1a84370e665f11be224d
yubin <dbqls2821@gmail.com>
2023-10-22 16:22:31 +0900

    feat: 출력이라는 책임을 가진 클래스 추가

---
+++ src/main/java/baseball/OutputCommander.java
@@ -0,0 +1,22 @@
+package baseball;
+
+class OutputCommander {
+
+    void printGameStart() {
+        System.out.println(OutputMessage.START.message);
+    }
+
+    void printReceivingInput() {
+        System.out.print(OutputMessage.INPUT_NUMBER.message);
+    }
+
+    void printGameScore(String score) {
+        System.out.println(score);
+    }
+
+    void printGameClearAndAskRetry() {
+        System.out.println(OutputMessage.GAME_CLEAR.message);
+        System.out.println(OutputMessage.RETRY_OR_END.message);
+    }
+
+}
