eced6f547bd85e0d036d5fde1e2f436e7076b8ab
Woongbae Yun <devbelly@naver.com>
2023-10-22 15:13:10 +0900

    feat : 게임 재시작 혹은 종료 안내 문구를 출력하는 기능 추가

--- src/main/java/baseball/Information.java
+++ src/main/java/baseball/Information.java
@@ -2,8 +2,11 @@ package baseball;
 
 public class Information {
 
+    public static final int MAX_DIGIT = 3;
     private static final String START_GAME = "숫자 야구 게임을 시작합니다.";
     private static final String INPUT_NUMBER = "숫자를 입력해주세요 : ";
+    private static final String GUESS_ALL = "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String END_GAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public static void printStartGame() {
         System.out.println(START_GAME);
@@ -23,5 +26,13 @@ public class Information {
         if (strike != 0) {
             System.out.println(strike + "스트라이크");
         }
+    }
+
+    public static void printGuessAll() {
+        System.out.println(MAX_DIGIT + GUESS_ALL);
+    }
+
+    public static void printEndGame() {
+        System.out.println(END_GAME);
     }
 }
