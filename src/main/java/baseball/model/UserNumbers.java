5c540ff29c509c24d27efc14ed34529662e553bc
kimtaesoo <kimtaesoo7@naver.com>
2023-10-22 13:28:19 +0900

    refactor: 검증에 대한 책임을 분리 시킴

--- src/main/java/baseball/model/UserNumbers.java
+++ src/main/java/baseball/model/UserNumbers.java
@@ -9,8 +9,6 @@ public class UserNumbers {
     private static final int REQUIRED_INPUT_LENGTH = 3;
     private static final String ZERO = "0";
     private static final String INVALID_LENGTH_MESSAGE = "숫자는 세 가지를 고를 수 있습니다.";
-    private static final String NON_NUMERIC_MESSAGE = "숫자를 입력해주세요.";
-    private static final String NUMERIC_PATTERN = "\\d+";
     private static final String ZERO_ERROR_MESSAGE = "0이 포함되어서는 안됩니다.";
     private static final String DUPLICATE_ERROR_MESSAGE = "중복된 숫자는 입력할 수 없습니다.";
 
@@ -28,7 +26,6 @@ public class UserNumbers {
 
     private static void validateInput(final String input) {
         validateInputLength(input);
-        validateInputIsNumeric(input);
         validateNoZeros(input);
         validateNoDuplicates(input);
     }
@@ -37,16 +34,6 @@ public class UserNumbers {
         if (input.length() != REQUIRED_INPUT_LENGTH) {
             throw new IllegalArgumentException(INVALID_LENGTH_MESSAGE);
         }
-    }
-
-    private static void validateInputIsNumeric(final String input) {
-        if (!isNumeric(input)) {
-            throw new IllegalArgumentException(NON_NUMERIC_MESSAGE);
-        }
-    }
-
-    private static boolean isNumeric(final String input) {
-        return input.matches(NUMERIC_PATTERN);
     }
 
     private static void validateNoZeros(final String input) {
--- src/main/java/baseball/util/Converter.java
+++ src/main/java/baseball/util/Converter.java
@@ -6,13 +6,27 @@ import java.util.List;
 public class Converter {
 
     private static final String SEPARATOR = "";
+    private static final String NON_NUMERIC_MESSAGE = "숫자를 입력해주세요.";
+    private static final String NUMERIC_PATTERN = "\\d+";
 
     private Converter() {
     }
 
     public static List<Integer> convertStringToIntegerList(final String string) {
+        validateInputIsNumeric(string);
+
         return Arrays.stream(string.split(SEPARATOR))
                 .map(Integer::parseInt)
                 .toList();
+    }
+
+    private static void validateInputIsNumeric(final String input) {
+        if (!isNumeric(input)) {
+            throw new IllegalArgumentException(NON_NUMERIC_MESSAGE);
+        }
+    }
+
+    private static boolean isNumeric(final String input) {
+        return input.matches(NUMERIC_PATTERN);
     }
 }
