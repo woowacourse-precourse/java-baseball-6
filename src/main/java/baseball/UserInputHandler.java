dcc9acce69bfc7f23b74a2299fe83d715a11bcfd
hyunseok <hyun104467@naver.com>
2023-10-22 23:46:26 +0900

    [Feat] UserInputHandler/유저 입력 메소드

--- src/main/java/baseball/UserInputHandler.java
+++ src/main/java/baseball/UserInputHandler.java
@@ -1,11 +1,20 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.List;
 
 public class UserInputHandler {
     private boolean checkDigitFromInput() {}
     private boolean checkLengthFromInput() {}
-    private boolean checkInput() {}
+    private boolean isCorrectInput() {}
     public List<Integer> makeUserCount() {}
-    public List<Integer> takeInputCount() {}
+    public List<Integer> takeInput() {
+        System.out.println("숫자를 입력해주세요 : ");
+        String userInput = Console.readLine();
+        if (!isCorrectInput(userInput)) {
+            throw new IllegalArgumentException("입력 오류");
+        }
+        return makeUserCount();
+    }
 }
