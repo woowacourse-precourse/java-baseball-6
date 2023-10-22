73e7a34ec383e0b97ec22a2747e68aebb6178571
1015s <1015_s@naver.com>
2023-10-22 01:32:56 -0700

    feat(InputNumberController) : 사용자의 입력 유효성 검사하는 기능 구현

---
+++ src/main/java/baseball/controller/InputNumberController.java
@@ -0,0 +1,42 @@
+package baseball.controller;
+
+import baseball.domain.GameNumber;
+import baseball.view.InputView;
+
+public class InputNumberController {
+    private static final String GAME_RETRY_ANSWER = "1";
+    private static final String GAME_END_ANSWER = "2";
+    InputView inputView = new InputView();
+
+    public GameNumber guessNumber(){
+        String inputNumber = inputView.inputNumber();
+
+        if(!isNumericString(inputNumber)){
+            throw new IllegalArgumentException();
+        }
+        return convertStringToGameNumber(inputNumber);
+    }
+    
+    private boolean isNumericString(String inputNumber){
+        return inputNumber.matches("[1-9]+");
+    }
+
+    private GameNumber convertStringToGameNumber(String inputNumber){
+        GameNumber gameNumber = new GameNumber();
+
+
+    }
+
+
+    public boolean retryNumber(){
+        String retryNumber = inputView.retryNumber();
+
+        if(retryNumber.equals(GAME_RETRY_ANSWER)){
+            return true;
+        }else if(retryNumber.equals(GAME_END_ANSWER)){
+            return false;
+        }
+        throw new IllegalArgumentException();
+    }
+
+}
