43ec4fae0afd6471383fc1eacf82cd9178005829
LooanCheong <jjyejun@naver.com>
2023-10-22 15:11:43 +0900

    게임 재시작 여부 확인, 재시작 while문 설정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,66 +12,77 @@ public class Application {
         //게임 시작
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-        //랜덤 숫자 생성
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
-        }
-        System.out.println(computer);
+        int playing = 1;
 
-        //사용자 숫자 입력(맞을 때까지 반복)
-        int ans = 0;
-        
-        while(ans == 0) {
-            //잘못 입력 시 (IllegalArgumentException 발생)
+        while (playing == 1) {
+            //랜덤 숫자 생성
+            List<Integer> computer = new ArrayList<>();
+            while (computer.size() < 3) {
+                int randomNumber = Randoms.pickNumberInRange(1, 9);
+                if (!computer.contains(randomNumber)) {
+                    computer.add(randomNumber);
+                }
+            }
+            System.out.println(computer);
+
+            //사용자 숫자 입력(맞을 때까지 반복)
+            int ans = 0;
             Scanner sc = new Scanner(System.in);
-            System.out.print("숫자를 입력해주세요 : ");
-            List<Integer> user = new ArrayList<>();
-            String input = sc.next();
 
-            for (String num : input.split("")) {
-                user.add(Integer.parseInt(num));
-            }
+            while (ans == 0) {
+                //잘못 입력 시 (IllegalArgumentException 발생)
 
-            //숫자 비교
-            //총 개수 확인
-            int cnt = 0;
+                System.out.print("숫자를 입력해주세요 : ");
+                List<Integer> user = new ArrayList<>();
+                String input = sc.next();
 
-            for (int i = 0; i < user.size(); i++) {
-                if (computer.contains(user.get(i))) {
-                    cnt += 1;
+                for (String num : input.split("")) {
+                    user.add(Integer.parseInt(num));
+                }
+
+                //숫자 비교
+                //총 개수 확인
+                int cnt = 0;
+
+                for (int i = 0; i < user.size(); i++) {
+                    if (computer.contains(user.get(i))) {
+                        cnt += 1;
+                    }
+                }
+
+                //스트라이크 개수 확인
+                int strike = 0;
+
+                for (int i = 0; i < user.size(); i++) {
+                    if (computer.get(i) == user.get(i)) {
+                        strike += 1;
+                    }
+                }
+
+                //볼 개수 확인
+                int ball = cnt - strike;
+
+                //최종 출력
+
+                if (cnt == 0) {
+                    System.out.println("낫싱");
+                } else if (strike == 0) {
+                    System.out.println(ball + "볼");
+                } else if (strike == 3) {
+                    System.out.println("3스트라이크");
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    ans = 1;
+                } else if (ball == 0) {
+                    System.out.println(strike + "스트라이크");
+                } else {
+                    System.out.println(ball + "볼 " + strike + "스트라이크");
                 }
             }
-
-            //스트라이크 개수 확인
-            int strike = 0;
-
-            for (int i = 0; i < user.size(); i++) {
-                if (computer.get(i) == user.get(i)) {
-                    strike += 1;
-                }
-            }
-
-            //볼 개수 확인
-            int ball = cnt - strike;
-
-            //최종 출력
-
-            if (cnt == 0) {
-                System.out.println("낫싱");
-            } else if (strike == 0) {
-                System.out.println(ball + "볼");
-            } else if (strike == 3) {
-                System.out.println("3스트라이크");
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                ans = 1;
-            } else if (ball == 0) {
-                System.out.println(strike + "스트라이크");
-            } else {
-                System.out.println(ball + "볼 " + strike + "스트라이크");
+            //게임 재시작 여부 확인
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            Integer playAgain = sc.nextInt();
+            if (playAgain == 2){
+                playing = 0;
             }
         }
     }
