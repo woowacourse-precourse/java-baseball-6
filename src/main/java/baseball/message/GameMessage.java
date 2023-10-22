12df03935153a695f65976f5733992aba1188a64
Jihyun3478 <leejihyun3478@gmail.com>
2023-10-23 03:33:23 +0900

    Feat(GameMessage): toString 메소드 추가

--- src/main/java/baseball/message/GameMessage.java
+++ src/main/java/baseball/message/GameMessage.java
@@ -6,6 +6,14 @@ public enum GameMessage {
     ClEAR_MESSAGE("3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
     GAME_STATUS_MESSAGE("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
+    private final String message;
+
     GameMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
     }
 }
