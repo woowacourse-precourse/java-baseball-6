c3d16c7b2d45e9882c970f31af4fba3c42cb5402
chs98412 <chs98412@gmail.com>
2023-10-22 17:15:57 +0900

    feat: 개행문자 없는 출력 함수 작성

--- src/main/java/baseball/controller/OutputController.java
+++ src/main/java/baseball/controller/OutputController.java
@@ -3,8 +3,13 @@ package baseball.controller;
 public class OutputController {
 
     public final static String START = "숫자 야구 게임을 시작합니다.";
+    public final static String INPUT = "숫자를 입력해주세요 : ";
+
+    public void cliOutputln(String ment) {
+        System.out.println(ment);
+    }
 
     public void cliOutput(String ment) {
-        System.out.println(ment);
+        System.out.print(ment);
     }
 }
