b6cca0a0f6b1079d7bbfad1b0671334925892fd2
GS_song <20003204@sju.ac.kr>
2023-10-22 15:06:49 +0900

    feat: InputValidation의 모든 메서드를 static으로 설정해 어디서든 접근가능하게 설정

--- src/main/java/baseball/validation/InputValidation.java
+++ src/main/java/baseball/validation/InputValidation.java
@@ -1,31 +1,30 @@
 package baseball.validation;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import static baseball.constant.Constants.*;
 
-public class InputValidation {
+public final class InputValidation {
 
-    public Boolean checkSize (List<Integer> target) {
+    public static Boolean isSizeThree(List<Integer> target) {
         return target.size() == INPUT_MAX_SIZE;
     }
 
-    public Boolean checkRange (List<Integer> target) {
+    public static Boolean isInRange(List<Integer> target) {
         for (int t : target) {
             if (t < INPUT_NUM_MIN || t > INPUT_NUM_MAX) return false;
         }
         return true;
     }
 
-    public Boolean checkDuplicate(List<Integer> target) {
+    public static Boolean isDuplicate(List<Integer> target) {
         Set<Integer> targetSet = target.stream().collect(Collectors.toSet());
-        return targetSet.size() == INPUT_MAX_SIZE;
+        return targetSet.size() != INPUT_MAX_SIZE;
     }
 
-    public Boolean checkRestart (int input) {
-        return input == INPUT_RESTART || input == INPUT_STOP;
+    public static Boolean isRestartInput (int input) {
+        return input != INPUT_RESTART && input != INPUT_STOP;
     }
 }
