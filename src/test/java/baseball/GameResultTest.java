f31b902b19e770d888bd4f0f823a20194569b56c
kg9585 <kg9585@naver.com>
2023-10-22 15:36:20 +0900

    feat: 게임 재시작과 종료 입력값 테스트 코드를 작성한다

--- src/test/java/baseball/GameResultTest.java
+++ src/test/java/baseball/GameResultTest.java
@@ -1,8 +1,8 @@
 package baseball;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -17,17 +17,14 @@ public class GameResultTest {
 
   @Test
   void isInputNumber1Or2() {
-    game.setRestartOrQuitInput("1");
-    assertTrue(game.inputArrList.contains(game.restartOrQuitInput));
+    assertDoesNotThrow(() -> game.setRestartOrQuitInput("1"));
+    ;
 
   }
 
   @Test
   void isInputNumberNot1Nor2() {
-    Assertions.assertThrows(IllegalArgumentException.class, () -> {
-      game.setRestartOrQuitInput("3");
-    });
+    assertThrows(IllegalArgumentException.class, () -> game.setRestartOrQuitInput("3"));
   }
-
 
 }
