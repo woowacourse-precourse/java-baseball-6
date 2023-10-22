c875299ac4cc39ffa0acfd45b1c7c82d33d03814
LooanCheong <jjyejun@naver.com>
2023-10-22 15:06:00 +0900

    정답 체크 구간 반복 설정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -23,52 +23,56 @@ public class Application {
         System.out.println(computer);
 
         //사용자 숫자 입력(맞을 때까지 반복)
-        //잘못 입력 시 (IllegalArgumentException 발생)
+        int ans = 0;
+        
+        while(ans == 0) {
+            //잘못 입력 시 (IllegalArgumentException 발생)
             Scanner sc = new Scanner(System.in);
             System.out.print("숫자를 입력해주세요 : ");
             List<Integer> user = new ArrayList<>();
             String input = sc.next();
 
-            for(String num: input.split("")){
+            for (String num : input.split("")) {
                 user.add(Integer.parseInt(num));
             }
 
-        //숫자 비교
-        //총 개수 확인
-        int cnt = 0;
+            //숫자 비교
+            //총 개수 확인
+            int cnt = 0;
 
-        for(int i = 0; i < user.size(); i++){
-            if(computer.contains(user.get(i))){
-                cnt += 1;
+            for (int i = 0; i < user.size(); i++) {
+                if (computer.contains(user.get(i))) {
+                    cnt += 1;
+                }
+            }
+
+            //스트라이크 개수 확인
+            int strike = 0;
+
+            for (int i = 0; i < user.size(); i++) {
+                if (computer.get(i) == user.get(i)) {
+                    strike += 1;
+                }
+            }
+
+            //볼 개수 확인
+            int ball = cnt - strike;
+
+            //최종 출력
+
+            if (cnt == 0) {
+                System.out.println("낫싱");
+            } else if (strike == 0) {
+                System.out.println(ball + "볼");
+            } else if (strike == 3) {
+                System.out.println("3스트라이크");
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                ans = 1;
+            } else if (ball == 0) {
+                System.out.println(strike + "스트라이크");
+            } else {
+                System.out.println(ball + "볼 " + strike + "스트라이크");
             }
         }
-
-        //스트라이크 개수 확인
-        int strike = 0;
-
-        for(int i = 0; i < user.size(); i++){
-            if(computer.get(i) == user.get(i)){
-                strike += 1;
-            }
-        }
-        
-        //볼 개수 확인
-        int ball = cnt - strike;
-        
-        //최종 출력
-        
-        if(cnt == 0){
-            System.out.println("낫싱");
-        }else if(strike == 0){
-            System.out.println(ball + "볼");
-        }else if(strike == 3){
-            System.out.println("3스트라이크");
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        }else if(ball == 0){
-            System.out.println(strike + "스트라이크");
-        }else {
-            System.out.println(ball + "볼 " + strike + "스트라이크");
-        }
-
     }
 }
