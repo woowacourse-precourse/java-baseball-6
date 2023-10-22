fb4248a5d4f427c830b35363deaf90af33a7be1e
eom-tae-in <eti0728@daum.net>
2023-10-22 14:27:44 +0900

    refactor: Computer 객체에 부여된 책임을 줄이기 위한 코드 수정

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,99 +1,30 @@
 package baseball.domain;
 
-import static baseball.constant.Constant.BASEBALL_GAME_NUMBER_DIGIT;
-
-import baseball.domain.dto.GuessNumberResultResponse;
-import java.util.ArrayList;
+import baseball.util.BaseBallGameNumberGenerator;
 import java.util.List;
 
 public class Computer {
 
-    private static final int THREE_STRIKE = 3;
-    private static final int ONE_STRIKE = 1;
-    private static final int ONE_BALL = 1;
-    private static final int NO_STRIKE = 0;
-    private static final int NO_BALL = 0;
-    private static final String NOTHING = "낫싱";
+    private final List<Integer> numbers;
 
-    private List<Integer> numbers;
-
-    public Computer(final List<Integer> numbers) {
+    private Computer(final List<Integer> numbers) {
         this.numbers = numbers;
     }
 
-    public String calculateGuessNumberResult(final List<Integer> guessNumbers) {
-        int ballCount = NO_BALL;
-        int strikeCount = NO_STRIKE;
-        for (int index = 0; index < BASEBALL_GAME_NUMBER_DIGIT; index++) {
-            ballCount += calculateBall(getList(), guessNumbers.get(index), index);
-            strikeCount += calculateStrike(getList().get(index), guessNumbers.get(index));
-        }
-        checkThreeStrike(strikeCount);
-        return conditionalResult(strikeCount, ballCount).getResult();
-    }
-
-    private int calculateBall(final List<Integer> computerNumber, final int guessNumber, final int index) {
-        if (isBall(computerNumber, guessNumber, index)) {
-            return ONE_BALL;
-        }
-        return NO_BALL;
-    }
-
-    private boolean isBall(final List<Integer> computerNumber, final int guessNumber, final int index) {
-        return computerNumber.contains(guessNumber) && computerNumber.get(index) != guessNumber;
-    }
-
-    private int calculateStrike(final int computerNumber, final int guessNumber) {
-        if (isStrike(computerNumber, guessNumber)) {
-            return ONE_STRIKE;
-        }
-        return NO_STRIKE;
-    }
-
-    private boolean isStrike(int computerNumber, int guessNumber) {
-        return computerNumber == guessNumber;
-    }
-
-    private void checkThreeStrike(final int strikeCount) {
-        if (isThreeStrike(strikeCount)) {
-            threeStrike = true;
-        }
-    }
-
-    private boolean isThreeStrike(final int strikeCount) {
-        return strikeCount == THREE_STRIKE;
-    }
-
-    private GuessNumberResultResponse conditionalResult(final int strikeCount, final int ballCount) {
-        if (isNothing(strikeCount, ballCount)) {
-            return GuessNumberResultResponse.from(NOTHING);
-        }
-        return GuessNumberResultResponse.of(ballCount, strikeCount);
-    }
-
-    private boolean isNothing(final int strikeCount, final int ballCount) {
-        return strikeCount == NO_STRIKE && ballCount == NO_BALL;
-    }
-
-    private List<Integer> getList() {
-        if (numbers == null) {
-            numbers = new ArrayList<>();
-            createNumbers();
-        }
-        return numbers;
-    }
-
-    private void createNumbers() {
+    public static Computer createDefault() {
         BaseBallGameNumberGenerator baseBallGameNumberGenerator = new BaseBallGameNumberGenerator();
-        numbers = baseBallGameNumberGenerator.generateNumbers();
+        return new Computer(baseBallGameNumberGenerator.generateNumbers());
     }
 
-    public boolean isGameOver() {
-        return threeStrike;
+    public boolean isExistNumber(final int number) {
+        return numbers.contains(number);
     }
 
-    public void end() {
-        numbers = null;
-        threeStrike = false;
+    public boolean isBall(final int guessNumber, final int index) {
+        return isExistNumber(guessNumber) && numbers.get(index) != guessNumber;
+    }
+
+    public boolean isStrike(final int guessNumber, final int index) {
+        return numbers.get(index) == guessNumber;
     }
 }
