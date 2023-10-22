87b5254e560cac293a63b40a3dc9bc91559e7f21
yubin <dbqls2821@gmail.com>
2023-10-22 16:32:20 +0900

    refactor: 볼과 스트라이크 점수를 String으로 만들 때 가독성있도록 메소드 내부 리팩토링

--- src/main/java/baseball/Result.java
+++ src/main/java/baseball/Result.java
@@ -7,12 +7,10 @@ record Result(int ballCount, int strikeCount) {
     }
 
     String getResultInKorean() {
-        if(ballCount > 0 && strikeCount > 0)
-            return ballCount + OutputMessage.BALL.message + " " + strikeCount + OutputMessage.STRIKE.message;
-        else if (ballCount > 0)
-            return ballCount + OutputMessage.BALL.message;
-        else if (strikeCount > 0)
-            return strikeCount + OutputMessage.STRIKE.message;
-        else return OutputMessage.NOTHING.message;
+        String ball = ballCount > 0 ? ballCount + OutputMessage.BALL.message : "";
+        String strike = strikeCount > 0 ? strikeCount + OutputMessage.STRIKE.message : "";
+
+        String result = (ball.isEmpty() && strike.isEmpty()) ? OutputMessage.NOTHING.message : ball + " " + strike;
+        return result.trim();
     }
 }
