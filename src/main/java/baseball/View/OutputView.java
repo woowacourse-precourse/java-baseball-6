b0a4f21af0d5dac716e40b83b1b56f6353949f46
sungwon2598 <tjddnjs2598@gmail.com>
2023-10-22 09:46:35 +0900

    refactor(OutputView) : 문자열 메시지를 상수로 변경

--- src/main/java/baseball/View/OutputView.java
+++ src/main/java/baseball/View/OutputView.java
@@ -1,20 +1,25 @@
 package baseball.View;
 
 public class OutputView {
+    private static final String START_GAME_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String BALL_MESSAGE = "볼 ";
+    private static final String STRIKE_MESSAGE = "스트라이크";
+    private static final String NOTHING_MESSAGE ="낫싱";
+    private static final int NO_COUNT = 0;
 
     public void printGameStart(){
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println(START_GAME_MESSAGE);
     }
     public void printHint(int ballCount, int strikeCount) {
         String hint = "";
-        if (ballCount > 0) {
-            hint += ballCount + "볼 ";
+        if (ballCount > NO_COUNT) {
+            hint += ballCount + BALL_MESSAGE;
         }
-        if (strikeCount > 0) {
-            hint += strikeCount + "스트라이크";
+        if (strikeCount > NO_COUNT) {
+            hint += strikeCount + STRIKE_MESSAGE;
         }
         if (hint.isEmpty()) {
-            hint = "낫싱";
+            hint = NOTHING_MESSAGE;
         }
         System.out.println(hint);
     }
