c4295726a6e65a2f72d2768028355c5c955ea367
Brian <bwook9908@gmail.com>
2023-10-23 00:53:45 +0900

    refactor: Inning 로직 추가
    - result를 저장하는 필드 추가

--- src/main/java/baseball/model/Inning.java
+++ src/main/java/baseball/model/Inning.java
@@ -8,12 +8,14 @@ public class Inning {
     private final ArrayList<Integer> input;
     private final int strike;
     private final int ball;
+    private final String result;
 
     public Inning(ArrayList<Integer> answer, ArrayList<Integer> input) {
         this.answer = answer;
         this.input = input;
         this.strike = checkSameNumberAndOrder();
         this.ball = checkSameNumber() - strike;
+        this.result = calculateResult(ball, strike);
     }
 
 
@@ -33,12 +35,19 @@ public class Inning {
         }
         return count;
     }
-
-    public int getStrike() {
-        return strike;
+    private String calculateResult(int ball, int strike) {
+        if (ball > 0 && strike > 0) {
+            return ball + "볼 " + strike + "스트라이크";
+        }
+        if (ball > 0 && strike == 0) {
+            return ball + "볼";
+        }
+        if (ball == 0 && strike > 0) {
+            return strike + "스트라이크";
+        }
+        return "낫싱";
     }
-
-    public int getBall() {
-        return ball;
+    public String getResult() {
+        return result;
     }
 }
