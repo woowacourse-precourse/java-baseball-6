b3d9e7080148f7aa62d7491cd3c404ff7cd8dcb1
JINU-CHANG <jenny010925@naver.com>
2023-10-22 22:28:29 +0900

    [feat] 입력값 생성하는 기능 구현

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,12 +1,18 @@
 package baseball.controller;
 
+import baseball.domain.InputNumber;
+import camp.nextstep.edu.missionutils.Console;
 import baseball.domain.TargetNumber;
+import baseball.view.InputView;
 
 public class GameController {
 	private static int NUMBER_SIZE = 3;
 	private TargetNumber targetNumber;
+	private InputNumber inputNumber;
 
 	public void run() {
 		targetNumber = TargetNumber.create(NUMBER_SIZE);
+		InputView.askNumber();
+		inputNumber = InputNumber.create(Console.readLine(),NUMBER_SIZE);
 	}
 }
---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,10 @@
+package baseball.view;
+
+public class InputView {
+
+	private static final String ASK_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+
+	public static void askNumber() {
+		System.out.print(ASK_INPUT_MESSAGE);
+	}
+}
