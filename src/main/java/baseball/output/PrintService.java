8c6112246d4c998ebeac4406f6d6de724217a1a9
cousim46 <wjdghldns24@naver.com>
2023-10-22 14:52:33 +0900

    Add : 게임 시작, 값 입력 요청, 입력값, 게임 정답시에 대한 문구 응답해주는 메서드 추가

--- src/main/java/baseball/output/PrintService.java
+++ src/main/java/baseball/output/PrintService.java
@@ -1,6 +1,19 @@
 package baseball.output;
 
 public class PrintService {
-
-
+    public static void start() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+    public static void input() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+    public static void inputValue(String value) {
+        System.out.println(value);
+    }
+    public static void end() {
+        System.out.print("""
+            3개의 숫자를 모두 맞히셨습니다! 게임 종료
+            게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.
+            """);
+    }
 }
