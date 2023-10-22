4d772dbe17893f5c984453933179baed37a8d944
Annjieun <dkslwdms2121@naver.com>
2023-10-22 14:16:39 +0900

    feat: 컴퓨터의 랜덤 숫자를 생성하는 기능 구현

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -2,8 +2,12 @@ package baseball;
 
 import baseball.validation.Validator;
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static baseball.constant.NumberConstant.*;
 
 public class Baseball {
     private final Validator validator = new Validator();
@@ -12,5 +16,16 @@ public class Baseball {
         List<Integer> player = validator.validateInputNumber(input);
 
         return player;
+    }
+
+    public List<Integer> getComputerNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < INPUT_LENGTH) {
+            int randomNumber = Randoms.pickNumberInRange(MINIMUM_RANGE_NUMBER, MAXIMUM_RANGE_NUMBER);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 }
