a461e0d68ce7abad1d4ab473228d5c6e3c68e1dc
Kuen Hee Lee <Leekh10195@gmail.com>
2023-10-22 20:24:05 +0900

    feat: Announcer 구현

--- src/main/java/baseball/NumberBaseballGameConfig.java
+++ src/main/java/baseball/NumberBaseballGameConfig.java
@@ -9,4 +9,10 @@ public class NumberBaseballGameConfig {
     public static final int CONTINUE_GAME = 1;
     public static final int EXIT_GAME = 2;
 
+    // 메시지 관련 상수
+    public static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public static final String INPUT_PROMPT = "숫자를 입력해주세요 : ";
+    public static final String WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String CONTINUE_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
 }
