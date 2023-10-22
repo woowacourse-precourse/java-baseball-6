bdc44c4c3de5dbccddf9b32297ec98611ae29451
Woongbae Yun <devbelly@naver.com>
2023-10-22 15:30:53 +0900

    fix : ball, strike 갯수 출력 로직 수정

--- src/main/java/baseball/Information.java
+++ src/main/java/baseball/Information.java
@@ -19,12 +19,19 @@ public class Information {
     public static void printBallAndStrike(int ball, int strike) {
         if (ball == 0 && strike == 0) {
             System.out.println("낫싱");
+            return;
+        }
+        if (ball != 0 && strike != 0) {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+            return;
         }
         if (ball != 0) {
             System.out.println(ball + "볼");
+            return;
         }
         if (strike != 0) {
             System.out.println(strike + "스트라이크");
+            return;
         }
     }
 
