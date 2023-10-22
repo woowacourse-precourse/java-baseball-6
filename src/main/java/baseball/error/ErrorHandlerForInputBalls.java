679d785ab204dc62d4ec5669a39809251badeb27
JangHongJoon <wkdghdwns19969@gmail.com>
2023-10-23 05:34:09 +0900

    Feat : 에러 검증 ErrorHandlerForInputBalls.java 객체 생성

---
+++ src/main/java/baseball/error/ErrorHandlerForInputBalls.java
@@ -0,0 +1,19 @@
+package baseball.error;
+
+public class ErrorHandlerForInputBalls implements ErrorHandler {
+
+    @Override
+    public boolean checkString(String numberString){
+        try{
+            new CheckerForLengthOfInputBalls().check(numberString);
+            new CheckerForValidIntegerInputOfInputBalls().check(numberString);
+            new CheckerForRangeOfInputBalls().check(numberString);
+            new CheckerForDuplicationOfInputBalls().check(numberString);
+        }
+        catch (IllegalArgumentException illegalArgumentException){
+            throw illegalArgumentException;
+        }
+
+        return true;
+    }
+}
