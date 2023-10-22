58a88473055762742c4f27031bdd79e32920e88d
EunKyo Chae <dmsry1022@gmail.com>
2023-10-22 15:45:35 +0900

    Computer 기능 구현

--- src/main/java/baseball/ComputerGame.java
+++ src/main/java/baseball/ComputerGame.java
@@ -1,4 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+
 public class ComputerGame {
+    static final int MIN_NUMBER = 1;
+    static final int MAX_NUMBER = 9;
+    static final int NUM_COUNT = 3;
+    public static void RandomNumber(){
+        // 컴퓨터의 숫자 랜덤으로 3자리 뽑아내기
+        Set<Integer> numberSet = new HashSet<>(); // HashSet은 중복저장하지 않는다.
+        while(numberSet.size() < NUM_COUNT){ // 0, 1, 2 => 3자리
+            int randomNum = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER); // 1~9에서 랜덤
+            if(!numberSet.contains(randomNum)){ //
+                numberSet.add(randomNum);
+            }
+        }
+
+
+    }
 }
