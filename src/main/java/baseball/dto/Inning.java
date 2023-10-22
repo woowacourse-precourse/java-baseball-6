34242ea0db84e089e6e12c2ec7362e12aa3b8cba
NAM GIBEOM <rlqja0523@naver.com>
2023-10-22 22:17:48 +0900

    feat(Inning): strike 필드 추가

--- src/main/java/baseball/dto/Inning.java
+++ src/main/java/baseball/dto/Inning.java
@@ -2,20 +2,20 @@ package baseball.dto;
 
 import baseball.enums.Hint;
 
-public record Inning(String result) {
+public record Inning(int strike, String result) {
     public static Inning ofBall(int ball) {
-        return new Inning(ball + Hint.BALL.getValue());
+        return new Inning(0, ball + Hint.BALL.getValue());
     }
 
     public static Inning ofStrike(int strike) {
-        return new Inning(strike + Hint.STRIKE.getValue());
+        return new Inning(strike, strike + Hint.STRIKE.getValue());
     }
 
     public static Inning ofBallAndStrike(int ball, int strike) {
-        return new Inning(ball + Hint.BALL.getValue() + " " + strike + Hint.STRIKE.getValue());
+        return new Inning(strike, ball + Hint.BALL.getValue() + " " + strike + Hint.STRIKE.getValue());
     }
 
     public static Inning ofNothing() {
-        return new Inning(Hint.NOTHING.getValue());
+        return new Inning(0, Hint.NOTHING.getValue());
     }
 }
