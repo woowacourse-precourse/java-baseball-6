59f443c2be44b8b070491c5b820a4989d826e888
acycoco <pink10257@naver.com>
2023-10-22 19:35:33 +0900

    feat(Computer): 스트라이크, 볼 개수 세는 기능 구현

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -2,18 +2,20 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class Computer {
 
     private final int[] secretNumbers = new int[3];
+    private final Set<Integer> numberSet = new HashSet<>();
 
     public Computer() {
     }
 
     public int[] pickNumbers() {
-        Set<Integer> numberSet = new HashSet<>();
         while (numberSet.size() < 3) {
             int newNumber = Randoms.pickNumberInRange(1, 9);
             if (numberSet.contains(newNumber))
@@ -24,5 +26,21 @@ public class Computer {
         return secretNumbers;
     }
 
+    public Map<String, Integer> countStrikesAndBalls(int[] guessNumbers) {
+        int strikes = 0;
+        int balls = 0;
+        for (int idx = 0; idx < 3; idx++) {
+            if (secretNumbers[idx] == guessNumbers[idx]) {
+                strikes++;
+            } else {
+                if (numberSet.contains(guessNumbers[idx]))
+                    balls++;
+            }
+        }
+        Map<String, Integer> result = new HashMap<>();
+        result.put("strikes", strikes);
+        result.put("balls", balls);
+        return result;
+    }
 
 }
