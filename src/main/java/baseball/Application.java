7bab732c1094bf7c313d25f11ce5d9c1ef33b43d
lee jihyeon <lejinhy@gmail.com>
2023-10-22 13:41:36 +0900

    fix: try-catch문 삭제(테스트시 오류가 발생했음에도 테스트 통과하지 못한 문제 해결)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,11 +3,6 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         GameController gameController = new GameController();
-        try {
-            gameController.start();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-        }
+        gameController.start();
     }
 }
