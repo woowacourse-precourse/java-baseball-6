bee73edeaa68d420e14a13adb51ea881120dfb57
jaehee <y2hjjh@naver.com>
2023-10-23 01:48:21 +0900

    feat: 낫싱 출력 기능 작성

--- src/main/java/baseball/controller/PrintController.java
+++ src/main/java/baseball/controller/PrintController.java
@@ -1,2 +1,26 @@
-package baseball.controller;public class PrintController {
+package baseball.controller;
+
+import baseball.model.InputNumber;
+
+public class PrintController {
+    private CountController countController = new CountController();
+    private int strikeCount;
+    private int ballCount;
+
+    public String printCount(InputNumber inputNumber) {
+        StringBuilder output = new StringBuilder();
+        strikeCount = countController.getStrikeCount(inputNumber);;
+        ballCount = countController.getBallCount(inputNumber);;
+
+        if (ballCount != 0) {
+            output.append(ballCount + "볼 ");
+        }
+        if (strikeCount != 0) {
+            output.append(strikeCount + "스트라이크");
+        }
+        if (output.isEmpty()) {
+            return "낫싱";
+        }
+        return output.toString();
+    }
 }
