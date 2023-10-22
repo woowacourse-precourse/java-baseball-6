2c5ade23cfff14e0480ce6d87cf0c8a220c945f7
dlrjs2360 <lhg3670@naver.com>
2023-10-22 19:59:47 +0900

    :sparkles: feat: 게임 시작 메시지 출력
    - 게임을 처음 시작할 때의 메시지를 출력합니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 게임 시작
+        System.out.println("숫자 야구 게임을 시작합니다.");
     }
 }
