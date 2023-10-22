76ad2d24e656f96e5aea5332e141ecac0f6da466
706com <ehdgusjdbswl@gmail.com>
2023-10-22 19:11:16 +0900

    refactor : 입력 메세지 분리

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -6,12 +6,16 @@ import java.util.Scanner;
 
 public class InputView {
 
+    private final String inputNumberMessage = "숫자를 입력해주세요 : ";
+
+    private final String inputRestartMessage = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
     public String inputNumbers(){
-        System.out.print("숫자를 입력해주세요 : ");
+        System.out.print(inputNumberMessage);
         return Console.readLine();
     }
     public String inputRestart(){
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println(inputRestartMessage);
         return Console.readLine();
     }
 }
