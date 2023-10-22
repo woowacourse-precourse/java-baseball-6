fd52470fb03029267e4929aca1efa595e12d2af4
Happeeee <cmg118@naver.com>
2023-10-23 00:02:54 +0900

    Feat: Add gameStart
    게임 흐름 뼈대 구현

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,27 @@
+package baseball;
+
+import java.util.List;
+
+public class Game {
+    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String NUMBER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String GAME_OVER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private List<Integer> computerNumber;
+    private List<Integer> playerNumber;
+    private boolean exit = false;
+    private NumberGenerator numberGenerator = new NumberGenerator();
+    private Calculator calculator = new Calculator();
+
+    public void gameStart() {
+        System.out.println(GAME_START_MESSAGE);
+        computerNumber = numberGenerator.setComputerNumber(); // 컴퓨터 번호 셋팅
+        while (!exit) {
+            playerNumber = numberGenerator.setPlayerNumber(); // 플레이어 번호 셋팅
+            check(computerNumber, playerNumber); // 결과 확인 + 조건 분기 (종료 or 재시작)
+        }
+    }
+
+    private void check(List<Integer> computerNumber, List<Integer> playerNumber) {
+        return;
+    }
+}
---
+++ src/main/java/baseball/NumberGenerator.java
@@ -0,0 +1,18 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberGenerator {
+    private static final int SIZE = 3;
+
+    public List<Integer> setComputerNumber() {
+        List<Integer> dummy = new ArrayList<>();
+        return dummy;
+    }
+
+    public List<Integer> setPlayerNumber() {
+        List<Integer> dummy = new ArrayList<>();
+        return dummy;
+    }
+}
