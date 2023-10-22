b45d8e35c25752bfc80c7d74597217ca039b1f68
Mr.Kim <joajy98@gmail.com>
2023-10-22 09:47:06 +0900

    refactor: Constant import를 통해 식 간소화(Constant. 생략)

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -24,14 +24,14 @@ public class Validator {
 
     public static void checkNumberLength(String number) {
         // 입력받은 값의 길이가 3이 아닐 경우
-        if (number.length() != Constant.NUMBER_SIZE) {
+        if (number.length() != NUMBER_SIZE) {
             throw new IllegalArgumentException();
         }
     }
 
     public static void checkNumberType(String number) {
         // 1부터 9 사이의 "숫자"로 구성되지 않았을 경우
-        if (!number.matches(Constant.NUM_REGEX)) {
+        if (!number.matches(NUM_REGEX)) {
             throw new IllegalArgumentException();
         }
     }
