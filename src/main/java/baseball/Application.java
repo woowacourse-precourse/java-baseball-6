56af301005ab261dbea7b02236d0a7cc29d34da7
soyoungxxx <otherplet@gmail.com>
2023-10-23 05:36:45 +0900

    style(Application): gamecontroller 이름 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,7 +5,7 @@ import baseball.game.GameControl;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        GameControl gamecontoller = new GameControl();
-        gamecontoller.startGame();
+        GameControl controller = new GameControl();
+        controller.startGame();
     }
 }
