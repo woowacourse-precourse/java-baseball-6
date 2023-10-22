54c2f0d55f6bb32afcb314ebe7c555f698251331
meridaKim <sujin7187@naver.com>
2023-10-22 23:21:42 +0900

    feat:add computer function creating random number
    Write implementing function list before start

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,20 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
+
     public static void main(String[] args) {
+
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        Player player = new Player(Integer.parseInt(Console.readLine()));
+        player.validateNumber(player);
+        Computer computer = new Computer(Randoms.pickNumberInRange(1, 9) * 100 + Randoms.pickNumberInRange(1, 9) * 10 + Randoms.pickNumberInRange(1, 9));
+        System.out.println("player num: "+player.player_num);
+        System.out.println("computer num: "+computer.computer_num);
+
+
     }
 }
---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,12 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Computer {
+    int computer_num;
+
+    public Computer(int computer_num) {
+        this.computer_num = computer_num;
+    }
+}
