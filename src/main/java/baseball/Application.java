179939b88aaf51c9c3a906fdbf3472991b5f3cdc
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-10-22 14:57:16 +0900

    feat: 게임 시작 문구 추가
    - Computer Class에 게임 시작 문구 출력 함수 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,11 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        boolean progress = true;
+        Computer computer = new Computer();
+        while (progress) {
+            computer.gameStartPhrase();
+        }
+
     }
 }
