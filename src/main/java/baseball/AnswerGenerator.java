94cfbb2af838735733db99b406c44a3f37c28a67
jihyuk <kwlgur0416@gmail.com>
2023-10-23 00:20:32 +0900

    feature : 1부터 9까지 서로 다른 수로 이루어진 3자리 수 생성

--- src/main/java/baseball/AnswerGenerator.java
+++ src/main/java/baseball/AnswerGenerator.java
@@ -1,12 +1,32 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AnswerGenerator {
 
+    private final static int startInclusive = 1;
+    private final static int endInclusive = 9;
+    private final static int answerSize = 3;
+
     public List<Integer> generateAnswer() {
-        return new ArrayList<>();
+        List<Integer> answer = new ArrayList<>();
+        while (answer.size() < answerSize) {
+            int randomNumber = Randoms.pickNumberInRange(startInclusive, endInclusive);
+            addRandomNumber(answer, randomNumber);
+        }
+        return answer;
+    }
+
+    private void addRandomNumber(List<Integer> answer, int randomNumber) {
+        if (validate(answer, randomNumber)) {
+            answer.add(randomNumber);
+        }
+    }
+
+    private boolean validate(List<Integer> answer, int randomNumber) {
+        return !answer.contains(randomNumber);
     }
 
 }
