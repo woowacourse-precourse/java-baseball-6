33268738b1fdd45a7395f890ca0f1a8f0f550061
Jinseo Jeong <js020315@gmail.com>
2023-10-22 16:03:38 +0900

    [FEAT] WhetherRestart
    숫자를 맞춘 후 게임 재시작과 종료 여부를 입력받는 함수

--- src/main/java/baseball/WhetherRestart.java
+++ src/main/java/baseball/WhetherRestart.java
@@ -1,16 +1,20 @@
 package baseball;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
 
 public class WhetherRestart {
-    int gameEndNumber;
-    void printGameEnd() {
+    boolean scanGameEndNumber() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-    }
 
-    void scanGameEndNumber() {
-        Scanner scan = new Scanner(System.in);
-        gameEndNumber = scan.nextInt();
+        String endNumber = Console.readLine();
+
+        if (endNumber.equals("1")) {
+            return true;
+        } else if (endNumber.equals("2")) {
+            return false;
+        } else {
+            throw new IllegalArgumentException("Invalid argument: " + endNumber);
+        }
     }
 }
