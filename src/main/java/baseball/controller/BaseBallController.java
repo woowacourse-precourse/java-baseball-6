b93e0a14733795a7089cae2d25761d4d228ca1e2
Relaxed-Mind <dlwlstn0709@uos.ac.kr>
2023-10-22 10:42:46 +0900

    Rename: fix a typo in BaseBallController BaseBallController가 static이어서 바로 사용한 것 수정 Generating computer Number #2

--- src/main/java/baseball/controller/BaseBallController.java
+++ src/main/java/baseball/controller/BaseBallController.java
@@ -5,12 +5,14 @@ import baseball.service.SystemService;
 import java.util.List;
 
 public class BaseBallController {
+    private BaseBallService baseBallService = new BaseBallService();
     private SystemService systemService = new SystemService();
 
     public void start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
         List<Integer> computerNum = systemService.generateComputerNum();
-        BaseBallService.init(computerNum);
+        baseBallService.init(computerNum);
+        
 
     }
 }
