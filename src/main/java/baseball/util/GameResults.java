4c783767a3278d704bb724492d23a4e4c65cc993
leesh <qwqw8019@naver.com>
2023-10-22 19:32:29 +0900

    rename: 문자열 상수 디렉토리 구조 변경
    - ConsoleMessages
      - Console에 의해 입출력되는 문자열 상수 정의
    - GameResultMessageGenerator
      - 게임 결과에 따른 문자열 생성기
    - GameResults
      - 게임 결과 문자열 상수 정의

---
+++ src/main/java/baseball/util/GameResults.java
@@ -0,0 +1,17 @@
+package baseball.util;
+
+public class GameResults {
+
+    public static final String BALL = "볼 ";
+    public static final String STRIKE = "스트라이크";
+    public static final String NOTHING = "낫싱";
+
+
+    public static final String ONE_BALL = "1볼 ";
+    public static final String TWO_BALL = "2볼 ";
+    public static final String THREE_BALL = "3볼 ";
+    public static final String ONE_STRIKE = "1스트라이크";
+    public static final String TWO_STRIKE = "2스트라이크";
+    public static final String THREE_STRIKE = "3스트라이크";
+    public static final String GAME_OVER = "게임 종료";
+}
