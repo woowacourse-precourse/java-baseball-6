0ec58d273d9d09bd9046cafa8b84292273b907f9
jy016011 <39334789+jy016011@users.noreply.github.com>
2023-10-22 15:59:50 +0900

    style: 문맥에 맞게 상수 이름 변경

--- src/main/java/baseball/models/Constants.java
+++ src/main/java/baseball/models/Constants.java
@@ -9,6 +9,6 @@ public class Constants {
     public static final int USER_ANSWER_LOWER_LIMIT = 1;
     public static final int USER_ANSWER_UPPER_LIMIT = 9;
     public static final int WANNA_KEEP_PLAYING = 1;
-    public static final int END_NUMBER = 2;
+    public static final int WANNA_END = 2;
     public static final int MAX_STRIKES = ANSWER_LENGTH;
 }
