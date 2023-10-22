ac1c389fda85dde3cfa9729c0e70053836e7e865
kkonii <119216472+kkonii@users.noreply.github.com>
2023-10-23 02:09:20 +0900

    Feat(result) : 사용자의 숫자와 랜덤숫자의 인덱스 값을 비교한다
    - ball과 strike의 값을 관리하는 객체이다
    - caculate()는 사용자의 숫자 리스트와 랜덤 숫자 리스트를 매개변수로 받는다
    - isSameIndex()에서 두 리스트의 인덱스 값이 일치하는지를 검사한다

---
+++ src/main/java/baseball/Result.java
@@ -0,0 +1,19 @@
+package baseball;
+
+import java.util.List;
+
+public class Result {
+    private int ball;
+    private int strike;
+
+    public void calculate(List<Character> userNumbers, List<Character> randomNumbers) {
+        for (int index = 0; index < userNumbers.size(); index++) {
+            char number = userNumbers.get(index);
+
+        }
+    }
+
+    private boolean isSameIndex(int index, char number, List<Character> randomNumbers) {
+        return index == randomNumbers.indexOf(number);
+    }
+}
