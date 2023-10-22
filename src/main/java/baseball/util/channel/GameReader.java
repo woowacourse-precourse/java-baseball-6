88f08f76aa9d70e7dec175c37bc08ce51e67372c
yummygyudon <bang2brew@gmail.com>
2023-10-23 00:11:31 +0900

    DOCS : 야구 게임에서 사용되는 사용자 입력기 GameReader 객체 주석 작성

--- src/main/java/baseball/util/channel/GameReader.java
+++ src/main/java/baseball/util/channel/GameReader.java
@@ -9,12 +9,18 @@ public abstract class GameReader {
         Printer.print(command.getCommand());
         String input = Console.readLine().trim();
         validate(input);
+
+        // 사용자의 Pitching 입력 값을 요청하는 명령인데 입력한 숫자가 3 자리가 아닐 경우
         if (command.equals(Command.PITCHING) && !(input.length() == 3)) {
             throw new IllegalArgumentException();
         }
         return input;
     }
 
+    /**
+     * @param input 사용자의 입력 값
+     * @exception IllegalArgumentException 빈 값을 입력한 경우 & 슷지가 아닌 값을 입력한 경우
+     */
     private static void validate(String input) {
         if (input.isBlank()) {
             throw new IllegalArgumentException();
