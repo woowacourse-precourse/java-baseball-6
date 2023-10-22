6b2ee8b29a4d28d5b5efae7a1384c18fade76a0b
Jihyun3478 <leejihyun3478@gmail.com>
2023-10-23 06:31:29 +0900

    Feat(ProgressService): 결과 메시지 반환 기능 구현

---
+++ src/main/java/baseball/service/ProgressService.java
@@ -0,0 +1,72 @@
+package baseball.service;
+
+import static baseball.constant.Constant.END;
+import static baseball.constant.Constant.MAX_NUMBER;
+import static baseball.constant.Constant.MIN_NUMBER;
+import static baseball.constant.Constant.NUMBER_LENGTH;
+import static baseball.constant.Constant.RESTART;
+import static baseball.message.CountMessage.NOTHING;
+
+import baseball.validation.NumberValidation;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Arrays;
+
+public class ProgressService {
+
+    private int[] computerNumber;
+    private CountService countService;
+    private boolean isEnd = false;
+
+    public ProgressService() {
+        resetGame();
+    }
+
+    public void resetGame() {
+        computerNumber = getComputerNumber();
+        countService = new CountService(computerNumber);
+        isEnd = false;
+    }
+
+    public int[] getComputerNumber() {
+        int[] number;
+        do {
+            number = createComputerNumber();
+        } while (!NumberValidation.validateComputerDuplicate(number));
+        computerNumber = number;
+        return computerNumber;
+    }
+
+    private static int[] createComputerNumber() {
+        return Arrays.stream(new int[NUMBER_LENGTH])
+                .map(i -> Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER))
+                .toArray();
+    }
+
+    public String getResultMessage(int[] playerNumber) {
+        int strikeCount = countService.getStrikeCount(computerNumber, playerNumber);
+        int ballCount = countService.getBallCount(computerNumber, playerNumber);
+        checkGameStatus(strikeCount, ballCount);
+        if (countService.isNothing()) {
+            return NOTHING.getMessage();
+        }
+        return countService.getResultCount(computerNumber, playerNumber);
+    }
+
+    public boolean isGameClear() {
+        return isEnd;
+    }
+
+    public int getResultGameStatus(String input) {
+        NumberValidation.validateGameStatus(input);
+        if (input.equals("1")) {
+            return RESTART;
+        }
+        return END;
+    }
+
+    public void checkGameStatus(int strikeCount, int ballCount) {
+        if (strikeCount == 3 && ballCount == 0) {
+            this.isEnd = true;
+        }
+    }
+}
