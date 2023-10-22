14b99514609145e0904cd74e8ef95de80d9a3c8f
cousim46 <wjdghldns24@naver.com>
2023-10-22 14:50:07 +0900

    Add : 사용자로부터 값을 요청받는 메서드 추가, 게임 종료시 Console 닫는 메서드 추가

--- src/main/java/baseball/input/InputService.java
+++ src/main/java/baseball/input/InputService.java
@@ -1,6 +1,12 @@
 package baseball.input;
 
+import camp.nextstep.edu.missionutils.Console;
 public class InputService {
+    public static String readLine() {
+        return Console.readLine();
+    }
 
-
+    public static void close() {
+        Console.close();
+    }
 }
