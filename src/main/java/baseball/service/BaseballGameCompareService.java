761cfc70f03a778ce161b5fc0646ebc57854b9ee
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-10-22 16:01:03 +0900

    refactor: 모호한 변수명 변경
    의도가 불분명한 `target1`과 `target2`를 `userNumberList`와 `randomNumberList`로 변경하였음.

--- src/main/java/baseball/service/BaseballGameCompareService.java
+++ src/main/java/baseball/service/BaseballGameCompareService.java
@@ -6,40 +6,40 @@ import java.util.List;
 import java.util.Objects;
 
 public class BaseballGameCompareService {
-    public void compareUserNumberAndRandomNumber(List<Integer> target1, List<Integer> target2) {
+    public void compareUserNumberAndRandomNumber(List<Integer> userNumberList, List<Integer> randomNumberList) {
         OutputView outputView = new OutputView();
-        int strikeCount = countStrike(target1, target2);
-        int ballCount = countBall(target1, target2) - strikeCount;
+        int strikeCount = countStrike(userNumberList, randomNumberList);
+        int ballCount = countBall(userNumberList, randomNumberList) - strikeCount;
         outputView.printGameResultBallAndStrike(ballCount, strikeCount);
-        isThreeStrike(strikeCount, target2);
+        isThreeStrike(strikeCount, randomNumberList);
     }
 
-    private int countStrike(List<Integer> target1, List<Integer> target2) {
+    private int countStrike(List<Integer> userNumberList, List<Integer> randomNumberList) {
         int strikeCount = 0;
-        for (int i = 0; i < target1.size(); i++) {
-            if (Objects.equals(target1.get(i), target2.get(i))) {
+        for (int i = 0; i < userNumberList.size(); i++) {
+            if (Objects.equals(userNumberList.get(i), randomNumberList.get(i))) {
                 strikeCount++;
             }
         }
         return strikeCount;
     }
 
-    private int countBall(List<Integer> target1, List<Integer> target2) {
+    private int countBall(List<Integer> userNumberList, List<Integer> randomNumberList) {
         int ballCount = 0;
-        for (int i = 0; i < target1.size(); i++) {
-            if (target2.contains(target1.get(i))) {
+        for (int i = 0; i < userNumberList.size(); i++) {
+            if (randomNumberList.contains(userNumberList.get(i))) {
                 ballCount++;
             }
         }
         return ballCount;
     }
 
-    private void isThreeStrike(int strikeCount, List<Integer> target2) {
+    private void isThreeStrike(int strikeCount, List<Integer> randomNumberList) {
         BaseballGameService baseballGameService = new BaseballGameService();
         if (strikeCount == 3) {
             baseballGameService.endBaseballGame();
         } else {
-            baseballGameService.startBaseballGame(target2);
+            baseballGameService.startBaseballGame(randomNumberList);
         }
     }
 }
