a42d054b1974fa36a1607fc3c5a819260602096c
ssuzyn <asj7090@gmail.com>
2023-10-22 16:33:01 +0900

    feat: 사용자 입력 검증 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -26,6 +26,44 @@ class Computer{
     }
 }
 
+class User {
+    private final List<Integer> userNum;
+    public User() {
+        this.userNum = new ArrayList<>();
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+
+        try{
+            isValidInput(input);
+        }catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+    }
+    public List<Integer> getUserNum() {
+        return userNum;
+    }
+
+    private void isValidInput(String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("3개의 숫자를 입력해주세요.");
+        }
+
+        char[] chars = input.toCharArray();
+        for (char c : chars) {
+            if (c < '1' || c > '9') {
+                throw new IllegalArgumentException("1과 9 사이의 숫자를 입력해주세요.");
+            }
+
+            int num = Character.getNumericValue(c);
+            if (userNum.contains(num)) {
+                throw new IllegalArgumentException("서로 다른 숫자를 입력해주세요.");
+            }
+            userNum.add(num);
+
+        }
+    }
+}
+
 class Game {
     private Computer computer;
 
@@ -38,7 +76,6 @@ class Game {
             System.out.println("숫자 야구 게임을 시작합니다.");
         }
     }
-
 
 }
 public class Application {
