5e6b7705a25a0f29444e90618e7837528a5a3af1
임강호 <55732952+Icln@users.noreply.github.com>
2023-10-22 19:43:02 +0900

    refactor: 검증 객체 생성

---
+++ src/main/java/baseball/Validation.java
@@ -0,0 +1,9 @@
+package baseball;
+
+public class Validation {
+    private static final int ZERO = 0;
+    private static final int RESTART_NUMBER = 1;
+    private static final int QUIT_NUMBER = 2;
+    private static final int CORRECT_INPUT_RANGE = 3;
+
+}
