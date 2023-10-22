8a9ecfaac37a2292ff5e981623cfd76473282b66
ohhamma <judyham10634@naver.com>
2023-10-22 18:34:07 +0900

    :sparkles: feat: 랜덤값 지정 기능 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,5 +1,24 @@
 package baseball;
 
-public class Game {
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+    List<Integer> randomInt;
+
+    public Game() {
+        SetRandomInt();
+    }
+
+    public void SetRandomInt() {
+        randomInt = new ArrayList<>();
+        while (randomInt.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!randomInt.contains(randomNumber)) {
+                randomInt.add(randomNumber);
+            }
+        }
+    }
 }
