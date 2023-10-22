cc043a153b9892bdda7c08ccd379cf96ce09cd4c
NAM GIBEOM <rlqja0523@naver.com>
2023-10-22 21:43:19 +0900

    refactor(GameButton): class로 변경해서 GameStatus를 필드로 가지도록 변경

--- src/main/java/baseball/domain/GameButton.java
+++ src/main/java/baseball/domain/GameButton.java
@@ -2,24 +2,14 @@ package baseball.domain;
 
 import baseball.enums.GameStatus;
 
-public record GameButton(int number) {
-    public GameButton {
-        validateNumber();
+public class GameButton {
+    private GameStatus gameStatus = GameStatus.PLAY;
+
+    public void turnButton(String code) {
+        this.gameStatus = GameStatus.of(code);
     }
 
-    private void validateNumber() {
-        int restartNumber = GameStatus.PLAY.getStatus().charAt(0);
-        int exitNumber = GameStatus.EXIT.getStatus().charAt(0);
-        if (number != restartNumber && number != exitNumber) {
-            throw new IllegalArgumentException(String.format("게임 재시작은 %d, 종료는 %d이어야 합니다.", restartNumber, exitNumber));
-        }
-    }
-
-    public boolean isRestart() {
-        return number == GameStatus.PLAY.getStatus().charAt(0);
-    }
-
-    public boolean isExit() {
-        return number == GameStatus.EXIT.getStatus().charAt(0);
+    public boolean isPlay() {
+        return this.gameStatus.equals(GameStatus.PLAY);
     }
 }
