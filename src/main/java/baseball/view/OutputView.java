2661f8ce5955e27a2894ba19b579e929408113b9
baksakcci <pshsh910@gmail.com>
2023-10-23 00:29:35 +0900

    refactor($OutputView): List<Integer> 타입 출력 메서드 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,7 +1,13 @@
 package baseball.view;
 
+import java.util.List;
+
 public class OutputView {
     public void displayString(String result) {
         System.out.println(result);
+    }
+
+    public void displayList(List<?> result) {
+        result.stream().forEach(System.out::print);
     }
 }
