64deaf252b6335521867df41c71fe236e2e9c4ac
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-10-22 12:23:30 +0900

    feat: strike와 ball 개수를 반환하는 기능 구현
    strike는 해당 인덱스에 같은 수가 있으면 개수를 더하는 방식으로 구현하였고, ball은 `contains`를 사용해서 strike와 ball에 해당하는 수에 strike 개수를 빼는 방식으로 구현하였음.

--- src/main/java/baseball/service/BaseballGameCompareService.java
+++ src/main/java/baseball/service/BaseballGameCompareService.java
@@ -1,9 +1,35 @@
 package baseball.service;
 
+import baseball.view.OutputView;
+
 import java.util.List;
+import java.util.Objects;
 
 public class BaseballGameCompareService {
     public void compareUserNumberAndRandomNumber(List<Integer> target1, List<Integer> target2) {
+        OutputView outputView = new OutputView();
+        int strikeCount = countStrike(target1, target2);
+        int ballCount = countBall(target1, target2) - strikeCount;
+        outputView.printGameResultBallAndStrike(ballCount, strikeCount);
+    }
 
+    private int countStrike(List<Integer> target1, List<Integer> target2) {
+        int strikeCount = 0;
+        for (int i = 0; i < target1.size(); i++) {
+            if (Objects.equals(target1.get(i), target2.get(i))) {
+                strikeCount++;
+            }
+        }
+        return strikeCount;
+    }
+
+    private int countBall(List<Integer> target1, List<Integer> target2) {
+        int ballCount = 0;
+        for (int i = 0; i < target1.size(); i++) {
+            if (target2.contains(target1.get(i))) {
+                ballCount++;
+            }
+        }
+        return ballCount;
     }
 }
