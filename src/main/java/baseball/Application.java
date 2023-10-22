48faa91294aaa0865ad9719b1d4b947aa8e1eaeb
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-10-22 20:45:52 +0900

    refactor : 컴퓨터가 생성한 숫자를 저장하는 리스트명 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,11 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
@@ -11,23 +13,22 @@ public class Application {
 
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-        List<Integer> answers = new ArrayList<>();
+        List<Integer> randomNumbers = new ArrayList<>();
 
         for (int i = 0; i < 3; i++) {
 
             int number = Randoms.pickNumberInRange(0, 9);
 
-            while (answers.contains(number)) {
+            while (randomNumbers.contains(number)) {
 
                 number = Randoms.pickNumberInRange(0, 9);
 
             }
 
-            answers.add(number);
+            randomNumbers.add(number);
         }
 
-
-
+        
 
 
     }
