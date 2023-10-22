a132ba413d73f4dad7850fb7928288722a07d783
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-10-22 20:09:23 +0900

    feat : 컴퓨터가 숫자를 선택

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,34 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
         System.out.println("숫자 야구 게임을 시작합니다.");
+
+        List<Integer> answers = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+
+            int number = Randoms.pickNumberInRange(0, 9);
+
+            while (answers.contains(number)) {
+
+                number = Randoms.pickNumberInRange(0, 9);
+
+            }
+
+            answers.add(number);
+        }
+
+
+
+
+
     }
 }
