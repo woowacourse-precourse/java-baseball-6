3d07facba210f9a292b3317d4e7702ea26c249ab
Son JunHyung <kingjh1125@gmail.com>
2023-10-22 17:03:18 +0900

    rename: Application.java 위치 변경

---
+++ src/main/java/Application.java
@@ -0,0 +1,21 @@
+/*
+ * <pre>
+ * Class : Application
+ * Comment: Application 시작 클래스
+ * History
+ * ================================================================
+ * DATE             AUTHOR           NOTE
+ * ----------------------------------------------------------------
+ * 2023-10-22       손준형           최초 생성
+ * </pre>
+ *
+ * @author 손준형
+ * @version 1.0.0
+ * @see
+ */
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
