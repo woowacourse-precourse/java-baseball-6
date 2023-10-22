64bc84866941658222910782d26129dd8435448c
Kuen Hee Lee <Leekh10195@gmail.com>
2023-10-22 17:46:07 +0900

    feat: InputReader에 validate 적용

--- src/main/java/baseball/InputReader.java
+++ src/main/java/baseball/InputReader.java
@@ -8,9 +8,18 @@ import java.util.stream.Collectors;
 
 public class InputReader {
 
+    private final InputValidator guessedNumberValidator;
+    private final InputValidator continueModeNumberValidator;
+
+    public InputReader() {
+        this.guessedNumberValidator = new GuessedNumberValidator();
+        this.continueModeNumberValidator = new ContinueModeNumberValidator();
+    }
+
     // 유저가 추측한 값을 입력 받고 List 형태로 반환
     public List<Integer> readUserGuessNumber() {
         String guessedNumberString = Console.readLine();
+        guessedNumberValidator.validate(guessedNumberString);
         List<Integer> guessedNumber = Arrays.stream(guessedNumberString.split(""))
                 .map(Integer::valueOf)
                 .collect(Collectors.toList());
@@ -20,6 +29,7 @@ public class InputReader {
     // 게임 Continue 여부를 입력받고 반환
     public Integer readContinueMode() {
         String continueModeNumberString = Console.readLine();
+        continueModeNumberValidator.validate(continueModeNumberString);
         int continueModeNumber = Integer.parseInt(continueModeNumberString);
         return continueModeNumber;
     }
