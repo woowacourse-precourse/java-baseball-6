85171e2c8cc79f7ee96b25200f6aeec1966f33ef
paulham <pauljungho.ham@gmail.com>
2023-10-22 20:54:45 +0900

    fix: 출력 개행문자 에러 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -125,36 +125,45 @@ class MainGame {
 }
 class View{
     private static final String GAME_START_MESSAGE = "게임을 시작합니다.";
-    private static final String GAME_START_INPUT_MESSAGE = "숫자를 입력해주세요: ";
+    private static final String GAME_INPUT_MESSAGE = "숫자를 입력해주세요: ";
     private static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String GAME_END_INPUT_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요";
-    private static final String GAME_NUMBER = "숫자를 입력해주세요";
     public List<Integer> strToList(String player){
         String[] playerInput = player.split("");
         List<Integer> list = new ArrayList<>();
         for(String s : playerInput){
             list.add(Integer.parseInt(s));
         }
-        System.out.println(list);
         return list;
     }
     public static void printStart(){
         System.out.println(GAME_START_MESSAGE);
-        System.out.println(GAME_START_INPUT_MESSAGE);
+        System.out.print(GAME_INPUT_MESSAGE);
     }
     public static void printResult(){
         System.out.println(GAME_END_MESSAGE);
         System.out.println(GAME_END_INPUT_MESSAGE);
     }
     public static void printNumber(){
-        System.out.println(GAME_NUMBER);
+        System.out.print(GAME_INPUT_MESSAGE);
     }
 }
 class Exception{
     private static final String REGEX  = "[0-9]+";
     public void checkInputValidation(String s){
-        if(s.length() < 3 || !s.matches(REGEX)){
+        if(s.length() > 3 || !s.matches(REGEX) || checkInputDuplication(s)){
             throw new IllegalArgumentException("잘못된 입력입니다.");
         }
+    }
+    public boolean checkInputDuplication(String s){
+        View view = new View();
+        List<Integer> list = view.strToList(s);
+        for(int i = 0; i < list.size(); i++){
+            for(int j = 0; j < list.size(); j++){
+                if(i == j) continue;
+                if(list.get(i).equals(list.get(j))) return true;
+            }
+        }
+        return false;
     }
 }
