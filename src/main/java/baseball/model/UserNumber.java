5509cbc3bb694e9565d4a19b74bb81482d155d8a
oxdjww <oxdjww@gmail.com>
2023-10-23 03:25:36 +0900

    refactor : 예외 메시지 구체화

--- src/main/java/baseball/model/UserNumber.java
+++ src/main/java/baseball/model/UserNumber.java
@@ -19,21 +19,21 @@ public class UserNumber {
         try {
             Integer.parseInt(userInput);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("잘못된 입력입니다.");
+            throw new IllegalArgumentException("잘못된 입력입니다. 숫자가 아닙니다.");
         }
 
         // 3자리 수 검증 확인
         if (userInput.length() != 3) {
-            throw new IllegalArgumentException("잘못된 입력입니다.");
+            throw new IllegalArgumentException("잘못된 입력입니다. 3자리 숫자가 아닙니다.");
         }
 
-        // 중복 자리수 검증
+        // 중복 자리 수 검증
         for (int i = 0; i < userInput.length(); i++) {
             int num = Character.getNumericValue(userInput.charAt(i));
             if (!this.userNumber.contains(num)) {
                 this.userNumber.add(num);
             } else {
-                throw new IllegalArgumentException("잘못된 입력입니다.");
+                throw new IllegalArgumentException("잘못된 입력입니다. 중복된 자리 수가 있습니다.");
             }
         }
     }
