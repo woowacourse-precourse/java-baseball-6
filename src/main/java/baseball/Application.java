ce23e5fef4a8741f47d7edd9ab06b8ff99fa8160
chanwoo yang <moomsss@naver.com>
2023-10-22 22:33:51 +0900

    게임 반복 구현 게임 종료 후 재시작 또는 종료 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,7 +2,7 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
-import jdk.swing.interop.SwingInterOpUtils;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,7 +10,23 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String reGame = "1";
+        String stop = "2";
         System.out.println("숫자 야구 게임을 시작합니다.");
+        while(reGame.equals("1")){
+            baseball();
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String reStart = Console.readLine();
+            if(reStart.equals(stop)){
+                break;
+            }
+        }
+
+
+
+    }
+
+    private static void baseball() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -19,48 +35,57 @@ public class Application {
             }
         }
         System.out.println(computer);
-        System.out.print("숫자를 입력해주세요 : ");
-        String playerInput = Console.readLine();
-        int[] playerNumber = new int[3];
-        for (int i = 0; i < 3; i++){
-            playerNumber[i] = playerInput.charAt(i) - '0';
-        }
         int strike = 0;
         int ball = 0;
-        boolean nothing = true;
-        for(int i =0; i < 3; i++){
-            for(int j = 0; j<3; j++){
-                if(computer.get(i) == playerNumber[j]){
-                    if(i == j){
-                        strike++;
-                        nothing = false;
-                        continue;
-                    }else{
-                        ball++;
-                        nothing= false;
-                        continue;
+        boolean nothing = false;
+        while (strike != 3) {
+
+            System.out.print("숫자를 입력해주세요 : ");
+            String playerInput = Console.readLine();
+            int[] playerNumber = new int[3];
+            for (int i = 0; i < 3; i++) {
+                playerNumber[i] = playerInput.charAt(i) - '0';
+            }
+
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (computer.get(i) == playerNumber[j]) {
+                        if (i == j) {
+                            strike++;
+                            nothing = false;
+
+                        } else {
+                            ball++;
+                            nothing = false;
+
+                        }
+                    } else {
+                        nothing = true;
+
                     }
-                }else{
-                    nothing = true;
-                    continue;
                 }
             }
-        }if(strike > 0&& ball == 0){
-            System.out.println(strike + "스트라이크 ");
-        }else if(strike ==0 && ball > 0) {
-            System.out.println(ball + "볼 ");
-        }else if(strike > 0 && ball > 0){
-            System.out.println(strike + "스트라이크 " + ball + "볼 ");
-        }else if(nothing == true){
-            System.out.println("낫싱");
+            if(strike == 3){
+                break;
+            }else if (strike > 0 && ball == 0) {
+                System.out.println(strike + "스트라이크 ");
+                strike = 0;
+
+            } else if (strike == 0 && ball > 0) {
+                System.out.println(ball + "볼 ");
+                ball = 0;
+            } else if (strike > 0 && ball > 0) {
+                System.out.println(strike + "스트라이크 " + ball + "볼 ");
+                strike = 0;
+                ball = 0;
+
+            } else if (nothing == true) {
+                System.out.println("낫싱");
+                nothing = false;
+            }
         }
-        if(strike == 3){
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        }
-
-
-
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 
 
     }
