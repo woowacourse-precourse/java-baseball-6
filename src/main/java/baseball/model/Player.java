f2cd05549ce165769bd5e10a09befc715a2557eb
snaag <snaag.dev@gmail.com>
2023-10-23 02:42:03 +0900

    feat: (Player) Player class 구현

---
+++ src/main/java/baseball/model/Player.java
@@ -0,0 +1,73 @@
+package baseball.model;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static baseball.utils.Constants.*;
+
+public class Player {
+    private String answer;
+    private int[] answerSequence;
+    private int[] digitPositions;
+
+    public void setAnswer(String answer) {
+        this.checkAnswerValid(answer);
+
+        this.answer = answer;
+        this.answerSequence = new int[BALL_MAX];
+        this.digitPositions = new int[RANGE_END];
+        this.setAnswerSequence();
+        this.setDigitPositions();
+    }
+
+
+    private void setAnswerSequence() {
+        // [3,9,1]
+        this.answerSequence = this.answer.chars()
+                .map(Character::getNumericValue)
+                .toArray();
+    }
+
+    private void setDigitPositions() {
+        // [-1, -1, -1, -1, -1, -1, -1, -1, -1, 1]
+        Arrays.fill(digitPositions, INIT_VALUE);
+
+        // [-1, 2, -1, 0, -1, -1, -1, -1, -1, 1]
+        for(int i=0; i<BALL_MAX; i++) {
+            int position = answerSequence[i];
+            digitPositions[position] = i;
+        }
+    }
+
+
+    private void checkAnswerValid(String answer) {
+        // 글자수, 문자 포함 여부 체크
+        Pattern pattern = Pattern.compile(NUMBER_REGEX);
+        Matcher matcher = pattern.matcher(answer);
+        boolean matches = matcher.matches();
+        if(!matches) {
+            throw new IllegalArgumentException();
+        }
+
+        // 같은 수 입력 여부 체크
+        boolean[] exist = new boolean[RANGE_END];
+        Arrays.fill(exist, false);
+        int[] answerSequence = answer.chars()
+                        .map(Character::getNumericValue)
+                        .toArray();
+
+        for(int i=0; i<BALL_MAX; i++) {
+            int v = answerSequence[i];
+            if(exist[v]) {
+                throw new IllegalArgumentException();
+            }
+
+            exist[v] = true;
+        }
+    }
+
+    public int[] getAnswerSequence() {
+        return answerSequence;
+    }
+}
