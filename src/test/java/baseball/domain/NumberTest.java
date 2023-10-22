94f20e2803e1b5f2dcfb8889cf97e3f5c0b708cf
ma-an-jong <alswhd1113@gmail.com>
2023-10-23 02:07:38 +0900

    test(NumberTest) : compareTo, equals 메소드 테스트
    compareTo, equals 메소드의 기능을 테스트하였다.

--- src/test/java/baseball/domain/NumberTest.java
+++ src/test/java/baseball/domain/NumberTest.java
@@ -23,4 +23,20 @@ class NumberTest {
 
     }
 
+    @Test
+    public void compareTo() throws Exception {
+        Number a = new Number(new int[]{1,2,3});
+        Number b= new Number(new int[]{2,3,4});
+        Assertions.assertTrue(a.compareTo(b) < 0);
+        Assertions.assertTrue(b.compareTo(a) > 0);
+    }
+
+    @Test
+    public void equals() throws Exception {
+        int[] numbers = {4, 5, 7};
+        Number number = new Number(numbers); Number o = new Number(numbers);
+
+        Assertions.assertEquals(number, o);
+    }
+
 }
