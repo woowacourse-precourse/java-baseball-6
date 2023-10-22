7632c9393ba073c9af01437361ac1ff364cbdc1a
paulham <pauljungho.ham@gmail.com>
2023-10-22 16:14:34 +0900

    feat: strike, ball 판별하는 computer 클래스 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,14 +10,14 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Number makeNumbers = new Number();
+        List<Integer> computer = makeNumbers.makeRandomNumbers();
 
-        int a = 425;
-        List<Integer> as = new ArrayList<>();
     }
 }
 /**
- * Number 클래스 처음 숫자를 만들어 냄
- * Computer 클래스 입력받은 숫자로 strike, ball 판단
+ * Number 클래스 처음 숫자를 만들어 냄 (완료)
+ * Computer 클래스 입력받은 숫자로 strike, ball 판단 (완료)
  * Game 클래스 stirke, ball 가지고 게임의 완료 여부
  * Input 클래스 게임의 진행을 위해 입출력을 담당
  * Exception 클래스 예외를 담당하는 클래스
@@ -35,6 +35,30 @@ class Number{
             }
         }
         return randomNumbers;
+    }
+}
+class Computer{
+    private int strike = 0;
+    private int ball = 0;
+    public int[] count(List<Integer> computer, List<Integer> player){
+        Number randNums = new Number();
+
+        for(int i=0; i<player.size(); i++){
+            int target = player.get(i);
+            for(int j=0; j<computer.size(); j++){
+                int checking = computer.get(j);
+                if(i == j && target == checking){
+                    // 숫자도 맞고 자릿수도 맞고
+                    strike++;
+                } else if (i != j && target == checking) {
+                    // 숫자는 맞는데 자릿수가 안맞아
+                    ball++;
+                }
+            }
+        }
+        int[] sb = {strike, ball};
+        System.out.println(Arrays.toString(sb));
+        return sb;
     }
 }
 
