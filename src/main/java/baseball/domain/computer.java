9713f6b6853cd0fbc660927396f3778a39b3760d
SunYerim <syl1602@naver.com>
2023-10-22 19:53:22 +0900

    [feature] computer 숫자생성

--- src/main/java/baseball/domain/computer.java
+++ src/main/java/baseball/domain/computer.java
@@ -1,4 +1,22 @@
 package baseball.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class computer {
+    // 컴퓨터가 임의의 랜덤한 숫자 3개를 생성해낸다.
+    public List<Integer> createNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
+
+
 }
