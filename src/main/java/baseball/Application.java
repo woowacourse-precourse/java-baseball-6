200a150586337d86ccfd19464a93203f46fd3e41
Young1 <dlduddnjs198@naver.com>
2023-10-22 23:04:30 +0900

    1부터 9까지 숫자 중 3개의 중복되지 않는 숫자 생성 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,34 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try{
+            int[] answerNum = new int[3];
+
+            generateAnswer(answerNum);
+
+            System.out.println("숫자 야구 게임을 시작합니다.");
+        }catch(IllegalArgumentException e){
+            // 그냥 프로그램 종료
+        }finally{
+            Console.close();
+        }
+
+    }
+
+    private static void generateAnswer(int[] answerNum) {
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        while (uniqueNumbers.size() < 3) {
+            uniqueNumbers.add(Randoms.pickNumberInRange(1, 9));
+        }
+        int idx = 0;
+        for (Integer num : uniqueNumbers) {
+            answerNum[idx++] = num;
+        }
     }
 }
