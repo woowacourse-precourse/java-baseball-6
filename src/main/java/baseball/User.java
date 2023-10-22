93f836e16f85cbe43c3f769808ce4a0d55fc03fa
GO-TE <alsrudxo9810@naver.com>
2023-10-22 22:09:37 +0900

    Style: 유저 입력 중복 시 메세지 출력 추가

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -39,7 +39,7 @@ public class User {
         Set<Character> uniqueInput = new HashSet<>();
         for(char c : input.toCharArray()) {
             if (!uniqueInput.add(c)){
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException(INPUT_ERROR_MESSAGE);
             }
         }
     }
