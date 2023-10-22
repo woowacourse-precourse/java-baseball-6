37d86ca4001f70ae0d457e0aa33c769b410c6b73
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-10-23 02:16:33 +0900

    refactor : startGame 메서드 쪼개기
    - displayStartMessage

--- src/main/java/baseball/StartGame.java
+++ src/main/java/baseball/StartGame.java
@@ -6,21 +6,25 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class StartGame {
-    public static void startGame(){
-        System.out.println("숫자 야구 게임을 시작합니다.");
-    }
 
-    public static String createThreeNums(){
-        Set<Integer> computer = new HashSet<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            computer.add(randomNumber);
-        }
-        StringBuilder sb = new StringBuilder();
-        for (int num : computer){
-            sb.append(num);
-        }
+  public static void startGame() {
+    displayStartMessage();
+  }
 
-        return sb.toString();
+  private static void displayStartMessage() {
+    System.out.println("숫자 야구 게임을 시작합니다.");
+  }
+
+  public static String createThreeNums() {
+    Set<Integer> computer = new HashSet<>();
+    while (computer.size() < 3) {
+      int randomNumber = Randoms.pickNumberInRange(1, 9);
+      computer.add(randomNumber);
     }
+    StringBuilder sb = new StringBuilder();
+    for (int num : computer) {
+      sb.append(num);
+    }
+    return sb.toString();
+  }
 }
