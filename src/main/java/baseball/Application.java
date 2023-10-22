1baf411c96aa8a51eb20bab2a2cf033c8f845a90
KyungsooLee <jmtkd9196@gmail.com>
2023-10-23 03:57:39 +0900

    feat: 랜덤넘버 생성함수 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    // 컴퓨터가 랜덤 넘버를 생성
+    public static List<Integer> createRandomNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
     public static void main(String[] args) {
+
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        List<Integer> computer = createRandomNumber();
     }
 }
