db53752ee264f2eea64b84562e59c029aed7f43f
hongyoung <qwe916@naver.com>
2023-10-23 03:12:29 +0900

    feat: 랜덤한 서로 다른 3자리 숫자를 생성하는 RandomNumberGenerator 구현

---
+++ src/main/java/baseball/generator/RandomNumberGenerator.java
@@ -0,0 +1,36 @@
+package baseball.generator;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomNumberGenerator {
+    private static final int REQUIRED_NUMBERS_SIZE = 3;
+    private final List<Integer> numbers = new ArrayList<>();
+
+    public RandomNumberGenerator() {
+        while (numbers.size() < REQUIRED_NUMBERS_SIZE) {
+            addUniqueRandomNumber();
+        }
+    }
+
+    public int generateRandomNumber() {
+        return Randoms.pickNumberInRange(1, 9);
+    }
+
+    public boolean isNumberExisted(int number){
+        return numbers.contains(number);
+    }
+
+    public List<Integer> getNumbers() {
+        return new ArrayList<>(numbers);
+    }
+
+    private void addUniqueRandomNumber() {
+        int randomNumber = generateRandomNumber();
+        if (!isNumberExisted(randomNumber)) {
+            numbers.add(randomNumber);
+        }
+    }
+}
