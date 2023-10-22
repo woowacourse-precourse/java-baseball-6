103464e34e1452538c7668bf77866d98bdc7ea03
bbbang105 <2018111366@dgu.ac.kr>
2023-10-23 01:42:12 +0900

    Style : createRandomNumber() 메소드 내 변수명 수정 가독성을 위해 변수명 nums를 computerNums로 변경

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -5,14 +5,14 @@ import java.util.List;
 
 public class Baseball {
 
-    public static List<Integer> createRandomNumber() {
-        List<Integer> nums = new ArrayList<>();
-        while (nums.size() < 3) {
+    public List<Integer> createRandomNumber() {
+        List<Integer> computerNums = new ArrayList<>();
+        while (computerNums.size() < 3) {
             int num = camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(1,9);
-            if (!nums.contains(num)) {
-                nums.add(num);
+            if (!computerNums.contains(num)) {
+                computerNums.add(num);
             }
         }
-        return nums;
+        return computerNums;
     }
 }
