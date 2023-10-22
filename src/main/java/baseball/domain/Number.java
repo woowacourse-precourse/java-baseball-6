0ac65675512220be067268c23b1d1c1f139b38f8
ma-an-jong <alswhd1113@gmail.com>
2023-10-23 02:03:51 +0900

    feat(Number) : Comparable 인터페이스 상속, compareTo, equals 메소드 재정의
    숫자 야구 요구사항에서 비교하는 기능이 자주 필요하다.
    Number 객체를 비교하기 위해서 Comparable을 상속하고 compareTo 메소드를 재정의하였다.
    비교 방법은 단순 숫자의 대소비교를 하였다.
    equals 메소드는 null과 Number 타입인지 확인하고 각 숫자가 모두 동일한지 확인하였다.

--- src/main/java/baseball/domain/Number.java
+++ src/main/java/baseball/domain/Number.java
@@ -8,7 +8,7 @@ import baseball.utils.message.ErrorMessage;
  * 또한 일치 여부 확인을 위한 equals() 메소드를 오버라이딩한다.
  * 힌드 출력을 위한 로직은 Number 도메인 객체에서 진행해야 할까?
  */
-public class Number {
+public class Number implements Comparable<Number> {
 
     private static final Integer MAX_NUMBER_SIZE = 3;
 
@@ -42,6 +42,41 @@ public class Number {
         }
 
         return true;
+    }
+
+    /**
+     *
+     * 두 숫자를 비교하기 위해 compareTo 메소드를 오버라이드한다.
+     * 중간에 숫자가 서로 다를경우 대소비교를 위해서 자바에서 사용하는 방식과 동일하게 두 수의 차이를 반환한다.
+     * (음수면 앞에것이 더 작다는 의미이며 , 양수라면 앞에것이 더 크다는 의미이다)
+     * 만약 3자리수 모두 같은 경우 0을 리턴한다. (같은 경우)
+     */
+    @Override
+    public int compareTo(Number o) {
+
+        for (int indexOfNumbers = 0; indexOfNumbers < MAX_NUMBER_SIZE; indexOfNumbers++) {
+            if(!numbers[indexOfNumbers].equals(o.numbers[indexOfNumbers])) {
+                return numbers[indexOfNumbers] - o.numbers[indexOfNumbers];
+            }
+        }
+
+        return 0;
+    }
+
+    /**
+     * this의 주소값이 o와 같다면 같은 객체를 가리키고 있기 때문에 true이다.
+     * 만약 o가 null일 경우 비교 불가능하기 때문에 false이다.
+     * Number 클래스가 아니라면 false이다.
+     * Number 클래스의 두 숫자를 비교하여 0이 나오면 true, 그렇지 않으면 false이다.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || o.getClass() != Number.class) return false;
+        Number number = (Number) o;
+        int resultOfCompare = number.compareTo(this);
+
+        return resultOfCompare == 0;
     }
 
 }
