dbd0a501faee639d9ee2309fc7239b2572d1e0e1
CJY <c0512jy@naver.com>
2023-10-22 23:56:30 +0900

    feat: 콘솔 출력 역할
    콘솔 출력 역할을 하는 클래스

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,11 @@
+package baseball.view;
+
+public class OutputView {
+    public void printResult(String result) {
+        System.out.println(result);
+    }
+
+    public void printSetEnd() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+}
