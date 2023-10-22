2052e76e673403a529be7dcad804287b6b5748a0
soyoungxxx <otherplet@gmail.com>
2023-10-23 03:38:48 +0900

    feat: PlayerNumber 입력 코드 작성

--- src/main/java/baseball/variable/PlayerNumber.java
+++ src/main/java/baseball/variable/PlayerNumber.java
@@ -1,4 +1,21 @@
 package baseball.variable;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayerNumber {
+    public List<Integer> playerNumber = new ArrayList<>();
+
+    public void getPlayerNumber() {
+        String tempString = Console.readLine();
+        String[] str = splitString(tempString);
+        for (int i = 0; i < str.size(); i++) {
+            playerNumber.add(parseInt(str[i]));
+        }
+    }
+
+    private String[] splitString(String str) {
+        return str.split("");
+    }
 }
