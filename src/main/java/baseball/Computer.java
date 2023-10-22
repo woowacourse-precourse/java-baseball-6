e7b1f2679ed178b7413e3f23cc955ae80307aae1
Woongbae Yun <devbelly@naver.com>
2023-10-22 16:34:00 +0900

    refactor : Computer, Player에서 사용하는 컬렉션을 일급 컬렉션으로 적용

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,25 +1,12 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
 
-    public static final int MAX_DIGIT = 3;
-    private List<Integer> computer;
-
-    public Computer() {
-        this.computer = new ArrayList<>();
-        while (computer.size() < MAX_DIGIT) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
-        }
-    }
+    Numbers numbers = new Numbers();
 
     public List<Integer> getComputerNumbers() {
-        return List.copyOf(computer);
+        return List.copyOf(numbers.getList());
     }
 }
--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,31 +1,16 @@
 package baseball;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
 
-    private List<Integer> list = new ArrayList<>();
+    private Numbers numbers;
 
     public List<Integer> getPlayerNumbers() {
-        return List.copyOf(list);
+        return List.copyOf(numbers.getList());
     }
 
     public void guessNumber(String input) {
-        validateInput(input);
-        for (int i = 0; i < input.length(); i++) {
-            list.add(Character.getNumericValue(input.charAt(i)));
-        }
-    }
-
-    private void validateInput(String input) {
-        if (!input.matches("[1-9]{3}")) {
-            throw new IllegalArgumentException("1~9 사이의 숫자를 입력해주세요.");
-        }
-
-        if (input.charAt(0) == input.charAt(1) || input.charAt(1) == input.charAt(2)
-            || input.charAt(0) == input.charAt(2)) {
-            throw new IllegalArgumentException("중복되지 않은 숫자를 입력해주세요.");
-        }
+        numbers = new Numbers(input);
     }
 }
