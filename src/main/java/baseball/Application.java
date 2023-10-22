b51901ffbf851b6173145269d6dc38c3792f91a3
unknown <kimji1026@gmail.com>
2023-10-22 15:03:12 +0900

    feat : Application 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,5 +2,12 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-    }
+        BaseballGameImpl baseball = new BaseballGameImpl();
+        while(true) {
+            baseball.startGame();
+            if(baseball.endGame()) {
+                break;
+            }
+        }
+    }    
 }
