ab88ee2b79f038bba49aa5f827df65a956b9d56c
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-10-22 19:48:23 +0900

    feat: add game start method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,5 +2,6 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
+        BaseballGame.gameStart();
     }
 }
--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,8 +1,34 @@
 package baseball;
 
+import java.util.List;
+
 public class BaseballGame {
+    private static final BaseballReferee baseballReferee = new BaseballReferee();
+    private static final InputReader inputReader = new InputReader();
+    private static final NumberGenerator numberGenerator = new NumberGenerator();
+    private static final OutputWriter outputWriter = new OutputWriter();
 
     public static void gameStart(){
+        outputWriter.printGameStartMessage();
+
+        while(true) {
+            List<Integer> goal = numberGenerator.generateNumber();
+
+            while (true) {
+                outputWriter.printInputMessage();
+                List<Integer> expect = inputReader.readUserGameInput();
+
+                int[] result = baseballReferee.scoreInput(goal, expect);
+                outputWriter.printResultOfInput(result[0], result[1]);
+
+                if (result[1] == 3) break;
+            }
+
+            outputWriter.printGameFinishMessage();
+            int restart = inputReader.readUserGameFinishInput();
+
+            if(restart == 2) break;
+        }
 
     }
 }
