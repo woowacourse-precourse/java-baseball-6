98418d706e6b156a1501c8e8ba89713db463089d
cyberjam <jamsince1994@naver.com>
2023-10-22 17:46:07 +0900

    feat: add function getting inputNumbers from user

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,14 +1,28 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
         List<Integer> randomNumbers = getRandomNumbers();
         System.out.println(randomNumbers);
+
+
+        System.out.print("숫자를 입력해주세요 : ");
+
+        StringTokenizer st = new StringTokenizer(readLine(), " ");
+        List<Integer> inputNumbers = new ArrayList<>();
+        while (st.hasMoreTokens()) {
+            inputNumbers.add(Integer.parseInt(st.nextToken()));
+        }
+        System.out.println(inputNumbers);
     }
     private static List<Integer> getRandomNumbers() {
         // get random numbers from 1 to 9
