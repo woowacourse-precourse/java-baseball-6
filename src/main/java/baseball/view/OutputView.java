d9b30653af3156467dc3482b9cac8766b67091b7
ykmxxi <ykmxxi97@gmail.com>
2023-10-22 15:40:02 +0900

    fix(OutputView): 가이드 메시지 오타 수정

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,7 +4,7 @@ public class OutputView {
 
 	private static final String START_GUIDE = "숫자 야구 게임을 시작합니다.";
 	private static final String NUMBERS_INPUT_GUIDE = "숫자를 입력해주세요 : ";
-	private static final String GAME_END_GUIDE = "33개의 숫자를 모두 맞히셨습니다! 게임 종료";
+	private static final String GAME_END_GUIDE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 	private static final String REPLAY_GUIDE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
 	public static void printStartGuide() {
