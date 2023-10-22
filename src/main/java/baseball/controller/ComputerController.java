2c0fc8cda859793dfb022e1899853130d48be88e
yunju-m <dbswnfnal@naver.com>
2023-10-22 15:12:29 +0900

    feat(ComputerController): 추가 게임시작 문구 출력 및 플레이어 숫자 입력 받기 기능

--- src/main/java/baseball/controller/ComputerController.java
+++ src/main/java/baseball/controller/ComputerController.java
@@ -1,11 +1,17 @@
 package baseball.controller;
 
 import baseball.model.GameNumber;
+import baseball.view.InputView;
+import baseball.view.OutputView;
 
 public class ComputerController {
     GameNumber gameNumber = new GameNumber();
+    OutputView outputView = new OutputView();
+    InputView inputView = new InputView();
 
     public void startGame() {
+        outputView.printStartGame();
         gameNumber.setRandomNumbers();
+        inputView.enterGameNumber();
     }
 }
