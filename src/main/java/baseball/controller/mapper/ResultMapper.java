33fb23494a8d98509d040664e1aeda184b46e0d1
kimtaesoo <kimtaesoo7@naver.com>
2023-10-22 12:57:33 +0900

    refactor: 공통 메서드로 분리하여 사용하도록 수정

--- src/main/java/baseball/controller/mapper/ResultMapper.java
+++ src/main/java/baseball/controller/mapper/ResultMapper.java
@@ -2,7 +2,7 @@ package baseball.controller.mapper;
 
 public class ResultMapper {
 
-    private static final int DEFAULT_COUNT = 0;
+    private static final int ZERO = 0;
     private static final String NOTHING = "낫싱";
     private static final String BALL = "볼 ";
     private static final String STRIKE = "스트라이크";
@@ -15,34 +15,25 @@ public class ResultMapper {
         return buildStatus(ballCount, strikeCount);
     }
 
+    private static boolean isOut(final int ballCount, final int strikeCount) {
+        return ballCount == ZERO && strikeCount == ZERO;
+    }
+
     private static String buildStatus(final int ballCount, final int strikeCount) {
         StringBuilder status = new StringBuilder();
-        appendBallCount(status, ballCount);
-        appendStrikeCount(status, strikeCount);
+        appendIfNonZero(status, ballCount, BALL);
+        appendIfNonZero(status, strikeCount, STRIKE);
+
         return status.toString();
     }
 
-    private static void appendBallCount(final StringBuilder status, final int ballCount) {
-        if (hasBallCount(ballCount)) {
-            status.append(ballCount).append(BALL);
+    private static void appendIfNonZero(final StringBuilder status, final int count, final String symbol) {
+        if (hasCount(count)) {
+            status.append(count).append(symbol);
         }
     }
 
-    private static void appendStrikeCount(final StringBuilder status, final int strikeCount) {
-        if (hasStrikeCount(strikeCount)) {
-            status.append(strikeCount).append(STRIKE);
-        }
-    }
-
-    private static boolean hasBallCount(final int ballCount) {
-        return ballCount != DEFAULT_COUNT;
-    }
-
-    private static boolean hasStrikeCount(final int strikeCount) {
-        return strikeCount != DEFAULT_COUNT;
-    }
-
-    private static boolean isOut(final int ballCount, final int strikeCount) {
-        return ballCount == DEFAULT_COUNT && strikeCount == DEFAULT_COUNT;
+    private static boolean hasCount(final int count) {
+        return count != ZERO;
     }
 }
