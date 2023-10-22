322d72014651cd05910fd78d84efc6e37351bbf7
mayy24th <86235986+mayy24th@users.noreply.github.com>
2023-10-22 18:01:22 +0900

    feat: 다시 시작 입력 기능 구현

---
+++ src/main/java/baseball/input/RestartInput.java
@@ -0,0 +1,18 @@
+package baseball.input;
+
+import baseball.Error.CheckRestartNumber;
+import baseball.output.Message;
+import camp.nextstep.edu.missionutils.Console;
+
+public class RestartInput {
+
+    public static boolean inputRestartNumber(){
+        Message.printRestartMessage();
+        String input;
+        input = Console.readLine();
+        CheckRestartNumber.checkLength(input);
+        CheckRestartNumber.checkNumber(input);
+
+        return input.charAt(0) == '1';
+    }
+}
