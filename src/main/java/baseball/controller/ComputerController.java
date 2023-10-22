61025604113ac4894375674f520a9bc26a1fd5e6
isyoo1213 <moethns@gmail.com>
2023-10-22 19:24:17 +0900

    feat: 힌트변수(스트라이크/볼) 0으로 초기화 기능추가 ComputerController - initStrikeAndBallCount() 추가 README.md - 기능 명세 구현 체크

--- src/main/java/baseball/controller/ComputerController.java
+++ src/main/java/baseball/controller/ComputerController.java
@@ -7,10 +7,19 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class ComputerController {
+    private static final int INIT_ZERO = 0;
+    private int strikeCount;
+    private int ballCount;
+
     RandomUtility randomUtility = new RandomUtility();
 
     public void startGame(GameNumber gameNumber) {
         List<Integer> computerGenerateNumbers = Arrays.stream(randomUtility.generateAnswerNumbers()).boxed().collect(Collectors.toList());
         gameNumber.setComputerGenerateNumbers(computerGenerateNumbers);
+    }
+
+    public void initStrikeAndBallCount() {
+        strikeCount = INIT_ZERO;
+        ballCount = INIT_ZERO;
     }
 }
