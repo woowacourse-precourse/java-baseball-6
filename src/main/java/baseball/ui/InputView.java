90c6030f6473ac0578071bb171afa8b0bd11cc35
NAM GIBEOM <rlqja0523@naver.com>
2023-10-22 14:36:04 +0900

    feat(InputView): 게임 종료 시 1, 2 입력 기능 추가

--- src/main/java/baseball/ui/InputView.java
+++ src/main/java/baseball/ui/InputView.java
@@ -13,4 +13,9 @@ public class InputView {
         System.out.println("숫자를 입력해주세요 : ");
         return scanner.nextLine();
     }
+
+    public String inputContinueGame() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return scanner.nextLine();
+    }
 }
