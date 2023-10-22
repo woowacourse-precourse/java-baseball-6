6cf8dc25b818979bbf520e7b674badc10fc21d0e
HyungUk Ryu <hw0603@naver.com>
2023-10-22 16:18:52 +0900

    Refactor: 사용자 입력 검증 예외 시 이유를 포함하도록 변경

--- src/main/java/baseball/utils/ValidationUtil.java
+++ src/main/java/baseball/utils/ValidationUtil.java
@@ -3,7 +3,10 @@ package baseball.utils;
 import java.util.HashSet;
 
 public class ValidationUtil {
-    HashSet<Character> charSet = new HashSet<>();
+    private final HashSet<Character> charSet = new HashSet<>();
+    private static final String LENGTH_MISMATCH = "세 자리로 입력해 주세요.";
+    private static final String RANGE_MISMATCH = "1이상 9 이하 숫자로 입력해 주세요.";
+    private static final String DUPLICATE_EXIST = "중복되는 숫자가 존재합니다.";
 
     public void validate(String userInput) throws IllegalArgumentException {
         charSet.clear();
@@ -17,19 +20,19 @@ public class ValidationUtil {
 
     private void checkLength(String userInput) {
         if (userInput.length() != 3) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(LENGTH_MISMATCH);
         }
     }
 
     private void checkRange(char ch) {
         if (ch < '1' || ch > '9') {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(RANGE_MISMATCH);
         }
     }
 
     private void checkDuplicate(char ch) {
         if (charSet.contains(ch)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(DUPLICATE_EXIST);
         }
         charSet.add(ch);
     }
