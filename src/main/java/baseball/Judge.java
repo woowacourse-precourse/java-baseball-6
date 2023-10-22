c198da208e89bed0cfb479ea45db01a41737d627
eunseob <seobseob0810@naver.com>
2023-10-22 23:09:52 +0900

    'fix: 출력 문구 추가

--- src/main/java/baseball/Judge.java
+++ src/main/java/baseball/Judge.java
@@ -30,6 +30,7 @@ public class Judge {
         } else if (strike == 0) {
             System.out.print(ball + "볼");
         } else if (strike == 3) {
+            System.out.print("3스트라이크");
             System.out.println("3개의 숫자를 다 맞히셨습니다! 게임 종료");
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
