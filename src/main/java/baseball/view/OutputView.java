f6092da43cd5a7eaff1997ef7383d4488a6741ae
김명준 <86913355+mjj111@users.noreply.github.com>
2023-10-23 06:01:39 +0900

    refactor(OutputView): 게임 결과를 보여주는 notifyGameResult 메서드로 변경
    게임 결과를 받으면 게임결과를 출력하고,
    게임 결과가 "3스트라이크"일 경우 게임의 종료를 추가로 보여줍니다.

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,8 +3,16 @@ package baseball.view;
 public class OutputView {
     private final static String ALL_CLEAR_GAME_OVER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
-    public void notifyEnd(String result) {
+    public void notifyStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void notifyGameResult(String result) {
+        if (result == "3스트라이크") {
+            System.out.println(result);
+            System.out.println(ALL_CLEAR_GAME_OVER);
+            return;
+        }
         System.out.println(result);
-        System.out.println(ALL_CLEAR_GAME_OVER);
     }
 }
