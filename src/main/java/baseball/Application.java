c9ab6d4c8ddb3e958e3929e40505df9c3f6a006e
ohhamma <judyham10634@naver.com>
2023-10-22 22:49:07 +0900

    :art: refactor: startPhrase 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,8 +2,7 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        String startPhrase = "숫자 야구 게임을 시작합니다.";
-        System.out.println(startPhrase);
+        System.out.println("숫자 야구 게임을 시작합니다.");
 
         boolean isFinished = false;
 
