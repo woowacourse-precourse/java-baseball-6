4ece5f74367ce67b17e527e464a08cc970f276fd
haero77 <shineecard5@gmail.com>
2023-10-22 13:54:32 +0900

    refactor: return 을 이용하여 프로그램을 종료하도록 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,6 +7,8 @@ public class Application {
     public static void main(String[] args) {
         GameManager gameManager = GameManager.create();
         gameManager.startGame();
+
+        return;
     }
 
 }
