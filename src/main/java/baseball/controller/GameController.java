b4cffeb613abbc815860ce182a6575f6636df103
Hoony0321 <eogns0321@gmail.com>
2023-10-22 22:31:14 +0900

    feat : GameController에 GameService 연결 및 run() 메서드 구현 완료

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,34 +1,31 @@
 package baseball.controller;
 
-import java.util.List;
+import baseball.service.GameService;
 
 /* 게임을 컨트롤하는 객체 */
 public class GameController {
-    private static List<Integer> programNumbers; // 컴퓨터가 생성한 숫자
-    private static boolean gameState; // 게임 상태
+    public GameService gameService = new GameService();
 
-    public void run() {
-        // TODO : 게임을 컨트롤하는 메소드 구현
-        while(gameState){
+    public void run() throws IllegalArgumentException {
             initGame();
             playGame();
-        }
-        endGame();
+            endGame();
     }
 
     /* 게임을 초기화하는 메소드 */
     public void initGame() {
-        // TODO : 게임을 초기화하는 메소드 구현
+        gameService.initGame();
     }
 
     /* 게임을 진행하는 메소드 */
-    public void playGame() {
-        // TODO : 게임을 진행하는 메소드 구현
+    public void playGame() throws IllegalArgumentException{
+        gameService.playGame();
     }
 
-    /* 게임을 종료하는 메소드 */
-    public boolean endGame() {
-        // TODO : 게임을 종료하는 메소드 구현
-        return true;
+    public void endGame() {
+        boolean isEnd = gameService.endGame();
+        if(!isEnd) {
+            run();
+        }
     }
 }
