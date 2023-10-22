f0337beba35714d3fa3b46db55aeaf9dd3f9c1dd
상원 최 <swchoi1@swm.ai>
2023-10-22 23:39:29 +0900

    refactor: "MessageType 이름 변경"
    #6-baseball-score

--- src/main/java/baseball/game/GameMessageType.java
+++ src/main/java/baseball/game/GameMessageType.java
@@ -1,6 +1,6 @@
-package baseball.modules.console;
+package baseball.game;
 
-public enum MessageType{
+public enum GameMessageType {
     START("숫자 야구 게임을 시작합니다."),
     INPUT_REQUEST("숫자를 입력해주세요 : "),
     SUCCESS("3개의 숫자를 모두 맞히셨습니다! "),
@@ -10,7 +10,7 @@ public enum MessageType{
 
     private final String message;
 
-    MessageType(final String message) {
+    GameMessageType(final String message) {
         this.message = message;
     }
 
