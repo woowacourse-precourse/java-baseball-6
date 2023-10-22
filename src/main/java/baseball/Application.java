a4ad54310066e705fc348f7f29abdb9fe907ef8f
Yang-soeun <soeun8636@naver.com>
2023-10-22 18:00:13 +0900

    중첩 와일문 재귀형태로 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,46 +9,51 @@ import java.util.List;
 public class Application {
   public static void main(String[] args) {
     // TODO: 프로그램 구현
-    int restart = 1;
 
     System.out.println("숫자 야구 게임을 시작합니다.");
+    playGame();
 
-    while (restart == 1) {
-      List<Integer> computer = createAnswer();//서로 다른 3자리 수 생성
-      int ball = 0, strike = 0, pos = 0;
+  }
 
-      while (strike != 3) {
-        ball = 0;
-        strike = 0;
-        pos = 0;
+  private static void playGame() {
+    List<Integer> computer = createAnswer();//서로 다른 3자리 수 생성
+    int ball=0, strike=0, pos=0;
+    int restart = 0;
 
-        System.out.printf("숫자를 입력해주세요 : ");
-        String num = readLine();
+    while (strike != 3) {
+      ball = 0;
+      strike = 0;
+      pos = 0;
 
-        verifiedInput(num);
+      System.out.printf("숫자를 입력해주세요 : ");
+      String num = readLine();
 
-        for (int i = 0; i < 3; i++) {
-          int m = num.charAt(i) - '0';
-          int index = computer.indexOf(m);
-          if (index != -1) {
-            if (index == pos) {
-              strike++;
-            } else {
-              ball++;
-            }
+      verifiedInput(num);
+
+      for (int i = 0; i < 3; i++) {
+        int m = Character.getNumericValue(num.charAt(i));
+        int index = computer.indexOf(m);
+        if (index != -1) {
+          if (index == pos) {
+            strike++;
+          } else {
+            ball++;
           }
-          pos++;
         }
-
-        getHint(ball, strike);//입력한 수에 대한 결과 출력
-
-        if (strike == 3) {//3개의 숫자를 모두 맞힐경우
-          System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
-          System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-          restart = Integer.parseInt(readLine());
-        }
+        pos++;
       }
-      if (restart == 2) {
+
+      getHint(ball, strike);//입력한 수에 대한 결과 출력
+
+      if (strike == 3) {//3개의 숫자를 모두 맞힐경우
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        restart = Integer.parseInt(readLine());
+      }
+
+      if (restart == 1) {
+        playGame();
+      } else if (restart == 2) {
         System.out.println("게임 종료");
       }
     }
