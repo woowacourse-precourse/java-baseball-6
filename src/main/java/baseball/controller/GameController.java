1f56f6ab7db131e15ea783f7a6d670d1926c2547
msung99 <msung6924@naver.com>
2023-10-22 14:22:17 +0900

    feat: 스트라이크, 볼 개수 계산하기

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -29,7 +29,8 @@ public class GameController {
         do {
             GameView.printRequireNumberMessage();
             BaseBalls userNumbers = BaseBalls.getUserBaseBallNumbers(Console.readLine());
-            // TODO : 게임 진행(스트라이크, 볼 계산. 즉, 정답과 사용자 입력을 비교하는 로직), 결과 출력 등
+            int strikeCount = answers.getStrikeCounts(userNumbers);
+            int ballCount = answers.getBallCounts(userNumbers);
         } while(true);
     }
 }
--- src/main/java/baseball/domain/BaseBalls.java
+++ src/main/java/baseball/domain/BaseBalls.java
@@ -1,6 +1,7 @@
 package baseball.domain;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class BaseBalls {
     private List<BaseBall> numbers;
@@ -30,5 +31,22 @@ public class BaseBalls {
         if(numbers.stream().distinct().count() != SIZE){
             throw new IllegalArgumentException("중복되는 숫자없이, 서로 다른 3개의 숫자를 입력해야합니다.");
         }
+    }
+
+    // 스트라이크 : 인덱스 값, 실제 값 모두 똑같아야함
+    public int getStrikeCounts(BaseBalls otherBaseBalls){
+        return (int) IntStream.range(0, SIZE)
+                .filter(index -> numbers.get(index).equals(otherBaseBalls.numbers.get(index)))
+                .count();
+    }
+
+    // 볼 : 실제 값은 같으나, 인덱스 값이 다르다.
+    // 1) answer 가 포함하는 것을 필터링
+    // 2) 스트라이크인 것을 필터링 (인덱스 값, 실제 값 모두 똑같은 것)
+    public int getBallCounts(BaseBalls otherBaseBalls){
+        return (int) IntStream.range(0, SIZE)
+                .filter(index -> numbers.contains(otherBaseBalls.numbers.get(index)))
+                .filter(index -> !numbers.get(index).equals(otherBaseBalls.numbers.get(index)))
+                .count();
     }
 }
