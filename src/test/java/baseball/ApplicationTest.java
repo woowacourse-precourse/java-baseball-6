ed043b18641b5db974b4548543282db6c6bc94c7
Woongbae Yun <devbelly@naver.com>
2023-10-22 15:48:43 +0900

    test : 잘못된 입력에 대한 테스트 케이스 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,31 +1,55 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
 class ApplicationTest extends NsTest {
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
-                () -> {
-                    run("246", "135", "1", "597", "589", "2");
-                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
-                },
-                1, 3, 5, 5, 8, 9
+            () -> {
+                run("246", "135", "1", "597", "589", "2");
+                assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+            },
+            1, 3, 5, 5, 8, 9
         );
     }
 
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("1234"))
-                        .isInstanceOf(IllegalArgumentException.class)
+            assertThatThrownBy(() -> runException("1234"))
+                .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Nested
+    class PlayerTest {
+
+        @Test
+        void 숫자_이외의_값을_입력하면_IllegalArgumentException을_던진다() {
+            assertThatThrownBy(() -> new Player().guessNumber("abc"))
+                .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 길이가_3이_넘어가면_IllegalArgumentException을_던진다() {
+            assertThatThrownBy(() -> new Player().guessNumber("1234"))
+                .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 중복된_숫자가_존재하면_IllegalArgumentException을_던진다() {
+            assertThatThrownBy(() -> new Player().guessNumber("112"))
+                .isInstanceOf(IllegalArgumentException.class);
+        }
     }
 
     @Override
