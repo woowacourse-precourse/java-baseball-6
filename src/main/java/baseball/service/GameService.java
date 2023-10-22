2a557604b197ea58ded53396e2ebfb62a2145844
ykmxxi <ykmxxi97@gmail.com>
2023-10-22 13:13:12 +0900

    feat(GameService): 심판에게 판정을 요청해 결과를 받아오는 기능 구현

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -1,11 +1,24 @@
 package baseball.service;
 
+import baseball.domain.Balls;
 import baseball.domain.Computer;
+import baseball.domain.GameResult;
+import baseball.domain.Player;
+import baseball.domain.Referee;
 
 public class GameService {
 
 	public Computer createComputer() {
 		return new Computer();
+	}
+
+	public String requestJudgment(final Computer computer, final Player player) {
+		Referee referee = new Referee();
+		Balls computerBalls = computer.getBalls();
+		Balls playerBalls = player.getBalls();
+
+		GameResult gameResult = referee.compare(computerBalls, playerBalls);
+		return referee.notifyGameResult(gameResult);
 	}
 
 }
