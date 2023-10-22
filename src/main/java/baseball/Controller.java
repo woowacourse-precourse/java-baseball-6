c74d11a88851e5fb752a65db043fdc1d1b5d9185
김강년 <64246800+ekek54@users.noreply.github.com>
2023-10-22 17:55:29 +0900

    feat: 게임 재시작 플로우, 프로세스 전체 플로우 구현

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -6,17 +6,45 @@ import baseball.model.Game;
 import baseball.model.JudgeResult;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Controller {
     private static final String NUMBER_INPUT_ERROR = "숫자를 입력해야 합니다.";
     private static final String DIGIT_REGEXP = "[0-9]*";
     private static final String RESTART_INPUT_ERROR = "1 또는 2를 입력해야 합니다.";
-    private static final String ONE_OR_TWO_REGEXP = "[1|2]{1}";
-    private View view;
+    private static final String ONE_OR_TWO_REGEXP = "[1|2]";
+    private static final int RESTART = 1;
+    private static final int STOP = 2;
+
+    private final View view;
 
     public Controller(View view) {
         this.view = view;
+    }
+
+    public void run() {
+        view.printProgramStart();
+        do {
+            startGame();
+            view.printGameOver();
+        } while (shouldRestart());
+        view.printExitGame();
+    }
+
+    private boolean shouldRestart() {
+        int restartNum;
+        String restartInput = view.getRestartInput();
+        validOneOrTwo(restartInput);
+        restartNum = Integer.parseInt(restartInput);
+        return restartNum == RESTART;
+    }
+
+    private static void validOneOrTwo(String restartInput) {
+        if (!restartInput.matches(ONE_OR_TWO_REGEXP)) {
+            throw new IllegalArgumentException(RESTART_INPUT_ERROR);
+        }
     }
 
     private void startGame() {
@@ -30,14 +58,12 @@ public class Controller {
         }
     }
 
-    private void restartGame() {
-
-    }
-
     private static BaseballNumbers baseballNumbersByUser(String userInput) {
         List<BaseballNumber> baseballNumberList = new ArrayList<>();
         for (char c : userInput.toCharArray()) {
-            baseballNumberList.add(BaseballNumber.of((int) c));
+            int parseInt = Integer.parseInt(Character.toString(c));
+            BaseballNumber userBaseballNumber = BaseballNumber.of(parseInt);
+            baseballNumberList.add(userBaseballNumber);
         }
         return new BaseballNumbers(baseballNumberList);
     }
@@ -49,6 +75,6 @@ public class Controller {
     }
 
     private static boolean isDigit(String userInput) {
-        return userInput.matches("[0-9]*");
+        return userInput.matches(DIGIT_REGEXP);
     }
 }
