e45e9d33eb1b194dcee1abaed7fa3cdc6bef84af
sleeg00 <slee000220@gmail.com>
2023-10-22 14:55:54 +0900

    Feat: 입력, 랜덤한 3자리수 비교 기능 추가 Check.class 추가, 예외처리

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -7,11 +7,13 @@ public class BaseBallGame {
     private List<Integer> computerNumber;
     private Input input;
     private Rand rand;
+    private Check check;
 
     BaseBallGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
         input = new Input();
         rand = new Rand();
+        check = new Check();
     }
 
     void start() {
@@ -19,6 +21,7 @@ public class BaseBallGame {
         do {
             System.out.print("숫자를 입력해주세요 : ");
             inputNumber = input.getNumber();
+            check.compareNumber(inputNumber, computerNumber);
         } while (true);
 
     }
---
+++ src/main/java/baseball/Check.java
@@ -0,0 +1,58 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
+public class Check {
+    Check() {
+
+    }
+
+    void compareNumber(List<Integer> user, List<Integer> computer) {
+        int strike = 0;
+        int ball = 0;
+        int nothing = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (user.get(i) == computer.get(j)) {
+                    if (i == j) {
+                        strike++;
+                    } else {
+                        ball++;
+                    }
+                }
+            }
+        }
+        if (strike == 0 && ball == 0) {
+            nothing++;
+        }
+        printResult(strike, ball, nothing);
+    }
+
+    void printResult(int strike, int ball, int nothing) {
+        if (ball > 0) {
+            System.out.print(ball + "볼 ");
+        }
+        if (strike > 0) {
+            if (strike == 3) {
+                System.out.println("3스트라이크\n" + "3개의 숫자를 모두 맞히셨습니다!\n" + "게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                String number = Console.readLine();
+
+                if (number.equals("1")) {
+                    BaseBallGame baseBallGame = new BaseBallGame();
+                    baseBallGame.start();
+                } else {
+                    System.exit(0);
+                }
+            } else {
+                System.out.print(strike + "스트라이크 ");
+            }
+        }
+        if (nothing > 0) {
+            System.out.print("낫싱");
+        }
+        System.out.println();
+    }
+}
