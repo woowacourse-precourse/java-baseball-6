ba39f18d7644d1ffe7397ca7b774601a6eb9ef5f
devhoya97 <diajbh123@gmail.com>
2023-10-22 16:51:44 +0900

    refactor : BaseballNumbers 클래스가 추가됨에 따라 필요한 수정을 진행했습니다.

--- src/main/java/baseball/model/BaseballGame.java
+++ src/main/java/baseball/model/BaseballGame.java
@@ -1,27 +1,25 @@
 package baseball.model;
 
-import java.util.List;
-
 public class BaseballGame {
     private static final int BASEBALL_NUMBERS_LENGTH = 3;
 
-    private final List<BaseballNumber> computerBaseballNumbers;
-    private List<BaseballNumber> userBaseballNumbers;
+    private final BaseballNumbers computerBaseballNumbers;
+    private BaseballNumbers userBaseballNumbers;
 
-    public BaseballGame(List<BaseballNumber> computerBaseballNumbers, List<BaseballNumber> userBaseballNumbers) {
+    public BaseballGame(BaseballNumbers computerBaseballNumbers, BaseballNumbers userBaseballNumbers) {
         this.computerBaseballNumbers = computerBaseballNumbers;
         this.userBaseballNumbers = userBaseballNumbers;
     }
 
-    public void resetUserBaseballNumbers(List<BaseballNumber> userBaseballNumbers) {
+    public void resetUserBaseballNumbers(BaseballNumbers userBaseballNumbers) {
         this.userBaseballNumbers = userBaseballNumbers;
     }
 
     public int getBallCount() {
         int ballCount = 0;
         for (int index = 0; index < BASEBALL_NUMBERS_LENGTH; index++) {
-            BaseballNumber userBaseballNumber = userBaseballNumbers.get(index);
-            BaseballNumber randomBaseballNumber = computerBaseballNumbers.get(index);
+            BaseballNumber userBaseballNumber = userBaseballNumbers.getBaseballNumber(index);
+            BaseballNumber randomBaseballNumber = computerBaseballNumbers.getBaseballNumber(index);
             if (!userBaseballNumber.equals(randomBaseballNumber) && computerBaseballNumbers.contains(
                     userBaseballNumber)) {
                 ballCount++;
@@ -33,8 +31,8 @@ public class BaseballGame {
     public int getStrikeCount() {
         int strikeCount = 0;
         for (int index = 0; index < BASEBALL_NUMBERS_LENGTH; index++) {
-            BaseballNumber userBaseballNumber = userBaseballNumbers.get(index);
-            BaseballNumber randomBaseballNumber = computerBaseballNumbers.get(index);
+            BaseballNumber userBaseballNumber = userBaseballNumbers.getBaseballNumber(index);
+            BaseballNumber randomBaseballNumber = computerBaseballNumbers.getBaseballNumber(index);
             if (userBaseballNumber.equals(randomBaseballNumber)) {
                 strikeCount++;
             }
