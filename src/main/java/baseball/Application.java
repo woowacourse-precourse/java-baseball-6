faec67962e6d2686898bd1574320d2e4faf3638d
opixxx <opix0306@naver.com>
2023-10-22 17:47:29 +0900

    feat: add features for strike, ball, and nothing outcomes

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+    private static int validateStrike(List<Integer> computer, List<Integer> player) {
+        int strikeCount = 0;
+        for (int i = 0; i < 3; i++) {
+            if (computer.get(i).equals(player.get(i))) strikeCount++;
+        }
+        return strikeCount;
+    }
+
+    private static int validateBall(List<Integer> computer, List<Integer> player) {
+        int ballCount = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (computer.get(i).equals(player.get(j)) && i != j) ballCount++;
+            }
+        }
+        return  ballCount;
+    }
+
     public static void main(String[] args) {
 
         // 컴퓨터가 서로 다른 3자리를 뽑는 기능
@@ -16,6 +35,31 @@ public class Application {
                 computer.add(randomNumber);
             }
         }
+
+        while (true) {
+            System.out.println("숫자를 입력해주세요 : ");
+            List<Integer> player = InputConverter.convert(Console.readLine());
+
+            int strikeCount = validateStrike(computer, player);
+            int ballCount = validateBall(computer, player);
+
+            if (ballCount > 0) {
+                System.out.print(ballCount + "볼 ");
+            }
+            if (strikeCount > 0) {
+                System.out.print(strikeCount + "스트라이크");
+            }
+            if (ballCount == 0 && strikeCount == 0) {
+                System.out.print("낫싱");
+            }
+            System.out.println();
+
+            if (strikeCount == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            }
+        }
+
     }
 
 }
