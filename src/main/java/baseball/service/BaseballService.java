59f777462e82a3d4bef3fd398c93a1049289afaf
hong <kevin0928@naver.com>
2023-10-22 22:55:38 +0900

    feat: 입력 받은 값이 올바른 값인지 확인한다 구현

--- src/main/java/baseball/service/BaseballService.java
+++ src/main/java/baseball/service/BaseballService.java
@@ -1,12 +1,16 @@
 package baseball.service;
 
+import baseball.util.ExceptionHandler;
 import baseball.view.InputView;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class BaseballService {
     private final InputView inputView = new InputView();
+    private final ExceptionHandler exceptionHandler = new ExceptionHandler();
 
     public List<Integer> createRandomNumber() {
         List<Integer> computer = new ArrayList<>();
@@ -23,8 +27,36 @@ public class BaseballService {
         boolean isDifferent = true;
         while (isDifferent) {
             String input = inputView.getInput();
-
+            isValidNumber(input);
         }
+    }
+
+    private void isValidNumber(String str) {
+        if (isThreeDigits(str) && hasNoZero(str) && hasUniqueDigits(str)) {
+            return;
+        }
+        throw new IllegalArgumentException();
+    }
+
+    private boolean isThreeDigits(String str) {
+        return str.length() == 3;
+    }
+
+    private boolean hasNoZero(String str) {
+        return !str.contains("0");
+    }
+
+    public boolean hasUniqueDigits(String str) {
+        char[] digits = str.toCharArray();
+        Set<Character> uniqueSet = new HashSet<>();
+
+        for (char digit : digits) {
+            if (uniqueSet.contains(digit)) {
+                return false;
+            }
+            uniqueSet.add(digit);
+        }
+        return true;
     }
 
 }
--- src/main/java/baseball/util/ExceptionHandler.java
+++ src/main/java/baseball/util/ExceptionHandler.java
@@ -1,4 +1,7 @@
 package baseball.util;
 
 public class ExceptionHandler {
+    public void ThrowException() {
+        throw new IllegalStateException();
+    }
 }
