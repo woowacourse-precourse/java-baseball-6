4e38dd53c9cc47247349f1ac684dfabf6b27a6b5
JisooPyo <audwl326@gmail.com>
2023-10-23 05:43:24 +0900

    refactor: Number 클래스 생성
    * Number 클래스에서 컴퓨터 숫자와 인풋 숫자를 생성할 수 있도록 하여 랜덤 숫자를 생성하는 메서드와 입력값이 유효한지 검증하는 메서드를 이 클래스로 옮겼다.

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -1,17 +1,17 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
 
 public class BaseBallGame {
-	String computerNumber;
+	Number computerNumber;
+	Number inputNumber;
 
 	public BaseBallGame() {
 		start();
 	}
 
 	private void start() {
-		this.computerNumber = generateRandomNumber();
+		this.computerNumber = new Number();
 		System.out.println("숫자 야구 게임을 시작합니다.");
 
 		guessTheNumbers();
@@ -19,6 +19,44 @@ public class BaseBallGame {
 		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 
 		askWantToRestart();
+	}
+
+	// 숫자 맞히는 과정 분리
+	private void guessTheNumbers() {
+		String judgment = "";
+
+		while (!judgment.equals("3스트라이크")) {
+			System.out.print("숫자를 입력해주세요 : ");
+			inputNumber = new Number(Console.readLine());
+
+			judgment = judgeInputNumber(inputNumber.number);
+			System.out.println(judgment);
+		}
+	}
+
+	// 스트라이크, 볼 판정
+	private String judgeInputNumber(String inputNumber) {
+		int strike = 0;
+		int ball = 0;
+		for (int i = 0; i <= 2; i++) {
+			if (inputNumber.charAt(i) == computerNumber.number.charAt(i)) {
+				strike++;
+				continue;
+			}
+			if (computerNumber.number.contains(Character.toString(inputNumber.charAt(i)))) {
+				ball++;
+			}
+		}
+		if (strike == 0 && ball == 0) {
+			return "낫싱";
+		}
+		if (strike == 0) {
+			return ball + "볼";
+		}
+		if (ball == 0) {
+			return strike + "스트라이크";
+		}
+		return ball + "볼 " + strike + "스트라이크";
 	}
 
 	// 다시 시작할 것인지 물어본다.
@@ -36,74 +74,5 @@ public class BaseBallGame {
 		}
 		throw new IllegalArgumentException("잘못된 값을 입력하셨습니다. 프로그램이 종료됩니다.");
 	}
-
-	// 숫자 맞히는 과정 분리
-	private void guessTheNumbers() {
-		String judgment = "";
-
-		while (!judgment.equals("3스트라이크")) {
-			System.out.print("숫자를 입력해주세요 : ");
-			String myNumber = Console.readLine();
-			validateMyNumber(myNumber);
-
-			judgment = judgeMyNumber(myNumber);
-			System.out.println(judgment);
-		}
-	}
-
-	// 스트라이크, 볼 판정
-	private String judgeMyNumber(String myNumber) {
-		int strike = 0;
-		int ball = 0;
-		for (int i = 0; i <= 2; i++) {
-			if (myNumber.charAt(i) == computerNumber.charAt(i)) {
-				strike++;
-				continue;
-			}
-			if (computerNumber.contains(Character.toString(myNumber.charAt(i)))) {
-				ball++;
-			}
-		}
-		if (strike == 0 && ball == 0) {
-			return "낫싱";
-		}
-		if (strike == 0) {
-			return ball + "볼";
-		}
-		if (ball == 0) {
-			return strike + "스트라이크";
-		}
-		return ball + "볼 " + strike + "스트라이크";
-	}
-
-	// 입력값 유효한지 검증
-	private void validateMyNumber(String myNumber) {
-		char[] arr = myNumber.toCharArray();
-		if (arr.length != 3) {
-			throw new IllegalArgumentException("3자리 숫자를 입력해야 합니다.");
-		}
-		if (arr[0] == arr[1] || arr[1] == arr[2] || arr[2] == arr[0]) {
-			throw new IllegalArgumentException("서로 다른 임의의 수 3개를 입력해야 합니다.");
-		}
-		if (!('1' <= arr[0] && arr[0] <= '9') ||
-				!('1' <= arr[1] && arr[1] <= '9') ||
-				!('1' <= arr[2] && arr[2] <= '9')) {
-			throw new IllegalArgumentException("숫자를 입력해야 합니다.");
-		}
-	}
-
-	// 1에서 9까지 서로 다른 임의의 수 3개를 선택하여 문자열로 출력합니다.
-	private String generateRandomNumber() {
-		String str = "";
-		str += Randoms.pickNumberInRange(1, 9);
-		while (str.length() != 3) {
-			int randomNumber = Randoms.pickNumberInRange(1, 9);
-			if (!str.contains(String.valueOf(randomNumber))) {
-				str += randomNumber;
-			}
-		}
-		return str;
-	}
-
 
 }
