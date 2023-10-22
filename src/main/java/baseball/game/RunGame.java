9ea246ea14760547ab9144fd5a446bbaa494ac95
ahma0 <dsd932@naver.com>
2023-10-22 16:20:16 +0900

    refactor: 수 입력, 검증하는 기능 분리

--- src/main/java/baseball/game/RunGame.java
+++ src/main/java/baseball/game/RunGame.java
@@ -22,9 +22,8 @@ public class RunGame {
         Game.generateNumber();
 
         while (true) {
-            String userAnswer = getUserAnswer();
-            checkUserAnswer(userAnswer);
-            Game.compareNumber(userAnswer);
+            User.getUserAnswer();
+            Game.compareNumber(User.getAnswer());
             if(Game.printResult())
                 break;
         }
@@ -48,27 +47,4 @@ public class RunGame {
         return answer.equals("1") || answer.equals("2");
     }
 
-    private static String getUserAnswer() {
-        System.out.print("숫자를 입력해주세요 : ");
-        return Console.readLine();
-    }
-
-    private static void checkUserAnswer(String answer) {
-        if(!isAnswerLengthRight(answer)
-                || !isAnswerDigit(answer)
-                || isAnswerContainZero(answer))
-            throw new IllegalArgumentException("잘못된 입력입니다.");
-    }
-
-    private static boolean isAnswerLengthRight(String answer) {
-        return answer.length() == 3;
-    }
-
-    private static boolean isAnswerDigit(String answer) {
-        return answer.chars().allMatch(Character::isDigit);
-    }
-
-    private static boolean isAnswerContainZero(String answer) {
-        return answer.chars().anyMatch(ch -> ch == '0');
-    }
 }
---
+++ src/main/java/baseball/game/User.java
@@ -0,0 +1,37 @@
+package baseball.game;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class User {
+
+    private static String answer;
+
+    public static String getAnswer() {
+        return answer;
+    }
+
+    public static void getUserAnswer() {
+        System.out.print("숫자를 입력해주세요 : ");
+        answer = Console.readLine();
+        checkUserAnswer();
+    }
+
+    private static void checkUserAnswer() {
+        if(!isAnswerLengthRight()
+                || !isAnswerDigit()
+                || isAnswerContainZero())
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+    }
+
+    private static boolean isAnswerLengthRight() {
+        return answer.length() == 3;
+    }
+
+    private static boolean isAnswerDigit() {
+        return answer.chars().allMatch(Character::isDigit);
+    }
+
+    private static boolean isAnswerContainZero() {
+        return answer.chars().anyMatch(ch -> ch == '0');
+    }
+}
