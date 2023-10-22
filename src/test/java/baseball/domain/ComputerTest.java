eb04d203432450d9d173528dd4743fce4502a97e
kunsanglee <gosmdochee2@gmail.com>
2023-10-22 19:19:44 +0900

    refactor(ComputerTest): 테스트코드 내 클래스명 변경

--- src/test/java/baseball/domain/ComputerTest.java
+++ src/test/java/baseball/domain/ComputerTest.java
@@ -3,6 +3,7 @@ package baseball.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
+import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -14,19 +15,27 @@ public class ComputerTest {
     @DisplayName("컴퓨터가 생성한 숫자와 사용자가 입력한 숫자를 비교하여 Score를 반환한다.")
     @ParameterizedTest
     @MethodSource("provideGetScoreTestArguments")
-    void getScoreTest(String input, Score expectedScore) {
-        Computer computer = new Computer(new FixedNumbersGenerator());
-        Score score = computer.getScore(input);
+    void getScoreTest(BaseballNumbers inputBaseballNumbers, Score expectedScore) {
+        Computer computer = new Computer(new FixedBaseballNumbersGenerator());
+        Score score = computer.getScore(inputBaseballNumbers);
         assertThat(score).isEqualTo(expectedScore);
     }
 
     static Stream<Arguments> provideGetScoreTestArguments() {
         return Stream.of(
-                arguments("123", new Score(new Ball(1), new Strike(1))),
-                arguments("145", new Score(new Ball(1), new Strike(0))),
-                arguments("671", new Score(new Ball(2), new Strike(0))),
-                arguments("216", new Score(new Ball(0), new Strike(1))),
-                arguments("713", new Score(new Ball(0), new Strike(3)))
+                arguments(setInputBaseballNumbers(1, 2, 3), setExpectedScore(1, 1)),
+                arguments(setInputBaseballNumbers(1, 2, 3), setExpectedScore(1, 0)),
+                arguments(setInputBaseballNumbers(1, 2, 3), setExpectedScore(2, 0)),
+                arguments(setInputBaseballNumbers(1, 2, 3), setExpectedScore(0, 1)),
+                arguments(setInputBaseballNumbers(1, 2, 3), setExpectedScore(0, 3))
         );
+    }
+
+    private static List<BaseballNumber> setInputBaseballNumbers(int x, int y, int z) {
+        return Stream.of(x, y, z).map(BaseballNumber::new).toList();
+    }
+
+    private static Score setExpectedScore(int ball, int ball1) {
+        return new Score(new Ball(ball), new Strike(ball1));
     }
 }
