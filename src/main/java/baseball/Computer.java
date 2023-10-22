95ddd295e0b1b2ac5202944822a5d0d7b5a539de
hyeonwoo <ziczin7176@gmail.com>
2023-10-23 04:01:57 +0900

    Computer가 숫자 고르는 기능 검증과정 추가

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -3,10 +3,11 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Computer {
+    private static final int TOTAL_COMPUTER_NUMBERS = 3;
     private String[] computerNumbers;
     private String[] playerNumbers;
     private GameMachine gameMachine;
-    private static final int TOTAL_COMPUTER_NUMBERS = 3;
+    String[] pickedNumbers = new String[TOTAL_COMPUTER_NUMBERS];
 
     public String[] getComputerNumbers() {
         return computerNumbers;
@@ -34,9 +35,12 @@ public class Computer {
     // Computer 클래스가 Randoms 클래스에 의존함.
     // 변경될 가능성이 있거나 테스트에서 랜덤 값이 아닌 고정 값을 받는 경우 의존성 주입을 고려할 수 있음.
     public void pickComputerNumbers() {
-        String[] pickedNumbers = new String[TOTAL_COMPUTER_NUMBERS];
         for (int count = 0; count < TOTAL_COMPUTER_NUMBERS; count++) {
             pickedNumbers[count] = String.valueOf(Randoms.pickNumberInRange(1, 9));
+        }
+        if (pickedNumbers[0].equals(pickedNumbers[1]) || pickedNumbers[0].equals(pickedNumbers[2])
+                || pickedNumbers[1].equals(pickedNumbers[2])) {
+            this.pickComputerNumbers();
         }
         this.computerNumbers = pickedNumbers;
     }
