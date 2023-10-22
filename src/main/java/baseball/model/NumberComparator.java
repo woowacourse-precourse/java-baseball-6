a546d175f08e7bc26a15e384a47e2c753fd1b8bb
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-10-22 20:53:47 +0900

    style : 메서드 위치 변경 기능 순서별로 메서드 위치 변경

--- src/main/java/baseball/model/NumberComparator.java
+++ src/main/java/baseball/model/NumberComparator.java
@@ -4,16 +4,24 @@ public class NumberComparator {
     private int strikes = 0;
     private int balls = 0;
 
+    /**
+     * 볼, 스트라이크 개수 세기
+     */
+    public int[] calculateResult(String userNumber, String programNumber) {
+        resetResult();
+        checkBall(userNumber, programNumber);
+        checkStrike(userNumber, programNumber);
+        updateBall();
+
+        return new int[]{balls, strikes};
+    }
 
     /**
-     * 스트라이크 개수 체크
+     * 새로운 게임이 시작되었을 때, 기존 게임의 결과 초기화
      */
-    private void checkStrike(String userNumber, String programNumber) {
-        for (int i = 0; i < userNumber.length(); i++) {
-            if (userNumber.charAt(i) == programNumber.charAt(i)) {
-                strikes++;
-            }
-        }
+    private void resetResult() {
+        strikes = 0;
+        balls = 0;
     }
 
     /**
@@ -28,6 +36,18 @@ public class NumberComparator {
     }
 
     /**
+     * 스트라이크 개수 체크
+     */
+    private void checkStrike(String userNumber, String programNumber) {
+        for (int i = 0; i < userNumber.length(); i++) {
+            if (userNumber.charAt(i) == programNumber.charAt(i)) {
+                strikes++;
+            }
+        }
+    }
+
+
+    /**
      * 스트라이크, 볼 개수 조정 위 로직에는 볼 개수를 셀 때, 스트라이크인 숫자를 제외하는 기능이 없다.
      */
     private void updateBall() {
@@ -37,25 +57,6 @@ public class NumberComparator {
         }
     }
 
-    /**
-     * 새로운 게임이 시작되었을 때, 기존 게임의 결과 초기화
-     */
-    private void resetResult() {
-        strikes = 0;
-        balls = 0;
-    }
-
-    /**
-     * 볼, 스트라이크 개수 세기
-     */
-    public int[] calculateResult(String userNumber, String programNumber) {
-        resetResult();
-        checkStrike(userNumber, programNumber);
-        checkBall(userNumber, programNumber);
-        updateBall();
-
-        return new int[]{balls, strikes};
-    }
 
     /**
      * 전부 스트라이크면 true반환
--- src/main/java/baseball/model/ProgramNumber.java
+++ src/main/java/baseball/model/ProgramNumber.java
@@ -7,19 +7,9 @@ import java.util.Set;
 public class ProgramNumber {
     private String programNumber;
 
-    //생성된 프로그램 숫자 반환
-    public String getProgramNumber() {
-        return programNumber;
-    }
-
     //생성 시 바로 프로그램 숫자를 만들 수 있도록 한다.
     public ProgramNumber() {
         createProgramNumber();
-    }
-
-    //1 ~ 9 범위 내의 랜덤한 숫자 하나 획득
-    private String getOneNumber() {
-        return Integer.toString(Randoms.pickNumberInRange(NumberStatus.START_NUMBER, NumberStatus.END_NUMBER));
     }
 
     //세자리 자연수 획득
@@ -31,5 +21,15 @@ public class ProgramNumber {
         }
 
         programNumber = String.join("", programNumberSet);
+    }
+
+    //1 ~ 9 범위 내의 랜덤한 숫자 하나 획득
+    private String getOneNumber() {
+        return Integer.toString(Randoms.pickNumberInRange(NumberStatus.START_NUMBER, NumberStatus.END_NUMBER));
+    }
+
+    //생성된 프로그램 숫자 반환
+    public String getProgramNumber() {
+        return programNumber;
     }
 }
