7a535612dde4e112d82e52405bcd8dfc9d9aabfd
hyunseok <hyun104467@naver.com>
2023-10-23 00:06:54 +0900

    [Feat] UserInputHandler/유저 입력 검증

--- src/main/java/baseball/UserInputHandler.java
+++ src/main/java/baseball/UserInputHandler.java
@@ -5,9 +5,23 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class UserInputHandler {
-    private boolean checkDigitFromInput() {}
-    private boolean checkLengthFromInput() {}
-    private boolean isCorrectInput() {}
+    private boolean isCorrectDigit(String input) {
+        for (char c: input.toCharArray()) {
+            if (isZero(c) && !Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    private boolean isZero(char c) {
+        return c == '0';
+    }
+    private boolean isCorrectLengthFromInput(String input) {
+        return input.length() == 3;
+    }
+    private boolean isCorrectInput(String input) {
+        return isCorrectDigit(input) && isCorrectLengthFromInput(input);
+    }
     public List<Integer> makeUserCount() {}
     public List<Integer> takeInput() {
         System.out.println("숫자를 입력해주세요 : ");
