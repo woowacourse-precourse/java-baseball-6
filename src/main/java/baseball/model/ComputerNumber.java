3ba3e83225a181182d868bbbb970492e098b333b
oxdjww <oxdjww@gmail.com>
2023-10-23 00:40:55 +0900

    refactor : ComputerNumber 생성자 구현 (자체적으로 랜덤 컴퓨터 숫자를 갖도록)

--- src/main/java/baseball/model/ComputerNumber.java
+++ src/main/java/baseball/model/ComputerNumber.java
@@ -1,13 +1,21 @@
 package baseball.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ComputerNumber {
-    private final List<Integer> computerNumber;
+    private final List<Integer> computerNumber = new ArrayList<>();
 
-    public ComputerNumber(List<Integer> computerNumber) {
-        this.computerNumber = computerNumber;
+    public ComputerNumber() {
+        while (computerNumber.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumber.contains(randomNumber)) {
+                computerNumber.add(randomNumber);
+            }
+        }
     }
+
 
     public void addDigit(Integer digit) {
         validateComputerNumber(digit);
