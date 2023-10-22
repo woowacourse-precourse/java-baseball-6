42bdd5f42d33ebea54aeea37bfc205151cade413
Hyeon0208 <dmsgktn0208@gmail.com>
2023-10-22 23:34:22 +0900

    feat : 게임 커맨드 값을 갖는 Enum 클래스 추가

---
+++ src/main/java/baseball/view/GameCommand.java
@@ -0,0 +1,16 @@
+package baseball.view;
+
+public enum GameCommand {
+    RESTART("1"),
+    END("2");
+
+    private final String String;
+
+    GameCommand(java.lang.String string) {
+        String = string;
+    }
+
+    public String getString() {
+        return String;
+    }
+}
