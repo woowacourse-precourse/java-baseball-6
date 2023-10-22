edcc0efc4b201e8e7dc53b9d467420fc863bb92a
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-10-22 18:09:37 +0900

    feat: service interface 생성

---
+++ src/main/java/baseball/service/BullsAndCowsService.java
@@ -0,0 +1,15 @@
+package baseball.service;
+
+import java.util.List;
+
+public interface BullsAndCowsService {
+
+    // 컴퓨터 랜덤값 3자리 생성값 반환
+    List<Integer> createComputerRandomValue();
+
+    // 사용자 추측값 3자리 입력값 반환
+    List<Integer> getThreeDigits();
+
+    // 게임 종료 여부 값 반환
+    boolean isGameRunning();
+}
