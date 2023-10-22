6b844b1954c61a18db99af6708953afcee4b4320
soyeonii <dingdingcrong@naver.com>
2023-10-22 23:58:42 +0900

    refactor: 사용자 추측 숫자와 컴퓨터 생성 숫자 비교 로직 수정

--- src/main/java/baseball/game/BaseballGame.java
+++ src/main/java/baseball/game/BaseballGame.java
@@ -1,7 +1,6 @@
 package baseball.game;
 
 import baseball.view.GameView;
-import java.util.Arrays;
 
 public class BaseballGame {
     public static void start() throws IllegalAccessException {
@@ -12,13 +11,12 @@ public class BaseballGame {
 
     private static void playGame() throws IllegalAccessException {
         int[] randomNumbers = Computer.generateRandomNumbers();
-        System.out.println("randomNumbers = " + Arrays.toString(randomNumbers));
         while (true) {
             GameView.displayUserInputPrompt();
             int[] guessNumbers = User.getGuessNumbers();
-            int[] calculateResult = Computer.calculateResult(randomNumbers, guessNumbers);
-            String formatResult = Computer.formatResult(calculateResult);
-            GameView.displayGuessResult(formatResult);
+
+            int[] result = Computer.calculateResult(guessNumbers, randomNumbers);
+            GameView.displayGuessResult(Computer.formatResult(result));
         }
     }
 }
--- src/main/java/baseball/game/Computer.java
+++ src/main/java/baseball/game/Computer.java
@@ -18,11 +18,11 @@ public class Computer {
                 .toArray();
     }
 
-    public static int[] calculateResult(int[] randomNumbers, int[] guessNumbers) {
+    public static int[] calculateResult(int[] guessNumbers, int[] randomNumbers) {
         int[] result = new int[2];
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                if (randomNumbers[i] == guessNumbers[j]) {
+                if (guessNumbers[i] == randomNumbers[j]) {
                     if (i == j) {
                         result[1]++;
                     } else {
