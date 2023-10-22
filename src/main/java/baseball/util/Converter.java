3c9522523519ab6ca152892ff0ec3db62caebc49
eom-tae-in <eti0728@daum.net>
2023-10-22 22:33:17 +0900

    refactor: 문자열을 정수로 바꿀 때 이를 검증하는 로직 추가

--- src/main/java/baseball/util/Converter.java
+++ src/main/java/baseball/util/Converter.java
@@ -1,5 +1,8 @@
 package baseball.util;
 
+import static java.util.regex.Pattern.matches;
+
+import baseball.exception.consoleView.ConsoleInputCanNotChangeToIntException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -7,14 +10,22 @@ import java.util.stream.Collectors;
 public class Converter {
 
     private static final String EMPTY_STRING = "";
+    private static final String REGEXP_PATTERN_NUMBER = "^[\\d]*$";
 
     private Converter() {
     }
 
     public static List<Integer> convertStringToIntegerList(final String string) {
+        validateCanConvertToInt(string);
         return Arrays.stream(string.split(EMPTY_STRING))
                 .map(Integer::parseInt)
                 .toList();
+    }
+
+    private static void validateCanConvertToInt(final String string) {
+        if (!matches(REGEXP_PATTERN_NUMBER, string)) {
+            throw new ConsoleInputCanNotChangeToIntException(string);
+        }
     }
 
     public static String convertIntegerListToString(final List<Integer> numbers) {
