cb2711f67c9eb91d84a4e8cc9c74203468b18449
ohhamma <judyham10634@naver.com>
2023-10-22 22:54:53 +0900

    :art: refactor: 중첩 if문 분리

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -49,16 +49,14 @@ public class Game {
         if (ball > 0 && strike > 0) {
             result = ball + "볼 " + strike + "스트라이크";
         }
-        else if (ball == 0 && strike == 0) {
-            result = "낫싱";
+        else if (ball > 0 && strike == 0) {
+            result = ball + "볼";
+        }
+        else if (ball == 0 && strike > 0) {
+            result = strike + "스트라이크";
         }
         else {
-            if (ball > 0) {
-                result = ball + "볼";
-            }
-            else {
-                result = strike + "스트라이크";
-            }
+            result = "낫싱";
         }
         System.out.println(result);
         if (strike == randomLen) {
