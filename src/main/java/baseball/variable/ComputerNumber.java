53a2085431301dbbd56e1da5d26730373330c6ee
soyoungxxx <otherplet@gmail.com>
2023-10-23 03:29:21 +0900

    feat: ComputerNumber.java 작성 - computerNumber 변수에 Random 값 할당

--- src/main/java/baseball/variable/ComputerNumber.java
+++ src/main/java/baseball/variable/ComputerNumber.java
@@ -1,4 +1,26 @@
 package baseball.variable;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import java.util.ArrayList;
+
 public class ComputerNumber {
+    public List<Integer> computerNumber = new ArrayList<>();
+
+    public void setComputerNumber() {
+        while (computerNumber.size() < 3) {
+            getRandomNumber();
+        }
+    }
+
+    private void getRandomNumber() {
+        int randomNumber = Randoms.pickNumberInRange(1, 9);
+        if (!computerNumber.contains(randomNumber)) {
+            computerNumber.add(randomNumber);
+        }
+    }
+
+    public void clearComputerNumber() {
+        computerNumber.clear();
+    }
 }
