8292bcfe1ec61775171c8df7353e0455f23da718
devhoya97 <diajbh123@gmail.com>
2023-10-22 16:10:04 +0900

    refactor : 새로운 모델 클래스 생성 '숫자로만 이루어진 String'을 입력받는 메서드를 InputView에 추가할 때, 이와 같은 객체를 활용하면 값 검증에 대한 책임을 적절하게 분담할 수 있지 않을까 생각되어 추가했습니다

--- src/main/java/baseball/model/StringNumbers.java
+++ src/main/java/baseball/model/StringNumbers.java
@@ -1,2 +1,26 @@
-package baseball.model;public class StringNumbers {
+package baseball.model;
+
+import baseball.utils.Validator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StringNumbers {
+    private final String stringNumbers;
+
+    public StringNumbers(String stringNumbers) {
+        validateStringNumbers(stringNumbers);
+        this.stringNumbers = stringNumbers;
+    }
+
+    private void validateStringNumbers(String stringNumbers) {
+        Validator.validateStringEmptyOrNull(stringNumbers);
+        Validator.validateStringDigit(stringNumbers);
+    }
+
+    public List<Integer> translateToIntegerList() {
+        return stringNumbers.chars()
+                .map(Character::getNumericValue)
+                .boxed()
+                .collect(Collectors.toList());
+    }
 }
