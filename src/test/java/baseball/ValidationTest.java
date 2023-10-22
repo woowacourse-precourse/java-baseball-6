49406cbdbcdf7ea2b477319abc145dc44d21286e
seonghoo1217 <seonghoo1217@naver.com>
2023-10-23 07:13:48 +0900

    refactor : modify test logic on validate exception when user value input

--- src/test/java/baseball/ValidationTest.java
+++ src/test/java/baseball/ValidationTest.java
@@ -15,7 +15,8 @@ public class ValidationTest extends NsTest {
     @ValueSource(strings = {"1234","12","1 3","12a","222","012",""})
     void 사용자_게임_입력_값_예외_테스트(String input_value){
         assertThatThrownBy(()->GameValidation.verifyForRetryValue(input_value))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[Err]");
     }
 
     @ParameterizedTest
@@ -23,9 +24,8 @@ public class ValidationTest extends NsTest {
     void 사용자_게임_재도전_여부_입력_값_예외_테스트(String input_retry){
         assertThatThrownBy(()->GameValidation.verifyForRetryValue(input_retry))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("값");
+                .hasMessageContaining("[Err]");
     }
-
 
     @Override
     protected void runMain() {
