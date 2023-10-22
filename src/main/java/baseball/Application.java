0f7a3e3ee4a2b873593010aba648e2752609b124
Su-daa <subin.kk019@gmail.com>
2023-10-22 23:43:17 +0900

    # 본문은 아래에 작성
    # 꼬릿말은 아래에 작성: ex) Github issue #23
    Feat: 랜덤 숫자(볼) 3개 생성 기능 추가
    
    1-9 사이 중복되지 않는 3개 숫자 생성 하여 randomBalls 에 저장
    # --- COMMIT END ---
    #   <타입> 리스트
    #   feat        : 기능 (새로운 기능)
    #   fix         : 버그 (버그 수정)
    #   refactor    : 리팩토링
    #   design      : CSS 등 사용자 UI 디자인 변경
    #   comment     : 필요한 주석 추가 및 변경
    #   style       : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs        : 문서 수정 (문서 추가, 수정, 삭제, README)
    #   test        : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore       : 기타 변경사항 (빌드 스크립트 수정, assets, 패키지 매니저 등)
    #   init        : 초기 생성
    #   rename      : 파일 혹은 폴더명을 수정하거나 옮기는 작업만 한 경우
    #   remove      : 파일을 삭제하는 작업만 수행한 경우
    # ------------------
    #   제목 첫 글자를 대문자로
    #   제목은 명령문으로
    #   제목 끝에 마침표(.) 금지
    #   제목과 본문을 한 줄 띄워 분리하기
    #   본문은 "어떻게" 보다 "무엇을", "왜"를 설명한다.
    #   본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    # ------------------
    #   <꼬리말>
    #   필수가 아닌 optioanl
    #   Fixes        :이슈 수정중 (아직 해결되지 않은 경우)
    #   Resolves     : 이슈 해결했을 때 사용
    #   Ref          : 참고할 이슈가 있을 때 사용
    #   Related to   : 해당 커밋에 관련된 이슈번호 (아직 해결되지 않은 경우)
    #   ex) Fixes: #47 Related to: #32, #21

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,31 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
     }
+
+    /**
+     * 랜덤 볼 3개 만들기
+     */
+    private List<Integer> makeRandomBalls() {
+        List<Integer> randomBalls = new ArrayList<>();
+
+        while (randomBalls.size() < 3) {
+            int randomBall = Randoms.pickNumberInRange(1, 9);
+            if (!randomBalls.contains(randomBall)) {
+                randomBalls.add(randomBall);
+            }
+        }
+
+        return randomBalls;
+    }
+
+
 }
