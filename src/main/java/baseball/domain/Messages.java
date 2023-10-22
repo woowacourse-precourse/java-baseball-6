5fc32d3027a7e0dfff86b25c4f249b07d13d6208
qudwns017 <qudwns017@naver.com>
2023-10-23 03:18:48 +0900

    Feat: 게임 메세지 정의하는 Messages 클래스 추가

--- src/main/java/baseball/domain/Messages.java
+++ src/main/java/baseball/domain/Messages.java
@@ -4,6 +4,8 @@ public class Messages {
     public static final String START_GAME = "숫자 야구 게임을 시작합니다.";
     public static final String END_GAME = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"
             + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-
+    public static final String BALL = "볼";
+    public static final String STRIKE = "스트라이크";
+    public static final String NOTHING = "낫싱";
+    public static final String INPUT = "숫자를 입력해주세요 : ";
 }
-
