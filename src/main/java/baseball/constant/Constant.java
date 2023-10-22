794f760c8b96d9da1abfb04aa0345e7417df938c
Jihyun3478 <leejihyun3478@gmail.com>
2023-10-23 02:55:19 +0900

    Feat(Constant): 상수 클래스 추가

---
+++ src/main/java/baseball/constant/Constant.java
@@ -0,0 +1,10 @@
+package baseball.constant;
+
+public class Constant {
+
+    private static final int NUMBER_LENGTH = 3;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+    private static final int RESTART = 1;
+    private static final int END = 2;
+}
