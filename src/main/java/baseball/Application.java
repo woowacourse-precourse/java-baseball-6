9491da5c16e2b253b4cf4357448b97efd589da68
YuJin Kwon <tokyj515@naver.com>
2023-10-22 21:54:58 +0900

    Feat: 숫자 매칭, 결과 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -63,20 +63,43 @@ public class Application {
 
 
                 //사용자 숫자와 컴퓨터 숫자 확인하기
+                int ball = 0;
+                int strike = 0;
 
+                for(int i = 0; i < comNum.size(); i++){
+                    for(int j = 0; j < userNum.size(); j++){
+                        if(comNum.get(i) == userNum.get(j)){
+                            if(i == j) strike++;
+                            else ball++;
+                        }
+                    }
+                }
 
 
 
                 //결과 출력
+                if(ball > 0 && strike > 0){
+                    System.out.println(ball+"볼 "+strike+"스트라이크");
+                } else if(ball == 0 && strike > 0){
+                    if(strike == 3){
+                        System.out.println(strike+"스트라이크\n"+"3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                        break;
+                    } else
+                        System.out.println(strike+"스트라이크");
+                } else if(ball > 0 && strike == 0){
+                    System.out.println(ball+"볼");
+                }else if(ball == 0 && strike == 0){
+                    System.out.println("낫싱");
+                }
 
 
 
             }
 
 
-            //System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            //String s = Console.readLine();
-            //if(s.equals("2")) break;
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String s = Console.readLine();
+            if(s.equals("2")) break;
 
 //            System.out.println("---------------------------------------------");
         }
