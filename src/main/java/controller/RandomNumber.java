6742b22aa082f614cd88afeec101503c64e64b2e
Lee JaeHyuk <80966413+JH22222@users.noreply.github.com>
2023-10-22 19:27:01 +0900

    fix: 난수 생성 리스트 computer로 되어 있던 것 수정

--- src/main/java/controller/RandomNumber.java
+++ src/main/java/controller/RandomNumber.java
@@ -17,11 +17,10 @@ public class RandomNumber {
     }
 
     public void generateNumbers(){
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
+        while (randomNumbers.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
+            if (!randomNumbers.contains(randomNumber)) {
+                randomNumbers.add(randomNumber);
             }
         }
     }
