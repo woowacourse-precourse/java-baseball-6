758a3c9246993bb571dd6cb1954de80852661244
eunjin3481 <eunjin3481@gmail.com>
2023-10-22 21:26:13 +0900

    Feat: BaseballGame class 구조

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,24 @@
+package baseball;
+
+class BaseballGame{
+    private HashMap<Integer, Integer> computerNumbers;
+    private int[] userNumbers;
+
+    public void play(){
+    }
+
+    public void generateComputerNumbers(){
+    }
+
+    public void userNumbersInput(){
+    }
+
+    public int countStrikes(){
+    }
+
+    public int countBalls(){
+    }
+
+    public boolean gameResult(int strikeNum, int ballNum){
+    }
+}
