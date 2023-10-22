27eaefc46070eff05e23b8959d44a97155b3a0f1
Son JunHyung <kingjh1125@gmail.com>
2023-10-23 01:09:26 +0900

    refactor: 유저가 입력한 값에 대해 UserBall로 일급 컬렉션 적용

---
+++ src/main/java/vo/UserBall.java
@@ -0,0 +1,31 @@
+/*
+ * <pre>
+ * Class : UserBall
+ * Comment: 유저가 입력한 값을 객체화
+ * History
+ * ================================================================
+ * DATE             AUTHOR           NOTE
+ * ----------------------------------------------------------------
+ * 2023-10-23       손준형           최초 생성
+ * </pre>
+ *
+ * @author 손준형
+ * @version 1.0.0
+ * @see
+ */
+
+package vo;
+
+import java.util.List;
+
+public class UserBall {
+    private final List<Integer> userBall;
+
+    public UserBall(List<Integer> answerBall) {
+        this.userBall = answerBall;
+    }
+
+    public List<Integer> getUserBall() {
+        return userBall;
+    }
+}
