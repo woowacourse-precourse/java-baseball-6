15c0cfef65f7a07078fe89757feaac3e8e56a492
twkwon0417 <kwontony123@konkuk.ac.kr>
2023-10-22 15:18:55 +0900

    feat: 게임로직 구현

--- src/main/java/baseball/View/OutputView.java
+++ src/main/java/baseball/View/OutputView.java
@@ -9,7 +9,7 @@ public class OutputView {
         System.out.println(result);
     }
 
-    public void printQuitOrRestart() {
+    public void printGameOver() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 }
--- src/main/java/baseball/controller/BaseballGameController.java
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -2,16 +2,34 @@ package baseball.controller;
 
 import baseball.View.InputView;
 import baseball.View.OutputView;
+import baseball.domain.BallCount;
+import baseball.domain.Opponent;
 
 public class BaseballGameController {
     OutputView outputView = new OutputView();
     InputView inputView = new InputView();
 
     public void game() {
-        outputView.printInit();
-        inputView.inputUserGuess();
-        ///
+        Opponent opponent = initOpponent();
+
+        while(true) {
+            String userInput = inputView.inputUserGuess();
+            BallCount ballCount = opponent.analyzeTry(userInput);
+            outputView.printResult(ballCount.comment());
+            if(ballCount.gameEnd()) {
+                outputView.printGameOver();
+                String quitOrRestart = inputView.inputQuitOrRestart();
+                if(quitOrRestart.equals("1")) {
+                    game();
+                }
+                break;
+            }
+        }
     }
 
+    private Opponent initOpponent() {
+        outputView.printInit();
 
+        return new Opponent();
+    }
 }
