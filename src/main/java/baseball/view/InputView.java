b1c74ab37a2d3efea3ff58b16cd396487eda15ce
siwol25 <cyj5695@naver.com>
2023-10-22 22:43:42 +0900

    refactor: change command constant retrieval method due to conversion of GameCommand class to enum

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -5,7 +5,7 @@ import baseball.constant.GameCommand;
 public class InputView {
     private final String GUESS_PROMPT = "숫자를 입력해주세요 : ";
     private final String GAME_OVER_MESSAGE =
-            "게임을 새로 시작하려면 " + GameCommand.GAME_RESTART_COMMAND + ", 종료하려면 " + GameCommand.GAME_OVER_COMMAND
+            "게임을 새로 시작하려면 " + GameCommand.GAME_RESTART.getCommand() + ", 종료하려면 " + GameCommand.GAME_OVER.getCommand()
                     + "를 입력하세요.";
 
     public void printGuessPrompt() {
