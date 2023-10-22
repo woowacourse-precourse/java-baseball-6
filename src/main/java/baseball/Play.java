8a82ba3514dbea6c3ac06532681bc567627b6ebf
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-10-22 19:37:29 +0900

    style(Play): Revise methods' order Play 클래스의 메소드 배치 순서 변경 & 메소드별 주석 추가

--- src/main/java/baseball/Play.java
+++ src/main/java/baseball/Play.java
@@ -2,7 +2,7 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class Play {
+public class Play { // 숫자 야구 게임 진행 관련 클래스
 
     private final User user;
     private final Computer com;
@@ -14,42 +14,18 @@ public class Play {
         rules = new Rules();
     }
 
-    public void numberBaseBallGame() {
+    public void numberBaseBallGame() {  // 숫자 야구 게임 실행
         startGame();
         runGame();
         restartOrFinishGame();
     }
 
-    private void startGame() {
+    private void startGame() {  // 게임 시작 단계
         System.out.println("숫자 야구 게임을 시작합니다.");
         com.setCom();
     }
 
-    private String setInput(String msg) {
-        System.out.print(msg);
-        String temp = Console.readLine();
-        checkInputHasChar(temp);
-        return temp;
-    }
-
-    private void checkInputHasChar(String input) {
-        try {
-            int temp = Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("This input is not Integer: " + input);
-        }
-    }
-
-    private int checkMenuNumber(String input) {
-        checkInputHasChar(input);
-        int menuNum = Integer.parseInt(input);
-        if (menuNum < 1 || menuNum > 2) {
-            throw new IllegalArgumentException("Invalid Menu Number: " + menuNum);
-        }
-        return menuNum;
-    }
-
-    private void runGame() {
+    private void runGame() {    // 게임 진행 단계
         while (true) {
             String input = setInput("숫자를 입력해주세요 : ");
             user.setUser(input);
@@ -63,7 +39,25 @@ public class Play {
         }
     }
 
-    private void restartOrFinishGame() {
+    private String setInput(String msg) {   // 입력값을 문자열 형태로 저장
+        System.out.print(msg);              // 입력 메시지 출력
+        String temp = Console.readLine();
+        checkInputHasChar(temp);            // 첫번째, 두번째 입력 모두 문자 없는 숫자만 필요
+        return temp;
+    }
+
+    // 입력값에서 숫자 이외 문자가 포함되면 IllegalArgumentException 발생
+    // 두번의 입력(사용자의 숫자, 재시작) 모두 숫자만 필요하므로 공통으로 수행하는 유효성 검사
+    private void checkInputHasChar(String input) {
+        try {
+            int temp = Integer.parseInt(input); // 입력값에 문자가 섞이면 Exception 발생
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("This input is not Integer: " + input);
+        }
+    }
+
+    private void restartOrFinishGame() {    // 게임 재시작 or 종료 단계
+        // 게임 재시작을 2회 이상 수행하려면 반복문 필수
         while (true) {
             String input = setInput("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
             int menuNum = checkMenuNumber(input);
@@ -76,6 +70,18 @@ public class Play {
         }
     }
 
+    // 재시작 메뉴 번호 입력 시 1, 2 이외의 숫자가 입력되면 IllegalArgumentException 발생
+    private int checkMenuNumber(String input) {
+        checkInputHasChar(input);
+        int menuNum = Integer.parseInt(input);
+        if (menuNum < 1 || menuNum > 2) {
+            throw new IllegalArgumentException("Invalid Menu Number: " + menuNum);
+        }
+        return menuNum;
+    }
+
+    // 정답 이후 재시작 시 사용자의 숫자 & 상대방의 숫자를 모두 초기화한 후 게임 진행
+    // 사용자 입력값을 초기화하지 않으면 재시작 직후 입력값에 대해 잘못된 결과 출력
     private void restartGame() {
         user.clearUser();
         com.clearCom();
