4da94d934f03dfd2167942a12baa893f99a25890
박정현 <ros28081@naver.com>
2023-10-22 16:27:41 +0900

    feat: Game 클래스에 SIZE, MIN_NUM, MAX_NUM 상수 추가

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,10 @@
+package baseball;
+
+public class Game {
+
+    // generateNum
+    public static final int SIZE = 3;
+    public static final int MIN_NUM = 3;
+    public static final int MAX_NUM = 3;
+    
+}
