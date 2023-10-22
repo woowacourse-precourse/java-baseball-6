8ece0b225cbc4bf1c1876188404ee8a7dff87261
swoolee97 <swoolee97@ajou.ac.kr>
2023-10-22 13:33:51 +0900

    refactor: 결과를 문자열로 변환하는 메서드 책임 분리

--- src/main/java/baseball/util/JudgeResultFormatter.java
+++ src/main/java/baseball/util/JudgeResultFormatter.java
@@ -9,16 +9,24 @@ public class JudgeResultFormatter {
             return Constants.NOTHING_STRING;
         }
         StringBuilder sb = new StringBuilder();
+        formatBall(judgeResult, sb);
+        formatStrike(judgeResult, sb);
+        return sb.toString();
+    }
+
+    private static void formatBall(JudgeResult judgeResult, StringBuilder sb) {
         if (judgeResult.hasBall()) {
             sb.append(judgeResult.getBallCount());
             sb.append(Constants.BALL_STRING);
             sb.append(Constants.SPACE);
         }
+    }
+
+    private static void formatStrike(JudgeResult judgeResult, StringBuilder sb) {
         if (judgeResult.hasStrike()) {
             sb.append(judgeResult.getStrikeCount());
             sb.append(Constants.STRIKE_STRING);
         }
-        return sb.toString();
     }
 
 }
