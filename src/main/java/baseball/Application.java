8a05193df572d277631ebfc4123dba1f20f3354d
HeeHeeg <qmffndoden@naver.com>
2023-10-23 00:37:57 +0900

    게임을 다시 시작하거나 완전히 종료할 수 있는 기능 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,7 +9,25 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        while (true) {
 
+            System.out.println("숫자 야구 게임을 시작합니다.");
+            playGame();
+
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String choice = Console.readLine();
+            int pick = Integer.parseInt(choice);
+            if (pick == 1) {
+                continue;
+            } else if (pick == 2) {
+                break;
+            } else {
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            }
+        }
+    }
+
+    public static void playGame() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -17,9 +35,8 @@ public class Application {
                 computer.add(randomNumber);
             }
         }
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        System.out.print("숫자를 입력해주세요 : ");
         while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
             String input = Console.readLine();
             char[] player = input.toCharArray();
             int strike = 0, ball = 0;
@@ -32,27 +49,16 @@ public class Application {
                     ball++;
                 }
             }
-            if (strike != 0 || ball != 0) {
-                if (strike == 3 && ball == 0) {
-                    System.out.println(strike + "스트라이크");
-                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                    String choice = Console.readLine();
-                    int pick = Integer.parseInt(choice);
-                    if (pick == 1) {
-                        main(args);
-                    } else if (pick == 2) {
-                        break;
-                    } else {
-                        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                    }
-                } else if (strike == 0 && ball != 0) {
-                    System.out.println(ball + "볼");
-                } else if (strike != 0 && ball == 0) {
-                    System.out.println(strike + "스트라이크");
-                } else {
-                    System.out.println(strike + "스트라이크, " + ball + "볼");
-                }
+            if (strike == 3 && ball == 0) {
+                System.out.println(strike + "스트라이크");
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            } else if (strike == 0 && ball != 0) {
+                System.out.println(ball + "볼");
+            } else if (strike != 0 && ball == 0) {
+                System.out.println(strike + "스트라이크");
+            } else if (strike != 0 || ball != 0) {
+                System.out.println(ball + "볼" + " "+strike + "스트라이크, ");
             } else {
                 System.out.println(nothing);
             }
@@ -61,3 +67,4 @@ public class Application {
         }
     }
 }
+
