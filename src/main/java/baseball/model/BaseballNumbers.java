3b4c795fff8e8b70d9095c13dcd002d55ed050fd
devhoya97 <diajbh123@gmail.com>
2023-10-22 16:31:17 +0900

    refactor : BaseballNumbers 클래스에 역할 위임 BaesballNumber를 담고 있는 리스트는, 자신이 갖고 있는 값들 중 중복된 값들이 없으며, 자신의 길이가 올바른지 스스로 관리할 책임이 있습니다. 따라서 기존에 List<BaseballNumbers> 형태로 사용하던 객체들을 클래스화 시켜 본인의 상태를 스스로 관리하도록 역할분담 하였습니다. 또한 볼카운트, 스트라이크카운트를 계산할 때, index에 맞는 Baseball을 꺼내오는 기능이 필요하므로 getBaseballNumber 메서드도 추가했습니다

---
+++ src/main/java/baseball/model/BaseballNumbers.java
@@ -0,0 +1,29 @@
+package baseball.model;
+
+import java.util.List;
+
+public class BaseballNumbers {
+    private static final int BASEBALL_NUMBERS_LENGTH = 3;
+    private final List<BaseballNumber> baseballNumbers;
+
+    public BaseballNumbers(List<BaseballNumber> baseballNumbers) {
+        validateBaseballNumbers(baseballNumbers);
+        this.baseballNumbers = baseballNumbers;
+    }
+
+    private void validateBaseballNumbers(List<BaseballNumber> baseballNumbers) {
+        if (baseballNumbers.size() != BASEBALL_NUMBERS_LENGTH) {
+            throw new IllegalArgumentException("[ERROR] 숫자야구 값의 길이가 " + BASEBALL_NUMBERS_LENGTH + "(이)가 아닙니다.");
+        }
+        List<BaseballNumber> distinctBaseballNumbers = baseballNumbers.stream()
+                .distinct()
+                .toList();
+        if (baseballNumbers.size() != distinctBaseballNumbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 숫자야구 값에 중복 값이 있습니다.");
+        }
+    }
+
+    public BaseballNumber getBaseballNumber(int index) {
+        return baseballNumbers.get(index);
+    }
+}
