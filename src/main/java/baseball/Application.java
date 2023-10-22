0da7ce96c69c32266c6d7fe1ee52b980dae39d63
JongukYang <didwhddnr123@naver.com>
2023-10-22 16:42:57 +0900

    Fix: Todo 삭제!!

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,7 +4,6 @@ import baseball.game.Game;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         Game game = new Game();
         try {
             game.startGame();
