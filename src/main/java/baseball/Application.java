7a761b1f999b04ee56fbb5f4a60dd2e7c7d8d4fc
Seyeon <wjdtpdus828@naver.com>
2023-10-22 21:48:38 +0900

    Revert "feat(Model): controller에게 게임 시작 신호 구현"
    This reverts commit 9acd5abca5a9df011ef580843f9e33faf9d96fcf.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,7 +2,6 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        Controller controller = new Controller();
-        controller.gameStart();
+        // TODO: 프로그램 구현
     }
 }
