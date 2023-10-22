d9eb8612d0e0cf8e4ed5f1f88885edeafa6d58e2
kunsanglee <gosmdochee2@gmail.com>
2023-10-22 19:20:10 +0900

    refactor(AnswerTest): 테스트코드 내 클래스명 변경

--- src/test/java/baseball/domain/AnswerTest.java
+++ src/test/java/baseball/domain/AnswerTest.java
@@ -3,7 +3,6 @@ package baseball.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -15,16 +14,28 @@ public class AnswerTest {
     @DisplayName("Answer의 match()를 호출하면 입력값에 맞는 Score를 반환한다.")
     @ParameterizedTest
     @MethodSource("provideMatchTestArguments")
-    void matchTest(Answer answer, String input, Score expectedScore) {
-        assertThat(answer.match(input)).isEqualTo(expectedScore);
+    void matchTest(Answer answer, BaseballNumbers inputBaseballNumbers, Score expectedScore) {
+        assertThat(answer.match(inputBaseballNumbers)).isEqualTo(expectedScore);
     }
 
     static Stream<Arguments> provideMatchTestArguments() {
         return Stream.of(
-                arguments(new Answer(new Numbers(List.of(1, 2, 3))), "321", new Score(new Ball(2), new Strike(1))),
-                arguments(new Answer(new Numbers(List.of(4, 5, 6))), "564", new Score(new Ball(3), new Strike(0))),
-                arguments(new Answer(new Numbers(List.of(4, 5, 6))), "321", new Score(new Ball(0), new Strike(0))),
-                arguments(new Answer(new Numbers(List.of(3, 2, 1))), "321", new Score(new Ball(0), new Strike(3)))
+                arguments(setAnswer(1, 2, 3), setInputBaseballNumbers(3, 2, 1), setExpectedScore(2, 1)),
+                arguments(setAnswer(4, 5, 6), setInputBaseballNumbers(5, 6, 4), setExpectedScore(3, 0)),
+                arguments(setAnswer(4, 5, 6), setInputBaseballNumbers(3, 2, 1), setExpectedScore(0, 0)),
+                arguments(setAnswer(3, 2, 1), setInputBaseballNumbers(3, 2, 1), setExpectedScore(0, 3))
         );
+    }
+
+    private static Score setExpectedScore(int ball, int strike) {
+        return new Score(new Ball(ball), new Strike(strike));
+    }
+
+    private static BaseballNumbers setInputBaseballNumbers(int x, int x1, int x2) {
+        return new BaseballNumbers(Stream.of(x, x1, x2).map(BaseballNumber::new).toList());
+    }
+
+    private static Answer setAnswer(int x, int x1, int x2) {
+        return new Answer(setInputBaseballNumbers(x, x1, x2));
     }
 }
