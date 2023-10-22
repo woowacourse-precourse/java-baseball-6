6235f0b928095e9f69c8781a0ccf33683367d705
kunsanglee <gosmdochee2@gmail.com>
2023-10-22 12:45:40 +0900

    refactor(Score): Score 생성시 볼과 스트라이크 타입 변경 기존에 int 타입으로 받던 매개변수를 Ball과 Strike를 생성하여 넣어주는 것으로 변경. 그에따라 볼과 스트라이크의 카운트를 검증하던 로직에서 Ball과 Strike의 getValue() 메서드 호출.

--- src/main/java/baseball/domain/Score.java
+++ src/main/java/baseball/domain/Score.java
@@ -4,22 +4,22 @@ package baseball.domain;
 import java.util.Objects;
 
 public class Score {
-    private static final String NO_BALL_NO_STRIKE_MESSAGE = "낫싱";
-    private static final String SPACE = " ";
     private static final int MIN_COUNT = 0;
     private static final int MAX_COUNT = 3;
     private static final String TOTAL_COUNT_RANGE_EXCEPTION_MESSAGE = "볼과 스트라이크 갯수의 합은 0 이상 3 이하여야 합니다.";
+    private static final String NO_BALL_NO_STRIKE_MESSAGE = "낫싱";
+    private static final String SPACE = " ";
     private final Ball ball;
     private final Strike strike;
 
-    public Score(int ball, int strike) {
+    public Score(Ball ball, Strike strike) {
         validateTotalCount(ball, strike);
-        this.ball = new Ball(ball);
-        this.strike = new Strike(strike);
+        this.ball = ball;
+        this.strike = strike;
     }
 
-    private static void validateTotalCount(int ball, int strike) {
-        int totalCount = ball + strike;
+    private static void validateTotalCount(Ball ball, Strike strike) {
+        int totalCount = ball.getValue() + strike.getValue();
         if (totalCount < MIN_COUNT || MAX_COUNT < totalCount) {
             throw new IllegalArgumentException(TOTAL_COUNT_RANGE_EXCEPTION_MESSAGE);
         }
