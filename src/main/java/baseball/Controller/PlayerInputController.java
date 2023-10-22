0d176134fd138bcf0c28c800a9c7bce3bb28ec1c
sungwon2598 <tjddnjs2598@gmail.com>
2023-10-22 10:02:25 +0900

    refactor(PlayerInputController) : 매직넘버 제거 및 상수로 변환

--- src/main/java/baseball/Controller/PlayerInputController.java
+++ src/main/java/baseball/Controller/PlayerInputController.java
@@ -3,6 +3,7 @@ package baseball.Controller;
 import java.util.ArrayList;
 
 public class PlayerInputController {
+    private static final int PLAYER_INPUT_LENGTH = 3;
 
     Validator validator = new Validator();
 
@@ -10,7 +11,7 @@ public class PlayerInputController {
         validator.validatePlayerInput(playerFirstInput);
         String[] numbers = playerFirstInput.replace(" ", "").split("");
         ArrayList<Integer> playerInput = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < PLAYER_INPUT_LENGTH; i++) {
             playerInput.add(Integer.valueOf(numbers[i]));
         }
         return playerInput;
