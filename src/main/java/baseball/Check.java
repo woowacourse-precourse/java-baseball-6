f529df305234f4e8ba734f5dd61d6c12141e2385
stop-zero <jungjy0097@naver.com>
2023-10-23 02:40:51 +0900

    :hammer: fix: if(), Java convention

--- src/main/java/baseball/Check.java
+++ src/main/java/baseball/Check.java
@@ -4,15 +4,14 @@ import java.util.List;
 
 public class Check {
     public int countHits(List<Integer> computer, List<Integer> player) {
-        int hits = 0;
+        int hist = 0;
 
         for (int i = 0; i < player.size(); i++) {
             if (computer.contains(player.get(i))) {
-                hits++;
+                hist++;
             }
         }
-
-        return hits;
+        return hist;
     }
 
     public int countStrikes(List<Integer> computer, List<Integer> player) {
@@ -23,25 +22,24 @@ public class Check {
                 strikes++;
             }
         }
-
         return strikes;
     }
 
-    public String classifyResult(List<Integer> computer, List<Integer> player) {
+    public String Classification(List<Integer> computer, List<Integer> player) {
         int hits = countHits(computer, player);
         int strikes = countStrikes(computer, player);
         int balls = hits - strikes;
+        String result;
 
-        if (strikes == 0) {
-            if (balls == 0) {
-                return "낫싱";
-            } else {
-                return balls + "볼";
-            }
+        if (hits == 0) {
+            result = "낫싱";
+        } else if (strikes == 0) {
+            result = balls + "볼";
         } else if (balls == 0) {
-            return strikes + "스트라이크";
+            result = strikes + "스트라이크";
         } else {
-            return balls + "볼" + strikes + "스트라이크";
+            result = balls + "볼 " + strikes + "스트라이크";
         }
+        return result;
     }
 }
