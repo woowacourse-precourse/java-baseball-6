2c79d861cf2004f9b24ab5db85553e2a9e870040
mary <jogrowing@gmail.com>
2023-10-22 19:11:23 +0900

    feat: 에러 메세지 추가

---
+++ src/main/java/baseball/constants/ErrorMessage.java
@@ -0,0 +1,9 @@
+package baseball.constants;
+
+public class ErrorMessage {
+    public static final String DUPLICATED = "중복된 숫자는 입력할 수 없습니다.";
+
+    public static final String NOT_ENOUGH_LENGTH = "1-9까지의 3자리 숫자를 입력해주세요.";
+    public static final String NOT_RESTART_OPTION = "1, 2 중에 한 개만 입력해주세요.";
+
+}
