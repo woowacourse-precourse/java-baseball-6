b982216f4769bca88c99b855fe95bc6d22b4d1d0
GO-TE <alsrudxo9810@naver.com>
2023-10-22 21:56:47 +0900

    Feat: 3개의 숫자 맞추기 게임 진행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
 
+
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Validator validator = new Validator();
+        validator.startGame();
     }
 }
