3c0e8c94b6d92fb976fd5f3a2f72d485209c89f2
Hyeon-hak Kim <07ily@naver.com>
2023-10-22 11:24:20 +0900

    Feat: Computer.getNewAnswer

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
@@ -13,4 +15,14 @@ public class Computer {
         this.answer = getNewAnswer();
     }
 
+    private List<Integer> getNewAnswer() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
 }
