c75879f4d12fc51c1a662fc1e279819f97594b08
hyeonnkang <hyeonkang.def@gmail.com>
2023-10-22 23:54:06 +0900

    게임 시작 문구 출력 및 컴퓨터가 숫자 3개 고르기 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Application {
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        // 컴퓨터가 숫자 3개를 고른다.
+        List<Integer> computer = chooseNumByComputer();
+        
+    }
+
+    private static List<Integer> chooseNumByComputer() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 }
