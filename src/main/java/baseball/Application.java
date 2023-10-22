4cbfeb2a59ca83da7f8a125439b11ea797ccd15c
eunjin3481 <eunjin3481@gmail.com>
2023-10-22 21:28:07 +0900

    Feat: askExit 메서드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -14,7 +14,12 @@ public class Application {
             runFlag = askExit();
         }
     }
+
     public static boolean askExit(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        int userInput = Integer.parseInt(readLine());
+        // 예외처리
+        return userInput == 1;
     }
 }
 }
