abad9cf5485967973ee5a3d129a697ce103709a5
k-kmw <kimmw2_@naver.com>
2023-10-22 09:41:03 +0900

    Feat: 스트라이크와 볼의 개수에 따른 출력 및 재시작/종료 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -51,6 +51,30 @@ public class Application {
                     }
                 }
             }
+
+            /**
+             * 스트라이크와 볼의 개수에 따라 출력
+             * 스트라이크 3개 -> 새로운 게임 및 종료
+             */
+            if(strike == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                gameStatus = Console.readLine();
+                continue;
+            }
+
+            if(strike != 0 && ball != 0) {
+                System.out.println(ball + "볼" + " " + strike + "스트라이크");
+            }
+            else if(strike != 0) {
+                System.out.println(strike + "스트라이크");
+            }
+            else if(ball != 0) {
+                System.out.println(ball + "볼");
+            }
+            else {
+                System.out.println("낫싱");
+            }
         }
     }
 }
