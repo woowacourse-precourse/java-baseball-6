0d46395103188be79c59f555bc7186bc2cc73a85
ahma0 <dsd932@naver.com>
2023-10-22 16:10:53 +0900

    refactor: Application.java 내에 있는 메소드 위치 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,37 +1,11 @@
 package baseball;
 
 import baseball.game.RunGame;
-import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
 
     public static void main(String[] args) {
-
-        printStart();
-
-        while(true) {
-            new RunGame();
-            if(!wantToRestartOrEnd())
-                break;
-        }
-
+        new RunGame();
     }
 
-    public static void printStart() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
-    }
-
-    public static boolean wantToRestartOrEnd() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        String answer = Console.readLine();
-
-        if(!isRightAnswer(answer))
-            throw new IllegalArgumentException("잘못된 수를 입력하였습니다.");
-
-        return answer.equals("1");      //1일 경우 게임을 재시작합니다.
-    }
-
-    public static boolean isRightAnswer(String answer) {
-        return answer.equals("1") || answer.equals("2");
-    }
 }
--- src/main/java/baseball/game/RunGame.java
+++ src/main/java/baseball/game/RunGame.java
@@ -5,7 +5,17 @@ import camp.nextstep.edu.missionutils.Console;
 public class RunGame {
 
     public RunGame() {
-        runBaseBallGame();
+        startGame();
+    }
+
+    private static void startGame() {
+        printStart();
+
+        while(true) {
+            runBaseBallGame();
+            if(!wantToRestartOrEnd())
+                break;
+        }
     }
 
     private static void runBaseBallGame() {
@@ -18,6 +28,24 @@ public class RunGame {
             if(Game.printResult())
                 break;
         }
+    }
+
+    public static void printStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static boolean wantToRestartOrEnd() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String answer = Console.readLine();
+
+        if(!isRightAnswer(answer))
+            throw new IllegalArgumentException("잘못된 수를 입력하였습니다.");
+
+        return answer.equals("1");      //1일 경우 게임을 재시작합니다.
+    }
+
+    public static boolean isRightAnswer(String answer) {
+        return answer.equals("1") || answer.equals("2");
     }
 
     private static String getUserAnswer() {
