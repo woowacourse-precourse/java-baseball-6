a0c7f62026a81aec8e86a2e3cd443b4d43a1dcb8
hunko <rhwlgns4386@naver.com>
2023-10-22 23:40:25 +0900

    refactor : expect class
    Extract the string logic of BaseBallNumberList into NumericString

--- src/main/java/baseball/domain/BaseBallNumberList.java
+++ src/main/java/baseball/domain/BaseBallNumberList.java
@@ -4,32 +4,16 @@ import java.util.List;
 
 public class BaseBallNumberList {
 
-    private BaseBallNumberList(List<Integer> list) {
-        if (list.size() != 3) {
+    private final List<Integer> numbers;
+
+    public BaseBallNumberList(List<Integer> numbers) {
+        this.numbers = numbers;
+        validateSize();
+    }
+
+    private void validateSize() {
+        if (this.numbers.size() != 3) {
             throw new IllegalArgumentException("숫자를 3개를 입력해주세요");
-        }
-    }
-
-    public static BaseBallNumberList from(String numericInput) {
-        validateNumericInput(numericInput);
-
-        return new BaseBallNumberList(getList(numericInput));
-    }
-
-    public static BaseBallNumberList from(List<Integer> baseBallNumbers) {
-        return new BaseBallNumberList(baseBallNumbers);
-    }
-
-    private static List<Integer> getList(String numericString) {
-        return numericString
-                .chars()
-                .mapToObj(Character::getNumericValue)
-                .toList();
-    }
-
-    private static void validateNumericInput(String numericString) {
-        if (!numericString.chars().allMatch(Character::isDigit)) {
-            throw new IllegalArgumentException("숫자만을 입력하셔야 합니다.");
         }
     }
 }
---
+++ src/main/java/baseball/domain/NumericString.java
@@ -0,0 +1,26 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class NumericString {
+
+    private final String numeric;
+
+    public NumericString(String numeric) {
+        this.numeric = numeric;
+        validate();
+    }
+
+    public List<Integer> toList() {
+        return this.numeric
+                .chars()
+                .mapToObj(Character::getNumericValue)
+                .toList();
+    }
+
+    private void validate() {
+        if (!this.numeric.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException("숫자만을 입력하셔야 합니다.");
+        }
+    }
+}
