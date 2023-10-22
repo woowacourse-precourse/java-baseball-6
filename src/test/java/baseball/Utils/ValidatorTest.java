73a70674c3ad986c2677653b09f5bd1706c89a48
Firedrago95 <yonghwa95@naver.com>
2023-10-22 12:26:30 +0900

    test Validator 테스트 작성

--- src/test/java/baseball/Utils/ValidatorTest.java
+++ src/test/java/baseball/Utils/ValidatorTest.java
@@ -1,19 +1,20 @@
-package baseball.View;
+package baseball.Utils;
 
 import baseball.Util.Validator;
+import baseball.View.InputView;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class InputViewTest {
+public class ValidatorTest {
 
     @Test
     @DisplayName("null 유효성검사 테스트")
     public void isNullTest() {
         Throwable exception = assertThrows(IllegalArgumentException.class,
-                () -> InputView.validateInput(null));
+                () -> Validator.isNullOrEmpty(null));
         assertEquals(Validator.NULL_OR_EMPTY_MESSAGE, exception.getMessage());
     }
 
@@ -21,7 +22,7 @@ public class InputViewTest {
     @DisplayName("빈 문자열 유효성검사 테스트")
     public void isEmptyTest() {
         Throwable exception = assertThrows(IllegalArgumentException.class,
-                () -> InputView.validateInput(Validator.EMPTY));
+                () -> Validator.isNullOrEmpty(Validator.EMPTY));
         assertEquals(Validator.NULL_OR_EMPTY_MESSAGE, exception.getMessage());
     }
 
@@ -29,7 +30,7 @@ public class InputViewTest {
     @DisplayName("세자리 숫자 유효성검사 테스트")
     public void isThreeDigitNumberTest() {
         Throwable exception = assertThrows(IllegalArgumentException.class,
-                () -> InputView.validateInput("2222"));
+                () -> Validator.isThreeDigitNumber("2222"));
         assertEquals(Validator.NOT_THREE_DIGIT_NUMBER_MESSAGE, exception.getMessage());
     }
 
@@ -37,7 +38,15 @@ public class InputViewTest {
     @DisplayName("숫자 중복 유효성검사 테스트")
     public void isUniqueNumberTest() {
         Throwable exception = assertThrows(IllegalArgumentException.class,
-                () -> InputView.validateInput("222"));
+                () -> Validator.isUniqueNumber("222"));
         assertEquals(Validator.NOT_UNIQUE_NUMBER_MESSAGE, exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("1 혹은 2 유효성테스트")
+    public void isOneOrTwoTest() {
+        Throwable exception = assertThrows(IllegalArgumentException.class,
+                () -> Validator.isOneOrTwo("3"));
+        assertEquals(Validator.NOT_ONE_OR_TWO, exception.getMessage());
     }
 }
