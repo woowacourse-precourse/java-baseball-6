2b52d05e52eed4a8b985d20bb1009f7943350d34
toychip <dlawnsgud427@naver.com>
2023-10-22 21:52:16 +0900

    feat: 게임 결과 String 반환 추가

--- src/main/java/baseball/model/GameJudge.java
+++ src/main/java/baseball/model/GameJudge.java
@@ -2,17 +2,21 @@ package baseball.model;
 
 import java.util.List;
 
-public class GameScore {
+public class GameJudge {
 
     private final List<Integer> userNumber;
     private final List<Integer> computerNumber;
 
-    private GameScore(List<Integer> userNumber, List<Integer> computerNumber) {
+    private GameJudge(List<Integer> userNumber, List<Integer> computerNumber) {
         this.userNumber = userNumber;
         this.computerNumber = computerNumber;
     }
 
-    public void judge() {
+    public static GameJudge gameJudgeFactory(List<Integer> userNumber, List<Integer> computerNumber) {
+        return new GameJudge(userNumber, computerNumber);
+    }
+
+    public GameResult judge() {
         int strikeCount = 0;
         int ballCount = 0;
 
@@ -26,7 +30,7 @@ public class GameScore {
             }
         }
 
-        // todo 볼과 스트라이크 카운트로 결과를 정할 객체 필요
+        return GameResult.gameResultFactory(strikeCount, ballCount);
     }
 
     private boolean isStrike(int index) {
