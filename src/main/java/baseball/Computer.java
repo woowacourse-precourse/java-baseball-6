e4ae1cb9d7201e8f2e93b5b2759d780ae02d082f
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-10-22 16:04:57 +0900

    feat: 게임 종료 시 문구 출력
    - Computer Class에 게임 종료 시 재시작 및 종료 문구 출력

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -25,4 +25,8 @@ public class Computer {
     public void inputNumberPhrase() {
         System.out.print("숫자를 입력해주세요 : ");
     }
+
+    public void restartOrEndPhrase() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
