4b5ba3bfb9e7a6097b4cf6018399264d89e13fd2
Jiwoo Hong <hjw6112@gmail.com>
2023-10-22 22:21:03 +0900

    feat: JAVABASEBALL set GameService

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -1,4 +1,33 @@
 package baseball.service;
 
+import baseball.domain.Game;
+import baseball.repository.GameRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 public class GameService {
+    private final GameRepository gameRepository;
+
+    public GameService(GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
+
+    /* 새로운 게임 생성 */
+    public Long join(Game game){
+        gameRepository.save(game);
+
+        return game.getId();
+    }
+
+    /* 특정 게임 조회 */
+    public Optional<Game> findOne(Long gameId) {
+        return gameRepository.findById(gameId);
+    }
+
+    /* 특정 유저 id로 진행된 게임 리스트 조회 */
+    public List<Game> findGame(Long userId){
+        return gameRepository.findByUserId(userId).toList();
+    }
+
 }
