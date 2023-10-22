1f840059f7dd577eedadc8302279eb4858f72275
kimdoha <kkhhjj3393@gmail.com>
2023-10-22 23:51:23 +0900

    feat(player): ComputerPlayer 객체 정의 및 랜덤 세 자리 수 추출 구현

---
+++ src/main/java/baseball/player/ComputerPlayer.java
@@ -0,0 +1,26 @@
+package baseball.player;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashSet;
+
+public class ComputerPlayer {
+    private static final int startInclusive = 1;
+    private static final int endInclusive = 9;
+    private static final int digits = 3;
+    private HashSet<Integer> computer = new HashSet<>();
+
+    public ComputerPlayer() {
+        pickNumber();
+    }
+
+    public void pickNumber() {
+        while(computer.size() < digits) {
+            int randomNum = Randoms.pickNumberInRange(startInclusive, endInclusive);
+            computer.add(randomNum);
+        }
+    }
+
+    public void clear() {
+        this.computer.clear();
+    }
+}
