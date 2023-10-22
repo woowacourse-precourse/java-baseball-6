56b0c2fd225213f37b7b11b6574b93429581b07f
ssunnykku <ssunnykku@gmail.com>
2023-10-22 21:18:04 +0900

    feat: 게임 종료 후 재시작 or 종료하기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,11 +11,18 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
-
         List<Integer> computer = selectNumbers();
-        String input = inputNumbers();
+        game(computer);
+    }
 
-        isStrikeOrBall(computer, input);
+    public static String inputNumbers() {
+        System.out.print("숫자를 입력해주세요: ");
+        return Console.readLine();
+    }
+
+    public static String inputResultNumber() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return Console.readLine();
     }
 
     public static List<Integer> selectNumbers() {
@@ -29,16 +36,18 @@ public class Application {
         return computer;
     }
 
-    public static String inputNumbers() {
-        System.out.print("숫자를 입력해주세요: ");
-        return Console.readLine();
+
+    public static void game(List<Integer> computer) {
+
+        String input = inputNumbers();
+        isStrikeOrBall(computer, input);
     }
 
     public static void isStrikeOrBall(List<Integer> computer, String input) {
         int cntStrike = 0;
         int cntBall = 0;
         for(int i=0; i<computer.size(); i++) {
-            int num = Character.getNumericValue(input.charAt(i));
+            int num = stringToInt(input, i);
             if((num) == computer.get(i)) {
                 cntStrike += 1;
             } else if (computer.contains(num)) {
@@ -46,14 +55,40 @@ public class Application {
             }
         }
         gameResult(cntStrike, cntBall);
+
+        if (cntStrike == 3 && cntBall == 0) {
+            restartOrNot(computer);
+        } else {
+            game(computer);
+        }
+    }
+
+    public static int stringToInt(String str, int i) {
+        int num = Character.getNumericValue(str.charAt(i));
+        return num;
     }
 
     public static void gameResult(int cntStrike, int cntBall) {
         if (cntStrike == 0 && cntBall == 0) System.out.println("낫싱");
-        else if (cntStrike > 0 && cntBall == 0) System.out.println(cntStrike + "스트라이크");
         else if (cntStrike == 0 && cntBall > 0 ) System.out.println(cntBall + "볼");
-        else if (cntStrike != 0 && cntBall != 0) System.out.println(cntBall + "볼 "+ cntStrike + "스트라이크");
+        else if (cntStrike > 0 && cntBall > 0) System.out.println(cntBall + "볼 "+ cntStrike + "스트라이크");
+        else if (cntStrike > 0 && cntBall == 0 ) {
+            System.out.println(cntStrike + "스트라이크");
+            if (cntStrike ==3) System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
 
     }
+
+    public static void restartOrNot(List<Integer> computer) {
+        int num = stringToInt(inputResultNumber(),0);
+        if (num == 1) gameRestart();
+    }
+
+
+    public static void gameRestart() {
+        List<Integer> computer = selectNumbers();
+        game(computer);
+    }
+
 
 }
