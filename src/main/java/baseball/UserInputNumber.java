81526457ab4dfaed8030ea530c11c0630fa0f13d
munis <aaaa6658@naver.com>
2023-10-22 23:51:41 +0900

    feat : 사용자 입력 검증 및 배열 변환을 위해 UserInputNumber 클래스 생성

---
+++ src/main/java/baseball/UserInputNumber.java
@@ -0,0 +1,9 @@
+package baseball;
+
+public class UserInputNumber {
+    private final UserInputValidation userInputValidation;
+
+    public UserInputNumber() {
+        userInputValidation = new UserInputValidation();
+    }
+}
