3eeb36232454c53e1e21078720307f17598c0d65
handwoong <handwoong@gmail.com>
2023-10-22 23:11:45 +0900

    feat: add string is numeric validator
    Add validator
    - string is numeric

--- src/main/java/baseball/validation/Validator.java
+++ src/main/java/baseball/validation/Validator.java
@@ -4,4 +4,13 @@ public class Validator {
     public static boolean isSize(final String target, final int size) {
         return target.length() == size;
     }
+
+    public static boolean isDigit(final String target) {
+        for (char c : target.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
