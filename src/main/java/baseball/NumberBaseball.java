512af3e217d1b257af43029884c722699baebfbe
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-10-23 02:14:24 +0900

    refactor : baseballGame 메서드 쪼개기
    - countStrike : 스트라이크 개수
    - countBall : 볼 개수
    - generateResultMessage : strike, ball 개수에 따라 메세지 리턴

--- src/main/java/baseball/NumberBaseball.java
+++ src/main/java/baseball/NumberBaseball.java
@@ -1,48 +1,53 @@
 package baseball;
 
-import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class NumberBaseball {
 
-    public static String baseballGame(String input, String answer){
-        int strike = 0;
-        int ball = 0;
+  public static String baseballGame(String input, String answer) {
+    Set<Character> answerSet = createAnswerSet(answer);
+    int strike = countStrike(input, answer);
+    int ball = countBall(input, answerSet, strike);
 
-        Set<Character> answerSet = createanswerSet(answer);
+    return generateResultMessage(strike, ball);
+  }
 
-        for (int i = 0; i < 3; i++){
-            if (input.charAt(i) == answer.charAt(i)){
-                strike++;
-            }
-            else if (answerSet.contains(input.charAt(i))){
-                ball++;
-            }
-        }
-
-        if (ball == 0 && strike != 0){
-            return (strike + "스트라이크");
-        }
-        else if (strike == 0 && ball != 0){
-            return (ball + "볼");
-        }
-        else if (ball == 0 && strike == 0){
-            return "낫싱";
-        }
-        else if (ball > 0 && strike > 0){
-            return (ball + "볼 " + strike + "스트라이크");
-        }
-        return "";
+  private static int countStrike(String input, String answer) {
+    int strike = 0;
+    for (int i = 0; i < 3; i++) {
+      if (input.charAt(i) == answer.charAt(i)) {
+        strike++;
+      }
     }
+    return strike;
+  }
 
-    private static Set<Character> createanswerSet(String answer){
-        Set<Character> answerSet = new HashSet<>();
-        for (char x : answer.toCharArray()){
-            answerSet.add(x);
-        }
-        return answerSet;
+  private static int countBall(String input, Set<Character> answerSet, int strike) {
+    int ball = 0;
+    for (char c : input.toCharArray()) {
+      if (answerSet.contains(c)) {
+        ball++;
+      }
     }
+    return ball - strike;
+  }
 
+  public static String generateResultMessage(int strike, int ball) {
+    if (strike == 0 && ball == 0) {
+      return "낫싱";
+    } else if (strike == 0) {
+      return ball + "볼";
+    } else if (ball == 0) {
+      return strike + "스트라이크";
+    } else {
+      return ball + "볼 " + strike + "스트라이크";
+    }
+  }
+
+  private static Set<Character> createAnswerSet(String answer) {
+    return answer.chars().mapToObj(c -> (char) c).collect(Collectors.toSet());
+  }
 
 
 }
