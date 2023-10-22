2697427a29ea9d47fef262a86e4869f38c26f5a6
NAM GIBEOM <rlqja0523@naver.com>
2023-10-22 16:54:10 +0900

    feat(Converter): int를 list로 변환하는 기능 추가

--- src/main/java/baseball/converter/ValueConverter.java
+++ src/main/java/baseball/converter/ValueConverter.java
@@ -1,5 +1,8 @@
 package baseball.converter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ValueConverter {
     public int convertToInt(String number) {
         try {
@@ -7,5 +10,14 @@ public class ValueConverter {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자가 아닙니다.");
         }
+    }
+
+    public List<Integer> convertToList(int number) {
+        List<Integer> digits = new ArrayList<>();
+        while (number > 0) {
+            digits.add(0, number % 10);
+            number /= 10;
+        }
+        return digits;
     }
 }
