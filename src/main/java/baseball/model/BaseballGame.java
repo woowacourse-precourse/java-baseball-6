6cbb437e16f1889bfb0b88cd3c67703e629e3acb
handwoong <handwoong@gmail.com>
2023-10-23 02:34:44 +0900

    feat: initialize baseball game score board
    Initialize baseball game score board
    - add score enum
    - add initialize score board method

--- src/main/java/baseball/model/BaseballGame.java
+++ src/main/java/baseball/model/BaseballGame.java
@@ -1,7 +1,13 @@
 package baseball.model;
 
+import static baseball.model.BallType.BALL;
+import static baseball.model.BallType.NOTHING;
+import static baseball.model.BallType.STRIKE;
+
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
@@ -9,8 +15,9 @@ public class BaseballGame {
     public static final int MIN_BALL = 1;
     public static final int MAX_BALL = 9;
     public static final int MAX_BALL_SIZE = 3;
+    public static final int SCORE_ZERO = 0;
     private final List<Integer> computerBalls = new ArrayList<>();
-
+    private final Map<BallType, Integer> scoreBoard = new EnumMap<>(BallType.class);
 
     public BaseballGame() {
         initComputerBalls();
@@ -23,5 +30,14 @@ public class BaseballGame {
                 computerBalls.add(ball);
             }
         }
+    }
+
+    public String getResult(final Player player) {
+        initScoreBoard();
+    }
+
+    private void initScoreBoard() {
+        scoreBoard.put(STRIKE, SCORE_ZERO);
+        scoreBoard.put(BALL, SCORE_ZERO);
     }
 }
