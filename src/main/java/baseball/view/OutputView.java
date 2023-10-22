b1a859d8a4e7055ddcba03a1c74a34940d3156a3
kimtaesoo <kimtaesoo7@naver.com>
2023-10-22 14:00:55 +0900

    refactor: 시작 문구 수정

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -2,7 +2,7 @@ package baseball.view;
 
 public class OutputView {
 
-    private static final String START = "야구 게임을 시작합니다.";
+    private static final String START = "숫자 야구 게임을 시작합니다.";
     private static final String END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static final String INPUT_NUMBER = "숫자를 입력해주세요 : ";
