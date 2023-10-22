63d0d227fcdc0de9e964c38a94640481569a446d
InGyu Moon <ansdlsrb1121@naver.com>
2023-10-22 16:33:34 +0900

    refactor: NewGameInput 클래스 else 예약어 대신 if문으로만 표현

--- src/main/java/baseball/input/NewGameInput.java
+++ src/main/java/baseball/input/NewGameInput.java
@@ -8,19 +8,14 @@ public class NewGameInput {
     public int newGame(){
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String userInput = readLine();
-        int inputNumber;
         try{
-            inputNumber = Integer.parseInt(userInput);
+            int inputNumber = Integer.parseInt(userInput);
+            if(inputNumber != GameStatus.PLAY.status && inputNumber != GameStatus.FINISH.status){
+                throw new IllegalArgumentException("입력값은 1 또는 2이어야 합니다.");
+            }
+            return inputNumber;
         }catch (Exception e){
             throw new IllegalArgumentException("잘못된 입력값입니다.");
         }
-
-        if (inputNumber == GameStatus.PLAY.status || inputNumber == GameStatus.FINISH.status) {
-            return inputNumber;
-        }
-        else {
-            throw new IllegalArgumentException("입력값은 1 또는 2이어야 합니다.");
-        }
     }
-
 }
