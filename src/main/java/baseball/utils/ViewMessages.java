bdd028866f69f0adc0b7247f88893993efcbea22
twoosky <lhjae3@naver.com>
2023-10-23 01:59:30 +0900

    feat: outputView 메시지 정의

---
+++ src/main/java/baseball/utils/ViewMessages.java
@@ -0,0 +1,9 @@
+package baseball.utils;
+
+public class ViewMessages {
+    public static final String INPUT_NUMBER = "숫자를 입력해주세요 : ";
+    public static final String INPUT_RETRY_OR_QUIT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public static final String OUTPUT_START = "숫자 야구 게임을 시작합니다.";
+    public static final String OUTPUT_DONE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+}
