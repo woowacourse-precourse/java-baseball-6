f48c9dcb766217ace9aad6752441d5c51bbebd0a
opixxx <opix0306@naver.com>
2023-10-22 17:33:06 +0900

    feat: Implement the feature for the computer to pick three different digits

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,21 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        // 컴퓨터가 서로 다른 3자리를 뽑는 기능
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
     }
+
 }
