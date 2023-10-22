d40b9f3a7268d78a4a50b5fbe2dec0e5ed174305
dltjdn <hana40004@naver.com>
2023-10-22 19:17:55 +0900

    feat(GameView): 게임 결과 출력 기능 구현

--- src/main/java/baseball/view/GameView.java
+++ src/main/java/baseball/view/GameView.java
@@ -6,6 +6,12 @@ public class GameView {
     private final String SUCCESS_AND_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private final String RESTART_OR_END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
+    private final String BALL_RESULT = "볼";
+    private final String STRIKE_RESULT = "스트라이크";
+    private final String NOTHING_RESULT = "낫싱";
+    private final String BLANK = " ";
+
+
     public void printStartMessage() {
         System.out.println(START_MESSAGE);
     }
@@ -20,6 +26,18 @@ public class GameView {
 
     public void printRestartOrEndMessage() {
         System.out.println(RESTART_OR_END_MESSAGE);
+    }
+
+    public void printResultNothing() {
+        System.out.println(NOTHING_RESULT);
+    }
+
+    public void printResultStrike(int strikeCount) {
+        System.out.println(strikeCount + STRIKE_RESULT);
+    }
+
+    public void printResultBall(int ballCount) {
+        System.out.println(ballCount + BALL_RESULT + BLANK);
     }
 
 }
