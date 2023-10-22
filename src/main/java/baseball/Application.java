2588a335d85bfd8408099a672258152a21f8c419
mk3058 <kmk3058@icloud.com>
2023-10-22 22:03:59 +0900

    refactor(validation): 값 검증 메서드 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,19 +4,27 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
-        String isEnd;
+        String input;
+        Game game;
 
         do {
-            Game game = new Game(new User(), new Computer());
-            
+            game = new Game(new User(), new Computer());
+
             game.play();
             GameMessage.GAME_RESTART_PROMPT.print();
-            isEnd = readLine();
-            if (isEnd.length() != 1 || !Character.isDigit(isEnd.charAt(0)) || !(1 <= Integer.parseInt(isEnd)
-                    && Integer.parseInt(isEnd) <= 2)) {
+            input = readLine();
+            if (isValid(input)) {
                 throw new IllegalArgumentException("잘못된 입력입니다.");
             }
-        } while (Integer.parseInt(isEnd) == 1);
+        } while (Integer.parseInt(input) == 1);
 
+    }
+
+    private static boolean isValid(String input) {
+        if (input.length() != 1 || !Character.isDigit(input.charAt(0)) ||
+                !(1 <= Integer.parseInt(input) && Integer.parseInt(input) <= 2)) {
+            return false;
+        }
+        return true;
     }
 }
