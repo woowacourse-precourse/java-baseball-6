42f2e2089ec1727d3d55f0a39a6784a72f3f8e78
jaehee <y2hjjh@naver.com>
2023-10-22 21:13:56 +0900

    feat: 스트라이크 개수 반환 메서드 작성

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,13 +1,33 @@
 package baseball.controller;
 
 import baseball.model.AnswerNumber;
+import baseball.model.InputNumber;
+
+import java.util.List;
 
 public class GameController {
     static final AnswerNumber answerNumber = new AnswerNumber();
+    static InputNumber inputNumber = new InputNumber();
 
     public void createAnswer() {
         answerNumber.createRandomNumberList();
         System.out.println("정답: " + answerNumber.getRandomNumber());
     }
+
+    public int getStrikeCount() {
+        int strikeCount = 0;
+
+        for (int i = 0; i < answerNumber.getRandomNumber().size(); i++) {
+            int elementOfAnswer = answerNumber.getRandomNumber().get(i);
+            int elementOfInput = inputNumber.getInputNumber().get(i);
+
+            if (elementOfAnswer == elementOfInput) {
+                strikeCount++;
+            }
+        }
+        System.out.println("스트라이크 : " + strikeCount);
+        return strikeCount;
+    }
+
 }
 
