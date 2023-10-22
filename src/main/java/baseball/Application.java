ca89cb8667250b63021977520bc704b75dac8c07
hsyoodev <hsyoodev@gmail.com>
2023-10-22 17:18:20 +0900

    feat: application restart game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,9 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
 
+    private static final String RESTART_COMMAND = "1";
+    private static final String EXIT_COMMAND = "2";
+
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        startGame();
+        do {
+            startGame();
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        } while (isRestartGame());
     }
 
     private static void startGame() {
@@ -22,5 +28,25 @@ public class Application {
 
     private static boolean isClear() {
         return BaseballNumber.getStrike() == BaseballNumber.getAnswer();
+    }
+
+    private static boolean isRestartGame() {
+        String command = Console.readLine();
+        validateCommand(command);
+        return command.equals(RESTART_COMMAND);
+    }
+
+    private static void validateCommand(String command) {
+        if (!isRestartCommand(command) && !isExitCommand(command)) {
+            throw new IllegalArgumentException("INVALID COMMAND");
+        }
+    }
+
+    private static boolean isRestartCommand(String command) {
+        return command.equals(RESTART_COMMAND);
+    }
+
+    private static boolean isExitCommand(String command) {
+        return command.equals(EXIT_COMMAND);
     }
 }
