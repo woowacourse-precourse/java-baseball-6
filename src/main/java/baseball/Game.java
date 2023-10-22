8f394a4316383f971be336256e8e697b0ee12d97
H-server <hani103201@naver.com>
2023-10-22 19:52:50 +0900

    feat: 게임 결과 출력(볼, 스트라이크, 낫싱)

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -36,6 +36,23 @@ public class Game {
                 }
             }
         }
+        call();
+    }
+
+    void call() {
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+        } else if (strike == 3) {
+            isPlaying = false;
+            System.out.println(strike + "스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        } else if (strike == 0) {
+            System.out.println(ball + "볼");
+        } else if (ball == 0) {
+            System.out.println(strike + "스트라이크");
+        } else {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
     }
 }
 
