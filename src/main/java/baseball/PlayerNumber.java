34f72862f9044409e302d04817d5a2bd42b9753e
sujnkim <blackout3210@naver.com>
2023-10-22 15:22:37 +0900

    feat: add PlayerNumber class
    사용자가 입력하는 세자리 수를 관리하는 클래스

---
+++ src/main/java/baseball/PlayerNumber.java
@@ -0,0 +1,30 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayerNumber {
+
+    private final List<Integer> playerNumber;
+
+    public PlayerNumber(List<Integer> playerNumber) {
+        this.playerNumber = createPlayerNumber();
+    }
+
+
+    private List<Integer> createPlayerNumber() throws IllegalArgumentException {
+
+        List<Integer> numbers = new ArrayList<>();
+
+        //사용자 입력
+
+        return numbers;
+    }
+
+
+    public List<Integer> getPlayerNumber() {
+        return playerNumber;
+    }
+}
