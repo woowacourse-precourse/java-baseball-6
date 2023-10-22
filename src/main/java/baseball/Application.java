fe1dcc2709b5ef961c3e2b698fb2c95b47e6560d
shlihs95 <fhzptvh@gmail.com>
2023-10-22 21:21:34 +0900

    feat: 2. 입력 받은 수 검증 - 3자리 수가 맞는지 확인

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -7,6 +8,10 @@ import java.util.List;
 
 public class Application {
 
+    /**
+     * 상대방(컴퓨터)이 숫자 생성
+     * @return List<Integer> computer
+     */
     public List<Integer> makeComputerNumber() {
         List<Integer> computer = new ArrayList<>();
 
@@ -20,9 +25,44 @@ public class Application {
         return computer;
     }
 
+    /**
+     * 입력 받은 수 검증
+     * @param input
+     * @return boolean true || false
+     */
+    public boolean inputCheckAll(String input) {
+        //3자리 수가 맞는지 확인
+        if (!inputCheckLength(input)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean inputCheckLength(String input) {
+        if (input.length() != 3) {
+            return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         Application application = new Application();
-        List<Integer> computer = application.makeComputerNumber();
+        List<Integer> computer = application.makeComputerNumber();  //상대방(컴퓨터)이 숫자 생성
+        String input;                                               //입력받는 값을 넣어 줄 변수
+        boolean gameFlag = true;                                    //게임 계속 실행할지 그만둘지 표시
 
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        while(gameFlag) {
+            System.out.println("숫자를 입력해주세요 : ");
+
+            input = Console.readLine();                             //사용자가 숫자를 입력할 수 있는 환경 구현
+            if (!application.inputCheckAll(input)) {
+                throw new IllegalArgumentException();
+            }
+
+
+        }
     }
 }
