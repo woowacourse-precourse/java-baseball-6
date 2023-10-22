d21ed861fad2bf7fcf494eb4a1dded00a70b45e4
Junsu An <junsu6752@gmail.com>
2023-10-22 18:45:15 +0900

    랜덤숫자 3개, 숫자 3개 입력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,28 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        System.out.println(computer);
+        String number1 = readLine();
+        String number2 = readLine();
+        String number3 = readLine();
+        List<String> number = new ArrayList<>();
+        number.add(number1);
+        number.add(number2);
+        number.add(number3);
+        System.out.println(number);
     }
 }
