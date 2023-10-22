9190a3fc95121cc8b517707b6bd38835b9822833
hosung-222 <gik11kr@naver.com>
2023-10-22 14:17:54 +0900

    refactor : 결과 출력 부분 분리

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -3,6 +3,7 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 public class GameController {
+    private static final int NUM_LENGTH = 3;
     private int strikeCnt = 0;
     private int ballCnt = 0;
 
@@ -20,6 +21,7 @@ public class GameController {
         //랜덤 값 생성
         RandomNumbers computer = new RandomNumbers();
         computer.setRandomNumbers();
+        System.out.println(computer.getAllNumbers());
 
         while (true) {
             resetCnt();
@@ -27,12 +29,12 @@ public class GameController {
             // 사용자 값 입력
             System.out.print("숫자를 입력해주세요 : ");
             String input = Console.readLine();
-            if (input.length() != 3) {
+            if (input.length() != NUM_LENGTH) {
                 throw new IllegalArgumentException("입력 값 오류");
             }
 
             String[] inputArr = input.split("");
-            for (int i = 0; i < 3; i++) {
+            for (int i = 0; i < NUM_LENGTH; i++) {
                 if (computer.getNumber(i) == Integer.parseInt(inputArr[i])) {
                     strikeCnt++;
                 } else if (computer.contains(Integer.parseInt(inputArr[i]))) {
@@ -40,19 +42,23 @@ public class GameController {
                 }
             }
             // 출력
-            if (strikeCnt + ballCnt == 0) {
-                System.out.println("낫싱");
-            } else if (strikeCnt == 0) {
-                System.out.println(ballCnt + "볼 ");
-            } else if (ballCnt == 0) {
-                System.out.println(strikeCnt + "스트라이크");
-            } else {
-                System.out.println(ballCnt + "볼 " + strikeCnt + "스트라이크");
-            }
+            printResult(strikeCnt, ballCnt);
 
             if (strikeCnt == 3) {
                 break;
             }
+        }
+    }
+
+    public void printResult(int strikeCnt, int ballCnt) {
+        if (strikeCnt + ballCnt == 0) {
+            System.out.println("낫싱");
+        } else if (strikeCnt == 0) {
+            System.out.println(ballCnt + "볼 ");
+        } else if (ballCnt == 0) {
+            System.out.println(strikeCnt + "스트라이크");
+        } else if (ballCnt > 0 & strikeCnt > 0) {
+            System.out.println(ballCnt + "볼 " + strikeCnt + "스트라이크");
         }
     }
 
