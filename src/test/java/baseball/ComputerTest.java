64b0d4fff3fc4b68dabb02507af71617d21f58d7
minjun7410 <minjun7410@gmail.com>
2023-10-22 18:12:51 +0900

    refactor(ComputerTest): 판정 테스트 리펙토링
    * computer 변수 전역으로 옮김
    * Judgement를 생성하고 judgement의 결과가 옮은지 테스트하는 함수 assertJudgeResultOutput 추가

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -6,9 +6,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 public class ComputerTest {
+    private final Computer computer = new Computer();
     @Test
     void 난수생성중복검사(){
-        Computer computer = new Computer();
         for(int i = 0; i < 100; i++) {
             int[] numbers = computer.initComputerNumbers();
             boolean[] duplicateCheck = new boolean[10];
@@ -23,29 +23,18 @@ public class ComputerTest {
     void 판정테스트(){
         ByteArrayOutputStream testOut = new ByteArrayOutputStream();
         System.setOut(new PrintStream(testOut));
-
-        Computer computer = new Computer();
         int[] computerNumbers = computer.initComputerNumbers();
-        Judgement result1 = computer.getJudgeResult(computerNumbers);
-        Judgement result2 = computer.getJudgeResult(new int[] {computerNumbers[1], computerNumbers[2], computerNumbers[0]});
-        Judgement result3 = computer.getJudgeResult(new int[] {0, computerNumbers[0], 0});
-        Judgement result4 = computer.getJudgeResult(new int[] {computerNumbers[0], computerNumbers[2], 0});
-        Judgement result5 = computer.getJudgeResult(new int[] {computerNumbers[0], computerNumbers[2], computerNumbers[1]});
 
-        result1.printResult();
-        Assertions.assertThat(testOut.toString()).isEqualTo("3스트라이크\n");
-        testOut.reset();
-        result2.printResult();
-        Assertions.assertThat(testOut.toString()).isEqualTo("3볼\n");
-        testOut.reset();
-        result3.printResult();
-        Assertions.assertThat(testOut.toString()).isEqualTo("1볼\n");
-        testOut.reset();
-        result4.printResult();
-        Assertions.assertThat(testOut.toString()).isEqualTo("1볼 1스트라이크\n");
-        testOut.reset();
-        result5.printResult();
-        Assertions.assertThat(testOut.toString()).isEqualTo("2볼 1스트라이크\n");
+        assertJudgeResultOutput(testOut, computerNumbers, "3스트라이크\n");
+        assertJudgeResultOutput(testOut, new int[] {computerNumbers[1], computerNumbers[2], computerNumbers[0]}, "3볼\n");
+        assertJudgeResultOutput(testOut, new int[] {0, computerNumbers[0], 0}, "1볼\n");
+        assertJudgeResultOutput(testOut, new int[] {computerNumbers[0], computerNumbers[2], 0}, "1볼 1스트라이크\n");
+        assertJudgeResultOutput(testOut, new int[] {computerNumbers[0], computerNumbers[2], computerNumbers[1]}, "2볼 1스트라이크\n");
+    }
+    void assertJudgeResultOutput(ByteArrayOutputStream testOut, int[] userNumbers, String outputToCompare){
+        Judgement result = computer.getJudgeResult(userNumbers);
+        result.printResult();
+        Assertions.assertThat(testOut.toString()).isEqualTo(outputToCompare);
         testOut.reset();
     }
 
