30df990ee72b8a0c8114fa11d7af2671f30a7386
Jason <jasonecy@gmail.com>
2023-10-22 20:29:23 +0900

    feat: 컴퓨터 숫자 생성 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        gameStart();
+    }
+
+    private static void gameStart() {
+        String computer = generateRandomNumber();
+    }
+
+    private static String generateRandomNumber() {
+        String computer = "";
+        while (computer.length() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(Integer.toString(randomNumber))) computer += randomNumber;
+        }
+        return computer;
     }
 }
