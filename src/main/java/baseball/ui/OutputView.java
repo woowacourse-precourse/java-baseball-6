56859fd4617d4cd6b5cde85afb9988f92fd60065
NAM GIBEOM <rlqja0523@naver.com>
2023-10-22 20:33:17 +0900

    feat(OutputView): 게임 결과 출력 기능 추가

--- src/main/java/baseball/ui/OutputView.java
+++ src/main/java/baseball/ui/OutputView.java
@@ -1,7 +1,13 @@
 package baseball.ui;
 
+import baseball.dto.Inning;
+
 public class OutputView {
     public void printGameStartMessage() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void printResult(Inning inning) {
+        System.out.println(inning.result());
     }
 }
