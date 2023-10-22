2f97bf53c0ab59daa44d2693b5a6b028664240ea
jy016011 <39334789+jy016011@users.noreply.github.com>
2023-10-22 17:26:41 +0900

    refactor: Computer클래스 변경에 따른 구조변경
    Computer의 멤버변수를 직접 수정하고 참조한것을 간접적으로 하도록 바꾸었음.
    그 과정에서 UserAction과의 구조 통일성을 위해 난수 리스트 생성기능을 RandomGenerator로부터 가져옴.

--- src/main/java/baseball/gameLogic/ComputerAction.java
+++ src/main/java/baseball/gameLogic/ComputerAction.java
@@ -1,10 +1,13 @@
 package baseball.gameLogic;
 
 import baseball.models.Computer;
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import static baseball.utils.RandomNumberGenerator.generateRandomNumberList;
+import static baseball.utils.Constants.*;
+import static baseball.utils.RandomNumberGenerator.pickRandomNumberValidRange;
 
 /*
   Computer의 행동들을 수행하는 클래스
@@ -14,10 +17,20 @@ public class ComputerAction {
     private final Computer computer = new Computer();
 
     public void generateNumber(){
-        computer.numberList = generateRandomNumberList();
+        while (computer.getListSize() < ANSWER_LENGTH){
+            int randomNumber = pickRandomNumberValidRange();
+            List<Integer> randomNumberList = showNumberList();
+
+            if (!randomNumberList.contains(randomNumber))
+                computer.saveNumber(randomNumber);
+        }
     }
 
-    public List<Integer> getNumberList(){
-        return computer.numberList;
+    public List<Integer> showNumberList(){
+        return computer.showNumberList();
+    }
+
+    public void clearAnswer(){
+        computer.clearAnswerList();
     }
 }
