44304128c6af186a2ca3e0ab74ca8fba7c94e22e
InGyu Moon <ansdlsrb1121@naver.com>
2023-10-22 16:42:16 +0900

    refactor: NewGameInput에서 입력 오류 발생시 출력 오류메시지가 항상 잘못된 입력값입니다. 로 나오는 오류 수정

--- src/main/java/baseball/input/NewGameInput.java
+++ src/main/java/baseball/input/NewGameInput.java
@@ -8,14 +8,18 @@ public class NewGameInput {
     public int newGame(){
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String userInput = readLine();
+        int inputNumber;
         try{
-            int inputNumber = Integer.parseInt(userInput);
-            if(inputNumber != GameStatus.PLAY.status && inputNumber != GameStatus.FINISH.status){
-                throw new IllegalArgumentException("입력값은 1 또는 2이어야 합니다.");
-            }
-            return inputNumber;
+            inputNumber = Integer.parseInt(userInput);
         }catch (Exception e){
             throw new IllegalArgumentException("잘못된 입력값입니다.");
         }
+
+        if (inputNumber != GameStatus.PLAY.status && inputNumber != GameStatus.FINISH.status) {
+            throw new IllegalArgumentException("입력값은 1 또는 2이어야 합니다.");
+        }
+
+        return inputNumber;
     }
+
 }
