684e4d56fea4b24165352a87bb495718fa140b9b
JuSeong1130 <jsmoon1130@naver.com>
2023-10-22 22:39:33 +0900

    feat(GameController) : 게임 Run 기능 구현

---
+++ src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,65 @@
+package baseball.controller;
+
+import baseball.domain.Balls;
+import baseball.domain.Game;
+import baseball.domain.GameResult;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class GameController {
+
+    private Game game;
+
+    public void run() {
+        init();
+        play();
+        checkRestart();
+    }
+
+    private void init() {
+        game = new Game();
+    }
+    private void play() {
+        while (game.isRun()) {
+            Balls playerBalls = createPlayerBalls();
+            GameResult gameResult = game.compareBalls(playerBalls);
+            OutputView.printResult(gameResult);
+        }
+    }
+    private void checkRestart() {
+        OutputView.printEnd();
+        String command = InputView.askRestart();
+        game.performCommand(command);
+        if(game.isRun()) {
+            play();
+        }
+    }
+
+    private Balls createPlayerBalls() {
+        String number = InputView.askNumber();
+        validateNumber(number);
+        List<Integer> numbers = stringToList(number);
+        return new Balls(numbers);
+    }
+
+    private void validateNumber(String number) {
+        try {
+            Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자를 입력해야 합니다.");
+        }
+    }
+
+    private List<Integer> stringToList(String number) {
+        return Arrays.stream(number.split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+
+}
