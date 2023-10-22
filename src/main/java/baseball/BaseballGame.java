9773eb739ae29bce4229e8dbb367562d901022ef
eunjin3481 <eunjin3481@gmail.com>
2023-10-22 21:33:42 +0900

    Feat: generateComputerNumbers 메서드 구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import java.util.HashMap;
+import camp.nextstep.edu.missionutils.Randoms;
 class BaseballGame{
     private HashMap<Integer, Integer> computerNumbers;
     private int[] userNumbers;
@@ -19,6 +21,14 @@ class BaseballGame{
     }
 
     public void generateComputerNumbers(){
+        computerNumbers = new HashMap<>();
+        int idx = 0;
+        while(computerNumbers.size() < 3){
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if(!computerNumbers.containsKey(randomNumber)){
+                computerNumbers.put(randomNumber, idx++);
+            }
+        }
     }
 
     public void userNumbersInput(){
