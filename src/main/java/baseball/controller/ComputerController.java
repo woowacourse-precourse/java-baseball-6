0c7b43848e9e4335b83ce191b4aa6a2b8c9fc3c0
yunju-m <dbswnfnal@naver.com>
2023-10-22 16:12:51 +0900

    feat(ComputerController): 추가 비교 결과에 따른 힌트 제공 함수

--- src/main/java/baseball/controller/ComputerController.java
+++ src/main/java/baseball/controller/ComputerController.java
@@ -13,6 +13,9 @@ public class ComputerController {
     InputView inputView = new InputView();
     
     private static final int BASEBALL_NUMBER_LENGTH = 3;
+    private static final String STRIKE_MESSAGE = "스트라이크";
+    private static final String BALL_MESSAGE = "볼";
+    private static final String NOTHING_MESSAGE = "낫싱";
     private int strikeCount;
     private int ballCount;
 
@@ -29,9 +32,8 @@ public class ComputerController {
             countStrike(playerNumbers[i], i);
             countBall(playerNumbers[i], i);
         }
-
-        System.out.println("strikeCount: " + strikeCount);
-        System.out.println("ballCount: " + ballCount);
+        
+        provideHint();
     }
 
     private void countStrike(int playerNumber, int numberIndex) {
@@ -45,5 +47,23 @@ public class ComputerController {
         if ((randomNumbers[numberIndex] != playerNumber) && Arrays.stream(randomNumbers).anyMatch(num -> num == playerNumber)) {
             ballCount++;
         } 
+    }
+
+    private void provideHint() {
+        String hint = "";
+
+        if (ballCount > 0) {
+            hint += ballCount + BALL_MESSAGE + " ";
+        }
+
+        if (strikeCount > 0) {
+            hint += strikeCount + STRIKE_MESSAGE + " ";
+        }
+
+        if (ballCount == 0 && strikeCount == 0) {
+            hint += NOTHING_MESSAGE;
+        }
+
+        System.out.println(hint);
     }
 }
