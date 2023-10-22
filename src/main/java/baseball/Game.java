569d50ef87dbd73de56d00851e8324aa6dfdf336
ffolabear <ffolabear@gmail.com>
2023-10-22 23:59:40 +0900

    refactor : 사용자의 입력을 처리하는 방식 수정

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,4 +1,30 @@
 package baseball;
 
+import baseball.util.NumberGenerator;
+
+import java.util.HashMap;
+import java.util.List;
+
 public class Game {
+
+    private final Player player;
+    private List<Integer> answer;
+
+    public Game(Player player) {
+        this.player = player;
+    }
+
+    private List<Integer> generateNumber() {
+        NumberGenerator generator = new NumberGenerator();
+        answer = generator.generate();
+        return answer;
+    }
+
+    private HashMap<String, Integer> scoring() {
+        HashMap<String, Integer> gameScore = new HashMap<>();
+        gameScore.put("볼", 0);
+        gameScore.put("스트라이크", 0);
+        return gameScore;
+    }
+
 }
