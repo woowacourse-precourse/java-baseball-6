853de5380a4ee98206c8158915d8af803a0cbdbd
kkonii <119216472+kkonii@users.noreply.github.com>
2023-10-22 23:15:44 +0900

    Feat : 공백 값을 입력하면 예외를 발생시킨다
    - Validator 객체에서 validate()를 호출해 입력값의 공백 여부를 검사한다

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,10 +5,14 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Validator validator = new Validator();
+
         System.out.println("숫자 야구 게임을 시작합니다.");
 
         System.out.print("숫자를 입력해주세요 : ");
 
         String userInput = Console.readLine();
+
+        validator.validate(userInput);
     }
 }
--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -1,6 +1,12 @@
 package baseball;
 
 public class Validator {
+    public void validate(String input) {
+        if (isEmptyValue(input)) {
+            throw new IllegalArgumentException("빈 값은 입력할 수 없습니다.");
+        }
+    }
+
     private boolean isEmptyValue(String input) {
         return input.isBlank();
     }
