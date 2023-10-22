c8e1b5dec68f2bef1cf93bce4b27056856b6979b
noxknow <chris2769@naver.com>
2023-10-22 18:42:34 +0900

    test(UserBaseballNumberTest) : 중복된 숫자가 있는 경우 예외가 발생한다

--- src/test/java/baseball/domain/UserBaseballNumberTest.java
+++ src/test/java/baseball/domain/UserBaseballNumberTest.java
@@ -13,10 +13,19 @@ public class UserBaseballNumberTest {
 
     @DisplayName("3자리의 숫자가 아닌 경우 예외가 발생한다")
     @ParameterizedTest(name = "[{index}] Input : {0}")
-    @ValueSource(strings = {"1, 2, 3, 4", "1, 2"})
+    @ValueSource(strings = {"1,2,3,4", "1,2"})
     void createNumberWithInvalidLength(String inputString) {
         assertThatThrownBy(() -> new UserBaseballNumber(Arrays.asList(inputString.split(","))))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorHandler.INVALID_LENGTH.getException().getMessage());
+    }
+
+    @DisplayName("중복된 숫자가 있는 경우 예외가 발생한다")
+    @ParameterizedTest(name = "[{index}] Input : {0}")
+    @ValueSource(strings = {"1,2,2", "2,2,3"})
+    void createNumberWithDuplicate(String inputString) {
+        assertThatThrownBy(() -> new UserBaseballNumber(Arrays.asList(inputString.split(","))))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorHandler.DUPLICATE_NUMBER.getException().getMessage());
     }
 }
