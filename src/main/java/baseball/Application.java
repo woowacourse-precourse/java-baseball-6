b00e8d68f52d033b7b5a9db8c13822b3aaf1fe4b
seaniiio <cnc4750@naver.com>
2023-10-22 19:08:51 +0900

    feat: 게임 종료 후 1 누르면 다시, 2 누르면 종료하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,9 +5,21 @@ import baseball.User.User;
 
 
 public class Application {
+    static Integer restart = 0;
     public static void main(String[] args) {
+        System.out.printf("숫자 야구 게임을 시작합니다.\n");
+        Play();
         // TODO: 프로그램 구현
+        if(restart == 1) {
+            Play();
+        }
+    }
+    public static void Play() {
         Play play = new Play();
         play.PlayGame();
+        while(play.getStrikes() != 3) {
+            play.PlayGame();
+        }
+        restart = play.getRestart();
     }
 }
