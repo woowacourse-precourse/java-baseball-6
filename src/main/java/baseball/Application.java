d5bd4eaa302cc6227e153ab147241108db227d77
LEM <ppoppo9802@naver.com>
2023-10-22 17:59:41 +0900

    사용자의 입력의 예외처리기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,5 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import camp.nextstep.edu.missionutils.Console;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Application {
     public static void main(String[] args) {
         int restart = 1;
@@ -21,6 +15,5 @@ public class Application {
                 break;
             }
         }
-        return;
     }
 }
--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -3,22 +3,63 @@ package baseball;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class User {
     public List<Integer> userNumber() {
         System.out.print("숫자를 입력해주세요 : ");
         List<Integer> numbers = new ArrayList<>();
         String input = readLine();
-
+        int tempNum;
+        checkExceptionMain(input);
         for (String num: input.split("")) {
-            numbers.add(Integer.parseInt(num));
+            tempNum = Integer.parseInt(num);
+            numbers.add(tempNum);
         }
         return numbers;
     }
     public int userDecision() {
         int decision;
         String input = readLine();
-        return decision = Integer.parseInt(input);
+        checkExceptionDecision(input);
+        decision = Integer.parseInt(input);
+        return decision;
+    }
+    public void checkExceptionMain(String input) {
+        isEmpty(input);
+        checkLength(input);
+        checkZero(input);
+        checkDuplication(input);
+    }
+    public void checkExceptionDecision(String input) {
+        if (!(input.equals("1") || input.equals("2"))) {
+            throw new IllegalArgumentException();
+        }
+    }
+    public void isEmpty(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+    }
+    public void checkLength(String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+    }
+    public void checkZero(String input) {
+        if (input.contains("0")) {
+            throw new IllegalArgumentException();
+        }
+    }
+    public void checkDuplication(String input) {
+        Set<Character> numSet = new HashSet<>();
+        for (char num : input.toCharArray()) {
+            numSet.add(num);
+        }
+        if (numSet.size() != 3) {
+            throw new IllegalArgumentException();
+        }
     }
 }
