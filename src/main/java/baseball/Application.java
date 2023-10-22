594dc7f670b9defe4e102719f73c6ccb3006e81d
gimdaeyeon <rlaeodus1306@naver.com>
2023-10-23 01:01:14 +0900

    reset: 계속 발생되는 오류로 인해 전체 프로그램 설계부터 처음부터 시작

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,24 +1,9 @@
 package baseball;
 
 
-import baseball.domain.BaseBallGame;
-import baseball.domain.Player;
-import baseball.domain.PlayerType;
-import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args)  {
-        int replayNumber = 1;
-
-        while(true){
-            if(replayNumber == 2){
-                break;
-            }
-            BaseBallGame.getNewGame(new Player(PlayerType.HUMAN), new Player(PlayerType.COMPUTER));
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            replayNumber = Integer.parseInt(Console.readLine());
-        }
-
 
 
 
