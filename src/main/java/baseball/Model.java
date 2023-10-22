b740df9461d12a3b7a7bc05e531798bda9161f2b
Seyeon <wjdtpdus828@naver.com>
2023-10-22 21:23:59 +0900

    feat(Model): initComputerNum() 메소드 생성

--- src/main/java/baseball/Model.java
+++ src/main/java/baseball/Model.java
@@ -2,6 +2,19 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class Model{
+import java.util.ArrayList;
+import java.util.List;
 
+public class Model{
+    private static final int NUM_SIZE = 3;
+    public List<Integer> initComputerNum(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < NUM_SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
 }
