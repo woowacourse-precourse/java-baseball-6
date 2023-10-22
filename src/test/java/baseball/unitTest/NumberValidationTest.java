3cb4aca3a188d4cd39c235fca9f00d5c99b715b3
baksakcci <pshsh910@gmail.com>
2023-10-22 23:47:14 +0900

    test: 숫자 검증 테스트 케이스 추가

---
+++ src/test/java/baseball/unitTest/NumberValidationTest.java
@@ -0,0 +1,24 @@
+package baseball.unitTest;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class NumberValidationTest {
+    @Test
+    @DisplayName("입력값이_숫자가 아닐 경우_예외를 발생한다")
+    void input_isNotNumber_exception() {
+
+    }
+
+    @Test
+    @DisplayName("입력값이_3자리가 아닐 경우_예외를 발생한다")
+    void input_isNot3Digit_exception() {
+
+    }
+
+    @Test
+    @DisplayName("입력값이_각 자리마다 중복이 발생할 경우_예외를 발생한다")
+    void input_duplicateNumber_exception() {
+
+    }
+}
