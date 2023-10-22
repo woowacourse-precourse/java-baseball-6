5213c6679063afdb5d43d4b82aa483ddd6c85101
Knight <kangrack333@gmail.com>
2023-10-22 15:43:11 +0900

    게임실행 메시지 출력(플레이어)

---
+++ src/main/java/player/Start.java
@@ -0,0 +1,12 @@
+package player;
+
+import constant.GuideMessageConstant;
+
+public class Start {
+
+    public Start() {
+        System.out.println(GuideMessageConstant.START_MESSAGE);
+    }
+
+
+}
