8757333faab5e0643e9c34c2581e9e2ea1a0df9c
NAM GIBEOM <rlqja0523@naver.com>
2023-10-22 15:23:46 +0900

    feat(Config): 게임 종료 숫자 추가

--- src/main/java/baseball/enums/GameConfig.java
+++ src/main/java/baseball/enums/GameConfig.java
@@ -3,7 +3,9 @@ package baseball.enums;
 public enum GameConfig {
     BALL_COUNT(3),
     MIN_NUMBER(1),
-    MAX_NUMBER(9);
+    MAX_NUMBER(9),
+    GAME_CONTINUE(1),
+    GAME_EXIT(2);
 
     private final int value;
 
