24db07da337808054c2e55cc31f223e93b28942f
Hyeji <hyejijjang10@gmail.com>
2023-10-23 00:28:27 +0900

    test

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        //컴퓨터가 생성한 3자리수
+        Randoms.pickUniqueNumbersInRange(1, 10, 3); // [1, 2]
+
+
     }
 }
