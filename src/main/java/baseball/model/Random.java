284cc7ddf1c63c41052272da14df22189c327af9
sj <hera4224@naver.com>
2023-10-22 21:27:16 +0900

    [Feat]model 구현완료

--- src/main/java/baseball/model/Random.java
+++ src/main/java/baseball/model/Random.java
@@ -1,4 +1,19 @@
 package baseball.model;
 //랜덤 세자리 수 생성
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Random {
+    public static List<Integer> ComNum(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
 }
