2c4d98c96cf03af95947a938471200b1149699b9
wda067 <mg6359@naver.com>
2023-10-23 00:29:19 +0900

    feat: main메소드에서 Game 클래스의 run메소드 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,8 @@
 package baseball;
 
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        System.out.println("test");
+        Game.run();
     }
 }
