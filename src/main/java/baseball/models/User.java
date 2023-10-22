a9daca7addf0f1e7e8a1e3dba1bd20811ab54493
jy016011 <39334789+jy016011@users.noreply.github.com>
2023-10-22 15:56:51 +0900

    refactor: UserAction으로부터 문자 검증 기능을 위임 받음
    UserAction에서 문자열 검증만 하도록 시키고,
    User에서는 문자에대한 검증 후 저장하도록 변경

--- src/main/java/baseball/models/User.java
+++ src/main/java/baseball/models/User.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static baseball.models.Constants.*;
+import static baseball.utils.InputValidator.validateAnswerCharacter;
 
 /*
  User의 변수들을 기억하는 클래스
@@ -13,7 +14,8 @@ public class User {
     private final List<Integer> numberList = new ArrayList<>();
     private int mind;
 
-    public void saveNumber(int digit){
+    public void saveNumber(char userAnswerChar){
+        int digit = validateAnswerCharacter(userAnswerChar);
         numberList.add(digit);
     }
 
