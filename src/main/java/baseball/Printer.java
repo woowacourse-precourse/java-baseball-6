d0353ca7a4d807b40d808f4d5575f3cff247cc14
Moly <102847513+jminkkk@users.noreply.github.com>
2023-10-22 20:01:32 +0900

    refactor: 게임 코멘트 및 결과를 출력하기 위한 Printer 클래스 생성

---
+++ src/main/java/baseball/Printer.java
@@ -0,0 +1,43 @@
+package baseball;
+
+public class Printer {
+    private static final String START_GAME_COMMENT = "숫자 야구 게임을 시작합니다.";
+    private static final String INPUT_NUMBER_COMMENT = "숫자를 입력해주세요 : ";
+    private static final String END_GAME_COMMENT = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String RESTART_GAME_COMMENT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public static void printStartGameComment() {
+        System.out.println(START_GAME_COMMENT);
+    }
+
+    public static void printInputNumberComment() {
+        System.out.println(INPUT_NUMBER_COMMENT);
+    }
+
+    public static void printEndGameComment() {
+        System.out.println(END_GAME_COMMENT);
+    }
+
+    public static void printRestartGameComment() {
+        System.out.println(RESTART_GAME_COMMENT);
+    }
+
+    public static void printRoundResult(Result result) {
+        int strike = result.strike();
+        int ball = result.ball();
+
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+            return;
+        }
+
+        if (ball > 0) {
+            System.out.print(ball + "볼 ");
+        }
+
+        if (strike > 0) {
+            System.out.print(strike + "스트라이크");
+        }
+        System.out.println();
+    }
+}
