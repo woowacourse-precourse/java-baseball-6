0d8c62c12b9de55fd97b01ef9ee32fb9e89eadc7
chaeshee0908 <chaeshee0908@naver.com>
2023-10-22 15:38:08 +0900

    [update] 입력 예외 처리 추가

--- src/main/java/baseball/UserNumber/UserNumber.java
+++ src/main/java/baseball/UserNumber/UserNumber.java
@@ -8,9 +8,25 @@ import java.util.List;
 public class UserNumber {
 
     public static void validateInputNumber(String input) {
-        if (input.length() != 3 || !input.matches("\\d+")) {
-            throw new IllegalArgumentException("유효한 숫자가 아닙니다.");
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("3자리 숫자를 입력해야합니다.");
+        } else if (!input.matches("^[1-9]+$")) {
+            throw new IllegalArgumentException("1-9 사이의 숫자가 아닙니다.");
+        } else if (checkDuplicate(input)) {
+            throw new IllegalArgumentException("서로 다른 숫자를 입력해야합니다.");
         }
+    }
+
+    public static boolean checkDuplicate(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+            for (int j = i + 1; j < input.length(); j++) {
+                if (currentChar == input.charAt(j)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     private static List<Integer> toIntegerList(String input) {
