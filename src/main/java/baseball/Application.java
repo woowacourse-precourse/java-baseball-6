df5c02034d4f2c1a511c89090fa503688757478c
saera <yook.saera@gmail.com>
2023-10-22 21:37:14 +0900

    feat: Application이 게임을 한 판만 진행하라

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,13 +2,20 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
+        ComNumber comNumber = new ComNumber();
+        comNumber.pickNumbers();
+        PlayerNumber playerNumber = new PlayerNumber();
+        CompareNumbers compareNumbers = new CompareNumbers();
+        while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
+            playerNumber.input();
 
-//        CompareNumbers compareNumbers = new CompareNumbers();
-//        String actual = compareNumbers.compare(comNumber, playerNumber);
-//        if ("3스트라이크".equals(actual)){
-//            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-//        } else {
-//
-//        }
+            String result = compareNumbers.compare(comNumber, playerNumber);
+            System.out.println(result);
+            if ("3스트라이크".equals(result)) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            }
+        }
     }
 }
