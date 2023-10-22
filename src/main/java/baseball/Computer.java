18a1590816384305c496f1eb3d6a870776211049
ddolboghi <rhdfyd128@gmail.com>
2023-10-22 19:21:39 +0900

    fix: 컴퓨터 숫자 생성 기능 수정
    pickUniqueNumbersInRange가 아닌 pickNumberInRange를 이용

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,11 +1,27 @@
 package baseball;
 
-import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
 
-import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Computer {
     public List<Integer> createComputerNumbers() {
-        return pickUniqueNumbersInRange(1, 9, Constants.BASEBALL_NUMBER_SIZE);
+        List<Integer> computerNumbers = new ArrayList<>();
+        while (computerNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            addRandomNumber(computerNumbers, randomNumber);
+        }
+        return computerNumbers;
+    }
+
+    private static boolean isDuplicated(List<Integer> computerNumbers, int randomNumber) {
+        return computerNumbers.contains(randomNumber);
+    }
+
+    private static void addRandomNumber(List<Integer> computerNumbers, int randomNumber) {
+        if (!isDuplicated(computerNumbers, randomNumber)) {
+            computerNumbers.add(randomNumber);
+        }
     }
 }
--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -6,13 +6,13 @@ import java.util.List;
 import java.util.Set;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
-import static camp.nextstep.edu.missionutils.Randoms.pickNumberInList;
 
 public class User {
     public List<Integer> createUserNumbers() {
         List<Integer> userNumbers = new ArrayList<>();
         System.out.print("숫자를 입력해주세요 : ");
         String userInput = readLine();
+
         for (int i=0;i<userInput.length();i++) {
             String str = userInput.substring(i, i+1);
             userNumbers.add(Integer.parseInt(str));
@@ -22,10 +22,6 @@ public class User {
     }
 
     private static void validateUserNumber(List<Integer> userNumbers) {
-        int randomUserNumber = pickNumberInList(userNumbers);
-        if (randomUserNumber < 1) {
-            throw new IllegalArgumentException("각 숫자는 1부터 9까지만 입력해주세요.");
-        }
         if (userNumbers.size() != Constants.BASEBALL_NUMBER_SIZE) {
             throw new IllegalArgumentException("숫자 3개만 입력해주세요.");
         }
