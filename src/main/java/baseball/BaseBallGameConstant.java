91fe4420a79616c44273ed122b8c0c58b6e9b335
minjong kim <godminjong@naver.com>
2023-10-23 00:33:33 +0900

    style: console 출력에 되는 반복적인 문자열 상수로 선언
    숫자 야구 게임 콘솔에 출력되는 상수들을 BaseBallGameConstant.java 에 묶어서 초기화 시킴

---
+++ src/main/java/baseball/BaseBallGameConstant.java
@@ -0,0 +1,12 @@
+package baseball;
+
+public abstract class BaseBallGameConstant {
+
+    private BaseBallGameConstant(){
+    }
+    static final String START_MESSAGE = "숫자 야구를 시작합니다.";
+    static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    static final String CLEAR_MESSAGE = "3개의 숫자를 모두 맞히셨습니다.! 게임 종료";
+    static final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+}
