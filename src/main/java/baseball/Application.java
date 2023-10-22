827264d5df566cb6be8298f6a7c873b18ad2d73e
ohhamma <judyham10634@naver.com>
2023-10-22 17:02:05 +0900

    :art: refactor: 메소드 이름 변환

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,6 @@ public class Application {
     public static void main(String[] args) {
         Game game = new Game();
 
-        game.printStartPhrase();
+        game.startGame();
     }
 }
--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -2,7 +2,7 @@ package baseball;
 
 public class Game {
     /* Starts the game */
-    public void printStartPhrase() {
+    public void startGame() {
         String startPhrase = "숫자 야구 게임을 시작합니다.";
         System.out.println(startPhrase);
     }
