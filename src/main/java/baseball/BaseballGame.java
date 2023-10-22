05e2ae03959febbbfbeff2ac89a6c262c70560ba
bboodd <92453279+bboodd@users.noreply.github.com>
2023-10-22 23:14:17 +0900

    feat:숫자 입력기능 구현

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,16 @@
+package baseball;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class BaseballGame {
+
+    private Computer computer;
+    public void start(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (true){
+            System.out.print("숫자를 입력해주세요 : ");
+            String inputNum = readLine();
+        }
+
+    }
+}
