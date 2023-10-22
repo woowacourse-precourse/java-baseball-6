0229688997a74509e16d947684de1559bfba0bf9
zangsu <zangsu_@naver.com>
2023-10-22 14:50:00 +0900

    refactor : seperate NumberFormat validation, duplicated validation

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,7 +1,6 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,56 +15,63 @@ public class Player {
 
     /**
      * 사용자에게 서로 다른 3가지 정수를 입력받는다.
+     *
      * @return 사용자가 입력한 정수를 List<Integer>에 담아 반환
      * @throws IllegalArgumentException
      */
-    public List<Integer> getInput() throws IllegalArgumentException{
+    public List<Integer> getInput() throws IllegalArgumentException {
         String input = Console.readLine();
         return getInput(input);
     }
 
-    public List<Integer> getInput(String input) throws IllegalArgumentException{
+    public List<Integer> getInput(String input) throws IllegalArgumentException {
         String[] tokens = input.split("");
-        if(tokens.length != 3){
+        if (tokens.length != 3) {
             throw new IllegalArgumentException(INPUT_MUST_3_NUMBER_EXCEPTION);
         }
-        ArrayList<Integer> inputNumbers = new ArrayList<>();
-        for(int i = 0; i<3; i++){
-            try{
-                int n = Integer.parseInt(tokens[i]);
-                if(inputNumbers.contains(n)){
-                    throw new IllegalArgumentException(INPUT_MUST_NOT_DUPLICATED_EXCEPTION);
-                }
-                inputNumbers.add(n);
-            }catch (NumberFormatException e){
+
+        int[] intTokens = new int[3];
+        for (int i = 0; i < 3; i++) {
+            try {
+                intTokens[i] = Integer.parseInt(tokens[i]);
+            } catch (NumberFormatException e) {
                 throw new IllegalArgumentException(NUMBER_FORMAT_EXCEPTION);
             }
+        }
+
+        ArrayList<Integer> inputNumbers = new ArrayList<>();
+        for (int number : intTokens) {
+            if (inputNumbers.contains(number)) {
+                throw new IllegalArgumentException(INPUT_MUST_NOT_DUPLICATED_EXCEPTION);
+            }
+            inputNumbers.add(number);
         }
         return inputNumbers;
     }
 
     /**
      * 재시작 여부를 반환한다.
+     *
      * @return true - 재시작의 경우, false - 종료의 경우
      * @throws IllegalArgumentException
      */
-    public boolean restart() throws IllegalArgumentException{
+    public boolean restart() throws IllegalArgumentException {
         String input = Console.readLine();
         return restart(input);
     }
 
     //테스트를 위한 오버로딩
-    public boolean restart(String s) throws IllegalArgumentException{
+    public boolean restart(String s) throws IllegalArgumentException {
         try {
             int input = Integer.parseInt(s);
-            if(input == RESTART) {
+            if (input == RESTART) {
                 return true;
             }
             if (input == END) {
                 return false;
             }
             throw new IllegalArgumentException(WRONG_INPUT_IN_RESTART);
-        }catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException(NUMBER_FORMAT_EXCEPTION);
         }
     }
