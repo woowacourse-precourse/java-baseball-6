7ee17340ec61337967fa93a805008cfbe2a88247
sangwopark19 <sangwopark19@gmail.com>
2023-10-22 22:53:22 +0900

    RandomNumber, BallHint, BallInput 멤버변수 추가

--- src/main/java/baseball/game/BaseballGame.java
+++ src/main/java/baseball/game/BaseballGame.java
@@ -1,4 +1,13 @@
 package baseball.game;
 
+import baseball.domain.BallHint;
+import baseball.domain.BallInput;
+import baseball.domain.RandomNumber;
+
 public class BaseballGame {
+    private final RandomNumber randomNumber;
+    private final BallHint ballHint;
+    private final BallInput ballInput;
+
+
 }
