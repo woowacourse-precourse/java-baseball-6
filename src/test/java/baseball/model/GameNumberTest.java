ec53b0915945c74cdeafa8e10ffd72ccbe7cef2f
yoonjuho <dbswn990@naver.com>
2023-10-23 03:52:03 +0900

    test(GameNumberTest): 숫자는 특정 수와 자신을 비교할 수 있다

--- src/test/java/baseball/model/GameNumberTest.java
+++ src/test/java/baseball/model/GameNumberTest.java
@@ -1,14 +1,18 @@
 package baseball.model;
 
+import baseball.constant.GameResult;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameNumberTest {
@@ -34,6 +38,26 @@ class GameNumberTest {
         assertThrows(IllegalArgumentException.class,() -> new GameNumber(inValidNumbers));
     }
 
+    @DisplayName("같은 수애 같은 자리에 있는 수의 개수를 구할 수 있다")
+    @ParameterizedTest
+    @MethodSource("matchCountData")
+    void matchCount(List<Integer> computerNumber, List<Integer> userNumber, long expected){
+        GameNumber computerGameNumber = new GameNumber(computerNumber);
+        GameNumber userGameNumber = new GameNumber(userNumber);
+        long result = computerGameNumber.matchCount(userGameNumber);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("같은 수에 다른 자리에 있는 수의 개수를 구할 수 있다")
+    @ParameterizedTest
+    @MethodSource("containsCountData")
+    void containsCount(List<Integer> computerNumber, List<Integer> userNumber, long expected){
+        GameNumber computerGameNumber = new GameNumber(computerNumber);
+        GameNumber userGameNumber = new GameNumber(userNumber);
+        long result = computerGameNumber.containsCount(userGameNumber);
+        assertThat(result).isEqualTo(expected);
+    }
+
 
     static Stream<Arguments> inValidSizeData() {
         return Stream.of(
@@ -48,11 +72,24 @@ class GameNumberTest {
                 Arguments.of(Arrays.asList(11, 22, 33))
         );
     }
-    static Stream<Arguments> inValidDuplicateData() {
+    static Stream<Arguments> matchCountData() {
+            List<Integer> computerNumber = Arrays.asList(4, 2, 3);
+            return Stream.of(
+                    Arguments.of(computerNumber, Arrays.asList(4, 2, 3), 3L),
+                    Arguments.of(computerNumber, Arrays.asList(1, 2, 3), 2L),
+                    Arguments.of(computerNumber, Arrays.asList(4, 3, 2), 1L),
+                    Arguments.of(computerNumber, Arrays.asList(3, 4, 5), 0L)
+        );
+    }
+    static Stream<Arguments> containsCountData() {
+        List<Integer> computerNumber = Arrays.asList(4, 2, 3);
         return Stream.of(
-                Arguments.of(Arrays.asList(1, 2, 1)),
-                Arguments.of(Arrays.asList(2, 2, 2)),
-                Arguments.of(Arrays.asList(5, 3, 3))
+                Arguments.of(computerNumber, Arrays.asList(4, 2, 3), 3L),
+                Arguments.of(computerNumber, Arrays.asList(4, 3, 2), 3L),
+                Arguments.of(computerNumber, Arrays.asList(1, 2, 3), 2L),
+                Arguments.of(computerNumber, Arrays.asList(3, 2, 5), 2L),
+                Arguments.of(computerNumber, Arrays.asList(3, 6, 5), 1L),
+                Arguments.of(computerNumber, Arrays.asList(6, 7, 8), 0L)
         );
     }
 }
