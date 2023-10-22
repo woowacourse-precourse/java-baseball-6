de44132dd19b88ce4e6148b54512340021169d75
bboodd <92453279+bboodd@users.noreply.github.com>
2023-10-23 01:13:40 +0900

    refactor:숫자 입력기능 메서드로 리펙토링

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -4,13 +4,27 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class BaseballGame {
 
+    String inputNumbers;
+    int strike = 0;
+    int ball = 0;
+
     private Computer computer;
+
     public void start(){
+        computer = new Computer();
+        computer.createNumbers();
         System.out.println("숫자 야구 게임을 시작합니다.");
         while (true){
-            System.out.print("숫자를 입력해주세요 : ");
-            String inputNum = readLine();
+            inputNumbers();
         }
-
     }
+
+    public void inputNumbers(){
+        // check point
+        // null인지, size가 3인지, 숫자인지, 서로다른 숫자인지
+        System.out.print("숫자를 입력해주세요 : ");
+        String temp = readLine();
+        inputNumbers = temp;
+    }
+
 }
