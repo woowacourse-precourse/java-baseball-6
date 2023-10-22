783c22d3dff32578c8cea0943a7300156ae00206
leesangjun <hubls@naver.com>
2023-10-22 13:53:18 +0900

    style(Computer): tab을 space로 수정

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -1,23 +1,24 @@
 package baseball.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
-	List<Integer> computerNumber = new ArrayList<>();
+    List<Integer> computerNumber = new ArrayList<>();
 
-	public void setComputerNumber() {
-		computerNumber.clear();
-		while (computerNumber.size() < 3) {
-			int randomNumber = Randoms.pickNumberInRange(1, 9);
-			if (!computerNumber.contains(randomNumber)) {
-				computerNumber.add(randomNumber);
-			}
-		}
-	}
+    public void setComputerNumber() {
+        computerNumber.clear();
+        while (computerNumber.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumber.contains(randomNumber)) {
+                computerNumber.add(randomNumber);
+            }
+        }
+    }
 
-	public List<Integer> getComputerNumber() {
-		return computerNumber;
-	}
+    public List<Integer> getComputerNumber() {
+        return computerNumber;
+    }
 }
