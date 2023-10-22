6f5c3bf88b285e2b7eaf78e075ddfd4e7c145ee0
JaeHunJang <wuddlaa@gmail.com>
2023-10-22 22:56:36 +0900

    fix(InputView): change print() to println()

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -9,7 +9,7 @@ public class InputView {
     }
 
     public static String inputRestart() {
-        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         return Console.readLine();
     }
 }
