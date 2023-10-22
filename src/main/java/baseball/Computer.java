333b08a847b5c23d725a5e40d10525136f96f291
wda067 <mg6359@naver.com>
2023-10-23 00:24:58 +0900

    feat: 컴퓨터 랜덤 수 생성하는 기능 구현

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,30 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+
+    private List<Integer> computerNumber;
+
+    public Computer() {
+        makeNumber();
+//        System.out.println(computerNumber);
+    }
+
+    public void makeNumber() {
+        computerNumber = new ArrayList<>();
+        while (computerNumber.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumber.contains(randomNumber)) {
+                computerNumber.add(randomNumber);
+            }
+        }
+    }
+
+    public List<Integer> getComputerNumber() {
+        return computerNumber;
+    }
+
+}
