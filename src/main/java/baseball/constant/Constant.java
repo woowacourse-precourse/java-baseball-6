ebd83a9e710edc1835fa94ac5c4df2e3505d4611
Jihyun3478 <leejihyun3478@gmail.com>
2023-10-23 04:32:04 +0900

    Fix(Constant): 접근제어자 수정

--- src/main/java/baseball/constant/Constant.java
+++ src/main/java/baseball/constant/Constant.java
@@ -2,9 +2,9 @@ package baseball.constant;
 
 public class Constant {
 
-    private static final int NUMBER_LENGTH = 3;
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 9;
-    private static final int RESTART = 1;
-    private static final int END = 2;
+    public static final int NUMBER_LENGTH = 3;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 9;
+    public static final int RESTART = 1;
+    public static final int END = 2;
 }
