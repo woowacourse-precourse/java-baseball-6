7ee64633170476376b5063cbbde0786c77e241d9
waibu95 <wpgur1514@gmail.com>
2023-10-22 20:09:46 +0900

    refactor : add Umpire
    Umpire(심판)을 추가해서 기능을 분리했습니다. 게임 당 심판을 호출해서 게임을 진행하고 종료되면 시작, 재시작을 판단하도록 책임을 줬습니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,6 +5,6 @@ public class Application {
 
     public static void main(String[] args) {
         NumberBaseballGame game = new NumberBaseballGame();
-        game.StartGame();
+        game.PlayBall();
     }
 }
--- src/main/java/baseball/NumberBaseballGame.java
+++ src/main/java/baseball/NumberBaseballGame.java
@@ -1,8 +1,6 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class NumberBaseballGame {
@@ -11,84 +9,58 @@ public class NumberBaseballGame {
     private static final String INPUT_MSG = "숫자를 입력해주세요 :";
 
 
-    void StartGame() {
+    void PlayBall() {
         System.out.printf(GAME_START_MSG);
         boolean isGaming = true;
         while (isGaming) {
-
-            PlayBall();
-
-            System.out.printf(GAME_OVER_MSG);
-            String userChoice = camp.nextstep.edu.missionutils.Console.readLine();
-
-            if (userChoice.equals("2")) {
-                isGaming = false;
-            } else if (!userChoice.equals("1")) {
-                throw new IllegalArgumentException("잘못된 입력입니다.");
-            }
+            Umpire umpire = new Umpire();
+            isGaming = StartInning(umpire);
         }
     }
 
-    void PlayBall() {
-        boolean isCorrect = false;
-        List<Integer> answer = new ArrayList<>();
+    private static boolean ChoicePlayGameToUser() {
+        String userChoice = camp.nextstep.edu.missionutils.Console.readLine();
 
-        while (answer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!answer.contains(randomNumber)) {
-                answer.add(randomNumber);
-            }
+        if (userChoice.equals("1")) {
+            return true;
         }
+        if (userChoice.equals("2")) {
+            return false;
+        }
+        throw new IllegalArgumentException("잘못된 입력 입니다.");
+    }
 
-        while (!isCorrect) {
-            List<Integer> guess = new ArrayList<>();
+    private static boolean StartInning(Umpire umpire) {
+
+        while (!Umpire.IsCorrect()) {
+            List<Integer> pitchBall;
+            String input;
+
             System.out.println(INPUT_MSG);
-            String input = camp.nextstep.edu.missionutils.Console.readLine();
-            //입력값 컨버젼
-            if (input.length() != 3) {
-                throw new IllegalArgumentException("입력값이 3이 아닙니다");
-            }
-            for (int i = 0; i < 3; i++) {
-                int number = Character.getNumericValue(input.charAt(i));
-                if (!guess.contains(number)) {
-                    guess.add(number);
-                } else {
-                    throw new IllegalArgumentException("숫자가 아닙니다");
-                }
-            }
-            //결과 판정
-            HashMap<String, Integer> result = new HashMap<>();
-            result.put("스트라이크", 0);
-            result.put("볼", 0);
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    if (i == j && answer.get(i).equals(guess.get(j))) {
-                        result.put("스트라이크", result.get("스트라이크") + 1);
-                    }
-                    if (i != j && answer.get(i).equals(guess.get(j))) {
-                        result.put("볼", result.get("볼") + 1);
-                    }
-                }
-            }
-            //결과 출력
-            String result_msg = "";
-            if (result.get("볼") != 0) {
-                result_msg += result.get("볼") + "볼";
-            }
-            if (result.get("스트라이크") != 0) {
-                result_msg += " " + result.get("스트라이크") + "스트라이크";
-                if (result.get("스트라이크") == 3) {
-                    result_msg += "\n3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-                }
-            }
-            if (result_msg.isEmpty()) {
-                result_msg = "낫싱";
-            }
+            input = camp.nextstep.edu.missionutils.Console.readLine();
+            pitchBall = ChoicePitchBall(input);
+            umpire.Pitch(pitchBall);
 
-            if (result.get("스트라이크") == 3) {
-                isCorrect = true;
-            }
-            System.out.println(result_msg.trim());
         }
+        System.out.printf(GAME_OVER_MSG);
+        return ChoicePlayGameToUser();
+    }
+
+    private static List<Integer> ChoicePitchBall(String input) {
+        //입력값 컨버젼
+        List<Integer> guess = new ArrayList<>();
+
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("값의 크기가 3이 아닙니다");
+        }
+        for (int i = 0; i < 3; i++) {
+            int number = Character.getNumericValue(input.charAt(i));
+            if (!guess.contains(number)) {
+                guess.add(number);
+            } else {
+                throw new IllegalArgumentException("올바른 선택지가 아닙니다");
+            }
+        }
+        return guess;
     }
 }
