9183439ae3bd47c81c8336eb7a0615e0472bcfe9
chs98412 <chs98412@gmail.com>
2023-10-22 17:38:02 +0900

    feat: 임시 게임 흐름 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,20 +1,55 @@
 package baseball;
 
+import baseball.controller.GameController;
 import baseball.controller.InputController;
 import baseball.controller.OutputController;
+import baseball.entity.Game;
 import baseball.entity.Player;
+import baseball.entity.Result;
+
 import static baseball.controller.OutputController.*;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
+        Game game = new Game();
+
         Player player = new Player();
         InputController inputController = new InputController();
         OutputController outputController = new OutputController();
+        GameController gameController = new GameController();
+        game.printAnswer();
+        outputController.cliOutputln(START);
 
-        outputController.cliOutput(START);
+        while (true) {
+
+
+        outputController.cliOutput(INPUT);
         player.setInputString(inputController.playerInput());
-        System.out.println(player.getInputString());
+        Result result = gameController.game(player, game);
+
+            System.out.println(player.getInputString());
+
+            if (result.isAnswer) {
+                outputController.cliOutputln(ANSWER);
+                outputController.cliOutputln(AFTER);
+                break;
+            } else if (result.strike == 0 && result.ball != 0) {
+                System.out.print(result.ball);
+                outputController.cliOutputln(BALL);
+            } else if (result.strike != 0 && result.ball == 0) {
+                System.out.print(result.strike);
+                outputController.cliOutputln(STRIKE);
+            } else {
+                System.out.print(result.ball);
+                outputController.cliOutput(BALL);
+                System.out.print(result.strike);
+                outputController.cliOutputln(STRIKE);
+            }
+
+
+
+        }
     }
 }
