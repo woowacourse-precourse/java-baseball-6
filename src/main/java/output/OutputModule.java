1468b38bfcdcd46d20449bbf6bf201278ed905a0
Lee JaeHyuk <80966413+JH22222@users.noreply.github.com>
2023-10-22 19:05:24 +0900

    refactor: 볼, 스트라이크 출력문 변경

--- src/main/java/output/OutputModule.java
+++ src/main/java/output/OutputModule.java
@@ -4,8 +4,8 @@ public class OutputModule {
 
     private final String startGame = "숫자 야구 게임을 시작합니다.";
     private final String inputMessage = "숫자를 입력해주세요 : ";
-    private final String ball = "볼";
-    private final String strike = "스트라이크";
+    private final String ballMessage = "볼";
+    private final String strikeMessage = "스트라이크";
     private final String out = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private final String oneMoreTime = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
@@ -20,12 +20,12 @@ public class OutputModule {
         System.out.print(inputMessage);
     }
 
-    public void printBall() {
-        System.out.print(ball);
+    public void printBall(int ball) {
+        System.out.print(ball + ballMessage);
     }
 
-    public void printStrike() {
-        System.out.print(strike);
+    public void printStrike(int strike) {
+        System.out.print(strike + strikeMessage);
     }
 
     public void printOut() {
