31afad5fc03801ba440f2ccd0858d2b3cbc2dbdb
jinnxyoung <wlsdud7722@naver.com>
2023-10-22 18:32:37 +0900

    Revert "Revert "feat : 게임 실행 도메인 Game.java 구현""
    This reverts commit b6b9caceb43cf46f9cddf79bcdc2fc00fccee19c.

---
+++ src/main/java/baseball/domain/User.java
@@ -0,0 +1,13 @@
+package baseball.domain;
+
+public class User {
+    int[] userNumbers;
+
+    public int[] getUserNumbers() {
+        return userNumbers;
+    }
+
+    public void setUserNumbers(int[] userNumbers) {
+        this.userNumbers = userNumbers;
+    }
+}
