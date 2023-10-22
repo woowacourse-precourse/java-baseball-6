df2b8da6007aab9e72a3966d751ddd24fcef5730
SSung023 <50323157+SSung023@users.noreply.github.com>
2023-10-22 15:19:11 +0900

    feat: add printing start & input message
    - 게임 시작 메시지와 입력 메시지 출력하는 기능 추가

---
+++ src/main/java/baseball/Util/MessageUtil.java
@@ -0,0 +1,13 @@
+package baseball.Util;
+
+import baseball.constants.Message;
+
+public class MessageUtil {
+    public void printStart() {
+        System.out.println(Message.GAME_START);
+    }
+
+    public void printInput() {
+        System.out.print(Message.INPUT);
+    }
+}
