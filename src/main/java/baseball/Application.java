1e145246e60b7b45da13bba6c3e3572076112eea
ohhamma <judyham10634@naver.com>
2023-10-22 15:02:51 +0900

    :sparkles: feat: 게임 시작 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,9 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Game game = new Game();
+
+        // 1. 게임 시작
+        game.PrintStartPhrase();
     }
 }
---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,11 @@
+package baseball;
+
+public class Game {
+
+    private String startPhrase;
+
+    public void PrintStartPhrase() {
+        startPhrase = "숫자 야구 게임을 시작합니다.";
+        System.out.println(startPhrase);
+    }
+}
