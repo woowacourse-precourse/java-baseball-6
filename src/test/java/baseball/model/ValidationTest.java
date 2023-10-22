64284136239404b01f786ef89f58330e7d981eb5
leeedohyun <ldh19738@gmail.com>
2023-10-22 12:59:59 +0900

    test: 입력한 숫자 검증 로직 통합 후 리팩토링

--- src/test/java/baseball/model/ValidationTest.java
+++ src/test/java/baseball/model/ValidationTest.java
@@ -8,25 +8,25 @@ class ValidationTest {
 
     @Test
     public void 중복된_숫자_입력시_예외_발생() {
-        assertThatThrownBy(() -> Validation.validateSameNumber("144"))
+        assertThatThrownBy(() -> Validation.validateInputThreeNumber("144"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void 입력한_문자열에_문자가_포함되어_있으면_예외_발생() {
-        assertThatThrownBy(() -> Validation.validateThatInputIsNumeric("14j"))
+        assertThatThrownBy(() -> Validation.validateInputThreeNumber("14j"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void 입력한_숫자_개수가_3개가_넘으면_예외_발생() {
-        assertThatThrownBy(() -> Validation.validateNumberOfNumbers("1234"))
+        assertThatThrownBy(() -> Validation.validateInputThreeNumber("1234"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void 입력한_숫자_개수가_3개보다_적으면_예외_발생() {
-        assertThatThrownBy(() -> Validation.validateNumberOfNumbers("34"))
+        assertThatThrownBy(() -> Validation.validateInputThreeNumber("34"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
