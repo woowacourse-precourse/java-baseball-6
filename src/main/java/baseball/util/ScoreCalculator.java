a68292a0722377da5b0c66b9e9503f0f2735837c
Dongwoongkim <rlaehddnd0422@naver.com>
2023-10-22 15:35:40 +0900

    feat : (ScoreCalculator) Strike, Ball 계산 기능 구현

--- src/main/java/baseball/util/ScoreCalculator.java
+++ src/main/java/baseball/util/ScoreCalculator.java
@@ -1,9 +1,16 @@
 package baseball.util;
 
+import baseball.model.Score;
 import java.util.List;
 import java.util.stream.IntStream;
 
 public class ScoreCalculator {
+
+    public Score calculateScore(List<Integer> playerInput, List<Integer> answer) {
+        Integer strike = calculateStrike(playerInput, answer);
+        Integer ball = calculateBall(playerInput, answer);
+        return new Score(strike, ball);
+    }
 
     public Integer calculateStrike(List<Integer> playerInput, List<Integer> answer) {
         return (int) IntStream.range(0, answer.size())
