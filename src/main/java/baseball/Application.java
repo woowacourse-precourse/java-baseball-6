883878d2fb1b0416442538f75f26e54d3332d01c
5tr1ker <tjseocld@naver.com>
2023-10-22 23:40:44 +0900

    fix: 로직 버그 수정
    게임 무한 반복 버그 수정 및 문자열 입력 시 예외처리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,15 +9,15 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.printf("숫자 야구 게임을 시작합니다.");
+        System.out.println("숫자 야구 게임을 시작합니다.");
 
-        while(true) {
+        while (true) {
             runBullsAndCows();
 
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             int input = checkValidInputAtMain(readLine());
 
-            if(input == 2) {
+            if (input == 2) {
                 break;
             }
         }
@@ -26,23 +26,25 @@ public class Application {
 
     public static void runBullsAndCows() {
         int answer = createRandomNumber();
-        while(true) {
-            System.out.printf("\n숫자를 입력해주세요 : ");
+
+        while (true) {
+            System.out.printf("숫자를 입력해주세요 : ");
             int input = checkValidInputAtGame(readLine());
 
-            if(isCorrect(input , answer)) {
+            if (isCorrect(input, answer)) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                return;
             }
         }
     }
 
-    public static boolean isCorrect(int input , int answer) {
+    public static boolean isCorrect(int input, int answer) {
         int ball = 0;
         int strike = 0;
 
         int inputDigit_1 = input % 10;
         int answerDigit_1 = answer % 10;
-        if(inputDigit_1 == answerDigit_1) {
+        if (inputDigit_1 == answerDigit_1) {
             strike += 1;
         } else if (checkNumber[inputDigit_1]) {
             ball += 1;
@@ -50,7 +52,7 @@ public class Application {
 
         int inputDigit_10 = input % 100 / 10;
         int answerDigit_10 = answer % 100 / 10;
-        if(inputDigit_10 == answerDigit_10) {
+        if (inputDigit_10 == answerDigit_10) {
             strike += 1;
         } else if (checkNumber[inputDigit_10]) {
             ball += 1;
@@ -58,60 +60,70 @@ public class Application {
 
         int inputDigit_100 = input / 100;
         int answerDigit_100 = answer / 100;
-        if(inputDigit_100 == answerDigit_100) {
+        if (inputDigit_100 == answerDigit_100) {
             strike += 1;
         } else if (checkNumber[inputDigit_100]) {
             ball += 1;
         }
 
-        if(strike == 3) {
+        if (strike == 3) {
             System.out.println("3스트라이크");
             return true;
         }
 
-        if(ball == 0) {
+        if (ball == 0 && strike == 0) {
+            System.out.println("낫싱");
+        } else if (ball == 0) {
             System.out.println(strike + "스트라이크");
-        } else if(strike == 0) {
-            System.out.println(ball + "볼 " + strike + "스트라이크");
+        } else if (strike == 0) {
+            System.out.println(ball + "볼");
         } else {
-            System.out.printf("%d볼 %d스트라이크" , ball , strike);
+            System.out.printf("%d볼 %d스트라이크\n", ball, strike);
         }
         return false;
     }
 
     public static int checkValidInputAtGame(String input) {
-        int userInput = Integer.parseInt(input);
+        try {
+            int userInput = Integer.parseInt(input);
 
-        if(userInput < 100 || userInput > 999) {
+            if (userInput < 100 || userInput > 999) {
+                throw new IllegalArgumentException("Wrong Input");
+            }
+            return userInput;
+
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("Wrong Input");
         }
-
-        return userInput;
     }
 
     public static int checkValidInputAtMain(String input) {
-        int userInput = Integer.parseInt(input);
+        try {
+            int userInput = Integer.parseInt(input);
 
-        if(userInput < 1 || userInput > 2) {
+            if (userInput < 1 || userInput > 2) {
+                throw new IllegalArgumentException("Wrong Input");
+            }
+            return userInput;
+
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("Wrong Input");
         }
-
-        return userInput;
     }
 
     public static int createRandomNumber() {
         checkNumber = new boolean[10];
         int number = 0;
 
-        for(int i = 0; i < 3; i++) {
-            int randomNumber = pickNumberInRange(1 , 9);
+        for (int i = 0; i < 3; i++) {
+            int randomNumber = pickNumberInRange(1, 9);
 
-            if(checkNumber[randomNumber]) {
-                i --;
+            if (checkNumber[randomNumber]) {
+                i--;
                 continue;
             }
 
-            number += Math.pow(10 , i) * randomNumber;
+            number += Math.pow(10, i) * randomNumber;
             checkNumber[randomNumber] = true;
         }
 
