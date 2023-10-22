8e1600e231b72c53b5444be105ce06b8a004f99f
gpsrlin115 <lefd123@gmail.com>
2023-10-22 14:01:46 +0900

    숫자 입력 확인

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자를 입력해주세요 : ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        System.out.println("input : " + input);
+        
     }
 }
