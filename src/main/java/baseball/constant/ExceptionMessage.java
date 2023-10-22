62b17b38e1c8ef1e77d5306bdbc779edec04aa23
Jung Yunho <a01049048063@gmail.com>
2023-10-23 07:55:30 +0900

    refactor(ExceptionMessage): 공통된 문자로 상수로 변경

--- src/main/java/baseball/constant/ExceptionMessage.java
+++ src/main/java/baseball/constant/ExceptionMessage.java
@@ -1,12 +1,13 @@
 package baseball.constant;
 
 public enum ExceptionMessage {
-    OUT_OF_ONE_TO_NIME("[ERROR] 1 ~ 9 사이의 값만 입력해주세요"),
-    OUT_OF_NUMBER_RANGE("[ERROR] %d자리 숫자를 입력하셔야 합니다"),
-    HAVE_DUPLICATE_NUMBER("[ERROR] 중복된 값이 있습니다."),
-    NOT_AVAILABLE_STRING("[ERROR] 숫자만 입력해주세요."),
+    OUT_OF_ONE_TO_NIME("1 ~ 9 사이의 값만 입력해주세요"),
+    OUT_OF_NUMBER_RANGE("%d자리 숫자를 입력하셔야 합니다"),
+    HAVE_DUPLICATE_NUMBER("중복된 값이 있습니다."),
+    NOT_AVAILABLE_STRING("숫자만 입력해주세요."),
     ;
 
+    private static final String ERROR = "[ERROR]";
     private final String message;
 
     private ExceptionMessage(String message) {
@@ -14,6 +15,6 @@ public enum ExceptionMessage {
     }
 
     public String getMessage() {
-        return message;
+        return ERROR + message;
     }
 }
