12aec4c70bd293c043ede60b467e5e5336d69b25
Jay <injaesong0@gmail.com>
2023-10-23 01:50:26 +0900

    feat: implement printStart

---
+++ src/main/java/baseball/Communicator.java
@@ -0,0 +1,10 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Communicator {
+    private final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public void printStart() {
+        System.out.println(START_MESSAGE);
+    }
+}
