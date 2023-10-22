dde61536d509766af5d470b14bd0cdd803cdd0f0
Hanjaemo <hjm2530@gmail.com>
2023-10-23 03:58:16 +0900

    refactor(PitchResult): 불필요한 코드 제거

--- src/main/java/baseball/domain/PitchResult.java
+++ src/main/java/baseball/domain/PitchResult.java
@@ -2,11 +2,5 @@ package baseball.domain;
 
 public enum PitchResult {
 
-    BALL("볼"), STRIKE("스트라이크");
-
-    private final String description;
-
-    PitchResult(String description) {
-        this.description = description;
-    }
+    BALL, STRIKE
 }
