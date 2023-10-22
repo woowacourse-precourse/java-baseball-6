1af225d71999d40d06e761cdb2cd42e0cbb29490
irerin07 <counterchord89@gmail.com>
2023-10-23 03:00:05 +0900

    #10 랜덤 숫자 생성 기능 구현 - property에서 지정한 자릿수의 숫자를 생성하는 기능 구현

--- src/main/java/baseball/random/RandomNumberGenerator.java
+++ src/main/java/baseball/random/RandomNumberGenerator.java
@@ -1,9 +1,30 @@
 package baseball.random;
 
+import baseball.property.BaseballGameProperty;
+import camp.nextstep.edu.missionutils.Randoms;
+
 /**
  * @author 민경수
  * @description random number generator
  * @since 2023.10.23
  **********************************************************************************************************************/
-public class RandomNumberGenerator {
+public class RandomNumberGenerator implements NumberGenerator{
+
+  private BaseballGameProperty baseballGameProperty;
+
+  public RandomNumberGenerator(BaseballGameProperty baseballGameProperty) {
+    this.baseballGameProperty = baseballGameProperty;
+  }
+
+  @Override
+  public int[] generateNumber() {
+    int[] randomNumbers = new int[baseballGameProperty.digit()];
+
+    for (int i = 0; i < baseballGameProperty.digit(); i++) {
+      randomNumbers[i] = Randoms.pickNumberInRange(baseballGameProperty.usableNumberFrom(), baseballGameProperty.usableSingleNumberTo());
+    }
+
+    return randomNumbers;
+  }
+
 }
