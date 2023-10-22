dec92caeea5cea3a137fb7a245a9c5b939a52add
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2023-10-22 15:24:55 +0900

    feat: 컴퓨터의 수 random 값을 생성한다

--- src/main/java/baseball/controller/PlayBaseballGame.java
+++ src/main/java/baseball/controller/PlayBaseballGame.java
@@ -1,8 +1,14 @@
 package baseball.controller;
 
+import baseball.domain.Ball;
+import baseball.service.Computer;
+
 public class PlayBaseballGame {
+    private final Computer computer = new Computer();
+    private Ball computerNumber;
 
     public void play() {
-
+        computerNumber = computer.createRandomNumber();
     }
+
 }
---
+++ src/main/java/baseball/domain/Ball.java
@@ -0,0 +1,12 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Ball {
+    private List<Integer> numbers;
+
+    public Ball(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+}
+
