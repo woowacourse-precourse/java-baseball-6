dd162562de49891b25c68f35edfc6ce062f52a15
yummygyudon <bang2brew@gmail.com>
2023-10-22 23:51:05 +0900

    DOCS : GameLauncher 메서드 주석 작성

--- src/main/java/baseball/application/GameLauncher.java
+++ src/main/java/baseball/application/GameLauncher.java
@@ -18,6 +18,11 @@ public class GameLauncher {
     public GameLauncher(Computer computer) {
         this.computer = computer;
     }
+
+    /**
+     * 한 게임을 실행하는 유일한 public 메서드
+     * @see baseball.Application
+     */
     public void execute() {
         boolean finishFlag;
         do {
@@ -29,6 +34,14 @@ public class GameLauncher {
         } while (!finishFlag);
         Printer.print(Command.END_GAME.getCommand());
     }
+
+    /**
+     * 입력값을 한 글자씩 분할하여 Pitch 객체로 변환하는 메서드
+     *
+     * @param input 사용자의 입력값
+     * @return 입력값에 대한 Pitch 변환 객체
+     * @see Pitch
+     */
     private Pitch createPitch(String input) {
         List<Integer> pitches = Arrays.stream(input.split(""))
                 .map(Integer::parseInt)
@@ -36,10 +49,16 @@ public class GameLauncher {
         return new Pitch(pitches);
     }
 
-    private Judgement judge(Pitch player) {
+    /**
+     * Pitch 에 대해 본 게임의 Computer 와 비교하여 구종을 판별하는 메서드
+     * @param pitch 사용자가 입력한 구종
+     * @return 판정 결과
+     * @see Judgement
+     */
+    private Judgement judge(Pitch pitch) {
         return new Judgement(
-                computer.countBall(player.getPitches()),
-                computer.countStrike(player.getPitches())
+                computer.countBall(pitch.getPitches()),
+                computer.countStrike(pitch.getPitches())
         );
     }
 
