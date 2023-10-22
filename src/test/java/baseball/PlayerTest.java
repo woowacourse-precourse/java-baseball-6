9a9a7ba62aadd7261cdc703b4416f92143dd1cd4
wochae <officialwochae@gmail.com>
2023-10-22 16:30:37 +0900

    [CHORE]: following java convention

--- src/test/java/baseball/PlayerTest.java
+++ src/test/java/baseball/PlayerTest.java
@@ -1,63 +1,67 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-
 public class PlayerTest extends NsTest {
+
   private static final InputStream originalSystemIn = System.in;
 
-  @BeforeEach
-  public void setUp() {
-  }
 
   @AfterEach
   public void teardown() {
-
     System.setIn(originalSystemIn);
   }
 
   @Test
-  @DisplayName("숫자가 세 자리 수가 아닌 경우")
+  public void test() {
+    System.out.println("test");
+  }
+
+
+  @Test()
   public void invalidThreeNumbers() {
-    System.setIn(new ByteArrayInputStream("1234".getBytes()));
-    String read = Console.readLine();
+    System.setIn(new ByteArrayInputStream("1234\n".getBytes()));
+
     Player player = new Player();
 
     //when,
-    boolean isValid = player.validateInput(read);
 
     //then
-    assertEquals(false, isValid);
+    assertSimpleTest(
+        () -> {
+          boolean isValid = player.validateInput(Console.readLine());
+          assertFalse(isValid);
+        }
+    );
     Console.close();
-    // 첫 번째 테스트
   }
 
   @Test
-  public void test2() {
-    System.setIn(new ByteArrayInputStream("abcd".getBytes()));
-    String read = Console.readLine();
-    //when, then
+  public void invalidaNotNumbers() {
+    System.setIn(new ByteArrayInputStream("abcd\n".getBytes()));
     Player player = new Player();
-    boolean isValid = player.validateInput(read);
+
+    //when, then
 
     //then
-    assertEquals(false, isValid);
-
+    assertSimpleTest(
+        () -> {
+          boolean isValid = player.validateInput(Console.readLine());
+          assertFalse(isValid);
+        }
+    );
     Console.close();
-    //when, then
-  }
-  @Override
-  protected void runMain() {
 
+
+  }
+
+  public void runMain() {
   }
 }
--- src/test/java/baseball/RefereeTest.java
+++ src/test/java/baseball/RefereeTest.java
@@ -1,13 +1,12 @@
 package baseball;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
-
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class RefereeTest extends NsTest {
 
@@ -22,6 +21,21 @@ public class RefereeTest extends NsTest {
     String test = "597";
 
     // when, then
+
+    referee.determine(test);
+    assertThat(output()).contains("1볼 1스트라이크");
+
+  }
+
+  @Test
+  void 볼인지_스트라이크인지_판별1() {
+    // given
+    Referee referee = new Referee();
+    List<Integer> givenNumbers = new ArrayList<>(Arrays.asList(5, 8, 9));
+    referee.setGivenNumbers(givenNumbers);
+    String test = "597";
+
+//     when, then
     assertSimpleTest(
         () -> {
           referee.determine(test);
