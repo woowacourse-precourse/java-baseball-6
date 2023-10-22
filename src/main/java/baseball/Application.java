99b523f7f60220123e84b09ea4aab49c20232e05
k-kmw <kimmw2_@naver.com>
2023-10-22 09:58:31 +0900

    Fix: 3스트라이크 출력 코드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -54,6 +54,7 @@ public class Application {
              * 스트라이크 3개 -> 새로운 게임 및 종료
              */
             if(strike == 3) {
+                System.out.println(strike + "스트라이크");
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 gameStatus = Console.readLine();
