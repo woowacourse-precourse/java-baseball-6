9f56928c65661852fe1f4114430e33c53bf79480
Dowon <3120kang@gmail.com>
2023-10-22 11:02:00 +0900

    style: BaseBallGame클래스 내부 메소드들의 indent를 1로 수정

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -8,33 +8,39 @@ import view.OutputView;
 
 public class BaseBallGame implements Game{
 
-    private Balls userBalls;
-    private Balls computerBalls;
-    private static User user=new User();
-    private Computer computer = new Computer();
     private static GameRule rule = new BaseBallRule();
     private static Comparator comparator = new Comparator();
     private static Hint hint = new Hint();
     private static OutputView outputView=new OutputView();
+
+    private Balls userBalls;
+    private Balls computerBalls;
+    private User user=new User();
+    private Computer computer = new Computer();
+
     @Override
     public void start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
         computerBalls=computer.makeComputerNumber();
         userBalls=user.makeUserNumber();
         comparator.compare(computerBalls,userBalls);
-        gameProgress();
+        rulePassCheck();
         over();
     }
 
-    public void gameProgress(){
+    public void rulePassCheck(){
         boolean pass;
         do{
             pass=ruleApply();
-            if(pass==false){
-                userBalls=user.makeUserNumber();
-                comparator.compare(computerBalls,userBalls);
-            }
+            getUserNumberAndCompare(pass);
         }while(!pass);
+    }
+
+    public void getUserNumberAndCompare(boolean pass){
+        if(!pass){
+            userBalls=user.makeUserNumber();
+            comparator.compare(computerBalls,userBalls);
+        }
     }
 
     public boolean ruleApply(){
