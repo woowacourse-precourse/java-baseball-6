f5501ff0a7bd6978ab7033b7a409a9c48d2eeec0
jy016011 <39334789+jy016011@users.noreply.github.com>
2023-10-22 15:43:58 +0900

    refactor: User 클래스 캡슐화
    기존 클래스 외부에서 직접 멤버변수를 수정할 수 있던 것을 수정하였음.
    캡슐화를 무의미하게 만들 getter와 setter를 지양하고자 최대한 노력하였음.
    numberList의 경우 UserAction에서 입력 검증 후 입력받도록 하였으며, 외부로 numberList를 주게될경우 복사본을 주어 외부의 침해를 차단하도록 함.

--- src/main/java/baseball/models/User.java
+++ src/main/java/baseball/models/User.java
@@ -1,12 +1,40 @@
 package baseball.models;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static baseball.models.Constants.*;
 
 /*
  User의 변수들을 기억하는 클래스
  */
 
 public class User {
-    public List<Integer> numberList;
-    public int mind;
+    private final List<Integer> numberList = new ArrayList<>();
+    private int mind;
+
+    public void saveNumber(int digit){
+        numberList.add(digit);
+    }
+
+    public void changeMind(int restartInt){
+        mind = restartInt;
+    }
+
+    public List<Integer> showNumberList(){
+        List<Integer> clonedList = new ArrayList<>();
+        for (int i = 0; i < ANSWER_LENGTH; i++) {
+            clonedList.add(numberList.get(i));
+        }
+        return clonedList;
+    }
+
+    public int showMind(){
+        return mind;
+    }
+
+    public void clearAnswerList(){
+        numberList.clear();
+    }
+
 }
