074e9c4a067994bd2d9ea3fbaa8d91b51756adae
NAM GIBEOM <rlqja0523@naver.com>
2023-10-22 21:41:35 +0900

    refactor(Status): 필드명 변경

--- src/main/java/baseball/enums/GameStatus.java
+++ src/main/java/baseball/enums/GameStatus.java
@@ -7,18 +7,18 @@ public enum GameStatus {
     EXIT("2");
 
     private static final GameStatus[] gameStatuses = GameStatus.values();
-    private final String status;
+    private final String code;
 
-    GameStatus(String status) {
-        this.status = status;
+    GameStatus(String code) {
+        this.code = code;
     }
 
-    public static GameStatus of(String status) {
+    public static GameStatus of(String code) {
         return Arrays.stream(gameStatuses)
-                .filter(gameStatus -> gameStatus.status.equals(status))
+                .filter(gameStatus -> gameStatus.code.equals(code))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(
-                        String.format("게임 재시작은 %s, 종료는 %s이어야 합니다.", GameStatus.PLAY.status, GameStatus.EXIT.status))
+                        String.format("게임 재시작은 %s, 종료는 %s이어야 합니다.", GameStatus.PLAY.code, GameStatus.EXIT.code))
                 );
     }
 }
