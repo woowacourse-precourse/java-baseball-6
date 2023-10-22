ad87946b67ffc0893f674b39561b346133b9d49f
euniiiii <hee9920@naver.com>
2023-10-22 15:57:44 +0900

    test: 사용자 입력 관련 유효성 확인 메서드 테스트 코드 작성

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -10,8 +10,7 @@ import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -72,6 +71,144 @@ class ApplicationTest extends NsTest {
 
         // then
         assertThat(computerNumberList.size()).isEqualTo(new HashSet<>(computerNumberList).size());
+    }
+
+    @DisplayName("사용자 숫자 입력 정상처리 테스트")
+    @Test
+    void userNumberListRangeTest() {
+        // given
+        final int size = 3;
+        final String input1 = "123";
+        final String input2 = "456";
+
+        // when
+        final Throwable throwable1 = catchThrowable(() -> {
+            Player.isValidInput(input1, size);
+        });
+        final Throwable throwable2 = catchThrowable(() -> {
+            Player.isValidInput(input2, size);
+        });
+
+        // then
+        assertThat(throwable1).doesNotThrowAnyException();
+        assertThat(throwable2).doesNotThrowAnyException();
+    }
+
+    @DisplayName("사용자 숫자 입력 예외처리 테스트 - 범위")
+    @Test
+    void userNumberListRangeExceptionTest() {
+        // given
+        final int size = 3;
+        final String input1 = "String";
+        final String input2 = "12 3";
+        final String input3 = "12!";
+        final String input4 = "abcd7";
+
+        // when
+        final Throwable throwable1 = catchThrowable(() -> {
+            Player.isValidInput(input1, size);
+        });
+        final Throwable throwable2 = catchThrowable(() -> {
+            Player.isValidInput(input2, size);
+        });
+        final Throwable throwable3 = catchThrowable(() -> {
+            Player.isValidInput(input3, size);
+        });
+        final Throwable throwable4 = catchThrowable(() -> {
+            Player.isValidInput(input4, size);
+        });
+
+        // then
+        assertThat(throwable1)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자연수만 입력해야합니다.");
+        assertThat(throwable2)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자연수만 입력해야합니다.");
+        assertThat(throwable3)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자연수만 입력해야합니다.");
+        assertThat(throwable4)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자연수만 입력해야합니다.");
+    }
+
+    @DisplayName("사용자 숫자 입력 예외처리 테스트 - 숫자 개수")
+    @Test
+    void userNumberListSizeExceptionTest() {
+        // given
+        final int size = 3;
+        final String input1 = "1";
+        final String input2 = "22";
+        final String input3 = "4444";
+        final String input4 = "55555";
+
+        // when
+        final Throwable throwable1 = catchThrowable(() -> {
+            Player.isValidInput(input1, size);
+        });
+        final Throwable throwable2 = catchThrowable(() -> {
+            Player.isValidInput(input2, size);
+        });
+        final Throwable throwable3 = catchThrowable(() -> {
+            Player.isValidInput(input3, size);
+        });
+        final Throwable throwable4 = catchThrowable(() -> {
+            Player.isValidInput(input4, size);
+        });
+
+        // then
+        assertThat(throwable1)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("세 개의 수를 입력해야합니다.");
+        assertThat(throwable2)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("세 개의 수를 입력해야합니다.");
+        assertThat(throwable3)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("세 개의 수를 입력해야합니다.");
+        assertThat(throwable4)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("세 개의 수를 입력해야합니다.");
+    }
+
+    @DisplayName("사용자 숫자 입력 예외처리 테스트 - 중복")
+    @Test
+    void userNumberListDuplicationExceptionTest() {
+        // given
+        final int size = 3;
+        final String input1 = "111";
+        final String input2 = "112";
+        final String input3 = "121";
+        final String input4 = "122";
+
+        // when
+        final Throwable throwable1 = catchThrowable(() -> {
+            Player.isValidInput(input1, size);
+        });
+        final Throwable throwable2 = catchThrowable(() -> {
+            Player.isValidInput(input2, size);
+        });
+        final Throwable throwable3 = catchThrowable(() -> {
+            Player.isValidInput(input3, size);
+        });
+        final Throwable throwable4 = catchThrowable(() -> {
+            Player.isValidInput(input4, size);
+        });
+
+        // then
+        assertThat(throwable1)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("중복된 수를 입력했습니다.");
+        assertThat(throwable2)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("중복된 수를 입력했습니다.");
+        assertThat(throwable3)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("중복된 수를 입력했습니다.");
+        assertThat(throwable4)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("중복된 수를 입력했습니다.");
     }
 
     @Override
