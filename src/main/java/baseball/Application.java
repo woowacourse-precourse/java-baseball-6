de9b83c3ad22c90c215dbb3171a515e98d4775b0
belljun3395 <195850@jnu.ac.kr>
2023-10-23 03:47:57 +0900

    feat: 숫자야구 시작 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import baseball.domain.game.Game;
+import baseball.initializer.GameInitializer;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+		Game game = GameInitializer.getGame();
+		game.start();
+	}
 }
