f8f911219716d543d0c8a61ccdb13b6861dd8bb2
Jay <injaesong0@gmail.com>
2023-10-23 02:41:40 +0900

    enhance: specify messages as constants

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -3,6 +3,12 @@ package baseball;
 import java.util.List;
 
 public class Validator {
+    private static final String NON_NUMBER_CONTAINED = "입력 중 숫자가 아닌 것이 포함되었습니다.";
+    private static final String REDUNDANT_NUMBER_DETECTED = "서로 다른 세자리 수여야 합니다.";
+    private static final String INPUT_NOT_THREE_DIGITS = "입력하신 수가 세자리가 아닙니다.";
+    private static final String WHITE_SPACE_INCLUDED = "입력에는 공백이 포함되면 안됩니다.";
+    private static final String INPUT_OUT_OF_RANGE = "1~9의 수로 이루어져 있어야 합니다.";
+
     public void validateInputString(String inputString) {
         validateInputNoWhiteSpace(inputString);
         validateInputLength(inputString);
@@ -17,31 +23,31 @@ public class Validator {
     private void validateInputIsNumber(char input) {
         int inputNumber = Character.getNumericValue(input);
         if (inputNumber < 1 || inputNumber > 9) {
-            throw new IllegalArgumentException("입력 중 숫자가 아닌 것이 포함되었습니다.");
+            throw new IllegalArgumentException(NON_NUMBER_CONTAINED);
         }
     }
 
     private void validateRedundancy(List<Integer> inputNumbers, int input) {
         if (inputNumbers.contains(input)) {
-            throw new IllegalArgumentException("서로 다른 세자리 수여야 합니다.");
+            throw new IllegalArgumentException(REDUNDANT_NUMBER_DETECTED);
         }
     }
 
     private void validateInputLength(String inputString) {
         if (inputString.length() != 3) {
-            throw new IllegalArgumentException("입력하신 수가 세자리가 아닙니다.");
+            throw new IllegalArgumentException(INPUT_NOT_THREE_DIGITS);
         }
     }
 
     private void validateInputNoWhiteSpace(String inputString) {
         if (inputString.contains(" ")) {
-            throw new IllegalArgumentException("입력에는 공백이 포함되면 안됩니다.");
+            throw new IllegalArgumentException(WHITE_SPACE_INCLUDED);
         }
     }
 
     private void validateInputNoZero(String inputString) {
         if (inputString.contains("0")) {
-            throw new IllegalArgumentException("1~9의 수로 이루어져 있어야 합니다.");
+            throw new IllegalArgumentException(INPUT_OUT_OF_RANGE);
         }
     }
 }
