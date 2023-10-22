8c7c2d99a66dba60e61fa1d64da95f792c2d71b7
Lee JaeHyuk <80966413+JH22222@users.noreply.github.com>
2023-10-22 21:23:35 +0900

    feat: 게임 진행시 입력값 중복 검증 로직 추가

--- src/main/java/validate/GameInputValidator.java
+++ src/main/java/validate/GameInputValidator.java
@@ -10,6 +10,10 @@ public class GameInputValidator {
             throw new IllegalArgumentException("입력에 공백이 포함되면 안 됩니다.");
         }
 
+        if (input.chars().distinct().count() != input.length()) {
+            throw new IllegalArgumentException("입력의 숫자들은 중복되면 안 됩니다.");
+        }
+
         for (char c : input.toCharArray()) {
             if (!Character.isDigit(c)) {
                 throw new IllegalArgumentException("입력은 숫자로만 이루어져야 합니다.");
