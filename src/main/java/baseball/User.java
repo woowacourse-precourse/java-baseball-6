6e8a36a52bc86b5cce34a6750d34b62fc585b296
KimSangHyeok <tkdgur0906@naver.com>
2023-10-22 17:48:05 +0900

    feat : 유저가 선택한 숫자 저장하는 유저 클래스 생성

---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,14 @@
+package baseball;
+
+public class User {
+    private String userNumber;
+
+    public String getUserNumber() {
+        return userNumber;
+    }
+
+    public void setUserNumber(String userNumber) {
+        this.userNumber = userNumber;
+    }
+
+}
