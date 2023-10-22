da4e171781e5c54a25a51ce43356c0537f068f48
chs98412 <chs98412@gmail.com>
2023-10-22 17:37:50 +0900

    feat: 메세지 추가

--- src/main/java/baseball/controller/OutputController.java
+++ src/main/java/baseball/controller/OutputController.java
@@ -5,6 +5,10 @@ public class OutputController {
     public final static String START = "숫자 야구 게임을 시작합니다.";
     public final static String INPUT = "숫자를 입력해주세요 : ";
 
+    public final static String AFTER = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public final static String STRIKE = "스트라이크";
+    public final static String BALL = "볼";
+    public final static String ANSWER ="3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     public void cliOutputln(String ment) {
         System.out.println(ment);
     }
