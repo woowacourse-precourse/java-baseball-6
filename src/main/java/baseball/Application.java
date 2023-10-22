06beb283aff4fb7425cf4db80a08d6c154bcb30e
WonyuChoi <phillip07290@naver.com>
2023-10-22 16:04:46 +0900

    refactor: 필요 없는 코드 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,7 +6,6 @@ import baseball.view.InputView;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println(NumericRange.COUNT);
         GameController controller = new GameController(new InputView());
         controller.startGameUntilUserWant();
     }
