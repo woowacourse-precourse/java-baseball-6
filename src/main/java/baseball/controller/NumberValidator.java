43be6e3e92922355c6333108d2975d1f76326146
yunju-m <dbswnfnal@naver.com>
2023-10-22 18:48:00 +0900

    style(NumberValidator): 수정 세 자리 숫자가 아닌 입력 오류 메시지

--- src/main/java/baseball/controller/NumberValidator.java
+++ src/main/java/baseball/controller/NumberValidator.java
@@ -7,7 +7,7 @@ import java.util.stream.Stream;
 
 public class NumberValidator {
     public static final int BASEBALL_NUMBER_LENGTH = 3;
-    private static final String WRONG_LEGNTH_NUMBER_MESSAGE = "세 자리 숫자가 아닙니다."; 
+    private static final String WRONG_LEGNTH_NUMBER_MESSAGE = "세 자리 숫자를 입력해주세요."; 
     private static final String WRONG_RANGE_NUMBER_MESSAGE = "1~9 사이의 숫자만 입력해주세요.";
     private static final String WRONG_SAME_NUMBER_MESSAGE = "서로 다른 세 자리 숫자만 입력해주세요.";
 
