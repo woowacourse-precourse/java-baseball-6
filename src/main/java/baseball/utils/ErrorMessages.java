3b3f4ce8f602cefae25d30c0e21aefb1d046feea
twoosky <lhjae3@naver.com>
2023-10-22 15:41:45 +0900

    feat: 사용자 입력값에 대한 errorMessage 정의

---
+++ src/main/java/baseball/utils/ErrorMessages.java
@@ -0,0 +1,10 @@
+package baseball.utils;
+
+public class ErrorMessages {
+    public static final String PLAYER_NUMBERS_NOT_INTEGER = "[ERROR] 숫자만 입력 가능합니다.";
+    public static final String PLAYER_NUMBERS_DUPLICATE = "[ERROR] 중복된 숫자는 입력할 수 없습니다.";
+    public static final String PLAYER_NUMBERS_INVALID_SIZE = "[ERROR] 3자리 수만 입력 가능합니다.";
+
+    public static final String NOT_MATCH_BASEBALL_RESULT = "[ERROR] 일치하는 결과값이 없습니다.";
+    public static final String NOT_INVALID_RETRY_INPUT = "[ERROR] 재시작: 1, 종료:2 를 입력해야 합니다.";
+}
