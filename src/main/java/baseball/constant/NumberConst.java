70d2ae1f5f191ba1d918a60addf8542969bc0e03
acs0209 <annjae4676@naver.com>
2023-10-22 18:41:58 +0900

    feat: add number constant

---
+++ src/main/java/baseball/constant/NumberConst.java
@@ -0,0 +1,19 @@
+package baseball.constant;
+
+/**
+ * 숫자 관련 상수 관리 클래스
+ */
+public class NumberConst {
+
+    public static final Integer RESTART_NUM = 1;
+
+    public static final Integer EXIT_NUM = 2;
+
+    public static final Integer START_NUM = 1;
+
+    public static final Integer END_NUM = 9;
+
+    public static final Integer SUCCESS_STRIKE = 3;
+
+    public static final Integer INPUT_LENGTH = 3;
+}
