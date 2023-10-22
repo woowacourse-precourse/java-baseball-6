e9a4b2cedd047a8f23837c52e1e9e380e457bc37
pyounani <yndbsk9372@gmail.com>
2023-10-23 00:41:27 +0900

    feat: 점수 계산 기능 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,25 +1,95 @@
 package baseball;
 
+import java.util.regex.Pattern;
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class Game {
     private static final String BALL_STR = "볼";
     private static final String STRIKE_STR = "스트라이크";
     private static final String NOTHING = "낫싱";
+    private static final int CNT_NUMBER = 3;
 
     private int[] player;
     private int[] computer;
+    private boolean exit = false;
 
-    private int strike;
-    private int ball;
+    private int strike = 0;
+    private int ball = 0;
 
     public Game() {}
 
-    public Game(int[] player, int[] computer) {
-        this.player = player;
-        this.computer = computer;
+    public void startGame() {
+        Number computerNumber = new Number();
+        computerNumber.setRandomNumber();
+        computer = computerNumber.getDigits();
+        Number playerNumber = new Number();
+
+        do {
+            playerNumber.inputAnswer();
+            player = playerNumber.getDigits();
+
+            resetScore();
+            calculateScore();
+            printResult();
+
+            if(strike == 3) {
+                inputExit();
+                computerNumber.setRandomNumber();
+                computer = computerNumber.getDigits();
+            }
+        } while(!exit);
     }
 
-    public void printResult() {
+    private void calculateScore() {
+        for(int i=0;i<CNT_NUMBER;i++) {
+            int score = isStrikeOrBall(player[i], i);
+            if (score == 1) {
+                ball++;
+            } else if (score == 2) {
+                strike++;
+            }
+        }
+    }
+
+    private void resetScore() {
+        strike = 0;
+        ball = 0;
+    }
+
+    private void inputExit() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = Console.readLine();
+        if(!isRightAnswer(input)) {
+            throw new IllegalArgumentException();
+        }
+        if("2".equals(input)) {
+            exit = true;
+        }
+    }
+
+    private boolean isRightAnswer(String input) {
+        Pattern pattern = Pattern.compile("[1-2]");
+        return pattern.matcher(input).matches();
+    }
+
+    private int isStrikeOrBall(int num, int index) {
+        for (int i = 0; i < CNT_NUMBER; i++) {
+            if (computer[i] == num && i == index) {
+                return 2;
+            }
+            if (computer[i] == num) {
+                return 1;
+            }
+        }
+        return -1;
+    }
+
+    private void printResult() {
         System.out.println(getResultString());
+        if(strike == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
     }
 
     private String getResultString() {
