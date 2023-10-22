8f794f6acf6067a617f607036a1d3d357ddc7bd1
ohhamma <judyham10634@naver.com>
2023-10-22 17:01:14 +0900

    :art: refactor: startPhrase 지역 변수로 변환

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,11 +1,9 @@
 package baseball;
 
 public class Game {
-    private String startPhrase;
-
     /* Starts the game */
     public void printStartPhrase() {
-        startPhrase = "숫자 야구 게임을 시작합니다.";
+        String startPhrase = "숫자 야구 게임을 시작합니다.";
         System.out.println(startPhrase);
     }
 }
