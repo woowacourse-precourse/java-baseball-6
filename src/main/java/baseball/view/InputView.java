09d00d3b0077bbd0c280dc12fa26e4ac4e3d769d
706com <ehdgusjdbswl@gmail.com>
2023-10-22 18:58:30 +0900

    feat : 입출력 메세지 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -9,4 +9,7 @@ public class InputView {
         System.out.print("숫자를 입력해주세요 : ");
         return Console.readLine();
     }
+    public String restart(){
+        return Console.readLine();
+    }
 }
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,9 +1,28 @@
 package baseball.view;
 
 public class OutputView {
+
+    public final String gameStartMessage = "숫자 야구 게임을 시작합니다.";
+
+    public final String matchAllNumberMessage = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    public final String provideRestartMessage = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
     public final String ballMessage = "볼 ";
+
     public final String strikeMessage = "스트라이크 ";
+
     public final String nothingMessage = "낫싱";
+
+    public void printGameStartMessage(){
+        System.out.println(gameStartMessage);
+    }
+    public void printMatchAllNumberMessage(){
+        System.out.println(matchAllNumberMessage);
+    }
+    public void printProvideRestartMessage(){
+        System.out.println(provideRestartMessage);
+    }
 
     public void printResultMessage(int strike, int ball){
         if(ball != 0){
