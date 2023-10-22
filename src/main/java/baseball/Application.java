81d4a850cc80577898d042eadd7f20a2fb803017
NamJihyun99 <namjihyun9936@gmail.com>
2023-10-22 15:46:56 +0900

    feat:> 게임지속여부 입력받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    private static int askToContinue() {
+        int choice = Integer.parseInt(Console.readLine());
+        if (choice != 1 && choice != 2) {
+            throw new IllegalArgumentException("잘못된 입력값입니다.");
+        }
+        return choice;
+    }
+
     public static void main(String[] args) {
         int choice;
         do {
             playBaseBall();
-            choice = getChoice();
+            choice = askToContinue();
         } while(choice == 1);
     }
 }
