a3aed5a24cba38c3f9df65387c40a7508fcf26bb
GO-TE <alsrudxo9810@naver.com>
2023-10-22 21:27:46 +0900

    Fix: checkBall 메소드 수정

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Validator {
     private static final String STRIKE_MESSAGE = "스트라이크";
     private static final String BALL_MESSAGE = "볼";
@@ -7,6 +9,10 @@ public class Validator {
     private static final String WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final int INPUT_LENGTH = 3;
+    private static final String EXCEPTION_MESSAGE = "1 또는 2를 입력하세요.";
+    private static final String RETRY_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String RESTART_NUMBER = "1";
+    private static final String QUIT_NUMBER = "2";
 
 
     Computer computer = new Computer();
@@ -29,6 +35,8 @@ public class Validator {
         this.ballCount = 0;
         for(int i = 0; i < INPUT_LENGTH; i ++){
             this.ballCount += countSameNumber(i);
+        }
+        if(this.strikeCount > 0) {
             this.ballCount -= this.strikeCount;
         }
     }
@@ -56,5 +64,6 @@ public class Validator {
         }
         System.out.println(result);
     }
+
 
 }
