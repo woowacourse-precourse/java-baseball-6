291dc5554f7b6367fb2b00c1e973913727b0f498
Hoony0321 <eogns0321@gmail.com>
2023-10-22 22:20:15 +0900

    feat : InputView 추가

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,12 @@
+package baseball.view;
+
+public class InputView {
+
+        public static void requestInputNumbers() {
+            System.out.print("숫자를 입력해주세요 : ");
+        }
+
+        public static void requestInputRestart() {
+            System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        }
+}
