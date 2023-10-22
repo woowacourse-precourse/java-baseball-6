84d84651c8ba771dc609dc75a223665dbd65aaea
park_yeongyu <sjjs7771@naver.com>
2023-10-22 21:46:57 +0900

    FEAT : 실질적으로 게임에 필요한 코드 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,35 @@
 package baseball;
 
 public class Application {
+    static Game game = new Game();
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        game.startGame();
+        game.setGame();
+       while (true){
+           playGame();
+           game.endGame();
+           if(game.reStart() == 2){
+               System.out.println("게임 종료");
+               break;
+           }
+           else{
+               reStartGame();
+           }
+       }
+
+
+    }
+    public static void playGame(){
+
+        while(!game.isOver()){
+            game.inputNum();
+            game.check_num();
+            game.printResult();
+        }
+
+    }
+    public static void reStartGame(){
+        game.setGame();
+        game.setStrikeAndBall();
     }
 }
