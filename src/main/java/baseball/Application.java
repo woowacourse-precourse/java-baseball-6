0fc0331cecb119ca19369fef483a08c91dece31c
ji eun lee <jieunlee@jiui-MacBookAir.local>
2023-10-22 19:56:58 +0900

    Feat: 플레이어가 입력한 값에 대한 예외 처리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -34,6 +34,40 @@ public class Application {
 
             // playerInput에 플레이어가 입력한 값 저장
             String playerInput = Console.readLine();
+
+            try {
+                // 플레이어가 3자리를 입력하지 않을 경우 예외 처리
+                if (playerInput.length() != 3) {
+                    throw new IllegalArgumentException("3자리 수로 입력해 주세요.");
+                }
+
+                for (int i = 0; i < playerInput.length(); i++) {
+                    char playChar = playerInput.charAt(i);
+
+                    // 플레이어가 숫자가 아닌 값을 입력할 경우 예외 처리
+                    if (!Character.isDigit(playChar)) {
+                        throw new IllegalArgumentException("숫자로 입력해 주세요.");
+                    }
+
+                    int playerNum = Character.getNumericValue(playChar);
+
+                    // 플레이어가 입력한 수 중 같은 수가 있을 경우 예외 처리
+                    if (player.contains(playerNum)) {
+                        throw new IllegalArgumentException("서로 다른 수로 입력해 주세요.");
+                    }
+
+                    // 플레이어가 0을 입력했을 경우 예외 처리
+                    if (playerNum == 0) {
+                        throw new IllegalArgumentException("1부터 9까지의 숫자로 입력해 주세요.");
+                    }
+                    player.add(playerNum);
+                }
+            }
+            // 플레이어가 입력한 값에 대한 예외 처리
+            catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                throw e;
+            }
         }
     }
 }
