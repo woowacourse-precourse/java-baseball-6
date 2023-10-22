c871fab8222f835ff0cf8812902e42b4d61c3099
WooYoungDoo <duden5000@naver.com>
2023-10-23 00:51:19 +0900

    refactor(OutputView) : 게임 결과 후 재시작 기능 분할

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -17,6 +17,14 @@ public class OutputView {
 
     public void printGameResultMessage() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public void printRetryOrEndMessage() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    public void printRetryOrEndMessageProcess() {
+        printGameResultMessage();
+        printRetryOrEndMessage();
     }
 }
