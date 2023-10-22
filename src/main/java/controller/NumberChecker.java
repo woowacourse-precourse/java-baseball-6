308dce77654fcea70f186e2ebe5f9b74506d6b4e
Lee JaeHyuk <80966413+JH22222@users.noreply.github.com>
2023-10-22 18:59:31 +0900

    refactor: 입력 리스트를 통한 검사로 수정

--- src/main/java/controller/NumberChecker.java
+++ src/main/java/controller/NumberChecker.java
@@ -10,31 +10,31 @@ public class NumberChecker {
         this.randomNumbers = randomNumbers;
     }
 
-    public int getBall(int[] inputArr){
+    public int getBall(List<Integer> inputList){
         int ball = 0;
-        for(int i = 0; i < randomNumbers.size(); i++){
-            ball += ballCheck(inputArr, i);
+        for(int i = 0; i < inputList.size(); i++){
+            ball += ballCheck(inputList, i);
         }
         return ball;
     }
 
-    private int ballCheck(int[] inputArr, int i) {
-        if(randomNumbers.contains(inputArr[i]) && i != randomNumbers.indexOf(inputArr[i])){
+    private int ballCheck(List<Integer> inputList, int i) {
+        if(randomNumbers.contains(inputList.get(i)) && i != randomNumbers.indexOf(inputList.get(i))){
             return 1;
         }
         return 0;
     }
 
-    public int getStrike(int[] inputArr){
+    public int getStrike(List<Integer> inputList){
         int strike = 0;
         for(int i = 0; i < randomNumbers.size(); i++){
-            strike += strikeCheck(inputArr, i);
+            strike += strikeCheck(inputList, i);
         }
         return strike;
     }
 
-    private int strikeCheck(int[] inputArr, int i) {
-        if(randomNumbers.contains(inputArr[i]) && i == randomNumbers.indexOf(inputArr[i])){
+    private int strikeCheck(List<Integer> inputList, int i) {
+        if(randomNumbers.contains(inputList.get(i)) && i == randomNumbers.indexOf(inputList.get(i))){
             return 1;
         }
         return 0;
