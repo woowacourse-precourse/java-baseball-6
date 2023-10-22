e86c0bcc413f4f9e866c35efe64e002db16fd0ef
DohunHyun <dohun.hyun@gmail.com>
2023-10-22 22:42:15 +0900

    Style : 코드 정리 및 주석 삽입

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,5 @@
 package baseball;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
@@ -13,9 +8,9 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
         int endSign = 1; // 게임 지속 : 1, 게임 종료 : 2
-        boolean isCorrect = false;
+        boolean isCorrect = false; // 문제 정답 여부
 
-        // 게임 진행. 문제를 맞춰도 다시 시작 가능.
+        // 게임 진행. 문제를 맞춰도 다시 시작 가능해야함.
         while(endSign == 1) {
             // 랜덤 숫자 생성.
             isCorrect = false;
@@ -28,7 +23,7 @@ public class Application {
                 }
                 checkArray[randomNumber-1] = true;
                 randomNumberArray[i] = randomNumber;
-                System.out.println(randomNumberArray[i]);
+                // System.out.println(randomNumberArray[i]); // 정답 미리보기 for test
             }
 
             // 숫자 맞추기 게임 시작
@@ -47,6 +42,8 @@ public class Application {
                     System.out.println("세자리 숫자를 입력해 주세요.");
                     throw new IllegalArgumentException();
                 }
+
+
                 for (int i=0; i<inputArray.length; i++) {
                     try { // 입력 값이 숫자인지 체크
                         inputToIntArray[i] = Integer.parseInt(inputArray[i]);
@@ -58,9 +55,10 @@ public class Application {
                         throw new IllegalArgumentException();
                     }
 
+                    // 중복된 숫자 체크.
                     if(!checkList[inputToIntArray[i]-1]) {
                         checkList[inputToIntArray[i]-1] = true;
-                    } else { // 중복된 숫자 체크.
+                    } else {
                         throw new IllegalArgumentException();
                     }
                 }
@@ -68,7 +66,6 @@ public class Application {
                 // 입력 후 판단.
                 int strike = 0;
                 int ball = 0;
-
                 for(int i=0; i<randomNumberArray.length; i++) {
                     if(randomNumberArray[i] == inputToIntArray[i]) strike++;
                     for(int j=0; j<randomNumberArray.length; j++) {
@@ -78,9 +75,9 @@ public class Application {
 
                 if(strike == 3) { // 정답인 경우
                     // 정답이라면 while문 break.
-                    System.out.println("3스트라이크");
-                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                    System.out.println("3스트라이크"
+                        + "\n3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+                        + "\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                     int inputInt = Integer.parseInt(Console.readLine());
                     if(inputInt == 2) { // 게임 종료
                         endSign = 2;
