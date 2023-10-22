9012e30dad1cfe3205079a07b1081ba0470dc4d9
박정현 <ros28081@naver.com>
2023-10-22 21:44:34 +0900

    feat: application 실행문 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,13 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Game game = new Game();
+        do {
+            game.startGame();
+            game.playGame();
+            if (game.checkFinish()) {
+                break;
+            }
+        } while (true);
     }
 }
