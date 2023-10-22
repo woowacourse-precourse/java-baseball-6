667515948d1114ffb25d6a276632a33277c2ecf6
gemstoneyang <bennyws99@gmail.com>
2023-10-22 14:06:45 +0900

    test: Add test of getGameResult() in Game class - Game class 의 getGameResult 메소드를 테스트하기 위한 코드 추가

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -39,4 +39,23 @@ public class GameTest {
         List<Integer> answerList = game.makeRandomAnswer();
         assertEquals(answerList.size(), answerList.stream().distinct().count());
     }
+
+    @Test
+    void 사용자의_입력과_정답을_비교하여_결과를_반환한다() {
+        Game game = new Game();
+        game.answerList.clear();
+        game.answerList.addAll(List.of(4, 0, 9));
+
+        GameResult gameResult1 = game.getGameResult(List.of(1, 2, 3));
+        assertEquals(new GameResult(0, 0), gameResult1);
+
+        GameResult gameResult2 = game.getGameResult(List.of(6, 0, 8));
+        assertEquals(new GameResult(1, 0), gameResult2);
+
+        GameResult gameResult3 = game.getGameResult(List.of(0, 4, 9));
+        assertEquals(new GameResult(1, 2), gameResult3);
+
+        GameResult gameResult4 = game.getGameResult(List.of(4, 0, 9));
+        assertEquals(new GameResult(3, 0), gameResult4);
+    }
 }
