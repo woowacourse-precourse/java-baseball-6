b3adc046e0530d27feda15b0ab41034fb80b53a5
pyounani <yndbsk9372@gmail.com>
2023-10-22 23:53:23 +0900

    feat: 사용자 입력 및 검증 기능 구현

--- src/main/java/baseball/Number.java
+++ src/main/java/baseball/Number.java
@@ -1,13 +1,17 @@
 package baseball;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.regex.Pattern;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 public class Number {
     private static final int CNT_NUMBER = 3;
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 9;
+    private static final String regax = "^["+MIN_NUMBER+"-"+MAX_NUMBER+"]*$";
 
     private int[] digits;
 
@@ -27,6 +31,15 @@ public class Number {
         this.digits = convertIntegerSetToIntArray(digitSet);
     }
 
+    public void inputAnswer() {
+        String input = Console.readLine();
+        setDigits(input);
+    }
+
+    public void inputAnswer(String str) {
+        setDigits(str);
+    }
+
     private int[] convertIntegerSetToIntArray (Set<Integer> set) {
         return set.stream()
                 .mapToInt(Integer::intValue)
@@ -35,5 +48,38 @@ public class Number {
 
     private int getRandomNumber() {
         return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+    }
+
+    private void setDigits(String input) {
+        if(!isValidString(input)) {
+            throw new IllegalArgumentException();
+        }
+        this.digits = Arrays.stream(input.split(""))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
+
+    private boolean isValidString(String str) {
+        if(!isRightLength(str) || !isMinToMax(str) || !isNotDuplicate(str)) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isRightLength(String input) {
+        return input.length() == CNT_NUMBER;
+    }
+
+    private boolean isMinToMax(String input) {
+        Pattern numberPattern = Pattern.compile(regax);
+        return numberPattern.matcher(input).matches();
+    }
+
+    private boolean isNotDuplicate(String input) {
+        Set<Character> set = new HashSet<>();
+        for(char s: input.toCharArray()) {
+            set.add(s);
+        }
+        return input.length() == set.size();
     }
 }
--- src/test/java/baseball/NumberTest.java
+++ src/test/java/baseball/NumberTest.java
@@ -1,12 +1,13 @@
 package baseball;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class NumberTest {
@@ -14,31 +15,77 @@ public class NumberTest {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 9;
 
+    private static Number number;
+
+    @BeforeEach
+    private void createNumber() {
+        number = new Number();
+    }
+
     @Test
     void 컴퓨터_숫자_검증() {
-        Number computer = new Number();
-        computer.setRandomNumber();
-
-        checkRange(computer);
-        checkCnt(computer);
-        checkDuplicate(computer);
+        number.setRandomNumber();
+        assertTrue(checkVaild());
     }
 
-    private void checkRange(Number number) {
+    @Test
+    void 올바른_숫자_입력() {
+        number.inputAnswer("123");
+        assertTrue(checkVaild());
+    }
+
+    @Test
+    void 문자_입력() {
+        assertThrows( IllegalArgumentException.class,
+                () -> number.inputAnswer("abc")
+        );
+    }
+
+    @Test
+    void 올바르지_않은_입력_길이() {
+        assertThrows( IllegalArgumentException.class,
+                () -> number.inputAnswer("1234")
+        );
+    }
+
+    @Test
+    void 올바르지_않은_입력_범위() {
+        assertThrows( IllegalArgumentException.class,
+                () -> number.inputAnswer("102")
+        );
+    }
+
+    @Test
+    void 중복이_존재하는_입력() {
+        assertThrows( IllegalArgumentException.class,
+                () -> number.inputAnswer("112")
+        );
+    }
+
+    private boolean checkVaild() {
+        return checkRange()
+                && checkCnt()
+                && checkDuplicate();
+    }
+
+    private boolean checkRange() {
         for(int digit: number.getDigits()) {
-            assertFalse(digit < MIN_NUMBER || digit > MAX_NUMBER);
+            if(digit < MIN_NUMBER || digit > MAX_NUMBER) {
+                return false;
+            }
         }
+        return true;
     }
 
-    private void checkCnt(Number number) {
-        assertTrue(CNT_NUMBER == number.getDigits().length);
+    private boolean checkCnt() {
+        return CNT_NUMBER == number.getDigits().length;
     }
 
-    private void checkDuplicate(Number number) {
+    private boolean checkDuplicate() {
         int[] digits = number.getDigits();
         Set<Integer> digitSet = convertArrToSet(digits);
 
-        assertTrue(digits.length == digitSet.size());
+        return digits.length == digitSet.size();
     }
 
     private Set<Integer> convertArrToSet(int[] arr) {
