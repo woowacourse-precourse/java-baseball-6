603d91e45e4aa1a5fa2a2d5b8478c0f6097c3229
SB8919-LAPTOP\sb891 <sb8919@naver.com>
2023-10-22 20:49:29 +0900

    feat(outputView): displayExitMessage 메서드 구현

--- src/main/java/baseball/view/outputView.java
+++ src/main/java/baseball/view/outputView.java
@@ -2,9 +2,13 @@ package baseball.view;
 
 public class outputView {
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String GAME_CLEAR_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     public void displayNewGameMessage() {
         System.out.println(GAME_START_MESSAGE);
     }
 
+    public void displayExitMessage() {
+        System.out.println(GAME_CLEAR_MESSAGE);
+    }
 }
