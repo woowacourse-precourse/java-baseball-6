22358894c5ca25539e232094147f1a982df496a7
GS_song <20003204@sju.ac.kr>
2023-10-22 16:15:50 +0900

    feat: Game을 관리하는 GameManager 생성

--- src/main/java/baseball/manager/GameManager.java
+++ src/main/java/baseball/manager/GameManager.java
@@ -1,18 +1,30 @@
 package baseball.manager;
 
-import static baseball.constant.Constants.GAME_START_MESSAGE;
+import baseball.io.InputHandler;
+import baseball.model.Baseball;
+import baseball.model.Game;
+
+import static baseball.constant.Constants.*;
 
 public class GameManager {
+    private final InputHandler inputHandler;
+
+    public GameManager() {
+        inputHandler = new InputHandler();
+    }
 
     public void play() {
+        int restart;
         // 게임 시작 메시지 출력
         System.out.println(GAME_START_MESSAGE);
 
-        // Game 초기화 init? -> random 값 생성
-
-        // 게임 실행
-
-        // 게임이 종료되면 게임 재시작 여부 확인
-
+        do {
+            // Game 초기화 init? -> random 값 생성
+            Game game = new Baseball(inputHandler);
+            // 게임 실행
+            game.playGame();
+            System.out.println(INPUT_RESTART_MESSAGE);
+            restart = inputHandler.scanInteger();
+        } while (restart == INPUT_RESTART);
     }
 }
