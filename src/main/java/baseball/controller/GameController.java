16fc4bde23b86d2b61559c1784f178641abb6fcf
yunju-m <dbswnfnal@naver.com>
2023-10-22 23:23:35 +0900

    refactor(GameController): 수정 게임 시작 메시지 한 번만 출력하도록 설정

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -17,8 +17,8 @@ public class GameController {
 
     public void startGame() {
         boolean gameState = true;
+        outputView.printStartGame();
         while (gameState) {
-            outputView.printStartGame();
             computerController.startGame(gameNumber);
 
             repeatGussingAnswer();
