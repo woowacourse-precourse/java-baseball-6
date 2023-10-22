7b07be4f8b58ece616b21a372761c2ef2eb252f6
Son JunHyung <kingjh1125@gmail.com>
2023-10-22 17:03:44 +0900

    feat: BaseballController.java 생성

---
+++ src/main/java/controller/BaseballController.java
@@ -0,0 +1,20 @@
+/*
+ * <pre>
+ * Class : BaseballController
+ * Comment: Application 시작 클래스
+ * History
+ * ================================================================
+ * DATE             AUTHOR           NOTE
+ * ----------------------------------------------------------------
+ * 2023-10-22       손준형           최초 생성
+ * </pre>
+ *
+ * @author 손준형
+ * @version 1.0.0
+ * @see
+ */
+
+package controller;
+
+public class BaseballController {
+}
