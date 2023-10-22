6963801bb7d33a26aeb83f775de50376c8be98dd
PlutoWooSeok <plutoxxx1014@gmail.com>
2023-10-22 22:46:29 +0900

    game컨트롤 기능 구현

---
+++ src/main/java/baseball/Controller/GameController.java
@@ -0,0 +1,61 @@
+package baseball.Controller;
+
+import baseball.Model.PlayNumber;
+import baseball.View.InputView;
+import baseball.View.OutputView;
+
+public class GameController {
+    private static final String wrongAnswerRestartMSG = "1 또는 2만 입력이 가능합니다.";
+
+    PlayNumber playNumber = new PlayNumber();
+    CheckAnswer checkAnswer = new CheckAnswer();
+    NumberValidation numberValidation = new NumberValidation();
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+
+    public void playGame() {
+        boolean gameStata = true;
+
+        while (gameStata) {
+            outputView.printStartMsg();
+            checkAnswer.startGame(playNumber);
+            againAskAnswer();
+            gameStata = askRestartGame();
+        }
+    }
+
+    private boolean askRestartGame() {
+        String answer = inputView.enterAskRestart();
+        return wantRestartGame(answer);
+    }
+
+    private boolean wantRestartGame(String answer) {
+        if (answer.equals("1")) {
+            return true;
+        } else if (answer.equals("2")) {
+            return false;
+        }
+        throw new IllegalArgumentException(wrongAnswerRestartMSG);
+    }
+
+    private void againAskAnswer() {
+        boolean correctAns = false;
+
+        while (!correctAns) {
+            setAndSavePlayerNumbers();
+            correctAns = checkAnswer.toDo(playNumber);
+            sendEndMsgCorrect(correctAns);
+        }
+    }
+
+    private void setAndSavePlayerNumbers() {
+        playNumber.setInputNumber(inputView.enterAskNumber());
+        int[] playerNumber = numberValidation.validationNumber(playNumber.getInputNumber());
+    }
+
+    private void sendEndMsgCorrect(boolean correctAns) {
+        if (correctAns) {
+            outputView.printEndMsg();
+        }
+    }
+}
