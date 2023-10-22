e26ee61175b5f2956fee63995083176c02cae8d4
Jiyun <alex68566856@gmail.com>
2023-10-22 18:48:15 +0900

    refactor: 한파일에서 코딩 -> 여러 class로 분리

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,70 +1,36 @@
 package baseball;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
-import camp.nextstep.edu.missionutils.Randoms;
+import baseball.controller.Computer;
+import baseball.controller.User;
+import baseball.model.Numbers;
+import baseball.model.Score;
 import camp.nextstep.edu.missionutils.Console;
 
-import javax.sound.midi.Soundbank;
-
 public class BaseballGame {
-    private final List<Integer> computer = new ArrayList<Integer>();
+    private final static User user = new User();
+    public void play() {
+        while (true) {
+            String isFinish = userTry();
 
-    public void run() {
-        while (computer.size()<3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
+            System.out.println("checking game end! " + isFinish);
+            if (isFinish.equals("2")) {
+                break;
             }
+
         }
-        System.out.print("Computer: ");
-        computer.forEach(System.out::print);
-        System.out.println();
-
-        System.out.println("숫자 야구 게임을 시작합니다.");
-
-        boolean haveToStop = false;
-        while (!haveToStop) {
-            System.out.print("숫자를 입력해주세요 : ");
-            String userInput = Console.readLine();
-            List<Integer> user = Arrays.stream(userInput.split(""))
-                    .mapToInt(Integer::parseInt)
-                    .boxed()
-                    .toList();
-
-            int strike = 0;
-            int ball = 0;
-
-            for (int i = 0; i < 3; i++) {
-                if (computer.get(i) == user.get(i)) {
-                    strike++;
-                }
-            }
-
-            for (int i = 0; i < 3; i++) {
-                int compareNumber = computer.get(i);
-                if (user.contains(compareNumber)) {
-                    if (i != user.indexOf(compareNumber)) {
-                        ball++;
-                    }
-                }
-            }
-
-            System.out.println(ball + " 볼 " + strike + " 스트라이크");
-
-
-            if (strike == 3) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-
-                String a = Console.readLine();
-                if (Objects.equals(a, "2")) {
-                    haveToStop = true;
-                }
-            }
+    }
+    private String userTry() {
+        Computer computer = new Computer();
+        System.out.println("정답: " + computer);
+        while (true) {
+            List<Integer> userInput = user.userInput();
+            Score score = computer.compare(userInput);
+            if (!computer.isWin(score)) break;
         }
+
+        return Console.readLine();
     }
 }
