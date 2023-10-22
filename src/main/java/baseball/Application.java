3bfe3a434fc5a9059f26401259e9c04789c4d8c0
k-kmw <kimmw2_@naver.com>
2023-10-22 09:26:22 +0900

    Feat: computer 리스트에 랜덤한 3개의 수를 저장하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        /**
+         * 1~9로 이루어진 서로 다른 3자리 수 생성
+         * computer 리스트에 3개의 수를 저장
+         */
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            System.out.print(randomNumber);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
     }
 }
