2c63661eca434a868933f90e468b0a52a3f12aa8
Dongwoongkim <rlaehddnd0422@naver.com>
2023-10-22 15:52:10 +0900

    feat : (Computer) 점수를 계산하여 리턴하는 기능 구현

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -1,17 +1,29 @@
 package baseball.model;
 
 import baseball.util.NumberGenerator;
+import baseball.util.ScoreCalculator;
 import java.util.List;
 
 public class Computer {
+
     private List<Integer> answer;
+    private Score score;
 
     public void generateNumber() {
         NumberGenerator numberGenerator = new NumberGenerator();
         this.answer = numberGenerator.getGeneratedNumbers();
     }
 
+    public void generateScore(List<Integer> playerInput) {
+        ScoreCalculator scoreCalculator = new ScoreCalculator();
+        this.score = scoreCalculator.calculateScore(playerInput, getAnswer());
+    }
+
     public List<Integer> getAnswer() {
         return answer;
+    }
+
+    public Score getScore() {
+        return score;
     }
 }
