362fcee4fdefb98b29caa8e522a581fee83fa250
ParkDH0809 <znxn1237@gmail.com>
2023-10-22 14:25:13 +0900

    feat(컨트롤러): 결과에 따른 출력 요청 구현 및 예외처리 구분

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -9,14 +9,18 @@ public class GameController {
     InputView inputView = new InputView();
     OutputView outputView = new OutputView();
 
-    public void startProgram() {
+    public void manageProgram() {
         try {
-            outputView.outputStartProgram();
-            Game game = initGame();
-            compareNumber(game);
+            startProgram();
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    private void startProgram() {
+        outputView.outputStartProgram();
+        Game game = initGame();
+        startGame(game);
     }
 
     private Game initGame() {
@@ -24,7 +28,42 @@ public class GameController {
         return Game.numberOf(gameService.getRandomNumbers());
     }
 
+    private void startGame(Game game) {
+        inputNumber(game);
+        compareNumber(game);
+        printResult(game);
+    }
+
+    private void inputNumber(Game game) {
+        game.setInputNumber(inputView.inputNumber());
+    }
+
     private void compareNumber(Game game) {
-        game.compareNumber(inputView.inputNumber());
+        game.compareNumber();
+    }
+
+    private void printResult(Game game) {
+        if(game.isThreeStrike()) {
+            outputView.outputOnlyStrike(game.getStrikeNumber());
+            outputView.outputEndProgram();
+            return;
+        }
+
+        if(game.isOnlyStrike()) {
+            outputView.outputOnlyStrike(game.getStrikeNumber());
+            return;
+        }
+
+        if(game.isOnlyBall()) {
+            outputView.outputOnlyBall(game.getBallNumber());
+            return;
+        }
+
+        if(game.isNothing()) {
+            outputView.outputNothing();
+            return;
+        }
+
+        outputView.outputBallStrike(game.getBallNumber(), game.getStrikeNumber());
     }
 }
