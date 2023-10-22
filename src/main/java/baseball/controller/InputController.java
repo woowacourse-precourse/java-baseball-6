858960cd8683f11b2b09033e1bec10d1c2732a18
devholic22 <hyunjoon.tech@gmail.com>
2023-10-22 14:58:18 +0900

    Refactor: StringInputConverter toInt 메서드 삭제
    Integer.parseInt와 별다른 게 없기 때문에 삭제 진행

--- src/main/java/baseball/controller/InputController.java
+++ src/main/java/baseball/controller/InputController.java
@@ -4,7 +4,6 @@ import static baseball.Constants.END_WANT;
 import static baseball.Constants.PLAY_NUMBER_DIGIT;
 import static baseball.Constants.PLAY_WANT;
 
-import baseball.converter.StringInputConverter;
 import baseball.validator.NumberValidator;
 import baseball.validator.ResumeValidator;
 import camp.nextstep.edu.missionutils.Console;
@@ -15,13 +14,13 @@ public class InputController {
         String userInput = Console.readLine();
         NumberValidator.assertInputNumberWithLength(userInput, PLAY_NUMBER_DIGIT);
 
-        return StringInputConverter.toInt(userInput);
+        return Integer.parseInt(userInput);
     }
 
     public static int receiveResumeNumber() {
         String resumeInput = Console.readLine();
         ResumeValidator.assertResumeInput(resumeInput, PLAY_WANT, END_WANT);
 
-        return StringInputConverter.toInt(resumeInput);
+        return Integer.parseInt(resumeInput);
     }
 }
--- src/main/java/baseball/converter/StringInputConverter.java
+++ src/main/java/baseball/converter/StringInputConverter.java
@@ -2,10 +2,6 @@ package baseball.converter;
 
 public class StringInputConverter {
 
-    public static int toInt(final String input) {
-        return Integer.parseInt(input);
-    }
-
     public static String[] toArray(final String input) {
         return input.split("");
     }
