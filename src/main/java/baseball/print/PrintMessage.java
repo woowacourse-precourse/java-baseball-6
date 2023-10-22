0fc3e356b0538919b378965c57f8a86b0e47251a
soyoungxxx <otherplet@gmail.com>
2023-10-23 02:52:11 +0900

    feat: PrintMessage.java 작성 - printStartMessage(), printEndMessage() 작성 - 게임 시작과 끝에 출력할 문자열 선언 후 출력하는 메소드

--- src/main/java/baseball/print/PrintMessage.java
+++ src/main/java/baseball/print/PrintMessage.java
@@ -1,4 +1,17 @@
 package baseball.print;
 
 public class PrintMessage {
+    private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String NEW_OR_END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+
+    public void printStartMessage() {
+        System.out.println(START_MESSAGE);
+    }
+
+    public void printEndMessage() {
+        System.out.println(END_MESSAGE);
+        System.out.println(NEW_OR_END_MESSAGE);
+    }
 }
