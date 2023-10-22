623ade356c4ebed6dfa7216a6b7113d90e8056b5
minseo  jo <liging12@naver.com>
2023-10-22 13:38:44 +0900

    refactor: 에러메시지 상수로 변경

--- src/main/java/baseball/domain/Balls.java
+++ src/main/java/baseball/domain/Balls.java
@@ -7,6 +7,8 @@ import java.util.Set;
 import static baseball.domain.Ball.*;
 
 public record Balls (List<Ball> balls) {
+    private static final String ERROR_MESSAGE = "입력한 숫자는 1에서 9 사이의 중복되지 않는 3자리여야 합니다.";
+
     public Balls(List<Ball> balls) {
         validate(balls);
         this.balls = List.copyOf(balls);
@@ -62,6 +64,6 @@ public record Balls (List<Ball> balls) {
     }
 
     private void throwIllegalArgumentException() {
-        throw new IllegalArgumentException("입력한 숫자는 1에서 9 사이의 중복되지 않는 3자리여야 합니다.");
+        throw new IllegalArgumentException(ERROR_MESSAGE);
     }
 }
