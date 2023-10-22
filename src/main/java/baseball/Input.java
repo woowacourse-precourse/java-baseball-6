3314c5a8cdeb7fd0db4ab85cea7d48902d12ce15
baksakcci <pshsh910@gmail.com>
2023-10-22 19:03:24 +0900

    refactor($Input, Output): 입력 및 출력 객체 생성

---
+++ src/main/java/baseball/Input.java
@@ -0,0 +1,11 @@
+package baseball;
+
+import java.util.Scanner;
+
+public class Input {
+
+    public String get() {
+        Scanner in = new Scanner(System.in);
+        return in.next();
+    }
+}
