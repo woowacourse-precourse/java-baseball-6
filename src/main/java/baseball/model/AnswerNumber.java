4d9b477926ff9243cc9514e7deb88ed9bc4a2400
jaehee <y2hjjh@naver.com>
2023-10-22 20:33:34 +0900

    refactor: 정답 숫자 모델 수정

--- src/main/java/baseball/model/AnswerNumber.java
+++ src/main/java/baseball/model/AnswerNumber.java
@@ -4,31 +4,20 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.*;
 public class AnswerNumber {
-    private String answer = "";
-    public List<Integer> createRandomNumberList() {
+    private List<Integer> randomNumber = new ArrayList<>();
+
+    public void createRandomNumberList() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
+            int random = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(random)) {
+                computer.add(random);
             }
         }
-        return computer;
+        randomNumber = computer;
     }
 
-    public String changeListToNumber(List<Integer> randomNumber) {
-        StringBuilder computer = new StringBuilder();
-        for (Integer i : randomNumber) {
-            computer.append(i);
-        }
-        return computer.toString();
-    }
-
-    public String getAnswer() {
-        return answer;
-    }
-
-    public void setAnswer(String randomNumber) {
-        answer = randomNumber;
+    public List<Integer> getRandomNumber() {
+        return randomNumber;
     }
 }
