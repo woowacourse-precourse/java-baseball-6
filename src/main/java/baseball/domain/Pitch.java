0b93197aa981f1cc1be6adfe8ef698db717ae4d9
NAM GIBEOM <rlqja0523@naver.com>
2023-10-22 17:35:57 +0900

    refactor(Pitch): 숫자 검증 Pitch 클래스로 이동

--- src/main/java/baseball/domain/Pitch.java
+++ src/main/java/baseball/domain/Pitch.java
@@ -1,6 +1,35 @@
 package baseball.domain;
 
+import baseball.enums.GameConfig;
+
 import java.util.List;
 
 public record Pitch(List<Integer> numbers) {
+    public Pitch {
+        validateDuplicateCount(numbers);
+        validateRange(numbers);
+    }
+
+    private void validateDuplicateCount(List<Integer> numbers) {
+        long distinctCount = numbers
+                .stream()
+                .distinct()
+                .count();
+        if (distinctCount != GameConfig.PITCH_COUNT.getValue()) {
+            throw new IllegalArgumentException("중복된 숫자가 있습니다.");
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        int minNumber = GameConfig.MIN_NUMBER.getValue();
+        int maxNumber = GameConfig.MAX_NUMBER.getValue();
+        long withinRangeCount = numbers
+                .stream()
+                .filter(number -> number >= minNumber && number <= maxNumber)
+                .count();
+
+        if (withinRangeCount != GameConfig.PITCH_COUNT.getValue()) {
+            throw new IllegalArgumentException(String.format("%d ~ %d 사이의 숫자여야 합니다.", minNumber, maxNumber));
+        }
+    }
 }
--- src/main/java/baseball/validator/InputValidator.java
+++ src/main/java/baseball/validator/InputValidator.java
@@ -36,33 +36,6 @@ public class InputValidator {
         }
     }
 
-    private void validateNumber(String input) {
-        try {
-            Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자가 아닙니다.");
-        }
-    }
-
-    private void validateRange(int number) {
-        int minNumber = GameConfig.MIN_NUMBER.getValue();
-        int maxNumber = GameConfig.MAX_NUMBER.getValue();
-        if (number < minNumber || number > maxNumber) {
-            throw new IllegalArgumentException(String.format("입력값이 %d~%d 사이의 값이 아닙니다.", minNumber, maxNumber));
-        }
-    }
-
-    private void validateDuplicate(String input) {
-        int pitchCount = GameConfig.PITCH_COUNT.getValue();
-        for (int i = 0; i < pitchCount; i++) {
-            for (int j = i + 1; j < pitchCount; j++) {
-                if (input.charAt(i) == input.charAt(j)) {
-                    throw new IllegalArgumentException("중복된 숫자가 있습니다.");
-                }
-            }
-        }
-    }
-
     private void validateContinueNumber(String input) {
         int gameContinue = GameConfig.GAME_RESTART.getValue();
         int gameExit = GameConfig.GAME_EXIT.getValue();
