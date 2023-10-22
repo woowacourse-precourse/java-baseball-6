e8f90d38f56f87aba0e204fcc360a4f8ff390d81
cyberjam <jamsince1994@naver.com>
2023-10-22 16:53:16 +0900

    feat: add function picking random numbers

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,21 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // input computer
+        List<Integer> randomNumbers = new ArrayList<>();
+        while (randomNumbers.size() < 3) {
+            int pickNumber = pickNumberInRange(1,9);;
+            if (!randomNumbers.contains(pickNumber)) {
+                randomNumbers.add(pickNumber);
+            }
+        }
+
+        System.out.println(randomNumbers);
     }
 }
