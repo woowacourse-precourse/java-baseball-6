db7b262480bebdf51f87a28770425744881f6045
devhoya97 <diajbh123@gmail.com>
2023-10-22 12:36:39 +0900

    refactor : BaseballGame 객체에게 역할분담 한 게임을 할 때, computerBaseballNumbers와 userBaseballNumbers 쌍이 하나씩 필요하므로, 이 둘을 멤버변수로 가지면서 ballCount, strikeCount, 정답여부를 계산해주는 객체로서 BaseballGame에게 역할분담 하였습니다

---
+++ src/main/java/baseball/controller/BaseballGame.java
@@ -0,0 +1,45 @@
+package baseball.controller;
+
+import baseball.model.BaseballNumber;
+import java.util.List;
+
+public class BaseballGame {
+    private static final int BASEBALL_NUMBERS_LENGTH = 3;
+
+    private final List<BaseballNumber> computerBaseballNumbers;
+    private final List<BaseballNumber> userBaseballNumbers;
+
+    public BaseballGame(List<BaseballNumber> computerBaseballNumbers, List<BaseballNumber> userBaseballNumbers) {
+        this.computerBaseballNumbers = computerBaseballNumbers;
+        this.userBaseballNumbers = userBaseballNumbers;
+    }
+
+    public int getBallCount() {
+        int ballCount = 0;
+        for (int index = 0; index < BASEBALL_NUMBERS_LENGTH; index++) {
+            BaseballNumber userBaseballNumber = userBaseballNumbers.get(index);
+            BaseballNumber randomBaseballNumber = computerBaseballNumbers.get(index);
+            if (!userBaseballNumber.equals(randomBaseballNumber) && computerBaseballNumbers.contains(
+                    userBaseballNumber)) {
+                ballCount++;
+            }
+        }
+        return ballCount;
+    }
+
+    public int getStrikeCount() {
+        int strikeCount = 0;
+        for (int index = 0; index < BASEBALL_NUMBERS_LENGTH; index++) {
+            BaseballNumber userBaseballNumber = userBaseballNumbers.get(index);
+            BaseballNumber randomBaseballNumber = computerBaseballNumbers.get(index);
+            if (userBaseballNumber.equals(randomBaseballNumber)) {
+                strikeCount++;
+            }
+        }
+        return strikeCount;
+    }
+
+    public boolean isAnswer() {
+        return getStrikeCount() == BASEBALL_NUMBERS_LENGTH;
+    }
+}
