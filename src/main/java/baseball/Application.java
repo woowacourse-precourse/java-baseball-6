a36c513e399f39a19b85f08fde1fbe49ab319fcd
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-10-23 02:12:21 +0900

    refactor : EndGame 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,24 +2,23 @@ package baseball;
 
 import java.util.Scanner;
 
-import static baseball.EndGame.endGame;
 import static baseball.PlayGame.playGame;
 import static baseball.StartGame.startGame;
 import static baseball.UserDecision.userDecision;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        Scanner sharedScanner = new Scanner(System.in);
 
-        startGame();
-        String userDecision;
+  public static void main(String[] args) {
 
-        do {
-            playGame(sharedScanner);
-            userDecision = userDecision(sharedScanner);
-        } while (userDecision.equals("1"));
+    Scanner sharedScanner = new Scanner(System.in);
 
-        endGame();
-    }
+    startGame();
+    String userDecision;
+
+    do {
+      playGame(sharedScanner);
+      userDecision = userDecision(sharedScanner);
+    } while (userDecision.equals("1"));
+
+  }
 }
