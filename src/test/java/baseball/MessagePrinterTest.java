e339688f517f472b64334c53de1b20270eda6f83
MinseokGo <rhalstjr1999@naver.com>
2023-10-22 15:48:12 +0900

    test: 낫싱 출력 테스트

--- src/test/java/baseball/MessagePrinterTest.java
+++ src/test/java/baseball/MessagePrinterTest.java
@@ -76,4 +76,28 @@ class MessagePrinterTest {
             System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
         }
     }
+
+    @Test
+    @DisplayName("스트라이크와 볼 모두 존재하지 않을 경우 낫싱 힌트를 출력한다.")
+    void test4() {
+        int strikeCount = 0;
+        int ballCount = 0;
+        List<Integer> computerNumbers = List.of(1, 2, 3);
+        String playerNumbers = "456";
+
+        for (int i=0; i<playerNumbers.length(); i++) {
+            int playerNumber = playerNumbers.charAt(i) - 48;
+            int computerNumber = computerNumbers.get(i);
+
+            if (playerNumber == computerNumber) {
+                strikeCount += 1;
+            } else if (computerNumbers.contains(playerNumber)) {
+                ballCount += 1;
+            }
+        }
+
+        if (strikeCount == 0 && ballCount == 0) {
+            System.out.println("낫싱");
+        }
+    }
 }
