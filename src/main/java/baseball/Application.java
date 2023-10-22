94b9a71f9dadf726019be2a62ba5cace79aaab9a
eunseob <seobseob0810@naver.com>
2023-10-22 23:02:10 +0900

    fix: ì숫자 야구 게임 통합

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,23 +1,20 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
-        User user = new User();
-        Computer computer = new Computer();
+        Game game = new Game();
+        Validation validation = new Validation();
+        String restart;
 
-        while (true) {
-            System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        do {
+            game.play();
 
-            System.out.println(computer.createAnswerNumber());
-            user.userNumber();
+            restart = readLine();
+            validation.isOkRestart(restart);
 
-            /*System.out.println("낫싱");
-            System.out.print("볼");
-            System.out.print("스트라이크");
-            System.out.println("3개의 숫자를 다 맞히셨습니다! 게임 종료");
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");*/
-
-        }
+        } while (Integer.parseInt(restart) != 2);
     }
 }
