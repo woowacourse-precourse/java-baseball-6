25160269f71f36260ce4bf289b9868f3a5497e29
soyoungxxx <otherplet@gmail.com>
2023-10-23 05:51:38 +0900

    feat(Validator): 중복 입력 예외 처리

--- src/main/java/baseball/variable/Validator.java
+++ src/main/java/baseball/variable/Validator.java
@@ -1,10 +1,13 @@
 package baseball.variable;
 
 import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Validator {
     private static final String NOT_THREE = "세 자리 숫자가 아닙니다.";
     private static final String INCLUE_ZERO = "1 ~ 9 사이의 값을 입력해주세요. 0은 입력할 수 없습니다.";
+    private static final String DUPLICATE = "중복된 숫자는 입력할 수 없습니다.";
 
     public void isValid(List number) {
         if (!isThreeNumbers(number)) {
@@ -12,6 +15,9 @@ public class Validator {
         }
         if (isZeroInclue(number)) {
             throw new IllegalArgumentException(INCLUE_ZERO);
+        }
+        if (isDuplicate(number)) {
+            throw new IllegalArgumentException(DUPLICATE);
         }
     }
 
@@ -21,6 +27,12 @@ public class Validator {
 
     private boolean isZeroInclue(List number) {
         return number.contains(0);
+    }
+
+    private boolean isDuplicate(List number) {
+        Set<Integer> numSet = new HashSet<>(number);
+
+        return numSet.size() != number.size();
     }
 
     // 숫자가 세 자리가 아닌 경우
