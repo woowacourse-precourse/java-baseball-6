366af27a85ffd9befc5155f6d7096ff792e4b370
Jihyun3478 <leejihyun3478@gmail.com>
2023-10-23 05:02:52 +0900

    Feat(NumberValidation): 플레이어 입력 값 검증 기능 구현

---
+++ src/main/java/baseball/validation/NumberValidation.java
@@ -0,0 +1,22 @@
+package baseball.validation;
+
+import java.util.Arrays;
+
+public class NumberValidation {
+    public void validationAll(int[] input) {
+        validateDuplicate(input);
+        validateRangeAndType(input);
+    }
+
+    public static void validateDuplicate(int[] input) {
+        if (!(input.length == Arrays.stream(input).distinct().count())) {
+            throw new IllegalArgumentException("숫자의 형식이 올바르지 않습니다.");
+        }
+    }
+
+    private void validateRangeAndType(int[] input) {
+        if (!(input.length == Arrays.stream(input).filter(i -> i >= 1 && i <= 9).count())) {
+            throw new IllegalArgumentException("숫자의 형식이 올바르지 않습니다.");
+        }
+    }
+}
