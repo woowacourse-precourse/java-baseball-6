b2e992a567a9ebea99b37b7d208a695c7fecb2c9
Arachneee <jhg2819@naver.com>
2023-10-22 11:49:16 +0900

    refactor: converter 역할 controller로 전이

--- src/main/java/baseball/controller/PlayerController.java
+++ src/main/java/baseball/controller/PlayerController.java
@@ -1,11 +1,11 @@
 package baseball.controller;
 
-import static baseball.converter.BaseBallConverter.stringToNumbers;
-
 import baseball.domain.number.Numbers;
 import baseball.domain.player.Player;
 import baseball.domain.result.Result;
 import baseball.view.BaseBallView;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class PlayerController {
     private static final BaseBallView baseBallView = BaseBallView.create();
@@ -17,13 +17,33 @@ public class PlayerController {
         boolean isContinueBattle = Boolean.TRUE;
 
         while (isContinueBattle) {
-            Numbers numbers = stringToNumbers(baseBallView.numbersInputView());
-            Player player = Player.create(numbers);
+            Player player = changePlayerNumbers(baseBallView.numbersInputView());
 
             Result result = RefereeController.umpiredPlayers(player, opponent);
 
             isContinueBattle = result.isContinue();
         }
     }
+
+    private static Player changePlayerNumbers(String numberString) {
+        return Player.create(convertStringToNumbers(numberString));
+    }
+
+    private static Numbers convertStringToNumbers(String numberString) {
+        validateIsInteger(numberString);
+        
+        return Numbers.create(Arrays.stream(numberString.split(""))
+                .map(Integer::valueOf)
+                .collect(Collectors.toList()));
+    }
+
+    private static void validateIsInteger(String input) {
+        try {
+            Integer.valueOf(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력가능합니다.");
+        }
+    }
+    
 
 }
