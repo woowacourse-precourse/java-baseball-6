827f5b19e2b273c878c49a6e5d0f9aae492c8ffc
혜민 <alddlv81@gmail.com>
2023-10-22 18:12:48 +0900

    refactor: Game 클래스, Hint 클래스 레벨이 달라 Game 안에 Hint 넣음

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -15,6 +15,8 @@ public class Application {
 }
 
 class Game{
+    int ball, strike;
+
     void start(){
         System.out.println("숫자 야구 게임을 시작합니다.");
         boolean check = false;
@@ -25,10 +27,9 @@ class Game{
             while (true) {
                 List<Integer> userNumber = getUserNumber();
 
-                Hint hint = new Hint();
-                hint.calculateScore(userNumber, computerNumber);
+                calculateScore(userNumber, computerNumber);
 
-                if (hint.result()) {
+                if (result()) {
                     System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                     int choice = Integer.parseInt(Console.readLine());
                     if (choice == 1) {
@@ -81,10 +82,6 @@ class Game{
             userNumber.add(num);
         }
     }
-}
-
-class Hint{
-    int ball, strike;
 
     void calculateScore(List<Integer>user, List<Integer>computer){
         ball = 0;
