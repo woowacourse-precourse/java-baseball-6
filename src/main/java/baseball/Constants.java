68de50143cfb0da3883e3e4c1457277135a55a9f
wochae <officialwochae@gmail.com>
2023-10-22 21:17:10 +0900

    [TEST] : player input validation

---
+++ src/main/java/baseball/Constants.java
@@ -0,0 +1,6 @@
+package baseball;
+
+public class Constants {
+  public static final int[] IN_GAME = {3, 9};
+  public static final int[] RE_GAME = {1, 2};
+}
--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,19 +1,41 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Player {
+
+  private String input;
 
   public Player() {
   }
 
-  public boolean validateInput(String input) {
-    if (input.length() != 3) {
+  public void setInput(String input) {
+    this.input = input;
+  }
+
+  public boolean userInputNumbers(final int[] flag) {
+    input = Console.readLine();
+    setInput(input);
+    return validateNumericInput(input, flag);
+  }
+
+  public boolean validateNumericInput(String input, final int[] flag) {
+    if (input.length() != flag[0]) {
       return false;
     }
     for (Character ch : input.toCharArray()) {
-      if (ch  < '0' || ch > '9') {
+      if (ch < '0' || ch > flag[1] - '0') {
         return false;
       }
     }
     return true;
+  }
+
+  public boolean checkSwingResult(int correctNumber, int index) {
+    return correctNumber == input.charAt(index) - '0';
+  }
+
+  public boolean findBallAfterSwing(String numbers, int index) {
+    return numbers.contains(String.valueOf(input.charAt(index)));
   }
 }
