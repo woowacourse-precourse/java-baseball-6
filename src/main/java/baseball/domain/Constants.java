72ba0fc8a713e8290844cb21eec4a0287e450e65
qudwns017 <qudwns017@naver.com>
2023-10-22 17:08:16 +0900

    Feat: 상수 분리 선언

---
+++ src/main/java/baseball/domain/Constants.java
@@ -0,0 +1,8 @@
+package baseball.domain;
+
+public class Constants {
+    public static final Integer NUMBERS_SIZE = 3;
+    public static final Integer STRIKE_SIZE = 3;
+    public static final Integer NUMBER_MIN_RANGE = 1;
+    public static final Integer NUMBER_MAX_RANGE = 9;
+}
