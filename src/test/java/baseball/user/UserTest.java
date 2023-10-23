6c4d5b52d77c9941204f3c27f15cf3248eb5a539
jangseop <kgh010529@naver.com>
2023-10-23 17:20:11 +0900

    feat: UserTest 생성
    UserTest생성 및 테스트 요구사항 주석 입력

---
+++ src/test/java/baseball/user/UserTest.java
@@ -0,0 +1,13 @@
+package baseball.user;
+
+class UserTest {
+
+    // TODO GameDispatch.dispatch의 출력값, GameResponse.result = SUCCESS 이면 msg를 출력한다.
+
+    // TODO GameDispatch.dispatch의 출력값, GameResponse.result = FAIL 이면 에러를 쓰로우한다.
+
+    // TODO GameDispatch.dispatch의 출력값, GameResponse.nextCmd = END 면 프로그램을 종료한다.
+
+    // TODO GameDispatch.dispatch의 출력값, GameResponse.nextCmd = "command" 면 User.doGame(command, nargs)를 호출한다.
+
+}
