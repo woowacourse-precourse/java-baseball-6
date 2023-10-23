28a2b0c28dde71379993ee8eddc7a6cc996fb275
eora21 <eora21@naver.com>
2023-10-23 17:01:58 +0900

    refactor: 💡 수정 View 인터페이스 적용

--- src/main/java/baseball/view/ConsoleInputView.java
+++ src/main/java/baseball/view/ConsoleInputView.java
@@ -2,24 +2,43 @@ package baseball.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
-import baseball.util.ConsoleStopGameDecisionMapper;
+import java.util.List;
 
-public class ConsoleInputView {
+public class ConsoleInputView implements InputView {
+    private static final String MORE_TEXT = "1";
+    private static final String STOP_TEXT = "2";
 
-    public String request() {
-        return readLine();
+    @Override
+    public List<Integer> getPlayerNumber() {
+        return readLine()
+                .chars()
+                .map(Character::getNumericValue)
+                .boxed()
+                .toList();
     }
 
-    public String moreGameText() {
-        return ConsoleStopGameDecisionMapper.MORE_TEXT;
+    @Override
+    public String moreGameValue() {
+        return MORE_TEXT;
     }
 
-    public String stopGameText() {
-        return ConsoleStopGameDecisionMapper.STOP_TEXT;
+    @Override
+    public String stopGameValue() {
+        return STOP_TEXT;
     }
 
+    @Override
     public boolean isWantStopGame() {
         String input = readLine();
-        return ConsoleStopGameDecisionMapper.getDecision(input);
+
+        if (MORE_TEXT.equals(input)) {
+            return false;
+        }
+
+        if (STOP_TEXT.equals(input)) {
+            return true;
+        }
+
+        throw new IllegalArgumentException();
     }
 }
--- src/main/java/baseball/view/ConsoleOutputView.java
+++ src/main/java/baseball/view/ConsoleOutputView.java
@@ -3,20 +3,23 @@ package baseball.view;
 import baseball.model.GameResult;
 import java.util.StringJoiner;
 
-public class ConsoleOutputView {
+public class ConsoleOutputView implements OutputView {
     private static final String GAME_START = "숫자 야구 게임을 시작합니다.";
     private static final String REQUEST_NUMBER = "숫자를 입력해주세요 : ";
     public static final String MORE_GAME = "게임을 새로 시작하려면 %s, 종료하려면 %s를 입력하세요.";
     private static final String GOOD_GAME = "개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
+    @Override
     public void gameStart() {
         System.out.println(GAME_START);
     }
 
+    @Override
     public void requestNumber() {
         System.out.print(REQUEST_NUMBER);
     }
 
+    @Override
     public void showGameResult(GameResult gameResult) {
         String gameResultMessage = createGameResultMessage(gameResult);
         System.out.println(gameResultMessage);
@@ -43,11 +46,13 @@ public class ConsoleOutputView {
         return stringJoiner.toString();
     }
 
-    public void moreGame(final String moreNumber, final String endNumber) {
-        System.out.printf(MORE_GAME, moreNumber, endNumber);
-    }
-
+    @Override
     public void goodGame(final int targetSize) {
         System.out.println(targetSize + GOOD_GAME);
+    }
+
+    @Override
+    public void areYouWantMoreGame(final String moreValue, final String stopValue) {
+        System.out.printf(MORE_GAME, moreValue, stopValue);
     }
 }
