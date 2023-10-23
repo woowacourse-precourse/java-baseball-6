a67be86b0da85bb9fb59279f3e71a3899d9931d1
hyebin <xxico89@gmail.com>
2023-10-23 16:43:18 +0900

    [Refactor] 코드 모듈화 및 불필요한 코드 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,19 +7,17 @@ import java.util.*;
 
 
 public class Application {
-    public static int baseballGame(List<Integer> computerNum) {
 
-        System.out.print("숫자를 입력해주세요 : ");
-        String userNum = Console.readLine();
-
-        if (userNum.length() != 3 || userNum.chars().distinct().count() != 3) {
+    private static void checkUserNum(String user) {
+        if (user.length() != 3 || user.chars().distinct().count() != 3) {
             throw new IllegalArgumentException();
         }
+    }
 
-
+    private static List<Integer> calculateStrikeBall(String userNum, List<Integer> computerNum){
+        List<Integer> result = new ArrayList<>();
         int strike = 0;
         int ball = 0;
-
 
         for (int i=0; i<3; i++) {
             int userDigit = Character.getNumericValue(userNum.charAt(i));
@@ -32,8 +30,15 @@ public class Application {
             }
         }
 
+        result.add(strike);
+        result.add(ball);
 
+        return result;
+    }
+
+    private static String makeAnswer(int strike, int ball) {
         String answer = "";
+
         if (strike == 0 && ball == 0) {
             answer = "낫싱";
         }
@@ -42,32 +47,59 @@ public class Application {
             if (strike > 0) answer += String.format(" %d스트라이크", strike);
         }
 
+        return answer;
+    }
+    private static int baseballGame(List<Integer> computerNum) {
+
+        System.out.print("숫자를 입력해주세요 : ");
+        String userNum = Console.readLine();
+
+        checkUserNum(userNum);
+
+        List<Integer> result = calculateStrikeBall(userNum, computerNum);
+        int strike = result.get(0);
+        int ball = result.get(1);
+
+        String answer = makeAnswer(strike, ball);
         System.out.println(answer);
+
         if (strike != 3) return baseballGame(computerNum);
-        else return strike;
+        else {
+            return isReplay();
+        }
+    }
+
+    private static List<Integer> makeComputerNum() {
+        List<Integer> computerNum = new ArrayList<>();
+
+        while (computerNum.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNum.contains(randomNumber)) {
+                computerNum.add(randomNumber);
+            }
+        }
+        return computerNum;
+    }
+
+    private static void gameStart(int replay) {
+        List<Integer> computer;
+
+        while (replay == 1) {
+            computer = makeComputerNum();
+            replay = baseballGame(computer);
+        }
+    }
+
+    private static int isReplay() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        return Integer.parseInt(Console.readLine());                            // = replay
     }
     
     public static void main(String[] args) {
-        int regame = 1;
-
+        int replay = 1;
         System.out.println("숫자 야구 게임을 시작합니다.");
-        List<Integer> computer = new ArrayList<>();
-
-        while (regame == 1) {
-            computer.clear();
-            while (computer.size() < 3) {
-                int randomNumber = Randoms.pickNumberInRange(1, 9);
-                if (!computer.contains(randomNumber)) {
-                    computer.add(randomNumber);
-                }
-            }
-            int strike = baseballGame(computer);
-            if (strike == 3) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                regame = Integer.parseInt(Console.readLine());
-            }
-        }
-            
+        gameStart(replay);
     }
 }
 
