08e3de4eaf740500bf3f1dcf8377a0a78b177e3c
문고은 <>
2023-10-24 03:48:17 +0900

    feat: BaseballGame 클래스 생성
    인스턴스 변수, 클래스 변수 추가
    1에서 9 사이 서로 다른 3자리 랜덤 숫자 생성하는 메서드 추가

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,35 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseballGame {
+    private int strikeCount;
+    private int ballCount;
+    private List<Integer> computerNum;
+
+    private static final int MIN_RANGE = 1;
+    private static final int MAX_RANGE = 9;
+    private static final int NUMBER_LENGTH = 3;
+
+    //1에서 9 사이 서로 다른 3자리 랜덤 숫자 생성
+    public List<Integer> computer(){
+        List<Integer> computer = new ArrayList<>();
+        int randomNum = randomNum();
+        computer.add(randomNum);
+        while(computer.size() < NUMBER_LENGTH){
+            randomNum = randomNum();
+            if(!computer.contains(randomNum)) computer.add(randomNum);
+        }
+        this.computerNum = computer;
+        //컴퓨터 랜덤값 출력 테스트
+        System.out.println("BaseballGame computerNum : " + computerNum);
+        return computerNum;
+    }
+
+    public int randomNum(){
+        return Randoms.pickNumberInRange(MIN_RANGE, MAX_RANGE);
+    }
+}
