ca81c1478bd2f24eb37e7b02b80220a6f62d03be
Jinwoo Lee <binaryWooo@gmail.com>
2023-10-23 23:17:49 +0900

    fix: player 입력 시 list 초기화 로직 추가

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -4,15 +4,32 @@ import static camp.nextstep.edu.missionutils.Console.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class Player {
 	private List<Integer> numberList = new ArrayList<>();
 
 	public void init() {
+		numberList.clear();
+		
 		System.out.print("숫자를 입력해주세요 : ");
-		String[] input = readLine().split("");
-		for (String s : input) {
-			numberList.add(Integer.parseInt(s));
+
+		try {
+			String[] input = readLine().split("");
+
+			if (input.length != 3) {
+				throw new IllegalArgumentException("3개의 숫자를 입력해야 합니다.");
+			}
+
+			try {
+				for (String s : input) {
+					numberList.add(Integer.parseInt(s));
+				}
+			} catch (IllegalArgumentException e) {
+				throw new IllegalArgumentException("숫자만 입력 가능합니다.");
+			}
+		} catch (NoSuchElementException e) {
+			throw new IllegalArgumentException("값을 입력해야 합니다.");
 		}
 	}
 
