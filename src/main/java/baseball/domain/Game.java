a2d74901b8574b4b3f8a20b4cad3492525964cd9
sunghyun <shlim0287@naver.com>
2023-10-23 14:08:17 +0900

    refactor : update result_arr and add ball,strike count

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -3,7 +3,12 @@ package baseball.domain;
 public class Game {
     int strike;
     int ball;
-    int result_arr;
+    int[] result_arr;
+
+    public void intiField() {
+        strike = 0;
+        ball = 0;
+    }
 
     public int getStrike() {
         return strike;
@@ -21,11 +26,19 @@ public class Game {
         this.ball = ball;
     }
 
-    public int getResult_arr() {
+    public int[] getResult_arr() {
         return result_arr;
     }
 
-    public void setResult_arr(int result_arr) {
+    public void setResult_arr(int[] result_arr) {
         this.result_arr = result_arr;
+    }
+
+    public void strikeCount() {
+        strike++;
+    }
+
+    public void ballCount() {
+        ball++;
     }
 }
