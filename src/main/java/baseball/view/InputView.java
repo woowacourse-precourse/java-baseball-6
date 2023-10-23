1c7fbb5e820386a5f53be1d4e4419f2e7b45decf
jiwon1217 <koagjw@gmail.com>
2023-10-24 02:20:55 +0900

    :sparkles: feat: 재시작 또는 종료 여부를 묻는 문구를 출력하고 숫자를 입력한다.

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -12,4 +12,11 @@ public class InputView {
         Validator.validateInputNumbers(numbers);
         return StringUtil.convertStringToIntList(numbers);
     }
+
+    public static Integer inputRestartOrExitNumber() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String number = Console.readLine();
+        Validator.validateRestartOrExitNumber(number);
+        return StringUtil.convertStringToInt(number);
+    }
 }
