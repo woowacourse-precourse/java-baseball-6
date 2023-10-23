5df747e4d735853d81a5685b84005889acf59ff6
leedohyun <lucky77795@naver.com>
2023-10-23 19:32:11 +0900

    refactor: controller에서 service의 로직을 이용해 게임을 진행하도록 변경

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,42 +1,34 @@
 package baseball.controller;
 
-import baseball.domain.ComputerNum;
-import baseball.domain.EnterNum;
-import baseball.validator.ResultValidator;
+import baseball.domain.UserNum;
+import baseball.service.GameService;
 import camp.nextstep.edu.missionutils.Console;
 
 public class GameController {
-
-    private final EnterNum enterNum;
-    private ResultValidator resultValidator;
-
+    private GameService gameService;
     public GameController() {
-        this.enterNum = new EnterNum();
-        this.resultValidator = new ResultValidator(new ComputerNum());
+        this.gameService = new GameService();
     }
 
     public void gameStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
-
         while(true) {
-            System.out.print("숫자를 입력해주세요 : ");
-            enterNum.setNum(Console.readLine());
-            enterNum.validation();
+            gameService.inputUserNumber();
+            gameService.showBallCount();
 
-            resultValidator.enterCheckNum(enterNum);
+            if (gameService.checkGameOver()) {
+                boolean restart = gameService.askRestartGame();
 
-            if (resultValidator.checkResult()) {
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                int checkOver = Integer.parseInt(Console.readLine());
-
-                if (checkOver == 2) {
+                if (restart) {
+                    restartGame();
+                } else if (!restart) {
                     break;
-                } else if (checkOver == 1) {
-                    resultValidator = new ResultValidator(new ComputerNum());
-                } else {
-                    throw new IllegalArgumentException();
                 }
             }
         }
+    }
+
+    private void restartGame() {
+        gameService = new GameService();
     }
 }
