78e57961d8e87c46ae459ec04559a72f5c5127f7
space <enaenen@naver.com>
2023-10-23 15:44:08 +0900

    FEAT: 결과 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -72,6 +72,23 @@ public class Application {
 				}
 			}
 
+			// Output Process
+			if (strike == 0 && ball == 0) {
+				System.out.println("낫싱");
+			} else if (strike == 0 && ball != 0) {
+				System.out.println(ball + "볼");
+			} else if (strike != 0 && ball == 0) {
+				System.out.println(strike + "스트라이크");
+			} else {
+				System.out.println(ball + "볼 " + strike + "스트라이크");
+			}
+
+
+			// isCorrect
+			if (strike == 3){
+				isCorrect = true;
+				System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+			}
 		}
 	}
 }
