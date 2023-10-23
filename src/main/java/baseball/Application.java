20a6de25bf4f782e15ec04de8966d33a28831467
chooh <chocomilk66@naver.com>
2023-10-23 18:49:57 +0900

    fix: 문구가 반복 출력되는 오류 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,8 +8,6 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
-        System.out.print("숫자를 입력해주세요 : ");
-        String input = Console.readLine();
         while (true) {
             ArrayList<Integer> numbers = new ArrayList<>();
             while (numbers.size() < 3) {
@@ -19,14 +17,16 @@ public class Application {
                 }
             }
             while (true) {
-               if (input.length() !=3) {
-                   throw new IllegalArgumentException("세 개의 숫자를 입력해주세요.");
-               }
-               try {
-                   Integer.parseInt(input);
-               } catch (Exception e) {
-                   throw new IllegalArgumentException("숫자가 아닌 값은 입력할 수 없습니다.");
-               }
+                System.out.print("숫자를 입력해주세요 : ");
+                String input = Console.readLine();
+                if (input.length() != 3) {
+                    throw new IllegalArgumentException("세 개의 숫자를 입력해주세요.");
+                }
+                try {
+                    Integer.parseInt(input);
+                } catch (Exception e) {
+                    throw new IllegalArgumentException("숫자가 아닌 값은 입력할 수 없습니다.");
+                }
             }
         }
     }
