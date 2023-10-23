aaa56a21cc7755378573f86e02f5aa56c45b1fc4
YunSeok-Choi <dbstjr9898@naver.com>
2023-10-23 22:25:40 +0900

    FEAT : 재시작 여부 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,49 +10,67 @@ public class Application {
 
         String computerNumber = shuffleNumber();
 
-        System.out.print("숫자를 입력해 주세요 : ");
-        String user = Console.readLine();
+        while (true) {
+            System.out.print("숫자를 입력해 주세요 : ");
 
-        if (user.length() != 3) {
-            throw new IllegalArgumentException("3개의 값을 입력해주세요");
-        }
-        if (!user.chars().allMatch(Character::isDigit)) {
-            throw new IllegalArgumentException("숫자만 입력해주세요");
-        }
-        String tmp = "";
-        for (char c : user.toCharArray()) {
-            if (tmp.equals(String.valueOf(c))) {
-                throw new IllegalArgumentException("중복되지 않는 숫자로 입력해주세요");
+            String user = Console.readLine();
+            if (user.length() != 3) {
+                throw new IllegalArgumentException("3개의 값을 입력해주세요");
             }
-            tmp += String.valueOf(c);
-        }
-
-        int ball = 0;
-        int strike = 0;
-        for (int i = 0; i < computerNumber.length(); i++) {
-            int computerDigit = computerNumber.charAt(i);
-            int userDigit = user.charAt(i);
-
-            if (computerDigit - 1 == userDigit || computerDigit + 1 == userDigit) {
-                ball += 1;
+            if (!user.chars().allMatch(Character::isDigit)) {
+                throw new IllegalArgumentException("숫자만 입력해주세요");
+            }
+            String tmp = "";
+            for (char c : user.toCharArray()) {
+                if (tmp.equals(String.valueOf(c))) {
+                    throw new IllegalArgumentException("중복되지 않는 숫자로 입력해주세요");
+                }
+                tmp += String.valueOf(c);
             }
 
-            if (computerDigit == user.charAt(i)) {
-                strike += 1;
+            int ball = 0;
+            int strike = 0;
+            for (int i = 0; i < computerNumber.length(); i++) {
+                int computerDigit = computerNumber.charAt(i);
+                int userDigit = user.charAt(i);
+
+                if (computerDigit - 1 == userDigit || computerDigit + 1 == userDigit) {
+                    ball += 1;
+                }
+
+                if (computerDigit == user.charAt(i)) {
+                    strike += 1;
+                }
+            }
+
+            if (ball != 0) {
+                System.out.print(ball + "볼 ");
+            }
+            if (strike != 0) {
+                System.out.println(strike + "스트라이크");
+            }
+            if (strike == 0 && ball == 0) {
+                System.out.println("낫싱");
+            }
+
+            if (strike == 3) {
+                int reStartValue;
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+                reStartValue = Integer.parseInt(Console.readLine());
+                if (reStartValue != 1 && reStartValue != 2) {
+                    throw new IllegalArgumentException("1 혹은 2의 값만 입력해주세요");
+                }
+
+                if (reStartValue == 2) {
+                    return;
+                }
+
+                computerNumber = shuffleNumber();
+                tmp = "";
+                continue;
             }
         }
-
-        if (ball != 0) {
-            System.out.print(ball + "볼 ");
-        }
-        if (strike != 0) {
-            System.out.println(strike + "스트라이크");
-        }
-        if (strike == 0 && ball == 0) {
-            System.out.println("낫싱");
-        }
-
-
     }
 
     public static String shuffleNumber() {
