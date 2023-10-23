3c67a343cfbd3294b37056999772184e4a5aabf5
baksakcci <pshsh910@gmail.com>
2023-10-23 23:50:58 +0900

    feat: 게임 반복하기 구현
    - 3스트라이크 나올 때 까지 게임 반복

--- src/main/java/baseball/controller/NBGameController.java
+++ src/main/java/baseball/controller/NBGameController.java
@@ -20,23 +20,35 @@ public class NBGameController {
         Computer computer = Computer.generateRandomNumber();
         out.displayList(computer.getComputerNumber());
 
-        // 사용자로부터 console 입력 받기
-        String user = in.inputStringNumber();
+        int strike = 0;
+        // 게임 반복하기
+        while (is3Strike(strike)) {
+            // 사용자로부터 console 입력 받기
+            String user = in.inputStringNumber();
 
-        // 숫자 검증하기
-        NumberValidation numberValidation = new NumberValidation();
-        numberValidation.validateNumber(user);
+            // 숫자 검증하기
+            NumberValidation numberValidation = new NumberValidation();
+            numberValidation.validateNumber(user);
 
-        // 숫자 타입 변경하기
-        NumberTypeConvertor numberTypeConvertor = new NumberTypeConvertor();
-        List<Integer> userIntList = numberTypeConvertor.toListInteger(user);
+            // 숫자 타입 변경하기
+            NumberTypeConvertor numberTypeConvertor = new NumberTypeConvertor();
+            List<Integer> userIntList = numberTypeConvertor.toListInteger(user);
 
-        // 숫자 야구 시작하기
-        BaseballCount baseballCount = new BaseballCount();
-        List<Integer> comIntList = computer.getComputerNumber();
-        Map<String, Integer> pitched = baseballCount.pitch(userIntList, comIntList);
+            // 숫자 야구 시작하기
+            BaseballCount baseballCount = new BaseballCount();
+            List<Integer> comIntList = computer.getComputerNumber();
+            Map<String, Integer> pitched = baseballCount.pitch(userIntList, comIntList);
 
-        // 결과 출력하기
-        out.displayResult(pitched.get("strike"), pitched.get("ball"));
+            // 결과 출력하기
+            out.displayResult(pitched.get("strike"), pitched.get("ball"));
+
+            // update strike
+            strike = pitched.get("strike");
+        }
+        out.displayEndMessage();
+    }
+
+    private boolean is3Strike(int strike) {
+        return strike != 3;
     }
 }
