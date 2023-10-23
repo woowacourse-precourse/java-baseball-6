4794e1c0bc4601c0a49d0ff06eb263e115ab0cca
hunko <rhwlgns4386@naver.com>
2023-10-24 02:10:21 +0900

    test(BaseBallGameControllerTest) : add test
    Add test for BaseBallGameControllerTest
    - if GameUseCase return not zero ball value then return ball string
    - if GameUseCase return zero strike and zero ball value then return nothing string

--- src/test/java/baseball/controller/BaseBallGameControllerTest.java
+++ src/test/java/baseball/controller/BaseBallGameControllerTest.java
@@ -37,6 +37,42 @@ public class BaseBallGameControllerTest {
         assertThat(result).isEqualTo("3스트라이크");
     }
 
+    @Test
+    @DisplayName("Result에 3볼이 있을 시 3볼 스트링을 반환한다")
+    public void should_returnThreeBallString_when_gameRuleReturnThreeBallResult() {
+        BaseBallGameController controller = new BaseBallGameController(
+                new TestBaseBallGameUseCase(new BaseBallResult(0, 3)));
+        String result = controller.play("123");
+        assertThat(result).isEqualTo("3볼");
+    }
+
+    @Test
+    @DisplayName("Result에 3볼이 있을 시 3볼 스트링을 반환한다")
+    public void should_returnTwoBallString_when_gameRuleReturnTwoBallResult() {
+        BaseBallGameController controller = new BaseBallGameController(
+                new TestBaseBallGameUseCase(new BaseBallResult(0, 2)));
+        String result = controller.play("123");
+        assertThat(result).isEqualTo("2볼");
+    }
+
+    @Test
+    @DisplayName("Result에 1볼이 있을 시 1볼 스트링을 반환한다")
+    public void should_returnOneBallString_when_gameRuleReturnOneBallResult() {
+        BaseBallGameController controller = new BaseBallGameController(
+                new TestBaseBallGameUseCase(new BaseBallResult(0, 1)));
+        String result = controller.play("123");
+        assertThat(result).isEqualTo("1볼");
+    }
+
+    @Test
+    @DisplayName("Result에 볼과 스트라이가 없을 시 낫싱 을 반환한다")
+    public void should_returnNothingString_when_gameRuleReturnZeroStrikeAndZeroBallResult() {
+        BaseBallGameController controller = new BaseBallGameController(
+                new TestBaseBallGameUseCase(new BaseBallResult(0, 0)));
+        String result = controller.play("123");
+        assertThat(result).isEqualTo("낫싱");
+    }
+
     static class TestBaseBallGameUseCase implements GameUseCase {
 
         private final BaseBallResult baseBallResult;
