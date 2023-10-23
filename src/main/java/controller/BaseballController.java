fbf93fd4351fc9739de57db02edfc79712faf3fe
jiu6525 <jiu6525@daum.net>
2023-10-23 16:30:57 +0900

    add InputView

--- src/main/java/controller/BaseballController.java
+++ src/main/java/controller/BaseballController.java
@@ -1,8 +1,13 @@
 package controller;
 
+import static view.InputView.getUserNumbersInput;
+
+import vo.UserNumbers;
+
 public class BaseballController {
 
     public static void gameStart() {
-
+        UserNumbers userNumbersInput = getUserNumbersInput();
+        System.out.println("userNumbersInput = " + userNumbersInput);
     }
 }
