956e1661f8aee9e111b501414eca8ba69e4ddb7e
IMWoo94 <leemeawoo94@gmail.com>
2023-10-23 20:37:12 +0900

    refactor : 게임 환경 설정 저장소 설정 정보 getter,setter 생성
    게임 진행 상태 변경 메소드 제공 및 일부 게임 설정 옵션 시 예외 처리

--- src/main/java/baseball/repository/GameSetRespository.java
+++ src/main/java/baseball/repository/GameSetRespository.java
@@ -4,23 +4,44 @@ import java.util.HashMap;
 
 public class GameSetRespository {
 
+    // 게임 관련 환경 설정
     private static final HashMap<String, Integer> gameSetStore = new HashMap<>();
 
+    // 게임 상태
+    private static final HashMap<String, Boolean> gameStatus = new HashMap<>();
+
+    // 해당 리포지토리는 최초 생성 될 때만 초기화
     static {
         // 게임 설정 초기화
-        System.out.println("게임 설정 초기화 시작");
-        gameSetStore.put("capacity", 3);
-        gameSetStore.put("startRange", 1);
-        gameSetStore.put("endRange", 9);
-        System.out.println("게임 설정 초기화 완료");
+        System.out.println("=====게임 설정 초기화 시작=====");
+        gameStatusBegin();  // 게임 상태 초기화
+        saveGameSet("capacity", 3);
+        saveGameSet("beginRange", 1);
+        saveGameSet("endRange", 9);
+        System.out.println("=====게임 설정 초기화 완료=====");
     }
 
-    public void save(String key, Integer num) {
+    public static void saveGameSet(String key, Integer num) {
         gameSetStore.put(key, num);
     }
 
-    public Integer get(String key) {
+    public static Integer getGameSet(String key) {
+        if (!gameSetStore.containsKey(key)) {
+            throw new IllegalArgumentException("해당 옵션은 존재하지 않는 옵션 입니다.");
+        }
         return gameSetStore.get(key);
+    }
+
+    public static boolean isGameStatus() {
+        return gameStatus.get("status");
+    }
+
+    public static void gameStatusBegin() {
+        gameStatus.put("status", true);
+    }
+
+    public static void gameStatusEnd() {
+        gameStatus.put("status", false);
     }
 
 }
