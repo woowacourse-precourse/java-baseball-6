ee6cc7000860b45b14c7407662e3fe657f78fb4a
SeolHuiGwan9478 <shg5977@naver.com>
2023-10-23 15:37:35 +0900

    [style] : ValidationCheck 함수 이름 수정

--- src/test/java/baseball/util/ValidationCheck.java
+++ src/test/java/baseball/util/ValidationCheck.java
@@ -14,24 +14,24 @@ public class ValidationCheck {
     }
 
     // 유효성 검사 : 재시작/종료 확인
-    public boolean validUserCommand(String cmdStr){
+    public boolean validateUserCommand(String cmdStr){
         int cmdInt = validateNumber(cmdStr);
         if(cmdInt == 1 || cmdInt == 2) return true;
         return false;
     }
 
     // 유효성 검사 : 숫자 자리수 확인
-    public boolean validUserNumber(String cmdStr){
+    public boolean validateUserNumber(String cmdStr){
         int cmdInt = validateNumber(cmdStr);
         int digit = Integer.toString(cmdInt).length();
         if(digit == 3){
-            return validDuplicate(cmdStr); // 중복 체크
+            return validateDuplicate(cmdStr); // 중복 체크
         }
         return false;
     }
 
     // 유효성 검사 : 중복 숫자 검사
-    private boolean validDuplicate(String cmdStr){
+    private boolean validateDuplicate(String cmdStr){
         Set<Character> uniqueChars = new HashSet<>();
         for(char c : cmdStr.toCharArray()){
             if(!uniqueChars.add(c)){
