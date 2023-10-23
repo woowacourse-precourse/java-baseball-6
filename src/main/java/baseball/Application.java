3ecfd1b8414feaec34b0f656db74690bbbd9d444
saaut <saaut60@naver.com>
2023-10-23 22:44:27 +0900

    프로젝트 구조 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import baseball.View.BaseballGame;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseballGame baseballGame= new BaseballGame();
+
+        baseballGame.run();
     }
 }
---
+++ src/main/java/baseball/View/BaseballGame.java
@@ -0,0 +1,21 @@
+package baseball.View;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class BaseballGame {
+
+    Integer[] userNumbers={};
+    Integer[] randomNumbers={};
+    public void run(){
+        //게임 전체 루프
+        while (true) {
+            System.out.println("숫자 야구 게임을 시작합니다.");
+
+
+        }
+    }
+
+
+
+}
+
