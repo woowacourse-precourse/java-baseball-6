b7d12cfd012d3287aa3af004eed5a7332cd48cc4
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-10-24 02:19:40 +0900

    #3 Refactoring: 게임 종료 후 재시작 입출력 코드 변경
    게임 종료 후 재시작 혹은 게임 종료를 묻는 코드에 대한 사용자의 입력 부분에서 함수가 겹쳐서 잘못 출력이 되어 코드 수정
    
    Resolves: #2

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -32,7 +32,7 @@ public class Application {
 			}
 
 			System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-			String endingNum = InputPlayerNum();
+			String endingNum = Console.readLine();
 
 			if (endingNum.equals("2")) {
 				break;
