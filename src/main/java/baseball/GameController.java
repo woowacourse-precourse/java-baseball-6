e8fc94bdac6165d31707abbbfbf478f91e9c7bfd
wook <dong930112@gmail.com>
2023-10-23 10:03:07 +0900

    feat(GameController): 게임 초기 세팅과 분기에 따른 진행 기능 추가 게임 초기 설정: 정답숫자 생성 진행 분기 설정: 숫자 입력에 따른 'ball', 'strike' 출력, 정답을 맞췄을 경우 진행 분기 세팅

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -1,23 +1,39 @@
 package baseball;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class GameController {
 
+  private NumberController numberController;
+
+  public GameController(NumberController numberController) {
+    this.numberController = numberController;
+  }
 
   public void gameStart() {
+    List<Integer> resultNumList = numberController.generate(); //정답숫자리스트
+
+    Integer gameStatusNumber = 0; // 게임진행 상태 숫자
+    Integer strike = 0;
+    Integer ball = 0;
+
+    System.out.println("숫자 야구 게임을 시작합니다.");
 
     while (true) {
-      int result = 0; // 정답을 생성하여 주입
-      int gameStatusNumber = 0; // 게임진행 상태 숫자
-
-      System.out.println("숫자 야구 게임을 시작합니다.");
       System.out.print("숫자를 입력해주세요 : ");
 
       Scanner scanner = new Scanner(System.in);
-      int input = scanner.nextInt();
+      int inputNum = scanner.nextInt();
+      List<Integer> inputNumList = numberController.splitter(inputNum);
 
-      if(result == input) {
+      Map<String, Integer> report = numberController.compareNumberList(resultNumList, inputNumList);
+      strike = report.get("strike");
+      ball = report.get("ball");
+
+      if(strike == 3) {
+        System.out.println("3스트라이크");
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 
         while (true) {
@@ -32,10 +48,25 @@ public class GameController {
         }
 
         if(gameStatusNumber == 1) {
+          resultNumList = numberController.generate(); //정답숫자리스트
+
           continue;
         }
         break;
       }
+      else if(strike == 0 && ball == 0) {
+        System.out.println("낫싱");
+      }
+      else if(strike != 0 && ball != 0) {
+        System.out.println(ball + "볼" + " " + strike + "스트라이크");
+      }
+      else if(strike != 0 && ball == 0) {
+        System.out.println(strike + "스트라이크");
+      }
+      else if(strike == 0 && ball != 0) {
+        System.out.println(ball + "볼");
+      }
+
     }
   }
 }
