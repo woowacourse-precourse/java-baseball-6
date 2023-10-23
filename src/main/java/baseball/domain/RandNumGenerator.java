5b4443fb2ccf929447b6a4470ad4bcad8bf7f738
Hyoseop Song <crad_on25@naver.com>
2023-10-23 13:23:07 +0900

    feat(RandNumGenerator): RandNumGenerator 클래스 생성자 및 변수 선언

---
+++ src/main/java/baseball/domain/RandNumGenerator.java
@@ -0,0 +1,9 @@
+package baseball.domain;
+
+public class RandNumGenerator {
+    public static final int START_INCLUSIVE = 1;
+    public static final int END_INCLUSIVE = 9;
+
+    private RandNumGenerator() {
+    }
+}
