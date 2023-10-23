260fd46f9c15355e358bf4b108ed5d526a854faf
hee9841 <lee98417716@gmail.com>
2023-10-24 07:32:20 +0900

    :white_check_mark: add Game Test
    - game 실행 테스트 케이스 추가

--- src/test/java/baseball/service/GameTest.java
+++ src/test/java/baseball/service/GameTest.java
@@ -26,13 +26,19 @@ class GameTest {
     }
 
     protected final String output() {
-        return captor.toString();
+        return captor.toString().replace("\r\n", ",").trim();
     }
-
 
     //2. 게임 실행
     @Test
     void runGame() {
+        //입력 받음
+        //게임 실행
+    }
+
+
+    @Test
+    void compareNumbers() {
         //given
         //컴퓨터 랜덤 수 초기화
         String computer_number = "123";
@@ -40,30 +46,25 @@ class GameTest {
 
         //when & then
         //3볼
-        game.runGame(computer_number, "312");
-        assertEquals(output(), "3볼");
+        game.compareNumbers(computer_number, "312", 0, 0);
         //2볼
-        game.runGame(computer_number, "352");
-        assertEquals(output(), "2볼");
+        game.compareNumbers(computer_number, "352", 0, 0);
         //1볼
-        game.runGame(computer_number, "347");
-        assertEquals(output(), "1볼");
+        game.compareNumbers(computer_number, "347", 0, 0);
         //1볼 1스트라이크
-        game.runGame(computer_number, "142");
-        assertEquals(output(), "1볼 1스트라이크");
+        game.compareNumbers(computer_number, "142", 0, 0);
         //1스트라이크
-        game.runGame(computer_number, "147");
-        assertEquals(output(), "1스트라이크");
+        game.compareNumbers(computer_number, "147", 0, 0);
         //2스트라이크
-        game.runGame(computer_number, "127");
-        assertEquals(output(), "2스트라이크");
+        game.compareNumbers(computer_number, "127", 0, 0);
         //3스트라이크
-        game.runGame(computer_number, "123");
-        assertEquals(output(), "3스트라이크");
+        game.compareNumbers(computer_number, "123", 0, 0);
         //낫싱
-        game.runGame(computer_number, "456");
-        assertEquals(output(), "낫싱");
+        game.compareNumbers(computer_number, "456", 0, 0);
 
+        String output = "";
+        output = output();
+        assertEquals(output, "3볼,2볼,1볼,1볼 1스트라이크,1스트라이크,2스트라이크,3스트라이크,낫싱,");
     }
 
     //3. 게임 초기화
