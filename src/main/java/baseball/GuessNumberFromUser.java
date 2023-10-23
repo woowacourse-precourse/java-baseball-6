03135d36d5b974cff204b63e828107937ee2ba15
jihyuk <kwlgur0416@gmail.com>
2023-10-23 12:50:44 +0900

    feature : 추측값 받기 기능의 구조 작성

---
+++ src/main/java/baseball/GuessNumberFromUser.java
@@ -0,0 +1,16 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GuessNumberFromUser {
+
+    public List<Integer> getGuessNumber() {
+        return new ArrayList<>();
+    }
+
+    private void validate() {
+
+    }
+
+}
