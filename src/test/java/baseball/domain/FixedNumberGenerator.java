f84c05389b04343cf59de0ed566723d9f60ff41c
minisyu <iswater.kim@gmail.com>
2023-10-23 20:40:08 +0900

    refactor(FixedNumberGenerator): 생성자 리팩토링

--- src/test/java/baseball/domain/FixedNumberGenerator.java
+++ src/test/java/baseball/domain/FixedNumberGenerator.java
@@ -1,13 +1,15 @@
 package baseball.domain;
 
 import baseball.util.NumberGenerator;
+import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.List;
 
 public class FixedNumberGenerator implements NumberGenerator {
     private final Deque<Integer> fixedNumbers;
 
-    public FixedNumberGenerator(Deque<Integer> fixedNumbers) {
-        this.fixedNumbers = fixedNumbers;
+    public FixedNumberGenerator(List<Integer> fixedNumbers) {
+        this.fixedNumbers = new ArrayDeque<>(fixedNumbers);
     }
 
     @Override
