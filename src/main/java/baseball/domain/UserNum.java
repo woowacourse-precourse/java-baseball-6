6511b56ff6524f65692a3a1545b5fd5d79acd328
leedohyun <lucky77795@naver.com>
2023-10-23 17:55:31 +0900

    chore: 유저가 입력하는 숫자 객체 이름 변경

--- src/main/java/baseball/domain/UserNum.java
+++ src/main/java/baseball/domain/UserNum.java
@@ -3,13 +3,11 @@ package baseball.domain;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
-public class EnterNum {
+public class UserNum {
     private List<Integer> number;
 
-    public EnterNum() {
+    public UserNum() {
         this.number = new ArrayList<>();
     }
 
