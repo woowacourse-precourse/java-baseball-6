dc115b6aab7ff99f8cdb7275456ea110fb7bcd6c
euniiiii <hee9920@naver.com>
2023-10-23 16:51:23 +0900

    feat: 사용자 입력에 대한 결과 출력하는 메서드 구현

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -58,4 +58,12 @@ public class Player {
 
         return cnt;
     }
+
+    public static void printResult(int strike, int ball) {
+        if (strike == 0 && ball == 0) System.out.println("낫싱");
+        else if (strike > 0 && ball == 0) System.out.println(strike + "스트라이크");
+        else if (strike == 0 && ball > 0) System.out.println(ball + "볼");
+        else if (strike > 0 && ball > 0) System.out.println(ball + "볼" + " " + strike + "스트라이크");
+    }
+
 }
