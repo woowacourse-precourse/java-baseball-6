ea8ea0233a5d886f6d66311189866a4a24ef19da
win9 <krrr883@gmail.com>
2023-10-24 03:56:21 +0900

    [Feat]: 컴퓨터 클래스 생성
    상대의 역할을 할 컴퓨터 클래스를 생성

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,11 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import jdk.jshell.execution.Util;
+
+import java.util.Arrays;
+
+public class Computer {
+    private int[] numbers = new int[3];
+
+}
