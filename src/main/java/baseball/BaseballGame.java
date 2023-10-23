7c69c50e1b71f3ab095afda50743a57170c881cd
suhyun__ <thebbbb1206@gmail.com>
2023-10-23 16:57:42 +0900

    feat: 숫자 입력 문구 출력 기능 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import java.util.Scanner;
+
 public class BaseballGame {
     private static void showStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    private static void showInputUserNumber() {
+        System.out.println("숫자를 입력해주세요 : ");
     }
 }
