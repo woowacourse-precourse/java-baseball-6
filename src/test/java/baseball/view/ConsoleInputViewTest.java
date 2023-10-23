af8b5ca79b46fdafb0b53566d434d46ff8aae5b6
eom-tae-in <eti0728@daum.net>
2023-10-24 01:35:47 +0900

    test: 입력 테스트의 예외 테스트들을 하나로 합침 && 테스트 로직 수정

--- src/test/java/baseball/view/ConsoleInputViewTest.java
+++ src/test/java/baseball/view/ConsoleInputViewTest.java
@@ -1,37 +1,37 @@
 package baseball.view;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import baseball.Application;
 import baseball.exception.consoleView.ConsoleInputBlankException;
 import baseball.exception.consoleView.ConsoleInputCanNotChangeToIntException;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 @DisplayNameGeneration(ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
 class ConsoleInputViewTest extends NsTest {
 
-    @Test
-    void 입력_값이_비어있으면_예외를_발생시킨다() {
-        // given
-        String blank = " ";
-
+    @MethodSource("wrongInputProvider")
+    @ParameterizedTest(name = "{0}")
+    void 입력_값이_비어있으면_예외를_발생시킨다(String message, String text, Class<? extends Throwable> expected) {
         // when & then
-        assertThatThrownBy(() -> run(blank))
-                .isInstanceOf(ConsoleInputBlankException.class);
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(text))
+                        .isInstanceOf(expected)
+        );
     }
 
-    @Test
-    void 입력_문자열이_숫자로_변환이_불가능하면_예외를_발생시킨다() {
-        // given
-        String str = "test";
-
-        // when & then
-        assertThatThrownBy(() -> run(str))
-               .isInstanceOf(ConsoleInputCanNotChangeToIntException.class);
+    private static Stream<Arguments> wrongInputProvider() {
+        return Stream.of(
+                Arguments.of("입력이 비었을 경우", " ", ConsoleInputBlankException.class),
+                Arguments.of("입력이 숫자가 아닌 문자인 경우", "test", ConsoleInputCanNotChangeToIntException.class));
     }
 
     @Override
