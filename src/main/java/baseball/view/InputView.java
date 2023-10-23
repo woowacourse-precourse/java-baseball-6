f796468052e5f36334cfbabcb825f5c9adca3c02
yoonjuho <dbswn990@naver.com>
2023-10-24 03:36:10 +0900

    refactor(InputView): 숫자 입력 검증 메서드 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -5,15 +5,30 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class InputView {
+    private static final Pattern NUMERIC_PATTERN = Pattern.compile("^[0-9]*$");
+
     private static final String SEPARATOR = "";
-    public static List<Integer> inputNumbers() {
+    public List<Integer> inputGameNumbers() {
         System.out.println("숫자를 입력해주세요 : ");
         String numbers = Console.readLine();
+        validateNumeric(numbers);
         return Arrays.stream(numbers.split(SEPARATOR))
                 .map(Integer::valueOf)
                 .collect(Collectors.toUnmodifiableList());
     }
+
+
+
+    private void validateNumeric(String input){
+        if(input.isBlank() || !NUMERIC_PATTERN.matcher(input).matches()){
+            throw new IllegalArgumentException("숫자를 입력해야 합니다.");
+        }
+    }
+
+
+
 }
