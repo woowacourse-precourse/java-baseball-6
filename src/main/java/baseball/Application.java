a5e7339fe2297437d9b264ca3a240084b516859e
ssunnykku <ssunnykku@gmail.com>
2023-10-24 08:36:44 +0900

    fix: 게임 종료 후 입력값 예외처리(restartOrNot)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,17 +4,20 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
         System.out.println("숫자 야구 게임을 시작합니다.");
         List<Integer> computer = selectNumbers();
         game(computer);
     }
 
+    //
     public static String inputNumbers() {
         System.out.print("숫자를 입력해주세요: ");
         return Console.readLine();
@@ -25,6 +28,7 @@ public class Application {
         return Console.readLine();
     }
 
+//    computer
     public static List<Integer> selectNumbers() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
@@ -36,42 +40,47 @@ public class Application {
         return computer;
     }
 
-
+//    player
     public static void game(List<Integer> computer) {
           String input = inputNumbers();
           if(input.length() != 3) {
                 throw new IllegalArgumentException("숫자 3개를 입력하세요.");
             }
             gameResult(computer, input);
-
     }
 
+//    computer 역할
     public static void gameResult(List<Integer> computer, String input) {
         int cntStrike = 0;
         int cntBall = 0;
+        HashSet<Integer> checkOverlap = new HashSet<>();
         for(int i=0; i<computer.size(); i++) {
             int num = stringToInt(input, i);
+            checkOverlap.add(num);
             if((num) == computer.get(i)) {
                 cntStrike += 1;
             } else if (computer.contains(num)) {
                 cntBall += 1;
             }
         }
+        if(checkOverlap.size() < 3) {
+            throw new IllegalArgumentException("서로 다른 3자리의 수를 입력하세요.");
+        }
         count(cntStrike, cntBall);
 
         if (cntStrike == 3 && cntBall == 0) {
-            restartOrNot(computer);
+            restartOrNot();
         } else {
             game(computer);
         }
     }
-
 
     public static int stringToInt(String str, int i) {
         int num = Character.getNumericValue(str.charAt(i));
         return num;
     }
 
+    // computer 역할
     public static void count(int cntStrike, int cntBall) {
         if (cntStrike == 0 && cntBall == 0) System.out.println("낫싱");
         else if (cntStrike == 0 && cntBall > 0 ) System.out.println(cntBall + "볼");
@@ -82,13 +91,15 @@ public class Application {
         }
 
     }
-
-    public static void restartOrNot(List<Integer> computer) {
-        int num = stringToInt(inputResultNumber(),0);
+//
+    public static void restartOrNot() {
+        String input = inputResultNumber();
+        int num = stringToInt(input,0);
         if (num == 1) gameRestart();
+        else if (num != 2) throw new IllegalArgumentException("서로 다른 3자리의 수를 입력하세요.");
     }
 
-
+// computer 게임 재시작 , 구현
     public static void gameRestart() {
         List<Integer> computer = selectNumbers();
         game(computer);
