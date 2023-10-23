2f518e44c2b522abff93ce981a132a9cea721d6a
minsu20 <syt07100@gmail.com>
2023-10-23 21:44:40 +0900

    feat: 힌트 값을 담은 Baseball 도메인 생성

---
+++ src/main/java/baseball/domain/BaseBall.java
@@ -0,0 +1,22 @@
+package baseball.domain;
+
+public class BaseBall {
+    private int ball;
+    private int strike;
+
+    public void addBall(){
+        this.ball++;
+    }
+    public void addStrike(){
+        this.strike++;
+    }
+    public int getBall(){
+        return this.ball;
+    }
+    public int getStrike(){
+        return this.strike;
+    }
+    public boolean checkNothing(){
+        return (this.ball==0&&this.strike==0);
+    }
+}
