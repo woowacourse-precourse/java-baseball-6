ba3f30ec7824299ca9a9a9d8e9643e3b9a45e99f
sangwopark19 <sangwopark19@gmail.com>
2023-10-23 23:41:42 +0900

    Scanner와 UserInterface 멤버변수 추가 및 생성자 주입

--- src/main/java/baseball/Interface/UserInterface.java
+++ src/main/java/baseball/Interface/UserInterface.java
@@ -1,4 +1,14 @@
 package baseball.Interface;
 
+import baseball.game.BaseballGame;
+import java.util.Scanner;
+
 public class UserInterface {
+    private final Scanner scanner;
+    private final BaseballGame baseballGame;
+
+    public UserInterface(Scanner scanner, BaseballGame baseballGame) {
+        this.scanner = scanner;
+        this.baseballGame = baseballGame;
+    }
 }
