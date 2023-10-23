934de869c061033b9ada3e63afe88d848ae566f8
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2023-10-23 19:46:02 +0900

    refactor: 게임 종료 판단 로직 분리
    - 게임 종료 판단 로직을 따로 메서드로 빼서 분리

--- src/main/java/baseball/GameService.java
+++ src/main/java/baseball/GameService.java
@@ -28,18 +28,24 @@ public class GameService {
             String result = compareService.run(inputValueList, computer);
             System.out.println(result);
 
-            if(result.equals("3스트라이크")) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
-                        "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                String s = Console.readLine();
-                // 다시 시작
-                if(s.equals("1")) {
-                    computer.clear();
-                    randomService.random(computer);
-                }
-                // 종료
-                else return;
-            }
+            if (isGameOver(result) == 0) return;
         }
+    }
+
+    // 결과값으로 게임 종료를 판단하는 로직
+    private int isGameOver(String result) {
+        if(result.equals("3스트라이크")) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+                    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String input = Console.readLine();
+            // 다시 시작
+            if(input.equals("1")) {
+                computer.clear();
+                randomService.random(computer);
+            }
+            // 종료
+            else return 0;
+        }
+        return 1;
     }
 }
