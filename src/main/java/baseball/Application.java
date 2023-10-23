35fb0a27aa8f00f9f0ad900c07688bc22d3699cc
takoyakimchi <c4nyou@gmail.com>
2023-10-23 17:38:42 +0900

    컴퓨터 숫자 생성 / 유저 숫자 생성 메소드화 GameResult에 toString 사용

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,6 +6,51 @@ import static camp.nextstep.edu.missionutils.Console.*;
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class Application {
+    public static void main(String[] args) {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        boolean isEnd = false; // 게임 종료 여부
+
+        // 전체 게임 루프
+        while (!isEnd) {
+            // 게임 승리 여부
+            boolean isWin = false;
+
+            // 컴퓨터 숫자 생성
+            List<Integer> computer = generateComputerNums();
+            for (Integer i : computer) {
+                System.out.println("i = " + i);
+            }
+
+            // 매 라운드 루프
+            while (!isWin) {
+                // 유저 숫자 입력 및 생성
+                List<Integer> user = generateUserNums();
+
+                // 게임 진행
+                GameResult result = play(user, computer);
+
+                // 게임 결과 출력
+                System.out.println(result);
+
+                // 게임 승리 시
+                if (result.getStrike() == 3) {
+                    isWin = true;
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                    String startOrEnd = readLine();
+
+                    if (startOrEnd.equals("1")) {
+                        continue;
+                    } else if (startOrEnd.equals("2")) {
+                        isEnd = true;
+                    } else {
+                        throw new IllegalArgumentException();
+                    }
+                }
+            } // 매 라운드 루프
+        } // 전체 게임 루프
+    }
+
     public static void checkInput(String str) {
         List<Character> legalValues = Arrays.asList('1', '2', '3', '4', '5', '6', '7', '8', '9');
 
@@ -47,77 +92,31 @@ public class Application {
         return result;
     }
 
-    private static void printGameResult(GameResult result) {
-        if (result.getBall() > 0 && result.getStrike() == 0) {
-            // ball only
-            System.out.println(result.getBall() + "볼");
-        } else if (result.getBall() == 0 && result.getStrike() > 0) {
-            // strike only
-            System.out.println(result.getStrike() + "스트라이크");
-        } else if (result.getBall() > 0 && result.getStrike() > 0) {
-            // ball and strike
-            System.out.println(result.getBall() + "볼 " + result.getStrike() + "스트라이크");
-        } else {
-            // nothing
-            System.out.println("낫싱");
+    private static List<Integer> generateComputerNums() {
+        List<Integer> computer = new ArrayList<>();
+
+        while (computer.size() < 3) {
+            int digit = pickNumberInRange(1, 9);
+            if (!computer.contains(digit)) {
+                computer.add(digit);
+            }
         }
+
+        return computer;
     }
 
-    public static void main(String[] args) {
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        boolean isEnd = false; // 게임 진행 여부
-        boolean isWin; // 게임 승리 여부
+    private static List<Integer> generateUserNums() {
+        List<Integer> user = new ArrayList<>();
 
-        // 전체 게임 루프
-        while (!isEnd) {
-            // 게임 승리 여부 초기화
-            isWin = false;
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = readLine();
+        checkInput(input); // 입력값 예외 처리
 
-            // (컴퓨터) 랜덤 숫자 생성
-            List<Integer> computer = new ArrayList<>();
-            while (computer.size() < 3) {
-                int digit = pickNumberInRange(1, 9);
-                if (!computer.contains(digit)) {
-                    computer.add(digit);
-                }
-            }
+        for (int i = 0; i < 3; i++) {
+            int digit = Character.getNumericValue(input.charAt(i)); // char -> int 변환
+            user.add(digit);
+        }
 
-            // 매 라운드 루프
-            while (!isWin) {
-                // 사용자 입력
-                System.out.print("숫자를 입력해주세요 : ");
-                String input = readLine();
-                checkInput(input); // 입력값 예외 처리
-
-                // 사용자 입력값 -> List 변환
-                List<Integer> user = new ArrayList<>();
-                for (int i = 0; i < 3; i++) {
-                    int digit = Character.getNumericValue(input.charAt(i));
-                    user.add(digit);
-                }
-
-                // 게임 진행
-                GameResult result = play(user, computer);
-
-                // 게임 결과 출력
-                printGameResult(result);
-
-                // 게임 승리 시
-                if (result.getStrike() == 3) {
-                    isWin = true;
-                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                    String startOrEnd = readLine();
-
-                    if (startOrEnd.equals("1")) {
-                        continue;
-                    } else if (startOrEnd.equals("2")) {
-                        isEnd = true;
-                    } else {
-                        throw new IllegalArgumentException();
-                    }
-                }
-            } // 매 라운드 루프
-        } // 전체 게임 루프
+        return user;
     }
 }
--- src/main/java/baseball/GameResult.java
+++ src/main/java/baseball/GameResult.java
@@ -24,4 +24,17 @@ public class GameResult {
     public void addStrike() {
         this.strike++;
     }
+
+    @Override
+    public String toString() {
+        if (ball > 0 && strike == 0) {
+            return ball + "볼";
+        } else if (ball == 0 && strike > 0) {
+            return strike + "스트라이크";
+        } else if (ball > 0 && strike > 0) {
+            return ball + "볼 " + strike + "스트라이크";
+        } else {
+            return "낫싱";
+        }
+    }
 }
