e45c72c5505c34c247b913e6e6a9068d90221e9e
MingyeomKim <mingyum119@naver.com>
2023-10-23 16:53:00 +0900

    feat(GameController): 게임을 시작하는 함수 생성 후 Application에서 호출
    게임을 시작하고 랜덤 숫자를 생성하는 함수를 생성하고 Application에서 실행될 수 있도록 호출한다.

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -4,7 +4,13 @@ public class GameController {
 
     private final String GAME_START_GUIDE = "숫자 야구 게임을 시작합니다.";
 
+    private ComputerController computerController = new ComputerController();
+
     public GameController() {
         System.out.println(GAME_START_GUIDE);
+    }
+
+    public void playGame() {
+        computerController.saveRandomNumbers();
     }
 }
