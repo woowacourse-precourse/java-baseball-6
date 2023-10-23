2a7f16a6c616c495c797c2041452df74162b91e3
EvelynKimm <evekimmy09@gmail.com>
2023-10-23 19:45:55 +0900

    rename: change class name

--- src/main/java/baseball/model/NumConstant.java
+++ src/main/java/baseball/model/NumConstant.java
@@ -1,15 +1,16 @@
 package baseball.model;
 
-public final class NumConst {
+public final class NumConstant {
 
     public static final int FULL_STRIKE = 3;
+
     public static final int NUM_LENGTH = 3;
 
     public static final int MIN_NUMBER = 1;
 
     public static final int MAX_NUMBER = 9;
 
-    public static final String RESTART = String.valueOf(1);
+    public static final String RESTART = "1";
 
-    public static final String END = String.valueOf(2);
+    public static final String END = "2";
 }
