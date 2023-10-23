af9f7aac96469b5656276742b1695a5afb6d0e34
seondays <agseon9@gmail.com>
2023-10-23 18:11:53 +0900

    feat: 게임 시작을 위한 main 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,10 @@
 package baseball;
 
+import baseball.controller.Service;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Service service = new Service();
+        service.gameStart();
     }
 }
