3d08e47585ae591400c6fec94552aa3c79ed6335
SungKyum Kim <sungkyum1@naver.com>
2023-10-23 18:33:02 +0900

    feat: 전체 기능 구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,38 +1,87 @@
 package baseball;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BaseballGame {
 
     private boolean isProgress = true;
-    private String randomNumber;
-    private String inputNumber;
 
     public void startGame(){
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-        Scanner scanner = new Scanner(System.in);
-
         while (isProgress) {
-            System.out.println("숫자를 입력해주세요 : ");
-            inputNumber = scanner.next();
-            // result = 판단
-//            if (result) {
-//
-//            }
+            List<Integer> randomNumbers = RandomNumberGenerator.generate();
+
+            String result = "";
+
+            while (!result.equals("3스트라이크")) {
+                System.out.print("숫자를 입력해주세요 : ");
+                List<Integer> inputNumbers = setInputNumber();
+                result = compareNumbers(randomNumbers, inputNumbers);
+                System.out.println(result);
+            }
 
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            int select = scanner.nextInt();
-
-            if (select == 1) {
+            int selection = Integer.parseInt(Console.readLine());
+            if (selection == 1) {
                 isProgress = true;
             }
-            if (select == 2) {
+            if (selection == 2) {
                 isProgress = false;
             }
         }
 
+    }
+
+    private List<Integer> setInputNumber() {
+        String line = Console.readLine();
+
+        List<Integer> numbers = new ArrayList<>();
+
+        if (line.length() > 3) {
+            throw new IllegalArgumentException();
+        }
+        for (String number : line.split("")) {
+            numbers.add(Integer.valueOf(number));
+        }
+
+        return numbers;
+    }
+
+    private String compareNumbers(List<Integer> randomNumbers, List<Integer> inputNumbers) {
+        int ball = 0;
+        int strike = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (randomNumbers.get(i) == inputNumbers.get(j) && i == j) {
+                    strike++;
+                }
+                if (randomNumbers.get(i) == inputNumbers.get(j) && i != j) {
+                    ball++;
+                }
+            }
+        }
+
+        String result = "";
+
+        if (strike == 3) {
+            return "3스트라이크";
+        }
+        if (ball == 0 && strike == 0) {
+            return "낫싱";
+        }
+        if (ball != 0) {
+            result += ball + "볼 ";
+        }
+        if (strike != 0) {
+            result += strike +"스트라이크 ";
+        }
+
+        return result;
     }
 }
