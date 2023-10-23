097606d59335bc81ba7f9db0be9add56430101f7
leedohyun <lucky77795@naver.com>
2023-10-23 19:30:01 +0900

    refactor: Service에 있던 ball,strike 카운팅을 옮김.

---
+++ src/main/java/baseball/service/BallCounter.java
@@ -0,0 +1,26 @@
+package baseball.service;
+
+import java.util.List;
+
+public class BallCounter {
+
+    public Integer countStrike(List<Integer> userNum, List<Integer> comNum) {
+        int strike = 0;
+        for (int i = 0; i < userNum.size(); i++) {
+            if (userNum.get(i) == comNum.get(i)) {
+                strike++;
+            }
+        }
+        return strike;
+    }
+
+    public Integer countCorrect(List<Integer> userNum, List<Integer> comNum) {
+        int correct = 0;
+        for (int i = 0; i < userNum.size(); i++) {
+            if (comNum.contains(userNum.get(i))) {
+                correct++;
+            }
+        }
+        return correct;
+    }
+}
