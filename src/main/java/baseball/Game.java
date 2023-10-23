12dd5ef2e09cc479948e7186d3d47fbbbcf28b84
foura1201 <84717712+foura1201@users.noreply.github.com>
2023-10-23 18:34:29 +0900

    feat: Generate random number

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,7 +1,9 @@
 package baseball;
+
 import java.util.List;
 
 import baseball.global.Announcement;
+import baseball.global.util.RandomUtil;
 
 public class Game extends GameController {
 
@@ -11,5 +13,10 @@ public class Game extends GameController {
     Game() {
         System.out.println(Announcement.GAME_START);
         this.terminate = false;
+    }
+
+    // 게임 진행
+    public void playGame() {
+        this.answer = RandomUtil.randomNumber();
     }
 }
