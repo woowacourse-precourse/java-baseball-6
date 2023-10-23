805eb22481b590ef0e20d612f3adeefe8117ed42
gyulim5 <ohrbfla1004@khu.ac.kr>
2023-10-23 18:04:31 +0900

    ✨ Feat: 종류 별 출력함수 생성

--- src/main/java/baseball/Output.java
+++ src/main/java/baseball/Output.java
@@ -2,8 +2,8 @@ package baseball;
 
 public class Output {
 
-    private static final String startingMessage = "숫자 야구 게임을 시작합니다.";
-    private static final String inputNumberMessage = "숫자를 입력해주세요 : ";
+    private static final String startMessage = "숫자 야구 게임을 시작합니다.";
+    private static final String inputMessage = "숫자를 입력해주세요 : ";
     private static final String gameEndMessage = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String gameRestartMessage = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
@@ -24,5 +24,21 @@ public class Output {
         else if (strike > 0) {
             System.out.println(strike+STRIKE);
         }
+    }
+
+    public void printStartMessage() {
+        System.out.println(startMessage);
+    }
+
+    public void printInputMessage() {
+        System.out.print(inputMessage);
+    }
+
+    public void printGameEndMessage() {
+        System.out.println(gameEndMessage);
+    }
+
+    public void printGameRestartMessage() {
+        System.out.println(gameRestartMessage);
     }
 }
