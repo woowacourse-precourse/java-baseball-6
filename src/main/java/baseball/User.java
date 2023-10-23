3911ca8fab4bbbe5ff5d965ca5e4a9a914240879
inwoo <korean5078@gmail.com>
2023-10-23 19:13:43 +0900

    fix: User 객체 입력 받을 시 제한 사항 적용 1. input3DigitNumber함수는 3자리 숫자를 입력받을 때 사용. 입력받은 숫자가 아니거나 혹은 3자리 수가 아니거나 중복된 숫자가 있을 때 IllegalStateException 발생 2. inputRestartNumber함수는 게임 재시작 여부 입력받을 때 사용. 1, 2가 아닌 값을 입력받을 때 IllegalStateException 발생

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,9 +1,44 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.HashSet;
+import java.util.Set;
 
 public class User {
-	public int inputNumber() {
-		return Integer.parseInt(Console.readLine());
+	public int input3DigitNumber() {
+		String input = Console.readLine();
+
+		// 입력받은 숫자가 3자리가 아닐 때 예외 처리
+		if (input.length() != 3) {
+			throw new IllegalStateException();
+		}
+
+		// 3자리 중에서 하나라도 1-9까지 수가 아닐 때 예외 처리
+		for (int i = 0; i < input.length(); i++) {
+			if (input.charAt(i) < 49 || input.charAt(i) > 57) {
+				throw new IllegalStateException();
+			}
+		}
+
+		// 3자리 수 중에서 숫자가 중복되면 예외 처리
+		for (int i = 0; i < input.length(); i++) {
+			for (int j = i; j < input.length(); j++) {
+				if (input.charAt(i) == input.charAt(j)) {
+					throw new IllegalStateException();
+				}
+			}
+		}
+
+		return Integer.parseInt(input);
+	}
+
+	public int inputRestartNumber() {
+		String input = Console.readLine();
+
+		if (!input.equals("1") && !input.equals("2")) {
+			throw new IllegalStateException();
+		}
+
+		return Integer.parseInt(input);
 	}
 }
