f4005d3db96124972f7d3c06253a7b03fe571033
MingyeomKim <mingyum119@naver.com>
2023-10-23 16:11:31 +0900

    refactor(ComputerController): 중복 없이 3자리의 수가 만들어지도록 수정
    기존 코드에서 중복을 고려하지 않고 리스트에 값을 넣고 있었다. List.contains 함수를 사용해 이미 리스트에 있는 값은 넣지 않도록 한다.

--- src/main/java/baseball/controller/ComputerController.java
+++ src/main/java/baseball/controller/ComputerController.java
@@ -12,7 +12,9 @@ public class ComputerController {
         List<Integer> numbers = new ArrayList<>();
         while (numbers.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
-            numbers.add(randomNumber);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
         }
         computer.saveNumbers(numbers);
     }
