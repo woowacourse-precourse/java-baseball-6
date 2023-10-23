5aa297a41d65b9081a2ed2ea9fd5dbace37fbb6d
limzzum <chfhddl1234@naver.com>
2023-10-23 19:34:21 +0900

    :sparkles: feat : 사용자 입력값에 대한 결과 반환 메서드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import baseball.dto.StrikeBallCnt;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -30,7 +31,25 @@ public class Application {
             System.out.print("숫자를 입력해주세요 : ");
             input = br.readLine();
             checkValidNumber(input);
+            StrikeBallCnt result = getResult(input, answerNumber);
         }
+    }
+
+    private static StrikeBallCnt getResult(String input, List<Integer> answer) {
+        int strikeCnt = 0;
+        int ballCnt = 0;
+
+        char[] chars = input.toCharArray();
+        for (int i = 0; i < 3; i++) {
+            if(chars[i] - '0' == answer.get(i)){
+                strikeCnt += 1;
+            }else if(answer.contains(chars[i] - '0')){
+                ballCnt += 1;
+            }
+        }
+
+        return new StrikeBallCnt(strikeCnt, ballCnt);
+
     }
 
     private static void checkValidNumber(String input) {
