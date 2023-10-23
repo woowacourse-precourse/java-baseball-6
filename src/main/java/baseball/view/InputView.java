90d5e963cd9f09b847a1ca440627d1c80f792dd7
khsrla9806 <kimhunsope@naver.com>
2023-10-23 11:14:43 +0900

    feat: 유저가 입력한 세글자 숫자에 대한 유효성 검사 구현
    * 숫자만 입력했는지
    * 3자리 숫자가 맞는지
    * 1부터 9까지의 숫자가 맞는지
    * 중복된 숫자가 없는지

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,14 +1,54 @@
 package baseball.view;
 
+
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.*;
+
 public class InputView {
+    private static final int INPUT_NUMBER_SIZE = 3;
+
     public static String readUserNumber() {
         OutputView.printInputNumberMessage();
         String inputNumber = Console.readLine();
 
+        checkIsNumber(inputNumber);
+        checkNumberSize(inputNumber);
+        checkNumberRange(inputNumber);
+        checkDuplicatedNumber(inputNumber);
+
         return inputNumber;
     }
+
+    private static void checkIsNumber(String inputNumber) {
+        try {
+            Integer.parseInt(inputNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자만 입력이 가능합니다.");
+        }
+    }
+
+    private static void checkNumberSize(String inputNumber) {
+        if (inputNumber.length() != INPUT_NUMBER_SIZE) {
+            throw new IllegalArgumentException("반드시 3개의 숫자여야 합니다.");
+        }
+    }
+
+    private static void checkNumberRange(String inputNumber) {
+        if (inputNumber.contains("0")) {
+            throw new IllegalArgumentException("1부터 9까지의 숫자만 입력이 가능합니다.");
+        }
+    }
+
+    private static void checkDuplicatedNumber(String inputNumber) {
+        String[] numbers = inputNumber.split("");
+        long count = Arrays.stream(numbers).distinct().count();
+
+        if (count != INPUT_NUMBER_SIZE) {
+            throw new IllegalArgumentException("중복된 숫자는 입력할 수 없습니다.");
+        }
+    }
+
     public static int readGameEndCommand() {
         OutputView.printGameEndMessage();
         String inputCommand = Console.readLine();
