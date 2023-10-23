1b31bc9dbfe9f9ae18587718f4ac440a7cda4805
euniiiii <hee9920@naver.com>
2023-10-23 10:04:21 +0900

    test: 스트라이크 개수 세는 메서드 테스트 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
@@ -209,6 +210,46 @@ class ApplicationTest extends NsTest {
         assertThat(throwable4)
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("중복된 수를 입력했습니다.");
+    }
+
+    @DisplayName("스트라이크 개수 세는 테스트")
+    @Test
+    void countStrikeTest() {
+        // given
+        final int size = 3;
+        final List<Integer> computer = Arrays.asList(1, 3, 5);
+
+        final List<Integer> user1 = Arrays.asList(2, 4, 8); // 0 strike
+
+        final List<Integer> user2 = Arrays.asList(1, 7, 9); // 1 strike
+        final List<Integer> user3 = Arrays.asList(2, 3, 8); // 1 strike
+        final List<Integer> user4 = Arrays.asList(4, 7, 5); // 1 strike
+
+        final List<Integer> user5 = Arrays.asList(1, 3, 9); // 2 strike
+        final List<Integer> user6 = Arrays.asList(1, 2, 5); // 2 strike
+        final List<Integer> user7 = Arrays.asList(2, 3, 5); // 2 strike
+
+        final List<Integer> user8 = Arrays.asList(1, 3, 5); // 3 strike
+
+        // when
+        final int result1 = Player.countStrike(computer, user1, 3);
+        final int result2 = Player.countStrike(computer, user2, 3);
+        final int result3 = Player.countStrike(computer, user3, 3);
+        final int result4 = Player.countStrike(computer, user4, 3);
+        final int result5 = Player.countStrike(computer, user5, 3);
+        final int result6 = Player.countStrike(computer, user6, 3);
+        final int result7 = Player.countStrike(computer, user7, 3);
+        final int result8 = Player.countStrike(computer, user8, 3);
+
+        // then
+        assertThat(result1).isEqualTo(0);
+        assertThat(result2).isEqualTo(1);
+        assertThat(result3).isEqualTo(1);
+        assertThat(result4).isEqualTo(1);
+        assertThat(result5).isEqualTo(2);
+        assertThat(result6).isEqualTo(2);
+        assertThat(result7).isEqualTo(2);
+        assertThat(result8).isEqualTo(3);
     }
 
     @Override
