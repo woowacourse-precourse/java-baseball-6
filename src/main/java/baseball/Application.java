9bd3115cdaf1710374e8330666605da50e5fc86f
김민정 <mjeong@sookmyung.ac.kr>
2023-10-23 11:54:12 +0900

    feat: 숫자 입력 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -9,7 +10,7 @@ public class Application {
     public static void main(String[] args) {
         Application NumberBaseball = new Application();
         List<Integer> computer = NumberBaseball.generateRandomNumber();
-        List<Integer> player = NumberBaseball.generateRandomNumber();
+        List<Integer> player = NumberBaseball.getPlayerNumber();
         System.out.println(computer);
         System.out.println(player);
         System.out.println(Arrays.toString(NumberBaseball.calculateResult(computer, player)));
@@ -36,5 +37,30 @@ public class Application {
             }
         }
         return result;
+    }
+
+    public List<Integer> getPlayerNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+        return validatePlayerNumber(input);
+    }
+
+    public List<Integer> validatePlayerNumber(String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("3자리가 아닙니다.");
+        }
+        List<Integer> player = new ArrayList<>();
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c < '1' || c > '9') {
+                throw new IllegalArgumentException("수가 아닙니다.");
+            }
+            int playerNumber = Character.getNumericValue(c);
+            if (player.contains(playerNumber)) {
+                throw new IllegalArgumentException("서로 다르지 않습니다.");
+            }
+            player.add(playerNumber);
+        }
+        return player;
     }
 }
