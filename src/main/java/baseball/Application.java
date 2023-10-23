5fa610b431a44fb736699e85f1a407c4e1782df0
RashdannHakim <hakim@nex.market>
2023-10-24 08:12:32 +0900

    feat:generate random answer

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,36 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+class Computer {
+
+    private int createRandomNumber() {
+        return Randoms.pickNumberInRange(1, 9);
+
+    }
+
+    public List<Integer> createAnswer() {
+        final int answerSize = 3;
+        List<Integer> computerAnswer = new ArrayList<>();
+
+        while (computerAnswer.size() < answerSize) {
+            int randomNumber = createRandomNumber();
+            if (!computerAnswer.contains(randomNumber)) {
+                computerAnswer.add(randomNumber);
+            }
+        }
+        return computerAnswer;
+    }
+
+}
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        final Computer computer = new Computer();
+        List<Integer> answer = computer.createAnswer();
+        System.out.println(answer);
     }
 }
