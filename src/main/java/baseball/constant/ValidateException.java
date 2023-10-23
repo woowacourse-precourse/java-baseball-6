c12541f995ed6102b8fcb12f8438438832342473
김효준 <khyojun1104@gmail.com>
2023-10-24 01:04:58 +0900

    [refactor] ValidateException : INPUT_NUMBER_EXCEPTION 수정

--- src/main/java/baseball/constant/ValidateException.java
+++ src/main/java/baseball/constant/ValidateException.java
@@ -2,7 +2,7 @@ package baseball.constant;
 
 
 public enum ValidateException {
-    INPUT_NUMBER_EXCEPTION("숫자로 입력이 되어야 합니다!"),
+    INPUT_NUMBER_EXCEPTION("1~9 사이의 숫자로 입력이 되어야 합니다!"),
     INPUT_NUMBER_LENGTH_EXCEPTION("숫자의 길이는 3입니다!"),
     RESTART_INPUT_NUMBER_EXCEPTION("재시작 관련 입력은 1 또는 2의 숫자여야 합니다!"),
     RESTART_INPUT_NUMBER_LENGTH_EXCEPTION("1 또는 2 의 숫자로 한 번만 입력해주세요!"),
