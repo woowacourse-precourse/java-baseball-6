eba79c7b15b68cfe31302cd82700845202ad6b4b
koomchang <leahpar0401@gmail.com>
2023-10-24 01:24:37 +0900

    test: 게임 재시작 혹은 종료에 대한 입력값에 대한 검증 테스트 코드 작성

--- src/test/java/baseball/validator/NewGameOrEndInputValidationTest.java
+++ src/test/java/baseball/validator/NewGameOrEndInputValidationTest.java
@@ -1,4 +1,27 @@
+package baseball.validator;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class NewGameOrEndInputValidationTest {
-  
+
+    NewGameOrEndInputValidation newGameOrEndInputValidation = new NewGameOrEndInputValidation();
+
+    @Test
+    @DisplayName("1. 숫자 판별 2. 1 혹은 2인지 판별")
+    void validate() {
+        Throwable isNumericException = assertThrows(
+                IllegalArgumentException.class,
+                () -> newGameOrEndInputValidation.validate(" ")
+        );
+        assertEquals("숫자가 아닌 값이 포함되어 있습니다.", isNumericException.getMessage());
+
+        Throwable isValueOneOrTwoException = assertThrows(
+                IllegalArgumentException.class,
+                () -> newGameOrEndInputValidation.validate("3")
+        );
+        assertEquals("1과 2중 입력해야 합니다.", isValueOneOrTwoException.getMessage());
+    }
 }
