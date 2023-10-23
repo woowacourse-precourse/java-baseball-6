3e7472b595a88e6489422fc082195ea523d92c2e
Dreaming-J <kimjjong321@naver.com>
2023-10-23 15:33:35 +0900

    :white_check_mark: feat. 3개의 야구공 중 1개 판별

--- src/main/java/baseball/model/BallStatus.java
+++ src/main/java/baseball/model/BallStatus.java
@@ -2,4 +2,8 @@ package baseball.model;
 
 public enum BallStatus {
     BALL, STRIKE, NOTHING;
+
+    public boolean isNotNothing() {
+        return this != NOTHING;
+    }
 }
--- src/main/java/baseball/model/Balls.java
+++ src/main/java/baseball/model/Balls.java
@@ -7,19 +7,27 @@ public class Balls {
 
     private static final String WRONG_BALLS_NO = "[ERROR] 야구공은 서로 다른 3자리의 수입니다.";
     private static final int BALL_COUNT = 3;
-    private final List<Ball> answer;
+    private final List<Ball> answers;
 
-    public Balls(List<Integer> answer) {
-        validate(answer);
+    public Balls(List<Integer> answers) {
+        validate(answers);
 
-        this.answer = answer.stream()
-                .map(number -> new Ball(answer.indexOf(number), number))
+        this.answers = answers.stream()
+                .map(number -> new Ball(answers.indexOf(number), number))
                 .collect(Collectors.toList());
     }
 
-    private void validate(List<Integer> answer) {
-        if ((int) answer.stream().distinct().count() != BALL_COUNT) {
+    private void validate(List<Integer> answers) {
+        if (answers.stream().distinct().count() != BALL_COUNT) {
             throw new IllegalArgumentException(WRONG_BALLS_NO);
         }
+    }
+
+    public BallStatus play(Ball userBall) {
+        return answers.stream()
+                .map(answer -> answer.play(userBall))
+                .filter(BallStatus::isNotNothing)
+                .findFirst()
+                .orElse(BallStatus.NOTHING);
     }
 }
