382ced8eab9cc3d75ba383ee461518b11e00cca6
morenow <jjang9877@gmail.com>
2023-10-24 01:36:03 +0900

    feat: 게임과 출력에 관련된 유틸 클래스 추가
    게임 유틸 클래스 GameUtil, 출력 유틸 클래스 MessageUtil 추가

---
+++ src/main/java/baseball/util/GameUtil.java
@@ -0,0 +1,63 @@
+package baseball.util;
+
+import baseball.constant.Message;
+import baseball.constant.Number;
+import baseball.domain.BaseballCount;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 게임 관리를 위한 메서드를 가지는 유틸 클래스
+ */
+public class GameUtil {
+    public static BaseballCount compareBaseball(List<Integer> computer, List<Integer> userNum) {
+        int ball = 0, strike = 0;
+        for (int i = 0; i < Number.GAME_LENGTH; i++) {
+            if (userNum.get(i).equals(computer.get(i))) {
+                strike++;
+            } else if (computer.contains(userNum.get(i))) {
+                ball++;
+            }
+        }
+        return new BaseballCount(ball, strike);
+    }
+
+    public static boolean isGameRestart() {
+        System.out.println(Message.GAME_RESTART_MSG);
+        int restartNum = Integer.parseInt(Console.readLine());
+        if (restartNum == Number.RESTART_NUM) {
+            return true;
+        } else if (restartNum == Number.FINISH_NUM) {
+            return false;
+        } else {
+            // TODO : 다른 수가 입력될 경우 예외처리
+            return false;
+        }
+    }
+
+    public static List<Integer> inputNumber() {
+        String input = Console.readLine(); //길이, 서로 다른 세 숫자 예외처리 필요
+        if (input.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+        List<Integer> userNum = new ArrayList<>();
+        for (String s : input.split("")) {
+            userNum.add(Integer.parseInt(s));
+        }
+        return userNum;
+    }
+
+    public static List<Integer> initRandomNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
+}
---
+++ src/main/java/baseball/util/MessageUtil.java
@@ -0,0 +1,32 @@
+package baseball.util;
+
+import baseball.constant.Message;
+import baseball.domain.BaseballCount;
+
+/**
+ * 메시지 출력을 위한 메서드를 가지는 유틸 클래스
+ */
+public final class MessageUtil {
+
+    public static void printStartMsg() {
+        System.out.println(Message.GAME_START_MSG);
+    }
+
+    public static void printInputMsg() {
+        System.out.print(Message.INPUT_MSG);
+    }
+
+    public static void printBaseballCount(BaseballCount count) {
+        if (count.getBall() == 0 && count.getStrike() == 0) {
+            System.out.println(Message.NOTHING_MSG);
+        } else {
+            if (count.getBall() != 0) {
+                System.out.print(count.getBall() + Message.BALL_MSG + " ");
+            }
+            if (count.getStrike() != 0) {
+                System.out.print(count.getStrike() + Message.STRIKE_MSG);
+            }
+            System.out.println();
+        }
+    }
+}
