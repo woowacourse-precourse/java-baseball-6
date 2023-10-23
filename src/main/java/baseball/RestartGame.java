e3d64a6cc77f0b283ac15e5631df9920f991a06e
jisoo0609 <jisu20000609@gmail.com>
2023-10-23 16:50:44 +0900

    예외 테스트 통과

--- src/main/java/baseball/RestartGame.java
+++ src/main/java/baseball/RestartGame.java
@@ -7,19 +7,31 @@ import java.util.Scanner;
 
 public class RestartGame {
     public boolean restart() {
-        Scanner sc = new Scanner(System.in);
-
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        int reset = sc.nextInt();
+        try {
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+            String input = reader.readLine();
 
-        if (reset == 1) {
-            return true;
-        } else if (reset == 2) {
-            return false;
-        } else {
-            throw new IllegalArgumentException();
+            if (input != null && !input.isEmpty()) {
+                int restart = Integer.parseInt(input);
+
+                if (restart != 1 && restart != 2) {
+                    throw new IllegalArgumentException();
+                }
+
+                if (input.equals("1")) {
+                    return true;
+                } else if (input.equals("2")) {
+                    return false;
+                } else {
+                    throw new IllegalArgumentException();
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException();
         }
+        return false;
     }
 }
