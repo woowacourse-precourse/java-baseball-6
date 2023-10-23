4fc994a996eee306167e1b5dadc9c74768a10c5f
ikjo39 <chi4321@naver.com>
2023-10-23 23:09:25 +0900

    feat: 열거형 Message 생성

---
+++ src/main/java/baseball/Message.java
@@ -0,0 +1,20 @@
+package baseball;
+
+public enum Message {
+
+    GAME_START("숫자 야구 게임을 시작합니다."),
+    INSERT_GAME_INPUT("숫자를 입력해주세요 : "),
+    USER_WIN("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
+    NOTHING("낫싱"),
+    INSERT_END_INPUT("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+    private final String text;
+
+    Message(String text) {
+        this.text = text;
+    }
+
+    public String text() {
+        return text;
+    }
+}
