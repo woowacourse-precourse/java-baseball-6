4c4dab78e17e007973d319da50c1f844fb8af135
gpsrlin115 <lefd123@gmail.com>
2023-10-24 01:01:21 +0900

    숫자 입력 예외처리 후 표시

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,6 +13,18 @@ public class Application {
         if (input.length() != 3) {
             System.out.println("3자리 숫자를 입력해주세요.");
         }
-        
+
+        int[] numbers = new int[3];
+
+        // 문자열의 각 문자를 int로 변환하여 배열에 저장
+        for (int i = 0; i < 3; i++) {
+            // 문자를 숫자로 변환하고 배열에 저장
+            numbers[i] = Integer.parseInt(input.substring(i, i + 1));
+        }
+        System.out.println("Numbers : " + numbers[0] + numbers[1] + numbers[2]);
+
+
+
+
     }
 }
