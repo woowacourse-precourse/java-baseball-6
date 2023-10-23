ca95e00d8c08f663f91d8c3af038e8032792519b
eom-tae-in <eti0728@daum.net>
2023-10-24 01:30:35 +0900

    test: StateTest의 테스트 데이터 추가 및 테스트 변수명 수정

--- src/test/java/baseball/model/StateTest.java
+++ src/test/java/baseball/model/StateTest.java
@@ -7,21 +7,21 @@ import baseball.exception.state.NotMatchGameStateException;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 @DisplayNameGeneration(ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
 class StateTest {
 
-    @Test
-    void 재시작_입력이_들어오면_true를_반환한다() {
-        // given
-        State restartState = State.PLAY;
-
+    @CsvSource({"END, false", "PLAY, true"})
+    @ParameterizedTest
+    void 현재_게임_플레이_중이라면_true를_반환정한다(State state, boolean expected) {
         // when
-        final boolean result = State.isMoreGame(restartState);
+        final boolean result = State.isMoreGame(state);
 
         // then
-        assertThat(result).isTrue();
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
