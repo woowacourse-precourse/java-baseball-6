98ce176e7ed1a420937c0671057a3dcde57b2231
MrKeeplearning <jaeseok1115@gmail.com>
2023-10-24 00:11:21 +0900

    feat: 기능 별 메서드 구현
    - feat: 사용자의 입력이 주어진 조건에 맞는 입력인지 검사하는
      isValidateNumber 메서드 구현
    - feat: 사용자의 입력에 중복된 문자가 포함되어 있는지 검사하는
      isDuplicate 메서드 구현
    - feat: isValidateNumber 메서드에 맞게 getUserInput 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,21 +1,24 @@
 package baseball;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Console.*;
 import static camp.nextstep.edu.missionutils.Randoms.*;
+import static java.lang.Integer.*;
 
 public class Application {
     public static void main(String[] args) {
+
         startGame();
 
         int computerNumber = generateComputerNumber();
         System.out.println("computerNumber = " + computerNumber);
 
+        // userInput이 0인 경우 프로그램을 종료하도록 수정해야 한다.
         int userInput = getUserInput();
         System.out.println("userInput = " + userInput);
-
 
     }
 
@@ -33,24 +36,50 @@ public class Application {
                 sb.append(randomNumber);
             }
         }
-        return Integer.parseInt(sb.toString());
+        return parseInt(sb.toString());
     }
 
     private static int getUserInput() {
-        // 사용자로부터 Console 로 입력 받기
-        // 입력 받은 문자열을 검증 -> isValidateNumber
-        // StringTokenizer로 한 자씩 분리해서 배열 형태로 만들 필요가 있음.
+        int validateNumber = 0;
 
         System.out.print("숫자를 입력해주세요 : ");
-        int userInput = Integer.parseInt(readLine());
+        String userInputString = readLine();
 
-        return userInput;
+        try {
+            validateNumber = isValidateNumber(userInputString);
+        } catch (IllegalArgumentException e) {
+            String message = e.getMessage();
+            System.out.println(message);
+        }
+        return validateNumber;
     }
 
-    private static int isValidateNumber(int num) {
-        // 사용자 입력 검증
-        // 3자리 숫자가 아닌 경우 exception 발생
-        return 0;
+    private static int isValidateNumber(String userInput){
+        int inputNumber;
+
+        // Integer.parseInt가 되지 않는 경우
+        try {
+            inputNumber = parseInt(userInput);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("정수가 아닌 문자열입니다.");
+        }
+
+        // 3자리 숫자가 아닌 경우 예외 발생
+        if (userInput.length() != 3) {
+            throw new IllegalArgumentException("3자리 숫자가 아닙니다.");
+        }
+
+        // 사용자의 입력에 0이 포함된 경우 예외 발생
+        if (userInput.contains("0")) {
+            throw new IllegalArgumentException("0이 포함되어 있으면 안됩니다.");
+        }
+
+        // 사용자의 입력에 중복된 숫자가 포함된 경우 예외 발생
+        if (isDuplicate(userInput)) {
+            throw new IllegalArgumentException("중복된 숫자가 포함되어 있으면 안됩니다.");
+        }
+        
+        return inputNumber;
     }
 
     private static void printHint(int num) {
@@ -67,4 +96,18 @@ public class Application {
         return true;
     }
 
+    /**
+     * 사용자가 입력한 문자열 안에 중복된 숫자가 있는지 검사한다.
+     * set은 중복을 허용하지 않기 때문에 중복된 숫자가 있다면,
+     * 처음 입력받은 userInput과 charSet의 길이가 다를 것이다.
+     * @param userInput
+     * @return charSet.size() != userInput.length()
+     */
+    private static boolean isDuplicate(String userInput) {
+        HashSet<Character> charSet = new HashSet<>();
+        for (int i = 0; i < userInput.length(); i++) {
+            charSet.add(userInput.charAt(i));
+        }
+        return charSet.size() != userInput.length();    // 길이가 달라야 중복인 것.
+    }
 }
