fd3fba709da0ef7f2ab446f647d7059b2da54f73
Ukj0ng <xb253q@gmail.com>
2023-10-23 17:34:50 +0900

    feat: View 게임 결과 출력 및 인트로 구문 추가

--- src/main/java/baseball/view/GameView.java
+++ src/main/java/baseball/view/GameView.java
@@ -3,9 +3,10 @@ package baseball.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class GameView {
-    private final String ANSWERMESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+    private final String ANSWERMESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     public String getPlayerInput() {
+        System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
         validatePlayNumber(input);
 
@@ -13,12 +14,16 @@ public class GameView {
     }
 
     public int gameContinue() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String input = Console.readLine();
         validateGameContinue(input);
 
         return Integer.parseInt(input);
     }
 
+    public void showIntro() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
     public void showResult(int strike, int ball) {
         if(strike > 0 && ball == 0) {
             System.out.println(strike + "스트라이크");
