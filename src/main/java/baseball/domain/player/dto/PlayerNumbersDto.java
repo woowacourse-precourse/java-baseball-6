2130d4a1aa31206e6ca676b74a45ed33a51fd51e
penrose15 <seongju99@naver.com>
2023-10-23 22:53:31 +0900

    Feat: input 추가 및 Player 입력 받는 PlayerNumbersDto 추가

---
+++ src/main/java/baseball/domain/player/dto/PlayerNumbersDto.java
@@ -0,0 +1,42 @@
+package baseball.domain.player.dto;
+
+import baseball.global.exception.CustomObjects;
+
+import java.util.Arrays;
+import java.util.List;
+
+public record PlayerNumbersDto(String playerNumbers) {
+
+    public PlayerNumbersDto {
+        CustomObjects.requireNonNull(playerNumbers, "input cannot be null");
+        if (playerNumbers.equals("")) throw new IllegalArgumentException("숫자 3자리로 입력하세요");
+        validateInputNumbers(playerNumbers);
+    }
+
+    public static PlayerNumbersDto of(String playerNumbers) {
+        return new PlayerNumbersDto(playerNumbers);
+    }
+
+    public void validateInputNumbers(String number) {
+        List<String> inputNumbers = Arrays.stream(number.split("")).toList();
+        validateInputNumbersSize(inputNumbers);
+        validateInputIsNumbers(inputNumbers);
+    }
+
+    private void validateInputNumbersSize(List<String> inputNumbers) {
+        if(inputNumbers.size() != 3) {
+            throw new IllegalArgumentException("숫자는 서로 다른 세자리 수 입력");
+        }
+    }
+
+    private void validateInputIsNumbers(List<String> inputNumbers) {
+        for (String number : inputNumbers) {
+            try {
+                Integer.parseInt(number);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException(e);
+            }
+        }
+    }
+
+}
---
+++ src/main/java/baseball/global/view/input/InputView.java
@@ -0,0 +1,12 @@
+package baseball.global.view.input;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String input() {
+        return Console.readLine();
+    }
+
+
+}
