e5069234eae15bea2311c41cdfca96a26896cdd8
이정우 <acg6138@naver.com>
2023-10-23 10:31:37 +0900

    feat: Computer 클래스 구현(상대 역할인 컴퓨터는 숫자를 생성하고 결과를 반환할 수 있다)

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,34 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static baseball.Baseball.MAX_NUMBER;
+import static baseball.Baseball.MIN_NUMBER;
+import static baseball.Baseballs.BASEBALLS_SIZE;
+
+public class Computer {
+    private final Baseballs baseballs;
+
+    public Computer(Baseballs baseballs) {
+        this.baseballs = createBaseballs();
+    }
+
+    public Baseballs createBaseballs() {
+        try {
+            List<Baseball> baseballs = new ArrayList<>();
+            for (int i = 0; i < BASEBALLS_SIZE; i++) baseballs.add(new Baseball(Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER)));
+            return new Baseballs(baseballs);
+        } catch (IllegalArgumentException e) {
+            return createBaseballs();
+        }
+    }
+
+    public String getResult(Baseballs baseballs) {
+        Result result = new Result(this.baseballs.countStrike(baseballs), this.baseballs.countBall(baseballs));
+        return result.getResult();
+    }
+
+}
