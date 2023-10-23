5a462540b6054df665da9079e0b5a3ab530ae074
jooys130 <wndustn130@gmail.com>
2023-10-23 10:44:06 +0900

    feat: 사용자에게 1에서 9까지 서로 다른 3개의 숫자를 입력 받는다

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,13 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class Application {
+
+    private final static int NUMBER_SIZE = 3;
+    private final static int MIN_RANGE_NUM = 1;
+    private final static int MAX_RANGE_NUM = 9;
 
     public static void main(String[] args) {
         PrintMessage printMessage = new PrintMessage();
@@ -10,5 +15,46 @@ public class Application {
 
         AnswerNumberGenerator answerNumberGenerator = new AnswerNumberGenerator();
         List<Integer> answerNumberList = answerNumberGenerator.getAnswerNumberList();
+
+        printMessage.printReadNumberMessage();
+        String userInputNumber = Console.readLine();
+        try {
+            validateUserInputNumber(userInputNumber);
+        } catch (IllegalArgumentException e) {
+            System.out.println("IllegalArgumentException: " + e.getMessage());
+        }
+
+    }
+
+    public static void validateUserInputNumber(String userInputNumber)
+        throws IllegalArgumentException {
+        if (userInputNumber.length() != NUMBER_SIZE) {
+            throw new IllegalArgumentException("세자리 수를 입력해주세요.");
+        }
+        if (!ensureRange(userInputNumber, MIN_RANGE_NUM, MAX_RANGE_NUM)) {
+            throw new IllegalArgumentException("1과 9 사이의 숫자를 입력해주세요.");
+        }
+        if (existsDuplicateNumber(userInputNumber)) {
+            throw new IllegalArgumentException("중복이 없는 숫자를 입력해주세요.");
+        }
+    }
+
+    public static boolean ensureRange(String userInputNumber, int min, int max) {
+        for (char digit : userInputNumber.toCharArray()) {
+            int numericValue = Character.getNumericValue(digit);
+            if (numericValue < min || numericValue > max) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean existsDuplicateNumber(String userInputNumber) {
+        for (char digit : userInputNumber.toCharArray()) {
+            if (userInputNumber.chars().filter(c -> c == digit).count() != 1) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/main/java/baseball/PrintMessage.java
+++ src/main/java/baseball/PrintMessage.java
@@ -6,4 +6,8 @@ public class PrintMessage {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
+    public void printReadNumberMessage() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
 }
