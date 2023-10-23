7e3bf9da71bef3b1da45c9d382be95a9fec325a7
win9 <krrr883@gmail.com>
2023-10-24 03:58:08 +0900

    [Feat]: 컴퓨터 랜덤 생성 함수 구현
    1과 9사이의 3가지 숫자를 무작위로 생성하는 메소드를 구현

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -8,4 +8,16 @@ import java.util.Arrays;
 public class Computer {
     private int[] numbers = new int[3];
 
+    public void generateNumber(){
+        do {
+            numbers = pickNumbers();
+        } while (Util.isDuplicated(numbers));
+
+    }
+
+    public int[] pickNumbers(){
+        return Arrays.stream(new int[3])
+                .map(num -> Randoms.pickNumberInRange(1, 9))
+                .toArray();
+    }
 }
