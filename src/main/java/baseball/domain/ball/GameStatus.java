9dfb06c2f576b4c4caa0bcfbd156813c4a562cf9
mainfn <_@mainfn.dev>
2023-10-23 23:02:20 +0900

    refact: GameStatus enum constant 정적 팩토리 메소드 일반화

--- src/main/java/baseball/domain/ball/GameStatus.java
+++ src/main/java/baseball/domain/ball/GameStatus.java
@@ -1,20 +1,27 @@
 package baseball.domain.ball;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public enum GameStatus {
     UNKNOWN("_"),
     REPLAY("1"),
     EXIT("2");
 
+    private static final Map<String, GameStatus> enumMap =
+            Arrays.stream(values())
+                    .collect(Collectors.toMap(
+                            gameStatus -> gameStatus.input,
+                            gameStatus -> gameStatus
+                    ));
+    private final String input;
+
     GameStatus(final String input) {
+        this.input = input;
     }
 
     public static GameStatus of(final String input) {
-        if ("1".equals(input)) {
-            return REPLAY;
-        }
-        if ("2".equals(input)) {
-            return EXIT;
-        }
-        return UNKNOWN;
+        return enumMap.getOrDefault(input, UNKNOWN);
     }
 }
