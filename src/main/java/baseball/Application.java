f9ac6a18b8659bee6f1eac30fdce18c3de4ac8ea
olsohee <dlthgml0108@daum.net>
2023-10-23 15:31:40 +0900

    refactor: UI 로직을 View 패키지로 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,13 +1,13 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
-
-import java.util.List;
+import baseball.view.InputView;
+import baseball.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
 
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        OutputView.start();
+
         boolean exitFlag = false;
 
         while(!exitFlag) {
@@ -15,23 +15,10 @@ public class Application {
             Game game = new Game();
             game.play();
 
-            exitFlag = askExit();
-        }
-        System.out.println("게임을 종료합니다.");
-    }
-
-    public static boolean askExit() {
-
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-
-        String userInput = Console.readLine();
-
-        Util.validateExitCode(userInput);
-
-        if(userInput.equals("2")) {
-            return true;
+            OutputView.askExit();
+            exitFlag = InputView.askExit();
         }
 
-        return false;
+        OutputView.exit();
     }
 }
