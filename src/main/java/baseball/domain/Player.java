2214907ffdb49654decb974ecdad1144806a83c4
nylonmusk <2912rla@gmail.com>
2023-10-24 02:45:47 +0900

    refactor : 불필요한 조건문 제거, 유효성 검사 메소드 제거

--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -1,47 +1,25 @@
 package baseball.domain;
 
+
 import baseball.constant.Regex;
-import baseball.view.InputView;
-import baseball.view.OutputView;
+import baseball.util.InputValidator;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Player {
 
-    private final InputView inputView;
-    private final OutputView outputView;
-
-    public Player() {
-        this.inputView = new InputView();
-        this.outputView = new OutputView();
+    public List<Integer> getInputNumber(String input) {
+        if (InputValidator.isValid(input)) {
+            return convertStringToList(input);
+        }
+        throw new IllegalArgumentException();
     }
 
-    public List<Integer> getInputNumber() {
-        List<Integer> inputNumbers;
-        while (true) {
-            outputView.requestNumber();
-            String input = inputView.receiveInputNumber();
-            if (isValid(input)) {
-                inputNumbers = convertStringToList(input);
-                break;
-            }
+    public int receiveRetryOption(String input) {
+        if (Regex.ONE_OR_TWO.matches(input)) {
+            return Integer.parseInt(input);
         }
-        return inputNumbers;
-    }
-
-    public int receiveRetryOption() {
-        outputView.restartOrQuit();
-        int option;
-        while (true) {
-            String input = inputView.receiveInputNumber();
-            if (Regex.ONE_OR_TWO.matches(input)) {
-                option = Integer.parseInt(input);
-                break;
-            }
-        }
-        return option;
+        throw new IllegalArgumentException();
     }
 
     private List<Integer> convertStringToList(String input) {
@@ -50,22 +28,5 @@ public class Player {
             inputNumbers.add(Character.getNumericValue(ch));
         }
         return inputNumbers;
-    }
-
-    private boolean isValid(String input) {
-        if (!Regex.ONE_TO_NINE.matches(input) || !hasNoDuplicateDigits(input)) {
-//            outputView.printMessage("비정상적인 입력입니다. 게임이 종료됩니다.");
-            throw new IllegalArgumentException();
-        }
-        return true;
-    }
-
-
-    private boolean hasNoDuplicateDigits(String input) {
-        Set<Character> uniqueDigits = new HashSet<>();
-        for (char digit : input.toCharArray()) {
-            uniqueDigits.add(digit);
-        }
-        return uniqueDigits.size() == input.length();
     }
 }
