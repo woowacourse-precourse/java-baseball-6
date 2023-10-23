46e10878a14c70e80b53d3005f58ff42b496349b
wonseok2877 <wonseok2877@gmail.com>
2023-10-23 23:51:59 +0900

    feat: 정답 3자리수 생성 메서드

--- src/main/java/baseball/game/Game.java
+++ src/main/java/baseball/game/Game.java
@@ -1,7 +1,28 @@
 package baseball.game;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
     public void start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+
+        // computer 초기화
+        List<Integer> computer = initializeComputer();
+
+        // 숫자 야구 게임
+
+    }
+
+    private List<Integer> initializeComputer() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 }
