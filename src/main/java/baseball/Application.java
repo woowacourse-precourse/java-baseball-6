24c45871240e9a66d216ec17a1f949aee2f0575f
gyu_hyeok <jjookki33@gmail.com>
2023-10-23 17:36:56 +0900

    fix/숫자 생성 및 사용자 입력 기능 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,14 +3,17 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.io.IOException;
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
 //      List<Integer> computer = createComputerNumber();
 //      System.out.println(computer);
+        System.out.println("서로다른 숫자를 입력해 주세요 : ");
+        List<String> playerNumber = playerInputNumber();
+//      validatePlayerNumber(playerNumber);
     }
 
     private static final int MIN_NUMBER = 1;
@@ -18,19 +21,22 @@ public class Application {
     private static final int BASEBALL_SIZE = 3;
 
     //랜덤 숫자 생성 기능
-    private static List<Integer> createComputerNumber(){
-        List<Integer> numbers = new ArrayList<>();
+    private static List<String> createComputerNumber(){
+        List<String> numbers = new ArrayList<>();
         while(numbers.size() < BASEBALL_SIZE){
-            int randomNumber = Randoms.pickNumberInRange(MIN_NUMBER,MAX_NUMBER);
+            String randomNumber = String.valueOf(Randoms.pickNumberInRange(MIN_NUMBER,MAX_NUMBER));
             if(!numbers.contains(randomNumber))
                 numbers.add(randomNumber);
         }
         return numbers;
     }
 
-    private static List<Integer> playerInputNum(){
-        return Arrays.stream(Console.readLine().split(""))
-                .map(Integer::parseInt)
-                .toList();
+    private static List<String> playerInputNumber(){
+        return Arrays.asList(
+                Console.readLine()
+                        .split("")
+        );
     }
+
+
 }
