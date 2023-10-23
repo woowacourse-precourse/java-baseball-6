be86e49a4b572b9041d8d4210651edbea329a06a
jaehee <y2hjjh@naver.com>
2023-10-24 01:58:39 +0900

    feat: 사용자 입력 숫자 받기 메서드 작성

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,5 +1,13 @@
 package baseball.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    private static final String PLAYER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+
+    public String getPlayerInput() {
+        System.out.print(PLAYER_INPUT_MESSAGE);
+        return Console.readLine();
+    }
 
 }
