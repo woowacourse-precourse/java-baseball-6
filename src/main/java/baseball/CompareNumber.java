e8736eb26dd59128693270d08c0e86c05b79b68a
suhyun__ <thebbbb1206@gmail.com>
2023-10-24 01:25:18 +0900

    feat: 재시작 고려하여 볼, 스트라이크 개수 초기화 기능 추가

--- src/main/java/baseball/CompareNumber.java
+++ src/main/java/baseball/CompareNumber.java
@@ -6,6 +6,11 @@ public class CompareNumber {
     int ball;
     int strike;
 
+    public void initCount() {
+        ball = 0;
+        strike = 0;
+    }
+
     public void countBall(List<String> user, List<String> computer) {
         for (int i = 0; i < user.size(); i++) {
             if (computer.contains(user.get(i))) {
