0b60b0901203cc69896f4a6df1fab5c09cce532d
jifrozen0110 <answldjs1836@gmail.com>
2023-10-24 00:52:32 +0900

    refactor : BaseballSystemConst 상수 관리

---
+++ src/main/java/baseball/domain/BaseballSystemConst.java
@@ -0,0 +1,8 @@
+package baseball.domain;
+
+public class BaseballSystemConst {
+    public static final int MAX_BALL_SIZE=3;
+    public static final int MIN_DIGIT_BALL=1;
+    public static final int MAX_DIGIT_BALL=9;
+
+}
--- src/main/java/baseball/utils/NumberGenerator.java
+++ src/main/java/baseball/utils/NumberGenerator.java
@@ -1,5 +1,6 @@
 package baseball.utils;
 
+import baseball.domain.BaseballSystemConst;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,8 +8,8 @@ import java.util.List;
 public class NumberGenerator {
     public static List<Integer> makeRandomNumber() {
         List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+        while (computer.size() < BaseballSystemConst.MAX_BALL_SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(BaseballSystemConst.MIN_DIGIT_BALL, BaseballSystemConst.MAX_DIGIT_BALL);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
