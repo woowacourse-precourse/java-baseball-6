63f7977ec2eb7b4e54afa237e2d4b7e0ffa42fbf
손지현_회원/결제개발팀 <hdg12jh@interpark.com>
2023-10-23 21:28:31 +0900

    fetch Game Constant 생성

---
+++ src/main/java/baseball/constant/Game.java
@@ -0,0 +1,12 @@
+package baseball.constant;
+
+public final class Game {
+    public static final int STRIKE_COUNT = 3;
+    public static final String REGEX = "[1-9]+";
+    public static final String RETRY = "1";
+    public static final String FINISH = "2";
+    public static final String[] GAME_FLAG_LIST = {RETRY, FINISH};
+    public final static String BALL = "볼";
+    public final static String STRIKE = "스트라이크";
+    public final static String NOTHING = "낫싱";
+}
