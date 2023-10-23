a95415d1f9b6ce62228cb3c932086cc0c3e3e332
seung-won-kim <hesjs5@skuniv.ac.kr>
2023-10-23 23:00:06 +0900

    Feat: 사용자로부터 1 또는 2를 입력받는 기능 구현

--- src/main/java/baseball/Output.java
+++ src/main/java/baseball/Output.java
@@ -5,4 +5,8 @@ public class Output {
     public static void printUserNumbersMessage() {
         System.out.print("숫자를 입력해주세요 : ");
     }
+
+    public static void printRestartOrExitMessage() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
