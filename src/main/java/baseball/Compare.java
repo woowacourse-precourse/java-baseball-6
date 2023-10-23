0c2bf0c26fea27bb5129627fa41053b2a414379f
junu8529 <cairo8529@gmail.com>
2023-10-24 02:43:13 +0900

    feat: 숫자 비교 기능 구현

---
+++ src/main/java/baseball/Compare.java
@@ -0,0 +1,50 @@
+package baseball;
+
+import java.util.List;
+
+public class Compare {
+    int strike;
+    int ball;
+
+    public int CountStrike(
+            List<Integer> player,
+            List<Integer> computer
+    ) {
+        strike = 0;
+        for (int i=0;i<3;i++) {
+            if (player.get(i).equals(computer.get(i))) {
+                strike++;
+            }
+        }
+        return strike;
+    }
+
+    public int CountBall(
+            List<Integer> player,
+            List<Integer> computer
+    ) {
+        ball = 0;
+        for (int i=0;i<3;i++) {
+            if (computer.contains(player.get(i)) & player.get(i) == computer.indexOf(i)) {
+                ball++;
+            }
+        }
+        return ball;
+    }
+
+    public String TotalCount(
+            List<Integer> player,
+            List<Integer> computer
+    ) {
+        strike = CountStrike(player,computer);
+        ball = CountBall(player,computer);
+        if (strike == 0 & ball == 0) {
+            return "낫싱";
+        } else if (strike==0) {
+            return ball+"볼";
+        } else if (ball==0) {
+            return strike+"스트라이크";
+        }
+        return ball+"볼 "+strike+"스트라이크";
+    }
+}
