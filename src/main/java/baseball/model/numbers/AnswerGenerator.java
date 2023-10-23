f472019409e70241e6d45398a9a4dc7683463a1e
haiseong <wjdgotjd9908@gmail.com>
2023-10-23 15:17:10 +0900

    refactor: getRandomValueNotInList: 불필요한 인자 제거

--- src/main/java/baseball/model/numbers/AnswerGenerator.java
+++ src/main/java/baseball/model/numbers/AnswerGenerator.java
@@ -3,6 +3,7 @@ package baseball.model.numbers;
 import static baseball.model.numbers.GameNumbers.GAME_NUMBERS_SIZE;
 
 import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,17 +14,16 @@ public class AnswerGenerator {
     public GameNumbers generateAnswer() {
         List<Integer> list = new ArrayList<>(GAME_NUMBERS_SIZE);
         for (int i = 0; i < GAME_NUMBERS_SIZE; i++) {
-            int randomNumber = getRandomValueNotInList(list, START_INCLUSIVE, END_INCLUSIVE);
+            int randomNumber = getRandomValueNotInList(list);
             list.add(randomNumber);
         }
         return new GameNumbers(list);
     }
 
-    private static int getRandomValueNotInList(List<Integer> list, final int startInclusive,
-            final int endInclusive) {
-        int randomNumber = Randoms.pickNumberInRange(startInclusive, endInclusive);
+    private static int getRandomValueNotInList(List<Integer> list) {
+        int randomNumber = Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
         while (list.contains(randomNumber)) {
-            randomNumber = Randoms.pickNumberInRange(startInclusive, endInclusive);
+            randomNumber = Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
         }
         return randomNumber;
     }
