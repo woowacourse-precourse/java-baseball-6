29dde44777f4b36bbb4335a6256e440ac459003f
Nara Park <skfk6352@naver.com>
2023-10-24 02:53:35 +0900

    <feat>(<gamemaincontroller>): create method in gamemaincontroller

--- src/main/java/baseball/controller/GameMainController.java
+++ src/main/java/baseball/controller/GameMainController.java
@@ -1,5 +1,38 @@
 package baseball.controller;
 
+import baseball.service.GameService;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class GameMainController {
 
+    private static final String THREESTRIKE = "3스트라이크";
+
+    public GameMainController() {
+        InputView.startBaseballGame();
+    }
+
+    public static void startGame() {
+        boolean gameEnd = false;
+        while (!gameEnd) {
+            GameService.setGame();
+            playGame();
+            gameEnd = endGame();
+        }
+    }
+
+    public static void playGame() {
+        while (!GameService.playGame().equals(THREESTRIKE)) {
+            String result = GameService.playGame();
+            OutputView.printStrikeOrBall(result);
+        }
+
+        OutputView.printStrikeOrBall(THREESTRIKE);
+        OutputView.printCorrectAnswer();
+    }
+
+    public static boolean endGame() {
+        InputView.requestGameState();
+        return GameService.endGame();
+    }
 }
