763f15ee607a27119432c3b43ed8e8613698445f
min050410 <min050410@gmail.com>
2023-10-23 13:33:49 +0900

    feat: ComputerBalls 일급 컬렉션 사용

---
+++ src/main/java/baseball/collections/ComputerBalls.java
@@ -0,0 +1,13 @@
+package baseball.collections;
+
+import java.util.HashMap;
+
+public class ComputerBalls {
+
+    private HashMap<Integer, Integer> computerBalls;
+
+    public ComputerBalls(HashMap<Integer, Integer> computerBalls) {
+        this.computerBalls = computerBalls;
+    }
+
+}
