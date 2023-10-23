54e698a35eac609a542ae5049d5b4fc504d57594
Jinwoo Lee <binaryWooo@gmail.com>
2023-10-23 22:18:28 +0900

    refactor: Player 클래스 정의 및 기능 구현

---
+++ src/main/java/baseball/Player.java
@@ -0,0 +1,22 @@
+package baseball;
+
+import static camp.nextstep.edu.missionutils.Console.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+	private List<Integer> numberList = new ArrayList<>();
+
+	public void init(String str) {
+		System.out.print("숫자를 입력해주세요 : ");
+		String[] input = readLine().split("");
+		for (String s : input) {
+			numberList.add(Integer.parseInt(s));
+		}
+	}
+
+	public List<Integer> getNumberList() {
+		return numberList;
+	}
+}
