dec0d47ec3dbfa85d24fb73faf976f022dce06dc
jaehee <y2hjjh@naver.com>
2023-10-24 01:56:33 +0900

    refactor: 자릿수 상수 처리

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,9 +1,20 @@
 package baseball.view;
 
+import baseball.model.InputNumber;
+
 public class OutputView {
+    private static final int NUMBER_OF_DIGITS = 3;
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
 
     public String printStart() {
         return GAME_START_MESSAGE;
+    }
+
+    public String printHint(String HintMessage) {
+        return HintMessage;
+    }
+
+    public String printSuccess() {
+        return NUMBER_OF_DIGITS + "스트라이크\n" + NUMBER_OF_DIGITS + "개의 숫자를 모두 맞히셨습니다! 게임 종료";
     }
 }
