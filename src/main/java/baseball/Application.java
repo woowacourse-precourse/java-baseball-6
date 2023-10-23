ecda426b112d4a5ff12e881fde2ac259bc0fda1e
sosojean <thwlsdl0989@gmail.com>
2023-10-23 14:39:21 +0900

    메인 함수 기능구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,29 @@
 package baseball;
-
+import java.util.List;
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        boolean playerWantToPlay = true;
+
+        while(playerWantToPlay) {
+
+            List<Integer> computer;
+            List<Integer> player;
+            BaseballFunction baseballFunction = new BaseballFunction();
+            Score score = new Score(0, 0);
+
+            System.out.println("숫자 야구 게임을 시작합니다.");
+
+            computer = baseballFunction.genNum();
+
+            while (score.strike != 3){
+                player = baseballFunction.getNum();
+                score = baseballFunction.compare(computer, player);
+                baseballFunction.printScore(score);
+            }
+
+            playerWantToPlay = baseballFunction.threeStrike();
+
+        }
     }
 }
