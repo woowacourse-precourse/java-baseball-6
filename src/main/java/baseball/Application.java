d8793c9424b116ddeb6ff2d3faf41667e0e0fad4
parkheeddong <phj7214go@gmail.com>
2023-10-23 17:17:23 +0900

    [ feat ] 사용자 입력값을 검사하고, 잘못된 경우 Exception 발생 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,16 @@
 package baseball;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+        // 1. 정답 생성
         CreatingAnswer answerMachine = new CreatingAnswer();
         int randomAnswer = answerMachine.getRandomNumber();
         System.out.println(randomAnswer);
+
+        // 2. 사용자 입력 받기
+        String input = Console.readLine();
+        CheckingAnswer checkMachine = new CheckingAnswer();
+        checkMachine.CheckAll(input);
     }
 }
---
+++ src/main/java/baseball/CheckingAnswer.java
@@ -0,0 +1,34 @@
+package baseball;
+
+public class CheckingAnswer {
+
+    /*
+    2️⃣ 잘못된 값을 검사하는 기능
+    - 사용자의 입력값이 잘못된 경우, Exception을 발생시킨다.
+     */
+
+    public void CheckAll(String input) throws IllegalArgumentException {
+        if ((CheckRange(input) && CheckDifference(input)) == false) {
+            throw new IllegalArgumentException();
+        }
+    }
+    public boolean CheckRange (String input) {
+        Integer int_input = Integer.parseInt(input);
+        if (int_input > 111 && int_input < 999) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean CheckDifference (String input) {
+        char firstNum = input.charAt(0);
+        char secondNum = input.charAt(1);
+        char thirdNum= input.charAt(2);
+        if (firstNum != secondNum && secondNum != thirdNum && thirdNum != firstNum) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
