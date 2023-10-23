1114813f2bf0f6c2cc2582dbf70988ccc7d9291c
wochae <officialwochae@gmail.com>
2023-10-23 18:38:30 +0900

    [REFECTOR] : print output message methodify, finish

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -19,7 +19,9 @@ public class Referee {
   public void startGameMessage() {
     System.out.println("숫자 야구 게임을 시작합니다.");
   }
-
+  private void announceGameFinish() {
+    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+  }
   public void inputGameNumbersMessage() {
     System.out.print("숫자를 입력해주세요 : ");
   }
@@ -51,7 +53,7 @@ public class Referee {
       System.out.println(ball + "볼 " + strike + "스트라이크");
     }
     if (strike == 3) {
-      System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      announceGameFinish();
       return 0;
     }
     return 1;
