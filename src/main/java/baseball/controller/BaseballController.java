625796f227a2e95b29afa4b2d3455698704f380a
hong <kevin0928@naver.com>
2023-10-23 13:01:46 +0900

    refactor: inputView를 통해서 입력 받는 코드를 BaseballService에서 BaseballController로 이전

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -2,12 +2,14 @@ package baseball.controller;
 
 import baseball.service.BaseballService;
 import baseball.service.ComputerService;
+import baseball.service.ValidationService;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 import java.util.List;
 
 public class BaseballController {
     private final BaseballService baseballService = new BaseballService();
+    private final ValidationService validationService = new ValidationService();
     private final ComputerService computerService = new ComputerService();
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
@@ -19,6 +21,11 @@ public class BaseballController {
 
     public void gameStart() {
         List<Integer> randomNumber = computerService.createRandomNumber();
-        baseballService.baseballGameStart(randomNumber);
+        Boolean isDifferent = true;
+        while (isDifferent) {
+            String input = inputView.getInput();
+            validationService.isValidNumber(input);
+
+        }
     }
 }
--- src/main/java/baseball/service/BaseballService.java
+++ src/main/java/baseball/service/BaseballService.java
@@ -1,19 +1,4 @@
 package baseball.service;
 
-import baseball.view.InputView;
-import java.util.List;
-
 public class BaseballService {
-    private final InputView inputView = new InputView();
-    private final ValidationService validationService = new ValidationService();
-
-    public void baseballGameStart(List<Integer> integerList) {
-        boolean isDifferent = true;
-        while (isDifferent) {
-            String input = inputView.getInput();
-            validationService.isValidNumber(input);
-        }
-    }
-
-
 }
