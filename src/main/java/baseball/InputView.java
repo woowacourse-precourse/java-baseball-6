e470f1c9cefe43e34a23b65635160bdb8873fe20
bamm2 <xxknu88@naver.com>
2023-10-24 01:09:06 +0900

    Refactor: Input 클래스 내 불명확한 책임으로 InputView로 책임을 명확히 구분하기 위해 수정

--- src/main/java/baseball/InputView.java
+++ src/main/java/baseball/InputView.java
@@ -9,23 +9,27 @@ public class Input {
 
     private List<Integer> inputList;
     private InputManager inputManager;
-    private static final int INPUTSIZE = 3;
+    private static final int ORIGINCOUNT = 3;
 
     public Input() {
         System.out.print("숫자를 입력해주세요 : " );
     }
 
-    protected List<Integer> getUserInput(){
-        String input = Console.readLine();
-        this.inputManager = new InputManager(input);
-        inputManager.validateInput();
-        inputList = convertList(input);
-        return this.inputList;
+    protected void printGameStartMessage(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
+//    protected List<Integer> getUserInput(){
+//        String input = Console.readLine();
+////        this.inputManager = new InputManager(input);
+////        inputManager.validateInput();
+////        inputList = convertList(input);
+////        return this.inputList;
+//    }
 
     public List<Integer> convertList(String input) {
         List<Integer> userInput = new ArrayList<>();
-        for (int i = 0; i < INPUTSIZE; i++) {
+        for (int i = 0; i < ORIGINCOUNT; i++) {
             userInput.add(input.charAt(i) - '0');
         }
         return userInput;
