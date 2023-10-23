e37fb5116b330c0d9e9f4c6a22056bacf94f5547
sinminseok12 <sinminseok12@naver.com>
2023-10-24 02:05:37 +0900

    feat : 3스트라이가 될때까지 게임 진행되는 기능 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -3,13 +3,24 @@ package baseball;
 public class Game {
     private Computer computer;
     private User user;
+    private Result result;
 
     public void run() {
         setGame();
+        playGame();
     }
 
     private void setGame() {
         computer = new Computer();
         user = new User();
+        result = new Result();
+    }
+
+    private void playGame() {
+        while (!result.isThreeStrike()) {
+            user.inputNumbers();
+            result.checkResult(user.getNumbers(), computer.getNumbers());
+            result.printResult();
+        }
     }
 }
