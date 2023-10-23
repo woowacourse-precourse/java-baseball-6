60054036cd14faa5780de583ead64bce99f4a04f
정찬영 <jcy0342@gmail.com>
2023-10-23 16:29:18 +0900

    feat(Application): GameManager를 생성하여 게임 실행
    - GameManager를 실행하기 위해 필요한 객체를 외부에서 주입하기 위해 main 메서드에서 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+
+import baseball.controller.GameManager;
+import baseball.model.Computer;
+import baseball.model.Player;
+import baseball.service.NumberBaseballService;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        Computer computer = new Computer();
+        Player player = new Player();
+
+        NumberBaseballService numberBaseballService = new NumberBaseballService(computer, player);
+        GameManager gm = new GameManager(inputView, outputView, numberBaseballService);
+
+        gm.startGame();
     }
 }
