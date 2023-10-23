ce843dfa714f89ed95f68daec5e2e50668b22d68
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-10-24 00:28:53 +0900

    fix: 난수 발생 범위 오류 수정

--- src/main/java/baseball/utils/RandomGenerateNumRange.java
+++ src/main/java/baseball/utils/RandomGenerateNumRange.java
@@ -2,7 +2,7 @@ package baseball.utils;
 
 public enum RandomGenerateNumRange {
     START_RANGE_GENERATE_NUM(1),
-    END_RANGE_GENERATE_NUM(2),
+    END_RANGE_GENERATE_NUM(9),
     COUNT(3);
 
     private final int value;
