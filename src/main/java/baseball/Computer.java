04e76abaefa5ba5e2e45e50fa0b61b699118e612
LEEJUNGWOO <jungwoo1002@gmail.com>
2023-10-23 09:28:29 +0900

    refactor: 컴퓨터 생성 수를 멤버변수로 변경

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -2,14 +2,24 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Computer {
     private static final int DIGIT_COUNT = 3;
-    private static final int MAX_NUMBER = 9;
     private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+    private final Numbers computerNumbers;
 
-    public List<Integer> createRandomNumbers() {
+    public Computer() {
+        this.computerNumbers = new Numbers(createRandomNumbers());
+    }
+
+    public static Numbers randomNumbers() {
+        return new Numbers(createRandomNumbers());
+    }
+
+    private static List<Integer> createRandomNumbers() {
         List<Integer> numbers = new ArrayList<>();
         while (numbers.size() < DIGIT_COUNT) {
             int randomNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
@@ -17,6 +27,6 @@ public class Computer {
                 numbers.add(randomNumber);
             }
         }
-        return numbers;
+        return Collections.unmodifiableList(numbers);
     }
 }
