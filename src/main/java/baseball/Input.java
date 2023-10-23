99aa87aee0987680b73b2b1900d6ae78f206ebcf
KimSangHyeok <tkdgur0906@naver.com>
2023-10-23 20:25:14 +0900

    feat: 사용자가 입력한 숫자의 각자리가 서로 다른지 검증하도록 추가

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -2,13 +2,13 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import static baseball.Validator.isInRange;
-import static baseball.Validator.isNumber;
+import static baseball.Validator.*;
 
 public class Input {
     private final String noNumericMessage = "숫자가 아닌 값입니다. 숫자를 입력해주세요.";
     private final String outOfRangeMessage = "3자리 자연수를 입력해주세요.";
     private final String wrongResetFlagMessage = "잘못된 숫자입니다. 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private final String duplicateNumberMessage = "각 자리의 숫자는 서로 달라야합니다.";
 
     public String readUserNumber() {
         String userNumber = Console.readLine();
@@ -17,6 +17,9 @@ public class Input {
         }
         if (!isInRange(userNumber, 100, 999)) {
             throw new IllegalArgumentException(outOfRangeMessage);
+        }
+        if (!isDigitsUnique(userNumber)) {
+            throw new IllegalArgumentException(duplicateNumberMessage);
         }
         return userNumber;
     }
