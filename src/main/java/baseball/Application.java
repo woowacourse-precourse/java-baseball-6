66c6456213f80eae91c80ada48e0231ba18e7bc3
HaHyunkyung <87869664+HaHyunkyung@users.noreply.github.com>
2023-10-24 01:17:04 +0900

    clear

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,29 +1,7 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.*;
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.*;
-import java.util.stream.Collectors;
-
 public class Application {
-	private static List<Integer> computer = new ArrayList<>();
-	
-    public static void main (String[] args){
-    	System.out.println("숫자 야구 게임을 시작합니다.");
-    	RandomNumbers();
+	public static void main (String[] args){
+    	//TODO: 프로그램 구현
     }
-    
-	public static List<Integer> RandomNumbers() {
-		while(computer.size()<3) {
-			int randomNumber = Randoms.pickNumberInRange(1, 9);
-			if(!computer.contains(randomNumber)) {
-				computer.add(randomNumber);
-			}
-    	}
-		return computer;
-	}
-	
-	
-	
-    
 }
