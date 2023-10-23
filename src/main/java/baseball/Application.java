a959043ce1585d41d11cd47c1bb4266264ac8a75
myonjin <kjinabc@gmail.com>
2023-10-24 04:07:51 +0900

    Fix : 재시작 부분 스트라이크 문자 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -80,6 +80,7 @@ public class Application {
                     }
                 }
                 if (strike == 3) {
+                    System.out.println(strike + "스트라이크");
                     System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                     System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                     String again = Console.readLine();
