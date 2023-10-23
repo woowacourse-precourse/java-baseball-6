8e1be0442dea6dd40995c2bef19edc9a7f9b3316
“sangjun121” <juncho12011201@gmail.com>
2023-10-23 21:09:18 +0900

    feat: Utility.Constants 생성 및 상수 선언
    view에서 사용하는 상수들로, 사용자 유저 인터페이스에 출력되는 반복되는 문장을 상수로 선언하였다.

---
+++ src/main/java/baseball/utility/Constants.java
@@ -0,0 +1,12 @@
+package baseball.utility;
+
+public final class Constants { //상속을 막기 위해 final 사용
+    private Constants(){} //생성자를 private로 지정하여 인스턴스 생성 막기
+
+    public static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public static final String NUMBER_INPUT_PROMPT_MESSAGE = "숫자를 입력해주세요 : ";
+    public static final String BALL= "볼";
+    public static final String STRIKE= "스트라이크";
+    public static final String NUMBER_CORRECT_MESSAGE= "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String GAME_PAUSE_OR_RESUME_PROMPT_MESSAGE= "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+}
