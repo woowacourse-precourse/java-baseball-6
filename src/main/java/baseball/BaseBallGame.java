f85fc92f2baae03aa591f18ed817a09fed23d21e
h3136514 <3136514@naver.com>
2023-10-23 16:36:23 +0900

    feat(computer): 상대방(컴퓨터)의 수를 생성한다.

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -1,7 +1,18 @@
 package baseball;
-
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.*;
 public class BaseBallGame {
-
+    public List<Integer> computer;
+    //상대방(컴퓨터)의 수 생성 함수
+    public void RandomNum() {
+        computer =new ArrayList<>();
+        while(computer.size() < 3){
+            int randomNumber =Randoms.pickNumberInRange(1, 9);
+            if(!computer.contains(randomNumber)){
+                computer.add(randomNumber);
+            }
+        }
+    }
     public BaseBallGame(){
 
     }
