3de3db29da0b026be63325bc7b6ae34f9339fd33
anseongjun <dkstjdwns@kookmin.ac.kr>
2023-10-24 03:04:57 +0900

    feat: 게임 시작 및 흐름 제어

--- src/main/java/baseball/controller/BaseBallGame.java
+++ src/main/java/baseball/controller/BaseBallGame.java
@@ -1,5 +1,25 @@
 package baseball.controller;
 
-public class GameStart {
-    
+import baseball.model.Number;
+import baseball.view.InputNumber;
+import java.util.List;
+
+public class BaseBallGame {
+    public static int restart = 0;
+    public static boolean correct = false;
+
+    public static void GameStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        while (restart != 2) {
+            List<Integer> computer = Number.GenerateNumbers();
+            System.out.println(computer); // 확인용
+            correct = false;
+            while (!correct) {
+                String userInput = InputNumber.getUserInput();
+                CheckNumber.checkNumberRules(userInput);
+                CheckNumber.strikesAndBalls(computer, userInput);
+            }
+        }
+    }
 }
