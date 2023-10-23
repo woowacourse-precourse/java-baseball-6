9e5c478b7ac602c855150f88ae9c70a4d4c147de
Kim Chae Lim <121853238+notcherry222@users.noreply.github.com>
2023-10-23 16:05:16 +0900

    feat: 메인 메소드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,9 +6,14 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Application {
+    static int strikeCnt = 0;
+    static int ballCnt = 0;
+    static String choice = "1";
+
     public String setRandomDistinctNumbers() {
         int number = Randoms.pickNumberInRange(1, 9);
         return String.valueOf(number);
@@ -17,10 +22,10 @@ public class Application {
     public String startGame() {
         return "숫자 야구 게임을 시작합니다.";
     }
+    //TODO : 리팩토링 메소드 명 변경
+    //TODO : convertToList 메소드 만들기
     public int compareDigitsNumbers(String answer, String input) {
         int strikeCnt = 0;
-        answer = setRandomDistinctNumbers();
-        input = Console.readLine();
         checkInputValidation(input);
 
         List<String> answerList = new ArrayList<>();
@@ -38,9 +43,56 @@ public class Application {
         }
         return strikeCnt;
     }
-    public void countCommonNumbers() {}
-    public void showBaseballOutcome() {}
-    public void confirmGameEnd() {}
+    public int countCommonNumbers(String answer, String input) {
+        int ballCnt = 0;
+        checkInputValidation(input);
+
+        List<String> answerList = new ArrayList<>();
+        List<String> inputList = new ArrayList<>();
+
+        for(int i = 0; i<3; i++){
+            answerList.add(Arrays.toString(answer.split("")));
+            inputList.add(Arrays.toString(input.split("")));
+        }
+
+        List<String> matchList = inputList.stream().filter(n -> answerList.stream()
+                .anyMatch(Predicate.isEqual(n))).collect(Collectors.toList());
+        if (matchList.isEmpty()) {
+            ballCnt = 0;
+        } else {
+            ballCnt = matchList.size();
+        }
+        return ballCnt;
+    }
+
+    public String showBaseballOutcome(String answer, String input) {
+        strikeCnt = compareDigitsNumbers(answer, input);
+        ballCnt = countCommonNumbers(answer, input);
+
+        if (strikeCnt == 0) {
+            if(ballCnt == 0){
+                return "낫싱";
+            }
+            return ballCnt + "볼";
+        }
+        ballCnt -= strikeCnt;
+        if (ballCnt == 0) {
+            return strikeCnt + "스트라이크";
+        } else {
+            return ballCnt + "볼 " + strikeCnt + "스트라이크";
+        }
+    }
+
+    public  String confirmGameEnd() {
+        String choice = Console.readLine();
+        checkExitChoiceValidation(choice);
+
+        System.out.println("3스트라이크");
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        return choice;
+    }
     public void checkInputValidation(String input) {
         if (input == null || input.isEmpty() || !input.matches("\\d{3}")) {
             throw new IllegalArgumentException();
@@ -60,6 +112,21 @@ public class Application {
         }
     }
     public static void main(String[] args) {
+        Application application = new Application();
+        application.startGame();
+
+        while (choice.equals("1")) {
+            String answer = application.setRandomDistinctNumbers();
+            String input = Console.readLine();
+
+            while(!input.equals(answer)) {
+                System.out.println("숫자를 입력해주세요 : ");
+                input = Console.readLine();
+                application.showBaseballOutcome(answer, input);
+            }
+            application.confirmGameEnd();
+            choice = Console.readLine();
+        }
 
     }
 }
