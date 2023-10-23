c01d18028becd1b02866f10a2a348d4a94c75a0d
inpink <inpink@kakao.com>
2023-10-24 00:56:47 +0900

    refactor: BallController에서 Ball 생성하는 로직 변경
    메소드명을 create로 하여 더 알기 쉽게 함.
    HintItem에 isActive가 추가된 것을 고려하여 setActive 추가.
    외부에서 Ball의 activce 여부를 알고 싶을때, Ball에서 boolean을 쓰는지, 어떤 식으로 판단하는지 로직을 알지 못하기 위해 isActive() 메소드로 메시지만 전달하게 함. 내부 은닉화 가능.

--- src/main/java/baseball/controller/BallController.java
+++ src/main/java/baseball/controller/BallController.java
@@ -7,11 +7,16 @@ import baseball.service.BallService;
 public class BallController {
     private static final BallService ballService = BallService.getInstance();
 
-    public static Ball countBall(Baseball computerBaseball, Baseball inputBaseball) {
+    public static Ball create(Baseball computerBaseball, Baseball inputBaseball) {
         Ball ball = new Ball();
 
         ball.setCount(ballService.count(computerBaseball, inputBaseball));
-
+        ball.setActive(ballService.active(ball));
         return ball;
     }
+
+    public static boolean isActive(Ball ball) {
+        return ball.isActive();
+    }
+
 }
