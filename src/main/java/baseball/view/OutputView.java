030ccbeee985fdcb1d1a11d272bd99a3f6f796b0
khsrla9806 <kimhunsope@naver.com>
2023-10-23 11:17:22 +0900

    feat: 숫자 입력 메시지 출력 메서드 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,4 +4,9 @@ public class OutputView {
     public static void printGameStartMessage() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
+    public static void printInputNumberMessage() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
 }
