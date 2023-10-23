ebe458ff75858e7bd09a1bee98510ae68c344a33
inwoo <korean5078@gmail.com>
2023-10-23 22:05:45 +0900

    feat: 사용자 숫자와 컴퓨터 숫자를 비교하는 기능 추가 1. 사용자 숫자와 컴퓨터 숫자 비교하는 함수 compare 생성 및 구현 2. 비교한 결과를 출력하는 printScore 함수 생성 및 구현 3. makeRandomNumber 함수 랜덤 숫자 생성 범위 변경

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -4,12 +4,14 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class BaseBallGame {
 	private static int randomNumber;
+	private static int hundred, ten, one;
+	private int ball, strike;
+
 	BaseBallGame() {
 		makeRandomNumber();
 	}
 
 	private static void makeRandomNumber() {
-		int hundred, ten, one;
 		randomNumber = Randoms.pickNumberInRange(123, 988);
 
 		while (true) {
@@ -21,7 +23,7 @@ public class BaseBallGame {
 				break;
 			}
 
-			randomNumber = Randoms.pickNumberInRange(1, 1000);
+			randomNumber = Randoms.pickNumberInRange(123, 988);
 		}
 	}
 
@@ -31,5 +33,60 @@ public class BaseBallGame {
 
 	public void printFirst() {
 		System.out.println("숫자 야구 게임을 시작합니다.");
+	}
+
+	public void compare(int userNumber) {
+		strike = 0;
+		ball = 0;
+		int[] numbers = new int[3];
+
+		numbers[0] = userNumber / 100;
+		numbers[1] = userNumber % 100 / 10;
+		numbers[2] = userNumber % 100 % 10;
+
+		for (int i = 0; i < 3; i++) {
+			if (hundred == numbers[i]) {
+				if (i == 0) {
+					strike++;
+				} else {
+					ball++;
+				}
+			}
+
+			if (ten == numbers[i]) {
+				if (i == 1) {
+					strike++;
+				} else {
+					ball++;
+				}
+			}
+
+			if (one == numbers[i]) {
+				if (i == 2) {
+					strike++;
+				} else {
+					ball++;
+				}
+			}
+		}
+
+		printScore();
+	}
+
+	public void printScore() {
+		if (strike == 0 && ball == 0) {
+			System.out.println("낫싱");
+			return;
+		}
+		if (ball > 0) {
+			System.out.println(ball + "볼 ");
+		}
+		if (strike > 0) {
+			System.out.println(strike + "스트라이크");
+		}
+
+		if (strike == 3) {
+			System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+		}
 	}
 }
