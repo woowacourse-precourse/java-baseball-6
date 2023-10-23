da37da29f9a0a07bc977d9ede51e3aaec82df1a1
inpink <inpink@kakao.com>
2023-10-24 01:37:03 +0900

    refactor: NothingController, NothingService에도 Nothing 객체 생성하는 코드 작성.
    BallController, BallService와 로직 거의 유사.

---
+++ src/main/java/baseball/service/hint/NothingService.java
@@ -0,0 +1,45 @@
+package baseball.service.hint;
+
+import baseball.domain.Baseball;
+import baseball.domain.GameConstants;
+import baseball.domain.hint.Nothing;
+import baseball.util.ObjectUtil;
+
+import java.util.List;
+
+public class NothingService implements HintItemService<Nothing> {
+
+    private static final NothingService nothingService = new NothingService();
+
+    public static NothingService getInstance(){
+        return nothingService;
+    }
+
+    private NothingService() {
+
+    }
+
+    public int count(Baseball computerBaseball, Baseball inputBaseball) {
+        int count = 0;
+        List computer = computerBaseball.getValues();
+        List input = inputBaseball.getValues();
+
+        for (int i = 0; i < GameConstants.NUMBER_LENGTH; i++) {
+            for (int j = 0; j < GameConstants.NUMBER_LENGTH; j++) {
+                if (ObjectUtil.isSame(computer.get(i), input.get(j))) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public boolean active(Nothing nothing) {
+        if (nothing.getCount() == 0 ) {
+            return true;
+        } return false;
+    }
+
+}
+
