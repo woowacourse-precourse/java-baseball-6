8dbe3ea6cc0606ddd3e4eb028c085a6cf821bf48
HaHyunkyung <87869664+HaHyunkyung@users.noreply.github.com>
2023-10-24 00:18:40 +0900

    feat: 난수 발생 함수 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,31 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.*;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+	private static List<Integer> computer = new ArrayList<>();
+	
+    public static void main (String[] args){
+    	System.out.println("숫자 야구 게임을 시작합니다.");
+    	RandomNumbers();
+    	for (int i = 0; i < 3; i++) {
+    		System.out.println(computer.get(i));
+		}
     }
+    
+	public static List<Integer> RandomNumbers() {
+		while(computer.size()<3) {
+			int randomNumber = Randoms.pickNumberInRange(1, 9);
+			if(!computer.contains(randomNumber)) {
+				computer.add(randomNumber);
+			}
+    	}
+		return computer;
+	}
+	
+	
+    
 }
