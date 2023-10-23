d7fd1ad22a3e9860b7f2b97b6c3dfeae2084bb8c
qudwns017 <qudwns017@naver.com>
2023-10-24 04:27:16 +0900

    Feat: 0, 1 상수 추가

--- src/main/java/baseball/util/Constants.java
+++ src/main/java/baseball/util/Constants.java
@@ -5,4 +5,6 @@ public class Constants {
     public static final Integer STRIKE_SIZE = 3;
     public static final Integer NUMBER_MIN_RANGE = 1;
     public static final Integer NUMBER_MAX_RANGE = 9;
+    public static final Integer ZERO = 0;
+    public static final Integer ONE = 1;
 }
