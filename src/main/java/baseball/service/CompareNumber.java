102ecca5a66e6e55d5391765723228c5b38b2962
chan-won <clw8679@naver.com>
2023-10-23 21:59:59 +0900

    Feat: 볼 개수 확인 기능 추가

--- src/main/java/baseball/service/CompareNumber.java
+++ src/main/java/baseball/service/CompareNumber.java
@@ -15,7 +15,15 @@ public class CompareNumber {
         return count;
     }
 
-
+    public static int CountBall(List<Integer> computer, List<Integer> user) {
+        int count = 0;
+        for (int i = 0; i < 3; i++) {
+            if (computer.contains(user.get(i)) && !Objects.equals(computer.get(i), user.get(i))) {
+                count++;
+            }
+        }
+        return count;
+    }
 
 }
 
