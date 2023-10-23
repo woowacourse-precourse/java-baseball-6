d0e982479a11eefcb79aa31cff5963eacf7b924f
HyunJoong Kim <80106238+hjk0761@users.noreply.github.com>
2023-10-23 15:20:59 +0900

    feat: add test code for comparing inputs

--- src/main/java/test/test.java
+++ src/main/java/test/test.java
@@ -1,6 +1,7 @@
 package test;
 import baseball.UserInput;
 import baseball.GenerateAnswer;
+import baseball.CompareInput;
 
 import java.util.List;
 
@@ -8,6 +9,7 @@ public class test {
     public static void main(String[] args){
         UserInputTest();
         GenerateAnswerTest();
+        CompareInputTest();
     }
 
     public static void UserInputTest(){
@@ -25,6 +27,17 @@ public class test {
         for (int i = 0; i < 10; i ++){
             GenerateAnswer generateAnswer = new GenerateAnswer(3);
             System.out.println(generateAnswer.genAnswer());
+        }
+    }
+
+    public static void CompareInputTest(){
+        List<Integer> answer = List.of(1, 2, 3);
+        UserInput userInput = new UserInput(3);
+        CompareInput compareInput = new CompareInput(answer, 3);
+        for (int i = 0; i < 10; i++){
+            List<Integer> temp = userInput.getInput();
+            List<Integer> result = compareInput.countMatch(temp);
+            System.out.println(result);
         }
     }
 
