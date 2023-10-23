f11b05890f81caaad837bc8588858b9dbd53b19d
Seungjida <esg0107@pusan.ac.kr>
2023-10-23 22:10:37 +0900

    Feat: "3자리 수가 아닌 경우 판단 기능 추가"

--- src/main/java/others/ExceptionHandling.java
+++ src/main/java/others/ExceptionHandling.java
@@ -1,9 +1,18 @@
 package others;
 
 public class ExceptionHandling {
+    private static final int numberOfNumbers = 3;
+
     private static void isNull(final String strNumber) {
         if (strNumber.isBlank()) {
             throw new IllegalArgumentException("문자열이 비어있거나 빈 공백으로 이뤄져 있습니다. 게임을 종료합니다.");
         }
     }
+
+    private static void isThree(final String strNumber) {
+        if (strNumber.length() != numberOfNumbers) {
+            throw new IllegalArgumentException("3자리 수를 입력하셔야 합니다. 게임을 종료합니다.");
+        }
+    }
+
 }
