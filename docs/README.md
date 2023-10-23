614346fdca542aad5ebb0e11fb7317786673207a
enduf7686 <enduf768640@gmail.com>
2023-10-23 20:05:51 +0900

    InputChecker 예외 처리 정규 표현식으로 교체

--- docs/README.md
+++ docs/README.md
@@ -1,28 +1,31 @@
 ## 1. Application
+
 - main()
-   - 숫자 야구 게임 시작
-   - 게임이 끝난 경우 재시작/종료를 구분하는 숫자 입력 시 예외 발생
+    - 숫자 야구 게임 시작
+    - 게임이 끝난 경우 재시작/종료를 구분하는 숫자 입력 시 예외 발생
 
 ## 2. BaseballGame
+
 - BaseballGame 싱글톤 객체로 관리
 - play()
-  - 실제 숫자 야구 로직 들어 있음
+    - 실제 숫자 야구 로직 들어 있음
 - endCheck()
-  - 숫자 야구 한판이 끝난 후 재시작할지 종료할 지 판단하는 메서드
+    - 숫자 야구 한판이 끝난 후 재시작할지 종료할 지 판단하는 메서드
 - setComputerNumber()
-  - 컴퓨터 숫자 세팅
-  - checkDuplicationAndSetNumber(List<Integer> computer, int randomNumber)
-    - 컴퓨터 숫자 중복 검사 후 세팅 
+    - 컴퓨터 숫자 세팅
+    - checkDuplicationAndSetNumber(List<Integer> computer, int randomNumber)
+        - 컴퓨터 숫자 중복 검사 후 세팅
 - setMyNumber()
-  - 내 숫자 세팅
+    - 내 숫자 세팅
 - compareNumber(List, List)
-  - 컴퓨터의 숫자와 내 숫자 비교
+    - 컴퓨터의 숫자와 내 숫자 비교
 - printResult(Result)
-  - result에 들어있는 ball, strike 개수를 요구사항에 맞춰 출력
+    - result에 들어있는 ball, strike 개수를 요구사항에 맞춰 출력
 - isMyNumberCorrect(Result result)
-  - result가 3스트라이트면 게임을 종료할 수 있도록 true 반환
+    - result가 3스트라이트면 게임을 종료할 수 있도록 true 반환
 
 ## 3. Constant
+
 - 상수 전용 클래스
 - RESTART_NUMBER: 1
 - END_NUMBER: 2
@@ -30,11 +33,15 @@
 - MY_NUMBER_INPUT_LENGTH : 게임 진행 시 숫자 길이(3)
 
 ## 4. InputChecker</h2>
+
 - 입력 예외 처리용 객체
 - checkMyNumber(String input)
-  - 게임 중 숫자 입력 예외 체크
+    - 게임 중 숫자 입력 예외 체크
+    - 정규 표현식 활용(1부터 9까지의 서로 다른 3개의 숫자)
 - checkRestartNumber(String input)
-  - 게임 종료 후 재시작 여부 확인 숫자 입력 예외 체크
+    - 게임 종료 후 재시작 여부 확인 숫자 입력 예외 체크
+    - 정규 표현식 활용(1 또는 2)
 
 <h2>5. Result</h2>
+
 - ball, strike 변수를 위한 DTO
--- src/main/java/baseball/InputChecker.java
+++ src/main/java/baseball/InputChecker.java
@@ -1,51 +1,22 @@
 package baseball;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static baseball.Constant.END_NUMBER;
-import static baseball.Constant.MY_NUMBER_INPUT_LENGTH;
-import static baseball.Constant.RESTART_NUMBER;
-import static baseball.Constant.RESTART_OR_END_INPUT_LENGTH;
+import java.util.regex.Pattern;
 
 public class InputChecker {
-
-    private static final int ASCII_CODE_ONE = 49;
-    private static final int ASCII_CODE_NINE = 57;
 
     private InputChecker() {
     }
 
     public static void checkMyNumber(String input) {
-        if (input.length() != MY_NUMBER_INPUT_LENGTH) {
-            throw new IllegalArgumentException("3개의 다른 수를 입력하세요.");
-        }
-
-        if (Arrays.stream(input.split("")).collect(Collectors.toSet()).size() != MY_NUMBER_INPUT_LENGTH) {
+        //입력 값(input)이 1부터 9까지의 서로 다른 3개의 숫자가 아니면 예외 발생
+        if (!Pattern.matches("^(?!([0-9]).*?\\1)[1-9]{3}$", input)) {
             throw new IllegalArgumentException("숫자를 입력해주세요.");
-        }
-
-        for (char c : input.toCharArray()) {
-            if (c < ASCII_CODE_ONE || c > ASCII_CODE_NINE) {
-                throw new IllegalArgumentException("1부터 9사이의 숫자를 입력해주세요.");
-            }
         }
     }
 
     public static void checkRestartNumber(String input) {
-        if (input.length() != RESTART_OR_END_INPUT_LENGTH) {
-            throw new IllegalArgumentException("1 또는 2를 입력해주세요.");
-        }
-
-        int num;
-        try {
-            num = Integer.valueOf(input);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자를 입력해주세요.");
-        }
-
-        if (num != RESTART_NUMBER && num != END_NUMBER) {
+        //입력 값(input)이 1 또는 2가 아니면 예외 발생
+        if (!Pattern.matches("^[1-2]$", input)) {
             throw new IllegalArgumentException("1 또는 2를 입력해주세요.");
         }
     }
