b2a3d9e8881a99c362db098dcf3eaaba86912c0a
devholic22 <hyunjoon.tech@gmail.com>
2023-10-23 14:28:45 +0900

    Test: 플레이 숫자는 세자리여야만 한다

---
+++ src/test/java/baseball/PlayNumberTest.java
@@ -0,0 +1,15 @@
+package baseball;
+
+import baseball.model.PlayNumber;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PlayNumberTest {
+
+    @Test
+    void 플레이_숫자는_세자리여야만_한다() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            PlayNumber playNumber = PlayNumber.from("1234");
+        });
+    }
+}
