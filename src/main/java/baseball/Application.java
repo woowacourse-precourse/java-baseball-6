646b1d3886a873edf9458198006ffd50ccc53762
jaehyuuk <doogun97@gmail.com>
2023-10-23 17:27:32 +0900

    최적화 및 테스트
    스트라이크와 볼 개수를 세는 로직을 좀 더 보기 쉽게 변경함
    결과값을 출력하는 과정을 단순화함
    모든 숫자를 맞춘 경우 출력하는 메세지를 변수명으로 변경함

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -38,20 +38,19 @@ public class Application {
 
                 for (int i = 0; i < numberSize; i++) {
                     for (int j = 0; j < numberSize; j++) {
-                        if (j != i) {
-                            if (computer.get(i).equals(user[j])) ball++;
-                        }
+                        if (i != j) { if (computer.get(i).equals(user[j])) ball++; }
+                        else        { if (computer.get(i).equals(user[j])) strike++; }
                     }
-                    if (computer.get(i).equals(user[i])) strike++;
                 }
 
-                if      (ball > 0 && strike == 0) System.out.println(ball + "볼");
-                else if (strike > 0 && ball == 0) System.out.println(strike + "스트라이크");
-                else if (ball > 0 && strike > 0)  System.out.println(ball + "볼 " + strike + "스트라이크");
-                else                              System.out.println("낫싱");
+                String result = "";
+                if (ball > 0)         result += ball + "볼 ";
+                if (strike > 0)       result += strike + "스트라이크";
+                if (result.isBlank()) result += "낫싱";
+                System.out.println(result);
 
                 if (strike == numberSize) {
-                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    System.out.println(numberSize + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
                     System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                     int restart = Integer.parseInt(Console.readLine());
                     if      (restart == 1) break;
