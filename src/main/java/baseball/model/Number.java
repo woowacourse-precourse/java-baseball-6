f4b4784914d356bac29ad9c2d11bfacab402e718
anseongjun <dkstjdwns@kookmin.ac.kr>
2023-10-24 03:03:08 +0900

    feat: 컴퓨터 숫자 생성

--- src/main/java/baseball/model/Number.java
+++ src/main/java/baseball/model/Number.java
@@ -1,4 +1,20 @@
 package baseball.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Number {
+
+    public static List<Integer> GenerateNumbers() {
+        List<Integer> computerNumber = new ArrayList<>();
+        while (computerNumber.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumber.contains(randomNumber)) {
+                computerNumber.add(randomNumber);
+            }
+        }
+        return computerNumber;
+    }
+
 }
