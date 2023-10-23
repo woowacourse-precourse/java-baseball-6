881499284408dc7b3aca0ade8195e377da419816
MingyeomKim <mingyum119@naver.com>
2023-10-23 22:15:15 +0900

    feat(PlayerController): 잘못된 문자가 입력된 경우에 대한 유효성 검사
    숫자를 검증하는 로직을 따로 함수로 만든다. 유효성 검사에 대한 함수를 만들어 이 함수에 의존하도록 하고, 검사 실패 시 IllegalArgumentException을 발생시킨다.

--- src/main/java/baseball/controller/PlayerController.java
+++ src/main/java/baseball/controller/PlayerController.java
@@ -10,7 +10,8 @@ public class PlayerController {
 
     public List<Integer> readAndValidateInputNumbers() {
         String inputString = inputView.readInputNumbers();
-        return parseInputString(inputString);
+        List<Integer> inputList = parseInputString(inputString);
+        validateNumbers(inputList);
     }
 
     private List<Integer> parseInputString(String inputNumbers) {
@@ -19,5 +20,18 @@ public class PlayerController {
             result.add(Integer.parseInt(number));
         }
         return result;
+    }
+
+    private void validateNumbers(List<Integer> inputList) {
+        validateOutOfRange(inputList);
+    }
+
+    private void validateOutOfRange(List<Integer> inputList) {
+        inputList.stream()
+                .filter(number -> number < 1 || number > 9)
+                .findFirst()
+                .ifPresent(number -> {
+                    throw new IllegalArgumentException("숫자는 1 ~ 9 사이의 숫자만 입력할 수 있습니다.");
+                });
     }
 }
