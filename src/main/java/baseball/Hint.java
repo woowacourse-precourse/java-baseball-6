e2fd77d72cbc8b8c1a760a300e792edefe97ce52
jihyuk <kwlgur0416@gmail.com>
2023-10-23 15:32:00 +0900

    feature : 채점 결과를 저장하는 Hint 도메인 작성

---
+++ src/main/java/baseball/Hint.java
@@ -0,0 +1,36 @@
+package baseball;
+
+public class Hint {
+
+    private int strike;
+    private int ball;
+
+    public Hint() {
+        this.strike = 0;
+        this.ball = 0;
+    }
+
+    @Override
+    public String toString() {
+        if (ball == 0 && strike == 0) {
+            return "낫싱";
+        } else if (ball == 0 && strike > 0) {
+            return strike + "스트라이크";
+        } else if (ball > 0 && strike == 0) {
+            return ball + "볼";
+        } else if (ball > 0 && strike > 0) {
+            return ball + "볼 " + strike + "스트라이크";
+        } else {
+            throw new RuntimeException("볼과 스트라이크가 음수입니다.");
+        }
+    }
+
+    public void increaseBall() {
+        ball++;
+    }
+
+    public void increaseStrike() {
+        strike++;
+    }
+
+}
