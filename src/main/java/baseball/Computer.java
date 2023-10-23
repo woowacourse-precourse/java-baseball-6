35e333a1c0d193119a12d1f95d219c78d0128850
sinminseok12 <sinminseok12@naver.com>
2023-10-24 01:17:56 +0900

    feat : 컴퓨터가 랜덤 3자리 숫자를 생성하는 기능 구현

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,4 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static baseball.Constants.*;
+
 public class Computer {
+    private final List<Integer> numbers;
+
+    public Computer() {
+        this.numbers = generateNumbers();
+    }
+
+    private List<Integer> generateNumbers() {
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        while (uniqueNumbers.size() < NUMBERS_SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(NUMBER_MIN_RANGE, NUMBER_MAX_RANGE);
+            uniqueNumbers.add(randomNumber);
+        }
+        return new ArrayList<>(uniqueNumbers);
+    }
 }
--- src/main/java/baseball/Constants.java
+++ src/main/java/baseball/Constants.java
@@ -1,4 +1,7 @@
 package baseball;
 
 public class Constants {
+    public static final Integer NUMBERS_SIZE = 3;
+    public static final Integer NUMBER_MIN_RANGE = 1;
+    public static final Integer NUMBER_MAX_RANGE = 9;
 }
