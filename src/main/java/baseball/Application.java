4162bc6279434c88285957df7c2d55f48145fd9d
chs98412 <chs98412@gmail.com>
2023-10-23 22:05:00 +0900

    fix: 메인함수에서 게임에 관련된 로직 줄이기1

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,7 +5,6 @@ import baseball.controller.InputController;
 import baseball.controller.OutputController;
 import baseball.entity.Game;
 import baseball.entity.Player;
-import baseball.entity.Result;
 
 import static baseball.controller.OutputController.*;
 
@@ -15,71 +14,26 @@ public class Application {
 
         InputController inputController = new InputController();
         OutputController outputController = new OutputController();
-        GameController gameController = new GameController();
-        outputController.cliOutputln(START);
+
+        outputController.printNoticeln(START);
         Game game;
         Player player;
 
         while (true) {
-            boolean flag = false;
 
             game = new Game();
 
             player  = new Player();
 
             game.printAnswer();
+            GameController gameController = new GameController(inputController,outputController,game,player);
 
+            gameController.baseballGame();
 
-            while (true) {
-
-
-                outputController.cliOutput(INPUT);
-                player.setInputString(inputController.playerInput());
-                Result result = gameController.game(player, game);
-
-                System.out.println(player.getInputString());
-                if (result.strike != 0 && result.ball == 0) {
-                    System.out.print(result.strike);
-                    outputController.cliOutputln(STRIKE);
-
-                    if (result.isAnswer) {
-                        outputController.cliOutputln(ANSWER);
-                        outputController.cliOutputln(AFTER);
-                        String regame = inputController.reGameInput();
-                        if (regame.equals("1") ){
-                            System.out.println("here");
-                            flag = true;
-                        }
-                        break;
-                    }
-                }
-                 else if (result.strike == 0 && result.ball != 0) {
-                    System.out.print(result.ball);
-                    outputController.cliOutputln(BALL);
-                }else if (result.strike == 0 && result.ball == 0) {
-
-                    outputController.cliOutputln(NOTHING);
-                }
-                else {
-                    System.out.print(result.ball);
-                    outputController.cliOutput(BALL);
-                    outputController.cliOutput(" ");
-                    System.out.print(result.strike);
-                    outputController.cliOutputln(STRIKE);
-                }
-
-
-            }
-            System.out.println(flag);
-            if (!flag) {
+            if (!gameController.re()) {
                 break;
             }
         }
     }
 
-    private void init() {
-    }
-
-    private void initgame() {
-    }
 }
