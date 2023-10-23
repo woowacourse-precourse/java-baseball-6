d40354f15c960e5558e3a9fecf238bf1bb807d4f
wooyong99 <wulovesk@naver.com>
2023-10-24 00:40:39 +0900

    [test] : 중복 값 예외 테스트

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -36,6 +36,13 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 중복_값_예외_테스트() {
+        assertSimpleTest(() ->
+                                 assertThatThrownBy(() -> runException("122"))
+                                         .isInstanceOf(IllegalArgumentException.class));
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
