934ea5393b17d8c6c458673474cd1a86a759fafb
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-10-23 14:33:00 +0900

    style(Computer): Revise comments

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -26,7 +26,7 @@ public class Computer {
         return com;
     }
 
-    // 게임 재시작 전 상대방의 숫자를 저장할 배열 비우기
+    // 상대방이 선택한 숫자가 저장된 배열 비우기
     public void clearCom() {
         com.clear();
     }
