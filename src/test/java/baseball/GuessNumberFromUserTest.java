ee1be5cad4122b078a6b07256d29f632e03bb256
jihyuk <kwlgur0416@gmail.com>
2023-10-23 14:51:29 +0900

    test : 추측값 개별 테스트 후 scanner close 추가

--- src/test/java/baseball/GuessNumberFromUserTest.java
+++ src/test/java/baseball/GuessNumberFromUserTest.java
@@ -3,24 +3,30 @@ package baseball;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.List;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 class GuessNumberFromUserTest {
 
     GuessNumberFromUser guessNumberFromUser = new GuessNumberFromUser();
 
+    @AfterEach
+    void closeScanner() {
+        Console.close();
+    }
+
     @Test
     void 추측값_정상_로직_테스트() {
         // given
         String input = "123";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
+        settingForInputStream(input);
 
         OutputStream out = new ByteArrayOutputStream();
         System.setOut(new PrintStream(out));
@@ -35,9 +41,9 @@ class GuessNumberFromUserTest {
 
     @Test
     void 추측값_타입_예외_테스트() {
+        // given
         String input = "avx\nㅁㄱㄴ\n\"   \"";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
+        settingForInputStream(input);
 
         // when & then
         String[] inputList = input.split("\n");
@@ -49,9 +55,9 @@ class GuessNumberFromUserTest {
 
     @Test
     void 추측값_자리수_예외_테스트() {
+        // given
         String input = "12\n1256\n9246";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
+        settingForInputStream(input);
 
         // when & then
         String[] inputList = input.split("\n");
@@ -63,9 +69,9 @@ class GuessNumberFromUserTest {
 
     @Test
     void 추측값_범위_예외_테스트() {
+        // given
         String input = "102\n012\n9086";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
+        settingForInputStream(input);
 
         // when & then
         String[] inputList = input.split("\n");
@@ -77,9 +83,9 @@ class GuessNumberFromUserTest {
 
     @Test
     void 추측값_중복_예외_테스트() {
+        // given
         String input = "112\n999\n9774";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
+        settingForInputStream(input);
 
         // when & then
         String[] inputList = input.split("\n");
@@ -87,6 +93,11 @@ class GuessNumberFromUserTest {
             assertThatThrownBy(() -> guessNumberFromUser.getGuessNumber())
                 .isInstanceOf(IllegalArgumentException.class);
         }
+    }
+
+    private void settingForInputStream(String input) {
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
     }
 
 }
