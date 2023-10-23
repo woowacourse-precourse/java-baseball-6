20561937ed7752b6220db28d6dfd43754c67b425
jifrozen0110 <answldjs1836@gmail.com>
2023-10-24 02:22:06 +0900

    fix : NumberGenerator - 상수 변경

--- src/main/java/baseball/utils/NumberGenerator.java
+++ src/main/java/baseball/utils/NumberGenerator.java
@@ -9,7 +9,8 @@ public class NumberGenerator {
     public static List<Integer> makeRandomNumber() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < BaseballSystemConst.MAX_BALL_SIZE) {
-            int randomNumber = Randoms.pickNumberInRange(BaseballSystemConst.MIN_DIGIT_BALL, BaseballSystemConst.MAX_DIGIT_BALL);
+            int randomNumber = Randoms.pickNumberInRange(BaseballSystemConst.MIN_DIGIT_BALL,
+                    BaseballSystemConst.MAX_DIGIT_BALL);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
