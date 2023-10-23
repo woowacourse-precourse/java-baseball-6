260a0925a56223d1ce514e01dfeddd41f451596c
Hyun Woo <hwsa10041@gmail.com>
2023-10-23 20:09:39 +0900

    refactor : createRandomNumber() 테스트 수정
    - 자리의 수들이 전부 각기 다른 수인지 확인하게 변경

--- src/test/java/baseball/model/GameNumberTest.java
+++ src/test/java/baseball/model/GameNumberTest.java
@@ -3,24 +3,30 @@ package baseball.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class GameNumberTest {
 
-    @DisplayName("랜덤한 3자리 숫자를 생성한다.")
+    @DisplayName("랜덤한 각 자리수가 다른 3자리 숫자를 생성한다.")
     @Test
     void createRandomNumber() {
         // given
         int T = 1000;
 
-        while (T --> 0) {
+        while (T-- > 0) {
             // when
             Game game = Game.createRandomNumber();
-            char[] randNumbers = game.getRandNumber();
+            Set<Character> randNumberSet = new HashSet<>();
+            for (char randNumber : game.getRandNumber()) {
+                randNumberSet.add(randNumber);
+            }
 
             // then
-            assertThat(randNumbers).hasSize(3);
-            for (char c : randNumbers) {
+            assertThat(randNumberSet).hasSize(3);
+            for (char c : randNumberSet) {
                 assertThat(Character.isDigit(c)).isTrue();
             }
         }
