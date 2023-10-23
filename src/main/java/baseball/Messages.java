653917ffbcd6c2ba9c3fcb7ab493f699188f4c92
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-10-23 18:21:23 +0900

    refactor: reorder variables

--- src/main/java/baseball/Messages.java
+++ src/main/java/baseball/Messages.java
@@ -4,12 +4,11 @@ public class Messages {
 
     private String introMessage = "숫자 야구 게임을 시작합니다.";
     private String inputMessage = "숫자를 입력해주세요 : ";
-    ;
+    private String sameNumberMessage = "볼";
+    private String sameDigitMessage = "스트라이크";
+    private String wrongMessage = "낫싱";
     private String outroMessage = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private String willRestartMessage = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-    private String sameDigitMessage = "스트라이크";
-    private String sameNumberMessage = "볼";
-    private String wrongMessage = "낫싱";
 
     public String getIntroMessage() {
         return introMessage;
@@ -27,12 +26,12 @@ public class Messages {
         this.inputMessage = inputMessage;
     }
 
-    public String getOutroMessage() {
-        return outroMessage;
+    public String getSameNumberMessage() {
+        return sameNumberMessage;
     }
 
-    public void setOutroMessage(String outroMessage) {
-        this.outroMessage = outroMessage;
+    public void setSameNumberMessage(String sameNumberMessage) {
+        this.sameNumberMessage = sameNumberMessage;
     }
 
     public String getSameDigitMessage() {
@@ -43,20 +42,20 @@ public class Messages {
         this.sameDigitMessage = sameDigitMessage;
     }
 
-    public String getSameNumberMessage() {
-        return sameNumberMessage;
-    }
-
-    public void setSameNumberMessage(String sameNumberMessage) {
-        this.sameNumberMessage = sameNumberMessage;
-    }
-
     public String getWrongMessage() {
         return wrongMessage;
     }
 
     public void setWrongMessage(String wrongMessage) {
         this.wrongMessage = wrongMessage;
+    }
+
+    public String getOutroMessage() {
+        return outroMessage;
+    }
+
+    public void setOutroMessage(String outroMessage) {
+        this.outroMessage = outroMessage;
     }
 
     public String getWillRestartMessage() {
