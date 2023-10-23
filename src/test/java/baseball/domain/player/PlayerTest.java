97056ca4023c2c7435f067c4160322d03076f5db
sejin park <seijin0722@naver.com>
2023-10-23 17:09:57 +0900

    test : 4개 미만 수가 들어왔을때 정상적으로 작동하는 테스트코드

--- src/test/java/baseball/domain/player/PlayerTest.java
+++ src/test/java/baseball/domain/player/PlayerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class PlayerTest {
@@ -20,6 +21,17 @@ class PlayerTest {
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> Player.createByIntegerNumbers(input))
                 .withMessageMatching("3개 이상의 공이 입력될 수 없습니다.");
+    }
+    @Test
+    @DisplayName("3개의 수가 입력될 경우 정상적으로 생성되어야 한다.")
+    public void createPlayerWithThreeNumbers() {
+        List<Integer> inputNumbers = Arrays.asList(1, 2, 3);
+
+        // when
+        Player player = Player.createByIntegerNumbers(inputNumbers);
+
+        // then
+        assertThat(player).isNotNull();
     }
     @Test
     @DisplayName("중복된 숫자가 입력될 경우 Exception이 발생해야 한다.")
