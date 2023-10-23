e4a23c5e61da38fbcccd6001732f99f10065f203
Ukj0ng <xb253q@gmail.com>
2023-10-23 17:41:24 +0900

    fix: Computer의 숫자 생성시 항시 초기화후 생성으로 변경

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -6,21 +6,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
-    private List<Integer> number;
+    private final List<Integer> number;
 
     public Computer () {
         number = new ArrayList<>();
+        setNumber();
     }
 
     public void setNumber() {
+        resetNumber();
         while (number.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!number.contains(randomNumber)) {
                 number.add(randomNumber);
+                System.out.print(randomNumber);
             }
         }
+        System.out.println();
     }
+
     public List<Integer> getNumber() {
         return number;
+    }
+
+    public void resetNumber() {
+        number.clear();
     }
 }
