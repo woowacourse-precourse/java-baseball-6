c29f66be62ed559314d74e66da9aa9cda75e1f74
suhyun__ <thebbbb1206@gmail.com>
2023-10-24 01:53:51 +0900

    feat: 게임 시작 기능 일부 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,6 +1,21 @@
 package baseball;
 
+import java.util.List;
+
 public class BaseballGame {
+    private List<Integer> computerNumberList;
+    private List<Integer> userNumberList;
+
+    public void start() {
+        showStart();
+        Computer computer = new Computer();
+        computerNumberList = computer.generateComputerNumber();
+        showInputUserNumber();
+        User user = new User();
+        String userNumberStr = new String();
+        user.inputUserNumber(userNumberStr);
+    }
+
     private static void showStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
