c80727a0e31e718ae37fb702a4ca07f8710f66ae
yshong1998 <yshong1998@gmail.com>
2023-10-24 00:31:30 +0900

    Feat(GameMessage): 게임 내 입출력 메세지를 관리하는 GameMessage 클래스 구현

--- src/main/java/baseball/global/message/GameMessage.java
+++ src/main/java/baseball/global/message/GameMessage.java
@@ -1,4 +1,17 @@
 package baseball.global.message;
 
+import baseball.global.constant.GameCode;
+import baseball.global.constant.RuleValue;
+
 public class GameMessage {
+    private static final String STRIKE = "스트라이크";
+    private static final String BALL = "볼";
+    private static final String NOTHING = "낫싱";
+    private static final String REQUEST_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다";
+    private static final String GAME_WIN_MESSAGE =
+            RuleValue.BASEBALL_NUMBER_SIZE + "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String RESTART_OR_NOT_MESSAGE =
+            "게임을 새로 시작하려면 " + GameCode.RESTART_CODE +
+                    " 종료하려면 " + GameCode.QUIT_CODE + "를 입력하세요.";
 }
