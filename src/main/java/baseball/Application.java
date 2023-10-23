cc64597baa8a784c9ce8ba449c32c794c3e85068
zeroung13 <nwj6535@gmail.com>
2023-10-23 18:22:50 +0900

    feat: 랜덤 정답 생성기 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<Integer> randomNumbers = GenerateAnswer.generate();
+        System.out.println(randomNumbers);
+
     }
 }
