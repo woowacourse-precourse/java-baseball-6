cf0943aa36d0683ad6aa6ce1bb9a7088e17d8cc8
sangwopark19 <sangwopark19@gmail.com>
2023-10-23 23:52:32 +0900

    프로그램을 시작하고 종료하는 start메서드

--- src/main/java/baseball/Interface/UserInterface.java
+++ src/main/java/baseball/Interface/UserInterface.java
@@ -13,9 +13,17 @@ public class UserInterface {
     }
 
     public void start() {
-        startGame
+        System.out.println("숫자 야구 게임을 시작합니다.");
         while (true) {
-            System.out.println();
+            startGame();
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String input = console.readLine();
+            if (input.equals("2")) {
+                break;
+            }
+            if (!input.equals("1")) {
+                throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.");
+            }
         }
     }
 
