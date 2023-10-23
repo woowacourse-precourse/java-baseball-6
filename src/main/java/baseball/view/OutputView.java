e5064c73e3a82e89becb619bb2ea383da67ce1ce
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-10-23 17:36:51 +0900

    feat(OutputView): 힌트 출력 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,7 +1,24 @@
 package baseball.view;
 
+import baseball.game.Hint;
+
 public class OutputView {
     public void printStartGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void printHint(Hint hint) {
+        int ball = 0;
+        int strike = 0;
+
+        if (ball != 0 && strike == 0) {
+            System.out.println(ball + "볼");
+        } else if (ball == 0 && strike != 0) {
+            System.out.println(strike + "스트라이크");
+        } else if (ball != 0 && strike != 0) {
+            System.out.println(ball + "볼" + strike + "스트라이크");
+        } else if (ball == 0 && strike == 0) {
+            System.out.println("낫싱");
+        }
     }
 }
