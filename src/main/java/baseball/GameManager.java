6f1464d45a7c95b7bf319137221e8ace31687d69
MinseokGo <rhalstjr1999@naver.com>
2023-10-23 23:26:44 +0900

    refactor: 컴퓨터와 플레이어 입력 비교 기능 책임 분리

--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -1,10 +1,16 @@
 package baseball;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class GameManager {
     private static final int FIXED_PLAYER_NUMBERS_LENGTH = 3;
+    private static final int NOTHING = 0;
+    private static final int INCREMENT = 1;
+    private static final int WINNING_STRIKE_COUNT = 3;
+
+
     private static final char MIN_INPUT_VALUE = '1';
     private static final char MAX_INPUT_VALUE = '9';
 
@@ -24,5 +30,39 @@ public class GameManager {
             }
             uniqueCharNumbers.add(charNumber);
         }
+    }
+
+    public static boolean compareNumbers(List<Integer> computerNumbers, String playerNumbers) {
+        int strikeCount = NOTHING;
+        int ballCount = NOTHING;
+        for (int i=0; i<playerNumbers.length(); i++) {
+            int playerNumber = playerNumbers.charAt(i) - '0';
+            int computerNumber = computerNumbers.get(i);
+            if (playerNumber == computerNumber) {
+                strikeCount += INCREMENT;
+            }
+            if (playerNumber != computerNumber && computerNumbers.contains(playerNumber)) {
+                ballCount += INCREMENT;
+            }
+        }
+
+        if (strikeCount != NOTHING && ballCount == NOTHING) {
+            System.out.println(strikeCount + "스트라이크");
+        }
+        if (strikeCount == NOTHING && ballCount != NOTHING) {
+            System.out.println(ballCount + "볼");
+        }
+        if (strikeCount != NOTHING && ballCount != NOTHING) {
+            System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+        }
+        if (strikeCount == NOTHING && ballCount == NOTHING) {
+            System.out.println("낫싱");
+        }
+
+        if(strikeCount == WINNING_STRIKE_COUNT) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return false;
+        }
+        return true;
     }
 }
