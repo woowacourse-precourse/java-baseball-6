22d0b4cbf783235cff9f03f29267b57d72dd0d2a
hong <kevin0928@naver.com>
2023-10-23 12:57:42 +0900

    refactor: BaseballService에서 랜덤 숫자 생성 코드를 ComputerService로 이전

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -1,12 +1,14 @@
 package baseball.controller;
 
 import baseball.service.BaseballService;
+import baseball.service.ComputerService;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 import java.util.List;
 
 public class BaseballController {
     private final BaseballService baseballService = new BaseballService();
+    private final ComputerService computerService = new ComputerService();
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
 
@@ -16,7 +18,7 @@ public class BaseballController {
     }
 
     public void gameStart() {
-        List<Integer> randomNumber = baseballService.createRandomNumber();
+        List<Integer> randomNumber = computerService.createRandomNumber();
         baseballService.baseballGameStart(randomNumber);
     }
 }
--- src/main/java/baseball/service/BaseballService.java
+++ src/main/java/baseball/service/BaseballService.java
@@ -1,24 +1,11 @@
 package baseball.service;
 
 import baseball.view.InputView;
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
 import java.util.List;
 
 public class BaseballService {
     private final InputView inputView = new InputView();
     private final ValidationService validationService = new ValidationService();
-
-    public List<Integer> createRandomNumber() {
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
-        }
-        return computer;
-    }
 
     public void baseballGameStart(List<Integer> integerList) {
         boolean isDifferent = true;
