0a2eac5a3583d5de72249ac66641daeb2a65ae97
kangaroo0 <ljw42871@gmail.com>
2023-10-23 23:44:22 +0900

    Feat: ComputerBallGenerator (시작할 때) 컴퓨터가 생각하는 임의의 세자리 수 만들어주기 - Randoms API를 사용하여 구현

--- src/main/java/baseball/service/BaseballService.java
+++ src/main/java/baseball/service/BaseballService.java
@@ -1,30 +1,37 @@
 package baseball.service;
 
 import baseball.util.ComputerBallGenerator;
+import baseball.util.InputCalculator;
 import baseball.view.InputView;
 import camp.nextstep.edu.missionutils.Console;
 
 public class BaseballService {
 
-    public static void gameStart(){
+    public static void gameStart() {
 
         InputView.getStartMessage();
 
+        do {
+            gamePlay();
+        } while (gameEnd());
+
+    }
+
+
+    private static void gamePlay() {
+
+        int[] computerBall = ComputerBallGenerator.generateComputerBall();
+
+
         int[] userInput = InputView.userInputNumber();
 
-    }
-
-
-    private static void gamePlay(){
-
 
 
     }
 
-    private static void gameEnd(){
+    private static boolean gameEnd() {
 
-
-
+        return false;
     }
 
 }
--- src/main/java/baseball/util/ComputerBallGenerator.java
+++ src/main/java/baseball/util/ComputerBallGenerator.java
@@ -1,5 +1,19 @@
 package baseball.util;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class ComputerBallGenerator {
+
+    public static int[] generateComputerBall() {
+
+        int[] computerBall = new int[10];
+
+        for (int x : Randoms.pickUniqueNumbersInRange(1, 9, 3)) {
+            computerBall[x]++;
+        }
+
+        return computerBall;
+
+    }
 
 }
