7e0100bf0535e5d0ac2ea7c321d1f6ed3e7e4e8b
june-777 <wlwhswnsrl96@gmail.com>
2023-10-23 13:35:10 +0900

    refactor: BaseballValidator 기능을 BaseballCollection 에 통합
    이유: 야구공의 서로 다른 1 ~ 9 숫자 검증 기능은 컬렉션 내부에서 모두 담당해야 할 역할이라고 판단함

--- src/main/java/baseball/service/BaseballCollection.java
+++ src/main/java/baseball/service/BaseballCollection.java
@@ -1,13 +1,17 @@
 package baseball.service;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class BaseballCollection {
     private final int DEFAULT_CAPACITY = 3;
+    private final int BASEBALL_START_NUMBER = 1;
+    private final int BASEBALL_END_NUMBER = 9;
     private final List<Integer> baseball;
-    private BaseballValidator baseballValidator = new BaseballValidator();
 
     public BaseballCollection(NumberGenerator numberGenerator) {
         this.baseball = createComputerBalls(numberGenerator);
@@ -19,7 +23,7 @@ public class BaseballCollection {
 
     // 기능: 사용자의 공을 입력 받고 생성한다
     private List<Integer> createPlayerBalls(String number) {
-        baseballValidator.validateBaseball(number);
+        validate(number);
         return number
                 .chars()
                 .mapToObj(Character::getNumericValue)
@@ -68,5 +72,50 @@ public class BaseballCollection {
             }
         }
         return ballCount;
+    }
+
+    private void validate(String number) {
+        validateLength(number, DEFAULT_CAPACITY);
+        validateNumeric(number);
+        validateUnique(number);
+        validateRange(number, BASEBALL_START_NUMBER, BASEBALL_END_NUMBER);
+    }
+
+    // 기능: length 자릿수로 입력해야 한다
+    private void validateLength(String input, int length) {
+        if (input.length() != length) {
+            throw new IllegalArgumentException(length + " 자릿수로 입력해야 합니다.");
+        }
+    }
+
+    // 기능: 숫자만 입력해야 한다
+    private void validateNumeric(String input) {
+        boolean onlyNumeric = Pattern.matches("^[\\d]*$", input);
+        if (!onlyNumeric) {
+            throw new IllegalArgumentException("숫자만 입력해야 합니다.");
+        }
+    }
+
+    // 기능: 서로 다른 숫자로만 입력해야 한다
+    private void validateUnique(String input) {
+        Set<Character> uniqueNums = new HashSet<>();
+        for (int numberIdx = 0; numberIdx < input.length(); numberIdx++) {
+            uniqueNums.add(input.charAt(numberIdx));
+        }
+        if (uniqueNums.size() != 3) {
+            throw new IllegalArgumentException("서로 다른 숫자만 입력해야 합니다.");
+        }
+    }
+
+    private void validateRange(String baseBalls, int start, int end) {
+        for (char baseball : baseBalls.toCharArray()) {
+            if (!isInRange(start, end, baseball - '0')) {
+                throw new IllegalArgumentException(String.format("%d~%d 범위의 숫자만 허용됩니다.", start, end));
+            }
+        }
+    }
+
+    private boolean isInRange(int start, int end, int baseball) {
+        return start <= baseball && baseball <= end;
     }
 }
