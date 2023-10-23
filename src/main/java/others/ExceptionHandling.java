d6ae76d2c94cefd5cef280be4f5804a8fbde74c3
Seungjida <esg0107@pusan.ac.kr>
2023-10-23 22:15:14 +0900

    Feat: "예측 숫자에 대한 예외 처리 기능들을 묶은 기능 추가"

--- src/main/java/others/ExceptionHandling.java
+++ src/main/java/others/ExceptionHandling.java
@@ -3,6 +3,13 @@ package others;
 public class ExceptionHandling {
     private static final int numberOfNumbers = 3;
 
+    public static void digitsExceptionTesting(final String strNumber) {
+        isNull(strNumber);
+        isThree(strNumber);
+        isDigits(strNumber);
+        isDifferent(strNumber);
+    }
+
     private static void isNull(final String strNumber) {
         if (strNumber.isBlank()) {
             throw new IllegalArgumentException("문자열이 비어있거나 빈 공백으로 이뤄져 있습니다. 게임을 종료합니다.");
