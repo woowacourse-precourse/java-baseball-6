d0ff49b735bde1f9e407477e867219cb3694f728
hee9841 <lee98417716@gmail.com>
2023-10-24 05:54:04 +0900

    :sparkles: add Computer Constructor and Methods
    - Computer Constructor 추가
    - computer_number getter 추가

--- src/main/java/baseball/service/handler/Computer.java
+++ src/main/java/baseball/service/handler/Computer.java
@@ -6,9 +6,29 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 
 public class Computer {
+    String computer_number; //컴퓨터의 random number
 
+    /**
+     * Computer Constructor : Create computer number
+     *
+     * @param start_range : startRange
+     * @param end_range   : endRange
+     */
+    public Computer(int start_range, int end_range) {
+        this.computer_number = getRandomNumber(start_range, end_range);
+    }
 
-    String getRandomNumber(int start_range, int end_range) {
+    /**
+     * Get random number
+     * <p>
+     * 상대방(컴퓨터)의 랜덤수
+     * </p>
+     *
+     * @param start_range :시작 범위
+     * @param end_range   : 끝나는 범위
+     * @return String : random number
+     */
+    private String getRandomNumber(int start_range, int end_range) {
         StringBuilder number = new StringBuilder();
 
         while (number.length() < NUMBER_OF_DIGITS) {
@@ -21,4 +41,8 @@ public class Computer {
         return number.toString();
     }
 
+
+    public String getComputerNumber() {
+        return this.computer_number;
+    }
 }
