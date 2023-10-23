6c07fdf6686b6e3d249b6f8cbc80b896d48f0eb5
임강호 <55732952+Icln@users.noreply.github.com>
2023-10-23 22:58:15 +0900

    fix: 필요하지 않는 상수 제거

--- src/main/java/baseball/PitchRange.java
+++ src/main/java/baseball/PitchRange.java
@@ -3,9 +3,7 @@ package baseball;
 public enum PitchRange {
     MIN_STRIKES(0),
     MAX_STRIKES(3),
-    MIN_BALLS(0),
-    MAX_BALLS(3);
-
+    MIN_BALLS(0);
     private final int value;
 
     PitchRange(int value) {
