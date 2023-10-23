cf66defaf10a784c15752a125df851490c0c281a
hunko <rhwlgns4386@naver.com>
2023-10-24 04:22:31 +0900

    feat(Application) : add execution logic
    Add execution logic for Application main

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import baseball.manager.BaseBallGameManager;
+import baseball.manager.GameManager;
+import baseball.ui.ConsoleInput;
+import baseball.ui.ConsoleOutput;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameManager gameManager = new BaseBallGameManager(new ConsoleInput(),
+                new ConsoleOutput());
+        gameManager.execute();
     }
 }
