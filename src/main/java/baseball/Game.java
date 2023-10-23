a3c9741f41da280a1d1c97aa587f63d318707f93
Happeeee <cmg118@naver.com>
2023-10-23 23:56:21 +0900

    Style: 멤버 초기화 코드에 세로 공백 삽입
    연속적인 멤버 초기화에서 공백을 삽입해서 논리적 그룹 형성

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -7,9 +7,11 @@ public class Game {
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String GAME_OVER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static final String STRIKE3 = "3스트라이크";
+
     private List<Integer> computerNumber;
     private List<Integer> playerNumber;
     private boolean exit = false;
+
     private NumberGenerator numberGenerator = new NumberGenerator();
     private HintGenerator hintGenerator = new HintGenerator();
 
