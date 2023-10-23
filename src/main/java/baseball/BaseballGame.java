2e22b028430906669b8b885eb39c2abf0b71aea9
rlfrkdms1 <rlfrkdms1@naver.com>
2023-10-24 00:01:12 +0900

    feat: 게임 시작 문구 출력 기능
    게임의 시작을 알리는 문구를 출력
    (기능 목록에 구현 기능 체크)

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -6,11 +6,14 @@ public class BaseballGame {
 
     private final Computer computer;
 
+    private static final String PRINT_GAME_START = "숫자 야구 게임을 시작합니다.";
+
     public BaseballGame(Computer computer) {
         this.computer = computer;
     }
 
     public void run() {
         List<Integer> correctAnswer = computer.createCorrectAnswer();
+        System.out.println(PRINT_GAME_START);
     }
 }
