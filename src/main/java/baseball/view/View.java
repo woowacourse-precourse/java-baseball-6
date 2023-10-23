3db8afbac92c514cc9f775bc5f80a4d3da8f569c
ay-eonii <naayen@naver.com>
2023-10-23 15:28:19 +0900

    feat: 사용자로부터 숫자 입력받는 기능

--- src/main/java/baseball/view/View.java
+++ src/main/java/baseball/view/View.java
@@ -1,9 +1,17 @@
 package baseball.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class View {
     private final static String START_COMMENT = "숫자 야구 게임을 시작합니다.";
+    private final static String INPUT_COMMENT = "숫자를 입력해주세요 : ";
 
     public void printStart() {
         System.out.println(START_COMMENT);
+    }
+
+    public String inputNumber() {
+        System.out.print(INPUT_COMMENT);
+        return Console.readLine();
     }
 }
