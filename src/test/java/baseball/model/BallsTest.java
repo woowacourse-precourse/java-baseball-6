122633114b1ce640eb29bfce985638b3f611df91
Dreaming-J <kimjjong321@naver.com>
2023-10-23 15:29:09 +0900

    :test_tube: test. 3개의 야구공 중 1개 판별

--- src/test/java/baseball/model/BallsTest.java
+++ src/test/java/baseball/model/BallsTest.java
@@ -6,8 +6,8 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BallsTest {
 
@@ -24,5 +24,20 @@ public class BallsTest {
                 () -> assertThatThrownBy(() -> new Balls(List.of(1, 2))).isInstanceOf(IllegalArgumentException.class),
                 () -> assertThatThrownBy(() -> new Balls(List.of(1, 2, 2))).isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void strike() {
+        assertEquals(answer.play(new Ball(0, 4)), BallStatus.STRIKE);
+    }
+
+    @Test
+    void ball() {
+        assertEquals(answer.play(new Ball(1, 4)), BallStatus.BALL);
+    }
+
+    @Test
+    void nothing() {
+        assertEquals(answer.play(new Ball(0, 1)), BallStatus.NOTHING);
     }
 }
