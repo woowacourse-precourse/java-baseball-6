b5b5a3b808ba548a1c4d9cf66b2938db36a3353c
jifrozen0110 <answldjs1836@gmail.com>
2023-10-24 00:40:23 +0900

    refactor : BaseballMessageConst 추가

--- src/main/java/baseball/utils/NumberValidator.java
+++ src/main/java/baseball/utils/NumberValidator.java
@@ -1,17 +1,18 @@
 package baseball.utils;
 
+import baseball.domain.BaseballMessageConst;
 import java.util.HashSet;
 import java.util.List;
 
 public class NumberValidator {
     public static void isValid(List<Integer> numbers) {
         if (numbers.size() != 3) {
-            throw new IllegalArgumentException("숫자가 3개가 아닙니다.");
+            throw new IllegalArgumentException(BaseballMessageConst.NOT_BALL_SIZE_THREE);
         }
         HashSet<Integer> hashSet = new HashSet<>(numbers);
 
-        if(numbers.size() != hashSet.size()){
-            throw new IllegalArgumentException("서로 다른 숫자가 아닙니다.");
+        if (numbers.size() != hashSet.size()) {
+            throw new IllegalArgumentException(BaseballMessageConst.EXIST_BALL);
         }
     }
 }
