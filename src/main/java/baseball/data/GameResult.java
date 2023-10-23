c6fba0123ba8c755d8c526b81697b5273699a6e3
Jangjun <daki403.1@gmail.com>
2023-10-23 19:59:29 +0900

    Feat: Add GameResult for DTO
    strike와 ball에 대한 정보를 주고받기 위한 클래스 입니다.
    생성 및 getter가 가능합니다.

---
+++ src/main/java/baseball/data/GameResult.java
@@ -0,0 +1,19 @@
+package baseball.data;
+
+public class GameResult {
+    private int strike;
+    private int ball;
+
+    public GameResult(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+}
