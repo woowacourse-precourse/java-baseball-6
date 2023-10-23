ba9eecc3f8f2c340b47393771de3aee57acaf4f6
hayannn <dlgkdis801@naver.com>
2023-10-23 12:30:33 +0900

    :recycle: controller의 필드 및 초기화 위치 분리, 게임 로직 수행 분리, 불필요한 주석 제거

--- src/main/java/baseball/Controller/BaseballGameController.java
+++ src/main/java/baseball/Controller/BaseballGameController.java
@@ -10,41 +10,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BaseballGameController {
-    private Computer computer = new Computer();
-    private Player player = new Player();
+    private Computer computer;
+    private Player player;
+    private GameFlowManagementService gameFlowManagementService;
+    private GameNumberCheckService gameNumberCheckService;
+    private FirstGamePlayerInputView firstGamePlayerInputView;
+    private GameScoreView gameScoreView;
+    private List<Integer> countStrikeBall;
 
-    /* 숫자의 비교를 통한 게임 진행을 위해 변수 생성 */
-    private static final int STRIKE_COUNT_INDEX = 0; //현재 스트라이크 개수 -> 인덱스로 저장
-    private static final int STRIKE_COUNT_THREE = 3; //현재의 스트라이크 개수와 비교하기 위한 3스트라이크의 값 생성
+    private static final int STRIKE_COUNT_INDEX = 0; //현재 스트라이크 개수
+    private static final int STRIKE_COUNT_THREE = 3; //비교를 위한 3스트라이크
 
-    private final GameFlowManagementService gameFlowManagementService = new GameFlowManagementService(); //숫자 야구 게임의 흐름 및 다음 게임의 'Replay or Quit' question 담당 서비스를 연결
-    private final GameNumberCheckService gameNumberCheckService = new GameNumberCheckService(); //컴퓨터와 플레이어의 숫자 비교-'스트라이크, 볼'을 카운트하는 서비스를 연결
-    private final FirstGamePlayerInputView firstGamePlayerInputView = new FirstGamePlayerInputView(); //플레이어의 숫자 입력 실제 처리 및 저장하는 뷰를 연결
-    private final GameScoreView gameScoreView = new GameScoreView(); //숫자 야구 게임의 플레이 상황에 따른 적절한 결과를 출력하는 뷰를 연결
-    private List<Integer> countStrikeBall = new ArrayList<>(); //스트라이크와 볼의 개수 카운트하는 리스트 생성
+    public BaseballGameController() { // 필드 선언 위치와 초기화 위치 분리
+        computer = new Computer();
+        player = new Player();
+        gameFlowManagementService = new GameFlowManagementService();
+        gameNumberCheckService = new GameNumberCheckService();
+        firstGamePlayerInputView = new FirstGamePlayerInputView();
+        gameScoreView = new GameScoreView();
+        countStrikeBall = new ArrayList<>();
+    }
 
-    //게임 시작
     public void gameStart() {
         computer.setComputerNumbers();
         while (!gameFlowManagementService.gameQuit()) {
-            playerNumbers();
-            calculateCountStrikeBall();
-            getGameScore();
+            playGameRound(); // 게임 진행 로직을 별도 메서드로 분리
         }
     }
 
-    //1. 플레이어의 숫자 입력
     public void playerNumbers() {
         player.setPlayerNumbers(firstGamePlayerInputView.setPlayerNumbers());
     }
 
-    //2. 플레이어의 숫자와 컴퓨터의 숫자를 비교해 스트라이크와 볼을 계산
     public void calculateCountStrikeBall() {
         countStrikeBall = gameNumberCheckService.compareGameNumber(player.getPlayerNumbers(),
                 computer.getComputerNumbers());
     }
 
-    // 3. 인덱스해둔 스코어를 가져와 3스트라이크와 일치하는지 확인 후 -> 3스트라이크 빌생 시 -> 게임을 종료하고, 게임을 재시작할 것인지 묻는 로직으로 이어주기
     public void getGameScore() {
         if (countStrikeBall.get(STRIKE_COUNT_INDEX) == STRIKE_COUNT_THREE) {
             gameScoreView.completeThreeStrikeScore(); //3스트라이크 발생
@@ -53,5 +55,11 @@ public class BaseballGameController {
         if (countStrikeBall.get(STRIKE_COUNT_INDEX) != STRIKE_COUNT_THREE) {
             gameScoreView.incompleteThreeStrikeScore(countStrikeBall); //3스트라이크 미완성
         }
+    }
+
+    private void playGameRound() { // 분리한 메서드
+        playerNumbers();
+        calculateCountStrikeBall();
+        getGameScore();
     }
 }
