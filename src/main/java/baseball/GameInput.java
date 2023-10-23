73f4bb7525f29480fa710257f99771154e7c119e
ikjo39 <chi4321@naver.com>
2023-10-23 21:16:17 +0900

    feat: 유저 입력값 검증 기능 추가

--- src/main/java/baseball/GameInput.java
+++ src/main/java/baseball/GameInput.java
@@ -7,10 +7,35 @@ public class GameInput {
     private final String input;
 
     public GameInput() {
-        this.input = receiveUserInput();
+        String input = receiveUserInput();
+        validateInputSize(input);
+        validateInputDigit(input);
+        validateInputDuplicated(input);
+        this.input = input;
     }
 
     private String receiveUserInput() {
         return Console.readLine();
+    }
+
+    private void validateInputSize(String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateInputDigit(String input) {
+        if (!input.matches("[0-9]{3}")) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateInputDuplicated(String input) {
+        long count = input.chars()
+                .distinct()
+                .count();
+        if (count != 3) {
+            throw new IllegalArgumentException();
+        }
     }
 }
