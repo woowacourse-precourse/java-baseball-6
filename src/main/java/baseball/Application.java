49adbb557f5dfd893ecd37ad4c600035d6071bb9
Kim Chae Lim <121853238+notcherry222@users.noreply.github.com>
2023-10-23 14:16:42 +0900

    feat: setRandomDistinctNumbers 메소드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Random;
+
 public class Application {
+    public String setRandomDistinctNumbers() {
+        int number = Randoms.pickNumberInRange(1, 9);
+        return String.valueOf(number);
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
 }
