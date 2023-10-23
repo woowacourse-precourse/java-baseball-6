6747d18526a5c1c92ad664a60e4efabd82bf855e
qudwns017 <qudwns017@naver.com>
2023-10-24 04:25:27 +0900

    Feat: 볼/스트라이크/낫싱 판정하는 UmpireService 클래스 추가

---
+++ src/main/java/baseball/service/UmpireService.java
@@ -0,0 +1,46 @@
+package baseball.service;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class UmpireService {
+    private int ballCount = 0;
+    private int strikeCount = 0;
+
+    public UmpireService() {
+        initCount();
+    }
+
+    public void initCount() {
+        this.strikeCount = 0;
+        this.ballCount = 0;
+    }
+
+    public ArrayList<Integer> getResult(ArrayList<Integer> randomNumbers, ArrayList<Integer> inputNumbers) {
+        ArrayList<Integer> ballStrikeCount = new ArrayList<>();
+        ballStrikeCount.add(getStrikeCount(randomNumbers, inputNumbers));
+        ballStrikeCount.add(getBallCount(randomNumbers, inputNumbers));
+
+        return ballStrikeCount;
+    }
+
+    private int getStrikeCount(ArrayList<Integer> randomNumbers, ArrayList<Integer> inputNumbers) {
+        for (int i = 0; i < randomNumbers.size(); i++) {
+            if (Objects.equals(randomNumbers.get(i), inputNumbers.get(i))) {
+                this.strikeCount++;
+            }
+        }
+        return strikeCount;
+    }
+
+    private int getBallCount(ArrayList<Integer> randomNumbers, ArrayList<Integer> inputNumbers) {
+        for (int i = 0; i < randomNumbers.size(); i++) {
+            if (!(Objects.equals(randomNumbers.get(i), inputNumbers.get(i))) && randomNumbers.contains(
+                    inputNumbers.get(i))) {
+                this.ballCount++;
+            }
+        }
+        return ballCount;
+    }
+}
+
