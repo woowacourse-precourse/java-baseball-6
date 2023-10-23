820ec28e92028e565c446cbcbd71162444f63cdc
gkwk <124476558+gkwk@users.noreply.github.com>
2023-10-23 22:22:50 +0900

    ballCount, strikeCount 메서드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,32 @@
 package baseball;
 
+import java.util.Scanner;
+import java.util.HashMap;
+
 public class Application {
+
+    public static int ballCount(HashMap<Integer, Integer> ballHashMap) {
+        int sum = 0;
+
+        for (int ballHashMapKey : ballHashMap.keySet()) {
+            sum += ballHashMap.get(ballHashMapKey);
+        }
+        
+        return sum;
+    }
+
+    public static int strikeCount(int[] inputIntegerArray,int[] answerIntegerArray) {
+        int sum = 0;
+
+        for (int i = 0; i < 3; i++) {
+            if (inputIntegerArray[i] == answerIntegerArray[i]) {
+                sum += 1;
+            }
+        }
+
+        return sum;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
