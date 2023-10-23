f574c04e7c8044d35a5e3a4e32098b39f4f4c001
Junobee25 <wnsdhqo@naver.com>
2023-10-23 19:04:06 +0900

    test: displayBaseballGameResult 테스트 코드 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,10 +1,13 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,19 +18,36 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     private Application baseballGame;
+    private List<Integer> guess;
+    private String myNumber;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
    @BeforeEach
    public void setUp(){
        baseballGame = new Application();
+       guess = Arrays.asList(1,3,2);
+       myNumber = "123";
+       System.setOut(new PrintStream(outContent));
    }
+    @AfterEach
+    public void restoreOut() {
+        System.setOut(originalOut); // 콘솔 출력을 원래대로 복구
+    }
 
    @Test
    void 스트라이크_볼_갯수세기(){
-       List<Integer> guess = Arrays.asList(2,2,3);
-       String myNumber = "123";
        baseballGame.countStrikeAndBall(myNumber,guess);
-       Assertions.assertEquals(2,Application.strike);
+       Assertions.assertEquals(1,Application.strike);
 
    }
+
+   @Test
+   void 숫자야구_결과_테스트(){
+       baseballGame.displayBaseballGameResult(2,0);
+       String consoleOutput = outContent.toString().trim();
+       Assertions.assertEquals("2스트라이크",consoleOutput);
+
+       }
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
