954dac0852edd95a677fabae2daf04cfd04d39d5
sinminseok12 <sinminseok12@naver.com>
2023-10-24 01:19:51 +0900

    feat : 사용자가 숫자를 입력받는 기능 구현

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,4 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static baseball.Constants.*;
+
 public class User {
+
+    public static List<Integer> inputNumbers() {
+        System.out.print(INPUT_NUMBERS_MESSAGE);
+        String input = Console.readLine();
+        return parseIntegers(input);
+    }
+
+    private static List<Integer> parseIntegers(String input) {
+        return input.chars()
+                .map(Character::getNumericValue)
+                .boxed()
+                .collect(Collectors.toList());
+    }
 }
