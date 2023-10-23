4d91ec3b37f140d9922ac0d6461d055ec304299e
KimSehoon <shggm2000@gmail.com>
2023-10-24 03:53:51 +0900

    feat: printBallAndStrikeMessage method 구현
    - 볼과 스트라이크를 출력할 printBallAndStrikeMessage method 구현

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -6,11 +6,15 @@ public class View {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    public static void printNumberInsertMessage(){
+    public static void printNumberInsertMessage() {
         System.out.print("숫자를 입력해주세요 : ");
     }
 
-    public static void printNothingMessage(){
+    public static void printNothingMessage() {
         System.out.println("낫싱");
+    }
+
+    public static void printBallAndStrikeMessage(int ball, int strike) {
+        System.out.println(ball + "볼 " + strike + "스트라이크");
     }
 }
