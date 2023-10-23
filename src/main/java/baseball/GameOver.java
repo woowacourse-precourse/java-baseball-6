4a44bff213667912932e30c98bbea44909a709d5
gonixxxx <akdntm946@gmail.com>
2023-10-23 22:08:42 +0900

    feat: 게임 종료

---
+++ src/main/java/baseball/GameOver.java
@@ -0,0 +1,12 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class GameOver {
+
+    public boolean isGameOver() {
+        String command = Console.readLine();
+        return command.equals("1");
+    }
+
+}
