3e4df7f4532546e17f05d847acc5f449dd920208
snaag <snaag.dev@gmail.com>
2023-10-24 01:11:11 +0900

    refactor: (Constants) WrapperClass -> primitive type

--- src/main/java/baseball/utils/Constants.java
+++ src/main/java/baseball/utils/Constants.java
@@ -1,10 +1,10 @@
 package baseball.utils;
 
 public class Constants {
-    public static Integer INIT_VALUE = -1;
-    public static Integer ZERO = 0;
-    public static Integer BALL_MAX = 3;
-    public static Integer RANGE_END = 10;
+    public static int INIT_VALUE = -1;
+    public static int ZERO = 0;
+    public static int BALL_MAX = 3;
+    public static int RANGE_END = 10;
 
     public static String GAME_FLAG_START = "1";
     public static String GAME_FLAG_END = "2";
