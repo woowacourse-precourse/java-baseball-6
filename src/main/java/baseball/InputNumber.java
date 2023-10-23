1f55dcabb502adec6200cad5056baeb912ad092b
Sinorang <aodkrak@naver.com>
2023-10-24 01:46:48 +0900

    숫자 입력받는 클래스 구현
    - 플레이어(사용자)로부터 값을 입력받는 클래스 구현
    - 플레이어로부터 값을 입력받는 메소드 구현
    - String 타입으로 입력받은 값을 3개로 분리하여 int 타입으로 변환하여 List<Integer> 타입으로 반환해줌

---
+++ src/main/java/baseball/InputNumber.java
@@ -0,0 +1,18 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputNumber {
+    public List<Integer> playerNumber() {
+        System.out.println("숫자를 입력해주세요 : ");
+        List<Integer> playerNum = new ArrayList<>();
+        String inputNum = Console.readLine();
+
+        for (String number : inputNum.split("")) {
+            playerNum.add(Integer.parseInt(number));
+        }
+        return playerNum;
+    }
+}
