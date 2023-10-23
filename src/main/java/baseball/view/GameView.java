8d10a74ec17ab99485c864b9d430b24f9e4c2603
Ukj0ng <xb253q@gmail.com>
2023-10-23 23:20:55 +0900

    fix: GameView 검증 로직 제거

--- src/main/java/baseball/view/GameView.java
+++ src/main/java/baseball/view/GameView.java
@@ -1,5 +1,6 @@
 package baseball.view;
 
+import baseball.validator.InputValidator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class GameView {
@@ -8,7 +9,7 @@ public class GameView {
     public String getPlayerInput() {
         System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
-        validatePlayNumber(input);
+        InputValidator.validatePlayNumber(input);
 
         return input;
     }
@@ -16,7 +17,7 @@ public class GameView {
     public int gameContinue() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String input = Console.readLine();
-        validateGameContinue(input);
+        InputValidator.validateGameContinue(input);
 
         return Integer.parseInt(input);
     }
@@ -43,37 +44,5 @@ public class GameView {
         else {
             System.out.println(ball + "볼 " + strike + "스트라이크");
         }
-    }
-
-    private void validatePlayNumber(String input) {
-        if (!isInteger(input) || !isThreeDigit(input)) {
-            throw new IllegalArgumentException("잘못된 입력값입니다.");
-        }
-    }
-
-    private void validateGameContinue(String input) {
-        if (!isInteger(input) || !isOneOrTwo(input)) {
-            throw new IllegalArgumentException("잘못된 입력값입니다.");
-        }
-    }
-
-    private boolean isInteger(String input) {
-        try {
-            int number = Integer.parseInt(input);
-            return true;
-        } catch (IllegalArgumentException e) {
-            System.out.println("잘못된 입력값입니다.");
-        }
-        return false;
-    }
-
-    private boolean isThreeDigit(String input) {
-        int number = Integer.parseInt(input);
-        return number >= 100 && number <= 999;
-    }
-
-    private boolean isOneOrTwo(String input) {
-        int number = Integer.parseInt(input);
-        return number == 1 || number == 2;
     }
 }
