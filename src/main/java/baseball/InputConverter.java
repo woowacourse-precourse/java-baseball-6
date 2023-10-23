db229a87f5e45be8f5061671ddad81c8c7150ee1
opixxx <opix0306@naver.com>
2023-10-23 21:25:30 +0900

    feat: 예외 처리 기능 구현
    player 가 Console.readLine() 을 통해 input 값을 convert 에 넘기면 유효성 검사, 예외 처리를 하는 기능 구현

--- src/main/java/baseball/InputConverter.java
+++ src/main/java/baseball/InputConverter.java
@@ -1,7 +1,9 @@
 package baseball;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class InputConverter {
 
@@ -10,7 +12,25 @@ public class InputConverter {
         List<Integer> result = new ArrayList<>();
 
         for (char ch : player.toCharArray()) {
-            result.add(Integer.parseInt(String.valueOf(ch)));
+            try {
+                result.add(Integer.parseInt(String.valueOf(ch)));
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("숫자를 입력하세요");
+            }
+        }
+
+        if (result.contains(0)) {
+            throw new IllegalArgumentException("1~9 까지의 숫자를 입력하세요");
+        }
+
+        if (result.size() != 3) {
+            throw new IllegalArgumentException("서로 다른 3자리 수를 입력하세요.");
+        }
+
+        Set<Integer> resultSet = new HashSet<>(result);
+
+        if (resultSet.size() != 3) {
+            throw new IllegalArgumentException("서로 다른 3자리 수를 입력하세요.");
         }
         return result;
     }
