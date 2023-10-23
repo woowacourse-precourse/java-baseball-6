9544762309b4cd2280b528adfbe7bebe8e6b2c0a
nylonmusk <2912rla@gmail.com>
2023-10-24 02:03:15 +0900

    refactor : 정규표현식 추가

--- src/main/java/baseball/constant/Regex.java
+++ src/main/java/baseball/constant/Regex.java
@@ -1,5 +1,17 @@
 package baseball.constant;
 
 public enum Regex {
+    ONE_OR_TWO("[12]"),
+    ONE_TO_NINE("^[1-9]{3}$");
 
+    private final String regex;
+
+    Regex(String regex) {
+        this.regex = regex;
+    }
+
+    public boolean matches(String input) {
+        return input.matches(regex);
+    }
 }
+
