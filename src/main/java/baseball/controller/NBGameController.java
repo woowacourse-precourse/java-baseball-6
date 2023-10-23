8a4b55d4690f20a299e768d3ad31963cb42a2994
baksakcci <pshsh910@gmail.com>
2023-10-24 01:06:03 +0900

    refactor: displayList() 메서드 제거 및 주석 제거

--- src/main/java/baseball/controller/NBGameController.java
+++ src/main/java/baseball/controller/NBGameController.java
@@ -30,19 +30,15 @@ public class NBGameController {
     }
 
     public void run() {
-        // 게임 시작 메세지 출력하기
         out.displayStartMessage();
         int restart = RESTART_GAME;
-
         while (restart == RESTART_GAME) {
-            // 숫자 생성하기
             Computer computer = Computer.generateRandomNumber();
-            out.displayList(computer.getComputerNumber());
 
             startGame(computer);
-            // 게임 종료 메세지 출력하기
+
             out.displayEndMessage();
-            // 다시 시작 여부 묻기
+
             out.displayRestart();
             restart = Integer.parseInt(in.inputStringNumber());
             restartNumberValidation.validateNumber(restart);
@@ -51,26 +47,19 @@ public class NBGameController {
 
     private void startGame(Computer computer) {
         int strike = 0;
-        // 게임 반복하기
         while (is3Strike(strike)) {
-            // 사용자로부터 console 입력 받기
             String user = in.inputStringNumber();
 
-            // 숫자 검증하기
             numberValidation.validateNumber(user);
 
-            // 숫자 타입 변경하기
             List<Integer> userIntList = numberTypeConvertor.toListInteger(user);
 
-            // 숫자 야구 시작하기
             BaseballCount baseballCount = new BaseballCount();
             List<Integer> comIntList = computer.getComputerNumber();
             Map<String, Integer> pitched = baseballCount.pitch(userIntList, comIntList);
 
-            // 결과 출력하기
             out.displayResult(pitched.get("strike"), pitched.get("ball"));
 
-            // update strike
             strike = pitched.get("strike");
         }
     }
