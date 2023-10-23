d9eaa2606f64254e644be2698caa8d7488d89d53
masonkimseoul <masonkimseoul@gmail.com>
2023-10-23 15:55:44 +0900

    feat : (Application.java) Add user input and exception checking function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,7 +10,7 @@ public class Application {
 
     }
 
-    static List<Integer> chooseRandomNumbers() {
+    private List<Integer> chooseRandomNumbers() {
         List<Integer> computerNumbers = new ArrayList<>();
 
         while (computerNumbers.size() < 3) {
@@ -20,5 +21,28 @@ public class Application {
         }
 
         return computerNumbers;
+    }
+
+    private int getNumbers() {
+        String userString = Console.readLine();
+        int userNumbers;
+
+        try {
+            userNumbers = Integer.parseInt(userString);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자를 입력해주세요.");
+        }
+
+        if (userString.length() != 3) {
+            throw new IllegalArgumentException("3자리의 수를 입력해주세요.");
+        }
+
+        if (userString.charAt(0) == userString.charAt(1)
+                || userString.charAt(1) == userString.charAt(2)
+                || userString.charAt(0) == userString.charAt(2)) {
+            throw new IllegalArgumentException("서로 다른 3자리의 수를 입력해주세요.");
+        }
+
+        return userNumbers;
     }
 }
