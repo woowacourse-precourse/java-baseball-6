566535a9b3e0bbce57684f80f7a5b9d5a5b8980c
minsub <chrkb1569@naver.com>
2023-10-24 00:40:25 +0900

    refactor : 로그 출력 구문 메서드화

--- src/main/java/baseball/game/Game.java
+++ src/main/java/baseball/game/Game.java
@@ -1,43 +1,63 @@
 package baseball.game;
 
-import baseball.checker.BaseBallChecker;
-import baseball.factory.NumberFactory;
 import camp.nextstep.edu.missionutils.Console;
 
+import static baseball.checker.BaseBallChecker.*;
 import static baseball.checker.InputValueChecker.*;
+import static baseball.factory.NumberFactory.*;
 import static baseball.message.GameDefaultMessage.*;
 import static baseball.message.GameResultMessage.*;
 
 // 게임 진행을 위한 클래스
 public class Game {
     public static void startGame() {
-        System.out.println(GAME_START.getMessage());
+        printGameStartMessage();
 
         String inputValue;
-        String targetNumber = NumberFactory.getRandomNumber();
+        String targetNumber = getRandomNumber();
 
         while(true) {
-            System.out.print(INPUT_VALUE.getMessage());
+            printInputValueFromUserMessage();
             inputValue = Console.readLine();
             checkNumberValidation(inputValue);
 
-            int resultCode = BaseBallChecker.checkGameResult(inputValue, targetNumber);
+            int resultCode = checkGameResult(inputValue, targetNumber);
 
-            System.out.println(getMessageByCode(resultCode));
+            printGameResultMessage(resultCode);
 
-            if(resultCode != STRIKE_3.getCode()) continue;
+            if(resultCode != getTargetCode()) continue;
 
-            System.out.println(GAME_END.getMessage());
-            System.out.println(INPUT_SIGNAL.getMessage());
+            printGameEndMessage();
+            printInputSignalFromUserMessage();
 
             String userSignal = Console.readLine();
             checkSignalValidation(userSignal);
 
             if(userSignal.equals("2")) break; // 사용자가 2를 입력할 경우, 게임 종료
 
-            targetNumber = NumberFactory.getRandomNumber();
+            targetNumber = getRandomNumber();
         }
 
         Console.close();
+    }
+
+    private static void printGameStartMessage() {
+        System.out.println(GAME_START.getMessage());
+    }
+
+    private static void printInputValueFromUserMessage() {
+        System.out.print(INPUT_VALUE.getMessage());
+    }
+
+    private static void printGameResultMessage(int resultCode) {
+        System.out.println(getMessageByCode(resultCode));
+    }
+
+    private static void printGameEndMessage() {
+        System.out.println(GAME_END.getMessage());
+    }
+
+    private static void printInputSignalFromUserMessage() {
+        System.out.println(INPUT_SIGNAL.getMessage());
     }
 }
