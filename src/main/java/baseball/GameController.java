979b6869effdff5b3adbc9a3a3038596a4bb5445
j-yong98 <chlwodyd9829@gmail.com>
2023-10-24 00:56:58 +0900

    rename: Game -> GameController 변경

---
+++ src/main/java/baseball/GameController.java
@@ -0,0 +1,30 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class GameController {
+    private final int SIZE = 3;
+    private final int START_INCLUSIVE = 1;
+    private final int END_INCLUSIVE = 9;
+    private final int RESTART = 1;
+    private final int GAME_OVER = 2;
+
+    Play play = new Play();
+
+    public void start() {
+        View.start();
+        do {
+            play.setPlay(SIZE, START_INCLUSIVE, END_INCLUSIVE);
+            play.run();
+        } while (restart());
+    }
+
+    private boolean restart() {
+        View.restart();
+        int result = Integer.parseInt(Console.readLine());
+        if (result != RESTART && result != GAME_OVER) {
+            throw new IllegalArgumentException("bad input!!!");
+        }
+        return result == RESTART;
+    }
+}
