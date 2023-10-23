0d0a2545b925f4eeb26bdeec783c2fcfdfedae32
devhoya97 <diajbh123@gmail.com>
2023-10-23 22:03:59 +0900

    style : BaseballNumber에서 오버라이딩 한 메서드에 대한 Javadoc 추가 오버라이딩 한 메서드에 대한 Javadoc을 추가하면, 왜 이 메서드들을 오버라이딩 했는지 독자 입장에서 도움이 될 것 같아 추가했습니다.

--- src/main/java/baseball/model/BaseballNumber.java
+++ src/main/java/baseball/model/BaseballNumber.java
@@ -30,6 +30,16 @@ public class BaseballNumber {
         }
     }
 
+    /**
+     * baseballNumbers 필드를 사용하여 객체 간의 동등성을 확인합니다.
+     *
+     * <p>숫자 야구 게임에서는 두 BaseballNumber 객체를 비교하여 동일성 여부를 판단하는 동작이 게임의 핵심입니다.
+     * 이 메서드는 두 객체가 동일한 baseballNumber 필드 값을 가지면 같다고 판단합니다.</p>
+     *
+     * @param object 비교할 대상 객체
+     * @return 객체와 대상이 가진 baseballNumber 필드 값이 같은지 여부를 나타내는 판단
+     */
+
     @Override
     public boolean equals(Object object) {
         if (!(object instanceof BaseballNumber)) {
@@ -37,6 +47,15 @@ public class BaseballNumber {
         }
         return baseballNumber == ((BaseballNumber) object).baseballNumber;
     }
+
+    /**
+     * hashCode 메서드를 baseballNumber 필드를 활용하여 오버라이딩합니다.
+     *
+     * <p>이 메서드는 현재 숫자야구 게임에서 사용되지 않습니다. 하지만 equals 메서드를 오버라이딩할 때에는
+     * hashCode 메서드도 오버라이딩하여 equals 메서드와 일관된 동작을 보장하는 것이 좋습니다.</p>
+     *
+     * @return 객체의 baseballNumber 필드를 기반으로 생성된 해시코드
+     */
 
     @Override
     public int hashCode() {
