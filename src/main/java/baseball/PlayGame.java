9ae54261a4778ea32496b897c017aad80cb8a632
hiidy <hiidy115@gmail.com>
2023-10-23 14:58:39 +0900

    :recycle: refact: 난수 생성 기능과 입력값 메서드 리팩토링 -입력값을 받는 메서드명은 startGame이 의도를 잘 드러낸다고 생각해서 변경. -게임 시작 메서드에서 make answer 메서드를 사용하기 위해서 static으로 변경.

--- src/main/java/baseball/PlayGame.java
+++ src/main/java/baseball/PlayGame.java
@@ -9,12 +9,12 @@ public class PlayGame {
     private int ball;
     private int strike;
 
-    public void userInput() {
+    public void startGame() {
         System.out.println("숫자를 입력해주세요 : ");
         String originalInput = Console.readLine();
         validateInput(originalInput);
         ArrayList<Integer> userNumberList = inputToArrayList(originalInput);
-
+        ArrayList<Integer> answerNumbers = GenerateNum.makeAnswer();
     }
 
     public ArrayList<Integer> inputToArrayList(String numInput) {
