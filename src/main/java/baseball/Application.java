c7ae8cbf750e8f8ac4fba9980c0c7da8eac0421d
Minsu Choi <cms102510@naver.com>
2023-10-23 20:06:01 +0900

    feat: 게임 시작 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,19 +1,13 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
-import java.util.List;
+import baseball.controller.GameController;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
-        }
+
+        GameController game = new GameController();
+
+        game.start();
+
     }
 }
