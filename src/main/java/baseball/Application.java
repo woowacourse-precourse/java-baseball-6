c343f2d29eb5e76a0cd352e53b37093d65c33fce
nylonmusk <2912rla@gmail.com>
2023-10-24 04:44:13 +0900

    refactor : 프로그램 시작시 객체생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,23 @@
 package baseball;
 
-import baseball.controller.GameInitializer;
+import baseball.controller.Controller;
+import baseball.domain.Baseball;
+import baseball.domain.Computer;
+import baseball.domain.Player;
+import baseball.view.InputView;
+import baseball.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        GameInitializer gameInitializer = new GameInitializer();
-        gameInitializer.startGame();
+        Baseball baseball = new Baseball();
+        Computer computer = new Computer();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        Player player = new Player();
+
+        Controller controller = new Controller(baseball, computer, inputView, outputView, player);
+
+        outputView.startGame();
+        controller.playGame();
     }
 }
