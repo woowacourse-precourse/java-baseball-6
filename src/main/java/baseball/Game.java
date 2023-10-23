c470879617562d3c06b527517c5585fb8a70c5e4
fanta4715 <fanta4715@naver.com>
2023-10-23 21:43:09 +0900

    feat: 게임을 시작하고 반복하는 기능 추가
    Game 객체에서 Computer객체의 게임을 실행시키고, 조건에 따라 반복시키는 함수를 구현했습니다.

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,20 @@
+package baseball;
+
+public class Game {
+
+    private Computer computer;
+
+    //Computer를 주입 받는 생성자
+    public Game(Computer computer) {
+        this.computer = computer;
+    }
+
+    //게임을 시작하고 반복하는 기능
+    public void start() {
+        while (true) {
+            boolean shouldRestartGame = computer.play();
+            if (!shouldRestartGame) break;
+        }
+    }
+
+}
