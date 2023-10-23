45d252c2fe4fda3eb3f689126ab319088d2fe61b
wook <dong930112@gmail.com>
2023-10-24 00:30:06 +0900

    refactor(NumberController): 'generateComputerNumberList' 메서드 구현 야구게임의 컴퓨터정답값을 만드는 메서드

--- src/main/java/baseball/NumberController.java
+++ src/main/java/baseball/NumberController.java
@@ -1,12 +1,24 @@
 package baseball;
 
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class NumberController {
+
+  public List<Integer> generateComputerNumberList() {
+    List<Integer> computer = new ArrayList<>();
+    while (computer.size() < 3) {
+      int randomNumber = Randoms.pickNumberInRange(1, 9);
+      if (!computer.contains(randomNumber)) {
+        computer.add(randomNumber);
+      }
+    }
+    return computer;
+  }
 
     public List<Integer> splitter(int num) {
       List<Integer> numList = new ArrayList<>(3);
