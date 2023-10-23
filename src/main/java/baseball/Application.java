de5af1361698f1f6ecf21daf4be8348bb043beac
YunSeok-Choi <dbstjr9898@naver.com>
2023-10-23 21:56:06 +0900

    FEAT : 숫자입력받기 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +20,22 @@ public class Application {
 
         String computerNumber = String.join("",computer);
 
+        System.out.print("숫자를 입력해 주세요 : ");
+
+        String user = Console.readLine();
+        if (user.length() != 3) {
+            throw new IllegalArgumentException("3개의 값을 입력해주세요");
+        }
+        if (!user.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException("숫자만 입력해주세요");
+        }
+        String tmp = "";
+        for (char c : user.toCharArray()) {
+            if (tmp.equals(String.valueOf(c))) {
+                throw new IllegalArgumentException("중복되지 않는 숫자로 입력해주세요");
+            }
+            tmp += String.valueOf(c);
+        }
 
     }
 }
