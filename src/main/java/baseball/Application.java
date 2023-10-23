498f156bfa9b0b916afe2e5df6237a5870e30c11
Tiger <skdlghwns@gmail.com>
2023-10-23 21:02:03 +0900

    입력 유효판별기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,11 +8,13 @@ import java.util.*;
 public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        int gameState = 1;//1:게임진행, 2:게임종료
-        List<Integer> computer = new ArrayList<>();//컴퓨터 수 컨테이너 할당
+        String gameState = "1";//1:게임진행, 2:게임종료
+        List<Integer> computer = new ArrayList<>();//컴퓨터 수
+        String input;//유저 입력
+        int ball=0; int strike=0;//볼, 스트라이크 개수
 
         //게임 시작
-        while(gameState==1){
+        while(gameState.equals("1")){
             //1.상대방 수 생성
             while (computer.size() < 3){
                 int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -23,9 +25,26 @@ public class Application {
             System.out.println(computer.toString());
             //2.3자리수 입력 시작
             while(true){
-                String in = Console.readLine();
-                System.out.println(in);
+                System.out.printf("숫자를 입력해주세요 : ");
+                input = Console.readLine();
+                if(isValidInput(input)){
+                    System.out.println("유효");
+                } else {
+                    System.out.println("무효");
+                }
             }
         }
+    }
+    //2. 사용자 입력이 유효한지 확인하는 메소드
+    public static boolean isValidInput(String input){
+        if(input == null || input.length()!=3){
+            return false;
+        }
+        try {
+            int n = Integer.parseInt(input);
+        } catch (NumberFormatException e){
+            return false;
+        }
+        return true;
     }
 }
