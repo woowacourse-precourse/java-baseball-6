f273844a95748bd6565c17eb1568fe36fbf3595e
jum0624 <jum0624@naver.com>
2023-10-24 04:43:51 +0900

    refactor: 랜덤 숫자 생성 코드 리펙터링
    랜덤 숫자 크기를 상수 값으로 고정

--- src/main/java/process/RandomNumber.java
+++ src/main/java/process/RandomNumber.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RandomNumber {
+    private final int RANDOM_NUMBER_SIZE = 3;
     private RandomNumber() {
     }
 
@@ -16,7 +17,7 @@ public class RandomNumber {
 
     public List<Integer> getRandomNumber() {
         List<Integer> computerNumber = new ArrayList<>();
-        while (computerNumber.size() < 3) {
+        while (computerNumber.size() < RANDOM_NUMBER_SIZE) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computerNumber.contains(randomNumber)) {
                 computerNumber.add(randomNumber);
