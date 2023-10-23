42f451df1d7b6a132951658cff75097dadf4e3d3
taehoon <xogns1514@gmail.com>
2023-10-23 09:41:19 +0900

    refactor:야구 게임 한개 진행 메서드 분리
    야구 게임 실행 메서드에서 게임 하나를 진행하는 메서드를 분리했다.

--- src/main/java/baseball/entity/BaseBallGame.java
+++ src/main/java/baseball/entity/BaseBallGame.java
@@ -4,46 +4,39 @@ public class BaseBallGame {
     private final UserInput userInput;
     private final Random random;
     private final Hint hint;
-    private boolean correct = false;
-    
+
     public BaseBallGame(UserInput userInput, Random random, Hint hint) {
         this.userInput = userInput;
         this.random = random;
         this.hint = hint;
     }
 
-
     // 야구 게임 시작 함수
     public void run() {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        String randomNumber = null;
 
         // 재시작을 하지 않을 때까지 반복
+        do {
+            playGame();
+        } while (isRestart());
+    }
+
+    private void playGame() {
+        String randomNumber = random.generateRandomNumber();
+
         while (true) {
-            correct = false;
-            // 숫자를 맞출때 까지 반복
-            while (!correct) {
-                // 랜덤값 생성 및 사용자 입력받기
-                if(randomNumber == null) random.generateRandomNumber();
+            // 사용자 입력받기
+            String inputNumber = userInput.receiveInputNumber();
 
-                randomNumber = random.getRandomNumber();
-                String inputNumber = userInput.receiveInputNumber();
+            // 힌트 생성
+            hint.generateHint(randomNumber, inputNumber);
 
-                // 힌트 생성
-                hint.generateHint(randomNumber, inputNumber);
-
-                // 랜덤값과 동일한 숫자를 입력하면 게임 종료
-                if (isCorrect(randomNumber, inputNumber)) {
-                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                    break;
-                }
+            // 랜덤값과 동일한 숫자를 입력하면 게임 종료
+            if (isCorrect(randomNumber, inputNumber)) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
             }
-            // 게임 재시작 입력 받기
-            if(!isRestart()) break;
-            randomNumber = null;
         }
-
-
     }
 
     // 게임 재시작 여부 확인
