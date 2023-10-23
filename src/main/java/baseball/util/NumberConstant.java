bbd065e436ddfabab044231b790ddcce7069e51b
koomchang <leahpar0401@gmail.com>
2023-10-23 14:29:36 +0900

    feat: Number와 관련된 중복되는 상수들을 하나의 클래스에서 static으로 생성

---
+++ src/main/java/baseball/util/NumberConstant.java
@@ -0,0 +1,9 @@
+package baseball.util;
+
+public class NumberConstant {
+
+    public static final int MIN_VALUE = 1;
+    public static final int MAX_VALUE = 9;
+    public static final int LENGTH = 3;
+
+}
