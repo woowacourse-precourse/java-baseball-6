9073bfde47888bce933617baa0aff91f7b9e81b8
penrose15 <seongju99@naver.com>
2023-10-23 23:06:44 +0900

    Feat: add GameController

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,21 @@
 package baseball;
 
+import baseball.controller.GameController;
+import baseball.global.converter.NumbersConverter;
+import baseball.global.view.input.InputView;
+import baseball.global.view.output.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        OutputView outputView = new OutputView();
+        InputView inputView = new InputView();
+        NumbersConverter numbersConverter = new NumbersConverter();
+
+        GameController gameController = new GameController(inputView, outputView, numbersConverter);
+
+        gameController.run();
+
+
     }
 }
