fc9a1abf51e60d0c9035bb9c12c66a4a24e32daa
yumin1209 <55646526+yumin1209@users.noreply.github.com>
2023-10-23 20:22:18 +0900

    feat : 사용자 숫자 입력 기능 추가

---
+++ src/main/java/baseball/RandomNum.java
@@ -0,0 +1,25 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNum {
+    public List<Integer> RandomNumber() {
+
+        List<Integer> comNum = new ArrayList<>();
+        while (comNum.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!comNum.contains(randomNumber)) {
+                comNum.add(randomNumber);
+
+            }
+
+        }
+        return comNum;
+
+    }
+
+
+}
