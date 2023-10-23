207f7b84c52f16cada717a296da7ba750c63706a
gyu_hyeok <jjookki33@gmail.com>
2023-10-23 16:37:50 +0900

    feat/랜덤한 세자리 숫자 생성 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,29 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+//      List<Integer> computer = createComputerNumber();
+//      System.out.println(computer);
     }
+
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+    private static final int BASEBALL_SIZE = 3;
+
+    private static List<Integer> createComputerNumber(){
+        List<Integer> numbers = new ArrayList<>();
+        while(numbers.size() < BASEBALL_SIZE){
+            int randomNumber = Randoms.pickNumberInRange(MIN_NUMBER,MAX_NUMBER);
+            if(!numbers.contains(randomNumber))
+                numbers.add(randomNumber);
+        }
+        return numbers;
+    }
+
 }
