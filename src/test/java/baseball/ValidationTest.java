0f920e6822be645a91f33838a7506f58e947415b
seonghoo1217 <seonghoo1217@naver.com>
2023-10-23 16:24:57 +0900

    style : add displayname in test method

--- src/test/java/baseball/ValidationTest.java
+++ src/test/java/baseball/ValidationTest.java
@@ -2,7 +2,7 @@ package baseball;
 
 import baseball.validation.GameValidation;
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -12,6 +12,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class ValidationTest extends NsTest {
 
     @ParameterizedTest
+    @DisplayName("사용자 게임 입력 값 예외 테스트")
     @ValueSource(strings = {"1234","12","1 3","12a","222","012",""})
     void 사용자_게임_입력_값_예외_테스트(String input_value){
         assertThatThrownBy(()->GameValidation.verifyForRetryValue(input_value))
@@ -20,6 +21,7 @@ public class ValidationTest extends NsTest {
     }
 
     @ParameterizedTest
+    @DisplayName("사용자 게임 재도전 여부 입력 값 예외 테스트")
     @ValueSource(strings = {"3","12",""," ","a"})
     void 사용자_게임_재도전_여부_입력_값_예외_테스트(String input_retry){
         assertThatThrownBy(()->GameValidation.verifyForRetryValue(input_retry))
