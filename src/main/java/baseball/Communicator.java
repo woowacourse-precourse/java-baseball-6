20e6915365cd7cff77c61648e329d8800aea3e2e
Jay <injaesong0@gmail.com>
2023-10-24 05:43:13 +0900

    feat: add Nothing for printResult

--- src/main/java/baseball/Communicator.java
+++ src/main/java/baseball/Communicator.java
@@ -6,11 +6,15 @@ import java.util.List;
 import java.util.Map;
 
 public class Communicator {
+    //---Messages---
     private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String USER_WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String MENU_SELECTION_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static final String TERMINATE_MESSAGE = "시스템을 종료합니다.";
+    private static final String NOTHING = "낫싱";
+    //---------
+
     private final Validator validator;
 
     public Communicator(Validator validator) {
@@ -46,6 +50,10 @@ public class Communicator {
         String strikeMark = Count.STRIKE.getCountMarkFrom(strikeCount);
 
         System.out.println(ballMark + strikeMark);
+
+        if (ballCount == 0 && strikeCount == 0) {
+            System.out.println(NOTHING);
+        }
 
         if (strikeCount == 3) {
             System.out.println(USER_WIN_MESSAGE);
