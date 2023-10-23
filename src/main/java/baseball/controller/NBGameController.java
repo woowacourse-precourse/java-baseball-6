3d0e909d17c39071e68dfc5b71471adadac6d999
baksakcci <pshsh910@gmail.com>
2023-10-23 22:03:48 +0900

    feat: 타입 변경 및 스트라이크 판단 로직 Controller 추가
    - 통합 테스트(프로그램 테스트) 완료

--- src/main/java/baseball/controller/NBGameController.java
+++ src/main/java/baseball/controller/NBGameController.java
@@ -1,9 +1,13 @@
 package baseball.controller;
 
 import baseball.model.Computer;
+import baseball.model.BaseballCount;
+import baseball.model.convertor.NumberTypeConvertor;
 import baseball.model.validator.NumberValidation;
 import baseball.view.InputView;
 import baseball.view.OutputView;
+import java.util.List;
+import java.util.Map;
 
 public class NBGameController {
     public void start() {
@@ -11,14 +15,27 @@ public class NBGameController {
         OutputView out = new OutputView();
         // 게임 시작 메세지 출력하기
         out.displayString("숫자 야구 게임을 시작합니다.");
+
         // 숫자 생성하기
         Computer computer = Computer.generateRandomNumber();
         out.displayList(computer.getComputerNumber());
+
         // 사용자로부터 console 입력 받기
         String user = in.inputString();
         out.displayString(user);
+
         // 숫자 검증하기
         NumberValidation numberValidation = new NumberValidation();
         numberValidation.validateNumber(user);
+
+        // 숫자 타입 변경하기
+        NumberTypeConvertor numberTypeConvertor = new NumberTypeConvertor();
+        List<Integer> userIntList = numberTypeConvertor.toListInteger(user);
+
+        // 숫자 야구 시작하기
+        BaseballCount baseballCount = new BaseballCount();
+        List<Integer> comIntList = computer.getComputerNumber();
+        Map<String, Integer> pitched = baseballCount.pitch(userIntList, comIntList);
+        pitched.forEach((k, v) -> System.out.print(k + " " + v));
     }
 }
