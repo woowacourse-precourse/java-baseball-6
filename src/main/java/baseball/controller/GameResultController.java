e84cfed463685a73981a1c9ec5e41ed637d19018
mksystem98 <mksystem98@gmail.com>
2023-10-23 22:37:55 +0900

    feat : 볼, 스트라이크 낫싱 계산 추가
    2스트라이크가 나와야 하는데 0볼 2스트라이크가 나옴으로 수정

--- src/main/java/baseball/controller/GameResultController.java
+++ src/main/java/baseball/controller/GameResultController.java
@@ -16,15 +16,32 @@ public class GameResultController {
             }
         }
 
+        return generateResultMessage(ballCount, strikeCount);
+    }
+
+    private String generateResultMessage(int ballCount, int strikeCount) {
+        StringBuilder result = new StringBuilder();
+
         if(strikeCount == 3) {
-            return "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
-                    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+            return "3 스트라이크";
         }
 
-        if(strikeCount == 0 && ballCount == 0) {
+        if(ballCount > 0) {
+            result.append(ballCount).append("볼");
+        }
+
+        if(ballCount > 0 && strikeCount > 0) {
+            result.append(" ");
+        }
+
+        if(strikeCount > 0) {
+            result.append(strikeCount).append("스트라이크");
+        }
+
+        if(ballCount == 0 && strikeCount == 0) {
             return "낫싱";
         }
 
-        return ballCount + "볼 " + strikeCount + "스트라이크";
+        return result.toString();
     }
 }
