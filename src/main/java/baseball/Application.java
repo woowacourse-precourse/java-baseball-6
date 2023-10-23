468223ab2aab1ad593338a85ed48fb8ce8505890
minnim1010 <pms1010123@daum.net>
2023-10-24 00:23:09 +0900

    feat(controller): 숫자 야구 게임의 정답 숫자 생성 로직 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import baseball.controller.BaseballController;
+import baseball.service.BaseballService;
 
 public class Application {
     public static void main(String[] args) {
@@ -8,6 +9,7 @@ public class Application {
     }
 
     private static BaseballController createBaseballController() {
-        return new BaseballController();
+        BaseballService baseballService = new BaseballService();
+        return new BaseballController(baseballService);
     }
 }
--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -1,11 +1,20 @@
 package baseball.controller;
 
+import baseball.model.Baseball;
+import baseball.service.BaseballService;
+
 public class BaseballController {
+
+    private final BaseballService baseballService;
+
+    public BaseballController(BaseballService baseballService) {
+        this.baseballService = baseballService;
+    }
 
     public void run() {
         boolean start = true;
         while (start) {
-
+            Baseball answer = baseballService.createAnswerBaseball();
             playGame();
             start = isRestart();
         }
