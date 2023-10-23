7482a9f2a544e17a3798624954d23168355f8a1a
olsohee <dlthgml0108@daum.net>
2023-10-23 15:30:57 +0900

    feat: OutputView 생성

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,19 @@
+package baseball.view;
+
+public class OutputView {
+
+    public static void start() {
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void exit() {
+
+        System.out.println("게임을 종료합니다.");
+    }
+
+    public static void askExit() {
+
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+}
