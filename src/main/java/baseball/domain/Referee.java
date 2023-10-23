69d0c006f71529be2116b7d248f3bdba4eda4668
prao <pch5521@gmail.com>
2023-10-23 13:06:43 +0900

    refactor: 심판 기능 내 중복 코드 제거 및 메서드 분리
    - 스트라이크가 3인지 확인하는 boolean 메서드 isThreeStrike()와 judge()에 동일한 코드를 count() 메서드로 분리하여 중복 코드 제거
    - ball과 strike의 개수를 판별하여 출력하는 printJudgment() 내의 로직을 각각 조건별로 분리하여 복잡도를 낮춤

--- src/main/java/baseball/domain/Referee.java
+++ src/main/java/baseball/domain/Referee.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 public class Referee {
     private int ball;
-
     private int strike;
 
     public Referee() {
@@ -14,17 +13,16 @@ public class Referee {
 
     public boolean isThreeStrike(List<Integer> computerNumbers, List<Integer> userNumbers) {
         strike = 0;
-        for (int i = 0; i < userNumbers.size(); i++) {
-            int computerNumber = computerNumbers.get(i);
-            int userNumber = userNumbers.get(i);
-            if (isStrike(computerNumber, userNumber)) {
-                strike += 1;
-            }
-        }
+        count(computerNumbers, userNumbers);
         return strike == 3;
     }
 
     public void judge(List<Integer> computerNumbers, List<Integer> userNumbers) {
+        count(computerNumbers, userNumbers);
+        printJudgment(ball, strike);
+    }
+
+    private void count(List<Integer> computerNumbers, List<Integer> userNumbers) {
         for (int i = 0; i < userNumbers.size(); i++) {
             int computerNumber = computerNumbers.get(i);
             int userNumber = userNumbers.get(i);
@@ -35,7 +33,6 @@ public class Referee {
                 ball += 1;
             }
         }
-        printJudgement(ball, strike);
     }
 
     private boolean isStrike(int computerNumber, int userNumber) {
@@ -46,16 +43,32 @@ public class Referee {
         return computerNumbers.contains(userNumber);
     }
 
-    private void printJudgement(int ball, int strike) {
+    private void printJudgment(int ball, int strike) {
+        nothing(ball, strike);
+        onlyBall(ball, strike);
+        onlyStrike(ball, strike);
+        ballStrike(ball, strike);
+    }
+
+    private void nothing(int ball, int strike) {
         if (ball == 0 && strike == 0) {
             System.out.println("낫싱");
         }
-        if (ball == 0 && strike != 0) {
-            System.out.println(strike + "스트라이크");
-        }
+    }
+
+    private void onlyBall(int ball, int strike) {
         if (ball != 0 && strike == 0) {
             System.out.println(ball + "볼");
         }
+    }
+
+    private void onlyStrike(int ball, int strike) {
+        if (ball == 0 && strike != 0) {
+            System.out.println(strike + "스트라이크");
+        }
+    }
+
+    private void ballStrike(int ball, int strike) {
         if (ball != 0 && strike != 0) {
             System.out.println(ball + "볼 " + strike + "스트라이크");
         }
