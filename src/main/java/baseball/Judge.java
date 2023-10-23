12f0d2883c9cdaa60eedc55c65d36baecf912be2
ysparrk <youngseo0703@gmail.com>
2023-10-24 01:45:03 +0900

    feat: 입력한 숫자 판별 기능 구현

---
+++ src/main/java/baseball/Judge.java
@@ -0,0 +1,22 @@
+package baseball;
+
+import java.util.List;
+
+public class Judge {
+    CompareBalls compareBalls = new CompareBalls();
+
+    public String judgement(List<Integer> computer, List<Integer> player){
+        int total = compareBalls.countTotal(computer, player);
+        int strike = compareBalls.countStrike(computer, player);
+        int ball = total - strike;
+
+        if(total == 0){
+            return "낫싱";
+        }else if(strike == 0){
+            return ball + "볼";
+        }else if(ball == 0){
+            return strike + "스트라이크";
+        }
+        return ball + "볼 " + strike + "스트라이크";
+    }
+}
