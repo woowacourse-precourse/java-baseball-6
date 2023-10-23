3b95f3cf302bc6985f763ae38a9bdac756f6e842
iamjunhyeong <dddodo447@gmail.com>
2023-10-23 19:21:44 +0900

    # <타입> : <제목> 형식으로 작성하며 제목은 최대 50글자 정도로만 입력 # 제목을 아랫줄에 작성, 제목 끝에 마침표 금지, 무엇을 했는지 명확하게 작성 <feat>: 메인함수 구현 ################ # 본문(추가 설명)을 아랫줄에 작성 메인함수에서 BaseballGame을 호출한다. 그리고 게임 종료시 리트라이 여부도 처리함. ################ # 꼬릿말(footer)을 아랫줄에 작성 (관련된 이슈 번호 등 추가)
    ################
    # feat : 기능 추가
    # fix : 버그 수정
    # docs : 문서 수정
    # test : 테스트 코드 추가
    # refactor : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    # cicd : CI/CD 관련 설정
    ################

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import baseball.controller.BaseballGame;
+import baseball.model.Retry;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OutputView.printGameStart();
+        do {
+            BaseballGame baseballGame = new BaseballGame();
+            baseballGame.start();
+        } while (askRetry());
+    }
+
+    public static boolean askRetry() {
+        Retry retry = new Retry(InputView.setRetryNumber());
+
+        if (retry.getRetryNumber().equals("1")) {
+            return true;
+        }
+
+        return false;
     }
 }
