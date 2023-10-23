20042d12f28a6080444eb83dc4b3bf9581582fe1
Seonghwan <aiya0422@gmail.com>
2023-10-23 20:11:21 +0900

    TEST: 리팩토링에 따른 테스트 수정

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -24,7 +24,6 @@ class ApplicationTest extends NsTest {
     @Test
     void showResult() {
         // given
-        View view = new View();
         Result case1 = new Result(0, 0);
         Result case2 = new Result(0, 2);
         Result case3 = new Result(2, 0);
@@ -33,19 +32,19 @@ class ApplicationTest extends NsTest {
         // when
         OutputStream result1 = new ByteArrayOutputStream();
         System.setOut(new PrintStream(result1));
-        view.showResult(case1);
+        View.showResult(case1);
 
         OutputStream result2 = new ByteArrayOutputStream();
         System.setOut(new PrintStream(result2));
-        view.showResult(case2);
+        View.showResult(case2);
 
         OutputStream result3 = new ByteArrayOutputStream();
         System.setOut(new PrintStream(result3));
-        view.showResult(case3);
+        View.showResult(case3);
 
         OutputStream result4 = new ByteArrayOutputStream();
         System.setOut(new PrintStream(result4));
-        view.showResult(case4);
+        View.showResult(case4);
 
         // then
         assertThat(result1.toString().strip()).as("0 strike 0 ball").isEqualTo("낫싱");
@@ -58,7 +57,6 @@ class ApplicationTest extends NsTest {
     @Test
     void validateDuplication() {
         // given
-        Validation validation = new Validation();
         String case1 = "123";
         String case2 = "112";
         String case3 = "a23";
@@ -66,16 +64,16 @@ class ApplicationTest extends NsTest {
 
         // when
         Throwable result1 = catchThrowable(() -> {
-            validation.validateDuplication(case1);
+            Validation.validateDuplication(case1);
         });
         Throwable result2 = catchThrowable(() -> {
-            validation.validateDuplication(case2);
+            Validation.validateDuplication(case2);
         });
         Throwable result3 = catchThrowable(() -> {
-            validation.validateDuplication(case3);
+            Validation.validateDuplication(case3);
         });
         Throwable result4 = catchThrowable(() -> {
-            validation.validateDuplication(case4);
+            Validation.validateDuplication(case4);
         });
 
         // then
@@ -88,7 +86,6 @@ class ApplicationTest extends NsTest {
     @Test
     void validateNumber() {
         // given
-        Validation validation = new Validation();
         String case1 = "123";
         String case2 = "012";
         String case3 = "abc";
@@ -96,16 +93,16 @@ class ApplicationTest extends NsTest {
 
         // when
         Throwable result1 = catchThrowable(() -> {
-            validation.validateNumber(case1);
+            Validation.validateNumber(case1);
         });
         Throwable result2 = catchThrowable(() -> {
-            validation.validateNumber(case2);
+            Validation.validateNumber(case2);
         });
         Throwable result3 = catchThrowable(() -> {
-            validation.validateNumber(case3);
+            Validation.validateNumber(case3);
         });
         Throwable result4 = catchThrowable(() -> {
-            validation.validateNumber(case4);
+            Validation.validateNumber(case4);
         });
 
         // then
@@ -118,45 +115,48 @@ class ApplicationTest extends NsTest {
     @Test
     void validateOneOrTwo() {
         // given
-        Validation validation = new Validation();
-        int case1 = 1;
-        int case2 = 2;
-        int case3 = 5;
+        String case1 = "1";
+        String case2 = "2";
+        String case3 = "3";
+        String case4 = "a";
 
         // when
         Throwable result1 = catchThrowable(() -> {
-            validation.validateOneOrTwo(case1);
+            Validation.validateOneOrTwo(case1, 1);
         });
         Throwable result2 = catchThrowable(() -> {
-            validation.validateOneOrTwo(case2);
+            Validation.validateOneOrTwo(case2, 1);
         });
         Throwable result3 = catchThrowable(() -> {
-            validation.validateOneOrTwo(case3);
+            Validation.validateOneOrTwo(case3, 1);
+        });
+        Throwable result4 = catchThrowable(() -> {
+            Validation.validateOneOrTwo(case4, 1);
         });
 
         // then
         assertThat(result1).as("input: 1").doesNotThrowAnyException();
         assertThat(result2).as("input: 2").doesNotThrowAnyException();
         assertThat(result3).as("invalid input").isInstanceOf(IllegalArgumentException.class);
+        assertThat(result4).as("invalid input").isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void validateInputLength() {
         // given
-        Validation validation = new Validation();
         String case1 = "123";
         String case2 = "12";
         String case3 = "1234";
 
         // when
         Throwable result1 = catchThrowable(() -> {
-            validation.validateInputLength(case1, 3);
+            Validation.validateInputLength(case1, 3);
         });
         Throwable result2 = catchThrowable(() -> {
-            validation.validateInputLength(case2, 3);
+            Validation.validateInputLength(case2, 3);
         });
         Throwable result3 = catchThrowable(() -> {
-            validation.validateInputLength(case3, 3);
+            Validation.validateInputLength(case3, 3);
         });
 
         // then
@@ -168,13 +168,12 @@ class ApplicationTest extends NsTest {
     @Test
     void generateNumericInputList() {
         // given
-        Generator generator = new Generator();
         String case1 = "123";
         String case2 = "1234";
 
         // when
-        List<Integer> result1 = generator.generateNumericInputList(case1);
-        List<Integer> result2 = generator.generateNumericInputList(case2);
+        List<Integer> result1 = Generator.generateNumericInputList(case1);
+        List<Integer> result2 = Generator.generateNumericInputList(case2);
 
         // then
         assertThat(result1).as("case1").isEqualTo(List.of(1, 2, 3));
@@ -184,11 +183,10 @@ class ApplicationTest extends NsTest {
     @Test
     void generateAnswer() {
         // given
-        Generator generator = new Generator();
         int listLen = 3;
 
         // when
-        List<Integer> result = generator.generateAnswer(listLen);
+        List<Integer> result = Generator.generateAnswer(listLen);
 
         // then
         assertThat(result.size()).as("list size").isEqualTo(listLen);
@@ -203,17 +201,16 @@ class ApplicationTest extends NsTest {
     @Test
     void isNotOver() {
         // given
-        Core core = new Core();
         int case1 = 0;
         int case2 = 1;
         int case3 = 2;
         int case4 = 3;
 
         // when
-        boolean result1 = core.isNotOver(case1);
-        boolean result2 = core.isNotOver(case2);
-        boolean result3 = core.isNotOver(case3);
-        boolean result4 = core.isNotOver(case4);
+        boolean result1 = Core.isNotOver(case1);
+        boolean result2 = Core.isNotOver(case2);
+        boolean result3 = Core.isNotOver(case3);
+        boolean result4 = Core.isNotOver(case4);
 
         // then
         assertThat(result1).as("nothing").isEqualTo(true);
@@ -225,7 +222,6 @@ class ApplicationTest extends NsTest {
     @Test
     void countBall() {
         // given
-        Core core = new Core();
         List<List<Integer>> case1 = List.of(List.of(1, 2, 3), List.of(4, 5, 6)); // nothing
         List<List<Integer>> case2 = List.of(List.of(1, 2, 3), List.of(4, 1, 6)); // 1 ball
         List<List<Integer>> case3 = List.of(List.of(1, 2, 3), List.of(4, 1, 2)); // 2 ball
@@ -237,15 +233,15 @@ class ApplicationTest extends NsTest {
         List<List<Integer>> case9 = List.of(List.of(1, 2, 3), List.of(1, 2, 3)); // 3 strike
 
         // when
-        int result1 = core.countBall(case1.get(0), case1.get(1));
-        int result2 = core.countBall(case2.get(0), case2.get(1));
-        int result3 = core.countBall(case3.get(0), case3.get(1));
-        int result4 = core.countBall(case4.get(0), case4.get(1));
-        int result5 = core.countBall(case5.get(0), case5.get(1));
-        int result6 = core.countBall(case6.get(0), case6.get(1));
-        int result7 = core.countBall(case7.get(0), case7.get(1));
-        int result8 = core.countBall(case8.get(0), case8.get(1));
-        int result9 = core.countBall(case9.get(0), case9.get(1));
+        int result1 = Core.countBall(case1.get(0), case1.get(1));
+        int result2 = Core.countBall(case2.get(0), case2.get(1));
+        int result3 = Core.countBall(case3.get(0), case3.get(1));
+        int result4 = Core.countBall(case4.get(0), case4.get(1));
+        int result5 = Core.countBall(case5.get(0), case5.get(1));
+        int result6 = Core.countBall(case6.get(0), case6.get(1));
+        int result7 = Core.countBall(case7.get(0), case7.get(1));
+        int result8 = Core.countBall(case8.get(0), case8.get(1));
+        int result9 = Core.countBall(case9.get(0), case9.get(1));
 
         // then
         assertThat(result1).as("nothing").isEqualTo(0);
@@ -263,7 +259,6 @@ class ApplicationTest extends NsTest {
     @Test
     void countStrike() {
         // given
-        Core core = new Core();
         List<List<Integer>> case1 = List.of(List.of(1, 2, 3), List.of(4, 5, 6)); // nothing
         List<List<Integer>> case2 = List.of(List.of(1, 2, 3), List.of(4, 1, 6)); // 1 ball
         List<List<Integer>> case3 = List.of(List.of(1, 2, 3), List.of(4, 1, 2)); // 2 ball
@@ -275,15 +270,15 @@ class ApplicationTest extends NsTest {
         List<List<Integer>> case9 = List.of(List.of(1, 2, 3), List.of(1, 2, 3)); // 3 strike
 
         // when
-        int result1 = core.countStrike(case1.get(0), case1.get(1));
-        int result2 = core.countStrike(case2.get(0), case2.get(1));
-        int result3 = core.countStrike(case3.get(0), case3.get(1));
-        int result4 = core.countStrike(case4.get(0), case4.get(1));
-        int result5 = core.countStrike(case5.get(0), case5.get(1));
-        int result6 = core.countStrike(case6.get(0), case6.get(1));
-        int result7 = core.countStrike(case7.get(0), case7.get(1));
-        int result8 = core.countStrike(case8.get(0), case8.get(1));
-        int result9 = core.countStrike(case9.get(0), case9.get(1));
+        int result1 = Core.countStrike(case1.get(0), case1.get(1));
+        int result2 = Core.countStrike(case2.get(0), case2.get(1));
+        int result3 = Core.countStrike(case3.get(0), case3.get(1));
+        int result4 = Core.countStrike(case4.get(0), case4.get(1));
+        int result5 = Core.countStrike(case5.get(0), case5.get(1));
+        int result6 = Core.countStrike(case6.get(0), case6.get(1));
+        int result7 = Core.countStrike(case7.get(0), case7.get(1));
+        int result8 = Core.countStrike(case8.get(0), case8.get(1));
+        int result9 = Core.countStrike(case9.get(0), case9.get(1));
 
         // then
         assertThat(result1).as("nothing").isEqualTo(0);
