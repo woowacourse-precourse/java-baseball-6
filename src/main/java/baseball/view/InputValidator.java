0753fc03777f5663f8180966ddbbf35745ab5de5
kunsanglee <gosmdochee2@gmail.com>
2023-10-23 17:01:47 +0900

    refactor(InputValidator): 게임 종료 후 받은 사용자 입력 검증 제거 게임이 종료한 후 재시작할지 종료할지 사용자에게 입력을 받아서 검증하는데, 검증 하는 부분의 로직을 ContinueOrExit enum이 책임을 갖게 되어서 검증 메서드 삭제.

--- src/main/java/baseball/view/InputValidator.java
+++ src/main/java/baseball/view/InputValidator.java
@@ -4,16 +4,11 @@ package baseball.view;
 public class InputValidator {
 
     private static final int INPUT_NUMBER_LENGTH = 3;
-    private static final int CONTINUE_OR_EXIT_NUMBER_LENGTH = 1;
     private static final char MIN_NUMBER = '1';
     private static final char MAX_NUMBER = '9';
-    private static final String CONTINUE_NUMBER = "1";
-    private static final String EXIT_NUMBER = "2";
     private static final String INPUT_NUMBER_LENGTH_EXCEPTION_MESSAGE = "입력값은 3자리 수만 가능합니다.";
     private static final String INPUT_NUMBER_RANGE_EXCEPTION_MESSAGE = "입력값은 1~9 범위의 숫자만 가능합니다.";
     private static final String INPUT_NUMBER_DUPLICATED_EXCEPTION_MESSAGE = "입력값은 중복되지 않는 서로 다른 숫자만 가능합니다.";
-    private static final String CONTINUE_OR_EXIT_NUMBER_EXCEPTION_MESSAGE = "입력값은 1 또는 2 만 가능합니다.";
-    private static final String CONTINUE_OR_EXIT_NUMBER_LENGTH_EXCEPTION_MESSAGE = "입력값은 한 자리 수만 입력 가능합니다.";
 
     public static void validateBaseballNumber(String input) {
         validInputLength(input);
@@ -42,23 +37,6 @@ public class InputValidator {
                 .count();
         if (count != INPUT_NUMBER_LENGTH) {
             throw new IllegalArgumentException(INPUT_NUMBER_DUPLICATED_EXCEPTION_MESSAGE);
-        }
-    }
-
-    public static void validateContinueOrExitNumber(String input) {
-        validContinueOrExitNumberLength(input);
-        validIsContinueOrExitNumber(input);
-    }
-
-    private static void validContinueOrExitNumberLength(String input) {
-        if (input.length() != CONTINUE_OR_EXIT_NUMBER_LENGTH) {
-            throw new IllegalArgumentException(CONTINUE_OR_EXIT_NUMBER_LENGTH_EXCEPTION_MESSAGE);
-        }
-    }
-
-    private static void validIsContinueOrExitNumber(String input) {
-        if (!input.equals(CONTINUE_NUMBER) && !input.equals(EXIT_NUMBER)) {
-            throw new IllegalArgumentException(CONTINUE_OR_EXIT_NUMBER_EXCEPTION_MESSAGE);
         }
     }
 }
