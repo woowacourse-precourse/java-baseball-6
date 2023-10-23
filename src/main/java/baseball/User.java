8f00b86382af21b1a510ea5428a6100dc2ef42ae
H-server <hani103201@naver.com>
2023-10-23 17:51:19 +0900

    refactor: 사용자 입력값 예외처리 추가

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -7,7 +7,6 @@ public class User {
     private Integer userNum;
 
     private String REGEXP_USER_NUM = "^[1-9]{3}$";
-    // 사용자가 112와 같이 중복된 숫자를 입력하는 건 재량으로 봐야하나?
 
     public void setUserNum() {
         System.out.print("숫자를 입력해주세요 : ");
@@ -21,7 +20,10 @@ public class User {
     }
 
     public void checkUserNum(String input) {
-        if (input.matches(REGEXP_USER_NUM)) {
+        boolean isSameDigit1 = input.charAt(0) != input.charAt(1);
+        boolean isSameDigit2 = input.charAt(0) != input.charAt(2);
+        boolean isSameDigit3 = input.charAt(1) != input.charAt(2);
+        if (input.matches(REGEXP_USER_NUM) && isSameDigit1 && isSameDigit2 && isSameDigit3) {
             userNum = Integer.parseInt(input);
         } else {
             throw new IllegalArgumentException("잘못된 입력값입니다.");
