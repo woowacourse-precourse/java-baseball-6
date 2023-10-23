da98cb344454f510e9d5f4092fbe6a1de1303bc9
FL <refrain2k@gmail.com>
2023-10-23 18:01:07 +0900

    feat: 컴퓨터의 값을 생성하고 저장하는 클래스 구현

---
+++ src/main/java/baseball/ComputerNumber.java
@@ -0,0 +1,25 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComputerNumber {
+    List<Integer> computerNumbers;
+
+    public ComputerNumber() {
+        makeRandomNumber();
+    }
+
+    public void makeRandomNumber() {
+        List<Integer> randoms = new ArrayList<>();
+        while (randoms.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!randoms.contains(randomNumber)) {
+                randoms.add(randomNumber);
+            }
+        }
+        computerNumbers = randoms;
+    }
+}
