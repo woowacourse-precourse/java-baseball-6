2a60925ac5edcff50be314e596d57846de64fcba
KJBig <kjgi73k@gmail.com>
2023-10-23 21:30:07 +0900

    feat: Game (게임) 기능 구현
    - 게임이 플레이어, 컴퓨터, 심판을 생성하여 갖는 구조 구현.
    - 게임이 종료된 후 재시작 기능 구현.

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,42 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Game {
+
+    private final Computer computer;
+    private final Player player;
+    private final Judge judge;
+
+    public Game() {
+        this.computer = new Computer();
+        this.player = new Player();
+        this.judge = new Judge();
+    }
+
+    public boolean start() {
+
+        boolean isFinished = false;
+
+        while (!isFinished) {
+            System.out.print("숫자를 입력해주세요 : ");
+            player.enterNumber();
+            isFinished = judge.judgmentFinish(player, computer);
+        }
+
+        return checkRestart();
+    }
+
+    private boolean checkRestart() {
+        boolean isReStarted = true;
+
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String s = Console.readLine();
+
+        if (s.equals("2")) {
+            isReStarted = false;
+        }
+
+        return isReStarted;
+    }
+}
