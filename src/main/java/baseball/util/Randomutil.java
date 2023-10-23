3c4721e2d089082560b9b3c33099aab3999f5dd5
5jeong <wjdgns4385@naver.com>
2023-10-23 21:35:17 +0900

    [Refactor] : 랜덤수 생성 반환형 리팩토링

--- src/main/java/baseball/util/Randomutil.java
+++ src/main/java/baseball/util/Randomutil.java
@@ -1,16 +1,15 @@
 package baseball.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.LinkedList;
 
 public class Randomutil {
 
-    public static LinkedList<Integer> CreateRandomNum() {
-        LinkedList<Integer> randomNumComputer = new LinkedList<>();
-        while (randomNumComputer.size() < 3) {
-            int randomNum = Randoms.pickNumberInRange(1, 9);
+    public static String CreateRandomNum() {
+        String randomNumComputer = "";
+        while (randomNumComputer.length() < 3) {
+            String randomNum = String.valueOf(Randoms.pickNumberInRange(1, 9));
             if (!randomNumComputer.contains(randomNum)) {
-                randomNumComputer.add(randomNum);
+                randomNumComputer += randomNum;
             }
         }
         return randomNumComputer;
