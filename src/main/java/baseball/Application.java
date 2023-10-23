91403da676347e70807a089a2bbac776ccef25c3
Kim Chae Lim <121853238+notcherry222@users.noreply.github.com>
2023-10-23 20:51:51 +0900

    fix: setRandomDistinctNumbers() 메소드 로직 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,25 +1,32 @@
 package baseball;
 
-import javax.security.auth.callback.CallbackHandler;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
 
-//    public String setRandomDistinctNumbers() {
-//        List<String> computer = new ArrayList<>();
-//        while (computer.size() < 3) {
-//            int randomNumber = Randoms.pickNumberInRange(1, 9);
-//            if (!computer.contains(randomNumber)) {
-//                computer.add(String.valueOf(randomNumber));
-//            }
-//        } return computer;
-//    }
+    public String setRandomDistinctNumbers() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        StringBuilder number = new StringBuilder();
+        for(int num : computer) {
+            number.append(num);
+        }
+        return number.toString();
+    }
 
     public String startGame() {
         return "숫자 야구 게임을 시작합니다.";
     }
-    //TODO : convertToList 메소드 만들기
+
     public List<Character> convertToList(String str) {
         List<Character> list = new ArrayList<>();
         for(int i = 0; i<str.length(); i++){
@@ -57,6 +64,8 @@ public class Application {
     }
 
     public String showBaseballOutcome(String answer, String input) {
+        System.out.println("숫자를 입력해주세요 : ");
+        checkInputValidation(input);
         int strikeCnt = countStrike(answer, input);
         int ballCnt = countBall(answer, input);
 
@@ -68,7 +77,11 @@ public class Application {
         }
         ballCnt -= strikeCnt;
         if (ballCnt == 0) {
-            return strikeCnt + "스트라이크";
+            if(strikeCnt == 3){
+                return "3스트라이크"+"3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+            }else{
+                return strikeCnt + "스트라이크";
+            }
         } else {
             return ballCnt + "볼 " + strikeCnt + "스트라이크";
         }
@@ -76,10 +89,7 @@ public class Application {
 
     public void confirmGameEnd(String choice, String answer, String input) {
 
-        System.out.println("3스트라이크");
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-
         switch (choice){
             case "1" ->{showBaseballOutcome(answer,input); }
             case "2" ->{break;}
@@ -104,6 +114,11 @@ public class Application {
     }
 
     public static void main(String[] args) {
+        Application application = new Application();
+        application.startGame();
+        String answer = application.setRandomDistinctNumbers();
+        String input = Console.readLine();
+        application.showBaseballOutcome(answer, input);
 
     }
 }
