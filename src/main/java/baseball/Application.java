f3a57dd2b863c5650f3b4aa890007e6fa748a7ae
KiminSon <kimin0331@naver.com>
2023-10-23 21:31:15 +0900

    refactor: 리팩터링 및 코드 스타일 수정
    코드 컨벤션에 따른 코드 스타일 수정 및 리팩터링

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -20,36 +20,49 @@ public class Application {
     }
 
     public static void findInputNumberError(String inputNum) {
-        if (inputNum.length() != 3) throw new IllegalArgumentException();
-
-        for (int i = 0; i < 3; i++) {
-            int num = inputNum.charAt(i);
-            if (num < '1' || num > '9') throw new IllegalArgumentException();
+        if (inputNum.length() != 3) {
+            throw new IllegalArgumentException();
         }
 
-        char num1 = inputNum.charAt(0);
-        char num2 = inputNum.charAt(1);
-        char num3 = inputNum.charAt(2);
-        if (num1 == num2 || num2 == num3 || num3 == num1) throw new IllegalArgumentException();
+        boolean[] duplicateBall = new boolean[10];
+        for (int i = 0; i < 3; i++) {
+            int num = inputNum.charAt(i) - '0';
+            if (num < 1 || num > 9 || duplicateBall[num]) {
+                throw new IllegalArgumentException();
+            }
+            duplicateBall[num] = true;
+        }
     }
 
     public static boolean findBaseballNumber(List<Integer> answerNum, String inputNum) {
-        int ballCount = 0, strikeCount = 0;
-
         boolean[] findBall = new boolean[10];
-        for (int i = 0; i < 3; i++)
-            findBall[answerNum.get(i)] = true;
-
         for (int i = 0; i < 3; i++) {
-            int num = inputNum.charAt(i) - '0';
-            if (answerNum.get(i).equals(num)) strikeCount++;
-            else if (findBall[num]) ballCount++;
+            findBall[answerNum.get(i)] = true;
         }
 
-        if (ballCount == 0 && strikeCount == 0) System.out.print("낫싱");
-        if (ballCount != 0) System.out.print(ballCount + "볼");
-        if (ballCount != 0 && strikeCount != 0) System.out.print(" ");
-        if (strikeCount != 0) System.out.print(strikeCount + "스트라이크");
+        int ballCount = 0;
+        int strikeCount = 0;
+        for (int i = 0; i < 3; i++) {
+            int num = inputNum.charAt(i) - '0';
+            if (num == answerNum.get(i)) {
+                strikeCount++;
+            } else if (findBall[num]) {
+                ballCount++;
+            }
+        }
+
+        if (ballCount == 0 && strikeCount == 0) {
+            System.out.print("낫싱");
+        }
+        if (ballCount != 0) {
+            System.out.print(ballCount + "볼");
+        }
+        if (ballCount != 0 && strikeCount != 0) {
+            System.out.print(" ");
+        }
+        if (strikeCount != 0) {
+            System.out.print(strikeCount + "스트라이크");
+        }
         System.out.print("\n");
 
         return strikeCount == 3;
@@ -69,8 +82,7 @@ public class Application {
             String inputNumber = convertInputError();
             findInputNumberError(inputNumber);
 
-            boolean isGameClear = findBaseballNumber(computer, inputNumber);
-            if (isGameClear) {
+            if (findBaseballNumber(computer, inputNumber)) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 break;
             }
@@ -84,8 +96,11 @@ public class Application {
 
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             String replayGame = convertInputError();
-            if (replayGame.equals("2")) break;
-            else if (!replayGame.equals("1")) throw new IllegalArgumentException();
+            if (replayGame.equals("2")) {
+                break;
+            } else if (!replayGame.equals("1")) {
+                throw new IllegalArgumentException();
+            }
         }
     }
 }
