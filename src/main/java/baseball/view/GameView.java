12ed1fdddd40612271a05bb6baf41d6dd38e38ea
seohyun kwon <seohyunkwon98@gmail.com>
2023-10-24 07:28:48 +0900

    Feat: add GameView.java

--- src/main/java/baseball/view/GameView.java
+++ src/main/java/baseball/view/GameView.java
@@ -1,5 +1,21 @@
 package baseball.view;
 
-public class GameView {
+import baseball.utils.Constants;
 
+public class GameView {
+    public void startGame(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void isContinue(){
+        System.out.println("게임을 새로 시작하려면 " + Constants.RETRY + ", 종료하려면 " + Constants.EXIT + "를 입력하세요.");
+    }
+
+    public void exitGame(){
+        System.out.println("게임 종료");
+    }
+
+    public void inputNumber(){
+        System.out.print("숫자를 입력해주세요 : ");
+    }
 }
