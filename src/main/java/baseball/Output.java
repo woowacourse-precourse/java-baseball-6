6e6199c05aff167d5ef91b974e089d233ff40a64
gyulim5 <ohrbfla1004@khu.ac.kr>
2023-10-23 17:29:53 +0900

    ✨ Feat: 출력 함수 및 메세지 추가

--- src/main/java/baseball/Output.java
+++ src/main/java/baseball/Output.java
@@ -1,4 +1,28 @@
 package baseball;
 
 public class Output {
+
+    private static final String startingMessage = "숫자 야구 게임을 시작합니다.";
+    private static final String inputNumberMessage = "숫자를 입력해주세요 : ";
+    private static final String gameEndMessage = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String gameRestartMessage = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    private static final String BALL = "볼";
+    private static final String STRIKE = "스트라이크";
+    private static final String NOTHING = "낫싱";
+
+    public void printResult(int ball, int strike, int nothing) {
+        if (nothing > 0) {
+            System.out.println(NOTHING);
+        }
+        else if (ball > 0 && strike > 0) {
+            System.out.println(ball+BALL+" "+strike+STRIKE);
+        }
+        else if (ball > 0) {
+            System.out.println(ball+BALL);
+        }
+        else if (strike > 0) {
+            System.out.println(strike+STRIKE);
+        }
+    }
 }
