c21919f626221eadfb91435f1eea0eb557161b9c
seonghoo1217 <seonghoo1217@naver.com>
2023-10-23 18:43:42 +0900

    refactor : modify method name cause, validation test case is all

--- src/test/java/baseball/ValidationTest.java
+++ src/test/java/baseball/ValidationTest.java
@@ -12,18 +12,18 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class ValidationTest extends NsTest {
 
     @ParameterizedTest
-    @DisplayName("사용자 게임 입력 값 예외 테스트")
+    @DisplayName("사용자 게임 입력 값 예외 통합 테스트")
     @ValueSource(strings = {"1234","12","1 3","12a","222","012",""})
-    void 사용자_게임_입력_값_예외_테스트(String input_value){
+    void 사용자_게임_입력_값_예외_통합_테스트(String input_value){
         assertThatThrownBy(()->GameValidation.verifyForRetryValue(input_value))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[Err]");
     }
 
     @ParameterizedTest
-    @DisplayName("사용자 게임 재도전 여부 입력 값 예외 테스트")
+    @DisplayName("사용자 게임 재도전 여부 입력 값 예외 통합 테스트")
     @ValueSource(strings = {"3","12",""," ","a"})
-    void 사용자_게임_재도전_여부_입력_값_예외_테스트(String input_retry){
+    void 사용자_게임_재도전_여부_입력_값_예외_통합_테스트(String input_retry){
         assertThatThrownBy(()->GameValidation.verifyForRetryValue(input_retry))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[Err]");
