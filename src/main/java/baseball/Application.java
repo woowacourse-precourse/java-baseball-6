1829e3b325501881704890c1c1bed62dd9de65a6
leemiyeon <lemingul7@gmail.com>
2023-10-23 19:19:41 +0900

    refactor: 중첩 생성 변수 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,21 @@
 package baseball;
 
+import baseball.entity.Ball;
+import baseball.entity.GameResult;
+import baseball.entity.Strike;
+
 public class Application {
     public static void main(String[] args) {
-        Controller controller = new Controller(new Model(), new View());
+
+        Strike strike = new Strike();
+        Ball ball = new Ball();
+        GameResult gameResult = new GameResult(strike, ball, false);
+
+        Model model = new Model(gameResult);
+
+        View view = new View();
+
+        Controller controller = new Controller(model, view);
         controller.startGame();
     }
 }
