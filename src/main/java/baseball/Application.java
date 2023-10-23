a8c945b46f104c62d1b8e78772dd376c119a865b
space <enaenen@naver.com>
2023-10-23 15:43:21 +0900

    FEAT: User input process

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -44,6 +44,9 @@ public class Application {
 	}
 
 	public static void main(String[] args) {
+
+		final int INPUT_LIMIT = 3;
+
 		System.out.println("숫자 야구 게임을 시작합니다.");
 
 		// Computer Randomized Number Generate
