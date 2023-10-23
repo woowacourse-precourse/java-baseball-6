984861677bec89887b24b611b9b36a60b8eb76b7
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-10-23 17:27:52 +0900

    feat(InputView): 숫자 입력 메시지 출력 기능 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,2 +1,27 @@
-package baseball.view;public class InputView {
+package baseball.view;
+
+import baseball.game.GameNumbers;
+import baseball.utils.Parser;
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public GameNumbers readUserNumbers() {
+        System.out.println("숫자를 입력해주세요 : ");
+        String userInput = readLineWithoutGap();
+        validateIsDigit(userInput);
+
+        return new GameNumbers(Parser.stringToList(userInput));
+    }
+
+    private String readLineWithoutGap() {
+        return Console.readLine().trim();
+    }
+
+    private void validateIsDigit(String userInput) {
+        try {
+            Integer.parseInt(userInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
