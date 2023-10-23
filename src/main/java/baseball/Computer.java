3275280fa9758851d950b37cd2d2ffc99e9b4a1d
HongYeseul <ghd8119@naver.com>
2023-10-23 21:02:25 +0900

    feat: 컴퓨터가 1에서 9까지 서로 다른 임의의 수(랜덤) 3개 선택

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,4 +1,19 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Computer {
+    List<Integer> computer = new ArrayList<>();
+
+    public void selectRandomNumber(){
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+    }
 }
