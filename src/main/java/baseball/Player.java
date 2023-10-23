f917c8a14135c7e0e332f20e24c592100a68b92c
euniiiii <hee9920@naver.com>
2023-10-23 17:05:34 +0900

    feat: 게임 재시작/종료 입력 유효성 확인 메서드 구현

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -66,4 +66,9 @@ public class Player {
         else if (strike > 0 && ball > 0) System.out.println(ball + "볼" + " " + strike + "스트라이크");
     }
 
+    public static void isOneOrTwo(String input) {
+        if (!input.equals("1") && !input.equals("2"))
+            throw new IllegalArgumentException("1 또는 2만 입력해야합니다.");
+    }
+
 }
