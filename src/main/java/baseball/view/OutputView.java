4ddcffca581a6e3f3aadf8b24194274a486148a3
seondays <agseon9@gmail.com>
2023-10-23 15:41:57 +0900

    feat: 게임 관련 출력메시지 구현

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,13 @@
+package baseball.view;
+
+
+public class OutputView {
+    public static final String STARTING_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public static final String INPUT_MESSAGE = "숫자를 입력해지주세요 : ";
+    public static final String STRIKE_MESSAGE = "스트라이크";
+    public static final String BALL_MESSAGE = "볼";
+    public static final String NOTHING_MESSAGE = "낫싱";
+    public static final String CONTINUE_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String EXIT_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+}
