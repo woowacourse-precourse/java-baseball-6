401fd851810365858c3b662539031e16245e9fa2
Sinorang <aodkrak@naver.com>
2023-10-24 02:19:05 +0900

    게임 실행 클래스 구현
    - 게임 실행 클래스 구현
    - 각각의 클래스로 나누어 구현했던 부분을 객체로 호출
    - while 반복문 조건에 String 타입으로 선언한 result 변수의 문자열이 "3스트라이크"라는 문자열과 동일할 때까지 반복하는 반복문 구현
    - 게임 재시작 여부를 확인하기 위해 재시작 메소드 호출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,24 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        
+        GenerateRandomNumber randomNumber = new GenerateRandomNumber();
+        InputNumber inputNumber = new InputNumber();
+        JudgementResult judgementResult = new JudgementResult();
+        RestartGame restartGame = new RestartGame();
+        boolean restart = true;
+
+        while (restart) {
+            List<Integer> computer = randomNumber.create();
+            String result = "";
+            while (!result.equals("3스트라이크")) {
+                result = judgementResult.judgementResult(computer, inputNumber.playerNumber());
+                System.out.println(result);
+            }
+            restart = restartGame.restartGame();
+        }
     }
 }
