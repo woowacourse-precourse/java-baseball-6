c522288ac3a211f34c7e6077acb695f77a7a6a72
junho <ssmm0205@naver.com>
2023-10-24 04:31:51 +0900

    refactor: 입력 출력 기술 조합해 콘솔 객체로 변경

---
+++ src/main/java/baseball/view/Console.java
@@ -0,0 +1,51 @@
+package baseball.view;
+
+import baseball.view.constant.GameResult;
+
+import java.text.MessageFormat;
+
+public class Console {
+
+    private final Input input;
+    private final Output output;
+
+
+    public Console(Input input, Output output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    public String getNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+        return input.read();
+    }
+
+    public String getRestartCode() {
+        return input.read();
+    }
+
+    public void printStartMessage() {
+        output.print("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void printEndMessage() {
+        output.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public void printRestartMessage() {
+        output.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    public void printGameResult(int strikeCount, int ballCount) {
+        GameResult result = GameResult.calculate(strikeCount, ballCount);
+
+        switch (result) {
+            case NOTHING -> output.print("낫싱");
+            case BALL -> output.print(ballCount + "볼");
+            case STRIKE -> output.print(strikeCount + "스트라이크");
+            case OTHER -> output.print(MessageFormat.format("{0}볼 {1}스트라이크", ballCount, strikeCount));
+        }
+
+    }
+
+}
