6d667ebbaa2b4adb98e72b1e8c2c175c0a9bdfb8
saaut <saaut60@naver.com>
2023-10-23 22:48:26 +0900

    랜덤 수 생성 구현

--- src/main/java/baseball/Controller/NumberController.java
+++ src/main/java/baseball/Controller/NumberController.java
@@ -1,4 +1,24 @@
 package baseball.Controller;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 public class NumberController {
+    static final int NUMOFDIGIT = 3;
+
+    public static Integer[] createRandomNumber(){
+
+        Set<Integer> uniqueNumbers = new HashSet<>();
+
+        while (uniqueNumbers.size() < NUMOFDIGIT) {
+            int randomNumbers = pickNumberInRange(1,9);
+            uniqueNumbers.add(randomNumbers);
+        }
+
+        // Set return
+        return uniqueNumbers.toArray(new Integer[0]);
+
+    }
 }
--- src/main/java/baseball/View/BaseballGame.java
+++ src/main/java/baseball/View/BaseballGame.java
@@ -1,8 +1,12 @@
 package baseball.View;
 
+import baseball.Controller.NumberController;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class BaseballGame {
+    NumberController numbers=new NumberController();
+
 
     Integer[] userNumbers={};
     Integer[] randomNumbers={};
@@ -10,7 +14,7 @@ public class BaseballGame {
         //게임 전체 루프
         while (true) {
             System.out.println("숫자 야구 게임을 시작합니다.");
-
+            randomNumbers=numbers.createRandomNumber();
 
         }
     }
