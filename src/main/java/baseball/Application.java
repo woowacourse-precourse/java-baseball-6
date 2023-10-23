a9d84be2eb60bb3ad90ce2882e40cf33eb847738
su-giana <giananews@gmail.com>
2023-10-23 13:52:56 +0900

    Oct. 23 (baseball) : 1차 제출을 위한 커밋

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -68,7 +68,7 @@ public class Application {
             if(ball>0)
             {
                 System.out.print(ball + "볼");
-                if(strike>0)    System.out.println(" ");
+                if(strike>0)    System.out.print(" ");
             }
             if(strike>0)    System.out.print(strike+"스트라이크");
             if(ball == 0 && strike == 0)    System.out.print("낫싱");
