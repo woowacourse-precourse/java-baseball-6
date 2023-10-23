7dd7920db957832d33cb8ef8620dc2cc43a81e5c
minkyuu <minkyu023@gmail.com>
2023-10-23 23:06:36 +0900

    feat: main() 로직 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
+import baseball.game.Game;
+import baseball.message.Message;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println(Message.FIRST_GAME_START);
+        do {
+            Game game = new Game();
+            game.start();
+            System.out.println(Message.GAME_END);
+        } while (Console.readLine().equals("1"));
     }
 }
