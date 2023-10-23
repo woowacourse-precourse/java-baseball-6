62649212e8e042964fa18ef979f04aff1d1071df
mini1115 <kmini9611@naver.com>
2023-10-23 16:07:37 +0900

    숫자야구 요구사항 - 정답 시 게임 종료(다시시작,완전종료)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,7 +9,8 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        while (true) {
+        boolean game = true;
+        while (game) {
             List<Integer> computer = new ArrayList<>();
             while (computer.size() < 3) {
                 int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -17,7 +18,7 @@ public class Application {
                     computer.add(randomNumber);
                 }
             }//컴퓨터 숫자 생성 while문
-            while (true) {
+            while (game) {
                 System.out.println("숫자 야구 게임을 시작합니다.");
                 System.out.print("숫자를 입력해주세요 : ");
                 String num = String.valueOf(Console.readLine());
@@ -29,21 +30,28 @@ public class Application {
                 }
                 List<Integer> result = baseball(computer, me);
                 //스트라이크,볼,낫싱 판별//
-                if(result.get(0).equals(0) && result.get(1).equals(0)){
+                if (result.get(0).equals(0) && result.get(1).equals(0)) {
                     System.out.println("낫싱");
-                }
-                else{
-                    if(result.get(0).equals(0)){
-                        System.out.println(result.get(1)+"볼");
+                } else {
+                    if (result.get(0).equals(0)) {
+                        System.out.println(result.get(1) + "볼");
                     } else if (result.get(1).equals(0)) {
-                        if(result.get(0).equals(3)){
-                            System.out.println("3스트라이크");
-                        }else {
-                            System.out.println(result.get(0)+"스트라이크");
+                        if (result.get(0).equals(3)) {
+                            System.out.println("3스트라이크\n" +
+                                    "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+                                    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                            int data = Integer.parseInt(Console.readLine());
+                            if (data == 2) {
+                                game = false;
+                            } else {
+                                break;
+                            }
+                        } else {
+                            System.out.println(result.get(0) + "스트라이크");
                         }
 
-                    }else {
-                        System.out.println(result.get(1)+"볼 "+result.get(0)+"스트라이크");
+                    } else {
+                        System.out.println(result.get(1) + "볼 " + result.get(0) + "스트라이크");
                     }
                 }
             }//사용자 입력 while문
@@ -53,9 +61,9 @@ public class Application {
     }//메인함수
 
     public static List<Integer> baseball(List<Integer> com, List<Integer> me) {
-        List<Integer> result = new ArrayList<>(Arrays.asList(0,0));
-        int strike= 0;
-        int ball =0;
+        List<Integer> result = new ArrayList<>(Arrays.asList(0, 0));
+        int strike = 0;
+        int ball = 0;
         for (int i = 0; i < 3; i++) {
             if (com.get(i).equals(me.get(i))) {
                 strike += 1;
