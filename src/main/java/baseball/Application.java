7bd1ee183a68ec997dad1c727db5f1e683764129
myminimin <triplem0604@kakao.com>
2023-10-23 22:21:05 +0900

    [Feat] 1-9 사이의 3자리 수를 pickNumberInRange()를 활용해 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        generateRandomNumbers();
     }
-}
+
+
+        public static void generateRandomNumbers() {
+            List<Integer> computerNumber = new ArrayList<>();
+            while (computerNumber.size() < 3) {
+                int randomNumber = Randoms.pickNumberInRange(1, 9);
+                if (!computerNumber.contains(randomNumber)) {
+                    computerNumber.add(randomNumber);
+                }
+            }
+        }
+    }
+
