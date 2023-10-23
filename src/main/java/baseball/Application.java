a1ce6daee46e3c9ec86ed029399f2778f4c40017
K-KY <kyuyoungk@naver.com>
2023-10-23 22:20:31 +0900

    Feat(Application) : 게임시작 메소드 호출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameManager gameManager = new GameManager();
+        gameManager.startGame();
     }
 }
