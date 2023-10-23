4376d6c8ecd8106bdb24defa753fa3c9cf4836e4
SeongIl <biuld1234@gmail.com>
2023-10-23 22:05:45 +0900

    [Restart] Feat: Add 볼, 스트라이크, 낫싱 출력문

--- src/main/java/baseball/RestartApplication.java
+++ src/main/java/baseball/RestartApplication.java
@@ -9,6 +9,8 @@ public class RestartApplication {
         int[] pickedNumber = new int[3];
         int[] userNumbers = new int[3];
         int index = 0;
+        int strikeCount = 0;
+        int ballCount = 0;
         //1. 서로 다른 임의의 수 3개 선택
 
         while (index < 3) {
@@ -32,6 +34,7 @@ public class RestartApplication {
 
         System.out.println("숫자 야구 게임을 시작합니다.");
         while (true) {
+            //3. 입력값 받기
             System.out.print("숫자를 입력해주세요 : ");
             String userInput = Console.readLine();
 
@@ -47,7 +50,7 @@ public class RestartApplication {
                 for (int i = 0; i < 3; i++) {
                     userNumbers[i] = Integer.parseInt(userInputChars[i]);
                     //1~9까지의 범위를 지키는가?
-                    if (userNumbers[i] == 0){
+                    if (userNumbers[i] == 0) {
                         throw new IllegalArgumentException("1~9까지의 숫자를 입력하세요.");
                     }
                 }
@@ -56,17 +59,37 @@ public class RestartApplication {
                 throw new IllegalArgumentException("숫자만 입력하세요.");
             }
 //             - 서로 다른 숫자들로만 구성되어 있는가?
-            for(int i=0; i<3; i++){
-                for(int j=i+1; j<3; j++){
-                    if(userNumbers[i] == userNumbers[j]){
+            for (int i = 0; i < 3; i++) {
+                for (int j = i + 1; j < 3; j++) {
+                    if (userNumbers[i] == userNumbers[j]) {
                         throw new IllegalArgumentException("서로 다른 숫자를 입력하세요.");
                     }
                 }
             }
+//            4. 입력값을 제시된 규칙에 따라 출력시키기
+//                    - 로직 구현 하기(볼, 스트라이크, 낫싱)
+//            - 형식에 맞춰 출력문 구성
 
-
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if ((userNumbers[i] == pickedNumber[j]) && (i == j)) {
+                        strikeCount++;
+                    } else if (userNumbers[i] == pickedNumber[j]) {
+                        ballCount++;
+                    }
+                }
+            }
+            if (ballCount != 0) {
+                System.out.println(ballCount + "볼");
+            }
+            if (strikeCount != 0) {
+                System.out.println(strikeCount + "스트라이크");
+            }
+            if ((ballCount + strikeCount) == 0) {
+                System.out.println("낫싱");
+            }
+            ballCount =0;
+            strikeCount =0;
         }
-
-
     }
 }
