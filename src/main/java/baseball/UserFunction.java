fcba7ddf51f9482464b36a25795eee04c8e7f95e
sheepysnoopy <qkffldlfdjsk12@naver.com>
2023-10-24 03:04:31 +0900

    feat(UserFunction): 예외처리 작성

--- src/main/java/baseball/UserFunction.java
+++ src/main/java/baseball/UserFunction.java
@@ -3,10 +3,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class UserFunction {
     private String userNumber;
+    ExceptionContoller exceptionContoller = new ExceptionContoller();
 
     public String getUserNumber(){  //유저 숫자 받기
-        System.out.print("숫자를 입력해주세요 : ");
+        System.out.print("숫자를 입력해 주세요 : ");
         userNumber = Console.readLine();
+
+        exceptionContoller.notIntegerException(userNumber);
+        exceptionContoller.notThreeDigitException(userNumber);
+        exceptionContoller.reduplicationException(userNumber);
+        exceptionContoller.zeroValueException(userNumber);
 
         return userNumber;
     }
