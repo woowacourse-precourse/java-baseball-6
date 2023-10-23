b3ab4edbcd2e264c060733ac050e24f56eb92a92
bamm2 <xxknu88@naver.com>
2023-10-24 02:06:55 +0900

    Refactor: 입력클래스 필요한 책임만으로 최소화

--- src/main/java/baseball/InputView.java
+++ src/main/java/baseball/InputView.java
@@ -1,38 +1,15 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
+public class InputView {
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class Input {
-
-    private List<Integer> inputList;
-    private InputManager inputManager;
-    private static final int ORIGINCOUNT = 3;
-
-    public Input() {
-        System.out.print("숫자를 입력해주세요 : " );
+    public InputView() {
     }
 
     protected void printGameStartMessage(){
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-//    protected List<Integer> getUserInput(){
-//        String input = Console.readLine();
-////        this.inputManager = new InputManager(input);
-////        inputManager.validateInput();
-////        inputList = convertList(input);
-////        return this.inputList;
-//    }
-
-    public List<Integer> convertList(String input) {
-        List<Integer> userInput = new ArrayList<>();
-        for (int i = 0; i < ORIGINCOUNT; i++) {
-            userInput.add(input.charAt(i) - '0');
-        }
-        return userInput;
+    protected void printNumberMessage(){
+        System.out.print("숫자를 입력해주세요 : ");
     }
-
 }
