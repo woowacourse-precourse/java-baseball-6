a42cb55f60ddbcf773d0c8385d8492137f804ef0
gyulim5 <ohrbfla1004@khu.ac.kr>
2023-10-24 00:35:36 +0900

    🔥 Remove: 중복 함수 삭제

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -4,11 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
     public String readInput() {
-        String userInput = Console.readLine();
-        return userInput;
-    }
-
-    public int readUserChoice() {
-        return Integer.parseInt(Console.readLine());
+        return Console.readLine();
     }
 }
