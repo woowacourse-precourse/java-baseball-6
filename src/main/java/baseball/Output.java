f3dbde559818e621480ed4a5e364538e8fa7a412
seung-won-kim <hesjs5@skuniv.ac.kr>
2023-10-23 23:14:13 +0900

    Feat: 볼, 스트라이크의 갯수를 화면에 출력하는 기능 구현

--- src/main/java/baseball/Output.java
+++ src/main/java/baseball/Output.java
@@ -9,4 +9,8 @@ public class Output {
     public static void printRestartOrExitMessage() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
+
+    public static void printHint(final GameResult gameResult) {
+        System.out.println(gameResult.toString());
+    }
 }
