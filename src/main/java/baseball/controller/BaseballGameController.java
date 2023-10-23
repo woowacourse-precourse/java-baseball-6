86c1d7783528dae832ccc25759a0c1dc1edbdd34
Hugh <lee2317884@gmail.com>
2023-10-24 00:53:00 +0900

    feat: '볼/스트라이크/낫싱'을 결과에 따라 출력

--- src/main/java/baseball/controller/BaseballGameController.java
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -1,10 +1,18 @@
 package baseball.controller;
 
 import baseball.constant.Constant;
+import baseball.domain.BaseballGameResult;
+import baseball.domain.ComputerNumber;
+import baseball.domain.UserNumber;
 import baseball.view.InputView;
+import baseball.view.OutputView;
+import java.util.List;
 
 public class BaseballGameController {
+    private static final int CLEAR_NUMBER = 3;
+
     public BaseballGameController() {
+        OutputView.printStartMessage();
     }
 
     public void gamePlay() {
@@ -12,17 +20,31 @@ public class BaseballGameController {
     }
 
     private void gameProcess() {
-        createUserGameNumber();
+        List<Integer> computerNumber = new ComputerNumber().getComputerNumber();
+        gameCycleProcess(computerNumber);
+        OutputView.printClearMessage();
+        System.out.println(InputView.INPUT_GAME_RESTART_MESSAGE);
     }
 
-    private String createUserGameNumber() {
-        String input = Constant.BLANK_VALUE;
-        try {
+    private void gameCycleProcess(List<Integer> computerNumber) {
+        BaseballGameResult gameResult;
+        List<Integer> userNumber;
+        do {
+            System.out.println(computerNumber);
             System.out.print(InputView.INPUT_GAME_NUMBER_MESSAGE);
-            input = InputView.inputGameNumberMessage();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-        return input;
+            String input = InputView.inputGameNumberMessage();
+            userNumber = new UserNumber(input).getUserNumber();
+            gameResult = new BaseballGameResult(computerNumber, userNumber);
+            new OutputView(gameResult).printGameResult();
+        } while (!validateGameClear(gameResult.strikeCount));
     }
+
+    private boolean validateGameClear(int strikeCount) {
+        return strikeCount == CLEAR_NUMBER;
+    }
+
+    private boolean validateGameReplay(String input) {
+        return input.equals(Constant.GAME_RESTART_NUMBER);
+    }
+
 }
--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,5 +1,6 @@
 package baseball.view;
 
+import baseball.constant.Constant;
 import baseball.constant.errorMessage.UserInputNumberError;
 import camp.nextstep.edu.missionutils.Console;
 
@@ -8,8 +9,11 @@ public class InputView {
     public static final String INPUT_GAME_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
     public static final String INPUT_GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
-    private static final String GAME_RESTART_NUMBER = "1";
-    private static final String GAME_END_NUMBER = "2";
+    private static void validateBlankInputGameNumber(String input) {
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException(UserInputNumberError.USER_INPUT_BLANK_ERROR_MESSAGE);
+        }
+    }
 
     public static String inputGameNumberMessage() {
         String input = getInput();
@@ -20,19 +24,12 @@ public class InputView {
     public static String inputGameRestartMessage() {
         String input = getInput();
         validateWrongGameRestartInput(input);
-        validateBlankInputGameNumber(input);
         return input;
-    }
-
-    private static void validateBlankInputGameNumber(String input) {
-        if (input == null || input.isBlank()) {
-            throw new IllegalArgumentException(UserInputNumberError.USER_INPUT_BLANK_ERROR_MESSAGE);
-        }
     }
 
 
     private static void validateWrongGameRestartInput(String input) {
-        if (!input.equals(GAME_RESTART_NUMBER) && !input.equals(GAME_END_NUMBER)) {
+        if (!input.equals(Constant.GAME_RESTART_NUMBER) && !input.equals(Constant.GAME_END_NUMBER)) {
             throw new IllegalArgumentException(UserInputNumberError.USER_RESTART_INPUT_ERROR_MESSAGE);
         }
     }
