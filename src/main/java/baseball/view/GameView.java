10e80a0b846eb300d4dfb45309d8e18c60128315
aeyongdodam <kikititi0510@gmail.com>
2023-10-24 05:29:11 +0900

    feat(GameView): make endBaseBall function

--- src/main/java/baseball/view/GameView.java
+++ src/main/java/baseball/view/GameView.java
@@ -4,13 +4,21 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class GameView {
 
-    public void startBaseballGame() {
+    public void startBaseball() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
     public String getInput() {
-        System.out.print("숫자 입력하세요 : ");
+        System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
         return input;
     }
+
+    public String endBaseBall() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = Console.readLine();
+        return input;
+    }
+    
 }
