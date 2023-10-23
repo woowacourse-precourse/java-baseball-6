413c7eb93df76e65a5cccb4708b558a9cf6e94cc
김관진 <cozyschooll@gmail.com>
2023-10-23 17:44:28 +0900

    docs 수정, 정답 생성 기능 수정, 서로 다른 수 검사 기능 구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -4,15 +4,36 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class BaseballGame {
 
-    private int answer;
+    private int[] answer = new int[3];
 
     //게임을 생성합니다.
     public BaseballGame() {
         this.answer = generateAnswer();
     }
 
-    public int generateAnswer() {
-        return Randoms.pickNumberInRange(1, 9)*100 + Randoms.pickNumberInRange(1, 9)*10
-                + Randoms.pickNumberInRange(1, 9);
+    //정답을 생성합니다.
+    public int[] generateAnswer() {
+        int[] newAnswer = new int[3];
+        int index;
+
+        newAnswer[0] = Randoms.pickNumberInRange(1, 9);
+
+        for (int i = 0; i<2; i++) {
+            do {
+                index = Randoms.pickNumberInRange(0, 9);
+            } while (checkNums(newAnswer, i));
+            newAnswer[i + 1] = index;
+        }
+        return newAnswer;
+    }
+
+    //서로 다른 수인지 검사합니다.
+    public boolean checkNums(int[] nums, int num) {
+        for (int index : nums) {
+            if (index == num) {
+                return false;
+            }
+        }
+        return true;
     }
 }
