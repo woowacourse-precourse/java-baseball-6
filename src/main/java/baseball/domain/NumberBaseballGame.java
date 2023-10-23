b4ba8539e0251978e85c0998c1dbd3763915cd4b
yshong1998 <yshong1998@gmail.com>
2023-10-24 05:19:10 +0900

    Feat(NumberBaseballgame): 계산된 결과를 반환하는 makeResult 메서드 생성

--- src/main/java/baseball/domain/NumberBaseballGame.java
+++ src/main/java/baseball/domain/NumberBaseballGame.java
@@ -1,13 +1,22 @@
 package baseball.domain;
 
+import baseball.domain.dto.Result;
 import baseball.global.constant.RuleValue;
 import baseball.global.util.RandomBaseballNumberGenerator;
+
+import java.util.List;
 
 public class NumberBaseballGame {
 
     private BaseballNumber computerNumber;
+    private final Referee referee = new Referee();
 
     public void initGameSetting(){
         computerNumber = new BaseballNumber(RandomBaseballNumberGenerator.generateRandomBaseballNumbers(RuleValue.BASEBALL_NUMBER_SIZE));
     }
+    public Result makeResult(List<Integer> playerInput){
+        BaseballNumber playerNumber = new BaseballNumber(playerInput);
+        return referee.alertResult(computerNumber, playerNumber);
+    }
+
 }
