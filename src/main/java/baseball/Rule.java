ad7075876826e6628518801443aa188ede9f66a4
hiidy <hiidy115@gmail.com>
2023-10-23 15:31:38 +0900

    :recycle: refact: PlayGame 클래스에서 Rule의 메서드를 사용하게 함 static을 선언함으로써 스트라이크와 볼을 세는 메서드를 PlayGame에서 사용하게 함

--- src/main/java/baseball/Rule.java
+++ src/main/java/baseball/Rule.java
@@ -3,7 +3,7 @@ package baseball;
 import java.util.ArrayList;
 
 public class Rule {
-    public int countBalls(ArrayList<Integer> answerNumber, ArrayList<Integer> userNumber) {
+    public static int countBalls(ArrayList<Integer> answerNumber, ArrayList<Integer> userNumber) {
         int count = 0;
         for (Integer num : userNumber) {
             if (answerNumber.contains(num)) {
@@ -13,7 +13,7 @@ public class Rule {
         return count;
     }
 
-    public int countStrikes(ArrayList<Integer> answerNumber, ArrayList<Integer> userNumber) {
+    public static int countStrikes(ArrayList<Integer> answerNumber, ArrayList<Integer> userNumber) {
         int count = 0;
         for (int i = 0; i < answerNumber.size(); i++) {
             if (answerNumber.get(i).equals(userNumber.get(i))) {
