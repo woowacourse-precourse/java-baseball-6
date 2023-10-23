27e4e7a467cd3ab411a0a82a2fee2e189b1ae5df
taehyeon <kth0373@naver.com>
2023-10-23 23:53:04 +0900

    플레이거가 숫자를 입력받는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,39 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    static List<Integer> readInput() {
+        String input = Console.readLine();
+        List<Integer> player = new ArrayList<>();
+
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+        for (int i = 0; i < 3; ++i) {
+            int number = input.charAt(i) - '0';
+
+            if (number < 1 || number > 9 || player.contains(number)) {
+                throw new IllegalArgumentException("잘못된 입력입니다.");
+            }
+            player.add(number);
+        }
+        return player;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<Integer> player = readInput();
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
     }
 }
