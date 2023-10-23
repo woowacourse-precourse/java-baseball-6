cbd68cf9e34cf371efe37356f651b73a10058384
jihyuk <kwlgur0416@gmail.com>
2023-10-23 15:06:12 +0900

    refactor : 사용자에게서 서로 다른 3자리 수를 입력 받는 기능 작성

--- src/main/java/baseball/GuessNumberFromUser.java
+++ src/main/java/baseball/GuessNumberFromUser.java
@@ -1,16 +1,57 @@
 package baseball;
 
-import java.util.ArrayList;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class GuessNumberFromUser {
 
-    public List<Integer> getGuessNumber() {
-        return new ArrayList<>();
+    private final static String requestMessage = "숫자를 입력해주세요 : ";
+
+    public List<Integer> getGuessNumber() throws IllegalArgumentException {
+        String inputString = getInputString();
+        List<Integer> guessNumber = StringToIntegerList(inputString);
+        if (!validate(guessNumber)) {
+            throw new IllegalArgumentException();
+        }
+
+        return guessNumber;
     }
 
-    private void validate() {
+    private String getInputString() {
+        requestForGuessNumber();
+        return Console.readLine();
+    }
 
+    private void requestForGuessNumber() {
+        System.out.print(requestMessage);
+    }
+
+    private List<Integer> StringToIntegerList(String str) {
+        return Arrays.stream(str.split(""))
+            .map(Integer::parseInt)
+            .toList();
+    }
+
+
+    private boolean validate(List<Integer> guessNumber) {
+        return sizeValidate(guessNumber)
+            && numberRangeValidate(guessNumber)
+            && distinctValidate(guessNumber);
+    }
+
+    private boolean sizeValidate(List<Integer> guessNumber) {
+        return guessNumber.size() == 3;
+    }
+
+    private boolean numberRangeValidate(List<Integer> guessNumber) {
+        return guessNumber.stream().allMatch(num -> num > 0 && num < 10);
+    }
+
+    private boolean distinctValidate(List<Integer> guessNumber) {
+        HashSet<Integer> setGuessNumber = new HashSet<>(guessNumber);
+        return guessNumber.size() == setGuessNumber.size();
     }
 
 }
