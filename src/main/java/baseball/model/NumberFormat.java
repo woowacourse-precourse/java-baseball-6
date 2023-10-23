2b6baa89ef1d4f2df698efcae5d74cdc071a2567
dmong <dt2980_hee@naver.com>
2023-10-23 11:30:19 +0900

    feat: data model 생성

---
+++ src/main/java/baseball/model/NumberFormat.java
@@ -0,0 +1,13 @@
+package baseball.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberFormat {
+     public static final int DIGIT_CONSTRAINT = 3;
+
+     private List<Integer> numberList;
+     
+}
