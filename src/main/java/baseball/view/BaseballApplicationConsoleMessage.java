20419ffeb61ffb56989e4dbc6cbb4b177e621afb
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-10-23 15:17:44 +0900

    feat: ConsoleMessage toString 메서드 구현

--- src/main/java/baseball/view/BaseballApplicationConsoleMessage.java
+++ src/main/java/baseball/view/BaseballApplicationConsoleMessage.java
@@ -4,12 +4,17 @@ public enum BaseballApplicationConsoleMessage {
     GAME_START("숫자 야구 게임을 시작합니다."),
     INPUT_PLAYER_NUMBER("숫자를 입력해주세요 : "),
     GAME_END("3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
-    GAME_RESTART("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-    ;
+    GAME_RESTART("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
     private final String message;
 
     BaseballApplicationConsoleMessage(String message) {
         this.message = message;
     }
+
+
+    @Override
+    public String toString() {
+        return message;
     }
+}
