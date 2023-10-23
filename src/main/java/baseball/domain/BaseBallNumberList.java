49204b4042392f098e10708a2ca12d630f4d77ca
hunko <rhwlgns4386@naver.com>
2023-10-23 15:07:48 +0900

    feat(BaseBallNumberList) : add validateSameItem logic
    Add new logic for BaseBallNumberList
    if list include same number then throws IllegalArgumentException

--- src/main/java/baseball/domain/BaseBallNumberList.java
+++ src/main/java/baseball/domain/BaseBallNumberList.java
@@ -9,11 +9,26 @@ public class BaseBallNumberList {
     public BaseBallNumberList(List<Integer> numbers) {
         this.numbers = numbers;
         validateSize();
+        validateSameItem();
+    }
+
+    private void validateSameItem() {
+        if (haveSameItem() || hasThreeItems()) {
+            throw new IllegalArgumentException("중복된 값을 입력하지 말아 주세요");
+        }
     }
 
     private void validateSize() {
-        if (this.numbers.size() != 3) {
+        if (hasThreeItems()) {
             throw new IllegalArgumentException("숫자를 3개를 입력해주세요");
         }
+    }
+
+    private boolean hasThreeItems() {
+        return this.numbers.size() != 3;
+    }
+
+    private boolean haveSameItem() {
+        return this.numbers.stream().distinct().count() != this.numbers.size();
     }
 }
