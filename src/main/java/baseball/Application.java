1f2e8209e442277463ee75a46fb215af284ecee7
JongMinCh0i <wonderland314592@gmail.com>
2023-10-23 13:51:51 +0900

    style(Application): 메서드 카멜 케이스 적용

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,7 +3,7 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         DiContainer diFactory = new DiContainer();
-        ComputerController computerController = diFactory.ComputerController();
+        ComputerController computerController = diFactory.computerController();
         computerController.run();
     }
 }
