12d87472a59a41eaee31ecc6cca80b243c062a45
twoosky <lhjae3@naver.com>
2023-10-24 02:56:49 +0900

    feat: ball, strike 개수에 대한 검증 추가

--- src/main/java/baseball/domain/Baseball.java
+++ src/main/java/baseball/domain/Baseball.java
@@ -1,12 +1,44 @@
 package baseball.domain;
 
+import baseball.utils.ErrorMessages;
+import java.util.Objects;
+
 public class Baseball {
+    private static final int NUMBER_SIZE = 3;
     private final int ball;
     private final int strike;
 
     public Baseball(int ball, int strike) {
+        validate(ball, strike);
         this.ball = ball;
         this.strike = strike;
+    }
+
+    public void validate(int ball, int strike) {
+        int total = ball + strike;
+        if (total > NUMBER_SIZE) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID_BASEBALL_CNT);
+        }
+    }
+
+    public Boolean isBallAndStrike() {
+        return ball > 0 && strike > 0;
+    }
+
+    public Boolean isBall() {
+        return ball > 0 && strike == 0;
+    }
+
+    public Boolean isAllStrike() {
+        return ball == 0 && strike == NUMBER_SIZE;
+    }
+
+    public Boolean isStrike() {
+        return ball == 0 && strike > 0 && strike != NUMBER_SIZE;
+    }
+
+    public Boolean isNothing() {
+        return ball == 0 && strike == 0;
     }
 
     public int getBall() {
@@ -15,5 +47,22 @@ public class Baseball {
 
     public int getStrike() {
         return strike;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Baseball baseball = (Baseball) o;
+        return ball == baseball.ball && strike == baseball.strike;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ball, strike);
     }
 }
