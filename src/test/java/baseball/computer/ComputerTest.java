b42b0e1cb060b619016c1c59b0d604c6f5426735
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-10-23 16:37:54 +0900

    test(ComputerTest): 생성한 정답 숫자 범위 테스트 구현

--- src/test/java/baseball/computer/ComputerTest.java
+++ src/test/java/baseball/computer/ComputerTest.java
@@ -2,6 +2,8 @@ package baseball.computer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,5 +23,22 @@ public class ComputerTest {
     void 정답_자리수_테스트() {
         //then
         assertThat(answer.size()).isEqualTo(3);
+    }
+
+    @DisplayName("정답은 1~9사이의 숫자로 이루어져 있어야 한다.")
+    @Test
+    void 정답_숫자_범위_테스트() {
+        //given
+        List<Integer> target = new ArrayList<>();
+
+        //when
+        for (int i = 0; i < answer.size(); i++) {
+            target.add(answer.getNumberByIndex(i));
+        }
+
+        //then
+        assertThat(target.stream()
+                .allMatch(v -> 1 <= v && v <= 9))
+                .isTrue();
     }
 }
