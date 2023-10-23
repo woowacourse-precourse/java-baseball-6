211aca5e0d28e2cc9235142d4be26b8e4eafcd62
LastCow9000 <sys19912002@hanmail.net>
2023-10-24 01:34:57 +0900

    fix: 상수 스네이크 케이스로 변경

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -3,16 +3,16 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
-    private static final String inputMessage = "숫자를 입력해주세요 : ";
-    private static final String inputRegameMessage = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String INPUT_REGAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public static String inputBalls() {
-        System.out.print(inputMessage);
+        System.out.print(INPUT_MESSAGE);
         return Console.readLine();
     }
 
     public static int inputRegame() {
-        System.out.println(inputRegameMessage);
+        System.out.println(INPUT_REGAME_MESSAGE);
         String inputData = Console.readLine();//validate필요
         return Integer.parseInt(inputData);
     }
