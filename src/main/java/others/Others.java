bf77267313d0198d45a3116c94ea97efed3bd12c
Seungjida <esg0107@pusan.ac.kr>
2023-10-23 21:54:06 +0900

    Feat: "숫자 요청 문구 출력 및 입력 기능 추가"

--- src/main/java/others/Others.java
+++ src/main/java/others/Others.java
@@ -1,3 +1,5 @@
+// Others.java
+
 package others;
 
 import static others.ExceptionHandling.restartException;
@@ -7,6 +9,11 @@ import camp.nextstep.edu.missionutils.Console;
 public class Others {
     public static void printGameStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static String getNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+        return Console.readLine().trim().replaceAll(" ", "");
     }
 
     public static boolean ifRestart() {
