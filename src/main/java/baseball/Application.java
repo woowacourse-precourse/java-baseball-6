ece608692092159138679ee76b3e39f73635500b
newworld <newworld-@naver.com>
2023-10-23 22:39:30 +0900

    feat: 실수한 순서도 배치 수정 및 정답 입력 메서드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,4 +1,5 @@
 package baseball;
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -7,17 +8,24 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
 
+        Baseballgame game = new Baseballgame();
+
+        // 0 : 게임 시작시 정답을 생성한다.
+        List<Integer> answer = game.createAnswer();
+        System.out.println(answer);
+
+        // 반복문 제어를 위한 변수 선언 - 조건이 달성 된다면 false 로 전환
         boolean gameStatus = true;
-        while (!gameStatus) {
-
-            Baseballgame game = new Baseballgame();
-
-            // 0 : 게임 시작시 정답을 생성한다.
-            List<Integer> answer = game.createAnswer();
-            System.out.println(answer);
+        while (gameStatus) {
 
             // 1 : 게임 시작 문구 출력
             System.out.print("숫자 야구 게임을 시작합니다.");
+
+            // 2 : 플레이어의 정답을 입력받는다.
+            System.out.print("숫자를 입력해주세요 : ");
+            // 플레이어의 입력 데이터를 검증해서 리스트에 저장해서 반환한다.
+            List<Integer> s = game.enterCorrectAnswer(Console.readLine());
+            System.out.println(s);
 
         }
 
@@ -34,5 +42,17 @@ class Baseballgame {
             }
         }
         return answer;
+    }
+
+    public List<Integer> enterCorrectAnswer(String var) {
+        List<Integer> input = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            int num = var.charAt(i) - 48;
+            if (!input.contains(num)) {
+                input.add(num);
+            }
+            else throw new IllegalArgumentException();
+        }
+        return input;
     }
 }
