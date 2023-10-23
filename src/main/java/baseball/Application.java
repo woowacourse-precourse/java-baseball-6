0063da6163c05eac5ca487324ed2b849de11635a
최진원 <etudent39@gmail.com>
2023-10-24 00:40:52 +0900

    컴퓨터와 유저 수 비교 및 출력에 대한 기능 구현 : 수정예정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,24 +9,20 @@ import java.util.ArrayList;
 import java.util.Set;
 public class Application {
     public static void main(String[] args) {
-        String input = Console.readLine();
+        /**String input = Console.readLine();
         if(input.length() != 3){
             System.exit(0);
         }
         List<Integer> userNumberList = new ArrayList<Integer>();
-        Set<Integer> userNumberSet = new HashSet<Integer>();
         for(int i = 0; i < input.length(); i++){
-            if(!('1' <= input.charAt(i) && input.charAt(i) <= '9')){
+            if(!('1' <= input.charAt(i) && input.charAt(i) <= '9') || userNumberList.contains((int) input.charAt(i))){
                 System.exit(0);
             }
             else{
                 userNumberList.add((int) input.charAt(i));
-                userNumberSet.add((int) input.charAt(i));
             }
-        }
-        if(userNumberSet.size() != userNumberList.size()){
-            System.exit(0);
-        }
+        } */
+
 
         List<Integer> computer = new ArrayList<Integer>();
         while(computer.size() < 3){
@@ -35,6 +31,60 @@ public class Application {
                 computer.add(randomNumber);
             }
         }
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while(true){
+            System.out.println("숫자를 입력해주세요 : ");
+            String input = Console.readLine();
+            if(input.length() != 3){
+                System.exit(0);
+            }
+            List<Integer> userNumberList = new ArrayList<Integer>();
+            for(int i = 0; i < input.length(); i++){
+                if(!('1' <= input.charAt(i) && input.charAt(i) <= '9') || userNumberList.contains((int) input.charAt(i))){
+                    System.exit(0);
+                }
+                else{
+                    userNumberList.add((int) (input.charAt(i) - '0'));
+                }
+            }
 
+            int ball = 0;
+            int strike = 0;
+            for(int i = 0; i < userNumberList.size(); i++){
+                int num = userNumberList.get(i);
+                if(computer.contains(num)){
+                    if(computer.indexOf(num) == i)
+                        strike++;
+                    else
+                        ball++;
+                }
+            }
+            if(ball == 0 && strike == 0){
+                System.out.println("낫싱");
+            }
+            else if (ball != 0 && strike == 0) {
+                System.out.println(Integer.toString(ball) + "볼");
+            }
+            else if (ball == 0 && strike != 0){
+                System.out.println(Integer.toString(strike) + "스트라이크");
+            }
+            else{
+                System.out.println(Integer.toString(ball) + "볼" + " " + Integer.toString(strike) + "스트라이크");
+            }
+
+            if(strike == 3){
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                String regame = Console.readLine();
+                if(regame.equals("1")){
+                    continue;
+                } else if (regame.equals("2")) {
+                    break;
+                }
+                else{ // 잘못 입력
+                    break;
+                }
+            }
+        }
     }
 }
