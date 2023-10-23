e2cadf378bb5dfb795ac4b83e6a44a28f5c1c626
dmong <dt2980_hee@naver.com>
2023-10-23 14:42:33 +0900

    feat: 사용자 입력값에 따른 view 출력 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,46 @@
 package baseball;
 
+import baseball.controller.GameController;
+import baseball.model.NumberFormat;
+import baseball.model.UserAction;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static baseball.controller.GameController.*;
+import static baseball.model.NumberFormat.createNumberFormat;
+import static baseball.model.NumberFormat.generateRandomNumber;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameController gameController = new GameController();
+        boolean continueMode = true;
+        boolean isFinished;
+
+        printInitMessage();
+        try {
+            while (continueMode) {
+                isFinished = false;
+                gameController.setComputerNumber(generateRandomNumber());
+                while (!isFinished){
+                    System.out.print("숫자를 입력하세요 : ");
+                    Integer userNumber = Integer.valueOf(Console.readLine());
+                    gameController.setUserNumber(createNumberFormat(userNumber));
+                    if (gameController.checkGameResult()) isFinished = true;
+                    System.out.println();
+                }
+
+
+                printEndMessage();
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+            throw new IllegalArgumentException();
+        }
+
+
     }
 }
--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,5 +1,9 @@
 package baseball.controller;
 
+import baseball.model.NumberFormat;
+import baseball.service.GameService;
+import java.util.List;
+
 public class GameController {
 
     public static void printInitMessage(){
@@ -9,5 +13,19 @@ public class GameController {
     public static void printGameEnd(){
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+    public boolean checkGameResult(){
+        List<Integer> result = GameService.compareNumberFormat(computerNumber, userNumber);
+        printResultMessage(result.get(0), result.get(1));
+        return (result.get(0) == 3) ? true : false;
+    }
+    public void printResultMessage(int strikeCount, int ballCount){
+        if (strikeCount > 0 && ballCount > 0)
+            System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+        else if (ballCount > 0)
+            System.out.println(ballCount + "볼");
+        else if (strikeCount > 0)
+            System.out.println(strikeCount + "스트라이크");
+        else System.out.println("낫싱");
     }
 }
