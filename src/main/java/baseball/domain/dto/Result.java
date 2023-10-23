0335b5571ec2ca7a9082cfb1cd133ed3981d251e
yshong1998 <yshong1998@gmail.com>
2023-10-23 21:27:28 +0900

    Feat(Result): 결과를 저장 및 반환하는 Result 클래스 생성

--- src/main/java/baseball/domain/dto/Result.java
+++ src/main/java/baseball/domain/dto/Result.java
@@ -1,4 +1,22 @@
 package baseball.domain.dto;
 
 public class Result {
+    private final int ball;
+    private final int strike;
+
+    // 스트라이크, 볼 저장 기능
+    Result(int ball, int strike){
+        this.ball = ball;
+        this.strike = strike;
+    }
+
+    // 스트라이크, 볼 결과 반환 기능
+
+    public int getBall(){
+        return ball;
+    }
+
+    public int getStrike(){
+        return strike;
+    }
 }
