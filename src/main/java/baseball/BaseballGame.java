fd1008e03def01068549963a286953a403b0f6cd
문고은 <>
2023-10-24 04:20:02 +0900

    feat: 사용자가 입력한 값 예외 체크 구현
    입력값이 비어있을 경우, 3자리 숫자가 아닐 경우, 0을 입력했거나 숫자가 아닌 값을 입력했을 경우, 중복된 숫자를 입력했을 경우 IllegalArgumentException 발생 시킴

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -3,7 +3,9 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class BaseballGame {
     private int strikeCount;
@@ -29,12 +31,12 @@ public class BaseballGame {
         return computerNum;
     }
 
-    public int randomNum(){
+    private int randomNum(){
         return Randoms.pickNumberInRange(MIN_RANGE, MAX_RANGE);
     }
 
     //사용자 입력 문자열을 숫자 목록으로 변환
-    public static List<Integer> playerNum(String playerStr){
+    private static List<Integer> playerNum(String playerStr){
         List<Integer> playerNum = new ArrayList<>();
         for(int i = 0; i < playerStr.length(); i++){
             char playerChar = playerStr.charAt(i);
@@ -42,5 +44,39 @@ public class BaseballGame {
             playerNum.add(playerInt);
         }
         return playerNum;
+    }
+
+    //숫자 확인
+    private boolean isNumeric(String playerStr){
+        return playerStr.matches("\\d+");
+    }
+    //중복 숫자 체크
+    private boolean containsRepeats(String playerStr){
+        boolean result = false;
+        List<Integer> playerNumList = BaseballGame.playerNum(playerStr);
+        Set<Integer> playerNumSet = new HashSet<>(playerNumList);
+
+        if(playerNumList.size() == playerNumSet.size()) result = true;
+
+        return result;
+    }
+
+    //입력값 예외 체크
+    public void inputChk(String playerStr){
+        if (playerStr == null || playerStr.isEmpty()) {
+            throw new IllegalArgumentException("입력값이 비어있습니다.");
+        }
+        else if(playerStr.length() != NUMBER_LENGTH) {
+            throw new IllegalArgumentException("3자리 숫자만 입력 가능합니다. 입력한 값 : " + playerStr);
+        }
+        else if(playerStr.contains("0")) {
+            throw new IllegalArgumentException("숫자는 1부터 9까지 입력 가능합니다. 입력한 값 : " + playerStr);
+        }
+        else if(!isNumeric(playerStr)) {
+            throw new IllegalArgumentException("숫자만 입력 가능합니다. 입력한 값 : " + playerStr);
+        }
+        else if(!containsRepeats(playerStr)) {
+            throw new IllegalArgumentException("중복된 숫자는 입력이 불가능합니다. 입력한 값 : " + playerStr);
+        }
     }
 }
