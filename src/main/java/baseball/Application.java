b312af071b06f611547e052c4392a3d74b3f97bc
Controllls <whdrms9459@naver.com>
2023-10-23 17:38:28 +0900

    add : 플레이어가 숫자 3개를 컴퓨터에게 제출 하고 컴퓨터가 값을 판단 후 사용자에게 알려주는 기능 제작(기능 2,3,4번)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         //개임 시작 문구 출력
@@ -8,6 +10,11 @@ public class Application {
         Computer computer = new Computer();
         //컴퓨터가 서로 다른 랜덤한 숫자 3개를 선택한다(1)
         computer.GenerateRandomNumber();
+        //플레이어 (사람)가 숫자 3개를 컴퓨터에게 답을 제출한다(2)
+        String inputNumber = Console.readLine();
+        String submitResult = computer.submitAnswer(inputNumber);
+        //컴퓨터는 판단한 결과를 플레이어에게 알려준다(4)
+        System.out.println(submitResult);
     }
 
 
--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -4,10 +4,13 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Computer {
     private List<Integer> numbers;
 
+    //랜덤 숫자 3개 만들기 함수
     public void GenerateRandomNumber() {
         List<Integer> numberList = new ArrayList<>();
         while (numberList.size() < 3) {
@@ -17,6 +20,97 @@ public class Computer {
             }
         }
         this.numbers = numberList;
+    }
+
+    //제출값 결과 반환
+    public String submitAnswer(String inputNumber) {
+        //인풋 값 검증
+        if (!isNumberAndIsSizeThree(inputNumber)) {
+            throw new IllegalArgumentException();
+        }
+        //String -> List<int> 로 변환
+        List<Integer> intInputNumbers = parseIntList(inputNumber);
+
+        //서로 다른값인지 검증
+        if (containSameNumber(intInputNumbers)) {
+            throw new IllegalArgumentException();
+        }
+
+        //컴퓨터는 플레이어의 숫자 3개를 보고 볼, 스트라이크, 아웃을 판단한다(3)
+        return inputNumberResult(intInputNumbers);
+    }
+
+    //볼, 스트라이크, 아웃 판단 함수
+    private String inputNumberResult(List<Integer> playerInputNumbers) {
+        int strike = 0;
+        int ball = 0;
+
+        //스트라이크 판단
+        for (int i = 0; i < this.numbers.size(); i++) {
+            if (this.numbers.get(i).equals(playerInputNumbers.get(i))) {
+                strike++;
+            }
+        }
+
+        //볼 판단
+        for (int i = 0; i < this.numbers.size(); i++) {
+            for (int j = 0; j < this.numbers.size(); j++) {
+                if (i != j) {
+                    if (this.numbers.get(i).equals(playerInputNumbers.get(j))) {
+                        ball++;
+                    }
+                }
+            }
+        }
+
+        if (ball == 0 && strike == 0) {
+            return "낫싱";
+        }
+
+        if (strike == 0) {
+            return ball + "볼";
+        }
+
+        if (ball == 0) {
+            return strike + "스트라이크";
+        }
+
+        return ball + "볼 " + strike + "스트라이크";
+    }
+
+    //입력으로 받은 String값이 3자리 숫자인지 검증크는 코드
+    private Boolean isNumberAndIsSizeThree(String str) {
+        // 패턴을 이용하여 1~9 까지의 3자리 숫자인지 검증(공백)
+        String pattern = "^[1-9]{3}$";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(str);
+
+        return m.matches();
+    }
+
+    //String -> List<Integer>
+    private List<Integer> parseIntList(String str) {
+        String[] numbers = str.split("");
+        List<Integer> numbersToInt = new ArrayList<>();
+        for (String number : numbers) {
+            int numberToInt = Integer.parseInt(number);
+            numbersToInt.add(numberToInt);
+        }
+        return numbersToInt;
+    }
+
+    //같은 숫자가 있는지 검증하는 함수
+    private Boolean containSameNumber(List<Integer> integerList) {
+        if (integerList.size() > 1) {
+            for (int i = 0; i < integerList.size() - 1; i++) {
+                for (int j = (i + 1); j < integerList.size(); j++) {
+                    if (integerList.get(i).equals(integerList.get(j))) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     public List<Integer> getNumbers() {
