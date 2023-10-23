1ba0bb94a47b197ca4079effbae7d94a7b95c476
LastCow9000 <sys19912002@hanmail.net>
2023-10-24 04:45:13 +0900

    refactor: 문자열 상수화

--- src/main/java/baseball/Output.java
+++ src/main/java/baseball/Output.java
@@ -4,6 +4,9 @@ public class Output {
     private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String END_MESSAGE = "3스트라이크\n" + "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String NOTHING_MESSAGE = "낫싱";
+    private static final String BALL = "볼";
+    private static final String SPACE = " ";
+    private static final String STRIKE = "스트라이크";
 
     public static void printStartMessage() {
         System.out.println(START_MESSAGE);
@@ -20,11 +23,17 @@ public class Output {
     public static void printGameResult(int strikeCount, int ballCount) {
         StringBuilder stringBuilder = new StringBuilder();
         if (ballCount > 0) {
-            stringBuilder.append(ballCount).append("볼 ");
+            stringBuilder
+                    .append(ballCount)
+                    .append(BALL)
+                    .append(SPACE);
         }
         if (strikeCount > 0) {
-            stringBuilder.append(strikeCount).append("스트라이크");
+            stringBuilder
+                    .append(strikeCount)
+                    .append(STRIKE);
         }
+
         System.out.println(stringBuilder);
     }
 }
