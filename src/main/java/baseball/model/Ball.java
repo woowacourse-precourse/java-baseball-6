2e5fdf3851d7a90899b6a5a2858d3f6d4be4a03e
Dreaming-J <kimjjong321@naver.com>
2023-10-23 10:58:47 +0900

    :white_check_mark:  test. 야구공 1~9사이의 숫자

--- src/main/java/baseball/model/Ball.java
+++ src/main/java/baseball/model/Ball.java
@@ -4,23 +4,23 @@ import java.util.Objects;
 
 public class Ball {
     private final int position;
-    private final int number;
+    private final BallNo ballNo;
 
-    public Ball(int position, int number) {
+    public Ball(int position, BallNo ballNo) {
         this.position = position;
-        this.number = number;
+        this.ballNo = ballNo;
     }
 
     public BallStatus play(Ball ball) {
         if (this.equals(ball)) return BallStatus.STRIKE;
 
-        if (ball.matchBallNo(this.number)) return BallStatus.BALL;
+        if (ball.matchBallNo(this.ballNo)) return BallStatus.BALL;
 
         return BallStatus.NOTHING;
     }
 
-    private boolean matchBallNo(int number) {
-        return this.number == number;
+    private boolean matchBallNo(BallNo number) {
+        return this.ballNo.equals(number);
     }
 
     @Override
@@ -28,11 +28,11 @@ public class Ball {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Ball ball = (Ball) o;
-        return position == ball.position && number == ball.number;
+        return position == ball.position && Objects.equals(ballNo, ball.ballNo);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(position, number);
+        return Objects.hash(position, ballNo);
     }
 }
---
+++ src/main/java/baseball/model/BallNo.java
@@ -0,0 +1,34 @@
+package baseball.model;
+
+import java.util.Objects;
+
+public class BallNo {
+    private static final int FIRST_NO = 1;
+    private static final int END_NO = 9;
+    private static final String WRONG_RANGE_OF_NUMBER = "[ERROR] 야구공은 1에서 9사이의 숫자만 가능합니다.";
+    private final int number;
+
+    public BallNo(int number) {
+        validateNo(number);
+        this.number = number;
+    }
+
+    private void validateNo(int number) {
+        if (number < FIRST_NO || number > END_NO) {
+            throw new IllegalArgumentException(WRONG_RANGE_OF_NUMBER);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BallNo ballNo = (BallNo) o;
+        return number == ballNo.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+}
