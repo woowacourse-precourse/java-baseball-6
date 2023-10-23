d840dfd8efea148f90b4e66b4487eec2818deb81
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-10-23 18:03:02 +0900

    fix(OutputView): 볼, 스트라이크 출력 형식 오류 변경

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -16,7 +16,7 @@ public class OutputView {
         } else if (ball == 0 && strike != 0) {
             System.out.println(strike + "스트라이크");
         } else if (ball != 0 && strike != 0) {
-            System.out.println(ball + "볼" + strike + "스트라이크");
+            System.out.println(ball + "볼 " + strike + "스트라이크");
         } else if (ball == 0 && strike == 0) {
             System.out.println("낫싱");
         }
