b196163c361386d43b2a7b452d20c86685b36c91
ikjo39 <chi4321@naver.com>
2023-10-24 00:12:52 +0900

    refactor: 컴퓨터 로직 분리

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,31 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private static final int COMPUTER_NUMBERS_SIZE = 3;
+    private static final int RANDOM_NUMBER_MIN = 1;
+    private static final int RANDOM_NUMBER_MAX = 9;
+    private final Numbers numbers;
+
+    public Computer() {
+        this.numbers = new Numbers(createComputerNumbers());
+    }
+
+    private List<Integer> createComputerNumbers() {
+        List<Integer> computerNumbers = new ArrayList<>();
+        while (computerNumbers.size() < COMPUTER_NUMBERS_SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(RANDOM_NUMBER_MIN, RANDOM_NUMBER_MAX);
+            if (!computerNumbers.contains(randomNumber)) {
+                computerNumbers.add(randomNumber);
+            }
+        }
+        return computerNumbers;
+    }
+
+    public Score calculateScore(Numbers user) {
+        return this.numbers.calculateScore(user);
+    }
+}
--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -4,14 +4,7 @@ import static baseball.Message.GAME_START;
 import static baseball.Message.INSERT_END_INPUT;
 import static baseball.Message.INSERT_GAME_INPUT;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Game {
-    private static final int COMPUTER_NUMBERS_SIZE = 3;
-    private static final int RANDOM_NUMBER_MIN = 1;
-    private static final int RANDOM_NUMBER_MAX = 3;
     private final IOService ioService;
 
     public Game() {
@@ -21,25 +14,15 @@ public class Game {
     public void run() {
         ioService.println(GAME_START);
         do {
-            Numbers computerNumbers = new Numbers(createComputerNumbers());
+            Computer computer = new Computer();
             Score score = new Score();
             while (score.isUserLose()) {
                 GameInput gameInput = new GameInput(ioService.readInput(INSERT_GAME_INPUT));
                 Numbers userNumbers = new Numbers(gameInput.convertInputToUserNumbers());
-                score = computerNumbers.calculateScore(userNumbers);
+                score = computer.calculateScore(userNumbers);
                 ioService.println(score.getHint());
             }
         } while (new EndInput(ioService.readInput(INSERT_END_INPUT)).isPressResume());
     }
 
-    private List<Integer> createComputerNumbers() {
-        List<Integer> computerNumbers = new ArrayList<>();
-        while (computerNumbers.size() < COMPUTER_NUMBERS_SIZE) {
-            int randomNumber = Randoms.pickNumberInRange(RANDOM_NUMBER_MIN, RANDOM_NUMBER_MAX);
-            if (!computerNumbers.contains(randomNumber)) {
-                computerNumbers.add(randomNumber);
-            }
-        }
-        return computerNumbers;
-    }
 }
