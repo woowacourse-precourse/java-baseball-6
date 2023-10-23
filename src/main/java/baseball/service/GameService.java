a77b330bdc306872f34a0d21135f070ad2f31154
Nara Park <skfk6352@naver.com>
2023-10-24 01:56:29 +0900

    <feat>(<gameservice>): create method for asking game restart

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -7,9 +7,11 @@ import baseball.validation.InputNumberValidator;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class GameService {
 
+    private static final Integer STOP = 2;
     private static List<Integer> computerNumber;
 
     /*
@@ -27,5 +29,15 @@ public class GameService {
         List<Integer> inputList = InputNumberValidator.validateInputNumber(input);
         Map<String, Integer> resultMap = MessageConverter.getResultMessage(inputList, computerNumber);
         return MessageConverter.getResultMessageToString(resultMap);
+    }
+
+    /*
+    * 게임 종료 메서드
+    * true : 게임 종료 false : 재시작
+    * */
+    public static boolean endGame() {
+        String input = UserUtil.getUserNumber();
+
+        return Objects.equals(input, STOP);
     }
 }
