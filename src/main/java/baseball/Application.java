c46d7e9e232082880bf12a7a1e41670a383e17db
Choijake <shameless8@naver.com>
2023-10-23 16:14:06 +0900

    Feat : Validate 클래스에 각 자리 수의 범위 검증하는 메서드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -70,7 +70,7 @@ class Ball {
     int getInputNumber() {
         number = Integer.parseInt(Console.readLine());
 
-        if (!Validate.verifyTripleDigit(number) || !Validate.verifyEachDigitDuplicate(number)) {
+        if (!Validate.verifyTripleDigit(number) || !Validate.verifyEachDigitDuplicate(number) || !Validate.verifyEachDigitInRange(number)) {
             return number;
         } else {
             throw new IllegalArgumentException("올바른 입력 형식이 아닙니다.");
@@ -90,27 +90,6 @@ class Ball {
     }
 }
 
-class Validate {
-    public static boolean verifyTripleDigit(int number) {
-        return String.valueOf(number).length() != 3;
-    }
-
-    public static boolean verifyEachDigitDuplicate(int number) {
-        boolean[] digitSeen = new boolean[10];
-
-        while (number > 0) {
-            int digit = number % 10;
-            if (digitSeen[digit]) {
-                return false;
-            }
-            digitSeen[digit] = true;
-            number /= 10;
-        }
-        return true;
-    }
-
-
-}
 
 class Referee {
     int[] result;
@@ -210,6 +189,38 @@ class Message {
 
     void printEndMessage() {
         System.out.println(EndMessage);
+    }
+}
+
+
+class Validate {
+    public static boolean verifyTripleDigit(int number) {
+        return String.valueOf(number).length() != 3;
+    }
+
+    public static boolean verifyEachDigitDuplicate(int number) {
+        boolean[] digitSeen = new boolean[10];
+
+        while (number > 0) {
+            int digit = number % 10;
+            if (digitSeen[digit]) {
+                return false;
+            }
+            digitSeen[digit] = true;
+            number /= 10;
+        }
+        return true;
+    }
+
+    public static boolean verifyEachDigitInRange(int number) {
+        while (number > 0) {
+            int digit = number % 10;
+            if (digit < 1 || digit > 9) {
+                return false;
+            }
+            number /= 10;
+        }
+        return true;
     }
 }
 
