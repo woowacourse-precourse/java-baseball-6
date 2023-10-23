401e6a8f91c1df6e14e7766a2e9d7f1875c917da
dongwonkwon <ehddnjs0710@gmail.com>
2023-10-23 18:20:57 +0900

    feat: Computer 클래스 구현

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,25 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private List<Integer> randomNumbers;
+
+    public void makeRandomNumbers() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        randomNumbers = computer;
+    }
+
+    public List<Integer> getRandomNumbers() {
+        return randomNumbers;
+    }
+}
