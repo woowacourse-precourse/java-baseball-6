a46fda121f4d53844a21e2ee6e3f25f2c183c917
inpink <inpink@kakao.com>
2023-10-24 01:39:08 +0900

    test: NothingService의 count() Test
    BallService와 로직 매우 유사.

---
+++ src/test/java/baseball/service/NothingServiceTest.java
@@ -0,0 +1,49 @@
+package baseball.service;
+
+import baseball.domain.Baseball;
+import baseball.service.hint.NothingService;
+import baseball.util.IntegerListUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class NothingServiceTest {
+
+    private final NothingService nothingService = NothingService.getInstance();
+    private Baseball computerBaseball;
+    private Baseball inputBaseball;
+
+    @BeforeEach
+    void setup() {
+        computerBaseball = new Baseball();
+        inputBaseball = new Baseball();
+    }
+
+    @Test
+    void count_zeroNothing() {
+        //given
+        computerBaseball.setValues(IntegerListUtil.parseIntegerList("123"));
+        inputBaseball.setValues(IntegerListUtil.parseIntegerList("456"));
+
+        //when
+        int count = nothingService.count(computerBaseball, inputBaseball);
+
+        //then
+        assertThat(count).isZero();
+    }
+
+    @Test
+    void count_oneNothing() {
+        //given
+        computerBaseball.setValues(IntegerListUtil.parseIntegerList("123"));
+        inputBaseball.setValues(IntegerListUtil.parseIntegerList("416"));
+
+        //when
+        int count = nothingService.count(computerBaseball, inputBaseball);
+
+        //then
+        assertThat(count).isOne();
+    }
+
+}
