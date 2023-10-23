604f22b7173f1cc7d7647c7e9c497f8f35fbc2a4
JaehongDev <println@kakao.com>
2023-10-23 21:42:08 +0900

    feat: InputView, OuputView 기능 추가

--- src/main/java/baseball/view/core/InputView.java
+++ src/main/java/baseball/view/core/InputView.java
@@ -6,7 +6,17 @@ public final class InputView extends ConsoleWriter {
     public String readUserInput() {
         this.print("숫자를 입력해주세요 : ");
         final var input = Console.readLine();
+        System.out.println(input);
         return input;
     }
 
+    public int readRetryCommand() {
+        this.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        final var input = Console.readLine();
+        try {
+            return Integer.parseInt(input);
+        } catch (Exception exception) {
+            throw new IllegalArgumentException("재시작 명령어는 숫자여야 합니다.");
+        }
+    }
 }
