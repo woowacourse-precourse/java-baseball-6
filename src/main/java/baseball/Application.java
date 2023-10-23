43170b3c899284454cdf6c9a524edfbae270c6e4
SEUNGJIN OH <59600377+maison01006@users.noreply.github.com>
2023-10-24 01:19:27 +0900

    야구 게임의 정답(랜덤 숫자) 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,37 @@
 package baseball;
 
+import java.util.ArrayList;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        
+        BaseballGame baseballGame = new BaseballGame();
+        baseballGame.run();
+
     }
+
+    private static class BaseballGame {
+
+        ArrayList<Integer> computerInputNums;
+
+        private void run() {
+         
+            computerInputNums = randomInputNums();
+        }
+
+        private ArrayList<Integer> randomInputNums() {
+            ArrayList<Integer> radomNums = new ArrayList<>();
+        
+            while (radomNums.size() < 3) {
+                int randomNum = Randoms.pickNumberInRange(1, 9);
+                if (!radomNums.contains(randomNum)) {
+                    radomNums.add(randomNum);
+                }
+            }
+            return radomNums;
+        }
+    }
+
 }
