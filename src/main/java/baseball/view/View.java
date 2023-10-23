9547da8acd032792fc51a78aa4cc68b41c219dce
ay-eonii <naayen@naver.com>
2023-10-23 15:24:35 +0900

    feat: 게임 시작 문구 출력

---
+++ src/main/java/baseball/view/View.java
@@ -0,0 +1,9 @@
+package baseball.view;
+
+public class View {
+    private final static String START_COMMENT = "숫자 야구 게임을 시작합니다.";
+
+    public void printStart() {
+        System.out.println(START_COMMENT);
+    }
+}
