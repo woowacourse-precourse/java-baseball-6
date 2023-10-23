fe9c5e6aec65ce1b2a876f47b34e28bc06d84d9f
JaehongDev <println@kakao.com>
2023-10-23 21:44:01 +0900

    refactor: Scenario에 View 바인딩

--- src/main/java/baseball/BaseballApplication.java
+++ src/main/java/baseball/BaseballApplication.java
@@ -1,56 +1,48 @@
 package baseball;
 
 import baseball.scenario.Scenario;
-import baseball.scenario.Scenario.ScenarioResultType;
+import baseball.view.core.ViewContainer;
+import baseball.view.core.ViewType;
 
-public class BaseballApplication {
+public record BaseballApplication(ViewContainer viewContainer) {
 
     public void run() {
         gameStartScenario().play();
     }
 
     Scenario gameStartScenario() {
+        final var view = viewContainer.findView(ViewType.GAME_START);
         return Scenario.create()
                 .next(this::initializeComputer)
-                .build(() -> {
-                    System.out.println("게임 스타트");
-                    return ScenarioResultType.NEXT;
-                });
+                .build(view::execute);
     }
 
     Scenario initializeComputer() {
+        final var view = viewContainer.findView(ViewType.COMPUTER_INIT);
         return Scenario.create()
                 .next(this::initializeUser)
-                .build(() -> {
-                    System.out.println("컴퓨터 초기화");
-                    return ScenarioResultType.NEXT;
-                });
+                .build(view::execute);
     }
 
     Scenario initializeUser() {
+        final var view = viewContainer.findView(ViewType.PLAYER_INIT);
         return Scenario.create()
                 .next(this::refereeScenario)
-                .build(() -> {
-                    System.out.println("사용자 초기화");
-                    return ScenarioResultType.NEXT;
-                });
+                .build(view::execute);
     }
 
     Scenario refereeScenario() {
+        final var view = viewContainer.findView(ViewType.REFEREE);
         return Scenario.create()
+                .previous(this::initializeUser)
                 .next(this::retryScenario)
-                .build(() -> {
-                    System.out.println("검사");
-                    return ScenarioResultType.NEXT;
-                });
+                .build(view::execute);
     }
 
     Scenario retryScenario() {
+        final var view = viewContainer.findView(ViewType.RETRY);
         return Scenario.create()
-                .previous(this::initializeComputer)  // 이 부분을 수정함
-                .build(() -> {
-                    System.out.println("리트라이");
-                    return ScenarioResultType.NEXT;
-                });
+                .previous(this::initializeComputer)
+                .build(view::execute);
     }
 }
