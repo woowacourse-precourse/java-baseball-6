09798174ec5349f44858128f7a5f7d1181f72b86
nylonmusk <2912rla@gmail.com>
2023-10-24 01:19:31 +0900

    feat : Player 클래스 생성 및 사용자의 번호를 입력받는 기능 구현

---
+++ src/main/java/baseball/domain/Player.java
@@ -0,0 +1,70 @@
+package baseball.domain;
+
+import baseball.view.InputView;
+import baseball.view.OutputView;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Player {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public Player() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public List<Integer> getInputNumber() {
+        List<Integer> inputNumbers;
+        while (true) {
+            outputView.requestNumber();
+            String input = inputView.receiveInputNumber();
+            if (isValid(input)) {
+                inputNumbers = convertStringToList(input);
+                break;
+            }
+        }
+        return inputNumbers;
+    }
+
+    public int receiveRetryOption() {
+        outputView.restartOrQuit();
+        int option;
+        while(true) {
+            String input = inputView.receiveInputNumber();
+            if (input.matches("[12]")) {
+                option = Integer.parseInt(input);
+                break;
+            }
+        }
+        return option;
+    }
+
+    private List<Integer> convertStringToList(String input) {
+        List<Integer> inputNumbers = new ArrayList<>();
+        for (char ch : input.toCharArray()) {
+            inputNumbers.add(Character.getNumericValue(ch));
+        }
+        return inputNumbers;
+    }
+
+    private boolean isValid(String input) {
+        if (!input.matches("^[1-9]{3}$") || !hasNoDuplicateDigits(input)) {
+            outputView.showMessage("비정상적인 입력입니다. 게임이 종료됩니다.");
+            throw new IllegalArgumentException();
+        }
+        return true;
+    }
+
+
+    private boolean hasNoDuplicateDigits(String input) {
+        Set<Character> uniqueDigits = new HashSet<>();
+        for (char digit : input.toCharArray()) {
+            uniqueDigits.add(digit);
+        }
+        return uniqueDigits.size() == input.length();
+    }
+}
