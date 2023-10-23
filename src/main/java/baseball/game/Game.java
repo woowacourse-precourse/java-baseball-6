34f897d18908ec9dcbebcdf55a246daf22925289
mistarson <yeowuli2@naver.com>
2023-10-23 18:46:40 +0900

    Feat: 게임 재시작 루틴 구현
    정답을 맞춰서 게임이 끝난 경우 재시작: 1 / 종료: 2 중 하나의 수를 받아 게임을 재시작하거나 종료하는 루틴 구현

--- src/main/java/baseball/game/Game.java
+++ src/main/java/baseball/game/Game.java
@@ -1,12 +1,39 @@
 package baseball.game;
 
 import baseball.util.InputUtil;
+import baseball.util.OutputUtil;
+import baseball.util.RandomNumberUtil;
+import baseball.util.StrikeBallUtil;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Game {
-    public static void start() {
-        while (true) {
-            String s = InputUtil.inputNumber();
-        }
+    static boolean restartFlag = true;
 
+    public static void start() {
+        while (restartFlag) {
+
+            final String answerNumbers = RandomNumberUtil.getThreeRandomNumbers();
+
+            boolean isCorrect = false;
+            while (!isCorrect) {
+
+                String userNumbers = InputUtil.inputNumber();
+
+                Hint hint = StrikeBallUtil.getHint(answerNumbers, userNumbers);
+                StrikeBallUtil.printHint(hint);
+
+                if (isThreeStrike(hint.getStrike())) {
+                    isCorrect = true;
+
+                    OutputUtil.printRestartOrClose();
+                    restartFlag = InputUtil.inputRestartOrClose();
+                }
+            }
+        }
+        Console.close();
+    }
+
+    private static boolean isThreeStrike(int strike) {
+        return strike == 3;
     }
 }
--- src/main/java/baseball/util/InputUtil.java
+++ src/main/java/baseball/util/InputUtil.java
@@ -1,5 +1,7 @@
 package baseball.util;
 
+import static baseball.constant.Constant.CLOSE;
+
 import baseball.validation.InputValidator;
 import camp.nextstep.edu.missionutils.Console;
 
@@ -10,5 +12,14 @@ public class InputUtil {
         InputValidator.validateInputNumber(inputStr);
 
         return inputStr;
+    }
+
+    public static boolean inputRestartOrClose(){
+        String inputStr = Console.readLine();
+        InputValidator.validateRestartOrClose(inputStr);
+
+        int input = Integer.parseInt(inputStr);
+
+        return !inputStr.equals(CLOSE);
     }
 }
