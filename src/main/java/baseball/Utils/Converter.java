ce0ddaad1e445c783c0707ee382ecf2d3c1f5bb1
win9 <krrr883@gmail.com>
2023-10-24 05:12:16 +0900

    [Feat]: 변환 클래스 생성
    문자열 및 정수 변환용 클래스 생성

---
+++ src/main/java/baseball/Utils/Converter.java
@@ -0,0 +1,12 @@
+package baseball.Utils;
+
+public class Converter {
+    public int[] convertUserInputToInt(String input) {
+        int[] output = new int[3];
+        for(int i = 0; i < 3; i++) {
+            output[i] = Integer.parseInt(input.substring(i, i + 1));
+        }
+
+        return output;
+    }
+}
