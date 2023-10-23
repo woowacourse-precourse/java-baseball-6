0bf05e51f7168081a0806cd176731a7528389f64
ikjo39 <chi4321@naver.com>
2023-10-23 21:47:07 +0900

    feat: 종료 입력 요구 문구 출력 및 종료 입력 기능 추가

---
+++ src/main/java/baseball/EndInput.java
@@ -0,0 +1,11 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class EndInput {
+    private final String input;
+
+    public EndInput() {
+        this.input = Console.readLine();
+    }
+}
--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -16,6 +16,7 @@ public class Game {
             score = calculateScore(computerNumbers, userNumbers);
             System.out.println(score.getHint());
         }
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
     private Score calculateScore(List<Integer> computerNumbers, List<Integer> userNumbers) {
