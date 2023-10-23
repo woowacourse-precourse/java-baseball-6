d35d9e11437ce6a0324044f8b093b4a990a366c5
nhlee98 <e5slnh9876@gmail.com>
2023-10-23 11:26:51 +0900

    refactor: GameNumbers의 검증 로직 분리
    GameNumbers가 변하는 이유가 하나만 되도록 검증 로직을 클래스로 분리한다.

--- src/main/java/baseball/domain/GameNumbers.java
+++ src/main/java/baseball/domain/GameNumbers.java
@@ -7,40 +7,16 @@ import java.util.stream.IntStream;
 
 public final class GameNumbers {
     public static final int LENGTH = 3;
-    private static final String VALID_NUMBER_LENGTH = String.format("^\\d{%d}$", LENGTH);
 
     private final List<GameNumber> gameNumbers;
 
     public GameNumbers(String gameNumbers) {
-        validate(gameNumbers);
+        GameNumbersValidator.validate(gameNumbers);
 
         this.gameNumbers = Arrays.stream(gameNumbers.split(""))
                 .map(Integer::parseInt)
                 .map(GameNumber::from)
                 .collect(Collectors.toList());
-    }
-
-    private void validate(String gameNumbers) {
-        validateNumberLength(gameNumbers);
-        validateDuplication(gameNumbers);
-    }
-
-    private void validateNumberLength(String gameNumbers) {
-        if (!gameNumbers.matches(VALID_NUMBER_LENGTH)) {
-            throw new IllegalArgumentException("Error : 게임 숫자는 3개의 수로 이루어져야 합니다.");
-        }
-    }
-
-    private void validateDuplication(String gameNumbers) {
-        if (countDistinctNumbers(gameNumbers) != LENGTH) {
-            throw new IllegalArgumentException("게임 숫자의 각 자리수는 중복될 수 없습니다.");
-        }
-    }
-
-    private int countDistinctNumbers(String gameNumbers) {
-        return (int) gameNumbers.chars()
-                .distinct()
-                .count();
     }
 
     public int countStrike(GameNumbers otherGameNumbers) {
---
+++ src/main/java/baseball/domain/GameNumbersValidator.java
@@ -0,0 +1,30 @@
+package baseball.domain;
+
+public class GameNumbersValidator {
+    private static final String VALID_NUMBER_LENGTH = String.format("^\\d{%d}$", GameNumbers.LENGTH);
+
+    private GameNumbersValidator() {}
+
+    static void validate(String gameNumbers) {
+        validateNumberLength(gameNumbers);
+        validateDuplication(gameNumbers);
+    }
+
+    private static void validateNumberLength(String gameNumbers) {
+        if (!gameNumbers.matches(VALID_NUMBER_LENGTH)) {
+            throw new IllegalArgumentException("Error : 게임 숫자는 3개의 수로 이루어져야 합니다.");
+        }
+    }
+
+    private static void validateDuplication(String gameNumbers) {
+        if (countDistinctNumbers(gameNumbers) != GameNumbers.LENGTH) {
+            throw new IllegalArgumentException("게임 숫자의 각 자리수는 중복될 수 없습니다.");
+        }
+    }
+
+    private static int countDistinctNumbers(String gameNumbers) {
+        return (int) gameNumbers.chars()
+                .distinct()
+                .count();
+    }
+}
