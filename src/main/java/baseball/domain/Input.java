ae89c6abbddb95b6577816607181fe23c43c56ec
AidenRoh <colohrby@gmail.com>
2023-10-23 21:01:17 +0900

    refactor: type change into character array

--- src/main/java/baseball/domain/Input.java
+++ src/main/java/baseball/domain/Input.java
@@ -1,9 +1,9 @@
 package baseball.domain;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 import java.util.ArrayList;
 import java.util.List;
-
-import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Input {
 
@@ -11,17 +11,17 @@ public class Input {
 
 
     public List<Integer> playerInput(int dataType) {
-        String[] player = readLine().split("");
+        char[] player = readLine().toCharArray();
         validation.runValidation(player, dataType);
         return convertIntoList(player);
     }
 
     // 인풋 값을 List<Integer> 로 변환합니다.
-    private List<Integer> convertIntoList(String[] target) {
+    private List<Integer> convertIntoList(char[] target) {
         List<Integer> convertedData = new ArrayList<>();
         //검증 후 입력값 파싱하기
-        for (String str: target) {
-            convertedData.add(Integer.valueOf(str));
+        for (char chr : target) {
+            convertedData.add(chr - '0');
         }
         return convertedData;
     }
