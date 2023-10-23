6a4acb044c8b907c110e5707113cbfcb3fe4b09f
seung-won-kim <hesjs5@skuniv.ac.kr>
2023-10-24 00:35:25 +0900

    Feat: 출력 문구 추가

--- src/main/java/baseball/Output.java
+++ src/main/java/baseball/Output.java
@@ -2,6 +2,10 @@ package baseball;
 
 public class Output {
 
+    public static void printIntroMessage() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
     public static void printUserNumbersMessage() {
         System.out.print("숫자를 입력해주세요 : ");
     }
@@ -12,5 +16,9 @@ public class Output {
 
     public static void printHint(final GameResult gameResult) {
         System.out.println(gameResult.toString());
+    }
+
+    public static void printWinningMessage() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 }
