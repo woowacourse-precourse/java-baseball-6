e3de560a40d28bedd7892763c3271638447a3248
jiwon1217 <koagjw@gmail.com>
2023-10-24 05:45:59 +0900

    :recycle: refactor: 메서드 이름 변경

--- src/test/java/baseball/util/ValidatorTest.java
+++ src/test/java/baseball/util/ValidatorTest.java
@@ -20,7 +20,7 @@ class ValidatorTest {
     @DisplayName("정확히 3개의 숫자를 입력하지 않으면 예외가 발생한다.")
     @ValueSource(strings = {"", " ", "1", "12", "1234"})
     void isValidNumbersLength_숫자_개수_예외처리_테스트(String numbers) {
-        assertThatThrownBy(() -> Validator.isValidNumbersLength(numbers))
+        assertThatThrownBy(() -> Validator.validateNumbersLength(numbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -28,7 +28,7 @@ class ValidatorTest {
     @DisplayName("숫자가 아닌 값을 입력하면 예외가 발생한다.")
     @ValueSource(strings = {"abc", "가나다", "1a2"})
     void isValidNumbersType_숫자_자료형_예외처리_테스트(String numbers) {
-        assertThatThrownBy(() -> Validator.isValidNumbersType(numbers))
+        assertThatThrownBy(() -> Validator.validateNumbersType(numbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -36,7 +36,7 @@ class ValidatorTest {
     @DisplayName("1보다 작은 값이 포함되었다면 예외가 발생한다.")
     @ValueSource(strings = {"012", "-112", "001"})
     void isValidNumbersRange_숫자_범위_예외처리_테스트(String numbers) {
-        assertThatThrownBy(() -> Validator.isValidNumbersRange(numbers))
+        assertThatThrownBy(() -> Validator.validateNumbersRange(numbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -44,7 +44,7 @@ class ValidatorTest {
     @DisplayName("중복된 숫자를 입력하면 예외가 발생한다.")
     @ValueSource(strings = {"333", "222", "111"})
     void isDuplicate_숫자_중복_예외처리_테스트(String numbers) {
-        assertThatThrownBy(() -> Validator.isDuplicate(numbers))
+        assertThatThrownBy(() -> Validator.validateNoDuplicate(numbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -60,7 +60,7 @@ class ValidatorTest {
     @DisplayName("숫자가 아닌 종료 문자를 입력하면 예외가 발생한다.")
     @ValueSource(strings = {"a", "가", "b"})
     void isValidRestartOrExitNumberType_재시작_또는_종료_숫자_자료형_예외처리_테스트(String numbers) {
-        assertThatThrownBy(() -> Validator.isValidRestartOrExitNumberType(numbers))
+        assertThatThrownBy(() -> Validator.validateRestartOrExitNumberType(numbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -68,7 +68,7 @@ class ValidatorTest {
     @DisplayName("1 또는 2가 아닌 숫자를 입력하면 예외가 발생한다.")
     @ValueSource(strings = {"0", "3", "123"})
     void isValidRestartOrExitNumber_재시작_또는_종료_숫자_일치_여부_예외처리_테스트(String numbers) {
-        assertThatThrownBy(() -> Validator.isValidRestartOrExitNumber(numbers))
+        assertThatThrownBy(() -> Validator.validateRestartOrExitNumberValue(numbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
