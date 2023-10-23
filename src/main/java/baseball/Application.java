e9beb8c1ef245cf7a258379a5b516c862ab9729b
HongYeseul <ghd8119@naver.com>
2023-10-23 20:53:07 +0900

    feat: 메인 클래스 기능 목록 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,10 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO: 상대방(컴퓨터)은 1에서 9까지 서로 다른 임의의 수(랜덤) 3개를 선택
+
+        // TODO: 사용자로부터 입력 받기
+
+        // TODO: 입력한 숫자에 대한 결과 출력
     }
 }
