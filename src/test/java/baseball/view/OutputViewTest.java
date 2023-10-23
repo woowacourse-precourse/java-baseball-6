baaf5e0b2e8add052cdf31ebe319760b31f522b0
Hyun Woo <hwsa10041@gmail.com>
2023-10-23 14:43:53 +0900

    chore : printResult() 테스트 기타 수정
    - 컨벤션 수정
    - 사용되지 않는 import 제거

--- src/test/java/baseball/view/OutputViewTest.java
+++ src/test/java/baseball/view/OutputViewTest.java
@@ -1,6 +1,5 @@
 package baseball.view;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -9,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class OutputViewTest {
 
@@ -31,10 +30,10 @@ class OutputViewTest {
     @Test
     void printGameStart() {
         // given
-        
+
         // when
         outputView.printGameStart();
-        
+
         // then
         String expectedOutput = "숫자 야구 게임을 시작합니다.\n";
         assertThat(outContent.toString()).isEqualTo(expectedOutput);
@@ -46,19 +45,20 @@ class OutputViewTest {
         // given
 
         // when
-        outputView.printResult(2,0);
+        outputView.printResult(2, 0);
 
         // then
         String expectedOutput = "2볼\n";
         assertThat(outContent.toString()).isEqualTo(expectedOutput);
     }
+
     @DisplayName("게임 한판 당 결과 메시지를 출력한다.")
     @Test
     void printResultWithStrike() {
         // given
 
         // when
-        outputView.printResult(0,2);
+        outputView.printResult(0, 2);
 
         // then
         String expectedOutput = "2스트라이크\n";
@@ -72,7 +72,7 @@ class OutputViewTest {
         // given
 
         // when
-        outputView.printResult(1,2);
+        outputView.printResult(1, 2);
 
         // then
         String expectedOutput = "1볼 2스트라이크\n";
@@ -85,7 +85,7 @@ class OutputViewTest {
         // given
 
         // when
-        outputView.printResult(0,0);
+        outputView.printResult(0, 0);
 
         // then
         String expectedOutput = "낫싱\n";
