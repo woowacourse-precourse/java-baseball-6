1ce22a63a7cc054de334ae7f326148cc1b9e88f0
choimung <103022503+choimung@users.noreply.github.com>
2023-10-23 17:32:53 +0900

    style : 변수 및 메서드 이름 수정 가독성이 좋은 코드를 작성하기 위해 변수 및 메서드의 이름을 재작성해보았습니다.

--- src/main/java/baseball/NumberCompare.java
+++ src/main/java/baseball/NumberCompare.java
@@ -3,38 +3,39 @@ package baseball;
 /**
  * 나는 숫자를 전달해주면 숫자를 비교하고 결과값을 출력해줄게
  */
+
 public class NumberCompare {
     private static final int CHANCE = 3;
-    private int ball;
-    private int strike;
+    private int balls;
+    private int strikes;
 
 
-    public int[] Compare(String userNumber, String computerNumber) {
+    public int[] calculateBallAndStrike(String userNumber, String computerNumber) {
         resetCount();
         CountStrike(userNumber, computerNumber);
         CountBall(userNumber, computerNumber);
-        return new int[]{ball, strike};
+        return new int[]{balls, strikes};
     }
 
     public void resetCount() {
-        ball = 0;
-        strike = 0;
+        balls = 0;
+        strikes = 0;
     }
 
 
     public void CountBall(String userNumber, String computerNumber) {
         for (int i = 0; i < CHANCE; i++) {
             if (computerNumber.contains(Character.toString(userNumber.charAt(i)))) {
-                ball++;
+                balls++;
             }
         }
-        ball = ball - strike;
+        balls = balls - strikes;
     }
 
     public void CountStrike(String userNumber, String computerNumber) {
         for (int i = 0; i < CHANCE; i++) {
             if (computerNumber.charAt(i) == userNumber.charAt(i)) {
-                strike++;
+                strikes++;
             }
         }
     }
