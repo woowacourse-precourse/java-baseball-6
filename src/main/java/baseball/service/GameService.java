085c81d2493e0cb91736f8828e6d14a35d01d0d4
Nara Park <skfk6352@naver.com>
2023-10-24 01:32:21 +0900

    <test>(<gameservice>): create method for making logic playing game

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -1,8 +1,12 @@
 package baseball.service;
 
 import baseball.util.ComputerUtil;
+import baseball.util.MessageConverter;
+import baseball.util.UserUtil;
+import baseball.validation.InputNumberValidator;
 
 import java.util.List;
+import java.util.Map;
 
 public class GameService {
 
@@ -10,9 +14,18 @@ public class GameService {
 
     /*
      * 게임 초기설정 메서드
-     * @Param input 플레이어의 숫자
      * */
     public static void setGame() {
         computerNumber = ComputerUtil.generateComputerNumber();
+    }
+
+    /*
+     * 게임 진행 메서드
+     * */
+    public static String playGame() {
+        String input = UserUtil.getUserNumber();
+        List<Integer> inputList = InputNumberValidator.validateInputNumber(input);
+        Map<String, Integer> resultMap = MessageConverter.getResultMessage(inputList, computerNumber);
+        return MessageConverter.getResultMessageToString(resultMap);
     }
 }
