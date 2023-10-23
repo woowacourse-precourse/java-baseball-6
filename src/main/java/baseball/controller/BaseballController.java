808325e7dd88183011af6df198dd2b391ffb30e2
minnim1010 <pms1010123@daum.net>
2023-10-24 00:17:13 +0900

    feat(controller): 숫자 야구 게임을 시작하는 로직 구현

---
+++ src/main/java/baseball/controller/BaseballController.java
@@ -0,0 +1,23 @@
+package baseball.controller;
+
+public class BaseballController {
+
+    public void run() {
+        boolean start = true;
+        while (start) {
+
+            playGame();
+            start = isRestart();
+        }
+    }
+
+    private void playGame() {
+        //TODO Auto-generated
+    }
+
+
+    private boolean isRestart() {
+        //TODO Auto-generated
+        return false;
+    }
+}
