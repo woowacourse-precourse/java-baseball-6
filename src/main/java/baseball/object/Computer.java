761c1c21fcb7d3939e5b78c9ea14a05910a6202e
GaHee99 <u1879@sookmyung.ac.kr>
2023-10-23 14:18:24 +0900

    feat: 정확한 사용자의 입력을 받으면, 자신의 숫자와 비교하여 결과 출력하기 #4

--- src/main/java/baseball/object/Computer.java
+++ src/main/java/baseball/object/Computer.java
@@ -2,14 +2,35 @@ package baseball.object;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Computer {
 
-    //  TODO(2) - 사용자의 입력을 받아, 맞는 입력인지 검사하기
+    //  TODO(4) - 사용자의 입력과 컴퓨터의 입력을 검사하기
+    public int[] ismatch(List<Integer> computerNum, List<Integer> userNum) {
+        int[] resultList = new int[3];
+        int strikeCount = 0;
+        int ballCount = 0;
+        int nothingCount = 0;
+
+        for (int index = 0 ; index<3 ; index++) {
+            if (Objects.equals(computerNum.get(index), userNum.get(index))) {
+                strikeCount +=1;
+            } else if (computerNum.contains(userNum.get(index))) {
+                ballCount +=1;
+            }
+        }
+        if (strikeCount+ballCount ==0 ){
+            nothingCount+=1;
+        }
+        resultList[0] = ballCount;
+        resultList[1] = strikeCount;
+        resultList[2] = nothingCount;
+
+        return resultList;
+    }
+
+    //  TODO(2, 3) - 사용자의 입력을 받아, 맞는 입력인지 검사하기
     public List<Integer> checkUserNum(String userNum) {
         final String REGEX = "[1-9]+";
 
