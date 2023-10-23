fe72e9b468234a4c4562c366db064e7ed3e73d27
sinminseok12 <sinminseok12@naver.com>
2023-10-24 01:30:16 +0900

    feat : 볼 개수를 계산하는 기능 구현

--- src/main/java/baseball/Result.java
+++ src/main/java/baseball/Result.java
@@ -1,6 +1,8 @@
 package baseball;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.IntStream;
 
 public class Result {
@@ -9,7 +11,7 @@ public class Result {
 
     public Result(List<Integer> computerNumbers, List<Integer> userNumbers) {
         this.strike = checkStrike(computerNumbers, userNumbers);
-        this.ball = ball;
+        this.ball = checkBall(computerNumbers, userNumbers);
     }
 
     private Integer checkStrike(List<Integer> userNumbers, List<Integer> computerNumbers) {
@@ -17,4 +19,12 @@ public class Result {
                 .filter(i -> userNumbers.get(i).equals(computerNumbers.get(i)))
                 .count();
     }
+
+    private Integer checkBall(List<Integer> userNumbers, List<Integer> computerNumbers) {
+        Set<Integer> setNumbers = new HashSet<>(userNumbers);
+        Set<Integer> setCompareNumbers = new HashSet<>(computerNumbers);
+        setNumbers.retainAll(setCompareNumbers);
+        return setNumbers.size() - checkStrike(userNumbers, computerNumbers);
+    }
 }
+
