12108b2300ba8af98db649519f1803881b96ca0c
LastCow9000 <sys19912002@hanmail.net>
2023-10-24 00:44:20 +0900

    refactor: input 객체의 책임 분리

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -1,14 +1,12 @@
 package baseball;
 
-import baseball.domain.UserBalls;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
     private static final String inputMessage = "숫자를 입력해주세요 : ";
-    public UserBalls inputBalls() {
+    public String inputBalls() {
         System.out.print(inputMessage);
-        String input = Console.readLine();
-        return new UserBalls(input);
+        return Console.readLine();
     }
 }
 
