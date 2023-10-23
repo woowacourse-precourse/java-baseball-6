0dd2442e41f72e999558ea153720a99d412ac075
mainfn <_@mainfn.dev>
2023-10-23 23:02:56 +0900

    refact: 적합한 메소드 명으로 변경

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -10,39 +10,38 @@ import baseball.view.OutputView;
 
 public final class GameController {
 
-  private final InputView inputView;
-  private final OutputView outputView;
-  private final AnswerCreator answerCreator;
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final AnswerCreator answerCreator;
 
-  public GameController(
-      final InputView inputView,
-      final OutputView outputView,
-      final AnswerCreator answerCreator
-  ) {
-    this.inputView = inputView;
-    this.outputView = outputView;
-    this.answerCreator = answerCreator;
-  }
-
-  public void run() {
-    final Answer answer = answerCreator.create();
-    final GameStatus status = play(answer);
-
-    if (GameStatus.REPLAY == status) {
-      run();
+    public GameController(
+            final InputView inputView,
+            final OutputView outputView,
+            final AnswerCreator answerCreator
+    ) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.answerCreator = answerCreator;
     }
-  }
 
-  private GameStatus play(final Answer answer) {
-    final GuessResult result = answer.guess(
-        Guess.of(inputView.inputBallNumbers())
-    );
+    public void run() {
+        final Answer answer = answerCreator.create();
+        guessRecursive(answer);
 
-    outputView.printResult(result);
-
-    if (!result.isThreeStrike()) {
-      return play(answer);
+        if (GameStatus.REPLAY == inputView.inputNextGameStatus()) {
+            run();
+        }
     }
-    return inputView.inputWillReplay();
-  }
+
+    private void guessRecursive(final Answer answer) {
+        final GuessResult result = answer.guess(
+                Guess.of(inputView.inputBallNumbers())
+        );
+
+        outputView.printResult(result);
+
+        if (!result.isThreeStrike()) {
+            guessRecursive(answer);
+        }
+    }
 }
