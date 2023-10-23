50e7b68ddaddd66c7f105f6fcc5985d4ad6953da
minsu20 <syt07100@gmail.com>
2023-10-23 22:26:02 +0900

    feat: 게임 결과 (힌트) 생성

---
+++ src/main/java/baseball/util/HintGenerator.java
@@ -0,0 +1,29 @@
+package baseball.util;
+
+import baseball.domain.BaseBall;
+
+public class HintGenerator {
+
+    public static BaseBall getHint(int[] answer, int[] guess) {
+        if(answer.length!=guess.length) throw new IllegalArgumentException();
+        int size=answer.length;
+        BaseBall result = new BaseBall();
+        for (int i = 0; i < size ; i++) {
+            if (answer[i] == guess[i]) {
+                result.addStrike();
+            } else if (contains(guess, answer[i])) {
+                result.addBall();
+            }
+        }
+        return result;
+    }
+
+    private static boolean contains(int[] array, int element) {
+        for (int number : array) {
+            if (number == element) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
