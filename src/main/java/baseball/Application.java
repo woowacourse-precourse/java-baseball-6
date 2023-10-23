ac0e65fae566dc63f08951b24e7035abe29fe6b9
sojungpp <sophie2458@naver.com>
2023-10-24 03:59:29 +0900

    feat: 사용자로부터 3자리의 수 입력 받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -24,5 +24,26 @@ public class Application {
             }
         }
 
+        // 3자리의 수 입력 받기
+        System.out.println("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+
+        List<Integer> userNums = new ArrayList<>();
+        Arrays.stream(input.split("")).forEach(str -> {
+            try {
+                Integer num = Integer.parseInt(str);
+                // 각 자릿수끼리 중복된 수를 입력한 경우 || 각 자릿수가 1 ~ 9까지의 수가 아닌 경우
+                if (userNums.contains(num) || num == 0) throw new IllegalArgumentException();
+                userNums.add(num);
+            }
+            // 숫자 외의 문자를 입력한 경우
+            catch (NumberFormatException e) {
+                throw new IllegalArgumentException();
+            }
+        });
+
+        // 입력 받은 수가 3자리가 아닌 경우
+        if (userNums.size() != 3) throw new IllegalArgumentException();
+
     }
 }
