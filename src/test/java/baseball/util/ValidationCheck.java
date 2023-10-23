240d6e3a812c39eeac14092ae644bb5a59720176
SeolHuiGwan9478 <shg5977@naver.com>
2023-10-23 15:36:19 +0900

    [feat] : ValidationCheck 클래스 구현 - 유효성 검사와 관련된 클래스 - validateNumber : 매개변수로 받은 문자열이 숫자인지 확인, 아니면 에러 - validateUserCommand : 재시작/종료 커맨드 입력시 앞서 구현한 validateNumber()를 이용하여 숫자 유효성 검사 후 입력된 숫자가 1 또는 2인지 확인 - validateUserNumber : 사용자가 입력한 문자열이 숫자인지 확인하는 메서드. 해당 메서드 또한 validateNumber()를 이용하여 입력된 문자열이 숫자인지 확인 후, 3자리가 맞는지 확인한다. 아니라면 에러 발생 - validateDuplicate : 사용자가 입력한 숫자에 중복이 있는지 확인하는 메서드이다. Set 자료형의 .add 메서드가 중복된 숫자가 들어올 경우 false를 반환하는 점을 이용하여 중복 확인을 시도하였다. 만약 중복된 숫자가 없다면 true, 있다면 false 반환

---
+++ src/test/java/baseball/util/ValidationCheck.java
@@ -0,0 +1,43 @@
+package baseball.util;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ValidationCheck {
+    // 유효성 검사 : 숫자 확인
+    private int validateNumber(String cmdStr){
+        try {
+            return Integer.parseInt(cmdStr);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException("게임 종료");
+        }
+    }
+
+    // 유효성 검사 : 재시작/종료 확인
+    public boolean validUserCommand(String cmdStr){
+        int cmdInt = validateNumber(cmdStr);
+        if(cmdInt == 1 || cmdInt == 2) return true;
+        return false;
+    }
+
+    // 유효성 검사 : 숫자 자리수 확인
+    public boolean validUserNumber(String cmdStr){
+        int cmdInt = validateNumber(cmdStr);
+        int digit = Integer.toString(cmdInt).length();
+        if(digit == 3){
+            return validDuplicate(cmdStr); // 중복 체크
+        }
+        return false;
+    }
+
+    // 유효성 검사 : 중복 숫자 검사
+    private boolean validDuplicate(String cmdStr){
+        Set<Character> uniqueChars = new HashSet<>();
+        for(char c : cmdStr.toCharArray()){
+            if(!uniqueChars.add(c)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
