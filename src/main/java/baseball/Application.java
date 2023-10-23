cd21189b33258647d17bdff1d888ecbba744e3c8
myonjin <kjinabc@gmail.com>
2023-10-24 03:08:05 +0900

    Feat : StringToList 매서드

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -25,24 +25,36 @@ public class Application {
         if (!input.matches("^[0-9]+$")) {
             return false;
         }
-        if (input.charAt(0) == input.charAt(1) || input.charAt(0) == input.charAt(2) || input.charAt(1) == input.charAt(2)) {
+        if (input.charAt(0) == input.charAt(1) || input.charAt(0) == input.charAt(2) || input.charAt(1) == input.charAt(
+                2)) {
             return false;
         }
         return true;
+    }
+
+    public static List<Integer> stringToList(String input) {
+        char [] strChar = input.toCharArray();
+        List<Integer> userNum = new ArrayList<Integer>();
+        for (int i=0; i < input.length(); i++){
+            int n = Character.getNumericValue(strChar[i]);
+            userNum.add(n);
+        }
+        return userNum;
     }
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
         System.out.println("숫자 야구 게임을 시작합니다.");
-
+        List<Integer> computerNum = randomNumMaker();
         while (true) {
             System.out.print("숫자를 입력해주세요 : ");
             String input = Console.readLine();
             if (!isValidNumber(input)) {
                 throw new IllegalArgumentException();
-            } else{
-                System.out.println("진실");
+            } else {
+                List<Integer> userNum = stringToList(input);
+                System.out.println(userNum+""+computerNum);
             }
         }
 
