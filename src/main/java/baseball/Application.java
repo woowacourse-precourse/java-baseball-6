88b4d31d1942704e084a662067221e1a8ce1fd81
SeungMin Park <tonydragon@naver.com>
2023-10-23 16:11:00 +0900

    커밋이 왜 안되지

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,7 +2,7 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("fork 된건가?");
+        System.out.println("fork 된건가??");
         // TODO: 프로그램 구현
     }
 }
