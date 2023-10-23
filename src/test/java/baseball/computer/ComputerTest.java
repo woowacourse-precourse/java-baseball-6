f7a76bc39259ad82c9624278b12923d0a997c168
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-10-23 16:31:00 +0900

    test(ComputerTest): 생성한 정답 자리수 테스트 구현

--- src/test/java/baseball/computer/ComputerTest.java
+++ src/test/java/baseball/computer/ComputerTest.java
@@ -1,2 +1,25 @@
-package baseball.computer;public class ComputerTest {
+package baseball.computer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ComputerTest {
+    Computer computer;
+    GameNumbers answer;
+
+    @BeforeEach
+    void setComputer() {
+        computer = new Computer();
+        answer = computer.generateAnswer();
+    }
+
+    @DisplayName("정답은 임의의 3자리 수여야 한다.")
+    @Test
+    void 정답_자리수_테스트() {
+        //then
+        assertThat(answer.size()).isEqualTo(3);
+    }
 }
