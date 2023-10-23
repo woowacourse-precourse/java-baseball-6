1825ae0242107e6ced50a9fa74175fb6a92a35a1
sangeun-b <baek0007@algonquinlive.com>
2023-10-23 23:22:53 +0900

    feat: 게임 시작 메세지 출력

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,4 +1,16 @@
 package baseball.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class InputView {
+    public static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    public static final String OPTION_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public void startGame() {
+        System.out.println(START_MESSAGE);
+    }
 }
