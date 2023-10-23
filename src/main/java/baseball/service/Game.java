46edc052d591c69dfc6f3ab109af40083f1c6786
olsohee <dlthgml0108@daum.net>
2023-10-23 16:34:12 +0900

    refactor: UI 로직 분리

---
+++ src/main/java/baseball/service/Game.java
@@ -0,0 +1,31 @@
+package baseball.service;
+
+import java.util.List;
+
+import baseball.domain.Score;
+import baseball.dto.ScoreDto;
+import baseball.util.Util;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+public class Game {
+
+
+    public static void play() {
+
+        List<Integer> answerNumbers = Util.getRandomNumber(3);
+
+        while(true) {
+            OutputView.askUserNumbers();
+            List<Integer> userNumbers = InputView.askUserNumbers();
+
+            Score score = Score.getScore(answerNumbers, userNumbers);
+            ScoreDto scoreDto = new ScoreDto(score.getStrike(), score.getBall());
+            OutputView.printScore(scoreDto);
+
+            if(score.getStrike() == 3) {
+                break;
+            }
+        }
+    }
+}
