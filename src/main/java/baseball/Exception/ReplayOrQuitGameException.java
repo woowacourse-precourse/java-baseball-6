f00ede2b9b3d4ce2749be39f7beeccd26bf50f57
hayannn <dlgkdis801@naver.com>
2023-10-23 20:25:55 +0900

    :recycle: 숫자의 종류가 1,2가 아닌 경우를 문자 입력을 포함하기 위해 변수명 변경

--- src/main/java/baseball/Exception/ReplayOrQuitGameException.java
+++ src/main/java/baseball/Exception/ReplayOrQuitGameException.java
@@ -6,7 +6,7 @@ public class ReplayOrQuitGameException {
 
     public void selectReplayOrQuitGameException(String numberCasesOfString) {
         notOneDigitNumberCase(numberCasesOfString); // 숫자의 개수 예외
-        notRangeOfNumberCase(numberCasesOfString); // 숫자의 종류 예외
+        notRangeOfNumberOrCharacterCase(numberCasesOfString); // 숫자의 종류가 1,2가 아닌 경우와 문자 입력 예외
     }
 
     //숫자의 개수가 1개가 아닌 경우
@@ -16,10 +16,10 @@ public class ReplayOrQuitGameException {
         }
     }
 
-    //숫자의 종류가 1,2가 아닌 경우
-    public void notRangeOfNumberCase(String numberCasesOfString) {
+    //숫자의 종류가 1,2가 아닌 경우와 문자를 입력한 경우
+    public void notRangeOfNumberOrCharacterCase(String numberCasesOfString) {
         if (!numberCasesOfString.matches(RE_NUMBER_REGEX)) {
-            throw new IllegalArgumentException("1,2의 수 중 하나만을 입력해야 합니다.");
+            throw new IllegalArgumentException("1,2의 숫자 중 하나만을 입력해야 합니다.");
         }
     }
 }
