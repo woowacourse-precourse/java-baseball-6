40603933355ffeac208d4f40070a40b305d20b1d
SeongIl <biuld1234@gmail.com>
2023-10-24 00:55:45 +0900

    [Restart] Refactor: 볼,스트라이크 계산 로직을 메서드로 변경

--- src/main/java/baseball/RestartApplication.java
+++ src/main/java/baseball/RestartApplication.java
@@ -6,7 +6,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class RestartApplication {
     private static int[] pickedNumber = new int[3];
     private static int[] userNumbers = new int[3];
-    public static void selectThreeNumber(){
+
+    private static int strikeCount = 0;
+    private static int ballCount = 0;
+
+    public static void selectThreeNumber() {
         int index = 0;
         while (index < 3) {
             boolean isDuplicate = false;
@@ -27,7 +31,7 @@ public class RestartApplication {
 
     }
 
-    public static String getUserInput(){
+    public static String getUserInput() {
 
 
         String userInput = Console.readLine();
@@ -36,7 +40,8 @@ public class RestartApplication {
         }
         return userInput;
     }
-    public static void convertUserInput(String userInput){
+
+    public static void convertUserInput(String userInput) {
         String[] userInputChars = userInput.split("");
 
         try {
@@ -47,19 +52,36 @@ public class RestartApplication {
                 if (userNumbers[i] == 0) {
                     throw new IllegalArgumentException("1~9까지의 숫자를 입력하세요.");
                 }
+                //서로 다른 숫자들로만 구성되어 있는가?
+                for (int j = i + 1; j < 3; j++) {
+                    if (userNumbers[i] == Integer.parseInt(userInputChars[j])) {
+                        throw new IllegalArgumentException("서로 다른 숫자를 입력하세요.");
+                    }
+                }
             }
         } catch (NumberFormatException ex) {
             // 숫자들로만 구성되어 있는가?
             throw new IllegalArgumentException("숫자만 입력하세요.");
         }
     }
+
+    public static void countStrikeAndBall() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if ((userNumbers[i] == pickedNumber[j]) && (i == j)) {
+                    strikeCount++;
+                } else if (userNumbers[i] == pickedNumber[j]) {
+                    ballCount++;
+                }
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
 
 
 
-
-        int strikeCount = 0;
-        int ballCount = 0;
 
         //1. 서로 다른 임의의 수 3개 선택
         selectThreeNumber();
@@ -75,32 +97,12 @@ public class RestartApplication {
             System.out.print("숫자를 입력해주세요 : ");
             convertUserInput(getUserInput());
 
-//            - 3개의 입력이 들어왔는가?
 
-
-
-
-//             - 서로 다른 숫자들로만 구성되어 있는가?
-            for (int i = 0; i < 3; i++) {
-                for (int j = i + 1; j < 3; j++) {
-                    if (userNumbers[i] == userNumbers[j]) {
-                        throw new IllegalArgumentException("서로 다른 숫자를 입력하세요.");
-                    }
-                }
-            }
 //            4. 입력값을 제시된 규칙에 따라 출력시키기
 //                    - 로직 구현 하기(볼, 스트라이크, 낫싱)
 //            - 형식에 맞춰 출력문 구성
+            countStrikeAndBall();
 
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    if ((userNumbers[i] == pickedNumber[j]) && (i == j)) {
-                        strikeCount++;
-                    } else if (userNumbers[i] == pickedNumber[j]) {
-                        ballCount++;
-                    }
-                }
-            }
             if (ballCount != 0) {
                 System.out.print(ballCount + "볼 ");
             }
@@ -112,7 +114,7 @@ public class RestartApplication {
             }
             System.out.println();
             //5. 게임이 종료된 후, 다시시작 & 종료 선택 기능 넣기
-            if(strikeCount == 3){
+            if (strikeCount == 3) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 userInput = Console.readLine();
@@ -123,15 +125,15 @@ public class RestartApplication {
                 }
 
                 //숫자들로만 구성되어 있는가?
-                try{
-                    int exitNumber= Integer.parseInt(userInput);
+                try {
+                    int exitNumber = Integer.parseInt(userInput);
                     //            - 1~2까지의 범위를 지키는가?:
-                    if((exitNumber != 1) && (exitNumber !=2)){
+                    if ((exitNumber != 1) && (exitNumber != 2)) {
                         throw new IllegalArgumentException("1~2 숫자를 입력하세요.");
                     }
-                    if(exitNumber ==1){
+                    if (exitNumber == 1) {
                         //다시 선택
-                        index =0;
+                        index = 0;
                         while (index < 3) {
                             boolean isDuplicate = false;
                             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -150,10 +152,10 @@ public class RestartApplication {
                             System.out.println("i1 = " + i1);
                         }
                     }
-                    if(exitNumber == 2){
-                       break;
+                    if (exitNumber == 2) {
+                        break;
                     }
-                }catch (NumberFormatException ex){
+                } catch (NumberFormatException ex) {
                     throw new IllegalArgumentException("숫자를 입력하세요.");
                 }
             }
