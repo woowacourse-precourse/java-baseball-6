f1a0eb84adf0085e1f581afbde731bb892ea7dc3
임강호 <55732952+Icln@users.noreply.github.com>
2023-10-24 00:02:06 +0900

    refactor: try-catch 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,31 +9,27 @@ public class Application {
         System.out.println("숫자 야구 게임을 시작합니다.");
         PitchResult pitchResult = new PitchResult(randomNumber);
 
-        try {
-            while (true) {
-                System.out.printf("숫자를 입력하세요 : ");
-                String inputString = Console.readLine();
-                InputRange.inputValidation(inputString);
+        while (true) {
+            System.out.printf("숫자를 입력하세요 : ");
+            String inputString = Console.readLine();
+            InputRange.inputValidation(inputString);
 
-                pitchResult.judgePitch(inputString);
-                pitchResult.printPitchResult();
+            pitchResult.judgePitch(inputString);
+            pitchResult.printPitchResult();
 
-                if (pitchResult.getStrike() == PitchRange.MAX_STRIKES.getValue()) {
-                    int gameOption = Integer.parseInt(Console.readLine());
-                    GameOption.gameOptionValidation(gameOption);
-                    if (gameOption == GameOption.RESTART_GAME.getOption()) {
-                        randomNumber.randomNumberGenerate();
-                    }
-                    if (gameOption == GameOption.QUIT_GAME.getOption()) {
-                        break;
-                    }
+            if (pitchResult.getStrike() == PitchRange.MAX_STRIKES.getValue()) {
+                int gameOption = Integer.parseInt(Console.readLine());
+                GameOption.gameOptionValidation(gameOption);
+                if (gameOption == GameOption.RESTART_GAME.getOption()) {
+                    randomNumber.randomNumberGenerate();
                 }
-                pitchResult.pitchClear();
+                if (gameOption == GameOption.QUIT_GAME.getOption()) {
+                    break;
+                }
             }
-        } catch (IllegalArgumentException e) {
-            System.out.println("입력이 잘못되었습니다.");
-            throw e;
+            pitchResult.pitchClear();
         }
+
     }
 }
 
