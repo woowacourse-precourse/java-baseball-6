6ff4149a18897c1c612e8ad9bf872cde68fd88f3
parking0 <p980710@gmail.com>
2023-10-23 15:05:35 +0900

    refactor : 기능3 변경

--- src/main/java/baseball/Judge.java
+++ src/main/java/baseball/Judge.java
@@ -1,2 +1,51 @@
-package baseball;public class Judge {
+package baseball;
+
+import java.util.List;
+
+public class Judge {
+
+    public boolean compare(Computer computer, Player player){
+        List<Integer> a = computer.getRandomNums();
+        List<Integer> b = player.getSelectedNums();
+
+        int sameNum = countSameNum(a,b);
+        int strikes = countStrike(a,b);
+        int balls = sameNum-strikes;
+
+        Score score = new Score();
+
+        if(strikes == 0 && balls == 0)
+            score.setNothing(true);
+
+        score.setStrike(strikes);
+        score.setBall(balls);
+
+        boolean success = score.print(score);
+        if(success){
+            Restart restart = new Restart();
+            restart.askPlayer();
+        }
+
+        return success;
+    }
+
+    public int countSameNum(List<Integer> a, List<Integer> b){
+        int result = 0;
+        for(int i=0; i<3; i++){
+            if(a.contains(b.get(i)))
+                result++;
+        }
+        return result;
+    }
+
+    public int countStrike(List<Integer> a, List<Integer> b){
+        int result = 0;
+        for(int i=0; i<3; i++){
+            if(a.get(i) == b.get(i))
+                result++;
+        }
+        return result;
+    }
+
 }
+
--- src/main/java/baseball/PlayGame.java
+++ src/main/java/baseball/PlayGame.java
@@ -1,8 +1,19 @@
 package baseball;
 
 public class PlayGame {
+
     public void run(){
         System.out.println("숫자 야구 게임을 시작합니다.");
+        Computer computer = new Computer();
+
+        //System.out.println("컴퓨터 : " + computer.getRandomNums());
+
         boolean success = false;
+
+        while(!success){
+            Player player = new Player();
+            Judge judge = new Judge();
+            success = judge.compare(computer,player);
+        }
     }
 }
