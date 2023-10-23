5d562bcdc82f5fbd4c997d5369e2421040e2dcd4
20191777 <20191777@o365.hanbat.ac.kr>
2023-10-23 21:45:35 +0900

    FEAT: 게임결과 판정기능과 숫자 변수 클래스 추가 - 게임의 결과를 판정 하는 함수 구현 - 나(mine)와 상대방(computer) 변수에 대한 Nums 클래스 구현 - 숫자 변수의 변경을 최소화 하기 위해 private 설정함

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,6 +7,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+
+    static Nums nums = new Nums();
+
     public static void getNums(){   // 나와 상대방(컴퓨터)의 숫자 생성
         // 상대방(컴퓨터) 숫자
         List<Integer> computer = new ArrayList<>();
@@ -26,14 +29,52 @@ public class Application {
         }
         List<Integer> mine = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
-            int num = inputNumber.charAt(i);
+            int num = Character.getNumericValue(inputNumber.charAt(i));
             if (mine.contains(num)) {  // 중복입력 예외처리
                 throw new IllegalArgumentException();
             }else mine.add(num);
         }
+        nums.setComputer(computer);
+        nums.setMine(mine);
     }
+
+    public static void getGameResultJudgment(List computer, List mine){
+        // 스트라이크 개수 세기
+        int strike = 0;
+        for (int i = 0; i < 3; i++) {
+            if (computer.get(i) == mine.get(i)){
+                strike ++;
+            }
+        }
+        // 볼 개수 세기
+        int ball = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i != j && computer.get(i) == mine.get(j)){
+                    ball ++;
+                }
+            }
+        }
+        if (ball == 0 && strike == 0) System.out.println("낫싱");
+        else if (ball > 0 && strike == 0){
+            System.out.printf("%d볼", ball);
+            System.out.println();
+        }
+        else if(ball == 0 && strike > 0){
+            System.out.printf("%d스트라이크", strike);
+            System.out.println();
+        }
+        else{
+            System.out.printf("%d볼 %d스트라이크", ball, strike);
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        getNums();  // 숫자 입력 받기
+        for (int i = 0; i < 4; i++) {
+            getNums();  // 숫자 입력 받기
+            getGameResultJudgment(nums.getComputer(), nums.getMine());
+        }
     }
 }
---
+++ src/main/java/baseball/Nums.java
@@ -0,0 +1,29 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Nums {
+    private List<Integer> computer = new ArrayList<>();
+    private List<Integer> mine = new ArrayList<>();
+
+//    public Nums(List computer, List inputNumber){
+//        this.computer = computer;
+//        this.inputNumber = inputNumber;
+//    }
+    public List<Integer> getComputer() {
+        return computer;
+    }
+
+    public List<Integer> getMine() {
+        return mine;
+    }
+
+    public void setComputer(List<Integer> computer) {
+        this.computer = computer;
+    }
+
+    public void setMine(List<Integer> inputNumber) {
+        this.mine = inputNumber;
+    }
+}
