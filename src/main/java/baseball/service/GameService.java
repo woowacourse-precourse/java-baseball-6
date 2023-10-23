8586563b941f685c70bf5bb684233b8aba9928d4
leedohyun <lucky77795@naver.com>
2023-10-23 19:31:43 +0900

    refactor: service에서 전체적인 게임 진행의 세부 로직을 맡도록 변경.

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -2,61 +2,61 @@ package baseball.service;
 
 import baseball.domain.ComputerNum;
 import baseball.domain.UserNum;
+import camp.nextstep.edu.missionutils.Console;
 
 public class GameService {
 
     private final UserNum userNum;
     private final ComputerNum computerNum;
-    private int strike;
-    private int ball;
+    private final BallCounter ballCounter;
+    private Integer strike;
+    private Integer ball;
 
-    public GameService(UserNum userNum) {
-        this.userNum = userNum;
+    public GameService() {
+        this.userNum = new UserNum();
         this.computerNum = new ComputerNum();
-        this.strike = 0;
-        this.ball = 0;
+        this.ballCounter = new BallCounter();
     }
 
-    private void countStrike(boolean[] isChecked) {
-        for (int i = 0; i < 3; i++) {
-            if (userNum.getNum().get(i) == computerNum.get(i)) {
-                this.strike++;
-                isChecked[i] = true;
-            }
-        }
+    public void inputUserNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+        userNum.setNum(Console.readLine());
     }
 
-    private void countBall(boolean[] isChecked) {
-        for (int i = 0; i < enterNum.size(); i++) {
-            if (!isChecked[i] && computerNum.contains(enterNum.get(i))) {
-                ball++;
-            }
-        }
-    }
+    public void showBallCount() {
+        int correctNumberSize = ballCounter.countCorrect(userNum.getNum(), computerNum.getNum());
+        strike = ballCounter.countStrike(userNum.getNum(), computerNum.getNum());
+        ball = correctNumberSize - strike;
 
-    public boolean checkResult() {
-        boolean[] checkedBall = new boolean[computerNum.size()];
-        int strikeCnt = countStrike(checkedBall);
-        int ballCnt = countBall(checkedBall);
-
-        if (strikeCnt == 0 && ballCnt == 0) {
+        if (strike == 0 && ball == 0) {
             System.out.println("낫싱");
-        } else if (strikeCnt == 0 && ballCnt > 0) {
-            System.out.printf("%d볼\n", ballCnt);
-        } else if (ballCnt == 0 && strikeCnt > 0) {
-            System.out.printf("%d스트라이크\n", strikeCnt);
-        } else {
-            System.out.printf("%d볼 %d스트라이크\n", ballCnt, strikeCnt);
+        } else if (strike == 0 && ball > 0) {
+            System.out.printf("%d볼\n", ball);
+        } else if (strike > 0 && ball == 0) {
+            System.out.printf("%d스트라이크\n", strike);
+        } else if (strike > 0 && ball > 0){
+            System.out.printf("%d볼 %d스트라이크\n", ball, strike);
         }
-
-        return checkGameOver(strikeCnt);
     }
 
-    private boolean checkGameOver(int strikeCount) {
-        if (strikeCount == 3) {
+    public boolean checkGameOver() {
+        if (strike == 3) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             return true;
         }
+        return false;
+    }
+
+    public boolean askRestartGame() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        Integer enterValue = Integer.parseInt(Console.readLine());
+
+        if (enterValue == 1) {
+            return true;
+        } else if (enterValue != 2) {
+            throw new IllegalArgumentException();
+        }
+
         return false;
     }
 }
