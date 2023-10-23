a209ae88d7b35dad2fe587b08de4bcea2593baa1
TAEMIN\gkxoa <gkxoals33@gmail.com>
2023-10-23 23:54:39 +0900

    스트라이크 볼 위치 변경,예외처리

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -12,6 +12,9 @@ public class User {
         List<Integer> userNumbers = new ArrayList<>();
         System.out.print("숫자를 입력해주세요 : ");
         String userNumber = Console.readLine();
+        if(userNumber.length()!=3){
+            throw new IllegalArgumentException("3자리 수를 입력해 주세요.");
+        }
        for(int i=0; i< userNumber.length();i++) {
             char userchar = userNumber.charAt(i);
             int translateNumber = Character.getNumericValue(userchar);
