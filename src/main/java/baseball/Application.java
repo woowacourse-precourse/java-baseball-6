002077e32f29e22e5c58600429ce88c779490f2e
Yang-soeun <soeun8636@naver.com>
2023-10-23 19:06:29 +0900

    [refactor]: else 예약어를 쓰지 않도록 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,20 +7,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
-
   private static final int INPUT_SIZE = 3;
   public static void main(String[] args) {
     // TODO: 프로그램 구현
 
     System.out.println("숫자 야구 게임을 시작합니다.");
     playGame();
-
+    System.out.println("게임 종료");
   }
 
   private static void playGame() {
     List<Integer> computer = createAnswer();//서로 다른 3자리 수 생성
-    int ball=0, strike=0, pos=0;
-    int restart = 0;
+    int strike = 0, ball, pos;
+    int restart;
 
     while (strike != INPUT_SIZE) {
       ball = 0;
@@ -35,29 +34,32 @@ public class Application {
       for (int i = 0; i < INPUT_SIZE; i++) {
         int m = Character.getNumericValue(num.charAt(i));
         int index = computer.indexOf(m);
-        if (index != -1) {
-          if (index == pos) {
-            strike++;
-          } else {
-            ball++;
-          }
+
+        if (index != pos && index != -1) {
+          ball++;
+        }
+        if (index == pos) {
+          strike++;
         }
         pos++;
       }
 
       getHint(ball, strike);//입력한 수에 대한 결과 출력
+    }
 
-      if (strike == INPUT_SIZE) {//3개의 숫자를 모두 맞힐경우
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        restart = Integer.parseInt(readLine());
-      }
+    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
+    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    restart = Integer.parseInt(readLine());
 
-      if (restart == 1) {
-        playGame();
-      } else if (restart == 2) {
-        System.out.println("게임 종료");
-      }
+    doRestart(restart);
+  }
+
+  private static void doRestart(int restart) {
+    if (restart != 1 && restart != 2) {
+      throw new IllegalArgumentException("1아니면 2를 입력 해야 합니다.");
+    }
+    if (restart == 1) {
+      playGame();
     }
   }
 
@@ -69,7 +71,6 @@ public class Application {
     if (num.length() != INPUT_SIZE) {//3자리 수인지 확인
       throw new IllegalArgumentException("세자리 수를 입력 해야 합니다.");
     }
-
     for (int i = 0; i < INPUT_SIZE-1; i++) {//중복된 숫자로 이루어져 있는지 확인
       String s1 = num.substring(i, i + 1);
       String s2 = num.substring(i + 1);
