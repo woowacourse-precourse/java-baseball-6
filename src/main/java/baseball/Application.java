9585f80310ced049bbc2ef69ab4f68f1811c35db
songsunkook <songsunkook@gmail.com>
2023-10-23 11:41:52 +0900

    feat: Controller 게임 전반 로직 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,7 +6,6 @@ public class Application {
     private static final BaseballController baseballController = new BaseballController();
     public static void main(String[] args) {
         do {
-            baseballController.initGame();
             baseballController.playGame();
         } while (baseballController.restartGame());
     }
--- src/main/java/controller/BaseballController.java
+++ src/main/java/controller/BaseballController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import domain.Number;
 import view.InputView;
 import view.OutputView;
 
@@ -7,17 +8,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BaseballController {
-
-    public void initGame() {
-
-    }
+    private List<Number> numbers = new ArrayList<>();
 
     public void playGame() {
-        
+        OutputView.startGame();
+        do {
+            playRound();
+        } while (!isEnd());
     }
 
     private void playRound() {
+        OutputView.inputNumber();
+        numbers = inputNumbers();
+        printResult();
+    }
 
+    private List<Number> inputNumbers() {
+        return InputView.inputNumbers().stream()
+                .map(Number::new)
+                .toList();
+    }
+
+    private void printResult() {
+        OutputView.printResult(Number.getStrike(numbers), Number.getBall(numbers));
+    }
+
+    private boolean isEnd() {
+        return Number.getStrike(numbers) == 3;
     }
 
     public boolean restartGame() {
