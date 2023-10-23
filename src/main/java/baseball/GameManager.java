7a4e503dc07167b973b6d8bed83cdd69e1f3cb1a
Zho <chlwlgh1011@naver.com>
2023-10-24 01:50:45 +0900

    Class 분류

---
+++ src/main/java/baseball/GameManager.java
@@ -0,0 +1,12 @@
+package baseball;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class GameManager {
+    public void start() {
+        BufferedReader num = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+}
