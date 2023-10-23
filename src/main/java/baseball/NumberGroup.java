cefcb00f60479a48ef39a7d32893342b71abba14
HongSP99 <tmdvy0805@naver.com>
2023-10-23 23:58:52 +0900

    feat : Number객체를 List Collection으로 가지는 일급 컬렉션 클래스 생성

---
+++ src/main/java/baseball/NumberGroup.java
@@ -0,0 +1,9 @@
+package baseball;
+
+import java.util.List;
+public class NumberGroup {
+    private List<Number> numbers;
+    public NumberGroup(List<Number> numbers){
+        this.numbers = numbers;
+    }
+}
