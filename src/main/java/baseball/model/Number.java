6ca630197494d657161a2d647ed10232ce6cff5f
SB8919-LAPTOP\sb891 <sb8919@naver.com>
2023-10-23 22:56:10 +0900

    fix(Number): getBallCount 메서드 수정

--- src/main/java/baseball/model/Number.java
+++ src/main/java/baseball/model/Number.java
@@ -47,7 +47,7 @@ public class Number {
     public int getBallCount(ArrayList<Integer> inputNumber) {
         int ball = 0;
         for (int i = 0; i < 3; i++) {
-            if (number.get(i).equals(inputNumber.get(i))) {
+            if (number.contains(inputNumber.get(i)) && !number.get(i).equals(inputNumber.get(i))) {
                 ball++;
             }
         }
