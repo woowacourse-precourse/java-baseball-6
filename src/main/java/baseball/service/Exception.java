c98d117b4ccdf9f9fd44a0dcbdf9b72075f2a276
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2023-10-23 22:17:04 +0900

    feat: 플레이어가 잘못된 값을 입력했을 때 예외 처리를 한다

--- src/main/java/baseball/service/Exception.java
+++ src/main/java/baseball/service/Exception.java
@@ -1,13 +1,43 @@
 package baseball.service;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Exception {
     public void validateInputNumber(String number) {
-        String exceptionMessage = "";
+        isThreeDigits(number);
+        isOneToNine(number);
+        isDifferentDigits(number);
+    }
 
+    private void isThreeDigits(String number) {
         if (number.length() != 3) {
-            exceptionMessage = "세 자리 수를 입력해 주세요";
+            throw new IllegalArgumentException("입력값은 세 자리 수여야 합니다.");
         }
+    }
 
-        throw new IllegalArgumentException(exceptionMessage);
+    private void isOneToNine(String number) {
+        boolean isValid = true;
+
+        for (int i = 0; i < 3; i++) {
+            char digit = number.charAt(i);
+            if (digit < '1' || digit > '9') {
+                isValid = false;
+                break;
+            }
+        }
+        if (!isValid) {
+            throw new IllegalArgumentException("입력값은 1부터 9까지의 수로 이루어진 값이어야 합니다.");
+        }
+    }
+
+    private void isDifferentDigits(String numbers) {
+        Set<Character> numberHashSet = new HashSet<>();
+
+        for (char number : numbers.toCharArray()) {
+            if (!numberHashSet.add(number)) {
+                throw new IllegalArgumentException("입력값은 서로 다른 수로 이루어진 값이어야 합니다.");
+            }
+        }
     }
 }
