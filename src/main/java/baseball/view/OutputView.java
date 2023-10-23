ed53f94c0138ef87eea403f93d13cc61011f3030
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-10-24 00:06:37 +0900

    refactor : 경기 결과 출력하는 outputResult 메서드 기능에 따라 분리

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,15 +4,30 @@ import baseball.utils.GameResult;
 import java.util.List;
 
 public class OutputView {
-    public static boolean outputResult(List<Integer> numberResult) {
-        // TODO : 리팩토링
-        Integer strikes = numberResult.get(0);
-        Integer balls = numberResult.get(1);
-        boolean correctAnswer = false;
 
+    private static Integer strikes;
+    private static Integer balls;
+    private static boolean correctAnswer = false;
+
+    public static boolean outputResult(List<Integer> numberResult) {
+        //TODO : strikes, balls 받아오는 방법 더 생각해 보기
+        strikes = numberResult.get(0);
+        balls = numberResult.get(1);
+        correctAnswer = false;
+
+        countBallNumbers();
+        countStrikeNumbers();
+        nothingInValidResult();
+        return correctAnswer;
+    }
+
+    public static void countBallNumbers() {
         if (balls > 0) {
             System.out.print(balls + GameResult.BALL.getMessage() + " ");
         }
+    }
+
+    public static void countStrikeNumbers() {
         if (strikes > 0) {
             System.out.println(strikes + GameResult.STRIKE.getMessage());
             if (checkAnswer(strikes)) {
@@ -20,11 +35,12 @@ public class OutputView {
                 System.out.println(GameResult.WIN_STRIKES.getMessage());
             }
         }
+    }
+
+    public static void nothingInValidResult() {
         if (balls == 0 && strikes == 0) {
             System.out.println(GameResult.NOTHING.getMessage());
         }
-
-        return correctAnswer;
     }
 
 
