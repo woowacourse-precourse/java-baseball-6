b4e181904517c4a275d7baa67c78b1be977f97a1
Yehyeok Bang <qkddpgur318@gmail.com>
2023-10-23 11:57:41 +0900

    feat: 숫자 야구 게임 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -37,6 +37,56 @@ public class Application {
             if (user.get(0).equals(user.get(1)) || user.get(0).equals(user.get(2)) || user.get(1).equals(user.get(2))) {
                 throw new IllegalArgumentException("중복된 숫자가 있습니다.");
             }
+
+            int strike = 0;
+            int ball = 0;
+
+            for (int i = 0; i < computer.size(); i++) {
+                if (computer.get(i).equals(user.get(i))) {
+                    strike++;
+                }
+
+                if (computer.contains(user.get(i)) && !computer.get(i).equals(user.get(i))) {
+                    ball++;
+                }
+            }
+
+            if (strike == 3) {
+                System.out.println("3스트라이크");
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                int restart = Integer.parseInt(Console.readLine());
+
+                if (restart == 1) {
+                    computer.clear();
+                    while (computer.size() < 3) {
+                        int randomNumber = Randoms.pickNumberInRange(1, 9);
+                        if (!computer.contains(randomNumber)) {
+                            computer.add(randomNumber);
+                        }
+                    }
+
+                    continue;
+                }
+
+                System.out.println("게임 종료");
+                break;
+            }
+
+            if (strike == 0 && ball == 0) {
+                System.out.println("낫싱");
+            }
+
+            if (ball > 0) {
+                System.out.print(ball + "볼 ");
+            }
+
+            if (strike > 0) {
+                System.out.println(strike + "스트라이크");
+            }
+
+            System.out.println();
         }
     }
 }
