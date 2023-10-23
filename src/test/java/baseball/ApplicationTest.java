9357c364a798b3c3a97d3fbbb6f3226bbe0f72d8
Flash-KIM142 <topqr123q@gmail.com>
2023-10-23 17:19:04 +0900

    test: 사용자 입력값 예외 테스트 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,12 +1,12 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -26,6 +26,34 @@ class ApplicationTest extends NsTest {
                 assertThatThrownBy(() -> runException("1234"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 영문자_포함_입력값_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("4f3"))
+                        .isInstanceOf(IllegalArgumentException.class));
+    }
+
+    @Test
+    void 숫자_0_포함_입력값_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("032"))
+                        .isInstanceOf(IllegalArgumentException.class));
+    }
+
+    @Test
+    void 세자리수_아닌_입력값_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("12"))
+                        .isInstanceOf(IllegalArgumentException.class));
+    }
+
+    @Test
+    void 중복_숫자_입력값_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("662"))
+                        .isInstanceOf(IllegalArgumentException.class));
     }
 
     @Override
