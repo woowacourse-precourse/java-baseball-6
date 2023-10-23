fdfd225f69fdf6dc5a5585e66a39dac7eae7459d
SeongIl <biuld1234@gmail.com>
2023-10-23 21:51:39 +0900

    [Restart] Feat: Add 출력문 & 입력값 받기
    - 다시 만들기: 기능 추가

--- src/main/java/baseball/RestartApplication.java
+++ src/main/java/baseball/RestartApplication.java
@@ -1,12 +1,14 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RestartApplication {
     public static void main(String[] args) {
 
         int[] pickedNumber = new int[3];
-        int index= 0;
+        int[] userNumbers = new int[3];
+        int index = 0;
         //1. 서로 다른 임의의 수 3개 선택
 
         while (index < 3) {
@@ -18,13 +20,53 @@ public class RestartApplication {
                     break;
                 }
             }
-            if(!isDuplicate){
+            if (!isDuplicate) {
                 pickedNumber[index++] = randomNumber;
             }
         }
         for (int i1 : pickedNumber) {
             System.out.println("i1 = " + i1);
         }
+
+        //2. 출력문구성
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
+            String userInput = Console.readLine();
+
+
+//            - 3개의 입력이 들어왔는가?
+            if (userInput.length() != 3) {
+                throw new IllegalArgumentException("3개의 숫자를 입력하세요.");
+            }
+            String[] userInputChars = userInput.split("");
+
+            try {
+                //int 배열로 변환
+                for (int i = 0; i < 3; i++) {
+                    userNumbers[i] = Integer.parseInt(userInputChars[i]);
+                    //1~9까지의 범위를 지키는가?
+                    if (userNumbers[i] == 0){
+                        throw new IllegalArgumentException("1~9까지의 숫자를 입력하세요.");
+                    }
+                }
+            } catch (NumberFormatException ex) {
+                // 숫자들로만 구성되어 있는가?
+                throw new IllegalArgumentException("숫자만 입력하세요.");
+            }
+//             - 서로 다른 숫자들로만 구성되어 있는가?
+            for(int i=0; i<3; i++){
+                for(int j=i+1; j<3; j++){
+                    if(userNumbers[i] == userNumbers[j]){
+                        throw new IllegalArgumentException("서로 다른 숫자를 입력하세요.");
+                    }
+                }
+            }
+
+
+        }
+
 
     }
 }
