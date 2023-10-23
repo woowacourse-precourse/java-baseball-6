30d085067d297b81a5d2c4f86999b9a1c5d35e06
jaehee <y2hjjh@naver.com>
2023-10-23 18:22:17 +0900

    refactor: OutputView에서 게임 시작 문구 출력

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -2,9 +2,4 @@ package baseball.view;
 
 public class InputView {
 
-    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
-
-    public String printStart() {
-        return GAME_START_MESSAGE;
-    }
 }
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,2 +1,9 @@
-package baseball.view;public class OutputView {
+package baseball.view;
+
+public class OutputView {
+    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+
+    public String printStart() {
+        return GAME_START_MESSAGE;
+    }
 }
