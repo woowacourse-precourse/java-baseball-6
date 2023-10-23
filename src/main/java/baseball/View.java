400e526314d0cfad52a642dd1e483deb297402e3
KimSehoon <shggm2000@gmail.com>
2023-10-24 04:53:31 +0900

    feat: printPlayAgain method 구현
    - 게임 재시작/종료 문구를 출력하는 printPlayAgain method 구현

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -29,4 +29,8 @@ public class View {
     public static void printGameEndMessage(){
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
+
+    public static void printPlayAgain(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
