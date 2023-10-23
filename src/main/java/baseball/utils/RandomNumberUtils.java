8ba1ae353ecb0bfc887d2eaef91717f2a8534d98
IMWoo94 <leemeawoo94@gmail.com>
2023-10-23 21:28:24 +0900

    refactor : RandomNumberUtils 랜덤 갯수 파라미터로 받아서 처리
    RandomNumberUtils 랜덤 갯수 파라미터로 받아서 처리

--- src/main/java/baseball/utils/RandomNumberUtils.java
+++ src/main/java/baseball/utils/RandomNumberUtils.java
@@ -1,7 +1,5 @@
 package baseball.utils;
 
-import static baseball.message.Message.COUNT;
-
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +9,7 @@ public class RandomNumberUtils {
     /**
      * pickNumberInRange 사용한 랜덤 결과
      */
-    public static List<Integer> pickNumberInRange(int startInclusive, int endInclusive) {
+    public static List<Integer> pickNumberInRange(int startInclusive, int endInclusive, int capacity) {
         /**
          * Randoms.pickNumberInRange(final int startInclusive, final int endInclusive);
          * startInclusive ~ endInclusive 사이의 숫자 1개 리턴
@@ -26,8 +24,8 @@ public class RandomNumberUtils {
          */
         // 장점 : 범위 체크를 진행 하면 단순 범위의 랜덤 값 하나만 뽑아 낼 수 있어 간단 하다.
         // 단점 : 중복된 값이 나올 수 있다. -> duplicateCheck 메소드를 활용 하여서 중복 숫자의 경우 List 에 add 되지 않도록 처리
-        List<Integer> result = new ArrayList<>(COUNT);
-        while (result.size() < COUNT) {
+        List<Integer> result = new ArrayList<>(capacity);
+        while (result.size() < capacity) {
             int num = Randoms.pickNumberInRange(startInclusive, endInclusive);
             // 중복 숫자 확인
             duplicateCheck(result, num);
@@ -38,7 +36,7 @@ public class RandomNumberUtils {
     /**
      * pickNumberInList 사용한 랜덤 결과
      */
-    public static List<Integer> pickNumberInList(List<Integer> numbers) {
+    public static List<Integer> pickNumberInList(List<Integer> numbers, int capacity) {
         /**
          * Randoms.pickNumberInList(final List<Integer> numbers);
          * numbers 리스트 안의 숫자 리스트 정보가 랜덤 하게 선택 되어 출력
@@ -51,8 +49,8 @@ public class RandomNumberUtils {
          */
         // 장점 : 랜덤 범위의 리스트를 주면 해당 리스트 안에서 랜덤한 인덱스에 해당 하는 값을 던저 준다.
         // 단점 : 랜덤 범위의 리스트를 생성 해서 던져 주어야 하며, 해당 메소드 또한 중복을 피할 수 없다. -> duplicateCheck 메소드를 활용 하여서 중복 숫자의 경우 List 에 add 되지 않도록 처리
-        List<Integer> result = new ArrayList<>(COUNT);
-        while (result.size() < COUNT) {
+        List<Integer> result = new ArrayList<>(capacity);
+        while (result.size() < capacity) {
             int num = Randoms.pickNumberInList(numbers);
             // 중복 숫자 확인
             duplicateCheck(result, num);
@@ -73,7 +71,7 @@ public class RandomNumberUtils {
      * pickUniqueNumbersInRange 사용한 랜덤 결과
      */
     public static List<Integer> pickUniqueNumbersInRange(int startInclusive, int endInclusive,
-                                                         int count) {
+                                                         int capacity) {
         /**
          * List<Integer> pickUniqueNumbersInRange( final int startInclusive, final int endInclusive, final int count );
          * startInclusive ~ endInclusive 사이의 숫자를 리스트 나열 후 셔플 하여 count 만큼 잘라서 리스트 리턴
@@ -87,6 +85,6 @@ public class RandomNumberUtils {
          */
         // 장점 : 범위의 숫자가 나열 식으로 생성 되고 셔플을 통해서 리스트의 순번을 섞어서 count 만큼 갯수만 가져 오면서 중복된 숫자가 없다.
         // 단점 : 셔플을 진행 하게 되면서 단순 값 하나만 가져 오는 Random 함수 보다 사용 되는 비용이 조금 더 비싸 보인다.
-        return Randoms.pickUniqueNumbersInRange(startInclusive, endInclusive, count);
+        return Randoms.pickUniqueNumbersInRange(startInclusive, endInclusive, capacity);
     }
 }
