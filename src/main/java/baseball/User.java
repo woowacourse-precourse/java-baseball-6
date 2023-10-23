cca11a8baecb143254e58b7e4d4f250a08b77c7a
minwoo <mark42son@gmail.com>
2023-10-23 13:59:02 +0900

    refactor : 상수 이름 수정 및 메서드 선언 순서 변경

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -3,16 +3,13 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 public class User {
-    private static final int NUMBER_LENGTH = 3;
-    private static final String OUTPUT_ENTER_NUMBER = "숫자를 입력해주세요 : ";
+    private static final int LENGTH_OF_NUMBER = 3;
+    private static final String ENTER_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String EXCEPTION_MESSAGE = "1부터 9까지 서로 다른 숫자를 입력해주세요.";
-    int userNumber;
-
-    public User() {
-    }
+    public static int userNumber;
 
     public void setUserNumber() {
-        System.out.print(OUTPUT_ENTER_NUMBER);
+        System.out.print(ENTER_MESSAGE);
         String input = inputUserNumber();
         validateInput(input);
         this.userNumber = Integer.parseInt(input);
@@ -29,22 +26,17 @@ public class User {
 
 
     private void validateInput(String input) {
-        validateInputLength(input);
-        validateInputDigits(input);
-    }
 
-    private void validateInputDigits(String input) {
+        if (input.length() != LENGTH_OF_NUMBER) {
+            throw new IllegalArgumentException(EXCEPTION_MESSAGE);
+        }
+
         for (char c : input.toCharArray()) {
             if (c < '1' || c > '9') {
                 throw new IllegalArgumentException(EXCEPTION_MESSAGE);
             }
         }
-    }
 
 
-    private static void validateInputLength(String input) {
-        if (input.length() != NUMBER_LENGTH) {
-            throw new IllegalArgumentException(EXCEPTION_MESSAGE);
-        }
     }
 }
