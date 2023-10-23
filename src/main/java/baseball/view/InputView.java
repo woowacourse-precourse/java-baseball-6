032abcb7fe660c8db4e590836d33d0b8f3ba831a
PARK9898 <wjddjs21@naver.com>
2023-10-23 22:37:42 +0900

    "feat: 재시작 여부 결정 메서드 구현"

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,6 +1,5 @@
 package baseball.view;
 
-import java.sql.SQLOutput;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,6 +8,9 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
 	private static final String NUMBER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+	private static final String RESTART_DECISION_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+	private static final String RESTART = "1";
+	private static final String END = "2";
 
 	public List<Integer> createPlayerNumber() {
 		System.out.println(NUMBER_INPUT_MESSAGE);
@@ -18,5 +20,12 @@ public class InputView {
 			.collect(Collectors.toList());
 	}
 
-
+	public String decideRestart() {
+		System.out.println(RESTART_DECISION_MESSAGE);
+		String input = Console.readLine();
+		if(!input.equals(RESTART) && !input.equals(END)) {
+			throw new IllegalArgumentException("잘못된 재시작 여부 입력값입니다");
+		}
+		return input;
+	}
 }
