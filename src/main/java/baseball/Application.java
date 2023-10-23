58a2f43d9376df1c7a6c73e8972e18abfc2165a6
myonjin <kjinabc@gmail.com>
2023-10-24 01:06:12 +0900

    Feat : randomNumMaker 매서드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Application {
+    public List<Integer> randomNumMaker() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("git test");
