f834ff74b6ec8017464a849639fdc71dcf6d69d2
jisang-park <chlrh9009@gmail.com>
2023-10-23 23:07:18 +0900

    정답 생성 메소드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    private static List<Integer> createAnswer() {
+        List<Integer> answer = new ArrayList<>(3);
+        while (answer.size() < 3) {
+            int number = Randoms.pickNumberInRange(1, 9);
+            if (!answer.contains(number)) {
+                answer.add(number);
+            }
+        }
+        return answer;
     }
 }
