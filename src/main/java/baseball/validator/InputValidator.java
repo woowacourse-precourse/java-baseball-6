8d9ad224ce65be66cd82ea93a5ee7cb46f2e8d6f
jang010505 <jang010505@inu.ac.kr>
2023-10-24 02:03:46 +0900

    feat: validateCheckRestartGameInput 함수 작성

--- src/main/java/baseball/validator/InputValidator.java
+++ src/main/java/baseball/validator/InputValidator.java
@@ -2,6 +2,9 @@ package baseball.validator;
 
 public class InputValidator {
     public static void validateCheckRestartGameInput(String input) {
-
+        if (!(input.equals("1") || input.equals("2"))) {
+            throw new IllegalArgumentException("잘못된 값을 입력했습니다.");
+        }
     }
+
 }
