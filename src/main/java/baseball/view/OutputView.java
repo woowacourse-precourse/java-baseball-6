ac8805c9ac0d3899f6d3511f871281293ea8e3db
Han <yulmam1@naver.com>
2023-10-23 19:33:47 +0900

    feat(view) : Create InputView and OutputView

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,2 +1,32 @@
-package baseball.view;public class OutputView {
+package baseball.view;
+
+public class OutputView {
+
+
+    public void  printStartMessage(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+    public void printScoreMessage(int strikeCount, int ballCount){
+        if(strikeCount==3){
+            System.out.println("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return;
+        }
+        if(strikeCount == 0 && ballCount == 0){
+            System.out.println("낫싱");
+            return;
+        }
+        if(!(ballCount == 0)) {
+            System.out.println(ballCount+"볼");
+        }
+        if(!(strikeCount == 0)) {
+            System.out.println(strikeCount + "스트라이크");
+        }
+    }
+
+    public void printGameEndingMessage(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+
+
 }
