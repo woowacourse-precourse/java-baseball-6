e7c09eda85d0e5d0572603e31933f752ba33e0f0
ParkJinYong <mej1406@gmail.com>
2023-10-23 22:08:58 +0900

    feat: 심판 역할 및 경기 결과 객체 반환 기능 추가

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -1,4 +1,21 @@
 package baseball;
 
+import java.util.List;
+
 public class Referee {
+    public GameResult judgeGameResult(List<Integer> catcher, List<Integer> pitcher) {
+        int strike = 0, ball = 0;
+
+        for (int i = 0; i < catcher.size(); i++) {
+            int pitcherNum = pitcher.get(i);
+            if (pitcherNum == catcher.get(i)) {
+                strike++;
+            }
+            else if (catcher.contains(pitcherNum)) {
+                ball++;
+            }
+        }
+
+        return new GameResult(strike, ball);
+    }
 }
