d4a05c6ec330d091a258da4b4009d60363e2567c
davemins <davemins4125@naver.com>
2023-10-23 17:32:59 +0900

    feat(Output) : 게임 종료 메시지 출력 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,6 +3,6 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         Output output = new Output();
-        output.printStartGame();
+        output.printEndGame();
     }
 }
