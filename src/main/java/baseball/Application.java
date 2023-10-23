d21574b6ac2624fb2234677c79fa8559006a177d
GeonWooPark <gomudayya@gmail.com>
2023-10-23 16:40:19 +0900

    feat : 숫자 생성 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+
+        // 역할 책임 협력의 관점에서 생각을 해보자
+        // 이 어플리케이션은 사용자가 숫자야구 게임을 원활히 하도록 객체들이 협력해야한다.
+        //
     }
 }
---
+++ src/main/java/baseball/NumberFactory.java
@@ -0,0 +1,28 @@
+package baseball;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberFactory {
+
+    public static int createNumber() {
+        List<Integer> numberList = new ArrayList<>();
+
+        while (numberList.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if(!numberList.contains(randomNumber)) {
+                numberList.add(randomNumber);
+            }
+        }
+
+        int createdNumber = combineNumber(numberList);
+        return createdNumber;
+    }
+
+    private static int combineNumber(List<Integer> numberList) {
+        return numberList.get(0) * 100 + numberList.get(1) * 10 + numberList.get(2);
+    }
+
+
+}
