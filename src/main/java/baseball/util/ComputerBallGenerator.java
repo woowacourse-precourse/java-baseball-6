4cb7fa25c35ff3ed7c43bc2c6bee685ac40ebe58
kangaroo0 <ljw42871@gmail.com>
2023-10-23 23:58:49 +0900

    Refactor: ComputerBallGenerator 리팩토링을 통해서 확장성 있게 구현 및 위치 같이 기록하기 - computerBall에 위치 기록하여 비교할 수 있도록 구현하기

--- src/main/java/baseball/util/ComputerBallGenerator.java
+++ src/main/java/baseball/util/ComputerBallGenerator.java
@@ -4,12 +4,19 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class ComputerBallGenerator {
 
+    private static final int NUMBER_POSITIONS = 10;
+    private static final int START_RANGE = 1;
+    private static final int END_RANGE = 9;
+    private static final int SELECTION_COUNT = 3;
+
+
     public static int[] generateComputerBall() {
 
-        int[] computerBall = new int[10];
+        int[] computerBall = new int[NUMBER_POSITIONS];
+        int index = SELECTION_COUNT;
 
-        for (int x : Randoms.pickUniqueNumbersInRange(1, 9, 3)) {
-            computerBall[x]++;
+        for (int x : Randoms.pickUniqueNumbersInRange(START_RANGE, END_RANGE, SELECTION_COUNT)) {
+            computerBall[x] += index--;
         }
 
         return computerBall;
