e6e0fcf54901cbe17b7fb89f6dcc465de9265e40
MingyeomKim <mingyum119@naver.com>
2023-10-23 16:51:47 +0900

    fix(Computer): savedNumbers가 null이 되는 오류 수정
    Computer 클래스에서 Numbers 객체를 선언 후 초기화하지 않아 "savedNumbers is null"오류가 발생하는 것을 수정한다.

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -3,7 +3,7 @@ package baseball.model;
 import java.util.List;
 
 public class Computer {
-    private Numbers savedNumbers;
+    private Numbers savedNumbers = new Numbers();
 
     public void saveNumbers(List<Integer> generatedNumbers) {
         savedNumbers.from(generatedNumbers);
