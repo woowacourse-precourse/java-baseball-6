e33d6d84a8234202dc9631099a325c234d3c6244
KJBig <kjgi73k@gmail.com>
2023-10-23 20:45:18 +0900

    feat: Player (플레이어) 기능 구현
    - 플레이어의 숫자 입력을 구현.
    - 입력 받은 숫자를 List에 설정하는 기능 구현.
    - 플레이어의 숫자를 조회하는 기능 구현.

---
+++ src/main/java/baseball/Player.java
@@ -0,0 +1,29 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    private final List<Integer> numberList = new ArrayList<>();
+
+    public Player() {
+    }
+
+    public void enterNumber() {
+        String input = Console.readLine();
+        setNumberList(input);
+    }
+
+    private void setNumberList(String input) {
+        String[] inputList = input.split("");
+        numberList.clear();
+        for (String number : inputList) {
+            numberList.add(Integer.parseInt(number));
+        }
+    }
+
+    public List<Integer> getNumberList() {
+        return numberList;
+    }
+}
