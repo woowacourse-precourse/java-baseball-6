f741e2ff9e016e3260e5f1940bc056c291c5faa4
EvelynKimm <evekimmy09@gmail.com>
2023-10-23 19:53:58 +0900

    refactor: create an object for calling other class

--- src/main/java/baseball/view/ResultView.java
+++ src/main/java/baseball/view/ResultView.java
@@ -1,37 +1,33 @@
 package baseball.view;
 
-import static baseball.model.MsgConst.BALL;
-import static baseball.model.MsgConst.NOTHING;
-import static baseball.model.MsgConst.STRIKE;
-import static baseball.model.NumConst.FULL_STRIKE;
+import static baseball.model.MsgConstant.BALL;
+import static baseball.model.MsgConstant.NOTHING;
+import static baseball.model.MsgConstant.STRIKE;
+import static baseball.model.NumConstant.FULL_STRIKE;
 
 import baseball.Baseball;
 
-
 public class ResultView {
 
-    public static void givingHint(int ball, int strike) {
+    private static final Baseball baseball = new Baseball();
+
+    public static void givingResult(int ball, int strike) {
         StringBuilder answer = new StringBuilder("");
         answer.setLength(0);
 
         if (ball == 0 && strike == 0) {
             answer.append(NOTHING);
         }
-
         if (ball > 0) {
             answer.append(ball).append(BALL).append(" ");
         }
-
         if (strike > 0) {
             answer.append(strike).append(STRIKE);
         }
         System.out.println(answer);
 
         if (strike == FULL_STRIKE) {
-            Baseball.restartOrExit();
+            baseball.restartOrExit();
         }
-
-
     }
-
 }
