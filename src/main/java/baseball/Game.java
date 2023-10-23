47468dd351bcd8c782bacfaec0bcee0638765fd6
kkonii <119216472+kkonii@users.noreply.github.com>
2023-10-24 03:40:05 +0900

    Feat : 게임 종료 후 사용자가 1을 입력하면 게임을 재시작한다

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -43,6 +43,12 @@ public class Game {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        String restartInput = Console.readLine();
+
+        if (restartInput.equals("1")) {
+            game();
+        }
     }
 
     private void resetRandomNumbers() {
