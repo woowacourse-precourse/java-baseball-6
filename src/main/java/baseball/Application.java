8a508fcc2796066b869387027628862fb25fd36b
GyyymShark <sangwoo0795@naver.com>
2023-10-23 11:08:50 +0900

    style: 안쓰는 코드 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,7 +8,7 @@ import java.util.*;
 public class Application {
     public static void main(String[] args){
         // TODO: 프로그램
-        Scanner sc=new Scanner(System.in);
+
         boolean restart=true;
         List<Integer> computer = new ArrayList<>();
         List<Integer> user = new ArrayList<>();
