0e5d1f5aa3876803929179e457eefe2fdfbba3ae
seokmyungham <hellotjraud@gmail.com>
2023-10-24 06:41:10 +0900

    feat(Constants): 클래스에 NUMBER_OF_BASEBALL 상수 추가

--- src/main/java/baseball/domain/Constants.java
+++ src/main/java/baseball/domain/Constants.java
@@ -5,4 +5,6 @@ public class Constants {
     public static final String USER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
     public static final String END_GAME_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     public static final String RESTART_GAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public static final int NUMBER_OF_BASEBALL = 3;
 }
