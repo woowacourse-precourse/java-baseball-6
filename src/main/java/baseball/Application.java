71ee645ee06e8f9688fd42ee2655e42aa0f2e07f
Sungkyu <kxu4583@naver.com>
2023-10-23 16:49:15 +0900

    '기능 1: 서로 다른 3개의 1~9 사이의 숫자를 생성하기' 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,21 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import org.assertj.core.internal.IntArrays;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        //정답이 될 3개의 숫자를 만들기
+        int[] answer = new int[3];
+        for(int i=0;i<3;i++){
+            answer[i] = Randoms.pickNumberInRange(1, 9);
+            for(int j=0; j < i && answer[j] == answer[i]; j++){
+                answer[i] = Randoms.pickNumberInRange(1, 9);
+                j=0;
+            }
+        }
+
     }
 }
