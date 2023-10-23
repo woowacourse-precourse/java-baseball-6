ec4813948f5cf0d237146c39739f89fd481c6b29
yshong1998 <yshong1998@gmail.com>
2023-10-24 01:51:59 +0900

    Feat(InputValidator): 유저의 입력이 규칙에 맞는지 검증하는 InputValidator 클래스 구현

--- src/main/java/baseball/global/validator/InputValidator.java
+++ src/main/java/baseball/global/validator/InputValidator.java
@@ -1,4 +1,40 @@
 package baseball.global.validator;
 
+import baseball.global.constant.RuleValue;
+import baseball.global.message.ExceptionMessage;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class InputValidator {
+
+    private void validateInput(String playerInput){
+        validateInputNumeric(playerInput);
+        validateInputSize(playerInput);
+        validateInputDistinct(playerInput);
+    }
+
+    private void validateInputNumeric(String playerInput){
+        try {
+            Integer.parseInt(playerInput);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateInputSize(String playerInput){
+        if (playerInput.length() != RuleValue.BASEBALL_NUMBER_SIZE){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateInputDistinct(String playerInput){
+        String[] seperatedString = playerInput.split("");
+        Set<String> distinctStringSet = new HashSet<>(Arrays.asList(seperatedString));
+        if(distinctStringSet.size() != playerInput.length()){
+            throw new IllegalArgumentException();
+        }
+    }
 }
