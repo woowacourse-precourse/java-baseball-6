5ac0076d084166785374572f3134587abd6505ca
beomsic <qjatjr29@khu.ac.kr>
2023-10-24 01:51:46 +0900

    :recycle: refactor: BaseBallNumber에서 입력받은 숫자들을 검증하도록 변경

--- src/main/java/baseball/model/BaseBallNumber.java
+++ src/main/java/baseball/model/BaseBallNumber.java
@@ -7,16 +7,19 @@ import java.util.function.BiConsumer;
 
 public class BaseBallNumber {
 
+    private static final int DIGIT = 3;
     private final int[] numbers;
 
     private BaseBallNumber(int[] numbers) {
+        validateSize(numbers);
+        validateWithinRange(numbers);
         this.numbers = numbers;
     }
 
-    public static BaseBallNumber generateRandomNumbers(int digit) {
+    public static BaseBallNumber generateRandomNumbers() {
         List<Integer> randomNumbers = new ArrayList<>();
 
-        while (randomNumbers.size() < digit) {
+        while (randomNumbers.size() < DIGIT) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
 
             if (!randomNumbers.contains(randomNumber)) {
@@ -40,6 +43,20 @@ public class BaseBallNumber {
     public void calculate(BiConsumer<Integer,Integer> biConsumer){
         for(int i = 0; i < numbers.length; i++) {
             biConsumer.accept(numbers[i], i);
+        }
+    }
+
+    public int getBaseBallNumberCount() {
+        return numbers.length;
+    }
+
+    private void validateSize(int[] numbers) {
+        if(numbers.length != DIGIT) throw new IllegalArgumentException("입력된 숫자는 3개로만 이루어져야 합니다.");
+    }
+
+    private void validateWithinRange(int[] numbers) {
+        for(int number : numbers) {
+            if(number < 1 || number > 9) throw new IllegalArgumentException("숫자는 1 ~ 9 사이의 값만 가능합니다.");
         }
     }
 
