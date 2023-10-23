d7e3a935679e5ce05627ecaee7098b9cd30c0d8a
SeolHuiGwan9478 <shg5977@naver.com>
2023-10-23 16:02:30 +0900

    [feat] : Application main() 작성 - Baseball 객체를 생성하여 run()을 호출하였음. - 마지막에 종료 직전에 할당받은 Scanner를 명시적으로 할당해제 하였음

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
 
+import baseball.controller.BaseballGame;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        // Create BaseballGame Instance
+        BaseballGame game = new BaseballGame();
+        // 게임 실행
+        game.run();
+        // computing scanner resource return
+        Console.close();
     }
 }
