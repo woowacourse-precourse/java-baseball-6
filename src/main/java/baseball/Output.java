9a3db959f13d701cbf8c6a7df7f46d0943cd0b1c
jiohh <dkswlgh99@naver.com>
2023-10-24 03:32:14 +0900

    feat:힌트 출력기능

---
+++ src/main/java/baseball/Output.java
@@ -0,0 +1,18 @@
+package baseball;
+
+public class Output {
+    public void printHint(int strikeCount, int ballCount) {
+        if (strikeCount == 3) {
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        } else if (strikeCount == 0 && ballCount == 0) {
+            System.out.println("낫싱");
+        } else if (strikeCount == 0 && ballCount > 0) {
+            System.out.println(ballCount + "볼");
+        } else if (strikeCount > 0 && ballCount == 0) {
+            System.out.println(strikeCount + "스트라이크");
+        } else if (strikeCount > 0 && ballCount > 0) {
+            System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+        }
+    }
+}
