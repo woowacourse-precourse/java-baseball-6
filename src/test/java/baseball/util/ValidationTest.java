c310a55d6377fec304ce9f8c6589af18414fd915
junseoks-99 <angec5645@naver.com>
2023-10-24 05:36:21 +0900

    test: "문자열 유효길이 여부 테스트"

--- src/test/java/baseball/util/ValidationTest.java
+++ src/test/java/baseball/util/ValidationTest.java
@@ -3,6 +3,7 @@ package baseball.util;
 import static baseball.util.Validation.MAX_INPUT_LENGTH;
 import static baseball.util.Validation.containsLetter;
 import static baseball.util.Validation.containsZero;
+import static baseball.util.Validation.validLength;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
@@ -24,6 +25,16 @@ class ValidationTest {
 
         for (String str : testArr) {
             assertThatThrownBy(() -> containsLetter(str, MAX_INPUT_LENGTH)).isInstanceOf(
+                    IllegalArgumentException.class);
+        }
+    }
+
+    @Test
+    void 문자열_유효길이_테스트() {
+        String[] testArr = {"1234", "12345", "123456", "1234567", "12345678", "123456789"};
+
+        for (String str : testArr) {
+            assertThatThrownBy(() -> validLength(str, MAX_INPUT_LENGTH)).isInstanceOf(
                     IllegalArgumentException.class);
         }
     }
