db230625bf8cca3fe66268463a903568e36260a7
gywns0417 <gywns0417@gmail.com>
2023-10-23 12:22:56 +0900

    feat: 사용자에게서 입력 받은 숫자 검증 기능 구현 완료

---
+++ src/main/java/baseball/model/InputNumberValidator.java
@@ -0,0 +1,49 @@
+package baseball.model;
+
+public class InputNumberValidator {
+
+    public String validateAllInput(String input) {
+        hasThreeDigits(input);
+        isNumber(input);
+        isNumber(input);
+        areDigitsUnique(input);
+        return input;
+    }
+
+    public String validateRestartInput(String input) {
+        isOneOrTwo(input);
+        return input;
+    }
+
+    private void throwIAExceptionWithMessage(String message) {
+        throw new IllegalArgumentException(message);
+    }
+
+    public boolean hasThreeDigits(String input) {
+        if (input.length() != 3) {
+            throwIAExceptionWithMessage("입력값은 3자리여야 합니다.");
+        }
+        return true;
+    }
+
+    public boolean isNumber(String input) {
+        if (!input.matches("[+-]?\\d*(\\.\\d+)?")) {
+            throwIAExceptionWithMessage("숫자만 입력해야 합니다.");
+        }
+        return true;
+    }
+
+    public boolean areDigitsUnique(String input) {
+        if (input.length() != input.chars().distinct().count()) {
+            throwIAExceptionWithMessage("서로 다른 수들만 입력해야 합니다.");
+        }
+        return true;
+    }
+
+    public String isOneOrTwo(String input) {
+        if (!input.equals("1") && !input.equals("2")) {
+            throwIAExceptionWithMessage("1 또는 2만 입력해야 합니다.");
+        }
+        return input;
+    }
+}
