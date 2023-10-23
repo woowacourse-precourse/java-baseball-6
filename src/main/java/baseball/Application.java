b92be45680b271918078b6a7ff01638ddf26a9a6
Hyeji <hyejijjang10@gmail.com>
2023-10-23 23:16:43 +0900

    Faild to Restart Test,delete gamewon

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,18 +3,18 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.List;
-
+//ToDo : 입력방식 변경 (엔터로 구분)
 public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
         Inspection inspection = new Inspection();
         Game game = new Game();
-        boolean gameWon = false, restart = false;
+        boolean restart = false;
         //컴퓨터 패 생성
         List<Integer> comList = Randoms.pickUniqueNumbersInRange(1, 9, 3);
         comList.forEach(element -> System.out.println(element));
 
-        while (!gameWon || !restart) {
+        while (!restart) {
             //사용자 패 생성
             List<Integer> userList = inspection.userInputToList();
             userList.forEach(element -> System.out.println(element));
@@ -23,16 +23,11 @@ public class Application {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 comList = Randoms.pickUniqueNumbersInRange(1, 9, 3);
                 comList.forEach(element -> System.out.println(element));
-                gameWon = true;
                 if (inspection.RestartInput() == 2) {
                     restart = true;
                 }
-
             }
-
-
         }
-
 
     }
 }
