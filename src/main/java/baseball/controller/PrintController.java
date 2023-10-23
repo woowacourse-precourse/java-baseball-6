a99b252a5da6144e0f1109da45e9a038fdc183d2
jaehee <y2hjjh@naver.com>
2023-10-23 16:05:53 +0900

    feat: 성공 문구 출력 메서드 작성

--- src/main/java/baseball/controller/PrintController.java
+++ src/main/java/baseball/controller/PrintController.java
@@ -27,4 +27,8 @@ public class PrintController {
         }
         return output.toString();
     }
+
+    public String printSuccess() {
+        return "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    }
 }
