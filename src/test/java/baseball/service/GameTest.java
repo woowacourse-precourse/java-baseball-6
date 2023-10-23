2bed3058795acb73cd08c6746f05d08cb48371d0
hee9841 <lee98417716@gmail.com>
2023-10-24 06:15:35 +0900

    :white_check_mark: add GameTest
    - add game Test

---
+++ src/test/java/baseball/service/GameTest.java
@@ -0,0 +1,26 @@
+package baseball.service;
+
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+
+    //1. 게임 초기화
+    @Test
+    void initGame() {
+
+    }
+
+
+    //2. 게임 실행
+    @Test
+    void runGame() {
+
+    }
+
+    //3. 게임 초기화
+    @Test
+    void resetGame() {
+        
+    }
+
+}
