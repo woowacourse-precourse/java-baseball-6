2161143a00730fb81bf6d40a1de571ffdffa411d
jungeun-choi <jungeun5.choi@gmail.com>
2023-10-24 00:22:48 +0900

    feat: ball 판정

--- src/main/java/baseball/BaseballUmpire.java
+++ src/main/java/baseball/BaseballUmpire.java
@@ -2,6 +2,16 @@ package baseball;
 
 /* 플레이어가 제시한 숫자를 판정하고 결과를 반환 */
 public class BaseballUmpire {
+	public int umpireBall(String com, String user) {
+		int ball = 0;
+		for (int i = 0; i < user.length(); i++) {
+			if (isContains(com, user, i) && !isEquals(com, user, i)) {
+				ball++;
+			}
+		}
+		return ball;
+	}
+
 	private int umpireStrike(String com, String user) {
 		int strike = 0;
 		for (int i = 0; i < user.length(); i++) {
@@ -15,5 +25,10 @@ public class BaseballUmpire {
 	private boolean isEquals(String com, String user, int i) {
 		// com의 i번째 문자와 user의 i번째 문자가 서로 같다면 true
 		return com.charAt(i) == user.charAt(i);
+	}
+
+	private boolean isContains(String com, String user, int i) {
+		// com 문자열이 user의 i번째 문자를 포함하면 true
+		return com.contains(Character.toString(user.charAt(i)));
 	}
 }
