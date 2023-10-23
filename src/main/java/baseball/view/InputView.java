6b6dc5b791b738e7ebb4815419bf12ddbd5b0a81
정찬영 <jcy0342@gmail.com>
2023-10-23 15:59:29 +0900

    feat(InputView): 사용자의 숫자와 재실행 여부를 입력 받는 InputView 클래스 구현
    - 사용자가 입력한 숫자를 받아오기 위한 enterPlayerNumber()
    - 사용자의 재실행 여부를 입력받기 위한 enterRestartGame()
    - 우테코 프로래밍 요구사항에 따라 camp.nextstep.edu.missionutils.Console 사용

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,19 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    private final String ASK_RESET_GAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ";
+
+
+    public String enterPlayerNumber() {
+        System.out.print(INPUT_NUMBER_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String enterRestartGame() {
+        System.out.print(ASK_RESET_GAME_MESSAGE);
+        return Console.readLine();
+    }
+}
