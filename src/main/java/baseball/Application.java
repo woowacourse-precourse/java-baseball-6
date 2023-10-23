6656ec8e7e2d9544cb24511179ec8653abee4fa6
Kite_Z <duswl339@ajou.ac.kr>
2023-10-23 18:22:29 +0900

    Comment: Add TODO Comment
    Flow Chart 순서대로 TODO Comment 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // TODO: START
+
+        // TODO: Make Computer Baseball Number
+
+        // TODO: Input User Expect Number
+
+        // TODO: Compare User Number and Computer Baseball Number
+
+        // TODO: Print Result
+
+        // TODO: Re-game ?
+
+        // TODO: END
     }
+
 }
