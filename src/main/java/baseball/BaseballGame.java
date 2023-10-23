d3f13fbca123cce3b8bd1503eacadfa322e2d6ef
bboodd <92453279+bboodd@users.noreply.github.com>
2023-10-23 16:27:10 +0900

    feat: 입력한 숫자와 컴퓨터의 숫자 비교기능구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -22,6 +22,52 @@ public class BaseballGame {
         System.out.println("숫자 야구 게임을 시작합니다.");
         while (true){
             inputNumbersAndValidate();
+            compareNumbers();
+            if(ball < 1 && strike < 1){
+                System.out.println("낫씽");
+                continue;
+            }
+            if(ball>0 && strike<1){
+                System.out.println(ball + "볼");
+                continue;
+            }
+            if(ball<1 && strike >0){
+                System.out.println(strike + "스트라이크");
+                if(strike == 3){
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                }
+                continue;
+            }
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
+    }
+
+    public void compareNumbers(){
+        ball = 0;
+        strike = 0;
+
+        ballCount(inputNumbers);
+        strikeCount(inputNumbers);
+    }
+
+    public void ballCount(String numbers){
+
+        for(String num : numbers.split("")){
+            if(computer.contains(Integer.parseInt(num))){
+                ball++;
+            }
+        }
+
+    }
+
+    public void strikeCount(String numbers){
+
+        for(int i=0;i<INPUT_NUMBERS_SIZE;i++){
+            if(computer.get(i).equals(Integer.parseInt(numbers.split("")[i]))){
+                strike++;
+                ball--;
+            }
         }
     }
 
