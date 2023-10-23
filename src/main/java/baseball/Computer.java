55a69beb85274e4bc66e193a502d13733e39ec5c
Kohyeonjeong <toyu7870@naver.com>
2023-10-23 13:58:39 +0900

    Feat: 추가 min에서 max 서로 다른 임의의 수 3개 생성하는 RandomNumberGenerator함수
    Computer.java 생성 & 게임시작시  min에서 max 서로 다른 임의의 수 3개 생성하는 RandomNumberGenerator함수 생성

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,14 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class Computer {
+
+    public class RandomNumberGenerator {
+        public static List<Integer> generateRandomNumbers(int startInclusive, int endInclusive, int count) {
+            return Randoms.pickUniqueNumbersInRange(startInclusive, endInclusive, count);
+        }
+
+    }
+}
--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -3,9 +3,11 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 public class GameController {
-    private static final String INPUT_PROMPT = "숫자를 입력해주세요: ";
+    private static final String START_PROMPT = "숫자 야구 게임을 시작합니다.";
+    private static final String INPUT_PROMPT = "숫자를 입력해주세요 :";
 
     public void startGame() {
+        System.out.println(START_PROMPT);
         String userInput = readUserInput(INPUT_PROMPT);
         System.out.println(userInput);
     }
