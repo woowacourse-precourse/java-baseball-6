cc4f741782942a831b0a65bb23a5a6757f592aa3
ParkJinYong <mej1406@gmail.com>
2023-10-23 18:03:35 +0900

    feat: 포수 역할 및 랜덤 숫자 생성 기능 추가

---
+++ src/main/java/baseball/Catcher.java
@@ -0,0 +1,29 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+
+public class Catcher {
+    private List<Integer> numbers;
+    private int maxSize;
+
+    public Catcher(int maxSize) {
+        this.numbers = new ArrayList<>();
+        this.maxSize = maxSize;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public void generateRandomNumbers() {
+        while (numbers.size() < maxSize) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+    }
+}
