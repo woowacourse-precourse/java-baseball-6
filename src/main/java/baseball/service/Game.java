8c7e1e248887c7e1acb25119d6322819c135bdc2
hee9841 <lee98417716@gmail.com>
2023-10-24 07:32:51 +0900

    :sparkles: add Game method
    - game 실행 메소드 추가

--- src/main/java/baseball/service/Game.java
+++ src/main/java/baseball/service/Game.java
@@ -1,5 +1,8 @@
 package baseball.service;
 
+import baseball.service.handler.Computer;
+
+
 public class Game {
 
     public static final int NUMBER_OF_DIGITS = 3;       //자리수
@@ -16,5 +19,46 @@ public class Game {
     private static final String SUCCESS_MESSAGE = NUMBER_OF_DIGITS + "개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String NEW_GAME_CHECK_MESSAGE =
             "게임을 새로 시작하려면 " + NEW_GAME_FLAG + ", 종료하려면 " + EXIT_GAME_FLAG + "를 입력하세요.";
+
+
+    private Computer initComputer() {
+        //컴퓨터 랜덤 수 초기화
+        return new Computer(START_RANGE, END_RANGE, NUMBER_OF_DIGITS);
+    }
+
+    public void compareNumbers(String computer_number, String player_number, int strikeCnt, int ballCnt) {
+        int length = player_number.length();
+        //입력한 수의 자리수 만큼 반복
+        for (int i = 0; i < length; i++) {
+            char player_i_num = player_number.charAt(i);  //i번쨰 자리수의 수
+            if (computer_number.contains(String.valueOf(player_i_num))) {
+                if (computer_number.charAt(i) == player_i_num) {
+                    strikeCnt++;
+                } else {
+                    ballCnt++;
+                }
+            }
+        }
+
+        String resultout = "";
+        if (ballCnt == 0 && strikeCnt == 0) {
+            resultout = "낫싱";
+
+        } else {
+            if (ballCnt != 0) {
+                resultout += ballCnt + "볼";
+            }
+            if (strikeCnt != 0) {
+                if (!resultout.equals("")) {
+                    resultout += " ";
+                }
+                resultout += strikeCnt + "스트라이크";
+            }
+        }
+
+        System.out.println(resultout);
+
+    }
+
 
 }
