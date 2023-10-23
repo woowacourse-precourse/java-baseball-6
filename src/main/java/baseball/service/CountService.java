6a81b823a1943a3467bfa50572ffa217e37de38b
Hyeon0208 <dmsgktn0208@gmail.com>
2023-10-23 20:51:36 +0900

    refactor : 한 단계 들여쓰기를 사용 - 로직을 stream으로 변경

--- src/main/java/baseball/service/CountService.java
+++ src/main/java/baseball/service/CountService.java
@@ -3,6 +3,7 @@ package baseball.service;
 import baseball.domain.ComputerNumber;
 import baseball.domain.PlayerNumber;
 import baseball.dto.CountResultDto;
+import java.util.stream.IntStream;
 
 public class CountService {
 
@@ -13,22 +14,14 @@ public class CountService {
     }
 
     private int calculateStrikeCount(ComputerNumber computerNumber, PlayerNumber playerNumber) {
-        int count = 0;
-        for (int i = 0; i < computerNumber.size(); i++) {
-            if (computerNumber.isSamePosition(playerNumber, i)) {
-                count++;
-            }
-        }
-        return count;
+        return (int) IntStream.range(0, computerNumber.size())
+                .filter(i -> computerNumber.isSamePosition(playerNumber, i))
+                .count();
     }
 
     private int calculateBallCount(ComputerNumber computerNumber, PlayerNumber playerNumber) {
-        int count = 0;
-        for (int i = 0; i < computerNumber.size(); i++) {
-            if (computerNumber.isContainNumber(playerNumber, i)) {
-                count++;
-            }
-        }
-        return count;
+        return (int) IntStream.range(0, computerNumber.size())
+                .filter(i -> computerNumber.isContainNumber(playerNumber, i))
+                .count();
     }
 }
