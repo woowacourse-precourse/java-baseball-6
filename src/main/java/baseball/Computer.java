6db7b8f436b35cc63d9eeb19bb9b2310d6429cc5
coh <och5405@naver.com>
2023-10-23 19:30:10 +0900

    Make method CheckResult

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -4,8 +4,8 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 public class Computer {
     private static final int SIZE = 3;
-    private static boolean[][] baseballFlag = new boolean[3][3];
-    private static int[] numbers = new int[SIZE];
+    private final static int[] baseballFlag = {0, 0, 0};
+    private final static int[] numbers = new int[SIZE];
     public Computer() {
         int randomNumber;
         for (int i = 0; i < SIZE; i++){
@@ -22,5 +22,28 @@ public class Computer {
                 return true;
         }
         return false;
+    }
+
+    private void checkResult(String userInput){
+        int tempNumber;
+        for (int i = 0; i < SIZE; i++){
+            tempNumber = Integer.parseInt(userInput.charAt(i) + "");
+            updateFlag(tempNumber, i);
+        }
+    }
+
+    private void updateFlag(int userInput, int digit){
+        for (int currentDigit = 0; currentDigit < SIZE; currentDigit++){
+            if (digit == currentDigit && userInput == numbers[currentDigit]) {
+                baseballFlag[0] += 1; // strike
+                return;
+            }
+            if (digit != currentDigit && userInput == numbers[currentDigit]) {
+                baseballFlag[1] += 1; // ball
+                return;
+            }
+            if (currentDigit == SIZE - 1)
+                baseballFlag[2] += 1; // nothing
+        }
     }
 }
