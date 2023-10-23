e8c5a3e7b4f2c279624ebd06ef3f89a3eddcd723
JongMinCh0i <wonderland314592@gmail.com>
2023-10-23 23:17:53 +0900

    style(Validation): util 패키징 이동

--- src/test/java/baseball/ValidationTest.java
+++ src/test/java/baseball/ValidationTest.java
@@ -3,6 +3,7 @@ package baseball;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import baseball.util.Validation;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
