8010d70dfe9f78026526c0869d15a7e16162a212
its-sky <smc9919@naver.com>
2023-10-24 07:57:28 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/main/java/baseball/ui/InputView.java
@@ -0,0 +1,9 @@
+package baseball.ui;
+
+public class InputView {
+	private final OutputView outputView;
+
+	public InputView(OutputView outputView) {
+		this.outputView = outputView;
+	}
+}
---
+++ src/main/java/baseball/ui/OutputView.java
@@ -0,0 +1,9 @@
+package baseball.ui;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
+public class OutputView {
+
+}
