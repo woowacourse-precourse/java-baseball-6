cf1a325134eae93e899cca465f724407448144cf
junho <ssmm0205@naver.com>
2023-10-24 02:52:41 +0900

    refactor: GameCommand로 이름 변경 및 getter 제거

---
+++ src/main/java/baseball/controller/GameCommand.java
@@ -0,0 +1,22 @@
+package baseball.controller;
+
+import java.util.Arrays;
+
+public enum GameCommand {
+    RESTART("1"),
+    EXIT("2");
+
+    private final String number;
+
+    GameCommand(String number) {
+        this.number = number;
+    }
+
+    public static GameCommand from(String command) {
+        return Arrays.stream(values())
+                .filter(code -> code.number.equals(command))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("잘못된 코드를 입력하셨습니다."));
+    }
+
+}
