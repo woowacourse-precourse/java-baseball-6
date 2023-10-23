faf92650a8ba8d8fe1d0bfb0addefa553e27588e
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-10-23 16:54:24 +0900

    feat(Computer): 힌트 반환 기능 구현

--- src/main/java/baseball/computer/Computer.java
+++ src/main/java/baseball/computer/Computer.java
@@ -8,9 +8,10 @@ public class Computer {
     private static final int GAME_NUMBER_SIZE = 3;
     private static final int MIN_GAME_NUMBER_RANGE = 1;
     private static final int MAX_GAME_NUMBER_RANGE = 9;
+    private List<Integer> answer;
 
     public GameNumbers generateAnswer() {
-        List<Integer> answer = new ArrayList<>();
+        answer = new ArrayList<>();
 
         while (answer.size() != GAME_NUMBER_SIZE) {
             int randomNumber = pickRandomNumber();
@@ -22,6 +23,24 @@ public class Computer {
             answer.add(randomNumber);
         }
         return new GameNumbers(answer);
+    }
+
+    public Hint getHint(List<Integer> user) {
+        int ball = 0;
+        int strike = 0;
+
+        for (int i = 0; i < GAME_NUMBER_SIZE; i++) {
+            int userNumber = user.get(i);
+            int computerNumber = answer.get(i);
+
+            if (answer.contains(userNumber) && userNumber != computerNumber) {
+                ball++;
+            } else if (answer.contains(userNumber) && userNumber == computerNumber) {
+                strike++;
+            }
+        }
+
+        return new Hint(List.of(ball, strike));
     }
 
     private static int pickRandomNumber() {
