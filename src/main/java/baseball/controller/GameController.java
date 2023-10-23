423e1eb15683c83f6f1dccabe200486d1f84bf90
chs98412 <chs98412@gmail.com>
2023-10-23 22:05:42 +0900

    feat: 게임 로직 추가 무한루프를 없애고 싶은데 좋은 방법이 없을지 고민해보기

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -4,7 +4,47 @@ import baseball.entity.Game;
 import baseball.entity.Player;
 import baseball.entity.Result;
 
+import static baseball.controller.OutputController.*;
+import static baseball.controller.OutputController.STRIKE;
+
 public class GameController {
+
+    InputController inputController;
+    OutputController outputController;
+    Game game;
+    Player player;
+
+    public GameController(InputController inputController, OutputController outputController, Game game, Player player) {
+        this.inputController = inputController;
+        this.outputController = outputController;
+        this.game = game;
+        this.player = player;
+    }
+
+    public void baseballGame() {
+
+        while (true) {
+            outputController.printNotice(INPUT);
+            player.setInputString(inputController.playerInput());
+            Result result = game(player, game);
+
+            outputController.printResult(result.strike, result.ball);
+            if (result.isAnswer) {
+                outputController.printNoticeln(ANSWER);
+                return;
+            }
+        }
+
+    }
+
+    public boolean re() {
+        outputController.printNoticeln(AFTER);
+        String regame = inputController.reGameInput();
+        if (regame.equals("1") ){
+            return true;
+        }
+        return false;
+    }
 
     public Result game(Player player,Game game) {
         player.setStrikeCount(game.countStrike(player.getInputString()));
