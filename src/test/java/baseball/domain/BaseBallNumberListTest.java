bf2f6c394fbbbee953e5a323f4a9c8f8dbb1e467
hunko <rhwlgns4386@naver.com>
2023-10-23 17:20:39 +0900

    test : unification with assertj

--- src/test/java/baseball/domain/BaseBallNumberListTest.java
+++ src/test/java/baseball/domain/BaseBallNumberListTest.java
@@ -1,7 +1,7 @@
 package baseball.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -19,31 +19,35 @@ public class BaseBallNumberListTest {
 
         assertThat(result.size()).isEqualTo(3);
         assertThat(result).isEqualTo(testNumberList);
-        assertThrows(UnsupportedOperationException.class, () -> result.add(1));
-        assertThrows(UnsupportedOperationException.class, () -> result.remove(1));
+        assertThatThrownBy(() -> result.add(1)).isInstanceOf(UnsupportedOperationException.class);
+        assertThatThrownBy(() -> result.remove(1)).isInstanceOf(
+                UnsupportedOperationException.class);
     }
 
     @Test
     @DisplayName("리스트의 길이가 3이 아닐때 IllegalArgumentException이 발생한다")
     public void should_throwsIllegalArgumentException_when_lengthOfTheListIsNot3() {
 
-        assertThrows(IllegalArgumentException.class, () ->
+        assertThatThrownBy(() ->
                 new BaseBallNumberList(
-                        List.of(1, 5, 9, 6)));
+                        List.of(1, 5, 9, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("문자로 숫자를 3개가 아닌 입력시 IllegalArgumentException이 발생한다")
     public void should_throwsIllegalArgumentException_when_inputForStringLengthIsNot3() {
-        assertThrows(IllegalArgumentException.class, () ->
+        assertThatThrownBy(() ->
                 new BaseBallNumberList(new NumericString(
-                        "41").toList()));
+                        "41").toList()))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("같은 숫자가 존재 할시 IllegalArgumentException이 발생한다")
     public void should_throwsIllegalArgumentException_when_haveASameNumber() {
-        assertThrows(IllegalArgumentException.class, () ->
-                new BaseBallNumberList(List.of(1, 4, 1)));
+        assertThatThrownBy(() ->
+                new BaseBallNumberList(List.of(1, 4, 1)))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
