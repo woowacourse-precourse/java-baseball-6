7fabc6ac24e9fe37f8b3d784fa0400553d09f26a
isyoo1213 <moethns@gmail.com>
2023-10-23 14:26:30 +0900

    feat: 힌트 알림 및 반복 기능 추가 GameProgressController - progressGame()의 while구문 추가 - userGuessing() 추가 - saveComputerGenerateNumbers() 추가 - saveUserNumbers() 추가 README.md - 기능 명세 구현 체크

--- src/main/java/baseball/controller/GameProgressController.java
+++ src/main/java/baseball/controller/GameProgressController.java
@@ -1,14 +1,43 @@
 package baseball.controller;
 
+import baseball.model.GameNumber;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 
 public class GameProgressController {
     OutputView outputView = new OutputView();
     InputView inputView = new InputView();
+    GameNumber gameNumber = new GameNumber();
+    UserInputValidator userInputValidator = new UserInputValidator();
+    ComputerController computerController = new ComputerController();
 
     public void progressGame() {
-        outputView.printGameStart();
-        String userInputString = inputView.getUserGuessingNumbers();
+        boolean progressState = true;
+
+        while (progressState) {
+            outputView.printGameStart();
+            saveComputerGenerateNumbers();
+            userGuessing();
+        }
+    }
+
+    private void userGuessing() {
+        boolean isAnswer = false;
+        while (!isAnswer) {
+            String userInputString = inputView.getUserGuessingNumbers();
+            saveUserNumbers(userInputString);
+            isAnswer = computerController.checkUserInputWithAnswer(gameNumber.getUserInputNumbers(), gameNumber.getComputerGenerateNumbers());
+            outputView.printHint(computerController.createHintMessage());
+        }
+
+    }
+
+    private void saveComputerGenerateNumbers() {
+        computerController.startGame(gameNumber);
+    }
+
+    private void saveUserNumbers(String userInputString) {
+        gameNumber.setUserInputString(userInputValidator.userInputStringValidate(userInputString));
+        gameNumber.setUserInputNumbers(userInputValidator.userInputNumbersValidate(gameNumber.getUserInputString()));
     }
 }
