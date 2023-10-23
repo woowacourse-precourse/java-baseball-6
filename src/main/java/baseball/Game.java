f10d3fee53ea1b93af3c40a6e2aa7fd7d0aa8c4c
ikjo39 <chi4321@naver.com>
2023-10-23 21:30:55 +0900

    feat: 점수 클래스 및 점수 계산 기능 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -11,6 +11,23 @@ public class Game {
         List<Integer> computerNumbers = createComputerNumbers();
         System.out.print("숫자를 입력해주세요 : ");
         List<Integer> userNumbers = new GameInput().convertInputToUserNumbers();
+        Score score = calculateScore(computerNumbers, userNumbers);
+    }
+
+    private Score calculateScore(List<Integer> computerNumbers, List<Integer> userNumbers) {
+        int strike = 0;
+        int ball = 0;
+
+        for (int i = 0; i < computerNumbers.size(); i++) {
+            int nowComputer = computerNumbers.get(i);
+            int nowUser = userNumbers.get(i);
+            if (nowComputer == nowUser) {
+                strike++;
+            } else if (computerNumbers.contains(nowUser)) {
+                ball++;
+            }
+        }
+        return new Score(strike, ball);
     }
 
     private List<Integer> createComputerNumbers() {
---
+++ src/main/java/baseball/Score.java
@@ -0,0 +1,11 @@
+package baseball;
+
+public class Score {
+    private final int strike;
+    private final int ball;
+
+    public Score(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+}
