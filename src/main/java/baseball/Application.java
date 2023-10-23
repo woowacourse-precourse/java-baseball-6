3ba655394f5f435d499e410605f4fb7981dc35e3
Sung wook Park <109124287+respectwo2@users.noreply.github.com>
2023-10-23 14:42:58 +0900

    feat: 사용자 입력 값 받기 및 랜덤 난수 Comsball 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Comsball comsball = new Comsball();
+        int[] coms_ball = comsball.make_ball();
+        String input;
+
+        System.out.print("숫자를 입력하세요: ");
+        input = Console.readLine();
+
+    	
     }
 }
