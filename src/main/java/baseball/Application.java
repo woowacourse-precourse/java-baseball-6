a9dd3d1cd8b3f76f5ecd6481d3f9f682f27c83a3
김주영 <gimjuyeong0620@gmail.com>
2023-10-23 16:42:57 +0900

    refactoring

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,17 +8,14 @@ import java.util.Objects;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
+        Game game = new Game();
+        game.gameStart();
         while(true){
-            RandomNumber randomNumber = new RandomNumber();
-            System.out.println(randomNumber.number);
-            CompareNumber compareNumber = new CompareNumber();
-            compareNumber.go(randomNumber.number);
-            String input = Console.readLine();
-            if (Objects.equals(input, "2")){
-                System.out.println("게임 종료");
+            game.playGame();
+            if(!game.keepGo()){
                 break;
             }
         }
+
     }
 }
