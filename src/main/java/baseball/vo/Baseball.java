7f82b548a86cbec2e90e58deeb2a22784511db74
rlatneorp <rlatneorp@gmail.com>
2023-10-23 12:52:51 +0900

    feat: vo에서 사용 안하는 것들 삭제

--- src/main/java/baseball/vo/Baseball.java
+++ src/main/java/baseball/vo/Baseball.java
@@ -1,32 +1,11 @@
 package baseball.vo;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Baseball {
-	private String[] userChoiceNumberResult;
-	private ArrayList<Character> computerChoiceNumberResult;
 	private String computerNumber;
 	private int ball;
 	private int strike;
 
 	public Baseball() {}
-
-	public String[] getUserChoiceNumberResult() {
-		return userChoiceNumberResult;
-	}
-
-	public void setUserChoiceNumberResult(String[] userChoiceNumberResult) {
-		this.userChoiceNumberResult = userChoiceNumberResult;
-	}
-
-	public ArrayList<Character> getComputerChoiceNumberResult() {
-		return computerChoiceNumberResult;
-	}
-
-	public void setComputerChoiceNumberResult(ArrayList<Character> computerChoiceNumberResult) {
-		this.computerChoiceNumberResult = computerChoiceNumberResult;
-	}
 
 	public String getComputerNumber() {
 		return computerNumber;
@@ -54,13 +33,10 @@ public class Baseball {
 
 	@Override
 	public String toString() {
-		return "Baseball [userChoiceNumberResult=" + Arrays.toString(userChoiceNumberResult)
-				+ ", computerChoiceNumberResult=" + computerChoiceNumberResult + ", computerNumber=" + computerNumber
-				+ ", ball=" + ball + ", strike=" + strike + "]";
+		return "Baseball [computerNumber=" + computerNumber + ", ball=" + ball + ", strike=" + strike + "]";
 	}
-	
-	
 
+	
 
 
 
