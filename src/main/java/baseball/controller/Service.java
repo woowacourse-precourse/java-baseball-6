d76c4897ebee892943f706f1ce2b4597dfd70c72
seondays <agseon9@gmail.com>
2023-10-23 17:57:03 +0900

    feat: 게임 시작과 진행 기능 구현

--- src/main/java/baseball/controller/Service.java
+++ src/main/java/baseball/controller/Service.java
@@ -3,16 +3,51 @@ package baseball.controller;
 import baseball.model.Computer;
 import baseball.model.User;
 import baseball.view.InputView;
+import baseball.view.OutputView;
 import java.util.List;
 
 public class Service {
     Computer computer = new Computer();
     User user = new User();
     InputView inputView = new InputView();
+    boolean gameStop = false;
     int ball;
     int strike;
     List<Integer> computerNumbers;
     List<Integer> userNumbers;
+
+    public void gameStart() {
+        initializeGame();
+        System.out.println(OutputView.STARTING_MESSAGE);
+        while (!gameStop) {
+            ball = 0;
+            strike = 0;
+            System.out.print(OutputView.INPUT_MESSAGE);
+            userNumbers = getUserInput(inputView.getUserGameNumber());
+            compareComputerAndUser(userNumbers, computerNumbers);
+            if (strike == 3) {
+                gameStop = true;
+                System.out.println(strike + OutputView.STRIKE_MESSAGE);
+                System.out.println(OutputView.EXIT_MESSAGE);
+                System.out.println(OutputView.CONTINUE_MESSAGE);
+                askRestartGame(inputView.getUserGameContinue());
+            }
+            if (strike != 3) {
+                if (ball == 0 && strike == 0) {
+                    System.out.println(OutputView.NOTHING_MESSAGE);
+                }
+                if (ball > 0 && strike == 0) {
+                    System.out.println(ball + OutputView.BALL_MESSAGE);
+                }
+                if (ball == 0 && strike > 0) {
+                    System.out.println(strike + OutputView.STRIKE_MESSAGE);
+                }
+                if (ball > 0 && strike > 0) {
+                    System.out.println(ball + OutputView.BALL_MESSAGE + " " + strike + OutputView.STRIKE_MESSAGE);
+                }
+            }
+        }
+    }
 
     public void initializeGame() {
         computer.setNumbers();
@@ -31,6 +66,13 @@ public class Service {
             } else if (computer.contains(user.get(i))) {
                 ball++;
             }
+        }
+    }
+
+    public void askRestartGame(String input) {
+        if (input.equals("1")) {
+            gameStop = false;
+            gameStart();
         }
     }
 }
