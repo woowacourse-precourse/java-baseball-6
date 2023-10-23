7d1ae92eccac18af601841d8e3ddc8558cf26397
davemins <davemins4125@naver.com>
2023-10-23 17:30:17 +0900

    feat(Output) : 게임 시작 시작 메시지 출력 기능 추가

---
+++ src/main/java/baseball/Output.java
@@ -0,0 +1,11 @@
+package baseball;
+
+public class Output {
+    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+
+    public void printStartGame() {
+        System.out.println(GAME_START_MESSAGE);
+    }
+
+
+}
