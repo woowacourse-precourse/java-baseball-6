fe136008b7b93b74c0141795b4404a36a476edad
정성욱 <rkfka7849@gmail.com>
2023-10-23 17:59:17 +0900

    docs(README): 기능 목록 수정

--- src/main/java/baseball/domain/GameScore.java
+++ src/main/java/baseball/domain/GameScore.java
@@ -18,5 +18,6 @@ public class GameScore {
 
     public boolean isStrikesAllOfBalls() {
         return numberOfStrikes.isStrikeAllOfBalls();
+        //TODO 2023 10 22 의미없이 2중으로 연결된 메서드를 어떻게 해결하면 좋을지 고민
     }
 }
