3622ec6685df9d9946c3cb72f751e4a4b7e00f3e
BellWin98 <jongseunghan@naver.com>
2023-10-23 14:49:30 +0900

    [Feat] 숫자 랜덤 뽑기 클래스 생성(컴퓨터 랜덤 숫자 생성 메소드 구현)

--- src/main/java/baseball/RandomNumber.java
+++ src/main/java/baseball/RandomNumber.java
@@ -1,2 +1,22 @@
-package baseball;public class RandomNumber {
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomNumber {
+    private final Validation validation = new Validation();
+
+    public RandomNumber() {
+    }
+
+    public List<Integer> getRandomNumbers(){
+        List<Integer> computer = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            computer.add(Randoms.pickNumberInRange(1, 9));
+        }
+        validation.isNumberDuplicate(computer);
+        return computer;
+    }
 }
