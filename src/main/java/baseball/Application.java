7679569a2dd1c2f7fa259798cc4041414a99ec6c
chaerlo127 <monynony0203@gmail.com>
2023-10-23 15:18:18 +0900

    chore: Constant 생성 후 상수로 코드 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,6 +8,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
+import static baseball.Constant.PlayGame.*;
+import static baseball.Constant.StartGame.*;
+
 public class Application {
     public static void main(String[] args) {
         Application ap = new Application();
@@ -19,29 +22,29 @@ public class Application {
         String input = null;
         do {
             // 게임 시작
-            System.out.println("숫자 야구 게임을 시작합니다.");
+            System.out.println(GAME_START_MESSAGE);
             // 게임 함수로 이동
             play();
             // 재시작 or 종료
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            System.out.println(RESTART_OR_QUIT_MESSAGE);
             input = Console.readLine();
-            if(!input.equals("1") && !input.equals("2")) throw new IllegalArgumentException(); // 에러 확인
-        }while(!input.equals("2"));
+            if(!input.equals(START_MENU) && !input.equals(END_MENU)) throw new IllegalArgumentException(); // 에러 확인
+        }while(!input.equals(END_MENU));
     }
 
     // 야구 게임
     private void play() {
         List<Integer> randomNums = createRanNum();
         while(true){
-            System.out.print("숫자를 입력해주세요 : ");
+            System.out.print(PLAY_START_MESSAGE);
             String num = Console.readLine();
 
             // 숫자가 3자리가 아닌 경우
-            if(num.length() > 3) throw new IllegalArgumentException();
+            if(num.length() > NUMBER_LENGTH) throw new IllegalArgumentException();
             List<Integer> numbers = new ArrayList<>();
             // 같은 값을 입력하거나 숫자가 아닌 값을 입력한 경우
             try{
-               Arrays.stream(num.split("")).forEach(i -> {
+               Arrays.stream(num.split(CROP_UNIT)).forEach(i -> {
                    if (numbers.contains(Integer.parseInt(i))) throw new IllegalArgumentException();
                    else numbers.add(Integer.parseInt(i));
                });
@@ -49,18 +52,18 @@ public class Application {
                 throw new IllegalArgumentException();
             }
 
-            boolean[] visited = new boolean[numbers.size()];
+            boolean[] visited = new boolean[NUMBER_LENGTH];
             int strike = checkStrike(randomNums, numbers, visited);
-            if(strike == 3) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            if(strike == NUMBER_LENGTH) {
+                System.out.println(PLAY_END_MESSAGE);
                 return;
             }
             // 볼 체크
             int ball = checkBall(randomNums, numbers, visited);
 
-            if(strike != 0) System.out.print(strike+"스트라이크");
-            if(ball != 0) System.out.print(ball+"볼");
-            if (strike == 0 && ball == 0)System.out.print("낫싱");
+            if(strike != 0) System.out.print(strike+STRIKE_STRING);
+            if(ball != 0) System.out.print(ball+BALL_STRING);
+            if (strike == 0 && ball == 0)System.out.print(NOTHING_STRING);
             System.out.println();
         }
     }
@@ -88,7 +91,7 @@ public class Application {
 
     private List<Integer> createRanNum() {
         List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
+        while (computer.size() < NUMBER_LENGTH) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
