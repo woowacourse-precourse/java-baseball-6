f9408b54138b8d137115ff47d5afee98a5e26722
SongJwans <yhh5916@naver.com>
2023-10-24 02:30:16 +0900

    style : 불필요한 공백 삭제

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -4,14 +4,13 @@ import java.util.regex.Pattern;
 
 public class Validator {
     private static final int NUMBER_LENGTH = 3;
-    
+
     private static final String DUPLICATE_ERROR = "중복된 숫자가 존재합니다.";
     private static final String LENGTH_ERROR = "입력 숫자의 길이가 3이 아닙니다.";
     private static final String OUT_RANGE_ERROR = "1과 9사이의 숫자가 아닙니다.";
 
     private static final Pattern INPUT_REGEX = Pattern.compile("[1-9]{3}");
     private static final Pattern INPUT_REPEAT = Pattern.compile("^(?!.*(\\d).*\\1)[1-9]{3}$");
-
 
     public void validateInputLength(String input) {
         if (input.length() != NUMBER_LENGTH) {
