f78141a7c18bd5b0bcbbf72b886ea73040ccff74
myminimin <triplem0604@kakao.com>
2023-10-23 22:44:45 +0900

    [Feat] 사용자 입력과 컴퓨터의 숫자를 비교하고 ballCount/StrikeCount를 설정하는 메서드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,6 +7,10 @@ import java.util.List;
 
 public class Application {
 
+    private static List<Integer> computerNumber;
+    private static int ballCount;
+    private static int strikeCount;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -15,7 +19,7 @@ public class Application {
 
 
         public static List<Integer> generateRandomNumbers() {
-            List<Integer> computerNumber = new ArrayList<>();
+            computerNumber = new ArrayList<>();
             while (computerNumber.size() < 3) {
                 int randomNumber = Randoms.pickNumberInRange(1, 9);
                 if (!computerNumber.contains(randomNumber)) {
@@ -23,6 +27,19 @@ public class Application {
                 }
             }
             return computerNumber;
+        }
+
+        public static void compareNumbers(List<Integer> userInput){
+            ballCount = 0;
+            strikeCount = 0;
+
+            for(int i=0; i<3; i++){
+                if(userInput.get(i).equals(computerNumber.get(i))){
+                    strikeCount++;
+                } else if (computerNumber.contains(userInput.get(i))) {
+                    ballCount++;
+                }
+            }
         }
     }
 
