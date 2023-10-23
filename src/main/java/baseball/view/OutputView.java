7d04a95a4f89a28fdda0b9bb7e7f7417ce840373
seondays <agseon9@gmail.com>
2023-10-23 16:18:42 +0900

    fix: 오타 수정

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,7 +3,7 @@ package baseball.view;
 
 public class OutputView {
     public static final String STARTING_MESSAGE = "숫자 야구 게임을 시작합니다.";
-    public static final String INPUT_MESSAGE = "숫자를 입력해지주세요 : ";
+    public static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
     public static final String STRIKE_MESSAGE = "스트라이크";
     public static final String BALL_MESSAGE = "볼";
     public static final String NOTHING_MESSAGE = "낫싱";
