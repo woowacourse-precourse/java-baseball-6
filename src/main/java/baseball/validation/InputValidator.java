23118588dc0a36079dc4702228f2f1ce82d9f3b1
정찬영 <jcy0342@gmail.com>
2023-10-23 16:07:38 +0900

    feat(InputValidator): 각 숫자가 유니크한 값인지 검증하는 기능 구현
    - 각 숫자가 유니크한지 검증하기 위한 validateUniqueNumber
    - 유니크 검사와 숫자 범위 검증은 같은 타이밍에 발생하므로 동시에 호출하는 validatePatternWithUnique 구현

--- src/main/java/baseball/validation/InputValidator.java
+++ src/main/java/baseball/validation/InputValidator.java
@@ -4,6 +4,11 @@ import baseball.model.Rule;
 
 public class InputValidator {
 
+    public static void validatePatternWithUnique(String stringNumber) {
+        validateNumberWithPattern(stringNumber);
+        validateUniqueNumber(stringNumber);
+    }
+
     private static void validateNumberWithPattern(String stringNumber) {
         String pattern = "^["
                 + Rule.START_NUMBER
@@ -15,4 +20,17 @@ public class InputValidator {
             throw new IllegalArgumentException("1~9 사이의 숫자를 3개 입력해주세요.");
         }
     }
+
+    private static void validateUniqueNumber(String stringNumber) {
+        int[] digitCounts = new int[10];
+
+        for (char c : stringNumber.toCharArray()) {
+            int digit = Character.getNumericValue(c);
+            digitCounts[digit]++;
+            if (digitCounts[digit] > 1) {
+                throw new IllegalArgumentException("각 숫자는 한 번만 나와야 합니다.");
+            }
+        }
+    }
+
 }
