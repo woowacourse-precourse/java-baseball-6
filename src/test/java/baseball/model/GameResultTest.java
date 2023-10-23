4ae2cc4e39acfef17a995254ecdf634189a8a87b
SungWoo <sungwoo166@gmail.com>
2023-10-23 16:30:01 +0900

    test: Implement test code for GameResult

---
+++ src/test/java/baseball/model/GameResultTest.java
@@ -0,0 +1,45 @@
+package baseball.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import baseball.BaseballScoreMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class GameResultTest {
+
+    @Test
+    @DisplayName("2스트라이크 1볼일 경우")
+    void testToStringWithBothStrikeAndBall() {
+        GameResult gameResult = new GameResult(2, 1);
+        String result = gameResult.toString();
+        assertEquals(BaseballScoreMessage.BALL.getMessageWithCount(1) + " "
+                + BaseballScoreMessage.STRIKE.getMessageWithCount(2), result);
+    }
+
+    @Test
+    @DisplayName("2스트라이크일 경우")
+    void testToStringWithOnlyStrike() {
+        GameResult gameResult = new GameResult(2, 0);
+        String result = gameResult.toString();
+        assertEquals(BaseballScoreMessage.STRIKE.getMessageWithCount(2), result);
+    }
+
+    @Test
+    @DisplayName("2볼일 경우")
+    void testToStringWithOnlyBall() {
+        GameResult gameResult = new GameResult(0, 2);
+        String result = gameResult.toString();
+        assertEquals(BaseballScoreMessage.BALL.getMessageWithCount(2), result);
+    }
+
+    @Test
+    @DisplayName("0스트라이트 0볼일 경우")
+    void testToStringWithNeitherStrikeNorBall() {
+        GameResult gameResult = new GameResult(0, 0);
+        String result = gameResult.toString();
+        assertEquals(BaseballScoreMessage.NOTHING.toString(), result);
+    }
+
+
+}
