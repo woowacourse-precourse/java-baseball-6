3463d32a47f257704ab5b564df2e815f09c2bdc1
YongHyeon <pyh4616@naver.com>
2023-10-24 01:15:00 +0900

    Feat: GameController, 게임을 진행하는 기능 구현

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,6 +1,8 @@
 package baseball.controller;
 
 import baseball.model.ComputerNumber;
+import baseball.model.RestartOrExitNumber;
+import baseball.model.UserNumber;
 import baseball.service.GameService;
 import baseball.view.InputView;
 import baseball.view.OutputView;
@@ -19,16 +21,23 @@ public class GameController {
     }
 
     public void playGame() {
-        int restartOrExitNumber = 1;
+        int restartOrExitNumber = 0;
         while (restartOrExitNumber != 2) {
             outputView.printGameStartMessage();
-            List<Integer> computerNumber = getComputerNumber();
+            List<Integer> computerNumber = gameService.createComputerNumber();
+            new ComputerNumber(computerNumber);
+            String compareResult = "";
+            while (!compareResult.equals("3스트라이크")) {
+                outputView.printInputNumberMessage();
+                List<Integer> userNumber = inputView.inputUserNumber();
+                new UserNumber(userNumber);
+                compareResult = gameService.getCompareResult(computerNumber, userNumber);
+                outputView.printCompareResult(compareResult);
+            }
+            outputView.printGameOverMessage();
+            outputView.printGameContinueMessage();
+            restartOrExitNumber = inputView.inputRestartOrExitNumber();
+            new RestartOrExitNumber(restartOrExitNumber);
         }
-    }
-
-    private List<Integer> getComputerNumber() {
-        List<Integer> computerNumber = gameService.createComputerNumber();
-        new ComputerNumber(computerNumber);
-        return computerNumber;
     }
 }
