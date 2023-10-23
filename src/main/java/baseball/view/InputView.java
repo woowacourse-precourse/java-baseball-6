a5577ebee266f24c0cff9ca21b6750c702f12275
qudwns017 <qudwns017@naver.com>
2023-10-24 01:50:04 +0900

    Refactor: static 키워드 삭제

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,10 +1,9 @@
 package baseball.view;
 
-import baseball.domain.Messages;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static String getInput() {
+    public String getInput() {
         return Console.readLine();
     }
 }
