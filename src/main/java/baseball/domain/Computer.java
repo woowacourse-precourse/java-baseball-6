b46cb8e14ac71e14bb1b15d4739673ee1ec50788
youKeon <yukeon97@gmail.com>
2023-10-23 22:27:41 +0900

    refactor: 컴퓨터 숫자 선택 기능 리팩토링

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,32 +1,27 @@
 package baseball.domain;
 
-import baseball.constant.Constants;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
-    private final String numbers;
+    private final List<Integer> numbers;
 
     public Computer() {
         this.numbers = generateNumber();
     }
 
-    private String generateNumber() {
+    private List<Integer> generateNumber() {
         List<Integer> generatedNumbers = new ArrayList<>();
         while (generatedNumbers.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!generatedNumbers.contains(randomNumber)) generatedNumbers.add(randomNumber);
         }
-
-        StringBuilder sb = new StringBuilder();
-        for (int num : generatedNumbers) sb.append(num);
-
-        return sb.toString();
+        return generatedNumbers;
     }
 
-    public String getNumbers() {
+    public List<Integer> getNumbers() {
         return numbers;
     }
 }
