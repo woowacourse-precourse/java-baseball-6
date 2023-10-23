6e6fa18ac93f365c4996b58d07358653e97c8dfa
chan-won <clw8679@naver.com>
2023-10-23 23:09:44 +0900

    Feat: Application 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,33 @@
 package baseball;
 
+import baseball.Io.Input;
 import baseball.Io.Output;
+import baseball.service.CompareNumber;
+import baseball.service.RandomNumberGenerator;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         Output.printGameStart();
 
+        List<Integer> computer = RandomNumberGenerator.generate();
+        boolean gameEndFlag = true;
+        while(gameEndFlag) {
+            while (true) {
+                List<Integer> user = Input.Numbers();
+
+                int strikeCount = CompareNumber.CountStrike(computer, user);
+                int ballCount = CompareNumber.CountBall(computer, user);
+
+                Output.printGameResult(strikeCount, ballCount);
+
+                if (strikeCount == 3) {
+                    Output.printGameEnd();
+                    break;
+                }
+            }
+
+            gameEndFlag = Input.GameEndFlag();
+        }
     }
 }
