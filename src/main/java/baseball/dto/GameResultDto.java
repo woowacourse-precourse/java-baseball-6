bf02ccb137e1212c1cd418a96a5c0636d4b4e051
gywns0417 <gywns0417@gmail.com>
2023-10-24 00:03:37 +0900

    refactor: dto 클래스 분리

--- src/main/java/baseball/dto/GameResultDto.java
+++ src/main/java/baseball/dto/GameResultDto.java
@@ -1,47 +1,9 @@
 package baseball.dto;
 
 public class GameResultDto {
-    private String randomNumber;
-    private String validateInput;
-    private int strike;
-    private int ball;
     private String gameResultMessage;
-
     private boolean isRestart;
     private boolean isCorrectAnswer;
-
-
-    public String getRandomNumber() {
-        return randomNumber;
-    }
-
-    public void setRandomNumber(String randomNumber) {
-        this.randomNumber = randomNumber;
-    }
-
-    public String getValidateInput() {
-        return validateInput;
-    }
-
-    public void setValidateInput(String validateInput) {
-        this.validateInput = validateInput;
-    }
-
-    public int getStrike() {
-        return strike;
-    }
-
-    public void setStrike(int strike) {
-        this.strike = strike;
-    }
-
-    public int getBall() {
-        return ball;
-    }
-
-    public void setBall(int ball) {
-        this.ball = ball;
-    }
 
     public String getGameResultMessage() {
         return gameResultMessage;
---
+++ src/main/java/baseball/dto/NumberDto.java
@@ -0,0 +1,22 @@
+package baseball.dto;
+
+public class NumberDto {
+    private String randomNumber;
+    private String validateInput;
+
+    public String getRandomNumber() {
+        return randomNumber;
+    }
+
+    public void setRandomNumber(String randomNumber) {
+        this.randomNumber = randomNumber;
+    }
+
+    public String getValidateInput() {
+        return validateInput;
+    }
+
+    public void setValidateInput(String validateInput) {
+        this.validateInput = validateInput;
+    }
+}
