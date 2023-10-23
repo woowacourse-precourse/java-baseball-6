7d09b3ee6b99f343e5f2cad84d61edd80761863f
yeseong31 <yeseong31@naver.com>
2023-10-24 00:45:11 +0900

    refactor: 사용자 입력을 PlayerService에서 처리하도록 변경

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -1,19 +1,14 @@
 package baseball.controller;
 
 import static baseball.constant.BaseballConstant.BASEBALL_THREE_STRIKES;
-import static baseball.constant.BaseballConstant.RESTART_OPTION;
-import static baseball.constant.BaseballConstant.TURN_OFF_OPTION;
 
 import baseball.domain.Computer;
 import baseball.service.ComputerService;
 import baseball.service.MessageService;
 import baseball.service.PlayerService;
 import baseball.validator.BaseballValidator;
-import camp.nextstep.edu.missionutils.Console;
 
 public class BaseballController {
-
-    private static final String WRONG_CHOICE_MESSAGE = "잘못된 선택입니다.";
 
     private final ComputerService computerService;
     private final PlayerService playerService;
@@ -29,24 +24,7 @@ public class BaseballController {
     public void start() {
         do {
             playGame();
-        } while (askToContinue());
-    }
-
-    private boolean askToContinue() {
-        String number = Console.readLine();
-        return selectOption(number);
-    }
-
-    private boolean selectOption(String number) {
-        switch (Integer.parseInt(number)) {
-            case RESTART_OPTION -> {
-                return true;
-            }
-            case TURN_OFF_OPTION -> {
-                return false;
-            }
-            default -> throw new IllegalArgumentException(WRONG_CHOICE_MESSAGE);
-        }
+        } while (playerService.askToContinue());
     }
 
     private void playGame() {
@@ -58,7 +36,6 @@ public class BaseballController {
             messageService.inputPlayerNumber();
 
             String playerBaseballNumber = playerService.receiveBaseballNumber();
-
             balls = computer.countBalls(playerBaseballNumber);
             strikes = computer.countStrikes(playerBaseballNumber);
 
--- src/main/java/baseball/service/PlayerService.java
+++ src/main/java/baseball/service/PlayerService.java
@@ -1,9 +1,14 @@
 package baseball.service;
 
+import static baseball.constant.BaseballConstant.RESTART_OPTION;
+import static baseball.constant.BaseballConstant.TURN_OFF_OPTION;
+
 import baseball.validator.BaseballValidator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class PlayerService {
+
+    private static final String WRONG_CHOICE_MESSAGE = "잘못된 선택입니다.";
 
     private final BaseballValidator baseballValidator;
 
@@ -15,5 +20,22 @@ public class PlayerService {
         String playerBaseballNumber = Console.readLine();
         baseballValidator.validate(playerBaseballNumber);
         return playerBaseballNumber;
+    }
+
+    public boolean askToContinue() {
+        String optionNumber = Console.readLine();
+        return selectOption(optionNumber);
+    }
+
+    private boolean selectOption(String optionNumber) {
+        switch (Integer.parseInt(optionNumber)) {
+            case RESTART_OPTION -> {
+                return true;
+            }
+            case TURN_OFF_OPTION -> {
+                return false;
+            }
+            default -> throw new IllegalArgumentException(WRONG_CHOICE_MESSAGE);
+        }
     }
 }
