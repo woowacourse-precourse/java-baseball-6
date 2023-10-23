e8a9f82a91cdcc84c6a1a58119aabbde7333853c
eom-tae-in <eti0728@daum.net>
2023-10-24 01:17:02 +0900

    test: 볼과 스트라이크 수를 계산하는 메서드를 하나로 합쳐 테스트를 진행행하는 방식으로 변경

--- src/test/java/baseball/model/ComputerTest.java
+++ src/test/java/baseball/model/ComputerTest.java
@@ -4,12 +4,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import baseball.helper.TestNumberGenerator;
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 @DisplayNameGeneration(ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
@@ -22,44 +24,39 @@ class ComputerTest {
         computer = Computer.createWith(new TestNumberGenerator(List.of(1, 5, 9)));
     }
 
-    @CsvSource({"1,2,3,1","4,5,6,1","7,8,9,1", "1,5,9,3"})
+    @MethodSource("guessNumbersAndResultProvider")
     @ParameterizedTest
-    void 스트라이크_수를_계산한다(int firstNumber, int secondNumber, int thirdNumber, int expected) {
-        // given
-        List<Integer> guessNumbers = List.of(firstNumber, secondNumber, thirdNumber);
-
+    void 볼과_스트라이크의_수를_계산한다(List<Integer> guessNumbers, int expectedBalls, int expectedStrikes) {
         // when
+        int ballCount = computer.countBalls(guessNumbers);
         int strikeCount = computer.countStrikes(guessNumbers);
 
         // then
-        assertThat(strikeCount).isEqualTo(expected);
+        assertThat(ballCount).isEqualTo(expectedBalls);
+        assertThat(strikeCount).isEqualTo(expectedStrikes);
     }
 
-    // 볼 개수 계산
-    @Test
-    void 볼_수를_계산한다() {
+    private static Stream<Arguments> guessNumbersAndResultProvider() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3), 0, 1),
+                Arguments.of(List.of(4, 5, 6), 0, 1),
+                Arguments.of(List.of(7, 8, 9), 0, 1),
+                Arguments.of(List.of(5, 1, 9), 2, 1),
+                Arguments.of(List.of(1, 5, 9), 0, 3)
+        );
+    }
+
+    @CsvSource({"3,2,1,false", "6,5,4,false", "1,5,9,true"})
+    @ParameterizedTest
+    void 모든_숫자를_맞추면_true를_반환한다(int firstNumber, int secondNumber, int thirdNumber, boolean expected) {
         // given
-        List<Integer> guessNumbers = List.of(1, 5, 9);
+        List<Integer> guessNumbers = List.of(firstNumber, secondNumber, thirdNumber);
 
         // when
         boolean result = computer.isGameOver(guessNumbers);
 
         // then
-        assertThat(result).isTrue();
-    }
-
-    // 게임이 끝났는지 확인
-    @CsvSource({"3,2,1,1","6,5,4,0","9,8,7,1", "9,5,1,2"})
-    @ParameterizedTest
-    void 모든_숫자를_맞추면_true를_반환한다(int firstNumber, int secondNumber, int thirdNumber, int expected) {
-        // given
-        List<Integer> guessNumbers = List.of(firstNumber, secondNumber, thirdNumber);
-
-        // when
-        int ballCount = computer.countBalls(guessNumbers);
-
-        // then
-        assertThat(ballCount).isEqualTo(expected);
+        assertThat(result).isEqualTo(expected);
     }
 }
 
