20072af8ea28664a5d6d418aaf3a163d01ae096a
ParkDH0809 <znxn1237@gmail.com>
2023-10-23 22:25:42 +0900

    refactor(게임): 메서드 수정
    플레이어 객체 생성에 따른 메서드 일부 수정

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 public class Game {
     private List<Integer> randomNumbers;
-    private List<Integer> inputNumbers;
     private int[] comparisonResult;
     private Game(List<Integer> randomNumbers) {
         this.randomNumbers = randomNumbers;
@@ -16,55 +15,14 @@ public class Game {
         return new Game(randomNumbers);
     }
 
-    public void setInputNumber(String inputValue) {
-        validateInputNumber(inputValue);
-        inputNumbers = changeStringToList(inputValue);
-    }
-
-    private void validateInputNumber(String inputValue) {
-        validateLengthThree(inputValue);
-        validateEachNumber(inputValue);
-    }
-
-    private void validateLengthThree(String inputValue) {
-        if(inputValue.length() != 3) {
-            throw new IllegalArgumentException("입력값이 3자리가 아닙니다.");
-        }
-    }
-
-    private void validateEachNumber(String inputValue) {
-        List<Character> compareList = new ArrayList<>();
-        for(char currentCharacter : inputValue.toCharArray()) {
-            isBetweenOneAndNine(currentCharacter);
-            if(compareList.contains(currentCharacter)) {
-                throw new IllegalArgumentException("서로 다른 숫자가 아닙니다.");
-            }
-            compareList.add(currentCharacter);
-        }
-    }
-
-    private void isBetweenOneAndNine(char currentCharacter) {
-        if(!('1' <= currentCharacter && currentCharacter <= '9')) {
-            throw new IllegalArgumentException("1 ~ 9 사이의 숫자가 아닙니다.");
-        }
-    }
-
-    private List<Integer> changeStringToList(String inputValue) {
-        List<Integer> inputNumbers = new ArrayList<>();
-        for (char currentCharacter : inputValue.toCharArray()) {
-            inputNumbers.add((int) (currentCharacter - '0'));
-        }
-        return inputNumbers;
-    }
-
-    public void compareNumber() {
+    public void compareNumber(List<Integer> inputNumbers) {
         comparisonResult = new int[]{0, 0};
         for(int currentPosition = 0; currentPosition < randomNumbers.size(); currentPosition++) {
-            compareInputNumber(currentPosition);
+            compareInputNumber(currentPosition, inputNumbers);
         }
     }
 
-    private void compareInputNumber(int currentPosition) {
+    private void compareInputNumber(int currentPosition, List<Integer> inputNumbers) {
         if(randomNumbers.get(currentPosition) == inputNumbers.get(currentPosition)) {
             comparisonResult[1]++;
             return;
