675cd4261f8f959fa3bff914a9d12cf8b8b85f33
styughjvbn <3338761@naver.com>
2023-10-23 19:23:42 +0900

    <feat> : 숫자 관련 클래스 생성 ------------------------------------------- 1. 숫자 클래스 생성   1. 입력된 문자가 숫자인지 체크 2. 숫자리스트 클래스 생성   1. 각 숫자들이 중복되지 않는지 체크   2. 리스트의 개수가 MAX_NUM_LEN을 넘지 않는지 체크

---
+++ src/main/java/baseball/Number.java
@@ -0,0 +1,21 @@
+package baseball;
+
+public class Number {
+    public final int num;
+
+    public Number(char num) {
+        if (Character.isDigit(num)) {
+            this.num = Character.getNumericValue(num);
+        } else {
+            throw new IllegalArgumentException("is not digit");
+        }
+    }
+
+    public Number(int num) {
+        this.num = num;
+    }
+
+    public boolean equals(Number obj) {
+        return this.num == obj.num;
+    }
+}
