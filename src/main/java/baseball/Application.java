1e790b54f7c88e93aa2a47f0b8794fd3d8ad5b4e
Sungkyu <kxu4583@naver.com>
2023-10-23 17:30:13 +0900

    '기능 2: 사용자로부터 서로 다른 3개의 1~9 사이의 숫자를 입력받기' 구현 및 한글 깨짐 오류 해결

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,6 +7,8 @@ import org.assertj.core.internal.IntArrays;
 public class Application {
     public static void main(String[] args) {
 
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
         //정답이 될 3개의 숫자를 만들기
         int[] answer = new int[3];
         for(int i=0;i<3;i++){
@@ -16,6 +18,15 @@ public class Application {
                 j=0;
             }
         }
+
+        System.out.print("숫자를 입력해주세요 : ");
+
+        int[] ask = new int[3];
+        String readLine = Console.readLine();
+        for(int i=0; i<3; i++){
+            ask[i] = readLine.charAt(i)-'0';
+        }
+
 
     }
 }
