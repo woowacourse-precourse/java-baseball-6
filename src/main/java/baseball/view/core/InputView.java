251480a0081b8574a737c2a3a479396aac42385a
JaehongDev <println@kakao.com>
2023-10-23 14:52:40 +0900

    feat: 세자리 숫자를 받아오는 View구현

--- src/main/java/baseball/view/core/InputView.java
+++ src/main/java/baseball/view/core/InputView.java
@@ -1,4 +1,12 @@
 package baseball.view.core;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public final class InputView extends ConsoleWriter {
+    public String readUserInput() {
+        this.print("숫자를 입력해주세요 : ");
+        final var input = Console.readLine();
+        return input;
+    }
+
 }
