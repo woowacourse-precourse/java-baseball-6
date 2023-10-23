a1c2d68e7dd0c839d8788b4fdd2f8655564293d6
compasstar <woohong24@naver.com>
2023-10-23 14:51:45 +0900

    feat: 추가 컨트롤러 및 뷰

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,20 @@
 package baseball;
 
+
+import baseball.controller.BaseballController;
+import baseball.repository.AnswerRepository;
+import baseball.service.AnswerService;
+import baseball.view.View;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        AnswerRepository answerRepository = new AnswerRepository();
+        AnswerService answerService = new AnswerService(answerRepository);
+        View view = new View();
+        BaseballController baseballController = new BaseballController(answerService, view);
+
+        //실행
+        baseballController.run();
     }
 }
