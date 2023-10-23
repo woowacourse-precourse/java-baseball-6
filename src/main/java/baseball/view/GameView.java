26686b2c6b3a995644452ec8a2d41a3464f15a1f
oxdjww <oxdjww@gmail.com>
2023-10-23 17:09:57 +0900

    refactor : 출력이 필요한 모든 기능들을 모아 GameView 생성

---
+++ src/main/java/baseball/view/GameView.java
@@ -0,0 +1,30 @@
+package baseball.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import baseball.model.BaseballGameCountsMessage;
+import baseball.model.UserNumber;
+import java.util.Objects;
+
+public class GameView {
+
+    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String GAME_WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임종료";
+
+    public static void printGameStartMessage() {
+        System.out.println(GAME_START_MESSAGE);
+    }
+
+    public static void printGameWinMessage() {
+        System.out.println(GAME_WIN_MESSAGE);
+    }
+
+    public static UserNumber getUserInput() {
+        System.out.print("숫자를 입력해주세요 : ");
+        return new UserNumber(readLine());
+    }
+
+    public static void printBaseballGameCountsMessage(BaseballGameCountsMessage message) {
+        System.out.println(Objects.requireNonNullElse(message.getMessage(), "낫싱"));
+    }
+}
