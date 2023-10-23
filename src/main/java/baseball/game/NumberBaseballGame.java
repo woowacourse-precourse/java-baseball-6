39c0b2785676c5fcd79aea53ce199f92df61207c
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-10-23 17:47:34 +0900

    feat(BaseballNumberGame): 게임 진행 기능 구현

--- src/main/java/baseball/game/NumberBaseballGame.java
+++ src/main/java/baseball/game/NumberBaseballGame.java
@@ -1,11 +1,18 @@
 package baseball.game;
 
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class NumberBaseballGame {
     private final Computer computer;
+    private final InputView inputView;
+    private final OutputView outputView;
     private GameNumbers answer;
 
     public NumberBaseballGame() {
         this.computer = new Computer();
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
     }
 
     public void run() {
@@ -19,7 +26,17 @@ public class NumberBaseballGame {
     }
 
     private void playGame() {
+        outputView.printStartGame();
 
+        while (true) {
+            GameNumbers userNumbers = inputView.readUserNumbers();
+            Hint hint = computer.getHint(userNumbers);
+            outputView.printHint(hint);
+
+            if (hint.getStrike() == 3) {
+                break;
+            }
+        }
     }
 
     private void endGame() {
