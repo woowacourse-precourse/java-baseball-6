7f4ee85d733d0377e6e1a43c29b44dfcc6cf8461
sheepysnoopy <qkffldlfdjsk12@naver.com>
2023-10-24 03:05:35 +0900

    feat(Application): 시작 멘트 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,9 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseballGame game = new BaseballGame();
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        game.run();
     }
 }
