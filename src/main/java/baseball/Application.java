61651b70e66ef73a46fcb21e4a74ae90d8a432a1
woo <wsk221e@gmail.com>
2023-10-23 22:42:48 +0900

    refactor: 역할에 따라 1차 클래스 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,6 +5,129 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
+class Defender {
+    private static final StringBuilder target = new StringBuilder();
+    private static int ballCount = 0;
+    private static int strikeCount = 0;
+
+    // 정답(3자리 숫자) 생성
+    public static void reroll() {
+        target.setLength(0);
+        List<Integer> temp = new ArrayList<>();
+        while (temp.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!temp.contains(randomNumber)) {
+                temp.add(randomNumber);
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            target.append(temp.get(i));
+        }
+        System.out.println(target); // 테스트용
+    }
+
+    // 정답 확인하기 - 볼, 스트라이크, 낫싱 출력하기
+    public static void printScore(String answer) {
+        calculateBallCount(answer);
+        calculateStrikeCount(answer);
+
+        if (ballCount != 0 && strikeCount == 0) {
+            System.out.printf("%d볼 ", ballCount);
+            System.out.println();
+        }
+        if (ballCount == 0 && strikeCount != 0) {
+            System.out.printf("%d스트라이크", strikeCount);
+            System.out.println();
+        }
+        if (ballCount != 0 && strikeCount != 0) {
+            System.out.printf("%d볼 ", ballCount);
+            System.out.printf("%d스트라이크", strikeCount);
+            System.out.println();
+        }
+        if (ballCount == 0 && strikeCount == 0) {
+            System.out.println("낫싱");
+        }
+    }
+
+    // 정답 확인하기 - 볼
+    public static void calculateBallCount(String answer) {
+        ballCount = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i != j && answer.charAt(i) == target.charAt(j)) {
+                    ballCount++;
+                }
+            }
+        }
+    }
+
+    // 정답 확인하기 - 스트라이크
+    public static void calculateStrikeCount(String answer) {
+        strikeCount = 0;
+
+        for (int i = 0; i < 3; i++) {
+            if (answer.charAt(i) == target.charAt(i)) {
+                strikeCount++;
+            }
+        }
+    }
+
+    // 조건 확인
+    public static Boolean isThreeStrikes() {
+        return strikeCount == 3;
+    }
+
+}
+
+class Session {
+    public static void startGame() {
+        while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
+            String answer = Console.readLine();
+
+            Validation.checkAnswer(answer);
+            Defender.printScore(answer);
+
+            if (endGame(answer)) {
+                break;
+            }
+        }
+    }
+
+    // 프로그램 종료하기
+    public static Boolean endGame(String answer) {
+        if (Defender.isThreeStrikes()) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String temp2 = Console.readLine();
+            // 1. 빈 입력값
+            if (temp2.isEmpty()) {
+                throw new IllegalArgumentException("1 또는 2만 입력해주세요.[빈 입력값]");
+            }
+            // 2. 문자열 입력
+            int reset;
+            try {
+                reset = Integer.parseInt(temp2);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("1 또는 2만 입력해주세요.[문자열 입력]");
+            }
+            // 3. 1 또는 2가 아닌 수 입력
+            if (reset != 1 && reset != 2) {
+                throw new IllegalArgumentException("1 또는 2만 입력해주세요.[다른 수 입력]");
+            }
+            if (reset == 1) {
+                Defender.reroll();
+            }
+            if (reset == 2) {
+                System.out.println("프로그램을 종료합니다.");
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
 class Validation {
     private static final String answerErrorMessage = "각 자리 수가 1~9이며, 서로 중복되지 않는 3자리 수를 입력해주세요.";
     private static final String resetErrorMessage = "1 또는 2로만 입력해주세요.";
@@ -61,102 +184,14 @@ class Validation {
 }
 
 public class Application {
-    static StringBuilder target = new StringBuilder();
-
-    // 정답(3자리 숫자) 생성
-    public static void reroll() {
-        target.setLength(0);
-        List<Integer> temp = new ArrayList<>();
-        while (temp.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!temp.contains(randomNumber)) {
-                temp.add(randomNumber);
-            }
-        }
-        for (int i = 0; i < 3; i++) {
-            target.append(temp.get(i));
-        }
-//        System.out.println(target); // 테스트용
-    }
-
-    public static void startGameSession() {
-        while (true) {
-            System.out.print("숫자를 입력해주세요 : ");
-            String answer = Console.readLine();
-
-            Validation.checkAnswer(answer);
-
-            // 정답 확인하기 - 볼
-            int ballCount = 0;
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    if (i != j && answer.charAt(i) == target.charAt(j)) {
-                        ballCount++;
-                    }
-                }
-            }
-            // 정답 확인하기 - 스트라이크
-            int strCount = 0;
-            for (int i = 0; i < 3; i++) {
-                if (answer.charAt(i) == target.charAt(i)) {
-                    strCount++;
-                }
-            }
-            // 정답 확인하기 - 볼, 스트라이크, 낫싱 출력하기
-            if (ballCount != 0 && strCount == 0) {
-                System.out.printf("%d볼 ", ballCount);
-                System.out.println();
-            }
-            if (ballCount == 0 && strCount != 0) {
-                System.out.printf("%d스트라이크", strCount);
-                System.out.println();
-            }
-            if (ballCount != 0 && strCount != 0) {
-                System.out.printf("%d볼 ", ballCount);
-                System.out.printf("%d스트라이크", strCount);
-                System.out.println();
-            }
-            if (ballCount == 0 && strCount == 0) {
-                System.out.println("낫싱");
-            }
-            // 프로그램 종료하기
-            if (strCount == 3) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                String temp2 = Console.readLine();
-                // 1. 빈 입력값
-                if (temp2.isEmpty()) {
-                    throw new IllegalArgumentException("1 또는 2만 입력해주세요.[빈 입력값]");
-                }
-                // 2. 문자열 입력
-                int reset;
-                try {
-                    reset = Integer.parseInt(temp2);
-                } catch (NumberFormatException e) {
-                    throw new IllegalArgumentException("1 또는 2만 입력해주세요.[문자열 입력]");
-                }
-                // 3. 1 또는 2가 아닌 수 입력
-                if (reset != 1 && reset != 2) {
-                    throw new IllegalArgumentException("1 또는 2만 입력해주세요.[다른 수 입력]");
-                }
-                if (reset == 1) {
-                    reroll();
-                }
-                if (reset == 2) {
-                    System.out.println("프로그램을 종료합니다.");
-                    break;
-                }
-            }
-        }
-    }
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        reroll();
+        Defender.reroll();
 
         // 게임 시작, 입력 받기
         System.out.println("숫자 야구 게임을 시작합니다.");
-        startGameSession();
+        Session.startGame();
 
     }
 }
