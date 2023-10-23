4e39effb7ecdd7c6c8a33a3108a0e28106a36c4f
sosojean <thwlsdl0989@gmail.com>
2023-10-23 14:38:04 +0900

    3스트라이크 시 재시작을 위한 기능 구현

--- src/main/java/baseball/BaseballFunction.java
+++ src/main/java/baseball/BaseballFunction.java
@@ -69,4 +69,19 @@ public class BaseballFunction {
             return;
         }
     }
+
+    public boolean threeStrike(){
+
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        int input = Integer.parseInt(Console.readLine());
+
+        if (input == 1) return true;
+        if (input == 2) return false;
+
+        throw new IllegalArgumentException();
+
+    }
+
 }
