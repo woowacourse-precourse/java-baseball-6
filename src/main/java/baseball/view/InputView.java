e3d6ed0289c5c65eabfdca965e859a18b29816e9
olsohee <dlthgml0108@daum.net>
2023-10-23 15:30:47 +0900

    feat: InputView 생성

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,19 @@
+package baseball.view;
+
+import baseball.util.Util;
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static boolean askExit() {
+
+        String userInput = Console.readLine();
+        Util.validateExitCode(userInput);
+
+        if(userInput.equals("2")) {
+            return true;
+        }
+
+        return false;
+    }
+}
