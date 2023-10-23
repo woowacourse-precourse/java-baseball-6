06c57b0ce2cb497550f8fc2a317ddb07fa496b49
jiwon1217 <koagjw@gmail.com>
2023-10-24 02:10:38 +0900

    :sparkles: feat: 플레이어가 입력한 수에 대한 게임 결과를 출력한다.

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,7 +1,27 @@
 package baseball.view;
 
+import baseball.model.Computer;
+
 public class OutputView {
     public static void printGameStartMessage() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void printResult(Computer computer) {
+        int ballCount = computer.getBallCount();
+        int strikeCount = computer.getStrikeCount();
+
+        if (strikeCount == 0 && ballCount == 0) {
+            System.out.println("낫싱");
+        } else if (strikeCount == 3) {
+            System.out.println("3스트라이크\n"
+                    + "3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        } else if (strikeCount == 0) {
+            System.out.println(ballCount + "볼");
+        } else if (ballCount == 0) {
+            System.out.println(strikeCount + "스트라이크");
+        } else {
+            System.out.println(ballCount + "볼" + " " + strikeCount + "스트라이크");
+        }
     }
 }
