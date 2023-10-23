81a5eb4f7b72fa8879be7297e74f32f5e9a9be36
aka-nick <bechalrang@gmail.com>
2023-10-23 15:01:17 +0900

    추가: 정수열을 입력받아서 List<Integer>로 전달하는 Input.consoleNumbers() 구현

--- src/main/java/baseball/io/Input.java
+++ src/main/java/baseball/io/Input.java
@@ -1,6 +1,10 @@
 package baseball.io;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.PatternSyntaxException;
+import java.util.stream.Collectors;
 
 public class Input {
 
@@ -8,12 +12,27 @@ public class Input {
         return Console.readLine();
     }
 
-    // TODO : 특정 타입의 인풋 메서드를 지원해보자
     public static Integer consoleNumber() {
         try {
             return Integer.parseInt(Console.readLine());
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("잘못된 입력값입니다", e);
+        }
+    }
+
+    public static List<Integer> consoleNumbers() {
+        return consoleNumbers("");
+    }
+
+    public static List<Integer> consoleNumbers(String regex) {
+        try {
+            return Arrays.stream(Console.readLine().split(regex))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        } catch (PatternSyntaxException e) {
+            throw new IllegalArgumentException("적절하지 않은 구분 정규식", e);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("잘못된 입력값입니다");
         }
     }
 
