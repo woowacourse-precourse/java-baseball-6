5cb76138ca037d1110c45cd34ff2772cb1704404
lsj5657 <lsj56575@naver.com>
2023-10-23 11:19:46 +0900

    Output 클래스에 게임종료시 출력 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -29,8 +29,7 @@ public class Application {
 
             boolean error = false;
             if (output.userWin()) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+               output.printGameEnd();
                 String continueOrEnd = Console.readLine();
                 if (continueOrEnd.length() != 1) error = true;
 
