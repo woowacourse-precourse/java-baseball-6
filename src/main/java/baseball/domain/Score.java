70d650ae398dd23750972bf04ee1d5500f2507d0
Hyoseop Song <crad_on25@naver.com>
2023-10-23 13:03:41 +0900

    feat(Score): Score 클래스 생성자 및 변수 선언

---
+++ src/main/java/baseball/domain/Score.java
@@ -0,0 +1,11 @@
+package baseball.domain;
+
+public class Score {
+    private int strike = 0;
+    private int ball = 0;
+
+    public Score(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+}
