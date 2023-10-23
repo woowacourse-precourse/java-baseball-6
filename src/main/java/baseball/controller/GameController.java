9972c6c4bd0f90eae6ec73648da90d4c23e5e51a
Ukj0ng <xb253q@gmail.com>
2023-10-23 17:50:17 +0900

    feat: Controller 플레이어의 입력값 판별 및 게임 진행 구현

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,2 +1,52 @@
-package baseball.controller;public class GameController {
+package baseball.controller;
+
+import baseball.model.Computer;
+import baseball.view.GameView;
+
+import java.util.List;
+
+public class GameController {
+    private final Computer computer;
+    private final GameView gameView;
+    private int strike;
+    private int ball;
+
+    public GameController() {
+        computer = new Computer();
+        gameView = new GameView();
+    }
+
+    public void startGame() {
+        gameView.showIntro();
+        int start = 1;
+        while(start == 1) {
+            String playInput = gameView.getPlayerInput();
+            strike = 0;
+            ball = 0;
+
+            judge(computer.getNumber(), playInput);
+            gameView.showResult(strike, ball);
+
+            if(strike == 3) {
+                start = gameView.gameContinue();
+                computer.setNumber();
+            }
+        }
+    }
+
+    private void judge(List<Integer> number, String playInput) {
+        for (int i = 0; i < playInput.length(); i++) {
+            int asdf = Character.getNumericValue(playInput.charAt(i));
+            if(isStrike(computer.getNumber(), asdf, i)) strike++;
+            else if(isBall(computer.getNumber(), asdf)) ball++;
+        }
+    }
+
+    private boolean isStrike(List<Integer> number, int input, int index) {
+        return number.get(index) == input;
+    }
+
+    private boolean isBall(List<Integer> number, int input) {
+        return number.contains(input);
+    }
 }
