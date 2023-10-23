6b0b32a665db19dde1c42edeae49acf50e0c8d40
robinjoon <robin980108@naver.com>
2023-10-23 16:19:22 +0900

    feat(게임 진행 가이드 출력) : 게임 진행 가이드 출력 기능 구현

---
+++ src/main/java/baseball/io/GameGuideOutput.java
@@ -0,0 +1,18 @@
+package baseball.io;
+
+import static java.lang.System.out;
+
+public class GameGuideOutput {
+    public static void startGame() {
+        out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void inputBaseBall() {
+        out.println("숫자를 입력해주세요 : ");
+    }
+
+    public static void gameEnd() {
+        out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+}
