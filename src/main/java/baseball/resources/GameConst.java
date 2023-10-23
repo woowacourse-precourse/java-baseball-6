4707891182afbfcf37095bf441e38479fc760ac0
leedohyun <lucky77795@naver.com>
2023-10-23 22:18:57 +0900

    refactor: 출력 메시지들과 상수들을 각각의 클래스에서 모두 관리하도록 변경

--- src/main/java/baseball/resources/GameConst.java
+++ src/main/java/baseball/resources/GameConst.java
@@ -5,4 +5,7 @@ public class GameConst {
     public static final int RANGE_START = 1; //가능한 Number 범위 시작 값
     public static final int RANGE_END = 9; //가능한 Number 범위 끝 값
     public static final int ZERO = 0;
+    public static final int ALL_STRIKE = 3;
+    public static final int WANT_RESTART = 1;
+    public static final int WANT_END = 2;
 }
