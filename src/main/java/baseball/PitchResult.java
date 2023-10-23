4d5871586e731be015f21e310c1377c522c2eb1d
임강호 <55732952+Icln@users.noreply.github.com>
2023-10-23 21:30:19 +0900

    refactor: enum 사용해서 조건문 수정

--- src/main/java/baseball/PitchResult.java
+++ src/main/java/baseball/PitchResult.java
@@ -33,7 +33,7 @@ public class PitchResult {
     }
 
     public void judgePitch(String inputString) {
-        for (int i = 0; i < 3; i++) {
+        for (int i = InputRange.START_INDEX.getValue(); i < InputRange.CORRECT_INPUT_RANGE.getValue(); i++) {
             if (randomNumber.getRandomNumber().get(i).equals(Character.getNumericValue(inputString.charAt(i)))) {
                 addStrike(strike);
             } else if (randomNumber.getRandomNumber().contains(Character.getNumericValue(inputString.charAt(i)))) {
@@ -43,29 +43,30 @@ public class PitchResult {
     }
 
     public void printPitchResult() {
-        if (strike == 0 && ball == 0) {
+        if (strike == PitchRange.MIN_STRIKES.getValue() && ball == PitchRange.MAX_BALLS.getValue()) {
             System.out.println("낫싱");
             return;
         }
 
-        if (strike == 3) {
+        if (strike == PitchRange.MAX_STRIKES.getValue()) {
             System.out.println("3스트라이크");
             System.out.println("3개의 숫자를 모두 모두 맞히셨습니다! 게임 종료");
             System.out.println("게임을 새로 시작하려면 1, 종료하시면 2를 입력하세요.");
+
             setRestartNumber(Integer.parseInt(Console.readLine()));
             validation.restartValidation(restartNumber);
-            if (restartNumber == 1) {
+            if (restartNumber == GameOption.RESTART_GAME.getOption()) {
                 randomNumber.randomNumberGenerate();
             }
             return;
         }
 
-        if (ball == 0) {
+        if (ball == PitchRange.MIN_BALLS.getValue()) {
             System.out.println(strike + "스트라이크");
             return;
         }
 
-        if (strike == 0) {
+        if (strike == PitchRange.MIN_STRIKES.getValue()) {
             System.out.println(ball + "볼");
             return;
         }
