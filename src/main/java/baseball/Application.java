8bd6254d833ea2875df4cf5d47152258f4cbbf38
IronD414 <kcy1760@gmail.com>
2023-10-23 23:07:28 +0900

    Modify Scanner to Console, Add Exception Handling

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,9 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Objects;
-import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -18,7 +18,7 @@ public class Application {
         }
         return computer;
     }
-    private static String evaluateBaseballGuess(List<Integer> answer, int guess) throws IllegalArgumentException{
+    private static String evaluateBaseballGuess(List<Integer> answer, Integer guess) throws IllegalArgumentException{
         int num_of_strikes = 0, num_of_balls = 0;
         List<Integer> guess_arr = new ArrayList<>(3);
         guess_arr.add(guess / 100);
@@ -41,41 +41,37 @@ public class Application {
         return num_of_balls + "볼" + num_of_strikes + "스트라이크";
     }
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         String evaluate_result;
-        // TODO: 숫자야구 시작 메시지 출력
+        boolean stop_flag = false;
         System.out.println("숫자 야구 게임을 시작합니다.");
-        // TODO: computer 의 임의 숫자 생성
-        List<Integer> baseball_num = createRandomBaseballNumber();
-        List<Integer> baseball_guess = new ArrayList<Integer>();
-        //System.out.println(baseball_num);
-        // TODO: while 로 exception 또는 게임 종료 시까지 guess 진행
-        while (true)
-        {
-            try
-            {
-                System.out.print("숫자를 입력해주세요 : ");
-                evaluate_result = evaluateBaseballGuess(baseball_num, scanner.nextInt());
-                System.out.println(evaluate_result);
-                if (Objects.equals(evaluate_result, "3스트라이크"))
-                {
-                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
-                            "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                    int restart_selection = scanner.nextInt();
-                    if (restart_selection == 1) /* */;
-                    else if (restart_selection == 2) break;
+        while (!stop_flag) {
+            List<Integer> baseball_num = createRandomBaseballNumber();
+            List<Integer> baseball_guess = new ArrayList<Integer>();
+            while (true) {
+                try {
+                    System.out.print("숫자를 입력해주세요 : ");
+                    evaluate_result = evaluateBaseballGuess(baseball_num, Integer.valueOf(Console.readLine()));
+                    System.out.println(evaluate_result);
+                    if (Objects.equals(evaluate_result, "3스트라이크")) {
+                        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+                                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                        String restart_selection = Console.readLine();
+                        if (Objects.equals(restart_selection, "1")) break;
+                        else if (Objects.equals(restart_selection, "2"))
+                        {
+                            stop_flag = true;
+                            break;
+                        }
+                    }
+                } catch (IllegalArgumentException e) {
+                    String exception_message = e.getMessage();
+                    System.out.println("IllegalArgumentException " + exception_message);
+                    stop_flag = true;
+                    break;
                 }
-            }
-            catch (IllegalArgumentException e)
-            {
-                break;
-            }
-            catch (Exception e)
-            {
-                break;
+                /* finally{} */
             }
         }
-
-        scanner.close();
+        Console.close();
     }
 }
