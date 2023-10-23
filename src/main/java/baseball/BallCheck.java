0551513f3ea1782fbc05e289c4a567dcbcb1247b
Sung wook Park <109124287+respectwo2@users.noreply.github.com>
2023-10-23 16:55:28 +0900

    refactor: ballcheck 클래스의 중첩루프문 리팩토링

--- src/main/java/baseball/BallCheck.java
+++ src/main/java/baseball/BallCheck.java
@@ -2,25 +2,24 @@ package baseball;
 
 public class BallCheck {
 
-	public static int check(int[] coms_ball,int[] myball) {
-		
+	public static int check(int[] coms_ball, int[] myball) {
+
 		int bcnt = 0;
-		
-		for(int i=0; i<coms_ball.length; i++) {
-			if(check(myball,coms_ball[i],i)) {
+
+		for (int i = 0; i < coms_ball.length; i++) {
+			if (check(myball, coms_ball[i], i)) {
 				bcnt++;
 			}
+		}
+		return bcnt;
 	}
-	return bcnt;
-}
 
 	private static boolean check(int[] arr, int val, int exval) {
-		for(int i = 0; i < arr.length ; i++) {
-			if(i != exval && arr[i] == val) {
+		for (int i = 0; i < arr.length; i++) {
+			if (i != exval && arr[i] == val) {
 				return true;
 			}
 		}
 		return false;
-	
 	}
 }
