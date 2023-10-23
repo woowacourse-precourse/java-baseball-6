90eff4b53922876da6d9b6fc1235f8b4a825f571
h3136514 <3136514@naver.com>
2023-10-23 17:04:13 +0900

    feat(computer): 게임 재시작 / 프로그램 종료 출력 함수를 생성한다.

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -160,4 +160,8 @@ public class BaseBallGame {
     public void WinPrint(){
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
+    //게임 재시작 / 프로그램 종료 출력 함수
+    public void RestartPrint(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
