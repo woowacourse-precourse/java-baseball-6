57e737912b788b9c26e6be6efdd5ee5f1b011d5c
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-10-23 17:35:56 +0900

    feat: 게임 재시작 여부 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -17,6 +17,10 @@ public class Application {
     }
 
     private static boolean isRestart() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        Integer input = Integer.parseInt(Console.readLine());
+        if (input == 1)
+            return true;
         return false;
     }
 
