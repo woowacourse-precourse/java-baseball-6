84098c7862e7114582e07f551553a015b98144a4
zaeuk <zzaeuk@gmail.com>
2023-10-23 23:04:47 +0900

    feat: printGameResult 메서드 구현 ball의 개수와 strike의 개수를 입력받아 게임의 결과를 출력 ball의 개수와 strike의 개수가 모두 0일경우 낫싱 출력 그 이외에 경우 볼과 스트라이크의 개수 표시하여 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -28,6 +28,19 @@ public class Application {
             List<Integer> user = getUserNumber();
             ball = countBall(computer, user);
             strike = countStrike(computer, user);
+            printGameResult(ball, strike);
+        }
+    }
+
+    private static void printGameResult(int ball, int strike) {
+        if (ball == 0 && strike == 0) {
+            System.out.println("낫싱");
+        } else if (ball == 0) {
+            System.out.printf("%d스트라이크\n", strike);
+        } else if (strike == 0) {
+            System.out.printf("%d볼\n", ball);
+        } else {
+            System.out.printf("%d볼 %d스트라이크\n", ball, strike);
         }
     }
 
