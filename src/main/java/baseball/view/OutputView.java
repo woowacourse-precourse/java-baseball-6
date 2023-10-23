0a872a0907fbc1711f1e55e9e8129048f03e106e
choheejin <hjcho9510@gmail.com>
2023-10-23 17:29:05 +0900

    refactor: change conditions in displayScoreboard method

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -7,20 +7,23 @@ public class OutputView {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    // TODO: 더 깔끔한 코드 고민해보기
     public void displayScoreboard(HumanModel humanModel) {
         int strike = humanModel.getStrike();
         int ball = humanModel.getBall();
 
         if (humanModel.getNothing() != 0) {
             System.out.println("낫싱");
-        } else if (strike > 0 && ball > 0) {
-            System.out.println(ball + "볼 " + strike + "스트라이크");
-        } else if (strike > 0) {
-            System.out.println(strike + "스트라이크");
-        } else {
-            System.out.println(ball + "볼");
+            return;
         }
+        if (strike == 0) {
+            System.out.println(ball + "볼");
+            return;
+        }
+        if (ball == 0) {
+            System.out.println(strike + "스트라이크");
+            return;
+        }
+        System.out.println(ball + "볼 " + strike + "스트라이크");
     }
 
     public void displayGameExit() {
