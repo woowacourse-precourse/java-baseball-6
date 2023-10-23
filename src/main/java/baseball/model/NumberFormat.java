ba3cbad1f1ba5564ed86a8e88fd2eec0c12cbef8
dmong <dt2980_hee@naver.com>
2023-10-23 11:52:26 +0900

    feat: 랜덤 3자리 수 생성 기능

--- src/main/java/baseball/model/NumberFormat.java
+++ src/main/java/baseball/model/NumberFormat.java
@@ -9,5 +9,17 @@ public class NumberFormat {
      public static final int DIGIT_CONSTRAINT = 3;
 
      private List<Integer> numberList;
-     
+
+     protected NumberFormat(){
+          this.numberList = new ArrayList<>();
+     }
+
+     protected NumberFormat(List<Integer> numberList){
+          this.numberList = numberList;
+     }
+
+     public static NumberFormat generateRandomNumber(){
+          List<Integer> randomNumberList = Randoms.pickUniqueNumbersInRange(1, 9, 3);
+          return new NumberFormat(randomNumberList);
+     }
 }
