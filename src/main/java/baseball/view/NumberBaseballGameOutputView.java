9d3b028c9a106131117cbca5d901be65734bdde7
wnsgh12s <junho1273@gmail.com>
2023-10-23 19:57:18 +0900

    Feat: 숫자를 입력해주세요 메시지 출력 기능추가

--- src/main/java/baseball/view/NumberBaseballGameOutputView.java
+++ src/main/java/baseball/view/NumberBaseballGameOutputView.java
@@ -2,9 +2,13 @@ package baseball.view;
 
 public class NumberBaseballGameOutputView {
     static final String START_GAME_MESSAGE = " 게임을 시작합니다.";
+    static final String NUMBER_INPUT_REQUEST = "숫자를 입력해주세요.";
 
     void printStartGameMessage(String gameName) {
         System.out.println(gameName + START_GAME_MESSAGE);
     }
 
+    void printNumberInputRequest() {
+        System.out.println(NUMBER_INPUT_REQUEST);
+    }
 }
