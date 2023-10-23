772db57e08b6afc1664dd108f4b8209fa9a9156a
flown222 <jangu3384@gmail.com>
2023-10-23 19:21:27 +0900

    feat: add Balls

--- src/main/java/baseball/model/GameResult.java
+++ src/main/java/baseball/model/GameResult.java
@@ -1,8 +1,11 @@
 package baseball.model;
 
 public class GameResult {
-    private final Integer ballCount;
-    private final Integer strikeCount;
+    private int ballCount;
+    private int strikeCount;
+
+    public GameResult() {
+    }
 
     public GameResult(Integer ballCount, Integer strikeCount) {
         this.ballCount = ballCount;
@@ -17,7 +20,20 @@ public class GameResult {
         return strikeCount;
     }
 
+    public boolean isNothing() {
+        return ballCount == 0 && strikeCount == 0;
+    }
+
     public boolean isAnswer(int gameSize) {
         return strikeCount == gameSize;
+    }
+
+    public void updateResult(BallStatus ballStatus) {
+        if (ballStatus.equals(BallStatus.BALL)) {
+            ballCount += 1;
+        }
+        if (ballStatus.equals(BallStatus.STRIKE)) {
+            strikeCount += 1;
+        }
     }
 }
