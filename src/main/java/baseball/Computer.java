3148685325b74d78242da98f65d6157bd36ba8e5
YoonseolLee <gujueng32@naver.com>
2023-10-23 21:33:38 +0900

    feat: 컴퓨터 난수 생성

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,4 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Computer {
+    public static final int COUNTS = 3;
+    public static final int MAXIMUM_NUMBER = 9;
+    public static final int MINIMUM_NUMBER = 1;
+
+    private List<Integer> randomNumbers;
+
+    private void generateRandomNumbers() {
+        randomNumbers = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+
+        while (set.size() < COUNTS) {
+            set.add(Randoms.pickNumberInRange(MINIMUM_NUMBER, MAXIMUM_NUMBER));
+        }
+        randomNumbers.addAll(set);
+    }
+
 }
