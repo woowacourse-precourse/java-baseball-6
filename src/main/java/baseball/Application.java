37aee82fd59fa0e97f6d903873732c09b04990c3
김민정 <mjeong@sookmyung.ac.kr>
2023-10-23 11:36:19 +0900

    feat: 결과 계산 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,12 +2,17 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         Application NumberBaseball = new Application();
-        System.out.println(NumberBaseball.generateRandomNumber());
+        List<Integer> computer = NumberBaseball.generateRandomNumber();
+        List<Integer> player = NumberBaseball.generateRandomNumber();
+        System.out.println(computer);
+        System.out.println(player);
+        System.out.println(Arrays.toString(NumberBaseball.calculateResult(computer, player)));
     }
 
     public List<Integer> generateRandomNumber() {
@@ -19,5 +24,17 @@ public class Application {
             }
         }
         return computer;
+    }
+
+    public int[] calculateResult(List<Integer> computer, List<Integer> player) {
+        int[] result = new int[2]; //[strike, ball]
+        for (int i = 0; i < player.size(); i++) {
+            if (player.get(i).equals(computer.get(i))) {
+                result[0]++;
+            } else if (computer.contains(player.get(i))) {
+                result[1]++;
+            }
+        }
+        return result;
     }
 }
