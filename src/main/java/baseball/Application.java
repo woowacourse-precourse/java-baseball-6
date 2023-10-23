148dad66a70e1553f7eca1cd388bbe071068d496
임강호 <55732952+Icln@users.noreply.github.com>
2023-10-23 23:02:07 +0900

    refactor: 재시작 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,19 +12,27 @@ public class Application {
             while (true) {
                 System.out.printf("숫자를 입력하세요 : ");
                 String inputString = Console.readLine();
+                InputRange.inputValidation(inputString);
 
-                Validation validation = new Validation();
-                validation.inputValidation(inputString);
-
-                PitchResult pitchResult = new PitchResult(randomNumber, validation);
+                PitchResult pitchResult = new PitchResult(randomNumber);
                 pitchResult.judgePitch(inputString);
                 pitchResult.printPitchResult();
-                if (pitchResult.getRestartNumber() == GameOption.QUIT_GAME.getOption()) {
-                    break;
+
+                if (pitchResult.getStrike() == PitchRange.MAX_STRIKES.getValue()) {
+                    int gameOption = Integer.parseInt(Console.readLine());
+                    GameOption.gameOptionValidation(gameOption);
+                    if (gameOption == GameOption.RESTART_GAME.getOption()) {
+                        randomNumber.randomNumberGenerate();
+                    }
+                    if (gameOption == GameOption.QUIT_GAME.getOption()) {
+                        break;
+                    }
                 }
             }
         } catch (IllegalArgumentException e) {
             System.out.println("입력이 잘못되었습니다.");
+            throw e;
         }
     }
 }
+
