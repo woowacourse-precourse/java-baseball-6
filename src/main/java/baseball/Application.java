599efdd85356bd749c5f710cec629a7aee0b3c02
Kite_Z <duswl339@ajou.ac.kr>
2023-10-24 02:13:10 +0900

    Feat: Add Game Class

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,66 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
-        // 프로그램 구현
-
-        // Start Game
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        int flag = 1;
-
-        // Generate Computer Number
-        StringBuilder computerNumber = new StringBuilder();
-        for(int i = 0; i < 3 ; i++) {
-            computerNumber.append(Randoms.pickNumberInRange(1, 9));
-        }
-
-        while (flag == 1) {
-            // System.out.println(computerNumber);
-
-            System.out.print("숫자를 입력해주세요 : ");
-            StringBuilder userNumber = new StringBuilder(Console.readLine());
-            System.out.println(userNumber);
-
-            // Compare User Number and Computer Baseball Number
-            /* scoreBoard [0]: ball 수 저장
-             *  scoreBoard [1]: strike 수 저장
-             *  */
-            int[] scoreBoard = {0, 0};
-
-            for(int i = 0; i < 3 ; i++) {
-                String userSubNum = userNumber.substring(i , i+1);
-                if(computerNumber.indexOf(userSubNum) > -1) {
-                    if(computerNumber.substring(i, i+1).equals(userSubNum)) {
-                        scoreBoard[1]++;
-                    } else {
-                        scoreBoard[0]++;
-                    }
-                }
-            }
-
-            // Print Result
-            if(scoreBoard[1] == 3) {
-                System.out.println("3스트라이크");
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-
-                // Re-game ?
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력해주세요.");
-                flag = Integer.parseInt(Console.readLine());
-
-                if(flag == 1) {
-                    computerNumber = new StringBuilder();
-                    for(int i = 0; i < 3 ; i++) {
-                        computerNumber.append(Randoms.pickNumberInRange(1, 9));
-                    }
-                }
-            } else if(scoreBoard[0] + scoreBoard[1] == 0) {
-                System.out.println("낫싱");
-            } else if(scoreBoard[0] > 0 && scoreBoard[1] > 0) {
-                System.out.println(scoreBoard[0]+"볼 "+scoreBoard[1]+"스트라이크");
-            } else if(scoreBoard[0] > 0) {
-                System.out.println(scoreBoard[0]+"볼");
-            } else {
-                System.out.println(scoreBoard[1]+"스트라이크");
-            }
-        }
+        Game baseballGame = new Game();
+        baseballGame.start();
     }
 }
---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,86 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Game {
+    private int gameFlag;
+    private StringBuilder computerNumber;
+    private StringBuilder userNumber;
+
+    int[] scoreBoard = {0, 0};
+    Game() {
+
+    }
+
+    public void start() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        this.gameFlag = 1;
+        this.generateComputerNumber();
+
+        while(this.gameFlag == 1) {
+            this.inputUserNumber();
+            this.updateScoreBoard();
+            this.printGameResult();
+        }
+    }
+
+    // Generate Computer Number
+    private void generateComputerNumber() {
+        StringBuilder computerNumber = new StringBuilder();
+        for(int i = 0; i < 3 ; i++) {
+            computerNumber.append(Randoms.pickNumberInRange(1, 9));
+        }
+        this.computerNumber = computerNumber;
+    }
+
+    // Input User Baseball Number
+    private void inputUserNumber() {
+        this.scoreBoard = new int[]{0, 0};
+        System.out.print("숫자를 입력해주세요 : ");
+        StringBuilder userNumber = new StringBuilder(Console.readLine());
+        System.out.println(userNumber);
+        this.userNumber = userNumber;
+    }
+
+    private void updateScoreBoard() {
+        for(int i = 0; i < 3 ; i++) {
+            String userSubNum = this.userNumber.substring(i , i+1);
+            if(this.computerNumber.indexOf(userSubNum) > -1) {
+                if(this.computerNumber.substring(i, i+1).equals(userSubNum)) {
+                    this.scoreBoard[1]++;
+                } else {
+                    this.scoreBoard[0]++;
+                }
+            }
+        }
+    }
+
+    private void printGameResult() {
+        if(this.scoreBoard[1] == 3) {
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
+            // updateGameFlag
+            this.updateGameFlag();
+
+            if(this.gameFlag == 1) {
+                // generateComputerNumber();
+                this.generateComputerNumber();
+            }
+        } else if(this.scoreBoard[0] + this.scoreBoard[1] == 0) {
+            System.out.println("낫싱");
+        } else if(this.scoreBoard[0] > 0 && this.scoreBoard[1] > 0) {
+            System.out.println(this.scoreBoard[0]+"볼 "+this.scoreBoard[1]+"스트라이크");
+        } else if(this.scoreBoard[0] > 0) {
+            System.out.println(this.scoreBoard[0]+"볼");
+        } else {
+            System.out.println(this.scoreBoard[1]+"스트라이크");
+        }
+    }
+
+    private void updateGameFlag() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력해주세요.");
+        this.gameFlag = Integer.parseInt(Console.readLine());
+    }
+}
