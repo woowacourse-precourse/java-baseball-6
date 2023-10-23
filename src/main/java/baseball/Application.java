177ab03dbf8f4113c6660dd3929e769aaec0ee68
Seungjida <esg0107@pusan.ac.kr>
2023-10-23 22:20:19 +0900

    Feat: "숫자 야구 게임을 실행하는 main 코드 추가"

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,34 @@
 package baseball;
 
+import static others.ExceptionHandling.digitsExceptionTesting;
+import static others.Others.getNumber;
+import static others.Others.ifRestart;
+import static others.Others.printGameStart;
+import static others.Value.compareDigits;
+import static others.Value.computerGenerateNum;
+import static others.Value.playerGenerateNum;
+import static others.Value.printResult;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        printGameStart();
+        boolean ifGameContinue = true;
+        while (ifGameContinue) {
+            List<Integer> computerNum = computerGenerateNum();
+            boolean guess = false;
+            while (!guess) {
+                String getStrNumber = getNumber();
+                digitsExceptionTesting(getStrNumber);
+                List<Integer> playerNum = playerGenerateNum(getStrNumber);
+                compareDigits(computerNum, playerNum);
+                guess = printResult();
+                if (guess) {
+                    ifGameContinue = ifRestart();
+                }
+            }
+        }
     }
 }
