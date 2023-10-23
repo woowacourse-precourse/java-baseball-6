f98d6cb8e340243cf1e98e7f0292d1bdd2a8af56
sinminseok12 <sinminseok12@naver.com>
2023-10-24 01:29:16 +0900

    feat : 스트라이크 개수를 계산하는 기능 구현

--- src/main/java/baseball/Result.java
+++ src/main/java/baseball/Result.java
@@ -1,4 +1,20 @@
 package baseball;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Result {
+    private Integer strike;
+    private Integer ball;
+
+    public Result(List<Integer> computerNumbers, List<Integer> userNumbers) {
+        this.strike = checkStrike(computerNumbers, userNumbers);
+        this.ball = ball;
+    }
+
+    private Integer checkStrike(List<Integer> userNumbers, List<Integer> computerNumbers) {
+        return (int) IntStream.range(0, userNumbers.size())
+                .filter(i -> userNumbers.get(i).equals(computerNumbers.get(i)))
+                .count();
+    }
 }
