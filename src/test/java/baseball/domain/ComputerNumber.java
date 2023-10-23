7ace81958d677eaaa1a0f662c48195123b25dae9
ts3083 <ts04031@gmail.com>
2023-10-23 16:51:12 +0900

    feat: PlayerNumber, ComputerNumber 도메인 생성

---
+++ src/test/java/baseball/domain/ComputerNumber.java
@@ -0,0 +1,12 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class ComputerNumber {
+
+    private List<Integer> number;
+
+    public void setNumber(List<Integer> number) {
+        this.number = number;
+    }
+}
