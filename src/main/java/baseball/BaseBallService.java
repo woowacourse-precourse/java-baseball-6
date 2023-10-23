b2c362856a6cb4817e16cfa3d69e3d5db7e5f188
wooyong99 <wulovesk@naver.com>
2023-10-23 10:41:59 +0900

    [feat] : 스트라이크, 볼 확인 기능 구현

--- src/main/java/baseball/BaseBallService.java
+++ src/main/java/baseball/BaseBallService.java
@@ -9,15 +9,27 @@ import java.util.List;
 public class BaseBallService {
   private final String START_GAME_MESSAGE = "숫자 야구 게임을 시작합니다";
   private final String ENTER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+  private int strike_cnt = 0;
+  private int ball_cnt = 0;
   List<Integer> com = new ArrayList<>();
   public void exec() {
     com = createComNums();
     System.out.println(START_GAME_MESSAGE);
+    com.stream().forEach(System.out::println);
     do{
       System.out.printf(ENTER_INPUT_MESSAGE);
       List<Integer> user = createUserNums();
-      user.stream().forEach(System.out::println);
-    }while(true);
+      strike_cnt = 0;
+      ball_cnt = 0;
+      for(int i = 0; i < com.size(); i++) {
+        if(user.contains(com.get(i)) && user.get(i) == com.get(i)){
+          strike_cnt++;
+        } else if (user.contains(com.get(i)) && user.get(i) != com.get(i)) {
+          ball_cnt++;
+        }
+      }
+      System.out.println(String.format("%d스트라이크 %d볼", strike_cnt, ball_cnt));
+    }while(strike_cnt != 3);
   }
   List<Integer> createComNums(){
     return Randoms.pickUniqueNumbersInRange(1,9,3);
