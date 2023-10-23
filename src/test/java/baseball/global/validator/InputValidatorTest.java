068e34de09a81ab2e0a4267585746004e25ddf39
yshong1998 <yshong1998@gmail.com>
2023-10-24 03:03:50 +0900

    Test(InputValidator): 테스트 네이밍 변경

--- src/test/java/baseball/global/validator/InputValidatorTest.java
+++ src/test/java/baseball/global/validator/InputValidatorTest.java
@@ -15,7 +15,7 @@ class InputValidatorTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"12a", "1a2", "a12"})
-    @DisplayName("입력 값이 숫자인지 테스트")
+    @DisplayName("숫자 이외의 값 입력에 대한 예외 처리 테스트")
     void validateInputNumericTest(String playerInput) {
         assertThrows(IllegalArgumentException.class,
                 () -> validator.validateBaseballNumberInput(playerInput),"숫자 이외에는 입력할 수 없습니다.");
