46c30d511d576c26ee611e4dcd266bc3f08a3d97
kangaroo0 <ljw42871@gmail.com>
2023-10-23 17:33:02 +0900

    Feat: 맨 처음 시작할 때 메시지 띄우기 - InputView의 getStartMessage에서 처리

---
+++ src/main/java/baseball/service/BaseballService.java
@@ -0,0 +1,26 @@
+package baseball.service;
+
+import baseball.util.ComputerBallGenerator;
+import baseball.view.InputView;
+import camp.nextstep.edu.missionutils.Console;
+
+public class BaseballService {
+
+    public static void gameStart(){
+
+        InputView.getStartMessage();
+        
+    }
+    private static void gamePlay(){
+
+
+
+    }
+
+    private static void gameEnd(){
+
+
+
+    }
+
+}
---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,17 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String USER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+
+
+    public static void getStartMessage(){
+        System.out.println(START_MESSAGE);
+    }
+
+
+
+}
