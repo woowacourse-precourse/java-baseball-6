285eb02f04378b53012434e96e534a25aea8d338
mainfn <_@mainfn.dev>
2023-10-23 22:47:53 +0900

    refact: remove unused field

--- src/main/java/baseball/domain/ball/GameStatus.java
+++ src/main/java/baseball/domain/ball/GameStatus.java
@@ -5,10 +5,7 @@ public enum GameStatus {
     REPLAY("1"),
     EXIT("2");
 
-    private final String input;
-
     GameStatus(final String input) {
-        this.input = input;
     }
 
     public static GameStatus of(final String input) {
