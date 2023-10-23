322741bde303d70663b3803071446cdb04f9cabb
GS_song <20003204@sju.ac.kr>
2023-10-23 13:50:54 +0900

    refactor: InputValidation에서 예측값의 유효성을 검사하는 로직을 하나로 묶어서 표현

--- src/main/java/baseball/io/InputHandler.java
+++ src/main/java/baseball/io/InputHandler.java
@@ -34,7 +34,7 @@ public class InputHandler {
             input /= 10;
         }
         // 3가지 예외처리를 담당 1.예측값 충복체크 2.(1-9)범위 체크 3.3개의 숫자인지 체크
-        if(!isSizeThree(expectedNums) || !isInRange(expectedNums) || isDuplicate(expectedNums)){
+        if(!validExpectedNums(expectedNums)){
             throw new IllegalArgumentException();
         }
 
--- src/main/java/baseball/validation/InputValidation.java
+++ src/main/java/baseball/validation/InputValidation.java
@@ -8,23 +8,27 @@ import static baseball.constant.Constants.*;
 
 public final class InputValidation {
 
-    public static Boolean isSizeThree(List<Integer> target) {
+    public static Boolean validExpectedNums(List<Integer> expectedNums){
+        return isSizeThree(expectedNums) && isInRange(expectedNums) && !isDuplicate(expectedNums);
+    }
+
+    public static Boolean isRestartInput (int input) {
+        return input == INPUT_RESTART || input == INPUT_STOP;
+    }
+
+    private static Boolean isSizeThree(List<Integer> target) {
         return target.size() == INPUT_MAX_SIZE;
     }
 
-    public static Boolean isInRange(List<Integer> target) {
+    private static Boolean isInRange(List<Integer> target) {
         for (int t : target) {
             if (t < INPUT_NUM_MIN || t > INPUT_NUM_MAX) return false;
         }
         return true;
     }
 
-    public static Boolean isDuplicate(List<Integer> target) {
+    private static Boolean isDuplicate(List<Integer> target) {
         Set<Integer> targetSet = target.stream().collect(Collectors.toSet());
         return targetSet.size() != INPUT_MAX_SIZE;
-    }
-
-    public static Boolean isRestartInput (int input) {
-        return input == INPUT_RESTART || input == INPUT_STOP;
     }
 }
