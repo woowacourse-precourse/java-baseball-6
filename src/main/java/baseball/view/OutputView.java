85d83501cd03b1dd7db44e76016865c964584cb5
khsrla9806 <kimhunsope@naver.com>
2023-10-23 11:17:56 +0900

    feat: 볼, 스트라이크 개수를 받아 결과 메시지를 출력하는 메서드 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -9,4 +9,21 @@ public class OutputView {
         System.out.print("숫자를 입력해주세요 : ");
     }
 
+    public static void printResultMessage(int ballCount, int strikeCount) {
+
+        if (ballCount == 0 && strikeCount == 0) {
+            System.out.println("낫싱");
+            return;
+        }
+
+        StringBuilder builder = new StringBuilder();
+        if (ballCount != 0) {
+            builder.append(ballCount).append("볼 ");
+        }
+        if (strikeCount != 0) {
+            builder.append(strikeCount).append("스트라이크");
+        }
+        System.out.println(builder);
+    }
+
 }
