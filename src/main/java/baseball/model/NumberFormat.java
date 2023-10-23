14446c8bb275639c46797a167fede4e9204f9d2f
dmong <dt2980_hee@naver.com>
2023-10-23 14:48:02 +0900

    fix: 랜덤 숫자 생성 로직 변경

--- src/main/java/baseball/model/NumberFormat.java
+++ src/main/java/baseball/model/NumberFormat.java
@@ -19,7 +19,13 @@ public class NumberFormat {
      }
 
      public static NumberFormat generateRandomNumber(){
-          List<Integer> randomNumberList = Randoms.pickUniqueNumbersInRange(1, 9, 3);
+//          List<Integer> randomNumberList = Randoms.pickUniqueNumbersInRange(1, 9, 3);
+          List<Integer> randomNumberList = new ArrayList<>();
+          while (randomNumberList.size() < DIGIT_CONSTRAINT){
+               int randomNumber = Randoms.pickNumberInRange(1, 9);
+               if (!randomNumberList.contains(randomNumber))
+                    randomNumberList.add(randomNumber);
+          }
           return new NumberFormat(randomNumberList);
      }
 
