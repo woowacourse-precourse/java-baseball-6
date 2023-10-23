62277fba219da005ea97e793594018052acbd96e
rlfrkdms1 <rlfrkdms1@naver.com>
2023-10-23 22:02:16 +0900

    feat: 서로 다른 3자리 수를 생성해 저장하는 기능 추가
    숫자 야구 게임의 정답 생성 기능 중 일부
    정답을 생성해서 반환

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,15 +1,25 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Randoms;현
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
 
+    private List<Integer> correctAnswer = new ArrayList<>();
+
     private static final int NUMBER_START_RANGE = 1;
     private static final int NUMBER_END_RANGE = 9;
+    private static final int ANSWER_DIGIT = 3;
 
     public List<Integer> createCorrectAnswer() {
-        Randoms.pickNumberInRange(NUMBER_START_RANGE, NUMBER_END_RANGE);
+        while (correctAnswer.size() < ANSWER_DIGIT) {
+            int pickedNumberInRange = Randoms.pickNumberInRange(NUMBER_START_RANGE, NUMBER_END_RANGE);
+            if (!correctAnswer.contains(pickedNumberInRange)) {
+                correctAnswer.add(pickedNumberInRange);
+            }
+        }
+        return correctAnswer;
     }
 
 
