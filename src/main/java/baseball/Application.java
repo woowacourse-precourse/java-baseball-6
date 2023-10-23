8b4e133f9c7086a46e1360f43f604974f151199b
최진원 <etudent39@gmail.com>
2023-10-23 23:53:51 +0900

    서로 다른 세 자리수 입력받는 기능 구현: 수정예정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,31 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = Console.readLine();
+        if(input.length() != 3){
+            System.exit(0);
+        }
+        List<Integer> userNumberList = new ArrayList<Integer>();
+        Set<Integer> userNumberSet = new HashSet<Integer>();
+        for(int i = 0; i < input.length(); i++){
+            if(!('1' <= input.charAt(i) && input.charAt(i) <= '9')){
+                System.exit(0);
+            }
+            else{
+                userNumberList.add((int) input.charAt(i));
+                userNumberSet.add((int) input.charAt(i));
+            }
+        }
+        if(userNumberSet.size() != userNumberList.size()){
+            System.exit(0);
+        }
     }
 }
