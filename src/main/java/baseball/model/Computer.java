3d45ee8c9d878e46de64fdc88927117c2df3f051
jiwon1217 <koagjw@gmail.com>
2023-10-24 02:10:09 +0900

    :sparkles: feat: 플레이어가 입력한 수에 대한 게임 결과를 판단한다.

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 public class Computer {
@@ -11,6 +12,21 @@ public class Computer {
     private static final int LOWER_INCLUSIVE = 1;
     private static final int UPPER_INCLUSIVE = 9;
     private List<Integer> numbers;
+    private int ballCount;
+    private int strikeCount;
+
+    public void resetCount() {
+        ballCount = 0;
+        strikeCount = 0;
+    }
+
+    public int getBallCount() {
+        return ballCount;
+    }
+
+    public int getStrikeCount() {
+        return strikeCount;
+    }
 
     public void generateRandomNumbers() {
         Set<Integer> randomNumbers = new HashSet<>();
@@ -20,5 +36,25 @@ public class Computer {
             randomNumbers.add(randomNumber);
         }
         numbers = new ArrayList<>(randomNumbers);
+    }
+
+    public void countScore(List<Integer> playerNumbers) {
+        for (int index = 0; index < 3; index++) {
+            Integer playerNumber = playerNumbers.get(index);
+
+            if (isStrike(index, playerNumber)) {
+                strikeCount++;
+            } else if (isBall(playerNumber)) {
+                ballCount++;
+            }
+        }
+    }
+
+    private boolean isBall(Integer playerNumber) {
+        return numbers.contains(playerNumber);
+    }
+
+    private boolean isStrike(Integer index, Integer playerNumber) {
+        return Objects.equals(numbers.get(index), playerNumber);
     }
 }
