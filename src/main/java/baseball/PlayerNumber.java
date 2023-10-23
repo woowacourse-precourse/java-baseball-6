47621963e2611a9f28b5c412c9bae378feeb6a92
junu8529 <cairo8529@gmail.com>
2023-10-24 01:10:14 +0900

    feat: 플레이어 숫자 생성 기능 구현

---
+++ src/main/java/baseball/PlayerNumber.java
@@ -0,0 +1,27 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayerNumber {
+
+    public List<Integer> getPlayer() {
+        String inputNumber = Console.readLine();
+        List<Integer> player = new ArrayList<>();
+
+        exception_ExceedLength(inputNumber);
+
+        for(int i=0;i<3;i++) {
+            player.add(Integer.parseInt(inputNumber.substring(i,i+1)));
+        }
+        return player;
+    }
+
+    public void exception_ExceedLength(String inputNumber) {
+        if (inputNumber.length() != 3) {
+            throw new IllegalArgumentException("세자리 수보다 크거나 작음");
+        }
+    }
+
+}
