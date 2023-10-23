1faef767e539b1e54ac24edf054329ae682b0a98
aka-nick <bechalrang@gmail.com>
2023-10-23 15:16:04 +0900

    리팩토링: BaseballNumbersGenerator에 개선된 Input메서드 적용 및 불필요해진 유효성검증 로직 제거

--- src/main/java/baseball/object/number/generator/BaseballNumbersGenerator.java
+++ src/main/java/baseball/object/number/generator/BaseballNumbersGenerator.java
@@ -9,24 +9,19 @@ public class BaseballNumbersGenerator implements NumbersGenerator {
     @Override
     public List<Integer> generate() {
         Output.console("숫자를 입력해주세요 : ");
-        String inputNumbersString = Input.consoleLine();
 
-        if (inputNumbersString.length() != MAX_SIZE) {
+        List<Integer> numbers = Input.consoleNumbers();
+        if (numbers.size() != MAX_SIZE) {
             throw new IllegalArgumentException("야구숫자는 세 자리여야 합니다.");
         }
-        if (inputNumbersString.replaceAll("[0-9]", "").length() != 0) {
-            throw new IllegalArgumentException("숫자만 입력 가능합니다.");
-        }
-        if (inputNumbersString.length() != inputNumbersString.chars().distinct().count()) {
+        if (numbers.stream().distinct().count() != MAX_SIZE) {
             throw new IllegalArgumentException("입력값은 중복되지 않아야 합니다.");
         }
-        if (inputNumbersString.chars().mapToObj(Character::getNumericValue)
-                .anyMatch(number -> number.compareTo(0) < 0)) {
+        if (numbers.stream().anyMatch(number -> number.compareTo(0) < 0)) {
             throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
         }
 
-        return inputNumbersString.chars()
-                .mapToObj(Character::getNumericValue)
-                .toList();
+        return numbers;
     }
+
 }
