99357c066539b9027a733224ce382c11415e1270
LastCow9000 <sys19912002@hanmail.net>
2023-10-24 01:09:00 +0900

    refactor: 게임 종료 시 호출할 코드를 input과 output객체로 책임 분리

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -4,9 +4,18 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
     private static final String inputMessage = "숫자를 입력해주세요 : ";
+    private static final String inputRegameMessage = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
     public static String inputBalls() {
         System.out.print(inputMessage);
         return Console.readLine();
     }
+
+    public static int inputRegame() {
+        System.out.println(inputRegameMessage);
+        String inputData = Console.readLine();//validate필요
+        return Integer.parseInt(inputData);
+    }
+
 }
 
--- src/main/java/baseball/Output.java
+++ src/main/java/baseball/Output.java
@@ -2,8 +2,13 @@ package baseball;
 
 public class Output {
     private static final String startMessage = "숫자 야구 게임을 시작합니다.";
+    private static final String endMessage = "3스트라이크\n" + "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     public static void printStartMessage() {
         System.out.println(startMessage);
+    }
+
+    public static void printEndMessage() {
+        System.out.println(endMessage);
     }
 }
