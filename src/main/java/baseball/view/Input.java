59d9c8955622b5daebc54f584193773e31d2271f
Maljaaa <Maljaaa@github.com>
2023-10-23 16:54:11 +0900

    refactor: 유저 번호 삭제

--- src/main/java/baseball/view/Input.java
+++ src/main/java/baseball/view/Input.java
@@ -3,14 +3,11 @@ package baseball.view;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Input {
-
-    // player가 선택한 서로 다른 3가지 숫자
-    private String userNumber;
-
     // player가 서로 다른 숫자 3가지 입력
-    public void inputUserNumber() {
+    public String inputUserNumber() {
         System.out.print("숫자를 입력해주세요 : ");
-        userNumber = readLine();
+
+        return readLine();
     }
 
     // player가 게임 종료 후 재시작/종료 선택
@@ -18,9 +15,5 @@ public class Input {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
         return readLine();
-    }
-
-    public String getUserNumber() {
-        return userNumber;
     }
 }
