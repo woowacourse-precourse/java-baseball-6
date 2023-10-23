46335616b42570d465858bca821bb7454b4aca3f
anseongjun <dkstjdwns@kookmin.ac.kr>
2023-10-24 02:59:45 +0900

    feat: 사용자가 입력한 숫자 확인 및 스트라이크,볼 확인

---
+++ src/main/java/baseball/controller/CheckNumber.java
@@ -0,0 +1,56 @@
+package baseball.controller;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class CheckNumber {
+
+    public static void checkNumberRules(String userInput) {
+        String afterRegexInput = userInput.replaceAll("[^1-9]", "");
+        Set<Character> inputSet = new HashSet<>();
+        for (char c : afterRegexInput.toCharArray()) {
+            inputSet.add(c);
+        }
+        if (inputSet.size() != 3) {
+            String errorMessage = "길이 3, 중복없는 1~9까지의 숫자 필요";
+            throw new IllegalArgumentException(errorMessage);
+        }
+    }
+
+    public static void strikesAndBalls(List<Integer> computer, String userInput) {
+        int strike = 0;
+        int ball = 0;
+
+        for (int i = 0; i < 3; i++) {
+            int charUserInput = userInput.charAt(i) - '0';
+            if (computer.get(i) == charUserInput) {
+                strike++;
+            } else if (computer.contains(charUserInput)) {
+                ball++;
+            }
+        }
+
+        if (strike == 3) {
+            System.out.println("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            BaseBallGame.restart = Integer.parseInt(Console.readLine());
+            BaseBallGame.correct = true;
+        } else if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+            BaseBallGame.correct = false;
+        } else if (strike == 0 && ball != 0) {
+            System.out.println(ball + "볼");
+            BaseBallGame.correct = false;
+        } else if (strike != 0 && ball == 0) {
+            System.out.println(strike + "스트라이크");
+            BaseBallGame.correct = false;
+        } else if (strike != 0 && ball != 0) {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+            BaseBallGame.correct = false;
+        }
+
+    }
+
+
+}
