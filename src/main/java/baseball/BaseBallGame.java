bdf2ef00da584188d7be2f0e14964811b25601e9
JeongHunHui <deca3248@gmail.com>
2023-10-23 20:26:02 +0900

    feat: 게임 생성 및 진행하기

---
+++ src/main/java/baseball/BaseBallGame.java
@@ -0,0 +1,14 @@
+package baseball;
+
+public class BaseBallGame {
+
+    private Computer computer;
+
+    public void play() {
+        
+    }
+
+    public BaseBallGame() {
+        computer = new Computer();
+    }
+}
--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -2,11 +2,14 @@ package baseball;
 
 public class GameManager {
 
+    private BaseBallGame baseBallGame;
+
     public void start() {
         System.out.println(GameMessage.GAME_START);
+        baseBallGame.play();
     }
 
     public GameManager() {
-
+        baseBallGame = new BaseBallGame();
     }
 }
