603b1b3fd86dcac03ee8d2a9b9b937c452337b60
minsang-alt <mazinger97@naver.com>
2023-10-23 15:54:53 +0900

    refactor: 애플리케이션에서 생성자주입 수정
    - model 클래스 주입 제거
    - 서비스와 view클래스만을 컨트롤러에 주입

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,6 @@
 package baseball;
 
 import baseball.controller.BaseballController;
-import baseball.model.ComputerModel;
-import baseball.model.UserModel;
 import baseball.util.exception.TerminateApplicationException;
 import baseball.view.UserInputView;
 import baseball.view.UserOutputView;
@@ -13,8 +11,8 @@ public class Application {
     public static void main(String[] args) {
         try {
             BaseballController baseballController = new BaseballController(new UserInputView(), new UserOutputView(),
-                    new ComputerModel(), new UserModel());
-            baseballController.startGame();
+                    new BaseballService());
+            baseballController.run();
         } catch (TerminateApplicationException e) {
             System.out.println(e.getMessage());
             Console.close();
