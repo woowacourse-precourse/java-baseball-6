c4404778fef4a8aaccc93ec0d25bd06be7681484
김주형 <wnguddl96@knou.ac.kr>
2023-10-23 11:50:51 +0900

    test(ComparingTest): 컴퓨터와 유저 비교 테스트

---
+++ src/main/java/baseball/Comparing.java
@@ -0,0 +1,43 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Comparing {
+    int equalsNumber = 0;
+    int equalsPosition = 0;
+    public Result compareNumbers(String readLine, List<Integer> computer) {
+        List<Integer> user = new ArrayList<>();
+
+        generateUserNumbers(readLine, user);
+
+        compareIndex(computer, user);
+
+        return new Result(equalsNumber, equalsPosition);
+    }
+
+    private static void generateUserNumbers(String readLine, List<Integer> user) {
+        for (int i = 0; i < readLine.length(); i++) {
+            user.add(Character.getNumericValue(readLine.charAt(i)));
+        }
+    }
+
+    private void compareIndex(List<Integer> computer, List<Integer> user) {
+        for (int i = 0; i < user.size(); i++) {
+            int userIndex = user.get(i);
+            int computerIndex = computer.get(i);
+
+            countEqualsNumberAndPosition(computer, userIndex, computerIndex);
+        }
+    }
+
+    private void countEqualsNumberAndPosition(List<Integer> computer, Integer userIndex, Integer computerIndex) {
+        if (userIndex == computerIndex) {
+            equalsNumber++;
+            equalsPosition++;
+        }
+        if (computer.contains(userIndex)) {
+            equalsNumber++;
+        }
+    }
+}
---
+++ src/main/java/baseball/Result.java
@@ -0,0 +1,20 @@
+package baseball;
+
+public class Result {
+
+    private int equalsNumber;
+    private int equalsPosition;
+
+    public Result(int equalsNumber, int equalsPosition) {
+        this.equalsNumber = equalsNumber;
+        this.equalsPosition = equalsPosition;
+    }
+
+    public int getEqualsNumber() {
+        return equalsNumber;
+    }
+
+    public int getEqualsPosition() {
+        return equalsPosition;
+    }
+}
