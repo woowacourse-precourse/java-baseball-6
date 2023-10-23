cfb3ace7ced6d5677392bc0b4ed67de47ea7b52f
yshong1998 <yshong1998@gmail.com>
2023-10-24 00:46:28 +0900

    Style(RandomBaseballNumberGeneratorTest): 불필요한 import 제거

--- src/test/java/baseball/global/util/RandomBaseballNumberGeneratorTest.java
+++ src/test/java/baseball/global/util/RandomBaseballNumberGeneratorTest.java
@@ -1,14 +1,12 @@
 package baseball.global.util;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
-import java.util.stream.IntStream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class RandomBaseballNumberGeneratorTest {
 
