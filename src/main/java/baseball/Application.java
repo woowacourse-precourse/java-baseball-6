fc7bfc37aee681959c71a68b6aee9813a760e983
m5s3 <devmyoungsoo2@gmail.com>
2023-10-24 00:11:21 +0900

    fix: 저장 순서를 저장하기 위해서 `Hashset` -> `LinkedHashSet` 으로 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -66,7 +67,7 @@ public class Application {
     }
 
     private static List<Integer> generateComputerNumbers() {
-        Set<Integer> computerNumbers = new HashSet<>();
+        Set<Integer> computerNumbers = new LinkedHashSet<>();
 
         while (computerNumbers.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
