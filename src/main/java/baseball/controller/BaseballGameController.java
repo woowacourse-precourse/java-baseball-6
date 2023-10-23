93cfd1405185aff91a64d0d3495a673b61438d18
5jeong <wjdgns4385@naver.com>
2023-10-23 21:52:41 +0900

    [Refactor] : 메소드 수정 ,상수 리팩토링
    - restartOrEnd메소드의 게임상태를 가독성높게 수정
    - 상수를 통해 변수의 역할을 명확하게 사용

--- src/main/java/baseball/controller/BaseballGameController.java
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -3,22 +3,23 @@ package baseball.controller;
 import baseball.service.BaseballGameService;
 import baseball.view.InputView;
 import baseball.view.OutputView;
-
 import java.util.HashMap;
 
 public class BaseballGameController {
-    private static final String PLAY_GAME_STATUS = "playing";
-    private static final String END_GAME_STATUS = "end";
+    private static final String PLAY_GAME_STATUS = "PLAYING";
+    private static final String END_GAME_STATUS = "END";
+    private static final String STRIKE_KEY = "STRIKE";
+    private static final String BALL_KEY = "BALL";
+    private static final int THREE_STRIKE_COUNT = 3;
+    private static final String RESTART_NUM = "1";
+    private static final String END_NUM = "2";
 
-    private static final int RESTART_NUM = 1;
-    private static final int END_NUM = 2;
 
     private static String gameStatus;
 
     BaseballGameService baseballGameService = new BaseballGameService();
 
     public void run() {
-        gameStatus = PLAY_GAME_STATUS;
         gameStart();
         while (gameStatus.equals(PLAY_GAME_STATUS)) {
             baseballGameService.createComputer();
@@ -30,6 +31,7 @@ public class BaseballGameController {
     }
 
     public void gameStart() {
+        gameStatus = PLAY_GAME_STATUS;
         OutputView.gameStartOutput();
     }
 
@@ -38,11 +40,13 @@ public class BaseballGameController {
     }
 
     public void gamePlay() {
-        while (baseballGameService.StrikeCount() != 3) {
+        while (baseballGameService.StrikeCount() != THREE_STRIKE_COUNT) {
             String playerNum = InputView.inputNum();
+            baseballGameService.judgeCorrectNum(playerNum);
             gameInit();
-            HashMap<String, Integer> result = baseballGameService.judgeResult(playerNum);
-            OutputView.gameResult(result.get("STRIKE"), result.get("BALL"));
+            HashMap<String, Integer> result = baseballGameService
+                    .judgeResult(playerNum, baseballGameService.getComputer().getComputerNum());
+            OutputView.gameResultOutput(result.get(STRIKE_KEY), result.get(BALL_KEY));
         }
     }
 
@@ -51,9 +55,17 @@ public class BaseballGameController {
     }
 
     public void restartOrEnd() {
-        int restartNum = InputView.restart();
-        if (restartNum == END_NUM) {
+        String restartNum = InputView.restart();
+        baseballGameService.judgeCorrectRestartNum(restartNum);
+        if (restartNum.equals(RESTART_NUM)) {
+            gameStatus = PLAY_GAME_STATUS;
+        }
+        if (restartNum.equals(END_NUM)) {
             gameStatus = END_GAME_STATUS;
         }
+    }
+
+    public String getGameStatus() {
+        return gameStatus;
     }
 }
