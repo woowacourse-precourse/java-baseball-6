3fb153cf9586c3a1684ff4121a8c61a10829b4a8
taewan625 <taewan1997625@gmail.com>
2023-10-23 23:19:25 +0900

    4차 코드 리팩토링

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -17,35 +17,23 @@ public class Application {
          *
          * 5. 시작 문구   : while 사용
          * 6. 재시작/종료  : 1(새로 시작) 2 (종료) * System.exit()를 호출하지 않는다. -> break, continue 사용
+         *
+         * 7. println 에러 확인 : 2볼 0스트라이크 -> 2볼
          * */
 
-        int gameStatus = 0;
-        System.out.println("gameStatus = " + gameStatus);
         List<Integer> computer = new ArrayList<>();
 
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
+            if (!computer.contains(randomNumber)) computer.add(randomNumber);
         }
         System.out.println("숫자 야구 게임을 시작합니다.");
-        System.out.println("randomNumber = " + computer);
 
         while (true) {
             // random 수 설정
-            if (gameStatus == 1) {
-                computer = new ArrayList<>();
-                while (computer.size() < 3) {
-                    int randomNumber = Randoms.pickNumberInRange(1, 9);
-                    if (!computer.contains(randomNumber)) {
-                        computer.add(randomNumber);
-                    }
-                }
-                System.out.println("randomNumber = " + computer);
-
-
-                gameStatus = 0;
+            while (computer.size() < 3) {
+                int randomNumber = Randoms.pickNumberInRange(1, 9);
+                if (!computer.contains(randomNumber)) computer.add(randomNumber);
             }
 
             // 초기 문구
@@ -55,7 +43,7 @@ public class Application {
             // 예외 처리
             if (inputValue.length() > 3) throw new IllegalArgumentException();
 
-            // strike, boll count
+            // 스트라이크, 볼 개수 count
             int strikeCnt = 0;
             int bollCnt = 0;
             char[] chars = inputValue.toCharArray();
@@ -64,21 +52,23 @@ public class Application {
                 else if (computer.contains(chars[i] - '0')) bollCnt++;
             }
 
-            // 출력
+            // 출력 - 0개인 경우 세지 않는다.
             if (strikeCnt + bollCnt == 0) System.out.println("낫싱");
-            if (strikeCnt == 3) {
-                System.out.println("3스트라이크 \n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-
-                // 게임 재시작 or 종료
-                String choiceNum = Console.readLine();
-                if (choiceNum.equals("1")) {
-                    gameStatus = 1;
-                    continue;
+            else if (strikeCnt == 0 && bollCnt != 0) System.out.println(bollCnt + "볼");
+            else if (bollCnt == 0 && strikeCnt != 0) {
+                System.out.println(strikeCnt + "스트라이크");
+                if (strikeCnt == 3) {
+                    // 게임 재시작 or 종료
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                    String choiceNum = Console.readLine();
+                    if (choiceNum.equals("1")) {
+                        computer.clear(); // list 비우기
+                        continue;
+                    }
+                    if (choiceNum.equals("2")) break;
                 }
-                if (choiceNum.equals("2")) break;
-            }
-            System.out.println(bollCnt + "볼 " + strikeCnt + "스트라이크");
+            } else System.out.println(bollCnt + "볼 " + strikeCnt + "스트라이크");
         }
     } // main 끝
 }
