2edb361e66c0e5948babc1cec6a079bddff5ee3c
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-10-23 13:41:47 +0900

    feat: 유저 입력 및 유효성 검사 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,10 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Application {
@@ -20,6 +22,31 @@ public class Application {
 
     private static void play() {
         List<Integer> computer = createRandomNumbers();
+        do {
+            String input = Console.readLine();
+            List<Integer> user = parse(input);
+        } while (false);
+    }
+
+    private static List<Integer> parse(String input) {
+        List<Integer> parsedInput = new ArrayList<>();
+        try {
+            if (input.length() != 3) {
+                throw new IllegalArgumentException();
+            }
+            List<String> listedInput = Arrays.asList(input);
+            while(parsedInput.size() < 3) {
+                int number = Integer.parseInt(listedInput.remove(0));
+                if (!parsedInput.contains(number)) {
+                    parsedInput.add(number);
+                } else {
+                    throw new IllegalArgumentException();
+                }
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+        return parsedInput;
     }
 
     private static List<Integer> createRandomNumbers() {
