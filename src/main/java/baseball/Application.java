1d9232557635309dc79aa8c8a99da8a1bbc11b40
D0ri123 <lsh81550@gmail.com>
2023-10-23 14:59:38 +0900

    feat: 분리한 객체 클래스로 main 메서드에서 동작하도록 구현 - 2차 구현 최종
    2차 구현 단계에서는 객체를 나누는 것을 목표로 했습니다.
    나눈 객체의 메서드를 이용하여 기능 요구사항을 충족시키도록 구현했습니다.
    아직까지 예외 처리에 대해서는 고려하지 않았습니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,105 +1,28 @@
 package baseball;
 
-import static baseball.Type.*;
-
-import java.util.ArrayList;
-import java.util.List;
-import camp.nextstep.edu.missionutils.Randoms;
-import camp.nextstep.edu.missionutils.Console;
-
 public class Application {
     public static void main(String[] args) {
+        GameMaster gameMaster = new GameMaster();
 
-        //2. 시작 문구 출력
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        while(gameMaster.shouldContinueRound) {
+            Computer computer = new Computer();
+            gameMaster.init();
+//            System.out.println(computer.randomNumber);
+            gameMaster.start();
+            while(!gameMaster.isCorrectAnswerFound) {
+                gameMaster.progress();
 
-        //3. 입력 문구 및 숫자 입력
-        boolean isContinued = true;
+                Validator validator = new Validator(computer.randomNumber, gameMaster.inputNumber);
+                validator.init();
+                validator.changeInputBaseballType();
+                String s = gameMaster.printResult(validator.calculateResult());
 
-        while(isContinued) {
-            //1. 난수 생성
-            List<Integer> computer = new ArrayList<>();
-            String computerRandom = "";
-            while (computer.size() < 3) {
-                int randomNumber = Randoms.pickNumberInRange(1, 9);
-                if (!computer.contains(randomNumber)) {
-                    computer.add(randomNumber);
-                    computerRandom += String.valueOf(randomNumber);
-                }
-            }
-
-            int randomNumber = Integer.parseInt(computerRandom);
-            System.out.println(randomNumber);
-            boolean isFinished = true;
-
-            //4. 랜덤 숫자와 입력 숫자를 비교하는 과정
-            while (isFinished) {
-                System.out.print("숫자를 입력해주세요 : ");
-
-                int number = Integer.parseInt(Console.readLine());
-
-                System.out.println(validator(randomNumber, number));
-                if (randomNumber == number) {
-                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                    int continued = Integer.parseInt(Console.readLine());
-                    if (continued == 2) {
-                        isFinished = false;
-                        isContinued = false;
-                    } else {
-                        isFinished = false;
-                    }
+                String decide = gameMaster.checkResult(s);
+                if(decide != null) {
+                    gameMaster.shouldContinueRound = validator.answerOfProgress(decide);
+                    gameMaster.isCorrectAnswerFound = true;
                 }
             }
         }
-
-    }
-
-    public static String validator(int randomNum, int num) {
-        //스트라이크 검증기와 볼 검증기는 다르게 검증할 수 있을까?
-        //볼을 먼저 검증하고, 그 다음으로 스트라이크를 검증해야게따.
-        int ball = 0;
-        int strike = 0;
-
-        String[] splitRandom = String.valueOf(randomNum).split("");
-        String[] splitInput = String.valueOf(num).split("");
-
-        ArrayList<String> randomList = new ArrayList<>();
-        ArrayList<Baseball> inputList = new ArrayList<>();
-
-        for(int i=0; i<3; i++) {
-            randomList.add(splitRandom[i]);
-            inputList.add(new Baseball(NONE, Integer.parseInt(splitInput[i]), i));
-        }
-
-        for(Baseball baseball : inputList) {
-            String baseballNumber = String.valueOf(baseball.number);
-            if(randomList.contains(baseballNumber)) {
-                if(randomList.indexOf(baseballNumber) == baseball.index) {
-                    baseball.changeType(STRIKE);
-                } else {
-                    baseball.changeType(BALL);
-                }
-            }
-        }
-
-        for(Baseball baseball : inputList) {
-            if(baseball.type == STRIKE) {
-                strike++;
-            } else if(baseball.type == BALL) {
-                ball++;
-            }
-        }
-
-        if(strike==0 && ball==0) {
-            return "낫싱";
-        } else if(strike!=0 && ball==0) {
-            return strike + "스트라이크";
-        } else if(strike==0 && ball!=0) {
-            return ball + "볼";
-        } else {
-            return ball + "볼 " + strike + "스트라이크";
-        }
-
     }
 }
