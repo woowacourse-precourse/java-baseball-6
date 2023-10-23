0248d48b416f103dc32d10913d86e843c8313e63
MinseokGo <rhalstjr1999@naver.com>
2023-10-23 22:31:22 +0900

    feat: 컴퓨터의 랜덤 숫자를 생성하는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        do {
+            //게임 시작
+        } while (true); //게임 재시작 여부
+    }
+
+    private static List<Integer> createRandomNumber() {
+        List<Integer> computerNumbers = new ArrayList<>();
+        while (computerNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumbers.contains(randomNumber)) {
+                computerNumbers.add(randomNumber);
+            }
+        }
+        return computerNumbers;
     }
 }
