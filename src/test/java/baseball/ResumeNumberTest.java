92fb5cf942ca3ead2c42f39386df668a49d62c75
devholic22 <hyunjoon.tech@gmail.com>
2023-10-23 14:49:50 +0900

    Test: 재시작 또는 종료값은 한자릿수여야만 한다

---
+++ src/test/java/baseball/ResumeNumberTest.java
@@ -0,0 +1,23 @@
+package baseball;
+
+import baseball.model.ResumeNumber;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ResumeNumberTest {
+
+    private ResumeNumber resumeNumber;
+
+    @BeforeEach
+    void beforeEach() {
+        resumeNumber = ResumeNumber.createDefault();
+    }
+
+    @Test
+    void 재시작_또는_종료값은_한자릿수여야만_한다() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            resumeNumber.updateNumber("123");
+        });
+    }
+}
