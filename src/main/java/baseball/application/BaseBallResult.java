1c8f90b2db0b59a8fe96c24388ee30be389f6c29
hunko <rhwlgns4386@naver.com>
2023-10-24 00:43:31 +0900

    feat : add logic
    Add ball save logic

--- src/main/java/baseball/application/BaseBallResult.java
+++ src/main/java/baseball/application/BaseBallResult.java
@@ -3,12 +3,18 @@ package baseball.application;
 public class BaseBallResult {
 
     private final int strike;
+    private final int ball;
 
-    public BaseBallResult(int strike) {
+    public BaseBallResult(int strike, int ball) {
         this.strike = strike;
+        this.ball = ball;
     }
 
     public int getStrike() {
         return strike;
+    }
+
+    public int getBall() {
+        return ball;
     }
 }
