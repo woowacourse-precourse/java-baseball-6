d50c5e7dee4f1b06f9af93d00950385ebd6fdb58
doka <posmane5@gmail.com>
2023-10-23 20:09:32 +0900

    docs: readme.md, changelog.md  -구현할 기능들을 세부적으로 나누고 readme.md 파일에 정리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int myNum = Integer.parseInt(Console.readLine());
+        int randomNum = Randoms.pickNumberInRange(1,myNum);
+        System.out.println(randomNum);
+        Console.close();
     }
 }
