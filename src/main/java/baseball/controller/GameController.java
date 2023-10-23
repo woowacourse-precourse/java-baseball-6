b06dc38de277fa2ada234685006209cf27b0683f
MingyeomKim <mingyum119@naver.com>
2023-10-23 14:34:55 +0900

    feat(GameController): 게임을 시작하는 함수에서 출력
    GameController 클래스를 생성할 때 게임 시작 문구가 출력되도록 생성자를 정의한다.

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -4,4 +4,7 @@ public class GameController {
 
     private final String GAME_START_GUIDE = "숫자 야구 게임을 시작합니다.";
 
+    public GameController() {
+        System.out.println(GAME_START_GUIDE);
+    }
 }
