539daf83be1f813695368c13c373674407e675da
prefer52 <tjsgh151@naver.com>
2023-10-23 23:09:20 +0900

    스트라이크 볼 카운트 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,15 +1,7 @@
 package baseball;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Baseball baseball = new Baseball();
-        List<Character> characters = new ArrayList<>();
-        for(char num = '1' ; num <= '9' ; num += 1)
-            characters.add(Character.valueOf(num));
-        baseball.setDomain(characters);
     }
 }
--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -1,17 +1,9 @@
 package baseball;
 
-import java.util.List;
+import camp.nextstep.edu.missionutils.*;
 
 public class Baseball {
-    private int computerNumber;
-    private List<Character> domain;
-
-    /**
-     * 입력 값 유효 범위 설정 함수
-     */
-    public void setDomain(List<Character> domain) {
-        this.domain = domain;
-    }
+    private String computerNumber;
 
     /**
      * 숫자 야구 진행 함수
@@ -23,16 +15,10 @@ public class Baseball {
     /**
      * 입력 값 검증 함수
      * 입력: userNumber(사용자의 입력 값)
-     *      domain(입력 값 유효 범위)
      * 출력: boolean(입력 값이 유효 범위 내에 존재하는 값일 경우 true 반환)
      */
     boolean isValid(String userNumber) {
-        // 사용자의 입력값 자리수마다 확인하여 하나라도 domain에 없는 값일 경우 false 반환
-        for(int i = 0 ; i < userNumber.length() ; i++) {
-            if (!domain.contains((Character)userNumber.charAt(i)))
-                return false;
-        }
-
+        // 사용자가 입력한 값이 3자리 문자인지 확인
         return true;
     }
 
@@ -42,19 +28,55 @@ public class Baseball {
      *      result(결과 배열, 0, 1번째 인덱스는 각각 스트라이크 개수, 볼 개수)
      * 출력: boolean(3 스트라이크일 경우 true 반환)
      */
-    boolean checkNumber(int userNumber, int result[]) {return true;}
+    boolean checkNumber(String userNumber, int result[]) {
+        result[0] = countStrike(userNumber);
+        // 볼의 개수를 셀 때, 스트라이크도 볼로 카운트하므로 스트라이크 개수 제외
+        result[1] = countBall(userNumber) - result[0];
+        if (result[1] > 0) {
+            System.out.print(result[1] + "볼");
+            if (result[0] > 0)
+                System.out.print(" " + result[0] + "스트라이크");
+            System.out.println();
+        } else if (result[0] > 0) {
+            if (result[0] > 0)
+                System.out.println(result[0] + "스트라이크");
+        } else {
+            System.out.println("낫싱");
+        }
+
+        if (result[0] == 3)
+            return true;
+        else
+            return false;
+    }
 
     /**
      * 스트라이크 개수 확인 함수
      * 입력: userNumber(사용자의 입력 값)
      * 출력: int(스트라이크 개수)
      */
-    int countStrike(int userNumber) {return 0;}
+    int countStrike(String userNumber) {
+        int result = 0;
+        for (int i = 0; i < userNumber.length(); i++) {
+            if (userNumber.charAt(i) == computerNumber.charAt(i))
+                result++;
+        }
+
+        return result;
+    }
 
     /**
      * 볼 개수 확인 함수
      * 입력: userNumber(사용자의 입력 값)
      * 출력: int(볼 개수)
      */
-    int countBall(int userNumber) {return 0;}
+    int countBall(String userNumber) {
+        int result = 0;
+        for(int i = 0 ; i < userNumber.length() ; i++) {
+            if (computerNumber.contains(userNumber.charAt(i) + ""))
+                result++;
+        }
+
+        return result;
+    }
 }
