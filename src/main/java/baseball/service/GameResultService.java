ed08affcc7aa7f38ee9128a8e7642158cf403cd8
Lieslot <rlaxodud1999@gmail.com>
2023-10-23 16:44:30 +0900

    [feat] BallCount 객체 추가

--- src/main/java/baseball/service/GameResultService.java
+++ src/main/java/baseball/service/GameResultService.java
@@ -2,6 +2,7 @@ package baseball.service;
 
 import static baseball.enums.MessageEnum.*;
 
+import baseball.domain.BallCount;
 import baseball.dto.BallCountDto;
 import baseball.dto.GameResultDto;
 import baseball.enums.MessageEnum;
@@ -9,21 +10,24 @@ import baseball.enums.MessageEnum;
 public class GameResultService {
 
 
+    private BallCount ballCount;
+
    public GameResultDto calculateGameResult(BallCountDto ballCountDto) {
-       String message = createBallCountMessage(ballCountDto.showStrike(), ballCountDto.showBall());
+       ballCount = new BallCount(ballCountDto.showStrike(), ballCountDto.showBall());
+       String message = createBallCountMessage();
        return new GameResultDto(ballCountDto.showStrike() == 3, message);
    }
 
-    private String createBallCountMessage(int strike, int ball) {
+    private String createBallCountMessage() {
         String message = "";
-        if (strike == 0 && ball == 0) {
+        if (ballCount.hasNothing()) {
             message = NOTHING.show();
         }
-        if (ball != 0) {
-            message += ball + BALL.show()+ " ";
+        if (ballCount.hasBall()) {
+            message += ballCount.showBall() + BALL.show()+ " ";
         }
-        if (strike != 0) {
-            message += strike + STRIKE.show();
+        if (ballCount.hasStrike()) {
+            message += ballCount.showStrike() + STRIKE.show();
         }
 
         return message;
