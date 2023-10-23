92f11e844c1d23f56ca62f12332f4e37eebcc4d3
gywns0417 <gywns0417@gmail.com>
2023-10-23 22:21:55 +0900

    refactor: 메서드 순서 변경

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -17,11 +17,11 @@ public class OutputView {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
-    public static void gameResultMessage(String message) {
-        System.out.println(message);
-    }
-
     public static void endGameMessage() {
         System.out.println("게임 종료");
+    }
+
+    public static void gameResultMessage(String message) {
+        System.out.println(message);
     }
 }
