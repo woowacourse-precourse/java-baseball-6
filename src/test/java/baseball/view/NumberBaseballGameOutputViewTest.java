57ddec737d1cf3c683572c1c64139264e08fbd16
wnsgh12s <junho1273@gmail.com>
2023-10-23 19:55:09 +0900

    Test: 사용자에게 올바른 시작 메시지가 출력되는지 테스트
    setOut을 통해 System.out 즉 PrintStream객체를 재할당해서 상태를 테스트합니다.

---
+++ src/test/java/baseball/view/NumberBaseballGameOutputViewTest.java
@@ -0,0 +1,36 @@
+package baseball.view;
+
+import static baseball.view.NumberBaseballGameOutputView.START_GAME_MESSAGE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class NumberBaseballGameOutputViewTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    void 시작_메시지가_올바르게_출력되는지_테스트() {
+        NumberBaseballGameOutputView outputView = new NumberBaseballGameOutputView();
+        String gameName = "숫자 야구";
+        outputView.printStartGameMessage(gameName);
+        assertEquals(gameName + START_GAME_MESSAGE + "\r\n", outContent.toString());
+    }
+}
