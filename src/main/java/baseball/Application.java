f44d9bfbaa82e579ba40c7066c5e70de8c283466
mummummm <eunbipnoeul88@gmail.com>
2023-10-23 22:24:54 +0900

    함수를 적용해 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,65 +7,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+    private static final int INPUT_LENGTH = 3;
+
     public static void main(String[] args) {
 
-        int inputNum = 0; //사용자가 입력하는 숫자
-        List<Integer> user = new ArrayList<>();// 사용자가 입력하는 숫자를 담을 리스트
-
-        List<Integer> computer = makeRandomNumber(); //랜덤 숫자 생성
-
-        int strikeCount = 0; // 스트라이크 카운팅
-        int ballCount = 0; // 볼 카운팅
+        List<Integer> computer = makeRandomNumber();
+        int strikeCnt = 0;
+        int ballCnt = 0;
 
         System.out.println("숫자 야구 게임을 시작합니다.");
         while (true) {
             System.out.print("숫자를 입력해주세요 : ");
-            String inputNumberStr = Console.readLine();
-            if(inputNumberStr.length() != 3) {  //입력받은 수가 3자리가 아니라면 예외 발생
-                throw new IllegalArgumentException();
-            }
+            String inputNumStr = Console.readLine();
 
-            inputNum = Integer.parseInt(inputNumberStr);
+            validateInputLength(inputNumStr);
 
-            user.add(inputNum / 100);
-            user.add((inputNum % 100) / 10);
-            user.add(inputNum % 10);
+            List<Integer> users = parseAndAddInputsToList(inputNumStr);
 
-            //사용자가 입력한 숫자와 랜덤 숫자를 비교해 볼 수와 스트라이크 수를 구한다.
-            if(!computer.equals(user)) {
-                for (int i = 0; i < computer.size(); i++) {
-                    if(computer.get(i).equals(user.get(i))) {
-                        ++strikeCount;
-                    }
-                    for (int j = 0; j < user.size(); j++) {
-                        if(computer.get(i).equals(user.get(j))) {
-                            ++ballCount;
-                            if(computer.get(i).equals(user.get(i))) {
-                                --ballCount;
-                            }
-                        }
-                    }
-                }
-                //볼 수와 스트라이크 수를 출력한다.
-                if(ballCount > 0 && strikeCount > 0) {
-                    System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
-                    ballCount = 0;
-                    strikeCount = 0;
-                } else if(strikeCount > 0) {
-                    System.out.println(strikeCount + "스트라이크");
-                    strikeCount = 0;
-                } else if(ballCount > 0) {
-                    System.out.println(ballCount + "볼");
-                    ballCount = 0;
-                } else {
-                    System.out.println("낫싱");
-                }
+            if (!computer.equals(users)) {
+                strikeCnt = getStrikeCount(computer, users);
+                ballCnt = getBallCount(computer, users);
+
+                printBallCountAndStrikeCount(ballCnt, strikeCnt);
             } else {
-                System.out.println("3스트라이크");
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-
-                int start = Integer.parseInt(Console.readLine());
+                print3Strike();
+                int start = isRestart();
                 if(start == 1) {
                     computer = makeRandomNumber();
                 } else if(start == 2) {
@@ -73,19 +39,82 @@ public class Application {
                     break;
                 }
             }
-            user.clear();
         }
     }
 
-    //랜덤 숫자를 만드는 메서드
     private static List<Integer> makeRandomNumber() {
         List<Integer> randomList = new ArrayList<>();
-        while (randomList.size() < 3) {
+        while (randomList.size() < INPUT_LENGTH) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!randomList.contains(randomNumber)) {
                 randomList.add(randomNumber);
             }
         }
         return randomList;
+    }
+
+    private static void validateInputLength(String inputNumber) {
+        if(inputNumber.length() != INPUT_LENGTH) {
+            throw new IllegalArgumentException(INPUT_LENGTH + "자리를 입력해야 합니다.");
+        }
+    }
+
+    private static List<Integer> parseAndAddInputsToList(String inputNumber) {
+        int inputNum = Integer.parseInt(inputNumber);
+
+        List<Integer> user = new ArrayList<>();
+        user.add(inputNum / 100);
+        user.add((inputNum % 100) / 10);
+        user.add(inputNum % 10);
+
+        return user;
+    }
+
+    private static int getStrikeCount(List<Integer> computer, List<Integer> user) {
+        int strikeCount = 0;
+        for (int i = 0; i < computer.size(); i++) {
+            if (computer.get(i).equals(user.get(i))) {
+                ++strikeCount;
+            }
+        }
+        return strikeCount;
+    }
+
+    private static int getBallCount(List<Integer> computer, List<Integer> user) {
+        int ballCount = 0;
+        for (int i = 0; i < computer.size(); i++) {
+            for (int j = 0; j < user.size(); j++) {
+                if (computer.get(i).equals(user.get(j))) {
+                    ++ballCount;
+                    if (computer.get(i).equals(user.get(i))) {
+                        --ballCount;
+                    }
+                }
+            }
+        }
+        return ballCount;
+    }
+
+    private static void printBallCountAndStrikeCount(int ballCount, int strikeCount) {
+        if (ballCount > 0 && strikeCount > 0) {
+            System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+        } else if (strikeCount > 0) {
+            System.out.println(strikeCount + "스트라이크");
+        } else if (ballCount > 0) {
+            System.out.println(ballCount + "볼");
+        } else {
+            System.out.println("낫싱");
+        }
+    }
+
+    private static void print3Strike() {
+        System.out.println("3스트라이크");
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    private static int isRestart() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        int start = Integer.parseInt(Console.readLine());
+        return start;
     }
 }
