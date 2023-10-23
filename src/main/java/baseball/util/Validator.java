0bd5bf63f753b1975d012680993f3a6019014814
jiwon1217 <koagjw@gmail.com>
2023-10-24 05:50:01 +0900

    :recycle: refactor: 메서드 이름 변경, 스트림을 사용하여 비교 로직 간편화

--- src/main/java/baseball/util/Validator.java
+++ src/main/java/baseball/util/Validator.java
@@ -1,26 +1,24 @@
 package baseball.util;
 
-import java.util.Objects;
-
 public class Validator {
     private static final int MAX_SIZE = 3;
     private static final String RESTART = "1";
     private static final String EXIT = "2";
 
     public static void validateInputNumbers(String numbers) {
-        isValidNumbersLength(numbers);
-        isValidNumbersType(numbers);
-        isValidNumbersRange(numbers);
-        isDuplicate(numbers);
+        validateNumbersLength(numbers);
+        validateNumbersType(numbers);
+        validateNumbersRange(numbers);
+        validateNoDuplicate(numbers);
     }
 
-    public static void isValidNumbersLength(String numbers) {
+    public static void validateNumbersLength(String numbers) {
         if (numbers.length() != MAX_SIZE) {
             throw new IllegalArgumentException();
         }
     }
 
-    public static void isValidNumbersType(String numbers) {
+    public static void validateNumbersType(String numbers) {
         try {
             Integer.parseInt(numbers);
         } catch (NumberFormatException e) {
@@ -28,39 +26,32 @@ public class Validator {
         }
     }
 
-    public static void isValidNumbersRange(String numbers) {
+    public static void validateNumbersRange(String numbers) {
         if (!hasValidNumberRange(numbers)) {
             throw new IllegalArgumentException();
         }
     }
 
     public static boolean hasValidNumberRange(String numbers) {
-        for (char digit : numbers.toCharArray()) {
-            int number = Character.getNumericValue(digit);
-            if (number < 1) {
-                return false;
-            }
-        }
-        return true;
+        return numbers.chars().allMatch(number -> number >= '1' && number <= '9');
     }
 
-    public static void isDuplicate(String numbers) {
+    public static void validateNoDuplicate(String numbers) {
         if (hasDuplicateNumber(numbers)) {
             throw new IllegalArgumentException();
         }
     }
 
     public static boolean hasDuplicateNumber(String numbers) {
-        return numbers.charAt(0) == numbers.charAt(1) || numbers.charAt(1) == numbers.charAt(2)
-                || numbers.charAt(0) == numbers.charAt(2);
+        return numbers.chars().distinct().count() < numbers.length();
     }
 
     public static void validateRestartOrExitNumber(String number) {
-        isValidRestartOrExitNumberType(number);
-        isValidRestartOrExitNumber(number);
+        validateRestartOrExitNumberType(number);
+        validateRestartOrExitNumberValue(number);
     }
 
-    public static void isValidRestartOrExitNumberType(String number) {
+    public static void validateRestartOrExitNumberType(String number) {
         try {
             Integer.parseInt(number);
         } catch (NumberFormatException e) {
@@ -68,13 +59,13 @@ public class Validator {
         }
     }
 
-    public static void isValidRestartOrExitNumber(String number) {
+    public static void validateRestartOrExitNumberValue(String number) {
         if (!isRestartOrExitNumber(number)) {
             throw new IllegalArgumentException();
         }
     }
 
     public static boolean isRestartOrExitNumber(String number) {
-        return Objects.equals(number, RESTART) || Objects.equals(number, EXIT);
+        return number.equals(RESTART) || number.equals(EXIT);
     }
 }
