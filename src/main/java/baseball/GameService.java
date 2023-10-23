bee688c08efdc0f252004bac02ad2ae8bbe6d3b6
JongMinCh0i <wonderland314592@gmail.com>
2023-10-23 14:05:03 +0900

    feat(GameService): 게임 실행 서비스를 제공하는 클래스 추가

---
+++ src/main/java/baseball/GameService.java
@@ -0,0 +1,98 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
+public class GameService {
+    private final Validate validate;
+    boolean play = true;
+
+    public GameService(Validate validate) {
+        this.validate = validate;
+    }
+
+    public void solvingProblem() {
+        Computer Computer = new Computer();
+        List<Integer> answerList = Computer.makeAnswerList();
+
+        while (play) {
+            System.out.print("숫자를 입력해주세요 : ");
+            String str = Console.readLine();
+
+            try {
+                validate.combinedValidation(str);
+            } catch (IllegalArgumentException e) {
+                play = false;
+                e.setStackTrace(e.getStackTrace());
+            }
+
+            if ((validate.combinedValidation(str))) {
+                checkResult(str, answerList);
+            }
+        }
+    }
+
+    public void checkResult(String input, List<Integer> answerList) {
+        int strikeCount = 0;
+        int ballCount = 0;
+
+        int[] intArr = new int[answerList.size()];
+
+        for (int i = 0; i < answerList.size(); i++) {
+            intArr[i] = Integer.parseInt(input.substring(i, i + 1));
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (answerList.get(i) == (intArr[i])) {
+                ++strikeCount;
+            }
+
+            if (answerList.get(i) != (intArr[i]) && answerList.contains(intArr[i])) {
+                ++ballCount;
+            }
+        }
+        printResultMessage(strikeCount, ballCount);
+
+        if (strikeCount == 3) {
+            printGameEndMessage();
+        }
+    }
+
+    public void printResultMessage(int StrikeCount, int BallCount) {
+        if (BallCount > 0) {
+            System.out.print(BallCount + "볼 ");
+        }
+
+        if (StrikeCount > 0) {
+            System.out.print(StrikeCount + "스트라이크");
+        }
+
+        if (StrikeCount == 0 && BallCount == 0) {
+            System.out.print("낫싱");
+        }
+
+        System.out.println();
+    }
+
+    public void printGameEndMessage() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료 \n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = Console.readLine();
+
+        try {
+            if (validate.validationReplayInput(input)) {
+                if (input.equals("1")) {
+                    solvingProblem();
+                }
+
+                if (input.equals("2")) {
+                    play = false;
+                    System.out.println("게임을 종료합니다.");
+                }
+            }
+        } catch (IllegalArgumentException e) {
+            play = false;
+            System.out.println("잘못된 입력입니다.");
+            e.getMessage();
+        }
+    }
+}
