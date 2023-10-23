6e834347de93c4639c3dd886db82ba148c24bb8a
K-KY <kyuyoungk@naver.com>
2023-10-23 23:49:05 +0900

    Refactor(DataTypeChanger) : List를 CompareNumber 타입으로 변경하는 메소드 옮김

--- src/main/java/baseball/DataTypeChanger.java
+++ src/main/java/baseball/DataTypeChanger.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,4 +8,13 @@ public class DataTypeChanger {
     public static List<Integer> mapToInt(List<String> userBalls) {
         return userBalls.stream().mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
     }
+
+    public static List<CompareNumber> compareNumberFormat(List<Integer> generatedBall) {
+        ArrayList<CompareNumber> compareNumbers = new ArrayList<>();
+        generatedBall.forEach(v -> {
+            compareNumbers.add(new CompareNumber(compareNumbers.size() + 1, v));
+        });
+        return compareNumbers;
+    }
+
 }
