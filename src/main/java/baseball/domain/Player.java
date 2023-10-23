7eddeb5e41ab276116958cd342ea957a7d2d991b
이지은 <jieun7056@naver.com>
2023-10-23 22:51:29 +0900

    refactor: 사용자가 잘못된 값을 입력할 경우 IllegalArgumentException 예외 처리

--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -5,6 +5,9 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
 
+import static baseball.logic.BaseballGame.EXIT;
+import static baseball.logic.BaseballGame.RESTART;
+
 public class Player {
     private List<Integer> guessNumbers;
 
@@ -18,6 +21,11 @@ public class Player {
     public void guess() {
         System.out.print("숫자를 입력해주세요 : ");
         String answer = Console.readLine();
+
+        if(answer.length() <= 0 || answer.length() > 3 || isDuplicate(answer)){
+            throwIllegalArgumentException();
+        }
+
         for (int i = 0; i < 3; i++) {
             guessNumbers.add(answer.charAt(i) - '0');
         }
@@ -25,6 +33,31 @@ public class Player {
 
     public String replayOrExit() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        return Console.readLine();
+        String answer = Console.readLine();
+        if (answer.equals(RESTART) || answer.equals(EXIT)) {
+            return answer;
+        }
+        throwIllegalArgumentException();
+        return null;
+    }
+
+    public boolean isDuplicate(String answer) {
+        String temp = "";
+
+        for (char c: answer.toCharArray()) {
+            // 포함되지 않는다면
+            if (!temp.contains(String.valueOf(c))) {
+                temp += String.valueOf(c);
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void throwIllegalArgumentException() {
+        System.out.println("예외 발생");
+        throw new IllegalArgumentException();
     }
 }
