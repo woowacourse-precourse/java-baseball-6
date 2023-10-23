796b21b9d385ac2ac42baee7af2e39e35efce5e3
Maljaaa <Maljaaa@github.com>
2023-10-23 20:05:01 +0900

    style: 변수명, 메서드명 변경

--- src/main/java/baseball/model/GameManager.java
+++ src/main/java/baseball/model/GameManager.java
@@ -1,6 +1,9 @@
 package baseball.model;
 
 public class GameManager {
+
+    private static final String GAME_MODE_CONTINUE = "1";
+    private static final String GAME_MODE_END = "2";
     private String gameMode;
 
     public String getGameMode() {
@@ -8,13 +11,13 @@ public class GameManager {
     }
 
     public void setGameMode(String gameMode) {
-        if(validateGameNumber(gameMode)) {
+        if(validateGameMode(gameMode)) {
             this.gameMode = gameMode;
         }
     }
 
-    public boolean validateGameNumber(String gameMode) {
-        if(gameMode.equals("1") || gameMode.equals("2")) {
+    private boolean validateGameMode(String gameMode) {
+        if(GAME_MODE_CONTINUE.equals(gameMode) || GAME_MODE_END.equals(gameMode)) {
             return true;
         } else {
             throw new IllegalArgumentException("1과 2 중에서 선택해야합니다.");
