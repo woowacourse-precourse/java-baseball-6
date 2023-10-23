da7c870a129c22e67bae317a8a0cf84dd1e30e03
bbbang105 <2018111366@dgu.ac.kr>
2023-10-23 17:27:23 +0900

    Refactor : 메인 메소드 수정 메인에서는 반복문을 실행하지 않고, 1번의 run 메소드만 실행하도록 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,13 +3,6 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         Baseball baseball = new Baseball();
-
-        while (true) {
-            baseball.run();
-
-            if (!baseball.checkRestart()) {
-                break;
-            }
-        }
+        baseball.run();
     }
 }
