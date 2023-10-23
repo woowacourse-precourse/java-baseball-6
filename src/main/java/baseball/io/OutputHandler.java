b2235c4a9bc1f9bf8efe0595081526018391db98
GS_song <20003204@sju.ac.kr>
2023-10-23 16:22:53 +0900

    style: if else문 정리

--- src/main/java/baseball/io/OutputHandler.java
+++ src/main/java/baseball/io/OutputHandler.java
@@ -4,19 +4,19 @@ import baseball.model.ResultScoreBoard;
 
 public class OutputHandler {
 
-    public void printMessage (String message) {
+    public void printMessage(String message) {
         System.out.println(message);
     }
 
-    public void printScore (ResultScoreBoard resultScoreBoard) {
-        if (resultScoreBoard.strike() == 0 && resultScoreBoard.ball() == 0) {
-            System.out.println("낫싱");
-        }else if(resultScoreBoard.ball() == 0) {
-            System.out.println(resultScoreBoard.strike() +"스트라이크");
-        }else if(resultScoreBoard.strike() == 0) {
-            System.out.println(resultScoreBoard.ball() + "볼 ");
-        }else if(resultScoreBoard.strike() != 0 && resultScoreBoard.ball() != 0){
-            System.out.println(resultScoreBoard.ball() + "볼 " + resultScoreBoard.strike() + "스트라이크");
-        }
+    public void printScore(ResultScoreBoard score) {
+
+        if (score.strike() == 0 && score.ball() == 0)
+            System.out.print("낫싱");
+        if (score.ball() != 0)
+            System.out.print(score.ball() + "볼 ");
+        if (score.strike() != 0)
+            System.out.print(score.strike() + "스트라이크");
+        System.out.println();
+
     }
 }
