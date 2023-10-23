f7f73da03862f6f100513a2c883cac1785392246
LastCow9000 <sys19912002@hanmail.net>
2023-10-24 01:36:31 +0900

    refactor: 낫싱 메시지 출력 기능  output 객체로 책임 분리

--- src/main/java/baseball/Output.java
+++ src/main/java/baseball/Output.java
@@ -3,6 +3,7 @@ package baseball;
 public class Output {
     private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String END_MESSAGE = "3스트라이크\n" + "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String NOTHING_MESSAGE = "낫싱";
 
     public static void printStartMessage() {
         System.out.println(START_MESSAGE);
@@ -10,5 +11,9 @@ public class Output {
 
     public static void printEndMessage() {
         System.out.println(END_MESSAGE);
+    }
+    
+    public static void printNothingMessage() {
+        System.out.println(NOTHING_MESSAGE);
     }
 }
