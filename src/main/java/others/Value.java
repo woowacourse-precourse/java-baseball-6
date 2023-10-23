3ace65495ee861603a8a7e14f68f2603868361b8
Seungjida <esg0107@pusan.ac.kr>
2023-10-23 22:07:16 +0900

    Feat: "비교한 결과 출력 기능 추가"

--- src/main/java/others/Value.java
+++ src/main/java/others/Value.java
@@ -47,4 +47,20 @@ public class Value {
             }
         }
     }
+
+    public static boolean printResult() {
+        if (strike == 3) {
+            System.out.println(strike + "스트라이크" + "\n" + "3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        } else if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+        } else if (strike == 0 && ball != 0) {
+            System.out.println(ball + "볼");
+        } else if (strike != 0 && ball == 0) {
+            System.out.println(strike + "스트라이크");
+        } else if (strike != 0 && ball != 0) {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
+        return false;
+    }
 }
