72fd4e81bf3b99a9f587b0f5c4a2235eae2488fe
HyunJoong Kim <80106238+hjk0761@users.noreply.github.com>
2023-10-23 23:49:27 +0900

    feat: create view to print messages

---
+++ src/main/java/baseball/View.java
@@ -0,0 +1,40 @@
+package baseball;
+
+import java.util.List;
+
+class Message{
+    public static final String Start = "숫자 야구 게임을 시작합니다.";
+    public static final String GetInput = "숫자를 입력해주세요 : ";
+    public static final String Ball = "볼";
+    public static final String Strike = "스트라이크";
+    public static final String Nothing = "낫싱";
+    public static final String Match = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String Restart = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+}
+
+public class View {
+
+    public void printStart(){
+        System.out.println(Message.Start);
+    }
+
+    public void printInput(){
+        System.out.println(Message.GetInput);
+    }
+
+    public void printResult(List<Integer> ballCount){
+        int ball = ballCount.get(0);
+        int strike = ballCount.get(1);
+        if (ball == 0 && strike == 0){
+            System.out.println(Message.Nothing);
+            return;
+        }
+        System.out.println(ball + Message.Ball + " " + strike + Message.Strike);
+    }
+
+    public void printRestart(){
+        System.out.println(Message.Match);
+        System.out.println(Message.Restart);
+    }
+
+}
