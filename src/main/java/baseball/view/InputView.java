68f788f0a97cd8c15456d20ed1391d202976fec1
lesw <lesw1216@gmail.com>
2023-10-23 16:50:14 +0900

    feat: add input view

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,21 @@
+package baseball.view;
+
+public class InputView {
+    private static final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String INPUT_REQUEST_MESSAGE = "숫자를 입력해주세요 : ";
+
+
+    public void printInputRequestMessage() {
+        System.out.print(INPUT_REQUEST_MESSAGE);
+    }
+
+    public void printStartMessage() {
+        System.out.println(START_MESSAGE);
+    }
+    public void printRestartMessage() {
+        System.out.println(RESTART_MESSAGE);
+    }
+
+
+}
