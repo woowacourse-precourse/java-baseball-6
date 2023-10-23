f04ecf784ff53576796cd58a859c25206e77f6c6
세탁기 <116627038+saetakki@users.noreply.github.com>
2023-10-24 01:38:14 +0900

    feat: 값 입력기능 추가 1. 유저로부터 3자리 수를 입력받는 기능 추가 2. 유저가 3자리가 아닌 수를 입력하거나 문자열을 입력하여 IllegalArgumentException 발생 3. 유저가 3자리수 중 중복된 수가 존재하는 수를 입력하면 마찬자기로 IllegalArgumentException 발생

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner sc = new Scanner(System.in);
+        int userInput = sc.nextInt();
+        if (userInput > 999 || userInput < 123) {
+            throw new IllegalArgumentException("1부터 9까지 서로다른 3자리 수로 입력해야합니다.");
+        }
+        Set<Character> digits = new HashSet<>();
+        for (char digit : Integer.toString(userInput).toCharArray()) {
+            if (digits.contains(digit)) {
+                throw new IllegalArgumentException("중복되지 않은 3자리 숫자를 입력하세요.");
+            }
+            digits.add(digit);
+        }
+        System.out.println(digits);
     }
 }
