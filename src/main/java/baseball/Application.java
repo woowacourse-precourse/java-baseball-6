142f6dd8bda30c9165afe343dc6f1a05d92401bd
morenow <jjang9877@gmail.com>
2023-10-24 01:37:00 +0900

    feat: 게임 진행을 위한 메인 컨트롤 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,30 @@
 package baseball;
 
+import baseball.constant.Message;
+import baseball.constant.Number;
+import baseball.domain.BaseballCount;
+import baseball.util.GameUtil;
+import baseball.util.MessageUtil;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        MessageUtil.printStartMsg();
+        List<Integer> computer = GameUtil.initRandomNumber();
+        while (true) {
+            MessageUtil.printInputMsg();
+            List<Integer> userNum = GameUtil.inputNumber();
+            BaseballCount count = GameUtil.compareBaseball(computer, userNum);
+            MessageUtil.printBaseballCount(count);
+
+            if (count.getStrike() == Number.GAME_LENGTH) {
+                System.out.println(Message.GAME_FINISH_MSG);
+                if (GameUtil.isGameRestart()) {
+                    computer = GameUtil.initRandomNumber();
+                } else {
+                    break;
+                }
+            }
+        }
     }
 }
