03e6957f3e4e6b924c484177b278b7d2de7fd475
RESD\dkanf <dkanfjgwls@naver.com>
2023-10-23 19:06:11 +0900

    README 내용 추가 및 Application 줄 정리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,40 +11,33 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         int start = 1;
-        String input;
-
         while (start == 1) {
             playBaseballGame();
-
-            System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n입력 : ");
-            // try catch사용시 예외 발생하지 않아 테스트 통과 불가능.
-            input = Console.readLine();
+            System.out.print("게임을 새로 시작 하려면 1, 종료 하려면 2를 입력 하세요.\n입력 : ");
+            String input = Console.readLine();
             if (!input.equals("1") && !input.equals("2")) {
-                throw new IllegalArgumentException("입력값은 1 또는 2여야 합니다.");
+                throw new IllegalArgumentException("입력 값은 1 또는 2가 입력 되어야 합니다.");
             }
             start = Integer.parseInt(input);
 
         }
+
     }
 
     private static boolean playBaseballGame() {
         List<Integer> computerNumber = generateComputerNumber();
         boolean endGame = false;
-        String input = "";
 
-        System.out.println("숫자 야구 게임을 시작합니다.");
-
+        System.out.println("숫자 야구 게임을 시작 합니다.");
         while (!endGame) {
-            System.out.print("숫자를 입력해주세요 : ");
-            input = Console.readLine();
-            // 서로 다른 3자리의 수이고 Int형으로 변환이 가능한 문자열 외에 모든 값 예외처리
+            System.out.print("숫자를 입력해 주세요 : ");
+            String input = Console.readLine();
             if (!checkInputNumber(input)) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("서로 다른 3자리 숫자가 입력 되어야 합니다.");
             }
 
             int strike = checkStrike(input, computerNumber);
             int ball = checkBall(input, computerNumber);
-
             if (printResult(ball, strike)) {
                 endGame = true;
             }
@@ -52,12 +45,10 @@ public class Application {
         return true;
     }
 
-
     private static boolean checkInputNumber(String input) {
         return input.matches("^[1-9]{3}$") && input.chars().distinct().count() == 3;
     }
 
-    // 3스트라이크 = true, 그의 모두 false(반복돌리기)
     private static boolean printResult(int ball, int strike) {
         if (strike == 3) {
             System.out.println(strike + "스트라이크\n" + "3개의 숫자를 모두 맞히셨습니다! 게임 종료");
@@ -118,8 +109,8 @@ public class Application {
         noCheck = new int[3];
         for (int i = 0; i < 3; i++) {
             if (computerNumber.get(i) == Integer.parseInt(String.valueOf(input.charAt(i)))) {
-                strikeCount++;
-                noCheck[i] = 1;
+                    strikeCount++;
+                    noCheck[i] = 1;
             }
         }
         return strikeCount;
