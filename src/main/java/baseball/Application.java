cae1a29a6423e90d2bec075fd9ff49dab356a81f
chan-won <clw8679@naver.com>
2023-10-23 23:14:11 +0900

    Fix: Application 오류 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,10 +9,9 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         Output.printGameStart();
-
-        List<Integer> computer = RandomNumberGenerator.generate();
         boolean gameEndFlag = true;
         while(gameEndFlag) {
+            List<Integer> computer = RandomNumberGenerator.generate();
             while (true) {
                 List<Integer> user = Input.Numbers();
 
