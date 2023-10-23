70e0197fb398b5b57508b475b612035a2ab5d442
unknown <t0paz_2357@naver.com>
2023-10-23 16:01:43 +0900

    Application.java Update

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,62 +6,114 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
+
+	public static String RESTART = "1";
+	public static String THEEND = "2";
+
 	public static void main(String[] args) throws IllegalArgumentException {
-		try {
-			System.out.println("숫자 야구 게임을 시작합니다.");
-			
-			List<Integer> computer = new ArrayList<>();
-			// 컴퓨터의 랜덤 3개 숫자를 저장할 List
-			
-			while (computer.size() < 3) { // 저장한 숫자 개수가 3개 미만일 때
-				int randNum = Randoms.pickNumberInRange(1, 9);
-				// 1 ~ 9 까지의 숫자 중 하나를 랜덤으로 선택
-				
-				if(!computer.contains(randNum)) {
-					computer.add(randNum);
-				} // 컴퓨터 숫자 리스트에 랜덤 숫자가 없다면 리스트에 추가
-			} // while end
-			// 컴퓨터가 랜덤 숫자 3개를 저장함
-			
-			String userInput = Console.readLine();
-			// 사용자가 숫자 3개를 입력함
-			
-			List<Integer> userNum = new ArrayList<>();
-			// 사용자가 입력한 숫자를 저장할 리스트
-			
-			int ball = hitBall(computer, userNum); // 볼의 개수
-			int strike = hitStrike(computer, userNum); // 스트라이크의 개수
-			
-			if (ball == 0 && strike == 0)
-				hitNothing();
-			else if (ball == 0 && strike == 3) {
-				System.out.println("" + strike + "스트라이크");
-				System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-				return;
-			} else if ()
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
+		System.out.println("숫자 야구 게임을 시작합니다.");
+
+		String restart = RESTART;
+		boolean flag = true;
+
+		while (restart.equals(RESTART)) {
+			List<Integer> comNumList = comNumList();
+			List<Integer> userNumList = null;
+
+			flag = true;
+
+			while (flag) {
+				System.out.print("숫자를 입력해주세요 : ");
+				userNumList = userNumList();
+				String message = countHit(comNumList, userNumList);
+
+				if (message.equals("3스트라이크")) {
+					System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+					System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+					flag = false;
+					restart = Console.readLine();
+
+					if (restart.equals(RESTART))
+						break;
+					else if (restart.equals(THEEND))
+						return;
+					else
+						throw new IllegalArgumentException();
+				} else {
+					System.out.println(message);
+				}
+			}
 		}
 	}
 
-	public static int hitBall(List com, List user) { // 볼 계산
-		int ball = 0;
-		
-		return ball;
+	public static List<Integer> comNumList() {
+		List<Integer> comNum = new ArrayList<>();
+
+		while (comNum.size() < 3) {
+			int randNum = Randoms.pickNumberInRange(1, 9);
+
+			if (!comNum.contains(randNum)) {
+				comNum.add(randNum);
+			}
+		}
+
+		return comNum;
 	}
 
-	public static int hitStrike(List com, List user) { // 스트라이크 계산
+	public static List<Integer> userNumList() throws IllegalArgumentException {
+		String[] userInput = Console.readLine().split("");
+		List<Integer> userNum = new ArrayList<>();
+
+		if (!userNum.isEmpty())
+			userNum.removeAll(userNum);
+
+		for (int i = 0; i < 3; i++) {
+			userNum.add(Integer.parseInt(userInput[i]));
+		}
+
+		if (userNum.size() > 3)
+			throw new IllegalArgumentException();
+
+		return userNum;
+	}
+
+	public static int countContain(List<Integer> com, List<Integer> user) {
+		int contain = 0;
+
+		for (int i = 0; i < 3; i++) {
+			if (com.contains(user.get(i)))
+				contain++;
+		}
+
+		return contain;
+	}
+
+	public static int countStrike(List<Integer> com, List<Integer> user) {
 		int strike = 0;
-		
+
 		for (int i = 0; i < 3; i++) {
 			if (com.get(i) == user.get(i))
 				strike++;
 		}
-		
+
 		return strike;
 	}
 
-	public static void hitNothing() {
-		System.out.println("낫싱");
+	public static String countHit(List<Integer> com, List<Integer> user) {
+		int contain = countContain(com, user);
+		int strike = countStrike(com, user);
+		int ball = contain - strike;
+
+		if (contain == 0) {
+			return "낫싱";
+		} else if (ball == 0) {
+			return strike + "스트라이크";
+		} else if (strike == 0) {
+			return ball + "볼";
+		} else if (ball < 3 && strike < 3) {
+			return ball + "볼 " + strike + "스트라이크";
+		} else
+			throw new IllegalArgumentException();
 	}
 }
