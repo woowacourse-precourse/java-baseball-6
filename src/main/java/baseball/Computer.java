170387f6dc7d3764052528e88704f59ee8cb7682
gonixxxx <akdntm946@gmail.com>
2023-10-23 22:08:16 +0900

    feat: 최종 결과 반환

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,18 @@
+package baseball;
+
+import java.util.List;
+
+public class Computer {
+
+    private final GamePrint gamePrint = new GamePrint();
+    private final StrikeBallCount strikeBallCount = new StrikeBallCount();
+
+    public boolean compareNumber(List<Integer> computer, List<Integer> user) {
+        int strike = strikeBallCount.countStrike(computer, user);
+        int ball = strikeBallCount.countBall(computer, user);
+        gamePrint.printGameResult(strike, ball);
+
+        return strike == 3;
+    }
+
+}
