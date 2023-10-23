61c66956bd794ba34efa2040588f09596ae8aeff
songsunkook <songsunkook@gmail.com>
2023-10-23 11:27:25 +0900

    feat: BaseballController 골격 생성

---
+++ src/main/java/controller/BaseballController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import view.InputView;
+import view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseballController {
+
+    public void initGame() {
+
+    }
+
+    public void playGame() {
+        
+    }
+
+    private void playRound() {
+
+    }
+
+    public boolean restartGame() {
+        return false;
+    }
+}
