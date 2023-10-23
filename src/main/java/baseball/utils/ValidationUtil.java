ab28748b14f1d59d7f58e3a5b7e9bc92d39cf81c
HyungUk Ryu <hw0603@naver.com>
2023-10-23 21:13:23 +0900

    Refactor: ValidationUtil 매직넘버 상수화

--- src/main/java/baseball/utils/ValidationUtil.java
+++ src/main/java/baseball/utils/ValidationUtil.java
@@ -4,8 +4,11 @@ import java.util.HashSet;
 
 public class ValidationUtil {
     private final HashSet<Character> charSet = new HashSet<>();
+    private static final int INPUT_LENGTH = 3;
+    private static final char NUMBER_MIN = '1';
+    private static final char NUMBER_MAX = '9';
     private static final String LENGTH_MISMATCH = "세 자리로 입력해 주세요.";
-    private static final String RANGE_MISMATCH = "1이상 9 이하 숫자로 입력해 주세요.";
+    private static final String RANGE_MISMATCH = NUMBER_MIN+" 이상"+NUMBER_MAX+"이하 숫자로 입력해 주세요.";
     private static final String DUPLICATE_EXIST = "중복되는 숫자가 존재합니다.";
 
     public void validate(String userInput) throws IllegalArgumentException {
@@ -19,13 +22,13 @@ public class ValidationUtil {
     }
 
     private void checkLength(String userInput) {
-        if (userInput.length() != 3) {
+        if (userInput.length() != INPUT_LENGTH) {
             throw new IllegalArgumentException(LENGTH_MISMATCH);
         }
     }
 
     private void checkRange(char ch) {
-        if (ch < '1' || ch > '9') {
+        if (ch < NUMBER_MIN || ch > NUMBER_MAX) {
             throw new IllegalArgumentException(RANGE_MISMATCH);
         }
     }
