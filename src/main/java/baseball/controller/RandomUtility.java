a5665d8a483f178a3f916a4ccc081ba627f114ae
isyoo1213 <moethns@gmail.com>
2023-10-23 19:29:07 +0900

    refactor: 난수생성 관련 메서드 수정 RandomUtility - 기존 필드변수였던 answerNumbers 배열을 generateAnswerNumbers()의   지역변수로 수정 - generateRandonNumber()가 answerNumbers를 인자로 받을 수 있도록 추가

--- src/main/java/baseball/controller/RandomUtility.java
+++ src/main/java/baseball/controller/RandomUtility.java
@@ -2,28 +2,26 @@ package baseball.controller;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.Arrays;
 
 public class RandomUtility {
     private static final int ANSWER_LENGTH = 3;
     private static final int RANDOM_NUMBER_START = 1;
     private static final int RANDOM_NUMBER_END = 9;
-    int[] answerNumbers = new int[ANSWER_LENGTH];
 
     public int[] generateAnswerNumbers() {
+        int[] answerNumbers = new int[ANSWER_LENGTH];
         for (int i = 0; i < answerNumbers.length; i++) {
-            answerNumbers[i] = generateRandomNumber();
+            answerNumbers[i] = generateRandomNumber(answerNumbers);
         }
         return answerNumbers;
     }
 
-    private int generateRandomNumber() {
+    private int generateRandomNumber(int[] answerNumbers) {
         while (true) {
             int randomNumber = Randoms.pickNumberInRange(RANDOM_NUMBER_START, RANDOM_NUMBER_END);
             if (isNotDuplicatedNumber(answerNumbers, randomNumber)) {
                 return randomNumber;
             }
-
         }
     }
 
