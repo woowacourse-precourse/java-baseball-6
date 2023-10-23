8bb7223b0cd2c175027f67959e3eba6b6cc01c80
chooh <chocomilk66@naver.com>
2023-10-23 19:25:40 +0900

    feat: 게임이 끝난 후 게임 다시 시작 또는 종료 선택 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,8 +5,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
         while (true) {
             ArrayList<Integer> numbers = new ArrayList<>();
@@ -59,6 +59,13 @@ public class Application {
                 }
             }
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String endInput = Console.readLine();
+            if ("2".equals(endInput)) {
+                break;
+            } else if (!"1".equals(endInput)) {
+                throw new IllegalArgumentException("잘못된 입력입니다.");
+            }
         }
     }
 }
