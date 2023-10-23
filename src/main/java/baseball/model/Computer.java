75570e7e99f64ef2464e46c2acbbccc062981fe4
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2023-10-24 02:14:41 +0900

    feat: 컴퓨터 숫자 생성 기능
    createRandomNumbers 메서드를 통해 각 자릿수가 다른 random한 3자리 숫자를 리스트로 생성

---
+++ src/main/java/baseball/model/Computer.java
@@ -0,0 +1,28 @@
+package baseball.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+
+    private List<Integer> numbers;
+
+    private Computer(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public static Computer createRandomNumbers() {
+        List<Integer> createNumbersList = new ArrayList<>();
+        while (createNumbersList.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if(!createNumbersList.contains(randomNumber)) {
+                createNumbersList.add(randomNumber);
+            }
+        }
+
+        return new Computer(createNumbersList);
+    }
+
+}
