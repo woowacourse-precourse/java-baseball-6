28a45b88a7881fd0deec874a9c1783beb4432dba
JaehongDev <println@kakao.com>
2023-10-23 21:43:26 +0900

    refactor: ViewContainer에 등록할 View추가

--- src/main/java/baseball/view/core/ViewContainer.java
+++ src/main/java/baseball/view/core/ViewContainer.java
@@ -1,25 +1,28 @@
 package baseball.view.core;
 
+import baseball.event.EventListener;
 import baseball.view.part.GameStartView;
+import baseball.view.part.InitializeComputerView;
+import baseball.view.part.InitializeUserView;
+import baseball.view.part.RefereeView;
+import baseball.view.part.RetryGameView;
 import java.util.HashMap;
 import java.util.Map;
 
 public final class ViewContainer {
-    private final InputView inputView;
-    private final OutputView outputView;
-
     private final Map<ViewType, DefaultView> viewMap;
 
     public ViewContainer(
             InputView inputView,
-            OutputView outputView
+            OutputView outputView,
+            EventListener eventListener
     ) {
-        this.inputView = inputView;
-        this.outputView = outputView;
-
         this.viewMap = new HashMap<>() {{
             this.put(ViewType.GAME_START, new GameStartView(outputView));
-
+            this.put(ViewType.COMPUTER_INIT, new InitializeComputerView(eventListener));
+            this.put(ViewType.PLAYER_INIT, new InitializeUserView(inputView, eventListener));
+            this.put(ViewType.REFEREE, new RefereeView(eventListener, outputView));
+            this.put(ViewType.RETRY, new RetryGameView(inputView));
         }};
     }
 
