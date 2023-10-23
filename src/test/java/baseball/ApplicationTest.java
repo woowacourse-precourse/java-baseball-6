38f6f609e16658c7d3fcd76e0546a9c1c172b1c6
kkkkimtaehyeon <kth1927@naver.com>
2023-10-24 02:32:42 +0900

    23.10.24 02:21

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,7 +1,9 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Nested;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -19,7 +21,6 @@ class ApplicationTest extends NsTest {
                 1, 3, 5, 5, 8, 9
         );
     }
-
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
@@ -33,3 +34,34 @@ class ApplicationTest extends NsTest {
         Application.main(new String[]{});
     }
 }
+
+@DisplayName("예외처리테스트")
+class exceptionTest{
+    @DisplayName("자연수가 아닌 숫자")
+    @Test
+    void 자연수_아닌_숫자() {
+        String input = "김태현";
+        assertThatThrownBy(() -> Application.isNotDigitNumber(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("중복된 숫자 입력")
+    @Test
+    void 중복된_숫자_입력(){
+        String input = "333";
+        assertThatThrownBy(() -> Application.isSameNumber(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @Nested
+    @DisplayName("1이나 2가 아닌 재시작 번호")
+    @Test
+    void 잘못된_재시작_번호(){
+        int retryNum = 3;
+        assertThatThrownBy(() -> Application.isCorrectRetryNumber(retryNum))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
+
+
+
+
