64859162ba639d51cc39e42af985cdffef18ada4
Gyunseo Lee <rbstj0311@naver.com>
2023-10-24 02:05:15 +0900

    feat: implement core game play feature - Coputer class를 구현했습니다. - coputer instance의 메서드를 이용해 코어 게임 플레이 기능을 구현했습니다. docs: add core game play feature details - 코어 게임 플레이 기능 설명을 적었습니다.

--- docs/README.md
+++ docs/README.md
@@ -7,4 +7,20 @@
 - ### 게임 종료 조건이 충족되면, `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.` 문구가 출력되고, 사용자는 다음 줄에서 `1` 또는 `2`를 입력한다.
 - ### 사용자가 입력하는 값은 `camp.nextstep.edu.missionutils.Console`의 `readLine()`을 활용한다.
 - ### 사용자가 잘못된 값을 입력할 경우 `IllegalArgumentException`을 발생시킨 후 애플리케이션은 종료되어야 한다.
-    - 이때 `isUserInputValidInGame()`과 `isUserInputValidAfterGame()` 메서드를 활용한다.
+    - 이때 `User` class의 `isUserInputValidInGame()`과 `isUserInputValidAfterGame()` 메서드를 활용한다.
+
+## 2. 코어 게임 플레이 기능
+
+- ### 같은 수가 같은 자리에 있으면 스트라이크, 다른 자리에 있으면 볼, 같은 수가 전혀 없으면 낫싱이란 힌트를 얻고, 그 힌트를 이용해서 먼저 상대방(컴퓨터)의 수를 맞추면 승리한다.
+    - 예) 상대방(컴퓨터)의 수가 425일 때
+        - 123을 제시한 경우 : 1스트라이크
+        - 456을 제시한 경우 : 1볼 1스트라이크
+        - 789를 제시한 경우 : 낫싱
+    - `Computer` class의 `getUserGuessResult()`와 `printUserGuessResult()` 메서드를 활용한다.
+- ### 위 숫자 야구 게임에서 상대방의 역할을 컴퓨터가 한다.
+    - `Computer` class를 활용한다.
+- ### 컴퓨터는 1에서 9까지 서로 다른 임의의 수 3개를 선택한다.
+    - `Computer` class의 생성자를 활용한다.
+- ### 게임 플레이어(사용자)는 컴퓨터가 생각하고 있는 서로 다른 3개의 숫자를 입력하고, 컴퓨터는 입력한 숫자에 대한 결과를 출력한다.
+    - `User` class의 `getUserInput()`과 `Computer` Class의 `printUserGuessResult()` 메서드를 활용한다.
+- ### 이 같은 과정을 반복해 컴퓨터가 선택한 3개의 숫자를 모두 맞히면 게임이 종료된다.
--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,13 +1,43 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+class Pair<T, U> {
+    private final T first;
+    private final U second;
+
+    public Pair(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public U getSecond() {
+        return second;
+    }
+}
 
 class User {
     private String userInput;
-    private boolean winning = false;
+    private boolean winning;
+
+    public User() {
+        this.userInput = "";
+        this.winning = false;
+    }
 
     public void userWins() {
         winning = true;
+    }
+
+    public void userLoses() {
+        winning = false;
     }
 
     public boolean didUserWin() {
@@ -60,13 +90,54 @@ class User {
     }
 }
 
+class Computer {
+    private final List<Integer> numbers;
+
+    public Computer() {
+        this.numbers = new ArrayList<>();
+        // 1~9까지 서로 다른 임의의 수 3개를 생성하여 저장
+        while (numbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+    }
+
+    public Pair<Integer, Integer> getUserGuessResult(String userGuess) {
+        int strike = 0;
+        int ball = 0;
+        for (int i = 0; i < userGuess.length(); i++) {
+            int userGuessNumber = userGuess.charAt(i) - '0';
+            if (numbers.get(i) == userGuessNumber) {
+                strike++;
+            } else if (numbers.contains(userGuessNumber)) {
+                ball++;
+            }
+        }
+        return new Pair<Integer, Integer>(strike, ball);
+    }
+
+    public void printUserGuessResult(Pair<Integer, Integer> userGuessResult) {
+        if (userGuessResult.getFirst() == 0 && userGuessResult.getSecond() == 0) {
+            System.out.printf("낫싱\n");
+        } else if (userGuessResult.getFirst() == 0) {
+            System.out.printf("%d볼\n", userGuessResult.getSecond());
+        } else if (userGuessResult.getSecond() == 0) {
+            System.out.printf("%d스트라이크\n", userGuessResult.getFirst());
+        } else {
+            System.out.printf("%d스트라이크 %d볼\n", userGuessResult.getFirst(), userGuessResult.getSecond());
+        }
+    }
+
+}
 
 public class Application {
 
 
     public static void main(String[] args) {
-        // 3스트라이크가 되면, userWins가 true가 되며 게임이 종료된다.
         User user = new User();
+        Computer computer = new Computer();
         System.out.printf("숫자 야구 게임을 시작합니다.\n");
         while (!user.didUserWin()) {
             try {
@@ -75,20 +146,27 @@ public class Application {
                 if (!user.isUserInputValidInGame()) {
                     throw new IllegalArgumentException();
                 }
-                System.out.printf("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n");
-                System.out.printf("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
-                user.readUserInput();
-                if (!user.isUserInputValidAfterGame()) {
-                    throw new IllegalArgumentException();
-                }
-                switch (user.getUserInput()) {
-                    case "1":
-                        continue;
-                    case "2":
-                        break;
-                    default:
+                Pair<Integer, Integer> guessResult = computer.getUserGuessResult(user.getUserInput());
+                computer.printUserGuessResult(guessResult);
+                if (guessResult.getFirst() == 3) {
+                    user.userWins();
+                    System.out.printf("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n");
+                    System.out.printf("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+                    user.readUserInput();
+                    if (!user.isUserInputValidAfterGame()) {
                         throw new IllegalArgumentException();
+                    }
+                    switch (user.getUserInput()) {
+                        case "1":
+                            user.userLoses();
+                            continue;
+                        case "2":
+                            continue;
+                        default:
+                            throw new IllegalArgumentException();
+                    }
                 }
+
             } catch (IllegalArgumentException error) {
                 user.userWins();
                 continue;
