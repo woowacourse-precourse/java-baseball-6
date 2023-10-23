8f2a03612741f10cd5c771d683e96ac2c1c03577
Bosub Kim <grapeflavoredapple@gmail.com>
2023-10-23 14:49:24 +0900

    feat - 입력 예외 처리 기능 구현 (#4)
    - 입력시 올바르지 않은 입력이 들어오면 `IlligalArgumentException`을 발생시키도록 Input 수정
    - 기존에 존재하는 테스트 코드는 모두 통과하도록 구현함

--- src/main/java/baseball/console/Input.java
+++ src/main/java/baseball/console/Input.java
@@ -6,11 +6,35 @@ public class Input {
 
     public static String getNumbers() {
         System.out.print("숫자를 입력해주세요 : ");
-        return Console.readLine();
+        String input;
+
+        try {
+            input = Console.readLine();
+        } catch (Exception e) {
+            throw new IllegalArgumentException("올바르지 않은 입력입니다.");
+        }
+
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("올바르지 않은 입력입니다.");
+        }
+
+        return input;
     }
 
     public static String chooseRestart() {
         System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
-        return Console.readLine();
+        String input;
+
+        try {
+            input = Console.readLine();
+        } catch (Exception e) {
+            throw new IllegalArgumentException("올바르지 않은 입력입니다.");
+        }
+
+        if (input.length() != 1) {
+            throw new IllegalArgumentException("올바르지 않은 입력입니다.");
+        }
+
+        return input;
     }
 }
