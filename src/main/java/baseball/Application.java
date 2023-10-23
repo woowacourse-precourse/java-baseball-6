fdfe1afa7a75386c0ba7cc040e6a0841c7bb8a2c
stats-dev <hohun94@naver.com>
2023-10-23 20:04:02 +0900

    fix: 불필요한 import Scanner 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,10 @@
 package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
-import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public int strike = 0;
