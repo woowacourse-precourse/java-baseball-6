8a6d73704f79f8bd7671da280da7c36d17f4ddcf
jiohh <dkswlgh99@naver.com>
2023-10-24 02:18:18 +0900

    refactor:태스트 클래스 리팩토링

--- src/test/java/baseball/NoduleTest/UserNumberTest.java
+++ src/test/java/baseball/NoduleTest/UserNumberTest.java
@@ -1,20 +1,13 @@
-package baseball;
+package baseball.NoduleTest;
 
+import baseball.UserNumber;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class ModuleTest {
-
-    @Test
-    void 컴퓨터_랜덤_숫자_확인() {
-        ComputerNumber computerNumberService = new ComputerNumber();
-        List<Integer> computerNumber = computerNumberService.createComputerNumber();
-        System.out.println("computerNumber = " + computerNumber);
-        Assertions.assertEquals(computerNumber.size(), 3);
-    }
-
+public class UserNumberTest {
+    
     @Test
     void 입력값_길이_확인() {
         UserNumber userNumber = new UserNumber();
