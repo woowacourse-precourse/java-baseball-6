7b07f6b85a3fa44cd0d8279c9225925a617fda34
h3136514 <3136514@naver.com>
2023-10-23 16:58:37 +0900

    feat(computer): 볼인지 확인하는 함수를 생성한다.

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -13,22 +13,24 @@ public class BaseBallGame {
 
     //상대방(컴퓨터)의 수 생성 함수
     public void RandomNum() {
-        computer =new ArrayList<>();
-        while(computer.size() < 3){
-            int randomNumber =Randoms.pickNumberInRange(1, 9);
-            if(!computer.contains(randomNumber)){
+        computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
         }
     }
-    public BaseBallGame(){
+
+    public BaseBallGame() {
         RandomNum();
         Play();
     }
+
     //게임 진행 함수
-    public void Play(){
+    public void Play() {
         StartGamePrint();
-        while(cycle) {
+        while (cycle) {
             Reset();
             StartPrint();
             userString = InputNum();
@@ -37,68 +39,91 @@ public class BaseBallGame {
             IsOverlap(userInteger);
             IsZero();
             IsStrike();
+            IsBall();
         }
         StopGamePrint();
     }
+
     //게임 시작 알림 함수
-    public void StartGamePrint(){
+    public void StartGamePrint() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
     //게임 종료 알림 함수
-    public void StopGamePrint(){
+    public void StopGamePrint() {
         System.out.print("게임 종료");
     }
+
     //숫자 입력 알림 함수
-    public void StartPrint(){
+    public void StartPrint() {
         System.out.print("숫자를 입력해주세요 : ");
     }
+
     //플레이어 숫자 입력 함수
-    public String InputNum(){
+    public String InputNum() {
         String player = Console.readLine();
         return player;
     }
+
     // 입력값 길이 판별 함수
-    public void IsLength(String s){
-        if(s.length() !=3){
+    public void IsLength(String s) {
+        if (s.length() != 3) {
             throw new IllegalArgumentException("세자리 수가 아닙니다.");
         }
     }
+
     // 입력값 숫자 판별 함수
-    public void IsInteger(){
-        try{
+    public void IsInteger() {
+        try {
             userInteger = Integer.parseInt(userString);
-        }catch(NumberFormatException ex){
+        } catch (NumberFormatException ex) {
             throw new IllegalArgumentException("숫자가 아닙니다.");
         }
     }
+
     //중복 확인 함수
-    public void IsOverlap(int n){
+    public void IsOverlap(int n) {
         user = new ArrayList<>();
-        while(user.size() < 3){
-            int userNumber = n%10;
-            n=n/10;
+        while (user.size() < 3) {
+            int userNumber = n % 10;
+            n = n / 10;
             if (user.contains(userNumber)) {
                 throw new IllegalArgumentException("같은 숫자가 중복된 값입니다.");
             }
             user.add(userNumber);
         }
     }
+
     // 입력값 1~9숫자인지 판별 함수
     public void IsZero() {
         if (user.contains(0)) {
             throw new IllegalArgumentException("1~9 숫자 이외의 값입니다.");
         }
     }
+
     //점수 초기화 함수
-    public void Reset(){
+    public void Reset() {
         strike = 0;
         ball = 0;
     }
+
     //스트라이크 개수 확인
-    public void IsStrike(){
-        for(int i=0;i<user.size();i++){
-            if(user.get(i)==computer.get(i)){
+    public void IsStrike() {
+        for (int i = 0; i < user.size(); i++) {
+            if (user.get(i) == computer.get(i)) {
                 strike++;
             }
         }
     }
+    //볼 개수 확인
+    public void IsBall(){
+        for(int i=0;i<user.size();i++) {
+            int target = user.get(i);
+            if(target !=computer.get(i)){
+                if(computer.contains(target)){
+                    ball++;
+                }
+            }
+        }
+    }
+}
