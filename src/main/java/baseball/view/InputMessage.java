643f6ffe2179c05e4488a9803c99704e39c4253e
win9 <krrr883@gmail.com>
2023-10-24 05:13:06 +0900

    [Feat]: 메시지 클래스 생성
    게임 진행시 출력되는 메시지의 입, 출력을 담당

---
+++ src/main/java/baseball/view/InputMessage.java
@@ -0,0 +1,7 @@
+package baseball.view;
+
+public class InputMessage {
+    public static void printUserInputMessage() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+}
---
+++ src/main/java/baseball/view/OutputMessage.java
@@ -0,0 +1,7 @@
+package baseball.view;
+
+public class OutputMessage {
+    public static void printStartMessage() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+}
