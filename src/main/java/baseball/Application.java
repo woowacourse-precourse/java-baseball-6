8056936fa7f1d060440824926d88f166db77d55e
sojungpp <sophie2458@naver.com>
2023-10-24 03:58:41 +0900

    feat: 3자리의 수 랜덤 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,28 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        // 게임 시작
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        // 랜덤 3자리의 수 생성
+        List<Integer> computerNums = new ArrayList<>();
+        while (computerNums.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNums.contains(randomNumber)) {
+                computerNums.add(randomNumber);
+            }
+        }
+
     }
 }
