b93e268558895ad5e0a8a074a072c0752121a18f
morenow <jjang9877@gmail.com>
2023-10-24 01:33:09 +0900

    fix: "낫싱" 메시지에 "스트라이크"가 들어가있던 오류 수정

--- src/main/java/baseball/constant/Message.java
+++ src/main/java/baseball/constant/Message.java
@@ -8,7 +8,7 @@ public final class Message {
     public final static String INPUT_MSG = "숫자를 입력해주세요 : ";
     public final static String BALL_MSG = "볼";
     public final static String STRIKE_MSG = "스트라이크";
-    public final static String NOTHING_MSG = "스트라이크";
+    public final static String NOTHING_MSG = "낫싱";
     public final static String GAME_FINISH_MSG = Number.GAME_LENGTH + "개의 숫자를 모두 맞히셨습니다! 게임 종료";
     public final static String GAME_RESTART_MSG = "게임을 새로 시작하려면 " + Number.RESTART_NUM +
             ", 종료하려면 " + Number.FINISH_NUM + "를 입력하세요.";
