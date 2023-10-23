d43a26663ceee5c8a81e705877ae9bea41e16df4
jjh <pang024@naver.com>
2023-10-23 23:50:10 +0900

    PlayerInputAccepter Class implemet.

---
+++ src/main/java/baseball/PlayerInputAccepter.java
@@ -0,0 +1,22 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+import camp.nextstep.edu.missionutils.*;
+public class PlayerInputAccepter {
+	String ReadNum;
+	List<Integer> PlayerInput;
+	public void InputUserNum(String input) {
+		List<Integer> PlayerInput = new ArrayList<>();
+		String[] SplitInput = input.split("");
+		for(String s : SplitInput) {
+			PlayerInput.add(Integer.parseInt(s));
+		}
+		this.PlayerInput = PlayerInput;
+	}
+	
+	public void ReadNumber() {
+		ReadNum = Console.readLine();
+	}
+}
+
