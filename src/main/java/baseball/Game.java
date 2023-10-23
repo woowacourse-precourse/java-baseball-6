a835b1188bfb5ec735a65aef49a3122f06afd6c3
KimSehoon <shggm2000@gmail.com>
2023-10-24 04:08:47 +0900

    feat: compareNumbers method 구현
    - userNumberList와 answerNumberList를 비교해서 ball과 strike 개수를 판별하는 compareNumbers method 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,12 +1,28 @@
 package baseball;
 
 import baseball.domain.Computer;
+import baseball.domain.User;
+import java.util.List;
 
 public class Game {
     Computer computer = new Computer();
+    User user = new User();
 
     public void startGame() {
         View.printStartGameMessage();
-        computer.createAnswerNumberList();
+        List<Integer> answerNumberList = computer.createAnswerNumberList();
+    }
+
+    private void compareNumbers(List<Integer> answerNumberList, List<Integer> userNumberList) {
+        int ball = 0;
+        int strike = 0;
+
+        for (int oneDigit = 0; oneDigit < answerNumberList.size(); oneDigit++) {
+            if (answerNumberList.get(oneDigit).equals(userNumberList.get(oneDigit))) {
+                strike++;
+            } else if (answerNumberList.contains(userNumberList.get(oneDigit))) {
+                ball++;
+            }
+        }
     }
 }
