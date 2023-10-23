a829fb03278bb59956cf3d841090778a2fcdf94e
inpink <inpink@kakao.com>
2023-10-24 04:52:34 +0900

    refactor: Baseball Model에서 Baseball 생성 역할을 맡음.
    웹 구조를 생각한다면 HttpServlet으로부터 데이터를 받아서 Controller에서 생성해줄 수 있음.
    하지만 현재 우리 구조에서는 Model에서 Baseball을 생성하는 것이 가장 적절하다 판단.
    그 이유는 Controller에서 Baseball을 생성할 경우 Controller끼리 서로 호출해야 하며 의존관계가 꼬임.

--- src/main/java/baseball/domain/Baseball.java
+++ src/main/java/baseball/domain/Baseball.java
@@ -1,16 +1,36 @@
 package baseball.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Baseball<T> {
     protected List<T> values;
 
-    public Baseball() {
+    private Baseball() { //생성자 대신 정적 팩터리 메서드 사용
 
     }
 
-    public Baseball(List<T> values) {
+    private Baseball(List<T> values) {
         this.values = values;
+    }
+
+    public static <T> Baseball<T> createBaseball(List<T> values) {
+        return new Baseball<>(values);
+    }
+
+    public static Baseball createRandomBaseball() { //정적 팩터리 메서드
+        List<Integer> computerList = new ArrayList<>();
+
+        while (computerList.size() < GameConstants.NUMBER_LENGTH) {
+            int randomNumber = Randoms.pickNumberInRange(GameConstants.MIN_DIGIT, GameConstants.MAX_DIGIT);
+            if (!computerList.contains(randomNumber)) {
+                computerList.add(randomNumber);
+            }
+        }
+
+        return createBaseball(computerList);
     }
 
     public List<T> getValues() {
@@ -20,4 +40,7 @@ public class Baseball<T> {
     public void setValues(List<T> values) {
         this.values = values;
     }
+
+
+
 }
