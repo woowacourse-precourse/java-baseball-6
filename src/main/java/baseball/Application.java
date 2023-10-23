1d377b8bb3f1740f383b3bcf60e91aeb6e2f743e
Kite_Z <duswl339@ajou.ac.kr>
2023-10-23 18:23:45 +0900

    Feat: Generate Baseball Number
    Generate Computer's baseball Number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,9 +6,14 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        System.out.print("숫자 야구 게임을 시작합니다.");
+        System.out.println("숫자 야구 게임을 시작합니다.");
 
-        // TODO: Make Computer Baseball Number
+        StringBuilder computerNumber = new StringBuilder();
+        for(int i = 0; i < 3 ; i++) {
+            computerNumber.append(Randoms.pickNumberInRange(1, 9));
+        }
+
+        System.out.println(computerNumber);
 
         // TODO: Input User Expect Number
 
