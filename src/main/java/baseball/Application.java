10f4a1ea4a66eabf7619f6b7d9c8ea9ef7d23fb0
gyu_hyeok <jjookki33@gmail.com>
2023-10-23 17:48:40 +0900

    feat/사용자 입력에 대한 예외처리 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,7 +13,7 @@ public class Application {
 //      System.out.println(computer);
         System.out.println("서로다른 숫자를 입력해 주세요 : ");
         List<String> playerNumber = playerInputNumber();
-//      validatePlayerNumber(playerNumber);
+        validatePlayerNumber(playerNumber);
     }
 
     private static final int MIN_NUMBER = 1;
@@ -38,5 +38,45 @@ public class Application {
         );
     }
 
+    private static void validatePlayerNumber(List<String> playerNum){
+        isThreeNumbers(playerNum);
+        isRepeatedNumbers(playerNum);
+        isDigitNumbers(playerNum);
+        checkZero(playerNum);
+    }
 
+    private static void isThreeNumbers(List<String> playerNum){
+        if(playerNum.size() != BASEBALL_SIZE){
+            throw new IllegalArgumentException("입력된 숫자가 3자리가 아닙니다. 게임을 종료합니다");
+        }
+    }
+
+    private static void isRepeatedNumbers(List<String> playerNum){
+        Set<String> numset = new HashSet<>();
+        for(String num : playerNum){
+            numset.add(num);
+        }
+        if(numset.size() != BASEBALL_SIZE){
+            throw new IllegalArgumentException("중복된 값이 존재합니다. 게임을 종료합니다.");
+        }
+    }
+
+    private static void isDigitNumbers(List<String> playerNum){
+        try{
+            for(String number : playerNum){
+                Integer.parseInt(number);
+            }
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException("잘못된 값이 입력되었습니다. 게임을 종료합니다.");
+        }
+    }
+
+    private static void checkZero(List<String> playerNum){
+        for(String number : playerNum){
+            int num = Integer.parseInt(number);
+            if(num == 0){
+                throw new IllegalArgumentException("입력된 값에 0이 포함되어 있습니다. 게임을 종료합니다.");
+            }
+        }
+    }
 }
