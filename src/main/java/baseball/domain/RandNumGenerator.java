82e489d2eb710925697c55f6418a3351abd38bc6
Hyoseop Song <crad_on25@naver.com>
2023-10-23 13:23:41 +0900

    feat(RandNumGenerator): makeRandNumInRange 메소드 구현

--- src/main/java/baseball/domain/RandNumGenerator.java
+++ src/main/java/baseball/domain/RandNumGenerator.java
@@ -1,9 +1,15 @@
 package baseball.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RandNumGenerator {
     public static final int START_INCLUSIVE = 1;
     public static final int END_INCLUSIVE = 9;
 
     private RandNumGenerator() {
+    }
+
+    private static int makeRandNumInRange() {
+        return Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
     }
 }
