4c34abc2852c2125a5bf7ecd08ef9cbd208b38c4
YunSeok-Choi <dbstjr9898@naver.com>
2023-10-23 22:22:07 +0900

    FEAT : ball, strike, notsink 출력 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,22 +7,12 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
 
-        // 랜덤값 생성
-        List<String> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            String randomNumber = String.valueOf(Randoms.pickNumberInRange(1, 9));
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
-        }
-
-        String computerNumber = String.join("",computer);
+        String computerNumber = shuffleNumber();
 
         System.out.print("숫자를 입력해 주세요 : ");
-
         String user = Console.readLine();
+
         if (user.length() != 3) {
             throw new IllegalArgumentException("3개의 값을 입력해주세요");
         }
@@ -37,5 +27,44 @@ public class Application {
             tmp += String.valueOf(c);
         }
 
+        int ball = 0;
+        int strike = 0;
+        for (int i = 0; i < computerNumber.length(); i++) {
+            int computerDigit = computerNumber.charAt(i);
+            int userDigit = user.charAt(i);
+
+            if (computerDigit - 1 == userDigit || computerDigit + 1 == userDigit) {
+                ball += 1;
+            }
+
+            if (computerDigit == user.charAt(i)) {
+                strike += 1;
+            }
+        }
+
+        if (ball != 0) {
+            System.out.print(ball + "볼 ");
+        }
+        if (strike != 0) {
+            System.out.println(strike + "스트라이크");
+        }
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+        }
+
+
+    }
+
+    public static String shuffleNumber() {
+        // 랜덤값 생성
+        List<String> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            String randomNumber = String.valueOf(Randoms.pickNumberInRange(1, 9));
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        return String.join("", computer);
     }
 }
