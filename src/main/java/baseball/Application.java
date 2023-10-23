3147bb79f629f13e23f218f2ed2ba2aea65a61ca
moomint8 <office05053@gmail.com>
2023-10-23 21:30:26 +0900

    2023/10/23 21:30 2차 개선

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,57 +2,59 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        mainLoop();
+    }
+
+    private static void mainLoop() {
         List<Integer> computer = new ArrayList<>(); // 정답
-        String strInput;    // String 형식 유저 값
-        List<Integer> intInput = new ArrayList<>(); // int List 형식의 유저 값
 
-        MainLoop:
         while (true) {
-            makeRandomList(computer);   // 랜덤한 서로 다른 세 자리 수 생성
+            makeRandomNumberList(computer);   // 랜덤한 서로 다른 세 자리 수 생성
             System.out.println("숫자 야구 게임을 시작합니다.");
+            subLoop(computer);
 
-            while (true) {
-                System.out.print("숫자를 입력해주세요 : ");
-                strInput = Console.readLine();  // 입력값
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");  // 종료 분기
+            String checkEnd = Console.readLine();
+            checkInputLength(checkEnd, 1);  // 자릿수 검사
+            checkInputRange(checkEnd, '1', '2');    // 값 범위 검사
 
-                checkInputLength(strInput, 3);  // 자릿수 검사
-                checkInputRange(strInput, '0', '9');    // 값 범위 검사
-                checkSameValue(strInput);   // 같은 값 존재 검사
-
-                strToIntList(intInput, strInput);   // int 로 변환 저장
-
-                int[] result = getResult(computer, intInput);   // {스트라이크, 볼, 낫싱}
-                printResult(result);    // 결과 출력
-
-                if (result[0] == 3) {   // 3스트라이크, 종료 분기
-                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-
-                    String checkEnd = Console.readLine();
-
-                    checkInputLength(checkEnd, 1);  // 자릿수 검사
-                    checkInputRange(checkEnd, '1', '2');    // 값 범위 검사
-
-                    if (checkEnd.charAt(0) == '1') {    // 재시작
-                        break;
-                    }
-                    if (checkEnd.charAt(0) == '2') {    // 종료
-                        break MainLoop;
-                    }
-                }
+            if (checkEnd.charAt(0) == '2') {    // 종료
+                break;
             }
         }
         Console.close();
     }
 
-    private static void makeRandomList(List list) { // 랜덤한 서로 다른 세 자리 수 생성
+    private static void subLoop(List computer) {
+        String strInput;    // String 형식 유저 값
+        List<Integer> intInput = new ArrayList<>(); // int List 형식의 유저 값
+
+        while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
+            strInput = Console.readLine();  // 입력값
+
+            checkInputLength(strInput, 3);  // 자릿수 검사
+            checkInputRange(strInput, '0', '9');    // 값 범위 검사
+            checkSameValue(strInput);   // 같은 값 존재 검사
+            strToIntList(intInput, strInput);   // int 로 변환 저장
+
+            int[] result = getResult(computer, intInput);   // {스트라이크, 볼, 낫싱}
+            printResult(result);    // 결과 출력
+
+            if (result[0] == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                return;
+            }
+        }
+    }
+
+    private static void makeRandomNumberList(List list) { // 랜덤한 서로 다른 세 자리 수 생성
         list.clear();   // 배열 초기화
         while (list.size() < 3) {   // 서로 다른 숫자 세 개 생성
             int randomNumber = Randoms.pickNumberInRange(1, 9);
