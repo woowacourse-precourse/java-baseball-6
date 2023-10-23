a150bcb4d6d32889754cc29de0c438bc2c1d0db0
dmong <dt2980_hee@naver.com>
2023-10-23 13:54:08 +0900

    feat: 게임 진행 중 사용자가 제시한 값에 대한 결과 판별 기능

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -1,4 +1,35 @@
 package baseball.service;
 
+import baseball.model.NumberFormat;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameService {
+
+    /**
+     *
+     * @return {strike 개수, ball 개수}
+     */
+    public static List<Integer> compareNumberFormat(NumberFormat answer, NumberFormat input){
+        int strikeCount = 0;
+        int ballCount = 0;
+
+        List<Integer> inputNumberList = input.getNumberList();
+        List<Integer> answerNumberList = answer.getNumberList();
+
+        for (int i = 0; i< inputNumberList.size(); i++) {
+            Integer value = inputNumberList.get(i);
+            if (value == answerNumberList.get(i)) strikeCount++;
+            else if (answerNumberList.contains(value)) ballCount++;
+        }
+
+        List<Integer> result = new ArrayList<>();
+        result.add(strikeCount);
+        result.add(ballCount);
+
+        return result;
+    }
+
+
 }
