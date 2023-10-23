369f67f94f16ff0e77f67ad8358b6b06f383ed25
yshong1998 <yshong1998@gmail.com>
2023-10-24 03:25:22 +0900

    Feat(InputView): 유저에게 입력을 요청하는 InputView 클래스 생성
    3자리 숫자를 입력받는 requestInput 메서드 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,4 +1,20 @@
 package baseball.view;
 
+import baseball.global.message.GameMessage;
+import baseball.global.validator.InputValidator;
+import camp.nextstep.edu.missionutils.Console;
+
+
 public class InputView {
+    private final InputValidator inputValidator;
+    InputView(){
+        this.inputValidator = new InputValidator();
+    }
+
+    public int requestInput(){
+        System.out.println(GameMessage.REQUEST_INPUT_MESSAGE);
+        String playerInput = Console.readLine();
+        inputValidator.validateBaseballNumberInput(playerInput);
+        return Integer.parseInt(playerInput);
+    }
 }
