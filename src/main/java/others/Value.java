265cb4e9bf83aea32fefb4b9620e9132f98f0202
Seungjida <esg0107@pusan.ac.kr>
2023-10-23 22:06:11 +0900

    Feat: "컴퓨터와 사용자의 숫자 비교 기능 추가"

--- src/main/java/others/Value.java
+++ src/main/java/others/Value.java
@@ -3,11 +3,14 @@ package others;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Value {
     private static final int numberOfNumbers = 3;
     private static final int minRange = 1;
     private static final int maxRange = 9;
+    private static int strike;
+    private static int ball;
 
     public static List<Integer> computerGenerateNum() {
         List<Integer> computer = new ArrayList<>();
@@ -27,5 +30,21 @@ public class Value {
             player.add(intNumber);
         }
         return player;
+    }
+
+    public static void compareDigits(final List<Integer> computer, final List<Integer> player) {
+        strike = 0;
+        ball = 0;
+        for (int i = 0; i < numberOfNumbers; i++) {
+            if (Objects.equals(computer.get(i), player.get(i))) {
+                strike++;
+            } else {
+                for (int j = 0; j < numberOfNumbers; j++) {
+                    if (i != j && Objects.equals(computer.get(j), player.get(i))) {
+                        ball++;
+                    }
+                }
+            }
+        }
     }
 }
