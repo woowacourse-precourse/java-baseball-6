b74496efc4cb720ab39df17edc8b8f9d2523ee63
“sangjun121” <juncho12011201@gmail.com>
2023-10-23 21:27:35 +0900

    feat: 게임 시작 안내문 출력 기능 구현
    UserView.printGameStartMessage() 함수 구현 및 GameManager에서 호출

--- src/main/java/baseball/controller/GameManager.java
+++ src/main/java/baseball/controller/GameManager.java
@@ -1,10 +1,12 @@
 package baseball.controller;
 
+import baseball.view.UserView;
 import java.util.List;
 
 public class GameManager {
     //인스턴스
     private Computer computer;
+    private UserView userView;
 
     //변수명
     private List<Integer> computerRandomNumbers;
@@ -12,9 +14,11 @@ public class GameManager {
     //생성자
     public GameManager(){
         computer = new Computer();
+        userView = new UserView();
     };
 
     public void startGame(){
-        computerRandomNumbers = computer.getRandomComputerNumber();
+        computerRandomNumbers = computer.getRandomComputerNumber(); //랜덤한 3개 컴퓨터 숫자 생성
+        userView.printGameStartMessage(); //게임시작 메시지 출력
     }
 }
---
+++ src/main/java/baseball/view/UserView.java
@@ -0,0 +1,9 @@
+package baseball.view;
+
+import static baseball.utility.Constants.GAME_START_MESSAGE;
+public class UserView {
+    public UserView(){};
+    public void printGameStartMessage(){
+        System.out.println(GAME_START_MESSAGE);
+    }
+}
