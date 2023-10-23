4f2761de820369314c1aa8b685a65dabb9a0b69e
Cycrypto <jh01love@naver.com>
2023-10-24 00:33:51 +0900

    feat:플레이어 입력에대한 유효성 검사

--- src/main/java/baseball/GamePlayer.java
+++ src/main/java/baseball/GamePlayer.java
@@ -1,2 +1,45 @@
-package baseball;public class GamePlayer {
+package baseball;
+
+import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
+enum GameStatus{
+        PLAYING, STOP
+}
+
+public class GamePlayer {
+    private String playerGuessedNumber;
+    private GameStatus playerStatus;
+    private Game game;
+
+    public GamePlayer(){
+        this.playerStatus = GameStatus.PLAYING;
+        this.game = new Game();
+    }
+    public void playGameByRequest(){
+        while(playerStatus == GameStatus.PLAYING){
+            this.playerGuessedNumber = Console.readLine();
+            if (isValidForm(this.playerGuessedNumber)){
+                this.playerStatus = game.comparePlayerNumberWithComputer(numberToList(this.guessedNumber));
+            }
+        }
+    }
+    public List<Integer> numberToList(String number){
+
+    }
+    public void sendNumberToGame(List<Integer> playerGuess){
+
+    }
+
+    public boolean isValidForm(String target){
+        boolean flag = true;
+        if (target.length() != 3){
+            flag = false;
+        }
+        for (int i = 0; i < target.length(); i++){
+          if (!Character.isDigit(target.charAt(i)))
+              flag = false;
+        }
+        return flag;
+    }
+
 }
