779503de9b75c20f790c6aa7fb68de429f2da4a6
Woo Joo Chae <enaenen@naver.com>
2023-10-23 23:46:06 +0900

    REFACTOR: Game Wrapper Class 추가 및 Main 구성 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,75 +1,110 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.*;
+import java.util.InputMismatchException;
 import java.util.List;
 
 public class Application {
+//
+//    private static List<Integer> computerNumbers;
+    private static final String STARTING_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+//    private static final String ERROR_MESSAGE_INVALID_INPUT = "3자리 양의 정수만 입력해주세요.";
+//    private static final String ERROR_MESSAGE_NOT_POSITIVE_NUMBER = "양의 정수만 입력해주세요.";
+//    private static final int INPUT_LIMIT = 3;
+//
+//    public static void isValidInput(String line) {
+//        if (line.length() != INPUT_LIMIT) {
+//            throw new IllegalArgumentException(ERROR_MESSAGE_INVALID_INPUT);
+//        }
+//        char[] charArray = line.toCharArray();
+//        for (char c : charArray) {
+//            if (!Character.isDigit(c) && c == '0') {
+//                throw new IllegalArgumentException(ERROR_MESSAGE_NOT_POSITIVE_NUMBER);
+//            }
+//        }
+//    }
+//
+//    public static boolean runGame() {
+//        System.out.print(INPUT_MESSAGE);
+//        String line = Console.readLine();
+//
+//        isValidInput(line);
+//
+//        int ball = 0, strike = 0;
+//        for (int index = 0; index < INPUT_LIMIT; index++) {
+//            int num = Character.getNumericValue(line.charAt(index));
+//            if (computerNumbers.get(index) == num) {
+//                strike++;
+//            } else if (computerNumbers.contains(num)) {
+//                ball++;
+//            }
+//        }
+//
+//        // Output Process
+//        scorePrint(strike, ball);
+//        scoreGame(strike, ball);
+//        return false;
+//
+//    }
+//
+//    private static boolean scoreGame(int strike, int ball) {
+//
+//        if (strike == 3) {
+//            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+//            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+//            String retryInput = Console.readLine();
+//            if (retryInput == null || retryInput.length() != 1 || !Character.isDigit(
+//                    retryInput.charAt(0)) || retryInput.charAt(0) > '2') {
+//                throw new IllegalArgumentException();
+//            } else {
+//                return !retryInput.equals("2");
+//            }
+//        }
+//        return false;
+//    }
+//
+//    private static void scorePrint(int strike, int ball) {
+//        if (strike == 0 && ball == 0) {
+//            System.out.println("낫싱");
+//        } else if (strike == 0 && ball != 0) {
+//            System.out.println(ball + "볼");
+//        } else if (strike != 0 && ball == 0) {
+//            System.out.println(strike + "스트라이크");
+//        } else {
+//            System.out.println(ball + "볼 " + strike + "스트라이크");
+//        }
+//    }
+//
+//
+//    public static void initGame() {
+//
+//        computerNumbers = Randoms.pickUniqueNumbersInRange(1, 9, 3);
+//        // NEED TO BE DELETED AT FINAL
+//        System.out.println(computerNumbers);
+//        System.out.println(STARTING_MESSAGE);
+//    }
 
     public static void main(String[] args) {
+        System.out.println(STARTING_MESSAGE);
 
-        final int INPUT_LIMIT = 3;
+        GameNumber computerNumber = new GameNumber();
+        GameNumber userInput;
+        while (true) {
+//            System.out.println(computerNumber.getBoard());
+            System.out.println(INPUT_MESSAGE);
+            userInput = new GameNumber(Console.readLine());
 
-        // User Input process
-        boolean isQuit = false;
-        while (!isQuit) {
-            System.out.println("숫자 야구 게임을 시작합니다.");
-
-            // Computer Randomized Number Generate
-            List<Integer> computerNumbers = Randoms.pickUniqueNumbersInRange(1, 9, 3);
-            System.out.println(computerNumbers);
-
-            System.out.print("숫자를 입력해주세요 : ");
-
-            // TODO: Input Validation
-            String line = Console.readLine();
-            if (line.length() != INPUT_LIMIT) {
-                throw new IllegalArgumentException("3자리 숫자를 입력해주세요.");
-            }
-            char[] charArray = line.toCharArray();
-            line = null;
-            for (char c : charArray) {
-                if (!Character.isDigit(c)) {
-                    throw new IllegalArgumentException("숫자만 입력해주세요.");
+            int strikes = computerNumber.countAndPrintGameScore(userInput);
+            if (strikes == 3) {
+                String line = Console.readLine();
+                if (Character.getNumericValue(line.charAt(0)) == 2) {
+                    System.out.println("게임 종료");
+                    return;
                 }
-                if (c < '1' || '9' < c) {
-                    throw new IllegalArgumentException("숫자의 범위는 0~9로 입력해주세요.");
-                }
-            }
-            int ball = 0, strike = 0;
-            for (int index = 0; index < INPUT_LIMIT; index++) {
-                int num = Character.getNumericValue(charArray[index]);
-                if (computerNumbers.get(index) == num) {
-                    strike++;
-                } else if (computerNumbers.contains(num)) {
-                    ball++;
-                }
-            }
-
-            // Output Process
-            if (strike == 0 && ball == 0) {
-                System.out.println("낫싱");
-            } else if (strike == 0 && ball != 0) {
-                System.out.println(ball + "볼");
-            } else if (strike != 0 && ball == 0) {
-                System.out.println(strike + "스트라이크");
-            } else {
-                System.out.println(ball + "볼 " + strike + "스트라이크");
-            }
-
-            // isCorrect
-            if (strike == 3) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                String retryInput = Console.readLine();
-                if (retryInput == null || retryInput.length() != 1) {
-                    throw new IllegalArgumentException("잘못된 입력값을 입력하셨습니다.");
-                } else if (retryInput.equals("2")) {
-                    isQuit = true;
-                } else if (retryInput.equals("1")) {
-                    System.out.println(computerNumbers);
-                }
-
+                computerNumber = new GameNumber();
             }
         }
+
     }
 }
---
+++ src/main/java/baseball/GameNumber.java
@@ -0,0 +1,84 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameNumber {
+    private static final int BOARD_LIMIT = 3;
+    private List<Integer> board = new ArrayList<Integer>();
+
+
+    public GameNumber(String number) {
+        validateNumber(number);
+        for (char c : number.toCharArray()) {
+            int numericValue = Character.getNumericValue(c);
+            validateDuplicateNumber(numericValue);
+            board.add(numericValue);
+        }
+    }
+
+    public GameNumber() {
+        generateRandomizeNumber();
+    }
+    private void validateDuplicateNumber(int number){
+        if(board.contains(number))
+            throw new IllegalArgumentException();
+    }
+
+    private void validateNumber(String number) {
+        if (number.length() > BOARD_LIMIT) {
+            throw new IllegalArgumentException();
+        }
+        for (char c : number.toCharArray()) {
+            int integerValue = Character.getNumericValue(c);
+            if (integerValue == -1 || integerValue == 0) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private void generateRandomizeNumber() {
+        while (board.size() < BOARD_LIMIT) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!board.contains(randomNumber)) {
+                board.add(randomNumber);
+            }
+        }
+    }
+
+    public List<Integer> getBoard() {
+        return this.board;
+    }
+
+    private void printGameScore(int strike, int ball) {
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+        } else if (strike == 0 && ball != 0) {
+            System.out.println(ball + "볼");
+        } else if (strike != 0 && ball == 0) {
+            System.out.println(strike + "스트라이크");
+        } else {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
+    }
+
+    public int countAndPrintGameScore(GameNumber gameNumber) {
+        int strike = 0;
+        int ball = 0;
+
+        for (int i = 0; i < BOARD_LIMIT; i++) {
+            if (this.board.equals(gameNumber.getBoard())) {
+                strike = 3;
+                break;
+            } else if (this.board.get(i).equals(gameNumber.getBoard().get(i))) {
+                strike++;
+            } else if (this.board.contains(gameNumber.getBoard().get(i))) {
+                ball++;
+            }
+        }
+        printGameScore(strike, ball);
+        return strike;
+    }
+
+}
