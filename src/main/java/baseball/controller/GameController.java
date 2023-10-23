c0481578f6b086e9aa270f599580d5b126e5c590
qudwns017 <qudwns017@naver.com>
2023-10-24 05:09:09 +0900

    Feat: input에 대한 결과값(스트라이크/볼/낫싱) 출력 구현

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -3,9 +3,11 @@ package baseball.controller;
 import baseball.service.ComputerService;
 import baseball.service.UserService;
 import baseball.service.UmpireService;
+import baseball.util.Constants;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class GameController {
     OutputView output = new OutputView();
@@ -15,16 +17,21 @@ public class GameController {
 
     public GameController() {
         computer = new ComputerService();
-        output.printStart();
     }
 
     public void run() {
+        output.printStart();
         UmpireService umpire = new UmpireService();
-        output.printInputNum();
         ArrayList<Integer> randomNumbers = computer.getRandomNumbers();
-        ArrayList<Integer> inputNumbers = user.getInputNumbers(input.getInput());
+        ArrayList<Integer> ballStrikeCount;
+        do {
+            output.printInputNum();
+            umpire.initCount();
+            ArrayList<Integer> inputNumbers = user.getInputNumbers(input.getInput());
 
-        ArrayList<Integer> ballStrikeCount = umpire.getResult(randomNumbers, inputNumbers);
-        output.printResult(ballStrikeCount);
+            ballStrikeCount = umpire.getResult(randomNumbers, inputNumbers);
+            output.printResult(ballStrikeCount);
+        } while (!Objects.equals(ballStrikeCount.get(Constants.ONE), Constants.STRIKE_SIZE));
+        output.printEndGame();
     }
 }
