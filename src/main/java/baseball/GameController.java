30cd3a7ea578779352fba6105906e2aa3529fc23
lzlkolo <lzlko1209@gmail.com>
2023-10-23 14:43:46 +0900

    feat: Input 3-digit number

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -5,17 +5,24 @@ import java.util.List;
 
 public class GameController {
     private final Computer computer;
+    private final Input input;
     private final Output output;
     private List<Integer> targetNumbers;
 
     public GameController() {
         this.computer = new Computer();
+        this.input = new Input();
         this.output = new Output();
         this.targetNumbers = new ArrayList<>();
     }
 
     public void play() {
+        boolean isGameOver = false;
+
         output.gameStart();
         targetNumbers = computer.generateRandomNumbers();
+        while (!isGameOver) {
+            List<Integer> userGuess = input.getUserGuess();
+        }
     }
 }
