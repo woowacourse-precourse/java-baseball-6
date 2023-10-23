00cab8341625c15b4df4b3c2b00d8785c154f1c6
jungeun-choi <jungeun5.choi@gmail.com>
2023-10-24 00:22:29 +0900

    feat: strike 판정

--- src/main/java/baseball/BaseballUmpire.java
+++ src/main/java/baseball/BaseballUmpire.java
@@ -2,5 +2,18 @@ package baseball;
 
 /* 플레이어가 제시한 숫자를 판정하고 결과를 반환 */
 public class BaseballUmpire {
+	private int umpireStrike(String com, String user) {
+		int strike = 0;
+		for (int i = 0; i < user.length(); i++) {
+			if (isEquals(com, user, i)) {
+				strike++;
+			}
+		}
+		return strike;
+	}
 
+	private boolean isEquals(String com, String user, int i) {
+		// com의 i번째 문자와 user의 i번째 문자가 서로 같다면 true
+		return com.charAt(i) == user.charAt(i);
+	}
 }
