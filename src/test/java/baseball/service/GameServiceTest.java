4f8b1e2c743d06700b7831f96a57f3572e2b2df3
BullChallenger <am23am45@naver.com>
2023-10-23 18:42:41 +0900

    [TEST] GameServiceImpl 테스트 작성
    게임 진행 시, 상황에 맞게 메시지가 잘 작성되어 전달되는지 확인했다

--- src/test/java/baseball/service/GameServiceTest.java
+++ src/test/java/baseball/service/GameServiceTest.java
@@ -40,4 +40,74 @@ class GameServiceTest {
         assertThat(hintDTO.getMessage()).isEqualTo("3볼");
     }
 
+    @Test
+    @DisplayName("입력 값에 따른 상태 값이 잘 설정되는지 확인 : Nothing의 경우")
+    void isNothing() {
+        AppConfig appConfig = new AppConfig();
+        GameService gameService = appConfig.gameService();
+
+        List<Integer> baseballNumbers = new ArrayList<>(Arrays.asList(1, 2, 3));
+        int[] inputNumbers = new int[]{4, 5, 6};
+        HintDTO hintDTO = gameService.process(baseballNumbers, inputNumbers);
+
+        assertThat(hintDTO.isAllStrike()).isFalse();
+        assertThat(hintDTO.getMessage()).isEqualTo("낫싱");
+    }
+
+    @Test
+    @DisplayName("입력 값에 따른 상태 값이 잘 설정되는지 확인 : 1볼 1스트라이크의 경우")
+    void is1Ball1Strike() {
+        AppConfig appConfig = new AppConfig();
+        GameService gameService = appConfig.gameService();
+
+        List<Integer> baseballNumbers = new ArrayList<>(Arrays.asList(1, 2, 3));
+        int[] inputNumbers = new int[]{3, 2, 4};
+        HintDTO hintDTO = gameService.process(baseballNumbers, inputNumbers);
+
+        assertThat(hintDTO.isAllStrike()).isFalse();
+        assertThat(hintDTO.getMessage()).isEqualTo("1볼 1스트라이크");
+    }
+
+    @Test
+    @DisplayName("입력 값에 따른 상태 값이 잘 설정되는지 확인 : 2볼 1스트라이크의 경우")
+    void is2Ball1Strike() {
+        AppConfig appConfig = new AppConfig();
+        GameService gameService = appConfig.gameService();
+
+        List<Integer> baseballNumbers = new ArrayList<>(Arrays.asList(1, 2, 3));
+        int[] inputNumbers = new int[]{3, 2, 1};
+        HintDTO hintDTO = gameService.process(baseballNumbers, inputNumbers);
+
+        assertThat(hintDTO.isAllStrike()).isFalse();
+        assertThat(hintDTO.getMessage()).isEqualTo("2볼 1스트라이크");
+    }
+
+    @Test
+    @DisplayName("입력 값에 따른 상태 값이 잘 설정되는지 확인 : 2스트라이크의 경우")
+    void is2Strike() {
+        AppConfig appConfig = new AppConfig();
+        GameService gameService = appConfig.gameService();
+
+        List<Integer> baseballNumbers = new ArrayList<>(Arrays.asList(1, 2, 3));
+        int[] inputNumbers = new int[]{4, 2, 3};
+        HintDTO hintDTO = gameService.process(baseballNumbers, inputNumbers);
+
+        assertThat(hintDTO.isAllStrike()).isFalse();
+        assertThat(hintDTO.getMessage()).isEqualTo("2스트라이크");
+    }
+
+    @Test
+    @DisplayName("입력 값에 따른 상태 값이 잘 설정되는지 확인 : 2볼의 경우")
+    void is2Ball() {
+        AppConfig appConfig = new AppConfig();
+        GameService gameService = appConfig.gameService();
+
+        List<Integer> baseballNumbers = new ArrayList<>(Arrays.asList(1, 2, 3));
+        int[] inputNumbers = new int[]{4, 3, 1};
+        HintDTO hintDTO = gameService.process(baseballNumbers, inputNumbers);
+
+        assertThat(hintDTO.isAllStrike()).isFalse();
+        assertThat(hintDTO.getMessage()).isEqualTo("2볼");
+    }
+
 }
