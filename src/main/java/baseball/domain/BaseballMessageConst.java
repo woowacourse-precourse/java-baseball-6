293d6fcb0a8e364fc2f26fef19e904f6c5536f51
jifrozen0110 <answldjs1836@gmail.com>
2023-10-24 00:42:04 +0900

    feat : BaseballMessageConst

---
+++ src/main/java/baseball/domain/BaseballMessageConst.java
@@ -0,0 +1,14 @@
+package baseball.domain;
+
+public class BaseballMessageConst {
+    public static final String START_GAME = "숫자 야구 게임을 시작합니다.";
+    public static final String INPUT_NUMBER = "숫자를 입력해주세요 : ";
+    public static final String RESTART_OR_EXIT = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String BALL = "%d볼";
+    public static final String STRIKE = "%d스트라이크";
+    public static final String NOTHING = "낫싱";
+
+    public static final String NOT_BALL_SIZE_THREE = "공이 3개가 아닙니다.";
+    public static final String EXIST_BALL = "중복된 공이 존재합니다.";
+
+}
