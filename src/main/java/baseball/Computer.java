1182bbd4f858f35ac0f87bd2df12af8dc9bccbce
iamgyu <iamgyu@naver.com>
2023-10-23 16:45:19 +0900

    Refactor : Computer 부분 리펙토링

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
+    private static final int RANDOM_NUMBER_LENGTH = 3;
+    private static final String REGEX = "[^1-9]";
+    private static final String SPLIT = "";
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
     private String randomNumber;
 
     public Computer() {
@@ -14,18 +19,19 @@ public class Computer {
     public void makeRandomNumber() {
         List<Integer> numberList = new ArrayList<>();
 
-        while (numberList.size() < 3) {
+        while (numberList.size() < RANDOM_NUMBER_LENGTH) {
             int pickNumber = pickRandomNumber();
+            // 메소드 분리
             if (!numberList.contains(pickNumber)) {
                 numberList.add(pickNumber);
             }
         }
 
-        this.randomNumber = numberList.toString().replaceAll("[^1-9]", "");
+        this.randomNumber = numberList.toString().replaceAll(REGEX, SPLIT);
     }
 
     public int pickRandomNumber() {
-        return Randoms.pickNumberInRange(1, 9);
+        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
     }
 
     public String getRandomNumber() {
