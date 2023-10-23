78d7e8bd9617f7c00193cc9db018741be525c86a
yuseonjun <seonjun0906@gmail.com>
2023-10-23 16:35:17 +0900

    test: compare 메서드는 컴퓨터와 플레이어의 자리수가 1개가 같고 1개가 다르면 1볼 1스트라이크를 반환한다.

--- src/test/java/baseball/domain/BaseballRefereeTest.java
+++ src/test/java/baseball/domain/BaseballRefereeTest.java
@@ -19,4 +19,16 @@ class BaseballRefereeTest {
 		assertEquals(3, hint.getStrikeCount());
 		assertEquals(0, hint.getBallCount());
 	}
+
+	@Test
+	void compare_메서드는_컴퓨터와_플레이어의_숫자의_자리수가_1개가_같고_1개가_다르면_1볼_1스트라이크를_반환한다() {
+		BaseballReferee baseballReferee = new BaseballReferee();
+		BaseballNumber computerNumbers = new BaseballNumber(List.of(1, 2, 3));
+		BaseballNumber playerNumbers = new BaseballNumber(List.of(1, 4, 2));
+
+		Hint hint = baseballReferee.compare(computerNumbers, playerNumbers);
+
+		assertEquals(1, hint.getStrikeCount());
+		assertEquals(1, hint.getBallCount());
+	}
 }
