c85fededeb37ff03f55e5a8971a455f951444f88
Seokhyun Lee <lseokhyun99@gmail.com>
2023-10-23 17:13:47 +0900

    feat(OutputView): 게임 완전히 종료 메시지 출력 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,6 +4,7 @@ public class OutputView {
 
     private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String COMPLETELY_EXIT_MESSAGE = "숫자 야구 게임을 종료합니다.";
 
     public static void printStartMessage() {
         System.out.println(START_MESSAGE);
@@ -15,6 +16,10 @@ public class OutputView {
 
     public static void printEndMessage() {
         System.out.println(GAME_END_MESSAGE);
+    }
+
+    public static void printCompletelyEndMessage() {
+        System.out.println(COMPLETELY_EXIT_MESSAGE);
     }
 
 }
