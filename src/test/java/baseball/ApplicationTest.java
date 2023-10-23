3a0ea069059709a4e38a11c026fdd467058c3b23
devhoya97 <diajbh123@gmail.com>
2023-10-23 17:14:10 +0900

    refactor : 개인적으로 만든 테스트코드 삭제 이번 주차의 요구사항은 테스트코드까지 신경써서 짜라고 하지 않았기 때문에, 소스파일 리팩토링에 좀 더 집중하고자 처음 받은 테스트코드의 형태로 복원하였습니다

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -9,19 +9,6 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
-//    private InputStream originalSystemIn;
-//
-//    @BeforeEach
-//    void setup() {
-//        originalSystemIn = System.in;
-//    }
-//
-//    @AfterEach
-//    void teardown() {
-//        System.setIn(originalSystemIn);
-//        Console.close();
-//    }
-
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
@@ -40,67 +27,6 @@ class ApplicationTest extends NsTest {
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
-
-//    @Test
-//    void getComputerNumbersList_테스트() {
-//        //given
-//        //when
-//        List<Integer> computerNumbersList = Application.getComputerNumbersList();
-//        System.out.println(computerNumbersList);
-//        //then
-//        assertThat(computerNumbersList).hasSize(3).doesNotHaveDuplicates()
-//                .allMatch(i -> i > 0 && i < 10);
-//    }
-//
-//    @Test
-//    void getUserNumbersList_정상작동_테스트() {
-//        //given
-//        String input = "123";
-//        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
-//        System.setIn(inputStream);
-//        //when
-//        List<Integer> userNumbersList = Application.getUserNumbersList();
-//        //then
-//        assertThat(userNumbersList).containsExactly(1, 2, 3);
-//    }
-//
-//    @Test
-//    void getUserNumbersList_정상작동_테스트2() {
-//        //given
-//        String input = "294";
-//        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
-//        System.setIn(inputStream);
-//        //when
-//        List<Integer> userNumbersList = Application.getUserNumbersList();
-//        //then
-//        assertThat(userNumbersList).containsExactly(2, 9, 4);
-//    }
-//
-//    @Test
-//    void getUserNumbersList_예외상황_테스트() {
-//        //given
-//        String input = "11123";
-//        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
-//        System.setIn(inputStream);
-//        //when
-//        Throwable thrown = catchThrowable(Application::getUserNumbersList);
-//        //then
-//        assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
-//                .hasMessageContaining("[ERROR]");
-//    }
-//
-//    @Test
-//    void getBallStrikeCount_테스트() {
-//        //given
-//        List<Integer> computerNumbersList = Arrays.asList(1, 2, 3);
-//        List<Integer> userNumbersList = Arrays.asList(1, 3, 9);
-//        //when
-//        int ballCount = Application.getBallCount(computerNumbersList, userNumbersList);
-//        int strikeCount = Application.getStrikeCount(computerNumbersList, userNumbersList);
-//        //then
-//        assertThat(ballCount).isEqualTo(1);
-//        assertThat(strikeCount).isEqualTo(1);
-//    }
 
     @Override
     public void runMain() {
