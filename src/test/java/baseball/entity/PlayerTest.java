481375ebc5cfdc6e8bb5de19e78ba8abe723cb92
youngkyunIm <yee0230@naver.com>
2023-10-23 23:41:40 +0900

    test : Player 테스트 구현

--- src/test/java/baseball/entity/PlayerTest.java
+++ src/test/java/baseball/entity/PlayerTest.java
@@ -1,4 +1,36 @@
-import static org.junit.jupiter.api.Assertions.*;
+package baseball.entity;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 class PlayerTest {
-  
+    @ParameterizedTest
+    @ValueSource(ints = {123, 456, 789, 987})
+    void 정상_값_정수_입력(int input) {
+        new Player(input);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {122, 988, 203})
+    void 오류_값_정수_입력(int input) {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Player(input);
+        });
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"123", "456", "789", "987"})
+    void 정상_값_문자_입력(String input) {
+        new Player(input);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"122", "988", "203", "", "a12", "aaa"})
+    void 오류_값_문자_입력(String input) {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Player(input);
+        });
+    }
 }
