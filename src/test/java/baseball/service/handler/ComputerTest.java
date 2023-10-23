bd6c7c8008d63079753c3ad32dbe8f93d76d2c5b
hee9841 <lee98417716@gmail.com>
2023-10-24 06:14:52 +0900

    :white_check_mark: update ComputerTest
    - 테스트케이스 수정

--- src/test/java/baseball/service/handler/ComputerTest.java
+++ src/test/java/baseball/service/handler/ComputerTest.java
@@ -1,24 +1,31 @@
 package baseball.service.handler;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Arrays;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ComputerTest {
 
-    Computer computer = new Computer();
 
     @Test
-    void getRandomNumber() {
+    @DisplayName("컴퓨터생성(랜덤수 생성)")
+    void 컴퓨터_생성() {
         //given
         int start_range = 1;
         int end_range = 9;
+        int number_of_digits = 3;
 
         //when
-        String result = computer.getRandomNumber(start_range, end_range);
+        Computer computer = new Computer(start_range, end_range, number_of_digits);
+        String result = computer.getComputerNumber();
 
         //then
         assertTrue(result.matches("[1-9]{3}"));
+        int result_size = (Arrays.stream(result.split("")).distinct().toList()).size();
+        assertEquals(result_size, number_of_digits);
     }
 
 
