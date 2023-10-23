3b7a9d61422b00204c5c83a51c6efbb311fa56c8
yuseonjun <seonjun0906@gmail.com>
2023-10-23 16:33:30 +0900

    test: from 메서드는 1또는 2의 값이 들어오면 Restart 객체를 생성한다.

--- src/test/java/baseball/domain/RestartTest.java
+++ src/test/java/baseball/domain/RestartTest.java
@@ -1,6 +1,7 @@
 package baseball.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -12,5 +13,11 @@ class RestartTest {
 	void Restart의_from_메서드는_매개변수가_1또는_2가_아닌경우_IllegalArgumentException가_발생한다(int command) {
 		assertThatThrownBy(() -> Restart.from(command))
 			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {1, 2})
+	void Restart의_from_메서드는_매개변수가_1또는_2인_경우_Restart_객체를_생성한다(int command) {
+		assertThat(Restart.from(command)).isInstanceOf(Restart.class);
 	}
 }
