b97e8be9143028288395c3234d6237e006c949b7
PARK9898 <wjddjs21@naver.com>
2023-10-24 00:47:20 +0900

    "feat:numbers size 검증 메서드 구현"
    -Numbers 객체의 필드 변수인 List<Integer> numbers의 size를 검증한다.

--- src/main/java/baseball/domain/NumbersValidator.java
+++ src/main/java/baseball/domain/NumbersValidator.java
@@ -3,9 +3,20 @@ package baseball.domain;
 import java.util.List;
 
 public class NumbersValidator {
+	private static final int MIN_RANDOM_NUMBER = 1;
+	private static final int MAX_RANDOM_NUMBER = 9;
+	private static final int NUMBERS_LENGTH = 3;
+
 	public static void validateDuplicateNumber(List<Integer> numbers) {
 		if (numbers.size() != numbers.stream().distinct().count()) {
 			throw new IllegalArgumentException("숫자는 중복되면 안됩니다.");
 		}
 	}
+
+	public static void validateNumbersSize(List<Integer> numbers) {
+		if (numbers.size() != NUMBERS_LENGTH) {
+			throw new IllegalArgumentException("숫자는 3자리여야 합니다.");
+		}
+	}
+
 }
