af046680aaa1f0676ea07f0a699e2588ff5b5dc8
KMK_HM <kyun9151@gmail.com>
2023-10-24 01:12:42 +0900

    refactor: 입력값 검증 메소드 분리

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,22 +1,15 @@
 package baseball;
 
-import baseball.utils.ExceptionMessage;
 import baseball.utils.PlayMessage;
+import baseball.utils.PlayerInputValidator;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.regex.Pattern;
 
 public class Player {
 
     private final int MAX_LENGTH = 3;
-    private static final Pattern PLAYER_REGEX = Pattern.compile("[1-9]{3,3}");
-    private static final String START = "1";
-    private static final String END = "9";
 
-    private String pickedPlayerNumbers;
     private List<Character> playerNumbers = new ArrayList<>();
 
     public List<Character> getPlayerNumbers() {
@@ -26,7 +19,7 @@ public class Player {
     public void setPlayerNumbers() {
         System.out.print(PlayMessage.INPUT_MESSAGE.getMessage());
         String input = Console.readLine();
-        validatePlayerNumber(input);
+        PlayerInputValidator.validatePlayerNumber(input);
         stringToList(input);
     }
 
@@ -37,37 +30,5 @@ public class Player {
         }
 
     }
-
-    private void validatePlayerNumber(String numbers) {
-        validateNumbersLength(numbers);
-        validateNumbersType(numbers);
-        validateDuplicateNumbers(numbers);
-    }
-
-    private void validateNumbersLength(String numbers) {
-        if (numbers.length() != MAX_LENGTH) {
-            throw new IllegalArgumentException(ExceptionMessage.NUMBERS_LENGTH_ERROR.getMessage());
-        }
-    }
-
-    private void validateDuplicateNumbers(String numbers) {
-        Set<Character> numberSet = new HashSet<>();
-
-        for (char c : numbers.toCharArray()) {
-
-            if (numberSet.contains(c)) {
-                throw new IllegalArgumentException(ExceptionMessage.DUPLICATE_NUMBER_ERROR.getMessage());
-            }
-            numberSet.add(c);
-        }
-
-    }
-
-    private void validateNumbersType(String numbers) {
-        if (!PLAYER_REGEX.matcher(numbers).matches()) {
-            throw new IllegalArgumentException(ExceptionMessage.NOT_NUMBER_ERROR.getMessage());
-        }
-    }
-
 
 }
---
+++ src/main/java/baseball/utils/PlayerInputValidator.java
@@ -0,0 +1,43 @@
+package baseball.utils;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+public class PlayerInputValidator {
+
+    private static final int MAX_LENGTH = 3;
+    private static final Pattern PLAYER_REGEX = Pattern.compile("[1-9]{3,3}");
+
+    public static void validatePlayerNumber(String playerNumber) {
+        validateNumbersLength(playerNumber);
+        validateNumbersType(playerNumber);
+        validateDuplicateNumbers(playerNumber);
+    }
+
+
+    private static void validateNumbersLength(String playerNumber) {
+        if (playerNumber.length() != MAX_LENGTH) {
+            throw new IllegalArgumentException(ExceptionMessage.NUMBERS_LENGTH_ERROR.getMessage());
+        }
+    }
+
+    private static void validateDuplicateNumbers(String playerNumber) {
+        Set<Character> numberSet = new HashSet<>();
+
+        for (char c : playerNumber.toCharArray()) {
+
+            if (numberSet.contains(c)) {
+                throw new IllegalArgumentException(ExceptionMessage.DUPLICATE_NUMBER_ERROR.getMessage());
+            }
+            numberSet.add(c);
+        }
+
+    }
+
+    private static void validateNumbersType(String playerNumber) {
+        if (!PLAYER_REGEX.matcher(playerNumber).matches()) {
+            throw new IllegalArgumentException(ExceptionMessage.NOT_NUMBER_ERROR.getMessage());
+        }
+    }
+}
