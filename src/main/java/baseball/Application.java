48cac1a843a3fef0a21d21cb26b2aece4fa0c049
chs98412 <chs98412@gmail.com>
2023-10-23 21:12:03 +0900

    feat: 게임 흐름 작성 메인에 그냥 때려박았는데 수정 필요해보임

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,43 +13,73 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        Game game = new Game();
-
-        Player player = new Player();
         InputController inputController = new InputController();
         OutputController outputController = new OutputController();
         GameController gameController = new GameController();
-        game.printAnswer();
         outputController.cliOutputln(START);
+        Game game;
+        Player player;
 
         while (true) {
+            boolean flag = false;
+
+            game = new Game();
+
+            player  = new Player();
+
+            game.printAnswer();
 
 
-        outputController.cliOutput(INPUT);
-        player.setInputString(inputController.playerInput());
-        Result result = gameController.game(player, game);
+            while (true) {
 
-            System.out.println(player.getInputString());
 
-            if (result.isAnswer) {
-                outputController.cliOutputln(ANSWER);
-                outputController.cliOutputln(AFTER);
-                break;
-            } else if (result.strike == 0 && result.ball != 0) {
-                System.out.print(result.ball);
-                outputController.cliOutputln(BALL);
-            } else if (result.strike != 0 && result.ball == 0) {
-                System.out.print(result.strike);
-                outputController.cliOutputln(STRIKE);
-            } else {
-                System.out.print(result.ball);
-                outputController.cliOutput(BALL);
-                System.out.print(result.strike);
-                outputController.cliOutputln(STRIKE);
+                outputController.cliOutput(INPUT);
+                player.setInputString(inputController.playerInput());
+                Result result = gameController.game(player, game);
+
+                System.out.println(player.getInputString());
+                if (result.strike != 0 && result.ball == 0) {
+                    System.out.print(result.strike);
+                    outputController.cliOutputln(STRIKE);
+
+                    if (result.isAnswer) {
+                        outputController.cliOutputln(ANSWER);
+                        outputController.cliOutputln(AFTER);
+                        String regame = inputController.reGameInput();
+                        if (regame.equals("1") ){
+                            System.out.println("here");
+                            flag = true;
+                        }
+                        break;
+                    }
+                }
+                 else if (result.strike == 0 && result.ball != 0) {
+                    System.out.print(result.ball);
+                    outputController.cliOutputln(BALL);
+                }else if (result.strike == 0 && result.ball == 0) {
+
+                    outputController.cliOutputln(NOTHING);
+                }
+                else {
+                    System.out.print(result.ball);
+                    outputController.cliOutput(BALL);
+                    outputController.cliOutput(" ");
+                    System.out.print(result.strike);
+                    outputController.cliOutputln(STRIKE);
+                }
+
+
             }
-
-
-
+            System.out.println(flag);
+            if (!flag) {
+                break;
+            }
         }
+    }
+
+    private void init() {
+    }
+
+    private void initgame() {
     }
 }
