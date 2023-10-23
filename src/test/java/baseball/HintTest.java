d0e8b4ad3571f4c176c6d2696f3f396f690d2005
jihyuk <kwlgur0416@gmail.com>
2023-10-23 19:24:49 +0900

    test : Hint 변수를 초기화에만 설정 가능하도록 한 것에 대한 test 수정

--- src/test/java/baseball/HintTest.java
+++ src/test/java/baseball/HintTest.java
@@ -6,10 +6,12 @@ import org.junit.jupiter.api.Test;
 
 class HintTest {
 
-    Hint hint = new Hint();
-
     @Test
     void 볼과_스트라이크_둘다_존재하지_않을_때의_출력_문자열_테스트() {
+        // given
+        int strike = 0, ball = 0;
+        Hint hint = new Hint(strike, ball);
+
         // given & when
         String hintString = hint.toString();
 
@@ -20,41 +22,40 @@ class HintTest {
     @Test
     void 볼만_존재할_때의_출력_문자열_테스트() {
         // given
-        hint.increaseBall();
-        hint.increaseBall();
+        int strike = 0, ball = 2;
+        Hint hint = new Hint(strike, ball);
 
         // when
         String hintString = hint.toString();
 
         // then
-        Assertions.assertThat(hintString).isEqualTo("2볼");
+        Assertions.assertThat(hintString).isEqualTo(ball + "볼");
     }
 
     @Test
     void 스트라이크만_존재할_때의_출력_문자열_테스트() {
         // given
-        hint.increaseStrike();
-        hint.increaseStrike();
+        int strike = 2, ball = 0;
+        Hint hint = new Hint(strike, ball);
 
         // when
         String hintString = hint.toString();
 
         // then
-        Assertions.assertThat(hintString).isEqualTo("2스트라이크");
+        Assertions.assertThat(hintString).isEqualTo(strike + "스트라이크");
     }
 
     @Test
     void 볼과_스트라이크_모두_존재할_때의_출력_문자열_테스트() {
         // given
-        hint.increaseBall();
-        hint.increaseStrike();
-        hint.increaseStrike();
+        int strike = 2, ball = 1;
+        Hint hint = new Hint(strike, ball);
 
         // when
         String hintString = hint.toString();
 
         // then
-        Assertions.assertThat(hintString).isEqualTo("1볼 2스트라이크");
+        Assertions.assertThat(hintString).isEqualTo(ball + "볼 " + strike + "스트라이크");
     }
 
 }
