2e5aa63318e45cbf73006b631a9504e10796054b
EunKyo Chae <dmsry1022@gmail.com>
2023-10-23 16:00:14 +0900

    feat:canRestart 메서드에서 예외처리구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,6 +1,7 @@
 package baseball;
-
 import camp.nextstep.edu.missionutils.Console;
+
+import java.util.regex.Pattern;
 
 public class Game {
     private UserGame usergame = new UserGame(); //UserGame 클래스 객체 생성
@@ -13,11 +14,26 @@ public class Game {
             computergame.randomNumber();
             usergame.inputUserNum();
         } while (canRestart());
-
     }
     public boolean canRestart() { // 게임을 종료할건지 재시작할건지?
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String input = Console.readLine(); // input => 1 혹은 2만 가능
+        if(!isExceptionExam(input)){
+            throw new IllegalArgumentException();
+        }
         return Integer.parseInt(input) == 1;
+    }
+    // countStrike + Strike -> 1 스트라이크 , countBall + Ball -> 1 볼
+    // 컴퓨터 값과 입력한 값을 비교하여 같을 경우에 따라
+
+    public boolean isResultException(String input){ // 0과 1이 아닌 숫자 시 예외
+        String pattern = "^[1-2]$";
+        return Pattern.matches(pattern, input);
+    }
+    public boolean isExceptionExam(String input){ // 예외처리
+        if(!isResultException(input)){
+            return false;
+        }
+        return true;
     }
 }
--- src/main/java/baseball/UserGame.java
+++ src/main/java/baseball/UserGame.java
@@ -1,17 +1,24 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class UserGame {
-
-    public int inputUserNum(){ // User의 숫자 입력
+    public List<Integer> inputUserNum(){ // User의 숫자 입력
         System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine(); // 제공된 입력문이 String으로 받아야하는 조건이 있다.
+        String[] strArr = input.split(""); // String으로 받아서 배열로 쪼개기
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i<strArr.length; i++){
+            list.add(Integer.valueOf(strArr[i]));
+        }
         if(!isExceptionEx(input)){ // 입력문 예외처리
             throw new IllegalArgumentException(); // 예외 던지기
         }
-        return Integer.parseInt(input); // ????? 아직 잘 모름
+        return list;
     }
     public boolean isExceptionEx(String input){ // 예외처리
         if(!isNumber(input) || !isInputNumCount(input)){ // 숫자가 아닐때 또는 3자리가 아닐때
