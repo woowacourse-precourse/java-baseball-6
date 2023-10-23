fcc74de0fecd7e5975dfdcf298c4f643cadd1dab
Young1 <dlduddnjs198@naver.com>
2023-10-23 18:47:17 +0900

    UPDATE : Application.java 초기 설정 & 사용자 숫자 입력 구현 완료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,17 +8,24 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         try{
+            int[] userNum = new int[3];
             int[] answerNum = new int[3];
 
             generateAnswer(answerNum);
 
             System.out.println("숫자 야구 게임을 시작합니다.");
+
+            while (true){
+                System.out.print("숫자를 입력해주세요 : ");
+                String input = Console.readLine();
+
+                validateInput(input, userNum);
+            }
         }catch(IllegalArgumentException e){
             // 그냥 프로그램 종료
-        }finally{
+        }finally {
             Console.close();
         }
-
     }
 
     private static void generateAnswer(int[] answerNum) {
@@ -29,6 +36,37 @@ public class Application {
         int idx = 0;
         for (Integer num : uniqueNumbers) {
             answerNum[idx++] = num;
+        }
+    }
+
+    private static void validateInput(String input, int[] userNum) throws IllegalArgumentException {
+        validateInputLength(input);
+        makeUserNum(input, userNum);
+        validateUserNumDuplicates(userNum);
+    }
+
+    private static void validateInputLength(String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("3자리의 숫자를 입력해주세요.");
+        }
+    }
+
+    private static void makeUserNum(String input, int[] userNum) {
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (!Character.isDigit(c)) {
+                throw new IllegalArgumentException("숫자만 입력해주세요.");
+            }
+            userNum[i] = Character.getNumericValue(c);
+        }
+    }
+
+    private static void validateUserNumDuplicates(int[] userNum) {
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        for (int num : userNum) {
+            if (!uniqueNumbers.add(num)) {
+                throw new IllegalArgumentException("중복된 숫자를 입력하셨습니다.");
+            }
         }
     }
 }
