f461d44d0daf6ffdb4a4a9a8f5a0c3c949142e48
jiwon1217 <koagjw@gmail.com>
2023-10-24 05:31:11 +0900

    :recycle: refactor: @BeforeEach를 활용하여 테스트 코드 수정

--- src/test/java/baseball/model/ComputerTest.java
+++ src/test/java/baseball/model/ComputerTest.java
@@ -3,47 +3,32 @@ package baseball.model;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 
 class ComputerTest {
+    private Computer computer;
     final int MAX_SIZE = 3;
     final int LOWER_INCLUSIVE = 1;
     final int UPPER_INCLUSIVE = 9;
 
-    @RepeatedTest(100)
-    @DisplayName("컴퓨터는 숫자를 생성한다.")
-    void test() {
-        generateRandomNumbers_숫자_범위_테스트();
-        generateRandomNumbers_숫자_3개_생성_테스트();
-        generateRandomNumbers_중복되지_않는_숫자_생성_테스트();
+    @BeforeEach
+    void setUp() {
+        computer = new Computer();
+        computer.generateRandomNumbers();
     }
 
-    @DisplayName("컴퓨터는 랜덤으로 3개의 자연수를 생성한다.")
-    void generateRandomNumbers_숫자_3개_생성_테스트() {
-        Computer computer = new Computer();
-        computer.generateRandomNumbers();
+    @RepeatedTest(100)
+    @DisplayName("컴퓨터는 1이상 9이하의 서로 다른 숫자 3개를 생성한다.")
+    void generateRandomNumbers_1이상_9이하_서로_다른_숫자_3개_생성_테스트() {
         List<Integer> numbers = computer.getNumbers();
 
         assertThat(numbers).hasSize(MAX_SIZE);
-    }
-
-    @DisplayName("각 자릿수는 1이상 9이하이다.")
-    void generateRandomNumbers_숫자_범위_테스트() {
-        Computer computer = new Computer();
-        computer.generateRandomNumbers();
-        List<Integer> numbers = computer.getNumbers();
 
         for (Integer number : numbers) {
             assertThat(number).isBetween(LOWER_INCLUSIVE, UPPER_INCLUSIVE);
         }
-    }
-
-    @DisplayName("컴퓨터가 생성한 3개의 자연수는 서로 중복되지 않는다.")
-    void generateRandomNumbers_중복되지_않는_숫자_생성_테스트() {
-        Computer computer = new Computer();
-        computer.generateRandomNumbers();
-        List<Integer> numbers = computer.getNumbers();
 
         assertThat(numbers).doesNotHaveDuplicates();
     }
