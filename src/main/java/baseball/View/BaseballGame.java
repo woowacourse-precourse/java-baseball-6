d971054b0412bb57ad364b3bcb1bba9dd0c3e13d
saaut <saaut60@naver.com>
2023-10-23 23:05:00 +0900

    유저 숫자를 검사한다.

--- src/main/java/baseball/View/BaseballGame.java
+++ src/main/java/baseball/View/BaseballGame.java
@@ -1,5 +1,6 @@
 package baseball.View;
 
+import baseball.Controller.CheckNumberController;
 import baseball.Controller.NumberController;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
@@ -7,6 +8,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class BaseballGame {
     NumberController numbers=new NumberController();
 
+    CheckNumberController checkNumbers= new CheckNumberController();
 
     Integer[] userNumbers={};
     Integer[] randomNumbers={};
@@ -16,16 +18,27 @@ public class BaseballGame {
             System.out.println("숫자 야구 게임을 시작합니다.");
             randomNumbers=numbers.createRandomNumber();
 
-            readUserNumber();
+            do {
+                readUserNumber();
+            }while(!checkUserNumber());
 
         }
     }
     public void readUserNumber(){
-        while (true) {
-            System.out.println("숫자를 입력해주세요 :");
-            String userSay = readLine();
-            userNumbers=numbers.getUserNumber(userSay);
-        }
+
+        System.out.println("숫자를 입력해주세요 :");
+        String userSay = readLine();
+        userNumbers=numbers.getUserNumber(userSay);
+
+    }
+    public boolean checkUserNumber(){
+        //검사
+        //ball의 경우
+        checkNumbers.countBall(userNumbers,randomNumbers);
+        //strike의 경우
+        checkNumbers.countStrike(userNumbers,randomNumbers);
+        if(checkNumbers.isCorrected()) return true;
+        else return false;
     }
 
 
