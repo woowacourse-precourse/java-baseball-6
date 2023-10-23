dc65050fbb997add49e9af3b2711be18c5e7d671
noteasymin <noteasymin@gmail.com>
2023-10-23 21:14:59 +0900

    feat: 사용자 입력값 기능 구현
    사용자가 입력하는 값을 리스트로 저장

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -13,7 +14,7 @@ public class Application {
 
     private static List<Integer> createRandomNumber() {
         List<Integer> computer = new ArrayList<>();
-        
+
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computer.contains(randomNumber)) {
@@ -21,5 +22,21 @@ public class Application {
             }
         }
         return computer;
-    };
+    }
+
+    private static List<Integer> getUserNumber() {
+        List<Integer> userNumber = new ArrayList<>();
+        String Number;
+        String[] tempNumber;
+
+        System.out.println("숫자를 입력해주세요 : ");
+        Number = Console.readLine();
+
+        tempNumber = Number.split("");
+        for (String s : tempNumber) {
+            userNumber.add(Integer.parseInt(s));
+        }
+
+        return userNumber;
+    }
 }
