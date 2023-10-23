cfcb8e8906098b6a360cb9652172f471f0adab0d
taehoon <xogns1514@gmail.com>
2023-10-23 09:35:50 +0900

    refactor: 랜덤값 생성 메서드 반환값 수정
    getter로 랜덤값을 반환하는 것에서, generateRandomNumber 메서드에서 바로 랜덤값을 반환하도록 수정했다.

--- src/main/java/baseball/entity/Random.java
+++ src/main/java/baseball/entity/Random.java
@@ -6,12 +6,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Random {
-    private String randomNumber;
-
     public Random() {}
 
     // 1부터 9까지 서로 다른 수로 이루어진 랜덤 3자리수 발생시키는 함수
-    public void generateRandomNumber() {
+    public String generateRandomNumber() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -19,11 +17,7 @@ public class Random {
                 computer.add(randomNumber);
             }
         }
-        this.randomNumber = NumberListConverter.integerListToString(computer);
-    }
-
-    public String getRandomNumber() {
-        return this.randomNumber;
+        return NumberListConverter.integerListToString(computer);
     }
 
 }
