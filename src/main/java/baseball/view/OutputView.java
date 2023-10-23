afa7018db52a62e84bc10f17cecea33911e09749
jiwon1217 <koagjw@gmail.com>
2023-10-24 02:13:10 +0900

    :sparkles: feat: 정답인 경우, 게임 종료 문구를 출력한다.

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -24,4 +24,8 @@ public class OutputView {
             System.out.println(ballCount + "볼" + " " + strikeCount + "스트라이크");
         }
     }
+
+    public static void printGameExitMessage() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
 }
