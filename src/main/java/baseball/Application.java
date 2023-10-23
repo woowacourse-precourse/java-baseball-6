69c499eca6934bf1a52952df4343b92377547c97
서창덕 <44630705+Garodden@users.noreply.github.com>
2023-10-24 00:48:03 +0900

    docs: (6)번 수정 .23.10.23 -서창덕

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,8 +4,17 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        boolean result=false;
+        ShootBall randomBall = new ShootBall();
+        ShootBall inputBall = new ShootBall();
 
-
-
+        while(1) {
+            randomBall.randomNumGenerator();
+            while (!result) {
+                inputBall.inputNumGenerator();
+                result = randomBall.baseballReferee(inputBall);
+            }
+            system
+        }
     }
 }
