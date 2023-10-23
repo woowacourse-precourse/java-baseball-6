c2462c823af54ae9329a2b73a69da9cc9f13b116
Annjieun <dkslwdms2121@naver.com>
2023-10-23 17:23:56 +0900

    refactor: Player에서 입력받는 부분을 메소드로 분리

--- src/main/java/baseball/core/Player.java
+++ src/main/java/baseball/core/Player.java
@@ -9,16 +9,20 @@ public class Player {
     private final Validator validator = new Validator();
 
     public List<Integer> getPlayerNumber() {
-        String input = Console.readLine();
+        String input = readInput();
         List<Integer> player = validator.validateInputNumber(input);
 
         return player;
     }
 
     public int getRestartOrExitNumber() {
-        String choice = Console.readLine();
+        String choice = readInput();
         validator.validateRestartOrExitNumber(choice);
 
         return Integer.parseInt(choice);
+    }
+
+    public String readInput() {
+        return Console.readLine();
     }
 }
