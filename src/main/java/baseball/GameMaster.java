41960a658205ef610cfada0ba95f0ac32669aac7
D0ri123 <lsh81550@gmail.com>
2023-10-23 12:30:24 +0900

    feat: 입력 숫자와 난수를 비교하여 결과 반환 기능 구현 - 2차 구현
    숫자를 비교하는 역할을 가진 Validator의 메서드를 main 메서드에서 분리했습니다. 결과를 받으면 출력하는 역할을 가진 GameMaster의 메서드도 분리했습니다.

--- src/main/java/baseball/GameMaster.java
+++ src/main/java/baseball/GameMaster.java
@@ -1,33 +1,52 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Map;
 
 public class GameMaster {
 
   boolean isContinued;
+  int inputNumber;
 
   public void start() {
     this.isContinued = true;
     System.out.println("숫자 야구 게임을 시작합니다.");
   }
 
-  public String progress() {
+  public void progress() {
     System.out.print("숫자를 입력해주세요 : ");
-    return Console.readLine();
+    this.inputNumber = Integer.parseInt(Console.readLine());
   }
 
-  public void printResult(String result) {
+  public void checkResult(String result) {
+    System.out.println(result);
     if(result.equals("3스트라이크")) {
       System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
       String answer = progressOrNot();
       //TODO: answer를 validator로 보내서 입력값이 올바른지 검증한다.
-      return;
     }
-    System.out.println(result);
   }
 
   public String progressOrNot() {
     System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     return Console.readLine();
+  }
+
+  public String printResult(Map<String, Integer> resultTable) {
+    StringBuilder sb = new StringBuilder();
+    int strike = resultTable.get("스트라이크");
+    int ball = resultTable.get("볼");
+
+    if (strike == 0 && ball == 0) {
+      sb.append("낫싱");
+    } else if(strike != 0 && ball == 0) {
+      sb.append(strike).append("스트라이크");
+    } else if(strike == 0 && ball != 0) {
+      sb.append(ball).append("볼");
+    } else {
+      sb.append(ball).append("볼 ").append(strike).append("스트라이크");
+    }
+
+    return sb.toString();
   }
 }
---
+++ src/main/java/baseball/Validator.java
@@ -0,0 +1,68 @@
+package baseball;
+
+import static baseball.Type.BALL;
+import static baseball.Type.NONE;
+import static baseball.Type.STRIKE;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Validator {
+  int randomNumber;
+  int inputNumber;
+  ArrayList<String> randomList = new ArrayList<>();
+  ArrayList<Baseball> inputList = new ArrayList<>();
+
+  public Validator(int randomNumber, int inputNumber) {
+    this.randomNumber = randomNumber;
+    this.inputNumber = inputNumber;
+  }
+
+  public void init() {
+    String[] splitRandom = String.valueOf(randomNumber).split("");
+    String[] splitInput = String.valueOf(inputNumber).split("");
+    addToList(splitRandom, splitInput);
+  }
+
+  public void addToList(String[] randomArr, String[] inputArr) {
+    for(int i=0; i<3; i++) {
+      this.randomList.add(randomArr[i]);
+      this.inputList.add(new Baseball(NONE, Integer.parseInt(inputArr[i]), i));
+    }
+  }
+
+  //TODO: 해당 로직 리팩토링 필요
+  public void changeInputBaseballType() {
+    for(Baseball baseball : inputList) {
+      String baseballNumber = String.valueOf(baseball.number);
+      if(randomList.contains(baseballNumber)) {
+        if(randomList.indexOf(baseballNumber) == baseball.index) {
+          baseball.changeType(STRIKE);
+        } else {
+          baseball.changeType(BALL);
+        }
+      }
+    }
+  }
+
+  public Map<String, Integer> calculateResult() {
+    Map<String, Integer> resultTable = new HashMap<>();
+    int ball = 0;
+    int strike = 0;
+    resultTable.put("스트라이크", 0);
+    resultTable.put("볼", 0);
+
+    for(Baseball baseball : inputList) {
+      if(baseball.type == STRIKE) {
+        resultTable.put("스트라이크", resultTable.get("스트라이크") + 1);
+//        strike++;
+      } else if(baseball.type == BALL) {
+        resultTable.put("볼", resultTable.get("볼") + 1);
+//        ball++;
+      }
+    }
+    return resultTable;
+  }
+
+}
