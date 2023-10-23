aab81a9f6e4d1b77d74a2bd391995e12f07c2988
Seokhyun Lee <lseokhyun99@gmail.com>
2023-10-24 00:08:30 +0900

    feat(BaseballGame): 숫자 야구 게임 한 판을 실행하는 기능 구현

--- src/main/java/baseball/controller/BaseballGame.java
+++ src/main/java/baseball/controller/BaseballGame.java
@@ -1,12 +1,34 @@
 package baseball.controller;
 
+import baseball.service.Hint;
+import baseball.service.Referee;
 import baseball.util.InputParser;
 import baseball.util.InputValidator;
+import baseball.util.RandomNumberGenerator;
 import baseball.view.InputView;
 
+import baseball.view.OutputView;
 import java.util.List;
 
 public class BaseballGame {
+
+    private static void playBaseballGame() {
+        List<Integer> computer = RandomNumberGenerator.generateRandomNumberList();
+
+        while (true) {
+            List<Integer> user = getUserNumberList();
+
+            int ballCount = Referee.calculateBallCount(computer, user);
+            int strikeCount = Referee.calculateStrikeCount(computer, user);
+
+            String hint = Hint.makeHint(ballCount, strikeCount);
+            OutputView.printHintMessage(hint);
+
+            if (Referee.isGameFinished(strikeCount)) {
+                break;
+            }
+        }
+    }
 
     private static List<Integer> getUserNumberList() {
         String userNumber = InputView.getUserNumber();
