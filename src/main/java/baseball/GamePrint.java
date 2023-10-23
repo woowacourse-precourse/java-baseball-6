12207640414df47e7cbfca508e31f3c9f148c1fd
gonixxxx <akdntm946@gmail.com>
2023-10-23 22:23:01 +0900

    style: 볼=>스트라이크로 출력 순서 변경

--- src/main/java/baseball/GamePrint.java
+++ src/main/java/baseball/GamePrint.java
@@ -8,11 +8,11 @@ public class GamePrint {
 
     public String printHint(int strike, int ball) {
         String result = "";
-        if (strike != 0) {
-            result += strike + "스트라이크 ";
-        }
         if (ball != 0) {
             result += ball + "볼 ";
+        }
+        if (strike != 0) {
+            result += strike + "스트라이크 ";
         }
         if (result.isEmpty()) {
             result = "낫싱";
