b579533e1de25ce67073a384ce678e041b35986f
mksystem98 <mksystem98@gmail.com>
2023-10-23 10:15:27 +0900

    feat : UserInputView 클래스 생성

--- src/main/java/baseball/view/GameView.java
+++ src/main/java/baseball/view/GameView.java
@@ -5,8 +5,4 @@ public class GameView {
     public void startGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
-
-    public void requestNumberInput(){
-        System.out.println("숫자를 입력해주세요 : ");
-    }
 }
