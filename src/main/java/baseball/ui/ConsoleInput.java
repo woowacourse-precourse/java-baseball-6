867d90e85a8ccde143a79b7eb06b1493222b9dde
choiejae <ejae8319@gmail.com>
2023-10-23 15:28:21 +0900

    refactor: 게임 명령어 enum 타입 구현으로 인해 사용하지 않는 메서드 삭제

--- src/main/java/baseball/ui/ConsoleInput.java
+++ src/main/java/baseball/ui/ConsoleInput.java
@@ -2,9 +2,13 @@ package baseball.ui;
 
 import baseball.utils.Utility;
 import camp.nextstep.edu.missionutils.Console;
-import java.util.List;
 
 public class ConsoleInput implements IInput {
+
+    @Override
+    public String scan() {
+        return Console.readLine();
+    }
 
     public String scanGameNumber(final int length) {
         String input = scan();
@@ -13,18 +17,5 @@ public class ConsoleInput implements IInput {
         Utility.checkIfIsValidLength(length, input);
 
         return input;
-    }
-
-    public String scanCommand(List<String> gameCommand) {
-        String input = Console.readLine();
-
-        Utility.checkIfIsValidCommand(gameCommand, input);
-
-        return input;
-    }
-
-    @Override
-    public String scan() {
-        return Console.readLine();
     }
 }
