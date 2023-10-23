c20a468a01c6cd4950018e4575de1826e51074ae
Han <yulmam1@naver.com>
2023-10-23 19:33:47 +0900

    feat(view) : InputView와 OutputView 생성 및 구현합니다

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,2 +1,14 @@
-package baseball.view;public class InputView {
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public void printNumberRequestMessage() {
+        System.out.println("숫자를 입력해 주세요 : ");
+    }
+
+    public void printRetryMessage() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력해 주세요");
+    }
 }
