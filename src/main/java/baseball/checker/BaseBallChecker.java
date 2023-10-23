33a0703cf1b5be66bc2b5d2ed5f77bc64f7f858a
minsub <chrkb1569@naver.com>
2023-10-24 00:25:13 +0900

    refactor : BaseBallChecker 내부 Result 클래스 생성 및 로직 수정

--- src/main/java/baseball/checker/BaseBallChecker.java
+++ src/main/java/baseball/checker/BaseBallChecker.java
@@ -3,15 +3,35 @@ package baseball.checker;
 // 사용자가 입력한 값의 결과를 반환하는 클래스, 볼 카운트는 10, 20, 30으로 스트라이크 카운트는 1, 2, 3으로 구분
 public class BaseBallChecker {
     public static int checkGameResult(String userInput, String targetNumber) {
-        int ballCount = 0;
-        int strikeCount = 0;
+        Result result = new Result();
 
         for(int i = 0; i < 3; i++) {
             char value = userInput.charAt(i);
-            if(targetNumber.charAt(i) == value) strikeCount++;
-            else if(targetNumber.contains(String.valueOf(value))) ballCount++;
+            result.addCount(value, targetNumber, i);
         }
 
-        return 10 * ballCount + strikeCount;
+        return result.getResult();
+    }
+
+    private static class Result {
+        private int ballCount;
+        private int strikeCount;
+
+        public Result() {
+            this.ballCount = 0;
+            this.strikeCount = 0;
+        }
+
+        public void addCount(char inputValue, String targetNumber, int index) {
+            if(targetNumber.charAt(index) == inputValue) {
+                strikeCount++;
+                return;
+            }
+            if(targetNumber.contains(String.valueOf(inputValue))) ballCount++;
+        }
+
+        public int getResult() {
+            return 10 * ballCount + strikeCount;
+        }
     }
 }
