c5a3efae036897d3714da54d9996c93eb43e4ee2
davemins <davemins4125@naver.com>
2023-10-24 02:24:05 +0900

    feat (Input) : 재시작/완전 종료 질문 및 실행 기능 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,27 +1,45 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Game {
     String randomNumber;
     String userNumber;
+    Boolean quit = true;
 
     public void play() {
+
         Output output = new Output();
         output.printStartGame();
-
         RandomNumberCreater randomNumberCreater = new RandomNumberCreater();
 
         randomNumber = randomNumberCreater.getRandomNumber();
         System.out.println(randomNumber);
 
-        Input input = new Input();
-        userNumber = input.enterGameNumber();
+        while(quit) {
 
-        CheckInputNumber checkInputNumber = new CheckInputNumber();
+            Input input = new Input();
+            userNumber = input.enterGameNumber();
 
-        int strike = checkInputNumber.checkStrike(randomNumber, userNumber);
-        int ball = checkInputNumber.checkBall(randomNumber, userNumber);
+            CheckInputNumber checkInputNumber = new CheckInputNumber();
 
-        output.printResult(strike, ball);
+            int strike = checkInputNumber.checkStrike(randomNumber, userNumber);
+            int ball = checkInputNumber.checkBall(randomNumber, userNumber);
+
+            output.printResult(strike, ball);
+
+            if (strike == 3) {
+                if (input.enterAnswerRestartGame().equals("1")) {
+                    randomNumber = randomNumberCreater.getRandomNumber();
+                    System.out.println(randomNumber);
+                    quit = true;
+                } else {
+                    quit = false;
+                }
+            }
+        }
+
+
 
     }
 
