149d7da5cf8d698ec682dfae97d2169f36ad5c55
min050410 <min050410@gmail.com>
2023-10-23 21:34:24 +0900

    feat: Player 클래스 정의

---
+++ src/main/java/baseball/player/Player.java
@@ -0,0 +1,15 @@
+package baseball.player;
+
+public class Player {
+
+    String playerNumString;
+
+    public void setPlayerNumString(String playerNumString) {
+        this.playerNumString = playerNumString;
+    }
+
+    public String getPlayerNumString() {
+        return playerNumString;
+    }
+
+}
