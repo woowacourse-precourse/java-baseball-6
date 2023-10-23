ec71417938571ab58557c11bce644a9ef95f31c7
anseongjun <dkstjdwns@kookmin.ac.kr>
2023-10-24 03:06:27 +0900

    feat: 사용자로부터 숫자 입력받기

--- src/main/java/baseball/view/InputNumber.java
+++ src/main/java/baseball/view/InputNumber.java
@@ -1,5 +1,10 @@
 package baseball.view;
 
-public class UserInput {
-    
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputNumber {
+    public static String getUserInput() {
+        System.out.print("숫자를 입력해주세요 : ");
+        return Console.readLine();
+    }
 }
