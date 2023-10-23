0f6533df9fda8e4179650b1ba088504f026f78ce
beomsic <qjatjr29@khu.ac.kr>
2023-10-23 14:45:32 +0900

    :sparkles: feat: 컴퓨터 숫자와 입력받은 숫자를 비교해 스트라이크, 볼 개수를 계산하는 로직 추가

--- src/main/java/baseball/model/BaseBallGame.java
+++ src/main/java/baseball/model/BaseBallGame.java
@@ -1,10 +1,30 @@
 package baseball.model;
 
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class BaseBallGame {
     private final BaseBallNumber computerNumber;
     public BaseBallGame(int digit) {
         computerNumber = BaseBallNumber.generateRandomNumbers(digit);
     }
+    private BallCount calculateStrikeAndBall(BaseBallNumber inputBaseBallNumber) {
+        AtomicInteger strike = new AtomicInteger(0);
+        AtomicInteger ball = new AtomicInteger(0);
 
+        computerNumber.calculate((targetNumber, computerIndex) ->
+                inputBaseBallNumber.calculate((inputNumber, inputIndex) -> {
+                    if(!Objects.equals(targetNumber, inputNumber)) return;
 
+                    if(computerIndex.equals(inputIndex)) {
+                        strike.getAndIncrement();
+                    }
+                    else {
+                        ball.getAndIncrement();
+                    }
+                })
+        );
+
+        return new BallCount(strike.get(), ball.get());
+    }
 }
