e31e412793d104dee16be0205cbf1738fa99555e
chooh <chocomilk66@naver.com>
2023-10-23 17:56:44 +0900

    feat: 3개의 랜덤 숫자를 컴퓨터가 출력하는 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,8 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 
 public class Application {
     public static void main(String[] args) {
@@ -8,7 +10,14 @@ public class Application {
         System.out.println("숫자 야구 게임을 시작합니다.");
         System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
-        System.out.println(input);
-
+        while (true) {
+            ArrayList<Integer> numbers = new ArrayList<>();
+            while (numbers.size() < 3) {
+                int number = Randoms.pickNumberInRange(1, 9);
+                if (!numbers.contains(number)) {
+                    numbers.add(number);
+                }
+            }
+        }
     }
 }
