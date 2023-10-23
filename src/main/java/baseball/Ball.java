b5a0bd4b0a0165f8e12f72177dc2e89fd047db4c
sangdol <priceelees@gmail.com>
2023-10-23 15:01:25 +0900

    refactor: Ball 클래스에 record 적용
    < description >
    - record를 활용하여 생성자, getter, equals / hashcode 삭제

--- src/main/java/baseball/Ball.java
+++ src/main/java/baseball/Ball.java
@@ -1,47 +1,12 @@
 package baseball;
 
-import java.util.Objects;
-
-public class Ball {
-    private final int number;
-    private final int location;
-
-    public Ball(int number, int location) {
-        this.number = number;
-        this.location = location;
-    }
-
+public record Ball(int number, int location) {
     public boolean isStrike(Ball other) {
         return this.equals(other);
     }
 
     public boolean isBall(Ball other) {
-        return number == other.getNumber() &&
-                location != other.getLocation();
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    public int getLocation() {
-        return location;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Ball ball = (Ball) o;
-        return getNumber() == ball.getNumber() && getLocation() == ball.getLocation();
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getNumber(), getLocation());
+        return number == other.number() &&
+                location != other.location();
     }
 }
