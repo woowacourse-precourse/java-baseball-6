c1f69b08f9a1317256f6a2b4dfd56619747d73b4
sangdol <priceelees@gmail.com>
2023-10-23 15:27:47 +0900

    test: BeforeAll을 이용한 BallContainerTest, BallTest의 중복 코드 축소

--- src/test/java/baseball/BallContainerTest.java
+++ src/test/java/baseball/BallContainerTest.java
@@ -3,10 +3,17 @@ package baseball;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BallContainerTest {
+    static BallContainer ballContainer;
+
+    @BeforeAll
+    static void setUp() {
+        ballContainer = BallContainer.getFromNumbers(List.of(5, 6, 2));
+    }
 
     @DisplayName("숫자 리스트를 List<Ball>로 정확하게 매핑하는지 확인합니다.")
     @Test
@@ -16,104 +23,93 @@ class BallContainerTest {
                 new Ball(6, 1),
                 new Ball(2, 2));
 
-        BallContainer ballContainer = BallContainer.getFromNumbers(List.of(5, 6, 2));
-
         assertThat(ballContainer.getBalls()).isEqualTo(expected);
     }
 
     @DisplayName("3스트라이크 결과를 정확히 리턴하는지 확인합니다.")
     @Test
     void pitch_is3Strike() {
-        BallContainer container = BallContainer.getFromNumbers(List.of(1, 5, 9));
-        BallContainer otherContainer = BallContainer.getFromNumbers(List.of(1, 5, 9));
+        BallContainer otherContainer = BallContainer.getFromNumbers(List.of(5, 6, 2));
 
         PitchResult expected = new PitchResult(3, 0);
 
-        assertThat(container.pitch(otherContainer))
+        assertThat(ballContainer.pitch(otherContainer))
                 .isEqualTo(expected);
     }
 
     @DisplayName("2스트라이크 결과를 정확히 리턴하는지 확인합니다.")
     @Test
     void pitch_is2Strike() {
-        BallContainer container = BallContainer.getFromNumbers(List.of(2, 5, 9));
-        BallContainer otherContainer = BallContainer.getFromNumbers(List.of(1, 5, 9));
+        BallContainer otherContainer = BallContainer.getFromNumbers(List.of(5, 6, 7));
 
         PitchResult expected = new PitchResult(2, 0);
 
-        assertThat(container.pitch(otherContainer))
+        assertThat(ballContainer.pitch(otherContainer))
                 .isEqualTo(expected);
     }
 
     @DisplayName("1스트라이크 결과를 정확히 리턴하는지 확인합니다.")
     @Test
     void pitch_is1Strike() {
-        BallContainer container = BallContainer.getFromNumbers(List.of(2, 3, 9));
-        BallContainer otherContainer = BallContainer.getFromNumbers(List.of(1, 5, 9));
-
+        BallContainer otherContainer = BallContainer.getFromNumbers(List.of(5, 1, 9));
         PitchResult expected = new PitchResult(1, 0);
 
-        assertThat(container.pitch(otherContainer))
+        assertThat(ballContainer.pitch(otherContainer))
                 .isEqualTo(expected);
     }
 
     @DisplayName("2볼 1스트라이크 결과를 정확히 리턴하는지 확인합니다.")
     @Test
     void pitch_is1Strike2Ball() {
-        BallContainer container = BallContainer.getFromNumbers(List.of(5, 1, 9));
-        BallContainer otherContainer = BallContainer.getFromNumbers(List.of(1, 5, 9));
+        BallContainer otherContainer = BallContainer.getFromNumbers(List.of(6, 5, 2));
 
         PitchResult expected = new PitchResult(1, 2);
 
-        assertThat(container.pitch(otherContainer))
+        assertThat(ballContainer.pitch(otherContainer))
                 .isEqualTo(expected);
     }
 
     @DisplayName("1볼 1스트라이크 결과를 정확히 리턴하는지 확인합니다.")
     @Test
     void pitch_is1Strike1Ball() {
-        BallContainer container = BallContainer.getFromNumbers(List.of(7, 1, 9));
-        BallContainer otherContainer = BallContainer.getFromNumbers(List.of(1, 5, 9));
+        BallContainer otherContainer = BallContainer.getFromNumbers(List.of(2, 6, 1));
 
         PitchResult expected = new PitchResult(1, 1);
 
-        assertThat(container.pitch(otherContainer))
+        assertThat(ballContainer.pitch(otherContainer))
                 .isEqualTo(expected);
     }
 
     @DisplayName("2볼 결과를 정확히 리턴하는지 확인합니다.")
     @Test
     void pitch_is2Ball() {
-        BallContainer container = BallContainer.getFromNumbers(List.of(5, 1, 9));
-        BallContainer otherContainer = BallContainer.getFromNumbers(List.of(1, 5, 8));
+        BallContainer otherContainer = BallContainer.getFromNumbers(List.of(2, 5, 8));
 
         PitchResult expected = new PitchResult(0, 2);
 
-        assertThat(container.pitch(otherContainer))
+        assertThat(ballContainer.pitch(otherContainer))
                 .isEqualTo(expected);
     }
 
     @DisplayName("1볼 결과를 정확히 리턴하는지 확인합니다.")
     @Test
     void pitch_is1Ball() {
-        BallContainer container = BallContainer.getFromNumbers(List.of(3, 1, 9));
         BallContainer otherContainer = BallContainer.getFromNumbers(List.of(1, 5, 8));
 
         PitchResult expected = new PitchResult(0, 1);
 
-        assertThat(container.pitch(otherContainer))
+        assertThat(ballContainer.pitch(otherContainer))
                 .isEqualTo(expected);
     }
 
     @DisplayName("낫싱 결과를 정확히 리턴하는지 확인합니다.")
     @Test
     void pitch_isNothing() {
-        BallContainer container = BallContainer.getFromNumbers(List.of(3, 1, 9));
-        BallContainer otherContainer = BallContainer.getFromNumbers(List.of(2, 4, 6));
+        BallContainer otherContainer = BallContainer.getFromNumbers(List.of(1, 3, 9));
 
         PitchResult expected = new PitchResult(0, 0);
 
-        assertThat(container.pitch(otherContainer))
+        assertThat(ballContainer.pitch(otherContainer))
                 .isEqualTo(expected);
     }
 }
--- src/test/java/baseball/BallTest.java
+++ src/test/java/baseball/BallTest.java
@@ -2,15 +2,23 @@ package baseball;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class BallTest {
+    static Ball ball;
+
+    @BeforeAll
+    static void setUp() {
+        ball = new Ball(3, 2);
+    }
 
     @DisplayName("스트라이크 결과를 정확히 판단하는지 확인합니다.")
     @Test
     void isStrike() {
-        Ball ball = new Ball(3, 2);
         Ball other = new Ball(3, 2);
 
         assertThat(ball.isStrike(other))
@@ -18,10 +26,10 @@ class BallTest {
     }
 
     @DisplayName("볼 결과를 정확히 판단하는지 확인합니다.")
-    @Test
-    void isBall() {
-        Ball ball = new Ball(3, 1);
-        Ball other = new Ball(3, 2);
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1})
+    void isBall(int location) {
+        Ball other = new Ball(3, location);
 
         assertThat(ball.isBall(other))
                 .isTrue();
