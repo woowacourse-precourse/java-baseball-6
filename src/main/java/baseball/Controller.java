dfc01e545d0e5a5a8f9f47154a44d6471fc3e358
Seyeon <wjdtpdus828@naver.com>
2023-10-23 20:59:39 +0900

    feat(Controller): sendHint, sendRestartOrExit, setFlage 메소드 구현

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -12,26 +12,40 @@ public class Controller{
     Model model = new Model();
     View view = new View();
     public void gameStart(){
-        view.startMassge();//view에서 startMassage() 실행
+        view.startMassage();//view에서 startMassage() 실행
         computerNum = model.initComputerNum();// model에서 initComputerNum() 실행
 
         while(!flag) { //while을 통해 반복하고, flag로 반복을 제어한다.
             playerNum = model.initPlayerNum();// model에서 initPlaterNum() 실행
             countBallStrike(computerNum, playerNum);
         }
-
     }
     public void countBallStrike(List<Integer> computer, List<Integer> player){
         int ballCount = 0;
         int strikeCount = 0;
-        ballCount = model.countball(computerNum,playerNum);
-        strikeCount = model.countstrike(computerNum, playerNum);
-
+        ballCount = model.countBall(computerNum,playerNum);
+        strikeCount = model.countStrike(computerNum, playerNum);
+        sendHint(model.initHint(ballCount, strikeCount));
+    }
+    public void sendHint(String hint){
+        view.printHint(hint);
+        if (hint.equals(ANSWER_SENTENCE)){
+            sendRestartOrexit();
+        }
+    }
+    public void sendRestartOrExit(){
+        view.printRestartOrExit();
+        setFlage(model.inputRestartOrExit());
+    }
+    public void setFlage(int setNum){
+        if (setNum == 2){
+            flag = true;
+        }
+        else{
+            computerNum = model.initComputerNum();
+        }
 
     }
-
-
-
 
 
 }
