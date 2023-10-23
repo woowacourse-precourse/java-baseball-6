3322150271704467decb2f1e5bea5cf5e28904ad
최진원 <etudent39@gmail.com>
2023-10-23 23:57:37 +0900

    컴퓨터의 입력 처리에 대한 기능 구현: 수정예정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -27,5 +27,14 @@ public class Application {
         if(userNumberSet.size() != userNumberList.size()){
             System.exit(0);
         }
+
+        List<Integer> computer = new ArrayList<Integer>();
+        while(computer.size() < 3){
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if(!computer.contains(randomNumber)){
+                computer.add(randomNumber);
+            }
+        }
+
     }
 }
