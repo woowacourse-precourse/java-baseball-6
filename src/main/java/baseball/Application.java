9ea213288c49c0f319e4afb182992e121d5d2953
Controllls <whdrms9459@naver.com>
2023-10-23 15:37:34 +0900

    add : 컴퓨터가 서로 다른 랜덤한 숫자 3개를 선택 기능 제작

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,13 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        //개임 시작 문구 출력
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        Computer computer = new Computer();
+        //컴퓨터가 서로 다른 랜덤한 숫자 3개를 선택한다(1)
+        computer.GenerateRandomNumber();
     }
+
+
 }
---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,25 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private List<Integer> numbers;
+
+    public void GenerateRandomNumber() {
+        List<Integer> numberList = new ArrayList<>();
+        while (numberList.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!numberList.contains(randomNumber)) {
+                numberList.add(randomNumber);
+            }
+        }
+        this.numbers = numberList;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+}
