d111d8bc2a3b5adf49656811bb3a975bdb23fee1
ysparrk <youngseo0703@gmail.com>
2023-10-24 01:45:43 +0900

    feat: 어플리케이션 실행 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        GenerateRandomNum randomNum = new GenerateRandomNum();
+        Input input = new Input();
+        Judge judge = new Judge();
+        PlayAgain playagain = new PlayAgain();
+        boolean again = true;
+
+        while (again){
+            List<Integer> computer = randomNum.create();
+            String result = "";
+            while (!result.equals("3스트라이크")){
+                result = judge.judgement(computer, input.playerNumber());
+                System.out.println(result);
+            }
+            again = playagain.playAgain();
+        }
     }
 }
