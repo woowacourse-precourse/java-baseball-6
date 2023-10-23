055fd2c548b62fd0b142085d436748bf10ce78e6
Sully <2dcoder@naver.com>
2023-10-23 11:42:54 +0900

    refactor: 게임 실행 로직을 인스턴스화하여 실행하도록 변경
    그 전에는 static 메서드였으나 인스턴스 메서드로 변경되었기 때문이다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,7 +3,8 @@ package baseball;
 public class Application {
 
     public static void main(String[] args) {
-        Game.start();
+        Game game = new Game();
+        game.start();
     }
 
 }
