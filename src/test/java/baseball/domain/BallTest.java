c23eba70ad34b0c8e998ed34d7a1da1e66afcc10
gmelon <hsh1769@naver.com>
2023-10-23 17:11:18 +0900

    refactor: 테스트 메서드에서 public 접근제어자 제거

--- src/test/java/baseball/domain/BallTest.java
+++ src/test/java/baseball/domain/BallTest.java
@@ -14,7 +14,7 @@ class BallTest {
     @DisplayName("1-9사이의 한 자리 숫자가 입력되면 Ball이 생성된다")
     @ParameterizedTest
     @ValueSource(strings = {"1", "5", "9"})
-    public void from(String givenValue) {
+    void from(String givenValue) {
         Ball ball = assertDoesNotThrow(() -> Ball.from(givenValue));
 
         assertThat(ball.number()).isEqualTo(Integer.parseInt(givenValue));
@@ -23,14 +23,14 @@ class BallTest {
     @DisplayName("숫자가 아닌 문자나 2자리 이상 숫자, 0이 입력되면 예외가 발생한다")
     @ParameterizedTest
     @ValueSource(strings = {"abc", "1a", "$a", "53", "0", ""})
-    public void invalid_from(String givenValue) {
+    void invalid_from(String givenValue) {
         assertThatThrownBy(() -> Ball.from(givenValue))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("Ball이 가진 number를 기준으로 동등성 비교가 이루어진다")
     @Test
-    public void equals() {
+    void equals() {
         Ball three = Ball.from("3");
 
         assertThat(three.equals(Ball.from("3"))).isTrue();
--- src/test/java/baseball/domain/BallsTest.java
+++ src/test/java/baseball/domain/BallsTest.java
@@ -12,14 +12,14 @@ class BallsTest {
     @DisplayName("3자리 1-9 사이의 숫자가 입력되면 Balls가 생성된다")
     @ParameterizedTest
     @ValueSource(strings = {"123", "542", "193"})
-    public void from(String givenValue) {
+    void from(String givenValue) {
         assertDoesNotThrow(() -> Balls.from(givenValue));
     }
 
     @DisplayName("숫자가 아닌 문자, 3자리가 아닌 숫자, 중복된 숫자, 0을 포함한 숫자가 입력되면 예외가 발생한다")
     @ParameterizedTest
     @ValueSource(strings = {"aㅇ1", "$", "", "1", "12", "112", "902"})
-    public void invalid_from(String givenValue) {
+    void invalid_from(String givenValue) {
         assertThatThrownBy(() -> Balls.from(givenValue))
                 .isInstanceOf(IllegalArgumentException.class);
     }
