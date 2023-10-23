f742ea862608936b51353842807af1a1839b7201
PARK9898 <wjddjs21@naver.com>
2023-10-24 01:35:19 +0900

    "feat: Strike 계산 메서드 구현"

--- src/main/java/baseball/domain/Numbers.java
+++ src/main/java/baseball/domain/Numbers.java
@@ -4,6 +4,8 @@ import static baseball.domain.NumbersValidator.*;
 
 import java.util.List;
 
+import java.util.stream.IntStream;
+
 public class Numbers {
 	private static final int NUMBERS_LENGTH = 3;
 	private final List<Integer> numbers;
@@ -20,4 +22,9 @@ public class Numbers {
 		this.numbers = randomNumbersGenerator.generateRandomNumbers();
 	}
 
+	public int countStrike(Numbers inputNumbers) {
+		return (int)IntStream.range(0, NUMBERS_LENGTH)
+			.filter(i -> numbers.get(i).equals(inputNumbers.numbers.get(i)))
+			.count();
+	}
 }
