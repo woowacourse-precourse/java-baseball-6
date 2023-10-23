cfe7232c144882078efc72d46f763daa2815c8b7
toychip <dlawnsgud427@naver.com>
2023-10-24 00:58:54 +0900

    fix: Scanner 사용시 전체 테스트 오류 발생 -> Console 사용으로 해결

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,11 +1,11 @@
 package baseball.view;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
+
 
 public class InputView {
-    private static final Scanner scanner = new Scanner(System.in);
 
     public static String getUserInput() {
-        return scanner.nextLine();
+        return Console.readLine();
     }
 }
