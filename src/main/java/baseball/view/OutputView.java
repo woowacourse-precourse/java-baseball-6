57b090599c155085f7fe680715cadae8305b6f4e
kongji <kongji4092@gmail.com>
2023-10-23 13:56:01 +0900

    refactor: OutputView 숫자 야구 결과 줄바꿈 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -5,9 +5,9 @@ public class OutputView {
     private static final String START_GAME_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String GET_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String NOTING_MESSAGE = "낫싱";
-    private static final String STRIKE_MESSAGE = "%d스트라이크";
-    private static final String BALL_MESSAGE = "%d볼";
-    private static final String BALL_STRIKE_MESSAGE = "%d볼 %d스트라이크";
+    private static final String STRIKE_MESSAGE = "%d스트라이크\n";
+    private static final String BALL_MESSAGE = "%d볼\n";
+    private static final String BALL_STRIKE_MESSAGE = "%d볼 %d스트라이크\n";
 
 
 
