d9c4b2a0e40024ba5db2d0929019936615ceefeb
qudwns017 <qudwns017@naver.com>
2023-10-23 22:17:46 +0900

    Feat: InputView 클래스의 getInput 함수 추가

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,10 @@
+package baseball.view;
+
+import baseball.domain.Messages;
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static String getInput() {
+        return Console.readLine();
+    }
+}
