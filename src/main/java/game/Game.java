0ebfd3a347552ce54fd9e9bf6d2749f82c4c3134
SeokEui Hong <hongs7701@naver.com>
2023-10-23 22:42:00 +0900

    #2. 10월 23일 작업 1) 요구 사항 및 기능 추가 -컨벤션 내용 중 확인할 것 2) 구현부 작업 (1) 입출력 형식 구현 : 완료
    (2) 내일 필수 수정 사항 :
    -컴퓨터 랜덤 숫자 발생
    -예외 처리

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -4,27 +4,27 @@ import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Game {
+    static boolean restart = true;
+
     public void start(){
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-        while(true){
+        while(restart){
             play();
-            break;
         }
-
     }
 
     private void play(){
-        String computer[] = getComputerPlay();
+        String[] computer = getComputerPlay();
 
-        String player[] = getUserPlay();
+        String[] player = getUserPlay();
 
-        judgePlay(computer, player);
+        checkPlay(computer, player);
     }
 
     private String[] getComputerPlay(){
-        //서로 다른 숫자 입력할 수 잇게끔 해야함
-        String computerPlay[] = new String[3];
+        //서로 다른 숫자 입력할 수 잇게끔 해야함 -> 리드미 참조!!
+        String[] computerPlay = new String[3];
 
         String firstBall = String.valueOf(Randoms.pickNumberInRange(1, 9));
         computerPlay[0] = firstBall;
@@ -39,12 +39,48 @@ public class Game {
     private String[] getUserPlay(){
         System.out.print("숫자를 입력해주세요 : ");
 
-        String userPlay[] = Console.readLine().split("");
+        String[] userPlay = Console.readLine().split("");
 
         return userPlay;
     }
 
-    private void judgePlay(String[] computerPlay, String[] userPlay){
-        System.out.println("x볼 x스트라이크");
+    private void checkPlay(String[] computerPlay, String[] userPlay){
+        int ballCount = 0;
+        int strikeCount = 0;
+
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                if(userPlay[i].equals(computerPlay[j]) && i == j){
+                    strikeCount++;
+                    break;
+                } else if(userPlay[i].equals(computerPlay[j]) && i != j){
+                    ballCount++;
+                }
+            }
+        }
+
+        if(ballCount != 0 && strikeCount != 0){
+            System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+        } else if(ballCount != 0 && strikeCount == 0){
+            System.out.println(ballCount + "볼");
+        } else if(ballCount == 0 && strikeCount != 0){
+            System.out.println(strikeCount + "스트라이크");
+            isRestart(strikeCount);
+        } else{
+            System.out.println("낫싱");
+        }
+    }
+
+    private void isRestart(int strikeCount){
+        if(strikeCount == 3){
+            String choose;
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+            choose = Console.readLine();
+            if(choose.equals("2")) {
+                restart = false;
+            }
+        }
     }
 }
