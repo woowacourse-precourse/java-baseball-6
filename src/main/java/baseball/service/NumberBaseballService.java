dc62c7c6cde8e02bb393a0da77de553ef11417cf
정찬영 <jcy0342@gmail.com>
2023-10-23 16:18:44 +0900

    feat(NumberBaseballService): 사용자와 컴퓨터의 숫자 비교 메서드 구현
    - 사용자와 컴퓨터의 숫자 비교를 위한 비교 메서드 구현

--- src/main/java/baseball/service/NumberBaseballService.java
+++ src/main/java/baseball/service/NumberBaseballService.java
@@ -2,6 +2,7 @@ package baseball.service;
 
 import baseball.model.Computer;
 import baseball.model.Player;
+import baseball.model.Rule;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,6 +21,29 @@ public class NumberBaseballService {
 
     public void setPlayerNumbers(String playerNumber) {
         player.setNumbers(stringToIntegerList(playerNumber));
+    }
+
+    public void comparePlayerWithComputer() {
+        for (int i = 0; i < Rule.MAX_LENGTH; i++) {
+            int playerNumber = player.getNumbers().get(i);
+            int computerNumber = computer.getNumbers().get(i);
+
+            if (isStrike(playerNumber, computerNumber)) {
+                player.addStrikeCount();
+                continue;
+            }
+            if (isBall(playerNumber)) {
+                player.addBallCount();
+            }
+        }
+    }
+
+    private boolean isStrike(int playerNumber, int computerNumber) {
+        return playerNumber == computerNumber;
+    }
+
+    private boolean isBall(int playerNumber) {
+        return computer.getNumbers().contains(playerNumber);
     }
 
     private List<Integer> stringToIntegerList(String stringNumber) {
