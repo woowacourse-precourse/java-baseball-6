04e71cfeece197285bb32a3deefc4a8c64d8da5e
ParkJinYong <mej1406@gmail.com>
2023-10-23 19:03:15 +0900

    feat: 투수 역할 및 숫자 입력 기능 추가

--- src/main/java/baseball/Pitcher.java
+++ src/main/java/baseball/Pitcher.java
@@ -1,4 +1,42 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class Pitcher {
+    private List<Integer> numbers;
+    private int maxSize;
+
+    public Pitcher(int maxSize) {
+        this.numbers = new ArrayList<>();
+        this.maxSize = maxSize;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public void getPlayerInput() {
+        int randomNumber = Integer.parseInt(Console.readLine());
+        Stack<Integer> split = splitNumbers(randomNumber);
+
+        while (numbers.size() < maxSize) {
+            numbers.add(split.pop());
+        }
+    }
+
+    public Stack<Integer> splitNumbers(int number) {
+        Stack<Integer> split = new Stack<>();
+
+        while (number != 0) {
+            int digit = number % 10;
+            split.push(digit);
+            number /= 10;
+        }
+
+        return split;
+    }
 }
