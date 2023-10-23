74f16876c02af97f408ff387545ff788001f7982
devholic22 <hyunjoon.tech@gmail.com>
2023-10-23 15:02:05 +0900

    Refactor: RefereeTest에서 BeforeEach 삭제

--- src/test/java/baseball/RefereeTest.java
+++ src/test/java/baseball/RefereeTest.java
@@ -6,27 +6,18 @@ import baseball.model.PlayNumber;
 import baseball.model.Referee;
 import baseball.model.StrikeRule;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RefereeTest {
 
-    private GameRule strikeRule;
-    private GameRule ballRule;
-
-    @BeforeEach
-    void beforeEach() {
-        strikeRule = new StrikeRule();
-        ballRule = new BallRule();
-    }
-
     @Test
     void 스트라이크_테스트() {
         // given
+        GameRule gameRule = new StrikeRule();
         PlayNumber computerNumber = PlayNumber.from("325");
         PlayNumber userNumber = PlayNumber.from("123");
         // when
-        int strike = Referee.answerResult(strikeRule, computerNumber, userNumber);
+        int strike = Referee.answerResult(gameRule, computerNumber, userNumber);
         // then
         Assertions.assertThat(strike).isEqualTo(1);
     }
@@ -34,10 +25,11 @@ public class RefereeTest {
     @Test
     void 볼_테스트() {
         // given
+        GameRule gameRule = new BallRule();
         PlayNumber computerNumber = PlayNumber.from("123");
         PlayNumber userNumber = PlayNumber.from("256");
         // when
-        int ball = Referee.answerResult(ballRule, computerNumber, userNumber);
+        int ball = Referee.answerResult(gameRule, computerNumber, userNumber);
         // then
         Assertions.assertThat(ball).isEqualTo(1);
     }
