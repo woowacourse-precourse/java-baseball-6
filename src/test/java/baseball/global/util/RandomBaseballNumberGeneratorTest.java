55d518c88a45fd5d91d885783aba8ebf3da678fe
yshong1998 <yshong1998@gmail.com>
2023-10-23 22:23:42 +0900

    Test(RandomBaseballNumberGenerator): generateRandomBaseballNumbers의 테스트 생성

---
+++ src/test/java/baseball/global/util/RandomBaseballNumberGeneratorTest.java
@@ -0,0 +1,24 @@
+package baseball.global.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RandomBaseballNumberGeneratorTest {
+
+    @DisplayName("자릿수에 맞게 서로 다른 난수 생성 후 리스트로 반환")
+    @ParameterizedTest
+    @ValueSource(ints = {2,3,4})
+    void generateRandomBaseballNumberTest(int size){
+        List<Integer> randomBaseballNumbers = RandomBaseballNumberGenerator.generateRandomBaseballNumbers(size);
+        assertTrue(randomBaseballNumbers.stream()
+                .distinct()
+                .count() == randomBaseballNumbers.size());
+    }
+}
