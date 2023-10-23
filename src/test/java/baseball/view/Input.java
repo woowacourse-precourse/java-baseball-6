d05895dc65f6325b424b2d9edf1fe6fe37389bf2
ts3083 <ts04031@gmail.com>
2023-10-23 16:31:46 +0900

    fix: Input 출력 메시지 열바꿈 수정

--- src/test/java/baseball/view/Input.java
+++ src/test/java/baseball/view/Input.java
@@ -5,10 +5,10 @@ import camp.nextstep.edu.missionutils.Console;
 public class Input {
 
     public static final String askNumberMessage = "숫자를 입력해주세요 : ";
-    public static final String askRestartOrEndMessage = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
+    public static final String askRestartOrEndMessage = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public String getPlayerNumber() {
-        System.out.println(askNumberMessage);
+        System.out.print(askNumberMessage);
         return Console.readLine();
     }
 
