15b538cf7b53b2b6b1ba7069becd79798bceac75
Hyunsoo Kim <sgn06221@gmail.com>
2023-10-23 15:07:21 +0900

    [feat] 플레이어 입력한 숫자 반환

--- src/main/java/baseball/model/PlayerNumber.java
+++ src/main/java/baseball/model/PlayerNumber.java
@@ -1,4 +1,18 @@
 package baseball.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayerNumber {
+    //플레이어가 저장한 3개의 숫자 저장
+    private List<Integer> playerNumberList;
+
+    public PlayerNumber(){
+        playerNumberList = new ArrayList<>();
+    }
+
+    //플레이어가 입력한 숫자 반환
+    public void getPlayerNumber(List<Integer> numbers){
+        playerNumberList = new ArrayList<>(numbers);
+    }
 }
