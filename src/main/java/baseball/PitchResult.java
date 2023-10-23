cd0fd555540731fe66843bf75798d01637df4b95
임강호 <55732952+Icln@users.noreply.github.com>
2023-10-23 22:59:26 +0900

    refactor: 재시작 기능 분리 및 변수 수정

--- src/main/java/baseball/PitchResult.java
+++ src/main/java/baseball/PitchResult.java
@@ -1,19 +1,17 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class PitchResult {
     private int ball;
     private int strike;
 
-    private int restartNumber;
-    private RandomNumber randomNumber;
+    private final RandomNumber randomNumber;
 
-    private Validation validation;
-
-    public PitchResult(RandomNumber randomNumber, Validation validation) {
+    public PitchResult(RandomNumber randomNumber) {
         this.randomNumber = randomNumber;
-        this.validation = validation;
+    }
+
+    public int getStrike() {
+        return strike;
     }
 
     public void addBall(int ball) {
@@ -24,26 +22,18 @@ public class PitchResult {
         this.strike = strike + 1;
     }
 
-    public void setRestartNumber(int restartNumber) {
-        this.restartNumber = restartNumber;
-    }
-
-    public int getRestartNumber() {
-        return restartNumber;
-    }
-
     public void judgePitch(String inputString) {
-        for (int i = InputRange.START_INDEX.getValue(); i < InputRange.CORRECT_INPUT_RANGE.getValue(); i++) {
-            if (randomNumber.getRandomNumber().get(i).equals(Character.getNumericValue(inputString.charAt(i)))) {
+        for (int idx = InputRange.START_INDEX.getValue(); idx < InputRange.CORRECT_INPUT_RANGE.getValue(); idx++) {
+            if (randomNumber.getRandomNumber().get(idx).equals(Character.getNumericValue(inputString.charAt(idx)))) {
                 addStrike(strike);
-            } else if (randomNumber.getRandomNumber().contains(Character.getNumericValue(inputString.charAt(i)))) {
+            } else if (randomNumber.getRandomNumber().contains(Character.getNumericValue(inputString.charAt(idx)))) {
                 addBall(ball);
             }
         }
     }
 
     public void printPitchResult() {
-        if (strike == PitchRange.MIN_STRIKES.getValue() && ball == PitchRange.MAX_BALLS.getValue()) {
+        if (strike == PitchRange.MIN_STRIKES.getValue() && ball == PitchRange.MIN_BALLS.getValue()) {
             System.out.println("낫싱");
             return;
         }
@@ -52,12 +42,6 @@ public class PitchResult {
             System.out.println("3스트라이크");
             System.out.println("3개의 숫자를 모두 모두 맞히셨습니다! 게임 종료");
             System.out.println("게임을 새로 시작하려면 1, 종료하시면 2를 입력하세요.");
-
-            setRestartNumber(Integer.parseInt(Console.readLine()));
-            validation.restartValidation(restartNumber);
-            if (restartNumber == GameOption.RESTART_GAME.getOption()) {
-                randomNumber.randomNumberGenerate();
-            }
             return;
         }
 
