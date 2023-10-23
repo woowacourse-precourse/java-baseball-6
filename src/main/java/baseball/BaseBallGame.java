70a423e965b0bb0b697722e32b7e95d53797fea6
JeongHunHui <deca3248@gmail.com>
2023-10-23 20:30:45 +0900

    feat: 숫자 입력 메시지 출력하기

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -5,7 +5,7 @@ public class BaseBallGame {
     private Computer computer;
 
     public void play() {
-        
+        System.out.print(GameMessage.NUMBER_INPUT);
     }
 
     public BaseBallGame() {
--- src/main/java/baseball/GameMessage.java
+++ src/main/java/baseball/GameMessage.java
@@ -1,7 +1,8 @@
 package baseball;
 
 public enum GameMessage {
-    GAME_START("숫자 야구 게임을 시작합니다.");
+    GAME_START("숫자 야구 게임을 시작합니다."),
+    NUMBER_INPUT("숫자를 입력해주세요 : ");
 
     private final String message;
 
