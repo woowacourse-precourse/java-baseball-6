268cae7b72465de82a444ae6510ad093afef9510
sojungpp <sophie2458@naver.com>
2023-10-24 04:02:04 +0900

    feat: 3스트라이크인 경우, 재시작/종료 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -15,51 +15,68 @@ public class Application {
         // 게임 시작
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-        // 랜덤 3자리의 수 생성
-        List<Integer> computerNums = new ArrayList<>();
-        while (computerNums.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computerNums.contains(randomNumber)) {
-                computerNums.add(randomNumber);
+        int cmd;
+        do {
+            cmd = 0;
+            // 랜덤 3자리의 수 생성
+            List<Integer> computerNums = new ArrayList<>();
+            while (computerNums.size() < 3) {
+                int randomNumber = Randoms.pickNumberInRange(1, 9);
+                if (!computerNums.contains(randomNumber)) {
+                    computerNums.add(randomNumber);
+                }
             }
-        }
 
-        // 3자리의 수 입력 받기
-        System.out.println("숫자를 입력해주세요 : ");
-        String input = Console.readLine();
+            do {
+                // 3자리의 수 입력 받기
+                System.out.println("숫자를 입력해주세요 : ");
+                String input = Console.readLine();
 
-        List<Integer> userNums = new ArrayList<>();
-        Arrays.stream(input.split("")).forEach(str -> {
-            try {
-                Integer num = Integer.parseInt(str);
-                // 각 자릿수끼리 중복된 수를 입력한 경우 || 각 자릿수가 1 ~ 9까지의 수가 아닌 경우
-                if (userNums.contains(num) || num == 0) throw new IllegalArgumentException();
-                userNums.add(num);
-            }
-            // 숫자 외의 문자를 입력한 경우
-            catch (NumberFormatException e) {
-                throw new IllegalArgumentException();
-            }
-        });
+                List<Integer> userNums = new ArrayList<>();
+                Arrays.stream(input.split("")).forEach(str -> {
+                    try {
+                        Integer num = Integer.parseInt(str);
+                        // 각 자릿수끼리 중복된 수를 입력한 경우 || 각 자릿수가 1 ~ 9까지의 수가 아닌 경우
+                        if (userNums.contains(num) || num == 0) throw new IllegalArgumentException();
+                        userNums.add(num);
+                    }
+                    // 숫자 외의 문자를 입력한 경우
+                    catch (NumberFormatException e) {
+                        throw new IllegalArgumentException();
+                    }
+                });
 
-        // 입력 받은 수가 3자리가 아닌 경우
-        if (userNums.size() != 3) throw new IllegalArgumentException();
+                // 입력 받은 수가 3자리가 아닌 경우
+                if (userNums.size() != 3) throw new IllegalArgumentException();
 
-        int ball = 0;
-        int strike = 0;
 
-        // 비교
-        for (int i = 0; i < 3; i++) {
-            if (computerNums.contains(userNums.get(i))) {
-                if (computerNums.get(i).equals(userNums.get(i))) strike++;
-                else ball++;
-            }
-        }
+                int ball = 0;
+                int strike = 0;
 
-        // 힌트 제공
-        if (ball == 0 && strike == 0) System.out.print("낫싱");
-        if (ball!=0) System.out.print(ball + "볼" + " ");
-        if (strike!=0) System.out.print(strike + "스트라이크");
+                // 비교
+                for (int i = 0; i < 3; i++) {
+                    if (computerNums.contains(userNums.get(i))) {
+                        if (computerNums.get(i).equals(userNums.get(i))) strike++;
+                        else ball++;
+                    }
+                }
 
+                // 힌트 제공
+                if (ball == 0 && strike == 0) System.out.print("낫싱");
+                if (ball!=0) System.out.print(ball + "볼" + " ");
+                if (strike!=0) System.out.print(strike + "스트라이크");
+                if(strike == 3) {
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                    try{
+                        cmd = Integer.parseInt(Console.readLine());
+                        if(cmd!=1 && cmd!=2) throw new IllegalArgumentException();
+                    } catch (NumberFormatException e) {
+                        throw new IllegalArgumentException();
+                    }
+                }
+
+            } while(cmd != 2 && cmd != 1);
+        } while(cmd != 2);
     }
 }
