79c83af6a09e16b30d520b796c5180380f69c267
HongSP99 <tmdvy0805@naver.com>
2023-10-24 00:26:56 +0900

    feat : 유저의 숫자를 입력받는 기능과 List 크기, 숫자  중복 체크 기능 추가

--- src/main/java/baseball/Error.java
+++ src/main/java/baseball/Error.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import java.util.List;
+
 public class Error {
     //숫자인지 아닌지 확인하는 예외처리
     public static boolean is_Not_number(String str) {
@@ -8,6 +10,21 @@ public class Error {
             return false;
         } catch (IllegalArgumentException e){
             return true;
+        }
+    }
+
+    //숫자지만 잘못된 값 또는 잘못된 개수 또는 중복된 숫자를 입력한 예외처리
+    public static void check_Numsize(List<Number> User_num) {
+        //List의 크기가 3이상이면 예외처리
+        if(User_num.size() != 3) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void is_Duplicate(List<Number> User_num){
+        //중복된 숫자가 있다면 예외처리
+        if(User_num.size() != User_num.stream().distinct().count()) {
+            throw new IllegalArgumentException();
         }
     }
 }
--- src/main/java/baseball/NumberGroup.java
+++ src/main/java/baseball/NumberGroup.java
@@ -1,9 +1,29 @@
 package baseball;
 
+import java.util.ArrayList;
 import java.util.List;
 public class NumberGroup {
     private List<Number> numbers;
     public NumberGroup(List<Number> numbers){
+        Error.check_Numsize(numbers);
+        Error.is_Duplicate(numbers);
         this.numbers = numbers;
+    }
+
+    public static NumberGroup user_num(String user_num_str){
+        List<Number> user_num = new ArrayList<>();
+
+        if(Error.is_Not_number(user_num_str)){
+            throw new IllegalArgumentException("숫자를 입력해주세요..");
+        }
+
+        String[] token = user_num_str.split("");
+
+        for(int i = 0 ; i<user_num_str.length() ; i++){
+            Number num = new Number(Integer.parseInt(token[i]));
+            user_num.add(num);
+        }
+
+        return new NumberGroup(user_num);
     }
 }
