63ce4c670d6310f26e8c053eafd7604d2b5a5dc0
minjun7410 <minjun7410@gmail.com>
2023-10-23 14:58:04 +0900

    refactor(NumberInput): NumberInput 생성
    * 숫자 입력할 때 사이즈 예외처리를 하고 문자를 리턴하는 NumberInput 클래스 생성

---
+++ src/main/java/baseball/NumberInput.java
@@ -0,0 +1,17 @@
+package baseball;
+
+public class NumberInput {
+    private String numberInput;
+    NumberInput(String numberInput){
+        validateUserInputSize(numberInput);
+        this.numberInput = numberInput;
+    }
+    public void validateUserInputSize(String input){
+        if(input.length() != 3){
+            throw new IllegalArgumentException("3자리 숫자를 입력해주세요.");
+        }
+    }
+    public char getNumberByIndex(int index){
+        return numberInput.charAt(index);
+    }
+}
