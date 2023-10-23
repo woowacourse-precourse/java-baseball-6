85a036ec48294d1acf7a99ea0d33ab9351ac2eee
Seungjida <esg0107@pusan.ac.kr>
2023-10-23 21:40:47 +0900

    Feat: "재게임 여부 입력 기능 추가"

--- src/main/java/others/Others.java
+++ src/main/java/others/Others.java
@@ -2,6 +2,8 @@ package others;
 
 import static others.ExceptionHandling.restartException;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Others {
     public static boolean ifRestart() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
