f28fae90ac0f2c76b700fc03cfdac344ff9753a0
biiit4894 <ghiesf@naver.com>
2023-10-23 17:15:43 +0900

    [1주차 온보딩 미션]난수 생성과 사용자 입력 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,31 @@
 package baseball;
+import camp.nextstep.edu.missionutils.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        ComputerNumbers();
+        PlayerNumbers();
+    }
+
+    public static void ComputerNumbers() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if(!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+    }
+
+    public static ArrayList<String> PlayerNumbers() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String playerNumber = Console.readLine();
+        ArrayList<String> playerNumbers = new ArrayList<>();
+        playerNumbers.add(playerNumber);
+        return playerNumbers;
     }
 }
