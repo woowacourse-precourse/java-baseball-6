2d945ea287e42eab130ca3dde641ca86f0dc4324
qudwns017 <qudwns017@naver.com>
2023-10-24 01:50:47 +0900

    Refactor: 필요없는 단어 및 변수 이름 간략화

--- src/main/java/baseball/service/ComputerService.java
+++ src/main/java/baseball/service/ComputerService.java
@@ -1,6 +1,7 @@
 package baseball.service;
 
-import baseball.domain.Constants;
+import static baseball.util.Constants.*;
+import baseball.util.Constants;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,9 +18,9 @@ public class ComputerService {
     }
 
     private static List<Integer> generateRandomNumbers() {
-        List<Integer> RandomNumbers = new ArrayList<Integer>();
+        List<Integer> RandomNumbers = new ArrayList<>();
         while (RandomNumbers.size() < Constants.NUMBERS_SIZE) {
-            int num = Randoms.pickNumberInRange(Constants.NUMBER_MIN_RANGE, Constants.NUMBER_MAX_RANGE);
+            int num = Randoms.pickNumberInRange(NUMBER_MIN_RANGE, NUMBER_MAX_RANGE);
             if (!RandomNumbers.contains(num)) {
                 RandomNumbers.add(num);
             }
