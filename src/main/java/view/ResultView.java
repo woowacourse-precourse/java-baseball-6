768d4ecdc17e65db3eb2e3b3632c4d45e42ee377
Son JunHyung <kingjh1125@gmail.com>
2023-10-23 13:57:19 +0900

    refactor: 매직넘버 상수로 대체

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -7,6 +7,7 @@
  * DATE             AUTHOR           NOTE
  * ----------------------------------------------------------------
  * 2023-10-22       손준형           최초 생성
+ * 2023-10-23       손준형           매직넘버 상수 대체
  * </pre>
  *
  * @author 손준형
@@ -19,18 +20,25 @@ package view;
 import vo.ResultBall;
 
 public class ResultView {
+    public static final int STRIKE_THREE = 3;
+
+    public static final int STRIKE_ZERO = 0;
+    public static final int BALL_THREE = 0;
+    public static final int BALL_ZERO = 0;
+
     public static Boolean resultView(ResultBall resultBall) {
-        if (resultBall.getStrike() == 3) {
+        int strike = resultBall.getStrike();
+        int ball = resultBall.getBall();
+        if (strike == STRIKE_THREE) {
             System.out.println(
                     "3스트라이크\n"
                             + "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"
                             + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             return true;
-        } else if (resultBall.getStrike() == 0 && resultBall.getBall() == 0) {
+        } else if (strike == STRIKE_ZERO && ball == BALL_ZERO) {
             System.out.println("낫싱");
-        } else if (0 <= resultBall.getStrike() && resultBall.getStrike() <= 2 && 0 <= resultBall.getBall()
-                && resultBall.getBall() <= 2) {
-            System.out.println(resultBall.getBall() + "볼 " + resultBall.getStrike() + "스트라이크");
+        } else if (STRIKE_ZERO <= strike && strike < STRIKE_THREE && BALL_ZERO <= ball && ball < BALL_THREE) {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
         }
         return false;
     }
