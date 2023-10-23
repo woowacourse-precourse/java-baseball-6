598ab249a2a4fc91a3927890716d83422cb958bb
shin5774 <babo5774@naver.com>
2023-10-24 00:50:07 +0900

    Test: "테스트파일 정리"
    - 기존 테스트코드를 클래스들의 구조가 변경됨에 따라 제거 및 수정을 진행함.

--- src/test/java/baseball/controller/GameControllerTest.java
+++ src/test/java/baseball/controller/GameControllerTest.java
@@ -1,48 +1,5 @@
 package baseball.controller;
 
-import baseball.domain.Numbers;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-
 public class GameControllerTest {
 
-    @DisplayName("String을 Numbers로 변환해주는 기능")
-    @Test
-    void 사용자_입력값_convert(){
-        //given
-        GameController controller=new GameController();
-        String input="123";
-
-        //when
-        Numbers numbers=controller.convertToNumbers(input);
-
-        //then
-        assertThat(numbers.splitNumbers()).isEqualTo(input.split(""));
-    }
-
-    @DisplayName("재시작여부 입력값의 validate 기능")
-    @ParameterizedTest
-    @ValueSource(strings={"11","21","1a","a1","a","4","0"})
-    void 재시작_입력값_validate(String input){
-        //given
-        GameController controller=new GameController();
-
-        //when&then
-        assertThatThrownBy(()->controller.isContinue(input))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("입력값에 따라 게임 재시작여부를 결정하는 기능")
-    @ParameterizedTest
-    @CsvSource(value = {"1:true","2:false"},delimiter = ':')
-    void 재시작_기능(String input,boolean expected){
-        GameController controller=new GameController();
-        assertThat(controller.isContinue(input)).isEqualTo(expected);
-    }
 }
--- src/test/java/baseball/view/InputViewTest.java
+++ src/test/java/baseball/view/InputViewTest.java
@@ -1,6 +1,5 @@
 package baseball.view;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,31 +9,27 @@ import java.io.InputStream;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class InputViewTest {
-    private static InputView view;
 
-    @BeforeAll
-    static void 뷰_초기화(){
-        view=new InputView();
-    }
+    private static InputView view=new InputView();
 
     @DisplayName("사용자 Numbers 입력 기능")
     @Test
     void 사용자_Numbers_입력_테스트(){
         String input="123";
-        InputStream in=new ByteArrayInputStream((input).getBytes());
+        InputStream in=new ByteArrayInputStream((input+"\n").getBytes());
         System.setIn(in);
 
         assertThat(view.inputNumbers()).isEqualTo(input);
     }
-
+    /*
     @DisplayName("사용자 게임 재시작 여부 입력 기능")
     @Test
     void 사용자_재시작_입력_테스트(){
         String input="1";
-        InputStream in=new ByteArrayInputStream((input).getBytes());
+        InputStream in=new ByteArrayInputStream((input+'\n').getBytes());
         System.setIn(in);
 
         assertThat(view.inputContinue()).isEqualTo(input);
     }
-
+     */
 }
