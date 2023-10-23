1130132868d303d37ec814b8f7d38e616577e414
twoosky <lhjae3@naver.com>
2023-10-24 02:58:59 +0900

    refactor: Baseball 객체에서 ball, strike 개수 비교하도록 리팩토링

--- src/main/java/baseball/domain/BaseballResult.java
+++ src/main/java/baseball/domain/BaseballResult.java
@@ -2,30 +2,27 @@ package baseball.domain;
 
 import baseball.utils.ErrorMessages;
 import java.util.Arrays;
-import java.util.function.BiPredicate;
+import java.util.function.Function;
 
 public enum BaseballResult {
-    BALL_STRIKE("{0}볼 {1}스트라이크", (ball, strike) -> ball > 0 && strike > 0),
-    BALL("{0}볼", (ball, strike) -> ball > 0 && strike == 0),
-    ALL_STRIKE("{1}스트라이크", (ball, strike) -> ball == 0 && strike == 3),
-    STRIKE("{1}스트라이크", (ball, strike) -> ball == 0 && strike > 0),
-    NOTHING("낫싱", (ball, strike) -> ball == 0 && strike == 0),
+    BALL_STRIKE("{0}볼 {1}스트라이크", Baseball::isBallAndStrike),
+    BALL("{0}볼", Baseball::isBall),
+    ALL_STRIKE("{1}스트라이크", Baseball::isAllStrike),
+    STRIKE("{1}스트라이크", Baseball::isStrike),
+    NOTHING("낫싱", Baseball::isNothing),
     ;
 
     private final String messageFormat;
-    private final BiPredicate<Integer, Integer> matcher;
+    private final Function<Baseball, Boolean> matcher;
 
-    private BaseballResult(String messageFormat, BiPredicate<Integer, Integer> matcher) {
+    private BaseballResult(String messageFormat, Function<Baseball, Boolean> matcher) {
         this.messageFormat = messageFormat;
         this.matcher = matcher;
     }
 
     public static BaseballResult of(Baseball baseball) {
-        int ball = baseball.getBall();
-        int strike = baseball.getStrike();
-
         return Arrays.stream(BaseballResult.values())
-                .filter(value -> value.matcher.test(ball, strike))
+                .filter(value -> value.matcher.apply(baseball))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(ErrorMessages.NOT_MATCH_BASEBALL_RESULT));
     }
