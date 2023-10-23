ce6f7d132e757de31fced8e36dee10ba8b3150a9
HongSP99 <tmdvy0805@naver.com>
2023-10-24 00:58:40 +0900

    feat : main 구현 및 Strike 값을 구하기 위해 equals() override

--- src/main/java/baseball/Number.java
+++ src/main/java/baseball/Number.java
@@ -6,4 +6,17 @@ public class Number {
     public Number(int num) {
         this.num = num;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;  // 같은 객체인 경우 true 반환
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Number otherNumber = (Number) obj;
+        return this.num == otherNumber.num;
+    }
 }
