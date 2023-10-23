8199ece4c927dee41fcc9de7eab314ba4f3ba37c
Kim Mi Ju <76895039+miju0515@users.noreply.github.com>
2023-10-23 15:33:19 +0900

    refactor: 랜덤 숫자 생성 메서드 추출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,12 +12,8 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
-        while(answer.size()<3){
-            int randomNumber = Randoms.pickNumberInRange(1,9);
-            if(!answer.contains(randomNumber)){
-                answer.add(randomNumber);
-            }
-        }
+        makeNumber();
+
         while(true){
             System.out.print("숫자를 입력해주세요 : ");
             String input = readLine();
@@ -27,6 +23,17 @@ public class Application {
                 if(endGame() == 2){
                     break;
                 }
+                makeNumber();
+            }
+        }
+    }
+
+    public static void makeNumber(){
+        answer.clear();
+        while(answer.size()<3){
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if(!answer.contains(randomNumber)){
+                answer.add(randomNumber);
             }
         }
     }
