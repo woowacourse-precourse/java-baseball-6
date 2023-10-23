28fb3e773efa5a1d077acb17f4d89a7686d37301
PARK9898 <wjddjs21@naver.com>
2023-10-24 02:16:26 +0900

    "test: 랜덤 숫자 각각의 range 검증 test"

--- src/test/java/baseball/RandomNumbersGeneratorTest.java
+++ src/test/java/baseball/RandomNumbersGeneratorTest.java
@@ -17,4 +17,13 @@ public class RandomNumbersGeneratorTest {
 
 		Assertions.assertThat(randomNumbers.size()).isEqualTo(3);
 	}
+
+	@Test
+	void 생성된_각각의_랜덤_숫자는_1과9_사이의_숫자이다() {
+		List<Integer> randomNumbers = randomNumbersGenerator.generateRandomNumbers();
+
+		for(Integer number : randomNumbers) {
+			Assertions.assertThat(number).isBetween(0,10);
+		}
+	}
 }
