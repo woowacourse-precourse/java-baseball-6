ac7461953af1a426ae087672d4c93f0a6de2a7ce
= <littlebigman.ai@gmail.com>
2023-10-24 02:48:30 +0900

    update countStrike, countBall logic and test

---
+++ src/main/java/baseball/entity/BallCount.java
@@ -0,0 +1,22 @@
+package baseball.entity;
+
+public class BallCount {
+    private int strikes;
+    private int balls;
+
+    public int getStrikes() {
+        return strikes;
+    }
+
+    public void setStrikes(int strikes) {
+        this.strikes = strikes;
+    }
+
+    public int getBalls() {
+        return balls;
+    }
+
+    public void setBalls(int balls) {
+        this.balls = balls;
+    }
+}
