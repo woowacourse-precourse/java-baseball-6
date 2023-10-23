f2a9d3b18c3a595f29caae89eedfee63037aafbe
aka-nick <bechalrang@gmail.com>
2023-10-23 12:40:42 +0900

    추가: 숫자 하나를 입력값으로 받는 Input.consoleNumber() 기능 구현

--- src/main/java/baseball/io/Input.java
+++ src/main/java/baseball/io/Input.java
@@ -9,5 +9,12 @@ public class Input {
     }
 
     // TODO : 특정 타입의 인풋 메서드를 지원해보자
+    public static Integer consoleNumber() {
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("잘못된 입력값입니다", e);
+        }
+    }
 
 }
