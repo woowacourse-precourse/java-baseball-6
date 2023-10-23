10f9e6ba71052a2ebf8efa2a5527fe29c2d5027d
Seungjida <esg0107@pusan.ac.kr>
2023-10-23 21:49:57 +0900

    Feat: "게임 시작 문구 출력 기능 추가"

--- src/main/java/others/Others.java
+++ src/main/java/others/Others.java
@@ -5,6 +5,10 @@ import static others.ExceptionHandling.restartException;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Others {
+    public static void printGameStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
     public static boolean ifRestart() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String restart = Console.readLine().trim().replaceAll(" ", "");
