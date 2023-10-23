218ba224725a8b269713e0074c8da789bf8f01fb
space <enaenen@naver.com>
2023-10-24 04:34:38 +0900

    REFACTOR: 게임 객체 방식으로 리팩토링

---
+++ src/main/java/baseball/ApplicationRefactor.java
@@ -0,0 +1,48 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class ApplicationRefactor {
+
+    private static final String STARTING_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String GAMEOVER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+            "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final int EXIT_CODE = 2;
+    private static final int ALL_STRIKE = 3;
+
+    public static void inputValidate(String line) {
+        if (line == null || line.length() != 1 || !Character.isDigit(line.charAt(0)) || line.charAt(0) > '2') {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(STARTING_MESSAGE);
+        BaseballGame baseballGame = BaseballGame.getInstance();
+        boolean isGameEnd = false;
+
+        while (!isGameEnd) {
+            baseballGame.inputNumbers();
+            baseballGame.calculateScore();
+            baseballGame.printGameScore();
+            if (baseballGame.getStrike() != ALL_STRIKE) {
+                continue;
+            }
+
+            // GAME OVER PROCESS
+            System.out.println(GAMEOVER_MESSAGE);
+
+            String line = Console.readLine();
+            inputValidate(line);
+            int code = Integer.parseInt(line);
+            if (code == EXIT_CODE) {
+                isGameEnd = true;
+            } else {
+                baseballGame.randomizeComputerNumber();
+            }
+
+        }
+    }
+}
