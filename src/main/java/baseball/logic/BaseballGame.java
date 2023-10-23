90e5450eab77a8763dba8705fb85a090d1d329ad
이지은 <jieun7056@naver.com>
2023-10-23 21:52:18 +0900

    feat: 컴퓨터의 수를 랜덤으로 추출하는 기능 구현

---
+++ src/main/java/baseball/logic/BaseballGame.java
@@ -0,0 +1,32 @@
+package baseball.logic;
+
+import baseball.domain.Player;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseballGame {
+    private List<Integer> computerNumbers;
+
+
+    public BaseballGame() {
+        this.computerNumbers = new ArrayList<>();
+    }
+
+    public void play() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        randomlyExtractNumber();
+    }
+
+    private List<Integer> randomlyExtractNumber() {
+        while (computerNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumbers.contains(randomNumber)) {
+                computerNumbers.add(randomNumber);
+            }
+        }
+        System.out.println("컴퓨터 숫자: "+ computerNumbers);
+        return computerNumbers;
+    }
+}
