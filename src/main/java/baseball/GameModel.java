99beb7f0ea2aacc817432142522843d1b897b78e
zzawang <hobin0116@naver.com>
2023-10-24 03:09:43 +0900

    refactor: GameModel 클래스로 분리
    - 컴퓨터의 수 생성, 예외 처리, 게임 결과 기능들을 클래스로 분리
    - MVC 패턴 적용

---
+++ src/main/java/baseball/GameModel.java
@@ -0,0 +1,99 @@
+package baseball;
+
+import static baseball.Constant.GAME_NUM_SIZE;
+import static baseball.Constant.MAX_RANDOM_NUMBER;
+import static baseball.Constant.MIN_RANDOM_NUMBER;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.IntStream;
+
+public class GameModel {
+    private List<String> computerNum;
+
+    public GameModel() {
+        this.computerNum = createComputerNumber();
+    }
+
+    public List<String> createComputerNumber() {
+        Set<String> numSet = new HashSet<>();
+        while (numSet.size() < GAME_NUM_SIZE) {
+            String randNum = String.valueOf(Randoms.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER));
+            numSet.add(randNum);
+        }
+        return new ArrayList<>(numSet);
+    }
+
+    public String matchPlayerNum(List<String> playerNum) {
+        long ballCount = computeBallCount(playerNum);
+        long strikeCount = computeStrikeCount(playerNum);
+
+        return getGameResult(ballCount, strikeCount);
+    }
+
+    public void verifyPlayerNum(List<String> playerNum) {
+        isNonDigit(playerNum);
+        isThreeDigitNumber(playerNum);
+        hasRepeatedDigitNumber(playerNum);
+    }
+
+
+    private long computeBallCount(List<String> playerNum) {
+        return playerNum.stream()
+                .filter(num -> computerNum.contains(num) && !computerNum.get(playerNum.indexOf(num)).equals(num))
+                .count();
+    }
+
+    private long computeStrikeCount(List<String> playerNum) {
+        return IntStream.range(0, GAME_NUM_SIZE)
+                .filter(i -> computerNum.get(i).equals(playerNum.get(i)))
+                .count();
+    }
+
+    private String getGameResult(long ballCount, long strikeCount) {
+        String result = "";
+
+        if (ballCount > 0) {
+            result += ballCount + "볼";
+        }
+        if (strikeCount > 0) {
+            result += (result.isEmpty() ? "" : " ") + strikeCount + "스트라이크";
+        }
+
+        return result.isEmpty() ? "낫싱" : result;
+    }
+
+    // 사용자가 입력한 값이 숫자인지 확인한다.
+    private void isNonDigit(List<String> playerNum) {
+        try {
+            for (String pNum : playerNum) {
+                Integer.parseInt(pNum);
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("입력하신 값이 숫자가 아닙니다. 게임이 종료됩니다.");
+        }
+    }
+
+    // 사용자가 입력한 값이 3자리 숫자인지 확인한다.
+    private void isThreeDigitNumber(List<String> playerNum) {
+        if (playerNum.size() != 3) {
+            throw new IllegalArgumentException("숫자가 3자리가 아닙니다. 게임이 종료됩니다.");
+        }
+    }
+
+    // 사용자가 입력한 값에서 2개 이상 중복되는 숫자가 있는 경우
+    private void hasRepeatedDigitNumber(List<String> playerNum) {
+        Set<String> digitNumSet = new HashSet<>();
+
+        for (String pNum : playerNum) {
+            if (digitNumSet.contains(pNum)) {
+                throw new IllegalArgumentException("서로 다른 3자리의 수가 아닙니다. 게임이 종료됩니다.");
+            }
+            digitNumSet.add(pNum);
+        }
+    }
+}
+
