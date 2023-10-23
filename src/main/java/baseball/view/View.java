52e3fe2eb8df3cdde463d0e24704b4c3e7839d68
ay-eonii <naayen@naver.com>
2023-10-23 15:44:48 +0900

    feat: 재시작/종료 선택 문구 출력

--- src/main/java/baseball/view/View.java
+++ src/main/java/baseball/view/View.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class View {
     private final static String START_COMMENT = "숫자 야구 게임을 시작합니다.";
     private final static String INPUT_COMMENT = "숫자를 입력해주세요 : ";
+    private final static String END_COMMENT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public void printStart() {
         System.out.println(START_COMMENT);
@@ -17,5 +18,14 @@ public class View {
 
     public void printResult(String result) {
         System.out.println(result);
+    }
+
+    public String inputEnd() {
+        System.out.println(END_COMMENT);
+        return Console.readLine();
+    }
+
+    public void closeConsole() {
+        Console.close();
     }
 }
