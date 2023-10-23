3d4918d010112ee817b6ba171bb3cd86b52e6b75
yy8305 <yy8305@naver.com>
2023-10-23 16:23:24 +0900

    refactor(OutputHandler): 코드 리팩토링

--- src/main/java/baseball/OutputHandler.java
+++ src/main/java/baseball/OutputHandler.java
@@ -4,40 +4,70 @@ package baseball;
  * 게임에 필요한 메시지 출력을 담당 하는 클래스 이다.
  */
 public class OutputHandler {
+    private static final int ZERO = 0;
+
+    private static void println(String message) {
+        System.out.println(message);
+    }
+
+    private static String getBlankChar(String message) {
+        if(!message.isEmpty()) {
+            return " ";
+        }
+
+        return "";
+    }
+
+    private static boolean hasCount(int count) {
+        if(count > ZERO) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static boolean hasNotCount(int[] counts) {
+        for(int cnt : counts) {
+            if(cnt != ZERO) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static void showGameStartMessage() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        println("숫자 야구 게임을 시작합니다.");
     }
 
     public static void showGamePromptNumbersMessage() {
-        System.out.println("숫자를 입력해주세요 : ");
+        println("숫자를 입력해주세요 : ");
     }
 
     public static void showNumberCheckResultMessage(int ballCnt, int strikeCnt) {
         String resultMessage = "";
-        if(ballCnt > 0) {
+
+        if(hasCount(ballCnt)) {
             resultMessage += ballCnt + "볼";
         }
 
-        if(strikeCnt > 0) {
-            if(!resultMessage.isEmpty()) {
-                resultMessage += " ";
-            }
-
+        if(hasCount(strikeCnt)) {
+            resultMessage += getBlankChar(resultMessage);
             resultMessage += strikeCnt + "스트라이크";
         }
 
-        if(ballCnt == 0 && strikeCnt == 0) {
+        if(hasNotCount(new int[]{ballCnt, strikeCnt})) {
             resultMessage += "낫싱";
         }
 
-        System.out.println(resultMessage);
+        println(resultMessage);
     }
 
     public static void showGameEndMessage() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
     public static void showRestartPrompt() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 }
