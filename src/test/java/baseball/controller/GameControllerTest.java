bf6d88a849cb5b0237b571b0bf9b21ffd83bd9f7
suihub <qkdcrt@gmail.com>
2023-10-23 12:39:29 +0900

    <test>: "볼 생성 테스트 로직 수정"
    수정된 볼 생성 로직에 맞춰, 볼 생성 관련 테스트 로직 수정.

--- src/test/java/baseball/controller/GameControllerTest.java
+++ src/test/java/baseball/controller/GameControllerTest.java
@@ -1,32 +1,40 @@
 package baseball.controller;
 
+import baseball.domain.Computer;
+import baseball.domain.Player;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 class GameControllerTest {
-    private final GameController controller = new GameController();
-
     @Test
     void 컴퓨터측_볼_생성(){
+        List<Integer> ballList = new ArrayList<>();
 
-    }
+        for (int i=1; i<4; i++){
+            ballList.add(i);
+        }
 
-    @Test
-    void 컴퓨터측_볼_검증(){
+        Computer computer = new Computer(ballList);
+        List<Integer> computerBall = computer.getBallList();
 
-
+        assertThat(computerBall.size()).isEqualTo(3);
     }
 
     @Test
     void 유저측_볼_생성(){
+        List<Integer> ballList = new ArrayList<>();
 
-    }
+        for (int i=1; i<4; i++){
+            ballList.add(i);
+        }
 
-    @Test
-    void 유저측_볼_내용_검증(){
+        Player player = new Player(ballList);
+        List<Integer> playerBall = player.getBallList();
 
+        assertThat(playerBall.size()).isEqualTo(3);
     }
 }
