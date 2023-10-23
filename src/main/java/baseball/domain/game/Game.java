c378a4a41cca867746800b2340288e5b83dbb521
penrose15 <seongju99@naver.com>
2023-10-23 23:04:08 +0900

    Feat: add Game Domain

---
+++ src/main/java/baseball/domain/game/Game.java
@@ -0,0 +1,50 @@
+package baseball.domain.game;
+
+import baseball.domain.computer.Computer;
+import baseball.domain.player.Player;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Game {
+    private final Computer computer;
+    private Player player;
+    private PlayResult playResult;
+
+    public Game(Computer computer, Player player) {
+        playResult = new PlayResult();
+        this.computer = computer;
+        this.player = player;
+    }
+
+    public String getPlayerResult() {
+//        playResult.getCnt();
+        playResult.clearResult();
+        PlayResult result = compareComputerNumbersAndPlayerNumbers();
+//        playResult.getCnt();
+        return result.returnResult();
+    }
+
+
+
+    public PlayResult compareComputerNumbersAndPlayerNumbers() {
+        List<Integer> computerNumbers = computer.getComputerThreeNumbers();
+        List<Integer> playerNumbers = player.getPlayerThreeNumbers();
+
+        for(int computerIdx = 0; computerIdx < computerNumbers.size(); computerIdx++) {
+            for(int playerIdx = 0; playerIdx < playerNumbers.size(); playerIdx++) {
+                Integer computerNumber = computerNumbers.get(computerIdx);
+                Integer playerNumber = playerNumbers.get(playerIdx);
+
+                if(Objects.equals(computerNumber, playerNumber) && computerIdx == playerIdx) {
+                    playResult.plusStrikeCnt();
+                    continue;
+                }
+                if(Objects.equals(computerNumber, playerNumber)) {
+                    playResult.plusBallCnt();
+                }
+            }
+        }
+        return playResult;
+    }
+}
---
+++ src/main/java/baseball/domain/game/PlayResult.java
@@ -0,0 +1,46 @@
+package baseball.domain.game;
+
+import baseball.global.message.BallMessage;
+
+import static baseball.global.message.BallMessage.*;
+
+public class PlayResult {
+    private int ballCnt;
+    private int strikeCnt;
+
+    public PlayResult() {
+        this.ballCnt = 0;
+        this.strikeCnt = 0;
+    }
+
+    public void clearResult() {
+        this.ballCnt = 0;
+        this.strikeCnt = 0;
+    }
+
+    public void plusBallCnt() {
+        this.ballCnt++;
+    }
+    public void plusStrikeCnt() {
+        this.strikeCnt++;
+    }
+
+    public String returnResult() {
+        if(ballCnt > 0 && strikeCnt > 0) {
+            return BALL.apply(ballCnt) + " " +STRIKE.apply(strikeCnt);
+        }
+        if(ballCnt == 0 && strikeCnt > 0) {
+            return STRIKE.apply(strikeCnt);
+        }
+        if(ballCnt > 0 && strikeCnt == 0) {
+            return BALL.apply(ballCnt);
+        }
+        else {
+            return NOTHING.getMessage();
+        }
+    }
+
+    public void getCnt() {
+        System.out.println("ballCnt : "+this.ballCnt + "strikeCnt : " + this.strikeCnt);
+    }
+}
