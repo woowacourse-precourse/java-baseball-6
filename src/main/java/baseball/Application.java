62f0a514bcfa0e20f73bbccb460be27875b91af2
Juho <jeuslee99@naver.com>
2023-10-24 04:01:03 +0900

    feat: Add UserInput function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,12 +3,46 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.lang.Object;
 
 public class Application {
-    public static int RanNumber() {
+    public static void main(String[] args) {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        boolean restartFlag = true;
+
+        while(restartFlag == true) {
+            //랜덤 세 자리 수를 생성하는 함수
+            ArrayList<Integer> computer = RanNumber();
+
+            //player의 입력을 받는 함수
+            ArrayList<Integer> player = UserInput();
+
+
+        }
+
+
+
+
+    }
+
+    public static ArrayList<Integer> UserInput() {
+        String playerInputString = Console.readLine();
+        ArrayList<Integer> playerInputArray = new ArrayList<>();
+
+        for(int i=0; i< playerInputString.length(); i++) {
+            char charDigit = playerInputString.charAt(i);
+            int digit = Character.getNumericValue(charDigit);
+            playerInputArray.add(digit);
+        }
+
+        return playerInputArray;
+    }
+
+    public static ArrayList<Integer> RanNumber() {
         ArrayList<Integer> randomNumArray = new ArrayList<>();
+
         while(randomNumArray.size() != 3) {
             int randomNumber = Randoms.pickNumberInRange(1,9);
 
@@ -17,14 +51,7 @@ public class Application {
             }
         }
 
-        int answer = 100 * randomNumArray.get(0) + 10 * randomNumArray.get(1) + randomNumArray.get(2);
-
-        return answer;
+        return randomNumArray;
     }
-    public static void main(String[] args) {
-        System.out.println("숫자 야구 게임을 시작합니다.");
 
-        //랜덤 세 자리 수를 생성하는 함수
-        System.out.println(RanNumber());
-    }
 }
