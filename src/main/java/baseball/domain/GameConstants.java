b6e199972b41e653ded4c78332066807e88c87f4
inpink <inpink@kakao.com>
2023-10-23 21:22:04 +0900

    refactor: 스트라이크, 볼, 낫싱 String 상수를 Constants class로 재이동
    이 값들은 게임 전역에서 사용될 수 있는 변경되지 않는 값임.
    캡슐화로 은닉해야 할 데이터가 아니기에, 자주 쓰는 값을 모아놓은 GameConstants로 이동함.

--- src/main/java/baseball/domain/GameConstants.java
+++ src/main/java/baseball/domain/GameConstants.java
@@ -4,6 +4,11 @@ public final class GameConstants {
     public static final int NUMBER_LENGTH = 3;
     public static final int MIN_DIGIT = 1;
     public static final int MAX_DIGIT = 9;
+    public static final int MIN_HINT_COUNT = 0;
+
+    public static final String STRIKE_TEXT = "스트라이크";
+    public static final String BALL_TEXT = "볼";
+    public static final String NOTHING_TEXT = "낫싱";
 
     private GameConstants(){
 
