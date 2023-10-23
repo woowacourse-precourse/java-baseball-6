d99476142b107bf6ca942152e2cf7fdc96715f69
morenow <jjang9877@gmail.com>
2023-10-23 16:26:21 +0900

    feat: 숫자 불변 클래스 추가 및 메시지 클래스에 사용
    게임에 쓰이는 숫자를 모아놓은 불변 클래스 추가 후 해당 숫자를 메시지 클래스에 사용

---
+++ src/main/java/baseball/constant/Number.java
@@ -0,0 +1,10 @@
+package baseball.constant;
+
+/**
+ * 숫자 불변 클래스
+ */
+public final class Number {
+    public final static int GAME_LENGTH = 3;
+    public final static int RESTART_NUM = 1;
+    public final static int FINISH_NUM = 2;
+}
