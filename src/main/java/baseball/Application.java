d78d9166a97e18a0d35405b2cb5e7160092bf646
Kite_Z <duswl339@ajou.ac.kr>
2023-10-23 18:23:02 +0900

    Feat: Print Start Game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,7 +6,7 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        // TODO: START
+        System.out.print("숫자 야구 게임을 시작합니다.");
 
         // TODO: Make Computer Baseball Number
 
