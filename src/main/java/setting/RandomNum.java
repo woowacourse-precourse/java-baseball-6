8375dafbb9bd2139092371be6f749e68ed973634
rkf06 <rkf06@DESKTOP-DUIQUNE>
2023-10-24 00:42:37 +0900

    랜덤 생성 숫자 String 타입으로 가져오기

--- src/main/java/setting/RandomNum.java
+++ src/main/java/setting/RandomNum.java
@@ -6,29 +6,18 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNum {
-	
+
 	private List<Integer> answerNum = new ArrayList<>();
-	
-	public RandomNum() {
-		getRandomNum();
-	}
-	
-	public void setRandomNum() {
-		//
-	}
-	
-	public List<Integer> getRandomNum() {
+	private String strAnswer = "";
+
+	public String setRandomNum() {
 		while (answerNum.size() < 3) {
 			int randomNumber = Randoms.pickNumberInRange(1, 9);
 			if (!answerNum.contains(randomNumber)) {
 				answerNum.add(randomNumber);
+				strAnswer += randomNumber;
 			}
 		}
-		System.out.println(answerNum);
-		return answerNum;
-		
+		return strAnswer;
 	}
-	
-
-	
 }
