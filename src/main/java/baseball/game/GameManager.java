b1aa83df74572150e5e08aeb64aa6eb34277698b
leedohyun <lucky77795@naver.com>
2023-10-23 22:19:36 +0900

    refactor: printer를 사용하도록 변경

--- src/main/java/baseball/game/GameManager.java
+++ src/main/java/baseball/game/GameManager.java
@@ -1,7 +1,12 @@
 package baseball.game;
 
+import static baseball.resources.GameConst.*;
+
+
 import baseball.domain.ComputerNum;
 import baseball.domain.UserNum;
+import baseball.printer.Printer;
+import baseball.resources.GameMessage;
 import camp.nextstep.edu.missionutils.Console;
 
 public class GameManager {
@@ -9,6 +14,8 @@ public class GameManager {
     private final UserNum userNum;
     private final ComputerNum computerNum;
     private final BallCounter ballCounter;
+    private final Printer printer;
+
     private Integer strike;
     private Integer ball;
 
@@ -16,14 +23,15 @@ public class GameManager {
         this.userNum = new UserNum();
         this.computerNum = new ComputerNum();
         this.ballCounter = new BallCounter();
+        this.printer = new Printer();
     }
 
     public void startGame() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        printer.printStartMessage();
     }
 
     public void inputUserNumber() {
-        System.out.print("숫자를 입력해주세요 : ");
+        printer.printInputMessage();
         userNum.setNum(Console.readLine());
     }
 
@@ -32,35 +40,27 @@ public class GameManager {
         strike = ballCounter.countStrike(userNum.getNum(), computerNum.getNum());
         ball = correctNumberSize - strike;
 
-        if (strike == 0 && ball == 0) {
-            System.out.println("낫싱");
-        } else if (strike == 0 && ball > 0) {
-            System.out.printf("%d볼\n", ball);
-        } else if (strike > 0 && ball == 0) {
-            System.out.printf("%d스트라이크\n", strike);
-        } else if (strike > 0 && ball > 0){
-            System.out.printf("%d볼 %d스트라이크\n", ball, strike);
-        }
+        printer.printBallCount(strike, ball);
     }
 
     public boolean checkGameOver() {
-        if (strike == 3) {
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        if (strike == ALL_STRIKE) {
+            printer.printCorrectMessage();
             return true;
         }
         return false;
     }
 
     public boolean askRestartGame() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        printer.printAskRestart();
         Integer enterValue = Integer.parseInt(Console.readLine());
 
-        if (enterValue == 1) {
+        if (enterValue == WANT_RESTART) {
             return true;
-        } else if (enterValue != 2) {
-            throw new IllegalArgumentException();
+        } else if (enterValue == WANT_END) {
+            return false;
         }
 
-        return false;
+        throw new IllegalArgumentException();
     }
 }
