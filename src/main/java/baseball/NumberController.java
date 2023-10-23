4a9af2af9441028c0919a4ef6aba76914c776f51
wook <dong930112@gmail.com>
2023-10-23 21:09:54 +0900

    refactor(NumberController): 'generate()' 메소드 삭제 `camp.nextstep.edu.missionutils` 라이브러리에서 제공하는 `Randoms.pickUniqueNumbersInRange()`로 대체

--- src/main/java/baseball/NumberController.java
+++ src/main/java/baseball/NumberController.java
@@ -2,25 +2,11 @@ package baseball;
 
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class NumberController {
-
-  public List<Integer> generate() {
-    List<Integer> numList = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9));
-    Collections.shuffle(numList);
-
-    List<Integer> result = new ArrayList<>(3);
-    result.add(numList.get(0));
-    result.add(numList.get(1));
-    result.add(numList.get(2));
-
-    return result;
-    }
 
     public List<Integer> splitter(int num) {
       List<Integer> numList = new ArrayList<>(3);
