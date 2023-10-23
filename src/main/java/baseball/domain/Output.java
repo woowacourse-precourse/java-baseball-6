e511c48b94f2ba9e8eb3bdadee910c1b8fb396e7
AidenRoh <colohrby@gmail.com>
2023-10-23 17:56:42 +0900

    feat: implement output class to print out result

---
+++ src/main/java/baseball/domain/Output.java
@@ -0,0 +1,20 @@
+package baseball.domain;
+
+public class Output {
+
+    public Integer printResult(int[] result) {
+
+        if (result[0] == 3) {
+            System.out.println("3스트라이크");
+        } else if (result[0] == 0 && result[1] == 0) {
+            System.out.println("낫싱");
+        } else if (result[0] == 0) {
+            System.out.println(result[1] + "볼");
+        } else if (result[1] == 0) {
+            System.out.println(result[0] + "스트라이크");
+        } else if (result[0] != 0 && result[1] != 0) {
+            System.out.println(result[1] + "볼 " + result[0] + "스트라이크");
+        }
+        return result[0];
+    }
+}
