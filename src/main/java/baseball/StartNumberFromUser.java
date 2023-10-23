e1c39305e54c3772568c2819ea8772dd9935ee1d
jihyuk <kwlgur0416@gmail.com>
2023-10-23 22:41:06 +0900

    refactor : 재시작 여부 받기 기능 추가

--- src/main/java/baseball/StartNumberFromUser.java
+++ src/main/java/baseball/StartNumberFromUser.java
@@ -4,10 +4,10 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class StartNumberFromUser {
 
-    private final static String requestMessage = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private final static String requestMessage = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
 
     public int getStartNumber() throws IllegalArgumentException {
-        String inputString = getInputString();
+        final String inputString = getInputString();
         final int startNumber = Integer.parseInt(inputString);
         if (!validate(startNumber)) {
             throw new IllegalArgumentException();
@@ -22,12 +22,11 @@ public class StartNumberFromUser {
     }
 
     private void requestForStartNumber() {
-        System.out.println(requestMessage);
+        System.out.print(requestMessage);
     }
 
     private boolean validate(int startNumber) {
-        // TODO
-        return true;
+        return startNumber == 1 || startNumber == 2;
     }
 
 }
