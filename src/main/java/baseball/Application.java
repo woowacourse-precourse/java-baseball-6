d562ec8e909667b78a2d4e5c8608e0b1579a3fa4
Juho <jeuslee99@naver.com>
2023-10-24 03:21:51 +0900

    feat: Add RanNumber Function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.lang.Object;
+
 public class Application {
+    public static int RanNumber() {
+        ArrayList<Integer> randomNumArray = new ArrayList<>();
+        while(randomNumArray.size() != 3) {
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+
+            if(!randomNumArray.contains(randomNumber)) {
+                randomNumArray.add(randomNumber);
+            }
+        }
+
+        int answer = 100 * randomNumArray.get(0) + 10 * randomNumArray.get(1) + randomNumArray.get(2);
+
+        return answer;
+    }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        //랜덤 세 자리 수를 생성하는 함수
+        System.out.println(RanNumber());
     }
 }
