e916f7a28e6c8bf1bcc73d8723403a09723e720a
Tiger <skdlghwns@gmail.com>
2023-10-23 20:34:10 +0900

    컴퓨터수생성 및 유저 수입력 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,31 @@
 package baseball;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.*;
+
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        int gameState = 1;//1:게임진행, 2:게임종료
+        List<Integer> computer = new ArrayList<>();//컴퓨터 수 컨테이너 할당
+
+        //게임 시작
+        while(gameState==1){
+            //1.상대방 수 생성
+            while (computer.size() < 3){
+                int randomNumber = Randoms.pickNumberInRange(1, 9);
+                if(!computer.contains(randomNumber)){
+                    computer.add(randomNumber);
+                }
+            }
+            System.out.println(computer.toString());
+            //2.3자리수 입력 시작
+            while(true){
+                String in = Console.readLine();
+                System.out.println(in);
+            }
+        }
     }
 }
