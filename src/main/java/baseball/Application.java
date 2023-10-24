cdae231bb3d1fefdb73bbf480a7d9b35e854e1ae
LeeJaeHong <0915jhlee@daum.net>
2023-10-24 10:26:01 +0900

    Feat: 사용자 숫자 입력 함수 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,6 +3,7 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
@@ -17,5 +18,14 @@ public class Application {
             }
         }
         return computer;
+    }
+
+    public static List<Integer> inputUserNumber(){
+        String s = Console.readLine();
+        List<Integer> inputNumber = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            inputNumber.add(s.charAt(i) - '0');
+        }
+        return inputNumber;
     }
 }
