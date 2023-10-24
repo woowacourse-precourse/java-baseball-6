17a72df2bb2845f48702da3af5ee6a066505744f
Choo <choo000407@naver.com>
2023-10-24 15:11:20 +0900

    feat(InputView): 사용자 입력 메서드 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,4 +1,19 @@
 package baseball.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    private static final String ASK_SUGGESTED_NUMBER = "숫자를 입력해주세요 : ";
+    private static final String ASK_GAME_CONTINUE_CHOICE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요";
+
+    public static String getSuggestedNumber() {
+        System.out.println(ASK_SUGGESTED_NUMBER);
+        return Console.readLine();
+    }
+
+    public static String getGameContinueChoice() {
+        System.out.println(ASK_GAME_CONTINUE_CHOICE);
+        return Console.readLine();
+    }
 }
