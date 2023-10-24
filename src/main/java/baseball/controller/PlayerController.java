4794e93124e140b126021c03baf75735a32a566c
MingyeomKim <mingyum119@naver.com>
2023-10-24 19:12:18 +0900

    fix(PlayerController): NumberFormatException을 방지하기 위해 String을 검증하는 로직으로 수정
    받아온 String 문자열을 List<Integer>로 변환하였는데, 잘못된 입력값이 들어왔을 때 IllegalArgumentException이 아닌 NumberFormatException를 던지는 문제가 있었다. 변환하기 전에 문자열을 검증하는 로직으로 수정하여 이를 해결한다.

--- src/main/java/baseball/controller/PlayerController.java
+++ src/main/java/baseball/controller/PlayerController.java
@@ -10,8 +10,8 @@ public class PlayerController {
 
     public List<Integer> readAndValidateInputNumbers() {
         String inputString = inputView.readInputNumbers();
+        validateNumbers(inputString);
         List<Integer> inputList = parseInputString(inputString);
-        validateNumbers(inputList);
         return inputList;
     }
 
@@ -35,29 +35,27 @@ public class PlayerController {
         return result;
     }
 
-    private void validateNumbers(List<Integer> inputList) {
-        validateOutOfRange(inputList);
-        validateInputSize(inputList);
-        validateDuplicates(inputList);
+    private void validateNumbers(String inputString) {
+        validateOutOfRange(inputString);
+        validateInputSize(inputString);
+        validateDuplicates(inputString);
     }
 
-    private void validateOutOfRange(List<Integer> inputList) {
-        inputList.stream()
-                .filter(number -> number < 1 || number > 9)
-                .findFirst()
-                .ifPresent(number -> {
-                    throw new IllegalArgumentException("숫자는 1 ~ 9 사이의 숫자만 입력할 수 있습니다.");
-                });
+    private void validateOutOfRange(String inputString) {
+        boolean allMatch = inputString.chars().allMatch(c -> c >= '1' && c <= '9');
+        if (!allMatch) {
+            throw new IllegalArgumentException("1과 9 사이의 숫자를 입력해주세요.");
+        }
     }
 
-    private void validateInputSize(List<Integer> inputList) {
-        if (inputList.size() != 3) {
+    private void validateInputSize(String inputString) {
+        if (inputString.length() != 3) {
             throw new IllegalArgumentException("3개의 숫자만 입력할 수 있습니다.");
         }
     }
 
-    private void validateDuplicates(List<Integer> inputList) {
-        if (inputList.stream().distinct().count() != inputList.size()) {
+    private void validateDuplicates(String inputString) {
+        if (inputString.chars().distinct().count() != inputString.length()) {
             throw new IllegalArgumentException("중복 없이 숫자를 입력해주세요.");
         }
     }
