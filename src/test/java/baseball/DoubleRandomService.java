7a1c89743235287078946f40e341d72ac6236dcb
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-10-25 00:37:58 +0900

    refactor : DoubleRandomService 값의 상수화

--- src/test/java/baseball/DoubleRandomService.java
+++ src/test/java/baseball/DoubleRandomService.java
@@ -1,6 +1,7 @@
 package baseball;
 
 public class DoubleRandomService implements RandomService {
+    public static final String NOT_RANGE_IN_ONE_TO_NINE_ERROR_MESSAGE = "랜덤 값이 1 ~ 9 사이가 아닙니다.";
     private int nowNumber;
 
     public DoubleRandomService(int nowNumber) {
@@ -11,7 +12,7 @@ public class DoubleRandomService implements RandomService {
     public int pickNumberInRange(final int startInclusive, final int endInclusive) {
         int returnNumber = this.nowNumber;
         if (startInclusive > nowNumber || nowNumber > endInclusive) {
-            throw new IllegalArgumentException("랜덤 값이 1 ~ 9 사이가 아닙니다.");
+            throw new IllegalArgumentException(NOT_RANGE_IN_ONE_TO_NINE_ERROR_MESSAGE);
         }
 
         nowNumber += 1;
