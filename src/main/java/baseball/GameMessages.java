db9e2b59f1f23700482aea95e43b652be052cd5c
hyunjin <guswlsdl0121@gmail.com>
2023-10-24 17:56:21 +0900

    refactor: GameMessages 상황별 출력 로직 개선
    StringBuilder를 사용해서 문장 구성 로직을 수정했고 조건문을 단순화함.

--- src/main/java/baseball/GameMessages.java
+++ src/main/java/baseball/GameMessages.java
@@ -10,28 +10,31 @@ public class GameMessages {
     }
 
     public static void printStatusMessage(int ball, int strike) {
+        StringBuilder message = new StringBuilder();
+
         if (strike == 3) {
-            System.out.println("3스트라이크");
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            return;
-        }
-
-        if (strike > 0 && ball > 0) {
-            System.out.println(ball + "볼" + " " + strike + "스트라이크");
-            return;
-        }
-
-        if (strike > 0) {
-            System.out.println(strike + "스트라이크");
+            printThreeStrikeMessage();
             return;
         }
 
         if (ball > 0) {
-            System.out.println(ball + "볼");
-            return;
+            message.append(ball).append("볼 ");
         }
 
-        System.out.println("낫싱");
+        if (strike > 0) {
+            message.append(strike).append("스트라이크 ");
+        }
+
+        if (message.length() == 0) {
+            message.append("낫싱");
+        }
+
+        System.out.println(message.toString().trim());
+    }
+
+    private static void printThreeStrikeMessage() {
+        System.out.println("3스트라이크");
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 }
