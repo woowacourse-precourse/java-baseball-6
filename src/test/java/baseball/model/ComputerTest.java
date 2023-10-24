5231361a04e6c5b9f38030660c49d38494d1c630
dltjdn <hana40004@naver.com>
2023-10-25 06:06:29 +0900

    test(ComputerTest): 변경된 코드에 맞게 ComputerTest 수정

--- src/test/java/baseball/model/ComputerTest.java
+++ src/test/java/baseball/model/ComputerTest.java
@@ -1,6 +1,6 @@
 package baseball.model;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -10,25 +10,27 @@ class ComputerTest {
     @Test
     public void 랜덤숫자_3자리수가_생성된다() {
         // given
-        Computer generator = new Computer();
+        Computer computer = new Computer();
 
-        // When
-        List<Integer> result = generator.getRandomNumber();
+        // when
+        computer.generateRandomNumber();
+        List<Integer> randomNumber = computer.getRandomNumber();
 
         // then
-        assertThat(result.size()).isEqualTo(Number.NUMBER_SIZE);
+        assertEquals(randomNumber.size(), Number.NUMBER_SIZE);
 
     }
 
     @Test
     public void 랜덤숫자의_각_자릿수가_모두_다르다() {
         //given
-        Computer generator = new Computer();
+        Computer computer = new Computer();
 
         //when
-        List<Integer> result = generator.getRandomNumber();
+        computer.generateRandomNumber();
+        List<Integer> randomNumber = computer.getRandomNumber();
 
         //then
-        assertThat(result.stream().distinct().count()).isEqualTo(Number.NUMBER_SIZE);
+        assertEquals(randomNumber.stream().distinct().count(), Number.NUMBER_SIZE);
     }
 }
