561395d3186b4657d979e4397be4926f18ed1056
hiidy <hiidy115@gmail.com>
2023-10-25 01:04:18 +0900

    :lipstick: style: makeAnswer의 공백 수정 makeAnswer의 의미없는 공백을 삭제함.

--- src/main/java/baseball/GenerateNum.java
+++ src/main/java/baseball/GenerateNum.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 
 public class GenerateNum {
-    public static  ArrayList<Integer> makeAnswer() {
+    public static ArrayList<Integer> makeAnswer() {
         ArrayList<Integer> answerNumbers = new ArrayList<>();
         while (answerNumbers.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
