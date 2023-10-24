7f7a3a0ff05364e282e5df4b037eaa32a2b624a3
mingadinga <minpearl0826@gmail.com>
2023-10-24 23:34:27 +0900

    TEST 깨진 테스트 수정, 중복 개선

--- src/test/java/baseball/ui/GameViewTest.java
+++ src/test/java/baseball/ui/GameViewTest.java
@@ -5,20 +5,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import baseball.controller.AbstractGameController;
 import baseball.controller.GameController;
-import baseball.controller.GameControllerImpl;
+import baseball.controller.RandomNumberCreator;
+import baseball.controller.Scoring;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BooleanSupplier;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GameViewTest {
-
-    final GameView gameView = new GameView(new GameControllerImpl(), new UserInputValidator(), () -> {
-        return "123";
-    });
 
     final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
     final PrintStream standardOut = System.out;
@@ -35,29 +34,32 @@ class GameViewTest {
 
     @Test
     public void 게임시작_시작안내문구를_출력한다() {
+        AtomicInteger count = new AtomicInteger(0);
+        GameController mockGameControllerOnce = getGameController(() -> count.getAndIncrement() < 1);
+        GameView gameView = new GameView(mockGameControllerOnce, new UserInputValidator(), () -> "123");
+
         gameView.start();
-        assertEquals("숫자 야구 게임을 시작합니다.", outputStreamCaptor.toString().trim());
+
+        assertThat(outputStreamCaptor.toString().trim().contains("숫자 야구 게임을 시작합니다."));
+
     }
 
     @Test
     public void 추측시작_입력안내문구를_출력한다() {
+        AtomicInteger count = new AtomicInteger(0);
+        GameController mockGameControllerOnce = getGameController(() -> count.getAndIncrement() < 1);
+        GameView gameView = getGameView(mockGameControllerOnce);
+
         gameView.guessUntilCorrect();
-        assertEquals("숫자를 입력해주세요 : ", outputStreamCaptor.toString().trim());
+
+        assertThat(outputStreamCaptor.toString().trim().contains("숫자를 입력해주세요 : "));
     }
 
     @Test
     public void 추측_실패시_다시_추측한다() {
-        GameController mockGameControllerTerminateAfterTwice = new AbstractGameController() {
-            int count = 0;
-
-            @Override
-            public boolean isStillWrong() {
-                return count++ < 2;
-            }
-        };
-        GameView gameView = new GameView(mockGameControllerTerminateAfterTwice, new UserInputValidator(), () -> {
-            return "123";
-        });
+        AtomicInteger count = new AtomicInteger(0);
+        GameController mockGameControllerTerminateAfterTwice = getGameController(() -> count.getAndIncrement() < 2);
+        GameView gameView = getGameView(mockGameControllerTerminateAfterTwice);
 
         gameView.guessUntilCorrect();
 
@@ -66,25 +68,39 @@ class GameViewTest {
 
     @Test
     public void 추측_성공시_성공문구_출력한다() {
-        GameController gameController = new AbstractGameController() {
-            @Override
-            public boolean isStillWrong() {
-                return false;
-            }
-        };
-        GameView gameView = new GameView(gameController, new UserInputValidator(), () -> {
-            return "123";
-        });
+        GameController mockGameControllerTerminateInstantly = getGameController(() -> false);
+        GameView gameView = getGameView(mockGameControllerTerminateInstantly);
 
         gameView.guessUntilCorrect();
 
         assertEquals("3개의 숫자를 모두 맞히셨습니다! 게임 종료", outputStreamCaptor.toString().trim());
     }
 
+    private GameView getGameView(GameController gameController) {
+        return new GameView(gameController, new UserInputValidator(), () -> {
+            return "123";
+        });
+    }
+
+    private static GameController getGameController(BooleanSupplier booleanSupplier) {
+        GameController mockGameController = new AbstractGameController(
+                getDefaultScoring()) {
+            @Override
+            public boolean isStillWrong() {
+                return booleanSupplier.getAsBoolean();
+            }
+        };
+        return mockGameController;
+    }
+
     private static void mockInput() {
         String input = "123";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
+    }
+
+    private static Scoring getDefaultScoring() {
+        return new Scoring(RandomNumberCreator.create(3));
     }
 
 }
