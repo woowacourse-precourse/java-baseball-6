d11ae95fb7d7a23ca5685fe2605f4b1935b35708
youngkyunIm <yee0230@naver.com>
2023-10-24 14:21:38 +0900

    feat : GameResult 구현
    게임 결과를 가지고 있는 객체 구현

--- src/main/java/baseball/entity/GameResult.java
+++ src/main/java/baseball/entity/GameResult.java
@@ -1,2 +1,26 @@
-package baseball.entity;public class GameResult {
+package baseball.entity;
+
+public class GameResult {
+    private final int strike;
+    private final int ball;
+
+    public GameResult(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+
+    public String getResult() {
+        StringBuilder sb = new StringBuilder();
+        if (ball == 0 && strike == 0) {
+            sb.append("낫싱");
+        }
+        if (ball != 0) {
+            sb.append(ball).append("볼").append(" ");
+        }
+        if (strike != 0) {
+            sb.append(strike).append("스트라이크");
+        }
+        return sb.toString();
+    }
+
 }
