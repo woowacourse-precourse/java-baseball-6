7db625b6195f166895e76a49b6f9d40f13b3bb67
peeaaccee <wlgp8958@naver.com>
2023-10-25 08:22:09 +0900

    test(User): 사용자 입력이 3자리 리스트인 객체로 저장되는지 재확인

--- src/test/java/baseball/domain/model/UserTest.java
+++ src/test/java/baseball/domain/model/UserTest.java
@@ -16,4 +16,10 @@ class UserTest {
         Assertions.assertThat(new User(userInput)).isEqualTo(new User(List.of("1", "2", "3")));
     }
 
+    @Test
+    @DisplayName("사용자 입력 456")
+    void 사용자_입력을_객체로_저장2() {
+        String userInput = "456";
+        Assertions.assertThat(new User(userInput)).isEqualTo(new User(List.of("4", "5", "6")));
+    }
 }
