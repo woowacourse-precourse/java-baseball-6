b3cce492b70f794f68f8724e4bc2c9783aa74fb1
jihoon98 <tlswlgns1003@sju.ac.kr>
2023-10-24 18:09:52 +0900

    git commit -m "test:controller와 main연결"

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,7 +1,14 @@
 package baseball.controller;
 
+import baseball.view.StartView;
+
 public class GameController {
     public void startGame(){
+        StartView startView=new StartView();
+        startView.beginMessage();
+    }
+
+    public void playGame(){
 
     }
 }
