24693b3b761e297368191993e51721b229f6a382
DK <laancer4@gmail.com>
2023-10-24 21:08:53 +0900

    refactor : 쓸모없는 변수 및 매서드 제거

--- src/main/java/ball/BallStatus.java
+++ src/main/java/ball/BallStatus.java
@@ -4,26 +4,4 @@ public enum BallStatus {
     STRIKE,
     BALL,
     NOTHING;
-    private int strikeCount;
-    private int ballCount;
-
-    public void addStrikeCount(){
-        this.strikeCount++;
-    }
-
-    public void addBallCount(){
-        this.ballCount++;
-    }
-
-    public int getStrikeCount() {
-        return strikeCount;
-    }
-
-    public int getBallCount() {
-        return ballCount;
-    }
-
-    public boolean isNothing(){
-        return this.ballCount == 0 && this.strikeCount == 0;
-    }
 }
