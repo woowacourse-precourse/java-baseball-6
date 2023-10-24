9fb8db92b91284026c606a0802e9e4d2e17ade94
Flash-KIM142 <topqr123q@gmail.com>
2023-10-25 02:18:38 +0900

    feat: Referee의 채점 기능 구현

--- src/main/java/baseball/model/Referee.java
+++ src/main/java/baseball/model/Referee.java
@@ -1,13 +1,13 @@
 package baseball.model;
 
+import baseball.constant.GameConstant;
 import baseball.model.dto.PlayerDto;
 import baseball.model.dto.ResultDto;
 import java.util.List;
 
 public class Referee {
 
-    private List<Integer> answer;
-
+    private final List<Integer> answer;
     private Boolean keepGoing = true;
 
     public Referee(List<Integer> answer) {
@@ -19,7 +19,23 @@ public class Referee {
     }
 
     public ResultDto decidesResult(PlayerDto playerDto) {
+        List<Integer> playerInputList = playerDto.getInputList();
+        Integer strike = 0;
+        Integer ball = 0;
 
+        for (int i = 0; i < GameConstant.GAME_NUMBER_LENGTH; i++) {
+            Integer answerNumber = answer.get(i);
+            Integer playerNumber = playerInputList.get(i);
+
+            if (answerNumber.equals(playerNumber)) {
+                strike++;
+            } else if (answer.contains(playerNumber)) {
+                ball++;
+            }
+        }
+
+        isGameover(strike);
+        return new ResultDto(strike, ball);
     }
 
     private void isGameover(Integer strike) {
