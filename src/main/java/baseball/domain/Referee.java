ac33ee08354ac583e4688a8bc70c07af9ecb7b31
PARK9898 <wjddjs21@naver.com>
2023-10-25 02:17:38 +0900

    "style: 코드 포맷팅 & 상수 선언"

--- src/main/java/baseball/domain/Referee.java
+++ src/main/java/baseball/domain/Referee.java
@@ -2,33 +2,35 @@ package baseball.domain;
 
 public class Referee {
 
-	private final Numbers answerNumbers;
+    private final int NUMBERS_LENGTH = 3;
 
-	public Referee(Numbers randomNumbers) {
-		this.answerNumbers = randomNumbers;
-	}
+    private final Numbers answerNumbers;
 
-	public int countStrike(Numbers playerNumbers) {
-		int position = 0;
-		int strike = 0;
-		for(int i = 0; i < 3; i++) {
-			if(answerNumbers.isSamePosition(playerNumbers,position)) {
-				strike++;
-			}
-			position++;
-		}
-		return strike;
-	}
+    public Referee(Numbers randomNumbers) {
+        this.answerNumbers = randomNumbers;
+    }
 
-	public int countBall(Numbers playerNumbers) {
-		int position = 0;
-		int ball = 0;
-		for(int i = 0; i < 3; i++) {
-			if(answerNumbers.isSameNumber(playerNumbers,position)) {
-				ball++;
-			}
-			position++;
-		}
-		return ball;
-	}
+    public int countStrike(Numbers playerNumbers) {
+        int position = 0;
+        int strike = 0;
+        for (int i = 0; i < NUMBERS_LENGTH; i++) {
+            if (answerNumbers.isSamePosition(playerNumbers, position)) {
+                strike++;
+            }
+            position++;
+        }
+        return strike;
+    }
+
+    public int countBall(Numbers playerNumbers) {
+        int position = 0;
+        int ball = 0;
+        for (int i = 0; i < 3; i++) {
+            if (answerNumbers.isSameNumber(playerNumbers, position)) {
+                ball++;
+            }
+            position++;
+        }
+        return ball;
+    }
 }
