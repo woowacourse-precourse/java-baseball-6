b169d4564efdbffbac3c34925a84cc4802609ccb
Chris <hereokay88@gmail.com>
2023-10-24 13:19:05 +0900

    feat: 컴퓨터 랜덤 숫자 지정하기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,6 +9,11 @@ public class Application {
         System.out.println("숫자 야구 게임을 시작합니다.");
         int execInput = 1;
         while (execInput == 1){
+            int[] computerNumber = {
+                    Randoms.pickNumberInRange(1, 9),
+                    Randoms.pickNumberInRange(1, 9),
+                    Randoms.pickNumberInRange(1, 9)
+            };
 
 
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
