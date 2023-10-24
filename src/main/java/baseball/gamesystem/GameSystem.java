a82aad2983b743ca6cc1faadd1998ec7f1d1a85b
wschoe <chldntjr0425@naver.com>
2023-10-25 03:19:55 +0900

    feat: add doGame method in GameSystem

--- src/main/java/baseball/gamesystem/GameSystem.java
+++ src/main/java/baseball/gamesystem/GameSystem.java
@@ -1,23 +1,34 @@
 package baseball.gamesystem;
 
+import baseball.broadcaster.Broadcaster;
+import baseball.broadcaster.RESULT;
+import baseball.computer.Computer;
 import baseball.exception.IllegalArgumentException;
+import baseball.umpire.Umpire;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class GameSystem {
 
-    public void start(){
+    public static void start(){
         System.out.println("숫자 야구 게임을 시작합니다.");
-        doGame();
     }
 
-    private void doGame(){
+    private static RESULT doGame(List<Integer> computerNumbers){
+        System.out.println("숫자를 입력해주세요 : ");
+        String userInput = Console.readLine();
 
+        validate(userInput);
+        List<Integer> userNumbers = convertIntoNumbers(userInput);
+
+        return Broadcaster.broadcast(Umpire.judge(computerNumbers, userNumbers));
     }
 
-    private void validate(String userInput){
+    private static void validate(String userInput){
         Set<Character> uniqueInputs = new HashSet<>();
         if(userInput.length() != 3){
             throw new IllegalArgumentException("서로 다른 3개의 수를 입력해주세요.");
@@ -30,5 +41,13 @@ public class GameSystem {
                 throw new IllegalArgumentException("중복이 발생했습니다.");
             }
         }
+    }
+
+    private static List<Integer> convertIntoNumbers(String userInput){
+        List<Integer> numbers = new ArrayList<>();
+        for (char ch : userInput.toCharArray()) {
+            numbers.add(Character.getNumericValue(ch));
+        }
+        return numbers;
     }
 }
