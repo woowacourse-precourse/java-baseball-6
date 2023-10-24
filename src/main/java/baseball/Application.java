b64787946f06dc8ef19b9ec1e231831238cabc30
onlySaying <48788534+onlySaying@users.noreply.github.com>
2023-10-25 02:00:42 +0900

    dd

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,6 +8,7 @@ public class Application {
     public static void main(String[] args) 
     {
         // TODO: 프로그램 구현
+    	//실행
     	System.out.println("숫자 야구 게임을 시작합니다.");
     	
     	while(true)
