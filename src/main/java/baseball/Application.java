15e980757f5192afd683499af392ffa101b078ee
jacomyou1026 <jacomyou1026@naver.com>
2023-10-25 00:11:54 +0900

    fix: 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
