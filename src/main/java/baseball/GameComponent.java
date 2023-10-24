552007af5c2d8d56b622d05927f95fef83048a57
Eunji1217 <94281256+Eunji1217@users.noreply.github.com>
2023-10-25 02:04:32 +0900

    Feat : 메인 게임 class 구현

--- src/main/java/baseball/GameComponent.java
+++ src/main/java/baseball/GameComponent.java
@@ -10,6 +10,7 @@ public class GameComponent {
     public static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     public static final String INPUT_MESSAGE = "숫자를 입력해 주세요 : ";
     public static final String COMPLETE_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public static String randNum = "";
     public static String userNum = "";
