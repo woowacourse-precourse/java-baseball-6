6347c1b5721ae015e0f16689727af3a513f9ba2e
YoonseolLee <gujueng32@naver.com>
2023-10-24 13:27:19 +0900

    feat: 숫자야구 게임을 실행하는 기능

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -1,11 +1,27 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Controller {
     private static final String RESTART_REQUEST_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static final String RESTART_GAME = "1";
     private static final String PROGRAM_EXIT = "2";
 
     public Controller() {
+    }
+
+    public void startGame() {
+        Computer computer = new Computer();
+        Player player = new Player();
+        String status;
+
+        do {
+            player.init();
+            player.play();
+            printRestartRequestMessage();
+            status = readLine();
+            validateRestartRange(status);
+        } while (RESTART_GAME.equals(status));
     }
 
     private void printRestartRequestMessage() {
