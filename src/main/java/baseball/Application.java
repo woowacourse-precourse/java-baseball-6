a28a86a9c77c2aed9f0ae0dd795211f7815ad7de
LeeJaeHong <0915jhlee@daum.net>
2023-10-24 10:53:30 +0900

    Feat: 스트라이크 및 볼 체크 함수 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,9 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.*;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
@@ -10,14 +11,14 @@ public class Application {
     }
 
     public static List<Integer> makeRandomNumber() {
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
+        List<Integer> computerNumber = new ArrayList<>();
+        while (computerNumber.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
+            if (!computerNumber.contains(randomNumber)) {
+                computerNumber.add(randomNumber);
             }
         }
-        return computer;
+        return computerNumber;
     }
 
     public static List<Integer> inputUserNumber(){
@@ -29,12 +30,35 @@ public class Application {
         return inputNumber;
     }
 
+    public static Result checkResult(List<Integer> computerNumber, List<Integer> inputNumber) {
+        Result result = new Result();
+
+        List<Integer> ballList = new ArrayList<>();
+        ballList.addAll(computerNumber);
+
+        for (int i = 0; i < 3; i++) {
+            if(computerNumber.get(i).equals(inputNumber.get(i))){
+                result.strike++;
+                ballList.set(i, -1);
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            if (ballList.contains(inputNumber.get(i))) {
+                result.ball++;
+            }
+        }
+        if(result.ball==0&&result.strike==0){
+            result.isNothing=true;
+        }
+        return result;
+    }
 
     public static class Result{
         int strike;
         int ball;
         boolean isNothing;
         public Result() {
+            this.isNothing=false;
         }
     }
 }
