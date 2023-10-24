8b464c2bb738cefc4eb6768dc736c52615be6753
Dowon <3120kang@gmail.com>
2023-10-24 21:39:15 +0900

    test: Hint에 새로운 값이 저장될 때, 기존의 값은 잘 삭제되었는지 확인하는 테스트 추가

--- src/test/java/computer/HintTest.java
+++ src/test/java/computer/HintTest.java
@@ -23,4 +23,20 @@ class HintTest {
         Assertions.assertThat(hint.loadBall()).isEqualTo(ball);
 
     }
+
+    @Test
+    @DisplayName("힌트가 새로 입력될때마다 기존의 값이 초기화되고 새로 입력된 값이 저장되어야 한다.")
+    void 힌트_클리어_테스트(){
+        //given
+        Hint hint=Hint.getInstance();
+        int strike=2;
+        int ball=0;
+
+        //when
+        hint.save(strike,ball);
+
+        //then
+        Assertions.assertThat(hint.loadStrike()).isEqualTo(strike);
+        Assertions.assertThat(hint.loadBall()).isEqualTo(ball);
+    }
 }
