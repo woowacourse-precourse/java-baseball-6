cf359577f0c0233239863127391c9aee8592b92b
peeaaccee <wlgp8958@naver.com>
2023-10-25 07:46:59 +0900

    test(InputValidator): 3자리 미만인 2자리 수일때 오류인지 테스트

--- src/test/java/baseball/utils/InputValidatorTest.java
+++ src/test/java/baseball/utils/InputValidatorTest.java
@@ -24,4 +24,13 @@ class InputValidatorTest {
         );
     }
 
+    @Test
+    @DisplayName("입력 오류 3자리 미만 : 2자리")
+    void 사용자_입력_오류2_2() {
+        String userInput = "12";
+        assertThrows(IllegalArgumentException.class,
+                () -> InputValidator.checkUserBallInput(userInput)
+        );
+    }
+
 }
