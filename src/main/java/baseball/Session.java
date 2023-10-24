a0d669bdf23203d6d23ecea6763ecfcde3a64969
woo <wsk221e@gmail.com>
2023-10-24 10:36:32 +0900

    refactor: Session 클래스 리팩토링(반복문 및 출력 구조)

--- src/main/java/baseball/Session.java
+++ src/main/java/baseball/Session.java
@@ -3,27 +3,25 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Session {
+    // 프로그램 시작, 응답받기
     public static void startGame() {
-        while (true) {
+        do {
             System.out.print("숫자를 입력해주세요 : ");
             String answer = Console.readLine();
 
             Validation.isValidAnswer(answer);
             Defender.printScore(answer);
 
-            if (endGame()) {
-                break;
-            }
-        }
+        } while (!endGame());
     }
 
     // 프로그램 종료하기
     public static boolean endGame() {
         boolean isRestart = false;
+        String endMessage = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
         if (Defender.isThreeStrikes()) {
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            System.out.println(endMessage);
             String restart = Console.readLine();
 
             // TODO: Validation 클래스 이용
