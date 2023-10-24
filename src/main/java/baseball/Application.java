5cb557acc2389022cf5d96c3cd76b8e2d05f0527
lesw <lesw1216@gmail.com>
2023-10-24 19:32:57 +0900

    refactor: delete throws exception

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,7 +7,7 @@ import baseball.view.InputView;
 import baseball.view.OutputView;
 
 public class Application {
-    public static void main(String[] args) throws IllegalArgumentException{
+    public static void main(String[] args) {
         // TODO: 프로그램 구현
         BaseballMachine baseballMachine = new BaseballMachine(new Computer(), new Player());
         baseballMachine.run();
