45434b1bbbac1655d6c9e464d1e7d611b99ad968
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-10-24 16:19:17 +0900

    Feat: 메인 함수 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import baseball.domain.GameHost;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameHost gameHost = new GameHost();
+        gameHost.proceedGame();
     }
 }
