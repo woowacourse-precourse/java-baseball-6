f004c7d8603a2f0560824d781af31b659068eaa9
honeydanji <tjdwls3680@naver.com>
2023-10-24 17:30:24 +0900

    FEAT(GameMessage) : "야구게임" 안내 메세지 상수 선언
    - 게임 진행 중 나오는 안내 메시지는 전부 여기에 구현
    - 게임 안내 메시지는 대부분 변경이 없기 때문에 상수로 선언
    - 메시지 변경이 필요하면 해당 클래스 내에서 수정할 수 있어 효율적임

---
+++ src/main/java/baseball/Utils/GameMessage.java
@@ -0,0 +1,11 @@
+package baseball.Utils;
+
+public class GameMessage {
+    public static final String START = "게임을 시작 하겠습니다.";
+    public static final String INPUT = "숫자를 입력해주세요 : ";
+    public static final String OUTPUT = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String STRIKE = "스트라이크";
+    public static final String BALL = "볼 ";
+    public static final String NOTHING = "낫싱";
+    public static final String END = "게임을 시작하려면 1, 종료하려면 2를 입력하세요.";
+}
