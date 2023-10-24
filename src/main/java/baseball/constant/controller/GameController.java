80f774bffa46050719b0cc3fbf265fc8a2e6c301
jongheonleee <qwefghnm1212@gmail.com>
2023-10-24 22:00:52 +0900

    [feat] 게임 컨트롤러 구현

---
+++ src/main/java/baseball/constant/controller/GameController.java
@@ -0,0 +1,48 @@
+package baseball.controller;
+
+import baseball.domain.ComputerBalls;
+import baseball.domain.PlayerBalls;
+import baseball.domain.GameResult;
+import baseball.service.GameService;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+
+import baseball.constant.Constant;
+
+public class GameController {
+
+    public void play() {
+        OutputView.printGameStartMessage();
+        do {
+            gameProcess();
+        } while (InputView.gameRestart());
+    }
+
+
+    public void gameProcess() {
+        ComputerBalls computerBalls = new ComputerBalls();
+        GameResult playerResult = oneRound(computerBalls);
+        while (!checkGameClear(playerResult)) {
+            playerResult = oneRound(computerBalls);
+        }
+    }
+
+
+    public GameResult oneRound(ComputerBalls computerBalls) {
+        OutputView.printInputMessage();
+        PlayerBalls playerBalls = new PlayerBalls(InputView.inputPlayerBalls());
+        GameResult playerResult = GameService.judge(playerBalls.getBalls(), computerBalls.getBalls());
+        OutputView.printPlayerResultMessage(playerResult);
+        return playerResult;
+    }
+
+    public Boolean checkGameClear(GameResult playerResult) {
+        if (playerResult.getStrike() == 3) {
+            OutputView.printGameEndMessage();
+            return true;
+        }
+        return false;
+    }
+
+}
