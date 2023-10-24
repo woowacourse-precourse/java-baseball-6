62fb3913e1ef1b63dfc5937fa0ea70e0733c38b0
dbstn1325 <dbstn1325@naver.com>
2023-10-25 05:02:33 +0900

    feat: 컴퓨터 숫자(랜덤 숫자) 생성

---
+++ src/main/java/baseball/Baseball.java
@@ -0,0 +1,11 @@
+package baseball;
+
+import baseball.utils.Utils;
+import java.util.List;
+
+public class Baseball {
+
+    public void startGame() {
+        List<Integer> computerNumber = Utils.generateRandomNumber(3);
+    }
+}
