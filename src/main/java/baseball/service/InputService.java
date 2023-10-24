5b33a4c77677acb765e2a69a3967916e202651d6
minsu20 <syt07100@gmail.com>
2023-10-24 20:53:12 +0900

    feat: 게임 재실행 여부 입력 받기

--- src/main/java/baseball/service/InputService.java
+++ src/main/java/baseball/service/InputService.java
@@ -5,13 +5,22 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputService {
     public static String getNumber(int size){
         String input=Console.readLine();
-        if(!isValidNumberInput(input,size)){
+        if(!isValidNumberInput(input,1,9,size)){
             throw new IllegalArgumentException("입력값이 유효하지 않습니다.");
         }
         return input;
     }
-    private static boolean isValidNumberInput(String input, int size) {
-        String regex = "^[1-9]{" + size + "}$";
-        return input.matches(regex);
+    public static boolean restart(int restartNum, int exitNum){
+        String input=Console.readLine();
+        if(!isValidNumberInput(input,1,2,1)){
+            throw new IllegalArgumentException("일력값이 유효하지 않습니다.");
+        }
+        int isRestart=Integer.parseInt(input);
+        return isRestart==restartNum;
+    }
+
+    private static boolean isValidNumberInput(String input, int minNum, int maxNum, int size) {
+        String pattern = "[" + minNum + "-" + maxNum + "]{" + size + "}";
+        return input.matches(pattern);
     }
 }
