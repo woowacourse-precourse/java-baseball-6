752fbef8ffb932d26070c8b204c7e98a8f9a30b5
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-10-24 20:51:30 +0900

    feat : 게임 종료 문구 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -9,7 +9,11 @@ public class OutputView {
         System.out.print("숫자를 입력해주세요 : ");
     }
 
-    public void printExitOption(){
+    public void printExitOptionPhrase(){
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    public void printExitPhrase(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 }
