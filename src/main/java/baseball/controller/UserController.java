54cfe6c58a6be76287822b5740af4675369f6871
JongMinCh0i <wonderland314592@gmail.com>
2023-10-24 22:52:34 +0900

    feat(UserController): View 의존성 추가

--- src/main/java/baseball/controller/UserController.java
+++ src/main/java/baseball/controller/UserController.java
@@ -1,16 +1,20 @@
 package baseball.controller;
 
 import baseball.service.GameService;
+import baseball.view.View;
 
 public class UserController implements Controller {
+    private final View computerView;
     private final GameService gameService;
 
-    public UserController(GameService gameService) {
+    public UserController(View computerView, GameService gameService) {
+        this.computerView = computerView;
         this.gameService = gameService;
     }
 
     @Override
     public void gameStart() {
+        computerView.renderView();
         gameService.solvingProblem();
     }
 }
