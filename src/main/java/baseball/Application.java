70c595255b9b506b44f0638c9bffc77ea68598de
jihun-24k <gi020477@gmail.com>
2023-10-25 02:08:17 +0900

    Feat: 사용자에게 받아온 값을 Integer 값으로 변환

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,18 +9,28 @@ public class Application {
     public static void main(String[] args) {
         final List<Integer> computer = makeRandomNumbers();
 
+        String input;
         System.out.println("숫자 야구 게임을 시작합니다.");
 
         outer:
         while (true) {
             System.out.print("숫자를 입력해주세요 : ");
-
+            input = Console.readLine();
+            List<Integer> inputNumbers = toIntegerArray(input);
             break outer;
         }
     }
 
+    private static List<Integer> toIntegerArray(String input) {
+        final List<Integer> inputList = new ArrayList<>();
+        for (String s : input.split("")) {
+            inputList.add(Integer.valueOf(s));
+        }
+        return inputList;
+    }
+
     public static List<Integer> makeRandomNumbers() {
-        List<Integer> computer = new ArrayList<>();
+        final List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computer.contains(randomNumber)) {
