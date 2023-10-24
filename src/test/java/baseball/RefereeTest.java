d0cbb1332fad6271c782e473e6e67517ba5205c7
PARK9898 <wjddjs21@naver.com>
2023-10-25 00:51:04 +0900

    "refactor: Referee 스트라이크 test 수정"

--- src/test/java/baseball/RefereeTest.java
+++ src/test/java/baseball/RefereeTest.java
@@ -11,10 +11,12 @@ import baseball.domain.Referee;
 public class RefereeTest {
 	@Test
 	void 심판은_스트라이크_개수를_셀_수_있다() {
-		Referee referee = new Referee(List.of(1,2,7));
+		List<Integer> randomNumbers = List.of(1,2,7);
 		List<Integer> inputNumbers = List.of(1,2,3);
 		Numbers playerNumbers = new Numbers(inputNumbers);
+		Numbers answerNumbers = new Numbers(randomNumbers);
 
+		Referee referee = new Referee(answerNumbers);
 		Assertions.assertThat(referee.countStrike(playerNumbers)).isEqualTo(2);
 	}
 }
