dd9c5cc329b311e7f2eedc1bb1200553528d49fd
yangyanghyunjung <hyunjungyang0922@gmail.com>
2023-10-24 23:55:42 +0900

    🎯feat(view / InputView): 숫자 입력 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,8 +1,14 @@
 package view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputView {
     private static final String GAME_START_MESSAGE ="숫자 야구 게임을 시작합니다.";
     private static final String INPUT_NUMBER= "숫자를 입력해주세요: ";
+    List<Integer> userInputNumberList = new ArrayList<>();
 
     public void StartGameMessage() {
         System.out.println(GAME_START_MESSAGE); // 프로그램 시작
@@ -11,4 +17,17 @@ public class InputView {
         System.out.println(INPUT_NUMBER); // 숫자입력 msg
     }
 
+    public List<Integer> InputUserNumber() {
+        while (userInputNumberList.size() < 3) {
+            System.out.println(INPUT_NUMBER);
+            String inputs = Console.readLine();
+            for (int i = 0; i < inputs.length(); i++) {
+                char input = inputs.charAt(i);
+                if (!userInputNumberList.contains(input - '0')) {
+                    userInputNumberList.add(Integer.valueOf(input - '0'));
+                }
+            }
+        }
+        return userInputNumberList;
+    }
 }
