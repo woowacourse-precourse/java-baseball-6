ccb3220859b3bcf348bc4052acb123ef87bb7207
mingadinga <minpearl0826@gmail.com>
2023-10-24 22:02:14 +0900

    FEATURE GameView에서 start와 guessUntilCorrect 연결

--- src/main/java/baseball/ui/GameView.java
+++ src/main/java/baseball/ui/GameView.java
@@ -17,6 +17,7 @@ public class GameView {
     public void start() {
         gameController.start();
         System.out.println("숫자 야구 게임을 시작합니다.");
+        guessUntilCorrect();
     }
 
     public void guessUntilCorrect() {
