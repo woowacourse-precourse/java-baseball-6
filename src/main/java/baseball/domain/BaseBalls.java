dbe68638264aacbd9ac19a3435eef5baab05d11e
K-KY <kyuyoungk@naver.com>
2023-10-25 01:10:37 +0900

    Refactor(BaseBalls) : getScore()메소드 삭제, compare()메소드에서 forEach문 사용하도록 변경

--- src/main/java/baseball/domain/BaseBalls.java
+++ src/main/java/baseball/domain/BaseBalls.java
@@ -3,26 +3,18 @@ package baseball.domain;
 import java.util.List;
 
 public class BaseBalls {
-    private final List<CompareNumber> generatedBalls;//컴퓨터가 생성한 번호
+    private final ComputerNumber computerNumber;//컴퓨터가 생성한 번호
 
     public BaseBalls(List<CompareNumber> generatedBall) {
-        this.generatedBalls = generatedBall;
-    }
-    public GameResult compare(List<CompareNumber> userBalls) {
-        GameResult gameResult = new GameResult();
-        for (int i = 0; i < userBalls.size(); i++) {
-            CompareNumber compareNumber = userBalls.get(i);
-            gameResult.saveResult(getScore(compareNumber));
-        }
-        return gameResult;
+        this.computerNumber = new ComputerNumber(generatedBall);
     }
 
-    private BallScore getScore(CompareNumber userBall) {
-        //인자로 CompareNumber(pos, num)을 받는다. -> generatedBalls의 각 객체의 해시코드와 num값을 비교한다.
-        //generatedBalls 에는 각 자리의 숫자를 CompareNumber(pos, num) 의 형식으로 변환한 객체가 들어있다.
-        return generatedBalls.stream()
-                .map(v -> v.calculateScore(userBall))
-                .filter(v -> !v.equals(BallScore.NOTHING))
-                .findFirst().orElse(BallScore.NOTHING);
+    public GameResult compare(List<CompareNumber> userBalls) {
+        GameResult gameResult = new GameResult();
+        userBalls.forEach(v -> {
+            BallScore ballScore = computerNumber.getScore(v);
+            gameResult.saveResult(ballScore);
+        });
+        return gameResult;
     }
 }
