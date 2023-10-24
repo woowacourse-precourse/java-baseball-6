055a093fda4a80b890083d1a53afbdca91f73330
foreverfall123 <foreverfall@naver.com>
2023-10-24 20:04:45 +0900

    [function] create set answer function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public List<Integer> setAnswer() {
+        List<Integer> result = new ArrayList<>();
+        while (result.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!result.contains(randomNumber)) {
+                result.add(randomNumber);
+            }
+        }
+
+        return result;
     }
 }
