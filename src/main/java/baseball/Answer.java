a7f85128e3cabdae8aa519e85a07c2cb0af71b89
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-10-25 08:49:14 +0900

    feat: 숫자야구의 답 Answer 클래스 추가
    1. 답을 초기화 할 때 서로 다른 랜덤 수 3개로 초기화
    2. 사용자가 입력한 값을 채점해서 GradeResult 객체로 반환한다

---
+++ src/main/java/baseball/Answer.java
@@ -0,0 +1,63 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Answer {
+
+
+    private static final char ZERO_CHAR = '0';
+    private static final int ANSWER_LENGTH = 3;
+    private static int ball;
+    private static int strike;
+    private List<Integer> input;
+    private List<Integer> answer;
+
+    public Answer() {
+        answer = new ArrayList<>();
+        while (answer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!answer.contains(randomNumber)) {
+                answer.add(randomNumber);
+            }
+        }
+    }
+
+    public GradeResult gradeInput(String answerInput) {
+
+        strike = 0;
+        ball = 0;
+        input = new ArrayList<>();
+
+        for (char number : answerInput.toCharArray()) {
+            input.add(charToInt(number));
+        }
+
+        compareAnswerWithInput();
+
+        return new GradeResult(ball, strike);
+    }
+
+    private void compareAnswerWithInput() {
+        for (int answerIndex = 0; answerIndex < ANSWER_LENGTH; answerIndex++) {
+            for (int inputIndex = 0; inputIndex < ANSWER_LENGTH; inputIndex++) {
+                increaseValue(answerIndex, inputIndex);
+            }
+        }
+    }
+
+    private void increaseValue(int answerIndex, int inputIndex) {
+        if (answer.get(answerIndex).equals(input.get(inputIndex))) {
+            if (answerIndex == inputIndex) {
+                strike += 1;
+                return;
+            }
+            ball += 1;
+        }
+    }
+
+    private int charToInt(char number) {
+        return number - ZERO_CHAR;
+    }
+}
