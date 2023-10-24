be36188fca28ff3f23f84f2f1e480a5b468e114e
Kim Taeyeong <tyeong1102@gmail.com>
2023-10-24 22:32:42 +0900

    refactor: 변수명 변경

--- src/main/java/baseball/constant/Number.java
+++ src/main/java/baseball/constant/Number.java
@@ -6,7 +6,7 @@ public class Number {
 
     public static final Integer END_NUM = 2;
 
-    public static final Integer LENGTH = 3;
+    public static final Integer INPUT_LENGTH = 3;
 
     public static final Integer NUM_FIRST = 1;
 
