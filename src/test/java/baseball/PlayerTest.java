6c4aa9356948b52aa6cf632ba2f7f98742e58392
jmj005 <jmj00005@naver.com>
2023-10-24 21:05:26 +0900

    teat: 플레이어 숫자 공백 테스트 추가

--- src/test/java/baseball/PlayerTest.java
+++ src/test/java/baseball/PlayerTest.java
@@ -3,6 +3,7 @@ package baseball;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -23,11 +24,16 @@ public class PlayerTest {
     @CsvSource({
             "1234",
             "q",
-            "",
             "112"
     })
     void 플레이어_숫자_유효성_실패_테스트(String playerNumbers) {
         assertThrows(IllegalArgumentException.class,
                 () -> new Player(playerNumbers));
+    }
+
+    @Test
+    void 플레이어_숫자_공백_테스트() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new Player(""));
     }
 }
