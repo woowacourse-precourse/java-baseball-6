b6e9deaff41d4e4348603d334042dcbf87d9ee3f
Subin <rla0010101@gmail.com>
2023-10-24 17:14:56 +0900

    Feat: 스트라이크 횟수 판별 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     private static String randomNumber;
+    private static String inputNumber;
 
     public static void main(String[] args) throws IllegalArgumentException {
         //TODO: 숫자 야구 게임 구현
@@ -20,6 +21,13 @@ public class Application {
         return randomNumber;
     }
 
-
+    public static int calculateStrikeCount() {
+        int cnt = 0;
+        for (int i = 0; i < 3; i++) {
+            if(randomNumber.charAt(i) == inputNumber.charAt(i))
+                cnt++;
+        }
+        return cnt;
+    }
 
 }
