368e24363ec025c8a327d97da0f9b9f0b807a8e8
Hyun Woo <hwsa10041@gmail.com>
2023-10-24 14:01:19 +0900

    feat : checkScoreResult() 구현

--- src/main/java/baseball/model/Game.java
+++ src/main/java/baseball/model/Game.java
@@ -3,6 +3,8 @@ package baseball.model;
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class Game {
+    public static final int STRIKE = 1;
+    public static final int BALL = 0;
     private final char[] randNumber;
 
     private Game() {
@@ -18,8 +20,40 @@ public class Game {
         this.randNumber = sb.toString().toCharArray();
     }
 
+    private Game(char[] randNumber) {
+        this.randNumber = randNumber;
+    }
+
     public static Game createRandomNumber() {
         return new Game();
+    }
+
+    public static Game createRandomNumberForInput(char[] input) {
+        return new Game(input);
+    }
+
+    public int[] checkScoreResult(String input) {
+        int[] array = new int[2];
+        char[] computerNumber = this.randNumber;
+        char[] inputNumber = input.toCharArray();
+
+        for (int i = 0; i < inputNumber.length; i++) {
+            for (int j = 0; j < computerNumber.length; j++) {
+                countStrikeAndBall(array, computerNumber, inputNumber, i, j);
+            }
+        }
+
+        return array;
+    }
+
+    private static void countStrikeAndBall(int[] array, char[] computerNumber, char[] inputNumber, int i, int j) {
+        if (inputNumber[i] == computerNumber[j]) {
+            if (i == j) {
+                array[STRIKE]++;
+            } else {
+                array[BALL]++;
+            }
+        }
     }
 
     public char[] getRandNumber() {
