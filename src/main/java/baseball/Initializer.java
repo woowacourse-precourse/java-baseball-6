488688e358ba2bad195dfa9c4310e1c30baaf087
Limwngur <ovg07047@naver.com>
2023-10-25 00:51:10 +0900

    Refactor: setUser() 아스키 코드값 출력 -> 정수 출력으로 변경

--- src/main/java/baseball/Initializer.java
+++ src/main/java/baseball/Initializer.java
@@ -3,13 +3,14 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Initializer {
 
-    private List<Integer> comNum;
-    private List<Integer> userNum;
+    private List<Integer> comNum=new ArrayList<>();
+    private List<Integer> userNum=new ArrayList<>();
     public List<Integer> setCom(){
 
         while(comNum.size()<3){
@@ -21,14 +22,16 @@ public class Initializer {
     public List<Integer> setUser(){
         String userInStr =Console.readLine();
         int inputSize = userInStr.length();
-
+        System.out.println(userInStr.charAt(0));
         if(inputSize>3){
             //TODO: 예외처리
             new ArrayIndexOutOfBoundsException("잘못된 입력입니다.");
         }
 
         for (int i = 0; i < 3; ++i) {
-            this.userNum.add(Integer.valueOf(userInStr.charAt(i)));            ;
+            this.userNum.add(
+                    Integer.valueOf(userInStr.charAt(i)-'0')
+            );            ;
         }
 
         return this.userNum;
