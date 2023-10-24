2dfbc5fb835749270ab695b53fddf51504caac55
LeeSeungUk <tmddnr5410@gmail.com>
2023-10-24 18:12:14 +0900

    fix : 정답 생성시 각 자리가 다른 숫자를 생성하도록 코드수정

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -2,6 +2,8 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BaseballGame {
     private String answerComputer;
@@ -26,7 +28,6 @@ public class BaseballGame {
 
         String messageScore;
 
-        System.out.print("생성된 숫자는 " + answerComputer + "\n");
         do {
 
             System.out.print("숫자를 입력해주세요 : ");
@@ -81,8 +82,17 @@ public class BaseballGame {
 
 
     public void initAnswer() {
-        int randomNum = Randoms.pickNumberInRange(111, 999);
-        this.answerComputer = Integer.toString(randomNum);
+        List<Integer> computer = new ArrayList<>();
+        StringBuilder answerBuilder = new StringBuilder();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+                answerBuilder.append(Integer.toString(randomNumber));
+            }
+        }
+        this.answerComputer = answerBuilder.toString();
+        System.out.print("생성된 숫자는 " + answerComputer + "\n");
     }
 
     public boolean restartGame() {
@@ -125,6 +135,6 @@ public class BaseballGame {
             }
         }
     }
-    
+
 
 }
