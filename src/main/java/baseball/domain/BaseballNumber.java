2fc2fb09ef0207c3520e883204898896b321c2c4
kunsanglee <gosmdochee2@gmail.com>
2023-10-24 13:37:40 +0900

    refactor(BaseballNumber): isContainedIn() 메서드 제거

--- src/main/java/baseball/domain/BaseballNumber.java
+++ src/main/java/baseball/domain/BaseballNumber.java
@@ -1,8 +1,6 @@
 package baseball.domain;
 
 
-import java.util.List;
-
 public class BaseballNumber {
     private final int value;
 
@@ -12,10 +10,5 @@ public class BaseballNumber {
 
     public boolean isSameValue(BaseballNumber anotherNumber) {
         return this.value == anotherNumber.value;
-    }
-
-    public boolean isContainedIn(List<BaseballNumber> baseballNumbers) {
-        return baseballNumbers.stream()
-                .anyMatch(baseballNumber -> baseballNumber.isSameValue(this));
     }
 }
