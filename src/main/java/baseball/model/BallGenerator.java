222b11d3ec17aa4238da6e28d3c9f2a5302f8cce
peeaaccee <wlgp8958@naver.com>
2023-10-25 07:29:28 +0900

    feat(BallGenerator): 필드 상수와 생성자 만들기

---
+++ src/main/java/baseball/model/BallGenerator.java
@@ -0,0 +1,9 @@
+package baseball.model;
+
+public class BallGenerator {
+    private static final int COMPUTER_MAX_SIZE = 3;
+    public static final int START_INCLUSIVE = 1;
+    public static final int END_INCLUSIVE = 9;
+    private BallGenerator() {
+    }
+}
