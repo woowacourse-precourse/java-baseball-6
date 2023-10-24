5c601c84a0be1a76c92352fd18988e4f4497b210
inwoo <korean5078@gmail.com>
2023-10-24 20:16:57 +0900

    feat: 숫자 야구 게임 Main 함수 작성 1. 게임 시작 멘트 출력 2. BaseballGame 객체 생성 및 게임 재시작 여부 확인

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,18 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        int restart = 1;
+        while (restart == 1) {
+            BaseBallGame baseBallGame = new BaseBallGame();
+            for (int i : baseBallGame.getRandomNumber()) {
+                System.out.print(i);
+            }
+            System.out.println();
+            User user = new User();
+            baseBallGame.gameStart();
+            baseBallGame.printRestartQuestion();
+            restart = user.inputRestartNumber();
+        }
     }
 }
