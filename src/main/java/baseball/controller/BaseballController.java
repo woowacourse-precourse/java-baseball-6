dc1f2b4f40c576f5fe2108d581874d588bdd7764
Woo-Shinyoung <tlsdud8205@naver.com>
2023-10-24 22:23:17 +0900

    Feat[controller] 플레이어 숫자 입력 받기

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -2,12 +2,15 @@ package baseball.controller;
 
 import baseball.domain.BaseballNumber;
 import baseball.service.BaseballService;
+import baseball.view.InputView;
 
 public class BaseballController {
     private final BaseballService baseballService;
+    private final InputView inputView;
 
     public BaseballController() {
         this.baseballService = new BaseballService();
+        this.inputView = new InputView();
     }
 
     public void playGame() {
