576a9b62363166dac0c22d311b69d1ba32e272e6
민경 <tjalsrud0101@gmail.com>
2023-10-24 17:02:39 +0900

    feat: print stat a game

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,14 @@
+package baseball;
+
+public class BaseballGame {
+
+    public void play() {
+        printStart();
+    }
+
+    private void printStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+}
+
