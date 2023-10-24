a098983b4d9952596d07d4faf0f41b550123c2a0
dltjdgh0428 <dltjdgh0428@naver.com>
2023-10-25 00:44:26 +0900

    refactor: Controller 개선

--- src/main/java/baseball/Controller/GameController.java
+++ src/main/java/baseball/Controller/GameController.java
@@ -7,9 +7,6 @@ import baseball.View.GameView;
 import java.util.List;
 
 public class GameController {
-    private static final int BALL = 0;
-    private static final int STRIKE = 1;
-
     private Helper helper = new Helper();
     private BaseBallGame baseBallGame = new BaseBallGame(helper);
     private GameView gameView = new GameView();
@@ -20,10 +17,10 @@ public class GameController {
             String input = Valid.checkValid(gameView.printInputMessage());
             List<Integer> userInput = helper.parseUserInput(input);
             List<Integer> ballStrikeCount = baseBallGame.checkBallCount(userInput);
-            int balls = ballStrikeCount.get(BALL);
-            int strikes = ballStrikeCount.get(STRIKE);
 
-            baseBallGame.gameOverCheck(strikes);
+            gameView.printBallStrikeMessage(ballStrikeCount);
+
+            baseBallGame.gameOverCheck(ballStrikeCount.get(1));
 
             if (baseBallGame.isGameOver()) {
                 String inputRestart = gameView.printGameOverMessage();
