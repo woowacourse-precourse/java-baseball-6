cc49529b356c354478c12ed442c19d614932ae94
nahyeon99 <nahyeonee99@gmail.com>
2023-10-24 16:11:11 +0900

    feat: 게임 전체 진행을 관리하는 클래스 구현 - BaseballGame.java 1. playGame() : 게임의 시작부터 정답까지 전체 게임을 관리 2. tryGame() : 유저의 숫자 입력 시도 1회 + 힌트 출력 관리 3. generateComputerNumbers() : 컴퓨터의 세 자리 수 생성 4. receiveUserNumbers() : 유저의 세 자리 수 입력 5. isCorrect() : 컴퓨터 수, 유저 수를 비교햐여 정답 여부 체크 6. printResult() : 유저의 입력 1회 시도 시 힌트 출력하는 메서드

---
+++ src/main/java/baseball/domain/BaseballGame.java
@@ -0,0 +1,85 @@
+package baseball.domain;
+
+import static baseball.console.Constant.INT_ZERO;
+import static baseball.console.Constant.RESULT_BALL;
+import static baseball.console.Constant.RESULT_NOTHING;
+import static baseball.console.Constant.RESULT_STRIKE;
+import static baseball.domain.BaseballNumber.NUMBER_MAX_VALUE;
+import static baseball.domain.BaseballNumber.NUMBER_MIN_VALUE;
+
+import baseball.console.Constant;
+import baseball.console.Input;
+import baseball.console.Output;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class BaseballGame {
+
+    private BaseballNumbers computerNumbers;
+    private BaseballNumbers userNumbers;
+
+
+    public void playGame() {
+        computerNumbers = generateComputerNumbers();
+        do {
+            tryGame();
+        } while (isCorrect());
+    }
+
+    private void tryGame() {
+        Output.printBaseballGameNumberRequest();
+        userNumbers = receiveUserNumbers();
+        printResult();
+    }
+
+    private BaseballNumbers generateComputerNumbers() {
+        List<BaseballNumber> computerNumbers = new ArrayList<>();
+        Set<Integer> isDuplicateSet = new HashSet<>();
+
+        while (computerNumbers.size() < BaseballNumbers.NUMBERS_MAX_SIXE) {
+            int randomNumber = Randoms.pickNumberInRange(NUMBER_MIN_VALUE, NUMBER_MAX_VALUE);
+
+            isDuplicateSet.add(randomNumber);
+            if (computerNumbers.size() + 1 != isDuplicateSet.size()) {
+                continue;
+            }
+
+            computerNumbers.add(new BaseballNumber(randomNumber));
+        }
+        return new BaseballNumbers(computerNumbers);
+    }
+
+    private BaseballNumbers receiveUserNumbers() {
+
+        String numberStr = Input.receiveBaseballNumber();
+        List<BaseballNumber> userNumbers = new ArrayList<>();
+
+        while (userNumbers.size() < numberStr.length()) {
+            userNumbers.add(new BaseballNumber(numberStr.charAt(userNumbers.size()) - Constant.CHAR_ZERO));
+        }
+        return new BaseballNumbers(userNumbers);
+    }
+
+    private boolean isCorrect() {
+        return computerNumbers.equals(userNumbers);
+    }
+
+    private void printResult() {
+
+        int strikeCount = computerNumbers.getStrikeCount(userNumbers);
+        int ballCount = computerNumbers.getBallCount(userNumbers);
+
+        if (strikeCount == INT_ZERO && ballCount == INT_ZERO) {
+            Output.printBaseballGameHint(RESULT_NOTHING);
+        } else if (strikeCount != INT_ZERO && ballCount != INT_ZERO) {
+            Output.printBaseballGameHint(ballCount + RESULT_BALL + " " + strikeCount + RESULT_STRIKE);
+        } else if (strikeCount != INT_ZERO) {
+            Output.printBaseballGameHint(strikeCount + RESULT_STRIKE);
+        } else if (ballCount != INT_ZERO) {
+            Output.printBaseballGameHint(ballCount + RESULT_BALL);
+        }
+    }
+}
