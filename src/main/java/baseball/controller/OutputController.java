7bc59c7d9748b5ca3686bdc2655863b9ac30cf28
chosunghyun18 <krismon@naver.com>
2023-10-25 01:39:27 +0900

    feat : add output controller for message

--- src/main/java/baseball/controller/OutputController.java
+++ src/main/java/baseball/controller/OutputController.java
@@ -1,2 +1,26 @@
-package baseball.controller;public class OutputController {
+package baseball.controller;
+
+import baseball.model.SwingResult;
+import baseball.view.OutputView;
+
+public class OutputController {
+    public void displayHint(SwingResult result) {
+        OutputView.showHint(result.getResultMessage());
+    }
+
+    public void showIntroMessage() {
+        OutputView.showIntroMessage();
+    }
+
+    public void showGameRestartInputMessage() {
+        OutputView.showGameRestartInputMessage();
+    }
+
+    public void showGameInputMessage() {
+        OutputView.showGameInputMessage();
+    }
+
+    public void showGameClearMessage() {
+        OutputView.showGameClearMessage();
+    }
 }
