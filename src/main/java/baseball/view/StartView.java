ecbbe43adb492766e5772d805273f43b6fd33012
jihoon98 <tlswlgns1003@sju.ac.kr>
2023-10-24 16:17:03 +0900

    git commit -m "feat:시작화면 작성"

--- src/main/java/baseball/view/StartView.java
+++ src/main/java/baseball/view/StartView.java
@@ -1,4 +1,13 @@
 package baseball.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class StartView {
+    public static void beginMessage(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+    public static String requestUserInput(){
+        System.out.print("숫자를 입력해 주세요");
+        return Console.readLine();
+    }
 }
