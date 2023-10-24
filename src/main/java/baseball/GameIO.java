7d7dc11e7f0552f3070db2f56c92b52d9f8c57eb
giljin <68088970+giljin@users.noreply.github.com>
2023-10-24 12:45:15 +0900

    feat: 게임 결과 표시 메소드 추가

--- src/main/java/baseball/GameIO.java
+++ src/main/java/baseball/GameIO.java
@@ -4,6 +4,10 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class GameIO {
 
+    static void gameResultPrint(GameResult result) {
+        System.out.println(result);
+    }
+
     static void gameRestartPrint() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"
                 + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
