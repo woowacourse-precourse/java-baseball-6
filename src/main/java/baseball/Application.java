d11b8621e2f969da066eda31bab4d344a99152d7
psrom <psrom721@gmail.com>
2023-10-25 00:08:57 +0900

    feat: Generating a 3-digit random number
    generateRandomNumber() 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,32 @@
 package baseball;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        while (true) {
+            List<Integer> computer = generateRandomNumber();
+            System.out.println("숫자 야구 게임을 시작합니다.");
+
+        }
     }
+
+
+    private static List<Integer> generateRandomNumber() {
+        List<Integer> computer = new ArrayList<>();
+
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+
+            if (!computer.contains((randomNumber))) {
+                computer.add(randomNumber);
+            }
+        }
+
+        return computer;
+    }
+
 }
