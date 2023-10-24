5427aee3984d9499142b519ef436011d15c1066b
je488 <pje488@naver.com>
2023-10-24 19:27:03 +0900

    서로 다른 3자리 수 입력받고 잘못된 값 입력 시 예외 발생

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -10,6 +11,7 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
 
+        //1부터 9까지 서로 다른 임의의 수 3개 선택하여 정답 생성
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -17,7 +19,25 @@ public class Application {
                 computer.add(randomNumber);
             }
         }
-
         System.out.println(computer);
+
+        //서로 다른 3자리 수 입력받고 잘못된 값 입력 시 예외 발생 후 프로그램 종료
+        List<Integer> user = new ArrayList<>();
+        IllegalArgumentException exp = new IllegalArgumentException();
+
+        System.out.print("숫자를 입력해주세요 : ");
+        String str = Console.readLine();
+        if (str.length() != 3) {
+            throw exp;
+        }
+        for (int i = 0; i < 3; i++) {
+            if (!Character.isDigit(str.charAt(i))) {
+                throw exp;
+            } else {
+                user.add(str.charAt(i) - '0');
+            }
+        }
+        System.out.println(user);
+
     }
 }
