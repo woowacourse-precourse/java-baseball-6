3647ec1833c9b5c843d7ad54b6431023749f8d95
EveKristinLee <jylisa0612@naver.com>
2023-10-24 18:03:36 +0900

    feat : 재시작 여부 입력받기

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -17,12 +17,13 @@ public class GameController {
 
     public void startGame() {
         view.startGame();
-        boolean choice = true; //true : 1, false : 2
+        boolean choice;//true : 1, false : 2
 
         do {
             List<Integer> computer = model.generateComputerNums();
             // TODO: 게임 플레이
-            // TODO: 추가 플레이 여부 확인
+            
+            choice = view.replay();
         } while (choice);
     }
 }
