746039f924e31c4316fa6f6a37fe74798f072bf3
cherBlair <cher.blair.h@gmail.com>
2023-10-24 15:12:39 +0900

    [FEAT] 게임 점수 현황 view 추가

--- src/main/java/view/GameScoreView.java
+++ src/main/java/view/GameScoreView.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.util.List;
 import util.GameMessage;
 
 public class GameScoreView {
@@ -11,5 +12,23 @@ public class GameScoreView {
         System.out.println("3스트라이크");
         System.out.println(GameMessage.FINISH_MESSAGE);
         System.out.println(GameMessage.RESTART_SELECT_MESSAGE);
+    }
+
+    public void incompleteThreeStrikeScore(List<Integer> incompleteThreeStrikeScore) { // 3스트라이크 미완성
+        int countStrike = incompleteThreeStrikeScore.get(STRIKE_COUNT_INDEX);
+        int countBall = incompleteThreeStrikeScore.get(BALL_COUNT_INDEX);
+
+        if (countStrike == 0 && countBall == 0) {
+            System.out.println("낫싱");
+        }
+        if (countStrike != 0 && countBall == 0) {
+            System.out.println(countStrike + "스트라이크");
+        }
+        if (countStrike == 0 && countBall != 0) {
+            System.out.println(countBall + "볼");
+        }
+        if (countStrike != 0 && countBall != 0) {
+            System.out.println(countBall + "볼 " + countStrike + "스트라이크");
+        }
     }
 }
