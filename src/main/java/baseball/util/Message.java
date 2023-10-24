b4759ad6eb2b1ac24a0b90d55942f0c78f9ea5fe
sudhdkso <waga1009@gmail.com>
2023-10-24 23:09:37 +0900

    refactor: 출력 메세지 상수화 및 함수명 변경

--- src/main/java/baseball/util/Message.java
+++ src/main/java/baseball/util/Message.java
@@ -1,40 +1,42 @@
 package baseball.util;
 
+import static baseball.domain.enums.Constants.NUM_SIZE;
+
 public class Message {
-    //TODO
-    // - `printScore()` : 몇개를 맞췄는지 출력하는 함수
-    // - `printAskMessage()` : "숫자를 입력하세요 :"
-    // - `printStartMessage()` : 시작 메세지 출력
-    // - `printEndMessage()` : 종료 메세지 출력
-    // - `printRetryMessage()` : 재시작 메세지 출력
-    public void printStartMessage(){
-        System.out.println("숫자 야구 게임을 시작합니다.");
+    private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String END_MESSAGE = "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String RETRY_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
+    private static final String NOTHING = "낫싱";
+    private static final String BALL = "볼";
+    private static final String STRIKE = "스트라이크";
+
+    public void printStartMsg() {
+        System.out.println(START_MESSAGE);
     }
 
-    public void printAskMessage(String num){
-        System.out.println("숫자를 입력해주세요 : "+num);
+    public void printInputMsg(String num) {
+        System.out.println(INPUT_MESSAGE + num);
     }
-    public void printScore(int strikeCount, int ballCount){
-        if(strikeCount == 0 && ballCount == 0) {
-            System.out.println("낫싱");
-        }
-        else if(strikeCount == 0) {
-            System.out.println(ballCount+"볼");
-        }
-        else if(ballCount == 0){
-            System.out.println(strikeCount+"스트라이크");
-        }
-        else{
-            System.out.println(ballCount+"볼 "+strikeCount+"스트라이크");
+
+    public void printScore(int strikeCount, int ballCount) {
+        if (strikeCount == 0 && ballCount == 0) {
+            System.out.println(NOTHING);
+        } else if (strikeCount == 0) {
+            System.out.println(ballCount + BALL);
+        } else if (ballCount == 0) {
+            System.out.println(strikeCount + STRIKE);
+        } else {
+            System.out.println(ballCount + BALL + " " + strikeCount + STRIKE);
         }
     }
 
-    public void printEndMessage(int size){
-        System.out.println(size+"개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    public void printEndMsg() {
+        System.out.println(NUM_SIZE.getValue() + END_MESSAGE);
     }
 
-    public void printRetryMessage(int answer){
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"+answer);
+    public void printRetryMsg(int answer) {
+        System.out.println(RETRY_MESSAGE + answer);
     }
 
 }
