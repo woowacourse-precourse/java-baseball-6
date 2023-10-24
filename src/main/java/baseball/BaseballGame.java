00aa4d5127615923114b2213ec0756484ccc2271
문고은 <>
2023-10-24 16:27:54 +0900

    style: import 정리
    필요 없는 import 정리

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -3,9 +3,7 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class BaseballGame {
     private int strikeCount;
