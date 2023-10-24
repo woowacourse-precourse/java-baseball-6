c6eb57884b3c06da11af1e89c0d2171a38663a3d
HongChaa <jun761_1@naver.com>
2023-10-24 16:30:56 +0900

    refactor(GameView): 결과힌트메세지 출력 메서드 매개변수 변경

--- src/main/java/baseball/view/GameView.java
+++ src/main/java/baseball/view/GameView.java
@@ -9,10 +9,8 @@ public class GameView {
     }
 
     public void printSuccessGameMessage() {
-
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료 ");
         printOptionMessage();
-
     }
 
     private void printOptionMessage() {
@@ -23,21 +21,9 @@ public class GameView {
         System.out.print("숫자를 입력해주세요 : ");
     }
 
-    public void printAnswerHintMessage(PlayerAnswer playerAnswer) {
-        System.out.println(printBallOrStrike(playerAnswer));
-    }
-
-    private String printBallOrStrike(PlayerAnswer playerAnswer) {
-        String hint = "";
-
-        if (playerAnswer.getBall() > 0) {
-            hint += playerAnswer.getBall() + "볼 ";
-        }
-        if (playerAnswer.getStrike() > 0) {
-            hint += playerAnswer.getStrike() + "스트라이크";
-        }
-
-        return hint.equals("") ? "낫싱" : hint;
+    public void printAnswerHintMessage(String hint) {
+        System.out.println(hint);
     }
 
 }
+
