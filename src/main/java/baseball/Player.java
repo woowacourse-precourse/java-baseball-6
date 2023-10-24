21c0966a72022d16e4451bccf01d1006d3473241
BDlhj <akk808@naver.com>
2023-10-24 15:10:32 +0900

    feat: Implement convertStringToList() in src/main/java/baseball/Player.java

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,2 +1,10 @@
-package baseball;public class Player {
+package baseball;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Player {
+    public List<String> convertStringToList(String playerInput) {
+        return Arrays.asList(playerInput.split(""));
+    }
 }
