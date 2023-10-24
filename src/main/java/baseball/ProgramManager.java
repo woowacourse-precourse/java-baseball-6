08701a09c0e50c76c7769fadc874d62e323effab
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-10-24 18:32:44 +0900

    feat: ProgramManager 로직 개발
    1. 게임을 시작하는 startGame 메서드 추가
    Answer를 초기화하고 시작 문구 출력 후 proceedGame 호출
    
    2. 게임을 진행하는 proceedGame 메서드 추가
    사용자에게 입력값을 받은 후 Validator로 검증
    gradeAnswer로 답 채점
    답 채점 결과에 따라 while을 탈출하거나 결과 출력
    while을 탈출하면 다음 게임을 위한 nextGame 호출
    
    3. 다음 게임을 진행하는 nextGame 메서드  추가
    사용자의 입력값에 따라 다음 게임을 진행하거나 게임 종료

--- src/main/java/baseball/ProgramManager.java
+++ src/main/java/baseball/ProgramManager.java
@@ -2,16 +2,45 @@ package baseball;
 
 public class ProgramManager {
 
-    private Reader reader;
-    private Writer writer;
+    private final String RESTART_INPUT = "1";
     private Answer answer;
-    private Validator validator;
+
 
     public ProgramManager() {
-        reader = new Reader();
-        writer = new Writer();
-        answer = new Answer();
-        validator = new Validator();
     }
 
+    public void startGame() {
+        answer = new Answer();
+        Writer.writeStartMent();
+        proceedGame();
+    }
+
+    public void proceedGame() {
+        while (true) {
+
+            Writer.writeInputMent();
+            String answerInput = Reader.readInput();
+            Validator.validateAnswerInput(answerInput);
+
+            GradeResult gradeResult = answer.gradeAnswer(answerInput);
+
+            if (gradeResult.isAnswer()) {
+                Writer.writeCorrectMent();
+                break;
+            }
+
+            Writer.writeGradeResult(gradeResult);
+        }
+
+        Writer.writeRestartMent();
+        String endInput = Reader.readInput();
+        Validator.validateRestartInput(endInput);
+        nextGame(endInput);
+    }
+
+    public void nextGame(String endInput) {
+        if (endInput.equals(RESTART_INPUT)) {
+            startGame();
+        }
+    }
 }
