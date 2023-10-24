a7654706bdfdb5c20f863c7ddb24772629ae0e44
dustjq1004 <dustjq1005@naver.com>
2023-10-24 16:08:30 +0900

    yeonsup/feat : Process 클래스 추가 - createRandomBalls 구현 - isCorrectBall 구현

---
+++ src/main/java/baseball/Process.java
@@ -0,0 +1,39 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Process {
+
+    private List<Hint> hintLog;
+    private Ball ball;
+    private int size;
+
+    public Process(int size) {
+        this.size = size;
+        createRandomBalls(size);
+    }
+
+    public void createRandomBalls(int size) {
+        List<Integer> randomBalls = new ArrayList<>();
+        while (randomBalls.size() < size) {
+            int number = Randoms.pickNumberInRange(1, 9);
+            if (!randomBalls.contains(number)) {
+                randomBalls.add(number);
+            }
+        }
+
+        ball = new Ball(randomBalls);
+    }
+
+    public boolean isCorrectBall(String userBalls) {
+        Hint currentHint = ball.makeHint(userBalls);
+        hintLog.add(currentHint);
+
+        if (currentHint.isStrike(size)) {
+            return false;
+        }
+        return true;
+    }
+}
