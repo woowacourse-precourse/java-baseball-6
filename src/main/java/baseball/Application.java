8aa97ff60f187ae8dc9a0cf429ad86b6d07fa2d0
Geon <quso12358@naver.com>
2023-10-24 21:46:09 +0900

    :sparkles: Feat: Add Matching Feature
    - isAnswer 함수 추가
    - 사용자 입력 및 숫자 매칭 반복 로직 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -8,7 +9,7 @@ import java.util.List;
 public class Application {
     private static final String WELCOME_MSG = "숫자 야구 게임을 시작합니다.";
     private static final String INPUT_MSG = "숫자를 입력해주세요 : ";
-    private static final String BALL_MSG = "볼";
+    private static final String BALL_MSG = "볼 ";
     private static final String STRIKE_MSG = "스트라이크";
     private static final String NOTHING_MSG = "낫싱";
     private static final String END_MSG = "3스트라이크\n" +
@@ -25,6 +26,58 @@ public class Application {
         // 랜덤 3자리 숫자 생성
         List<Integer> randNum = Randoms.pickUniqueNumbersInRange(START_NUM, END_NUM, COUNT);
 
+        while(true){
+            // 사용자 입력 받기
+            print(INPUT_MSG);
+            String userInput = Console.readLine();
+
+            // 결과 판정 후 루프 탈출 결정
+            if(isAnswer(randNum, userInput)) break;
+        }
+
+        // Scanner 종료
+        Console.close();
+    }
+
+    private static boolean isAnswer(List<Integer> randNum, String userInput){
+        int strike = 0;
+        int ball = 0;
+
+        int length = userInput.length();
+
+        // 판정 로직
+        for(int idx=0; idx<length; idx++){
+            int num = userInput.charAt(idx) - '0';
+            int matchingIdx = randNum.indexOf(num);
+            // 숫자가 없는 경우: 볼이나 스트라이크가 아님
+            if(matchingIdx < 0) continue;
+
+            // 숫자와 인덱스가 일치: 스트라이크
+            if(matchingIdx == idx){
+                strike++;
+            }else{  // 인덱스가 불일치: 볼
+                ball++;
+            }
+        }
+
+        // 판정 결과
+        if(strike == 3){ // 정답인 경우
+            println(END_MSG);
+            return true;
+        }
+
+        if(strike + ball == 0){ // 하나도 못 맞춘 경우
+            println(NOTHING_MSG);
+        }else{ // 부분 정답인 경우
+            print((ball == 0) ? "" : ball + BALL_MSG);
+            println((strike == 0) ? "" : strike + STRIKE_MSG);
+        }
+
+        return false;
+    }
+
+    private static void print(String msg){
+        System.out.print(msg);
     }
 
     private static void println(String msg){
