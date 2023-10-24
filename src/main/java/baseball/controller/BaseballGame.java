d3309b7573751f2345f87583d67fbab9138aa28c
Lee Yeon Su <yslee0540@gmail.com>
2023-10-25 01:32:44 +0900

    feat(BaseballGame): 정답 맞췄을 경우 게임 종료하는 기능 구현

--- src/main/java/baseball/controller/BaseballGame.java
+++ src/main/java/baseball/controller/BaseballGame.java
@@ -10,52 +10,56 @@ import java.util.List;
 
 public class BaseballGame {
     private static final int BASEBALL_GAME_NUMBER_LENGTH = 3;
-    private int strikeCount;
-    private int ballCount;
 
     InputView inputView = new InputView();
     GenerateRandomNumbers generateRandomNumbers = new GenerateRandomNumbers();
     NumberValidator numberValidator = new NumberValidator();
+    GameNumber gameNumber = new GameNumber();
 
     public void startGame() {
-        GameNumber gameNumber = new GameNumber();
         List<Integer> getRandomNumbers = generateRandomNumbers.getRandomNumbers();
         gameNumber.setComputerNumbers(getRandomNumbers);
         System.out.println(getRandomNumbers);
+        oneGame();
+    }
 
-        String inputNumber = inputView.readGameNumber();
-        gameNumber.setPlayerNumbers(numberValidator.toValidateData(inputNumber));
-
-        List<Integer> playerNumbers = gameNumber.getPlayerNumbers();
-        List<Integer> computerNumbers = gameNumber.getComputerNumbers();
-
+    public void oneGame() {
         GameResult result;
-        result = compareNumbers(playerNumbers, computerNumbers);
-        OutputView.printResult(result);
+        do {
+            String inputNumber = inputView.readGameNumber();
+            gameNumber.setPlayerNumbers(numberValidator.toValidateData(inputNumber));
+            List<Integer> playerNumbers = gameNumber.getPlayerNumbers();
+            List<Integer> computerNumbers = gameNumber.getComputerNumbers();
+            result = compareNumbers(playerNumbers, computerNumbers);
+            OutputView.printResult(result);
+        } while (!result.isSuccess());
+        OutputView.printSuccessMessage();
     }
 
     public GameResult compareNumbers(List<Integer> playerNumbers, List<Integer> computerNumbers) {
-        for (int i = 0; i < BASEBALL_GAME_NUMBER_LENGTH; i++) {
-            countStrike(computerNumbers.get(i), playerNumbers.get(i));
-            countBall(computerNumbers, playerNumbers.get(i), i);
-        }
+        int ballCount = countBall(playerNumbers, computerNumbers);
+        int strikeCount = countStrike(playerNumbers, computerNumbers);
         return new GameResult(ballCount, strikeCount);
     }
 
-    private void countStrike(int computerNumber, int playerNumber) {
-        if (computerNumber == playerNumber) {
-            strikeCount++;
+    private int countStrike(List<Integer> playerNumbers, List<Integer> computerNumbers) {
+        int strikeCount = 0;
+        for (int i = 0; i < BASEBALL_GAME_NUMBER_LENGTH; i++) {
+            if (computerNumbers.get(i) == playerNumbers.get(i)) {
+                strikeCount++;
+            }
         }
+        return strikeCount;
     }
 
-    private void countBall(List<Integer> computerNumbers, int playerNumber, int numberIndex) {
-        if (computerNumbers.get(numberIndex) != playerNumber && computerNumbers.contains(playerNumber)) {
-            ballCount++;
+    private int countBall(List<Integer> playerNumbers, List<Integer> computerNumbers) {
+        int ballCount = 0;
+        for (int i = 0; i < BASEBALL_GAME_NUMBER_LENGTH; i++) {
+            if (computerNumbers.get(i) != playerNumbers.get(i) && computerNumbers.contains(playerNumbers.get(i))) {
+                ballCount++;
+            }
         }
-    }
-
-    public boolean isThreeStrike() {
-        return strikeCount == 3;
+        return ballCount;
     }
 
 }
