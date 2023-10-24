d7bf753fcb86fb7dd40d6111c6576ce5eb3b2b03
jaegyeongkim <dongji_11@naver.com>
2023-10-24 13:03:06 +0900

    feat(Balls): Ball, Strike 판단을 위해 Balls 객체들을 비교하는 기능 추가

--- src/main/java/balls/Balls.java
+++ src/main/java/balls/Balls.java
@@ -14,4 +14,28 @@ public class Balls {
         utils.userGameStartValidation(input);
         this.balls= utils.stringToIntList(input);
     }
+    public int getBallsSize(){
+        return balls.size();
+    }
+    public int getBalls(int index){
+        return balls.get(index);
+    }
+    public boolean compareToByBall(Balls anotherBalls, int digit){
+        for(int index=0; index< getBallsSize(); index++){
+            if(digit==index){
+                continue;
+            }
+            if(anotherBalls.getBalls(index)==getBalls(digit)){
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean compareToByStrike(Balls anotherBalls, int digit){
+        if(anotherBalls.getBalls(digit)==getBalls(digit)){
+            return true;
+        }
+        return false;
+    }
+
 }
