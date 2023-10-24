13b83a2ba1cf1b5530f343317e01bc291f4e6f5b
jangseop <kgh010529@naver.com>
2023-10-25 03:37:36 +0900

    feat: Application에 숫자게임 시작 기능 구현
    User를 통해 야구게임 시작

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
 
+import baseball.user.ConsoleUser;
+import baseball.user.User;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        User user = new ConsoleUser();
+        user.doGame("start", 0);
     }
 }
