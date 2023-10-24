74ef7b62320dc00920445089d569add51b18cfa7
YuJeongYun <yunyj99@naver.com>
2023-10-24 21:55:01 +0900

    feat: 콘솔 출력 기능 추가

---
+++ src/main/java/baseball/baseballGame/PrintConsole.java
@@ -0,0 +1,33 @@
+package baseball.baseballGame;
+
+import camp.nextstep.edu.missionutils.Console;
+
+/**
+ * 콘솔에 출력하는 함수
+ */
+public class PrintConsole {
+
+    public void printStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public int printInput() {
+        System.out.println("숫자를 입력해주세요 : ");
+
+        return Integer.parseInt(Console.readLine());
+    }
+
+    public void printOutput() {
+        System.out.println("낫싱");
+    }
+
+    public void printOutput(int strike, int ball) {
+        System.out.println(ball + "볼 " + strike + "스트라이크");
+    }
+
+    public void printEnd() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+}
+
