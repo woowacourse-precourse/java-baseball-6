5c0b36db4238e7bbd43e98975dfd35ffefc49ae7
moong2 <www.castlehi@gmail.com>
2023-10-25 02:05:36 +0900

    test: [#10] wrap Result's parameter with Status record

--- src/test/java/baseball/util/ResultTest.java
+++ src/test/java/baseball/util/ResultTest.java
@@ -22,7 +22,7 @@ public class ResultTest {
     @DisplayName("범위밖이라면_IllegalArgumentException을 반환한다.")
     public void 범위밖이라면_IllegalArgumentException을_반환한다() {
         // given
-        Result result = new Result(-1, 4);
+        Result result = new Result(new Status(-1), new Status(4));
 
         // when & then
         assertThrows(IllegalArgumentException.class, () -> result.checkValidate());
@@ -32,7 +32,7 @@ public class ResultTest {
     @DisplayName("낫싱일 경우 true를 반환한다.")
     public void 낫싱일_경우_true를_반환한다() {
         // given
-        Result result = new Result(0, 0);
+        Result result = new Result(new Status(0), new Status(0));
 
         // when
         boolean isNothing = result.checkNothing();
@@ -45,7 +45,7 @@ public class ResultTest {
     @DisplayName("낫싱이 아닐 경우 false를 반환한다.")
     public void 낫싱이_아닐_경우_false를_반환한다() {
         // given
-        Result result = new Result(0, 1);
+        Result result = new Result(new Status(0), new Status(1));
 
         // when
         boolean isNothing = result.checkNothing();
@@ -58,7 +58,7 @@ public class ResultTest {
     @DisplayName("3스트라이크일 경우 3스트라이크를 반환한다.")
     public void 스트라이크3일_경우_3스트라이크를_반환한다() {
         // given
-        Result result = new Result(0, 3);
+        Result result = new Result(new Status(0), new Status(3));
 
         // when
         result.printResult();
@@ -72,7 +72,7 @@ public class ResultTest {
     @DisplayName("1볼 2스트라이크일 경우 1볼 2스트라이크를 반환한다.")
     public void 볼1_스트라이크2일_경우_1볼_2스트라이크를_반환한다() {
         // given
-        Result result = new Result(1, 2);
+        Result result = new Result(new Status(1), new Status(2));
 
         // when
         result.printResult();
@@ -86,7 +86,7 @@ public class ResultTest {
     @DisplayName("3볼일 경우 3볼을 반환한다.")
     public void 볼3일_경우_3볼을_반환한다() {
         // given
-        Result result = new Result(3, 0);
+        Result result = new Result(new Status(3), new Status(0));
 
         // when
         result.printResult();
