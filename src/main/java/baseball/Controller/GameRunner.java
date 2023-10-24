1965aec96d1e3ae7fa59000272e1cc889bb54a39
Beomsu <gobeumsu@gmail.com>
2023-10-25 00:05:30 +0900

    feat(GameRunner): 사용자 입력 받기와 결과 출력 로직 추가
    ## GameRunner 클래스에 사용자로부터 숫자 입력을 받는 getUserInput 메서드를 추가
    
    - 입력 유효성 검사를 수행합니다.
    - 유효한 입력을 정수 리스트로 변환합니다.
    ## 스트라이크와 볼의 결과를 출력하는 printResult 메서드를 추가
    - '낫싱', 볼만, 스트라이크만, 또는 볼과 스트라이크 모두 있을 경우에 따라 다른 메시지를 출력합니다.

--- src/main/java/baseball/Controller/GameRunner.java
+++ src/main/java/baseball/Controller/GameRunner.java
@@ -7,8 +7,35 @@ import baseball.utils.Utils;
 import java.util.List;
 
 public class GameRunner {
+
     private final Baseball baseball;
+
     public GameRunner(Baseball baseball) {
         this.baseball = baseball;
+    }
+
+    private List<Integer> getUserInput() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String userInput = readLine();
+        Utils.validateUserInput(userInput);
+        return Utils.stringToIntegerList(userInput);
+    }
+
+    private void printResult(int strikes, int balls) {
+        if (strikes == 0 && balls == 0) {
+            System.out.println("낫싱");
+            return;
+        }
+        if (strikes > 0 && balls > 0) {
+            System.out.printf("%d볼 %d스트라이크\n", balls, strikes);
+            return;
+        }
+        if (strikes > 0) {
+            System.out.printf("%d스트라이크\n", strikes);
+            return;
+        }
+        if (balls > 0) {
+            System.out.printf("%d볼\n", balls);
+        }
     }
 }
