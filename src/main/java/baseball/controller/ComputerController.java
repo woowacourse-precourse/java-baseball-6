178ca63b391f73bbdab9a4d76da36647c8fe2319
JongMinCh0i <wonderland314592@gmail.com>
2023-10-24 16:47:50 +0900

    refactoring(ComputerController): 매직 넘버 제거

--- src/main/java/baseball/controller/ComputerController.java
+++ src/main/java/baseball/controller/ComputerController.java
@@ -4,9 +4,11 @@ import baseball.config.DiContainer;
 
 public class ComputerController implements Controller {
 
+    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+
     @Override
     public void gameStart() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println(GAME_START_MESSAGE);
         new DiContainer().userController().gameStart();
     }
 }
