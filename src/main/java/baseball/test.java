9dea44982379d903abbba3250ce218d17aa281c1
ksngh <chzhqk98@naver.com>
2023-10-24 22:27:51 +0900

    yet

--- src/main/java/baseball/test.java
+++ src/main/java/baseball/test.java
@@ -1,7 +1,7 @@
 package baseball;
 import java.util.Scanner;
 
-public class Application {
+public class test {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     	int s= 0 ; int b = 0 ;
