3565fa1285d4cea7259f752a550adc6df3259881
세탁기 <116627038+saetakki@users.noreply.github.com>
2023-10-24 18:05:16 +0900

    fix: 게임 로직 수정 1. 추가 및 수정된 타 클래스, 메서드를 import 하는 부분 수정 2. 3스트라이크 도달 시 while문 멈추게 수정

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,19 +1,44 @@
 package baseball.controller;
 
-public class BaseballGame {
 
-    public void
+import baseball.model.Game;
+import baseball.model.Validator;
+import java.util.List;
+import java.util.Scanner;
 
 
-    public void baseballGameLogic() {
-        do {
-            //유저로부터 결과를 받고
-            //컴퓨터의 값과 비교하여
-            //점수를 매기고
-            //힌트를 제공하고
-            //종료 조건을 달성하면 종료한다
-        } while (!endCondition);
+public class GameController {
+    private final Validator validator;
+    private final Game game;
+
+    public GameController() {
+        this.validator = new Validator();
+        this.game = new Game();
+
     }
 
+    public void run() {
+        boolean isContinue = true;
+        Scanner sc = new Scanner(System.in);
+        Validator validator = new Validator();
+        List<Integer> computer = game.generateComputersNumber();
+
+        while (isContinue) {
+            try {
+                String userInput = sc.nextLine();
+                validator.validateUserInput(userInput);
+                List<Integer> userGuess = validator.splitNumbersByDigits(Integer.parseInt(userInput));
+                List<Integer> result = game.calculateResult(userGuess, computer);
+
+                System.out.println("Result: " + result + ", User Guess: " + userGuess + ", Computer: " + computer);
+
+                if (result.get(1) == 3) {
+                    isContinue = !isContinue;
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println(e);
+            }
+        }
+    }
 
 }
