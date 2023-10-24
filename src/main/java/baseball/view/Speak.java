7c865210808b290839361be12e88545a8152f7fa
jongchan0109 <jongchan1009@naver.com>
2023-10-24 14:34:39 +0900

    feat: 사용되는 말을 상수로 변환한 클래스 구현

---
+++ src/main/java/baseball/view/Speak.java
@@ -0,0 +1,12 @@
+package baseball.view;
+
+class Speak{
+    final static String inputNumberSpeak = "숫자를 입력해주세요 : ";
+    final static String startGameSpeak = "숫자 야구 게임을 시작합니다.";
+    final static String startOrEndSpeak = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    final static String gameEndSpeak = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    final static String notThingSpeak = "낫싱";
+    final static String ballSpeak = "볼";
+    final static String strikeSpeak = "스트라이크";
+    final static String inputIsUnvalid = "잘못된 값을 입력하였습니다.";
+}
