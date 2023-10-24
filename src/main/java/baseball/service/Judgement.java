d9bc5af16dcce1ef447ebbaac612e54352d84fb9
june-777 <wlwhswnsrl96@gmail.com>
2023-10-24 18:21:38 +0900

    refactor: 게임결과를 계산하는 기능은 GameResult에게 위임

--- src/main/java/baseball/service/Judgement.java
+++ src/main/java/baseball/service/Judgement.java
@@ -4,23 +4,9 @@ import baseball.service.domain.BaseballCollection;
 
 public class Judgement {
     // 기능: 힌트 얻기
-    public String calculateHint(BaseballCollection computerBalls, BaseballCollection playerBalls) {
+    public GameResult calculateHint(BaseballCollection computerBalls, BaseballCollection playerBalls) {
         int strikeCount = computerBalls.calculateStrikeCount(playerBalls);
         int ballCount = computerBalls.calculateBallCount(playerBalls);
-        return appendingResult(ballCount, strikeCount);
-    }
-
-    private String appendingResult(int ballCount, int strikeCount) {
-        StringBuilder result = new StringBuilder();
-        if (ballCount == 0 && strikeCount == 0) {
-            return result.append("낫싱").toString();
-        }
-        if (ballCount > 0) {
-            result.append(String.format("%d볼 ", ballCount));
-        }
-        if (strikeCount > 0) {
-            result.append(String.format("%d스트라이크", strikeCount));
-        }
-        return result.toString().trim();
+        return new GameResult(ballCount, strikeCount);
     }
 }
