c4e0e596b19f31d774c7a7d196338a53b25e8495
jammy102 <dnjscksv@naver.com>
2023-10-24 22:29:21 +0900

    우아한프리코스

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,8 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+
 
 public class Application {
     public static void main(String[] args) {
