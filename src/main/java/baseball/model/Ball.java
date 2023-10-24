bac654f5b88dfaa564ad2dbccd486a5b511bb4b1
becooq81 <becooq81@gmail.com>
2023-10-25 01:36:15 +0900

    feat(Ball): completed Ball class that represents each of the three balls computer/user has - variables: min/max value allowed for number (char/int), number to save value Ball is created for, error message saved - methods   - constructor for char input (for user input based Ball)   - constructor for int input (for randomly created Ball for computer)   - equals method to compare number values of two Ball objects   - validation methods for int/char values each to check whether value is within accepted scope

---
+++ src/main/java/baseball/model/Ball.java
@@ -0,0 +1,37 @@
+package baseball.model;
+
+public class Ball {
+    private static final char CHAR_MIN_NUMBER = '1';
+    private static final char CHAR_MAX_NUMBER = '9';
+    private static final int INT_MIN_NUMBER = 1;
+    private static final int INT_MAX_NUMBER = 9;
+    private static final String OUT_OF_RANGE_MESSAGE = "각 입력값은 " + INT_MIN_NUMBER + "과 "+ INT_MAX_NUMBER +" 사이의 숫자여야 합니다.";
+
+    private final Integer number;
+
+    public Ball(char val) {
+        this.number = validateIsNaturalNumber(val);
+    }
+
+    public Ball(int val) {
+        this.number = validateIsInRange(val);
+    }
+
+    public static boolean equals(Ball a, Ball b) {
+        return a.number == b.number;
+    }
+
+    private Integer validateIsNaturalNumber(char val) {
+        if (val < CHAR_MIN_NUMBER || val > CHAR_MAX_NUMBER) {
+            throw new IllegalArgumentException(OUT_OF_RANGE_MESSAGE);
+        }
+        return Character.getNumericValue(val);
+    }
+
+    private Integer validateIsInRange(int val) {
+        if (val < INT_MIN_NUMBER || val > INT_MAX_NUMBER) {
+            throw new IllegalArgumentException(OUT_OF_RANGE_MESSAGE);
+        }
+        return val;
+    }
+}
