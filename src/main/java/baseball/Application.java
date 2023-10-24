55973a2ccb75471df46ccad62b14bbd4eb6b6581
ljt528 <ljt528@naver.com>
2023-10-25 02:49:40 +0900

    4. 사용자 숫자와 컴퓨터 숫자 비교 후 결과(힌트) 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -19,6 +19,9 @@ public class Application {
                 computer.add(randomNumber);
             }
         }
+
+        int resetcoin = 1;
+        while (resetcoin < 2) {
         // 사용자 숫자 입력 받기
         System.out.print("숫자를 입력해주세요 : ");
         String userNumber = Console.readLine();
@@ -41,6 +44,35 @@ public class Application {
         // Null값 예외 처리
         if (user[0].equals(" ")) {
             throw new IllegalArgumentException("숫자를 입력해주세요.");
+        }
+            // 게임 시작
+            int ball = 0;
+            int strike = 0;
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                    if (j == k && computer.get(j) == Integer.valueOf(user[k])) {
+                        strike++;
+                        continue;
+                    } else if (j != k && computer.get(j) == Integer.valueOf(user[k])) {
+                        ball++;
+                        continue;
+                    }
+                }
+            }
+            if (strike == 0 && ball == 0) {
+                System.out.println("낫싱");
+            } else if (strike > 0 && ball > 0){
+                System.out.println(ball + "볼" + " " + strike + "스트라이크");
+            } else if (ball > 0) {
+                System.out.println(ball + "볼");
+            } else if (strike > 0) {
+                System.out.println(strike + "스트라이크");
+            }
+
+            if (strike == 3) {
+                resetcoin++;
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            }
         }
     }
 }
