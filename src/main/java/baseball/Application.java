dc0bd8c638e679c7ff2eafd957a350ad7df75c63
jiaeYoon <yoon529@sookmyung.ac.kr>
2023-10-24 16:11:21 +0900

    Feat: '사용자로부터 3자리 수 입력, 검증 기능' 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Application {
@@ -15,6 +16,9 @@ public class Application {
         while (status == 1) {
 
             String computer = makeRandomNums();
+
+            String user = enterNums();
+//            correct = check(computer, user);
 
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             status = Integer.parseInt(Console.readLine());
@@ -30,5 +34,36 @@ public class Application {
             }
         }
         return "" + computer.get(0) + computer.get(1) + computer.get(2);
+    }
+
+    private static String enterNums() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+
+        // 숫자 자릿수가 3이 아닌 경우
+        if (input.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+
+        // 범위 밖의 숫자나 문자를 입력한 경우
+        int[] nums = Arrays.stream(input.split("")).mapToInt(Integer::parseInt).toArray();
+        for (int i = 0; i < 3; i++) {
+            int num = nums[i];
+            if (!(num >= 1 && num <= 9)) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        //중복된 수를 입력한 경우
+        List<Integer> dup_check = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            if (!dup_check.contains(nums[i])) {
+                dup_check.add(nums[i]);
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        return input;
     }
 }
