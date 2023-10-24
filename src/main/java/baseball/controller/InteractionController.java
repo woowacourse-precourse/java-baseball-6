4eebaa20fdf9eec8041af3bd3f48f73940bf3e14
sunwoong <sunwoong1122@gmail.com>
2023-10-24 20:17:33 +0900

    feat: 사용자 텍스트 입력 로직 구현

---
+++ src/main/java/baseball/controller/InteractionController.java
@@ -0,0 +1,11 @@
+package baseball.controller;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InteractionController {
+    public String readInputData() {
+        String inputData = Console.readLine();
+        // TODO input data validate logic
+        return inputData;
+    }
+}
