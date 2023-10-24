b710cc93bc58e9b50e8e74ac304dcc504f663226
leorb159 <ldgsocial159@naver.com>
2023-10-25 02:56:21 +0900

    feat(Output): 게임 메시지 출력 기능 구현

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,28 @@
+package baseball;
+
+public class BaseballGame {
+
+    public static void run(){
+        //야구 게임 시작 메시지 출력
+        while (true){
+            //게임 시작
+            gameStart();
+            //종료선택시 프로그램 종료
+            if(){
+                break;
+            }
+        }
+        return;
+
+    }
+    public static void gameStart(){
+        //컴퓨터 숫자 생성
+        while (true){
+            //숫자입력 메시지 출력
+            //입력에 대한 결과 출력
+            //맞췄을 시 종료
+        }
+        
+
+    }
+}
