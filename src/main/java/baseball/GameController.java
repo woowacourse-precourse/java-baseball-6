69bd6c0da89eeb8e80a6f6e419ab245866ef0cbb
EveKristinLee <jylisa0612@naver.com>
2023-10-24 17:56:24 +0900

    feat : 게임 시작시 컴퓨터 숫자 생성

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -1,8 +1,12 @@
 package baseball;
 
+import java.util.List;
+
 public class GameController {
 
     private static GameController gameController = new GameController();
+    private GameView view = GameView.getInstance();
+    private GameModel model = GameModel.getInstance();
 
     public GameController() {
     }
@@ -12,9 +16,11 @@ public class GameController {
     }
 
     public void startGame() {
+        view.startGame();
         boolean choice = true; //true : 1, false : 2
 
         do {
+            List<Integer> computer = model.generateComputerNums();
             // TODO: 게임 플레이
             // TODO: 추가 플레이 여부 확인
         } while (choice);
