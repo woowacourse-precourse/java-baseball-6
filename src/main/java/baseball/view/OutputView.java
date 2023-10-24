e8be60db6281fa4f4e9bfefecf01b474d2eca628
Woo-Shinyoung <tlsdud8205@naver.com>
2023-10-24 22:31:27 +0900

    Feat[view] 게임 시작 문구 생성

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,4 +1,8 @@
 package baseball.view;
 
+import baseball.domain.BaseballResult;
+
 public class OutputView {
-}
+    public void printGameStart(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
