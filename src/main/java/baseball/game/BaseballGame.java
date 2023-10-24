097014b99a6a95f60fe2366b53204f00e35309f9
Daseul <daseull.rep@gmail.com>
2023-10-24 14:09:02 +0900

    refactor : simplify BaseballGame
    뷰 분리

--- src/main/java/baseball/game/BaseballGame.java
+++ src/main/java/baseball/game/BaseballGame.java
@@ -10,13 +10,6 @@ public class BaseballGame {
 
     private static final int NUM_DIGIT = 3;
 
-    private static final String OPENING_LINE = "숫자 야구 게임을 시작합니다.";
-    private static final String QUESTION_LINE = "숫자를 입력해주세요 : ";
-    private static final String CLOSING_LINE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    private static final String REPLAY_LINE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-    private static final String BALL = "볼 ";
-    private static final String STRIKE = "스트라이크";
-    private static final String NOTHING = "낫싱";
     private List<Integer> computer;
     private List<Integer> user;
 
@@ -53,20 +46,6 @@ public class BaseballGame {
             }
         }
         return cnt;
-    }
-
-    public void printHint(int ball, int strike) {
-        if (ball == 0 && strike == 0) {
-            System.out.println(NOTHING);
-            return;
-        }
-        if (ball > 0) {
-            System.out.print(ball + BALL);
-        }
-        if (strike > 0) {
-            System.out.print(strike + STRIKE);
-        }
-        System.out.println();
     }
 
     protected void setComputer(String computer) {
