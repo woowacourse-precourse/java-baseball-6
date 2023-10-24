669f2c47a8123d6ff52a915793e13f60b6a86dca
oxdjww <oxdjww@gmail.com>
2023-10-24 21:53:49 +0900

    refactor : 메서드에서 직접 객체를 다루지 않고 생성자에서 List<integer>를 추출하는 방식으로 변환

--- src/main/java/baseball/model/BaseballGameCounts.java
+++ src/main/java/baseball/model/BaseballGameCounts.java
@@ -13,36 +13,39 @@ public class BaseballGameCounts {
     private final List<Integer> counts = Arrays.asList(0, 0);
 
     public BaseballGameCounts(ComputerNumber computer, UserNumber user) {
-        this.checkStrikes(computer, user);
-        this.checkBalls(computer, user);
+        // 일급 컬렉션 객체에서 컬렉션 추출
+        List<Integer> computerNum = computer.getComputerNumber();
+        List<Integer> userNum = user.getUserNumber();
+        this.checkStrikes(computerNum, userNum);
+        this.checkBalls(computerNum, userNum);
     }
 
     // 입력받은 유저 번호에 따라 스트라이크를 체크하는 함수
-    private void checkStrikes(ComputerNumber computer, UserNumber user) {
-        for (int idx = 0; idx < user.getUserNumber().size(); idx++) {
-            addStrikeCounts(computer, user, idx);
+    private void checkStrikes(List<Integer> computerNum, List<Integer> userNum) {
+        for (int idx = 0; idx <userNum.size(); idx++) {
+            addStrikeCounts(computerNum, userNum, idx);
         }
     }
 
-    private void addStrikeCounts(ComputerNumber computer, UserNumber user, Integer idx) {
-        if (user.getUserNumber().get(idx).equals(computer.getComputerNumber().get(idx))) {
+    private void addStrikeCounts(List<Integer> computerNum, List<Integer> userNum, Integer idx) {
+        if (userNum.get(idx).equals(computerNum.get(idx))) {
             counts.set(STRIKE, counts.get(STRIKE) + 1);
         }
     }
 
     // 입력받은 유저 번호에 따라 볼을 체크하는 함수
-    private void checkBalls(ComputerNumber computer, UserNumber user) {
+    private void checkBalls(List<Integer> computerNum, List<Integer> userNum) {
         Integer ballCounts = 0;
-        for (Integer userNumberDigit : user.getUserNumber()) {
-            ballCounts = addBallCounts(computer, userNumberDigit, ballCounts);
+        for (Integer userNumberDigit : userNum) {
+            ballCounts = addBallCounts(computerNum, userNumberDigit, ballCounts);
         }
 
         ballCounts -= counts.get(STRIKE);
         counts.set(BALL, ballCounts);
     }
 
-    private Integer addBallCounts(ComputerNumber computer, Integer userNumberDigit, Integer ballCounts) {
-        if (computer.getComputerNumber().contains(userNumberDigit)) {
+    private Integer addBallCounts(List<Integer> computerNum, Integer userNumberDigit, Integer ballCounts) {
+        if (computerNum.contains(userNumberDigit)) {
             ballCounts++;
         }
         return ballCounts;
---
+++ src/test/java/baseball/model/BaseballGameCountsTest.java
@@ -0,0 +1,18 @@
+package baseball.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BaseballGameCountsTest {
+    @Test
+    public void 스트라이크_테스트() throws Exception{
+        // given
+
+        // when
+        
+        // then
+        
+    }
+
+}
