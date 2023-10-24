e78e7593caf92dddea526e7351c9e27b11102ac3
june-777 <wlwhswnsrl96@gmail.com>
2023-10-24 21:12:25 +0900

    chore: 상수값 수정

--- src/main/java/baseball/service/domain/BaseballConstants.java
+++ src/main/java/baseball/service/domain/BaseballConstants.java
@@ -2,7 +2,7 @@ package baseball.service.domain;
 
 public class BaseballConstants {
     public static int BASEBALL_START_NUMBER = 1;
-    public static int BASEBALL_END_NUMBER = 5;
+    public static int BASEBALL_END_NUMBER = 9;
     public static int DEFAULT_CAPACITY = 3;
 
     private BaseballConstants() {
