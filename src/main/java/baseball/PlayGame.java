e78c01cbc59ac1acb917ceea9340440e551dfb8d
hiidy <hiidy115@gmail.com>
2023-10-24 16:14:08 +0900

    :sparkles: feat: 게임을 다시 시작할지 입력값을 받는 기능 추가 구현 1일 경우 startGame()을 다시 실행하고 2일 경우 프로그램 종료.

--- src/main/java/baseball/PlayGame.java
+++ src/main/java/baseball/PlayGame.java
@@ -11,7 +11,7 @@ public class PlayGame {
 
     public void run() {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        while (strike != 3) {
+        while (this.strike != 3) {
             startGame();
         }
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
@@ -28,9 +28,17 @@ public class PlayGame {
         printScore();
     }
 
-    private void retryGame() {
+    public boolean retryGame() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String retryInput = Console.readLine();
+
+        if (retryInput.equals("1")) {
+            return true;
+        }
+        if (retryInput.equals("2")) {
+            return false;
+        }
+        throw new IllegalArgumentException("입력값을 1 또는 2만 입력하세요.");
     }
 
     public ArrayList<Integer> inputToArrayList(String numInput) {
@@ -90,6 +98,7 @@ public class PlayGame {
         }
         if (this.strike > 0 && this.ball > 0) {
             System.out.printf("%d볼 %d스트라이크", this.ball, this.strike);
+            System.out.println("\n");
             return;
         }
         if (this.strike > 0) {
