4e04b8b614fca41e29671221130d3e28db7ef7cb
Big-Cir97 <eodnjs01477@gmail.com>
2023-10-24 18:10:44 +0900

    feat: inputView, 입력을 담당하는 Class 추가

---
+++ src/main/java/baseball/InputView.java
@@ -0,0 +1,30 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private Validator validator = new Validator();
+    public String inputNumbers() {
+        String readNumbers = Console.readLine();
+        checkNumbersValidation(readNumbers);
+        return readNumbers;
+    }
+
+    public String inputRestartOrEndGame() {
+        String readNumber = Console.readLine();
+        checkNumberValidation(readNumber);
+        return readNumber;
+    }
+
+    private void checkNumberValidation(String readNumber) {
+        validator.validateInputNumberOfRestartAndEnd(readNumber);
+    }
+
+    private void checkNumbersValidation(String readNumbers) {
+        validator.validateInputNumbersSize(readNumbers);
+        validator.validateInputNumbersDuplicated(readNumbers);
+        validator.validateInputNumberIsNumeric(readNumbers);
+    }
+
+}
