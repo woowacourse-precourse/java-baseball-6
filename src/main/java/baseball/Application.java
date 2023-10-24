e628479b85bf822212da3ccfa69fe2eed264d4ca
bboodd <92453279+bboodd@users.noreply.github.com>
2023-10-24 23:16:47 +0900

    feat: 메인함수 게임시작 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
 public class Application {
+//    private static BaseballGame baseballGame;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BaseballGame baseballGame = new BaseballGame();
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        baseballGame.start();
+
+
     }
 }
