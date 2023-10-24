fa3841aa90d534e9cecb8bdaa0e85e41be9ea154
sengjun0624 <sengjun0624@naver.com>
2023-10-25 03:16:18 +0900

    feat(getParseInt): add parsing method string to int[]

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -28,7 +28,12 @@ public class GameService {
 
     private int[] getParseInt(String input, int size) throws IllegalArgumentException {
         int[] parseInt = new int[size];
-
+        for (int i = 0; i < input.length(); i++) {
+            if (!('0' <= input.charAt(i) && input.charAt(i) <= '9')) {
+                throw new IllegalArgumentException();
+            }
+            parseInt[i] = input.charAt(i) - '0';
+        }
         return parseInt;
     }
 }
