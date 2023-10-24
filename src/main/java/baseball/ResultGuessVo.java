5feace128b89f8fac0c33e8f8b454e0c2f0df82f
youjae <youjae0803@gmail.com>
2023-10-25 00:04:17 +0900

    feat: add construct using 2 int parameter

--- src/main/java/baseball/ResultGuessVo.java
+++ src/main/java/baseball/ResultGuessVo.java
@@ -3,6 +3,11 @@ public class ResultGuessVo {
     private int cntBall;
     private int cntStrike;
 
+    public ResultGuessVo(int cntBall, int cntStrike) {
+        this.cntBall = cntBall;
+        this.cntStrike = cntStrike;
+    }
+
     public int getCntBall() {
         return cntBall;
     }
