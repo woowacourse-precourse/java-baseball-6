ba43ad284951455a9eb472bcfa2e124898dd668a
heiler <threepebbles@naver.com>
2023-10-25 07:46:14 +0900

    feat: InputController에 사용자 입력에 대한 검증 기능 추가

--- src/main/java/baseball/controller/BaseballGameController.java
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -3,9 +3,12 @@ package baseball.controller;
 import baseball.model.Computer;
 import baseball.model.GameStatus;
 import baseball.model.NumberList;
+import baseball.validator.Validator;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.HashSet;
+import java.util.Set;
 
 public class BaseballGameController extends GameController {
     private final Computer computer;
@@ -49,12 +52,43 @@ public class BaseballGameController extends GameController {
     private static class InputController {
         public static String scanGuess() {
             InputView.printEnterGuess();
-            return Console.readLine();
+            String userInput = Console.readLine();
+            validateGuess(userInput);
+            return userInput;
         }
 
         public static String scanStatus() {
             InputView.printEnterStatus();
-            return Console.readLine();
+            String userInput = Console.readLine();
+            validateStatus(userInput);
+            return userInput;
+        }
+
+        private static void validateGuess(String guess) {
+            Validator.validateNull(guess);
+            Validator.validateSize(guess, 3);
+            validateIsInRange(guess, 1, 9);
+            validateRepeatedNumber(guess);
+        }
+
+        private static void validateIsInRange(String guess, int startInclusive, int endInclusive) {
+            for (int i = 0; i < guess.length(); i++) {
+                Validator.validateInRange(guess.charAt(i) - '0', startInclusive, endInclusive);
+            }
+        }
+
+        private static void validateRepeatedNumber(String guess) {
+            Set<Character> set = new HashSet<>();
+            for (int i = 0; i < guess.length(); i++) {
+                set.add(guess.charAt(i));
+            }
+            if (set.size() != guess.length()) {
+                throw new IllegalArgumentException("중복된 원소가 있습니다.");
+            }
+        }
+
+        private static void validateStatus(String status) {
+            validateIsInRange(status, 1, 2);
         }
     }
 }
