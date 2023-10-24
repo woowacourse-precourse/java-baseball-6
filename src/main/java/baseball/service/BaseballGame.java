66795b5adeaefd0701b9d3795a4cd0be3c183380
jhsong76 <20190976@sungshin.ac.kr>
2023-10-24 16:13:14 +0900

    refactor: 메서드 이름 변경

--- src/main/java/baseball/service/BaseballGame.java
+++ src/main/java/baseball/service/BaseballGame.java
@@ -23,7 +23,7 @@ public class BaseballGame {
             displayResult(result);
             if (result[0] == NUMBER_LENGTH) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                if (Restart()) {
+                if (restartGame()) {
                     generateComputerNumbers();
                 } else {
                     break;
@@ -61,7 +61,7 @@ public class BaseballGame {
         System.out.println();
     }
 
-    private boolean Restart() {
+    private boolean restartGame() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요: ");
         String input = Console.readLine();
         return input.equals("1");
