b838bf0fd2c03db42056dfb5598dc37f021ac98d
akskflwn <akskflwn@gmail.com>
2023-10-24 16:41:18 +0900

    feat: 게임 진행을 위한 클레스 구현
    ApplicationTest 통과 분기점

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -1,9 +1,36 @@
 package baseball;
 
 import baseball.view.InputView;
+import baseball.view.OutputView;
 import java.util.List;
 
 public class GameController {
+
+    public void run() {
+        OutputView.printStartMessage();
+        playGame();
+    }
+
+    private void playGame() {
+        playRounds(createAnswerBalls());
+        endGame();
+    }
+
+    private void playRounds(Balls answer) {
+        Balls playerBalls = getPlayerBalls();
+        RoundResult roundResult = playerBalls.compareWith(answer);
+        OutputView.print(roundResult);
+        if (roundResult.isGameEnd()) {
+            return;
+        }
+        playRounds(answer);
+    }
+
+    private void endGame() {
+        if (InputView.retryOrExit()) {
+            playGame();
+        }
+    }
 
     private Balls createAnswerBalls() {
         List<Integer> numbers = RandomNumberGenerator.generate();
