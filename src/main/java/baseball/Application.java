1fd3e74d20ed04477618cd9c3e937dbb614f5ffd
jaegyeongkim <dongji_11@naver.com>
2023-10-24 10:10:19 +0900

    feat(Application): 프로그램 실행,종료 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
 public class Application {
+    public final static int NUMBER_LENGTH = 3;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BaseballGame baseballGame = new BaseballGame();
+        boolean run= true;
+        while(run){
+            if(baseballGame.gameStart()){
+                run= baseballGame.restartOrEnd();
+            }
+        }
     }
 }
