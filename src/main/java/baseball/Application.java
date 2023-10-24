5b442458855ffa095bf1a9e9fd4d87586e6ca96c
chech2 <chaelim3396@gmail.com>
2023-10-25 04:56:12 +0900

    Add) 게임 시작 문구 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    public static String userInput;
     public static void main(String[] args) {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
         // TODO: 프로그램 구현
     }
 }
