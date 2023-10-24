bc14b78f9b13d5ac59bb1cb3b538dcd428389b25
dy102 <132787602+dy102@users.noreply.github.com>
2023-10-24 22:47:01 +0900

    refactor: Baseball클래스만 이용하도록 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,27 +1,17 @@
 package baseball;
 
-import baseball.domain.Count;
-import baseball.domain.Computer;
-import baseball.domain.Restart;
-import baseball.domain.User;
-
 public class Application {
     public static void main(String[] args) {
-        User user = new User();
-        Computer computer = new Computer();
-        Count count = new Count();
-        Restart restart = new Restart();
+        BaseballGame baseballGame = new BaseballGame();
 
         System.out.println("숫자 야구 게임을 시작합니다.");
 
         while (true) {
-            user.inputUserNumber();
+            baseballGame.inputUserNumber();
 
-            count.countAndShow(computer.getRandomNumbers(), user.getUserNumbers());
+            baseballGame.countAndShow();
 
-            if (count.checkStrike() && restart.doRestart()) {
-                computer = new Computer();
-            } else if (count.checkStrike()) {
+            if (!baseballGame.checkAndRestart()) {
                 break;
             }
         }
