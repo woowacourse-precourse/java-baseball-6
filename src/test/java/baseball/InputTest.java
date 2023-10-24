0c42d85411d2c28924eff905ac268c566642ac6a
inhoru126 <inhoru126@gmail.com>
2023-10-25 06:52:45 +0900

    feat(InputTest):입력 예외 테스트기능 추가

--- src/test/java/baseball/InputTest.java
+++ src/test/java/baseball/InputTest.java
@@ -1,5 +1,7 @@
 package baseball;
 
+
+import baseball.view.InputView;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
@@ -8,26 +10,28 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class ApplicationTest extends NsTest {
+class InputTest extends NsTest {
     @Test
-    void 게임종료_후_재시작() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("246", "135", "1", "597", "589", "2");
-                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
-                },
-                1, 3, 5, 5, 8, 9
-        );
-    }
-
-    @Test
-    void 예외_테스트() {
+    void 길이_테스트() {
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("1234"))
+                assertThatThrownBy(() -> InputView.validationNumber("12"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
-
+    @Test
+    void 입력_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> InputView.validationNumber("as1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    void 중복_값_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> InputView.validationNumber("112"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
     @Override
     public void runMain() {
         Application.main(new String[]{});
