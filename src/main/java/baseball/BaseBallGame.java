8c0693c7edb72f0d506249eb6a25fb403f3039c9
ASak1104 <khw56184@gmail.com>
2023-10-25 03:13:52 +0900

    feat : 사용자 입력을 리스트로 반환하도록 수정

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -3,22 +3,23 @@ package baseball;
 import static java.text.MessageFormat.format;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class BaseBallGame {
-    private static final int NUMBER_LENGTH = 3;
+    private static final int TARGET_LENGTH = 3;
 
     private final Opponent opponent;
 
     private boolean endFlag;
 
     public BaseBallGame() {
-        opponent = new Opponent(NUMBER_LENGTH);
+        opponent = new Opponent(TARGET_LENGTH);
         endFlag = false;
     }
 
     public void start() {
         while (!endFlag) {
-            String userGuess = getUserGuess();
+            List<Integer> userGuess = getUserGuess();
             GuessResult guessResult = opponent.guess(userGuess);
             String message = resolveResult(guessResult);
 
@@ -28,17 +29,22 @@ public class BaseBallGame {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
-    private String getUserGuess() {
+    private List<Integer> getUserGuess() {
         System.out.print("숫자를 입력해주세요 : ");
 
-        return Console.readLine();
+        String userInput = Console.readLine();
+
+        return userInput.chars()
+                .map(character -> character - '0')
+                .boxed()
+                .toList();
     }
 
     private String resolveResult(GuessResult guessResult) {
         int ball = guessResult.ball();
         int strike = guessResult.strike();
 
-        endFlag = strike == NUMBER_LENGTH;
+        endFlag = strike == TARGET_LENGTH;
 
         if (strike == 0) {
             return format("{0}볼", ball);
