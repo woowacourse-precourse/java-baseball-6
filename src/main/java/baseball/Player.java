1fc3a332536248953bf9b1def15bfa5daf2af4d1
fnzl54 <rnjscksdud1@naver.com>
2023-10-24 17:49:26 +0900

    style: 숫자를 입력받는 로직 변경

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -12,6 +13,10 @@ public class Player {
    * 사용자에게 서로 다른 숫자 3개를 입력
    * 문자, 서로 같은 숫자 경우 IllegalArgumentException
    */
+  public List<Integer> getInput() throws IllegalArgumentException {
+    String input = Console.readLine();
+    return getInput(input);
+  }
 
   public List<Integer> getInput(String input) throws IllegalArgumentException {
     String[] inputSplit = input.split("");
@@ -40,12 +45,26 @@ public class Player {
    * 사용자에게 재시작 여부를 1(재시작), 2(종료)
    * 1, 2 이외의 숫자를 입력한 경우 IllegalArgumentException
    */
-  public boolean getRestart(int input) throws IllegalArgumentException {
-    if (input == 1) {
+  public boolean getRestart() throws IllegalArgumentException {
+    String input = Console.readLine();
+    return getRestart(input);
+  }
+
+  public boolean getRestart(String input) throws IllegalArgumentException {
+    int inputNumber = parseInt(input);
+    if (inputNumber == 1) {
       return true;
-    } else if (input == 2) {
+    } else if (inputNumber == 2) {
       return false;
     } else {
+      throw new IllegalArgumentException();
+    }
+  }
+
+  private int parseInt(String input) {
+    try {
+      return Integer.parseInt(input);
+    } catch (NumberFormatException e) {
       throw new IllegalArgumentException();
     }
   }
