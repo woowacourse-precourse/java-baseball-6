705257725506b09d393cacf7d7d8d8c438e96ed2
dlawotn3 <imjisu@imjisuui-MacBookPro.local>
2023-10-24 22:57:06 +0900

    feat: 게임 재시작 로직 추가

--- src/main/java/baseball/game/Game.java
+++ src/main/java/baseball/game/Game.java
@@ -1,5 +1,7 @@
 package baseball.game;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Game {
 
     private Player player;
@@ -11,8 +13,10 @@ public class Game {
         player = p;
     }
 
-    public void start() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+    public void start(boolean isStart) {
+        if (isStart) {
+            System.out.println("숫자 야구 게임을 시작합니다.");
+        }
         state = State.PROGRESS;
         playGame();
     }
@@ -32,6 +36,15 @@ public class Game {
     }
 
     public void restart() {
-        // TODO: 게임 재시작 로직 추
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = Console.readLine();
+
+        if (input.equals("1")) {
+            start(false);
+        } else if (input.equals("2")) {
+
+        } else {
+            throw new IllegalArgumentException("입력 받은 값이 유효하지 않습니다.");
+        }
     }
 }
