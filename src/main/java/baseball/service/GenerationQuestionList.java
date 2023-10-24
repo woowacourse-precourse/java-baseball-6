8b3038a282d0e0421d38def129139a896afb5ce8
JongMinCh0i <wonderland314592@gmail.com>
2023-10-24 23:02:48 +0900

    refactoring(GenerationQuestionList): generateRandomNumberList()의 파라미터(시작 범위, 종료 범위)를 record 객체(withinRange) 로 변경

--- src/main/java/baseball/service/GenerationQuestionList.java
+++ src/main/java/baseball/service/GenerationQuestionList.java
@@ -1,5 +1,6 @@
 package baseball.service;
 
+import baseball.config.withinRange;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,15 +8,17 @@ import java.util.List;
 public class GenerationQuestionList {
 
     private final List<Integer> ballCountList;
+    private static final int OUT_COUNT = 3;
 
     public GenerationQuestionList(ArrayList<Integer> ballCountList) {
         this.ballCountList = ballCountList;
     }
 
-    public List<Integer> generateRandomNumberList(int startInclusive, int endInclusive, int outCount) {
+    public List<Integer> generateRandomNumberList(withinRange randomNumberInRange) {
 
-        while (ballCountList.size() < outCount) {
-            int randomNumber = Randoms.pickNumberInRange(startInclusive, endInclusive);
+        while (ballCountList.size() < OUT_COUNT) {
+            int randomNumber = Randoms.pickNumberInRange(randomNumberInRange.startInclusive(),
+                    randomNumberInRange.endInclusive());
 
             if (!includeNumber(randomNumber, ballCountList)) {
                 ballCountList.add(randomNumber);
