62411ecedc20ff76d5d3bad4e2b5f947fbc2edb9
Hyun Woo <hwsa10041@gmail.com>
2023-10-24 14:20:18 +0900

    feat : main 메서드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import baseball.controller.BaseBallController;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BaseBallController baseBallController = new BaseBallController(new InputView(), new OutputView());
+        baseBallController.run();
     }
 }
