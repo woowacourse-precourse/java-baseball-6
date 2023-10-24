402076361f848a8383ff9b7a16391be77b63f51f
Jihye Lee <zerom980706@gmail.com>
2023-10-24 19:18:16 +0900

    change title

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -43,7 +43,7 @@ public class Application {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 restart = Validation.isValidKey(Console.readLine()); //1 입력시 restart == true, 2 입력시 restart == false
-                go = restart; //2 입력시 go == false가 되어 게임 종료
+                go = restart; //2 입력시 go == false가 되어 게임 종료.
 
             } else if (strike == 0 && ball == 0) {
                 System.out.println("낫싱");
