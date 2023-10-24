687da62290003c8e52d050fb8861bc151c03493c
한별 <hanbyeol@hanbyeols-MacBook-Pro.local>
2023-10-25 02:46:44 +0900

    feat.사용자 입력값 받는 메서드 추가 사용자 입력을 받는 메서드인 getInputnum을 선언하고 호출하는 코드 추가했습니다.

--- src/test/java/constant/Constant.java
+++ src/test/java/constant/Constant.java
@@ -1,7 +1,8 @@
 package constant;
 
 public class Constant{
-    public static String ASK_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static String ASK_RESTART_MSG = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static String ASK_INPUT_NUMBER_MSG = "숫자를 입력해주세요 : ";
     public static int RESTART_GAME_NUMBER = 1;
     public static int EXIT_GAME_NUMBER = 2;
     public static int NUM_DIGITS=3;
--- src/test/java/game/Game.java
+++ src/test/java/game/Game.java
@@ -10,13 +10,16 @@ public class Game{
     public static void start(){
         do{
             List<Integer> targetNumber = new ArrayList<>(getRandomNum());
+            System.out.print(ASK_INPUT_NUMBER_MSG);
+            List<Integer> inputNumber = new ArrayList<>(getInputNum());
+
         }while(restart());
     }
 
 //  게임 재시작 여부
     private boolean restart(){
-        System.out.println(ASK_RESTART_MESSAGE);
-        int UserInput = Console.readLine();
+        System.out.println(ASK_RESTART_MSG);
+        String UserInput = Console.readLine();
         return compareRestart(UserInput);
     }
     private boolean compareRestart(int UserInput){
@@ -34,5 +37,21 @@ public class Game{
             }
         }
         return randomNumber;
+    }
+
+//사용자 입력값
+    private List<Integer> getInputNum(){
+        String UserInput = Console.readLine();
+        //예외상황
+        List<Integer> inputNum = new ArrayList<>();
+        for(int i=0;i<UserInput.length();i++){
+            int num = charToInt(UserInput.charAt(i));
+            inputNum.add(num);
+        }
+        return inputNum;
+    }
+
+    private int charToInt(char ch){
+        return ch-'0';
     }
 }
