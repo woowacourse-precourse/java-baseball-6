73af8898c663abda59e5f612c61402a2d9e98828
jiaeYoon <yoon529@sookmyung.ac.kr>
2023-10-24 16:09:45 +0900

    Feat: '3자리 수의 랜덤 숫자생성 기능' 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,34 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        int status = 1;
+        while (status == 1) {
+
+            String computer = makeRandomNums();
+
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            status = Integer.parseInt(Console.readLine());
+        }
+    }
+
+    private static String makeRandomNums() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return "" + computer.get(0) + computer.get(1) + computer.get(2);
     }
 }
