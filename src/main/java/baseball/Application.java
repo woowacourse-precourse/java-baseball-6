f156e20bbda5b209708db17e08440f369c0f5c2c
chae401 <chae401@sju.ac.kr>
2023-10-25 01:17:04 +0900

    refactor: 생성자 주입으로 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,8 +4,7 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         BaseballGame baseballGame = new BaseballGame();
-        Human user = new Human();
-        baseballGame.start(user);
+        baseballGame.start();
     }
 
 }
--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -3,29 +3,31 @@ package baseball;
 public class BaseballGame {
     private boolean running;
     private final Computer computer;
+    private final Human human;
 
     public BaseballGame() {
         this.running = true;
         this.computer = new Computer();
+        this.human = new Human();
     }
 
     // 게임 시작
-    public void start(Human user) {
+    public void start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
         computer.chooseRandomValues();
         while (running) {
-            run(user);
+            run();
         }
 
         // 게임 종료 or 재시작 선택
-        isRestart(user);
+        isRestart();
     }
 
     // 게임 실행 중
-    private void run(Human user) {
+    private void run() {
         System.out.print("숫자를 입력해주세요. : ");
-        user.chooseValues();
-        ResultDTO result = computer.compare(user.getValues());
+        human.chooseValues();
+        ResultDTO result = computer.compare(human.getValues());
         if (result.getStrike() == 3) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             running = false;
@@ -33,11 +35,11 @@ public class BaseballGame {
     }
 
     // 게임 재시작
-    private void isRestart(Human user) {
+    private void isRestart() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        running = user.isRestart();
+        running = human.isRestart();
         if (running) {
-            start(user);
+            start();
         }
     }
 }
