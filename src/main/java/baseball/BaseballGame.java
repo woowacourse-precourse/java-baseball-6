734bf1672a99c6a7f9743ef1247cdd7b0bd45b3c
xodnjs8287 <xodnjs8287@naver.com>
2023-10-24 19:36:14 +0900

    fix : 불필요한 import 문 제거

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,6 +1,5 @@
 package baseball;
 
-import java.util.List;
 import java.util.Scanner;
 
 public class BaseballGame {
--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,8 +1,5 @@
 package baseball;
 
-import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Computer {
