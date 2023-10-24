8c496869af4aea90f4c53cd54cfa8b15c550e79f
wooyongjeong <wooyong.dev@gmail.com>
2023-10-24 21:31:19 +0900

    feat(BaseballGame): 숫자 야구 게임 시작 로직 작성

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,15 @@
+package baseball;
+
+public class BaseballGame {
+
+    private final Input input;
+    private final Output output;
+
+    public BaseballGame(Input input, Output output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    public void playBaseball() {
+    }
+}
---
+++ src/main/java/baseball/Input.java
@@ -0,0 +1,8 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+
+public class Input {
+}
