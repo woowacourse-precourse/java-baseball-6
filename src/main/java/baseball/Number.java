60efbecc91709892b157e5bf60e6f590782d40ae
LEE-sh1673 <lsh901673@gmail.com>
2023-10-24 19:14:00 +0900

    refactor: 클래스를 레코드 타입으로 변경
    Number는 야구 숫자를 나타내는 불변 데이터이며, 더 간결하게 표현하기 위해 레코드로 변경함

--- src/main/java/baseball/Number.java
+++ src/main/java/baseball/Number.java
@@ -1,18 +1,13 @@
 package baseball;
 
-import java.util.Objects;
-
-public class Number {
+public record Number(int number) {
 
     private static final int MIN_NUMBER = 1;
 
     private static final int MAX_NUMBER = 9;
 
-    private final int number;
-
-    private Number(final int number) {
+    public Number {
         validate(number);
-        this.number = number;
     }
 
     private void validate(final int number) {
@@ -27,22 +22,5 @@ public class Number {
 
     public static Number of(final int number) {
         return new Number(number);
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        final Number other = (Number) o;
-        return number == other.number;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(number);
     }
 }
