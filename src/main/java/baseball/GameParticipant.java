5633f95ea400b0c9bd7027353ad5463a034931f0
Cycrypto <jh01love@naver.com>
2023-10-24 16:46:16 +0900

    feat: 다시플레이 질의 추가

--- src/main/java/baseball/GameParticipant.java
+++ src/main/java/baseball/GameParticipant.java
@@ -2,6 +2,8 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Objects;
+
 
 public class GameParticipant {
     private String playerGuessedNumber;
@@ -12,11 +14,17 @@ public class GameParticipant {
         this.playerStatus = GameStatus.PLAYING;
         this.game = new GameManager();
     }
-    public void playGameByRequest(){
+    public GameStatus playGameByRequest(){
         while(playerStatus == GameStatus.PLAYING){
             System.out.print("숫자를 입력해주세요 :");
             this.playerGuessedNumber = Console.readLine();
-            this.game.processTurn(this.playerGuessedNumber);    // 게임을 진행시킴
+            playerStatus = this.game.processTurn(this.playerGuessedNumber);    // 게임을 진행시킴
         }
+        System.out.print("다시 플레이? : ");
+        String replay = Console.readLine();
+        if (Objects.equals(replay, "1")){
+            playerStatus = GameStatus.PLAYING;
+        }
+        return playerStatus;
     }
 }
