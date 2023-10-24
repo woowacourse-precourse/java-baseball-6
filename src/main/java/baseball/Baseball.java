92567e6859e425300bbf006ff345585f6568c160
eunji <dmswl6310@gmail.com>
2023-10-25 00:29:25 +0900

    refactor: 변수명을 명료하게 수정

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -8,35 +8,38 @@ public class Baseball {
 
     // 생성자
     public Baseball(){
-        initBaseball();
+        initGame();
     }
 
-    public void initBaseball(){
+    public void initGame(){
         // 게임 시작 알림
         System.out.println("숫자 야구 게임을 시작합니다.");
 
         // 컴퓨터 수 세팅
-        this.computerNum=setComputerNum();
+        this.computerNum=makeRandomNum();
     }
 
-    public String setComputerNum(){
-        String computer = "";
+    public String makeRandomNum(){
+        String randomStr = "";
 
-        while(computer.length()<3) {
+        while(randomStr.length()<3) {
+
             // 서로 다른 세자리 수 생성
             int randomNum = Randoms.pickNumberInRange(1, 9);
             String numToString=Integer.toString(randomNum);
 
-            if (!computer.contains(numToString)) {
-                computer = computer.concat(numToString);
+            if (!randomStr.contains(numToString)) {
+                randomStr = randomStr.concat(numToString);
             }
         }
 
-        return computer;
+        return randomStr;
     }
+
+    // 콘솔에 게임 결과를 출력하는 함수
     public void printResult(int[] result){
-        int ballNum=result[0];
-        int strikeNum=result[1];
+        int ballNum = result[0];
+        int strikeNum = result[1];
 
         if(ballNum==0 && strikeNum==0) {
             System.out.println("낫싱");
@@ -48,24 +51,28 @@ public class Baseball {
         System.out.print("\n");
     }
 
+    // 볼과 스트라이크 갯수를 세는 함수
     public int[] countResult(String input){
         int ballNum=0;
         int strikeNum=0;
 
         for(int i=0;i<3;i++){
-            if(input.charAt(i)==computerNum.charAt(i)) strikeNum++;
+            if(input.charAt(i) == computerNum.charAt(i)) strikeNum++;
             else if(computerNum.contains(String.valueOf(input.charAt(i)))){
                 ballNum++;
             }
         }
 
-        return new int[]{ballNum,strikeNum};
+        return new int[] {ballNum,strikeNum};
     }
 
-    public void playBaseball(){
+    public boolean isCorrect(int strikeNum){
+        return strikeNum == 3;
+    }
 
-        do {
+    public void playGame(){
 
+        while(true) {
             System.out.print("숫자를 입력해주세요 : ");
             String input=Console.readLine();
 
@@ -80,18 +87,19 @@ public class Baseball {
             int[] result=countResult(input);
             printResult(result);
 
-            if(result[1]==3) {
+            if(isCorrect(result[1])) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 break;
             }
-        }while(true);
+        }
 
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String input=Console.readLine();
+
         if(input.equals("1")){
 //            initBaseball();
-            this.computerNum=setComputerNum();
-            playBaseball();
+            this.computerNum=makeRandomNum();
+            playGame();
         }else if(!input.equals("2")){
             throw new IllegalArgumentException();
         }
