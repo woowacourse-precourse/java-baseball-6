4cce300b46163ada9e8f132cfd203fd7f79d06f6
gonixxxx <akdntm946@gmail.com>
2023-10-25 00:09:39 +0900

    style: while문 부분을 메소드로 분리

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -8,13 +8,14 @@ public class Game {
     public void start() {
         User userObj = new User();
         Computer computerObj = new Computer();
-
         List<Integer> computer = new GenerateRandomNumber().generateNumber();
+        compareStart(userObj, computerObj, computer);
+    }
+
+    public void compareStart(User userObj, Computer computerObj, List<Integer> computer) {
         while (true) {
             List<Integer> user = userObj.inputNumber();
             if (computerObj.compareNumber(computer, user)) {
-                System.out.println(
-                        "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 break;
             }
         }
