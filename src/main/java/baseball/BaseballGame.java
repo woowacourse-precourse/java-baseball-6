0c6d74e8aee3b8f575d369c98b768f8f86d704fe
gahyeon <1000ji1@naver.com>
2023-10-24 16:24:14 +0900

    feat : create randomNumber one time

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -2,20 +2,16 @@ package baseball;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class BaseballGame {
-    private int ball = 0;
-    private int strike = 0;
-    private String userNumber;
-    private String userInput;
+    private int ball;
+    private int strike;
     private static final String CONTINUE = "1";
     private static final String QUIT = "2";
+    private static String isContinue = CONTINUE;
 
     BaseballGame() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
     public void init() {
@@ -23,56 +19,57 @@ public class BaseballGame {
         this.strike = 0;
     }
 
-    public void getRandomNum(List<Integer> computer) {
-        RandomNum RN = new RandomNum(computer);
+    public List<Integer> getRandomNum() {
+        RandomNum RN = new RandomNum();
+        return RN.getComputerNumbers();
     }
 
     public void checkInput(String userNumber) {
-        // 3자리 수 초과 입력
-        Array.checkArrayLength(userNumber);
-        // 0이 포함된 경우
-        Array.checkZeroInArray(userNumber);
-        // 숫자가 아닌 경우
         Array.checkArrayNumber(userNumber);
+        Array.checkArrayLength(userNumber);
+        Array.checkRange(userNumber);
     }
 
-    public void inputNumber() {
+    public String inputNumber() {
         System.out.print("숫자를 입력해주세요 : ");
-        this.userNumber = readLine(); // 서로 다른 3개의 숫자 입력
+        return readLine(); // 서로 다른 3개의 숫자 입력
     }
 
     public void getAnswer(List<Integer> computer, List<Integer> userNum) {
         // 컴퓨터가 만든 서로 다른 랜덤 3자리 수 computer 와 사용자 입력을 받은 서로 다른 3자리 수 userNum 비교
         for (int i = 0; i < computer.size(); i++) {
-            if (computer.get(i).equals(userNum.get(i))) { // 같은 수가 같은 자리에 있으면 스트라이크
+            if (userNum.get(i).equals(computer.get(i))) { // 같은 수가 같은 자리에 있으면 스트라이크
                 this.strike++;
-                continue;
-            }
-            if (userNum.contains(computer.get(i))) {
+            } else if (userNum.contains(computer.get(i))) {
                 this.ball++;
             }
         }
+        printAnswer(); // 출력
     }
 
     public String startNewGame() {
+        String userInput;
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
-        this.userInput = readLine();
-        Array.checkArrayNumber(this.userInput);
-        Array.checkOneOrTwo(this.userInput);
+        userInput = readLine();
+        Array.checkArrayNumber(userInput);
+        Array.checkOneOrTwo(userInput);
 
-        if (this.userInput.equals("1")) { // 1이면 게임 다시
+        if (userInput.equals("2")) { // 1이면 게임 다시
             // 게임 다시
-            return CONTINUE;
-        } else if (this.userInput.equals("2")) { // 2이면 게임 종료
-            // 게임 종료
             return QUIT;
         }
-        return null;
+        return CONTINUE;
+    }
+
+    public boolean isAllStrike() {
+        if (strike == 3 && ball == 0) { // 3개의 숫자를 모두 맞히면
+            return true;
+        }
+        return false;
     }
 
     public void printAnswer() {
-        String isContinue = CONTINUE;
 
         if (strike == 0 && ball == 0) { // 같은 수가 전혀 없으면
             System.out.println("낫싱");
@@ -83,9 +80,7 @@ public class BaseballGame {
 
             isContinue = startNewGame();
         }
-        // 볼만 있을 경우
-        // 스트라이크만 있을 경우
-        // 볼, 스트라이크 둘 다 있으면
+        // 볼만 있을 경우, 스트라이크만 있을 경우, 볼, 스트라이크 둘 다 있으면
         if (ball > 0) {
             System.out.print(ball + "볼 ");
             if (strike > 0) {
@@ -93,37 +88,29 @@ public class BaseballGame {
             }
             System.out.println();
         }
-
-        if (Objects.equals(isContinue, "1")) {
-            play();
-        } else if ((Objects.equals(isContinue, "2"))) {
-            return;
-        }
-
     }
 
     public void play() {
-        init();
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        do {
+            game();
+        } while (isContinue.equals(CONTINUE));
+    }
 
-        //서로 다른 random 3자리 수 만들기
-        List<Integer> computer = new ArrayList<>();
-        this.getRandomNum(computer);
+    public void game() {
+        String userNumber;
+        List<Integer> computer = this.getRandomNum();
 
-        // 입력
-        this.inputNumber();
+        do {
+            init();
 
-        // 입력 확인
-        this.checkInput(this.userNumber);
+            userNumber = inputNumber();
+            checkInput(userNumber);
 
-        // 같은 수 포함
-        List<Integer> userNum = new ArrayList<>();
-        Array.checkForDuplicateNumbers(userNum, this.userNumber);
-        //System.out.println(userNum);
+            List<Integer> userNum = Array.checkForDuplicateNumbers(userNumber);
 
-        // 볼, 스트라이크 세기
-        this.getAnswer(computer, userNum);
-        // 출력
-        this.printAnswer();
+            getAnswer(computer, userNum); // 볼, 스트라이크 세기
+        } while (!isAllStrike());
 
     }
 }
