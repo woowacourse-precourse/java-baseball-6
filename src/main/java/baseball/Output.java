7affb89931116527d9755e3c903f43f7072781f5
wooyongjeong <wooyong.dev@gmail.com>
2023-10-25 02:09:57 +0900

    feat(Output): Output 추가
    화면에 문자열을 출력하는 Output 클래스 추가
    게임 진행 상황을 출력하는 메서드들 작성

--- src/main/java/baseball/Output.java
+++ src/main/java/baseball/Output.java
@@ -1,4 +1,28 @@
 package baseball;
 
 public class Output {
+
+    public void gameStart() {
+        System.out.println(OutputMessage.GAME_START.getMessage());
+    }
+
+    public void inputPlayerNumber() {
+        System.out.print(OutputMessage.INPUT_PLAYER_NUMBER.getMessage());
+    }
+
+    public void inningEnd() {
+        System.out.println(OutputMessage.INNING_END.getMessage());
+    }
+
+    public void inputGameCommand() {
+        System.out.println(OutputMessage.INPUT_GAME_COMMAND.getMessage());
+    }
+
+    public void gameEnd() {
+        System.out.println(OutputMessage.GAME_END.getMessage());
+    }
+
+    public void printExceptionMessage(Exception exception) {
+        System.out.println("[ERROR] " + exception.getMessage());
+    }
 }
