9ec8009fdaa460dff6c9ed1844183d8a727d4b6a
practice-hyeon <dusdj0813@khu.ac.kr>
2023-10-24 16:20:15 +0900

    fix: add num variable
    git 계정 문제로 코드를 옮기는 과정에서 누락되어 추가합니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -27,6 +27,7 @@ class Baseball {
     private static final String SUCCESS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String RESTART_CHECK_MESSAGE= "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
+    String num = "";
 
     Baseball() {
         String randomNum;
