f1cd053fdb1a2d274788db89a7d62f83d5441755
oxdjww <oxdjww@gmail.com>
2023-10-24 22:46:58 +0900

    refactor : 검증 함수에서 책임을 분리 (list.add하는 책임을 생성자에 위임)

--- src/main/java/baseball/model/UserNumber.java
+++ src/main/java/baseball/model/UserNumber.java
@@ -8,40 +8,41 @@ public class UserNumber {
     private final List<Integer> userNumber = new ArrayList<>();
 
     public UserNumber(String userNumber) {
-        validateUserNumber(userNumber);
+        isNumeric(userNumber);
+        isThreeDigitNumber(userNumber);
+        for (int i = 0; i < userNumber.length(); i++) {
+            Integer num = Character.getNumericValue(userNumber.charAt(i));
+            isDuplicateDigit(num);
+            // 중복까지 통과하면 하나씩 더하기
+            this.userNumber.add(num);
+        }
+    }
+
+    // 중복된 수가 있는지 검증
+    private void isDuplicateDigit(Integer num) {
+        if (this.userNumber.contains(num)) {
+            throw new IllegalArgumentException("잘못된 입력입니다. 중복된 자리 수가 있습니다.");
+        }
+    }
+
+    // 3자리 수 인지 검증
+    private void isThreeDigitNumber(String userNumber) {
+        if (userNumber.length() != 3) {
+            throw new IllegalArgumentException("잘못된 입력입니다. 3자리 숫자가 아닙니다.");
+        }
+    }
+
+    // 숫자인지 확인
+    private void isNumeric(String userNumber) {
+        try {
+            Integer.parseInt(userNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("잘못된 입력입니다. 숫자가 아닙니다.");
+        }
     }
 
     // 유저 번호를 반환해주는 함수
     public List<Integer> getUserNumber() {
         return this.userNumber;
-    }
-
-    private void validateUserNumber(String userInput) {
-        // 입력이 숫자인지 검증
-        try {
-            Integer.parseInt(userInput);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("잘못된 입력입니다. 숫자가 아닙니다.");
-        }
-
-        // 숫자인 경우, 3자리 수 검증 확인
-        if (userInput.length() != 3) {
-            throw new IllegalArgumentException("잘못된 입력입니다. 3자리 숫자가 아닙니다.");
-        }
-
-        // 숫자이고, 3자리 수인 경우, 중복 자리 수 검증
-        for (int i = 0; i < userInput.length(); i++) {
-            Integer num = Character.getNumericValue(userInput.charAt(i));
-            checkDuplicateNum(num);
-        }
-    }
-
-    // 중복 수 검증 함수
-    private void checkDuplicateNum(Integer num) {
-        if (!this.userNumber.contains(num)) {
-            this.userNumber.add(num);
-        } else {
-            throw new IllegalArgumentException("잘못된 입력입니다. 중복된 자리 수가 있습니다.");
-        }
     }
 }
