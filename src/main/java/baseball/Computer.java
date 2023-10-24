ed3ab8d629c7163e637394a56d1db16ded0ae4d4
psionic0324 <psionic0324@naver.com>
2023-10-24 23:27:55 +0900

    Refactor: Computer수정
    자바코딩 규칙을 참고하여 코드 수정

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -4,30 +4,36 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 객체 생성시 랜덤숫자 3개를 부여하고, 유저 입력 숫자3개를 받아 스트라이크, 볼 개수를 판정하여 반환
+ */
 public class Computer {
     private final List<Integer> computerNumbers = new ArrayList<>();
 
     public Computer() { // 랜덤숫자 3개 부여
-        while(computerNumbers.size() < 3) {
+        int numLengthLimit = 3;
+        while (computerNumbers.size() < numLengthLimit) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if(!computerNumbers.contains(randomNumber)) {
+            if (!computerNumbers.contains(randomNumber)) {
                 computerNumbers.add(randomNumber);
             }
         }
     }
+
     public int strikeCheck(List<Integer> userInput) {
         int count = 0;
-        for(int i = 0; i < 3; i++) {
-            if(computerNumbers.get(i).equals(userInput.get(i))) {
+        for (int i = 0; i < 3; i++) {
+            if (computerNumbers.get(i).equals(userInput.get(i))) {
                 count++;
             }
         }
         return count;
     }
+
     public int ballCheck(List<Integer> userInput) {
         int count = 0;
-        for(int i = 0; i < 3; i++) {
-            if(computerNumbers.contains(userInput.get(i))) {
+        for (int i = 0; i < 3; i++) {
+            if (computerNumbers.contains(userInput.get(i))) {
                 count++;
             }
         }
