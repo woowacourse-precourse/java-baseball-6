62b351ef445f07ea20998438e9c851718853dd92
Dowon <3120kang@gmail.com>
2023-10-24 20:01:21 +0900

    Refactor: InputView클래스에서 유저의 입력을 받도록 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,12 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+
+
     public void start(){
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    public void enterUserAnswer(){
+    public String enterUserAnswer(){
         System.out.println("숫자를 입력해주세요 : ");
+        return Console.readLine();
     }
 
     public boolean restart(){
