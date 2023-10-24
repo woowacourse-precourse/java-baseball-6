0ded2ff482ab23e85882c55172c652ecdac4f5e1
Hayoon <hy97@sju.ac.kr>
2023-10-24 18:51:08 +0900

    feat(BaseBallService): checkStrikeAndBallCount는 스트라이크/볼 카운트를 구하는 기능을 한다

--- src/main/java/baseball/service/BaseBallService.java
+++ src/main/java/baseball/service/BaseBallService.java
@@ -17,4 +17,23 @@ public class BaseBallService {
         return IntStream.range(0, playerBallNumbers.size())
                 .allMatch(i -> Objects.equals(playerBallNumbers.get(i), computerBallNumbers.get(i)));
     }
+
+    public int[] checkStrikeAndBallCount(BaseBall player, BaseBall computer) {
+        List<Integer> playerBallNumbers = player.getInputBallNumber();
+        List<Integer> computerBallNumbers = computer.getInputBallNumber();
+
+        int[] score = new int[2];
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                if(playerBallNumbers.get(i).equals(computerBallNumbers.get(j)) && i == j) {
+                    score[0]++;
+                    break;
+                } else if(playerBallNumbers.get(i).equals(computerBallNumbers.get(j)) && i != j) {
+                    score[1]++;
+                    break;
+                }
+            }
+        }
+        return score;
+    }
 }
