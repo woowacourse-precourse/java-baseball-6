4d703f4d6920fc8b4ebda72cd8d5fcea2e57692c
akskflwn <akskflwn@gmail.com>
2023-10-24 16:37:43 +0900

    feat: 재시작 여부 입력받는 기능

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -9,6 +9,10 @@ public class InputView {
 
     private static final String NUMBER_RANGE = "^[1-9]*$";
     private static final String NUMBER_SEPARATE_DELIMITER = "";
+    private static final String YES = "1";
+    private static final String NO = "2";
+
+    private static final String RETRY_OR_END_GAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static final String GET_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
 
     public static List<Integer> getNumbers() {
@@ -32,5 +36,18 @@ public class InputView {
 
     private static String[] separate(String input) {
         return input.split(NUMBER_SEPARATE_DELIMITER);
+    }
+
+    public static boolean retryOrExit() {
+        System.out.println(RETRY_OR_END_GAME_MESSAGE);
+        String input = Console.readLine();
+        validateUserChoice(input);
+        return input.equals(YES);
+    }
+
+    public static void validateUserChoice(String input) {
+        if (!input.equals(YES) && !input.equals(NO)) {
+            throw new IllegalArgumentException();
+        }
     }
 }
