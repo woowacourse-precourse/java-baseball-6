98270c32e8b8424f08b670cf2cf7adf98915e7af
song <brownmole01@naver.com>
2023-10-24 18:01:42 +0900

    1024

--- src/main/java/game/GameImp.java
+++ src/main/java/game/GameImp.java
@@ -4,93 +4,102 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
-
 public class GameImp implements Game{
-    private List<Integer> comnum ;
-    private List<Integer> usernum;
+    private static List<Integer> comnum ;
+    private static List<Integer> usernum;
     private Integer ball = 0;
     private Integer strike = 0;
+    private static final String strBall = "볼";
+    private static final String strNothing = "낫싱";
+    private static final String strStrike = "스트라이크";
+    private static final String restart = "1";
+    private static final String finish = "2";
+    private static final int size = 3;
+    private static final int zero = 0;
+    private static final int strikeOut = size;
+    private static final char charZero = '0';
+
     public void start() {
-        strike =0;
-        ball = 0;
+        strike =zero;
+        ball = zero;
         comnum = new ArrayList<>();
-        while(comnum.size() < 3) {
+        usernum = new ArrayList<>();
+        while(comnum.size() < size) {
             int randomNumber = Randoms.pickNumberInRange(1,9);
             if(!comnum.contains(randomNumber)) {
                 comnum.add(randomNumber);
             }
         }
-        return ;
     }
     @Override
     public boolean gameInput(String num) throws IllegalArgumentException {
         usernum = new ArrayList<>();
-        Integer numInt = -1;
-        if(num.length() != 3) {
+        if(num.length() != size) {
             throw new IllegalArgumentException();
         }
-        for(int i=0;i<3;i++) {
-            for(int j=0;j<3;j++) {
+        for(int i=zero;i<size;i++) {
+            for(int j=zero;j<size;j++) {
                 if(i == j)
                     continue;
                 if(num.charAt(i) == num.charAt(j)) {
                     throw new IllegalArgumentException();
                 }
-                if(num.charAt(i) == '0') {
+                if(num.charAt(i) == charZero) {
                     throw new IllegalArgumentException();
                 }
             }
         }
         try{
-            numInt = Integer.parseInt(num);
+            Integer.parseInt(num);
         } catch (Exception e) {
             throw new IllegalArgumentException();
         }
-        for(int i=0;i<3;i++) {
-            usernum.add(num.charAt(i)-'0');
+        for(int i=zero;i<size;i++) {
+            usernum.add(num.charAt(i)-charZero);
         }
         return true;
     }
     @Override
     public String gameoutput() {
         String ans = "";
-
-        strike = 0;
-        for(int i=0;i<3;i++) {
-            if(comnum.get(i) == usernum.get(i)) {
+        strike = zero;
+        for(int i=zero;i<size;i++) {
+            Integer nownum = comnum.get(i);
+            if(nownum.equals(usernum.get(i))) {
                 strike++;
-                usernum.set(i, 0);
+                usernum.set(i, zero);
             }
         }
-        ball = 0;
-        for(int i=0;i<3;i++) {
-            if(usernum.get(i) == 0)
+        ball = zero;
+        for(int i=zero;i<size;i++) {
+            if(usernum.get(i) == zero)
                 continue;
             if(comnum.contains(usernum.get(i)))
                 ball++;
         }
-        if(ball != 0)
-            ans += (ball + "볼");
-        if(ball != 0 && strike != 0)
+        if(ball != zero)
+            ans += (ball + strBall);
+        if(ball != zero && strike != zero)
             ans += " ";
-        if(strike != 0)
-            ans += (strike + "스트라이크");
-        if(ans == "")
-            ans = "낫싱";
+        if(strike != zero)
+            ans += (strike + strStrike);
+        if(ans.isEmpty())
+            ans = strNothing;
         return ans;
     }
     @Override
     public boolean getStatus() {
-        if(strike == 3)
+
+        if(strike.equals(strikeOut))
             return true;
         return false;
     }
     @Override
     public boolean restart(String reinput) throws IllegalArgumentException{
-        if(reinput.equals("1") && reinput.equals("2")){
+        if(!reinput.equals(restart) && !reinput.equals(finish)){
             throw new IllegalArgumentException();
         }
-        if(reinput.equals("1"))
+        if(reinput.equals(restart))
             return true;
         return false;
     }
