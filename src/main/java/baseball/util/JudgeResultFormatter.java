3f677e9f0be16c37099bb996451b7e7ba120a558
swoolee97 <swoolee97@ajou.ac.kr>
2023-10-25 02:43:50 +0900

    refactor: 재사용 되지 않는 상수값 정리

--- src/main/java/baseball/util/JudgeResultFormatter.java
+++ src/main/java/baseball/util/JudgeResultFormatter.java
@@ -4,10 +4,14 @@ import baseball.model.JudgeResult;
 
 public class JudgeResultFormatter {
     private static final int NO_COUNT = 0;
+    private static final String NOTHING_STRING = "낫싱";
+    private static final String BALL_STRING = "볼";
+    private static final String STRIKE_STRING = "스트라이크";
+    private static final String SPACE = " ";
 
     public static String format(JudgeResult judgeResult) {
         if (judgeResult.getNothingCount() == Constants.BALL_LENGTH) {
-            return Constants.NOTHING_STRING;
+            return NOTHING_STRING;
         }
         StringBuilder sb = new StringBuilder();
         formatBall(judgeResult, sb);
@@ -18,15 +22,15 @@ public class JudgeResultFormatter {
     private static void formatBall(JudgeResult judgeResult, StringBuilder sb) {
         if (judgeResult.getBallCount() > NO_COUNT) {
             sb.append(judgeResult.getBallCount());
-            sb.append(Constants.BALL_STRING);
-            sb.append(Constants.SPACE);
+            sb.append(BALL_STRING);
+            sb.append(SPACE);
         }
     }
 
     private static void formatStrike(JudgeResult judgeResult, StringBuilder sb) {
         if (judgeResult.getStrikeCount() > NO_COUNT) {
             sb.append(judgeResult.getStrikeCount());
-            sb.append(Constants.STRIKE_STRING);
+            sb.append(STRIKE_STRING);
         }
     }
 
