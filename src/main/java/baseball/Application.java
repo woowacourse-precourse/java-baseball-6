d8ec4a2fd55fc2fd36a0e2ef13caa791e40b95b1
bbbig5360 <bbbig5360>
2023-10-25 00:21:30 +0900

    스트라이크, 볼에 따라 결과 출력 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,44 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }    
+    
+        ArrayList<Integer> StrikeBallList = new ArrayList<Integer>();
+        StrikeBallList.add(3);
+        StrikeBallList.add(0);
+        System.out.println(StrikeBallList);
+
+        while (true){
+            // ikeBallList =  ArrayList<Integer>(strike, ball)
+            if (StrikeBallList.get(0) == 3){
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            }else if (StrikeBallList.get(0) == 0){
+                if (StrikeBallList.get(1) == 0){
+                    System.out.println("낫싱");
+                }else{
+                    System.out.println(StrikeBallList.get(1)+"볼");
+                }
+            }else{
+                if (StrikeBallList.get(1) != 0){
+                    System.out.println(StrikeBallList.get(1)+"볼 " + StrikeBallList.get(0) + "스트라이크");
+                }else{
+                    System.out.println(StrikeBallList.get(0)+"스트라이크");
+                }
+            }
+        }
+
     }
 }
