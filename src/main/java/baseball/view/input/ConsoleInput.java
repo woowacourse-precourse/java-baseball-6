0e0b1859c1f5bef36c7f369403d47edaf2d850d5
5uhwann <5uhwann27@gmail.com>
2023-10-25 02:00:23 +0900

    refactor: 오류 메시지 util화

--- src/main/java/baseball/view/input/ConsoleInput.java
+++ src/main/java/baseball/view/input/ConsoleInput.java
@@ -1,5 +1,8 @@
 package baseball.view.input;
 
+import static baseball.utils.message.message.ExceptionMessage.BASEBALL_INPUT_EXCEPTION_MESSAGE;
+import static baseball.utils.message.message.ExceptionMessage.END_INPUT_EXCEPTION_MESSAGE;
+
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
@@ -29,13 +32,13 @@ public class ConsoleInput implements Input {
                 .distinct()
                 .toList();
         if (!baseballInput.matches(REGEX_BASEBALL) || distinctNumber.size() != 3) {
-            throw new IllegalArgumentException("서로 다른 3자리 숫자만 입력 가능합니다.");
+            throw new IllegalArgumentException(BASEBALL_INPUT_EXCEPTION_MESSAGE);
         }
     }
 
     private void validateEndInput(String endInput) {
         if (!endInput.matches(REGEX_END)) {
-            throw new IllegalArgumentException("새로운 게임을 시작혀려면 1, 종료하려면 2를 입력하세요.");
+            throw new IllegalArgumentException(END_INPUT_EXCEPTION_MESSAGE);
         }
     }
 }
