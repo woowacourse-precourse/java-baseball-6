febf789c9f805c931f2ff1e0d121c21d1ffc2fd9
34suuuuu <psh7492@gmail.com>
2023-10-24 18:06:39 +0900

    feat: 낫싱메시지 상수 추가

--- src/main/java/constants/MessageConstants.java
+++ src/main/java/constants/MessageConstants.java
@@ -1,9 +1,10 @@
-package constansts;
+package constants;
 
 public class MessageConstants {
 
     public static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     public static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    public static final String NOTHING_MESSAGE = "낫싱";
     public static final String GAME_FINISH_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     public static final String RESTART_OR_FINISH_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
