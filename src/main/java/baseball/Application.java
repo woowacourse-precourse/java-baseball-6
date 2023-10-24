d63764652c9fd946392d8ae4bcc48a00360ce79c
김민주 <69795199+ks00919@users.noreply.github.com>
2023-10-24 23:48:28 +0900

    feat: 입력 메소드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,38 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            startBaseballShell();
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+        Console.close();
     }
+
+    public static void startBaseballShell() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (true) {
+            String answer = getInput("숫자를 입력해주세요 : ", "\\d\\d\\d");
+        }
+    }
+
+    public static String getInput(String message, String regex) {
+        System.out.print(message);
+        String userInput = Console.readLine();
+        isMatches(userInput, regex);
+        return userInput;
+    }
+
+    public static boolean isMatches(String string, String regex) {
+        if (string.matches(regex)) {
+            return true;
+        }
+        throw new IllegalArgumentException(string + " 은(는) 형식에 맞지 않습니다.");
+    }
+
+
 }
