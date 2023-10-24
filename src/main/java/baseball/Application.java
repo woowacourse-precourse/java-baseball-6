d1d01e16c31c2b4ced37c812e22817de666de4e4
Hayoon <hy97@sju.ac.kr>
2023-10-24 20:58:02 +0900

    style(Application): 상수 매직넘버로 치환

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,13 +1,17 @@
 package baseball;
 
 import baseball.console.BaseBallGame;
+import baseball.domain.BaseBall;
+import baseball.util.Constant;
 
 import java.io.IOException;
 
+import static baseball.util.Constant.*;
+
 public class Application {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         // TODO: 프로그램 구현
         BaseBallGame baseBallGame = new BaseBallGame();
-        baseBallGame.playGame();
+        while(baseBallGame.playGame() != RESTART);
     }
 }
