b74b354201bc06c92b96d4484bb939caea6a4dc0
hajungIm <ihj_ewha@ewhain.net>
2023-10-25 01:21:04 +0900

    Feat: 전체 게임 제어 메소드 작성
    게임 시작, 재시작, 완전 종료 제어
    재시작 여부 안내문 출력 메소드 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,35 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         printStartStmt();
         startGame();
     }
+
+    /**
+     * gameControl Methods
+     */
+    private static void startGame() {
+        boolean restartGame;
+
+        do {
+            baseballGame();
+
+            printAskRestartStmt();
+
+            String restartInput = Console.readLine();
+            restartGame = restartInput.equals("1");
+        } while (restartGame);
+    }
+
     private static void printStartStmt() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+
+    private static void printAskRestartStmt() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 }
