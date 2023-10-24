3478d310f1f20707a70b28bc4fef4e35dc0cb97b
masonkimseoul <masonkimseoul@gmail.com>
2023-10-24 21:08:57 +0900

    refactor : (OutputView.java) Extract string literals to constants

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -6,6 +6,9 @@ public class OutputView {
     private static final String GET_USER_NUMBERS_MSG = "숫자를 입력해주세요 : ";
     private static final String END_GAME_MSG = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String GET_RESTART_MSG = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String NOTHING = "낫싱";
+    private static final String BALL = "볼";
+    private static final String STRIKE = "스트라이크";
 
     public static void printStartGameMsg() {
         System.out.println(START_GAME_MSG);
@@ -27,13 +30,13 @@ public class OutputView {
         String answer = "";
 
         if (balls == 0 && strikes == 0) {
-            answer += "낫싱";
+            answer += NOTHING;
         }
         if (balls > 0) {
-            answer += (balls + "볼 ");
+            answer += (balls + BALL);
         }
         if (strikes > 0) {
-            answer += (strikes + "스트라이크");
+            answer += (strikes + STRIKE);
         }
 
         System.out.println(answer);
