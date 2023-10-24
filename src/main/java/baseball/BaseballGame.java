37799c2d17e798b1e27b0905241cf233eb581fbd
cyPark95 <parkcy1007@gmail.com>
2023-10-24 21:30:13 +0900

    feat: 서로 다른 랜덤 3자리 수 컴퓨터에게 부여
    - 컴퓨터에게 지정하기 위한 서로 다른 임의의 3자리 숫자 지정

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -2,6 +2,7 @@ package baseball;
 
 import static baseball.view.InputView.getNumbers;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 import utils.ValidationUtils;
@@ -9,7 +10,19 @@ import utils.ValidationUtils;
 public class BaseballGame {
 
     public void play() {
+        List<Integer> computer = createComputer();
         List<Integer> balls = getBallNumbers();
+    }
+
+    List<Integer> createComputer() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 
     private List<Integer> getBallNumbers() {
