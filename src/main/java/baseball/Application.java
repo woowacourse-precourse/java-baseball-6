bd452f05998ef9636d3452fd7b1ae6f1f2058e5d
seunghye218 <seunghye@student.42seoul.kr>
2023-10-24 15:22:00 +0900

    feat: game logic

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,18 +4,44 @@ import java.util.List;
 import java.util.ArrayList;
 
 import camp.nextstep.edu.missionutils.Randoms;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
 
     public static void main(String[] args) {
         List<Integer> computer = getRandomList();
+        int[] score = new int[2];
+        String result = "";
         System.out.println("숫자 야구 게임을 시작합니다.");
-
         while (true) {
             System.out.print("숫자를 입력해주세요 : ");
             String data = readLine().trim();
             isValidInput(data);
+            for (int i = 0; i < 3; ++i) {
+                int n = data.charAt(i) - '0';
+                if (computer.get(i) == n) {
+                    ++score[0];
+                } else if (computer.contains(n)) {
+                    ++score[1];
+                }
+            }
+            if (score[1] != 0) {
+                result += score[1] + "볼";
+            }
+            if (score[0] != 0) {
+                if (!result.isEmpty()) {
+                    result += ' ';
+                }
+                result += score[0] + "스트라이크";
+            }
+            if (result.isEmpty()) {
+                result += "낫싱";
+            }
+            System.out.println(result);
+            result = "";
+            score[0] = 0;
+            score[1] = 0;
         }
     }
 
