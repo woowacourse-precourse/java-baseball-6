661658f7b44b57d598ae154c1112e6948f7c8cb3
Yehyeok Bang <qkddpgur318@gmail.com>
2023-10-25 00:58:01 +0900

    refactor: NumberBaseballGame 객체로 게임을 시작하도록 변경
    - Application에서는 run() 인스턴스 메소드 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,45 +1,10 @@
 package baseball;
 
-import baseball.model.Computer;
-import baseball.model.Player;
-import camp.nextstep.edu.missionutils.Console;
-
-import static baseball.message.ErrorMessages.INVALID_RESTART;
-import static baseball.message.Messages.CHOOSE_RESTART;
-import static baseball.message.Messages.GAME_START;
-import static baseball.message.Messages.INPUT_NUMBERS;
-import static baseball.message.Messages.RESULT_3_STRIKE;
+import baseball.arcade.NumberBaseballGame;
 
 public class Application {
     public static void main(String[] args) {
-        Computer computer = new Computer();
-        Player player = new Player();
-
-        System.out.println(GAME_START);
-
-        while (true) {
-            System.out.print(INPUT_NUMBERS);
-
-            String input = Console.readLine();
-            player.inputNumbers(input);
-
-            String gameResultMessage = computer.getGameResultMessage(player);
-            System.out.println(gameResultMessage);
-
-            if (gameResultMessage.equals(RESULT_3_STRIKE)) {
-                System.out.println(CHOOSE_RESTART);
-                int restart = Integer.parseInt(Console.readLine());
-
-                if (restart == 1) {
-                    computer.generateNumbers();
-                    continue;
-                }
-                if (restart == 2) {
-                    break;
-                }
-
-                throw new IllegalArgumentException(INVALID_RESTART);
-            }
-        }
+        NumberBaseballGame numberBaseballGame = new NumberBaseballGame();
+        numberBaseballGame.run();
     }
 }
---
+++ src/main/java/baseball/arcade/NumberBaseballGame.java
@@ -0,0 +1,45 @@
+package baseball.arcade;
+
+import baseball.model.Computer;
+import baseball.model.Player;
+import camp.nextstep.edu.missionutils.Console;
+
+import static baseball.message.ErrorMessages.INVALID_RESTART;
+import static baseball.message.Messages.CHOOSE_RESTART;
+import static baseball.message.Messages.GAME_START;
+import static baseball.message.Messages.INPUT_NUMBERS;
+import static baseball.message.Messages.RESULT_3_STRIKE;
+
+public class NumberBaseballGame {
+    public void run() {
+        Computer computer = new Computer();
+        Player player = new Player();
+
+        System.out.println(GAME_START);
+
+        while (true) {
+            System.out.print(INPUT_NUMBERS);
+
+            String input = Console.readLine();
+            player.inputNumbers(input);
+
+            String gameResultMessage = computer.generateGameResultMessage(player);
+            System.out.println(gameResultMessage);
+
+            if (gameResultMessage.equals(RESULT_3_STRIKE)) {
+                System.out.println(CHOOSE_RESTART);
+                int restart = Integer.parseInt(Console.readLine());
+
+                if (restart == 1) {
+                    computer.generateNumbers();
+                    continue;
+                }
+                if (restart == 2) {
+                    break;
+                }
+
+                throw new IllegalArgumentException(INVALID_RESTART);
+            }
+        }
+    }
+}
