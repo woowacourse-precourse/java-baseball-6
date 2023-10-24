722c7df43b1502144663711341144522344627c5
gunsookim <ccpc4896@naver.com>
2023-10-24 20:38:09 +0900

    refactor : 메소드 코드 리펙토링
    메소드당 하나의 일만 담당하도록 리펙토링.
    메세지 별로 string 초기화.
    displayResult 생성.
    isNoHits 생성.
    isWinning 생성.
    displayBallCount 생성.
    displayStrikeCount 생성.
    displayWinningMessage 생성.

--- src/main/java/baseball/ShowResult.java
+++ src/main/java/baseball/ShowResult.java
@@ -2,22 +2,50 @@ package baseball;
 
 class ShowResult {
     private final int numberOfDigits;
+    private static final String NO_HITS_MESSAGE = "낫싱";
+    private static final String BALL_MESSAGE = "%d볼 ";
+    private static final String STRIKE_MESSAGE = "%d스트라이크";
+    private static final String WIN_MESSAGE = "%d개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
     public ShowResult(int numberOfDigits) {
         this.numberOfDigits = numberOfDigits;
     }
+
     // 볼, 스트라이크 계산 후 결과 출력
-    public void result(int strike, int ball) {
-        if (strike == 0 && ball == 0) {
-            System.out.println("낫싱");
-        } else {
-            if (ball > 0) System.out.print(ball + "볼 ");
-            if (strike > 0) System.out.print(strike + "스트라이크");
-            System.out.println();
+    public void displayResult(int strike, int ball) {
+        if (isNoHits(strike, ball)) {
+            System.out.println(NO_HITS_MESSAGE);
+            return;
+        }
+        displayBallCount(ball);
+        displayStrikeCount(strike);
+        System.out.println();
+        if (isWinning(strike)){
+            displayWinningMessage(strike);
         }
     }
 
-    public void win(int strike) {
-        System.out.println(strike + "스트라이크");
-        System.out.println(numberOfDigits + "개의 숫자를 모두 맞히셨습니다! 게임 종료"); // all strike인 경우 게임 종료 문구 출력
+    private boolean isNoHits(int strike, int ball) {
+        return strike == 0 && ball == 0;
+    }
+
+    private boolean isWinning(int strike) {
+        return strike == numberOfDigits;
+    }
+
+    private void displayBallCount(int ball) {
+        if (ball > 0) {
+            System.out.printf(BALL_MESSAGE, ball);
+        }
+    }
+
+    private void displayStrikeCount(int strike) {
+        if (strike > 0) {
+            System.out.printf(STRIKE_MESSAGE, strike);
+        }
+    }
+
+    public void displayWinningMessage(int strike) {
+        System.out.printf(WIN_MESSAGE + "%n", numberOfDigits); // all strike인 경우 게임 종료 문구 출력
     }
 }
