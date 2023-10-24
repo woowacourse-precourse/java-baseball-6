4ad13b8a37bdf96894798424b6af9b842e312145
cyPark95 <parkcy1007@gmail.com>
2023-10-24 23:16:14 +0900

    feat: 야구공 숫자를 비교하여 힌트 생성
    - 사용자가 입력한 숫자와 컴퓨터의 숫자를 비교하여 힌트 생성

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -2,6 +2,8 @@ package baseball;
 
 import static baseball.view.InputView.getNumbers;
 
+import baseball.domain.BallStatus;
+import baseball.domain.Balls;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,12 +11,34 @@ import utils.ValidationUtils;
 
 public class BaseballGame {
 
-    public void play() {
-        List<Integer> computer = createComputer();
-        List<Integer> balls = getBallNumbers();
+    private static final int BOARD_SIZE = 3;
+    private static final int BALL_SIZE = 3;
+
+    private int[] board;
+
+    public void start() {
+        Balls computer = createComputerBalls();
+        initBoard();
+        play(computer);
     }
 
-    List<Integer> createComputer() {
+    private Balls createComputerBalls() {
+        return new Balls(createComputer());
+    }
+
+    private void initBoard() {
+        board = new int[BOARD_SIZE];
+    }
+
+    private void play(Balls computer) {
+        List<Integer> balls = getBallNumbers();
+        for (int i = 0; i < BALL_SIZE; i++) {
+            int result = computer.compare(balls.get(i), i);
+            board[result]++;
+        }
+    }
+
+    private List<Integer> createComputer() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -51,13 +75,13 @@ public class BaseballGame {
         for (Integer number : numbers) {
             validateNumber(number);
         }
-        if (ValidationUtils.isOtherNumbers(numbers)) {
+        if (!ValidationUtils.isOtherNumbers(numbers)) {
             throw new IllegalArgumentException("잘못된 입력입니다. 서로 다른 숫자를 입력해 주세요.");
         }
     }
 
     private static void validateNumber(Integer number) {
-        if (ValidationUtils.isSingleNumber(number)) {
+        if (!ValidationUtils.isSingleNumber(number)) {
             throw new IllegalArgumentException("잘못된 입력입니다. 1-9 사이의 숫자를 입력해 주세요.");
         }
     }
