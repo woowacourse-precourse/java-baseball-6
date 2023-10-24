02d505e362e2125d14d62cf602988109a05bf0a5
ksw13 <ksw48483@gmail.com>
2023-10-24 14:38:48 +0900

    refactor: 문자열 연결 성능 개선

--- src/main/java/baseball/model/RandomGenerator.java
+++ src/main/java/baseball/model/RandomGenerator.java
@@ -6,15 +6,15 @@ import java.util.List;
 
 public class RandomGenerator {
     public String createNumbers() {
-        String selectedNumbers = "";
+        StringBuilder selectedNumbers = new StringBuilder();
         List<Integer> selectedList = new ArrayList<>();
         while (selectedList.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!selectedList.contains(randomNumber)) {
                 selectedList.add(randomNumber);
-                selectedNumbers += randomNumber;
+                selectedNumbers.append(randomNumber);
             }
         }
-        return selectedNumbers;
+        return selectedNumbers.toString();
     }
 }
