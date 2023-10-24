0f45dce92d7cecc643a0b9f90a4e4235039a3bf3
sudhdkso <waga1009@gmail.com>
2023-10-24 21:43:00 +0900

    feat: 사용자 숫자 중복 입력시 에러를 return하도록 기능 추가

--- src/main/java/baseball/domain/User.java
+++ src/main/java/baseball/domain/User.java
@@ -1,10 +1,12 @@
 package baseball.domain;
 
+import java.util.*;
+
 public class User {
 
     public int[] parseUserNum(String userNum, int size){
 
-        checkUserNumSize(userNum, size);
+        checkException(userNum, size);
 
         int[] user = new int[size];
 
@@ -15,16 +17,28 @@ public class User {
         return user;
     }
 
-    public void checkUserNumSize(String userNum, int size){
-        if(userNum.length() != size){
-            throw new IllegalArgumentException();
-        }
-    }
-
     public int getNumbericValue(char ch){
         if(ch >= '1' && ch <= '9'){
             return Character.getNumericValue(ch);
         }
-        throw new IllegalArgumentException();
+        throw new IllegalArgumentException("1-9사이의 숫자만 입력해주세요.");
+    }
+
+    public void checkException(String userNum, int size){
+        checkUserNumSize(userNum,  size);
+        checkDuplicateNum(userNum, size);
+    }
+
+    public void checkUserNumSize(String userNum, int size){
+        if(userNum.length() != size){
+            throw new IllegalArgumentException("3자리의 숫자만 입력해주세요.");
+        }
+    }
+
+    public void checkDuplicateNum(String userNum, int size){
+        Set<String> userSet = new HashSet<>(Arrays.asList(userNum.split("")));
+        if(userSet.size() != size){
+            throw new IllegalArgumentException("중복된 숫자가 존재합니다.");
+        }
     }
 }
