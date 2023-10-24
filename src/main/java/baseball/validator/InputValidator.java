4fdbcd983fc13af957c5951e3371d7872bc663f3
CJY <c0512jy@naver.com>
2023-10-25 00:59:28 +0900

    docs: 주석 추가
    주석을 추가했습니다.

--- src/main/java/baseball/validator/InputValidator.java
+++ src/main/java/baseball/validator/InputValidator.java
@@ -5,13 +5,20 @@ public class InputValidator {
     private InputValidator() {
     }
 
+    /**
+     * 사용자의 게임 중 입력값이 유효한지 검증한다.
+     * @param input 사용자 입력값
+     */
     public static void isValidInput(String input) {
-        //예외처리
         validateSize(input);
         validateNumber(input);
         validateDistinct(input);
     }
 
+    /**
+     * 사용자의 재시작 여부 입력값이 유효한지 검증한다.
+     * @param restart 사용자 입력값
+     */
     public static void isValidRestart(String restart) {
         if (!restart.equals("1") && !restart.equals("2")) {
             throw new IllegalArgumentException("1 또는 2만 입력 가능합니다.");
