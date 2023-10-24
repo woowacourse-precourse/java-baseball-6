00ab33db5c981c74b002065f2da8153cfa321e86
chjcoder <chjcoder@gmail.com>
2023-10-24 21:59:15 +0900

    [comment] 필요없는 주석 제거

--- src/main/java/baseball/model/InGame.java
+++ src/main/java/baseball/model/InGame.java
@@ -1,7 +1,6 @@
 package baseball.model;
 
 import baseball.view.Message;
-
 import java.util.List;
 
 public class InGame {
@@ -23,24 +22,19 @@ public class InGame {
         if (strike == THREE_STRIKE) {
             threeStrike = true;
             Message.threeStrikeMessage();
-//            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         }
         return threeStrike;
     }
 
     public static void strikeAndBallMessage(int strike, int ball) {
         if (strike > 0 && ball == 0) {
-//            System.out.println(strike+"스트라이크");
             Message.onlyStrikeMessage(strike);
         } else if (strike == 0 && ball > 0) {
             Message.onlyBallMessage(ball);
-//            System.out.println(ball+"볼");
         } else if (strike > 0 && ball > 0) {
             Message.ballWithStrikeMessage(strike, ball);
-//            System.out.println(ball + "볼 " + strike + "스트라이크");
         } else {
             Message.noBallNoStrikeMessage();
-//            System.out.println("낫싱");
         }
     }
 }
