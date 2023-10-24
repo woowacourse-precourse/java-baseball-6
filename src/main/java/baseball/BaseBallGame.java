a58253e5c1fc47dea75db65b95b9097d562ebb48
DrivenByCode <jaykey1004@gmail.com>
2023-10-24 14:34:49 +0900

    bug: fix bug (game can be terminated))

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -1,15 +1,19 @@
 package baseball;
 
 import baseball.constant.ServiceMessage;
-import baseball.constant.StatusMessage;
 import baseball.counter.Ball;
 import baseball.counter.Strike;
-import baseball.hint.Message;
 import baseball.hint.Printer;
+import baseball.hint.TotalMessage;
+import baseball.input.EndValidator;
 import baseball.input.Validator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class BaseBallGame {
+    public BaseBallGame() {
+        System.out.print(ServiceMessage.START.serviceMessage);
+    }
+
     public void startGame() {
         // 서로 다른 3자리 수를 컴퓨터가 지정한다.
         InitNumber initNumber = new InitNumber();
@@ -18,32 +22,35 @@ public class BaseBallGame {
         System.out.println("answer = " + answer);
 
         while (true) {
-            System.out.print(ServiceMessage.INPUT.serviceMessage + " ");
+            System.out.print(ServiceMessage.INPUT.serviceMessage);
 
             String line = "";
             line = Console.readLine();
-            // 예외 처리 모듈화 필요
-            if (!Validator.isValid(line)) { // 예: 빈 문자열 또는 null을 부적절한 입력으로 처리
-                throw new IllegalArgumentException("입력값이 잘못됐습니다.");
+
+            if (!Validator.isValid(line)) {
+                throw new IllegalArgumentException("입력값이 잘못됐습니다. 1-9까지의 서로 다른 3자리 수를 입력해 주세요.");
             }
 
             int ballCount = Ball.countBalls(answer, line);
             int strikeCount = Strike.countStrike(answer, line);
 
-            String message = Message.getMessage(ballCount, StatusMessage.BALL) + " " + Message.getMessage(strikeCount,
-                    StatusMessage.STRIKE);
+            String message = TotalMessage.sumMessage(ballCount, strikeCount);
+            Printer.print(message);
             if (Printer.isFinished(message)) {
-                Printer.print(message);
                 System.out.print(ServiceMessage.SUCCESS.serviceMessage);
                 System.out.print(ServiceMessage.RETRY.serviceMessage);
-                // 입력값 예외 처리 필요
-                if (Integer.parseInt(Console.readLine()) == 2) {
-                    return;
+
+                line = Console.readLine();
+
+                EndValidator.isValid(line);
+                if (Integer.parseInt(line) == 2) {
+                    Console.close();
+                    break;
                 }
                 startGame();
+                Console.close();
+                break;
             }
-
-            Printer.print(message);
         }
     }
 }
