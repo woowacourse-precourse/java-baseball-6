3e0a9fbfbf11ddb2f4f2be0a32671bcbcc1bc53a
HYUNJUN\sajun <sajun28@naver.com>
2023-10-24 15:02:41 +0900

    실험

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,5 +7,6 @@ public class Application {
     public static void main(String[] args) {
         GameController gameController = new GameController();
         gameController.start();
+        System.out.println(1);
     }
 }
