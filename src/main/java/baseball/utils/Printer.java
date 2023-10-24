29e206b7d50b1383063484a7c51e346bd19ff982
zaeuk <zzaeuk@gmail.com>
2023-10-24 13:46:51 +0900

    refactor: else if, else 예약어 제거 코드 가독성 향상을 위해 else if 및 else 문 제거

--- src/main/java/baseball/utils/Printer.java
+++ src/main/java/baseball/utils/Printer.java
@@ -4,22 +4,30 @@ public class Printer {
     public void printGameResult(int ball, int strike) {
         if (ball == 0 && strike == 0) {
             System.out.println("낫싱");
-        } else if (ball == 0) {
+            return;
+        }
+        if (ball == 0) {
             System.out.printf("%d스트라이크\n", strike);
-        } else if (strike == 0) {
+            return;
+        }
+        if (strike == 0) {
             System.out.printf("%d볼\n", ball);
-        } else {
+            return;
+        }
+        if (ball > 0 && strike > 0) {
             System.out.printf("%d볼 %d스트라이크\n", ball, strike);
         }
     }
-    public void printGameStart(){
+
+    public void printGameStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    public void printGameEnd(){
+    public void printGameEnd() {
         System.out.println("게임이 종료됩니다.");
     }
-    public void printCorrect(){
+
+    public void printCorrect() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 }
