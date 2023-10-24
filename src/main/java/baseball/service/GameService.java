79076931ceeb8a58769bfd8357b7b17559143f08
ghdeo <goldencorn7@naver.com>
2023-10-25 01:09:58 +0900

    feat: Add getUserNumber method

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.*;
 
 public class GameService {
+    private final Scanner scanner = new Scanner(System.in);
     private List<Integer> computer;
 
     public void playGame() {
@@ -21,5 +22,22 @@ public class GameService {
         }
 
         return computer;
+    }
+
+    List<Integer> getUserNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String userInput = scanner.nextLine();
+        List<Integer> userNumber = new ArrayList<>();
+
+        if (userInput.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < 3; i++) {
+            if (userInput.charAt(i) < '1' || userInput.charAt(i) > '9') {
+                throw new IllegalArgumentException();
+            }
+            userNumber.add(userInput.charAt(i) - '0');
+        }
+        return userNumber;
     }
 }
