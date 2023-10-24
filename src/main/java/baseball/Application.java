abc5cb2d5b6b9ba44c12285190d24e8b12f9c02f
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-10-24 23:50:26 +0900

    refactor : 어플리케이션에서 비즈니스 로직 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,24 +1,14 @@
 package baseball;
 
-import java.util.Scanner;
-
-import static baseball.PlayGame.playGame;
+import static baseball.NumberBaseballGame.playGame;
 import static baseball.StartGame.startGame;
-import static baseball.UserDecision.userDecision;
 
 public class Application {
 
   public static void main(String[] args) {
 
-    Scanner sharedScanner = new Scanner(System.in);
-
     startGame();
-    String userDecision;
-
-    do {
-      playGame(sharedScanner);
-      userDecision = userDecision(sharedScanner);
-    } while (userDecision.equals("1"));
+    playGame();
 
   }
 }
