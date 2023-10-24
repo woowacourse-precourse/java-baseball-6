a386a6e453efcc0e0c295afd2974b4e2cf837ba8
Sinorang <aodkrak@naver.com>
2023-10-24 19:25:13 +0900

    같은 수, 스트라이크 수 확인 테스트 코드 작성
    - 컴퓨터의 수와 플레이어의 수를 비교하여 같은 숫자 개수와 스트라이크 수를 확인하는 단위 테스트 코드 작성

---
+++ src/test/java/baseball/CompareNumberTest.java
@@ -0,0 +1,29 @@
+package baseball;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CompareNumberTest {
+    CompareNumber compareNumber;
+
+    @BeforeEach
+    void setUp() {
+        compareNumber = new CompareNumber();
+    }
+
+    @Test
+    @DisplayName("같은 수가 몇 개인지 확인")
+    void count(){
+        assertThat(3).isEqualTo(compareNumber.countSameNumber(Arrays.asList(1,2,3), Arrays.asList(3,1,2)));
+    }
+
+    @Test
+    @DisplayName("스트라이크 개수 확인")
+    void strike() {
+        assertThat(1).isEqualTo(compareNumber.countStrike(Arrays.asList(3,2,4), Arrays.asList(3,1,2)));
+    }
+}
