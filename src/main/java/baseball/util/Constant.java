70b9d5580dda0c9f34ef8f9ced616495e21a8081
Hayoon <hy97@sju.ac.kr>
2023-10-24 20:36:49 +0900

    feat(Constant): 볼/스트라이크 검사 결과 출력 상수

--- src/main/java/baseball/util/Constant.java
+++ src/main/java/baseball/util/Constant.java
@@ -5,4 +5,9 @@ public class Constant {
     public static final String INPUT_NUMBERS = "숫자를 입력해주세요 : ";
     public static final String FINISH_GAME = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     public static final String RESTART_GAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String BALL_AND_STRIKE_FORMAT = "%d볼 %d스트라이크\n";
+    public static final String BALL_FORMAT = "%d볼\n";
+    public static final String STRIKE_FORMAT = "%d스트라이크\n";
+    public static final String NOTHING = "낫싱\n";
+
 }
