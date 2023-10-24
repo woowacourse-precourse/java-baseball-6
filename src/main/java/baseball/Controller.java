9c7e253bd86301d7f5e79fd204732d10a9ea7a58
YoonseolLee <gujueng32@naver.com>
2023-10-24 13:13:46 +0900

    feat: 재시작/종료 문구 출력 및 설정

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -1,4 +1,14 @@
 package baseball;
 
 public class Controller {
+    private static final String RESTART_REQUEST_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String GAME_START = "1";
+    private static final String PROGRAM_EXIT = "2";
+
+    public Controller() {
+    }
+
+    private void printRestartRequestMessage() {
+        System.out.println(RESTART_REQUEST_MESSAGE);
+    }
 }
