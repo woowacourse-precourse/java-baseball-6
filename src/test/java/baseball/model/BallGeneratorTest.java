51971eb09865c0c25e3f503be6bd37fb639ccdce
peeaaccee <wlgp8958@naver.com>
2023-10-25 07:36:18 +0900

    test(BallGenerator): 중복된 수 있는지 검사

--- src/test/java/baseball/model/BallGeneratorTest.java
+++ src/test/java/baseball/model/BallGeneratorTest.java
@@ -3,8 +3,10 @@ package baseball.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BallGeneratorTest {
@@ -20,6 +22,12 @@ class BallGeneratorTest {
     @Test
     void 랜덤_공번호_생성() {
         assertTrue(range.contains(ballGenerator.createRandomBall()));
+    }
+
+    @Test
+    void 컴퓨터_공번호_중복숫자_판별() {
+        List<String> balls = ballGenerator.makeComputerBalls();
+        assertThat(new HashSet<>(balls).size()).isEqualTo(balls.size());
     }
 
 }
