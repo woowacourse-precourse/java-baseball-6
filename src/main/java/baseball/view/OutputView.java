3f49b5e7a6c466875e82d8d6b9d142a926a3f626
kimmokalover <kimmokalover@gmail.com>
2023-10-24 10:44:13 +0900

    feat : Input & Output View class 작성 완료

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,5 +1,15 @@
 package baseball.view;
 
 public class OutputView {
+    public static void ballAndStrike(int ball, int strike){
+        System.out.println(ball + "볼 " + strike + "스트라이크");
+    }
 
+    public static void onlyBall(int ball){
+        System.out.println(ball + "볼");
+    }
+
+    public static void onlyStrike(int strike){
+        System.out.println(strike + "스트라이크");
+    }
 }
