a852d0181ac9b0714a55b24a0c97e5935a5af639
zaeuk <zzaeuk@gmail.com>
2023-10-24 09:34:43 +0900

    refactor: 코드 간결화 분기문 제거를 통해 코드 간결화

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -24,10 +24,7 @@ public class Application {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String input = Console.readLine();
         ValidationUtils.validateOneOrTwo(input);
-        if (input.equals("1")) {
-            return true;
-        }
-        return false;
+        return input.equals("1");
     }
 
 
