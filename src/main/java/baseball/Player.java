a8d0258eb9aa1735c3ec27d7da0eebd545f30848
YoonseolLee <gujueng32@naver.com>
2023-10-24 10:37:09 +0900

    feat: 스트라이크, 볼 카운트 초기화 기능

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,4 +1,17 @@
 package baseball;
 
 public class Player {
+    private static final String SUCCESS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private Computer computer = new Computer();
+    private int strike;
+    private int ball;
+
+    public Player() {
+    }
+
+    public void init() {
+        computer.init();
+        strike = 0;
+        ball = 0;
+    }
 }
