ed62cd978f3026a2fc29101068ba875fc0c37bb4
kmw2378 <rlarla677@gmail.com>
2023-10-24 14:47:57 +0900

    feat: OutputView에 게임 진행에 필요한 문구를 출력하는 메소드 추가 게임 시작/중지 및 재개/종료 시 출력을 하는 메소드 추가하였습니다.

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,4 +1,19 @@
 package baseball.view;
 
+import static baseball.view.message.GameMessage.EXIT_MESSAGE;
+import static baseball.view.message.GameMessage.GAME_OVER_MESSAGE;
+import static baseball.view.message.GameMessage.START_MESSAGE;
+
 public class OutputView {
+    public static void printStartMessage() {
+        System.out.println(START_MESSAGE);
+    }
+
+    public static void printGameOverMessage() {
+        System.out.println(GAME_OVER_MESSAGE);
+    }
+
+    public static void printExitMessage() {
+        System.out.println(EXIT_MESSAGE);
+    }
 }
