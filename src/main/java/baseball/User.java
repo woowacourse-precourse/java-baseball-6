ba776aa9b4492ca0c5f46ec17ee9136cb2034d68
songjayhyun <thewinner1st@naver.com>
2023-10-25 03:13:06 +0900

    fix: user에서 NullPointException 예외 발생
    - userNumbers로 초기화해주면서 수정

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 import java.util.List;
 
 public class User {
@@ -17,13 +18,17 @@ public class User {
     }
 
     public void generateNumbers() {
+        List<Integer> userNumbers = new ArrayList<>();
+
         String userInput = Console.readLine();
         UserInputValidator userInputValidator = new UserInputValidator();
 
         userInputValidator.validateInputNumbers(userInput);
 
         for (char input : userInput.toCharArray()) {
-            numbers.add(Character.getNumericValue(input));
+            userNumbers.add(Character.getNumericValue(input));
         }
+
+        numbers = userNumbers;
     }
 }
