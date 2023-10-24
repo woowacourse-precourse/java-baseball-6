df174981215d7f6073eda89561989719b1a3d198
jihyuk <kwlgur0416@gmail.com>
2023-10-25 01:21:13 +0900

    test : 정답 숫자 생성 기능 테스트 추가 및 구체화

--- src/test/java/baseball/GuessInputConverterTest.java
+++ src/test/java/baseball/GuessInputConverterTest.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -23,7 +24,7 @@ class GuessInputConverterTest {
     void 추측값_정상_로직_테스트() {
         // given
         String input = "123";
-        settingForInputStream(input);
+        command(input);
 
         // when
         List<Integer> guessNumber = guessInputConverter.getValue();
@@ -35,60 +36,69 @@ class GuessInputConverterTest {
     @Test
     void 추측값_타입_예외_테스트() {
         // given
-        String input = "avx\nㅁㄱㄴ\n\"   \"";
-        settingForInputStream(input);
+        String input = "avx";
+        command(input);
 
         // when & then
-        String[] inputList = input.split("\n");
-        for (int i = 0; i < inputList.length; i++) {
+        assertSimpleTest(() ->
             assertThatThrownBy(() -> guessInputConverter.getValue())
-                .isInstanceOf(IllegalArgumentException.class);
-        }
+                .isInstanceOf(IllegalArgumentException.class)
+        );
     }
 
     @Test
-    void 추측값_자리수_예외_테스트() {
+    void 추측값_2자리_예외_테스트() {
         // given
-        String input = "12\n1256\n9246";
-        settingForInputStream(input);
+        String input = "12";
+        command(input);
 
         // when & then
-        String[] inputList = input.split("\n");
-        for (int i = 0; i < inputList.length; i++) {
+        assertSimpleTest(() ->
             assertThatThrownBy(() -> guessInputConverter.getValue())
-                .isInstanceOf(IllegalArgumentException.class);
-        }
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 추측값_4자리_예외_테스트() {
+        // given
+        String input = "9246";
+        command(input);
+
+        // when & then
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> guessInputConverter.getValue())
+                .isInstanceOf(IllegalArgumentException.class)
+        );
     }
 
     @Test
     void 추측값_범위_예외_테스트() {
         // given
-        String input = "102\n012\n9086";
-        settingForInputStream(input);
+        String input = "102";
+        command(input);
 
         // when & then
-        String[] inputList = input.split("\n");
-        for (int i = 0; i < inputList.length; i++) {
+        assertSimpleTest(() ->
             assertThatThrownBy(() -> guessInputConverter.getValue())
-                .isInstanceOf(IllegalArgumentException.class);
-        }
+                .isInstanceOf(IllegalArgumentException.class)
+        );
     }
 
     @Test
     void 추측값_중복_예외_테스트() {
         // given
-        String input = "112\n999\n9774";
-        settingForInputStream(input);
+        String input = "122";
+        command(input);
 
         // when & then
-        String[] inputList = input.split("\n");
-        for (int i = 0; i < inputList.length; i++) {
+        assertSimpleTest(() ->
             assertThatThrownBy(() -> guessInputConverter.getValue())
-                .isInstanceOf(IllegalArgumentException.class);
-        }
+                .isInstanceOf(IllegalArgumentException.class)
+        );
     }
 
-    private void settingForInputStream(String input) {
+    private void command(String input) {
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
     }
