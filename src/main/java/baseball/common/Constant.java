62aef3f541495a0830ff3a944a78ee90a1f74ca6
05AM <anytime0224@gmail.com>
2023-10-24 22:11:59 +0900

    [FEAT] 상수 선언

---
+++ src/main/java/baseball/common/Constant.java
@@ -0,0 +1,12 @@
+package baseball.common;
+
+public class Constant {
+    public static final Integer ZERO = 0;
+
+    public static final Integer MIN_NUMBER_RANGE = 1;
+    public static final Integer MAX_NUMBER_RANGE = 9;
+    public static final Integer MAX_STRIKE_NUMBER = 3;
+
+    public static final Integer VALID_ANSWER_SIZE = 3;
+    public static final Integer VALID_INPUT_SIZE = 3;
+}
