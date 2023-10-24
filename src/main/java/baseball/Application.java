604cb80a081727e7f4f04cdd5e7a00b565396928
ehdwoKIM <kinbell19@gmail.com>
2023-10-24 22:43:32 +0900

    [feat] 게임 재시작 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,13 +1,25 @@
 package baseball;
 
 import baseball.controllers.GameController;
+import baseball.models.RegameNumber;
+import baseball.views.InputView;
 import baseball.views.OutputView;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         OutputView.printGameStart();
-        GameController baseballGame = new GameController();
-        baseballGame.start();
+        do {
+            GameController baseballGame = new GameController();
+            baseballGame.start();
+        } while (askRegame());
+    }
+
+    public static boolean askRegame() {
+        RegameNumber regameNumber = new RegameNumber(InputView.setRegameNumber());
+        if (regameNumber.getRegameNumber().equals("1")) {
+            return true;
+        }
+        return false;
     }
 
 }
