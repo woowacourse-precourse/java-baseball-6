e820313393cd28c391322c1581b726a1b6ef2295
lee-JunR <wns2610@icloud.com>
2023-10-24 21:14:47 +0900

    style : "주석 추가"

--- src/main/java/baseball/Score.java
+++ src/main/java/baseball/Score.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 public class Score {
 
+  /**
+   플레이어 숫자와 컴퓨터 숫자를 비교하여 볼과 스트라이크 개수를 계산합니다.
+   @param playerNumbers 플레이어의 숫자 목록입니다.
+   @param computerNumbers 컴퓨터의 숫자 목록입니다.
+   @return 볼 개수는 인덱스 0에, 스트라이크 개수는 인덱스 1에 담긴 정수 배열을 반환합니다. */
   public int[] calculateScore(List<Integer> playerNumbers, List<Integer> computerNumbers) {
     int ballCount = 0;
     int strikeCount = 0;
-    System.out.println("bc, sc 초기화");
     // 플레이어의 입력과 컴퓨터의 숫자를 비교하여 볼과 스트라이크 개수를 계산
     for (int i = 0; i < computerNumbers.size(); i++) {
       if (computerNumbers.get(i) == playerNumbers.get(i)) {
