baf88655efbad01d50044fafaa80e35c4844cb39
DryLeonhard <ygangmin@gmail.com>
2023-10-25 01:13:31 +0900

    Feat : Ball 객체에서 랜덤 목표 숫자를 설정하고, 체크하는 로직

---
+++ src/main/java/baseball/ball/Ball.java
@@ -0,0 +1,52 @@
+package baseball.ball;
+
+import baseball.random.Pick;
+import baseball.random.RandomUtil;
+
+import java.util.List;
+
+public class Ball {
+    private static final Ball instance = new Ball();
+
+    private final RandomUtil randomUtil = Pick.getInstance();
+
+    private Ball() {
+
+    }
+
+    public static Ball getInstance() {
+        return instance;
+    }
+
+    private List<Integer> target;
+
+    public List<Integer> updateTarget() {
+        target = null;
+
+        return target = randomUtil.getBallNumber();
+    }
+
+    public List<Integer> getTarget() {
+        return target;
+    }
+
+    public Result checkNumber(int num) {
+        int nBall = 0, nStrike = 0;
+
+        char[] arr = String.valueOf(num).toCharArray();
+        if (arr.length != 3) throw new IllegalArgumentException();
+
+        for (int i = 0; i < 3; i++) {
+            int temp = arr[i] - 48;
+            if (target.contains(temp)) {
+                if (target.indexOf(temp) == i) {
+                    nStrike++;
+                } else {
+                    nBall++;
+                }
+            }
+        }
+        return new Result(nBall, nStrike);
+    }
+
+}
