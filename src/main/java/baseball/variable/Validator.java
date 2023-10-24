832ef5af3a718ce2c5f689e3411eaf57a9fa0cfb
soyoungxxx <otherplet@gmail.com>
2023-10-24 17:49:49 +0900

    style(Validator): 오타 수정

--- src/main/java/baseball/variable/Validator.java
+++ src/main/java/baseball/variable/Validator.java
@@ -6,15 +6,15 @@ import java.util.Set;
 
 public class Validator {
     private static final String NOT_THREE = "세 자리 숫자가 아닙니다.";
-    private static final String INCLUE_ZERO = "1 ~ 9 사이의 값을 입력해주세요. 0은 입력할 수 없습니다.";
+    private static final String INCLUDE_ZERO = "1 ~ 9 사이의 값을 입력해주세요. 0은 입력할 수 없습니다.";
     private static final String DUPLICATE = "중복된 숫자는 입력할 수 없습니다.";
 
     public void isValid(List<Integer> number) {
         if (!isThreeNumbers(number)) {
             throw new IllegalArgumentException(NOT_THREE);
         }
-        if (isZeroInclue(number)) {
-            throw new IllegalArgumentException(INCLUE_ZERO);
+        if (isZeroInclude(number)) {
+            throw new IllegalArgumentException(INCLUDE_ZERO);
         }
         if (isDuplicate(number)) {
             throw new IllegalArgumentException(DUPLICATE);
