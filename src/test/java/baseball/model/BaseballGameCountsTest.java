65b84be38a6a84fe960e4482f317fe55d53e970b
oxdjww <oxdjww@gmail.com>
2023-10-25 03:08:45 +0900

    test : 테스트를 위한 생성자 추가에 따른 코드 수정 및 나머지 테스트 코드 구현 완료

--- src/test/java/baseball/model/BaseballGameCountsTest.java
+++ src/test/java/baseball/model/BaseballGameCountsTest.java
@@ -1,9 +1,9 @@
 package baseball.model;
 
+import static baseball.model.BaseballGameCounts.BALL;
 import static baseball.model.BaseballGameCounts.STRIKE;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import java.util.stream.Collectors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,14 +12,9 @@ class BaseballGameCountsTest {
     @DisplayName("컴퓨터 번호와 동일한 유저 번호를 대입한 BasballGameCounts 객체가 3스트라이크가 되는지 테스트")
     public void 카운트_생성자_3스트라이크_테스트() throws Exception {
         // given
-        ComputerNumber computer = new ComputerNumber();
+        ComputerNumber computer = new ComputerNumber("123");
 
-        UserNumber user = new UserNumber(
-                // computerNumber의 List -> String화
-                computer.getComputerNumber().stream()
-                        .map(Object::toString)
-                        .collect(Collectors.joining())
-        );
+        UserNumber user = new UserNumber("123");
 
         // when
         BaseballGameCounts counts = new BaseballGameCounts(computer, user);
@@ -31,16 +26,44 @@ class BaseballGameCountsTest {
 
 
     @Test
+    @DisplayName("스트라이크와 볼이 둘다 들어왔을 때, counts에 올바른 값이 들어간다.")
+    public void 스트라이크_볼_테스트() throws Exception {
+        // given
+        ComputerNumber computer = new ComputerNumber("123");
+
+        UserNumber user = new UserNumber("136");
+
+        // when
+        BaseballGameCounts counts = new BaseballGameCounts(computer, user);
+
+        // then
+        assertThat(counts.getCounts().get(BALL)).isEqualTo(1);
+        assertThat(counts.getCounts().get(STRIKE)).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("스트라이크/볼이 없을 때 카운트는 0,0으로 들어간다.")
+    public void 낫싱_테스트() throws Exception{
+        // given
+        ComputerNumber computer = new ComputerNumber("123");
+
+        UserNumber user = new UserNumber("456");
+
+        // when
+        BaseballGameCounts counts = new BaseballGameCounts(computer, user);
+
+        // then
+        assertThat(counts.getCounts().get(BALL)).isEqualTo(0);
+        assertThat(counts.getCounts().get(STRIKE)).isEqualTo(0);
+    }
+
+    @Test
     @DisplayName("승리 조건(3스트라이크)을 가진 BaseballGameCounts 객체가 승리조건을 만족하는지 확인한다.")
     public void 카운트_승리조건_테스트() throws Exception {
         // given
-        ComputerNumber computer = new ComputerNumber();
+        ComputerNumber computer = new ComputerNumber("123");
 
-        UserNumber user = new UserNumber(
-                computer.getComputerNumber().stream()
-                        .map(Object::toString)
-                        .collect(Collectors.joining())
-        );
+        UserNumber user = new UserNumber("123");
 
         // when
         BaseballGameCounts counts = new BaseballGameCounts(computer, user);
