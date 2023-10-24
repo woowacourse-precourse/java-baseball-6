fd36bc742c1d63ba0e5c5cbe4703c952760effa9
qlido <jason7530@naver.com>
2023-10-24 12:09:57 +0900

    Feat : 서로 다른 3가지의 숫자를 List에 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,19 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Integer> systemNumber = new ArrayList<>();
+        while (systemNumber.size() < 3) {
+            Integer number = Randoms.pickNumberInRange(0, 9);
+            if (!systemNumber.contains(number)) {
+                systemNumber.add(number);
+            }
+        }
+
+
     }
 }
