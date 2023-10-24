d1767b576b473f635eff5b668e4d329ab6fa5f17
lesw <lesw1216@gmail.com>
2023-10-24 17:19:39 +0900

    feat: insert baseballMachine in main

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import baseball.baseballmachine.BaseballMachine;
+import baseball.computer.Computer;
+import baseball.player.Player;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalArgumentException{
         // TODO: 프로그램 구현
+        BaseballMachine baseballMachine = new BaseballMachine(new Computer(), new Player());
+        baseballMachine.run();
     }
 }
