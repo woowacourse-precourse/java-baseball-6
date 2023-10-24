9296bbefe107ad94a35593a85106efeee4a90bed
박흥영 <keung@BBookE.local>
2023-10-24 10:23:31 +0900

    rename: 유저 숫자 입력 함수 numInput 작성

--- src/main/java/baseball/numInput.java
+++ src/main/java/baseball/numInput.java
@@ -1,5 +1,20 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class numInput {
 
+    List<Integer> userNum() throws IllegalArgumentException {
+        List<Integer> userInput = new ArrayList<>();
+        System.out.print("숫자를 입력해주세요 : ");
+        String st = Console.readLine();
+        String[] divide = st.split("");
+        for (int i = 0; i < 3; i++) {
+            userInput.add(Integer.parseInt(divide[i]));
+        }
+        return userInput;
+    }
 }
