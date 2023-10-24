d8c7d1e0cd3f2232a3a1f6d72f35a5e3ccd9e207
InJun2 <os2864@naver.com>
2023-10-24 21:31:57 +0900

    :sparkles: BaseBallRun 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+
+import baseball.run.BaseBallRun;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseBallRun baseBallRun = new BaseBallRun();
+        baseBallRun.run();
     }
 }
