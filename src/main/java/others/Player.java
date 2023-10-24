0916c9314472201eeb2a01b21fcd3c73787c1de5
Seungjida <esg0107@pusan.ac.kr>
2023-10-24 16:04:53 +0900

    Refactor: "사용자 클래스를 만들고 예측 숫자를 입력 받는 메서드를 생성"ㅐ

--- src/main/java/others/Player.java
+++ src/main/java/others/Player.java
@@ -1,4 +1,9 @@
 package others;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Player {
+    public static String playerInputGuessingNumber() {
+        return Console.readLine().trim().replaceAll(" ", "");
+    }
 }
