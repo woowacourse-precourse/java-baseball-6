2cf56350ee85a56f3240e8b1bd1da154dc9c2da2
Han <yulmam1@naver.com>
2023-10-24 11:21:34 +0900

    feat(domain) : Score, TargetNumber, UserNumber 클래스 생성 및 구현합니다

--- src/main/java/baseball/model/Score.java
+++ src/main/java/baseball/model/Score.java
@@ -1,2 +1,29 @@
-package baseball.model;public class Score {
+package baseball.model;
+
+public class Score {
+
+    private int ballCount;
+    private int strikeCount;
+
+    public Score() {
+        ballCount = 0;
+        strikeCount = 0;
+    }
+    public int getBallCount(){
+        return ballCount;
+    }
+    public int getStrikeCount() {
+        return strikeCount;
+    }
+    public void resetScore() {
+        strikeCount = 0;
+        ballCount = 0;
+    }
+    public void incStrikeCount() {
+        strikeCount += 1;
+    }
+    public void incBallCount() {
+        ballCount += 1;
+    }
+
 }
--- src/main/java/baseball/model/UserNumber.java
+++ src/main/java/baseball/model/UserNumber.java
@@ -1,2 +1,57 @@
-package baseball.model;public class UserNumber {
+package baseball.model;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserNumber {
+    private List<Integer> userNumber;
+
+    public UserNumber(String userNumber) {
+        isValid(userNumber);
+        this.userNumber =  Arrays.stream(userNumber.split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    public List<Integer> getUserNumber() {
+        return userNumber;
+    }
+
+    private void isValid(String userNumber) {
+        isCorrectSize(userNumber);
+        isInteger(userNumber);
+        isContainZero(userNumber);
+        isDupicate(userNumber);
+    }
+
+    private void isCorrectSize(String userNumber) {
+        if(userNumber.length()!=3)
+            throw new IllegalArgumentException();
+    }
+
+    private void isInteger(String userNumber) {
+        try {
+            Integer.parseInt(userNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void isContainZero(String userNumber) {
+        if(userNumber.contains("0"))
+            throw new IllegalArgumentException();
+    }
+
+
+    private void isDupicate(String userNumber) {
+        int hashset = Arrays.asList(userNumber.split("")).stream()
+                .distinct()
+                .collect(Collectors.joining()).length();
+        if(!(hashset==3))
+            throw new IllegalArgumentException();
+    }
+
+
 }
