d6cd4a86d13231f0a241bb184a3b3e17c9843e14
sj <hera4224@naver.com>
2023-10-24 22:17:43 +0900

    [Feat]야구 게임 구현 완료

--- src/main/java/baseball/controller/BaseballGame.java
+++ src/main/java/baseball/controller/BaseballGame.java
@@ -1,4 +1,73 @@
 package baseball.controller;
+import baseball.model.ComputerNumber;
+import baseball.util.NumberCompare;
+import baseball.model.UserNumber;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 //전체적인 게임 진행
 public class BaseballGame {
+    public static void startGame() {
+        OutputView.printGameStart();
+        String comNumber = ComputerNumber.makeComNum();
+        continueGame(comNumber);
+    }
+
+    public static void continueGame(String comNumber){
+        String userNumber = UserNumber.userNumber();
+        int[] result = NumberCompare.resultBallStrike(userNumber, comNumber);
+
+        if (result[1] != 3) {
+            printBallStrikeResult(result[0], result[1]);
+            printStrikeResult(result[0], result[1]);
+            printBallResult(result[0], result[1]);
+            printNothingResult(result[0], result[1]);
+            continueGame(comNumber);
+        } else {
+            finishGame(result[1]);
+        }
+    }
+
+    public static void printNothingResult(int ball, int strike) {
+        if(ball == 0 && strike == 0){
+                OutputView.printNothingMessage();
+        }
+    }
+
+    public static void printStrikeResult(int ball, int strike) {
+        if(ball == 0 && strike != 0){
+            OutputView.printCount(strike);
+            OutputView.printStrikeMessage();
+        }
+    }
+
+    public static void printBallResult(int ball, int strike) {
+        if(ball != 0 && strike == 0){
+            OutputView.printCount(ball);
+            OutputView.printBallMessage();
+        }
+    }
+
+    public static void printBallStrikeResult(int ball, int strike) {
+        if(ball != 0 && strike != 0){
+                OutputView.printCount(ball);
+                OutputView.printBallMessage();
+                OutputView.printCount(strike);
+                OutputView.printStrikeMessage();
+        }
+    }
+
+    public static void finishGame(int strike) throws IllegalArgumentException {
+        OutputView.printCount(strike);
+        OutputView.printStrikeMessage();
+        OutputView.printGameEnd();
+        String ask = InputView.askForRestart();
+        if (!ask.equals("1") && !ask.equals("2")) {
+            throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.");
+        }
+        if (ask.equals("1")) {
+            startGame();
+        }
+    }
+
 }
--- src/main/java/baseball/model/ComputerNumber.java
+++ src/main/java/baseball/model/ComputerNumber.java
@@ -5,8 +5,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Random {
-    public static List<Integer> ComNum(){
+public class ComputerNumber {
+    public static String makeComNum(){
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -14,6 +14,11 @@ public class Random {
                 computer.add(randomNumber);
             }
         }
-        return computer;
+
+        String result = "";
+        for (int num : computer) {
+            result += num;
+        }
+        return result;
     }
 }
