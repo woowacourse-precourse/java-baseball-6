31eabcb3cddd265e5fb95b362d5a6e2f805b7e0c
박승민 <tmddudals369@naver.com>
2023-10-24 18:41:35 +0900

    feat(outputView): 게임 종료 메세지를 출력하는 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -6,6 +6,7 @@ public class OutputView {
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String USER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String GAME_OVER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     public void printGameStartMessage() {
         System.out.println(GAME_START_MESSAGE);
@@ -34,5 +35,9 @@ public class OutputView {
 
     public void printGameRestartMessage() {
         System.out.println(GAME_RESTART_MESSAGE);
+    }
+
+    public void printGameOver() {
+        System.out.println(GAME_OVER_MESSAGE);
     }
 }
