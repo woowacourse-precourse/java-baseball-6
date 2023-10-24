f9bdb3e076e34fd7579c3d577440cee0e708063a
34suuuuu <psh7492@gmail.com>
2023-10-24 21:12:31 +0900

    feat: Player 기능 구현

--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -1,2 +1,28 @@
-package player;public class Player {
+package player;
+
+import constants.MessageConstants;
+import view.Input;
+
+import static constants.MessageConstants.*;
+import static constants.NumConstants.*;
+import static view.Input.readOption;
+
+
+public class Player {
+
+    public Player(){}
+
+    public String start(){
+        String inputString = Input.readInput();
+        chkInputString(inputString);
+        return inputString;
+    }
+
+    private void chkInputString(String inputString){
+        if (inputString.length() != INPUT_LENGTH) {
+            throw new IllegalStateException(INPUT_LENGTH_ERROR_MESSAGE);
+        }
+        // 예외처리 추가
+    }
+
 }
