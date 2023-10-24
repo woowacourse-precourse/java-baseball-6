c1818b7a5b2a4b6c35b4988c61d424ddbd5f5ea6
lomayd <jinho10167@gmail.com>
2023-10-25 04:39:01 +0900

    comment: 게임 기능 및 필요 메소드

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -16,11 +16,13 @@ public class GameService {
     GameOutputView gameOutputView = new GameOutputView();
     boolean[] isStrike;
 
+    // 게임 준비: 컴퓨터 임의의 세 자리 숫자 생성
     public void setGame() {
         gameOutputView.printStartGame();
         game = new Game(randomUtils.getGameNumber());
     }
 
+    // 게임 시작: 스트라이크 3개 뜰 때 까지 진행
     public void playGame() {
         int strike = 0;
 
@@ -31,6 +33,7 @@ public class GameService {
         }
     }
 
+    // 게임 진행: 사용자로부터 입력값 받은 후 검증, 점수 계산
     public void play() {
         game.initGame();
         gameOutputView.printInputNumber();
@@ -38,10 +41,12 @@ public class GameService {
         computeScore(game.getGameNumber(), user.getUserNumber());
     }
 
+    // 게임 종료: 게임 종료 문구 출력
     public void endGame() {
         gameOutputView.printGameOver();
     }
 
+    // 게임 재시작: 입력값을 받아 게임 재시작 또는 종료하기
     public int retryGame() throws IllegalArgumentException{
         gameOutputView.printRetryGame();
         int number = parser.parseUserInput(gameInputView.getUserInput(),1)[0];
@@ -52,6 +57,7 @@ public class GameService {
         return number;
     }
 
+    // 각각의 숫자값마다 비교
     public void computeScore(int[] gameNumber, int[] userNumber) {
         isStrike = new boolean[3];
         for(int i=0; i<gameNumber.length; i++) {
@@ -59,6 +65,7 @@ public class GameService {
         }
     }
 
+    // 숫자값 비교 후 스트라이크, 볼 결정
     public void compareNumber(int[] gameNumber, int[] userNumber, int index) {
         for(int i=0; i<gameNumber.length; i++) {
             if(userNumber[index] == gameNumber[i] && index == i) {
--- src/main/java/baseball/utils/Parser.java
+++ src/main/java/baseball/utils/Parser.java
@@ -2,26 +2,31 @@ package baseball.utils;
 
 public class Parser {
 
+    // 입력값받아 크기, 숫자 검증 후 정수 배열로 변환
     public int[] parseUserInput(String input, int size) {
         checkSize(input, size);
         return parseInt(input, size);
     }
 
-    private void checkSize(String input, int size) {
+    // 입력값 크기 검증
+    private void checkSize(String input, int size) throws IllegalArgumentException {
         if(input.length() != size) {
             throw new IllegalArgumentException();
         }
     }
 
+    // 입력값 숫자 검증
     private boolean checkDigit(String input, int index) {
         return input.charAt(index) >= '1' && input.charAt(index) <= '9';
     }
 
+    // 문자를 정수로 변환
     private int convertInt(char c) {
         return c - '0';
     }
 
-    private int[] parseInt(String input, int size) {
+    // 입력값 숫자 검증 후 정수 배열로 변환
+    private int[] parseInt(String input, int size) throws IllegalArgumentException {
         int[] numbers = new int[size];
 
         for(int i=0; i<size; i++) {
