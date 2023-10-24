e801226f19da922a99aa7fe32c4b09b19360f1f1
2020006026 <jms0538@hanyang.ac.kr>
2023-10-24 19:14:57 +0900

    form: Add a comment docs: Add a content README

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,7 +9,7 @@ public class Application {
         BaseballGameController baseballGameController = new BaseballGameController();
         BaseballGameView baseballGameView = new BaseballGameView();
 
-        baseballGameView.startView();
+        baseballGameView.startView(); // 첫 시작에만 나오기 때문에 컨트롤러 밖에서 한 번 호출
         restart = baseballGameController.startGame(); // 컨트롤러를 통해서 게임 시작
 
         while (restart) { // 게임 종료 후 반환 값을 받아서 재시작인지 아닌지 판단
