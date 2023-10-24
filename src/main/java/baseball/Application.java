f236a71abcd9c5a201c0e93402a22a32c693ffbe
kijen723 <kij2646@gmail.com>
2023-10-24 21:20:09 +0900

    feat: 랜덤 숫자 설정 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,29 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    public static List<Integer> getRandomNumber() {
+        List<Integer> randomNumberList = new ArrayList<>();
+        while (randomNumberList.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!randomNumberList.contains(randomNumber)) {
+                randomNumberList.add(randomNumber);
+            }
+        }
+
+        return randomNumberList;
+    }
+
     public static void main(String[] args) {
         // TODO1: 게임시작 메세지 출력
         System.out.println("숫자 야구 게임을 시작합니다.");
+
+        // TODO2: 랜덤 숫자 설정
+        List<Integer> computer = getRandomNumber();
 
     }
 }
