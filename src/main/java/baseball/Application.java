2a2e9732358bb6854c4db64856d22ee43b7c10da
minisyu <iswater.kim@gmail.com>
2023-10-24 11:25:19 +0900

    refactor(Application): 3차 리팩토링 완료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,8 @@
 package baseball;
 
 import baseball.controller.GameController;
-import baseball.domain.ComputerNumberGenerator;
+import baseball.controller.InputController;
+import baseball.domain.game.ComputerNumberGenerator;
 import baseball.util.RandomNumberGenerator;
 import baseball.view.InputView;
 import baseball.view.OutputView;
@@ -10,10 +11,11 @@ public class Application {
     public static void main(String[] args) {
         OutputView outputView = new OutputView();
         InputView inputView = new InputView(outputView);
+        InputController inputController = new InputController(inputView);
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator(1, 9);
         ComputerNumberGenerator randomComputerNumberGenerator = new ComputerNumberGenerator(
                 randomNumberGenerator);
-        GameController gameController = new GameController(outputView, inputView, randomComputerNumberGenerator);
+        GameController gameController = new GameController(outputView, inputController, randomComputerNumberGenerator);
         gameController.start();
     }
 }
