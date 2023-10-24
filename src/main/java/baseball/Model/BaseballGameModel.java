21ef72b1779292196629d485c24ba66861b8a65c
jiixon <jr0107380@gmail.com>
2023-10-25 02:28:42 +0900

    feat: 랜덤 숫자 생성

--- src/main/java/baseball/Model/BaseballGameModel.java
+++ src/main/java/baseball/Model/BaseballGameModel.java
@@ -1,2 +1,21 @@
-package baseball.Model;public class BaseballGameModel {
+package baseball.Model;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseballGameModel {
+    //랜덤 숫자 생성하기
+    public void createRandomNumbers(){
+        List<Integer> numbers = new ArrayList<>();
+
+        while (numbers.size()<3){
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+    }
+
 }
