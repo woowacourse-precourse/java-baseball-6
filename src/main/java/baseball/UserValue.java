50a0015fc624fb11c7059cadbb953e6e528330b3
yoonseo choi <choiyoonseo02@gmail.com>
2023-10-24 20:21:39 +0900

    [추가] 잘못된 값을 입력할 경우 예외처리

--- src/main/java/baseball/UserValue.java
+++ src/main/java/baseball/UserValue.java
@@ -1,10 +1,43 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.HashSet;
+import java.util.Set;
 
 public class UserValue {
+    private static String userNumberString;
+
     public static int getUserInput() {
         System.out.print("숫자를 입력해주세요 : ");
         return Integer.parseInt(Console.readLine());
+    }
+
+    public static void validateNumber(int userNumber) {
+        validateIsThreeDigitNumber(userNumber);
+        validateNoDuplicateDigits(userNumber);
+        validateNoZeroDigits();
+    }
+
+    static void validateIsThreeDigitNumber(int userNumber) {
+        if (userNumber < 100 || userNumber > 999) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    static void validateNoDuplicateDigits(int userNumber) {
+        userNumberString = String.valueOf(userNumber);
+        Set<Character> digits = new HashSet<>();
+
+        for (char digit : userNumberString.toCharArray()) {
+            if (!Character.isDigit(digit) || !digits.add(digit)) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    static void validateNoZeroDigits() {
+        if (userNumberString.contains("0")) {
+            throw new IllegalArgumentException();
+        }
     }
 }
