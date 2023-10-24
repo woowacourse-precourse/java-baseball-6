a6de1a634e1178699a51b2f77bd1cf9421aef67f
chahyunsoo <chahs1012@naver.com>
2023-10-24 22:31:41 +0900

    feat : 랜덤 숫자 3개를 생성하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    private static final int MAX_NUM = 9;
+    private static final int NUM_SIZE = 3;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
+    private static List<Integer> makeRandomNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < NUM_SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(1, MAX_NUM);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+        return numbers;
+    }
+
+
 }
