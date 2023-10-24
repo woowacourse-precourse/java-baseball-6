85c14089f46d21c3b73278c8612e3bd9955c4ee9
MrKeeplearning <jaeseok1115@gmail.com>
2023-10-24 20:45:47 +0900

    feat: printHint 메서드 수정
    - feat: 볼이 0이거나 스트라이크가 0인 경우도 모두 함께 힌트로 제공되었던
      것을 0인 경우 표시하지 않도록 조건을 추가함

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -123,6 +123,17 @@ public class Application {
             System.out.println("낫싱");
             return;
         }
+
+        if (strike == 0) {
+            System.out.printf("%d볼\n", ball);
+            return;
+        }
+
+        if (ball == 0) {
+            System.out.printf("%d스트라이크\n", strike);
+            return;
+        }
+
         System.out.printf("%d볼 %d스트라이크\n", ball, strike);
 
     }
