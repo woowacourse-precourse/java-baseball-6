445b54413cf0db14914361542a89a1f346d3712c
gomudayya <gomudayya@gmail.com>
2023-10-24 16:27:50 +0900

    feat : 중복된 숫자입력에 대한 예외처리기능 추가

--- src/main/java/baseball/InputManager.java
+++ src/main/java/baseball/InputManager.java
@@ -2,8 +2,13 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class InputManager {
     private static InputManager instance;
+
     private InputManager() {}
 
     public String readNumber() {
@@ -17,6 +22,9 @@ public class InputManager {
             throw new IllegalArgumentException("숫자만 입력해주세요.");
         if(input.length() != 3)
             throw new IllegalArgumentException("3자리 숫자만 입력해 주세요.");
+        if(hasDuplicatedNumber(input)) {
+            throw new IllegalArgumentException("중복이 포함되는 숫자는 입력할 수 없어요");
+        }
     }
 
     public int readWeatherRestartOrEnd() {
@@ -33,6 +41,15 @@ public class InputManager {
     private boolean isNumeric(String str) {
         final String REGEX = "[0-9]+";
         return str.matches(REGEX);
+    }
+
+    private boolean hasDuplicatedNumber(String input) {
+        Set<Character> set = new HashSet<>();
+
+        for (char c : input.toCharArray())
+            set.add(c);
+
+        return input.length() != set.size();
     }
 
     public static InputManager getInstance() {
