7d28dcaa28c9e4df116301e72fb2b2ee5cf9f075
inwoo <korean5078@gmail.com>
2023-10-24 20:35:56 +0900

    fix: 컴퓨터 숫자 출력 취소 1. Main함수에서 컴퓨터 숫자가 출력되는 것 수정 2. BaseBallGame 클래스에서 결과 출력 코드 printScore함수에서 줄바꿈 수정

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -74,8 +74,10 @@ public class BaseBallGame {
 			System.out.print(ball + "볼 ");
 		}
 		if (strike > 0) {
-			System.out.println(strike + "스트라이크");
+			System.out.print(strike + "스트라이크");
 		}
+
+		System.out.println();
 
 		if (strike == 3) {
 			System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
