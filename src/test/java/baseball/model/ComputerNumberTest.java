1fa0866a0de8bc0fead3c574682f268d9704dbee
oxdjww <oxdjww@gmail.com>
2023-10-25 00:17:18 +0900

    test : ComputerNumberTest, UserNumberTest 추가

---
+++ src/test/java/baseball/model/ComputerNumberTest.java
@@ -0,0 +1,26 @@
+package baseball.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ComputerNumberTest {
+    @Test
+    @DisplayName("컴퓨터 생성한 랜덤 번호가 실제로 중복 자리수가 없는지 검증한다.")
+    public void 컴퓨터_생성자_테스트() throws Exception{
+        // given
+        ComputerNumber computer = new ComputerNumber();
+
+        // when
+        List<Integer> computerNum = computer.getComputerNumber();
+
+        // then
+        assertThat(computerNum.get(0)).isNotEqualTo(computerNum.get(1));
+        assertThat(computerNum.get(1)).isNotEqualTo(computerNum.get(2));
+        assertThat(computerNum.get(2)).isNotEqualTo(computerNum.get(0));
+    }
+
+}
---
+++ src/test/java/baseball/model/UserNumberTest.java
@@ -0,0 +1,45 @@
+package baseball.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class UserNumberTest {
+    @Test
+    @DisplayName("유효한 숫자를 입력했을 때 제대로 생성되는지 테스트한다.")
+    public void 유저_생성자_테스트() throws Exception{
+        // given
+        String input = "123";
+
+        // when
+        List<Integer> user = new UserNumber(input).getUserNumber();
+
+        // then
+        assertThat(user.get(0)).isNotEqualTo(user.get(1));
+        assertThat(user.get(1)).isNotEqualTo(user.get(2));
+        assertThat(user.get(2)).isNotEqualTo(user.get(0));
+
+    }
+
+    @Test
+    @DisplayName("중복이 있는 숫자를 입력했을 때 예외가 발생한다.")
+    public void 유저_생성자_중복_자리수_예외_테스트() throws Exception{
+        // given
+        String input = "111";
+
+        // when & then
+        assertThrows(IllegalArgumentException.class, () -> new UserNumber(input));
+    }
+
+    @Test
+    public void 유저_생성자_문자열_입력시_예외_테스트() throws Exception{
+        // given
+        String input = "문자열";
+
+        // when & then
+        assertThrows(IllegalArgumentException.class, () -> new UserNumber(input));
+    }
+}
