0c38f635ff0c349748cad0feffa714c1e5e7961b
YoonseolLee <gujueng32@naver.com>
2023-10-24 10:40:57 +0900

    feat: 사용자 입력값을 받는 기능

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import static baseball.Computer.COUNTS;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class Player {
     private static final String SUCCESS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String INPUT_REQUEST_MESSAGE = "숫자를 입력해주세요 : ";
     private Computer computer = new Computer();
     private int strike;
     private int ball;
@@ -13,5 +19,13 @@ public class Player {
         computer.init();
         strike = 0;
         ball = 0;
+    }
+
+    public void play() {
+        List<Integer> userNumbers;
+        while (strike < COUNTS) {
+            System.out.print(INPUT_REQUEST_MESSAGE);
+            String userInput = Console.readLine();
+        }
     }
 }
