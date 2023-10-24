4563f0e0abe9388426d4e95a95dd110c2e42d09c
chjcoder <chjcoder@gmail.com>
2023-10-24 21:55:52 +0900

    [refactor] 쓰지 않는 import 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,19 +1,9 @@
 package baseball;
 import baseball.controller.Controller;
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static camp.nextstep.edu.missionutils.Console.*;
-
 
 public class Application {
     public static void main(String[] args) {
         Controller controller = new Controller();
         controller.run();
-//        int i = Randoms.pickNumberInRange(1,9);
-//        System.out.println(i);
     }
 }
