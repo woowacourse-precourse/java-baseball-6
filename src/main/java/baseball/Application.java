6ef09a4eeace1e0349a6900610d2ffd7ec65b526
Chris <hereokay88@gmail.com>
2023-10-24 13:18:28 +0900

    docs: 구현할 기능 목록 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
@@ -8,14 +9,13 @@ public class Application {
         System.out.println("숫자 야구 게임을 시작합니다.");
         int execInput = 1;
         while (execInput == 1){
-            System.out.print("숫자를 입력해주세요 : ");
-            String number = Console.readLine();
+
 
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             execInput = Integer.parseInt(Console.readLine());
 
-            if (execInput == 1 || execInput == 2){
-                Exception IllegalArgumentException;
+            if (!(execInput == 1 || execInput == 2)){
+                throw new IllegalArgumentException("1 또는 2 중에서 입력해주세요.");
             }
 
         }
