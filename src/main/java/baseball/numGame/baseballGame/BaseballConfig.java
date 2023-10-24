1f6ae2c5b1ba25b15377d15a124cefcfe3c4e6a3
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-10-25 02:59:02 +0900

    feat(BaseballConfig) : create BaseballConfig

---
+++ src/main/java/baseball/numGame/baseballGame/BaseballConfig.java
@@ -0,0 +1,39 @@
+package baseball.numGame.baseballGame;
+
+import baseball.game.Config;
+
+public class BaseballConfig implements Config {
+    int minOfRange;
+    int maxOfRange;
+    int numOfNumbers;
+    int playOption;
+    int endOption;
+    @Override
+    public int getPlayOption() {
+        return playOption;
+    }
+    @Override
+    public int getEndOption() {
+        return endOption;
+    }
+    BaseballConfig(){
+        minOfRange = 1;
+        maxOfRange = 9;
+        numOfNumbers = 3;
+        playOption = 1;
+        endOption = 2;
+    }
+    public int getMinOfRange() {
+        return minOfRange;
+    }
+
+    public int getMaxOfRange() {
+        return maxOfRange;
+    }
+
+    public int getNumOfNumbers() {
+        return numOfNumbers;
+    }
+
+
+}
