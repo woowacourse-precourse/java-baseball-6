b2701507ac1d018b6cc69894d82b5ef350260c28
peeaaccee <wlgp8958@naver.com>
2023-10-25 07:34:13 +0900

    feat(BallGenerator): 3자리 수, 중복없는 볼 만들기

--- src/main/java/baseball/model/BallGenerator.java
+++ src/main/java/baseball/model/BallGenerator.java
@@ -2,6 +2,9 @@ package baseball.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BallGenerator {
     private static final int COMPUTER_MAX_SIZE = 3;
     public static final int START_INCLUSIVE = 1;
@@ -19,5 +22,20 @@ public class BallGenerator {
 
     public String createRandomBall() {
         return String.valueOf(Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE));
+    }
+
+    public List<String> makeComputerBalls() {
+        List<String> balls = new ArrayList<>();
+        while (balls.size() < COMPUTER_MAX_SIZE) {
+            checkBallDuplication(balls);
+        }
+        return balls;
+    }
+
+    private void checkBallDuplication(List<String> balls) {
+        String randomBall = createRandomBall();
+        if (!balls.contains(randomBall)) {
+            balls.add(randomBall);
+        }
     }
 }
