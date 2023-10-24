52d5af87bd0b7e0e5a5121433cefd5d2afabf64c
its-sky <smc9919@naver.com>
2023-10-24 23:48:07 +0900

    remove: 사용되지 않는 메시지 삭제

--- src/main/java/baseball/global/constant/OutputMessage.java
+++ src/main/java/baseball/global/constant/OutputMessage.java
@@ -6,7 +6,6 @@ public class OutputMessage {
     public static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     public static final String GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     public static final String THREE_STRIKE_MESSAGE = "3스트라이크";
-    public static final String GAME_RESULT_MESSAGE = "%d볼 %d스트라이크";
     public static final String NOTHING_MESSAGE = "낫싱";
     public static final String BALL_MESSAGE = "%d볼";
     public static final String STRIKE_WITH_BALL_MESSAGE = " %d스트라이크";
