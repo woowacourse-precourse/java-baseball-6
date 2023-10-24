65cc7f0591ed65059272b333c0c2e7a7f8b7a27f
jangseop <kgh010529@naver.com>
2023-10-25 01:16:54 +0900

    feat: GameDispatcherTest 생성
    GameDispatcherTest 생성및 테스트 요구사항 입력

--- src/test/java/baseball/game/GameDispatcherTest.java
+++ src/test/java/baseball/game/GameDispatcherTest.java
@@ -1,4 +1,12 @@
-import static org.junit.jupiter.api.Assertions.*;
+package baseball.game;
+
 class GameDispatcherTest {
-  
+
+    // TODO GameRequest.cmd == "predict"면, predictGamePlay 구현체의 메서드를 호출한다
+
+    // TODO GameRequest.cmd == "start"면, startGamePlay 구현체의 메서드를 호춣한다.
+
+    // TODO 사용자가 숫자를 맞췄을 경우, 다음 액션으로 END를 보낸다.
+
+    // TODO 예외처리
 }
