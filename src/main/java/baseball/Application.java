c8d721df2d4604e27a7cc9f792cec1166662c004
kijen723 <kij2646@gmail.com>
2023-10-25 04:34:03 +0900

    refactor: 랜덤 숫자 기능 위치 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,18 +7,6 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Application {
-    public static List<Integer> getRandomNumber() {
-        List<Integer> randomNumberList = new ArrayList<>();
-        while (randomNumberList.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!randomNumberList.contains(randomNumber)) {
-                randomNumberList.add(randomNumber);
-            }
-        }
-
-        return randomNumberList;
-    }
-
     public static void main(String[] args) {
         // TODO1: 게임시작 메세지 출력
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -27,7 +15,13 @@ public class Application {
 
         while (gamestart == 1) {
             // TODO2: 랜덤 숫자 설정
-            List<Integer> computer = getRandomNumber();
+            List<Integer> computer = new ArrayList<>();
+            while (computer.size() < 3) {
+                int randomNumber = Randoms.pickNumberInRange(1, 9);
+                if (!computer.contains(randomNumber)) {
+                    computer.add(randomNumber);
+                }
+            }
 
             int restart = 1;
 
