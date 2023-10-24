76ccd7de389fef213afc49df65567563c0a48ff5
DaeyeonCho <daecho8@gmail.com>
2023-10-25 01:46:32 +0900

    refactor: Counter, Game 클래스 리팩토링
    메소드 이름 수정하고 기능들을 작은 단위의 메소드로 나누었습니다.
    구성을 간결하게 변경하는데 불필요해진 클래스(Answer, User)는 제거했습니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,17 +1,16 @@
 package baseball;
 
-import baseball.domain.User;
 import baseball.game.Game;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
 
         Game game = new Game();
-        User gameUser = new User();
 
-        game.welcome(gameUser);
+        game.welcome();
         do {
-            game.play(gameUser);
-        } while (gameUser.isReady());
+            game.play();
+        } while (game.isAgain(Console.readLine()));
     }
 }
