10f2067e984f5aa2de7f135a604208c86f4fa232
DohunHyun <dohun.hyun@gmail.com>
2023-10-24 18:17:39 +0900

    Style : 코드 재구성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -18,7 +18,7 @@ public class Application {
             // 랜덤 숫자 생성.
             makeRandomNumber();
 
-            // 숫자 맞추기 게임 시작
+            // 숫자 맞추기 게임 시작q
             gameStart();
         }
     }
@@ -38,18 +38,13 @@ public class Application {
         }
     }
 
-    static int[] checkInputNumber() {
-        System.out.print("숫자를 입력해주세요. : ");
-        // 입력 : 1~9 숫자 중 3가지로 이루어진 세자리 숫자.
-        // 입력 시 같은 숫자는 사용 금지.
-        // 입력 시 숫자만 입력 가능.
-        String input = Console.readLine();
-        String[] inputArray = input.split("");
+    // 입력 값 확인
+    static int[] checkInputNumber(String[] inputArray) {
         int[] inputToIntArray = new int[3];
         boolean[] checkList = new boolean[9];
 
         // 3자리 초과 시 오류
-        if(input.length() > 3) {
+        if(inputArray.length > 3) {
             System.out.println("세자리 숫자를 입력해 주세요.");
             throw new IllegalArgumentException();
         }
@@ -71,12 +66,54 @@ public class Application {
                 throw new IllegalArgumentException();
             }
         }
+
         return inputToIntArray;
     }
+
+    // 숫자 입력 받기
+    static int[] getInputNumber() {
+        System.out.print("숫자를 입력해주세요. : ");
+        // 입력 : 1~9 숫자 중 3가지로 이루어진 세자리 숫자.
+        // 입력 시 같은 숫자는 사용 금지.
+        // 입력 시 숫자만 입력 가능.
+        String input = Console.readLine();
+        String[] inputArray = input.split("");
+
+        return checkInputNumber(inputArray);
+    }
+
+    static void printResult(int strike, int ball) {
+        if(strike == 3) { // 정답인 경우
+            // 정답이라면 while문 break.
+            System.out.println("3스트라이크"
+                + "\n3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+                + "\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            int inputInt = Integer.parseInt(Console.readLine());
+            if(inputInt == 2) { // 게임 종료
+                endSign = 2;
+            } else { // 이번 게임은 끝. 다음 게임 다시 시작.
+                endSign = 1;
+            }
+            isCorrect = true;
+        } else if(strike == 0 && ball == 0) { // 하나도 못맞춘 경우
+            System.out.println("낫싱");
+        } else { // 볼 또는 스트라이크가 있는 경우
+            if(ball > 0) {
+                System.out.print(ball + "볼 ");
+                if(strike > 0) {
+                    System.out.print(strike + "스트라이크");
+                }
+            } else {
+                System.out.print(strike + "스트라이크");
+            }
+            System.out.println(); // 개행
+        }
+    }
+
     static void gameStart() {
         while(!isCorrect) {
             // 입력 값 확인
-            int[] inputToIntArray = checkInputNumber();
+            int[] inputToIntArray = getInputNumber();
 
             // 입력 후 판단.
             int strike = 0;
@@ -91,33 +128,8 @@ public class Application {
                     }
                 }
             }
-
-            if(strike == 3) { // 정답인 경우
-                // 정답이라면 while문 break.
-                System.out.println("3스트라이크"
-                    + "\n3개의 숫자를 모두 맞히셨습니다! 게임 종료"
-                    + "\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                int inputInt = Integer.parseInt(Console.readLine());
-                if(inputInt == 2) { // 게임 종료
-                    endSign = 2;
-                } else { // 이번 게임은 끝. 다음 게임 다시 시작.
-                    endSign = 1;
-                }
-                isCorrect = true;
-            } else if(strike == 0 && ball == 0) { // 하나도 못맞춘 경우
-                System.out.println("낫싱");
-            } else { // 볼 또는 스트라이크가 있는 경우
-                if(ball > 0) {
-                    System.out.print(ball + "볼 ");
-                    if(strike > 0) {
-                        System.out.print(strike + "스트라이크");
-                    }
-                } else {
-                    System.out.print(strike + "스트라이크");
-                }
-                System.out.println(); // 개행
-            }
+            printResult(strike, ball);
         }
-
     }
+
 }
