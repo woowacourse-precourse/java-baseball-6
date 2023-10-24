4f7efa5ba7316c7e8024c8616bbc8ea5e0d2fc8a
wda067 <mg6359@naver.com>
2023-10-25 06:18:16 +0900

    refactor: 하드코딩 상수로 리팩토링

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -6,18 +6,32 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Player {
 
-    private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final int INPUT_MESSAGE_LENGTH = 3;
+    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
-    private static final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String RESTART_OR_QUIT_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String INPUT_NUMBER_CONTAIN_BLANK_EXCEPTION = "공백은 입력할 수 없습니다.";
+    private static final String INPUT_NUMBER_IS_NOT_DIGIT_EXCEPTION = "1에서 9까지의 숫자만 입력하세요.";
+    private static final String INPUT_NUMBER_CONTAIN_ZERO_EXCEPTION = "0을 포함할 수 없습니다.";
+    private static final String INPUT_NUMBER_LENGTH_EXCEPTION = "3자리의 숫자만 입력하세요.";
+    private static final String INPUT_NUMBER_IS_DUPLICATE_NUMBER_EXCEPTION = "서로 다른 숫자를 입력하세요.";
+    private static final String INPUT_NUMBER_IS_NOT_ONE_OR_TWO = "1또는 2만 입력하세요.";
+    private static final String ZERO_VALUE = "0";
+    private static final String RERUN_VALUE = "1";
+    private static final String QUIT_VALUE = "2";
+    private static final String BLANK = " ";
+    private static final String NOT_DIGIT_REGEX = "\\D+";
     private static boolean isFirst = true;
     private static List<Integer> playerNumber;
 
     public static void inputNumber() {
         if (isFirst) {
-            System.out.println(START_MESSAGE);
+            System.out.println(GAME_START_MESSAGE);
         }
         isFirst = false;
         System.out.print(INPUT_MESSAGE);
@@ -37,22 +51,35 @@ public class Player {
         return playerNumber;
     }
 
-    public static boolean isRerun() {
-        System.out.println(RESTART_MESSAGE);
+    public static boolean isRestart() {
+        System.out.println(RESTART_OR_QUIT_MESSAGE);
         String input = Console.readLine();
-        if (input.equals("1")) {
+        if (input.equals(RERUN_VALUE)) {
             return true;
-        } else if (input.equals("2")) {
-            return false;
-        } else {
-            throw new IllegalArgumentException("1또는 2만 입력하세요.");
         }
+        if (input.equals(QUIT_VALUE)) {
+            return false;
+        }
+        throw new IllegalArgumentException(INPUT_NUMBER_IS_NOT_ONE_OR_TWO);
     }
 
     public static void isException(String str) {
         Set<String> set = new HashSet<>(Arrays.asList(str.split("")));
-        if (!str.matches("[1-9]{3}") || set.size() != 3) {
-            throw new IllegalArgumentException("0이 아닌 3자리의 서로 다른 수를 입력하세요.");
+        Matcher matcher = Pattern.compile(NOT_DIGIT_REGEX).matcher(str);
+        if (str.contains(BLANK)) {
+            throw new IllegalArgumentException(INPUT_NUMBER_CONTAIN_BLANK_EXCEPTION);
+        }
+        if (matcher.find()) {
+            throw new IllegalArgumentException(INPUT_NUMBER_IS_NOT_DIGIT_EXCEPTION);
+        }
+        if (str.contains(ZERO_VALUE)) {
+            throw new IllegalArgumentException(INPUT_NUMBER_CONTAIN_ZERO_EXCEPTION);
+        }
+        if (str.length() != INPUT_MESSAGE_LENGTH) {
+            throw new IllegalArgumentException(INPUT_NUMBER_LENGTH_EXCEPTION);
+        }
+        if (set.size() != INPUT_MESSAGE_LENGTH) {
+            throw new IllegalArgumentException(INPUT_NUMBER_IS_DUPLICATE_NUMBER_EXCEPTION);
         }
     }
 }
