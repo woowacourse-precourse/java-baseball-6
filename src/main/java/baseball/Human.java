d52ac1393b97547ded0a54f5bb52d5a9f9a0e5cb
Sanghee0820 <lsh277604@gmail.com>
2023-10-24 21:10:51 +0900

    feat: add new class(Human)

---
+++ src/main/java/baseball/Human.java
@@ -0,0 +1,13 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Human {
+    private List<Integer> Number = new ArrayList<>(3);
+
+    public void setNumber() {
+
+    }
+
+}
