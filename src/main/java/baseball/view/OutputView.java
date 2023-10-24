05973e68156a2f4468297941872a47301703b92c
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-10-24 20:40:30 +0900

    feat : OutputView 초기문구, 안내문구 메소드 작성

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,15 @@
+package baseball.view;
+
+public class OutputView {
+    public void printInitialMessage(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void printInputInfoMessage(){
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
+    public void printResultMessage(Integer strike, Integer ball){
+
+    }
+}
