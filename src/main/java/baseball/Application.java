555de8636040de94447c8330fdc91be3507533f4
kangdaehoon <eogns47@konkuk.ac.kr>
2023-10-24 14:03:18 +0900

    Feat: 게임 플레이 기능 구현 date : 2023/10/24 1.game 인터페이스 구현체 구현 완료 2.새로운 기능 관련 테스트 코드 추가 완료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,7 +10,8 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-
+        GameImplementation gameImplementation = new GameImplementation();
+        gameImplementation.gameStart();
 
     }
 }
