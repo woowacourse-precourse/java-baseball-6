a6d06ab26e7c9fc05c3ddb10fa469e7780875b26
jiixon <jr0107380@gmail.com>
2023-10-25 04:12:06 +0900

    feat: 게임 종료시 출력

--- src/main/java/baseball/View/BaseballGameView.java
+++ src/main/java/baseball/View/BaseballGameView.java
@@ -9,4 +9,14 @@ public class BaseballGameView {
         System.out.print("숫자를 입력해주세요 :");
         return Console.readLine();
     }
+    public void print(String result){
+        System.out.println(result);
+    }
+    public void endPrint(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+    public String endUserInput(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return Console.readLine();
+    }
 }
