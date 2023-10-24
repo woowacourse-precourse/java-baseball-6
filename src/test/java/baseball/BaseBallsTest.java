c220ee38f5c604480b19542000eb702614d68225
K-KY <kyuyoungk@naver.com>
2023-10-24 18:28:06 +0900

    Test(BaseBallTest) : 테스트 코드 변경

--- src/test/java/baseball/BaseBallsTest.java
+++ src/test/java/baseball/BaseBallsTest.java
@@ -4,9 +4,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import baseball.domain.BallScore;
 import baseball.domain.BaseBalls;
+import baseball.domain.CompareNumber;
 import baseball.domain.DataTypeChanger;
 import baseball.domain.GameResult;
 import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -19,21 +21,24 @@ public class BaseBallsTest {
 
     @Test
     void baseBall_1b_1s() {
-        GameResult compare = baseBalls.compare(Arrays.asList(3, 2, 4));
+        List<CompareNumber> userBalls = DataTypeChanger.compareNumberFormat(Arrays.asList(3, 2, 4));
+        GameResult compare = baseBalls.compare(userBalls);
 
         assertThat(compare.getResult(BallScore.STRIKE)).isEqualTo(1);
         assertThat(compare.getResult(BallScore.BALL)).isEqualTo(1);
     }
     @Test
     void baseBall_3b() {
-        GameResult compare = baseBalls.compare(Arrays.asList(3, 1, 2));
+        List<CompareNumber> userBalls = DataTypeChanger.compareNumberFormat(Arrays.asList(3, 1, 2));
+        GameResult compare = baseBalls.compare(userBalls);
 
         assertThat(compare.getResult(BallScore.STRIKE)).isEqualTo(0);
         assertThat(compare.getResult(BallScore.BALL)).isEqualTo(3);
     }
     @Test
     void baseBall_3s() {
-        GameResult compare = baseBalls.compare(Arrays.asList(1, 2, 3));
+        List<CompareNumber> userBalls = DataTypeChanger.compareNumberFormat(Arrays.asList(1, 2, 3));
+        GameResult compare = baseBalls.compare(userBalls);
 
         assertThat(compare.getResult(BallScore.STRIKE)).isEqualTo(3);
         assertThat(compare.getResult(BallScore.BALL)).isEqualTo(0);
@@ -41,11 +46,11 @@ public class BaseBallsTest {
 
     @Test
     void baseBall_3n() {
-        GameResult compare = baseBalls.compare(Arrays.asList(4, 5, 6));
+        List<CompareNumber> userBalls = DataTypeChanger.compareNumberFormat(Arrays.asList(4, 5, 6));
+        GameResult compare = baseBalls.compare(userBalls);
 
         assertThat(compare.getResult(BallScore.STRIKE)).isEqualTo(0);
         assertThat(compare.getResult(BallScore.BALL)).isEqualTo(0);
-        assertThat(compare.isNothing()).isTrue();
     }
 
 }
