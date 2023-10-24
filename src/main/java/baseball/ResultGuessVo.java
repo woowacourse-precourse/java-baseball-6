eb2ff40311cdbcb046aacf330a4f9c01ed8b0710
youjae <youjae0803@gmail.com>
2023-10-24 23:32:18 +0900

    feat: make ResultGuessVo class

---
+++ src/main/java/baseball/ResultGuessVo.java
@@ -0,0 +1,13 @@
+package baseball;
+public class ResultGuessVo {
+    private int cntBall;
+    private int cntStrike;
+
+    public int getCntBall() {
+        return cntBall;
+    }
+
+    public int getCntStrike() {
+        return cntStrike;
+    }
+}
