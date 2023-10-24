84f15994814b0c545b4ffc41e0710c5e8d89c6e1
kariskan <zkfzpf56@naver.com>
2023-10-24 18:57:58 +0900

    feat: BaseballController로 게임 시작
    - BaseballService 의존성 주입

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
 
+import baseball.domain.controller.BaseballController;
+import baseball.domain.service.BaseballService;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+
+	public static void main(String[] args) {
+		BaseballController baseballController = new BaseballController(new BaseballService());
+		baseballController.run();
+	}
 }
