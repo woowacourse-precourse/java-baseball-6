5b8b13f2379cdfc95aa96a71b96ac289406ca789
asd <p_caso@naver.com>
2023-10-24 15:15:59 +0900

    사용자 입력 처리와 결과 계산 사용자 입력 처리하고 결과 계산 parseInput 메서드 추가 calculateResult 메서드 추가 게임 종료 메시지 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +12,14 @@ public class Application {
         List<Integer> computerNumbers = generateRandomNumbers();
         System.out.println("컴퓨터가 숫자를 정했습니다.");
 
-        //
+        while (true) {
+            System.out.print("세 자리 숫자를 입력하세요: ");
+            String input = Console.readLine();
+            if (!isValidInput(input)) {
+                System.out.println("잘못된 입력입니다. 1부터 9까지의 서로 다른 숫자를 입력하세요.");
+                continue;
+            }
+        }
     }
 
     private static List<Integer> generateRandomNumbers() {
@@ -25,5 +33,23 @@ public class Application {
         return numbers;
     }
 
-    //
+    private static boolean isValidInput(String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("입력 길이는 3이어야 합니다.");
+        }
+        for (char digit : input.toCharArray()) {
+            if (digit < '1' || digit > '9') {
+                throw new IllegalArgumentException("1부터 9까지의 숫자를 입력해야 합니다.");
+            }
+        }
+        if (!areDigitsUnique(input)) {
+            throw new IllegalArgumentException("서로 다른 숫자를 입력해야 합니다.");
+        }
+        return true;
+    }
+
+    private static boolean areDigitsUnique(String input) {
+        return input.chars().distinct().count() == 3;
+    }
+
 }
