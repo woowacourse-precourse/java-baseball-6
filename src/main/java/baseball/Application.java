1545e23bd3bfd9ca07a7ebbaf6c25eefece4962e
BackJiwan <bn1357nb@gmail.com>
2023-10-24 22:55:21 +0900

    feat: 사용자 입력 받기
    - 3자리 숫자 입력
    - 1~9 사용,음수제외, 예외발생

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +21,27 @@ public class Application {
         }
     }
 
+    static class User{
+        public void inputValidationCheck(int input){
+            if (!(input >= 111 && input <= 999 && input % 10 != 0 && (input / 10) % 10 != 0 && (input / 100) % 10 != 0)) {
+                throw new IllegalArgumentException("Please");
+            }
+        }
+
+        public void setUserNumber(){
+            System.out.print("숫자를 입력해주세요 : ");
+            String input = Console.readLine();
+            int inputNum = Integer.parseInt(input);
+            try{
+                inputValidationCheck(inputNum);
+            }catch (IllegalArgumentException e){
+                e.printStackTrace();
+            }
+            System.out.println(inputNum);
+        }
+
+    }
+
     static class Game{
         public void play(){
             System.out.println("숫자 야구 게임을 시작합니다.");
@@ -30,8 +52,10 @@ public class Application {
     public static void main(String[] args) {
         Game game = new Game();
         Computer computer = new Computer();
+        User user = new User();
         game.play();
         System.out.println(computer.getRandomNumber());
+        user.setUserNumber();
     }
 
 }
