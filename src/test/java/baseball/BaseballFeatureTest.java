2d7bd05a213f5dd01b25008da737108beda79e7a
ckkim817 <ckkim817@konkuk.ac.kr>
2023-10-25 03:50:58 +0900

    [Test] 컴퓨터 숫자 생성 기능 검증을 위한 테스트 작성

---
+++ src/test/java/baseball/BaseballFeatureTest.java
@@ -0,0 +1,22 @@
+package baseball;
+
+import static baseball.global.baseballConstant.GUESS_NUMBER_SIZE;
+import static baseball.global.baseballConstant.MAX_RANGE;
+import static baseball.global.baseballConstant.MIN_RANGE;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import baseball.domain.Computer;
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.Test;
+
+public class BaseballFeatureTest {
+
+    @Test
+    void 랜덤하게_고른_숫자가_1에서_9까지의_서로_다른_3자리_숫자인지_확인() {
+        Computer computer = new Computer();
+        int count = (int) IntStream.rangeClosed(MIN_RANGE, MAX_RANGE)
+                .filter(computer::numbersContains)
+                .count();
+        assertThat(count).isEqualTo(GUESS_NUMBER_SIZE);
+    }
+}
