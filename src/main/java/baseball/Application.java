6b6e8dbd04198ebea04b3f05ce50ff35cfbad9f2
MSDN-SPECIAL\Administrator <jsw1767@gmail.com>
2023-10-24 23:04:53 +0900

    IllegalArgumentException 발생시 오류 내용 출력되도록 재수정, 가독성 고려하여 주석 일부 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,10 +13,10 @@ public class Application {
 
         boolean startGame = true;
         while (startGame) {
-            String computerNumberStr = makeComputerNumber(); // 정답으로 사용할 서로 다른 3자리 수 생성
-            playGame(computerNumberStr); // 입력값이 정답과 일치할 때까지 게임 진행
+            String computerNumberStr = makeComputerNumber(); // 정답 생성
+            playGame(computerNumberStr); // 게임 진행
 
-            // 정답을 입력한 경우 게임 재시작 or 종료를 선택
+            // 게임 완료시 재시작 or 종료 선택
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             String userDecision = readLine();
             if (userDecision.equals("2")) {
@@ -24,12 +24,12 @@ public class Application {
             } else if (userDecision.equals("1")) {
                 // 게임 재시작
             } else {
-                throw new IllegalArgumentException(); // 1, 2 외의 값일 때 입력 오류 발생
+                throw new IllegalArgumentException("입력 오류: 1과 2 외의 입력값");
             }
         }
     }
 
-    // 서로 다른 3자리 수를 생성하고 문자열 형태로 반환
+    // 서로 다른 3개의 숫자를 리스트에 생성하고, 해당 숫자들로 이어진 3자리 수를 문자열 형태로 반환
     public static String makeComputerNumber() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
@@ -55,7 +55,7 @@ public class Application {
                 break;
             }
             checkInputError(userNumberStr); // 입력값이 서로 다른 3자리 수인지 확인
-            compareNumber(computerNumberStr, userNumberStr); // 정답과 입력값을 비교한 결과 출력
+            compareNumber(computerNumberStr, userNumberStr); // 정답과 입력값 비교 후 결과 출력
         }
     }
 
@@ -65,22 +65,22 @@ public class Application {
         try {
             Integer.parseInt(userNumberStr);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("입력 오류: 숫자 외의 문자 포함");
         }
         // 3자리가 아닐때
         if (userNumberStr.length() != 3) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("입력 오류: 3자리가 아님");
         }
         // 같은 숫자가 있을 때
         char digit1 = userNumberStr.charAt(0);
         char digit2 = userNumberStr.charAt(1);
         char digit3 = userNumberStr.charAt(2);
         if (digit1 == digit2 || digit1 == digit3 || digit2 == digit3) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("입력 오류: 중복된 숫자 포함");
         }
     }
 
-    // 정답(computerNumberStr)과 입력값(userNumberStr)을 비교한 결과를 출력
+    // 정답(computerNumberStr)과 입력값(userNumberStr)을 비교한 후 결과를 출력
     public static void compareNumber(String computerNumberStr, String userNumberStr) {
         // 입력값과 정답을 한 자리씩 비교하고 결과에 따라 스트라이크, 볼 개수 세기
         int strike = 0;
