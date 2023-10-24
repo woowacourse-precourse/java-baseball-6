b4718b8b5fff371eafdb964e03dfe7eaa9e2ebe6
AidenRoh <colohrby@gmail.com>
2023-10-24 14:48:30 +0900

    refactor: refactor game operating logic

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,37 +1,13 @@
 package baseball;
 
-import baseball.domain.GameOperator;
-import baseball.domain.Input;
-import baseball.domain.Judgement;
-import baseball.domain.Output;
-import java.util.List;
+import baseball.controller.GameController;
 
 public class Application {
     public static void main(String[] args) {
 
-        GameOperator gameOperator = new GameOperator();
-        Input input = new Input();
-        Output output = new Output();
-        Judgement judge = new Judgement();
+        GameController gc = new GameController();
 
-        System.out.println("숫자 야구 게임을 시작합니다.");
-
-        boolean play = true;
-
-        while (play) {
-            List<Integer> opponent = gameOperator.createOpponent();
-            System.out.println(opponent);
-            boolean threeStrike = true;
-            while (threeStrike) {
-                System.out.print("숫자를 입력해주세요 : ");
-                int[] resultData = judge.judgement(opponent, input.playerInput(3));
-                threeStrike = output.printResult(resultData);
-            }
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            play = gameOperator.restart(input.playerInput(1));
-        }
-        System.out.print("게임 종료");
+        gc.playGame();
     }
 }
 
---
+++ src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,63 @@
+package baseball.controller;
+
+import baseball.domain.Input;
+import baseball.domain.Judgement;
+import baseball.domain.Output;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class GameController {
+    Input input = new Input();
+    Judgement judge = new Judgement();
+    Output output = new Output();
+
+    public void playGame() {
+        System.out.println("야구 게임을 시작합니다.");
+
+        boolean run = true;
+
+        while (run) {
+            List<Integer> opponent = createOpponent();
+            guessTilSuccess(opponent);
+            run = restart();
+        }
+
+        System.out.println("게임 종료");
+    }
+
+    private List<Integer> createOpponent() {
+        Set<Integer> opponent = new HashSet<Integer>();
+
+        while (opponent.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            opponent.add(randomNumber);
+        }
+        return setIntoList(opponent);
+    }
+
+    private void guessTilSuccess(List<Integer> opponent) {
+        System.out.print("숫자를 입력해주세요 : ");
+
+        int[] resultData = judge.judgement(opponent, getInput(3));
+        //Recursive
+        if (output.printResult(resultData)) {
+            guessTilSuccess(opponent);
+        }
+    }
+
+    private boolean restart() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        return getInput(1).get(0) == 1;
+    }
+
+    private List<Integer> getInput(int inputType) {
+        return input.playerInput(inputType);
+    }
+
+    private List<Integer> setIntoList(Set<Integer> target) {
+        return target.stream().toList();
+    }
+}
