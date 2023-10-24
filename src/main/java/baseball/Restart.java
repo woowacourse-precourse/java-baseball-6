1766afce42dcedb239d3be8face29cb2d2c6b5e9
sleeg00 <slee000220@gmail.com>
2023-10-24 22:13:32 +0900

    Refector: 주석 추가

--- src/main/java/baseball/Restart.java
+++ src/main/java/baseball/Restart.java
@@ -13,9 +13,9 @@ public class Restart {
     public boolean choiceRestart() { // 게임 종료시 false 반환하는 함수
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String number = Console.readLine(); // 게임을 이어나갈지 사용자에게 값 전달받기
-        Console.close();
         if (number.equals(EXIT)) {
             System.out.print("게임 종료");
+            Console.close(); //콘솔을 닫아야 함
             return false;
         }
         return true;
