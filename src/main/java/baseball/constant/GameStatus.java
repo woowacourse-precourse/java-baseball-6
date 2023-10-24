504f43b57a68922307f8f4f72f690e30bb083137
yoonjuho <dbswn990@naver.com>
2023-10-25 02:34:47 +0900

    refactor(GameStatus): 변수, 메서드 이름 변경

--- src/main/java/baseball/constant/GameStatus.java
+++ src/main/java/baseball/constant/GameStatus.java
@@ -4,17 +4,17 @@ import java.util.Arrays;
 
 public enum GameStatus {
     RESTART(1),
-    EXIT(2);
+    QUIT(2);
 
-    private int signatureNumber;
+    private int decisionNumber;
 
-    GameStatus(int signatureNumber) {
-        this.signatureNumber = signatureNumber;
+    GameStatus(int decisionNumber) {
+        this.decisionNumber = decisionNumber;
     }
 
-    public static GameStatus find(Integer number) {
+    public static GameStatus apply(Integer number) {
         return Arrays.stream(GameStatus.values())
-                .filter(status -> status.signatureNumber == number)
+                .filter(status -> status.decisionNumber == number)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("숫자 1 혹은 2를 입력해야 합니다."));
     }
