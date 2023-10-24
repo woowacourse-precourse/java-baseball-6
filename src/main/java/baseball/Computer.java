9f277f779ca453caf9f9027c54adc823f4156ab0
YOOSUNAH <tjsdkyd4027@gmail.com>
2023-10-25 00:44:24 +0900

    주석처리

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -108,7 +108,7 @@ public class Computer {
                 computer.add(randomNumber);
             }
         }
-        showln("right answer for test : " + String.join("", computer.stream().map(String::valueOf).toList())); // 나중에 지워야 함.
+//        showln("right answer for test : " + String.join("", computer.stream().map(String::valueOf).toList())); // 나중에 지워야 함.
         return computer;
     }
 }
