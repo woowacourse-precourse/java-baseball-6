8c46b2553fdfabfc319e8add19572c14a7451836
박은지 <qkrdmswl1018@naver.com>
2023-10-24 10:27:42 +0900

    refactor: 결과 매칭 로직에 switch문 적용

--- src/main/java/baseball/evaluator/ResultEvaluator.java
+++ src/main/java/baseball/evaluator/ResultEvaluator.java
@@ -4,6 +4,13 @@ import baseball.game.Baseball;
 import baseball.message.Printer;
 import camp.nextstep.edu.missionutils.Console;
 
+
+import static baseball.evaluator.MatchResult.CORRECT;
+import static baseball.evaluator.MatchResult.NOTHING;
+import static baseball.evaluator.MatchResult.ONLY_BALL;
+import static baseball.evaluator.MatchResult.ONLY_STRIKE;
+import static baseball.evaluator.MatchResult.OTHER;
+
 public class ResultEvaluator {
 
     private static final int FULL_CORRECT_COUNT = 3;
@@ -12,17 +19,28 @@ public class ResultEvaluator {
         int strike = value.strike();
         int ball = value.ball();
 
+        MatchResult result;
         if (strike == FULL_CORRECT_COUNT) {
-            Printer.correct();
-            return true;
+            result = CORRECT;
         } else if (strike == 0 && ball == 0) {
-            Printer.nothing();
+            result = NOTHING;
         } else if (strike != 0 && ball == 0) {
-            Printer.strike(strike);
+            result = ONLY_STRIKE;
         } else if (strike == 0 && ball != 0) {
-            Printer.ball(ball);
+            result = ONLY_BALL;
         } else {
-            Printer.other(ball, strike);
+            result = OTHER;
+        }
+
+        switch (result) {
+            case CORRECT -> {
+                Printer.correct();
+                return true;
+            }
+            case NOTHING -> Printer.nothing();
+            case ONLY_STRIKE -> Printer.strike(strike);
+            case ONLY_BALL -> Printer.ball(ball);
+            case OTHER -> Printer.other(ball, strike);
         }
 
         return false;
@@ -40,10 +58,10 @@ public class ResultEvaluator {
                 } else {
                     throw new IllegalArgumentException("잘못된 입력입니다. 1(재시작) 또는 2(종료)를 입력해주세요.");
                 }
+
             } catch (NumberFormatException e) {
                 throw new IllegalArgumentException("잘못된 입력입니다. 숫자를 입력해주세요.", e);
             }
-
         }
     }
 }
