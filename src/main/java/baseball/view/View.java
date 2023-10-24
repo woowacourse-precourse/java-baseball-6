d8672eddde8d10f022df9b8f70a63816dc87d3ce
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-10-24 22:54:02 +0900

    Refactor: 사용하지 않는 자원 삭제

--- src/main/java/baseball/view/View.java
+++ src/main/java/baseball/view/View.java
@@ -5,15 +5,11 @@ import baseball.domain.PlayerNumbers;
 import baseball.domain.Result;
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Scanner;
-
 public class View {
 
     private static final String MESSAGE_INPUT_NUMBERS = "숫자를 입력해주세요 : ";
     private static final String MESSAGE_INPUT_RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static final String MESSAGE_OUTPUT_GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-
-    private final Scanner scanner = new Scanner(System.in);
 
     public static PlayerNumbers inputPlayerNumbers() {
         System.out.print(MESSAGE_INPUT_NUMBERS);
