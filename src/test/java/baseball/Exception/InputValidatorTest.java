fb3c041212f2590565c5f78ac080aac864196a63
chansoo <chansoo38@gmail.com>
2023-10-24 16:48:42 +0900

    <Refactor> : InputValidatorTest IllegalArgumentException의 인스턴스를 반환하는지 확인하는 구문 추가 및 오탈자 수정

--- src/test/java/baseball/Exception/InputValidatorTest.java
+++ src/test/java/baseball/Exception/InputValidatorTest.java
@@ -4,35 +4,42 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class InputValidatorTest {
     InputValidator inputValidator;
+
     @BeforeEach
     void set() {
         inputValidator = new InputValidator();
     }
+
     @Test
     void 세글자가_아닌_입력() {
 
         String test1 = "1234";
 
-        Assertions.assertThatThrownBy(() -> inputValidator.doValidate(test1)).hasMessage(InputException.NOT_THREE.getMessage());
+        Assertions.assertThatThrownBy(() -> inputValidator.doValidate(test1))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage(InputException.NOT_THREE.getMessage());
     }
+
     @Test
-    void 숫자가_아닌_입략() {
+    void 숫자가_아닌_입력() {
         String test2 = "abc";
-        Assertions.assertThatThrownBy(() -> inputValidator.doValidate(test2)).hasMessage(InputException.NOT_NUMBER.getMessage());
+        Assertions.assertThatThrownBy(() -> inputValidator.doValidate(test2))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage(InputException.NOT_NUMBER.getMessage());
     }
+
     @Test
     void 중복_숫자_입력() {
         String test3 = "122";
-        Assertions.assertThatThrownBy(() -> inputValidator.doValidate(test3)).hasMessage(InputException.HAS_DUPLICATE_NUMBER.getMessage());
+        Assertions.assertThatThrownBy(() -> inputValidator.doValidate(test3))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(InputException.HAS_DUPLICATE_NUMBER.getMessage());
     }
 
     @Test
     void 올바르지_않은_명령() {
         String wrongCommand = "a";
-        Assertions.assertThatThrownBy(() -> inputValidator.validateCommand(wrongCommand)).hasMessage(InputException.WRONG_COMMAND.getMessage());
+        Assertions.assertThatThrownBy(() -> inputValidator.validateCommand(wrongCommand))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage(InputException.WRONG_COMMAND.getMessage());
     }
 }
