79e5683933b6d022f4c61bd4e07b7c6dffc0bdae
Seyoung Goo <0917sy@gmail.com>
2023-10-25 00:32:38 +0900

    feat: 야구 게임 관련 상수 추가

---
+++ src/main/java/baseball/util/constants/BaseballGameConstants.java
@@ -0,0 +1,11 @@
+package baseball.util.constants;
+
+public class BaseballGameConstants {
+    // MAX_NUM - MIN_NUM + 1 >= NUM_LENGTH 를 만족해야만함
+    public static final int NUM_LENGTH = 3;
+    public static final int MIN_NUM = 1;
+    public static final int MAX_NUM = 9;
+
+    public static final String RESTART_OPTION_RESTART = "1";
+    public static final String RESTART_OPTION_EXIT = "2";
+}
