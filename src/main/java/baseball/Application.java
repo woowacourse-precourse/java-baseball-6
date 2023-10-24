43374986d796dbe4c3c219b6d5bb152e5366e7b3
SungKyum Kim <sungkyum1@naver.com>
2023-10-25 04:07:11 +0900

    refactor: 의존성 외부 주입 / 수정된 명세서에 맞도록 startGame 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,7 +3,13 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        final BaseballGame baseballGame = new BaseballGame();
+        // 인스턴스 생성
+        UserInputManager userInputManager = new UserInputManager();
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        ScoreCalculator scoreCalculator = new ScoreCalculator();
+
+        // 의존성 주입 및 프로세스 시작
+        final BaseballGame baseballGame = new BaseballGame(userInputManager, randomNumberGenerator ,scoreCalculator);
         baseballGame.startGame();
     }
 }
