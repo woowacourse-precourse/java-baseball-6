382e37bd871ee6de65a5154bc69fe1381d5d5c62
ts3083 <ts04031@gmail.com>
2023-10-24 21:21:15 +0900

    feat: Application 컨트롤러를 통한 게임 실행 코드 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import baseball.controller.MainController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        MainController mainController = new MainController();
+        mainController.gameStart();
     }
 }
