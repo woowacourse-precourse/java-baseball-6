549a30f13e01b12e85abd556806ab26444990710
peeaaccee <wlgp8958@naver.com>
2023-10-25 07:36:54 +0900

    test(BallGenerator): 무작위 수인지 검사

--- src/test/java/baseball/model/BallGeneratorTest.java
+++ src/test/java/baseball/model/BallGeneratorTest.java
@@ -3,8 +3,10 @@ package baseball.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -28,6 +30,16 @@ class BallGeneratorTest {
     void 컴퓨터_공번호_중복숫자_판별() {
         List<String> balls = ballGenerator.makeComputerBalls();
         assertThat(new HashSet<>(balls).size()).isEqualTo(balls.size());
+    }
+
+    @Test
+    void HashSet_유사정렬_확인() {
+        Set<String> tmp = new HashSet<>();
+        tmp.add("2");
+        tmp.add("8");
+        tmp.add("1");
+        tmp.add("4");
+        assertThat(new ArrayList<>(tmp)).isEqualTo(List.of("1", "2", "4", "8"));
     }
 
 }
