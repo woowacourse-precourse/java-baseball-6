4007fd70968e0c0e36ffaa556baf72ec9fe8eb1a
seunghye218 <seunghye@student.42seoul.kr>
2023-10-24 15:14:17 +0900

    feat: handling user input during games

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,12 +4,28 @@ import java.util.List;
 import java.util.ArrayList;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
 
     public static void main(String[] args) {
         List<Integer> computer = getRandomList();
         System.out.println("숫자 야구 게임을 시작합니다.");
+
+        while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
+            String data = readLine().trim();
+            isValidInput(data);
+        }
+    }
+
+    private static void isValidInput(final String input) {
+        if (input.length() != 3 || !input.matches("\\d+") ||
+                input.charAt(0) == input.charAt(1) ||
+                input.charAt(0) == input.charAt(2) ||
+                input.charAt(1) == input.charAt(2)) {
+            throw new IllegalArgumentException();
+        }
     }
 
     private static List<Integer> getRandomList() {
