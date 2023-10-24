e1b45f13951603fe3552d306dfe6b4de15c0240a
leesangjun <hubls@naver.com>
2023-10-24 21:58:27 +0900

    refactor(InputView): 미사용 라이브러리 제거

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -2,9 +2,6 @@ package baseball.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class InputView {
     private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String RESTART_OR_END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
