099f192690799b5a8f235430f2c6e59abdbab174
chs98412 <chs98412@gmail.com>
2023-10-24 16:46:40 +0900

    fix: 예외에 메세지 추가

--- src/main/java/baseball/entity/Input.java
+++ src/main/java/baseball/entity/Input.java
@@ -34,20 +34,20 @@ public class Input {
 
     private void checkLength(int length) {
         if (inputString.length() != length) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(String.format("입력 길이 초과입니다. 입력길이 : [%s],  예상 길이: [%s]",inputString.length(),length));
         }
     }
     private void checkIsNum(){
         try {
             Integer.parseInt(inputString);
         } catch (Exception e) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(String.format("입력이 숫자가 아닙니다. 입력한 문자 : [%s]",inputString));
         }
     }
 
     private void checkAllDiffer( ) {
         if (inputString.charAt(0) == inputString.charAt(1) || inputString.charAt(0) == inputString.charAt(2) || inputString.charAt(1) == inputString.charAt(2)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("입력에서 중복된 숫자가 존재합니다.");
         }
     }
 }
