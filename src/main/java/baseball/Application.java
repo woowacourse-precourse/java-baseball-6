ed8105871916097f6a8c3f017b94c926edd5d9d7
asd <p_caso@naver.com>
2023-10-24 15:10:55 +0900

    게임 시작 및 컴퓨터 숫자 생성 게임 시작 메시지 출력 컴퓨터가 무작위로 3자리 숫자를 생성 generateRandomNumbers 메서드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,29 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        List<Integer> computerNumbers = generateRandomNumbers();
+        System.out.println("컴퓨터가 숫자를 정했습니다.");
+
+        //
     }
+
+    private static List<Integer> generateRandomNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < 3) {
+            int num = Randoms.pickNumberInRange(1, 9);
+            if (!numbers.contains(num)) {
+                numbers.add(num);
+            }
+        }
+        return numbers;
+    }
+
+    //
 }
