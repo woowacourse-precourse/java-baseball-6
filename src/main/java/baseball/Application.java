4408ba4b03e81a3364137d215f4ada5a6fb06948
elizabethgim <yujingim@naver.com>
2023-10-24 19:09:24 +0900

    feat: 시작 문구 출력 및 입력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
+import baseball.Game;
+
+import java.io.IOException;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Game baseballGame = new Game();
+        baseballGame.start();
     }
 }
--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,5 +1,13 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Game {
 
+    public void start() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+        System.out.println(input);
+    }
 }
