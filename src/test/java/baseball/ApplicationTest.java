24baf24bec133f6084ab9b83889e535e00ab9f04
donghoonyeom <dua5410773@naver.com>
2023-10-24 23:51:55 +0900

    [baseball]Test: 게임 재시작에서 1,2를 제외한 입력 예외 테스트

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -20,29 +20,59 @@ class ApplicationTest extends NsTest {
         );
     }
 
+
+    @Test
+    void 게임종료_후_재시작_잘못된_입력_종료이후_다시하기() {
+        assertThatThrownBy(() -> {
+            assertRandomNumberInRangeTest(
+                    () -> {
+                        run("246", "135", "1", "597", "589", "3");
+                        assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+                    },
+                    1, 3, 5, 5, 8, 9
+            );
+            assertRandomNumberInRangeTest(
+                    () -> {
+                        run("246", "135", "1", "597", "589", "12");
+                        assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+                    },
+                    1, 3, 5, 5, 8, 9
+            );
+            assertRandomNumberInRangeTest(
+                    () -> {
+                        run("246", "135", "1", "597", "589", "#");
+                        assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+                    },
+                    1, 3, 5, 5, 8, 9
+            );
+            assertRandomNumberInRangeTest(
+                    () -> {
+                        run("246", "135", "1", "597", "589", "a");
+                        assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+                    },
+                    1, 3, 5, 5, 8, 9
+            );
+
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+
     @Test
     void 예외_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("1234"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("1234"))
+                    .isInstanceOf(IllegalArgumentException.class);
+            assertThatThrownBy(() -> runException("abc"))
+                    .isInstanceOf(IllegalArgumentException.class);
+            assertThatThrownBy(() -> runException("112"))
+                    .isInstanceOf(IllegalArgumentException.class);
+            assertThatThrownBy(() -> runException("ㄱㄴㄷ"))
+                    .isInstanceOf(IllegalArgumentException.class);
+            assertThatThrownBy(() -> runException("#$!"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
     }
 
-    @Test
-    void 문자_예외_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("abc"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void 중복_숫자_예외_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("111"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
 
     @Override
     public void runMain() {
