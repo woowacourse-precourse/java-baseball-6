d55e7963793aa861716366f9acd7e1bd58c2525a
donghoonyeom <dua5410773@naver.com>
2023-10-24 18:36:19 +0900

    [baseball]Test: 중복 숫자 입력 예외 처리 테스트 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -29,14 +29,21 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트2() {
+    void 문자_예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("abc"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
-    
+    @Test
+    void 중복_숫자_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("111"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
