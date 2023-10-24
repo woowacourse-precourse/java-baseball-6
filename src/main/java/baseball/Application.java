d7b6c7b01350060ee2529b66307a94fabdc22c2b
k1mjunyoung <vovrwm@gmail.com>
2023-10-24 16:56:22 +0900

    feat: 2차 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,26 +11,14 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        String booter = "1";
-
-        if (game().get(0) == 3) {
-            System.out.println(game().get(0) + "스트라이크");
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            String input2 = Console.readLine();
-            if (input2.equals("1")){
-                game();
-            }
-        }
-
-
-    }
-    public static List<Integer> game() throws IllegalArgumentException {
         int ball = 0;
         int strike = 0;
-        String input = new String();
+        int restart = 0;
 
+        // 게임 시작
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        // 컴퓨터 임의 숫자 생성
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -39,26 +27,32 @@ public class Application {
             }
         }
 
-        int inputNum = Integer.parseInt(input);
-        int inputNum1 = inputNum / 100;
-        int inputNum2 = inputNum % 100 / 10;
-        int inputNum3 = inputNum % 10;
+        // 사용자 입력
+        String input = new String();
+
+        try {
+            input = Console.readLine();
+            int inputNum = Integer.parseInt(input);
+            int inputNum1 = inputNum / 100;
+            int inputNum2 = inputNum % 100 / 10;
+            int inputNum3 = inputNum % 10;
+        }
 
         List<Integer> user = new ArrayList<>(Arrays.asList(inputNum1, inputNum2, inputNum3));
 
         for (int i = 0; i < computer.size(); i++) {
-            for (int j = 0; j <user.size(); j++) {
+            for (int j = 0; j < user.size(); j++) {
                 if (computer.get(i) == user.get(i)) {
-                    strike ++;
-                }
-                else if (computer.get(i) == user.get(j)) {
-                    ball ++;
-                }
-                else continue;
+                    strike++;
+                } else if (computer.get(i) == user.get(j)) {
+                    ball++;
+                } else continue;
             }
         }
 
-        List<Integer> result = new ArrayList<>(Arrays.asList(strike, ball));
-        return result;
+    }
+
+    List<Integer> parseInput(String input) throws IllegalArgumentException {
+        int inputNum = Integer.parseInt(input);
     }
 }
