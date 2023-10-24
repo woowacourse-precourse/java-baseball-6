b79e63842a1d84ab32deb8c1806f1dbf579f1450
Seungjida <esg0107@pusan.ac.kr>
2023-10-24 15:57:20 +0900

    Refactor: "컴퓨터 클래스를 따로 생성해 관련 정보 관리하도록 수정"ㅐ

--- src/main/java/others/Computer.java
+++ src/main/java/others/Computer.java
@@ -1,2 +1,22 @@
-package others;public class Computer {
+package others;
+
+import static others.Constant.maxRange;
+import static others.Constant.minRange;
+import static others.Constant.numberOfNumbers;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    public static List<Integer> computerGenerateNum() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < numberOfNumbers) {
+            int randomNumber = Randoms.pickNumberInRange(minRange, maxRange);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
 }
