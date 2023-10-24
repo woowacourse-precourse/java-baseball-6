4fa8487cb1d023a99c75740ee82b71f247ec444c
ssung2sin <ssung2sin@naver.com>
2023-10-24 15:03:56 +0900

    코드 리팩토링 완료

--- src/main/java/baseball/model/ControllNumber.java
+++ src/main/java/baseball/model/ControllNumber.java
@@ -2,8 +2,8 @@ package baseball.model;
 
 public class ControllNumber {
 
-    private static String exitNumber = "1";
-    private static int strikeCount = 0;
+    private static String exitNumber;
+    private static int strikeCount;
 
     public static String getExitNumber() {
         return exitNumber;
