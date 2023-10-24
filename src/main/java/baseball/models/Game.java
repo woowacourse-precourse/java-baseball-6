026edd0f784cd68f06600868a7150dd45a9d478c
zeomzzz <jmlee2705@gmail.com>
2023-10-25 03:30:51 +0900

    feat: Game에 게임을 시작하는 기능 구현

--- src/main/java/baseball/models/Game.java
+++ src/main/java/baseball/models/Game.java
@@ -8,4 +8,10 @@ public class Game {
         return status;
     }
 
+    // 게임 시작하는 메소드
+    public void init() {
+        this.status = true;
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
 }
