811a3ad84e354aa880e53bcfd0e419612c376822
hwan2-99 <thh1233123@gmail.com>
2023-10-24 15:30:38 +0900

    test: 유저 숫자입력 검증 기능 테스트 코드 작성

--- src/test/java/baseball/serviceTest/UserServiceTest.java
+++ src/test/java/baseball/serviceTest/UserServiceTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class UserServiceTest {
 
@@ -21,7 +22,7 @@ public class UserServiceTest {
         numberValidation = new NumberValidation();
 
         userService = Mockito.mock(UserService.class);
-        Mockito.when(userService.inputUserNumber()).thenReturn("321");
+
     }
 
     @Test
@@ -30,13 +31,41 @@ public class UserServiceTest {
         test.add(3);
         test.add(2);
         test.add(1);
+        Mockito.when(userService.inputUserNumber()).thenReturn("321");
 
         List<Integer> user = numberValidation.validateInputNumber(userService.inputUserNumber());
 
         for (int i = 0; i < test.size(); i++) {
             assertThat(test.get(i)).isEqualTo(user.get(i));
         }
-
     }
+
+    @Test
+    void 사용자_입력_3자리_검증_테스트() {
+        Mockito.when(userService.inputUserNumber()).thenReturn("32");
+
+        assertThatThrownBy(() -> {
+            List<Integer> user = numberValidation.validateInputNumber(userService.inputUserNumber());
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 사용자_입력_중복_검증_테스트() {
+        Mockito.when(userService.inputUserNumber()).thenReturn("332");
+
+        assertThatThrownBy(() -> {
+            List<Integer> user = numberValidation.validateInputNumber(userService.inputUserNumber());
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 사용자_입력_유효범위_검증_테스트() {
+        Mockito.when(userService.inputUserNumber()).thenReturn("302");
+
+        assertThatThrownBy(() -> {
+            List<Integer> user = numberValidation.validateInputNumber(userService.inputUserNumber());
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
 
 }
