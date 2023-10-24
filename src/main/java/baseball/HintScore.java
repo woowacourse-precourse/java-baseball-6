1294a8b31b6065fd14c96884fd77ea055536e7b7
rlfrkdms1 <rlfrkdms1@naver.com>
2023-10-24 23:01:21 +0900

    feat: strike 출력 문구 생성 기능 추가
    %d스트라이크 출력형식 상수 선언
    힌트 점수 출력 문구 생성

--- src/main/java/baseball/HintScore.java
+++ src/main/java/baseball/HintScore.java
@@ -8,6 +8,7 @@ public class HintScore {
     private int ball;
 
     private static final String PRINT_BALL_FORMAT = "%d볼";
+    private static final String PRINT_STRIKE_FORMAT = "%d스트라이크";
 
     public void raiseStrike() {
         strike++;
@@ -22,6 +23,9 @@ public class HintScore {
         StringJoiner stringJoiner = new StringJoiner(" ");
         if (ball != 0) {
             stringJoiner.add(String.format(PRINT_BALL_FORMAT, ball));
+        }
+        if (strike != 0) {
+            stringJoiner.add(String.format(PRINT_STRIKE_FORMAT, strike));
         }
         return stringJoiner.toString();
     }
