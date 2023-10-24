202975840132a5e3f419845c7ee272e19ab4bf37
Sanghyeon Sim <5338095@gmail.com>
2023-10-24 23:41:40 +0900

    feat: GameInterface를 구현하는 BaseballGame 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,7 +4,9 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 enum ScoreType {
     STRIKE, BALL
@@ -18,42 +20,51 @@ interface GameInterface {
     boolean defer();
 }
 
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        while (true) {
-            pickComputerNumber(computerNumber);
-            System.out.println(computerNumber);
-            playGame(computerNumber);
-            computerNumber.clear();
-            if (isEndGame()) {
-                return;
-            }
-        }
+class BaseballGame implements GameInterface {
+    private final int numberSize = 3;
+    private List<Integer> computerNumber;
+
+    @Override
+    public void initialize() {
+        computerNumber = new ArrayList<>();
+        pickComputerNumber();
     }
 
-    private static void pickComputerNumber(List<Integer> computerNumber) {
-        int randomNumber;
-        while (computerNumber.size() < 3) {
-            randomNumber = Randoms.pickNumberInRange(1, 9);
+    @Override
+    public void start() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        playGame();
+    }
+
+    @Override
+    public boolean defer() {
+        return isEndGame();
+    }
+
+    private void pickComputerNumber() {
+        while (computerNumber.size() < numberSize) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computerNumber.contains(randomNumber)) {
                 computerNumber.add(randomNumber);
             }
         }
+        System.out.println(computerNumber);
     }
 
-    private static void playGame(List<Integer> computerNumber) throws IllegalArgumentException {
+    private void playGame() {
         List<Integer> parsedInput = new ArrayList<>();
         HashMap<ScoreType, Integer> result = new HashMap<>();
 
         while (true) {
             System.out.print("숫자를 입력해주세요 : ");
             String input = Console.readLine();
-            if (input.length() != 3) {
-                throw new IllegalArgumentException();
-            }
+
+            InputValidator.validateGameInput(input);
+
             parseString(parsedInput, input);
-            calculateScore(parsedInput, computerNumber, result);
-            printResult(result.get(ScoreType.strike), result.get(ScoreType.ball));
-            if (result.get(ScoreType.strike) == 3) {
+            calculateScore(parsedInput, result);
+            printResult(result.get(ScoreType.STRIKE), result.get(ScoreType.BALL));
+            if (result.get(ScoreType.STRIKE) == 3) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 return;
             }
@@ -62,21 +73,21 @@ interface GameInterface {
         }
     }
 
-    private static void calculateScore(List<Integer> input, List<Integer> computerNumber, HashMap result) {
+    private void calculateScore(List<Integer> input, HashMap<ScoreType, Integer> result) {
         int strike = 0;
         int ball = 0;
         for (int i = 0; i < input.size(); i++) {
-            if (input.get(i) == computerNumber.get(i)) {
+            if (input.get(i).equals(computerNumber.get(i))) {
                 strike++;
             } else if (computerNumber.contains(input.get(i))) {
                 ball++;
             }
         }
-        result.put(ScoreType.strike, strike);
-        result.put(ScoreType.ball, ball);
+        result.put(ScoreType.STRIKE, strike);
+        result.put(ScoreType.BALL, ball);
     }
 
-    private static void printResult(int strike, int ball) {
+    private void printResult(int strike, int ball) {
         String resultString = "";
         if (strike == 0 && ball == 0) {
             resultString = "낫싱";
@@ -90,7 +101,10 @@ interface GameInterface {
         System.out.println(resultString);
     }
 
-    private static void parseString(List<Integer> result, String target) throws IllegalArgumentException {
+    private void parseString(List<Integer> result, String target) {
+        if (target.length() != numberSize) {
+            throw new IllegalArgumentException();
+        }
         try {
             for (int i = 0; i < target.length(); i++) {
                 result.add(target.charAt(i) - '0');
@@ -103,11 +117,15 @@ interface GameInterface {
     private static boolean isEndGame() throws IllegalArgumentException {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
         String input = Console.readLine();
-        if (input.length() == 1 && input.charAt(0) == '1') {
+
+        InputValidator.validateRestartOrEndInput(input);
+
+        if (input.charAt(0) == '1') {
             return false;
         }
-        if (input.length() == 1 && input.charAt(0) == '2') {
-            return true;
+        return true;
+    }
+}
 
 class InputValidator {
 
