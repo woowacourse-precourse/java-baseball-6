8bc34b456d911df120c4c004468ad9bcdc3b4f48
pletain <pletain@dgu.ac.kr>
2023-10-25 02:59:52 +0900

    Feat: 사용자가 잘못된 값을 입력할 경우 IllegalArgumentException을 발생시킨 후 애플리케이션은 종료하는 기능 추가

--- src/main/java/baseball/Gamer.java
+++ src/main/java/baseball/Gamer.java
@@ -1,6 +1,7 @@
 package baseball;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
@@ -17,10 +18,21 @@ public class Gamer {
             System.out.print("숫자를 입력해주세요 : ");
             String input = readLine(); // readLine() 메소드를 사용하여 입력값을 받아옴
             number = Arrays.stream(input.split("")).map(Integer::parseInt).toList(); // 입력값을 Integer로 변환하여 리스트에 저장
+            validateNumber(); // 입력한 숫자의 유효성 검사
         } catch (IllegalArgumentException e) { // 예외 발생 시 프로그램 종료
             System.out.println(e.getMessage());
             System.exit(0);
         }
         return number;
+    }
+    private void validateNumber () {
+        // 입력한 숫자의 크기가 3자리 수가 아니면 예외 발생
+        if (number.size() != 3) {
+            throw new IllegalArgumentException();
+        } else if ( // 스트림을 사용하여 중복된 요소를 제거한 후 배열의 길이와, 원래 배열의 길이를 비교하여 입력값에 중복된 숫자가 있으면 예외 발생
+            number.stream()
+            .distinct()
+            .collect(Collectors.toList())
+            .size() != number.size()) throw new IllegalArgumentException();
     }
 }
