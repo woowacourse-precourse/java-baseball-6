d08187aaf753e94329d0db46afe8be4e151b1e50
songjayhyun <thewinner1st@naver.com>
2023-10-25 01:21:09 +0900

    feat: 서로 다른 수를 생성하는 클래스를 추가
    - RandomNumbersGenerator 클래스를 추가한다.
    - 1~9까지 서로 다른 임의의 수 3개를 만드는 로직을 추가한다.

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -7,9 +7,11 @@ public class Computer {
     private List<Integer> numbers;
 
     public void generateNumbers() {
+        RandomNumbersGenerator randomNumbersGenerator = new RandomNumbersGenerator();
+        this.numbers = randomNumbersGenerator.generateNumbers();
     }
 
     public List<Integer> getNumbers() {
-        return numbers;
+        return this.numbers;
     }
 }
---
+++ src/main/java/baseball/RandomNumbersGenerator.java
@@ -0,0 +1,25 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomNumbersGenerator {
+
+    private static final int NUMBER_LENGTH = 3;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+
+    public List<Integer> generateNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+
+        for (int i = 0; i < NUMBER_LENGTH; i++) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_NUMBER,MAX_NUMBER);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+
+        return numbers;
+    }
+}
