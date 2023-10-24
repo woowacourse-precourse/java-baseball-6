aeff1a392a5f7a684b1502a6567c5af837ab15cb
JW0321 <digneotn123@gmail.com>
2023-10-25 00:28:44 +0900

    Refactor: 인스턴스 생성 통일화
    인스턴스 생성 기능을 전부 Input 클래스로 옮겨서 인스턴스 생성 클래스 통일화 및 Comparison 클래스는 객체 생성이 필요 없는 유틸리티 클래스로 변경
    지역 변수의 중복 사용 삭제를 통한 코드 간소화
    값 비교 메소드인 compare 메소드 매개변수를 Input 클래스 객체로 변경

--- src/main/java/baseball/Comparison.java
+++ src/main/java/baseball/Comparison.java
@@ -1,23 +1,11 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Randoms; //랜덤 값 생성
 import java.util.ArrayList;
 import java.util.List;
 
 public class Comparison {
 
-    static private List<Integer> computer;
     static private boolean over;
-    Comparison() {
-        over = false;
-        computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
-        }
-    }
 
     static int countStrike(List<Integer> list1, List<Integer> list2) {
         int count = 0;
@@ -36,20 +24,20 @@ public class Comparison {
 
         tempList1.removeAll(list2);
 
-        int count = list1.size() - tempList1.size() - sameCount;
-        return count;
+        return list1.size() - tempList1.size() - sameCount;
     }
 
     static boolean isOver () {
         return over;
     }
 
-    static void compare (List<Integer> player) {
+    static void compare (Input computer, Input player) { //if문이 너무 많아 코드가 이쁘지 않다. 이를 줄일 수 있는 리팩토링은 무엇일까?
         int strike;
         int ball;
 
-        strike = countStrike(computer, player);
-        ball = countBall(computer, player);
+        strike = countStrike(computer.getNumber(), player.getNumber()); //좋은 객체지향은 getter 사용을 지양한다고 한다. 어떤 식으로 리팩토링 해야할까...
+        ball = countBall(computer.getNumber(), player.getNumber());
+        over = false;
 
         if (strike == 3) {
             System.out.printf("%d스트라이크%n", strike);
