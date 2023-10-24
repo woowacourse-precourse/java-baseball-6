d31fad3d151f2eded8953b88d12a01c6234072f7
minseo  jo <liging12@naver.com>
2023-10-24 16:54:29 +0900

    refactor: 볼의 개수가 변경되도 정상적인 출력이 되게끔 변경

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,9 +1,11 @@
 package baseball.view;
 
+import static baseball.domain.Baseballs.BALL_SIZE;
+
 public class OutputView {
 
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.\n";
-    private static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n";
+    private static final String GAME_END_MESSAGE = BALL_SIZE + "개의 숫자를 모두 맞히셨습니다! 게임 종료\n";
     private static final String LINE = "\n";
 
     public void printStartGameMessage() {
