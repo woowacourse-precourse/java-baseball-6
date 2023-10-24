f86968b7c39ecfa634ec1d5d381f409f9a35ebb9
K-KY <kyuyoungk@naver.com>
2023-10-24 18:17:38 +0900

    Refactor(DataTypeChanger) : 스트링 타입의 리스트를 바로 CompareNumber 타입으로 바꿔 리턴하도록 변경

--- src/main/java/baseball/domain/DataTypeChanger.java
+++ src/main/java/baseball/domain/DataTypeChanger.java
@@ -5,14 +5,18 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class DataTypeChanger {
-    public static List<Integer> mapToInt(List<String> userBalls) {
-        return userBalls.stream().mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
-    }
 
     public static List<CompareNumber> compareNumberFormat(List<Integer> generatedBall) {
         ArrayList<CompareNumber> compareNumbers = new ArrayList<>();
         generatedBall.forEach(v -> {
             compareNumbers.add(new CompareNumber(compareNumbers.size() + 1, v));
+        });
+        return compareNumbers;
+    }
+    public static List<CompareNumber> compareStringNumberFormat(List<String> generatedBall) {
+        ArrayList<CompareNumber> compareNumbers = new ArrayList<>();
+        generatedBall.forEach(v -> {
+            compareNumbers.add(new CompareNumber(compareNumbers.size() + 1, Integer.parseInt(v)));
         });
         return compareNumbers;
     }
