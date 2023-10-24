31a63223b9c8ad6a97bf3e86897dc5c25febc134
hyeonwoo <ziczin7176@gmail.com>
2023-10-24 17:07:50 +0900

    문자열 상수를 관리하기 위한 Message 클래스 생성

---
+++ src/main/java/baseball/Message.java
@@ -0,0 +1,15 @@
+package baseball;
+
+public class Message {
+    public static final String GAME_START = "숫자 야구 게임을 시작합니다.";
+    public static final String NOTHING = "낫싱";
+    public static final String BALL = "볼";
+    public static final String STRIKE = "스트라이크";
+    public static final String GAME_FINISH = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String GAME_RESTART_OR_QUIT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String TYPE_NUMBERS = "숫자를 입력해주세요 : ";
+    public static final String GAME_MACHINE_IS_NULL = "NULL인 GameMachine을 가동하려 했습니다.";
+    public static final String NOT_NUMBERS_OR_BLANK = "입력한 값이 숫자가 아니거나 빈 입력입니다.";
+    public static final String NOT_THREE_NUMBERS = "입력 값이 3자리가 아닙니다.";
+    public static final String REPEATED_NUMBERS = "중복된 숫자를 입력했습니다.";
+}
