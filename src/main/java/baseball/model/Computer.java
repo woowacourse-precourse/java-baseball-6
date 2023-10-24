2d352bda453041427a005ea58bb50bc0765b18ee
Choo <choo000407@naver.com>
2023-10-24 18:16:12 +0900

    feat(Computer): secretNumber 생성 및 반환 메서드 추가

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -1,4 +1,24 @@
 package baseball.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Computer {
+    private final List<Integer> secretNumber;
+
+    public Computer() {
+        secretNumber = new ArrayList<>();
+        while (secretNumber.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!secretNumber.contains(randomNumber)) {
+                secretNumber.add(randomNumber);
+            }
+        }
+    }
+
+    public List<Integer> getSecretNumber() {
+        return Collections.unmodifiableList(this.secretNumber);
+    }
 }
