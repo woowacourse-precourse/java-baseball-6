cfc56edfe5aac52585aebf257da6618a751c85a0
chjcoder <chjcoder@gmail.com>
2023-10-24 21:52:14 +0900

    [refactor] playerIntegerList 변수 private으로 설정

--- src/main/java/baseball/model/Player.java
+++ src/main/java/baseball/model/Player.java
@@ -9,7 +9,7 @@ public class Player {
     static final int NUMBER_OF_BALLS = 3;
     static final int MINIMUM_PLAYER_NUMBER = 1;
     static final int MAXIMUM_PLAYER_NUMBER = 9;
-    List<Integer> playerIntegerList;    // private로 설정?
+    private final List<Integer> playerIntegerList;
     public Player(String playerNumStr) {
         Validation.validateInteger(playerNumStr);
         playerIntegerList = Util.stringToIntegerList(playerNumStr);
@@ -19,6 +19,6 @@ public class Player {
     }
 
     public List<Integer> getPlayerIntegerList() {
-        return this.playerIntegerList;
+        return playerIntegerList;
     }
 }
