5a91bd0f1c2c4e2249e75e5a59f2a7c3a80ac105
heiler <threepebbles@naver.com>
2023-10-25 07:49:46 +0900

    refactor: 필요 없는 코드 제거

--- src/main/java/baseball/utils/Convert.java
+++ src/main/java/baseball/utils/Convert.java
@@ -2,7 +2,7 @@ package baseball.utils;
 
 public class Convert {
     public static int booleanToInt(boolean b) {
-        if (b == true) {
+        if (b) {
             return 1;
         }
         return 0;
