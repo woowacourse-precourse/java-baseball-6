c858ad91311b2de5970a02f645cc1bce91299520
ddingmin <ddingminyt@gmail.com>
2023-10-25 01:10:30 +0900

    구현: 사용자 입력 기능의 예외 처리를 구현한다.
    1. 올바른 숫자 입력 검증 기능 구현
    2. 게임 중 3자리의 숫자 입력 검증 구현
    3. 게임 종류 후 올바른 명령어 검증 구현

--- src/main/java/baseball/InputView.java
+++ src/main/java/baseball/InputView.java
@@ -5,7 +5,10 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 	public static final int GAME_START_CMD = 1;
 	public static final int GAME_END_CMD = 2;
+	public static final String WRONG_COMMAND_EXCEPTION_MESSAGE =
+		"올바른 게임 명령어가 입력되지 않았습니다.";
 	public static final int CORRECT_LENGTH = 3;
+	public static final String WRONG_LENGTH_EXCEPTION_MESSAGE = "게임 입력으로 " + CORRECT_LENGTH + "자리 숫자를 입력해야 합니다.";
 	public static final String INPUT_GAME_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
 	public static final String INPUT_GAME_PROCESS_MESSAGE =
 		"게임을 새로 시작하려면 " + GAME_START_CMD + ", 종료하려면 " + GAME_END_CMD + "를 입력하세요.";
@@ -14,16 +17,42 @@ public class InputView {
 		System.out.print(INPUT_GAME_NUMBER_MESSAGE);
 		String input = Console.readLine();
 
+		validateGameNumber(input);
+
 		return input;
+	}
+
+	private static void validateGameNumber(String input) {
+		if (input.length() != CORRECT_LENGTH) {
+			throw new IllegalArgumentException(WRONG_LENGTH_EXCEPTION_MESSAGE);
+		}
+		validateInputNumber(input);
 	}
 
 	public static boolean getProcessUserInput() {
 		System.out.println(INPUT_GAME_PROCESS_MESSAGE);
 		String input = Console.readLine();
 
+		return validateCommandNumber(input);
+	}
+
+	private static boolean validateCommandNumber(String input) {
+		validateInputNumber(input);
 		if (Integer.valueOf(input).equals(1)) {
 			return true;
 		}
-		return false;
+		if (Integer.valueOf(input).equals(2)) {
+			return false;
+		}
+		throw new IllegalArgumentException(WRONG_COMMAND_EXCEPTION_MESSAGE);
+	}
+
+	private static boolean validateInputNumber(String input) {
+		try {
+			Integer.parseInt(input);
+			return true;
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("숫자를 입력해야 합니다.");
+		}
 	}
 }
