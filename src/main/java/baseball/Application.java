c04e9ee65a27e1194f4846fd5bbff1b8a887a796
soyoungxxx <otherplet@gmail.com>
2023-10-24 17:36:44 +0900

    style(Application): 변수명 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,7 +4,7 @@ import baseball.game.GameControl;
 
 public class Application {
     public static void main(String[] args) {
-        GameControl gamecontoller = new GameControl();
-        gamecontoller.startGame();
+        GameControl gameContoller = new GameControl();
+        gameContoller.startGame();
     }
 }
