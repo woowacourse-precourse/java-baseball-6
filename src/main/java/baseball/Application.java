cb84afe018f53372c93e49214e67c5d50800da67
jiaeYoon <yoon529@sookmyung.ac.kr>
2023-10-24 17:03:00 +0900

    Refator: 자바 코드 컨벤션에 따라 리팩토링

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -20,8 +20,9 @@ public class Application {
             while (!correct) {
                 String user = enterNums();
                 correct = check(computer, user);
-
-                if (correct) System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                if (correct) {
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                }
             }
 
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
@@ -58,11 +59,10 @@ public class Application {
         List<Integer> dup_check = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
             int num = Integer.parseInt(input.substring(i, i + 1));
-            if (!dup_check.contains(num)) {
-                dup_check.add(num);
-            } else {
+            if (dup_check.contains(num)) {
                 throw new IllegalArgumentException();
             }
+            dup_check.add(num);
         }
 
         return input;
@@ -75,16 +75,24 @@ public class Application {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (user.charAt(i) == computer.charAt(j)) {
-                    if (i == j) strike++;
-                    else ball++;
+                    if (i == j) {
+                        strike++;
+                    } else {
+                        ball++;
+                    }
                 }
             }
         }
 
-        if (ball == 0 && strike == 0) System.out.println("낫싱");
-        else if (ball > 0 && strike > 0) System.out.println(ball + "볼 " + strike + "스트라이크");
-        else if (ball > 0) System.out.println(ball + "볼");
-        else if (strike > 0) System.out.println(strike + "스트라이크");
+        if (ball == 0 && strike == 0) {
+            System.out.println("낫싱");
+        } else if (ball > 0 && strike > 0) {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        } else if (ball > 0) {
+            System.out.println(ball + "볼");
+        } else if (strike > 0) {
+            System.out.println(strike + "스트라이크");
+        }
 
         return strike == 3;
     }
