3048e4c2f5b670d74da0083cce309f748731d498
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2023-10-24 22:43:47 +0900

    fix: Error를 catch 하지 않아 테스트를 통과할 수 있게끔 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,8 +6,6 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
         Game game = new Game();
-        try{
-            game.gameStart();
-        } catch (Exception e){}
+        game.gameStart();
     }
 }
