7b3e9f579dfefc0219d3083e3660877158aef3bc
kimbeomjoon99 <tnldls99@naver.com>
2023-10-24 18:41:13 +0900

    refactor

--- docs/README.md
+++ docs/README.md
@@ -39,3 +39,17 @@
   - countStrikes 메소드는 playGame 메소드 내에서 스트라이크를 계산하는 기능을 수행한다.
   - countBalls 메소드는 playGame 메소드 내에서 볼을 계산하는 기능을 수행한다.
   - (구현 뒤 수정) playGame 메소드는 printStart 메소드를 호출하여 게임 시작 메시지를 출력한 뒤 반복문에서 initBallcount 메소드를 호출하여 볼카운트를 초기화하고, getInput 메소드를 호출하여 사용자의 입력을 받는다. 만약 유저가 3스트라이크에 성공하면 성공 메시지를 출력하고 게임을 계속할지 여부를 입력받는다. 실패할 경우 initChecker 메소드를 호출하여 usedAnsNumChecker 및 usedInputNumChecker 배열을 초기화하고 countStrikes, countBalls 메소드를 호출하여 볼카운트를 계산한 뒤 printResult 메소드를 호출하여 결과를 출력한다.
+
+
+# 4. 테스트 후 리팩토링
+  - 클래스 하나에 인스턴스 변수가 9개 존재, 클래스가 지나치게 큼
+    - 클래스를 더 작은 형태로 분해한다.
+    - 기존의 BaseballGame 클래스에서 기존 클래스, 출력 역할을 수행하는 printGameMessage 클래스, 볼카운트를 관리하는 Ballcount 클래스로 분리한다.
+    - NEWGAME, EXITGAME 상수를 static으로 선언한다.
+    - START, EXIT 상수를 추가한다.
+  - 불필요한 깊이의 depth를 가진 메소드가 3개 존재
+    - countStrikes, countBalls 메소드 반복문에서 각 depth에 해당하는 계산을 수행하는 메소드를 추가하여 depth를 줄인다.
+    - playGame 메소드에서 사용자가 3스트라이크에 성공했을 때의 로직을 관리하는 메소드를 추가한다.
+  - else문 2개 존재
+    - 사용자가 3스트라이크에 성공했을 경우 exception을 발생시키는 코드의 else를 없앤다.
+    - playGame 메소드에서 사용자가 3스트라이크에 실패했을 경우의 명령어들을 감싼 else를 없앤다.
--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,104 +3,152 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
-class BaseballGame {
-	private String answer;
-	private String userInput;
-	private String newGameCommand;
-	private int strike;
-	private int ball;
-	private boolean[] usedAnsNumChecker;
-	private boolean[] usedInputNumChecker;
-	private final String NEWGAME = "1";
-	private final String EXITGAME = "2";
-	
-	BaseballGame() {
-		initGame();
-	}
-
-	private void initGame() {
-		answer = Integer.toString(100 * Randoms.pickNumberInRange(1, 10)
-				 + 10 * Randoms.pickNumberInRange(1, 10) + Randoms.pickNumberInRange(1, 10));
-		usedAnsNumChecker = new boolean[3];
-		usedInputNumChecker = new boolean[3];
-	}
-	
-	private void printStart() {
+class PrintMessage{
+	public void printStart() {
 		System.out.println("숫자 야구 게임을 시작합니다.");
 	}
 	
-	private void initBallcount(){
-		strike = 0;
-		ball = 0;
-	}
-	
-	private void getInput() throws IllegalArgumentException {
-		System.out.print("숫자를 입력해주세요 : ");
-		userInput = Console.readLine();
-		if (userInput.length() != 3) throw new IllegalArgumentException();
-	}
-	
-	private void initChecker() {
-		usedAnsNumChecker[0] = usedAnsNumChecker[1] = usedAnsNumChecker[2] = false;
-		usedInputNumChecker[0] = usedInputNumChecker[1] = usedInputNumChecker[2] = false;
-	}
-	
-	private void countStrikes() {
-		for (int i = 0; i < 3; i++) {
-			if (userInput.charAt(i) == answer.charAt(i)) {
-				strike++;
-				usedAnsNumChecker[i] = usedInputNumChecker[i] = true;
-			}
-		}
-	}
-	
-	private void countBalls() {
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				if (i == j || usedAnsNumChecker[i] == true || usedInputNumChecker[j] == true)
-					continue;
-				if (userInput.charAt(j) == answer.charAt(i)) {
-					ball++;
-					usedAnsNumChecker[i] = usedInputNumChecker[j] = true;
-				}
-			}
-		}
-	}
-	
-	private void printResult() {
+	public void printResult(int ball, int strike) {
 		if (ball > 0) System.out.print(ball + "볼 ");
 		if (strike > 0) System.out.print(strike + "스트라이크");
 		if (strike == 0 && ball == 0) System.out.print("낫싱");
 		System.out.println();
 	}
 	
-	private void printSuccess() {
+	public void printSuccess() {
 		System.out.println("3스트라이크");
 		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 	}
+}
+
+class Ballcount{
+	private int strike;
+	private int ball;
+	private boolean[] usedAnsNumChecker;
+	private boolean[] usedInputNumChecker;
+	
+	Ballcount(){
+		initCount();
+		usedAnsNumChecker = new boolean[3];
+		usedInputNumChecker = new boolean[3];
+	}
+	
+	public void initCount() {
+		strike = 0;
+		ball = 0;
+	}
+	
+	public void initChecker() {
+		usedAnsNumChecker[0] = false;
+		usedAnsNumChecker[1] = false;
+		usedAnsNumChecker[2] = false;
+		usedInputNumChecker[0] = false;
+		usedInputNumChecker[1] = false;
+		usedInputNumChecker[2] = false;
+	}
+	
+	private void calculateStrikes(int index, String answer, String userInput) {
+		if (answer.charAt(index) == userInput.charAt(index)) {
+			strike++;
+			usedAnsNumChecker[index] = true;
+			usedInputNumChecker[index] = true;
+		}
+	}
+	
+	public int countStrikes(String answer, String userInput) {
+		for (int i = 0; i < 3; i++) {
+			calculateStrikes(i, answer, userInput);
+		}
+		
+		return strike;
+	}
+	
+	private void calculateBalls(int index, String answer, String userInput) {
+		for (int i = 0; i < 3; i++) {
+			if (i == index || usedAnsNumChecker[index] == true || usedInputNumChecker[i] == true) {
+				continue;
+			}
+			if (answer.charAt(index) == userInput.charAt(i)) {
+				usedAnsNumChecker[index] = true;
+				usedInputNumChecker[i] = true;
+				ball++;
+			}
+		}
+	}
+	
+	public int countBalls(String answer, String userInput) {
+		for (int i = 0; i < 3; i++) {
+			calculateBalls(i, answer, userInput);
+		}
+		
+		return ball;
+	}
+}
+
+class BaseballGame {
+	private static final String NEWGAME = "1";
+	private static final String EXITGAME = "2";
+	private static final int START = 1;
+	private static final int EXIT = 2;
+	
+	private String answer;
+	private String userInput;
+	
+	BaseballGame() {
+		initGame();
+	}
+
+	private void initGame() {
+		answer = Integer.toString(100 * Randoms.pickNumberInRange(1, 9)
+				 + 10 * Randoms.pickNumberInRange(1, 9) + Randoms.pickNumberInRange(1, 9));
+	}
+	
+	private void getInput() throws IllegalArgumentException {
+		System.out.print("숫자를 입력해주세요 : ");
+		userInput = Console.readLine();
+		if (userInput.length() != 3 || userInput.contains("0")) {
+			throw new IllegalArgumentException();
+		}
+	}
+	
+	private int success() throws IllegalArgumentException {
+		if (userInput.equals(answer)) {
+			PrintMessage printer = new PrintMessage();
+			printer.printSuccess();
+			
+			String newGameCommand = Console.readLine();
+			if (newGameCommand.equals(NEWGAME)) return 1;
+			else if (newGameCommand.equals(EXITGAME)) return 2;
+			throw new IllegalArgumentException();
+		}
+		
+		return 0;
+	}
 	
 	public void playGame() throws IllegalArgumentException {
-		printStart();
+		PrintMessage printer = new PrintMessage();
+		Ballcount count = new Ballcount();
+		
+		printer.printStart();
 		
 		while(true) {
-			initBallcount();
+			count.initCount();
 			getInput();
 			
-			if (userInput.equals(answer)) {
-				printSuccess();
-				
-				newGameCommand = Console.readLine();
-				if (newGameCommand.equals(NEWGAME)) initGame();
-				else if (newGameCommand.equals(EXITGAME)) break;
-				else throw new IllegalArgumentException();
+			int startNewGame = success();
+			if (startNewGame == START) {
+				initGame();
+				continue;
 			}
-			else {
-				initChecker();
-				countStrikes();
-				countBalls();
-				printResult();
+			else if (startNewGame == EXIT) {
+				break;
 			}
+			
+			count.initChecker();
+			int strike = count.countStrikes(answer, userInput);
+			int ball = count.countBalls(answer, userInput);
+			printer.printResult(ball, strike);
 		}	
 	}
 }
