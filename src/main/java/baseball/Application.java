b9d1b410420f09fd3480c8ee8d7c3a48d33b1cb6
Minji Kim <minji1289@naver.com>
2023-10-25 00:01:10 +0900

    [fix] 출력 형식 요구사항에 맞춰 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,7 +10,7 @@ public class Application {
     private static final int DIGIT_COUNT = 3;
 
     public static void main(String[] args) {
-        System.out.println("숫자 야구 게임을 시작합니다");
+        System.out.println("숫자 야구 게임을 시작합니다.");
         while (true) {
             if (playGame()) {
                 break;
@@ -47,7 +47,7 @@ public class Application {
     }
 
     private static List<Integer> getUserNumbers() {
-        System.out.print("숫자를 입력해주세요: ");
+        System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
         return parseUserNumbers(input);
     }
