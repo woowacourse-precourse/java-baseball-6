1eec1b1312165db40d9ef6a438af796e5bc2345d
dlawotn3 <imjisu@imjisuui-MacBookPro.local>
2023-10-24 22:48:22 +0900

    feat: 게임 흐름을 위한 초기 로직 작성

---
+++ src/main/java/baseball/game/Game.java
@@ -0,0 +1,28 @@
+package baseball.game;
+
+public class Game {
+
+    private Player player;
+    private Computer computer;
+
+    public Game(Computer c, Player p) {
+        computer = c;
+        player = p;
+    }
+
+    public void start() {
+        // TODO: 숫자 야구 게임 시작 프린트 문 추가
+    }
+
+    public void playGame() {
+        // TODO: 숫자 야구 게임 로직 추가
+    }
+
+    public void end() {
+        // TODO: 숫자 야구 게임 종료 로직 추가
+    }
+
+    public void restart() {
+        // TODO: 게임 재시작 로직 추가
+    }
+}
