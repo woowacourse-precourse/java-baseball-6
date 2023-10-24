97fe1181555abebed5046b0b9d368814c7a21738
Kim, JeongHwan <jhkim980214@gmail.com>
2023-10-24 16:43:17 +0900

    출력 부분 변경

--- src/main/java/baseball/PrintResult.java
+++ src/main/java/baseball/PrintResult.java
@@ -2,15 +2,24 @@ package baseball;
 
 public class PrintResult {
 
+	private static final int BALL = 0;
+	private static final int STRIKE = 1;
+	private static final String STRING_BALL = "볼";
+	private static final String STRING_STRIKE = "스트라이크";
+	private static final String STRING_NOTHING = "낫싱";
+
 	public void print(int[] result) {
-		if (result[0] == 0 && result[1] == 0) {
-			System.out.println("낫싱");
-		} else if (result[0] == 0) {
-			System.out.println(result[1] + "스트라이크");
-		} else if (result[1] == 0) {
-			System.out.println(result[0] + "볼");
+		int ball = result[BALL];
+		int strike = result[STRIKE];
+
+		if (ball == 0 && strike == 0) {
+			System.out.println(STRING_NOTHING);
+		} else if (ball == 0) {
+			System.out.println(strike + STRING_STRIKE);
+		} else if (strike == 0) {
+			System.out.println(ball + STRING_BALL);
 		} else {
-			System.out.println(result[0] + "볼" + " " + result[1] + "스트라이크");
+			System.out.println(ball + STRING_BALL + " " + strike + STRING_STRIKE);
 		}
 	}
 }
