497371b9f6e5abf18937d7ed9785466c90fdc08b
Seunghyun-Jeong <95638057+Seunghyun-Jeong@users.noreply.github.com>
2023-10-24 15:15:42 +0900

    refactor:  코드 리팩토링

--- src/main/java/baseball/NumberBaseball.java
+++ src/main/java/baseball/NumberBaseball.java
@@ -12,17 +12,18 @@ public class NumberBaseball {
 
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-        int randomNumber1, randomNumber2, randomNumber3;
-        // 랜덤(타겟)숫자 생성
+        int[] randomNumber = new int[3];
+        // 중복 되지 않는 랜덤(타겟)숫자 생성
         do {
-            randomNumber1 = Randoms.pickNumberInRange(1, 9);
-            randomNumber2 = Randoms.pickNumberInRange(1, 9);
-            randomNumber3 = Randoms.pickNumberInRange(1, 9);
-        } while (randomNumber1 == randomNumber2 || randomNumber1 == randomNumber3 || randomNumber2 == randomNumber3);
+            randomNumber[0] = Randoms.pickNumberInRange(1, 9);
+            randomNumber[1] = Randoms.pickNumberInRange(1, 9);
+            randomNumber[2] = Randoms.pickNumberInRange(1, 9);
+        } while (randomNumber[0] == randomNumber[1] || randomNumber[0] == randomNumber[2]
+                || randomNumber[1] == randomNumber[2]);
 
-         System.out.println(randomNumber1);
-         System.out.println(randomNumber2);
-         System.out.println(randomNumber3);
+//        System.out.print(randomNumber[0]);
+//        System.out.print(randomNumber[1]);
+//        System.out.println(randomNumber[2]);
 
         while (true) {
             int strike = 0;
@@ -30,44 +31,38 @@ public class NumberBaseball {
             // 게이머(사용자)의 숫자를 입력을 받는다.
             // 3자리 숫자가 아닌 경우, 에러 발생
             System.out.print("숫자를 입력해주세요 : ");
-            String gamerNumber = Console.readLine();
-            if (gamerNumber.length() != 3) {
+            String gamerInput = Console.readLine();
+
+            if (gamerInput.length() != 3) {
                 throw new IllegalArgumentException();
             }
-            // 게이머(사용자)가 입력한 숫자를 랜덤(타겟)숫자와 비교
+
+            int[] gamerNumber = new int[3];
             // 입력이 숫자가 아닌 경우, 에러 발생
-            int gamerNumber1;
-            int gamerNumber2;
-            int gamerNumber3;
             try {
-                char[] gNArr = gamerNumber.toCharArray();
-                gamerNumber1 = Character.getNumericValue(gNArr[0]);
-                gamerNumber2 = Character.getNumericValue(gNArr[1]);
-                gamerNumber3 = Character.getNumericValue(gNArr[2]);
+                char[] gNArr = gamerInput.toCharArray();
+                gamerNumber[0] = Character.getNumericValue(gNArr[0]);
+                gamerNumber[1] = Character.getNumericValue(gNArr[1]);
+                gamerNumber[2] = Character.getNumericValue(gNArr[2]);
             } catch (Exception e) {
                 throw new IllegalArgumentException();
             }
             // 중복된 숫자 입력한 경우, 에러 발생
-            if (gamerNumber1 == gamerNumber2 || gamerNumber1 == gamerNumber3 || gamerNumber2 == gamerNumber3) {
+            if (gamerNumber[0] == gamerNumber[1] || gamerNumber[0] == gamerNumber[2]
+                    || gamerNumber[1] == gamerNumber[2]) {
                 throw new IllegalArgumentException();
             }
             // 게이머(사용자) 숫자와 랜덤(타겟) 숫자를 비교 하여 볼, 스트라이크 갯수 확인
-            if (gamerNumber1 == randomNumber1) {
-                strike++;
-            } else if (gamerNumber1 == randomNumber2 || gamerNumber1 == randomNumber3) {
-                ball++;
-            }
-
-            if (gamerNumber2 == randomNumber2) {
-                strike++;
-            } else if (gamerNumber2 == randomNumber1 || gamerNumber2 == randomNumber3) {
-                ball++;
-            }
-
-            if (gamerNumber3 == randomNumber3) {
-                strike++;
-            } else if (gamerNumber3 == randomNumber1 || gamerNumber3 == randomNumber2) {
-                ball++;
+            for (int i = 0; i < gamerNumber.length; i++) {
+                for (int k = 0; k < randomNumber.length; k++) {
+                    if (gamerNumber[i] == randomNumber[k]) {
+                        if (i == k) {
+                            strike++;
+                        } else {
+                            ball++;
+                        }
+                    }
+                }
             }
             // 볼, 스트라이크 출력
             if (strike == 3) {
@@ -75,20 +70,29 @@ public class NumberBaseball {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 break;
-            } else if (ball == 0 && strike == 0) {
-                System.out.println("낫싱");
-            } else if (ball > 0 && strike == 0) {
-                System.out.println(ball + "볼");
-            } else if (ball == 0 && strike > 0) {
-                System.out.println(strike + "스트라이크");
-            } else {
-                System.out.println(ball + "볼 " + strike + "스트라이크");
             }
+
+            if (ball == 0 && strike == 0) {
+                System.out.println("낫싱");
+                continue;
+            }
+
+            String resultMessage = "";
+            if (ball > 0) {
+                resultMessage += ball + "볼";
+            }
+            if (strike > 0) {
+                if (!resultMessage.isBlank()) {
+                    resultMessage += " ";
+                }
+                resultMessage += strike + "스트라이크";
+            }
+            System.out.println(resultMessage);
+
         }
 
         String Restart = Console.readLine();
-        // 게임 재시작 및 종료 여부 확인
-        // RESTART_INPUT, EXIT_INPUT를 입력 하지 않은 경우, 에러 발생
+
         switch (Integer.parseInt(Restart)) {
             case RESTART_INPUT:
                 return true;
