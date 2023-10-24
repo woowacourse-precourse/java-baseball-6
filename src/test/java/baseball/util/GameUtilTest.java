26e47c363f6822b1382bae1f79e296742c41c16a
jang010505 <jang010505@inu.ac.kr>
2023-10-24 15:24:17 +0900

    test: computeBallCount 태스트 코드 작성

--- src/test/java/baseball/util/GameUtilTest.java
+++ src/test/java/baseball/util/GameUtilTest.java
@@ -31,4 +31,20 @@ class GameUtilTest {
         Assertions.assertEquals(method.invoke(gameUtil, computerNumber1, playerNumber1), 2);
         Assertions.assertEquals(method.invoke(gameUtil, computerNumber2, playerNumber2), 1);
     }
+
+    @Test
+    @DisplayName("Ball Count 확인")
+    void computeStrikeCount() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        String computerNumber1 = "123";
+        String playerNumber1 = "123";
+
+        String computerNumber2 = "143";
+        String playerNumber2 = "523";
+
+        Method method = gameUtil.getClass().getDeclaredMethod("computeStrikeCount", String.class, String.class);
+        method.setAccessible(true);
+
+        Assertions.assertEquals(method.invoke(gameUtil, computerNumber1, playerNumber1), 3);
+        Assertions.assertEquals(method.invoke(gameUtil, computerNumber2, playerNumber2), 1);
+    }
 }
