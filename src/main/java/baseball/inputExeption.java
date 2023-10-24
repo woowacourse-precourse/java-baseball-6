fb8c30d32c8287d836ccadcf15882e05c5831b2e
mseo39 <a01063294905@gmail.com>
2023-10-24 19:18:02 +0900

    feat: add isUnigueInput
    Ability to check if different numbers are entered

--- src/main/java/baseball/inputExeption.java
+++ src/main/java/baseball/inputExeption.java
@@ -1,5 +1,19 @@
 package baseball;
 
 public class inputExeption {
-
+    public static boolean isUnigueInput(String[] input){
+        int cnt;
+        for(String num:input){
+            cnt=0;
+            for(String compare_num:input){
+                if (num.equals(compare_num)){
+                    cnt++;
+                }
+            }
+            if (cnt>1){
+                throw new IllegalArgumentException();
+            }
+        }
+        return true;
+    }
 }
