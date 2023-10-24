0c930afecaae2b09e49e9b135d78bf55a522239c
lzlkolo <lzlko1209@gmail.com>
2023-10-24 17:23:34 +0900

    refactor: Rename constant ZERO to NO_SCORE

--- src/main/java/baseball/Output.java
+++ src/main/java/baseball/Output.java
@@ -3,18 +3,18 @@ package baseball;
 import java.util.Map;
 
 public class Output {
-    private static final int ZERO = 0;
+    private static final int NO_SCORE = 0;
 
     public void gameStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
     public void hint(Map<String, Integer> result) {
-        if (result.get(KeyType.STRIKE.getKey()) == ZERO && result.get(KeyType.BALL.getKey()) == ZERO) {
+        if (result.get(KeyType.STRIKE.getKey()) == NO_SCORE && result.get(KeyType.BALL.getKey()) == NO_SCORE) {
             System.out.print("낫싱");
         }
         for (Map.Entry<String, Integer> entry : result.entrySet()) {
-            if (entry.getValue() != ZERO) {
+            if (entry.getValue() != NO_SCORE) {
                 System.out.print(entry.getValue() + entry.getKey() + " ");
             }
         }
