6199d7758a2859afc15c6c3289b5ebcb1436c7d3
anseongjun <dkstjdwns@kookmin.ac.kr>
2023-10-24 14:53:40 +0900

    예외처리 통과, 게임 재시작 실패

--- src/main/java/baseball/controller/CheckNumber.java
+++ src/main/java/baseball/controller/CheckNumber.java
@@ -8,14 +8,17 @@ import java.util.Set;
 public class CheckNumber {
 
     public static void checkNumberRules(String userInput) {
+        if (userInput.length() != 3) {
+            throw new IllegalArgumentException();
+        }
         String afterRegexInput = userInput.replaceAll("[^1-9]", "");
         Set<Character> inputSet = new HashSet<>();
         for (char c : afterRegexInput.toCharArray()) {
             inputSet.add(c);
         }
         if (inputSet.size() != 3) {
-            String errorMessage = "길이 3, 중복없는 1~9까지의 숫자 필요";
-            throw new IllegalArgumentException(errorMessage);
+//            String errorMessage = "길이 3, 중복없는 1~9까지의 숫자 필요";
+            throw new IllegalArgumentException();
         }
     }
 
@@ -35,6 +38,9 @@ public class CheckNumber {
         if (strike == 3) {
             System.out.println("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             BaseBallGame.restart = Integer.parseInt(Console.readLine());
+            if (BaseBallGame.restart == 1 || BaseBallGame.restart == 2) {
+                throw new IllegalArgumentException();
+            }
             BaseBallGame.correct = true;
         } else if (strike == 0 && ball == 0) {
             System.out.println("낫싱");
