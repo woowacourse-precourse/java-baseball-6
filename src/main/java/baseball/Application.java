55518d5c92051a1c360299f542cf33854b741ede
김민주 <69795199+ks00919@users.noreply.github.com>
2023-10-25 08:45:48 +0900

    feat: 컴퓨터, 사용자 숫자 저장하는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,10 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
@@ -13,11 +17,36 @@ public class Application {
         Console.close();
     }
 
+    private Application() {
+    }
+
+    private static List<Integer> computer;
+    private static List<Integer> user;
+
     public static void startBaseballShell() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+        setComputer();
         while (true) {
             String answer = getInput("숫자를 입력해주세요 : ", "\\d\\d\\d");
+            setUser(answer);
         }
+    }
+
+    private static void setComputer() {
+        computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+    }
+
+    private static void setUser(String string) {
+        user = Arrays.stream(string.split(""))
+                .mapToInt(Integer::valueOf)
+                .boxed()
+                .toList();
     }
 
     public static String getInput(String message, String regex) {
