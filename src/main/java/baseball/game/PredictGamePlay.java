1991e061507c1f67d3c5835da5a257f356255288
jangseop <kgh010529@naver.com>
2023-10-25 03:02:53 +0900

    feat: PredictGamePlay 구현

--- src/main/java/baseball/game/PredictGamePlay.java
+++ src/main/java/baseball/game/PredictGamePlay.java
@@ -1,10 +1,54 @@
 package baseball.game;
 
+import baseball.Computer;
 import java.util.List;
 
 public class PredictGamePlay implements GamePlay {
     @Override
     public String play(List<String> args) {
-        return null;
+        String inputNumber = args.get(0);
+        String errMsg = "잘못된 입력입니다, 예측 가능한 수는 세자리(1~9) 숫자입니다.";
+
+        // exception handling
+        if (inputNumber.length() != 3) {
+            throw new IllegalArgumentException(errMsg);
+        }
+
+        if (inputNumber.contains("0")) {
+            throw new IllegalArgumentException(errMsg);
+        }
+
+        int nStrike = 0;
+        int nBall = 0;
+
+        for (int i = 0; i < 3; ++i) {
+
+            char target = inputNumber.charAt(i);
+
+            if (target == Computer.number.charAt(i)) {
+                nStrike++;
+            } else if (Computer.number.contains("" + target)) {
+                nBall++;
+            }
+        }
+
+        String ret = "";
+
+        if (nBall > 0) {
+            ret += nBall + "볼";
+        }
+
+        if (nStrike > 0) {
+            if (nBall > 0) {
+                ret += " ";
+            }
+            ret += nStrike + "스트라이크";
+        }
+
+        if (ret == "") {
+            ret = "넛싱";
+        }
+
+        return ret;
     }
 }
