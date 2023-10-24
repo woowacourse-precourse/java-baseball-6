7ce94ba7bdde77a71bfd40b6d091753d0bde366b
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-10-25 00:07:13 +0900

    [FEAT] 컴퓨터 기능 구현
    컴퓨터 기능 구현 작성 (randnum)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,28 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        System.out.println("HI");
+    public static void main(String[] args){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        List<Integer> computerNumberList = computerRanNumList();
+    }
+
+    // computer
+    private static List<Integer> computerRanNumList() {
+        List<Integer> computerNumberList = new ArrayList<>();
+        while (computerNumberList.size() < 3) {
+            int randNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumberList.contains(randNumber)) {
+                computerNumberList.add(randNumber);
+            }
+        }
+        return computerNumberList;
     }
 }
+
+
