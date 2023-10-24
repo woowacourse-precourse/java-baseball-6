24998b6ddbfc11795369aebd1af0827ccf201dae
wlgh1553 <112055561+wlgh1553@users.noreply.github.com>
2023-10-24 23:56:12 +0900

    [REFACTOR] 예외 커스텀 삭제 및 모든 예외 종류 통일. 전달되는 예외 메시지로 오류 나타냄.

--- src/main/java/baseball/validation/CheckExitInput.java
+++ src/main/java/baseball/validation/CheckExitInput.java
@@ -1,9 +1,11 @@
 package baseball.validation;
 
 public class CheckExitInput {
+    public static final String RESTART_OR_EXIT_EXCEPTION_MSG = "입력이 1 또는 2여야 합니다.";
+
     public void checkValidationOfExit(String in) throws IllegalArgumentException {
         if (!isOneOrTwo(in)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(RESTART_OR_EXIT_EXCEPTION_MSG);
         }
     }
 
--- src/main/java/baseball/validation/CheckGameNumbers.java
+++ src/main/java/baseball/validation/CheckGameNumbers.java
@@ -3,32 +3,11 @@ package baseball.validation;
 import java.util.HashSet;
 import java.util.Set;
 
-class ConstExceptionMessages {
+public class CheckGameNumbers {
     public static final String THREE_DIGIT_EXCEPTION_MSG = "세 자리 입력이 아닙니다.";
     public static final String RANGE_EXCEPTION_MSG = "입력 범위가 올바르지 않습니다. 1이상 9이하의 숫자를 입력해야 합니다.";
     public static final String DIFF_EACH_OTHER_EXCEPTION_MSG = "서로 다른 수를 입력해야 합니다.";
-}
 
-class ThreeDigitException extends IllegalArgumentException {
-    ThreeDigitException() {
-        super(ConstExceptionMessages.THREE_DIGIT_EXCEPTION_MSG);
-    }
-}
-
-class NumberRangeException extends IllegalArgumentException {
-    NumberRangeException() {
-        super(ConstExceptionMessages.RANGE_EXCEPTION_MSG);
-    }
-}
-
-class DifferentEachOtherException extends IllegalArgumentException {
-    DifferentEachOtherException() {
-        super(ConstExceptionMessages.DIFF_EACH_OTHER_EXCEPTION_MSG);
-    }
-}
-
-
-public class CheckGameNumbers {
     public void checkValidationOfNums(String in) throws IllegalArgumentException {
         try {
             checkExceptions(in);
@@ -38,15 +17,15 @@ public class CheckGameNumbers {
     }
 
     private void checkExceptions(String in)
-            throws ThreeDigitException, NumberRangeException, DifferentEachOtherException {
+            throws IllegalArgumentException {
         if (!isThreeDigitNumbers(in)) {
-            throw new ThreeDigitException();
+            throw new IllegalArgumentException(THREE_DIGIT_EXCEPTION_MSG);
         }
         if (!isRightRangeNumbers(in)) {
-            throw new NumberRangeException();
+            throw new IllegalArgumentException(RANGE_EXCEPTION_MSG);
         }
         if (!isDifferentEachOther(in)) {
-            throw new DifferentEachOtherException();
+            throw new IllegalArgumentException(DIFF_EACH_OTHER_EXCEPTION_MSG);
         }
     }
 
