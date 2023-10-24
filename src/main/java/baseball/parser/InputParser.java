856796839c47356eabf21420f04d59e80d94d087
mainfn <_@mainfn.dev>
2023-10-24 14:53:41 +0900

    feat: Parser 분리

---
+++ src/main/java/baseball/parser/InputParser.java
@@ -0,0 +1,39 @@
+package baseball.parser;
+
+import baseball.domain.ball.BallNumber;
+import baseball.domain.game.GameStatus;
+import baseball.exception.InvalidGameStatusNumberInput;
+import baseball.validator.InputValidator;
+import collection.Triple;
+import java.util.Arrays;
+import java.util.List;
+
+public final class InputParser {
+
+    private final InputValidator inputValidator;
+
+    public InputParser(final InputValidator inputValidator) {
+        this.inputValidator = inputValidator;
+    }
+
+    public Triple<BallNumber> parseBallNumbersInput(final String input) {
+        inputValidator.validateNumbersInput(input);
+        final List<BallNumber> numbers = toBallNumberList(input);
+        return Triple.fromList(numbers);
+    }
+
+    public GameStatus parseGameStatusInput(final String input) {
+        final GameStatus gameStatus = GameStatus.of(input);
+        return switch (gameStatus) {
+            case UNKNOWN -> throw new InvalidGameStatusNumberInput();
+            default -> gameStatus;
+        };
+    }
+
+    private List<BallNumber> toBallNumberList(final String input) {
+        return Arrays.stream(input.split(""))
+                .map(Integer::parseInt)
+                .map(BallNumber::new)
+                .toList();
+    }
+}
