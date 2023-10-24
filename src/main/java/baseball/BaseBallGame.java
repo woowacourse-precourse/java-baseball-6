159f84f0454f61876aa172d034c1c453a72ac943
DESKTOP-0PS92PR\전서현 <tjgus5935@gmail.com>
2023-10-25 03:45:33 +0900

    feat: 결과 출력 기능 구현 Result에서 결과 값을 계산하고 출력합니다.

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -8,6 +8,7 @@ public class BaseBallGame {
 
     private NumberGenerator numberGenerator;
     private PlayerNumber playerNumber;
+
     private boolean allStrike;
     private boolean playOrNot;
 
@@ -21,6 +22,11 @@ public class BaseBallGame {
 
         //게임 실행 메소드 호출
         while (playOrNot) {
+            //랜덤 숫자 생성
+            numberGenerator = NumberGenerator.generateComputer();
+            System.out.println(numberGenerator.getComputer());
+
+            //게임 실행
             runGame();
 
             //게임 재시작/종료 입력
@@ -30,15 +36,17 @@ public class BaseBallGame {
     //게임 실행
     public void runGame(){
         while (!allStrike) {
-            //랜덤 숫자 생성
-            numberGenerator = NumberGenerator.generateComputer();
-            //System.out.println(numberGenerator.getComputer());
-
             //입력 받기
             playerNumber = PlayerNumber.inputPlayer();
-            //결과 계산 및 출력
-            //allStrike 확인
 
+            //결과 계산 및 출력
+            Result result = new Result();
+            int strike = result.resultGame(numberGenerator.getComputer(), playerNumber.getPlayer());
+
+            //allStrike 확인
+            if (strike == 3){
+                allStrike = true;
+            }
         }
     }
 
