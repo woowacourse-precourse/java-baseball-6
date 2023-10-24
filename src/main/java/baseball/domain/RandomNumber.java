cc25bb843a995fc3e67fef8a02711df521a7bab4
kimmokalover <kimmokalover@gmail.com>
2023-10-24 14:16:51 +0900

    feat : RandomGeneratorService class 구현

--- src/main/java/baseball/domain/RandomNumber.java
+++ src/main/java/baseball/domain/RandomNumber.java
@@ -3,6 +3,10 @@ package baseball.domain;
 public class RandomNumber {
     String randomNumber;
 
+    public RandomNumber(String randomNumber) {
+        this.randomNumber = randomNumber;
+    }
+
 
     public String getRandomNumber() {
         return randomNumber;
--- src/main/java/baseball/service/RandomGeneratorService.java
+++ src/main/java/baseball/service/RandomGeneratorService.java
@@ -1,5 +1,18 @@
 package baseball.service;
-
+import baseball.domain.RandomNumber;
+import camp.nextstep.edu.missionutils.Randoms;
 public class RandomGeneratorService {
 
+    public RandomNumber createRandomNumber(){
+        String stringRandomNumber = "";
+        int MAX_LENGTH = 3;
+        for (int i = 0; i < MAX_LENGTH; i++){
+            stringRandomNumber = stringRandomNumber.concat(integerToString(Randoms.pickNumberInRange(1, 9)));
+        }
+        return new RandomNumber(stringRandomNumber);
+    }
+
+    private String integerToString(int number){
+        return Integer.toString(number);
+    }
 }
