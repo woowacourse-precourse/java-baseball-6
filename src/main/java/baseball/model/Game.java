680721dec96e364b0b98b8d7cf319bc09617b4e6
Choo <choo000407@naver.com>
2023-10-24 23:21:56 +0900

    feat(Game): 게임 상태 관리 메서드 추가

--- src/main/java/baseball/model/Game.java
+++ src/main/java/baseball/model/Game.java
@@ -1,4 +1,27 @@
 package baseball.model;
 
+import baseball.enums.GameStatus;
+
 public class Game {
+    private GameStatus status;
+
+    public Game() {
+        this.status = GameStatus.IN_PROGRESS;
+    }
+
+    public GameStatus getStatus() {
+        return status;
+    }
+
+    public void pauseGame() {
+        this.status = GameStatus.PAUSE;
+    }
+
+    public void restartGame() {
+        this.status = GameStatus.IN_PROGRESS;
+    }
+
+    public void endGame() {
+        this.status = GameStatus.END;
+    }
 }
