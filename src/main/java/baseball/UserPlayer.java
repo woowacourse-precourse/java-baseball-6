3a5d9c7d7810ac963830f34d74f8dd3eda797e58
Minjeong63 <kmj06030@naver.com>
2023-10-25 01:33:43 +0900

    기능 목록 작성, 객체지향적으로 코드 리팩토링

---
+++ src/main/java/baseball/UserPlayer.java
@@ -0,0 +1,22 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class UserPlayer {
+    private String input;
+    private int[] numList;
+
+    // 이름 반환 메서드
+    public String getNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+
+        input = Console.readLine();
+        return input;
+    }
+
+    // 사용자 입력값 배열에 저장
+    public int[] saveUserInput(String input) {
+        int[] numList = {input.charAt(0) - '0', input.charAt(1) - '0', input.charAt(2) - '0'};
+        return numList;
+    }
+}
