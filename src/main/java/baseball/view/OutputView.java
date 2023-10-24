bb4b29382405ec128d2587d96372ee48040a937d
jiwon1217 <koagjw@gmail.com>
2023-10-25 02:22:41 +0900

    :recycle: refactor: OutputView 문자열 상수를 별도의 클래스로 분리

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,26 +1,28 @@
 package baseball.view;
 
+import baseball.constants.OutputMessage;
+
 public class OutputView {
     public static void printGameStartMessage() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println(OutputMessage.GAME_START);
     }
 
     public static void printResult(int ballCount, int strikeCount) {
         if (strikeCount == 0 && ballCount == 0) {
-            System.out.println("낫싱");
+            System.out.println(OutputMessage.NOTHING);
         } else if (strikeCount == 3) {
-            System.out.println("3스트라이크\n"
-                    + "3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println(3 + OutputMessage.STRIKE
+                    + System.lineSeparator() + OutputMessage.GAME_EXIT);
         } else if (strikeCount == 0) {
-            System.out.println(ballCount + "볼");
+            System.out.println(ballCount + OutputMessage.BALL);
         } else if (ballCount == 0) {
-            System.out.println(strikeCount + "스트라이크");
+            System.out.println(strikeCount + OutputMessage.STRIKE);
         } else {
-            System.out.println(ballCount + "볼" + " " + strikeCount + "스트라이크");
+            System.out.println(ballCount + OutputMessage.BALL + " " + strikeCount + OutputMessage.STRIKE);
         }
     }
 
     public static void printGameExitMessage() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println(OutputMessage.GAME_EXIT);
     }
 }
