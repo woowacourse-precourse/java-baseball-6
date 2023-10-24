eecdf9a4570d73f00dd30174f3ba26c4322a235d
OhJongMin <jmoh7480@gmail.com>
2023-10-25 04:25:32 +0900

    feat:Add makeGameNumber method

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,5 +1,12 @@
 package baseball.view;
 
-public class InputView {
+import baseball.domain.User;
+import baseball.service.InputException;
+import java.util.List;
 
+public class InputView {
+    public static void makeGameNumber(String userInput) {
+        List<Integer> userNumber = InputException.changeStringToNumbers(userInput);
+        User.setUser(userNumber);
+    }
 }
