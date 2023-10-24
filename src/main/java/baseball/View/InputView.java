fa60a3d1b5fa9ab11199b607874078a22d835d91
doyoungkim97 <eh4536@naver.com>
2023-10-25 00:21:11 +0900

    feat : 사용자 입력, 사용자 결과 출력

---
+++ src/main/java/baseball/View/InputView.java
@@ -0,0 +1,18 @@
+package baseball.View;
+import camp.nextstep.edu.missionutils.Console;
+
+import static baseball.View.OutputView.printRetry;
+import static baseball.View.OutputView.printStart;
+
+public class InputView {
+
+    public static String readGameNumber() {
+        String input = Console.readLine();
+        return input;
+    }
+
+    public static int readRetryNumber() {
+        printRetry();
+        return Integer.parseInt(Console.readLine());
+    }
+}
