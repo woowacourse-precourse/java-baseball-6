b2cb8f7a764542b085754bbbabff2c1ad270bcb5
JudyYeon <dev.judykim@gmail.com>
2023-10-24 18:19:08 +0900

    add : String Error 메시지
    - Error 시 출력하는 String 문구를 좀 더 직관적으로 나누어 개선하였습니다.
    - null일 때 '잘못된 입력..'을 반환하지 않고 '입력 값이 없..'을 반환하여 좀 더 직관적인 문구를 전달하도록 메시지를 나누었습니다.

--- src/main/java/baseball/constant/StringError.java
+++ src/main/java/baseball/constant/StringError.java
@@ -3,5 +3,6 @@ package baseball.constant;
 public class StringError {
     public static String wrongInputError = "잘못된 입력입니다.";
     public static String wrongLengthInputError = "잘못된 길이의 입력입니다.";
-
+    public static String notInputError = "입력된 값이 없습니다.";
+    public static String duplicatedInputError = "중복된 입력입니다.";
 }
