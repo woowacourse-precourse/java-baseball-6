70749d839c9252f945fcaf9c27245b5f772b7196
ASak1104 <khw56184@gmail.com>
2023-10-25 01:25:02 +0900

    feat : 애플리케이션 실행 로직 구현
    ################
    # <타입> : <제목> 의 형식으로 제목을 아래 공백줄에 작성
    # 제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 금지
    # 예) feat : 로그인 기능 추가
    
    # 바로 아래 공백은 지우지 마세요 (제목과 본문의 분리를 위함)
    
    ################
    # 본문(구체적인 내용)을 아랫줄에 작성
    # 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (현재 커밋과 관련된 이슈 번호 추가 등)
    # 예) Close #7
    
    ################
    # feat : 새로운 기능 추가
    # fix : 버그 수정
    # docs : 문서 수정
    # test : 테스트 코드 추가
    # refact : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    ################

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,36 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    private static final String RESTART = "1";
+    private static final String EXIT = "2";
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        boolean runningFlag = true;
+
+        while (runningFlag) {
+            startGame();
+
+            runningFlag = isRestart();
+        }
+
+        Console.close();
+    }
+
+    public static void startGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    private static boolean isRestart() {
+        String userInput = Console.readLine();
+
+        return switch (userInput) {
+            case RESTART -> true;
+            case EXIT -> false;
+            default -> throw new IllegalArgumentException();
+        };
     }
 }
