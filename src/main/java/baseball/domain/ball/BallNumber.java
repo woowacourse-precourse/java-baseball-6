e8aa51e26df10fa929517bfa7c555e09cabd78e3
mainfn <_@mainfn.dev>
2023-10-24 14:55:34 +0900

    refact: 외부에서 사용되지 않는 상수 access 권한 강화

--- src/main/java/baseball/domain/ball/BallNumber.java
+++ src/main/java/baseball/domain/ball/BallNumber.java
@@ -3,9 +3,9 @@ package baseball.domain.ball;
 public record BallNumber(
         int number
 ) {
-    public static final int MIN_BALL_NUMBER = 1;
+    private static final int MIN_BALL_NUMBER = 1;
 
-    public static final int MAX_BALL_NUMBER = 9;
+    private static final int MAX_BALL_NUMBER = 9;
 
     public BallNumber {
         if (number < MIN_BALL_NUMBER || number > MAX_BALL_NUMBER) {
