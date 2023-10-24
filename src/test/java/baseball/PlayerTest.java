33bd3b0fae62f7964f56294a29e2ef54d8b15898
ru-kka <ruka4739@gmail.com>
2023-10-24 15:18:23 +0900

    refactor: player 코드 리팩토링, feat: studyTest에 공부했던 것들 기반 테스트코드 작성

--- src/test/java/baseball/PlayerTest.java
+++ src/test/java/baseball/PlayerTest.java
@@ -7,20 +7,6 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PlayerTest extends NsTest {
-//    @Test
-//    void 사용자_입력_리스트_정상_변환_테스트() {
-//        Player player = new Player();
-//        String input = "123";
-//        InputStream in = new ByteArrayInputStream(input.getBytes());
-//        System.setIn(in);
-//
-//        List<Integer> playerNum = player.enter();
-//        System.out.println(playerNum);
-//
-//        List<Integer> expected = List.of(1,2,3);
-//        assertThat(expected).isEqualTo(playerNum);
-//    }
-
     @Test
     void 사용자_중복된_숫자_입력_예외_테스트() {
         assertSimpleTest(() ->
---
+++ src/test/java/study/StudyTest.java
@@ -0,0 +1,51 @@
+package study;
+
+import baseball.Player;
+import camp.nextstep.edu.missionutils.Randoms;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.allOf;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StudyTest {
+    @Test
+    void Randoms_메서드_테스트() {
+        int randomNumber = Randoms.pickNumberInRange(1, 9);
+        Boolean inRange = true;
+        if(randomNumber >= 1 && randomNumber <= 9){
+            inRange = true;
+        } else {
+            inRange = false;
+        }
+        assertThat(inRange).isTrue();
+    }
+
+    @Test
+    void 사용자_입력_리스트_정상_변환_테스트() {
+        String input = "123";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        List<Integer> playerNum = Player.enter();
+
+        List<Integer> expected = List.of(1,2,3);
+        assertThat(expected).isEqualTo(playerNum);
+    }
+
+    @Test
+    void 문자열에서_특정_위치_문자_찾기_테스트() {
+        String playerString = "123";
+        char digitChar = playerString.charAt(0);
+        assertThat(digitChar).isEqualTo('1');
+    }
+
+    @Test
+    void 문자에서_숫자로_변환_테스트() {
+        int digit = Character.getNumericValue('1');
+        assertThat(digit).isEqualTo(1);
+    }
+}
