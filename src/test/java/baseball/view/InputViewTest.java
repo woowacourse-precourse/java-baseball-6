973d26b446a8b6395f03c25eb300f22c8aaae9d4
jongheonleee <qwefghnm1212@gmail.com>
2023-10-24 21:38:34 +0900

    [test] 정수 배열 반환 테스트

---
+++ src/test/java/baseball/view/InputViewTest.java
@@ -0,0 +1,16 @@
+package baseball.view;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputViewTest {
+
+    @Test
+    void 정수_배열_반환_테스트() {
+        Integer[] expect = {1, 2, 3};
+        Integer[] result = InputView.convertStrToIntArr("123");
+        Assertions.assertArrayEquals(expect, result);
+    }
+}
