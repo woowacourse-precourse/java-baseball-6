de8e1d1adbaddedabfe517f80d4a2632a7b795e7
Winter <kso8760@daum.net>
2023-10-24 14:07:13 +0900

    잘못된 문법 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,11 +3,13 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public static boolean isUniqueThreeDigitNumber(int number) {
     if (number < 100 || number > 999) {
-    return false;
+        return false;
     }
+
     int digit1 = number % 10;
     int digit2 = (number / 10) % 10;
     int digit3 = (number / 100) % 10;
+
     return digit1 != digit2 && digit2 != digit3 && digit1 != digit3;
 }
 public class Application {
@@ -66,14 +68,14 @@ public class Application {
                     System.out.print("%d볼 ", ball);
                 }
                 if (strike > 0) {
-                    System.out.print("%d스트라이크", strike);
+                    System.out.println("%d스트라이크", strike);
                     if (strike == 3) {
                         System.out.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
                     }
                 }
             }
 
-            System.out.printIn("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
 
             int answer = scanner.nextInt();
             if (answer == 1) {
