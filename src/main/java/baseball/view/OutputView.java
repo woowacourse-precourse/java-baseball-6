0c506cb5ef5cad4823b89342cffa1679f49c234d
KongJunWoo <joonoo3@naver.com>
2023-10-25 01:56:54 +0900

    feat : 게임 시작과 끝 출력 메세지 출력

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,15 @@
+package baseball.view;
+
+public class OutputView {
+
+    //게임 시작
+    public static void start() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    //게임 종료
+    public static void end() {
+        System.out.println("게임을 종료합니다.");
+    }
+
+}
