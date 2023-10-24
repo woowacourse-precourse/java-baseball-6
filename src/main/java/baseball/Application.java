244cc2b139a0c7081b6e5c9fca0a1866dd36c088
kijen723 <kij2646@gmail.com>
2023-10-25 05:15:23 +0900

    feat: 사용자 입력 예외 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -30,19 +30,24 @@ public class Application {
                 Scanner scanner = new Scanner(System.in);
                 System.out.print("숫자를 입력해주세요 : ");
                 int selectNumber = scanner.nextInt();
+                int checkNumber = selectNumber;
                 List<Integer> user = new ArrayList<>();
                 user.add(selectNumber / 100);
                 selectNumber %= 100;
                 user.add(selectNumber / 10);
                 selectNumber %= 10;
                 user.add(selectNumber);
+                if (checkNumber < 100 || checkNumber > 999 || user.get(0) == 0 || user.get(1) == 0 || user.get(2) == 0
+                        || user.get(0) == user.get(1) || user.get(1) == user.get(2) || user.get(2) == user.get(0)) {
+                    throw new IllegalArgumentException("잘못된 수를 입력하였습니다.");
+                }
 
                 // TODO4: 볼, 스트라이크 갯수 세기
                 int ball = 0;
                 int strike = 0;
                 for (int i = 0; i < 3; i++) {
                     for (int j = 0; j < 3; j++) {
-                        if (i == j & user.get(i) == computer.get(j)) {
+                        if (i == j && user.get(i) == computer.get(j)) {
                             strike += 1;
                         } else if (user.get(i) == computer.get(j)) {
                             ball += 1;
@@ -53,8 +58,9 @@ public class Application {
                 // TODO5: 정답을 맞출 경우 게임 종료 아니면 재입력
                 if (strike == 3) {
                     restart = 0;
-                    System.out.printf("%d스트라이크\n", strike);
-                } else if (ball > 0 & strike > 0) {
+                    System.out.println("3스트라이크\n" +
+                            "3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                } else if (ball > 0 && strike > 0) {
                     System.out.printf("%d볼 %d스트라이크\n", ball, strike);
                 } else if (ball > 0) {
                     System.out.printf("%d볼\n", ball);
@@ -66,7 +72,6 @@ public class Application {
             }
 
             // TODO6: 게임 종료 후 게임 재시작 유무를 결정
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             Scanner scanner = new Scanner(System.in);
             gamestart = scanner.nextInt();
