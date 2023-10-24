63e1c690f2f0d7747c9c249a67ab5c75954394e6
moong2 <www.castlehi@gmail.com>
2023-10-25 07:07:00 +0900

    fix: [#15] change userInputTool to Console

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,16 +6,8 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Game game = new Game();
-        try {
-            playBaseball(game);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    private static void playBaseball(Game game) {
-        while (game.checkRestart()) {
+        do {
             game.startGame();
-        }
+        } while (game.checkRestart());
     }
 }
