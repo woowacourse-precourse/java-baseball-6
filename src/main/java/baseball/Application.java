4e48c28bb1b41596cab9ba3397277a8e144ee440
신용대 <fshiel8165@naver.com>
2023-10-24 16:34:36 +0900

    필요없는 import 정리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,5 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class Application {
     public static Baseball_Machine baseballMachine = new Baseball_Machine();
 
