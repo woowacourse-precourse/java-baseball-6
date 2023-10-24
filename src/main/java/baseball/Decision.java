650bdcf9ab0f86890efc274f10198abac95608b3
Chani17 <cucute8878@naver.com>
2023-10-24 23:32:45 +0900

    style: Formatting change to google Java Style Guide

--- src/main/java/baseball/Decision.java
+++ src/main/java/baseball/Decision.java
@@ -18,17 +18,27 @@ public class Decision {
 
         for(int i=0; i<3; i++) {
             for(int j=0; j<3; j++) {
-                if((i!=j) & (answer.get(i).equals(userNum.get(j)))) ballCount++;
+                if((i!=j) & (answer.get(i).equals(userNum.get(j)))) {
+                    ballCount++;
+                }
             }
         }
         return ballCount;
     }
 
     public void referee(int strikeCount, int ballCount) {
-        if((strikeCount > 0) & (ballCount > 0)) System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
-        else if((strikeCount > 0) & (ballCount < 1)) System.out.println(strikeCount + "스트라이크");
-        else if ((ballCount > 0) & (strikeCount < 1)) System.out.println(ballCount + "볼");
-        else System.out.println("낫싱");
+        if((strikeCount > 0) & (ballCount > 0)) {
+            System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+        }
+        else if((strikeCount > 0) & (ballCount < 1)) {
+            System.out.println(strikeCount + "스트라이크");
+        }
+        else if ((ballCount > 0) & (strikeCount < 1)) {
+            System.out.println(ballCount + "볼");
+        }
+        else {
+            System.out.println("낫싱");
+        }
     }
 
 }
--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -24,7 +24,9 @@ public class Game {
 
             try {
                 String inputNumber = Console.readLine();
-                if(inputNumber.length() != 3) throw new IllegalArgumentException("잘못된 입력입니다. 3자리 숫자를 입력하세요.");
+                if(inputNumber.length() != 3) {
+                    throw new IllegalArgumentException("잘못된 입력입니다. 3자리 숫자를 입력하세요.");
+                }
 
                 inputs = inputNumber.split("");
 
@@ -41,7 +43,9 @@ public class Game {
                     System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                     start = parseInt(Console.readLine());
 
-                    if(start == 1) answer = randomNumber.createRandomNumber();
+                    if(start == 1) {
+                        answer = randomNumber.createRandomNumber();
+                    }
                 }
             } catch (IllegalArgumentException e) {
                 throw new IllegalArgumentException("입력값이 잘못되었습니다. 게임을 종료합니다.");
