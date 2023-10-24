14142d42c9848f2b9470d19ffe71105968a774bc
JW0321 <digneotn123@gmail.com>
2023-10-25 00:37:54 +0900

    Refactor: 인스턴스 생성 통일화
    인스턴스 생성 기능을 전부 Input 클래스로 옮겨서 인스턴스 생성 클래스 통일화
    인스턴스 생성 시 필드에는 빈 정수 리스트만 존재하도록 하고 랜덤숫자 생성 메소드와 입력 값 변환 메소드를 구현하여 컴퓨터, 플레이어 두 객체를 완성할 수 있도록 함
    입력값 판별 및 예외 발생 부분에서 기존에 판별하지 못했던 3자리 숫자가 아닌 문자를 판별하는 코드를 추가
    If문 중복을 줄이기 위해 입력값이 한정되는 종료 및 재시작 부분 코드를 Switch/case 문으로 변경, 예외처리 부분도 throws 사용을 통해 간소화

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -9,19 +10,29 @@ public class Input {
 
     private final List<Integer> inputs;
 
-    Input(String s) throws IllegalArgumentException {
+    Input(){
         this.inputs = new ArrayList<>();
+    }
 
+    void setRandomNumber() {
+        while (inputs.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!inputs.contains(randomNumber)) {
+                inputs.add(randomNumber);
+            }
+        }
+    }
+
+    void setPlayerNumber (String s) throws IllegalArgumentException { //입력 과정에서 들어올 수 있는 모든 경우를 체크해서 예외가 발생하도록 해야 한다.
         for (char c : s.toCharArray()) {
+            if (!Character.isDigit(c)){
+                throw new IllegalArgumentException();
+            }
             inputs.add(Character.getNumericValue(c));
         }
-        if (inputs.size() != 3) {
+        if (inputs.size() != 3 || hasDuplicateElements(inputs) || inputs.contains(0)) {
             throw new IllegalArgumentException();
         }
-        if (hasDuplicateElements(inputs)) {
-            throw new IllegalArgumentException();
-        }
-
     }
 
     boolean hasDuplicateElements(List<Integer> list) {
@@ -37,24 +48,20 @@ public class Input {
         return false;
     }
 
-    List<Integer> getNumber() {
+    List<Integer> getNumber() { //get 말고 다른 방법이 있진 않았을까?
         return inputs;
     }
 
-    static int isEnd(String d) {
-        int returnNumber = 0;
-        try {
-            int i = Integer.parseInt(d);
-            if (i == 1) {
-                returnNumber = 1;
-            } else if (i == 2) {
+    static boolean isEnd(String d) throws IllegalArgumentException { //여기서는 허용되는 입력값이 1, 2 밖에 없으므로 if문 대신 switch/case를 활용해보자.
+        int endNumber = Integer.parseInt(d);
+
+        return switch (endNumber) {
+            case 1 -> false;
+            case 2 -> {
                 System.out.println("게임이 종료 되었습니다.");
-            } else {
-                throw new IllegalArgumentException();
+                yield true;
             }
-            return returnNumber;
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException();
-        }
+            default -> throw new IllegalArgumentException();
+        };
     }
 }
