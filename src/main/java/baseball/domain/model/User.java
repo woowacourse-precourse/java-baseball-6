9f0253b9b8644c5b6f5312ec5a93d780fb4e4f96
peeaaccee <wlgp8958@naver.com>
2023-10-25 08:19:53 +0900

    feat(User): 컴퓨터의 볼 숫자와 유저의 볼 숫자를 비교하여 볼카운트

--- src/main/java/baseball/domain/model/User.java
+++ src/main/java/baseball/domain/model/User.java
@@ -3,6 +3,8 @@ package baseball.domain.model;
 import baseball.utils.InputValidator;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class User {
     private final List<String> user;
@@ -19,4 +21,32 @@ public class User {
     private List<String> makeBallsFromInput(final String ballInput) {
         return List.of(ballInput.split(""));
     }
+
+    public Map<BallCount, Integer> compareWithComputerBalls(final Computer computer,
+                                                            final Map<BallCount, Integer> score) {
+
+        for (String userBall : this.user) {
+            BallCount ballCount = computer.compareWithUserBall(userBall, this.user.indexOf(userBall));
+            score.put(ballCount, score.get(ballCount) + 1);
+        }
+        return score;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user1 = (User) o;
+        return Objects.equals(user, user1.user);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user);
+    }
+
 }
