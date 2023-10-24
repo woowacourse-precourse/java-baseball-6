81f0f0d313a98c360a4fc762c6d84617214cf7e2
RheeMingyu <stone262@naver.com>
2023-10-24 21:36:02 +0900

    remove comment

--- src/main/java/baseball/EncouterException.java
+++ src/main/java/baseball/EncouterException.java
@@ -9,37 +9,11 @@ public class EncouterException {
 	private final int greatest_3digits=999;
 	
 	public EncouterException(String input) {
-    	
-    	//try {
-	    /*if(!isNumber(input)) {
-	   		//this.flag=true;
-	   		throw new IllegalArgumentException("숫자만 입력해주세요");
-	   	}*/	   	
+    		   	
 	   	int inputNum=Integer.parseInt(validInput(input));
-    	
-    	/*int first_digit=inputNum/100;
-        int second_digit=(inputNum%100)/10;
-        int third_digit=inputNum%10;*/
 	   	int [] parsedData=proc.parsingData(inputNum);
 	    
 	   	exceptionmeter(inputNum, parsedData);
-	    /*if(inputNum<100||inputNum>999) {
-	    	//this.flag=true;
-	    	throw new IllegalArgumentException("3자리 숫자만 입력해주세요");
-		}
-		else if(first_digit==0||second_digit==0||third_digit==0) {
-			//this.flag=true;
-			throw new IllegalArgumentException("각 자리 수는 1~9만 가능합니다");
-		}
-		else if(first_digit==second_digit||first_digit==third_digit||second_digit==third_digit) {
-			//this.flag=true;
-			throw new IllegalArgumentException("모든 자리 수는 달라야 합니다");
-		}*/
-    	//} catch(IllegalArgumentException e) {
-    		//System.out.println(e.toString());
-    		//e.printStackTrace();
-    		//this.flag=true;
-    	//}
 	}
 	
 	public void exceptionmeter(int inputNum,int [] parsedData) {
