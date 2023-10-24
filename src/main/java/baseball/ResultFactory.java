ba813c87a5dd679c1101fb1409937aa81731fc06
DoohyunHwang97 <ghkdengus97@gmail.com>
2023-10-24 16:15:42 +0900

    refactor(ResultFactory): simplify ResultFactory role
    delete comparing method
    intend to focus on role of returning the Result object

--- src/main/java/baseball/ResultFactory.java
+++ src/main/java/baseball/ResultFactory.java
@@ -1,50 +1,9 @@
 package baseball;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class ResultFactory {
-    public Result compareAndGetResult(UserPrediction userPrediction, Answer answer){
-        List<Hint> hints = new ArrayList<>();
-        hints.add(getHint(answer, userPrediction.getFirstNumber(), Order.FIRST));
-        hints.add(getHint(answer, userPrediction.getSecondNumber(), Order.SECOND));
-        hints.add(getHint(answer, userPrediction.getThirdNumber(), Order.THIRD));
-        return getResult(hints);
-    }
-
-    private Hint getHint(Answer answer, Integer number, Order order){
-        if (order == Order.FIRST){
-            if (answer.getFirstNumber() == number){
-                return Hint.STRIKE;
-            }
-            if (answer.getSecondNumber() == number || answer.getThirdNumber() == number){
-                return Hint.BALL;
-            }
-            return Hint.NOTHING;
-        }
-        if (order == Order.SECOND){
-            if (answer.getSecondNumber() == number){
-                return Hint.STRIKE;
-            }
-            if (answer.getFirstNumber() == number || answer.getThirdNumber() == number){
-                return Hint.BALL;
-            }
-            return Hint.NOTHING;
-        }
-        if (order == Order.THIRD){
-            if (answer.getThirdNumber() == number){
-                return Hint.STRIKE;
-            }
-            if (answer.getFirstNumber() == number || answer.getSecondNumber() == number){
-                return Hint.BALL;
-            }
-            return Hint.NOTHING;
-        }
-        return Hint.NOTHING;
-    }
-
-    private Result getResult(List<Hint> hints){
+    public Result getResult(List<Hint> hints){
         int numOfBalls = 0;
         int numOfStrikes = 0;
         for(Hint hint : hints){
@@ -57,6 +16,5 @@ public class ResultFactory {
         }
         return new Result(numOfBalls, numOfStrikes);
     }
-
 }
 
