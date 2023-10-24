56264c160bba009b269df908581ed294a2c7b0ca
jungeun-choi <jungeun5.choi@gmail.com>
2023-10-24 23:01:46 +0900

    feat: 예외처리 - 사용자가 잘못된 값을 입력할 경우, `IllegalArgumentException` 발생

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,10 +5,14 @@ public class Application {
         BaseballView baseballView = new BaseballView();
         BaseballComputer baseballComputer = new BaseballComputer();
         BaseballUmpire baseballUmpire = new BaseballUmpire();
-        BaseballGame baseballGame = new BaseballGame(baseballView, baseballComputer, baseballUmpire);
 
         // 게임 시작
-        baseballView.outputGameStart();
-        baseballGame.start();
+        BaseballGame baseballGame = new BaseballGame(baseballView, baseballComputer, baseballUmpire);
+        try {
+            baseballView.outputGameStart();
+            baseballGame.start();
+        } catch (Exception e) {
+            throw new IllegalArgumentException("1~9 사이의 서로 다른 세 개의 숫자를 공백없이 입력해주세요.");
+        }
     }
 }
