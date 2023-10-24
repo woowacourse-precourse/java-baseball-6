2203e6f56a14c47670e2ec8c3497bf0ac548aae0
kkonii <119216472+kkonii@users.noreply.github.com>
2023-10-25 01:39:01 +0900

    Refactor(validator) : 에러 메세지의 내용을 상수로 치환
    - 코드의 가독성을 높이기 위해 메세지의 의미를 반영한 상수명을 사용했습니다

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -1,24 +1,30 @@
 package baseball;
 
 public class Validator {
+    private final String EMPTY_VALUE_ERROR_MESSAGE = "빈 값은 입력할 수 없습니다.";
+    private final String LENGTH_ERROR_MESSAGE = "세 자리 이외의 값은 입력할 수 없습니다.";
+    private final String TYPE_ERROR_MESSAGE = "숫자 이외의 값은 입력할 수 없습니다.";
+    private final String CONTAIN_ZERO_ERROR_MESSAGE = "0은 입력할 수 없습니다.";
+    private final String RESTART_INPUT_ERROR_MESSAGE = "1과 2 이외의 숫자는 입력할 수 없습니다.";
+
     public void validateNumberInput(String input) {
         if (isEmptyValue(input)) {
-            throw new IllegalArgumentException("빈 값은 입력할 수 없습니다.");
+            throw new IllegalArgumentException(EMPTY_VALUE_ERROR_MESSAGE);
         }
         if (!isCorrectLength(input)) {
-            throw new IllegalArgumentException("세 자리 이외의 값은 입력할 수 없습니다.");
+            throw new IllegalArgumentException(LENGTH_ERROR_MESSAGE);
         }
         if (!isNumberType(input)) {
-            throw new IllegalArgumentException("숫자 이외의 값은 입력할 수 없습니다.");
+            throw new IllegalArgumentException(TYPE_ERROR_MESSAGE);
         }
         if (isContainingZero(input)) {
-            throw new IllegalArgumentException("0은 입력할 수 없습니다.");
+            throw new IllegalArgumentException(CONTAIN_ZERO_ERROR_MESSAGE);
         }
     }
 
     public void validateRestartInput(String input) {
         if (isNotOneOrTwo(input)) {
-            throw new IllegalArgumentException("1과 2 이외의 숫자는 입력할 수 없습니다.");
+            throw new IllegalArgumentException(RESTART_INPUT_ERROR_MESSAGE);
         }
     }
 
