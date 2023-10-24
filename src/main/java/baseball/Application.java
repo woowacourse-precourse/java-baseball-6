66e1f0dd11ccd6b1177aea86a1d58b6587221159
Jamieie <116743317+Jamieie@users.noreply.github.com>
2023-10-24 14:23:07 +0900

    Feat(Applicatio): 메인 코드 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,56 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        GameOnOff gameOnOff = new GameOnOff();
+
+        int gameButton;
+        gameButton = gameOnOff.gameStart();
+        while (gameButton == 1) {
+            // 컴퓨터 랜덤 숫자 선택
+            Computer computer = new Computer();
+            List<Integer> computerNumber = computer.getComputerNumber();
+            System.out.println(computerNumber);
+
+            String result = "";
+            while (!result.equals("3스트라이크")){
+                try {
+                    // 플레이어 숫자 입력
+                    Player player = new Player();
+                    List<Integer> playerNumber = player.getPlayerNumber();
+
+                    // 숫자 비교
+                    Result compareResult = new Result();
+                    String compared = compareResult.compareNumbers(computerNumber, playerNumber);
+                    System.out.println(compared);
+
+                    result = compared;
+                }
+                catch (IllegalArgumentException ex) {
+                    System.out.println(ex.getClass());
+                    return;
+                }
+            }
+
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
+            // 게임 지속 여부 입력
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            try {
+                gameButton = gameOnOff.gameContinue(Console.readLine());
+            }
+            catch (IllegalArgumentException ex){
+                System.out.println(ex.getClass());
+                return;
+            }
+        }
+
+        System.out.println("게임을 종료합니다.");
+
     }
 }
