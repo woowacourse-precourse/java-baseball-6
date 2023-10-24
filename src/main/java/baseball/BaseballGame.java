008ecd88fdd79c73c0260201555e63676176ae9a
mseo39 <a01063294905@gmail.com>
2023-10-25 02:11:48 +0900

    feat: add generateRandomNumber

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -9,4 +9,15 @@ public class BaseballGame {
     final static int randomRangestart=0;
     final static int randomRangeend=9;
     final static int listSize=3;
+
+    public static void generateRandomNumber(){
+        List<Integer> tempComputerNumber = new ArrayList<>();
+        while (tempComputerNumber.size() < listSize) {
+            int randomNumber = Randoms.pickNumberInRange(randomRangestart, randomRangeend);
+            if (!tempComputerNumber.contains(randomNumber)) {
+                tempComputerNumber.add(randomNumber);
+            }
+        }
+        computerNumber=tempComputerNumber;
+    }
 }
