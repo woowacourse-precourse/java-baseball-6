1b9302e0df26c73439a18d1b601b439c3cfca56f
Yang woosung <ngxk1217@gmail.com>
2023-10-25 01:35:01 +0900

    사용자로 부터 세자리 수 입력 받음

--- src/main/java/baseball/random/RandomNumbers.java
+++ src/main/java/baseball/random/RandomNumbers.java
@@ -1,4 +1,34 @@
 package baseball.random;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class RandomNumbers {
+    private static final int START_INCLUSIVE = 1;
+    private static final int END_INCLUSIVE = 9;
+    private static final int COUNT = 3;
+
+    private static List<BaseBallNumber> randomNumbers;
+
+    public RandomNumbers() {
+        this.randomNumbers = extractRandomNumbers();
+    }
+
+    private List<BaseBallNumber> extractRandomNumbers() {
+        List<BaseBallNumber> randomNumbers = new ArrayList<>();
+
+        while (randomNumbers.size() != COUNT) {
+            BaseBallNumber randomNumber = new BaseBallNumber(Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE));
+            if (!randomNumbers.contains(randomNumber)) {
+                randomNumbers.add(randomNumber);
+            }
+        }
+        return randomNumbers;
+    }
+
+    public List<BaseBallNumber> randomNumbers() {
+        return Collections.unmodifiableList(randomNumbers);
+    }
 }
