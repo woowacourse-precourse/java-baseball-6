ee51e94ec1c944725ae932dc4075a8db47843f00
chan-won <clw8679@naver.com>
2023-10-24 10:38:47 +0900

    Refactor: Application 구조 개선

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,24 +9,31 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         Output.printGameStart();
-        boolean gameEndFlag = true;
-        while(gameEndFlag) {
-            List<Integer> computer = RandomNumberGenerator.generate();
-            while (true) {
-                List<Integer> user = Input.numbers();
 
-                int strikeCount = CompareNumber.countStrike(computer, user);
-                int ballCount = CompareNumber.countBall(computer, user);
+        boolean playAgain = true;
 
-                Output.printGameResult(strikeCount, ballCount);
+        while(playAgain) {
+            playGame();
 
-                if (strikeCount == 3) {
-                    Output.printGameEnd();
-                    break;
-                }
+            Output.printGameEnd();
+            playAgain = Input.playAgain();
+        }
+    }
+
+    private static void playGame() {
+        List<Integer> computer = RandomNumberGenerator.generate();
+
+        while (true) {
+            List<Integer> user = Input.numbers();
+
+            int strikeCount = CompareNumber.countStrike(computer, user);
+            int ballCount = CompareNumber.countBall(computer, user);
+
+            Output.printGameResult(strikeCount, ballCount);
+
+            if (strikeCount == 3) {
+                break;
             }
-
-            gameEndFlag = Input.playAgain();
         }
     }
 }
