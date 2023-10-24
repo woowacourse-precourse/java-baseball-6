8bcee70ea243025b23ede2112c67342a2fc825be
nahyeon99 <nahyeonee99@gmail.com>
2023-10-24 16:05:44 +0900

    feat: 스트라이크, 볼 개수 구하는 기능 및 equals 구현 BaseballNumbers.java 1. 스트라이크, 볼 개수 구하는 메서드 추가 2. 유저와 컴퓨터 수 비교를 위한 equals(), hashCode() 오버라이드

--- src/main/java/baseball/domain/BaseballNumbers.java
+++ src/main/java/baseball/domain/BaseballNumbers.java
@@ -3,11 +3,14 @@ package baseball.domain;
 import static baseball.console.Constant.EXCEPTION_BASEBALL_NUMBERS_DUPLICATE_MESSAGE;
 import static baseball.console.Constant.EXCEPTION_BASEBALL_NUMBERS_SIZE_MESSAGE;
 
+import baseball.console.Constant;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
 
 public class BaseballNumbers {
 
-    private static final int NUMBERS_MAX_SIXE = 3;
+    static final int NUMBERS_MAX_SIXE = 3;
     private final List<BaseballNumber> numbers;
 
     public BaseballNumbers(List<BaseballNumber> numbers) {
@@ -26,5 +29,39 @@ public class BaseballNumbers {
         if (numbers.stream().distinct().count() != NUMBERS_MAX_SIXE) {
             throw new IllegalArgumentException(EXCEPTION_BASEBALL_NUMBERS_DUPLICATE_MESSAGE);
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        BaseballNumbers that = (BaseballNumbers) o;
+        return NUMBERS_MAX_SIXE == (int) IntStream.range(Constant.INT_ZERO, NUMBERS_MAX_SIXE)
+                .filter(i -> numbers.get(i).equals(that.numbers.get(i)))
+                .count();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numbers);
+    }
+
+    int getStrikeCount(BaseballNumbers o) {
+        return (int) IntStream.range(Constant.INT_ZERO, NUMBERS_MAX_SIXE)
+                .filter(i -> numbers.get(i).equals(o.numbers.get(i)))
+                .count();
+    }
+
+    int getBallCount(BaseballNumbers o) {
+        return (int) IntStream.range(Constant.INT_ZERO, NUMBERS_MAX_SIXE)
+                .filter(i -> !numbers.get(i).equals(o.numbers.get(i)) &&
+                        o.numbers.stream()
+                                .filter(j -> j.equals(this.numbers.get(i)))
+                                .count() == 1)
+                .count();
     }
 }
