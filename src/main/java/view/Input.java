27b5870b2247e6290786fbbca664256292cf356d
34suuuuu <psh7492@gmail.com>
2023-10-24 20:21:09 +0900

    style: 출력 메시지 줄바꿈 수정

--- src/main/java/view/Input.java
+++ src/main/java/view/Input.java
@@ -7,14 +7,14 @@ public class Input {
 
     // Player: 3자리 숫자 입력
     public static String readInput(){
-        System.out.println(MessageConstants.INPUT_MESSAGE);
+        System.out.print(MessageConstants.INPUT_MESSAGE);
         String input = Console.readLine();
         return input;
     }
 
     // Player: 재시작(1) or 종료(2) 입력
     public static String readOption(){
-        System.out.println(MessageConstants.RESTART_OR_FINISH_MESSAGE);
+        System.out.print(MessageConstants.RESTART_OR_FINISH_MESSAGE);
         String input = Console.readLine();
         return input;
     }
