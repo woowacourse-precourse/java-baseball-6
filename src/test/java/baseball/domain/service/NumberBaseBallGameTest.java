fdc2c601361ddcec12af5eb0ad63c61c426671d8
wnsgh12s <junho1273@gmail.com>
2023-10-25 02:30:30 +0900

    Test: 게임종료 번호에 따라 게임상태가 올바르게 변경되는지 테스트

--- src/test/java/baseball/domain/service/NumberBaseBallGameTest.java
+++ src/test/java/baseball/domain/service/NumberBaseBallGameTest.java
@@ -1,7 +1,37 @@
 package baseball.domain.service;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import baseball.domain.util.RandomNumberGenerator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 class NumberBaseBallGameTest {
 
+    private static final String GENERATED_NUMBER = "133";
+
+    @Mock
+    private RandomNumberGenerator randomNumberGenerator;
+
+    private NumberBaseballScoreEvaluator evaluator;
+    private NumberBaseBallGame numberBaseBallGame;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        evaluator = new NumberBaseballScoreEvaluator();
+        when(randomNumberGenerator.generateNumbers()).thenReturn(GENERATED_NUMBER);
+        numberBaseBallGame = new NumberBaseBallGame(randomNumberGenerator, evaluator);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"123", "456", "789", GENERATED_NUMBER})
+    void 입력된_숫자에_따른_게임오버_여부_테스트(String inputNumber) {
+        RoundEvaluationResult result = numberBaseBallGame.evaluateRound(inputNumber);
+        assertEquals(GENERATED_NUMBER.equals(inputNumber), result.isgameOver());
+    }
 }
