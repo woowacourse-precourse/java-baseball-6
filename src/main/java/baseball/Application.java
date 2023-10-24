8e30fb87fdf0aa6f6863008c572e965542280196
kijen723 <kij2646@gmail.com>
2023-10-25 04:26:23 +0900

    fix: 재시작 선택 기능 버그 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -72,13 +72,13 @@ public class Application {
                     System.out.println("낫싱");
                 }
             }
-        }
 
-        // TODO6: 게임 종료 후 게임 재시작 유무를 결정
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        Scanner scanner = new Scanner(System.in);
-        gamestart = scanner.nextInt();
+            // TODO6: 게임 종료 후 게임 재시작 유무를 결정
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            Scanner scanner = new Scanner(System.in);
+            gamestart = scanner.nextInt();
+        }
 
     }
 }
