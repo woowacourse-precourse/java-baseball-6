c800a4c373ccc4134aef775692f44188404e6946
jaepyo99 <jaepyo99@naver.com>
2023-10-24 13:22:38 +0900

    [feat] 컴퓨터가 야구숫자 만드는 기능 추가

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,4 +1,21 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Computer {
+    Validator validator= new Validator();
+    private static final List<Integer> baseballNum = new ArrayList<>();
+    public List<Integer>createValidBaseballNum(){
+        while(baseballNum.size()<3){
+            int invalidNum = Randoms.pickNumberInRange(1, 9);
+            if(!validator.validNumNotInList(invalidNum,baseballNum)){
+                continue;
+            }
+            baseballNum.add(invalidNum);
+        }
+        return baseballNum;
+    }
 }
--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 public class Validator {
 
+    public Validator() {
+    }
+
     // 컴퓨터를 통한 랜덤 야구번호를 검증해주는 메서드
     public Boolean validNumNotInList(Integer invalidNum, List<Integer>validNumList){
         return validNumList.contains(invalidNum);
