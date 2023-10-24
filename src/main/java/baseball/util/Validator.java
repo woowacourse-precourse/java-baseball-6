871d8a038b86d39c26a5876370515c71e2a9b40c
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2023-10-24 15:48:44 +0900

    feat: 생성된 리스트에 중복된 숫자가 있는지 확인한다.

--- src/main/java/baseball/util/Validator.java
+++ src/main/java/baseball/util/Validator.java
@@ -1,11 +1,20 @@
 package baseball.util;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public final class Validator {
-    public static void validateListSize(List<Integer> list, int size) throws IllegalArgumentException {
+    public static void validateListSize(List<Integer> list, int size) {
         if(list.size() != size) {
             throw new IllegalArgumentException("리스트의 크기는 3이어야 합니다!");
+        }
+    }
+
+    public static void validateDuplicateList(List<Integer> list) {
+        Set<Integer> validationList = new HashSet<>(list);
+        if (validationList.size() != list.size()) {
+            throw new IllegalArgumentException("중복된 숫자가 존재합니다.");
         }
     }
 }
