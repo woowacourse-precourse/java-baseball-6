dfe034f51dea551a0b9b896b46ae6b18776ef17b
donghoony <aru0504@naver.com>
2023-10-24 23:07:10 +0900

    docs: 주석 설명 수정

--- src/main/java/baseball/NumbersInputValidator.java
+++ src/main/java/baseball/NumbersInputValidator.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 /**
  * 사용자의 입력을 검증하는 클래스.
- * <p>단순한 입력 검증이 목표이며, 검증 성공 여부만을 알려준다. 추가적인 예외 발생 등은 PlayerInput에서 처리한다.
+ * <p>단순한 입력 검증이 목표이며, 검증 성공 여부만을 알려준다.
  */
 public class NumbersInputValidator {
 
