db34e3a6bb2ba1fac3df7be069500ad4e3baca80
chosunghyun18 <krismon@naver.com>
2023-10-25 00:05:41 +0900

    feat : add overRide equals and hash code to test init balls in computer - 3가지 숫자를 선택하여 공을 만든다.

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -1,12 +1,14 @@
 package baseball.model;
 
+import baseball.model.ball.Balls;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
-    List<Integer> numbers = new ArrayList<>();
+    Balls balls;
     public List<Integer> pickNumbers() {
+        List<Integer> numbers = new ArrayList<>();
         while (numbers.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!numbers.contains(randomNumber)) {
@@ -14,5 +16,9 @@ public class Computer {
             }
         }
         return numbers;
+    }
+    public Balls initBallsForGame(List<Integer> numbers) {
+        balls = new Balls(numbers);
+        return balls;
     }
 }
--- src/main/java/baseball/model/ball/Balls.java
+++ src/main/java/baseball/model/ball/Balls.java
@@ -3,6 +3,7 @@ package baseball.model.ball;
 import baseball.model.SwingResult;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Balls {
     private List<Ball> balls = new ArrayList<>();
@@ -28,5 +29,21 @@ public class Balls {
             swingResult.report(this.match(item));
         }
         return swingResult;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Balls balls1 = (Balls) o;
+        return Objects.equals(balls, balls1.balls);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(balls);
     }
 }
