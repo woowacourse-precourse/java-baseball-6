a376db473bc0248e1f6fc24dd8b7764039f39a6c
dhl1924 <dhl1924@naver.com>
2023-10-24 14:17:48 +0900

    feat: manage the constant

--- src/main/java/baseball/constants/NumberConstants.java
+++ src/main/java/baseball/constants/NumberConstants.java
@@ -4,11 +4,11 @@ public class NumberConstants {
     private final int RESTART = 1;
     private final int QUIT = 2;
 
-    public boolean doRestart(int number) {
+    public boolean wantRestart(int number) {
         return number == RESTART;
     }
 
-    public boolean doQuit(int number) {
+    public boolean wantQuit(int number) {
         return number == QUIT;
     }
 }
