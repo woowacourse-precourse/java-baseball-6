638443c6c815ce8dc89e940b6607faf6c3faf62c
dltjdn <hana40004@naver.com>
2023-10-25 05:06:07 +0900

    fix(InputView): 프린트 후 줄바꿈 오류 수정

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -10,7 +10,7 @@ public class InputView {
     private static final String RESTART_OR_END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public Number readNumberInput() {
-        System.out.println(ASK_INPUT_MESSAGE);
+        System.out.print(ASK_INPUT_MESSAGE);
         int number = readLineParseInt();
 
         validateInputNumberSize(number);
