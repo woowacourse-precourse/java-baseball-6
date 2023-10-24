e70b21198aff1bf476fca4570f22b1faf41b5748
hak2711 <hak271111@gmail.com>
2023-10-24 13:29:25 +0900

    Feat: 프로그램 종료 기능 구현
    게임 종료 후에 사용자 선택에 따라 게임을 새로 시작하거나 완전히 종료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,13 +1,33 @@
 package baseball;
 
 import baseball.controller.BaseballGame;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Objects;
 
 public class Application {
+    public static boolean continueGame(String s) {
+        if (Objects.equals(s, "2")) {
+            return false;
+        } else if (Objects.equals(s, "1")) {
+            return true;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        BaseballGame baseballGame = new BaseballGame();
-        baseballGame.startGame();
+        System.out.println("숫자 야구 게임을 시작합니다.");
 
+        while (true) {
+            BaseballGame baseballGame = new BaseballGame();
+            baseballGame.startGame();
 
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String s = Console.readLine();
+
+            if (!continueGame(s)) {
+                break;
+            }
+        }
     }
 }
