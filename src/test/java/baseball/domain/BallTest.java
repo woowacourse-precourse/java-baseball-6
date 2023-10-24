0529c23173600e5c48ea3b9de0b4b97b26350a28
jifrozen0110 <answldjs1836@gmail.com>
2023-10-24 18:12:51 +0900

    test : BallTest

---
+++ src/test/java/baseball/domain/BallTest.java
@@ -0,0 +1,24 @@
+package baseball.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+class BallTest {
+
+    @Test
+    void 정상으로_Ball_enum이_생성() {
+        assertEquals(Ball.ZERO, Ball.of(0));
+        assertEquals(Ball.ONE, Ball.of(1));
+        assertEquals(Ball.TWO, Ball.of(2));
+        assertEquals(Ball.THREE, Ball.of(3));
+    }
+
+    @Test
+    void 범위에_벗어나면_IllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> Ball.of(4), BaseballMessageConst.OUT_OF_RANGE_BALL_ERROR);
+        assertThrows(IllegalArgumentException.class, () -> Ball.of(-1), BaseballMessageConst.OUT_OF_RANGE_BALL_ERROR);
+    }
+
+}
