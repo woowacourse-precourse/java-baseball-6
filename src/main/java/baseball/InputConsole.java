07ec8286fded22a7aec784a4ae5631006921f777
DoohyunHwang97 <ghkdengus97@gmail.com>
2023-10-24 18:55:05 +0900

    refactor(InputConsole, OutputConsole): change strings into constant properties

--- src/main/java/baseball/InputConsole.java
+++ src/main/java/baseball/InputConsole.java
@@ -7,6 +7,11 @@ import java.util.List;
 import java.util.Objects;
 
 public class InputConsole {
+    private static final String ALLOWONLYONEORTWO = "게임 재시작 여부는 1과 2만 받을 수 있습니다.";
+    private static final String LENGTHSHOULDBETHREE = "입력길이는 3이어야 합니다.";
+    private static final String ALLOWONLYDIGIT = "입력길이는 3이어야 합니다.";
+    private static final String NOTALLOWDUPLICATED = "입력길이는 3이어야 합니다.";
+
     public String readPrediction(){
         String input = Console.readLine();
         return getValidated(input);
@@ -16,7 +21,7 @@ public class InputConsole {
         String input = Console.readLine();
         if (Objects.equals(input, "1") || Objects.equals(input, "2")){
             return input;
-        } else throw new IllegalArgumentException("게임 재시작 여부는 1과 2만 받을 수 있습니다.");
+        } else throw new IllegalArgumentException(ALLOWONLYONEORTWO);
     }
 
     private String getValidated(String input){
@@ -27,14 +32,14 @@ public class InputConsole {
     }
 
     private void validateLength(String input){
-        if (input.length() != 3) throw new IllegalArgumentException("입력길이는 3이어야 합니다.");
+        if (input.length() != 3) throw new IllegalArgumentException(LENGTHSHOULDBETHREE);
     }
 
     private void validateDigitOnly(String input){
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
             if (!Character.isDigit(c)) {
-                throw new IllegalArgumentException("숫자가 아닌 입력이 있습니다.");
+                throw new IllegalArgumentException(ALLOWONLYDIGIT);
             }
         }
     }
@@ -50,6 +55,6 @@ public class InputConsole {
             }
         }
 
-        if (result.size() != 3) throw new IllegalArgumentException("중복된 숫자가 있습니다.");
+        if (result.size() != 3) throw new IllegalArgumentException(NOTALLOWDUPLICATED);
     }
 }
--- src/main/java/baseball/OutputConsole.java
+++ src/main/java/baseball/OutputConsole.java
@@ -1,33 +1,41 @@
 package baseball;
 
 public class OutputConsole {
+    private static final String BALL = "볼";
+    private static final String STRIKE = "스트라이크";
+    private static final String NOTHING = "낫싱";
+    private static final String PREDICTIONINPUTFORM = "숫자를 입력해주세요 : ";
+    private static final String STARTMESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String REGAMEMESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String GAMEFINISHEDMESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
     public void printResult(int numOfBalls, int numOfStrikes){
         if (numOfBalls != 0 && numOfStrikes != 0){
-            System.out.println(numOfBalls + "볼 " + numOfStrikes + "스트라이크");
+            System.out.println(numOfBalls + BALL + " " + numOfStrikes + STRIKE);
         }
         else if (numOfBalls != 0 & numOfStrikes == 0){
-            System.out.println(numOfBalls + "볼");
+            System.out.println(numOfBalls + BALL);
         }
         else if (numOfStrikes != 0) {
-            System.out.println(numOfStrikes + "스트라이크");
+            System.out.println(numOfStrikes + STRIKE);
         }
         else {
-            System.out.println("낫싱");
+            System.out.println(NOTHING);
         }
     }
     public void printInputForm(){
-        System.out.print("숫자를 입력해주세요 : ");
+        System.out.print(PREDICTIONINPUTFORM);
     }
 
     public void printStartMessage(){
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println(STARTMESSAGE);
     }
 
     public void printRegameMessage(){
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println(REGAMEMESSAGE);
     }
 
     public void printGameFinishedMessage(){
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println(GAMEFINISHEDMESSAGE);
     }
 }
