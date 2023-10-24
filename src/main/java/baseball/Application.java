eb0354ddac9ce00d2a132d584ceb22209a48409b
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-10-24 14:49:48 +0900

    [Refactor] 숫자 야구 게임 구현 리팩토링

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,23 +1,19 @@
 package baseball;
 
-import static baseball.User.END_GAME;
-
 public class Application {
-    
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
 
     public static void main(String[] args) {
 
         System.out.println(GAME_START_MESSAGE);
 
-        Game game = new Game();
+        BaseballGame baseballGame = new BaseballGame();
 
         while (true) {
-            game.startGame();
-            if (END_GAME.equals(User.startNewGameOrNot())) {
+            baseballGame.start();
+            if (!baseballGame.isNewGame()) {
                 break;
             }
         }
-
     }
 }
--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -5,15 +5,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
-    
+
     private static final int START_NUMBER_OF_RANDOM = 1;
     private static final int END_NUMBER_OF_RANDOM = 9;
-    private static final int COMPUTER_NUMBER_LENGTH = 3;
 
-    public static List<Integer> createRandomComputerNumber() {
-        List<Integer> computerNumber = new ArrayList<>();
+    private List<Integer> computerNumber;
+    
+    public List<Integer> createNumber() {
+        computerNumber = new ArrayList<>();
 
-        while (computerNumber.size() < COMPUTER_NUMBER_LENGTH) {
+        while (computerNumber.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(START_NUMBER_OF_RANDOM, END_NUMBER_OF_RANDOM);
             if (!computerNumber.contains(randomNumber)) {
                 computerNumber.add(randomNumber);
