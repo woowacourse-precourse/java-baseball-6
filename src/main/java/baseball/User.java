e69812d5e7dc9f47cd47f9d38fb318031fcc1afc
jaepyo99 <jaepyo99@naver.com>
2023-10-24 19:44:48 +0900

    [Feat[ 게임 지속 여부 확인 메서드 추가

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,7 +1,8 @@
 package baseball;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
-
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
 
 public class User {
     private static final String INPUT_NUMBER_MENT = "숫자를 입력해주세요 : ";
@@ -14,5 +15,13 @@ public class User {
         return readLine();
     }
 
-
+    public Boolean continueGame() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String flag = readLine();
+        if (flag.equals("1")) {
+            return TRUE;
+        }
+        return FALSE;
+    }
 }
