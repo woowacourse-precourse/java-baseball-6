c4c6fc1ee70526aea8f3abc1dc1b314ead1d4a41
haiseong <wjdgotjd9908@gmail.com>
2023-10-24 12:09:12 +0900

    fix: getRandomValueNotInList: static 제어자 제거

--- src/main/java/baseball/model/numbers/RandomAnswerGenerator.java
+++ src/main/java/baseball/model/numbers/RandomAnswerGenerator.java
@@ -7,7 +7,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RandomAnswerGenerator implements AnswerGenerator{
+public class RandomAnswerGenerator implements AnswerGenerator {
     private static final int START_INCLUSIVE = 1;
     private static final int END_INCLUSIVE = 9;
 
@@ -20,7 +20,7 @@ public class RandomAnswerGenerator implements AnswerGenerator{
         return new GameNumbers(list);
     }
 
-    private static int getRandomValueNotInList(List<Integer> list) {
+    private int getRandomValueNotInList(List<Integer> list) {
         int randomNumber = Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
         while (list.contains(randomNumber)) {
             randomNumber = Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
