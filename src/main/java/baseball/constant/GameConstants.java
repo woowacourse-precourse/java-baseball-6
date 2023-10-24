92419a430e3e0714369cb3af55eef6d6cb5d8891
yoon <leeyt1201@gmail.com>
2023-10-25 00:35:28 +0900

    feat: implemented the player number input function - InputValidator: 길이 검증, 숫자 검증, 중복 검증 - Parser: 문자열을 정수형의 리스트로 변환

--- src/main/java/baseball/constant/GameConstants.java
+++ src/main/java/baseball/constant/GameConstants.java
@@ -2,7 +2,9 @@ package baseball.constant;
 
 public final class GameConstants {
 
+    public static final String NUMBER_REGX = "\\d+";
+
     public static final int BALL_MIN_NUMBER = 1;
-    public static final int BALL_MAX_NUMBER = 1;
+    public static final int BALL_MAX_NUMBER = 9;
     public static final int BALL_COUNT = 3;
 }
