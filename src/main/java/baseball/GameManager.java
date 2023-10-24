97e460869407bb6dd9f987e4b7aff0e3ba6f737f
Cycrypto <jh01love@naver.com>
2023-10-24 13:51:57 +0900

    feat: 사용자 입력 검증 매서드 완료

--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -1,12 +1,17 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class GameManager {
     private String playerGuessedNumber;
-    private GameCompititor counterPlayer;
+//    private GameCompititor counterPlayer;
     private GameStatus gameStatus;
 
     public GameManager(){
-        this.computerGuessedNumber = new GameCompititor();
+//        this.computerGuessedNumber = new GameCompititor();
     }
 
     public GameStatus processTurn(String playerNumber){
@@ -19,19 +24,26 @@ public class GameManager {
         }
         return gameStatus;
     }
-    public GameStatus compareBetweenNumbers(String playerNomber){
-        String computerNumber = counterPlayer.generateRandomNumber();
-        return GameStatus.PLAYING;
-    }
+//    public GameStatus compareBetweenNumbers(String playerNomber){
+//
+//    }
     public boolean isValidForm(String target){
         boolean flag = true;
+        Set<Object> set = new HashSet<>();
+
         if (target.length() != 3){
             flag = false;
         }
         for (int i = 0; i < target.length(); i++){
-            if (!Character.isDigit(target.charAt(i)))
+            char indexValue = target.charAt(i);
+            if (!Character.isDigit(indexValue) || !(('0' < indexValue)&&(indexValue < '9')))
                 flag = false;
+
+            if(!set.add(indexValue)){
+                flag = false;
+            }
         }
+
         return flag;
     }
 }
