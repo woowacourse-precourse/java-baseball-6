6e69fdcb794952d0c8c6d004c8250a7a0d7dcc41
한별 <hanbyeol@hanbyeols-MacBook-Pro.local>
2023-10-25 01:54:04 +0900

    feat.랜덤숫자 생성 메서드 추가 랜덤 숫자를 생성하는 메서드 정의, 호출 코드 추가했습니다.

--- src/test/java/constant/Constant.java
+++ src/test/java/constant/Constant.java
@@ -4,5 +4,6 @@ public class Constant{
     public static String ASK_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     public static int RESTART_GAME_NUMBER = 1;
     public static int EXIT_GAME_NUMBER = 2;
+    public static int NUM_DIGITS=3;
        
 }
--- src/test/java/game/Game.java
+++ src/test/java/game/Game.java
@@ -7,6 +7,12 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Game{
 
+    public static void start(){
+        do{
+            List<Integer> targetNumber = new ArrayList<>(getRandomNum());
+        }while(restart());
+    }
+
 //  게임 재시작 여부
     private boolean restart(){
         System.out.println(ASK_RESTART_MESSAGE);
@@ -18,4 +24,15 @@ public class Game{
         if(UserInput==EXIT_GAME_NUMBER) return false;
     }
 
+//랜덤값 추출
+    private List<Integer> getRandomNum(){
+        List<Integer> randomNumber = new ArrayList<>();
+        while (randomNumber.size() < 3) {
+            int Num = Randoms.pickNumberInRange(1, 9);
+            if (!randomNumber.contains(Num)) {
+                randomNumber.add(Num);
+            }
+        }
+        return randomNumber;
+    }
 }
