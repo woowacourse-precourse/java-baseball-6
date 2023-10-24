5d0392be5b2a58f4aaa9585ec1a2fcfba8a72f4e
Cycrypto <jh01love@naver.com>
2023-10-24 17:49:43 +0900

    refactor: 프로그램 시작점 역할 수정
    run에서 판단하던 재실행 사이클 player로 이동

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,17 +1,10 @@
 package baseball;
 
 public class Application {
-    private GameParticipant player;
-    private GameStatus replay;
+
     public static void main(String[] args) {
-        Application app = new Application();
+        GameParticipant app = new GameParticipant();
         app.run();
     }
-    public void run(){
-        while(this.replay != GameStatus.STOP){
-            player = new GameParticipant();
-            replay = player.playGameByRequest();
-           System.out.println("3개의 숫자를 모두 맞췄습니다. 게임 종료");
-        }
-    }
+
 }
