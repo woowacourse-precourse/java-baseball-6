99a9f51fb8f7ef05d6accbd6ca4c038a0f941d97
leesangjun <hubls@naver.com>
2023-10-24 20:42:26 +0900

    feat(InputValidator): 예외 문구 출력 추가

--- src/main/java/baseball/util/InputValidator.java
+++ src/main/java/baseball/util/InputValidator.java
@@ -4,6 +4,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class InputValidator {
+    private static final String INVALID_RESTART_OR_END_NUMBER = "1, 2 가 아닌 잘못된 값을 입력하였습니다.";
+    private static final String INVALID_PLAYER_NUMBER  = "잘못 입력하였습니다.";
     private static final Pattern playerNumberValidationPattern = Pattern.compile("^(?!.*(.).*\\1)[1-9]{3}$");
     private static final Pattern oneOrTwoPattern = Pattern.compile("^[12]$");
 
@@ -11,7 +13,7 @@ public class InputValidator {
         Matcher matcher = playerNumberValidationPattern.matcher(number);
 
         if (!matcher.matches()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(INVALID_PLAYER_NUMBER);
         }
     }
 
@@ -19,7 +21,7 @@ public class InputValidator {
         Matcher matcher = oneOrTwoPattern.matcher(number);
 
         if (!matcher.matches()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(INVALID_RESTART_OR_END_NUMBER);
         }
     }
 }
