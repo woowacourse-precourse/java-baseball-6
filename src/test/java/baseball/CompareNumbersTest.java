97720c132f91c87f12b6d6692a4e5ba98d82a419
ChanUi <hcu55@naver.com>
2023-10-24 23:30:26 +0900

    :sparkles: 사용자 입력과 상대방(컴퓨터) 랜덤 숫자 비교 검증자 테스트

--- src/test/java/baseball/CompareNumbersTest.java
+++ src/test/java/baseball/CompareNumbersTest.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import baseball.model.CompareNumbers;
+import baseball.validator.CompareValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -43,5 +44,40 @@ public class CompareNumbersTest {
         final List<Integer> playerNumbers = List.of(3, 5, 1);
 
         assertThat(new CompareNumbers().ballCount(computerNumbers, playerNumbers)).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("숫자 비교 볼 개수 테스트")
+    void 볼_개수_비교_검증테스트() {
+        CompareValidator compareValidator = new CompareValidator();
+        final List<Integer> computerNumbers = List.of(1, 3, 5);
+        final List<Integer> playerNumbers = List.of(4, 6, 1);
+
+        int[] resultBall = compareValidator.getNumberCompare(computerNumbers, playerNumbers);
+        assertThat(resultBall[0]).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("숫자 비교 스트라이크 개수 테스트")
+    void 스트라이크_개수_비교_검증테스트() {
+        CompareValidator compareValidator = new CompareValidator();
+        final List<Integer> computerNumbers = List.of(1, 3, 5);
+        final List<Integer> playerNumbers = List.of(1, 4, 5);
+
+        int[] resultStrike = compareValidator.getNumberCompare(computerNumbers, playerNumbers);
+        assertThat(resultStrike[1]).isEqualTo(2);
+
+    }
+
+    @Test
+    @DisplayName("숫자 비교 볼, 스트라이크 개수 테스트")
+    void 볼_스트라이크_개수_비교_검증테스트() {
+        CompareValidator compareValidator = new CompareValidator();
+        final List<Integer> computerNumbers = List.of(1, 3, 5);
+        final List<Integer> playerNumbers = List.of(4, 1, 5);
+
+        int[] resultCount = compareValidator.getNumberCompare(computerNumbers, playerNumbers);
+        assertThat(resultCount[0]).isEqualTo(1);
+        assertThat(resultCount[1]).isEqualTo(1);
     }
 }
