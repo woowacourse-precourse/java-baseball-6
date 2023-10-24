c5132c4d93795bdcc94e896dea8da1865d291b43
minyeong-lee <lmy1366@gmail.com>
2023-10-25 00:29:53 +0900

    feat(baseball): create Judgement code

---
+++ src/main/java/baseball/domain/Judgement.java
@@ -0,0 +1,22 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Judgement {
+
+  public int checkCount(List<Integer> computer, List<Integer> player){
+
+    return 0;
+  }
+
+
+  public boolean specificKnow(int placeNumber, int number){
+
+    return false;
+  }
+
+
+
+
+
+}
