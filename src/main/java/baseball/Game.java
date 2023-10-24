b54efe650f7685759d1b0a0d71719a24fd17e07c
wonjun <wonjon0930@daum.net>
2023-10-24 17:31:06 +0900

    refactor: refactor code

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -21,11 +21,15 @@ public class Game {
         this.status = Status.START;
         System.out.println("숫자 야구 게임을 시작합니다.");
         while(status != Status.TERMINATED){
-            switch (status) {
-                case START -> start();
-                case PROGRESS -> progress();
-                case FINISH -> finish();
-            }
+            playGame();
+        }
+    }
+
+    private void playGame(){
+        switch (status) {
+            case START -> start();
+            case PROGRESS -> progress();
+            case FINISH -> finish();
         }
     }
 
