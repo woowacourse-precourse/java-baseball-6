9dfb19514e7dd1fd2fea7a18e64262cd3a3fe7e4
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-10-25 00:35:22 +0900

    refactor : InputValidator 값의 상수화

--- src/main/java/baseball/InputValidator.java
+++ src/main/java/baseball/InputValidator.java
@@ -9,10 +9,11 @@ public class InputValidator {
     public static final String NOT_INT_ERROR_MESSAGE = "숫자가 아닙니다.";
     public static final String CONTAINS_ZERO_ERROR_MESSAGE = "자릿 수 값에 0이 존재합니다.";
     public static final String NOT_THREE_DIGIT_ERROR_MESSAGE = "자릿 수가 3자리 초과거나, 미만입니다.";
-    public static final char ZERO = '0';
+    public static final int ZERO = 0;
     public static final String DUPLICATE_NUMBER_ERROR_MESSAGE = "중복되는 숫자가 존재합니다.";
     public static final String RESTART_NUMBER = "1";
     public static final String EXIT_NUMBER = "2";
+    public static final int CORRECT_SIZE = 3;
 
     public void valid(String value) {
         isNotInteger(value);
@@ -37,15 +38,15 @@ public class InputValidator {
 
     private void isZero(String value) {
         for (char c : value.toCharArray()) {
-            int number = c - ZERO;
-            if (number == 0) {
+            int number = c - '0';
+            if (number == ZERO) {
                 throw new IllegalArgumentException(CONTAINS_ZERO_ERROR_MESSAGE);
             }
         }
     }
 
     private void isNot3Digit(String value) {
-        if (value.length() != 3) {
+        if (value.length() != CORRECT_SIZE) {
             throw new IllegalArgumentException(NOT_THREE_DIGIT_ERROR_MESSAGE);
         }
     }
@@ -53,11 +54,11 @@ public class InputValidator {
     private void isNotDuplicate(String value) {
         Set<Integer> duplicationTestSet = new HashSet<>();
         for (char c : value.toCharArray()) {
-            int number = c - ZERO;
+            int number = c - '0';
             duplicationTestSet.add(number);
         }
 
-        if (duplicationTestSet.size() != 3) {
+        if (duplicationTestSet.size() != CORRECT_SIZE) {
             throw new IllegalArgumentException(DUPLICATE_NUMBER_ERROR_MESSAGE);
         }
     }
