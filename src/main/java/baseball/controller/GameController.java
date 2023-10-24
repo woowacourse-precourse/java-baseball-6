ac6abd150113928b0fcdf2d7fb718a040d424eb4
SunYerim <syl1602@naver.com>
2023-10-24 23:25:18 +0900

    [FEATURE] controller 수정

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -14,6 +14,8 @@ public class GameController {
         this.isGameFinished = false;
     }
 
+
+
     public void startGame() {
         OutputView.printGameStartMessage();
         gameService.startNewGame();
