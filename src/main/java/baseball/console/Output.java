83b8ac871504202b7d4474181eb2246c8f201d78
Bosub Kim <grapeflavoredapple@gmail.com>
2023-10-24 16:46:08 +0900

    Refactor 메인로직 리팩토링 (#7)
    * code: 숫자야구 메인 로직 주석 삭제
    
    * code: Application 에서 숫자야구 게임을 호출해서 실행하는 코드 포함하도록 변경
    
    * refactor-역할 분리를 위한 Numbers 클래스 생성
    - 해당 클래스는 컴퓨터와 사용자의 숫자 정보를 저장하는 일급 클래스의 역할을 담당한다
    
    * refactor-Numbers Dto 생성
    - 스트라이크와 볼의 계산은 다른 곳에서 수행할 것이므로 데이터 전달을 위한 dto를 생성한다.
    
    * refactor-게임 정보 저장을위한 GameStatus 클래스생성
    
    - 게임의 스트라이크와 볼의 정보를 저장하고 계산하기 위한 역할 분리 + 일급 컬렉션 + 원시값 포장을 위한 클래스를 생성한다.
    - 해당 클래스에서는 숫자 정보를 받아 스트라이크와 볼을 계산한다
    - 계산된 정보를 출력하기 위해 문자열을 리턴하는 메소드도 존재한다.
    
    * refactor-GameStatus 와 역할분리
    
    - GameStatus 에서 볼과 스트라이크를 관리함에 따라 클래스 코드 수정
    
    * refactor-메인 로직 리팩토링
    
    - 볼, 스트라이크 정보를 관리하지 않도록 분리
    - 정답과 유저 입력을 일급 컬렉션으로 관리하도록 변경
    - 앞선 리팩토링 클래스를 모두 수용하는 클래스로 번경

--- src/main/java/baseball/console/Output.java
+++ src/main/java/baseball/console/Output.java
@@ -1,6 +1,6 @@
 package baseball.console;
 
-import java.util.Objects;
+import baseball.game.GameStatus;
 
 public class Output {
 
@@ -8,19 +8,7 @@ public class Output {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    public static Boolean showResult(Integer ball, Integer strike) {
-        if (Objects.equals(strike, Integer.valueOf(3))) {
-            System.out.println("3스트라이크");
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            return Boolean.TRUE;
-        }
-
-        if (strike + ball == 0) {
-            System.out.println("낫싱");
-            return Boolean.FALSE;
-        }
-
-        System.out.println(ball + "볼 " + strike + "스트라이크");
-        return Boolean.FALSE;
+    public static void showResult(GameStatus gameStatus) {
+        System.out.println(gameStatus.gameResultString());
     }
 }
--- src/main/java/baseball/game/BaseballGame.java
+++ src/main/java/baseball/game/BaseballGame.java
@@ -13,107 +13,48 @@ import java.util.Objects;
 
 public class BaseballGame {
 
-    private Integer strike;
-    private Integer ball;
-    private List<Integer> answer;
-    private Boolean isKeepPlaying;
+    private GameStatus gameStatus;
+    private Numbers answer, userData;
+    private boolean isKeppPlaying;
+
 
     public BaseballGame() {
-        this.strike = 0;
-        this.ball = 0;
-        this.isKeepPlaying = Boolean.TRUE;
-        answer = new ArrayList<>();
+        gameStatus = new GameStatus();
+        answer = new Numbers();
+        userData = new Numbers();
+        isKeppPlaying = true;
     }
 
     public void start() {
-        while (isKeepPlaying) {
-            // 개임 시작, 재시작
-            initGame();
-
-            while (true) {
-                // 정답을 맞출 때 까지 계속 입력 받기
-                String inputData = getNumbers();
-                calcInfo(inputData);
-
-                // System.out.println("B: " + ball + ", S: " + strike );
-
-                if (strike == 3) {
-                    break;
-                }
-
-                clearData();
-            }
-
-            String stopOrGo = chooseRestart();
-            Integer restartFlag = Integer.parseInt(stopOrGo);
-            if (restartFlag == 2) {
-                isKeepPlaying = Boolean.FALSE;
-            }
+        while (isKeppPlaying) {
+            startGame();
+            clearGame();
+            playGame();
+            setRestartFlag(chooseRestart());
         }
     }
 
-    private void initGame() {
-        this.strike = 0;
-        this.ball = 0;
-        this.isKeepPlaying = Boolean.TRUE;
-        // answer = Randoms.pickUniqueNumbersInRange(0, 9, 3);
-        answer = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            int tmp = Randoms.pickNumberInRange(1, 9);
-            if (!answer.contains(tmp)) {
-                answer.add(tmp);
-            }
-        }
-
-//        System.out.print("ans: ");
-//        for (int i = 0; i < 3; i++) {
-//            System.out.print(answer.get(i) + ", ");
-//        }
-//        System.out.println();
-    }
-
-
-    private void calcInfo(String info) {
-        Integer numData = Integer.parseInt(info);
-        List<Integer> parsedData = new ArrayList<>();
-
-        while (numData > 0) {
-            parsedData.add(numData % 10);
-            numData /= 10;
-        }
-
-        Collections.reverse(parsedData);
-
-//        System.out.print("input data : ");
-//        for (int i = 0; i < 3; i++) {
-//            System.out.print(parsedData.get(i) + ", ");
-//        }
-//        System.out.println();
-
-        increaseBall(parsedData);
-        increaseStrike(parsedData);
-        showResult(ball, strike);
-    }
-
-    private void increaseBall(List<Integer> data) {
-        for (int i = 0; i < 3; i++) {
-            if (answer.contains(data.get(i))) {
-                this.ball++;
-            }
+    private void playGame() {
+        while (!gameStatus.isGameOver()) {
+            gameStatus.clearGameStatus();
+            userData.makeNumbersFromString(getNumbers());
+            gameStatus.calcScore(answer.toDto(), userData.toDto());
+            showResult(gameStatus);
         }
     }
 
-    private void increaseStrike(List<Integer> data) {
-        for (int i = 0; i < 3; i++) {
-            if (Objects.equals(answer.get(i), data.get(i))) {
-                this.ball--;
-                this.strike++;
-            }
+    private void clearGame() {
+        gameStatus.clearGameStatus();
+        answer.makeRandomNumbers();
+        isKeppPlaying = true;
+        userData = new Numbers();
+    }
+
+    private void setRestartFlag(String userInput) {
+        int restartNum = Integer.parseInt(userInput);
+        if (restartNum == 2) {
+            isKeppPlaying = false;
         }
     }
 
-    private void clearData() {
-        this.strike = 0;
-        this.ball = 0;
-    }
 }
