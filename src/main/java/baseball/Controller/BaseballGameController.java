4d1f013e8e7fa782c96d3a02af1d15063e77b980
jiixon <jr0107380@gmail.com>
2023-10-25 04:12:58 +0900

    feat: 게임 시작 controller

--- src/main/java/baseball/Controller/BaseballGameController.java
+++ src/main/java/baseball/Controller/BaseballGameController.java
@@ -1,5 +1,23 @@
 package baseball.Controller;
 
-public class BaseballController {
+import baseball.Model.BaseballGameModel;
+import baseball.View.BaseballGameView;
 
+public class BaseballGameController {
+
+    private final static BaseballGameModel model = new BaseballGameModel();
+    private final static BaseballGameView view = new BaseballGameView();
+    private static final String SINGLE_GAME_END = "3스트라이크";
+
+    private void playSingleGame(){
+        view.gameStart();
+        model.createRandomNumbers();
+        String gameResult;
+        do {
+            String userInput = view.getUserInput();
+            gameResult = model.calculateResult(userInput);
+            view.print(gameResult);
+        } while (!gameResult.equals(SINGLE_GAME_END));
+        view.endPrint();
+    }
 }
