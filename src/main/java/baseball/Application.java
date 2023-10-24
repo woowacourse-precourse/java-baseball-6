b21b4f8c4340112c125b7627f0afa8005a22e516
psionic0324 <psionic0324@naver.com>
2023-10-24 23:35:30 +0900

    Refactor: Application 수정
    자바코딩 규칙을 참고하여 코드 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,18 +1,25 @@
+/**
+ * 제목: 숫자 야구 게임
+ */
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
+/**
+ * Game클래스 객체 생성, 게임 종료 후 재시작 or 종료 유무 확인
+ */
 public class Application {
     public static void main(String[] args) {
         UserInterface userInterface = new UserInterface();
 
         userInterface.startMessage();
-        while(true) {
+        while (true) {
             Game game = new Game();
             game.game();
 
             String retryCheckNumber = Console.readLine();
-            if(retryCheckNumber.equals("2")) {
+            /* 재시작: 1, 종료: 2 */
+            if (retryCheckNumber.equals("2")) {
                 break;
             }
         }
