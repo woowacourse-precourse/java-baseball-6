6be20df588d5696308f8bf408c9603168a6876aa
youngkyunIm <yee0230@naver.com>
2023-10-24 21:31:31 +0900

    feat : view 입력 기능 추가
    camp.nextstep.edu.missionutils.Console을 사용해 readLine()을 사용

--- src/main/java/baseball/view/Viewer.java
+++ src/main/java/baseball/view/Viewer.java
@@ -1,20 +1,24 @@
 package baseball.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Viewer {
 
     public void startGameView() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    public void pleaseInputNumberView() {
-        System.out.println("숫자를 입력해주세요 : ");
+    public String pleaseInputNumberView() {
+        System.out.print("숫자를 입력해주세요 : ");
+        return Console.readLine();
     }
 
     public void threeStrikeView() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
-    public void restartGameView() {
+    public String restartGameView() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return Console.readLine();
     }
 }
