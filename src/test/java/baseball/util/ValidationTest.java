6d4b5246cc617919504bc4eab437fc0a5d951641
김효준 <khyojun1104@gmail.com>
2023-10-24 14:37:47 +0900

    [test] ValidationTest : 재시작 번호가 잘못된 경우 테스트를 위한 코드!

--- src/test/java/baseball/util/ValidationTest.java
+++ src/test/java/baseball/util/ValidationTest.java
@@ -29,4 +29,20 @@ class ValidationTest {
     }
 
 
+    @Test
+    @DisplayName("재시작 번호 입력이 잘못되 경우 테스트!")
+    void validateRestart() {
+
+        String restart_number_wrong_length="12";
+        String restart_number_wrong_range1 = "0";
+        String restart_number_wrong_range2 = "3";
+        String restart_number_wrong="12a";
+
+        Assertions.assertThatThrownBy(() -> validation.validateRestart(restart_number_wrong)).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> validation.validateRestart(restart_number_wrong_range1)).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> validation.validateRestart(restart_number_wrong_range2)).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> validation.validateRestart(restart_number_wrong_length)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+
 }
