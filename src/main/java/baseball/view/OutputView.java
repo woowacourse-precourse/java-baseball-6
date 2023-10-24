15c1af4fc779059f6593c3972db6e07e7deb8d68
yoonjuho <dbswn990@naver.com>
2023-10-25 04:18:08 +0900

    refactor(OutputView): 메서드 분리

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class OutputView {
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String GAME_FINISH_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    private static final Long NOT_EXIST = 0L;
+    private static final Long ZERO = 0L;
     private static final String BLANK = " ";
 
     public void printGameStart() {
@@ -22,17 +22,19 @@ public class OutputView {
     }
 
     private String convertResultToString(Map<GameResult, Long> result) {
-        StringBuilder resultBuilder = new StringBuilder();
-        for (Map.Entry<GameResult, Long> entry : result.entrySet()) {
-            if (entry.getKey().equals(GameResult.NOTHING)) {
-                return GameResult.NOTHING.getKorean();
-            }
-            if (entry.getValue() != NOT_EXIST) {
-                resultBuilder.append(entry.getValue());
-                resultBuilder.append(entry.getKey().getKorean());
-                resultBuilder.append(BLANK);
-            }
+        if(result.containsKey(GameResult.NOTHING)){
+            return GameResult.NOTHING.getKorean();
         }
-        return resultBuilder.toString().trim();
+        String convertedResult = attach(GameResult.BALL, result.get(GameResult.BALL))
+                + attach(GameResult.STRIKE, result.get(GameResult.STRIKE));
+        return convertedResult.trim();
     }
+
+    private String attach(GameResult gameResult, Long count){
+        if(count != ZERO){
+            return count + gameResult.getKorean() + BLANK;
+        }
+        return BLANK;
+    }
+
 }
