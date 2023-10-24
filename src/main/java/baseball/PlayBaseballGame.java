8fa6c2111faa982e3f8d60dc2bcbd8096ff26739
jjminsuh <minsuh3203@gmail.com>
2023-10-24 21:04:58 +0900

    refact: 클래스 분리

--- src/main/java/baseball/PlayBaseballGame.java
+++ src/main/java/baseball/PlayBaseballGame.java
@@ -1,17 +1,17 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-import static camp.nextstep.edu.missionutils.Console.readLine;
+class BaseballGameSet {
+    List<Integer> computerNumberList = new ArrayList<>();
 
-public class PlayBaseballGame {
-    static List<Integer> getRandomNum() {
-
-        List<Integer> computerNumberList = new ArrayList<>();
+    List<Integer> getRandomNum() {
         while (computerNumberList.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computerNumberList.contains(randomNumber)) {
@@ -22,7 +22,7 @@ public class PlayBaseballGame {
         return computerNumberList;
     }
 
-    static List<Integer> getPlayerGuess() throws IOException {
+    List<Integer> getPlayerGuess() throws IOException {
 
         String numberGuessInput = readLine();
         int numberGuess = Integer.parseInt(numberGuessInput);
@@ -31,79 +31,97 @@ public class PlayBaseballGame {
         int secondNum;
         int thirdNum;
 
+        firstNum = numberGuess / 100;
 
-        firstNum = numberGuess/100;
-
-        if(firstNum > 9 || firstNum == 0) {
+        if (firstNum > 9 || firstNum == 0) {
             throw new IllegalArgumentException();
         }
 
-        numberGuess = numberGuess%100;
-        secondNum = numberGuess/10;
-        if(secondNum == 0) {
+        numberGuess = numberGuess % 100;
+        secondNum = numberGuess / 10;
+        if (secondNum == 0) {
             throw new IllegalArgumentException();
         }
 
-        thirdNum = numberGuess%10;
-        if(thirdNum == 0) {
+        thirdNum = numberGuess % 10;
+        if (thirdNum == 0) {
             throw new IllegalArgumentException();
         }
 
         return List.of(firstNum, secondNum, thirdNum);
 
     }
+}
 
-    static boolean checkStrike(List<Integer> computerNum, List<Integer> playerNum) {
-        int ballCnt = 0;
-        int strikeCnt = 0;
+record StrikeData(int strikeCnt, int ballCnt) {
+}
 
-        for(int i = 0; i < 3; i++) {
-            if(Objects.equals(playerNum.get(i), computerNum.get(i))) {
-                strikeCnt ++;
-            } else if(computerNum.contains(playerNum.get(i))) {
-                ballCnt ++;
+class StrikeChecker {
+    int ballCnt = 0;
+    int strikeCnt = 0;
+
+    StrikeData checkStrike(List<Integer> computerNum, List<Integer> playerNum) {
+        for (int i = 0; i < 3; i++) {
+            if (Objects.equals(playerNum.get(i), computerNum.get(i))) {
+                strikeCnt++;
+            } else if (computerNum.contains(playerNum.get(i))) {
+                ballCnt++;
             }
         }
 
-        if(ballCnt == 0 && strikeCnt == 0) {
+        return new StrikeData(strikeCnt, ballCnt);
+    }
+}
+
+public class PlayBaseballGame {
+    
+    static boolean checkStrike(List<Integer> computerNum, List<Integer> playerNum) {
+
+        StrikeChecker newStrikeChecker = new StrikeChecker();
+        StrikeData strikeCheckResult = newStrikeChecker.checkStrike(computerNum, playerNum);
+
+        if (strikeCheckResult.ballCnt() == 0 && strikeCheckResult.strikeCnt() == 0) {
             System.out.println("낫싱");
             return false;
-        } else if(strikeCnt == 3) {
-            System.out.printf("%d스트라이크\n", strikeCnt);
+        } else if (strikeCheckResult.strikeCnt() == 3) {
+            System.out.printf("%d스트라이크\n", strikeCheckResult.strikeCnt());
             return true;
         } else {
-            System.out.printf("%d볼 %d스트라이크\n", ballCnt, strikeCnt);
+            System.out.printf("%d볼 %d스트라이크\n", strikeCheckResult.ballCnt(), strikeCheckResult.strikeCnt());
             return false;
         }
     }
 
-    static boolean askNewGame() throws IOException{
+    static boolean askNewGame() throws IOException {
         System.out.println("게임을 새로 시작하시려면 1, 종료하려면 2를 입력하세요.");
 
         String newGameAnswerString = readLine();
         int newGameAnswer = Integer.parseInt(newGameAnswerString);
 
-        if(newGameAnswer == 1) {
+        if (newGameAnswer == 1) {
             return true;
-        } else if(newGameAnswer == 2) {
+        } else if (newGameAnswer == 2) {
             return false;
         } else {
             throw new IllegalArgumentException();
         }
     }
+
     public static void playGame() {
 
         boolean isCorrect = false;
 
+        BaseballGameSet newSet = new BaseballGameSet();
+
         // set the number of the computer
-        List<Integer> computerNumberList = getRandomNum();
+        List<Integer> computerNumberList = newSet.getRandomNum();
         List<Integer> playerGuessNumberList;
 
-        while(!isCorrect) {
+        while (!isCorrect) {
             System.out.print("숫자를 입력해주세요 : ");
 
             try {
-                playerGuessNumberList = getPlayerGuess();
+                playerGuessNumberList = newSet.getPlayerGuess();
             } catch (IOException e) {
                 return;
             }
@@ -121,7 +139,7 @@ public class PlayBaseballGame {
             throw new IllegalArgumentException();
         }
 
-        if(startNewGame) {
+        if (startNewGame) {
             playGame();
         }
     }
