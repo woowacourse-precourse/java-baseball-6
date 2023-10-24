1c0d6dbff6dababab935d9dd18aaf9d191ed6094
jang010505 <jang010505@inu.ac.kr>
2023-10-24 15:20:38 +0900

    feat: computeBallCount 함수 코드 작성

--- src/main/java/baseball/util/GameUtil.java
+++ src/main/java/baseball/util/GameUtil.java
@@ -4,6 +4,7 @@ public class GameUtil {
 
     private int ballCount = 0;
     private int strikeCount = 0;
+    private static final int NUMBER_LENGTH = 3;
 
     public void computeCount(String computerNumber, String playerNumber) {
         this.strikeCount = computeStrikeCount(computerNumber, playerNumber);
@@ -16,7 +17,17 @@ public class GameUtil {
     }
 
     private int computeBallCount(String computerNumber, String playerNumber) {
+        int ball = 0;
 
-        return 0;
+        for (int playerIndex = 0; playerIndex < NUMBER_LENGTH; playerIndex++) {
+            char target =  playerNumber.charAt(playerIndex);
+            int computerIndex = computerNumber.indexOf(target);
+
+            if (computerIndex != -1 && computerIndex != playerIndex) {
+                ball++;
+            }
+        }
+
+        return ball;
     }
 }
