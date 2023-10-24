329fdc8a848883e02058e1a78a9013fc4454c7e6
JangHongJoon <wkdghdwns19969@gmail.com>
2023-10-24 23:36:07 +0900

    Feat : 입력한 값이 숫자가 아니면 IllegalArgumentException 반환하는 CheckerForUnvalidCharOfSelection.java 생성

---
+++ src/main/java/baseball/error/CheckerForUnvalidCharOfSelection.java
@@ -0,0 +1,12 @@
+package baseball.error;
+
+public class CheckerForUnvalidCharOfSelection {
+    public void check(String gameContinueSelection) {
+        try{
+            Integer.parseInt(gameContinueSelection);
+        }
+        catch (Exception e){
+            throw new IllegalArgumentException();
+        }
+    }
+}
