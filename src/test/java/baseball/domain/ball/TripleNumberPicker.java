13053b0a6b1e610e10d90adadf5e06e784454c43
mainfn <_@mainfn.dev>
2023-10-24 14:57:22 +0900

    feat: 테스트 시 사용할 고정 3자리 숫자 선택자

---
+++ src/test/java/baseball/domain/ball/TripleNumberPicker.java
@@ -0,0 +1,26 @@
+package baseball.domain.ball;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import number.NumberPicker;
+
+final class TripleNumberPicker implements NumberPicker {
+
+    private final Queue<Integer> numbers;
+
+    public TripleNumberPicker(
+            final int first,
+            final int second,
+            final int third
+    ) {
+        this.numbers = new LinkedList<>();
+        numbers.add(first);
+        numbers.add(second);
+        numbers.add(third);
+    }
+
+    @Override
+    public int pick() {
+        return numbers.remove();
+    }
+}
