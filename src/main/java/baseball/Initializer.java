42738633de6f93c5e89ae19ea8e158c9eb923ae1
Limwngur <ovg07047@naver.com>
2023-10-25 00:43:11 +0900

    Feat: Game Initializer 구현

--- src/main/java/baseball/Initializer.java
+++ src/main/java/baseball/Initializer.java
@@ -1,2 +1,36 @@
-package baseball;public class Initializer {
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Initializer {
+
+    private List<Integer> comNum;
+    private List<Integer> userNum;
+    public List<Integer> setCom(){
+
+        while(comNum.size()<3){
+            this.comNum.add(Randoms.pickNumberInRange(1,9));
+        }
+
+        return this.comNum;
+    }
+    public List<Integer> setUser(){
+        String userInStr =Console.readLine();
+        int inputSize = userInStr.length();
+
+        if(inputSize>3){
+            //TODO: 예외처리
+            new ArrayIndexOutOfBoundsException("잘못된 입력입니다.");
+        }
+
+        for (int i = 0; i < 3; ++i) {
+            this.userNum.add(Integer.valueOf(userInStr.charAt(i)));            ;
+        }
+
+        return this.userNum;
+    }
 }
