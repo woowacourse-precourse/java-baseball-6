9caf4ed9a3a325e379804136f48d7b526558abce
MingyeomKim <mingyum119@naver.com>
2023-10-24 20:52:33 +0900

    refactor(OutputView): static을 이용해 콘솔에 출력하는 문자열의 상수화
    고정된 문자열을 상수화하기 위해 static 예약어를 추가한다.

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,11 +1,10 @@
 package baseball.view;
 
 public class OutputView {
-    private final String GAME_START_GUIDE = "숫자 야구 게임을 시작합니다.";
-    private final String CORRECT_ANSWER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    private final String RESTART_OR_EXIT_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-
-    private final String NUMBER_REQUEST_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String GAME_START_GUIDE = "숫자 야구 게임을 시작합니다.";
+    private static final String CORRECT_ANSWER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String RESTART_OR_EXIT_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String NUMBER_REQUEST_MESSAGE = "숫자를 입력해주세요 : ";
 
     public void gameStart() {
         System.out.println(GAME_START_GUIDE);
