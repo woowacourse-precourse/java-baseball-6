463902c52271c77360376276c5c761d8e3dce954
HyunJun KIM <hihj070914@icloud.com>
2023-10-24 20:55:21 +0900

    feat: pickNumber and userInput - 중복되지 않는 3자리 숫자를 뽑는 pickNumber() 구현 - 사용자 입력 테스트

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,31 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+
+
 public class Application {
+
+    public static String pickNumber() {
+        ArrayList<Integer> computer = new ArrayList<>();
+        int result = 0;
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+                result = result * 10 + randomNumber;
+            }
+        }
+        return Integer.toString(result);
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
+            String userInput = Console.readLine();
+            System.out.printf("User : %s, Cpu : %s\n", userInput, pickNumber());
+        }
     }
 }
