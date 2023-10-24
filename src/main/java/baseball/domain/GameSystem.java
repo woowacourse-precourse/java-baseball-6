dbcbaa68ae9ec3626255288959f26530d84e1944
kimmokalover <kimmokalover@gmail.com>
2023-10-24 10:56:46 +0900

    feat: UserInput & GameSystem class 추가

--- src/main/java/baseball/domain/GameSystem.java
+++ src/main/java/baseball/domain/GameSystem.java
@@ -1,5 +1,32 @@
 package baseball.domain;
 
 public class GameSystem {
+    private String roundNumber;
+    private Long strike;
+    private Long ballCount;
 
+
+    public String getRoundNumber() {
+        return roundNumber;
+    }
+
+    public void setRoundNumber(String roundNumber) {
+        this.roundNumber = roundNumber;
+    }
+
+    public Long getStrike() {
+        return strike;
+    }
+
+    public void setStrike(Long strike) {
+        this.strike = strike;
+    }
+
+    public Long getBallCount() {
+        return ballCount;
+    }
+
+    public void setBallCount(Long ballCount) {
+        this.ballCount = ballCount;
+    }
 }
--- src/main/java/baseball/domain/UserInput.java
+++ src/main/java/baseball/domain/UserInput.java
@@ -1,5 +1,13 @@
 package baseball.domain;
 
 public class UserInput {
+    String userInput;
 
+    public String getUserInput() {
+        return userInput;
+    }
+
+    public void setUserInput(String userInput) {
+        this.userInput = userInput;
+    }
 }
