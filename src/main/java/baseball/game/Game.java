8b91b794cdaf0910fb071fe076a7a7d23072c355
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-10-25 02:23:53 +0900

    feat: 역할별 class 생성 및 기능 구현 진행

---
+++ src/main/java/baseball/game/Game.java
@@ -0,0 +1,35 @@
+package baseball.game;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Game {
+
+    private final GamePlayer gamePlayer = new GamePlayer();
+
+    public void run() {
+        boolean restart = true;
+        GamePhrases.startPhrase();
+        do {
+            BaseBallGameRunner();
+            switch (gamePlayer.userRestartInput()) {
+                case "1":
+                    continue;
+                case "2":
+                    Console.close();
+                    restart = false;
+            }
+        } while (restart);
+    }
+
+    private void BaseBallGameRunner() {
+        GameController gameController = new GameController();
+        gameController.randoms();
+        boolean isCorrect;
+
+        do {
+            GamePhrases.inputPhrase();
+            gameController.isStrikeOrBall(gamePlayer.userAnswerInput());
+            isCorrect = gameController.printResultPhrase();
+        } while (isCorrect);
+    }
+}
---
+++ src/main/java/baseball/game/GameController.java
@@ -0,0 +1,68 @@
+package baseball.game;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameController {
+    private final List<Integer> answer = new ArrayList<>();
+    private int strike = 0;
+    private int ball = 0;
+
+
+    private void clearStrikeAndBall() {
+        this.strike = 0;
+        this.ball = 0;
+    }
+
+    protected void randoms() {
+
+        if (!this.answer.isEmpty()) {
+            this.answer.clear();
+        }
+
+        while (this.answer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!this.answer.contains(randomNumber)) {
+                this.answer.add(randomNumber);
+            }
+        }
+    }
+
+    protected void isStrikeOrBall(List<Integer> userInput) {
+
+        if (userInput.equals(answer)) {
+            this.strike = 3;
+            return;
+        }
+
+        for (int i = 0; i < userInput.size(); i++) {
+            if (answer.get(i).equals(userInput.get(i))) {
+                this.strike++;
+            } else if (answer.contains(userInput.get(i))) {
+                this.ball++;
+            }
+        }
+    }
+
+    protected boolean printResultPhrase() {
+        if (strike == 3) {
+            GamePhrases.strikePhrase(strike);
+            GamePhrases.answerPhrase();
+            GamePhrases.endPhrase();
+            return false;
+        } else if ((strike + ball) == 0) {
+            GamePhrases.outPhrase();
+        } else if (strike != 0 && ball != 0) {
+            GamePhrases.ballAndStrikePhrase(strike, ball);
+            clearStrikeAndBall();
+        } else if (strike > 0) {
+            GamePhrases.strikePhrase(strike);
+            clearStrikeAndBall();
+        } else if (ball > 0) {
+            GamePhrases.ballPhrase(ball);
+            clearStrikeAndBall();
+        }
+        return true;
+    }
+}
