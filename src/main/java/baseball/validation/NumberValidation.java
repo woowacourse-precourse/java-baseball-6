28fc4364b982116729e54229c3c574ca50bf55ad
Jihyun3478 <leejihyun3478@gmail.com>
2023-10-24 15:07:06 +0900

    Fix(NumberValidation): 함수 순서 수정

--- src/main/java/baseball/validation/NumberValidation.java
+++ src/main/java/baseball/validation/NumberValidation.java
@@ -1,8 +1,8 @@
 package baseball.validation;
 
-import static baseball.constant.Constant.NUMBER_LENGTH;
-
 import java.util.Arrays;
+
+import static baseball.constant.Constant.*;
 
 public class NumberValidation {
 
@@ -25,22 +25,22 @@ public class NumberValidation {
     }
 
     private void validateRangeAndType(int[] input) {
-        if (!(input.length == Arrays.stream(input).filter(i -> i >= 1 && i <= 9).count())) {
+        if (!(input.length == Arrays.stream(input).filter(i -> i >= MIN_NUMBER && i <= MAX_NUMBER).count())) {
             throw new IllegalArgumentException("1과 9 사이의 숫자를 입력해주세요.");
         }
     }
 
-    public static void validateGameStatus(String input) {
-        if (!(input.equals("1") || input.equals("2"))) {
-            throw new IllegalArgumentException("1 또는 2를 입력해주세요.");
-        }
+    public static boolean validateIsInArray(int[] computerNumber, int num) {
+        return Arrays.stream(computerNumber).anyMatch(array -> array == num);
     }
 
     public static boolean validateComputerDuplicate(int[] input) {
         return input.length == Arrays.stream(input).distinct().count();
     }
 
-    public static boolean isInArray(int[] computerNumber, int num) {
-        return Arrays.stream(computerNumber).anyMatch(array -> array == num);
+    public static void validateGameStatus(String input) {
+        if (!(input.equals("1") || input.equals("2"))) {
+            throw new IllegalArgumentException("1 또는 2를 입력해주세요.");
+        }
     }
 }
