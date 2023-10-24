4c5b3dc2de9d87ab2b823713c5e5b94dc2b126b1
Hayoon <hy97@sju.ac.kr>
2023-10-24 20:56:05 +0900

    feat(Constant): 상수 추가

--- src/main/java/baseball/util/Constant.java
+++ src/main/java/baseball/util/Constant.java
@@ -7,7 +7,16 @@ public class Constant {
     public static final String RESTART_GAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     public static final String BALL_AND_STRIKE_FORMAT = "%d볼 %d스트라이크\n";
     public static final String BALL_FORMAT = "%d볼\n";
+    public static final String BALL = "볼\n";
     public static final String STRIKE_FORMAT = "%d스트라이크\n";
+    public static final String STRIKE = "스트라이크\n";
     public static final String NOTHING = "낫싱\n";
+    public static final int NUMBER_ZERO = 0;
+    public static final int NUMBER_ONE = 1;
+    public static final int FINISH = 1;
+    public static final int NUMBER_TWO = 2;
+    public static final int RESTART = 2;
+    public static final int NUMBER_THREE = 3;
+    public static final int NUMBER_NINE = 9;
 
 }
