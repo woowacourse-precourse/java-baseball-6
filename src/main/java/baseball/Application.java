0d901bff08e2ca549e46638107cad05b324466f4
yeonsu <jack0810@kookmin.ac.kr>
2023-10-24 11:07:00 +0900

    기능 요구사항 정리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,29 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
+
+    private static void gamePlay(){
+
+        // TODO : 게임 종료 혹은 재시작
+    }
+
+    private static boolean isCorrect() {
+        // TODO : inputNum 구현
+
+        // TODO : gameExecute 구현
+    }
+    private static void inputNum() {
+        // TODO : 사용자 입력이 유효한지 판별
+    }
+
+    private static void gameExecute() {
+        // TODO : 스트라이크, 볼, 낫싱 판별
+    }
+
+    private static void gameEnd(){
+
+    }
+
 }
