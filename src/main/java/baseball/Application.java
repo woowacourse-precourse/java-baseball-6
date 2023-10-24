fb5d6b2a076e35e5e92388d4566eb226632a12d3
pletain <pletain@dgu.ac.kr>
2023-10-25 03:05:50 +0900

    Feat: main 함수에서 게임을 운영하는 기능들 추가 숫자를 맞출 때까지 반복해서 게임을 진행하는 기능  게임 종료 후 재시작/종료 선택하는 기능 재시작 선택 시 게임 시작하는 기능 종료 선택 시 게임 종료하는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
+import java.util.List;
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Game game = new Game();
+        Gamer gamer = new Gamer();
+        List<Integer> gamer_number;
+
+    while(true) {
+            gamer_number = gamer.receiveNumber();
+            if(game.getResult(gamer_number)) {
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                String input = readLine();
+                if(input.equals("1")) {
+                    game = new Game();
+                } else if(input.equals("2")) {
+                    break;
+                }
+            }
+        }
     }
 }
