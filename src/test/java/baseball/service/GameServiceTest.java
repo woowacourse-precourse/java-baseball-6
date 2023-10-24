e7d5ef53761e0af817d515d39d29019cd86e21b8
jongheonleee <qwefghnm1212@gmail.com>
2023-10-24 21:55:57 +0900

    [test] 낫싱 판단 테스트

--- src/test/java/baseball/service/GameServiceTest.java
+++ src/test/java/baseball/service/GameServiceTest.java
@@ -33,5 +33,16 @@ class GameServiceTest {
         assertEquals(2, result.getBall());
     }
 
+    @Test
+    void 낫싱_판단_테스트() {
+        Integer[] playerNumbers = {1, 2, 3};
+        PlayerBalls playerBalls = new PlayerBalls(playerNumbers);
+        List<Ball> computerBalls = List.of(new Ball(9), new Ball(8), new Ball(7));
+        GameService service = new GameService();
+        GameResult result = service.judge(playerBalls.getBalls(), computerBalls);
+
+        assertEquals(0, result.getStrike());
+        assertEquals(0, result.getBall());
+    }
 
 }
