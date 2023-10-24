9032e7824e97994f2b82906d35632557b95bfd5c
chech2 <chaelim3396@gmail.com>
2023-10-25 05:05:42 +0900

    Add) controller로 구조 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,13 +1,13 @@
 package baseball;
 
+import baseball.controller.BaseballGameController;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-    private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
-    public static String userInput;
     public static void main(String[] args) {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        BaseballGameController baseballGameController = new BaseballGameController();
 
         // TODO: 프로그램 구현
+        baseballGameController.gameStart();
     }
 }
---
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -0,0 +1,20 @@
+package baseball.controller;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class BaseballGameController {
+    private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String RESTART_OR_SHUTDOWN_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static String userInput;
+
+    public void gameStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        while (true) {
+            System.out.print(INPUT_MESSAGE);
+            // Todo: 컴퓨터가 가질 값 랜덤 생성
+
+            userInput = Console.readLine();
+        }
+    }
+}
