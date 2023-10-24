960d213bcf44d8623040bb572332d0b381dbb4e5
Yehyeok Bang <qkddpgur318@gmail.com>
2023-10-24 22:28:49 +0900

    feat: 에러 메시지 문자열 상수로 선언

---
+++ src/main/java/baseball/message/ErrorMessages.java
@@ -0,0 +1,9 @@
+package baseball.message;
+
+public class ErrorMessages {
+    public static final String INVALID_INPUT = "잘못된 입력입니다.";
+    public static final String INVALID_LENGTH = "3자리 숫자를 입력해주세요.";
+    public static final String INVALID_RANGE = "1부터 9까지의 숫자만 입력해주세요.";
+    public static final String INVALID_DUPLICATE = "중복된 숫자가 있습니다.";
+    public static final String INVALID_RESTART = "1 또는 2를 입력해주세요.";
+}
