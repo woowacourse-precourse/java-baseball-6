ca44eb2f7eae5dce46f4993edd6f1cfa403bd174
gunsookim <ccpc4896@naver.com>
2023-10-24 18:47:48 +0900

    refactor : 코드 리펙토링 클래스 이름 변경
    결과 출력 부분에서 사용한 Player 클래스를 ShowResult 클래스로 rename

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -4,20 +4,22 @@ import java.util.List;
 
 class BaseballGame {
     private static final int NUMBER_OF_DIGITS = 3; // 숫자 자릿수 초기화
-    private final Computer computer;
+    private final NumberGenerator numberGenerator;
     private final Player player;
     private final CheckScore checkScore;
+    private final ShowResult showResult;
 
     public BaseballGame() {
         // 인스턴스 초기화
-        this.computer = new Computer(NUMBER_OF_DIGITS);
+        this.numberGenerator = new NumberGenerator(NUMBER_OF_DIGITS);
         this.player = new Player();
         this.checkScore = new CheckScore(NUMBER_OF_DIGITS);
+        this.showResult = new ShowResult(NUMBER_OF_DIGITS);
     }
 
     // 야구 게임 진행
     public void play() {
-        List<Integer> computerNumbers = computer.generateNumbers();
+        List<Integer> computerNumbers = numberGenerator.generateNumbers();
 
         // player가 all strike할 때까지 반복
         while (true) {
@@ -26,10 +28,10 @@ class BaseballGame {
             int ball = checkScore.countBall(computerNumbers, playerNumbers);
 
             if (checkScore.isGameWon(strike)) {
-                player.win(strike); // all strike인 경우 승리 문구 출력
+                showResult.win(strike); // all strike인 경우 승리 문구 출력
                 break;
             } else {
-                player.showResult(strike, ball); // all strike가 아닌 경우 결과 출력
+                showResult.result(strike, ball); // all strike가 아닌 경우 결과 출력
             }
         }
     }
