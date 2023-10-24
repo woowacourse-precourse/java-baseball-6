17f549bd2f55da8cbb410c91258a3a6a9c554bfb
peeaaccee <wlgp8958@naver.com>
2023-10-25 08:05:27 +0900

    feat(User): 유저가 숫자를 입력하면 숫자 각각을 리스트화

--- src/main/java/baseball/domain/model/User.java
+++ src/main/java/baseball/domain/model/User.java
@@ -1,4 +1,22 @@
 package baseball.domain.model;
 
+import baseball.utils.InputValidator;
+
+import java.util.List;
+
 public class User {
+    private final List<String> user;
+
+    public User(final List<String> user) {
+        this.user = user;
+    }
+
+    public User(final String ballInput) {
+        InputValidator.checkUserBallInput(ballInput);
+        this.user = makeBallsFromInput(ballInput);
+    }
+
+    private List<String> makeBallsFromInput(final String ballInput) {
+        return List.of(ballInput.split(""));
+    }
 }
