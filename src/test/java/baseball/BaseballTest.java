15d77991940d13060d4ecb9ab35eb2a4dadc66f8
masonkimseoul <masonkimseoul@gmail.com>
2023-10-24 22:46:53 +0900

    test : (BaseballTest.java) Add random number's each digit is between 0 and 10

--- src/test/java/baseball/BaseballTest.java
+++ src/test/java/baseball/BaseballTest.java
@@ -24,4 +24,14 @@ public class BaseballTest {
         List<Integer> computerNumbers = (List<Integer>) method.invoke(baseball);
         assertThat(computerNumbers).hasSize(3);
     }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    void 랜덤_숫자_생성_범위_테스트() throws Exception {
+        Method method = baseball.getClass().getDeclaredMethod("chooseRandomNumbers");
+        method.setAccessible(true);
+
+        List<Integer> computerNumbers = (List<Integer>) method.invoke(baseball);
+        assertThat(computerNumbers).allMatch(num -> num > 0 && num < 10);
+    }
 }
