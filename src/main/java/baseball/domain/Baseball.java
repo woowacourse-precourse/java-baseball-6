c83f093758f4b0e4bd8c684a35dccd5a4103ca99
Beomsu <gobeumsu@gmail.com>
2023-10-25 00:07:20 +0900

    feat(Baseball): Baseball 클래스와 초기화 로직 추가

---
+++ src/main/java/baseball/domain/Baseball.java
@@ -0,0 +1,9 @@
+package baseball.domain;
+
+import java.util.List;
+public class Baseball {
+    private final List<Integer> computerNumber;
+    public Baseball(List<Integer> computerNumber) {
+        this.computerNumber = computerNumber;
+    }
+}
