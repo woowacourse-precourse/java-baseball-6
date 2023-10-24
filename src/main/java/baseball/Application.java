7ac799bb2b98345f6ae15bbaeaaa03d0e5c52239
hyunjin <guswlsdl0121@gmail.com>
2023-10-24 18:48:18 +0900

    refactor: DefaultRandomGenerator -> NsRandomGenerator로 명칭 변경
    nextStep의 라이브러리를 사용한 구현체이기 때문에

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,7 +2,7 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        RandomGenerator randomGenerator = new DefaultRandomGenerator();
+        RandomGenerator randomGenerator = new NsRandomGenerator();
         Person person = new Person();
         Computer computer = new Computer(randomGenerator);
         GameRunner gameRunner = new GameRunner(person, computer);
--- src/main/java/baseball/NsRandomGenerator.java
+++ src/main/java/baseball/NsRandomGenerator.java
@@ -2,7 +2,7 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class DefaultRandomGenerator implements RandomGenerator {
+public class NsRandomGenerator implements RandomGenerator {
     @Override
     public int pickNumberInRange(int start, int end) {
         return Randoms.pickNumberInRange(start, end);
