2dd56ed8cc8084ad4d84c4f8e95218a3246db93f
gyulim5 <ohrbfla1004@khu.ac.kr>
2023-10-24 18:09:49 +0900

    🎨 Refactor: Game 코드 간략화

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -3,6 +3,7 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Game{
@@ -10,42 +11,95 @@ public class Game{
         private final Input input = new Input();
         private final InputValidator inputValidator = new InputValidator();
         private final Output output = new Output();
+        private String computerNumber;
+        private GameState gameState = GameState.INIT;
+        private static final String RESTART = "1";
 
 
         public void playGame() {
-                boolean isStartState = true; // 게임 초기 상태를 나타내는 변수면 init이 더 좋음
-                String serverNumber = "";
 
-                while (true) // true -> 종료 조건 명시 ex) choice가 2라면 등등등..
+                while (!gameState.equals(GameState.END))
                 {
-                        // Method로 빼기
-                        if (isStartState) {
-                                serverNumber = generateNumber();
-                                output.printStartMessage();
-                                isStartState = false;
-                        }
+                        initGame();
 
-                        output.printInputMessage();
-                        String userInput = input.readInput();
-                        System.out.println(userInput);
-                        inputValidator.isValidInput(userInput);
-
-                        if (checkNumberMatches(serverNumber, userInput)) //유저가 정답을 맞추면.
+                        if (isAnswer(getUserInput()))
                         {
                                 output.printGameEndMessage();
-                                output.printGameRestartMessage();
-                                // `재시작여부 설정`이라는 method정도 들어가면 어떨까?
-                                String choice = input.readInput();
-                                inputValidator.isValidChoice(choice);
-                                isStartState = choice.equals("1");
-                                if (!isStartState) break; // while 문으로 빼는게 더 좋을 듯, 이 방식이 나쁜것은 아님
+                                gameState = getGameState();
                         }
+                }
+        }
 
+        private void initGame() {
+                if (gameState.equals(GameState.INIT)) {
+                        output.printStartMessage();
+                        computerNumber = generateNumber();
+                        gameState = GameState.START;
+                }
+        }
+
+        private String getUserInput() {
+                String userInput = input.readInput();
+                output.printInputMessage(userInput);
+                inputValidator.isValidInput(userInput);
+
+                return userInput;
+        }
+
+        private boolean isAnswer(String clientNumber) {
+
+                int[] matchList = getMatchList(clientNumber);
+
+                output.printResult(matchList);
+
+                return matchList[MatchState.STRIKE.getValue()] == 3;
+        }
+
+        private int[] getMatchList(String clientNumber) {
+
+                int[] matchList = new int[3];
+                HashMap<Character, Integer> serverHash = stringToHashMap(computerNumber);
+                HashMap<Character, Integer> clientHash = stringToHashMap(clientNumber);
+
+                for (Character serverNum : serverHash.keySet()) {
+                        if (clientHash.containsKey(serverNum) &&
+                                clientHash.get(serverNum).equals(serverHash.get(serverNum))) {
+                                matchList[MatchState.STRIKE.getValue()] += 1;
+                        } else if (clientHash.containsKey(serverNum)) {
+                                matchList[MatchState.BALL.getValue()] += 1;
+                        }
+                }
+
+                if (matchList[MatchState.STRIKE.getValue()] +
+                        matchList[MatchState.BALL.getValue()] == 0)
+                {
+                        matchList[MatchState.NOTHING.getValue()] = 1;
+                }
+
+                return matchList;
+        }
+
+
+        private HashMap<Character, Integer> stringToHashMap(String number) {
+                HashMap<Character, Integer> numberHash = new HashMap<>();
+                for (int i = 0; i < number.length(); i++) {
+                        numberHash.put(number.charAt(i), i);
+                }
+                return numberHash;
+        }
+
+        private GameState getGameState() {
+                String restartChoice = input.readInput();
+                inputValidator.isValidChoice(restartChoice);
+
+                if (restartChoice.equals(RESTART)) {
+                        return GameState.INIT;
+                } else {
+                        return GameState.END;
                 }
         }
 
         private String generateNumber() {
-
                 List<Integer> computer = new ArrayList<>();
                 while (computer.size() < 3) {
                         int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -55,35 +109,5 @@ public class Game{
                 }
 
                 return computer.toString().replaceAll("\\D","");
-        }
-
-        private boolean checkNumberMatches(String serverNumber, String clientNumber) {
-                int strike = 0;
-                int ball = 0;
-                int nothing = 0;
-
-                boolean[] checkNumArr = new boolean[9];
-                checkNumArr[serverNumber.charAt(0) - '1'] = true;
-                checkNumArr[serverNumber.charAt(1) - '1'] = true;
-                checkNumArr[serverNumber.charAt(2) - '1'] = true;
-
-                for (int i = 0; i < 3; i++) {
-                        char serverChar = serverNumber.charAt(i);
-                        char clientChar = clientNumber.charAt(i);
-
-                        if (serverChar == clientChar) {
-                                strike++;
-                                continue;
-                        }
-
-                        if (checkNumArr[clientChar - '1']) {
-                                ball++;
-                        }
-                }
-
-                if (strike == 0 && ball == 0) nothing = 1;
-                output.printResult(ball, strike, nothing);
-
-                return strike == 3;
         }
 }
