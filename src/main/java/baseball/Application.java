2a38e6ae22d683341ba4944ffbf4eeb3a4f2c194
bobo1201 <121841746+bobo1201@users.noreply.github.com>
2023-10-24 17:50:25 +0900

    23/10/24 17:50 코드 간소화

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,22 +8,11 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        StringBuilder sb = new StringBuilder();
 
         // 입력 받은 내용을 처리하거나 출력하기
         List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
-        }
 
-        String tf = "";
-
-        System.out.println(computer);
-
+        String tf = "1";
 
         System.out.println("숫자 야구 게임을 시작합니다.");
 
@@ -43,13 +32,10 @@ public class Application {
                 System.out.println(computer);
             }
 
-
             System.out.print("숫자를 입력해주세요 : ");
-
             String userInput = readLine();
 
             System.out.println("사용자가 입력한 값: " + userInput);
-
             String str[] = userInput.split("");
 
             int strike = 0;
