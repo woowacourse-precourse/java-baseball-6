a37b44c1c6a49ff76d2441aa0cd5da347d8f2a94
wooteco-daram <wooteco.daram@gmail.com>
2023-10-24 19:49:10 +0900

    test: `PrintSplitWhenBallAndStrikeExistRuleTest` 테스트 케이스에서 조건 알맞게 수정

--- src/test/java/baseball/output/rule/PrintSplitWhenBallAndStrikeExistRuleTest.java
+++ src/test/java/baseball/output/rule/PrintSplitWhenBallAndStrikeExistRuleTest.java
@@ -10,7 +10,7 @@ public class PrintSplitWhenBallAndStrikeExistRuleTest extends PrintOutputTest {
     @Test
     void 볼과_스트라이크가_모두_있을시_공백_출력() {
         final PrintSplitWhenBallAndStrikeExistRule rule = new PrintSplitWhenBallAndStrikeExistRule(
-                new BallAndStrike(0, 0)
+                new BallAndStrike(1, 1)
         );
         rule.print();
         assertThat(output()).contains(" ");
@@ -19,7 +19,7 @@ public class PrintSplitWhenBallAndStrikeExistRuleTest extends PrintOutputTest {
     @Test
     void 볼이_없을시_공백_출력하지_않음() {
         final PrintSplitWhenBallAndStrikeExistRule rule = new PrintSplitWhenBallAndStrikeExistRule(
-                new BallAndStrike(0, 0)
+                new BallAndStrike(0, 1)
         );
         rule.print();
         assertThat(output()).isEmpty();
@@ -28,7 +28,7 @@ public class PrintSplitWhenBallAndStrikeExistRuleTest extends PrintOutputTest {
     @Test
     void 스트라이크가_없을시_공백_출력하지_않음() {
         final PrintSplitWhenBallAndStrikeExistRule rule = new PrintSplitWhenBallAndStrikeExistRule(
-                new BallAndStrike(0, 0)
+                new BallAndStrike(1, 0)
         );
         rule.print();
         assertThat(output()).isEmpty();
