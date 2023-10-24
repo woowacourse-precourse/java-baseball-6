2c01d29b291413e999511671ec73444a0a6bb8b2
aka-nick <bechalrang@gmail.com>
2023-10-24 15:44:15 +0900

    리팩토링: BaseballGameRestarts를 추출하면서 관련된 기능(입력값을 재시작/종료값으로 판별하는 기능, 판별된 입력값이 '종료'에 해당하는지 확인하는 기능)을 편의 메서드로 구현

---
+++ src/main/java/baseball/game/BaseballGameRestarts.java
@@ -0,0 +1,31 @@
+package baseball.game;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public enum BaseballGameRestarts {
+
+    GAME_RESTART(1),
+    GAME_FINISH(2),
+    ;
+
+    private static final String MUST_INPUT_RESTARTS_MESSAGE = "1 또는 2를 입력해야 합니다.";
+
+    private final int inputRestart;
+
+    BaseballGameRestarts(int inputRestart) {
+        this.inputRestart = inputRestart;
+    }
+
+    public static BaseballGameRestarts find(Integer restartNumber) {
+        return Arrays.stream(BaseballGameRestarts.values())
+                .filter(gameRestart -> Objects.equals(gameRestart.inputRestart, restartNumber))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(MUST_INPUT_RESTARTS_MESSAGE));
+    }
+
+    public boolean wantToFinish() {
+        return this == GAME_FINISH;
+    }
+
+}
