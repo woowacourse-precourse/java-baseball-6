d90cabf27145705a57d461c1a5221a9fd35803d6
Kim, JeongHwan <jhkim980214@gmail.com>
2023-10-24 16:43:50 +0900

    출력 부분 변경

--- src/main/java/baseball/UserNumber.java
+++ src/main/java/baseball/UserNumber.java
@@ -6,10 +6,13 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Console;
 
 public class UserNumber {
+	private static final int NUMBER_LENGTH = 3;
+	private static final String OUTPUT_ENTER_NUMBER = "숫자를 입력해주세요 : ";
+
 	public List<Integer> userNumbers() {
 		List<Integer> user = new ArrayList<>();
-		while (user.size() < 3) {
-			System.out.print("숫자를 입력해주세요 : ");
+		while (user.size() < NUMBER_LENGTH) {
+			System.out.print(OUTPUT_ENTER_NUMBER);
 
 			String input = Console.readLine();
 
