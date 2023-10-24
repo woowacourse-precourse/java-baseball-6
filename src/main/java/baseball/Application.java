b7eac001df16fd30bec0cbc925cdf2f635572487
haeseon lee <haeseon4816@gmail.com>
2023-10-24 17:47:01 +0900

    README.md 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
+
 }
+
+
