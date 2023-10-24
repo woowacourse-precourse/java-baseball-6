d96a876b07578c85fc60b1a77192dafc6e215747
Woo-Shinyoung <tlsdud8205@naver.com>
2023-10-24 23:03:45 +0900

    Feat[view] 게임 재시작 여부 입력 문구 출력

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -8,5 +8,11 @@ public class InputView {
         String input = readLine();
         return input;
     }
+
+    public String readGameRestart(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = readLine();
+        return input;
+    }
 }
 
