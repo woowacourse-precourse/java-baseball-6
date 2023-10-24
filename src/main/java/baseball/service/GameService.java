80646af4838efccf7d02d00604313268afd8a6e7
ghdeo <goldencorn7@naver.com>
2023-10-25 01:11:29 +0900

    feat: Add playAgain method

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -70,4 +70,17 @@ public class GameService {
         System.out.println();
         return false;
     }
+
+    public boolean playAgain() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String userInput = scanner.nextLine();
+
+        if (userInput.equals("1")) {
+            return true;
+        } else if (userInput.equals("2")){
+            return false;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
 }
