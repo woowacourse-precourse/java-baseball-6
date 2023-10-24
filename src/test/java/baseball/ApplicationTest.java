5ccc8bd2abf63f19962ee5c57b5a13ca78fb92e6
yeon409 <yeon000409@gmail.com>
2023-10-24 23:14:30 +0900

    Add more application tests

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -24,9 +24,28 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 게임_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("471", "316", "513", "153", "135", "2");
+                    assertThat(output()).contains("1볼", "2볼", "3볼", "2볼 1스트라이크", "3스트라이크", "게임 종료");
+                },
+                1, 3, 5
+        );
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_문자입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1a3"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
