fcafaf605c7c795874a512734299b130250facaa
jmj005 <jmj00005@naver.com>
2023-10-24 23:25:40 +0900

    test: 재시작, 종료 유효성 테스트 추가

--- src/test/java/baseball/BaseballGameTest.java
+++ src/test/java/baseball/BaseballGameTest.java
@@ -1,2 +1,25 @@
-package baseball;public class BaseballGameTest {
+package baseball;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class BaseballGameTest extends ApplicationTest {
+    @ParameterizedTest
+    @CsvSource({
+            "0",
+            "3",
+            "q"
+    })
+    void 재시작_종료_유효성_실패_테스트(String playerChoice) {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThrows(IllegalArgumentException.class,
+                            () -> run("246", "135", playerChoice));
+                },
+                1, 3, 5
+        );
+    }
 }
