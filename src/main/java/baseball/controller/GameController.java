37097149764dd46c491fd8d59c5073169c925abc
HongChaa <jun761_1@naver.com>
2023-10-24 13:33:14 +0900

    refactor(GameController): Indent Depth 2 초과 부분 메서드로 분리

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -18,99 +18,86 @@ public class GameController {
     PlayerAnswer playerAnswer = new PlayerAnswer();
 
     public void playGame() {
-
+        List<Integer> computerRandomNumberList;
         boolean proceedGameFlag = true;
 
         gameView.printStartGameMessage();
 
         while (proceedGameFlag) {
-            computer.setComputerRandomNumber(computerController.getComputerRandomNumber());
-            proceedGameFlag = repeatFindAnswer();
-
+            computerRandomNumberList = computer.getComputerRandomNumber();
+            proceedGameFlag = repeatFindAnswer(computerRandomNumberList);
         }
     }
 
-    private boolean repeatFindAnswer() {
-
+    private boolean repeatFindAnswer(List<Integer> computerRandomNumberList) {
         boolean correctAnswerFlag = false;
         List<Integer> playerNumberList = new ArrayList<>();
+        String playerRetry;
 
         while (!correctAnswerFlag) {
-
             gameView.printInputNumberMessage();
             playerNumberList = player.getPlayerNumberList(Console.readLine());
 
             validate(playerNumberList);
 
-            correctAnswerFlag = compareAnswer(computer.getComputerRandomNumber(), playerNumberList);
+            correctAnswerFlag = compareAnswer(computerRandomNumberList, playerNumberList);
             gameView.printAnswerHintMessage(playerAnswer);
 
             if (correctAnswerFlag) {
                 gameView.printSuccessGameMessage();
-                String playerRetry = Console.readLine();
+                playerRetry = Console.readLine();
                 correctAnswerFlag = retryGame(playerRetry);
                 break;
             }
-
         }
 
         return correctAnswerFlag;
     }
 
-    private boolean compareAnswer(List<Integer> computerNumberList, List<Integer> playerNumberList) {
+    private boolean compareAnswer(List<Integer> computerRandomNumberList, List<Integer> playerNumberList) {
         playerAnswer.setStrike(0);
         playerAnswer.setBall(0);
 
         for (int i = 0; i < 3; i++) {
-
-            if (computerNumberList.get(i) == playerNumberList.get(i)) {
+            if (computerRandomNumberList.get(i) == playerNumberList.get(i)) {
                 playerAnswer.setStrike(playerAnswer.getStrike() + 1);
             }
-            if (computerNumberList.get(i) != playerNumberList.get(i)) {
-                if (computerNumberList.contains(playerNumberList.get(i))) {
+            if (computerRandomNumberList.get(i) != playerNumberList.get(i)) {
+                if (computerRandomNumberList.contains(playerNumberList.get(i))) {
                     playerAnswer.setBall(playerAnswer.getBall() + 1);
                 }
             }
-
         }
-
         return playerAnswer.getStrike() == 3;
     }
 
     private boolean retryGame(String playerOption) {
-
         switch (playerOption) {
-
             case "1" : return true;
 
             case "2" : return false;
 
             default: throw new IllegalArgumentException();
-
         }
-
     }
 
     private void validate(List<Integer> playerNumberList) {
-
         if (playerNumberList.size() != 3) {
-            System.out.println("playerNumberList 크기 : "+playerNumberList.size());
-            System.out.println("playerNumberList[] : " + playerNumberList.toString());
             throw new IllegalArgumentException();
         }
 
         for (int i = 0; i < playerNumberList.size(); i++) {
-            if (playerNumberList.get(i) < 1 || playerNumberList.get(i) > 9) {
-                System.out.println("범위 벗어남");
-                throw new IllegalArgumentException();
-            }
+            checkNumberRange(playerNumberList, i);
         }
 
         if (playerNumberList.size() != playerNumberList.stream().distinct().count()) {
             throw new IllegalArgumentException();
         }
-
-
     }
 
+    private void checkNumberRange(List<Integer> playerNumberList, int index) {
+        if (playerNumberList.get(index) < 1 || playerNumberList.get(index) > 9) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
