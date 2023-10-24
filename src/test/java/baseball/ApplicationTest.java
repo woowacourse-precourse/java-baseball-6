2b5e562d263f01e154678464fb51c1f3886703fe
Jaewon-pro <jaewonlee.pro@gmail.com>
2023-10-24 19:16:37 +0900

    feat(testing): 게임 전체 테스트 && 0 관련 예외 테스트 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,12 +1,15 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import baseball.locales.GameScripts;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -21,15 +24,61 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 게임_낫싱_볼_스트라이크_전체_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("246", "532", "541", "513", "135", "2");
+                    assertThat(output()).contains("낫싱", "1볼 1스트라이크", "2볼", "3볼", "3스트라이크", "게임 종료");
+                },
+                1, 3, 5
+        );
+    }
+
+
+    @Test
+    void 예외_테스트_잘못된_범위_초과_숫자() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
+    @Test
+    void 예외_테스트_잘못된_0_포함_숫자() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("203"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_잘못된_0으로_시작_숫자() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("013"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_잘못된_0으로_끝나는_숫자() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("890"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_잘못된_중복되는_숫자() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("223"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
     }
+
 }
