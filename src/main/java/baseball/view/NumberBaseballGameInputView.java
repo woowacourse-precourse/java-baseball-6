922fd5cfa48a1067f683bf330be72e15e256ef81
wnsgh12s <junho1273@gmail.com>
2023-10-24 13:24:33 +0900

    Feat: 입력 값 유효성 검사 기능 추가
    패턴 매칭을 통해 유효성 검사를 진행하였습니다. 또한 만약 사용자가 스캐너에 입력을 하지 않고 종료시 발생하는 NoSuchElementException 예외를 처리하는 로직을 추가했습니다.

--- src/main/java/baseball/view/NumberBaseballGameInputView.java
+++ src/main/java/baseball/view/NumberBaseballGameInputView.java
@@ -1,15 +1,35 @@
 package baseball.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
+import java.util.regex.Pattern;
 
 public class NumberBaseballGameInputView {
 
+
     public final String readUserInput() {
-        String input = Console.readLine();
-        if (input.length() != 3) {
-            throw new IllegalArgumentException("입력값은 3자리여야 합니다.");
+        String input = readNonNullInput();
+
+        if (!isValidInput(input)) {
+            throw new IllegalArgumentException("입력값은 3자리의 숫자여야 합니다.");
+        }
+
+        return input;
+    }
+
+    private static String readNonNullInput() {
+        String input;
+        try {
+            input = Console.readLine();
+        } catch (NoSuchElementException e) {
+            throw new IllegalArgumentException("입력값을 제공해야 합니다.");
         }
         return input;
     }
+
+    private boolean isValidInput(String str) {
+        return Pattern.matches("[0-9]{3}", str);
+    }
+
 
 }
