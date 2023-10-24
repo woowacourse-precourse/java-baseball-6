99c29b8a400ab28aa810401326a2fa600c34bbd6
noteasymin <noteasymin@gmail.com>
2023-10-24 16:34:26 +0900

    refactor: 상수 정리, 볼스트라이크 연산 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,13 +2,24 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
 
+    private static final int NUMBER_LENGTH = 3;
+    private static final int MIN_RANDOM_DIGIT = 1;
+    private static final int MAX_RANDOM_DIGIT = 9;
+    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String INPUT_PROMPT = "숫자를 입력해주세요 : ";
+    private static final String GAME_WIN_MESSAGE = "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String RESTART_GAME_CODE = "1";
+    private static final String EXIT_GAME_CODE = "2";
+
     public static void main(String[] args) {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println(GAME_START_MESSAGE);
         playBaseballGame();
     }
 
@@ -17,37 +28,51 @@ public class Application {
         do {
             List<Integer> randomNumber = createRandomNumber();
             gameCode = checkUserNumber(randomNumber);
-        } while (gameCode.equals("1"));
+        } while (gameCode.equals(RESTART_GAME_CODE));
     }
 
     private static String checkUserNumber(List<Integer> randomNumber) {
         while (true) {
             List<Integer> userNumber = getUserNumber();
-            int ball = 0;
-            int strike = 0;
+            int[] ballAndStrike = calculateBallAndStrike(userNumber, randomNumber);
 
-            for (int i = 0; i < 3; i++) {
+            for (int i = 0; i < NUMBER_LENGTH; i++) {
                 if (randomNumber.get(i).equals(userNumber.get(i))) {
                     strike += 1;
                 } else if (randomNumber.contains(userNumber.get(i))) {
                     ball += 1;
                 }
             }
-            printResult(strike, ball);
-            if (strike == 3) {
+            printResult(ballAndStrike[0], ballAndStrike[1]);
+            if (ballAndStrike[1] == NUMBER_LENGTH) {
                 break;
             }
         }
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println(GAME_RESTART_MESSAGE);
         return (Console.readLine());
     }
 
+    private static int[] calculateBallAndStrike(List<Integer> userNumber, List<Integer> randomNumber) {
+        int ball = 0;
+        int strike = 0;
+
+        for (int i = 0; i < NUMBER_LENGTH; i++) {
+            if (randomNumber.get(i).equals(userNumber.get(i))) {
+                strike += 1;
+            } else if (randomNumber.contains(userNumber.get(i))) {
+                ball += 1;
+            }
+        }
+
+        return new int[]{ball, strike};
+    }
+
     private static void printResult(int strike, int ball) {
-        if (strike == 3) {
+        if (strike == NUMBER_LENGTH) {
             System.out.println(strike + "스트라이크");
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        } else if ((ball > 0) && (strike > 0) && (strike < 3)) {
-            System.out.println(ball + "볼" + " " + strike + "스트라이크");
+            System.out.println(NUMBER_LENGTH + GAME_WIN_MESSAGE);
+        } else if (ball > 0 && strike > 0) {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
         } else if (ball > 0) {
             System.out.println(ball + "볼");
         } else if (strike > 0) {
@@ -60,8 +85,8 @@ public class Application {
     private static List<Integer> createRandomNumber() {
         List<Integer> computer = new ArrayList<>();
 
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+        while (computer.size() < NUMBER_LENGTH) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_RANDOM_DIGIT, MAX_RANDOM_DIGIT);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
@@ -71,16 +96,16 @@ public class Application {
 
     private static List<Integer> getUserNumber() {
         List<Integer> userNumber = new ArrayList<>();
-        String Number;
+        String number;
         String[] tempNumber;
         boolean isValid;
 
-        System.out.print("숫자를 입력해주세요 : ");
-        Number = Console.readLine();
-        isValid = isValidNumber(Number);
+        System.out.print(INPUT_PROMPT);
+        number = Console.readLine();
+        isValid = isValidNumber(number);
 
         if (isValid) {
-            tempNumber = Number.split("");
+            tempNumber = number.split("");
             for (String s : tempNumber) {
                 userNumber.add(Integer.parseInt(s));
             }
@@ -92,7 +117,7 @@ public class Application {
     }
 
     private static boolean isValidNumber(String input) {
-        if (input.length() != 3) {
+        if (input.length() != NUMBER_LENGTH) {
             return false;
         }
 
