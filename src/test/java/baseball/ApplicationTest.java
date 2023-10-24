0b59ebf19d2368f09778aab5be13dc8dbfc35579
parkseryu <pjh971212@gmail.com>
2023-10-24 23:21:02 +0900

    test: 랜덤 숫자 생성 관련 테스트 코드 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,14 +1,35 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import baseball.controller.ComputerController;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.HashSet;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
 class ApplicationTest extends NsTest {
+
+    @Test
+    void 컴퓨터가_생성하는_숫자가_올바른지_테스트() {
+        List<Integer> generatedNumbers = ComputerController.getBaseballNumber();
+
+        // 숫자가 정확히 3개인지 확인
+        assertThat(generatedNumbers).hasSize(3);
+
+        // 모든 숫자가 1~9 범위 안에 있는지 확인
+        for (int num : generatedNumbers) {
+            assertThat(num).isBetween(1, 9);
+        }
+
+        // 중복된 숫자가 없는지 확인
+        int distinctCount = new HashSet<>(generatedNumbers).size();
+        assertThat(distinctCount).isEqualTo(3);
+    }
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
