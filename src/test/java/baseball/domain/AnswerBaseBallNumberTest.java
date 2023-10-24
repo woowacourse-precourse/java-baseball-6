00a8006896f7cf20d40f524022e80b434419ebb0
howudong <tjdvy963@naver.com>
2023-10-24 11:47:51 +0900

    refactor : AnswerBaseBallNumber로 클래스 이름 변경 및 상속으로 변경, 함수 추가

--- src/test/java/baseball/domain/AnswerBaseBallNumberTest.java
+++ src/test/java/baseball/domain/AnswerBaseBallNumberTest.java
@@ -1,7 +1,8 @@
 package baseball.domain;
 
+import baseball.domain.numbers.AnswerBaseBallNumber;
 import baseball.domain.numbers.AnswerNumberGenerator;
-import baseball.domain.numbers.AnswerNumbers;
+import baseball.domain.numbers.BaseBallNumber;
 import baseball.domain.numbers.NumberGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,15 +11,15 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class AnswerNumbersTest {
-    private final AnswerNumbers answerNumbers = AnswerNumbers.createAnswerNumbers(new AnswerNumberGenerator());
+class AnswerBaseBallNumberTest {
+    private final AnswerBaseBallNumber answerBaseBallNumber = AnswerBaseBallNumber.createAnswerNumbers(new AnswerNumberGenerator());
 
     @Test
     @DisplayName("정답 번호가 1,2,3이고 플레이어 번호도 1,2,3이면 true를 반환하면 성공한다.")
     void 플레이어_번호_일치_O() {
         //given
-        AnswerNumbers answerNumbers = AnswerNumbers.createAnswerNumbers(new TestGenerator());
-        boolean result = answerNumbers.isSameWithAnswer(List.of(1, 2, 3));
+        AnswerBaseBallNumber answerBaseBallNumber = AnswerBaseBallNumber.createAnswerNumbers(new TestGenerator());
+        boolean result = answerBaseBallNumber.isSameWithAnswer(new BaseBallNumber(List.of(1, 2, 3)));
         //when
         assertThat(result).isTrue();
     }
@@ -27,8 +28,8 @@ class AnswerNumbersTest {
     @DisplayName("정답 번호가 1,2,3이고 플레이어 번호가 4,2,1이면 false를 반환하면 성공한다.")
     void 플레이어_번호_일치_X() {
         //given
-        AnswerNumbers answerNumbers = AnswerNumbers.createAnswerNumbers(new TestGenerator());
-        boolean result = answerNumbers.isSameWithAnswer(List.of(4, 2, 1));
+        AnswerBaseBallNumber answerBaseBallNumber = AnswerBaseBallNumber.createAnswerNumbers(new TestGenerator());
+        boolean result = answerBaseBallNumber.isSameWithAnswer(new BaseBallNumber(List.of(4, 2, 1)));
         //when
         assertThat(result).isFalse();
     }
--- src/test/java/baseball/domain/HintProviderTest.java
+++ src/test/java/baseball/domain/HintProviderTest.java
@@ -2,9 +2,9 @@ package baseball.domain;
 
 import baseball.domain.hints.HintProvider;
 import baseball.domain.hints.HintType;
-import baseball.domain.numbers.AnswerNumbers;
+import baseball.domain.numbers.AnswerBaseBallNumber;
+import baseball.domain.numbers.BaseBallNumber;
 import baseball.domain.numbers.NumberGenerator;
-import baseball.domain.numbers.PlayerNumbers;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,16 +13,16 @@ import java.util.List;
 import java.util.Map;
 
 class HintProviderTest {
-    private final AnswerNumbers answerNumbers = AnswerNumbers.createAnswerNumbers(new TestGenerator());
-    private final HintProvider hintProvider = new HintProvider(answerNumbers);
+    private final AnswerBaseBallNumber answerBaseBallNumber = AnswerBaseBallNumber.createAnswerNumbers(new TestGenerator());
+    private final HintProvider hintProvider = new HintProvider(answerBaseBallNumber);
 
     @Test
     @DisplayName("플레이어의 숫자가 1,2,3 일 때 STRIKE가 3개가 나오면 성공한다.")
     void STRIKE_3() {
         //given
-        List<Integer> playerNumbers = new PlayerNumbers(List.of(1, 2, 3)).getNumbers();
+        BaseBallNumber playerNumber = new BaseBallNumber(List.of(1, 2, 3));
         //when
-        Map<HintType, Integer> hints = hintProvider.compareNumbers(playerNumbers);
+        Map<HintType, Integer> hints = hintProvider.compareNumbers(playerNumber);
         //then
         Assertions.assertThat(hints.get(HintType.STRIKE)).isEqualTo(3);
     }
@@ -31,9 +31,9 @@ class HintProviderTest {
     @DisplayName("플레이어의 숫자가 3,1,2 일 때 BALL이 3개가 나오면 성공한다.")
     void BALL_3() {
         //given
-        List<Integer> playerNumbers = new PlayerNumbers(List.of(3, 1, 2)).getNumbers();
+        BaseBallNumber playerNumber = new BaseBallNumber(List.of(3, 1, 2));
         //when
-        Map<HintType, Integer> hints = hintProvider.compareNumbers(playerNumbers);
+        Map<HintType, Integer> hints = hintProvider.compareNumbers(playerNumber);
         //then
         Assertions.assertThat(hints.get(HintType.BALL)).isEqualTo(3);
     }
@@ -42,9 +42,9 @@ class HintProviderTest {
     @DisplayName("플레이어의 숫자가 3,2,1 일 때 2BALL, 1 STRIKE이면 성공한다.")
     void STRIKE_1_AND_BALL_2() {
         //given
-        List<Integer> playerNumbers = new PlayerNumbers(List.of(3, 2, 1)).getNumbers();
+        BaseBallNumber playerNumber = new BaseBallNumber(List.of(3, 2, 1));
         //when
-        Map<HintType, Integer> hints = hintProvider.compareNumbers(playerNumbers);
+        Map<HintType, Integer> hints = hintProvider.compareNumbers(playerNumber);
         //then
         Assertions.assertThat(hints.get(HintType.BALL)).isEqualTo(2);
         Assertions.assertThat(hints.get(HintType.STRIKE)).isEqualTo(1);
@@ -54,9 +54,9 @@ class HintProviderTest {
     @DisplayName("플레이어의 숫자가 4,5,6 일 때 1 NOTHING 이면 성공한다.")
     void ONLY_1_NOTHING() {
         //given
-        List<Integer> playerNumbers = new PlayerNumbers(List.of(4, 5, 6)).getNumbers();
+        BaseBallNumber playerNumber = new BaseBallNumber(List.of(4, 5, 6));
         //when
-        Map<HintType, Integer> hints = hintProvider.compareNumbers(playerNumbers);
+        Map<HintType, Integer> hints = hintProvider.compareNumbers(playerNumber);
         //then
         Assertions.assertThat(hints.get(HintType.NOTHING)).isEqualTo(1);
     }
