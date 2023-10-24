69ae47c86bf4eb5c98c695c72b9bbb520f2baed9
seastamp <seastamp10@gmail.com>
2023-10-25 02:07:38 +0900

    feat: Score 구현 및 게임종료 메세지 출력 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -7,28 +7,37 @@ public class Game {
 
     private List<Integer> computer; // 정답을 담을 리스트
     private List<Integer> player; // 사용자의 입력값을 담을 리스트
-    public boolean playBaseball(){
+
+    public boolean playBaseball() {
         // startGame();
         // while(!onGame())
         // if(!quiteGame()) (
         // return true;
         return false;
     }
-    public void startGame(){
+
+    public void startGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
         computer = RandomUtil.createRandomNumbers();
     }
 
-    public boolean onGame(){
-        System.out.print("숫자를 입력해주세요 : ");
-        // 입력받는 메서드 호출
-        // 점수 계산하는 score 메서드 호출 / 결과값 출력
-        // 정답을 맞추면 return true / 틀리면 return false
+    public boolean onGame() {
+        while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
+            try {
+                player = InputDevice.readPlayerNumbers();
+            } catch (IllegalArgumentException e) {
+                return false;
+            }
+            if (Score.calculateScore(computer, player) == 3) {
+                break;
+            }
+        }
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
         return true;
     }
 
-    public boolean quitGame(){
-        // 3개의 숫자를 모두 맞히셨습니다! 게임종료
+    public boolean quitGame() {
         // 게임을 새로 시작혀려면 1, 종료하려면 2를 입력하세요.
         // 입력받는 메서드 호출
         // 결과 값에 따라 프로그램 종료 or start game()
--- src/main/java/baseball/Socre.java
+++
@@ -1,5 +0,0 @@
-package baseball;
-
-public class Socre {
-    // 점수를 계산 하고 결과값 출력하는 메서드가 담길 예정
-}
