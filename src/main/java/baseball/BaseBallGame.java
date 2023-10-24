d536088258dc7873b592850aab5cd1d5274a149f
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-10-24 22:56:48 +0900

    feat : BaseBallGame 게임 결과를 알려주는 기능 추가

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -14,10 +14,8 @@ public class BaseBallGame {
         return randomNumberGenerator.createRandomNumbers();
     }
 
-    public void createGameResult(List<Integer> tryNumbers, List<Integer> answerNumbers) {
+    public String createGameResult(List<Integer> tryNumbers, List<Integer> answerNumbers) {
         GameReferee gameReferee = new GameReferee(new Ball(answerNumbers));
-        Score score = gameReferee.judge(new Ball(tryNumbers));
-
-        // TODO: 게임 결과 반환하기!
+        return gameReferee.judge(new Ball(tryNumbers)).name();
     }
 }
