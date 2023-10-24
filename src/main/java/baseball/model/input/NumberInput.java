830d1e5156403e74bcaacbd3d0d4d04cff6dd4d7
haiseong <wjdgotjd9908@gmail.com>
2023-10-24 12:04:55 +0900

    refactor: check -> validate 로 이름 변경 & validateNumber 숫자로 비교연산 하도록 변경  & 스트림 문법 사용으로 코드 간결화

--- src/main/java/baseball/model/input/NumberInput.java
+++ src/main/java/baseball/model/input/NumberInput.java
@@ -4,15 +4,13 @@ import static baseball.enums.NumberInputErrorMessage.DUPLICATE_NUMBER;
 import static baseball.enums.NumberInputErrorMessage.INVALID_CHARACTER;
 import static baseball.enums.NumberInputErrorMessage.INVALID_LENGTH;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.stream.Collectors;
 
 public class NumberInput extends PlayerInput {
     private static final int VALID_INPUT_LENGTH = 3;
-    private static final char VALID_MIN_CHAR = '1';
-    private static final char VALID_MAX_CHAR = '9';
+    private static final char VALID_MIN_NUM = 1;
+    private static final char VALID_MAX_NUM = 9;
 
     public NumberInput(String input) {
         super(input);
@@ -20,43 +18,42 @@ public class NumberInput extends PlayerInput {
     }
 
     protected void validate() {
-        checkInputLength();
-        checkAllCharacter();
-        checkDuplicateChar();
+        validateInputLength();
+        validateAllNumbers();
+        validateDuplicateCharacter();
     }
 
-    private void checkInputLength() {
+    private void validateInputLength() {
         if (input.length() != VALID_INPUT_LENGTH)
             throw new IllegalArgumentException(INVALID_LENGTH.getMessage());
     }
 
-    private void checkCharacter(char ch) {
-        if ((ch < VALID_MIN_CHAR) || (ch > VALID_MAX_CHAR)) {
+    private void validateAllNumbers() {
+        for (int i = 0; i < VALID_INPUT_LENGTH; i++) {
+            int num = Character.getNumericValue(input.charAt(i));
+            validateNumber(num);
+        }
+    }
+
+    private void validateNumber(int num) {
+        if ((num < VALID_MIN_NUM) || (num > VALID_MAX_NUM)) {
             throw new IllegalArgumentException(INVALID_CHARACTER.getMessage());
         }
     }
 
-    private void checkAllCharacter() {
-        for (int i = 0; i < VALID_INPUT_LENGTH; i++) {
-            checkCharacter(input.charAt(i));
-        }
-    }
+    private void validateDuplicateCharacter() {
+        long count = input.chars()
+                .distinct()
+                .count();
 
-    private void checkDuplicateChar() {
-        String[] split = input.split("");
-        Set<String> set = new HashSet<>(List.of(split));
-        if (set.size() != VALID_INPUT_LENGTH) {
+        if (count != VALID_INPUT_LENGTH) {
             throw new IllegalArgumentException(DUPLICATE_NUMBER.getMessage());
         }
     }
 
     public List<Integer> parseInputToIntegerList() {
-        List<Integer> list = new ArrayList<>(3);
-        String[] split = input.split("");
-        for (int i = 0; i < VALID_INPUT_LENGTH; i++) {
-            Integer num = Integer.parseInt(split[i]);
-            list.add(num);
-        }
-        return list;
+        return input.chars()
+                .mapToObj(Character::getNumericValue)
+                .collect(Collectors.toList());
     }
 }
