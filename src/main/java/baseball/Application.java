85ebc4058b136a11a714dea03f53cf76167ffebe
cmsxi <cms0755@naver.com>
2023-10-24 21:50:43 +0900

    feat : 야구 게임 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import static baseball.BaseballGame.playGame;
+import static baseball.BaseballGame.reset;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        boolean play = true;
+        while(play){
+            playGame();
+            play = reset();
+        }
     }
 }
