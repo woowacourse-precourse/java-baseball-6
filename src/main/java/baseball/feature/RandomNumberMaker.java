82adaba2a6d2ee3923f5c87f601544c85c9d5ad3
doka <posmane5@gmail.com>
2023-10-24 18:38:19 +0900

    chore : 주석 수정

--- src/main/java/baseball/feature/RandomNumberMaker.java
+++ src/main/java/baseball/feature/RandomNumberMaker.java
@@ -6,6 +6,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNumberMaker {
     public List<Integer> maker() {
+        // 숫자를 담을 리스트 생성
+        // 리스트에 숫자가 3개가 차면 종료 (반복)
+        // 1~9 랜덤한 수 생성
+        // 리스트 내부에 존재하지 않는수라면 리스트에 담기
+
         List<Integer> computerNumbers = new ArrayList<>();
         //1~9 숫자 생성
         while (computerNumbers.size() < 3) {
