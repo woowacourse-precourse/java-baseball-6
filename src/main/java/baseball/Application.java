cdf8b8aaf533ab1e42a1ff5ee91ef2f5687e0a6e
becooq81 <becooq81@gmail.com>
2023-10-25 02:10:56 +0900

    refactor(Application): replaced string literal with message constant variable for better maintainability

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,6 +6,7 @@ import baseball.view.InputView;
 public class Application {
     private static final String RESTART = "1";
     private static final String END = "2";
+    private static final String RESTART_OR_END_ONLY_MESSAGE = "1 또는 2를 입력해주세요.";
 
     public static void main(String[] args) {
         boolean isContinue = true;
@@ -19,7 +20,7 @@ public class Application {
     private static boolean restart() {
         String input = InputView.restartGame();
         if (!input.equals(RESTART) && !input.equals(END)) {
-            throw new IllegalArgumentException("1 또는 2를 입력해주세요.");
+            throw new IllegalArgumentException(RESTART_OR_END_ONLY_MESSAGE);
         }
         return input.equals(RESTART);
     }
