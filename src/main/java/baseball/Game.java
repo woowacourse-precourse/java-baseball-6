c279a187b72ffaa0095bc01992dbf3c1141e2d68
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-10-24 13:10:01 +0900

    2023.10.24_FEAT : Messages class 구현 완료

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,12 @@
+package baseball;
+
+import static baseball.Messages.*;
+
+public class Game {
+    private final String winMessage = Messages.WIN_MESSAGE;
+    private Oppoent oppoent;
+    private int strike;
+    private int ball;
+
+
+}
---
+++ src/main/java/baseball/Messages.java
@@ -0,0 +1,13 @@
+package baseball;
+
+public class Messages {
+
+    public static final String WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String INPUT_MESSAGE ="숫자를 입력해주세요 : ";
+    public static final String NOTHING = "낫싱";
+    public static final String BALL = "볼";
+    public static final String STRIKE = "스트라이크";
+    public static final String RESTART_OR_FINISH = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String RESTART_GAME = "1";
+    public static final String FINISH_GAME = "2";
+}
