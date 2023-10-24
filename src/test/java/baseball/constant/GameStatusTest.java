f03b3314d52e108ba52cc3e82ab65037f79b0386
yoonjuho <dbswn990@naver.com>
2023-10-25 04:15:29 +0900

    test(GameStatusTest): 예외 테스트 추가

--- src/test/java/baseball/constant/GameStatusTest.java
+++ src/test/java/baseball/constant/GameStatusTest.java
@@ -1,29 +1,35 @@
 package baseball.constant;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.assertj.core.api.Assertions.*;
 
 class GameStatusTest {
 
     @DisplayName("재시작에 해당하는 수를 입력 시 게임을 다시 시작할 수 있다")
     @Test
-    void restart(){
+    void restart() {
         Integer retryNumber = 1;
-        GameStatus expected = GameStatus.find(retryNumber);
-        assertThat(GameStatus.RESTART).isEqualTo(expected);
+        GameStatus result = GameStatus.apply(retryNumber);
+        assertThat(result).isEqualTo(GameStatus.RESTART);
     }
 
     @DisplayName("종료에 해당하는 수를 입력 시 게임을 종료할 수 있다")
     @Test
-    void exit(){
-        Integer exitNumber = 2;
-        GameStatus expected = GameStatus.find(exitNumber);
-        assertThat(GameStatus.EXIT).isEqualTo(expected);
+    void quit() {
+        Integer quitNumber = 2;
+        GameStatus result = GameStatus.apply(quitNumber);
+        assertThat(result).isEqualTo(GameStatus.QUIT);
     }
 
+    @DisplayName("종료/재시작에 해당하지 않는 수를 입력 시 예외를 던진다")
+    @Test
+    void exception() {
+        Integer inValidNumber = 3;
+        assertThrows(IllegalArgumentException.class, () -> GameStatus.apply(inValidNumber));
+    }
 
 
 }
