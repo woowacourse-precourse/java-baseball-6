2ffea54a2432f91ce7151b5f60a33b51486f7140
mango <esudatcom@gmail.com>
2023-10-24 10:37:37 +0900

    feat: isValid

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,8 +9,9 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-            int[] computerNumbers = generateComputerNumbers();
-            System.out.println(Arrays.toString(computerNumbers));
+        int[] computerNumbers = generateComputerNumbers();
+        System.out.println(Arrays.toString(computerNumbers));
+        System.out.println(isValid("124"));
 
     }
 
@@ -38,10 +39,46 @@ public class Application {
     }
 
     // 게임 진행
+    private static void playGame(int[] computerNumbers) {
+
+        // 숫자 입력받기
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+
+        if (isValid(input)) {
+            System.out.println("⭐ 올바른 값입니다.");
+        } else {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+    }
+
     // 사용자가 입력한 값 유효성 확인
+    private static boolean isValid(String input) {
+        // 3자리 수이며, 1~9까지의 숫자만 포함
+        if (!input.matches("^[1-9]{3}$")) {
+            return false;
+        }
+
+        // 중복된 숫자가 없는지 확인
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = i + 1; j < input.length(); j++) {
+                if (input.charAt(i) == input.charAt(j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
     // 문자열 입력 값 -> 정수 배열로 변환
+
+
     // 스트라이크 수 계산
-    // 볼 수 계산 (스트라이크 포함)
+
+
+    // 볼 수 계산
+
+
     // 결과 출력
 
 }
