575571982e5b5a04be8119f8bb742f48365168b9
박준영 <wnsdud6969@naver.com>
2023-10-25 01:02:06 +0900

    feat(Constant): add user request, response message

--- src/main/java/baseball/Constant.java
+++ src/main/java/baseball/Constant.java
@@ -1,4 +1,32 @@
 package baseball;
 
 public class Constant {
+
+    public enum UserRequestMessage {
+        GUESS_NUMBER_REQUEST_MESSAGE("숫자를 입력해주세요 : "),
+        REPEAT_MESSAGE("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        private final String message;
+
+        UserRequestMessage(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+
+    public enum UserResponseMessage{
+        BALL_MESSAGE("볼 "),
+        STRIKE_MESSAGE("스트라이크"),
+        NOTHING_MESSAGE("낫싱"),
+        ALL_CORRECT_MESSAGE("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
+        private final String message;
+
+        UserResponseMessage(String message) {
+            this.message = message;
+        }
+    }
 }
