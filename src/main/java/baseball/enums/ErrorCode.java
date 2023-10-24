28853001dfc22f0cf3edc4b2cd3ffda89b37a9e7
Choo <choo000407@naver.com>
2023-10-25 05:58:10 +0900

    feat(ErrorCode): 에러 메시지 enum 추가

--- src/main/java/baseball/enums/ErrorCode.java
+++ src/main/java/baseball/enums/ErrorCode.java
@@ -1,4 +1,18 @@
 package baseball.enums;
 
 public enum ErrorCode {
+    WRONG_LENGTH("숫자의 길이가 3이 아닙니다."),
+    DUPLICATE_NUMBER("중복된 숫자가 있습니다."),
+    OUT_OF_RANGE("1~9 사이의 숫자가 아닙니다."),
+    INVALID_GAME_CHOICE("지속 여부를 표시하는 1~2 사이의 숫자가 아닙니다.");
+
+    private final String message;
+
+    ErrorCode(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
