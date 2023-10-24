b1335ee6cfd945bca4bb703f3a9c283cc2d4aaed
Kim Taeyeong <tyeong1102@gmail.com>
2023-10-24 16:14:51 +0900

    feat: GameController 생성

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,16 +1,21 @@
 package baseball.controller;
 
+import baseball.view.Input;
 import baseball.view.Output;
+import java.util.List;
 
 public class GameController {
 
+    private final Input input = new Input();
     private final Output output = new Output();
+    private final UserUtil userUtil = new UserUtil();
+    private final RandomUtil randomUtil = new RandomUtil();
+
+    private List<Integer> computerNumbers;
 
     public GameController() {
         output.printStartGame();
     }
 
-    public void startGame() {
-        
-    }
+    
 }
