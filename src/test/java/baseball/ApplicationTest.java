cd95f68f771451ed678106a17b2fedf3f34d8c97
YuJeongYun <yunyj99@naver.com>
2023-10-24 23:43:05 +0900

    test: 컴퓨터 숫자와 사용자 입력 숫자 비교 기능 테스트 코드 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,8 +1,11 @@
 package baseball;
 
 import baseball.baseballGame.BaseBallGame;
+import baseball.baseballGame.CompareNum;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -36,6 +39,27 @@ class ApplicationTest extends NsTest {
         BaseBallGame bGame = new BaseBallGame(cnt);
 
         assertThat(bGame.getNumbers().size()).isEqualTo(cnt);
+    }
+
+    @Test
+    void 숫자_비교_테스트() {
+        CompareNum cn = new CompareNum();
+
+        Map<String, Integer> map = cn.compare(123, 123);
+        assertThat(map.get("strike")).isEqualTo(3);
+        assertThat(map.get("ball")).isEqualTo(0);
+
+        map = cn.compare(125, 123);
+        assertThat(map.get("strike")).isEqualTo(2);
+        assertThat(map.get("ball")).isEqualTo(0);
+
+        map = cn.compare(531, 248);
+        assertThat(map.get("strike")).isEqualTo(0);
+        assertThat(map.get("ball")).isEqualTo(0);
+
+        map = cn.compare(125, 851);
+        assertThat(map.get("strike")).isEqualTo(0);
+        assertThat(map.get("ball")).isEqualTo(2);
     }
 
     @Override
