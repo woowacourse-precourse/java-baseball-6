5db95c0650e8764648f4854f1ec7c12a6cc977a9
jangseop <kgh010529@naver.com>
2023-10-25 01:25:04 +0900

    feat: GamePlay 인터페이스 구현체 생성
    숫자 예측을 위한 PredictGamePlay, 게임시작을 위한 StartGamePlay 생성

--- src/main/java/baseball/game/PredictGamePlay.java
+++ src/main/java/baseball/game/PredictGamePlay.java
@@ -1,2 +1,8 @@
-package baseball.game;public class PredictGamePlay {
+package baseball.game;
+
+public class PredictGamePlay implements GamePlay {
+    @Override
+    public String play(String... args) {
+        return null;
+    }
 }
--- src/main/java/baseball/game/StartGamePlay.java
+++ src/main/java/baseball/game/StartGamePlay.java
@@ -1,2 +1,8 @@
-package baseball.game;public class StartGamePlay {
+package baseball.game;
+
+public class StartGamePlay implements GamePlay {
+    @Override
+    public String play(String... args) {
+        return null;
+    }
 }
