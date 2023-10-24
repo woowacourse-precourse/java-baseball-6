8b81c1dedd98ccf8eb800465a65fc62c49012728
j2noo <wlsdndml213@naver.com>
2023-10-24 23:18:23 +0900

    feat : 예외처리 코드 추가

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -12,10 +12,38 @@ public class User {
   ArrayList<Integer> userNumbers = new ArrayList<Integer>();
   public  void inputUserNumbers(){
     userNumbers.clear();
-    String input = Console.readLine();
-    for(int i=0;i<3;i++){
-      userNumbers.add(input.charAt(i)-'0');
-    }
+ 
+     
+      try {
+        String inputStr = Console.readLine();
+        if(inputStr.length()>3 || inputStr.length()<1)
+          throw new IllegalArgumentException("3개의 숫자가 입력되지 않았습니다.");
+
+        //ArrayList<Integer> inputNumbers = new ArrayList<Integer>(3);
+        for(int i=0;i<3;i++){
+          int parsedNum=inputStr.charAt(i)-'0';
+          if(parsedNum<1 || parsedNum>9)
+            throw new IllegalArgumentException("1에서 9사이의 숫자를 입력해 주세요");
+          
+          for(int j=0;j<i;j++){
+            if(userNumbers.get(j)==parsedNum)
+              throw new IllegalArgumentException("숫자에 중복이 존재합니다");
+          }
+
+          userNumbers.add(parsedNum);
+        }
+        
+      }
+      catch (Exception e){
+        System.out.println(e);
+      }
+//      for(int i=0;i<3;i++){
+//        userNumbers.add(input.charAt(i)-'0');
+////        userNumbers.add(1);
+//      }
+    
+
+   
 
   }
 }
