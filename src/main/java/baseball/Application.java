a074fd1fe0a0fee13ab6d4c80c1cf87d46b46bbb
minj-L <wonder_minji@naver.com>
2023-10-24 23:51:28 +0900

    [ADD] 3개의 중복되지 않는 숫자 생성 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,32 @@
 package baseball;
+import java.lang.String;
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+		
+    	List<Integer> computer = new ArrayList<>();
+    	while (computer.size() < 3) {
+    	    int randomNumber = Randoms.pickNumberInRange(1, 9);
+    	    if (!computer.contains(randomNumber)) {
+    	        computer.add(randomNumber);
+    	    }
+    	}
+    
+    	
+		/*
+		 * System.out.println("숫자 야구 게임을 시작합니다."); System.out.print("숫자를 입력해주세요 : ");
+		 * 
+		 * String gamerNum = Console.readLine();
+		 * 
+		 * System.out.println("프로그램 종료");
+		 */
+    	
+    	System.out.println("computer : "  +computer);
+    	//Console.readLine();
     }
 }
