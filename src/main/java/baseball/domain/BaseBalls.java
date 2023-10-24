2cd28cf55f58475411a634a22b3ff8f71288e214
K-KY <kyuyoungk@naver.com>
2023-10-24 18:15:44 +0900

    Refactor(BaseBalls) : 불필요한 변수선언 제거

--- src/main/java/baseball/domain/BaseBalls.java
+++ src/main/java/baseball/domain/BaseBalls.java
@@ -20,10 +20,9 @@ public class BaseBalls {
     private BallScore getScore(CompareNumber userBall) {
         //인자로 CompareNumber(pos, num)을 받는다. -> generatedBalls의 각 객체의 해시코드와 num값을 비교한다.
         //generatedBalls 에는 각 자리의 숫자를 CompareNumber(pos, num) 의 형식으로 변환한 객체가 들어있다.
-        BallScore ballScore = generatedBalls.stream()
+        return generatedBalls.stream()
                 .map(v -> v.calculateScore(userBall))
                 .filter(v -> !v.equals(BallScore.NOTHING))
                 .findFirst().orElse(BallScore.NOTHING);
-        return ballScore;
     }
 }
