128ce956b7793bc965634633a093aa26ac4b8306
yoonjuho <dbswn990@naver.com>
2023-10-25 04:14:41 +0900

    refactor(Computer): 변수 이름 변경

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -2,19 +2,18 @@ package baseball.model;
 
 import baseball.constant.GameResult;
 import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class Computer {
-    private GameNumber gameNumber;
+    private final Numbers numbers;
 
-    public Computer(List<Integer> numbers){
-        gameNumber = new GameNumber(numbers);
+    public Computer(List<Integer> numbers) {
+        this.numbers = new Numbers(numbers);
     }
 
-    public static List<Integer> createNumbers(){
+    public static List<Integer> createNumbers() {
         List<Integer> numbers = new ArrayList<>();
         while (numbers.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -25,12 +24,12 @@ public class Computer {
         return numbers;
     }
 
-    public Map<GameResult, Long> compare(GameNumber userGameNumber){
-        long matchCount = gameNumber.containsCount(userGameNumber);
-        long strikeCount = gameNumber.matchCount(userGameNumber);
-        long ballCount = Math.abs(strikeCount - matchCount);
-        if(strikeCount == 0 && ballCount == 0){
-            return Map.of(GameResult.NOTHING, matchCount);
+    public Map<GameResult, Long> compare(Numbers userNumbers) {
+        long totalCount = numbers.countContains(userNumbers);
+        long strikeCount = numbers.countMatching(userNumbers);
+        long ballCount = Math.abs(strikeCount - totalCount);
+        if (strikeCount == 0 && ballCount == 0) {
+            return Map.of(GameResult.NOTHING, totalCount);
         }
         return Map.of(GameResult.STRIKE, strikeCount, GameResult.BALL, ballCount);
 
--- src/test/java/baseball/model/ComputerTest.java
+++ src/test/java/baseball/model/ComputerTest.java
@@ -1,30 +1,29 @@
 package baseball.model;
 
-import baseball.constant.GameResult;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import baseball.constant.GameResult;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 
 class ComputerTest {
 
     @Test
     @DisplayName("컴퓨터는 숫자를 생성할 수 있다")
-    void createNumbers(){
+    void createNumbers() {
         List<Integer> numbers = Computer.createNumbers();
         Set<Integer> nonDuplicateNumbers = numbers.stream().collect(Collectors.toSet());
-        List<Integer> validNumbersRange = List.of(1,2,3,4,5,6,7,8,9);
+        List<Integer> validNumbersRange = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
 
         assertThat(numbers.size()).isEqualTo(3);
         assertThat(nonDuplicateNumbers.size()).isEqualTo(3);
@@ -34,21 +33,21 @@ class ComputerTest {
     @DisplayName("컴퓨터는 숫자를 비교해 스트라이크와 볼 개수를 반환한다")
     @ParameterizedTest
     @MethodSource("compareData")
-    void compare(List<Integer> computerNumber, List<Integer> userNumber, Map<GameResult ,Long> expected){
+    void compare(List<Integer> computerNumber, List<Integer> userNumber, Map<GameResult, Long> expected) {
         Computer computer = new Computer(computerNumber);
-        GameNumber userGameNumber = new GameNumber(userNumber);
-        Map<GameResult, Long> result = computer.compare(userGameNumber);
+        Numbers userNumbers = new Numbers(userNumber);
+        Map<GameResult, Long> result = computer.compare(userNumbers);
 
-        assertThat(expected.get(GameResult.STRIKE)).isEqualTo(result.get(GameResult.STRIKE));
-        assertThat(expected.get(GameResult.BALL)).isEqualTo(result.get(GameResult.BALL));
+        assertThat(result.get(GameResult.STRIKE)).isEqualTo(expected.get(GameResult.STRIKE));
+        assertThat(result.get(GameResult.BALL)).isEqualTo(expected.get(GameResult.BALL));
     }
 
     @DisplayName("컴퓨터는 숫자를 비교해 같은 수가 없으면 낫싱을 반환한다")
     @Test
-    void compareNothing(){
+    void compareAndReturnNothing() {
         Computer computer = new Computer(List.of(1, 2, 3));
-        GameNumber userGameNumber = new GameNumber(List.of(4, 5, 6));
-        Map<GameResult, Long> result = computer.compare(userGameNumber);
+        Numbers userNumbers = new Numbers(List.of(4, 5, 6));
+        Map<GameResult, Long> result = computer.compare(userNumbers);
 
         assertThat(result.size()).isEqualTo(1);
         assertThat(result.containsKey(GameResult.NOTHING)).isTrue();
@@ -58,13 +57,20 @@ class ComputerTest {
     static Stream<Arguments> compareData() {
         List<Integer> computerNumber = Arrays.asList(4, 2, 3);
         return Stream.of(
-                Arguments.of(computerNumber, Arrays.asList(4, 2, 3), Map.of(GameResult.STRIKE, 3L, GameResult.BALL, 0L)),
-                Arguments.of(computerNumber, Arrays.asList(1, 2, 3), Map.of(GameResult.STRIKE, 2L, GameResult.BALL, 0L)),
-                Arguments.of(computerNumber, Arrays.asList(4, 3, 2), Map.of(GameResult.STRIKE, 1L, GameResult.BALL, 2L)),
-                Arguments.of(computerNumber, Arrays.asList(3, 2, 5), Map.of(GameResult.STRIKE, 1L, GameResult.BALL, 1L)),
-                Arguments.of(computerNumber, Arrays.asList(4, 5, 8), Map.of(GameResult.STRIKE, 1L, GameResult.BALL, 0L)),
-                Arguments.of(computerNumber, Arrays.asList(2, 3, 4), Map.of(GameResult.STRIKE, 0L, GameResult.BALL, 3L)),
-                Arguments.of(computerNumber, Arrays.asList(2, 3, 5), Map.of(GameResult.STRIKE, 0L, GameResult.BALL, 2L)),
+                Arguments.of(computerNumber, Arrays.asList(4, 2, 3),
+                        Map.of(GameResult.STRIKE, 3L, GameResult.BALL, 0L)),
+                Arguments.of(computerNumber, Arrays.asList(1, 2, 3),
+                        Map.of(GameResult.STRIKE, 2L, GameResult.BALL, 0L)),
+                Arguments.of(computerNumber, Arrays.asList(4, 3, 2),
+                        Map.of(GameResult.STRIKE, 1L, GameResult.BALL, 2L)),
+                Arguments.of(computerNumber, Arrays.asList(3, 2, 5),
+                        Map.of(GameResult.STRIKE, 1L, GameResult.BALL, 1L)),
+                Arguments.of(computerNumber, Arrays.asList(4, 5, 8),
+                        Map.of(GameResult.STRIKE, 1L, GameResult.BALL, 0L)),
+                Arguments.of(computerNumber, Arrays.asList(2, 3, 4),
+                        Map.of(GameResult.STRIKE, 0L, GameResult.BALL, 3L)),
+                Arguments.of(computerNumber, Arrays.asList(2, 3, 5),
+                        Map.of(GameResult.STRIKE, 0L, GameResult.BALL, 2L)),
                 Arguments.of(computerNumber, Arrays.asList(2, 5, 6), Map.of(GameResult.STRIKE, 0L, GameResult.BALL, 1L))
         );
     }
