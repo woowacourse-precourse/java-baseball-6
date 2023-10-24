9c5ad7c29b38877a1ccca774908ae0d99d03a5ef
jaepyo99 <jaepyo99@naver.com>
2023-10-24 21:40:07 +0900

    [Feat] 스트라이크, 볼 스코어 객체 생성

---
+++ src/main/java/baseball/Score.java
@@ -0,0 +1,19 @@
+package baseball;
+
+public class Score {
+    int strike;
+    int ball;
+
+    public Score(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+}
