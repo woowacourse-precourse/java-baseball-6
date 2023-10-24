ac4a9d2a7132df7b59ac4e68178722ababa92322
minsu20 <syt07100@gmail.com>
2023-10-24 21:34:36 +0900

    feat: 게임을 종료하는 숫자를 입력받을때까지 반복하기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
 
+import baseball.controller.GameController;
+import baseball.util.Converter;
+import baseball.util.RandomGenerator;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameController controller;
+
+        do {
+            controller = new GameController(RandomGenerator.getRandomNumbers(3,1,9),3);
+            controller.startGame();
+        } while (controller.restartOrExit());
     }
 }
