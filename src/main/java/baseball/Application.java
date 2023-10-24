38f4ea4e8e137957a4b23c401e0501d62cf38561
saera <yook.saera@gmail.com>
2023-10-25 06:11:18 +0900

    feat: Application이 게임을 한 판만 진행하라

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        Computer computer = new Computer();
+
+        while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
+            Player player = new Player(Console.readLine());
+            System.out.println(computer.compare(player));
+
+            if ("3스트라이크".equals(computer.compare(player))) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            }
+        }
     }
 }
