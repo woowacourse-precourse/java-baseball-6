66a99568cdb4f0275f3a5164d1fe66bb42c23620
Geon <quso12358@naver.com>
2023-10-24 21:18:37 +0900

    :sparkles: Feat: Add Create Random Number Feature

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,10 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     private static final String WELCOME_MSG = "숫자 야구 게임을 시작합니다.";
     private static final String INPUT_MSG = "숫자를 입력해주세요 : ";
@@ -9,9 +14,17 @@ public class Application {
     private static final String END_MSG = "3스트라이크\n" +
             "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String CONTINUE_MSG = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    private static final int START_NUM = 1;
+    private static final int END_NUM = 9;
+    private static final int COUNT = 3;
     public static void main(String[] args) {
         // 게임 시작
         println(WELCOME_MSG);
+
+        // 랜덤 3자리 숫자 생성
+        List<Integer> randNum = Randoms.pickUniqueNumbersInRange(START_NUM, END_NUM, COUNT);
+
     }
 
     private static void println(String msg){
