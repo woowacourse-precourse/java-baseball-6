274d55820f5d34399842ca94310a73248024ebf6
voyager003 <miraclex000@gmail.com>
2023-10-24 23:55:13 +0900

    Feat: BaseballRule 클래스 추가
    GameRule의 구현체로 숫자를 비교하여 게임의 상태(GameStatus)를 반환한다.

---
+++ src/main/java/baseball/BaseballRule.java
@@ -0,0 +1,35 @@
+package baseball;
+
+import java.util.List;
+
+public class BaseballRule implements GameRule {
+
+    @Override
+    public GameStatus determineGameStatus(List<Integer> botNumbers, List<Integer> playerNumbers) {
+
+        int strikes = 0;
+        int balls = 0;
+
+        for (int i = 0; i < botNumbers.size(); i++) {
+            if (botNumbers.get(i).equals(playerNumbers.get(i))) {
+                strikes++;
+            } else if (botNumbers.contains(playerNumbers.get(i))) {
+                balls++;
+            }
+        }
+
+        if (strikes == 3) {
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return GameStatus.END;
+        }
+
+        if (strikes == 0 && balls == 0) {
+            System.out.println("낫싱");
+            return GameStatus.ONGOING;
+        }
+
+        System.out.println(balls + "볼 " + strikes + "스트라이크");
+        return GameStatus.ONGOING;
+    }
+}
