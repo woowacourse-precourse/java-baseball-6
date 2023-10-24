09278e5094f5b1219f86c978ee93b3a1e67c2e80
mainfn <_@mainfn.dev>
2023-10-24 15:17:32 +0900

    feat: Configuration으로 객체 초기화 및 의존 주입 로직 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,24 +1,12 @@
 package baseball;
 
+import baseball.config.Configuration;
 import baseball.controller.GameController;
-import baseball.domain.ball.AnswerCreator;
-import baseball.domain.game.Computer;
-import baseball.util.NumberPicker;
-import baseball.util.RandomNumberPicker;
-import baseball.view.InputView;
-import baseball.view.OutputView;
 
 public final class Application {
 
     public static void main(final String[] args) {
-
-        final InputView inputView = new InputView();
-        final OutputView outputView = new OutputView();
-        final NumberPicker numberPicker = new RandomNumberPicker();
-        final AnswerCreator answerCreator = new AnswerCreator(numberPicker);
-        final Computer computer = new Computer(answerCreator);
-        final GameController gameController = new GameController(inputView, outputView, computer);
-
+        final GameController gameController = Configuration.createUniqueGameController();
         gameController.play();
     }
 }
---
+++ src/main/java/baseball/config/Configuration.java
@@ -0,0 +1,35 @@
+package baseball.config;
+
+import baseball.controller.GameController;
+import baseball.domain.ball.AnswerCreator;
+import baseball.domain.game.Computer;
+import baseball.parser.InputParser;
+import baseball.validator.InputValidator;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+import number.RandomNumberPicker;
+
+public final class Configuration {
+
+    private static boolean isInitialized = false;
+
+    public static GameController createUniqueGameController() {
+        ensureIsNotInitialized();
+
+        final AnswerCreator answerCreator = new AnswerCreator(new RandomNumberPicker());
+        final InputParser inputParser = new InputParser(new InputValidator());
+
+        return new GameController(
+                new InputView(inputParser),
+                new OutputView(),
+                new Computer(answerCreator)
+        );
+    }
+
+    private static void ensureIsNotInitialized() {
+        if (isInitialized) {
+            throw new IllegalStateException("이미 Application이 초기화 되었습니다.");
+        }
+        isInitialized = true;
+    }
+}
