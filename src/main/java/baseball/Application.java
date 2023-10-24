36d2983490005873cfe85afc72e3ef0e0ed63314
Beomsu <gobeumsu@gmail.com>
2023-10-24 23:59:39 +0900

    feat(Application): Add methods for game continuation prompt

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,15 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    private static boolean askContinue() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        String userInput = readLine();
+        Utils.validateRestartOrExitInput(userInput);
+        return isContinue(userInput);
+    }
+
+    private static boolean isContinue(String userInput) {
+        return !userInput.equals("2");
     }
 }
