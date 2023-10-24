1aeb7d512921b369f4bd6c098ce054f08c0164f0
SeongHun <tjdgns5506@gmail.com>
2023-10-24 14:03:54 +0900

    다듬기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,35 +13,34 @@ public class Application {
 
         Set<Integer> computerSet = new HashSet<>();
 
-        //computer의 랜덤 숫자 완성
+        //computer의 랜덤 숫자 생성 후 List와 Set에 add 함.
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
-                computerSet.add(randomNumber);//내가 추가
+                computerSet.add(randomNumber);
             }
         }
+
         while(true){
 
             //user의 숫자를 입력 받는다.
-
             System.out.print("숫자를 입력해주세요 : ");
             String n = Console.readLine();
 
 
-
-            //길이가 3이 아닌경우
+            //숫자 길이가 3이 아닌경우
             if(n.length()!=3){
                 throw new IllegalArgumentException();
             }
 
-            //중복인 경우
+            //중복 숫자가 입력된 경우
             if(n.charAt(0)==n.charAt(1)||n.charAt(0)==n.charAt(2)||n.charAt(1)==n.charAt(2)){
                 throw new IllegalArgumentException();
             }
 
-            //숫자가 아닌게 입력되는 경우
+            //숫자가 아닌 문자를 입력하는 경우
             Integer[] nums = {1,2,3,4,5,6,7,8,9};
             Set<Integer> numberSet = new HashSet<>(Arrays.asList(nums));
             for(int i=0;i<3;i++){
@@ -56,24 +55,26 @@ public class Application {
                 user.add(Character.getNumericValue(n.charAt(i)));
             }
 
-            //스트라이트 판단
+            //스트라이트 개수를 카운팅 한다.
             int strikeCnt = 0;
             for(int i=0; i<3 ; i++){
                 //if(computer.get(i) == user.get(i)) strikeCnt++;
                 if(computer.get(i).equals(user.get(i))) strikeCnt++;
             }
 
-            //ball
-            int ballCnt=0;
+            //컴퓨터의 숫자와 유저의 숫자가 몇개가 같은지 카운팅한다.
+            int sameNumCnt=0;
             for(int i=0; i<3 ; i++){
-                if(computerSet.contains(user.get(i))) ballCnt++;
+                if(computerSet.contains(user.get(i))) sameNumCnt++;
             }
 
-            if(strikeCnt == 0 && ballCnt == 0) System.out.println("낫싱");
-            else if(strikeCnt == 0) System.out.printf("%d볼%n",ballCnt-strikeCnt);
-            else if(ballCnt-strikeCnt == 0) System.out.printf("%d스트라이크%n",strikeCnt);
-            else System.out.printf("%d볼 %d스트라이크%n",ballCnt-strikeCnt,strikeCnt);
+            //스크라이크, 볼, 낫싱을 판단 후 출력한다.
+            if(strikeCnt == 0 && sameNumCnt == 0) System.out.println("낫싱");
+            else if(strikeCnt == 0) System.out.printf("%d볼%n",sameNumCnt-strikeCnt);
+            else if(sameNumCnt-strikeCnt == 0) System.out.printf("%d스트라이크%n",strikeCnt);
+            else System.out.printf("%d볼 %d스트라이크%n",sameNumCnt-strikeCnt,strikeCnt);
 
+            //스트라이크가 3개 인것 판단
             if(strikeCnt==3) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 break;
@@ -103,6 +104,9 @@ public class Application {
             }
 
         }
+
+
+
 
     }
 
