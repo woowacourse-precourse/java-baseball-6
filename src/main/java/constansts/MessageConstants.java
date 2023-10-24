68b022f3ae3aaa78bfa1517fd7800e903f56d4a6
34suuuuu <psh7492@gmail.com>
2023-10-24 09:52:08 +0900

    refactor: 에러Constants 메시지 추가

--- src/main/java/constansts/MessageConstants.java
+++ src/main/java/constansts/MessageConstants.java
@@ -6,4 +6,10 @@ public class MessageConstants {
     public static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
     public static final String GAME_FINISH_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     public static final String RESTART_OR_FINISH_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public static final String ERROR_MESSAGE = "Error: ";
+    public static final String INPUT_SAME_NUMBER_ERROR_MESSAGE = "중복된 숫자입니다.";
+    public static final String INPUT_ONLY_INTEGER_ERROR_MESSAGE = "숫자만 입력하세요.";
+    public static final String INPUT_LENGTH_ERROR_MESSAGE = "3자리의 숫자만 입력하세요.";
+    public static final String OPTION_INPUT_ERROR_MESSAGE = "1 (재시작) 혹은 2 (종료) 만 입력하세요.";
 }
