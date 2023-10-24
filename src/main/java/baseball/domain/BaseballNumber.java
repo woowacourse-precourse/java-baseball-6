aa9b5526f52e259ba27a231ba075d2b5cbce7af7
Woo-Shinyoung <tlsdud8205@naver.com>
2023-10-24 18:43:49 +0900

    Feat[domain] 야구 게임 숫자 도메인 형식 검증

--- src/main/java/baseball/domain/BaseballNumber.java
+++ src/main/java/baseball/domain/BaseballNumber.java
@@ -1,13 +1,21 @@
 package baseball.domain;
 
+import baseball.validation.BaseballNumberValidator;
+
 public class BaseballNumber {
     private int first;
     private int second;
     private int third;
 
     public BaseballNumber(int first, int second, int third) {
+        validateNumber(first, second, third);
+
         this.first = first;
         this.second = second;
         this.third = third;
+    }
+
+    private void validateNumber(int first, int second, int third) {
+        BaseballNumberValidator.validate(first, second, third);
     }
 }
