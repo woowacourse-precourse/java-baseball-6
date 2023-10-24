ad222ab8c587577e44af3e912a944b9e1925b065
chae401 <chae401@sju.ac.kr>
2023-10-24 15:08:24 +0900

    feature description

---
+++ src/main/java/baseball/ResultDTO.java
@@ -0,0 +1,15 @@
+package baseball;
+
+public class Result {
+    private int strike;
+    private int ball;
+
+    public Result(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+
+    public int getStrike() {
+        return this.strike;
+    }
+}
