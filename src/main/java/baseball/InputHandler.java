ed9218a5922419637f04597071b0f136202ff527
swhyeon98 <smreo75@gmail.com>
2023-10-25 00:26:32 +0900

    feat: 플레이어로부터 받은 입력값의 숫자 사이즈를 검증하는 기능 추가

--- src/main/java/baseball/InputHandler.java
+++ src/main/java/baseball/InputHandler.java
@@ -6,18 +6,26 @@ import java.util.regex.Pattern;
 
 public class InputHandler {
 
+    private static final int MAX_NUMBER_SIZE = 3;
     private static final Pattern NUMBER_PATTERN = Pattern.compile("^[1-9]+$");
 
     public String getPlayInput() {
         System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
         validateIsNumber(input);
+        validateNumberSize(input);
         return input;
     }
 
     private void validateIsNumber(String input) {
         if (!NUMBER_PATTERN.matcher(input).matches()) {
             throw new IllegalArgumentException("숫자만 입력해주세요.");
+        }
+    }
+
+    private static void validateNumberSize(String input) {
+        if (input.length() != MAX_NUMBER_SIZE) {
+            throw new IllegalArgumentException(MAX_NUMBER_SIZE + "자리의 수를 입력해주세요.");
         }
     }
 }
