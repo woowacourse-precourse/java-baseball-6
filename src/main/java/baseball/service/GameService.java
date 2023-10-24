e58d305021efa9af2c784bc0662ebe3c8c9dde5e
ghdeo <goldencorn7@naver.com>
2023-10-25 01:12:14 +0900

    feat: Add playGame logic

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -9,7 +9,16 @@ public class GameService {
     private List<Integer> computer;
 
     public void playGame() {
+        do {
+            System.out.print("숫자 야구 게임을 시작합니다.\n");
+            this.computer = getComputerNumber();
 
+            while (true) {
+                if (isUserNumberCorrect(getUserNumber())) {
+                    break;
+                }
+            }
+        } while (playAgain());
     }
 
     List<Integer> getComputerNumber() {
