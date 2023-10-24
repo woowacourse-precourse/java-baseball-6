590a71f8c4454861e9915b500f1329f47bffa6ed
soyoungxxx <otherplet@gmail.com>
2023-10-24 17:24:32 +0900

    style(Application): 주석 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,7 +4,6 @@ import baseball.game.GameControl;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         GameControl gamecontoller = new GameControl();
         gamecontoller.startGame();
     }
