a42780bb859e0555ee6520784e76e9097c26ad79
silverwest8 <silverwest8@gmail.com>
2023-10-24 21:57:35 +0900

    User input validation check

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -8,11 +8,7 @@ import java.util.HashSet;
 public class User {
     ArrayList<Integer> numbers = new ArrayList<>();
 
-    public void selectNumber() {
-        this.numbers.clear();
-        String input = Console.readLine();
-
-        // 예외 처리
+    private static void validate(String input) {
         // 3자리 확인
         if (input.length() != 3) {
             throw new IllegalArgumentException("3자리 숫자가 아닙니다.");
@@ -26,16 +22,30 @@ public class User {
             }
         }
 
+        ArrayList<Integer> temp = new ArrayList<>();
         for (int i = 0; i < input.length(); i++) {
-            if (!this.numbers.contains((int) input.charAt(i) - '0')) {
-                this.numbers.add((int) input.charAt(i) - '0');
+            if (!temp.contains((int) input.charAt(i) - '0')) {
+                temp.add((int) input.charAt(i) - '0');
             }
         }
 
         // 중복 여부 확인
-        HashSet<Integer> numSet = new HashSet<>(this.numbers);
-        if(numSet.size() != this.numbers.size()){
+        HashSet<Integer> numSet = new HashSet<>(temp);
+        if(numSet.size() != temp.size()){
             throw new IllegalArgumentException("숫자는 중복될 수 없습니다.");
+        }
+    }
+
+    public void selectNumber() {
+        this.numbers.clear();
+        String input = Console.readLine();
+
+        validate(input);
+
+        for (int i = 0; i < input.length(); i++) {
+            if (!this.numbers.contains((int) input.charAt(i) - '0')) {
+                this.numbers.add((int) input.charAt(i) - '0');
+            }
         }
     }
 }
