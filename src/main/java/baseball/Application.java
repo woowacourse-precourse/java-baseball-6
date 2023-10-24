a626a66dbda217aac58132904db4f9e12d0cd052
bbungjin <leejintwo12@naver.com>
2023-10-24 11:30:52 +0900

    구현 완료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,98 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        String scanner;
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        int num;
+        List<Integer> computer;
+        computer = getBall();
+        System.out.println(computer);
+
+        while(true){
+            int set;
+            System.out.println("숫자를 입력해주세요 : ");
+
+            try{
+                scanner = Console.readLine();
+            } catch (IllegalArgumentException e){
+                break;
+            }
+
+            num = Integer.parseInt(scanner);
+            set = checkBall(computer,num);
+            if(set == 3){
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                String str = Console.readLine();
+                if(str.equals("2")){
+                    break;
+                }
+                else{
+                    computer = getBall();
+                }
+            }
+
+        }
+    }
+
+    // 볼, 스트라이크 갯수 확인
+    public static int checkBall(List computer,int num){
+        int idx[] = new int[3];
+        idx[0] = num/100;
+        idx[1] = (num%100)/10;
+        idx[2] = num%10;
+
+        int ball=0;
+        int strike=0;
+
+        for(int i=0; i<3; i++){
+            if(computer.contains(idx[i])){
+                if(computer.indexOf(idx[i])==i){
+                    strike++;
+                }
+                else{
+                    ball++;
+                }
+            }
+        }
+
+        if((ball != 0) || (strike !=0)){
+            if(strike == 3){
+                System.out.println("3스트라이크");
+            }
+            else if(ball==0){
+                System.out.println(strike+"스트라이크");
+            }
+            else if(strike==0){
+                System.out.println(ball+"볼");
+            }
+            else{
+                System.out.println(ball+"볼 "+strike+"스트라이크");
+            }
+        }
+        else{
+            System.out.println("낫싱");
+        }
+
+        return strike;
+    }
+
+    // 새로운 숫자 만들기
+    public static List getBall(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 }
