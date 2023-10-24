37e3e970d84f3347d3ff5fe6e5b858ccdf622780
zeomzzz <jmlee2705@gmail.com>
2023-10-25 03:22:52 +0900

    feat: Computer에 서로 다른 수로 이루어진 수를 선택하는 기능 구현

---
+++ src/main/java/baseball/models/Computer.java
@@ -0,0 +1,27 @@
+package baseball.models;
+
+import java.util.List;
+
+import baseball.utils.Utils;
+
+public class Computer {
+
+    private String answerNumber;
+
+    public String getAnswerNumber() {
+        return answerNumber;
+    }
+
+    // 1부터 9까지 서로 다른 수로 이루어진 수 선택
+    public void chooseNumber(int length) {
+        String result = "";
+
+        List<Integer> randomNumberList = Utils.generateRandomNumber(length);
+        for (int number : randomNumberList) {
+            result += Integer.toString(number);
+        }
+
+        answerNumber = result;
+    }
+
+}
