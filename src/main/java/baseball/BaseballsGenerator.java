6d4308e659b7d573abe163441604b92ca4131ae3
이정우 <acg6138@naver.com>
2023-10-24 11:25:03 +0900

    update: 메서드 이름 수정

--- src/main/java/baseball/BaseballsGenerator.java
+++ src/main/java/baseball/BaseballsGenerator.java
@@ -1,20 +1,38 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import static baseball.Baseball.MAX_NUMBER;
+import static baseball.Baseball.MIN_NUMBER;
+import static baseball.Baseballs.BASEBALLS_SIZE;
+
 public class BaseballsGenerator {
 
-    public static Baseballs createBaseballs(String ballNumbers) {
+    private static final char ASCII_ZERO_CHAR = '0';
+
+    public static Baseballs createBaseballsByUserInput(String ballNumbers) {
         List<Baseball> baseballs = new ArrayList<>();
         for (int index = 0; index < ballNumbers.length(); index++) {
             try {
-                baseballs.add(new Baseball(Integer.parseInt(String.valueOf(ballNumbers.charAt(index)))));
+                baseballs.add(new Baseball(ballNumbers.charAt(index) - ASCII_ZERO_CHAR));
             } catch (NumberFormatException e) {
                 throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
             }
         }
         return new Baseballs(baseballs);
+    }
+
+    public static Baseballs createBaseballsByRandomNumber() {
+        try {
+            List<Baseball> baseballs = new ArrayList<>();
+            for (int i = 0; i < BASEBALLS_SIZE; i++) baseballs.add(new Baseball(Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER)));
+            return new Baseballs(baseballs);
+        } catch (IllegalArgumentException e) {
+            return createBaseballsByRandomNumber();
+        }
     }
 
 }
