412fba3ea845449d9a74b136e80d30bc138703c1
sleeg00 <slee000220@gmail.com>
2023-10-24 22:04:06 +0900

    Fix: Console.close() 추가

--- src/main/java/baseball/Restart.java
+++ src/main/java/baseball/Restart.java
@@ -13,6 +13,7 @@ public class Restart {
     public boolean choiceRestart() { // 게임 종료시 false 반환하는 함수
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String number = Console.readLine(); // 게임을 이어나갈지 사용자에게 값 전달받기
+        Console.close();
         if (number.equals(EXIT)) {
             System.out.print("게임 종료");
             return false;
