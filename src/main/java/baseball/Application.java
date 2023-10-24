60b4276f99a386a296f0b70ecb2ae3d7208291c3
jacomyou1026 <jacomyou1026@naver.com>
2023-10-24 17:25:33 +0900

    fix:README.md

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,79 +1,77 @@
 package baseball;
+
 import camp.nextstep.edu.missionutils.Randoms;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        List<Integer> computer = getComputerNums();
+        Scanner sc = new Scanner(System.in);
         outer: while (true) {
-            System.out.println("숫자 야구 게임을 시작합니다.");
-            List<Integer> computer = new ArrayList<>();
-            while (computer.size() < 3) {
-                int randomNumber = Randoms.pickNumberInRange(1, 9);
-                if (!computer.contains(randomNumber)) {
-                    computer.add(randomNumber);
-                }
-            }
+            String userNum = sc.nextLine();
+            LinkedList<Integer> user = getUser(userNum);
+            int strikes = 0;
+            int balls = 0;
 
-            inner : while (true) {
-                int myIntNum = 0;
-                try {
-                    myIntNum= Integer.parseInt(br.readLine());
-                } catch (IOException e) {
-                    throw new IllegalArgumentException("값을 잘못입력하였습니다.");
+            strikes = findstrikes(computer, user, strikes);
+            balls = findBall(computer, user, balls);
+            if (strikes == 3) { //s =3
+                System.out.println("3스트라이크");
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                int play = sc.nextInt();
+                if(play ==1){
+                    computer = getComputerNums();
+                    sc.nextLine(); // 개행문자 비우기
+                } else if (play == 2) {
+                    sc.close();
+                    break outer;
+                }else {
+                    throw new IllegalStateException("잘못 입력하였습니다");
                 }
 
-                String myNum = "" + myIntNum;
-
-                if (myNum.length() != 3) {
-                    throw new IllegalArgumentException("세 자리수를 입력하세요");
-                }
-                List<Integer> user = Arrays.stream(myNum.split("")).map(Integer::parseInt).collect(Collectors.toList());
-                int strikes = 0;
-                int balls = 0;
-
-                strikes = findstrikes(computer, user, strikes);
-                balls = findBall(computer, user, balls);
-                int result = result(br, strikes, balls);
-                switch(result){
-                    case 1:
-                        break inner;
-                    case 2:
-                        break outer;
-                }
+            } else {
+                result(strikes, balls);
             }
         }
+
     } //main
 
-    private static int result(BufferedReader br,int strikes, int balls) {
-        if (strikes == 3) { //s =3
-            System.out.println("3스트라이크");
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-
-            int play = 0;
-            try {
-                play = Integer.parseInt(br.readLine());
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+    private static List<Integer> getComputerNums() {
+        ArrayList<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
             }
-
-            switch(play){
-                case 1:
-                    return 1;
-                case 2:
-                    return 2;
-            }
-
         }
+        return computer;
+    }
 
+    private static LinkedList<Integer> getUser(String s_num) {
+        LinkedList<Integer> myNum = new LinkedList<>();
+        if (s_num.length() != 3 || !s_num.matches("[0-9]+")) { //길이 체크 및 숫자 인지
+            throw new IllegalArgumentException("값을 제대로 입력해주세요");
+        }
+        String[] split = s_num.split("");
+        for (int i = 0; i < 3; i++) {
+            int num = Integer.parseInt(split[i]);
+            if (num < 1 || num > 9 || myNum.contains(num)) {  //음수처리 및 서로 다른 문자인지
+                throw new IllegalArgumentException("값을 제대로 입력해주세요");
+            }
+            myNum.add(num);
+        }
+        return myNum;
+    }
+
+    private static void result(int strikes, int balls) {
         if (balls > 0 && strikes > 0) { //둘다
             System.out.println(balls + "볼 " + strikes + "스트라이크");
         } else if (balls > 0 && strikes == 0) {//ball만
@@ -83,7 +81,6 @@ public class Application {
         } else {//아무거도 없어
             System.out.println("낫싱");
         }
-        return 0;
     }
 
     private static int findBall(List<Integer> computer, List<Integer> user, int balls) {
