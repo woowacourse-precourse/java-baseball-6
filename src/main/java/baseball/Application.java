03e6382116f6c039e5c191f06810f36872e668fc
Donghyun Oh <dhgus44@naver.com>
2023-10-25 00:22:02 +0900

    BaseBall

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,7 +3,6 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        RandomNum randomNum = new RandomNum();
-        System.out.println(randomNum.ranDomNumbers());
+        new BaseBallGame().play();
     }
 }
