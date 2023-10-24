11d0441e4449246ad3e637c47a2a966ee00f6057
jihoon98 <tlswlgns1003@sju.ac.kr>
2023-10-24 16:15:32 +0900

    git commit -m "feat:ball과 strike를 계산하는 메서드 작성"

--- src/main/java/baseball/model/GameStatusNumber.java
+++ src/main/java/baseball/model/GameStatusNumber.java
@@ -1,4 +1,25 @@
 package baseball.model;
 
+import java.util.List;
+
 public class GameStatusNumber {
+    public int countStrike(List<Integer> userNumbers, List<Integer> computerNumbers){
+        int strike=0;
+        for(int i=0;i< userNumbers.size();i++){
+            if(userNumbers.get(i).equals(computerNumbers.get(i))){
+                strike++;
+            }
+        }
+        return strike;
+    }
+
+    public int countBall(List<Integer> userNumbers, List<Integer> computerNumbers){
+        int ball=0;
+        for(int i=0;i<userNumbers.size();i++){
+            if (userNumbers.contains(computerNumbers.get(i)) && !userNumbers.get(i).equals(computerNumbers.get(i))) {
+                ball++;
+            }
+        }
+        return ball;
+    }
 }
