2e84f909d35d0c72f060b3ce8dfcdde4d6580a46
JUNChangMin <jsjs1209@skku.edu>
2023-10-24 16:47:43 +0900

    Feat:make the program

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,6 +11,107 @@ public class Application {
     static boolean ansFlag = true;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int restart = 1;
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (restart == 1) {
+            restart = Game();
+        }
+
+    }
+    public static int Game(){
+        GenerateNum();
+        ansFlag = false;
+
+        while (!ansFlag) {
+            ArrayList<Integer> playerNum = InputNum();
+            if (check(playerNum.get(0), playerNum.get(1),playerNum.get(2)) == 3) {
+                ansFlag = true;
+            }
+        }
+
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+
+        return RestartCheck();
+    }
+    public static void GenerateNum(){
+        computer.clear();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+    }
+
+    public static ArrayList<Integer> InputNum() {
+        ArrayList<Integer> playerNum = new ArrayList<>();
+
+        System.out.print("숫자를 입력해주세요 : ");
+        String playerNumString = Console.readLine();
+
+        if (playerNumString.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+
+        try {
+            int temp = Integer.parseInt(playerNumString);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException();
+        }
+
+        for(int i=0; i<3; i++){
+            playerNum.add(Character.getNumericValue(playerNumString.charAt(i)));
+        }
+
+        return playerNum;
+    }
+    public static int RestartCheck(){
+        String playerChooseString = Console.readLine();
+        if (playerChooseString.length() != 1) {
+            throw new IllegalArgumentException();
+        }
+
+        try {
+            int temp = Integer.parseInt(playerChooseString);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException();
+        }
+
+        int playerChoose = Character.getNumericValue(playerChooseString.charAt(0));
+        if (playerChoose < 1 || playerChoose > 2) throw new IllegalArgumentException();
+
+        return playerChoose;
+    }
+
+
+
+    public static int check(int first, int second, int third) {
+        int ball = 0;
+        int strike = 0;
+        if (first == computer.get(0)) strike++;
+        else if (first == computer.get(1) || first == computer.get(2) ) ball++;
+
+        if (second == computer.get(1)) strike++;
+        else if (second == computer.get(0) || second == computer.get(2) ) ball++;
+
+        if (third == computer.get(2)) strike++;
+        else if (third == computer.get(1) || third == computer.get(0) ) ball++;
+
+        if (ball == 0 && strike == 0){
+            System.out.println("낫싱");
+        }
+        else if (strike == 0){
+            System.out.println(ball+"볼");
+        }
+        else if (ball == 0){
+            System.out.println(strike+"스트라이크");
+        }
+        else{
+            System.out.println(ball+"볼 "+strike+"스트라이크");
+        }
+        return strike;
     }
 
 
