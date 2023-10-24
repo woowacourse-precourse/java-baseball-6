caddec9faf5f6c7b281365c07c03aedac853dbdc
gonixxxx <akdntm946@gmail.com>
2023-10-24 23:58:20 +0900

    refactor: 사용자 입력 예외 처리 기능을 클래스로 분리

--- src/main/java/baseball/domain/User.java
+++ src/main/java/baseball/domain/User.java
@@ -3,42 +3,21 @@ package baseball.domain;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
+import baseball.feature.CheckInput;
 
 public class User {
+
+    private final CheckInput checkInput = new CheckInput();
 
     public List<Integer> inputNumber() {
         System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
-        checkInput(input);
+        checkInput.checkInput(input);
         return modifyStringToList(input);
     }
 
     private List<Integer> modifyStringToList(String input) {
         return Arrays.stream(input.split("")).map(Integer::parseInt).toList();
-    }
-
-    public void checkInput(String input) {
-        isNotNumber(input);
-        isNotFitLength(input);
-        isNotDuplicated(input);
-    }
-
-    public void isNotNumber(String input) {
-        if (!input.chars().allMatch(Character::isDigit)) {
-            throw new IllegalArgumentException("1부터 9까지의 수를 입력하세요");
-        }
-    }
-
-    public void isNotFitLength(String input) {
-        if (input.length() != 3) {
-            throw new IllegalArgumentException("3자리의 수를 입력하세요");
-        }
-    }
-
-    public void isNotDuplicated(String input) {
-        if (input.chars().distinct().count() != 3) {
-            throw new IllegalArgumentException("중복된 수가 존재합니다");
-        }
     }
 
 }
