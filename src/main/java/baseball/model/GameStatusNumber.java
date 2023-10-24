c0359273cb8ce77910b2c881f73473809444b157
jihoon98 <tlswlgns1003@sju.ac.kr>
2023-10-24 21:24:51 +0900

    "refactor:낫싱 메서드 추가"

--- src/main/java/baseball/model/GameStatusNumber.java
+++ src/main/java/baseball/model/GameStatusNumber.java
@@ -22,4 +22,8 @@ public class GameStatusNumber {
         }
         return ball;
     }
+    public int countNothing(List<Integer> userNumbers, List<Integer> computerNumbers){
+        int nothing = userNumbers.size() - countStrike(userNumbers, computerNumbers) - countBall(userNumbers, computerNumbers);
+        return nothing;
+    }
 }
