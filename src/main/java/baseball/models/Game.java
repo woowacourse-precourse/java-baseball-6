cad0af9afc376a2bb15857e65c830d91e49be5bf
zeomzzz <jmlee2705@gmail.com>
2023-10-25 03:52:14 +0900

    feat: Game에 사용자에게 숫자 입력을 요청하는 기능 구현

--- src/main/java/baseball/models/Game.java
+++ src/main/java/baseball/models/Game.java
@@ -1,5 +1,7 @@
 package baseball.models;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Game {
 
     private boolean status; // 게임 진행 여부
@@ -20,6 +22,16 @@ public class Game {
             this.status = false;
             System.out.println("숫자 야구 게임을 종료합니다.");
         }
+    }
+
+    // 사용자에게 숫자 입력을 요청하는 메소드
+    public String askForGuess() {
+        String result = "";
+
+        System.out.print("숫자를 입력해주세요 : ");
+        result = Console.readLine();
+
+        return result;
     }
 
 }
