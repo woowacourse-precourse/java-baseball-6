c1a04040c6f3791b7dbe42744aec2f9ed7d00c8f
jifrozen0110 <answldjs1836@gmail.com>
2023-10-24 17:46:33 +0900

    test : NumberValidatorTest

---
+++ src/test/java/baseball/utils/NumberValidatorTest.java
@@ -0,0 +1,33 @@
+package baseball.utils;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import baseball.domain.BaseballMessageConst;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class NumberValidatorTest {
+
+    @Test
+    void 사이즈가_3이_아닌_경우_IllegalArgumentException() {
+        List<Integer> numbers = Arrays.asList(1, 2);
+        assertThrows(IllegalArgumentException.class, () -> NumberValidator.isValid(numbers),
+                BaseballMessageConst.INVALID_BALL_COUNT_ERROR);
+    }
+
+    @Test
+    void 중복된_숫자가_있는_경우_IllegalArgumentException() {
+        List<Integer> numbers = Arrays.asList(1, 2, 2);
+        assertThrows(IllegalArgumentException.class, () -> NumberValidator.isValid(numbers),
+                BaseballMessageConst.EXIST_BALL_ERROR);
+    }
+
+    @Test
+    void 정상적으로_수행되는_경우() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3);
+        assertDoesNotThrow(() -> NumberValidator.isValid(numbers));
+    }
+
+}
