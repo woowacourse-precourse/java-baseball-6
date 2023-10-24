eaea7ab5ebe65895e3cfeca9c30e4ff29a98e583
hiidy <hiidy115@gmail.com>
2023-10-24 16:31:15 +0900

    :bug: fix: 스트라이크가 늘어나면 ball도 같이 증가하는 버그 수정. 스트라이크인 경우에 볼의 개수도 늘어나느 버그가 생겨서 index가 같고 같은 값인 경우에는 볼의 개수를 늘어나지 않게함.

--- src/main/java/baseball/Rule.java
+++ src/main/java/baseball/Rule.java
@@ -5,8 +5,11 @@ import java.util.ArrayList;
 public class Rule {
     public static int countBalls(ArrayList<Integer> answerNumber, ArrayList<Integer> userNumber) {
         int count = 0;
-        for (Integer num : userNumber) {
-            if (answerNumber.contains(num)) {
+        for (int i = 0; i < answerNumber.size(); i++) {
+            if (answerNumber.get(i).equals(userNumber.get(i))) {
+                continue;
+            }
+            if (answerNumber.contains(userNumber.get(i))) {
                 count++;
             }
         }
