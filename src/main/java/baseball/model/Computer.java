621989c52be5b656ee33ed9a3e09fbdeacb7ee4d
HongChaa <jun761_1@naver.com>
2023-10-24 13:34:04 +0900

    feat(Computer): 컴퓨터 랜덤 숫자 리스트 생성 메서드 추가

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -1,17 +1,34 @@
 package baseball.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
 
-    private List<Integer> computerRandomNumber;
+    private List<Integer> randomNumberList;
 
     public List<Integer> getComputerRandomNumber() {
-        return computerRandomNumber;
+        randomNumberList = new ArrayList<>();
+
+        return generateRandomNumber();
     }
 
-    public void setComputerRandomNumber(List<Integer> computerRandomNumber) {
-        this.computerRandomNumber = computerRandomNumber;
+    private List<Integer> generateRandomNumber() {
+        int newRandomNumber;
+
+        while(randomNumberList.size() < 3) {
+
+            newRandomNumber = Randoms.pickNumberInRange(1, 9);
+
+            if (!randomNumberList.contains(newRandomNumber)) {
+                randomNumberList.add(newRandomNumber);
+            }
+
+        }
+
+        return randomNumberList;
     }
 
 }
