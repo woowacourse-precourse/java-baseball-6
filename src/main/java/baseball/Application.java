fafc92fb39d083620423dfb0c34fa5111861d92c
wonjun <wonjon0930@daum.net>
2023-10-24 20:28:06 +0900

    feat: all features are completely made

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,6 +3,6 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
        Game game = new Game();
-       // game.init(); // Test 중이라 실행 안됨. ( 무한 루프 )
+       game.init();
     }
 }
