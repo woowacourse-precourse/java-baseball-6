09421bdffd36d2f708779b126bd860cfe7f4ef5a
gonixxxx <akdntm946@gmail.com>
2023-10-25 00:06:07 +0900

    style: 게임오버 클래스에서 게임종료 문구가 나오도록 수정

--- src/main/java/baseball/feature/GameOver.java
+++ src/main/java/baseball/feature/GameOver.java
@@ -5,6 +5,8 @@ import camp.nextstep.edu.missionutils.Console;
 public class GameOver {
 
     public boolean isGameOver() {
+        System.out.println(
+                "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String command = Console.readLine();
         return command.equals("1");
     }
