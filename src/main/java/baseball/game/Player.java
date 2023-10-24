9bdf09314a769f3e6c8d041a883613e4edf05fcf
dlawotn3 <imjisu@imjisuui-MacBookPro.local>
2023-10-24 22:43:12 +0900

    feat: player가 입력한 숫자에 대해 검증하는 메서드 구현

--- src/main/java/baseball/game/Player.java
+++ src/main/java/baseball/game/Player.java
@@ -8,8 +8,25 @@ public class Player {
         System.out.println("숫자를 입력해주세요 : ");
         String answer = Console.readLine();
 
-        //TODO: 입력한 숫자 검증 로직 필요
-
+        if (!isValidNum(answer)) {
+            throw new IllegalArgumentException("입력 받은 숫자가 유효하지 않습니다.");
+        }
         return answer;
+    }
+
+    private boolean isValidNum(String str) {
+        return hasValidLength(str) && hasNoDuplicateChars(str);
+    }
+
+    private boolean hasValidLength(String str) {
+        return str.length() == 3;
+    }
+
+    private boolean hasNoDuplicateChars(String str) {
+        char first = str.charAt(0);
+        char second = str.charAt(1);
+        char third = str.charAt(2);
+
+        return first != second && second != third && first != third;
     }
 }
