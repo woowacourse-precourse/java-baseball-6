04deb5daf7a9d92f93627487d8d69c623d6265cf
Kim Taeyeong <tyeong1102@gmail.com>
2023-10-24 22:26:15 +0900

    feat: Number 구현

---
+++ src/main/java/baseball/constant/Number.java
@@ -0,0 +1,16 @@
+package baseball.constant;
+
+public class Number {
+
+    public static final Integer RESTART_NUM = 1;
+
+    public static final Integer END_NUM = 2;
+
+    public static final Integer LENGTH = 3;
+
+    public static final Integer NUM_FIRST = 1;
+
+    public static final Integer NUM_LAST = 9;
+
+    public static final Integer STRIKE_OUT = 3;
+}
