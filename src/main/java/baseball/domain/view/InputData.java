0c4c5c1804d2cac32fe46a7137676762ad0d4b3d
peeaaccee <wlgp8958@naver.com>
2023-10-25 08:01:56 +0900

    feat(InputData): 유저가 볼 숫자를 입력하게 하는 기능

--- src/main/java/baseball/domain/view/InputData.java
+++ src/main/java/baseball/domain/view/InputData.java
@@ -1,9 +1,16 @@
 package baseball.domain.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputData {
     private static final String NUMBER_OF_USER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     private InputData() {
+    }
+
+    public static String inputUserBallNumber() {
+        System.out.print(NUMBER_OF_USER_INPUT_MESSAGE);
+        return Console.readLine();
     }
 }
