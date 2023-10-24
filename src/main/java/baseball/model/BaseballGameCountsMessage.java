13b5640ea5e63791b10ccbcc2ee12b6d8b03b409
oxdjww <oxdjww@gmail.com>
2023-10-25 02:48:44 +0900

    refactor : editMessage() 기능을 생성자에 구현

--- src/main/java/baseball/model/BaseballGameCountsMessage.java
+++ src/main/java/baseball/model/BaseballGameCountsMessage.java
@@ -13,23 +13,18 @@ public class BaseballGameCountsMessage {
 
         // 스트라이크 카운트가 존재하면 메시지에 덧붙임
         if (strikeCounts > 0) {
-            editMessage(String.format("%d스트라이크", strikeCounts));
+            this.message = String.format("%d스트라이크", strikeCounts);
         }
 
         // 볼 카운트가 존재하면 메시지에 덧붙임
         if (ballCounts > 0) {
-            editMessage(String.format("%d볼 ", ballCounts));
+            this.message = String.format("%d볼 ", ballCounts) + this.message;
         }
 
         // 스트라이크, 볼이 아무것도 없을 경우 '낫싱'으로 초기화
         if (strikeCounts == 0 && ballCounts == 0) {
-            editMessage("낫싱");
+            this.message = "낫싱";
         }
-    }
-
-    // 조건에 따라 메시지를 수정해주는 함수
-    public void editMessage(String frag) {
-        this.message = frag + this.message;
     }
 
     // 메시지를 출력하는 함수
