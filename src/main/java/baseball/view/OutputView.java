d513088dfbafcea9656507a88afb8d60368400e7
CJY <c0512jy@naver.com>
2023-10-25 01:01:34 +0900

    refactor: 게임 시작 출력 추가
    InputView start()함수는 OutputView의 역할인 것 같아 수정했습니다.

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,6 +1,11 @@
 package baseball.view;
 
 public class OutputView {
+
+    public void start() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
     public void printResult(String result) {
         System.out.println(result);
     }
