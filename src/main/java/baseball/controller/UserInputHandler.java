56ca4eaac85be4838fef92cfed9273cdb70a7d40
jihoon98 <tlswlgns1003@sju.ac.kr>
2023-10-25 00:19:48 +0900

    "refactor:중복 숫자 입력 방지"

--- src/main/java/baseball/controller/UserInputHandler.java
+++ src/main/java/baseball/controller/UserInputHandler.java
@@ -9,12 +9,41 @@ import java.util.List;
 
 public class UserInputHandler {
     EndView endView=new EndView();
+    private List<List<Integer>> duplicatedNum=new ArrayList<>();
+
     public UserNumber getUserInputNumber(){//사용자로부터 숫자 입력
         System.out.print("숫자를 입력해주세요 : ");
         String userInput= Console.readLine();
         List<Integer> userNumber=changeUserInputNumber(userInput);
+        if(!containNumber(duplicatedNum,userNumber)&&userNumber.size()==3){
+            duplicatedNum.add(userNumber);
+            return new UserNumber(userNumber);
+        }
+        else if(containNumber(duplicatedNum,userNumber)){
+            System.out.println("중복된 숫자입니다. 다시 입력해주세요");
+        }
+        else{
+            System.out.println("3자리 자연수를 입력하세요");
+        }
         return new UserNumber(userNumber);
     }
+    public boolean containNumber(List<List<Integer>> numberList,List<Integer> target){
+        for(int i=0;i<numberList.size();i++){
+            List<Integer> number=numberList.get(i);
+            boolean isEqual=true;
+            for(int j=0;j<number.size();j++){
+                if(!number.get(j).equals(target.get(i))){
+                    isEqual=false;
+                    break;
+                }
+            }
+            if(isEqual){
+                return false;
+            }
+        }
+        return false;
+    }
+
     public List<Integer> changeUserInputNumber(String userInputNumber){
         List<Integer> inputNumber=new ArrayList<>();
         for(int i=0;i<userInputNumber.length();i++){
@@ -38,10 +67,15 @@ public class UserInputHandler {
         endView.displayRestartMessage();
         while(true){
             int restartInput=Integer.parseInt(Console.readLine());
-            if(restartInput!=1&&restartInput!=2){
-                throw new IllegalArgumentException();
+            if(restartInput==1){
+                return true;
             }
-            return restartInput==1;
+            else if(restartInput==2){
+                return false;
+            }
+            else{
+                System.out.println("숫자를 다시 입력해주세요.");
+            }
         }
     }
     public int getExitInput(){//다시 시작할지 입력
