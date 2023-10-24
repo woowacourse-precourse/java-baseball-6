ebb50a0bf5933a738c5ef1d200353c7ed763a41f
YongHyeon <pyh4616@naver.com>
2023-10-24 19:07:57 +0900

    Refactor: GameController, 3개인 인스턴스 변수를 줄이기 위해 InputView와 OutputView를 메서드의 인자로 전달

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -11,23 +11,21 @@ import java.util.List;
 public class GameController {
 
     private GameService gameService;
-    private InputView inputView;
-    private OutputView outputView;
 
     {
         gameService = new GameService();
-        inputView = new InputView();
-        outputView = new OutputView();
     }
 
     public void playGame() {
+        OutputView outputView = new OutputView();
+        InputView inputView = new InputView();
         int restartOrExitNumber = 1;
         while (restartOrExitNumber != 2) {
             outputView.printGameStartMessage();
             List<Integer> computerNumber = getComputerNumber();
-            playOneGame(computerNumber);
-            printGameOverAndContinueMessage();
-            restartOrExitNumber = getRestartOrExitNumber();
+            playOneGame(computerNumber, outputView, inputView);
+            printGameOverAndContinueMessage(outputView);
+            restartOrExitNumber = getRestartOrExitNumber(inputView);
         }
     }
 
@@ -37,28 +35,28 @@ public class GameController {
         return computerNumber;
     }
 
-    private void playOneGame(List<Integer> computerNumber) {
+    private void playOneGame(List<Integer> computerNumber, OutputView outputView, InputView inputView) {
         String compareResult = "";
         while (!compareResult.equals("3스트라이크")) {
             outputView.printInputNumberMessage();
-            List<Integer> userNumber = getUserNumber();
+            List<Integer> userNumber = getUserNumber(inputView);
             compareResult = gameService.getCompareResult(computerNumber, userNumber);
             outputView.printCompareResult(compareResult);
         }
     }
 
-    private List<Integer> getUserNumber() {
+    private List<Integer> getUserNumber(InputView inputView) {
         List<Integer> userNumber = inputView.inputUserNumber();
         new UserNumber(userNumber);
         return userNumber;
     }
 
-    private void printGameOverAndContinueMessage() {
+    private void printGameOverAndContinueMessage(OutputView outputView) {
         outputView.printGameOverMessage();
         outputView.printGameContinueMessage();
     }
 
-    private int getRestartOrExitNumber() {
+    private int getRestartOrExitNumber(InputView inputView) {
         int restartOrExitNumber = inputView.inputRestartOrExitNumber();
         new RestartOrExitNumber(restartOrExitNumber);
         return restartOrExitNumber;
