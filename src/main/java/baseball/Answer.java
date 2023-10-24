e85fd550a92f5624b4c11d5c5687f1f38f83f2bf
Elandland <68230815+Elandland@users.noreply.github.com>
2023-10-24 17:45:42 +0900

    Feat : Make random answer , Input num

--- src/main/java/baseball/Answer.java
+++ src/main/java/baseball/Answer.java
@@ -5,13 +5,18 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Answer {
-    //랜덤으로 정답값을 생성
-    List<Integer> computer = new ArrayList<>();
-    while (computer.size() < 3) {
-        int randomNumber = Randoms.pickNumberInRange(1, 9);
-        if (!computer.contains(randomNumber)) {
-            computer.add(randomNumber);
+    public static List<Integer> RandomAns()     //랜덤으로 정답값을 생성 주어진 예시가 List의 형식
+    {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
         }
+        return computer;
     }
 }
--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -2,14 +2,24 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
 import java.util.List;
 
+
 public class Input {
-    public List<Integer> input(){
+    public static List<Integer> inputnum() {
+        //contain함수를 쓰기 위해 입력값도 각 자리수를 List의 원소로 사용하여 List를 만든다.
+
+        List<Integer> numlist = new ArrayList<>();
+
         System.out.printf("숫자를 입력해 주세요 : ");
+        String inputnum = Console.readLine();
 
+        String[] temp = inputnum.split("");
 
-        return null;
+        for(int i =0; i<inputnum.length(); i++){
+            numlist.add(Integer.parseInt(temp[i]));
+        }
+        return numlist;
     }
-
 }
