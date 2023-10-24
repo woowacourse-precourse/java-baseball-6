18c93321b0bdc86f5ac18b1b44088a3ca19bdb6a
wnsgh12s <junho1273@gmail.com>
2023-10-25 02:37:10 +0900

    Feat: Application 초기 설정 및 초기화

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,19 @@
 package baseball;
 
+import baseball.controller.NumberBaseballGameController;
+import baseball.domain.service.NumberBaseBallGame;
+import baseball.domain.service.NumberBaseballScoreEvaluator;
+import baseball.domain.util.RandomNumberGenerator;
+import baseball.view.NumberBaseballGameInputView;
+import baseball.view.NumberBaseballGameOutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        new NumberBaseballGameController(
+                new NumberBaseballGameInputView(),
+                new NumberBaseballGameOutputView(),
+                new NumberBaseBallGame(new RandomNumberGenerator(), new NumberBaseballScoreEvaluator())
+        ).playGame();
     }
 }
