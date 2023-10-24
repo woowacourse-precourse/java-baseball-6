36efba4821bb6c8121e6b591aab81c6b47711a92
minhee <bsmq100@gmail.com>
2023-10-24 21:12:31 +0900

    refactor : switch->if문으로 리팩토링

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -23,8 +23,8 @@ public class Application {
 
                 List<Integer> user = new ArrayList<>();
                 userNumberList(user, Numbers);
-
                 userListValid(user);
+
                 HashMap<Integer, Integer> map = new HashMap();
                 computerUserComparisonList(map, user, computer);
                 int ball = 0;
@@ -38,32 +38,26 @@ public class Application {
                         }
                     }
                 }
-                switch (strike) {
-                    case 3:
-                        System.out.println("3스트라이크\n"
-                                + "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"
-                                +"게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                        String userOpinion = Console.readLine();
-                        if (userOpinion.equals("2")) {
-                            loof = !loof;
-                            break;
-                        }
-                        if (userOpinion.equals("1")) {
-                            break;
-                        }
-
-                    default:
-                        if (ball == 0 && strike == 0) {
-                            System.out.println("낫싱");
-                            break;
-                        }
-                        System.out.println(ball + "볼 " + strike + "스트라이크");
-                }
-                if (strike == 3) {
+                if(strike==3){
+                    System.out.println("3스트라이크\n"
+                            + "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"
+                            +"게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                    String userOpinion = Console.readLine();
+                    if (userOpinion.equals("2")) {
+                        loof = !loof;
+                    }
                     break;
+                }
+
+                if (ball == 0 && strike == 0) {
+                    System.out.println("낫싱");
+                }
+                if(!(ball == 0 && strike == 0)){
+                    System.out.println(ball + "볼 " + strike + "스트라이크");
                 }
             }
         }
+
 
     }
     public static void computerUserComparisonList(HashMap<Integer, Integer> map, List<Integer> user, List<Integer> computer){
