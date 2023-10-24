bf8ca16cf1dd79469d9a797c285f6d199230ac08
anseongjun <dkstjdwns@kookmin.ac.kr>
2023-10-25 01:16:35 +0900

    refactor: output 수정

--- src/main/java/baseball/view/Output.java
+++ src/main/java/baseball/view/Output.java
@@ -1,8 +1,6 @@
 package baseball.view;
 
-import baseball.controller.BaseBallGame;
 import baseball.model.Settings;
-import java.util.List;
 
 public class Output {
 
@@ -10,36 +8,24 @@ public class Output {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    public static void InputMessage() {
+    public static void RequestInputMessage() {
         System.out.print("숫자를 입력해주세요 : ");
     }
 
-    public static void Hint(List<Integer> computer, String userInput) {
-        if (Settings.strike == 3) {
-            System.out.println("3스트라이크");
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            Settings.restart = InputNumber.GetRestartInput();
-            if (!Settings.restart.equals("1") && !Settings.restart.equals("2")) {
-                throw new IllegalArgumentException();
-            }
-            BaseBallGame.correct = true;
-        } else if (Settings.strike == 0 && Settings.ball == 0) {
+    public static void CompleteMessage() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    public static void HintMessage() {
+        if (Settings.strike == 0 && Settings.ball == 0) {
             System.out.println("낫싱");
-            InputMessage();
-            BaseBallGame.correct = false;
-        } else if (Settings.strike == 0) {
-            System.out.println(Settings.ball + "볼");
-            BaseBallGame.correct = false;
-            InputMessage();
-        } else if (Settings.ball == 0) {
-            System.out.println(Settings.strike + "스트라이크");
-            BaseBallGame.correct = false;
-            InputMessage();
         } else if (Settings.strike != 0 && Settings.ball != 0) {
             System.out.println(Settings.ball + "볼 " + Settings.strike + "스트라이크");
-            BaseBallGame.correct = false;
-            InputMessage();
+        } else if (Settings.strike != 0) {
+            System.out.println(Settings.strike + "스트라이크");
+        } else if (Settings.ball != 0) {
+            System.out.println(Settings.ball + "볼");
         }
     }
 }
