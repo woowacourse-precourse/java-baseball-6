7c7d6a048a25d20079a401915b632b727fa75a8d
yoonseo choi <choiyoonseo02@gmail.com>
2023-10-24 20:26:12 +0900

    [추가] 사용자가 정답을 맞히면 게임 종료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,6 +7,23 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
+        gameStart();
+    }
+
+    static void gameStart() {
+        List<Integer> computerNumber = randomNumber();
+
+        int userNumber = 0;
+        String gameHint = Hint.generateHint(userNumber, computerNumber);
+
+        while (!gameHint.equals("3스트라이크")) {
+            userNumber = UserValue.getUserInput();
+            UserValue.validateNumber(userNumber);
+            gameHint = Hint.generateHint(userNumber, computerNumber);
+            System.out.println(gameHint);
+        }
+
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
     static List<Integer> randomNumber() {
