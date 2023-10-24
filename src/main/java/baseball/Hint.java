ce8f336f4e7cdcc1cda167badf9a4f2b576f04ba
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-10-25 04:16:16 +0900

    feat: 모든 힌트를 초기화, 3스트라이크 여부 확인

--- src/main/java/baseball/Hint.java
+++ src/main/java/baseball/Hint.java
@@ -7,7 +7,14 @@ public class Hint {
     private int strikeCount;
     private int ballCount;
 
+    /** compareNumber : 컴퓨터가 선택한 숫자와 사용자가 입력한 숫자 간의 비교
+     *  같은 수가 같은 자리에 있을 경우(스트라이크)
+     *  같은 수가 다른 자리에 있을 경우(볼)
+     *  같은 수가 전혀 없을 경우(낫싱)
+     */
     public void compareNumber(List<Integer> computerNumbers, List<Integer> userNumbers) {
+
+        reset();
 
         for (int i = 0; i < computerNumbers.size(); i++) {
 
@@ -17,12 +24,29 @@ public class Hint {
 
             } else {
 
-                ballCount++;
+                if (computerNumbers.contains(userNumbers.get(i))) {
 
+                    ballCount++;
+
+                }
             }
         }
 
         printHint(strikeCount, ballCount);
+    }
+
+    // 모든 힌트를 초기화
+    public void reset() {
+
+        strikeCount = 0;
+        ballCount = 0;
+
+    }
+
+    // 3스트라이크인지 확인
+    public boolean isThreeStrike() {
+
+        return strikeCount == 3;
     }
 
     // 힌트 출력
@@ -49,6 +73,4 @@ public class Hint {
             }
         }
     }
-
-
 }
