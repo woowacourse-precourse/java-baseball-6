d71500defe7d11d1acc6f21ba052f09fa612605d
jungeun-choi <jungeun5.choi@gmail.com>
2023-10-24 19:20:22 +0900

    refactor: 메서드 명 변경, print~() -> output~()

--- src/main/java/baseball/BaseballView.java
+++ src/main/java/baseball/BaseballView.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 /* input, output 화면(메세지) 출력 */
 public class BaseballView {
     // 게임 시작 메세지
-    public void printGameStart() {
+    public void outputGameStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
     // 유저 숫자 입력 요청 메세지
-    public void printUserNumber() {
+    public void outputUserNumber() {
         System.out.print("숫자를 입력해주세요 : ");
     }
 
@@ -19,21 +19,25 @@ public class BaseballView {
         System.out.printf("%d볼\n", count);
     }
 
-    public void printResultStrike(int count) {
-        System.out.printf("%d스트라이크\n", count);
+    public void outputResultBall(int count) {
+        System.out.println("%d볼", count);
     }
 
-    public void printResultNothing() {
+    public void outputResultStrike(int count) {
+        System.out.println("%d스트라이크", count);
+    }
+
+    public void outputResultNothing() {
         System.out.println("낫싱");
     }
 
     // 게임 종료 메세지
-    public void printFinishGame() {
+    public void outputFinishGame() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
     // 게임 재시작 혹은 종료 선택 입력 요청 메세지
-    public void printSelectRestartOrEnd() {
+    public void outputSelectRestartOrEnd() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
@@ -41,14 +45,14 @@ public class BaseballView {
     // 유저 숫자 입력
     public String inputUserNumber() {
         Scanner sc = new Scanner(System.in);
-        printUserNumber();
+        outputUserNumber();
         return sc.nextLine();
     }
 
     // 게임 재시작 혹은 종료 선택 입력
     public int inputSelectRestartOrEnd() {
         Scanner sc = new Scanner(System.in);
-        printSelectRestartOrEnd();
+        outputSelectRestartOrEnd();
         return sc.nextInt();
     }
 }
