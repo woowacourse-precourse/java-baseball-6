4bf783920d9c745e31ec5970aa1db5afa2bbf0c7
U-keun Song <sukeun319@gmail.com>
2023-10-24 14:50:26 +0900

    Test : Referee 클래스 테스트 코드 수정
    Referee 클래스를 수정하면서 바뀐 메서드를 테스트하도록 로직을 수정하고, 테스트 이름을 변경했다.

--- src/test/java/baseball/people/preparation/RefereeTest.java
+++ src/test/java/baseball/people/preparation/RefereeTest.java
@@ -1,6 +1,6 @@
 package baseball.people.preparation;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,14 +8,14 @@ import org.junit.jupiter.api.Test;
 class RefereeTest {
 
     @Test
-    @DisplayName("judge 메서드 작동 확인 테스트")
-    void judge() {
+    @DisplayName("strikeOut 메서드 작동 확인 테스트")
+    void strikeOut() {
         Referee referee = Referee.enter();
         referee.playBall(3, 5, 7);
 
         int[] input = new int[]{1, 5, 3};
-        int[] judge = referee.judge(input);
+        boolean strikeOut = referee.strikeOut(input);
 
-        assertTrue(judge[0] == 2 && judge[1] == 1);
+        assertFalse(strikeOut);
     }
 }
