15cb9a20397d5845b9c6dd8a3f87a99bbddd3c58
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-10-25 01:31:51 +0900

    refactor: 상수 클래스 생성
    - 코드 가독성 향상 및 유지보수성 향상을 위해서 상수 클래스 생성

---
+++ src/main/java/baseball/utils/Constants.java
@@ -0,0 +1,7 @@
+package baseball.utils;
+
+public final class Constants {
+    public final static int MAX_INPUT_LENGTH = 3;
+    public final static String RESTART_INPUT = "1";
+    public final static String EXIT_INPUT = "2";
+}
