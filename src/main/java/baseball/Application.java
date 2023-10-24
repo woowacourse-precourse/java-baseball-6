5d15d464b746cd5fc9f0d63b94f99ad5f0326c17
Kaeun-Lee <lgeun7026@hanmail.net>
2023-10-25 05:34:24 +0900

    feat: Add basic user interface template

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,12 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.print("숫자를 입력해주세요 : ");
+        System.out.println("숫자는 3자리만 입력할 수 있습니다.");
+        System.out.println(1 + "볼 " + 1 + "스트라이크");
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println("게임을 종료합니다.");
     }
 }
