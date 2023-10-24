9005b4a439fc098a0438f87ac254c4ea4a57b0cc
wonjun <wonjon0930@daum.net>
2023-10-24 18:49:58 +0900

    feat: add feature to get resume-or-not input when game ends

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -12,7 +12,6 @@ public class Input {
         validateBaseballInput(userInput);
         return parseUserInput(userInput);
     }
-
     private void validateBaseballInput(String userInput){
         checkIfInputNumberLengthIsGivenLength(userInput,3);
         checkIfInputNumberIsNotNumberFormat(userInput);
@@ -47,6 +46,26 @@ public class Input {
             throw new IllegalArgumentException("잘못된 사용자 입력입니다. : 중복된 숫자를 입력했습니다.");
         }
     }
+    public boolean getResumeInput(){
+        String userInput = Console.readLine();
+        validateResumeInput(userInput);
+        return parseResumeInput(userInput);
+    }
+
+    private void validateResumeInput(String userInput){
+        checkIfInputNumberLengthIsGivenLength(userInput,1);
+        checkIfInputNumberIsNotNumberFormat(userInput);
+    }
+    private boolean parseResumeInput(String userInput){
+        if(userInput.equals("1")){
+            return true;
+        }
+        if(userInput.equals("2")){
+            return false;
+        }
+        throw new IllegalArgumentException("잘못된 입력입니다. : 1 또는 2 값이 아닙니다.");
+    }
+
     private List<Integer> parseUserInput(String userInput){
         List<Integer> userInputNumbers = new ArrayList<>();
         String[] userInputArr = userInput.split("");
