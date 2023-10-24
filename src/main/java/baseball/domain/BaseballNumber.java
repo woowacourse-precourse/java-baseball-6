b34bdb30606440872947870146e88dbd7ef89292
Woo-Shinyoung <tlsdud8205@naver.com>
2023-10-24 19:51:24 +0900

    Feat[domain] 야구 게임 숫자 비교 메서드 작성

--- src/main/java/baseball/domain/BaseballNumber.java
+++ src/main/java/baseball/domain/BaseballNumber.java
@@ -18,4 +18,34 @@ public class BaseballNumber {
     private void validateNumber(int first, int second, int third) {
         BaseballNumberValidator.validate(first, second, third);
     }
+
+    public boolean hasNumber(int number) {
+        return first == number || second == number || third == number;
+    }
+
+    public boolean matchesAtPosition(int number, int position) {
+        switch (position) {
+            case 0:
+                return first == number;
+            case 1:
+                return second == number;
+            case 2:
+                return third == number;
+            default:
+                return false;
+        }
+    }
+
+    public int getNumberAtPosition(int position) {
+        switch (position) {
+            case 0:
+                return first;
+            case 1:
+                return second;
+            case 2:
+                return third;
+            default:
+                return -1;
+        }
+    }
 }
