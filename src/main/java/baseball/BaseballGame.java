13e7173cd8d09ab483efc9dc56333956053d0db0
dealim <136411152+dealim@users.noreply.github.com>
2023-10-24 15:48:45 +0900

    [complete]commit for submission

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -44,6 +44,7 @@ public class BaseballGame {
         return computer;
     }
 
+    // 플레이어가 숫자를 입력
     private List<Integer> inputNumber() {
         List<Integer> numbers = new ArrayList<>();
 
@@ -62,14 +63,15 @@ public class BaseballGame {
         return numbers;
     }
 
-    private void judge(List<Integer> computer, List<Integer> player) {
+    // 이번 게임에 정해진 답과 플레이어가 입력한 값을 넣어 결과를 판단
+    private void judge(List<Integer> answer, List<Integer> player) {
         strike = 0;
         ball = 0;
 
-        for (int i = 0; i < computer.size(); i++) {
-            if (computer.get(i).equals(player.get(i))) {
+        for (int i = 0; i < answer.size(); i++) {
+            if (answer.get(i).equals(player.get(i))) {
                 strike++;
-            } else if (player.contains(computer.get(i))) {
+            } else if (player.contains(answer.get(i))) {
                 ball++;
             }
         }
@@ -77,6 +79,7 @@ public class BaseballGame {
         printJudge();
     }
 
+    // 게임 종료 메세지를 띄우고 게임을 종료
     private void end() {
         if (strike == TARGET_STRIKE) {
             System.out.println(NUMBER_LENGTH + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
@@ -84,6 +87,7 @@ public class BaseballGame {
         }
     }
 
+    // 플레이어가 게임을 종료할지 재시작할지 선택
     private void inputEnd() {
         String endString;
 
@@ -103,6 +107,7 @@ public class BaseballGame {
         }
     }
 
+    // 결과값을 출력
     private void printJudge() {
         if (ball == 0 && strike == 0) {
             System.out.println("낫싱");
@@ -111,6 +116,7 @@ public class BaseballGame {
         }
     }
 
+    //볼의 갯수에 따라 출력
     private String ballToString() {
         if (ball == 0) {
             return "";
@@ -119,6 +125,7 @@ public class BaseballGame {
         }
     }
 
+    //스트라이크의 갯수에 따라 출력
     private String strikeToString() {
         if (strike == 0) {
             return "";
