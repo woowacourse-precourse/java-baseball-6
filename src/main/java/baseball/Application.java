b783db21984c5ed059300bdac1442c867e1ad49a
JW0321 <digneotn123@gmail.com>
2023-10-25 01:15:55 +0900

    Refactor: 불필요한 기능 제거
    요구사항에 명시된 출력값 형식을 지켜야 한다고 생각해서 요구사항에 기재되지 않는 추가적인 기능은 제거하였습니다.
    문서도 동일한 부분은 제거하였습니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,7 +7,6 @@ public class Application {
     public static void main(String[] args) throws IllegalArgumentException {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
-        System.out.println("000을 입력하면 게임을 새로 시작하거나 종료할 수 있습니다.");
         while(true) {
             Input computer = new Input();
             computer.setRandomNumber();
@@ -15,9 +14,6 @@ public class Application {
                 Input player = new Input();
                 System.out.print("숫자를 입력해주세요 : ");
                 String input = Console.readLine();
-                if (input.equals("000")) {
-                    break;
-                }
                 player.setPlayerNumber(input);
                 Comparison.compare(computer, player);
             } while (!Comparison.isOver());
