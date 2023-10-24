95bdb9ddf6178ff1346cd0df3334373278462bdc
devhoya97 <diajbh123@gmail.com>
2023-10-24 17:41:19 +0900

    style : BaseballNumbers생성자에 Javadoc 주석 내용 변경 매개변수로 받은 객체의 주소값을 그대로 사용하지 않고, 새로운 객체를 만드는 이유에 대해 자세히 설명하여 독자의 이해를 돕도록 Javadoc의 내용을 변경하였습니다.

--- src/main/java/baseball/model/BaseballNumbers.java
+++ src/main/java/baseball/model/BaseballNumbers.java
@@ -15,6 +15,10 @@ public class BaseballNumbers {
     /**
      * 숫자야구 값이 담긴 리스트에 중복이 있는지, 길이가 적합한지 검사 후 객체를 반환합니다.
      *
+     * <p>매개변수가 가리키는 리스트를 그대로 baseballNumbers가 가리키게 된다면,
+     * 매개변수에 의해 BaseballNumbers 객체가 갖는 리스트의 요소가 변동될 위험성이 있습니다. BaseballNumbers 객체는 한 번 할당받은 BaseballNumber 리스트가 변경되지 않도록
+     * 의도하여 설계되었으므로 새로운 리스트 객체를 생성하여 멤버변수가 가리키도록 합니다.</p>
+     *
      * @param baseballNumbers 숫자야구 값을 담은 리스트
      */
 
