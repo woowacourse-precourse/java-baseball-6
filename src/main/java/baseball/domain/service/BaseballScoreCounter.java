28cb4c0b35572ad8e8c636401346ff9ebefe3063
wnsgh12s <junho1273@gmail.com>
2023-10-25 07:24:45 +0900

    rename: 잘못된 인자 이름 수정

--- src/main/java/baseball/domain/service/BaseballScoreCounter.java
+++ src/main/java/baseball/domain/service/BaseballScoreCounter.java
@@ -4,29 +4,29 @@ import baseball.domain.model.BaseballNumber;
 import java.util.stream.IntStream;
 
 public class BaseballScoreCounter {
-    private final BaseballNumber inputNumber;
+    private final BaseballNumber ComputerNumber;
     private final BaseballNumber targetNumber;
 
-    public BaseballScoreCounter(BaseballNumber inputNumber, BaseballNumber targetNumber) {
-        this.inputNumber = inputNumber;
-        this.targetNumber = targetNumber;
+    public BaseballScoreCounter(BaseballNumber computerNumber, BaseballNumber inputNumber) {
+        this.ComputerNumber = computerNumber;
+        this.targetNumber = inputNumber;
     }
 
     public int countStrikes() {
         return (int) IntStream.range(0, targetNumber.length())
-                .filter(i -> targetNumber.charAt(i) == inputNumber.charAt(i))
+                .filter(i -> targetNumber.charAt(i) == ComputerNumber.charAt(i))
                 .count();
     }
 
     public int countBalls() {
-        return (int) IntStream.range(0, inputNumber.length())
+        return (int) IntStream.range(0, ComputerNumber.length())
                 .filter(this::isBall)
                 .count();
     }
 
     private boolean isBall(int i) {
-        return targetNumber.indexOf(inputNumber.charAt(i)) != -1
-                && targetNumber.charAt(i) != inputNumber.charAt(i);
+        return targetNumber.indexOf(ComputerNumber.charAt(i)) != -1
+                && targetNumber.charAt(i) != ComputerNumber.charAt(i);
     }
 }
 
