a7593a61f0e6b4dbdbb4be44c8e0041b3981bcf9
hiidy <hiidy115@gmail.com>
2023-10-25 00:15:57 +0900

    :recycle: refact: PlayGame의 멤버 변수들의 접근자 변경 ball, strike, answerNumbers의 접근자를 prviate로 바꿔서 캡슐화를 강화함.

--- src/main/java/baseball/PlayGame.java
+++ src/main/java/baseball/PlayGame.java
@@ -6,30 +6,30 @@ import java.util.HashSet;
 
 public class PlayGame {
 
-    public int ball;
-    public int strike;
-    public ArrayList<Integer> answerNumbers;
+    private int ball;
+    private int strike;
+    private ArrayList<Integer> answerNumbers;
 
     public void run() {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        this.answerNumbers = GenerateNum.makeAnswer();
-        this.ball = 0;
-        this.strike = 0;
-        while (this.strike != 3) {
+        answerNumbers = GenerateNum.makeAnswer();
+        ball = 0;
+        strike = 0;
+        while (strike != 3) {
             startGame();
         }
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
     public void startGame() {
-        this.ball = 0;
-        this.strike = 0;
+        ball = 0;
+        strike = 0;
         System.out.println("숫자를 입력해주세요 : ");
         String originalInput = Console.readLine();
         validateInput(originalInput);
         ArrayList<Integer> userNumberList = inputToArrayList(originalInput);
-        this.ball = Rule.countBalls(answerNumbers, userNumberList);
-        this.strike = Rule.countStrikes(answerNumbers, userNumberList);
+        ball = Rule.countBalls(answerNumbers, userNumberList);
+        strike = Rule.countStrikes(answerNumbers, userNumberList);
         printScore();
     }
 
@@ -97,21 +97,20 @@ public class PlayGame {
     }
 
     public void printScore() {
-        if (this.strike == 0 && this.ball == 0) {
+        if (strike == 0 && ball == 0) {
             System.out.println("낫싱");
             return;
         }
-        if (this.strike > 0 && this.ball > 0) {
-            System.out.printf("%d볼 %d스트라이크\n", this.ball, this.strike);
+        if (strike > 0 && ball > 0) {
+            System.out.printf("%d볼 %d스트라이크\n", ball, strike);
             return;
         }
-        if (this.strike > 0) {
-            System.out.printf("%d스트라이크\n", this.strike);
+        if (strike > 0) {
+            System.out.printf("%d스트라이크\n", strike);
             return;
         }
-        if (this.ball > 0) {
-            System.out.printf("%d볼\n", this.ball);
-            return;
+        if (ball > 0) {
+            System.out.printf("%d볼\n", ball);
         }
     }
 }
