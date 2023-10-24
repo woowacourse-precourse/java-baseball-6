aa81fa502a1b3e08ed2190cce4d0c34310004e36
c1ce <chalicenuclear@gmail.com>
2023-10-25 04:40:40 +0900

    feat : 게임 반복실행 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,21 +1,42 @@
 package baseball;
 
 import java.util.List;
+import java.util.Map;
 
 public class Game {
     private Random random = new Random();
     private User user = new User();
+    private Compare compare = new Compare();
+    private Output output = new Output();
+    private boolean quit = true;
+    private boolean randomCreate = true;
     public void start(){
-
-        while (true) {
+        output.startPrint();
+        List<Integer> randomNumber = null;
+        while (quit) {
 
             //랜덤숫자 생성
-            List<Integer> randomNumber=random.createNumber();
+            if (randomCreate) {
+                randomNumber = random.createNumber();
+                System.out.println(randomNumber);
+                randomCreate = false;
+            }
             //사용자 숫자 입력
-            List<Integer> userNumber= user.inputNumber();
+            output.inputPrint();
+            List<Integer> userNumber = user.inputNumber();
+            System.out.println(userNumber);
             //숫자비교
-
+            Map<String, Integer> result = compare.compareNumber(randomNumber, userNumber);
+            System.out.println(result.toString());
             //종료여부파악
+            if (!output.resultPrint(result)) {
+                if (!user.inputRestart()) {
+                    quit = false;
+                }
+                randomCreate = true;
+            }
+            ;
+
         }
     }
 }
