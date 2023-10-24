9e027c15c6f95d44ca35bf8eca1e540c55f07599
wnsgh12s <junho1273@gmail.com>
2023-10-24 18:01:23 +0900

    Feat: 랜덤한 숫자들을 생성하는 기능

--- src/main/java/baseball/domain/util/RandomNumberGenerator.java
+++ src/main/java/baseball/domain/util/RandomNumberGenerator.java
@@ -6,4 +6,12 @@ public class RandomNumberGenerator {
     public int generate() {
         return Randoms.pickNumberInRange(1, 9);
     }
+
+    public String  generateNumbers() {
+        StringBuilder numbers = new StringBuilder();
+        for (int i = 0; i < 3; i++) {
+            numbers.append(generate());
+        }
+        return numbers.toString();
+    }
 }
