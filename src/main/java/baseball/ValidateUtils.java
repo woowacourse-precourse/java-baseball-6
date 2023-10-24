714ddf1235883faffa9df7465837fb988475beec
chosunghyun18 <krismon@naver.com>
2023-10-24 14:09:24 +0900

    feat : add validateNo - 야구 숫자 1,9 검증

---
+++ src/main/java/baseball/ValidateUtils.java
@@ -0,0 +1,10 @@
+package baseball;
+
+public class ValidateUtils {
+    public static final int MIN_VALUE = 1;
+    public static final int MAX_VALUE = 9;
+
+    public static boolean validNo(int givenNumber) {
+        return givenNumber >= MIN_VALUE && givenNumber <= MAX_VALUE;
+    }
+}
