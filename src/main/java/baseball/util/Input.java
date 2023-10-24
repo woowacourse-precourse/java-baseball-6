e95cb0da345dc3a6a8a2c65aa858ed27b0b0ab76
sudo-dustle <asdf748596123@gmail.com>
2023-10-25 02:53:35 +0900

    feat: 사용자의 입력값을 받기 위한 Input 클래스 생성

--- src/main/java/baseball/util/Input.java
+++ src/main/java/baseball/util/Input.java
@@ -1,2 +1,44 @@
-package util;public class Input {
+package baseball.util;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Input {
+    private final InputValidator inputValidator;
+
+    public Input(InputValidator inputValidator) {
+        this.inputValidator = inputValidator;
+    }
+
+    public List<Integer> getUserNumbers() {
+        String readLine = Console.readLine();
+        inputValidator.validateDigits(readLine);
+
+        List<Integer> numbers = splitFromString(readLine);
+        validateNumbers(numbers);
+
+        return numbers;
+    }
+
+    private static List<Integer> splitFromString(String readLine) {
+        return Arrays.stream(readLine.split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    private void validateNumbers(List<Integer> numbers) {
+        inputValidator.validateInputSize(numbers);
+        inputValidator.validateDuplicate(numbers);
+        inputValidator.validateNumberRange(numbers);
+    }
+
+    public String getCommandAfterGameWin() {
+        String readCommand = Console.readLine();
+        inputValidator.validateCommandAfterGame(readCommand);
+        inputValidator.validateDigits(readCommand);
+        return readCommand;
+    }
 }
