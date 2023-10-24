f10439a17c6fdf0d0edbbc24bb4c16dbdb21f4a5
BDlhj <akk808@naver.com>
2023-10-24 14:23:43 +0900

    feat: Implement validateLength() in src/main/java/baseball/Validators.java

--- src/main/java/baseball/Validators.java
+++ src/main/java/baseball/Validators.java
@@ -1,2 +1,22 @@
-package baseball;public class Validators {
+package baseball;
+
+import java.util.List;
+
+public class Validators {
+    private static final int ANSWER_LENGTH = 3;
+    boolean isLengthValid;
+//    public boolean validatePlayerInput() {
+//
+//    }
+
+    private boolean validateLength(List<Integer> playerInput) {
+        if (playerInput.size() == ANSWER_LENGTH) {
+            isLengthValid = true;
+        }
+        return isLengthValid;
+    }
+
+//    private boolean validateRange() {
+//
+//    }
 }
