03bc76911e22bce29155752d8bc80af5be95a099
Jamieie <116743317+Jamieie@users.noreply.github.com>
2023-10-24 11:52:06 +0900

    Feat(GameOnOff): 게임 지속 여부 확인

---
+++ src/main/java/baseball/GameOnOff.java
@@ -0,0 +1,37 @@
+package baseball;
+
+public class GameOnOff {
+
+    int inputNum;
+
+    public int gameStart() {
+        return 1;
+    }
+
+    // 게임 지속 의사 확인
+    public int gameContinue(String input) throws IllegalArgumentException {
+        validationNumber(input);
+        validationOneTwo();
+        return inputNum;
+    }
+
+    // 입력값 유효성 확인: 숫자 여부
+    private void validationNumber(String input) throws IllegalArgumentException {
+        try {
+            int num = Integer.parseInt(input);
+            inputNum = num;
+        }
+        catch (NumberFormatException ex){
+            System.out.println("잘못된 값을 입력하였습니다.");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    // 입력값 유효성 확인: 1 or 2
+    private void validationOneTwo() throws IllegalArgumentException {
+        if (inputNum != 1 && inputNum != 2) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+}
