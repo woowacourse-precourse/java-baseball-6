b2b4d855ef081e22ec57a94abddba25cb660890e
minkyole <7719s@nave.com>
2023-10-24 15:42:31 +0900

    feat : 게임 시작 시 및 숫자 입력 받을 떄 안내메시지 출력

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -6,6 +6,7 @@ public class User {
     String userNum;
 
     void inputNum() throws IllegalArgumentException {
+        System.out.print("숫자를 입력해주세요 : ");
         userNum = readLine();
         if (userNum.length() != 3) {
             throw new IllegalArgumentException();
