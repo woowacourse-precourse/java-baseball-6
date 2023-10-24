facbcee87a8b946c90d1a533cc3e3a59cf730308
freekyr7529 <freekyr7529@gmail.com>
2023-10-24 21:16:30 +0900

    refactor: 인스턴스 변수 감소
    checkGameDone 메서드와 receiveDecision 메서드의 반환값을 boolean으로 변경 후 flag를 지역 변수로 이동시켜 변수 값을 재할당 하도록 하여 인스턴스 변수의 개수를 줄였다.

--- src/main/java/baseball/controller/Controller.java
+++ src/main/java/baseball/controller/Controller.java
@@ -11,35 +11,42 @@ public class Controller {
     private Answer answer;
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
-    private boolean flag = true;
 
     public void run() {
         inputView.greetingMsg();
+
         NumberGenerator generator = new NumberGenerator();
         answer = generator.createAnswer();
+
+        boolean flag;
 
         do {
             inputView.inputGuideMsg();
             List<Integer> input = inputView.inputByConsole();
+
             GameScore gameScore = answer.calcScore(input);
             gameScore.printResult();
-            checkGameDone(gameScore, generator);
+
+            flag = checkGameDone(gameScore, generator);
         } while (flag);
     }
 
-    private void checkGameDone(GameScore gameScore, NumberGenerator generator) {
+    private boolean checkGameDone(GameScore gameScore, NumberGenerator generator) {
         if (gameScore.isUserFindAnswer()) {
             outputView.roundEndMsg();
             inputView.askRestartMsg();
-            receiveDecision(generator);
+            return receiveDecision(generator);
         }
+
+        return true;
     }
 
-    private void receiveDecision(NumberGenerator generator) {
+    private boolean receiveDecision(NumberGenerator generator) {
         if (inputView.receiveRestartDecisionFromUser()) {
             answer = generator.createAnswer();
-            return;
+            return true;
         }
-        flag = false;
+
+        return false;
     }
 }
