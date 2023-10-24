3bbbe7e18bab3769b05340e487fb1f367209ba94
OhJongMin <jmoh7480@gmail.com>
2023-10-25 04:17:47 +0900

    feat:Add initGame method

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -2,6 +2,7 @@ package baseball.service;
 
 import baseball.domain.Computer;
 import baseball.domain.GameRule;
+import baseball.domain.User;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +10,13 @@ import java.util.List;
 public class GameService {
     public static List<Integer> userNumbers;
     public static List<Integer> computerNumbers;
+
+    public static void initGame() {
+        Computer.setBall(GameRule.defaultValue);
+        Computer.setStrike(GameRule.defaultValue);
+        User.setRestart(GameRule.restartNumber);
+        makeAnswer();
+    }
 
     public static void makeAnswer() {
         List<Integer> computer = new ArrayList<>();
