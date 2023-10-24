225459d9941942b5fd955d80132159cd2b49b104
min1018 <102647148+min1018@users.noreply.github.com>
2023-10-25 02:20:34 +0900

    feat: turn users input into list

---
+++ src/main/java/utils/UserNumList.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class UserNumList {
+
+
+    public static int[] UserInputList(String num) {
+        int[] userInputList = new int[num.length()];
+        for (int i = 0; i < num.length(); i++) {
+            userInputList[i] = num.charAt(i) - '0';
+        }
+        return userInputList;
+    }
+}
