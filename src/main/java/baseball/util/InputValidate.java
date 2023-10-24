97a6f18715118198ec6a041a566d3c100ff583f9
Hayoon <hy97@sju.ac.kr>
2023-10-24 20:49:47 +0900

    style(InputValidate): 상수로 매직넘버로 치환

--- src/main/java/baseball/util/InputValidate.java
+++ src/main/java/baseball/util/InputValidate.java
@@ -1,16 +1,39 @@
 package baseball.util;
 
+import static baseball.util.Constant.*;
+
 public class InputValidate {
 
+    /**
+     * 입력값의 길이를 검증
+     *
+     * @param input
+     */
     public static void validateLength(String[] input) {
-        if(input.length != 3) {
+        if (input.length != NUMBER_THREE) {
             throw new IllegalArgumentException("입력 값은 총 3자리여야 합니다.");
         }
     }
 
+    /**
+     * 입력값의 숫자가 1~9인지 검증
+     *
+     * @param number
+     */
     public static void validateNumberRange(int number) {
-        if (number < 1 || number > 9) {
+        if (number < NUMBER_ONE || number > NUMBER_NINE) {
             throw new IllegalArgumentException("1부터 9사이의 숫자를 입력해야 합니다.");
         }
+    }
+
+    /**
+     * 게임종료 시 1 또는 2인지 검증
+     */
+    public static int validateRestartGame(int number) {
+        if (number != NUMBER_ONE && number != NUMBER_TWO) {
+            throw new IllegalArgumentException("1부터 9사이의 숫자를 입력해야 합니다.");
+        }
+
+        return number;
     }
 }
