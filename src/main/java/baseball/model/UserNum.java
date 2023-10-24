f9e7d48da46ad32b98c368d809ea84b2e8e8b4c8
신동현 <donghyeun02@gmail.com>
2023-10-24 23:21:19 +0900

    feat : 정규식에 맞는 유저 input값 입력 기능

---
+++ src/main/java/baseball/model/UserNum.java
@@ -0,0 +1,33 @@
+package baseball.model;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class UserNum {
+  public List<Integer> userNum() {
+
+    System.out.println("숫자를 입력해주세요 : ");
+
+    String input = Console.readLine();
+
+    if (!ValidationInput(input)) {
+      throw new IllegalArgumentException("1에서 9까지 서로 다른 3자리 숫자를 입력하세요.");
+    }
+
+    List<Integer> player_num = new ArrayList<>();
+
+    for(String number : input.split("")) {
+      player_num.add(Integer.parseInt(number));
+    }
+
+    return player_num;
+  }
+
+  private boolean ValidationInput(String input) {
+    return Pattern.matches("^[1-9]{3}$", input)
+        && input.chars().distinct().count() == 3;
+  }
+}
