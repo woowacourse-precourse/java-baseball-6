89a5a5878da2476b17f2e82acef94651dec16585
jongheonleee <qwefghnm1212@gmail.com>
2023-10-24 20:37:26 +0900

    [test] 입력 개수 초과한 경우 테스트

--- src/test/java/baseball/domain/PlayerBallsTest.java
+++ src/test/java/baseball/domain/PlayerBallsTest.java
@@ -15,4 +15,12 @@ class PlayerBallsTest {
         List<Ball> expect = List.of(new Ball(1), new Ball(2), new Ball(3));
         assertEquals(expect, result.getBalls());
     }
+
+    @Test
+    void 입력_개수_초과_테스트() {
+        Integer[] input = {1, 2, 3, 4, 5};
+        assertThrows(IllegalArgumentException.class, () ->
+                new PlayerBalls(input)
+        );
+    }
 }
