0ae5814ce9c7abcf9e2449ba5845841e9a3b312c
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-10-24 15:33:52 +0900

    refactor: 결과 출력하는 메서드를 경우에 따라 분리

--- src/main/java/baseball/Printer.java
+++ src/main/java/baseball/Printer.java
@@ -26,18 +26,25 @@ public class Printer {
         int strike = result.strike();
         int ball = result.ball();
 
-        if (strike == 0 && ball == 0) {
-            System.out.println("낫싱");
-            return;
-        }
+        printBall(ball);
+        printStrike(strike);
+        printNotThing(result);
+    }
 
+    private static void printNotThing(Result result) {
+        if (result.ball() == 0 && result.strike() == 0) {
+            System.out.println("낫싱");
+        }
+    }
+
+    private static void printBall(int ball) {
         if (ball > 0) {
             System.out.print(ball + "볼 ");
         }
-
+    }
+    private static void printStrike(int strike) {
         if (strike > 0) {
-            System.out.print(strike + "스트라이크");
+            System.out.println(strike + "스트라이크");
         }
-        System.out.println();
     }
 }
