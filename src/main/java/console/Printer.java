6f124a192dfd3d74cfc223043037e62150d6e414
julie0005 <julie0005@ajou.ac.kr>
2023-10-25 01:54:26 +0900

    feat: 사용자에게 메세지 출력하는 Printer 클래스 구현

---
+++ src/main/java/console/Printer.java
@@ -0,0 +1,15 @@
+package console;
+
+public class Printer {
+    public static void printQuit(int perfectNumber) {
+        System.out.println(perfectNumber+"개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public static void printStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void printExitOrRestart(int restart, int exit) {
+        System.out.println("게임을 새로 시작하려면 "+restart+", 종료하려면 "+exit+"를 입력하세요");
+    }
+}
