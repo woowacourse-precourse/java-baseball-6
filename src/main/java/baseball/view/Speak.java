6a0a9c80f2c5494a97710da38a605593d8ad1829
jongchan0109 <jongchan1009@naver.com>
2023-10-24 15:20:51 +0900

    feat: 사용되는 문장 구현

--- src/main/java/baseball/view/Speak.java
+++ src/main/java/baseball/view/Speak.java
@@ -1,8 +1,8 @@
 package baseball.view;
 
-class Speak{
+public class Speak{
     final static String inputNumberSpeak = "숫자를 입력해주세요 : ";
-    final static String startGameSpeak = "숫자 야구 게임을 시작합니다.";
+    public final static String startGameSpeak = "숫자 야구 게임을 시작합니다.";
     final static String startOrEndSpeak = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     final static String gameEndSpeak = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     final static String notThingSpeak = "낫싱";
