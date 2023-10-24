e404afa8f06d5193a7a61b41e04c153882a013ff
biiit4894 <ghiesf@naver.com>
2023-10-24 21:21:45 +0900

    [FEAT] 스트라이크, 볼, 낫싱 판정 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,21 +4,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        ComputerNumbers();
-        PlayerNumbers();
+        int strike = 0;
+        int ball = 0;
+        Application app = new Application();
+        ArrayList<Integer> computerNumbers = ComputerNumbers();
+
+        while(strike < 3) {
+            strike = 0;
+            ball = 0;
+            ArrayList<Integer> playerNumbers = PlayerNumbers();
+            for(int i=0; i<computerNumbers.size(); i++) {
+                if(computerNumbers.get(i) == playerNumbers.get(i)) {
+                    strike += 1;
+                } else if (
+                        (computerNumbers.contains(playerNumbers.get(i))) && (computerNumbers.get(i) != playerNumbers.get(i))
+                ) {
+                    ball += 1;
+                }
+            }
+            if(strike != 0 && ball != 0) {
+                System.out.println(ball+"볼 "+strike+"스트라이크");
+            } else if (strike == 0 && ball != 0) {
+                System.out.println(ball+"볼");
+            } else if(strike != 0 && ball == 0) {
+                System.out.println(strike+"스트라이크");
+            } else {
+                System.out.println("낫싱");
+            }
+            if(strike == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            }
+        }
     }
 
-    public static void ComputerNumbers() {
+    public static ArrayList<Integer> ComputerNumbers() {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        List<Integer> computer = new ArrayList<>();
+        ArrayList<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if(!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
         }
+        return computer;
     }
 
     public static ArrayList<Integer> PlayerNumbers() {
