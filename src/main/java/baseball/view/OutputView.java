af23a24da25480be8733fc4ad2ea7139fbe945ec
liarreez <liarreez@gmail.com>
2023-10-24 15:57:39 +0900

    Style(OutputView) : 불필요한 공백 제거

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,7 +4,7 @@ import baseball.controller.BaseballGameController;
 
 public class OutputView {
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
-    private static final String GAME_CLEAR_MESSAGE = String.format("%d스트라이크\n %d개의 숫자를 모두 맞히셨습니다! 게임 종료",
+    private static final String GAME_CLEAR_MESSAGE = String.format("%d스트라이크\n%d개의 숫자를 모두 맞히셨습니다! 게임 종료",
             BaseballGameController.GAME_NUMBER_DIGIT, BaseballGameController.GAME_NUMBER_DIGIT);
     private static final String BALL_STRIKE_MESSAGE = "%d볼 %d스트라이크";
     private static final String NO_BALL_AND_STRIKE_MESSAGE = "낫싱";
