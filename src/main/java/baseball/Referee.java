d0a173dd0c2f1aae04d4ec75254139713b1710d2
BDlhj <akk808@naver.com>
2023-10-24 19:24:48 +0900

    feat: Implement countBall() in src/main/java/baseball/Referee.java

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class Referee {
     private int strike;
+    private int ball;
 
     public Integer countStrike(List<Integer> answer, List<Integer> playerInputIntegerList) {
         for (int i = 0; i < 3; i++) {
@@ -12,5 +13,16 @@ public class Referee {
             }
         }
         return strike;
+    }
+
+    public Integer countBall(List<Integer> answer, List<Integer> playerInputIntegerList) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (answer.get(i).equals(playerInputIntegerList.get(i)) && (i != j)) {
+                    ball += 1;
+                }
+            }
+        }
+        return ball;
     }
 }
