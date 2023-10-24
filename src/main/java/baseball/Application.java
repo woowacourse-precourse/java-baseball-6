54664c566b1225ca8b588ad1ee82e6033264ccbe
yewonahn <yewonahn@naver.com>
2023-10-24 19:19:13 +0900

    README 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -66,28 +66,24 @@ public class Application {
                     }
                 }
 
-                if (strike == 3) {
-                    System.out.println("3스트라이크");
-                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                    check = readLine();
-                    if (check != "1" || check != "2") {
-                        throw new IllegalArgumentException();
-                    }
+                if (strike == 0 && ball == 0) {
+                    System.out.println("낫싱");
                 }
-
-                else {
-                    if (strike == 0 && ball == 0) {
-                        System.out.println("낫싱");
-                    }
-                    else if (strike != 0 && ball == 0) {
-                        System.out.printf("%d스트라이크\n", strike);
-                    }
-                    else if (strike == 0 && ball != 0) {
-                        System.out.printf("%d볼\n", ball);
-                    }
-                    else if (strike != 0 && ball != 0) {
-                        System.out.printf("%d볼 %d스트라이크\n", ball, strike);
+                else if (strike == 0 && ball != 0) {
+                    System.out.printf("%d볼\n", ball);
+                }
+                else if (strike != 0 && ball != 0) {
+                    System.out.printf("%d볼 %d스트라이크\n", ball, strike);
+                }
+                else if (strike != 0 && ball == 0) {
+                    System.out.printf("%d스트라이크\n", strike);
+                    if (strike == 3) {
+                        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                        check = readLine();
+                        if (!check.equals("1") && !check.equals("2")) {
+                            throw new IllegalArgumentException();
+                        }
                     }
                 }
             } while ("3".equals(check));
