56c1e2cf309e7f6f5491aa8fa82d4c0949c45eef
Park Eunhyeong <silbroeh@gmail.com>
2023-10-24 15:36:16 +0900

    feat : inputComputer() 메소드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -57,7 +58,12 @@ public class Application {
     }
 
     private static void inputComputer() {
-
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
     }
 
     private static void inputUser(String userString) {
