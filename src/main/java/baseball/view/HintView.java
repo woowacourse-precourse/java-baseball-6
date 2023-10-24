e40f84537af41f476b71ea805701ffb20898ef53
inpink <inpink@kakao.com>
2023-10-25 03:46:02 +0900

    refactor: HintView는 Hint객체가 아닌 출력할 값(숫자, 이름) 리스트만 받아서 출력.

--- src/main/java/baseball/view/HintView.java
+++ src/main/java/baseball/view/HintView.java
@@ -1,19 +1,13 @@
 package baseball.view;
 
-import baseball.controller.hint.HintController;
 import baseball.domain.hint.Hint;
 
 import java.util.List;
 
 public class HintView {
 
-    public void displayHintMessage(Hint hint) {
-
-        List<String> countList = HintController.counts(hint);
-        List<String> nameList = HintController.names(hint);
-
+    public void displayHintMessage(List<String> countList, List<String> nameList) {
         StringBuilder hintBuilder = new StringBuilder();
-
         for (int i = 0; i < countList.size(); i++) {
             hintBuilder.append(countList.get(i) + nameList.get(i));
             addSpaceIfNotEmpty(hintBuilder);
