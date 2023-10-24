644f420f084638d00d0bae7e70c5d9b08c68f1fb
박승찬 <seungchan141414@gmail.com>
2023-10-24 23:53:55 +0900

    [Refactor(baseball)] 심판의 게임 체크하는 메서드 기능 분할
    CheckProgressBaseballGame 메서드가 낫싱, strike, ball의 체크를 하는 막중한 책임감에서 해방 되었습니다.
    이전에는 CheckProgressBaseballGame메서드 안에서 게임의 결과에 관한 모든 책임을 가지고 있었지만
    현제 CheckProgressBaseballGame은 모든 일을 버리고 메세지를 반환하는 역활을 가지게 되었습니다.
    그로인해 CheckProgressBaseballGame 비교적 이전보다 가벼운 협력적인 메서드를 호출하여 적절한 문자열을 리턴하는 역활을 합니다.

--- src/main/java/baseball/domain/Judge.java
+++ src/main/java/baseball/domain/Judge.java
@@ -8,21 +8,15 @@ import java.util.List;
 public class Judge {
     public final static int NOT_THING_SAME_RESULT = 0;
     GameStandard gameStandard;
+    String resultHintOfJudge;
+    List<Integer> computerList;
+    List<Integer> userList;
 
     public Judge() {
         gameStandard = new GameStandard();
+        resultHintOfJudge = "";
     }
 
-    /**
-     * Ball의 갯수를 얻는다.
-     *
-     * @param cntTotalSameUserAndComputer user와 computer의 같은 숫자의 갯수
-     * @param cntStrike                   스트라이크의 갯수
-     * @return ball의 갯수를 반환한다.
-     */
-    private static int getCntBall(int cntTotalSameUserAndComputer, int cntStrike) {
-        return cntTotalSameUserAndComputer - cntStrike;
-    }
 
     /**
      * computerList와 userList을
@@ -32,58 +26,140 @@ public class Judge {
      * @return
      */
     public String CheckProgressBaseballGame(List<Integer> computerList, List<Integer> userList) {
-        int cntSameAllNumUserAndComputer = 0; // computerList 중에 userList와 같은 것이 몇개인가.
-        int cntStrike = 0; // strike의 갯수
-        int cntBall = 0; // ball의 갯수
+        this.computerList = computerList;
+        this.userList = userList;
 
-        // 1. 숫자가 같은 갯수 - computerList 중에 userList와 같은 것이 몇개인지 구한다.
-        cntSameAllNumUserAndComputer = getSameAllNumberAndComputer(computerList, userList);
+        // 1. 숫자가 같은 갯수가 없다면 "낫싱"
+        if (isHintNotThing()) {
+            return resultHintOfJudge;
+        }
 
-        // 2. 스트라이크 갯수 - userList의 각각의 숫자와 computerList 중에 같은 것이 몇개인지 구한다
-        cntStrike = getCntStrike(computerList, userList);
+        // 2. 볼이 없다면 "3스트라이크"
+        if (isHintNotBallOnlyStrike()) {
+            return resultHintOfJudge;
+        }
 
-        // 3. 볼의 갯수 - 숫자가 같은 갯수 - 스트라이크 갯수 는 볼의 갯수이다.
-        cntBall = getCntBall(cntSameAllNumUserAndComputer, cntStrike);
+        // 3. 볼만 있다면
+        if (isHintNotStrikeOnlyBall()) {
+            return resultHintOfJudge;
+        }
 
-        // 4. 숫자가 같은 갯수가 없다면 "낫싱"
+        // 4. 볼과 스트라이크이 있다면 "1볼 1스트라이크"
+        if (isHintBallStrike()) {
+            return resultHintOfJudge;
+        }
+
+        return "";
+    }
+
+    /**
+     * 볼과 스트라이크가 존제하지 않는다면 (0이다.)
+     * "낫싱";
+     *
+     * @return
+     */
+    private boolean isHintNotThing() {
+        int cntSameAllNumUserAndComputer = getSameAllNumberAndComputer();
         if (Utill.isSameInteger(cntSameAllNumUserAndComputer, NOT_THING_SAME_RESULT)) {
-            return MyConstants.MSG_NOT_THING();
+            resultHintOfJudge = MyConstants.MSG_NOT_THING();
+            return true;
         }
+        return false;
+    }
 
-        // 5. 볼이 없다면 "3스트라이크"
+    /**
+     * 볼이 0이고 스트라이크가 존제한다면
+     * cntStrike + "스트라이크";
+     *
+     * @return
+     */
+    private boolean isHintNotBallOnlyStrike() {
+        int cntStrike = getCntStrike();
+        int cntBall = getCntBall();
+
         if (Utill.isSameInteger(cntBall, NOT_THING_SAME_RESULT)) {
-            return MyConstants.MSG_ONLY_STRIKE(cntStrike);
+            resultHintOfJudge = MyConstants.MSG_ONLY_STRIKE(cntStrike);
+            return true;
         }
+        return false;
+    }
 
-        // 6. 볼만 있다면
+    /**
+     * 스트라이크가 0이고 볼이 존제한다면
+     * cntBall + "볼"; 을 반환
+     *
+     * @return
+     */
+    private boolean isHintNotStrikeOnlyBall() {
+        int cntStrike = getCntStrike();
+        int cntBall = getCntBall();
+
         if (Utill.isSameInteger(cntStrike, NOT_THING_SAME_RESULT)) {
-            return MyConstants.MSG_ONLY_BALL(cntBall);
+            resultHintOfJudge = MyConstants.MSG_ONLY_BALL(cntBall);
+            return true;
         }
+        return false;
+    }
 
-        // 7. 볼과 스트라이크이 있다면 "1볼 1스트라이크"
-        return MyConstants.MSG_BALL_STRIKE(cntBall, cntStrike);
+    /**
+     * 스트라이크와 볼이 0이 아니면
+     * 심판의 cntBall + "볼 " + cntStrike + "스트라이크"; 을 반환
+     *
+     * @return
+     */
+    private boolean isHintBallStrike() {
+        int cntStrike = getCntStrike();
+        int cntBall = getCntBall();
+
+        if (isExistStrikeBall(cntStrike, cntBall)) {
+            resultHintOfJudge = MyConstants.MSG_BALL_STRIKE(cntBall, cntStrike);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 스트라이크 와 볼이 0이 아니면 true을 리턴.
+     *
+     * @param cntStrike
+     * @param cntBall
+     * @return
+     */
+    private boolean isExistStrikeBall(int cntStrike, int cntBall) {
+        return !Utill.isSameInteger(cntStrike, NOT_THING_SAME_RESULT) &&
+                !Utill.isSameInteger(cntBall, NOT_THING_SAME_RESULT);
     }
 
     /**
      * 유저의 숫자와 컴퓨터의 숫자중 같은 숫자가 몇개인가 확인한다.
      * 만약 user = (1, 2, 3) com = (1, 2, 4) 이면 결과는 2이다.
      *
-     * @param computerList 컴퓨터의 임의의 3개의 숫자가 있는 리스트
-     * @param userList     유저의 임의의 3개의 숫자가 있는 리스트.
      * @return
      */
-    private int getSameAllNumberAndComputer(List<Integer> computerList, List<Integer> userList) {
+    private int getSameAllNumberAndComputer() {
         return gameStandard.getSameUserNumber2Computer(computerList, userList);
+    }
+
+    /**
+     * Ball의 갯수를 얻는다.
+     *
+     * @return ball의 갯수를 반환한다.
+     */
+    private int getCntBall() {
+        int cntSameAllNumUserAndComputer = 0; // computerList 중에 userList와 같은 것이 몇개인가.
+        int cntStrike = getCntStrike();
+
+        cntSameAllNumUserAndComputer = getSameAllNumberAndComputer();
+
+        return cntSameAllNumUserAndComputer - cntStrike;
     }
 
     /**
      * 스트라이크의 갯수를 얻는다.
      *
-     * @param computerList
-     * @param userList
      * @return
      */
-    private int getCntStrike(List<Integer> computerList, List<Integer> userList) {
+    private int getCntStrike() {
         int cntStrike = 0;
         int valueIndexOfUserList;
         boolean isSamePlaceIndexUserAndCom;
@@ -91,7 +167,7 @@ public class Judge {
         int sizeUserList = userList.size();
         for (int indexUserList = 0; indexUserList < sizeUserList; indexUserList++) {
             valueIndexOfUserList = userList.get(indexUserList); // index의 UserList의 값
-            isSamePlaceIndexUserAndCom = isSamePlaceNumUserAndComputer(computerList, valueIndexOfUserList, indexUserList); // user의 특정 index의 값과 computer의 특정 index의 값이 같은가.
+            isSamePlaceIndexUserAndCom = isSamePlaceNumUserAndComputer(valueIndexOfUserList, indexUserList); // user의 특정 index의 값과 computer의 특정 index의 값이 같은가.
 
             if (isSamePlaceIndexUserAndCom) { // user의 특정 index의 값과 computer의 특정 index의 값이 같으면.
                 cntStrike = cntStrike + 1; // 스트라이크의 갯수 증가.
@@ -103,12 +179,11 @@ public class Judge {
     /**
      * 유저의 특정한 index의 값과 컴퓨터리스트의 특정한 index의 값이 같은지 확인한다.
      *
-     * @param computerList         컴퓨터 3개의 랜덤 숫자가 있는 LIST
      * @param valueIndexOfUserList 유저의 특정한 값의 index
      * @param indexUserList        유저의 특정한 값이 있는 index
      * @return
      */
-    private boolean isSamePlaceNumUserAndComputer(List<Integer> computerList, int valueIndexOfUserList, int indexUserList) {
+    private boolean isSamePlaceNumUserAndComputer(int valueIndexOfUserList, int indexUserList) {
         return gameStandard.isSamePlaceIndexNumUser2ComNumList(computerList, indexUserList, valueIndexOfUserList);
     }
 }
