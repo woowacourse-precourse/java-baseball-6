9fae0ff26952f7649f480dcf08765673370112fa
jaepyo99 <jaepyo99@naver.com>
2023-10-24 21:45:01 +0900

    [Refactory] static메서드로 리팩토링

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -12,26 +12,26 @@ public class Validator {
     }
 
     // 컴퓨터를 통한 랜덤 야구번호를 검증해주는 메서드
-    public Boolean validNumNotInList(String invalidNum, List<String> validNumList) {
+    public static Boolean validNumNotInList(String invalidNum, List<String> validNumList) {
         return validNumList.contains(invalidNum);
     }
 
     // 사용자가 입력한 숫자가 3자리 숫자인지 확인해주는 메서드
-    public Boolean validNums(String invalidNums) throws IllegalArgumentException {
+    public static Boolean validNum(String invalidNums){
         if (validLength(invalidNums) && validNonZero(invalidNums)) {
             return TRUE;
         }
-        throw new  IllegalArgumentException();
+        return FALSE;
     }
 
-    public Boolean validLength(String invalidNums) {
+    private static Boolean validLength(String invalidNums) {
         if (invalidNums.length() != 3) {
             return FALSE;
         }
         return TRUE;
     }
 
-    public Boolean validNonZero(String invalidNums) {
+    private static Boolean validNonZero(String invalidNums) {
         if (invalidNums.contains("0")) {
             return FALSE;
         }
