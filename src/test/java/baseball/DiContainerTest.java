3160048af223a8075d19cdb045e0ee10fdc02a49
JongMinCh0i <wonderland314592@gmail.com>
2023-10-25 01:58:02 +0900

    style(DiContainer): OutputVIew 임포트 추가

--- src/test/java/baseball/DiContainerTest.java
+++ src/test/java/baseball/DiContainerTest.java
@@ -8,6 +8,7 @@ import baseball.controller.UserController;
 import baseball.service.GameService;
 import baseball.util.Validation;
 import baseball.view.InputView;
+import baseball.view.OutputVIew;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -37,7 +38,7 @@ class DiContainerTest {
         //when
         Controller userControllerGenerateByDI = diContainer.userController();
         Controller userControllerGenerateByNew = new UserController(new InputView(),
-                new GameService(new Validation()));
+                new GameService(new Validation(), new OutputVIew()));
 
         //then
         assertThat(userControllerGenerateByDI).isInstanceOf(userControllerGenerateByNew.getClass());
