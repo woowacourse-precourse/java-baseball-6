f78941d934180a5a543eb9f8bec0bcee02b7a40a
TAEMIN\gkxoa <gkxoals33@gmail.com>
2023-10-25 00:08:11 +0900

    서로다른 수 예외처리

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -18,6 +18,9 @@ public class User {
        for(int i=0; i< userNumber.length();i++) {
             char userchar = userNumber.charAt(i);
             int translateNumber = Character.getNumericValue(userchar);
+                if (userNumbers.contains(translateNumber)) {
+                    throw new IllegalArgumentException("서로 다른 3자리 수를 입력해 주세요.");
+                }
             userNumbers.add(translateNumber);
         }
         return userNumbers;
