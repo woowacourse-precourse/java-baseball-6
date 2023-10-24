d2618dd6b167cb15485f4a76d43bcebffead5bfb
Park Eunhyeong <silbroeh@gmail.com>
2023-10-24 16:18:00 +0900

    feat : printHint() 메소드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -103,7 +103,15 @@ public class Application {
     }
 
     private static void printHint() {
-
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+        } else if (strike == 0) {
+            System.out.println(ball + "볼");
+        } else if (ball == 0) {
+            System.out.println(strike + "스트라이크");
+        } else {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
     }
 
     private static boolean isAnswer() {
