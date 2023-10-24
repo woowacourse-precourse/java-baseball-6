19ddbc5ae7cd51c41a410e68802f09aab4cebf54
ma-an-jong <alswhd1113@gmail.com>
2023-10-24 10:57:40 +0900

    feat(View) : 입출력 View 생
    사용자에게 입력을 받고 결과를 보여주는 IO View를 만들었다.

--- src/main/java/baseball/utils/message/IOMessage.java
+++ src/main/java/baseball/utils/message/IOMessage.java
@@ -5,4 +5,6 @@ public class IOMessage {
 
     public static final String INPUT_NUMBER = "숫자를 입력해주세요 : ";
     public static final String CONTINUE_INPUT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
+
+    public static final String IOEXCEPTION_WHILE_RUNTIME = "입출력도중 문제 발생....\n";
 }
