8904b590fe9ae3b9c943511512eab15c399d7c77
COBI-98 <tkdgus9688@gmail.com>
2023-10-24 22:12:52 +0900

    feat: 게임 시작 문구 출력뷰 구현

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,10 @@
+package baseball.view;
+
+import java.util.List;
+
+public class OutputView {
+    private static final String GAME_START = "숫자 야구 게임을 시작합니다.";
+    public void printCreateController() {
+        System.out.println(GAME_START);
+    }
+}
