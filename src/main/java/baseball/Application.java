eadb3b46c33c037804afab3c138b8895f54ca5e8
ISMINIMIN <summmm.224@gmail.com>
2023-10-25 02:48:14 +0900

    feat: 비교 결과 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -72,4 +72,20 @@ public class Application {
         }
         return strike;
     }
+
+    // 사용자 숫자 결과 출력
+    public static void printResult(int ball, int strike) {
+        if(strike == 3) {
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        } else if(ball == 0 && strike > 0) {
+            System.out.println(strike + "스트라이크");
+        } else if(ball > 0 && strike == 0) {
+            System.out.println(ball + "볼");
+        } else if(ball > 0 && strike > 0) {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        } else {
+            System.out.println("낫싱");
+        }
+    }
 }
