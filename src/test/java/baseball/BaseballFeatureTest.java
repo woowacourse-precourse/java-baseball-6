8f73d75e8720d0f0580a47ac4ef70af374fd6521
ckkim817 <ckkim817@konkuk.ac.kr>
2023-10-25 07:51:31 +0900

    [Test] 플레이어 숫자 입력 기능 검증을 위한 테스트 작성

--- src/test/java/baseball/BaseballFeatureTest.java
+++ src/test/java/baseball/BaseballFeatureTest.java
@@ -3,13 +3,17 @@ package baseball;
 import static baseball.global.BaseballConstant.GUESS_NUMBER_SIZE;
 import static baseball.global.BaseballConstant.MAX_RANGE;
 import static baseball.global.BaseballConstant.MIN_RANGE;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import baseball.domain.Computer;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
 import java.util.stream.IntStream;
 import org.junit.jupiter.api.Test;
 
-public class BaseballFeatureTest {
+public class BaseballFeatureTest extends NsTest {
 
     @Test
     void 랜덤하게_고른_숫자가_1에서_9까지의_서로_다른_3자리_숫자인지_확인() {
@@ -18,5 +22,65 @@ public class BaseballFeatureTest {
                 .filter(computer::numbersContains)
                 .count();
         assertThat(count).isEqualTo(GUESS_NUMBER_SIZE);
+    }
+
+    @Test
+    void 입력_값이_빈_값일_경우_예외_처리() {
+        List<String> testInputs = List.of(" ", "   ", "    ", "\n", " 1 ", "1 2 3");
+        for (String input : testInputs) {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException(input))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+    }
+
+    @Test
+    void 입력_값이_숫자가_아닐_경우_예외_처리() {
+        List<String> testInputs = List.of("우테코", "WTC", "!@#", "우T#");
+        for (String input : testInputs) {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException(input))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+    }
+
+    @Test
+    void 입력_값이_3자리가_아닐_경우_예외_처리() {
+        List<String> testInputs = List.of("1", "22", "4444", "55555");
+        for (String input : testInputs) {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException(input))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+    }
+
+    @Test
+    void 입력_값_중_0이_있을_경우_예외_처리() {
+        List<String> testInputs = List.of("012", "102", "120");
+        for (String input : testInputs) {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException(input))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+    }
+
+    @Test
+    void 입력_값_중_같은_숫자가_있을_경우_예외_처리() {
+        List<String> testInputs = List.of("112", "121", "211");
+        for (String input : testInputs) {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException(input))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
     }
 }
