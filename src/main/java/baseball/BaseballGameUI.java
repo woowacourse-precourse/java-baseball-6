89877c463392002d3270a6587bbea9d1694d10eb
zjacom <eephus625@icloud.com>
2023-10-24 21:10:02 +0900

    refactor : 메소드명 변경
    inputPlayerNum() -> inputPlayerNumMessage()로 변경
    플레이어 숫자를 입력받는 "메시지"임을 명확하게 하기 위해서 변경함

--- src/main/java/baseball/BaseballGameUI.java
+++ src/main/java/baseball/BaseballGameUI.java
@@ -14,7 +14,7 @@ public class BaseballGameUI {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    public void inputPlayerNum() {
+    public void inputPlayerNumMessage() {
         System.out.print("숫자를 입력해주세요 : ");
     }
 
@@ -82,7 +82,7 @@ public class BaseballGameUI {
         // System.out.println(computer.getComputerBall());
 
         while (button) {
-            inputPlayerNum();
+            inputPlayerNumMessage();
             String inputValue = readLine();
             Player player = new Player(inputValue);
             CalculateResult calculateResult = new CalculateResult(computer.getComputerBall(), player.getPlayerBall());
