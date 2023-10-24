b89744341f30ad3c5b57efecb6a793d328361fae
eunzinn <dhr2176@gmail.com>
2023-10-24 12:31:44 +0900

    Disunite method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,13 +6,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+    static int strike, ball;
+    static List<Integer> computer;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
 
         int restart = 0;
         do{
-            List<Integer> computer = new ArrayList<>();
+            computer = new ArrayList<>();
             while(computer.size() < 3){
                 int randomNumber = Randoms.pickNumberInRange(1, 9);
                 if(!computer.contains(randomNumber)) {
@@ -28,19 +30,10 @@ public class Application {
                     throw new IllegalArgumentException();
                 }
 
-                int ball = 0;
-                int strike = 0;
+                ball = 0;
+                strike = 0;
 
-                for(int i = 0; i < computer.size(); i++){
-                    for(int j = 0; j < computer.size(); j++){
-                        if(computer.get(i) == Integer.parseInt(str[j]) && i == j){
-                            strike++;
-                        }
-                        if(computer.get(i) == Integer.parseInt(str[j]) && i != j){
-                            ball++;
-                        }
-                    }
-                }
+                strikeBall(str);
 
                 if(strike == 3){
                     finish = true;
@@ -48,7 +41,7 @@ public class Application {
                     System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 } else if(strike > 0 || ball > 0) {
                     System.out.println(ball + "볼 " + strike + "스트라이크");
-                } else {
+                } else if(strike == 0 && ball == 0) {
                     System.out.println("낫싱");
                 }
             }
@@ -56,5 +49,18 @@ public class Application {
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             restart = Integer.parseInt(readLine());
         }while(restart == 1);
+    }
+
+    public static void strikeBall(String[] arr){
+        for(int i = 0; i < computer.size(); i++){
+            for(int j = 0; j < computer.size(); j++){
+                if(computer.get(i) == Integer.parseInt(arr[j]) && i == j){
+                    strike++;
+                }
+                if(computer.get(i) == Integer.parseInt(arr[j]) && i != j){
+                    ball++;
+                }
+            }
+        }
     }
 }
