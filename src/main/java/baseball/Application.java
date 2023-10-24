2f708e9e850a768f3cb77f742a0b0e5dfa943ad7
parkchangeon <jg9870@naver.com>
2023-10-24 20:06:40 +0900

    try catch 블록 제거, Console.close() 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,29 +12,25 @@ public class Application {
         int reGame = 1;
         List<Integer> computer = new ArrayList<>();
 
-        try {
-            System.out.println("숫자 야구 게임을 시작합니다.");
-            while (reGame != 2) {
-                if (reGame == 1) {
-                    computer = NumberProducer.makeNumberInRange(1, 9, 3);
-                    reGame = 0; // reGame 초기화
-                    // test
-//                    System.out.println("computer = " + computer);
-                }
-                System.out.print("숫자를 입력해주세요 : ");
-                String result = NumberMatcher.numberMatcherResult(computer, 1, 9, 3);
-                // todo 결과를 어떤 식으로 줘서 판별할지 고민
-                if (result.equals("3스트라이크")) {
-                    System.out.println(result);
-                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                    reGame = Integer.parseInt(Console.readLine());
-                } else {
-                    System.out.println(result);
-                }
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        while (reGame != 2) {
+            if (reGame == 1) {
+                computer = NumberProducer.makeNumberInRange(1, 9, 3);
+                reGame = 0; // reGame 초기화
             }
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException(e.getMessage());
+            System.out.print("숫자를 입력해주세요 : ");
+            String result = NumberMatcher.numberMatcherResult(computer, 1, 9, 3);
+
+            if (result.equals("3스트라이크")) {
+                System.out.println(result);
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                reGame = Integer.parseInt(Console.readLine());
+            } else {
+                System.out.println(result);
+            }
         }
+        Console.close();
     }
 }
