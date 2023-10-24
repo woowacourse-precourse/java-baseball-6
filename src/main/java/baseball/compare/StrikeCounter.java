b27284d3b244afd8697c54e9ac5f4a6fb6a0c873
JangHongJoon <wkdghdwns19969@gmail.com>
2023-10-24 23:28:43 +0900

    Feat : 숫자열 비교하고 스트라이크 개수 카운트하는 StrikeCounter.java 생성

---
+++ src/main/java/baseball/compare/StrikeCounter.java
@@ -0,0 +1,22 @@
+package baseball.compare;
+
+import baseball.balls.Balls;
+
+public class StrikeCounter {
+
+    private final Integer MAX_CAPACITY = 3;
+    private int strikeCountStatus = 0;
+    private int arrayIndexStatus = 0;
+    public int strikeCount(Balls answerBalls, Balls playerBalls) {
+        int answerBall = answerBalls.getBallsList().get(arrayIndexStatus);
+        int playerBall = playerBalls.getBallsList().get(arrayIndexStatus);
+        if (answerBall == playerBall) {
+            strikeCountStatus = strikeCountStatus + 1;
+        }
+        arrayIndexStatus = arrayIndexStatus + 1;
+        if (arrayIndexStatus != MAX_CAPACITY){
+            return strikeCount(answerBalls, playerBalls);
+        }
+        return strikeCountStatus;
+    }
+}
