5e2c0a89f073dcaf468d413e8ba56bb32931dcd4
rayark1 <max980203@snu.ac.kr>
2023-10-24 18:26:09 +0900

    feat: Add BaseballGame class and implement game start logic

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,10 @@
 package baseball;
 
+import baseball.game.BaseballGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseballGame baseballGame = new BaseballGame();
+        baseballGame.startGame();
     }
 }
---
+++ src/main/java/baseball/game/BaseballGame.java
@@ -0,0 +1,14 @@
+package baseball.game;
+
+
+public class BaseballGame {
+
+    public void startGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        executeGameLoop();
+    }
+
+    private void executeGameLoop() {
+        // Todo: Implement this method
+    }
+}
