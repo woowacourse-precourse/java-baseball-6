60c0af649f730d60598618127415c162254dad90
jiaeYoon <yoon529@sookmyung.ac.kr>
2023-10-24 16:12:54 +0900

    Feat: '자릿수 체크 기능' 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -17,8 +17,13 @@ public class Application {
 
             String computer = makeRandomNums();
 
-            String user = enterNums();
-//            correct = check(computer, user);
+            boolean correct = false;
+            while (!correct) {
+                String user = enterNums();
+                correct = check(computer, user);
+
+                if (correct) System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            }
 
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             status = Integer.parseInt(Console.readLine());
@@ -65,5 +70,26 @@ public class Application {
         }
 
         return input;
+    }
+
+    private static boolean check(String computer, String user) {
+        int strike = 0;
+        int ball = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (user.charAt(i) == computer.charAt(j)) {
+                    if (i == j) strike++;
+                    else ball++;
+                }
+            }
+        }
+
+        if (ball == 0 && strike == 0) System.out.println("낫싱");
+        else if (ball > 0 && strike > 0) System.out.println(ball + "볼 " + strike + "스트라이크");
+        else if (ball > 0) System.out.println(ball + "볼");
+        else if (strike > 0) System.out.println(strike + "스트라이크");
+
+        return strike == 3;
     }
 }
