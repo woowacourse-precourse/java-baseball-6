a2a1b24ef133987a6f7565ea6ba50f38a68838bc
wooni97 <jhw970714@gmail.com>
2023-10-25 04:27:49 +0900

    fix(Computer): change create random number
    - 이 커밋에서는 컴퓨터가 random number를 생성하는데에 버그가 존재하여 수정했습니다.

--- src/main/java/baseball/entity/Computer.java
+++ src/main/java/baseball/entity/Computer.java
@@ -6,11 +6,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
-    private List<Integer> computerNumber = new ArrayList<>();
+    private int RANDOM_MAX_NUM = 9;
+    private int RANDOM_MIN_NUM = 1;
 
     public List<Integer> createRandomNumber(){
+        List<Integer> computerNumber = new ArrayList<>();
         while (computerNumber.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            int randomNumber = Randoms.pickNumberInRange(RANDOM_MIN_NUM, RANDOM_MAX_NUM);
             if (!computerNumber.contains(randomNumber)) {
                 computerNumber.add(randomNumber);
             }
