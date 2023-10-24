230e20ec22047893aec9f21eb91efdf19f9c3019
park <sjnnj@naver.com>
2023-10-24 14:37:30 +0900

    랜덤 값 자동 생성 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,29 @@
 package baseball;
 
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        //test
+        // 숫자 야구 게임 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        String restart = "1";
+
+        // 랜덤 값 자동 생성
+        while (restart.equals("1")) {
+            List<Integer> computer = new ArrayList<>();
+
+            while (computer.size() < 3) {
+                int randomNumber = Randoms.pickNumberInRange(1, 9);
+                if (!computer.contains(randomNumber)) {
+                    computer.add(randomNumber);
+                }
+            }
+
     }
 }
