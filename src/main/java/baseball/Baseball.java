4c063c0474df156708ddcef0c125e18bfeb27e85
eunji <dmswl6310@gmail.com>
2023-10-24 23:53:02 +0900

    fix: 스트라이크와 볼 순서 수정(볼이 앞쪽 출력)

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -13,6 +13,7 @@ public class Baseball {
 
 
     public void initBaseball(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
         // 컴퓨터 수 세팅
         this.computerNum=setComputerNum();
         // 사용자 입력 숫자 초기화
@@ -35,22 +36,22 @@ public class Baseball {
         return computer;
     }
     public void printResult(int[] result){
-        int strikeNum=result[0];
-        int ballNum=result[1];
+        int ballNum=result[0];
+        int strikeNum=result[1];
 
-        if(strikeNum==0 && ballNum==0) {
+        if(ballNum==0 && strikeNum==0) {
             System.out.println("낫싱");
             return;
         }
 
-        if(strikeNum!=0) System.out.print(strikeNum+"스트라이크 ");
-        if(ballNum!=0) System.out.print(ballNum+"볼");
+        if(ballNum!=0) System.out.print(ballNum+"볼 ");
+        if(strikeNum!=0) System.out.print(strikeNum+"스트라이크");
         System.out.print("\n");
     }
 
     public int[] countResult(String input){
-        int strikeNum=0;
         int ballNum=0;
+        int strikeNum=0;
 
         for(int i=0;i<3;i++){
             if(input.charAt(i)==computerNum.charAt(i)) strikeNum++;
@@ -59,11 +60,10 @@ public class Baseball {
             }
         }
 
-        return new int[]{strikeNum,ballNum};
+        return new int[]{ballNum,strikeNum};
     }
 
     public void playBaseball(){
-        System.out.println("숫자 야구 게임을 시작합니다.");
 //        System.out.println(computerNum);
         do {
 
@@ -73,10 +73,15 @@ public class Baseball {
             if(input.length()!=3){
                 throw new IllegalArgumentException();
             }
+            for(int i=0;i<3;i++){
+                for(int j=i+1;j<3;j++){
+                    if(input.charAt(i)==input.charAt(j)) throw new IllegalArgumentException();
+                }
+            }
             int[] result=countResult(input);
             printResult(result);
 
-            if(result[0]==3) {
+            if(result[1]==3) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 break;
             }
@@ -85,9 +90,10 @@ public class Baseball {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String input=Console.readLine();
         if(input.equals("1")){
-            initBaseball();
+//            initBaseball();
+            this.computerNum=setComputerNum();
             playBaseball();
-        }else if(input.equals("2")){
+        }else if(!input.equals("2")){
             // 종료
             throw new IllegalArgumentException();
         }
