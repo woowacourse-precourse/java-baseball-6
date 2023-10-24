95ae8d1b094403db388832c4a0b42c49c522112d
HiiWee <wpdlzhf159@gmail.com>
2023-10-24 20:04:49 +0900

    refactor: BaseballGame 패키지 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import baseball.game.BaseballGame;
+
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/baseball/game/BaseballGame.java
+++ src/main/java/baseball/game/BaseballGame.java
@@ -1,4 +1,4 @@
-package baseball;
+package baseball.game;
 
 import baseball.domain.Computer;
 import baseball.dto.GameResult;
