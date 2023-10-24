4449b5953a0da9d582d04a0dbcae0a605d785d93
BDlhj <akk808@naver.com>
2023-10-25 02:14:34 +0900

    feat: Implement feature that plays game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,14 +2,7 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // 게임 시작 문구 출력
-        // do-while
-        // 컴퓨터 정답 생성
-        // 플레이어 입력값 받기
-        // 플레이어 입력값 유효성 검사
-        // 판정 실시
-        // 게임 종료 문구 출력
-        // 재시작 혹은 최종 종료 문구 출력
-        // 플레이어 입력값 받기
+        Game game = new Game();
+        game.play();
     }
 }
