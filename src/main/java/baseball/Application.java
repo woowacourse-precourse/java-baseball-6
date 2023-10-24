1f89d54f6079ae333de566ad69e35944a4781507
Tiger <skdlghwns@gmail.com>
2023-10-24 22:22:10 +0900

    주석제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,12 +8,11 @@ import java.util.*;
 public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        String gameState = "1";//1:게임진행, 2:게임종료
-        List<Integer> computer = new ArrayList<>();//컴퓨터 수
-        String input;//유저 입력
-        int ball=0; int strike=0;//볼, 스트라이크 개수
+        String gameState = "1";
+        List<Integer> computer = new ArrayList<>();
+        String input;
+        int ball=0; int strike=0;
 
-        //게임 시작 4.1, 4.2 1일 경우 무한히 재시작, 2일 경우 반복문 탈출
         while(gameState.equals("1")){
             //1.상대방 수 생성
             while (computer.size() < 3){
@@ -23,7 +22,6 @@ public class Application {
                 }
             }
 
-            //2.3자리수 입력 시작
             while(true){
                 System.out.printf("숫자를 입력해주세요 : ");
                 input = Console.readLine();
@@ -40,7 +38,6 @@ public class Application {
                         }
                     }
 
-                    //3. 출력기능
                     if(ball!=0){System.out.printf("%d볼 ",ball);}
                     if(strike!=0){System.out.printf("%d스트라이크",strike);}
                     if(ball==0&&strike==0){System.out.print("낫싱");}
@@ -58,16 +55,14 @@ public class Application {
                 }
             }
 
-            //4. 게임 재시작/종료 선택
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             gameState = Console.readLine();
             if(!gameState.equals("1") && !gameState.equals("2")){
                 throw new IllegalArgumentException("유효하지 않은 입력 발생");// 4.3 1또는 2를 입력하지 않았을 경우
             }
         }
-        
+
     }
-    //2. 사용자 입력이 유효한지 확인하는 메소드
     public static boolean isValidInput(String input){
         if(input == null || input.length()!=3){
             return false;
