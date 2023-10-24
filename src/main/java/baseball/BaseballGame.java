3ec5d35993e9bd709472bd5586dd590debf1f9b2
voyager003 <miraclex000@gmail.com>
2023-10-25 00:04:03 +0900

    Refactor: 메서드 분리 및 네이밍 수정
    start()의 메서드를 다음과 같이 수정
    start()->startGame()로 네이밍 수정
    generateBotNumbers(), executePlayerTurn()로 메서드를 분리

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -7,26 +7,48 @@ import java.util.List;
 
 public class BaseballGame {
 
-    public void start() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        GameStatus gameStatus = GameStatus.ONGOING;
-        BotPlayer botPlayer = new BotPlayer(new ArrayList<>());
-        List<Integer> botNumbers = botPlayer.generateBotNumbers();
+    private GameStatus gameStatus;
+    private BotPlayer botPlayer;
+    private final BaseballUmpire umpire;
 
+    public BaseballGame(GameStatus gameStatus, BotPlayer botPlayer, BaseballUmpire umpire) {
+        this.gameStatus = gameStatus;
+        this.botPlayer = botPlayer;
+        this.umpire = umpire;
+    }
+
+    public void startGame() {
+        List<Integer> botNumbers = generateBotNumbers();
         do {
-            List<Integer> playerNumbers = Player.getPlayerInputNumbers();
-            gameStatus = BaseballUmpire.judgeGameStatus(botNumbers, playerNumbers);
+            executePlayerTurn(botNumbers);
         } while (gameStatus == GameStatus.ONGOING);
         askForRestartOrExit();
     }
 
+    private List<Integer> generateBotNumbers() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        botPlayer = new BotPlayer(new ArrayList<>());
+        return botPlayer.generateBotNumbers();
+    }
+
+    private void executePlayerTurn(List<Integer> botNumbers) {
+        List<Integer> playerNumbers = Player.getPlayerInputNumbers();
+        gameStatus = umpire.getGameStatus(botNumbers, playerNumbers);
+    }
+
     private void askForRestartOrExit() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        String input = Console.readLine();
-        if (input.equals("1")) {
-            start();
-        } else if (input.equals("2")) {
-            System.out.println("게임을 종료합니다.");
+        String playerInput = Console.readLine();
+
+        switch (playerInput) {
+            case "1":
+                startGame();
+                break;
+            case "2":
+                System.out.println("게임을 종료합니다.");
+                break;
+            default:
+                throw new IllegalArgumentException("올바르지 않은 입력입니다. 게임을 종료합니다.");
         }
     }
 }
