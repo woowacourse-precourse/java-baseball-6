7afa0db555f29888cca2eda9f01c6b9bdc82c752
OhJongMin <jmoh7480@gmail.com>
2023-10-25 04:26:56 +0900

    feat:Add OutputView System output method

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,34 @@
+package baseball.view;
+
+import baseball.domain.Computer;
+
+public class OutputView {
+    public static void printStartGame() {
+        System.out.print("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void printGameMessage() {
+        System.out.println();
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
+    public static void printHint() {
+        int ball = Computer.getBall();
+        int strike = Computer.getStrike();
+
+        if (ball == 0 && strike == 0) {
+            System.out.print("낫싱");
+        }
+        if (ball > 0) {
+            System.out.print(ball + "볼 ");
+        }
+        if (strike > 0 && strike < 3) {
+            System.out.print(strike + "스트라이크");
+        }
+    }
+
+    public static void printCollect() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+}
