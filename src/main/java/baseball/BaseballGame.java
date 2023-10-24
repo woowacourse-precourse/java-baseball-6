a1e5192c20c3d9a9ddace38b0cd6961706ada9fa
jungeun-choi <jungeun5.choi@gmail.com>
2023-10-24 17:23:56 +0900

    refactor: 3 strike 및 게임 종료 여부를 체크하는 변수 대신 메서드 사용

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -6,31 +6,32 @@ public class BaseballGame {
 	private final BaseballComputer baseballComputer; // 게임에 필요한 숫자를 생성
 	private final BaseballUmpire baseballUmpire; // 유저가 입력한 숫자를 판정
 
-	private boolean isCorrect; // 게임의 정답을 맞췄는지 여부
-	private boolean isGameEnd; // 게임이 완전히 종료되는지 여부
-
 	public BaseballGame(
 		BaseballView baseballView, BaseballComputer baseballComputer, BaseballUmpire baseballUmpire
 	) {
 		this.baseballView = baseballView;
 		this.baseballComputer = baseballComputer;
 		this.baseballUmpire = baseballUmpire;
-
-		isCorrect = false;
-		isGameEnd = false;
 	}
 
 	public void startBaseballGame() {
 		do {
-			// 컴퓨터 숫자 생성
-			String computerNumber = createComputerNumbers();
-			// 정답 여부 초기화
-			isCorrect = false; 
+			String computerNumber = baseballComputer.createComputerNumbers();
+			System.out.println("테스트 용 컴퓨터 숫자 출력:: " + computerNumber);
 			do {
-				// 게임 진행
-				processBaseballGame(computerNumber);
-			} while (!isCorrect);
-		} while (!isGameEnd);
+				processBaseballGame(computerNumber); // 게임 진행
+			} while (!isThreeStrike());
+		} while (!isGameEnd());
+	}
+
+	private boolean isGameEnd() {
+		// 게임 종료 여부, 1 = 재시작, 2 = 종료
+		return baseballView.inputSelectRestartOrEnd() == 2;
+	}
+
+	private boolean isThreeStrike() {
+		// 3 strike인지 확인
+		return baseballUmpire.getStrike() >= 3;
 	}
 
 	private void processBaseballGame(String computerNumber) {
@@ -55,25 +56,6 @@ public class BaseballGame {
 			}
 		}
 
-		// 게임 종료 메세지
-		if (strike == 3) {
-			baseballView.printFinishGame();
-			isCorrect = true;
-
-			// 유저가 게임 재시작, 종료 선택
-			baseballView.printSelectRestartOrEnd();
-			int selectNumber = baseballView.inputSelectRestartOrEnd();
-			if (selectNumber == 2) {
-				isGameEnd = true;
-			}
-		}
-	}
-
-	private String createComputerNumbers() {
-		// 숫자 생성
-		String computerNumber = baseballComputer.createComputerNumbers();
-		System.out.println("테스트 용 컴퓨터 숫자 출력:: " + computerNumber);
-		return computerNumber;
 	}
 
 	private boolean isUmpireResultsNothing(int ball, int strike) {
