a6f060c3ad59e02405f1ef2f6207d5fed1193d00
gyahn <pop2413@gmail.com>
2023-10-24 20:09:36 +0900

    fix: 난수 생성 수정

--- src/main/java/baseball/RandomNumber.java
+++ src/main/java/baseball/RandomNumber.java
@@ -2,17 +2,22 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class RandomNumber {
     public int randomNum() {
         return Randoms.pickNumberInRange(1,9);
     }
 
     public int[] createRanArr(){
-        int[] ranArr = new int[3];
-
-        for(int i = 0;i<ranArr.length;i++){
-            ranArr[i] = randomNum();
+//        int[] ranArr = new int[3];
+        Set<Integer> numSet = new HashSet<>();
+        while(numSet.size()<3){
+            int ranNum = randomNum();
+            numSet.add(ranNum);
         }
+        int[] ranArr = numSet.stream().mapToInt(Integer::intValue).toArray();
         return ranArr;
     }
 }
