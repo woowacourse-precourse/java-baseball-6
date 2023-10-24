6777b398464af2f2255805ac8f90d06fcb7867d7
ehdwoKIM <kinbell19@gmail.com>
2023-10-24 22:28:46 +0900

    [feat] 게임 입출력 컨트롤러 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
 
+import baseball.controllers.GameController;
+import baseball.views.OutputView;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        OutputView.printGameStart();
+        GameController baseballGame = new GameController();
+        baseballGame.start();
     }
 }
