8ccbb2a77222952604329d756206732da4111ccf
hansg931 <hansg0509@cau.ac.kr>
2023-10-25 00:26:34 +0900

    feat: 난수 생성 메소드 추가

---
+++ src/main/java/baseball/model/NumberGenerator.java
@@ -0,0 +1,19 @@
+package baseball.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+import baseball.util.Constant;
+
+public class NumberGenerator {
+	private final List<Integer> computerNumber = new ArrayList<>();
+
+	private void generateNumber() {
+		while (computerNumber.size() < Constant.NUMBER_LENGTH) {
+			int randomNumber = Randoms.pickNumberInRange(Constant.MIN_NUMBER, Constant.MAX_NUMBER);
+			if (!computerNumber.contains(randomNumber)) {
+				computerNumber.add(randomNumber);
+			}
+		}
+	}
+}
