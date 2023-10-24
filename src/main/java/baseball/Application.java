63c43c5a991d9a2b25cad6535baf02a765ad49ff
ssunnykku <ssunnykku@gmail.com>
2023-10-24 12:45:49 +0900

    Refactor: 함수명 변경, Exception 메소드 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -34,36 +34,50 @@ public class Application {
 
     public static void game(List<Integer> computer) {
           String input = inputNumbers();
-          if(input.length() != 3) {
-                throw new IllegalArgumentException("숫자 3개를 입력하세요.");
-            }
-            gameResult(computer, input);
+          int len = input.length();
+          checkInputSize(len);
+          compare(computer, input);
     }
 
-    public static void gameResult(List<Integer> computer, String input) {
+    public static void compare(List<Integer> computer, String input) {
         int cntStrike = 0;
         int cntBall = 0;
-        HashSet<Integer> checkOverlap = new HashSet<>();
+        HashSet<Integer> checkRepetition = new HashSet<>();
+
         for(int i=0; i<computer.size(); i++) {
             int num = stringToInt(input, i);
-            checkOverlap.add(num);
-            if (num<1 | num>9) throw new IllegalArgumentException("1~9를 이용한 3자리의 수를 입력하세요.");
-            if((num) == computer.get(i)) {
-                cntStrike += 1;
-            } else if (computer.contains(num)) {
-                cntBall += 1;
-            }
+            numberRangeValidation(num);
+            checkRepetition.add(num);
+
+            if((num) == computer.get(i)) cntStrike += 1;
+            else if (computer.contains(num)) cntBall += 1;
+
         }
-        if(checkOverlap.size() != 3) {
+        checkOverlap(checkRepetition);
+        countResult(cntStrike, cntBall);
+        gameResult(computer, cntStrike, cntBall);
+
+    }
+
+    public static void checkInputSize(int len) {
+        if(len != 3) {
+            throw new IllegalArgumentException("숫자 3개를 입력하세요.");
+        }
+    }
+
+    public static void checkOverlap(HashSet<Integer> array) {
+        if(array.size() != 3) {
             throw new IllegalArgumentException("서로 다른 3자리의 수를 입력하세요.");
         }
-        count(cntStrike, cntBall);
+    }
 
-        if (cntStrike == 3 && cntBall == 0) {
-            restartOrNot();
-        } else {
-            game(computer);
-        }
+    public static void numberRangeValidation(int num) {
+        if (num<1 | num>9) throw new IllegalArgumentException("1~9를 이용한 3자리의 수만 입력할 수 있습니다.");
+    }
+
+    public static void gameResult(List<Integer> computer, int cntStrike, int cntBall) {
+        if (cntStrike == 3 && cntBall == 0) restartOrNot();
+        else game(computer);
     }
 
     public static int stringToInt(String str, int i) {
@@ -71,7 +85,7 @@ public class Application {
         return num;
     }
 
-    public static void count(int cntStrike, int cntBall) {
+    public static void countResult(int cntStrike, int cntBall) {
         if (cntStrike == 0 && cntBall == 0) System.out.println("낫싱");
         else if (cntStrike == 0 && cntBall > 0 ) System.out.println(cntBall + "볼");
         else if (cntStrike > 0 && cntBall > 0) System.out.println(cntBall + "볼 "+ cntStrike + "스트라이크");
@@ -82,16 +96,21 @@ public class Application {
 
     }
 
-    public static String inputResultNumber() {
+    public static String inputRestartStatus() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         return Console.readLine();
     }
 
     public static void restartOrNot() {
-        String input = inputResultNumber();
+        String input = inputRestartStatus();
         int num = stringToInt(input,0);
         if (num == 1) gameRestart();
-        else if (num != 2) throw new IllegalArgumentException("1,2 이외의 숫자는 입력할 수 없습니다.");
+        else if (num == 2) return;
+        else inputRestartStatusValidation();
+    }
+
+    public static void inputRestartStatusValidation() {
+        throw new IllegalArgumentException("1,2 이외의 값은 입력할 수 없습니다.");
     }
 
     public static void gameRestart() {
