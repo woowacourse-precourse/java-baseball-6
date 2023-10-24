07f1a722a2c5f106d852397198b8d85783ef4fb6
elizabethgim <yujingim@naver.com>
2023-10-24 22:40:42 +0900

    feat: 3스트라이크 체크

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -47,6 +47,13 @@ public class Game {
         System.out.println();
     }
 
+    public boolean checkStriked(){
+        if(strike == 3){
+            return true;
+        }
+        return false;
+    }
+
     public void start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
         System.out.print("숫자를 입력해주세요 : ");
