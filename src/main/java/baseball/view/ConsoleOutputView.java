07a70c9ed34bc07c888be58a0019e14185f269f6
eora21 <eora21@naver.com>
2023-10-24 21:14:21 +0900

    refactor: 💡 수정 새 게임 시작 메시지 private로 변경

--- src/main/java/baseball/view/ConsoleOutputView.java
+++ src/main/java/baseball/view/ConsoleOutputView.java
@@ -6,7 +6,7 @@ import java.util.StringJoiner;
 public class ConsoleOutputView implements OutputView {
     private static final String GAME_START = "숫자 야구 게임을 시작합니다.";
     private static final String REQUEST_NUMBER = "숫자를 입력해주세요 : ";
-    public static final String MORE_GAME = "게임을 새로 시작하려면 %s, 종료하려면 %s를 입력하세요.";
+    private static final String MORE_GAME = "게임을 새로 시작하려면 %s, 종료하려면 %s를 입력하세요.";
     private static final String GOOD_GAME = "개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     @Override
