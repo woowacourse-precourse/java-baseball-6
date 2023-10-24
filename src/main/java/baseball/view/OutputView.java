5ad65dd838da8856042bc20def7a113a1b9024a0
박승민 <tmddudals369@naver.com>
2023-10-25 02:35:14 +0900

    refactor(output): 게임 결과 출력 메세지 윈시값 변수로 변경

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -7,6 +7,10 @@ public class OutputView {
     private static final String USER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static final String GAME_OVER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String NOTHING = "낫싱";
+    private static final String BALL = "%d볼\n";
+    private static final String STRIKE = "%d스트라이크\n";
+    private static final String BALL_AND_STRIKE = "%d볼 %d스트라이크\n";
 
     public void printGameStartMessage() {
         System.out.println(GAME_START_MESSAGE);
@@ -18,19 +22,19 @@ public class OutputView {
 
     public void printScore(Score score) {
         if (score.getBall() == 0 && score.getStrike() == 0){
-            System.out.println("낫싱");
+            System.out.println(NOTHING);
             return;
         }
         if (score.getStrike() == 0){
-            System.out.printf("%d볼%n", score.getBall());
+            System.out.printf(BALL, score.getBall());
             return;
         }
         if (score.getBall() == 0){
-            System.out.printf("%d스트라이크\n", score.getStrike());
+            System.out.printf(STRIKE, score.getStrike());
             return;
         }
 
-        System.out.printf("%d볼 %d스트라이크\n", score.getBall(), score.getStrike());
+        System.out.printf(BALL_AND_STRIKE, score.getBall(), score.getStrike());
     }
 
     public void printGameRestartMessage() {
