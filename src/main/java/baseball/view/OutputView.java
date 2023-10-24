e28fb09b9fffc83691b4b18790654aa5756dd4bc
PARKHWANYEONG <welcome0828@naver.com>
2023-10-25 02:37:46 +0900

    refactor: 게임 결과 출력 메서드 중복 문자열 상수 사용 및 코드 단순화

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,30 +4,29 @@ import baseball.domain.GameResult;
 
 public class OutputView {
 
-    private static final String LINE_CHANGE = "\n";
     private static final String GAME_START = "숫자 야구 게임을 시작합니다.";
     private static final String NOTHING = "낫싱";
-    private static final String BALL = "%d볼";
-    private static final String STRIKE = "%d스트라이크";
+    private static final String BALL_FORMAT = "%d볼 ";
+    private static final String STRIKE_FORMAT = "%d스트라이크";
     private static final String GAME_SUCCESS = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     public static void gameStart() {
         System.out.println(GAME_START);
     }
     public static void printGameResult(GameResult result) {
+        String hint = "";
+
         if (result.isNothing()) {
             System.out.println(NOTHING);
-        }
-        if (result.hasBall() && result.hasStrike()) {
-            System.out.printf(BALL+" "+STRIKE+LINE_CHANGE,result.getBall(),result.getStrike());
             return;
         }
         if (result.hasBall()) {
-            System.out.printf(BALL+LINE_CHANGE,result.getBall());
+            hint += String.format(BALL_FORMAT, result.getBall());
         }
         if (result.hasStrike()) {
-            System.out.printf(STRIKE+LINE_CHANGE,result.getStrike());
+            hint += String.format(STRIKE_FORMAT, result.getStrike());
         }
+        System.out.println(hint);
     }
 
     public static void gameSuccess() {
