029f04160dcd1b99036700902ff2182ca8d7d730
liarreez <liarreez@gmail.com>
2023-10-24 15:58:17 +0900

    Style(InputView) : 불필요한 줄바꿈 제거

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String INPUT_NUMBERS_MESSAGE = "숫자를 입력해주세요 : ";
-    private static final String INPUT_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
+    private static final String INPUT_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     private InputView() {}
 
