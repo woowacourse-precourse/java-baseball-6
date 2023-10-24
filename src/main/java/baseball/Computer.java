8eeba2d11e779a1c51ed2cf7450fc24bf7730701
fnzl54 <rnjscksdud1@naver.com>
2023-10-24 16:16:44 +0900

    feat: computer 객체 생성 및 사용에 필요한 함수 작성

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,41 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+  private List<Integer> answer;
+
+  /**
+   * 서로 다른 3개의 정수를 List<Integer>형의 answer로 저장
+   */
+  public void makeAnswer() {
+    answer = new ArrayList<>();
+
+    while (answer.size() != 3) {
+      int randomNumber = Randoms.pickNumberInRange(1, 9);
+      if (!(answer.contains(randomNumber))) {
+        answer.add(randomNumber);
+      }
+    }
+  }
+
+  public Result getResult(List<Integer> input) {
+    Result result = new Result();
+
+    for (int i = 0; i < 3; i++) {
+
+      if (answer.contains(input)) {
+        if (answer.get(i) == input.get(i)) {
+          result.addStrike();
+        } else {
+          result.addBall();
+        }
+      }
+    }
+
+    return result;
+  }
+
+}
