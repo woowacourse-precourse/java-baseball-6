040483c1816ae58cef754e2ad954891a947f18d8
이정우 <acg6138@naver.com>
2023-10-24 11:22:39 +0900

    refactor: User 클래스 구현 및 Computer의 기능 축소

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,33 +1,14 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static baseball.Baseball.MAX_NUMBER;
-import static baseball.Baseball.MIN_NUMBER;
-import static baseball.Baseballs.BASEBALLS_SIZE;
-
 public class Computer {
     private Baseballs baseballs;
 
-    public Baseballs createBaseballs() {
-        try {
-            List<Baseball> baseballs = new ArrayList<>();
-            for (int i = 0; i < BASEBALLS_SIZE; i++) baseballs.add(new Baseball(Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER)));
-            return new Baseballs(baseballs);
-        } catch (IllegalArgumentException e) {
-            return createBaseballs();
-        }
+    public void createBaseballs() {
+        baseballs = BaseballsGenerator.createBaseballsByRandomNumber();
     }
 
-    public Result getResult(Baseballs baseballs) {
-        return new Result(this.baseballs.countStrike(baseballs), this.baseballs.countBall(baseballs));
-    }
-
-    public void resetBaseballs() {
-        this.baseballs = createBaseballs();
+    public Baseballs getBaseballs() {
+        return baseballs;
     }
 
 }
