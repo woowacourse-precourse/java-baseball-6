c84c9dab1af82b0225614470180f80be6e11dd6c
fnzl54 <rnjscksdud1@naver.com>
2023-10-24 17:54:15 +0900

    feat: game 실행을 위한 코드 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            Game game = new Game();
+            game.startGame();
+        } finally {
+            Console.close();
+        }
     }
 }
