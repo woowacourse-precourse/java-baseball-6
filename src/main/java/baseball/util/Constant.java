cf83af48d14a965a0678791a58272f799abeddf9
Hayoon <hy97@sju.ac.kr>
2023-10-24 19:45:56 +0900

    feat(Constant): 상수 클래스 생성

---
+++ src/main/java/baseball/util/Constant.java
@@ -0,0 +1,6 @@
+package baseball.util;
+
+public class Constant {
+    public static final String START_GAME = "숫자 야구 게임을 시작합니다.";
+    public static final String INPUT_NUMBERS = "숫자를 입력해주세요 : ";
+}
