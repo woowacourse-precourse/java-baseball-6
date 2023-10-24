90a24f8a5da3944043de19dab76973a687456bdb
JangHongJoon <wkdghdwns19969@gmail.com>
2023-10-25 00:02:48 +0900

    Refactor : 기호 상수로 치환

--- src/main/java/baseball/validation/ValidNumberPicker.java
+++ src/main/java/baseball/validation/ValidNumberPicker.java
@@ -2,8 +2,10 @@ package baseball.validation;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 public class ValidNumberPicker {
+    private final Integer rangeStartNumber = 1;
+    private final Integer rangeEndNumber = 9;
     public int pickValidNumber(List<Integer> validBallsList) {
-        int randomNumber = Randoms.pickNumberInRange(1,9);
+        int randomNumber = Randoms.pickNumberInRange(rangeStartNumber,rangeEndNumber);
         if (validBallsList.contains(randomNumber)){
             return pickValidNumber(validBallsList);
         }
