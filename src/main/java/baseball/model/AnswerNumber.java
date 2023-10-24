525e530fe8245692816dfd6931f248f238a156ae
jaehee <y2hjjh@naver.com>
2023-10-24 23:22:24 +0900

    refactor: 자릿수 상수 처리

--- src/main/java/baseball/model/AnswerNumber.java
+++ src/main/java/baseball/model/AnswerNumber.java
@@ -4,6 +4,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.*;
 public class AnswerNumber {
+
+    private static final int NUMBER_OF_DIGITS = 3;
     private List<Integer> randomNumber = new ArrayList<>();
 
     public List<Integer> getRandomNumber() {
@@ -12,7 +14,7 @@ public class AnswerNumber {
 
     public void setRandomNumber() {
         List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
+        while (computer.size() < NUMBER_OF_DIGITS) {
             int random = Randoms.pickNumberInRange(1, 9);
             if (!computer.contains(random)) {
                 computer.add(random);
