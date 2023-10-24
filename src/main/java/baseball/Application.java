a802f590e0f20d0e855e6eaa6117a950dd81d6e0
gahee99 <u1879@sookmyung.ac.kr>
2023-10-25 00:10:57 +0900

    feat: MVC리펙토링- 리팩토링 결과에 따른 main파일 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,19 @@
 package baseball;
 
-import baseball.object.BaseBallGame;
+import baseball.controller.BaseBallGameController;
+import baseball.model.TryAgain;
+import baseball.model.BaseBallGame;
+import baseball.valid.InputValidator;
+import baseball.view.ConsoleView;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         BaseBallGame baseBallGame = new BaseBallGame();
-        baseBallGame.start();
+        InputValidator inputValidator = new InputValidator();
+        ConsoleView consoleView = new ConsoleView();
+        TryAgain tryAgain = new TryAgain();
+        BaseBallGameController baseBallGameController = new BaseBallGameController(consoleView, baseBallGame, inputValidator, tryAgain);
+        baseBallGameController.startGame();
     }
 }
