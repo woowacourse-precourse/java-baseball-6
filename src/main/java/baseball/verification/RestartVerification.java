dabbc9f727c555092f84d536b56ceb7ee3602a4a
dhl1924 <dhl1924@naver.com>
2023-10-24 14:08:00 +0900

    feat: try-catch로 1 or 2만 입력하도록 기능 추가

--- src/main/java/baseball/verification/RestartVerification.java
+++ src/main/java/baseball/verification/RestartVerification.java
@@ -19,7 +19,7 @@ public class RestartVerification {
                 }
                 System.out.println("잘못된 입력입니다. 1(재시작) 또는 2(종료)를 입력해주세요.");
             } catch (NumberFormatException e) {
-                System.out.println("잘못된 입력입니다. 숫자를 입력해주세요.");
+                System.out.println("잘못된 입력입니다. 숫자(1 or 2)를 입력해주세요.");
             }
         }
     }
