eb3ddd7b25f8f2dc709542b798f4f28e3006553c
wooni97 <jhw970714@gmail.com>
2023-10-24 17:18:18 +0900

    refactor(User): extract input validation into a seperate method
    - Moved input validation and error handling into a separate function to improve code modularity and maintainability.

--- src/main/java/baseball/entity/User.java
+++ src/main/java/baseball/entity/User.java
@@ -2,7 +2,6 @@ package baseball.entity;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import camp.nextstep.edu.missionutils.Console;
 public class User {
     private List<Integer> userNumber = new ArrayList<>();
@@ -10,19 +9,23 @@ public class User {
     public List<Integer> inputNumber(){
         String input = Console.readLine();
 
-        //사용자 입력이 세자리가 아닐 시
+        validateInput(input, userNumber);
+
+        return userNumber;
+    }
+
+    public void validateInput(String input, List<Integer> userNumber) {
         if (input.length() != 3) {
             throw new IllegalArgumentException();
         }
 
         char[] charArr = input.toCharArray();
-        List<Integer> inputList = new ArrayList<>();
 
         for (char c: charArr) {
             if (Character.isDigit(c)) {
                 int number = Character.getNumericValue(c);
-                if (number >= 0 && !inputList.contains(number)) {
-                    inputList.add(number);
+                if (number >= 0 && !userNumber.contains(number)) {
+                    userNumber.add(number);
                 } else { //1 - 9 사이의 수가 아니거나 중복 입력 시
                     throw new IllegalArgumentException();
                 }
@@ -30,12 +33,6 @@ public class User {
                 throw new IllegalArgumentException();
             }
         }
-
-        if (inputList.size() != 3) {
-            throw new IllegalArgumentException();
-        }
-
-        return inputList;
     }
 
 }
