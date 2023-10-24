e08cd6f1e8964aef53f9c75aa18569703428dada
SunHyeYoon <haehae96@jbnu.ac.kr>
2023-10-24 21:35:37 +0900

    refactor: 매직 넘버를 상수로 치환

--- src/main/java/baseball/utils/Validation.java
+++ src/main/java/baseball/utils/Validation.java
@@ -4,25 +4,33 @@ import java.util.Collections;
 import java.util.List;
 
 public class Validation {
+
+    public static final int THREE_DIGITS = 3;
+    public static final int MAX_FREQUENCY = 1;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 9;
+    public static final String RESTART = "1";
+    public static final String END = "2";
+
     private Validation() {
     }
 
-    public static void validateNumber(List<Integer> input) {
-        if (input.size() != 3) {
+    public static void validateInputNumber(List<Integer> input) {
+        if (input.size() != THREE_DIGITS) {
             throw new IllegalArgumentException("3자리의 수를 입력해 주세요.");
         }
         for (Integer number : input) {
-            if (Collections.frequency(input, number) != 1) {
+            if (Collections.frequency(input, number) > MAX_FREQUENCY) {
                 throw new IllegalArgumentException("서로 다른 수를 입력해 주세요.");
             }
-            else if (number < 1 || number > 9) {
-                throw new IllegalArgumentException("1부터 9 사이의 값을 입력해 주세요.");
+            else if (number < MIN_NUMBER || number > MAX_NUMBER) {
+                throw new IllegalArgumentException("1부터 9 사이의 정수를 입력해 주세요.");
             }
         }
     }
 
-    public static void validateRestart(String string) {
-        if (!string.equals("1") && !string.equals("2")) {
+    public static void validateRestartNumber(String string) {
+        if (!string.equals(RESTART) && !string.equals(END)) {
             throw new IllegalArgumentException("1 또는 2를 입력해 주세요.");
         }
     }
