299f5f1d6b10ca528e5e760b216ef6c1bc3fd85f
Hanjjong <tim4974@naver.com>
2023-10-24 21:41:19 +0900

    [feat] : create Player generater

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,11 +1,16 @@
 package baseball;
 
 import java.security.PublicKey;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
     private List<Integer> Numbers;
 
+    public Player()
+    {
+        Numbers = new ArrayList<>();
+    }
     public void setNumbers(List<Integer> Number) {
         this.Numbers = Number;
     }
