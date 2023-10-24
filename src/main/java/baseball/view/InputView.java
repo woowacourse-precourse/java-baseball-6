eb7bc85028effb25d9fdc772be08875ac63cadee
kmw2378 <rlarla677@gmail.com>
2023-10-24 14:19:37 +0900

    feat: InputView 에 재시작 여부 입력 메소드 추가 및 검증 메소드 추출 재시작 여부를 입력받을 때 사용되는 메소드를 추가하였습니다. 입력 메소드에서 사용되는 공통 검증 로직을 메소드로 추출하였습니다.

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,20 +1,41 @@
 package baseball.view;
 
 import baseball.common.exception.input.InputException;
+import baseball.dto.request.RestartAnswerRequest;
 import baseball.dto.request.UserNumberRequest;
 import camp.nextstep.edu.missionutils.Console;
 
 import static baseball.common.exception.input.InputErrorCode.INVALID_INPUT_VALUE;
+import static baseball.view.message.GameMessage.CONTINUE_MESSAGE;
 import static baseball.view.message.GameMessage.QUESTION_MESSAGE;
 
 public class InputView {
     public static UserNumberRequest requestUserNumber() {
         System.out.println(QUESTION_MESSAGE);
+        final String arg = inputAndCheckNullOrBlank();
+        return new UserNumberRequest(arg);
+    }
+
+    public static RestartAnswerRequest requestContinueAnswer() {
+        try {
+            System.out.println(CONTINUE_MESSAGE);
+            final String arg = inputAndCheckNullOrBlank();
+            return new RestartAnswerRequest(arg);
+        } catch (InputException e) {
+            System.out.println(e.getMessage());
+            return requestContinueAnswer();
+        }
+    }
+
+    private static String inputAndCheckNullOrBlank() {
         final String arg = Console.readLine();
+        checkNullOrBlank(arg);
+        return arg;
+    }
+
+    private static void checkNullOrBlank(final String arg) {
         if (arg == null || arg.isBlank()) {
             throw new InputException(INVALID_INPUT_VALUE);
         }
-
-        return new UserNumberRequest(arg);
     }
 }
