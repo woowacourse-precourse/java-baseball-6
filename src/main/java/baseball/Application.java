dce3a1d553d0912f075f1ac892c5d45cdfeda1e3
www-author <dream970215@naver.com>
2023-10-24 17:56:47 +0900

    :sparkles: feat: 야구 게임 컨트롤러 호출 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import baseball.controller.BaseballController;
+
 public class Application {
-    public static void main(String[] args) {
+
+    public static void main(String[] args)  {
         // TODO: 프로그램 구현
+        BaseballController baseballGame = new BaseballController();
+        baseballGame.start();
     }
+
 }
