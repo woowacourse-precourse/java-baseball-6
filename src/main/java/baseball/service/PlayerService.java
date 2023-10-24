e3e51aed683e700dc739919a9112be759321d714
minseo  jo <liging12@naver.com>
2023-10-24 22:46:30 +0900

    refactor: Converter 기능을 PlayerService 에 넣을지 고민 중
    convertStringToBaseballs() 함수는 util 에 있지만 사실, 플레이어 입력에 대해서만 관여한다. 그래서 util 패키지의 Converter 를 없애고 PlayerService 에 넣는게 더 좋아보임.

--- src/main/java/baseball/service/PlayerService.java
+++ src/main/java/baseball/service/PlayerService.java
@@ -2,7 +2,10 @@ package baseball.service;
 
 import baseball.domain.Baseballs;
 import baseball.domain.Player;
-import baseball.util.Converter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class PlayerService {
 
@@ -17,6 +20,16 @@ public class PlayerService {
     }
 
     private Baseballs generateBaseballs(String playerGuess) {
-        return new Baseballs(Converter.convertStringToBaseballs(playerGuess));
+        return new Baseballs(convertStringToBaseballs(playerGuess));
+    }
+
+    private List<Integer> convertStringToBaseballs(String playerGuess) {
+        if (playerGuess == null || playerGuess.isBlank()) {
+            throw new IllegalArgumentException("입력값은 공백이 들어갈 수 없습니다.");
+        }
+
+        return IntStream.range(0, playerGuess.length())
+                .mapToObj(index -> (playerGuess.charAt(index) - '0'))
+                .collect(Collectors.toList());
     }
 }
