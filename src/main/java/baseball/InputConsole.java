3ce3461d3dad94451bebc8e27ae6ed4d91314335
DoohyunHwang97 <ghkdengus97@gmail.com>
2023-10-24 18:32:02 +0900

    refactor(InputConsole): seperate Exceptions to each validation method
    change method name
    change method return type(boolean -> void) to throw Exception depending on their conditions

--- src/main/java/baseball/InputConsole.java
+++ src/main/java/baseball/InputConsole.java
@@ -9,9 +9,7 @@ import java.util.Objects;
 public class InputConsole {
     public String readPrediction(){
         String input = Console.readLine();
-        if (isFormValid(input)){
-            return input;
-        } else throw new IllegalArgumentException("잘못된 예측값 포맷입니다.");
+        return getValidated(input);
     }
 
     public String readUserIntentionForNewGame(){
@@ -21,26 +19,27 @@ public class InputConsole {
         } else throw new IllegalArgumentException("게임 재시작 여부는 1과 2만 받을 수 있습니다.");
     }
 
-    private boolean isFormValid(String input){
-        return isLengthThree(input) && containsOnlyDigit(input) && notContainsDuplicatedNumber(input);
+    private String getValidated(String input){
+        validateLength(input);
+        validateDigitOnly(input);
+        validateNoDuplication(input);
+        return input;
     }
 
-    private boolean isLengthThree(String input){
-        if (input.length() != 3) throw new IllegalArgumentException("예측값 길이는 3이어야 합니다.");
-        else return true;
+    private void validateLength(String input){
+        if (input.length() != 3) throw new IllegalArgumentException("입력길이는 3이어야 합니다.");
     }
 
-    private boolean containsOnlyDigit(String input){
+    private void validateDigitOnly(String input){
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
             if (!Character.isDigit(c)) {
-                return false;
+                throw new IllegalArgumentException("숫자가 아닌 입력이 있습니다.");
             }
         }
-        return true;
     }
 
-    private boolean notContainsDuplicatedNumber(String input){
+    private void validateNoDuplication(String input){
         HashSet<Integer> result = new HashSet<>();
 
         for (int i = 0; i < input.length(); i++) {
@@ -51,6 +50,6 @@ public class InputConsole {
             }
         }
 
-        return result.size() == 3;
+        if (result.size() != 3) throw new IllegalArgumentException("중복된 숫자가 있습니다.");
     }
 }
