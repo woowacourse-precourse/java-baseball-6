c70294e1b914ea977ce43c45a94f0675dbb5af35
PJH <parkjh0858@gmail.com>
2023-10-25 01:24:02 +0900

    feat:사용자 입력 기능 추가

---
+++ src/main/java/player/User.java
@@ -0,0 +1,11 @@
+package player;
+
+import model.NumberInput;
+
+public class User {
+    private NumberInput userNumber;
+
+    public User(String inputNumber) {
+        userNumber = new NumberInput(inputNumber);
+    }
+}
