eae578889e3dc4f552c4bcdf356ae041c03287c4
Woo Joo Chae <enaenen@naver.com>
2023-10-24 20:56:33 +0900

    REFACTOR: 접근 지정자 public to private 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,14 +3,14 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-    름
+    
     private static final String STARTING_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String GAMEOVER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
             "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static final int EXIT_CODE = 2;
     private static final int ALL_STRIKE = 3;
 
-    public static void inputValidate(String line) {
+    private static void inputValidate(String line) {
         if (line == null || line.length() != 1 || !Character.isDigit(line.charAt(0)) || line.charAt(0) > '2') {
             throw new IllegalArgumentException();
         }
