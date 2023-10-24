b9b12f6c169862458d99ba79aba5e25a70431653
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-10-25 00:09:27 +0900

    [FEAT] 사용자 기능 구현
    사용자 기능 구현 : 사용자 입력 리스트로 변경, 길이가 3인지 체크, 0인지 체크 해주는 함수 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,6 +10,14 @@ public class Application {
         System.out.println("숫자 야구 게임을 시작합니다.");
 
         List<Integer> computerNumberList = computerRanNumList();
+
+        boolean inGame = true;
+
+        while(inGame) {
+            System.out.println("숫자를 입력해주세요 : ");
+            String userInput = Console.readLine();
+            List<Integer> userNumberList = userInputNumList(userInput);
+        }
     }
 
     // computer
@@ -23,6 +31,34 @@ public class Application {
         }
         return computerNumberList;
     }
+    //user
+    private static List<Integer> userInputNumList(String userInput){
+        List<Integer> userInputNumberList = new ArrayList<>();
+        checkLength(userInput);
+        for(String splitUserNumber : userInput.split("")){
+            if(!splitUserNumber.isEmpty()){
+                userInputNumberList.add(Integer.parseInt(splitUserNumber));
+            }
+        }
+        for(int i = 0; i < userInputNumberList.size(); i++){
+            checkZero(userInputNumberList.get(i));
+        }
+        return userInputNumberList;
+    }
+
+    private static void checkLength(String inputNumber) {
+        if (inputNumber.length() != 3){
+            throw new IllegalArgumentException("3개의 숫자를 입력하세요.");
+        }
+    }
+
+    private static void checkZero(int number){
+        if (number <= 0){
+            throw new IllegalArgumentException("1부터 9사이의 숫자만 입력하세요.");
+        }
+    }
+
+
 }
 
 
