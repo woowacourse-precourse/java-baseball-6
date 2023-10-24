b852c0a83c5f8218d7ee5b7ae522fd0e93cc14ef
Annjieun <dkslwdms2121@naver.com>
2023-10-24 11:48:21 +0900

    refactor: convertStringToIntList() 메서드 행위의 가독성 높이기

--- src/main/java/baseball/utils/Utils.java
+++ src/main/java/baseball/utils/Utils.java
@@ -6,8 +6,9 @@ import java.util.List;
 public class Utils {
     public static List<Integer> convertStringToIntList(String input) {
         List<Integer> playerNumber = new ArrayList<>();
+        String[] numbers = input.split("");
 
-        for (String number : input.split("")) {
+        for (String number : numbers) {
             playerNumber.add(Integer.parseInt(number));
         }
 
