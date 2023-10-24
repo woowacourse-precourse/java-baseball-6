49f5cb7dbcf4564e9d567a140c9a873a20717819
MyStraw <kikongdosa@gmail.com>
2023-10-24 20:37:30 +0900

    10.24 (2)일단 해보자
    야구게임을 일단 구현하는걸 목표로 뼈대잡기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,10 +3,12 @@ package baseball;
 import java.util.ArrayList;
 import java.util.List;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+    	
 
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
@@ -15,8 +17,65 @@ public class Application {
                 computer.add(randomNumber);
             }
         }
-
-
-
+        
+        System.out.println(computer);   
+        
+        
+        int strike = 0;
+        int ball = 0;
+        
+        int firstNumber = computer.get(0);
+        int secondNumber = computer.get(1);
+        int thirdNumber = computer.get(2);
+        
+        char first = Integer.toString(firstNumber).charAt(0);
+        char second = Integer.toString(secondNumber).charAt(0);
+        char third = Integer.toString(thirdNumber).charAt(0);     
+        
+        
+        while(strike != 3) {
+        	String myNumber = Console.readLine();        	
+        	
+        	if (first == myNumber.charAt(0)) {
+        		strike++;
+        	}    
+        	
+        	if (second == myNumber.charAt(1)) {
+        		strike++;
+        	}  
+        	
+        	if (third == myNumber.charAt(2)) {
+        		strike++;
+        	}  
+        	
+        	if (first == myNumber.charAt(1)) {
+        		ball++;
+        	}    
+        	
+        	if (first == myNumber.charAt(2)) {
+        		ball++;
+        	}    
+        	
+        	if (second == myNumber.charAt(0)) {
+        		ball++;
+        	}    
+        	
+        	if (second == myNumber.charAt(2)) {
+        		ball++;
+        	}    
+        	
+        	if (third == myNumber.charAt(0)) {
+        		ball++;
+        	}    
+        	
+        	if (third == myNumber.charAt(1)) {
+        		ball++;
+        	}    
+        	
+        	System.out.println(strike);
+        	System.out.println(ball);
+        	
+        }    
+        
     }
 }
