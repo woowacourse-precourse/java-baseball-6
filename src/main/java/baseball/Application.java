4db73bd6710759d6c512a1aac8c5a1eb8dc867e4
Beomsu <gobeumsu@gmail.com>
2023-10-25 00:01:29 +0900

    feat(Application): Implement main game loop
    - 메인 게임 루프를 구현하여 게임을 반복적으로 실행할 수 있게 함
    - 게임이 끝난 후에 사용자에게 게임을 계속할지 묻는 기능 추가.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,26 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import baseball.Controller.GameRunner;
+import baseball.domain.Baseball;
+import baseball.utils.Utils;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        boolean isContinue = true;
+
+        while (isContinue) {
+            Baseball baseball = new Baseball(Utils.createRandomNumberList(3));
+            GameRunner gameRunner = new GameRunner(baseball);
+            gameRunner.run();
+            isContinue = askContinue();
+        }
+        System.out.println("프로그램을 종료합니다.");
+    }
+
     private static boolean askContinue() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
