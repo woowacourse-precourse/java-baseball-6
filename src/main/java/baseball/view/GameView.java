41ba3f06e6a96a6aa1938c6fdefda3c6bd8b40f2
aeyongdodam <kikititi0510@gmail.com>
2023-10-24 12:47:20 +0900

    refactor(GameView): seperate input method

--- src/main/java/baseball/view/GameView.java
+++ src/main/java/baseball/view/GameView.java
@@ -8,14 +8,17 @@ public class GameView {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    public String getInput() {
+    public String getStartInput() {
         System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
         return input;
     }
 
-    public String endBaseBall() {
+    public void endBaseBall() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public String getEndInput() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String input = Console.readLine();
         return input;
