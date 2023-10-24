22b2e145c43886ef92560c0d0c09832ff2c880f1
HongChaa <jun761_1@naver.com>
2023-10-24 11:07:51 +0900

    feat(Player): 사용자가 입력한 수를 리스트에 담아 리턴하는 메서드 추가

--- src/main/java/baseball/model/Player.java
+++ src/main/java/baseball/model/Player.java
@@ -1,16 +1,26 @@
 package baseball.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
 
     private List<Integer> playerNumberList;
 
-    public List<Integer> getPlayerNumberList() {
+    public List<Integer> getPlayerNumberList(String playerNumber) {
+
+        playerNumberList = new ArrayList<>();
+
+        for (int i = 0; i < playerNumber.length(); i++) {
+            playerNumberList.add(i, convertCharacterToInteger(playerNumber, i));
+        }
+
         return playerNumberList;
     }
 
-    public void setPlayerNumberList(List<Integer> playerNumberList) {
-        this.playerNumberList = playerNumberList;
+    private int convertCharacterToInteger(String playerNumber, int i) {
+        return playerNumber.charAt(i) - '0';
     }
+
+
 }
