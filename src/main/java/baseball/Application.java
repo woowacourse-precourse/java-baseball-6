dccc93e82105a35c796c48a34beba379696b30c6
leedohyun <lucky77795@naver.com>
2023-10-25 06:39:52 +0900

    refactor: 변경된 코드에 알맞게 어플리케이션 코드 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,10 @@
 package baseball;
 
-import baseball.controller.Game;
+import baseball.controller.GameController;
 
 public class Application {
     public static void main(String[] args) {
-        Game game = new Game();
-        game.gameStart();
+        GameController gameController = new GameController();
+        gameController.gameStart();
     }
 }
