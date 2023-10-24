48c7dd9635d51c8948a0fafdc6d9838ca2b50c8e
jemin <jemin03120111@gmail.com>
2023-10-25 03:26:56 +0900

    feat: 게임 시작 문구 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,6 +8,7 @@ public class Application {
 
         boolean restart = true;
         while (restart) {
+            System.out.println("숫자 야구 게임을 시작합니다.");
             Game game = new Game();
             game.play();
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
