717c1ce1c3ccd637909f40aaaa065def538629f2
Kim Taeyeong <tyeong1102@gmail.com>
2023-10-24 15:05:28 +0900

    feat: 난수 생성 구현

---
+++ src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,16 @@
+package baseball.controller;
+
+import baseball.view.Output;
+
+public class GameController {
+
+    private final Output output = new Output();
+
+    public GameController() {
+        output.printStartGame();
+    }
+
+    public void startGame() {
+        
+    }
+}
