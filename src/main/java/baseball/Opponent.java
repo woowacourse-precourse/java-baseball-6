c9cec0867273fca76b41389a94f61688f21ec04b
ASak1104 <khw56184@gmail.com>
2023-10-25 03:13:33 +0900

    feat : Opponent 클래스에 guess 로직 구현

--- src/main/java/baseball/Opponent.java
+++ src/main/java/baseball/Opponent.java
@@ -1,41 +1,57 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
 public class Opponent {
     private static final int MAXIMUM_NUMBER = 9;
     private static final int MINIMUM_NUMBER = 1;
 
-    private final int numberLength;
+    private final int targetLength;
     private final boolean[] isTargetNumber;
-    private final String target;
+    private final List<Integer> targetNumbers;
 
-    public Opponent(int numberLength) {
-        this.numberLength = numberLength;
-        this.isTargetNumber = new boolean[numberLength];
-        this.target = generateTarget();
+    public Opponent(int targetLength) {
+        this.targetLength = targetLength;
+        this.targetNumbers = generateTarget();
+        this.isTargetNumber = new boolean[MAXIMUM_NUMBER + 1];
+
+        targetNumbers.forEach(number -> isTargetNumber[number] = true);
     }
 
-    public GuessResult guess(String guess) {
-        validate(guess);
+    public GuessResult guess(List<Integer> guessNumbers) {
+        validate(guessNumbers);
 
-        int strike = 3;
+        int strike = 0;
         int ball = 0;
+
+        for (int i = 0; i < targetLength; i++) {
+            int guessNumber = guessNumbers.get(i);
+            int targetNumber = targetNumbers.get(i);
+
+            if (guessNumber == targetNumber) {
+                strike++;
+            } else if (isTargetNumber[guessNumber]) {
+                ball++;
+            }
+        }
 
         return new GuessResult(ball, strike);
     }
 
-    private String generateTarget() {
-        return "123";
+    private List<Integer> generateTarget() {
+        return Randoms.pickUniqueNumbersInRange(MINIMUM_NUMBER, MAXIMUM_NUMBER, targetLength);
     }
 
-    private void validate(String guess) {
-        long uniqueNumberCount = guess.chars()
+    private void validate(List<Integer> guessNumbers) {
+        boolean isValid = guessNumbers.size() == targetLength;
+
+        long uniqueNumberCount = guessNumbers.stream()
                 .distinct()
                 .count();
 
-        boolean isValid = uniqueNumberCount == numberLength;
-
-        isValid &= guess.chars()
-                .map(character -> character - '0')
+        isValid &= uniqueNumberCount == targetLength;
+        isValid &= guessNumbers.stream()
                 .allMatch(this::isNumberInRange);
 
         if (!isValid) {
