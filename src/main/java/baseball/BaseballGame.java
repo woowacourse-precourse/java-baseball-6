130e819fd1745959ec280233201f4c005674d1b3
gunsookim <ccpc4896@naver.com>
2023-10-24 22:15:57 +0900

    refactor : 코드 리펙토링 인스턴스 추가
    인스턴스 gameChoice 추가.
    주석 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -3,18 +3,40 @@ package baseball;
 import java.util.List;
 
 class BaseballGame {
-    private static final int NUMBER_OF_DIGITS = 3; // 숫자 자릿수 초기화
+    // 숫자 자릿수 초기화
+    private static final int NUMBER_OF_DIGITS = 3;
+
+    // 게임의 랜덤 숫자를 생성하기 위한 인스턴스
     private final NumberGenerator numberGenerator;
+
+    // 게임에서의 플레이어를 나타내는 인스턴스
     private final Player player;
+
+    // stike, ball 개수 계산을 위한 인스턴스
     private final CheckScore checkScore;
+
+    // 게임 결과를 표시를 위한 인스턴스
     private final ShowResult showResult;
 
+    // 게임 재시작 또는 종료를 처리하기 위한 인스턴스
+    private final GameChoice gameChoice;
+
+    // 모든 게임 컴포넌트를 초기화하는 생성자
     public BaseballGame() {
-        // 인스턴스 초기화
+        // 정의된 숫자 자릿수로 숫자 생성기 초기화
         this.numberGenerator = new NumberGenerator(NUMBER_OF_DIGITS);
-        this.player = new Player();
+
+        // 정의된 숫자 자릿수로 플레이어 초기화
+        this.player = new Player(NUMBER_OF_DIGITS);
+
+        // 정의된 숫자 자릿수로 점수 검사기 초기화
         this.checkScore = new CheckScore(NUMBER_OF_DIGITS);
+
+        // 정의된 숫자 자릿수로 결과 표시기 초기화
         this.showResult = new ShowResult(NUMBER_OF_DIGITS);
+
+        // 정의된 숫자 자릿수로 결과 표시기 초기화
+        this.gameChoice = new GameChoice();
     }
 
     // 야구 게임 진행
@@ -22,20 +44,21 @@ class BaseballGame {
         List<Integer> computerNumbers = numberGenerator.generateNumbers();
 
         // player가 all strike할 때까지 반복
-        while (true) {
-            List<Integer> playerNumbers = player.getInput();
-            int strike = checkScore.countStrike(computerNumbers, playerNumbers);
-            int ball = checkScore.countBall(computerNumbers, playerNumbers);
+        int strike;
+        int ball;
+        do {
+            List<Integer> playerNumbers = player.getNumbers();
+            strike = checkScore.countStrike(computerNumbers, playerNumbers);
+            ball = checkScore.countBall(computerNumbers, playerNumbers);
 
-            if (checkScore.isGameWon(strike)) {
-                showResult.win(strike); // all strike인 경우 승리 문구 출력
-                break;
-            } else {
-                showResult.result(strike, ball); // all strike가 아닌 경우 결과 출력
-            }
-        }
+            showResult.displayResult(strike, ball);
+        } while (!isGameWon(strike));
     }
+    private boolean isGameWon(int strike) {
+        return strike == NUMBER_OF_DIGITS;
+    }
+
     public boolean choiceRestart() {
-        return player.choiceRestart();
+        return gameChoice.choiceRestart();
     }
 }
