e1bbabe408e80213fce56a73847094bc2523d2cd
kijen723 <kij2646@gmail.com>
2023-10-25 04:24:04 +0900

    feat: 정답 개산 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -23,29 +23,53 @@ public class Application {
         // TODO1: 게임시작 메세지 출력
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-        // TODO2: 랜덤 숫자 설정
-        List<Integer> computer = getRandomNumber();
+        int gamestart = 1;
 
-        // TODO3: 사용자에게 숫자를 입력받기
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("숫자를 입력해주세요 : ");
-        int selectNumber = scanner.nextInt();
-        List<Integer> user = new ArrayList<>();
-        user.add(selectNumber / 100);
-        selectNumber %= 100;
-        user.add(selectNumber / 10);
-        selectNumber %= 10;
-        user.add(selectNumber);
+        while (gamestart == 1) {
+            // TODO2: 랜덤 숫자 설정
+            List<Integer> computer = getRandomNumber();
 
-        // TODO4: 볼, 스트라이크 갯수 세기
-        int ball = 0;
-        int strike = 0;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (i == j & user.get(i) == computer.get(j)) {
-                    strike += 1;
-                } else if (user.get(i) == computer.get(j)) {
-                    ball += 1;
+            System.out.println(computer);
+
+            int restart = 1;
+
+            while (restart == 1) {
+                // TODO3: 사용자에게 숫자를 입력받기
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("숫자를 입력해주세요 : ");
+                int selectNumber = scanner.nextInt();
+                List<Integer> user = new ArrayList<>();
+                user.add(selectNumber / 100);
+                selectNumber %= 100;
+                user.add(selectNumber / 10);
+                selectNumber %= 10;
+                user.add(selectNumber);
+
+                // TODO4: 볼, 스트라이크 갯수 세기
+                int ball = 0;
+                int strike = 0;
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        if (i == j & user.get(i) == computer.get(j)) {
+                            strike += 1;
+                        } else if (user.get(i) == computer.get(j)) {
+                            ball += 1;
+                        }
+                    }
+                }
+
+                // TODO5: 정답을 맞출 경우 게임 종료 아니면 재입력
+                if (strike == 3) {
+                    restart = 0;
+                    System.out.printf("%d스트라이크\n", strike);
+                } else if (ball > 0 & strike > 0) {
+                    System.out.printf("%d볼 %d스트라이크\n", ball, strike);
+                } else if (ball > 0) {
+                    System.out.printf("%d볼\n", ball);
+                } else if (strike > 0) {
+                    System.out.printf("%d스트라이크\n", strike);
+                } else {
+                    System.out.println("낫싱");
                 }
             }
         }
