fd8009065d6679e7ab6394cf779ab771a2a33ee0
BackJiwan <bn1357nb@gmail.com>
2023-10-24 20:08:02 +0900

    feat: 컴퓨터 랜덤 숫자 생성
    3자리 서로 다른 자연수 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,17 +1,37 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    static class Computer{
+        public List<Integer> getRandomNumber() {
+            List<Integer> randomNumber = new ArrayList<>();
+            while(randomNumber.size() <3) {
+                int randomDigit = Randoms.pickNumberInRange(1, 9);
+                if(!randomNumber.contains(randomDigit)) {
+                    randomNumber.add(randomDigit);
+                }
+            }
+            return randomNumber;
+        }
+    }
 
     static class Game{
         public void play(){
             System.out.println("숫자 야구 게임을 시작합니다.");
         }
+
     }
 
     public static void main(String[] args) {
         Game game = new Game();
+        Computer computer = new Computer();
         game.play();
+        System.out.println(computer.getRandomNumber());
     }
-
 
 }
