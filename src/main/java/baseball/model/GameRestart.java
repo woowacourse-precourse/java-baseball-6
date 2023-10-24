d4a7c653ecd484567d2c071e00089138c045428d
ChanUi <hcu55@naver.com>
2023-10-25 05:00:31 +0900

    :sparkles: 게임 재시작 기능 구현

---
+++ src/main/java/baseball/model/GameRestart.java
@@ -0,0 +1,20 @@
+package baseball.model;
+
+public class GameRestart {
+    private String restartNum;
+
+    public String getRetryNumber(){
+        return restartNum;
+    }
+
+    public GameRestart(String number) {
+        validateRestartNumber(number);
+        this.restartNum = number;
+    }
+
+    public static void validateRestartNumber(String number) throws IllegalArgumentException {
+        if (!number.equals("1") && !number.equals("2")) {
+            throw new IllegalArgumentException("1또는 2를 입력해야 합니다.");
+        }
+    }
+}
