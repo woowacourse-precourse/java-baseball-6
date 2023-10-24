c812d7d30407c642853a7a334c25fa1151262960
LEEJUNGWOO <jungwoo1002@gmail.com>
2023-10-24 13:16:53 +0900

    refactor: 재시작 책임을 BaseballGame으로 변경

--- src/main/java/baseball/controller/BaseballGame.java
+++ src/main/java/baseball/controller/BaseballGame.java
@@ -1,23 +1,26 @@
 package baseball.controller;
 
 import static baseball.view.InputView.inputPlayerNumbers;
-import static baseball.view.InputView.shouldContinue;
 
 import baseball.model.Computer;
 import baseball.model.GameResult;
 import baseball.model.Numbers;
 import baseball.model.Player;
 import baseball.view.HintView;
+import baseball.view.InputView;
 
 public class BaseballGame {
     private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String END_GAME_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String RETRY = "1";
+    private static final String END_GAME = "2";
+    private static final String INVALID_INPUT_MESSAGE = "올바른 수를 입력하세요.";
 
     public void play() {
         System.out.println(START_MESSAGE);
         do {
             start();
-        } while (shouldContinue());
+        } while (askRestartOrExit());
     }
 
     private void start() {
@@ -27,13 +30,19 @@ public class BaseballGame {
             Player player = Player.from(inputPlayerNumbers());
             Numbers playerNumbers = player.getPlayerNumbers();
             result = computerNumbers.calculateResult(playerNumbers);
-            printHint(result);
+            new HintView(result).printHint();
         } while (!result.isThreeStrikes());
         System.out.println(END_GAME_MESSAGE);
     }
 
-    private void printHint(GameResult result) {
-        HintView hintView = new HintView(result);
-        hintView.printHint();
+    private boolean askRestartOrExit() {
+        String answer = InputView.promptContinueGame();
+        if (answer.equals(RETRY)) {
+            return true;
+        }
+        if (answer.equals(END_GAME)) {
+            return false;
+        }
+        throw new IllegalArgumentException(INVALID_INPUT_MESSAGE);
     }
 }
--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -5,25 +5,15 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-    private static final String RETRY = "1";
-    private static final String END_GAME = "2";
-    private static final String INVALID_INPUT_MESSAGE = "올바른 수를 입력하세요.";
 
     public static String inputPlayerNumbers() {
         System.out.print(INPUT_MESSAGE);
         return Console.readLine();
     }
 
-    public static boolean shouldContinue() {
+    public static String promptContinueGame() {
         System.out.println(RESTART_MESSAGE);
-        String userInput = Console.readLine();
-        if (RETRY.equals(userInput)) {
-            return true;
-        }
-        if (END_GAME.equals(userInput)) {
-            return false;
-        }
-        throw new IllegalArgumentException(INVALID_INPUT_MESSAGE);
+        return Console.readLine();
     }
 
     private InputView() {
