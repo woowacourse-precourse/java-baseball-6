a94be4bd7495ff59482674238a9d69ab3870ffec
JongMinCh0i <wonderland314592@gmail.com>
2023-10-24 16:44:27 +0900

    refactoring(UserController): Controller 인터페이스 구현

--- src/main/java/baseball/controller/UserController.java
+++ src/main/java/baseball/controller/UserController.java
@@ -2,13 +2,14 @@ package baseball.controller;
 
 import baseball.service.GameService;
 
-public class UserController {
+public class UserController implements Controller {
     private final GameService gameService;
 
     public UserController(GameService gameService) {
         this.gameService = gameService;
     }
 
+    @Override
     public void gameStart() {
         gameService.solvingProblem();
     }
