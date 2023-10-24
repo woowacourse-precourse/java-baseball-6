8dfaa232fad5e21293788866ab6d78edf065050e
jaepyo99 <jaepyo99@naver.com>
2023-10-24 19:39:27 +0900

    [Feat] 카운트 출력 기능 추가

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -10,7 +10,24 @@ public class Referee {
     Validator validator = new Validator();
     Computer computer = new Computer();
     User user = new User();
-
+    
+    public Boolean countBallAndStrike(List<Integer> strikeAndBall) {//카운트 세주는
+        int strike = strikeAndBall.get(0);
+        int ball = strikeAndBall.get(1);
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+        } else if (strike == 3) {
+            System.out.println("3스트라이크");
+            return TRUE;
+        } else if (strike == 0) {
+            System.out.printf("%d볼\n", ball);
+        } else if (ball == 0) {
+            System.out.printf("%d스트라이크\n", strike);
+        } else {
+            System.out.printf("%d볼 %d스트라이크\n", ball, strike);
+        }
+        return FALSE;
+    }
     public List<Integer> judgeNum(String baseballNum, String userInputNum) {
         int strike = 0;
         int ball = 0;
