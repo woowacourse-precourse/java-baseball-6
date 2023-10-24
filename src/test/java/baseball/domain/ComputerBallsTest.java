290ae0e83e5bdf1f9d72b189037ea10775a66e83
jongheonleee <qwefghnm1212@gmail.com>
2023-10-24 20:19:34 +0900

    [test] 생성된 볼 중복 테스트

--- src/test/java/baseball/domain/ComputerBallsTest.java
+++ src/test/java/baseball/domain/ComputerBallsTest.java
@@ -3,6 +3,8 @@ package baseball.domain;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,6 +14,17 @@ class ComputerBallsTest {
     void 생성된_볼_개수_테스트() {
         ComputerBalls computerBall = new ComputerBalls();
         List<Ball> result = computerBall.getBalls();
+
+        assertEquals(3, result.size());
+    }
+
+    @Test
+    void 생성된_볼_중복_테스트() {
+        ComputerBalls computerBall = new ComputerBalls();
+        Set<Ball> result = computerBall.getBalls()
+                .stream()
+                .peek(System.out::print)
+                .collect(Collectors.toSet());
 
         assertEquals(3, result.size());
     }
