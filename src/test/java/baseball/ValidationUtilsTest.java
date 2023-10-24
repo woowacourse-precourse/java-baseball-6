320c6ac1c184da4e026a8e4098099f4a01470464
chosunghyun18 <krismon@naver.com>
2023-10-24 14:08:56 +0900

    test : add 야구 숫자 1,9 검증

---
+++ src/test/java/baseball/ValidationUtilsTest.java
@@ -0,0 +1,18 @@
+package baseball;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ValidationUtilsTest {
+
+    @Test
+    @DisplayName("야구 숫자 1,9 검증")
+    void 야구_숫자_1_9_검증() {
+        assertThat(ValidateUtils.validNo(9)).isTrue();
+        assertThat(ValidateUtils.validNo(1)).isTrue();
+        assertThat(ValidateUtils.validNo(0)).isFalse();
+        assertThat(ValidateUtils.validNo(10)).isFalse();
+    }
+}
