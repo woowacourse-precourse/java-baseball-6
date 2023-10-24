2e3b268a5fe4ac0dbfca13b5c7ce7c4902f1dcec
woong2e <tlswodnd12345@naver.com>
2023-10-25 03:40:28 +0900

    modify7_Application.java

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,6 +8,7 @@ import camp.nextstep.edu.missionutils.*;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        
         GameLauncher game = new GameLauncher();
         game.start();
     }
