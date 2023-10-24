09ca0e0dd9da236c83d36fb103863c08920f6ccf
KYBYK <dbqks56231106@gamil.com>
2023-10-24 15:44:01 +0900

    Referee 클래스 생성 및 구현

---
+++ src/main/java/baseball/referee/Referee.java
@@ -0,0 +1,92 @@
+package baseball.referee;
+
+import baseball.computer.Computer;
+import baseball.player.Player;
+
+import java.util.Objects;
+
+public class Referee {
+    Player player = new Player();
+    Computer computer = new Computer();
+
+    private int strike, ball;
+    private boolean restart;
+
+    public Referee() {
+        startGame();
+        setRestart(true);
+    }
+    public void setStrike(int strike) {
+        this.strike = strike;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public void setBall(int ball) {
+        this.ball = ball;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+
+    public void setRestart(boolean restart) {
+        this.restart = restart;
+    }
+
+    public boolean getRestart() {
+        return restart;
+    }
+
+    public void startGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        computer.makeRandomNumber();
+    }
+    public void askNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+        player.askNumber();
+    }
+    public void askRestart() {
+        if(Objects.equals(player.askRestart(), "0")){
+            setRestart(false);
+            return;
+        }
+        startGame();
+    }
+    public void endGame() {
+        System.out.print("3스트라이크");
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        askRestart();
+    }
+
+    public void compareNumber(){
+        String playerNumber = player.getNumber();
+        String computerNumber = computer.getNumber();
+        int strike = 0;
+        int ball = 0;
+        for(int i = 0; i < computerNumber.length(); i++){
+            for(int j = 0; j < playerNumber.length(); j++){
+                if(computerNumber.charAt(i) == playerNumber.charAt(j)){
+                    if(i == j){
+                        strike++;
+                    }else {
+                        ball++;
+                    }
+                }
+            }
+        }
+        setStrike(strike);
+        setBall(ball);
+    }
+
+    public void getResult() {
+        if(getStrike() == 3) {
+            endGame();
+            return;
+        }
+        System.out.println(getBall() + "볼 " + getStrike() + "스트라이크");
+    }
+}
