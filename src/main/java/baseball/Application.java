2ade7791750bdfa664186772c78492a828cbe79c
JongMinCh0i <wonderland314592@gmail.com>
2023-10-24 16:45:57 +0900

    style(Application): run -> gameStart 메서드 이름 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,6 +7,6 @@ public class Application {
     public static void main(String[] args) {
         DiContainer diFactory = new DiContainer();
         ComputerController computerController = diFactory.computerController();
-        computerController.run();
+        computerController.gameStart();
     }
 }
