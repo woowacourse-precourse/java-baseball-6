0c3d224924740b4dea6d3ffd316b17268384ad2a
Kim Yuhyeon <37354574+youhyeoneee@users.noreply.github.com>
2023-10-24 19:52:48 +0900

    feat: 컴퓨터_플레이어에게 입력 받은 숫자에 대해서 힌트를 출력한다.

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Computer {
 
-    public String number; // 컴퓨터의 서로 다른 3자리의 수
+    private String number; // 컴퓨터의 서로 다른 3자리의 수
 
     // 서로 다른 임의의 수 3개를 선택한다.
     public void setNumber() {
@@ -22,5 +22,66 @@ public class Computer {
         for (Integer item : list) {
             number += Integer.toString(item);
         }
+
+    }
+
+    // 플레이어에게 입력 받은 숫자에 대해서 힌트를 출력한다. 맞추면 true를 반환한다.
+    public boolean printResult(String playerNum) {
+        /*
+        같은 수가 같은 자리에 있으면 스트라이크, 다른 자리에 있으면 볼, 같은 수가 전혀 없으면 낫싱
+         */
+
+        int strike = 0; // 스트라이크 개수
+        int ball = 0; // 볼 개수
+
+        for (int idx = 0; idx < number.length(); idx++) {
+            // 스트라이크
+            if (isStrike(number, playerNum, idx)) {
+                strike++;
+            } else {
+                // 볼
+                if (isBall(number, playerNum.charAt(idx))) {
+                    ball++;
+                }
+            }
+        }
+
+        boolean result = false; // 일치 여부 결과
+        String hint = ""; // 힌트 메세지
+
+        if (strike == 3) { // 완벽히 일치하는 경우
+            hint = "3스트라이크";
+            result = true;
+        } else if (strike == 0 && ball == 0) { // 같은 수가 전혀 없는 경우
+            hint = "낫싱";
+        } else {
+            if (ball > 0) {
+                hint += ball + "볼 ";
+            }
+            if (strike > 0) {
+                hint += strike + "스트라이크";
+            }
+        }
+
+        System.out.println(hint);
+
+        return result;
+    }
+
+
+    // idx 위치에 있는 문자가 같은지
+    boolean isStrike(String str1, String str2, int idx) {
+        return str1.charAt(idx) == str2.charAt(idx);
+    }
+
+    // 문자열(str)이 문자(char)를 포함 하는 지
+    boolean isBall(String str, char ch) {
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ch) {
+                return true;
+            }
+        }
+        return false;
     }
 }
