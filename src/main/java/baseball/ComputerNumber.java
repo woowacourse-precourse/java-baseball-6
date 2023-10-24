40b1dbcfca2d8558aa5a672561aa7b101c92a4a7
Kim, JeongHwan <jhkim980214@gmail.com>
2023-10-24 16:41:55 +0900

    상수 처리

--- src/main/java/baseball/ComputerNumber.java
+++ src/main/java/baseball/ComputerNumber.java
@@ -6,10 +6,14 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class ComputerNumber {
+	private static final int NUMBER_LENGTH = 3;
+	private static final int MIN = 1;
+	private static final int MAX = 9;
+
 	public List<Integer> computerNumbers() {
 		List<Integer> computer = new ArrayList<>();
-		while (computer.size() < 3) {
-			int randomNumber = Randoms.pickNumberInRange(1, 9);
+		while (computer.size() < NUMBER_LENGTH) {
+			int randomNumber = Randoms.pickNumberInRange(MIN, MAX);
 			if (!computer.contains(randomNumber)) {
 				computer.add(randomNumber);
 			}
