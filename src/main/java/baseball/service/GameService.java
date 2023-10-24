2dc1ef33ff5c1ba1c7e312bfb4243b18e69a2af3
OhJongMin <jmoh7480@gmail.com>
2023-10-25 04:16:21 +0900

    feat:Add makeAnswer method

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -1,8 +1,23 @@
 package baseball.service;
 
+import baseball.domain.Computer;
+import baseball.domain.GameRule;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameService {
     public static List<Integer> userNumbers;
     public static List<Integer> computerNumbers;
+
+    public static void makeAnswer() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < GameRule.answerLength) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        Computer.setAnswer(computer);
+    }
 }
