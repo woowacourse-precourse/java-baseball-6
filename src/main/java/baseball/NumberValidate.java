684b991d8e4ec626b15ba7de4ddfae70fbb47148
chosunghyun18 <krismon@naver.com>
2023-10-24 20:52:34 +0900

    feat : add check number validate in range -숫자_범위_확인_1_9

--- src/main/java/baseball/NumberValidate.java
+++ src/main/java/baseball/NumberValidate.java
@@ -1,2 +1,9 @@
-package baseball;public class NumberValidate {
+package baseball;
+
+public class NumberValidate {
+    public static Integer MAX_VALUE = 9;
+    public static Integer MIN_VALUE = 1;
+    public static boolean isInRange(int givenNumber) {
+        return MIN_VALUE <= givenNumber && givenNumber <= MAX_VALUE ;
+    }
 }
--- src/test/java/baseball/NumberValidateTest.java
+++ src/test/java/baseball/NumberValidateTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 public class NumberValidateTest {
     @Test
-    public void 숫자_범위_확인_1_9(){
+    public void v(){
         assertThat(NumberValidate.isInRange(1)).isTrue();
         assertThat(NumberValidate.isInRange(9)).isTrue();
         assertThat(NumberValidate.isInRange(0)).isFalse();
