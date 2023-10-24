4d9af84f390334dc0e98844c46a4fdc49cc15f06
chosunghyun18 <krismon@naver.com>
2023-10-24 20:59:49 +0900

    test : add number validate test

--- src/test/java/baseball/NumberValidateTest.java
+++ src/test/java/baseball/NumberValidateTest.java
@@ -5,10 +5,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 public class NumberValidateTest {
     @Test
-    public void v(){
+    public void 숫자_범위_확인_1_9_test(){
         assertThat(NumberValidate.isInRange(1)).isTrue();
         assertThat(NumberValidate.isInRange(9)).isTrue();
         assertThat(NumberValidate.isInRange(0)).isFalse();
         assertThat(NumberValidate.isInRange(10)).isFalse();
+    }
+    @Test
+    public void 문자열_숫자_인지_확인_test(){
+        assertThat(NumberValidate.isNumber("1")).isTrue();
+        assertThat(NumberValidate.isNumber("!")).isFalse();
     }
 }
