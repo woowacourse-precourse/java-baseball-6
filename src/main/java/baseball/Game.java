4df8f9007f5ee8bd1894be389ce4c010b2b5f91e
minjun7410 <minjun7410@gmail.com>
2023-10-24 21:18:00 +0900

    style(Game): 변수 이름 변경
    * judgeResult -> judgement로 변경

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -3,18 +3,19 @@ package baseball;
 public class Game {
     private final InputView inputView = new InputView();
     private final Computer computer = new Computer();
+
     public Game(){
         OutputView.printStartText();
     }
     public void playGame(){
         computer.initComputerNumbers();
-        Judgement judgeResult;
+        Judgement judgement;
         do{
             Numbers userNumbers = inputView.getUserNumbers();
-            judgeResult = computer.getJudgement(userNumbers);
-            judgeResult.printResult();
+            judgement = computer.getJudgement(userNumbers);
+            judgement.printResult();
         }
-        while(!judgeResult.isOut());
+        while(!judgement.isOut());
         OutputView.printGameOverText();
     }
 }
