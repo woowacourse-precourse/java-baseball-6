fbf28c8bd5b489e739f56d3429d7fd8c94abcb67
parkseryu <pjh971212@gmail.com>
2023-10-25 00:29:02 +0900

    feat: 메인 메소드에서 반환 받게끔 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
 
+import baseball.controller.ComputerController;
+import baseball.view.UserInputView;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<Integer> baseballNumber = ComputerController.getBaseballNumber();
+        UserInputView userInputView = new UserInputView();
+        List<Integer> userInputNumber = userInputView.gameStart();
+
+
     }
 }
