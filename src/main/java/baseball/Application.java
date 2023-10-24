82e62b3affaba2b0883f5967f3e483da82160340
gkwk <124476558+gkwk@users.noreply.github.com>
2023-10-24 11:51:58 +0900

    구현사항 추가 및 ballCount 메소드 구현
    ballCount 메소드는 1에서 9까지 서로 다른 임의의 수 3개라는 조건에 따라 HashSet으로 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,28 +1,31 @@
 package baseball;
 
-import java.util.Scanner;
-import java.util.HashMap;
+import java.util.HashSet;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
-
-    public static int ballCount(HashMap<Integer, Integer> ballHashMap) {
+    public static int ballCount(int[] inputIntegerArray, int[] answerIntegerArray) {
         int sum = 0;
 
-        for (int ballHashMapKey : ballHashMap.keySet()) {
-            sum += ballHashMap.get(ballHashMapKey);
-        }
-        
-        return sum;
-    }
+        HashSet<Integer> ballHashSet = new HashSet<>(3);
 
-    public static int strikeCount(int[] inputIntegerArray,int[] answerIntegerArray) {
-        int sum = 0;
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < answerIntegerArray.length; i++) {
             if (inputIntegerArray[i] == answerIntegerArray[i]) {
-                sum += 1;
+                continue;
+            }
+            else {
+                for (int j = 0; j < answerIntegerArray.length; j++) {
+                    if (inputIntegerArray[i] == answerIntegerArray[j]) {
+                        ballHashSet.add(inputIntegerArray[i]);
+                        continue;
+                    }
+                }
             }
         }
+
+        sum = ballHashSet.size();
 
         return sum;
     }
