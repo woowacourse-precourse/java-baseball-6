c53fd5b8deefd0800072c7f8a4b4fd135fabc67f
ddshpp <dshp753@gmail.com>
2023-10-24 21:46:55 +0900

    feat: 게임을 재시작 및 종료하는 기능구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,84 +10,95 @@ import java.util.Set;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-
-        Set<Integer> tempHashSet1 = new HashSet<>();
-
-        while (tempHashSet1.size() < 3) {
-            tempHashSet1.add(Randoms.pickNumberInRange(1, 9));
-        }
-
-        List<Integer> numbers = new ArrayList<>(tempHashSet1);
-
-        System.out.println(numbers);
-
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-
         while (true) {
-            System.out.print("숫자를 입력해주세요 : ");
-            String input = Console.readLine();
+            Set<Integer> tempHashSet1 = new HashSet<>();
 
-            if (input.length() != 3) {
-                throw new IllegalArgumentException("잘못된 입력입니다.");
+            while (tempHashSet1.size() < 3) {
+                tempHashSet1.add(Randoms.pickNumberInRange(1, 9));
             }
 
-            for (int i = 0; i < input.length(); i++) {
-                char c = input.charAt(i);
+            List<Integer> numbers = new ArrayList<>(tempHashSet1);
 
-                if (c < '0' || c > '9') {
+            while (true) {
+                System.out.print("숫자를 입력해주세요 : ");
+                String input = Console.readLine();
+
+                if (input.length() != 3) {
                     throw new IllegalArgumentException("잘못된 입력입니다.");
                 }
-            }
 
-            Set<Integer> tempHashSet2 = new LinkedHashSet<>();
+                for (int i = 0; i < input.length(); i++) {
+                    char c = input.charAt(i);
 
-            for (int i = 0; i < input.length(); i++) {
-                tempHashSet2.add(input.charAt(i) - '0');
-            }
+                    if (c < '0' || c > '9') {
+                        throw new IllegalArgumentException("잘못된 입력입니다.");
+                    }
+                }
 
-            if (tempHashSet2.size() < 3) {
-                throw new IllegalArgumentException("잘못된 입력입니다.");
-            }
+                Set<Integer> tempHashSet2 = new LinkedHashSet<>();
 
-            List<Integer> inputNumbers = new ArrayList<>(tempHashSet2);
+                for (int i = 0; i < input.length(); i++) {
+                    tempHashSet2.add(input.charAt(i) - '0');
+                }
 
-            int strikeCount = 0;
+                if (tempHashSet2.size() < 3) {
+                    throw new IllegalArgumentException("잘못된 입력입니다.");
+                }
 
-            for (int i = 0; i < numbers.size(); i++) {
-                if (inputNumbers.get(i).equals(numbers.get(i))) {
-                    strikeCount++;
+                List<Integer> inputNumbers = new ArrayList<>(tempHashSet2);
+
+                int strikeCount = 0;
+
+                for (int i = 0; i < numbers.size(); i++) {
+                    if (inputNumbers.get(i).equals(numbers.get(i))) {
+                        strikeCount++;
+                    }
+                }
+
+                int ballCount = 0;
+
+                for (int i = 0; i < numbers.size(); i++) {
+                    for (int j = 0; j < inputNumbers.size(); j++) {
+                        if (i == j) {
+                            continue;
+                        }
+
+                        if (numbers.get(i).equals(inputNumbers.get(j))) {
+                            ballCount++;
+                        }
+                    }
+                }
+
+                if (ballCount == 0 && strikeCount == 0) {
+                    System.out.println("낫싱");
+                }
+                if (ballCount != 0) {
+                    System.out.print(ballCount + "볼 ");
+                }
+                if (strikeCount != 0) {
+                    System.out.print(strikeCount + "스트라이크");
+                }
+                System.out.println();
+
+                if (strikeCount == 3) {
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    break;
                 }
             }
 
-            int ballCount = 0;
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
-            for (int i = 0; i < numbers.size(); i++) {
-                for (int j = 0; j < inputNumbers.size(); j++) {
-                    if (i == j) {
-                        continue;
-                    }
+            String lastInput = Console.readLine();
 
-                    if (numbers.get(i).equals(inputNumbers.get(j))) {
-                        ballCount++;
-                    }
-                }
+            if (lastInput.equals("1")) {
+                continue;
             }
-
-            if (ballCount == 0 && strikeCount == 0) {
-                System.out.println("낫싱");
-            }
-            if (ballCount != 0) {
-                System.out.print(ballCount + "볼 ");
-            }
-            if (strikeCount != 0) {
-                System.out.print(strikeCount + "스트라이크");
-            }
-            System.out.println();
-
-            if (strikeCount == 3) {
+            if (lastInput.equals("2")) {
                 break;
+            } else {
+                throw new IllegalArgumentException("잘못된 입력입니다.");
             }
         }
     }
