9840b29e1cabcc87585d5ca8c81662cf9fc53780
sohyun9527 <sohyun95277@gmail.com>
2023-10-24 12:53:39 +0900

    test(NumberBaseballGameTest): 단위 테스트 추가 게임 도중 에서 발생 테스트, 볼, 스트라이크 카운트에 대한 테스트 추가

--- src/test/java/baseball/NumberBaseballGameTest.java
+++ src/test/java/baseball/NumberBaseballGameTest.java
@@ -1,19 +1,43 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.test.NsTest;
 import java.io.ByteArrayInputStream;
 import java.lang.reflect.Method;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class NumberBaseballGameTest {
+class NumberBaseballGameTest extends NsTest {
+    NumberBaseballGame numberBaseballGame = new NumberBaseballGame();
+
+    @DisplayName("게임 도중 에러 발생 테스트")
+    @Test
+    public void exceptionDuringGame() throws Exception {
+        assertThrows(IllegalArgumentException.class, () -> {
+            run("216", "241", "999");
+        });
+    }
+
+    @DisplayName("볼, 스트라이크 카운트에 대한 테스트")
+    @Test
+    public void contTest() throws Exception {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("216", "891", "273", "916", "378", "2");
+                    assertThat(output()).contains("낫싱", "1볼", "1볼 1스트라이크", "낫싱", "3스트라이크");
+                },
+                3, 7, 8
+        );
+
+    }
 
     @DisplayName("종료 입력에 대한 false 반환 테스트")
     @Test
     public void stopTest() throws Exception {
-        NumberBaseballGame numberBaseballGame = new NumberBaseballGame();
 
         //given
         String sign = "2";
@@ -34,7 +58,6 @@ class NumberBaseballGameTest {
     @DisplayName("재시작 입력에 대한 true 반환 테스트")
     @Test
     public void restartTest() throws Exception {
-        NumberBaseballGame numberBaseballGame = new NumberBaseballGame();
 
         //given
         String sign = "1";
@@ -53,4 +76,8 @@ class NumberBaseballGameTest {
     }
 
 
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
 }
