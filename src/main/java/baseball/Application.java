fd611cd5e8deb6773019434cbef33fe2ed308621
PARK9898 <wjddjs21@naver.com>
2023-10-24 17:31:07 +0900

    "feat: Application 동작 구현"

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
+import baseball.controller.Game;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class Application {
+    private static final InputView INPUT_VIEW = new InputView();
+    private static final OutputView OUTPUT_VIEW = new OutputView();
+    private static final String RESTART = "1";
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        do{
+            new Game(INPUT_VIEW,OUTPUT_VIEW).playOnce();
+        }while (INPUT_VIEW.decideRestart().equals(RESTART));
     }
 }
