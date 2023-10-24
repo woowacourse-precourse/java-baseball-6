02bc30353b5c4d51ba5ed84d6d984519221dcae9
minjun7410 <minjun7410@gmail.com>
2023-10-24 20:59:18 +0900

    refactor(ComputerTest, JudgementTest): JudgementTest 클래스 추가
    * 판정 관련 테스트 클래스 JudgementTest 생성했다.
    * ComputerTest의 판정테스트를 JudgementTest로 옮겼다.

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -9,11 +9,9 @@ import java.util.List;
 
 public class ComputerTest {
     private final Computer computer = new Computer();
-    ByteArrayOutputStream testOut = new ByteArrayOutputStream();
     @Test
     void 난수생성중복검사(){
         for(int i = 0; i < 100; i++) {
-
             Numbers numbers = computer.initComputerNumbers();
             List<Number> duplicateCheck = new ArrayList();
             for(int j = 0; j < 3; j++){
@@ -22,29 +20,4 @@ public class ComputerTest {
             }
         }
     }
-
-    @Test
-    void 판정테스트(){
-        System.setOut(new PrintStream(testOut));
-        Numbers computerNumbers = makeNumbers(new int[] {1, 2, 3});
-        assertJudgeResultOutput(makeNumbers(new int[] {1, 2, 3}), computerNumbers,"3스트라이크\n");
-        assertJudgeResultOutput(makeNumbers(new int[] {2, 3, 1}), computerNumbers,"3볼\n");
-        assertJudgeResultOutput(makeNumbers(new int[] {3, 5, 6}), computerNumbers,"1볼\n");
-        assertJudgeResultOutput(makeNumbers(new int[] {1, 3, 6}), computerNumbers,"1볼 1스트라이크\n");
-        assertJudgeResultOutput( makeNumbers(new int[] {3, 2, 1}), computerNumbers,"2볼 1스트라이크\n");
-    }
-    Numbers makeNumbers(int[] list){
-        Numbers numbers = new Numbers();
-        for(int number : list){
-            numbers.addNumber(new Number(number));
-        }
-        return numbers;
-    }
-    void assertJudgeResultOutput(Numbers userNumbers, Numbers computerNumbers, String outputToCompare){
-        Judgement result = new Judgement(userNumbers, computerNumbers);
-        result.printResult();
-        Assertions.assertThat(testOut.toString()).isEqualTo(outputToCompare);
-        testOut.reset();
-    }
-
 }
