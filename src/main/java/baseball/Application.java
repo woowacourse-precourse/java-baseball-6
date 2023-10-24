871dd88fe51dcb467d1c1d771c25a56897014f7b
Minji Kim <minji1289@naver.com>
2023-10-24 23:41:35 +0900

    [feat] 컴퓨터가 서로 다른 3가지 수를 선택

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,32 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    private static final int DIGIT_COUNT = 3;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
     }
+
+
+    public static ArrayList<Integer> getRandomNumber() {
+        ArrayList<Integer> answerNumberList = new ArrayList<>();
+
+        while (answerNumberList.size() < DIGIT_COUNT) {
+            checkExistsInList(Randoms.pickNumberInRange(1, 9), answerNumberList);
+        }
+        return answerNumberList;
+    }
+
+    public static void checkExistsInList(int number, ArrayList<Integer> list) {
+        if (!list.contains(number)) {
+            list.add(number);
+        }
+    }
+
+
 }
