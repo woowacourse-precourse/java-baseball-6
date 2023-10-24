f54e5bb4567a69d2b0170147d8686b8e7d590d09
shapeofmy <114326457+rudtn409@users.noreply.github.com>
2023-10-25 02:53:46 +0900

    게임종류_후_재시작 테스트까지

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,92 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        baseballClass baseball = new baseballClass();
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        baseball.Run();
+
+    }
+}
+
+class baseballClass {
+    private List<Integer> computer = new ArrayList<>(3);
+    private List<Integer> player = new ArrayList<>(3);
+    private int status = 1;
+
+    private void setComputer(){
+        computer.clear();
+        while(computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+    }
+
+    public baseballClass(){
+        setComputer();
+    }
+
+    private void setPlayer(){
+        try {
+            player.clear();
+            System.out.print("숫자를 입력해주세요 : ");
+            String choiceNumber = Console.readLine();
+
+            if(choiceNumber.length() !=3){
+                throw new IllegalArgumentException();
+            }
+
+            for(char c : choiceNumber.toCharArray()){
+                int numericValue = Character.getNumericValue(c);
+
+                if (numericValue < 1 || numericValue > 9 || player.contains(numericValue)) {
+                    throw new IllegalArgumentException();
+                }
+                player.add(numericValue);
+            }
+        } catch (IllegalArgumentException e) {
+            status = 2;
+        }
+    }
+
+    public void Run(){
+        while(status!=2){
+            int strike = 0;
+            int ball = 0;
+            int nothing = 0;
+            setPlayer();
+            if(status==2) break;
+            for(int i = 0; i < 3; i++){
+                if(!computer.contains(player.get(i))){
+                    nothing++;
+                } else if(computer.indexOf(player.get(i))==i){
+                    strike++;
+                } else{
+                    ball++;
+                }
+            }
+            if(nothing==3){
+                System.out.println("낫싱");
+            }else if(strike==3){
+                System.out.println("3스트라이크");
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+                status = Integer.parseInt(Console.readLine());
+                if(status==1) setComputer();
+            }else{
+                if(ball!=0) System.out.printf("%d볼 ",ball);
+                if(strike!=0) System.out.printf("%d스트라이크",strike);
+                System.out.println();
+            }
+
+        }
     }
 }
