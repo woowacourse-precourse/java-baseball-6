72f551c41f615f48571209e45fda9ed24f428f8d
answer <answer3dw@gmail.com>
2023-10-25 01:54:14 +0900

    test : 테스트 코드 수정

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -13,16 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
-    @Test
-    void 게임종료_후_재시작() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("246", "135", "1", "597", "589", "2");
-                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
-                },
-                1, 3, 5, 5, 8, 9
-        );
-    }
+
     @Test
     void 컴퓨터숫자_랜덤_생성(){
         ComputerNumber computerNumber = new ComputerNumber();
