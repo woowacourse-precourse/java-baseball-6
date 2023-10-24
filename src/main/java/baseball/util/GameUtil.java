e8fa129f8e02c1e8327c944625d7147caca5d5b7
jang010505 <jang010505@inu.ac.kr>
2023-10-24 14:57:27 +0900

    feat: computeCount 함수 세부 내용 작성

--- src/main/java/baseball/util/GameUtil.java
+++ src/main/java/baseball/util/GameUtil.java
@@ -2,7 +2,21 @@ package baseball.util;
 
 public class GameUtil {
 
-    public void computeCount(String computerNumber, String playerNumber) {
+    private int ballCount = 0;
+    private int strikeCount = 0;
 
+    public void computeCount(String computerNumber, String playerNumber) {
+        this.strikeCount = computeStrikeCount(computerNumber, playerNumber);
+        this.ballCount = computeBallCount(computerNumber, playerNumber);
+    }
+
+    private int computeStrikeCount(String computerNumber, String playerNumber) {
+
+        return 0;
+    }
+
+    private int computeBallCount(String computerNumber, String playerNumber) {
+
+        return 0;
     }
 }
