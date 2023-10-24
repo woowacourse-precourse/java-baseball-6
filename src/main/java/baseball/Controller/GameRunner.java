0c5de851eec35307b86446b0a1b6ee8dbdeb331b
Beomsu <gobeumsu@gmail.com>
2023-10-25 00:06:17 +0900

    feat(GameRunner): 게임 실행 로직 구현
    1. GameRunner 클래스의 run 메서드에서 게임의 주 실행 로직을 구현합니다.
    2. 사용자로부터 숫자를 입력받습니다.
    3. 입력받은 숫자에 대해 스트라이크와 볼을 계산합니다.
    4. 계산 결과를 출력합니다.
    5. 3개의 숫자를 모두 맞추면 게임을 종료합니다.

--- src/main/java/baseball/Controller/GameRunner.java
+++ src/main/java/baseball/Controller/GameRunner.java
@@ -14,6 +14,20 @@ public class GameRunner {
         this.baseball = baseball;
     }
 
+    public void run() {
+        while (true) {
+            List<Integer> playerNumber = getUserInput();
+            int strikes = baseball.countStrikes(playerNumber);
+            int balls = baseball.countBalls(playerNumber);
+            printResult(strikes, balls);
+
+            if (baseball.isGameWon(strikes)) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            }
+        }
+    }
+
     private List<Integer> getUserInput() {
         System.out.print("숫자를 입력해주세요 : ");
         String userInput = readLine();
