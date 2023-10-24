6769af52053430176778e3b77c355ebf75ea4493
YuJeongYun <yunyj99@naver.com>
2023-10-24 23:58:43 +0900

    refactor: 숫자 비교 결과 콘솔 출력 기능 수정
    숫자 비교 결과 map을 반환하므로 비교 결과 출력 함수에서 매개변수로 map을 받아와서 출력하도록 수정

--- src/main/java/baseball/baseballGame/PrintConsole.java
+++ src/main/java/baseball/baseballGame/PrintConsole.java
@@ -2,6 +2,8 @@ package baseball.baseballGame;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Map;
+
 /**
  * 콘솔에 출력하는 함수
  */
@@ -17,17 +19,19 @@ public class PrintConsole {
         return Integer.parseInt(Console.readLine());
     }
 
-    public void printOutput() {
-        System.out.println("낫싱");
-    }
+    public void printOutput(Map<String, Integer> map) {
+        int strike = map.get("strike");
+        int ball = map.get("ball");
 
-    public void printOutput(int strike, int ball) {
-        System.out.println(ball + "볼 " + strike + "스트라이크");
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+        } else {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
     }
 
     public void printEnd() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
-                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 }
 
