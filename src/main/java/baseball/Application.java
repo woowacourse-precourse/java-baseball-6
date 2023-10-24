6bea44dbf58a49a85e1ffe11b2082acb758bb518
vega <vega99c@naver.com>
2023-10-25 01:45:23 +0900

    feat(inputAndGetUserNumbers) : 유저로부터 3자리 수 입력받기 - 게임 시작을 알리는 메시지 출력 추가 - 입력받은 수가 숫자가 아닌 경우 IllegalArgumentException 처리 - 입력받은 수가 숫자인데 3자리가 아닌 경우 IllegalArgumentException 처리 - 다음 구현 할 힌트제공 함수에 인자값으로 제공하기 위해 입력한 수 반환

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,7 +18,40 @@ public class Application {
         return computer;
     }
 
+    public static int[] inputAndGetUserNumbers() {
+        int[] arrayPlayerNumber = null;
+        int userInputNumber = 0;
+        String stringPlayerNumber = "";
+
+        try {
+            userInputNumber = Integer.parseInt(Console.readLine());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("정수 3자리만 입력해야 합니다.");
+        }
+
+        //입력된 수를 문자열로 변환 후 숫자 배열로 변환
+        stringPlayerNumber = Integer.toString(userInputNumber);
+        arrayPlayerNumber = new int[stringPlayerNumber.length()];
+
+        //예외 처리2-2, 2-3에 대한 에러 처리
+        for (int i = 0; i < stringPlayerNumber.length(); i++) {
+            arrayPlayerNumber[i] = stringPlayerNumber.charAt(i) - '0';
+        }
+        if (arrayPlayerNumber.length != 3) {
+            throw new IllegalArgumentException("3자리의 정수가 입력되지 않았습니다.");
+        }
+        return arrayPlayerNumber;
+    }
+    
     public static void main(String[] args) {
         List<Integer> listAnswerNumber = makeRandomNumberList();
+        int[] arrayPlayerNumber = null;
+
+        System.out.println("숫자 야구 게임을 시작합니다. ");
+        while (true) {
+            System.out.print("숫자를 입력해주세요: ");
+            arrayPlayerNumber = inputAndGetUserNumbers();
+        }
+
     }
 }
