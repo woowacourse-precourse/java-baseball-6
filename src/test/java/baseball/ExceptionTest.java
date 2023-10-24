f2522387d51fce415cfae529dc30b2593a72dd7d
zzawang <hobin0116@naver.com>
2023-10-24 20:41:59 +0900

    test: 사용자의 숫자가 중복될 때 테스트 코드 추가
    - 사용자가 122, 333 등 중복된 수를 입력했을 때 IllegalArgumentException가 발생하는지 테스트

--- src/test/java/baseball/ExceptionTest.java
+++ src/test/java/baseball/ExceptionTest.java
@@ -27,6 +27,16 @@ public class ExceptionTest extends NsTest {
         }
     }
 
+    @Test
+    void 사용자의_숫자가_중복됨() {
+        String[] testValues = {"122", "333", "000"};
+
+        for (String testValue : testValues) {
+            assertThatThrownBy(() -> runException(testValue))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
     @Override
     protected void runMain() {
         Application.main(new String[]{});
