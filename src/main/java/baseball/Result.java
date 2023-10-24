2d4796b9eae97a4ea701b0aaa8a99029cb012e52
bnfkim <kgr3819@gmail.com>
2023-10-24 17:41:37 +0900

    Refactor: Result Message

--- src/main/java/baseball/Result.java
+++ src/main/java/baseball/Result.java
@@ -1,4 +1,15 @@
 package baseball;
 
 public class Result {
+    private static final String NOTHING = "낫싱";
+    private static final String BALL = "볼";
+    private static final String STRIKE = "스트라이크";
+
+    private final int ball;
+    private final int strike;
+
+    public Result(int ball, int strike){
+        this.ball = ball;
+        this.strike = strike;
+    }
 }
