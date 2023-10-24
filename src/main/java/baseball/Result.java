3ed00d260eefd6316213dfd87030c54760cd01ce
julie0005 <julie0005@ajou.ac.kr>
2023-10-25 02:21:01 +0900

    feat: 사용자가 입력한 수와, 정답을 비교하여 계산하는 메소드 구현

--- src/main/java/baseball/Result.java
+++ src/main/java/baseball/Result.java
@@ -4,10 +4,34 @@ public class Result {
     int ball = 0;
     int strike = 0;
 
+    public Result(int ball, int strike) {
+        this.ball = ball;
+        this.strike = strike;
+    }
+
+    public static Result calculate(String input, String answer) {
+        int strike = 0;
+        int ball = 0;
+        for(int i=0; i<input.length(); i++) {
+            String number = input.substring(i, i+1);
+            if (input.charAt(i) == answer.charAt(i)) strike++;
+            else if (answer.contains(number)) ball++;
+        }
+        return new Result(ball, strike);
+    }
+
     @Override
     public String toString() {
         if (strike == 3) { return "3스트라이크"; }
         else if (ball == 0 && strike == 0) { return "낫싱"; }
-        else { return ball+"볼" + " " + strike + "strike"; }
+        else { return ball+"볼" + " " + strike + "스트라이크"; }
+    }
+
+    public int getBall() {
+        return ball;
+    }
+
+    public int getStrike() {
+        return strike;
     }
 }
