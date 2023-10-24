d5666fe617e3db534013961a4110b6071d768d02
miro-22 <rlawlgus96@snu.ac.kr>
2023-10-24 13:37:41 +0900

    fix: method has only one step indent for optimization

--- src/main/java/baseball/Validation.java
+++ src/main/java/baseball/Validation.java
@@ -1,5 +1,8 @@
 package baseball;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Validation {
     private static final int START_INCLUSIVE = Constants.START_INCLUSIVE;
     private static final int END_INCLUSIVE = Constants.END_INCLUSIVE;
@@ -8,26 +11,33 @@ public class Validation {
     private static final String QUIT = Constants.QUIT;
 
     public void validateNumberInput(String input) {
-        // 입력값이 3자리 숫자인지 확인
+        validateLengthOfDigits(input);
+        validateFormatOfDigits(input);
+        validateNoDuplicateDigits(input);
+    }
+
+    private void validateLengthOfDigits(String input) {
         if (input.length() != ANSWER_DIGIT) {
             throw new IllegalArgumentException();
         }
+    }
 
-        for (int i = 0; i < input.length(); i++) {
-            char digit = input.charAt(i);
-
-            // 입력값이 숫자가 아닌 경우
-            if (digit < (char) ('0' + START_INCLUSIVE) || digit > (char) ('0' + END_INCLUSIVE)) {
+    private void validateFormatOfDigits(String input) {
+        for (char digit : input.toCharArray()) {
+            if (!Character.isDigit(digit) || digit < '0' + START_INCLUSIVE || digit > '0' + END_INCLUSIVE) {
                 throw new IllegalArgumentException();
             }
+        }
+    }
 
-            // 입력값 중복 확인
-            for (int j = 0; j < i; j++) {
-                if (input.charAt(j) == digit) {
-                    throw new IllegalArgumentException();
-                }
+    private void validateNoDuplicateDigits(String input) {
+        Set<Character> uniqueDigits = new HashSet<>();
+        for (char digit : input.toCharArray()) {
+            if (!uniqueDigits.add(digit)) {
+                throw new IllegalArgumentException();
             }
         }
+
     }
 
     public void validateGameStatusInput(String gameStatusInput) {
