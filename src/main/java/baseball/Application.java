eb0099c8302d378e81a8432dbb27180a119723f7
sunwoong <sunwoong1122@gmail.com>
2023-10-24 21:13:21 +0900

    feat: 숫자야구 게임 시작 로직 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import baseball.config.DependencyContainer;
+import baseball.controller.MainController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        DependencyContainer dependencyContainer = new DependencyContainer();
+        MainController mainController = dependencyContainer.getMainController();
+        mainController.startBaseballGame();
     }
 }
