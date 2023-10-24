a44f98c0a90a5e261c357b158ebdfbf67572b252
mainfn <_@mainfn.dev>
2023-10-24 17:47:59 +0900

    test: 예외 메세지 public

--- src/main/java/baseball/exception/InvalidBallNumbersInputException.java
+++ src/main/java/baseball/exception/InvalidBallNumbersInputException.java
@@ -2,7 +2,7 @@ package baseball.exception;
 
 public final class InvalidBallNumbersInputException extends IllegalArgumentException {
 
-    private static final String INVALID_NUMBERS_INPUT_MESSAGE = "유효하지 않은 입력입니다.\n반드시 3자리 숫자로 입력하셔야 합니다.";
+    public static final String INVALID_NUMBERS_INPUT_MESSAGE = "유효하지 않은 입력입니다.\n반드시 3자리 숫자로 입력하셔야 합니다.";
 
     public InvalidBallNumbersInputException() {
         super(INVALID_NUMBERS_INPUT_MESSAGE);
--- src/main/java/baseball/exception/InvalidGameStatusNumberInput.java
+++ src/main/java/baseball/exception/InvalidGameStatusNumberInput.java
@@ -2,7 +2,7 @@ package baseball.exception;
 
 public final class InvalidGameStatusNumberInput extends IllegalArgumentException {
 
-    private static final String INVALID_GAME_STATUS_INPUT_MESSAGE = "유효하지 않은 입력입니다.\n재시작 여부는 반드시 1, 2 중 하나를 입력하셔야 합니다.";
+    public static final String INVALID_GAME_STATUS_INPUT_MESSAGE = "유효하지 않은 입력입니다.\n재시작 여부는 반드시 1, 2 중 하나를 입력하셔야 합니다.";
 
     public InvalidGameStatusNumberInput() {
         super(INVALID_GAME_STATUS_INPUT_MESSAGE);
