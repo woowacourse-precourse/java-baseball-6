3df72cf0490c163d2ded5102169083de6356fe95
hajungIm <ihj_ewha@ewhain.net>
2023-10-25 01:13:35 +0900

    Feat: main 코드 작성
    시작 안내문 출력,
    게임 시작 메소드 호출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,10 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        printStartStmt();
+        startGame();
+    }
+    private static void printStartStmt() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
     }
 }
