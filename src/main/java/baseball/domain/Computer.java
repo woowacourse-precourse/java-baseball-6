3f4565b49c8c4b34155dc605612045e798b23471
jun <ggprgrkjh@naver.com>
2023-10-24 17:30:44 +0900

    refactor: Computer 도메인 매직 넘버 제거

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -10,11 +10,13 @@ public class Computer {
     private static final int COMPUTER_START_NUMBER = 1;
     private static final int COMPUTER_END_NUMBER = 9;
 
+    private static final int COMPUTER_NUMBER_MAX_SIZE = 3;
+
     List<Integer> computerNumber;
 
     public List<Integer> randomComputerNumber() {
         computerNumber = new ArrayList<>();
-        while (computerNumber.size() < 3) {
+        while (computerNumber.size() < COMPUTER_NUMBER_MAX_SIZE) {
             int randomNumber = Randoms.pickNumberInRange(COMPUTER_START_NUMBER, COMPUTER_END_NUMBER);
             if (!computerNumber.contains(randomNumber)) {
                 computerNumber.add(randomNumber);
