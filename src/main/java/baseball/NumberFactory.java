4b67d324f87d12da2ffab6fe0ca06a0a03923766
박준영 <wnsdud6969@naver.com>
2023-10-24 17:50:34 +0900

    feat(NumberFactory): add NumberFactory

---
+++ src/main/java/baseball/NumberFactory.java
@@ -0,0 +1,51 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberFactory {
+
+    public static Number createByComputer(NumberGameConfiguration numberGameConfiguration) {
+        int minimumNumberOfRange = numberGameConfiguration.getMinimumNumberOfRange();
+        int maximumNumberOfRange = numberGameConfiguration.getMaximumNumberOfRange();
+        int numberOfDigits = numberGameConfiguration.getNumberOfDigits();
+
+        List<Integer> computerNumbers = new ArrayList<>();
+
+        while (computerNumbers.size() < numberOfDigits) {
+            int randomNumber = Randoms.pickNumberInRange(minimumNumberOfRange, maximumNumberOfRange);
+
+            if (!computerNumbers.contains(randomNumber)) {
+                computerNumbers.add(randomNumber);
+            }
+        }
+
+        return new Number(computerNumbers);
+    }
+
+    public static Number createByUser() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String userInput = Console.readLine();
+        List<Integer> userNumbers = parseUserInput(userInput);
+
+        return new Number(userNumbers);
+    }
+
+    private static List<Integer> parseUserInput(String userInput) {
+        List<Integer> userNumbers = new ArrayList<>();
+
+        for (char digit : userInput.toCharArray()) {
+            if (!Character.isDigit(digit)) {
+                throw new IllegalArgumentException();
+            }
+
+            int number = Integer.parseInt(String.valueOf(digit));
+            userNumbers.add(number);
+        }
+
+        return userNumbers;
+    }
+}
