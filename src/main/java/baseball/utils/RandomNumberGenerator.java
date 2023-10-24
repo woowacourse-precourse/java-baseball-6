f96b2e37f96162d9de2c90fe1d977b390056ed41
twoosky <lhjae3@naver.com>
2023-10-25 01:10:37 +0900

    fix: randomNumberGenrator 로직 수정

--- src/main/java/baseball/utils/RandomNumberGenerator.java
+++ src/main/java/baseball/utils/RandomNumberGenerator.java
@@ -2,7 +2,6 @@ package baseball.utils;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 public class RandomNumberGenerator implements NumberGenerator {
@@ -10,12 +9,24 @@ public class RandomNumberGenerator implements NumberGenerator {
     public static final int MIN_RANGE = 1;
     public static final int MAX_RANGE = 9;
 
+//    @Override
+//    public List<Integer> generate() {
+//        HashSet<Integer> picks = new HashSet<>();
+//        while (picks.size() < NUMBER_LENGTH) {
+//            picks.add(Randoms.pickNumberInRange(MIN_RANGE, MAX_RANGE));
+//        }
+//        return new ArrayList<>(picks);
+//    }
+
     @Override
     public List<Integer> generate() {
-        HashSet<Integer> picks = new HashSet<>();
-        while (picks.size() < NUMBER_LENGTH) {
-            picks.add(Randoms.pickNumberInRange(MIN_RANGE, MAX_RANGE));
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
         }
-        return new ArrayList<>(picks);
+        return computer;
     }
 }
--- src/test/java/baseball/domain/BaseballGameTest.java
+++ src/test/java/baseball/domain/BaseballGameTest.java
@@ -1,41 +1,41 @@
-//package baseball.domain;
-//
-//import baseball.config.ComputerTestConfig;
-//import java.util.List;
-//import java.util.stream.Stream;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.DisplayNameGeneration;
-//import org.junit.jupiter.api.DisplayNameGenerator;
-//import org.junit.jupiter.api.Nested;
-//import org.junit.jupiter.params.ParameterizedTest;
-//import org.junit.jupiter.params.provider.Arguments;
-//import org.junit.jupiter.params.provider.MethodSource;
-//
-//@DisplayName("BaseballGame 클래스")
-//public class BaseballGameTest {
-//
-//    @Nested
-//    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-//    class compare_메소드는 extends ComputerTestConfig {
-//        private static Stream<Arguments> providePlayerAndComputerNumbers() {
-//            return Stream.of(
-//                    Arguments.of("132", List.of(1, 2, 3), 2, 1),
-//                    Arguments.of("123", List.of(1, 2, 3), 0, 3),
-//                    Arguments.of("312", List.of(1, 2, 3), 3, 0)
-//            );
-//        }
-//
-//        @ParameterizedTest
-//        @MethodSource("providePlayerAndComputerNumbers")
-//        void Player와_Computer의_숫자를_비교해_Baseball_객체를_반환한다(
-//                String playerNumbers, List<Integer> computerNumbers, int ballCnt, int strikeCnt
-//        ) {
-//            Player player = new Player(playerNumbers);
-//            Computer computer = new TestComputer(computerNumbers);
-//            BaseballGame baseballGame = new BaseballGame(computer, player);
-//
-//            Assertions.assertEquals(baseballGame.compare(), new Baseball(ballCnt, strikeCnt));
-//        }
-//    }
-//}
+package baseball.domain;
+
+import baseball.config.ComputerTestConfig;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+@DisplayName("BaseballGame 클래스")
+public class BaseballGameTest {
+
+    @Nested
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+    class compare_메소드는 extends ComputerTestConfig {
+        private static Stream<Arguments> providePlayerAndComputerNumbers() {
+            return Stream.of(
+                    Arguments.of("132", List.of(1, 2, 3), 2, 1),
+                    Arguments.of("123", List.of(1, 2, 3), 0, 3),
+                    Arguments.of("312", List.of(1, 2, 3), 3, 0)
+            );
+        }
+
+        @ParameterizedTest
+        @MethodSource("providePlayerAndComputerNumbers")
+        void Player와_Computer의_숫자를_비교해_Baseball_객체를_반환한다(
+                String playerNumbers, List<Integer> computerNumbers, int ballCnt, int strikeCnt
+        ) {
+            Player player = new Player(playerNumbers);
+            Computer computer = new TestComputer(computerNumbers);
+            BaseballGame baseballGame = new BaseballGame(computer, player);
+
+            Assertions.assertEquals(baseballGame.compare(), new Baseball(ballCnt, strikeCnt));
+        }
+    }
+}
