13db9f91dc95f19432a1c90c597f58e5c387e3a3
becooq81 <becooq81@gmail.com>
2023-10-25 01:40:07 +0900

    feat(ComputerBalls): class to represent balls created for computer side in the game - variables: static final variables for number of balls, min/max value acceptable to create balls, list to save created balls - methods   - no args constructor (no args necessary b/c numbers will be randomly selected)   - getter for list of balls: necessary to access each ball for accurate score calculation   - generateComputerBalls: randomly picks a number, added to set if not repeated, and creates balls based on resulting set

---
+++ src/main/java/baseball/model/ComputerBalls.java
@@ -0,0 +1,32 @@
+package baseball.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ComputerBalls {
+    private static final int BALLS_SIZE = 3;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+
+    private final List<Ball> computerBalls;
+
+    public ComputerBalls() {
+        this.computerBalls = generateComputerBalls();
+    }
+
+    public List<Ball> getComputerBalls() {
+        return this.computerBalls;
+    }
+
+    public List<Ball> generateComputerBalls() {
+        Set<Integer> candidates = new HashSet<>();
+        while (candidates.size() < BALLS_SIZE) {
+            candidates.add(Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER));
+        }
+        return candidates.stream()
+                .map(Ball::new)
+                .collect(java.util.stream.Collectors.toList());
+    }
+}
