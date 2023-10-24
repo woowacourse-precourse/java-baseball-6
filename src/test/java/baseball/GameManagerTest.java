b94c2311e3fda6f3b63a4a62f462663a18f85631
bamm2 <xxknu88@naver.com>
2023-10-24 13:49:30 +0900

    Test: 게임종료 후 재시작또는 종료할지 선택하는 입력에서 잘못된 값 입력테스트

--- src/test/java/baseball/GameManagerTest.java
+++ src/test/java/baseball/GameManagerTest.java
@@ -1,4 +1,19 @@
-import static org.junit.jupiter.api.Assertions.*;
+package baseball;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class GameManagerTest {
-  
+
+    private GameManager gameManager = new GameManager();
+
+    @Test
+    @DisplayName("게임 종료 후 , 재시작 또는 종료 외의 다른 입력시, 예외처리")
+    public void GameManagerTest() {
+        assertThatThrownBy(()->gameManager.validateRestartOrExitWrongInput("3"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
