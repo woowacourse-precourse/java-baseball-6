6e473029379ecae1569102aecf3d893a38ab29c7
ahma0 <dsd932@naver.com>
2023-10-24 17:55:33 +0900

    refactor: 게임 재시작 로직 수정

--- src/main/java/baseball/play/PlayGame.java
+++ src/main/java/baseball/play/PlayGame.java
@@ -9,6 +9,7 @@ public class PlayGame {
 
     public final static String RESTART = "1";
     public final static String END = "2";
+    private static final String NOT_RIGHT_ANSWER = "1 혹은 2만 입력하실 수 있습니다.";
 
     private final Print print;
     private final Computer computer;
@@ -46,14 +47,11 @@ public class PlayGame {
         print.printWantToRestartOrEnd();
         String answer = Console.readLine();
 
-//        if(!isRightAnswer(answer))
-//            throw new IllegalArgumentException("잘못된 수를 입력하였습니다.");
-
-        return answer.equals(RESTART);      //1일 경우 게임을 재시작합니다.
-    }
-
-    private boolean isRightAnswer(String answer) {
-        return answer.equals(RESTART) || answer.equals(END);
+        return switch (answer) {
+            case RESTART -> true;
+            case END -> false;
+            default -> throw new IllegalArgumentException(NOT_RIGHT_ANSWER);
+        };
     }
 
 }
