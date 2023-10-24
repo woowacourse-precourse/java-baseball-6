160c2efc1e840aa372826d7c3ced73c2aaf6f08a
masonkimseoul <masonkimseoul@gmail.com>
2023-10-24 22:49:09 +0900

    test : (BaseballTest.java) Add checking result is correct or not

--- src/test/java/baseball/BaseballTest.java
+++ src/test/java/baseball/BaseballTest.java
@@ -34,4 +34,16 @@ public class BaseballTest {
         List<Integer> computerNumbers = (List<Integer>) method.invoke(baseball);
         assertThat(computerNumbers).allMatch(num -> num > 0 && num < 10);
     }
+
+    @Test
+    void 비교_결과_테스트() throws Exception {
+        Method method = baseball.getClass().getDeclaredMethod("checkNumbers", List.class, int.class);
+        method.setAccessible(true);
+
+        boolean baseballResult = (boolean) method.invoke(baseball, List.of(3, 5, 7), 573);
+        assertThat(baseballResult).isEqualTo(false);
+
+        baseballResult = (boolean) method.invoke(baseball, List.of(5, 7, 3), 573);
+        assertThat(baseballResult).isEqualTo(true);
+    }
 }
