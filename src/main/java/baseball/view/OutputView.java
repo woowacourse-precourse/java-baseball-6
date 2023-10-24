1bcdd61b16f8254274dd151627d34b23aa0fde63
soochan <tncksdl05@gmail.com>
2023-10-24 21:10:10 +0900

    refactor: 게임 결과 출력을 여러 상황에 맞게 출력
    게임 결과에 맞는 결과만 출력하도록 수정
    공백 라인 출력을 하드 코딩에서 벗어나도록 수정

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,11 +4,12 @@ import baseball.dto.response.GameResultDto;
 
 public class OutputView {
 
-    public static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
-    private static final String NOTHING_MESSAGE = "낫싱";
-    private static final String BALL_COUNT_MESSAGE_FORMAT = "%d볼 ";
-    private static final String STRIKE_COUNT_MESSAGE_FORMAT = "%d스트라이크";
+    public static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n";
+    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.\n";
+    private static final String NOTHING_MESSAGE = "낫싱\n";
+    private static final String BALL_COUNT_MESSAGE_FORMAT = "%d볼\n";
+    private static final String BALL_AND_STRIKE_MESSAGE_FORMAT = "%d볼 %d스트라이크\n";
+    private static final String STRIKE_COUNT_MESSAGE_FORMAT = "%d스트라이크\n";
     private static final int ZERO_STRIKE_COUNT = 0;
     private static final int ZERO_BALL_COUNT = 0;
     private static final int THREE_STRIKE_COUNT = 3;
@@ -21,51 +22,76 @@ public class OutputView {
     }
 
     public void printGameStartMessage() {
-        System.out.println(GAME_START_MESSAGE);
+        System.out.printf(GAME_START_MESSAGE);
     }
 
     public void printGameResult(GameResultDto gameResultDto) {
         int ballCount = gameResultDto.getBallCount();
         int strikeCount = gameResultDto.getStrikeCount();
         if (isNothing(ballCount, strikeCount)) {
-            System.out.println(NOTHING_MESSAGE);
+            printNothingResultMessage();
             return;
         }
-        printResult(ballCount, strikeCount);
-    }
 
-    private void printResult(int ballCount, int strikeCount) {
-        printIfHasBall(ballCount);
-        printIfHasStrike(ballCount, strikeCount);
-    }
-
-    private void printIfHasStrike(int ballCount, int strikeCount) {
-        if (strikeCount > ZERO_STRIKE_COUNT) {
-            System.out.printf(STRIKE_COUNT_MESSAGE_FORMAT, strikeCount);
-            printEndMessageIfAllStrike(ballCount, strikeCount);
+        if (isOnlyBall(ballCount, strikeCount)) {
+            printOnlyBall(ballCount);
+            return;
         }
-        printEmptyLine();
-    }
 
-    private void printEndMessageIfAllStrike(int ballCount, int strikeCount) {
-        if (ballCount == ZERO_BALL_COUNT && strikeCount == THREE_STRIKE_COUNT) {
-            printEmptyLine();
-            System.out.print(GAME_END_MESSAGE);
+        if (isBallAndStrike(ballCount, strikeCount)) {
+            printBallAndStrike(ballCount, strikeCount);
+            return;
         }
-    }
 
-    private void printEmptyLine() {
-        System.out.println();
-    }
+        if (isOnlyStrike(ballCount, strikeCount)) {
+            printOnlyStrike(strikeCount);
+            return;
+        }
 
-    private void printIfHasBall(int ballCount) {
-        if (ballCount > ZERO_BALL_COUNT) {
-            System.out.printf(BALL_COUNT_MESSAGE_FORMAT, ballCount);
+        if (isThreeStrike(ballCount, strikeCount)) {
+            printThreeStrikes(strikeCount);
         }
     }
 
     private boolean isNothing(int ballCount, int strikeCount) {
         return ballCount == ZERO_BALL_COUNT && strikeCount == ZERO_STRIKE_COUNT;
+    }
+
+    private void printNothingResultMessage() {
+        System.out.printf(NOTHING_MESSAGE);
+    }
+
+    private boolean isOnlyBall(int ballCount, int strikeCount) {
+        return ballCount > ZERO_BALL_COUNT && strikeCount == ZERO_STRIKE_COUNT;
+    }
+
+    private void printOnlyBall(int ballCount) {
+        System.out.printf(BALL_COUNT_MESSAGE_FORMAT, ballCount);
+    }
+
+    private boolean isBallAndStrike(int ballCount, int strikeCount) {
+        return ballCount > ZERO_BALL_COUNT && strikeCount > ZERO_STRIKE_COUNT && strikeCount < THREE_STRIKE_COUNT;
+    }
+
+    private void printBallAndStrike(int ballCount, int strikeCount) {
+        System.out.printf(BALL_AND_STRIKE_MESSAGE_FORMAT, ballCount, strikeCount);
+    }
+
+    private boolean isOnlyStrike(int ballCount, int strikeCount) {
+        return ballCount == ZERO_BALL_COUNT && strikeCount > ZERO_STRIKE_COUNT && strikeCount < THREE_STRIKE_COUNT;
+    }
+
+    private void printOnlyStrike(int strikeCount) {
+        System.out.printf(STRIKE_COUNT_MESSAGE_FORMAT, strikeCount);
+    }
+
+    private boolean isThreeStrike(int ballCount, int strikeCount) {
+        return ballCount == ZERO_BALL_COUNT && strikeCount == THREE_STRIKE_COUNT;
+    }
+
+    private void printThreeStrikes(int strikeCount) {
+        System.out.printf(STRIKE_COUNT_MESSAGE_FORMAT, strikeCount);
+        System.out.print(GAME_END_MESSAGE);
     }
 
     private static class LazyHolder {
