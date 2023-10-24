8965d1c92e34b58f130ad954a5b926570a750086
BDlhj <akk808@naver.com>
2023-10-24 09:52:43 +0900

    feat: Implement printStartGameMessage() in src/main/java/baseball/GameStartView.java

--- src/main/java/baseball/GameStartView.java
+++ src/main/java/baseball/GameStartView.java
@@ -1,2 +1,9 @@
-package baseball;public class GameStartView {
+package baseball;
+
+public class GameStartView {
+    private static final String GameStartMessage = "숫자 야구 게임을 시작합니다.";
+
+    public void printStartGameMessage() {
+        System.out.println(GameStartMessage);
+    }
 }
