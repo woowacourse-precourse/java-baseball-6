2b8cc902eb413a5733f60a017544f6971dc52d85
ChoiSeungWoo98 <csw980928@gmail.com>
2023-10-24 22:45:08 +0900

    주어진 요구사항 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,29 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
+// 기능목록
+//
+//  게임 준비
+//      "숫자 야구 게임을 시작합니다." 문구 출력
+//      서로 다른 3가지 숫자 주어진 랜덤 함수로 뽑아오기
+//
+//  게임 시작(사용자가 맞힐 때 까지 반복)
+//      사용자로 부터 서로 다른 3자리 숫자를 입력 받는다.
+//      만약 3자리가 아니거나 숫자만 있지 않거나 서로 다르지 않다면 IllegalArgumentException 에러 발생
+//      이때 "숫자를 입력해주세요 : " 문구를 줄바꿈 없이 출
+//      입력된 값의 스트라이크, 볼의 갯수를 계산
+//      같은 자리수를 비교해 스트라이크 카운트, indexOf를 이용하여 볼 카운트
+//      볼 - 스트라이크 = 실제 볼 수
+//      스트라이크만 존재한다면 x스트라이크 출력
+//      볼 만 존재한다면 x볼 출력
+//      볼, 스트라이크가 같이 존재한다면 x볼 x스트라이크 출력
+//      볼, 스트라이크가 존재하지 않는다면 낫싱 출력
+//
+//  사용자 정답
+//      정답 시 "3개의 숫자를 모두 맞히셨습니다! 게임 종료" 문구 출력 후
+//      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요." 출력해 게임 지속여부 확인
+//      만약 숫자가 아니거나 숫자 1 혹은 2가 아니거나 1글자를 넘어서면 IllegalArgumentException 에러 발생
+
     static final String NUM_REG = "[0-9]+";
 
     public static void main(String[] args) {
