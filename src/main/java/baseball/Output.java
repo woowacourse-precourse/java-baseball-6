0fb5f355ef810528e9739ff46eac0f133e442746
acycoco <pink10257@naver.com>
2023-10-25 00:39:02 +0900

    refactor(Output): 볼, 스트라이크, 낫싱 상수로 변경

--- src/main/java/baseball/Output.java
+++ src/main/java/baseball/Output.java
@@ -4,6 +4,9 @@ public class Output {
 
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String BALL_MESSAGE = "볼";
+    private static final String STRIKE_MESSAGE = "스트라이크";
+    private static final String NOTHING_MESSAGE = "낫싱";
 
     public void printGameStart() {
         System.out.println(GAME_START_MESSAGE);
@@ -19,15 +22,15 @@ public class Output {
         int balls = gameResult.getBalls();
 
         if (balls > 0) {
-            System.out.print(balls + "볼 ");
+            System.out.print(balls + BALL_MESSAGE + " ");
         }
 
         if (strikes > 0) {
-            System.out.print(strikes + "스트라이크");
+            System.out.print(strikes + STRIKE_MESSAGE);
         }
 
         if (gameResult.isNothing()) {
-            System.out.print("낫싱");
+            System.out.print(NOTHING_MESSAGE);
         }
 
         System.out.println();
