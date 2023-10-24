af63b19c4ab6407c9ccf52d74c2d60722f55c58d
sengjun0624 <sengjun0624@naver.com>
2023-10-25 02:09:14 +0900

    feat(User): add user class to manage user information

---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,13 @@
+package baseball;
+
+public class User {
+    int[] userNumbers;
+
+    public int[] getUserNumbers() {
+        return userNumbers;
+    }
+
+    public void setUserNumbers(int[] userNumbers) {
+        this.userNumbers = userNumbers;
+    }
+}
