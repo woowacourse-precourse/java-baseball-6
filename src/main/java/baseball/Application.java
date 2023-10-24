71596187208b6ad2f6087f651b05548eef0f7820
jihun-24k <gi020477@gmail.com>
2023-10-25 01:59:29 +0900

    feat: 사용자입력값 문구 출력 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,12 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
+
+        outer:
+        while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
+
+            break outer;
+        }
     }
 }
