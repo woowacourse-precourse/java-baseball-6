2f9ee189a83444c0e9c85a46f47a24d8d3fdbf31
Choi Seunghun <0015chseang@gmail.com>
2023-10-24 23:02:48 +0900

    fix: test 통과 위해 예외 처리 코드 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,26 +9,19 @@ public class Application {
         while (true) {
             BaseballGameService gameService = new BaseballGameService();
             GameResult result = null;
+            do {
+                System.out.print("숫자를 입력해주세요 : ");
+                String input = Console.readLine();
+                result = gameService.guess(input);
+                System.out.println(result);
+            } while (!result.isGameWon());
 
-            try {
-                do {
-                    System.out.print("숫자를 입력해주세요 : ");
-                    String input = Console.readLine();
-                    result = gameService.guess(input);
-                    System.out.println(result);
-                } while (!result.isGameWon());
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. : ");
+            String choice = Console.readLine();
 
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. : ");
-                String choice = Console.readLine();
-
-                if (choice.equals("2")) {
-                    break;
-                }
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-                System.out.println("잘못된 입력으로 인해 게임이 종료됩니다.");
-                return;
+            if (choice.equals("2")) {
+                break;
             }
         }
     }
