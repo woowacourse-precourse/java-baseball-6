cf1b63f297e6b7c5f10078fc1d72a1bd2ca2d6b2
its-sky <smc9919@naver.com>
2023-10-24 23:45:58 +0900

    feat: 출력 메시지 변수로 분리

--- src/main/java/baseball/global/constant/OutputMessage.java
+++ src/main/java/baseball/global/constant/OutputMessage.java
@@ -7,5 +7,8 @@ public class OutputMessage {
     public static final String GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     public static final String THREE_STRIKE_MESSAGE = "3스트라이크";
     public static final String GAME_RESULT_MESSAGE = "%d볼 %d스트라이크";
-    public static final String NOTHING_ON_STRIKE = "낫싱";
+리    public static final String NOTHING_MESSAGE = "낫싱";
+    public static final String BALL_MESSAGE = "%d볼";
+    public static final String STRIKE_WITH_BALL_MESSAGE = " %d스트라이크";
+    public static final String STRIKE_WITHOUT_BALL_MESSAGE = "%d스트라이크";
 }
