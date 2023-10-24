203e2b0c9716c0a4512887c0fabcd6889fdf8cee
gyahn <pop2413@gmail.com>
2023-10-24 19:42:46 +0900

    fix: 숫자 비교 기능 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,17 +2,30 @@ package baseball;
 
 import java.util.Arrays;
 
+
+
 public class Application {
     // TODO: 프로그램 구현
     public static void main(String[] args) {
         RandomNumber randomNumber = new RandomNumber();
         InputNumber inputNumber = new InputNumber();
         CompareNumber compareNumber = new CompareNumber();
-        Replay replay = new Replay();
-        replay.replay();
+//        CompareNumber.CountDTO countDTO = new CompareNumber.CountDTO();
+//        CompareNumber.CountDTO countDTO = new CompareNumber.CountDTO();
 //        int[] a = inputNumber.createInputArr();
 //        int[] b = randomNumber.createRanArr();
-//        compareNumber.compareNumber(a,b);
+//        int strike = countDTO.getStrike();
+//        int ball = countDTO.getBall();
+//        compareNumber.getCount(a,b);
+//        compareNumber.returnCount();
+        int[] b = randomNumber.createRanArr();
+        while(true){
+            int[] a = inputNumber.createInputArr();
+            System.out.println(Arrays.toString(a));
+            System.out.println(Arrays.toString(b));
+            System.out.println(compareNumber.getCount(a,b));
+        }
+
 
     }
 
--- src/main/java/baseball/CompareNumber.java
+++ src/main/java/baseball/CompareNumber.java
@@ -3,27 +3,25 @@ package baseball;
 import java.util.Arrays;
 
 public class CompareNumber {
-    public void compareNumber(int[] inputArr,int[] randomArr){
-        int strike = 0;
+    public int getCount(int[] inputArr, int[] randomArr) {
         int ball = 0;
-        for(int i = 0;i<inputArr.length;i++){
-            if (Arrays.binarySearch(randomArr,inputArr[i])>= 0){
+        for (int i = 0; i < inputArr.length; i++) {
+            if (Arrays.binarySearch(randomArr, inputArr[i]) >= 0) {
                 ball += 1;
-                if(inputArr[i] == randomArr[i]){
-                    ball -= 1;
+            }
+        }
+        return ball;
+    }
+
+    public int getStrike(int[] inputArr, int[] randomArr) {
+        int strike = 0;
+        for (int i = 0; i < inputArr.length; i++) {
+            if (Arrays.binarySearch(randomArr, inputArr[i]) >= 0) {
+                if (inputArr[i] == randomArr[i]) {
                     strike += 1;
                 }
             }
         }
-        if(strike!=0 && ball != 0){
-            System.out.println(ball+"볼 " + strike+"스트라이크");
-        } else if(strike!=0){
-            System.out.println(strike+"스트라이크");
-        } else if(ball!=0){
-            System.out.println(ball+"볼");
-        } else if (strike==0 && ball == 0) {
-            System.out.println("낫싱");
-        }
-
+        return strike;
     }
 }
