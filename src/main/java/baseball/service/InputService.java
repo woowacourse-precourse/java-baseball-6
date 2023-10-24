08d9c5ab65141a02f64b19126046f22758622b13
minsu20 <syt07100@gmail.com>
2023-10-25 01:07:04 +0900

    refactor: 입력받을 때 validation체크하는 부분 함수 분리

--- src/main/java/baseball/service/InputService.java
+++ src/main/java/baseball/service/InputService.java
@@ -2,36 +2,28 @@ package baseball.service;
 
 import baseball.exception.InvalidNumberInputException;
 import baseball.exception.InvalidRestartInputException;
+import baseball.validator.InputValidator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputService {
-    public static String getNumber(int size, int start, int end) {
-        String input=Console.readLine();
-        if (!isValidNumberInput(input, start, end, size)) {
+    private final InputValidator validator;
+
+    public InputService() {
+        this.validator = new InputValidator();
+    }
+
+    public String getNumber(int size, int start, int end) {
+        String input = Console.readLine();
+        if (!validator.isValidNumberInput(input, start, end, size)) {
             throw new InvalidNumberInputException();
         }
         return input;
     }
 
-    public static boolean restart(String input, int restart, int exit) {
-        if (!isValidRestartOrExit(input, restart, exit)) {
+    public boolean restart(String input, int restart, int exit) {
+        if (!validator.isValidRestartOrExit(input, restart, exit)) {
             throw new InvalidRestartInputException();
         }
-        int isRestart=Integer.parseInt(input);
-        return isRestart == restart;
-    }
-
-    private static boolean isValidNumberInput(String input, int start, int end, int size) {
-        String pattern = "[" + start + "-" + end + "]{" + size + "}";
-        return input.matches(pattern);
-    }
-
-    private static boolean isValidRestartOrExit(String input, int restart, int exit) {
-        try {
-            int inputValue = Integer.parseInt(input);
-            return inputValue == restart || inputValue == exit;
-        } catch (NumberFormatException e) {
-            return false;
-        }
+        return Integer.parseInt(input) == restart;
     }
 }
