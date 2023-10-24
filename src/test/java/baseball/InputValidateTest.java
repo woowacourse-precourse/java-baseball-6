309d5a68a4ee2c8ef8b8ffeceabb0e719c569d7d
bamm2 <xxknu88@naver.com>
2023-10-24 13:33:19 +0900

    Feat : 입력에 숫자가 아닌 다른문자 입력시 예외테스트

--- src/test/java/baseball/InputValidateTest.java
+++ src/test/java/baseball/InputValidateTest.java
@@ -11,9 +11,16 @@ class InputValidateTest  {
 
     @Test
     @DisplayName("3글자 미만 또는 초과 입력 시, 예외처리 ")
-    public void InputValidateTest()  {
+    void 입력_글자수_예외처리()  {
         inputValidate = new InputValidate("abcd");
         assertThatThrownBy(()->inputValidate.validateSize()).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("숫자가 아닌 다른 문자를 입력한 경우 , 예외처리 ")
+    void 입력_숫자인지_검증_예외처리(){
+        inputValidate = new InputValidate("woa");
+        assertThatThrownBy(()->inputValidate.validateNumber()).isInstanceOf(IllegalArgumentException.class);
     }
 
 
