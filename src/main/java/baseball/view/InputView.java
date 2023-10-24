8f516123fc3d821e10313bbcd4d9ac3b19ee2aef
kmw2378 <rlarla677@gmail.com>
2023-10-24 14:13:11 +0900

    feat: InputView에 사용자로부터 입력을 받는 메소드 추가 사용자로 부터 숫자를 입력받는 정적 메소드를 추가하였습니다. 기본적인 검증(null 여부, 공백 여부)를 체크한 후 호출원에 입력한 값을 통해 DTO 객체를 만들어 반환합니다.

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,4 +1,20 @@
 package baseball.view;
 
+import baseball.common.exception.input.InputException;
+import baseball.dto.request.UserNumberRequest;
+import camp.nextstep.edu.missionutils.Console;
+
+import static baseball.common.exception.input.InputErrorCode.INVALID_INPUT_VALUE;
+import static baseball.view.message.GameMessage.QUESTION_MESSAGE;
+
 public class InputView {
+    public static UserNumberRequest requestUserNumber() {
+        System.out.println(QUESTION_MESSAGE);
+        final String arg = Console.readLine();
+        if (arg == null || arg.isBlank()) {
+            throw new InputException(INVALID_INPUT_VALUE);
+        }
+
+        return new UserNumberRequest(arg);
+    }
 }
