bc0566606ebb725379cb2daa70225fe1e18ae391
chjcoder <chjcoder@gmail.com>
2023-10-24 10:00:56 +0900

    [feat] Application 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,19 @@
 package baseball;
+import baseball.controller.Controller;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.*;
+
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Controller controller = new Controller();
+        controller.run();
+//        int i = Randoms.pickNumberInRange(1,9);
+//        System.out.println(i);
     }
 }
