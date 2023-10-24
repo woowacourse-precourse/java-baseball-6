aa9aacd77de9b9abd2327d5131d0abbd10d5771a
OhJongMin <jmoh7480@gmail.com>
2023-10-25 00:59:50 +0900

    feat:Add Game Rule Static Value

---
+++ src/main/java/baseball/domain/GameRule.java
@@ -0,0 +1,9 @@
+package baseball.domain;
+
+public class GameRule {
+    public static final int userNumberLength = 3;
+    public static final int userNumberMax = 9;
+    public static final int userNumberMin = 1;
+    public static final int restartNumber = 1;
+    public static final int endGameNumber = 2;
+}
