7766d2563632cceaab65363db45c5fcd779c3263
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2023-10-24 18:38:55 +0900

    feat: Application.java에 게임 실행 코드 작성
    start, gameOver, restart 메서드가 gameFlag에 맞춰 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import baseball.service.BaseBallGameService;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        boolean gameFlag = true;
+
+        while(gameFlag) {
+            BaseBallGameService game = new BaseBallGameService();
+
+            game.start();
+            game.gameOver();
+            game.restart();
+        }
     }
 }
