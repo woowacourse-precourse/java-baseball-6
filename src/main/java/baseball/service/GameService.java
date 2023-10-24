e61b5c8b0e5c980ce89c199361683eaba80f6a2e
ghdeo <goldencorn7@naver.com>
2023-10-25 01:09:17 +0900

    feat: Add getComputerNumber method

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -1,7 +1,25 @@
 package baseball.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+
 public class GameService {
+    private List<Integer> computer;
+
     public void playGame() {
 
+    }
+
+    List<Integer> getComputerNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        return computer;
     }
 }
