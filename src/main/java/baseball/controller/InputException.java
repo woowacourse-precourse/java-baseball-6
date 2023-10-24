e4994fd32525c8d10a4920beba2b2226f121dc6d
“sangjun121” <juncho12011201@gmail.com>
2023-10-25 04:39:12 +0900

    refactor: 사용자 입력 적합성 판단 메소드 반환값 boolean -> void로 변경
    사용자 입력에 대한 예외처리를 해당 메소드에서 진행하므로 외부로 결과값을 반환할 필요가 없다고 판단되어 수정

--- src/main/java/baseball/controller/InputException.java
+++ src/main/java/baseball/controller/InputException.java
@@ -7,15 +7,11 @@ import static baseball.utility.Constants.VALID_NUMBER_PATTERN;
 public class InputException { //인스턴스 생성을 막아 메모리를 효율적으로 사용하고, 전역적으로 사용하기 위해 static 메소드 사용
     private InputException(){} //생성자 생성 못하도록 private 접근지정자 사용
 
-    public static boolean judgeValidInput(String input){
-        boolean isValidInput;
-
+    public static void judgeValidInput(String input){
         if(!judgeVaildNumber(input)){
             throw new IllegalArgumentException();
         }
 
-        isValidInput = true;
-        return isValidInput;
     }
 
     private static boolean judgeVaildNumber(String input){ //정규표현식을 사용하여 3자리의 적합한 숫자로 이루어진 문자열인지 확인한다.
