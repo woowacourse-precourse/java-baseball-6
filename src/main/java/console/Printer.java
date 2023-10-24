85ac253a6a85e8769f31b5e8284bba56fb4e6373
julie0005 <julie0005@ajou.ac.kr>
2023-10-25 02:22:23 +0900

    feat: 프린터가 결과 출력하도록 메소드 추가

--- src/main/java/console/Printer.java
+++ src/main/java/console/Printer.java
@@ -1,5 +1,7 @@
 package console;
 
+import baseball.Result;
+
 public class Printer {
     public static void printQuit(int perfectNumber) {
         System.out.println(perfectNumber+"개의 숫자를 모두 맞히셨습니다! 게임 종료");
@@ -11,5 +13,9 @@ public class Printer {
 
     public static void printExitOrRestart(int restart, int exit) {
         System.out.println("게임을 새로 시작하려면 "+restart+", 종료하려면 "+exit+"를 입력하세요");
+    }
+
+    public static void printResult(Result result) {
+        System.out.println(result.toString());
     }
 }
