c345405566fee39940e0370d38d929564b468061
mseo39 <a01063294905@gmail.com>
2023-10-24 21:54:19 +0900

    feat: add userInterface.class
    Classes with the ability to interact with users and computers

---
+++ src/main/java/baseball/userInterface.java
@@ -0,0 +1,19 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
+public class userInterface {
+    final static String startPhrases="숫자 야구 게임을 시작합니다.";
+    final static String inputPhrases="숫자를 입력해주세요 : ";
+    final static String endPhrases="개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    final static String restartChoicePhrases="게임을 새로 시작하려면 %d, 종료하려면 %d를 입력하세요.";
+    final static String ballPhrases="볼";
+    final static String strikePhrases="스트라이크";
+    final static String nothingPhrases="낫싱";
+    final static int endGameChoice=2;
+    final static int restartChoice=1;
+    public static List<Integer> userInputNumber;
+
+}
