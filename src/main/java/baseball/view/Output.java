0d37b0026c836d48f6502e9d6ab63df38e6b2962
jongchan0109 <jongchan1009@naver.com>
2023-10-24 15:20:38 +0900

    feat: 결과 출력 기능 구현

--- src/main/java/baseball/view/Output.java
+++ src/main/java/baseball/view/Output.java
@@ -1,21 +1,22 @@
 package baseball.view;
 
 public class Output{
-    void resultPrint(int ballCount, int strikeCount) {
+    public void resultPrint(int ballCount, int strikeCount) {
         if(strikeCount==3){
+            System.out.println(strikeCount+Speak.strikeSpeak);
             System.out.println(Speak.gameEndSpeak);
         }
         else if(strikeCount!=0 && ballCount!=0){
             System.out.println(ballCount+Speak.ballSpeak+" "+ strikeCount+Speak.strikeSpeak);
         }
-        else if(strikeCount==0){
-            System.out.println(ballCount+Speak.ballSpeak);
+        else if(strikeCount==0 && ballCount ==0){
+            System.out.println(Speak.notThingSpeak);
         }
         else if(ballCount == 0){
             System.out.println(strikeCount+Speak.strikeSpeak);
         }
         else{
-            System.out.println(Speak.notThingSpeak);
+            System.out.println(ballCount+Speak.ballSpeak);
         }
     }
 }
