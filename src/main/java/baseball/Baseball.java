ea4f936f0e3507c091a80153c8a3e09848d9a747
장세진 <jsejin95@gmail.com>
2023-10-25 01:16:35 +0900

    feat: 게임 시작 기능

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -7,11 +7,10 @@ import java.util.List;
 
 public class Baseball {
     private final Validator validator;
-    List<Integer> baseball;
 
-    public Baseball(Validator validator, List<Integer> baseball) {
+
+    public Baseball() {
         this.validator = new Validator();
-        this.baseball = baseball;
     }
 
 
@@ -19,9 +18,10 @@ public class Baseball {
         List<Integer> userNumList = new ArrayList<>();
         System.out.println("숫자를 입력해주세요 : ");
         String nums = Console.readLine();
-        if (!validator.validateUserNum(nums)){
-            throw new IllegalStateException("올바른 수를 입력하세요");
+        if (!validator.validateUserNum(nums)) {
+            throw new IllegalArgumentException("올바른 수를 입력하세요");
         }
+
         for (int i=0; i<3; i++) {
             userNumList.add(Integer.parseInt(nums.substring(i, i+1)));
         }
