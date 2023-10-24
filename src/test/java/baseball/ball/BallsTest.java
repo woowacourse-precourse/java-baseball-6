bb1f7eccaab03f8bd5e955a61264f603bf5462ea
chosunghyun18 <krismon@naver.com>
2023-10-24 22:17:39 +0900

    test : add test 3개의 공과 3개의 공을 비교하는 기능

--- src/test/java/baseball/ball/BallsTest.java
+++ src/test/java/baseball/ball/BallsTest.java
@@ -13,6 +13,45 @@ public class BallsTest {
     void setUp(){
         balls = new Balls(Arrays.asList(1,2,3));
     }
+    @Test
+    @DisplayName("복수 공 ,북수 공 비교 테스트 : 나싱")
+    public void BALLS_NOTHING_TEST(){
+        Balls targetBalls = new Balls(Arrays.asList(4,5,6));
+        SwingResult swingResult = balls.match(targetBalls);
+        assertThat(swingResult.getStrikeCount()).isEqualTo(0);
+        assertThat(swingResult.getBallCount()).isEqualTo(0);
+    }
+    @Test
+    @DisplayName("복수 공 ,북수 공 비교 테스트 : 2볼 1 스트라이크")
+    public void BALLS_2BALL_1STRIKE_TEST(){
+        Balls targetBalls = new Balls(Arrays.asList(1,3,2));
+        SwingResult swingResult = balls.match(targetBalls);
+        assertThat(swingResult.getStrikeCount()).isEqualTo(1);
+        assertThat(swingResult.getBallCount()).isEqualTo(2);
+
+        targetBalls = new Balls(Arrays.asList(3,2,1));
+        swingResult = balls.match(targetBalls);
+        assertThat(swingResult.getStrikeCount()).isEqualTo(1);
+        assertThat(swingResult.getBallCount()).isEqualTo(2);
+    }
+    @Test
+    @DisplayName("복수 공 ,북수 공 비교 테스트 : 1볼 1 스트라이크")
+    public void BALLS_1BALL_1STRIKE_TEST(){
+        Balls targetBalls = new Balls(Arrays.asList(2,7,3));
+        SwingResult swingResult = balls.match(targetBalls);
+        assertThat(swingResult.getStrikeCount()).isEqualTo(1);
+        assertThat(swingResult.getBallCount()).isEqualTo(1);
+    }
+    @Test
+    @DisplayName("복수 공 ,북수 공 비교 테스트 : 3 스트라이크")
+    public void BALLS_3STRIKE_TEST(){
+        Balls targetBalls = new Balls(Arrays.asList(1,2,3));
+        SwingResult swingResult = balls.match(targetBalls);
+        assertThat(swingResult.getStrikeCount()).isEqualTo(3);
+        assertThat(swingResult.getBallCount()).isEqualTo(0);
+        assertThat(swingResult.isAllStrike()).isTrue();
+    }
+
 
     @Test
     @DisplayName("복수 공 단일공 비교 테스트 : 나싱")
