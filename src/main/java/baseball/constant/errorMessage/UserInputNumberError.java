ae8a736d32013cfb461c501bec4f0773a49dabef
Hugh <lee2317884@gmail.com>
2023-10-25 08:57:28 +0900

    fix: 에러 상수 제거

--- src/main/java/baseball/constant/errorMessage/UserInputNumberError.java
+++
@@ -1,6 +0,0 @@
-package baseball.constant.errorMessage;
-
-public class UserInputNumberError {
-    public static final String USER_INPUT_BLANK_ERROR_MESSAGE = "[ERROR] 빈값을 입력할 수 없습니다. 다시 시도해주세요.";
-    public static final String USER_RESTART_INPUT_ERROR_MESSAGE = "[ERROR] 잘못된 값을 입력하셨습니다. 게임을 종료합니다.";
-}
--- src/main/java/baseball/constant/errorMessage/UserNumberError.java
+++
@@ -1,8 +0,0 @@
-package baseball.constant.errorMessage;
-
-public class UserNumberError {
-    public static final String USER_NUMBER_WRONG_INPUT_ERROR_MESSAGE = "[ERROR] 숫자가 아닌 값은 입력할 수 없습니다. 게임을 종료합니다.";
-    public static final String USER_NUMBER_WRONG_LENGTH_ERROR_MESSAGE = "[ERROR] 세 개의 숫자 입력이 필요합니다. 게임을 종료합니다.";
-    public static final String USER_NUMBER_WRONG_RANGE_ERROR_MESSAGE = "[ERROR] 1부터 9까지의 숫자를 입력해 주세요. 게임을 종료합니다.";
-    public static final String USER_NUMBER_DUPLICATE_ERROR_MESSAGE = "[ERROR] 중복된 값은 입력할 수 없습니다. 게임을 종료합니다.";
-}
