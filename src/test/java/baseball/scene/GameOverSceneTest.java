a9e3b7352a01f3e6a3afa5230e35489548874c28
wooteco-daram <wooteco.daram@gmail.com>
2023-10-24 19:50:55 +0900

    test: `GameOverSceneTest` 수정 하지 않는 테스트 케이스 수정 및 신규 예외 케이스 추가
    - `게임_종료_메시지_출력_호출_확인`
    - `게임_종료_메시지_출력_후_재시작`
    - `게임_종료_메시지_출력_후_종료`
    - `게임_종료_후_지원하지_않는_메뉴_입력시_IllegalArgumentException_예외`

--- src/test/java/baseball/scene/GameOverSceneTest.java
+++ src/test/java/baseball/scene/GameOverSceneTest.java
@@ -1,7 +1,7 @@
 package baseball.scene;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
@@ -9,25 +9,18 @@ import static org.mockito.internal.verification.VerificationModeFactory.times;
 import baseball.game.state.GameState;
 import baseball.game.state.GameStateManager;
 import baseball.input.GameOverSceneInput;
-import baseball.input.GameSceneInput;
 import baseball.output.GameOverSceneOutput;
-import baseball.output.GameSceneOutput;
-import baseball.util.RandomNumberGenerator;
-import camp.nextstep.edu.missionutils.test.NsTest;
-import java.io.ByteArrayInputStream;
-import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
 
 public class GameOverSceneTest {
 
     @Test
     void 게임_종료_메시지_출력_호출_확인() {
         final GameStateManager mockGameStateManager = mock(GameStateManager.class);
-        when(mockGameStateManager.isInGame()).thenReturn(true, false);
+        when(mockGameStateManager.isInGameOver()).thenReturn(true, false);
 
         final GameOverSceneInput mockInput = mock(GameOverSceneInput.class);
-        when(mockInput.getUserInput()).thenReturn("123");
+        when(mockInput.getUserInput()).thenReturn("1");
 
         final GameOverSceneOutput mockOutput = mock(GameOverSceneOutput.class);
         final GameOverScene gameOverScene = new GameOverScene(mockGameStateManager, mockInput, mockOutput);
@@ -37,78 +30,47 @@ public class GameOverSceneTest {
     }
 
     @Test
-    void 숫자_입력_메시지를_출력하는_메소드_호출_확인() {
-        try (final MockedStatic<RandomNumberGenerator> mockGenerator = mockStatic(RandomNumberGenerator.class)) {
-            mockGenerator.when(() -> RandomNumberGenerator.generate()).thenReturn(List.of(1, 2, 3));
+    void 게임_종료_메시지_출력_후_재시작() {
+        final GameStateManager mockGameStateManager = mock(GameStateManager.class);
+        when(mockGameStateManager.isInGameOver()).thenReturn(true, false);
 
-            final GameStateManager mockGameStateManager = mock(GameStateManager.class);
-            when(mockGameStateManager.isInGame()).thenReturn(true, false);
+        final GameOverSceneInput mockInput = mock(GameOverSceneInput.class);
+        when(mockInput.getUserInput()).thenReturn("1");
 
-            final GameSceneInput mockInput = mock(GameSceneInput.class);
-            when(mockInput.getUserInput()).thenReturn("123");
+        final GameOverSceneOutput mockOutput = mock(GameOverSceneOutput.class);
+        final GameOverScene gameOverScene = new GameOverScene(mockGameStateManager, mockInput, mockOutput);
+        gameOverScene.run();
 
-            final GameSceneOutput mockOutput = mock(GameSceneOutput.class);
-            final GameScene gameScene = new GameScene(mockGameStateManager, mockInput, mockOutput);
-            gameScene.run();
-
-            verify(mockOutput, times(1)).printInputMessage();
-        }
+        verify(mockGameStateManager, times(1)).update(GameState.GAME);
     }
 
     @Test
-    void 숫자_입력하는_메소드_호출_확인() {
-        try (final MockedStatic<RandomNumberGenerator> mockGenerator = mockStatic(RandomNumberGenerator.class)) {
-            mockGenerator.when(() -> RandomNumberGenerator.generate()).thenReturn(List.of(1, 2, 3));
+    void 게임_종료_메시지_출력_후_종료() {
+        final GameStateManager mockGameStateManager = mock(GameStateManager.class);
+        when(mockGameStateManager.isInGameOver()).thenReturn(true, false);
 
-            final GameStateManager mockGameStateManager = mock(GameStateManager.class);
-            when(mockGameStateManager.isInGame()).thenReturn(true, false);
+        final GameOverSceneInput mockInput = mock(GameOverSceneInput.class);
+        when(mockInput.getUserInput()).thenReturn("2");
 
-            final GameSceneInput mockInput = mock(GameSceneInput.class);
-            when(mockInput.getUserInput()).thenReturn("123");
+        final GameOverSceneOutput mockOutput = mock(GameOverSceneOutput.class);
+        final GameOverScene gameOverScene = new GameOverScene(mockGameStateManager, mockInput, mockOutput);
+        gameOverScene.run();
 
-            final GameSceneOutput mockOutput = mock(GameSceneOutput.class);
-            final GameScene gameScene = new GameScene(mockGameStateManager, mockInput, mockOutput);
-            gameScene.run();
-
-            verify(mockInput, times(1)).getUserInput();
-        }
+        verify(mockGameStateManager, times(1)).update(GameState.EXIT);
     }
 
     @Test
-    void 정답이_아닐시_재_입력_확인() {
-        try (final MockedStatic<RandomNumberGenerator> mockGenerator = mockStatic(RandomNumberGenerator.class)) {
-            mockGenerator.when(() -> RandomNumberGenerator.generate()).thenReturn(List.of(1, 2, 3));
+    void 게임_종료_후_지원하지_않는_메뉴_입력시_IllegalArgumentException_예외() {
+        final GameStateManager mockGameStateManager = mock(GameStateManager.class);
+        when(mockGameStateManager.isInGameOver()).thenReturn(true, false);
 
-            final GameStateManager mockGameStateManager = mock(GameStateManager.class);
-            when(mockGameStateManager.isInGame()).thenReturn(true, true, false);
+        final GameOverSceneInput mockInput = mock(GameOverSceneInput.class);
+        when(mockInput.getUserInput()).thenReturn("0");
 
-            final GameSceneInput mockInput = mock(GameSceneInput.class);
-            when(mockInput.getUserInput()).thenReturn("124", "123");
+        final GameOverSceneOutput mockOutput = mock(GameOverSceneOutput.class);
+        final GameOverScene gameOverScene = new GameOverScene(mockGameStateManager, mockInput, mockOutput);
 
-            final GameSceneOutput mockOutput = mock(GameSceneOutput.class);
-            final GameScene gameScene = new GameScene(mockGameStateManager, mockInput, mockOutput);
-            gameScene.run();
-
-            verify(mockInput, times(2)).getUserInput();
-        }
-    }
-
-    @Test
-    void 스트라이크로_정답을_맞출시_GAME_OVER_상태로_변경() {
-        try (final MockedStatic<RandomNumberGenerator> mockGenerator = mockStatic(RandomNumberGenerator.class)) {
-            mockGenerator.when(() -> RandomNumberGenerator.generate()).thenReturn(List.of(1, 2, 3));
-
-            final GameStateManager mockGameStateManager = mock(GameStateManager.class);
-            when(mockGameStateManager.isInGame()).thenReturn(true, false);
-
-            final GameSceneInput mockInput = mock(GameSceneInput.class);
-            when(mockInput.getUserInput()).thenReturn("123");
-
-            final GameSceneOutput mockOutput = mock(GameSceneOutput.class);
-            final GameScene gameScene = new GameScene(mockGameStateManager, mockInput, mockOutput);
-            gameScene.run();
-
-            verify(mockGameStateManager, times(1)).update(GameState.GAME_OVER);
-        }
+        assertThatThrownBy(() -> gameOverScene.run())
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
