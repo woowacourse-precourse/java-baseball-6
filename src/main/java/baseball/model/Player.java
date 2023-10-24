7cf89ab9858f86c5c0e3f63af87f36cf607cc909
chjcoder <chjcoder@gmail.com>
2023-10-24 17:20:33 +0900

    [feat] player로부터 입력값을 받는 기능

---
+++ src/main/java/baseball/model/Player.java
@@ -0,0 +1,24 @@
+package baseball.model;
+
+import baseball.utils.Util;
+import baseball.validation.Validation;
+
+import java.util.List;
+
+public class Player {
+    static final int NUMBER_OF_BALLS = 3;
+    static final int MINIMUM_PLAYER_NUMBER = 1;
+    static final int MAXIMUM_PLAYER_NUMBER = 9;
+    List<Integer> playerIntegerList;    // private로 설정?
+    public Player(String playerNumStr) {
+        Validation.validateInteger(playerNumStr);
+        playerIntegerList = Util.stringToIntegerList(playerNumStr);
+        Validation.validateLength(playerIntegerList, NUMBER_OF_BALLS);
+        Validation.validateRange(playerIntegerList, MINIMUM_PLAYER_NUMBER, MAXIMUM_PLAYER_NUMBER);
+        Validation.validateUnique(playerIntegerList);
+    }
+
+    public List<Integer> getPlayerIntegerList() {
+        return this.playerIntegerList;
+    }
+}
