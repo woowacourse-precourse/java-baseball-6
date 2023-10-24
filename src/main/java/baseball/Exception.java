a3cdd9f4ca3a5dbdf03c190091ef9776a18161e7
eunjin3481 <eunjin3481@gmail.com>
2023-10-24 23:49:29 +0900

    refactor: 예외 종료 메시지 변경

--- src/main/java/baseball/Exception.java
+++ src/main/java/baseball/Exception.java
@@ -7,7 +7,7 @@ public class Exception {
 
     public static void validateOneOrTwo(int userInput){
         if(userInput != 1 && userInput != 2){
-            throw new IllegalArgumentException("프로그램 종료");
+            throw new IllegalArgumentException("1 또는 2 이외의 숫자는 입력할 수 없습니다. 프로그램 종료");
         }
     }
 
@@ -20,14 +20,14 @@ public class Exception {
     public static void validateDigits(char[] userInput){
         for (char c : userInput) {
             if (!Character.isDigit(c)) {
-                throw new IllegalArgumentException("프로그램 종료");
+                throw new IllegalArgumentException("숫자 이외의 문자는 입력할 수 없습니다. 프로그램 종료");
             }
         }
     }
 
     public static void  validateLength(char[] userInput){
         if(userInput.length != 3){
-            throw new IllegalArgumentException("프로그램 종료");
+            throw new IllegalArgumentException("세 자리 숫자를 입력해야 합니다. 프로그램 종료");
         }
     }
 
@@ -38,7 +38,7 @@ public class Exception {
         }
 
         if(inputSet.size() != userInput.length){
-            throw new IllegalArgumentException("프로그램 종료");
+            throw new IllegalArgumentException("중복된 숫자는 입력할 수 없습니다. 프로그램 종료");
         }
     }
 }
