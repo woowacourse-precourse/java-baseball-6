b764d6cca54972dcde6685db23a5fc30b181157a
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-10-24 15:21:47 +0900

    2023.10.24_FEAT : CONTROLLER CLASS - create gameStart method

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -1,10 +1,24 @@
 package baseball;
 
 import static baseball.Utils.*;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Controller {
 
     public Controller() {}
 
+    public void gameStart(){
+        String state = START_GAME;
+        Oppoent oppoent = new Oppoent();
+        Game game = new Game(oppoent);
+        while (state.equals(START_GAME)) {
+            game.initialiser();
+            game.gamePlay();
+            printRequestRestartOrFinish();
+            state = readLine();
+            validateRange(state);
+        }
+    }
 
     private void printRequestRestartOrFinish() {
         System.out.println(RESTART_OR_FINISH);
