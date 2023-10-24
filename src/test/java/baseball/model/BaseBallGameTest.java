cdfcb1a75f8a3ca0f63953a8151f5c5d5db2bc31
soochan <tncksdl05@gmail.com>
2023-10-24 11:21:25 +0900

    style: test 메서드 명을 도메인 정책에 맞게 변경
    1. test method명을 도메인 정책에 맞게 추상적으로 변경
    2. 프러덕션 코드의 실제 메서드명을 사용하지 않도록 변경

--- src/test/java/baseball/model/BaseBallGameTest.java
+++ src/test/java/baseball/model/BaseBallGameTest.java
@@ -8,15 +8,16 @@ import org.junit.jupiter.api.Test;
 class BaseBallGameTest {
 
     @Test
-    void init메서드로_게임이_생성되는_경우는_게임초기상태가_결정된다() {
-        BaseBallNumbers computerNumbers = BaseBallNumbers.generateNumbers(List.of(1, 2, 3));
+    void 처음으로_생성된_게임은_내부적으로_게임이_끝나지_않는_상태를_가진다() {
+        List<Integer> numbers = List.of(1, 2, 3);
+        BaseBallNumbers computerNumbers = BaseBallNumbers.generateNumbers(numbers);
         BaseBallGame baseBallGame = BaseBallGame.init(computerNumbers);
 
         assertThat(baseBallGame.isNotEnd()).isTrue();
     }
 
     @Test
-    void calculateResult는_게임결과를_계산한다() {
+    void 사용자_야구숫자와_컴퓨터_숫자야구와_비교해서_게임결과를_계산한다() {
         BaseBallNumbers computerNumbers = BaseBallNumbers.generateNumbers(List.of(1, 2, 3));
         BaseBallGame baseBallGame = BaseBallGame.init(computerNumbers);
         BaseBallNumbers playerNumbers = BaseBallNumbers.generateNumbers(List.of(1, 2, 3));
@@ -28,15 +29,7 @@ class BaseBallGameTest {
     }
 
     @Test
-    void isNotEnd는_게임이_끝나지_않은_상태면_true를_응답한다() {
-        BaseBallNumbers computerNumbers = BaseBallNumbers.generateNumbers(List.of(1, 2, 3));
-        BaseBallGame baseBallGame = BaseBallGame.init(computerNumbers);
-
-        assertThat(baseBallGame.isNotEnd()).isTrue();
-    }
-
-    @Test
-    void isNotEnd는_게임이_끝난_상태면_false를_응답한다() {
+    void 사용자_야구숫자가_컴퓨터_야구숫자와_동일하다면_게임이_종료된다() {
         BaseBallNumbers computerNumbers = BaseBallNumbers.generateNumbers(List.of(1, 2, 3));
         BaseBallGame baseBallGame = BaseBallGame.init(computerNumbers);
         BaseBallNumbers playerNumbers = BaseBallNumbers.generateNumbers(List.of(1, 2, 3));
@@ -44,6 +37,17 @@ class BaseBallGameTest {
         baseBallGame.calculateResult(playerNumbers);
 
         assertThat(baseBallGame.isNotEnd()).isFalse();
+    }
+
+    @Test
+    void 사용자_야구숫자가_컴퓨터_야구숫자와_일치하지_않는다면_게임은_종료되지_않는다() {
+        BaseBallNumbers computerNumbers = BaseBallNumbers.generateNumbers(List.of(1, 2, 3));
+        BaseBallGame baseBallGame = BaseBallGame.init(computerNumbers);
+        BaseBallNumbers playerNumbers = BaseBallNumbers.generateNumbers(List.of(4, 5, 6));
+
+        baseBallGame.calculateResult(playerNumbers);
+
+        assertThat(baseBallGame.isNotEnd()).isTrue();
     }
 
 }
