20743bb91289d2c20817cbb7a5f37a001fec8849
wschoe <chldntjr0425@naver.com>
2023-10-25 03:48:15 +0900

    feat: add restart, exit, and start feature in GameSystem

--- src/main/java/baseball/gamesystem/GameSystem.java
+++ src/main/java/baseball/gamesystem/GameSystem.java
@@ -16,7 +16,25 @@ public class GameSystem {
 
     public static void start(){
         System.out.println("숫자 야구 게임을 시작합니다.");
-        doGame(Computer.getNumbers());
+        boolean isContinue;
+        do{
+            List<Integer> computerNumbers = Computer.getNumbers();
+            RESULT result = doGame(computerNumbers);
+            while(result == RESULT.LOSE){
+                result = doGame(computerNumbers);
+            }
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String userChoose = Console.readLine();
+
+            if(userChoose.equals("1")){
+                isContinue = true;
+            } else if(userChoose.equals("2")){
+                isContinue = false;
+            } else {
+                throw new IllegalArgumentException("1 또는 2만 입력 가능합니다.");
+            }
+        }while(isContinue);
     }
 
     private static RESULT doGame(List<Integer> computerNumbers){
