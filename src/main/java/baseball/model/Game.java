81bbee40da978498baead327ee1d7a2c6196c44d
leeseobin00 <leeseobin000709@gmail.com>
2023-10-25 06:11:49 +0900

    feat(Game): Game 모델 초기화 메서드 추가

--- src/main/java/baseball/model/Game.java
+++ src/main/java/baseball/model/Game.java
@@ -1,4 +1,25 @@
 package baseball.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
+    private List<Integer> computerNumbers;
+
+    public Game() {
+        computerNumbers = generateRandomNumber();
+    }
+
+    private List<Integer> generateRandomNumber() {
+        List<Integer> computerNumbers = new ArrayList<>();
+        while (computerNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumbers.contains(randomNumber)) {
+                computerNumbers.add(randomNumber);
+            }
+        }
+        return computerNumbers;
+    }
 }
