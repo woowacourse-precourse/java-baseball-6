44d91c066a1b99c975463cff4d748368e011bce1
inpink <inpink@kakao.com>
2023-10-25 00:54:17 +0900

    refactor: Nothing 객체 생성 방식 변경에 따른 StrikeHintServiceImpl 변경
    이또한 Ball, BallHintServiceImpl의 변경과 매우 유사함.

--- src/main/java/baseball/domain/hint/Nothing.java
+++ src/main/java/baseball/domain/hint/Nothing.java
@@ -2,5 +2,14 @@ package baseball.domain.hint;
 
 public class Nothing extends HintItem {
 
+    public Nothing(int count) {
+        super(count);
+    }
 
+    @Override
+    public boolean active() {
+        if (getCount() > 0) {
+            return false;
+        } return true;
+    }
 }
