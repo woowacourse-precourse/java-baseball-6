370a651d7c825091b4361d3f1cf3e76285fe8a5d
GS_song <20003204@sju.ac.kr>
2023-10-24 15:22:06 +0900

    refactor: 확장을 고려한 출력문구 변경

--- src/main/java/baseball/constant/Constants.java
+++ src/main/java/baseball/constant/Constants.java
@@ -9,10 +9,11 @@ public final class Constants {
     public final static String INPUT_PREDICTED_MESSAGE = "숫자를 입력해주세요 : ";
     public final static int INPUT_RESTART = 1;
     public final static int INPUT_STOP = 2;
-    public final static String INPUT_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public final static String INPUT_RESTART_MESSAGE =
+            "게임을 새로 시작하려면 " + INPUT_RESTART + ", 종료하려면 " + INPUT_STOP + "를 입력하세요.";
 
 
     //output
     public final static String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
-    public final static String GAME_STOP_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public final static String GAME_STOP_MESSAGE = INPUT_MAX_SIZE + "개의 숫자를 모두 맞히셨습니다! 게임 종료";
 }
