361f89ecc3da281b447739bf5d1f839fa0ceb373
junderer <junholim98@gmail.com>
2023-10-24 10:26:12 +0900

    feat: validate user Input

---
+++ src/main/java/baseball/model/Constant.java
@@ -0,0 +1,14 @@
+package baseball.model;
+
+public class Constant {
+    public static final int MIN_NUM_RANGE = 1;
+    public static final int MAX_NUM_RANGE = 9;
+    public static final int BASEBALL_NUM_LENGTH = 3;
+
+    public static final String NUM_INVALID_LENGTH = "3자리의 수만 입력 가능합니다.";
+    public static final String NUM_OUT_OF_RANGE = "1~9 사이의 숫자만 입력해 주세요.";
+    public static final String INPUT_NOT_NUM = "숫자만 입력해 주세요";
+    public static final String EXISTS_SAME_NUM = "서로 다른 수를 입력해 주세요";
+
+
+}
