6f5e7498b76b5108305b2f2a30f56d91bfac8623
Park Eunhyeong <silbroeh@gmail.com>
2023-10-24 14:01:45 +0900

    feat : 자료구조 정의 && 대상값 자릿수 설정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import java.util.List;
+
+
 public class Application {
+    static List<Integer> computer;  // 컴퓨터 랜덤값
+    static List<Integer> user;      // 사용자 입력값
+    static int strike;              // strike 수
+    static int ball;                // ball 수
+    static int digitNumber;         // 대상값 자릿수
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        digitNumber = 3;  // 대상값 자릿수 설정(3자리)
     }
 }
