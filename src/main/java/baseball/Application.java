0168265e1fa06d0b399d6bd5d99fc7ef76e976a9
YOOSUNAH <tjsdkyd4027@gmail.com>
2023-10-24 21:46:04 +0900

    게임 재시작 수정 중

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,62 +11,47 @@ import static baseball.CommandLine.showln;
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        baseballGame();
-    }
 
-    private static void baseballGame() {
-        List<Integer> computer = settingGame();
-        playGame(computer);
-
-    }
-
-    private static List<Integer> settingGame() {
         showln("숫자 야구 게임을 시작합니다.");
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
-        }
-        showln("right answer for test : " + String.join("", computer.stream().map(String::valueOf).toList())); // 나중에 지워야 함.
-        return computer;
-    }
-
-    private static void playGame(List<Integer> computer) {
         while (true) {
+            List<Integer> computer = settingGame();
+
             try {
-                String input = input("숫자를 입력해주세요: ");
-                String[] arr = input.split("");
-
-                int count = 0;
-                int countS = 0;
-                int countB = 0;
-                for (int i = 0; i < arr.length; i++) {
-                    for (int j = 0; j < computer.size(); j++) {
-                        if (arr[i].contains(String.valueOf(computer.get(j)))) {
-                            count++;
-                            if (i == j) {
-                                countS++;
-                                break;
-                            } else {
-                                countB++;
-                                break;
-                            }
-                        }
-                    }
-                }
-
-                if (gameResult(count, countB, countS))
+                if (playGame(computer))
                     return;
-            } catch (IllegalArgumentException e) {
+            }
+            catch (IllegalArgumentException e) {
                 return;
             }
         }
     }
 
-    private static boolean gameResult(int count, int countB, int countS) {
+    private static boolean playGame(List<Integer> computer) {
+        String input = input("숫자를 입력해주세요: ");
+        String[] arr = input.split("");
+
+        int count = 0;
+        int countS = 0;
+        int countB = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < computer.size(); j++) {
+                if (arr[i].contains(String.valueOf(computer.get(j)))) {
+                    count++;
+                    if (i == j) {
+                        countS++;
+                        break;
+                    } else {
+                        countB++;
+                        break;
+                    }
+                }
+            }
+        }
+        if (result(count, countB, countS)) return true;
+        return false;
+    }
+
+    private static boolean result(int count, int countB, int countS) {
         if (count == 0) {
             showln("낫싱");
         }
@@ -81,18 +66,32 @@ public class Application {
                 showln(countB + "볼 " + countS + "스트라이크");
             }
             if (countS == 3) {
-                showln("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-
-                showln("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                showln("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+                        "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 // 게임 재시작
                 String in = input();
-                if (in.equals("1"))
-                    baseballGame();
-                if (in.equals("2"))
+                if (in.equals("1")) {
                     return false;
+                }
+                if (in.equals("2")) {
+                    return true;
+                }
             }
         }
         return false;
+    }
+
+    private static List<Integer> settingGame() {
+        // TODO: 프로그램 구현
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        showln("right answer for test : " + String.join("", computer.stream().map(String::valueOf).toList())); // 나중에 지워야 함.
+        return computer;
     }
 }
 
