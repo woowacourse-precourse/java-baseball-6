56fa34cc56b9e9ce8221e2d762f95a2726411e48
Potato-Y <projectpotatoy@gmail.com>
2023-10-24 14:17:24 +0900

    style : 철자 오류 수정

--- src/main/java/baseball/util/Baseball.java
+++ src/main/java/baseball/util/Baseball.java
@@ -14,7 +14,7 @@ public class Baseball {
      * 유저가 제시한 숫자
      */
     private final List<Integer> user;
-    private int bool = 0;
+    private int ball = 0;
     private int strike = 0;
 
     /**
@@ -39,31 +39,31 @@ public class Baseball {
             if (search == i) {
                 strike++;
             } else if (search != -1) {
-                bool++;
+                ball++;
             }
         }
 
-        commentPrint(bool, strike);
+        commentPrint(ball, strike);
         return strike == 3;
     }
 
     /**
      * 게임 결과를 출력한다.
      *
-     * @param bool
+     * @param ball
      * @param strike
      */
-    private static void commentPrint(int bool, int strike) {
-        if (bool == 0 && strike == 0) {
+    private static void commentPrint(int ball, int strike) {
+        if (ball == 0 && strike == 0) {
             System.out.print("낫싱");
         }
-        if (bool > 0) {
-            System.out.print(bool + "볼");
+        if (ball > 0) {
+            System.out.print(ball + "볼");
         }
-        if (bool > 0 && strike > 0) {
+        if (ball > 0 && strike > 0) {
             System.out.print(" " + strike + "스트라이크");
         }
-        if (bool == 0 && strike > 0) {
+        if (ball == 0 && strike > 0) {
             System.out.print(strike + "스트라이크");
         }
 
