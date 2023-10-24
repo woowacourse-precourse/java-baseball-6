58458227b966f979d801441c416758b66af363a2
cyPark95 <parkcy1007@gmail.com>
2023-10-24 15:29:05 +0900

    fix: Console 을 통한 사용자 입력 받기
    - 요구사항에 따라 BufferedReader 에서 camp.nextstep.edu.missionutils.Console 로 변경

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,26 +1,14 @@
 package baseball.view;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-
-    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     private InputView() {
     }
 
     public static String getNumbers() {
         System.out.print("숫자를 입력해주세요 : ");
-        return readLine();
-    }
-
-    private static String readLine() {
-        try {
-            return reader.readLine();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        return Console.readLine();
     }
 }
