62d63b2f7b47b5d7764b496492907be4d5521726
sgo722 <sgo722@naver.com>
2023-10-25 00:44:43 +0900

    feat : 사용자 3자리 수 중복 검증기능 추가

--- src/main/java/baseball/utils/Validation.java
+++ src/main/java/baseball/utils/Validation.java
@@ -9,6 +9,7 @@ public class Validation {
     public static void verifyUserNumber(String userNumber){
         verifyUserNumberLength(userNumber);
         verifyUserNumberRange(userNumber);
+        verifyUserNumberDuplication(userNumber);
     }
 
     private static void verifyUserNumberLength(String userNumber) {
@@ -22,6 +23,16 @@ public class Validation {
             if(eachNumber >= 1 && eachNumber <= 9){
                 continue;
             }
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void verifyUserNumberDuplication(String userNumber){
+        Set<Character> count = new HashSet<>();
+        for(int i=0; i<3; i++){
+            count.add(userNumber.charAt(i));
+        }
+        if(count.size() != 3){
             throw new IllegalArgumentException();
         }
     }
