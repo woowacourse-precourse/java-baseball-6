40a021f42e5dd886666b7abf7aa19e6011ff04b2
seunghye218 <seunghye@student.42seoul.kr>
2023-10-24 17:55:18 +0900

    refactor: separate game logic - add NumberBaseballGame class

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,41 +1,10 @@
 package baseball;
 
-
 public class Application {
 
     public static void main(String[] args) {
-        Computer computer = new Computer();
-        String result = "";
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        while (true) {
-            System.out.print("숫자를 입력해주세요 : ");
-            String userInput = UserInput.getUserNumber();
-            int[] score = computer.getResult(userInput);
-            if (score[1] != 0) {
-                result += score[1] + "볼";
-            }
-            if (score[0] != 0) {
-                if (!result.isEmpty()) {
-                    result += ' ';
-                }
-                result += score[0] + "스트라이크";
-            }
-            if (result.isEmpty()) {
-                result += "낫싱";
-            }
-            System.out.println(result);
-            if (score[0] == 3) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                boolean restart = UserInput.getUserRestart();
-                if (restart) {
-                    computer.resetNumber();
-                } else {
-                    System.out.println("게임 종료");
-                    break;
-                }
-            }
-            result = "";
-        }
+        NumberBaseballGame game = new NumberBaseballGame();
+        game.gameLoop();
     }
 }
 
---
+++ src/main/java/baseball/NumberBaseballGame.java
@@ -0,0 +1,55 @@
+package baseball;
+
+public class NumberBaseballGame {
+
+    private final Computer computer;
+
+    public NumberBaseballGame() {
+        this.computer = new Computer();
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    private void printGameResult(int[] score) {
+        StringBuilder result = new StringBuilder();
+        if (score[1] != 0) {
+            result.append(score[1]);
+            result.append('볼');
+        }
+        if (score[0] != 0) {
+            if (!result.isEmpty()) {
+                result.append(' ');
+            }
+            result.append(score[0]);
+            result.append("스트라이크");
+        }
+        if (result.isEmpty()) {
+            result.append("낫싱");
+        }
+        System.out.println(result);
+    }
+
+    private boolean gameOver(int[] score) {
+        if (score[0] == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            if (UserInput.getUserRestart()) {
+                computer.resetNumber();
+                return false;
+            } else {
+                System.out.println("게임 종료");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void gameLoop() {
+        while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
+            int[] score = computer.getResult(UserInput.getUserNumber());
+            printGameResult(score);
+            if (gameOver(score)) {
+                break;
+            }
+        }
+    }
+}
