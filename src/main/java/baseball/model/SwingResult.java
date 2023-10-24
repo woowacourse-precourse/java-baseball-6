60acee78486e8abe9c042670d7d8b6178f7dcd90
chosunghyun18 <krismon@naver.com>
2023-10-24 23:29:55 +0900

    feat : add 결과에 맞는 메시지 문자열을 반환한다.

--- src/main/java/baseball/model/SwingResult.java
+++ src/main/java/baseball/model/SwingResult.java
@@ -3,22 +3,43 @@ package baseball.model;
 import baseball.model.ball.BallStatus;
 
 public class SwingResult {
-    int strikeCount = 0 ;
-    int ballCount = 0 ;
+    int strikeCount = 0;
+    int ballCount = 0;
+
     public void report(BallStatus ballStatus) {
-        if(ballStatus.isNothing()) return;
-        if(ballStatus.isStrike()) strikeCount++;
-        if(ballStatus.isBall()) ballCount++;
+        if (ballStatus.isNothing()) {
+            return;
+        }
+        if (ballStatus.isStrike()) {
+            strikeCount++;
+        }
+        if (ballStatus.isBall()) {
+            ballCount++;
+        }
     }
 
     public int getStrikeCount() {
         return strikeCount;
     }
+
     public int getBallCount() {
         return ballCount;
     }
 
     public boolean isAllStrike() {
         return strikeCount == 3;
+    }
+
+    public String getResultMessage() {
+        if (strikeCount == 0 && ballCount == 0) {
+            return "낫싱";
+        }
+        if (strikeCount == 0) {
+            return ballCount + "볼";
+        }
+        if (ballCount == 0) {
+            return strikeCount + "스트라이크";
+        }
+        return ballCount + "볼" + " " + strikeCount + "스트라이크";
     }
 }
