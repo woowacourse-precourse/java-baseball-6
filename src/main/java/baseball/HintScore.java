d26cc6e367210939fdede448b3610f4425344e73
rlfrkdms1 <rlfrkdms1@naver.com>
2023-10-24 23:00:19 +0900

    feat: ball 출력 문구 생성 기능 추가
    %d볼 형식 상수 선언
    힌트 점수 출력 문구 생성

--- src/main/java/baseball/HintScore.java
+++ src/main/java/baseball/HintScore.java
@@ -1,9 +1,13 @@
 package baseball;
 
+import java.util.StringJoiner;
+
 public class HintScore {
 
     private int strike;
     private int ball;
+
+    private static final String PRINT_BALL_FORMAT = "%d볼";
 
     public void raiseStrike() {
         strike++;
@@ -11,5 +15,14 @@ public class HintScore {
 
     public void raiseBall() {
         ball++;
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner stringJoiner = new StringJoiner(" ");
+        if (ball != 0) {
+            stringJoiner.add(String.format(PRINT_BALL_FORMAT, ball));
+        }
+        return stringJoiner.toString();
     }
 }
