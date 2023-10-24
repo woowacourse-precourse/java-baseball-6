ccd341fc7e7c622f1ec9c5c24096b68c11d7c4a8
suhyun__ <thebbbb1206@gmail.com>
2023-10-24 18:41:11 +0900

    style: 메소드명 변경

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
-    public List<Integer> generateAnswer() {
+    public List<Integer> generateComputerNumber() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
