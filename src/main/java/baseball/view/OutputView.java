e4d95f4fd2f09a0251801db090d770109b4a1d6d
K-KY <kyuyoungk@naver.com>
2023-10-25 02:57:52 +0900

    Style(OutputView) : 공백 제거

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,22 +4,22 @@ import baseball.domain.BallScore;
 import baseball.domain.GameResult;
 
 public class OutputView {
-    public static final String START_MESSAGE =  "숫자 야구 게임을 시작합니다. ";
+    public static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
 
-    public static final String INPUT_NUMBER_MESSAGE =  "숫자를 입력해주세요 : ";
+    public static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
 
     public static final String BALL = "볼 ";
 
-    public static final String STRIKE = "스트라이크 ";
+    public static final String STRIKE = "스트라이크";
 
-    public static final  String NOTHING = "낫싱";
+    public static final String NOTHING = "낫싱";
 
     public static final String GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     public static final String END_OR_RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     public static final String END = "게임종료";
 
-    public static final String NEW_LINE ="\n";
+    public static final String NEW_LINE = "\n";
 
     public static void printStartMessage() {
         System.out.println(START_MESSAGE);
@@ -49,6 +49,7 @@ public class OutputView {
         System.out.println(GAME_END);
         System.out.println(END_OR_RESTART);
     }
+
     public static void printEnd() {
         System.out.println(END);
     }
