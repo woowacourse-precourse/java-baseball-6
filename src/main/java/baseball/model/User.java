c827e062c36b3fb7600fe0a0cfc697eb3fa3f2d0
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2023-10-24 15:58:20 +0900

    feat: User 클래스 생성

---
+++ src/main/java/baseball/model/User.java
@@ -0,0 +1,12 @@
+package baseball.model;
+
+import java.util.List;
+
+public class User {
+
+    private List<Integer> numbers;
+
+    private User(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+}
