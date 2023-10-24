6e10b1611d92c041b2f5f0adf212264081ff95fe
ynco32 <ynco32@sogang.ac.kr>
2023-10-24 22:13:18 +0900

    게임 순서 구현

---
+++ src/main/java/baseball/Baseball.java
@@ -0,0 +1,83 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Baseball {
+    int score;
+
+    public void start() {
+        /* --- 컴퓨터 정답 생성 --- */
+        List<Integer> computer = getComputerNum();
+
+        /* --- 유저 입력값 받기 --- */
+        while (score < 4){
+            /* --- 유저 입력값 확인 --- */
+            List<Integer> user = getUserNum();
+
+            /* --- 야구 게임 진행 --- */
+            countScore(computer, user, score);
+
+        }
+
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
+
+
+    }
+
+
+    /* --- 컴퓨터 정답 생성 --- */
+    public static List<Integer> getComputerNum(){
+        List<Integer> newRandom = new ArrayList<>();
+        while (newRandom.size() < 3){
+            // 1~9 사이 숫자만 (0 불가)
+            int num = Randoms.pickNumberInRange(1,9);
+            // 중복 숫자 들어가지 않게끔
+            if (!newRandom.contains(num)){
+                newRandom.add(num);
+            }
+        }
+        return newRandom;
+    }
+
+    /* --- 유저 입력값 받기 --- */
+    public List<Integer> getUserNum(){
+        System.out.println("숫자를 입력해주세요 : ");
+        String userNumInput = readLine();
+        isUserNum(userNumInput);
+    }
+
+
+    /* --- 유저 입력값 확인 --- */
+    public static void isUserNum(String input){
+        // 세 자리 수인지
+        if (input.length() != 3){
+            throw new IllegalArgumentException("유효하지 않은 값을 입력하셨습니다. 프로그램을 종료합니다.");
+        }
+
+        // 서로 다른 세 숫자인지
+        char first = input.charAt(0);
+        char second = input.charAt(1);
+        char third = input.charAt(2);
+
+        if (first == second || second == third || first == third){
+            throw new IllegalArgumentException("유효하지 않은 값을 입력하셨습니다. 프로그램을 종료합니다.");
+        }
+
+
+        // 0이 들어갔는지
+        for (char c : input.toCharArray()){
+            if (!Character.isDigit(c) || c == '0'){
+                throw new IllegalArgumentException("유효하지 않은 값을 입력하셨습니다. 프로그램을 종료합니다.");
+            }
+        }
+
+    }
+
+
+}
