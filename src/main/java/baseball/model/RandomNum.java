6ecd33a124f5eb513ab1baa94c21c50982e177be
gahyeon <1000ji1@naver.com>
2023-10-25 02:52:54 +0900

    feat : create makeRandomNum function

---
+++ src/main/java/baseball/model/RandomNum.java
@@ -0,0 +1,20 @@
+package baseball.model;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomNum {
+    public static List<Integer> makeRandomNum() {
+        List<Integer> computerNumbers = new ArrayList<>();
+
+        while (computerNumbers.size() < 3) {
+            int randomNumber = pickNumberInRange(1, 9); // 1~9 랜덤 수 출력
+            if (!computerNumbers.contains(randomNumber)) { // 서로 다른 수
+                computerNumbers.add(randomNumber);
+            }
+        }
+        return computerNumbers;
+    }
+}
