2b3d2937f735a632a21847dd420db25e65b3c1db
SubinHeo0 <anmedi3623@naver.com>
2023-10-24 19:55:19 +0900

    주석 추가 및 예외 처리 문구 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class Application {
 
-    //컴퓨터 랜덤값 생성 함수(매개변수x, 리턴값 ArrayList<Integer>)
+    //컴퓨터 랜덤값 생성
     public static List<Integer> makeRandom() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
@@ -20,7 +20,7 @@ public class Application {
         return computer;
     }
 
-    //사용자 입력값 받는 함수(매개변수x, 리턴값 List<Integer>)
+    //사용자 값 입력
     public static List<Integer> inputUser() throws IllegalArgumentException {
         System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
@@ -28,12 +28,12 @@ public class Application {
         //예외처리
         //1. 입력값이 3글자가 아닐 때
         if (input.length() != 3) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("잘못된 값을 입력했습니다.");
         }
-        //2. 숫자가 아닌 경우(아스키코드로 0~9가 아닌 것)
+        //2. 숫자가 아닌 경우
         for (int i = 0; i < input.length(); i++) {
             if ('0' > input.charAt(i) && input.charAt(i) > '9') {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("잘못된 값을 입력했습니다.");
             }
         }
         //3. 중복된 수가 입력됐을 떄
@@ -41,29 +41,27 @@ public class Application {
         List<Integer> inputNum = new ArrayList<>();
         for (int i = 0; i < arr.length; i++) {
             if (inputNum.contains(Integer.parseInt(arr[i]))) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("잘못된 값을 입력했습니다.");
             }
             inputNum.add(Integer.parseInt(arr[i]));
         }
         return inputNum;
     }
 
-    // 입력값과 컴퓨터값 비교
-    // (매개변수(ArrayList<Integer>inputNum, ArrayList<Integer>computer))
-    // 리턴값 void
+    // 컴퓨터값과 입력값 비교
     public static void play() {
         //컴퓨터 랜덤값
         List<Integer> computer = makeRandom();
-        System.out.println("computer = " + computer);
+//        System.out.println("computer = " + computer);
 
         while (true) {
-            // 3 스트라이크가 되면 종료
             int strike = 0;
             int ball = 0;
 
             //사용자 입력값
             List<Integer> inputNum = inputUser();
 
+            // 값 비교
             for (int i = 0; i < inputNum.size(); i++) {
                 for (int j = 0; j < computer.size(); j++) {
                     if (inputNum.get(i) == computer.get(j) && i == j) {
@@ -74,7 +72,7 @@ public class Application {
                     }
 
                 }
-            } // 값 비교 완료
+            }
 
             if (ball > 0) {
                 System.out.print(ball + "볼 ");
@@ -93,28 +91,26 @@ public class Application {
 
             System.out.println();
 
-        } //while
+        } //게임 종료
 
+        // 게임 재시작 묻기
         int yesOrNo = replay();
-        if (yesOrNo == 1) { //재시작
-            play();
-        }
-        if (yesOrNo == 2) {//게임종료
-            //코드X
-            //if를 1과 2가 아닐때 예외처리날리면 될듯
-        }
+        // 재시작
+        if (yesOrNo == 1) { play();}
+        // 게임 종료
+        if (yesOrNo == 2) {}
 
     }
 
-    //게임 재시작(1), 종료(2) 입력 받는 함수
-    public static int replay() {
+    //게임 재시작(1), 종료(2) 입력
+    public static int replay() throws IllegalArgumentException {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         int user = Integer.parseInt(Console.readLine());
 
         //예외처리
-        //입력값이 1,2가 아니면 다 예외
+        //입력값이 1또는 2가 아닐 때
         if (user != 1 && user != 2) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("잘못된 값을 입력했습니다.");
         }
 
         return user;
@@ -124,14 +120,7 @@ public class Application {
         // TODO: 프로그램 구현
 
         System.out.println("숫자 야구 게임을 시작합니다.");
-        //예외발생 테스트
         play();
-
-//        try{
-//            play();
-//        }catch (IllegalArgumentException e){
-//            System.out.println("IllegalArgumentException 예외 발생");
-//        }
 
     }
 }
