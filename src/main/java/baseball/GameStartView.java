7890c1424e91f8ff7aa04f4f1eff2ea3bffd1eab
BDlhj <akk808@naver.com>
2023-10-24 21:10:44 +0900

    rename: Fix 'gameStartMessage' to 'GAME_START_MESSAGE'

--- src/main/java/baseball/GameStartView.java
+++ src/main/java/baseball/GameStartView.java
@@ -1,13 +1,13 @@
 package baseball;
 
 public class GameStartView {
-    private static final String GameStartMessage = "숫자 야구 게임을 시작합니다.";
+    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
 
     public void run() {
         this.printStartGameMessage();
     }
 
     public void printStartGameMessage() {
-        System.out.println(GameStartMessage);
+        System.out.println(GAME_START_MESSAGE);
     }
 }
