0782454f355b3aa5355ecdd4717ebda367445c4e
psrom <psrom721@gmail.com>
2023-10-25 00:27:57 +0900

    feat: Get user input
    세 자리 숫자가 아닌 경우 예외 처리 한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,6 +11,11 @@ public class Application {
             List<Integer> computer = generateRandomNumber();
             System.out.println("숫자 야구 게임을 시작합니다.");
 
+            boolean correctGuess = false;
+
+            while (!correctGuess) {
+                List<Integer> userInput = getUserInput();
+            }
         }
     }
 
@@ -27,6 +32,40 @@ public class Application {
         }
 
         return computer;
+    }
+
+
+    private static List<Integer> getUserInput() {
+        List<Integer> userInput = new ArrayList<>();
+
+        System.out.print("숫자를 입력해주세요 : ");
+        String inputString = Console.readLine();
+
+        try {
+            int number = Integer.parseInt(inputString);
+
+            if (number < 100 || number > 999) {
+                throw new IllegalArgumentException();
+            }
+
+            int digit1 = number % 10;
+            int digit2 = (number / 10) % 10;
+            int digit3 = number / 100;
+
+            if ((digit1 == digit2) || (digit1 == digit3) || (digit2 == digit3)
+                    || (digit1 == 0) || (digit2 == 0) || (digit3 == 0)) {
+                throw new IllegalArgumentException();
+            }
+
+            userInput.add(digit3);
+            userInput.add(digit2);
+            userInput.add(digit1);
+
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+
+        return userInput;
     }
 
 }
