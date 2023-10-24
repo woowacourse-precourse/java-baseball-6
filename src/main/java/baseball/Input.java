c8b3377a9b48440b8c01a47dbc063f18c2fe6a2e
Yang woosung <ngxk1217@gmail.com>
2023-10-25 03:22:33 +0900

    경기 재시작 여부 확인

---
+++ src/main/java/baseball/Input.java
@@ -0,0 +1,19 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Input {
+    public List<Integer> playerNumber(){
+        System.out.println("숫자를 입력해주세요");
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> playerNum = new ArrayList<>();
+        String input = scanner.next();
+
+        for(String number: input.split("")){
+            playerNum.add(Integer.parseInt(number));
+        }
+        return playerNum;
+    }
+}
