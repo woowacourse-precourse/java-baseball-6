6ed6af549b75bfa1876aa5ae0dc1336495f3bcca
JongMinCh0i <wonderland314592@gmail.com>
2023-10-24 22:49:34 +0900

    style(ComputerView): 메서드 명 변경(renderResult -> renderView)

--- src/main/java/baseball/view/ComputerView.java
+++ src/main/java/baseball/view/ComputerView.java
@@ -4,7 +4,8 @@ public class ComputerView implements View {
 
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
 
-    public void renderResult() {
+    @Override
+    public void renderView() {
         System.out.println(GAME_START_MESSAGE);
     }
 }
