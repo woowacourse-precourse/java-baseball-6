279db5e5641e716f2bfa4d63f8f09c92fa4f0ab2
sweun2 <46338536+sweun2@users.noreply.github.com>
2023-10-25 04:53:25 +0900

    feat : add validate feature

--- src/main/java/baseball/utils/HandleError.java
+++ src/main/java/baseball/utils/HandleError.java
@@ -1,4 +1,20 @@
 package baseball.utils;
 
 public class HandleError {
+    void validateInputString(String inputString) {
+        char[] characters = inputString.toCharArray();
+        Integer count = 0; //입력한 문자가 3자리 인지 확인하는 변수
+
+        for (char ch : characters) {
+            count+=1;
+            //입력한 문자가 숫자로 이루어져 있는지
+            if(!Character.isDigit(ch)) {
+                throw new IllegalArgumentException("input value is not number");
+            }
+        }
+        //입력한 문자가 3자리 인지
+        if(count!=3) {
+            throw new IllegalArgumentException("input number is not 3 characters long.");
+        }
+    }
 }
--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,7 +1,10 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -27,6 +30,29 @@ class ApplicationTest extends NsTest {
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
+    @Test
+    void 입력_테스트() {
+        String inputString = "123";
+        입력_받은_수의_에러_여부(inputString);
+    }
+    @Test
+    void 입력_받은_수의_에러_여부(String inputString) {
+        char[] characters = inputString.toCharArray();
+        Integer count = 0; //입력한 문자가 3자리 인지 확인하는 변수
+
+        for (char ch : characters) {
+            count+=1;
+            //입력한 문자가 숫자로 이루어져 있는지
+            if(!Character.isDigit(ch)) {
+                throw new IllegalArgumentException("input value is not number");
+            }
+        }
+        //입력한 문자가 3자리 인지
+        if(count!=3) {
+            throw new IllegalArgumentException("input number is not 3 characters long. ");
+        }
+    }
+
 
     @Override
     public void runMain() {
