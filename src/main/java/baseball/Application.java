7fd092a11412896d7a5b473a97f997242fbf5c73
kmk <kmk7347@naver.com>
2023-10-24 15:11:48 +0900

    [feat] 메인(Application) : 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,30 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    public static void main(String[] args) throws IllegalArgumentException{
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        ComputerNum computer = new ComputerNum();
+        UserInput userInput = new UserInput();
+        Compare compare = new Compare();
+        Restart retry = new Restart();
+
+        boolean again = true;
+
+        while(again) {
+            List<Integer> com_num = computer.ComputerNumber();
+
+            String result = "";
+
+            while(!result.equals("3스트라이크")) {
+                result = compare.Classification(com_num, userInput.player_num());
+                System.out.println(result);
+            }
+
+            again = retry.retry();
+        }
     }
 }
