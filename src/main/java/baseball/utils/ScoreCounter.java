a806945cb8626652679dae491b49d357ea8fc2bc
zaeuk <zzaeuk@gmail.com>
2023-10-24 13:36:59 +0900

    refactor: countStrike, countBall 메서드분리 isStrike 메서드와 isBall 메서드로 분리

--- src/main/java/baseball/utils/ScoreCounter.java
+++ src/main/java/baseball/utils/ScoreCounter.java
@@ -5,23 +5,43 @@ import java.util.List;
 public class ScoreCounter {
     public int countStrike(List<Integer> computer, List<Integer> user) {
         int count = 0;
+
         for (int i = 0; i < user.size(); i++) {
-            if (computer.get(i).equals(user.get(i))) {
+            if (isStrike(computer.get(i), user.get(i))) {
                 count++;
             }
         }
+
         return count;
+    }
+
+    private boolean isStrike(Integer computerDigit, Integer userDigit) {
+        return computerDigit.equals(userDigit);
     }
 
     public int countBall(List<Integer> computer, List<Integer> user) {
         int count = 0;
+
         for (int i = 0; i < computer.size(); i++) {
-            for (int j = 0; j < user.size(); j++) {
-                if (i != j && computer.get(i).equals(user.get(j))) {
-                    count++;
-                }
+            if (isBall(computer.get(i), user, i)) {
+                count++;
             }
         }
+
         return count;
     }
+
+    private boolean isBall(Integer computerDigit, List<Integer> user, Integer index) {
+        boolean isMatch=false;
+
+        for (int i = 0; i < user.size(); i++) {
+            if(i == index) continue;
+            if(user.get(i).equals(computerDigit)){
+                isMatch=true;
+            };
+        }
+
+        return isMatch;
+    }
+
 }
