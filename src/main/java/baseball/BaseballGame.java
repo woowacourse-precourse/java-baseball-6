336bac0c61649df113a6d7ddd786d72bdfb5bb29
진환 정 <joung.jinhawn@gmail.com>
2023-10-24 15:01:31 +0900

    코드 컨벤션 적용

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -23,7 +23,7 @@ public class BaseballGame {
     public void startGame() {
         this.initMessage();
 
-        while(this.isContinue && !this.isGameEnd) {
+        while (this.isContinue && !this.isGameEnd) {
             this.makeRamdomNumbers();
 
             while (!this.isGameEnd) {
@@ -44,7 +44,7 @@ public class BaseballGame {
         System.out.print("숫자를 입력해주세요 : ");
         String inputNumString = Console.readLine();
 
-        if(inputNumString.length() != 3){
+        if (inputNumString.length() != 3) {
             throw new IllegalArgumentException();
         }
 
@@ -56,7 +56,7 @@ public class BaseballGame {
     }
 
     private void printOutputMessage() {
-        if(this.ball != 0) {
+        if (this.ball != 0) {
             System.out.print(this.ball + "볼 ");
         }
         if (this.strike != 0) {
@@ -68,7 +68,7 @@ public class BaseballGame {
 
         System.out.println();
 
-        if(this.strike == 3) {
+        if (this.strike == 3) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             this.isGameEnd = true;
@@ -78,14 +78,14 @@ public class BaseballGame {
     private void continueOrNot() {
         String input = Console.readLine();
 
-        if(input.length() != 1){
+        if (input.length() != 1) {
             throw new IllegalArgumentException();
         }
 
         try {
             int restartOrEnd = Integer.parseInt(input);
 
-            if(restartOrEnd == 1) {
+            if (restartOrEnd == 1) {
                 this.isContinue = true;
                 this.isGameEnd = false;
             } else if (restartOrEnd == 2) {
@@ -114,11 +114,11 @@ public class BaseballGame {
     private int makeOneRamdomNumber(int[] arrNum, int arrIndex) {
         int randomNum = 0;
 
-        while(true) {
+        while (true) {
             randomNum = Randoms.pickNumberInRange(1, 9);
             boolean stop = checkRandomNumberSameOrNot(arrNum, randomNum);
 
-            if(stop){
+            if (stop) {
                 arrNum[arrIndex] = randomNum;
                 break;
             }
@@ -142,8 +142,8 @@ public class BaseballGame {
         int tempInputNum = this.inputNumber;
         int tempRandomNum = this.randomNumber;
 
-        for(int i = 0; i < 3; i++) {
-            if(tempInputNum % 10 == tempRandomNum % 10) {
+        for (int i = 0; i < 3; i++) {
+            if (tempInputNum % 10 == tempRandomNum % 10) {
                 this.strike++;
             }
 
@@ -156,21 +156,22 @@ public class BaseballGame {
         this.ball = 0;
         String tempInputNum = Integer.toString(this.inputNumber);
 
-        for(int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) {
             char currentDigit = tempInputNum.charAt(i);
 
-            if(checkOneBallNumber(currentDigit, i)) {
+            if (checkOneBallNumber(currentDigit, i)) {
                 this.ball++;
             }
         }
     }
+
     private boolean checkOneBallNumber(char inputOneNumber, int currentInputNumberIndex) {
         String tempRandomNum = Integer.toString(this.randomNumber);
 
-        for(int j = 0; j < 3; j++) {
-            if(currentInputNumberIndex == j){
+        for (int j = 0; j < 3; j++) {
+            if (currentInputNumberIndex == j) {
                 continue;
-            } else if(tempRandomNum.charAt(j) == inputOneNumber) {
+            } else if (tempRandomNum.charAt(j) == inputOneNumber) {
                 return true;
             }
         }
