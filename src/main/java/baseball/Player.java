ed20e11f06b8e508010179b0d3c4a3dbaaebfc6b
YoonseolLee <gujueng32@naver.com>
2023-10-24 12:39:54 +0900

    feat: 사용자 입력값의 유효성을 검증하는 기능

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,10 +1,14 @@
 package baseball;
 
 import static baseball.Computer.COUNTS;
+import static baseball.Computer.MAXIMUM_NUMBER;
+import static baseball.Computer.MINIMUM_NUMBER;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Player {
     private static final String SUCCESS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
@@ -29,6 +33,7 @@ public class Player {
             String playerInput = Console.readLine();
             validateNumber(playerInput);
             playerNumbers = convertToIntegerList(playerInput);
+            validatePlayerNumbers(playerNumbers);
         }
     }
 
@@ -39,6 +44,37 @@ public class Player {
             throw new IllegalArgumentException("Input is not an Integer.");
         }
     }
+
+    private void validatePlayerNumbers(List<Integer> playerNumbers) {
+        validateCount(playerNumbers);
+        validateRange(playerNumbers);
+        validateDuplicates(playerNumbers);
+    }
+
+    private void validateCount(List<Integer> playerNumbers) {
+        if (playerNumbers.size() != COUNTS) {
+            throw new IllegalArgumentException("");
+        }
+    }
+
+    private void validateRange(List<Integer> playerNumbers) {
+        boolean isInvalid = playerNumbers.stream()
+                .anyMatch(number -> number < MINIMUM_NUMBER || number > MAXIMUM_NUMBER);
+        if (isInvalid) {
+            throw new IllegalArgumentException("the input range is invalid.");
+        }
+    }
+
+    private void validateDuplicates(List<Integer> playerNumbers) {
+        Set<Integer> numberSet = new HashSet<>();
+        for (int number : playerNumbers) {
+            if (numberSet.contains(number)) {
+                throw new IllegalArgumentException("중복된 숫자가 입력되었습니다.");
+            }
+            numberSet.add(number);
+        }
+    }
+
 
     private List<Integer> convertToIntegerList(String userInput) {
         List<Integer> integerList = new ArrayList<>();
