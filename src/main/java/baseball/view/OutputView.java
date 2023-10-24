cebdb114d31af820ffeb654680a83e324f1f440a
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-10-24 20:53:21 +0900

    feat : 맞았을때 안내문구및 게임 계속 종료 선택문구 함수 작성

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -23,4 +23,12 @@ public class OutputView {
         }
 
     }
+
+    public void printClosingMessage(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public void printChoiceInfoMessage(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
