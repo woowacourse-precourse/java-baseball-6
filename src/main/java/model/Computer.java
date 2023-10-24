d1cdeefab18a1e39ecba24a22587f7f3fac305f3
cherBlair <cher.blair.h@gmail.com>
2023-10-24 14:35:58 +0900

    [REFACTOR] 컴퓨터 숫자 세팅 수정

--- src/main/java/model/Computer.java
+++ src/main/java/model/Computer.java
@@ -1,27 +1,18 @@
 package model;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+import util.ComputerNumberGenerator;
 
 public class Computer {
     private List<Integer> computerNumbers;
-
-    public List<Integer> computerNumberGenerator() {
-        computerNumbers.clear();
-        while (computerNumbers.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computerNumbers.contains(randomNumber)) {
-                computerNumbers.add(randomNumber);
-            }
-        }
-        return computerNumbers;
-    }
+    private ComputerNumberGenerator computerNumberGenerator = new ComputerNumberGenerator();
 
     public List<Integer> getComputerNumbers() {
         return computerNumbers;
     }
 
     public void setComputerNumbers() {
-        computerNumbers = this.computerNumberGenerator();
+        computerNumbers = computerNumberGenerator.computerNumberGenerator();
     }
+
 }
