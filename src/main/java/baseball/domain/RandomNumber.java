8545ce93e4edc3d5a5866b10c27d3a73838a589b
kimmokalover <kimmokalover@gmail.com>
2023-10-24 13:55:36 +0900

    feat : RandomNumber domain class 생성 & 구현

---
+++ src/main/java/baseball/domain/RandomNumber.java
@@ -0,0 +1,14 @@
+package baseball.domain;
+
+public class RandomNumber {
+    String randomNumber;
+
+
+    public String getRandomNumber() {
+        return randomNumber;
+    }
+
+    public void setRandomNumber(String randomNumber) {
+        this.randomNumber = randomNumber;
+    }
+}
