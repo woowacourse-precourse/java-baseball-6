0a198f30acf004ae8831dbd11df4b066efec0dea
Woo-Shinyoung <tlsdud8205@naver.com>
2023-10-24 22:22:37 +0900

    Feat[controller] 게임 시작시 컴퓨터 랜덤 숫자 생성

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -1,4 +1,17 @@
 package baseball.controller;
 
+import baseball.domain.BaseballNumber;
+import baseball.service.BaseballService;
+
 public class BaseballController {
+    private final BaseballService baseballService;
+
+    public BaseballController() {
+        this.baseballService = new BaseballService();
+    }
+
+    public void playGame() {
+        BaseballNumber computerNumber = baseballService.generateRandomNumbers();
+    }
+
 }
