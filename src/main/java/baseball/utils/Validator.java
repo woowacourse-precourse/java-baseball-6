9bcaba626afbb66d0dd2a2cb62a5bc37e267c233
SeongJunChun <csj991210@naver.com>
2023-10-24 17:57:19 +0900

    feat: 숫자 비교 결과 부분 구현

---
+++ src/main/java/baseball/utils/Validator.java
@@ -0,0 +1,19 @@
+package baseball.utils;
+
+public class Validator {
+    private final NumberCompare numberCompare;
+
+    public Validator(){
+        numberCompare = new NumberCompare();
+    }
+    public int[]  getNumberCompare(String playerNumber, String computerNumber){
+        return numberCompare.getCompareNumber(playerNumber,computerNumber);
+    }
+    public boolean idCorrect(){
+        if(numberCompare.getStrike()==3){
+            return true;
+        }
+        return false;
+    }
+
+}
