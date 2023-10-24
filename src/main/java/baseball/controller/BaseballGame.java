18fde4b1a79388cff61efce27e1e49e6d201362e
oxdjww <oxdjww@gmail.com>
2023-10-24 21:40:42 +0900

    refactor : run() 메서드 내부 종료문 수정 return; -> break;

--- src/main/java/baseball/controller/BaseballGame.java
+++ src/main/java/baseball/controller/BaseballGame.java
@@ -19,19 +19,22 @@ public class BaseballGame {
     }
 
     public void run() {
-
+        // 게임 시작 전 게임 시작 메시지 출력
         GameView.printGameStartMessage();
 
         while (true) {
+            // 유저 번호를 입력 받음
             UserNumber user = new UserNumber(InputView.getUserInput());
-
+            // 입력받은 유저 번호를 컴퓨터 번호하고 비교하여 카운트 생성
             BaseballGameCounts baseballGameCounts = new BaseballGameCounts(computer, user);
-
+            // 카운트에 기반한 메시지 출력
             GameView.printBaseballGameCountsMessage(new BaseballGameCountsMessage(baseballGameCounts));
 
             if (baseballGameCounts.isWinCondition()) {
+                // 승리조건 달성 시 승리 메시지 출력
                 GameView.printGameWinMessage();
-                return;
+                // 승리 시 반복문 종료
+                break;
             }
         }
     }
