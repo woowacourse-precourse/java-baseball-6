7fb33e74f534fdc11231920c5fbc29fe89ee6f83
pletain <pletain@dgu.ac.kr>
2023-10-25 02:53:10 +0900

    Feat: 컴퓨터가 1~9까지 서로 다른 임의의 수 3개를 선택하는 기능 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,4 +1,5 @@
 package baseball;
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,5 +10,15 @@ public class Game {
 
     public Game() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+        createComputerNumber();
+    }
+
+    private void createComputerNumber() { //컴퓨터가 3개의 숫자를 랜덤으로 생성
+        while (computer_number.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer_number.contains(randomNumber)) { //중복되지 않는 숫자인 경우에만 리스트에 추가
+                computer_number.add(randomNumber);
+            }
+        }
     }
 }
