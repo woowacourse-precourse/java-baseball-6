2df0104b77f1be0dca54f923727b687cbaa4f2ae
brixxt27 <brixxt27@gmail.com>
2023-10-25 03:11:08 +0900

    feat: 무작위 숫자 세 개와 내가 입력한 숫자 세 개를 출력하는 프로그램 완성
    - 상수 값은 모두 final static 키워드를 붙여서 인스턴스화가 되지 않아도 사용할 수 있도록 했다.
    - 구글 컨벤션에서 상수는 CONSTANT_CASE를 사용한다고 한다.
    - 길이가 3인 정적 배열을 통해 구현하려 했다. 길이가 이미 정해져 있으므로 비교적 무거운 Collection을 일부러 사용하지 않았다.
    - 무작위로 돌렸을 때 현재 나온 숫자가 이전에 나온 숫자와 동일한 숫자가 나오면 다시 하도록 구현했다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,44 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
+
 public class Application {
+    private final static int MAX_LENGTH = 3;
+    private final static int START_INCLUSIVE = 1;
+    private final static int END_INCLUSIVE = 9;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String str = readLine();
+        int[] numsArray = new int[MAX_LENGTH];
+        int i = 0;
+
+        while (true) {
+            boolean isSame = false;
+
+            if (i == MAX_LENGTH) {
+                break;
+            }
+            numsArray[i] = pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
+            for (int j = 0; j < i; j++) {
+                if (numsArray[j] == numsArray[i]) {
+                    isSame = true;
+                    break;
+                }
+            }
+            if (isSame) {
+                continue;
+            }
+            i++;
+        }
+
+        for (int j = 0; j < MAX_LENGTH; j++) {
+            System.out.print(numsArray[j]);
+        }
+        System.out.println("\n" + str);
     }
 }
