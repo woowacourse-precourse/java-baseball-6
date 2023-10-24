e15f2a3d742fecdfb138922449db8ced64dd845d
songjayhyun <thewinner1st@naver.com>
2023-10-25 00:57:08 +0900

    feat: 게임 진행 기능 추가
    - 게임을 진행하는 GameSystem.class를 추가한다.
    - 게임을 진행하는 메소드를 추가한다.
    - 게임을 시작하는 메소드를 추가한다.
    - 게임을 재시작하는 메소드를 추가한다.

---
+++ src/main/java/baseball/GameSystem.java
@@ -0,0 +1,18 @@
+package baseball;
+
+public class GameSystem {
+    public void playGame() {
+        do {
+            startGame();
+        } while (restartGame());
+    }
+
+    private void startGame() {
+    }
+
+    private boolean restartGame() {
+        // TO-DO restart game 로직 작성
+
+        return false;
+    }
+}
