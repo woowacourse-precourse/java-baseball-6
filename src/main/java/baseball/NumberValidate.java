65d70166a10376d51beb7030b503d43059073fa2
chosunghyun18 <krismon@naver.com>
2023-10-24 21:00:24 +0900

    feat : add number validate isNumber method - 숫자인지 파악한다

--- src/main/java/baseball/NumberValidate.java
+++ src/main/java/baseball/NumberValidate.java
@@ -6,4 +6,14 @@ public class NumberValidate {
     public static boolean isInRange(int givenNumber) {
         return MIN_VALUE <= givenNumber && givenNumber <= MAX_VALUE ;
     }
+
+    public static boolean isNumber(String number) {
+        try{
+            Integer.parseInt(number);
+            return true;
+        }catch (NumberFormatException e){
+            System.out.println(e+"숫자가 아님");
+            return false;
+        }
+    }
 }
