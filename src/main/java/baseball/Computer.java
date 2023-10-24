83b38191609d64dc427955a4d8accd8e67d50663
saera <yook.saera@gmail.com>
2023-10-25 03:11:03 +0900

    feat: Computer가 서로 다른 숫자로 구성된 3자리 수를 뽑아라

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,18 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private final List<Integer> computer = new ArrayList<>();
+
+    public Computer() {
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+    }
+}
