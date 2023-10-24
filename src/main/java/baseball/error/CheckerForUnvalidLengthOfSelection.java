642c794b3f80178071fd36547f2b664903cb0b67
JangHongJoon <wkdghdwns19969@gmail.com>
2023-10-24 23:42:54 +0900

    Refactor : 기호 상수로 치환

--- src/main/java/baseball/error/CheckerForUnvalidLengthOfSelection.java
+++ src/main/java/baseball/error/CheckerForUnvalidLengthOfSelection.java
@@ -1,8 +1,9 @@
 package baseball.error;
 
 public class CheckerForUnvalidLengthOfSelection {
+    private final Integer MAX_LENGTH_OF_SELECTION = 1;
     public void check(String gameContinueSelection) {
-        if (gameContinueSelection.length() != 1){
+        if (gameContinueSelection.length() != MAX_LENGTH_OF_SELECTION){
             throw new IllegalArgumentException();
         }
     }
