5858211c943c3226fe8874fee394af9e53c5b05c
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2023-10-24 18:33:55 +0900

    feat: 게임 종료, 게임 재시작 기능 구현
    gameOver, restart 메서드 구현

--- src/main/java/baseball/service/BaseBallGameService.java
+++ src/main/java/baseball/service/BaseBallGameService.java
@@ -23,12 +23,23 @@ public class BaseBallGameService {
             List<Integer> inputNumberList = user.getInputNumberList();
             playBaseBall(randomNumbersList, inputNumberList);
         }
+
     }
 
     public void playBaseBall(List<Integer> computerNumberList, List<Integer> UserNumberList) {
         int strike = baseballCount.countStrike(computerNumberList, UserNumberList);
         int ball = baseballCount.countBall(computerNumberList, UserNumberList);
         baseballCount.printCountResult(ball, strike);
+    }
+
+    public void gameOver() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public boolean restart() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String restartInputNumber = readLine();
+        return restartInputNumber.equals("1");
     }
 
 
