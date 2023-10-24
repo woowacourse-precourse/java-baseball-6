e775c1d161e1be344a796a94f1572bb204df3db2
D0ri123 <lsh81550@gmail.com>
2023-10-24 17:10:09 +0900

    refactor: Baseball 클래스 필드 수정, 메서드 추가 - 4차 구현
    클래스의 public 필드들을 private 필드로 바꿈으로써,
    클래스의 메서드로 접근할 수 있도록 변경했습니다.
    클래스가 가진 정보는 자기 자신이 직접 바꾸도록 분리된 메서드를 추가했습니다.

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -1,23 +1,59 @@
 package baseball;
 
+import static baseball.Type.*;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+//TODO: java17의 record 적용해보기
 class Baseball {
 
-  // 야구공 타입 - None, 스트라이크, 볼
-  Type type;
+  // 야구공 타입 - 스트라이크, 볼
+  private Type type;
 
   // 야구공 숫자
-  int number;
+  private final int number;
 
   // 야구공의 위치
-  int index;
+  private final int index;
 
-  public Baseball(Type type, int number, int index) {
-    this.type = type;
+  public Baseball(int number, int index) {
     this.number = number;
     this.index = index;
   }
 
   public void changeType(Type type) {
     this.type = type;
+  }
+
+  public Type getType() {
+    return this.type;
+  }
+
+  public int getNumber() {
+    return this.number;
+  }
+
+  public int getIndex() {
+    return this.index;
+  }
+
+  public void checkType(ArrayList<String> randomList) {
+    String baseballNumber = getNumberToString();
+    if(randomList.contains(baseballNumber)) {
+      changeType((randomList.indexOf(baseballNumber) == getIndex() ? STRIKE : BALL));
+    }
+  }
+
+  private String getNumberToString() {
+    return String.valueOf(getNumber());
+  }
+
+  public void calculate(Map<String, Integer> scoreBoard) {
+    if(getType() == null) {
+      return;
+    }
+    String myType = type.getType();
+    scoreBoard.put(myType, scoreBoard.get(myType)+1);
   }
 }
