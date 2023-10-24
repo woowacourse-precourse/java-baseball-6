d9ea4a8289ca64bd5453a0e9bb1c395e40965a09
Sinorang <aodkrak@naver.com>
2023-10-25 00:39:48 +0900

    Update CompareNumber.java

--- src/main/java/baseball/CompareNumber.java
+++ src/main/java/baseball/CompareNumber.java
@@ -16,7 +16,7 @@ public class CompareNumber {
     public int countStrike(List<Integer> computer, List<Integer> player) {
         int strike = 0;
         for (int i = 0; i < player.size(); i++) {
-            if (computer.get(i) == player.get(i)) {
+            if (computer.get(i) != null && computer.get(i).equals(player.get(i))) {
                 strike += 1;
             }
         }
