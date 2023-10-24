fc0f89f7fa2bd1cbcb2613bd4c049a19626c3c14
jihun-24k <gi020477@gmail.com>
2023-10-25 02:20:26 +0900

    Feat: 코멘트 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -17,6 +17,7 @@ public class Application {
             input = Console.readLine();
             List<Integer> inputNumbers = Util.toIntegerArray(input);
             String comment = takeTurn(computer, inputNumbers);
+            System.out.println(comment);
 
             break outer;
         }
@@ -27,15 +28,50 @@ public class Application {
         comment += countBall(computer, inputNumbers);
         comment += countStrike(computer, inputNumbers);
 
-        return comment;
+        return checkComment(comment);
+    }
+
+    private static String checkComment(String comment) {
+        if (comment.isEmpty()) {
+            comment = "낫싱";
+        }
     }
 
     private static String countStrike(List<Integer> computer, List<Integer> inputNumbers) {
-        return null;
+        int strikes = 0;
+        for (int i = 0; i < 3; i++) {
+            if (computer.get(i).equals(inputNumbers.get(i))) {
+                strikes++;
+            }
+        }
+        return setStrikeComment(strikes);
+    }
+
+    private static String setStrikeComment(int strikes) {
+        String strikeComment = String.format("%d스트라이크", strikes);
+        if (strikes == 0) {
+            strikeComment = "";
+        }
+        return strikeComment;
     }
 
     private static String countBall(List<Integer> computer, List<Integer> inputNumbers) {
-        return null;
+        int balls = 0;
+        for (int i = 0; i < 3; i++) {
+            Integer inputNumber = inputNumbers.get(i);
+            if (!computer.get(i).equals(inputNumber) && computer.contains(inputNumber)) {
+                balls++;
+            }
+        }
+        return setBallComment(balls);
+    }
+
+    private static String setBallComment(int balls) {
+        String ballComment = String.format("%d볼 ", balls);
+        if (balls == 0) {
+            ballComment = "";
+        }
+        return ballComment;
     }
 
 }
