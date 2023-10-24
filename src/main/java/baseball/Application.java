0b9a0553ddaad3e02cafac3ff442f16dbdcac194
akdntmrkdjqtsp <gytjd9516@naver.com>
2023-10-24 19:04:09 +0900

    오류 던지는 대신 트라이 캐치

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,21 +3,26 @@ package baseball;
 import java.io.*;
 
 public class Application {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         // TODO: 프로그램 구현
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    
+        try {
+            writer.write("숫자 야구 게임을 시작합니다.");
+            writer.flush();
+    
+            Baseball baseball = new Baseball();
+            baseball.start();
+            while(true) {
+                if (!baseball.play()) {
+                    break;
+                }
         
-        writer.write("숫자 야구 게임을 시작합니다.");
-        writer.flush();
-        
-        Baseball baseball = new Baseball();
-        baseball.start();
-        while(true) {
-            if (!baseball.play()) {
-                break;
             }
-            
+            writer.close();
+        } catch (IOException e) {
+        
         }
-        writer.close();
+        
     }
 }
