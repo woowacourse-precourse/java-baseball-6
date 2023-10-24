83b5d5300d1e02f376a75a48b23401af4e33e3e5
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-10-24 20:42:55 +0900

    feat : 숫자 입력 문구 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,7 +1,11 @@
 package baseball.view;
 
 public class OutputView {
-    public void printStartGuide(){
+    public void printStartPhrase(){
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void printNumberInputPhrase(){
+        System.out.print("숫자를 입력해주세요 : ");
     }
 }
