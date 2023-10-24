4958260e3ddbea3c649940ec28017e4f0c1bb674
yeahdy <yejine98@naver.com>
2023-10-25 03:35:15 +0900

    feat: View 메세지 출력 기능구현, MessageType enum 생성

--- src/main/java/view/BaseballGameView.java
+++ src/main/java/view/BaseballGameView.java
@@ -1,4 +1,18 @@
 package view;
 
+import constants.MessageType;
+
 public class BaseballGameView {
+
+    public String printMessage(MessageType messageType){
+        String message= "";
+        switch (messageType){
+            case GAME_START -> message = MessageType.GAME_START.getMessage();
+            case INPUT_NUMBER -> message = MessageType.INPUT_NUMBER.getMessage();
+            case STRIKE -> message = MessageType.STRIKE.getMessage();
+            case GAME_END -> message = MessageType.GAME_END.getMessage();
+        }
+        return message;
+    }
+
 }
