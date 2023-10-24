f48e4ef3a1068f1b2900b6166780dcc63cefd41b
JaehongDev <println@kakao.com>
2023-10-24 20:00:58 +0900

    docs: EventListener 주석 추가

--- src/main/java/baseball/event/EventListener.java
+++ src/main/java/baseball/event/EventListener.java
@@ -5,21 +5,48 @@ import java.util.function.Function;
 
 public record EventListener(EventContext eventContext) {
 
+    /**
+     * 결과와 파라미터가 없는 이벤트
+     *
+     * @param eventConstructor
+     */
     public void listen(Function<EventContext, Event> eventConstructor) {
         eventConstructor.apply(eventContext).execute();
     }
 
+
+    /**
+     * 결과만 있는 이벤트
+     *
+     * @param eventConstructor
+     * @param <R>
+     * @return
+     */
     public <R> R listenWithResult(Function<EventContext, ReturnEvent<R>> eventConstructor) {
         return eventConstructor.apply(eventContext).execute();
     }
 
+    /**
+     * 파라미터만 있는 이벤트
+     *
+     * @param eventConstructor
+     * @param <T>
+     * @return
+     */
     public <T> Consumer<T> listenWithParameter(Function<EventContext, ParameterEvent<T>> eventConstructor) {
         return (input) -> eventConstructor.apply(eventContext).execute(input);
     }
 
+    /**
+     * 파라미터와 결과 모두 있는 이벤트
+     *
+     * @param eventConstructor
+     * @param <T>
+     * @param <R>
+     * @return
+     */
     public <T, R> Function<T, R> listenWithParameterAndResult(Function<EventContext, ParameterAndReturnEvent<T, R>> eventConstructor) {
         return (input) -> eventConstructor.apply(eventContext).execute(input);
-
     }
 
 
