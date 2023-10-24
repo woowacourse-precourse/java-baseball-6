7eaa4a2643ba5eafa1b48a22c46c14144e623547
jihoon98 <tlswlgns1003@sju.ac.kr>
2023-10-25 00:20:39 +0900

    "feat:게임 종료 후 문구 출력 메세지 작성"

--- src/main/java/baseball/view/EndView.java
+++ src/main/java/baseball/view/EndView.java
@@ -1,4 +1,10 @@
 package baseball.view;
 
 public class EndView {
+    public void displayEndMessage(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
+    }
+    public void displayRestartMessage(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
