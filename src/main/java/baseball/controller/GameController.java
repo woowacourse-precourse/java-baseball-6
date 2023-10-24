715476a20ef8361b3def1f712c59924451fb3d92
mainfn <_@mainfn.dev>
2023-10-24 22:46:13 +0900

    feat: GameController 의존성 변경

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -5,23 +5,21 @@ import baseball.domain.ball.Guess;
 import baseball.domain.game.Computer;
 import baseball.domain.game.GameResult;
 import baseball.domain.game.GameStatus;
-import baseball.view.InputView;
-import baseball.view.OutputView;
 import collection.Triple;
 
 public final class GameController {
 
-    private final InputView inputView;
-    private final OutputView outputView;
     private final Computer computer;
+    private final InputManager inputManager;
+    private final OutputManager outputManager;
 
     public GameController(
-            final InputView inputView,
-            final OutputView outputView,
+            final InputManager inputManager,
+            final OutputManager outputManager,
             final Computer computer
     ) {
-        this.inputView = inputView;
-        this.outputView = outputView;
+        this.inputManager = inputManager;
+        this.outputManager = outputManager;
         this.computer = computer;
     }
 
@@ -33,7 +31,7 @@ public final class GameController {
 
     private void askUntilCorrectGuess(final Computer computer) {
         final GameResult result = askGuess(computer);
-        outputView.printResult(result);
+        outputManager.printHint(result);
 
         if (!result.hasThreeStrike()) {
             askUntilCorrectGuess(computer);
@@ -41,13 +39,13 @@ public final class GameController {
     }
 
     private GameResult askGuess(final Computer computer) {
-        final Triple<BallNumber> ballNumbers = inputView.inputBallNumbers();
+        final Triple<BallNumber> ballNumbers = inputManager.inputBallNumbers();
         final Guess guess = Guess.of(ballNumbers);
         return computer.check(guess);
     }
 
     private void replayOrExit() {
-        final GameStatus status = inputView.inputNextGameStatus();
+        final GameStatus status = inputManager.inputNextGameStatus();
         if (status == GameStatus.REPLAY) {
             play();
         }
