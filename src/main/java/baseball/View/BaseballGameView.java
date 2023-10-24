2b7d6191750070f0a2ad1ac5e3fa59ed9142aff6
jiixon <jr0107380@gmail.com>
2023-10-25 02:42:10 +0900

    feat: 사용자 숫자 입력

--- src/main/java/baseball/View/BaseballGameView.java
+++ src/main/java/baseball/View/BaseballGameView.java
@@ -1,7 +1,12 @@
 package baseball.View;
+import camp.nextstep.edu.missionutils.Console;
 
 public class BaseballGameView {
     public void gameStart(){
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+    public String getUserInput(){
+        System.out.println("숫자를 입력해주세요 :");
+        return Console.readLine();
     }
 }
