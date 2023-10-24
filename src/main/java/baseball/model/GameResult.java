dc029ffb8e2f7980a898f7b5029b362acaaa6fef
eora21 <eora21@naver.com>
2023-10-24 17:57:31 +0900

    feat: 🎸 추가 GameResult 생성 시 논리적으로 존재할 수 없을 상태에 대한 검증 메서드 작성

--- src/main/java/baseball/model/GameResult.java
+++ src/main/java/baseball/model/GameResult.java
@@ -1,6 +1,11 @@
 package baseball.model;
 
 public record GameResult(int perfectGameCount, int correctCount, int similarCount) {
+    public GameResult {
+        if (perfectGameCount < correctCount + similarCount) {
+            throw new IllegalArgumentException();
+        }
+    }
 
     public boolean isPerfectGame() {
         return correctCount() == perfectGameCount();
