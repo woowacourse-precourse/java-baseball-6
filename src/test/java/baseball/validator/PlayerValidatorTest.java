693a88cb85819516101f50b2c013d2dc82897675
GaBaljaintheroom <junsu1222@naver.com>
2023-10-24 22:06:54 +0900

    test: 사용자가 중복된 숫자를 입력하면 에러가 발생한다.

--- src/test/java/baseball/validator/PlayerValidatorTest.java
+++ src/test/java/baseball/validator/PlayerValidatorTest.java
@@ -3,6 +3,8 @@ package baseball.validator;
 import baseball.model.player.PlayerValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 class PlayerValidatorTest {
@@ -15,4 +17,16 @@ class PlayerValidatorTest {
         assertThatThrownBy(() -> playerValidator.validateExpectedNumbers("1234"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @ParameterizedTest
+    @DisplayName("사용자가 중복된 숫자를 입력하면 에러가 발생한다.")
+    @ValueSource(strings = {"133", "222", "1455", "9999"})
+    void input_duplicate_error(String number){
+        assertThatThrownBy(() -> playerValidator.validateExpectedNumbers(number))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+
+
 }
