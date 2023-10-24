893627ffde41a382f7a14ac10fa5456c19481f14
BDlhj <akk808@naver.com>
2023-10-24 21:32:34 +0900

    feat: Implement printGameEndingMessage() in src/main/java/baseball/GameEndView.java

--- src/main/java/baseball/GameEndView.java
+++ src/main/java/baseball/GameEndView.java
@@ -1,2 +1,9 @@
-package baseball;public class GameEndView {
+package baseball;
+
+public class GameEndView {
+    private static final String GAME_ENDING_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    public void printGameEndingMessage() {
+        System.out.println(GAME_ENDING_MESSAGE);
+    }
 }
