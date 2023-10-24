d55401bc0f6a68dd4d3f9269b082fc2c3dc5350b
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-10-24 20:52:50 +0900

    modify : 출력 클래스 static으로 변경

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,19 +1,22 @@
 package baseball.view;
 
 public class OutputView {
-    public void printStartPhrase(){
+
+    private OutputView(){}
+
+    public static void printStartPhrase(){
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    public void printNumberInputPhrase(){
+    public static void printNumberInputPhrase(){
         System.out.print("숫자를 입력해주세요 : ");
     }
 
-    public void printExitOptionPhrase(){
+    public static void printExitOptionPhrase(){
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
-    public void printExitPhrase(){
+    public static void printExitPhrase(){
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 }
