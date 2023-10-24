34f25bab47e9035440e8e84fbef3d692d98707ba
mywnajsldkf <mywnajsldkf@gmail.com>
2023-10-24 21:04:41 +0900

    test : player와 computer가 생성한 공을 제대로 비교하는지 확인한다. 외부 객체를 테스트하기 위해 stub을 생성한다.

---
+++ src/main/java/baseball/util/BaseBallManagerStub.java
@@ -0,0 +1,46 @@
+package baseball.util;
+
+import baseball.model.BallNumber;
+import baseball.model.BallNumberGroup;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BaseBallManagerStub {
+    private static BallNumberGroup computerBallGroup;
+    private static Map<Integer, Integer> stubComputerBallInfo;
+    private static Map<String, Integer> stubPlayerResult;
+
+    public void setComputerBaseBallNumber(BallNumberGroup ballNumberGroup) {
+        this.computerBallGroup = ballNumberGroup;
+        stubComputerBallInfo = new HashMap<>();
+        List<BallNumber> computerBall = computerBallGroup.getBallNumberGroup();
+        for (int ballIdx = 0; ballIdx < 3; ballIdx++) {
+            stubComputerBallInfo.put(computerBall.get(ballIdx).getNumber(), ballIdx);
+        }
+    }
+
+    public Map<String, Integer> compareBaseBall(BallNumberGroup playerBallGroup) {
+        stubPlayerResult = new HashMap<>();
+        List<BallNumber> playerBalls = playerBallGroup.getBallNumberGroup();
+        int nothingCount = 0;
+
+        for (int ballIdx = 0; ballIdx < 3; ballIdx++) {
+            Integer playerBall = playerBalls.get(ballIdx).getNumber();
+            if (!stubComputerBallInfo.containsKey(playerBall)) {
+                nothingCount++;
+            } else {
+                if (ballIdx == stubComputerBallInfo.get(playerBall)) {
+                    stubPlayerResult.put("스트라이크", stubPlayerResult.getOrDefault("스트라이크", 0) + 1);
+                } else if (ballIdx != stubComputerBallInfo.get(playerBall)) {
+                    stubPlayerResult.put("볼", stubPlayerResult.getOrDefault("볼", 0) + 1);
+                }
+            }
+        }
+
+        if (nothingCount == 3) stubPlayerResult.put("낫싱", 1);
+
+        return stubPlayerResult;
+    }
+}
---
+++ src/test/java/baseball/model/BaseBallManagerTest.java
@@ -0,0 +1,41 @@
+package baseball.model;
+
+import baseball.util.BaseBallManagerStub;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+class BaseBallManagerTest {
+
+    @Test
+    @DisplayName("player가 입력한 공과 computer가 생성한 공의 위치와 값이 같으면 스트라이크 3개가 나와야 한다.")
+    void compareBaseBall() {
+        // stub
+        List<Integer> stubBallNumbers = new ArrayList<>();
+        for (int ballNumber = 1; ballNumber <= 3; ballNumber++) {
+            stubBallNumbers.add(ballNumber);
+        }
+        BallNumberGroup stubBallNumberGroup = new BallNumberGroup(stubBallNumbers);
+
+        // set ballNumberGroup
+        BaseBallManagerStub stubManager = new BaseBallManagerStub();
+        stubManager.setComputerBaseBallNumber(stubBallNumberGroup);
+
+        // given
+        List<Integer> testPlayerBallNumberGroup = new ArrayList<>();
+        for (int ballNumber = 1; ballNumber <= 3; ballNumber++) {
+            testPlayerBallNumberGroup.add(ballNumber);
+        }
+        BallNumberGroup testPlayerBallNumber = new BallNumberGroup(testPlayerBallNumberGroup);
+
+        // when
+        Map<String, Integer> testCompareResult = stubManager.compareBaseBall(testPlayerBallNumber);
+
+        // then
+        Assertions.assertEquals(3, testCompareResult.get("스트라이크"));
+    }
+}
