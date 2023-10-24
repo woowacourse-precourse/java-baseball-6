6f12bebce7e725fe8881f77d3144e8bca84703b4
kmw2378 <rlarla677@gmail.com>
2023-10-24 14:57:14 +0900

    feat: GameService에 컴퓨터와 사용자 숫자를 비교하여 결과를 계산하는 메소드 추가 컴퓨터와 사용자 숫자를 비교하여 볼/스트라이크 개수를 계산 후 GameResultResponse 객체를 생성하는 메소드를 추가하였습니다.

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -1,4 +1,37 @@
 package baseball.service;
 
+import baseball.domain.Computer;
+import baseball.domain.User;
+import baseball.dto.response.GameResultResponse;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+import static baseball.common.config.GameConfig.NUMBER_SIZE;
+
 public class GameService {
+    public GameResultResponse getResult(final Computer computer, final User user) {
+        List<Integer> computerNumberList = computer.getNumbers().getNumberList();
+        List<Integer> userNumberList = user.getNumbers().getNumberList();
+
+        final Set<Integer> entryNumberSet = new HashSet<>(userNumberList);
+        entryNumberSet.addAll(computerNumberList);
+        int ballCount = userNumberList.size() + computerNumberList.size() - entryNumberSet.size();
+        int strikeCount = 0;
+
+        for (int i = 0; i < NUMBER_SIZE; i++) {
+            if (ballCount == 0) {
+                break;
+            }
+
+            if (Objects.equals(computerNumberList.get(i), userNumberList.get(i))) {
+                strikeCount++;
+                ballCount--;
+            }
+        }
+
+        return new GameResultResponse(ballCount, strikeCount);
+    }
 }
