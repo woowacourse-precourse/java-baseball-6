2e409a934639e0f807fc1f2b437b735677cf1b67
박준영 <wnsdud6969@naver.com>
2023-10-24 17:38:01 +0900

    feat(Number): add Number to be used in the number game

---
+++ src/main/java/baseball/Number.java
@@ -0,0 +1,11 @@
+package baseball;
+
+import java.util.List;
+
+public class Number {
+    private List<Integer> numbers;
+
+    public Number(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+}
