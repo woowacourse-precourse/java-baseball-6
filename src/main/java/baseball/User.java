ac9a7611d8ab6089ec17303a1899ef6476e86550
youjae <youjae0803@gmail.com>
2023-10-25 00:49:10 +0900

    feat: create User class

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,2 +1,13 @@
-package baseball;public class User {
+package baseball;
+
+public class User {
+    private GuessVo guessVo;
+
+    public GuessVo getGuessVo() {
+        return guessVo;
+    }
+
+    public void setGuessVo(GuessVo guessVo) {
+        this.guessVo = guessVo;
+    }
 }
