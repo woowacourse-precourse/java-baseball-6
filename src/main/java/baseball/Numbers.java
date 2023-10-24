9ae25ff4d7b0aace794808af3a7d3de5a4cb25db
20191777 <20191777@o365.hanbat.ac.kr>
2023-10-24 22:15:59 +0900

    REFACTOR: 중복 기능을 가진 클래스 합침 -  Nums 클래스와 GeneratingNumbers 클래스를 합침

--- src/main/java/baseball/Numbers.java
+++ src/main/java/baseball/Numbers.java
@@ -6,40 +6,41 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GeneratingNumbers {
-    static Nums nums = new Nums();
+public class Numbers {
+    private List<Integer> computer = new ArrayList<>();
+    private List<Integer> mine = new ArrayList<>();
 
-    // 상대방(컴퓨터) 숫자 생성
-    public void getComputerNums(){
-        List<Integer> computer = new ArrayList<>();
+    public List<Integer> getComputer() {
+        return computer;
+    }
+    public List<Integer> getMine() {
+        return mine;
+    }
+
+    public void setComputer() {
+        computer.clear();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
         }
-        nums.setComputer(computer);
     }
-
-    // 내 숫자 생성
-    public void getMyNums(){
+    public void setMine() {
+        mine.clear();
         System.out.print("숫자를 입력해주세요 : ");
         String inputNumber = Console.readLine();
 
-        // 숫자 개수가 3개가 아닐 경우, 문자를 입력할 경우 예외처리
         if (inputNumber.length() != 3 || !isInteger(inputNumber)) {
             throw new IllegalArgumentException();
         }
 
-        List<Integer> mine = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
             int num = Character.getNumericValue(inputNumber.charAt(i));
             if (mine.contains(num)) {  // 중복입력 예외처리
                 throw new IllegalArgumentException();
             }else mine.add(num);
         }
-
-        nums.setMine(mine);
     }
 
     public static boolean isInteger(String strValue) {
--- src/main/java/baseball/Play.java
+++ src/main/java/baseball/Play.java
@@ -1,16 +1,14 @@
 package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
-import static baseball.GeneratingNumbers.nums;
-
 public class Play {
-    GeneratingNumbers getNums = new GeneratingNumbers();
+    Numbers nums = new Numbers();
     Result result = new Result();
 
     public void play(){
         System.out.println("숫자 야구 게임을 시작합니다.");
         int flag = 1;
-        getNums.getComputerNums();
+        nums.setComputer();
 
         while (flag >= 1) {
             if (flag == 2) {  // 다시하기 분기
@@ -18,7 +16,7 @@ public class Play {
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 int ans = Integer.parseInt(Console.readLine());
                 if (ans == 1) { // 재시작
-                    getNums.getComputerNums();
+                    nums.setComputer();
                     flag = ans;
                 } else if (ans == 2) {  // 게임 종료
                     flag = 0;
@@ -26,7 +24,9 @@ public class Play {
                 } else
                     throw new IllegalArgumentException();
             } else if (flag == 1) {
-                getNums.getMyNums();
+                nums.setMine();
+                System.out.println(nums.getComputer());
+                System.out.println(nums.getMine());
                 flag = result.getGameResultJudgment(nums.getComputer(), nums.getMine());
             }
         }
