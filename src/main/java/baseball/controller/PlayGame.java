760ced40c3abf02ba0c6e002791ff33857fbc9b1
SeongJunChun <csj991210@naver.com>
2023-10-24 17:54:58 +0900

    feat:게임 컨트롤러 구현(재시작 기능추가)

---
+++ src/main/java/baseball/controller/PlayGame.java
@@ -0,0 +1,59 @@
+package baseball.controller;
+
+import baseball.model.GenerateRandomNum;
+import baseball.model.PlayerNumber;
+import baseball.model.ReplayGame;
+import baseball.utils.NumberCompare;
+import baseball.utils.Validator;
+import baseball.view.RequestMessage;
+import baseball.view.SystemMessage;
+
+public class PlayGame {
+    private GenerateRandomNum generateRandomNum;
+    private final Validator validator;
+    private static PlayerNumber player;
+
+    public PlayGame() {
+        validator = new Validator();
+    }
+
+    private void getNumber(){
+        player = new PlayerNumber(RequestMessage.requestNumber());
+        player.getPlayerNumber();
+    }
+
+    public void startGame(){
+        SystemMessage.printGameStart();
+        do{
+            generateRandomNum = new GenerateRandomNum();
+            playOneGame();
+        }while (isReplay());
+    }
+
+    public int[] numberCompare(){
+        return validator.getNumberCompare(player.getPlayerNumber(),generateRandomNum.getComputerNumber());
+    }
+
+    public void playOneGame(){
+        do{
+            getNumber();
+            SystemMessage.pintResult(numberCompare());
+        }while(!isGmaeset());
+    }
+
+    public boolean isGmaeset(){
+        if(validator.idCorrect()){
+            SystemMessage.printGameSetMessage();
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isReplay(){
+        ReplayGame replayGame = new ReplayGame(RequestMessage.requestPlay());
+        if(replayGame.getReplayNumber().equals("1")){
+            return true;
+        }
+        return false;
+    }
+}
