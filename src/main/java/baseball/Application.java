63b6cd298506c7a80323229ebcc215372eaa5377
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2023-10-24 12:56:37 +0900

    fix: match input and output with test code

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,53 +1,30 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     static final int NUM_OF_DIGITS = 3;
-    static boolean GAME = true;
-    static boolean WIN = false;
 
     public static void main(String[] args) {
+        GameState gameState = new GameState();
         System.out.println("숫자 야구 게임을 시작합니다.");
-        while (GAME){
+
+        while (gameState.isGameOn()) {
             List<Integer> computerNumber = createRandomNumbers();
-            while (!WIN) {
-                int[] userNumber = new int[NUM_OF_DIGITS];
-                getUserInput(userNumber);
-                String ballStrikeCheck = checkNumber(userNumber, computerNumber);
+            while (!gameState.isWin()) {
+                int[] userNumber = getUserInput();
+                String ballStrikeCheck = checkNumber(userNumber, computerNumber, gameState);
                 System.out.println(ballStrikeCheck);
-                if (WIN)
-                    break;
             }
-            Scanner scanner = new Scanner(System.in);
-            try {
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                int restartOption = scanner.nextInt();
-                if (restartOption != 1 && restartOption != 2) {
-                    throw new IllegalArgumentException();
-                }
-                if (restartOption == 1) {
-                    WIN = false;
-                }
-                if (restartOption == 2) {
-                    GAME = false;
-                }
-            } catch (IllegalArgumentException e) {
-                e.printStackTrace();
-            }
+            askForRestart(gameState);
         }
     }
 
-    /**
-     * 컴퓨터가 랜덤한 3 자리 정수를 생성하는 함수
-     *
-     * @return ArrayList[3]
-     */
-    public static List<Integer> createRandomNumbers() {
+    private static List<Integer> createRandomNumbers() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < NUM_OF_DIGITS) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -58,50 +35,102 @@ public class Application {
         return computer;
     }
 
-    /**
-     * 사용자가 입력한 숫자를 3 자리로 나눠 배열에 저장하는 함수
-     *
-     * @param userNumberDigits
-     */
-    private static void getUserInput(int[] userNumberDigits) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("숫자를 입력해주세요 : ");
-        Integer userNumber = null;
-        try {
-            String userNumberString = scanner.next();
-            userNumber = Integer.parseInt(userNumberString);
-            if (userNumberString.length() != NUM_OF_DIGITS)
-                throw new IllegalAccessException();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-        for (int i = NUM_OF_DIGITS - 1; i >= 0; i--) {
-            int filteredNum = (int) (userNumber % Math.pow(10, i + 1));
-            int digit = filteredNum / (int) Math.pow(10, i);
-            int index = NUM_OF_DIGITS - 1 - i;
-            userNumberDigits[index] = digit;
+    private static int[] getUserInput() {
+        int[] userNumberDigits = new int[NUM_OF_DIGITS];
+        while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
+            String userNumberString = readLine();
+            if (isValidInput(userNumberString)) {
+                for (int i = 0; i < NUM_OF_DIGITS; i++) {
+                    userNumberDigits[i] = userNumberString.charAt(i) - '0';
+                }
+                return userNumberDigits;
+            }
+            throw new IllegalArgumentException();
         }
     }
 
-    public static String checkNumber(int[] userNumbers, List<Integer> computerNumbers) {
+    private static boolean isValidInput(String input) {
+        if (input.length() != NUM_OF_DIGITS) {
+            return false;
+        }
+        for (char ch : input.toCharArray()) {
+            if (ch < '0' || ch > '9') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static String checkNumber(int[] userNumbers, List<Integer> computerNumbers, GameState gameState) {
         int ballCount = 0;
         int strikeCount = 0;
         for (int i = 0; i < NUM_OF_DIGITS; i++) {
             for (int j = 0; j < NUM_OF_DIGITS; j++) {
-                if (userNumbers[i] == computerNumbers.get(j) && i != j)
-                    ballCount++;
-                if (userNumbers[i] == computerNumbers.get(j) && i == j)
-                    strikeCount++;
+                if (userNumbers[i] == computerNumbers.get(j)) {
+                    if (i == j) {
+                        strikeCount++;
+                    } else {
+                        ballCount++;
+                    }
+                }
             }
         }
-        String result = "아웃";
-        if (ballCount != 0 && strikeCount == 0) result = String.format("%d볼", ballCount);
-        if (ballCount == 0 && strikeCount != 0) result = String.format("%d스트라이크", strikeCount);
-        if (ballCount != 0 && strikeCount != 0) result = String.format("%d볼 %d스트라이크", ballCount, strikeCount);
         if (strikeCount == NUM_OF_DIGITS) {
-            result = String.format("%d개의 숫자를 모두 맞히셨습니다! 게임종료", NUM_OF_DIGITS);
-            WIN = true;
+            gameState.rightAnswer();
+            return String.format("%d스트라이크\n%d개의 숫자를 모두 맞히셨습니다! 게임종료", NUM_OF_DIGITS, NUM_OF_DIGITS);
         }
-        return result;
+        if (ballCount == 0 && strikeCount == 0) {
+            return "낫싱";
+        }
+        if (ballCount == 0) {
+            return String.format("%d스트라이크", strikeCount);
+        }
+        if (strikeCount == 0) {
+            return String.format("%d볼", ballCount);
+        }
+        return String.format("%d볼 %d스트라이크", ballCount, strikeCount);
+    }
+
+    private static void askForRestart(GameState gameState) {
+        while (true) {
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String option = readLine();
+            if ("1".equals(option)) {
+                gameState.reset();
+                return;
+            } else if ("2".equals(option)) {
+                gameState.endGame();
+                System.out.println("게임 종료");
+                return;
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+}
+
+class GameState {
+    private boolean gameOn = true;
+    private boolean win = false;
+
+    public boolean isGameOn() {
+        return gameOn;
+    }
+
+    public boolean isWin() {
+        return win;
+    }
+
+    public void endGame() {
+        this.gameOn = false;
+    }
+
+    public void rightAnswer() {
+        this.win = true;
+    }
+
+    public void reset() {
+        this.win = false;
     }
 }
