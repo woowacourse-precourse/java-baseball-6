9248ad187b330410d2decedf43aa3009c29de331
suhyun__ <thebbbb1206@gmail.com>
2023-10-24 19:34:47 +0900

    fix: 재시작 시 라운드 반복 / 종료되도록 코드 수정

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -10,14 +10,19 @@ public class BaseballGame {
 
     public void start() {
         showStart();
-        Computer computer = new Computer();
-        computerNumberList = computer.generateComputerNumber();
-        showInputUserNumber();
-        userNumberList = getUsetNumberList();
-        int[] compareResult = new CompareNumber().getCompareResult(userNumberList, computerNumberList);
-        showCount(compareResult);
-        isMatchShowFinish(compareResult);
+        boolean playAgain = true;
+        while (playAgain) {
+            Computer computer = new Computer();
+            computerNumberList = computer.generateComputerNumber();
+            showInputUserNumber();
+            userNumberList = getUsetNumberList();
+            int[] compareResult = new CompareNumber().getCompareResult(userNumberList, computerNumberList);
+            showCount(compareResult);
+            isMatchShowFinish(compareResult);
+            playAgain = retry();
+        }
     }
+
 
     private static void showStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
