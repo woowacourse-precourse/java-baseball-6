967da0e864c665b52cc504be0f8bd00e9aa87875
hanslog <111857426+hanslog@users.noreply.github.com>
2023-10-24 17:11:21 +0900

    Update Application.java

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
-package baseball;
+package main.java.baseball;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+
+	public static void main(String[] args) {
+		Game game=new Game();
+		game.startGame();
+
+	}
+
 }
