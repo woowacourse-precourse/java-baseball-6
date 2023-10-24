146ca7b44972e0d32ef94f560bf1d68f0cc5bdca
wschoe <chldntjr0425@naver.com>
2023-10-25 03:54:38 +0900

    fix: change the place of strike and ball in format printing

--- src/main/java/baseball/broadcaster/Broadcaster.java
+++ src/main/java/baseball/broadcaster/Broadcaster.java
@@ -7,7 +7,7 @@ public class Broadcaster {
         Integer strike = count.get(0);
         Integer ball = count.get(1);
         if((strike > 0) && (ball > 0)){
-            System.out.printf("%d스트라이크 %d볼%n",strike,ball);
+            System.out.printf("%d볼 %d스트라이크%n",ball,strike);
         } else if((strike == 0) && (ball > 0)){
             System.out.printf("%d볼%n",ball);
         } else if((strike > 0) && (ball == 0)){
