72bbdbc90d1122bbf72f9923170a8c8730d004eb
HongChaa <jun761_1@naver.com>
2023-10-24 18:19:14 +0900

    refactor(GameView): 출력문 문자열 전부 상수로 초기화

--- src/main/java/baseball/view/GameView.java
+++ src/main/java/baseball/view/GameView.java
@@ -4,21 +4,26 @@ import baseball.model.PlayerAnswer;
 
 public class GameView {
 
+    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String GAME_SUCCESS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String GAME_OPTION_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+
     public void printStartGameMessage() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println(GAME_START_MESSAGE);
     }
 
     public void printSuccessGameMessage() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료 ");
+        System.out.println(GAME_SUCCESS_MESSAGE);
         printOptionMessage();
     }
 
     private void printOptionMessage() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println(GAME_OPTION_MESSAGE);
     }
 
     public void printInputNumberMessage() {
-        System.out.print("숫자를 입력해주세요 : ");
+        System.out.print(INPUT_NUMBER_MESSAGE);
     }
 
     public void printAnswerHintMessage(String hint) {
