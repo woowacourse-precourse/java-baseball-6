e784f28e8c40a9a7657ea742c870b77332d5458d
wooni97 <jhw970714@gmail.com>
2023-10-25 03:23:16 +0900

    feat(GameController): implement logic for number comparison
    - 이 커밋에서는 플레이어와 컴퓨터의 숫자 비교 결과에 따라 게임을 종료 or 숫자 다시 입력 로직을 구현했습니다.

---
+++ src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,43 @@
+package baseball.controller;
+
+import baseball.entity.Computer;
+import baseball.entity.GameLogic;
+import baseball.view.GameIO;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GameController {
+    private final GameIO gameIO;
+    private final Computer computer;
+    private final GameLogic gameLogic;
+
+    public GameController(GameIO gameIO, Computer computer, GameLogic gameLogic) {
+        this.gameIO = gameIO;
+        this.computer = computer;
+        this.gameLogic = gameLogic;
+    }
+
+    public void runGame() {
+        List<Integer> computerNumbers = new ArrayList<>();
+        computerNumbers = computer.createRandomNumber();
+
+        Map compareResult = new HashMap<>();
+        do {
+            String playerGuess = gameIO.readPlayerGuess();
+
+            List<Integer> playerGuessNumbers = new ArrayList<>();
+            playerGuessNumbers = gameLogic.validateAndReturnBaseBallNumber(playerGuess);
+
+            compareResult = gameLogic.compareNumbers(playerGuessNumbers, computerNumbers);
+            gameIO.printBallStrike(compareResult);
+
+        } while (!isPlayerGameComplete(compareResult));
+    }
+
+    public boolean isPlayerGameComplete(Map<String, Integer> compareResult){
+        return compareResult.get("strike") == 3;
+    }
+}
