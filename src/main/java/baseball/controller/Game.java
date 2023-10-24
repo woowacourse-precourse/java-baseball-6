039c66f093f08a945a753201f0b3a8bf8fd453d7
PARK9898 <wjddjs21@naver.com>
2023-10-25 02:16:25 +0900

    "fix: 랜덤 숫자 재생성 오류 해결"

--- src/main/java/baseball/controller/Game.java
+++ src/main/java/baseball/controller/Game.java
@@ -6,32 +6,36 @@ import baseball.view.InputView;
 import baseball.view.OutputView;
 
 public class Game {
-	private static final int THREE_STRIKE = 3;
-	private final InputView inputView;
-	private final OutputView outputView;
 
-	public Game(InputView inputView, OutputView outputView) {
-		this.inputView = inputView;
-		this.outputView = outputView;
-	}
+    private static final int THREE_STRIKE = 3;
 
-	public void playOnce() {
-		outputView.printGameStartMessage();
-		getGameResult();
-		outputView.printFinishMessage();
-	}
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final Numbers computerNumbers;
 
-	public void getGameResult() {
-		do {
-			Numbers playerNumbers = new Numbers(inputView.createPlayerNumber());
-			Numbers computerNumbers = new Numbers();
-			Referee referee = new Referee(computerNumbers);
-			int strike = referee.countStrike(playerNumbers);
-			int ball = referee.countBall(playerNumbers);
-			outputView.printResult(strike, ball);
-			if (strike == THREE_STRIKE) {
-				break;
-			}
-		} while (true);
-	}
+    public Game(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        computerNumbers = new Numbers();
+    }
+
+    public void playOnce() {
+        outputView.printGameStartMessage();
+        getGameResult();
+        outputView.printFinishMessage();
+    }
+
+    public void getGameResult() {
+        do {
+            Numbers playerNumbers = new Numbers(inputView.createPlayerNumber());
+            Referee referee = new Referee(computerNumbers);
+
+            int strike = referee.countStrike(playerNumbers);
+            int ball = referee.countBall(playerNumbers);
+            outputView.printResult(strike, ball);
+            if (strike == THREE_STRIKE) {
+                break;
+            }
+        } while (true);
+    }
 }
