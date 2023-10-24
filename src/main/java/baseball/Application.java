121c09c7e9fc7a62f5f67e45700329df1a61189f
jongheonleee <qwefghnm1212@gmail.com>
2023-10-24 18:21:48 +0900

    [fix] 줄바꿈 문자 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import baseball.controller.GameController;
+import baseball.domain.ComputerBalls;
+import baseball.service.GameService;
+
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         // TODO: 프로그램 구현
+        GameController gameController = new GameController();
+        gameController.play();
     }
 }
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -13,8 +13,8 @@ public class OutputView {
     public static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
 
     public static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
-    public static final String END_GAME_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n";
-    public static final String RESTART_GAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
+    public static final String END_GAME_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String RESTART_GAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     private static final StringBuilder stringbuilder = new StringBuilder();
 
