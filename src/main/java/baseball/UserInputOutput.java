3293323b5299500460f6f65124277256e467166a
SEOWONLEEE <9952sun@jj.ac.kr>
2023-10-24 14:14:00 +0900

    fix: 중복 메서드 제거

--- src/main/java/baseball/UserInputOutput.java
+++ src/main/java/baseball/UserInputOutput.java
@@ -6,61 +6,57 @@ public class UserInputOutput {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static int getUserNumber() {
-        int number = 0;
-        boolean validInput = false;
-        while (!validInput) {
+        int number;
+        do {
             System.out.print("숫자를 입력해주세요: ");
-            String input = scanner.nextLine();
-            try {
-                number = Integer.parseInt(input);
-                if (input.length() != 3 || !areDigitsUnique(number)) {
-                    throw new IllegalArgumentException("1부터 9까지 서로 다른 3자리 숫자를 입력해주세요.");
-                }
-                validInput = true;
-            } catch (NumberFormatException e) {
-                System.out.println("숫자가 아닌 다른 것이 입력되었습니다.");
-                System.exit(0);
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-                System.exit(0);
-            }
-        }
+            number = readUserInput();
+        } while (!isValidUserNumber(number));
         return number;
     }
 
-    public static void displayMessage(String message) {
-        System.out.println(message);
+    public static String getRestartOrQuitInput() {
+        String input;
+        do {
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            input = scanner.nextLine();
+        } while (!isValidRestartOrQuitInput(input));
+        return input;
     }
 
-    public static String getRestartOrQuitInput() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        String input = "";
-        boolean validInput = false;
-        while (!validInput) {
-            input = scanner.nextLine();
-            if (input.equals("1") || input.equals("2")) {
-                validInput = true;
-            } else {
-                System.out.println("1 또는 2를 입력해야 합니다.");
-            }
+    private static int readUserInput() {
+        try {
+            return Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println("숫자가 아닌 다른 것이 입력되었습니다.");
+            return -1;
         }
-        return input;
+    }
+
+    private static boolean isValidUserNumber(int number) {
+        if (number < 100 || number > 999 || !areDigitsUnique(number)) {
+            System.out.println("1부터 9까지 서로 다른 3자리 숫자를 입력해주세요.");
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean isValidRestartOrQuitInput(String input) {
+        if (!input.equals("1") && !input.equals("2")) {
+            System.out.println("1 또는 2를 입력해야 합니다.");
+            return false;
+        }
+        return true;
     }
 
     private static boolean areDigitsUnique(int number) {
         int[] digits = new int[10];
-        int count = 0;
         while (number > 0) {
             int digit = number % 10;
             if (digits[digit] > 0) {
-                throw new IllegalArgumentException("2개 이상의 중복되는 숫자가 입력되었습니다.");
+                return false;
             }
             digits[digit]++;
-            count++;
             number /= 10;
-        }
-        if (count != 3) {
-            throw new IllegalArgumentException("1부터 9까지 서로 다른 3자리 숫자를 입력해주세요.");
         }
         return true;
     }
