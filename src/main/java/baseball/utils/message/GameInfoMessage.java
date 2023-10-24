58b5370e8e9cd0e95485081d92a99aadcb39e52c
ma-an-jong <alswhd1113@gmail.com>
2023-10-24 10:54:15 +0900

    feat(Message) : 입출력, 게임 정보 메시지 데이터 추가
    입출력과 게임 진행 중 사용할 안내 문구를 Message 클래스에 추가하였다.

---
+++ src/main/java/baseball/utils/message/GameInfoMessage.java
@@ -0,0 +1,7 @@
+package baseball.utils.message;
+
+public class GameInfoMessage {
+    public static final String START_GAME = "숫자 야구 게임을 시작합니다.\n";
+    public static final String CORRECT_NUMBER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n";
+    public static final String END_GAME = "숫자 야구 게임을 종료합니다.";
+}
---
+++ src/main/java/baseball/utils/message/IOMessage.java
@@ -0,0 +1,8 @@
+package baseball.utils.message;
+
+public class IOMessage {
+
+
+    public static final String INPUT_NUMBER = "숫자를 입력해주세요 : ";
+    public static final String CONTINUE_INPUT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
+}
