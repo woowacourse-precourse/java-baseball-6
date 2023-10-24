cd85dd503c16f4b8976b64a334390979e9652eb0
lesw <lesw1216@gmail.com>
2023-10-25 08:59:29 +0900

    style: add comments

--- src/main/java/baseball/computer/Computer.java
+++ src/main/java/baseball/computer/Computer.java
@@ -8,11 +8,12 @@ import java.util.List;
 public class Computer {
     private static final List<Integer> randomList = new ArrayList<>();
 
-    // 난수 생성
+    /* 랜덤 숫자가 저장된 리스트 초기화 */
     public void init() {
         randomList.clear();
     }
 
+    /* 리스트에 랜덤 숫자 3개를 넣고 반환 */
     public List<Integer> getNewRandomList() {
         while (randomList.size() < 3) {
             isContain(Randoms.pickNumberInRange(1, 9));
@@ -21,6 +22,7 @@ public class Computer {
         return randomList;
     }
 
+    /* 리스트에 중복된 숫자가 있는지 체크 */
     private static void isContain(int randomNumber) {
         if (!randomList.contains(randomNumber)) {
             randomList.add(randomNumber);
