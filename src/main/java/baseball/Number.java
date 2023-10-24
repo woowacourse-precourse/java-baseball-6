206bb7a2b228de8abd2f362f967685a376828a07
heemin2728 <idkhm0728@gmail.com>
2023-10-24 13:01:38 +0900

    feat: 입력 예외처리 기능

--- src/main/java/baseball/Number.java
+++ src/main/java/baseball/Number.java
@@ -3,9 +3,11 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.NavigableMap;
+
 public class Number {
 
-    private final int NUM_COUNT=3;
+    private static final int NUM_COUNT=3;
     private final int NUM_START=1;
     private final int NUM_END=9;
     int[] randomNumber;
@@ -21,8 +23,47 @@ public class Number {
         return array;
     }
 
+    public static boolean checkLength(String input){
+        return input.length() == NUM_COUNT;
+    }
+
+    public static boolean checkDigit(String input){
+        for(int i=0;i<NUM_COUNT;i++) {
+            if(!Character.isDigit(input.charAt(i)) || input.charAt(i)=='0'){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkUnique(String input){
+        if(input.charAt(0)==input.charAt(1)){
+            return false;
+        }
+        if(input.charAt(1)==input.charAt(2)){
+            return false;
+        }
+        if(input.charAt(2)==input.charAt(0)){
+            return false;
+        }
+        return true;
+    }
+
+    public static void isValid(String input){
+        if(!checkLength(input)){
+            throw new IllegalArgumentException();
+        }
+        if(!checkDigit(input)){
+            throw new IllegalArgumentException();
+        }
+        if(!checkUnique(input)){
+            throw new IllegalArgumentException();
+        }
+    }
+
     public static int[] getUserInput(){
         String string=Console.readLine();
+        isValid(string);
         return stringToIntArray(string);
     }
 
