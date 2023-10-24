762964a4d3fbfd71eb87282c64fb1930038c1a3e
leesh <qwqw8019@naver.com>
2023-10-24 18:07:21 +0900

    faet: 테스트 상수 추가, 변수명 변경
    - 재시작 후 게임을 위한 문자열 상수 추가
    - 변수명 변경
      - SUCCESS_LIST이하 상수를 FULL_LIST로 변경

--- src/main/java/baseball/util/TestConstants.java
+++ src/main/java/baseball/util/TestConstants.java
@@ -1,20 +1,28 @@
 package baseball.util;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TestConstants {
 
     // Valid constants
     public static final String NULL_STRING = null;
     public static final String EMPTY_STRING = "";
-    public static final String SUCCESS_STRING = "123";
-    public static final List<Integer> SUCCESS_LIST = List.of(1, 2, 3);
-    public static final int SUCCESS_STRING_SIZE = SUCCESS_LIST.size();
-    public static final String SUCCESS_LIST_TO_STRING = SUCCESS_LIST.toString();
-    public static final int SUCCESS_LIST_FIRST_ELEMENT = SUCCESS_LIST.get(0);
-    public static final Integer[] SUCCESS_LIST_REMAINING_ELEMENTS =
-            SUCCESS_LIST.subList(1, SUCCESS_STRING_SIZE)
-                    .toArray(new Integer[SUCCESS_STRING_SIZE - 1]);
+    public static final String FIRST_SUCCESS_STRING = "123";
+    public static final String EXPECTED_TWO_BALL_ONE_STRIKE = "654";
+    public static final String SECOND_SUCCESS_STRING = "456";
+    public static final List<Integer> FIRST_LIST = List.of(1, 2, 3);
+    public static final List<Integer> SECOND_LIST = List.of(4, 5, 6);
+    public static final List<Integer> FULL_LIST = Stream.concat(FIRST_LIST.stream(), SECOND_LIST.stream())
+            .collect(Collectors.toList());
+
+    public static final String FIRST_LIST_TO_STRING = FIRST_LIST.toString();
+    public static final int FULL_LIST_SIZE = FULL_LIST.size();
+    public static final int FULL_LIST_FIRST_ELEMENT = FULL_LIST.get(0);
+    public static final Integer[] FULL_LIST_REMAINING_ELEMENTS =
+            FULL_LIST.subList(1, FULL_LIST_SIZE)
+                    .toArray(new Integer[FULL_LIST_SIZE - 1]);
     public static final String RESTART = "1";
     public static final String TERMINATE = "2";
 
