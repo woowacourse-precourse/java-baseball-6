dd0429b28745efc905d6e288bfe71915eb4b7899
Youth <boysoeng@naver.com>
2023-10-25 01:50:01 +0900

    feat: player getter와 setter 생성

--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -1,5 +1,6 @@
 package baseball.domain;
 
+import baseball.utils.Input;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,5 +9,22 @@ public class Player {
     private List<Integer> playerInput = new ArrayList<>();
     private String restartMessage;
 
+    public List<Integer> getPlayerInput() {
+        setPlayerInput();
+        return playerInput;
+    }
+
+    private void setPlayerInput() {
+        playerInput = Input.askPlayerInput();
+    }
+
+    public String getRestartMessage() {
+        setRestartMessage();
+        return restartMessage;
+    }
+
+    private void setRestartMessage() {
+        restartMessage = Input.askRestart();
+    }
 
 }
