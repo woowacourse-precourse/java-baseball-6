22c3b1daeec79d2611d4c61f2ac9a81853dc78df
Jedo0224 <qkrwotjr0@gmail.com>
2023-10-25 00:17:38 +0900

    [feat] util 패키지 추가 및 Result 클래스 추가

---
+++ src/main/java/baseball/util/Result.java
@@ -0,0 +1,11 @@
+package baseball.util;
+
+public class Result {
+    public int strike;
+    public int ball;
+
+    public Result(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+}
