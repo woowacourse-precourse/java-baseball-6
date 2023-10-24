a4d8ea33dc9db30ec285ee091c1202024eff1224
masonkimseoul <masonkimseoul@gmail.com>
2023-10-25 00:15:12 +0900

    test : (InputViewTest.java) Add checking input if too long and throw IllegalArgumentsException

--- src/test/java/baseball/view/InputViewTest.java
+++ src/test/java/baseball/view/InputViewTest.java
@@ -38,4 +38,13 @@ public class InputViewTest {
         assertThat(e.getClass()).isEqualTo(IllegalArgumentException.class);
         assertThat(e.getMessage()).isEqualTo("3자리의 수를 입력해주세요.");
     }
+
+    @Test
+    void 입력_길이_초과_예외_테스트() {
+        userInput("1234");
+
+        Exception e = assertThrows(IllegalAccessException.class, InputView::getUserNumbers);
+        assertThat(e.getClass()).isEqualTo(IllegalArgumentException.class);
+        assertThat(e.getMessage()).isEqualTo("3자리의 수를 입력해주세요.");
+    }
 }
