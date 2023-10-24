90bb8ca48311909d137a3874858b9c1639d122ed
Woo-Shinyoung <tlsdud8205@naver.com>
2023-10-24 22:36:10 +0900

    Feat[controller] 게임 시작 문구 출력

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -5,19 +5,22 @@ import baseball.domain.BaseballResult;
 import baseball.service.BaseballService;
 import baseball.util.BaseballNumberConverter;
 import baseball.view.InputView;
+import baseball.view.OutputView;
 
 public class BaseballController {
     private final BaseballService baseballService;
     private final InputView inputView;
+    private final OutputView outputView;
 
     public BaseballController() {
         this.baseballService = new BaseballService();
         this.inputView = new InputView();
+        this.outputView = new OutputView();
     }
 
     public void playGame() {
         BaseballNumber computerNumber = baseballService.generateRandomNumbers();
-
+        outputView.printGameStart();
         while (true) {
             BaseballNumber playerNumber = getPlayerInput();
             BaseballResult result = baseballService.calculateResult(computerNumber, playerNumber);
