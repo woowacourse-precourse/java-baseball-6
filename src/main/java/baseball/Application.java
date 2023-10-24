7f16086f28e7728bd1d590a6669113409d58e070
jihun-24k <gi020477@gmail.com>
2023-10-25 02:16:43 +0900

    Feat: 볼, 스트라이크 개수 세는 메서드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -16,7 +16,26 @@ public class Application {
             System.out.print("숫자를 입력해주세요 : ");
             input = Console.readLine();
             List<Integer> inputNumbers = Util.toIntegerArray(input);
+            String comment = takeTurn(computer, inputNumbers);
+
             break outer;
         }
     }
+
+    private static String takeTurn(List<Integer> computer, List<Integer> inputNumbers) {
+        String comment = "";
+        comment += countBall(computer, inputNumbers);
+        comment += countStrike(computer, inputNumbers);
+
+        return comment;
+    }
+
+    private static String countStrike(List<Integer> computer, List<Integer> inputNumbers) {
+        return null;
+    }
+
+    private static String countBall(List<Integer> computer, List<Integer> inputNumbers) {
+        return null;
+    }
+
 }
