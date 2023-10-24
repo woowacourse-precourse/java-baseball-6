596b91c013d5da51aacb48099628c41860ce2b4f
2021110806 <2021110806@dgu.ac.kr>
2023-10-24 09:10:13 +0900

    feat(Computer.java): strike와 ball의 갯수를 세는 기능 구현

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -16,4 +16,33 @@ public class Computer {
         }
         return (answer);
     }
+
+
+    int JudgeHowManyStrike(ArrayList<Integer> answer, ArrayList<Integer> userInput) {
+        int strike;
+
+        strike = 0;
+        if (answer.get(0).equals(userInput.get(0)))
+            strike++;
+        if (answer.get(1).equals(userInput.get(1)))
+            strike++;
+        if (answer.get(2).equals(userInput.get(2)))
+            strike++;
+        return (strike);
+    }
+
+    int JudgeHowManyBall(ArrayList<Integer> answer, ArrayList<Integer> userInput, int strike) {
+        int ball;
+
+        ball = 0;
+        if (answer.contains(userInput.get(0)))
+            ball++;
+        if (answer.contains(userInput.get(1)))
+            ball++;
+        if (answer.contains(userInput.get(2)))
+            ball++;
+        ball = ball - strike;
+        return (ball);
+    }
 }
+
