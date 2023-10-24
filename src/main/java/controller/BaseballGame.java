1752bfa21626231dce5ccb670aa33d361ad750b8
dsa04156 <dsa04156@naver.com>
2023-10-24 16:46:57 +0900

    fix : 출력 문구 수정

--- src/main/java/controller/BaseballGame.java
+++ src/main/java/controller/BaseballGame.java
@@ -29,7 +29,10 @@ public class BaseballGame {
             run();
         } else {
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
+=======
+>>>>>>> eea316e (fix : 출력 문구 수정)
             return;
 >>>>>>> a99376c (feat : 출력 문구 수정)
         }
