33a14351462ef00dbfa4f3f393c4fffe146e702d
SungWoo <sungwoo166@gmail.com>
2023-10-24 17:08:57 +0900

    refactor: Change Assertions imports in tests

--- src/test/java/baseball/model/GameResultTest.java
+++ src/test/java/baseball/model/GameResultTest.java
@@ -1,6 +1,6 @@
 package baseball.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import baseball.BaseballScoreMessage;
 import org.junit.jupiter.api.DisplayName;
--- src/test/java/baseball/service/BaseBallServiceTest.java
+++ src/test/java/baseball/service/BaseBallServiceTest.java
@@ -1,6 +1,7 @@
 package baseball.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import baseball.model.GameResult;
 import java.util.Arrays;
