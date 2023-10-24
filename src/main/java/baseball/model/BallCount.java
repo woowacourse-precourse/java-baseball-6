71165e90e8a808fb5ba0dea9836199644c4e333a
beomsic <qjatjr29@khu.ac.kr>
2023-10-25 03:49:19 +0900

    :recycle: refactor : record class로 변경

--- src/main/java/baseball/model/BallCount.java
+++ src/main/java/baseball/model/BallCount.java
@@ -1,20 +1,5 @@
 package baseball.model;
 
-public class BallCount {
+public record BallCount(int strike, int ball) {
 
-    private final int strike;
-    private final int ball;
-
-    public BallCount(final int strike, final int ball) {
-        this.strike = strike;
-        this.ball = ball;
-    }
-
-    public int getStrike() {
-        return strike;
-    }
-
-    public int getBall() {
-        return ball;
-    }
 }
