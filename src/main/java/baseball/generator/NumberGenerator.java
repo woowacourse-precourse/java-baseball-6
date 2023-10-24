39adff25721f8bb3aea3aecaa5d307c7257db2d6
yhcho0405 <yhcho0405@kakao.com>
2023-10-25 02:34:08 +0900

    refactor: generator 상수 constants로 분리

--- src/main/java/baseball/generator/NumberGenerator.java
+++ src/main/java/baseball/generator/NumberGenerator.java
@@ -1,5 +1,6 @@
 package baseball.generator;
 
+import baseball.constants.GameConstants;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -9,8 +10,8 @@ public class NumberGenerator {
 
     public String generateRandomBaseballNumber() {
         List<Integer> numbers = new ArrayList<>();
-        while (numbers.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+        while (numbers.size() < GameConstants.NUMBER_LENGTH) {
+            int randomNumber = Randoms.pickNumberInRange(GameConstants.START_NUMBER, GameConstants.END_NUMBER);
             if (!numbers.contains(randomNumber)) {
                 numbers.add(randomNumber);
             }
