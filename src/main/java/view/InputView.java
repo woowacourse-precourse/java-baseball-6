8eb27ca8efb1b8e51ae85657c19c5cc13da568d0
yangyanghyunjung <hyunjungyang0922@gmail.com>
2023-10-24 23:52:50 +0900

    🎯feat(view / InputView): 숫자 입력 메세지 출력

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,14 @@
+package view;
+
+public class InputView {
+    private static final String GAME_START_MESSAGE ="숫자 야구 게임을 시작합니다.";
+    private static final String INPUT_NUMBER= "숫자를 입력해주세요: ";
+
+    public void StartGameMessage() {
+        System.out.println(GAME_START_MESSAGE); // 프로그램 시작
+    }
+    public void InputMessage() {
+        System.out.println(INPUT_NUMBER); // 숫자입력 msg
+    }
+
+}
