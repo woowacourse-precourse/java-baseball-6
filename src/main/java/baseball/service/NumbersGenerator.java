b754cdc2247f58cc02b3f69009fd62cb6edf51df
김현재 <khjmr@naver.com>
2023-10-24 20:15:57 +0900

    feat(makeNumbers): 랜덤 방어 숫자 생성 로직 추가

---
+++ src/main/java/baseball/service/NumbersGenerator.java
@@ -0,0 +1,19 @@
+package baseball.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumbersGenerator {
+    public List<Integer> makeNumbers() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+}
