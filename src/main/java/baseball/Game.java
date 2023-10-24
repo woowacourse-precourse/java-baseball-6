e4f355b416ca0a159235e0b999241690ba667e7f
elizabethgim <yujingim@naver.com>
2023-10-24 22:38:12 +0900

    feat: 랜덤 숫자 생성

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,13 +1,30 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Game {
+
+    public List<Integer> makeRandomNumber(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
 
     public void start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
         System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
-        System.out.println(input);
+
+        List<Integer> number = makeRandomNumber();
+
     }
 }
