5ff1bdf16acc704d85e301ca80462948fe830af6
wooyongjeong <wooyong.dev@gmail.com>
2023-10-25 01:26:47 +0900

    feat(Input): 입력받은 수 검증, List로 변환 추가

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -8,6 +8,14 @@ public class Input {
 
     public List<Integer> readPlayerNumber() {
         String input = Console.readLine();
+        Validator.validatePlayerNumber(input);
         return convertToList(input);
     }
+
+    private List<Integer> convertToList(String input) {
+        return Arrays.stream(input.split(""))
+                .map(Integer::parseInt)
+                .toList();
+    }
+
 }
