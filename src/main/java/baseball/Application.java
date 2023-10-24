059d47e2fbcd35a49541914628ebc66b259048a7
Choo <choo000407@naver.com>
2023-10-25 06:01:39 +0900

    feat(Controller): 메인 실행 로직 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import baseball.controller.BaseballGameController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseballGameController gameController = new BaseballGameController();
+        while (!gameController.isEnd()) {
+            gameController.start();
+            gameController.play();
+            gameController.end();
+        }
     }
 }
--- src/main/java/baseball/controller/BaseBallGameController.java
+++ src/main/java/baseball/controller/BaseBallGameController.java
@@ -1,4 +1,50 @@
 package baseball.controller;
 
-public class BaseBallGameController {
+import baseball.enums.GameStatus;
+import baseball.model.Computer;
+import baseball.model.Game;
+import baseball.model.Round;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+import java.util.Objects;
+
+public class BaseballGameController {
+    private final Game game;
+    private final Computer computer;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public BaseballGameController() {
+        this.game = new Game();
+        this.computer = new Computer();
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public void start() {
+        computer.generateSecretNumber();
+        outputView.printGameStart();
+    }
+
+    public void play() {
+        String computerNumber = computer.getSecretNumber();
+        Round round;
+        do {
+            String playerNumber = inputView.getSuggestedNumber();
+            round = Round.fromPlayerAndSecretNumbers(playerNumber, computerNumber);
+            String result = round.generateResultMessage();
+            outputView.printRoundResult(result);
+        } while (!round.isCorrectGuess());
+    }
+
+    public void end() {
+        outputView.printGameEnd();
+        if (Objects.equals(inputView.getGameContinueChoice(), "2")) {
+            game.endGame();
+        }
+    }
+
+    public boolean isEnd() {
+        return game.getStatus() == GameStatus.END;
+    }
 }
