bbeaf38c311752f4622ba58722e6abaf08edb350
JongMinCh0i <wonderland314592@gmail.com>
2023-10-24 21:58:54 +0900

    test(AnswerRandomList): withinRange, 레코드 객체를 통한 단위 테스트 코드 작성

--- src/test/java/baseball/AnswerRandomListTest.java
+++ src/test/java/baseball/AnswerRandomListTest.java
@@ -2,6 +2,7 @@ package baseball;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import baseball.config.withinRange;
 import baseball.service.GenerationQuestionList;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,7 +11,7 @@ import org.junit.jupiter.api.Test;
 
 class AnswerRandomListTest {
 
-    @DisplayName("generateThreeRandomNumber()를 실행시 길이가 3인 List 를 반환한다.")
+    @DisplayName("generateRandomNumberList()를 실행시 길이가 3인 List 를 반환한다.")
     @Test
     void threeSizeOfList() {
 
@@ -18,13 +19,13 @@ class AnswerRandomListTest {
         GenerationQuestionList computer = new GenerationQuestionList(new ArrayList<>());
 
         //when
-        List<Integer> answerRandomList = computer.generateThreeRandomNumber();
+        List<Integer> answerRandomList = computer.generateRandomNumberList(new withinRange(1, 9));
 
         //then
         assertThat(answerRandomList.size()).isEqualTo(3);
     }
 
-    @DisplayName("generateThreeRandomNumber()를 실행시 중복값이 없는 List 를 반환한다.")
+    @DisplayName("generateRandomNumberList()를 실행시 중복값이 없는 List 를 반환한다.")
     @Test
     void notSameElementOfList() {
 
@@ -32,27 +33,26 @@ class AnswerRandomListTest {
         GenerationQuestionList computer = new GenerationQuestionList(new ArrayList<>());
 
         //when
-        List<Integer> answerRandomList = computer.generateThreeRandomNumber();
+        List<Integer> answerRandomList = computer.generateRandomNumberList(new withinRange(1, 9));
         long count = answerRandomList.stream().distinct().count();
 
         //then
         assertThat(count).isEqualTo(3);
     }
 
-    @DisplayName("generateThreeRandomNumber()를 실행시 각 값들은 1에서 9사이의 원소를 갖는 List 를 반환한다.")
+    @DisplayName("generateRandomNumberList()를 실행시 각 값들은 1에서 9사이의 원소를 갖는 List 를 반환한다.")
     @Test
-    void elementsValue1To9OfList() {
+    void elementsValue_1_To_9_OfList() {
 
         //given
         GenerationQuestionList computer = new GenerationQuestionList(new ArrayList<>());
-
+        withinRange withinRange = new withinRange(1, 9);
         //when
-        List<Integer> answerRandomList = computer.generateThreeRandomNumber();
+        List<Integer> answerRandomList = computer.generateRandomNumberList(withinRange);
 
         //then
         for (Integer number : answerRandomList) {
-            assertThat(number >= 1 && number <= 9).isTrue();
+            assertThat(number >= withinRange.startInclusive() && number <= withinRange.endInclusive()).isTrue();
         }
     }
-
 }
