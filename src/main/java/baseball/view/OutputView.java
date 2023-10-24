3dae2592434b25d649abcc52749bf177a3abad2b
minseo  jo <liging12@naver.com>
2023-10-24 15:49:33 +0900

    refactor: 입력을 위한 출력 (InputView), 출력을 위한 출력(OutputView)를 나눔

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,28 +3,18 @@ package baseball.view;
 public class OutputView {
 
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.\n";
-    private static final String INPUT_BALLS_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n";
-    private static final String INPUT_GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
     private static final String LINE = "\n";
 
-    public void gameStartMessage() {
+    public void printStartGameMessage() {
         displayMessage(GAME_START_MESSAGE);
     }
 
-    public void inputBallsMessage() {
-        displayMessage(INPUT_BALLS_MESSAGE);
-    }
-
-    public void gameEndMessage() {
+    public void printEndGameMessage() {
         displayMessage(GAME_END_MESSAGE);
     }
 
-    public void inputGameRestartMessage() {
-        displayMessage(INPUT_GAME_RESTART_MESSAGE);
-    }
-
-    public void resultMessage(String message) {
+    public void printResultMessage(String message) {
         displayMessage(message + LINE);
     }
 
