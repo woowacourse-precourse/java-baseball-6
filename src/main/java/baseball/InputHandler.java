2fb3faf5d7f1966096e1d053601929c2117a9b24
swhyeon98 <smreo75@gmail.com>
2023-10-25 00:23:04 +0900

    feat: 숫자를 입력 받는 'getPlayInput' 기능 추가

--- src/main/java/baseball/InputHandler.java
+++ src/main/java/baseball/InputHandler.java
@@ -1,4 +1,12 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputHandler {
+
+    public String getPlayInput() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+        return input;
+    }
 }
