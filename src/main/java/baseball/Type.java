fd75b2799d226a6d92d60c810d6798434b36dbf9
D0ri123 <lsh81550@gmail.com>
2023-10-24 17:14:11 +0900

    refactor: Type enum 클래스 필드, static 메서드 추가 - 4차 구현
    Map의 key value로 저장할 수 있도록 String타입 필드를 추가했습니다.
    다른 객체에서 어떤 타입이 있는지 알 필요 없이,
    Enum 클래스에서 리스트를 제공하는 static 메서드를 생성했습니다.

--- src/main/java/baseball/Type.java
+++ src/main/java/baseball/Type.java
@@ -1,5 +1,26 @@
 package baseball;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 public enum Type {
-  NONE, BALL, STRIKE
+  BALL("볼"),
+  STRIKE("스트라이크");
+
+  private final String type;
+
+  Type(String type) {
+    this.type = type;
+  }
+
+  public String getType() {
+    return this.type;
+  }
+
+  public static List<String> getAllKinds() {
+    return Stream.of(Type.values())
+        .map(Type::getType)
+        .toList();
+  }
+
 }
