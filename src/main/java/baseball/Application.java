bc7db12351a9a16fa53c7c016559439dbf7a41c0
happyryan <tjdgks120813@gmail.com>
2023-10-24 14:03:13 +0900

    rfeat: add askGameCountinueOrStop

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,17 +3,21 @@ import static baseball.Constants.*;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+    public  static  boolean askGameContinueOrStop(){
+        System.out.println(continueGameMessage);
+        String isContinueGame = Console.readLine(); // TODO: error handle
+        if (isContinueGame.equals(gameContinue)) {
+            System.out.println(endGameMessage);
+            return true;
+        }
+        return false;
+    }
     public static void main(String[] args){
         System.out.println(startGameMessage);
         while (true) {
             Game baseBallGame = new Game();
             baseBallGame.play();
-            System.out.println(continueGameMessage);
-            String isContinueGame = Console.readLine();
-            if (isContinueGame.equals(gameContinue)) { // TODO: error handle
-                System.out.println(endGameMessage);
-                break;
-            }
+            if(askGameContinueOrStop()) break;
         }
     }
 }
