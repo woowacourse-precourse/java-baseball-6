84d1781a70263f7aca7d4a904f274f0dc068ec07
HyunSoo730 <whgustn730@gmail.com>
2023-10-25 00:22:00 +0900

    Feat : else 사용 안하고 else if로 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,8 +11,8 @@ public class Application {
     public static void main(String[] args) {
         int flag = 0;
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
         while (true) {
-            System.out.println("숫자 야구 게임을 시작합니다.");
             List<Integer> computer = new ArrayList<>();
             while (computer.size() < 3) {
                 int randomNumber = Randoms.pickNumberInRange(1, 9);
