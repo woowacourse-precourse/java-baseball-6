cf5967ae618a6e728b62681353bb90305d0069de
eunji <dmswl6310@gmail.com>
2023-10-24 20:38:18 +0900

    feat: 게임 종료 시 문구 출력, 재시작

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -66,7 +66,7 @@ public class Baseball {
 
     public void playBaseball(){
         System.out.println("숫자 야구 게임을 시작합니다.");
-        System.out.println(computerNum);
+//        System.out.println(computerNum);
         do {
 
             System.out.print("숫자를 입력해주세요 : ");
@@ -75,8 +75,18 @@ public class Baseball {
             int[] result=countResult(input);
             printResult(result);
 
-            if(result[0]==3) break;
+            if(result[0]==3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            }
         }while(true);
+
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input=Console.readLine();
+        if(input.equals("1")){
+            playBaseball();
+        } // 종료
+        // 에러처리
 
     }
 }
