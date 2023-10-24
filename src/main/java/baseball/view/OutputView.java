a0025e4e3b8521235263078bbf40ab2d0b91b2e1
masonkimseoul <masonkimseoul@gmail.com>
2023-10-24 21:14:02 +0900

    fix : (OutputView.java) Add space for output format

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -7,7 +7,7 @@ public class OutputView {
     private static final String END_GAME_MSG = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String GET_RESTART_MSG = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static final String NOTHING = "낫싱";
-    private static final String BALL = "볼";
+    private static final String BALL = "볼 ";
     private static final String STRIKE = "스트라이크";
 
     public static void printStartGameMsg() {
