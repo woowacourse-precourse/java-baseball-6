5bd7e1e6c57cc626b61b0da9af185069cfde65e6
peeaaccee <wlgp8958@naver.com>
2023-10-25 07:30:04 +0900

    feat(BallGenerator): 랜덤값 생성 기능

--- src/main/java/baseball/model/BallGenerator.java
+++ src/main/java/baseball/model/BallGenerator.java
@@ -1,9 +1,23 @@
 package baseball.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class BallGenerator {
     private static final int COMPUTER_MAX_SIZE = 3;
     public static final int START_INCLUSIVE = 1;
     public static final int END_INCLUSIVE = 9;
     private BallGenerator() {
+    }
+
+    private static class BallHolder {
+        private static final BallGenerator INSTANCE = new BallGenerator();
+    }
+
+    public static BallGenerator getInstance() {
+        return BallHolder.INSTANCE;
+    }
+
+    public String createRandomBall() {
+        return String.valueOf(Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE));
     }
 }
