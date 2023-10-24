7c2da22b6192286ec653ea05b9855ee4b2eb92c7
Subin <rla0010101@gmail.com>
2023-10-24 16:46:59 +0900

    Feat: 난수 생성 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    private static String randomNumber;
+
+    public static void main(String[] args) throws IllegalArgumentException {
+        //TODO: 숫자 야구 게임 구현
+
     }
+
+    public static String makeRandomNumber() {
+        String randomNumber= "";
+        while (randomNumber.length() < 3) {
+            String n = String.valueOf(Randoms.pickNumberInRange(1,10));
+            if (!randomNumber.contains(n))
+                randomNumber += n;
+        }
+        return randomNumber;
+    }
+
+
+
 }
