aa0665522a254f2c8e324cebcafeb5e725a6d2cf
sangchu <didi1484@gmail.com>
2023-10-24 21:59:08 +0900

    refactor: 플레이어 선택 넘버 메서드 리팩토링(inputNumber)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,48 +2,21 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
-    public static List<Integer> pickComputerNumber(){
-        List<Integer> computer = new ArrayList<>();
-        while(computer.size() < 3){
-            int computerNumber = Randoms.pickNumberInRange(1,9);
-            if(!computer.contains(computerNumber)){
-                computer.add(computerNumber);
-            }
-        }
-        return computer;
-    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구게임을 시작합니다.");
 
         // 1. 컴퓨터
-        List<Integer> computer = pickComputerNumber();
+        List<Integer> computer = pickNumber();
 
         // 2. 플레이어
-        while(true) {
-            List<Integer> player = new ArrayList<>();
-            System.out.print("숫자를 입력해주세요 : ");
-            String playerNumber = Console.readLine();
-            if (playerNumber.length() != 3 || Integer.parseInt(playerNumber) < 0) {
-                throw new IllegalArgumentException("잘못된 입력입니다.");
-            }
-            for (int i = 0; i < 3; i++) {
-                int eachNumber = playerNumber.charAt(i) - '0';
-                if (eachNumber == 0) {
-                    throw new IllegalArgumentException("0을 포함할 수 없음");
-                }
-                if (!player.contains(eachNumber)) {
-                    player.add(eachNumber);
-                }
-            }
-            if (player.size() != 3) {
-                throw new IllegalArgumentException("잘못된 입력입니다.");
-            }
+        while (true) {
+            List<Integer> player = inputNumber();
+
             // 3. 점수판별
             int ball = 0;
             int strike = 0;
@@ -72,16 +45,49 @@ public class Application {
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 int menu = Integer.parseInt(Console.readLine());
                 if (menu == 1) {
-                    computer = pickComputerNumber();
+                    computer = pickNumber();
                     continue;
                 }
                 if (menu == 2) {
                     break;
                 }
-                if (menu != 1 || menu != 2){
+                if (menu != 1 || menu != 2) {
                     throw new IllegalArgumentException("잘못된 입력입니다.");
                 }
             }
         }
+    }
+
+    public static List<Integer> pickNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int computerNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(computerNumber)) {
+                computer.add(computerNumber);
+            }
+        }
+        return computer;
+    }
+
+    public static List<Integer> inputNumber() {
+        List<Integer> player = new ArrayList<>();
+        System.out.print("숫자를 입력해주세요 : ");
+        String playerNumber = Console.readLine();
+        if (playerNumber.length() != 3 || Integer.parseInt(playerNumber) < 0) {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+        for (int i = 0; i < 3; i++) {
+            int eachNumber = playerNumber.charAt(i) - '0';
+            if (eachNumber == 0) {
+                throw new IllegalArgumentException("0을 포함할 수 없음");
+            }
+            if (!player.contains(eachNumber)) {
+                player.add(eachNumber);
+            }
+        }
+        if (player.size() != 3) {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+        return player;
     }
 }
