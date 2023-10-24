c45f92e7696c62acd9c420f8d527dc4655c88bd0
jaepyo99 <jaepyo99@naver.com>
2023-10-24 19:46:47 +0900

    [Feat] 사용자 입력 숫자 검증 메서드 추가

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -3,21 +3,38 @@ package baseball;
 
 import java.util.List;
 
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+
 public class Validator {
 
     public Validator() {
     }
 
     // 컴퓨터를 통한 랜덤 야구번호를 검증해주는 메서드
-    public Boolean validNumNotInList(String invalidNum, List<String>validNumList){
+    public Boolean validNumNotInList(String invalidNum, List<String> validNumList) {
         return validNumList.contains(invalidNum);
     }
 
     // 사용자가 입력한 숫자가 3자리 숫자인지 확인해주는 메서드
-    public Boolean validNumsLength(String invalidNums) throws IllegalArgumentException {
-        if(invalidNums.length()!=3){
-            throw new IllegalArgumentException("입력된 숫자가 3자리 숫자가 아닙니다.");
+    public Boolean validNums(String invalidNums) throws IllegalArgumentException {
+        if (validLength(invalidNums) && validNonZero(invalidNums)) {
+            return TRUE;
         }
-        return Boolean.TRUE;
+        throw new  IllegalArgumentException();
+    }
+
+    public Boolean validLength(String invalidNums) {
+        if (invalidNums.length() != 3) {
+            return FALSE;
+        }
+        return TRUE;
+    }
+
+    public Boolean validNonZero(String invalidNums) {
+        if (invalidNums.contains("0")) {
+            return FALSE;
+        }
+        return TRUE;
     }
 }
