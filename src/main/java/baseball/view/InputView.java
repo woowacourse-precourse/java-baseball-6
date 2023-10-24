4a23d1254c8e25598689bd1552a2c04906df41a8
CJY <c0512jy@naver.com>
2023-10-25 01:00:38 +0900

    fix: 줄바꿈 추가
    print()를 println()으로 바꿨습니다.
    그리고 기존에 start()함수는 OutputView의 역할인 것 같아 수정했습니다.

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -4,17 +4,13 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public void start() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
-    }
-
     public String input() {
         System.out.print("숫자를 입력해주세요 : ");
         return Console.readLine();
     }
 
     public String restart() {
-        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         return Console.readLine();
     }
 
