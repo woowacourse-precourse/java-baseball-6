f6614292113a081a54e75a44efc1c7106406bcf0
chan-won <clw8679@naver.com>
2023-10-24 10:02:25 +0900

    Fix: 입력값 검증 로직 수정

--- src/main/java/baseball/validation/InputValidation.java
+++ src/main/java/baseball/validation/InputValidation.java
@@ -3,16 +3,17 @@ package baseball.validation;
 public class InputValidation {
 
     public static void number(String input) {
-        if (input.length() != 3) {
-            throw new IllegalArgumentException("3자리의 숫자를 입력해주세요.");
+        if (!input.matches("^[1-9][1-9]{2}$")) {
+            throw new IllegalArgumentException("세 자리 숫자이고, 각 자리수가 1에서 9까지인 숫자를 입력해주세요.\"");
         }
-        if (!input.matches("^[1-9]*$")) {
-            throw new IllegalArgumentException("숫자만 입력해주세요.");
+
+        if (input.charAt(0) == input.charAt(1) || input.charAt(1) == input.charAt(2) || input.charAt(2) == input.charAt(0)) {
+            throw new IllegalArgumentException("각 자리수가 중복되지 않는 숫자를 입력해주세요.");
         }
     }
 
     public static void gameEndCommand(String input) {
-        if (!input.matches("^[1-2]*$")) {
+        if (!input.matches("^[1-2]$")) {
             throw new IllegalArgumentException("1 또는 2를 입력해주세요.");
         }
     }
