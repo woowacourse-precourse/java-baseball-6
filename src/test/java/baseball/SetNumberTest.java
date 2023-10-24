26766732cb43f120bff9e61c0727d71ac234e7d9
{psm4171} <{kkjjhh4171@naver.com}>
2023-10-25 02:31:55 +0900

    Add SetNumber TestCode: isValidNumber & isNotDuplicate feature

--- src/test/java/baseball/SetNumberTest.java
+++ src/test/java/baseball/SetNumberTest.java
@@ -28,13 +28,48 @@ public class SetNumberTest {
     @Test
     void boolean_입력값이_숫자가_아닌_경우()
             throws InvocationTargetException, NoSuchMethodException,  IllegalAccessException {
-        Method method = Number.class.getDeclaredMethod("isValidNumber", String.class);
+        Method method = SetNumber.class.getDeclaredMethod("isNumber", String.class);
         method.setAccessible(true);
 
         String input = "a23";
         boolean expected = false;
 
         boolean result = (boolean) method.invoke(number, input);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void boolean_중복된_숫자를_입력받은_경우()
+            throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method = SetNumber.class.getDeclaredMethod("isNotDuplicate", String.class);
+        method.setAccessible(true);
+
+        String input = "455";
+        boolean expected = false;
+
+        boolean result = (boolean) method.invoke(number, input);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void boolean_유효하지_않는_숫자를_입력받은_경우_오류_발생()
+            throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method = SetNumber.class.getDeclaredMethod("isValidNumber", String.class);
+        method.setAccessible(true);
+
+        String notNumber = "12ㄱ";
+        String notThreeDigitsNumber = "12345";
+        String duplicateNumber = "122";
+
+        boolean expected = false;
+
+        boolean result = (boolean) method.invoke(number, notNumber);
+        assertThat(result).isEqualTo(expected);
+
+        result = (boolean) method.invoke(number, notThreeDigitsNumber);
+        assertThat(result).isEqualTo(expected);
+
+        result = (boolean) method.invoke(number, duplicateNumber);
         assertThat(result).isEqualTo(expected);
     }
 
