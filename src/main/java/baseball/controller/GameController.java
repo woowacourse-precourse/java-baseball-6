e4f88ccb4207da627aa98221be4f53a865a5e5c1
kimmokalover <kimmokalover@gmail.com>
2023-10-24 17:26:25 +0900

    feat : GameController class 구현 완료

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -7,7 +7,81 @@ import baseball.service.GameService;
 import baseball.service.RandomGeneratorService;
 import baseball.service.UserInputService;
 import baseball.view.InputView;
+import baseball.view.OutputView;
 
 public class GameController {
+    private final UserInputService userInputService;
+    private final GameService gameService;
+    private final RandomGeneratorService randomGeneratorService;
+    static RandomNumber randNum;
+    public GameController(UserInputService userInputService, GameService gameService,
+            RandomGeneratorService randomGeneratorService) {
+        this.userInputService = userInputService;
+        this.gameService = gameService;
+        this.randomGeneratorService = randomGeneratorService;
+    }
+    public GameState startBaseBallGame(GameState state){
+        UserInput userInput = promptForUserInput(state);
+        RandomNumber randomNumber = createNewRandomNumber(randNum, state);
+        GameSystem gameSystem = playBaseBallGame(userInput, randomNumber);
+
+        promptForStrike(gameSystem);
+        promptForThreeStrike(gameSystem);
+        promptForBall(gameSystem);
+        promptForBallAndStrike(gameSystem);
+        promptForNothing(gameSystem);
+
+        if (gameSystem.getStrike() == 3) {
+            return promptForContinue();
+        }
+        return GameState.CONTINUE;
+    }
+
+    private GameState promptForContinue(){
+        InputView.continueGameMessage();
+        UserInput userInput = userInputService.getUserInput();
+
+        if (userInput.getUserInput().equals("1"))
+            return GameState.RESTART;
+
+        OutputView.gameEnd();
+        return GameState.END;
+
+    }
+    private UserInput promptForUserInput(GameState gameState){
+        if (gameState == GameState.START)
+            InputView.startGameMessage();
+        InputView.requestInputMessage();
+        return userInputService.getUserInput();
+    }
+    private void promptForStrike(GameSystem gameSystem){
+        if (gameSystem.getStrike() != 0 && gameSystem.getBallCount() == 0)
+            OutputView.onlyStrike(gameSystem.getStrike());
+    }
+    private void promptForThreeStrike(GameSystem gameSystem){
+        if(gameSystem.getStrike() == 3)
+            OutputView.gameEnd();
+    }
+    private void promptForBall(GameSystem gameSystem){
+        if (gameSystem.getStrike() == 0 && gameSystem.getBallCount() != 0)
+            OutputView.onlyBall(gameSystem.getBallCount());
+    }
+    private void promptForBallAndStrike(GameSystem gameSystem){
+        if (gameSystem.getStrike() !=0 && gameSystem.getBallCount() != 0)
+            OutputView.ballAndStrike(gameSystem.getBallCount(), gameSystem.getStrike());
+    }
+    private void promptForNothing(GameSystem gameSystem){
+        if (gameSystem.getStrike() ==0 && gameSystem.getBallCount() == 0){
+            OutputView.nothing();
+        }
+    }
+    private RandomNumber createNewRandomNumber(RandomNumber randomNumber, GameState gameState){
+        if (gameState != GameState.CONTINUE)
+            return randomGeneratorService.createRandomNumber();
+        return randomNumber;
+    }
+    private GameSystem playBaseBallGame(UserInput userInput, RandomNumber randomNumber){
+        return gameService.playGame(userInput, randomNumber);
+    }
 
 }
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -12,4 +12,10 @@ public class OutputView {
     public static void onlyStrike(int strike){
         System.out.println(strike + "스트라이크");
     }
+    public static void nothing(){
+        System.out.println("낫싱");
+    }
+    public static void gameEnd(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
 }
