e7728a625576a56d593264293700ab80506d65ef
wooni97 <jhw970714@gmail.com>
2023-10-25 02:16:53 +0900

    feat(GameIO): add baseball game result print method and its corresponding test

--- src/main/java/baseball/view/GameIO.java
+++ src/main/java/baseball/view/GameIO.java
@@ -1,9 +1,32 @@
 package baseball.view;
 
+import java.util.Map;
 import camp.nextstep.edu.missionutils.Console;
 public class GameIO {
+
+    private String THREE_STRIKE = "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private String NOTHING = "낫싱";
+
     public String readPlayerGuess() {
         return Console.readLine();
     }
 
+    public void printBallStrike(Map<String, Integer> compareResult) {
+        int strike = compareResult.get("strike");
+        int balls = compareResult.get("balls");
+
+        if (strike == 3) {
+            System.out.println(THREE_STRIKE);
+            return;
+        }
+
+        if (strike == 0 && balls == 0) {
+            System.out.println(NOTHING);
+            return;
+        }
+
+        System.out.println(balls + "볼" + " " + strike + "스트라이크");
+
+
+    }
 }
