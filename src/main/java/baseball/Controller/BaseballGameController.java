ff70ae6e8edf9c7b5103569f184177e73f7a9d50
jiixon <jr0107380@gmail.com>
2023-10-25 04:20:22 +0900

    feat: 게임 재시작/종료 기능

--- src/main/java/baseball/Controller/BaseballGameController.java
+++ src/main/java/baseball/Controller/BaseballGameController.java
@@ -9,6 +9,12 @@ public class BaseballGameController {
     private final static BaseballGameView view = new BaseballGameView();
     private static final String SINGLE_GAME_END = "3스트라이크";
 
+    public void play(){
+        do{
+            playSingleGame();
+        }while (choiceStartNewGame());
+    }
+
     private void playSingleGame(){
         view.gameStart();
         model.createRandomNumbers();
@@ -19,5 +25,10 @@ public class BaseballGameController {
             view.print(gameResult);
         } while (!gameResult.equals(SINGLE_GAME_END));
         view.endPrint();
+    }
+
+    private boolean choiceStartNewGame(){
+        String endUserInput = view.endUserInput();
+        return endUserInput.equals("1");
     }
 }
