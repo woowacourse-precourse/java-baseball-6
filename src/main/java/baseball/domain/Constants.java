6c738840363cc4679c891d87bdb5df6d0f97f4a5
seokmyungham <hellotjraud@gmail.com>
2023-10-25 00:45:29 +0900

    feat(Constants): 볼, 스트라이크, 낫싱 상수 추가

--- src/main/java/baseball/domain/Constants.java
+++ src/main/java/baseball/domain/Constants.java
@@ -6,5 +6,9 @@ public class Constants {
     public static final String END_GAME_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     public static final String RESTART_GAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
+    public static final String BALL = "볼";
+    public static final String STRIKE = "스트라이크";
+    public static final String NOTHING = "낫싱";
+
     public static final int NUMBER_OF_BASEBALL = 3;
 }
