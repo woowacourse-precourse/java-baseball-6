c9f21bffcef479a1b9e9b5ea92b59971b99a6684
sunwoong <sunwoong1122@gmail.com>
2023-10-24 21:08:23 +0900

    refactor: View 클래스 Result model 클래스 의존하도록 변경

--- src/main/java/baseball/view/View.java
+++ src/main/java/baseball/view/View.java
@@ -1,5 +1,7 @@
 package baseball.view;
 
+import baseball.model.Result;
+
 public class View {
     public void showStartLine() {
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -14,8 +16,8 @@ public class View {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
-    public void showResultByPartialMismatchAnswer(int ball, int strike) {
-        System.out.println(ball + "볼 " + strike + "스트라이크");
+    public void showResultByPartialMismatchAnswer(Result result) {
+        System.out.println(result.getBallCount() + "볼 " + result.getStrikeCount() + "스트라이크");
     }
 
     public void showResultByAllMismatchAnswer() {
