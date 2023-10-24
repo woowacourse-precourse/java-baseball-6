8080251dac265e8a8feafbd047a8faf773e8ffa0
BackJiwan <bn1357nb@gmail.com>
2023-10-24 14:11:42 +0900

    feat: 게임시작 문구 출력
    게임 객체를 분리, 실행 메소드 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    static class Game{
+        public void play(){
+            System.out.println("숫자 야구 게임을 시작합니다.");
+        }
     }
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.play();
+    }
+
+
 }
