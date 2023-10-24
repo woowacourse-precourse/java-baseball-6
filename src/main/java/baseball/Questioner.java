165e8e1d881b1730788b48e9056bc2182525ebcd
uwoobeat <desktop3711@gmail.com>
2023-10-25 03:53:17 +0900

    feat: 타깃 넘버 생성 기능 구현

--- src/main/java/baseball/Questioner.java
+++ src/main/java/baseball/Questioner.java
@@ -1,4 +1,28 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Questioner {
+
+    private final GameNumber targetNumber;
+
+    public Questioner() {
+        this.targetNumber = generateTargetNumber();
+    }
+
+    private static GameNumber generateTargetNumber() {
+
+        while (true) {
+            int randInt = Randoms.pickNumberInRange(100, 999);
+            String currentTargetNumber = String.valueOf(randInt);
+
+            try {
+                return GameNumber.from(currentTargetNumber);
+            } catch (IllegalArgumentException e) {
+                continue;
+            }
+        }
+    }
+
+
 }
