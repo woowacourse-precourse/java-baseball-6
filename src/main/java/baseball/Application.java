b459e9decea4ef6d934550ce24971ddf7ca9313a
gywns0417 <gywns0417@gmail.com>
2023-10-24 21:29:11 +0900

    refactor: 공백 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,6 +6,5 @@ public class Application {
     public static void main(String[] args) {
         final BaseballGameController baseballGameController = new BaseballGameController();
         baseballGameController.run();
-
     }
 }
