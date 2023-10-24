f2d5f829bf6e6b24c6ce1ac8abe275034a770fc4
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-10-25 03:07:32 +0900

    feat(BaseballPrinter) : add printScore func to print result of user's guess

--- src/main/java/baseball/numGame/baseballGame/BaseballPrinter.java
+++ src/main/java/baseball/numGame/baseballGame/BaseballPrinter.java
@@ -16,4 +16,29 @@ public class BaseballPrinter implements Printer {
         System.out.print("숫자를 입력해주세요 : ");
     }
 
+    public void printScore(BaseballForm result) {
+        Boolean isAnyStrike = !(result.getStrikeCnt().equals(0));
+        Boolean isAnyBall = !(result.getBallCnt().equals(0));
+        Boolean isCorrect = result.getCorrect();
+        Boolean isNothing = result.getNothing();
+
+        if (isNothing) {
+            System.out.print("낫싱");
+        }
+        if (isAnyBall) {
+            System.out.print(result.getBallCnt() + "볼");
+        }
+        if (isAnyStrike) {
+            if (isAnyBall) {
+                System.out.print(" ");
+            }
+            System.out.print(result.getStrikeCnt() + "스트라이크");
+        }
+        if (isCorrect) {
+            System.out.println();
+            System.out.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
+        System.out.println();
+    }
+
 }
