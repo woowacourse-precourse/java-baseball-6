1d3899f270400a6000de6f64314372b2503ffa90
chae401 <chae401@sju.ac.kr>
2023-10-24 15:09:51 +0900

    human 인스턴스 외부에서 주입

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,47 +1,43 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class BaseballGame {
     private boolean running;
     private final Computer computer;
-    private final Human human;
 
-    public BaseballGame(){
-        running = true;
-        computer = new Computer();
-        human = new Human();
+    public BaseballGame() {
+        this.running = true;
+        this.computer = new Computer();
     }
 
     // 게임 시작
-    public void start(){
-
+    public void start(Human user) {
         System.out.println("숫자 야구 게임을 시작합니다.");
         computer.chooseRandomValues();
-        while(running){
-            run();
+        while (running) {
+            run(user);
         }
 
         // 게임 종료 or 재시작 선택
-        isRestart();
+        isRestart(user);
     }
 
     // 게임 실행 중
-    public void run(){
-        human.chooseValues();
-        int strike = computer.compare(human.getHuman());
-        if (strike == 3){
+    private void run(Human user) {
+        System.out.print("숫자를 입력해주세요. : ");
+        user.chooseValues();
+        ResultDTO result = computer.compare(user.getValues());
+        if (result.getStrike() == 3) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             running = false;
         }
     }
 
     // 게임 재시작
-    public void isRestart(){
+    private void isRestart(Human user) {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        running = human.isRestart();
-        if(running){
-            start();
+        running = user.isRestart();
+        if (running) {
+            start(user);
         }
     }
 }
