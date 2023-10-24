91d3491c022e2f1fae3ddf6f353e4610984e9753
leeseobin00 <leeseobin000709@gmail.com>
2023-10-25 06:29:51 +0900

    feat(GameController): 사용자로부터 입력값 파싱

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,4 +1,20 @@
 package baseball.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameController {
+    private List<Integer> parseUserInput(String userInput) {
+        List<Integer> userNumbers = parseInputNumbers(userInput);
+        return userNumbers;
+    }
+
+    private List<Integer> parseInputNumbers(String userInput) {
+        List<Integer> userNumbers = new ArrayList<>();
+        for (char digitChar : userInput.toCharArray()) {
+            int digit = Character.getNumericValue(digitChar);
+            userNumbers.add(digit);
+        }
+        return userNumbers;
+    }
 }
