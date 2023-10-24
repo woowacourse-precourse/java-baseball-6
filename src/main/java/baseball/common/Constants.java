c3f263930e59448acde26bc713b237ae80fc8fa6
kimdoha <kkhhjj3393@gmail.com>
2023-10-24 17:23:20 +0900

    feat: 기능 구현에 필요로 한 상수 정리

--- src/main/java/baseball/common/Constants.java
+++ src/main/java/baseball/common/Constants.java
@@ -2,6 +2,9 @@ package baseball.common;
 
 public class Constants {
     public static int numDigit = 3;
-    public static int startInclusive = 1;
-    public static int endInclusive = 9;
+    public static int startNum = 1;
+    public static int lastNum = 9;
+    public static int strikeTarget = 3;
+    public static int gameRestart = 1;
+    public static int gameExit = 2;
 }
