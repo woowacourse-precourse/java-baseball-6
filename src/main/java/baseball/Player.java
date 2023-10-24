9c451a06f4dd221acbc98ef1213320508109470a
BDlhj <akk808@naver.com>
2023-10-24 16:30:29 +0900

    rename: Change convertStringToList() to convertStringToCharList()

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
-    public List<Character> convertStringToList(String playerInput) {
+    public List<Character> convertStringToCharList(String playerInput) {
         List<Character> playerInputCharList = new ArrayList<>();
 
         for (char ch : playerInput.toCharArray()) {
