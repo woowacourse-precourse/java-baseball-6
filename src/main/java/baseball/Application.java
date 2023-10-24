a71f90b6c4144431f30745408c36d6936b36573f
happyryan <tjdgks120813@gmail.com>
2023-10-24 15:54:56 +0900

    refactor: delete Space

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,4 @@
 package baseball;
-import static baseball.Constants.*;
-import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args){
--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -3,7 +3,6 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 public class User {
-
     public String play(){
         return Console.readLine();
     }
