e5c6d52c740183d5e98018820e5d07cd4945d082
wnsgh12s <junho1273@gmail.com>
2023-10-24 15:28:01 +0900

    Feat: NumberBaseballScoreEvaluator 사용자가 입력한 숫자로 부터 스트라이크,볼,낫싱 판단 기능

---
+++ src/main/java/baseball/domain/service/NumberBaseballScoreEvaluator.java
@@ -0,0 +1,63 @@
+package baseball.domain.service;
+
+public class NumberBaseballScoreEvaluator {
+
+    private final String targetNumber;
+    private final String inputNumber;
+
+    public NumberBaseballScoreEvaluator(String targetNumber, String inputNumber) {
+        this.targetNumber = targetNumber;
+        this.inputNumber = inputNumber;
+    }
+
+    private int countStrikes() {
+        int strikes = 0;
+        for (int i = 0; i < targetNumber.length(); i++) {
+            if (targetNumber.charAt(i) == inputNumber.charAt(i)) {
+                strikes++;
+            }
+        }
+        return strikes;
+    }
+
+    private int countBalls() {
+        int balls = 0;
+        for (int i = 0; i < inputNumber.length(); i++) {
+            char currentChar = inputNumber.charAt(i);
+            if (targetNumber.indexOf(currentChar) != -1 && targetNumber.charAt(i) != currentChar) {
+                balls++;
+            }
+        }
+        return balls;
+    }
+
+    public String evaluate() {
+        int strikes = countStrikes();
+        int balls = countBalls();
+        return formatResult(strikes, balls);
+    }
+
+    private String formatResult(int strikes, int balls) {
+        StringBuilder result = new StringBuilder();
+
+        if (strikes == 0 && balls == 0) {
+            return "낫싱";
+        }
+
+
+        if (strikes > 0) {
+            result.append(strikes).append("스트라이크");
+        }
+
+        if (strikes > 0 && balls > 0) {
+            result.append(" ");
+        }
+
+        if (balls > 0) {
+            result.append(balls).append("볼");
+        }
+
+        return result.toString();
+    }
+
+}
