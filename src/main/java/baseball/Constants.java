1d4503dbf2685b8a54457ecd44c9045e38f458d2
eunjungL <dldmswjd322@gmail.com>
2023-10-24 19:04:04 +0900

    refactor: 숫자 길이 및 게임 멘트 Constants로 분리

---
+++ src/main/java/baseball/Constants.java
@@ -0,0 +1,19 @@
+package baseball;
+
+public class Constants {
+    /** 숫자 야구에 사용되는 숫자 목록 */
+    static final Integer GAME_NUM_LENGTH = 3;
+    static final String CONTINUE_INPUT = "1";
+
+    /** 숫자 야구에 사용되는 멘트 */
+    static final String START_COMMENT = "숫자 야구 게임을 시작합니다.";
+    static final String RECEIVE_USER_INPUT_COMMENT = "숫자를 입력해주세요 : ";
+    static final String END_COMMENT = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n";
+    static final String ASK_CONTINUE_COMMENT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    /** 사용자 입력 힌트 멘트 */
+    static final String NOT_MATCH_COMMENT= "낫싱";
+    static final String BALL_COMMENT = "%d볼";
+    static final String STRIKE_COMMENT = "%d스트라이크";
+    static final String BALL_AND_STRIKE_COMMENT = BALL_COMMENT + " " + STRIKE_COMMENT;
+}
--- src/main/java/baseball/GameUtil.java
+++ src/main/java/baseball/GameUtil.java
@@ -11,7 +11,7 @@ public class GameUtil {
     String userInput;
 
     public GameUtil() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println(Constants.START_COMMENT);
     }
 
     public void settingGame() {
@@ -20,32 +20,32 @@ public class GameUtil {
 
     private String makeAnswer() {
         ArrayList<Integer> numberList = new ArrayList<>();
-        while (numberList.size() < 3){
+        while (numberList.size() < Constants.GAME_NUM_LENGTH){
             int randomValue = Randoms.pickNumberInRange(1, 9);
             if (!numberList.contains(randomValue)) numberList.add(randomValue);
         }
 
         StringBuilder answer = new StringBuilder();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < Constants.GAME_NUM_LENGTH; i++) {
             answer.append(numberList.get(i));
         }
         return answer.toString();
     }
 
     public void receiveUserInput() {
-        System.out.print("숫자를 입력해주세요 : ");
+        System.out.print(Constants.RECEIVE_USER_INPUT_COMMENT);
         this.userInput = Console.readLine();
 
         validateInput(this.userInput);
     }
 
     private void validateInput(String userInput) {
-        if (userInput.length() != 3) {
+        if (userInput.length() != Constants.GAME_NUM_LENGTH) {
             throw new IllegalArgumentException();
         }
         else {
             HashMap<Character, Integer> countChar = new HashMap<>();
-            for (int i = 0; i < 3; i++) {
+            for (int i = 0; i < Constants.GAME_NUM_LENGTH; i++) {
                 if (countChar.containsKey(userInput.charAt(i)) || userInput.charAt(i) == '0') {
                     throw new IllegalArgumentException();
                 } else {
@@ -59,24 +59,24 @@ public class GameUtil {
         Integer ballCount = findBall(this.userInput, this.answer);
         Integer strikeCount = findStrike(this.userInput, this.answer);
 
-        String comment = "낫싱";
+        String comment = Constants.NOT_MATCH_COMMENT;
 
         if (ballCount == 0 && strikeCount > 0) {
-            comment = String.format("%d스트라이크", strikeCount);
+            comment = String.format(Constants.STRIKE_COMMENT, strikeCount);
         } else if (ballCount > 0 && strikeCount == 0) {
-            comment = String.format("%d볼", ballCount);
+            comment = String.format(Constants.BALL_COMMENT, ballCount);
         } else if (ballCount > 0 && strikeCount > 0){
-            comment = String.format("%d볼 %d스트라이크", ballCount, strikeCount);
+            comment = String.format(Constants.BALL_AND_STRIKE_COMMENT, ballCount, strikeCount);
         }
 
         System.out.println(comment);
-        return strikeCount == 3;
+        return strikeCount.equals(Constants.GAME_NUM_LENGTH);
     }
 
     private Integer findBall(String userInput, String answer) {
         int ballCount = 0;
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < Constants.GAME_NUM_LENGTH; i++) {
             if (userInput.charAt(i) != answer.charAt(i) && answer.indexOf(userInput.charAt(i)) != -1) ballCount += 1;
         }
 
@@ -86,7 +86,7 @@ public class GameUtil {
     private Integer findStrike(String userInput, String answer) {
         int strikeCount = 0;
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < Constants.GAME_NUM_LENGTH; i++) {
             if (userInput.charAt(i) == answer.charAt(i)) {
                 strikeCount += 1;
             }
@@ -96,10 +96,10 @@ public class GameUtil {
     }
 
     public Boolean checkIsContinue() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n");
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println(Constants.END_COMMENT);
+        System.out.println(Constants.ASK_CONTINUE_COMMENT);
 
         String userInput = Console.readLine();
-        return (userInput.equals("1"));
+        return (userInput.equals(Constants.CONTINUE_INPUT));
     }
 }
