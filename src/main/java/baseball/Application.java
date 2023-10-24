dec341f8b357af3e09d9c59a4fe066e1c89a5bf5
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-10-25 00:46:50 +0900

    feat : 게임 정답 생성 및 게임 시작 문구 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String play = "1";
+
+        while (play.equals("1")) {
+            List<Integer> computer = createComputerNumber();
+            System.out.println("숫자 야구 게임을 시작합니다.");
+
+            play = "2";
+        }
+    }
+    private static List<Integer> createComputerNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 }
+
