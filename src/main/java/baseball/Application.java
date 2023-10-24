b2f46e9f406181f736997f4c5c6d546dd3f2a531
JGM <cnxw4570123@gmail.com>
2023-10-24 23:38:42 +0900

    Fix : Fix bug with prepareAnswer and Change method name printScore to getScoreMessage prepareAnswer에서 내부적으로 사용됐던 Randoms.pickUniqueNumbersInRange를 Randoms.pickNumberInRange메소드로 교체함. 또한, printScore이 더이상 힌트를 출력만 하지않고 힌트를 문자열로 변환해 반환하도록 수정하였고 메소드명도 printScore에서 getScoreMessage로 수정함.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,21 +3,31 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Application {
 
-    static List<Integer> computer;
+    static List<Integer> answers;
     static List<Integer> userGuess;
 
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        computer = prepareAnswer();
-        userGuess = getNumbers();
-        printScore(computer, userGuess);
+        boolean moreGame = true;
+
+        while (moreGame) {
+            answers = prepareAnswer();
+            String s = "";
+
+            while (!s.equals("3스트라이크")) {
+                userGuess = getNumbers();
+                s = getScoreMessage(answers, userGuess);
+                System.out.println(s);
+            }
+
+            moreGame = isGameOver();
+        }
+        System.out.println("게임 종료");
 
     }
 
@@ -51,12 +61,19 @@ public class Application {
     }
 
     /**
-     * 정답을 준비하는 메소드
+     * 3개의 중복없는 무작위 수를 가진 리스트를 생성하는 메소드
      *
      * @return 중복이 없는 3개의 숫자를 가진 리스트
      */
     protected static List<Integer> prepareAnswer() {
-        return Randoms.pickUniqueNumbersInRange(1, 9, 3);
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() != 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 
 
@@ -88,18 +105,24 @@ public class Application {
      *
      * @param computer  유저가 입력한 수들과 비교할 정답 리스트
      * @param userGuess 유저가 입력한 수들의 리스트
+     * @return 힌트를 나타내는 문자열을 반환함
      */
-    protected static void printScore(final List<Integer> computer, final List<Integer> userGuess) {
+    protected static String getScoreMessage(final List<Integer> computer, final List<Integer> userGuess) {
         int[] score = compareWithAnswer(computer, userGuess);
         StringBuilder sb = new StringBuilder();
-        if (score[0] > 0) sb.append(score[0]).append("볼");
-        if (!sb.isEmpty()) sb.append(" ");
-        if (score[1] > 0) sb.append(score[1]).append("스트라이크");
+        if (score[0] > 0) sb.append(score[0]).append("볼 ");
+        if (score[1] > 0) sb.append(score[1]).append("스트라이크 ");
         if (sb.isEmpty()) sb.append("낫싱");
-
-        System.out.println(sb.toString());
+        return sb.toString().trim();
     }
 
+    /**
+     * 사용자에게 입력을 받아 1 또는 2를 받은 경우 각각 true(시작), false(종료)를 반환하고
+     * 입력값이 잘못된 경우 IllegalArgumentException을 반환하는 메소드
+     *
+     * @return boolean
+     * @throws IllegalArgumentException
+     */
     protected static boolean isGameOver() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String userChoiceString = Console.readLine();
@@ -108,7 +131,7 @@ public class Application {
             throw new IllegalArgumentException("your choice should exactly be one digit.");
         }
 
-        if(userChoiceString.charAt(0) > 2 || userChoiceString.charAt(0) < 1){
+        if (userChoiceString.charAt(0) > '2' || userChoiceString.charAt(0) < '1') {
             throw new IllegalArgumentException("your choice should be in range 1 to 2.");
         }
 
