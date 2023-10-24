5680dd3e66cb5388347bcdeaf926f3509d87a62f
IE-MangChi <hsm983278@gmail.com>
2023-10-24 16:51:22 +0900

    refactor: 필요없는 import 삭제

--- src/test/java/baseball/service/BaseBallGameServiceTest.java
+++ src/test/java/baseball/service/BaseBallGameServiceTest.java
@@ -2,11 +2,8 @@ package baseball.service;
 
 import baseball.model.Game;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class BaseBallGameServiceTest {
 
