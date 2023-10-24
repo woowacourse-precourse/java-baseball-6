4ac6b766affc4ce951fff44bbf2904e9af549df7
sgo722 <sgo722@naver.com>
2023-10-25 02:31:28 +0900

    feat : 사용자가 컴퓨터 숫자를 맞출 떄까지 게임반복되는 기능 추가

--- src/main/java/baseball/GameService.java
+++ src/main/java/baseball/GameService.java
@@ -19,10 +19,12 @@ public class GameService {
     public void executeGame(){
         System.out.println("숫자 야구 게임을 시작합니다.");
         computer.generateNumber();
-        user.inputUserNumber();
-        game.compare(computer.getComputerNumbers(), user.getUserNumber());
-        game.printCompareResult();
-
+        do {
+            user.inputUserNumber();
+            game.compare(computer.getComputerNumbers(), user.getUserNumber());
+            game.printCompareResult();
+        }while(game.getStrike() != 3);
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
 }
