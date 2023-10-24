d7f62a88ae620f44a843127a66d18f3c30b791ba
journiyoon <journiyoon@gmail.com>
2023-10-24 23:23:49 +0900

    feat: 입력 숫자 유효성 검증 추가

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -3,6 +3,8 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Player {
     public Player() {}
@@ -11,11 +13,22 @@ public class Player {
         List<Integer> playerNumber = new ArrayList<>();
         System.out.print("숫자를 입력해주세요 : ");
         String element = Console.readLine();
+        if (!validatePlayerNumber(element)) {
+            throw new IllegalArgumentException();
+        }
         System.out.println(element);
         String[] stringNumber = element.split("");
         for (String number : stringNumber) {
             playerNumber.add(Integer.parseInt(number));
         }
         return playerNumber;
+    }
+
+    // 사용자가 입력한 숫자, 유효성 검증
+    public Boolean validatePlayerNumber(String number) {
+        String threeNumberRegex = "^(?!.*(\\d).*\\1)\\d{3}$";
+        Pattern pattern = Pattern.compile(threeNumberRegex);
+        Matcher matcher = pattern.matcher(number);
+        return matcher.matches();
     }
 }
