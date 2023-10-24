d39e05fb91a0ef539412a1de5aaeb9042ca1f846
vega <vega99c@naver.com>
2023-10-25 01:31:09 +0900

    feat(makeRandomNumberList) 컴퓨터가 생성 할 3자리 난수 제공 구현 - 난수 생성 시 중복되는 수 없도록 구현 - 생성 된 3자리 수를 반환

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    public static List<Integer> makeRandomNumberList() {
+        List<Integer> computer = new ArrayList<>();
+        int randomNumber = 0;
+        while (computer.size() < 3) {
+            randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Integer> listAnswerNumber = makeRandomNumberList();
     }
 }
