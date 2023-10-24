31c92c8c3e4ad8862a3c6bf8a85051130cf44af3
mseo39 <a01063294905@gmail.com>
2023-10-25 02:10:44 +0900

    feat: add BaseballGame
    Class with game-related features

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,12 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseballGame {
+    private static List<Integer> computerNumber;
+    final static int randomRangestart=0;
+    final static int randomRangeend=9;
+    final static int listSize=3;
+}
