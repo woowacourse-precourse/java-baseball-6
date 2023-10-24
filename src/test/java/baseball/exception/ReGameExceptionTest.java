694291b9016196b5699c4821370c6774c58996c7
suihub <qkdcrt@gmail.com>
2023-10-24 14:52:14 +0900

    <refactor>: "test 코드 컨벤션에 따른 들여쓰기 정리"
    중구난방이던 test 코드 정리.
    들여쓰기 원칙 적용

--- src/test/java/baseball/exception/ReGameExceptionTest.java
+++ src/test/java/baseball/exception/ReGameExceptionTest.java
@@ -11,39 +11,39 @@ class ReGameExceptionTest {
     void 게임_재시작_미입력(){
         String request = "";
 
-        assertThatThrownBy(() ->
-                exception.validation(request)).isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() ->
-                exception.validation(request)).hasMessage("숫자 하나만 입력해주세요.");
+        assertThatThrownBy(() -> exception.validation(request))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> exception.validation(request))
+                .hasMessage("숫자 하나만 입력해주세요.");
     }
 
     @Test
     void 게임_재시작_2글자_이상_입력(){
         String request = "352";
 
-        assertThatThrownBy(() ->
-                exception.validation(request)).isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() ->
-                exception.validation(request)).hasMessage("숫자 하나만 입력해주세요.");
+        assertThatThrownBy(() -> exception.validation(request))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> exception.validation(request))
+                .hasMessage("숫자 하나만 입력해주세요.");
     }
 
     @Test
     void 게임_재시작_미지정_숫자_입력(){
         String request = "8";
 
-        assertThatThrownBy(() ->
-                exception.validation(request)).isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() ->
-                exception.validation(request)).hasMessage("숫자 1 또는 2를 입력해주세요.");
+        assertThatThrownBy(() -> exception.validation(request))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> exception.validation(request))
+                .hasMessage("숫자 1 또는 2를 입력해주세요.");
     }
 
     @Test
     void 게임_재시작_미지정_글자_입력(){
         String request = "ㄱ";
 
-        assertThatThrownBy(() ->
-                exception.validation(request)).isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() ->
-                exception.validation(request)).hasMessage("숫자 1 또는 2를 입력해주세요.");
+        assertThatThrownBy(() -> exception.validation(request))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> exception.validation(request))
+                .hasMessage("숫자 1 또는 2를 입력해주세요.");
     }
 }
