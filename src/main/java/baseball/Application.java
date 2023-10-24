c63139184d87c365f96a50bd7dee135bf2342d22
yeop0740 <ssucgoo@gmail.com>
2023-10-24 16:11:46 +0900

    feat: 임의의 숫자 3개를 선택하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
         while (true) {
             System.out.println("숫자 야구 게임을 시작합니다.");
+
+            List<Integer> numbers = new ArrayList<>();
+            int k = 3;
+            while (k > 0) {
+                int number = Randoms.pickNumberInRange(1, 9);
+                if (!numbers.contains(number)) {
+                    numbers.add(number);
+                    k--;
+                }
+            }
         }
 
     }
