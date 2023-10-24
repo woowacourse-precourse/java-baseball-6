438b626d64a7349fd7db42cc03864ab44a0c55db
morenow <jjang9877@gmail.com>
2023-10-25 03:04:18 +0900

    feat: 게임 종료, 재시작 메시지 출력 메소드 추가, else 예약어 삭제
    게임 종료, 재시작 메시지 출력 메소드 추가
    printBaseballCount 메소드의 else 예약어를 삭제하고
    return 로직으로 변경

--- src/main/java/baseball/util/MessageUtil.java
+++ src/main/java/baseball/util/MessageUtil.java
@@ -19,14 +19,22 @@ public final class MessageUtil {
     public static void printBaseballCount(BaseballCount count) {
         if (count.getBall() == 0 && count.getStrike() == 0) {
             System.out.println(Message.NOTHING_MSG);
-        } else {
-            if (count.getBall() != 0) {
-                System.out.print(count.getBall() + Message.BALL_MSG + " ");
-            }
-            if (count.getStrike() != 0) {
-                System.out.print(count.getStrike() + Message.STRIKE_MSG);
-            }
-            System.out.println();
+            return;
         }
+        if (count.getBall() != 0) {
+            System.out.print(count.getBall() + Message.BALL_MSG + " ");
+        }
+        if (count.getStrike() != 0) {
+            System.out.print(count.getStrike() + Message.STRIKE_MSG);
+        }
+        System.out.println();
+    }
+
+    public static void printFinishMsg() {
+        System.out.println(Message.GAME_FINISH_MSG);
+    }
+
+    public static void printRestartMsg() {
+        System.out.println(Message.GAME_RESTART_MSG);
     }
 }
