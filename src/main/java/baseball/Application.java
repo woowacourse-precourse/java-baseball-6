e10b182f74db7ee711ec69bad3ba35715bf2e477
shapeofmy <114326457+rudtn409@users.noreply.github.com>
2023-10-25 03:49:10 +0900

    실행 메소드에서 출력 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -70,19 +70,23 @@ class BaseballGame {
                 }
             }
 
-            if (nothing == 3) {
-                System.out.println("낫싱");
-            } else if (strike == 3) {
-                System.out.println("3스트라이크");
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
-                status = Integer.parseInt(Console.readLine());
-                if (status == 1) setComputer();
-            } else {
-                if (ball != 0) System.out.printf("%d볼 ", ball);
-                if (strike != 0) System.out.printf("%d스트라이크", strike);
-                System.out.println();
-            }
+            displayResult(strike, ball, nothing);
+        }
+    }
+
+    private void displayResult(int strike, int ball, int nothing) {
+        if (nothing == 3) {
+            System.out.println("낫싱");
+        } else if (strike == 3) {
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞혔습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+            status = Integer.parseInt(Console.readLine());
+            if (status == 1) setComputer();
+        } else {
+            if (ball != 0) System.out.printf("%d볼 ", ball);
+            if (strike != 0) System.out.printf("%d스트라이크", strike);
+            System.out.println();
         }
     }
 }
