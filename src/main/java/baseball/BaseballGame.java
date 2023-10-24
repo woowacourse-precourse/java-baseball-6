8d94d20cb889c86fa42af863cf73d29cc899004c
seminchoi <choicco89@gmail.com>
2023-10-24 22:20:31 +0900

    refactor : 코드 가독성을 위한 메서드 및 변수 네이밍 수정

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -6,20 +6,20 @@ public class BaseballGame {
     public void startBaseball() {
         System.out.println("숫자 야구 게임을 시작합니다.");
         do {
-            BaseballNumber answerNumberList = createBaseballAnswerNumberList();
+            BaseballNumber answerNumberList = createComputerBaseballNumber();
             playBaseball(answerNumberList);
-        } while (askToContinueGame());
+        } while (wantToContinueGame());
         System.out.println("게임 종료");
     }
 
-    private BaseballNumber createBaseballAnswerNumberList() {
+    private BaseballNumber createComputerBaseballNumber() {
         return BaseballNumber.createComputerNumber();
     }
 
     private void playBaseball(BaseballNumber computerNumber) {
         while (true) {
-            BaseballNumber userNumber = inputBaseballNumber();
-            BaseballCount baseballCount = checkBaseballResult(computerNumber, userNumber);
+            BaseballNumber userNumber = inputUserBaseballNumber();
+            BaseballCount baseballCount = createBaseballCount(computerNumber, userNumber);
             System.out.println(baseballCount.getResultBriefing());
             if (baseballCount.isAllStrike()) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
@@ -28,36 +28,36 @@ public class BaseballGame {
         }
     }
 
-    private BaseballNumber inputBaseballNumber() {
+    private BaseballNumber inputUserBaseballNumber() {
         System.out.print("숫자를 입력해주세요 : ");
         String inputString = Console.readLine();
         return BaseballNumber.createNumberByString(inputString);
     }
 
-    private BaseballCount checkBaseballResult(BaseballNumber computerNumber, BaseballNumber userNumber) {
+    private BaseballCount createBaseballCount(BaseballNumber computerNumber, BaseballNumber userNumber) {
         int strike = checkStrikeCount(computerNumber, userNumber);
         int ball = checkBallCount(computerNumber, userNumber);
         return new BaseballCount(strike, ball);
     }
 
-    private int checkStrikeCount(BaseballNumber computerNumber, BaseballNumber userNumber) {
+    private int checkStrikeCount(BaseballNumber computerBaseballNumber, BaseballNumber userBaseballNumber) {
         int strike = 0;
         for (int i = 0; i < 3; i++) {
-            if (computerNumber.get(i) == userNumber.get(i)) {
+            if (computerBaseballNumber.get(i) == userBaseballNumber.get(i)) {
                 strike++;
             }
         }
         return strike;
     }
 
-    private int checkBallCount(BaseballNumber computerNumber, BaseballNumber userNumber) {
+    private int checkBallCount(BaseballNumber computerBaseballNumber, BaseballNumber userBaseballNumber) {
         int ball = 0;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (i == j) {
                     continue;
                 }
-                if (computerNumber.get(i) == userNumber.get(j)) {
+                if (computerBaseballNumber.get(i) == userBaseballNumber.get(j)) {
                     ball++;
                 }
             }
@@ -65,7 +65,7 @@ public class BaseballGame {
         return ball;
     }
 
-    private boolean askToContinueGame() {
+    private boolean wantToContinueGame() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String inputString = Console.readLine();
         validateContinueGameInput(inputString);
