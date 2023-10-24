6ca56bf3978fb363d1c3d0f65be643626761db82
SeongJunChun <csj991210@naver.com>
2023-10-24 17:53:47 +0900

    feat:메인 함수 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,10 @@
 package baseball;
 
+import baseball.controller.PlayGame;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        PlayGame playGame = new PlayGame();
+        playGame.startGame();
     }
 }
