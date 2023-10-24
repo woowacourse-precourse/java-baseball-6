a0810ab6ef93670f5a89e0c2d655907e77d99658
wwan13 <wwan13@naver.com>
2023-10-24 23:20:55 +0900

    fix : `NextStepRandomGenerator` 임의 값 생성 로직 변경 - 기존에 pickUniqueNumbersInRange() 메소드를 사용하였지만 ApplicationTest에 성공하지 못해 구현 로직 변경.

--- src/main/java/baseball/util/random/NextStepRandomGenerator.java
+++ src/main/java/baseball/util/random/NextStepRandomGenerator.java
@@ -2,12 +2,19 @@ package baseball.util.random;
 
 import baseball.application.random.RandomGenerator;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 public class NextStepRandomGenerator implements RandomGenerator {
 
     public List<Integer> pickUniqueNumbersInRange(Range range, int count) {
-        return Randoms.pickUniqueNumbersInRange(
-            range.startInclusive(), range.endInclusive(), count);
+        Set<Integer> numbers = new LinkedHashSet<>();
+        while (numbers.size() < count) {
+            int number = Randoms.pickNumberInRange(range.startInclusive(), range.endInclusive());
+            numbers.add(number);
+        }
+        return new ArrayList<>(numbers);
     }
 }
