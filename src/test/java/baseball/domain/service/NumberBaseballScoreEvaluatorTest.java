dda00b90e83c16357d3ae829853329d1d3e7ced4
wnsgh12s <junho1273@gmail.com>
2023-10-24 15:28:29 +0900

    Test: 숫자 볼 스트라이크 판단 테스트

---
+++ src/test/java/baseball/domain/service/NumberBaseballScoreEvaluatorTest.java
@@ -0,0 +1,24 @@
+package baseball.domain.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class NumberBaseballScoreEvaluatorTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "123, 312, 3볼",
+            "123, 123, 3스트라이크",
+            "123, 456, 낫싱",
+            "123, 134, 1스트라이크 1볼",
+    })
+    void 숫자_볼_스트라이크_테스트(String target, String input, String expected) {
+        NumberBaseballScoreEvaluator evaluator = new NumberBaseballScoreEvaluator(target, input);
+        assertEquals(expected, evaluator.evaluate());
+    }
+}
