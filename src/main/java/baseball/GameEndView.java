31af44f03727372862b821402811fc1953fa85e3
BDlhj <akk808@naver.com>
2023-10-24 21:34:47 +0900

    feat: Implement printAskingRegameIntentionMessage() in src/main/java/baseball/GameEndView.java

--- src/main/java/baseball/GameEndView.java
+++ src/main/java/baseball/GameEndView.java
@@ -2,8 +2,13 @@ package baseball;
 
 public class GameEndView {
     private static final String GAME_ENDING_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String ASKING_REGAME_INTENTION_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public void printGameEndingMessage() {
         System.out.println(GAME_ENDING_MESSAGE);
+    }
+
+    public void printAskingRegameIntentionMessage() {
+        System.out.println(ASKING_REGAME_INTENTION_MESSAGE);
     }
 }
