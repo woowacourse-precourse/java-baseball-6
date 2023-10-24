2b78495f2c6d793dc4b5909e16a1473140976de3
Woo-Shinyoung <tlsdud8205@naver.com>
2023-10-24 16:36:44 +0900

    Feat[domain] 야구 게임 숫자 필드 생성

--- src/main/java/baseball/domain/BaseballNumber.java
+++ src/main/java/baseball/domain/BaseballNumber.java
@@ -1,4 +1,13 @@
 package baseball.domain;
 
 public class BaseballNumber {
+    private int first;
+    private int second;
+    private int third;
+
+    public BaseballNumber(int first, int second, int third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
 }
