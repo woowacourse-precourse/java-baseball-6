21499601c90bb54ba99f620ce869dad50337b5f1
LEEJUNGWOO <jungwoo1002@gmail.com>
2023-10-24 15:39:49 +0900

    refactor: numbers가 전략에 맞는 객체를 생성하도록 변경

--- src/main/java/baseball/model/Numbers.java
+++ src/main/java/baseball/model/Numbers.java
@@ -1,31 +1,43 @@
 package baseball.model;
 
+import baseball.model.numberstrategy.NumberSelectionStrategy;
 import java.util.List;
 import java.util.stream.IntStream;
 
 public class Numbers {
     private final List<Integer> values;
 
-    public Numbers(List<Integer> values) {
+    private Numbers(List<Integer> values) {
         this.values = values;
+    }
+
+    public static Numbers createNumbersFrom(NumberSelectionStrategy numberSelectionStrategy) {
+        return new Numbers(numberSelectionStrategy.createNumbers());
     }
 
     public GameResult calculateResult(Numbers other) {
         int strikeCount = countStrikes(other);
-        int ballCount = countBalls(other, strikeCount);
+        int ballCount = countTotalMatches(other) - strikeCount;
         return new GameResult(ballCount, strikeCount);
     }
 
     private int countStrikes(Numbers other) {
-        return (int) IntStream.range(0, this.values.size())
-                .filter(i -> this.values.get(i).equals(other.values.get(i)))
+        return (int) IntStream.range(0, values.size())
+                .filter(i -> getValueAt(i).equals(other.getValueAt(i)))
                 .count();
     }
 
-    private int countBalls(Numbers other, int strikeCount) {
-        long totalMatchedValues = other.values.stream()
-                .filter(this.values::contains)
+    private int countTotalMatches(Numbers other) {
+        return (int) this.values.stream()
+                .filter(other::contains)
                 .count();
-        return (int) totalMatchedValues - strikeCount;
+    }
+
+    public Integer getValueAt(int index) {
+        return values.get(index);
+    }
+
+    public boolean contains(Integer value) {
+        return values.contains(value);
     }
 }
