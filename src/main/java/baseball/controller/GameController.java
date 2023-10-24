5f1b5d4cfd3750d01839e7f726375c09ee867ed3
MingyeomKim <mingyum119@naver.com>
2023-10-24 14:16:42 +0900

    feat(GameController): 게임의 재시작 혹은 종료 여부를 물어보는 구문 출력
    사용자에게 게임의 재시작 혹은 종료 여부를 묻는 askIfContinue 함수를 구현한다.

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -5,6 +5,8 @@ import java.util.List;
 public class GameController {
 
     private final String GAME_START_GUIDE = "숫자 야구 게임을 시작합니다.";
+    private final String CORRECT_ANSWER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private final String GAME_RESTART_OR_OVER = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     private ComputerController computerController = new ComputerController();
 
@@ -19,6 +21,7 @@ public class GameController {
         while (isRunning) {
             computerController.saveRandomNumbers();
             proceedGame();
+            isRunning = askIfContinue();
         }
     }
 
@@ -28,5 +31,10 @@ public class GameController {
             List<Integer> inputNumbers = playerController.readAndValidateInputNumbers();
             computerController.provideHint(inputNumbers);
         }
+    }
+
+    private Boolean askIfContinue() {
+        System.out.println(CORRECT_ANSWER_MESSAGE);
+        System.out.println(GAME_RESTART_OR_OVER);
     }
 }
