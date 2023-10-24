01311af59522b436ee0645c86f36911d775a74df
Seyeon <wjdtpdus828@naver.com>
2023-10-24 17:51:30 +0900

    fix(View): 게임 종료 메세지 수정

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -6,7 +6,7 @@ public class View{
     private static final String START_SENTENCE = "숫자 야구 게임을 시작합니다.";
     private static final String INPUT_SENTENCE = "숫자를 입력해주세요 : ";
     private static final String RESTART_EXIT_SENTENCE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-    private static final String ANSWER_SENTENCE = "3개의 숫자를 모두 맞히셨습니다 ! 게임 종료";
+    private static final String ANSWER_SENTENCE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     public void startMassage(){
         System.out.println(START_SENTENCE);
