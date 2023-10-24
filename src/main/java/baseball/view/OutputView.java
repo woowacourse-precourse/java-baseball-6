bc5efea4f8a74b2efb6c1459e9e35d47a617042c
Han <yulmam1@naver.com>
2023-10-24 15:22:45 +0900

    fix(view) : Modify typeing error in OutputView class

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -2,31 +2,27 @@ package baseball.view;
 
 public class OutputView {
 
-
     public void  printStartMessage(){
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
     public void printScoreMessage(int strikeCount, int ballCount){
-        if(strikeCount==3){
-            System.out.println("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            return;
-        }
         if(strikeCount == 0 && ballCount == 0){
             System.out.println("낫싱");
             return;
         }
+        if(!(ballCount == 0) && !(strikeCount == 0)) {
+            System.out.print(ballCount+"볼 " + strikeCount + "스트라이크");
+            return;
+        }
         if(!(ballCount == 0)) {
-            System.out.println(ballCount+"볼");
+            System.out.println(ballCount + "볼");
+            return;
         }
-        if(!(strikeCount == 0)) {
-            System.out.println(strikeCount + "스트라이크");
-        }
+        System.out.println(strikeCount + "스트라이크");
     }
 
     public void printGameEndingMessage(){
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
-
-
-
 }
