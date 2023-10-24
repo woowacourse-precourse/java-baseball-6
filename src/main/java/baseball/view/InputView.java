1de1eb5b6974a4a092fdaf655a1d10a77203f5e6
Choo <choo000407@naver.com>
2023-10-24 15:23:11 +0900

    refactor(InputView): 상수명에 PROMPT 붙이게 수정

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -4,16 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    private static final String ASK_SUGGESTED_NUMBER = "숫자를 입력해주세요 : ";
-    private static final String ASK_GAME_CONTINUE_CHOICE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String SUGGESTED_NUMBER_PROMPT = "숫자를 입력해주세요 : ";
+    private static final String GAME_CONTINUE_CHOICE_PROMPT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public static String getSuggestedNumber() {
-        System.out.println(ASK_SUGGESTED_NUMBER);
+        System.out.println(SUGGESTED_NUMBER_PROMPT);
         return Console.readLine();
     }
 
     public static String getGameContinueChoice() {
-        System.out.println(ASK_GAME_CONTINUE_CHOICE);
+        System.out.println(GAME_CONTINUE_CHOICE_PROMPT);
         return Console.readLine();
     }
 }
