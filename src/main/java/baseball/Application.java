09b5eaf17d5cb0722536b336b48b56fb976c4f0a
powerlife145 <powerlife145@gmail.com>
2023-10-24 21:53:32 +0900

    feat: restart

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,31 +9,42 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Scanner scanner = new Scanner(System.in);
-        String target = generateRandomNumber();
-
-        System.out.println("숫자 야구 게임을 시작합니다.");
 
         while (true) {
-            System.out.print("숫자를 입력해주세요 : ");
-            String guess = scanner.nextLine().trim();
+            String target = generateRandomNumber();
 
-            if (guess.isEmpty()) {
-                System.out.println("낫싱");
-                continue;
+            System.out.println("숫자 야구 게임을 시작합니다.");
+
+            while (true) {
+                System.out.print("숫자를 입력해주세요 : ");
+                String guess = scanner.nextLine().trim();
+
+                if (guess.isEmpty()) {
+                    System.out.println("낫싱");
+                    continue;
+                }
+
+                if (!isValidInput(guess)) {
+                    scanner.close();
+                    throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.");
+                }
+
+                GuessResult result = evaluateGuess(target, guess);
+
+                if (result.strikes == 3) {
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다!");
+                    break;
+                } else {
+                    System.out.println(result.balls + "볼 " + result.strikes + "스트라이크");
+                }
             }
 
-            if (!isValidInput(guess)) {
-                scanner.close();
-                throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.");
-            }
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String choice = scanner.nextLine().trim();
 
-            GuessResult result = evaluateGuess(target, guess);
-
-            if (result.strikes == 3) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            if (!"1".equals(choice)) {
+                System.out.println("게임을 종료합니다.");
                 break;
-            } else {
-                System.out.println(result.balls + "볼 " + result.strikes + "스트라이크");
             }
         }
 
