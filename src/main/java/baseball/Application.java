fca6fb2e0d401a0256c27fb002569705995633bc
onlySaying <48788534+onlySaying@users.noreply.github.com>
2023-10-25 02:07:39 +0900

    Revert "dd"
    This reverts commit c1ce49464b000105a34c77b223e33767582c9353.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,6 +7,8 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) 
     {
+        // TODO: 프로그램 구현
+    	//실행
     	System.out.println("숫자 야구 게임을 시작합니다.");
     	
     	while(true)
