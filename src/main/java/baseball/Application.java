e68042949255864bbc85c082fa35a71093f2426a
Chris <hereokay88@gmail.com>
2023-10-24 14:07:53 +0900

    refactor: 상수 클래스 선언

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,7 +3,10 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import static baseball.Constants.*;
+
 public class Application {
+
 
     public static int[] getRandomComputerNumber(){
         int[] numbers = new int[3];
@@ -22,26 +25,22 @@ public class Application {
     }
 
     public static boolean isDuplication(int[] numbers){
-        if (numbers[0]==numbers[1] || numbers[1]==numbers[2] || numbers[2]==numbers[0]){
-            return true;
-        }
-
-        return false;
+        return numbers[0] == numbers[1] || numbers[1] == numbers[2] || numbers[2] == numbers[0];
     }
 
     public static int[] getUserNumber(){
         int[] numbers = new int[3];
 
         do {
-            System.out.print("숫자를 입력해주세요 : ");
+            System.out.print(ENTER_NUMBERS);
             String userInput = Console.readLine();
 
             if (userInput.length() != 3)
-                throw new IllegalArgumentException("세 자리 숫자를 입력해주세요.");
+                throw new IllegalArgumentException(ONLY_THREE_NUMBERS);
 
             for (int i = 0; i < 3; i++) {
                 if (!(userInput.charAt(i) > '0' || userInput.charAt(i) <= '9'))
-                    throw new IllegalArgumentException("모두 숫자를 입력해주세요.");
+                    throw new IllegalArgumentException(ONLY_NUMBER_ALLOWED);
             }
 
             for (int i = 0; i < 3; i++) {
@@ -52,16 +51,20 @@ public class Application {
 
         return numbers;
     }
+
+
+
+
     public static void printResult(int[] cnt){
         if (cnt[0] == 0 && cnt[1] == 0) {
-            System.out.println("낫싱");
+            System.out.println(NOTHING);
         } else if (cnt[0] == 3) {
-            System.out.println("3스트라이크");
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("3"+ STRIKE);
+            System.out.println(WIN_AND_END);
         } else {
             System.out.println(
-                    String.valueOf(cnt[1]) + "볼 "
-                            + String.valueOf(cnt[0]) + "스트라이크"
+                    String.valueOf(cnt[1]) + BALL
+                            + String.valueOf(cnt[0]) + STRIKE
             );
         }
     }
@@ -83,15 +86,12 @@ public class Application {
     }
 
     public static boolean isGameEnd(int[] cnt){
-        if (cnt[0]==3){
-            return true;
-        }
-
-        return false;
+        return cnt[0] == 3;
     }
+
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println(START_GAME);
         int execInput = 1;
         while (execInput == 1){
             int[] computerNumbers = getRandomComputerNumber();
@@ -112,11 +112,11 @@ public class Application {
                 }
             }
 
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            System.out.println(RESTART_GAME_OR_NOT);
             execInput = Integer.parseInt(Console.readLine());
 
             if (!(execInput == 1 || execInput == 2)){
-                throw new IllegalArgumentException("1 또는 2 중에서 입력해주세요.");
+                throw new IllegalArgumentException(CHOICE_ONE_OR_TWO);
             }
 
         }
---
+++ src/main/java/baseball/Constants.java
@@ -0,0 +1,14 @@
+package baseball;
+
+public class Constants {
+    final static String START_GAME = "숫자 야구 게임을 시작합니다.";
+    final static String RESTART_GAME_OR_NOT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    final static String CHOICE_ONE_OR_TWO = "1 또는 2 중에서 입력해주세요.";
+    final static String NOTHING = "낫싱";
+    final static String STRIKE = "스트라이크";
+    final static String WIN_AND_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    final static String BALL = "볼";
+    final static String ENTER_NUMBERS = "숫자를 입력해주세요 : ";
+    final static String ONLY_THREE_NUMBERS = "세 자리 숫자를 입력해주세요.";
+    final static String ONLY_NUMBER_ALLOWED = "모두 숫자를 입력해주세요.";
+}
