c62d10788fbe4c7ea73b3427abcca85fd31279ca
DrivenByCode <jaykey1004@gmail.com>
2023-10-24 14:33:25 +0900

    chore: move print message feature

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,9 @@
 package baseball;
 
-import baseball.constant.ServiceMessage;
-
 public class Application {
     public static void main(String[] args) {
         // 숫자 야구 게임 시작
         BaseBallGame game = new BaseBallGame();
-        System.out.print(ServiceMessage.START.serviceMessage);
         game.startGame();
     }
 }
