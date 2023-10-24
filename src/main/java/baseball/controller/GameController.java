fd2fda6cb8590af2da4f5c824c5724a7a4db7019
mainfn <_@mainfn.dev>
2023-10-24 15:16:53 +0900

    refact: 가독성을 위한 상태 변수 지역화

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,12 +1,13 @@
 package baseball.controller;
 
+import baseball.domain.ball.BallNumber;
 import baseball.domain.ball.Guess;
-import baseball.domain.ball.GuessResult;
 import baseball.domain.game.Computer;
+import baseball.domain.game.GameResult;
 import baseball.domain.game.GameStatus;
 import baseball.view.InputView;
 import baseball.view.OutputView;
-import java.util.List;
+import collection.Triple;
 
 public final class GameController {
 
@@ -25,13 +26,13 @@ public final class GameController {
     }
 
     public void play() {
-        computer.resetAnswer();
+        computer.createAnswer();
         askUntilCorrectGuess(computer);
         replayOrExit();
     }
 
     private void askUntilCorrectGuess(final Computer computer) {
-        final GuessResult result = askGuess(computer);
+        final GameResult result = askGuess(computer);
         outputView.printResult(result);
 
         if (!result.hasThreeStrike()) {
@@ -39,14 +40,15 @@ public final class GameController {
         }
     }
 
-    private GuessResult askGuess(final Computer computer) {
-        final List<Integer> numbers = inputView.inputBallNumbers();
-        final Guess guess = Guess.of(numbers);
+    private GameResult askGuess(final Computer computer) {
+        final Triple<BallNumber> ballNumbers = inputView.inputBallNumbers();
+        final Guess guess = Guess.of(ballNumbers);
         return computer.check(guess);
     }
 
     private void replayOrExit() {
-        if (GameStatus.REPLAY == inputView.inputNextGameStatus()) {
+        final GameStatus status = inputView.inputNextGameStatus();
+        if (status == GameStatus.REPLAY) {
             play();
         }
     }
