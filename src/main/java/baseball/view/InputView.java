174b0c74f0282ad1de15d7a74a7ddc2319a5f170
Choo <choo000407@naver.com>
2023-10-25 05:54:07 +0900

    refactor(View): non static 메서드로 변경

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -6,12 +6,12 @@ public class InputView {
     private static final String SUGGESTED_NUMBER_PROMPT = "숫자를 입력해주세요 : ";
     private static final String GAME_CONTINUE_CHOICE_PROMPT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
-    public static String getSuggestedNumber() {
+    public String getSuggestedNumber() {
         System.out.print(SUGGESTED_NUMBER_PROMPT);
         return Console.readLine();
     }
 
-    public static String getGameContinueChoice() {
+    public String getGameContinueChoice() {
         System.out.println(GAME_CONTINUE_CHOICE_PROMPT);
         return Console.readLine();
     }
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,15 +4,15 @@ public class OutputView {
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
-    public static void printGameStart() {
+    public void printGameStart() {
         System.out.println(GAME_START_MESSAGE);
     }
 
-    public static void printGameEnd() {
+    public void printGameEnd() {
         System.out.println(GAME_END_MESSAGE);
     }
 
-    public static void printRoundResult(String result) {
+    public void printRoundResult(String result) {
         System.out.println(result);
     }
 }
