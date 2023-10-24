811e5e8417d9c84bc6977e88a0535c6c314aed95
wooteco-daram <wooteco.daram@gmail.com>
2023-10-24 20:37:02 +0900

    test: `NsTest` 사용하지 않고 `PrintOutTest` 을 사용하도록 변경

--- src/test/java/baseball/output/rule/PrintBallWhenBallExistRuleTest.java
+++ src/test/java/baseball/output/rule/PrintBallWhenBallExistRuleTest.java
@@ -2,13 +2,11 @@ package baseball.output.rule;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import baseball.PrintOutputTest;
 import baseball.game.BallAndStrike;
-import baseball.output.BaseBallResultOutput;
-import camp.nextstep.edu.missionutils.test.NsTest;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 
-public class PrintBallWhenBallExistRuleTest extends NsTest {
+public class PrintBallWhenBallExistRuleTest extends PrintOutputTest {
     @Test
     void 볼이_없을때_출력하지_않음() {
         final PrintBallWhenBallExistRule rule = new PrintBallWhenBallExistRule(new BallAndStrike(0, 0));
@@ -21,9 +19,5 @@ public class PrintBallWhenBallExistRuleTest extends NsTest {
         final PrintBallWhenBallExistRule rule = new PrintBallWhenBallExistRule(new BallAndStrike(1, 0));
         rule.print();
         assertThat(output()).contains("1볼");
-    }
-    @Override
-    public void runMain() {
-        // Empty
     }
 }
