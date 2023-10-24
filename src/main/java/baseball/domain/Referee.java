e08b14e65dbf0b11eb2a6499adf4a6b23a226c64
PARK9898 <wjddjs21@naver.com>
2023-10-24 23:47:56 +0900

    "feat: Referee CountStrike 메서드 구현"

--- src/main/java/baseball/domain/Referee.java
+++ src/main/java/baseball/domain/Referee.java
@@ -10,4 +10,15 @@ public class Referee {
 		this.answerNumbers = randomNumbers;
 	}
 
+	public int countStrike(Numbers playerNumbers) {
+		int position = 0;
+		int strike = 0;
+		for(int i = 0; i < 3; i++) {
+			if(answerNumbers.isSamePosition(playerNumbers,position)) {
+				strike++;
+			}
+			position++;
+		}
+		return strike;
+	}
 }
