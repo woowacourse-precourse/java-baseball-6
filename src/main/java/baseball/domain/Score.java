e401e438bf6a0770335b23fff3a66edb6dcc0f9c
kunsanglee <gosmdochee2@gmail.com>
2023-10-24 12:45:29 +0900

    refactor(Score): Score enum으로 변경 기존에 Ball과 Strike가 클래스 상태일 때 getter를 사용하지 않으면 Ball과 Strike의 값을 알 수 없어서 enum으로 변경하여 각자가 자신의 상태에 맞게 toString() 오버라이딩 하여 반환하도록 변경하면서, Score 또한 게임 진행시 나올 수 있는 모든 경우의 수가 10가지 이므로 enum으로 변경. - from() 메서드 정의하여 Ball과 Strike에 맞는 Score 값이 반환. - toString() 오버라이딩하여 Score에 정의된 값에 맞게 문자열로 반환.

--- src/main/java/baseball/domain/Score.java
+++ src/main/java/baseball/domain/Score.java
@@ -1,36 +1,55 @@
 package baseball.domain;
 
 
-public class Score {
-    private static final int MIN_COUNT = 0;
-    private static final int MAX_COUNT = 3;
+import java.util.Arrays;
+
+public enum Score {
+    NO_BALL_NO_STRIKE(Ball.from(0), Strike.from(0)),
+    NO_BALL_ONE_STRIKE(Ball.from(0), Strike.from(1)),
+    NO_BALL_TWO_STRIKE(Ball.from(0), Strike.from(2)),
+    NO_BALL_THREE_STRIKE(Ball.from(0), Strike.from(3)),
+    ONE_BALL_NO_STRIKE(Ball.from(1), Strike.from(0)),
+    TWO_BALL_NO_STRIKE(Ball.from(2), Strike.from(0)),
+    THREE_BALL_NO_STRIKE(Ball.from(3), Strike.from(0)),
+    ONE_BALL_ONE_STRIKE(Ball.from(1), Strike.from(1)),
+    ONE_BALL_TWO_STRIKE(Ball.from(1), Strike.from(2)),
+    TWO_BALL_ONE_STRIKE(Ball.from(2), Strike.from(1));
+
     private static final String TOTAL_COUNT_RANGE_EXCEPTION_MESSAGE = "볼과 스트라이크 갯수의 합은 0 이상 3 이하여야 합니다.";
+    public static final String NOTHING = "낫싱";
+    public static final String SPACE = " ";
     private final Ball ball;
     private final Strike strike;
 
-    public Score(Ball ball, Strike strike) {
-        validateTotalCount(ball, strike);
+    Score(Ball ball, Strike strike) {
         this.ball = ball;
         this.strike = strike;
     }
 
-    private static void validateTotalCount(Ball ball, Strike strike) {
-        int totalCount = ball.getValue() + strike.getValue();
-        if (totalCount < MIN_COUNT || MAX_COUNT < totalCount) {
-            throw new IllegalArgumentException(TOTAL_COUNT_RANGE_EXCEPTION_MESSAGE);
-        }
-    }
-
-    public int getBallCount() {
-        return this.ball.getValue();
-    }
-
-    public int getStrikeCount() {
-        return this.strike.getValue();
+    public static Score from(Ball ball, Strike strike) {
+        return Arrays.stream(values())
+                .filter(score -> score.ball.equals(ball) && score.strike.equals(strike))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(TOTAL_COUNT_RANGE_EXCEPTION_MESSAGE));
     }
 
     public boolean isStrikeOut() {
         return this.strike.isFullCount();
     }
 
+    @Override
+    public String toString() {
+        boolean isZeroBall = this.ball.isEmpty();
+        boolean isZeroStrike = this.strike.isEmpty();
+        if (isZeroBall && isZeroStrike) {
+            return NOTHING;
+        }
+        if (isZeroBall) {
+            return this.strike.toString();
+        }
+        if (isZeroStrike) {
+            return this.ball.toString();
+        }
+        return this.ball + SPACE + this.strike;
+    }
 }
