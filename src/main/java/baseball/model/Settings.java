806f9f6dbe3d040827ba393e13afc9af24ec9115
anseongjun <dkstjdwns@kookmin.ac.kr>
2023-10-25 01:17:23 +0900

    refact: Settings 수정

--- src/main/java/baseball/model/Settings.java
+++ src/main/java/baseball/model/Settings.java
@@ -5,9 +5,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Settings {
+
+    public static List<Integer> computer = new ArrayList<>();
+    public static List<Integer> user = new ArrayList<>();
+    public static String restart = "1";
     public static int strike = 0;
     public static int ball = 0;
-    public static String restart = "1";
 
     public static List<Integer> GenerateNumbers() {
         List<Integer> computerNumber = new ArrayList<>();
@@ -18,14 +21,14 @@ public class Settings {
             }
         }
 
-        SetElements();
-
         return computerNumber;
     }
 
-    public static void SetElements() {
+    public static void Init() {
+        user = new ArrayList<>();
         strike = 0;
         ball = 0;
         restart = "1";
     }
+
 }
