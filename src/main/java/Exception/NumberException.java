2068f60b41fad4ccc7583eb1ef556222c5c6402d
dd <12202340@inha.edu>
2023-10-24 18:14:49 +0900

    style: NumberException 에러 메세지 상수로 분리

--- src/main/java/Exception/NumberException.java
+++ src/main/java/Exception/NumberException.java
@@ -9,6 +9,9 @@ import java.util.Set;
 public class NumberException {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 9;
+    private static final String LENGTH_ERROR_MESSAGE = "\nError: " + Computer.LENGTH + "개의 숫자를 입력해 주세요.";
+    private static final String DUP_ERROR_MESSAGE = "\nError: 중복되지 않은 숫자를 입력해 주세요.";
+    private static final String RANGE_ERROR_MESSAGE = "\nError: " + MIN_NUMBER + "부터 " + MAX_NUMBER + "까지 범위의 숫자를 입력해 주세요."
 
     public static void numberException(String inputNumbers) {
         validateNumberLength(inputNumbers);
@@ -18,7 +21,7 @@ public class NumberException {
 
     private static void validateNumberLength(String inputNumbers) {
         if (inputNumbers.length() != Computer.LENGTH) {
-            throw new IllegalArgumentException("\nError: " +  Computer.LENGTH + "개의 숫자를 입력해 주세요.");
+            throw new IllegalArgumentException(LENGTH_ERROR_MESSAGE);
         }
     }
 
@@ -26,7 +29,7 @@ public class NumberException {
         Set<Character> set = new HashSet<>();
         for (char c : inputNumbers.toCharArray()) {
             if (!set.add(c)) {
-                throw new IllegalArgumentException("\nError : 중복 되지 않은 숫자를 입력해 주세요.");
+                throw new IllegalArgumentException(DUP_ERROR_MESSAGE);
             }
         }
     }
@@ -38,7 +41,7 @@ public class NumberException {
 
         for (Integer number : numbers) {
             if (number < MIN_NUMBER || number > MAX_NUMBER) {
-                throw new IllegalArgumentException("\nError : 1부터 9까지의 범위를 가진 숫자를 입력해 주세요.");
+                throw new IllegalArgumentException(RANGE_ERROR_MESSAGE);
             }
         }
     }
