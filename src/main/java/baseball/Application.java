df76bd6bad3afc6ca42c97d533e6c59e4d462b04
anseongjun <dkstjdwns@kookmin.ac.kr>
2023-10-25 01:19:24 +0900

    refact: Application 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,15 +1,12 @@
 package baseball;
 
 import baseball.controller.BaseBallGame;
-import baseball.view.Output;
 
 public class Application {
-
-
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Output.StartMessage();
-        BaseBallGame Game = new BaseBallGame();
-        Game.GameStart();
+
+        BaseBallGame game = new BaseBallGame();
+        game.GameStart();
     }
 }
