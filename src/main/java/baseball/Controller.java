c86d8ab83b7f56616ae2194ac0afb30d6d1aec4a
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-10-24 15:13:33 +0900

    2023.10.24_FEAT : CONTROLLER CLASS - create printRequestRestartOrFinish method

---
+++ src/main/java/baseball/Controller.java
@@ -0,0 +1,12 @@
+package baseball;
+
+import static baseball.Utils.*;
+public class Controller {
+
+    public Controller() {}
+
+
+    private void printRequestRestartOrFinish() {
+        System.out.println(RESTART_OR_FINISH);
+    }
+}
