439c15d1a892b1b7d75920694abd2668a88a4ba3
Kim, JeongHwan <jhkim980214@gmail.com>
2023-10-24 16:42:34 +0900

    리스트 인덱스 명

--- src/main/java/baseball/CalculateResult.java
+++ src/main/java/baseball/CalculateResult.java
@@ -3,17 +3,22 @@ package baseball;
 import java.util.List;
 
 public class CalculateResult {
+
+	private static final int NUMBER_LENGTH = 3;
+	private static final int BALL = 0;
+	private static final int STRIKE = 1;
+
 	public int[] compare(List<Integer> computerNumbers, List<Integer> userNumbers) {
 
 		int[] result = new int[2];
 
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < NUMBER_LENGTH; i++) {
 			int userNumber = userNumbers.get(i);
 			int computerNumber = computerNumbers.get(i);
 			if (userNumber == computerNumber) {
-				result[1]++;
+				result[STRIKE]++;
 			} else if (computerNumbers.contains(userNumber)) {
-				result[0]++;
+				result[BALL]++;
 			}
 		}
 		return result;
