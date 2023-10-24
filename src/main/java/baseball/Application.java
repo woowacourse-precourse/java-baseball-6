75502d1f591ae6b784eef30120efd48550d6602a
SunYerim <syl1602@naver.com>
2023-10-24 23:25:57 +0900

    [FEATURE] application 구현 및 outputview 수정사항 commit

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,20 @@
 package baseball;
 
+import baseball.controller.GameController;
+import baseball.domain.Game;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameController gameController = new GameController(new Game());
+        gameController.startGame();
+
+        while (true) {
+            gameController.playGame();
+
+            if (gameController.restartGame())
+                break;
+        }
+
     }
 }
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -7,18 +7,8 @@ public class OutputView {
     }
 
     // 게임 결과 메시지 출력
-    public static void printGameResult(int strikes, int balls) {
-        if (strikes == 3 && balls == 0) {
-            System.out.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        } else if (strikes > 0) {
-            System.out.print(strikes + "스트라이크 ");
-        } else if (balls > 0) {
-            System.out.print(balls + "볼");
-        } else {
-            System.out.println("낫싱");
-        }
-
-        System.out.println();
+    public static void printGameResult(String resultMessage) {
+        System.out.println(resultMessage);
     }
 
 }
