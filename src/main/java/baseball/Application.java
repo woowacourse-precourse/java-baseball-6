681d59970c2559a8cf75fc32bf0ad8f49c9ab4e5
서창덕 <44630705+Garodden@users.noreply.github.com>
2023-10-24 20:29:13 +0900

    feat: Application.java 에서 게임 진행 기능 구현  23.10.23 -서창덕

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,20 +1,24 @@
 package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         boolean result=false;
+        String ifOver;
         ShootBall randomBall = new ShootBall();
         ShootBall inputBall = new ShootBall();
 
-        while(1) {
+        while(true) {
             randomBall.randomNumGenerator();
             while (!result) {
                 inputBall.inputNumGenerator();
                 result = randomBall.baseballReferee(inputBall);
             }
-            system
+            System.out.print("게임을 종료하시겠습니까? (yes: y /no: any key)");
+            ifOver = Console.readLine();
+            if(ifOver=="y") break;
         }
     }
 }
