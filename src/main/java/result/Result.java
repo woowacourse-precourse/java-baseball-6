2b2131b0ee7a8e2f44fd6a03c8a9f28417223557
jaegyeongkim <dongji_11@naver.com>
2023-10-24 14:15:11 +0900

    feat(Result): 게임 재시작, 종료를 Result 인스턴스에 저장, 검증 및 재시작 종료 판단 기능 추가

---
+++ src/main/java/result/Result.java
@@ -0,0 +1,21 @@
+package result;
+
+import utils.Utils;
+
+public class Result {
+    Utils utils= new Utils();
+    String restartOrEnd ="";
+    public Result(String restartOrEnd){
+        utils.userGameRestartOrEndValidation(restartOrEnd);
+        this.restartOrEnd = restartOrEnd;
+    }
+    public boolean judgeRestartOrEnd(){
+        if(restartOrEnd.equals("1")){
+            return true;
+        }
+        if(restartOrEnd.equals("2")){
+            return false;
+        }
+        return false;
+    }
+}
