bb64912ebfcc7064fc20b71a734aaec318f09de6
jang010505 <jang010505@inu.ac.kr>
2023-10-24 15:12:30 +0900

    test: computeBallCount 함수 테스트 코드 작성

---
+++ src/test/java/baseball/util/GameUtilTest.java
@@ -0,0 +1,34 @@
+package baseball.util;
+
+import org.assertj.core.api.junit.jupiter.InjectSoftAssertions;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+
+class GameUtilTest {
+
+    @InjectMocks
+    private GameUtil gameUtil = new GameUtil();
+
+    @Test
+    @DisplayName("Ball Count 확인")
+    void computeBallCount() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        String computerNumber1 = "123";
+        String playerNumber1 = "321";
+
+        String computerNumber2 = "143";
+        String playerNumber2 = "328";
+
+        Method method = gameUtil.getClass().getDeclaredMethod("computeBallCount", String.class, String.class);
+        method.setAccessible(true);
+
+        Assertions.assertEquals(method.invoke(gameUtil, computerNumber1, playerNumber1), 2);
+        Assertions.assertEquals(method.invoke(gameUtil, computerNumber2, playerNumber2), 1);
+    }
+}
