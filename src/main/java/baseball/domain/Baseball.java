9a996d389a90832a642ef5080c157a5970922948
Beomsu <gobeumsu@gmail.com>
2023-10-25 00:08:16 +0900

    feat(Baseball): 스트라이크 개수 세는 로직 추가

--- src/main/java/baseball/domain/Baseball.java
+++ src/main/java/baseball/domain/Baseball.java
@@ -1,9 +1,23 @@
 package baseball.domain;
 
+
 import java.util.List;
+
 public class Baseball {
+
     private final List<Integer> computerNumber;
+
     public Baseball(List<Integer> computerNumber) {
         this.computerNumber = computerNumber;
+    }
+
+    public int countStrikes(List<Integer> playerNumber) {
+        int strikes = 0;
+        for (int i = 0; i < 3; i++) {
+            if (computerNumber.get(i).equals(playerNumber.get(i))) {
+                strikes++;
+            }
+        }
+        return strikes;
     }
 }
