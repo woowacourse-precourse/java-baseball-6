7fa5f81eaff94d2c5c9e43d37d1e5188d2028ded
ISMINIMIN <summmm.224@gmail.com>
2023-10-25 02:54:34 +0900

    feat: 재시작 여부 확인

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -88,4 +88,13 @@ public class Application {
             System.out.println("낫싱");
         }
     }
+
+    // 게임 재시작 여부 확인
+    public static boolean checkRestart() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = Console.readLine();
+        if(input.equals("1")) return true;
+        else if(input.equals("2")) return false;
+        else throw new IllegalArgumentException();
+    }
 }
