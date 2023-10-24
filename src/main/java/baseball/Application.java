0f0fb253d1fd9c8961c1adcce850e2da371735a7
YuJeongYun <yunyj99@naver.com>
2023-10-25 01:39:04 +0900

    feat: 메인 메서드에서 숫자 야구 게임 실행되도록 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
 
+
+import baseball.baseballGame.BaseBallGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 숫자 3개로 숫자 야구 게임 시작
+        BaseBallGame bGame = new BaseBallGame(3);
+        bGame.start();
     }
 }
