c9a2ad45324f9cb81b49a55cb27f8659362a0d2d
kunsanglee <gosmdochee2@gmail.com>
2023-10-24 12:34:32 +0900

    refactor(Ball): enum으로 변경 - 미리 정의한 값들과 count 값을 비교하여 반환하는 from() 메서드 정의. - toString() 오버라이딩 하여 값이 0일 경우 공백, 값이 존재하는 경우 count와 볼을 더한 문자열 반환.

--- src/main/java/baseball/domain/Ball.java
+++ src/main/java/baseball/domain/Ball.java
@@ -1,45 +1,38 @@
 package baseball.domain;
 
-import java.util.Objects;
+public enum Ball {
+    ZERO(0),
+    ONE(1),
+    TWO(2),
+    THREE(3);
 
-public class Ball {
-    private static final int ZERO_COUNT = 0;
-    private static final int MAX_COUNT = 3;
     private static final String BALL_RANGE_EXCEPTION_MESSAGE = "볼의 유효 범위는 0~3 입니다.";
-    private final int ball;
+    public static final String EMPTY = "";
+    public static final String BALL_MESSAGE = "볼";
+    private final int count;
 
-    public Ball(int ball) {
-        validateBallRange(ball);
-        this.ball = ball;
+    Ball(int ball) {
+        this.count = ball;
     }
 
-    private static void validateBallRange(int ball) {
-        if (ball < ZERO_COUNT || MAX_COUNT < ball) {
-            throw new IllegalArgumentException(BALL_RANGE_EXCEPTION_MESSAGE);
+    public static Ball from(int count) {
+        for (Ball ball : values()) {
+            if (ball.count == count) {
+                return ball;
+            }
         }
-    }
-
-    int getValue() {
-        return this.ball;
+        throw new IllegalArgumentException(BALL_RANGE_EXCEPTION_MESSAGE);
     }
 
     public boolean isEmpty() {
-        return ball == ZERO_COUNT;
+        return Ball.ZERO.equals(this);
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
+    public String toString() {
+        if (isEmpty()) {
+            return EMPTY;
         }
-        if (!(o instanceof Ball ball1)) {
-            return false;
-        }
-        return ball == ball1.ball;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(ball);
+        return this.count + BALL_MESSAGE;
     }
 }
