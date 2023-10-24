dd363c0d09ac824ef6f5111a2990c2d2e476cfff
songyi <song21677@naver.com>
2023-10-25 03:00:32 +0900

    refactor:  Computer 클래스로 분리 서로 다른 임의의 숫자 3개를 만드는 것을 담당한다. 중복 검사 후 값을 추가해주는 Balls 클래스의 add 함수를 이용한다. 중복 되어 숫자를 만들지 못했다면 false를 반환하고 그렇지 않다면 true를 반환해 만들어진 숫자의 개수를 증가시킨다.

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,30 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Computer {
+	private Balls balls = new Balls();
+
+	public Balls makeNumbers() {
+
+		// 임의의 수 3개를 만든다.
+		int count = 0;
+
+		while (true) {
+			if (count == 3) break;
+			if (makeNumber()) count++;
+		}
+
+		return balls;
+	}
+
+	private boolean makeNumber() {
+		try {
+			int randomNumber = Randoms.pickNumberInRange(1, 9);
+				balls.add(randomNumber);
+		} catch (IllegalArgumentException e) {
+			return false;
+		}
+		return true;
+	}
+}
