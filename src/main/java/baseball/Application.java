7bf31e1653bfbfa1bcbe0db0486dfe9da2c33d93
kijen723 <kij2646@gmail.com>
2023-10-24 21:14:11 +0900

    feat: 게임시작 메세지 출력 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,8 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO1: 게임시작 메세지 출력
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
     }
 }
