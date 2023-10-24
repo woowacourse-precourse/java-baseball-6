8dcabdf59942b582b8ec6a8780f7fea804a44f02
U-keun Song <sukeun319@gmail.com>
2023-10-24 23:31:02 +0900

    Comment : Game 클래스 주석 수정
    javadoc 내용을 수정했다.

--- src/main/java/baseball/game/Game.java
+++ src/main/java/baseball/game/Game.java
@@ -10,15 +10,16 @@ public class Game {
     private Game() {
     }
 
-    /** Game 객체를 생성하는 정적 팩토리 메서드. */
+    /**
+     * Game 객체를 생성하는 정적 팩토리 메서드
+     * @return Game 객체
+     */
     public static Game generate() {
         return new Game();
     }
 
     /**
-    *게임을 시작하는 메서드.
-    *적절한 입력을 받는 것으로 진행할 수 있고,
-    *잘못된 입력이 들어오면 애플리케이션이 종료된다.
+     * 게임을 시작하는 메서드
      */
     public void start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
