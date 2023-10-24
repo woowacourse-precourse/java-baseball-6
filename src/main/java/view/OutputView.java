82acabf11b789810a04abc338294684777b91094
yangyanghyunjung <hyunjungyang0922@gmail.com>
2023-10-25 00:14:10 +0900

    🎯feat(view / OutputView): 결과 출력 기능 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,12 +1,21 @@
 package view;
 
 import java.util.List;
+import view.InputView;
 
 public class OutputView {
     private static final String STRIKE = "스트라이크";
     private static final String BALL = "볼";
     private static final String NOTHING = "낫싱";
+    private static final String CHOOES_RESTART_OR_EXIT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
+    InputView inputView = new InputView();
+
+    public void printResult(List<Integer> result) {
+        System.out.println(OutputFormat(result)); // format 출력
+        System.out.println(CHOOES_RESTART_OR_EXIT);
+        inputView.ProgramReStartOrExit();
+    }
     public String OutputFormat(List<Integer> result) {
         StringBuilder sb = new StringBuilder(524);
         if (result.get(0) > 0) { // 볼
