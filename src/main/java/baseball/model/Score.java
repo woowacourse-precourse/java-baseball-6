ea7c4cb6b92e92933f20e504bc18bdc0b5a21220
becooq81 <becooq81@gmail.com>
2023-10-25 01:26:22 +0900

    feat(Score): completed Score class that represents the score of strikes and balls user achieved - constant variable for number of strikes required for user to win victory (declared as variable instead of literal to enhance maintainability in case of having to alter winning requirements) - int variable for ball & strike counts - constructor: all args & no args - getter methods: necessary to print the output of each game/round - isThreeStrikes: checks if user met requirement to win the game

---
+++ src/main/java/baseball/model/Score.java
@@ -0,0 +1,28 @@
+package baseball.model;
+
+public class Score {
+    private static final int PERFECT_STRIKES = 3;
+    private final int ball;
+    private final int strike;
+
+    public Score(int ball, int strike) {
+        this.ball = ball;
+        this.strike = strike;
+    }
+
+    public Score() {
+        this(0, 0);
+    }
+
+    public int getBall() {
+        return ball;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public boolean isThreeStrikes() {
+        return strike == PERFECT_STRIKES;
+    }
+}
