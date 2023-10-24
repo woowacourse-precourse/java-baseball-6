c60c416794c3111b3e64743d0af7999b451a1329
hajungIm <ihj_ewha@ewhain.net>
2023-10-25 01:43:35 +0900

    Feat: baseball 게임 구현
    - `baseballGame( )` : baseball 게임 컨트롤 메소드
    
    -  `getComNums( )` : 게임의 정답인 3가지 숫자List 생성
    -  `getUserNums( )` : 유저의 입력을 받아 숫자List 생성
    
    - `compareNums(comNums, userNums)`
    : 정답인 comNums와 입력인 userNums을 비교하는 메소드
    HashMap에 strike, ball 결과값을 담아 반환
    
    - `printInputStmt( )` : 입력 안내문 출력
    - `printResult(strike, ball)`
    : 비교 결과를 바탕으로 낫싱 / 스트라이크 / 볼 출력
    - `printGameEndStmt( )` : 정답 안내문 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,10 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
@@ -24,10 +28,100 @@ public class Application {
         } while (restartGame);
     }
 
+    private static void baseballGame() {
+        List<Integer> comNums = getComNums();
+
+        int strike = 0;
+        int ball = 0;
+
+        do {
+            printInputStmt();
+
+            List<Integer> userNums = getUserNums();
+
+            HashMap<String, Integer> result = compareNums(comNums, userNums);
+            strike = result.get("strike");
+            ball = result.get("ball");
+
+            printResult(strike, ball);
+        } while (strike != 3);
+
+        printGameEndStmt();
+    }
+
+    /**
+     * input Methods
+     */
+    private static List<Integer> getComNums() {
+        List<Integer> comNums = new ArrayList<>();
+        while (comNums.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!comNums.contains(randomNumber)) {
+                comNums.add(randomNumber);
+            }
+        }
+        return comNums;
+    }
+
+    private static List<Integer> getUserNums() {
+        String input = Console.readLine();
+
+        List<Integer> userNums = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            userNums.add(input.charAt(i) - '0');
+        }
+        return userNums;
+    }
+
+    /**
+     * utility Methods
+     */
+    private static HashMap<String, Integer> compareNums(List<Integer> comNums, List<Integer> userNums) {
+        int strike = 0;
+        int ball = 0;
+
+        for (int i = 0; i < userNums.size(); i++) {
+            Integer cur = userNums.get(i);
+            if (comNums.get(i) == cur) {
+                strike++;
+            } else if (comNums.contains(cur)) {
+                ball++;
+            }
+        }
+
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("strike", strike);
+        map.put("ball", ball);
+
+        return map;
+    }
+
+    /**
+     * print Methods
+     */
     private static void printStartStmt() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
+    private static void printInputStmt() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
+    private static void printResult(int strike, int ball) {
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+        } else if (strike == 0) {
+            System.out.println(ball + "볼");
+        } else if (ball == 0) {
+            System.out.println(strike + "스트라이크");
+        } else {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
+    }
+
+    private static void printGameEndStmt() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
 
     private static void printAskRestartStmt() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
