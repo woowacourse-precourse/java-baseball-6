7d1148611ab15a54b4b46bb936450e484c78c1fa
박준영 <wnsdud6969@naver.com>
2023-10-24 18:07:05 +0900

    feat(Score): add Score to manage ball and strike counts

---
+++ src/main/java/baseball/Score.java
@@ -0,0 +1,19 @@
+package baseball;
+
+public class Score {
+    private int strikeCount;
+    private int ballCount;
+
+    public Score(int strikeCount, int ballCount) {
+        this.strikeCount = strikeCount;
+        this.ballCount = ballCount;
+    }
+
+    public int getStrikeCount() {
+        return strikeCount;
+    }
+
+    public int getBallCount() {
+        return ballCount;
+    }
+}
