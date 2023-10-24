e7734d3a58be913002e94a2e9fd560fcf2dbc229
minseo  jo <liging12@naver.com>
2023-10-24 22:47:13 +0900

    refactor: 상수 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -2,8 +2,9 @@ package baseball.view;
 
 public class InputView {
 
+    private static final String LINE = "\n";
     private static final String INPUT_BALLS_MESSAGE = "숫자를 입력해주세요 : ";
-    private static final String INPUT_GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
+    private static final String INPUT_GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요." + LINE;
 
     public void printPlayerGuessMessage() {
         displayMessage(INPUT_BALLS_MESSAGE);
