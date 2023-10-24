b9631cc04d9f1cca4d7ad7e785e6c1a3cd3a75e5
kg9585 <kg9585@naver.com>
2023-10-24 19:33:39 +0900

    feat: 컴퓨터의 수를 출력한다

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,10 +1,15 @@
 package baseball.view;
 
 import baseball.model.Result;
+import java.util.List;
 
 public class OutputView {
     public void printGameStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void printComputerNumber(List<Integer> computerNumber) {
+        System.out.println(computerNumber);
     }
 
     public void printGameResult(Result result) {
