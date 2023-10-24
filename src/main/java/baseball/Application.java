fef0712443bf179735291dc03aa35ea338250db9
songyi <song21677@naver.com>
2023-10-25 01:10:31 +0900

    feat: 서로 다른 임의의 수 3개 만들기 수가 중복이 되지 않도록, 3개가 넘지 않도록 3개의 수를 만들었습니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
+
+        List<Integer> computer = makeNumbers();
+    }
+
+    static List<Integer> makeNumbers () {
+        // 임의의 수 3개를 만든다.
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 }
