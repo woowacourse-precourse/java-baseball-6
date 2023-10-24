283dd1c584656faeee69161b9561d22f6517904b
swhyeon98 <smreo75@gmail.com>
2023-10-25 00:25:26 +0900

    feat: 플레이어로부터 받은 입력값이 숫자인지 검증하는 기능 추가

--- src/main/java/baseball/InputHandler.java
+++ src/main/java/baseball/InputHandler.java
@@ -2,11 +2,22 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.regex.Pattern;
+
 public class InputHandler {
+
+    private static final Pattern NUMBER_PATTERN = Pattern.compile("^[1-9]+$");
 
     public String getPlayInput() {
         System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
+        validateIsNumber(input);
         return input;
+    }
+
+    private void validateIsNumber(String input) {
+        if (!NUMBER_PATTERN.matcher(input).matches()) {
+            throw new IllegalArgumentException("숫자만 입력해주세요.");
+        }
     }
 }
