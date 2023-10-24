33ef5dc4e54ac0734e04be6883521dd2e5718e47
김영준 <rladudwns164@gmail.com>
2023-10-24 14:18:37 +0900

    refactor : "OutputView 클래스의 가독성 및 모듈화 개선
    -"BALL"과 "STRIKE" 문자열을 상수로 추출.
    -'isNoHit'라는 메소드 추가하여 볼, 스트라이크 없음을 판단하는 로직 명확화.
    -볼과 스트라이크 메시지 출력을 별도의 메소드 (printBallMessage, printStrikeMessage)로 분리하여 모듈화.
    -`printHits` 메소드에 `System.out.println()` 추가하여 볼과 스트라이크 출력 후 줄바꿈 하도록 수정, 출력 형식 개선.
    -Message enum 내부의 'getEndCount'를 'getCount'로, 'number' 필드를 'count'로 이름 변경,

--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -6,6 +6,9 @@ public class OutputView {
 
     private static final OutputView INSTANCE = new OutputView();
 
+    private static final String BALL = "BALL";
+    private static final String STRIKE = "STRIKE";
+
     public static OutputView getInstance() {
         return INSTANCE;
     }
@@ -14,63 +17,80 @@ public class OutputView {
     }
 
     public void printGameStart() {
-        System.out.println(Message.GAME_START.getMessage());
+        printMessage(Message.GAME_START);
     }
 
     public boolean printGamerResult(Map<String, Integer> comparisonResult) {
-        int balls = comparisonResult.get("BALL");
-        int strikes = comparisonResult.get("STRIKE");
+        int balls = comparisonResult.getOrDefault(BALL, Message.ZERO.getCount());
+        int strikes = comparisonResult.getOrDefault(STRIKE, Message.ZERO.getCount());
 
-        if (balls == 0 && strikes == 0) {
-            System.out.println(Message.NOTHING.getMessage());
+        if (isNoHit(balls, strikes)) {
+            printMessage(Message.NOTHING);
             return false;
         }
-        if (balls == 0) {
-            System.out.println(strikes + Message.STRIKE.getMessage());
 
-            if (strikes == Message.GAME_END.getEndCount()) {
-                return true;
-            }
+        return printHits(balls, strikes);
+    }
 
-            return false;
+
+    private boolean isNoHit(int balls, int strikes) {
+        return balls == Message.ZERO.getCount() && strikes == Message.ZERO.getCount();
+    }
+
+    private boolean printHits(int balls, int strikes) {
+
+        if (balls != Message.ZERO.getCount()) {
+            printBallMessage(balls);
         }
-        if (strikes == 0) {
-            System.out.println(balls + Message.BALL.getMessage());
-            return false;
-        }
-        System.out.println(
-                balls + Message.BALL.getMessage() +" " +strikes + Message.STRIKE.getMessage());
-        return false;
 
+        if (strikes != Message.ZERO.getCount()) {
+            printStrikeMessage(strikes);
+        }
+
+        System.out.println();
+
+        return isGameEnd(strikes);
+    }
+
+    private void printBallMessage(int balls) {
+        System.out.print(balls + Message.BALL.getMessage() + " ");
+    }
+
+    private void printStrikeMessage(int strikes) {
+        System.out.print(strikes + Message.STRIKE.getMessage());
+    }
+
+    private boolean isGameEnd(int strikes) {
+        return strikes == Message.GAME_END.getCount();
+
+    }
+
+    private void printMessage(Message message) {
+        System.out.println(message.getMessage());
     }
 
 
     private enum Message {
-        GAME_START("숫자 야구 게임을 시작합니다"),
-        NOTHING("낫싱"),
-        BALL("볼"),
-        STRIKE("스트라이크"),
-        GAME_END(3);
+        GAME_START("게임을 시작합니다."), NOTHING("낫싱"), BALL("볼"), STRIKE("스트라이크"), GAME_END(3), ZERO(0);
 
         private String message;
-        private int strikeEndCount;
+        private int count;
 
         Message(String message) {
             this.message = message;
         }
 
-        Message(int strikeEndCount) {
-            this.strikeEndCount = strikeEndCount;
+        Message(int count) {
+            this.count = count;
         }
 
         public String getMessage() {
             return message;
         }
 
-        public int getEndCount() {
-            return strikeEndCount;
+        public int getCount() {
+            return count;
         }
+
     }
-
-
 }
