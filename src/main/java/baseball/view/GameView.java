e58596d090d08d0b75c8f01a73db0caba6a0ef05
hangillee <skfcb10@naver.com>
2023-10-25 03:04:28 +0900

    chore: edit print to println for game over message

--- src/main/java/baseball/view/GameView.java
+++ src/main/java/baseball/view/GameView.java
@@ -11,7 +11,7 @@ public class GameView {
     public String printGameResult(String hint) {
         System.out.println(hint);
         if (hint.equals("3스트라이크")) {
-            System.out.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             return Console.readLine();
         }
