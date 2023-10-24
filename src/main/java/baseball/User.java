603726032861bd2e2025e56cea1968adb4c04ecc
eunjin <12191804@inha.edu>
2023-10-24 23:30:20 +0900

    feat: 유저 숫자 입력 기능 구현

---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,10 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class User {
+    public String input() {
+        System.out.print("숫자를 입력해주세요 : ");
+        return Console.readLine();
+    }
+}
