29ce185fc5095166287f5816b825e07c3ee6797b
peeaaccee <wlgp8958@naver.com>
2023-10-25 08:28:52 +0900

    feat(Score): 객체화하여 비교

--- src/main/java/baseball/domain/model/Score.java
+++ src/main/java/baseball/domain/model/Score.java
@@ -3,6 +3,7 @@ package baseball.domain.model;
 import java.util.Arrays;
 import java.util.EnumMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Score {
@@ -28,8 +29,32 @@ public class Score {
 
     public Map<String, Integer> getResult() {
         Object LinkedHashMap;
-        return score.keySet().stream()
+        Map<String, Integer> collect = score.keySet().stream()
                 .collect(Collectors.toMap(BallCount::getValue, score::get, (a, b) -> b, LinkedHashMap::new));
+        return collect;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Score score1 = (Score) o;
+        return Objects.equals(score, score1.score);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(score);
+    }
+
+    @Override
+    public String toString() {
+        return "Score{" +
+                "score=" + score +
+                '}';
+    }
 }
