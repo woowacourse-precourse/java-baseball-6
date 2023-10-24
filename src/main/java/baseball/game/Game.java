8b4942a6618a93290566e0cdaf3124a7d475df12
U-keun Song <sukeun319@gmail.com>
2023-10-24 14:48:13 +0900

    Refactor : strikeOut 메서드 수정으로 인한 변수 타입 변경 및 불필요한 변수 제거
    judge 메서드에서 strikeOut 메서드로 바뀌면서 matchStart 메서드의 반복문을 boolean 값으로 확인하면서 수행하도록 코드를 변경했다. 추가적으로 입력을 받을 때 input 객체를 생성했었는데, 불필요하다고 판단해서 삭제했다.

--- src/main/java/baseball/game/Game.java
+++ src/main/java/baseball/game/Game.java
@@ -25,24 +25,21 @@ public class Game {
      */
     public void start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        String input;
         do {
             matchStart(0, 0, 0);
             System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ");
-            input = Console.readLine();
-        } while (pitcher.restart(input));
+        } while (pitcher.restart(Console.readLine()));
         Console.close();
     }
 
     private void matchStart(int a, int b, int c) {
         referee.playBall(a, b, c);
-        int[] pitch, judge;
-        String input;
+        int[] pitch;
+        boolean strikeOut;
         do {
             System.out.print("숫자를 입력해주세요 : ");
-            input = Console.readLine();
-            pitch = pitcher.pitch(input);
-            judge = referee.judge(pitch);
-        } while (judge[1] != 3);
+            pitch = pitcher.pitch(Console.readLine());
+            strikeOut = referee.strikeOut(pitch);
+        } while (!strikeOut);
     }
 }
