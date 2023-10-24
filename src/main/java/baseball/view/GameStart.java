581e7dafe680becce587b228cc913eae7a30e2bf
박승찬 <seungchan141414@gmail.com>
2023-10-25 02:58:27 +0900

    [Refactor(baseball)] method 분할

--- src/main/java/baseball/view/GameStart.java
+++ src/main/java/baseball/view/GameStart.java
@@ -33,15 +33,22 @@ public class GameStart {
 
             // resultStr 이 "3스트라이크" 아니면 반복을 한다.
             while (Utill.isNotSameString(hintOfJudge, MyConstants.MSG_GAME_TERMINATION_CONDITION_STR())) {
-                inputRanNumOfUser(); // 1. 유저의 입력을 받아서 userList을 얻는다.
-                hintOfJudge = gameProgress.getHintOfJudge(computerList, userList); // 2. judge의 힌트를 얻는다.
-                System.out.println(hintOfJudge); // 3. 힌트를 출력한다.
+                playMainBaseBallGame();
             }
 
             if (isNotProceedGame()) { // 4.게임의 종료 상황을 알려주고, 4.1 숫자 2을 누르면 게임 종료 진행
                 break;
             }
         }
+    }
+
+    /**
+     * 메인 야구 게임의 진행 상황을 시작한다.
+     */
+    private void playMainBaseBallGame() {
+        inputRanNumOfUser(); // 1. 유저의 입력을 받아서 userList을 얻는다.
+        hintOfJudge = gameProgress.getHintOfJudge(computerList, userList); // 2. judge의 힌트를 얻는다.
+        printHintMsg();
     }
 
 
@@ -53,28 +60,6 @@ public class GameStart {
     private void initGame(NumbersGenerate numbersGenerate) {
         hintOfJudge = MyConstants.RESET_STRING(); // 결과 리셋
         computerList = numbersGenerate.createRandomNumList(); // 컴퓨터의 3개의 랜덤 숫자 리스트
-    }
-
-    /**
-     * 3스트라이크가 되면 게임 진행을 위한 절차를 호출하는 메서드.
-     *
-     * @return
-     */
-    private boolean isNotProceedGame() {
-        int inputInt = RESET_NUM;
-
-        System.out.println(MyConstants.MSG_END_GAME_COMMENT_STR()); // 1. "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
-        System.out.println(MyConstants.MSG_GAME_PROGRESS_SELECTION_STATUS_STR()); // 2. "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
-
-        inputInt = Utill.inputIntNum(); // 3. 숫자를 입력받는다.
-        ValidException.isValidOnlyInputOneTwo(inputInt); // 1과 2가 아니면 예외처리
-
-        if (Utill.isSameInteger(inputInt, NOT_AGAIN_GAME)) {  // 4. 입력 받는 숫자가 2이면 true - STOP
-            return true;
-        }
-
-        // 5. 입력 받는 숫자가 1이면 - start
-        return false;
     }
 
     /**
@@ -111,5 +96,41 @@ public class GameStart {
 
         inputStrArr = Utill.getSplitStrArr(numInputOfUser);
         Utill.makeListFromArr(inputList, inputStrArr);
+    }
+
+    /**
+     * 3스트라이크가 되면 게임 진행을 위한 절차를 호출하는 메서드.
+     *
+     * @return
+     */
+    private boolean isNotProceedGame() {
+        int inputInt = RESET_NUM;
+
+        printSecondProcessMsg();
+
+        inputInt = Utill.inputIntNum(); // 3. 숫자를 입력받는다.
+        ValidException.isValidOnlyInputOneTwo(inputInt); // 1과 2가 아니면 예외처리
+
+        if (Utill.isSameInteger(inputInt, NOT_AGAIN_GAME)) {  // 4. 입력 받는 숫자가 2이면 true - STOP
+            return true;
+        }
+
+        // 5. 입력 받는 숫자가 1이면 - start
+        return false;
+    }
+
+    /**
+     * 2번째 게임 진행상황의 문자열을 출력한다.
+     */
+    private void printSecondProcessMsg() {
+        System.out.println(MyConstants.MSG_END_GAME_COMMENT_STR()); // 1. "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+        System.out.println(MyConstants.MSG_GAME_PROGRESS_SELECTION_STATUS_STR()); // 2. "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+    }
+
+    /**
+     * 힌트를 출력해주는 메서드
+     */
+    private void printHintMsg() {
+        System.out.println(hintOfJudge); // 3. 힌트를 출력한다.
     }
 }
