78e6241954202e4f87cbaa98a1a8426099245337
동원 <ai06021@gmail.com>
2023-10-24 16:42:22 +0900

    style : List intí타ì ¼로 수정

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -1,28 +1,29 @@
 package baseball;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Input {
-    public List<Character> numbers() {
+    public List<Integer> numbers() {
         Scanner sc = new Scanner(System.in);
         String input = sc.next();
-        List<Character> inputNumber = new LinkedList<>();
+        List<Integer> inputNumber = new ArrayList<>();
 
         for (int i = 0; i < input.length(); i++) {
             char num = input.charAt(i);
             if (num < '0' || num > '9')
                 break;
-            inputNumber.add(num);
+            int value = Integer.parseInt(String.valueOf(num));
+            inputNumber.add(value);
         }
         if (!inputCheck(inputNumber))
             inputNumber.clear();
         return inputNumber;
     }
 
-
-    private boolean inputCheck(List<Character> numbers) {
+    private boolean inputCheck(List<Integer> numbers) {
         try {
             if (numbers.size() == 3)
                 throw new IllegalArgumentException();
