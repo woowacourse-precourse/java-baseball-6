67b34c97ce80a8d824cc8943def5a7f5174e033d
jae woo Jung <awfjol2008@naver.com>
2023-10-24 18:24:47 +0900

    refector: 게임 종료 및 재시작 enum 추가 구현

--- src/main/java/baseball/constant/SystemMessage.java
+++ src/main/java/baseball/constant/SystemMessage.java
@@ -4,8 +4,10 @@ public enum SystemMessage {
 
     TYPE_NUMBER("숫자를 입력해주세요 : "),
     GAME_END("3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
-    GAME_RESTART("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-
+    GAME_RESTART("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."),
+    EXIT_FLAG("1"),
+    RESTART_FLAG("2");
+    
     private final String message;
 
     SystemMessage(String message) {
