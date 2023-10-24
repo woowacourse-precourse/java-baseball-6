37f9a60bef244b0d7ac7cba40ffead5b60be1e32
ihun-il <hunil9978@gmail.com>
2023-10-24 21:11:35 +0900

    feat: Application 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import baseball.controller.GameController;
+import baseball.view.OutView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameController gameController = new GameController();
+        OutView.start();
+        do{
+            gameController.start();
+        }while(gameController.isReStart());
     }
 }
