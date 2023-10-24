1371864a8da53b5f8434eb47c90f012db2b192c6
minseo  jo <liging12@naver.com>
2023-10-24 16:55:21 +0900

    style: 상수 이용

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,10 +3,9 @@ package baseball.view;
 import static baseball.domain.Baseballs.BALL_SIZE;
 
 public class OutputView {
-
-    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.\n";
-    private static final String GAME_END_MESSAGE = BALL_SIZE + "개의 숫자를 모두 맞히셨습니다! 게임 종료\n";
     private static final String LINE = "\n";
+    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다." + LINE;
+    private static final String GAME_END_MESSAGE = BALL_SIZE + "개의 숫자를 모두 맞히셨습니다! 게임 종료" + LINE;
 
     public void printStartGameMessage() {
         displayMessage(GAME_START_MESSAGE);
