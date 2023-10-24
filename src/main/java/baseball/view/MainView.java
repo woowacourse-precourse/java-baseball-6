f7370592d66c38e0ffbf2add728ab225e914a61e
jihoon98 <tlswlgns1003@sju.ac.kr>
2023-10-24 21:24:15 +0900

    "feat:mainview 작성"

--- src/main/java/baseball/view/MainView.java
+++ src/main/java/baseball/view/MainView.java
@@ -1,4 +1,16 @@
 package baseball.view;
 
 public class MainView {
+    public void displayResult(int strike, int ball, int nothing) {
+        if (strike > 0) {
+            System.out.print(strike + "스트라이크 ");
+        }
+        if (ball > 0) {
+            System.out.print(ball + "볼 ");
+        }
+        if (nothing > 0) {
+            System.out.print(nothing + "낫싱 ");
+        }
+        System.out.println();
+    }
 }
