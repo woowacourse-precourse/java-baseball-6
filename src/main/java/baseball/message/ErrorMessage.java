2d80250e2438874e4bb7727845bc5672719cbc07
dradnats1012 <herjebi1012@gmail.com>
2023-10-24 19:37:40 +0900

    Feat : 에러 메시지 잘못된 게임플래그 입력시 메시지 추가

--- src/main/java/baseball/message/ErrorMessage.java
+++ src/main/java/baseball/message/ErrorMessage.java
@@ -4,7 +4,8 @@ public enum ErrorMessage {
     DUPLICATE_NUMBER("[ERROR] 중복되는 숫자가 존재합니다."),
     NUMBER_NOT_3("[ERROR] 숫자가 3개가 아닙니다."),
     NOT_RANGE_NUMBER("[ERROR] 숫자의 범위가 잘못되었습니다."),
-    NOT_NUMBER("[ERROR] 숫자가 아닙니다.");
+    NOT_NUMBER("[ERROR] 숫자가 아닙니다."),
+    WRONG_GAMEFLAG("[ERROR] 올바르지 않은 입력입니다.");
 
     private final String message;
 
