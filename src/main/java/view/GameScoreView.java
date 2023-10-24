57d7b219612598837e3a3043eead68ba719b1c36
cherBlair <cher.blair.h@gmail.com>
2023-10-24 15:33:54 +0900

    [FIX] 메시지 중복 삭제

--- src/main/java/view/GameScoreView.java
+++ src/main/java/view/GameScoreView.java
@@ -9,7 +9,6 @@ public class GameScoreView {
     private static final int BALL_COUNT_INDEX = 1;
 
     public void completeThreeStrikeScore() { // 3스트라이크 완성
-        System.out.println("3스트라이크");
         System.out.println(GameMessage.FINISH_MESSAGE);
         System.out.println(GameMessage.RESTART_SELECT_MESSAGE);
     }
