ee69b21c1426efafbe66c0a95f081d56c196c228
Subin <rla0010101@gmail.com>
2023-10-24 21:32:35 +0900

    Feat: 입출력 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     private static String randomNumber;
@@ -16,11 +17,18 @@ public class Application {
         //TODO: 숫자 야구 게임 구현
         isOver = false;
         while (!isOver) {
+            System.out.println("숫자 야구 게임을 시작합니다.");
             randomNumber = makeRandomNumber();
             isSucceed = false;
             while (!isSucceed) {
+                System.out.print("숫자를 입력해주세요 : ");
+                inputNumber = Console.readLine();
+                printAnswerHint();
                 isSucceed();
             }
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            restartOrEndFlag = Console.readLine();
             isOver();
         }
     }
