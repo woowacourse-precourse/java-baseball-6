f102b1d9a6578f017ad904da7a144793b05f8fa6
yj-melissa <melimelame@gmail.com>
2023-10-25 02:05:22 +0900

    feat: 숫자를 저장할 자료구조 추가

---
+++ src/main/java/baseball/model/Number.java
@@ -0,0 +1,13 @@
+package baseball.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Number {
+    int NUMBER_SIZE = 3;
+    List<Integer> numbers = new ArrayList<>();
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+}
