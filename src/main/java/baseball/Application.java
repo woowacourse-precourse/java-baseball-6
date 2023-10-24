bf025de99fa88425d8cffa2fadfefd858bfdec6c
JUNChangMin <jsjs1209@skku.edu>
2023-10-24 16:45:12 +0900

    Docs:Explain how the program work

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    static List<Integer> computer = new ArrayList<>();
+    static boolean ansFlag = true;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+
 }
+
