fdb2b9b2686047912e69cfd73e7d17e51d68ed8a
min1018 <102647148+min1018@users.noreply.github.com>
2023-10-25 03:29:21 +0900

    fix: add exception for user input

--- src/main/java/baseball/utils/UserNumList.java
+++ src/main/java/baseball/utils/UserNumList.java
@@ -1,12 +1,24 @@
-package utils;
+package baseball.utils;
 
 public class UserNumList {
 
     public static int[] UserInputList(String num) {
+        checkLength(num);
         int[] userInputList = new int[num.length()];
         for (int i = 0; i < num.length(); i++) {
-            userInputList[i] = num.charAt(i) - '0';
+            if (checkAvailableNum(num, i)){
+                userInputList[i] = num.charAt(i) - '0';
+            }
         }
         return userInputList;
+    }
+
+    private static void checkLength(String num) throws IllegalArgumentException{
+        if (num.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+    }
+    private static boolean checkAvailableNum(String num, int i) {
+        return '0' <= num.charAt(i) && num.charAt(i) <= '9';
     }
 }
