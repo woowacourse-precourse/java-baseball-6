84e541a8440b823a2f1dfb80d29f987247a46a5b
hyunsu4020 <dlgustn4020@naver.com>
2023-10-24 20:40:51 +0900

    Revert "기능 구현 완료"
    This reverts commit 293572fffa349e239eaf219dd8666b1d0a31b6a9.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,27 +1,7 @@
 package baseball;
 
-import baseball.controller.BaseballGame;
-import baseball.model.RetryNumber;
-import baseball.view.InputView;
-import baseball.view.OutputView;
-
 public class Application {
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
-        OutputView.printGameStart();
-        do {
-            BaseballGame baseballGame = new BaseballGame();
-            baseballGame.start();
-        } while (askRetry());
-    }
-
-    public static boolean askRetry() {
-        RetryNumber retryNumber = new RetryNumber(InputView.setRetryNumber());
-
-        if (retryNumber.getRetryNumber().equals("1")) {
-            return true;
-        }
-
-        return false;
+        // TODO: 프로그램 구현
     }
 }
