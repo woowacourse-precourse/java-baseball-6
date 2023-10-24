0f965eee80002ce39fbe7876426d724c225e0f83
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-10-25 05:36:00 +0900

    feat : View단 구현 사용자의 입력 값을 받고, 출력을 내보이는 view단의 기능을 구현함

--- src/main/java/baseball/view/UserOutputView.java
+++ src/main/java/baseball/view/UserOutputView.java
@@ -1,4 +1,51 @@
 package baseball.view;
 
+import baseball.domain.GameResult;
+
 public class UserOutputView {
+    private static final String startGameMessage = "숫자 야구 게임을 시작합니다";
+    private static final String strikeMessage = "스트라이크";
+    private static final String ballMessage = "볼";
+    private static final String nothingMessage = "낫싱";
+    private static final String gameFinishMessage = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final int nothingCount = 0;
+    private GameResult gameResult;
+
+    public UserOutputView(GameResult gameResult) {
+        this.gameResult = gameResult;
+    }
+    public void printDescription(){
+        strike();
+        ball();
+        ballAndStrike();
+        nothing();
+    }
+
+    public static void startGame() {
+        System.out.println(startGameMessage);
+    }
+    public static void finishGame() {
+        System.out.println(gameFinishMessage);
+    }
+    private void strike(){
+        if(gameResult.ballCount == nothingCount && gameResult.strikeCount != nothingCount){
+            System.out.println(gameResult.strikeCount + strikeMessage);
+        }
+    }
+    private void ball(){
+        if(gameResult.ballCount != nothingCount && gameResult.strikeCount == nothingCount) {
+            System.out.println(gameResult.ballCount + ballMessage);
+        }
+    }
+    private void ballAndStrike(){
+        if(gameResult.ballCount != nothingCount && gameResult.strikeCount != nothingCount){
+            System.out.println(gameResult.ballCount +ballMessage +" " + gameResult.strikeCount + strikeMessage);
+        }
+    }
+    private void nothing() {
+        if(gameResult.ballCount == nothingCount && gameResult.strikeCount == nothingCount) {
+            System.out.println(nothingMessage);
+        }
+    }
+
 }
