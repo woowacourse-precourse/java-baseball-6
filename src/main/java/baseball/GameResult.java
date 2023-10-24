792008c618a144d3be398ccbc094555eb1976891
ssook <60427387+ssook1222@users.noreply.github.com>
2023-10-24 07:26:45 +0000

    UPDATE : 출력 문구 요구사항에 맞게 변경

--- src/main/java/baseball/GameResult.java
+++ src/main/java/baseball/GameResult.java
@@ -15,9 +15,16 @@ public class GameResult {
 
     @Override
     public String toString() {
-        if (strikes == 0 && balls == 0) {
-            return "낫싱";
+        if (strikes == 0) {
+            if (balls == 0) {
+                return "낫싱";
+            } else {
+                return balls + "볼";
+            }
+        } else if (balls == 0) {
+            return strikes + "스트라이크";
+        } else {
+            return balls + "볼 " + strikes + "스트라이크";
         }
-        return balls + "볼 " + strikes + "스트라이크 " ;
     }
 }
