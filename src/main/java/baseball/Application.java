50ab7c2c21d163a0e55ed8590db97029f4f28e25
jihun-24k <gi020477@gmail.com>
2023-10-25 02:28:37 +0900

    Feat: 게임 종료 후 문구 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,6 +6,11 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        startGame();
+    }
+
+    public static void startGame() {
         final List<Integer> computer = Util.makeRandomNumbers();
 
         String input;
@@ -80,4 +85,13 @@ public class Application {
         return ballComment;
     }
 
+    private static void askContinueOrEnd(String comment) {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = Console.readLine();
+        answerContinueOrEnd(input);
+    }
+
+    private static void answerContinueOrEnd(String input) {
+
+    }
 }
