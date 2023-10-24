f2eb76e8dd7063f88fed61913c12bb081954810a
kimdoha <kkhhjj3393@gmail.com>
2023-10-24 18:20:32 +0900

    refactor: 입력 시 발생하는 에러 메세지 상수 관리

--- src/main/java/baseball/common/Constants.java
+++ src/main/java/baseball/common/Constants.java
@@ -1,10 +1,10 @@
 package baseball.common;
 
 public class Constants {
-    public static int numDigit = 3;
-    public static int startNum = 1;
-    public static int lastNum = 9;
-    public static int strikeTarget = 3;
-    public static int gameRestart = 1;
-    public static int gameExit = 2;
+    public static final int numDigit = 3;
+    public static final int startNum = 1;
+    public static final int lastNum = 9;
+    public static final int strikeTarget = 3;
+    public static final int gameRestart = 1;
+    public static final int gameExit = 2;
 }
---
+++ src/main/java/baseball/common/ErrorConstants.java
@@ -0,0 +1,8 @@
+package baseball.common;
+
+public class ErrorConstants {
+    public static final String EXCEEDS_THREE_DIGITS_ERROR = "입력한 숫자가 세 자리 수를 초과하였습니다.";
+    public static final String DUPLICATE_NUMBER_ERROR = "입력한 숫자가 중복되었습니다.";
+    public static final String NO_INPUT_NUMBER_ERROR = "입력한 숫자가 없습니다.";
+    public static final String INVALID_NUMBER_ERROR = "입력한 숫자가 유효하지 않습니다.";
+}
