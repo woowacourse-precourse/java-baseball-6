42554111cb37d9d6a20d1635759caf023f12708f
Hyun Woo <hwsa10041@gmail.com>
2023-10-24 14:34:42 +0900

    feat : inputNumber()를 통해 수를 입력받을 시 3자리를 확인하는 검증 메서드 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -7,7 +7,14 @@ public class InputView {
     public String inputNumber() {
         System.out.print("숫자를 입력해주세요 : ");
         String number = readLine();
+        validateThreeDigitNumber(number);
         return number;
+    }
+
+    public void validateThreeDigitNumber(String number) {
+        if (number.length() != 3) {
+            throw new IllegalArgumentException("[ERROR] 3자리의 숫자를 입력하여야 합니다.");
+        }
     }
 
     public int inputRestartChoice() {
