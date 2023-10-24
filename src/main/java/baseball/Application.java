a775284035e64bf2db1e62558125884f2c8445e3
rkf06 <rkf06@DESKTOP-DUIQUNE>
2023-10-24 17:23:53 +0900

    정답을 맞힌 후 1을 작성했을 때 재시작

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,23 +7,26 @@ import java.util.Scanner;
 import setting.RandomNum;
 
 public class Application {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IllegalArgumentException {
 
 		Scanner sc = new Scanner(System.in);
 
-		// 랜덤 숫자 생성
+		// 랜덤 숫자 변수명 설정
 		String strAnswer = "";
-		RandomNum ramdomNum = new RandomNum();
-		strAnswer = ramdomNum.setRandomNum();
-		System.out.println(strAnswer);
 
 		// 게임 시작
 		int checkNum = 0;
-		System.out.println("숫자 야구 게임을 시작합니다.");
+		int strikeCnt = 0;
+		int ballCnt = 0;
 
 		while (true) {
-			int strikeCnt = 0;
-			int ballCnt = 0;
+			if(strAnswer.equals("")) { //랜덤 숫자 생성
+				RandomNum ramdomNum = new RandomNum();
+				strAnswer = ramdomNum.setRandomNum();
+				System.out.println(strAnswer);
+				System.out.println("숫자 야구 게임을 시작합니다.");
+			}
+			
 			System.out.println("숫자를 입력해주세요 : ");
 			checkNum = sc.nextInt();
 			System.out.println(checkNum);
@@ -53,21 +56,20 @@ public class Application {
 				System.out.println(strikeCnt + "스트라이크");
 			}
 
-			//낫싱 체크
+			// 낫싱 체크
 			if (ballCnt == 0 && strikeCnt == 0) {
 				System.out.println("낫싱");
 			}
 
-			//정답
+			// 정답
 			if (strikeCnt == 3) {
 				System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 				int checkPlay = sc.nextInt();
 				if (checkPlay == 1) {
-					//
+					strAnswer = "";
+					continue;
 				} else if (checkPlay == 2) {
 					break;
-				} else {
-					//
 				}
 			}
 		}
