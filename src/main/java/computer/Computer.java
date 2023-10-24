3a629f70031cb1e97c33c47391690fb6b0c51446
34suuuuu <psh7492@gmail.com>
2023-10-24 17:55:24 +0900

    feat: 중복없는 3자리 난수 생성기능 추가

--- src/main/java/computer/Computer.java
+++ src/main/java/computer/Computer.java
@@ -1,2 +1,30 @@
-package computer;public class Computer {
+package computer;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import game.Ball;
+import static constants.NumConstants.*;
+
+public class Computer {
+
+    String randomString = new String();
+
+    public
+
+    // 3자리 난수 생성
+    public String getRandomNumbers(){
+        randomString = "";
+        while(randomString.length() < ANSWER_SIZE){
+            int randomNum = Randoms.pickNumberInRange(START_NUM, END_NUM);
+            if(!randomString.contains(String.valueOf(randomNum))){
+                randomString += String.valueOf(randomNum);
+            }
+        }
+        return randomString;
+    }
+
+
+
+
+
 }
