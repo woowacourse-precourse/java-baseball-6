ee0315e8eb0f94ede7493dea238dd1b6d20a578e
박승민 <tmddudals369@naver.com>
2023-10-24 18:09:05 +0900

    fix(gameOptionValidator): 게임 재시작 옵션 입력을 검증하는 기능 문제 해결

--- src/main/java/baseball/utils/GameOptionValidator.java
+++ src/main/java/baseball/utils/GameOptionValidator.java
@@ -7,7 +7,7 @@ public class GameOptionValidator {
     private static final String INVALID_RESTART_GAME_OPTION_ERROR_MESSAGE = "재시작 옵션은 1또는 2를 입력해야 합니다.";
 
     public static void validateGameRestartOption(String gamOption) {
-        if (Pattern.matches("^[1-2]$", gamOption)){
+        if (!Pattern.matches("^[1-2]$", gamOption)){
             throw new IllegalArgumentException(INVALID_RESTART_GAME_OPTION_ERROR_MESSAGE);
         }
     }
