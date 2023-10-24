54319d466fcfef389a33e3b54e54750ebd18afdf
kmw2378 <rlarla677@gmail.com>
2023-10-24 13:31:55 +0900

    feat: 출력되는 리터럴 문자열을 모아놓은 클래스 추가 View에서 사용되는 문자열들을 한 곳에 모아놓은 클래스를 추가하였습니다.

---
+++ src/main/java/baseball/view/message/GameMessage.java
@@ -0,0 +1,13 @@
+package baseball.view.message;
+
+public class GameMessage {
+    public static final String RESULT_BALL_FORMAT = "%d볼";
+    public static final String RESULT_REGEXP = " ";
+    public static final String RESULT_STRIKE_FORMAT = "%d스트라이크";
+    public static final String RESULT_NOTHING = "낫싱";
+    public static final String CONTINUE_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String GAME_OVER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public static final String QUESTION_MESSAGE = "숫자를 입력해주세요 : ";
+    public static final String EXIT_MESSAGE = "게임 종료";
+}
