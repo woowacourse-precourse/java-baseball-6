472a5326649ede5dd464377d0e4a0fd9749a0a1b
jongchan0109 <jongchan1009@naver.com>
2023-10-24 15:20:23 +0900

    feat: 사용자 입력 기능 구현

--- src/main/java/baseball/view/Input.java
+++ src/main/java/baseball/view/Input.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public class Input {
     public List<Integer> inputNumber(){
+        System.out.print(Speak.inputNumberSpeak);
         String inputString = Console.readLine();
         if(inputString.length()!=3){
             throw new IllegalArgumentException(Speak.inputIsUnvalid);
@@ -15,7 +16,7 @@ public class Input {
         List<Integer> inputList = new ArrayList<>();
         for(int i=0;i<3;i++) {
             char userInput = inputString.charAt(i);
-            if ('1' <= userInput && userInput <= '9'){
+            if ('1' <= userInput && userInput <= '9'&& !inputList.contains(Character.getNumericValue(userInput))){
                 inputList.add(Character.getNumericValue(userInput));
             }else {
                 throw new IllegalArgumentException(Speak.inputIsUnvalid);
@@ -23,7 +24,7 @@ public class Input {
         }
         return inputList;
     }
-    boolean startOrEnd(){
+    public boolean startOrEnd(){
         System.out.println(Speak.startOrEndSpeak);
         int choice = Integer.parseInt(Console.readLine());
 
