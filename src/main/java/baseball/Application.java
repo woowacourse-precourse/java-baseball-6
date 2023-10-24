52c05042c0a6426d45b28dc1c31b70b94305dabf
jihun-24k <gi020477@gmail.com>
2023-10-25 02:11:12 +0900

    Refactor: 랜덤값 생성, Integer값 변환을 Util 클래스로 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,13 +1,12 @@
 package baseball;
 
+import baseball.util.Util;
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        final List<Integer> computer = makeRandomNumbers();
+        final List<Integer> computer = Util.makeRandomNumbers();
 
         String input;
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -16,27 +15,8 @@ public class Application {
         while (true) {
             System.out.print("숫자를 입력해주세요 : ");
             input = Console.readLine();
-            List<Integer> inputNumbers = toIntegerArray(input);
+            List<Integer> inputNumbers = Util.toIntegerArray(input);
             break outer;
         }
-    }
-
-    private static List<Integer> toIntegerArray(String input) {
-        final List<Integer> inputList = new ArrayList<>();
-        for (String s : input.split("")) {
-            inputList.add(Integer.valueOf(s));
-        }
-        return inputList;
-    }
-
-    public static List<Integer> makeRandomNumbers() {
-        final List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
-        }
-        return computer;
     }
 }
