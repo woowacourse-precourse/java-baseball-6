bf951854bd7b931bc31f301af4c93b04663cf5b6
saera <yook.saera@gmail.com>
2023-10-25 04:02:10 +0900

    feat: Player가 사용자 입력을 검증하여 예외를 던지거나 정수 리스트로 변환하라

---
+++ src/main/java/baseball/Player.java
@@ -0,0 +1,26 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Player {
+    public List<Integer> player = new ArrayList<>();
+
+    public Player(String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+
+        char[] characterizedInput = input.toCharArray();
+        Set<Character> numberSet = new HashSet<>();
+
+        for (char number : characterizedInput) {
+            if (number < '1' || number > '9' || !numberSet.add(number)) {
+                throw new IllegalArgumentException();
+            }
+            player.add(number - '0');
+        }
+    }
+}
