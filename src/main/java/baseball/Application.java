985b7dbb60b2a237483c217711f668354a7ce545
seunghye218 <seunghye@student.42seoul.kr>
2023-10-24 14:56:50 +0900

    feat: init game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Integer> computer = getRandomList();
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    private static List<Integer> getRandomList() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 }
