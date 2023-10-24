83677a4d7e21ab960633e40ee71af36c74d118b5
ChanUi <hcu55@naver.com>
2023-10-25 03:30:32 +0900

    :sparkles: 숫자 야구 게임 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
 
+import baseball.controller.BaseballGame;
+import baseball.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OutputView.printGameStartMessage();
+        BaseballGame baseballGame = new BaseballGame();
+        baseballGame.start();
     }
 }
