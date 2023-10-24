127f8b22dba1cfa94038e1603bbaeaaf065c8e2f
julie0005 <julie0005@ajou.ac.kr>
2023-10-25 02:29:45 +0900

    feat: 1~9 중 임의의 수 3개를 선택하여 정답 생성하는 메소드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+    }
+
+    String makeRandomNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer.stream().map(Object::toString).collect(Collectors.joining());
     }
 }
