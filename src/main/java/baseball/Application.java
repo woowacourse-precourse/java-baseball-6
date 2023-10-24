a55f7adb9e8b685ca227b33a82d675e6da8323b3
happyryan <tjdgks120813@gmail.com>
2023-10-24 15:47:36 +0900

    feat: add GameController Class

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,23 +3,8 @@ import static baseball.Constants.*;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-    public  static  boolean askGameContinue(){
-        System.out.println(continueGameMessage);
-        String continueCommand = Console.readLine();
-        ExceptionHandler.raiseContinueCommandException(continueCommand);
-
-        if (continueCommand.equals(gameStop)) {
-            System.out.println(endGameMessage);
-            return true;
-        }
-        return false;
-    }
     public static void main(String[] args){
-        System.out.println(startGameMessage);
-        while (true) {
-            Game baseBallGame = new Game();
-            baseBallGame.play();
-            if(askGameContinue()) break;
-        }
+        GameController baseBallGameController = new GameController();
+        baseBallGameController.runGame();
     }
 }
--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -1,2 +1,29 @@
-package baseball;public class GameController {
+package baseball;
+
+import static baseball.Constants.*;
+import camp.nextstep.edu.missionutils.Console;
+
+public class GameController {
+    public GameController(){
+        System.out.println(startGameMessage);
+    }
+    void runGame(){
+        while (true) {
+            Game baseBallGame = new Game();
+            baseBallGame.play();
+            if(askGameContinue()) return;
+        }
+    }
+    boolean askGameContinue(){
+        System.out.println(continueGameMessage);
+
+        String continueCommand = Console.readLine();
+        ExceptionHandler.raiseContinueCommandException(continueCommand);
+
+        if (continueCommand.equals(gameStop)) {
+            System.out.println(endGameMessage);
+            return true;
+        }
+        return false;
+    }
 }
