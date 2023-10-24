19b5ef293807c6c11bfc4150ea883ea591bbe4bf
MingyeomKim <mingyum119@naver.com>
2023-10-24 11:12:01 +0900

    feat(Computer): 저장된 수와 입력된 수를 비교해서 힌트를 생성하는 로직 구현
    Computer 객체에 저장된 savedNumbers와 새로 입력된 inputNumbers를 비교하기 위해, Computer 클래스의 멤버 함수로 provideHint를 구현한다.

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -4,8 +4,14 @@ import java.util.List;
 
 public class Computer {
     private Numbers savedNumbers = new Numbers();
+    private Hint hint = new Hint();
 
     public void saveNumbers(List<Integer> generatedNumbers) {
         savedNumbers.from(generatedNumbers);
+    }
+
+    public String provideHint(Numbers inputNumbers) {
+        hint.compareNumbers(savedNumbers, inputNumbers);
+        return hint.generateHint();
     }
 }
