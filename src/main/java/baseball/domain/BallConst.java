e373f259ec3cbe50108641ebbb54424023c5481d
LeeHuSung <mn040820@naver.com>
2023-10-24 19:08:42 +0900

    feat(Common, Input, BallConst, GameConst, GameService) 게임 관련 필요 메서드 구현

---
+++ src/main/java/baseball/domain/BallConst.java
@@ -0,0 +1,23 @@
+package baseball.domain;
+
+public final class BallConst {
+
+
+    public static final int RANDOM_MIN_NUM = 1;
+    public static final int RANDOM_MAX_NUM = 9;
+    public static final int BALL_COUNT = 3;
+    public static final int MAX_STRIKE = 3;
+    public static final int NOTHING = 0;
+
+
+    public static final String NOTHING_MESSAGE = "낫싱";
+    public static final String BALL_MESSAGE = "볼 ";
+    public static final String STRIKE_MESSAGE = "스트라이크";
+
+    public static final String STRIKE_THREE = "3스트라이크";
+    public static final String STRIKE_THREE_GAME_SET = "3개의 스트라이크를 모두 맞히셨습니다! 게임 종료";
+    public static final String RETRY_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요";
+
+    public static final int RETRY_YES = 1;
+    public static final int RETRY_NO = 2;
+}
