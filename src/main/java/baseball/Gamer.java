c9c6348723153a461a1f8a903893d4f0fdfcef2e
pletain <pletain@dgu.ac.kr>
2023-10-25 02:50:47 +0900

    Feat: Gamer 클래스 추가 게이머의 숫자 입력과 검증에 관한 클래스

---
+++ src/main/java/baseball/Gamer.java
@@ -0,0 +1,10 @@
+package baseball;
+import java.util.List;
+public class Gamer {
+
+    private List<Integer> number;
+
+    public List<Integer> getNumber() {
+        return this.number;
+    }
+}
