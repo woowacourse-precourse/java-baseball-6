d3b4d39e0c0f458eb63d33e91493ad16706d2241
leedohyun <lucky77795@naver.com>
2023-10-24 21:33:10 +0900

    refactor: 필요한 상수 추가

--- src/main/java/baseball/resources/GameConst.java
+++ src/main/java/baseball/resources/GameConst.java
@@ -7,7 +7,11 @@ public class GameConst {
     public static final int ZERO = 0;
     public static final int ALL_STRIKE = 3; //게임이 종료되는 스트라이크 개수
     public static final int WANT_RESTART = 1; //재시작을 위한 값
-    public static final int WANT_END = 2; //종료를 위한 값
+    public static final int WANT_EXIT = 2; //종료를 위한 값
+    public static final boolean GAME_OVER = true; //게임 성공
+    public static final boolean NOT_GAME_OVER = false; //게임 실패
+    public static final boolean RESTART = true; //재시작
+    public static final boolean EXIT_GAME = false; //게임종료
 
     public GameConst() {
     }
