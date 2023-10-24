59d1a6504a07c9e764f26fa77c612066c1a3dfc5
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2023-10-24 17:48:08 +0900

    feat: 볼과 스트라이크에 따라 결과를 출력한다.
    볼과 스트라이크 개수에 따라 정해진 형식에 맞춰 결과를 출력한다.

--- src/main/java/baseball/model/baseballCount.java
+++ src/main/java/baseball/model/baseballCount.java
@@ -42,4 +42,22 @@ public class baseballCount {
             }
         }
     }
+
+    public void printCountResult(int ball, int strike) {
+        if(strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+            return ;
+        }
+        if(strike > 0 && ball > 0) {
+            System.out.printf("%d볼 %d스트라이크\n", ball, strike);
+            return;
+        }
+        if(strike > 0 && ball == 0) {
+            System.out.printf("%d스트라이크\n", strike);
+            return;
+        }
+        if(ball > 0 && strike == 0) {
+            System.out.printf("%d볼\n", ball);
+        }
+    }
 }
