1b4a540837e1a1f34991f319377351bc39116ce5
KWY <wodrk1001@gmail.com>
2023-10-24 22:29:48 +0900

    [feat] 야구 게임 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,24 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameModel gameModel = new GameModel(new Computer(""));
+        GameView gameView = new GameView();
+        boolean isGameExit = false;
+        do {
+            boolean isGameContinue = true;
+            gameModel.setRandomNumberInComputer();
+            gameView.printStartGame();
+            while (isGameContinue) {
+                String userNumber = gameView.getUserNumberInput();
+
+                ResultAndView resultAndView = gameModel.compareUserAndComputerNumber(userNumber);
+                gameView.printResult(resultAndView);
+
+                isGameContinue = resultAndView.getIsContinue();
+            }
+
+            String userInput = gameView.getUserReplayInput();
+            if (gameModel.isGameExit(userInput)) isGameExit = true;
+        } while (!isGameExit);
     }
 }
