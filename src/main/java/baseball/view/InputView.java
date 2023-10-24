2e293bc240d331b2ee572ee023bc320f48e98dcb
hyunsu4020 <dlgustn4020@naver.com>
2023-10-24 21:22:06 +0900

    입력 한ì¤ìinput 값 íãì숫자 입력값 한줄 띄움 삭제(println -> print)

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     public static String setUserNumber() {
-        System.out.println("숫자를 입력해주세요 : ");
+        System.out.print("숫자를 입력해주세요 : ");
         return Console.readLine();
     }
 
