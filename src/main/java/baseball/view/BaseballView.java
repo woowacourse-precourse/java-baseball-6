af510c29713f8f6e5ca9ec696a264b5517be1960
www-author <dream970215@naver.com>
2023-10-24 17:58:38 +0900

    :sparkles: feat: 게임 결과 및 사용자 입력 메세지 출력 뷰 생성

---
+++ src/main/java/baseball/view/BaseballView.java
@@ -0,0 +1,35 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class BaseballView {
+
+    public void printGameStartMessage(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void printUserInputGuideMessage(){
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
+    public void printGameOverMessage() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public void printWhetherRestartOrExitMessage() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    public void printResultGameMessage(String message) {
+        System.out.println(message);
+    }
+
+    public String getUserInput() {
+        printUserInputGuideMessage();
+        return Console.readLine();
+    }
+
+    public String getGameMenu() {
+        return Console.readLine();
+    }
+}
