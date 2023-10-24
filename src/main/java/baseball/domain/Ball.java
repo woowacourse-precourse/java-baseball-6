141e212f4046fb2f8a7f4e079d3870fafb03c4fd
olsohee <dlthgml0108@daum.net>
2023-10-24 11:33:09 +0900

    feat: 사용자가 입력하는 수 3개를 의미하는 Ball 클래스 생성

---
+++ src/main/java/baseball/domain/Ball.java
@@ -0,0 +1,12 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Ball {
+
+    private final List<Integer> balls;
+
+    public Ball(List<Integer> balls) {
+        this.balls = balls;
+    }
+}
