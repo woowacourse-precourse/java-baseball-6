dca806ab502eff30588e314f2c3bfd385b2b3854
kimmokalover <kimmokalover@gmail.com>
2023-10-24 13:50:49 +0900

    feat : UserInputService 구현 및 RandomGeneratorServie 추가

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -2,4 +2,8 @@ package baseball.controller;
 
 public class GameController {
 
+    public void getUserInput(){
+
+    }
+
 }
--- src/main/java/baseball/service/GameService.java
+++
@@ -1,5 +0,0 @@
-package baseball.service;
-
-public class GameService {
-
-}
