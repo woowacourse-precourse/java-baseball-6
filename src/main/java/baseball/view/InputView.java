000773f4e35c1d1944e8b8db80f5bfff206b43f9
Choo <choo000407@naver.com>
2023-10-24 15:35:27 +0900

    style(InputView): 시작줄 개행 제거

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -3,7 +3,6 @@ package baseball.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-
     private static final String SUGGESTED_NUMBER_PROMPT = "숫자를 입력해주세요 : ";
     private static final String GAME_CONTINUE_CHOICE_PROMPT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
