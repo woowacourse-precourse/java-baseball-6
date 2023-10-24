65fdb9fec9f66260d7383350d206e5dadfb40947
Woo-Shinyoung <tlsdud8205@naver.com>
2023-10-24 22:52:12 +0900

    Feat[domain] 게임 재시작 여부 확인 로직 생성

--- src/main/java/baseball/domain/BaseballRestartOption.java
+++ src/main/java/baseball/domain/BaseballRestartOption.java
@@ -1,4 +1,21 @@
 package baseball.domain;
 
 public enum BaseballRestartOption {
+    RETRY(1),
+    QUIT(2);
+
+    private static final String OPTION_ERROR_MESSAGE = "1 혹은 2의 숫자만 입력이 가능합니다.";
+    private final int option;
+
+    BaseballRestartOption(int option) {
+        this.option = option;
+    }
+    public static BaseballRestartOption from(int input) {
+        for (BaseballRestartOption option : values()) {
+            if (option.option == input) {
+                return option;
+            }
+        }
+        throw new IllegalArgumentException(OPTION_ERROR_MESSAGE);
+    }
 }
