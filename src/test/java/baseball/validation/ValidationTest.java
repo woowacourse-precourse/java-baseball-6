fd41f6696baae8e832ac7bcf5986679fb9c6296c
Hyeon0208 <dmsgktn0208@gmail.com>
2023-10-24 15:02:01 +0900

    feat : 재시작 명령어 예외 테스트 추가

--- src/test/java/baseball/validation/ValidationTest.java
+++ src/test/java/baseball/validation/ValidationTest.java
@@ -1,9 +1,12 @@
 package baseball.validation;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import baseball.Application;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.Arrays;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ValidationTest extends NsTest {
@@ -30,6 +33,15 @@ public class ValidationTest extends NsTest {
     void 입력값_중복_예외_테스트() {
         assertThatThrownBy(() -> runException("737"))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 재시작_명령어_예외_테스트() {
+        assertThatThrownBy(() -> assertRandomNumberInRangeTest(() -> {
+                    run("798", "3");
+                },
+                7, 9, 8
+        )).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Override
