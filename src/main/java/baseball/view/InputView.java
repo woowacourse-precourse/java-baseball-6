9377a7c3651eb38355df00dcb2d4ce73e496148a
Woo-Shinyoung <tlsdud8205@naver.com>
2023-10-24 22:08:31 +0900

    Feat[view] 플레이어 숫자 입력

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,4 +1,12 @@
 package baseball.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class InputView {
+    public String readPlayerNumbers() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = readLine();
+        return input;
+    }
 }
+
