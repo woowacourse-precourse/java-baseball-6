dbd718399c9c7600338985045358073ecdc2bef4
Juri Oh <julioh0603@gmail.com>
2023-10-25 05:35:37 +0900

    게임 결과
    - if문으로 4가지 조건문 생성

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -68,6 +68,19 @@ public class Game extends Text {
             }
         }
     }
+
+    public void gameResult() {
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+        }
+        if (ball != 0 && strike != 0) {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        } else if (ball != 0) {
+            System.out.println(ball + "볼 ");
+        } else if (strike != 0) {
+            System.out.println(strike + "스트라이크");
+        }
+    }
 }
 
 
