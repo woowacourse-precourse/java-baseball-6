8bedefe843d70742548bcce554abe54405fb9665
wonjun <wonjon0930@daum.net>
2023-10-24 18:25:11 +0900

    feat: add feature to print baseball result

---
+++ src/main/java/baseball/Output.java
@@ -0,0 +1,35 @@
+package baseball;
+
+import java.util.Map;
+
+public class Output {
+    public void printBaseballResult(Map<String, Integer> baseballResult){
+        int strikeCount = baseballResult.get("strike");
+        int ballCount = baseballResult.get("ball");
+        printStrikeAndBall(strikeCount, ballCount);
+    }
+
+    private void printStrikeAndBall(int strikeCount, int ballCount){
+       if(strikeCount == 0 && ballCount == 0){
+           System.out.println("낫싱");
+           return;
+       }
+       if(strikeCount == 3){
+           System.out.println("3스트라이크");
+           System.out.println("3개의 숫자를 모두 맞추셨습니다! 게임 종료");
+           System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+           return;
+       }
+       if(strikeCount == 0){
+           System.out.println(ballCount + "볼");
+           return;
+       }
+       if(ballCount == 0){
+           System.out.println(strikeCount + "스트라이크");
+           return;
+       }
+       if(ballCount > 0 && strikeCount > 0){
+           System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+       }
+    }
+}
