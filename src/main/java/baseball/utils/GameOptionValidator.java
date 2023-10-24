2f7fc33961597010de755efb7ff260c42a93324e
박승민 <tmddudals369@naver.com>
2023-10-25 01:49:04 +0900

    refactor(gameOptionValidator): 올바른 게임 재시작 옵션인지 검증하는 메서드의 기능 분리

--- src/main/java/baseball/utils/GameOptionValidator.java
+++ src/main/java/baseball/utils/GameOptionValidator.java
@@ -7,6 +7,10 @@ public class GameOptionValidator {
     private static final String INVALID_RESTART_GAME_OPTION_ERROR_MESSAGE = "재시작 옵션은 1또는 2를 입력해야 합니다.";
 
     public static void validateGameRestartOption(String gamOption) {
+        isValidRestartOption(gamOption);
+    }
+
+    private static void isValidRestartOption(String gamOption) {
         if (!Pattern.matches("^[1-2]$", gamOption)){
             throw new IllegalArgumentException(INVALID_RESTART_GAME_OPTION_ERROR_MESSAGE);
         }
