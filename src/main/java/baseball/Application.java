3716a80467e056eb2aa826289e12e9c106d5488d
wooni97 <jhw970714@gmail.com>
2023-10-25 04:21:33 +0900

    feat(Main): add game initation code in main method
    - 이 커밋에서는 게임을 시작하기 위해 메인 메소드에 필요한 코드를 추가했습니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
 
+
+import baseball.controller.GameController;
+import baseball.entity.Computer;
+import baseball.entity.GameLogic;
+import baseball.view.GameIO;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameController gameController = new GameController(new GameIO(), new Computer(), new GameLogic());
+        gameController.init();
+        gameController.runGame();
     }
 }
