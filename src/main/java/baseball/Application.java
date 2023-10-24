875d9ae9bfce3c527c901ffd8de0c32593174f11
Haerang <jbu418@naver.com>
2023-10-24 21:44:38 +0900

    기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        game play = new game();
+        do {
+            play.init();
+            play.ran();
+            while (play.finishCheck() == 0) {
+                play.userInput();
+                play.compareNumbers();
+                play.printResult();
+            }
+        } while (play.playerChoice() == 1);
     }
 }
