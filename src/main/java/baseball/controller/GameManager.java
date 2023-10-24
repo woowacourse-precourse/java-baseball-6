cbcb339fbe735aaa512e94598f2476fc7b3d7d31
“sangjun121” <juncho12011201@gmail.com>
2023-10-24 16:05:01 +0900

    feat: 사용자의 입력을 받고 GameManager로 데이터를 보내는 기능 구현 dquote> dquote> readLine으로 입력받은 문자열을 List<Integer>의 원소로 바꾸고 UserNumberDto를 이용하여 GameManager로 반환하는 기능 구현 완료

--- src/main/java/baseball/controller/GameManager.java
+++ src/main/java/baseball/controller/GameManager.java
@@ -1,19 +1,24 @@
 package baseball.controller;
 
+import baseball.model.UserNumberDto;
 import baseball.view.UserView;
 import java.util.List;
 
 public class GameManager {
     //인스턴스
     private Computer computer;
+    private User user;
     private UserView userView;
 
     //변수명
     private List<Integer> computerRandomNumbers;
 
+    private UserNumberDto userNumberDto;
+
     //생성자
     public GameManager(){
         computer = new Computer();
+        user = new User();
         userView = new UserView();
     };
 
@@ -21,6 +26,6 @@ public class GameManager {
         computerRandomNumbers = computer.getRandomComputerNumber(); //랜덤한 3개 컴퓨터 숫자 생성
         userView.printGameStartMessage(); //게임시작 메시지 출력
         userView.printNumberInputPrompt(); //user input 프롬포트 메시지 출력
-
+        userNumberDto = user.readUserNumberInput(); //user input 시행
     }
 }
---
+++ src/main/java/baseball/controller/User.java
@@ -0,0 +1,32 @@
+package baseball.controller;
+
+import baseball.model.UserNumberDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static baseball.utility.Constants.NUMBER_LIMIT_LENGTH;
+
+public class User {
+    private List<Integer> userNumbers;
+    private String userInput;
+    private UserNumberDto userNumberDto;
+
+    //생성자
+    public User(){
+        userNumbers = new ArrayList<>(3);
+        userNumberDto = new UserNumberDto();
+    }
+    public UserNumberDto readUserNumberInput(){
+        userInput = readLine();
+
+        for(int i = 0; i < NUMBER_LIMIT_LENGTH; i++){
+            userNumbers.add(userInput.charAt(i) - '0'); //사용자가 입력한 숫자 문자열을 문자단위로 나누어 Integer 리스트에 대입
+        }
+
+        userNumberDto.setUserNumber(userNumbers); //model DTO에 담아 다른 클래스로 넘겨주기
+
+        return userNumberDto;
+    }
+}
