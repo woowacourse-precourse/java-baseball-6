2c2218bae563fb7e834b7385521844f62101a14e
gmelon <hsh1769@naver.com>
2023-10-25 01:22:10 +0900

    fix: 게임 결과 계산 시 Strike임에도 Ball로 중복 카운트되는 오류 수정

--- src/main/java/baseball/domain/Balls.java
+++ src/main/java/baseball/domain/Balls.java
@@ -49,33 +49,31 @@ public class Balls {
         }
     }
 
-    public GuessResult guess(Balls other) {
+    public GuessResult guess(Balls otherBalls) {
         GuessResult guessResult = GuessResult.empty();
 
-        for (int thisBallIndex = 0; thisBallIndex < VALID_BALL_COUNT; thisBallIndex++) {
-            compareAndGenerateResults(thisBallIndex, other, guessResult);
+        for (int otherBallIndex = 0; otherBallIndex < VALID_BALL_COUNT; otherBallIndex++) {
+            BallStatus ballStatus = compare(otherBallIndex, otherBalls.ballAt(otherBallIndex));
+            guessResult.add(ballStatus);
         }
 
         return guessResult;
     }
 
-    private void compareAndGenerateResults(int thisBallIndex, Balls other, GuessResult guessResult) {
-        for (int otherBallIndex = 0; otherBallIndex < VALID_BALL_COUNT; otherBallIndex++) {
-            BallStatus ballStatus = BallStatus.of(isValueMatches(thisBallIndex, otherBallIndex, other),
-                    isIndexMatches(thisBallIndex, otherBallIndex));
-            guessResult.add(ballStatus);
+    private BallStatus compare(int otherBallIndex, Ball otherBall) {
+        if (this.ballAt(otherBallIndex).equals(otherBall)) {
+            return BallStatus.STRIKE;
         }
+
+        return balls.stream()
+                .filter(thisBall -> thisBall.equals(otherBall))
+                .findAny()
+                .map(thisBall -> BallStatus.BALL)
+                .orElse(BallStatus.NONE);
     }
 
-    private boolean isValueMatches(int thisBallIndex, int otherBallIndex, Balls other) {
-        Ball thisBall = this.balls.get(thisBallIndex);
-        Ball otherBall = other.balls.get(otherBallIndex);
-
-        return thisBall.equals(otherBall);
-    }
-
-    private boolean isIndexMatches(int thisBallIndex, int otherBallIndex) {
-        return thisBallIndex == otherBallIndex;
+    private Ball ballAt(int index) {
+        return balls.get(index);
     }
 
     @Override
--- src/main/java/baseball/domain/strategy/RandomBallNumbersGenerateStrategy.java
+++ src/main/java/baseball/domain/strategy/RandomBallNumbersGenerateStrategy.java
@@ -3,24 +3,30 @@ package baseball.domain.strategy;
 import baseball.domain.Ball;
 import baseball.domain.Balls;
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class RandomBallNumbersGenerateStrategy implements BallNumbersGenerateStrategy {
 
     @Override
     public String generate() {
-        Set<Integer> randomNumbers = new HashSet<>(Balls.VALID_BALL_COUNT);
+        List<Integer> randomNumbers = new ArrayList<>(Balls.VALID_BALL_COUNT);
 
         while (randomNumbers.size() < Balls.VALID_BALL_COUNT) {
             int randomNumber = Randoms.pickNumberInRange(Ball.MINIMUM_VALID_NUMBER, Ball.MAXIMUM_VALID_NUMBER);
-            randomNumbers.add(randomNumber);
+            addIfNotContains(randomNumbers, randomNumber);
         }
 
         return randomNumbers.stream()
                 .map(String::valueOf)
                 .collect(Collectors.joining());
+    }
+
+    private void addIfNotContains(List<Integer> randomNumbers, int randomNumber) {
+        if (!randomNumbers.contains(randomNumber)) {
+            randomNumbers.add(randomNumber);
+        }
     }
 
 }
