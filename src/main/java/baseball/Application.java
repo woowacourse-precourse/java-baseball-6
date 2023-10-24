f1f31aba71fdf8dfb9405ccfe6e3dd7217b4a9f3
jagarchi <relong1026@naver.com>
2023-10-24 21:03:54 +0900

    1이나 2를 입력했을때 오류가 발생하게 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -25,6 +25,9 @@ public class Application {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             gameRestart = Console.readLine();
+            if(!gameRestart.equals("1") && !gameRestart.equals("2")){
+                throw new IllegalArgumentException("1이나 2를 입력해주세요");
+            }
             result = ""; // rseult 초기화
         }
 
