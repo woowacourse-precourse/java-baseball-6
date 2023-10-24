b58e4354bfeaec835ace9c85d542d167fbe75b17
SungyoonRyu <rara0620@naver.com>
2023-10-25 01:54:27 +0900

    feat : add method that create 3 random number

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,15 +1,47 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
+    private final List<Integer> answerList = new ArrayList<>();
 
     private void init() {
-
+        answerList.clear();
+        while (answerList.size() < 3) {
+            // 랜덤한 1부터 9의 서로 다른 3자리의 수 생성
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!answerList.contains(randomNumber)) {
+                answerList.add(randomNumber);
+            }
+        }
     }
+
     public void run() {
+        boolean isRun = true;
+        while(isRun) {
+            // 게임 초기화
+            init();
+
+            playingGame();
+
+            isRun = checkPlayAgain();
+        }
+    }
+
+    private void playingGame() {
+        while(true) {
+
+        }
+    }
+
+    private void checkAnswer() {
 
     }
 
-    public void playingGame() {
-
+    private boolean checkPlayAgain() {
+        return false;
     }
 }
