3d27aeace31777069b9b1cdc7f1ac02c725bfe26
COBI-98 <tkdgus9688@gmail.com>
2023-10-25 06:40:18 +0900

    feat: 숫자 야구 게임 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,5 +5,6 @@ import baseball.controller.GamePlayController;
 public class Application {
     public static void main(String[] args) {
         GamePlayController gamePlayController = new GamePlayController();
+        gamePlayController.gameStart();
     }
 }
