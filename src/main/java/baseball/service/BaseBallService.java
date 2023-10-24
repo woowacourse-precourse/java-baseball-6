323e03d94b1776ead0b9200db596d9d326cf3c22
Hayoon <hy97@sju.ac.kr>
2023-10-24 19:11:31 +0900

    feat(BaseBallService): notificationResult 스트라이크/볼 카운트 갯수에 대한 콘솔 알림기능을 한다

--- src/main/java/baseball/service/BaseBallService.java
+++ src/main/java/baseball/service/BaseBallService.java
@@ -3,6 +3,9 @@ package baseball.service;
 import baseball.domain.BaseBall;
 import baseball.repository.BaseBallRepository;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.IntStream;
@@ -26,14 +29,31 @@ public class BaseBallService {
         for(int i = 0; i < 3; i++) {
             for(int j = 0; j < 3; j++) {
                 if(playerBallNumbers.get(i).equals(computerBallNumbers.get(j)) && i == j) {
-                    score[0]++;
+                    score[1]++;
                     break;
                 } else if(playerBallNumbers.get(i).equals(computerBallNumbers.get(j)) && i != j) {
-                    score[1]++;
+                    score[0]++;
                     break;
                 }
             }
         }
         return score;
+    }
+
+    public String notificationResult(int[] result) {
+        if(result[0] > 0 && result[1] > 0) {
+            return String.format("%d볼 %d스트라이크\n", result[0], result[1]);
+        } else if(result[0] > 0) {
+            return String.format("%d볼\n", result[0]);
+        } else if(result[1] > 0) {
+            return String.format("%d스트라이크\n", result[1]);
+        } return "낫싱";
+    }
+
+    public int restartGame() throws IOException {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        return Integer.parseInt(br.readLine());
     }
 }
