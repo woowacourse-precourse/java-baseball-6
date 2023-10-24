d5523988a217bd7a01bdc33b9b477c1cd2f003d3
Kaeun-Lee <lgeun7026@hanmail.net>
2023-10-25 06:09:21 +0900

    feat: Add logic to generate unique random numbers

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
+
+        // computer
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
         System.out.print("숫자를 입력해주세요 : ");
         System.out.println("숫자는 3자리만 입력할 수 있습니다.");
         System.out.println(1 + "볼 " + 1 + "스트라이크");
