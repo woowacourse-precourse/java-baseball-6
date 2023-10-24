8f4388f0249ea5dfe0fc8569f884a619e0bfc022
BDlhj <akk808@naver.com>
2023-10-24 11:52:33 +0900

    Comment: Write steps of program in src/main/java/baseball/Application.java

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,14 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 게임 시작 문구 출력
+        // do-while
+        // 컴퓨터 정답 생성
+        // 플레이어 입력값 받기
+        // 플레이어 입력값 유효성 검사
+        // 판정 실시
+        // 게임 종료 문구 출력
+        // 재시작 혹은 최종 종료 문구 출력
+        // 플레이어 입력값 받기
     }
 }
