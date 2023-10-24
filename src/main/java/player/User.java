9754a1b750e14e0df57e63633d3881802089b82d
yj kim <yjee0215@gmail.com>
2023-10-24 21:23:17 +0900

    refeat: User validation, 세부사항 리팩토링
    validation 내부 메소드 추가, 개행문자 추가, 변수 이름 변경

--- src/main/java/player/User.java
+++ src/main/java/player/User.java
@@ -3,18 +3,21 @@ package player;
 import camp.nextstep.edu.missionutils.Console;
 
 public class User implements Player {
-    private char[] number;
+    private char[] numbers;
 
     //todo: number 초기화 고려
     public User() {
-        number = new char[3];
+        numbers = new char[3];
     }
 
     public boolean chooseGameEnd() {
-        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
 
-        //todo: validate
-        int choice = Integer.parseInt(Console.readLine());
+        String userChoiceInput=Console.readLine();
+        endInputStringValidation(userChoiceInput);
+
+        int choice = Integer.parseInt(userChoiceInput);
+        endInputNumberValidation(choice);
 
         if (choice == 0)
             return false;
@@ -22,13 +25,37 @@ public class User implements Player {
         return true;
     }
 
-    @Override
-    public void setNumber() {
-        //validation
-        number = Console.readLine().toCharArray();
+    private void endInputStringValidation(String userInput){
+        if(userInput.length()!=1)
+            throw new IllegalArgumentException("user input is too many");
     }
 
+    private void endInputNumberValidation(int choice){
+        if(choice<0 || choice>1)
+            throw new IllegalArgumentException("user input is not appropriate");
+    }
+
+
+    @Override
+    public void setNumber() {
+        char[] userNumbers=Console.readLine().toCharArray();
+
+        numberValidation(userNumbers);
+        numbers=userNumbers;
+    }
+
+    private void numberValidation(char[] numbersInput){
+        if(numbersInput.length!=3)
+            throw new IllegalArgumentException("user input number size is not appropriate");
+
+        for(char number:numbersInput){
+            if(number<='0' || number>'9')
+                throw new IllegalArgumentException("user input is not appropriate");
+        }
+    }
+
+    //todo: private으로 접근할 순 없을까?
     public char getNumber(int index) {
-        return number[index];
+        return numbers[index];
     }
 }
