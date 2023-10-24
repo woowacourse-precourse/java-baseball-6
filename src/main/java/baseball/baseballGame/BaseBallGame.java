d70255ad6ec2802dd7e5ba2cdac40cd4303c61bc
YuJeongYun <yunyj99@naver.com>
2023-10-25 01:01:19 +0900

    refactor: 랜덤한 숫자 선택 기능 수정
    게임 재시작 시 클래스 새로 생성 없이 진행할 수 있도록 랜덤한 숫자 선택 기능을 생성자가 아니라 함수에서 처리하도록 수정

--- src/main/java/baseball/baseballGame/BaseBallGame.java
+++ src/main/java/baseball/baseballGame/BaseBallGame.java
@@ -1,20 +1,31 @@
 package baseball.baseballGame;
 
 import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.mockito.internal.stubbing.answers.ThrowsException;
 
 public class BaseBallGame {
     private int cnt; // 맞춰야 할 숫자의 개수
     private int computerNum; // 컴퓨터가 선택한 숫자
+    PrintConsole print = new PrintConsole(); // 콘솔 출력 위한 참조변수
+    CompareNum compareNum = new CompareNum(); // 숫자 비교 위한 참조변수
 
     public BaseBallGame() {
     }
 
     public BaseBallGame(int cnt) {
         this.cnt = cnt;
+    }
 
+    public int getCnt() {
+        return cnt;
+    }
+
+    public int getComputerNum() {
+        return computerNum;
+    }
+
+    // 랜덤한 숫자 생성
+    public void init() {
         int tmp = 0; // 현재 선택한 숫자 개수
         boolean[] ch = new boolean[10]; // 중복된 숫자 체크 위한 배열
 
@@ -30,23 +41,23 @@ public class BaseBallGame {
         }
     }
 
-    public int getCnt() {
-        return cnt;
-    }
-
-    public int getComputerNum() {
-        return computerNum;
-    }
-
     // 숫자 야구 게임 시작
     public void start() {
-        PrintConsole print = new PrintConsole();
-        CompareNum compareNum = new CompareNum();
+        init();
 
         print.printStart();
-        int num = print.printInput();
-        check(num);
+        while (true) {
+            int num = print.printInput();
+            print.printOutput(compareNum.compare(this.computerNum, num));
+
+            if (this.computerNum == num) {
+            }
+
+
+        }
     }
+
+
 
     // 컴퓨터 숫자와 사용자 입력 숫자 비교
     private void check(int num) {
