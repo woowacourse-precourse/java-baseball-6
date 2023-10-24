5a50e0465cadf4eeb39d696084e12aa383a00726
gyu_hyeok <jjookki33@gmail.com>
2023-10-25 02:00:59 +0900

    fix/검증 기능 함수의 주석 제거

--- src/main/java/baseball/service/ValidationInput.java
+++ src/main/java/baseball/service/ValidationInput.java
@@ -10,7 +10,6 @@ public class ValidationInput {
         return choice;
     }
 
-    //사용자 선택 검증 함수
     private void isValidChoice(String choice){
         if(!(choice.equals("1") || choice.equals("2"))){
             throw new IllegalArgumentException("사용자의 입력이 1또는 2가 아닙니다. 게임을 종료합니다.");
