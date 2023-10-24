2c6488e844607f6c08c272ab03fd4600a6e089a4
yoonjuho <dbswn990@naver.com>
2023-10-25 04:21:21 +0900

    refactor(Application): 변수 이름 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,19 +5,18 @@ import baseball.controller.Game;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 
-import java.util.List;
-
 public class Application {
     private static final InputView INPUT_VIEW = new InputView();
     private static final OutputView OUTPUT_VIEW = new OutputView();
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         GameStatus gameStatus;
-        OUTPUT_VIEW.printGameBegin();
-        do{
-            new Game().play(INPUT_VIEW.inputGameNumbers());
-            gameStatus = GameStatus.find(INPUT_VIEW.inputRestartNumber());
+        OUTPUT_VIEW.printGameStart();
+        do {
+            new Game().play(INPUT_VIEW.inputNumbers());
+            gameStatus = GameStatus.apply(INPUT_VIEW.inputRestartOrNot());
         }
-        while(gameStatus != GameStatus.EXIT);
+        while (gameStatus != GameStatus.QUIT);
     }
 }
