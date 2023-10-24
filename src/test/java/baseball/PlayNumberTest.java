5068d0ada7eed17d7407fd3a1dd118b306bbfe84
devholic22 <hyunjoon.tech@gmail.com>
2023-10-25 01:21:07 +0900

    Test: PlayNumber 테스트 시 예외 메시지가 같게 나오는지도 추가

--- src/test/java/baseball/PlayNumberTest.java
+++ src/test/java/baseball/PlayNumberTest.java
@@ -4,27 +4,38 @@ import baseball.model.number.PlayNumber;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PlayNumberTest {
 
     @Test
     void 플레이_숫자는_세자리여야만_한다() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+        // given & when
+        Throwable exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
             PlayNumber playNumber = PlayNumber.from("1234");
         });
+        // then
+        assertThat(exception.getMessage()).isEqualTo("길이는 3이어야 합니다.");
     }
 
     @Test
     void 플레이_숫자는_숫자여야만_한다() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+        // given & when
+        Throwable exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
             PlayNumber playNumber = PlayNumber.from("12a");
         });
+        // then
+        assertThat(exception.getMessage()).isEqualTo("1 ~ 9 사이의 값이어야만 합니다.");
     }
 
     @Test
     void 플레이_숫자는_중복되면_안된다() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+        // given & when
+        Throwable exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
             PlayNumber playNumber = PlayNumber.from("111");
         });
+        // then
+        assertThat(exception.getMessage()).isEqualTo("숫자들이 중복되면 안 됩니다.");
     }
 
     @Test
