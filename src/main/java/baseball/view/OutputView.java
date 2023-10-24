1d86db4d3406f93060f50db3d574e0be0602ff08
Woo-Shinyoung <tlsdud8205@naver.com>
2023-10-24 22:35:33 +0900

    Feat[view] 게임 결과 문구 생성

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -6,3 +6,8 @@ public class OutputView {
     public void printGameStart(){
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
+    public void printGameResult(BaseballResult baseballResult){
+        System.out.println(baseballResult.toString());
+    }
+}
