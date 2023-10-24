d9c6c0a2481dc31144f9c14e2eaf6ed2e6a1ebde
inpink <inpink@kakao.com>
2023-10-24 18:18:49 +0900

    test: IntegerUtil의 isInteger, validateInteger 테스트

--- src/test/java/baseball/util/IntegerUtilTest.java
+++ src/test/java/baseball/util/IntegerUtilTest.java
@@ -1,11 +1,25 @@
 package baseball.util;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 public class IntegerUtilTest {
 
+    @Test
+    public void isInteger_Integer() {
+        assertTrue(IntegerUtil.isInteger("123"));
+        assertTrue(IntegerUtil.isInteger("-456"));
+    }
+
+    @Test
+    public void isInteger_NonInteger() {
+        assertFalse(IntegerUtil.isInteger("abc"));
+        assertFalse(IntegerUtil.isInteger("12.34"));
+        assertFalse(IntegerUtil.isInteger("12345678901234567890"));
+    }
 
     @Test
     void validateInteger_Integer() {
