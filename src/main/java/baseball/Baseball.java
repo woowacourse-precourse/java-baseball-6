bdd7aa3900e3024527e7dcf1e3a301dc818685d9
dbstn1325 <dbstn1325@naver.com>
2023-10-25 07:28:24 +0900

    feat: 사용자에게 입력받은 숫자와 컴퓨터의 숫자를 비교하기

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -11,6 +11,8 @@ public class Baseball {
 
     public void startGame() {
         List<Integer> computerNumber = RandomUtil.generateRandomNumber(3);
+        int strike = 0;
+        int ball = 0;
 
         System.out.println("숫자 야구 게임을 시작합니다.");
         System.out.print("숫자를 입력해주세요 : ");
@@ -25,5 +27,27 @@ public class Baseball {
         }
 
         List<Integer> userNumber = UserInputUtil.convertStringToIntegerList(userInput);
+        for(int i=0; i<3; i++){
+            String result = compareNumber(computerNumber, userNumber.get(i), i);
+
+            if(result.equals("스트라이크")) {
+                strike++;
+            }else if(result.equals("볼")) {
+                ball++;
+            }
+        }
+
+        System.out.println(ball + " " + strike);
+    }
+
+    private String compareNumber(List<Integer> computerNumber, int userInputValue, int index) {
+        for(int i = 0; i < 3; i++) {
+            if(computerNumber.contains(userInputValue) && i == index) {
+                return "스트라이크";
+            } else if(computerNumber.contains(userInputValue) && i != index) {
+                return "볼";
+            }
+        }
+        return "낫싱";
     }
 }
