1260f5a2bacd2d3d4472f55bc554c439cade3003
dustjq1004 <dustjq1005@naver.com>
2023-10-24 16:23:51 +0900

    yeonsup/feat : Process 출력 포맷 생성과 조회 메서드 구현

--- src/main/java/baseball/Process.java
+++ src/main/java/baseball/Process.java
@@ -1,18 +1,30 @@
 package baseball;
 
+import baseball.view.format.Format;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Process {
 
     private List<Hint> hintLog;
     private Ball ball;
     private int size;
+    private Map<String, Format> formatMap;
 
     public Process(int size) {
         this.size = size;
         createRandomBalls(size);
+    }
+
+    private void loadFormats() {
+        formatMap = new HashMap<>();
+    }
+
+    public Format getFormat(String formatName) {
+        return formatMap.get(formatName);
     }
 
     public void createRandomBalls(int size) {
