36f20840897a8af2484f1c617ea9efb94cf52042
JongMinCh0i <wonderland314592@gmail.com>
2023-10-24 22:44:37 +0900

    refactoring(ComputerView): 매직 리터럴 제거

--- src/main/java/baseball/view/ComputerView.java
+++ src/main/java/baseball/view/ComputerView.java
@@ -2,8 +2,9 @@ package baseball.view;
 
 public class ComputerView implements View {
 
-    @Override
+    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+
     public void renderResult() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println(GAME_START_MESSAGE);
     }
 }
