da5de5e82223965f3ccb96b4f301b442d2b6a78b
ParkJinYong <mej1406@gmail.com>
2023-10-24 15:25:15 +0900

    feat: 투수 입력 예외처리 기능 추가

--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -29,7 +29,6 @@ public class GameManager {
                     displayGameEnd();
                     break;
                 }
-
             }
 
             int choice = askForRestart();
--- src/main/java/baseball/Pitcher.java
+++ src/main/java/baseball/Pitcher.java
@@ -19,26 +19,35 @@ public class Pitcher {
         return numbers;
     }
 
-    public void getPlayerInput() {
+    public void getPlayerInput() throws IllegalArgumentException {
         System.out.print("숫자를 입력해주세요 : ");
-        int randomNumber = Integer.parseInt(Console.readLine());
-        Stack<Integer> split = splitNumbers(randomNumber);
+        Stack<Integer> split = new Stack<>();
+        String inputStr = Console.readLine();
 
+        if (inputStr.length() > maxSize) {
+            throw new IllegalArgumentException("최대 길이 " + maxSize + "를 넘어가는 입력입니다.");
+        }
+
+        int inputNumber = Integer.parseInt(inputStr);
+        splitNumbers(split, inputNumber);
+        updateNumbers(split);
+    }
+
+    public void splitNumbers(Stack<Integer> split, int number) throws IllegalArgumentException {
+        while (number != 0) {
+            int digit = number % 10;
+            if (split.contains(digit)) {
+                throw new IllegalArgumentException("서로 다른 수를 입력 해야 합니다.");
+            }
+            split.push(digit);
+            number /= 10;
+        }
+    }
+
+    public void updateNumbers(Stack<Integer> split) {
         numbers.clear();
         while (numbers.size() < maxSize) {
             numbers.add(split.pop());
         }
-    }
-
-    public Stack<Integer> splitNumbers(int number) {
-        Stack<Integer> split = new Stack<>();
-
-        while (number != 0) {
-            int digit = number % 10;
-            split.push(digit);
-            number /= 10;
-        }
-
-        return split;
     }
 }
