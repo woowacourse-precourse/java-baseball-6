8f70f49fe163032a78b9696ef8a2f235ab53b255
EvelynKimm <evekimmy09@gmail.com>
2023-10-24 17:14:23 +0900

    refactor: make an object out of the method line

--- src/main/java/baseball/util/Converter.java
+++ src/main/java/baseball/util/Converter.java
@@ -4,9 +4,10 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Converter {
+    public static List<Integer> intList;
 
     public static List<Integer> toIntegerList(String str) {
-        List<Integer> intList = Arrays.stream(str.trim().split(""))
+        intList = Arrays.stream(str.trim().split(""))
                 .mapToInt(Integer::parseInt)
                 .boxed().toList();
         return intList;
