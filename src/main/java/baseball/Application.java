33e11b4a98a39eabece0a1e421c4eddacea3fbfa
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-10-25 02:59:49 +0900

    #4 Refactoring: 코드 구조 변경, main() 안의 코드를 각 기능을 담당하는 메서드로 변환하여 구현
    main()은 GameStart()라는 메서드만 호출하여 모든 기능을 메서드로 최소 하나씩 담당할 수 있도록 코드 구조를 변환하였고, 재귀 호출 구조로 변경
    
    Resolves: #3

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,36 +9,24 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
 	public static void main(String[] args) {
-		// 게임 시작 문구 출력
-		System.out.println("숫자 야구 게임을 시작합니다.");
+
 		// TODO: 프로그램 구현
-		while (true) {
-			boolean gameIsWon = false;
-			List<Integer> opponentNumbers = checkForDuplicateDigitsInRandomNumber();
-			// System.out.println(opponentNumbers);
+		GameStart();
+	}
 
-			while (true) {
-				String playerNumber = InputPlayerNum();
-				boolean playerGuess = checkPlayerNumber(playerNumber);
-				List<Integer> playerNumbers = stringToList(playerNumber);
-				int[] compareList = countStrikesAndBalls(opponentNumbers, playerNumbers);
-				String gameResult = displayGameResult(compareList);
-				System.out.println(gameResult);
-				if (compareList[0] == 3) {
-					System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-					gameIsWon = true;
-					break;
-				}
-			}
+	private static void GameStart() {
+		System.out.println("숫자 야구 게임을 시작합니다.");
+		playGame();
+	}
 
-			System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-			String endingNum = Console.readLine();
+	private static void playGame() {
+		List<Integer> opponentNumbers = checkForDuplicateDigitsInRandomNumber();
+		playingGame(opponentNumbers);
+	}
 
-			if (endingNum.equals("2")) {
-				break;
-			}
-		}
-
+	private static void playingGame(List<Integer> opponentNumbers) {
+		List<Integer> playerNumbers = InputPlayerNum();
+		countStrikesAndBalls(opponentNumbers, playerNumbers);
 	}
 
 	// 상대방(컴퓨터) 숫자 생성
@@ -51,32 +39,19 @@ public class Application {
 				nonDuplicateDigitsArray.add(noDuplicateDigits);
 			}
 		}
-
 		return nonDuplicateDigitsArray;
 	}
 
 	// 숫자 입력 받기
-	private static String InputPlayerNum() {
+	private static List<Integer> InputPlayerNum() {
 		System.out.println("숫자를 입력해 주세요");
 		String InputNum = Console.readLine();
-		return InputNum;
-
-	}
-
-	// 입력받은 숫자를 리스트로 만들기
-	private static List<Integer> stringToList(String playerNum) {
-		List<Integer> Nums = new ArrayList<>();
-		for (int i = 0; i < playerNum.length(); i++) {
-			char digit = playerNum.charAt(i);
-			int digitToInt = (int)(digit - '0');
-			Nums.add(digitToInt);
-		}
-		return Nums;
+		return checkPlayerNumber(InputNum);
 	}
 
 	// 플레이어의 숫자 확인(숫자길이, 숫자로만 이루어져 있는지, 중복된 숫자는 없는지)
-	private static boolean checkPlayerNumber(String playerNum) {
-		boolean flag = true;
+	private static List<Integer> checkPlayerNumber(String playerNum) {
+
 		if (!checkPlayerNumberLength(playerNum)) {
 			System.out.println("3자리의 숫자만 입력해주세요.");
 			throw new IllegalArgumentException();
@@ -89,9 +64,18 @@ public class Application {
 			System.out.println("입력하신 숫자 중 중복된 숫자가 있습니다.");
 			throw new IllegalArgumentException();
 		}
+		return stringToList(playerNum);
+	}
 
-		return flag;
-
+	// 입력받은 숫자를 리스트로 만들기
+	private static List<Integer> stringToList(String playerNum) {
+		List<Integer> Nums = new ArrayList<>();
+		for (int i = 0; i < playerNum.length(); i++) {
+			char digit = playerNum.charAt(i);
+			int digitToInt = (int)(digit - '0');
+			Nums.add(digitToInt);
+		}
+		return Nums;
 	}
 
 	// 플레이어 숫자의 길이가 3인지 확인
@@ -144,21 +128,22 @@ public class Application {
 	}
 
 	// 숫자를 대조해서 스트라이크와 볼의 개수를 파악
-	private static int[] countStrikesAndBalls(List<Integer> opponentNums, List<Integer> playerNums) {
+	private static void countStrikesAndBalls(List<Integer> opponentNumbers, List<Integer> playerNumbers) {
 		int strikeCount = 0;
 		int ballCount = 0;
 		for (int i = 0; i < 3; i++) {
-			if (playerNums.get(i).equals(opponentNums.get(i))) {
+			if (playerNumbers.get(i).equals(opponentNumbers.get(i))) {
 				strikeCount++;
-			} else if (opponentNums.contains(playerNums.get(i))) {
+			} else if (opponentNumbers.contains(playerNumbers.get(i))) {
 				ballCount++;
 			}
 		}
-		return new int[] {strikeCount, ballCount};
+		int[] compareList = new int[] {strikeCount, ballCount};
+		displayGameResult(compareList, opponentNumbers);
 	}
 
 	// 볼과 스트라이크 결과를 출력하고 둘 다 없으면 낫싱 출력
-	private static String displayGameResult(int[] compareList) {
+	private static void displayGameResult(int[] compareList, List<Integer> opponentNumbers) {
 		int strikeScore = compareList[0];
 		int ballScore = compareList[1];
 		String gameResult = "";
@@ -172,6 +157,25 @@ public class Application {
 		} else if (strikeScore == 0 && ballScore == 0) {
 			gameResult = "낫싱";
 		}
-		return gameResult;
+		System.out.println(gameResult);
+		endGame(strikeScore, opponentNumbers);
+	}
+
+	private static void endGame(int strikeCount, List<Integer> opponentNumbers) {
+		if (strikeCount == 3) {
+			System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+			selectGame();
+		}
+		if (strikeCount != 3) {
+			playingGame(opponentNumbers);
+		}
+	}
+
+	private static void selectGame() {
+		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+		String endingNum = Console.readLine();
+		if (endingNum.equals("1")) {
+			playGame();
+		}
 	}
 }
