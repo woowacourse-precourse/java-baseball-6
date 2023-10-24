a621f1c3848d4946878fd42b2b75ce59087121df
jongchan0109 <jongchan1009@naver.com>
2023-10-24 15:19:17 +0900

    feat: 프로그램 실행 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,74 +1,9 @@
 package baseball;
-import camp.nextstep.edu.missionutils.Randoms;
-import camp.nextstep.edu.missionutils.Console;
-
-class Model{
-
-}
-
-class View{
-    final static String inputNumberSpeak = "숫자를 입력해주세요 : ";
-    final static String startGameSpeak = "숫자 야구 게임을 시작합니다.";
-    final static String startOrEndSpeak = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-    final static String gameEndSpeak = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    final static String notThingSpeak = "낫싱";
-    final static String ballSpeak = "볼";
-    final static String strikeSpeak = "스트라이크";
-    final static String inputIsUnvalid = "잘못된 값을 입력하였습니다.";
-    static List<Integer> inputNumber(){
-        String inputString = Console.readLine();
-        if(inputString.length()!=3){
-            throws new IllegalArgumentException(inputIsUnvalid);
-        }
-        List<Integer> inputList = new ArrayList<>();
-        for(int i=0;i<3;i++) {
-            userInput = inputString.charAt(i);
-            if ('1' <= userInput && userInput <= '9'){
-                inputList.add(inputString.charAt(i));
-            }else {
-            throws new IllegalArgumentException(inputIsUnvalid);
-            }
-        }
-        return inputList;
-    }
-    static boolean startOrEnd(){
-        System.out.println(startOrEndSpeak);
-        int choice = Integer.pareseInt(Console.readLine());
-
-        if(choice ==1)
-            return true;
-        else
-            return false;
-    }
-
-    static void resultPrint(int ballCount, int strikeCount) {
-        if(strikeCount==3){
-            System.out.println(gameEndSpeak);
-        }
-        else if(strikeCount!=0 && ballCount!=0){
-            System.out.println(ballCount+ballSpeak+" "+ strikeCount+strikeSpeak);
-        }
-        else if(strikeCount==0){
-            System.out.println(ballCount+ballSpeak);
-        }
-        else if(ballCount == 0){
-            System.out.println(strikeCount+strikeSpeak);
-        }
-        else{
-            System.out.println(notThingSpeak);
-        }
-    }
-
-}
-
-
-class Controller{
-
-}
-
+import baseball.controller.GameController;
 
 public class Application {
     public static void main(String[] args) {
-        start();
+        GameController gameController = new GameController();
+        gameController.gameStart();
     }
 }
