48d922a726d3c5f59d37f0aa7d593ce3e67101b0
minju <minju9711@naver.com>
2023-10-25 02:45:17 +0900

    [DOCS] 리팩토링 과정 중 코드의 구조가 바뀌게 되어 새로 작성합니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,55 +1,16 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class Application {
-    private static final String GAME_RESTART = "1";
-    private static final String GAME_END = "2";
     public static void main(String[] args) {
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        Game game = new Game(); // 게임 객체 생성
-        game.createAnswer(); //랜덤의 답 생성
-        String judge = "-1"; //while문에 오류없이 진입 위해 음수로 설정
 
-        while(!judge.equals(GAME_END)){
-            try {
-                System.out.print("숫자를 입력해주세요 : ");
-                String input=Console.readLine();
-                checkCorrectInput(input);
-                int number = Integer.parseInt(input);
+        Constant constant = new Constant(); // 상수 클래스 생성
+        CheckForm checkForm = new CheckForm(constant.getGAME_RESTART(), constant.getGAME_END()); // 인증용 클래스 객체 생성
+        IO io = new IO(); // 입출력 객체 생성
+        Game game = new Game(io); // 게임 객체 생성
 
-                judge = game.judgeGame(number);
+        GameConfiguration gameConfiguration = new GameConfiguration(constant, checkForm, game); //의존성 주입 클래스(게임 관련만)
+        Runner runner = new Runner(gameConfiguration, io);
 
-                if(judge.equals("SUCCESS")){
-                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                    judge = Console.readLine();
-                    checkCorrectRestart_Input(judge); //사용자가 1,2 가 아닌 다른 수나 문자를 입력했을 때 예외처리
-                    if(judge.equals(GAME_RESTART)) game.createAnswer(); //새로운 답안 생성
-                }
-            }
-            catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-                throw e;
-            }
-        }
+        runner.run(); //게임 실행
     }
-
-    public static void checkCorrectInput(String number){
-        try {
-            Integer.parseInt(number); // 숫자로 변환을 시도
-        } catch (NumberFormatException e) { //만약 숫자가 아닌 값이 있다면 해당 예외 발생
-            throw new IllegalArgumentException("ERROR: 숫자만을 입력해주세요");
-        }
-        if (number.length() != 3 || number.contains("0")) {
-            throw new IllegalArgumentException("ERROR : 1~9의 3자리 숫자를 입력해주세요");
-        }
-    }
-
-    private static void checkCorrectRestart_Input(String judge) {
-        if (!judge.equals(GAME_RESTART) && !judge.equals(GAME_END)) {
-            throw new IllegalArgumentException("ERROR: 정답을 맞추신 후, 1 또는 2의 숫자를 입력해주세요.");
-        }
-    }
-
 }
