6a26b9c20e6039101faebd2dd7b4ed8fee35b2a2
hansg931 <hansg0509@cau.ac.kr>
2023-10-25 00:55:30 +0900

    feat: 유저 숫자 입력 메소드 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,4 +1,14 @@
 package baseball.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import baseball.util.Constant;
+import baseball.util.Message;
+
 public class InputView {
+	public int userInputNumber() {
+		System.out.print(Message.INPUT_NUMBER_MESSAGE);
+		String userInput = Console.readLine();
+
+		return Integer.parseInt(userInput);
+	}
 }
