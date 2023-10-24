270ae40237ec008ef2daaf2d48337c23b96b1025
edjjincode <138580800+edjjincode@users.noreply.github.com>
2023-10-25 08:52:23 +0900

    feat(apply): upload Application.java
    Add Application.java file:
    - create controller instance to run

---
+++ src/main/java/baseball/Application.java
@@ -0,0 +1,9 @@
+package baseball;
+
+public class Application {
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.run();
+    }
+}
+
