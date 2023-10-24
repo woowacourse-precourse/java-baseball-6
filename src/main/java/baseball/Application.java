0f9735635d691511d3bd5e4e5a058681e5940353
oyeong011 <oyeong011@gmail.com>
2023-10-24 22:08:36 +0900

    컴퓨터가 정한 숫자 출력 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,8 +10,7 @@ public class Application {
     public static void main(String[] args) {
         while (outerCondition) {
             String generatedNumber = numberGenerator();
-            System.out.println(generatedNumber);
-            System.out.print("숫자 야구 게임을 시작합니다.");
+            System.out.println("숫자 야구 게임을 시작합니다.");
             try {
                 while (innerCondition) {
                     System.out.print("숫자를 입력해주세요: ");
