beea400c4ac2249f2a1d454d81b8210657f6b0e6
Han Ju Kim <hjkim2902@gmail.com>
2023-10-24 16:08:50 +0900

    feat : 낫싱 메시지 추가

--- src/main/java/baseball/util/Message.java
+++ src/main/java/baseball/util/Message.java
@@ -5,6 +5,7 @@ public enum Message {
     INPUT("숫자를 입력해주세요 : "),
     BALL("볼"),
     STRIKE("스트라이크"),
+    NOTHING("낫싱"),
     SUCCESS("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
 
