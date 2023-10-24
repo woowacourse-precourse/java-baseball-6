3d2ad3da7feea62472d7ebd67745610557df069a
BDlhj <akk808@naver.com>
2023-10-24 22:21:47 +0900

    feat: Implement judge() in src/main/java/baseball/Referee.java

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -3,10 +3,26 @@ package baseball;
 import java.util.List;
 
 public class Referee {
-    private int strike;
-    private int ball;
+    public String judge(List<Integer> answer, List<Integer> playerInputIntegerList) {
+        int totalStrike = countStrike(answer, playerInputIntegerList);
+        int totalBall = countBall(answer, playerInputIntegerList);
+        String judgement;
+        if (totalStrike == 0 && totalBall == 0) {
+            judgement = "낫싱";
+        } else if (totalStrike == 3) {
+            judgement = String.format("%d스트라이크", totalStrike);
+        } else if (totalStrike == 0) {
+            judgement = String.format("%d볼", totalBall);
+        } else if (totalBall == 0) {
+            judgement = String.format("%d스트라이크", totalStrike);
+        } else {
+            judgement = String.format("%d볼 %d스트라이크", totalBall, totalStrike);
+        }
+        return judgement;
+    }
 
     public Integer countStrike(List<Integer> answer, List<Integer> playerInputIntegerList) {
+        int strike = 0;
         for (int i = 0; i < 3; i++) {
             if (answer.get(i).equals(playerInputIntegerList.get(i))) {
                 strike += 1;
@@ -16,6 +32,7 @@ public class Referee {
     }
 
     public Integer countBall(List<Integer> answer, List<Integer> playerInputIntegerList) {
+        int ball = 0;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (answer.get(i).equals(playerInputIntegerList.get(i)) && (i != j)) {
