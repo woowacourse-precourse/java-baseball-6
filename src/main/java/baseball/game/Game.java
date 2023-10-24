ef92429b61b5b4ae0fdd3b40341aafcadad00694
min050410 <min050410@gmail.com>
2023-10-24 09:23:09 +0900

    feat: 힌트 출력

--- src/main/java/baseball/game/Game.java
+++ src/main/java/baseball/game/Game.java
@@ -11,7 +11,7 @@ public class Game {
     private final static String SYSTEM_INPUT_MASSAGE = "숫자를 입력해주세요 : ";
     private final static String SYSTEM_STRIKE_MASSAGE = "스트라이크";
     private final static String SYSTEM_NOTHING_MASSAGE = "낫싱";
-    private final static String SYSTEM_BALL_MASSAGE = "볼 ";
+    private final static String SYSTEM_BALL_MASSAGE = "볼";
     private final static String SYSTEM_END_MASSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     public final static String RESTART_STRING = "1";
     private final static String END_STRING = "2";
@@ -65,7 +65,8 @@ public class Game {
             System.out.println(SYSTEM_NOTHING_MASSAGE);
         }
         if (ball != 0) {
-            System.out.println(ball + SYSTEM_BALL_MASSAGE);
+            if (strike == 0) System.out.println(ball + SYSTEM_BALL_MASSAGE);
+            else System.out.print(ball + SYSTEM_BALL_MASSAGE + " ");
         }
         if (strike != 0) {
             System.out.println(strike + SYSTEM_STRIKE_MASSAGE);
