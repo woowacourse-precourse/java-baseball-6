6ecf3e6e970687d2d405d7df61f3617cd746178c
박승민 <tmddudals369@naver.com>
2023-10-24 17:52:37 +0900

    feat(outputView): 결과를 출력하는 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,5 +1,7 @@
 package baseball.view;
 
+import baseball.vo.Score;
+
 public class OutputView {
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String USER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
@@ -10,5 +12,22 @@ public class OutputView {
 
     public void printUserInputMessage() {
         System.out.print(USER_INPUT_MESSAGE);
+    }
+
+    public void printScore(Score score) {
+        if (score.getBall() == 0 && score.getStrike() == 0){
+            System.out.println("낫싱");
+            return;
+        }
+        if (score.getStrike() == 0){
+            System.out.printf("%d볼%n", score.getBall());
+            return;
+        }
+        if (score.getBall() == 0){
+            System.out.printf("%d스트라이크\n", score.getStrike());
+            return;
+        }
+
+        System.out.printf("%d볼 %d스트라이크\n", score.getBall(), score.getStrike());
     }
 }
