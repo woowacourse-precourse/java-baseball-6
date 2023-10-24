23ba509a88a5cf7b5b79d2e9aef2b5335b5be4ac
SunHyeok <cghhld@naver.com>
2023-10-24 16:12:56 +0900

    정답확인 갯수확인

--- src/main/java/baseball/Model/BaseballGameModel.java
+++ src/main/java/baseball/Model/BaseballGameModel.java
@@ -25,4 +25,51 @@ public class BaseballGameModel {
     public int[] getAnswer() {
         return answer;
     }
+
+        public String guess(int[] guess) {
+        int strikes = 0;
+        int balls = 0;
+
+        for (int i = 0; i < 3; i++) {
+            if (guess[i] == answer[i]) {
+                strikes++;
+            } else if (contains(guess[i])) {
+                balls++;
+            }
+        }
+
+        if (strikes == 3) {
+            return "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+        }
+
+        return getResultMessage(strikes, balls);
+    }
+
+    private boolean contains(int number) {
+        for (int num : answer) {
+            if (num == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private String getResultMessage(int strikes, int balls) {
+        StringBuilder result = new StringBuilder();
+    
+        if (strikes > 0) {
+            result.append(strikes).append("스트라이크 ");
+        }
+    
+        if (balls > 0) {
+            result.append(balls).append("볼 ");
+        }
+    
+        if (strikes == 0 && balls == 0) {
+            result.append("낫싱");
+        }
+    
+        return result.toString();
+    }
+    
 }
