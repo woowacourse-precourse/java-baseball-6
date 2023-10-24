ccdcf65209d5d1cf1e68b11edf38dfd6dd2e99c6
aeyongdodam <kikititi0510@gmail.com>
2023-10-24 12:48:11 +0900

    refactor(InputEndChoice): set constant value

--- src/main/java/baseball/model/InputEndChoice.java
+++ src/main/java/baseball/model/InputEndChoice.java
@@ -3,7 +3,7 @@ package baseball.model;
 public class InputEndChoice {
     private static final int RESTART = 1;
     private static final int QUIT = 2;
-    private static final int INPUT_CHOICE = 1;
+    private static final int INPUT_CHOICE_LENGTH = 1;
 
     public enum Choice {
         RESTART, QUIT, INVALID
@@ -15,17 +15,17 @@ public class InputEndChoice {
     }
 
     private void checkSize(String input) {
-        if (input.length() != INPUT_CHOICE) {
+        if (input.length() != INPUT_CHOICE_LENGTH) {
             throw new IllegalArgumentException("1,2 만 입력 가능");
         }
     }
 
     private Choice checkValidNumber(String input) {
         char tmpNumber = input.charAt(0);
-        if (Character.getNumericValue(tmpNumber) == 1) {
+        if (Character.getNumericValue(tmpNumber) == RESTART) {
             return Choice.RESTART;
         }
-        if (Character.getNumericValue(tmpNumber) == 2) {
+        if (Character.getNumericValue(tmpNumber) == QUIT) {
             return Choice.QUIT;
         }
         return Choice.INVALID;
