61374e7d2211c08a2f0dce6dbd1abf80cdf2a952
songyi <song21677@naver.com>
2023-10-25 03:52:42 +0900

    style: Application 클래스에서 필요 없는 함수 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,6 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Application {
     static Computer computer;
@@ -46,58 +43,6 @@ public class Application {
         }
     }
 
-    static List<Integer> makeNumbers () {
-        // 임의의 수 3개를 만든다.
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
-        }
-        return computer;
-    }
-
-    static void validateLength (String playerInput){
-        // 길이 검증
-        if (playerInput.length() != 3) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    static int validateNumber (String playerInput){
-        int playerNumber;
-        // 숫자 검증
-        try {
-            playerNumber = Integer.parseInt(playerInput);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
-        }
-        return playerNumber;
-    }
-
-    private static void validateRange(int number) {
-        // 1에서 9까지의 수인지 검증
-        if (number < 1 || number > 9) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private static void validateDuplication(List<Integer> playerNumbers, int number) {
-        // 서로 다른지 검증
-        if (playerNumbers.contains(number)) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    static public List<Integer> reverse (List < Integer > playerNumbers) {
-        List<Integer> reverse = new ArrayList<>();
-        for (int i = playerNumbers.size() - 1; i >= 0; i--) {
-            reverse.add(playerNumbers.get(i));
-        }
-        return reverse;
-    }
-
     private static Result calculate(Balls computerBalls, Balls playerBalls) {
         Result result = new Result();
         for (int i=0; i<3; i++) {
@@ -109,12 +54,5 @@ public class Application {
         }
 
         return result;
-    }
-
-    private static void validateValue(int choose) throws IllegalArgumentException {
-        // 1, 2인지 검증한다.
-        if (choose != 1 && choose != 2) {
-            throw new IllegalArgumentException();
-        }
     }
 }
