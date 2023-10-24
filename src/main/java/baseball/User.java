f342cf8455ca0b28de8c30531bd9c44ed0170967
jaepyo99 <jaepyo99@naver.com>
2023-10-24 19:44:14 +0900

    [Feat] 사용자 야구숫자 입력 메서드 추가

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,4 +1,18 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+
 public class User {
+    private static final String INPUT_NUMBER_MENT = "숫자를 입력해주세요 : ";
+
+    public User() {
+    }
+
+    public String inputNum() {
+        System.out.print(INPUT_NUMBER_MENT);
+        return readLine();
+    }
+
+
 }
