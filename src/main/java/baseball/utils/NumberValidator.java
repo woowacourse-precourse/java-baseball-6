0db2812055569af68af9c6710c24c3fcddb88836
박승민 <tmddudals369@naver.com>
2023-10-25 01:46:14 +0900

    refactor(numberValidator): 올바른 수인지 검증하는 메서드의 기능 분리

--- src/main/java/baseball/utils/NumberValidator.java
+++ src/main/java/baseball/utils/NumberValidator.java
@@ -10,14 +10,24 @@ public class NumberValidator {
     private static final String DUPLICATE_DIGIT_ERROR_MESSAGE = "입력된 수는 서로 다른 수로 구성되어야 합니다.";
 
     public static void validateThreeDistinctDigitsInRange(String number) {
+        isDigit(number);
+        isValidSize(number);
+        isDuplicateDigitContain(number);
+    }
+
+    private static void isDigit(String number) {
         if (!Pattern.matches("^[1-9]+$", number)) {
             throw new IllegalArgumentException(NOT_DIGIT_ERROR_MESSAGE);
         }
+    }
 
+    private static void isValidSize(String number) {
         if (number.length() != VALID_SIZE) {
             throw new IllegalArgumentException(NOT_VALID_LENGTH_ERROR_MESSAGE);
         }
+    }
 
+    private static void isDuplicateDigitContain(String number) {
         if(number.charAt(0) == number.charAt(1) || number.charAt(1) == number.charAt(2) || number.charAt(0) == number.charAt(2)){
             throw new IllegalArgumentException(DUPLICATE_DIGIT_ERROR_MESSAGE);
         }
