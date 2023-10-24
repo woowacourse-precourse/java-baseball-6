186ec1494eb234b2b0fabaff955d1835fb91c0b9
aka-nick <bechalrang@gmail.com>
2023-10-24 15:43:56 +0900

    리팩토링: BallballGame의 재시작/종료 관련 클래스 상수를 BaseballGameRestarts라는 enum으로 추출. 더이상 사용되지 않는 Import문들도 함께 제거.

--- src/main/java/baseball/game/BaseballGame.java
+++ src/main/java/baseball/game/BaseballGame.java
@@ -3,15 +3,8 @@ package baseball.game;
 import baseball.game.message.BaseballGameMessage;
 import baseball.io.Input;
 import baseball.io.Output;
-import java.util.Objects;
-import java.util.Set;
 
 public class BaseballGame {
-
-    // TODO : 과도한 상수 사용 -> enum으로 교체 (메시지는 특히나)
-    private static final Integer GAME_RESTART = 1;
-    private static final Integer GAME_FINISH = 2;
-    private static final Set<Integer> INPUT_RESTARTS = Set.of(GAME_RESTART, GAME_FINISH);
 
     private boolean isStart = true;
 
@@ -35,16 +28,11 @@ public class BaseballGame {
         Output.consoleLine(BaseballGameMessage.GAME_WIN_MESSAGE.get());
     }
 
-    // TODO : 과도한 상수 사용 -> enum으로 교체
     private void askRestart() {
         Output.consoleLine(BaseballGameMessage.GAME_RESTART_MESSAGE.get());
-        Integer inputRestart = Input.consoleNumber();
+        BaseballGameRestarts restarts = BaseballGameRestarts.find(Input.consoleNumber());
 
-        if (!INPUT_RESTARTS.contains(inputRestart)) {
-            throw new IllegalArgumentException(BaseballGameMessage.MUST_INPUT_RESTARTS_MESSAGE.get());
-        }
-
-        if (Objects.equals(inputRestart, GAME_FINISH)) {
+        if (restarts.wantToFinish()) {
             isStart = false;
         }
     }
