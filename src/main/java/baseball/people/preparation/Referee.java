b23ff3ddca3435df226cd21ae650c6dfea2d8f7d
U-keun Song <sukeun319@gmail.com>
2023-10-24 14:36:31 +0900

    Fix : "0볼 2스트라이크"로 출력되는 버그 수정
    기존의 로직이 잘못되어 있었다. 볼에 스트라이크가 포함되는 로직을 구현한 것이라 오류가 발생했었다.

--- src/main/java/baseball/people/preparation/Referee.java
+++ src/main/java/baseball/people/preparation/Referee.java
@@ -30,37 +30,39 @@ public class Referee {
     볼과 스트라이크의 개수를 출력한다.
      */
     public int[] judge(int[] numbers) {
-        int[] judgment = new int[2];
+        int ball = 0, strike = 0;
         for (int i = 0; i < 3; i++) {
             boolean[] check = targetNumber.has(numbers[i], i);
-            if (check[0]) {
-                judgment[0]++;
-            }
+
             if (check[1]) {
-                judgment[1]++;
+                strike++;
+                continue;
+            }
+            if (check[0]) {
+                ball++;
             }
         }
 
-        declare(judgment);
+        declare(ball, strike);
 
-        return judgment;
+        return new int[]{ball, strike};
     }
 
-    private void declare(int[] judgment) {
-        if (judgment[1] == 3) {
+    private void declare(int ball, int strike) {
+        if (strike == 3) {
             System.out.println("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             return;
         }
-        if (judgment[0] == 0) {
+        if (ball == 0 && strike == 0) {
             System.out.println("낫싱");
             return;
         }
         StringBuilder print = new StringBuilder();
-        if (judgment[0] > 0) {
-            print.append(String.format("%d볼 ", judgment[0] - judgment[1]));
+        if (ball > 0) {
+            print.append(String.format("%d볼 ", ball));
         }
-        if (judgment[1] > 0) {
-            print.append(String.format("%d스트라이크", judgment[1]));
+        if (strike > 0) {
+            print.append(String.format("%d스트라이크", strike));
         }
         System.out.println(print);
     }
