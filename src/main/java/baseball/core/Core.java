7709c2bb53ae18870c1afc3fcc31b402d57615da
constant94-dev <constant94@naver.com>
2023-10-24 23:56:02 +0900

    [1024] countStrike, isStrike 메서드 및 테스트 코드 구현

--- src/main/java/baseball/core/Core.java
+++ src/main/java/baseball/core/Core.java
@@ -1,5 +1,21 @@
 package baseball.core;
 
-public class Core {
+import java.util.List;
 
+public class Core {
+    public int countStrike(List<Integer> computerNumbers, List<Integer> playerNumbers) {
+        System.out.println("스트라이크 확인 합시다!!");
+        int size = playerNumbers.size();
+        int count = 0;
+        for (int index = 0; index < size; index++) {
+            if (isStrike(computerNumbers, playerNumbers, index)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private boolean isStrike(List<Integer> computerNumbers, List<Integer> playerNumbers, int index) {
+        return computerNumbers.get(index).equals(playerNumbers.get(index));
+    }
 }
--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,12 +1,15 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import baseball.core.Core;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -26,6 +29,32 @@ class ApplicationTest extends NsTest {
                 assertThatThrownBy(() -> runException("1234"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 스트라이크_개수() {
+        // given(준비): 어떤 데이터가 준비되었을 때
+        final List<Integer> computerNumbers = new ArrayList<>() {
+            {
+                add(1);
+                add(2);
+                add(3);
+            }
+        };
+        final List<Integer> playerNumbers = new ArrayList<>() {
+            {
+                add(1);
+                add(9);
+                add(5);
+            }
+        };
+
+        // when(실행): 어떠한 함수를 실행하면
+        Core core = new Core();
+        int result = core.countStrike(computerNumbers, playerNumbers);
+
+        // then(검증): 어떠한 결과가 나와야 한다.
+        assertThat(result).isEqualTo(1);
     }
 
     @Override
