324a9aae769b093ddcffc3278fbfa22e096f40d2
sheepysnoopy <qkffldlfdjsk12@naver.com>
2023-10-24 17:23:49 +0900

    refactor(ComputerFunction): 랜덤 컴퓨터 숫자 생성 로직 변경

--- src/main/java/baseball/ComputerFunction.java
+++ src/main/java/baseball/ComputerFunction.java
@@ -1,2 +1,56 @@
-package baseball;public class ComputerFunction {
+package baseball;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+import static baseball.NumberInfo.*;
+import static baseball.Message.*;
+
+public class ComputerFunction {
+    UserFunction userFunction = new UserFunction();
+
+    public List<Integer> createRandomNumber(){
+        List<Integer> randomNumber = new ArrayList<>();
+
+        while(randomNumber.size() < SIZE_OF_NUMBER.getNumberInfo()){
+            int tmp = Randoms.pickNumberInRange(START_OF_RANGE.getNumberInfo(), END_OF_RANGE.getNumberInfo());
+
+            if(!randomNumber.contains(tmp)){
+                randomNumber.add(tmp);
+            }
+        }
+
+        return randomNumber;
+    }
+
+    public void countCalculate(List<Integer> randomNumber){
+        while(true) {
+            String userNumber = userFunction.getUserNumber();
+
+            int ballCount = 0;
+            int strikeCount = 0;
+
+            for (int i=0; i<SIZE_OF_NUMBER.getNumberInfo(); i++) {
+                int tmp = userNumber.charAt(i) - '0';
+
+                if (randomNumber.indexOf(tmp) == i) {
+                    strikeCount++;
+                }
+                if (randomNumber.contains(tmp)) {
+                    ballCount++;
+                }
+            }
+
+            if (strikeCount == SIZE_OF_NUMBER.getNumberInfo()) {
+                System.out.println(strikeCount + STRIKE_MESSAGE.getMessage());
+                System.out.println(CORRECT_MESSAGE.getMessage());
+                break;
+            } else if (strikeCount == 0 && ballCount == 0) {
+                System.out.println(NOTHING_MESSAGE.getMessage());
+                continue;
+            }
+            System.out.println((ballCount-strikeCount) + BALL_MESSAGE.getMessage() +
+                    strikeCount + STRIKE_MESSAGE.getMessage());
+        }
+    }
 }
