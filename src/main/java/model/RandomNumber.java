d951562d91d2a68873c3f69285bbe938a3753473
yangyanghyunjung <hyunjungyang0922@gmail.com>
2023-10-25 00:24:52 +0900

    🎯feat(model / RandomNumber): 3자리 random 값 생성 기능 구현

---
+++ src/main/java/model/RandomNumber.java
@@ -0,0 +1,19 @@
+package model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomNumber {
+    public List<Integer> GenerateRandomNumber () {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+}
