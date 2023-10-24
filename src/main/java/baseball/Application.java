7b4a89f9828cdd9ec6224be2e9dd624bbdc83a80
Cycrypto <jh01love@naver.com>
2023-10-24 16:47:54 +0900

    feat:다시플레이할때 인스턴스 새로 호출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,14 +2,16 @@ package baseball;
 
 public class Application {
     private GameParticipant player;
-    public Application(){
-        player = new GameParticipant();
-    }
+    private GameStatus replay;
     public static void main(String[] args) {
         Application app = new Application();
         app.run();
     }
     public void run(){
-        player.playGameByRequest();
+        while(this.replay != GameStatus.STOP){
+            player = new GameParticipant();
+            replay = player.playGameByRequest();
+           System.out.println("3개의 숫자를 모두 맞췄습니다. 게임 종료");
+        }
     }
 }
