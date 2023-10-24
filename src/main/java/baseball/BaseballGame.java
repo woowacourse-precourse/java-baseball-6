4ef3cecbd321c22ce98cdaf847ba37a82febd3f6
wooyongjeong <wooyong.dev@gmail.com>
2023-10-25 02:24:10 +0900

    feat(BaseballGame): 숫자 야구 게임 전체 로직 작성

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -11,5 +11,44 @@ public class BaseballGame {
     }
 
     public void playBaseball() {
+        try {
+            gameStart();
+        } catch (IllegalArgumentException exception) {
+            handleException(exception);
+        }
     }
+
+    private void gameStart() {
+        output.gameStart();
+        while (true) {
+            playUntilThreeStrikes();
+            output.inputGameCommand();
+            GameCommand gameCommand = input.readGameCommand();
+            if (!gameCommand.isContinue()) {
+                break;
+            }
+        }
+        output.gameEnd();
+    }
+
+    private void playUntilThreeStrikes() {
+        Computer computer = Computer.getInstanceByRandomNumbers();
+        while (true) {
+            output.inputPlayerNumber();
+            Player player = Player.getInstanceByNumber(input.readPlayerNumber());
+            Referee referee = Referee.getInstance(computer, player);
+            Result result = referee.judge();
+            output.printResult(result);
+            if (result.isThreeStrikes()) {
+                output.inningEnd();
+                break;
+            }
+        }
+    }
+
+    private void handleException(IllegalArgumentException exception) {
+        output.printExceptionMessage(exception);
+        throw exception;
+    }
+
 }
