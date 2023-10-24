88951e068d4c157a75daf58a919bc05c29dc59a9
BDlhj <akk808@naver.com>
2023-10-24 09:53:34 +0900

    feat: Implement run() in src/main/java/baseball/GameStartView.java

--- src/main/java/baseball/GameStartView.java
+++ src/main/java/baseball/GameStartView.java
@@ -3,6 +3,10 @@ package baseball;
 public class GameStartView {
     private static final String GameStartMessage = "숫자 야구 게임을 시작합니다.";
 
+    public void run() {
+        this.printStartGameMessage();
+    }
+
     public void printStartGameMessage() {
         System.out.println(GameStartMessage);
     }
