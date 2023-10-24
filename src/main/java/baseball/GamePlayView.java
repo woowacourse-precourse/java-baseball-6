dc35cdaaa7bb88665b1e16c8780569f849effae2
BDlhj <akk808@naver.com>
2023-10-24 09:58:23 +0900

    feat: Implement printAskingForInputMessage() in src/main/java/baseball/GamePlayView.java

--- src/main/java/baseball/GamePlayView.java
+++ src/main/java/baseball/GamePlayView.java
@@ -1,2 +1,9 @@
-package baseball;public class GamePlayView {
+package baseball;
+
+public class GamePlayView {
+    private static final String ASKING_FOR_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+
+    public void printAskingForInputMessage() {
+        System.out.print(ASKING_FOR_INPUT_MESSAGE);
+    }
 }
