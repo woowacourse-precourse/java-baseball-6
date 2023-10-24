b1cca92bb2e6902289c1738ca1d3499dabcc142c
34suuuuu <psh7492@gmail.com>
2023-10-24 21:10:55 +0900

    refactor: total갯수 세는 클래스를 ball갯수 세는 클래스로 변환

--- src/main/java/computer/Computer.java
+++ src/main/java/computer/Computer.java
@@ -1,70 +1,109 @@
 package computer;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import constants.MessageConstants;
 import player.Player;
+import view.Input;
 
 import static constants.MessageConstants.*;
 import static constants.NumConstants.*;
 
 public class Computer {
     String randomString = new String();
+    Computer computer;
+    Player player;
 
     // 3자리 난수 생성
-    public String getRandomNumbers(){
+    public String getRandomNumbers() {
         randomString = "";
-        while(randomString.length() < ANSWER_SIZE){
+        while (randomString.length() < ANSWER_SIZE) {
             int randomNum = Randoms.pickNumberInRange(START_NUM, END_NUM);
-            if(!randomString.contains(String.valueOf(randomNum))){
+            if (!randomString.contains(String.valueOf(randomNum))) {
                 randomString += String.valueOf(randomNum);
             }
         }
+        System.out.println("randomString: " + randomString);
         return randomString;
     }
 
+    public void start() {
+        computer = new Computer();
+        System.out.println(MessageConstants.GAME_START_MESSAGE);
 
-
-    // 스트라이크 갯수 파악
-    private int cntStrike(String inputNum){
-        int strike = 0;
-        for (int i = 0; i < INPUT_LENGTH; i++) {
-            if(randomString.charAt(i) == inputNum.charAt(i)){ strike++;
+        do {
+            computer.getRandomNumbers();    // Computer 난수 생성
+            computer.getInput();    // User의 입력
+            if (computer.chkRestartOrFinish()) {
+                computer = new Computer();
+            } else {
+                break;
             }
-        } return strike;
+        }while(true);
     }
 
-    // total = strike + ball
-    private int cntTotal(String inputNum){
-        int total = 0;
-        for (char c : inputNum.toCharArray()) {
-            if(randomString.contains(c+" ")){
-                total++;
+    public void getInput(){
+        player = new Player();
+        String playerInput;
+
+        do{
+            playerInput = player.start();
+        }while (!chkResult(playerInput));
+    }
+
+    // 스트라이크 갯수 파악
+    private int cntStrike(String inputNum) {
+        int strike = 0;
+        for (int i = 0; i < INPUT_LENGTH; i++) {
+            if (randomString.charAt(i) == inputNum.charAt(i)) {
+                strike++;
             }
         }
-        return total;
+        return strike;
+    }
+
+    private int cntBall(String inputNum) {
+        int ball = 0;
+        for (int i = 0; i < INPUT_LENGTH; i++) {
+            char tmp = inputNum.charAt(i);
+            if (randomString.contains(String.valueOf(tmp)) && randomString.charAt(i) != tmp) {
+                ball++;
+            }
+        }
+        return ball;
     }
 
     private boolean chkResult(String inputNum) {
-        int total = cntTotal(inputNum);
+        int ball = cntBall(inputNum);
         int strike = cntStrike(inputNum);
-        printResult(total, strike);
+        printResult(ball, strike);
 
         return strike == SUCCESS_STRIKE;
     }
 
-    // 결과 출력
-    private void printResult(int total, int strike){
-        int ball = total - strike;
+    private boolean chkRestartOrFinish(){
+        String playerOption = Input.readOption();
+        if (playerOption.equals(String.valueOf(OPTION_RESTART))) {
+            return true;
+        } else if (playerOption.equals(String.valueOf(OPTION_FINISH))) {
+            return false;
+        } else {
+            throw new IllegalStateException(OPTION_INPUT_ERROR_MESSAGE);
+        }
+    }
 
-        if(total == NOTHING){
+    // 결과 출력
+    private void printResult(int ball, int strike) {
+
+        if (ball == NOTHING && strike == NOTHING) {
             System.out.println(NOTHING_MESSAGE);
-        } else if(strike == SUCCESS_STRIKE){
+        } else if (strike == SUCCESS_STRIKE) {
             System.out.println(SUCCESS_MESSAGE);
-        }else {
-            if(ball == 0){
+        } else {
+            if (ball == NOTHING) {
                 System.out.println(strike + STRIKE_MESSAGE);
-            }else if(strike == 0){
+            } else if (strike == NOTHING) {
                 System.out.println(ball + BALL_MESSAGE);
-            }else {
+            } else {
                 System.out.println(ball + BALL_MESSAGE + " " + strike + STRIKE_MESSAGE);
             }
         }
