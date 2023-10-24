ca151e1ea6d2445801d6dd42b8e17ae9094fe77d
qlido <jason7530@naver.com>
2023-10-24 14:45:21 +0900

    Feat : 결과 출력하고 다시 시작하기 만들기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -16,7 +16,6 @@ public class Application {
                 systemNumber.add(number);
             }
         }
-
         while (isContinue) {
             int ball = 0, strike = 0;
 
@@ -42,6 +41,33 @@ public class Application {
                     } else {
                         ball++;
                     }
+                }
+            }
+            if (ball == 0 && strike == 0) {
+                System.out.println("낫싱");
+            } else if (ball == 0) {
+                System.out.println(strike + "스트라이크");
+            } else if (strike == 0) {
+                System.out.println(ball + "볼");
+            } else {
+                System.out.println(ball + "볼 " + strike + "스트라이크");
+            }
+
+            if (strike == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                String restartInput = Console.readLine();
+                if (restartInput.equals("1")) {
+                    isContinue = true;
+                    systemNumber.clear();
+                    while (systemNumber.size() < 3) {
+                        Integer number = Randoms.pickNumberInRange(0, 9);
+                        if (!systemNumber.contains(number)) {
+                            systemNumber.add(number);
+                        }
+                    }
+                } else {
+                    isContinue = false;
                 }
             }
         }
