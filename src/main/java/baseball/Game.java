ee198cff89af6d56961ce99ea9e1ec99219cf813
foura1201 <84717712+foura1201@users.noreply.github.com>
2023-10-24 23:10:07 +0900

    refactor: Do not use inheritance

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -6,7 +6,13 @@ import baseball.dto.Result;
 import baseball.global.Announcement;
 import baseball.global.util.RandomUtil;
 
-public class Game extends GameController {
+public class Game {
+
+    public static final int INPUT_LENGTH = 3;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 9;
+    public static final int GAME_AGAIN_CODE = 1;
+    public static final int GAME_TERMINATE_CODE = 2;
 
     private List<Character> answer;
     private int terminate;
@@ -21,15 +27,15 @@ public class Game extends GameController {
         this.answer = RandomUtil.randomNumber();
 
         Result result = new Result();
-        while (!isWin(result)) {
-            String input = getInGameInput();
-            result = recordResult(input, answer);
-            printResult(result);
+        while (!GameController.isWin(result)) {
+            String input = GameController.getInGameInput();
+            result = GameController.recordResult(input, answer);
+            GameController.printResult(result);
         }
 
         System.out.println(Announcement.GAME_WIN);
 
-        this.terminate = getTerminateInput();
+        this.terminate = GameController.getTerminateInput();
     }
 
     public boolean isTerminate() {
--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -8,12 +8,6 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class GameController {
 
-    public static final int INPUT_LENGTH = 3;
-    public static final int MIN_NUMBER = 1;
-    public static final int MAX_NUMBER = 9;
-    public static final int GAME_AGAIN_CODE = 1;
-    public static final int GAME_TERMINATE_CODE = 2;
-
     // 입력 받고 올바른 입력인지 확인
     public static String getInGameInput() {
         System.out.print(Announcement.IN_GAME);
@@ -23,7 +17,7 @@ public class GameController {
     // 받은 입력의 결과 기록
     public static Result recordResult(String input, List<Character> answer) {
         Result result = new Result();
-        for (int i = 0; i < INPUT_LENGTH; ++i) {
+        for (int i = 0; i < Game.INPUT_LENGTH; ++i) {
             char target = input.charAt(i);
             if (answer.contains(target)) {
                 if (answer.get(i) == target) {
@@ -54,7 +48,7 @@ public class GameController {
     }
 
     public static boolean isWin(Result result) {
-        if (result.getStrike() == INPUT_LENGTH) {
+        if (result.getStrike() == Game.INPUT_LENGTH) {
             return true;
         } else {
             return false;
