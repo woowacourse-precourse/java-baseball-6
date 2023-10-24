c7b0183762f1906a007c8fd3d8bde3cbdbc32aeb
jihoon98 <tlswlgns1003@sju.ac.kr>
2023-10-24 15:45:05 +0900

    git commit -m "feat:무작위 3자리자연수를 만드는 메서드 생성"

--- src/main/java/baseball/model/ComputerNumber.java
+++ src/main/java/baseball/model/ComputerNumber.java
@@ -1,4 +1,24 @@
 package baseball.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ComputerNumber {
+    private List<Integer> computerNumber;
+    public ComputerNumber(){
+        this.computerNumber=createRandomNumber();
+    }
+    private List<Integer> createRandomNumber() {//컴퓨터 3자리 자연수 랜덤 생성 메서드
+        List<Integer> randomNumbers = new ArrayList<>();
+        while (randomNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!randomNumbers.contains(randomNumber)) {
+                randomNumbers.add(randomNumber);
+            }
+        }
+        return randomNumbers;
+    }
+
 }
