db10035e5a2d27d4dc2295ba1c852114673e02d8
HyunJun KIM <hihj070914@icloud.com>
2023-10-24 21:27:27 +0900

    feat: executeBaseballGame placeholder & Game Loop - 숫자야구 게임 실행 함수 placeholder 구현 - 게임 종료 후 반복할 수 있도록 루프 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -28,15 +28,36 @@ public class Application {
         return userInput;
     }
 
+    public static boolean executeBaseballGame(String user, String cpu) {
+        if (user.equals(cpu)) {
+            System.out.println("3 스트라이크");
+            return true;
+        }
+        System.out.println("일단 3스트라이크는 아님");
+        return false;
+    }
+
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
         while (true) {
-            System.out.print("숫자를 입력해주세요 : ");
-            try {
-                String userInput = validateUserInput();
-                System.out.printf("User : %s, Cpu : %s\n", userInput, pickNumber());
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
+            String cpuNumber = pickNumber();
+            while (true) {
+                System.out.print("숫자를 입력해주세요 : ");
+                try {
+                    String userInput = validateUserInput();
+                    if (executeBaseballGame(userInput, cpuNumber)) {
+                        break;
+                    }
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                    break;
+                }
+            }
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String isContinue = Console.readLine();
+            if (isContinue.equals("2")) {
+                return;
             }
         }
     }
