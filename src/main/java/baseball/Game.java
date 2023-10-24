97ac5e0f3b99e3525ceffeaaba8863e6a471e730
장세진 <jsejin95@gmail.com>
2023-10-25 01:33:13 +0900

    edit: 출력 문구 수정

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -32,7 +32,7 @@ public class Game {
                     break;
                 }
             }
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             gameFlag = Integer.parseInt(Console.readLine());
         }
 
