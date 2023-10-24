ed47f735e919968f3f8fd371e3c36fa10969cb37
SB8919-LAPTOP\sb891 <sb8919@naver.com>
2023-10-24 23:11:32 +0900

    test(ValidatorTest) 플레이어_숫자_입력_자릿수마다_다르다 테스트 코드 작성

--- src/test/java/baseball/controller/ValidatorTest.java
+++ src/test/java/baseball/controller/ValidatorTest.java
@@ -34,4 +34,11 @@ public class ValidatorTest {
         String invalidInput = "1234";
         assertFalse(Validator.validateInputLength(invalidInput));
     }
+
+    @Test
+    @DisplayName("입력한 숫자가 자릿수마다 다르다")
+    void 플레이어_숫자_입력_자릿수마다_다르다() {
+        String validInput = "123";
+        assertTrue(Validator.validateInputWithDifferentDigit(validInput));
+    }
 }
