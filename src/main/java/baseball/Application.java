e1b7cb7a8da1880dab1a945e3e0e33b06e7752a0
ddshpp <dshp753@gmail.com>
2023-10-24 20:52:10 +0900

    feat: 사용자가 입력한 숫자가 중복이 없는지 확인하는 기능구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,10 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Application {
     public static void main(String[] args) {
@@ -21,5 +25,17 @@ public class Application {
                 throw new IllegalArgumentException("잘못된 입력입니다.");
             }
         }
+
+        Set<Integer> inputNumbers = new HashSet<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            inputNumbers.add(input.charAt(i) - '0');
+        }
+
+        if (inputNumbers.size() < 3) {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+
+
     }
 }
