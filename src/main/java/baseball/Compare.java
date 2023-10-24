0497920a444262240f28375bac047295341f8ab5
gunwoo kang <rkdrjsdn1000@gmail.com>
2023-10-24 16:51:38 +0900

    우테코 첫번째 프로젝트 숫자야구 게임 완료!

---
+++ src/main/java/baseball/Compare.java
@@ -0,0 +1,38 @@
+package baseball;
+
+import java.util.List;
+
+public class Compare {
+    // Compare Computer num and Player num, Count hit processing
+    public int Hit(List<Integer> computer, List<Integer> player) {
+        int result = 0;
+
+        for(int i = 0; i < player.size(); i++) {
+            if(computer.contains(player.get(i))) result++;
+        }
+        return result;
+    }
+
+    // Only Count hit strike
+    public int countStrike(List<Integer> computer, List<Integer> player) {
+        int strike = 0;
+
+        for(int i = 0; i < player.size(); i++) {
+            // Correct num and index = strike
+            if(computer.get(i) == player.get(i)) strike++;
+        }
+        return strike;
+    }
+
+    public String Classification(List<Integer> computer, List<Integer> player) {
+        int total = Hit(computer, player);
+        int strike = countStrike(computer, player);
+        int ball = total - strike;
+
+        if(total == 0) return "낫싱";
+        else if(strike == 0) return ball + "볼";
+        else if(ball == 0) return strike + "스트라이크";
+
+        return ball + "볼 " + strike + "스트라이크";
+    }
+}
