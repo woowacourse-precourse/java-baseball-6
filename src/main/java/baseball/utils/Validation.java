5534adb211bebfc961829fc574a2501dc388f4aa
sgo722 <sgo722@naver.com>
2023-10-25 00:44:12 +0900

    feat : 사용자 3자리 수 범위 검증기능 추가

--- src/main/java/baseball/utils/Validation.java
+++ src/main/java/baseball/utils/Validation.java
@@ -8,10 +8,20 @@ public class Validation {
 
     public static void verifyUserNumber(String userNumber){
         verifyUserNumberLength(userNumber);
+        verifyUserNumberRange(userNumber);
     }
 
     private static void verifyUserNumberLength(String userNumber) {
         if(userNumber.length() != 3){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void verifyUserNumberRange(String userNumber){
+        for(char eachNumber : userNumber.toCharArray()){
+            if(eachNumber >= 1 && eachNumber <= 9){
+                continue;
+            }
             throw new IllegalArgumentException();
         }
     }
