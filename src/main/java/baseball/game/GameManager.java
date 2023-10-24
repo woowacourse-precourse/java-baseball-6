872b67bfc4b33b099a9aab192c7e9e57a07e0895
wonseok2877 <wonseok2877@gmail.com>
2023-10-24 23:39:08 +0900

    refactor: GameManager 메서드 분리

--- src/main/java/baseball/game/GameManager.java
+++ src/main/java/baseball/game/GameManager.java
@@ -4,24 +4,40 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.Objects;
 
 public class GameManager {
+    private int decision;   // TODO: ENUM
+    private static final int RESTART_SIGN = 1;
+    private static final int END_SIGN = 2;
+    private static final String DECISION_GUIDE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String INVALID_DECISION_MESSAGE = "해당 입력은 1 또는 2만 가능합니다.";
+
     public void runGame() {
         do {
             Game game = new Game();
             game.start();
-        } while (isPlayerDecisionResume());
+            resolveDecisionInput();
+        } while (isPlayerDecisionRestart());
     }
 
-    private boolean isPlayerDecisionResume() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    private boolean isPlayerDecisionRestart() {
+        return Objects.equals(this.decision, RESTART_SIGN);
+    }
+
+    private void resolveDecisionInput() {
+        System.out.println(DECISION_GUIDE);
 
         String decisionInput = Console.readLine();
-        validateDecision(decisionInput);
-        return decisionInput.equals("1");
+        try {
+            int decision = Integer.parseInt(decisionInput);
+            validateDecisionInput(decision);
+            this.decision = decision;
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
     }
 
-    private void validateDecision(String decision) {    // TODO: String -> int
-        if (!Objects.equals(decision, "1") && !Objects.equals(decision, "2")) {
-            throw new IllegalArgumentException("해당 입력은 1 또는 2만 가능합니다.");
+    private void validateDecisionInput(int decisionInput) {
+        if (!Objects.equals(decisionInput, RESTART_SIGN) && !Objects.equals(decisionInput, END_SIGN)) {
+            throw new IllegalArgumentException(INVALID_DECISION_MESSAGE);
         }
     }
 }
