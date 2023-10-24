4bfc6b7c7c3d1cdac742033886034dc6749ae0d4
yoon <leeyt1201@gmail.com>
2023-10-25 04:48:37 +0900

    feat: implemented Score domain model - 점수 계산 - 결과 출력

--- src/main/java/baseball/constant/GameConstants.java
+++ src/main/java/baseball/constant/GameConstants.java
@@ -7,4 +7,8 @@ public final class GameConstants {
     public static final int BALL_MIN_NUMBER = 1;
     public static final int BALL_MAX_NUMBER = 9;
     public static final int BALL_COUNT = 3;
+
+    public static final String BALL = "볼";
+    public static final String STRIKE = "스트라이크";
+    public static final String NOTHING = "낫싱";
 }
