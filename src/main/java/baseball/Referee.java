d4434c0afdf38d0ac3a1fdd5ab7824bc2c21eb11
ddingmin <ddingminyt@gmail.com>
2023-10-25 02:26:51 +0900

    구현: 게임을 로직을 담당하는 Referee 클래스를 구현한다.
    1. 각 턴의 결과를 반환하는 기능 구현
        1. 볼을 계산하는 기능 구현
        2. 스트라이크를 계산하는 기능 구현
    2. 게임 결과를 반환하는 기능 구현

---
+++ src/main/java/baseball/Referee.java
@@ -0,0 +1,41 @@
+package baseball;
+
+public class Referee {
+	public static final int SUCCESS_STRIKE = 3;
+
+	public static TurnResult getTurnResult(String userNumber, String targetNumber) {
+		return new TurnResult(calculateBall(userNumber, targetNumber), calculateStrike(userNumber, targetNumber));
+	}
+
+	public static boolean getGameResult(TurnResult turnResult) {
+		if (turnResult.getStrike() == SUCCESS_STRIKE) {
+			return true;
+		}
+		return false;
+	}
+
+	private static int calculateBall(String userNumber, String targetNumber) {
+		int ballCount = 0;
+		for (int i = 0; i < InputView.CORRECT_LENGTH; i++) {
+			for (int j = 0; j < InputView.CORRECT_LENGTH; j++) {
+				if (i == j) {
+					continue;
+				}
+				if (userNumber.charAt(i) == targetNumber.charAt(j)) {
+					ballCount += 1;
+				}
+			}
+		}
+		return ballCount;
+	}
+
+	private static int calculateStrike(String userNumber, String targetNumber) {
+		int strikeCount = 0;
+		for (int i = 0; i < InputView.CORRECT_LENGTH; i++) {
+			if (userNumber.charAt(i) == targetNumber.charAt(i)) {
+				strikeCount += 1;
+			}
+		}
+		return strikeCount;
+	}
+}
