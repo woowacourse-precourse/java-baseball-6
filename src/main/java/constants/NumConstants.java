03ba8db41dd0202c3f161d2c945b982733cf8f44
34suuuuu <psh7492@gmail.com>
2023-10-24 18:04:36 +0900

    feat: 결과, 재시작, 종료 관련 상수 추가

--- src/main/java/constants/NumConstants.java
+++ src/main/java/constants/NumConstants.java
@@ -7,4 +7,9 @@ public class NumConstants {
     public static final int END_NUM = 9;
     public static final int INPUT_LENGTH = 3;
 
+    public static final int SUCCESS_STRIKE = 3;
+    public static final int NOTHING = 0;
+
+    public static final int INPUT_RESTART = 1;
+    public static final int INPUT_FINISH = 2;
 }
