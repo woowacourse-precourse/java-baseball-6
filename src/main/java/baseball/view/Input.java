7571bc919dd6d533faaa13e9efab97e453637517
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2023-10-24 23:04:54 +0900

    style: 줄바꿈 추가

--- src/main/java/baseball/view/Input.java
+++ src/main/java/baseball/view/Input.java
@@ -22,7 +22,7 @@ public class Input {
     }
 
     public boolean inputOneOrTwo() {
-        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String number = Console.readLine();
         exception.validateOneOrTwo(number);
         return number.equals("1");
