5710b1d96448989f60b95df20de82f8a7d45d80a
DrivenByCode <jaykey1004@gmail.com>
2023-10-24 14:37:29 +0900

    feat: validator can check whether input value is zero.

--- src/main/java/baseball/input/Validator.java
+++ src/main/java/baseball/input/Validator.java
@@ -2,7 +2,7 @@ package baseball.input;
 
 public class Validator {
     public static boolean isValid(String input) {
-        if (NullValidator.isValid(input) && LengthValidator.isValid(input) && NumberValidator.isValid(input)) {
+        if (ZeroValidator.isValid(input) && LengthValidator.isValid(input) && NumberValidator.isValid(input)) {
             return true;
         }
         return false;
