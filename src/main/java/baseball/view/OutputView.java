7f7a715f1e57905b9abee4c08f0f52b881947bdd
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-10-24 20:47:20 +0900

    feat : 판별결과 출력 함수 작성:

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -9,7 +9,18 @@ public class OutputView {
         System.out.print("숫자를 입력해주세요 : ");
     }
 
-    public void printResultMessage(Integer strike, Integer ball){
+    public void printResultMessage(int strike, int ball){
+        if(strike==0 && ball==0){
+            System.out.println("낫싱");
+        } else if (strike==0) {
+            System.out.println(ball+"볼");
+
+        } else if (ball ==0) {
+            System.out.println(strike+"스트라이크");
+        }
+        else{
+            System.out.println(ball+"볼 "+strike+"스트라이크");
+        }
 
     }
 }
