46bcda5469690608a609c09ff31cb654c5a17415
Zho <chlwlgh1011@naver.com>
2023-10-25 02:47:20 +0900

    1차 구현 완료_test fail

--- src/main/java/baseball/GameFunction.java
+++ src/main/java/baseball/GameFunction.java
@@ -2,10 +2,19 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class GameFunction {
+    private static final int[] userNum = new int[3];
+    private static final int[] compNum = new int[3];
+    static int strike = 0;
+    static int ball = 0;
+
     public static List<Integer> createRandomnumber(){
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
@@ -17,16 +26,60 @@ public class GameFunction {
         return computer;
         //System.out.println(computer);
     }
-
-    public static void gametry(String num) throws IllegalArgumentException{
-        List<Integer> computerNum = createRandomnumber();
+    public static void getUserNumber(String num) {
         if(num.length() != 3) throw new IllegalArgumentException();
-        int firstNum = num.indexOf(0);
-        int SecondNum = num.indexOf(1);
-        int ThirdNum = num.indexOf(2);
-        int firstComputerNum = computerNum.get(0);
-        int secondComputerNum = computerNum.get(1);
-        int thirdComputerNum = computerNum.get(2);
-        //System.out.println(firstNum  + SecondNum + ThirdNum + firstComputerNum + secondComputerNum + thirdComputerNum);
+        for(int i = 0; i < 3; i ++) {
+            userNum[i] = Character.getNumericValue(num.charAt(i));
+        }
+    }
+    public static void getComputerNumber() {
+        List<Integer> computerNum = createRandomnumber();
+        for(int i = 0; i < 3; i ++) {
+            compNum[i] = computerNum.get(i);
+        }
+    }
+
+    public static void gametry() throws IllegalArgumentException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        getComputerNumber();
+        while(true) {
+            strike = 0;
+            ball = 0;
+            System.out.print("숫자를 입력해주세요 : ");
+            String num = br.readLine();
+            getUserNumber(num);
+            System.out.println("computer num : " + Arrays.toString(compNum));
+            System.out.println("user num : " + Arrays.toString(userNum));
+            for (int i = 0; i < userNum.length; i++) {
+                for (int j = 0; j < compNum.length; j++) {
+                    if (userNum[i] == compNum[j]) {
+                        if (i == j) {
+                            strike++;
+                        } else {
+                            ball++;
+                        }
+                    }
+                }
+            }
+            if(strike == 0 && ball == 0) {
+                System.out.println("낫싱");
+            }
+            else if(strike == 0) {
+                System.out.println(ball + "볼");
+            }
+            else if(ball == 0) {
+                System.out.println(strike + "스트라이크");
+            }
+            else {
+                System.out.println(ball + "볼 " + strike + "스트라이크");
+            }
+            if(strike == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                int quitcheck = Integer.parseInt(br.readLine());
+                if(quitcheck == 2) break;
+            }
+        }
     }
 }
+        //System.out.println(firstNum  + SecondNum + ThirdNum + firstComputerNum + secondComputerNum + thirdComputerNum);
--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -1,18 +1,15 @@
 package baseball;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class GameManager {
     public void start() throws IOException {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        GameFunction.createRandomnumber();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        while(true){
-            System.out.print("숫자를 입력해주세요 : ");
-            String num = br.readLine();
-            GameFunction.gametry(num);
-        }
+        GameFunction.gametry();
+//        GameFunction.createRandomnumber();
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        System.out.print("숫자를 입력해주세요 : ");
+//        String num = br.readLine();
+//        GameFunction.gametry(num);
     }
 }
