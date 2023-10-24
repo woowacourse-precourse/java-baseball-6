ae52685f94de258a884a2e5dcaa212ed1507dd12
ffolabear <ffolabear@gmail.com>
2023-10-24 22:53:53 +0900

    feat : 변경된 게임 흐름 적용

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -1,4 +1,25 @@
 package baseball;
 
+import baseball.constant.Status;
+import baseball.constant.SystemMessage;
+
 public class GameController {
+
+    public static Status gameStatus = Status.NEW;
+
+    public void startGame() {
+        Game game = new Game();
+        SystemMessage.NEW.printMessage(gameStatus);
+        game.startGame();
+        System.out.println(gameStatus);
+        if (replayGame()) {
+            game.startGame();
+        }
+    }
+
+    private boolean replayGame(){
+        System.out.println("GameController.replayGame");
+        return gameStatus.equals(Status.GAME);
+    }
+
 }
