4bbb5dff958ed8a90d13f43b5ede8e70110de6ed
Jiyoung Choi <12201817@inha.edu>
2023-10-24 15:29:26 +0900

    style : 빈 줄 삭제

--- src/main/java/baseball/controller/HintCalculator.java
+++ src/main/java/baseball/controller/HintCalculator.java
@@ -80,5 +80,4 @@ public class HintCalculator {
         }
         return emptyString;
     }
-
 }
--- src/main/java/baseball/controller/NumberValidator.java
+++ src/main/java/baseball/controller/NumberValidator.java
@@ -57,5 +57,4 @@ public class NumberValidator {
                 .boxed()
                 .collect(Collectors.toList());
     }
-
 }
