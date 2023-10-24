3f595eacff413cd730035a051713a777e35b29c3
kimtaesoo <kimtaesoo7@naver.com>
2023-10-24 18:40:16 +0900

    feat: 야구의 단일 숫자 값을 포장하여 단일 값의 검증의 역할을 부여함

---
+++ src/main/java/baseball/model/Number.java
@@ -0,0 +1,44 @@
+package baseball.model;
+
+import java.util.Objects;
+
+public class Number {
+
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+    private static final String WRONG_VALUE_ERROR_MESSAGE = "1부터 9사이의 값이 아닙니다.";
+
+    private final int value;
+
+    public Number(int value) {
+        validateValueRange(value);
+        this.value = value;
+    }
+
+    private static void validateValueRange(final int value) {
+        if (!isBetweenOneAndNine(value)) {
+            throw new IllegalArgumentException(WRONG_VALUE_ERROR_MESSAGE);
+        }
+    }
+
+    private static boolean isBetweenOneAndNine(final int value) {
+        return MIN_NUMBER <= value && value <= MAX_NUMBER;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Number number = (Number) o;
+        return value == number.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
