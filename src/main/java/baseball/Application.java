fbe0cf57be1b3b21e8c7e92f7a36a4da4c351054
1027bestlife <1027bestlife@gmail.com>
2023-10-24 19:26:34 +0900

    Google Java Style Guide 적용

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,31 +5,35 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Random;
 
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
         System.out.println("숫자 야구 게임을 시작합니다.");
         List<Integer> computerNumbers = generateNumber();
 
-        int[] result = playGameWithGetGuessNumber(computerNumbers);
+        playGameWithGetGuessNumber(computerNumbers);
 
     }
 
     public static List<Integer> generateNumber() {
+
         List<Integer> computer = new ArrayList<>();
+
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
         }
+
         return computer;
     }
 
-    public static int[] playGameWithGetGuessNumber(List<Integer> computerNumbers) {
+    public static void playGameWithGetGuessNumber(List<Integer> computerNumbers) {
+
         while (true) {
             int[] result = new int[2]; // 볼, 스트라이크
             System.out.println("숫자를 입력해주세요 : ");
@@ -39,6 +43,7 @@ public class Application {
 
             int[] intArray = new int[parts.length];
             //intArray 에 문자를 숫자로 변경해서 변수에 할당
+
             for (int i = 0; i < parts.length; i++) {
                 intArray[i] = Integer.parseInt(parts[i]);
             }
@@ -67,12 +72,14 @@ public class Application {
             } else {
                 System.out.println("낫싱");
             }
+
             if (result[1] == 3) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
 
                 String answer = Console.readLine();
+
                 if ("1".equals(answer)) {
                     computerNumbers = generateNumber();
                     // 게임 재시작 하는 경우에 랜덤 숫자 갱신 필요
@@ -83,9 +90,6 @@ public class Application {
 
             }
         }
-
-
-        return new int[1];
     }
 
     public static boolean isCorrectGuess(int[] intArray) {
@@ -93,6 +97,7 @@ public class Application {
         if (intArray.length != 3) {
             return false;
         }
+
         // 1보다 작은숫자가 있는지 검사하는 코드
         for (int i = 0; i < intArray.length; i++) {
             if (intArray[i] < 1) {
