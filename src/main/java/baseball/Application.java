0a7bee4c1414662ad8a88db5ddb7a05296666475
kijen723 <kij2646@gmail.com>
2023-10-25 04:24:35 +0900

    feat: 재시작 선택 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -74,5 +74,11 @@ public class Application {
             }
         }
 
+        // TODO6: 게임 종료 후 게임 재시작 유무를 결정
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        Scanner scanner = new Scanner(System.in);
+        gamestart = scanner.nextInt();
+
     }
 }
