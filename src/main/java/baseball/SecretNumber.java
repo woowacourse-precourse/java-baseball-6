5a197b2ececb27f688684ed5609b08a450928073
Hepheir <hepheir@gmail.com>
2023-10-25 21:03:14 +0900

    Update SecretNumber to use multiple validators

--- src/main/java/baseball/SecretNumber.java
+++ src/main/java/baseball/SecretNumber.java
@@ -1,11 +1,20 @@
 package baseball;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SecretNumber extends ImmutableList<Digit> implements List<Digit> {
     public static final int SIZE = 3;
+    private static final List<SecretNumberValidator> validatorList = new ArrayList<>();
 
     public SecretNumber(List<Digit> digits) {
         super(digits);
+        validate();
+    }
+
+    private void validate() {
+        for (SecretNumberValidator validator : validatorList) {
+            validator.validate(this);
+        }
     }
 }
