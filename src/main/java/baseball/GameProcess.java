d2b5fe94ed19204cd0b27df0e271fba52ad36e24
2jin2.1031 <2jin2.1031@gmail.com>
2023-10-25 22:56:24 +0900

    style: gameprocess

--- src/main/java/baseball/GameProcess.java
+++ src/main/java/baseball/GameProcess.java
@@ -12,28 +12,28 @@ public class GameProcess {
         return input;
     }
 
-    Computer computer1 = new Computer();
+    Computer computer = new Computer();
 
     public static void process() {
 
         input = new ArrayList<>();
-        String num;
+        String inputData;
         int start_or_exit;
 
-        Computer computer1 = new Computer();
-        List<Integer> computerNumbers = computer1.generateRandomNumbers();
+        Computer computer = new Computer();
+        List<Integer> computerNumbers = computer.generateRandomNumbers();
         while (true) {
             try {
                 System.out.print("숫자를 입력해주세요 : ");
-                num = Console.readLine();
-                System.out.println(num.length());
-                if (num.length() != 3) {
+                inputData = Console.readLine();
+                System.out.println(inputData.length());
+                if (inputData.length() != 3) {
                     throw new IllegalArgumentException("입력된 숫자는 3자리가 아닙니다.");
                 }
                 input.clear();
 
                 for (int i = 0; i < 3; i++) {
-                    char digitChar = num.charAt(i); // 문자열 num에서 인덱스 i에 있는 문자를 가져와 digitChar 변수에 저장
+                    char digitChar = inputData.charAt(i); // 문자열 inputData에서 인덱스 i에 있는 문자를 가져와 digitChar 변수에 저장
                     String digitStr = String.valueOf(digitChar); // 문자 digitChar를 문자열로 반환하여 digitStr 변수에 저장
                     try {
                         int digit = Integer.parseInt(digitStr); // 문자열 digitStr을 정수로 변환하여 digit 변수에 저장
@@ -50,8 +50,10 @@ public class GameProcess {
                 if (inputset.size() != input.size()) {
                     throw new IllegalAccessException("입력된 숫자에는 중복된 숫자가 포함되어 있습니다.");
                 }
-
                 if (input.get(0) == input.get(1) || input.get(1) == input.get(2) || input.get(0) == input.get(2)) {
+                    throw new IllegalAccessException("입력된 숫자는 서로 다른 3자리 수가 아닙니다.");
+                }
+                if (Objects.equals(input.get(0), input.get(1)) || input.get(1) == input.get(2) || input.get(0) == input.get(2)) {
                     throw new IllegalAccessException("입력된 숫자는 서로 다른 3자리 수가 아닙니다.");
                 }
                 int strike = baseballScorer.countStrike(computerNumbers, input);
@@ -65,7 +67,7 @@ public class GameProcess {
                     if (start_or_exit == 2) { // 2일경우 게임 종료
                         break;
                     } else if (start_or_exit == 1) { // 1일 경우 게임 다시 시작
-                        computerNumbers = computer1.generateRandomNumbers(); //2
+                        computerNumbers = computer.generateRandomNumbers(); //2
                         continue;
                     }
                 } else {
