25d3503c5b6a4aaeacf38d48aa8a79359b931f02
kang jihyeong <her4386@gmail.com>
2023-10-25 18:46:59 +0900

    main : 게임 프로세스

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,12 +2,29 @@ package baseball;
 
 import baseball.startProcess.*;
 import baseball.finishProcess.*;
+import baseball.gameProcess.*;
+
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
+        GameRole gameRole = new GameRole();
+
+        StartProcess startProcess;
+        InProcess inProcess;
 
         while (true) {
-            startProcess startProcess = new startProcess();
+            startProcess = new StartProcess(gameRole);
+
+            List<Integer> computer = startProcess.getComputerNumber();
+
+            while (true) {
+                inProcess = new InProcess(gameRole, computer);
+                if (inProcess.isNumberCorrect()) break;
+            }
+
+            FinishProcess finishProcess = new FinishProcess();
+            if (finishProcess.isExitGame()) break;
         }
     }
 }
