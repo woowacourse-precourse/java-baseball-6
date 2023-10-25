dcd9219bee7d284883b71cde28be912aed0eccec
win9 <krrr883@gmail.com>
2023-10-25 16:50:16 +0900

    [Feat]: 맞는 부분이 없을 시 낫싱부를 출력하도록 변경
    볼이나 스트라이크가 둘다 0일시 낫싱을 출력

--- src/main/java/baseball/view/OutputMessage.java
+++ src/main/java/baseball/view/OutputMessage.java
@@ -6,7 +6,9 @@ public class OutputMessage {
     }
 
     public static void printResultMessage(int strike, int ball) {
-        if (strike == 0) {
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+        } else if (strike == 0) {
             System.out.println(ball + "볼");
         } else if (ball == 0){
             System.out.println(strike + "스트라이크");
