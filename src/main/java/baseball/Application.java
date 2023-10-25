ca3cf976184f9c93928193cf8b676535b8f88b8d
Dobby_Kim <kimdobby@g.skku.edu>
2023-10-25 19:51:07 +0900

    [fix: 출력문] 게임 시작시 출력되는 타이밍 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,7 +4,7 @@ public class Application {
     public static void main(String[] args) {
 
         boolean start = true;
-
+        System.out.println("숫자 야구 게임을 시작합니다.");
         while (start) {
 
             Player computer = new ComputerPlayer();
--- src/main/java/baseball/Judge.java
+++ src/main/java/baseball/Judge.java
@@ -43,7 +43,6 @@ public class Judge {
 
     public boolean startGame() {
         try {
-            System.out.println("숫자 야구 게임을 시작합니다.");
 
             boolean gameWin = false;
             List<Integer> computerNumbers = computer.getNumbers();
