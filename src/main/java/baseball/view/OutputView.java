991dedb55dfca22d0cca2a3a7126551afaae22fb
hansg931 <hansg0509@cau.ac.kr>
2023-10-25 09:11:09 +0900

    feat: 콘솔 출력 메소드 수정

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,31 +1,40 @@
 package baseball.view;
 
 import baseball.util.Message;
+
 public class OutputView {
-public static void printMessage(String message) {
+	public static void printMessage(String message) {
 		System.out.println(message);
 	}
+
 	public static void printGameStartMessage() {
 		printMessage(Message.GAME_START_MESSAGE);
 	}
+
 	public static void printInputNumberMessage() {
 		printMessage(Message.INPUT_NUMBER_MESSAGE);
 	}
+
 	public static void printGameEndMessage() {
 		printMessage(Message.GAME_END_MESSAGE);
 	}
+
 	public static void printGameRestartMessage() {
 		printMessage(Message.GAME_RESTART_MESSAGE);
 	}
+
 	public static void printBallMessage() {
 		printMessage(Message.BALL_MESSAGE);
 	}
+
 	public static void printStrikeMessage() {
 		printMessage(Message.STRIKE_MESSAGE);
 	}
+
 	public static void printNothingMessage() {
 		printMessage(Message.NOTHING_MESSAGE);
 	}
+
 	public static void printInputErrorMessage() {
 		printMessage(Message.INPUT_ERROR_MESSAGE);
 	}
