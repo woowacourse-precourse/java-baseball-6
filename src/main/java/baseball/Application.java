11b5e71f2e74ac0d91d4bcfd7712105dd6e2f81a
jieun4510@sju.ac.kr <jieun4510@sju.ac.kr>
2023-10-25 13:50:01 +0900

    feat: random number setting

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static int main(String[] args) {
+        // random number setting
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+//        System.out.print("computer: ");
+//        System.out.println(computer);
+        return 0;
     }
 }
