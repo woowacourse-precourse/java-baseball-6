354b5bbf8b283bf1547c15d7cbf0e1efa003dcaa
yeop0740 <ssucgoo@gmail.com>
2023-10-25 14:26:13 +0900

    refactor: 플레이어 관련 정보를 저장, 처리하는 User 클래스로 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,5 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,49 +9,28 @@ public class Application {
 
         printStartMessage();
 
-        boolean isContinued = true;
         Game game = new Game();
+        User user = new User();
 
-        while (isContinued) {
+        while (user.isContinued()) {
             game.init();
 
             while (!(game.getNumberOfStrike() == 3 && game.getNumberOfBall() == 0)) {
                 game.reset();
-                List<Integer> prediction = receiveUserInput();
-                game.countStrikeOrBall(prediction);
+                user.predict();
+                game.countStrikeOrBall(user.getPrediction());
                 printResultMessage(game.getNumberOfStrike(), game.getNumberOfBall());
             }
 
-            isContinued = isContinued();
+            user.decide();
         }
     }
 
-    private static boolean isContinued() throws IllegalArgumentException {
-        printReGameMessage();
-        String reply = Console.readLine();
-        if (reply.equals("1")) {
-            return true;
-        }
-
-        if (reply.equals("2")) {
-            return false;
-        }
-
-        throw new IllegalArgumentException("잘못된 값을 입력했습니다.");
-    }
-
-    private static void printReGameMessage() {
+    public static void printReGameMessage() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
-    private static List<Integer> receiveUserInput() {
-        printInputMessage();
-        String userInput = Console.readLine();
-        List<Integer> prediction = validateUserInput(userInput);
-        return prediction;
-    }
-
-    private static void printInputMessage() {
+    public static void printInputMessage() {
         System.out.print("숫자를 입력해주세요 : ");
     }
 
@@ -81,7 +58,7 @@ public class Application {
         System.out.printf("%d볼 %d스트라이크%n", numberOfBall, numberOfStrike);
     }
 
-    private static List<Integer> validateUserInput(String response) throws IllegalArgumentException {
+    public static List<Integer> validateUserInput(String response) throws IllegalArgumentException {
         List<Integer> prediction = new ArrayList<>();
 
         for (int i = 0; i < response.length(); i++) {
---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,49 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+
+    private final List<Integer> prediction;
+    private String continued;
+
+    public User() {
+        prediction = new ArrayList<>();
+        continued = "1";
+    }
+
+    public void predict() {
+        Application.printInputMessage();
+        String userInput = Console.readLine();
+        setPrediction(Application.validateUserInput(userInput));
+    }
+
+    public List<Integer> getPrediction() {
+        return prediction;
+    }
+
+    public boolean isContinued() throws IllegalArgumentException {
+        if (continued.equals("1")) {
+            return true;
+        }
+
+        if (continued.equals("2")) {
+            return false;
+        }
+
+        throw new IllegalArgumentException("잘못된 입력입니다.");
+    }
+
+    public void setPrediction(List<Integer> prediction) {
+        this.prediction.clear();
+        this.prediction.addAll(prediction);
+    }
+
+    public void decide() {
+        Application.printReGameMessage();
+        this.continued = Console.readLine();
+    }
+}
