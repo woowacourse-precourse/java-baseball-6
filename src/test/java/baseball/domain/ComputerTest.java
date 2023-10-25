27a58af57d9c7d96f8b726fc3d2684c67cc038d4
minu512 <minuet1215@jbnu.ac.kr>
2023-10-25 19:46:15 +0900

    test(ComputerTest): Computer 클래스를 테스트하는 코드 추가

---
+++ src/test/java/baseball/domain/ComputerTest.java
@@ -0,0 +1,25 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ComputerTest {
+    @Test
+    void 컴퓨터_난수_생성(){
+        Computer computer = new Computer();
+        assertThat(computer.generateRandomNumbers().size()).isEqualTo(3);
+    }
+
+    @Test
+    void 난수_중복_테스트() {
+        Computer computer = new Computer();
+
+        Map<Integer, Integer> computerMap = computer.generateRandomNumbers().stream()
+                .collect(Collectors.toMap(number -> number, number -> number));
+        assertThat(computerMap.size()).isEqualTo(3);
+    }
+}
