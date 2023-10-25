94dd96c292e09a98e5b753e795e32a9fe72a9c2b
chaehuichan <eqholic1125@gmail.com>
2023-10-25 21:54:04 +0900

    feat: 재시작 숫자 유효성 검사 기능 구현

--- src/main/java/baseball/number/RestartNumber.java
+++ src/main/java/baseball/number/RestartNumber.java
@@ -1,14 +1,26 @@
 package baseball.number;
 
 public class RestartNumber {
-    private boolean value;
+    private static final String TRUE = "1";
+    private static final String FALSE = "2";
+
+    private final boolean value;
 
     public RestartNumber(String restartNumber) {
-        // 1이면 true, 2면 false
-        // this.value = restartNumber;
+        this.value = validateRestartNumber(restartNumber);
     }
 
     public boolean getValue() {
         return value;
+    }
+
+    private boolean validateRestartNumber(String restartNumber) {
+        if (restartNumber.equals(TRUE)) {
+            return true;
+        } else if (restartNumber.equals(FALSE)) {
+            return false;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 }
