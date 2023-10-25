30995569af54a32e7b0d8292a42a6dc26906c307
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-10-25 16:49:03 +0900

    fix(Printer) : rename BaseballPrinter.java to Printer.java

--- src/main/java/baseball/Printer.java
+++ src/main/java/baseball/Printer.java
@@ -1,13 +1,10 @@
-package baseball.baseballGame;
+package baseball;
 
-import baseball.game.Printer;
-
-public class BaseballPrinter implements Printer {
-    @Override
+public class Printer {
     public void printStartMessage() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
-    @Override
+
     public void printReplayMessage() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
@@ -17,8 +14,8 @@ public class BaseballPrinter implements Printer {
     }
 
     public void printScore(BaseballForm result) {
-        Boolean isAnyStrike = !(result.getStrikeCnt().equals(0));
-        Boolean isAnyBall = !(result.getBallCnt().equals(0));
+        Boolean isAnyStrike = !(result.getNumberOfStrikes().equals(0));
+        Boolean isAnyBall = !(result.getNumberOfBalls().equals(0));
         Boolean isCorrect = result.getCorrect();
         Boolean isNothing = result.getNothing();
 
@@ -26,13 +23,13 @@ public class BaseballPrinter implements Printer {
             System.out.print("낫싱");
         }
         if (isAnyBall) {
-            System.out.print(result.getBallCnt() + "볼");
+            System.out.print(result.getNumberOfBalls() + "볼");
         }
         if (isAnyStrike) {
             if (isAnyBall) {
                 System.out.print(" ");
             }
-            System.out.print(result.getStrikeCnt() + "스트라이크");
+            System.out.print(result.getNumberOfStrikes() + "스트라이크");
         }
         if (isCorrect) {
             System.out.println();
