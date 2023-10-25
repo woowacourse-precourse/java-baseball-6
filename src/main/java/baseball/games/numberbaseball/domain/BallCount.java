4fa3bfe7eeb69b6e2b69fbfc4267e67fc01fc826
haebyun <bhk4392@gmail.com>
2023-10-25 19:20:56 +0900

    feat(BallCount): 현재 볼카운트 메세지를 문자열 형태로 반환해주는 메서드를 기능 구현

--- src/main/java/baseball/games/numberbaseball/domain/BallCount.java
+++ src/main/java/baseball/games/numberbaseball/domain/BallCount.java
@@ -1,6 +1,9 @@
 package baseball.games.numberbaseball.domain;
 
 public class BallCount {
+    private static final String BALL = "볼";
+    private static final String STRIKE = "스트라이크";
+    private static final String NOTHING = "낫싱";
     private final int ballCount;
     private final int strikeCount;
 
@@ -11,5 +14,18 @@ public class BallCount {
 
     public boolean isThreeStrike() {
         return strikeCount == 3;
+    }
+
+    public String createBallCountMessage() {
+        if (ballCount == 0 && strikeCount == 0) {
+            return NOTHING;
+        }
+        if (ballCount == 0) {
+            return strikeCount + STRIKE;
+        }
+        if (strikeCount == 0) {
+            return ballCount + BALL;
+        }
+        return ballCount + BALL + " " + strikeCount + STRIKE;
     }
 }
