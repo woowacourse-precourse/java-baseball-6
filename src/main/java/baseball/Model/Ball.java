902ae428702c16b6228fbe10b5486fbf7d93e254
gomdol <oioifor@naver.com>
2023-10-25 22:27:10 +0900

    feat : 유저 입력값 체크 함수

--- src/main/java/baseball/Model/Ball.java
+++ src/main/java/baseball/Model/Ball.java
@@ -3,17 +3,17 @@ package baseball.Model;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Ball {
-    private Integer numberLength = null;
+    private Integer length = null;
     private String number = "";
 
 
     public Ball(int numberLength){
-        this.numberLength = numberLength;
+        this.length = numberLength;
     }
 
     public String generateNumbers(){
         number = "";
-        while(number.length() < numberLength){
+        while(number.length() < length){
             int randomNumber = Randoms.pickNumberInRange(1, 9);
 
             if(!number.contains(Integer.toString(randomNumber))){
@@ -22,5 +22,23 @@ public class Ball {
         }
 
         return new String(number);
+    }
+
+    public void checkNumbers(String inputNumbers) throws IllegalArgumentException{
+        if(inputNumbers.length() != this.length){
+            throw new IllegalArgumentException();
+        }
+
+        for(int i = 0; i < this.length; i++){
+            String checkNumber = new String(inputNumbers);
+
+            if(!Character.isDigit(inputNumbers.charAt(i))){
+                throw new IllegalArgumentException();
+            }
+            if(checkNumber.replace(Character.toString(inputNumbers.charAt(i)), "").length() != this.length-1){
+                throw new IllegalArgumentException();
+            }
+
+        }
     }
 }
