f76dfca733e6db52f99f5cc0f254ef77d7f000a9
inpink <inpink@kakao.com>
2023-10-25 22:33:08 +0900

    refactor: createHint에서 active한 HintItem만 담아내는 기능 메서드로 분리.
    DB를 사용하는 구조라면 Repository에 의존하면 되지만 여기서는 그렇지 않으므로 Service가 다른 Service에 의존하고 있음.

--- src/main/java/baseball/service/hint/HintService.java
+++ src/main/java/baseball/service/hint/HintService.java
@@ -2,11 +2,14 @@ package baseball.service.hint;
 
 import baseball.domain.NumberBaseball;
 import baseball.domain.hint.Hint;
+import baseball.domain.hint.item.HintItem;
 import java.util.List;
 
 public interface HintService {
 
     Hint createHint(NumberBaseball computerBaseball, NumberBaseball inputBaseball);
+
+    List<HintItem> createActiveHintItems(NumberBaseball computerBaseball, NumberBaseball inputBaseball);
 
     List<String> counts(Hint hint);
 
--- src/main/java/baseball/service/hint/HintServiceImpl.java
+++ src/main/java/baseball/service/hint/HintServiceImpl.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 public class HintServiceImpl implements HintService {
 
-    //★DB를 사용하는 구조라면 Repository에 의존하면 되지만 여기서는 그렇지 않으므로 Service에 의존
     private List<HintItemService> hintItemServices;
 
     public HintServiceImpl(HintItemService... hintItemServices) {
@@ -18,15 +17,20 @@ public class HintServiceImpl implements HintService {
 
     @Override
     public Hint createHint(NumberBaseball computerBaseball, NumberBaseball inputBaseball) {
-        List<HintItem> hintItems = new ArrayList<>();
+        List<HintItem> hintItems =  createActiveHintItems(computerBaseball, inputBaseball);
+        return Hint.createHint(hintItems);
+    }
 
+    @Override
+    public List<HintItem> createActiveHintItems(NumberBaseball computerBaseball, NumberBaseball inputBaseball) {
+        List<HintItem> hintItems = new ArrayList<>();
         for (HintItemService hintItemServiceImpl : hintItemServices) {
             HintItem hintItem = hintItemServiceImpl.create(computerBaseball, inputBaseball);
             if (hintItemServiceImpl.isActive(hintItem)) {
                 hintItems.add(hintItem);
             }
         }
-        return Hint.createHint(hintItems);
+        return hintItems;
     }
 
     @Override
