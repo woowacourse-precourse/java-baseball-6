01deaa694f8b6ce3abc843886baa146e3ac2c0b6
win9 <krrr883@gmail.com>
2023-10-25 14:20:05 +0900

    [Feat]: 컨트롤러가 게임 흐름을 제어하도록 구현
    컨트롤러가 Service계층의 게임 중의 흐름을 이용하도록 구현

--- src/main/java/baseball/Controller/GameController.java
+++ src/main/java/baseball/Controller/GameController.java
@@ -1,5 +1,22 @@
 package baseball.Controller;
 
+import baseball.Service.GameService;
+
 public class GameController {
+    private GameService gameService = new GameService();
+
+    public void run() {
+        setGame();
+        playGame();
+    }
+
+    private void setGame() {
+        gameService.setGame();
+    }
+
+    private void playGame() {
+        gameService.play();
+    }
+
 
 }
