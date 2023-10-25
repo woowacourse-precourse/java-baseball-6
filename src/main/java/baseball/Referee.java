057793123b3382c0ab21398aac97650a0c88ad49
ParkJinYong <mej1406@gmail.com>
2023-10-25 20:31:44 +0900

    refactor: strike, ball count 리팩토링

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -1,21 +1,25 @@
 package baseball;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Referee {
     public RoundResult judge(List<Integer> catcher, List<Integer> pitcher) {
-        int strike = 0, ball = 0;
-
-        for (int i = 0; i < catcher.size(); i++) {
-            int pitcherNum = pitcher.get(i);
-            if (pitcherNum == catcher.get(i)) {
-                strike++;
-            }
-            else if (catcher.contains(pitcherNum)) {
-                ball++;
-            }
-        }
+        int strike = countStrike(catcher, pitcher);
+        int ball = countBall(catcher, pitcher);
 
         return new RoundResult(strike, ball);
+    }
+
+    public int countStrike(List<Integer> catcher, List<Integer> pitcher) {
+        return (int) Stream.iterate(0, i -> i + 1).limit(Constant.MAX_NUMBER_SIZE)
+                .filter(i -> catcher.get(i).equals(pitcher.get(i)))
+                .count();
+    }
+
+    public int countBall(List<Integer> catcher, List<Integer> pitcher) {
+        return (int)pitcher.stream()
+                .filter(catcher::contains)
+                .count();
     }
 }
