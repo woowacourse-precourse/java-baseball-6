3871e944bedf757532cedf31627b8871ed68ec08
doyoungkim97 <eh4536@naver.com>
2023-10-25 20:16:20 +0900

    fix : 게임 어플리케이션 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,32 @@
 package baseball;
 
+import baseball.Computer.Computer;
 import baseball.Game.Game;
+import baseball.View.InputView;
+import baseball.View.OutputView;
+
+import static baseball.Computer.Computer.checkNum;
+import static baseball.Computer.Computer.endRound;
+import static baseball.Game.Game.*;
 
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Game.Run();
+        Computer.InitGame();
+        OutputView.printStart();
+        while(retryEnd){
+            OutputView.printInput();
+            String userInputs = InputView.readGameNumber();
+            CreateBall(userInputs);
+            checkNum(userNumbers);
+            endRound();
+            checkGameEnd();
+            if(!gameContinue){
+                int userRetry = InputView.readRetryNumber();
+                checkRetry(userRetry);
+            }
+        }
+        OutputView.printEnd();
     }
 }
