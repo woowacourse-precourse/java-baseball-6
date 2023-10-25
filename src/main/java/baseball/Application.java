2706bba8c2772df341af3b8ab1e2b08f444c4ad8
신영한 <syhan7516@naver.com>
2023-10-25 23:47:44 +0900

    feat(Application.java) : 숫자 판단 기능 & 결과 문구 출력
    숫자 판단 기능
    (완료) 볼 : 숫자는 맞으나, 자리가 맞지 않을 경우
    (완료) 스트라이크 : 숫자와 자리가 모두 맞을 경우
    (완료) 낫싱 : 숫자와 자리 모두 맞지 않을 경우
    
    결과 문구 출력
    (완료) 숫자 판단에 따라 출력 포맷에 맞추어 결과 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -32,11 +32,94 @@ public class Application {
         return clientNumbers;
     }
 
+    // 낫싱 판단
+    static void checkNoting(List<Integer> randomNumbers, List<Integer> guessNumbers, List<Integer> numberCheckCount) {
+
+        for (int idx = 0; idx < 3; idx++) {
+            int checkNumber = guessNumbers.get(idx);
+            if (randomNumbers.contains(checkNumber)) {
+                numberCheckCount.add(0);
+                return;
+            }
+        }
+
+        numberCheckCount.add(1);
+    }
+
+    // 4.볼 판단
+    static void checkBall(List<Integer> randomNumbers, List<Integer> guessNumbers, List<Integer> numberCheckCount) {
+
+        int ballCount = 0;
+
+        for (int idx = 0; idx < 3; idx++) {
+            int checkNumber = guessNumbers.get(idx);
+
+            if (randomNumbers.contains(checkNumber) && checkNumber != randomNumbers.get(idx)) {
+                ballCount += 1;
+            }
+        }
+
+        numberCheckCount.add(ballCount);
+    }
+
+    // 5.스트라이크 판단
+    static void checkStrike(List<Integer> randomNumbers, List<Integer> guessNumbers, List<Integer> numberCheckCount) {
+
+        int strikeCount = 0;
+
+        for (int idx = 0; idx < 3; idx++) {
+            int checkNumber = guessNumbers.get(idx);
+            if (randomNumbers.contains(checkNumber) && checkNumber == randomNumbers.get(idx)) {
+                strikeCount += 1;
+            }
+        }
+
+        numberCheckCount.add(strikeCount);
+    }
+
+    // 6.숫자 판단 결과
+    static int getNoticeResult(List<Integer> numberCheckCount) {
+
+        // 각 결과 수
+        int nCount = numberCheckCount.get(0);
+        int bCount = numberCheckCount.get(1);
+        int sCount = numberCheckCount.get(2);
+
+        String noticeResult = "";
+
+        // 낫싱인 경우
+        if (nCount == 1) {
+            System.out.println("낫싱");
+            return -1;
+        }
+
+        // 볼이 있는 경우
+        if (bCount > 0) {
+            noticeResult = bCount + "볼" + " ";
+        }
+
+        // 스트라이크가 있는 경우
+        if (sCount > 0) {
+            noticeResult = noticeResult + sCount + "스트라이크";
+            System.out.println(noticeResult);
+
+            // 모두 맞힌 경우
+            if (sCount == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                return 1;
+            }
+        }
+
+        System.out.println(noticeResult);
+        return -1;
+    }
+
     public static void main(String[] args) {
 
-        // 임의의 수 3가지, 사용자 입력 수
+        // 임의의 수 3가지, 사용자 입력 수, 판단 리스트
         List<Integer> randomNumbers;
         List<Integer> guessNumbers;
+        List<Integer> numberCheckCount;
 
         // 게임 시작 문구 출력
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -54,5 +137,17 @@ public class Application {
         catch (IllegalArgumentException e) {
             return;
         }
+
+        // 숫자 판단 기능
+        numberCheckCount = new ArrayList<>();
+
+        // - 낫싱 판단
+        checkNoting(randomNumbers, guessNumbers, numberCheckCount);
+        // - 볼 판단
+        checkBall(randomNumbers, guessNumbers, numberCheckCount);
+        // - 스트라이크
+        checkStrike(randomNumbers, guessNumbers, numberCheckCount);
+        // - 숫자 판단 결과 함수 호출
+        getNoticeResult(numberCheckCount);
     }
 }
