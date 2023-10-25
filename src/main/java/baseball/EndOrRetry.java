d91156812e99cd2ecd87262e3a5d08935eed1844
phmhp <phm6318@sookmyung.ac.kr>
2023-10-25 18:12:08 +0900

    Comment : 주석추가

--- src/main/java/baseball/EndOrRetry.java
+++ src/main/java/baseball/EndOrRetry.java
@@ -2,23 +2,24 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.List;
-
 public class EndOrRetry {
     public boolean endOrRetry() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
         boolean reTry = true;
+
         while (reTry == true) {
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-
             char num = Console.readLine().charAt(0);
-            if (num == '1') //새로 시작
+
+            //게임 새로 시작
+            if (num == '1')
                 return true;
-            else if (num == '2') //종료
+            //게임 완전히 종료
+            else if (num == '2')
                 return false;
             else
                 reTry = exception_try(num);
-
         }
         return false;
     }
--- src/main/java/baseball/InputNum.java
+++ src/main/java/baseball/InputNum.java
@@ -1,28 +1,31 @@
 package baseball;
-
 import camp.nextstep.edu.missionutils.Console;
-
 import java.util.ArrayList;
 import java.util.List;
+
 public class InputNum {
+
     public List<Integer> getInputNum() {
         List<Integer> userNum = new ArrayList<>();
         boolean reInput = false;
+
         while (!reInput) {
             try {
                 System.out.print("숫자를 입력해주세요: ");
                 String inputNum = Console.readLine();
+                //IllegalArgumentException 실행
                 exception_input(inputNum);
 
                 for (String one : inputNum.split("")) {
                     userNum.add(Integer.parseInt(one));
                 }
-                System.out.println(userNum);
+
                 reInput = true;
             } catch (IllegalArgumentException e) {
                 throw new IllegalArgumentException("3자리 숫자를 입력해주세요.");
             }
         }
+        //제대로된 입력 값 전달
         return userNum;
     }
 
