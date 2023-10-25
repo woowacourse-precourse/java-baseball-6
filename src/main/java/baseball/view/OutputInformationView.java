6af21f3d87269de2dfb2a2798ee4fd4793b5631d
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-25 21:33:11 +0900

    [feat] : add start guide method

--- src/main/java/baseball/view/OutputInformationView.java
+++ src/main/java/baseball/view/OutputInformationView.java
@@ -6,4 +6,9 @@ public class OutputInformationView {
     private static final String checkAnswer = "3개의 숫자를 모두 맞추셨습니다. ";
     private static final String gameOver = "게임 종료.";
     private static final String gameEnd = "게임 실행을 종료합니다.";
+
+    // "숫자 야구 게임을 시작합니다." 문장을 출력하는 메서드.
+    public void startGuide() {
+        System.out.println(start);
+    }
 }
