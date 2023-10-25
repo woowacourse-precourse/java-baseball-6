cf7bf5b614ca590daed6de7daa16ca0a13890864
Jimin Kim <0305jimin@gmail.com>
2023-10-25 19:35:33 +0900

    Fix: static 변수 추가 및 자료형 변경, resetData 함수 시그니처 추가
    컴퓨터가 생성할 답안 수 저장 자료형을 int가 아닌 List<Interger>로 변경하고 strike와 ball 값을 추적할 변수를 추가합니다. + 게임 데이터 초기화 함수 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,16 +1,22 @@
 package baseball;
 
+
+import java.util.ArrayList;
+import java.util.List;
 public class Game {
-    static int target;
-    static boolean programRunning = 1;
-    static boolean gameRunning = 1;
+    static List<Integer> computer = new ArrayList<>();
+    static List<Integer> user = new ArrayList<>();
+    static boolean programRunning = true;
+    static boolean gameRunning = true;
+
+    static int strike = 0;
+    static int ball = 0;
 
     private void printGameStartMessage() {
 
     }
 
     private int generateRandomNumber() {
-
     }
 
     private void getUserInput() {
@@ -27,7 +33,7 @@ public class Game {
 
 
     private void startGame() {
-        target = generateRandomNumber();
+        generateRandomNumber();
 
         while (gameRunning) {
             getUserInput();
@@ -35,10 +41,14 @@ public class Game {
         }
     }
 
+    private void resetData() {
+    }
+
     public void startProgram() {
         printGameStartMessage();
         while (programRunning) {
             startGame();
+            resetData();
             examineNewGameOrNot();
         }
     }
