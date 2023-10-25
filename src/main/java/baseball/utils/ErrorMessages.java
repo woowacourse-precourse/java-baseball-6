978db37a187c70d874a9a04db111fbc05ebc5c5b
twoosky <lhjae3@naver.com>
2023-10-25 19:12:51 +0900

    feat: errorMessages 구현

---
+++ src/main/java/baseball/utils/ErrorMessages.java
@@ -0,0 +1,13 @@
+package baseball.utils;
+
+public class ErrorMessages {
+    public static final String PLAYER_NUMBERS_DUPLICATE = "[ERROR] 중복된 숫자는 입력할 수 없습니다.";
+    public static final String PLAYER_NUMBERS_INVALID_SIZE = "[ERROR] 3자리 수만 입력 가능합니다.";
+    public static final String PLAYER_NUMBERS_ZERO_CONTAIN = "[ERROR] 0이 포함된 수는 입력 불가능합니다.";
+
+    public static final String INVALID_BASEBALL_CNT = "[ERROR] 잘못된 BALL, STRIKE 개수입니다.";
+    public static final String NOT_MATCH_BASEBALL_RESULT = "[ERROR] 잘못된 숫자야구 결과입니다.";
+    public static final String NOT_INVALID_RETRY_INPUT = "[ERROR] 재시작: 1, 종료:2 를 입력해야 합니다.";
+
+    public static final String NOT_INTEGER = "[ERROR] 숫자가 아닙니다.";
+}
