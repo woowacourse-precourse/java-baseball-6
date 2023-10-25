e868530495d676434d4005093f6296a73d5adf41
chech2 <chaelim3396@gmail.com>
2023-10-25 18:47:34 +0900

    Fix) jdk

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,7 +6,6 @@ public class Application {
     public static void main(String[] args) {
         BaseballGameController baseballGameController = new BaseballGameController();
 
-        // TODO: 프로그램 구현
         baseballGameController.gameStart();
     }
 }
