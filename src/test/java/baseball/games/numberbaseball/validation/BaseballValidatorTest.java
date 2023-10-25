b0fad8d9494df3f556fd131a60e5d76971c45916
haebyun <bhk4392@gmail.com>
2023-10-25 18:15:21 +0900

    test(BaseballValidator): 숫자가 아닌 문자가 포함된 문자열에 대해 예외를 발생시키는 기능 테스트

--- src/test/java/baseball/games/numberbaseball/validation/BaseballValidatorTest.java
+++ src/test/java/baseball/games/numberbaseball/validation/BaseballValidatorTest.java
@@ -7,13 +7,20 @@ import org.junit.jupiter.api.Test;
 
 class BaseballValidatorTest {
     private final BaseballValidator validator = new BaseballValidator();
-
-
+    
     @Test
     @DisplayName("범위를 벗어난 수 입력에 대한 IllegalArgumentException")
     void validateOutRangeInput() {
         assertThatThrownBy(() -> {
             validator.validateNumbersInput("104");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("숫자가 아닌 입력에 대한 IllegalArgumentException")
+    void validateNotDigitInput() {
+        assertThatThrownBy(() -> {
+            validator.validateNumbersInput("a23");
         }).isInstanceOf(IllegalArgumentException.class);
     }
 }
