7ba483a91d3461a4eceba0b4d124eae8db9ac1d9
YoungJun-L <dudwns328@naver.com>
2023-10-25 19:09:11 +0900

    feat: 객체 의존성 주입

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -1,6 +1,18 @@
 package baseball.domain;
 
 public class Game {
+    private final Player player;
+    private final Computer computer;
+    private final Referee referee;
+    private final Printer printer;
+
+    public Game() {
+        this.player = new Player();
+        this.computer = new Computer();
+        this.referee = new Referee();
+        this.printer = new Printer();
+    }
+
     public void start() {
 
     }
--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -1,5 +1,9 @@
 package baseball.domain;
 
 public class Player {
+    private final Writer writer;
 
+    public Player() {
+        this.writer = new Writer();
+    }
 }
