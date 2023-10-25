c5a24671379af873bd17c9aab32677a643d197ea
김정민 <59995736+jmk445@users.noreply.github.com>
2023-10-25 21:34:40 +0900

    feat : exception 처리추가

--- src/main/java/baseball/ui/GameUI.java
+++ src/main/java/baseball/ui/GameUI.java
@@ -24,7 +24,7 @@ public class GameUI {
             if (guessValidator.isValidGuess(guess)) {
                 break;
             } else {
-                System.out.println("not valid input. please enter different 3 numbers");
+                throw new IllegalArgumentException("invalid input");
             }
         }
         return guess;
@@ -38,15 +38,19 @@ public class GameUI {
         }else if(result[0] != 0 && result[1] == 0){
             System.out.println(result[0] + "스트라이크");
         }else {
-            System.out.println(result[0] + "스트라이크" + result[1] + "볼");
+            System.out.println(result[1] + "볼 " + result[0] + "스트라이크");
         }
     }
 
     public int askForRestart() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        //System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        //System.out.println();
+        //scanner.nextLine();
         int choice = scanner.nextInt();
+
         if (choice != 1 && choice != 2) {
             System.out.println("not valid, just 2 or 1.");
+            //throw new IllegalArgumentException("invalid input");
             return askForRestart();
         }
         return choice;
