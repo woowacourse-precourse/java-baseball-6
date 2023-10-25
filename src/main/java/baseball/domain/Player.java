de29412a69391f1b748cefccb40e7364e1b5f805
minu512 <minuet1215@jbnu.ac.kr>
2023-10-25 19:47:25 +0900

    feat(Player): 입력 값을 저장하는 플레이어 클래스 추가

---
+++ src/main/java/baseball/domain/Player.java
@@ -0,0 +1,40 @@
+package baseball.domain;
+
+import baseball.view.InputView;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+public class Player {
+    private List<Integer> numbers;
+    public void inputNumbers() {
+        InputView.printInputPlayerNumbers();
+        String inputNumbers = Console.readLine();
+        validateInputNumbers(inputNumbers);
+        this.numbers = convertStringToList(inputNumbers);
+    }
+    private void validateInputNumbers(String inputNumbers) {
+        if (inputNumbers.length() != 3) {
+            throw new IllegalArgumentException("입력 값은 반드시 3자리여야 합니다.");
+        }
+
+        if (!inputNumbers.matches("^[0-9]*$")) {
+            throw new IllegalArgumentException("입력 값은 반드시 숫자여야 합니다.");
+        }
+
+        char[] digits = inputNumbers.toCharArray();
+        if (digits[0] == digits[1] || digits[0] == digits[2] || digits[1] == digits[2]) {
+            throw new IllegalArgumentException("입력 값은 중복되지 않은 숫자여야 합니다.");
+        }
+    }
+    private List<Integer> convertStringToList(String inputNumber) {
+        return Arrays.stream(inputNumber.split(""))
+                .mapToInt(Integer::parseInt)
+                .boxed().collect(Collectors.toList());
+    }
+
+    public List<Integer> getNumbers() {
+        return this.numbers;
+    }
+}
