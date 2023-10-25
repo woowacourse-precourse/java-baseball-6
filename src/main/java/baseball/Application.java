5a8b2a23f9b54bfb47f88da43df7162594fc4709
jeli01 <jeli01@naver.com>
2023-10-25 19:05:58 +0900

    else removed

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -19,11 +19,10 @@ public class Application {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 String s = readLine();
-                if (s.equals("1")) {
-                    baseballGame.updateGoalNum();
-                } else {
+                if (s.equals("2")) {
                     return;
                 }
+                baseballGame.updateGoalNum();
             }
         }
     }
