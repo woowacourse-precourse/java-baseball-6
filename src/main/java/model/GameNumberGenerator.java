4a0d2d4d60142112350876a8ffcf7e1585b1f2a5
JiEung2 <https://github.com/JiEung2>
2023-10-25 18:00:08 +0900

    클래스명 변경 및 최소 최대 숫자 상수 선언

--- src/main/java/model/GameNumberGenerator.java
+++ src/main/java/model/GameNumberGenerator.java
@@ -4,10 +4,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Queue;
 
-public class CreateGameNumber {
-    private final int number = 3;
+public class GameNumberGenerator {
+    private final int NUMBER = 3;
+    private final int MIN_NUMBER = 1;
+    private final int MAX_NUMBER = 9;
 
     /**
      * 컴퓨터가 각각 다른 3자리 숫자를 랜덤으로 생성해주는 기능
@@ -16,8 +17,8 @@ public class CreateGameNumber {
     public List<Integer> createNum(){
         List<Integer> computer = new ArrayList<>();
 
-        while (computer.size() < number) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+        while (computer.size() < NUMBER) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
