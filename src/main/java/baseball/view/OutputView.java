6f76b4727778c787cb81f51aeace20d6280c6f79
gundorit <packdev937@gmail.com>
2023-10-25 11:51:29 +0900

    feat (OutputView) : 숫자 입력 문구를 출력한다

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,8 +3,13 @@ package baseball.view;
 public class OutputView {
 
     public static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
 
     public static void printGameStart() {
         System.out.println(GAME_START_MESSAGE);
+    }
+
+    public static void printInputNumber() {
+        System.out.print(INPUT_NUMBER_MESSAGE);
     }
 }
