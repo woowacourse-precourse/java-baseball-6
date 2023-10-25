a4bbe506c471a1ee47da8ff0df13addedb44fc88
“sangjun121” <juncho12011201@gmail.com>
2023-10-25 20:35:11 +0900

    fix: 판단시 사용하는 변수들 리셋해 주는 함수 추가
    GameManager에서 Judgement 인스턴스를 실행 초기에 한번만 생성함으로 Judgement 내부의 변수값들(strikeCount, ballCount,gameResultDto)을 초기화 해줄 함수 setDefaultValue() 구현 완료

--- src/main/java/baseball/controller/Judgement.java
+++ src/main/java/baseball/controller/Judgement.java
@@ -13,19 +13,19 @@ public class Judgement {
     private List<Integer> userNumber;
     private int strikeCount;
     private int ballCount;
+    private boolean isCorrectNumber;
     private GameResultDto gameResultDto;
 
-    public Judgement(){
-        strikeCount = 0;
-        ballCount = 0;
-        gameResultDto = new GameResultDto();
-    }
+    public Judgement(){}
 
     public GameResultDto compareNumbers(ComputerNumberDto computerNumberDto, UserNumberDto userNumberDto){
+        setDefaultValue(); //변수값들 초기화
+
         computerNumber = computerNumberDto.getComputerNumber();
         userNumber = userNumberDto.getUserNumber();
         judgeStrike(); //스트라이크 여부 판단
         judgeBall(); //볼 여부 판단
+        judgeCorrectNumber(); //정답여부 판단
         setGameResultDto(); //결과값을 담아서 전달해줄 dto 값 설정
         return gameResultDto;
     }
@@ -40,14 +40,30 @@ public class Judgement {
 
     private void judgeBall(){
         for(int i = 0; i < NUMBER_LIMIT_LENGTH; i++){
-            if(computerNumber.get(i) != userNumber.get(i) && computerNumber.contains(userNumber.get(i))){
+            if((computerNumber.get(i) != userNumber.get(i)) && computerNumber.contains(userNumber.get(i))){
                 ballCount++;
             }
+        }
+    }
+
+    private void judgeCorrectNumber(){
+        if(strikeCount == 3){
+            isCorrectNumber = true;
+        }
+        else{
+            isCorrectNumber = false;
         }
     }
 
     private void setGameResultDto(){
         gameResultDto.setStrikeCount(strikeCount);
         gameResultDto.setBallCount(ballCount);
+        gameResultDto.setIsCorrectNumber(isCorrectNumber);
+    }
+
+    private void setDefaultValue(){
+        strikeCount = 0;
+        ballCount = 0;
+        gameResultDto = new GameResultDto();
     }
 }
