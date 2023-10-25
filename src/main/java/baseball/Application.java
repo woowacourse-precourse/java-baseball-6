c1ece24fea286941fe6bc30f9266e3e8b6aa7d73
동원 <ai06021@gmail.com>
2023-10-25 20:38:15 +0900

    refactor : 종료조건

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,7 +12,9 @@ public class Application {
                 break;
             }
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
-
+            if (play.newGameCheck() != 1) {
+                break;
+            }
         }
 
 
