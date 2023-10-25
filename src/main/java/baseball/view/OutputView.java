c0a0c7d91ad2bc7fa732e37c003745ef48264a31
gundorit <packdev937@gmail.com>
2023-10-25 11:55:40 +0900

    feat (OutputView) : 게임 재시작 문구를 출력한다

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -5,7 +5,7 @@ public class OutputView {
     public static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     public static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
     public static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-
+    public static final String GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public static void printGameStart() {
         System.out.println(GAME_START_MESSAGE);
@@ -19,4 +19,7 @@ public class OutputView {
         System.out.println(GAME_END_MESSAGE);
     }
 
+    public static void printGameRestart() {
+        System.out.println(GAME_RESTART_MESSAGE);
+    }
 }
