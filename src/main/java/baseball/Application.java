1764cf50081b3c6f23bf049d1bdbabf390d303a5
dongha922 <kdh4575@naver.com>
2023-10-25 22:19:45 +0900

    세팅

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,37 @@
 package baseball;
+//import camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+//import camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구를 시작합니다");
+    }
+}
+public class Game{
+    private List<Integer> number;
+    public Game(){
+
+    }
+    public void run(){
+        try{
+            do {
+                play();
+            } while (reStrart() == true);
+        }catch (IllegalArgumentException e){
+            throw e;
+        }
+
+    }
+    private void play(){
+        String input;
+        init();
+        do{
+            try{
+                input = getList();
+            }catch (IllegalArgumentException e){
+                throw e;
+            }
+        }while(checkResult(input) == false);
     }
 }
