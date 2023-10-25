3f693095cc7f32f299479f8df136aae002167141
realcold0 <realcold0@gmail.com>
2023-10-25 13:59:57 +0900

    feat: 입력한 정답 실제 정답과 비교하는 CompareAnswerWithPlayerNumber Service모델 기능 생성

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -2,17 +2,20 @@ package baseball.controller;
 
 import baseball.domain.AnswerCreator;
 import baseball.domain.Player;
+import baseball.service.CompareAnswerWithPlayerNumber;
 import baseball.view.InputView;
 
 public class GameController {
     private AnswerCreator answerCreator = new AnswerCreator();
     private Player player = new Player();
     private InputView inputView = new InputView();
+    private CompareAnswerWithPlayerNumber compareAnswerWithPlayerNumber = new CompareAnswerWithPlayerNumber();
 
     public void startGame() {
         answerCreator.setComputerAnswer();  //게임 정답 세팅
         player.setPlayerAnswer(inputView.inputPlayerNumbers());//플레이어의 정답 작성
-        //플레이어 정답, 게임정답 비교
+        compareAnswerWithPlayerNumber.compareStrikeBall(player.getPlayerAnswer(),
+                answerCreator.getComputerAnswer());//플레이어 정답, 게임정답 비교
         //게임 결과 확인
 
     }
---
+++ src/main/java/baseball/service/CompareAnswerWithPlayerNumber.java
@@ -0,0 +1,38 @@
+package baseball.service;
+
+import java.util.List;
+
+public class CompareAnswerWithPlayerNumber {
+    private int strike;
+    private int ball;
+
+
+    public void compareStrikeBall(List<Integer> player, List<Integer> computer) {
+        compareStrike(player, computer);
+        compareBall(player, computer);
+    }
+
+    public void compareStrike(List<Integer> player, List<Integer> computer) {
+        for (int i = 0; i < player.size(); i++) {
+            if (player.get(i).equals(computer.get(i))) {
+                strike++;
+            }
+        }
+    }
+
+    public void compareBall(List<Integer> player, List<Integer> computer) {
+        for (int i = 0; i < player.size(); i++) {
+            if (computer.contains(player.get(i))) {
+                ball++;
+            }
+        }
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+}
