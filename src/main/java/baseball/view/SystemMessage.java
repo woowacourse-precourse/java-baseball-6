af84e681f976aa254f06c511c810936b824fe313
edjjincode <138580800+edjjincode@users.noreply.github.com>
2023-10-25 09:44:05 +0900

    feat(apply): upload SystemMessage.java
    Add SystemMessage.java
    - print hints when user fill out the input

---
+++ src/main/java/baseball/view/SystemMessage.java
@@ -0,0 +1,23 @@
+package baseball.view;
+
+public class SystemMessage {
+
+    public void printScoreMessage(int ball, int strike) {
+        if (ball == 0 && strike == 0) {
+            System.out.println("낫싱");
+        }
+        if (ball == 0 && strike != 0) {
+            System.out.println(strike + "스트라이크");
+        }
+        if (ball != 0 && strike == 0) {
+            System.out.println(ball + "볼");
+        }
+        if (ball != 0 && strike != 0) {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
+    }
+
+    public static void printGameOverMessage() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+}
