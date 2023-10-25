23617866cee651090a8e9ee7cea1069bafd10d61
PlutoWooSeok <plutoxxx1014@gmail.com>
2023-10-25 16:14:46 +0900

    Application Run Msg

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,6 +6,7 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         GameController gameController = new GameController();
+        //프로그램 실행
         gameController.playGame();
     }
 }
