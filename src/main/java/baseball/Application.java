c9e52a6cd7a339445a02cf1c51b73258e4c07199
Jehyun Lee <76717407+Le-Jay@users.noreply.github.com>
2023-10-25 23:51:46 +0900

    Update Application.java

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -33,6 +33,7 @@ public class Application {
     public static void main(String[] args) {
 
         RandomNumberGenerator();
+        
         System.out.println("숫자 야구 게임을 시작합니다.");
         
         while(true) {
