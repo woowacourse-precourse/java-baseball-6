8f7b2affb800eb0d532291282bc96513e71a792a
DavinAn <blueapple1030@gmail.com>
2023-10-25 23:25:41 +0900

    feat(controller) : 게임 시작 기능

--- src/main/java/baseball/BaseballController.java
+++ src/main/java/baseball/BaseballController.java
@@ -1,10 +1,10 @@
 package baseball;
 
+import java.util.List;
+
 /**
  * 게임을 시작, 재시작, 종료하는 기본 로직
  */
-
-
 public class BaseballController {
 
   // Fields
@@ -24,10 +24,14 @@ public class BaseballController {
 
 
   public void playBaseball() {
+    List<Integer> computer = null; // 랜덤 3자리 숫자
+    List<Integer> player = null; // 사용자 입력 3자리 숫자
+    List<Integer> gameResult; // [스트라이크, 볼] 값을 담은 리스트
+
+    System.out.println("숫자 야구 게임을 시작합니다.");
+    computer = baseballRandomNumber.generate(computer);
+
 
   }
-
-
-
-
 }
+
