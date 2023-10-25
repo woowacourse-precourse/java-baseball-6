8a5830e8fc3199d07c372180eed029f3853fba3a
sheepysnoopy <qkffldlfdjsk12@naver.com>
2023-10-25 14:19:21 +0900

    refactor: ExceptionHandler 삭제, UserInputNumberManager에서 개별 예외처리

--- src/main/java/baseball/UserInputNumberManager.java
+++ src/main/java/baseball/UserInputNumberManager.java
@@ -1,24 +1,57 @@
 package baseball;
 
-import baseball.exeption.UserNumberExceptionContoller;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static baseball.enum_class.Message.INPUT_USERNUMBER_MESSAGE;
+import static baseball.enum_class.NumberInfo.COUNT;
 
 public class UserInputNumberManager implements UserNumberManager{
     private String userNumber;
-
-    UserNumberExceptionContoller exceptionContoller = new UserNumberExceptionContoller();
 
     public String getUserNumber() {
         System.out.print(INPUT_USERNUMBER_MESSAGE.getMessage());
         userNumber = Console.readLine();
 
-        exceptionContoller.notIntegerException(userNumber);
-        exceptionContoller.notThreeDigitException(userNumber);
-        exceptionContoller.duplicationException(userNumber);
-        exceptionContoller.zeroValueException(userNumber);
+        validInteger(userNumber);
+        validDuplication(userNumber);
+        validThreeDigit(userNumber);
+        validZeroValue(userNumber);
 
         return userNumber;
+    }
+
+    public void validInteger(String Number) {
+        for (int i = 0; i < COUNT.getNumberInfo(); i++) {
+            if (!Character.isDigit(Number.charAt(i))) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public void validDuplication(String Number) {
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < COUNT.getNumberInfo(); i++) {
+            set.add(Number.charAt(i));
+        }
+        if (!(set.size() == COUNT.getNumberInfo())) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void validThreeDigit(String Number) {
+        if (Number.length() != COUNT.getNumberInfo()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void validZeroValue(String Number) {
+        for (int i = 0; i < Number.length(); i++) {
+            if (Number.charAt(i) == '0') {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 }
