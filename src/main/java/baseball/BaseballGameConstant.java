377ecf6743506e11a4bfe0130776c11a42f60a0a
minjikim <inkmjj@sookmyung.ac.kr>
2023-10-25 22:55:46 +0900

    feat: BaseballGameConstant.java 구현

---
+++ src/main/java/baseball/BaseballGameConstant.java
@@ -0,0 +1,17 @@
+package baseball;
+
+public class BaseballGameConstant {
+
+    // messages
+    public static String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public static String GAME_ENDED_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static String GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static String GAME_INPUT_USER_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    public static String GAME_BALL_STRING = "볼 ";
+    public static String GAME_STRIKE_STRING = "스트라이크";
+    public static String GAME_NOTHING_STRING = "낫싱";
+
+    // numbers
+    public static int NUMBER_LENGTH = 3;
+
+}
