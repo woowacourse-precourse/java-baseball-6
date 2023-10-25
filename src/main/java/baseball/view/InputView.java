ff68ae7615b39650070c9cac3024645d8d7fae11
706com <ehdgusjdbswl@gmail.com>
2023-10-25 11:07:21 +0900

    refactor : 불변객체, 정적메서드로 전환

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -2,19 +2,17 @@ package baseball.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Scanner;
-
 public class InputView {
 
-    private final String inputNumberMessage = "숫자를 입력해주세요 : ";
+    private static final String inputNumberMessage = "숫자를 입력해주세요 : ";
+    private static final String inputRestartMessage = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
-    private final String inputRestartMessage = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-
-    public String inputNumbers(){
+    public static String inputNumbers() {
         System.out.print(inputNumberMessage);
         return Console.readLine();
     }
-    public String inputRestart(){
+
+    public static String inputRestart() {
         System.out.println(inputRestartMessage);
         return Console.readLine();
     }
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -2,31 +2,28 @@ package baseball.view;
 
 public class OutputView {
 
-    public final String gameStartMessage = "숫자 야구 게임을 시작합니다.";
+    public static final String gameStartMessage = "숫자 야구 게임을 시작합니다.";
+    public static final String matchAllNumberMessage = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String ballMessage = "볼 ";
+    public static final String strikeMessage = "스트라이크 ";
+    public static final String nothingMessage = "낫싱";
 
-    public final String matchAllNumberMessage = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-
-    public final String ballMessage = "볼 ";
-
-    public final String strikeMessage = "스트라이크 ";
-
-    public final String nothingMessage = "낫싱";
-
-    public void printGameStartMessage(){
+    public static void printGameStartMessage() {
         System.out.println(gameStartMessage);
     }
-    public void printMatchAllNumberMessage(){
+
+    public static void printMatchAllNumberMessage() {
         System.out.println(matchAllNumberMessage);
     }
 
-    public void printResultMessage(int strike, int ball){
-        if(ball != 0){
+    public static void printResultMessage(int strike, int ball) {
+        if (ball != 0) {
             System.out.print(ball + ballMessage);
         }
-        if(strike != 0){
+        if (strike != 0) {
             System.out.print(strike + strikeMessage);
         }
-        if(ball == 0 && strike == 0){
+        if (ball == 0 && strike == 0) {
             System.out.print(nothingMessage);
         }
         System.out.println();
