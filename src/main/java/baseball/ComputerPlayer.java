3450b440a6a88931d347230bb4c3b6afa7539eb3
7ngenious <zxvs1218@gmail.com>
2023-10-25 17:29:28 +0900

    feat: Add checkGuess&displayResult

--- src/main/java/baseball/ComputerPlayer.java
+++ src/main/java/baseball/ComputerPlayer.java
@@ -9,18 +9,51 @@ public class ComputerPlayer {
         numbers = new int[3];
     }
 
-//    public int[] getNumbers() {
-//        return numbers;
-//    }
-//
-//    public void setNumbers(int[] numbers) {
-//        this.numbers = numbers.clone();
-//    }
-
     public int[] generateRandomNumber() {
         for (int i = 0; i < 3; i++) {
             numbers[i] = Randoms.pickNumberInRange(1, 9);
         }
         return numbers;
+    }
+
+    public int[] checkGuess(int[] userGuess, int[] computerNumber) {
+        int strikes = 0;
+        int balls = 0;
+
+        for (int i = 0; i < 3; i++) {
+            if (userGuess[i] == computerNumber[i]) {
+                strikes++;
+            } else if (contains(userGuess[i], computerNumber)) {
+                balls++;
+            }
+        }
+
+        return new int[] { strikes, balls };
+    }
+
+    public void displayResult(int[] result) {
+        int strikes = result[0];
+        int balls = result[1];
+
+        if (strikes > 0) {
+            System.out.println(strikes + " 스트라이크");
+        }
+
+        if (balls > 0) {
+            System.out.println(balls + " 볼");
+        }
+
+        if (strikes == 0 && balls == 0) {
+            System.out.println("Nothing");
+        }
+    }
+
+    private boolean contains(int digit, int[] numberArray) {
+        for (int number : numberArray) {
+            if (number == digit) {
+                return true;
+            }
+        }
+        return false;
     }
 }
