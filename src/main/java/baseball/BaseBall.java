4c50c193ac3dca7fc00cca032a979a48e51af7e7
JunRain <sa2313@tukorea.ac.kr>
2023-10-25 14:07:18 +0900

    사용자 입력 리스트로 변환 및 형식 검사 구현

--- src/main/java/baseball/BaseBall.java
+++ src/main/java/baseball/BaseBall.java
@@ -1,16 +1,21 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Stream;
+
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class BaseBall {
 	Scanner scanner = new Scanner(System.in);
 
-	void startGame(){
+	void startGame() {
 		int gameFlag = 1;
 
 		while (gameFlag == 1) {
 			baseBallGame();
-			
+
 			System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 			gameFlag = scanner.nextInt();
 
@@ -23,5 +28,45 @@ public class BaseBall {
 	}
 
 	private void baseBallGame() {
+		List<Integer> computer = createNum();
+		int userInput;
+
+		while (true) {
+			int strike = 0;
+			int ball = 0;
+
+			userInput = scanner.nextInt();
+			List<Integer> user = checkNum(userInput);
+
+		}
+	}
+
+	private List<Integer> checkNum(int userInput) {
+		List<Integer> user = new ArrayList<>();
+		int userInputLength = (int)Math.log10(userInput) + 1;
+
+		if (userInputLength != 3) {
+			throw new IllegalArgumentException();
+		}
+
+		user = Stream.of(String.valueOf(userInput).split("")).mapToInt(Integer::parseInt).boxed().toList();
+
+		for (int i = 0; i < 3; i++) {
+			if (user.contains(user.get(i)))
+				throw new IllegalArgumentException();
+		}
+
+		return user;
+	}
+
+	private List<Integer> createNum() {
+		List<Integer> computer = new ArrayList<>();
+		while (computer.size() < 3) {
+			int randomNumber = Randoms.pickNumberInRange(1, 9);
+			if (!computer.contains(randomNumber)) {
+				computer.add(randomNumber);
+			}
+		}
+		return computer;
 	}
 }
