50f1bd1e3adfe5211c56d5f593c1237f173ce097
Jiyoon Kim <rlawldbs0911@naver.com>
2023-10-25 19:00:06 +0900

    feat(PlayerNumbers): Player 클래스명 변경 및 유효성 검증 메소드 추가

--- src/main/java/baseball/InputValidator.java
+++ src/main/java/baseball/InputValidator.java
@@ -1,45 +1,13 @@
 package baseball;
 
-import java.util.Arrays;
 import java.util.regex.Pattern;
 
 public class InputValidator {
     private static final String REGEX = "^[0-9]+$";
-    private static final String ELEMENT = "";
-    private static final int COUNT_OF_NUMBER = 3;
 
-    public void validatePlayerNumber(String number) {
-        validateCountOfNumber(number);
-        validateNumberIsDigit(number);
-        validateNumberHasDuplicate(number);
-    }
-
-    public void validateRestartNumber(String number) {
-        validateOneOrTwo(number);
-    }
-
-    private void validateOneOrTwo(String number) {
-        if (Arrays.stream(RestartStatus.values())
-                .noneMatch(status -> status.toString().equals(number))) {
-            throw new IllegalArgumentException(ExceptionMessage.IS_NOT_RESTART_NUMBER.toString());
-        }
-    }
-
-    private void validateNumberIsDigit(String number) {
+    public void validateIsDigit(String number) {
         if (!Pattern.matches(REGEX, number)) {
             throw new IllegalArgumentException(ExceptionMessage.IS_NOT_DIGIT.toString());
-        }
-    }
-
-    private void validateNumberHasDuplicate(String number) {
-        if(number.length() != Arrays.stream(number.split(ELEMENT)).distinct().count()){
-            throw new IllegalArgumentException(ExceptionMessage.HAS_DUPLICATE.toString());
-        }
-    }
-
-    private void validateCountOfNumber(String number) {
-        if (number.length() != COUNT_OF_NUMBER) {
-            throw new IllegalArgumentException(ExceptionMessage.IS_NOT_THREE.toString());
         }
     }
 }
---
+++ src/main/java/baseball/PlayerNumbers.java
@@ -0,0 +1,30 @@
+package baseball;
+
+import java.util.List;
+
+public class PlayerNumbers {
+    private static final int COUNT_OF_NUMBER = 3;
+    private final List<Integer> numbers;
+
+    public PlayerNumbers(List<Integer> numbers) {
+        validateCountOfNumber(numbers);
+        validateNumberHasDuplicate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validateCountOfNumber(List<Integer> numbers) {
+        if (numbers.size() != COUNT_OF_NUMBER) {
+            throw new IllegalArgumentException(ExceptionMessage.IS_NOT_THREE.toString());
+        }
+    }
+
+    private void validateNumberHasDuplicate(List<Integer> numbers) {
+        if(numbers.size() != numbers.stream().distinct().count()){
+            throw new IllegalArgumentException(ExceptionMessage.HAS_DUPLICATE.toString());
+        }
+    }
+
+    public int get(int index) {
+        return numbers.get(index);
+    }
+}
