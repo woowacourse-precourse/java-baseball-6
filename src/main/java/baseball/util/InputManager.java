8562e42adfe853a8acf22165cfba16ae05f5ec7a
YEJIN <yejon9858@gmail.com>
2023-10-25 11:08:58 +0900

    [FEAT] Player Input Integer List 변환

--- src/main/java/baseball/util/InputManager.java
+++ src/main/java/baseball/util/InputManager.java
@@ -7,8 +7,17 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputManager {
     public List<Integer> getPlayerInputNumbers() throws IllegalArgumentException{
-        //TODO Player 입력 숫자 Integer List로 변환
-        return List.of();
+        String inputLine = readLine();
+        if(inputLine.length() != 3) throw new IllegalArgumentException();
+        List<Integer> numbers = new ArrayList<>();
+
+        for(char c : inputLine.toCharArray()) {
+            int inputNum = c - '0';
+            if(inputNum < 1 || inputNum > 9) throw new IllegalArgumentException();
+            if(numbers.contains(inputNum)) throw new IllegalArgumentException();
+            numbers.add(inputNum);
+        }
+        return numbers;
     }
 
     public boolean checkUserContinue() throws IllegalArgumentException {
