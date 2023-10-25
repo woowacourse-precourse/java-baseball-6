33cd6f3d78eaaa4d778091b6f82a21ff1a41047a
shortboy7 <thdwldnd7@naver.com>
2023-10-25 14:03:11 +0900

    feat:implement menu phase

--- src/main/java/baseball/phase/impl/MenuPhase.java
+++ src/main/java/baseball/phase/impl/MenuPhase.java
@@ -13,17 +13,29 @@ public class MenuPhase implements Phase {
 
     @Override
     public void display() {
-
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
     @Override
     public void validateInput(String input) {
-
+        if (input.length() >= 2) {
+            throw new IllegalArgumentException();
+        }
+        char ch = input.charAt(0);
+        if ('1' <= ch && ch <= '2') {
+            throw new IllegalArgumentException();
+        }
     }
 
     @Override
     public PhaseID action(String input) {
-        return null;
+        char ch = input.charAt(0);
+        if (ch == '1') {
+            this.baseballGame.getEvaluator().resetAnswer();
+            return PhaseID.GUESS;
+        } else {
+            return PhaseID.FINISH;
+        }
     }
 
     @Override
