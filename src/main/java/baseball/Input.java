59ae88e6831654b39e1871fd5aea5eba6cbb9769
noeyodeel <sko8044@gmail.com>
2023-10-25 22:27:55 +0900

    feat : 입력 예외처리 기능 추가, 구체화
    답을 맞췄을때 1과 2이외의 잘못된 입력을 받았을때의 예외처리 기능 구현함.
    입력 숫자 예외처리 구체화함.

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -3,7 +3,7 @@ package baseball;
 import java.util.ArrayList;
 import java.util.List;
 import camp.nextstep.edu.missionutils.Console;
-import net.bytebuddy.implementation.bytecode.Throw;
+
 
 public class Input {
   public List<Integer> Input_num() {
@@ -12,24 +12,33 @@ public class Input {
     String input = Console.readLine();
     List<Integer> Input_num = new ArrayList<>();
 
-    for (String number : input.split("")) {
-      Input_num.add(Integer.parseInt(number));
+    try {
+      for (String number : input.split("")) {
+        Input_num.add(Integer.parseInt(number));
+      }
+    } catch (NumberFormatException e) {
+      throw new IllegalArgumentException("문자는 입력할 수 없습니다.");
     }
     exception_Length(input);
-
     return Input_num;
   }
 
   public void exception_Length(String s) {
+    if (s.length() != Application.size) {
+      throw new IllegalArgumentException("숫자가 3개보다 크거나 작습니다.");
+    }
+
     String answer = "";
-    
     for (int i = 0; i < s.length(); i++) {
-        if(s.indexOf(s.charAt(i)) == i) answer += s.charAt(i);
+      if (s.indexOf(s.charAt(i)) == i)
+        answer += s.charAt(i);
     }
-    if (s.contains("0") == true || answer.length() != Application.size ) {
-      throw new IllegalArgumentException("잘못된 값을 입력하였습니다.");
+    if (answer.length() != Application.size) {
+      throw new IllegalArgumentException("중복된 숫자가 포함되어 있습니다.");
     }
-    System.out.println();
+    if (s.contains("0") == true) {
+      throw new IllegalArgumentException("1부터 9까지의 숫자만 입력할 수 있습니다.");
+    }
+
   }
 }
-
--- src/main/java/baseball/Retry.java
+++ src/main/java/baseball/Retry.java
@@ -7,12 +7,19 @@ public class Retry {
   public boolean retry() {
     System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    
+    String answer = Console.readLine();
+    exception_num(answer);
+    char answer_char = answer.charAt(0);
 
-    char answer = Console.readLine().charAt(0);
-
-    if (answer == '1')
+    if (answer_char == '1')
       return true;
 
     return false;
+  }
+  public void exception_num(String s) {
+    if (s.contains("1") != true && s.contains("2") != true) {
+      throw new IllegalArgumentException("1과 2 외에는 입력할 수 없습니다.");
+    }
   }
 }
