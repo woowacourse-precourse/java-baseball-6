1cd932a17b4615fada051034f9579c3bd2f7c9f8
DDing77 <devseo2021@gmail.com>
2023-10-25 10:34:13 +0900

    feat: 출력 메시지 상수 생성

---
+++ src/main/java/baseball/constant/message/GameMessage.java
@@ -0,0 +1,15 @@
+package baseball.constant.message;
+
+public class GameMessage {
+
+    public final static String MESSAGE_GAME_START = "숫자 야구 게임을 시작합니다.";
+    public final static String MESSAGE_INPUT = "숫자를 입력해주세요 : ";
+    public final static String MESSAGE_GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public final static String MESSAGE_GAME_RETRY = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public final static String BALL = "볼 ";
+    public final static String STRIKE = "스트라이크";
+    public final static String NOTHING = "낫싱";
+
+    public static final String EXCEPTION_INPUT = "Not Valid Input";
+}
