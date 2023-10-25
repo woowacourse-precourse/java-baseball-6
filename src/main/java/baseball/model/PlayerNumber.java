0f4f7503b73b99c77e7dc7b30ccc06bc9ad35b1b
Minyongggg <naminyong97@gmail.com>
2023-10-25 22:17:03 +0900

    feat: 플레이어 숫자 유효성 검증 로직
    - validateLength() : 입력 문자열의 길이는 3
    - validateType() : 각 문자는 1부터 9까지의 자연수
    - validateDuplication() : 문자의 중복은 허용되지 않음

--- src/main/java/baseball/model/PlayerNumber.java
+++ src/main/java/baseball/model/PlayerNumber.java
@@ -1,11 +1,41 @@
 package baseball.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class PlayerNumber {
 
     private final String value;
 
     public PlayerNumber(String value) {
+        validateLength(value);
+        validateType(value);
+        validateDuplication(value);
         this.value = value;
+    }
+
+    private void validateLength(String value) {
+        if (value.length() != 3) {
+            throw new IllegalArgumentException("입력의 길이가 3이 아닙니다.");
+        }
+    }
+
+    private void validateType(String value) {
+        value.chars().forEach(c -> {
+            if (!Character.isDigit(c) || c == '0') {
+                throw new IllegalArgumentException("입력이 1부터 9까지의 자연수로 이루어진 3자리 수가 아닙니다.");
+            }
+        });
+    }
+
+    private void validateDuplication(String value) {
+        Set<Integer> set = new HashSet<>();
+        value.chars().forEach(c -> {
+            if (set.contains(c)) {
+                throw new IllegalArgumentException("입력에 중복으로 사용된 수가 있습니다.");
+            }
+            set.add(c);
+        });
     }
 
     public String getValue() {
