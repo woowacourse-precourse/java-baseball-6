7ce380be75cc8044fddc727c000778c572f808f1
m2nseop <pms246@naver.com>
2023-10-25 20:00:47 +0900

    feat: 예외처리 기능

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -11,6 +11,9 @@ public class BaseballGame {
     private boolean gameClearCheck = false; // 게임 진행 상태 true(진행), false(종료)
     private boolean replayCheck = false; // 재게임 의사
 
+    final int INPUT_ANSWER_RANGE_MIN = 1;
+    final int INPUT_ANSWER_RANGE_MAX = 9;
+    final int INPUT_ANSWER_LENGTH = 3;
 
     public void initGame() { //
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -18,7 +21,6 @@ public class BaseballGame {
             setGame(); // 관련 설정 초기화 세팅
             gameStart(); // 게임 루프 시작
         } while (this.replayCheck);
-
         // 위 루프가 끝난다는 것은 게임이 끝났다는 것
     }
 
@@ -31,7 +33,7 @@ public class BaseballGame {
         String result = "";
         do {
             getPlayerGuess(); // 입력받기
-            result = checkStrikeAndBall();
+            result = checkStrikeAndBall(playerAnswer);
             System.out.println(result);
             checkAnswer();
         } while (this.gameClearCheck == false); // 게임 클리어할때까지 반복
@@ -44,34 +46,31 @@ public class BaseballGame {
         }
     }
 
-    public String checkStrikeAndBall() {
+    public String checkStrikeAndBall(String input) {
         int countBall = 0;
         int countStrike = 0;
         String messageScore = "";
         String messageBall = "";
         String messageStrike = "";
-        System.out.println("길이: " + playerAnswer.length());
-        for (int i = 0; i < playerAnswer.length(); i++) {
-            char sourceChar = playerAnswer.charAt(i);
-            System.out.println(playerAnswer.charAt(i));
-            if (sourceChar == computerAnswer.charAt(i)) {
+        for (int i = 0; i < input.length(); i++) {
+            char inputChar = input.charAt(i);
+            if (inputChar == computerAnswer.charAt(i)) {
                 countStrike++;
-            } else if (playerAnswer.contains(String.valueOf(sourceChar))) {
+            } else if (computerAnswer.contains(String.valueOf(inputChar))) {
                 countBall++;
             }
         }
-
         if (countBall > 0) {
             messageBall = String.valueOf(countBall) + "볼 ";
         }
         if (countStrike > 0) {
             messageStrike = String.valueOf(countStrike) + "스트라이크";
         }
-
-        if (countBall == 3) {
-            messageScore = "낫싱";
-        } else if (countBall + countStrike > 0) {
+        if (countBall + countStrike > 0) {
             messageScore = messageBall + messageStrike;
+        }
+        if (countBall == 0 && countStrike == 0) {
+            messageScore = "낫싱";
         }
         return messageScore;
     }
@@ -88,7 +87,6 @@ public class BaseballGame {
             this.replayCheck = false;
         }
     }
-
 
     public void createRandomNumber() { // 정답 랜덤 번호 생성
         List<Integer> computer = new ArrayList<>();
@@ -108,16 +106,45 @@ public class BaseballGame {
     public void getPlayerGuess() {
         System.out.print("숫자를 입력해주세요: ");
         String input = Console.readLine();
+        checkInteger(input);
+        checkLength(input);
+        checkSameNumber(input);
+        checkRange(input);
         this.playerAnswer = input;
-        System.out.println("사용자입력숫자: " + this.playerAnswer);
     }
 
-    public void exceptNotInteger(String source) {
-        for (int i = 0; i < source.length(); i++) {
-            if (!Character.isDigit(source.charAt(i))) {
+    public void checkInteger(String input) { // 사용자가 입력한 값이 정수가 아닌경우 체크
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i))) {
                 throw new IllegalArgumentException("입력값은 반드시 정수여야 합니다.");
             }
         }
     }
 
+    public void checkSameNumber(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = i + 1; j < input.length(); j++) {
+                if (input.charAt(i) == input.charAt(j)) {
+                    throw new IllegalArgumentException("입력값은 서로 다른 숫자로 이루어져야 합니다.");
+                }
+            }
+        }
+    }
+
+    public void checkLength(String input) {
+        if (input.length() != INPUT_ANSWER_LENGTH) {
+            throw new IllegalArgumentException("입력값의 길이는 " + INPUT_ANSWER_LENGTH + "자리 이어야합니다.");
+        }
+    }
+
+    public void checkRange(String input) {
+        int eachDigit;
+        for (int i = 0; i < input.length(); i++) {
+            eachDigit = Integer.parseInt(String.valueOf(input.charAt(i)));
+            if (eachDigit < INPUT_ANSWER_RANGE_MIN || INPUT_ANSWER_RANGE_MAX < eachDigit) {
+                throw new IllegalArgumentException(
+                        "입력값은 " + INPUT_ANSWER_RANGE_MIN + " 이상" + INPUT_ANSWER_RANGE_MAX + " 이하여야 합니다.");
+            }
+        }
+    }
 }
