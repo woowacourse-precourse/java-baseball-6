905e7671e138db1e79c77e45e068bf59e12b23a9
yeahdy <yejine98@naver.com>
2023-10-25 20:47:31 +0900

    feat: 야구게임 실행 로직구현

--- src/main/java/service/BaseBallGameService.java
+++ src/main/java/service/BaseBallGameService.java
@@ -1,5 +1,13 @@
 package service;
 
+import static constants.MessageType.GAME_END;
+import static constants.MessageType.GAME_START;
+import static constants.MessageType.INPUT_NUMBER;
+import static constants.MessageType.NEW_GAME;
+import static constants.MessageType.STRIKE_3;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 import model.Computer;
 import model.Game;
 import model.User;
@@ -22,7 +30,39 @@ public class BaseBallGameService {
     public BaseBallGameService(){}
 
     public void runGame(){
+        //게임 시작
+        baseballGameView.print(baseballGameView.printMessage(GAME_START));
+        String flag = NEW_GAME.getMessage();
+        String computerNumber = "";
+        do{
+            //컴퓨터 랜덤 수 반환
+            if(computerNumber.isBlank()){
+                computerNumber = computer.createRandomNumber();
+                computer.setRandomNumber(computerNumber);
+            }
 
+            //사용자 입력값 받기
+            baseballGameView.print(baseballGameView.printMessage(INPUT_NUMBER));
+            user.setUserNumber();
+            String userNumber = user.getNumber();
+
+            //사용자 입력값 유효성 검사
+            if(game.isUserNumber(userNumber)){
+                //볼, 스트라이크, 낫싱 판단하기
+                List<String> baseballScoreList = game.createBaseballScoreList(computerNumber, userNumber);
+                //결과값 가공하기
+                String baseBallResult = baseballGameView.printBaseBallCalculation(baseballScoreList);
+                baseballGameView.print(baseBallResult);
+                //3스트라이크 판별하기
+                if(!STRIKE_3.getMessage().equals(baseBallResult)){
+                    continue;
+                }
+                //게임 종료
+                baseballGameView.print(baseballGameView.printMessage(GAME_END));
+                flag = Console.readLine();
+                computerNumber = "";
+            }
+        }while (NEW_GAME.getMessage().equals(flag));
     }
 
 }
