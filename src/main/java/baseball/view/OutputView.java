8a8191c09153f0d69ef6c83ed2cbd3b4b69ba42b
hansg931 <hansg0509@cau.ac.kr>
2023-10-25 19:29:04 +0900

    feat: 콘솔 출력 메소드 수정 및 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,10 +1,15 @@
 package baseball.view;
 
+import baseball.model.Score;
 import baseball.util.Message;
 
 public class OutputView {
 	public static void printMessage(String message) {
 		System.out.println(message);
+	}
+
+	public void printScore(Score userScore) {
+		printMessage(makeScoreString(userScore));
 	}
 
 	public static void printGameStartMessage() {
@@ -23,19 +28,19 @@ public class OutputView {
 		printMessage(Message.GAME_RESTART_MESSAGE);
 	}
 
-	public static void printBallMessage() {
-		printMessage(Message.BALL_MESSAGE);
-	}
-
-	public static void printStrikeMessage() {
-		printMessage(Message.STRIKE_MESSAGE);
-	}
-
-	public static void printNothingMessage() {
-		printMessage(Message.NOTHING_MESSAGE);
-	}
-
 	public static void printInputErrorMessage() {
 		printMessage(Message.INPUT_ERROR_MESSAGE);
+	}
+
+	private String makeScoreString(Score userScore) {
+		if (userScore.isNothing()) {
+			return Message.NOTHING_MESSAGE;
+		} else if (userScore.isOnlyStrike()) {
+			return userScore.getStrike()+ Message.STRIKE_MESSAGE;
+		} else if (userScore.isOnlyBall()) {
+			return userScore.getBall()+ Message.BALL_MESSAGE;
+		} else {
+			return userScore.getBall()+ Message.BALL_MESSAGE + " " + userScore.getStrike()+ Message.STRIKE_MESSAGE;
+		}
 	}
 }
