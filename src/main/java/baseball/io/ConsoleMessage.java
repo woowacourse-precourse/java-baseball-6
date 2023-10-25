8f30846113e57220a55bbdbdabf6d235d7ab7ab8
beomsic <qjatjr29@khu.ac.kr>
2023-10-25 12:11:47 +0900

    :recycle: refactor : 결과를 보여주는 메시지 수정

--- src/main/java/baseball/io/ConsoleMessage.java
+++ src/main/java/baseball/io/ConsoleMessage.java
@@ -7,7 +7,7 @@ public enum ConsoleMessage {
     STRIKE("스트라이크 "),
     BALL("볼 "),
     NOTHING("낫싱"),
-    CORRECT_ANSWER("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"),
+    CORRECT_ANSWER("개의 숫자를 모두 맞히셨습니다! 게임 종료\n"),
     RESTART("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
 
     private String message;
