be5e0e97b934153fd03212963c750aa96786934e
BigBird <waujoxc@gmail.com>
2023-10-25 23:18:03 +0900

    3. 게임 종료
    게임 재시작/종료 확인 메서드 추출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -42,16 +42,23 @@ public class Application {
                     System.out.println("낫싱");
                 }
             }
-            
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            int playAgain = Integer.parseInt(Console.readLine());
-            if (playAgain == 1) {
-                isPlayingGame = true;
-            } else if (playAgain == 2) {
-                isPlayingGame = false;
-            }
+
+            isPlayingGame = askToPlayAgain(isPlayingGame);
 
         }
+    }
+
+    private static boolean askToPlayAgain(boolean isPlayingGame) {
+
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        int playAgain = Integer.parseInt(Console.readLine());
+
+        if (playAgain == 1) {
+            isPlayingGame = true;
+        } else if (playAgain == 2) {
+            isPlayingGame = false;
+        }
+        return isPlayingGame;
     }
 
     private static List<Integer> createRandomAnswer() {
