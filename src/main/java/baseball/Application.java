3387ef625230128872be25eb6c5d10d74c873dcd
Lee Yoonbin <younbihn@naver.com>
2023-10-25 23:22:12 +0900

    final

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -22,7 +22,7 @@ public class Application {
                 attempts++;
 
                 if (result[0] == 3) {
-                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    System.out.println("3스트라이크! 게임 종료");
                     break;
                 }
             }
