d8257b01f402ae3443ed0769508f9168d7badfc8
chanwoo <mdsoo55828@gmail.com>
2023-10-25 19:52:41 +0900

    Application 게임시작 로직 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
 
+
+import baseball.controller.GameController;
+import baseball.view.OutputView;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static void main(String[] args)  {
+
+        GameController gameController = new GameController();
+        gameController.getStartMessage();
+        gameController.start();
+
     }
+
 }
