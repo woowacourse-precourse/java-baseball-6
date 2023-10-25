41ae49661ea21469dff87f13f43743c7c8922605
BaeDongHyeon <sasd730@gmail.com>
2023-10-25 23:56:38 +0900

    TEST: 코드 변경에 따른 테스트 코드 수정

--- src/test/java/baseball/domain/BaseballResultTest.java
+++ src/test/java/baseball/domain/BaseballResultTest.java
@@ -14,7 +14,10 @@ class BaseballResultTest {
     @Test
     @DisplayName("BALL은 2개 STRIKE는 1개 NOTHING은 0개가 저장되어야 한다.")
     void gameResult() {
-        assertThat(BaseballResult.generate(List.of(BALL, STRIKE, BALL))).isEqualTo(Map.of(
+        BaseballResult baseballResult = new BaseballResult();
+        baseballResult.translate(List.of(BALL, STRIKE, BALL));
+
+        assertThat(baseballResult.getTransResult()).isEqualTo(Map.of(
                 BALL, 2,
                 STRIKE, 1,
                 NOTHING, 0
