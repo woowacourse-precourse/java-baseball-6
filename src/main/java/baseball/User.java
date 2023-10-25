f1dc4829f4502e332080197ae0cb8e0e1f98646a
kdomo <dongho5088@naver.com>
2023-10-25 17:50:58 +0900

    feat: INPUT_LENGTH 선언과 userBaseBallNumber 멤버 필드 생성

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,19 +1,10 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Arrays;
 
 public class User {
     private static final int INPUT_LENGTH = 3;
     private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
 
     int[] userInput = new int[INPUT_LENGTH];
-
-    public void getUserInput() {
-        System.out.print(INPUT_MESSAGE);
-        String input = Console.readLine();
-        for (int i = 0; i < INPUT_LENGTH; i++) {
-            this.userInput[i] = Integer.parseInt(String.valueOf(input.charAt(i)));
-        }
-    }
 }
