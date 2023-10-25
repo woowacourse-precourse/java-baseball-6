3f7d17202750896243155ef5488ca8b25af18219
ohhyeon2 <dhgus1235@gmail.com>
2023-10-25 09:22:42 +0900

    [추가] 랜덤 숫자 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
+import java.util.*;
+import camp.nextstep.edu.missionutils.Randoms;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+    }
+
+    private static List<Integer> getRandomNumber() {
+        List<Integer> randomNumbers = new ArrayList<>();
+        while (randomNumbers.size() < 3) {
+            int random = Randoms.pickNumberInRange(1, 9);
+            if(!randomNumbers.contains(random)) {
+                randomNumbers.add(random);
+            }
+        }
+        return randomNumbers;
     }
 }
