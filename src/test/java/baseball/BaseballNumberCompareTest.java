aa96cfa629bcd7ae179e04148d64304c1f4844b5
JooDukHan <bfinecpa1@naver.com>
2023-10-25 20:10:57 +0900

    테스트 인스턴스 변경
    mvc 패턴을 적용함에 따라 테스트에 필요한 인스턴스가 변경

--- src/test/java/baseball/BaseballNumberCompareTest.java
+++ src/test/java/baseball/BaseballNumberCompareTest.java
@@ -1,10 +1,14 @@
 package baseball;
 
+import static org.mockito.Mockito.when;
+
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
-public class BaseballGameProcessorTest {
+class BaseballNumberCompareTest {
 
     private static final String COMPUTER_NUMBER = "123";
     private static final String TWO_STRIKE_NUMBER = "124";
@@ -15,48 +19,51 @@ public class BaseballGameProcessorTest {
     private static final String TWO_BALL_ONE_STRIKE_NUMBER = "321";
     private static final String ONE_BALL_ONE_STRIKE_NUMBER = "134";
     private static final String NOTHING_NUMBER = "456";
+    
+    private static final RandomNumberGenerator randomNumberGenerator = Mockito.mock(RandomNumberGenerator.class);
 
-
-    BaseballGameProcessor processor = new BaseballGameProcessorImpl();
-
-
+    @BeforeAll
+    static void setUp() {
+        when(randomNumberGenerator.generateNonRepeatingRandomDigitSequence(BaseballConstants.BASEBALL_NUMBER_SIZE)).thenReturn(COMPUTER_NUMBER);
+    }
+    BaseballNumber baseballNumber = BaseballNumber.getInstance(randomNumberGenerator);
 
     @Test
     @DisplayName("3스트라이크 테스트")
     void threeStrikeTest() {
-        Assertions.assertThat(processor.process(COMPUTER_NUMBER, COMPUTER_NUMBER)).isEqualTo("3스트라이크");
+        Assertions.assertThat(baseballNumber.compare(COMPUTER_NUMBER)).isEqualTo("3스트라이크");
     }
 
     @Test
     @DisplayName("2스트라이크 테스트")
     void twoStrikeTest() {
-        Assertions.assertThat(processor.process(COMPUTER_NUMBER, TWO_STRIKE_NUMBER)).isEqualTo("2스트라이크");
+        Assertions.assertThat(baseballNumber.compare(TWO_STRIKE_NUMBER)).isEqualTo("2스트라이크");
     }
 
     @Test
     @DisplayName("1스트라이크 테스트")
     void oneStrikeTest() {
-        Assertions.assertThat(processor.process(COMPUTER_NUMBER, ONE_STRIKE_NUMBER)).isEqualTo("1스트라이크");
+        Assertions.assertThat(baseballNumber.compare(ONE_STRIKE_NUMBER)).isEqualTo("1스트라이크");
     }
 
     @Test
     @DisplayName("3볼 테스트")
     void threeBallTest() {
-        Assertions.assertThat(processor.process(COMPUTER_NUMBER, THREE_BALL_NUMBER)).isEqualTo("3볼");
+        Assertions.assertThat(baseballNumber.compare(THREE_BALL_NUMBER)).isEqualTo("3볼");
 
     }
 
     @Test
     @DisplayName("2볼 테스트")
     void twoBallTest() {
-        Assertions.assertThat(processor.process(COMPUTER_NUMBER, TWO_BALL_NUMBER)).isEqualTo("2볼");
+        Assertions.assertThat(baseballNumber.compare(TWO_BALL_NUMBER)).isEqualTo("2볼");
 
     }
 
     @Test
     @DisplayName("1볼 테스트")
     void oneBallTest() {
-        Assertions.assertThat(processor.process(COMPUTER_NUMBER, ONE_BALL_NUMBER)).isEqualTo("1볼");
+        Assertions.assertThat(baseballNumber.compare(ONE_BALL_NUMBER)).isEqualTo("1볼");
 
     }
 
@@ -64,21 +71,21 @@ public class BaseballGameProcessorTest {
     @Test
     @DisplayName("2볼 1스트라이크 테스트")
     void twoBallOneStrikeTest() {
-        Assertions.assertThat(processor.process(COMPUTER_NUMBER, TWO_BALL_ONE_STRIKE_NUMBER)).isEqualTo("2볼 1스트라이크");
+        Assertions.assertThat(baseballNumber.compare(TWO_BALL_ONE_STRIKE_NUMBER)).isEqualTo("2볼 1스트라이크");
 
     }
 
     @Test
     @DisplayName("1볼 1스트라이크 테스트")
     void oneBallOneStrikeTest() {
-        Assertions.assertThat(processor.process(COMPUTER_NUMBER, ONE_BALL_ONE_STRIKE_NUMBER)).isEqualTo("1볼 1스트라이크");
+        Assertions.assertThat(baseballNumber.compare(ONE_BALL_ONE_STRIKE_NUMBER)).isEqualTo("1볼 1스트라이크");
 
     }
 
     @Test
     @DisplayName("낫싱 테스트")
     void nothingTest() {
-        Assertions.assertThat(processor.process(COMPUTER_NUMBER, NOTHING_NUMBER)).isEqualTo("낫싱");
+        Assertions.assertThat(baseballNumber.compare(NOTHING_NUMBER)).isEqualTo("낫싱");
 
     }
 
