7fab593cd25bfaf34a2c41ed850fc8c75fc818ec
BaeDongHyeon <sasd730@gmail.com>
2023-10-25 21:17:29 +0900

    FEAT: 랜덤 숫자 생성 및 저장

---
+++ src/main/java/baseball/domain/ComputerNumbers.java
@@ -0,0 +1,12 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class ComputerNumbers {
+
+    private final List<Integer> numbers;
+
+    public ComputerNumbers(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+}
---
+++ src/main/java/baseball/domain/RandomNumbers.java
@@ -0,0 +1,23 @@
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomNumbers {
+
+    private final List<Integer> numbers = new ArrayList<>();
+
+    private List<Integer> generate() {
+        while (numbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+
+        return numbers;
+    }
+
+}
