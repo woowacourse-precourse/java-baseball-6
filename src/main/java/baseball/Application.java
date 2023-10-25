faac894ff390a745098f8e5a0343579fb382fb9d
Hwang In Tae <sjobs66@gmail.com>
2023-10-25 15:34:09 +0900

    Constructor가 아닌 init 메서드에서 Application 초기화

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,10 +9,20 @@ import baseball.view.NumberBaseballGameView;
 import java.util.function.Supplier;
 
 public class Application {
-    private final GameController gameController;
 
-    public Application(Class<? extends Game> gameClass, Class<? extends GameView> viewClass,
-                       Class<? extends GameController> controllerClass, String messagePath) {
+    public static void main(String[] args) {
+        Application.run(NumberBaseballGame.class, NumberBaseballGameView.class,
+                NumberBaseballGameController.class, "src/main/resources/messages/messages.properties");
+    }
+
+    public static void run(Class<? extends Game> gameClass, Class<? extends GameView> viewClass,
+                           Class<? extends GameController> controllerClass, String messagePath) {
+        (new Application()).init(gameClass, viewClass, controllerClass, messagePath);
+    }
+
+    public void init(Class<? extends Game> gameClass, Class<? extends GameView> viewClass,
+                     Class<? extends GameController> controllerClass, String messagePath) {
+        GameController gameController;
         Supplier<Game> gameSupplier = () -> {
             try {
                 return gameClass.getDeclaredConstructor().newInstance();
@@ -28,19 +38,7 @@ public class Application {
         } catch (Exception e) {
             throw new RuntimeException("인스턴스 생성 오류", e);
         }
-    }
 
-    public static void main(String[] args) {
-        Application.run(NumberBaseballGame.class, NumberBaseballGameView.class,
-                NumberBaseballGameController.class, "src/main/resources/messages/messages.properties");
-    }
-
-    public static void run(Class<? extends Game> gameClass, Class<? extends GameView> viewClass,
-                           Class<? extends GameController> controllerClass, String messagePath) {
-        (new Application(gameClass, viewClass, controllerClass, messagePath)).run();
-    }
-
-    public void run() {
         gameController.play();
     }
 }
