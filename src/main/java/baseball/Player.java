05e7017ce0aeb0d4a4309b4797db069dd8cb39dd
Suyoung Oh <ohsusu515@gmail.com>
2023-10-25 15:18:11 +0900

    feat: 입력값 예외 처리 구현

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,8 +1,10 @@
 package baseball;
 
+import baseball.constant.ExceptionMessage;
 import baseball.constant.GameRule;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,10 +12,15 @@ public class Player {
     private List<Integer> playerNumber;
 
     public void generatePlayerNumber() {
-        String str = Console.readLine();
-        this.playerNumber = Arrays.stream(str.split(""))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+        try {
+            String str = Console.readLine();
+            this.playerNumber = Arrays.stream(str.split(""))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ExceptionMessage.NOT_NUMBER_ERROR, e);
+        }
+        checkValidPlayerNumber();
     }
 
     public List<Integer> getPlayerNumber() {
@@ -30,5 +37,24 @@ public class Player {
             return false;
         }
         return false;
+    }
+
+    public void checkValidPlayerNumber() {
+        for (Integer number : playerNumber) {
+            if (number < 1 || number > 9) {
+                throw new IllegalArgumentException(ExceptionMessage.NOT_NUMBER_ERROR);
+            }
+        }
+
+        if (playerNumber.size() != GameRule.DIGITS_NUMBER) {
+            throw new IllegalArgumentException(ExceptionMessage.LENGTH_ERROR);
+        }
+
+        HashSet<Integer> numberSet = new HashSet<>();
+        for (Integer number : playerNumber) {
+            if (!numberSet.add(number)) {
+                throw new IllegalArgumentException(ExceptionMessage.DUPLICATE_ERROR);
+            }
+        }
     }
 }
---
+++ src/main/java/baseball/constant/ExceptionMessage.java
@@ -0,0 +1,7 @@
+package baseball.constant;
+
+public class ExceptionMessage {
+    public static final String NOT_NUMBER_ERROR = "입력값은 1 ~ 9의 숫자여야 합니다.";
+    public static final String LENGTH_ERROR = "숫자의 길이는 3이어야 합니다.";
+    public static final String DUPLICATE_ERROR = "중복된 숫자가 존재합니다.";
+}
