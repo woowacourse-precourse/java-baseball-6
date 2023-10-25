9523cd5e631df9f0dee0cb95dee1ec1e80d24b45
KongJunWoo <joonoo3@naver.com>
2023-10-25 16:32:47 +0900

    feat : Computer 객체 생성 메소드
    - 생성자를 추가하고 List 초기화
    - 객체 생성을 위임 받을 수 있게 create메소드 작성

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -1,9 +1,22 @@
 package baseball.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
 
     private List<Integer> computerNum; //컴퓨터가 생성한 수를 저장할 배열
+
+    //생성자
+    private Computer(){
+        computerNum = new ArrayList<>();
+    }
+
+    //Computer 객체 생성해서 반환
+    public static Computer create(){
+        return new Computer();
+    }
 
 }
