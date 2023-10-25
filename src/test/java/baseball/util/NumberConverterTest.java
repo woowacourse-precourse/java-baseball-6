5f410d7646deac61671896d31fc66d806bb7b217
siyeonSon <siyeon0209@naver.com>
2023-10-25 13:59:41 +0900

    test: 사용자가 입력한 숫자의 수가 3개 이하일 경우 예외를 발생시킨다

--- src/test/java/baseball/util/NumberConverterTest.java
+++ src/test/java/baseball/util/NumberConverterTest.java
@@ -20,4 +20,11 @@ class NumberConverterTest {
                 NumberConverter.from("일23"));
         assertEquals(e.getMessage(), "문자가 아닌 숫자를 입력해주세요");
     }
+
+    @Test
+    void 사용자가_입력한_숫자의_수가_3개_이하일_경우_예외를_발생시킨다() {
+        Exception e = assertThrows(IllegalArgumentException.class, () ->
+                NumberConverter.from("1"));
+        assertEquals(e.getMessage(), "1부터 9까지 3개의 숫자를 입력해주세요");
+    }
 }
