feb96e6cac393a05b7907551d932f3bb9a195154
woookk <woook27@naver.com>
2023-10-25 21:39:55 +0900

    refactor: BaseballGame 클래스의 볼, 스트라이크 계산 로직 분리

--- src/main/java/baseball/service/BaseballGame.java
+++ src/main/java/baseball/service/BaseballGame.java
@@ -34,26 +34,42 @@ public class BaseballGame {
     private boolean isCorrect() {
         String number = user.getNumber();
         String answer = computer.getAnswer();
-        int strike = 0;
-        int ball = 0;
-        HashSet<Character> set = new HashSet<>();
-        for (int i = 0; i < number.length(); i++) {
-            if (number.charAt(i) == answer.charAt(i)) {
-                strike++;
-            }
-            set.add(answer.charAt(i));
-        }
-        for (int i = 0; i < number.length(); i++) {
-            if (number.charAt(i) != answer.charAt(i) && set.contains(number.charAt(i))) {
-                ball++;
-            }
-        }
+        int strike = countStrike(number, answer);
+        int ball = countBall(number, answer);
         Output.print(ball, strike);
 
         if (strike == 3) {
             return true;
         }
         return false;
+    }
+
+    public static int countBall(String number, String answer) {
+        HashSet<Character> set = new HashSet<>();
+        for (char c : answer.toCharArray()) {
+            set.add(c);
+        }
+        int ballCount = 0;
+        for (int index = 0; index < 3; index++) {
+            if (number.charAt(index) != answer.charAt(index) && set.contains(number.charAt(index))) {
+                ballCount++;
+            }
+        }
+        return ballCount;
+    }
+
+    public static int countStrike(String number, String answer) {
+        if (number.equals(answer)) {
+            return 3;
+        }
+
+        int strikeCount = 0;
+        for (int index = 0; index < 3; index++) {
+            if (number.charAt(index) == answer.charAt(index)) {
+                strikeCount++;
+            }
+        }
+        return strikeCount;
     }
 }
 
