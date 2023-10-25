b7b01426be54e41b7fdcc0e28aeacad16993b2b8
동원 <ai06021@gmail.com>
2023-10-25 18:29:12 +0900

    출력값이 ì fix : 장이 안되는 것 같아 수정

--- src/main/java/baseball/Play.java
+++ src/main/java/baseball/Play.java
@@ -3,48 +3,43 @@ package baseball;
 import java.util.List;
 
 public class Play {
-    int strike, ball;
+
 
     public void game() {
 
         System.out.println("숫자 야구 게임을 시작합니다");
         Computer computer = new Computer();
         List<Integer> another = computer.anotherNumber();
-        while (strike != 3) {
-            strike = 0;
-            ball = 0;
+        int value = 0;
+        while (value != 3) {
+
             Input input = new Input();
             List<Integer> player = input.numbers();
-            if (player.size() == 0)
+            if (player.size() == 0) {
                 return;
-            printNumber(player);
-            compare(player, another);
+            }
 
+            value = compare(player, another);
         }
-        Input input = new Input();
-        int newgame = input.newGameCheck();
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
+        int newgame = new Input().newGameCheck();
         if (newgame == 1) {
             System.out.println(1);
             game();
         }
     }
 
-    private void printNumber(List<Integer> input) {
-        for (Integer num : input) {
-            System.out.print(num);
-        }
-        System.out.println();
-    }
+    public int compare(List<Integer> player, List<Integer> answer) {
+        int strike = 0;
+        int ball = 0;
 
-    private void compare(List<Integer> player, List<Integer> answer) {
         for (int i = 0; i < player.size(); i++) {
             int now = player.get(i);
-            if (answer.contains(now)) {
-
-                if (answer.get(i) == now)
-                    strike++;
-                else
-                    ball++;
+            if (answer.get(i) == now) {
+                strike++;
+            } else if (answer.contains(now)) {
+                ball++;
             }
         }
         if (strike == 0 && ball == 0) {
@@ -56,5 +51,6 @@ public class Play {
         } else {
             System.out.println(ball + "볼 " + strike + "스트라이크");
         }
+        return strike;
     }
 }
