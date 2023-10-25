89f6de2dce66a046952ac5e0615e4171ba0ddd92
Hyoseop Song <crad_on25@naver.com>
2023-10-25 21:25:58 +0900

    feature(GameStatusValue): 게임 상태를 조작하는 사용자 입력값 정의
    - 해당 입력값이 정수로 요구되므로 별도의 configuration class를 정의하여 로직 내에서 비교할 수 있도록 구현

---
+++ src/main/java/baseball/SYSTEM_VALUE/GameStatusValue.java
@@ -0,0 +1,7 @@
+package baseball.SYSTEM_VALUE;
+
+public class GameStatusValue {
+    public static final int GAME_RESTART = 1;
+    public static final int GAME_EXIT = 2;
+    public static final int GAME_CONTINUE = 3;
+}
