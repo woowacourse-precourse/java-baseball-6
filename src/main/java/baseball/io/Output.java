0e566414699815a978537d23bf072b88e88388b2
Hercules <hyukkyulee@alumni.emory.edu>
2023-10-25 11:48:20 +0900

    reconstruct: preparing Output

---
+++ src/main/java/baseball/io/Output.java
@@ -0,0 +1,8 @@
+package baseball.io;
+import java.util.List;
+
+// before reconstruction
+public class Output {
+    public static void printAnswer(List<Integer> answer){System.out.printf("Answer: %s\n", answer);}
+
+}
