d9026054d01f3c6e510ec64ac8b03546a348cbe7
zeomzzz <jmlee2705@gmail.com>
2023-10-25 21:08:34 +0900

    feat: Game에 게임 재시작 여부 입력 요청하는 askForRestart 생성

--- src/main/java/baseball/models/Game.java
+++ src/main/java/baseball/models/Game.java
@@ -6,11 +6,14 @@ import baseball.utils.Utils;
 
 public class Game {
 
-    private boolean status; // 게임 진행 여부
+    public static final int NUM_LENGTH = 3;
+    public boolean status; // 게임 진행 여부
 
     public boolean isStatus() {
         return status;
     }
+
+    Utils utils = new Utils();
 
     // 게임 시작하는 메소드
     public void init() {
@@ -27,17 +30,24 @@ public class Game {
     }
 
     // 사용자에게 숫자 입력을 요청하는 메소드
-    public String askForGuess(int length) {
+    public String askForGuess() {
         String result = "";
         try {
             System.out.print("숫자를 입력해주세요 : ");
             result = Console.readLine();
-            Utils.validateInputValue(result, length);
+            Utils.validateInputValue(result, NUM_LENGTH);
         } catch (IllegalArgumentException e) {
             System.out.println("입력이 잘못되었습니다.");
             exit(2);
         }
         return result;
+    }
+
+    // 정답을 맞힌 경우 게임 재시작 여부 입력 요청하는 메소드
+    public int askForRestart() {
+        int playerInput = Integer.parseInt(Console.readLine());
+        utils.validateRestartInput(playerInput);
+        return playerInput;
     }
 
 }
