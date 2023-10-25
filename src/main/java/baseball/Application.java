78b41d527c80e23a140645f1eef8df27571c8b23
plan11plan <tjtfy7028@gmail.com>
2023-10-25 21:39:08 +0900

    style: 인라인화

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,12 +9,10 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-        RandomNumber randomNumbers = new RandomNumber();
-        List<Integer> cpuInput = randomNumbers.getRandomNumbers();
+        List<Integer> cpuInput = new RandomNumber().getRandomNumbers();
         System.out.println("컴퓨터 숫자:" + cpuInput);
         System.out.print("숫자를 입력해주세요 : ");
-        UserNumber userNumber = new UserNumber(readLine());
-        String userInput = userNumber.getUserNumber();
+        String userInput = new UserNumber(readLine()).getUserNumber();
 
         // BaseballGame 인스턴스 생성
         BaseballGame game = new BaseballGame();
