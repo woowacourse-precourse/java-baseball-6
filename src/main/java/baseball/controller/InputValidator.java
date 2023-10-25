bf8163f231f82dc41f136991fac288277f94a338
Choo <choo000407@naver.com>
2023-10-25 18:50:04 +0900

    style(InputValidator): 빈 라인 제거

--- src/main/java/baseball/controller/InputValidator.java
+++ src/main/java/baseball/controller/InputValidator.java
@@ -3,10 +3,8 @@ package baseball.controller;
 import baseball.enums.ErrorCode;
 
 public class InputValidator {
-
     private static final int VALID_NUMBER_LENGTH = 3;
     private static final String VALID_NUMBER_PATTERN = "[1-9]{3}";
-
 
     public static void validatePlayerNumber(String playerNumber) {
         if (playerNumber.length() != VALID_NUMBER_LENGTH) {
