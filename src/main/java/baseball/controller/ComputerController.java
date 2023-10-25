15097e19d9bc46220838e61a84daeb2161c4047c
ddohyeong <dohyeong7026@gmail.com>
2023-10-25 20:56:47 +0900

    feat(ComputerController) : 컴퓨터 서로 다른 3자리의 난수 생성
    docs : 게임 서로 다른 3자리의 난수 생성 완료 체크

---
+++ src/main/java/baseball/controller/ComputerController.java
@@ -0,0 +1,21 @@
+package baseball.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class ComputerController {
+	public List<Integer> getComputerNumber() {
+		List<Integer> computer = new ArrayList<>();
+
+		while (computer.size() < 3) {
+			int randomNumber = Randoms.pickNumberInRange(1, 9);
+			if (!computer.contains(randomNumber)) {
+				computer.add(randomNumber);
+			}
+		}
+		return computer;
+	}
+
+}
