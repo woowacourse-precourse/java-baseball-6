be6598fb16c04fb23f7df9fd3c56a049ca12900f
HoyiTT <pjs9177@naver.com>
2023-10-25 11:46:49 +0900

    컴퓨터 숫자 생성, 게임 판별 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,62 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Compare compare = new Compare();
+        Game game = new Game();
+        Generate generate = new Generate();
+
+        System.out.println("숫자야구 게임을 시작합니다.");
+
+        List<Integer> computer = new ArrayList<>();
+        List<Integer> user = new ArrayList<>();
+
+        computer = generate.comNum(computer);
+        for(;;){
+            user.clear();
+            System.out.print("숫자를 입력해주세요 : ");
+            String input  = Console.readLine();
+
+            for (int i = 0; i < input.length(); i++) {
+                char digitChar = input.charAt(i); // 문자열3에서 한 문자를 가져옴
+                int digit = Character.getNumericValue(digitChar); // 문자를 int로 변환
+                user.add(digit);
+            }
+
+            System.out.print("user: ");
+            for(int i =0; i < 3; i++) {
+                System.out.print(user.get(i));
+            }
+
+            System.out.println();
+            String strike = game.playGame(computer,user);
+
+            System.out.println(strike);
+            if (strike.equals("3스트라이크")){
+                System.out.println("종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                String reGame  = Console.readLine();
+                if(reGame.equals("1")){
+                    System.out.println("숫자 야구 게임을 시작합니다.");
+                    computer = generate.comNum(computer);
+                } else if (reGame.equals("2")) {
+                    break;
+                }
+
+
+            }
+
+        }
+
+
     }
 }
---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,23 @@
+package baseball;
+
+import java.util.List;
+
+public class Game {
+    public String playGame(List<Integer> computer, List<Integer> player){
+        Compare compare = new Compare();
+
+        int same = compare.sameNumber(computer, player);
+        int strike = compare.checkStrike(computer, player);
+        int ball = same - strike;
+
+        if(same == 0){
+            return "낫실";
+        }else if(strike == 0){
+            return ball + "볼";
+        }else if(ball == 0){
+            return strike + "스트라이크";
+        }
+        return ball + "볼" + strike + "스트라이크";
+
+    }
+}
