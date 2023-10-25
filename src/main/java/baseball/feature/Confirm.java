ef6cda7e84a77f975209774034db63ac6c12f768
doka <posmane5@gmail.com>
2023-10-25 12:59:07 +0900

    feat(Confirm) : Confirm 클래스의 accordNumber 메소드 추가 일치여부 확인 메소드인 Confirm클래스의 accordNumber메소드 추가

--- src/main/java/baseball/feature/Confirm.java
+++ src/main/java/baseball/feature/Confirm.java
@@ -1,6 +1,33 @@
 package baseball.feature;
 
+import java.util.*;
+
 public class Confirm {
-    public int maker() {
+
+    int accordCount;
+    List<Integer> userNumbers;
+    List<Integer> computerNumbers;
+
+    Confirm(List<Integer> UserNumbers, List<Integer> ComputerNumbers) {
+        userNumbers = UserNumbers;
+        computerNumbers = ComputerNumbers;
+    }
+
+    // 스트라이크 ⊂ 볼
+    // 따라서 일치갯수(볼) 먼저 구한다. accordNumber
+    // 그리고 일치갯수중 자리까지 일치하는 경우는 스트라이크
+    // 일치갯수중 스트라이크를 제외하면 볼의 수
+    public int accordNumber() {
+        int count = 0;
+        for (int num : userNumbers) {
+            if (computerNumbers.contains(num)) {
+                count++;
+            }
+        }
+        accordCount = count;
+        return count;
+    }
+
+
 
 }
