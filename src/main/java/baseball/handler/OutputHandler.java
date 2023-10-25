21ae54d15dff4597c859469c8c311619cc8b3bf0
99winnmin <ojysep9987@gmail.com>
2023-10-25 17:06:35 +0900

    :sparkles: Feat : 게임 시작 출력 메소드 추가

---
+++ src/main/java/baseball/handler/OutputHandler.java
@@ -0,0 +1,12 @@
+package baseball.handler;
+
+import baseball.utils.Constants;
+
+public class OutputHandler {
+
+  public static void printGameStartMessage() {
+    System.out.println(Constants.GAME_START_MESSAGE);
+  }
+
+  
+}
