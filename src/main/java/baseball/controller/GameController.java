b4b9ccec8e90df290465fa6ac29ff2117f816777
jongmin Lee <rjsqktod1@gmail.com>
2023-10-25 21:46:05 +0900

    add feature 4: restart or exit option
    Signed-off-by: jongmin Lee <rjsqktod1@gmail.com>

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -3,19 +3,27 @@ package baseball.controller;
 import baseball.service.BaseballService;
 
 public class GameController {
+    private static final int NEW_GAME = 1;
+    private boolean isCorrectGuess = false;
+
     public BaseballController baseballController = new BaseballController();
     public BaseballService baseballService = new BaseballService();
 
-    private boolean isCorrectGuess = false;
     public void Init() {
-        baseballService.startNewGame();
+        int gameStatus = NEW_GAME;
         System.out.println("숫자 야구 게임을 시작합니다.");
-        while (!isCorrectGuess) {
-            baseballController.readUserNumber();
-            int[] gameResult = baseballService.getGameResult();
-            System.out.println(baseballService.convertResultToString(gameResult));
-            isCorrectGuess = baseballService.isCorrectGuess(gameResult[0]);
+        while (gameStatus == NEW_GAME) {
+            baseballService.startNewGame();
+            while (!isCorrectGuess) {
+                baseballController.readUserNumber();
+                int[] gameResult = baseballService.getGameResult();
+                System.out.println(baseballService.convertResultToString(gameResult));
+                isCorrectGuess = baseballService.isCorrectGuess(gameResult[0]);
+            }
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            gameStatus = baseballController.askForNewGameOrExit();
+            isCorrectGuess = false;
         }
     }
-
 }
--- src/main/java/baseball/service/utils/InputValidatorUtils.java
+++ src/main/java/baseball/service/utils/InputValidatorUtils.java
@@ -5,12 +5,26 @@ import java.util.Set;
 
 public class InputValidatorUtils {
 
-    public boolean isValidInput(String input) {
-        if (input.length() == 3 && isNum(input) && !isDuplicated(input)) {
-            return true;
+    private static final int BASEBALL_GAME = 3;
+    private static final int NEW_GAME_START = 1;
+
+    public boolean isValidInput(String input, int inputType) {
+        if (inputType == BASEBALL_GAME) {
+            if (input.length() == BASEBALL_GAME && isNum(input) && !isDuplicated(input)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else if (inputType == NEW_GAME_START) {
+            if (input.length() == NEW_GAME_START && isRestartOrExit(input) && !isDuplicated(input)) {
+                return true;
+            } else {
+                return false;
+            }
         } else {
             return false;
         }
+
     }
 
     public boolean isNum(String input) {
@@ -33,4 +47,14 @@ public class InputValidatorUtils {
         }
         return false;
     }
+
+    public boolean isRestartOrExit(String input) {
+        for (char c : input.toCharArray()) {
+            if (Character.isDigit(c) && (c == '1' || c == '2')) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
