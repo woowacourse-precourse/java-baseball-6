ddfb8eb396c5f00dc4acdca72aea93a8d89ad9e9
Yujin <tkfkd7038@naver.com>
2023-10-25 20:28:50 +0900

    클래스와 메서드 사용하여 개선함

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,93 +2,117 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.*;
-public class Application {
-    public static void main(String[] args)throws IllegalArgumentException {
-        // TODO: 프로그램 구현
 
-        while(true){
-        List<Integer> computer = new ArrayList<>();
+class BaseballGame{
+    int strike = 0;
+    int ball = 0;
 
-        while (computer.size() < 3) {
+    void printStart(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+    List<Integer> getRandomNum(){
+        List<Integer> randomNum = new ArrayList<>();
+        while (randomNum.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
+            if (!randomNum.contains(randomNumber)) {
+                randomNum.add(randomNumber);
             }
         }
-        //System.out.println(Arrays.deepToString(computer.toArray()));
-        System.out.println("숫자 야구 게임을 시작합니다.");
+       // System.out.println(Arrays.deepToString(randomNum.toArray()));
+        return randomNum;
+    }
+    List<Integer> getUserInput() throws IllegalArgumentException{
+        List<Integer> userNum = new ArrayList<>();
+        System.out.println("숫자를 입력해주세요 : ");
+        String userInput = Console.readLine();
 
-        while(true) {
-            int strike = 0;
-            int ball = 0;
-            System.out.println("숫자를 입력해주세요 : ");
-            String userInput = Console.readLine();
+        // 예외 처리 0포함할 경우 OR 길이가 3보다 클 경우 OR 같은 숫자를 입력한 경우
+        if(userInput.contains("0") || userInput.length() != 3){
+            throw new IllegalArgumentException();
+        }
+        int guessNum = Integer.parseInt(userInput);
 
-            // 예외 처리 0포함할 경우 OR 길이가 3보다 클 경우 OR 같은 숫자를 입력한 경우
-            if(userInput.contains("0") || userInput.length() != 3){
-                throw new IllegalArgumentException();
-            }
-            int guessNum = Integer.parseInt(userInput);
+        userNum.add(guessNum / 100);
+        userNum.add((guessNum % 100) / 10);
+        userNum.add(guessNum % 10);
 
-            List<Integer> guessArr = new ArrayList<>();
+        return userNum;
+    }
+    void compareNum(List<Integer> randomNum, List<Integer> userNum){
+        strike = 0;
+        ball = 0;
 
-            guessArr.add(guessNum / 100);
-            guessArr.add((guessNum % 100) / 10);
-            guessArr.add(guessNum % 10);
-
-            for (int i = 0; i < computer.size(); i++) {
-                for (int j = 0; j < guessArr.size(); j++) {
-                    //if(guessArr.get(i).equals(guessArr.get(j)))
-                      //  throw new IllegalArgumentException();
-                    if (computer.get(i).equals(guessArr.get(j))) {
-                        if (i == j) {
-                            strike++;
-                        } else {
-                            ball++;
-                        }
-                    }
+        for (int i = 0; i < randomNum.size(); i++) {
+            for (int j = 0; j < userNum.size(); j++) {
+                if (randomNum.get(i).equals(userNum.get(j)) && i == j) {
+                    strike++;
+                }
+                if (randomNum.get(i).equals(userNum.get(j)) && i != j) {
+                    ball++;
                 }
             }
-            if (strike == 0 && ball == 0) {
-                System.out.println("낫싱");
-                // return;
-            }
+        }
 
+    }
+    int printHint(){
+        if (strike == 0 && ball == 0)
+            System.out.println("낫싱");
 
-            if(strike == 0 && ball != 0){
-                System.out.printf("%d볼\n", ball);
-            }
-            if(strike != 0 && ball == 0){
-                System.out.printf("%d스트라이크\n", strike);
-            }
-            if(strike !=0 && ball != 0) {
-                System.out.printf("%d볼 %d스트라이크\n", strike, ball);
-            }
-            if (strike == 3) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                break;
-            }
-          }
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            String playInput = Console.readLine();
-            int play = Integer.parseInt(playInput);
-            if(!(play == 1 || play == 2))
-                throw new IllegalArgumentException();
+        if(strike == 0 && ball != 0)
+            System.out.printf("%d볼\n", ball);
 
-            if(play == 2)
+        if(strike != 0 && ball == 0)
+            System.out.printf("%d스트라이크\n", strike);
+
+        if(strike !=0 && ball != 0)
+            System.out.printf("%d볼 %d스트라이크\n", strike, ball);
+
+        if (strike == 3)
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
+        return strike;
+    }
+
+    int continueGame()throws IllegalArgumentException{
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String playInput = Console.readLine();
+        int play = Integer.parseInt(playInput);
+        if(!(play == 1 || play == 2))
+            throw new IllegalArgumentException();
+
+        return play;
+
+    }
+}
+public class Application {
+    public static void main(String[] args){
+        // TODO: 프로그램 구현
+        BaseballGame b = new BaseballGame();
+
+        // 게임 시작멘트 출력
+        b.printStart();
+
+        while(true) {
+            // 랜덤으로 세 자리 숫자 생성
+            List<Integer> randomNum = b.getRandomNum();
+            while (true) {
+                // 사용자에게 세 자리 수 입력받기
+                List<Integer> userNum = b.getUserInput();
+                // 랜덤값과 사용자 입력값을 비교
+                b.compareNum(randomNum,userNum);
+                // 힌트 출력
+                int strikeResult = b.printHint();
+                // 3개의 숫자를 모두 맞히면 게임 종료
+                if (strikeResult == 3)
+                    break;
+            }
+            // 다시 시작할 건지 종료할 건지 멘트 출력
+            int playResult = b.continueGame();
+            // 사용자 입력이 2 -> 완전히 종료
+            if(playResult == 2)
                 break;
         }
     }
 }
-/*
-int play = 1;
- while(play==1){
- // 컴퓨터 랜덤값 생성
- // 사용자 입력받고 숫자비교하는 함수 -> 맞출때까지 반복
 
- if(사용자 입력값 == 2){
-    play = 2;
-    }
-}
 
- */
