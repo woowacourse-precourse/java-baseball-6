94f99fe2a846e96035c1a2372c2d11bdc7edd349
김경은 <109902932+KyeongzowD@users.noreply.github.com>
2023-10-25 15:17:08 +0900

    예외처리 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,14 +1,12 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
+
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
-import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
 
 public class Application {
     public static void main(String[] args) {
@@ -25,37 +23,38 @@ public class Application {
             }
             game(random_num);
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            if(readLine().equals("2"))
+            String restart=readLine();
+            if(restart.equals("2"))
                 break;
-            else if(readLine().equals("1")){}
+            else if(restart.equals("1")){}
             else{
                 throw new IllegalArgumentException("1 또는 2를 입력해야합니다.");
             }
         }
 
     }
-    public static void game(List rand_num){
+    public static void game(List random_num){
         while(true){
             int ball=0, strike=0;
             System.out.print("숫자를 입력해주세요 : ");
 
             List<Integer> input_num=make_List(readLine());
 
-            if(input_num.get(0).equals(rand_num.get(0))) {
+            if(input_num.get(0).equals(random_num.get(0))) {
                 strike++;
-            }else if(input_num.get(0).equals(rand_num.get(1))||input_num.get(0).equals(rand_num.get(2))){
+            }else if(input_num.get(0).equals(random_num.get(1))||input_num.get(0).equals(random_num.get(2))){
                 ball++;
             }
 
-            if(input_num.get(1).equals(rand_num.get(1))) {
+            if(input_num.get(1).equals(random_num.get(1))) {
                 strike++;
-            }else if(input_num.get(1).equals(rand_num.get(0))||input_num.get(1).equals(rand_num.get(2))){
+            }else if(input_num.get(1).equals(random_num.get(0))||input_num.get(1).equals(random_num.get(2))){
                 ball++;
             }
 
-            if(input_num.get(2).equals(rand_num.get(2))) {
+            if(input_num.get(2).equals(random_num.get(2))) {
                 strike++;
-            }else if(input_num.get(2).equals(rand_num.get(0))||input_num.get(2).equals(rand_num.get(1))){
+            }else if(input_num.get(2).equals(random_num.get(0))||input_num.get(2).equals(random_num.get(1))){
                 ball++;
             }
 
@@ -75,17 +74,20 @@ public class Application {
         }
     }
     public static List<Integer> make_List(String s){
-        List<Integer> input_num;
+        List<Integer> input_num=new ArrayList<>();
         if(s.length()!=3){//크기가 3이 아닐 경우
             throw new IllegalArgumentException("크기가 3이 아닙니다.");
         } else if (s.charAt(0)<49||s.charAt(0)>57||s.charAt(1)<49||s.charAt(1)>57||s.charAt(2)<49||s.charAt(2)>57) {//0==48, 9==57
             throw new IllegalArgumentException("1부터 9까지의 정수가 아닙니다.");
-        } else {
+        } else if(s.charAt(0)==s.charAt(1)||s.charAt(0)==s.charAt(2)||s.charAt(1)==s.charAt(2)) {
+            throw new IllegalArgumentException("숫자가 중복되었습니다.");
+        }else {
             input_num=new ArrayList<>();
             input_num.add(Character.getNumericValue(s.charAt(0)));
             input_num.add(Character.getNumericValue(s.charAt(1)));
             input_num.add(Character.getNumericValue(s.charAt(2)));
         }
+
         return input_num;
     }
 }
