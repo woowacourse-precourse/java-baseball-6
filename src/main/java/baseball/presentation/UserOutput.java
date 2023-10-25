f3582c64633f897d1f868a573f7dd77b22324215
TaeHeyeon_Kim <thk98k@gmail.com>
2023-10-25 19:58:34 +0900

    feat: 재시작 출력 메세지

--- src/main/java/baseball/presentation/UserOutput.java
+++ src/main/java/baseball/presentation/UserOutput.java
@@ -15,6 +15,12 @@ public class UserOutput {
         }
         System.out.println(message);
 
-        return strike == 3;
+        if (strike == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            return true;
+        }
+
+        return false;
     }
 }
