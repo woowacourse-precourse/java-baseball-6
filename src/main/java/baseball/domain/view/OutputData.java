8aa24af71d196d0ea15d9781ca7d15ca55762c0b
peeaaccee <wlgp8958@naver.com>
2023-10-25 18:40:56 +0900

    feat(OutputData): 결과값 창 객체의 필드와 생성자

---
+++ src/main/java/baseball/domain/view/OutputData.java
@@ -0,0 +1,13 @@
+package baseball.domain.view;
+
+public class OutputData {
+    public static final String THREE_STRIKE_WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String OUT = "아웃";
+    public static final String BALL = "볼";
+    public static final String STRIKE = "스트라이크";
+
+    private static String output = "";
+
+    private OutputData() {
+    }
+}
