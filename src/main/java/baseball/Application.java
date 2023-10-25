040098f6df19bf32f098878f0ff2658c18faf117
jongmyeongbak <withpleasure@gmail.com>
2023-10-25 16:55:20 +0900

    refactor: Variable name for clarity

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,7 +4,7 @@ import baseball.core.GameRunner;
 
 public class Application {
     public static void main(String[] args) {
-        GameRunner game = new GameRunner();
-        game.start();
+        GameRunner gameRunner = new GameRunner();
+        gameRunner.start();
     }
 }
