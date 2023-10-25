0999ec464b3ed57a6da5c4df7caae200cbdcf2a0
kdomo <dongho5088@naver.com>
2023-10-25 19:57:15 +0900

    refactor: BaseBall Range 값 상수 선언

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -4,11 +4,15 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Computer {
     private static final int INPUT_LENGTH = 3;
+    private static final int BASEBALL_MIN_RANGE = 1;
+    private static final int BASEBALL_MAX_RANGE = 9;
 
     int[] computerBaseBallNumber = new int[INPUT_LENGTH];
 
     public void generateComputerBaseBallNumber() {
-        Integer[] randomBaseBallNumber = Randoms.pickUniqueNumbersInRange(1, 9, INPUT_LENGTH).toArray(Integer[]::new);
+        Integer[] randomBaseBallNumber = Randoms
+                .pickUniqueNumbersInRange(BASEBALL_MIN_RANGE, BASEBALL_MAX_RANGE, INPUT_LENGTH)
+                .toArray(Integer[]::new);
         for (int i = 0; i < INPUT_LENGTH; i++) {
             this.computerBaseBallNumber[i] = randomBaseBallNumber[i];
         }
