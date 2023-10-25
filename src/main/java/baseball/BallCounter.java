759ca6e5a5d6502128c304af98f9514492c129eb
ImWonYong <ssassb123@daum.net>
2023-10-25 17:55:21 +0900

    feat: 카운터 출력, 게임 종료, 입력 예외 체크 - 현재 스트라이크와 볼 상태를 출력하는 기능 추가 - 3 스트라이크 시 게임 종료 기능 추가 - 입력 예외 추가 및 변경

--- src/main/java/baseball/BallCounter.java
+++ src/main/java/baseball/BallCounter.java
@@ -10,5 +10,38 @@ public class BallCounter {
         this.strike = strike;
         this.ball = ball;
     }
-    
+
+    public void printResult() {
+        StringBuilder messageBuilder = new StringBuilder();
+
+        if (ball > 0) {
+            messageBuilder.append(ball).append("볼");
+        }
+
+        if (strike > 0) {
+            appendSpace(messageBuilder);
+            messageBuilder.append(strike).append("스트라이크");
+        }
+
+        if (messageBuilder.isEmpty()) {
+            messageBuilder.append("낫싱");
+        }
+
+        System.out.println(messageBuilder);
+    }
+
+    private void appendSpace(StringBuilder messageBuilder) {
+        if (ball > 0) {
+            messageBuilder.append(" ");
+        }
+    }
+
+
+    public boolean isGameOver() {
+        if (strike == 3) {
+            return true;
+        }
+
+        return false;
+    }
 }
--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -9,35 +9,97 @@ import java.util.Set;
 public class BaseballGame {
 
     private List<Integer> inputBalls;
+    private boolean gameOver;
 
     public BaseballGame() {
+        gameOver = false;
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
     public void playGame() {
 
-        while (true) {
+        while (!gameOver) {
+            Computer computer = new Computer();
 
+            startBaseball(computer);
+
+            playNextGameOrStop();
+        }
+        System.out.println("게임 종료");
+    }
+
+    private void startBaseball(Computer computer) {
+        boolean strikeOut = false;
+
+        while (!strikeOut) {
             receiveInput();
 
+            BallCounter ballCounter = computer.checkAnswer(inputBalls);
+
+            ballCounter.printResult();
+
+            if (ballCounter.isGameOver()) {
+                strikeOut = true;
+            }
         }
+
     }
 
     private void receiveInput() {
         System.out.print("숫자를 입력해주세요: ");
         String balls = Console.readLine();
+
         checkInput(balls);
 
         makeInputBalls(balls);
     }
 
-    private void checkInput(String balls) {
-        char[] ballsCharArray = balls.toCharArray();
-        for (char input : ballsCharArray) {
-            isInputNumeric(input);
-        }
+    private void makeInputBalls(String balls) {
+        inputBalls = new ArrayList<>();
+        stringToInteger(balls);
+    }
 
-        checkForDuplicateNumbers(ballsCharArray);
+    private void stringToInteger(String balls) {
+        for (int i = 0; i < balls.length(); i++) {
+            inputBalls.add(balls.charAt(i) - '0');
+        }
+    }
+
+    private void playNextGameOrStop() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = Console.readLine();
+
+        checkInputLength(input, 1);
+        checkInputNumeric(input);
+
+        selectGameOver(Integer.valueOf(input));
+    }
+
+    private void selectGameOver(int input) {
+        if (input == 1) {
+            gameOver = false;
+        } else if (input == 2) {
+            gameOver = true;
+        }
+    }
+
+    private void checkInput(String balls) {
+        checkInputNumeric(balls);
+        checkInputLength(balls, 3);
+        checkForDuplicateNumbers(balls);
+    }
+
+    private void checkInputLength(String input, int length) {
+        if (input.length() != length) {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+    }
+
+    private void checkInputNumeric(String input) {
+        char[] ballsCharArray = input.toCharArray();
+        for (char ball : ballsCharArray) {
+            isInputNumeric(ball);
+        }
     }
 
     private void isInputNumeric(char input) {
@@ -46,25 +108,16 @@ public class BaseballGame {
         }
     }
 
-    private void checkForDuplicateNumbers(char[] ballsCharArray) {
+    private void checkForDuplicateNumbers(String input) {
         Set<Character> set = new HashSet<>();
-        for (char input : ballsCharArray) {
-            set.add(input);
+        for (char ball : input.toCharArray()) {
+            set.add(ball);
         }
 
-        if (set.size() != ballsCharArray.length) {
+        if (set.size() != input.length()) {
             throw new IllegalArgumentException("중복된 숫자가 입력되었습니다.");
         }
 
     }
-
-    private void makeInputBalls(String balls) {
-        inputBalls = new ArrayList<>();
-
-        for (int i = 0; i < balls.length(); i++) {
-            inputBalls.add(balls.charAt(i) - '0');
-        }
-    }
-
 
 }
