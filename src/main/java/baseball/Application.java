c5f7569eb9e83de1acf17b654b3a6a82e0a48f61
OneCrazyman <cjw7155@naver.com>
2023-10-25 23:22:43 +0900

    Test 1 suc Test 2 fail

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,65 @@
 //미션 - 숫자 야구
 package baseball;
 
+
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.*;
 
+
 public class Application {
+    public static void main(String[] args){
+        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (true){
+            List<Integer> computer = new ArrayList<>();
+            while (computer.size() < 3) {
+                int randomNumber = Randoms.pickNumberInRange(1, 9);
+                if (!computer.contains(randomNumber)) {
+                    computer.add(randomNumber);
+                }
+            }
+            while (true) { //루프
+                System.out.print("숫자를 입력해주세요 : ");
+                String num = new String();
+                try {
+                    num = Console.readLine(); //입력받음
+                    //예외발생
+                    if (num.length() != 3) {
+                        throw new IllegalArgumentException();
+                    } else if (!isNumber(num)) {
+                        throw new IllegalArgumentException();
+                    } else if (isOverlapNumber(num)) {
+                        throw new IllegalArgumentException();
+                    }
+
+                    Boolean result = compareNumber(num, computer);
+
+                    if (!result) {
+                        break;
+                    }
+
+                } catch (IllegalArgumentException e) {
+                    break;
+                }
+            }
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String ans = new String();
+            try{
+                ans = Console.readLine(); //입력받음
+                if (ans.equals("2")){
+                    break; //종료
+                } else if (!ans.equals("1")) {
+                    throw new IllegalArgumentException();
+                }
+            }
+            catch (NumberFormatException e){
+                throw new IllegalArgumentException();
+            }
+            //무한루프
+        }
+    }
     public static boolean isNumber(String n){
         try{
             Integer.parseInt(n);
@@ -32,7 +85,7 @@ public class Application {
         for (int digit : computer){
             comp += digit;
         }
-        
+
         for (int i = 0; i < 3; i++){
             if (user.charAt(i) == comp.charAt(i)){  //스트라이크 측정
                 strike+=1;
@@ -43,7 +96,7 @@ public class Application {
 
         //결과 출력
         if (strike==3){
-            System.out.println(strike + "스트라이크\n3개의 숫자를 모두 맞히셨습니다!");
+            System.out.println(strike + "스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             return false;
         } else if (ball>0 && strike>0) {
             System.out.println(ball + "볼 " + strike + "스트라이크");
@@ -60,48 +113,4 @@ public class Application {
         }
     }
 
-    public static void main(String[] args){
-        // TODO: 프로그램 구현
-        System.out.println("숫자 야구 게임을 시작합니다.");
-
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
-        }
-        while (true){
-            while (true) { //루프
-                try {
-                    System.out.println("숫자를 입력해주세요 : ");
-                    String num = Console.readLine(); //입력받음
-                    //예외발생
-                    if (num.length() != 3) {
-                        throw new IllegalArgumentException();
-                    } else if (!isNumber(num)) {
-                        throw new IllegalArgumentException();
-                    } else if (isOverlapNumber(num)) {
-                        throw new IllegalArgumentException();
-                    }
-
-                    Boolean result = compareNumber(num, computer);
-
-                    if (!result) {
-                        break;
-                    }
-
-                } catch (IllegalArgumentException e) {
-                    break;
-                }
-            }
-            System.out.println("게임 종료");
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            String ans = Console.readLine(); //입력받음
-            if (ans.equals("2")){
-                break; //종료
-            }
-            //무한루프
-        }
-    }
 }
