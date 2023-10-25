8c43e90b1e138d77c0c452f9f04829744cc5ad34
realcold0 <realcold0@gmail.com>
2023-10-25 19:01:30 +0900

    refactor : else if 를 if로 하여 가독성 증가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -15,7 +15,7 @@ public class InputView {
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
             if (Character.isDigit(c)) {
-                playerAnswer.add(Character.getNumericValue(c));
+                playerAnswer.add(c - '0');
             }
         }
         return playerAnswer;
@@ -29,7 +29,8 @@ public class InputView {
             char c = input.charAt(i);
             if (!Character.isDigit(c)) {
                 throw new IllegalArgumentException("1~9 사이의 숫자를입력하세요");
-            } else if (Character.isDigit(c) && Character.getNumericValue(c) == 0) {
+            }
+            if (Character.isDigit(c) && Character.getNumericValue(c) == 0) {
                 throw new IllegalArgumentException("1~9 사이의 숫자를입력하세요");
             }
         }
