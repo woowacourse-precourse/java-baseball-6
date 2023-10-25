ed7948dd708d5218f541c63003f657bb355589e8
= <littlebigman.ai@gmail.com>
2023-10-25 13:33:47 +0900

    update Application.java

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import baseball.service.BaseballGameService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseballGameService baseballGameService = new BaseballGameService();
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        boolean flag = true;
+        int size = 3;
+        while (flag) {
+            baseballGameService.run(size);
+            flag = baseballGameService.replay();
+        }
+        System.out.println("프로그램을 종료합니다.");
     }
 }
