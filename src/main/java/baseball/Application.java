ef5140e198bbd6864c1e165f05aa5831673020a7
moomint8 <office05053@gmail.com>
2023-10-25 21:29:17 +0900

    2023/10/25 21:30 3차 개선

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,10 +13,10 @@ public class Application {
 
     private static void mainLoop() {
         List<Integer> computer = new ArrayList<>(); // 정답
+        System.out.println("숫자 야구 게임을 시작합니다.");
 
         while (true) {
             makeRandomNumberList(computer);   // 랜덤한 서로 다른 세 자리 수 생성
-            System.out.println("숫자 야구 게임을 시작합니다.");
             subLoop(computer);
 
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");  // 종료 분기
