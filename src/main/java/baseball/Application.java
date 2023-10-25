043d3081e46b91410668776778aa4e7e4a636c0b
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-10-25 11:27:34 +0900

    프로그램 구현 완료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,14 +1,12 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
-import java.util.List;
+import baseball.manager.GameManager;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameManager manager = new GameManager();
+        manager.run();
     }
 
 }
