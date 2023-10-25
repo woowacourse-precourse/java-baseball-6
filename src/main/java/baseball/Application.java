51c65a99c9e3c0259b181d15f556b6d27325546b
Minhyuk Kim <kim.minhyuk@mintech.kr>
2023-10-25 23:08:54 +0900

    refactor: 기능별로 메서드 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,70 +11,76 @@ import java.util.stream.Collectors;
 public class Application {
     private static final int INPUT_SIZE = 3;
     public static void main(String[] args) {
+        baseballGame();
+    }
 
+    private static void baseballGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
         while (true) {
-            List<Integer> computer = new ArrayList<>();
-            while (computer.size() < INPUT_SIZE) {
-                int randomNumber = Randoms.pickNumberInRange(1, 9);
-                if (!computer.contains(randomNumber)) {
-                    computer.add(randomNumber);
+            List<Integer> computer = getRandomIntegers();
+            playOneRound(computer);
+
+            if (!askPlayAgain()) break;
+        }
+    }
+
+    private static void playOneRound(List<Integer> computer) {
+        while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
+            String user = Console.readLine();
+            inputValueExceptionHandler(user);
+            ArrayList<Integer> userInputList = convertStringToIntList(user);
+
+            int strikeCount = 0;
+            int ballCount = 0;
+            for (int i = 0; i < INPUT_SIZE; i++) {
+                if (userInputList.get(i) == computer.get(i)) {
+                    strikeCount++;
+                } else if (computer.contains(userInputList.get(i))) {
+                    ballCount++;
                 }
             }
 
-            while (true) {
-                System.out.print("숫자를 입력해주세요 : ");
-                String user = Console.readLine();
+            if (printResults(strikeCount, ballCount)) break;
+        }
+    }
 
-                if (user.length() != INPUT_SIZE || !isInteger(user)) {
-                    throw new IllegalArgumentException();
-                }
+    private static boolean askPlayAgain() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        int playAgain = Integer.parseInt(Console.readLine());
+        if (playAgain == 1) {
+            return true;
+        }
+        return false;
+    }
 
-                ArrayList<Integer> userInputList = user.chars()
-                        .mapToObj(Character::getNumericValue)
-                        .collect(Collectors.toCollection(ArrayList::new));
-
-                HashSet<Integer> uniqueSet = new HashSet<>();
-                for (Integer input : userInputList) {
-                    if (!uniqueSet.add(input)) {
-                        throw new IllegalArgumentException();
-                    }
-                }
-                int strikeCount = 0;
-                int ballCount = 0;
-                for (int i = 0; i < INPUT_SIZE; i++) {
-                    if (userInputList.get(i) == computer.get(i)) {
-                        strikeCount++;
-                    } else if (computer.contains(userInputList.get(i))) {
-                        ballCount++;
-                    }
-                }
-
-
-                if (strikeCount == 3) {
-                    System.out.println("3스트라이크");
-                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                    break;
-                }
-                if (strikeCount == 0 && ballCount == 0) {
-                    System.out.println("낫싱");
-                } else if (strikeCount > 0 && ballCount == 0) {
-                    System.out.println(strikeCount + "스트라이크");
-                } else if (strikeCount == 0 && ballCount > 0) {
-                    System.out.println(ballCount + "볼");
-                } else if (strikeCount > 0 && ballCount > 0) {
-                    System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
-                }
-            }
-
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            int wantMoreGame = Integer.parseInt(Console.readLine());
-            if (wantMoreGame == 2) {
-                break;
+    private static List<Integer> getRandomIntegers() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < INPUT_SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
             }
         }
+        return computer;
+    }
 
+    private static void inputValueExceptionHandler(String user) {
+        checkInputSize(user);
+        checkIsInteger(user);
+        checkDuplicate(user);
+    }
 
+    private static void checkInputSize(String user) {
+        if (user.length() != INPUT_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void checkIsInteger(String user) {
+        if (!isInteger(user)) {
+            throw new IllegalArgumentException();
+        }
     }
 
     private static boolean isInteger(String str) {
@@ -89,4 +95,49 @@ public class Application {
         }
     }
 
+    private static void checkDuplicate(String user) {
+        HashSet<Character> charSet = new HashSet<>();
+        for (int i = 0; i < user.length(); i++) {
+            char ch = user.charAt(i);
+
+            if (charSet.contains(ch)) {
+                throw new IllegalArgumentException();
+            }
+            charSet.add(ch);
+        }
+    }
+
+    private static ArrayList<Integer> convertStringToIntList(String user) {
+        ArrayList<Integer> userInputList = user.chars()
+                .mapToObj(Character::getNumericValue)
+                .collect(Collectors.toCollection(ArrayList::new));
+        return userInputList;
+    }
+
+    private static boolean printResults(int strikeCount, int ballCount) {
+        if (correctAnswer(strikeCount)) return true;
+        return wrongAnswer(strikeCount, ballCount);
+    }
+
+    private static boolean correctAnswer(int strikeCount) {
+        if (strikeCount == 3) {
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean wrongAnswer(int strikeCount, int ballCount) {
+        if (strikeCount == 0 && ballCount == 0) {
+            System.out.println("낫싱");
+        } else if (strikeCount > 0 && ballCount == 0) {
+            System.out.println(strikeCount + "스트라이크");
+        } else if (strikeCount == 0 && ballCount > 0) {
+            System.out.println(ballCount + "볼");
+        } else if (strikeCount > 0 && ballCount > 0) {
+            System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+        }
+        return false;
+    }
 }
