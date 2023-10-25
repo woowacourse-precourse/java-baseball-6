b77073ccbc63e52868888ca6cee18aac929e5a13
gundorit <packdev937@gmail.com>
2023-10-25 13:41:49 +0900

    feat (RandomCreator) : 컴퓨터는 랜덤한 세 자리 수를 생성한다

---
+++ src/main/java/baseball/domain/RandomCreator.java
@@ -0,0 +1,10 @@
+package baseball.domain;
+
+import java.util.List;
+
+public interface RandomCreator {
+
+    int createRandomNumber();
+
+    List<Integer> createRandomNumbers();
+}
---
+++ src/main/java/baseball/domain/SystemRandomCreator.java
@@ -0,0 +1,33 @@
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SystemRandomCreator implements RandomCreator {
+
+    public static final int NUMBER_SIZE = 3;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 9;
+
+    @Override
+    public int createRandomNumber() {
+        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+    }
+
+    @Override
+    public List<Integer> createRandomNumbers() {
+        List<Integer> randomNumbers = new ArrayList<>();
+        while (randomNumbers.size() < NUMBER_SIZE) {
+            addUniqueNumber(randomNumbers, createRandomNumber());
+        }
+        return randomNumbers;
+    }
+
+    private void addUniqueNumber(List<Integer> randomNumbers, int randomNumber) {
+        if (!randomNumbers.contains(randomNumber)) {
+            randomNumbers.add(randomNumber);
+        }
+    }
+}
