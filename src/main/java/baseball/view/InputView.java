c29e56c2912d26b251be292700dbe531429b661a
gavin-ko <kas6610@gmail.com>
2023-10-25 18:56:26 +0900

    feat : 숫자 입력 기능 구현
    - 야구 게임의 이닝 구간 중 숫자 입력 기능
    - 예외 처리(1~9 범위값만 입력)

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,4 +1,26 @@
 package baseball.view;
+import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    public String readGuess() {
+        String guess = readLine();
+        validateNumberString(guess);
+
+        return guess;
+    }
+
+    private void validateNumberString(String guess) {
+        if (!guess.matches("^[0-9]*$")) {
+            throw new NotNumberStringException();
+        }
+    }
+
+    private String readLine() {
+        return Console.readLine().replaceAll(" ", "");
+    }
+
+    public static class NotNumberStringException extends IllegalAccessError{
+    }
+
+
 }
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,4 +4,10 @@ public class OutputView {
     public void printGreet(){
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
+    public void printInputNumber(){
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
+    //결과, 스트라이크, 볼 추가 예정
 }
