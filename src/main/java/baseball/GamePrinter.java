353fbe70f0eda27adbae251be1360f59211be29f
haebyun <bhk4392@gmail.com>
2023-10-25 20:06:18 +0900

    fix(GamePrinter): 의존성 주입을 하는 의미가 없어져 버릴 수 있는 상태 상태 발생으로 static 삭제

--- src/main/java/baseball/GamePrinter.java
+++ src/main/java/baseball/GamePrinter.java
@@ -1,14 +1,7 @@
 package baseball;
 
 public interface GamePrinter {
-    String STARTING_MESSAGE = "숫자 야구 게임을 시작합니다.";
-    String ENDING_MESSAGE = "숫자 야구 게임을 종료합니다.";
+    void printStartingMessage();
 
-    static void printStartingMessage() {
-        System.out.println(STARTING_MESSAGE);
-    }
-
-    static void printEndingMessage() {
-        System.out.println(ENDING_MESSAGE);
-    }
+    void printEndingMessage();
 }
