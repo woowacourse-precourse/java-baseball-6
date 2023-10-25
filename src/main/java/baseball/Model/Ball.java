1b6b8f253d9f43a1574feca59c3340fb66bd5edf
gomdol <oioifor@naver.com>
2023-10-25 22:31:00 +0900

    feat : 스트라이크 낫싱 체크 함수

--- src/main/java/baseball/Model/Ball.java
+++ src/main/java/baseball/Model/Ball.java
@@ -3,17 +3,17 @@ package baseball.Model;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Ball {
-    private Integer length = null;
+    private Integer numberLength = null;
     private String number = "";
 
 
     public Ball(int numberLength){
-        this.length = numberLength;
+        this.numberLength = numberLength;
     }
 
-    public String generateNumbers(){
+    public String randomNumber(){
         number = "";
-        while(number.length() < length){
+        while(number.length() < numberLength){
             int randomNumber = Randoms.pickNumberInRange(1, 9);
 
             if(!number.contains(Integer.toString(randomNumber))){
@@ -24,21 +24,64 @@ public class Ball {
         return new String(number);
     }
 
-    public void checkNumbers(String inputNumbers) throws IllegalArgumentException{
-        if(inputNumbers.length() != this.length){
+    public void checkNumber(String inputNumbers) throws IllegalArgumentException{
+        if(inputNumbers.length() != this.numberLength){
             throw new IllegalArgumentException();
         }
 
-        for(int i = 0; i < this.length; i++){
+        for(int i = 0; i < this.numberLength; i++){
             String checkNumber = new String(inputNumbers);
 
             if(!Character.isDigit(inputNumbers.charAt(i))){
                 throw new IllegalArgumentException();
             }
-            if(checkNumber.replace(Character.toString(inputNumbers.charAt(i)), "").length() != this.length-1){
+            if(checkNumber.replace(Character.toString(inputNumbers.charAt(i)), "").length() != this.numberLength-1){
                 throw new IllegalArgumentException();
             }
 
         }
+    }
+
+
+    private int getBall(String inputNumbers){
+        int ballCount = 0;
+
+        for(int i = 0; i < this.numberLength; i++){
+            String tmpInputNumber = Character.toString(inputNumbers.charAt(i));
+            if(this.number.contains(tmpInputNumber) && i != number.indexOf(tmpInputNumber)){
+                ballCount += 1;
+            }
+        }
+
+        return ballCount;
+    }
+
+    private int getStrike(String inputNumbers){
+        int strikeCount = 0;
+
+        for(int i = 0; i < this.numberLength; i++){
+            if(this.number.indexOf(inputNumbers.charAt(i)) == i){
+                strikeCount += 1;
+            }
+        }
+
+        return strikeCount;
+    }
+
+    public boolean restartGame(int flag) throws IllegalArgumentException{
+        if(flag == 1){
+            randomNumber();
+            return true;
+        }
+        else if(flag == 2){
+            return false;
+        }
+        else{
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public String getNumbers(){
+        return new String(number);
     }
 }
