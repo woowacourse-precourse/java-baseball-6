51a0361c69ed5b8d8253c5ec9d5e31d1b78282a9
chosunghyun18 <krismon@naver.com>
2023-10-25 11:25:30 +0900

    test : add 사용자 공 생성 확인 - 게임플레이가 3자리의 숫자를 입력받아 공을 만든다.

--- src/test/java/baseball/model/BaseBallGamePlayerTest.java
+++ src/test/java/baseball/model/BaseBallGamePlayerTest.java
@@ -1,5 +1,26 @@
 package baseball.model;
 
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import baseball.model.ball.Balls;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class BaseBallGamePlayerTest {
+
+    @Test
+    @DisplayName("사용자 공 생성 확인")
+    public void playerBallsGenerateTest() {
+        BaseBallGamePlayer baseBallGamePlayer = new BaseBallGamePlayer();
+        List<Integer> givenNUmber = Arrays.asList(1, 2, 3);
+        Balls targetBalls = new Balls(givenNUmber);
+
+        Balls balls = baseBallGamePlayer.generateBalls(givenNUmber);
+        assertThat(balls).isEqualTo(targetBalls);
+    }
 
 }
