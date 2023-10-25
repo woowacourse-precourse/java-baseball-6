03dcbfefe5306de53b743536825e999ca82783d0
염규영 <yeomky98@naver.com>
2023-10-25 18:45:51 +0900

    fix: resolve problem in pickRandomNumbers method for correct number selection

--- src/main/java/baseball/model/RandomNumberGenerator.java
+++ src/main/java/baseball/model/RandomNumberGenerator.java
@@ -6,12 +6,14 @@ public class RandomNumberGenerator {
     public static int[] pickRandomNumbers(int size, int min, int max) {
         // Random 값 추출은 camp.nextstep.edu.missionutils.Randoms의 pickNumberInRange()를 활용한다.
         int[] pickedNumbers = new int[size];
-        for (int i = 0; i < size; i++) {
+        int index = 0;
+        while (index < size) {
             int randomNumber = Randoms.pickNumberInRange(min, max);
             if (isNumberInArray(randomNumber, pickedNumbers)) {
                 continue;
             }
-            pickedNumbers[i] = randomNumber;
+            pickedNumbers[index] = randomNumber;
+            index++;
         }
         return pickedNumbers;
     }
