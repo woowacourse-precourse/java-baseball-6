7917000e0a5c868111572e778b78291b1b9b407a
HongYeseul <ghd8119@naver.com>
2023-10-25 12:11:54 +0900

    :recycle: refactor: 메인 메소드 로직 수정 - 메인 메소드에서는 game.run()만 불러올 수 있도록 함   ↪ game.run()의 로직을 좀 더 간소화 시킬 것

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,32 +2,7 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        Computer computer = new Computer();
-        User user = new User();
         Game game = new Game();
-
-        game.setGame();
-
-        System.out.println("숫자 야구 게임을 시작합니다.");
-
-        while (game.isPlaying()){
-            computer.init();
-
-            // 상대방(컴퓨터)은제 1에서 9까지 서로 다른 임의의 수(랜덤) 3개를 선택
-            computer.selectRandomNumber();
-
-            while (game.isThreeStrike()){
-                System.out.print("숫자를 입력 해 주세요 : ");
-                user.init();
-                user.selectUserNumber();
-
-                // 입력한 숫자에 대한 결과 출력
-                game.play(computer, user);
-                System.out.println(game.getGameResult());
-            }
-
-            game.isReplay();
-        }
-
+        game.run();
     }
 }
--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -21,6 +21,35 @@ public class Game {
         return playing;
     }
 
+    public void run(){
+        // TODO: 해당 메소드 로직 줄이기
+        Computer computer = new Computer();
+        User user = new User();
+
+        setGame();
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        while (isPlaying()){
+            computer.init();
+
+            // 상대방(컴퓨터)은제 1에서 9까지 서로 다른 임의의 수(랜덤) 3개를 선택
+            computer.selectRandomNumber();
+
+            while (isThreeStrike()){
+                System.out.print("숫자를 입력 해 주세요 : ");
+                user.init();
+                user.selectUserNumber();
+
+                // 입력한 숫자에 대한 결과 출력
+                play(computer, user);
+                System.out.println(getGameResult());
+            }
+
+            isReplay();
+        }
+    }
+
     public void play(Computer computer, User user){
         init();
 
