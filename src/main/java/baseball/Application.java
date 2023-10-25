17ae26a1fd628250a659b74b6f36dcc4bb5ad1fa
kangaroo0 <ljw42871@gmail.com>
2023-10-25 21:07:02 +0900

    Fix: Application 쪽 구조 에러

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,6 +6,7 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        BaseballService.gameStart();
+        BaseballService baseballService = new BaseballService();
+        baseballService.gameStart();
     }
 }
