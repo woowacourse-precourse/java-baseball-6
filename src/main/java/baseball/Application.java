2cf9177f4ba92fcd6cd053b4d89c9429ed8130d6
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2023-10-25 23:02:52 +0900

    리팩토링

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import baseball.Controller.BaseballGame;
+import baseball.Model.RetryNumber;
+import baseball.View.InputView;
+import baseball.View.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO: 숫자 야구 게임 구현
+        OutputView.printStartGame();
+        do {
+            BaseballGame baseballGame = new BaseballGame();
+            baseballGame.start();
+        } while (Retry());
     }
+
+    public static boolean Retry() {
+        RetryNumber retryNumber = new RetryNumber(InputView.setRetryNumber());
+
+        if (retryNumber.getRetryNumber().equals("1")) {
+            return true;
+        }
+        return false;
+    }
+
 }
--- src/main/java/baseball/View/OutputView.java
+++ src/main/java/baseball/View/OutputView.java
@@ -1,31 +1,31 @@
 package baseball.View;
 
 public class OutputView {
-    public static void printStartGame(){
+    public static void printStartGame() {
         System.out.println("숫자 야구를 시작합니다.");
     }
 
-    public static void printStrikeBall(){
-        System.out.println("볼 ");
+    public static void printStrikeBall() {
+        System.out.print("볼 ");
     }
 
-    public static void printBall(){
+    public static void printBall() {
         System.out.println("볼");
     }
 
-    public static void printStrike(){
+    public static void printStrike() {
         System.out.println("스트라이크");
     }
 
-    public static void printNothing(){
+    public static void printNothing() {
         System.out.println("낫싱");
     }
 
-    public static void printCount(int count){
-        System.out.println(count);
+    public static void printCount(int count) {
+        System.out.print(count);
     }
 
-    public static void printSetGameMessage(){
+    public static void printSetGameMessage() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 }
