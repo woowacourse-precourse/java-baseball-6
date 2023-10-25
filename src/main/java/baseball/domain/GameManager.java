9b880f469a7921d0f724c14c75af163d948bcad8
Jmmk <jmk525@gmail.com>
2023-10-25 22:21:08 +0900

    feat: 숫자야구 로직 구현

--- src/main/java/baseball/domain/GameManager.java
+++ src/main/java/baseball/domain/GameManager.java
@@ -1,6 +1,14 @@
 package baseball.domain;
 
+import java.util.List;
+
+import static baseball.domain.Game.NUM_DIGITS;
+import static baseball.util.OutputUtil.printChooseOptionMessage;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class GameManager {
+    private static final String RESTART = "1";
+    private static final String END = "2";
     private Integer balls;
     private Integer strikes;
     private boolean isNumberCorrect;
@@ -10,6 +18,62 @@ public class GameManager {
         balls = 0;
         isNumberCorrect = false;
         gameResult = new GameResult();
+    }
+
+    public void checkGuessResult(List<Integer> targetNumber, List<Integer> guessNumber) {
+        int balls = calculateBalls(targetNumber, guessNumber);
+        int strikes = calculateStrikes(targetNumber, guessNumber);
+        createResultString(strikes, balls);
+    }
+
+    public boolean checkIsCorrectNumber() {
+        if (strikes == NUM_DIGITS) {
+            isNumberCorrect = true;
+            gameResult.addCorrectMessage();
+        }
+        return isNumberCorrect;
+    }
+
+    public boolean checkRestartGame() {
+        printChooseOptionMessage();
+        switch (readLine()) {
+            case RESTART:
+                return true;
+            case END:
+                return false;
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+
+    private void createResultString(Integer strikes, Integer balls) {
+        if (balls > 0) {
+            gameResult.addBallCount(balls);
+        }
+        if (strikes > 0) {
+            gameResult.addStrikeCount(strikes);
+        }
+        if (gameResult.isResultBlank()) {
+            gameResult.addNothingMessage();
+        }
+    }
+
+    private int calculateStrikes(List<Integer> targetNumber, List<Integer> guessNumber) {
+        for (int i = 0; i < NUM_DIGITS; i++) {
+            if (targetNumber.get(i).equals(guessNumber.get(i))) {
+                strikes++;
+            }
+        }
+        return strikes;
+    }
+
+    private int calculateBalls(List<Integer> targetNumber, List<Integer> guessNumber) {
+        for (int i = 0; i < NUM_DIGITS; i++) {
+            if (targetNumber.contains(guessNumber.get(i)) && !targetNumber.get(i).equals(guessNumber.get(i))) {
+                balls++;
+            }
+        }
+        return balls;
     }
 
     public void displayResult() {
--- src/main/java/baseball/domain/GameResult.java
+++ src/main/java/baseball/domain/GameResult.java
@@ -8,7 +8,27 @@ public class GameResult {
         result = "";
     }
 
+    public void addCorrectMessage() {
+        result += "\n" + "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    }
+
     public void displayResult() {
         System.out.println(result);
+    }
+
+    public void addBallCount(Integer balls) {
+        result += balls + "볼 ";
+    }
+
+    public void addStrikeCount(Integer strikes) {
+        result += strikes + "스트라이크";
+    }
+
+    public boolean isResultBlank() {
+        return (result.equals(""));
+    }
+
+    public void addNothingMessage() {
+        result += "낫싱";
     }
 }
