d59442fee85ef5b76414c7e7c526e2b32bdc6e8b
Nicholas <nkchung@ucsd.edu>
2023-10-25 22:31:52 +0900

    remove unused variable

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,7 +7,7 @@ public class Application {
     private static final String greeting = "숫자 야구 게임을 시작합니다.";
     private static final String winPrompt = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String playAgainPrompt = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-    private static String playInput = "";
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Game.testMode = false;
