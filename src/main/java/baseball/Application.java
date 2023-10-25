1da44795799c5bae349f96bc726685b8bba7b23d
minkyole <7719s@nave.com>
2023-10-25 23:24:14 +0900

    feat : BaseballGame class 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,14 +2,7 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        do {
-            Computer computer = new Computer();
-            User user = new User();
-            while (!computer.checkNum(user.getUserNum())) {
-                user.inputNum();
-            }
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        } while (User.chooseRestart());
+        BaseballGame baseballGame = new BaseballGame();
+        baseballGame.play();
     }
 }
