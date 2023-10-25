1c9d25e1c4008839e3affe2580ef34ef8f11c6c1
Kim mingyu <minq0302@naver.com>
2023-10-25 21:01:34 +0900

    feat: Application main 로직 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
 
+import baseball.Controller.BaseballGameController;
+import baseball.Model.BaseballGameModel;
+import baseball.View.BaseballGameView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BaseballGameModel baseballGameModel = new BaseballGameModel();
+        BaseballGameView baseballGameView = new BaseballGameView();
+        BaseballGameController baseballGameController = new BaseballGameController(baseballGameModel, baseballGameView);
+        
+        baseballGameController.play();
     }
 }
--- src/main/java/baseball/View/BaseballGameView.java
+++ src/main/java/baseball/View/BaseballGameView.java
@@ -32,6 +32,6 @@ public class BaseballGameView {
     }
 
     public void showResult(String result) {
-        System.out.println("result");
+        System.out.println(result);
     }
 }
