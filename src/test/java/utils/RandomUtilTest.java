daab92218e0ede79436d94246b1914591c325459
OhTaeHo-97 <57743934+OhTaeHo-97@users.noreply.github.com>
2023-10-25 17:41:20 +0900

    test: 상대방(컴퓨터) 값 생성 기능 테스트

--- src/test/java/utils/RandomUtilTest.java
+++ src/test/java/utils/RandomUtilTest.java
@@ -1,4 +1,30 @@
-import static org.junit.jupiter.api.Assertions.*;
-class RandomUtilTest {
-  
+package utils;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("상대방 수 생성 테스트")
+class RandomUtilTest extends NsTest {
+    @Test
+    @DisplayName("정상 작동")
+    void success() {
+        List<Integer> answer = List.of(1, 2, 3);
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("");
+                    assertThat(output()).isEqualTo(answer.toString());
+                },
+                1, 2, 3
+        );
+    }
+
+    @Override
+    protected void runMain() {
+        System.out.println(RandomUtil.generateBaseballNumber());
+    }
 }
