022326b181734d349e1fbf27c81054c4a1cccc90
ehdtndla123 <ehdtndla123@naver.com>
2023-10-25 21:24:12 +0900

    feat : 프로그램 구현 , PlayerFactory 를 이용하여 user 또는 computer 를 가져와서 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import game.Game;
+import util.PlayerFactory;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        final int ballNumberLength = 3;
+        Game game = new Game(PlayerFactory.getUser(ballNumberLength), PlayerFactory.getComputer());
+        game.run();
     }
 }
