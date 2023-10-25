a51014a41ac647e0b811f48cc6a0338f067c7f8b
용가리 <148646502+07rz1@users.noreply.github.com>
2023-10-25 23:21:50 +0900

    Application.java

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,7 +3,6 @@ package baseball;
 import java.util.ArrayList;
 import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
-import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
@@ -25,10 +24,10 @@ public class Application {
 
                     while(strike != 3){
                         try {
-                        System.out.println("숫자를 입력해주세요 : ");
-                        
-                        
-                                if(inputNum.size() != 3 || inputNum.contains(0)) {
+                            System.out.println("숫자를 입력해주세요 : ");
+                            
+
+                            if(inputNum.size() != 3 || inputNum.contains(0)) {
                                 throw new IllegalArgumentException("잘못된 값을 입력했습니다."); 
                            
                             }
@@ -42,12 +41,12 @@ public class Application {
                                     throw new IllegalArgumentException("잘못된 값을 입력했습니다.");
                                 }
                             }
-                        } 
-                catch (IllegalArgumentException e) {
+
+                        }catch (IllegalArgumentException e) {
                             playagain = 2;
                             break;
                             }
-                    }
+                    
                         
                             for(int i = 0; i < inputNum.size(); i++){
                                 if(randomNum.contains(inputNum.get(i))){
@@ -75,3 +74,6 @@ public class Application {
             }
                 
         }
+    }
+
+
