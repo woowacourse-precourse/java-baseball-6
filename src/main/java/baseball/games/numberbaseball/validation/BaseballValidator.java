2b407b412e765fb9474fb74af68aa5a88af156a3
haebyun <bhk4392@gmail.com>
2023-10-25 17:47:32 +0900

    feat(BaseballValidator): 플레이어의 입력 문자열 검증 기능 구현

--- src/main/java/baseball/games/numberbaseball/validation/BaseballValidator.java
+++ src/main/java/baseball/games/numberbaseball/validation/BaseballValidator.java
@@ -5,6 +5,18 @@ public class BaseballValidator {
     private static final Character NUMBERS_RANGE_START = '1';
     private static final Character NUMBERS_RANGE_END = '9';
 
+    public void validateNumbersInput(String input) {
+        if (isInvalidNumbersLength(input)) {
+            throw new IllegalArgumentException("3개의 숫자를 입력해야 합니다.");
+        }
+        if (containsOutRangeInput(input)) {
+            throw new IllegalArgumentException("모든 입력은 1에서 9까지의 숫자만 입력이 가능합니다.");
+        }
+        if (containsDuplicatedInput(input)) {
+            throw new IllegalArgumentException("중복된 숫자를 입력할 수 없습니다.");
+        }
+    }
+
     private boolean isInvalidNumbersLength(String input) {
         return input.length() != NUMBERS_INPUT_SIZE;
     }
