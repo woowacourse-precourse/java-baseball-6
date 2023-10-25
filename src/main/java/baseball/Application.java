1195e1e16117c326b92c0acc75670a9abfa01156
Sinorang <aodkrak@naver.com>
2023-10-25 09:19:08 +0900

    게임 재시작 메소드명 수정 및 코드 수정
    - 게임 재시작 메소드 타입이 boolean이므로 isRestartGame으로 메소드명 변경
    - 사용자가 잘못된 입력을 할 경우 IllegalArgumentException를 발생시키므로 잘못된 입력에 대한 기능을 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -21,7 +21,7 @@ public class Application {
                 System.out.println(result);
             }
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            restart = restartGame.restartGame();
+            restart = restartGame.isRestartGame();
         }
     }
 }
--- src/main/java/baseball/RestartGame.java
+++ src/main/java/baseball/RestartGame.java
@@ -4,24 +4,25 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class RestartGame {
 
-    public boolean restartGame() throws IllegalArgumentException {
-        while (true) {
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            int answer = getAnswer();
-            if (answer == ConstantVO.RESTART) {
-                return true;
-            } else if (answer == ConstantVO.GAME_OVER) {
-                return false;
-            } else {
-                System.out.println("유효한 입력이 아닙니다. 다시 입력하세요");
-            }
+    public boolean isRestartGame() throws IllegalArgumentException {
+        boolean restart = false;
+
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        int answer = getAnswer();
+
+        if (answer == ConstantVO.RESTART) {
+            restart = true;
+        } else if (answer == ConstantVO.GAME_OVER) {
+            restart = false;
         }
+        return restart;
     }
 
     private int getAnswer() throws IllegalArgumentException {
         int answer = Integer.parseInt(Console.readLine());
 
-        if(answer == 0 || answer > ConstantVO.GAME_OVER) {
+        if (answer == 0 || answer > ConstantVO.GAME_OVER) {
             throw new IllegalArgumentException();
         }
         return answer;
