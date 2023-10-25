0b9c836a08d762bc07bec5c529e5a80c207e0312
kkonii <119216472+kkonii@users.noreply.github.com>
2023-10-25 23:42:17 +0900

    Refactor(guideMessage) : 게임 가이드 메세지 상수들을 GuideMessage 클래스로 분리

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -4,11 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class Game {
-    private final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
-    private final String INPUT_REQUEST_MESSAGE = "숫자를 입력해주세요 : ";
-    private final String GAME_EXIT_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    private final String RESTART_INPUT_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-
     private Result result = new Result();
     private Validator validator = new Validator();
     private List<Character> randomNumbers;
@@ -20,13 +15,13 @@ public class Game {
     }
 
     private void start() {
-        System.out.println(GAME_START_MESSAGE);
+        System.out.println(GuideMessage.GAME_START_MESSAGE);
 
         resetRandomNumbers();
     }
 
     private void play() {
-        System.out.print(INPUT_REQUEST_MESSAGE);
+        System.out.print(GuideMessage.INPUT_REQUEST_MESSAGE);
 
         String userInput = Console.readLine();
 
@@ -42,9 +37,9 @@ public class Game {
     }
 
     private void end() {
-        System.out.println(GAME_EXIT_MESSAGE);
+        System.out.println(GuideMessage.GAME_EXIT_MESSAGE);
 
-        System.out.println(RESTART_INPUT_MESSAGE);
+        System.out.println(GuideMessage.RESTART_INPUT_MESSAGE);
 
         String restartInput = Console.readLine();
 
