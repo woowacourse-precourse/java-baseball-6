276556286aacd379def0930a7207e2b6f72218ce
hansg931 <hansg0509@cau.ac.kr>
2023-10-25 19:32:51 +0900

    feat: 콘솔 출력 메시지 수정

--- src/main/java/baseball/util/Message.java
+++ src/main/java/baseball/util/Message.java
@@ -4,7 +4,7 @@ public class Message {
 	public static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
 	public static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
 	public static final String GAME_END_MESSAGE = "개의 숫자를 모두 맞히셨습니다! 게임 종료";
-	public static final String GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+	public static final String GAME_RESTART_MESSAGE = "게임을 새로 시작하려면" + Constant.GAME_RESTART_FLAG + ", 종료하려면 " + Constant.GAME_END_FLAG + "를 입력하세요.";
 	public static final String BALL_MESSAGE = "볼";
 	public static final String STRIKE_MESSAGE = "스트라이크";
 	public static final String NOTHING_MESSAGE = "낫싱";
