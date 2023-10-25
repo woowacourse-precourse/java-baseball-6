204bd5f2cbe42a7483bb7a25741a1a3af70f75d1
Wangguri <anordinarykorean@gmail.com>
2023-10-25 23:24:07 +0900

    숫자 야구 게임 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+		System.out.println("숫자 야구 게임을 시작합니다.");
+		try {
+			BaseballUtil.comparingNumber();
+		} catch (IllegalArgumentException e) {
+			throw new IllegalArgumentException("잘못된 입력값입니다. 애플리케이션을 종료합니다.");
+		}
+		return;
+	}
 }
