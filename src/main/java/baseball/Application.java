82c872fcea09c4eb964d595664d14476363fb20f
hamings <wjdgpal9630@gmail.com>
2023-10-25 13:07:33 +0900

    Feat: 게임실행 및 입력값에 따른 반복 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,16 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        View.printStartMessage();
+        do {
+            Game game = new Game();
+            game.run();
+        }while (continueOrExit());
+
+    }
+    public static boolean continueOrExit(){
+        String answer = View.continueOrExitGame();
+        if(answer.equals("1")) return true;
+        return false;
     }
 }
