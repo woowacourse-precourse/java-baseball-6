fc3fd57763c046d5b5610d98a1a5454f07891f23
seona-moon <fhflwhwl5@gmail.com>
2023-10-25 14:30:29 +0900

    feat: GameSystem 기본적인 게임 진행 플로우 작성 및 check함수 구현

--- src/main/java/baseball/GameSystem.java
+++ src/main/java/baseball/GameSystem.java
@@ -1,2 +1,52 @@
-package baseball;public class GameSystem {
+package baseball;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+public class GameSystem {
+  private final static int NUMBER = 3;
+
+  public GameSystem() {
+    System.out.println("숫자 야구 게임을 시작합니다.");
+    playGame();
+  }
+
+  public void playGame() {
+    //컴퓨터의 값을 랜덤값으로 초기화한다.
+    Computer computer = new Computer();
+
+    while (true) {
+      //사용자의 수를 세팅한다.
+      Player player = new Player();
+
+      //컴퓨터와 숫자를 비교 후 출력한다.
+      String result = check(computer.getComputerNum(), player.getPlayerNum());
+      System.out.println(result);
+
+//      if (result.equals() {
+//        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+//        reStart();
+//        break;
+////      }
+    }
+  }
+
+
+  public String check(List<Integer> ComputerNum, List<Integer> playerNum) {
+    Result result = new Result();
+    Set<Integer> computerNumSet = new HashSet<>(ComputerNum);
+
+    for (int i = 0; i < NUMBER; i++) {
+      if (Objects.equals(playerNum.get(i), ComputerNum.get(i))) {
+        result.addStrike();
+      } else if (computerNumSet.contains(playerNum.get(i))) {
+        result.addBall();
+      }
+    }
+    return result.getResult();
+  }
 }
