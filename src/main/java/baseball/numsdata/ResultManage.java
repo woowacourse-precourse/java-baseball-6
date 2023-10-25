9a24254eca44b1178b9e02eee209a6840936be15
sora0319 <sojeoung536985@gmail.com>
2023-10-25 20:31:01 +0900

    refactor<gameSetResult> : add checkScore
    기존에는 인스턴스 생성시 고정으로 생성되게 checkScore를 하였다.
    그러나 GameSystem에서 ResultManage 인스턴스를 한번만 생성하게 하고 gameSetResult로 결과 값을 얻는 것으로 바꾸어서 checkScore 생성하는 것이 gameSetResult로 이동하였다

--- src/main/java/baseball/numsdata/ResultManage.java
+++ src/main/java/baseball/numsdata/ResultManage.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 public class ResultManage {
     private static final int MAX_SIZE = 3;
-    private final int [] checkScore = new int[MAX_SIZE];
     private int ball;
     private int strike;
 
     public void gameSetResult(List<Integer> userInput, List<Integer> randomNum) {
+        int[] checkScore = new int[MAX_SIZE];
+
         for(int computerNum = 0; computerNum < 3; computerNum++){
             for(int userNum = 0; userNum < 3; userNum++){
                 if(checkScore[userNum] == 2 ) continue;
