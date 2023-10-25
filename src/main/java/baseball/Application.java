88ada685732a2eb6f60f32860990d4e96e1af680
김라연 <rayeon@gimlayeon-ui-noteubug.local>
2023-10-25 10:34:03 +0900

    feat: 사용자로부터 숫자 입력받기 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,24 +1,49 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+    static BufferedReader br;
+
     public static void main(String[] args) {
 
         startPrint();
 
         List<Integer> computerNumbers = generateRandomNumbers();
 
+        while (true) {
+
+            List<Integer> userNumbers = inputNumberOfUser();
+
+        }
+
+    }
+
+    private static List<Integer> inputNumberOfUser() {
+        System.out.print("숫자를 입력해주세요 : ");
+
+        int number = Integer.parseInt(Console.readLine());
+        List<Integer> userNumber = new ArrayList<>();
+
+        for (int i = 0; i < 3; i ++){
+            userNumber.add(number % 10);
+            number /=10;
+        }
+
+        return userNumber;
     }
 
     private static void startPrint() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    public static List<Integer> generateRandomNumbers(){
+    public static List<Integer> generateRandomNumbers() {
         List<Integer> computerNumbers = new ArrayList<>();
         while (computerNumbers.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
