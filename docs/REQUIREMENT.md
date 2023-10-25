229366adb38d3a6aa43709e9d567328d8932c875
신영한 <syhan7516@naver.com>
2023-10-25 22:39:34 +0900

    feat(getRandomNumbers) : 임의의 수 3가지 부여 기능
    (완료) 숫자 범위 : 1~9
    (완료) 중복 여부 : X

--- docs/REQUIREMENT.md
+++ docs/REQUIREMENT.md
@@ -10,7 +10,7 @@
 - 프로그램 구현이 완료되면 ApplicationTest의 모든 테스트가 성공 확인
 - 프로그래밍 요구 사항에서 달리 명시하지 않는 한 파일, 패키지 이름을 수정하거나 이동 불가
 - 라이브러리는 camp.nextstep.edu.missionutils에서 제공하는 Randoms 및 Console API를 사용하여 구현
-- Random 값 추출은 camp.nextstep.edu.missionutils.Randoms의 pickNumberInRange()를 활용 
+- (완료) Random 값 추출은 camp.nextstep.edu.missionutils.Randoms의 pickNumberInRange()를 활용 
 - 사용자가 입력하는 값은 camp.nextstep.edu.missionutils.Console의 readLine()을 활용
 
 ## ✏️ 과제 진행 요구 사항
--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,33 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    // 임의의 수 3가지 부여 기능
+    static List<Integer> getRandomNumbers() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        return computer;
+    }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        // 임의의 수 3가지
+        List<Integer> randomNumbers;
+
+        // 게임 시작 문구 출력
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        // 임의의 수 3가지 부여
+        randomNumbers = getRandomNumbers();
     }
 }
