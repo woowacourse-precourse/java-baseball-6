699f6c62ac5b354135370632f9a361f4757e3fbb
sengjun0624 <sengjun0624@naver.com>
2023-10-25 15:31:56 +0900

    feat(GameService): add playGame method

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -12,14 +12,23 @@ public class GameService {
     Parser parser = new Parser();
 
 
-    public void playGame() {
-
-    }
-
     public void setGame(int min, int max, int size) {
         this.size = size;
         game = new Game(RandomUtils.getRandomNumbers(min, max, size));
     }
+
+    public void playGame() {
+        int strike = 0;
+        while (strike != 3) {
+            play();
+            System.out.println(game.getBallCount() + game.getStrikeCount());
+            strike = game.getStrikeCount();
+        }
+    }
+
+    private void play() {
+    }
+
 
     private int[] getUserNumber() {
         System.out.println("숫자를 입력해주세요 : ");
