c63f8c1c5a8e3a1653264ba95204dadaeff86409
HaLoomin <103578343+HaLoomin@users.noreply.github.com>
2023-10-25 15:40:55 +0900

    숫자범위 오류 수정
    숫자 범위가 0~9로 되어있던 부분을 1~9로 수정하였습니다.

--- src/main/java/baseball/AppConfig.java
+++ src/main/java/baseball/AppConfig.java
@@ -15,9 +15,9 @@ public class AppConfig {
     //Hitter Setting
     public static final int DIGIT_NUM = 3;
     public static final int MAX_NUMBER = 9;
-    public static final int MIN_NUMBER = 0;
+    public static final int MIN_NUMBER = 1;
     public static final char MAX_CHAR = '9';
-    public static final char MIN_CHAR = '0';
+    public static final char MIN_CHAR = '1';
 
     public GameSetting gameSetting() { return new GameSettingImpl(); }
 
