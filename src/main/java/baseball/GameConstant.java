1ca04fd81356ffed8f85c48be0252c0f80532b34
sa02045 <sa02045@naver.com>
2023-10-25 23:18:19 +0900

    "feat 게임 상수"

---
+++ src/main/java/baseball/GameConstant.java
@@ -0,0 +1,16 @@
+package baseball;
+
+public final class GameConstant {
+
+    public final static String STRIKE = "스트라이크";
+    public final static String BALL = "볼";
+    public final static String NOTHING = "낫싱";
+    public final static int PLAY_INPUT_LENGTH = 3;
+    public final static String GAME_END_CONDITION = PLAY_INPUT_LENGTH + STRIKE;
+    public static final String START_STAGE_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public static final String PLAY_STAGE_MESSAGE = "숫자를 입력해주세요 : ";
+    public static final String END_STAGE_MESSAGE = "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String END_STAGE_MESSAGE2 = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String END_STAGE_RESTART = "1";
+    public static final String END_STAGE_GAME_OVER = "2";
+}
