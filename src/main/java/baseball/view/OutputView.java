b12ff75dd887db284bfb08c77c779530ef1ed58b
ddohyeong <dohyeong7026@gmail.com>
2023-10-25 21:04:06 +0900

    feat(GameController) : 게임이 끝나면 재시작 혹은 종료를 묻는 기능 구현
    OutputView : 게임이 끝나는 메시지 출력을 위한 기능 구현
    InputView : 게임의 재시작 혹은 종료의 여부를 묻는 입력 기능 구현
    docs : 게임이 끝나면 재시작 혹은 종료를 묻는 기능 구현 완료 체크

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,9 @@
+package baseball.view;
+
+public class OutputView {
+	public static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+	public void printEndGame() {
+		System.out.println(GAME_END_MESSAGE);
+	}
+}
