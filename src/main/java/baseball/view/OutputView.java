2cf20a99c907d59a6d85ce24b5a5812e36dc6c19
gundorit <packdev937@gmail.com>
2023-10-25 11:50:45 +0900

    feat (OutputView) : 게임 시작 문구를 출력한다

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,10 @@
+package baseball.view;
+
+public class OutputView {
+
+    public static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+
+    public static void printGameStart() {
+        System.out.println(GAME_START_MESSAGE);
+    }
+}
