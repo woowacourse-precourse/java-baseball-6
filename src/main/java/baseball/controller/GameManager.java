fc5054eef54c5297a7f3dd33803d6f377d0d9aea
K-KY <kyuyoungk@naver.com>
2023-10-25 14:59:56 +0900

    Refactor(GameManager) : 숫자 상수화, compareNumbers 메소드가 userNumbe 객체를 사용하도록 변경

--- src/main/java/baseball/controller/GameManager.java
+++ src/main/java/baseball/controller/GameManager.java
@@ -7,11 +7,15 @@ import baseball.domain.CompareNumber;
 import baseball.domain.DataTypeChanger;
 import baseball.domain.GameResult;
 import baseball.domain.UserInput;
+import baseball.domain.UserNumber;
 import baseball.domain.Validator;
 import baseball.view.OutputView;
 import java.util.List;
 
 public class GameManager {
+    public static final int MAX_SCORE = 3;
+    public static final int RESTART = 1;
+    public static final BallScore STRIKE = BallScore.STRIKE;
     private BaseBalls generatedNumber;
 
     public GameManager() {
@@ -36,12 +40,13 @@ public class GameManager {
     }
 
     private boolean compareNumbers(List<CompareNumber> userBalls) {
-        GameResult compare = generatedNumber.compare(userBalls);
+        UserNumber userNumber = new UserNumber(userBalls);
+        GameResult compare = generatedNumber.compare(userNumber);
         return isEnd(compare);
     }
 
     private boolean isEnd(GameResult compare) {
-        if (compare.getResult(BallScore.STRIKE) == 3) {
+        if (compare.getResult(STRIKE) == MAX_SCORE) {
             OutputView.printScore(compare);
             OutputView.printGameEnd();
             return false;
@@ -52,7 +57,7 @@ public class GameManager {
 
     private void endOrRestart() {
         int option = UserInput.endOrRestart(new Validator());
-        if (option == 1) {
+        if (option == RESTART) {
             startGame();
         }
     }
