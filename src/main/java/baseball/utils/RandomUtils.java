da416dfb09ee4f55cb1fc1a92e0526ae14a79cfe
edjjincode <138580800+edjjincode@users.noreply.github.com>
2023-10-25 12:49:45 +0900

    feat(apply): surplus get RandomNumbers method
    - get random numbers that are different to each other
    - create numberList to use contains method
    - use camp.nextstep.edu.missionutils.random
    - use random.pickNumberInRange

--- src/main/java/baseball/utils/RandomUtils.java
+++ src/main/java/baseball/utils/RandomUtils.java
@@ -2,19 +2,37 @@ package baseball.utils;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RandomUtils {
 
     public static int[] getRandomNumbers(int size, int start, int end) {
-        int[] numbers = new int[3];
+        List<Integer> numbersList = new ArrayList<>();
 
         for (int i = 0; i < size; i++) {
-            numbers[i] = getUniqueRandomNumber(numbers, start, end, i);
+            numbersList.add(getUniqueRandomNumber(numbersList, start, end));
         }
+
+        int[] numbers = new int[size];
+        for (int i = 0; i < size; i++) {
+            numbers[i] = numbersList.get(i);
+        }
+
         return numbers;
     }
 
-    private static int getUniqueRandomNumber(int[] numbers, int start, int end, int i) {
+    private static int getUniqueRandomNumber(List<Integer> numbers, int start, int end) {
         int randomNumber = Randoms.pickNumberInRange(start, end);
-        /** numbers 배열안에 있는 숫자와 서로 다른 숫자가 될 수 있도록 램덤하게 숫자를 뽑는다**/
+        while (!isUnique(numbers, randomNumber)) {
+            randomNumber = Randoms.pickNumberInRange(start, end);
+        }
+        return randomNumber;
+    }
+
+    private static Boolean isUnique(List<Integer> numbers, int randomNumber) {
+        return !numbers.contains(randomNumber);
     }
 }
+
+
