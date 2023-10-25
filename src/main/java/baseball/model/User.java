7df18716a1fff922195e183d43af123a549805d1
KongJunWoo <joonoo3@naver.com>
2023-10-25 16:54:06 +0900

    feat : User가 입력한 난수 저장
    - 현재 userNum에 있는 값 삭제하는 dataInit
    - 입력받은 숫자를 입력받은 숫자를 userNum에 저장하는 convertInput
    - userNum을 반환하는 getUserNum

--- src/main/java/baseball/model/User.java
+++ src/main/java/baseball/model/User.java
@@ -17,4 +17,24 @@ public class User {
         return new User();
     }
 
+    //userNum 초기화
+    public void dataInit(){
+        userNum.clear();
+    }
+
+    //입력받은 숫자를 userNum에 저장
+    public void convertInput(String input){
+        dataInit();
+
+        for (int i = 0; i < 3; i++) {
+            userNum.add(input.charAt(i) - '0');
+        }
+
+    }
+
+    //userNum 반환
+    public List<Integer> getUserNum(){
+        return userNum;
+    }
+
 }
