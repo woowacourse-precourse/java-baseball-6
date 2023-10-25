0314441667e08b5260dd7eac30c27a82b59faab7
DryLeonhard <ygangmin@gmail.com>
2023-10-25 19:51:22 +0900

    Refactor : 미사용 메서드 제거

--- src/main/java/baseball/ball/Ball.java
+++ src/main/java/baseball/ball/Ball.java
@@ -3,6 +3,7 @@ package baseball.ball;
 import baseball.random.EasyPick;
 import baseball.random.Pick;
 import baseball.random.RandomUtil;
+
 import java.util.List;
 
 public class Ball {
@@ -10,43 +11,39 @@ public class Ball {
 
     private final RandomUtil randomUtil = Pick.getInstance();
 
-    private Ball(){
+    private Ball() {
 
     }
 
-    public static Ball getInstance(){
+    public static Ball getInstance() {
         return instance;
     }
 
     private List<Integer> target;
 
-    public List<Integer> updateTarget(){
+    public List<Integer> updateTarget() {
         target = null;
 
         return target = randomUtil.getBallNumber();
     }
 
-    public List<Integer> getTarget(){
-        return target;
-    }
-
-    public Result checkNumber(int num){
+    public Result checkNumber(int num) {
         int nBall = 0, nStrike = 0;
 
         char[] arr = String.valueOf(num).toCharArray();
-        if(arr.length!=3) throw new IllegalArgumentException();
+        if (arr.length != 3) throw new IllegalArgumentException();
 
-        for(int i=0; i<3; i++){
-            int temp = arr[i]-48;
-            if(target.contains(temp)){
-                if(target.indexOf(temp)==i){
-                    nStrike ++;
-                }else{
-                    nBall ++;
+        for (int i = 0; i < 3; i++) {
+            int temp = arr[i] - 48;
+            if (target.contains(temp)) {
+                if (target.indexOf(temp) == i) {
+                    nStrike++;
+                } else {
+                    nBall++;
                 }
             }
         }
-        return new Result(nBall,nStrike);
+        return new Result(nBall, nStrike);
     }
 
 }
--- src/main/java/baseball/ball/Result.java
+++ src/main/java/baseball/ball/Result.java
@@ -4,38 +4,22 @@ public class Result {
     private int nBall;
     private int nStrike;
 
-    public int getnBall() {
-        return nBall;
-    }
-
-    public void setnBall(int nBall) {
-        this.nBall = nBall;
-    }
-
-    public int getnStrike() {
-        return nStrike;
-    }
-
-    public void setnStrike(int nStrike) {
-        this.nStrike = nStrike;
-    }
-
     public Result(int nBall, int nStrike) {
         this.nBall = nBall;
         this.nStrike = nStrike;
     }
 
-    public boolean chkResult(Result result){
+    public boolean chkResult(Result result) {
         return result.nStrike == 3;
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        if(nBall==0&&nStrike==0) return "낫싱";
-        if(nBall>0) sb.append(nBall).append("볼");
-        if(nBall>0&&nStrike>0) sb.append(" ");
-        if(nStrike>0) sb.append(nStrike).append("스트라이크");
+        if (nBall == 0 && nStrike == 0) return "낫싱";
+        if (nBall > 0) sb.append(nBall).append("볼");
+        if (nBall > 0 && nStrike > 0) sb.append(" ");
+        if (nStrike > 0) sb.append(nStrike).append("스트라이크");
         return sb.toString();
     }
 }
