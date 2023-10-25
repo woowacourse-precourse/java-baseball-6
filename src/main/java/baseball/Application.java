5458a833e988fb33a0294d7eee778363d6b2d86b
seung-won-kim <hesjs5@skuniv.ac.kr>
2023-10-25 14:00:58 +0900

    Refactor: GameStatus 생성자 추가
    GameStatus에 매개변수가 있는 생성자를 추가해서 값을 초기화하도록 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,7 +3,8 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
 
-        Game game = new Game(new GameStatus());
+        Game game = new Game(new GameStatus(Status.PLAY,
+            Computer.createComputerNumbers(new BaseballNumberCreator())));
         game.start();
     }
 }
--- src/main/java/baseball/GameStatus.java
+++ src/main/java/baseball/GameStatus.java
@@ -5,13 +5,14 @@ public class GameStatus {
     private Status status;
     private Baseballs computerNumbers;
 
-    public GameStatus() {
-        init();
+    public GameStatus(final Status status, final Baseballs computerNumbers) {
+        this.status = status;
+        this.computerNumbers = computerNumbers;
     }
 
     public void init() {
         this.status = Status.PLAY;
-        this.computerNumbers = new Computer(new BaseballNumberCreator()).createComputerNumbers();
+        this.computerNumbers = Computer.createComputerNumbers(new BaseballNumberCreator());
     }
 
     public boolean isPlaying() {
