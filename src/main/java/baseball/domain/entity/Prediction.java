4cd9ee04e42aeae408cdcc443f0d75f4efb4654f
mingeun0507 <tqdlfkg@naver.com>
2023-10-25 14:35:10 +0900

    feat: 사용자가 예측한 숫자를 담은 Prediction record 구현

---
+++ src/main/java/baseball/domain/entity/Prediction.java
@@ -0,0 +1,32 @@
+package baseball.domain.entity;
+
+import baseball.constants.ErrorConst;
+
+import java.util.List;
+
+public record Prediction(List<Integer> numberList) {
+
+    public Prediction {
+        validateIfNull(numberList);
+        validateIfOutOfRange(numberList);
+        validateIfInvalidLength(numberList);
+    }
+
+    private void validateIfNull(final List<Integer> numberList) {
+        if (numberList == null) {
+            throw new IllegalArgumentException(ErrorConst.NULL_INPUT.getMessage());
+        }
+    }
+
+    private void validateIfOutOfRange(final List<Integer> numberList) {
+        if (numberList.stream().anyMatch(number -> number < 1 || number > 9)) {
+            throw new IllegalArgumentException(ErrorConst.OUT_OF_BOUNDARY.getMessage());
+        }
+    }
+
+    private void validateIfInvalidLength(final List<Integer> numberList) {
+        if (numberList.size() != Answer.ANSWER_LENGTH) {
+            throw new IllegalArgumentException(ErrorConst.INVALID_LENGTH.getMessage());
+        }
+    }
+}
