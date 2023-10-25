d7acfe9d8facfc0183a42e5898f5c68f05c1984a
동원 <ai06021@gmail.com>
2023-10-25 22:04:19 +0900

    fix : input check 통합

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,19 +4,18 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        String input = Console.readLine();
         while (true) {
             System.out.println("숫자 야구 게임을 시작합니다");
+            String input = Console.readLine();
             Play play = new Play();
-            if (play.game(input) == -1) {
+            if (!play.inputCheck(input)) {
                 break;
             }
+            play.game(input);
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
             if (play.newGameCheck() != 1) {
                 break;
             }
         }
-
-
     }
 }
