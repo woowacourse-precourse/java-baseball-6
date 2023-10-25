3bd1b9e7d248d78534eff2b2b09736e61343ee47
GyungA <GyungA@users.noreply.github.com>
2023-10-25 13:47:32 +0900

    feat: 숫자 야구 게임

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameController baseBall = new GameController();
+        baseBall.run();
     }
 }
