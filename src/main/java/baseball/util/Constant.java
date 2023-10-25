a6b27dc06a4ba6565d2dc0bc7f3ceaa471ef59df
hansg931 <hansg0509@cau.ac.kr>
2023-10-25 19:03:56 +0900

    feat: 상수 추가

--- src/main/java/baseball/util/Constant.java
+++ src/main/java/baseball/util/Constant.java
@@ -4,4 +4,6 @@ public class Constant {
 	public static final int NUMBER_LENGTH = 3;
 	public static final int MIN_NUMBER = 1;
 	public static final int MAX_NUMBER = 9;
+	public static final int GAME_RESTART_FLAG = 1;
+	public static final int GAME_END_FLAG = 2;
 }
