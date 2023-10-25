0eba7a5f3238cfded8d4a24bbd250fb6fdfc1167
박상현 <shawnpark98@naver.com>
2023-10-25 12:49:53 +0900

    feat: GameResult 객체 생성

--- src/main/java/baseball/model/GameResult.java
+++ src/main/java/baseball/model/GameResult.java
@@ -1,4 +1,19 @@
 package baseball.model;
 
 public class GameResult {
+    private int strikeCount;
+    private int ballCount;
+
+    public GameResult(int strikeCount, int ballCount) {
+        this.strikeCount = strikeCount;
+        this.ballCount = ballCount;
+    }
+
+    public int getStrikeCount() {
+        return strikeCount;
+    }
+
+    public int getBallCount() {
+        return ballCount;
+    }
 }
