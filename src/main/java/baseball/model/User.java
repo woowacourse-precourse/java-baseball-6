005d2e4f4dd6a78a92f3496cf8c61c9ece71ebc5
KongJunWoo <joonoo3@naver.com>
2023-10-25 16:49:44 +0900

    feat : User 객체 생성 메소드
    - 생성자를 추가하고 List 초기화
    - 객체 생성을 위임 받을 수 있게 create메소드 작성

--- src/main/java/baseball/model/User.java
+++ src/main/java/baseball/model/User.java
@@ -1,9 +1,20 @@
 package baseball.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class User {
 
     private List<Integer> userNum; //사용자가 입력한 수를 저장할 배열
+
+    //생성자
+    private User(){
+        userNum = new ArrayList<>();
+    }
+
+    //생성자
+    public static User create(){
+        return new User();
+    }
 
 }
