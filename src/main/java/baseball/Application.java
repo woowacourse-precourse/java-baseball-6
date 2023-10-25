18082ef33324364dbe1a5467d96f254327ad0d6d
GyungA <GyungA@users.noreply.github.com>
2023-10-25 23:01:10 +0900

    refactor: GameController 객체와 메서드 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,69 +1,48 @@
 package baseball;
 
 import baseball.domain.Computer;
+import baseball.domain.GameController;
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.List;
 import baseball.domain.User;
+import static baseball.domain.GameController.NUMBER_LENGTH;
+
 public class Application {
-    public static final int NUMBER_LENGTH = 3;
-    public static final String STRIKE = "스트라이크";
-    public static final String BALL = "볼";
-    public static final String NOTHING = "낫싱";
+
     public static void main(String[] args) {
         User user = new User();
         Computer computer = new Computer();
+        GameController gameController = new GameController();
         int exit = 1;
         while (exit == 1) { // 외부 while문 시작
-            System.out.println("숫자 야구 게임을 시작합니다.");
+            gameController.printGameStartNotice();
             // 1부터 9까지 서로 다른 세 자리의 수 생성 (컴퓨터)
             ArrayList<Integer> computerNumArray = computer.generateComputerNum(NUMBER_LENGTH);
 
-            int strike = 0;
-            int ball = 0;
+            int strike, ball;
+            strike = gameController.setValueZero();
 
             while (strike < NUMBER_LENGTH) { // 내부 while문 시작
                 ArrayList<Integer> userNumArray = user.inputUserGuess(NUMBER_LENGTH);
 
-                // 같은 수가 같은 자리에 있으면 스트라이크, 다른 자리에 있으면 볼, 같은 수가 전혀 없으면 낫싱
-                strike = 0;
-                ball = 0;
-
-                for (int i = 0; i < userNumArray.size(); i++) {
-                    if (userNumArray.get(i) == computerNumArray.get(i)) {
-                        userNumArray.set(i, 0);
-                        strike++;
-                    }
-                }
-
-                for (int i : userNumArray) {
-                    if (computerNumArray.contains(i)) {
-                        ball++;
-                    }
-                }
-
                 // 같은 수가 같은 자리에 있으면 스트라이크++, 값 0으로 바꾸기. 같은 수가 다른 자리에 있으면 볼++, 스트링 추가. 둘 다 0이면 낫싱.
+                strike = gameController.setValueZero();
+                ball = gameController.setValueZero();
+
+                strike = gameController.checkForStrike(userNumArray, computerNumArray, strike);
+                ball = gameController.checkForBall(userNumArray, computerNumArray, ball);
+
                 String output = "";
-                if (ball > 0) {
-                    output += String.format("%d%s", ball, BALL);
-                }
-                if (strike > 0) {
-                    if (ball > 0) {
-                        output += " ";
-                    }
-                    output += String.format("%d%s", strike, STRIKE);
-                }
-                if ((ball == 0) && (strike == 0)) {
-                    output += String.format("%s", NOTHING);
-                }
-                System.out.println(computerNumArray);
-                System.out.println(output);
+                output = gameController.addBallToOutput(output, ball);
+                output = gameController.addStrikeToOutput(output, strike, ball);
+                output = gameController.addNothingToOutput(output, strike, ball);
+
+                gameController.printOutput(output);
             } // 내부 while문 끝
             // 3개의 숫자를 모두 맞히면 종료, 다시 시작하거나 완전히 종료
-            System.out.println(String.format("%d개의 숫자를 모두 맞히셨습니다! 게임 종료", NUMBER_LENGTH));
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            exit = Integer.parseInt(Console.readLine());
+            gameController.printGameSuccess();
+            gameController.printNewGameNotice();
+            exit = gameController.inputIsNewGame();
         } // 외부 while문 끝
     }
 }
--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,9 +1,6 @@
 package baseball.domain;
-
 import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.ArrayList;
-import java.util.List;
 
 public class Computer {
     public static ArrayList<Integer> generateComputerNum(int NUMBER_LENGTH) {
