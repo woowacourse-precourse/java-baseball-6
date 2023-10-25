e96b894c36fabd56853c8fcea6aaa655fd333ac4
realcold0 <realcold0@gmail.com>
2023-10-25 17:52:57 +0900

    refactor : 입력시 오류 처리 함수로 정리

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -11,17 +11,27 @@ public class InputView {
         playerAnswer.clear();
         System.out.println("숫자를 입력해주세요 : ");
         String input = Console.readLine();  //입력받기
+        inputException(input);
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (Character.isDigit(c)) {
+                playerAnswer.add(Character.getNumericValue(c));
+            }
+        }
+        return playerAnswer;
+    }
+
+    public void inputException(String input) {
         if (input.length() != 3) {
             throw new IllegalArgumentException("3자리 숫자를 입력하세요");
         }
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
-            if (Character.isDigit(c)) {
-                playerAnswer.add(Character.getNumericValue(c));
-            } else if (!Character.isDigit(c)) {
+            if (!Character.isDigit(c)) {
+                throw new IllegalArgumentException("1~9 사이의 숫자를입력하세요");
+            } else if (Character.isDigit(c) && Character.getNumericValue(c) == 0) {
                 throw new IllegalArgumentException("1~9 사이의 숫자를입력하세요");
             }
         }
-        return playerAnswer;
     }
 }
