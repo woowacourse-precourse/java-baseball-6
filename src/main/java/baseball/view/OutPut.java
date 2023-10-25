ba7387950958b27df85c178a26a902debce75420
beomsic <qjatjr29@khu.ac.kr>
2023-10-25 13:54:30 +0900

    :fire: remove : 사용하지 않는 클래스 삭제

--- src/main/java/baseball/view/OutPut.java
+++
@@ -1,9 +0,0 @@
-package baseball.view;
-
-import java.io.IOException;
-
-public interface OutPut {
-    void gameStart() throws IOException;
-    void print(String msg) throws IOException;
-    void close() throws IOException;
-}
