1619293acd105dd606322903835e8fa4bd671ada
BaeDongHyeon <sasd730@gmail.com>
2023-10-25 23:37:39 +0900

    TEST: 게임결과를 변환하는 테스트 코드 작성

---
+++ src/test/java/baseball/domain/BaseballResultTest.java
@@ -0,0 +1,23 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static baseball.domain.BaseballFlag.*;
+import static org.assertj.core.api.Assertions.*;
+
+class BaseballResultTest {
+
+    @Test
+    @DisplayName("BALL은 2개 STRIKE는 1개 NOTHING은 0개가 저장되어야 한다.")
+    void gameResult() {
+        assertThat(BaseballResult.generate(List.of(BALL, STRIKE, BALL))).isEqualTo(Map.of(
+                BALL, 2,
+                STRIKE, 1,
+                NOTHING, 0
+        ));
+    }
+}
