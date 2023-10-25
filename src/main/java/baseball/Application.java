44579e52df9a5f86adfe9d3f64ee60a1b68c779f
김라연 <rayeon@gimlayeon-ui-noteubug.local>
2023-10-25 22:15:40 +0900

    refactor: restart() 함수에 게임을 재시작할지 묻는 출력문 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -104,6 +104,7 @@ public class Application {
     }
 
     private static boolean restart() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String restartGameInput = Console.readLine();
 
         if (restartGameInput.equals("2"))
