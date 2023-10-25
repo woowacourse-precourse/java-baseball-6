0f20c42d1a4a00d4e65532bc12ff06325692ccf9
Chaeros <chpark6851@koreatech.ac.kr>
2023-10-25 19:10:52 +0900

    feat: 랜덤 생성 수 중복 검증 메서드 추가
    랜덤으로 생성한 수가 저장된 List에 중복된 숫자가 있는지 검증하는
    메서드를 추가했습니다.

--- src/main/java/baseball/domain/ComputerNumber.java
+++ src/main/java/baseball/domain/ComputerNumber.java
@@ -2,7 +2,9 @@ package baseball.domain;
 
 import baseball.constant.Constant;
 import baseball.constant.errormessage.ComputerInputError;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ComputerNumber {
 
@@ -14,6 +16,18 @@ public class ComputerNumber {
     private void validateSize(List<Integer> computerNumbers) {
         if (computerNumbers.size() != Constant.GAME_NUMBERS_SIZE) {
             throw new IllegalArgumentException(ComputerInputError.COMPUTER_NUMBER_WRONG_INPUT_SIZE_MESSAGE);
+        }
+    }
+
+    /**
+     * 랜덤으로 생성한 숫자 배열에 중복되는 수가 있는 경우 예외 발생
+     *
+     * @param computerNumbers : 랜덤으로 생성한 컴퓨터가 가진 수
+     */
+    private void validateDuplicate(List<Integer> computerNumbers) {
+        Set<Integer> nonDuplicateNumbers = new HashSet<>(computerNumbers);
+        if (nonDuplicateNumbers.size() != Constant.GAME_NUMBERS_SIZE) {
+            throw new IllegalArgumentException(ComputerInputError.COMPUTER_NUMBER_WRONG_INPUT_DUPLICATE_MESSAGE);
         }
     }
 }
