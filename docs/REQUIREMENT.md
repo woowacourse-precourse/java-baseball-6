57fd8d31f76eec96c6a74dbab1f5facf23f80285
신영한 <syhan7516@naver.com>
2023-10-25 23:04:20 +0900

    feat(Application.java) : 사용자 숫자 입력
    (완료) 숫자 자리 개수 : 3자리
    (완료) 예외 처리 여부 : O
    
    사용자 입력 Console API를 사용하여 구현 완료

--- docs/REQUIREMENT.md
+++ docs/REQUIREMENT.md
@@ -3,7 +3,7 @@
 
 ## ✏️ 프로그래밍 요구 사항
 - (완료) JDK 17 버전 확인
-- 프로그램 실행의 시작점은 Application의 main()에서 수행
+- (완료) 프로그램 실행의 시작점은 Application의 main()에서 수행
 - build.gradle 파일을 변경할 수 없고, 외부 라이브러리를 사용 불가
 - Java 코드 컨벤션 가이드를 준수하며 프로그래밍 수행
 - 프로그램 종료 시 System.exit()를 호출 불가
@@ -11,7 +11,7 @@
 - 프로그래밍 요구 사항에서 달리 명시하지 않는 한 파일, 패키지 이름을 수정하거나 이동 불가
 - 라이브러리는 camp.nextstep.edu.missionutils에서 제공하는 Randoms 및 Console API를 사용하여 구현
 - (완료) Random 값 추출은 camp.nextstep.edu.missionutils.Randoms의 pickNumberInRange()를 활용 
-- 사용자가 입력하는 값은 camp.nextstep.edu.missionutils.Console의 readLine()을 활용
+- (완료) 사용자가 입력하는 값은 camp.nextstep.edu.missionutils.Console의 readLine()을 활용
 
 ## ✏️ 과제 진행 요구 사항
 - (완료) 미션은 java-baseball-6 저장소를 Fork & Clone해 시작
--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -23,6 +24,8 @@ public class Application {
 
         // 임의의 수 3가지
         List<Integer> randomNumbers;
+        // 사용자가 입력한 숫자
+        int guessNumbers;
 
         // 게임 시작 문구 출력
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -31,6 +34,14 @@ public class Application {
         randomNumbers = getRandomNumbers();
 
         // 숫자 입력 문구 출력
-        System.out.println("숫자를 입력해주세요 : ");
+        System.out.print("숫자를 입력해주세요 : ");
+
+        // 사용자 숫자 입력
+        try {
+            guessNumbers = Integer.parseInt(Console.readLine());
+        }
+        catch (IllegalArgumentException e) {
+            return;
+        }
     }
 }
