beb448f2831de8bc59fdf21090d7630839a55fa8
strongcookdas <goodjav141592@gmail.com>
2023-10-25 16:47:10 +0900

    feat : 램덤 3자리 숫자 생성 함수 구현 camp.nextstep.edu.missionutils.Randoms의 pickNumberInRange 사용

--- src/main/java/baseball/service/Computer.java
+++ src/main/java/baseball/service/Computer.java
@@ -1,4 +1,22 @@
 package baseball.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Computer {
+
+    private final int MAX_NUMBER = 9;
+    private final int MIN_NUMBER = 1;
+
+    public List<Integer> generateNumber() {
+        List<Integer> randomNumbers = new ArrayList<>();
+        while (randomNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!randomNumbers.contains(randomNumber)) {
+                randomNumbers.add(randomNumber);
+            }
+        }
+        return randomNumbers;
+    }
 }
