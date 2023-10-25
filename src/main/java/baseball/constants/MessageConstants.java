853b8d0c7f33a61065557e646ffbf1f49915df04
dhl1924 <dhl1924@naver.com>
2023-10-25 21:55:18 +0900

    feat: Message를 상수로 관리하는 클래스 추가

---
+++ src/main/java/baseball/constants/MessageConstants.java
@@ -0,0 +1,12 @@
+package baseball.constants;
+
+public class MessageConstants {
+    public static final String GAME_START = "숫자 야구 게임을 시작합니다.";
+    public static final String INPUT_NUMBER = "숫자를 입력해주세요 : ";
+    public static final String STRIKE = "스트라이크";
+    public static final String BALL = "볼";
+    public static final String NOTHING = "낫싱";
+    public static final String THREE_STRIKES = "3스트라이크";
+    public static final String GAME_OVER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String RESTART_OR_QUIT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+}
