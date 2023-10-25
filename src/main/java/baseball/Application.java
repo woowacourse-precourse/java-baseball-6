b6cdfb6ebeaf2a9d924d37f963ff58986b561c79
HoyiTT <pjs9177@naver.com>
2023-10-25 12:01:02 +0900

    결과 출력 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -36,7 +36,7 @@ public class Application {
 
             System.out.println(strike);
             if (strike.equals("3스트라이크")){
-                System.out.println("종료");
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 String reGame  = Console.readLine();
                 if(reGame.equals("1")){
