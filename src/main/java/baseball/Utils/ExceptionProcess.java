72057ee2098fb829987130fea043ae321a36e69c
honeydanji <tjdwls3680@naver.com>
2023-10-25 14:03:36 +0900

    REFACTOR(ExceptionProcess) : 사용자 입력 예외처리 코드 수정 및 추가
    - 기능 구현 목록에서 언급한 4가지 예외처리 작성완료
    - allUserException 메서드를 이용해서 4가지 예외 상황에 대해서 효율적으로 수행
    - 각 메서드의 "static" 삭제 >> 객체 지향 설계 원칙에 따름(OPP)

--- src/main/java/baseball/Utils/ExceptionProcess.java
+++ src/main/java/baseball/Utils/ExceptionProcess.java
@@ -1,28 +1,52 @@
 package baseball.Utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExceptionProcess {
     // 사용자 입력 예외처리
+    public void allUserException(String input) {
+        inputLengthExceededException(input); // 입력값 길이 초과
+        nonNumericInputException(input);
+        duplicateNumberException(input); // 입력값 중복
+        invalidNumberException(input); // 입력값 1 ~ 9 해당
+    }
 
     // 1. 숫자가 아닌 문자를 입력한 경우
-    public static void nonNumericInputException() {
-        throw new IllegalArgumentException("숫자를 입력해 주세요.");
+    public void nonNumericInputException(String input) {
+        for(char c : input.toCharArray()) {
+            if (!Character.isDigit(c)) throw new IllegalArgumentException("숫자를 입력해 주세요.");
+        }
     }
 
     // 2. 숫자를 3개 초과로 입력한 경우
-    public static void inputLengthExceededException() {
-        throw new IllegalArgumentException("숫자는 3개만 입력해 주세요.");
+    public void inputLengthExceededException(String input) {
+        if (input.length() > 3){
+            throw new IllegalArgumentException("숫자는 3개만 입력해 주세요.");
+        }
     }
 
     // 3. 중복이 존재하는 경우
-    public static void duplicateNumberException() {
-        throw new IllegalArgumentException("숫자 중복은 허용되지 않습니다.");
+    public void duplicateNumberException(String input) {
+        List<Integer> user = new ArrayList<>();
+        String[] userInput = input.split("");
+        for (int i = 0; i < 3; i++){
+            int num = Integer.parseInt(userInput[i]);
+            if (user.contains(num)) {
+                throw new IllegalArgumentException("숫자 중복은 허용되지 않습니다.");
+            }
+            user.add(num);
+        }
     }
 
     // 4. 1 ~ 9에 해당하지 않는 수가 있는 경우
-    public static void invalidNumberException() {
-        throw new IllegalArgumentException("1부터 9까지의 숫자만 입력해 주세요. ");
+    public void invalidNumberException(String input) {
+        String[] userInput = input.split("");
+        for (int i = 0; i < 3; i++){
+            int num = Integer.parseInt(userInput[i]);
+            if (num < 1 || num > 9) throw new IllegalArgumentException("1부터 9까지의 숫자만 입력해 주세요. ");
+        }
     }
-
 
     // 재시작/종료 여부를 입력할 때
 
