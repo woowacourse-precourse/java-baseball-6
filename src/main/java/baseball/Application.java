09ec7e937393098a2251bbd382f9654f4abb3dc0
Elandland <68230815+Elandland@users.noreply.github.com>
2023-10-25 22:43:27 +0900

    Fix : Test code 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -16,7 +16,7 @@ public class Application {
         System.out.println("숫자 야구 게임을 시작합니다.");         //처음에 시작할때 한번만
         do {                                                    //do while을 통해 무조건 한 번은 실행
             List<Integer> ans = answer.RandomAns();                  //랜덤 값 생성(Answer) 답은 ans에 List형식으로 저장
-            System.out.println(ans);
+
             while(!output.equals("3스트라이크")) {           //정답을 찾울때 까지 실행
                 List<Integer> inputnum = input.inputnum();               //입력값 받기(Input) 입력값은 inputnum에 List형식으로 저장
                 output = result.result(ans,inputnum);
