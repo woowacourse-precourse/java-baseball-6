22963812100723c6ab76128e92e3998d767d83ef
yeahdy <yejine98@naver.com>
2023-10-25 18:25:58 +0900

    feat: Controller 로직구현, Service 역할 위임

--- src/main/java/service/BaseBallGameService.java
+++ src/main/java/service/BaseBallGameService.java
@@ -1,4 +1,28 @@
 package service;
 
+import model.Computer;
+import model.Game;
+import model.User;
+import view.BaseballGameView;
+
 public class BaseBallGameService {
+
+    private BaseballGameView baseballGameView;
+    private Game game;
+    private User user;
+    private Computer computer;
+    public BaseBallGameService(BaseballGameView baseballGameView, Game game, User user, Computer computer){
+        this.baseballGameView = baseballGameView;
+        this.game = game;
+        this.user = user;
+        this.computer = computer;
+
+    }
+
+    public BaseBallGameService(){}
+
+    public void runGame(){
+
+    }
+
 }
