5109ca6dcdc1533431763afa003a479e3d97580b
yooooonshine <hl3470@naver.com>
2023-10-25 13:27:07 +0900

    Remove: 안쓰는 코드 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,22 +10,6 @@ import baseball.user.UserImpl;
 
 public class Application {
     public static void main(String[] args) {
-//        System.out.println("숫자 야구 게임을 시작합니다.");
-//
-//        List<Integer> computerNumList;
-//        List<Integer> userNumList;
-//        int[] resultArray;
-//
-//        do {
-//            computerNumList = generateComputerNumber();
-//            do {
-//                userNumList = receiveUserNumber();
-//                resultArray = compare(userNumList, computerNumList);
-//                printResult(resultArray);
-//
-//            } while (!checkResult(resultArray));
-//
-//        } while (askForRegame());
 
         User user = new UserImpl();
         Game game = new NumberBaseballGameImpl();
