7e7c6726361cfbe884accc27770ca3b48583a7c3
Jmmk <jmk525@gmail.com>
2023-10-25 21:58:20 +0900

    feat: 게임을 진행할 게임 클래스 구현

---
+++ src/main/java/baseball/domain/Game.java
@@ -0,0 +1,10 @@
+package baseball.domain;
+
+public class Game {
+    public static final int NUM_DIGITS = 3;
+    private Computer computer;
+    private Player player;
+    private boolean isGameOver;
+
+
+}
