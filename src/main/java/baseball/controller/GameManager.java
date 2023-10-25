f868083e29447d46832e532be394aec3b8f20911
“sangjun121” <juncho12011201@gmail.com>
2023-10-25 20:39:44 +0900

    feat: 올바른 정답 입력시 까지 사용자 입력 반복하도록 반복문 구현
    반복문의 실행 조건은 gameResultDto의 isCorrectNumber이 false일때만 반복하도록 설정

--- src/main/java/baseball/controller/GameManager.java
+++ src/main/java/baseball/controller/GameManager.java
@@ -17,20 +17,29 @@ public class GameManager {
     private UserNumberDto userNumberDto;
     private GameResultDto gameResultDto;
 
+    private boolean isCorrectNumber;
+
     //생성자
     public GameManager(){
         computer = new Computer();
         user = new User();
         userView = new UserView();
         judgement = new Judgement();
+
+        isCorrectNumber = false;
     }
 
     public void startGame(){
         computerNumberDto = computer.getRandomComputerNumber(); //랜덤한 3개 컴퓨터 숫자 생성
         userView.printGameStartMessage(); //게임시작 메시지 출력
-        userView.printNumberInputPrompt(); //user input 프롬포트 메시지 출력
-        userNumberDto = user.readUserNumberInput(); //user input 시행
-        gameResultDto = judgement.compareNumbers(computerNumberDto,userNumberDto); //컴퓨터 숫자와 사용자 입력 숫자 비교
-        userView.printResultOfJudgement(gameResultDto); //게임 결과 값 출력
+
+        while(!isCorrectNumber){ //정답을 맞출 때까지 입력 반복
+            userView.printNumberInputPrompt(); //user input 프롬포트 메시지 출력
+            userNumberDto = user.readUserNumberInput(); //user input 시행
+            gameResultDto = judgement.compareNumbers(computerNumberDto,userNumberDto); //컴퓨터 숫자와 사용자 입력 숫자 비교
+            userView.printResultOfJudgement(gameResultDto); //게임 결과 값 출력
+            isCorrectNumber = gameResultDto.getIsCorrectNumber(); //정답여부 확인
+        }
+
     }
 }
