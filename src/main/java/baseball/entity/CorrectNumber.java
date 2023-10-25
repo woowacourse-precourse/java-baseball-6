7212f3d7f996198829e2cc077ca32b5c6794aa4f
Jeonwoojae <dnwo0719@icloud.com>
2023-10-26 02:55:34 +0900

    refactor: 입력과 정답을 비교하는 comapreToInput 내부 로직 모듈화
    - 파싱, 스트라이크, 볼 계산 로직을 각각 메소드로 분리

--- src/main/java/baseball/entity/CorrectNumber.java
+++ src/main/java/baseball/entity/CorrectNumber.java
@@ -1,33 +1,55 @@
 package baseball.entity;
 
 import baseball.util.RandomNumGenerator;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 // CorrectNumber는 정답 생성, 관리, 비교
 public class CorrectNumber {
 
     private List<Integer> correctNumbers;
 
-    public Result compareToInput(String input) {
-        int strikeNumber = 0;
-        int ballNumber = 0;
-        String[] parsedString = input.split("");
-        for (int i = 0; i < input.length(); i++) {
-            int number = Integer.parseInt(parsedString[i]);
-            if (correctNumbers.contains(number)) {
-                if (number == correctNumbers.get(i)) {
-                    strikeNumber++;
-                } else {
-                    ballNumber++;
-                }
-            }
-        }
-        return new Result(strikeNumber, ballNumber);
-    }
-
     public void generateNewCorrectNum() {
         this.correctNumbers = RandomNumGenerator.generate();
     }
+
+    public Result compareToInput(String input) {
+        List<Integer> inputNumbers = parseInput(input);
+        int strikeCount = countStrikes(inputNumbers);
+        int ballCount = countBalls(inputNumbers);
+
+        return new Result(strikeCount, ballCount);
+    }
+
+    private List<Integer> parseInput(String input) {
+        return Arrays.stream(input.split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    private int countStrikes(List<Integer> inputNumbers) {
+        int strikeCount = 0;
+        for (int i = 0; i < inputNumbers.size(); i++) {
+            int inputNumber = inputNumbers.get(i);
+            if (inputNumber == correctNumbers.get(i)) {
+                strikeCount++;
+            }
+        }
+        return strikeCount;
+    }
+
+    private int countBalls(List<Integer> inputNumbers) {
+        int ballCount = 0;
+        for (int i = 0; i < inputNumbers.size(); i++) {
+            int inputNumber = inputNumbers.get(i);
+            if (correctNumbers.contains(inputNumber) && inputNumber != correctNumbers.get(i)) {
+                ballCount++;
+            }
+        }
+        return ballCount;
+    }
+
 
     public List<Integer> getCorrectNumbers() {
         return correctNumbers;
