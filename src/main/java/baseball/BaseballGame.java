56aee7f04869075d7ea6c971e3230d24c6dabbda
DavinAn <blueapple1030@gmail.com>
2023-10-25 23:29:02 +0900

    feat(baseballGame) : evaluate 메서드로 스트라이크와 볼 갯수를 셈

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,10 +1,23 @@
 package baseball;
 
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 랜덤 숫자와 사용자 숫자를 비교
  */
-
 public class BaseballGame {
 
+  public List<Integer> evaluate(List<Integer> computer, List<Integer> player) {
+    int strike = 0;
+    int ball = 0;
+    for (int i = 0; i < 3; i++) {
+      if (player.get(i).equals(computer.get(i))) {
+        strike++;
+      } else if (computer.contains(player.get(i))) {
+        ball++;
+      }
+    }
+    return Arrays.asList(strike, ball);
+  }
 }
