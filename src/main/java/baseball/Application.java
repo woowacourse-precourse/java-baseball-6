4adfc1c7ec8eb308f35afc90e85007b79de5f3b4
vega99c <vega99c@naver.com>
2023-10-25 16:26:33 +0900

    상수 추가, 일부 불필요 로직 변경
    - 승리 후 재시작/종료에 대한 반환값을 숫자로 보내는 건 직관적이지 못해서 변경
    - 삼항 연산자 부분을 조건문으로 변경
    - 생성 할 숫자 및 유저가 입력해야 하는 숫자 자릿수를 상수화
    - judgeGameResult 함수내에서 else문으로 빠져있는 예외처리를 밖으로 빼냄
    - 정수외의 데이터 입력시 예외처리 부분 문구 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,13 +8,15 @@ import java.util.List;
 import java.util.Set;
 
 public class Application {
+    static final int SIZE_REQUIRE_NUMBER = 3;
     static final int MAINTAIN_GAME = 0;
     static final int RESTART_GAME = 1;
+    static final int EXIT_GAME = 2;
 
     public static List<Integer> makeRandomNumberList() {
         List<Integer> computer = new ArrayList<>();
         int randomNumber = 0;
-        while (computer.size() < 3) {
+        while (computer.size() < SIZE_REQUIRE_NUMBER) {
             randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
@@ -32,7 +34,7 @@ public class Application {
         try {
             userInputNumber = Integer.parseInt(Console.readLine());
         } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("정수 3자리만 입력 해야 합니다.");
+            throw new IllegalArgumentException("정수만 입력 해야 합니다.");
         }
 
         stringPlayerNumber = Integer.toString(userInputNumber);
@@ -41,7 +43,7 @@ public class Application {
         for (int i = 0; i < stringPlayerNumber.length(); i++) {
             arrayPlayerNumber[i] = stringPlayerNumber.charAt(i) - '0';
         }
-        if (arrayPlayerNumber.length != 3) {
+        if (arrayPlayerNumber.length != SIZE_REQUIRE_NUMBER) {
             throw new IllegalArgumentException("3자리의 정수가 입력 되지 않았습니다.");
         }
         if (!checkDuplicateInput(arrayPlayerNumber)) {
@@ -65,7 +67,7 @@ public class Application {
 
     public static int getHintForAnswer(int[] arrayPlayerNumber, List<Integer> listAnswerNumber) {
         int numberOfStrikes = 0;
-        int numOfBalls = 0;
+        int numberOfBalls = 0;
 
         for (int i = 0; i < listAnswerNumber.size(); i++) {
             if (listAnswerNumber.get(i) == arrayPlayerNumber[i]) {
@@ -73,19 +75,24 @@ public class Application {
                 continue;
             }
             if (listAnswerNumber.contains(arrayPlayerNumber[i])) {
-                numOfBalls++;
+                numberOfBalls++;
             }
         }
 
-        System.out.printf((numOfBalls != 0) ? "%d볼 " : "", numOfBalls);
-        System.out.printf((numberOfStrikes != 0) ? "%d스트라이크" : "", numberOfStrikes);
-
-        if (numOfBalls != 0 || numberOfStrikes != 0) {
+        if ((numberOfBalls == 0) && (numberOfStrikes == 0)) {
             System.out.println();
-        }
-        if ((numOfBalls == 0) && (numberOfStrikes == 0)) {
             System.out.println("낫싱");
+            return MAINTAIN_GAME;
         }
+
+        if(numberOfBalls != 0) {
+            System.out.printf("%d볼 ", numberOfBalls);
+        }
+
+        if(numberOfStrikes != 0) {
+            System.out.printf("%d스트라이크", numberOfStrikes);
+        }
+        System.out.println();
 
         return numberOfStrikes;
     }
@@ -99,17 +106,16 @@ public class Application {
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             userInputNumber = Integer.parseInt(Console.readLine());
 
-            if (userInputNumber == 1) {
-                result = 1;
-            } else if (userInputNumber == 2) {
+            if (userInputNumber == RESTART_GAME) {
+                return RESTART_GAME;
+            } else if (userInputNumber == EXIT_GAME) {
                 System.out.println("게임 종료");
-                result = 2;
-            } else {
-                throw new IllegalArgumentException("1 또는 2를 선택하지 않았습니다.");
+                return EXIT_GAME;
             }
+            throw new IllegalArgumentException("1 또는 2를 선택하지 않았습니다.");
         }
 
-        return result;
+        return MAINTAIN_GAME;
     }
 
     public static void main(String[] args) {
@@ -119,7 +125,7 @@ public class Application {
         int numberOfStrikes = 0;
 
         System.out.println("숫자 야구 게임을 시작합니다. ");
-        while ((gameLoopSign == MAINTAIN_GAME) || (gameLoopSign == RESTART_GAME)) {
+        while (gameLoopSign != EXIT_GAME) {
             if (gameLoopSign == RESTART_GAME) {
                 listAnswerNumber = makeRandomNumberList();
             }
