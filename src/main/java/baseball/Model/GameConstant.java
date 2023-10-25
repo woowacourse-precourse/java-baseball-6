ff106ac0e5c6d5fb8f0d61fafc05ba4608f5ca89
shinel98 <75024473+shinel98@users.noreply.github.com>
2023-10-25 13:52:00 +0900

    fix(GameConstant): fix the value of QUIT

--- src/main/java/baseball/Model/GameConstant.java
+++ src/main/java/baseball/Model/GameConstant.java
@@ -3,5 +3,5 @@ package baseball.Model;
 public final class GameConstant {
     public static final int MAX_NUM = 3;
     public static final String RESTART = "1";
-    public static final String QUIT = "0";
+    public static final String QUIT = "2";
 }
