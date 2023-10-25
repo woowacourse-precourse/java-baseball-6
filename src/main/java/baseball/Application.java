291180ab30ced56b123521e2594241eec935ebf1
Somin Park <ps4708@naver.com>
2023-10-25 18:33:49 +0900

    feat: Application 클래스 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import baseball.controller.GameController;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.text.ParseException;
@@ -8,6 +9,7 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-
+        GameController gameController = new GameController();
+        gameController.gameStart();
     }
 }
