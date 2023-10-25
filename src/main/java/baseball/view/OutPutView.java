3904cca0b590689e15e1096e60499394fdaf72ba
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-10-25 18:13:21 +0900

    refactor: OutPutView Class 수정
    - Strike와 Ball 개수를 출력하는 함수 추가
    - 기존의 바로 출력하던 부분을 displayMessage 함수를 통해 출력하도록 변경

--- src/main/java/baseball/view/OutPutView.java
+++ src/main/java/baseball/view/OutPutView.java
@@ -3,17 +3,51 @@ package baseball.view;
 public class OutPutView {
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String GAME_SET_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String RESTART_OR_END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String NOTHING_MATCH = "낫싱";
+    private static final String ONLY_BALL = "볼";
+    private static final String BALL = "볼 ";
+    private static final String STRIKE = "스트라이크";
 
     public static void displayGameStartMessage() {
-        System.out.println(GAME_START_MESSAGE);
+        displayMessage(GAME_START_MESSAGE);
     }
 
     public static void displayInputNumberMessage() {
-        System.out.print(INPUT_NUMBER_MESSAGE);
+        displayMessage(INPUT_NUMBER_MESSAGE);
     }
 
     public static void displayRestartOrExitMessage() {
-        System.out.println(RESTART_OR_END_MESSAGE);
+        displayMessage(RESTART_OR_END_MESSAGE);
+    }
+
+    public static void displayGameSetMessage() {
+        displayMessage(GAME_SET_MESSAGE);
+    }
+
+    public static void displayNothingMatch(int strike, int ball) {
+        if (strike == 0 && ball == 0) {
+            displayMessage(NOTHING_MATCH);
+        }
+    }
+
+    public static void displayBall(int strike, int ball) {
+        if (strike == 0 && ball != 0) {
+            displayMessage(ball + ONLY_BALL);
+        }
+        if (strike != 0 && ball != 0) {
+            displayMessage(ball + BALL);
+        }
+    }
+
+    public static void displayStrike(int strike) {
+        if (strike != 0) {
+            displayMessage(strike + STRIKE);
+        }
+    }
+
+    private static void displayMessage(String message) {
+        System.out.println(message);
     }
 }
