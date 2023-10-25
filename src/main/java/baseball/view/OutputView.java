94a6e1e40cb7c49fc814dea5e928cc615771abf1
lesw <lesw1216@gmail.com>
2023-10-25 09:21:06 +0900

    style: delete output message dot

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,7 +3,7 @@ package baseball.view;
 public class OutputView {
 
     private static final String NOTHING_MESSAGE = "낫싱";
-    private static final String SUCCESS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료.";
+    private static final String SUCCESS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String BALL_MESSAGE = "볼";
     private static final String STRIKE_MESSAGE = "스트라이크";
 
