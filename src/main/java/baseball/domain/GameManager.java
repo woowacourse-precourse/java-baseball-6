618703f458e28812c63bfece7f3a21ddeb9272f5
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-25 21:25:31 +0900

    [feat] : add strike check method

--- src/main/java/baseball/domain/GameManager.java
+++ src/main/java/baseball/domain/GameManager.java
@@ -8,7 +8,19 @@ public class GameManager {
 
     // Computer 가 선택한 임의의 3자리 숫자와 Player 가 입력한 3자리 숫자를 비교하는 메서드.
     public void check(Computer computer, Player player) {
-//        strikeCount = strikeCheck(computer, player);
+        strikeCount = strikeCheck(computer, player);
 //        ballCount = ballCheck(computer, player);
+    }
+
+    // Computer 가 선택한 임의의 3자리 숫자와 Player 가 입력한 3자리 숫자에서 스트라이크 카운터를 리턴하는 메서드.
+    private int strikeCheck(Computer computer, Player player) {
+        strikeCount = 0;
+
+        for (int i = 0; i < LENGTH; i++) {
+            if (computer.getRandomNumber().charAt(i) == player.getInputNumber().charAt(i)) {
+                strikeCount++;
+            }
+        }
+        return strikeCount;
     }
 }
