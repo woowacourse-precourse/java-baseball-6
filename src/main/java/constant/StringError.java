f6f813a1ec9e8bd1deaf5309e91892b215340521
Hyun <hyeonhc328@gmail.com>
2023-10-25 23:20:15 +0900

    커밋

---
+++ src/main/java/constant/StringError.java
@@ -0,0 +1,7 @@
+package constant;
+
+public class StringError {
+    public static String wrongInputError = "잘못된 입력입니다.";
+    public static String wrongLengthInputError = "잘못된 길이의 입력입니다.";
+    public static String wrongTypeInputError = "잘못된 타입의 입력입니다.";
+}
