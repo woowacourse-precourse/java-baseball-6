b4c649708a62bf7bca15630f70b80c6577308655
gundorit <packdev937@gmail.com>
2023-10-25 11:52:28 +0900

    feat (OutputView) : 게임 종료 문구를 출력한다

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,6 +4,8 @@ public class OutputView {
 
     public static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     public static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    public static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
 
     public static void printGameStart() {
         System.out.println(GAME_START_MESSAGE);
@@ -12,4 +14,9 @@ public class OutputView {
     public static void printInputNumber() {
         System.out.print(INPUT_NUMBER_MESSAGE);
     }
+
+    public static void printGameEnd() {
+        System.out.println(GAME_END_MESSAGE);
+    }
+
 }
