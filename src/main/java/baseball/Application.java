b98aeffc3bb410ea951f0cae84f148890c20c499
Walkers15 <qortmd7777@gmail.com>
2023-10-25 21:45:26 +0900

    style: 우테코 코딩스탠다드 적용

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,23 +1,25 @@
 package baseball;
+
 import camp.nextstep.edu.missionutils.*;
 import java.util.ArrayList;
 
 public class Application {
     static final int NUMBER_SIZE = 3;
+
     public static void main(String[] args) throws IllegalArgumentException {
         System.out.println("숫자 야구 게임을 시작합니다.");
         try {
-            while(true) {
+            while (true) {
                 // 3자리 숫자 만들기
                 ArrayList<Integer> answer = new ArrayList<>();
-                while(answer.size() < NUMBER_SIZE) {
+                while (answer.size() < NUMBER_SIZE) {
                     int randomNumber = Randoms.pickNumberInRange(1, 9);
                     if (!answer.contains(randomNumber)) {
                         answer.add(randomNumber);
                     }
                 }
 
-                while(true) {
+                while (true) {
                     System.out.print("숫자를 입력해주세요 : ");
                     // 사용자가 입력한 숫자를 정답과 비교하여 출력하기
                     int input = Integer.parseInt(Console.readLine());
@@ -30,7 +32,7 @@ public class Application {
                     // 입력 배열 생성 및 입력 유효성 검사 (서로 다른 세 자리의 수)
                     ArrayList<Integer> question = new ArrayList<>();
                     for (int i = NUMBER_SIZE - 1; i >= 0; i--) {
-                        int divider = (int)(Math.pow(10, i));
+                        int divider = (int) (Math.pow(10, i));
                         int number = input / divider % 10;
                         if (question.contains(number)) {
                             throw new IllegalArgumentException();
@@ -64,14 +66,14 @@ public class Application {
                     }
                     System.out.println(sb);
 
-                    if(strike == NUMBER_SIZE) {
+                    if (strike == NUMBER_SIZE) {
                         // 모든 숫자를 맞춰 게임이 종료됨
                         System.out.println(NUMBER_SIZE + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
                         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                         int process = Integer.parseInt(Console.readLine());
                         if (process == 1) {
                             break;
-                        } else if (process == 2){
+                        } else if (process == 2) {
                             return;
                         } else {
                             throw new IllegalArgumentException();
