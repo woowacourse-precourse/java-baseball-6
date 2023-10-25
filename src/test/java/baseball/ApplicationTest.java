b3a8df8cdd92e36d9a37861817ee5aa185987d37
c1ce <chalicenuclear@gmail.com>
2023-10-25 14:24:56 +0900

    test : 유저의 입력에 관한 예외 관련 test코드 작성

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,7 +1,13 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -9,6 +15,87 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
+    //바뀔수있는부분은 유저쪽이기에 테스트코드도 유저쪽을 작성해야할것같다
+    //입력길이,재시작,0확인,문자입력,중복숫자확인
+    @Test
+    @DisplayName("문자열에서 리스트로 변환 테스트")
+    void 입력한_문자열_리스트로_변환(){
+        //Given
+        User user = new User();
+
+        //When
+        String testNumber= "123";
+        List<Integer> testList = Arrays.asList(1,2,3);
+
+        //Then
+        Assertions.assertEquals(user.makeListNumber(testNumber),testList);
+    }
+    @Test
+    @DisplayName("입력한 숫자 중 중복 숫자 확인 테스트")
+    void 입력한_숫자_중복확인(){
+        //Given
+        User user = new User();
+
+        //Then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            //when
+            user.makeListNumber("112");
+        });
+
+
+    }
+    @Test
+    @DisplayName("입력한 숫자의 길이 확인 테스트")
+    void 입력한_숫자_길이확인(){
+        //Given
+        User user = new User();
+
+        //Then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            //when
+            user.makeListNumber("1234");
+        });
+
+    }
+    @Test
+    @DisplayName("입력한 숫자 중 0이 있는지 확인")
+    void 입력한_숫자중_0유무_확인(){
+        //Given
+        User user = new User();
+
+        //Then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            //when
+            user.makeListNumber("012");
+        });
+
+    }
+    @Test
+    @DisplayName("문자를 입력했을 때 오류 확인")
+    void 입력한_숫자중_문자_확인(){
+        //Given
+        User user = new User();
+
+        //Then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            //when
+            user.makeListNumber("abc");
+        });
+    }
+    @Test
+    @DisplayName("재시작시 입력 오류 확인")
+    void 재시작시_입력문자_확인(){
+        //Given
+        User user = new User();
+
+        //then
+        Throwable exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            //when
+            user.makeListNumber("3");
+        });
+    }
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
