8ae668a080bf3d82dfe4173e6be9a927b530201a
moong2 <www.castlehi@gmail.com>
2023-10-25 21:49:04 +0900

    docs: add Javadocs

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,11 @@ package baseball;
 
 import baseball.game.Game;
 
+/**
+ * 숫자 야구 게임이다.
+ *
+ * @author Seongha Park
+ */
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
