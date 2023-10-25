d128897cb5eff1fdba0c47e02cf85e3cbeeac4b7
bokyung <bbookng@gmail.com>
2023-10-25 10:58:24 +0900

    [Feat] BaseballNumbers 구현 완료 - 생성자 구현 완료 (랜덤 번호 생성) - containNumber, checkStrikeNumber Method 구현 완료

--- src/main/java/baseball/model/BaseballNumbers.java
+++ src/main/java/baseball/model/BaseballNumbers.java
@@ -9,15 +9,22 @@ public class BaseballNumbers {
     private final List<Integer> numbers;
 
     public BaseballNumbers() {
+        List<Integer> baseballNumbers = new ArrayList<>();
 
+        for (int i = 0; i < 3; i++) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!baseballNumbers.contains(randomNumber)) baseballNumbers.add(randomNumber);
+        }
+
+        this.numbers = baseballNumbers;
     }
 
     public boolean containNumber(int number) {
-
+        return this.numbers.contains(number);
 
     }
 
     public boolean checkStrikeNumber(int index, int number) {
-
+        return this.numbers.get(index) == number;
     }
 }
