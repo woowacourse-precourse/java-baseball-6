cf51578557ef34dcacdce2bb3cfd5e5290b6b00b
siyeonSon <siyeon0209@naver.com>
2023-10-25 20:29:58 +0900

    refactor: indent 최소화

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,18 +7,16 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
+        boolean isAnswer;
 
         while (true) {
             Game game = new Game();
-
-            while (true) {
+            isAnswer = false;
+            while (!isAnswer) {
                 System.out.print("숫자를 입력해주세요 : ");
                 String input = readLine();
                 List<Integer> number = NumberConverter.from(input);
-                boolean isAnswer = game.play(number);
-                if (isAnswer) {
-                    break;
-                }
+                isAnswer = game.play(number);
             }
 
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
