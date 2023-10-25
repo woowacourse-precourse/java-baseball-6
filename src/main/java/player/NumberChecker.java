816864fec6d563aefa54b43f461b932b5284214b
ehdtndla123 <ehdtndla123@naver.com>
2023-10-25 20:39:20 +0900

    feat : User의 입력한 수를 검증해주는 NumberChecker 클래스 추가

---
+++ src/main/java/player/NumberChecker.java
@@ -0,0 +1,54 @@
+package player;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class NumberChecker {
+
+    private int numberLength;
+    private static final String SPLIT = "";
+    private static final int MORE_THAN_ONE = 1;
+
+
+    public NumberChecker(int numberLength) {
+        this.numberLength = numberLength;
+    }
+
+    public void validateNumbers(String inputNumbers) {
+        validateNumberLength(inputNumbers.length());
+
+        Set<String> numberSet = new HashSet<>();
+
+        for (String number : inputNumbers.split(SPLIT)) {
+            validateNumberFormat(number);
+            validateNumberRange(number);
+            numberSet.add(number);
+        }
+
+        validateDuplicated(numberSet.size(), inputNumbers.length());
+    }
+
+    private void validateNumberLength(int length) {
+        if (length != numberLength)
+            throw new IllegalArgumentException();
+    }
+
+    private void validateNumberRange(String number) {
+        if (Integer.parseInt(number) < MORE_THAN_ONE)
+            throw new IllegalArgumentException();
+    }
+
+    private void validateNumberFormat(String number) {
+        try {
+            Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplicated(int numberSetSize, int inputNumberLength) {
+        if(numberSetSize<inputNumberLength)
+            throw new IllegalArgumentException();
+    }
+}
