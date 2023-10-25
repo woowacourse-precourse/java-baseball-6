bd1816870db74f098f284d80ca8a63b727c53a6e
LEE-sh1673 <lsh901673@gmail.com>
2023-10-25 22:36:11 +0900

    refactor: 테스트 메서드 명 변경
    테스트 데이터 생성 메서드 이름 변경
    generateComputerNumbers -> generatePlayerNumbers
    
    변경 이유:
    - 기존 테스트 로직은 하나의 Balls에 대해 여러 Balls를 비교
    - 하나의 Balls은 computer, 다른 Balls는 Player가 더 가독성 있음
    - 따라서 여러 Balls를 생성하는 메서드 이름을 변경함

--- src/test/java/baseball/model/BallsTest.java
+++ src/test/java/baseball/model/BallsTest.java
@@ -44,35 +44,35 @@ public class BallsTest {
     void doesNotExistsSameNumber_Then_Nothing() {
         Balls player = new Balls(List.of(7, 8, 9));
         Balls computer = new Balls(List.of(4, 2, 5));
-        CompareResults compareResults = player.compareAll(computer);
+        CompareResults compareResults = computer.compareAll(player);
         assertThat(compareResults.isNothing()).isTrue();
     }
 
     @ParameterizedTest
-    @MethodSource("generateComputerNumbers")
+    @MethodSource("generatePlayerNumbers")
     @DisplayName("비교 결과에서 스트라이크의 개수를 구할 수 있다.")
     void existsSameNumberInSamePosition_Then_Strike(
-            final Balls computerNumbers,
+            final Balls playerNumbers,
             final long numberOfStrikes
     ) {
-        Balls player = new Balls(List.of(1, 2, 3));
-        CompareResults compareResults = player.compareAll(computerNumbers);
+        Balls computer = new Balls(List.of(1, 2, 3));
+        CompareResults compareResults = computer.compareAll(playerNumbers);
         assertThat(compareResults.getStrikes()).isEqualTo(numberOfStrikes);
     }
 
     @ParameterizedTest
-    @MethodSource("generateComputerNumbers")
+    @MethodSource("generatePlayerNumbers")
     @DisplayName("비교 결과에서 볼의 개수를 구할 수 있다.")
     void existsSameNumberInSamePosition_Then_Ball(
-            final Balls computerNumbers,
+            final Balls playerNumbers,
             final long numberOfStrikes
     ) {
-        Balls player = new Balls(List.of(3, 1, 2));
-        CompareResults compareResults = player.compareAll(computerNumbers);
+        Balls computer = new Balls(List.of(3, 1, 2));
+        CompareResults compareResults = computer.compareAll(playerNumbers);
         assertThat(compareResults.getBalls()).isEqualTo(numberOfStrikes);
     }
 
-    private static Stream<Arguments> generateComputerNumbers() {
+    private static Stream<Arguments> generatePlayerNumbers() {
         return Stream.of(
                 Arguments.of(new Balls(List.of(4, 2, 5)), 1L),
                 Arguments.of(new Balls(List.of(4, 2, 3)), 2L),
