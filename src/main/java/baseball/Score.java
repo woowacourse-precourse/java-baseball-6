5fa8e968a5c7f60d512665909cd968f83600dfce
qaknc8 <qaknc8@gmail.com>
2023-10-25 20:47:28 +0900

    fix: 잘못된 점수 문구 수정

--- src/main/java/baseball/Score.java
+++ src/main/java/baseball/Score.java
@@ -6,7 +6,7 @@ public class Score {
     int ball;
     int strike;
 
-    public void countScore(List<Integer> computer, List<Integer> player) {
+    Score(List<Integer> computer, List<Integer> player) {
         ball = 0;
         strike = 0;
         int[] scoreTable = new int[10];
@@ -30,10 +30,10 @@ public class Score {
         if (strike == 0 && ball == 0) {
             return "낫싱";
         }
-        if (strike == 0) {
+        if (ball == 0) {
             return strike + "스트라이크";
         }
-        if (ball == 0) {
+        if (strike == 0) {
             return ball + "볼";
         }
         return ball + "볼 " + strike + "스트라이크";
