992c108fdad6e893e6fcd76431126f027b37dd5b
kdomo <dongho5088@naver.com>
2023-10-25 19:55:38 +0900

    feat: Computer 랜덤 번호 생성 기능 구현

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,7 +1,16 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Computer {
     private static final int INPUT_LENGTH = 3;
 
     int[] computerBaseBallNumber = new int[INPUT_LENGTH];
+
+    public void generateComputerBaseBallNumber() {
+        Integer[] randomBaseBallNumber = Randoms.pickUniqueNumbersInRange(1, 9, INPUT_LENGTH).toArray(Integer[]::new);
+        for (int i = 0; i < INPUT_LENGTH; i++) {
+            this.computerBaseBallNumber[i] = randomBaseBallNumber[i];
+        }
+    }
 }
