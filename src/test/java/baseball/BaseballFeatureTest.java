6b08c9f5097a593b75e916dab62774a506fff1fd
ckkim817 <ckkim817@konkuk.ac.kr>
2023-10-25 21:54:11 +0900

    [Test] 게임 종료 후 재시작 여부 입력 기능 검증을 위한 테스트 작성

--- src/test/java/baseball/BaseballFeatureTest.java
+++ src/test/java/baseball/BaseballFeatureTest.java
@@ -3,6 +3,7 @@ package baseball;
 import static baseball.global.BaseballConstant.GUESS_NUMBER_SIZE;
 import static baseball.global.BaseballConstant.MAX_RANGE;
 import static baseball.global.BaseballConstant.MIN_RANGE;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
@@ -79,6 +80,28 @@ public class BaseballFeatureTest extends NsTest {
         }
     }
 
+    @Test
+    void 게임종료_후_재시작_여부_입력_값이_숫자가_아닐_경우_예외_처리() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> runException("123", "a"))
+                            .isInstanceOf(IllegalArgumentException.class);
+                },
+                1, 3, 5
+        );
+    }
+
+    @Test
+    void 게임종료_후_재시작_여부_입력_값이_정해진_숫자가_아닐_경우_예외_처리() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> runException("123", "3"))
+                            .isInstanceOf(IllegalArgumentException.class);
+                },
+                1, 3, 5
+        );
+    }
+    
     @Override
     public void runMain() {
         Application.main(new String[]{});
