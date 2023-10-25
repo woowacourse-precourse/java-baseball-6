9881ad3e968f3e078909f43e0266f55ae178cd35
dongho <dons97@naver.com>
2023-10-25 15:13:48 +0900

    feat: implement BaseballGameModel class

--- src/main/java/baseball/model/BaseballGameModel.java
+++ src/main/java/baseball/model/BaseballGameModel.java
@@ -1,19 +1,24 @@
 package baseball.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class BaseballGameModel {
 
-    List<Integer> computerNumbers;
+    private final List<Integer> computerNumbers = new ArrayList<>();
 
     void generateRandomNumbers(){
-
+        while (computerNumbers.size() < 3) {
+            int randomNumber = pickNumberInRange(1, 9);
+            if (!computerNumbers.contains(randomNumber)) {
+                computerNumbers.add(randomNumber);
+            }
+        }
     }
 
     List<Integer> getComputerNumbers(){
-
-        return null;
+        return computerNumbers;
     }
 }
