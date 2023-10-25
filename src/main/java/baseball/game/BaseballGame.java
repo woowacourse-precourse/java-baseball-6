098cc8ce36d6b0801de7e9d3bf3c3eb89c89dad1
seyeong <lys200209@naver.com>
2023-10-25 15:45:23 +0900

    feat: 플레이어가 1~9 서로 다른 수 3개 선택
    잘못 입력 시 예외 발생시킴

--- src/main/java/baseball/game/BaseballGame.java
+++ src/main/java/baseball/game/BaseballGame.java
@@ -2,12 +2,14 @@ package baseball.game;
 
 import baseball.computer.Computer;
 import baseball.player.Player;
+import baseball.utils.view.Input;
 import baseball.utils.view.Messages;
 import baseball.utils.view.Output;
 
 public class BaseballGame {
     private final Computer computer;
     private final Player player;
+    private final Input input = new Input();
     private final Output output = new Output();
     private GameStatus gameStatus = GameStatus.PLAY;
 
@@ -20,10 +22,22 @@ public class BaseballGame {
         output.printMessageLine(Messages.START.getMessage());
         while (gameStatus.isPlay()) {
             generateComputerNumbers();
+            selectPlayerNumbers();
         }
     }
 
     private void generateComputerNumbers() {
         computer.generateNumbers();
+    }
+
+    private void selectPlayerNumbers() {
+        while (gameStatus.isPlay()) {
+            inputNumbers();
+        }
+    }
+
+    private void inputNumbers() {
+        output.printMessage(Messages.INPUT.getMessage());
+        player.inputNumbers(input.inputNumbers());
     }
 }
