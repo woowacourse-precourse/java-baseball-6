96a582fc6a9fc9af2a9e3a1915948e78753b650e
wook <dong930112@gmail.com>
2023-10-25 20:48:21 +0900

    refactor(GameController): 'gameStart()' 메서드 내부기능 분리  -handleGameActive(): 정답을 맞췄을 경우 겡미 재시작, 종료의 분기를 관리하는 메서드  -reportGameResult(): 게임결과('strike', 'ball')를 출력해주는 메서드

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -15,55 +15,70 @@ public class GameController {
     this.exceptionController = exceptionController;
   }
 
-  public void gameStart() {
-    List<Integer> resultNumList = numberController.generateComputerNumberList();
+    public void gameStart() {
+        List<Integer> computerNumberList = numberController.generateComputerNumberList();
 
-    Integer gameProgressNumber = 0;
-    Integer strike = 0;
-    Integer ball = 0;
+        Integer strike = 0;
+        Integer ball = 0;
+        boolean gameActive = true;
 
-    System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println("숫자 야구 게임을 시작합니다.");
 
-    while (true) {
-      System.out.print("숫자를 입력해주세요 : ");
-      String input = Console.readLine();
-      Integer inputNum = Integer.parseInt(input);
-      exceptionController.validateThreeDigitNumber(inputNum);
-      List<Integer> inputNumList = numberController.splitter(inputNum);
+        while (gameActive) {
+            System.out.print("숫자를 입력해주세요 : ");
+            String input = Console.readLine();
+            Integer inputNum = Integer.parseInt(input);
+            exceptionController.validateThreeDigitNumber(inputNum);
+            List<Integer> inputNumList = numberController.splitter(inputNum);
 
-      Map<String, Integer> report = numberController.compareNumberList(resultNumList, inputNumList);
-      strike = report.get("strike");
-      ball = report.get("ball");
+            Map<String, Integer> report = numberController.compareNumberList(computerNumberList,
+                inputNumList);
+            strike = report.get("strike");
+            ball = report.get("ball");
 
-      if(strike == 3) {
-        System.out.println("3스트라이크");
+            gameActive = handleGameActive(strike, ball);
+
+            if (strike == 3 && gameActive == true) {
+                computerNumberList = numberController.generateComputerNumberList();
+            }
+        }
+    }
+
+    public boolean handleGameActive(int strike, int ball) {
+        reportGameResult(strike, ball);
+
+        if (strike != 3) {
+            return true;
+        }
+
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        gameProgressNumber = Integer.parseInt(Console.readLine());
+        int gameProgressNumber = Integer.parseInt(Console.readLine());
         exceptionController.validateGameProgressNumber(gameProgressNumber);
 
-        if(gameProgressNumber == 1) {
-          resultNumList = numberController.generateComputerNumberList();
-
-          continue;
+        if (gameProgressNumber == 1) {
+            return true;
         }
         System.out.println("게임 종료");
-        break;
-      }
-      else if(strike == 0 && ball == 0) {
-        System.out.println("낫싱");
-      }
-      else if(strike != 0 && ball != 0) {
-        System.out.println(ball + "볼" + " " + strike + "스트라이크");
-      }
-      else if(strike != 0 && ball == 0) {
-        System.out.println(strike + "스트라이크");
-      }
-      else if(strike == 0 && ball != 0) {
-        System.out.println(ball + "볼");
-      }
-
+        return false;
     }
-  }
+
+    public void reportGameResult(int strike, int ball) {
+        if (strike != 3) {
+            if (strike == 0 && ball == 0) {
+                System.out.println("낫싱");
+            }
+            if (strike != 0 && ball != 0) {
+                System.out.println(ball + "볼" + " " + strike + "스트라이크");
+            }
+            if (strike != 0 && ball == 0) {
+                System.out.println(strike + "스트라이크");
+            }
+            if (strike == 0 && ball != 0) {
+                System.out.println(ball + "볼");
+            }
+        }
+        System.out.println("3스트라이크");
+    }
+
 }
