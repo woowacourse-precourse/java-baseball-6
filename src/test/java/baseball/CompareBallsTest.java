028950b4c20210a97cceb7dee2b66aff82006af9
ysparrk <youngseo0703@gmail.com>
2023-10-25 22:58:36 +0900

    refactor: 테스트코드 beforeEach 추가 및 given, when, then 분류

--- src/test/java/baseball/CompareBallsTest.java
+++ src/test/java/baseball/CompareBallsTest.java
@@ -1,35 +1,48 @@
 package baseball;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CompareBallsTest {
 
+    CompareBalls compareBalls = new CompareBalls();
+
+    static List<Integer> computer;
+    static List<Integer> player;
+
+    @BeforeEach
+    void beforeEach() {
+        computer = Arrays.asList(1, 2, 3);
+        player = Arrays.asList(3, 2, 1);
+    }
+
     @Test
     @DisplayName("같은 숫자 개수 확인")
     void countTotal() {
         // given
-        CompareBalls compareBalls = new CompareBalls();
 
         // when
+        int total = compareBalls.countTotal(computer, player);
 
         // then
-        assertThat(3).isEqualTo(compareBalls.countTotal(Arrays.asList(1,2,3), Arrays.asList(3,1,2)));
+        assertThat(3).isEqualTo(total);
     }
 
     @Test
     @DisplayName("스트라이크 개수 확인")
     void countStrike() {
         // given
-        CompareBalls compareBalls = new CompareBalls();
 
         // when
+        int expectStrike = compareBalls.countStrike(computer, player);
 
         // then
-        assertThat(1).isEqualTo(compareBalls.countStrike(Arrays.asList(3,2,4),Arrays.asList(3,1,2)));
+        assertThat(1).isEqualTo(expectStrike);
     }
 }
--- src/test/java/baseball/JudgeTest.java
+++ src/test/java/baseball/JudgeTest.java
@@ -5,20 +5,32 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class JudgeTest {
 
+    Judge judge = new Judge();
+
+    static List<Integer> computer;
+
+    @BeforeEach
+    void beforeEach() {
+        computer = Arrays.asList(1, 2, 3);
+    }
+
     @Test
     @DisplayName("스트라이크 확인")
     void judgeStrike() {
         // given
-        Judge judge = new Judge();
+        List<Integer> player = Arrays.asList(1, 2, 3);
+
         // when
+        String expectStrike = judge.judgement(computer, player);
 
         // then
-        assertThat("3스트라이크").isEqualTo(judge.judgement(Arrays.asList(1,2,3), Arrays.asList(1,2,3)));
+        assertThat("3스트라이크").isEqualTo(expectStrike);
 
     }
 
@@ -26,21 +38,26 @@ class JudgeTest {
     @DisplayName("볼 확인")
     void judgeBall() {
         // given
-        Judge judge = new Judge();
+        List<Integer> player = Arrays.asList(3, 1, 2);
 
         // when
+        String expectBall = judge.judgement(computer, player);
+
         // then
-        assertThat("3볼").isEqualTo(judge.judgement(Arrays.asList(1,2,3), Arrays.asList(3,1,2)));
+        assertThat("3볼").isEqualTo(expectBall);
     }
 
     @Test
     @DisplayName("낫싱 확인")
     void judgeNothing() {
         // given
-        Judge judge = new Judge();
+        List<Integer> player = Arrays.asList(4, 5, 6);
+
         // when
+        String expectNothing = judge.judgement(computer, player);
+
         // then
-        assertThat("낫싱").isEqualTo(judge.judgement(Arrays.asList(1,2,3), Arrays.asList(4,5,6)));
+        assertThat("낫싱").isEqualTo(expectNothing);
 
     }
 
