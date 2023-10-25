f9afc9646991df08ed488753a21958a39f8a9f50
kssumin <201566@jnu.ac.kr>
2023-10-25 18:11:35 +0900

    chore : 들여쓰기 수준 +4 스페이스로 통일

--- src/main/java/baseball/PrintMessage.java
+++ src/main/java/baseball/PrintMessage.java
@@ -3,8 +3,8 @@ package baseball;
 public enum PrintMessage {
 	START("숫자 야구 게임을 시작합니다. \n"),
 	REQUESTNUMBER("숫자를 입력해주세요 : \n"),
-    SUCCESS("3개의 숫자를 모두 맞히셨습니다! 게임 종료 \n"),
-    REQUEST_REPLY("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. \n"),
+	SUCCESS("3개의 숫자를 모두 맞히셨습니다! 게임 종료 \n"),
+	REQUEST_REPLY("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. \n"),
 	NOTHING("낫싱 \n"),
 	BALL("볼"),
 	STRIKE("스트라이크"),
@@ -16,9 +16,10 @@ public enum PrintMessage {
 	PrintMessage(String message) {
 		this.message = message;
 	}
+
 	public String getMessage() {
-        return message;
-    }
+		return message;
+	}
 }
 
 
