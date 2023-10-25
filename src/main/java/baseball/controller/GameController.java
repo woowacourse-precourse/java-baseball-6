d918073e549c1b939b2b9985c4ad856fcae43f5e
세탁기 <116627038+saetakki@users.noreply.github.com>
2023-10-25 21:07:10 +0900

    refactor: 재실행 여부 코드 분리 1. 3스트라이크 여부를 판별하는 코드 분리 2. 해당 코드는 Game 으로 이돔

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -3,6 +3,7 @@ package baseball.controller;
 
 import baseball.model.Game;
 import baseball.model.Validator;
+import baseball.view.Messages;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
@@ -17,31 +18,20 @@ public class GameController {
     }
 
     public void gameModule() {
-        System.out.println("게임 시작");
+        Messages.printInitiateGameMessage();
         List<Integer> computer = game.generateComputersNumber(); // 게임이 시작할 때마다 컴퓨터의 숫자를 생성
         boolean isThreeStrike = false;
 
         while (!isThreeStrike) {
+            Messages.printInputMessage();
             String userInput = Console.readLine();
             validator.validateUserInput(userInput);
 
             List<Integer> userGuess = validator.splitNumbersByDigits(Integer.parseInt(userInput));
             List<Integer> result = game.calculateResult(userGuess, computer);
 
-//            System.out.println("Result: " + result + ", User Guess: " + userGuess + ", Computer: " + computer);
+            isThreeStrike = game.returnResult(result);
 
-            if (result.get(1) == 3) {
-                System.out.println("3스트라이크");
-                isThreeStrike = true;
-            } else if (!result.contains(0)) {
-                System.out.printf("%d볼 %d스트라이크\n", result.get(0), result.get(1));
-            } else if (result.contains(0) && result.get(0) != 0) {
-                System.out.printf("%d볼\n", result.get(0));
-            } else if (result.contains(0) && result.get(1) != 0) {
-                System.out.printf("%d스트라이크\n", result.get(1));
-            } else {
-                System.out.println("낫싱");
-            }
         }
     }
 
@@ -53,7 +43,7 @@ public class GameController {
             isContinue = game.retryHandler();
         } while (isContinue);
 
-        System.out.println("게임 종료");
+        Messages.printEndGameMessage();
     }
 
 
