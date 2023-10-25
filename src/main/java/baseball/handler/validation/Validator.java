34056defa243fc212de6a43f37ff0fdc5b6a922b
yujeonghyeon <dbdb1114@naver.com>
2023-10-25 23:16:54 +0900

    제한사항 파악 실수 수정 ( 입력 받는 수의 갯수 )

--- src/main/java/baseball/handler/validation/Validator.java
+++ src/main/java/baseball/handler/validation/Validator.java
@@ -1,41 +1,38 @@
 package baseball.handler.validation;
 
 import baseball.config.GameProperty;
-import camp.nextstep.edu.missionutils.Randoms;
-import java.io.FileInputStream;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Properties;
 import java.util.Set;
-import org.assertj.core.internal.Numbers;
 
 public class Validator {
 
 
-    private Validator(){
+    private Validator() {
     }
 
-    public static void answerValidate(List<Integer> userAnswer){
+    public static void answerValidate(List<Integer> userAnswer) {
         dupleNumValidate(userAnswer);
         lengthValidate(userAnswer);
         typeValidate(userAnswer);
     }
-    private static void dupleNumValidate(List<Integer> userAnswer){
-        Set<Integer> answerSet = new HashSet<>();
-        if(answerSet.size() != userAnswer.size()){
+
+    private static void dupleNumValidate(List<Integer> userAnswer) {
+        Set<Integer> answerSet = new HashSet<>(userAnswer);
+        if (answerSet.size() != userAnswer.size()) {
             throw new IllegalArgumentException();
         }
     }
-    private static void lengthValidate(List<Integer> userAnswer){
-        if(userAnswer.size() != GameProperty.getAnswerlength()){
+
+    private static void lengthValidate(List<Integer> userAnswer) {
+        if (userAnswer.size() > GameProperty.getAnswerlength()) {
             throw new IllegalArgumentException();
         }
     }
-    private static void typeValidate(List<Integer> userAnswer){
-        for (int num: userAnswer ) {
-            if(num < GameProperty.getStRange() || num > GameProperty.getEdRange()){
-                throw new IllegalArgumentException();
-            }
+
+    private static void typeValidate(List<Integer> userAnswer) {
+        for (int num : userAnswer) {
+            throw new IllegalArgumentException();
         }
     }
 }
