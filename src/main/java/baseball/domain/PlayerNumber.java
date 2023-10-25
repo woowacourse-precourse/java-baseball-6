3a10f225e53972dfe34877a81aaf09ef2b2b0eaf
Chaeros <chpark6851@koreatech.ac.kr>
2023-10-25 19:40:17 +0900

    feat: 사용자 입력 중복 검증 메서드 추가
    사용자가 입력한 값에 중복되는 수가 있는지 검증하는 메서드를
    추가했습니다.

--- src/main/java/baseball/domain/PlayerNumber.java
+++ src/main/java/baseball/domain/PlayerNumber.java
@@ -2,7 +2,9 @@ package baseball.domain;
 
 import baseball.constant.Constant;
 import baseball.constant.errormessage.PlayerInputError;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class PlayerNumber {
 
@@ -14,6 +16,18 @@ public class PlayerNumber {
     private void validateSize(List<Integer> playerNumbers) {
         if (playerNumbers.size() != Constant.GAME_NUMBERS_SIZE) {
             throw new IllegalArgumentException(PlayerInputError.PLAYER_NUMBER_WRONG_INPUT_SIZE_MESSAGE);
+        }
+    }
+
+    /**
+     * 입력한 문자열에 중복되는 문자가 포함되어있는 경우 예외 발생
+     *
+     * @param playerNumbers : 랜덤으로 생성한 컴퓨터가 가진 수
+     */
+    private void validateDuplicate(List<Integer> playerNumbers) {
+        Set<Integer> nonDuplicateNumbers = new HashSet<>(playerNumbers);
+        if (nonDuplicateNumbers.size() != Constant.GAME_NUMBERS_SIZE) {
+            throw new IllegalArgumentException(PlayerInputError.PLAYER_NUMBER_WRONG_INPUT_DUPLICATE_MESSAGE);
         }
     }
 }
