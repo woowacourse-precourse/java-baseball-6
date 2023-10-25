c108821be1a5f19307c1ec93cd348b5cd6de1211
GyeomFka <clazziquai01@gmail.com>
2023-10-25 23:27:57 +0900

    feat : 어플리케이션 시작

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import game.GameHost;
+import participant.Computer;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameHost gameHost = new GameHost();
     }
 }
