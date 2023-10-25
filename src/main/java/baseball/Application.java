59fba75b6e4a3fa2d416b84ae6b3c231b838a90c
Yang woosung <ngxk1217@gmail.com>
2023-10-25 14:10:47 +0900

    read me 작성하고 나서 파일 확인 test 파일 확인

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,26 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        //JUNIt?
+            GenerateRandomNum randomNum = new GenerateRandomNum();
+            Input input = new Input();
+            Judge judge = new Judge();
+            Playagain playagain = new Playagain();
+            boolean again = true;
+
+            while(again){
+                List<Integer> computer = randomNum.create();
+                String result ="";
+                while(!result.equals("3스트라이크")){
+                    result = judge.judgement(computer,input.playerNumber());
+                    System.out.println(result);
+                }
+                again = playagain.playagain();
+            }
+
 
     }
 }
