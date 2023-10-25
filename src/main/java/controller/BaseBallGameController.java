c06f88e662e8c77b4f9a68fa87886ef07afce68e
Huisu <87214089+ranunclulus@users.noreply.github.com>
2023-10-25 10:44:12 +0900

    ✨ feat: <Random 번호 생성> - 게임의 한 턴마다 RandomNumbers라는 객체에 1~9 사이 숫자 중 세 개를 중복 없이 골라 저장

--- src/main/java/controller/BaseBallGameController.java
+++ src/main/java/controller/BaseBallGameController.java
@@ -1,20 +1,25 @@
 package controller;
 
+import domain.RandomNumbers;
+
 public class BaseBallGameController {
     private static final String CONTINUE = "1";
     private static final String END = "2";
     private static final Integer THREE_STRIKE = 3;
 
     public static void start() {
-        startBaseBallGame();
+        playBaseBallGame();
         checkContinue();
     }
 
-    private static void startBaseBallGame() {
+    // playBaseBallGame: 게임 진행
+    private static void playBaseBallGame() {
         int strikeCnt = 0;
         int ballCnt = 0;
+        RandomNumbers randomNumbers = new RandomNumbers();
     }
 
+    // checkContinue: 게임을 계속할 것인지 확인
     private static void checkContinue() {
     }
 }
--- src/main/java/domain/RandomNumbers.java
+++ src/main/java/domain/RandomNumbers.java
@@ -1,4 +1,29 @@
 package domain;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RandomNumbers {
+    private static final int START_INCLUSIVE = 1;
+    private static final int END_INCLUSIVE = 9;
+    private final int RANDOM_COUNT = 3;
+
+    private static List<Integer> randomNumbers;
+
+    public RandomNumbers() {
+        this.randomNumbers = drawRandomNumbers();
+    }
+
+    // drawRandomNumbers: 랜덤 넘버를 세 개 지정
+    private List<Integer> drawRandomNumbers() {
+        randomNumbers = new ArrayList<>();
+        while (randomNumbers.size() != RANDOM_COUNT) {
+            int randomNumber = Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
+            if (!randomNumbers.contains(randomNumber)) {
+                randomNumbers.add(randomNumber);
+            }
+        }
+        return randomNumbers;
+    }
 }
