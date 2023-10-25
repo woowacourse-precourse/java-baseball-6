82c591eb65be1efc7911d8341777e8dbbb93579d
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-25 21:28:25 +0900

    [feat] : add input validation class and validation method

---
+++ src/main/java/baseball/utils/InputValidation.java
@@ -0,0 +1,13 @@
+package baseball.utils;
+
+public class InputValidation {
+
+    private static final int MAX_LENGTH = 3;
+
+    // Player 가 입력한 임의의 3자리 수를 검증하는 메서드.
+    public void validation(String inPutNumber) {
+//        lengthValidation(inPutNumber);
+//        numberValidation(inPutNumber);
+//        overlapValidation(inPutNumber);
+    }
+}
