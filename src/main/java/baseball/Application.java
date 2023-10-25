0d1cc38b1983c0766ea058fbebe5001cd78245d7
mingeun0507 <tqdlfkg@naver.com>
2023-10-25 16:12:13 +0900

    feat: main 메소드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import baseball.application.service.GameFacade;
+import baseball.domain.service.HintCreateService;
+import baseball.domain.service.PredictionCreateService;
+import baseball.presentation.GameController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        HintCreateService hintCreateService = new HintCreateService();
+        PredictionCreateService predictionCreateService = new PredictionCreateService();
+        GameFacade gameFacade = new GameFacade(hintCreateService, predictionCreateService);
+
+        GameController gameController = new GameController(hintCreateService, predictionCreateService, gameFacade);
+        gameController.init();
+        gameController.progress();
     }
 }
