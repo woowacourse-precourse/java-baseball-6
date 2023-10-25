cf7180fff74a82fb8afda3bd17e978a5a196f79e
Flash-KIM142 <topqr123q@gmail.com>
2023-10-25 15:53:57 +0900

    refactor: 도메인 모델 변경에 따른 상수값 삭제

--- src/main/java/baseball/constant/GameConstant.java
+++ src/main/java/baseball/constant/GameConstant.java
@@ -5,7 +5,4 @@ public class GameConstant {
     public static final int GAME_NUMBER_LENGTH = 3;
     public static final String ONLY_NUMBER_REGEX = "^[1-9]+$";
     public static final String RESTART_NUMBER_REGEX = "^[1-2]$";
-    public static final int STRIKE_INDEX = 0;
-    public static final int BALL_INDEX = 1;
-    public static final int THREE_STRIKE = 3;
 }
