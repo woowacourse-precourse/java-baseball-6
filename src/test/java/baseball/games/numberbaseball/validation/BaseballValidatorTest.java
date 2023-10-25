2e902e2f3a704d74faba4638778d32408853f84b
haebyun <bhk4392@gmail.com>
2023-10-25 18:17:46 +0900

    test(BaseballValidator): 세자리보다 짧은 문자열에 대해 예외를 발생시키는 기능 테스트

--- src/test/java/baseball/games/numberbaseball/validation/BaseballValidatorTest.java
+++ src/test/java/baseball/games/numberbaseball/validation/BaseballValidatorTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 class BaseballValidatorTest {
     private final BaseballValidator validator = new BaseballValidator();
-    
+
     @Test
     @DisplayName("범위를 벗어난 수 입력에 대한 IllegalArgumentException")
     void validateOutRangeInput() {
@@ -21,6 +21,14 @@ class BaseballValidatorTest {
     void validateNotDigitInput() {
         assertThatThrownBy(() -> {
             validator.validateNumbersInput("a23");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("짧은 문자열 입력에 대한 IllegalArgumentException")
+    void validateLessNumbersInput() {
+        assertThatThrownBy(() -> {
+            validator.validateNumbersInput("12");
         }).isInstanceOf(IllegalArgumentException.class);
     }
 }
