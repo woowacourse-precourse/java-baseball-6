5ccec287d7e8017e15d2fff000a7be3e06f07000
OhTaeHo-97 <57743934+OhTaeHo-97@users.noreply.github.com>
2023-10-25 17:08:50 +0900

    test: 재시작 여부 입력 테스트 코드 작성

--- src/test/java/utils/InputUtilTest.java
+++ src/test/java/utils/InputUtilTest.java
@@ -105,4 +105,75 @@ class InputUtilTest {
             System.out.println(InputUtil.getPlayerInput());
         }
     }
+
+    @Nested
+    @DisplayName("사용자의 3자리 수를 입력받는 기능")
+    class RestartInfoTest extends NsTest {
+        @Test
+        @DisplayName("정상 입력")
+        void success() {
+            assertSimpleTest(
+                    () -> {
+                        run("1");
+                        assertThat(output()).isEqualTo(String.valueOf(1));
+                    }
+            );
+        }
+
+        @Nested
+        @DisplayName("예외 테스트")
+        class ExceptionTest {
+            @Test
+            @DisplayName("null 테스트")
+            void nullTest() {
+                String nullStr = null;
+                assertSimpleTest(() ->
+                        assertThatThrownBy(() -> runException(nullStr))
+                                .isInstanceOf(IllegalArgumentException.class)
+                );
+            }
+
+            @Test
+            @DisplayName("빈 문자열 테스트")
+            void emptyTest() {
+                String emptyStr = new String();
+                assertSimpleTest(() ->
+                        assertThatThrownBy(() -> runException(emptyStr))
+                                .isInstanceOf(IllegalArgumentException.class)
+                );
+            }
+
+            @Test
+            @DisplayName("잘못된 타입 테스트")
+            void invalidTypeTest() {
+                assertSimpleTest(() ->
+                        assertThatThrownBy(() -> runException("a"))
+                                .isInstanceOf(IllegalArgumentException.class)
+                );
+            }
+
+            @Test
+            @DisplayName("1자리가 아닌 숫자 테스트")
+            void invalidLengthTest() {
+                assertSimpleTest(() ->
+                        assertThatThrownBy(() -> runException("12"))
+                                .isInstanceOf(IllegalArgumentException.class)
+                );
+            }
+
+            @Test
+            @DisplayName("0 테스트")
+            void invalidZeroTest() {
+                assertSimpleTest(() ->
+                        assertThatThrownBy(() -> runException("0"))
+                                .isInstanceOf(IllegalArgumentException.class)
+                );
+            }
+        }
+
+        @Override
+        protected void runMain() {
+            System.out.println(InputUtil.getRestartInfo());
+        }
+    }
 }
