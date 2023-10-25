97864d348fea582d32f7f378097799cd73cee719
JongMinCh0i <wonderland314592@gmail.com>
2023-10-25 10:53:15 +0900

    refactoring(ThreeOutCount): 생성자 접근지시자(public -> private)

--- src/main/java/baseball/util/ThreeOutCount.java
+++ src/main/java/baseball/util/ThreeOutCount.java
@@ -3,6 +3,9 @@ package baseball.util;
 public class ThreeOutCount {
     private static final int OutCount = 3;
 
+    private ThreeOutCount() {
+    }
+
     public static int getThreeOutCount() {
         return OutCount;
     }
