6cf834f0d65c8381955cd0bef67c8f74cd752643
Pearl <rkdwlswn227@gmail.com>
2023-10-25 18:38:48 +0900

    feat: application 실행 내용 추가
    순서대로 애플리케이션 실행하는 코드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,23 @@
 package baseball;
-import camp.nextstep.edu.missionutils.*;
 
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
+        // 1. 시작 문구 출력
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        // 2. 랜덤 숫자 생성
         RandNum randNumGenerator = new RandNum();
-        randNumGenerator.makeRandNum();
+        List<Integer> computerNum = randNumGenerator.makeRandNum();
+
+        // 3. 유저 입력 숫자 받기 (+입력 시 예외 처리)
+        UserNum consoleInput = new UserNum();
+        List<Integer> userNum = consoleInput.inputNum();
+
+        // 4. computerNum과 userNum을 비교하여 숫자 야구 진행
+        
     }
 }
