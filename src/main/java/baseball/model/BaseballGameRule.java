ec5a71f48ae0e90fdd08c0c7a4d994981b457070
chosunghyun18 <krismon@naver.com>
2023-10-25 16:12:48 +0900

    refact : modify game rule class type from abstract

--- src/main/java/baseball/model/BaseballGameRule.java
+++ src/main/java/baseball/model/BaseballGameRule.java
@@ -1,12 +1,12 @@
 package baseball.model;
 
-public abstract class BaseballGameRule {
-    protected static final Integer PLAY_BASEBALL_GAME = 1;
+public class BaseballGameRule {
+    protected final static Integer PLAY_BASEBALL_GAME = 1;
     protected final static Integer NO_HIT = 0;
     protected final static Integer BASEBALL_MIN_VALUE = 1;
     protected final static Integer BASEBALL_MAX_VALUE = 9;
     protected final static Integer BASEBALL_MAX_AMOUNT = 3;
-    protected static final Integer BASEBALL_START_POSITION = 1;
+    protected final static Integer BASEBALL_START_POSITION = 1;
     protected final static Integer BASEBALL_GAME_RESTART = 1;
     protected final static Integer BASEBALL_GAME_FINISH = 2;
     protected final static String BASEBALL_GAME_MESSAGE_NOTHING_KOR = "낫싱";
