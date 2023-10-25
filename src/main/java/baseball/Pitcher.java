3415ef99b2cde902a8955947a901312391985a4a
ParkJinYong <mej1406@gmail.com>
2023-10-25 20:22:45 +0900

    refactor: 유틸 클래스 추가, 예외처리 통합

--- src/main/java/baseball/Pitcher.java
+++ src/main/java/baseball/Pitcher.java
@@ -9,46 +9,15 @@ public class Pitcher extends Player {
     public void setNumbers() throws IllegalArgumentException {
         String input = inputNumbers();
 
-        if (isOverMaxLength(input)) {
-            throw new IllegalArgumentException("최대 길이 " + Constant.MAX_NUMBER_SIZE + "를 넘어가는 입력입니다.");
-        }
-        else if (isInValidInput(input)) {
-            throw new IllegalArgumentException("올바른 입력이 아닙니다. 정수를 입력해야합니다.");
-        }
+        Util.handleException(input);
 
-        int[] split = splitNumbers(input);
-        if (isDuplicate(split)) {
-            throw new IllegalArgumentException("입력한 숫자가 중복됩니다.");
-        }
+        int[] split = Util.stringToIntArray(input);
         updateNumbers(split);
     }
 
     public String inputNumbers() {
         System.out.print("숫자를 입력해주세요 : ");
         return Console.readLine();
-    }
-
-    public boolean isOverMaxLength(String input) {
-        return input.length() > Constant.MAX_NUMBER_SIZE;
-    }
-
-    public boolean isInValidInput(String input) {
-        try {
-            Integer.parseInt(input);
-            return false;
-        } catch (Exception e) {
-            return true;
-        }
-    }
-
-    public int[] splitNumbers(String number) throws IllegalArgumentException {
-        return number.chars()
-                .map(i -> i - '0')
-                .toArray();
-    }
-
-    public boolean isDuplicate(int[] split) {
-        return Arrays.stream(split).distinct().count() != split.length;
     }
 
     public void updateNumbers(int[] split) {
