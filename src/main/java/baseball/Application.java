9c515f7f4e33d42b5a6f536f508ad1361113da69
minisyu <iswater.kim@gmail.com>
2023-10-25 12:44:49 +0900

    refactor: InputController -> InputConvertor로 이름 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,7 @@
 package baseball;
 
 import baseball.controller.GameController;
-import baseball.controller.InputController;
+import baseball.convertor.InputConvertor;
 import baseball.domain.game.ComputerNumberGenerator;
 import baseball.util.RandomNumberGenerator;
 import baseball.view.InputView;
@@ -11,11 +11,11 @@ public class Application {
     public static void main(String[] args) {
         OutputView outputView = new OutputView();
         InputView inputView = new InputView(outputView);
-        InputController inputController = new InputController(inputView);
+        InputConvertor inputConvertor = new InputConvertor(inputView);
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator(1, 9);
         ComputerNumberGenerator randomComputerNumberGenerator = new ComputerNumberGenerator(
                 randomNumberGenerator);
-        GameController gameController = new GameController(outputView, inputController, randomComputerNumberGenerator);
+        GameController gameController = new GameController(outputView, inputConvertor, randomComputerNumberGenerator);
         gameController.start();
     }
 }
