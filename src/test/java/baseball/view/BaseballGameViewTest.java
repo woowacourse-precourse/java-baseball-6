b2c5fb7980139b29189c8f15299032cc42a2d4e3
dongho <dons97@naver.com>
2023-10-25 19:42:22 +0900

    fix: Edit typo in BaseballGameViewTest

--- src/test/java/baseball/view/BaseballGameViewTest.java
+++ src/test/java/baseball/view/BaseballGameViewTest.java
@@ -124,7 +124,7 @@ class BaseballGameViewTest {
         assertEquals("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n", outputMessage.toString());
         assertThat(restartInput).isEqualTo(1);
 
-        System.setIn(System.int);
+        System.setIn(System.in);
     }
 
     @Test
