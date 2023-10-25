dc68988270d717e3192265426aefb621dd336339
woookk <woook27@naver.com>
2023-10-25 22:20:51 +0900

    Test: BaseBallGame 클래스 테스트 추가

--- src/test/java/baseball/service/BaseballGameTest.java
+++ src/test/java/baseball/service/BaseballGameTest.java
@@ -2,7 +2,34 @@ package baseball.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import baseball.service.BaseballGame;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class BaseballGameTest {
 
+    private BaseballGame game;
 
+    @BeforeEach
+    void setUp() {
+        game = new BaseballGame();
+    }
+
+    @Test
+    void 볼_카운트_확인() {
+        assertEquals(0, game.countBall("123", "456"));
+        assertEquals(1, game.countBall("123", "345"));
+        assertEquals(2, game.countBall("123", "213"));
+        assertEquals(3, game.countBall("123", "312"));
+    }
+
+    @Test
+    void 스트라이크_카운트_확인() {
+        assertEquals(0, game.countStrike("123", "456"));
+        assertEquals(1, game.countStrike("123", "156"));
+        assertEquals(2, game.countStrike("123", "126"));
+        assertEquals(3, game.countStrike("123", "123"));
+    }
 }
