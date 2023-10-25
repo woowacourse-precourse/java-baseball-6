a4974097882f71abbb6c7554236157a6aa2084b4
horbiths <gkrtjs77@gmail.com>
2023-10-25 15:09:34 +0900

    10.25 baseball divide game method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,21 +1,20 @@
 package baseball;
 
-import java.util.List;
-
 public class Application {
 
 	public static void main(String[] args) {
 		
     	Exception RestartInput = new Exception();
     	PlayerInput Input = new PlayerInput();
-		
+		GameCollect Game = new GameCollect();
+    	
     	int start = 0;
     	
     	while(start == 0) {
     	
     	  int restart;
           
-    	  game();
+    	  Game.game();
     	  
     		
     	  System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");	
@@ -30,35 +29,6 @@ public class Application {
     	
      }
 
-
-	public static void game() {
-		Computer computer = new Computer();
-		PlayerInput Input = new PlayerInput();
-		Exception vaild = new Exception();
-		Compare pcCompare = new Compare();
-	
-		System.out.println("게임을 시작합니다.");
-		int strike = 0;
-		int GameStart = 1;
-
-	
-		List<Integer> RandomNum = computer.RandomComputerNum();
-
-	
-		while(GameStart == 1) {
-         System.out.println("숫자를 입력하세요. : ");
-         String InputNum = Input.PlayerInputNum();
-   	     List<Integer> PlayerNum = vaild.InputException(InputNum);
-    	 
-   	     strike = pcCompare.CompareNum(PlayerNum, RandomNum);
-    	 
-   	     if(strike == 3) {
- 		  strike = 0;
- 		  System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-    	  break;
-   	    }
-       }
-   }
 }
     	
 
