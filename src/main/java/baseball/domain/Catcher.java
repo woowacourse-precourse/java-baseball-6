01378c7bca7e7527737b2605dfe08e0e2c30654b
ckkim817 <ckkim817@konkuk.ac.kr>
2023-10-25 17:27:01 +0900

    [Feat] 생성된 숫자와 입력한 숫자 간 비교 기능 구현
    컴퓨터가 생성한 숫자, 플레이어가 입력한 숫자

---
+++ src/main/java/baseball/domain/Catcher.java
@@ -0,0 +1,45 @@
+package baseball.domain;
+
+import static baseball.global.BaseballConstant.GUESS_NUMBER_SIZE;
+
+import java.util.stream.IntStream;
+
+public class Catcher {
+
+    private final int strikeCount;
+    private final int ballCount;
+
+    public Catcher(Computer computer, Player player) {
+        this.strikeCount = countStrike(computer, player);
+        this.ballCount = countBall(computer, player);
+    }
+
+    private int countStrike(Computer computer, Player player) {
+        return (int) IntStream.range(0, GUESS_NUMBER_SIZE)
+                .filter(i -> computer.getNumberOf(i) == player.getNumberOf(i))
+                .count();
+    }
+
+    private int countBall(Computer computer, Player player) {
+        return (int) IntStream.range(0, GUESS_NUMBER_SIZE)
+                .map(player::getNumberOf)
+                .filter(computer::numbersContains)
+                .count() - countStrike(computer, player);
+    }
+
+    public boolean isNothing() {
+        return this.strikeCount == 0 && this.ballCount == 0;
+    }
+
+    public boolean isThreeStrike() {
+        return this.strikeCount == 3;
+    }
+
+    public int getStrikeCount() {
+        return strikeCount;
+    }
+
+    public int getBallCount() {
+        return ballCount;
+    }
+}
