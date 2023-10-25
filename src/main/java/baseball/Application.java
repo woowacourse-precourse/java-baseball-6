98c27de008d08378685a0337ac7105470b0160f5
uhyunglee <uhyunglee@naver.com>
2023-10-25 21:52:56 +0900

    [FEAT] : Generate Random Number function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import baseball.domain.NumberGenerator;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        NumberGenerator generator = new NumberGenerator();
+        List<Integer> numbers = generator.createRandomNumber();
+        System.out.println(numbers);
     }
 }
--- src/main/java/baseball/domain/NumberGenerator.java
+++ src/main/java/baseball/domain/NumberGenerator.java
@@ -1,9 +1,21 @@
 package baseball.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class NumberGenerator {
     public List<Integer> createRandomNumber(){
-        return null;
+        List<Integer> numbers = new ArrayList<>();
+
+        while(numbers.size() < 3){
+            int number = Randoms.pickNumberInRange(1, 9);
+            if(numbers.contains(number)){
+                continue;
+            }else {
+                numbers.add(number);
+            }
+        }
+        return numbers;
     }
 }
