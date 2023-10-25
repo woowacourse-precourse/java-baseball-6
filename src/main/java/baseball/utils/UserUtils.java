c0045255181ce003b7c625055f7a131f38db4a14
dhl1924 <dhl1924@naver.com>
2023-10-25 21:58:57 +0900

    refactor: 유저의 입력 값을 분리해 리스트에 추가하는 함수 생성

--- src/main/java/baseball/utils/UserUtils.java
+++ src/main/java/baseball/utils/UserUtils.java
@@ -1,9 +1,19 @@
 package baseball.utils;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserUtils {
     public String inputUserNumber() {
         return Console.readLine();
+    }
+
+    public static List<Integer> splitDigits(int number) {
+        List<Integer> digits = new ArrayList<>();
+        digits.add(number / 100); // 백의자리 추출하여 리스트에 추가
+        digits.add((number % 100) / 10); // 십의자리 추출하여 리스트에 추가
+        digits.add(number % 10); // 일의자리 추출하여 리스트에 추가
+        return digits;
     }
 }
