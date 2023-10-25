8536731b17f7b53fd765aaebaab85d29becd3f15
WinterHana <haha920@naver.com>
2023-10-25 23:12:20 +0900

    [Test] 테스트 코드 추가
    - 다양한 경우에 대한 테스트 코드 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -19,7 +19,6 @@ class ApplicationTest extends NsTest {
                 1, 3, 5, 5, 8, 9
         );
     }
-
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
@@ -27,7 +26,47 @@ class ApplicationTest extends NsTest {
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
-
+    @Test
+    void 게임종료_입력_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> runException("135", "12"))
+                            .isInstanceOf(IllegalArgumentException.class);
+                },
+                1, 3, 5
+        );
+    }
+    @Test
+    void 문자_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("S23"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    void 숫자_예외_2개_입력_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("21"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    void 숫자_예외_중복_숫자_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("121"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    void 정상_작동_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("523", "781", "572", "597", "759", "579", "2");
+                    assertThat(output()).contains("1스트라이크", "1볼", "2스트라이크", "2볼 1스트라이크", "3스트라이크", "게임 종료");
+                },
+                5, 7, 9
+        );
+    }
     @Override
     public void runMain() {
         Application.main(new String[]{});
