6f216b02782e03aa1b23fe34b3a02ca020cdf710
peeaaccee <wlgp8958@naver.com>
2023-10-25 19:28:36 +0900

    feat(GameController): 게임을 진행하고, 결과를 담은 ResultDto 객체를 반환

--- src/main/java/baseball/domain/controller/GameController.java
+++ src/main/java/baseball/domain/controller/GameController.java
@@ -1,6 +1,10 @@
 package baseball.domain.controller;
 
+import baseball.domain.dto.ResultDto;
+import baseball.domain.dto.UserInputDto;
 import baseball.domain.model.Computer;
+import baseball.domain.model.Score;
+import baseball.domain.model.User;
 
 import java.util.List;
 
@@ -9,5 +13,12 @@ public class GameController {
 
     public GameController(List<String> computerBalls) {
         this.computer = new Computer(computerBalls);
+    }
+
+    public ResultDto doGame(UserInputDto userInputDto) {
+        User user = new User(userInputDto.getUserInput());
+        Score score = new Score(this.computer, user);
+
+        return new ResultDto(score);
     }
 }
