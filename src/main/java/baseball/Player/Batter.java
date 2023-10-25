54869e600974063724b06b1304c6b91ec5838575
roasted-penguin <swkwo25@hanyang.ac.kr>
2023-10-25 19:00:45 +0900

    fix : Batter 중복탐지 오류 수정 -> String과 Character 비교하고 있었음

--- src/main/java/baseball/Player/Batter.java
+++ src/main/java/baseball/Player/Batter.java
@@ -9,15 +9,15 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Batter implements BaseballPlayer {
-    private List<Integer> choice;
-    private static final List<Character> validInput = Arrays.asList('1','2','3','4','5','6','7','8','9');
-
+    private static List<Integer> choice;
+    private static final List<Character> validCharacter = Arrays.asList('1','2','3','4','5','6','7','8','9');
+    
     @Override
     public void makeChoice(){
         String input = readLine();
 
         if(!isValidChoice(input)){
-            throw new IllegalArgumentException("올바른 입력이 아닙니다.");
+            throw new IllegalArgumentException(INVALID_INPUT);
         }
 
         choice = new ArrayList<>();
@@ -29,13 +29,13 @@ public class Batter implements BaseballPlayer {
     private boolean isValidChoice(String input){
         if (input.length()!=3) return false;
 
-        List<String> checkedCharacters = new ArrayList<>();
+        List<Character> checkedCharacters = new ArrayList<>();
         for (char c : input.toCharArray()){
             //올바른 입력 혹은 중복 탐지
-            if (!validInput.contains(c) || checkedCharacters.contains(c)){
+            if (!validCharacter.contains(c) || checkedCharacters.contains(c)){
                 return false;
             }
-            checkedCharacters.add(String.valueOf(c));
+            checkedCharacters.add(c);
         }
         return true;
     }
