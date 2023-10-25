2f0b579bc4676c9ee6c77f1ec9f1ad0a1dcd54fe
doka <posmane5@gmail.com>
2023-10-25 14:28:42 +0900

    test(Confirm) : Confirm 클래스의 checkPositionMatch 메소드 테스트 추가 자리 일치 갯수 반환 메소드인 Confirm클래스의 checkPositionMatch메소드 테스트 추가

--- src/test/java/baseball/feature/ConfirmTest.java
+++ src/test/java/baseball/feature/ConfirmTest.java
@@ -16,11 +16,13 @@ class ConfirmTest {
     // 일치 갯수를 잘 표현 하는지 확인
     @ParameterizedTest
     @MethodSource("provideTestCases")
-    public void testAccordNumber(List<Integer> userNumbers, List<Integer> computerNumbers, int expected) {
+    public void testAccordNumber(List<Integer> userNumbers, List<Integer> computerNumbers,
+            int expected) {
         Confirm confirm = new Confirm(userNumbers, computerNumbers);
         int result = confirm.accordNumber();
         assertEquals(expected, result, expected + "개의 숫자가 일치해야 합니다.");
     }
+
     // 일치 갯수를 잘 표현 하는지 확인
     private static Stream<Arguments> provideTestCases() {
         return Stream.of(
@@ -29,4 +31,22 @@ class ConfirmTest {
                 Arguments.of(List.of(1, 4, 7), List.of(1, 4, 7), 3)
         );
     }
+
+    @ParameterizedTest
+    @MethodSource("providePositionMatchTestCases")
+    public void testCheckPositionMatch(List<Integer> userNumbers, List<Integer> computerNumbers,
+            int expected) {
+        Confirm confirm = new Confirm(userNumbers, computerNumbers);
+        int result = confirm.checkPositionMatch();
+        assertEquals(expected, result, expected + "개의 숫자와 위치가 일치해야 합니다.");
+    }
+
+    private static Stream<Arguments> providePositionMatchTestCases() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3), List.of(1, 5, 6), 1),
+                Arguments.of(List.of(1, 2, 3), List.of(1, 2, 6), 2),
+                Arguments.of(List.of(1, 2, 3), List.of(1, 2, 3), 3)
+        );
+    }
+
 }
