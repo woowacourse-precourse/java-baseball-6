dac1aa7ae76e805fc68fea5528d5c7e1a5bd8a55
김라연 <rayeon@gimlayeon-ui-noteubug.local>
2023-10-25 21:58:26 +0900

    refactor: 들여쓰기 기준 맞추기 위한 리팩터링

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,8 +3,6 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -13,89 +11,46 @@ import java.util.Set;
 public class Application {
 
     static boolean[] isNumberUsedComputer;
+    static List<Integer> computerNumbers;
+    static List<Integer> userNumbers;
+    static int strikeCnt,ballCnt;
+    static final int NUMBER_COUNT = 3;
 
     public static void main(String[] args) {
 
-        startPrint();
-        while (true) {
-            List<Integer> computerNumbers = generateRandomNumbers();
-            markNumberUsed(computerNumbers);
+        System.out.println("숫자 야구 게임을 시작합니다.");
 
-            while (true) {
-                List<Integer> userNumbers;
-                int strikeCnt = 0;
-                int ballCnt = 0;
+        boolean playAgain = true;
 
-                try {
-                    userNumbers = inputNumberOfUser();
-                } catch (IllegalArgumentException e) {
-                    e.printStackTrace();
-                    break;
-                }
-
-                for (int index = 0; index < 3; index++) {
-                    int userNumber = userNumbers.get((userNumbers.size() - 1) - index);
-                    int computerNumber = computerNumbers.get(index);
-
-                    if (isStrike(userNumber, computerNumber)) {
-                        strikeCnt++;
-                        continue;
-                    }
-                    if (isBall(userNumber, computerNumber))
-                        ballCnt++;
-                }
-
-                if (isFinish(strikeCnt, ballCnt)) {
-                    break;
-                }
-
-            }
-            if (!restart())
-                break;
+        while (playAgain) {
+            playAgain=playRound();
         }
+
         System.out.println("게임을 종료합니다.");
 
     }
 
-    private static boolean restart() {
-        String restartGameInput = Console.readLine();
+    private static boolean playRound() {
 
-        if (restartGameInput.equals("2"))
-            return false;
-        if (restartGameInput.equals("1"))
-            return true;
-        throw new IllegalArgumentException("범위 밖의 입력입니다. 1과 2중 입력해 주세요");
-    }
+        setComputerNumbers();
 
-    private static boolean isFinish(int strikeCnt, int ballCnt) {
-        if (strikeCnt == 3) {
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            return true;
-        }
-        StringBuilder sb = new StringBuilder();
-        if (strikeCnt + ballCnt == 0) {
-            System.out.println("낫싱");
-            return false;
-        }
-        if (ballCnt != 0)
-            sb.append(ballCnt + "볼 ");
-        if (strikeCnt != 0) {
-            sb.append(strikeCnt + "스트라이크 ");
+        boolean isFinished = false;
+
+        while(!isFinished){
+            setUserNumbers();
+            isFinished=evaluateGuess();
         }
 
-        System.out.println(sb.toString());
-        return false;
-
+        return restart();
     }
 
-    private static boolean isBall(int userNumber, int computerNumber) {
-        return isNumberUsedComputer[userNumber];
+    private static void setUserNumbers() {
+        try {
+            userNumbers = inputNumberOfUser();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
     }
-
-    private static boolean isStrike(int userNumber, int computerNumber) {
-        return userNumber == computerNumber;
-    }
-
     private static List<Integer> inputNumberOfUser() {
         System.out.print("숫자를 입력해주세요 : ");
 
@@ -117,17 +72,86 @@ public class Application {
         return userNumber;
     }
 
+    private static void setComputerNumbers() {
+        computerNumbers = generateRandomNumbers();
+        markNumberUsed(computerNumbers);
+    }
+
+    private static boolean evaluateGuess() {
+         strikeCnt = 0;
+         ballCnt = 0;
+
+        for (int index = 0; index < 3; index++) {
+            int userNumber = userNumbers.get((userNumbers.size() - 1) - index);
+            int computerNumber = computerNumbers.get(index);
+
+            checkStrikeAndBall(userNumber,computerNumber);
+        }
+
+        if (isFinish(strikeCnt, ballCnt)) {
+            return false;
+        }
+        return true;
+    }
+
+    private static void checkStrikeAndBall(int userNumber, int computerNumber) {
+        if (isStrike(userNumber, computerNumber)) {
+            strikeCnt++;
+            return;
+        }
+        if (isBall(userNumber))
+            ballCnt++;
+    }
+
+    private static boolean restart() {
+        String restartGameInput = Console.readLine();
+
+        if (restartGameInput.equals("2"))
+            return false;
+        if (restartGameInput.equals("1"))
+            return true;
+        throw new IllegalArgumentException("범위 밖의 입력입니다. 1과 2중 입력해 주세요");
+    }
+
+    private static boolean isFinish(int strikeCnt, int ballCnt) {
+        if (strikeCnt == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        }
+
+        if (strikeCnt + ballCnt == 0) {
+            System.out.println("낫싱");
+            return false;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        if (ballCnt != 0)
+            sb.append(ballCnt + "볼 ");
+        if (strikeCnt != 0) {
+            sb.append(strikeCnt + "스트라이크 ");
+        }
+
+        System.out.println(sb.toString());
+        return false;
+
+    }
+
+    private static boolean isBall(int userNumber) {
+        return isNumberUsedComputer[userNumber];
+    }
+
+    private static boolean isStrike(int userNumber, int computerNumber) {
+        return userNumber == computerNumber;
+    }
+
     private static boolean isNumber(String inputString) {
         return inputString.matches("\\d+");
     }
-
-    private static void startPrint() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
-    }
-
+    
     private static boolean areDistinctNumbers(List<Integer> numbers) {
         Set<Integer> uniqueNumbers = new HashSet<>(numbers);
-        return uniqueNumbers.size() == numbers.size();
+        return uniqueNumbers.size() == NUMBER_COUNT;
     }
 
     public static List<Integer> generateRandomNumbers() {
@@ -136,11 +160,15 @@ public class Application {
         while (computerNumbers.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
 
-            if (!computerNumbers.contains(randomNumber)) {
-                computerNumbers.add(randomNumber);
-            }
+            addUniqueNumber(computerNumbers, randomNumber);
         }
         return computerNumbers;
+    }
+
+    private static void addUniqueNumber(List<Integer> numbers, int number) {
+        if (!numbers.contains(number)) {
+            numbers.add(number);
+        }
     }
 
     private static void markNumberUsed(List<Integer> computerNumber) {
@@ -148,6 +176,5 @@ public class Application {
         for (int index = 0; index < 3; index++)
             isNumberUsedComputer[computerNumber.get(index)] = true;
     }
-
 
 }
