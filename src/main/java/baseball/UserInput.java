1d48074e7f4f84d8f3c1d891828813d829526fc0
kijen723 <kij2646@gmail.com>
2023-10-25 18:43:38 +0900

    refactor: 유저 입력 기능 클래스 분류

--- src/main/java/baseball/UserInput.java
+++ src/main/java/baseball/UserInput.java
@@ -1,2 +1,17 @@
-package baseball;public class UserInput {
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class UserInput {
+    public static String userNumber() {
+        String userNumber = Console.readLine();
+        ExceptionControl.checkUserNumberInput(userNumber);
+        return userNumber;
+    }
+    public static int restartGame() {
+        String restart = Console.readLine();
+        int restartCount = Integer.parseInt(restart);
+        ExceptionControl.checkRestartGameInput(restartCount);
+        return restartCount;
+    }
 }
