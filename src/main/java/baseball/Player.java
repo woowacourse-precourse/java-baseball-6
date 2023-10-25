077c1c008a546a2df4a9ba6fa3ea5bca814515ba
swhyeon98 <smreo75@gmail.com>
2023-10-25 18:49:34 +0900

    feat: 입력받은 문자열을 List 형태로 변환하는 기능의 'convertStringToIntList' 메서드 추가

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,8 +1,17 @@
 package baseball;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Player {
 
     private List<Integer> numbers;
+
+    public List<Integer> convertStringToIntList(String input) {
+        List<Integer> numbers = Arrays.stream(input.split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        return numbers;
+    }
 }
