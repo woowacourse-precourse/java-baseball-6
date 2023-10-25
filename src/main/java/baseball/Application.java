08dd2abba6ffd342747f630718023b2912005d21
Project-Longinus <Team.Project.Longinus@gmail.com>
2023-10-25 22:42:13 +0900

    :>>>

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,8 +5,10 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalArgumentException {
 
+        String inputNumber;
+        int[] inputNumber2Array = new int[3];
         int[] randomNumber = new int[3];
         for (int i = 0; i < 3; i++) {
             randomNumber[i] = Randoms.pickNumberInRange(1, 9);
@@ -14,9 +16,19 @@ public class Application {
 
         System.out.println("숫자 야구 게임을 시작합니다. ");
         System.out.print("숫자를 입력해주세요 : ");
-        System.out.println(Console.readLine());
+        inputNumber = Console.readLine();
+
+        if (inputNumber.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+
+        for( int i = 0; i < 3; i++ ) {
+
+            inputNumber2Array[i] = inputNumber.charAt(i) - '0';
+        }
 
         System.out.println(Arrays.toString(randomNumber));
+        System.out.println(Arrays.toString(inputNumber2Array));
     }
 }
 
