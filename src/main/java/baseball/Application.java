e72b64b90fdb05d808783693eebe2ea1d020b2ec
kanghana1 <qazplm12093@naver.com>
2023-10-25 20:55:01 +0900

    작동시키기..

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,15 @@
 package baseball;
 
+import baseball.console.InputConsole;
+import baseball.console.MainController;
+import baseball.console.OutputConsole;
+import baseball.controller.GameController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameController gameController = new GameController(new MainController(new InputConsole(), new OutputConsole()));
+        gameController.start();
 
     }
 }
