3b56fa978932b579bdfce2b70118b7b5fc4ccd50
Chaeros <chpark6851@koreatech.ac.kr>
2023-10-25 19:35:29 +0900

    const: 사용자 입력 예외 출력 문자열 상수 추가
    사용자의 입력에서 예외가 발생했을 경우 출력할 문자열을 상수로 사용하기
    위해 추가했습니다.

--- src/main/java/baseball/constant/errormessage/PlayerInputError.java
+++ src/main/java/baseball/constant/errormessage/PlayerInputError.java
@@ -1,5 +1,11 @@
 package baseball.constant.errormessage;
 
+import baseball.constant.Constant;
+
 public class PlayerInputError {
     public static final String PLAYER_NUMBER_WRONG_INPUT_NOT_INTEGER_MESSAGE = "[ERROR] 1~9 사이 숫자가 아닌 다른 문자가 입력에 포함되어 있습니다.";
+    public static final String PLAYER_NUMBER_WRONG_INPUT_SIZE_MESSAGE =
+            "[ERROR] 야구 번호는 " + Constant.GAME_NUMBERS_SIZE + "개만 입력 가능합니다.";
+    public static final String PLAYER_NUMBER_WRONG_INPUT_DUPLICATE_MESSAGE = "[ERROR] 야구 번호들은 중복될 수 업습니다.";
+    public static final String PLAYER_NUMBER_WRONG_INPUT_RANGE_MESSAGE = "[ERROR] 입력한 문자열 중 1~9 사이의 수가 아닌 문자가 포함되어있습니다.";
 }
