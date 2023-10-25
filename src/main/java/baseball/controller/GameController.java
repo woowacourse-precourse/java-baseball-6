fb55c2d7dd28058b7f9e66d90139e709fc8efb83
realcold0 <realcold0@gmail.com>
2023-10-25 14:36:48 +0900

    feat: 비교한 스트라이크, 볼 갯수를 이용하여 출력하는 ResultVIew 생성

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -4,19 +4,21 @@ import baseball.domain.AnswerCreator;
 import baseball.domain.Player;
 import baseball.service.CompareAnswerWithPlayerNumber;
 import baseball.view.InputView;
+import baseball.view.ResultView;
 
 public class GameController {
     private AnswerCreator answerCreator = new AnswerCreator();
     private Player player = new Player();
     private InputView inputView = new InputView();
     private CompareAnswerWithPlayerNumber compareAnswerWithPlayerNumber = new CompareAnswerWithPlayerNumber();
+    private ResultView resultView = new ResultView();
 
     public void startGame() {
         answerCreator.setComputerAnswer();  //게임 정답 세팅
         player.setPlayerAnswer(inputView.inputPlayerNumbers());//플레이어의 정답 작성
         compareAnswerWithPlayerNumber.compareStrikeBall(player.getPlayerAnswer(),
                 answerCreator.getComputerAnswer());//플레이어 정답, 게임정답 비교
-        //게임 결과 확인
+        resultView.outputCompareResult(compareAnswerWithPlayerNumber);//게임 결과 확인
 
     }
 
---
+++ src/main/java/baseball/view/ResultView.java
@@ -0,0 +1,22 @@
+package baseball.view;
+
+import baseball.service.CompareAnswerWithPlayerNumber;
+
+public class ResultView {
+    private final String NOTHING = "낫싱";
+
+    public void outputCompareResult(CompareAnswerWithPlayerNumber compareAnswerWithPlayerNumber) {
+        int strike = compareAnswerWithPlayerNumber.getStrike();
+        int ball = compareAnswerWithPlayerNumber.getBall();
+
+        if (ball == 0 && strike == 0) {
+            System.out.println(NOTHING);
+        } else if (strike > 0 && ball == 0) {
+            System.out.println(strike + "스트라이크");
+        } else if (strike == 0 && ball > 0) {
+            System.out.println(ball + "볼");
+        } else if (strike > 0 && ball > 0) {
+            System.out.println(strike + "스트라이크" + ball + "볼");
+        }
+    }
+}
