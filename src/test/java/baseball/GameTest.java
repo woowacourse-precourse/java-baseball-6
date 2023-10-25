7ece1599aeae716ecb3ad3a804358a4d3620caa5
siyeonSon <siyeon0209@naver.com>
2023-10-25 20:19:33 +0900

    refactor: static import Assertions.assertThat

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -1,9 +1,10 @@
 package baseball;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class GameTest {
@@ -12,14 +13,14 @@ class GameTest {
 
     @Test
     void 세자리의_수를_생성한다() {
-        Assertions.assertThat(number.size())
+        assertThat(number.size())
                 .isEqualTo(3);
     }
 
     @Test
     void 서로_다른_수로_이루어진_세자리의_수를_생성한다() {
         Set<Integer> uniqueNumber = new HashSet<>(number);
-        Assertions.assertThat(uniqueNumber.size())
+        assertThat(uniqueNumber.size())
                 .isEqualTo(3);
     }
 }
