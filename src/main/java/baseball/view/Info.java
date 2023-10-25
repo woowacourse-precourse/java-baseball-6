a628190e698e1fe3b73049fe71294c40f71fea38
jeonseong-gwon <jeonsg9904@gmail.com>
2023-10-25 17:13:07 +0900

    Refactor : 줄바꿈 변경

--- src/main/java/baseball/view/Info.java
+++ src/main/java/baseball/view/Info.java
@@ -7,11 +7,13 @@ public class Info {
             System.out.print(balls + "볼 ");
         }
         if (strikes > 0) {
-            System.out.println(strikes + "스트라이크");
+            System.out.print(strikes + "스트라이크");
         }
 
         if (strikes == 0 && balls == 0) {
             System.out.println("낫싱");
+        } else{
+            System.out.println();
         }
 
         if (strikes == 3) {
