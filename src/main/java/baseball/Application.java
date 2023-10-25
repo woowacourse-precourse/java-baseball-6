e200781691168e4ef59c03f9b9ebe8a28cf40672
ycis5013@naver.com <ycis5013@naver.com>
2023-10-25 23:58:30 +0900

    refactor : 코드 리팩토링

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -35,9 +35,9 @@ public class Application {
 
     static List<Integer> getPlayerNum() {
         List<Integer> player = new ArrayList<>();
-        char[] user_chArr = Console.readLine().toCharArray();
-        for (char character : user_chArr) {
-            int num = character - '0';
+        char[] player_chArr = Console.readLine().toCharArray();
+        for (char c : player_chArr) {
+            int num = c - '0';
             //서로 다른 수가 아닌 경우
             if (player.contains(num)) {
                 throw new IllegalArgumentException();
@@ -57,7 +57,8 @@ public class Application {
     }
 
     static void printResult(List<Integer> computer, List<Integer> player) throws IllegalArgumentException {
-        int strike = 0, ball = 0;
+        int strike = 0;
+        int ball = 0;
         for (int i = 0; i < 3; i++) {
             if (computer.get(i).equals(player.get(i))) {
                 strike++;
@@ -70,13 +71,17 @@ public class Application {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             gameOver();
             return;
-        } else if (strike == 0 && ball == 0) {
+        }
+        if (strike == 0 && ball == 0) {
             System.out.println("낫싱");
-        } else if (ball == 0) {
+        }
+        if (strike != 0 && ball == 0) {
             System.out.println(strike + "스트라이크");
-        } else if (strike == 0) {
+        }
+        if (strike == 0 && ball != 0) {
             System.out.println(ball + "볼");
-        } else {
+        }
+        if (strike != 0 && ball != 0) {
             System.out.println(ball + "볼 " + strike + "스트라이크");
         }
         continueGame(computer);
