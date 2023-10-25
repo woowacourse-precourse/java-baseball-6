42fa7b03a5564878b15b07191b395ded9f6a7280
이승헌 <317tmdgjs@naver.com>
2023-10-25 12:25:07 +0900

    fix[게임진행]: 볼, 스트라이크 출력 순서 조정

--- src/main/java/baseball/service/BaseballService.java
+++ src/main/java/baseball/service/BaseballService.java
@@ -28,11 +28,11 @@ public class BaseballService {
         if (strike == 0 && ball == 0) {
             message.append("낫싱");
         }
-        if (strike != 0) {
-            message.append(strike).append("스트라이크 ");
-        }
         if (ball != 0) {
             message.append(ball).append("볼 ");
+        }
+        if (strike != 0) {
+            message.append(strike).append("스트라이크 ");
         }
         return message.toString();
     }
