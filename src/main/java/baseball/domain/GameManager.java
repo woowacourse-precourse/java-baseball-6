16283788253f2869e7ffa1109f43f9ecb08fc44e
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-25 21:26:32 +0900

    [feat] : add ball check method

--- src/main/java/baseball/domain/GameManager.java
+++ src/main/java/baseball/domain/GameManager.java
@@ -9,7 +9,7 @@ public class GameManager {
     // Computer 가 선택한 임의의 3자리 숫자와 Player 가 입력한 3자리 숫자를 비교하는 메서드.
     public void check(Computer computer, Player player) {
         strikeCount = strikeCheck(computer, player);
-//        ballCount = ballCheck(computer, player);
+        ballCount = ballCheck(computer, player);
     }
 
     // Computer 가 선택한 임의의 3자리 숫자와 Player 가 입력한 3자리 숫자에서 스트라이크 카운터를 리턴하는 메서드.
@@ -22,5 +22,18 @@ public class GameManager {
             }
         }
         return strikeCount;
+    }
+
+    // Computer 가 선택한 임의의 3자리 숫자와 Player 가 입력한 3자리 숫자에서 볼 카운터를 리턴하는 메서드.
+    private int ballCheck(Computer computer, Player player) {
+        ballCount = 0;
+
+        for (int i = 0; i < LENGTH; i++) {
+            if (computer.getRandomNumber().charAt(i) != player.getInputNumber().charAt(i)
+                    && computer.getRandomNumber().contains(Character.toString(player.getInputNumber().charAt(i)))) {
+                ballCount++;
+            }
+        }
+        return ballCount;
     }
 }
