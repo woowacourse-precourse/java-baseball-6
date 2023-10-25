ab9c3a47e63950f5aabb4b30c75a7bbad77aa45d
Sunkist18 <dandalf@o.cnu.ac.kr>
2023-10-25 21:41:58 +0900

    :sparkles: Feat: Check for invalid Number states

--- src/main/java/baseball/Numbers.java
+++ src/main/java/baseball/Numbers.java
@@ -6,6 +6,9 @@ public class Numbers {
     private final List<Integer> numbers;
 
     public Numbers(List<Integer> numbers) {
+        if (numbers == null || numbers.size() != 3) {
+            throw new IllegalArgumentException();
+        }
         this.numbers = numbers;
     }
 
