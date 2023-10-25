e09d4cbe9caf59fa23f50ebcf163fb6317d9bb90
oyoungsun <oyoungsun1030@gmail.com>
2023-10-25 17:47:14 +0900

    feat : 게임 등록

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,9 @@
 package baseball;
 
+import baseball.controller.PlayController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        PlayController playController = new PlayController();
     }
 }
