445dd2dc781032d2d22d70bc2864fa8ce00248d4
jooys130 <wndustn130@gmail.com>
2023-10-25 12:21:43 +0900

    refactor: 출력 메세지에서 상수 사용하도록 수정

--- src/main/java/baseball/constants/Constants.java
+++ src/main/java/baseball/constants/Constants.java
@@ -19,9 +19,11 @@ public class Constants {
     public static final String BLANK = " ";
 
     public static final String NUMBER_QUESTION = "숫자를 입력해주세요 : ";
-    public static final String RESTART_OR_QUIT_QUESTION = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String RESTART_OR_QUIT_QUESTION =
+        "게임을 새로 시작하려면 " + RESTART + ", 종료하려면 " + QUIT + "를 입력하세요.";
 
     public static final String NUMBER_BASKETBALL_GAME_START = "숫자 야구 게임을 시작합니다.";
-    public static final String SUCCESS_AND_GAME_FINISH = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String SUCCESS_AND_GAME_FINISH =
+        NUMBER_SIZE + "개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
 }
