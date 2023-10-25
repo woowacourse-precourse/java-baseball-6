f1194f563990fd8acae69e4e69d733dad06afea4
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-10-25 22:12:32 +0900

    feat: 볼, 스트라이크 카운팅 기능 추가

--- src/main/java/baseball/model/BaseballGame.java
+++ src/main/java/baseball/model/BaseballGame.java
@@ -26,4 +26,43 @@ public class BaseballGame {
         }
     }
 
+    private static Integer countCommonElements(List<Integer> computerNumber, List<Integer> userNumber){
+        // List를 Set으로 변환하여 중복을 제거
+        Set<Integer> set1 = new HashSet<>(computerNumber);
+        Set<Integer> set2 = new HashSet<>(userNumber);
+
+        // 두 set의 교집합
+        set1.retainAll(set2);
+
+        return set1.size();
+    }
+
+    private static Integer countStrikeElements(List<Integer> computerNumber, List<Integer> userNumber) {
+        int strikeCount = 0;
+
+        for (int i = 0; i < GAME_NUMBER_LENGTH; i++) {
+            if (computerNumber.get(i).equals(userNumber.get(i))) {
+                strikeCount++;
+            }
+        }
+
+        return strikeCount;
+    }
+
+    private static Integer countBallElements(List<Integer> computerNumber, List<Integer> userNumber) {
+        int ballCount = countCommonElements(computerNumber, userNumber) - countStrikeElements(computerNumber, userNumber);
+
+        return ballCount;
+    }
+
+    public List<Integer> countBallAndStrike(List<Integer> computerNumber, List<Integer> userNumber) {
+        Integer strikeCount = countStrikeElements(computerNumber, userNumber);
+        Integer ballCount = countBallElements(computerNumber, userNumber);
+        List<Integer> ballAndStrike = new ArrayList<>();
+
+        ballAndStrike.add(ballCount);
+        ballAndStrike.add(strikeCount);
+
+        return ballAndStrike;
+    }
 }
