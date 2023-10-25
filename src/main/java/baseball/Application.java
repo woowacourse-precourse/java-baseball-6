123073870c38b8f57d3b31bd0145019907d7ea33
plll0123 <jsh951227@gmail.com>
2023-10-25 20:43:19 +0900

    feat: BaseBallGameRunner 호출 코드 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (true) {
+            BaseBallGameRunner.run();
+            System.out.println("""
+				3개의 숫자를 모두 맞히셨습니다! 게임 종료
+				게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."""
+            );
+            final String inputValue = Console.readLine();
+            if (inputValue == null) {
+                throw new IllegalArgumentException();
+            }
+            final int result = Integer.parseInt(inputValue);
+            if (result == 2) {
+                break;
+            }
+        }
     }
 }
