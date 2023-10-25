7582273e9e89d8c641e3d5f05a7fc1d085df96b5
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-25 21:02:13 +0900

    [feat] : add restart guide method

--- src/main/java/baseball/view/InputInformationView.java
+++ src/main/java/baseball/view/InputInformationView.java
@@ -8,4 +8,9 @@ public class InputInformationView {
     public void numberInputGuide() {
         System.out.print(numberInput);
     }
+
+    // "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요." 문장을 출력하는 메서드.
+    public void reStartGuide() {
+        System.out.println(reStart);
+    }
 }
