10452f2fe3bb719e2e076b1ac639c1a7213e8e19
sangdol <priceelees@gmail.com>
2023-10-25 15:53:24 +0900

    test: NumberBaseball의 테스트 코드 리팩터링
    < description >
    - 기존에는 게임을 1회만 진행했으나, 게임을 3회 진행하며 모든 숫자와 모든 결과에 대해 검토하도록 설정

--- src/test/java/baseball/NumberBaseballTest.java
+++ src/test/java/baseball/NumberBaseballTest.java
@@ -2,14 +2,16 @@ package baseball;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.List;
-import java.util.Scanner;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -24,33 +26,55 @@ class NumberBaseballTest {
     void setUp() {
         out = new ByteArrayOutputStream();
         System.setOut(new PrintStream(out));
+
+        Console.close();
     }
 
-    @DisplayName("게임을 정확히 실행하는지 확인합니다.")
+    @DisplayName("게임을 반복 실행하여, 모든 가능한 결과에 대해 정상적으로 실행되는지 확인합니다.")
     @Test
-    void startGame() {
-        String input = "123\n345\n678\n789\n359\n2";
+    void startGame_CorrectExecution() {
+        String input = """
+                123
+                345
+                678
+                789
+                359
+                1
+                416
+                418
+                148
+                1
+                245
+                265
+                267
+                2
+                """;
         in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
 
-        NumberBaseball numberBaseball = new NumberBaseball() {
-            @Override
-            protected List<Integer> getComNumber() {
-                return List.of(3, 5, 9);
-            }
-        };
+        ComNumberGenerator comNumberGenerator = mock(ComNumberGenerator.class);
+        when(comNumberGenerator.generate())
+                .thenReturn(List.of(3, 5, 9))
+                .thenReturn(List.of(1, 4, 8))
+                .thenReturn(List.of(2, 6, 7));
+
+        NumberBaseball numberBaseball = new NumberBaseball(comNumberGenerator, new UserNumberGenerator());
 
         numberBaseball.startGame();
-        String printedMessage = out.toString();
 
+        String printedMessage = out.toString();
         assertThat(printedMessage).contains(
                 "숫자 야구 게임을 시작합니다.",
                 "1볼",
                 "1볼 1스트라이크",
                 "낫싱",
+                "3스트라이크",
+                "2볼",
+                "2볼 1스트라이크",
                 "1스트라이크",
-                "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
-                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+                "2스트라이크",
+                "게임 종료",
+                "새로 시작"
         );
     }
 
@@ -64,18 +88,7 @@ class NumberBaseballTest {
             "5829/입력되는 숫자는 3자리여야 합니다.",
             "121/중복된 숫자는 입력할 수 없습니다."}, delimiter = '/')
     void startGame_WithInvalidInput_ThrowsException(String input, String errorMessage) {
-        NumberBaseball numberBaseball = new NumberBaseball() {
-            @Override
-            protected BallContainer createUserBalls() {
-                Scanner scanner = new Scanner(System.in);
-                UserNumberGenerator numberGenerator = new UserNumberGenerator();
-                System.out.println("숫자를 입력해주세요 : ");
-                String input = scanner.nextLine();
-
-                List<Integer> userNumber = numberGenerator.generate(input);
-                return BallContainer.getFromNumbers(userNumber);
-            }
-        };
+        NumberBaseball numberBaseball = new NumberBaseball();
 
         System.setIn(new ByteArrayInputStream(input.getBytes()));
 
