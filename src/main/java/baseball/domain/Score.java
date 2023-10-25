a20885409aaad6e57186811b233c995d8327918c
Hyoseop Song <crad_on25@naver.com>
2023-10-25 21:45:18 +0900

    feature(Score): 유저와 채점 로직을 이어주는 점수 클래스 구현

--- src/main/java/baseball/domain/Score.java
+++ src/main/java/baseball/domain/Score.java
@@ -1,5 +1,7 @@
 package baseball.domain;
 
+import baseball.SYSTEM_VALUE.GameStatusValue;
+
 public class Score {
     private int strike = 0;
     private int ball = 0;
@@ -7,5 +9,29 @@ public class Score {
     public Score(int strike, int ball) {
         this.strike = strike;
         this.ball = ball;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+
+    public boolean isGameOver() {
+        return strike == GameStatusValue.GAME_FINISH;
+    }
+
+    public boolean isStrike() {
+        return strike == 3;
+    }
+
+    public boolean isBall() {
+        return ball == 4;
+    }
+
+    public boolean isNothing() {
+        return strike == 0 && ball == 0;
     }
 }
