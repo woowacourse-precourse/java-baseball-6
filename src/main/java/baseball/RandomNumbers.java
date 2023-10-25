3542e8d5bcafba09375d3799979c0b0c0894c6b9
brixxt27 <brixxt27@gmail.com>
2023-10-25 19:57:30 +0900

    refactor: RandomNumbers 정적 변수에서 ArrayList로 변경
    - 처음에는 다뤄야 할 데이터가 세 개로 정해져 있어서 길이가 컴파일 시 정해져 있는 정적 배열을 사용하려 했다. 데이터 크기 자체 자체에서 차이가 나므로 굳이 collection을 사용할 필요를 느끼지 못했던 것이다. 그러나 구현을 해보니 오히려 구현해야 할 것들이 늘어나고, 이게 가독성을 낮추는 결과까지 초래했다.
    - 위와 같은 이유로 이미 기능이 추상화 되어 메소드로 구현 되어 있는 Collection 중 하나인 ArrayList<Integer>를 사용하여 데이터 저장하기로 했다.

--- src/main/java/baseball/RandomNumbers.java
+++ src/main/java/baseball/RandomNumbers.java
@@ -2,37 +2,32 @@ package baseball;
 
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RandomNumbers {
     private final static int MAX_LENGTH = 3;
     private final static int START_INCLUSIVE = 1;
     private final static int END_INCLUSIVE = 9;
-    private final int[] numsArray;
+    private final List<Integer> nums;
 
     RandomNumbers() {
-        numsArray = new int[MAX_LENGTH];
-        int i = 0;
+        nums = new ArrayList<>();
 
         while (true) {
-            boolean isSame = false;
-
-            if (i == MAX_LENGTH) {
+            if (nums.size() == MAX_LENGTH) {
                 break;
             }
-            numsArray[i] = pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
-            for (int j = 0; j < i; j++) {
-                if (numsArray[j] == numsArray[i]) {
-                    isSame = true;
-                    break;
-                }
-            }
-            if (isSame) {
+            int target = pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
+
+            if (nums.contains(target)) {
                 continue;
             }
-            i++;
+            nums.add(target);
         }
     }
 
-    public int[] getNumsArray() {
-        return numsArray;
+    public List<Integer> getNums() {
+        return nums;
     }
 }
