4322113ad7e01b888905b7899e20d1883975f17f
sulee <ok6737@naver.com>
2023-10-25 17:40:45 +0900

    20231025_1740

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,33 +3,28 @@ package baseball;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import camp.nextstep.edu.missionutils.*;
-
+import camp.nextstep.edu.missionutils.Randoms;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
-class IllegalArgumentException extends Exception{
-    public IllegalArgumentException() {}
-    public IllegalArgumentException(String message){
-        super(message);
-    }
-}
-
 public class Application {
-    public static int countChar(String str, char ch) {
-        return str.length() - str.replace(String.valueOf(ch), "").length();
+
+    public static int countNumber(String str, char num) { // 입력받은 값에 중복된 숫자가 있는지 체크하기 위한 함수
+        return str.length() - str.replace(String.valueOf(num), "").length();
     }
-    public static void main(String[] args) throws IllegalArgumentException {
+    public static void main(String[] args)  {
         // TODO: 프로그램 구현
 
         Scanner scan = new Scanner(System.in);
         List<Integer> computer = new ArrayList<>();
 
         System.out.println("숫자 야구 게임을 시작합니다.");
-        int power = 1;
-        boolean flag = false;
+        int power = 1; // 재시작/종료 여부를 확인하기 위한 값 (1/2)
+        boolean flag = false; // 게임이 재시작될 때 computer가 서로 다른 임의의 수를 다시 설정할 수 있도록 체크해줌
 
-        while(power!=2){
+        while(power!=2){ // power를 2로 입력받을때까지 반복
+
             if(!flag) {
+                // flag가 false 일경우  1.게임이 처음 시작되었거나  2.게임이 재시작 되었기 떄문이므로 computer의 값을 재설정해줌
                 computer.clear();
                 while (computer.size() < 3) {
                     int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -37,41 +32,41 @@ public class Application {
                         computer.add(randomNumber);
                     }
                 }
-                flag = true;
+                flag = true; // 재설정 후 게임 종료 시까지 초기화 되지 않도록 flag 를 true로 변경
             }
-
-            System.out.printf("%s -> 숫자를 입력해주세요 : ", computer.toString());
+            System.out.print("숫자를 입력해주세요 : ");
             String str = readLine();
 
-            if (str.length() != 3)
-                throw new IllegalArgumentException("사용자 입력값 오류 : " + str);
+            //예외처리
+            if (str.length() != 3) // 1. 3자릿수 이외의 다른 값이 입력되었을 경우
+                throw new IllegalArgumentException("3자리의 수가 입력되지 않았습니다. : " + str);
             else if(str.charAt(0) == '0' ||
                     str.charAt(1) == '0' ||
-                    str.charAt(2) == '0')
-                throw new IllegalArgumentException("사용자 입력값 오류 : " + str);
-            else if(countChar(str,str.charAt(0))>1 ||
-                    countChar(str,str.charAt(1))>1 ||
-                    countChar(str,str.charAt(2))>1)
-                throw new IllegalArgumentException("사용자 입력값 오류 : " + str);
+                    str.charAt(2) == '0') // 입력값에 0이 포함되었을 경우 (1부터 9 사이의 숫자범위)
+                throw new IllegalArgumentException("입력값에 0이 포합되어있습니다. : " + str);
+            else if(countNumber(str,str.charAt(0))>1 ||
+                    countNumber(str,str.charAt(1))>1 ||
+                    countNumber(str,str.charAt(2))>1) // 입력값에 중복된 숫자가 있을 경우
+                throw new IllegalArgumentException("입력값에 중복된 숫자가 포함되어 있습니다. : " + str);
 
 
-            List<Integer> user = new ArrayList<>();
+            List<Integer> user = new ArrayList<>(); // 사용자에게 입력받은 숫자를 분할하여 ArrayList에 저장
             for (int i = 0; i < str.length(); i++)
-                user.add(str.charAt(i) - '0'); // 사용자에게 입력받은 숫자를 분할하여 ArrayList에 저장
+                user.add(str.charAt(i) - '0');
 
             int ball=0, strike = 0;
 
             for(int i=0;i<3;i++){
                 int check = user.indexOf(computer.get(i));
-                if(check==-1)
+                if(check==-1) // 같은 수가 포함되어있지 않음
                     continue;
-                else if(check==i)
+                else if(check==i) // 같은 수가 같은 자리에 있음
                     strike++;
-                else
+                else // 같은 수가 다른 자리에 있음
                     ball++;
             }
 
-            if(strike==0 && ball==0)
+            if(strike==0 && ball==0) // ball, strike의 개수를 출력
                 System.out.print("낫싱");
             else{
                 if(ball!=0)
@@ -81,15 +76,15 @@ public class Application {
             }
             System.out.println();
 
-            if(strike==3){
+            if(strike==3){ // strike가 3이 될 경우 게임 종료
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 power = Integer.parseInt(readLine());
 
-                if(power!=1 && power!=2)
-                    throw new IllegalArgumentException("power 입력값 오류: " + power);
+                if(power!=1 && power!=2) // 1, 2 이외의 값이 입력되었을 경우 예외발생
+                    throw new IllegalArgumentException("1,2 이외의 숫자를 입력하였습니다. : " + power);
 
-                flag = false;
+                flag = false; // computer의 값을 초기화 하기 위해 flag를 false로 변경
                 }
             }
         }
