36460ae6d22dbfdc4cf634dd060979fa3f6dbc43
hansg931 <hansg0509@cau.ac.kr>
2023-10-25 12:22:18 +0900

    feat: 게임 실행 및 진행 메소드 추가

--- src/main/java/baseball/controller/GameManager.java
+++ src/main/java/baseball/controller/GameManager.java
@@ -1,4 +1,39 @@
 package baseball.controller;
 
+import baseball.model.Calculator;
+import baseball.model.NumberGenerator;
+import baseball.model.Score;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class GameManager {
+	private final InputView inputView = new InputView();
+	private final OutputView outputView = new OutputView();
+	private final NumberGenerator numberGenerator = new NumberGenerator();
+
+	public void initGame() {
+		try {
+			playGameUntilEnd();
+		} catch (IllegalArgumentException e) {
+			outputView.printInputErrorMessage();
+			throw e;
+		}
+	}
+
+	private void playGameUntilEnd() {
+		int gameRestartFlag;
+		do {
+			outputView.printGameStartMessage();
+			playGameUntilOver();
+			gameRestartFlag = inputView.gameRestartFlag();
+		} while (gameRestartFlag == 1);
+	}
+
+	private void playGameUntilOver() {
+		Score userScore;
+		do {
+			String userInput = inputView.userInputNumber();
+			userScore = Calculator.calculateScore(userInput, numberGenerator.generateNumber());
+		} while (!userScore.isGameOver());
+	}
 }
