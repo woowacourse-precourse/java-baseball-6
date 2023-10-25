821ed0c8f89ee28af185f56e2e2ab7d5bb9c4d47
HwangSeokJin <ghkd3703@gmail.com>
2023-10-25 12:02:16 +0900

    fix: 숫자의 개수 3으로 설정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,6 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         BaseballGame baseballGame = new BaseballGame();
-        baseballGame.play(4);
+        baseballGame.play(3);
     }
 }
