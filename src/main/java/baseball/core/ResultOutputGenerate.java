1f8e70383849e0b2f21cb0b52d455bd532907a6a
seonghoo1217 <seonghoo1217@naver.com>
2023-10-25 16:07:57 +0900

    refactor : result output generate logic on separate for consider scalability

--- src/main/java/baseball/core/ResultOutputGenerate.java
+++ src/main/java/baseball/core/ResultOutputGenerate.java
@@ -4,28 +4,35 @@ import baseball.view.OutputView;
 
 public class ResultOutputGenerate {
 
+    private static final String BALL = "볼";
+    private static final String STRIKE = "스트라이크";
+    private static final String NOTHING = "낫싱";
+    private static final String SUCCESS = "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+
+
     public static void generateAllResultTextForOutput(int ball_count, int strike_count){
-        String result = ball_count+"볼 "+strike_count+"스트라이크";
+        String result = ball_count+BALL+" "+strike_count+STRIKE;
         OutputView.outputForBallAndStrikeScore(result);
     }
 
     public static void generateBallResultTextForOutput(int ball_count){
-        String result = ball_count+"볼";
+        String result = ball_count+BALL;
         OutputView.outputForBallScore(result);
     }
 
     public static void generateStrikeResultTextForOutput(int strike_count){
-        String result = strike_count+"스트라이크";
+        String result = strike_count+STRIKE;
         OutputView.outputForStrikeScore(result);
     }
 
     public static void generateNothingResultTextForOutput(){
-        String result = "낫싱";
+        String result = NOTHING;
         OutputView.outputForNothingScore(result);
     }
 
     public static void generateSuccessResultTextForOutput(int strike_count){
-        String result = strike_count+"스트라이크\n"+strike_count+"개의 숫자를 모두 맞히셨습니다! 게임 종료";
+        String result = strike_count+STRIKE+"\n"+strike_count+SUCCESS;
         OutputView.outputForGameSuccess(result);
     }
 
