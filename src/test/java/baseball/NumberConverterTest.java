b08e7cb36d441c28b29a37839b17b90f5dd56693
siyeonSon <siyeon0209@naver.com>
2023-10-25 20:18:51 +0900

    refactor: assertEquals -> assertThat().isEqualTo()로 컨벤션 통일

--- src/test/java/baseball/NumberConverterTest.java
+++ src/test/java/baseball/NumberConverterTest.java
@@ -1,7 +1,6 @@
 package baseball;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
@@ -16,29 +15,33 @@ class NumberConverterTest {
 
     @Test
     void 문자인_경우_예외를_발생시킨다() {
-        Exception e = assertThrows(IllegalArgumentException.class, () ->
-                NumberConverter.from("일23"));
-        assertEquals(e.getMessage(), "문자가 아닌 숫자를 입력해주세요");
+        Exception e = assertThrows(IllegalArgumentException.class,
+                () -> NumberConverter.from("일23"));
+        assertThat(e.getMessage())
+                .isEqualTo("문자가 아닌 숫자를 입력해주세요");
     }
 
     @Test
     void 숫자의_수가_3개_이하일_경우_예외를_발생시킨다() {
-        Exception e = assertThrows(IllegalArgumentException.class, () ->
-                NumberConverter.from("1"));
-        assertEquals(e.getMessage(), "1부터 9까지 3개의 숫자를 입력해주세요");
+        Exception e = assertThrows(IllegalArgumentException.class,
+                () -> NumberConverter.from("1"));
+        assertThat(e.getMessage())
+                .isEqualTo("1부터 9까지 3개의 숫자를 입력해주세요");
     }
 
     @Test
     void 숫자가_0을_포함할_경우_예외를_발생시킨다() {
-        Exception e = assertThrows(IllegalArgumentException.class, () ->
-                NumberConverter.from("012"));
-        assertEquals(e.getMessage(), "1부터 9까지 3개의 숫자를 입력해주세요");
+        Exception e = assertThrows(IllegalArgumentException.class,
+                () -> NumberConverter.from("012"));
+        assertThat(e.getMessage())
+                .isEqualTo("1부터 9까지 3개의 숫자를 입력해주세요");
     }
 
     @Test
     void 중복되는_숫자가_존재할_경우_예외를_발생시킨다() {
-        Exception e = assertThrows(IllegalArgumentException.class, () ->
-                NumberConverter.from("112"));
-        assertEquals(e.getMessage(), "서로 다른 숫자들을 입력해주세요");
+        Exception e = assertThrows(IllegalArgumentException.class,
+                () -> NumberConverter.from("112"));
+        assertThat(e.getMessage())
+                .isEqualTo("서로 다른 숫자들을 입력해주세요");
     }
 }
