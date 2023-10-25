b3c0a3ad1aeb05856b1c1ffa983edb988aca2c9c
leedoyeon849 <do83430208@gmail.com>
2023-10-25 18:28:27 +0900

    fix: 볼, 스트라이크, 낫싱 출력문 로직 수정
    - 3 스트라이크일 시에도 출력

--- src/main/java/baseball/domain/BaseballGame.java
+++ src/main/java/baseball/domain/BaseballGame.java
@@ -21,35 +21,33 @@ public class BaseballGame implements Game {
         System.out.println("숫자 야구 게임을 시작합니다");
 
         Boolean exit = false;
-        do {
+        while(!exit) {
             String userInput = Console.readLine();
             user.setNumbers(userInput);
 
             Score score = this.judgmentPolicy.compareNumbers(computer.getNumbers(), user.getNumbers());
+            System.out.println(score.explainScore());
+
             if (score.getStrike() == 3) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 
-                if(!isRestartGame()) return;
-            } else {
-                System.out.println(score.explainScore());
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                String isEnd = Console.readLine();
+                if (!isRestartGame(isEnd)) return;
             }
+        }
 
-        } while(!exit);
-
-        System.out.println("숫자 야구 게임을 종료합니다.");
     }
 
 
 
-    private boolean isRestartGame() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-
-        String isRestart = Console.readLine();
-        if ("2".equals(isRestart)) {
-            return true;
-        } else if ("1".equals(isRestart)) {
+    private boolean isRestartGame(String input) {
+        if ("2".equals(input)) {
+            return false;
+        } else if ("1".equals(input)) {
             computer.createNumbers();
+            System.out.println("숫자 야구 게임을 시작합니다");
         }
-        return false;
+        return true;
     }
 }
