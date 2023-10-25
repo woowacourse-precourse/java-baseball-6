c797fb47e461e342c313cee1ada89b5d9fb0c895
김민주 <69795199+ks00919@users.noreply.github.com>
2023-10-25 22:25:12 +0900

    fix: 게임 시작 출력문 수정
    기존 게임이 끝나고 새로운 게임이 시작할때는 시작합니다. 문구가 출력되지 않도록 수정

--- src/main/java/baseball/BaseballConsole.java
+++ src/main/java/baseball/BaseballConsole.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class BaseballConsole {
 
     public static void startBaseballShell() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
         while (true) {
             startNewGame();
             if (isStop()) {
@@ -20,7 +21,6 @@ public class BaseballConsole {
     }
 
     private static void startNewGame() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
         BaseballGame game = new BaseballGame();
         while (true) {
             String answer = getInput("숫자를 입력해주세요 : ", "[1-9]{3}");
