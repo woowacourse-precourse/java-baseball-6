b35f65ba14a02770afb8b6e8409d56b3f98b30e1
JiwonChoi0805 <won0805@ewhain.net>
2023-10-25 21:07:59 +0900

    feat: 메인 명 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,7 @@
 package baseball;
 import gameRunner.GameMachine;
 
-public class Main {
+public class Application {
     public static void main(String[] args) {
         new GameMachine().runBaseBallGameOnce();
     }
