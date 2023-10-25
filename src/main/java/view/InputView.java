d7d687923a827ebef2b5fda1402ff98b930c49d6
yangyanghyunjung <hyunjungyang0922@gmail.com>
2023-10-25 20:52:05 +0900

    🚀refactor(view / InputView):입력값 validation 숫자 형식, 중복검사 기능으로 분리

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -10,6 +10,7 @@ public class InputView {
     private static final String INPUT_NUMBER= "숫자를 입력해주세요: ";
     private static final String CHOOES_RESTART_OR_EXIT= "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     public static final String CORRECT_3NUMBER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    static final String NUMBER_REGULAR_EXPRESSION = "^[1-9]{3}$";
     public static final int NUMBER_LENGTH = 3;
     List<Integer> userInputNumberList = new ArrayList<>();
 
@@ -30,25 +31,25 @@ public class InputView {
     }
 
     public List<Integer> AddNumber(String input) {
-        List<Integer> userNumbers = new ArrayList<>(); // refect 이때 초기화가 되니까 clear 안해도 됨
+        List<Integer> userNumbers = new ArrayList<>();
         for (int i = 0; i < NUMBER_LENGTH; i++) {
             int number = input.charAt(i) - '0';
-            // 여기에 중복검사 함수 호출
+            ValidateDuplicate(userNumbers, number);
             userNumbers.add(number);
         }
         return userNumbers;
     }
 
-    public  String ValidateInputNumber(String inputs) {
-        if (inputs.length() != 3) { // 3자리수 입력 안한 경우
-            throw new IllegalArgumentException();
+    public void ValidateDuplicate(List<Integer> userNumbers, int number) {
+        if (userNumbers.contains(number)) {
+            throw new IllegalArgumentException("중복된 숫자를 입력헸습니다.");
         }
-        try { // int로 변경 불가인 경우
-            Integer.parseInt(inputs);
-        } catch (IllegalArgumentException e){
-            throw e;
+    }
+
+    public void ValidateInputNumber(String inputs) {
+        if (!inputs.matches(NUMBER_REGULAR_EXPRESSION)) {
+            throw new IllegalArgumentException("3자리의 숫자를 입력해야 합니다.");
         }
-        return inputs;
     }
 
     public boolean ProgramReStartOrExit() {
