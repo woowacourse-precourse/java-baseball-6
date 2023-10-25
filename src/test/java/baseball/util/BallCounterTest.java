4122f24eec975ffe58fef32fc6fd9c15101d028d
siyeonSon <siyeon0209@naver.com>
2023-10-25 13:43:45 +0900

    test: 볼이 1개 존재한다

--- src/test/java/baseball/util/BallCounterTest.java
+++ src/test/java/baseball/util/BallCounterTest.java
@@ -13,4 +13,10 @@ class BallCounterTest {
         assertThat(ballCounter.count(List.of(1, 2, 3), List.of(7, 8, 9)))
                 .isEqualTo(0);
     }
+
+    @Test
+    void 볼이_1개_존재한다() {
+        assertThat(ballCounter.count(List.of(1, 2, 3), List.of(2, 8, 9)))
+                .isEqualTo(1);
+    }
 }
