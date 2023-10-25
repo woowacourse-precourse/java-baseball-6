f361ac7f47b93b9b9586c81c910b7497be7d0b9e
ImWonYong <ssassb123@daum.net>
2023-10-25 15:06:43 +0900

    feat: 숫자로 된 세 자리 수 입력받기. - 숫자 입력 받기 - 같은 숫자 있으면 예외 발생 - 숫자가 아닌 입력이 있으면 예외 발생

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,32 +1,69 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class BaseballGame {
+
+    private List<Integer> inputBalls;
 
     public BaseballGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
     public void playGame() {
+
         while (true) {
 
-            if (!playNextGameOrStop()) {
-                break;
-            }
+            receiveInput();
+
         }
     }
 
-    private boolean playNextGameOrStop() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        String input = Console.readLine();
-        if (input.equals("1")) {
-            return true;
-        } else if (input.equals("2")) {
-            return false;
+    private void receiveInput() {
+        System.out.print("숫자를 입력해주세요: ");
+        String balls = Console.readLine();
+        checkInput(balls);
+
+        makeInputBalls(balls);
+    }
+
+    private void checkInput(String balls) {
+        char[] ballsCharArray = balls.toCharArray();
+        for (char input : ballsCharArray) {
+            isInputNumeric(input);
         }
 
-        throw new IllegalArgumentException("잘못된 입력입니다.");
+        checkForDuplicateNumbers(ballsCharArray);
+    }
+
+    private void isInputNumeric(char input) {
+        if (!Character.isDigit(input)) {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+    }
+
+    private void checkForDuplicateNumbers(char[] ballsCharArray) {
+        Set<Character> set = new HashSet<>();
+        for (char input : ballsCharArray) {
+            set.add(input);
+        }
+
+        if (set.size() != ballsCharArray.length) {
+            throw new IllegalArgumentException("중복된 숫자가 입력되었습니다.");
+        }
+
+    }
+
+    private void makeInputBalls(String balls) {
+        inputBalls = new ArrayList<>();
+
+        for (int i = 0; i < balls.length(); i++) {
+            inputBalls.add(balls.charAt(i) - '0');
+        }
     }
 
 
