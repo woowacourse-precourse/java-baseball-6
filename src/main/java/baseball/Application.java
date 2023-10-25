377fbad85893c8226782173aedd856baccadb9a0
PJH <parkjh0858@gmail.com>
2023-10-25 14:55:50 +0900

    feat: 게임 플레이 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import Game.BaseballGame;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+import model.NumberInput;
+import view.InputView;
+import view.OutputView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,17 +15,20 @@ import java.util.Set;
 
 public class Application {
     public static void main(String[] args){
-        String test = Console.readLine();
-        System.out.println(test);
+        OutputView.startMessage();
+        do {
+            BaseballGame game = new BaseballGame();
+            game.start();
+        } while(isContinue());
 
     }
 
-    public int makeNumber() {
-        StringBuilder sb = new StringBuilder();
-        Set<Integer> set = new HashSet<>();
-        Console.readLine();
-
-        return 0;
+    public static boolean isContinue() {
+        int checkNumber = Integer.parseInt(InputView.setRetryInput());
+        if(checkNumber == 1) {
+            return true;
+        }
+        return false;
 
     }
 }
