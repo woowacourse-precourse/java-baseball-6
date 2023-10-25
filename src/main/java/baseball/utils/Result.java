058ec569de1e4ca95c43286ac0d67b6c06968cda
leedabin2 <12193152@inha.edu>
2023-10-25 16:57:24 +0900

    Style: Modify Result class

--- src/main/java/baseball/utils/Result.java
+++ src/main/java/baseball/utils/Result.java
@@ -4,14 +4,13 @@ public class Result {
 
     public static void printResult(int strikeCount, int ballCount) {
         if (strikeCount > 0 && ballCount > 0) {
-            System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+            System.out.printf("%d볼 %d스트라이크%n", ballCount, strikeCount);
         } else if (strikeCount > 0) {
-            System.out.println(strikeCount + "스트라이크");
+            System.out.printf("%d스트라이크%n", strikeCount);
         } else if (ballCount > 0) {
-            System.out.println(ballCount + "볼");
+            System.out.printf("%d볼%n", ballCount);
         } else {
             System.out.println("낫싱");
         }
-
     }
 }
