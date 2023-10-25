657eb5db0a732cee431d0824d146fc6f9874f75f
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-25 20:29:17 +0900

    [feat] : add computer class and random number method

---
+++ src/main/java/baseball/domain/Computer.java
@@ -0,0 +1,20 @@
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Computer {
+    private String randomNumber;
+    private static final int RANDOM_NUMBER_MAX_LENGTH = 3;
+
+    // 1~9 사이의 랜덤한 임의의 숫자 3개를 선택하는 메서드.
+    public void randomNumber() {
+        String str = "";
+        while (str.length() < RANDOM_NUMBER_MAX_LENGTH) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!str.contains(String.valueOf(randomNumber))) {
+                str += randomNumber;
+            }
+        }
+        this.randomNumber = str;
+    }
+}
