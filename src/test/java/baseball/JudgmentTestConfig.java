f19883bf828dde31aadd2aca90ac21f2b0922c9c
twoosky <lhjae3@naver.com>
2023-10-25 20:14:30 +0900

    test: result 테스트 작성

---
+++ src/test/java/baseball/JudgmentTestConfig.java
@@ -0,0 +1,37 @@
+package baseball;
+
+import baseball.domain.Judgment;
+
+public class JudgmentTestConfig {
+
+    public static class TestJudgment extends Judgment {
+        public static final int NUMBER_SIZE = 3;
+        private final int ball;
+        private final int strike;
+
+        public TestJudgment(int ball, int strike) {
+            this.ball = ball;
+            this.strike = strike;
+        }
+
+        public Boolean isBallAndStrike() {
+            return ball > 0 && strike > 0;
+        }
+
+        public Boolean isBall() {
+            return ball > 0 && strike == 0;
+        }
+
+        public Boolean isAllStrike() {
+            return ball == 0 && strike == NUMBER_SIZE;
+        }
+
+        public Boolean isStrike() {
+            return ball == 0 && strike > 0;
+        }
+
+        public Boolean isNothing() {
+            return ball == 0 && strike == 0;
+        }
+    }
+}
---
+++ src/test/java/baseball/ResultTest.java
@@ -0,0 +1,51 @@
+package baseball;
+
+import static baseball.domain.Result.ALL_STRIKE;
+import static baseball.domain.Result.BALL;
+import static baseball.domain.Result.BALL_STRIKE;
+import static baseball.domain.Result.NOTHING;
+import static baseball.domain.Result.STRIKE;
+import static baseball.utils.ErrorMessages.NOT_MATCH_BASEBALL_RESULT;
+
+import baseball.JudgmentTestConfig.TestJudgment;
+import baseball.domain.Judgment;
+import baseball.domain.Result;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+@DisplayName("Result Enum")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class ResultTest {
+
+    private static Stream<Arguments> provideBallAndStrikeCnt() {
+        return Stream.of(
+                Arguments.of(1, 1, BALL_STRIKE),
+                Arguments.of(3, 0, BALL),
+                Arguments.of(0, 3, ALL_STRIKE),
+                Arguments.of(0, 2, STRIKE),
+                Arguments.of(0, 0, NOTHING)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideBallAndStrikeCnt")
+    void of_메서드는_조건에_맞는_결과값이_존재한다면_Result를_반환한다(int ball, int strike, Result expect) {
+        Judgment judgment = new TestJudgment(ball, strike);
+        Assertions.assertEquals(Result.of(judgment), expect);
+    }
+
+    @Test
+    void of_메서드는_조건에_맞는_결과값이_존재하지_않으면_예외를_던진다() {
+        Judgment judgment = new TestJudgment(-1, 0);
+        org.assertj.core.api.Assertions.assertThatThrownBy(() -> Result.of(judgment))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(NOT_MATCH_BASEBALL_RESULT);
+    }
+}
