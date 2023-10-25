a1990a52b40e2dff469d53447f426f05add86d81
hansg931 <hansg0509@cau.ac.kr>
2023-10-25 20:33:12 +0900

    feat: 숫자 관리 클래스 분리 및 메소드 추가

---
+++ src/main/java/baseball/model/ListNumber.java
@@ -0,0 +1,16 @@
+package baseball.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListNumber {
+	private final List<Integer> baseballNumber;
+
+	public ListNumber(int number) {
+		baseballNumber = new ArrayList<>();
+		String inputNumber = Integer.toString(number);
+		for (int i = 0; i < inputNumber.length(); i++) {
+			baseballNumber.add(Integer.parseInt(String.valueOf(inputNumber.charAt(i))));
+		}
+	}
+}
