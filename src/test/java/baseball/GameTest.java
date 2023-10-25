b2274d19b2d85c0141ae919a47565a14cfff33c2
siyeonSon <siyeon0209@naver.com>
2023-10-25 13:15:20 +0900

    test: final 키워드 사용

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -4,18 +4,11 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GameTest {
-    private Game game;
-    private List<Integer> number;
-
-    @BeforeEach
-    public void setUp() {
-        game = new Game();
-        number = game.getComputer();
-    }
+    final Game game = new Game();
+    final List<Integer> number = game.getComputer();
 
     @Test
     void 세자리의_수를_생성한다() {
