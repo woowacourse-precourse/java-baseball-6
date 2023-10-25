265ef59c0f6e66ea199dda57f77c07732b3bc999
kkonii <119216472+kkonii@users.noreply.github.com>
2023-10-25 22:46:23 +0900

    Refactor(game) : 게임 진행 가이드 메세지를 상수로 치환

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -4,9 +4,13 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class Game {
+    private final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private final String INPUT_REQUEST_MESSAGE = "숫자를 입력해주세요 : ";
+    private final String GAME_EXIT_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private final String RESTART_INPUT_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
     private Result result = new Result();
     private Validator validator = new Validator();
-
     private List<Character> randomNumbers;
 
     public void game() {
@@ -16,13 +20,13 @@ public class Game {
     }
 
     private void start() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println(GAME_START_MESSAGE);
 
         resetRandomNumbers();
     }
 
     private void play() {
-        System.out.print("숫자를 입력해주세요 : ");
+        System.out.print(INPUT_REQUEST_MESSAGE);
 
         String userInput = Console.readLine();
 
@@ -40,9 +44,9 @@ public class Game {
     }
 
     private void end() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println(GAME_EXIT_MESSAGE);
 
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println(RESTART_INPUT_MESSAGE);
 
         String restartInput = Console.readLine();
 
