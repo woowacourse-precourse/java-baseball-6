c2f7dd2a5f26ea6205b36360261b3705b33f2782
Yang woosung <ngxk1217@gmail.com>
2023-10-25 14:16:48 +0900

    세자리 숫자 생성 클래스

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,21 +5,7 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-            GenerateRandomNum randomNum = new GenerateRandomNum();
-            Input input = new Input();
-            Judge judge = new Judge();
-            Playagain playagain = new Playagain();
-            boolean again = true;
 
-            while(again){
-                List<Integer> computer = randomNum.create();
-                String result ="";
-                while(!result.equals("3스트라이크")){
-                    result = judge.judgement(computer,input.playerNumber());
-                    System.out.println(result);
-                }
-                again = playagain.playagain();
-            }
 
 
     }
