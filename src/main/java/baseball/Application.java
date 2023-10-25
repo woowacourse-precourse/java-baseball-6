15e91d59d158d0e288611611fc03efdca8166ddd
cozym <ddongyee99@daum.net>
2023-10-25 16:46:51 +0900

    function complete

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,10 @@
 package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
-import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 public class Application {
     public static void main(String[] args) throws IllegalArgumentException{
-        // TODO: 프로그램 구현
         List<Integer> computer = new ArrayList<>();
         String user_input = "";
         Scanner sc = new Scanner(System.in);
@@ -25,7 +23,7 @@ public class Application {
                     computer.add(randomNumber);
                 }
             }
-            //System.out.println(computer);
+
             while (true) {
                 strike = 0;
                 ball = 0;
@@ -63,18 +61,7 @@ public class Application {
                 }
             }
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            terminate = sc.nextInt();
+            terminate = sc.nextInt();   // 2를 입력 시 종료
         } while (terminate != 2);
     }
-
-    /**
-     * 랜덤 값 생성
-     * 입력값 검사
-     * 입력값 리스트 인덱스 단위 검사
-     * 생성된 값 내에 입력값이 있을경우 인덱스 까지 검사
-     * 인덱스까지 일치할 경우 strike+, 인덱스 일치하지 않을 경우 ball+
-     * 마지막 문자까지 없으면 낫싱
-     * 입력값이 잘못되거나 정답이 나올 때 까지 반복
-     * 정답이 나온 경우 1또는 2로 재시작/종료 결정
-     */
 }
