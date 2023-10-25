6e4224fd363d064d529e797c4c488dbe4d9ba5f9
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-10-25 23:33:19 +0900

    feat, refactor : view클래스 static으로 수정 및 재입력 기능 구현, 종료기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,22 +1,33 @@
 package baseball.view;
 
+import baseball.Judgement;
+
 public class OutputView {
 
-    private OutputView(){}
+    private OutputView() {
+    }
 
-    public static void printStartPhrase(){
+    public static void printStartPhrase() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    public static void printNumberInputPhrase(){
+    public static void printNumberInputPhrase() {
         System.out.print("숫자를 입력해주세요 : ");
     }
 
-    public static void printExitOptionPhrase(){
+    public static void printExitOptionPhrase() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
-    public static void printExitPhrase(){
+    public static void printExitPhrase() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public static void printJudgeState() {
+        System.out.println(Judgement.judgeState());
+    }
+
+    public static void printExit() {
+        System.out.println("게임을 완전히 종료합니다.");
     }
 }
