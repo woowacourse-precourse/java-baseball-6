7c5e5618084a7951e7e9ee9d28cca558689c43fa
giwoong <dev.choi0409@gmail.com>
2023-10-25 12:48:44 +0900

    feat: OutputView 구현

--- src/main/java/baseball/controller/GameMachine.java
+++ src/main/java/baseball/controller/GameMachine.java
@@ -3,32 +3,33 @@ package baseball.controller;
 import baseball.domain.BaseballReferee;
 import baseball.domain.ComputerNumber;
 import baseball.view.InputView;
-
+import baseball.view.OutputView;
 import java.util.List;
 
 public class GameMachine {
 
-    public static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
-
     private final InputView inputView;
+    private final OutputView outputView;
     private final BaseballReferee baseballReferee;
 
     public GameMachine() {
         inputView = new InputView();
+        outputView = new OutputView();
         baseballReferee = new BaseballReferee();
     }
 
     public void start() {
-        System.out.println(GAME_START_MESSAGE);
+        outputView.gameStartMessage();
 
         ComputerNumber computerNumber = new ComputerNumber();
         List<Integer> randomNumber = computerNumber.getComputer();
 
         while (true) {
+            outputView.inputNumberMessage();
             List<Integer> inputNumber = inputView.inputNumber();
 
-            System.out.println(baseballReferee.referee(randomNumber, inputNumber));
-            
+            outputView.refereeMessage(baseballReferee.referee(randomNumber, inputNumber));
+
             if (randomNumber.equals(inputNumber)) {
                 break;
             }
@@ -38,6 +39,7 @@ public class GameMachine {
     }
 
     private void restart() {
+        outputView.gameRestartMessage();
         if (inputView.restartCheck()) {
             start();
         }
--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,24 +1,18 @@
 package baseball.view;
 
 import camp.nextstep.edu.missionutils.Console;
-
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class InputView {
-
-    public static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
-    public static final String END_GAME_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    public static final String RE_GAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     public static final int INPUT_NUMBER_SIZE = 3;
     public static final int GAME_RESTART_NUMBER = 1;
 
     private List<Integer> inputNumber = new ArrayList<>();
 
     public List<Integer> inputNumber() {
-        System.out.print(INPUT_NUMBER_MESSAGE);
         inputNumber = integerNumber(List.of(Console.readLine().split("")));
         validationNotSize();
 
@@ -50,9 +44,6 @@ public class InputView {
     }
 
     public boolean restartCheck() {
-        System.out.println(END_GAME_MESSAGE);
-        System.out.println(RE_GAME_MESSAGE);
-
         return Integer.parseInt(Console.readLine()) == GAME_RESTART_NUMBER;
     }
 }
