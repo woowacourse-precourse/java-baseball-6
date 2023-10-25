a7e128f3c574a19ed3d5df3ed042b53be9411c55
kdomo <dongho5088@naver.com>
2023-10-25 21:49:20 +0900

    feat: BaseBallGame Class 에 상수 추가

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -4,8 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class BaseBallGame {
     private static final int INPUT_LENGTH = 3;
+    private static final String STRIKE_MESSAGE = "스트라이크";
+    private static final String BALL_MESSAGE = "볼";
+    private static final String NOTHING_MESSAGE = "낫싱";
+    private static final String QUIT_MESSAGE = "게임 종료";
     private static final String RESTART_NUMBER = "1";
     private static final String QUIT_NUMBER = "2";
+    private static final String WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String EXCEPTION_MESSAGE = "1 또는 2를 입력하세요.";
+    private static final String RETRY_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     private Computer computer = new Computer();
     private User user = new User();
