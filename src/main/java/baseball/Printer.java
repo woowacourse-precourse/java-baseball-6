03f896bb03aa16625cb8382644c3d4ff31b3f926
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-10-25 11:01:15 +0900

    refactor: static 메서드 최소화

--- src/main/java/baseball/Printer.java
+++ src/main/java/baseball/Printer.java
@@ -1,33 +1,41 @@
 package baseball;
 
 public class Printer {
+
     public static void print(String message) {
         System.out.print(message);
+    }
+    public static void println() {
+        System.out.println();
+    }
+    public static void println(String message) {
+        System.out.println(message);
     }
 
     public static void printRoundResult(Result result) {
         int strike = result.strike();
         int ball = result.ball();
 
+        printNotThing(result);
         printBall(ball);
         printStrike(strike);
-        printNotThing(result);
     }
 
     private static void printNotThing(Result result) {
         if (result.ball() == 0 && result.strike() == 0) {
-            System.out.println("낫싱");
+            println("낫싱");
         }
     }
 
     private static void printBall(int ball) {
         if (ball > 0) {
-            System.out.print(ball + "볼 ");
+            print(ball + "볼 ");
         }
     }
     private static void printStrike(int strike) {
         if (strike > 0) {
-            System.out.println(strike + "스트라이크");
+            print(strike + "스트라이크");
         }
+        println();
     }
 }
