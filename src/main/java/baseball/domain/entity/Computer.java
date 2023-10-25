0c47fd18dbe72eed8ce087ffef111ad5dc87d1eb
mingeun0507 <tqdlfkg@naver.com>
2023-10-25 13:36:28 +0900

    refactor: Computer 엔티티 record에서 클래스로 변경, 도메인 로직을 처리하는 메소드 구현

--- src/main/java/baseball/domain/entity/Computer.java
+++ src/main/java/baseball/domain/entity/Computer.java
@@ -1,7 +1,26 @@
 package baseball.domain.entity;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.List;
 
-public record Computer(List<Integer> answer) {
+public final class Computer {
+    private List<Integer> answer;
+
+    public Computer() {
+        this.answer = generateNumbersForBaseball();
+    }
+
+    public List<Integer> getAnswer() {
+        return answer;
+    }
+
+    public void resetAnswer() {
+        this.answer = generateNumbersForBaseball();
+    }
+
+    private List<Integer> generateNumbersForBaseball() {
+        return Randoms.pickUniqueNumbersInRange(1, 9, 3);
+    }
 
 }
