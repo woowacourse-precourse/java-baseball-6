edfa37a9cf5f92660c56aa869576d37ffdf9d55c
IDpluto <d0013159@gmail.com>
2023-10-25 20:54:09 +0900

    feat printInputMessage method add

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -24,6 +24,10 @@ public class OutputView {
     System.out.println("오류: " + message);
   }
 
+  public static void printInputMessage() {
+    System.out.print("숫자를 입력해주세요: ");
+  }
+
   public static void printAskPlayAgain() {
     System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
   }
