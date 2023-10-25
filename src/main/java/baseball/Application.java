46319a7e98d588ca5e149ab6b0146fd824ab7d95
mango <esudatcom@gmail.com>
2023-10-25 11:48:42 +0900

    refactor: 상수 추가 및 메소드 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,6 +6,9 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Arrays;
 
 public class Application {
+
+    private static final int NUMBER_COUNT = 3;
+
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
 
@@ -14,8 +17,7 @@ public class Application {
             playGame(computerNumbers);
 
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            String choice = Console.readLine();
-            if ("2".equals(choice)) {
+            if ("2".equals(Console.readLine())) {
                 break;
             }
         }
@@ -23,8 +25,8 @@ public class Application {
 
     // 컴퓨터 랜덤 숫자 3개 생성
     private static int[] generateComputerNumbers() {
-        int[] numbers = new int[3];
-        for (int i = 0; i < 3; i++) {
+        int[] numbers = new int[NUMBER_COUNT];
+        for (int i = 0; i < NUMBER_COUNT; i++) {
             int number;
             do {
                 number = Randoms.pickNumberInRange(1, 9);
@@ -47,23 +49,26 @@ public class Application {
     // 게임 진행
     private static void playGame(int[] computerNumbers) {
         while (true) {
-        // 숫자 입력받기
-        System.out.print("숫자를 입력해주세요 : ");
-        String input = Console.readLine();
+            int[] userNumbers = getUserInput();
+            int strikes = countStrikes(computerNumbers, userNumbers);
+            int balls = countBalls(computerNumbers, userNumbers, strikes);
 
+            if (strikes == NUMBER_COUNT) {
+                System.out.printf("%d스트라이크\n", strikes);
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                return;
+            }
+            printResult(strikes, balls);
+        }
+    }
+
+    // 숫자 입력받기
+    private static int[] getUserInput() {
+        while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
+            String input = Console.readLine();
             if (isValid(input)) {
-                int[] userNumbers = parseInput(input);
-                int strikes = countStrikes(computerNumbers, userNumbers);
-                int balls = countBalls(computerNumbers, userNumbers, strikes);
-
-                if (strikes == 3) {
-                    System.out.printf("%d스트라이크\n", strikes);
-                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                    return;
-                }
-
-                printResult(strikes, balls);
-
+                return parseInput(input);
             } else {
                 throw new IllegalArgumentException("잘못된 입력입니다.");
             }
@@ -72,12 +77,16 @@ public class Application {
 
     // 사용자가 입력한 값 유효성 확인
     private static boolean isValid(String input) {
-        // 3자리 수이며, 1~9까지의 숫자만 포함
-        if (!input.matches("^[1-9]{3}$")) {
-            return false;
-        }
+        return isThreeDigits(input) && hasNoDuplicateCharacters(input);
+    }
 
-        // 중복된 숫자가 없는지 확인
+    // 1 - 9 까지의 숫자이며 3자리 수인지 확인
+    private static boolean isThreeDigits(String input) {
+        return input.matches("^[1-9]{3}$");
+    }
+
+    // 중복된 숫자가 없는지 확인
+    private static boolean hasNoDuplicateCharacters(String input) {
         for (int i = 0; i < input.length(); i++) {
             for (int j = i + 1; j < input.length(); j++) {
                 if (input.charAt(i) == input.charAt(j)) {
