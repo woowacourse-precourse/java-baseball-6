081c88b2d3e0a367635c70c45b5a1b9772a1c00d
Seungjida <esg0107@pusan.ac.kr>
2023-10-25 18:56:41 +0900

    Test: "ExceptionHandling class의 예측 숫자 입력에 관한 Test 추가"

---
+++ src/test/java/baseball/others/ExceptionHandlingTest.java
@@ -0,0 +1,58 @@
+package baseball.others;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class ExceptionHandlingTest {
+    @DisplayName("Null값 예외 처리 테스트")
+    @Test
+    public void isNull_Test() {
+        // Given
+        String input = "";
+
+        // When and Then
+        assertThrows(IllegalArgumentException.class, () -> {
+            ExceptionHandling.isNull(input);
+        });
+    }
+
+    @DisplayName("3개의 값이 들어오지 않았을 때 예외 처리 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "56", "9782"})
+    public void isThree_Test(String input) {
+        // Given (provided by ValueSource)
+
+        // When and Then
+        assertThrows(IllegalArgumentException.class, () -> {
+            ExceptionHandling.isThree(input);
+        });
+    }
+
+    @DisplayName("1~9 이외의 값 예외 처리 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"12A", "ABC", "9h4", "015"})
+    public void isDigits_Test(String input) {
+        // Given (provided by ValueSource)
+
+        // When and Then
+        assertThrows(IllegalArgumentException.class, () -> {
+            ExceptionHandling.isDigits(input);
+        });
+    }
+
+    @DisplayName("중복 예외 처리 테스트")
+    @Test
+    public void isDifferent_Test() {
+        // Given
+        String input = "112";
+
+        // When and Then
+        assertThrows(IllegalArgumentException.class, () -> {
+            ExceptionHandling.isDifferent(input);
+        });
+    }
+}
