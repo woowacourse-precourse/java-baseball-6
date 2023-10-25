389c86841a8f27ff345639d92597af4fcdeae445
Yoo Hyunseok <brianyhs@naver.com>
2023-10-25 15:15:18 +0900

    feat: 숫자를 검증하는 InputValidator

---
+++ src/main/java/baseball/InputValidator.java
@@ -0,0 +1,30 @@
+package baseball;
+
+public class InputValidator {
+    private static InputValidator instance;
+
+    private InputValidator() {
+
+    }
+
+    public static InputValidator getInstance() {
+        if (instance == null) {
+            instance = new InputValidator();
+        }
+        return instance;
+    }
+
+    // validate 3-digit
+    public void validateThreeDigit(int inputNumber) throws IllegalArgumentException{
+        if (inputNumber < 100 || inputNumber > 999) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void validateEndInput(int inputNumber) throws IllegalArgumentException {
+        if (inputNumber != 1 && inputNumber != 2) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+}
