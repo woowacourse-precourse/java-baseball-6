490eb02029442c4b338c0a6a530d50b5d91cc340
권태현 <kygkth2011@gmail.com>
2023-10-25 20:02:11 +0900

    feat(UiGenerator) : Ui 프린트 메소드 구현

---
+++ src/main/java/baseball/UiGenerator.java
@@ -0,0 +1,29 @@
+package baseball;
+
+public class UiGenerator {
+    public void printGameStart() {
+        System.out.println("게임을 시작합니다.");
+    }
+
+    public void printRequestUserPrediction() {
+        System.out.print("숫자를 입력해주세요.");
+    }
+
+    public void printEndCurrentGameAndAskNewGame() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    public void printStrikeBall(int strike, int ball) {
+        if (strike + ball == 0) {
+            System.out.println("낫싱");
+            return;
+        }
+        if (ball > 0) {
+            System.out.print(ball + "볼 ");
+        }
+        if (strike > 0) {
+            System.out.print(strike + "스트라이크 ");
+        }
+        System.out.println();
+    }
+}
