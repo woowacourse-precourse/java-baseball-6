c1f776de884001a9ef99c552bf766314fe0d4b72
peeaaccee <wlgp8958@naver.com>
2023-10-25 19:54:10 +0900

    feat(Game): 게임 진행 뼈대 만들기

--- src/main/java/baseball/domain/controller/Game.java
+++ src/main/java/baseball/domain/controller/Game.java
@@ -1,5 +1,9 @@
 package baseball.domain.controller;
 
+import baseball.domain.model.BallGenerator;
+
+import java.util.List;
+
 public class Game {
     public static final String INIT_GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     public static final String RESTART_SIGNAL = "1";
@@ -15,4 +19,22 @@ public class Game {
     public static Game getInstance() {
         return GameHolder.INSTANCE;
     }
+
+    public void start() {
+        System.out.println(INIT_GAME_START_MESSAGE);
+
+        BallGenerator ballGenerator = BallGenerator.getInstance();
+
+        boolean isProgress = true;
+        while (isProgress) {
+            List<String> computerBalls = ballGenerator.makeComputerBalls();
+            GameController gameController = new GameController(computerBalls);
+
+            playBaseball(gameController);
+        }
+    }
+
+    private void playBaseball(GameController gameController) {
+    }
+
 }
