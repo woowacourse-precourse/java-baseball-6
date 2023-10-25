93181f024249d618dc95561fc8caf46bc9a1d71a
meridaKim <sujin7187@naver.com>
2023-10-25 20:50:11 +0900

    feat : main for running application

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,7 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.stream.Stream;
 
 public class Application {
 
@@ -11,9 +12,24 @@ public class Application {
         System.out.println("숫자 야구 게임을 시작합니다.");
         Player player = new Player(Integer.parseInt(Console.readLine()));
         player.validateNumber(player);
-        Computer computer = new Computer(Randoms.pickNumberInRange(1, 9) * 100 + Randoms.pickNumberInRange(1, 9) * 10 + Randoms.pickNumberInRange(1, 9));
-        System.out.println("player num: "+player.player_num);
-        System.out.println("computer num: "+computer.computer_num);
+        int[] player_numbers = Stream.of(String.valueOf(player.player_num).split("")).mapToInt(Integer::parseInt)
+                .toArray();
+        player.checkDuplicates(player_numbers);
+        int firstNumber = Randoms.pickNumberInRange(1, 9);
+        int secondNumber = Randoms.pickNumberInRange(1, 9);
+        int thirdNumber = Randoms.pickNumberInRange(1, 9);
+        Computer computer = new Computer(firstNumber, secondNumber
+                , thirdNumber);
+        int[] computer_numbers = Stream.of(
+                        String.valueOf(computer.createNumber(firstNumber, secondNumber, thirdNumber)).split(""))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        Controller controller = new Controller();
+        controller.checkStrike(player_numbers, computer_numbers);
+        controller.checkNothing(player_numbers, computer_numbers);
+        controller.checkBall(player_numbers, computer_numbers);
+        System.out.println("player num: " + player.player_num);
+        System.out.println("computer num: " + computer.createNumber(firstNumber, secondNumber, thirdNumber));
 
 
     }
