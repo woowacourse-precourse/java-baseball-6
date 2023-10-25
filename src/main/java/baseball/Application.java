cbec8ba8e35d9f14126ab7d083f9eb4e0feefa6e
ohjinseo <ojs835@naver.com>
2023-10-25 23:12:53 +0900

    feat : 랜덤 숫자 생성 로직 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,29 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    private static final int NUM_DIGITS = 3;
+    private static final int MAX_NUMBER = 9;
+    private static final int MIN_NUMBER = 1;
+
+    private static List<Integer> generateRandomNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < NUM_DIGITS) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+        return numbers;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
 }
