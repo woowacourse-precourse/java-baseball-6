cf1034ac9f8951565962ed364e90fa91139aba71
carpediem365 <k2309123@gmail.com>
2023-10-25 15:19:22 +0900

    랜덤한 3자리 숫자를 생성하는 함수

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    /**
+    랜덤한 3자리 숫자를 생성하는 함수
+    */
+    List<Integer> comRandomNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while(computer.size()<3){
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if(!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        
     }
 }
