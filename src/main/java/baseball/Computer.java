397c7c9a97439f533acd8bd2ef485b685c2d23e8
sayishuu <smile52673@gmail.com>
2023-10-25 23:37:08 +0900

    feat : Computer 클래스 추가

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,32 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+public class Computer {
+    private static final int MIN_NUM = 1;
+    private static final int MAX_NUM = 9;
+
+    private int[] computerNumbers;
+
+    public Computer() { //컴퓨터 숫자 초기화
+        this.computerNumbers = new int[3];
+        setComputerNumbers();
+    }
+    public int[] getComputerNumbers() {
+        return this.computerNumbers;
+    }
+
+    private void setComputerNumbers() {
+        for (int i = 0; i < 3; i++) {
+            HashSet<Integer> numbers = new LinkedHashSet<>();
+            while (numbers.size() < 3) {
+                int num = Randoms.pickNumberInRange(MIN_NUM, MAX_NUM);
+                numbers.add(num);
+            }
+            computerNumbers = numbers.stream().mapToInt(Integer::intValue).toArray();
+        }
+    }
+}
