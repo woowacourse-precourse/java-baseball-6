30e21430a3e9d47d948f71c13ddc2aad29033155
siyeonSon <siyeon0209@naver.com>
2023-10-25 13:43:07 +0900

    test: 볼이 없다

---
+++ src/test/java/baseball/util/BallCounterTest.java
@@ -0,0 +1,16 @@
+package baseball.util;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class BallCounterTest {
+    final Counter ballCounter = new BallCounter();
+
+    @Test
+    void 볼이_없다() {
+        assertThat(ballCounter.count(List.of(1, 2, 3), List.of(7, 8, 9)))
+                .isEqualTo(0);
+    }
+}
