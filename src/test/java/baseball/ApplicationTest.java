0fdbbb92fca46dd4a80055505829cbfab6ba7e4c
haebyun <bhk4392@gmail.com>
2023-10-25 20:37:32 +0900

    test: 플레이어의 입력이 볼만 존재하는 경우 볼의 카운트만 출력하는 지 테스트

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,12 +1,13 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -17,6 +18,18 @@ class ApplicationTest extends NsTest {
                     assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
                 },
                 1, 3, 5, 5, 8, 9
+        );
+    }
+
+    @Test
+    @DisplayName("볼만 존재하는 경우 볼만 출력")
+    void onlyBallCountTest() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("513", "351", "135", "2");
+                    assertThat(output()).contains("3볼", "3볼", "3스트라이크", "게임 종료");
+                },
+                1, 3, 5
         );
     }
 
