1e1d7e2c883dc769e91220b6c03de93eba6d593b
doka <posmane5@gmail.com>
2023-10-25 14:27:56 +0900

    feat(Confirm) : Confirm 클래스의 checkPositionMatch 메소드 추가 자리 일치 갯수 반환 메소드인 Confirm클래스의 checkPositionMatch메소드 추가

--- src/main/java/baseball/feature/Confirm.java
+++ src/main/java/baseball/feature/Confirm.java
@@ -5,18 +5,21 @@ import java.util.*;
 public class Confirm {
 
     int accordCount;
+    int positionMatchCount;
     List<Integer> userNumbers;
     List<Integer> computerNumbers;
 
-    Confirm(List<Integer> UserNumbers, List<Integer> ComputerNumbers) {
+    public Confirm(List<Integer> UserNumbers, List<Integer> ComputerNumbers) {
         userNumbers = UserNumbers;
         computerNumbers = ComputerNumbers;
     }
 
     // 스트라이크 ⊂ 볼
-    // 따라서 일치갯수(볼) 먼저 구한다. accordNumber
-    // 그리고 일치갯수중 자리까지 일치하는 경우는 스트라이크
+    // 따라서 일치갯수(볼) 먼저 구한다. - accordNumber
+    // 그리고 일치갯수중 자리까지 일치하는 경우는 스트라이크 - checkPositionMatch
     // 일치갯수중 스트라이크를 제외하면 볼의 수
+
+    //일치 하는 숫자의 갯수 찾기
     public int accordNumber() {
         int count = 0;
         for (int num : userNumbers) {
@@ -28,6 +31,25 @@ public class Confirm {
         return count;
     }
 
+    //자리와 숫자가 일치 하는 갯수 찾기
+    public int checkPositionMatch() {
+        int count = 0;
+        for (int i = 0; i < computerNumbers.size(); i++) {
+            if (computerNumbers.get(i) == userNumbers.get(i)) {
+                count++;
+            }
+
+        }
+        positionMatchCount = count;
+        return count;
+
+
+    }
+
+    // 볼, 스트라이크, 낫싱 판별
+    public String referee() {
+        return null;
+    }
 
 
 }
