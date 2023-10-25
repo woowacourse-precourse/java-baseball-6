9cf93917c0bbfe8c8e6a4a5fdffc72c60ac10dac
iceprins <junghankyul@gmail.com>
2023-10-25 22:26:34 +0900

    game 실행 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,10 @@
 package baseball;
 
+import baseball.control.GameControl;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameControl gameControl = new GameControl();
+        gameControl.startGame();
     }
 }
