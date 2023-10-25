498916693a3858a61bb4a010e679ab469c7c7d95
mary <jogrowing@gmail.com>
2023-10-25 19:51:32 +0900

    feat: 사용자에게 3자리 숫자 입력 요청

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,17 +1,29 @@
 package baseball.controller;
 
 import baseball.model.Computer;
+import baseball.view.InputView;
 import baseball.view.OutputView;
+import java.util.List;
 
 public class GameController {
+    private final InputView inputView;
     private final OutputView outputView;
 
-    public GameController(OutputView outputView) {
+    public GameController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
         this.outputView = outputView;
     }
+
 
     public void startGame() {
         outputView.printStart();
         Computer computer = new Computer();
+
+        List<Integer> userGuess = guessUser();
+    }
+
+    private List<Integer> guessUser() {
+        outputView.printUserGuess();
+        return inputView.readUserGuess();
     }
 }
