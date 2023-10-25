5b891f6bdf7a41e52e0af74cd190d9f55c3778aa
mr-silvester <126128590+mr-silvester@users.noreply.github.com>
2023-10-25 16:10:16 +0900

    update: 불필요한 주석 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,7 +3,6 @@ package baseball;
 import baseball.container.ApplicationContainer;
 
 public class Application {
-    /* Variable */
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         new ApplicationContainer().getGameController().gameStart();
