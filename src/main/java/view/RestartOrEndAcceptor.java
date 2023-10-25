bac118b44700028bf423f5a3e81fec25c21d71c1
JiEung2 <https://github.com/JiEung2>
2023-10-25 18:10:47 +0900

    패키지 변경, 클래스명 변경 및 필요없는 import 삭제

--- src/main/java/view/RestartOrEndAcceptor.java
+++ src/main/java/view/RestartOrEndAcceptor.java
@@ -1,11 +1,8 @@
-package controller;
+package view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class InputReOrEnd {
+public class RestartOrEndAcceptor {
     /**
      * 사용자가 게임을 재시작할지 종료할지를 코인을 넣는다는 것으로 표현해봄
      * 코인을 입력하는 기능 구현
