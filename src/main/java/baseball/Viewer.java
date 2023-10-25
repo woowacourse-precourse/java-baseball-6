9b4fbb790b2801ab7b77a077d596e53d79b1d860
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-10-25 14:25:41 +0900

    fix: 입력값이 숫자가 아닐 경우 NumberFormatException 처리

--- src/main/java/baseball/Viewer.java
+++ src/main/java/baseball/Viewer.java
@@ -4,11 +4,21 @@ package baseball;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Viewer {
+    static final String INPUT_ONLY_NUMBER = "숫자만 입력해주세요.";
     public static int readNumber() {
-        return Integer.parseInt(readLine());
+        try {
+            return Integer.parseInt(readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INPUT_ONLY_NUMBER);
+        }
     }
 
     public static String readNumbers() {
-        return readLine();
+        try {
+             int numbers = Integer.parseInt(readLine());
+             return String.valueOf(numbers);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INPUT_ONLY_NUMBER);
+        }
     }
 }
