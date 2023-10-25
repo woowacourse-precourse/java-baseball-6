b488d67f94d5f321ee670f75c7285308a911e8d8
siyeonSon <siyeon0209@naver.com>
2023-10-25 17:24:33 +0900

    refactor: enum 대신 static final 필드 사용

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -1,10 +1,5 @@
 package baseball;
 
-import static baseball.GameState.BALL;
-import static baseball.GameState.END_GAME;
-import static baseball.GameState.NOTHING;
-import static baseball.GameState.STRIKE;
-
 import baseball.util.BallCounter;
 import baseball.util.Counter;
 import baseball.util.StrikeCounter;
@@ -14,6 +9,12 @@ public class Referee {
     private final Counter ballCounter = new BallCounter();
     private final Counter strikeCounter = new StrikeCounter();
 
+    private static final String SINGLE_BLANK = " ";
+    private static final String BALL = "볼";
+    private static final String STRIKE = "스트라이크";
+    private static final String NOTHING = "낫싱";
+    private static final String END_GAME = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
     public boolean judge(List<Integer> computer, List<Integer> number) {
         int ballCount = ballCounter.count(computer, number);
         int strikeCount = strikeCounter.count(computer, number);
@@ -22,19 +23,19 @@ public class Referee {
 
     private boolean say(int ballCount, int strikeCount) {
         if (ballCount > 0) {
-            System.out.print(ballCount + BALL.getMessage() + " ");
+            System.out.print(ballCount + BALL + SINGLE_BLANK);
         }
         if (ballCount > 0 && strikeCount == 0) {
             System.out.println();
         }
         if (strikeCount > 0) {
-            System.out.println(strikeCount + STRIKE.getMessage());
+            System.out.println(strikeCount + STRIKE);
         }
         if (ballCount == 0 && strikeCount == 0) {
-            System.out.println(NOTHING.getMessage());
+            System.out.println(NOTHING);
         }
         if (strikeCount == 3) {
-            System.out.println(END_GAME.getMessage());
+            System.out.println(END_GAME);
             return true;
         }
 
