a07ab65db2e4024ba712732ba090e39e00b07a3d
세탁기 <116627038+saetakki@users.noreply.github.com>
2023-10-25 21:06:13 +0900

    feat: 메세지 추가 1. 숫자를 입력해주세요 메세지 추가 2. 그 외 누락된 메세지 추가

--- src/main/java/baseball/view/Messages.java
+++ src/main/java/baseball/view/Messages.java
@@ -1,28 +1,32 @@
 package baseball.view;
 
 public class Messages {
-    public void printInitiateGameMessage() {
+    public static void printInitiateGameMessage() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    public void printBallsAndStrikesCount(int ball, int strikes) {
+    public static void printInputMessage() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
+    public static void printBallsAndStrikesCount(int balls, int strikes) {
         System.out.printf("%d볼 %d스트라이크\n", balls, strikes);
     }
 
 
-    public void printBallsCount(int balls) {
+    public static void printBallsCount(int balls) {
         System.out.printf("%d볼\n", balls);
     }
 
-    public void printStrikesCount(int strikes) {
+    public static void printStrikesCount(int strikes) {
         System.out.printf("%d스트라이크\n", strikes);
     }
 
-    public void printNothing() {
+    public static void printNothing() {
         System.out.println("낫싱");
     }
 
-    public void printEndGameMessage() {
+    public static void printEndGameMessage() {
         System.out.println("숫자 야구 게임을 종료합니다.");
     }
 }
