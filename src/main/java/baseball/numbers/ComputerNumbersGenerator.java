1f10668604539920e5873802744c261b90c2dfd4
ddolboghi <rhdfyd128@gmail.com>
2023-10-25 15:48:01 +0900

    fix: 숫자 생성 기능의 템플릿 메서드 패턴 수정

--- src/main/java/baseball/numbers/ComputerNumbersGenerator.java
+++ src/main/java/baseball/numbers/ComputerNumbersGenerator.java
@@ -5,13 +5,9 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ComputerNumbersGenerator implements NumbersGenerator{
-    @Override
-    public List<Integer> generate() {
-        Numbers numbers = new Numbers(createComputerNumbers());
-        return numbers.getNumbers();
-    }
-    private List<Integer> createComputerNumbers() {
+public class ComputerNumbersGenerator extends NumbersGenerator{
+
+    public List<Integer> createNumbers() {
         List<Integer> computerNumbers = new ArrayList<>();
         while (computerNumbers.size() < Constants.BASEBALL_NUMBER_SIZE) {
             int randomNumber = Randoms.pickNumberInRange(Constants.COMPUTER_NUMBER_START, Constants.COMPUTER_NUMBER_END);
