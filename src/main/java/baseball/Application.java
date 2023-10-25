7c7c99165d13d67c591ca27878d2b7b9ba3546e5
TaeDongUm <etd93@naver.com>
2023-10-25 23:17:06 +0900

    [feat] PlayerNum 클래스 및 입력된 숫자 판단 메서드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,6 +3,7 @@ package baseball;
 import java.util.ArrayList;
 import java.util.List;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
@@ -26,6 +27,56 @@ public class Application {
 	        	numList.add(newNum);
 	        }
 	        return numList;
+	    }
+	}
+	
+	class PlayerNum{
+	    List<Integer> playerNum;
+
+	    public PlayerNum(){
+	        System.out.print("숫자를 입력해주세요 : ");
+	        playerNum = playerInput();
+	    }
+
+	    private List<Integer> playerInput(){
+	        String playerInputString = Console.readLine();
+	        checkInput(playerInputString);
+	        return integerListConversion(playerInputString);
+	    }
+
+	    private void checkInput(String playerInputString) {
+	    	if (!isOneToNineNum(playerInputString))
+	    		throw new IllegalArgumentException();
+	        if (playerInputString.length() != 3)
+	            throw new IllegalArgumentException();
+	        if (!allDifferentNum(playerInputString))
+	            throw new IllegalArgumentException();
+	    }
+
+	    private List<Integer> integerListConversion(String playerInputString) {
+	        List<Integer> playerInput = new ArrayList<>();
+	        for (int i = 0; i < 3; i++) {
+	            char charNum = playerInputString.charAt(i);
+	            int intNum = charNum - '0';
+	            playerInput.add(intNum);
+	        }
+	        return playerInput;
+	    }
+	    private boolean isOneToNineNum(String playerInputString) {
+	        String numbers = "123456789";
+	        for (int i = 0; i < 3; i++) {
+	            char check = playerInputString.charAt(i);
+	            if (numbers.indexOf(check) == -1)
+	                return false;
+	        }
+	        return true;
+	    }
+
+	    private boolean allDifferentNum(String playerInputString) {
+	        char first = playerInputString.charAt(0);
+	        char second = playerInputString.charAt(1);
+	        char third = playerInputString.charAt(2);
+	        return ((first != second) && (second != third) && (third != first));
 	    }
 	}
 }
