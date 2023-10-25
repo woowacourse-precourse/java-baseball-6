4c1b91a646614dce17e049840d23413494ae719a
Yerinnnn <shinyerin0216@gmail.com>
2023-10-25 17:11:14 +0900

    Refactor: "코드 리팩토링"
    -상수 사용
    -메소드 세분화
      #getInputNum()
        -게임 플레이어에게서 입력값 받기
    
      #checkResult()
        -게임 플레이어에게서 받은 입력값과 컴퓨터의 수를 비교
    
      #printResult()
        -비교한 결과값 출력
    
    -메소드 순서 정리
    -주석 정리

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -3,117 +3,136 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Arrays;
+
 public class Game {
 
-    public int[] com_num = new int[3];
-    public int[] input_num = new int[3];
-    public int strike = 0;
-    public int ball = 0;
-    public int state = 0;
+    final int NUM_LENGTH = 3;
+
+    final int RESTART = 1;
+    final int GAME_OVER = 2;
+
+    private int[] com_num = new int[NUM_LENGTH];
+    private int[] input_num = new int[NUM_LENGTH];
+
+    private int strike = 0;
+    private int ball = 0;
 
     public Game() {
     }
 
     public void run() {
-        try{
-            do {
-                play();
-            } while (restart() == true);
-        }catch (IllegalArgumentException e){
-            throw e;
-        }
+        do {
+            play();
+        } while (restart() == true);
     }
 
     private void play(){
         init();
 
         do {
-            try {
-                System.out.print("숫자를 입력해주세요 : ");
+            System.out.print("숫자를 입력해주세요 : ");
 
-                /***
-                 * 입력값 받아서 정수형 리스트로 변환
-                 */
-                String num = Console.readLine();
+            getInputNum();
+            checkResult();
+            printResult();
 
-                if (num.length() != 3) {
-                    throw new IllegalArgumentException("숫자를 잘못 입력했습니다.");
-                }
-
-                for (int i = 0; i < input_num.length; i++) {
-                    input_num[i] = num.charAt(i) - '0';
-                }
-
-                /**
-                 * strike, ball 초기화
-                 */
-                strike = 0;
-                ball = 0;
-
-                /**
-                 * 입력값 분석: 첫 번째 시도 최종본
-                 */
-                for (int i = 0; i < 3; i++) {
-                    for (int j = 0; j < 3; j++) {
-                        if (com_num[i] == input_num[j] && i == j) {
-                            strike++;
-                        } else if (com_num[i] == input_num[j] && i != j) {
-                            ball++;
-                        }
-                    }
-                }
-
-                /**
-                 * 결과값 출력
-                 */
-                if (strike > 0 && ball > 0) {
-                    System.out.println(ball + "볼 " + strike + "스트라이크");
-                } else if (strike > 0 && ball == 0) {
-                    System.out.println(strike + "스트라이크");
-                } else if (ball > 0 && strike == 0) {
-                    System.out.println(ball + "볼");
-                } else {
-                    System.out.println("낫싱");
-                }
-
-            } catch (IllegalArgumentException e){
-                throw e;
-            }
-        } while(strike < 3);
+        } while (strike < 3);
     }
 
-    private boolean restart() throws IllegalArgumentException{
+    /**
+     * 컴퓨터 넘버 셋팅
+     */
+    private void init(){
+
+        for (int i = 0;i < com_num.length;i++) {
+            // 랜덤 숫자 선택
+            com_num[i] = Randoms.pickNumberInRange(1, 9);
+
+            // 중복값 제거
+            for (int j = 0;j < i;j++) {
+                if (com_num[i] == com_num[j]) {
+                    i--;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(com_num));
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    /***
+     * 입력값 받은 후 정수형 리스트로 변환
+     */
+    private void getInputNum() {
+
+        String num = Console.readLine();
+
+        if (num.length() != NUM_LENGTH) {
+            throw new IllegalArgumentException("숫자를 잘못 입력했습니다.");
+        }
+
+        for (int i = 0; i < input_num.length; i++) {
+            input_num[i] = num.charAt(i) - '0';
+        }
+    }
+
+    /**
+     * 입력값 분석
+     */
+    private void checkResult() {
+        // strike, ball 초기화
+        strike = 0;
+        ball = 0;
+
+        for (int i = 0; i < NUM_LENGTH; i++) {
+            for (int j = 0; j < NUM_LENGTH; j++) {
+                if (com_num[i] == input_num[j] && i == j) {
+                    strike++;
+                } else if (com_num[i] == input_num[j] && i != j) {
+                    ball++;
+                }
+            }
+        }
+    }
+
+    /**
+     * 결과값 출력
+     */
+    private void printResult(){
+        if (strike > 0 && ball > 0) {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        } else if (strike > 0 && ball == 0) {
+            System.out.println(strike + "스트라이크");
+        } else if (ball > 0 && strike == 0) {
+            System.out.println(ball + "볼");
+        } else {
+            System.out.println("낫싱");
+        }
+    }
+
+    /***
+     * 게임 재실행
+     */
+    private boolean restart() throws IllegalArgumentException {
 
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
-        state = Integer.parseInt(Console.readLine());
-        if (state != 1 && state != 2) {
-            throw new IllegalArgumentException();
+        // 게임 플레이어가 숫자 입력
+        int inputNum = Integer.parseInt(Console.readLine());
+
+        if (inputNum != RESTART && inputNum != GAME_OVER) {
+            throw new IllegalArgumentException("잘못된 숫자입니다.");
         }
 
-        if (state == 1) {
+        if (inputNum == RESTART) {
             return true;
-        } else if (state == 2) {
+        } else if (inputNum == GAME_OVER) {
             return false;
         } else {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private void init(){
-        try {
-            for(int i = 0;i < com_num.length;i++) {
-                com_num[i] = Randoms.pickNumberInRange(1, 9);
-                for(int j = 0;j < i;j++) {
-                    if (com_num[i] == com_num[j]) {
-                        i--;
-                        break;
-                    }
-                }
-            }
-            System.out.println("숫자 야구 게임을 시작합니다.");
-        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("잘못된 숫자입니다.");
         }
     }
 }
