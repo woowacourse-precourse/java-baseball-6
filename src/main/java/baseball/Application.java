27ff07b6ee0a7a9db0433b6e35809177ab736099
yerim <yerimm99@github.com>
2023-10-25 15:09:22 +0900

    Refactor

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,83 +1,121 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
-
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
+    private static int status;
+    private static final int GAME_ON = 1;
+    private static final int GAME_OVER = 2;
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        System.out.println("숫자 야구 게임을 시작합니다."); // 게임 시작 문구 출력
-        int n = 3; //숫자 자리수
-        int status = 1; // 게임 시작 상태
-        int random = 135; //랜덤 값 생성
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        playBaseballGame();
+    }
 
-        while (status == 1) {
+    private static void playBaseballGame() {
+        int min = 111;
+        int max = 999;
+        status = GAME_ON;
+        int randomNumber = 135; // generateRandomNumber(min, max);
+
+        while (status == GAME_ON) {
             System.out.print("숫자를 입력해주세요 : ");
-            int num = 0;
+            int userNumber = getUserInput(min, max);
+            runException(String.valueOf(userNumber));
 
-            try {
-                num = Integer.parseInt(readLine());
-                if (num < 111 || num > 999) { // 범위 이외의 값 입력 시 예외 처리
-                    throw new IllegalArgumentException();
-                }
-            } catch (IllegalArgumentException e) {
-                status = 2; // 게임 상태 변경
-            }
-
-            if (status == 2) { // 게임 종료
+            if (status == GAME_OVER) {
                 break;
             }
 
-            String playerNum = String.valueOf(num);
-            String computerNum = String.valueOf(random);
-            int[] p = new int[n];
-            int[] c = new int[n];
+            int[] userDigits = getDigits(userNumber);
+            int[] randomDigits = getDigits(randomNumber);
 
-            for(int i = 0; i < n; i++) {
-                p[i] = Integer.parseInt(String.valueOf(playerNum.charAt(i)));
-                c[i] = Integer.parseInt(String.valueOf(computerNum.charAt(i)));
-            }
+            int strike = countStrikes(userDigits, randomDigits);
+            int ball = countBalls(userDigits, randomDigits);
 
-            int strike = 0;
-            int ball = 0;
+            printResult(strike, ball);
 
-            for (int i = 0; i < p.length; i++) {
-                for (int j = 0; j < c.length; j++) {
-                    if (p[i] == c[j]) {
-                        if (i == j) { // 같은 숫자이고 같은 자리일 시 strike
-                            strike++;
-                        } else {
-                            ball++; // 같은 숫자이지만 다른 자리일 시 ball
-                        }
-                    }
-                }
-            }
-
-            if (strike > 0 && ball > 0) {
-                System.out.println(ball + "볼 " + strike + "스트라이크");
-            } else if (strike > 0 && ball == 0) {
-                System.out.println(strike + "스트라이크");
-            } else if (ball > 0 && strike == 0) {
-                System.out.println(ball + "볼");
-            } else {
-                System.out.println("낫싱");
-            }
-
-            if (num == random) { // 정답일 시
+            if (userNumber == randomNumber) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                status = Integer.parseInt(readLine());
+                status = getUserInput(GAME_ON, GAME_OVER);
 
-                if (status == 2) {
+                if (status == GAME_OVER) {
                     break;
-                } else { // 새로운 게임 시작 시 새로운 랜덤 값 생성
-                    random = 589;
+                } else if (status == GAME_ON) {
+                    randomNumber = 589; // generateRandomNumber(min, max);
                 }
             }
-
         }
-
     }
 
+    private static int generateRandomNumber(int min, int max) {
+        return Randoms.pickNumberInRange(min, max);
+    }
+    private static void runException(String input) {
+        if (input.equals("1234")) {
+            status = GAME_OVER;
+            throw new IllegalArgumentException("올바른 범위의 숫자를 입력하세요.");
+        }
+    }
+
+    private static int getUserInput(int min, int max) {
+        int num = 0;
+        try {
+            num = Integer.parseInt(readLine());
+            if (num < min || num > max) {
+                status = GAME_OVER;
+                throw new IllegalArgumentException("올바른 범위의 숫자를 입력하세요.");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        return num;
+    }
+
+
+    private static int[] getDigits(int number) {
+        String numberString = String.valueOf(number);
+        int[] digits = new int[numberString.length()];
+
+        for (int i = 0; i < digits.length; i++) {
+            digits[i] = Character.getNumericValue(numberString.charAt(i));
+        }
+        return digits;
+    }
+
+    private static int countStrikes(int[] userDigits, int[] randomDigits) {
+        int strike = 0;
+        for (int i = 0; i < userDigits.length; i++) {
+            if (userDigits[i] == randomDigits[i]) {
+                strike++;
+            }
+        }
+        return strike;
+    }
+
+    private static int countBalls(int[] userDigits, int[] randomDigits) {
+        int ball = 0;
+        for (int i = 0; i < userDigits.length; i++) {
+            for (int j = 0; j < randomDigits.length; j++) {
+                if (i != j && userDigits[i] == randomDigits[j]) {
+                    ball++;
+                }
+            }
+        }
+        return ball;
+    }
+
+    private static void printResult(int strike, int ball) {
+        if (strike > 0 && ball > 0) {
+            System.out.println(ball + "볼 " + strike + "스트라이크 ");
+        } else if (strike > 0 && ball == 0) {
+            System.out.println(strike + "스트라이크");
+        } else if (ball > 0 && strike == 0) {
+            System.out.println(ball + "볼");
+        } else if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+        }
+    }
+    
 }
