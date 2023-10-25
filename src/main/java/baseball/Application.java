ae532be53d68ce36073da6754f24d1606eaf33ad
Dobby_Kim <kimdobby@g.skku.edu>
2023-10-25 11:42:14 +0900

    [chore: Application 수정] 게임 재시작을 위한 Loop 로직 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,16 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        boolean start = true;
+
+        while (start) {
+
+            Player computer = new ComputerPlayer();
+            Player human = new HumanPlayer();
+            Judge judge = new Judge(computer, human);
+
+            start = judge.startGame();
+        }
     }
 }
