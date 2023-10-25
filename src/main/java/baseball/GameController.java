42b22bfbdf3dbd50c93d1a59f14f572489d11ac3
booyaChael <chaeeul2022@gmail.com>
2023-10-25 20:31:22 +0900

    [REFACTOR] 변수 이름 및 주석 수정

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -14,57 +14,50 @@ public class GameController {
 		this.gameView = gameView;
 	}
 
-	//게임을 실행하는 메서드
+	//게임 실행
 	public void run(){
 		boolean isRetry = true;
-		//사용자가 재시도를 원할 떄까지 게임을 진행
 		while(isRetry){
 			runGame();
 			isRetry = askRetry();
 		}
 	}
 
-	//게임의 단계별 메서드를 호출하여 게임 진행하는 메서드
+	//게임의 단계별 메서드를 호출하여 게임 진행
 	private void runGame(){
 		gameView.printStartMessage();
-		//gameModel 생성 및 적용
-		makeGame();
+		List<Integer> computerNumber = makeComputerNumber();
+		this.gameModel = makeGameModelInstance(computerNumber);
 
-		List<Integer> computerNumber = gameModel.getComputerNumber();
 		while(!gameModel.getIsCorrect()){
-			handleGuess(computerNumber);
+			handleGuess(gameModel.getComputerNumber());
 		}
 	}
 
-	//사용자의 재시도 여부를 묻는 메서드
+	//사용자의 재시도 의사 확인
 	private boolean askRetry(){
-		String retry = gameView.getRetry();
 		boolean retryResult = false;
-		if(retry.equals(Constants.RETRY_TRUE)){
+
+		String input = gameView.getRetry();
+		if(input.equals(Constants.RETRY_TRUE)){
 			retryResult = true;
 		}
-		if(retry.equals(Constants.RETRY_FALSE)){
+		if(input.equals(Constants.RETRY_FALSE)){
 			retryResult = false;
 		}
 		return retryResult;
 	}
 
-
-	//게임 모델을 생성하는 메서드
-	private void makeGame(){
-		List<Integer> computerNumber = makeRandomNumber();
-		//만들어진 숫자로 gameModel 생성
-		GameModel gameModel = new GameModel(computerNumber);
-
-		//만들어진 gameModel을 이 GameController의 gameModel에 할당
-		this.gameModel = gameModel;
+	//GameModel 인스턴스 생성
+	private GameModel makeGameModelInstance(List<Integer> computerNumber){
+		return new GameModel(computerNumber);
 	}
 
-	//랜덤 숫자를 생성하여 반환하는 메서드
-	private List<Integer> makeRandomNumber(){
+
+	//컴퓨터 숫자 생성
+	private List<Integer> makeComputerNumber(){
 		List<Integer> computerNumber = new ArrayList<>();
 
-		//Randoms의 메서드를 이용해 서로 다른 3자리의 숫자 생성
 		while (computerNumber.size() < Constants.NUMBER_LENGTH) {
 			int randomNumber = Randoms.pickNumberInRange(Constants.MIN_NUMBER, Constants.MAX_NUMBER);
 			if (!computerNumber.contains(randomNumber)) {
@@ -74,21 +67,23 @@ public class GameController {
 		return computerNumber;
 	}
 
-	//사용자의 예상숫자를 처리하는 메서드
+	//사용자의 예상 숫자 처리
 	private void handleGuess(List<Integer> computerNumber){
-		//user가 입력한 숫자를 받는다.
-		String userGuess = gameView.getUserGuess();
-		List<Integer> userGuessNumber = convertStringToList(userGuess);
-		//user가 입력한 숫자에 대해 스트라이크와 볼의 개수를 프린트한다
+		String input = gameView.getUserGuess();
+		List<Integer> userGuessNumber = convertStringToList(input);
+
 		int numberOfStrike = countStrike(computerNumber, userGuessNumber);
 		int numberOfBall = countBall(computerNumber, userGuessNumber);
 		gameView.printGuessResult(numberOfStrike, numberOfBall);
 
-		//숫자를 맞추었으면 맞추었다는 메세지를 프린트한다
-		if(numberOfStrike == Constants.NUMBER_LENGTH){
-			gameModel.setIsCorrect(true);
-			gameView.printCorrectMessage();
+		boolean isCorrect = numberOfStrike == Constants.NUMBER_LENGTH;
+		if(isCorrect){
+			handleIsCorrect();
 		}
+	}
+	private void handleIsCorrect(){
+		gameModel.setIsCorrect(true);
+		gameView.printCorrectMessage();
 	}
 
 	//스트라이크의 수 계산
--- src/main/java/baseball/GameView.java
+++ src/main/java/baseball/GameView.java
@@ -13,31 +13,31 @@ public class GameView {
 
 	public String getUserGuess(){
 		System.out.print(Constants.GUESS_MESSAGE);
-		String userNumber = Console.readLine();
-		validateUserGuess(userNumber);
-		return userNumber;
+		String input = Console.readLine();
+		validateUserGuess(input);
+		return input;
 	}
 
-	private void validateUserGuess(String userNumber) {
+	private void validateUserGuess(String input) {
 		// 숫자로만 이루어져 있는지 확인
-		boolean isOnlyNumber = userNumber.matches("\\d+");
+		boolean isOnlyNumber = input.matches("\\d+");
 		// 3자리인지 확인
-		boolean isRightSize = userNumber.length() == Constants.NUMBER_LENGTH;
+		boolean isRightSize = input.length() == Constants.NUMBER_LENGTH;
 
 		if (!isOnlyNumber || !isRightSize) {
 			throw new IllegalArgumentException(Constants.INVALID_LENGTH_ERROR);
 		}
 
 		// 숫자가 서로 다른지 확인
-		if (!hasUniqueDigits(userNumber)) {
+		if (!hasUniqueDigits(input)) {
 			throw new IllegalArgumentException(Constants.DUPLICATE_DIGITS_ERROR);
 		}
 	}
 
 	// 사용자 입력에 중복된 숫자가 있는지 확인
-	private boolean hasUniqueDigits(String userNumber) {
-		for (char c : userNumber.toCharArray()) {
-			if (userNumber.indexOf(c) != userNumber.lastIndexOf(c)) {
+	private boolean hasUniqueDigits(String input) {
+		for (char c : input.toCharArray()) {
+			if (input.indexOf(c) != input.lastIndexOf(c)) {
 				return false;
 			}
 		}
@@ -47,7 +47,7 @@ public class GameView {
 	public void printGuessResult(int numberOfStrike, int numberOfBall){
 		StringBuilder guessResult = new StringBuilder();
 		if(numberOfBall > 0){
-			guessResult.append(numberOfBall).append("볼 ");
+			guessResult.append(numberOfBall).append("볼").append(" ");
 		}
 		if(numberOfStrike > 0){
 			guessResult.append(numberOfStrike).append("스트라이크");
@@ -65,13 +65,13 @@ public class GameView {
 	//0 혹은 1이 아니면 예외
 	public String getRetry(){
 		System.out.println(Constants.RETRY_MESSAGE);
-		String retry = Console.readLine();
-		validateRetryInput(retry);
-		return retry;
+		String input = Console.readLine();
+		validateRetry(input);
+		return input;
 	}
 
-	private void validateRetryInput(String retry){
-		boolean isOneOrTwo = retry.equals(Constants.RETRY_TRUE) || retry.equals(Constants.RETRY_FALSE);
+	private void validateRetry(String input){
+		boolean isOneOrTwo = input.equals(Constants.RETRY_TRUE) || input.equals(Constants.RETRY_FALSE);
 		if(!isOneOrTwo){
 			throw new IllegalArgumentException(Constants.INVALID_RETRY_INPUT_ERROR);
 		}
