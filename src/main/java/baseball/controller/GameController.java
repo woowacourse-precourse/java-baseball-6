fdddab69a60c569465149bd4513f7c33724da13c
IToriginal <gitshineit@gmail.com>
2023-10-25 19:52:39 +0900

    :recycle: [Refactor] #3 Controller 클래스 메서드 분리
     가독성을 증가시키고 객체지향적인 코드를 작성해보고자 객체지향 생활 체조 원칙 중 하나인
     "한 메서드에 오직 한 단계의 들여쓰기(indent)만 한다" 를 위배한 코드로 판단하여 메소드를 나눠 적용하였습니다.

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -8,8 +8,8 @@ import baseball.view.GameView;
 
 public class GameController {
 
-    private GameModel model;
-    private GameView view;
+    private final GameModel model;
+    private final GameView view;
     private boolean systemOnOff;
 
     public GameController(GameModel model, GameView view) {
@@ -22,28 +22,35 @@ public class GameController {
         view.showStartMessage();
 
         while (systemOnOff) {
-            String userInput = view.getUserInput();
-            try {
-                model.validateUserInput(userInput);
-                CalculationResult result = model.checkGameResult(userInput);
-                System.out.println(view.showGameResult(result));
-                if (result.getStrikeCnt() == LIMIT_NUMBERS_SIZE.getValue()) {
-                    String userChoice = view.getGameChoiceInput();
-                    model.validateUserChoice(userChoice);
-                    switch (userChoice) {
-                        case "1":
-                            model.reset();
-                            view.showStartMessage();
-                            continue;
-                        case "2":
-                            systemOnOff = false;
-                            break;
-                    }
-                }
-            } catch (IllegalArgumentException exception) {
-                throw new IllegalArgumentException();
-            }
+            playGame();
+        }
+    }
 
+    private void playGame() {
+        String userInput = view.getUserInput();
+        try {
+            model.validateUserInput(userInput);
+            CalculationResult result = model.checkGameResult(userInput);
+            System.out.println(view.showGameResult(result));
+            if (result.getStrikeCnt() == LIMIT_NUMBERS_SIZE.getValue()) {
+                gameChoice();
+            }
+        } catch (IllegalArgumentException exception) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void gameChoice() {
+        String userChoice = view.getGameChoiceInput();
+        model.validateUserChoice(userChoice);
+        switch (userChoice) {
+            case "1":
+                model.reset();
+                view.showStartMessage();
+                break;
+            case "2":
+                systemOnOff = false;
+                break;
         }
     }
 }
