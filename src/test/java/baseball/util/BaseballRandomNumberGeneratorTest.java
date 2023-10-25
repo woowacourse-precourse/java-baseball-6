e3ce2ae0067ecccae9fd517e94eada3a060d6690
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-10-25 17:36:48 +0900

    test : 랜덤 숫자 생성에서 0이 포함되어 생성되는지 테스트 구현

--- src/test/java/baseball/util/BaseballRandomNumberGeneratorTest.java
+++ src/test/java/baseball/util/BaseballRandomNumberGeneratorTest.java
@@ -23,7 +23,7 @@ class BaseballRandomNumberGeneratorTest {
     }
 
     @Test
-    void 컴퓨터_랜덤_생선된_숫자_사이즈_테스트(){
+    void 컴퓨터_랜덤_생성된_숫자_사이즈_테스트(){
         //given
         BaseballNumberGenerator baseballNumberGenerator = new BaseballRandomNumberGenerator();
 
@@ -32,5 +32,21 @@ class BaseballRandomNumberGeneratorTest {
 
         //then
         assertThat(case1.size() == 3).isEqualTo(true);
+    }
+
+    @Test
+    void 컴퓨터_랜덤_생성된_숫자_0_포함_테스트(){
+        //given
+        BaseballNumberGenerator baseballNumberGenerator = new BaseballRandomNumberGenerator();
+
+        //when
+        final List<Integer> case1 = baseballNumberGenerator.generate();
+        final List<Integer> case2 = baseballNumberGenerator.generate();
+        final List<Integer> case3 = baseballNumberGenerator.generate();
+
+        //then
+        assertThat(case1.contains(0)).isEqualTo(false);
+        assertThat(case2.contains(0)).isEqualTo(false);
+        assertThat(case3.contains(0)).isEqualTo(false);
     }
 }
