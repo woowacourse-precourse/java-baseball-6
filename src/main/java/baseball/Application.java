07df47ef8eaa4be4d94aa84672f0badfb0dcdae9
bobo1201 <121841746+bobo1201@users.noreply.github.com>
2023-10-25 10:40:43 +0900

    23/10/25 10:40 README 정리 및 코드 정리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,9 @@
 package baseball;
-import camp.nextstep.edu.missionutils.Randoms;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import static camp.nextstep.edu.missionutils.Console.readLine;
 import java.util.ArrayList;
 import java.util.List;
-
-import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
