45469885fce0e53e5b80e6c30c275cfa121961c1
heejae <moon49051130@gmail.com>
2023-10-25 16:29:55 +0900

    FIX

--- src/main/java/baseball/PrintMessage.java
+++ src/main/java/baseball/PrintMessage.java
@@ -1,14 +1,12 @@
 package baseball;
 
-import java.util.Map;
-
 public enum PrintMessage {
 
     START       ( "숫자 야구 게임을 시작합니다." ),
     ASK_INPUT   ( "숫자를 입력해주세요 : " ),
     CLEAR       ( "3개의 숫자를 모두 맞히셨습니다! 게임 종료" ),
     RESTART     ( "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요." ),
-    ERROR       ( "잘못된 입력입니다." ),
+    ERROR       ( "잘못된 입력입니다." )
     ;
 
     private final String message;
