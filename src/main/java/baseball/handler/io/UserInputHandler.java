e2bb4ca5380c524b554cbc1d8f345193fe33627d
yujeonghyeon <dbdb1114@naver.com>
2023-10-25 23:15:21 +0900

    정답시 출력문 변경 및 input 분리

--- src/main/java/baseball/handler/io/UserInputHandler.java
+++ src/main/java/baseball/handler/io/UserInputHandler.java
@@ -5,10 +5,26 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class UserInputHandler {
-    public static List<Integer> userSubmit(){
-        System.out.println("숫자를 입력해주세요 : ");
+    public static List<Integer> userSubmit() {
+        System.out.print("숫자를 입력해주세요 : ");
         String answer = Console.readLine();
         return answer.chars().mapToObj(item -> item - '0')
                 .collect(Collectors.toList());
     }
+
+    public static boolean isContinue() {
+        String isReTry;
+
+        do {
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            isReTry = Console.readLine();
+        } while (!isReTry.equals("1") && !isReTry.equals("2"));
+
+        if (isReTry.equals("1")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
