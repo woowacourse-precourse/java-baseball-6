0fb2b0e1e0f241f05dcddb7bde9ae15e6cb3bb98
Kaeun-Lee <lgeun7026@hanmail.net>
2023-10-25 09:20:47 +0900

    feat: Modify game loop to continue until correct guess

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -21,58 +21,61 @@ public class Application {
         }
 
         // user
-        System.out.println("숫자를 입력해주세요 : ");
+        while (true) {
+            System.out.println("숫자를 입력해주세요 : ");
 
-        String input = Console.readLine();
-        if (input.length() != 3) {
-            throw new IllegalArgumentException("3자리 숫자를 입력해 주세요.");
-        }
-        List<Integer> userNumber = new ArrayList<>();
-        Set<Integer> userNumberSet = new HashSet<>();
-        for (char ch : input.toCharArray()) {
-            int num = Character.getNumericValue(ch);
-            if (num < 1 || num > 9) {
-                throw new IllegalArgumentException("1부터 9까지의 숫자를 입력해주세요.");
+            String input = Console.readLine();
+            if (input.length() != 3) {
+                throw new IllegalArgumentException("3자리 숫자를 입력해 주세요.");
             }
-            userNumber.add(num);
-            userNumberSet.add(num);
-        }
+            List<Integer> userNumber = new ArrayList<>();
+            Set<Integer> userNumberSet = new HashSet<>();
+            for (char ch : input.toCharArray()) {
+                int num = Character.getNumericValue(ch);
+                if (num < 1 || num > 9) {
+                    throw new IllegalArgumentException("1부터 9까지의 숫자를 입력해주세요.");
+                }
+                userNumber.add(num);
+                userNumberSet.add(num);
+            }
 
-        if (userNumber.size() != userNumberSet.size()) {
-            throw new IllegalArgumentException("서로 다른 숫자를 입력해주세요.");
-        }
+            if (userNumber.size() != userNumberSet.size()) {
+                throw new IllegalArgumentException("서로 다른 숫자를 입력해주세요.");
+            }
 
-        System.out.println("컴퓨터의 숫자: " + computer);
-        System.out.println("사용자의 숫자: " + userNumber);
+            System.out.println("컴퓨터의 숫자: " + computer);
+            System.out.println("사용자의 숫자: " + userNumber);
 
-        // play game
-        int strike = 0;
-        int ball = 0;
-        for (int i = 0; i < 3; i++) {
-            if (userNumber.get(i).equals(computer.get(i))) {
-                strike++;
-            } else if (computer.contains(userNumber.get(i))) {
-                ball++;
+            // play game
+            int strike = 0;
+            int ball = 0;
+            for (int i = 0; i < 3; i++) {
+                if (userNumber.get(i).equals(computer.get(i))) {
+                    strike++;
+                } else if (computer.contains(userNumber.get(i))) {
+                    ball++;
+                }
+            }
+
+            if (strike == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            }
+
+            if (strike == 0 && ball == 0) {
+                System.out.println("낫싱");
+            } else {
+                String result = "";
+                if (ball > 0) {
+                    result += ball + "볼 ";
+                }
+                if (strike > 0) {
+                    result += strike + "스트라이크";
+                }
+                System.out.println(result.trim());
             }
         }
-
-        if (strike == 3) {
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        }
-
-        if (strike == 0 && ball == 0) {
-            System.out.println("낫싱");
-        } else {
-            String result = "";
-            if (ball > 0) {
-                result += ball + "볼 ";
-            }
-            if (strike > 0) {
-                result += strike + " 스트라이크";
-            }
-            System.out.println(result.trim());
-        }
-
+        
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         System.out.println("게임을 종료합니다.");
     }
