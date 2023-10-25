40eb3ed683796af6a0074e1070043279aa2ca884
sweun2 <46338536+sweun2@users.noreply.github.com>
2023-10-25 09:46:59 +0900

    fix : change create answer func
    change create answer func

--- src/main/java/baseball/game/Game.java
+++ src/main/java/baseball/game/Game.java
@@ -1,15 +1,22 @@
 package baseball.game;
 
+import baseball.utils.Constant;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 public class Game {
-    List<Integer> createAnswer() {
-        List<Integer> randNumbers = Randoms.pickUniqueNumbersInRange(1, 9, 3);
-        randNumbers.forEach(System.out::println);
-        return randNumbers;
+    public static List<Integer> createAnswer() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 
     String checkAnswer(List<Integer> inputNumbers, List<Integer> answer) {
