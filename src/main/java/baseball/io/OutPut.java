62354af71fe990b492d414c396ac3ddb1542949b
beomsic <qjatjr29@khu.ac.kr>
2023-10-25 12:00:23 +0900

    :recycle: refactor : 줄바꿈 간격 수정

--- src/main/java/baseball/io/OutPut.java
+++ src/main/java/baseball/io/OutPut.java
@@ -3,11 +3,8 @@ package baseball.io;
 import java.io.IOException;
 
 public interface OutPut {
-
     void gameStart();
     void print(String msg) throws IOException;
-
     void printResult(int strikeCount, int ballCount) throws IOException;
-
     void close() throws IOException;
 }
