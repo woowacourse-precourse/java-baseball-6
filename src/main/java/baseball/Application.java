06634753c88787c7402e529bc1a2bf415cf27fcc
dradnats1012 <herjebi1012@gmail.com>
2023-10-25 20:55:40 +0900

    Feat : Application 클래스 숫자 생성 전략 주입 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,11 @@
 package baseball;
 
 import baseball.controller.Controller;
+import baseball.model.RandomNumberGenerator;
 
 public class Application {
     public static void main(String[] args) {
-        Controller controller = new Controller();
+        Controller controller = new Controller(new RandomNumberGenerator());
         controller.run();
     }
 }
