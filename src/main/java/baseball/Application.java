be4e2eebdc0b439596eed44a73f6b7883a83abe3
HyunmoBae <tsi0520@naver.com>
2023-10-25 23:02:22 +0900

    [MODIFY] 메인함수 예외수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -106,7 +106,7 @@ public class Application {
         return false;
     }
 
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
         ArrayList<Integer> computer = createComputerNumber();
