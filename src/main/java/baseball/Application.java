b80e78f3ac214d0045d049c2e64ecde87c3e4af6
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2023-10-25 19:02:16 +0900

    fix: 주석 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -17,16 +17,6 @@ public class Application {
             }
             String computer = String.join("", computerHash);
 
-//            List<Integer> computer = new ArrayList<>();
-//            while (computer.size() < 3) {
-//                int randomNumber = Randoms.pickNumberInRange(1, 9);
-//                if (!computer.contains(randomNumber)) {
-//                    computer.add(randomNumber);
-//                }
-//            }
-//
-//            System.out.println(computer);
-
             boolean isAllStrike = false;
 
             while (!isAllStrike) {
