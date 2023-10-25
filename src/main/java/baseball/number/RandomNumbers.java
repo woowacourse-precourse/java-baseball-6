f3d06e95497ae446002665c7d676d1cff542e7a7
Yang woosung <ngxk1217@gmail.com>
2023-10-25 14:21:21 +0900

    클래스 레벨의 상수 선언

--- src/main/java/baseball/number/RandomNumbers.java
+++ src/main/java/baseball/number/RandomNumbers.java
@@ -1,4 +1,11 @@
 package baseball.number;
 
+import java.util.List;
+
 public class RandomNumbers {
+    private static final int START_INCLUSIVE = 1;
+    private static final int END_INCLUSIVE = 9;
+    private static final int COUNT = 3;
+
+
 }
