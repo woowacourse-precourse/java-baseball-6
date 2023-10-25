05e612c0aef4ab7fb05274f7a4b76acb2bd9ebc8
Hyoseop Song <crad_on25@naver.com>
2023-10-25 20:27:56 +0900

    feat(BallNumber): BallNumber 구현 및 기본 메소드 구현

---
+++ src/main/java/baseball/domain/BallNumber.java
@@ -0,0 +1,22 @@
+package baseball.domain;
+
+import baseball.Util.ListUtility;
+import java.util.List;
+
+public class BallNumber {
+    public static int LENGTH = 3;
+
+    private final List<Integer> splitNumber;
+
+    BallNumber(int number) {
+        this.splitNumber = ListUtility.splitDigitsIntoList(number);
+    }
+
+    public int getNumberAt(int index) {
+        return splitNumber.get(index);
+    }
+
+    public boolean contains(int number) {
+        return splitNumber.contains(number);
+    }
+}
