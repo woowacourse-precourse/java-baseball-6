8c4eae5564e85f60c2d8316f61c4066b9ae8179e
meanyong <my011208@gmail.com>
2023-10-25 21:26:36 +0900

    vv

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,7 +8,6 @@ public class Application {
         for (int i = 0; i <3 ; i++) {
             int r = new Random().nextInt(8) + 1;//난수를 r에 저장
         }
-
         String number;
         System.out.println("Enter the number.");
         Scanner sc = new Scanner(System.in);
