8f96e5666a564b087a71b99dff6f52c64ed07818
jieun4510@sju.ac.kr <jieun4510@sju.ac.kr>
2023-10-25 14:03:23 +0900

    feat: input user number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,7 +5,43 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.*;
 
+
 public class Application {
+    // input user number
+    public static List<Integer> userInput(){
+        // read user input
+        System.out.print("숫자를 입력해주세요 : ");
+        List<Integer> user = new ArrayList<>();
+        String userString = Console.readLine();
+        int userNumber;
+
+        // 입력값의 길이가 3이 아닌 경우
+        if (userString.length() != 3)
+            throw new IllegalArgumentException();
+
+        // userInput을 정수로 변환
+        try{
+            userNumber = Integer.parseInt(userString);
+            System.out.printf("userNumber: %d \n", userNumber);
+        }
+        catch (NumberFormatException ex){
+            throw new IllegalArgumentException();
+        }
+
+        // user 배열에 저장
+        for (int i = 0; i<3;i++){
+            int num = userString.charAt(i) - '0';
+            if (num != 0 && !user.contains(num)){
+                user.add(num);
+            }
+            else{
+                throw new IllegalArgumentException();
+            }
+        }
+        return user;
+    }
+
+
     public static int main(String[] args) {
         // random number setting
         List<Integer> computer = new ArrayList<>();
@@ -15,8 +51,12 @@ public class Application {
                 computer.add(randomNumber);
             }
         }
-//        System.out.print("computer: ");
-//        System.out.println(computer);
+        System.out.print("computer: ");
+        System.out.println(computer);
+
+        List<Integer> user = userInput();
+        System.out.print("user input: ");
+        System.out.println(user);
         return 0;
     }
 }
