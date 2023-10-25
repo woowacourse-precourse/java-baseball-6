68f4cf99340d722598b1d5605ea7796094c4978a
2jin2.1031 <2jin2.1031@gmail.com>
2023-10-25 22:00:36 +0900

    style: application unnessary line

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,7 +5,6 @@ public class Application {
         GameProcess gameProcess = new GameProcess();
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-        //Computer computer = new Computer(); // 3
         gameProcess.process();
     }
 }
