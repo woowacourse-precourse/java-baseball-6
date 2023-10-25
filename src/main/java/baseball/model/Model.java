347bb50c9ecc98071f80c4561eabfc277fd498e9
djc06048 <gsafe1213@gmail.com>
2023-10-25 20:32:15 +0900

    [feat] 사용자가 입력한 숫자 검증하는 로직

--- src/main/java/baseball/model/Model.java
+++ src/main/java/baseball/model/Model.java
@@ -24,5 +24,37 @@ public class Model {
     }
 
 
+    public void validateInputNumber(String input) throws IllegalArgumentException{
+        // 입력한 숫자의 길이가 3이 아닌 경우
+        if (input.length() != length) {
+            throw new IllegalArgumentException();
+        }
+
+        // 각 자리 수가 숫자가 아닌 경우
+        // 각 자리 수가 1부터 9로 이루어지지 않은 경우
+        char[] arr = input.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (!Character.isDigit(arr[i])) {
+                throw new IllegalArgumentException();
+            }else{
+                if (Character.getNumericValue(arr[i]) < 1 || Character.getNumericValue(arr[i]) > 9) {
+                    throw new IllegalArgumentException();
+                }
+            }
+
+        }
+
+        // 각 자리 수에 중복이 있는 경우
+        int[] cntarr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            int idx = Character.getNumericValue(arr[i]);
+            cntarr[idx]++;
+        }
+        for (int i = 1; i <= 9; i++) {
+            if (cntarr[i]>1) throw new IllegalArgumentException();
+
+        }
+    }
+
 
 }
