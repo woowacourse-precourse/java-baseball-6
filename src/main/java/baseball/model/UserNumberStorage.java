a9b079720213f5121824ca285a6eeca9b72b344c
chestnut1717 <psh56750855@gmail.com>
2023-10-25 23:09:02 +0900

    refactor: delete strToList method in UserNumberStorage 기존에는 Model의 UserNumberStorage 클래스에다 사용자의 문자열을 리스트로 담아 반환해주는 메소드를 기능을 넣음. 하지만 독립적으로 Controller이 담당하게 하는 것이 옳다고 생각하여 수정

--- src/main/java/baseball/model/UserNumberStorage.java
+++ src/main/java/baseball/model/UserNumberStorage.java
@@ -1,6 +1,5 @@
 package baseball.model;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class UserNumberStorage {
@@ -20,18 +19,6 @@ public class UserNumberStorage {
     // setter
     public void setNumbers(List<Integer> numbers) {
         _numbers = numbers;
-    }
-
-    // str to list
-    public static List<Integer> strToList(String str) {
-        List<Integer> intList = new ArrayList<>();
-        for (int i = 0; i < str.length(); i++) {
-            char tmp_char = str.charAt(i);
-            int digit = Character.getNumericValue(tmp_char);
-            intList.add(digit);
-        }
-
-        return intList;
     }
 
 }
