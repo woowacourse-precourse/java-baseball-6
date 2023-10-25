65be9d8d820749bafb05329c7afd1143791cdddc
Chaeros <chpark6851@koreatech.ac.kr>
2023-10-25 19:39:02 +0900

    feat: 사용자 입력 크기 검증 메서드 추가
    사용자가 입력한값이 3자리를 만족하는지 검증하는 메서드를 추가했습니다.

--- src/main/java/baseball/domain/PlayerNumber.java
+++ src/main/java/baseball/domain/PlayerNumber.java
@@ -1,4 +1,19 @@
 package baseball.domain;
 
+import baseball.constant.Constant;
+import baseball.constant.errormessage.PlayerInputError;
+import java.util.List;
+
 public class PlayerNumber {
+
+    /**
+     * 입력한 문자열의 길이가 3이 아닌 경우 예외 발생
+     *
+     * @param playerNumbers : 랜덤으로 생성한 컴퓨터가 가진 수
+     */
+    private void validateSize(List<Integer> playerNumbers) {
+        if (playerNumbers.size() != Constant.GAME_NUMBERS_SIZE) {
+            throw new IllegalArgumentException(PlayerInputError.PLAYER_NUMBER_WRONG_INPUT_SIZE_MESSAGE);
+        }
+    }
 }
