def64e72181f6df4b2cc108416269740134658ea
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2023-10-25 11:21:57 +0900

    Remove: remove trivial code comments

--- src/main/java/baseball/Convertor.java
+++ src/main/java/baseball/Convertor.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Convertor {
-	// 고민: Validating 로직과 리스트로 변환하는 로직을 따로 가져가고 싶은데 idDigit 안에 그 기능이 있어서 분리가 안 되는 느ㅊ무
-	// 리스트로만 반환하기 싫어서인듯? 내가 하고 싶은 것은 input이 list면 list, input이 string이면 string을 리턴하고 싶은 것일지도
-
 	public static List<Integer> convert(String input, int length) throws IllegalArgumentException {
 		// 최종 결과를 담을 List 생성
 		List<Integer> candidate = new ArrayList<>();
