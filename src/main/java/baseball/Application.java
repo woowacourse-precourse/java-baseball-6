445038e21ebdca59ee2fae650293d75201666ead
skj4244 <skj4244@naver.com>
2023-10-25 21:02:03 +0900

    feat: baseball Game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Optional;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BaseballGame baseball = new BaseballGame();
+        baseball.play();
     }
 }
