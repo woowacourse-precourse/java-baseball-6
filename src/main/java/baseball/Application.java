ea15830944931e897fb9b7d2fd98b948d045b48f
hanriverkitty <danieltls@naver.com>
2023-10-25 20:52:20 +0900

    IllegalArgumentException

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,9 +7,12 @@ import java.util.ArrayList;
 
 public class Application {
     static boolean cmd = true;
-    public static void main(String[] args) {
+
+    public static void main(String[] args)throws IllegalArgumentException{
         System.out.println("숫자 야구 게임을 시작합니다");
 
+        //게임시작
+        cmd=true;
         while(cmd) {
             ArrayList<Integer> num = new ArrayList<Integer>();
             while (num.size() < 3) {
@@ -18,14 +21,29 @@ public class Application {
                     num.add(ran_num);
                 }
             }
-            System.out.println(num);
+            //System.out.println(num);
+            //입력받아서 스트라이크와 볼 개수 맞추기
             while (true) {
                 int strike = 0;
                 int ball = 0;
 
-                //입력
+
                 System.out.print("숫자를 입력해주세요 : ");
-                String ask = Console.readLine();
+                String ask;
+                //System.out.println(ask.length());
+
+                //예외발생
+                try {
+                    ask=Console.readLine();
+                    check(ask);
+                } catch (IllegalArgumentException e) {
+                    // 예외 발생시 프로그램 종료
+                    cmd=false;
+                    throw e;
+
+                }
+
+                //입력
                 String[] ask_array = ask.split("");
 
                 //입력값 정수형으로 변환
@@ -54,16 +72,30 @@ public class Application {
                     System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                     int restart = Integer.parseInt(Console.readLine());
                     if (restart == 1) {
-
-                    } else if (restart==2) {
-                        cmd=false;
+                        break;
+                    } else if (restart == 2) {
+                        cmd = false;
                         break;
                     }
                 }
             }
             // TODO: 프로그램 구현
         }
-        return;
+
+    }
+
+    //예외사항
+    public static void check(String ask) throws IllegalArgumentException{
+        if(ask.length()!=3) {
+            cmd=false;
+            throw new IllegalArgumentException();
+        } else if ((ask.charAt(0)<123)&&(ask.charAt(0)>64))  {
+            throw new IllegalArgumentException();
+        }else if ((ask.charAt(1)<123)&&(ask.charAt(1)>64))  {
+            throw new IllegalArgumentException();
+        }else if ((ask.charAt(2)<123)&&(ask.charAt(2)>64))  {
+            throw new IllegalArgumentException();
+        }
     }
 
 }
