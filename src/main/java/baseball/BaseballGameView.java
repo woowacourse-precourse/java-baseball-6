98676840c39bf23bc8a202f08a66132441444624
grax <grax@192.168.35.119>
2023-10-25 23:22:51 +0900

    refactor: "Remove try/catch & add only throw"
    숫자야구_pjt_week01

--- src/main/java/baseball/BaseballGameView.java
+++ src/main/java/baseball/BaseballGameView.java
@@ -11,51 +11,46 @@ public class BaseballGameView {
 	// 사용자로부터 숫자 입력을 받고 배열로 변환
 	public int[] getGuessFromUser() {
 		int[] guessNumberList = new int[BaseballGameModel.getMaxSizeNumber()];
-		try {
-			displayMessage("숫자를 입력해주세요 : ");
-			String input = Console.readLine();
 
-			String[] tempList = input.split("");
+		displayMessage("숫자를 입력해주세요 : ");
+		String input = Console.readLine();
 
-			// 사용자 입력값의 자리수가 3자리가 아니면 프로그램 종료
-			if (input.length() != BaseballGameModel.getMaxSizeNumber()) {
-				throw new IllegalArgumentException();
-			}
+		String[] tempList = input.split("");
 
-			for (int i = 0; i < BaseballGameModel.getMaxSizeNumber(); i++) {
-				guessNumberList[i] = Integer.parseInt(tempList[i]);
-			}
+		// 사용자 입력값의 자리수가 3자리가 아니면 프로그램 종료
+		if (input.length() != BaseballGameModel.getMaxSizeNumber()) {
+			// try/catch문 제거: 예외를 상위 호출자에서 처리하면서 프로그램이 종료
+			throw new IllegalArgumentException("3자리 수를 입력해야 합니다.\n게임 종료!");
+		}
 
-			// 사용자 입력값이 중복이면 프로그램 종료
-			for (int i = 0; i < guessNumberList.length - 1; i++) {
-				for (int j = i + 1; j < guessNumberList.length; j++) {
-					if (guessNumberList[i] == guessNumberList[j]) {
-						throw new IllegalArgumentException();
-					}
+		for (int i = 0; i < BaseballGameModel.getMaxSizeNumber(); i++) {
+			guessNumberList[i] = Integer.parseInt(tempList[i]);
+		}
+
+		// 사용자 입력값이 중복이면 프로그램 종료
+		for (int i = 0; i < guessNumberList.length - 1; i++) {
+			for (int j = i + 1; j < guessNumberList.length; j++) {
+				if (guessNumberList[i] == guessNumberList[j]) {
+					// try/catch문 제거
+					throw new IllegalArgumentException("서로 다른 수를 입력해야 합니다.\n게임 종료!");
 				}
 			}
-
-		} catch (IllegalArgumentException e) {
-			guessNumberList = null; // 프로그램 종료
-
 		}
+
 		return guessNumberList;
 	}
 
 	public int getNewGameChoice() {
-		int inputToNumber = 0;
 
-		try {
-			displayMessage("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
-			String input = Console.readLine();
-			inputToNumber = Integer.parseInt(input);
+		displayMessage("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+		String input = Console.readLine();
+		int inputToNumber = Integer.parseInt(input);
 
-			if (inputToNumber != 1 && inputToNumber != 2) {
-				throw new IllegalArgumentException();
-			}
-		} catch (IllegalArgumentException e) {
-			inputToNumber = 2;	// return값을 2로 저장하여 게임 종료시키기
+		if (inputToNumber != 1 && inputToNumber != 2) {
+			// try/catch문 제거
+			throw new IllegalArgumentException("1과 2 중 선택해주세요.\n게임 종료!");
 		}
+
 		return inputToNumber;
 	}
 
