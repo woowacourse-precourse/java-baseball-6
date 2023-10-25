aa48a8551f1f03ca939643fff256a56d024ead83
yujeonghyeon <dbdb1114@naver.com>
2023-10-25 23:15:35 +0900

    정답시 출력문 변경 및 input 분리

--- src/main/java/baseball/handler/io/OutputHandler.java
+++ src/main/java/baseball/handler/io/OutputHandler.java
@@ -1,35 +1,33 @@
 package baseball.handler.io;
 
-import camp.nextstep.edu.missionutils.Console;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class OutputHandler {
 
-    private OutputHandler(){
+    private OutputHandler() {
     }
+
     public static void startGame() {
         System.out.println("숫자 야구게임을 시작합니다.");
     }
-    public static String correctAnswer() {
+
+    public static void correctAnswer() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        return Console.readLine();
     }
-    public static void provideHint(Map<String, Integer> hint){
+
+    public static void provideHint(Map<String, Integer> hint) {
 
         int ball = hint.get("ball");
         int strike = hint.get("strike");
-
         if (ball == 0 && strike == 0) {
-            System.out.println("낫싱");
+            System.out.print("낫싱");
         }
-        if(ball>0){
-            System.out.println(ball + "볼");
+
+        if (ball > 0) {
+            System.out.print(ball + "볼 ");
         }
-        if(strike > 0) {
-            System.out.println(strike + "스트라이크");
+        if (strike > 0) {
+            System.out.print(strike + "스트라이크");
         }
 
         System.out.println();
