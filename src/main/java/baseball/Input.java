acf8fceb7849a2e7edbd1875d0ee2a8c197700b8
leorb159 <ldgsocial159@naver.com>
2023-10-25 19:48:53 +0900

    feat(Input): 검증이 끝난 숫자를 반환하는 기능 구현

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -1,10 +1,21 @@
 package baseball;
 import camp.nextstep.edu.missionutils.Console;
+
 public class Input {
 
-
+    private Check check;
+    public Input(){
+        check = new Check();
+    }
     //숫자 3자리 입력받기
-
+     public int[] varlidateInput(String input){
+        check.checkingInput(input);
+        int[] inputList = new int[3];
+        for(int i=0; i<inputList.length; i++){
+            inputList[i] = input.charAt(i) - '0';
+        }
+        return inputList;
+     }
     //1또는 2 입력받기
 
 
