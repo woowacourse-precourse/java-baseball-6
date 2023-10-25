f6d4956626bec5f90bf92d5a5ed49777f6a05fa9
brixxt27 <brixxt27@gmail.com>
2023-10-25 23:02:26 +0900

    feat: main 문 구현 완료
    - 문자열 비교에는 equals() 를 사용해야 한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,17 +2,41 @@ package baseball;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
-
 public class Application {
     private final static int MAX_LENGTH = 3;
 
     public static void main(String[] args) {
-        String str = readLine();
         RandomNumbers randomNumbers = new RandomNumbers();
+        boolean isStarted = true;
 
-        for (int j = 0; j < MAX_LENGTH; j++) {
-            System.out.print(randomNumbers.getNumsArray()[j]);
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (true) {
+            if (isStarted) {
+                randomNumbers.generateRandomNums();
+                isStarted = false;
+            }
+            System.out.print("숫자를 입력해주세요 : ");
+            String input = readLine();
+
+            MyNumbers myNumbers = new MyNumbers(input);
+            Calculator.calculateGameResult(myNumbers.getNums(), randomNumbers.getNums());
+
+            ResultPrinter.printResult(Calculator.getBall(), Calculator.getStrike());
+            Calculator.initializeFields();
+            if (ResultPrinter.isDone()) {
+                ResultPrinter.initializeFiled();
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                String str = readLine();
+
+                if (str.equals("1")) {
+                    isStarted = true;
+                    continue;
+                } else if (str.equals("2")) {
+                    break;
+                } else {
+                    throw new IllegalArgumentException();
+                }
+            }
         }
-        System.out.println("\n" + str);
     }
 }
