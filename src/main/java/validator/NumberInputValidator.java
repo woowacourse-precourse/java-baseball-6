57792f9738492b388398bf75a4720a1d6f6d420c
LeeSungNo-ian <isnn0103@gmail.com>
2023-10-25 19:34:45 +0900

    feat: 공의 범위 확인을 위한 vaildNumberRange 함수 생성

--- src/main/java/validator/NumberInputValidator.java
+++ src/main/java/validator/NumberInputValidator.java
@@ -12,4 +12,12 @@ public class NumberInputValidator {
             throw new IllegalArgumentException("공의 개수가 3개 보다 적어요.");
         }
     }
+
+    void vaildNumberRange(String balls) {
+        for (char ball: balls.toCharArray()) {
+            if (ball < '0' && ball > '9') {
+                throw new IllegalArgumentException("숫자가 아니거나, 범위를 넘었어요.");
+            }
+        }
+    }
 }
