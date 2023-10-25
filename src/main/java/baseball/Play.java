e78daf889bd0677486b7bd9d4a64b2cba30e4630
동원 <ai06021@gmail.com>
2023-10-25 23:51:13 +0900

    fix : throw 던지기 수정

--- src/main/java/baseball/Play.java
+++ src/main/java/baseball/Play.java
@@ -1,6 +1,5 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,16 +8,22 @@ public class Play {
     public void game(String input) {
 
         List<Integer> computer = new Computer().anotherNumber();
-        int value = 0;
-        while (value != 3) {
+        String result;
+        while (true) {
             System.out.print("숫자를 입력해주세요 : ");
             List<Integer> player = change(input);
 
-            value = compare(player, computer);
+            result = compare(player, computer);
+            if (result.equals("3스트라이크")) {
+                System.out.println("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            }
+            System.out.println(result);
+
         }
-        System.out.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
+    //String형으로 들어온 input값 List<Integer>로 형변환
     public List<Integer> change(String input) {
 
         List<Integer> inputNumber = new ArrayList<>();
@@ -30,7 +35,9 @@ public class Play {
         return inputNumber;
     }
 
-    public boolean inputCheck(String input) {
+
+    // input으로 들어오는 값 전체 에러 확인
+    public void inputCheck(String input) {
         List<Character> inputNumber = new ArrayList<>();
         for (int i = 0; i < inputNumber.size(); i++) {
             char wordNumber = input.charAt(i);
@@ -45,40 +52,24 @@ public class Play {
                 throw new IllegalArgumentException();
             }
         } catch (IllegalArgumentException e) {
-            return false;
+            throw new IllegalArgumentException(e);
         }
-        return true;
     }
 
     private boolean numberCheck(String input) {
         for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) < '0' || input.charAt(i) > '9') {
+            if (!(input.charAt(i) >= '1' && input.charAt(i) <= '9')) {
                 return false;
             }
         }
         return true;
     }
 
-    public int newGameCheck() {
-        try {
-            String input = Console.readLine();
-            if (input.length() != 1) {
-                throw new IllegalArgumentException();
-            }
-            int newgame = Integer.parseInt(input);
-            if (newgame == 1) {
-                return 1;
-            } else if (newgame == 2) {
-                return 0;
-            } else {
-                throw new IllegalArgumentException();
-            }
-        } catch (IllegalArgumentException e) {
-            return 2;
-        }
-    }
+    // 새로운 게임 시작하느냐 결정, 중간 1, 2이외 다른값 예외처리
 
-    public int compare(List<Integer> player, List<Integer> answer) {
+
+    // 컴퓨터(상대방) 나의 숫자(input) 비교
+    public String compare(List<Integer> player, List<Integer> answer) {
         int strike = 0;
         int ball = 0;
 
@@ -91,14 +82,13 @@ public class Play {
             }
         }
         if (strike == 0 && ball == 0) {
-            System.out.println("낫싱");
+            return ("낫싱");
         } else if (strike == 0) {
-            System.out.println(ball + "볼");
+            return (ball + "볼");
         } else if (ball == 0) {
-            System.out.println(strike + "스트라이크");
+            return (strike + "스트라이크");
         } else {
-            System.out.println(ball + "볼 " + strike + "스트라이크");
+            return (ball + "볼 " + strike + "스트라이크");
         }
-        return strike;
     }
 }
