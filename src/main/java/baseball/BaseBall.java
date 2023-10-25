89be2c63177a547095ad0a3f81e0f845aac29500
JunRain <sa2313@tukorea.ac.kr>
2023-10-25 15:11:49 +0900

    3 스트라이크 시 게임 종료, 볼 스트라이크 검사 구현

--- src/main/java/baseball/BaseBall.java
+++ src/main/java/baseball/BaseBall.java
@@ -8,12 +8,18 @@ import java.util.stream.Stream;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class BaseBall {
-	Scanner scanner = new Scanner(System.in);
+	Scanner scanner;
+
+	public BaseBall() {
+		this.scanner = new Scanner(System.in);
+		startGame();
+	}
 
 	void startGame() {
 		int gameFlag = 1;
 
 		while (gameFlag == 1) {
+			System.out.println("숫자 야구 게임을 시작합니다.");
 			baseBallGame();
 
 			System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
@@ -38,6 +44,31 @@ public class BaseBall {
 			userInput = scanner.nextInt();
 			List<Integer> user = checkNum(userInput);
 
+			for (int i = 0; i < 3; i++) {
+				if (computer.contains(user.get(i))) {
+					ball++;
+				}
+				if (computer.get(i) == user.get(i)) {
+					strike++;
+					ball--;
+				}
+			}
+
+			if (ball == 0 && strike == 0) {
+				System.out.println("낫싱");
+			} else if (ball == 0) {
+				System.out.println(strike + "스트라이크");
+			} else if (strike == 0) {
+				System.out.println(ball + "볼");
+			} else if (ball > 0 && strike > 0) {
+				System.out.println(ball + "볼 " + strike + "스트라이크");
+			}
+
+			if (strike == 3) {
+				System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+				return;
+			}
+
 		}
 	}
 
@@ -51,9 +82,8 @@ public class BaseBall {
 
 		user = Stream.of(String.valueOf(userInput).split("")).mapToInt(Integer::parseInt).boxed().toList();
 
-		for (int i = 0; i < 3; i++) {
-			if (user.contains(user.get(i)))
-				throw new IllegalArgumentException();
+		if(user.size() != user.stream().distinct().count()){
+			throw new IllegalArgumentException();
 		}
 
 		return user;
