c7e09d13ba387a72a82742c38c6d696bbbd968b8
win9 <krrr883@gmail.com>
2023-10-25 17:23:06 +0900

    [Feat]: 3자리의 숫자가 아니면 에러를 발생하도록 변경
    요구사항에 따라 3자리의 숫자가 아니면 IllegalArgumentException을
    발생

--- src/main/java/baseball/Utils/Converter.java
+++ src/main/java/baseball/Utils/Converter.java
@@ -2,6 +2,10 @@ package baseball.Utils;
 
 public class Converter {
     public int[] convertUserChooseInputToInt(String input) {
+        if (!input.matches("\\d{3}")) {
+            throw new IllegalArgumentException();
+        }
+
         int[] output = new int[3];
         for(int i = 0; i < 3; i++) {
             output[i] = Integer.parseInt(input.substring(i, i + 1));
@@ -11,6 +15,9 @@ public class Converter {
     }
 
     public int convertUserInputToInt(String input) {
+        if (!(input.equals("1") || input.equals("2"))) {
+            throw new IllegalArgumentException();
+        }
         return Integer.parseInt(input);
     }
 
