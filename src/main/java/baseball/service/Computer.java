b5fa9a993dfd06612dc924fef4ddde79bffcef0b
strongcookdas <goodjav141592@gmail.com>
2023-10-25 20:37:43 +0900

    feat : 숫자 비교 메소드 getResult 구현

--- src/main/java/baseball/service/Computer.java
+++ src/main/java/baseball/service/Computer.java
@@ -9,6 +9,11 @@ public class Computer {
     private final int MAX_NUMBER = 9;
     private final int MIN_NUMBER = 1;
 
+    private final Integer LIST_SIZE = 3;
+
+    private Integer ball = 0;
+    private Integer strike = 0;
+
     public List<Integer> generateNumber() {
         List<Integer> randomNumbers = new ArrayList<>();
         while (randomNumbers.size() < 3) {
@@ -18,5 +23,35 @@ public class Computer {
             }
         }
         return randomNumbers;
+    }
+
+    public String getResult(List<Integer> randomNumbers, List<Integer> numbers) {
+        for (int i = 0; i < LIST_SIZE; i++) {
+            for (int j = 0; j < LIST_SIZE; j++) {
+                if (randomNumbers.get(i) == numbers.get(j)) {
+                    if (i == j) {
+                        this.strike++;
+                    } else {
+                        this.ball++;
+                    }
+                }
+            }
+        }
+
+        return this.getMessage();
+    }
+
+    public String getMessage() {
+
+        if (this.ball > 0 && this.strike > 0) {
+            return this.ball + "볼 " + this.strike + "스트라이크";
+        } else if (this.ball > 0) {
+            return this.ball + "볼";
+        } else if (this.strike == 3) {
+            return this.strike + "스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+        } else if (this.strike > 0) {
+            return this.strike + "스트라이크";
+        }
+        return "낫싱";
     }
 }
