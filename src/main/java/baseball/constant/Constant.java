8de7d86d75daf9499c1fe0f229941d8aaee808f9
Chaeros <chpark6851@koreatech.ac.kr>
2023-10-25 19:00:43 +0900

    chore: 숫자야구게임 상수 추가
    입력받을 수의 개수(3), 입력할 수 있는 수의 시작(1), 끝(9)을 뜻하는
    상수를 추가했습니다.

---
+++ src/main/java/baseball/constant/Constant.java
@@ -0,0 +1,7 @@
+package baseball.constant;
+
+public class Constant {
+    public static final int GAME_NUMBERS_SIZE = 3;
+    public static final int START_RANGE = 1;
+    public static final int END_RANGE = 9;
+}
