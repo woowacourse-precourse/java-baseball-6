cbabdc0df072ccddb3efa6677d89bde9ebaf772b
ljkhyeong <jolri24@gmail.com>
2023-10-25 23:04:33 +0900

    refactor : Computer 클래스 리팩토링

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,19 +1,26 @@
 package baseball;
 
+import java.util.Arrays;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Computer {
 
 	private Ball[] balls;
 
 	public Computer() {
-		balls = generateBalls();
 	}
 
-	private Ball[] generateBalls() {
+	public void generateBalls() {
 		Ball[] balls = new Ball[3];
 		BallBucket ballBucket = new BallBucket();
 		for (int i = 0; i < 3; i++) {
 			balls[i] = ballBucket.getBall();
 		}
+		this.balls = balls;
+	}
+
+	public Ball[] getBalls() {
 		return balls;
 	}
+
 }
