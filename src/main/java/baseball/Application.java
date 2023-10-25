741bae40d3c6abba086d226b65167c7bfd54725d
TaeDongUm <etd93@naver.com>
2023-10-25 23:15:31 +0900

    [feat] ComputerNum 클래스 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,33 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    	// test6
-    }
+	public static void main(String[] args) {
+		System.out.println("숫자 야구 게임을 시작합니다.");
+	}
+	
+	class ComputerNum {
+	    List<Integer> computerNum;
+
+	    public ComputerNum() {
+	        computerNum = pickRandomNum();
+	    }
+
+	    private List<Integer> pickRandomNum() {
+	        List<Integer> numList = new ArrayList<>();
+	        
+	        while(numList.size() <3) {
+	        	int newNum = Randoms.pickNumberInRange(1, 9);
+	        	if(numList.contains(newNum)) continue;
+	        	numList.add(newNum);
+	        }
+	        return numList;
+	    }
+	}
 }
+
+
