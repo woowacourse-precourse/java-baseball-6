22eb9898182c30b9c882f09f4b8d5d8625a7ff10
june-777 <wlwhswnsrl96@gmail.com>
2023-10-25 14:35:18 +0900

    refactor: 클래스명 수정본 미반영을 반영

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,7 +5,7 @@ import baseball.controller.GameManager;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        AppConfig appConfig = new AppConfig();
+        ApplicationConfig appConfig = new ApplicationConfig();
         GameManager gameManager = appConfig.gameController();
         gameManager.initialGameStart();
     }
