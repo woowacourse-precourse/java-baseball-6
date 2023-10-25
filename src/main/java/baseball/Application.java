6331b0a3e2b7350d44714055676ef59da692bdd5
Yminji <ymg0818@naver.com>
2023-10-25 19:24:18 +0900

    숫자야구 게임

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,7 +5,6 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class Application {
     public static void main(String[] args) {
