86d328220e58c3608923420d85398c90b164b5ad
gundorit <packdev937@gmail.com>
2023-10-25 17:19:57 +0900

    test(GameStatus) : 유효한 수 입력 시 올바른 값을 반환하는지 확인하는 테스트 코드를 작성한다

--- src/test/java/baseball/domain/GameStatusTest.java
+++ src/test/java/baseball/domain/GameStatusTest.java
@@ -11,4 +11,11 @@ class GameStatusTest {
     void 유효하지_않은_수를_입력하면_예외_발생() {
         assertThrows(IllegalArgumentException.class, () -> GameStatus.fromInt(3));
     }
+
+    @Test
+    void 유효한_수를_입력하면_올바른_값을_반환() {
+        assertEquals(GameStatus.CONTINUE, GameStatus.fromInt(0));
+        assertEquals(GameStatus.RESTART, GameStatus.fromInt(1));
+        assertEquals(GameStatus.END, GameStatus.fromInt(2));
+    }
 }
