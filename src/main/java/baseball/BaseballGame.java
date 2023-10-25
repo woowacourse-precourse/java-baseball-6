4e34c2e78f64211a8351256b49635617dac8bfb9
LeeSeungUk <tmddnr5410@gmail.com>
2023-10-25 19:58:16 +0900

    refactor : 게임 view를 직접 만들도록 하고 static 메소드 삭제

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -3,33 +3,32 @@ package baseball;
 import java.util.ArrayList;
 
 public class BaseballGame {
-    private String answerPlayer;
-    private boolean isPlaying;
     final int STRIKE = 0;
     final int BALL = 1;
 
 
     public void startGame() {
         Model baseballModel = new Model();
+        View baseballView = new View();
         baseballModel.initComputerAnswer();
-        View.outputGameStart();
+        baseballView.outputGameStart();
 
         String answerPlayer;
         boolean isPlaying=true;
         Integer[] score;
 
         do {
-            answerPlayer = View.inputPlayerAnswer();
+            answerPlayer = baseballView.inputPlayerAnswer();
             baseballModel.validateCheckInputAnswer(answerPlayer);
 
 
             score = baseballModel.countStrikeBallHits(answerPlayer);
-            View.outputGameScore(score[STRIKE], score[BALL]);
+            baseballView.outputGameScore(score[STRIKE], score[BALL]);
 
 
             //answer player
             if (score[STRIKE] == 3) {
-                String playerInput = View.inputPlayerRestart();
+                String playerInput = baseballView.inputPlayerRestart();
                 baseballModel.validateCheckInputRestart(playerInput);
                 isPlaying = baseballModel.restartGame(playerInput);
             }
--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -3,11 +3,11 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 public class View {
-    public static void outputGameStart() {
+    public void outputGameStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    public static void outputGameScore(final int strikeCnt, final int ballCnt) {
+    public void outputGameScore(final int strikeCnt, final int ballCnt) {
         String messageBall = "";
         String messageStrike = "";
 
@@ -27,12 +27,12 @@ public class View {
         }
     }
 
-    public static String inputPlayerAnswer() {
+    public String inputPlayerAnswer() {
         System.out.print("숫자를 입력해주세요 : ");
         return Console.readLine();
     }
 
-    public static String inputPlayerRestart() {
+    public String inputPlayerRestart() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         return Console.readLine();
