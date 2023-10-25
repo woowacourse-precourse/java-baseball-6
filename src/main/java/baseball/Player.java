a991dabbeb969181484e38255b7b0581c4ab749d
meridaKim <sujin7187@naver.com>
2023-10-25 20:07:07 +0900

    feat:add checkDuplicates to check Duplicate
    check Duplicate player's numbers

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,5 +1,8 @@
 package baseball;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Player {
     private static final int PLAYER_MAXIUM_NUM = 999;
     private static final int PLAYER_MINIUM_NUM = 100;
@@ -11,8 +14,18 @@ public class Player {
 
 
     void validateNumber(Player player) {
-        if (player.player_num > PLAYER_MAXIUM_NUM || player.player_num < PLAYER_MINIUM_NUM)
+        if (player.player_num > PLAYER_MAXIUM_NUM || player.player_num < PLAYER_MINIUM_NUM) {
             throw new IllegalArgumentException("예외입니다.");
+        }
+    }
+
+    void checkDuplicates(int[] player_numbers) {
+        Set<Integer> duplicate = new HashSet<>();
+        for (int i : player_numbers) {
+            if (!duplicate.add(i)) {
+                throw new IllegalArgumentException("중복입니다.");
+            }
+        }
     }
 
 
