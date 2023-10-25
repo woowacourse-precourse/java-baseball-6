7a8649737c99d6ff3bae68f806a3390bb6772909
Sungkyu <kxu4583@naver.com>
2023-10-25 09:35:51 +0900

    '기능 3: 컴퓨터와 사용자의 숫자를 받아 몇 볼 몇 스트라이크인지 출력하는 함수 만들기' 구현 및 정답 맞출 때까지 반복 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,6 +5,30 @@ import camp.nextstep.edu.missionutils.Randoms;
 import org.assertj.core.internal.IntArrays;
 
 public class Application {
+    static boolean judge(int[] answer, int[] ask){
+
+        int strike=0;
+        int ball=0;
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++) {
+                if (answer[j] == ask[i]) {
+                    if(i==j) strike++;
+                    else ball++;
+                }
+            }
+        }
+
+        if(ball==0 && strike==0) System.out.print("낫싱");
+        if(ball!=0) System.out.print(ball + "볼 ");
+        if(strike!=0) System.out.print(strike + "스트라이크");
+        System.out.println();
+        if(strike==3){
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        }
+        return false;
+    }
     public static void main(String[] args) {
 
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -19,14 +43,17 @@ public class Application {
             }
         }
 
-        System.out.print("숫자를 입력해주세요 : ");
-
         int[] ask = new int[3];
-        String readLine = Console.readLine();
-        for(int i=0; i<3; i++){
-            ask[i] = readLine.charAt(i)-'0';
-        }
+        do{
+            System.out.print("숫자를 입력해주세요 : ");
 
+            //사용자로부터 3개의 숫자 입력받기
+            String readLine = Console.readLine();
+            for(int i=0; i<3; i++){
+                ask[i] = readLine.charAt(i)-'0';
+            }
+        }
+        while(!judge(answer, ask));
 
     }
 }
