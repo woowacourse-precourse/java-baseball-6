fabeb280bdd1b066310b968c5939fba23384dc1b
www-author <dream970215@naver.com>
2023-10-25 20:50:11 +0900

    :speech_balloon: style: main 메서드 불 필요한 주석 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,7 +5,6 @@ import baseball.controller.BaseballController;
 public class Application {
 
     public static void main(String[] args)  {
-        // TODO: 프로그램 구현
         BaseballController baseballGame = new BaseballController();
         baseballGame.start();
     }
