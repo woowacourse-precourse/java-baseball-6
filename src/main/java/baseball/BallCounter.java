9a58abcd64a6045cf388caf353ed10b4ddb1ee8b
siyeonSon <siyeon0209@naver.com>
2023-10-25 11:58:58 +0900

    refactor: BallCounter, StrikeCounter 내 메서드 명 변경

--- src/main/java/baseball/BallCounter.java
+++ src/main/java/baseball/BallCounter.java
@@ -2,14 +2,14 @@ package baseball;
 
 import java.util.List;
 
-public class BallCounter {
+public class BallCounter implements Counter {
     public int judge(List<Integer> computer, List<Integer> number) {
         return (int) number.stream()
-                .filter(n -> isBall(computer, number, n))
+                .filter(n -> compare(computer, number, n))
                 .count();
     }
 
-    private boolean isBall(List<Integer> computer, List<Integer> number, Integer n) {
+    private boolean compare(List<Integer> computer, List<Integer> number, Integer n) {
         System.out.println("볼: " + computer.indexOf(n) + number.indexOf(n));
         return computer.contains(n) && computer.indexOf(n) != number.indexOf(n);
     }
--- src/main/java/baseball/StrikeCounter.java
+++ src/main/java/baseball/StrikeCounter.java
@@ -5,11 +5,11 @@ import java.util.List;
 public class StrikeCounter implements Counter {
     public int judge(List<Integer> computer, List<Integer> number) {
         return (int) number.stream()
-                .filter(n -> isStrike(computer, number, n))
+                .filter(n -> compare(computer, number, n))
                 .count();
     }
 
-    private boolean isStrike(List<Integer> computer, List<Integer> number, Integer n) {
+    private boolean compare(List<Integer> computer, List<Integer> number, Integer n) {
         System.out.println("스트라이크: " + computer.indexOf(n) + number.indexOf(n));
         return computer.indexOf(n) == number.indexOf(n);
     }
