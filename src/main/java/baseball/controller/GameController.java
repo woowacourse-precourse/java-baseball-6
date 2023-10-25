61b9046205e3d21e688beb6fbff8fa5c370229eb
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-10-25 22:15:36 +0900

    feat: 컨트롤러 세팅

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,4 +1,30 @@
 package baseball.controller;
 
+import baseball.model.BaseballGame;
+import baseball.model.Validator;
+import baseball.view.UserView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static baseball.model.Constants.*;
+
 public class GameController {
+
+    private UserView userView;
+    private Validator validator;
+    private BaseballGame baseballGame;
+
+    public GameController() {
+        userView = new UserView();
+        validator = new Validator();
+        baseballGame = new BaseballGame();
+    }
+
+    public void playBaseballGame() {
+        userView.displayStartMessage();
+
+        //TODO playGame 코드
+    }
+
 }
