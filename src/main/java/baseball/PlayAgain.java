54cdb58d3968c4a73561aad7f3da07cf9923baa4
ysparrk <youngseo0703@gmail.com>
2023-10-25 21:48:31 +0900

    fix: input readline으로 변경

--- src/main/java/baseball/PlayAgain.java
+++ src/main/java/baseball/PlayAgain.java
@@ -2,12 +2,15 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Scanner;
+
 public class PlayAgain {
     public boolean playAgain(){
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        String answer = Console.readLine();
-        if(answer.charAt(0) == '1'){
+        String input = Console.readLine();
+        char answer = input.charAt(0);
+        if(answer == '1'){
             return true;
         }
         return false;
