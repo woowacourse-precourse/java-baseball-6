a76482a9ccd1a2c2b660697990c4c46d548947be
김라연 <rayeon@gimlayeon-ui-noteubug.local>
2023-10-25 09:55:37 +0900

    feat:랜덤한 3개의 숫자 생성기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,16 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
 
         startPrint();
+
+        List<Integer> computerNumbers = generateRandomNumbers();
 
     }
 
@@ -13,6 +18,16 @@ public class Application {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
+    public static List<Integer> generateRandomNumbers(){
+        List<Integer> computerNumbers = new ArrayList<>();
+        while (computerNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumbers.contains(randomNumber)) {
+                computerNumbers.add(randomNumber);
+            }
+        }
+        return computerNumbers;
+    }
 
 
 }
