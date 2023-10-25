f46d4b1c4b6ccfc6a570d7ce2159ac37a574927e
Yang woosung <ngxk1217@gmail.com>
2023-10-25 14:38:51 +0900

    세자리 수 검증

--- src/main/java/baseball/number/RandomNumbers.java
+++ src/main/java/baseball/number/RandomNumbers.java
@@ -1,11 +1,13 @@
 package baseball.number;
 
 import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class RandomNumbers {
+
     private static final int START_INCLUSIVE = 1;
     private static final int END_INCLUSIVE = 9;
     private static final int COUNT = 3;
--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,4 +1,19 @@
 package baseball.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    private static final String REQUIRE_BASEBALL_NUMBER = "숫자를 입력해주세요";
+    private static final String CONTINUE_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public static String requireBASEBALLNUMBER(){
+        System.out.println(REQUIRE_BASEBALL_NUMBER);
+        return Console.readLine();
+    }
+    public static String askContinue(){
+        System.out.println(CONTINUE_MESSAGE);
+        return Console.readLine();
+    }
+
 }
