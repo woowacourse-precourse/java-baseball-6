16eeaed9ec9bcf19376a2439f90de3626554e9fa
ljh <dusk5588@naver.com>
2023-10-25 21:20:38 +0900

    [Feat] InputView 클래스 구현

--- src/main/java/baseball/View/InputView.java
+++ src/main/java/baseball/View/InputView.java
@@ -1,4 +1,16 @@
 package baseball.View;
 
 public class InputView {
+
+    String inputMessage = "숫자를 입력해주세요 : ";
+    String retryMessage = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public void printInputMessage() {
+        System.out.print(inputMessage);
+    }
+
+    public void printRetryMessage() {
+        System.out.println(retryMessage);
+    }
+
 }
