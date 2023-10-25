fd9555cad95d8e112643fc125087fb29d27b717a
minkyole <7719s@nave.com>
2023-10-25 20:31:07 +0900

    style : User class 내 restartNum class 변수에서 지역 변수로 변경

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -4,7 +4,6 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class User {
     private BaseballNum userNum;
-    static private RestartNum restartNum;
 
     void inputNum() {
         System.out.print("숫자를 입력해주세요 : ");
@@ -12,7 +11,7 @@ public class User {
     }
 
     static boolean chooseRestart() {
-        restartNum = new RestartNum();
+        RestartNum restartNum = new RestartNum();
         return (restartNum.getRestartNum() == 1);
     }
 
