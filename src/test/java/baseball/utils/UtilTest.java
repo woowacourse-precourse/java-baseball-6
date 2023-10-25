685f33993bf37a537ed3ce4b0e78481b007fb4a3
donglee99 <jhnj741@naver.com>
2023-10-25 09:32:39 +0900

    test : 변경 된 유틸 메소드에 맞게 테스트 코드 수정

--- src/test/java/baseball/utils/UtilTest.java
+++ src/test/java/baseball/utils/UtilTest.java
@@ -1,4 +1,4 @@
-package baseball.util;
+package baseball.utils;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,7 +13,7 @@ public class UtilTest {
     @Test
     @DisplayName("숫자 타입 검증 성공 케이스")
     void 숫자_타입_검증_성공() {
-        boolean result = Util.validNumberType(1);
+        boolean result = NumberUtil.validNumberType(1);
         assertThat(result).isEqualTo(true);
     }
 
@@ -21,14 +21,14 @@ public class UtilTest {
     @DisplayName("숫자 타입 검증 실패 케이스")
     void 숫자_타입_검증_실패() {
         assertThrows(IllegalArgumentException.class, () -> {
-            Util.validNumberType("String");
+            NumberUtil.validNumberType("String");
         });
     }
 
     @Test
     @DisplayName("숫자 범위 검증 성공 케이스")
     void 숫자_범위_검증_성공() {
-        boolean result = Util.validNumberRange(1, 9, 3);
+        boolean result = NumberUtil.validNumberRange(1, 9, 3);
         assertThat(result).isEqualTo(true);
     }
 
@@ -36,14 +36,14 @@ public class UtilTest {
     @DisplayName("숫자 범위 검증 실패 케이스")
     void 숫자_범위_검증_실패() {
         assertThrows(IllegalArgumentException.class, () -> {
-            Util.validNumberRange(1, 9, 10);
+            NumberUtil.validNumberRange(1, 9, 10);
         });
     }
 
     @Test
     @DisplayName("숫자 중복 검증 성공 케이스")
     void 숫자_중복_검증_성공() {
-        boolean result = Util.validDuplicatedNumbers(Arrays.asList(1, 2, 3));
+        boolean result = NumberUtil.validDuplicatedNumbers(Arrays.asList(1, 2, 3));
         assertThat(result).isTrue();
     }
 
@@ -51,14 +51,14 @@ public class UtilTest {
     @DisplayName("숫자 중복 검증 실패 케이스")
     void 숫자_중복_검증_실패() {
         assertThrows(IllegalArgumentException.class, () -> {
-            Util.validDuplicatedNumbers(Arrays.asList(1, 1, 1));
+            NumberUtil.validDuplicatedNumbers(Arrays.asList(1, 1, 1));
         });
     }
 
     @Test
     @DisplayName("숫자 3자리 수 인지 검증 성공 케이스")
     void 숫자_자리수_검증_성공() {
-        boolean result = Util.validNumbersCount(Arrays.asList(1, 2, 3), 3);
+        boolean result = NumberUtil.validNumbersCount(Arrays.asList(1, 2, 3), 3);
         assertThat(result).isTrue();
     }
 
@@ -66,7 +66,7 @@ public class UtilTest {
     @DisplayName("숫자 3자리 수 인지 검증 실패 케이스")
     void 숫자_자리수_검증_실패() {
         assertThrows(IllegalArgumentException.class, () -> {
-            Util.validNumbersCount(Arrays.asList(1, 2), 3);
+            NumberUtil.validNumbersCount(Arrays.asList(1, 2), 3);
         });
     }
 }
