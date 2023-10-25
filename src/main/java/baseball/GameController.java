2b2fc10748e1a751da94bedfd48bfdc6a8a16bdd
davemins <davemins4125@naver.com>
2023-10-25 20:11:34 +0900

    refactor(GameController) : hadleGameEnd() 메소드의 책임 분리

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -20,14 +20,11 @@ public class GameController {
         while(gameRunning) {
             getUserInput();
             checkInput();
-
             printHint();
-
-            if (strike == 3) {
-                handleGameEnd();
-            }
+            handleGameEnd();
         }
     }
+
 
     private void initializeGame() {
         gameOutputPrinter.printStartGame();
@@ -50,15 +47,33 @@ public class GameController {
     }
 
     private void handleGameEnd() {
+        if (isGameEnd()) {
+            restartOrEndGame();
+        }
+    }
+
+    private void restartOrEndGame() {
         userAnswer = gameInputReader.enterAnswerRestartGame();
         isValidUserAnswerInput(userAnswer);
+
         if (userAnswer.equals("1")) {
-            RandomNumberCreater randomNumberCreater = new RandomNumberCreater();
-            randomNumber = randomNumberCreater.getRandomNumber();
-            gameRunning = true;
+            restartGame();
         } else {
-            gameRunning = false;
+            endGame();
         }
+    }
+
+    private void restartGame() {
+        randomNumber = randomNumberCreater.getRandomNumber();
+        gameRunning = true;
+    }
+
+    private void endGame() {
+        gameRunning = false;
+    }
+
+    private boolean isGameEnd() {
+        return strike == 3;
     }
 
     private void isValidUserNumberInput(String input) {
