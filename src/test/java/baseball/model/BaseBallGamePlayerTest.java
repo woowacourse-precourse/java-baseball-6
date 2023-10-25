3f159c636313eb9cf6c9a654949bd216e4b40243
chosunghyun18 <krismon@naver.com>
2023-10-25 11:30:22 +0900

    test : add 용자가 4자리 수를 받아 공을 생성 실패

--- src/test/java/baseball/model/BaseBallGamePlayerTest.java
+++ src/test/java/baseball/model/BaseBallGamePlayerTest.java
@@ -1,8 +1,8 @@
 package baseball.model;
 
 
-
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import baseball.model.ball.Balls;
 import java.util.Arrays;
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.Test;
 public class BaseBallGamePlayerTest {
 
     @Test
-    @DisplayName("사용자 공 생성 확인")
+    @DisplayName("사용자가 3자리 수를 받아 공을 생성")
     public void playerBallsGenerateTest() {
         BaseBallGamePlayer baseBallGamePlayer = new BaseBallGamePlayer();
         List<Integer> givenNUmber = Arrays.asList(1, 2, 3);
@@ -21,6 +21,14 @@ public class BaseBallGamePlayerTest {
 
         Balls balls = baseBallGamePlayer.generateBalls(givenNUmber);
         assertThat(balls).isEqualTo(targetBalls);
+    }
+
+    @Test
+    @DisplayName("사용자가 4자리 수를 받아 공을 생성 실패")
+    public void playerBallsGenerateFailTest() {
+        BaseBallGamePlayer baseBallGamePlayer = new BaseBallGamePlayer();
+        List<Integer> givenNUmber = Arrays.asList(1, 2, 3, 4);
+        assertThatThrownBy(()->baseBallGamePlayer.generateBalls(givenNUmber)).isInstanceOf(IllegalArgumentException.class);
     }
 
 }
