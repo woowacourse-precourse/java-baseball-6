02ffdf87690ca3bb12ebec46faac48ad6cc8b771
jenakxm <jenakxm@gmail.com>
2023-10-25 17:29:15 +0900

    기능구현완료, 예외처리 테스트 미통과

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        PlayBaseball playBaseball = new PlayBaseball();
+        boolean playAgain = true;
+
+        System.out.print("숫자 야구 게임을 시작합니다.\n");
+        while (playAgain) {
+            playBaseball.playBall();
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String input = Console.readLine();
+
+            try{
+                if ("1".equals(input)) {
+                } else if ("2".equals(input)) {
+                    playAgain = false;
+                } else {
+                    throw new IllegalArgumentException();
+                }
+            } catch (IllegalArgumentException e){
+                System.out.println("잘못된 입력값입니다. 애플리케이션을 종료합니다.");
+                break;
+            }
+        }
+        System.out.print("게임 종료");
     }
 }
