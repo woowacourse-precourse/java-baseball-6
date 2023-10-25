f7bc229c4aa163b7530f0b251fb03850082a4fc4
Sur0n <skyzephyr333@gmail.com>
2023-10-25 20:17:47 +0900

    rafactor: Change Methods and Variable Name refactor Main , seperate Methods and Variable Names.

--- docs/README.md
+++ docs/README.md
@@ -1,34 +1,58 @@
 1. 미션은 기능 요구 사항, 프로그래밍 요구 사항, 과제 진행 요구 사항 세 가지로 구성되어 있다.
 2. 세 개의 요구 사항을 만족하기 위해 노력한다. 특히 기능을 구현하기 전에 기능 목록을 만든다.
 3. 기능 요구 사항에 기재되지 않은 내용은 스스로 판단하여 구현한다.
+
+https://velog.io/@mquat/git-git%EA%B3%BC-github-wrap-up
+--git 사용법
+
 https://jjeongil.tistory.com/2124
+--객체 클래스 개념
+
+라이브러리
+camp.nextstep.edu.missionutils에서 제공하는 Randoms 및 Console API를 사용하여 구현해야 한다.
+Random 값 추출은 camp.nextstep.edu.missionutils.Randoms의 pickNumberInRange()를 활용한다.
+사용자가 입력하는 값은 camp.nextstep.edu.missionutils.Console의 readLine()을 활용한다.
+
+https://velog.io/@im_chaedong/Intellij-%EC%97%90%EC%84%9C-java.lang-%EA%B0%99%EC%9D%80-%EA%B8%B0%EB%B3%B8-%EB%9D%BC%EC%9D%B4%EB%B8%8C%EB%9F%AC%EB%A6%AC-%EB%B3%B4%EB%8A%94-%EB%B0%A9%EB%B2%95
+--라이브러리 확인방법
+
+https://mainia.tistory.com/2323
+--리스트 자료구조 사용법
+
+https://playground.naragara.com/513/
+--리스트 비교법
+
+https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=heartflow89&logNo=220964832916
+--final뜻
 
 구현할 기능 목록 (기능이 작동하면 커밋해보기)
-1. 서로 다른 3자리의 수를 입력받는 객체
-https://st-lab.tistory.com/92
--- 변수 숫자가 아니어도 오류 // 미완성
--- tochararray 변환해서 length 3자리 이상일경우 IllegalArgumentException
-https://cheershennah.tistory.com/147
 
-https://www.freecodecamp.org/korean/news/java-string-to-int-how-to-convert-a-string-to-an-integer/
-완료
--- asdf넣으면 입력오류 // 미완성
-2. 3자리의 수가 서로 다른 수인지 확인하는 객체 1==2? 2==3? 1==3? 
--- IllegalArgumentException
-3. 컴퓨터가 서로 다른 수를 만드는 객체
-random num 활용
-https://offbyone.tistory.com/237
-https://roeldowney.tistory.com/492
-https://webclub.tistory.com/166
-4. 입력으로 3개의 를 받아 스트라이크 볼 낫싱 확인하는 객체
-제대로 작동함
+1. 컴퓨터가 서로 다른 수를 만드는 객체
+   --Random 값 추출은 camp.nextstep.edu.missionutils.Randoms의 pickNumberInRange()를 활용한다.
+
+2. 서로 다른 3자리의 수를 입력받는 객체
+   --사용자가 입력하는 값은 camp.nextstep.edu.missionutils.Console의 readLine()을 활용한다.
+   --tochararray 변환해서 length 3자리 이상일경우 IllegalArgumentException
+
+--https://cheershennah.tistory.com/147
+--예외처리
+
+-- https://www.freecodecamp.org/korean/news/java-string-to-int-how-to-convert-a-string-to-an-integer/
+-- 입력값 범위 확인을 위해 100~1000 string to integer 변경 Integer.parseInt(String string) 사용
+
+3. 3자리의 수가 서로 다른 수인지 확인하는 객체 1==2? 2==3? 1==3?
+   -- IllegalArgumentException
+   -- 비교형태로 list.get(i) 와 Character.getNumericValue(int value) 사용
+
+4. 입력으로 3개의 수를 받아 스트라이크 볼 낫싱 확인하는 객체
+   --string 변수 char[] 로 변경한 값을 통해
+
 5. 게임이 끝난 경우 재시작/종료를 구분하는 1과 2중 하나의 수를 입력 객체
--- 변수 오류일경우 IllegalArgumentException
+   -- 변수 오류일경우 IllegalArgumentException
 
-https://geojun.tistory.com/36 흠 중복값 set으로 제거하는 방법
+https://geojun.tistory.com/36 중복값 set으로 제거하는 방법1
 https://tosuccess.tistory.com/145 중복값 set으로 제거하는 방법2
 https://e-you.tistory.com/214 랜덤시드
-https://velog.io/@mquat/git-git%EA%B3%BC-github-wrap-up git 사용법
 
 🚀 기능 요구 사항
 기본적으로 1부터 9까지 서로 다른 수로 이루어진 3자리의 수를 맞추는 게임이다.
@@ -38,12 +62,11 @@ https://velog.io/@mquat/git-git%EA%B3%BC-github-wrap-up git 사용법
 123을 제시한 경우 : 1스트라이크
 456을 제시한 경우 : 1볼 1스트라이크
 789를 제시한 경우 : 낫싱
-위 숫자 야구 게임에서 상대방의 역할을 컴퓨터가 한다. 컴퓨터는 1에서 9까지 서로 다른 임의의 수 3개를 선택한다. 게임 플레이어는 컴퓨터가 생각하고 있는 서로 다른 3개의 숫자를 입력하고, 컴퓨터는 입력한 숫자에 대한 결과를 출력한다.
+위 숫자 야구 게임에서 상대방의 역할을 컴퓨터가 한다. 컴퓨터는 1에서 9까지 서로 다른 임의의 수 3개를 선택한다. 게임 플레이어는 컴퓨터가 생각하고 있는 서로 다른 3개의 숫자를 입력하고, 컴퓨터는 입력한 숫자에
+대한 결과를 출력한다.
 이 같은 과정을 반복해 컴퓨터가 선택한 3개의 숫자를 모두 맞히면 게임이 종료된다.
 게임을 종료한 후 게임을 다시 시작하거나 완전히 종료할 수 있다.
 사용자가 잘못된 값을 입력할 경우 IllegalArgumentException을 발생시킨 후 애플리케이션은 종료되어야 한다.
-
-
 
 JDK 17 버전에서 실행 가능해야 한다. JDK 17에서 정상적으로 동작하지 않을 경우 0점 처리한다.
 프로그램 실행의 시작점은 Application의 main()이다.
@@ -53,31 +76,13 @@ Java 코드 컨벤션 가이드를 준수하며 프로그래밍한다.
 프로그램 구현이 완료되면 ApplicationTest의 모든 테스트가 성공해야 한다. 테스트가 실패할 경우 0점 처리한다.
 프로그래밍 요구 사항에서 달리 명시하지 않는 한 파일, 패키지 이름을 수정하거나 이동하지 않는다.
 
-라이브러리
-camp.nextstep.edu.missionutils에서 제공하는 Randoms 및 Console API를 사용하여 구현해야 한다.
-Random 값 추출은 camp.nextstep.edu.missionutils.Randoms의 pickNumberInRange()를 활용한다.
-사용자가 입력하는 값은 camp.nextstep.edu.missionutils.Console의 readLine()을 활용한다.
-https://velog.io/@im_chaedong/Intellij-%EC%97%90%EC%84%9C-java.lang-%EA%B0%99%EC%9D%80-%EA%B8%B0%EB%B3%B8-%EB%9D%BC%EC%9D%B4%EB%B8%8C%EB%9F%AC%EB%A6%AC-%EB%B3%B4%EB%8A%94-%EB%B0%A9%EB%B2%95
-라이브러리 확인방법
-https://mainia.tistory.com/2323
-리스트!
-https://playground.naragara.com/513/
-리스트 비교법
-
-https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=heartflow89&logNo=220964832916
-final뜻
-
 ✏️ 과제 진행 요구 사항
 미션은 java-baseball-6 저장소를 Fork & Clone해 시작한다.
 기능을 구현하기 전 docs/README.md에 구현할 기능 목록을 정리해 추가한다.
 과제 진행 및 제출 방법은 프리코스 과제 제출 문서를 참고한다.
 
-
-
 https://nongue.tistory.com/152
 스캐너 nosuchelementexception 문제
-
-
 
 https://velog.io/@ssuh0o0/%ED%98%BC%EC%9E%90%EC%84%9C-%ED%95%B4%EA%B2%B0%ED%95%98%EA%B8%B0springExpecting-code-to-raise-a-throwable.-java.lang.AssertionError
 에러해결법
--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,145 +8,43 @@ import java.util.List;
 //코드에 중복이 되는 요소가 없는가?
 //한 메서드에 너무 많은 기능이 들어가 있지는 않는가?
 //객체 간 기능의 분리가 잘 되어있는가?
-//리팩토링
+//리팩토링 조건들
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-//Random 값 추출은 camp.nextstep.edu.missionutils.Randoms의 pickNumberInRange()를 활용한다.
-//사용자가 입력하는 값은 camp.nextstep.edu.missionutils.Console의 readLine()을 활용한다.
+        //Random 값 추출은 camp.nextstep.edu.missionutils.Randoms의 pickNumberInRange()를 활용한다.
+        //사용자가 입력하는 값은 camp.nextstep.edu.missionutils.Console의 readLine()을 활용한다.
 
-//        int pickNumberInList(final List<Integer> numbers)
-//  리스트 안 숫자 체크( 볼? )
-
-//        int pickNumberInRange(final int startInclusive, final int endInclusive)
-//        List<Integer> pickUniqueNumbersInRange
-//        List<T> shuffle(final List<T> list)
-//        void validateNumbers(final List<Integer> numbers)
-//        void validateRange(final int startInclusive, final int endInclusive)
-//        void validateCount(final int startInclusive, final int endInclusive, final int count)
-//        String readLine() {
-//                return getInstance().nextLine();
-//            }
-//        void close()
-//        Scanner getInstance()
-
-        int strike = 0;
-        int ball;
-        //1. 서로 다른 3자리의 수를 입력받는 객체
+        //!flag = true
         System.out.println("숫자 야구 게임을 시작합니다.");
-        //기능구현 3
-        boolean flag = false;
-        while (!flag) {
-            List<Integer> random_nums = make_random_nums();
-            while (strike != 3) {
-                try {
-                    strike = 0;
-                    ball = 0;
-                    System.out.print("숫자를 입력해주세요 : ");
-                    String str_baseball_num = Console.readLine();
-                    int baseball_num = Integer.parseInt(str_baseball_num);
-                    //지금 asdf 넣으면 에러생김 예외처리 필수
+        boolean flag1 = false;
+        while (!flag1) {
 
-                    if (100 <= baseball_num && baseball_num < 1000) {
-                        //기능구현 1
-                        String strBaseball_num = Integer.toString(baseball_num);
-                        char[] Baseball_numArr = strBaseball_num.toCharArray();
-                        //기능구현 2
-                        if (Baseball_numArr[0] != Baseball_numArr[1] && Baseball_numArr[1] != Baseball_numArr[2]
-                                && Baseball_numArr[0] != Baseball_numArr[2]) {
+            //기능1
+            List<Integer> computerRandomNums = getRandomNums();
+            System.out.println(computerRandomNums);
 
-                            //기능구현 4 입력으로 3개 받아서 스트라이크 볼 낫싱 확인
+            boolean flag2 = false;
+            while (!flag2) {
 
-                            //스트라이크 확인하는거
-                            //i j가 같으면 strike for문 사용, 다른경우는 ball +=1
-                            System.out.println("\n checking random nums  " + random_nums);
-                            System.out.println("checking intput nums  " + baseball_num);
+                //기능 2~3
+                char[] userNums = inputUserNums();
 
-                            //비교하는 좋은 방법 체크
-                            //그리고 객체 분리.
-                            for (int i = 0; i < 3; i++) {
-                                for (int j = 0; j < 3; j++) {
-//                                    System.out.println("int i= " + i +"  int j= " + j);
-                                    if (i == j) {
-//                                        System.out.println(random_nums.get(i));
-//                                        System.out.println(Character.valueOf(Baseball_numArr[j]));
-                                        if (random_nums.get(i) == Character.getNumericValue(Baseball_numArr[j])) {
-//                                            System.out.println("strike checking");
-//                                            System.out.println("Random num " + i + "= " + random_nums.get(i));
-//                                            System.out.println("User num " + j + "= " + Baseball_numArr[j]);
-                                            strike++;
-                                        }
-                                    } else {
-                                        if (random_nums.get(i) == Character.getNumericValue(Baseball_numArr[j])) {
-//                                            System.out.println("ball cheking");
-//                                            System.out.println("Random num " + i + "= " + random_nums.get(i));
-//                                            System.out.println("User num " + j + "= " + Baseball_numArr[j]);
-                                            ball++;
-                                        }
-                                    }
-                                }
-                            }
-                            if (ball != 0) {
-                                System.out.print(ball + "볼" + " ");
-                            }
-                            if (strike != 0) {
-                                System.out.print(strike + "스트라이크" + " ");
-                            }
-                            if (ball == 0 && strike == 0) {
-                                System.out.print("낫싱");
-                            }
-                            System.out.println();
-
-                        } else {
-                            throw new IllegalArgumentException("OMG! you input over the same of two numbers. Error.");
-                        }
-
-                    } else {
-                        throw new IllegalArgumentException("OMG! should input the nums well in range. Error.");
-                    }
-                } catch (IllegalArgumentException e) {
-                    throw new IllegalArgumentException("no. should not input out of variable");
-                }
-                //-- 0< num <1000
-                //-- 변수 숫자가 아니어도 오류
-                //-- tochararray 변환해서 length 3자리 이상일경우 IllegalArgumentException
-                // try catch문으로 IllegalArgumentException e로 처리.
+                //기능 4
+                flag2 = checkStrikeBall(computerRandomNums, userNums);
             }
-
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            boolean flag3 = false;
-            while (!flag3) {
-                //!flag3 = true
-                try {
 
-                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                    String choice = Console.readLine();
-                    if (choice.length() != 1) {
-                        System.out.println("should input only 1");
-                    } else if (Integer.parseInt(choice) == 1) {
-                        System.out.println(choice);
-                        flag3 = true;
-                        strike = 0;
-                    } else if (Integer.parseInt(choice) == 2) {
-                        flag = true;
-                        flag3 = true;
-                        System.out.println("게임 종료");
-                    } else {
-                        throw new IllegalArgumentException("Should input num 1 or 2");
-                    }
-                } catch (IllegalArgumentException e) {
-                    throw new IllegalArgumentException("no. should not input out of variable");
-                }
-            }
-
-
+            //flag1 false 다시 진행 flag2 true 진행 종료
+            flag1 = gameBranch(flag1);
         }
         Console.close();
-
     }
 
-    public static List<Integer> make_random_nums() {
+    public static List<Integer> getRandomNums() {
+        //기능구현 1
         List<Integer> computer = new ArrayList<>();
         //리스트에 1~9까지 넣는데 이미 포함되어있으면 작업 X 크기 3까지 작업
         while (computer.size() < 3) {
@@ -156,5 +54,92 @@ public class Application {
             }
         }
         return computer;
+    }
+
+    public static char[] inputUserNums() {
+        //기능구현 2
+        try {
+            System.out.print("숫자를 입력해주세요 : ");
+            String strBaseballNum = Console.readLine();
+            int intBaseballNum = Integer.parseInt(strBaseballNum);
+
+            if (100 <= intBaseballNum && intBaseballNum < 1000) {
+
+                //기능구현3
+                char[] BaseballNumCharArr = strBaseballNum.toCharArray();
+                if (BaseballNumCharArr[0] != BaseballNumCharArr[1] && BaseballNumCharArr[1] != BaseballNumCharArr[2]
+                        && BaseballNumCharArr[0] != BaseballNumCharArr[2]) {
+                    return BaseballNumCharArr;
+                } else {
+                    throw new IllegalArgumentException("OMG! you input over the same of two numbers. Error.");
+                }
+            } else {
+                throw new IllegalArgumentException("OMG! should input the nums well in range. Error.");
+            }
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("NO. should not input out of variable. Error.");
+        }
+    }
+
+    public static boolean checkStrikeBall(List<Integer> computerRandomNums,
+                                          char[] userNums) {
+        int strike = 0;
+        int ball = 0;
+        boolean flag2 = false;
+        //비교하는 좋은 방법 체크
+        //기능구현 4
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                // Character.getNumericValue(int value) 캐릭터의 int값 반환
+                if (i == j) {
+                    if (computerRandomNums.get(i) == Character.getNumericValue(userNums[j])) {
+                        strike++;
+                    }
+                } else {
+                    if (computerRandomNums.get(i) == Character.getNumericValue(userNums[j])) {
+                        ball++;
+                    }
+                }
+            }
+        }
+        if (ball != 0) {
+            System.out.print(ball + "볼" + " ");
+        }
+        if (strike != 0) {
+            System.out.print(strike + "스트라이크" + " ");
+        }
+        if (ball == 0 && strike == 0) {
+            System.out.print("낫싱");
+        }
+        if (strike == 3) {
+            flag2 = true;
+        }
+        System.out.println();
+        return flag2;
+    }
+
+    public static boolean gameBranch(boolean flag1) {
+        boolean flag = false;
+        while (!flag) {
+            try {
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                String Userchoice = Console.readLine();
+                if (Userchoice.length() != 1) {
+                    System.out.println("should input only length 1");
+                } else if (Integer.parseInt(Userchoice) == 1) {
+                    System.out.println(Userchoice);
+                    flag = true;
+                } else if (Integer.parseInt(Userchoice) == 2) {
+                    System.out.println("게임 종료");
+                    flag = true;
+                    flag1 = true;
+                } else {
+                    throw new IllegalArgumentException("Should input num 1 or 2");
+                }
+            } catch (IllegalArgumentException e) {
+                throw new IllegalArgumentException("no. should not input out of variable");
+            }
+        }
+        return flag1;
     }
 }
