bae3ead768540dce9a3522a7ee1ee3a4ad595f3c
민경 <tjalsrud0101@gmail.com>
2023-10-25 22:11:32 +0900

    feat: compare computer number and player number

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,31 +1,46 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class BaseballGame {
     private int ball;
     private int strike;
+    private List<Integer> computerNumbers;
+    private List<Integer> playerNumbers;
 
     public BaseballGame() {
-        this.ball = 0;
-        this.strike = 0;
+        ball = 0;
+        strike = 0;
+        computerNumbers = new ArrayList<>();
+        playerNumbers = new ArrayList<>();
     }
 
     public void play() {
         printStart();
-        List<Integer> computerNumbers = getComputerNumber();
-        List<Integer> playerNumbers = new ArrayList<>();
+        computerNumbers = getComputerNumber();
         while(strike != 3) {
             playerNumbers = getPlayerNumbers();
+            compareNumbers();
+        }
+    }
+
+
+
+    private void compareNumbers() {
+        for(int i = 0; i < 3; i++) {
+            if (playerNumbers.get(i) == computerNumbers.get(i)) {
+                strike++;
+            } else if (computerNumbers.contains(playerNumbers.get(i))) {
+                ball++;
+            }
         }
     }
 
     private List<Integer> getPlayerNumbers() {
         Player player = new Player();
         List<Integer> playerNumbers = player.getNumbers();
+        return playerNumbers;
     }
 
     private List<Integer> getComputerNumber() {
