4411e6cb9b49804f654c4927baa91285da71516d
Pearl <rkdwlswn227@gmail.com>
2023-10-25 16:43:18 +0900

    refactor: RandNum
    class 선언했을 때 기본적으로 List가 생성되도록 수정

--- src/main/java/baseball/RandNum.java
+++ src/main/java/baseball/RandNum.java
@@ -1,13 +1,15 @@
 package baseball;
 
-import java.util.*;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class RandNum {
 
-    public void makeRandNum(){
-        List<Integer> numArr = new ArrayList<>();
+    List<Integer> numArr = new ArrayList<>();
+
+    public void makeRandNum() {
 
         while (numArr.size() < 3) {
             int randNum = Randoms.pickNumberInRange(1, 9);
