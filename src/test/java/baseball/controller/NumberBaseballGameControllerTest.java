59c8b7cf4be10ae968751cf0c124db517183896f
Hwang In Tae <sjobs66@gmail.com>
2023-10-25 15:53:55 +0900

    NumberBaseballGameControllerTest Supplier<Game> 타입 supplier를 mock으로 변경

--- src/test/java/baseball/controller/NumberBaseballGameControllerTest.java
+++ src/test/java/baseball/controller/NumberBaseballGameControllerTest.java
@@ -3,7 +3,6 @@ package baseball.controller;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import baseball.model.Game;
-import baseball.model.NumberBaseballGame;
 import baseball.view.GameView;
 import baseball.view.NumberBaseballGameView;
 import java.io.ByteArrayInputStream;
@@ -13,19 +12,15 @@ import org.junit.jupiter.api.Test;
 class NumberBaseballGameControllerTest {
 
     private final GameView gameView = new NumberBaseballGameView("src/main/resources/messages/messages.properties");
-    private final Supplier<Game> gameSupplier = () -> {
-        try {
-            return NumberBaseballGame.class.getDeclaredConstructor().newInstance();
-        } catch (Exception e) {
-            throw new RuntimeException("게임 인스턴스 생성 오류", e);
-        }
+    private final Supplier<Game> mockSupplier = () -> {
+        return null;
     };
 
     @Test
     void InputNot1Or2() {
         final byte[] buf = "3\n".getBytes();
         System.setIn(new ByteArrayInputStream(buf));
-        NumberBaseballGameController controller = new NumberBaseballGameController(gameSupplier, gameView);
+        NumberBaseballGameController controller = new NumberBaseballGameController(mockSupplier, gameView);
         assertThrows(IllegalArgumentException.class, controller::checkResume);
     }
 }
