04887e9917f2e3a77eb842076c963c56c37b1e84
Hyeon0208 <dmsgktn0208@gmail.com>
2023-10-25 11:07:09 +0900

    test : isSamePosition() 메서드 테스트 코드 추가

--- src/test/java/baseball/domain/NumberTest.java
+++ src/test/java/baseball/domain/NumberTest.java
@@ -28,4 +28,24 @@ class NumberTest {
         assertTrue(secondIndex);
         assertTrue(thirdIndex);
     }
+
+    @Test
+    void isSamePosition() {
+        // given
+        List<Integer> list1 = new ArrayList<>(List.of(1, 2, 3));
+        List<Integer> list2 = new ArrayList<>(List.of(3, 2, 1));
+
+        Number number1 = new Number(list1);
+        Number number2 = new Number(list2);
+
+        // when
+        boolean firstIndex = number1.isSamePosition(number2, 0);
+        boolean secondIndex = number1.isSamePosition(number2, 1);
+        boolean thirdIndex = number1.isSamePosition(number2, 2);
+
+        // then
+        assertFalse(firstIndex);
+        assertTrue(secondIndex);
+        assertFalse(thirdIndex);
+    }
 }
