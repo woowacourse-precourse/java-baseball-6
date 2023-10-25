4f1cabc96cd1aebfef78f024b84c7fdbc7bfb9c1
ParkJinYong <mej1406@gmail.com>
2023-10-25 20:40:16 +0900

    fix: ball count 논리 오류 수정

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -18,8 +18,8 @@ public class Referee {
     }
 
     public int countBall(List<Integer> catcher, List<Integer> pitcher) {
-        return (int)pitcher.stream()
-                .filter(catcher::contains)
+        return (int) Stream.iterate(0, i -> i + 1).limit(Constant.MAX_NUMBER_SIZE)
+                .filter(i -> !catcher.get(i).equals(pitcher.get(i)) && catcher.contains(pitcher.get(i)))
                 .count();
     }
 }
