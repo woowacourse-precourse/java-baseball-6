31160af01b3825f587537eb97756d2382c076713
taewan625 <taewan1997625@gmail.com>
2023-10-25 23:10:15 +0900

    Feat: "Add Print method"
    Print class에프린트 관련 메서드를 생성, 역할 분리 목적

--- src/main/java/baseball/Print.java
+++ src/main/java/baseball/Print.java
@@ -12,6 +12,11 @@ public class Print {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
+    // 게임 중 문구
+    public void doGame() {
+        System.out.println("숫자를 입력해주세요 : ");
+    }
+
     // 결과 출력
     public void resultGame(int[] result) {
         if (result[0] + result[1] == 0) System.out.println("낫싱");
@@ -19,4 +24,11 @@ public class Print {
         else if (result[1] == 0 && result[0] != 0) System.out.println(result[0] + "스트라이크");
         else System.out.println(result[1] + "볼 " + result[0] + "스트라이크");
     }
+
+    // 게임 종료 문구
+    public void finishGame() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
 }
