278c704d71a5cc07eef6f86096937643f1712e4a
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-10-25 22:08:25 +0900

    feat: Constants 추가

--- src/main/java/baseball/model/Constants.java
+++ src/main/java/baseball/model/Constants.java
@@ -1,4 +1,23 @@
 package baseball.model;
 
 public class Constants {
+
+    public static final int GAME_NUMBER_LENGTH = 3;
+    public static final int MIN_RANGE_NUM = 1;
+    public static final int MAX_RANGE_NUM = 9;
+
+    public static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+
+    public static final String BALL = "볼";
+    public static final String STRIKE = "스트라이크";
+    public static final String NOTHING = "낫싱";
+
+    public static final String WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String RESTART_OR_END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public static final String END_MESSAGE = "게임 종료";
+
+
+    public static final String INTEGER_REGEX = "-?\\d+";
 }
