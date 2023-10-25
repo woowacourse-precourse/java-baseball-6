b94340c032f574e75d1619b6098f0a242b2ba021
saebyeok2 <tomato8269@naver.com>
2023-10-25 13:52:38 +0900

    feat: Computer 랜덤 숫자 생성 기능 구현

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,11 +1,25 @@
 package baseball.domain;
 
+import static baseball.constant.Baseball.NUMBER_SIZE;
+import static baseball.constant.Baseball.RANGE_END;
+import static baseball.constant.Baseball.RANGE_START;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
     private List<Integer> numbers;
 
+    // 1에서 9까지 서로 다른 임의의 수 3개를 선택해서 List에 저장
     public void init() {
+        numbers = new ArrayList<>();
 
+        while (numbers.size() < NUMBER_SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(RANGE_START, RANGE_END);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
     }
 }
