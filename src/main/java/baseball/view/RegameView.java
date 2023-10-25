0a3c7f410333a8c10a68b17abd96b6d83c5d02ba
realcold0 <realcold0@gmail.com>
2023-10-25 19:01:45 +0900

    refactor : else if 를 if로 하여 가독성 증가

--- src/main/java/baseball/view/RegameView.java
+++ src/main/java/baseball/view/RegameView.java
@@ -3,8 +3,8 @@ package baseball.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class RegameView {
-    private final String CORRECT_MENT = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    private final String REGAME_MENT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String CORRECT_MENT = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String REGAME_MENT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private boolean regame;
 
     public RegameView() {
@@ -17,7 +17,8 @@ public class RegameView {
         int regameInt = Integer.parseInt(Console.readLine());
         if (regameInt == 1) {
             regame = true;
-        } else if (regameInt == 2) {
+        }
+        if (regameInt == 2) {
             regame = false;
         }
         return regame;
--- src/main/java/baseball/view/ResultView.java
+++ src/main/java/baseball/view/ResultView.java
@@ -3,7 +3,7 @@ package baseball.view;
 import baseball.service.CompareAnswerWithPlayerNumber;
 
 public class ResultView {
-    private final String NOTHING = "낫싱";
+    private static final String NOTHING = "낫싱";
 
     public void outputCompareResult(CompareAnswerWithPlayerNumber compareAnswerWithPlayerNumber) {
         int strike = compareAnswerWithPlayerNumber.getStrike();
@@ -11,11 +11,14 @@ public class ResultView {
 
         if (ball == 0 && strike == 0) {
             System.out.println(NOTHING);
-        } else if (strike > 0 && ball == 0) {
+        }
+        if (strike > 0 && ball == 0) {
             System.out.println(strike + "스트라이크");
-        } else if (strike == 0 && ball > 0) {
+        }
+        if (strike == 0 && ball > 0) {
             System.out.println(ball + "볼");
-        } else if (strike > 0 && ball > 0) {
+        }
+        if (strike > 0 && ball > 0) {
             System.out.println(ball + "볼 " + strike + "스트라이크");
         }
     }
