09b600ee6e0031e78089d5bd13fad3422a65739d
유서린 <sprout3082@gmail.com>
2023-10-25 18:01:43 +0900

    [feat] Application 로직 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import baseball.game.BaseBallGame;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+
+	public static void main(String[] args) {
+		BaseBallGame game = BaseBallGame.newGame();
+		do {
+			game.play();
+		} while (game.askReplay());
+	}
 }
