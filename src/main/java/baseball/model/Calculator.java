21910244169784fc5a80c7a8804b1eb375324547
hansg931 <hansg0509@cau.ac.kr>
2023-10-25 21:45:55 +0900

    feat: 숫자가 null 인 경우 오류 처리

--- src/main/java/baseball/model/Calculator.java
+++ src/main/java/baseball/model/Calculator.java
@@ -1,6 +1,9 @@
 package baseball.model;
 
 import baseball.util.Constant;
+import baseball.util.Message;
+
+import java.util.stream.Stream;
 
 public class Calculator {
 	public static Score calculateScore(ListNumber userNumber, ListNumber computerNumber) {
@@ -8,22 +11,21 @@ public class Calculator {
 	}
 
 	private static int strikeCount(ListNumber userNumber, ListNumber computerNumber) {
-		int strikeCount = 0;
-		for (int i = 0; i < Constant.NUMBER_LENGTH; i++) {
-			if (userNumber.getIndex(i) == computerNumber.getIndex(i)) {
-				strikeCount++;
-			}
-		}
-		return strikeCount;
+		if (userNumber == null || computerNumber == null)
+			throw new IllegalArgumentException(Message.INPUT_ERROR_MESSAGE);
+		return (int) Stream.iterate(0, i -> i + 1)
+				.limit(Constant.NUMBER_LENGTH)
+				.filter(i -> userNumber.getIndex(i) == computerNumber.getIndex(i))
+				.count();
 	}
 
 	private static int ballCount(ListNumber userNumber, ListNumber computerNumber) {
-		int ballCount = 0;
-		for (int i = 0; i < Constant.NUMBER_LENGTH; i++) {
-			if (userNumber.getIndex(i) != computerNumber.getIndex(i) && userNumber.contains(computerNumber.getIndex(i))) {
-				ballCount++;
-			}
-		}
-		return ballCount;
+		if (userNumber == null || computerNumber == null)
+			throw new IllegalArgumentException(Message.INPUT_ERROR_MESSAGE);
+		return (int) Stream.iterate(0, i -> i + 1)
+				.limit(Constant.NUMBER_LENGTH)
+				.filter(i -> userNumber.contains(computerNumber.getIndex(i)))
+				.filter(i -> userNumber.getIndex(i) != computerNumber.getIndex(i))
+				.count();
 	}
 }
