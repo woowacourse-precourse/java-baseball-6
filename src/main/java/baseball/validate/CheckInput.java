5d8f8a00721a761b5232431e70daf33a21087a45
Youth <boysoeng@naver.com>
2023-10-25 14:47:24 +0900

    refactor: 입력 검증 에러 반환 메시지 설정

--- src/main/java/baseball/validate/CheckInput.java
+++ src/main/java/baseball/validate/CheckInput.java
@@ -5,6 +5,10 @@ import java.util.stream.Collectors;
 
 public class CheckInput {
     private static final int INPUT_LENGTH = 3;
+    private static final String LENGTH_EXCEPTION_MESSAGE = "입력한 숫자는 3자리여야 합니다.";
+    private static final String NON_NUMERIC_EXCEPTION_MESSAGE = "입력한 값은 숫자여야 합니다.";
+    private static final String RANGE_EXCEPTION_MESSAGE = "입력한 각각의 숫자는 1~9의 범위여야 합니다.";
+    private static final String DUPLICATE_EXCEPTION_MESSAGE = "중복된 값은 입력할 수 없습니다.";
 
     public static void validatePlayerInput(String userInput) {
         checkLength(userInput);
@@ -15,7 +19,7 @@ public class CheckInput {
 
     private static void checkLength(String userInput) {
         if (userInput.length() != INPUT_LENGTH) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(LENGTH_EXCEPTION_MESSAGE);
         }
     }
 
@@ -23,13 +27,13 @@ public class CheckInput {
         try {
             Integer.parseInt(userInput);
         } catch (IllegalArgumentException error) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NON_NUMERIC_EXCEPTION_MESSAGE);
         }
     }
 
     private static void checkHasZero(String userInput) {
         if (userInput.contains("0")) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(RANGE_EXCEPTION_MESSAGE);
         }
     }
 
@@ -38,7 +42,7 @@ public class CheckInput {
                 .mapToObj(ch -> (char) ch)
                 .collect(Collectors.toSet());
         if (characterSet.size() != INPUT_LENGTH) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(DUPLICATE_EXCEPTION_MESSAGE);
         }
     }
 }
