66de810c9a2ae73e490f53b886b1b69507d8484a
kokojihye <jihyevivianako@gmail.com>
2023-10-25 23:51:58 +0900

    feat: computer, user의 숫자 생성 및 입력받기

---
+++ src/main/java/game/model/Player.java
@@ -0,0 +1,14 @@
+package game.model;
+
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class Player {
+    public String getInputNumber() {
+        return Console.readLine();
+    }
+}
