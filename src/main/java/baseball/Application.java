61a96724f73ef31832477d7403609aeef6b3f947
PJH <parkjh0858@gmail.com>
2023-10-25 15:41:46 +0900

    refactor: 필요 없는 import 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,17 +1,9 @@
 package baseball;
 
 import Game.BaseballGame;
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-import model.NumberInput;
+
 import view.InputView;
 import view.OutputView;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
 
 public class Application {
     public static void main(String[] args){
@@ -25,7 +17,7 @@ public class Application {
 
     public static boolean isContinue() {
         int checkNumber = Integer.parseInt(InputView.setRetryInput());
-        if(checkNumber == 1) {
+        if (checkNumber == 1) {
             return true;
         }
         return false;
