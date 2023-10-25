d1d072e2aa91b8904eeffff331f530b2ade1420e
mksystem98 <mksystem98@gmail.com>
2023-10-25 09:47:23 +0900

    feat : 공백을 제거하는 메서드 추가
    removeSpaces() 메서드는 사용자가 공백을 포함하고 입력 시 공백을 제거해주는 메서드이다.

--- src/main/java/baseball/view/UserInputView.java
+++ src/main/java/baseball/view/UserInputView.java
@@ -6,6 +6,10 @@ public class UserInputView {
 
     public String requestNumberInput(){
         System.out.print("숫자를 입력해주세요 : ");
-        return Console.readLine();
+        return removeSpaces(Console.readLine());
+    }
+
+    private String removeSpaces(String input) {
+        return input.replace(" ","");
     }
 }
