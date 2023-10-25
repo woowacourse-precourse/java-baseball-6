ca9d79b2dd4954d56d0dfb6a5cc4afdd2a92f8f5
www-author <dream970215@naver.com>
2023-10-25 21:01:11 +0900

    :bug: fix: 게임 재시작 또는 종료 여부 메시지 메서드 수정

--- src/main/java/baseball/view/BaseballView.java
+++ src/main/java/baseball/view/BaseballView.java
@@ -17,6 +17,7 @@ public class BaseballView {
     }
 
     public void printWhetherRestartOrExitMessage() {
+        printGameOverMessage();
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
