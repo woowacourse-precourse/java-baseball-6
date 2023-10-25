11672eea2d8bb3993c1a6f57f263163475e10d41
DESKTOP-FH4O6OD\syt11 <syt1128@gmail.com>
2023-10-25 16:44:47 +0900

    기능 구현 성공 클래스랑 메소드 나누기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,65 +12,68 @@ import java.util.regex.Pattern;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        int newGame = 1;
-
-        do {
-
-            ComputerNum computerNum = new ComputerNum();
-            List<Integer> listComFixed = new ArrayList<>(computerNum.computer());
-            System.out.println(listComFixed);
-
-            boolean win = false;
-
-                do {
-
-                    System.out.print("숫자를 입력해주세요 : ");
-                    UserInput userInput = new UserInput();
-
-                    List<Integer> listUserFixed = new ArrayList<>(userInput.changeInList());
-                    int listUserFixedNum = listUserFixed.size();
-
-                    if (listUserFixedNum != 3) {
-                        throw new IllegalArgumentException();
-                    }
-                    CheckTheList checkTheList = new CheckTheList();
-                    int correctBalls = checkTheList.correctBallCount(listComFixed, listUserFixed);
-                    int strikes = checkTheList.strikeCount(listComFixed, listUserFixed);
-
-
-                    if (correctBalls > 0) {
-                        if (strikes == 0)
-                            System.out.println(correctBalls + "볼");
-                        if (strikes == 3) {
-                            System.out.println(strikes + "스트라이크");
-                            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                            break;
-                        }
-                        if (strikes > 0) {
-                            if (correctBalls - strikes == 0) {
-                                System.out.println(strikes + "스트라이크");
-                                continue;
-                            }
-                            System.out.print((correctBalls - strikes) + "볼");
-                            System.out.print(" ");
-                            System.out.println(strikes + "스트라이크");
-                        }
-
-                    }
-                    if (correctBalls == 0) {
-                        System.out.println("낫싱");
-                    }
-                }
-
-
-
-            while (win == false) ;
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            String gameOrNot = Console.readLine();
-            newGame = Integer.parseInt(gameOrNot);
-
-        } while(newGame == 1);
+        PlayingProgress playingProgress = new PlayingProgress();
+        playingProgress.gameLoop();
+//        PlayingProcess process = new PlayingProcess();
+//        process.streaming();
+//        System.out.println("숫자 야구 게임을 시작합니다.");
+//        int newGame =1;
+//        do {
+//
+//            ComputerNum computerNum = new ComputerNum();
+//            List<Integer> listComFixed = new ArrayList<>(computerNum.computer());
+//            System.out.println(listComFixed);
+//
+//            boolean win = false;
+//
+//                do {
+//
+//                    System.out.print("숫자를 입력해주세요 : ");
+//                    UserInput userInput = new UserInput();
+//
+//                    List<Integer> listUserFixed = new ArrayList<>(userInput.changeInList());
+//                    int listUserFixedNum = listUserFixed.size();
+//
+//                    if (listUserFixedNum != 3) {
+//                        throw new IllegalArgumentException();
+//                    }
+//                    CheckTheList checkTheList = new CheckTheList();
+//                    int correctBalls = checkTheList.correctBallCount(listComFixed, listUserFixed);
+//                    int strikes = checkTheList.strikeCount(listComFixed, listUserFixed);
+//
+//
+//                    if (correctBalls > 0) {
+//                        if (strikes == 0)
+//                            System.out.println(correctBalls + "볼");
+//                        if (strikes == 3) {
+//                            System.out.println(strikes + "스트라이크");
+//                            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+//                            break;
+//                        }
+//                        if (strikes > 0) {
+//                            if (correctBalls - strikes == 0) {
+//                                System.out.println(strikes + "스트라이크");
+//                                continue;
+//                            }
+//                            System.out.print((correctBalls - strikes) + "볼");
+//                            System.out.print(" ");
+//                            System.out.println(strikes + "스트라이크");
+//                        }
+//
+//                    }
+//                    if (correctBalls == 0) {
+//                        System.out.println("낫싱");
+//                    }
+//                }
+//
+//
+//
+//            while (win == false) ;
+//            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+//            String gameOrNot = Console.readLine();
+//            newGame = Integer.parseInt(gameOrNot);
+//
+//        } while(newGame == 1);
     }
 }
 
@@ -89,7 +92,6 @@ class ComputerNum {
             return computer;
     }
 }
-
 class UserInput {
 
     String userString = Console.readLine();
@@ -105,12 +107,6 @@ class UserInput {
             if (!Pattern.matches("[1-9]{3}",userString))
                 throw new IllegalArgumentException();
         }
-        //        for (int i = 0; i < userString.length(); i++) {
-//            char currentChar = userString.charAt(i);
-//            if (set.contains(currentChar)) {
-//                throw new IllegalArgumentException();
-//            }
-//        }
         return digits;
     }
     public List<Integer> changeInList() {
@@ -121,24 +117,8 @@ class UserInput {
         }
         return digitsInt;
     }
-//    public void userNumCheck(){
-//
-//        int listUserFixedsize = putInList().size();
-//        {
-//            try {if(listUserFixedsize != 3)
-//                throw new IllegalArgumentException();
-//            } catch (IllegalArgumentException e) {
-//                System.out.println("[ERROR] 잘못된 숫자 형식입니다");
-//                e.printStackTrace();
-//            }
-//        }
-//    }
 }
-
-
 class CheckTheList {
-
-
     public int strikeCount(List<Integer> list1, List<Integer> list2) {
         int strikeCounts = 0;
         for (int i = 0; i < 3; i++) {
@@ -159,19 +139,242 @@ class CheckTheList {
         return ballCount;
     }
 
-    public int nothing(List<Integer> list1, List<Integer> list2) {
-        int nothingCount = 0;
-        for (int i = 0; i < 3; i++) {
-            if (list1.get(i) != list2.get(i)) {
-                {
-                    nothingCount++;
-                }
-            }
+}
+class OutputForBalls extends CheckTheList {
 
+    public void checkingStream(List<Integer> list1) {
+        boolean win = false;
+        do {
+            System.out.print("숫자를 입력해주세요 : ");
+            UserInput users = new UserInput();
+
+            List<Integer>listUserFixed = new ArrayList<>(users.changeInList());
+            int listUserFixedNum = listUserFixed.size();
+
+            if (listUserFixedNum != 3) {
+                throw new IllegalArgumentException();
+            }
+            int correctBalls = correctBallCount(list1,listUserFixed);
+            int strikes = strikeCount(list1,listUserFixed);
+            if (correctBalls > 0) {
+                checkingNoStrikes(strikes,correctBalls);
+                if (checkingWin(strikes)) {
+                    break;
+                }
+                if (strikes > 0) {
+                    if (allCorrectBallisStrike(strikes,correctBalls)) {
+                        continue;
+                    }
+                    mixedCorrectBalls(strikes,correctBalls);
+                }
+
+            }
+            nothingCorrect(correctBalls);
+        } while (!win);
+    }
+
+    public void checkingNoStrikes (int strkies ,int correctBalls){
+        if(strkies == 0){
+            System.out.println(correctBalls + "볼");
         }
-        return nothingCount;
+    }
+
+    public boolean checkingWin(int strikes) {
+        boolean allStrikes = false;
+        if(strikes == 3) {
+            System.out.println(strikes + "스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            allStrikes = true;
+        }
+        return allStrikes;
+    }
+
+    public boolean allCorrectBallisStrike(int strikes, int correctBalls){
+        boolean allBallisStrkie = false;
+        if(correctBalls - strikes == 0){
+            System.out.println(strikes + "스트라이크");
+            allBallisStrkie = true;
+        }
+        return allBallisStrkie;
+    }
+
+    public void mixedCorrectBalls(int strikes, int correctBalls){
+        System.out.print((correctBalls - strikes) + "볼");
+        System.out.print(" ");
+        System.out.println(strikes + "스트라이크");
+    }
+
+    public void nothingCorrect(int correctBalls){
+        if(correctBalls == 0){
+            System.out.println("낫싱");
+        }
     }
 }
+class PlayingProgress {
+    public void streamGame() {
+        ComputerNum computerNum = new ComputerNum();
+        List<Integer> listComFixed = new ArrayList<>(computerNum.computer());
+        System.out.println(listComFixed);
+
+        OutputForBalls outputForBalls = new OutputForBalls();
+        outputForBalls.checkingStream(listComFixed);
+    }
+
+    public void gameLoop() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        int newGame = 1;
+        do {
+            streamGame();
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            newGame = continueOrNot();
+        } while (newGame == 1);
+    }
+
+    public int continueOrNot() {
+        String gameOrNot = Console.readLine();
+        int newOrEnd = Integer.parseInt(gameOrNot);
+        return newOrEnd;
+    }
+
+
+}
+
+
+//class PlayingProcess{
+//
+//    ComputerNum coms = new ComputerNum();
+//    UserInput users = new UserInput();
+//    CheckTheList checks = new CheckTheList();
+//
+//
+//    public void streaming() {
+//        int newGame = 1;
+//        do {
+//
+//            firstSettings();
+//            userTyping();
+//            checkingStream();
+//            newGame = askContinue();
+//
+//        }while (newGame ==1);
+//
+//    }
+//
+//    public void firstSettings() {
+//        ComputerNum coms = new ComputerNum();
+//        System.out.println("숫자야구를 시작합니다.");
+//        List<Integer> listComFixed = new ArrayList<>(coms.computer());
+//        System.out.println(listComFixed);
+//
+//    }
+//
+//    public void userTyping() {
+//
+//        System.out.print("숫자를 입력해주세요 : ");
+//        UserInput users = new UserInput();
+//        List<Integer>listUserFixed = new ArrayList<>(users.changeInList());
+//        int listUserFixedNum = listUserFixed.size();
+//
+//        if (listUserFixedNum != 3) {
+//            throw new IllegalArgumentException();
+//        }
+//    }
+//
+////    public void checking() {
+////        this.checks = new CheckTheList();
+////        this.correctBalls = checks.correctBallCount(this.listComFixed, this.listUserFixed);
+////        this.strikes = checks.strikeCount(this.listComFixed,this.listUserFixed);
+////        checkingContainsList(correctBalls,strikes);
+////        if (correctBalls > 0) {
+////            if (strikes == 0)
+////                System.out.println(correctBalls + "볼");
+////            if (strikes == 3) {
+////                System.out.println(strikes + "스트라이크");
+////                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+////                break;
+////            }
+////            if (strikes > 0) {
+////                if (correctBalls - strikes == 0) {
+////                    System.out.println(strikes + "스트라이크");
+////                    continue;
+////                }
+////                System.out.print((correctBalls - strikes) + "볼");
+////                System.out.print(" ");
+////                System.out.println(strikes + "스트라이크");
+////            }
+////
+////        }
+////        if (correctBalls == 0) {
+////            System.out.println("낫싱");
+////        }
+////    }
+//    public void checkingStream(){
+//
+//        do {
+//            userTyping();
+//            CheckTheList checks = new CheckTheList();
+//            int correctBalls = checks.correctBallCount(listComFixed, listUserFixed);
+//            int strikes = checks.strikeCount(listComFixed, listUserFixed);
+//            if (correctBalls > 0) {
+//                checkingNoStrikes();
+//                if (checkingWin()) {
+//                    break;
+//                }
+//                if (strikes > 0) {
+//                   if (allCorrectBallisStrike()) {
+//                        continue;
+//                   }
+//                   mixedCorrectBalls();
+//                }
+//                nothingCorrect();
+//            }
+//        }while (!win);
+//    }
+//
+//
+//    public void checkingNoStrikes(int i){
+//        if (strikes == 0)
+//            System.out.println(correctBalls + "볼");
+//    }
+//
+//    public boolean checkingWin(){
+//        if (strikes == 3) {
+//            System.out.println(strikes + "스트라이크");
+//            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+//            this.win = true;
+//        }
+//        return this.win;
+//    }
+//
+//    public boolean allCorrectBallisStrike(){
+//        if (correctBalls - strikes == 0) {
+//            System.out.println(strikes + "스트라이크");
+//        }
+//        return true;
+//    }
+//
+//    public void mixedCorrectBalls(){
+//        System.out.print(correctBalls - strikes) + "볼");
+//        System.out.print(" ");
+//        System.out.println(strikes + "스트라이크");
+//    }
+//
+//    public void nothingCorrect(){
+//        if (correctBalls == 0) {
+//            System.out.println("낫싱");
+//        }
+//    }
+//
+//
+//    public int askContinue(){
+//        int newGame;
+//        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+//        String gameOrNot = Console.readLine();
+//        newGame = Integer.parseInt(gameOrNot);
+//        return newGame;
+//    }
+//
+//}
 
 //class PlayingProcess {
 //    ComputerNum computerNum = new ComputerNum();
