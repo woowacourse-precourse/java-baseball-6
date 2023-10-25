779891fe21c2caf6dca8e5d27a35d2b766105f3b
706com <ehdgusjdbswl@gmail.com>
2023-10-25 10:33:34 +0900

    style : 개행 제거

--- src/main/java/baseball/service/BaseballService.java
+++ src/main/java/baseball/service/BaseballService.java
@@ -2,21 +2,19 @@ package baseball.service;
 
 public class BaseballService {
 
-    public void verifyException(String number){
-
-        if(number.length() != 3){
+    public void verifyException(String number) {
+        if (number.length() != 3) {
             throw new IllegalArgumentException();
         }
-
-        for(char x : number.toCharArray()){
-            if(x -'0' < 1 || x -'0' > 9){
+        for (char x : number.toCharArray()) {
+            if (x - '0' < 1 || x - '0' > 9) {
                 throw new IllegalArgumentException();
             }
         }
     }
 
-    public boolean restart(String restart){
-        if(restart.equals("1")){
+    public boolean restart(String restart) {
+        if (restart.equals("1")) {
             return true;
         }
         return false;
