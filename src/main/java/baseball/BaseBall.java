4e5c49077442f82bc2d7ab1f256d1adcdf334298
DaehoSon <thseogh135@gmail.com>
2023-10-25 23:31:45 +0900

    refact: 사용하지 않는 getter 제거

--- src/main/java/baseball/BaseBall.java
+++ src/main/java/baseball/BaseBall.java
@@ -2,8 +2,6 @@ package baseball;
 
 
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
@@ -13,25 +11,13 @@ public class BaseBall {
     private final int ANSWER_LENGTH = 3;
     private List<Integer> answer;
     private boolean ended = false;
-    private int ball;
-    private int strike;
+    private int ballCount;
+    private int strikeCount;
 
 
     BaseBall() {
         System.out.println("숫자 야구 게임을 시작합니다.");
         this.reset();
-    }
-
-    public boolean isEnded() {
-        return this.ended;
-    }
-
-    public int getStrike() {
-        return this.strike;
-    }
-
-    public int getBall() {
-        return this.ball;
     }
 
     public void reset() {
@@ -63,34 +49,32 @@ public class BaseBall {
         this.validateInputNumber(input);
         this.compareAnswerToInput(this.answer, input);
         this.printScore();
-        if (this.strike == this.ANSWER_LENGTH) {
+        if (this.strikeCount == this.ANSWER_LENGTH) {
             System.out.println(ANSWER_LENGTH + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
             this.ended = true;
         }
     }
 
     public void printScore() {
-        int ballCount = this.getBall();
-        int strikeCount = this.getStrike();
-        if (ballCount != 0 && strikeCount != 0) {
-            System.out.println(ballCount + "볼" + " " + strikeCount + "스트라이크");
-        } else if (ballCount != 0) {
-            System.out.println(ballCount + "볼");
-        } else if (strikeCount != 0) {
-            System.out.println(strikeCount + "스트라이크");
+        if (this.ballCount != 0 && this.strikeCount != 0) {
+            System.out.println(this.ballCount + "볼" + " " + this.strikeCount + "스트라이크");
+        } else if (this.ballCount != 0) {
+            System.out.println(this.ballCount + "볼");
+        } else if (this.strikeCount != 0) {
+            System.out.println(this.strikeCount + "스트라이크");
         } else {
             System.out.println("낫싱");
         }
     }
 
     private void compareAnswerToInput(List<Integer> answer, List<Integer> input) {
-        this.ball = 0;
-        this.strike = 0;
+        this.ballCount = 0;
+        this.strikeCount = 0;
         for (int i = 0; i < answer.size(); i++) {
             if (Objects.equals(answer.get(i), input.get(i))) {
-                this.strike += 1;
+                this.strikeCount += 1;
             } else if (answer.contains(input.get(i))) {
-                this.ball += 1;
+                this.ballCount += 1;
             }
         }
     }
