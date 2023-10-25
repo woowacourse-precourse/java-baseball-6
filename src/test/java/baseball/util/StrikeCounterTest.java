f5dba83ef1a7bebee979b46da8324c03cd45c2e3
siyeonSon <siyeon0209@naver.com>
2023-10-25 13:48:03 +0900

    refactor: 사용하지 않는 import 제거

--- src/test/java/baseball/util/StrikeCounterTest.java
+++ src/test/java/baseball/util/StrikeCounterTest.java
@@ -1,7 +1,6 @@
 package baseball.util;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
