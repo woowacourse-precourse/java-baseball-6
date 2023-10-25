406a739a40c22495021aac343058f764f2a3871e
inyoung <charmingyear@gmail.com>
2023-10-25 23:15:46 +0900

    Feat: 정답 값 생성 기능 구현

---
+++ src/main/java/baseball/util/ResultMessage.java
@@ -0,0 +1,7 @@
+package baseball.util;
+
+public class ResultMessage {
+    public static final String CORRECT_GAME = "3개의 숫자를 모두 맞히셨습니다!";
+    public static final String END_GAME = "게임 종료";
+    public static final String FINISH_GAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+}
