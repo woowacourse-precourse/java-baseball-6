3bbbf8c8e6e515514998ff001cf866a94adaa9b5
OhTaeHo-97 <57743934+OhTaeHo-97@users.noreply.github.com>
2023-10-25 15:28:33 +0900

    feat: 사용자 숫자 입력 기능

---
+++ src/main/java/utils/InputUtil.java
@@ -0,0 +1,29 @@
+package utils;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputUtil {
+    // 검증 객체
+    private static final Validation validation = new Validation();
+
+    public static List<Integer> getPlayerInput() {
+        String playerInput = Console.readLine();
+
+        // 사용자 인풋 검증
+        validation.validatePlayerInput(playerInput);
+
+        // 사용자 인풋 List로 변경하여 반환
+        return strToList(playerInput);
+    }
+
+    private static List<Integer> strToList(String playerInput) {
+        List<Integer> listPlayerInput = new ArrayList<>();
+        for (int idx = 0; idx < playerInput.length(); idx++) {
+            listPlayerInput.add(Integer.parseInt(String.valueOf(playerInput.charAt(idx))));
+        }
+
+        return listPlayerInput;
+    }
+}
