686ca4539fd854643f63d48771dd10e8d76ffe43
김효준 <khyojun1104@gmail.com>
2023-10-25 22:21:17 +0900

    [refactor] ValidatorTest : 자바 코드 컨벤션 적용!

--- src/test/java/baseball/util/ValidatorTest.java
+++ src/test/java/baseball/util/ValidatorTest.java
@@ -14,16 +14,20 @@ class ValidatorTest {
 
     @Test
     @DisplayName("초기 유저숫자 입력시 나는 오류 체크!")
-    void validateNumber(){
-        String user_number_wrong="12a";
+    void validateNumber() {
+        String user_number_wrong = "12a";
         String user_number_has_zero = "120";
-        String user_number_wrong_length="1234";
-        String user_no_number="\n";
+        String user_number_wrong_length = "1234";
+        String user_no_number = "\n";
 
-        Assertions.assertThatThrownBy(() -> validator.validateNumber(user_number_wrong)).isInstanceOf(IllegalArgumentException.class);
-        Assertions.assertThatThrownBy(() -> validator.validateNumber(user_number_has_zero)).isInstanceOf(IllegalArgumentException.class);
-        Assertions.assertThatThrownBy(() -> validator.validateNumber(user_number_wrong_length)).isInstanceOf(IllegalArgumentException.class);
-        Assertions.assertThatThrownBy(() -> validator.validateNumber(user_no_number)).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> validator.validateNumber(user_number_wrong))
+            .isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> validator.validateNumber(user_number_has_zero))
+            .isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> validator.validateNumber(user_number_wrong_length))
+            .isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> validator.validateNumber(user_no_number))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 
 
@@ -31,15 +35,19 @@ class ValidatorTest {
     @DisplayName("재시작 번호 입력이 잘못되 경우 테스트!")
     void validateRestart() {
 
-        String restart_number_wrong_length="12";
+        String restart_number_wrong_length = "12";
         String restart_number_wrong_range1 = "0";
         String restart_number_wrong_range2 = "3";
-        String restart_number_wrong="12a";
+        String restart_number_wrong = "12a";
 
-        Assertions.assertThatThrownBy(() -> validator.validateRestart(restart_number_wrong)).isInstanceOf(IllegalArgumentException.class);
-        Assertions.assertThatThrownBy(() -> validator.validateRestart(restart_number_wrong_range1)).isInstanceOf(IllegalArgumentException.class);
-        Assertions.assertThatThrownBy(() -> validator.validateRestart(restart_number_wrong_range2)).isInstanceOf(IllegalArgumentException.class);
-        Assertions.assertThatThrownBy(() -> validator.validateRestart(restart_number_wrong_length)).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> validator.validateRestart(restart_number_wrong))
+            .isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> validator.validateRestart(restart_number_wrong_range1))
+            .isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> validator.validateRestart(restart_number_wrong_range2))
+            .isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> validator.validateRestart(restart_number_wrong_length))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 
 
