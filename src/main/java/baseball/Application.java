5b8501490e4cae8396cfcafb3963aa4f42a97060
김라연 <rayeon@gimlayeon-ui-noteubug.local>
2023-10-25 09:47:56 +0900

    feat:게임시작알림문구 출력 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,15 +1,18 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Randoms;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
 
+        startPrint();
+
     }
+
+    private static void startPrint() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
 
 
 }
