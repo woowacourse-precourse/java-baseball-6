2d0ce1f87ed569fe6139d980a6681ed146e2e989
heejae <moon49051130@gmail.com>
2023-10-25 17:24:20 +0900

    FIX Test 코드에 따른 수정사항 Gradle 9.0에서는 assertThatThrownBy()와 관련된 메서드가 deprecated되었다.

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -4,9 +4,8 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -20,13 +19,12 @@ class ApplicationTest extends NsTest {
         );
     }
 
+
     @Test
     void 예외_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("222"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
+        assertThrows(IllegalArgumentException.class, () -> runException("222"));
     }
+
 
     @Override
     public void runMain() {
