39c2fc1bab7321290b1e441f00d45a965d2e4deb
seongwonny <wjdtjd29@gmail.com>
2023-10-25 23:14:54 +0900

    [숫자 야구] 정성원 미션 제출합니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -62,7 +62,6 @@ public class Application {
 
             NEWGAME:
             while (true) {
-                System.out.println(computer);
                 System.out.print("숫자를 입력해주세요 : ");
                 List<Integer> user = getUserNumber();
 
