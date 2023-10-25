e48e45a395956ec3ac76b53d45f638004d5dd5f7
yeahdy <yejine98@naver.com>
2023-10-25 20:52:58 +0900

    feat: main 실행, 테스트 완료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import controller.BaseBallGameController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseBallGameController baseBallGameController = new BaseBallGameController();
+        baseBallGameController.playBaseballGame();
     }
+
 }
