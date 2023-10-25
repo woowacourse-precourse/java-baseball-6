c4e84c9733cd8e6b2b282cc398816297ae46104f
hansg931 <hansg0509@cau.ac.kr>
2023-10-25 22:57:08 +0900

    refactor: 컨벤션에 따라 수정

--- src/main/java/baseball/util/InputValidator.java
+++ src/main/java/baseball/util/InputValidator.java
@@ -1,36 +1,33 @@
 package baseball.util;
 
-import baseball.util.Constant;
-import baseball.util.Message;
-
 public class InputValidator {
-	private void isValidInteger(String Input) {
+	private void isValidInteger(String userInput) {
 		try {
-			Integer.parseInt(Input);
+			Integer.parseInt(userInput);
 		} catch (NumberFormatException e) {
 			throw new IllegalArgumentException(Message.INPUT_ERROR_MESSAGE);
 		}
 	}
 
-	private void isValidLength(String Input) {
-		if (Input.length() != Constant.NUMBER_LENGTH) {
+	private void isValidLength(String userInput) {
+		if (userInput.length() != Constant.NUMBER_LENGTH) {
 			throw new IllegalArgumentException(Message.INPUT_ERROR_MESSAGE);
 		}
 	}
 
-	private void isDistinct(String Input) {
+	private void isDistinct(String userInput) {
 		for (int i = 0; i < Constant.NUMBER_LENGTH; i++) {
 			for (int j = i + 1; j < Constant.NUMBER_LENGTH; j++) {
-				if (Input.charAt(i) == Input.charAt(j)) {
+				if (userInput.charAt(i) == userInput.charAt(j)) {
 					throw new IllegalArgumentException(Message.INPUT_ERROR_MESSAGE);
 				}
 			}
 		}
 	}
 
-	public void isValidInput(String Input) {
-		isValidInteger(Input);
-		isValidLength(Input);
-		isDistinct(Input);
+	public void isValidInput(String userInput) {
+		isValidInteger(userInput);
+		isValidLength(userInput);
+		isDistinct(userInput);
 	}
 }
