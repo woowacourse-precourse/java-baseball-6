ba80689c4a41a745dbad300be2c1deda6b32c304
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-10-25 12:40:10 +0900

    feat: implement user input method and user input format check method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,19 +1,54 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Application {
+    public static boolean computerPower, gamePower;
     public static void main(String[] args) {
 
-        boolean computerPower = true;
-        while(computerPower == true){
+        // [대기 화면]
+        computerPower = true;
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while(computerPower){
             List<Integer> randomAnswer = generateRandomAnswer();
 
+            // [게임 시작]
+            boolean gamePower = true;
+            while(gamePower){
+                // 사용자 입력값 대기...
+                Set<Integer> userGuessSet = readUserGuess();
+//                formatCheck(userGuessSet) ? compareAnswer(randomAnswer, userGuessSet) : turnOff();
+
+                if(userGuessSet.size() != 3){
+                    computerPower = false;
+                    gamePower = false;
+                    throw new IllegalArgumentException("잘못된 형식의 입력입니다. 프로그램을 종료합니다.");
+                }
+
+            }
         }
 
+    }
+
+    public static boolean formatCheck(Set<Integer> set){
+        return set.size()==3 ? true : false;
+    }
+
+    public static Set<Integer> readUserGuess() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String userGuessStr = Console.readLine();
+        // 형식 확인
+        // 1. 입력이 3자리 수 인가
+        // 2. 3자리중 중복이 없는가
+        // set<int>에다 저장하고 size가 3인가 확인.
+        Set<Integer> userGuessSet = new LinkedHashSet<>();
+        userGuessSet.add(userGuessStr.charAt(0)-'0');
+        userGuessSet.add(userGuessStr.charAt(1)-'0');
+        userGuessSet.add(userGuessStr.charAt(2)-'0');
+        return userGuessSet;
     }
 
     public static List<Integer> generateRandomAnswer() {
--- src/test/java/baseball/MyTest.java
+++ src/test/java/baseball/MyTest.java
@@ -3,9 +3,13 @@ package baseball;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
-import static baseball.Application.generateRandomAnswer;
+import camp.nextstep.edu.missionutils.Console;
+import static baseball.Application.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MyTest{
 
@@ -18,5 +22,31 @@ public class MyTest{
         }
         System.out.println();
         Assertions.assertThat(answers.size()).isEqualTo(3);
+    }
+
+    @Test
+    void formatTest(){
+        Set<Integer> success = new LinkedHashSet<>();
+        success.add(1);
+        success.add(2);
+        success.add(3);
+
+        Set<Integer> fail1 = new LinkedHashSet<>();
+        fail1.add(3);
+        fail1.add(3);
+        fail1.add(2);
+
+        Set<Integer> fail2 = new LinkedHashSet<>();
+        fail2.add(1);
+        fail2.add(3);
+        fail2.add(2);
+        fail2.add(4);
+        boolean result1 = formatCheck(success);
+        boolean result2 = formatCheck(fail1);
+        boolean result3 = formatCheck(fail2);
+
+        Assertions.assertThat(result1).isEqualTo(true);
+        Assertions.assertThat(result2).isEqualTo(false);
+        Assertions.assertThat(result3).isEqualTo(false);
     }
 }
