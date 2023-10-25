aacbc013db9dbc3aa2d0b72914dd36dd1c35c47e
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-10-25 15:19:00 +0900

    refactor : Application 접근제어자 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,12 @@
 package baseball;
 
+import static baseball.Score.THREE_STRIKE;
+
 import java.util.List;
 
 public class Application {
 
-    public static final int EXIT_NUMBER = 2;
+    private static final int EXIT_NUMBER = 2;
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -17,14 +19,16 @@ public class Application {
 
         do {
             List<Integer> answerNumbers = baseBallGame.createAnswerNumbers();
+
             BaseBallGameController controller = new BaseBallGameController(inputDevice, baseBallGame, outputDevice);
             gameResult = controller.competeWith(answerNumbers);
+
             outputDevice.printGameFinish();
         } while (playingGame(gameResult, inputDevice));
     }
 
     private static boolean playingGame(String gameResult, InputDevice inputDevice) {
-        if (gameResult.equals(Score.THREE_STRIKE.getName())) {
+        if (gameResult.equals(THREE_STRIKE.getName())) {
             int restartOrExit = inputDevice.restartOrExit();
             return restartOrExit != EXIT_NUMBER;
         }
