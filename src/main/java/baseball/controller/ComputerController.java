87cead5b19f9afd72d9e36fc257a3f92b2ddf281
ddohyeong <dohyeong7026@gmail.com>
2023-10-25 21:00:48 +0900

    feat(ComputerController) : 플레이어의 데이터 입력 받는 기능 구현
    docs : 플레이어에게 데이터 입력 기능 구현 완료 체크

--- src/main/java/baseball/controller/ComputerController.java
+++ src/main/java/baseball/controller/ComputerController.java
@@ -2,10 +2,15 @@ package baseball.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import baseball.model.Game;
+import baseball.view.InputView;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class ComputerController {
+	InputView inputView = new InputView();
+
 	public List<Integer> getComputerNumber() {
 		List<Integer> computer = new ArrayList<>();
 
@@ -16,6 +21,22 @@ public class ComputerController {
 			}
 		}
 		return computer;
+	}
+
+	public Game getPlayerNumbers() {
+		String playerInputNumber = inputView.enterGameNumber();
+
+		Game playerGameData = new Game();
+		List<Integer> playerNumbers = convertNumberStringToList(playerInputNumber);
+		playerGameData.setGameNumbers(playerNumbers);
+
+		return playerGameData;
+	}
+
+	private static List<Integer> convertNumberStringToList(String InputNumber) {
+		return InputNumber.chars()
+			.mapToObj(Character::getNumericValue)
+			.collect(Collectors.toList());
 	}
 
 }
