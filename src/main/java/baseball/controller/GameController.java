370ad8850f6c86c3fd28318682012d20a940e339
yshong1998 <yshong1998@gmail.com>
2023-10-25 18:54:21 +0900

    Feat (GameController): 게임을 시작,종료하는  GameController 클래스 생성
    게임 1회성 시작 메서드 구현,

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,4 +1,32 @@
 package baseball.controller;
 
+import baseball.domain.NumberBaseballGame;
+import baseball.domain.dto.Result;
+import baseball.global.constant.RuleValue;
+import baseball.global.message.GameMessage;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class GameController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final NumberBaseballGame numberBaseballGame;
+
+    public GameController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.numberBaseballGame = new NumberBaseballGame();
+    }
+
+    public void run(){
+        System.out.println(GameMessage.GAME_START_MESSAGE.getMessage());
+        Result result;
+        do {
+            numberBaseballGame.initGameSetting();
+            int playerInput = inputView.requestBaseballNumberInput();
+            result = numberBaseballGame.makeResult(playerInput);
+            outputView.responseResult(result);
+        } while (result.getStrike() == RuleValue.BASEBALL_NUMBER_SIZE);
+    }
 }
