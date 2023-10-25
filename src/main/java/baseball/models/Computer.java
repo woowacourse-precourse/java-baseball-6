5bd1540a53adfba7e9dfa568a93b29c39e6c2f9e
zeomzzz <jmlee2705@gmail.com>
2023-10-25 22:09:57 +0900

    refractor: Computer에서 answerNumber와 Utils 생성자 작성 순서 수정

--- src/main/java/baseball/models/Computer.java
+++ src/main/java/baseball/models/Computer.java
@@ -6,9 +6,9 @@ import baseball.utils.Utils;
 
 public class Computer {
 
-    Utils utils = new Utils();
-
     private String answerNumber;
+
+    Utils utils = new Utils();
 
     // 1부터 9까지 서로 다른 수로 이루어진 수 선택
     public void chooseNumber(int length) {
