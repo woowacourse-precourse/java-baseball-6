6a0a7c919e83f826972c0d6f9105e7c23d25f9e5
pumpk <cong2738@gmail.com>
2023-10-25 14:40:39 +0900

    .

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,27 +8,8 @@ import camp.nextstep.edu.missionutils.Console;
 final class computer {
     public ArrayList<Integer> data;
     public HashMap<Integer,Boolean> hash;
-    public computer(ArrayList<Integer> data, HashMap<Integer,Boolean> hash){
-        this.data = data;
-        this.hash = hash;
-    }
-}
 
-public class Application {
-    
-    private static final String inputError = "형식에 맞지 않는 입력입니다. 프로그렘을 종료합니다.";
-
-    public static String jud(int s, int b){ 
-        String res = "";
-
-        if(b+s==0) return "낫싱";
-        if(b>0) res+=(Integer.toString(b)+"볼 ");
-        if(s>0) res+=(Integer.toString(s)+"스트라이크");
-
-        return res;
-    }
-
-    public static computer setcom(){
+    public computer(){
         ArrayList<Integer> comdata = new ArrayList<>();
         HashMap<Integer,Boolean> comhash = new HashMap<>();
 
@@ -40,29 +21,46 @@ public class Application {
             }
         }
 
-        return new computer(comdata, comhash);
+        this.data = comdata;
+        this.hash = comhash;
+    }
+    
+}
+
+public class Application {
+    
+    private static final String inputError = "형식에 맞지 않는 입력입니다. 프로그렘을 종료합니다.";
+
+    public static String jud(int s, int b){ 
+        String res = "";
+        
+        if(b+s==0) return "낫싱";
+        if(b>0) res+=(String.format("%d볼",b));
+        if(b > 0 && s > 0) res+=' ';
+        if(s>0) res+=(String.format("%d스트라이크",s));
+
+        return res;
     }
 
     public static ArrayList<Integer> chooseNumber(){
-        boolean dupError = false;
         ArrayList<Integer> user = new ArrayList<>();
         char[] s = Console.readLine().toCharArray();
 
+        if (s.length != 3) 
+            throw new IllegalArgumentException(inputError);
+
         for(int i = 0; i < s.length; i++){
-            if (user.contains(s[i]-48)) {
-                dupError = true;
-                break;
-            }
+            if (user.contains(s[i]-48)) 
+                throw new IllegalArgumentException(inputError);
             user.add(s[i]-48);
         }
-        
-        if (user.size()!=3 || dupError) 
-            throw new IllegalArgumentException(inputError);
 
         return user;
     }
     
-    public static int game(computer com){
+    public static boolean game(){
+        computer com = new computer();
+
         while(true){            
             System.out.print("숫자를 입력해 주세요 : ");
 
@@ -76,23 +74,21 @@ public class Application {
 
             System.out.println(jud(s,b));
 
-            if(s==3) return gameover();
+            if(s==3) break;
         }
-    }
 
-    public static int gameover(){
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료.\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
         int user = Integer.parseInt(Console.readLine());
 
-        if (user != 1 || user != 2) throw new IllegalArgumentException(inputError);
+        if (user != 1 && user != 2) throw new IllegalArgumentException(inputError);
 
-        return user;
+        return user == 1;
     }
 
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        while (game(setcom())!=2);
+        while (game());
     }
 
 }
