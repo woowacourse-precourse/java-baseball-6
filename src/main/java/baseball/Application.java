a796b096fd6d5efbdd6f267e154a87fefdb30f7f
meanyong <my011208@gmail.com>
2023-10-25 13:36:05 +0900

    vv

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
-
+import java.util.Scanner;
+import java.util.Random;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        for (int i = 0; i <3 ; i++) {
+            int r = new Random().nextInt(8) + 1;//난수를 r에 저장
+
+        }
+
+        String number;
+        System.out.println("Enter the number.");
+        Scanner sc = new Scanner(System.in);
+        number = sc.nextLine();
+        System.out.println("the number is "+number+".");
+
+
+        System.out.printf("%d", r);
+
+
     }
 }
