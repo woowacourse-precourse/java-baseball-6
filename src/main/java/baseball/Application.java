764663edc4441fc07f2b13d2f381487f88144564
inthejim <inthejim24@gmail.com>
2023-10-25 11:17:09 +0900

    feat: 문제 풀기(사용자 입력받기, 힌트계산&출력)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,9 @@ package baseball;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
@@ -17,5 +20,43 @@ public class Application {
                 computer.add(randomNumber);
             }
         }
+
+        // 문제 풀기
+        System.out.print("숫자를 입력해주세요 : ");
+        String answer= Console.readLine();
+
+        // 입력값에 대한 예외 처리 필요
+
+        // 힌트 출력
+        List<Integer> user = new ArrayList<>();
+        for(int i=0;i<3;i++){
+            user.add(Integer.parseInt(Character.toString(answer.charAt(i))));
+        }
+        int strike=0;
+        int ball=0;
+        for(int i=0;i<3;i++){
+            if(Objects.equals(user.get(i), computer.get(i))){
+                strike+=1;
+            }
+            else if(computer.contains(user.get(i))) {
+                ball += 1;
+            }
+        }
+
+        if(strike==0 && ball==0){
+            System.out.println("낫싱");
+        }
+        if(strike==0 && ball!=0){
+            System.out.println(strike+"스트라이크");
+        }
+        else if(strike!=0 && ball==0){
+            System.out.println(ball+"볼");
+        }
+        else{
+            System.out.println(ball+"볼 "+strike+"스트라이크");
+        }
+
+
+
     }
 }
