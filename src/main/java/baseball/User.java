55e947be843420181ea49e42edb099f910a40b5a
kdomo <dongho5088@naver.com>
2023-10-25 17:52:03 +0900

    feat: 유저 인풋을 받아 BaseBallNumber로 변환하는 getUserInput 메소드 생성

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -6,5 +6,13 @@ public class User {
     private static final int INPUT_LENGTH = 3;
     private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
 
-    int[] userInput = new int[INPUT_LENGTH];
+    int[] userBaseBallNumber = new int[INPUT_LENGTH];
+
+    public void getUserInput() {
+        System.out.print(INPUT_MESSAGE);
+        String input = Console.readLine();
+        for (int i = 0; i < INPUT_LENGTH; i++) {
+            this.userBaseBallNumber[i] = Integer.parseInt(String.valueOf(input.charAt(i)));
+        }
+    }
 }
