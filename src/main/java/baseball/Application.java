2ee7acef247384641380c6cab3764f51ccf7dc18
limzzum <chfhddl1234@naver.com>
2023-10-25 23:28:48 +0900

    :hammer: refactor : BaseBallGame 클래스에서 비지니스 로직 처리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,13 +8,11 @@ import static baseball.constant.PrintMessage.NO_MATCHING;
 import static baseball.constant.PrintMessage.STRIKE;
 import static baseball.constant.PrintMessage.SUCCESS_GAME_MESSAGE;
 
+import baseball.domain.BaseBallGame;
 import baseball.dto.StrikeBallCnt;
-import camp.nextstep.edu.missionutils.Randoms;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Application {
     static BufferedReader br;
@@ -34,14 +32,17 @@ public class Application {
     }
 
     private static void baseballGame() throws IOException {
-        List<Integer> answerNumber = createAnswerNumber();
+        BaseBallGame baseBallGame = new BaseBallGame();
+        baseBallGame.createAnswerNumber();
 
         String input;
         while (!isEnd){
             print(INPUT_NUMBER_MESSAGE);
             input = br.readLine();
-            checkValidNumber(input);
-            StrikeBallCnt result = getResult(input, answerNumber);
+            if(!baseBallGame.isValidNumber(input)){
+                throw new IllegalArgumentException(NOT_VALID_NUMBER);
+            }
+            StrikeBallCnt result = baseBallGame.getResult(input);
             printResult(result);
         }
 
@@ -65,23 +66,6 @@ public class Application {
         System.out.println(message);
     }
 
-    private static StrikeBallCnt getResult(String input, List<Integer> answer) {
-        int strikeCnt = 0;
-        int ballCnt = 0;
-
-        char[] chars = input.toCharArray();
-        for (int i = 0; i < 3; i++) {
-            if(chars[i] - '0' == answer.get(i)){
-                strikeCnt += 1;
-            }else if(answer.contains(chars[i] - '0')){
-                ballCnt += 1;
-            }
-        }
-
-        return new StrikeBallCnt(strikeCnt, ballCnt);
-
-    }
-
     private static void printResult(StrikeBallCnt strikeBallCnt) {
         int strikeCnt = strikeBallCnt.getStrikeCnt();
         int ballCnt = strikeBallCnt.getBallCnt();
@@ -101,24 +85,6 @@ public class Application {
             println(SUCCESS_GAME_MESSAGE);
         }
 
-    }
-
-    private static void checkValidNumber(String input) {
-        if(input.length() != 3){
-            throw new IllegalArgumentException(NOT_VALID_NUMBER);
-        }
-        Integer.parseInt(input);
-    }
-
-    private static List<Integer> createAnswerNumber() {
-        List<Integer> answer = new ArrayList<>();
-        while (answer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!answer.contains(randomNumber)) {
-                answer.add(randomNumber);
-            }
-        }
-        return answer;
     }
 
 }
