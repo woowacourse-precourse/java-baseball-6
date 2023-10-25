4175719abf57fe8abfe811df83bcd75fa04860ed
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-10-25 22:10:12 +0900

    feat: 컴퓨터 넘버 생성 기능 추가

--- src/main/java/baseball/model/BaseballGame.java
+++ src/main/java/baseball/model/BaseballGame.java
@@ -1,4 +1,29 @@
 package baseball.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static baseball.model.Constants.*;
+
 public class BaseballGame {
+
+    public List<Integer> getComputerNumber() {
+        List<Integer> computerNumber = new ArrayList<>();
+        while (computerNumber.size() < GAME_NUMBER_LENGTH) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_RANGE_NUM, MAX_RANGE_NUM);
+            addComputerNumber(computerNumber, randomNumber);
+        }
+        return computerNumber;
+    }
+
+    private static void addComputerNumber(List<Integer> computerNumber, int randomNumber) {
+        if (!computerNumber.contains(randomNumber)) {
+            computerNumber.add(randomNumber);
+        }
+    }
+
 }
