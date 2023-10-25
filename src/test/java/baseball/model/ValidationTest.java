d87e623136b2c580f84f02793b4f5f49beeea252
leeedohyun <ldh19738@gmail.com>
2023-10-25 17:07:13 +0900

    style: import statements 컨벤션 적용

--- src/test/java/baseball/model/ValidationTest.java
+++ src/test/java/baseball/model/ValidationTest.java
@@ -1,8 +1,8 @@
 package baseball.model;
 
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
 
 class ValidationTest {
 
