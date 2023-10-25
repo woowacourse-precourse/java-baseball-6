46151ad9a626e368b4a8f9da499f355a2e4d49b0
dhl1924 <dhl1924@naver.com>
2023-10-25 21:56:39 +0900

    refactor: 메세지를 상수로 치환

--- src/main/java/baseball/utils/MessageUtils.java
+++ src/main/java/baseball/utils/MessageUtils.java
@@ -1,31 +1,33 @@
 package baseball.utils;
 
+import baseball.constants.MessageConstants;
+
 public class MessageUtils {
     public static void startMessage() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println(MessageConstants.GAME_START);
     }
 
     public static void inputPromptMessage() {
-        System.out.print("숫자를 입력해주세요 : ");
+        System.out.print(MessageConstants.INPUT_NUMBER);
     }
 
     public static void resultMessage(int strike, int ball) {
         if (strike < 3 && strike > 0 && ball == 0) {
-            System.out.println(strike + "스트라이크");
+            System.out.println(strike + MessageConstants.STRIKE);
         }
         if (strike == 0 && ball != 0) {
-            System.out.println(ball + "볼");
+            System.out.println(ball + MessageConstants.BALL);
         }
         if (strike != 0 && ball != 0) {
-            System.out.println(ball + "볼 " + strike + "스트라이크");
+            System.out.println(ball + MessageConstants.BALL + " " + strike + MessageConstants.STRIKE);
         }
         if (strike == 0 && ball == 0) {
-            System.out.println("낫싱");
+            System.out.println(MessageConstants.NOTHING);
         }
         if (strike == 3) {
-            System.out.println("3스트라이크");
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            System.out.println(MessageConstants.THREE_STRIKES);
+            System.out.println(MessageConstants.GAME_OVER);
+            System.out.println(MessageConstants.RESTART_OR_QUIT);
         }
     }
 }
