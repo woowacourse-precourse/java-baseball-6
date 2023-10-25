d1b04be49b1819af2c8bba72dd045d37e8e6f586
“sangjun121” <juncho12011201@gmail.com>
2023-10-25 22:11:12 +0900

    feat: 게임 종료 입력 전까지 게임 반복하도록 반복문 구현 완료

--- src/main/java/baseball/controller/GameManager.java
+++ src/main/java/baseball/controller/GameManager.java
@@ -18,6 +18,7 @@ public class GameManager {
     private GameResultDto gameResultDto;
 
     private boolean isCorrectNumber;
+    private boolean isPauseGame;
 
     //생성자
     public GameManager(){
@@ -27,22 +28,25 @@ public class GameManager {
         judgement = new Judgement();
 
         isCorrectNumber = false;
+        isPauseGame = false;
     }
 
     public void startGame(){
-        computerNumberDto = computer.getRandomComputerNumber(); //랜덤한 3개 컴퓨터 숫자 생성
-        userView.printGameStartMessage(); //게임시작 메시지 출력
+        while(!isPauseGame){
+            computerNumberDto = computer.getRandomComputerNumber(); //랜덤한 3개 컴퓨터 숫자 생성
+            userView.printGameStartMessage(); //게임시작 메시지 출력
+            isCorrectNumber = false;
 
-        while(!isCorrectNumber){ //정답을 맞출 때까지 입력 반복
-            userView.printNumberInputPrompt(); //user input 프롬포트 메시지 출력
-            userNumberDto = user.readUserNumberInput(); //user input 시행
-            gameResultDto = judgement.compareNumbers(computerNumberDto,userNumberDto); //컴퓨터 숫자와 사용자 입력 숫자 비교
-            userView.printResultOfJudgement(gameResultDto); //게임 결과 값 출력
-            isCorrectNumber = gameResultDto.getIsCorrectNumber(); //정답여부 확인
+            while(!isCorrectNumber){ //정답을 맞출 때까지 입력 반복
+                userView.printNumberInputPrompt(); //user input 프롬포트 메시지 출력
+                userNumberDto = user.readUserNumberInput(); //user input 시행
+                gameResultDto = judgement.compareNumbers(computerNumberDto,userNumberDto); //컴퓨터 숫자와 사용자 입력 숫자 비교
+                userView.printResultOfJudgement(gameResultDto); //게임 결과 값 출력
+                isCorrectNumber = gameResultDto.getIsCorrectNumber(); //정답여부 확인
+            }
+
+            userView.printResumeInputPrompt(); //게임 재실행 여부 프롬포트 메시지 출력
+            isPauseGame = user.readUserPauseOrResumeInput(); //게임 재실행 여부 입력
         }
-
-        userView.printResumeInputPrompt(); //게임 재실행 여부 프롬포트 메시지 출력
-
-
     }
 }
