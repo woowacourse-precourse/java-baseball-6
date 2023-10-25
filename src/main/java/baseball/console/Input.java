28ade136848cb4b2665ababe399858253fb0e271
woookk <woook27@naver.com>
2023-10-25 21:27:55 +0900

    feat: 사용자 입력에 대한 유효성 검사 함수 추가

--- src/main/java/baseball/console/Input.java
+++ src/main/java/baseball/console/Input.java
@@ -2,6 +2,8 @@ package baseball.console;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.HashSet;
+
 public class Input {
 
     private static final String inputMessage = "숫자를 입력해주세요 : ";
@@ -10,12 +12,71 @@ public class Input {
     public static String get() {
         System.out.print(inputMessage);
         String number = Console.readLine();
+        if (!isValidateNumber(number)) {
+            throw new IllegalArgumentException();
+        }
         return number;
     }
 
     public static String restart() {
         System.out.println(restartMessage);
-        String number = Console.readLine();
-        return number;
+        String restartNumber = Console.readLine();
+        if (!isValidRestartNumber(restartNumber)) {
+            throw new IllegalArgumentException();
+        }
+
+        return restartNumber;
+    }
+
+    private static boolean isValidateNumber(String number) {
+        if (number.length() != 3) {
+            return false;
+        }
+        if (!isDigits(number)) {
+            return false;
+        }
+        if (containZero(number)) {
+            return false;
+        }
+        if (isDuplicate(number)) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isDigits(String number) {
+        for (char c : number.toCharArray()) {
+            if(!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean containZero(String number) {
+        for (char c: number.toCharArray()) {
+            if (c == '0') {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isDuplicate(String number) {
+        HashSet<Character> set = new HashSet<Character>();
+        for (char c : number.toCharArray()) {
+            if(!set.isEmpty() && set.contains(c)) {
+                return true;
+            }
+            set.add(c);
+        }
+        return false;
+    }
+
+    public static boolean isValidRestartNumber(String number) {
+        if (number.equals("1") || number.equals("2")) {
+            return true;
+        }
+        return false;
     }
 }
