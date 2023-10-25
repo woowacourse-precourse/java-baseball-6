d1f4e61ffbe2d4483c9f8b6a68d07995a00cf9d8
jaepyo99 <jaepyo99@naver.com>
2023-10-25 11:20:24 +0900

    [Refactory] 메서드에 static키워드 추가

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -8,17 +8,15 @@ import java.util.List;
 import static baseball.Constant.BASEBALL_NUM_LIMIT_LENGTH;
 
 public class Computer {
-    Validator validator = new Validator();
-
     public Computer() {
     }
 
 
-    public String createValidBaseballNum() {
+    public static String createValidBaseballNum() {
         List<String> baseballNum = new ArrayList<>();
         while (baseballNum.size() < BASEBALL_NUM_LIMIT_LENGTH) {
             String invalidNum = String.valueOf(Randoms.pickNumberInRange(1, 9));
-            if (validator.validNumNotInList(invalidNum, baseballNum)) {
+            if (Validator.validNumNotInList(invalidNum, baseballNum)) {
                 continue;
             }
             baseballNum.add(invalidNum);
