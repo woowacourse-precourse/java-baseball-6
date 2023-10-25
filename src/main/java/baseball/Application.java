0949a6ccd286b5bbe478eaea35c85ac1c740010f
EunhoRoh <deu04189@gmail.com>
2023-10-25 19:29:52 +0900

    feat: 요구사항 세팅

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,12 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
     }
 }
