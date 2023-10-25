eae499b4f8413218a75d0541fec874e038d78313
wonseok2877 <wonseok2877@gmail.com>
2023-10-25 18:55:28 +0900

    refactor: BaseBall equals 메서드 오버라이드

--- src/main/java/baseball/domain/BaseBall.java
+++ src/main/java/baseball/domain/BaseBall.java
@@ -1,5 +1,7 @@
 package baseball.domain;
 
+import java.util.Objects;
+
 public class BaseBall {
     public static final int MIN_VALUE = 1;
     public static final int MAX_VALUE = 9;
@@ -19,5 +21,22 @@ public class BaseBall {
         if (value < 1 || value > 9) {
             throw new IllegalArgumentException("1 ~ 9 만 입력해주세요");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        BaseBall baseBall = (BaseBall) o;
+        return this.value == baseBall.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
