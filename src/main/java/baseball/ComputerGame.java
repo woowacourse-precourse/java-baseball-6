ff3666bd9e4c117e60f9ea237f36d5cde76ebadd
EunKyo Chae <dmsry1022@gmail.com>
2023-10-25 18:40:30 +0900

    feat:gameResult 구현

--- src/main/java/baseball/ComputerGame.java
+++ src/main/java/baseball/ComputerGame.java
@@ -9,7 +9,7 @@ public class ComputerGame {
     private static final int MAX_NUMBER = 9;
     private static final int NUM_COUNT = 3;
     private List<Integer> numbers;
-    public void randomNumber(){
+    public List<Integer> randomNumber(){
         // 컴퓨터의 숫자 랜덤으로 3자리 뽑아내기
         numbers = new ArrayList<>(); // 실행시킬때마다 다르게 변경
         while(numbers.size() < NUM_COUNT){ // 0, 1, 2 => 3자리
@@ -18,6 +18,7 @@ public class ComputerGame {
                 numbers.add(randomNum);
             }
         }
+        return new ArrayList<>(numbers);
     }
 
 }
