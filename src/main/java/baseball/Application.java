e09635b013ee0e4aaf791010265b5ae0c2a92bd2
ulsandonghun <donghoon141@naver.com>
2023-10-25 22:36:52 +0900

    [feat] : 게임 질문 종료 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,6 +6,12 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         GameController gameController = new GameController();
-        gameController.gameInit();
+        while (true) {
+            gameController.gameInit();
+            String question = gameController.gameQuestion();
+            if (question.equals("2")) {
+                break;
+            }
+        }
     }
 }
--- src/main/java/baseball/GameController/GameController.java
+++ src/main/java/baseball/GameController/GameController.java
@@ -34,8 +34,13 @@ public class GameController {
             inputView();
             input = Console.readLine();
             checkInput(user, input);
-
         }
+    }
+
+    public String gameQuestion() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String s = Console.readLine();
+        return s;
     }
 
     void startView() {
