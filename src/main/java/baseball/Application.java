404287fed410004b6c7b300e34c0d25852e85a20
newoldname <thjbjs@outlook.com>
2023-10-25 17:47:27 +0900

    [Style] import문, if문, switch문에 대한 형식을 다시 수정함

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,18 +5,22 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        int TOTAL_NUMBER = 3;
+        int totalNumber = 3;
 
-        BaseballGame gameAI = new BaseballGame(TOTAL_NUMBER);
+        BaseballGame gameAI = new BaseballGame(totalNumber);
         System.out.println(gameAI + "을 시작합니다.");
 
         while (true) {
             gameAI.play();
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             String continueInput = readLine();
-            if (continueInput.equals("2")) return;
-            else if (continueInput.equals("1")) continue;
-            else throw new IllegalArgumentException();
+            if (continueInput.equals("2")) {
+                return;
+            } else if (continueInput.equals("1")) {
+                continue;
+            } else {
+                throw new IllegalArgumentException();
+            }
         }
     }
 }
--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,19 +1,14 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Randoms;
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
-import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class BaseballGame {
-    private enum BaseBallType {
-      NOTHING,
-      BALL,
-      STRIKE
-    }
-
+    private enum BaseBallType { NOTHING, BALL, STRIKE }
     private List<Integer> computerBallList;
     private int totalNumber;
 
@@ -22,8 +17,8 @@ public class BaseballGame {
     }
 
     private void initBallNumbers() {
-        this.computerBallList = new ArrayList<>();
-        while (computerBallList.size() < this.totalNumber) {
+        computerBallList = new ArrayList<>();
+        while (computerBallList.size() < totalNumber) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computerBallList.contains(randomNumber)) {
                 computerBallList.add(randomNumber);
@@ -48,7 +43,7 @@ public class BaseballGame {
         }
 
         // 입력값이 중복했는지 검사
-        for (int i = 0; i < this.totalNumber; i++) {
+        for (int i = 0; i < totalNumber; i++) {
             int nowInt = inputNumber % 10;
             if (!numberList.contains(nowInt)) {
                 numberList.add(0, nowInt);
@@ -62,10 +57,10 @@ public class BaseballGame {
     }
 
     private BaseBallType gradeOneNumber(int userNumber, int nowIndex) {
-        if (this.computerBallList.get(nowIndex) == userNumber) {
+        if (computerBallList.get(nowIndex) == userNumber) {
             return BaseBallType.STRIKE;
         } else {
-            for (int computerInt : this.computerBallList) {
+            for (int computerInt : computerBallList) {
                 if (computerInt == userNumber) {
                     return BaseBallType.BALL;
                 }
@@ -75,9 +70,15 @@ public class BaseballGame {
     }
 
     private void printResult(int ballTimes, int strikeTimes) {
-        if (ballTimes > 0) System.out.print(ballTimes + "볼 ");
-        if (strikeTimes > 0) System.out.print(strikeTimes + "스트라이크");
-        if (ballTimes + strikeTimes == 0) System.out.print("낫싱");
+        if (ballTimes > 0) {
+            System.out.print(ballTimes + "볼 ");
+        }
+        if (strikeTimes > 0) {
+            System.out.print(strikeTimes + "스트라이크");
+        }
+        if (ballTimes + strikeTimes == 0) {
+            System.out.print("낫싱");
+        }
         System.out.println();
     }
 
@@ -89,19 +90,24 @@ public class BaseballGame {
         List<Integer> userInputNumberList = stringToIntList(userInput);
 
         // 숫자 판단 부분
-        for (int i = 0; i < this.totalNumber; i++) {
+        for (int i = 0; i < totalNumber; i++) {
             BaseBallType result = gradeOneNumber(userInputNumberList.get(i), i);
             switch (result) {
-              case STRIKE -> strikeTimes += 1;
-              case BALL -> ballTimes += 1;
-              default -> {}
+                case STRIKE:
+                    strikeTimes += 1;
+                    break;
+                case BALL:
+                    ballTimes += 1;
+                    break;
+                default:
+                    break;
             }
         }
 
         // 결과 출력 부분
         printResult(ballTimes, strikeTimes);
-        if (strikeTimes == this.totalNumber) {
-            System.out.println(this.totalNumber + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        if (strikeTimes == totalNumber) {
+            System.out.println(totalNumber + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
             return true;
         }
         return false;
@@ -113,8 +119,9 @@ public class BaseballGame {
             System.out.print("숫자를 입력해주세요 : ");
             String userInput = readLine();
             boolean gameResult = gradeAllNumber(userInput);
-            if (gameResult) return;
-            else continue;
+            if (gameResult) {
+                return;
+            }
         }
     }
 
