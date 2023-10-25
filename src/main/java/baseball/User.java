d8157526ce61f28e16f4ae13bd280b41f8a1eb77
gourderased <gourderased@gmail.com>
2023-10-25 23:04:38 +0900

    Fix: "User 클래스 수정"
    - userNumbers List<Integer>로 변경

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,22 +1,30 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class User {
 
-    private String userNumber;
+    List<Integer> userNumbers;
 
     public void setUserNumbers() {
         System.out.println("숫자를 입력해주세요 : ");
-        String userNumber = Console.readLine();
+        String inputString = Console.readLine();
+        int inputNumber = Integer.parseInt(inputString);
         //3자리, 중복, 0여부 확인
-        if (userNumber.matches("^(?!.*(.).*\\1)[1-9]{3}$")) {
+        if (inputNumber < 100 || inputNumber > 999) {
             throw new IllegalArgumentException("잘못된 숫자 입력입니다.");
         }
-        this.userNumber = userNumber;
+        List<Integer> generatedNumbers = new ArrayList<>();
+        generatedNumbers.add(inputNumber / 100);
+        generatedNumbers.add(inputNumber / 10 % 10);
+        generatedNumbers.add(inputNumber % 10);
+
+        this.userNumbers = generatedNumbers;
     }
 
-    public String getUserNumbers() {
-        return userNumber;
+    public List<Integer> getUserNumbers() {
+        return userNumbers;
     }
 }
