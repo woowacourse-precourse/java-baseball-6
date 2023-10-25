d9d21fa64d023a59b4ae7a76582a6ce15c9bda1f
MingyeomKim <mingyum119@naver.com>
2023-10-25 21:02:46 +0900

    refactor: 논리 연산자를 사용한 구문의 경계를 명확히 표시
    논리 연산자를 사용한 구문의 경계를 괄호를 사용해 명확히 표시한다.

--- src/main/java/baseball/utils/HintGenerator.java
+++ src/main/java/baseball/utils/HintGenerator.java
@@ -8,7 +8,6 @@ import baseball.model.Numbers;
 import java.util.stream.IntStream;
 
 public class HintGenerator {
-
     public static Hint generate(Numbers computer, Numbers player) {
         return Hint.from(countStrike(computer, player), countBall(computer, player));
     }
@@ -21,9 +20,8 @@ public class HintGenerator {
 
     private static Integer countBall(Numbers computer, Numbers player) {
         return (int) IntStream.range(INITIAL_INDEX, LAST_INDEX)
-                .filter(index ->
-                        !computer.equalsAt(player.valueAt(index), index)
-                                && computer.contains(player.valueAt(index)))
+                .filter(index -> (!computer.equalsAt(player.valueAt(index), index)
+                        && computer.contains(player.valueAt(index))))
                 .count();
     }
 }
--- src/main/java/baseball/validators/RestartOrExitValidator.java
+++ src/main/java/baseball/validators/RestartOrExitValidator.java
@@ -1,9 +1,8 @@
 package baseball.validators;
 
 public class RestartOrExitValidator {
-
     public static void validateRestartOrExit(String inputString) {
-        if (inputString.length() != 1 || !inputString.equals("1") && !inputString.equals("2")) {
+        if (inputString.length() != 1 || (!inputString.equals("1") && !inputString.equals("2"))) {
             throw new IllegalArgumentException("1 또는 2를 입력해주세요.");
         }
     }
