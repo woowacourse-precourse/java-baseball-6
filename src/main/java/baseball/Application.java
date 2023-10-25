82411525a57c97d12090cf99792a05983372c44a
SonJimin <lijnar100@naver.com>
2023-10-25 21:19:01 +0900

    Feat : 숫자 야구 기능 구현 완료. 테스트 실패도 수정 후 테스트 통과 완료.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,109 +1,127 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
+
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        try {
-            // 컴퓨터의 랜덤 숫자
-            int[] computerNumbers = generateRandomNumbers();
 
-            // 게임 루프
-            while (true) {
-                int[] userNumbers = getUserInput(); // 유저가 입력한 숫자
-                int[] result = calculateResult(computerNumbers, userNumbers); // 컴퓨터 숫자와 유저 입력값 비교
-                System.out.println("컴퓨터숫자: " + computerNumbers[0]+computerNumbers[1]+computerNumbers[2]);
-//                int threeDigitNumber = computerNumbers[0] * 100 + computerNumbers[1] * 10 + computerNumbers[2];
-                displayResult(result); // 결과 출력 메서드
+        // 게임 재시작 또는 종료 여부를 확인하기 위한 변수
+        boolean restartGame = true;
 
-                if (result[0] == 3) { // 스트라이크 3개면 종료 또는 재시작 선택
-                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-
-                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                    String choice = Console.readLine();
-
-                    if (choice.equals("1")) {
-                        computerNumbers = generateRandomNumbers(); // 새로운 랜덤 숫자 생성
-                    } else if (choice.equals("2")) {
-                        System.out.println("게임을 종료합니다.");
-                        break; // 게임 종료
-                    } else {
-                        System.out.println("유효하지 않은 선택입니다. 게임을 종료합니다.");
-                        break; // 게임 종료
-                    }
-                }
-            }
-        } catch (IllegalArgumentException e) {
-            System.out.println("잘못된 입력: " + e.getMessage());
+        // 게임 재시작 또는 종료 여부에 따라 반복 실행
+        while (restartGame) {
+            playGame();
+            restartGame = askForRestart();
         }
+
+        System.out.println("게임 종료");
     }
 
-    // 1부터 9까지 서로 다른 3개의 숫자 무작위 선택
-    public static int[] generateRandomNumbers() {
-        int[] numbers = new int[3];
-        for (int i = 0; i < 3; i++) {
-            int randomNumber;
-            do {
-                randomNumber = Randoms.pickNumberInRange(1, 9);
-            } while (contains(numbers, randomNumber));
-            numbers[i] = randomNumber;
-        }
-        return numbers;
-    }
-
-    // 사용자로부터 3자리 숫자 입력 받기
-    public static int[] getUserInput() {
-        int[] numbers = new int[3];
-        while (true) {
-            System.out.print("숫자를 입력해주세요 : ");
-            String input = Console.readLine();
-            if (input.matches("^[1-9]{3}$")) {
-                for (int i = 0; i < 3; i++) {
-                    numbers[i] = Integer.parseInt(input.substring(i, i + 1));
-                }
-                return numbers;
-            } else {
-                throw new IllegalArgumentException("서로 다른 3자리 숫자를 입력하세요.");
+    // 게임을 실행하는 메서드
+    private static void playGame() {
+        List<Integer> computer = generateRandomNumbers(); // 랜덤 숫자를 발생시키는 메서드를 통해 숫자 생성
+        boolean gameIsOver = false; // 3스트라이크가 나올 때까지 반복을 위한 변수 (기본값: false)
+        System.out.println("computer숫자 : " + computer.get(0) + computer.get(1) + computer.get(2)); // 컴퓨터가 생성한 숫자를 테스트를 위해 출력
+        while (!gameIsOver) { // 3스트라이크가 나올 때까지 반복 (기본값: false)
+            List<Integer> userGuess = getUserGuess(); // 사용자 입력값과 컴퓨터 랜덤값 비교를 위해 가져옴.
+            String result = calculateResult(computer, userGuess); // 결과 계산
+            System.out.println(result); //결과 출력
+            if (result.equals("3스트라이크")) { // 3스트라이크면 게임 종료 true 로 변경
+                gameIsOver = true;
             }
         }
     }
 
-    // 결과 계산 메서드
-    public static int[] calculateResult(int[] computerNumbers, int[] userNumbers) {
-        int[] result = new int[2]; // result[0]: 스트라이크, result[1]: 볼
-
-        for (int i = 0; i < 3; i++) {
-            if (computerNumbers[i] == userNumbers[i]) {
-                result[0]++; // 스트라이크
-            } else if (contains(computerNumbers, userNumbers[i])) {
-                result[1]++; // 볼
+    // 랜덤 숫자를 생성하는 메서드
+    private static List<Integer> generateRandomNumbers() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
             }
         }
-        return result;
+        return computer;
     }
 
-    // 배열에 특정 숫자가 포함되어 있는지 확인하는 메서드
-    public static boolean contains(int[] arr, int num) {
-        for (int i : arr) {
-            if (i == num) {
-                return true;
+    // 사용자로부터 숫자 입력을 받아 리스트로 반환하는 메서드
+    private static List<Integer> getUserGuess() {
+        System.out.print("숫자를 입력해주세요 : "); // 사용자에게 숫자 입력을 요청
+        String input = Console.readLine(); // 사용자로부터 문자열 입력을 받음
+        List<Integer> userGuess = convertInputToNumberList(input); // 입력값을 숫자 리스트로 변환
+        return userGuess;
+    }
+
+    // 사용자 입력 문자열을 숫자 리스트로 변환하는 메서드
+    private static List<Integer> convertInputToNumberList(String input) {
+        List<Integer> numbers = new ArrayList<>(); // 결과 숫자 리스트를 저장하기 위한 리스트 생성
+        for (char digit : input.toCharArray()) {
+            int number = Character.getNumericValue(digit); // 문자를 숫자로 변환
+            if (number < 1 || number > 9) {
+                throw new IllegalArgumentException("1부터 9까지의 숫자만 입력 가능합니다."); // 유효하지 않은 입력 예외 처리
+            }
+            numbers.add(number); // 유효한 숫자를 리스트에 추가
+        }
+        if (numbers.size() != 3) {
+            throw new IllegalArgumentException("3자리 숫자를 입력해야 합니다."); // 3자리가 아닌 경우 예외 처리
+        }
+        return numbers; // 변환된 숫자 리스트 반환
+    }
+
+
+    // 게임 결과를 계산하고 반환하는 메서드
+    private static String calculateResult(List<Integer> computer, List<Integer> userGuess) {
+        int strikes = 0;
+        int balls = 0;
+
+        for (int i = 0; i < computer.size(); i++) {
+            if (computer.get(i).equals(userGuess.get(i))) {
+                strikes++;
+            } else if (computer.contains(userGuess.get(i))) {
+                balls++;
             }
         }
-        return false;
+
+        if (strikes == 3) {
+            System.out.println("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
+
+        return createResultString(strikes, balls);
     }
 
-    // 결과 출력 메서드
-    public static void displayResult(int[] result) {
-        if (result[0] == 0 && result[1] == 0) {
-            System.out.println("낫싱");
-        } else if (result[0] > 0 && result[1] == 0) {
-            System.out.println(result[0] + "스트라이크");
-        } else if (result[0] == 0 && result[1] > 0) {
-            System.out.println(result[1] + "볼");
+    // 게임 결과를 문자열로 만드는 메서드
+    private static String createResultString(int strikes, int balls) {
+        StringBuilder result = new StringBuilder();
+        if (balls > 0) {
+            result.append(balls).append("볼");
+        }
+        if (strikes > 0) {
+            if (result.length() > 0) {
+                result.append(" ");
+            }
+            result.append(strikes).append("스트라이크");
+        }
+        return result.length() > 0 ? result.toString() : "낫싱";
+    }
+
+    // 게임 재시작 또는 종료 여부를 확인하는 메서드
+    private static boolean askForRestart() {
+        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요: ");
+        String input = Console.readLine();
+        if ("1".equals(input)) {
+            return true;
+        } else if ("2".equals(input)) {
+            return false;
         } else {
-            System.out.println(result[1] + "볼 " + result[0] + "스트라이크");
+            System.out.println("1 또는 2만 입력 가능합니다. 게임을 종료합니다.");
+            System.exit(0);
+            return false;  // 도달할 수 없는 코드 (완성을 위해 추가됨)
         }
     }
 }
