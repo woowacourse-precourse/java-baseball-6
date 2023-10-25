5c00f2d653df33d724cb793fe446b50abfd08ff1
zeomzzz <jmlee2705@gmail.com>
2023-10-25 22:09:03 +0900

    refractor: Computer에서 getAnswerNumber 사용하지 않아서 제거

--- src/main/java/baseball/models/Computer.java
+++ src/main/java/baseball/models/Computer.java
@@ -10,10 +10,6 @@ public class Computer {
 
     private String answerNumber;
 
-    public String getAnswerNumber() {
-        return answerNumber;
-    }
-
     // 1부터 9까지 서로 다른 수로 이루어진 수 선택
     public void chooseNumber(int length) {
         String result = "";
