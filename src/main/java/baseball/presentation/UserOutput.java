4449e69fb63c06416929cbd38bc4cd52720e94b1
TaeHeyeon_Kim <thk98k@gmail.com>
2023-10-25 20:28:06 +0900

    refactor: 메서드 네이밍 변경 및 출력 값 수정

--- src/main/java/baseball/presentation/UserOutput.java
+++ src/main/java/baseball/presentation/UserOutput.java
@@ -2,10 +2,10 @@ package baseball.presentation;
 
 public class UserOutput {
 
-    public boolean outputMessage(int strike, int ball) {
+    public void printBallCount(int strike, int ball) {
         String message = "";
         if (strike > 0 && ball > 0) {
-            message = strike + "스트라이크 " + ball + "볼";
+            message = ball + "볼 " + strike + "스트라이크";
         } else if (strike > 0) {
             message = strike + "스트라이크";
         } else if (ball > 0) {
@@ -14,13 +14,9 @@ public class UserOutput {
             message = "낫싱";
         }
         System.out.println(message);
+    }
 
-        if (strike == 3) {
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            return true;
-        }
-
-        return false;
+    public void endingMessage() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 }
