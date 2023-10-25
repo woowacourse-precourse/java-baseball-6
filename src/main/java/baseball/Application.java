8629c4c85d68e104f757ed098fc10360e961a17d
Sungkyu <kxu4583@naver.com>
2023-10-25 18:06:52 +0900

    Google Style에 부합하도록 코드 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,13 +5,18 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
 
+    private static final int ANSWER_LENGTH = 3;
+    private static final int RESTART_GAME = 1;
+    private static final int END_GAME = 2;
+
+
     static int[] createNewArray(){
 
         System.out.println("숫자 야구 게임을 시작합니다.");
 
         //정답이 될 3개의 숫자를 만들기
-        int[] answer = new int[3];
-        for(int i=0;i<3;i++){
+        int[] answer = new int[ANSWER_LENGTH];
+        for(int i=0;i<ANSWER_LENGTH;i++){
             answer[i] = Randoms.pickNumberInRange(1, 9);
             for(int j=0; j < i; j++){
                 if(answer[j] == answer[i]) {
@@ -25,31 +30,37 @@ public class Application {
 
     static int[] receiveNewArray() {
 
-        int[] arr = new int[3];
+        int[] arr = new int[ANSWER_LENGTH];
 
         System.out.print("숫자를 입력해주세요 : ");
 
         //사용자로부터 3개의 숫자 입력받기
         String readLine = Console.readLine();
-        if(readLine.length()!=3) throw new IllegalArgumentException(); //길이가 3이 아니면 오류
-        for (int i = 0; i < 3; i++) {
+        if(readLine.length()!=ANSWER_LENGTH) {
+            throw new IllegalArgumentException("입력은 공백 없이 한자릿수 숫자 3개여야 합니다.");
+        }
+        for (int i = 0; i < ANSWER_LENGTH; i++) {
             int temp = readLine.charAt(i) - '0';
-            if (temp < 1 || 9 < temp) throw new IllegalArgumentException(); //정수 1~9 사이의 값이 아니면 오류
+            if (temp < 1 || 9 < temp) {
+                throw new IllegalArgumentException("세 숫자는 정수 1~9 사이여야 합니다.");
+            }
             arr[i] = temp;
             for(int j=0; j < i; j++){
-                if(arr[j] == arr[i]) throw new IllegalArgumentException(); //중복 숫자가 있으면 오류
+                if(arr[j] == arr[i]) {
+                    throw new IllegalArgumentException("세 숫자는 서로 달라야 합니다.");
+                }
             }
         }
         return arr;
     }
 
-    static boolean judge(int[] answer, int[] ask){
+    static boolean testInput(int[] answer, int[] ask){
 
         int strike=0;
         int ball=0;
 
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++) {
+        for(int i=0;i<ANSWER_LENGTH;i++){
+            for(int j=0;j<ANSWER_LENGTH;j++) {
                 if (answer[j] == ask[i]) {
                     if(i==j) strike++;
                     else ball++;
@@ -57,12 +68,18 @@ public class Application {
             }
         }
 
-        if(ball==0 && strike==0) System.out.print("낫싱");
-        if(ball!=0) System.out.print(ball + "볼 ");
-        if(strike!=0) System.out.print(strike + "스트라이크");
+        if(ball==0 && strike==0) {
+            System.out.print("낫싱");
+        }
+        if(ball!=0) {
+            System.out.print(ball + "볼 ");
+        }
+        if(strike!=0) {
+            System.out.print(strike + "스트라이크");
+        }
         System.out.println();
-        if(strike==3){
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        if(strike==ANSWER_LENGTH){
+            System.out.println(ANSWER_LENGTH + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
             return true;
         }
         return false;
@@ -71,9 +88,13 @@ public class Application {
     static int endGame(){
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String readLine = Console.readLine();
-        if(readLine.length()!=1) throw new IllegalArgumentException(); //길이가 1이 아니면 오류
+        if(readLine.length()!=1) {
+            throw new IllegalArgumentException("입력은 한자리 숫자 하나여야 합니다.");
+        }
         int temp = readLine.charAt(0) - '0';
-        if(temp < 1 || 2 < temp) throw new IllegalArgumentException(); //정수 1 또는 2가 아니면 오류
+        if(temp < RESTART_GAME || END_GAME < temp) {
+            throw new IllegalArgumentException("입력은 " + RESTART_GAME + " 또는 " + END_GAME + "여야 합니다.");
+        }
         return temp;
     }
 
@@ -82,13 +103,13 @@ public class Application {
         do {
 
             int[] answer = createNewArray();
-            int[] ask;
+            int[] userInput;
 
             try {
 
                 do {
-                    ask = receiveNewArray();
-                } while (!judge(answer, ask));
+                    userInput = receiveNewArray();
+                } while (!testInput(answer, userInput));
 
                 key = endGame();
 
