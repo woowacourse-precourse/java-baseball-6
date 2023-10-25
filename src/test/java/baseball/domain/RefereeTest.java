b7d58562f0ad8da9669ae31680922f73361b78fc
조은별 <joeunbyeol98@gmail.com>
2023-10-25 20:47:05 +0900

    refactor(RefereeTest): refree 변수 제거

--- src/test/java/baseball/domain/RefereeTest.java
+++ src/test/java/baseball/domain/RefereeTest.java
@@ -1,7 +1,8 @@
 package baseball.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,40 +11,36 @@ class RefereeTest {
     @DisplayName("1볼")
     @Test
     void ball() {
-        Referee referee = new Referee();
         List<Integer> computer = List.of(1, 2, 3);
         List<Integer> player = List.of(3, 4, 5);
-        Assertions.assertThat(referee.countBallAndStrike(computer, player))
+        assertThat(new Referee().countBallAndStrike(computer, player))
                 .isEqualTo(List.of(1, 0));
     }
 
     @DisplayName("2스트라이크")
     @Test
     void strike() {
-        Referee referee = new Referee();
         List<Integer> computer = List.of(1, 2, 3);
         List<Integer> player = List.of(9, 2, 3);
-        Assertions.assertThat(referee.countBallAndStrike(computer, player))
+        assertThat(new Referee().countBallAndStrike(computer, player))
                 .isEqualTo(List.of(0, 2));
     }
 
     @DisplayName("2볼 1스트라이크")
     @Test
     void ballAndStrike() {
-        Referee referee = new Referee();
         List<Integer> computer = List.of(5, 2, 8);
         List<Integer> player = List.of(8, 2, 5);
-        Assertions.assertThat(referee.countBallAndStrike(computer, player))
+        assertThat(new Referee().countBallAndStrike(computer, player))
                 .isEqualTo(List.of(2, 1));
     }
 
     @DisplayName("낫싱")
     @Test
     void nothing() {
-        Referee referee = new Referee();
         List<Integer> computer = List.of(5, 2, 8);
         List<Integer> player = List.of(4, 1, 7);
-        Assertions.assertThat(referee.countBallAndStrike(computer, player))
+        assertThat(new Referee().countBallAndStrike(computer, player))
                 .isEqualTo(List.of(0, 0));
     }
 }
