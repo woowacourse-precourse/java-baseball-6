6412d06dde663bc0f028483cdd7f642206ea6078
TaeHeyeon_Kim <thk98k@gmail.com>
2023-10-25 20:27:34 +0900

    feat: 재시작 메세지 출력

--- src/main/java/baseball/presentation/UserInput.java
+++ src/main/java/baseball/presentation/UserInput.java
@@ -13,6 +13,7 @@ public class UserInput {
     }
 
     public String inputRestartNumber() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String input = Console.readLine();
         inputRestartNumberExceptionHandler(input);
         return input;
