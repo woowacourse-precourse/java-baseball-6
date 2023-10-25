6a81ed1d1116ffe2df89781c0ed00690425d309c
Sung Hyobin <hellmir610@gmail.com>
2023-10-25 21:12:23 +0900

    feat: Add validator to validate data input by clients

---
+++ src/main/java/baseball/validation/InputFormatValidator.java
@@ -0,0 +1,37 @@
+package baseball.validation;
+
+import static baseball.constant.ErrorMessage.BASEBALL_NUMBERS_INPUT_EXCEPTION;
+import static baseball.constant.ErrorMessage.CHOICE_NUMBER_INPUT_EXCEPTION;
+
+public class InputFormatValidator {
+
+    public static void validateBaseballNumbers(String inputNumbers) {
+        if (inputNumbers.matches("^\\d{3}$") && validateNotDuplicatedNumbers(inputNumbers)) {
+            return;
+        }
+
+        throw new IllegalArgumentException(BASEBALL_NUMBERS_INPUT_EXCEPTION);
+    }
+
+    public static void validateChoiceNumber(String choiceOfRestart) {
+        if (choiceOfRestart.equals("1") || choiceOfRestart.equals("2")) {
+            return;
+        }
+
+        throw new IllegalArgumentException(CHOICE_NUMBER_INPUT_EXCEPTION);
+    }
+
+    private static boolean validateNotDuplicatedNumbers(String inputNumbers) {
+
+        for (int i = 0; i < inputNumbers.length() - 1; i++) {
+            for (int j = i + 1; j < inputNumbers.length(); j++) {
+                if (inputNumbers.charAt(i) == inputNumbers.charAt(j)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+}
