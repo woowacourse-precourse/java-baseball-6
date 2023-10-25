312713e240fcc0cd733bfe46f311640449ffcdb9
Jimin Kim <0305jimin@gmail.com>
2023-10-25 19:37:17 +0900

    Feat: 게임 답안 랜덤 수 생성 및 유저 인풋 받기, 인풋 저장

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,8 +1,14 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static java.lang.Integer.parseInt;
+
 public class Game {
     static List<Integer> computer = new ArrayList<>();
     static List<Integer> user = new ArrayList<>();
@@ -13,14 +19,40 @@ public class Game {
     static int ball = 0;
 
     private void printGameStartMessage() {
-
+        System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    private int generateRandomNumber() {
+    private void generateRandomNumber() {
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        System.out.print("답:");
+        for (int e: computer) {
+            System.out.print(e);
+        }
+        System.out.println("");
+
     }
 
     private void getUserInput() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String userInputString = readLine();
+        while (userInputString.length() != 3) {
+            System.out.println("잘못된 입력입니다. 세 자리 수로 다시 입력해주세요.");
+            userInputString = readLine();
+        }
 
+        int userInputInt = parseInt(userInputString);
+//        System.out.println(userInputInt);
+
+        for (int i = 0; i < 3; i++) {
+            user.add(userInputInt % 10);
+            userInputInt = userInputInt / 10;
+        }
+//        System.out.println("getUserInput 끝");
     }
 
     private void printResult() {
