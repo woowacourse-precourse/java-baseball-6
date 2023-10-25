4e93cd0d86eb30ec8e79ec7ac9f53795465bb190
shinel98 <75024473+shinel98@users.noreply.github.com>
2023-10-25 19:31:56 +0900

    refactor(OutputView): implement getResultMessage

--- src/main/java/baseball/View/OutputView.java
+++ src/main/java/baseball/View/OutputView.java
@@ -3,6 +3,9 @@ package baseball.View;
 public class OutputView {
     private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String STRIKE = "스트라이크";
+    private static final String BALL = "볼";
+    private static final String NOTHING = "낫싱";
 
     public static void printStart() {
         System.out.println(START_MESSAGE);
@@ -13,14 +16,19 @@ public class OutputView {
     }
 
     public static void printGameResult(int strikeCount, int ballCount) {
+        String result = getResultMessage(strikeCount, ballCount);
+        System.out.println(result);
+    }
+
+    private static String getResultMessage(int strikeCount, int ballCount) {
         if (hasBothStrikeAndBall(strikeCount, ballCount)) {
-            printBothStrikeAndBall(ballCount, strikeCount);
+            return ballCount + BALL + " " + strikeCount + STRIKE;
         } else if (hasStrikeOnly(strikeCount)) {
-            printStrikeOnly(strikeCount);
+            return strikeCount + STRIKE;
         } else if (hasBallOnly(ballCount)) {
-            printBallOnly(ballCount);
+            return ballCount + BALL;
         } else {
-            printNothing();
+            return NOTHING;
         }
     }
 
@@ -35,21 +43,4 @@ public class OutputView {
     private static boolean hasBallOnly(int ballCount) {
         return ballCount > 0;
     }
-
-    private static void printBothStrikeAndBall(int ballCount, int strikeCount) {
-        System.out.println(ballCount + "볼" + " " + strikeCount + "스트라이크");
-    }
-
-    private static void printStrikeOnly(int strikeCount) {
-        System.out.println(strikeCount + "스트라이크");
-    }
-
-    private static void printBallOnly(int ballCount) {
-        System.out.println(ballCount + "볼");
-    }
-
-    private static void printNothing() {
-        System.out.println("낫싱");
-    }
-
 }
