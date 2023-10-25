30dc36765fa3a9d1bb2e167b501b70fcba67a19a
Wo-ogie <siwall0105@gmail.com>
2023-10-25 19:00:08 +0900

    feat: 게임 시작 기능 및 게임 시작 안내 메시지 출력 기능 구현

---
+++ src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,10 @@
+package baseball.controller;
+
+import static baseball.view.GameOutputView.printGameStartMessage;
+
+public class GameController {
+
+    public void start() {
+        printGameStartMessage();
+    }
+}
