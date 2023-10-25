34f6011d81bb3c98962446e0f65a1f19fa1fa94b
J-petto <hminji4565@gmail.com>
2023-10-25 14:23:57 +0900

    player Num add list & result print

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -29,7 +29,7 @@ public class Application {
 
             //플레이어에게 받은 값이 잘못된 값일 경우
             try{
-                if (playerNum.length() != 3){
+                if (playerNum.length() != 3) {
                     throw new IllegalAccessException();
                 }
             } catch (IllegalAccessException e){
@@ -37,6 +37,32 @@ public class Application {
                 play = false;
             }
 
+            // 3자리 수 리스트에 넣기
+            while (player.size() < 3){
+                player.add(player.size(), Integer.parseInt(playerNum.substring(player.size(),player.size()+1)));
+                //System.out.println(player);
+            }
+
+            int strike = 0;
+            if(computer.get(0) == player.get(0)) strike += 1;
+            if(computer.get(1) == player.get(1)) strike += 1;
+            if(computer.get(2) == player.get(2)) strike += 1;
+
+            int ball = 0;
+            if(computer.get(1) == player.get(0) || computer.get(2) == player.get(0)) ball += 1;
+            if(computer.get(0) == player.get(1) || computer.get(2) == player.get(1)) ball += 1;
+            if(computer.get(0) == player.get(2) || computer.get(1) == player.get(2)) ball += 1;
+
+            if(strike == 3){
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            } else if (strike == 0) {
+                System.out.println(ball + "볼");
+            } else if (ball == 0) {
+                System.out.println(strike + "스트라이크");
+            }else {
+                System.out.println(ball + "볼" + strike + "스트라이크");
+            }
         }
     }
 }
