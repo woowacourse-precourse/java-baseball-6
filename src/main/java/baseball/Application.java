f059253934b8291cb86591083809fb16c57b816e
Minyongggg <naminyong97@gmail.com>
2023-10-25 22:27:13 +0900

    feat: 전체 어플리케이션 실행 로직 작성
    - 어플리케이션 시작 후 하나의 게임 객체를 생성하여 게임 진행
    - 해당 게임 종료 시, 재시도 여부 확인하여 1일 경우 새 게임 진행
    - 2일 경우 어플리케이션 종료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
+import baseball.io.InputHandler;
+import baseball.io.OutputHandler;
+import baseball.model.RetryOption;
+import baseball.service.BaseballGame;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        OutputHandler.printGameStart();
+        do {
+            BaseballGame baseballGame = new BaseballGame();
+            baseballGame.start();
+        } while (askRetry());
+    }
+
+    public static boolean askRetry() {
+        RetryOption retryOption = InputHandler.getRetryOption();
+        return retryOption.getValue().equals("1");
     }
 }
