5530a18dd5f87bf6dc85d577bbfb35e70f2dbf36
hj-k66 <hjx66@naver.com>
2023-10-25 17:56:04 +0900

    feat: 컴퓨터 랜덤 숫자 생성 기능 구현

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,5 +1,51 @@
 package baseball;
 
-public class Computer {
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private final List<Integer> computerNum;
+
+    public Computer(){
+        computerNum = new ArrayList<>();
+    }
+
+    public List<Integer> getComputerNum(){
+        createComputerNum();
+        try{
+            validateRangeForAll();
+            validateSize();
+        }catch (IllegalArgumentException e){
+            getComputerNum();
+        }
+        return computerNum;
+    }
+    public void createComputerNum() {
+        while (computerNum.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 13);
+            if(!computerNum.contains(randomNumber)){
+                computerNum.add(randomNumber);
+            }
+        }
+    }
+
+    public void validateRangeForAll(){
+        for(int elem : computerNum){
+            validateRange(elem);
+        }
+    }
+
+    public void validateRange(int num){
+        if(num < 1 || num > 9){
+            throw new IllegalArgumentException("숫자는 1~9범위이여야 합니다.");
+        }
+    }
+
+    public void validateSize(){
+        if(computerNum.size() > 3){
+            throw new IllegalArgumentException("3자리 수 숫자여야 합니다.");
+        }
+    }
 }
--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import java.util.List;
+
 public class Game {
     public void run(){
         System.out.println("숫자 야구 게임을 시작합니다.");
+        Computer computer = new Computer();
+        List<Integer> computerNum = computer.getComputerNum();
     }
 }
