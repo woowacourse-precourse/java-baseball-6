68f2e965c2d091ebfc8ea931c849a7553171da91
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-25 21:32:14 +0900

    [feat] : add constant list

---
+++ src/main/java/baseball/view/OutputInformationView.java
@@ -0,0 +1,9 @@
+package baseball.view;
+
+public class OutputInformationView {
+
+    private static final String start = "숫자 야구 게임을 시작합니다.";
+    private static final String checkAnswer = "3개의 숫자를 모두 맞추셨습니다. ";
+    private static final String gameOver = "게임 종료.";
+    private static final String gameEnd = "게임 실행을 종료합니다.";
+}
