6202661eac340085f820744ddaa908628b67cbfe
Hyoseop Song <crad_on25@naver.com>
2023-10-25 21:46:20 +0900

    refactor(GameStatusValue): 삼진아웃 구현에 따른 변수명 수정

--- src/main/java/baseball/SYSTEM_VALUE/GameStatusValue.java
+++ src/main/java/baseball/SYSTEM_VALUE/GameStatusValue.java
@@ -3,5 +3,5 @@ package baseball.SYSTEM_VALUE;
 public class GameStatusValue {
     public static final int GAME_RESTART = 1;
     public static final int GAME_EXIT = 2;
-    public static final int GAME_CONTINUE = 3;
+    public static final int GAME_FINISH = 3;
 }
