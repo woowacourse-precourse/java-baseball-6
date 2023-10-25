9507d79d94819e28a537b2d683c5bcb9d2647a46
beomsic <qjatjr29@khu.ac.kr>
2023-10-25 13:44:01 +0900

    :recycle: refactor : 기능에 따라 메서드로 나누도록 코드 변경 및 패키지 이동

---
+++ src/main/java/baseball/controller/BaseBallApp.java
@@ -0,0 +1,56 @@
+package baseball.controller;
+
+import static baseball.config.BaseBallGameConfig.BASEBALL_GAME_EXIT;
+import static baseball.config.BaseBallGameConfig.BASEBALL_GAME_RESTART;
+
+import baseball.model.BaseBallNumber;
+import baseball.view.ConsoleInput;
+import baseball.view.ConsoleOutput;
+import baseball.model.BallCount;
+import camp.nextstep.edu.missionutils.Console;
+
+public class BaseBallApp {
+
+    public void run() {
+        ConsoleOutput.gameStart();
+        do {
+            BaseBallNumber computerNumber = BaseBallNumber.generateRandomNumbers();
+            play(computerNumber);
+        } while(isRestart());
+    }
+
+    private void play(BaseBallNumber computerNumber) {
+        while(true) {
+            String inputString = ConsoleInput.askInputNumber();
+            BaseBallNumber inputNumber = BaseBallNumber.generateNumber(inputString);
+            BallCount ballCount = BallCount.calculate(computerNumber, inputNumber);
+            ConsoleOutput.print(ballCount.getResultMessage());
+            if(ballCount.isFinished()) {
+                ConsoleOutput.collect();
+                break;
+            }
+        }
+    }
+
+    private boolean isRestart() {
+        String command = ConsoleInput.askRestart();
+
+        if(!isExitCommand(command) && !isRestartCommand(command)) throw new IllegalArgumentException();
+        if(isExitCommand(command)) {
+            ConsoleOutput.close();
+            Console.close();
+            return false;
+        }
+        return isRestartCommand(command);
+    }
+
+    private boolean isExitCommand(String command) {
+        return command.equals(BASEBALL_GAME_EXIT.toString());
+    }
+
+    private boolean isRestartCommand(String command) {
+        return command.equals(BASEBALL_GAME_RESTART.toString());
+    }
+
+
+}
