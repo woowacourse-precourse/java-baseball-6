2e4d7befc1bbbb16ff67e987be443f97ec428528
HanHyeJi <hjhan.0621@gmail.com>
2023-10-25 21:04:00 +0900

    refactor: Score > public 생성자 삭제

--- src/main/java/baseball/Score.java
+++ src/main/java/baseball/Score.java
@@ -17,11 +17,6 @@ public class Score {
         return score;
     }
 
-    public Score(int numOfBall, int numOfStrike) {
-        this.numOfBall = numOfBall;
-        this.numOfStrike = numOfStrike;
-    }
-
     public void returnScore(Computer computer, User user) {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
