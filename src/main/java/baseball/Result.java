a536c7134b426a9b5beaac4e4e548995c87d561e
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-10-25 11:13:30 +0900

    refactor: 상수 추출, 및 메서드명 수정

--- src/main/java/baseball/Result.java
+++ src/main/java/baseball/Result.java
@@ -1,12 +1,13 @@
 package baseball;
 
-import static baseball.Exception.INVALID_STRIKE_OR_BALL_COUNT;
 
 public record Result(Integer strike, Integer ball) {
+    private static final String INVALID_STRIKE_OR_BALL_COUNT = "스트라이크와 볼은 0보다 작을 수 없습니다.";
+    private static final int ZERO = 0;
 
     public Result {
-        if (strike < 0 || ball < 0) {
-            throw new IllegalArgumentException(INVALID_STRIKE_OR_BALL_COUNT.getMessage());
+        if (strike < ZERO || ball < ZERO) {
+            throw new IllegalArgumentException(INVALID_STRIKE_OR_BALL_COUNT);
         }
     }
 
--- src/main/java/baseball/Viewer.java
+++ src/main/java/baseball/Viewer.java
@@ -1,9 +1,6 @@
 package baseball;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import static baseball.Exception.*;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Viewer {
@@ -11,62 +8,7 @@ public class Viewer {
         return Integer.parseInt(readLine());
     }
 
-    public static String readString() {
+    public static String readNumbers() {
         return readLine();
     }
-    public static boolean readIsFinish() {
-        int input = readNumber();
-        validateFinishGameNumber(input);
-        return input == 2;
-    }
-
-    public static List<Integer> readNumbers() {
-        String inputString = readString();
-        return changeStringToList(inputString);
-    }
-
-    public static List<Integer> changeStringToList(String input) {
-        String[] strings = input.split("");
-        validateInputSize(strings);
-        List<Integer> list = new ArrayList<>();
-
-        for (int i = 0; i < strings.length; i++) {
-            int number = Integer.parseInt(strings[i]);
-            validateNumber(list, number);
-            list.add(number);
-        }
-        return list;
-    }
-
-    private static void validateInputSize(String[] strings) {
-        if (strings.length != 3)
-            throw new IllegalArgumentException(INVALID_SIZE_OF_INPUT.getMessage());
-    }
-
-    private static void validateNumber(final List<Integer> list, final int number) {
-        validateNull(number);
-        validateNumberRange(number);
-        validateDuplication(list, number);
-    }
-
-    private static void validateNull(int number) {
-        if (number == 0)
-            throw new IllegalArgumentException(INVALID_TYPE_OF_INPUT.getMessage());
-    }
-
-    private static void validateNumberRange(final int number) {
-        if (number < 1 || number > 9)
-            throw new IllegalArgumentException(INVALID_RANGE_OF_INPUT.getMessage());
-    }
-
-    private static void validateDuplication(final List<Integer> randomNumberList, final int number) {
-        if (randomNumberList.contains(number))
-            throw new IllegalArgumentException(ALREADY_EXIST_NUMBER.getMessage());
-    }
-
-    private static void validateFinishGameNumber(int input) {
-        if (input != 1 && input != 2)
-            throw new IllegalArgumentException(ONLY_1_OR_2.getMessage());
-    }
-
 }
