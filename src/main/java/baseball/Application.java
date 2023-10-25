f6e93018c075b6b700e9b56268d963d78a6788ae
carpediem365 <k2309123@gmail.com>
2023-10-25 17:18:34 +0900

    사용자 입력 검증함수 & 숫자 비교함수

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -21,16 +21,71 @@ public class Application {
         return computer;
     }
 
-  
+
+    /**
+     * 사용자 입력 검증 함수
+     */
+    void validateInput(String inputNum) {
+        //중복된 숫자 검사
+        for (int i = 0; i < inputNum.length(); i++) {
+            char num1 = inputNum.charAt(i);
+            for (int j = i + 1; j < inputNum.length(); j++) {
+                if (num1 == inputNum.charAt(j)) {
+                    throw new IllegalArgumentException("중복된 수가 존재합니다.");
+                }
+            }
+        }
+        // 3자리 수 검사
+        if (inputNum.length() != 3) {
+            throw new IllegalArgumentException("3자리 수가 아닙니다.");
+        }
+
+        // 숫자를 입력했는지 검사
+        try {
+            int number = Integer.parseInt(inputNum);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자가 아닙니다.");
+        }
+
+    }
+
+    /**
+     * 숫자 비교 함수
+     */
+    int compareNumbers(String inputNum, List<Integer> randomNumber) {
+        int strike = 0;
+        int ball = 0;
+
+        for (int i = 0; i < inputNum.length(); i++) {
+            int userNum = Integer.parseInt(String.valueOf(inputNum.charAt(i)));
+            if (randomNumber.get(i).equals(userNum)) {
+                strike++;
+            } else if (randomNumber.contains(userNum)) {
+                ball++;
+            }
+        }
+
+        // 힌트 출력
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+        } else if (strike == 3) {
+            System.out.println(strike + "스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return strike;
+        } else {
+            System.out.println((ball > 0 ? ball + "볼 " : "") + (strike > 0 ? strike + "스트라이크" : ""));
+        }
+        return 0;
+    }
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다");
         Application game = new Application();
         List<Integer> randomNumber = game.comRandomNumber();
-        //System.out.println(randomNumber);
         System.out.print("숫자를 입력해주세요 : ");
         String inputNum = Console.readLine();
-        System.out.println(inputNum);
 
     }
 }
