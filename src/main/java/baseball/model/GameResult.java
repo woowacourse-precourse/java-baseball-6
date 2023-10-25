5ff0938ae22f4767e8baa3c2673ef30f4d38ac76
eora21 <eora21@naver.com>
2023-10-25 20:35:33 +0900

    refactor: 💡 추가 존재할 수 없는 경우의 수(볼 하나, 나머지 다 스트라이크) 검증 및 예외 메시지 작성
    볼이 하나만 나온 경우, 나머지가 다 스트라이크일 수는 없다. 나머지가 스트라이크라면 유효 숫자 자리는 하나이다. 이 한 자리를
    맞추면 스트라이크가 된다. 그러나 볼은 값은 맞췄으나 자리가 틀릴 경우이다. 중복된 값을 포함하지 않는 한 나올 수 없는 경우가
    된다.

--- src/main/java/baseball/model/GameResult.java
+++ src/main/java/baseball/model/GameResult.java
@@ -2,9 +2,18 @@ package baseball.model;
 
 public record GameResult(int tryCount, int correctAnswerCount, int similarAnswerCount) {
     public GameResult {
-        if (tryCount < correctAnswerCount + similarAnswerCount) {
-            throw new IllegalArgumentException();
+        if (isOverTryCount(tryCount, correctAnswerCount, similarAnswerCount)
+                || isOneSimilarAllOthersCorrect(tryCount, correctAnswerCount, similarAnswerCount)) {
+            throw new IllegalArgumentException("존재할 수 없는 게임 결과입니다.");
         }
+    }
+
+    private boolean isOverTryCount(int tryCount, int correctAnswerCount, int similarAnswerCount) {
+        return tryCount < correctAnswerCount + similarAnswerCount;
+    }
+
+    private boolean isOneSimilarAllOthersCorrect(int tryCount, int correctAnswerCount, int similarAnswerCount) {
+        return tryCount == correctAnswerCount + similarAnswerCount && similarAnswerCount == 1;
     }
 
     public boolean isPerfectGame() {
