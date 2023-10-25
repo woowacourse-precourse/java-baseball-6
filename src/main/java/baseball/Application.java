87d09b0c0109d81fa3d941cde5ce08447f353387
ljh <dusk5588@naver.com>
2023-10-25 22:46:57 +0900

    [Fix] try-catch 구문 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,15 +5,9 @@ import baseball.Controller.GameController;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        try {
-            GameController gameController = new GameController();
-            gameController.gameStart();
-        }
-        catch (IllegalArgumentException e){
-            System.out.println(e.getMessage());
-        }
-        catch (Exception e){
-            System.out.println("예상하지 못한 예외입니다.");
-        }
+
+        GameController gameController = new GameController();
+        gameController.gameStart();
+
     }
 }
