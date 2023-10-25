90fc16ad17cd6f73be1d83b709ad680881ed90f1
Minyongggg <naminyong97@gmail.com>
2023-10-25 22:16:12 +0900

    feat: 플레이어 숫자 값 객체

---
+++ src/main/java/baseball/model/PlayerNumber.java
@@ -0,0 +1,14 @@
+package baseball.model;
+
+public class PlayerNumber {
+
+    private final String value;
+
+    public PlayerNumber(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
