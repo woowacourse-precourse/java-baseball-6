fd5dfa00b5d540ddba5cfe085cba95196ab38720
hyo-limilimee <1001lily@naver.com>
2023-10-25 18:36:56 +0900

    refactor: maximize method length - BaseballGame.java

--- src/main/java/baseball/Model/BaseballGame.java
+++ src/main/java/baseball/Model/BaseballGame.java
@@ -9,37 +9,55 @@ import java.util.List;
 
 public class BaseballGame {
     private List<Integer> computerNumbers;
+    private static int INPUTLENGTH = 3;
+    private static int MIN_RANGE = 1;
+    private static int MAX_RANGE = 9;
 
     public void initializeGame() {
         computerNumbers = generateRandomNumbers();
     }
 
     public boolean play(InputView inputView, OutputView outputView) {
-        while (true) {
+        boolean isGameWon = false;
+        while (!isGameWon) {
             List<Integer> playerNumbers = inputView.readNumbers();
-            int strikes = 0;
-            int balls = 0;
-
-            for (int i = 0; i < 3; i++) {
-                if (computerNumbers.get(i).equals(playerNumbers.get(i))) {
-                    strikes++;
-                } else if (playerNumbers.contains(computerNumbers.get(i))) {
-                    balls++;
-                }
-            }
-
+            int strikes = countStrikes(playerNumbers);
+            int balls = countBalls(playerNumbers);
             outputView.displayGameResult(balls, strikes);
-
-            if (strikes == 3) {
-                return true;
+            if (strikes == INPUTLENGTH) {
+                isGameWon = true;
             }
         }
+        return isGameWon;
+    }
+
+    private int countStrikes(List<Integer> playerNumbers) {
+        int strikes = 0;
+        for (int i = 0; i < INPUTLENGTH; i++) {
+            if (computerNumbers.get(i).equals(playerNumbers.get(i))) {
+                strikes++;
+            }
+        }
+
+        return strikes;
+    }
+
+    private int countBalls(List<Integer> playerNumbers) {
+        int balls = 0;
+
+        for (int i = 0; i < INPUTLENGTH; i++) {
+            if (!computerNumbers.get(i).equals(playerNumbers.get(i)) && playerNumbers.contains(
+                    computerNumbers.get(i))) {
+                balls++;
+            }
+        }
+        return balls;
     }
 
     private List<Integer> generateRandomNumbers() {
         List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+        while (computer.size() < INPUTLENGTH) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_RANGE, MAX_RANGE);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
