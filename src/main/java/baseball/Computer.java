4e12cd9664e5727132685d08cf3fb4ff5c4a037d
seona-moon <fhflwhwl5@gmail.com>
2023-10-25 14:10:43 +0900

    refactor: 원시값 포장

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
+  private final static int NUMBER = 3;
+  private final static int MIN_NUMBER = 1;
+  private final static int MAX_NUMBER = 9;
 
   private List<Integer> ComputerNum = new ArrayList<>();
 
@@ -13,8 +16,8 @@ public class Computer {
   }
 
   public void GenerateRandomComputerNum() {
-    while (ComputerNum.size() < 3) {
-      int randomNumber = Randoms.pickNumberInRange(1, 9);
+    while (ComputerNum.size() < NUMBER) {
+      int randomNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
       if (!ComputerNum.contains(randomNumber)) {
         ComputerNum.add(randomNumber);
       }
