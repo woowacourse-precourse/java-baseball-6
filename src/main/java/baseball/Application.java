4feba420ba47433978d5d4fc3381d4d02780f2c2
ae-GYEONGWON <tjsxor50@gmail.com>
2023-10-25 18:49:32 +0900

    comment: 주석 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,6 +8,7 @@ public class Application {
         do {
             System.out.println("숫자 야구 게임을 시작합니다.");
             Computer computer = new Computer();
+            // 컴퓨터 숫자 생성
             List<Integer> generatedNumbers = Computer.getComputerNumbers();
             StrikeorBall checker = new StrikeorBall();
 
@@ -15,21 +16,24 @@ public class Application {
             StrikeorBall.Result result;
 
             do {
+                // 플레이어에게 숫자를 입력받기
                 List<Integer> playerNumbers = player.generateNumbers();
                 if (playerNumbers == null) {
                     return;
                 }
+                // 플레이어와 컴퓨터의 숫자를 비교
                 result = checker.check(generatedNumbers, playerNumbers);
                 System.out.println(result);
-            } while (result.getStrikes() < 3);
+            } while (result.getStrikes() < 3); // 3스트라이크이면 반복문 종료
 
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 
-        } while (isRestart());
+        } while (isRestart()); // 재시작할지 묻는 함수.
 
         System.out.println("게임 종료. 감사합니다!");
     }
 
+    // 게임을 재시작할지 묻고 재시작 or 종료하는 함수.
     private static boolean isRestart() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String input = Console.readLine();
