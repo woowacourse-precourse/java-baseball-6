c5c044b5d1fc4f922bf75afc10e85855cab94de5
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-25 21:24:08 +0900

    [feat] : add game manager class and check method

---
+++ src/main/java/baseball/domain/GameManager.java
@@ -0,0 +1,14 @@
+package baseball.domain;
+
+public class GameManager {
+
+    private static final int LENGTH = 3;
+    private int strikeCount;
+    private int ballCount;
+
+    // Computer 가 선택한 임의의 3자리 숫자와 Player 가 입력한 3자리 숫자를 비교하는 메서드.
+    public void check(Computer computer, Player player) {
+//        strikeCount = strikeCheck(computer, player);
+//        ballCount = ballCheck(computer, player);
+    }
+}
