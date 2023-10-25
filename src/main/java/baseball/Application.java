a5728207b68eca7e15e38a26ec9e61495509cd66
ohjinseo <ojs835@naver.com>
2023-10-25 23:14:11 +0900

    feat : 게임 재시작 여부 확인 로직 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -59,6 +59,15 @@ public class Application {
         System.out.println();
     }
 
+    private static boolean checkRestart() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        String input = Console.readLine();
+        return "1".equals(input);
+    }
+
+
     public static void main(String[] args) {
 
     }
