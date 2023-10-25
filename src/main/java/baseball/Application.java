1904269e7b0ab46f3ef0e6c2a82ea6d2b9f2fd36
yangyanghyunjung <hyunjungyang0922@gmail.com>
2023-10-25 21:31:43 +0900

    🎯feat(Application: main 함수구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import controller.BaseballGame;
+import model.RandomNumber;
+import view.InputView;
+import view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseballGame baseballGame = new BaseballGame(new InputView(), new OutputView(), new RandomNumber());
+        baseballGame.start();
     }
 }
