89c0d534e4c6f4adfaace0e3997f8972b028d56e
doyoungkim97 <eh4536@naver.com>
2023-10-25 19:43:40 +0900

    fix : 출력 뷰 수정

--- src/main/java/baseball/View/OutputView.java
+++ src/main/java/baseball/View/OutputView.java
@@ -2,7 +2,7 @@ package baseball.View;
 
 public class OutputView {
     public static String startMessage = "숫자 야구 게임을 시작합니다.";
-    public static String inputMessage = "숫자를 입력해주세요 : ";
+    public static String inputMessage = "\n숫자를 입력해주세요 : ";
     public static String ballMessage = "볼";
     public static String strikeMessage = "스트라이크";
     public static String nashingMesaage = "낫싱";
@@ -10,7 +10,7 @@ public class OutputView {
     public static String RetryMessage = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요";
 
     public static void printStart(){
-        System.out.println(startMessage);
+        System.out.print(startMessage);
     }
 
     public static void printEnd(){
