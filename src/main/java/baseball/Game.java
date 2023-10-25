5481e554863a674bf8a52c9055f30705c1a4ac7f
swhyeon98 <smreo75@gmail.com>
2023-10-25 21:58:15 +0900

    feat: 게임 종료 조건을 만족시키기 전까지 반복을 위한 'loop' 메서드 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -13,10 +13,20 @@ public class Game {
     public void start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
         init();
+        loop();
     }
 
     private void init() {
         computer.generateRandomNumbers();
         player.setNumbers(new InputHandler().getPlayInput());
+    }
+
+    private void loop() {
+        while (true) {
+            if (handleGameOver()){
+                return;
+            }
+            player.setNumbers(new InputHandler().getPlayInput());
+        }
     }
 }
