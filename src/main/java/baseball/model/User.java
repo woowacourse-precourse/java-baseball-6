2d02b0c761f20fc40fde3fd2c7554227ff926a68
www-author <dream970215@naver.com>
2023-10-25 21:34:01 +0900

    :bug: fix: 사용자 입력 중복 방지 메서드 추가

--- src/main/java/baseball/model/User.java
+++ src/main/java/baseball/model/User.java
@@ -1,5 +1,8 @@
 package baseball.model;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class User {
 
     private String userNumber;
@@ -15,9 +18,25 @@ public class User {
         String regex = "[1-9]+";
         boolean isInvalidDigit = !userInput.matches(regex);
 
-        if (isInvalidDigit || userInput.length() > 3) {
+        if (isInvalidDigit || userInput.length() > 3 || isDuplicationDigit(userInput)) {
             throw new IllegalArgumentException();
         }
+
+
+
     }
 
+    public boolean isDuplicationDigit(String userInput) {
+        HashSet<Character> userInputDigit = new HashSet<>();
+
+        for (char c : userInput.toCharArray()) {
+            if (userInputDigit.contains(c)){
+                return true;
+            }
+            userInputDigit.add(c);
+        }
+
+        return false;
+
+    }
 }
