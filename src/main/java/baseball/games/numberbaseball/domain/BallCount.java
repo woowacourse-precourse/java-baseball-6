1b8e50c2c3fe68c1b0431b76bfc80bf798fcfdec
haebyun <bhk4392@gmail.com>
2023-10-25 19:18:36 +0900

    feat(BallCount): 생성자를 통해 strike와 ball을 불변의 형태로 넣어주며 객체를 생성 하도록 BallCount 객체 구현

---
+++ src/main/java/baseball/games/numberbaseball/domain/BallCount.java
@@ -0,0 +1,11 @@
+package baseball.games.numberbaseball.domain;
+
+public class BallCount {
+    private final int ballCount;
+    private final int strikeCount;
+
+    public BallCount(int ballCount, int strikeCount) {
+        this.ballCount = ballCount;
+        this.strikeCount = strikeCount;
+    }
+}
