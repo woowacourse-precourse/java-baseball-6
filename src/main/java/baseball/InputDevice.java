40c03aaa4c674d9db74d660ef8f04dfa8fd803ed
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-10-25 16:12:24 +0900

    feat : InputDevice 출력 멘트 추가

--- src/main/java/baseball/InputDevice.java
+++ src/main/java/baseball/InputDevice.java
@@ -8,6 +8,7 @@ public class InputDevice {
 
     private static final String INPUT_NUMBER_MENT = "숫자를 입력해주세요 : ";
     private static final String NO_SPACE = "";
+    public static final String INPUT_RESTART_OR_EXIT_MENT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private final ConsoleService consoleService;
     private final InputValidator inputValidator;
 
@@ -24,6 +25,7 @@ public class InputDevice {
     }
 
     public int restartOrExit() {
+        System.out.println(INPUT_RESTART_OR_EXIT_MENT);
         String inputValue = consoleService.readLine();
         inputValidator.validRestartOrExit(inputValue);
         return Integer.parseInt(inputValue);
