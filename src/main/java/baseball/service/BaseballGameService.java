f816e24059e47e639004b571572d3dcf3f020c6b
dongsuu <suudh99@naver.com>
2023-10-25 23:48:31 +0900

    refactor: 상수, 메소드 분리

--- src/main/java/baseball/service/BaseballGameService.java
+++ src/main/java/baseball/service/BaseballGameService.java
@@ -8,49 +8,62 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class BaseballGameService {
+    private static final String EXIT_NUMBER = "2";
+    private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public void start(){
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println(START_MESSAGE);
         List<Integer> numbers = Computer.generateRandomThreeValue();
         User user = new User();
         BaseballGame baseballGame = new BaseballGame(numbers, user);
         playGame(baseballGame);
-
-
     }
 
     public void playGame(BaseballGame baseballGame){
         User user = baseballGame.getUser();
-
         List<Integer> targetNumbers = baseballGame.getTargetNumber();
 
         while(true){
-            System.out.print("숫자를 입력해주세요 : ");
+            System.out.print(INPUT_MESSAGE);
             String input = Console.readLine();
 
-            if(input.isEmpty() || input.length() > 3){
-                throw new IllegalArgumentException();
-            }
+            checkInputValidation(input);
 
-            for(int i=0; i<input.length(); i++){
-                int num = input.charAt(i) - '0';
-                user.addNumber(num);
-            }
+            makeUserNumbers(input, user);
 
             if(compareTwoNumbers(targetNumbers, user.getNumbers())){
                 finish();
                 String endInput = User.input();
-                if(endInput.equals("2")){
+                if(isEndBaseballGame(endInput)){
                     break;
                 }
+
                 targetNumbers.clear();
                 targetNumbers = Computer.generateRandomThreeValue();
             }
 
             user.getNumbers().clear();
         }
+    }
 
+    private boolean isEndBaseballGame(String endInput) {
+        return endInput.equals(EXIT_NUMBER);
+    }
 
+    private static void makeUserNumbers(String input, User user) {
+        for(int i = 0; i< input.length(); i++){
+            int num = input.charAt(i) - '0';
+            user.addNumber(num);
+        }
+    }
+
+    private static void checkInputValidation(String input) {
+        if(input.isEmpty() || input.length() > 3){
+            throw new IllegalArgumentException();
+        }
     }
 
     public boolean compareTwoNumbers(List<Integer> targetNumbers, List<Integer> userNumbers){
@@ -60,28 +73,34 @@ public class BaseballGameService {
 
         for(int i=0; i<targetNumbers.size(); i++){
             for(int j=0; j<userNumbers.size(); j++){
-                if(targetNumbers.get(i) == userNumbers.get(j) && i == j){
+                if(isStrike(targetNumbers, userNumbers, i, j)){
                     strike++;
                     break;
-                } else if(targetNumbers.get(i) == userNumbers.get(j) && i != j){
+                } else if(isBall(targetNumbers, userNumbers, i, j)){
                     ball++;
                     break;
                 }
             }
         }
 
-        if(strike + ball == 0){
+        if(isNothing(strike, ball)){
             isNothing = true;
         }
         printResult(isNothing, strike, ball);
 
-        if(strike == 3){
-            return true;
-        }
+        return strike == 3;
+    }
 
-        return false;
+    private static boolean isNothing(int strike, int ball) {
+        return strike + ball == 0;
+    }
 
+    private static boolean isBall(List<Integer> targetNumbers, List<Integer> userNumbers, int i, int j) {
+        return targetNumbers.get(i).equals(userNumbers.get(j)) && i != j;
+    }
 
+    private static boolean isStrike(List<Integer> targetNumbers, List<Integer> userNumbers, int i, int j) {
+        return targetNumbers.get(i).equals(userNumbers.get(j)) && i == j;
     }
 
     public void printResult(boolean isNothing, int strike, int ball){
@@ -100,8 +119,8 @@ public class BaseballGameService {
     }
 
     public void finish(){
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println(END_MESSAGE);
+        System.out.println(RESTART_MESSAGE);
     }
 
 }
