9b6ae851fcad89ffbf46e86828d4e611c07a7c5b
leedoyeon849 <do83430208@gmail.com>
2023-10-25 19:28:58 +0900

    refactor: Score 인터페이스 추가, 기존 Score.java > BaseballScore.java로 변경

--- src/main/java/baseball/utility/Score.java
+++ src/main/java/baseball/utility/Score.java
@@ -1,45 +1,6 @@
 package baseball.utility;
 
-public class Score {
-    private Integer strike;
-    private Integer ball;
-
-    public Score() {
-        this.strike = 0;
-        this.ball = 0;
-    }
-
-    public Integer getStrike() {
-        return strike;
-    }
-
-    public void setStrike(Integer strike) {
-        this.strike = strike;
-    }
-
-    public void plusStrike() {
-        this.strike += 1;
-    }
-
-    public void plusBall() {
-        this.ball += 1;
-    }
-
-    public String explainScore() {
-
-        if (ball == 0 && strike == 0) {
-            return "낫싱";
-        }
-
-        String result = "";
-        if (ball != 0) {
-            result += ball + "볼 ";
-        }
-        if (strike != 0) {
-            result += strike + "스트라이크";
-        }
-
-        return result.trim();
-    }
+public interface Score {
+    String explainScore();
 
 }
