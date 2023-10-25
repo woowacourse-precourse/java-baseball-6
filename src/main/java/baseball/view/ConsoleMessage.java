f143a07d651a051128596f86431951ff664ca898
beomsic <qjatjr29@khu.ac.kr>
2023-10-25 13:42:36 +0900

    :recycle: refactor : BaseBallConfig의 값을 이용하도록 변경

--- src/main/java/baseball/view/ConsoleMessage.java
+++ src/main/java/baseball/view/ConsoleMessage.java
@@ -1,13 +1,12 @@
 package baseball.view;
 
+import static baseball.config.BaseBallGameConfig.BASEBALL_NUMBER_SIZE;
+
 public enum ConsoleMessage {
 
     GAME_START("숫자 야구 게임을 시작합니다. \n"),
     ENTER_NUMBER("숫자를 입력해주세요 : "),
-    STRIKE("스트라이크 "),
-    BALL("볼 "),
-    NOTHING("낫싱"),
-    CORRECT_ANSWER("개의 숫자를 모두 맞히셨습니다! 게임 종료\n"),
+    CORRECT_ANSWER(String.format("%d개의 숫자를 모두 맞히셨습니다! 게임 종료%n", BASEBALL_NUMBER_SIZE.getValue())),
     RESTART("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
 
     private String message;
