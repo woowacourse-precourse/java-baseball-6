4140a5bc09abca2d380b6939d19938631976087e
davemins <davemins4125@naver.com>
2023-10-25 19:08:29 +0900

    refactor(InputValidator) : 가독성을 위해 상수 추가 후 코드 수정

--- src/main/java/baseball/InputValidator.java
+++ src/main/java/baseball/InputValidator.java
@@ -1,6 +1,11 @@
 package baseball;
-
 public class InputValidator {
+
+    private static final int REQUIRED_NUMBER_LENGTH = 3;
+    private static final String REQUIRED_NUMBER_MESSAGE = "세 자리 숫자를 입력하세요.";
+    private static final String NUMERIC_INPUT_MESSAGE = "숫자만 입력하세요.";
+    private static final String DISTINCT_DIGITS_MESSAGE = "세 자리 서로 다른 숫자를 입력하세요.";
+    private static final String INVALID_ANSWER_MESSAGE = "잘못된 답을 하셨습니다.";
 
     public static void isValidUserNumberInput(String input) {
         checkLength(input);
@@ -13,26 +18,26 @@ public class InputValidator {
     }
 
     private static void checkLength(String input) {
-        if (input.length() != 3) {
-            throw new IllegalArgumentException("세 자리 숫자를 입력하세요.");
+        if (input.length() != REQUIRED_NUMBER_LENGTH) {
+            throw new IllegalArgumentException(REQUIRED_NUMBER_MESSAGE);
         }
     }
 
     private static void checkNumeric(String input) {
         if (!input.matches("\\d+")) {
-            throw new IllegalArgumentException("숫자만 입력하세요.");
+            throw new IllegalArgumentException(NUMERIC_INPUT_MESSAGE);
         }
     }
 
     private static void checkDistinctDigits(String input) {
-        if (input.chars().distinct().count() != 3) {
-            throw new IllegalArgumentException("세 자리 서로 다른 숫자를 입력하세요.");
+        if (input.chars().distinct().count() != REQUIRED_NUMBER_LENGTH) {
+            throw new IllegalArgumentException(DISTINCT_DIGITS_MESSAGE);
         }
     }
 
     private static void checkZeroOrOne(String input) {
         if (!(input.equals("1") || input.equals("2"))) {
-            throw new IllegalArgumentException("잘못된 답을 하셨습니다.");
+            throw new IllegalArgumentException(INVALID_ANSWER_MESSAGE);
         }
     }
 }
