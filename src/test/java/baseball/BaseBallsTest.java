074ae46a336589c5cae1a7eed9e2d7ef1abca668
K-KY <kyuyoungk@naver.com>
2023-10-25 15:01:24 +0900

    Test(BaseBallTest) : UserBalls 객체를 사용하도록 변경

--- src/test/java/baseball/BaseBallsTest.java
+++ src/test/java/baseball/BaseBallsTest.java
@@ -7,6 +7,7 @@ import baseball.domain.BaseBalls;
 import baseball.domain.CompareNumber;
 import baseball.domain.DataTypeChanger;
 import baseball.domain.GameResult;
+import baseball.domain.UserNumber;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -22,7 +23,8 @@ public class BaseBallsTest {
     @Test
     void baseBall_1b_1s() {
         List<CompareNumber> userBalls = DataTypeChanger.compareNumberFormat(Arrays.asList(3, 2, 4));
-        GameResult compare = baseBalls.compare(userBalls);
+        UserNumber userNumber = new UserNumber(userBalls);
+        GameResult compare = baseBalls.compare(userNumber);
 
         assertThat(compare.getResult(BallScore.STRIKE)).isEqualTo(1);
         assertThat(compare.getResult(BallScore.BALL)).isEqualTo(1);
@@ -30,7 +32,8 @@ public class BaseBallsTest {
     @Test
     void baseBall_3b() {
         List<CompareNumber> userBalls = DataTypeChanger.compareNumberFormat(Arrays.asList(3, 1, 2));
-        GameResult compare = baseBalls.compare(userBalls);
+        UserNumber userNumber = new UserNumber(userBalls);
+        GameResult compare = baseBalls.compare(userNumber);
 
         assertThat(compare.getResult(BallScore.STRIKE)).isEqualTo(0);
         assertThat(compare.getResult(BallScore.BALL)).isEqualTo(3);
@@ -38,7 +41,8 @@ public class BaseBallsTest {
     @Test
     void baseBall_3s() {
         List<CompareNumber> userBalls = DataTypeChanger.compareNumberFormat(Arrays.asList(1, 2, 3));
-        GameResult compare = baseBalls.compare(userBalls);
+        UserNumber userNumber = new UserNumber(userBalls);
+        GameResult compare = baseBalls.compare(userNumber);
 
         assertThat(compare.getResult(BallScore.STRIKE)).isEqualTo(3);
         assertThat(compare.getResult(BallScore.BALL)).isEqualTo(0);
@@ -47,7 +51,8 @@ public class BaseBallsTest {
     @Test
     void baseBall_3n() {
         List<CompareNumber> userBalls = DataTypeChanger.compareNumberFormat(Arrays.asList(4, 5, 6));
-        GameResult compare = baseBalls.compare(userBalls);
+        UserNumber userNumber = new UserNumber(userBalls);
+        GameResult compare = baseBalls.compare(userNumber);
 
         assertThat(compare.getResult(BallScore.STRIKE)).isEqualTo(0);
         assertThat(compare.getResult(BallScore.BALL)).isEqualTo(0);
