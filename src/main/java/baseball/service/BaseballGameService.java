4d75320f6625416a31bafafda8503dc9597c7dbd
jjongwon7 <jjongwon7@naver.com>
2023-10-25 21:06:27 +0900

    feat: 게임을 위한 컴퓨터 값 세팅 기능 추가 상대방 역할을 수행할 컴퓨터의 랜덤한 서로 다른 3가지 수 생성

--- src/main/java/baseball/service/BaseballGameService.java
+++ src/main/java/baseball/service/BaseballGameService.java
@@ -1,8 +1,28 @@
 package baseball.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BaseballGameService {
+    public static final int ANSWER_LENGTH = 3;
+
     public void playBaseballGame() {
         // 1. 게임 시작 => 게임 시작 시 사용자에게 문구 출력
         System.out.println("숫자 야구 게임을 시작합니다.");
+
+        // 2. 게임을 위한 값 세팅 => 상대방 역할을 수행할 컴퓨터의 랜덤한 서로 다른 3가지 수 생성
+        List<Integer> computerAnswers = createComputerValue();
+    }
+
+    private List<Integer> createComputerValue() {
+        List<Integer> computerAnswers = new ArrayList<>();
+        while (computerAnswers.size() < ANSWER_LENGTH) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerAnswers.contains(randomNumber)) {
+                computerAnswers.add(randomNumber);
+            }
+        }
+        return computerAnswers;
     }
 }
