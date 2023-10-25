f2331008b021fdc1d91b92adbe4b258c82296540
bokyung <bbookng@gmail.com>
2023-10-25 10:34:27 +0900

    [Feat] Result Class 구조 완성

--- src/main/java/baseball/model/Result.java
+++ src/main/java/baseball/model/Result.java
@@ -1,4 +1,13 @@
 package baseball.model;
 
 public class Result {
+
+    private int strikeCount;
+    private int ballCount;
+    private String message;
+
+    public Result(int strikeCount, int ballCount) {
+
+    }
+
 }
