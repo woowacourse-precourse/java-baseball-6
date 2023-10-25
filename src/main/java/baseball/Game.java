72b1e41dd6891704a151e572afb03f989304050a
Yoo Hyunseok <brianyhs@naver.com>
2023-10-25 17:11:05 +0900

    refactor: Game 내의 로직을 덜게끔 변경

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -2,29 +2,29 @@ package baseball;
 
 import baseball.player.Opponent;
 import baseball.player.User;
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.Collections;
 import java.util.List;
 
 public class Game {
 
-    private Opponent opponent;
-    private User user;
-    private Score score;
+    private final Opponent opponent;
+    private final User user;
+    private final Score score;
     InputValidator inputValidator;
     StringPrinter stringPrinter;
     InputReader inputReader;
+    NumbersManager numbersManager;
 
-    public Game(Opponent opponent, User user, Score score) {
-        this.opponent = opponent;
-        this.user = user;
-        this.score = score;
+    public Game() {
+        opponent = new Opponent();
+        user = new User();
+        score = new Score();
         inputValidator = InputValidator.getInstance();
         stringPrinter = StringPrinter.getInstance();
         inputReader = InputReader.getInstance();
+        numbersManager = NumbersManager.getInstance();
     }
 
-    public void newGame() throws IllegalArgumentException{
+    public void newGame(){
         while(true){
             // empty each lists instead of creating new object on new game
             user.emptyList();
@@ -33,19 +33,17 @@ public class Game {
 
             // below process start upon user's win
             stringPrinter.printEndOfGame();
-            int endInput = inputReader.getInput();
+            int endInput = inputReader.getFinishedGameInput();
 
             // end loop on input number 2
             if (endInput == 2) {
                 break;
             }
-            // wrong input exception
-            inputValidator.validateEndInput(endInput);
         }
     }
 
     private void play() {
-        while (true) {
+        do {
             setUserNumbers();
             setOpponentNumbers();
             score.setScore(user, opponent);
@@ -54,50 +52,26 @@ public class Game {
             int strikeCounter = score.getStrikeCounter();
 
             stringPrinter.printGuessResult(ballCounter, strikeCounter);
-
-            if (isOver()) {
-                break;
-            }
-        }
+        } while (!isOver());
     }
 
     // set opponent's numbers
     private void setOpponentNumbers(){
         List<Integer> numbers = opponent.getNumbers();
-        while (numbers.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            // avoid duplicates
-            if (!numbers.contains(randomNumber)) {
-                numbers.add(randomNumber);
-            }
-        }
-        opponent.setNumbers(numbers);
+        opponent.setNumbers(numbersManager.setOpponentNumbers(numbers));
     }
 
-    // set user's numberList
+    // set user's numbers
     private void setUserNumbers(){
         List<Integer> numbers = user.getNumbers();
         stringPrinter.printPlayerInput();
-        int inputNumber = inputReader.getInput();
-
-        // validate 3-digit
-        inputValidator.validateThreeDigit(inputNumber);
-
+        int inputNumber = inputReader.getGuessInput();
         numbers.clear();
-        while (inputNumber > 0) {
-            int extractedNumber = inputNumber % 10; // extract number from 1's up to 100's
-            numbers.add(extractedNumber);
-            inputNumber /= 10; // remove last digit
-        }
-
-        // reverse arrange list for correct order (above logic adds 1s digit first)
-        Collections.reverse(numbers);
-        user.setNumbers(numbers);
+        user.setNumbers(numbersManager.setUserNumbers(numbers, inputNumber));
     }
 
     private boolean isOver(){
         int strikeCounter = score.getStrikeCounter();
         return strikeCounter == 3;
     }
-
 }
