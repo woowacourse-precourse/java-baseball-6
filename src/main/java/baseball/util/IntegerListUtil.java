6027bdb782fafa3360d0cb37a398510036dd91a6
inpink <inpink@kakao.com>
2023-10-25 21:57:41 +0900

    refactor: validateListType에서  Object가 Integer형인지 판단하는 기능을 분리
    각각이 Integer인지 검증하는 기능은 IntegerValidator의 역할이라고 판단.
    indent depth 2에서 1로 줄임.

--- src/main/java/baseball/util/IntegerListUtil.java
+++ src/main/java/baseball/util/IntegerListUtil.java
@@ -1,16 +1,15 @@
 package baseball.util;
 
+import baseball.validation.IntegerValidator;
 import java.util.ArrayList;
 import java.util.List;
 
 public class IntegerListUtil {
 
-    private static final String INVALID_INTEGER_LIST_ELEMENT_MESSAGE = "List의 요소가 Integer가 아닙니다.";
-
-    public static List<Integer> parseIntegerList(String string){
+    public static List<Integer> parseIntegerList(String string) {
         List<Integer> integerList = new ArrayList<>();
 
-        for (char character : string.toCharArray()){
+        for (char character : string.toCharArray()) {
             integerList.add(IntegerUtil.parseInteger(character));
         }
 
@@ -25,12 +24,7 @@ public class IntegerListUtil {
 
     public static void validateListType(List<?> list) {
         for (Object item : list) {
-            if (!(item instanceof Integer)) {
-                ExceptionUtil.throwInvalidValueException(INVALID_INTEGER_LIST_ELEMENT_MESSAGE);
-            }
+            IntegerValidator.validateInteger(item);
         }
     }
-
-
-
 }
--- src/main/java/baseball/validation/IntegerValidator.java
+++ src/main/java/baseball/validation/IntegerValidator.java
@@ -6,9 +6,16 @@ import baseball.util.IntegerUtil;
 public class IntegerValidator {
     private static final String INVALID_INTEGER_MESSAGE = "이 값은 정수로 변환할 수 없습니다.";
 
+    public static void validateInteger(Object object) {
+        if (!(object instanceof Integer)) {
+            ExceptionUtil.throwInvalidValueException(INVALID_INTEGER_MESSAGE);
+        }
+    }
+
     public static void validateInteger(String string) {
         if (!IntegerUtil.isInteger(string)) {
             ExceptionUtil.throwInvalidValueException(INVALID_INTEGER_MESSAGE);
         }
     }
+
 }
