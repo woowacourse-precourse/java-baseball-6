1a096cb90fbc7d872ec1e5def07ea9ef2a0fbfc9
unknown <damwon621@gmail.com>
2023-10-25 23:19:15 +0900

    filling Contents

--- src/main/java/baseball/NumberBallGame.java
+++ src/main/java/baseball/NumberBallGame.java
@@ -1,13 +1,13 @@
+// TODO [Control]
 package basball;
 
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
-import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+import baseball.Printer.View;
 
 public class NumberBallGame{
+    private String thing;
+    public static final String RESTART = "1";
+    public static final String EXIT = "2";
 
     public NumberBallGame(){
 
@@ -17,10 +17,20 @@ public class NumberBallGame{
         do{
             start();
 
-        }
+        }while(Replay());
     }
 
     public void start(){
+        public static void main(String[] args){
+            System.out.println("숫자 야구 게임을 시작합니다.");
+
+            System.out.println("숫자를 입력해 주세요 : ");
+            String guessData = readLine();
+            //숫자 체크받기
+
+        }
+
+        //숫자 만들기 호출
 
     }
 
