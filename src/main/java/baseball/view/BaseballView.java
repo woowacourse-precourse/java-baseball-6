c139f549610a59bac3766293d1b86df8216de053
fly-like-you <qkrwnsgh71w@gmail.com>
2023-10-25 19:00:47 +0900

    fix: "수정 뷰에서 사용되는 변수 명과 텍스트"

--- src/main/java/baseball/view/BaseballView.java
+++ src/main/java/baseball/view/BaseballView.java
@@ -4,18 +4,19 @@ import baseball.domain.BallAndStrike;
 
 public class BaseballView {
     // TODO: 야구 게임 화면 출력
-    private final static String gameStart = "숫자 야구 게임을 시작합니다.";
-    private final static String gameEnd = "게임 종료";
+    private final static String GAME_START = "숫자 야구 게임을 시작합니다.";
+    private final static String GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private final static String USER_INPUT = "숫자를 입력해주세요 : ";
     private final static String GAME_EXIT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
 
+
     public static void printGameStart(){
-        printView(gameStart);
+        printView(GAME_START);
     }
 
     public static void printGameEnd(){
-        printView(gameEnd);
+        printView(GAME_END);
     }
 
     public static void requestUserInput(){
@@ -33,4 +34,6 @@ public class BaseballView {
     private static void printView(String view){
         System.out.println(view);
     }
+
+
 }
