9df58573752de13d40fc3d68d66b18676ef0017a
uhyunglee <uhyunglee@naver.com>
2023-10-25 22:02:29 +0900

    [FEAT] : Count Same Number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,14 +1,20 @@
 package baseball;
 
+import baseball.domain.JudgeNumber;
 import baseball.domain.NumberGenerator;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        NumberGenerator generator = new NumberGenerator();
-        List<Integer> numbers = generator.createRandomNumber();
-        System.out.println(numbers);
+//        NumberGenerator generator = new NumberGenerator();
+//        List<Integer> numbers = generator.createRandomNumber();
+//        System.out.println(numbers);
+
+        JudgeNumber judgeNumber = new JudgeNumber();
+        int count = judgeNumber.count(Arrays.asList(1,2,4), Arrays.asList(1,2,3));
+        System.out.println(count);
     }
 }
--- src/main/java/baseball/domain/JudgeNumber.java
+++ src/main/java/baseball/domain/JudgeNumber.java
@@ -3,10 +3,17 @@ package baseball.domain;
 import java.util.List;
 
 public class JudgeNumber {
-    public int count(List<Integer> computer, List<Integer> player){
-        return 0;
+    public int count(List<Integer> computer, List<Integer> player) {
+        int result = 0;
+        for (int i = 0; i < computer.size(); i++){
+            if(player.contains(computer.get(i))){
+                result++;
+            }
+        }
+        return result;
     }
-    public boolean isNumber(){
+
+    public boolean isNumber() {
         return false;
     }
 }
