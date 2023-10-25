bff67b40dcc9b2f5cdf1a7efe565560a02d874b4
Kim Jeong Hyeon <kr100475@gmail.com>
2023-10-25 22:33:48 +0900

    [feat] main method Update

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,21 @@
 package baseball;
 
 public class Application {
+    private static String firstMent = "숫자 야구 게임을 시작합니다.";
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        //Singleton pattern으로 gameManager 객체 생성
+        GameManager gameManager = GameManager.getInstance();
 
+        //게임 시작
+        System.out.println(firstMent);
 
-
-
+        try {
+            gameManager.gameStart();
+        }
+        catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
 
 
     }
