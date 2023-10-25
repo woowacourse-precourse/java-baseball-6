d4e77c73060e79c6d8f47979b4645278f1df9a26
beomsic <qjatjr29@khu.ac.kr>
2023-10-25 13:37:02 +0900

    :sparkles: feat : 입력을 받는 클래스 추가

---
+++ src/main/java/baseball/view/ConsoleInput.java
@@ -0,0 +1,17 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class ConsoleInput {
+
+    public static String askInputNumber() {
+        ConsoleOutput.print(ConsoleMessage.ENTER_NUMBER.getMessage());
+        return Console.readLine();
+    }
+
+    public static String askRestart() {
+        ConsoleOutput.print(ConsoleMessage.RESTART.getMessage());
+        return Console.readLine();
+    }
+
+}
