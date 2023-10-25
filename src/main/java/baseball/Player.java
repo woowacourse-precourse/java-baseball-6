22dcfcdb9f744bd46385335e47362b20e0cbe5a3
jsyeo97 <jsyeo97@naver.com>
2023-10-25 19:14:02 +0900

    refactor: Player 클래스의 사용자 입력 처리 기능을 UserController 클래스로 분리

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,46 +1,23 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Player {
 
-    String userInput;
+    private String userInput;
     private List<Integer> playerNums;
+    private UserController userController;
 
-    public Player(int size) {
-        userInput = receiveUserInput();
-        checkStringLength(userInput, size);
-        checkInt(userInput);
+    public Player(int size) throws IllegalArgumentException {
+        userController = new UserController();
+
+        userInput = userController.receiveUserInput();
+        userController.checkStringLength(userInput, size);
+        userController.checkInt(userInput);
         playerNums = parseNums();
-        checkDuplicated(playerNums, size);
-    }
-
-    private String receiveUserInput() throws IllegalArgumentException {
-        System.out.print("숫자를 입력해주세요 : ");
-        return Console.readLine();
-    }
-
-    private void checkStringLength(String nums, int length) {
-        if (nums.length() != length) {
-            throw new IllegalArgumentException("3자릿수를 입력해주세요.");
-        }
-    }
-
-    private int checkInt(String nums) throws IllegalArgumentException {
-        try {
-            return Integer.parseInt(nums);
-        } catch (Exception e) {
-            throw new IllegalArgumentException("숫자를 입력해주세요.");
-        }
-    }
-
-    private void checkDuplicated(List<Integer> nums, int size) throws IllegalArgumentException {
-        if (nums.stream().distinct().count() != size) {
-            throw new IllegalArgumentException("서로 다른 3개의 수를 입력해주세요.");
-        }
+        userController.checkDuplicated(playerNums, size);
     }
 
     private List<Integer> parseNums() throws IllegalArgumentException {
@@ -53,7 +30,6 @@ public class Player {
         } catch (Exception e) {
             throw new IllegalArgumentException(e);
         }
-
     }
 
     public List<Integer> getPlayerNums() {
