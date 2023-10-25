4e76c61c829a77dd019770153ea131e0d235f7c0
swhyeon98 <smreo75@gmail.com>
2023-10-25 21:57:08 +0900

    feat: 게임 시작을 위한 'start' 메서드와 초기 세팅을 위한 'init' 메서드 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,4 +1,22 @@
 package baseball;
 
 public class Game {
+
+    private Computer computer;
+    private Player player;
+
+    public Game(Computer computer, Player player) {
+        this.computer = computer;
+        this.player = player;
+    }
+
+    public void start() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        init();
+    }
+
+    private void init() {
+        computer.generateRandomNumbers();
+        player.setNumbers(new InputHandler().getPlayInput());
+    }
 }
