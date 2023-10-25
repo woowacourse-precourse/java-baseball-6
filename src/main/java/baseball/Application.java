72b8cba93fbece4ae12eff4e93d3f0ddeca8e7de
kang jihyeong <her4386@gmail.com>
2023-10-25 15:31:30 +0900

    게임 시작 프로세스 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import baseball.startProcess.*;
+import baseball.finishProcess.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        while (true) {
+            startProcess startProcess = new startProcess();
+        }
     }
 }
