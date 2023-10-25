6238425dc5a0ddec03753a2563c2283ec9e26a10
동원 <ai06021@gmail.com>
2023-10-25 23:50:33 +0900

    fix : throw 예외 던지기 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,14 +8,12 @@ public class Application {
             System.out.println("숫자 야구 게임을 시작합니다.");
             String input = Console.readLine();
             Play play = new Play();
-            if (!play.inputCheck(input)) {
-                break;
-            }
+            play.inputCheck(input);
             play.game(input);
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            Console.close();
-            if (play.newGameCheck() != 1) {
-                Console.close();
+            String retry = Console.readLine();
+            Newgame newgame = new Newgame();
+            if (newgame.newGameCheck(retry) != 1) {
                 break;
             }
         }
