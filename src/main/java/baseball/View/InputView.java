a73a0f4755c71bdc9dd4d40003407cac99cf08da
ljh <dusk5588@naver.com>
2023-10-25 21:30:13 +0900

    [Feat] OutputView 클래스 구현

--- src/main/java/baseball/View/InputView.java
+++ src/main/java/baseball/View/InputView.java
@@ -2,8 +2,8 @@ package baseball.View;
 
 public class InputView {
 
-    String inputMessage = "숫자를 입력해주세요 : ";
-    String retryMessage = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private String inputMessage = "숫자를 입력해주세요 : ";
+    private String retryMessage = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public void printInputMessage() {
         System.out.print(inputMessage);
--- src/main/java/baseball/View/OutputView.java
+++ src/main/java/baseball/View/OutputView.java
@@ -1,4 +1,44 @@
 package baseball.View;
 
 public class OutputView {
+
+    private String startMessage = "숫자 야구 게임을 시작합니다.";
+    private String nothing = "낫싱";
+    private String ballResult = "%d볼";
+    private String strikeResult = "%d스트라이크";
+    private String ballAndStrikeResult = "%d볼 %d스트라이크";
+    private String successMessage = "%d개의 숫자를 모두 맞히셨습니다! 게임 종료.";
+    private String endMessage = "숫자 야구 게임을 종료합니다.";
+
+
+    public void printStartMessage() {
+        System.out.println(startMessage);
+    }
+
+    public void resultMessage(int ballNum, int strikeNum) {
+        if(ballNum == 0 && strikeNum == 0) {
+            System.out.println(nothing);
+        }
+        else if(ballNum == 0 && strikeNum != 0) {
+            System.out.printf(strikeResult, strikeNum);
+            System.out.println();
+        }
+        else if(ballNum != 0 && strikeNum == 0) {
+            System.out.printf(ballResult, ballNum);
+            System.out.println();
+        }
+        else {
+            System.out.printf(ballAndStrikeResult, ballNum, strikeNum);
+            System.out.println();
+        }
+    }
+
+    public void successMessage(int length) {
+        System.out.printf(successMessage, length);
+        System.out.println();
+    }
+
+    public void printEndMessage() {
+        System.out.println(endMessage);
+    }
 }
