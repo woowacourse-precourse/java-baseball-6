aa559eaeeb1e11504d76b0620aeca2f6974999e6
kang jihyeong <her4386@gmail.com>
2023-10-25 22:53:40 +0900

    Main 들여쓰기 개선

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,20 +6,22 @@ import baseball.gameProcess.*;
 
 public class Application {
     public static void main(String[] args) throws IllegalArgumentException{
-        GameSetting gameSetting = new GameSetting();
-
-        StartProcess startProcess = new StartProcess(gameSetting);
-        InProcess inProcess;
-        FinishProcess finishProcess;
-
         try {
+            // 게임 설정 세팅
+            GameSetting gameSetting = new GameSetting();
+
+            // 게임 시작
+            StartProcess startProcess = new StartProcess(gameSetting);
+
             while (true) {
+                // 게임 진행
                 while (true) {
-                    inProcess = new InProcess(gameSetting, startProcess.getComputerNumber());
+                    InProcess inProcess = new InProcess(gameSetting, startProcess.getComputerNumber());
                     if (inProcess.isNumberCorrect()) break;
                 }
 
-                finishProcess = new FinishProcess();
+                // 게임 끝
+                FinishProcess finishProcess = new FinishProcess();
                 if (!finishProcess.isContinueGame()) break;
 
                 startProcess.resetGame(gameSetting);
