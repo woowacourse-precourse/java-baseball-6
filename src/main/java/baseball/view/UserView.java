aee2edf032348967b0553bdd31410e1834b3d273
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-10-25 22:09:02 +0900

    feat: UserView 기능 추가

--- src/main/java/baseball/view/UserView.java
+++ src/main/java/baseball/view/UserView.java
@@ -1,4 +1,30 @@
 package baseball.view;
 
+import baseball.model.Constants;
+import camp.nextstep.edu.missionutils.Console;
+
+import static baseball.model.Constants.INPUT_MESSAGE;
+import static baseball.model.Constants.RESTART_OR_END_MESSAGE;
+
 public class UserView {
+
+    public void displayStartMessage() {
+        System.out.println(Constants.START_MESSAGE);
+    }
+
+    public String getUserNumber() {
+        System.out.println(INPUT_MESSAGE);
+        String userNumber = Console.readLine();
+        return userNumber;
+    }
+
+    public void displayResultMessage(String resultMessage) {
+        System.out.println(resultMessage);
+    }
+
+    public static String getRestartOrEnd() {
+        System.out.println(RESTART_OR_END_MESSAGE);
+        String restartOrEnd = Console.readLine();
+        return restartOrEnd;
+    }
 }
