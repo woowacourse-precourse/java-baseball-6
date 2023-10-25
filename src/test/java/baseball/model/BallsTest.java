5f883d07b7289c53e98457ea11cee2e9f82c6072
Dreaming-J <kimjjong321@naver.com>
2023-10-25 19:21:40 +0900

    :heavy_minus_sign: style. 의미없는 import 제거

--- src/test/java/baseball/model/BallsTest.java
+++ src/test/java/baseball/model/BallsTest.java
@@ -1,6 +1,5 @@
 package baseball.model;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
