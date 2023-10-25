865ac941382dfa01282ee4a875b703252af82f00
strongcookdas <goodjav141592@gmail.com>
2023-10-25 20:30:39 +0900

    test : 숫자 비교 테스트 코드 작성 테스트 케이스 - 숫자만 맞힌 경우 - 숫자와 위치를 맞힌 경우 - 모두 맞힌 경우 - 아무것도 맞추지 못한 경우

--- src/test/java/baseball/service/ComputerTest.java
+++ src/test/java/baseball/service/ComputerTest.java
@@ -1,6 +1,7 @@
 package baseball.service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -18,5 +19,57 @@ class ComputerTest {
         randomNumbers = computer.generateNumber();
         //then
         Assertions.assertEquals(3, randomNumbers.size());
+    }
+
+    @Test
+    @DisplayName("숫자 비교 결과1 : 숫자만 같을 경우")
+    void getResult_숫자만_같을_경우() {
+        //given
+        List<Integer> numbers1 = Arrays.asList(1, 2, 3);
+        List<Integer> numbers2 = Arrays.asList(2, 1, 7);
+        Player player = new Player();
+        //when
+        String result = player.getResult(numbers1, numbers1);
+        //then
+        Assertions.assertEquals("2볼", result);
+    }
+
+    @Test
+    @DisplayName("숫자 비교 결과2 : 숫자와 위치가 같을 경우")
+    void getResult_숫자와_위치가_같을_경우() {
+        //given
+        List<Integer> numbers1 = Arrays.asList(1, 2, 3);
+        List<Integer> numbers2 = Arrays.asList(1, 3, 5);
+        Player player = new Player();
+        //when
+        String result = player.getResult(numbers1, numbers1);
+        //then
+        Assertions.assertEquals("1볼 1스트라이크", result);
+    }
+
+    @Test
+    @DisplayName("숫자 비교 결과3 : 일치한 숫자가 없을 경우")
+    void getResult_일치한_숫자가_없을_경우() {
+        //given
+        List<Integer> numbers1 = Arrays.asList(1, 2, 3);
+        List<Integer> numbers2 = Arrays.asList(6, 7, 8);
+        Player player = new Player();
+        //when
+        String result = player.getResult(numbers1, numbers1);
+        //then
+        Assertions.assertEquals("낫싱", result);
+    }
+
+    @Test
+    @DisplayName("숫자 비교 결과4 : 숫자와 위치를 모두 맞춘 경우")
+    void getResult_숫자와_위치를_모두_맞춘_경우() {
+        //given
+        List<Integer> numbers1 = Arrays.asList(1, 2, 3);
+        List<Integer> numbers2 = Arrays.asList(1, 2, 3);
+        Player player = new Player();
+        //when
+        String result = player.getResult(numbers1, numbers1);
+        //then
+        Assertions.assertEquals("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료", result);
     }
 }
