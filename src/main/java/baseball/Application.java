6ddf7c6fc57629d1aa374d05171183712ba30912
edjjincode <138580800+edjjincode@users.noreply.github.com>
2023-10-25 13:28:06 +0900

    fix(apply): add First message
    -add first message "숫자 야구게임을 시작합니다."
    - make sure it only starts at the very first time

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,12 @@
 package baseball;
 
+
+import baseball.view.SystemMessage;
 public class Application {
     public static void main(String[] args) {
+        SystemMessage systemMessage = new SystemMessage();
         Controller controller = new Controller();
+        systemMessage.startGreeting();
         controller.run();
     }
 }
