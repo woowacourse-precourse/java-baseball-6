705c51b0da98474115e5702584386f5db48723da
yooooonshine <hl3470@naver.com>
2023-10-25 17:55:38 +0900

    Refactor: 출력text 분리

---
+++ src/main/java/baseball/game/GameOutputText.java
@@ -0,0 +1,11 @@
+package baseball.game;
+
+public class GameOutputText {
+    public static final String STARTING_GAME = "숫자 야구 게임을 시작합니다.\n";
+    public static final String ASKING_USER_INPUT = "숫자를 입력해주세요 : ";
+    public static final String ENDING = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n";
+    public static final String BALL = "볼";
+    public static final String STRIKE = "스트라이크";
+    public static final String NOTHING = "낫싱";
+    public static final String ASKING_REGAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
+}
