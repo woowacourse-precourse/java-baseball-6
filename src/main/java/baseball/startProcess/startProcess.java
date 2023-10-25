0a8b6c45aeb6958cf08a9683120da42d8da4dc34
kang jihyeong <her4386@gmail.com>
2023-10-25 15:30:54 +0900

    게임 시작 문구 출력

---
+++ src/main/java/baseball/startProcess/startProcess.java
@@ -0,0 +1,12 @@
+package baseball.startProcess;
+
+public class startProcess {
+
+    public startProcess() {
+        printStartPhrase();
+    }
+
+    private void printStartPhrase() {
+        System.out.println(startNotice.startPhrase);
+    }
+}
