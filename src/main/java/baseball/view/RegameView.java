eec10ca9407748c5885426dc729b84ee3e3ef948
realcold0 <realcold0@gmail.com>
2023-10-25 17:58:29 +0900

    fix : 게임 종료 출력 추가

--- src/main/java/baseball/view/RegameView.java
+++ src/main/java/baseball/view/RegameView.java
@@ -3,6 +3,7 @@ package baseball.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class RegameView {
+    private final String CORRECT_MENT = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private final String REGAME_MENT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private boolean regame;
 
@@ -11,6 +12,7 @@ public class RegameView {
     }
 
     public boolean inputRegame() {
+        System.out.println(CORRECT_MENT);
         System.out.println(REGAME_MENT);
         int regameInt = Integer.parseInt(Console.readLine());
         if (regameInt == 1) {
