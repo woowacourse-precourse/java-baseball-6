2bf3a468a0f5b33ca7c4872eed3535398d95ccb9
psrom <psrom721@gmail.com>
2023-10-25 16:04:54 +0900

    feat: Play baseball game
    userInput List와 computer List의 값을 비교하여
    스트라이크3을 맞출 때까지 사용자의 입력을 받는다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -15,6 +15,20 @@ public class Application {
 
             while (!correctGuess) {
                 List<Integer> userInput = getUserInput();
+
+                correctGuess = checkGuess(userInput, computer);
+                if (correctGuess) {
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                }
+            }
+
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+            String restartChoice = Console.readLine();
+            if (!"1".equals(restartChoice) && !"2".equals(restartChoice)) {
+                throw new IllegalArgumentException();
+            } else if ("2".equals(restartChoice)) {
+                break;
             }
         }
     }
@@ -25,7 +39,6 @@ public class Application {
 
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
-
             if (!computer.contains((randomNumber))) {
                 computer.add(randomNumber);
             }
@@ -43,7 +56,6 @@ public class Application {
 
         try {
             int number = Integer.parseInt(inputString);
-
             if (number < 100 || number > 999) {
                 throw new IllegalArgumentException();
             }
@@ -51,9 +63,7 @@ public class Application {
             int digit1 = number % 10;
             int digit2 = (number / 10) % 10;
             int digit3 = number / 100;
-
-            if ((digit1 == digit2) || (digit1 == digit3) || (digit2 == digit3)
-                    || (digit1 == 0) || (digit2 == 0) || (digit3 == 0)) {
+            if ((digit1 == digit2) || (digit1 == digit3) || (digit2 == digit3)|| (digit1 == 0) || (digit2 == 0)) {
                 throw new IllegalArgumentException();
             }
 
@@ -66,6 +76,33 @@ public class Application {
         }
 
         return userInput;
+    }
+
+
+    private static boolean checkGuess(List<Integer> userInput, List<Integer> computer) {
+        int ball = 0;
+        int strike = 0;
+
+        for (int i = 0; i < 3; i++) {
+            int currentUserInput = userInput.get(i);
+            int currentComputerValue = computer.get(i);
+            if (currentUserInput == currentComputerValue) {
+                strike++;
+            } else if (computer.contains(currentUserInput)) {
+                ball++;
+            }
+        }
+
+        if (strike == 3) {
+            System.out.println("3스트라이크");
+        } else if (strike > 0 || ball > 0) {
+            System.out.println((ball > 0 ? ball + "볼" : "") + (strike > 0 && ball > 0 ? " " : "")
+                    + (strike > 0 ? strike + "스트라이크" : ""));
+        } else {
+            System.out.println("낫싱");
+        }
+
+        return strike == 3;
     }
 
 }
