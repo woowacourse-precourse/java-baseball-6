71c1b69c26fd841ca9339f296b81b87cac8a1be4
남수연 <rnrmf0721@gmail.com>
2023-10-25 17:40:08 +0900

    fix: 재시작 시 예외 발생하는 버그 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -95,7 +95,7 @@ public class Application {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         int input = Integer.parseInt(Console.readLine());
 
-        if(input == 1 || input == 2){
+        if(input != 1 && input != 2){
             throw new IllegalArgumentException();
         }
 
