62860d0294ff52f64628b6565699b245f9eb650d
Ha0Day <ycis5013@naver.com>
2023-10-25 23:17:27 +0900

    feat : 플레이어의 숫자 세팅하는 함수 setPlayerNum() 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -35,6 +36,30 @@ public class Application {
         }
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료"); //숫자 3개 다 맞혔다면 게임 종료
         checkRestart(); //재시작, 종료 여부 확인
+    }
+
+    //플레이어의 숫자 세팅하는 함수
+    private static void setPlayerNum() throws IllegalArgumentException {
+        player = new ArrayList<>();
+        System.out.print("숫자를 입력해주세요 : ");
+        char[] player_chArr = Console.readLine().toCharArray();
+        for (char c : player_chArr) {
+            int num = c - '0';
+            //서로 다른 수가 아닌 경우
+            if (player.contains(num)) {
+                throw new IllegalArgumentException();
+            } else {
+                player.add(num);
+            }
+        }
+        //숫자 3개가 아닌 경우
+        if (player.size() != 3) throw new IllegalArgumentException();
+        //숫자가 아닌 경우
+        for (int i = 0; i < 3; i++) {
+            if (!(1 <= player.get(i) && player.get(i) <= 9)) {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 
     //컴퓨터의 숫자 세팅하는 함수
