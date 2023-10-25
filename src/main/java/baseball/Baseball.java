e640b4fc8f372fe8a0b03d030d07a9c518a07024
dbstn1325 <dbstn1325@naver.com>
2023-10-25 14:25:47 +0900

    feat: 사용자의 숫자와 컴퓨터의 숫자가 모두 일치할때까지 반복해서 입력받기

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -11,33 +11,42 @@ public class Baseball {
 
     public void startGame() {
         List<Integer> computerNumber = RandomUtil.generateRandomNumber(3);
-        int strike = 0;
-        int ball = 0;
 
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        System.out.print("숫자를 입력해주세요 : ");
-        String userInput = readLine();
-        if(userInput.length() != 3) {
-            throw new IllegalArgumentException("잘못된 값을 입력했습니다. 애플리케이션을 종료합니다.");
-        }
-        for(char c: userInput.toCharArray()) {
-            if(!Character.isDigit(c) || c == '0') {
+        while(true) {
+            int strike = 0;
+            int ball = 0;
+
+            System.out.println("숫자 야구 게임을 시작합니다.");
+            System.out.print("숫자를 입력해주세요 : ");
+            String userInput = readLine();
+            if (userInput.length() != 3) {
                 throw new IllegalArgumentException("잘못된 값을 입력했습니다. 애플리케이션을 종료합니다.");
             }
-        }
+            for (char c : userInput.toCharArray()) {
+                if (!Character.isDigit(c) || c == '0') {
+                    throw new IllegalArgumentException("잘못된 값을 입력했습니다. 애플리케이션을 종료합니다.");
+                }
+            }
 
-        List<Integer> userNumber = UserInputUtil.convertStringToIntegerList(userInput);
-        for(int i=0; i<3; i++){
-            String result = compareNumber(computerNumber, userNumber.get(i), i);
+            Score score = new Score();
+            List<Integer> userNumber = UserInputUtil.convertStringToIntegerList(userInput);
+            for (int i = 0; i < 3; i++) {
+                String result = compareNumber(computerNumber, userNumber.get(i), i);
 
-            if(result.equals("스트라이크")) {
-                strike++;
-            }else if(result.equals("볼")) {
-                ball++;
+                if (result.equals("스트라이크")) {
+                    strike++;
+                } else if (result.equals("볼")) {
+                    ball++;
+                }
+            }
+
+            score.setScore(ball, strike);
+            printHint(ball, strike);
+
+            if(score.getStrike() == 3) {
+                break;
             }
         }
-
-        printHint(ball, strike);
     }
 
     private String compareNumber(List<Integer> computerNumber, int userInputValue, int index) {
