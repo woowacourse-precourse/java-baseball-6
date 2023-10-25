9eeb776f96ba74d4f15950b5f12b4839ab7618b2
haebyun <bhk4392@gmail.com>
2023-10-25 19:37:34 +0900

    feat(BaseballExecutor): Baseball 객체를 통해 게임을 생성하는 기능 구현

--- src/main/java/baseball/games/numberbaseball/core/BaseballExecutor.java
+++ src/main/java/baseball/games/numberbaseball/core/BaseballExecutor.java
@@ -5,6 +5,8 @@ import baseball.GamingConsole;
 public class BaseballExecutor implements GamingConsole {
     @Override
     public void createNewGame() {
-
+        Baseball baseball = new Baseball();
+        baseball.playGame();
     }
+
 }
