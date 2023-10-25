c1a6c7e1ec73fe4d4a58b1f924cc0b96e925186a
sunwoong <sunwoong1122@gmail.com>
2023-10-25 21:02:07 +0900

    refactor: View 클래스 메서드 명 리팩터링

--- src/main/java/baseball/view/View.java
+++ src/main/java/baseball/view/View.java
@@ -11,16 +11,16 @@ public class View {
         System.out.print("숫자를 입력해주세요 : ");
     }
 
-    public void showResultByCorrectAnswer() {
+    public void showCorrectLine() {
         System.out.println("3스트라이크");
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
-    public void showResultByPartialMismatchAnswer(Result result) {
+    public void showPartialMismatchLine(Result result) {
         System.out.println(result.getBallCount() + "볼 " + result.getStrikeCount() + "스트라이크");
     }
 
-    public void showResultByAllMismatchAnswer() {
+    public void showAllMismatchLine() {
         System.out.println("낫싱");
     }
 
