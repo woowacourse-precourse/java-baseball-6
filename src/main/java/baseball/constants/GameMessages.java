0de6c7b394435354e1cf3e93035890d490147ba9
MingyeomKim <mingyum119@naver.com>
2023-10-25 10:27:46 +0900

    refactor(GameMessages): 메시지를 상수화한 변수의 접미사를 MESSAGE로 통일
    접두사를 통일하여 가독성이 좋게 만들고, 외부에서 메시지를 호출하여 사용하기 위해 getter 메서드를 생성한다.

--- src/main/java/baseball/constants/GameMessages.java
+++ src/main/java/baseball/constants/GameMessages.java
@@ -1,8 +1,8 @@
-package baseball.model;
+package baseball.constants;
 
 public enum GameMessages {
 
-    GAME_START_GUIDE("숫자 야구 게임을 시작합니다."),
+    GAME_START_MESSAGE("숫자 야구 게임을 시작합니다."),
     CORRECT_ANSWER_MESSAGE("3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
     RESTART_OR_EXIT_MESSAGE("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."),
     NUMBER_REQUEST_MESSAGE("숫자를 입력해주세요 : ");
@@ -11,5 +11,9 @@ public enum GameMessages {
 
     GameMessages(String message) {
         this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
     }
 }
