24481a4d1749b0a0ef5c9bd83310ad0546a826df
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-25 21:11:45 +0900

    [feat] : add restart method

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -3,8 +3,10 @@ package baseball.service;
 import baseball.domain.Computer;
 import baseball.domain.GameManager;
 import baseball.domain.Player;
+import baseball.utils.ExceptionMessage;
 import baseball.view.InputInformationView;
 import baseball.view.OutputInformationView;
+import camp.nextstep.edu.missionutils.Console;
 
 public class GameService {
     private final Computer computer;
@@ -37,5 +39,23 @@ public class GameService {
         outputInformationView.checkAnswerGuide();
         outputInformationView.gameOverGuide();
         inputInformationView.reStartGuide();
+        restart();
+    }
+
+    // 게임의 재시작 여부를 확인하고 재시작/종료하는 메서드.
+    private void restart() {
+        String reState = Console.readLine();
+
+        if (!reState.equals("1") && !reState.equals("2")) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID.getCode());
+        }
+
+        if (reState.equals("1")) {
+            start();
+        }
+
+        if (reState.equals("2")) {
+            outputInformationView.gameEndGuide();
+        }
     }
 }
