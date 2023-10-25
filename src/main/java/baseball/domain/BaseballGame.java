c9a89ebad43e47bdf7dd65167d34e9a29f3ee940
saebyeok2 <tomato8269@naver.com>
2023-10-25 15:52:50 +0900

    feat: 게임 종료 OR 재시작 확인

--- src/main/java/baseball/domain/BaseballGame.java
+++ src/main/java/baseball/domain/BaseballGame.java
@@ -1,6 +1,10 @@
 package baseball.domain;
 
+import static baseball.constant.GameMessage.INPUT_ERROR_MESSAGE;
+import static baseball.constant.GameMessage.RESTART_CHECK_MESSAGE;
 import static baseball.constant.GameMessage.START_MESSAGE;
+
+import camp.nextstep.edu.missionutils.Console;
 
 public class BaseballGame {
     private Computer computer;
@@ -17,9 +21,26 @@ public class BaseballGame {
 
     public void start() {
         System.out.println(START_MESSAGE);
+        do {
+            play();
+        } while (checkRestart());
     }
 
     public void play() {
 
+    }
+
+    // 게임 재시작 여부를 확인
+    public boolean checkRestart() {
+        System.out.println(RESTART_CHECK_MESSAGE);
+        String input = Console.readLine();
+
+        if (input.equals("1")) {
+            return true;
+        }
+        if (input.equals("2")) {
+            return false;
+        }
+        throw new IllegalArgumentException(INPUT_ERROR_MESSAGE);
     }
 }
