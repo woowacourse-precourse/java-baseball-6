2f2d01c0f5836d00de8e26eb05a5479b4c23c828
bora <youonl5@naver.com>
2023-10-25 19:22:57 +0900

    Feat: pickRandomNumbers() 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,5 +1,9 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
     private String answer;
     private String userInput;
@@ -9,6 +13,17 @@ public class Game {
     Game() {
         this.ball = 0;
         this.strike = 0;
+    }
+
+    public void pickRandomNumbers() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        this.answer = computer.toString().replaceAll("[^0-9]", "");
     }
 
 }
