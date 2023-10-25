95de813fd95508d8ae1aefeaa2fc2944c3b9630d
Ha0Day <ycis5013@naver.com>
2023-10-25 20:56:31 +0900

    feat : 게임 시작 문구를 출력하는 gameStart() 함수 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
+
+    public static List<Integer> computer; //컴퓨터의 숫자
+    public static List<Integer> player; //플레이어의 숫자
+    public static boolean CONTINUE_GAME = true; //재시작, 종료 구분하는 변수
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        gameStart(); //게임 시작 문구 출력
+
+        while (CONTINUE_GAME) {
+            game();
+        }
+    }
+
+    private static void gameStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
     }
 }
+
