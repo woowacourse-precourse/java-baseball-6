12ec5269eab5285efeab5dbe618d1262ff6fc24e
answer <answer3dw@gmail.com>
2023-10-25 22:04:50 +0900

    test : 테스트 코드 수정

--- src/main/java/baseball/model/ReplayNumber.java
+++ src/main/java/baseball/model/ReplayNumber.java
@@ -13,7 +13,7 @@ public class ReplayNumber {
 
     public static void isReplayNumberCorrect(String inputReplayNumber) throws IllegalArgumentException{
         if(!inputReplayNumber.equals("1")&&!inputReplayNumber.equals("2")){
-            throw new IllegalArgumentException("잘못된 값을 입력하셨습니다");
+            throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.");
         }
     }
 }
