6923d110f5fe50ebb958dd414cd318c66a0e7555
KongJunWoo <joonoo3@naver.com>
2023-10-25 17:41:51 +0900

    feat : 전체적인 게임 실행
    - computer 난수를 먼저 생성
    - 먼저 실행 후 3 스트라이크가 아닐 시 계속 실행
    - 숫자 입력 받고 비교하고 결과 출력

--- src/main/java/baseball/controller/Game.java
+++ src/main/java/baseball/controller/Game.java
@@ -24,6 +24,20 @@ public class Game {
         return new Game();
     }
 
+    //game 시작
+    public void start(){
+
+        createComputerNums(); //computer 난수 생성
+
+        //3스트라이크가 아닐 시 계속 실행
+        do{
+            userInput(); //숫자 입력받기
+            compareNum(); //입력 받은 수와 컴퓨터 수 비교
+            printResult(); //결과 출력
+        } while(Compare.isNotThreeStrike());
+
+    }
+
     //computer 난수 생성
     public void createComputerNums(){
         computer.createRandomNums();
@@ -45,5 +59,5 @@ public class Game {
     public void printResult(){
         OutputView.printResult(Compare.getStrike(), Compare.getBall());
     }
-    
+
 }
