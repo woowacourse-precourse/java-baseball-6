8fe7ff8747f2c61ad275f2541c7855749cb57070
박상현 <shawnpark98@naver.com>
2023-10-25 13:25:49 +0900

    feat: ExceptionHandler 구현

--- src/main/java/baseball/util/ExceptionHandler.java
+++ src/main/java/baseball/util/ExceptionHandler.java
@@ -2,4 +2,10 @@ package baseball.util;
 
 public class ExceptionHandler {
 
+    private static final String INPUT_EXCEPTION_MESSAGE = "잘못된 값을 입력했습니다.";
+
+    public static void inputException() {
+        throw new IllegalArgumentException(INPUT_EXCEPTION_MESSAGE);
+    }
+
 }
