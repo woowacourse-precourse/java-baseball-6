d217ec1467e4499a6229e17f89c8ccc930203928
OhTaeHo-97 <57743934+OhTaeHo-97@users.noreply.github.com>
2023-10-25 18:00:42 +0900

    feat: 숫자 야구 게임 전체 게임 틀 작성
    재시작 여부 체크 로직을 제외한 재시작 여부에 맞는 게임 진행 틀 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,12 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BaseballGame baseballGame = new BaseballGame();
+        boolean willBeRestarted = true;
+
+        do {
+            baseballGame.startGame();
+            willBeRestarted = checkRestart();
+        } while (willBeRestarted);
     }
 }
