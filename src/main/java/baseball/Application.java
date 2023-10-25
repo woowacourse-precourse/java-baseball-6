3e5e875a485974ffcca332b38f61cc7c8605e354
moonyeju <moonyeju8510@gmail.com>
2023-10-25 10:24:28 +0900

    FEAT: 1~9 서로 다른 수로 이루어진 임의의 수 3개 생성 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,29 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 랜덤 숫자를 생성.
+        int uniqueRandomNumber = generateUniqueRandomNumber(1, 9, 3);
+        System.out.println("상대방의 수: " + uniqueRandomNumber);
+    }
+
+    public static int generateUniqueRandomNumber(int start, int end, int count) {
+        int result = 0;
+        Set<Integer> uniqueNumbers = new HashSet<>();
+
+        //set을 이용해 중복 제거
+        while (uniqueNumbers.size() < count) {
+            int uniqueNumber = Randoms.pickNumberInRange(start, end);
+            uniqueNumbers.add(uniqueNumber);
+        }
+        for (int number : uniqueNumbers) {
+            result = result * 10 + number;
+        }
+
+        return result;
     }
 }
