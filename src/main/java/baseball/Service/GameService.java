c6d2a5b0b745dc73add5876e2407b39c3a953d9e
SSUHYUNKIM <yellosuhyun@naver.com>
2023-10-25 17:18:14 +0900

    feat: 게임 시작 및 플레이어로부터 입력 받기

--- src/main/java/baseball/Service/GameService.java
+++ src/main/java/baseball/Service/GameService.java
@@ -1,23 +1,24 @@
 package baseball.Service;
 
-import static camp.nextstep.edu.missionutils.Console.readLine;
+import camp.nextstep.edu.missionutils.Console;
 
 import baseball.Model.Player;
 import baseball.Model.Computer;
+import baseball.View.StartGameView;
+import baseball.View.PlayGameView;
+import baseball.View.EndGameView;
 
 public class GameService {
 
     public static void startGame(){
-        setGame();
-        playGame();
-    }
-    private static void setGame() {
+        StartGameView.printGameStart();
         Computer.setRandomNumber();
-        Player.getPlayerNumber(readLine());
+        playGame();
     }
 
     private static void playGame() {
-
+        PlayGameView.printInputNumber();
+        Player.getPlayerNumber(Console.readLine());
     }
 
     private static void endGame() {
---
+++ src/main/java/baseball/View/PlayGameView.java
@@ -0,0 +1,7 @@
+package baseball.View;
+
+public class PlayGameView {
+    public static void printInputNumber() {
+        System.out.println("숫자를 입력해주세요 :");
+    }
+}
