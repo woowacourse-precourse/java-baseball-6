ebc0babad237bee5920a2c5b6f8e00748930a12c
uwoobeat <desktop3711@gmail.com>
2023-10-25 16:35:27 +0900

    feat: 게임 결과 출력 기능 구현

---
+++ src/main/java/baseball/GameResult.java
@@ -0,0 +1,28 @@
+package baseball;
+
+public record GameResult(
+        int strikeCount,
+        int ballCount,
+        boolean isCorrectAnswer
+) {
+    public void announce() {
+        if (isCorrectAnswer) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return;
+        }
+
+        if (strikeCount > 0) {
+            System.out.print(strikeCount + "스트라이크 ");
+        }
+
+        if (ballCount > 0) {
+            System.out.print(ballCount + "볼");
+        }
+
+        if (strikeCount == 0 && ballCount == 0) {
+            System.out.print("낫싱");
+        }
+
+        System.out.println();
+    }
+}
