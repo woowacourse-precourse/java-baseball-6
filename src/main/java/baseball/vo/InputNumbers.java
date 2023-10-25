9e8f7a4d1b7e18fe9cb4f5bbb2106b9cec8624d2
InJun2 <os2864@naver.com>
2023-10-25 17:17:01 +0900

    :art: regex 변수 위치 변경 및 inputNumbers 기존 초기화 제거

--- src/main/java/baseball/vo/InputNumbers.java
+++ src/main/java/baseball/vo/InputNumbers.java
@@ -4,8 +4,7 @@ import java.util.Arrays;
 import java.util.regex.Pattern;
 
 public class InputNumbers {
-    private static final String INPUT_NUMBER_REGEX = "^[1-9]{3}$";
-    private int[] inputNumbers = new int[3];
+    private int[] inputNumbers;
 
     public InputNumbers(String input) {
         validateInputNumbers(input);
@@ -13,13 +12,15 @@ public class InputNumbers {
     }
 
     private void validateInputNumbers(String inputStr) {
-        if(!Pattern.compile(INPUT_NUMBER_REGEX).matcher(inputStr).matches()) {
+        String INPUT_NUMBER_REGEX = "^[1-9]{3}$";
+
+        if (!Pattern.compile(INPUT_NUMBER_REGEX).matcher(inputStr).matches()) {
             throw new IllegalArgumentException();
         }
     }
 
     private void addNumbers(String inputStr) {
-        inputNumbers = Arrays.stream(inputStr.split(""))
+        this.inputNumbers = Arrays.stream(inputStr.split(""))
                 .mapToInt(Integer::parseInt)
                 .toArray();
     }
