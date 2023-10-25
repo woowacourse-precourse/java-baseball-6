7ae78e870429c74ad484039afdd7ea8cbaab0c4b
edjjincode <138580800+edjjincode@users.noreply.github.com>
2023-10-25 13:32:57 +0900

    refactor(apply): adjust getRandomNumbers method

--- src/main/java/baseball/utils/RandomUtils.java
+++ src/main/java/baseball/utils/RandomUtils.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RandomUtils {
-
     public static int[] getRandomNumbers(int size, int start, int end) {
         List<Integer> numbersList = new ArrayList<>();
 
