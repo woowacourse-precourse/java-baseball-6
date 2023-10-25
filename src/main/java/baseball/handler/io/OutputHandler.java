0f39f0c1accaea02de2ce60c83a0391755f84d56
yujeonghyeon <dbdb1114@naver.com>
2023-10-25 20:34:22 +0900

    오타 수정 ( porvideHint => provideHint )

--- src/main/java/baseball/handler/io/OutputHandler.java
+++ src/main/java/baseball/handler/io/OutputHandler.java
@@ -17,7 +17,7 @@ public class OutputHandler {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         return Console.readLine();
     }
-    public static void porvideHint(Map<String, Integer> hint){
+    public static void provideHint(Map<String, Integer> hint){
 
         int ball = hint.get("ball");
         int strike = hint.get("strike");
