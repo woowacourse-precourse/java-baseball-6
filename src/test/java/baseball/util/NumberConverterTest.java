d529eb59be2836ae413470271c9bf0436669c1fa
siyeonSon <siyeon0209@naver.com>
2023-10-25 13:55:37 +0900

    test: 사용자가 문자를 입력한 경우 예외를 발생시킨다

--- src/test/java/baseball/util/NumberConverterTest.java
+++ src/test/java/baseball/util/NumberConverterTest.java
@@ -1,6 +1,8 @@
 package baseball.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -10,5 +12,12 @@ class NumberConverterTest {
     void 사용자가_입력한_글자의_수가_3개인지_확인한다() {
         assertThat(NumberConverter.from("123"))
                 .isEqualTo(List.of(1, 2, 3));
+    }
+
+    @Test
+    void 사용자가_문자를_입력한_경우_예외를_발생시킨다() {
+        Exception e = assertThrows(IllegalArgumentException.class, () ->
+                NumberConverter.from("일23"));
+        assertEquals(e.getMessage(), "문자가 아닌 숫자를 입력해주세요");
     }
 }
