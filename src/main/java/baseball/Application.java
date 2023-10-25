99b109decbe24a685d4880390dd8e119f1e9d832
benjaminuj <chosj1526@gmail.com>
2023-10-25 23:11:52 +0900

    fix: printResult함수 조건문의 연산자 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -107,11 +107,11 @@ public class Application {
     }
 
     public void printResult(int strikeCount, int ballCount) {
-        if (ballCount != 0 && strikeCount != 0) {
+        if (ballCount > 0 && strikeCount > 0) {
             System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
-        } else if (ballCount != 0 && strikeCount == 0) {
+        } else if (ballCount > 0 && strikeCount == 0) {
             System.out.println(ballCount + "볼");
-        } else if (ballCount == 0 && strikeCount != 0) {
+        } else if (ballCount == 0 && strikeCount > 0) {
             System.out.println(strikeCount + "스트라이크");
         } else if (ballCount == 0 && strikeCount == 0) {
             System.out.println("낫싱");
