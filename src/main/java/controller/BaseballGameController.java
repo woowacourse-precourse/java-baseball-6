b4a428f866424d4a36959a90a02c64c7122a31c2
LeeSungNo-ian <isnn0103@gmail.com>
2023-10-25 19:49:09 +0900

    feat: Computer 랜덤 숫자 로직 구현

--- src/main/java/controller/BaseballGameController.java
+++ src/main/java/controller/BaseballGameController.java
@@ -2,13 +2,16 @@ package controller;
 
 import view.PlayGameInformationOutputView;
 import view.PlayerNumberInputView;
+import model.Computer;
 
 public class BaseballGameController {
     PlayGameInformationOutputView playGameInformationOutputView = new PlayGameInformationOutputView();
     PlayerNumberInputView playerNumberInputView = new PlayerNumberInputView();
+    Computer computer = new Computer();
 
     public void gameProgress() {
         playGameInformationOutputView.printGameStart();
         playerNumberInputView.receiveNumber();
+        System.out.println(computer.generateBalls());
     }
 }
---
+++ src/main/java/model/Computer.java
@@ -0,0 +1,21 @@
+package model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Computer {
+    private final Set<Integer> computerBall = new LinkedHashSet<>();
+
+    public Set<Integer> generateBalls() {
+        while (computerBall.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (computerBall.contains(randomNumber)) {
+                continue;
+            }
+            computerBall.add(randomNumber);
+        }
+        return computerBall;
+    }
+}
