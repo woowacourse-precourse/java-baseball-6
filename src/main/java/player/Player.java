4927871d9e981ec7bceb600640ac0694413e1d21
34suuuuu <psh7492@gmail.com>
2023-10-25 10:43:41 +0900

    refactor: Input 클래스 삭제 후 Player와 Computer 클래스로 메서드 추가

--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -1,6 +1,7 @@
 package player;
 
-import view.Input;
+import camp.nextstep.edu.missionutils.Console;
+import constants.MessageConstants;
 
 import static constants.MessageConstants.*;
 import static constants.NumConstants.*;
@@ -11,9 +12,18 @@ public class Player {
     public Player(){}
 
     public String getInputString(){
-        String inputString = Input.readInput();
+        System.out.print(MessageConstants.INPUT_MESSAGE);
+        String inputString = Console.readLine();
+
         chkInputString(inputString);
         return inputString;
+    }
+
+    public boolean getOptionString(){
+        System.out.println(MessageConstants.RESTART_OR_FINISH_MESSAGE);
+        String optionString = Console.readLine();
+
+        return chkOptionString(optionString);
     }
 
     private void chkInputString(String inputString){
@@ -30,6 +40,16 @@ public class Player {
                 throw new IllegalArgumentException(ERROR_MESSAGE + INPUT_SAME_NUMBER_ERROR_MESSAGE);
             }
             tmp += String.valueOf(c);
+        }
+    }
+
+    private boolean chkOptionString(String optionString){
+        if (optionString.equals(String.valueOf(OPTION_RESTART))) {
+            return true;
+        } else if (optionString.equals(String.valueOf(OPTION_FINISH))) {
+            return false;
+        } else {
+            throw new IllegalArgumentException(OPTION_INPUT_ERROR_MESSAGE);
         }
     }
 
