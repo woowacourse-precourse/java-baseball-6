02c6b4917528d8a7a47543044e18acaf8b20292c
yujeonghyeon <dbdb1114@naver.com>
2023-10-25 23:14:27 +0900

    properties 초기화 방식 변경

--- src/main/java/baseball/config/GameProperty.java
+++ src/main/java/baseball/config/GameProperty.java
@@ -7,26 +7,28 @@ public class GameProperty {
 
     private final static Properties properties = new Properties();
 
-    private final static int ANSWERLENGTH = Integer.parseInt((String)properties.get("ANSWER.LENGTH"));
-    private final static int ST_RANGE = Integer.parseInt((String) properties.get("NUM.RANGE.ST"));
-    private final static int ED_RANGE = Integer.parseInt((String) properties.get("NUM.RANGE.ED"));
-
     static {
         try {
-            FileInputStream propertyFile = new FileInputStream("rule.properties");
+            FileInputStream propertyFile = new FileInputStream("src/main/resources/rule.properties");
             properties.load(propertyFile);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static int getAnswerlength(){
+    private final static int ANSWERLENGTH = Integer.parseInt((String) properties.get("ANSWER.LENGTH"));
+    private final static int ST_RANGE = Integer.parseInt((String) properties.get("NUM.RANGE.ST"));
+    private final static int ED_RANGE = Integer.parseInt((String) properties.get("NUM.RANGE.ED"));
+
+    public static int getAnswerlength() {
         return ANSWERLENGTH;
     }
-    public static int getStRange(){
+
+    public static int getStRange() {
         return ST_RANGE;
     }
-    public static int getEdRange(){
+
+    public static int getEdRange() {
         return ED_RANGE;
     }
 
