c06825d2b96891d0c017480b2ba377417034a7f7
kjm3757 <kjm03070519@gmail.com>
2023-10-25 22:21:43 +0900

    메소드 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,16 +3,41 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
-
+import java.util.Set;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
-
-
 public class Application {
+
+    static void str_startGame(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+    static void str_endGame(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+    static void str_restartGame(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    static void str_inputNum(){
+        System.out.print("숫자를 입력해주세요: ");
+    }
+
+    static final String ball = "볼 ";
+    static final String strike = "스트라이크";
+
+
+
+
+
+
+
+
+
     public static void main(String[] args) {
 
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        str_startGame();
 
         while(true){
             int[] num = new int[10];
@@ -39,7 +64,7 @@ public class Application {
                 int strikeCount = 0;
                 you.clear();
 
-                System.out.print("숫자를 입력해주세요: ");
+                str_inputNum();
                 String yourInput = readLine();
 
                 if (yourInput.length() != 3){
@@ -48,6 +73,12 @@ public class Application {
 
                 for (int i = 0; i < 3; i++) {
                     you.add((int) yourInput.charAt(i) - 48);
+                }
+
+                Set<Integer> youSet = new HashSet<>(you);
+
+                if(youSet.size()!= you.size()) {
+                    throw new IllegalArgumentException();
                 }
 
                 for (int i = 0; i < you.size(); i++) {
@@ -66,9 +97,9 @@ public class Application {
                 if (ballCount == 0 && strikeCount == 0) {
                     System.out.println("낫싱");
                 } else if (strikeCount == 3) {
-                    System.out.println(strikeCount + "스트라이크");
-                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                    System.out.println(strikeCount + strike);
+                    str_endGame();
+                    str_restartGame();
                     String gameEnd = readLine();
                     if (gameEnd.charAt(0) != '1' && gameEnd.charAt(0) != '2') {
                         throw new IllegalArgumentException();
@@ -82,13 +113,13 @@ public class Application {
                     }
                 }
                 else if (ballCount == 0) {
-                    System.out.println(strikeCount + "스트라이크");
+                    System.out.println(strikeCount + strike);
                 }
                 else if (strikeCount == 0) {
-                    System.out.println(ballCount + "볼");
+                    System.out.println(ballCount + ball);
                 }
                 else {
-                    System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+                    System.out.println(ballCount + ball + strikeCount + strike);
                 }
             }
 
