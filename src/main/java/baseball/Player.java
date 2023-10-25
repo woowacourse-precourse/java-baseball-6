7d1269556d784446e4c9a57cd878511940bbf2dc
uwoobeat <desktop3711@gmail.com>
2023-10-25 16:34:46 +0900

    feat: 플레이어 클래스의 예상 숫자 입력 기능 구현

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,4 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Player {
+    private GameNumber guessedNumber;
+
+    public GameNumber guess() {
+        this.guessedNumber = loadInputToGameNumber();
+        return guessedNumber;
+    }
+
+    private GameNumber loadInputToGameNumber() {
+        printGuessMessage();
+        String input = Console.readLine();
+        return GameNumber.from(input);
+    }
+
+    private static void printGuessMessage() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
 }
