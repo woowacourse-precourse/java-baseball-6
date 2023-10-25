c9ae641ed26bcf6d2eb30669a2336d0819536144
BaeDongHyeon <sasd730@gmail.com>
2023-10-26 00:36:40 +0900

    FEAT: 최종 프로그램 실행문 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
 
+import baseball.controller.BaseballController;
+import baseball.model.BaseballStadium;
+import baseball.view.BaseballView;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseballView baseballView = new BaseballView(new InputView(), new OutputView());
+        BaseballStadium baseballStadium = new BaseballStadium();
+        BaseballController baseballController = new BaseballController(baseballStadium, baseballView);
+        baseballController.gameStart();
     }
 }
