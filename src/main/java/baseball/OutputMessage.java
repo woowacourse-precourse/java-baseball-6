168ac67aa0e02101f717a21b992ea3006a9378de
HanHyeJi <hjhan.0621@gmail.com>
2023-10-25 23:43:48 +0900

    📍 refactor: 예외처리 메세지 추가

--- src/main/java/baseball/OutputMessage.java
+++ src/main/java/baseball/OutputMessage.java
@@ -1,9 +1,13 @@
 package baseball;
 
 public class OutputMessage {
+
     public static final String START_GAME = "숫자 야구 게임을 시작합니다.";
     public static final String ENTER_NUMBER = "\n숫자를 입력해주세요 : ";
-    public static final String END_OR_NOT = """
+    public static final String ASK_END_OR_NOT = """
             \n3개의 숫자를 모두 맞히셨습니다! 게임 종료
             게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.""";
+
+    public static final String USER_NUM_REG_EX = "1부터 9사이의 숫자로 이루어진, 중복되지 않는 숫자 세자리를 입력하세요.";
+    public static final String END_OR_NOT = "숫자 '1' 또는 '2'를 입력하세요.";
 }
