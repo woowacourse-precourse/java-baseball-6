b268ddbf5d98d10b89d713dd4a067de4f628f69b
binary_ho <dfghcvb11@naver.com>
2023-10-25 22:50:57 +0900

    feat : 모든 객체와 협력해 Baseball Game 게임을 진행하는 BaseballGameMachine 구현

---
+++ src/main/java/baseball/BaseballGameMachine.java
@@ -0,0 +1,55 @@
+package baseball;
+
+import baseball.util.BaseballGameNumbersConvertor;
+import baseball.util.RetryInputConvertor;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+import java.util.List;
+
+public class BaseballGameMachine {
+
+    private static final int RETRY = 1;
+    private static final int EXIT = 2;
+
+    private final BaseballGameNumbers baseballGameNumbers = new BaseballGameNumbers();
+
+    public void start() {
+        OutputView.printGameStartingPrompt();
+        Grader grader = baseballGameNumbers.getRandomNumbersGrader();
+
+        while (true) {
+            List<Integer> playerNumbers = getPlayerNumbers();
+            ScoreBoard scoreBoard = grader.grade(playerNumbers);
+            OutputView.printGameResult(scoreBoard.ballCount(), scoreBoard.strikeCount());
+
+            if (isFullStrike(scoreBoard, baseballGameNumbers.getNumberSize())) {
+                OutputView.printWinningPrompt(baseballGameNumbers.getNumberSize());
+                break;
+            }
+        }
+
+        askPlayerToRetry();
+    }
+
+    private List<Integer> getPlayerNumbers() {
+        String playerNumbersInput = InputView.requestNumbersInput();
+        List<Integer> playerNumbers = BaseballGameNumbersConvertor.convertBaseballGameNumbers(
+            playerNumbersInput, baseballGameNumbers.getNumberSize());
+
+        baseballGameNumbers.validatePlayerNumbers(playerNumbers);
+        return playerNumbers;
+    }
+
+    private boolean isFullStrike(ScoreBoard scoreBoard, int numberSize) {
+        return scoreBoard.strikeCount() == numberSize;
+    }
+
+    private void askPlayerToRetry() {
+        String playerRetryInput = InputView.requestPlayerRetryInput(RETRY, EXIT);
+        int playerRetryNumber = RetryInputConvertor.convertRetryInput(playerRetryInput);
+
+        if (playerRetryNumber == RETRY) {
+            start();
+        }
+    }
+}
