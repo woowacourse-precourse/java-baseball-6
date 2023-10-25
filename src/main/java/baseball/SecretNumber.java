d13a3d7e71f0578d27e3fa3f5a29049e4838c884
Hepheir <hepheir@gmail.com>
2023-10-25 21:02:03 +0900

    Update SecretNumber to extend ImmutableList

--- src/main/java/baseball/SecretNumber.java
+++ src/main/java/baseball/SecretNumber.java
@@ -1,23 +1,11 @@
 package baseball;
 
-import java.util.AbstractList;
 import java.util.List;
 
-public class SecretNumber extends AbstractList<Digit> implements List<Digit> {
+public class SecretNumber extends ImmutableList<Digit> implements List<Digit> {
     public static final int SIZE = 3;
-    private final List<Digit> digits;
 
     public SecretNumber(List<Digit> digits) {
-        this.digits = digits;
-    }
-
-    @Override
-    public Digit get(int index) {
-        return digits.get(index);
-    }
-
-    @Override
-    public int size() {
-        return digits.size();
+        super(digits);
     }
 }
