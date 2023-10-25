6e2c088b33a7c15ec706b86eba152f7bd3c83f52
bokyung <bbookng@gmail.com>
2023-10-25 10:33:44 +0900

    [Feat] BaseballNumbers 객체 구조 완성

--- src/main/java/baseball/model/BaseballNumbers.java
+++ src/main/java/baseball/model/BaseballNumbers.java
@@ -1,4 +1,23 @@
 package baseball.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BaseballNumbers {
+    private final List<Integer> numbers;
+
+    public BaseballNumbers() {
+
+    }
+
+    public boolean containNumber(int number) {
+
+
+    }
+
+    public boolean checkStrikeNumber(int index, int number) {
+
+    }
 }
