f76f1f81a4f685265f18cbad06ab0169270ef46b
kariskan <zkfzpf56@naver.com>
2023-10-25 13:36:33 +0900

    refactor: client service를 주입받고, 사용하도록 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,11 +2,14 @@ package baseball;
 
 import baseball.domain.controller.BaseballController;
 import baseball.domain.service.BaseballService;
+import baseball.domain.service.ClientService;
 
 public class Application {
 
 	public static void main(String[] args) {
-		BaseballController baseballController = new BaseballController(new BaseballService());
+		ClientService clientService = new ClientService();
+		BaseballService baseballService = new BaseballService();
+		BaseballController baseballController = new BaseballController(clientService, baseballService);
 		baseballController.run();
 	}
 }
