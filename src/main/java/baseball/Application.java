f60cb21e51f1d7c030bad5439e52d889a71f36e8
Ha0Day <ycis5013@naver.com>
2023-10-25 20:59:27 +0900

    feat : 게임 진행하는 함수 game() 틀 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,6 +7,7 @@ public class Application {
     public static List<Integer> computer; //컴퓨터의 숫자
     public static List<Integer> player; //플레이어의 숫자
     public static boolean CONTINUE_GAME = true; //재시작, 종료 구분하는 변수
+    public static boolean SUCCESS; //게임 종료 여부 확인하는 변수
 
     public static void main(String[] args) {
 
@@ -15,6 +16,22 @@ public class Application {
         while (CONTINUE_GAME) {
             game();
         }
+    }
+
+    private static void game() {
+
+        SUCCESS = false;
+        setComputerNum(); //컴퓨터의 숫자 세팅
+
+        //숫자 3개를 다 맞힐 때까지 반복
+        while (!SUCCESS) {
+            setPlayerNum(); //플레이어의 숫자 세팅
+
+            //플레이어와 컴퓨터의 숫자 비교 로직
+
+        }
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료"); //숫자 3개 다 맞혔다면 게임 종료
+        checkRestart(); //재시작, 종료 여부 확인
     }
 
     private static void gameStart() {
