c09ba5e3ede58c627cc1b322c89397d8e0f788d8
Seotriever <122240520+Seotriever@users.noreply.github.com>
2023-10-25 23:18:44 +0900

    23:15 뷰 화면 처리 완성본

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,14 +7,14 @@ import java.util.*;
 
 public class Application {
         public static void main(String[] args) {
+                System.out.println("숫자야구 게임을 시작 합니다.");
                 StartGame();
         }
 
         public static void StartGame() {                 //게임시작
-                System.out.println("숫자야구 게임을 시작 합니다.");
                 List<String> computer = ComputerInput();
                 UserInput(computer);
-                System.out.println("computer = " + computer);           //난수 확인값
+//                System.out.println("computer = " + computer);           //난수 확인값
         }
 
         // 랜덤숫자3자리 생성 메소드  =============================================================================
@@ -26,16 +26,16 @@ public class Application {
                                 computer.add(randomNumber);
                         }
                 }
-                System.out.println("computer = " + computer);           //난수 확인값
+//                System.out.println("computer = " + computer);           //난수 확인값
                 return computer;
         }
 
         // 입력값 메소드(입력후 전달받은 computer 값과 비교후 출력) ===================================================
         public static void UserInput(List<String> computer) {
                 List<String> user = new ArrayList<>();
-                System.out.println("중복이 되지 않는 세 자리의 숫자를 입력해 주세요.");
+                System.out.print("숫자를 입력해 주세요 : ");
                 String userInput = Console.readLine();
-                System.out.println("userInput = " + userInput);
+//                System.out.println("userInput = " + userInput);
                 try {
                         for (String s : userInput.split("")) { // 입력받은 값을 배열에 나눠서 저장
                                 if (!user.contains(s)) {  // 배열에 값이 없으면
@@ -55,7 +55,7 @@ public class Application {
                 if (userInput.length() != 3) { // 입력된 수가 3자리가 아니라면 Exception 발생
                         throw new IllegalArgumentException();
                 }
-                System.out.println("user = " + user);           // 입력 확인값
+//                System.out.println("user = " + user);           // 입력 확인값
 
                 // 판정 메소드 ===================================================================================
                 int ballCount = 0;
@@ -72,15 +72,15 @@ public class Application {
                 
                 // 출력 메소드 ===================================================================================
                 if (strikeCount == 3) {
-                        System.out.println("3스트라이크!");
-                        System.out.println("3개의 숫자를 모두 맞추셨습니다! 게임 종료\n" +
+                        System.out.println("3스트라이크");
+                        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
                                 "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                         String choice = Console.readLine();           // 재시작 또는 종료를 하기 위한 입력
                         if (choice.equals("1")) {
                                 System.out.println("게임을 재시작 합니다.\n");
                                 StartGame();            //게임 재시작
                         } else if (choice.equals("2")) {
-                                System.out.println("게임을 종료 합니다.");
+//                                System.out.println("게임을 종료 합니다.");
                                 return;
                         } else {
                                 try {
