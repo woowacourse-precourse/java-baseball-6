3f252d6f6fa022492572007acf556a60cf18fbfd
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-10-25 23:13:27 +0900

    feat: 새로운 게임 시작을 위한 로직 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        User player = new User();
+        Computer computer = new Computer();
+    }
+
+    public static void startNewGame(User player, Computer comp) {
+        greetings();
+        comp.generateNumber();
+        while (true) {
+            List<Integer> inputNum = player.enterNumber();
+            comp.count(inputNum);
+            comp.result();
+
+            if (comp.isCorrect()) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                break;
+            }
+        }
     }
 }
