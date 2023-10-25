b3a794672f0cb9aa98bc74ecd4dc34fbf040f6a1
LeeSungNo-ian <isnn0103@gmail.com>
2023-10-25 19:36:41 +0900

    feat: 중복된 공 확인을 위한 validContainSameNumber 함수 생성

--- src/main/java/validator/NumberInputValidator.java
+++ src/main/java/validator/NumberInputValidator.java
@@ -1,5 +1,8 @@
 package validator;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class NumberInputValidator {
     void vaildNumberOverThreeDigits(String balls) {
         if (balls.length() > 3) {
@@ -19,5 +22,22 @@ public class NumberInputValidator {
                 throw new IllegalArgumentException("숫자가 아니거나, 범위를 넘었어요.");
             }
         }
+    }
+
+    void validContainSameNumber(String balls) {
+        if (isContainSameNumber(balls)) {
+            throw new IllegalArgumentException("같은 숫자가 포함되었어요.");
+        }
+    }
+
+    boolean isContainSameNumber(String balls) {
+        Set<Character> detailNumber =  new HashSet<>();
+        for (char ball: balls.toCharArray()) {
+            if (detailNumber.contains(ball)) {
+                return true;
+            }
+            detailNumber.add(ball);
+        }
+        return false;
     }
 }
