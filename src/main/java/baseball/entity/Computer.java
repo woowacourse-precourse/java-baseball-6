187c17ac80a209445f9d1aff4b23315252a4da29
woookk <woook27@naver.com>
2023-10-25 23:37:24 +0900

    refactor: Computer 클래스의 랜덤 숫자 생성 로직 간소화

--- src/main/java/baseball/entity/Computer.java
+++ src/main/java/baseball/entity/Computer.java
@@ -3,6 +3,7 @@ package baseball.entity;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.HashSet;
+import java.util.stream.Collectors;
 
 import static baseball.constants.Constants.*;
 
@@ -15,15 +16,11 @@ public class Computer {
     }
 
     public void generateRandomAnswer() {
-        StringBuilder answerBuilder = new StringBuilder();
         HashSet<Integer> set = new HashSet<>();
-        while (set.size() != NUMBER_LENGTH.getValue()) {
+        while (set.size() != VALID_NUMBER_LENGTH.getValue()) {
             set.add(Randoms.pickNumberInRange(MIN_RANDOM_NUMBER.getValue(), MAX_RANDOM_NUMBER.getValue()));
         }
-        for (int i : set) {
-            answerBuilder.append(i);
-        }
-        answer = answerBuilder.toString();
+        answer = String.join("", set.stream().map(String::valueOf).collect(Collectors.toList()));
     }
 
     public String getAnswer() {
