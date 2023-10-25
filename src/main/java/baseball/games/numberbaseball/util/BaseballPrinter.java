8771ba7c3829abf0a151753346d83791f9eb5886
Hae-K <bhk4392@inha.edu>
2023-10-25 16:58:28 +0900

    feat(BaseballPrinter): 느슨한 결합을 하기 위한 인터페이스와 구현체 생성

---
+++ src/main/java/baseball/games/numberbaseball/util/BaseballPrinter.java
@@ -0,0 +1,15 @@
+package baseball.games.numberbaseball.util;
+
+import baseball.GamePrinter;
+
+public class BaseballPrinter implements GamePrinter {
+    @Override
+    public void printStartingMessage() {
+
+    }
+
+    @Override
+    public void printEndingMessage() {
+
+    }
+}
