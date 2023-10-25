ed24fbe2f241343a1976ffa620b4a2930ab0ebf6
Somin Park <ps4708@naver.com>
2023-10-25 14:02:13 +0900

    feat: Player 객체 생성

---
+++ src/main/java/baseball/model/Player.java
@@ -0,0 +1,24 @@
+package baseball.model;
+
+import baseball.util.NumberValidator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+
+    private final List<Integer> playerNumber;
+
+    public Player(String inputNumber){
+        String validatedNumber = NumberValidator.getValidatedNumber(inputNumber);
+        this. playerNumber = transformPlayerNumber(validatedNumber);
+    }
+
+    private List<Integer> transformPlayerNumber(String validatedNumber) {
+        List<Integer> numbers = new ArrayList<>();
+        for(char number : validatedNumber.toCharArray()){
+            numbers.add(Character.getNumericValue(number));
+        }
+        return numbers;
+    }
+}
