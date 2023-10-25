4ee2bccfd92138268dab0b6c832fe6300a4a4ec4
cyPark95 <parkcy1007@gmail.com>
2023-10-25 18:06:39 +0900

    test: Balls 생성 테스트
    - 3자리 숫자 입력 검증
    - 서로 다른 숫자인지 검증

--- src/test/java/baseball/domain/BallsTest.java
+++ src/test/java/baseball/domain/BallsTest.java
@@ -1,6 +1,9 @@
 package baseball.domain;
 
+import static baseball.domain.ErrorCode.INVALID_BALL_SIZE;
+import static baseball.domain.ErrorCode.NOT_OTHER_NUMBERS;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +16,54 @@ class BallsTest {
     @BeforeEach
     void setUp() {
         balls = new Balls(List.of(4, 3, 7));
+    }
+
+    @Test
+    void 숫자_사이즈_검증_실패() {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3, 4);
+
+        // when
+        // then
+        assertThatThrownBy(() -> new Balls(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_BALL_SIZE.getMessage());
+    }
+
+    @Test
+    void 숫자_사이즈_검증() {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3);
+
+        // when
+        Balls result = new Balls(numbers);
+
+        // then
+        assertThat(result).isNotNull();
+    }
+
+    @Test
+    void 서로다른_숫자_검증_실패() {
+        // given
+        List<Integer> numbers = List.of(1, 1, 7);
+
+        // when
+        // then
+        assertThatThrownBy(() -> new Balls(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NOT_OTHER_NUMBERS.getMessage());
+    }
+
+    @Test
+    void 서로다른_숫자_검증() {
+        // given
+        List<Integer> numbers = List.of(7, 3, 6);
+
+        // when
+        Balls result = new Balls(numbers);
+
+        // then
+        assertThat(result).isNotNull();
     }
 
     @Test
