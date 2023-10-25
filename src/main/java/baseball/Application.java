a347e5bb11e96fe4131f2a237ae9491330a1b203
Sim-GyuBin <skb0606@gmail.com>
2023-10-25 23:27:47 +0900

    feat: 기능구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,7 +5,24 @@ import java.util.*;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
-public class Application {
+
+class User {
+    public List<Integer> inputNumbers() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+        if (!checkNumCorrect(input)) {
+            throw new IllegalArgumentException();
+        }
+        return convertInputToList(input);
+    }
+
+    private List<Integer> convertInputToList(String input) {
+        List<Integer> numbers = new ArrayList<>();
+        for (char c : input.toCharArray()) {
+            numbers.add(Character.getNumericValue(c));
+        }
+        return numbers;
+    }
 
     private static boolean checkNumCorrect(String strValue) {
         //길이가 3이 아닌 경우 여부
@@ -24,79 +41,96 @@ public class Application {
         }
     }
 
-    static int[] makeArray() {
-        System.out.print("숫자를 입력해주세요 : ");
-        String userInput = Console.readLine();
+}
 
-        if (!checkNumCorrect(userInput)) {
-            throw new IllegalArgumentException();
-        }
+class Result {
+    private final int strikes;
+    private final int balls;
 
-        //입력값 배열에 담음
-        int[] userDigits = new int[userInput.length()];
-        for (int i = 0; i < userInput.length(); i++) {
-            userDigits[i] = userInput.charAt(i) - '0';    //char형의 숫자에서 '0'의 아스키코드 값 빼서 숫자로 변환
-        }
-
-        return userDigits;
+    public Result(int strikes, int balls) {
+        this.strikes = strikes;
+        this.balls = balls;
     }
 
-    public static void main(String[] args) {
+    public boolean isGameWon() {
+        return strikes == 3;
+    }
 
-        System.out.println("숫자 야구 게임을 시작합니다.");
 
-        //게임 계속 실행
-        while (true) {
+    public void printResult() {
+        if (strikes == 0 && balls == 0)
+            System.out.println("낫싱");
+        else if (strikes == 3)
+            System.out.println("3스트라이크");
+        else if (balls == 0)
+            System.out.println(strikes + "스트라이크");
+        else if (strikes == 0)
+            System.out.println(balls + "볼");
+        else
+            System.out.println(balls + "볼 " + strikes + "스트라이크");
+    }
+}
 
-            //컴퓨터 수 생성
-            List<Integer> computerDigits = new ArrayList<>();
-            while (computerDigits.size() < 3) {
-                int randomNumber = Randoms.pickNumberInRange(1, 9);
-                if (!computerDigits.contains(randomNumber)) {
-                    computerDigits.add(randomNumber);
+class BaseballGame {
+    private final List<Integer> computerNumbers;
+
+    private List<Integer> generateNumbers() {
+        List<Integer> digits = new ArrayList<>();
+        while (digits.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!digits.contains(randomNumber)) {
+                digits.add(randomNumber);
+            }
+        }
+        return digits;
+    }
+
+    public BaseballGame() {
+        this.computerNumbers = generateNumbers();
+    }
+
+    public Result playTurn(User user) {
+        List<Integer> userNumbers = user.inputNumbers();
+        int strikes = 0;
+        int balls = 0;
+
+        for (int userIndex = 0; userIndex < 3; userIndex++) {
+            for (int comIndex = 0; comIndex < 3; comIndex++) {
+                if (userNumbers.get(userIndex).equals(computerNumbers.get(comIndex))) {
+                    if (userIndex == comIndex)
+                        strikes += 1;
+                    else
+                        balls += 1;
                 }
             }
+        }
+        return new Result(strikes, balls);
+    }
+}
 
+public class Application {
+    public static void main(String[] args) {
+        while (true) {
+            BaseballGame game = new BaseballGame();
+            User user = new User();
+
+            System.out.println("숫자 야구 게임을 시작합니다.");
             while (true) {
-                //유저의 수 입력
-                int[] userDigits = makeArray();
+                Result result = game.playTurn(user);
+                result.printResult();
 
-                int strike = 0;
-                int ball = 0;
-                //비교 반복문
-                for (int userIndex = 0; userIndex < 3; userIndex++) {
-                    for (int comIndex = 0; comIndex < 3; comIndex++) {
-                        if (userDigits[userIndex] == computerDigits.get(comIndex)) {
-                            if (userIndex == comIndex)
-                                strike += 1;
-                            else
-                                ball += 1;
-                        }
-                    }
-                }
-
-                if (strike == 0 && ball == 0) {
-                    System.out.println("낫싱");
-                } else if (ball == 0) {
-                    System.out.println(strike + "스트라이크");
-                } else {
-                    System.out.println(ball + "볼 " + strike + "스트라이크");
-                }
-
-                if (strike == 3) {
+                if (result.isGameWon()) {
                     System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                     break;
                 }
             }
 
-
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-
             String choice = Console.readLine();
             if (choice.equals("2")) {
                 break;
             }
-
         }
     }
 }
+
