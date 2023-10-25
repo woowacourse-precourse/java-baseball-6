fb8e2b7c7b8983c7574118c02d68fcf00f589c07
ohjinseo <ojs835@naver.com>
2023-10-25 23:16:13 +0900

    feat : 메인 메서드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -103,6 +103,11 @@ public class Application {
 
 
     public static void main(String[] args) {
-
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (true) {
+            if (!playGame()) {
+                break;
+            }
+        }
     }
 }
