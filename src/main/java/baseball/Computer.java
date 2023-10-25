f1d2e843dda0fe5a049255456ff8ad2e32756438
Junobee25 <wnsdhqo@naver.com>
2023-10-25 12:56:06 +0900

    feat: 컴퓨터 숫자 정보를 담을 클래스 생성

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,15 @@
+package baseball;
+
+import java.util.List;
+
+public class Computer {
+    private List<Integer> computerNumber;
+
+    public List<Integer> getComputerNumber() {
+        return computerNumber;
+    }
+
+    public void setComputerNumber(List<Integer> computerNumber) {
+        this.computerNumber = computerNumber;
+    }
+}
