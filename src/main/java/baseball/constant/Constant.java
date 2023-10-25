f6befef77c65ec33a0aacc859656244c28b37e2e
Hugh <lee2317884@gmail.com>
2023-10-25 18:43:35 +0900

    style: codeStyle 변경

--- src/main/java/baseball/constant/Constant.java
+++ src/main/java/baseball/constant/Constant.java
@@ -1,6 +1,7 @@
 package baseball.constant;
 
 public class Constant {
+    
     public static final String GAME_RESTART_NUMBER = "1";
     public static final String GAME_END_NUMBER = "2";
     public static final int LENGTH_LIMIT = 3;
