124cad0b487ebb683d23e89ecdc3dd6c14d92086
shinel98 <75024473+shinel98@users.noreply.github.com>
2023-10-25 18:47:02 +0900

    refactor: modify the code to adhere to the Google Java Style Convention

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,13 +1,6 @@
 package baseball;
 
 import baseball.Controller.BaseballGameController;
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 
 public class Application {
     public static void main(String[] args) {
