c861a48083e8ab248f8493cec5bd83b6bd6b010e
Jedo0224 <qkrwotjr0@gmail.com>
2023-10-25 22:05:19 +0900

    [refactor] GameController 클래스 내부의 getNumberFromUser 메소드 분리.

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,61 +1,64 @@
 package baseball.controller;
-
 import baseball.model.ComputerNumber;
 import baseball.model.NumberComparator;
 import baseball.util.Result;
 import baseball.vailidation.NumberValidator;
 import baseball.view.InputView;
 import baseball.view.OutputView;
-
 import java.util.List;
-
 
 public class GameController {
 
     private final ComputerNumber computer;
-
 
     public GameController() {
         computer = new ComputerNumber();
     }
 
     public void start() {
+
         Result result;
         do {
-            System.out.println(computer.getNumbers());
             result = getNumberCompare();
             printCount(result);
         } while (!isGameSet(result));
-
     }
 
-    public List<String> getNumberFromUser(){
-        List<String> VaildateNumber = InputView.UserNumbertoList();
+    public List<String> getNumberFromUser() {
 
-        NumberValidator.checkLength(VaildateNumber);
-        NumberValidator.checkRedundancy(VaildateNumber);
-        NumberValidator.checkZero(VaildateNumber);
+        List<String> userInputNumbers = getUserInputAsList();
+        validateUserInputNumbers(userInputNumbers);
+        return userInputNumbers;
+    }
 
-        return VaildateNumber;
+    private List<String> getUserInputAsList() {
+
+        return InputView.UserNumbertoList();
+    }
+
+    private void validateUserInputNumbers(List<String> numbers) {
+
+        NumberValidator.checkLength(numbers);
+        NumberValidator.checkRedundancy(numbers);
+        NumberValidator.checkZero(numbers);
     }
 
     public Result getNumberCompare(){
-        return NumberComparator.compareNumber(computer.getNumbers(),getNumberFromUser());
 
+        return NumberComparator.compareNumber(computer.getNumbers(),getNumberFromUser());
     }
 
     public void printCount(Result count){
+
         OutputView.printResultMessage(count.ball, count.strike);
     }
 
-
-
     public boolean isGameSet(Result count) {
+
         if(count.strike == 3){
             OutputView.printGameSetMessage();
             computer.resetNumbers();
         }
         return true;
     }
-
 }
