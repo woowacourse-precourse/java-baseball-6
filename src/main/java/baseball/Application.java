f0ea47285a5f50388a82c0e84b312f7989f1e069
Project-Longinus <Team.Project.Longinus@gmail.com>
2023-10-25 22:55:58 +0900

    끝이 보인다ㅏㅏㅏ!!!

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,7 +2,6 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) throws IllegalArgumentException {
@@ -10,6 +9,9 @@ public class Application {
         String inputNumber;
         int[] inputNumber2Array = new int[3];
         int[] randomNumber = new int[3];
+        int strikes = 0;
+        int balls = 0;
+
         for (int i = 0; i < 3; i++) {
             randomNumber[i] = Randoms.pickNumberInRange(1, 9);
         }
@@ -27,8 +29,35 @@ public class Application {
             inputNumber2Array[i] = inputNumber.charAt(i) - '0';
         }
 
-        System.out.println(Arrays.toString(randomNumber));
-        System.out.println(Arrays.toString(inputNumber2Array));
-    }
-}
+        for (int i = 0; i < 3; i++) {
+            System.out.print(inputNumber2Array[i]+" ");
+            System.out.println(randomNumber[i]);
+            if (inputNumber2Array[i] == randomNumber[i]) {
+                strikes++;
+            } else if (contains(randomNumber, inputNumber2Array[i])) {
+                balls++;
+            }
+        }
 
+        if (strikes > 0 && balls == 0) {
+            System.out.println(strikes + "스트라이크");
+        } else if (balls > 0 && strikes == 0) {
+            System.out.println(balls + "볼");
+        } else if (strikes > 0 && balls > 0){
+            System.out.print(balls + "볼 ");
+            System.out.println(strikes + "스트라이크");
+        } else {
+            System.out.print("낫싱");
+        }
+    }
+
+    private static boolean contains(int[] array, int num) {
+        for (int i : array) {
+            if (i == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
