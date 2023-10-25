e809292afeb62a8b7aa1789971522f58895a5964
gourderased <gourderased@gmail.com>
2023-10-25 22:06:25 +0900

    Feat: "User 클래스 구현"
    - 유저 입력받기
    - 예외 추가
    - 유저의 숫자를 가져오는 메소드 추가

---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,22 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class User {
+
+    private String userNumber;
+
+    public void setUserNumbers() {
+        System.out.println("숫자를 입력해주세요 : ");
+        String userNumber = Console.readLine();
+        //3자리, 중복, 0여부 확인
+        if (userNumber.matches("^(?!.*(.).*\\1)[1-9]{3}$")) {
+            throw new IllegalArgumentException("잘못된 숫자 입력입니다.");
+        }
+        this.userNumber = userNumber;
+    }
+
+    public String getUserNumber() {
+        return userNumber;
+    }
+}
