5036426a9f75cfdc94cbd4603f507dea36daf47c
mistarson <yeowuli2@naver.com>
2023-10-25 11:59:11 +0900

    Refactor: 테스트 실행을 위한 테스트 순서 변경
    잘못된 입력에 대한 테스트인 assertSimpleTest의 경우 1초의 시간이 지나게 되면 테스트 실패로 간주하기 때문에 시간이 오래걸리는 실제 야구 게임을 테스트를 제일 나중 순서로 변경

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -6,22 +6,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ApplicationTest extends NsTest {
 
     @Test
-    void 게임종료_후_재시작() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("246", "135", "1", "597", "589", "2");
-                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
-                },
-                1, 3, 5, 5, 8, 9
-        );
-    }
-
-    @Test
+    @Order(1)
     void 예외_테스트_길이() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
@@ -30,6 +24,7 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    @Order(2)
     void 예외_테스트_숫자가아닌입력() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException(".qw"))
@@ -38,10 +33,23 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    @Order(3)
     void 예외_테스트_0인숫자() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("105"))
                         .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @Order(4)
+    void 게임종료_후_재시작() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("246", "135", "1", "597", "589", "2");
+                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+                },
+                1, 3, 5, 5, 8, 9
         );
     }
 
