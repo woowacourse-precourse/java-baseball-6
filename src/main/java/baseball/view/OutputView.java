a5510ee46f94981aa248999e71dc9ae3b7ebfbd6
gundorit <packdev937@gmail.com>
2023-10-25 14:00:48 +0900

    feat (OutputView) : 하나도 못 맞췄을 경우 낫싱을 출력한다

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,11 +1,15 @@
 package baseball.view;
 
+import baseball.response.GameResponse;
+
 public class OutputView {
 
     public static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     public static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
     public static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     public static final String GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String NOTHING_MESSAGE = "낫싱\n";
+
 
     public static void printGameStart() {
         System.out.println(GAME_START_MESSAGE);
@@ -21,5 +25,11 @@ public class OutputView {
 
     public static void printGameRestart() {
         System.out.println(GAME_RESTART_MESSAGE);
+    }
+
+    private static void printNothing(GameResponse response) {
+        if (response.hasNoStrike() && response.hasNoBall()) {
+            System.out.print(NOTHING_MESSAGE);
+        }
     }
 }
