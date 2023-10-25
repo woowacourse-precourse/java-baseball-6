662b0f752cc9851211de881c753cc4d3e7b694cb
realcold0 <realcold0@gmail.com>
2023-10-25 16:52:24 +0900

    refactor : 게임의 재시작 및 플레이어 숫자 재입력 flow 수정

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -14,18 +14,20 @@ public class GameController {
     private CompareAnswerWithPlayerNumber compareAnswerWithPlayerNumber = new CompareAnswerWithPlayerNumber();
     private ResultView resultView = new ResultView();
     private RegameView regameView = new RegameView();
+    private boolean regame = true;
 
     public void startGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        computer.setComputerAnswer();  //게임 정답 세팅
         do {
-            System.out.println("숫자 야구 게임을 시작합니다.");
-            computer.setComputerAnswer();  //게임 정답 세팅
             player.setPlayerAnswer(inputView.inputPlayerNumbers());//플레이어의 정답 작성
             compareAnswerWithPlayerNumber.compareStrikeBall(player.getPlayerAnswer(),
                     computer.getComputerAnswer());//플레이어 정답, 게임정답 비교
-            resultView.outputCompareResult(compareAnswerWithPlayerNumber);//게임 결과 확인
-        } while (regameView.inputRegame() == 1);
-
+            resultView.outputCompareResult(compareAnswerWithPlayerNumber);
+            if (compareAnswerWithPlayerNumber.getStrike() == 3) {
+                regame = regameView.inputRegame();
+                computer.setComputerAnswer();  //게임 정답 세팅
+            }
+        } while (regame);
     }
-
-
 }
--- src/main/java/baseball/view/RegameView.java
+++ src/main/java/baseball/view/RegameView.java
@@ -4,11 +4,20 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class RegameView {
     private final String REGAME_MENT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-    private int regame;
+    private boolean regame;
 
-    public int inputRegame() {
+    public RegameView() {
+        regame = false;
+    }
+
+    public boolean inputRegame() {
         System.out.println(REGAME_MENT);
-        regame = Integer.parseInt(Console.readLine());
+        int regameInt = Integer.parseInt(Console.readLine());
+        if (regameInt == 1) {
+            regame = true;
+        } else if (regameInt == 2) {
+            regame = false;
+        }
         return regame;
     }
 }
