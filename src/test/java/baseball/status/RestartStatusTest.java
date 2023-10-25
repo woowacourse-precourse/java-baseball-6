e23d74d37e642482d2313d19bbd5e50879bab4b6
Jiyoon Kim <rlawldbs0911@naver.com>
2023-10-25 20:42:28 +0900

    refactor(RestartStatusTest): 메소드명 오타 수정

--- src/test/java/baseball/status/RestartStatusTest.java
+++ src/test/java/baseball/status/RestartStatusTest.java
@@ -8,7 +8,7 @@ class RestartStatusTest {
 
     @Test
     @DisplayName("재시작 여부 입력값이 1 또는 2가 아니면 예외가 발생한다.")
-    void playerNumbersTest1() {
+    void restartStatusTest1() {
         int input = 3;
 
         Assertions.assertThrows(IllegalArgumentException.class, () -> RestartStatus.getStatus(input));
@@ -16,7 +16,7 @@ class RestartStatusTest {
 
     @Test
     @DisplayName("재시작 여부 입력값이 1이면 재시작이다.")
-    void playerNumbersTest2() {
+    void restartStatusTest2() {
         int input = 1;
 
         Assertions.assertEquals(RestartStatus.getStatus(input), RestartStatus.RESTART);
@@ -24,7 +24,7 @@ class RestartStatusTest {
 
     @Test
     @DisplayName("재시작 여부 입력값이 2이면 종료이다.")
-    void playerNumbersTes32() {
+    void restartStatusTest3() {
         int input = 2;
 
         Assertions.assertEquals(RestartStatus.getStatus(input), RestartStatus.EXIT);
