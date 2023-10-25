d6cf7b1b6d0e1bcd5ebfce462276e1b6e1d30a3c
Hercules <hyukkyulee@alumni.emory.edu>
2023-10-25 11:45:29 +0900

    chore: application

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import static baseball.io.ErrorHandler.checkRepeat;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        PlayBall playBall = new PlayBall();
+        playBall.run();
     }
 }
