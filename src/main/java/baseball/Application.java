1434709b21da9260a583320ca2302a9388f15fe5
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-10-25 17:31:21 +0900

    fix(Application) : fix Application to run Game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,8 @@
 package baseball;
 
-import baseball.baseballGame.BaseballGame;
-import baseball.game.UserController;
-
 public class Application {
     public static void main(String[] args) {
-        BaseballGame baseballGame = new BaseballGame();
-        UserController userController = new UserController(baseballGame);
-        userController.run();
+        GamePlayer baseballGamePlayer = new BaseballGamePlayer();
+        baseballGamePlayer.run();
     }
 }
