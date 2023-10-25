0d907068210b26f26c51ec0d304c88fcdd40d542
c1ce <chalicenuclear@gmail.com>
2023-10-25 14:26:21 +0900

    test : test코드 연습코드 작성

---
+++ src/test/java/baseball/StudyTest.java
@@ -0,0 +1,16 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+public class StudyTest extends NsTest {
+    @Test
+    void test(){
+
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
