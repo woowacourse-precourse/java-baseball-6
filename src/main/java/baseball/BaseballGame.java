bc7be13a12da7f14d60a14d8dd16bc6d93af78e5
inwoo <korean5078@gmail.com>
2023-10-25 20:17:31 +0900

    refactor: java convention에 맞게 코드 수정 1. Javadoc 작성 2. BaseballGame의 멤버 변수 따로 선언 3. Main 함수 변수 위로 가게 끔 수정

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -4,14 +4,22 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 숫자 야구 게임 객체의 클래스
+ *
+ * <p>게임 시작 시 랜덤 세자리 숫자 생성,
+ * 게임 시작 및 사용자 숫자와 비교 및 결과 출력하는 함수 구현</p>
+ */
 public class BaseballGame {
 	private static List<Integer> randomNumber;
-	private int ball, strike;
+	private int ball;
+	private int strike;
 
 	BaseballGame() {
 		makeRandomNumber();
 	}
 
+	/** 랜덤 숫자 3자리 생성 */
 	private static void makeRandomNumber() {
 		randomNumber = new ArrayList<>();
 
@@ -23,10 +31,16 @@ public class BaseballGame {
 		}
 	}
 
+	/** 3자리 랜덤 숫자를 반환 */
 	public List<Integer> getRandomNumber() {
 		return randomNumber;
 	}
 
+	/**
+	 *  int형 3자리 사용자 숫자를 배열로 반환
+	 * @param userNumber
+	 * @return userNumber in array
+	 */
 	private int[] userNumberToArray(int userNumber) {
 		int[] arr = new int[3];
 
@@ -37,6 +51,11 @@ public class BaseballGame {
 		return arr;
 	}
 
+	/**
+	 * 배열에 있는 3자리 사용자 숫자와 랜덤 숫자 비교 후 볼, 스트라이크 카운트
+	 * @param i
+	 * @param userNumbers
+	 */
 	private void compare(int i, int[] userNumbers) {
 		if (randomNumber.contains(userNumbers[i])) {
 			ball++;
@@ -47,6 +66,7 @@ public class BaseballGame {
 		}
 	}
 
+	/** 게임의 볼과 스트라이크 횟수 출력 */
 	private void printScore() {
 		if (strike == 0 && ball == 0) {
 			System.out.println("낫싱");
@@ -66,10 +86,15 @@ public class BaseballGame {
 		}
 	}
 
+	/** 숫자 묻는 질문 출력 */
 	private void printNumberQuestion() {
 		System.out.print("숫자를 입력해주세요 : ");
 	}
 
+	/**
+	 * 게임의 전체적인 함수를 실행하는 함수
+	 * @param user
+	 */
 	public void gameStart(User user) {
 		while (strike != 3) {
 			int[] userNumbers = userNumberToArray(user.input3DigitNumber());
