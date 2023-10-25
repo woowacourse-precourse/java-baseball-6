cfcec6c421d25794a1875c5f3baa10bc60ab8d71
Hyun Woo <hwsa10041@gmail.com>
2023-10-25 13:22:26 +0900

    test : 게임 재시작 번호 선택에 대한 Validate 테스트 작성

--- src/test/java/baseball/view/InputViewTest.java
+++ src/test/java/baseball/view/InputViewTest.java
@@ -3,9 +3,6 @@ package baseball.view;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class InputViewTest {
@@ -22,5 +19,16 @@ class InputViewTest {
         assertThatThrownBy(() -> inputView.validateThreeDigitNumber(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 3자리의 숫자를 입력하여야 합니다.");
+    }
+
+    @DisplayName("재시작 여부를 선택할 때 1 또는 2 가 아니면 Exception 발생한다.")
+    @Test
+    void validateIsOneOrTwo() {
+        // given
+        int choice = 3;
+        // when // then
+        assertThatThrownBy(() -> inputView.validateIsOneOrTwo(choice))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 1 또는 2의 숫자만 선택해야 합니다.");
     }
 }
