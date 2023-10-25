a9d1f24decd3cd49ccf3bb4b50012e2e9f5a6d19
doka <posmane5@gmail.com>
2023-10-25 19:15:54 +0900

    feat(Error) : Application 클래스 내부 checkValidity 메소드와 isNumeric 추가 isNumeric : 문자열이 숫자형태인지 확인 checkValidity : 입력된 숫자가 올바른 형태인지 확인

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -26,7 +26,13 @@ public class Application {
             while (!result.equals("3스트라이크")) {
                 // 입력, 문자열 리스트로 변환
                 System.out.print("숫자를 입력해주세요 : ");
-                userNumbers = makeUserNumber(Console.readLine());
+                String inputNumber = Console.readLine();
+
+                // 사용자가 잘못된 값을 입력 했는지 확인
+                checkValidity(inputNumber);
+
+                userNumbers = makeUserNumber(inputNumber);
+
                 baseballConfirm.takeNumbers(userNumbers);
                 result = baseballConfirm.referee();
                 System.out.println(result);
@@ -36,7 +42,15 @@ public class Application {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            restartToggle = Integer.parseInt(Console.readLine());
+            String restartInput = Console.readLine();
+
+            // 사용자가 1,2가 아닌 잘못된 값을 입력 했는지 확인
+            if (restartInput.equals("1") || restartInput.equals("2")) {
+                restartToggle = Integer.parseInt(restartInput);
+            } else {
+                throw new IllegalArgumentException("1 또는 2를 입력해주세요");
+            }
+
         }
 
 
@@ -50,6 +64,37 @@ public class Application {
         for (char numChar : userInput.toCharArray()) {
             UserNumbers.add(Character.getNumericValue(numChar));
         }
+
         return UserNumbers;
     }
+
+    public static void checkValidity(String value) {
+        //입력된 숫자가 3개인지 확인
+        int valueSize = value.length();
+        if (!(valueSize == 3)) {
+            throw new IllegalArgumentException("3개의 수를 입력해 주세요");
+        } else if (!isNumeric(value)) {
+            throw new IllegalArgumentException("숫자가 아닌 값을 입력하지 마세요.");
+        }
+        //겹치는값이 없는지 확인
+        Set<Character> charSet = new HashSet<>();
+        for (char ch : value.toCharArray()) {
+            // charSet에 이미 값이 있다면 서로다른 수가 아님
+            if (charSet.contains(ch)) {
+                throw new IllegalArgumentException("1~9까지의 서로 다른수 를 입력해주세요.");
+            }
+            charSet.add(ch);
+        }
+    }
+
+    // 문자열이 숫자 형태인지 판별하는 메소드.
+    public static boolean isNumeric(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
 }
