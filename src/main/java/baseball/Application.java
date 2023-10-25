5ef0edf5eb2c2266db1097ddcec8706976de1b60
kssumin <201566@jnu.ac.kr>
2023-10-25 17:56:57 +0900

    chore : 필요없는 주석 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,9 @@
 package baseball;
 
 public class Application {
-    public static void main(String[] args) {
+
+	public static void main(String[] args) {
 		Game game = new Game();
 		game.start();
-    }
+	}
 }
