79c66f028ec722d80cdf3676c320c76e3351a9ad
siyeonSon <siyeon0209@naver.com>
2023-10-25 13:31:53 +0900

    test: 같은 수가 다른 자리에 있으면 볼이다

--- src/test/java/baseball/RefereeTest.java
+++ src/test/java/baseball/RefereeTest.java
@@ -1,30 +1,34 @@
 package baseball;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 class RefereeTest {
-    @Test
-    public void 같은_수가_다른_자리에_있으면_볼이다() {
-        List<Integer> computer = List.of(1, 2, 3);
-        List<Integer> number = List.of(2, 4, 5);
+    final Referee referee = new Referee();
+    final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    final PrintStream standardOut = System.out;
 
-        boolean result = Referee.judge(computer, number);
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
 
-        assertThat(result).isEqualTo(true);
+    @AfterEach
+    void tearDown() {
+        System.setOut(standardOut);
     }
 
     @Test
-    public void 같은_수가_같은_자리에_있으면_스트라이크이다() {
-        List<Integer> computer = List.of(1, 2, 3);
-        List<Integer> number = List.of(1, 4, 3);
-
-        boolean result = Referee.judge(computer, number);
-
-        assertThat(result).isEqualTo(true);
+    void 같은_수가_다른_자리에_있으면_볼이다() {
+        referee.judge(List.of(1, 2, 3), List.of(2, 4, 5));
+        assertThat(outputStreamCaptor.toString().trim())
+                .isEqualTo("1볼");
     }
 }
