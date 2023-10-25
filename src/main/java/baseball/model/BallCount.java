3b7f8bac82e851649e73dfe9fafe5e9cec387527
beomsic <qjatjr29@khu.ac.kr>
2023-10-25 13:50:05 +0900

    :sparkles: feat : 숫자 야구의 결과인 BallCount 에서 직접 숫자들을 비교하고 결과를 생성하도록 로직 추가

--- src/main/java/baseball/model/BallCount.java
+++ src/main/java/baseball/model/BallCount.java
@@ -1,5 +1,43 @@
 package baseball.model;
 
-public record BallCount(int strike, int ball) {
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
 
+public class BallCount {
+
+    private final int strike;
+    private final int ball;
+
+    private BallCount(final BaseBallNumber computerNumber, final BaseBallNumber inputNumber) {
+
+        AtomicInteger strikeCount = new AtomicInteger(0);
+        AtomicInteger ballCount = new AtomicInteger(0);
+
+        calculateStrikeAndBall(computerNumber, inputNumber, strikeCount, ballCount);
+
+        this.strike = strikeCount.get();
+        this.ball = ballCount.get();
+    }
+
+    public static BallCount calculate(BaseBallNumber computerNumber, BaseBallNumber inputNumber) {
+        return new BallCount(computerNumber, inputNumber);
+    }
+
+    private void calculateStrikeAndBall(BaseBallNumber computerNumber,
+                                               BaseBallNumber inputNumber,
+                                               AtomicInteger strike,
+                                               AtomicInteger ball) {
+
+        computerNumber.compare((computerNum, computerIndex) ->
+                inputNumber.compare((inputNum, inputIndex) -> {
+                    if(!Objects.equals(computerNum, inputNum)) return;
+                    if(computerIndex.equals(inputIndex)) {
+                        strike.getAndIncrement();
+                    }
+                    else {
+                        ball.getAndIncrement();
+                    }
+                })
+        );
+    }
 }
