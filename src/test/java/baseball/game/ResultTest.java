654370bdb25afccc08189cba0835703cb6bd269d
itsmo1031 <itsmo1031@gmail.com>
2023-10-25 13:39:49 +0900

    :ambulance: hotfix(game): fix Ball result calculate logic
    calculate 메서드에서 index를 탐색할 때, 0 초과 인덱스에 대해서 ball의 수를 더하고 있어 0번 인덱스가 탐색되었을 때 볼로 체크되지 않는 이슈가 발생하여 급히 수정함.

--- src/test/java/baseball/game/ResultTest.java
+++ src/test/java/baseball/game/ResultTest.java
@@ -1,13 +1,17 @@
 package baseball.game;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import baseball.Application;
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
-public class ResultTest {
+public class ResultTest extends NsTest {
     @Test
     void 스트라이크만_있을_때() {
         // given
@@ -66,5 +70,21 @@ public class ResultTest {
                 () -> assertEquals(result.toString(), message),
                 () -> assertTrue(result.isWin())
         );
+    }
+
+    @Test
+    void 결과_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("123", "491", "2");
+                    assertThat(output()).contains("1볼", "3스트라이크");
+                },
+                4, 9, 1
+        );
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
     }
 }
