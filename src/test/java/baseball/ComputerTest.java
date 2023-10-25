d64ff61c6d0a46fa44843ef5b4b1d0d4fde77263
jsyeo97 <jsyeo97@naver.com>
2023-10-25 18:53:28 +0900

    refactor: 생성된 난수에 대한 단위 테스트를 생성된 난수의 길이, 중복, 범위 단위 테스트로 분리

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -10,10 +10,20 @@ class ComputerTest {
     Computer computer = new Computer(3);
 
     @Test
-    void createRandomNumsTest() {
+    void randomNumsLengthTest() {
         List<Integer> randomNums = computer.getNums();
         assertThat(randomNums.size()).isEqualTo(3);
+    }
+
+    @Test
+    void randomNumsDuplicateTest() {
+        List<Integer> randomNums = computer.getNums();
         assertThat(randomNums.stream().distinct().count()).isEqualTo(3);
+    }
+
+    @Test
+    void randomNumsRangeTest() {
+        List<Integer> randomNums = computer.getNums();
         assertThat(randomNums.stream().filter(num -> num >= 1 && num <= 9).count()).isEqualTo(3);
     }
 }
