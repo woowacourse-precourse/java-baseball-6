ff2f44126539d7799c8148b050e724829fae1b48
CJ <choijun0627@konkuk.ac.kr>
2023-10-25 09:42:49 +0900

    Fix : 결과를 출력하는 기능 수정
    입력한 수에 대한 결과를 볼, 스트라이크 개수로 출력하는 기능인
    void outputBaseball(int, int)에서 스트라이크만 있거나 볼만 있는 경우에
    대한 출력을 추가
    그리고 스트라이크와 볼 같이 있는 경우 스트라이크 개수도 볼의 개수에
    포함이 되어서 볼에서 스트라이크만큼 빼주는 로직 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -80,7 +80,12 @@ public class Application {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         } else if ((strike == 0) && (ball == 0)) {
             System.out.println("낫싱");
+        } else if ((strike == 0) && (ball != 0)) {
+            System.out.println(ball + "볼");
+        } else if ((strike != 0) && (ball == 0)) {
+            System.out.println(strike + "스트라이크");
         } else {
+            ball -= strike;
             System.out.println(ball + "볼 " + strike + "스트라이크");
         }
     }
