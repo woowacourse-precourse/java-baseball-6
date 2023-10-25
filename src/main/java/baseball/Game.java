b6b68caf4b299c6047c7a97a319b39bab0457700
cyzlcyzl <cyzlcyzl09@gmail.com>
2023-10-25 13:54:24 +0900

    refactor: 메시지 클래스 생성하여 별도 관리 및 메시지 오류 수정

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -7,11 +7,12 @@ import java.util.List;
 public class Game {
 
     private Constant constant;
+    private Messages messages;
 
     public void run(){
         while(true){
             play();
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            System.out.println(messages.EXIT_MESSAGE);
             int input = Integer.parseInt(Console.readLine());
             if(input == constant.RESTART_NUM) continue;
             else if(input == constant.EXIT_NUM) break;
@@ -26,17 +27,13 @@ public class Game {
         List<Integer> user = player.input();
         while(true){
             BallCount ballCount = computer.calBallCount(answer, user);
-            int strike = ballCount.getStrike();
-            int ball = ballCount.getBall();
+            System.out.println(Messages.hintMessage(ballCount));
 
+            int strike = ballCount.getStrike();
             if(strike==3) {
-                System.out.println(String.format("%d스트라이크",strike));
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println(Messages.CORRECT_MESSAGE);
                 break;
             }
-            else if (strike == 0 && ball == 0) System.out.println("낫싱");
-            else System.out.println(String.format("%d볼 %d스트라이크", ball, strike));
-
             user = player.input();
         }
     }
