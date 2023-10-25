fc7d961dd42b0a984f28971bd50c93faa6b3874e
strongcookdas <goodjav141592@gmail.com>
2023-10-25 18:37:27 +0900

    feat : 플레이어가 입력한 숫자열 유효성 체크 기준 - 3자리인지 - 숫자이외의 문자가 있는지 - 중복된 숫자를 입력했는지 - 범위내 숫자를 입력했는지

--- src/main/java/baseball/service/Player.java
+++ src/main/java/baseball/service/Player.java
@@ -1,5 +1,24 @@
 package baseball.service;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Player {
 
+    private final String REGEX = "[1-9]+";
+
+    public boolean isRandomNumbers(String input) {
+
+        Set<Character> charSet = new HashSet<>();
+
+        for (char c : input.toCharArray()) {
+            charSet.add(c);
+        }
+
+        if (!(charSet.size() == 3) || !(input.length() == 3) || !input.matches(REGEX)) {
+            return false;
+        }
+
+        return true;
+    }
 }
