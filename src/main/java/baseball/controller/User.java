502aad008a878d47e2f596f50e0139b08e0b5d83
“sangjun121” <juncho12011201@gmail.com>
2023-10-25 22:08:44 +0900

    feat: 게임 재실행 여부 입력받는 함수 구현
    readUserPauseOrResumeInput()함수를 구현하여 종료 여부를 반환하는 기능 추가

--- src/main/java/baseball/controller/User.java
+++ src/main/java/baseball/controller/User.java
@@ -6,8 +6,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static baseball.controller.InputException.judgeValidNumberInput;
+import static baseball.controller.InputException.judgeValidPauseOrResumeInput;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static baseball.utility.Constants.NUMBER_LIMIT_LENGTH;
+import static baseball.utility.Constants.PAUSE_GAME;
 
 public class User {
     private List<Integer> userNumbers;
@@ -33,6 +35,21 @@ public class User {
         userNumberDto.setUserNumber(userNumbers); //model DTO에 담아 다른 클래스로 넘겨주기
 
         return userNumberDto;
+    }
+
+    public boolean readUserPauseOrResumeInput(){
+        boolean isPauseGame;
+
+        userInput = readLine();
+        judgeValidPauseOrResumeInput(userInput);
+
+        if(userInput.equals(PAUSE_GAME)){ //2 입력시 게임 종료
+            isPauseGame = true;
+            return isPauseGame;
+        }
+
+        isPauseGame = false; //1 입력시 게임 재실행
+        return isPauseGame;
     }
 
     private void clearUserNumbersList(){
