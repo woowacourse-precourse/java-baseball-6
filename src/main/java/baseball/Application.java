a71aad6e69fd090d88f9a41a8c6a3e11b387719c
bohyeon <980926a@gmail.com>
2023-10-25 15:55:55 +0900

    fix: 출력문 변경 및 예외처리 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,9 +3,9 @@ import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-
-import static camp.nextstep.edu.missionutils.Console.readLine;
+import java.util.Set;
 
 public class Application {
     public static void main(String[] args) throws IllegalArgumentException{
@@ -28,9 +28,7 @@ public class Application {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         char answer = Console.readLine().charAt(0);
-        if (answer == '1') {
-            restart = true;
-        };
+        if (answer == '1') restart = true;
         return restart;
     }
 
@@ -44,16 +42,23 @@ public class Application {
         }
         return computer;
     }
-    public static void exceptionLength(String input) {
+    public static void exceptionLength(String input) throws IllegalArgumentException{
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < input.length(); i++) {
+            set.add(input.charAt(i));
+        }
+        if (set.size() != input.length()) {
+            throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.(중복된 숫자)");
+        }
         if (input.length() != 3) {
             throw new IllegalArgumentException("세자리 수보다 크거나 작음");
         }
     }
-    public static void baseballGame(List<Integer> ans) {
+    public static void baseballGame(List<Integer> ans) throws IllegalArgumentException{
         String result = "";
         while(!result.equals("3스트라이크")){
             // (1) 숫자 입력
-            System.out.println("숫자를 입력해주세요 : ");
+            System.out.print("숫자를 입력해주세요 : ");
             List<Integer> user = new ArrayList<>();
             String input = Console.readLine();
             for (String i : input.split("")) {
@@ -90,6 +95,6 @@ public class Application {
         } else if (ballCount == 0) {
             return strikeCount + "스트라이크";
         }
-        return ballCount + "볼" + strikeCount + "스트라이크";
+        return ballCount + "볼 " + strikeCount + "스트라이크";
     }
 }
