2501c8c8e922a77b4ad09ff6c70433cf97816e23
swhyeon98 <smreo75@gmail.com>
2023-10-25 22:40:04 +0900

    fix: 게임을 새로 시작할 때, 컴퓨터 난수가 새로 생성되도록 변경

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -39,8 +39,7 @@ public class Game {
         Umpire umpire = new Umpire(computer.getNumbers(), player.getNumbers());
         if (umpire.isGameSet()) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            computer.generateRandomNumbers();
-            if (!restartOrExitGame()){
+            if (!restartOrExitGame()) {
                 return true;
             }
         }
@@ -51,8 +50,13 @@ public class Game {
         System.out.println("게임을 새로 시작하려면 " + RESTART_KEYWORD + ", 종료하려면 " + EXIT_KEYWORD + "를 입력하세요.");
         String playerChoice = Console.readLine();
 
-        if (RESTART_KEYWORD.equals(playerChoice)) return true;
-        if (EXIT_KEYWORD.equals(playerChoice)) return false;
+        if (RESTART_KEYWORD.equals(playerChoice)){
+            computer.generateRandomNumbers();
+            return true;
+        }
+        if (EXIT_KEYWORD.equals(playerChoice)){
+            return false;
+        }
 
         throw new IllegalArgumentException(RESTART_KEYWORD + " 또는 " + EXIT_KEYWORD + " 중 하나만 선택하세요.");
     }
