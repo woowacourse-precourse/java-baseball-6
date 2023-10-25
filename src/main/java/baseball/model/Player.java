b6ad03e7a5881a3942088fa6e318b1d586bfa344
DDing77 <devseo2021@gmail.com>
2023-10-25 10:46:50 +0900

    feat: Player 클래스 구현

---
+++ src/main/java/baseball/model/Player.java
@@ -0,0 +1,16 @@
+package baseball.model;
+
+import java.util.List;
+
+public class Player {
+
+    private List<Integer> playerNumber;
+
+    public List<Integer> getPlayerNumber() {
+        return playerNumber;
+    }
+
+    public void setPlayerNumber(List<Integer> playerNumber) {
+        this.playerNumber = playerNumber;
+    }
+}
