64e4a5f164ae68399166f76502ae9def5bec476e
june-777 <wlwhswnsrl96@gmail.com>
2023-10-25 14:43:40 +0900

    refactor: 상수 처리

--- src/main/java/baseball/utils/RestartCommandValidator.java
+++ src/main/java/baseball/utils/RestartCommandValidator.java
@@ -7,28 +7,32 @@ import static baseball.utils.ExceptionMessage.OUT_OF_LENGTH;
 import java.util.regex.Pattern;
 
 public class RestartCommandValidator {
+    private static final int VALID_LENGTH = 1;
+    private static final String VALID_RESTART_COMMAND_VALUE_ONE = "1";
+    private static final String VALID_RESTART_COMMAND_VALUE_TWO = "2";
+    private static final String ONLY_NUMERIC_REGEX = "^[\\d]*$";
 
     public void validateRestartCommandInput(String restartCommand) {
-        validateLength(restartCommand, 1);
+        validateLength(restartCommand);
         validateNumeric(restartCommand);
         validateOneOrTwo(restartCommand);
     }
 
-    private void validateLength(String input, int length) {
-        if (input.length() != length) {
-            throw new IllegalArgumentException(OUT_OF_LENGTH.getError(input, length));
+    private void validateLength(String input) {
+        if (input.length() != VALID_LENGTH) {
+            throw new IllegalArgumentException(OUT_OF_LENGTH.getError(input, 1));
         }
     }
 
     private void validateNumeric(String input) {
-        boolean onlyNumeric = Pattern.matches("^[\\d]*$", input);
+        boolean onlyNumeric = Pattern.matches(ONLY_NUMERIC_REGEX, input);
         if (!onlyNumeric) {
             throw new IllegalArgumentException(NOT_NUMERIC.getError(input));
         }
     }
 
     private void validateOneOrTwo(String input) {
-        if (!input.equals("1") && !input.equals("2")) {
+        if (!input.equals(VALID_RESTART_COMMAND_VALUE_ONE) && !input.equals(VALID_RESTART_COMMAND_VALUE_TWO)) {
             throw new IllegalArgumentException(INVALID_RESTART_COMMAND.getError(input));
         }
     }
