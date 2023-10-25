1302568be459c25fdda2e644f1bb2fe10d4b43fc
realcold0 <realcold0@gmail.com>
2023-10-25 15:12:40 +0900

    refactor: doc수정에 따라 기존의 AnswerCreator domain을 Computer로 이름 변경, 정답 만드는 기능을 분리하여 정의한 AnswerCreator 클래스 구현

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,23 +1,23 @@
 package baseball.controller;
 
-import baseball.domain.AnswerCreator;
+import baseball.domain.Computer;
 import baseball.domain.Player;
 import baseball.service.CompareAnswerWithPlayerNumber;
 import baseball.view.InputView;
 import baseball.view.ResultView;
 
 public class GameController {
-    private AnswerCreator answerCreator = new AnswerCreator();
+    private Computer computer = new Computer();
     private Player player = new Player();
     private InputView inputView = new InputView();
     private CompareAnswerWithPlayerNumber compareAnswerWithPlayerNumber = new CompareAnswerWithPlayerNumber();
     private ResultView resultView = new ResultView();
 
     public void startGame() {
-        answerCreator.setComputerAnswer();  //게임 정답 세팅
+        computer.setComputerAnswer();  //게임 정답 세팅
         player.setPlayerAnswer(inputView.inputPlayerNumbers());//플레이어의 정답 작성
         compareAnswerWithPlayerNumber.compareStrikeBall(player.getPlayerAnswer(),
-                answerCreator.getComputerAnswer());//플레이어 정답, 게임정답 비교
+                computer.getComputerAnswer());//플레이어 정답, 게임정답 비교
         resultView.outputCompareResult(compareAnswerWithPlayerNumber);//게임 결과 확인
 
     }
---
+++ src/main/java/baseball/util/AnswerCreator.java
@@ -0,0 +1,21 @@
+package baseball.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnswerCreator {
+
+    private List<Integer> answer = new ArrayList<>();
+
+    public List<Integer> createAnswer() {
+        while (answer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!answer.contains(randomNumber)) {
+                answer.add(randomNumber);
+            }
+        }
+        return answer;
+    }
+
+}
