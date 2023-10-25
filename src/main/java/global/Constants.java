13263d8fe6b6ababeb356d607de7e8a1d3ad218c
minb7fd <minb7fd@daum.net>
2023-10-25 18:20:03 +0900

    Fix: 상수 네이밍 오타 수정

--- src/main/java/global/Constants.java
+++ src/main/java/global/Constants.java
@@ -2,5 +2,5 @@ package global;
 
 public class Constants {
     public static int RANDOM_NUM_LENGTH = 3;
-    public static String GEUSSING_NUM_REGEXP = "^[1-9]{3}";
+    public static String GUESSING_NUM_REGEXP = "^[1-9]{3}";
 }
--- src/main/java/service/ComputerService.java
+++ src/main/java/service/ComputerService.java
@@ -29,7 +29,7 @@ public class ComputerService {
     }
 
     private void validateGuessingNum(String guessingNum) {
-        if (!Pattern.matches(Constants.GEUSSING_NUM_REGEXP,guessingNum)) {
+        if (!Pattern.matches(Constants.GUESSING_NUM_REGEXP,guessingNum)) {
             throw new IllegalArgumentException("1~9까지의 세자리 숫자로만 입력할 수 있습니다.");
         }
     }
