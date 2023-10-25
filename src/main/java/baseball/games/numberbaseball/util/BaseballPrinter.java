d9d16f32a1ede53cd00e5124f9dad2c46a991590
haebyun <bhk4392@gmail.com>
2023-10-25 19:45:55 +0900

    feat(BaseballExecutor): 시작 메시지와 끝 메시지를 출력 하는 기능 구현

--- src/main/java/baseball/games/numberbaseball/util/BaseballPrinter.java
+++ src/main/java/baseball/games/numberbaseball/util/BaseballPrinter.java
@@ -3,13 +3,16 @@ package baseball.games.numberbaseball.util;
 import baseball.GamePrinter;
 
 public class BaseballPrinter implements GamePrinter {
+    private static final String STARTING_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String ENDING_MESSAGE = "숫자 야구 게임을 종료합니다.";
+    
     @Override
     public void printStartingMessage() {
-
+        System.out.println(STARTING_MESSAGE);
     }
 
     @Override
     public void printEndingMessage() {
-
+        System.out.println(ENDING_MESSAGE);
     }
 }
