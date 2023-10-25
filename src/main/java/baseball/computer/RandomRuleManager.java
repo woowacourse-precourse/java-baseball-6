7d7a1f47c4075a7122a75c50629a6a5c8f0bc937
sheepysnoopy <qkffldlfdjsk12@naver.com>
2023-10-25 16:43:24 +0900

    refactor: printCount, calculateCount 관심사 분리

--- src/main/java/baseball/computer/RandomRuleManager.java
+++ src/main/java/baseball/computer/RandomRuleManager.java
@@ -1,5 +1,6 @@
 package baseball.computer;
 
+import baseball.dto.Score;
 import baseball.player.UserNumberManager;
 import camp.nextstep.edu.missionutils.Randoms;
 
@@ -30,32 +31,42 @@ public class RandomRuleManager implements GameManager {
         return randomNumber;
     }
 
-    public void countCalculate(List<Integer> randomNumber) {
-        while (true) {
-            String userNumber = userNumberManager.getUserNumber();
+    public Score calculateCount(List<Integer> randomNumber){
+        String userNumber = userNumberManager.getUserNumber();
 
-            int ballCount = 0;
-            int strikeCount = 0;
+        int strikeCount = 0;
+        int ballCount = 0;
 
-            for (int i = 0; i < COUNT.getNumberInfo(); i++) {
-                int tmp = userNumber.charAt(i) - '0';
+        for (int i = 0; i < COUNT.getNumberInfo(); i++) {
+            int tmp = userNumber.charAt(i) - '0';
 
-                if (randomNumber.indexOf(tmp) == i) {
-                    strikeCount++;
-                }
-                if (randomNumber.contains(tmp)) {
-                    ballCount++;
-                }
+            if (randomNumber.indexOf(tmp) == i) {
+                strikeCount++;
             }
+            if (randomNumber.contains(tmp)) {
+                ballCount++;
+            }
+        }
+
+        return new Score(strikeCount, ballCount);
+    }
+
+    public void printCount(List<Integer> randomNumber) {
+        while (true) {
+            Score score = calculateCount(randomNumber);
+            int strikeCount = score.getStrikeCount();
+            int ballCount = score.getBallCount();
 
             if (strikeCount == COUNT.getNumberInfo()) {
                 System.out.println(strikeCount + STRIKE_MESSAGE.getMessage());
                 System.out.println(CORRECT_MESSAGE.getMessage());
                 break;
-            } else if (strikeCount == 0 && ballCount == 0) {
+            }
+            if (strikeCount == 0 && ballCount == 0) {
                 System.out.println(NOTHING_MESSAGE.getMessage());
                 continue;
             }
+
             System.out.println((ballCount - strikeCount) + BALL_MESSAGE.getMessage() +
                     strikeCount + STRIKE_MESSAGE.getMessage());
         }
