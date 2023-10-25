587790f43e56e242a64af70fb75812513609c134
dongha922 <kdh4575@naver.com>
2023-10-25 23:32:19 +0900

    변수명수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,7 +10,7 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Game rightAnswer = new Game();
-        //Game.init(rightAnswer);
+        Game.init(rightAnswer);
     }
 }
 class Array {
@@ -64,6 +64,143 @@ class Game {
 
     int[] number = new int[NUMBER_COUNT];
 
+    Game() {
+        int nowRandomNum;
+        int nowNumberIndex = 0;
+
+        while (nowNumberIndex < NUMBER_COUNT) {
+            nowRandomNum = this.getRandomNumber();
+
+            if (!Array.checkArrayContains(number, nowRandomNum)) {
+                number[nowNumberIndex++] = nowRandomNum;
+            }
+        }
+    }
+
+    Game(String str) {
+        for (int i = 0; i < Game.NUMBER_COUNT; i++) {
+            number[i] = Integer.parseInt(str.charAt(i) + "");
+        }
+    }
+
+    private int getRandomNumber() {
+        return pickNumberInRange(Game.START_RANGE, Game.END_RANGE);
+    }
+
+    public static void init(Game rightAnswer) {
+        //Game answer = Game.getAnswer();
+
+        Test test = new Test();
+        //test.compareAnswer(answer, rightAnswer);
+        test.showResult();
+
+        if (test.strike != Game.NUMBER_COUNT) {
+            Game.init(rightAnswer);
+            return;
+        }
+
+        int newGameAnswer = Game.checkNewGameStart();
+        if (newGameAnswer == Game.NEW_GAME) {
+            rightAnswer = new Game();
+            Game.init(rightAnswer);
+        }
+    }
+
+
+
+    private static int checkNewGameStart() {
+        System.out.println(SUCCESS_MESSAGE);
+        System.out.println(NEW_GAME_CHECK_MESSAGE);
+
+        String newGameAnswer = readLine();
+        return Game.getNewGameAnswerNumber(newGameAnswer);
+    }
+
+    private static int getNewGameAnswerNumber(String str) {
+        int intValue;
+        try {
+            intValue = Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+
+        if (intValue != 1 && intValue != 2) {
+            throw new IllegalArgumentException();
+        }
+
+        return intValue;
+    }
+
+    public String toString() {
+        return "number: " + this.number[0] + this.number[1] + this.number[2];
+    }
+
+    static void checkInputValue(final String str) {
+        if (!Game.checkInputLength(str)) {
+            throw new IllegalArgumentException();
+        }
+        if (!Game.checkInputNumber(str)) {
+            throw new IllegalArgumentException();
+        }
+        if (!Game.checkEqualNumber(str)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    static boolean checkInputLength(final String str) {
+        return str.length() == Game.NUMBER_COUNT;
+    }
+
+    static boolean checkInputNumber(String str) {
+        try {
+            int numberValue = Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        char[] numberArray = Array.getCharArrayFromString(str);
+        return !Array.checkArrayContains(numberArray, '0');
+    }
+
+    static boolean checkEqualNumber(String str) {
+        char[] checkEqual = new char[str.length()];
+        char[] word = Array.getCharArrayFromString(str);
+
+        for (int i = 0; i < str.length(); i++) {
+            if (Array.checkArrayContains(checkEqual, word[i])) {
+                return false;
+            }
+            checkEqual[i] = word[i];
+        }
+
+        return true;
+    }
+}
+class Test {
+    int ball, strike;
+
+    private static final String BALL_WORD = "볼";
+    private static final String STRIKE_WORD = "스트라이크";
+    private static final String NOT_MATCH_WORD = "낫싱";
+
+    public String toString() {
+        return "ball: " + this.ball + ", strike: " + this.strike;
+    }
+
+    private void addBallCount() {
+        this.ball++;
+    }
+
+    private void addStrikeCount() {
+        this.strike++;
+    }
+
+    void compareAnswer(Game answer, Game rightAnswer) {
+
+    }
+
+    void showResult() {
+
+    }
 }
 
 
