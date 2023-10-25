2cad10b0615bfbabf69e0297cf1f96c059e424da
ddol9 <drddol9@gmail.com>
2023-10-25 21:33:58 +0900

    fix error

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,132 +7,101 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        List<Integer> computer = generateRandomNumber();
-        playGame(computer);
-    }
-
-    private static List<Integer> generateRandomNumber() {
-        List<Integer> computer = new ArrayList<>();
-
-        while (computer.size() < 3) {
-            addUniqueRandomNumber(computer);
-        }
-
-        return computer;
-    }
-
-    private static void addUniqueRandomNumber(List<Integer> list) {
-        int randomNumber = Randoms.pickNumberInRange(1, 9);
-        if (!list.contains(randomNumber)) {
-            list.add(randomNumber);
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        boolean keepGoing = true;
+        while (keepGoing) {
+            keepGoing = startGame();
         }
     }
 
-    private static void playGame(List<Integer> computer) {
-        System.out.println("숫자야구 게임을 시작합니다.");
+    public static boolean startGame() {
+        List<Integer> computerNumbers = generateRandomNumbers();
+        boolean isCorrect = false;
+        while (!isCorrect) {
+            isCorrect = checkNumbers(computerNumbers, getUserNumbers());
+        }
+        return askRestart();
+    }
 
-        while (true) {
-            if (!playRound(computer) || !continueGame()) {
-                break;
+    public static List<Integer> generateRandomNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < 3) {
+            int randNum = Randoms.pickNumberInRange(1, 9);
+            if (!numbers.contains(randNum)) {
+                numbers.add(randNum);
             }
         }
+        return numbers;
     }
 
-    private static boolean playRound(List<Integer> computer) {
-        try {
-            List<Integer> user = getUserInput();
-            return !checkResult(computer, user);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return false;
-        }
-    }
-
-    private static List<Integer> getUserInput() throws IllegalArgumentException {
-        System.out.print("숫자를 입력해주세요 : ");
+    public static List<Integer> getUserNumbers() {
+        System.out.println("숫자를 입력해주세요 : ");
         String input = Console.readLine();
-        return checkException(input);
+
+        validateInput(input);
+        List<Integer> userNumbers = new ArrayList<>();
+        for (char c : input.toCharArray()) {
+            userNumbers.add(Character.getNumericValue(c));
+        }
+        return userNumbers;
     }
 
-    private static List<Integer> checkException(String input) throws IllegalArgumentException {
+    public static void validateInput(String input) {
         if (input.length() != 3) {
-            throw new IllegalArgumentException("잘못된 입력입니다.");
+            throw new IllegalArgumentException("3자리의 숫자를 입력해주세요.");
         }
-
-        if (!input.matches("\\d{3}")) {
-            throw new IllegalArgumentException("잘못된 입력입니다.");
-        }
-
-        List<Integer> user = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            int number = Integer.parseInt(input.substring(i, i + 1));
-
-            if (number == 0 || number > 9) {
-                throw new IllegalArgumentException("잘못된 입력입니다.");
+        for (char c : input.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                throw new IllegalArgumentException("숫자만 입력해주세요.");
             }
-
-            if (user.contains(number)) {
-                throw new IllegalArgumentException("잘못된 입력입니다.");
+            if (c == '0') {
+                throw new IllegalArgumentException("0은 입력할 수 없습니다.");
             }
-
-            user.add(number);
+            if (input.indexOf(c) != input.lastIndexOf(c)) {
+                throw new IllegalArgumentException("중복된 숫자를 입력할 수 없습니다.");
+            }
         }
-
-        return user;
     }
 
-    private static boolean checkResult(List<Integer> computer, List<Integer> user) {
-        int strike = countStrike(computer, user);
-        int ball = countBall(computer, user);
+    public static boolean checkNumbers(List<Integer> computerNumbers, List<Integer> userNumbers) {
+        int strike = 0;
+        int ball = 0;
+        for (int i = 0; i < 3; i++) {
+            if (computerNumbers.get(i).equals(userNumbers.get(i))) {
+                strike++;
+            } else if (computerNumbers.contains(userNumbers.get(i))) {
+                ball++;
+            }
+        }
+        return printResult(strike, ball);
+    }
 
-        printResult(strike, ball);
-
+    public static boolean printResult(int strike, int ball) {
         if (strike == 3) {
+            System.out.println("3스트라이크");
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             return true;
         }
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+            return false;
+        }
+        if (ball > 0) {
+            System.out.print(ball + "볼 ");
+        }
+        if (strike > 0) {
+            System.out.print(strike + "스트라이크");
+        }
+        System.out.println();
         return false;
     }
 
-    private static int countStrike(List<Integer> computer, List<Integer> user) {
-        int strike = 0;
-        for (int i = 0; i < computer.size(); i++) {
-            strike += compareIndex(computer, user, i);
-        }
-        return strike;
-    }
-
-    private static int compareIndex(List<Integer> computer, List<Integer> user, int i) {
-        return computer.get(i).equals(user.get(i)) ? 1 : 0;
-    }
-
-    private static int countBall(List<Integer> computer, List<Integer> user) {
-        int ball = 0;
-        for (int i = 0; i < computer.size(); i++) {
-            ball += checkBall(computer, user, i);
-        }
-        return ball;
-    }
-
-    private static int checkBall(List<Integer> computer, List<Integer> user, int i) {
-        return computer.contains(user.get(i)) && !computer.get(i).equals(user.get(i)) ? 1 : 0;
-    }
-
-    private static void printResult(int strike, int ball) {
-        if (strike > 0) {
-            System.out.println(strike + "스트라이크");
-        }
-        if (ball > 0) {
-            System.out.println(ball + "볼");
-        }
-        if (strike == 0 && ball == 0) {
-            System.out.println("낫싱");
-        }
-    }
-
-    private static boolean continueGame() {
+    public static boolean askRestart() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String input = Console.readLine();
-        return "1".equals(input);
+        if (!input.equals("1") && !input.equals("2")) {
+            throw new IllegalArgumentException("1 또는 2를 입력해주세요.");
+        }
+        return input.equals("1");
     }
 }
