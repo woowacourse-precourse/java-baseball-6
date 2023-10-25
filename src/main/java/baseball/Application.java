c2cdd427ac245fe216ef7f46c145d4f2e0199e01
leedoyeon849 <do83430208@gmail.com>
2023-10-25 14:16:41 +0900

    feat: Application에 start 로직 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
+import baseball.domain.BaseballComputer;
+import baseball.domain.BaseballGame;
+import baseball.domain.BaseballUser;
+import baseball.domain.Game;
+import baseball.policy.JudgmentPolicy;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        Game game = new BaseballGame(new BaseballComputer(), new BaseballUser(), new JudgmentPolicy());
+        game.start();
     }
+
+
+
+
+
+
+
 }
