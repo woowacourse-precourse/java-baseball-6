3b3e97d3ebaa02e27cb5cd5f7e523ff0928815fc
DahyeY <dhuniv@naver.com>
2023-10-25 13:29:06 +0900

    feat: 예외 처리 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,6 +3,7 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 public class Application {
@@ -10,7 +11,6 @@ public class Application {
         Game.start();
     }
 }
-
 class Computer {
     private static final int START_OF_RANGE = 1;
     private static final int END_OF_RANGE = 9;
@@ -21,13 +21,38 @@ class Computer {
     static void generateAnswer() {
         for(int i = 0; i<Game.NUMBERS; i++) {
             answer[i] = getRandomNumber();
-            for(int j=0; j<i; j++)
+            for(int j = 0; j < i; j++){
                 while(answer[i]==answer[j]) answer[i] = getRandomNumber();
+            }
         }
     }
 
 }
-
+class User {
+    static int[] answer = new int[Game.NUMBERS];
+    static void getUserNumbers() {
+        String strNum = Console.readLine();
+        if(strNum.length()> Game.NUMBERS) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+        for(int i = 0; i < Game.NUMBERS; i++) {
+            answer[i]=strNum.charAt(i) - '0';
+            if(answer[i]<1==answer[i]>9) {
+                throw new IllegalArgumentException("Invalid input");
+            }
+        }
+        for(int i = 0; i< Game.NUMBERS; i++){
+            for(int j = 0; j < Game.NUMBERS; j++){
+                if(i == j) {
+                    continue;
+                }
+                if(answer[i]==answer[j]) {
+                    throw new IllegalArgumentException("Invalid input");
+                }
+            }
+        }
+    }
+}
 class Result {
     private static final String BALL = "볼";
     private static final String STRIKE = "스트라이크";
@@ -42,26 +67,27 @@ class Result {
         int countStrike = 0;
         for(int i=0; i<Game.NUMBERS; i++) {
             for(int j=0; j<Game.NUMBERS; j++) {
-                if (arr1[i] == arr2[j])
-                    if(i==j) countStrike++;
-                    else countBall++;
+                if (arr1[i] == arr2[j]){
+                    if(i==j) {
+                        countStrike++;
+                    }
+                    else {
+                        countBall++;
+                    }
+                }
             }
         }
-        if(countStrike==0 && countBall==0) return NOTHING + "\n";
-        else if(countStrike==3) return END_MESSAGE;
-        else return countBall + BALL + " " + countStrike + STRIKE + "\n";
+        if(countStrike==0 && countBall==0) {
+            return NOTHING + "\n";
+        }
+        else if(countStrike==3) {
+            return END_MESSAGE;
+        }
+        else {
+            return countBall + BALL + " " + countStrike + STRIKE + "\n";
+        }
     }
 }
-
-class User {
-    static int[] answer = new int[Game.NUMBERS];
-    static void getUserNumbers() {
-        String strNum = Console.readLine();
-        if(strNum.length()> Game.NUMBERS) throw new IllegalArgumentException("Invalid input");
-        for(int i = 0; i < Game.NUMBERS; i++) answer[i]=strNum.charAt(i) - '0';
-    }
-}
-
 class Game {
     public static final int NUMBERS = 3;
     private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.\n";
@@ -72,7 +98,9 @@ class Game {
     }
     static void restart() {
         int num = Integer.parseInt(Console.readLine());
-        if(num == 1) start();
+        if(num == 1) {
+            start();
+        }
     }
     public static void start() {
         System.out.print(START_MESSAGE);
