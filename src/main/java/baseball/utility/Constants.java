009166685432555475c9e5ded65143a7178d91b3
“sangjun121” <juncho12011201@gmail.com>
2023-10-25 18:21:01 +0900

    feat: 결과값이 낫싱일때의 출력할 상수 NOTHING 추가

--- src/main/java/baseball/utility/Constants.java
+++ src/main/java/baseball/utility/Constants.java
@@ -7,6 +7,7 @@ public final class Constants { //상속을 막기 위해 final 사용
     public static final String NUMBER_INPUT_PROMPT_MESSAGE = "숫자를 입력해주세요 : ";
     public static final String BALL= "볼";
     public static final String STRIKE= "스트라이크";
+    public static final String NOTHING= "낫싱";
     public static final String NUMBER_CORRECT_MESSAGE= "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     public static final String GAME_PAUSE_OR_RESUME_PROMPT_MESSAGE= "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
