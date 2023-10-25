d4a9185929bb1d327105089940e922dd08ee8d41
Chaeros <chpark6851@koreatech.ac.kr>
2023-10-25 19:28:17 +0900

    feat: 사용자 입력 예외 검증 메서드추가
    사용자의 입력에 숫자가 아닌 문자가 입력된 경우 예외처리하는 검증
    메서드를 추가했습니다.

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,5 +1,6 @@
 package baseball.view;
 
+import baseball.constant.errormessage.PlayerInputError;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,5 +18,18 @@ public class InputView {
             list.add(Integer.parseInt(String.valueOf(numbers.charAt(i))));
         }
         return list;
+    }
+
+    /**
+     * 입력한 문자열 내의 문자가 1~9 사이의 수가 아닌 문자가 포함되어 있으면 예외처리
+     *
+     * @param numbers : 사용자로부터 입력받은 문자열
+     */
+    private void validatePlayerInputIsNumber(String numbers) {
+        for (int i = 0; i < numbers.length(); ++i) {
+            if (numbers.charAt(i) < 49 || numbers.charAt(i) > 57) {
+                throw new IllegalArgumentException(PlayerInputError.PLAYER_NUMBER_WRONG_INPUT_NOT_INTEGER_MESSAGE);
+            }
+        }
     }
 }
