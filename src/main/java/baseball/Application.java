b55018a0a0df1db99c02466876746d1035315bf5
sam <10502sam@gmail.com>
2023-10-25 21:02:16 +0900

    숫자 야구 구현 완료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Baseball baseball = new Baseball();
+        baseball.start();
     }
 }
