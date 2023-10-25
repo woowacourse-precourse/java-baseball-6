edb959c1b010a2fe4e85469c81fd425bf611728d
yeop0740 <ssucgoo@gmail.com>
2023-10-25 10:30:42 +0900

    refactor: 게임 시작 문구 출력 기능 메서드로 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,7 +10,7 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        printStartMessage();
 
         int continued = 1;
         while (continued == 1) {
@@ -91,5 +91,9 @@ public class Application {
             }
         }
 
+    }
+
+    private static void printStartMessage() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
     }
 }
