481623e6742ec0b77845fbc58cffca8143bc1653
yshong1998 <yshong1998@gmail.com>
2023-10-25 16:34:17 +0900

    Style(BaseballNumber): 불필요한 주석 제거

--- src/main/java/baseball/domain/BaseballNumber.java
+++ src/main/java/baseball/domain/BaseballNumber.java
@@ -5,8 +5,6 @@ import java.util.List;
 public class BaseballNumber {
     private final List<Integer> baseballNumberList;
 
-    // 3개의 숫자를 리스트로 저장하는 기능
-    // 리스트가 현재 3개라는 보장이 없기 때문에 3개라는 것을 보장하기 위한 제약이 필요한 상태
     BaseballNumber(List<Integer> baseballNumberList){
         this.baseballNumberList = baseballNumberList;
     }
