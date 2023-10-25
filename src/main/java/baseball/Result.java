52c1904f171c7a8db63ac6875227bc18ac8cb49f
Hepheir <hepheir@gmail.com>
2023-10-25 21:27:46 +0900

    Create class Result

---
+++ src/main/java/baseball/Result.java
@@ -0,0 +1,11 @@
+package baseball;
+
+public class Result {
+    public final Integer bulls;
+    public final Integer cows;
+
+    Result(Integer bulls, Integer cows) {
+        this.bulls = bulls;
+        this.cows = cows;
+    }
+}
