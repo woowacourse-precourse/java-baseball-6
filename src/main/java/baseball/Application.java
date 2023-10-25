709e05f5bd87f084871124e5ae89b5207a2a6cd3
dani <joyjoy312@naver.com>
2023-10-25 17:17:10 +0900

    refactor: 결과 타입 final변수로 변경, 기능 메서드 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,30 +2,22 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+import static baseball.ResultType.*;
+
 public class Application {
+    private static boolean status;
+    private static List<Integer> computer;
     public static void main(String[] args){
 
-        boolean status=true;
+        status = true;
 
         while (status) {
-            // 게임시작 안내
-            System.out.println(InfoMessage.START.getMessage());
-            // 랜덤 숫자 만들기
-            List<Integer> computer = new ArrayList<>();
-            while (computer.size() < 3) {
-                int randomNumber = Randoms.pickNumberInRange(1, 9);
-                if(!computer.contains(randomNumber))
-                    computer.add(randomNumber);
-            }
-
+            gameStart();
             boolean inputNum = true;
-            // 숫자 입력받기
+
             while (inputNum) {
                 System.out.print(InfoMessage.INPUT_NUMBER.getMessage());
                 String str = Console.readLine();
@@ -44,34 +36,48 @@ public class Application {
                 }
                 String ans ="";
                 if (strike == 3){
-                    ans="3스트라이크";
+                    ans="3"+STRIKE;
                     inputNum=false;
                 }
                 if(ball > 0){
-                    ans+= ball+"볼 ";
+                    ans+= ball+BALL+" ";
                 }
-                if (strike > 0) {
-                    ans += strike + "스트라이크 ";
+                if (strike!=3 && strike > 0) {
+                    ans += strike + STRIKE + " ";
                 }
                 if(strike==0&&ball==0){
-                    ans += "낫싱";
+                    ans += NOTHING;
                 }
                 System.out.println(ans);
             }//while
-            // 숫자를 모두 맞힌 경우
-            System.out.println(InfoMessage.CORRECT_ANSWER.getMessage());
-            System.out.println(InfoMessage.RESTART.getMessage());
-
-            int choice = Integer.parseInt(Console.readLine());
-            if (choice == 1) {
-                status = true;
-            } else if (choice == 2) {
-                status = false;
-            } else {
-                status = false; // 다시확인
-                throw new IllegalArgumentException();
-            }
-
+            SuccessCorrectAnswer();
         }
     }
+    private static void SuccessCorrectAnswer(){
+        System.out.println(InfoMessage.CORRECT_ANSWER.getMessage());
+        System.out.println(InfoMessage.RESTART.getMessage());
+
+        int choice = Integer.parseInt(Console.readLine());
+        if (choice == 1) {
+            status = true;
+        } else if (choice == 2) {
+            status = false;
+        } else {
+            status = false; // 다시확인
+            throw new IllegalArgumentException();
+        }
+    }
+    private static void makeRandomNumber(){
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if(!computer.contains(randomNumber))
+                computer.add(randomNumber);
+        }
+    }
+    private static void gameStart(){
+        System.out.println(InfoMessage.START.getMessage());
+        computer = new ArrayList<>();
+        makeRandomNumber();
+    }
+
 }
