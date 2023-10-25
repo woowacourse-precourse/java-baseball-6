1cb568e417122812e92827e925ab44ae2e4e18d0
kijen723 <kij2646@gmail.com>
2023-10-25 18:46:56 +0900

    refactor: 예외 처리를 위한 클래스 분류

--- src/main/java/baseball/ExceptionControl.java
+++ src/main/java/baseball/ExceptionControl.java
@@ -1,2 +1,19 @@
-package baseball;public class ExceptionControl {
+package baseball;
+
+public class ExceptionControl {
+    public static void checkUserNumberInput(String userNumber) {
+        int intUserNumber = Integer.parseInt(userNumber);
+        if (intUserNumber < 100 || intUserNumber > 999 ||
+                userNumber.charAt(0) == userNumber.charAt(1) ||
+                userNumber.charAt(1) == userNumber.charAt(2) ||
+                userNumber.charAt(2) == userNumber.charAt(0)) {
+            throw new IllegalArgumentException("잘못된 입력입니다. 0을 제외한 3가지의 수를 입력하세요");
+        }
+    }
+
+    public static void checkRestartGameInput(int restartCount) {
+        if (restartCount != 1 && restartCount != 2) {
+            throw new IllegalArgumentException("잘못된 입력입니다. 1 또는 2만 입력하세요");
+        }
+    }
 }
