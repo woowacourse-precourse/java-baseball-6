8ced519974e9313ce73924c12ffb2929bcdc64a7
OhTaeHo-97 <57743934+OhTaeHo-97@users.noreply.github.com>
2023-10-25 16:55:12 +0900

    refactor: 사용자 입력 테스트 별개  클래스 분리

--- src/test/java/utils/InputUtilTest.java
+++ src/test/java/utils/InputUtilTest.java
@@ -10,95 +10,99 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("사용자의 3자리 수를 입력받는 기능")
-class InputUtilTest extends NsTest {
-    @Test
-    @DisplayName("정상 입력")
-    void success() {
-        String input = "123";
-        List<Integer> list = List.of(1, 2, 3);
-        assertSimpleTest(
-                () -> {
-                    run(input);
-                    assertThat(output()).isEqualTo(list.toString());
-                }
-        );
-    }
-
+@DisplayName("입력 기능")
+class InputUtilTest {
     @Nested
-    @DisplayName("예외 테스트")
-    class ExceptionTest {
+    @DisplayName("사용자의 3자리 수를 입력받는 기능")
+    class PlayerInputTest extends NsTest {
         @Test
-        @DisplayName("null 테스트")
-        void nullTest() {
-            String nullStr = null;
-            assertSimpleTest(() ->
-                    assertThatThrownBy(() -> runException(nullStr))
-                            .isInstanceOf(IllegalArgumentException.class)
+        @DisplayName("정상 입력")
+        void success() {
+            String input = "123";
+            List<Integer> list = List.of(1, 2, 3);
+            assertSimpleTest(
+                    () -> {
+                        run(input);
+                        assertThat(output()).isEqualTo(list.toString());
+                    }
             );
         }
 
-        @Test
-        @DisplayName("빈 문자열 테스트")
-        void emptyTest() {
-            String emptyStr = new String();
-            assertSimpleTest(() ->
-                    assertThatThrownBy(() -> runException(emptyStr))
-                            .isInstanceOf(IllegalArgumentException.class)
-            );
+        @Nested
+        @DisplayName("예외 테스트")
+        class ExceptionTest {
+            @Test
+            @DisplayName("null 테스트")
+            void nullTest() {
+                String nullStr = null;
+                assertSimpleTest(() ->
+                        assertThatThrownBy(() -> runException(nullStr))
+                                .isInstanceOf(IllegalArgumentException.class)
+                );
+            }
+
+            @Test
+            @DisplayName("빈 문자열 테스트")
+            void emptyTest() {
+                String emptyStr = new String();
+                assertSimpleTest(() ->
+                        assertThatThrownBy(() -> runException(emptyStr))
+                                .isInstanceOf(IllegalArgumentException.class)
+                );
+            }
+
+            @Test
+            @DisplayName("잘못된 타입 테스트")
+            void invalidTypeTest() {
+                assertSimpleTest(() ->
+                        assertThatThrownBy(() -> runException("1a2"))
+                                .isInstanceOf(IllegalArgumentException.class)
+                );
+            }
+
+            @Test
+            @DisplayName("3자리가 아닌 숫자 테스트")
+            void invalidLengthTest() {
+                assertSimpleTest(() ->
+                        assertThatThrownBy(() -> runException("12"))
+                                .isInstanceOf(IllegalArgumentException.class)
+                );
+                assertSimpleTest(() ->
+                        assertThatThrownBy(() -> runException("1234"))
+                                .isInstanceOf(IllegalArgumentException.class)
+                );
+            }
+
+            @Test
+            @DisplayName("0 테스트")
+            void invalidZeroTest() {
+                assertSimpleTest(() ->
+                        assertThatThrownBy(() -> runException("012"))
+                                .isInstanceOf(IllegalArgumentException.class)
+                );
+                assertSimpleTest(() ->
+                        assertThatThrownBy(() -> runException("102"))
+                                .isInstanceOf(IllegalArgumentException.class)
+                );
+                assertSimpleTest(() ->
+                        assertThatThrownBy(() -> runException("120"))
+                                .isInstanceOf(IllegalArgumentException.class)
+                );
+            }
+
+            @Test
+            @DisplayName("중복 테스트")
+            void invalidDuplicateTest() {
+                assertSimpleTest(() ->
+                        assertThatThrownBy(() -> runException("112"))
+                                .isInstanceOf(IllegalArgumentException.class)
+                );
+            }
         }
 
-        @Test
-        @DisplayName("잘못된 타입 테스트")
-        void invalidTypeTest() {
-            assertSimpleTest(() ->
-                    assertThatThrownBy(() -> runException("1a2"))
-                            .isInstanceOf(IllegalArgumentException.class)
-            );
+        @Override
+        protected void runMain() {
+            System.out.println(InputUtil.getPlayerInput());
         }
-
-        @Test
-        @DisplayName("3자리가 아닌 숫자 테스트")
-        void invalidLengthTest() {
-            assertSimpleTest(() ->
-                    assertThatThrownBy(() -> runException("12"))
-                            .isInstanceOf(IllegalArgumentException.class)
-            );
-            assertSimpleTest(() ->
-                    assertThatThrownBy(() -> runException("1234"))
-                            .isInstanceOf(IllegalArgumentException.class)
-            );
-        }
-
-        @Test
-        @DisplayName("0 테스트")
-        void invalidZeroTest() {
-            assertSimpleTest(() ->
-                    assertThatThrownBy(() -> runException("012"))
-                            .isInstanceOf(IllegalArgumentException.class)
-            );
-            assertSimpleTest(() ->
-                    assertThatThrownBy(() -> runException("102"))
-                            .isInstanceOf(IllegalArgumentException.class)
-            );
-            assertSimpleTest(() ->
-                    assertThatThrownBy(() -> runException("120"))
-                            .isInstanceOf(IllegalArgumentException.class)
-            );
-        }
-
-        @Test
-        @DisplayName("중복 테스트")
-        void invalidDuplicateTest() {
-            assertSimpleTest(() ->
-                    assertThatThrownBy(() -> runException("112"))
-                            .isInstanceOf(IllegalArgumentException.class)
-            );
-        }
-    }
-
-    @Override
-    protected void runMain() {
-        System.out.println(InputUtil.getPlayerInput());
     }
 }
