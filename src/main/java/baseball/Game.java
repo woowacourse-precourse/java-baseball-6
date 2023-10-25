eedda50e6e97e7cc4d67e805ab00134e329e50bc
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-10-25 11:15:58 +0900

    refactor: Announcer 클래스 추가에 따른 수정

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,46 +1,54 @@
 package baseball;
 
+import baseball.model.Announcer;
+import baseball.model.Computer;
+import baseball.model.Judge;
+import baseball.model.Player;
+
 import java.util.List;
 
-import static baseball.Printer.printInputNumberComment;
 
 public class Game {
     private final Computer computer;
     private final Player player;
     private final Judge judge;
-    private boolean isOver = false;
+    private final Announcer announcer;
 
-    public Game(Computer computer, Player player, Judge judge) {
+    public Game(Computer computer, Player player, Judge judge, Announcer announcer) {
         this.computer = computer;
         this.player = player;
         this.judge = judge;
+        this.announcer = announcer;
     }
 
     public void start() {
-        judge.announceStartGame();
+        announcer.announceStartGame();
         play();
     }
 
     public void play() {
         List<Integer> answer = computer.speakNumbers();
-
-        while (!isOver) {
-            Result result = playRound(answer);
-            judge.announceResult(result);
-            isOver = judge.checkGameOver(result);
-        }
-
+        playRound(answer);
         end();
     }
 
     private Result playRound(List<Integer> answer) {
-        printInputNumberComment();
+        announcer.announceToSpeakNumber();
         List<Integer> playerNumbers = player.speakNumbers();
 
-        return judge.makeResult(answer, playerNumbers);
+        Result result = judge.makeResult(answer, playerNumbers);
+        announcer.announceResult(result);
+
+        if (!judge.checkGameOver(result)) playRound(answer);
+        return result;
     }
 
     private void end() {
-        judge.announceGameOver();
+        announcer.announceGameOver();
+    }
+
+    public boolean checkRestart() {
+        announcer.announceToSpeakRestart();
+        return player.speakRestart();
     }
 }
