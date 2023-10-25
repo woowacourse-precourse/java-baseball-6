d19cead8af30a897408cab9cc5a4dfd12b8395fe
doyoungkim97 <eh4536@naver.com>
2023-10-25 20:46:11 +0900

    fix : 컴퓨터 출력 부분 수정

--- src/main/java/baseball/Computer/Computer.java
+++ src/main/java/baseball/Computer/Computer.java
@@ -37,6 +37,6 @@ public class Computer {
             OutputView.printBall(BallCount.ballCount);
             OutputView.printStrike(BallCount.strikeCount);
         }
-        System.out.println();
+        System.out.print('\n');
     }
 }
