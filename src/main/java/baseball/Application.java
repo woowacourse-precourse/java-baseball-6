c61645f44575d73abc87ba363e9efe392acb6a16
shortboy7 <thdwldnd7@naver.com>
2023-10-25 13:58:40 +0900

    feat : merge module to main

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,12 +2,16 @@ package baseball;
 
 import baseball.game.BaseballGame;
 import baseball.game.impl.BaseballGameImpl;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         BaseballGame game = new BaseballGameImpl(3);
         while (!game.isOver()) {
-
+            game.display();
+            String userInput = Console.readLine();
+            game.validateInput(userInput);
+            game.action(userInput);
         }
     }
 }
