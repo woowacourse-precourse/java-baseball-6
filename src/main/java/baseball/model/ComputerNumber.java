a6551f02d90b03fef5d6ed48adcbfac9c2b4d342
jihoon98 <tlswlgns1003@sju.ac.kr>
2023-10-25 14:17:02 +0900

    "refactor:다시 시작할때 번호 리셋하는 로직 수정"

--- src/main/java/baseball/model/ComputerNumber.java
+++ src/main/java/baseball/model/ComputerNumber.java
@@ -10,7 +10,7 @@ public class ComputerNumber {
     public ComputerNumber(){
         this.computerNumber=createRandomNumber();
     }
-    private List<Integer> createRandomNumber() {//컴퓨터 3자리 자연수 랜덤 생성 메서드
+    public List<Integer> createRandomNumber() {//컴퓨터 3자리 자연수 랜덤 생성 메서드
         List<Integer> randomNumbers = new ArrayList<>();
         while (randomNumbers.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -25,4 +25,7 @@ public class ComputerNumber {
         return computerNumber;
     }
 
+    public void resetNumber(){
+        this.computerNumber=createRandomNumber();
+    }
 }
