9aa2e2773f56e473fa78a15a2c10e76a933629c2
haebyun <bhk4392@gmail.com>
2023-10-25 17:44:44 +0900

    feat(BaseballValidator): 플레이어가 숫자가 아닌 입력에 대한 검증 기능 구현

--- src/main/java/baseball/games/numberbaseball/validation/BaseballValidator.java
+++ src/main/java/baseball/games/numberbaseball/validation/BaseballValidator.java
@@ -2,8 +2,15 @@ package baseball.games.numberbaseball.validation;
 
 public class BaseballValidator {
     private static final Integer NUMBERS_INPUT_SIZE = 3;
+    private static final Character NUMBERS_RANGE_START = '1';
+    private static final Character NUMBERS_RANGE_END = '9';
 
     private boolean isInvalidNumbersLength(String input) {
         return input.length() != NUMBERS_INPUT_SIZE;
+    }
+
+    private boolean containsOutRangeInput(String input) {
+        return input.chars()
+                .anyMatch(c -> c < NUMBERS_RANGE_START || c > NUMBERS_RANGE_END);
     }
 }
