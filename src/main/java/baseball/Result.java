dd7133a67ba35526eddbbf5a4ed1eaad01aafb0d
seona-moon <fhflwhwl5@gmail.com>
2023-10-25 14:32:32 +0900

    feat: Result 클래스 생성, 필요로 하는 출력 관리

--- src/main/java/baseball/Result.java
+++ src/main/java/baseball/Result.java
@@ -1,2 +1,30 @@
-package baseball;public class Result {
+package baseball;
+
+public class Result {
+  private int Strike =0;
+  private int Ball =0;
+  public Result() {
+  }
+
+  public void addStrike() {
+    Strike++;
+  }
+
+  public void addBall() {
+    Ball++;
+  }
+
+  public String getResult() {
+    StringBuilder resultBuilder = new StringBuilder();
+    if (Ball == 0 && Strike == 0) {
+      return "낫싱";
+    }
+    if (Ball > 0) {
+      resultBuilder.append(Ball).append("볼 ");
+    }
+    if (Strike > 0) {
+      resultBuilder.append(Strike).append("스트라이크");
+    }
+    return resultBuilder.toString().trim();
+  }
 }
