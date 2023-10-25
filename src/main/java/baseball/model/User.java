b001cbbbebeb3cd6d1c172111a891f282ba920e3
www-author <dream970215@naver.com>
2023-10-25 20:53:26 +0900

    :bug: fix: 정규식 숫자 패턴 수정

--- src/main/java/baseball/model/User.java
+++ src/main/java/baseball/model/User.java
@@ -1,11 +1,5 @@
 package baseball.model;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-
 public class User {
 
     private String userNumber;
@@ -17,9 +11,8 @@ public class User {
         this.userNumber = userInput;
     }
 
-    // 1. 메인에 예외 처리를 전가 하기 위해 throws IllegalArgumentException 맞는지 ? 아래에 조건에서만 예외 발생시키면 되는거 아닌가?
     public void validateUserInput(String userInput) throws IllegalArgumentException {
-        String regex = "[0-9]+";
+        String regex = "[1-9]+";
         boolean isInvalidDigit = !userInput.matches(regex);
 
         if (isInvalidDigit || userInput.length() > 3) {
