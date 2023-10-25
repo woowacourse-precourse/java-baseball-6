54237eb6ac7f5327123b187e8a52587acc963594
zeomzzz <jmlee2705@gmail.com>
2023-10-25 21:53:20 +0900

    fix: Utils의 validateInputValue, validateRestartInput 에러 수정

--- src/main/java/baseball/utils/Utils.java
+++ src/main/java/baseball/utils/Utils.java
@@ -3,7 +3,9 @@ package baseball.utils;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Utils {
 
@@ -29,15 +31,20 @@ public class Utils {
         }
 
         // 2. 숫자가 맞는가?
+        try {
+            Integer.parseInt(inputValue);
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+
         // 3. 모두 다른 숫자인가?
-        int[] visited = new int[10];
-        for (int idx=0; idx<length; idx++) {
-            char charAtidx = inputValue.charAt(idx);
-            int charToInt = charAtidx - '0';
-            if (Character.isDigit(charAtidx) || visited[charToInt] == 1) {
+        char[] charArr = inputValue.toCharArray();
+        Set<Character> charSet = new HashSet<>();
+        for (char c : charArr) {
+            if (charSet.contains(c)) {
                 throw new IllegalArgumentException();
             }
-            visited[charToInt] = 1;
+            charSet.add(c);
         }
     }
 
@@ -69,9 +76,9 @@ public class Utils {
     }
 
     // 게임 재시작 여부 요청의 입력을 검증하는 메소드
-    public void validateRestartInput(int playerInput) {
-        if (playerInput != 1 || playerInput != 2) {
-            throw new IllegalArgumentException();
+    public void validateRestartInput(String playerInput) {
+        if (!playerInput.equals("1") && !playerInput.equals("2")) {
+            throw new IllegalArgumentException("입력이 잘못되어 게임을 종료합니다.");
         }
     }
 }
