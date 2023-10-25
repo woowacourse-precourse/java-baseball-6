13e2616b8a0c5388222049173195eeac746f6c9c
Hugh <lee2317884@gmail.com>
2023-10-25 09:31:34 +0900

    refactor: blank 상수 제거

--- src/main/java/baseball/constant/Constant.java
+++ src/main/java/baseball/constant/Constant.java
@@ -3,7 +3,6 @@ package baseball.constant;
 public class Constant {
     public static final String GAME_RESTART_NUMBER = "1";
     public static final String GAME_END_NUMBER = "2";
-    public static final String BLANK_VALUE = "";
     public static final int NUMBER_LENGTH_LIMIT = 3;
     public static final int START_NUMBER = 1;
     public static final int END_NUMBER = 9;
