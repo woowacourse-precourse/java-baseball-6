45f901d3d08b31231564f068617cf1785c1d4c35
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-10-25 23:11:03 +0900

    feat: 올바르지 않은 사용자 입력을 판단하는 로직 추가

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -34,5 +34,27 @@ public class User {
         return result;
     }
 
+    // 각 자리 숫자 1-9 이외의 잘못된 입력 처리
+    private boolean checkInvalid(String input) {
+        if (input.length() != LEN_NUM) {
+            return true;
+        }
+        if (!isDigitNumber(input)) {
+            return true;
+        }
+        return false;
+    }
+
+    // 각 자리 수가 숫자인지 판별
+    private boolean isDigitNumber(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            String digit = input.substring(i,i+1);
+            if (!digit.matches("-?\\d+(\\.\\d+)?")) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
 }
