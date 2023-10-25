14bfac376023839fff86cc79fa15b7806153fdb9
Jiihyun <cwlgusc@gmail.com>
2023-10-25 13:30:30 +0900

    fix(RandomBallNumberGenerator): 게임 정답 만들어 주는 메서드 리턴타입 BallNumbers로 변경

--- src/main/java/baseball/domain/RandomBallNumberGenerator.java
+++ src/main/java/baseball/domain/RandomBallNumberGenerator.java
@@ -4,7 +4,6 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import static baseball.util.NumberConst.*;
@@ -14,11 +13,11 @@ public class RandomBallNumberGenerator {
         return Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
     }
 
-    public List<Integer> generateAnswerNumbers() {
+    public BallNumbers generateAnswerNumbers() {
         Set<Integer> answer = new HashSet<>();
         while (answer.size() < NUMBER_SIZE) {
             answer.add(getRandomNumber1to9());
         }
-        return new ArrayList<>(answer);
+        return BallNumbers.from(new ArrayList<>(answer));
     }
 }
