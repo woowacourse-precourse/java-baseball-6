436999299b6d9c00da1456e1bd8bc733e723e9f4
carrotcountry <jaeyoon321@naver.com>
2023-10-25 12:28:13 +0900

    refactor: cleanup unused import statements

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,6 @@
 package baseball;
 
 import baseball.controller.Controller;
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
 
 public class Application {
     public static void main(String[] args) {
