7daefd53b5524e641cd3de48e4b39fc4087638c0
mikekks <thdalsrb79@naver.com>
2023-10-25 17:23:58 +0900

    [feat] static 상수 추가

---
+++ src/main/java/baseball/CONSTANT.java
@@ -0,0 +1,18 @@
+package baseball;
+
+public class CONSTANT {
+
+    // 게임 결과 관련 상수
+    public static final int NONE = 0;
+    public static final int STRIKE = 1;
+    public static final int BALL = 2;
+
+    // 게임 진행 상수
+    public static final int GAME_START = 0;
+    public static final int GAME_RESTART = 1;
+    public static final int GAME_END = 2;
+
+    // input 사이즈 관련 상수
+    public static final int GAME_SIZE = 3;
+    public static final int NEXT_SIZE = 1;
+}
--- src/main/java/baseball/controller/BasicController.java
+++ src/main/java/baseball/controller/BasicController.java
@@ -1,10 +1,11 @@
 package baseball.controller;
 
-import baseball.domain.GameResult;
-import baseball.service.CheckService;
-import baseball.view.ResultView;
-import camp.nextstep.edu.missionutils.Randoms;
+import static baseball.CONSTANT.GAME_SIZE;
 
+import baseball.dto.GameResultDTO;
+import baseball.service.CheckService;
+import baseball.view.ProcessView;
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,31 +13,31 @@ public class BasicController {
     private List<Integer> computerNumber;
 
     public void createComputerNumber() {
-        if(computerNumber != null)
-            return;
-
         computerNumber = new ArrayList<>();
-        while (computerNumber.size() < 3) {
+
+        while (computerNumber.size() < GAME_SIZE) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computerNumber.contains(randomNumber)) {
                 computerNumber.add(randomNumber);
             }
         }
+
     }
 
-    public Boolean checkNumber(String userInput){
+    public Boolean checkNumber(String userInput) {
         // String -> Integer
         List<Integer> userNum = makeIntegerArray(userInput);
 
         // service -> 1 0 0 과 같은 정보 리턴
-        GameResult gameResult = CheckService.matchNumber(userNum, computerNumber);
+        GameResultDTO gameResult = CheckService.matchNumber(userNum, computerNumber);
 
         // view -> 해당 결과 출력 요청
-        ResultView.printResult(gameResult);
+        ProcessView.printResult(gameResult);
 
         // 성공시 true, 아니면 false 반환
-        if(gameResult.getSuccess() == true)
+        if (gameResult.getSuccess() == true) {
             return true;
+        }
 
         return false;
 
@@ -44,7 +45,7 @@ public class BasicController {
 
     private List<Integer> makeIntegerArray(String userInput) {
         List<Integer> userNum = new ArrayList<>();
-        for(int i = 0; i< userInput.length(); i++){
+        for (int i = 0; i < GAME_SIZE; i++) {
             userNum.add(userInput.charAt(i) - '0');
         }
         return userNum;
