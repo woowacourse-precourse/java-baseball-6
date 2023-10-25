54d3466a8c1c96cfd3a40d0c2c324771656aeba3
kijen723 <kij2646@gmail.com>
2023-10-25 19:24:28 +0900

    refactor: 메인 클래스 정리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,5 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import camp.nextstep.edu.missionutils.Console;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Application {
     public static void main(String[] args) {
         GameProgress.gameStart();
