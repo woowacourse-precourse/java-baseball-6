fcfd5739b8f15ca5f7458442cfdd614b8a7d674e
mikekks <thdalsrb79@naver.com>
2023-10-25 20:08:15 +0900

    [refactor] 출력 문자열 상수화

--- src/main/java/baseball/CONSTANT.java
+++ src/main/java/baseball/CONSTANT.java
@@ -15,4 +15,15 @@ public class CONSTANT {
     // input 사이즈 관련 상수
     public static final int GAME_SIZE = 3;
     public static final int NEXT_SIZE = 1;
+
+    // 출력 관련 문자열
+    public static final String START_STR = "숫자 야구 게임을 시작합니다.";
+    public static final String INPUT_STR = "숫자를 입력해주세요 : ";
+    public static final String BALL_STR = "볼 ";
+    public static final String STRIKE_STR = "스트라이크 ";
+    public static final String NOTHING_STR = "낫싱";
+    public static final String OUTPUT_STR = "개의 숫자를 모두 맞히셨습니다! 게임 종료\n";
+    public static final String NEXTSTEP_STR = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ";
+
+
 }
