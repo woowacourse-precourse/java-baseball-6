4dca77269c5638a0935b0315df71ea02af94625c
hunm719 <hunm719@naver.com>
2023-10-25 12:15:59 +0900

    스타일 가이드에 맞게 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,49 +1,90 @@
 package baseball;
 
-import java.util.Random;
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     private static int[] computerNumbers;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        gameStart();
+    }
 
-        System.out.println("숫자 야구 게임을 시작합니다.");
+    private static void gameStart() {
+        System.out.println("숫자 야구 게임을 시작합니다");
 
-        while (true) {
-            //컴퓨터가 무작위 숫자를 생성
+        do {
             computerNumbers = randomNumbers();
 
-            //사용자에게 숫자를 입력 받고 판정 진행
-            gameStart();
+            while (true) {
+                System.out.print("숫자를 입력해주세요: ");
 
-            //게임이 끝나고 다음 게임 시작 혹은 게임 종료 선택지 출력
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            Scanner scanner = new Scanner(System.in);
-            int choice = scanner.nextInt();
-            if (choice == 2) {
-                break;
+                String userInput = Console.readLine();
+
+                int[] userNumbers = parseUserInput(userInput);
+                int[] result = calculateResult(userNumbers);
+
+                displayResult(result);
+
+                if (result[0] == 3) {
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    break;
+                }
+            }
+        } while (askRetry());
+    }
+
+    public static boolean askRetry() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String choice = Console.readLine();
+        return choice.equals("1");
+    }
+
+    private static int[] parseUserInput(String userInput) {
+        if (userInput.length() != 3) {
+            throw new IllegalArgumentException("3자리 수를 입력하세요.");
+        }
+        int[] userNumbers = new int[3];
+        for (int i = 0; i < 3; i++) {
+            userNumbers[i] = userInput.charAt(i) - '0';
+        }
+        return userNumbers;
+    }
+
+    private static int[] randomNumbers() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
             }
         }
-    }
 
-    //컴퓨터가 무작위 숫자를 생성
-    private static int[] randomNumbers() {
-        Random random = new Random();
-        int[] numbers = new int[3];
-
-        // 서로 다른 3개의 수를 생성하고 배열에 저장
+        int[] computerArray = new int[3];
         for (int i = 0; i < 3; i++) {
-            int num;
-            do {
-                num = random.nextInt(9) + 1;
-            } while (contains(numbers, num));
-            numbers[i] = num;
+            computerArray[i] = computer.get(i);
         }
-        return numbers;
+        return computerArray;
     }
 
-    //배열에 숫자가 포함되어 있는지 확인
+    private static int[] calculateResult(int[] userNumbers) {
+        int strikes = 0;
+        int balls = 0;
+
+        for (int i = 0; i < 3; i++) {
+            if (userNumbers[i] == computerNumbers[i]) {
+                strikes++;
+            } else if (contains(computerNumbers, userNumbers[i])) {
+                balls++;
+            }
+        }
+
+        return new int[]{strikes, balls};
+    }
+
     private static boolean contains(int[] array, int value) {
         for (int num : array) {
             if (num == value) {
@@ -53,50 +94,18 @@ public class Application {
         return false;
     }
 
-    //사용자에게 숫자를 입력 받고 판정 진행
-    private static void gameStart() {
-        Scanner scanner = new Scanner(System.in);
+    private static void displayResult(int[] result) {
+        int strikes = result[0];
+        int balls = result[1];
 
-        while (true) {
-            System.out.print("숫자를 입력해주세요 : ");
-
-            int[] userNumbers = new int[3];
-            int input = scanner.nextInt();
-
-            if (input < 100 || input > 999) {
-                throw new IllegalArgumentException("3자리 수를 입력하세요.");
-            }
-
-            userNumbers[0] = input / 100;
-            userNumbers[1] = (input % 100) / 10;
-            userNumbers[2] = input % 10;
-
-            int strikes = 0;
-            int balls = 0;
-
-            for (int i = 0; i < 3; i++) {
-                if (userNumbers[i] == computerNumbers[i]) {
-                    strikes++;
-                } else if (contains(computerNumbers, userNumbers[i])) {
-                    balls++;
-                }
-            }
-
-            if (strikes > 0 && balls > 0) {
-                System.out.println(strikes + "스트라이크 " + balls + "볼");
-            } else if (strikes > 0) {
-                System.out.println(strikes + "스트라이크");
-            } else if (balls > 0) {
-                System.out.println(balls + "볼");
-            } else {
-                System.out.println("낫싱");
-            }
-
-            if (strikes == 3) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                computerNumbers = randomNumbers(); // 게임 종료 후 새로운 게임을 위해 컴퓨터의 숫자를 재설정
-                break;
-            }
+        if (strikes > 0 && balls > 0) {
+            System.out.println(balls + "볼 " + strikes + "스트라이크");
+        } else if (strikes > 0) {
+            System.out.println(strikes + "스트라이크");
+        } else if (balls > 0) {
+            System.out.println(balls + "볼");
+        } else {
+            System.out.println("낫싱");
         }
     }
 }
