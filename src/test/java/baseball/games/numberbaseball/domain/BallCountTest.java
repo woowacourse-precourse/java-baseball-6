a1007370ccece96c07823a49aa3b691f55044b81
haebyun <bhk4392@gmail.com>
2023-10-25 19:26:17 +0900

    test(BallCount): 스트라이크와 볼에 따라서 알맞은 문자열을 반환하는 기능 테스트

--- src/test/java/baseball/games/numberbaseball/domain/BallCountTest.java
+++ src/test/java/baseball/games/numberbaseball/domain/BallCountTest.java
@@ -28,4 +28,48 @@ class BallCountTest {
         //then
         assertThat(isThreeStrike).isFalse();
     }
+
+    @Test
+    @DisplayName("볼과 스트라이크가 모두 0이라면 낫싱을 출력")
+    void printNothingMessage() {
+        //given
+        BallCount ballCount = new BallCount(0, 0);
+        //when
+        String ballCountMessage = ballCount.createBallCountMessage();
+        //then
+        assertThat(ballCountMessage).isEqualTo("낫싱");
+    }
+
+    @Test
+    @DisplayName("스트라이크만 존재 하면 스트라이크 수만 출력")
+    void printStrikeMessage() {
+        //given
+        BallCount ballCount = new BallCount(0, 1);
+        //when
+        String ballCountMessage = ballCount.createBallCountMessage();
+        //then
+        assertThat(ballCountMessage).isEqualTo("1스트라이크");
+    }
+
+    @Test
+    @DisplayName("볼만 존재 하면 볼의 수만 출력")
+    void printBallMessage() {
+        //given
+        BallCount ballCount = new BallCount(1, 0);
+        //when
+        String ballCountMessage = ballCount.createBallCountMessage();
+        //then
+        assertThat(ballCountMessage).isEqualTo("1볼");
+    }
+
+    @Test
+    @DisplayName("볼과 스트라이크가 모두 0이 아니라면 둘 다 출력")
+    void printBothMessage() {
+        //given
+        BallCount ballCount = new BallCount(1, 1);
+        //when
+        String ballCountMessage = ballCount.createBallCountMessage();
+        //then
+        assertThat(ballCountMessage).isEqualTo("1볼 1스트라이크");
+    }
 }
