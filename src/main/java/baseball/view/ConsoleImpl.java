2be5428aa759fad2d3a36a79e7a398e3bb53ad5f
mr-silvester <126128590+mr-silvester@users.noreply.github.com>
2023-10-25 19:20:55 +0900

    fix: readLine() 메서드 실행 후 자원 반납을 위한 close() 실행 구문 추가

--- src/main/java/baseball/view/ConsoleImpl.java
+++ src/main/java/baseball/view/ConsoleImpl.java
@@ -1,6 +1,7 @@
 package baseball.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static camp.nextstep.edu.missionutils.Console.close;
 
 public class ConsoleImpl implements Console {
     private void println(String message) {
@@ -33,7 +34,9 @@ public class ConsoleImpl implements Console {
 
     @Override
     public String getUserInput() {
-        return readLine();
+        String input = readLine();
+        close();
+        return input;
     }
 
     @Override
