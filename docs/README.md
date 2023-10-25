5e209644680e9e65f55d257c7b4646de8d5284ba
Gksrufdk09 <rufdk09@naver.com>
2023-10-25 22:50:57 +0900

    한결아 미션 제출합니다

---
+++ docs/README.md
@@ -0,0 +1,14 @@
+camp.nextstep.edu.missionutils.Console 와 camp.nextstep.edu.missionutils.Randoms 라이브러리를 사용하여 사용자에게 입력을 받고 랜덤 숫자를 생성하였습니다.
+
+generateRandomNumbers 는 랜덤으로 3자리 숫자를 생성합니다.
+
+contain은 배열에 숫자가 포함되어 있는지 확인합니다.
+
+playGame은 게임을 하는 메서드 입니다. 
+readLine을 이용하여 숫자를 입력받습니다.
+입력된 값을 숫자로 변환합니다.
+랜덤숫자와 사용자 입력 숫자를 비교하여 스트라이크와 볼 개수를 계산합니다.
+
+askForRestart 는 게임에 성공하여 재시작 종료 여부를 확인합니다. 
+사용자로부터 입력받아 choice에 저장하고 choice가 1인경우 재시작 합니다. 
+
--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,99 @@
 package baseball;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 게임 시작 문구 출력
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        // 게임 루프 시작
+        while (true) {
+            // 랜덤으로 서로 다른 3자리 숫자 생성
+            int[] computerNumbers = generateRandomNumbers();
+
+            // 게임 플레이
+            boolean isGameEnd = playGame(computerNumbers);
+
+            // 게임 종료 시 재시작 또는 종료 여부 확인
+            if (isGameEnd) {
+                if (!askForRestart()) {
+                    break; // 게임 종료
+                }
+            }
+        }
+    }
+
+    // 랜덤함수로 서로 다른 3자리 숫자 생성
+    private static int[] generateRandomNumbers() {
+        int[] numbers = new int[3];
+        for (int i = 0; i < 3; i++) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            while (contains(numbers, randomNumber)) {
+                randomNumber = Randoms.pickNumberInRange(1, 9);
+            }
+            numbers[i] = randomNumber;
+        }
+        return numbers;
+    }
+
+    // 배열에 특정 숫자가 포함되어 있는지 확인
+    private static boolean contains(int[] array, int number) {
+        for (int element : array) {
+            if (element == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 게임 시작
+    private static boolean playGame(int[] computerNumbers) {
+        int attempts = 0;
+
+        while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
+            String input = Console.readLine();
+
+            // 입력값을 숫자 배열로 변환
+            int[] userNumbers = new int[3];
+            for (int i = 0; i < 3; i++) {
+                userNumbers[i] = input.charAt(i) - '0';
+            }
+
+            // 볼과 스트라이크 계산
+            int balls = 0;
+            int strikes = 0;
+            for (int i = 0; i < 3; i++) {
+                if (userNumbers[i] == computerNumbers[i]) {
+                    strikes++;
+                } else if (contains(computerNumbers, userNumbers[i])) {
+                    balls++;
+                }
+            }
+
+            if (strikes == 3) {
+                System.out.println(strikes + "스트라이크");
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                return true; // 게임 종료
+            } else if (balls == 0 && strikes == 0) {
+                System.out.println("낫싱");
+            } else if (balls > 0 && strikes == 0) {
+                System.out.println(balls + "볼");
+            } else if (strikes > 0 && balls == 0) {
+                System.out.println(strikes + "스트라이크");
+            } else {
+                System.out.println(balls + "볼 " + strikes + "스트라이크");
+            }
+
+            attempts++;
+        }
+    }
+    private static boolean askForRestart() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        int choice = Integer.parseInt(Console.readLine());
+        return choice == 1; // 1이면 재시작, 2이면 종료
     }
 }
