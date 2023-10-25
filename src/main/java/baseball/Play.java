9ec9137234cc86c3cb719cfc5b70419b00dc5edf
동원 <ai06021@gmail.com>
2023-10-25 13:49:52 +0900

    feat : 게임결과 표시

--- src/main/java/baseball/Play.java
+++ src/main/java/baseball/Play.java
@@ -1,10 +1,49 @@
 package baseball;
 
+import java.util.List;
+
 public class Play {
+    int strike, ball;
+
     public void game() {
-        Input input = new Input();
-        if (input.numbers().size() == 0)
-            return;
+
+        Computer computer = new Computer();
+        List<Integer> another = computer.anotherNumber();
+        while (true) {
+            while (strike != 3) {
+                strike = 0;
+                ball = 0;
+                Input input = new Input();
+                List<Integer> player = input.numbers();
+                if (player.size() == 0)
+                    return;
+                compare(player, another);
+                if (strike == 0 && ball == 0) {
+                    System.out.println("낫싱");
+                } else if (strike == 0) {
+                    System.out.println(ball + "볼");
+                } else if (ball == 0) {
+                    System.out.println(strike + "스트라이크");
+                } else {
+                    System.out.println(ball + "볼 " + strike + "스트라이크");
+                }
+            }
+        }
+
 
     }
+
+    private void compare(List<Integer> player, List<Integer> answer) {
+        for (int i = 0; i < player.size(); i++) {
+            int now = player.get(i);
+            if (answer.contains(now)) {
+
+                if (answer.get(i) == now)
+                    strike++;
+                else
+                    ball++;
+            }
+        }
+    }
+
 }
