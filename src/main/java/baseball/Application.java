559e5b0963f7710a4526456c373fd621ccf8ca83
김민주 <69795199+ks00919@users.noreply.github.com>
2023-10-25 22:29:53 +0900

    refactor: TODO 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,7 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         BaseballConsole.startBaseballShell();
         Console.close();
     }
