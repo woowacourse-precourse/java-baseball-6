353f54897683238d8fbc17727d9a7986d54a44d0
jhsseonn <hs125bt@gmail.com>
2023-10-25 17:39:14 +0900

    feat: 야구 객체 추가

---
+++ src/main/java/baseball/Baseball.java
@@ -0,0 +1,40 @@
+package baseball;
+
+public class Baseball {
+    private String computer;
+    private String randomNum;
+    private int strike;
+    private int ball;
+
+    public String getComputer() {
+        return this.computer;
+    }
+
+    public String getRandomNum() {
+        return this.randomNum;
+    }
+
+    public int getStrike(){
+        return this.strike;
+    }
+
+    public int getBall(){
+        return this.ball;
+    }
+
+    public void setComputer(String computer) {
+        this.computer = computer;
+    }
+
+    public void setRandomNum(String randomNum){
+        this.randomNum = randomNum;
+    }
+
+    public void setStrike(){
+        this.strike = strike;
+    }
+
+    public void setBall() {
+        this.ball = ball;
+    }
+}
