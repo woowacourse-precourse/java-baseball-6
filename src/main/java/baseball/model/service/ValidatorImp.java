c4bf64c5a3c727aaa5199dd03f53618e8a714a82
GaGa-Kim <wn8925@sookmyung.ac.kr>
2023-10-25 11:05:22 +0900

    refactor: 메소드명 명확하게 변경

--- src/main/java/baseball/model/service/ValidatorImp.java
+++ src/main/java/baseball/model/service/ValidatorImp.java
@@ -17,19 +17,19 @@ import java.util.Set;
 public class ValidatorImp implements Validator {
     @Override
     public void validatePlayerNumbers(String inputNumbers) {
-        isNumber(inputNumbers);
-        isLength(inputNumbers);
-        isRange(inputNumbers);
-        isDuplicate(inputNumbers);
+        validateIfNumeric(inputNumbers);
+        validateLength(inputNumbers);
+        validateRange(inputNumbers);
+        validateDuplicates(inputNumbers);
     }
 
     @Override
     public void validateRestartOption(String inputOption) {
-        isNumber(inputOption);
-        isOption(inputOption);
+        validateIfNumeric(inputOption);
+        validateOption(inputOption);
     }
 
-    private static void isNumber(String input) {
+    private static void validateIfNumeric(String input) {
         input.chars().forEach(ch -> {
             if (!Character.isDigit(ch)) {
                 throw new IllegalArgumentException(NUMBER_NON_NUMBER.getMessage());
@@ -37,13 +37,13 @@ public class ValidatorImp implements Validator {
         });
     }
 
-    private static void isLength(String input) {
+    private static void validateLength(String input) {
         if (input.length() != GAME_NUMBERS_SIZE.getValue()) {
             throw new IllegalArgumentException(NUMBER_LENGTH_INVALID.getMessage());
         }
     }
 
-    private static void isRange(String input) {
+    private static void validateRange(String input) {
         input.chars().forEach(ch -> {
             int num = Character.getNumericValue(ch);
             if (num < GAME_NUMBERS_MIN_VALUE.getValue() || num > GAME_NUMBERS_MAX_VALUE.getValue()) {
@@ -52,7 +52,7 @@ public class ValidatorImp implements Validator {
         });
     }
 
-    private static void isDuplicate(String input) {
+    private static void validateDuplicates(String input) {
         Set<Integer> numbers = new HashSet<>();
         input.chars().forEach(num -> {
             if (numbers.contains(num)) {
@@ -62,7 +62,7 @@ public class ValidatorImp implements Validator {
         });
     }
 
-    private static void isOption(String input) {
+    private static void validateOption(String input) {
         int optionValue = Integer.parseInt(input);
         if (optionValue != RESTART_OPTION.getValue() && optionValue != QUIT_OPTION.getValue()) {
             throw new IllegalArgumentException(OPTION_INVALID.getMessage());
