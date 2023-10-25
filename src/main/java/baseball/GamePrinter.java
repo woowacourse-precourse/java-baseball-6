e3b1f79608bdcf758e0b72d91103138844a4c06f
haebyun <bhk4392@gmail.com>
2023-10-25 19:59:09 +0900

    refactor(GamePrinter): 시작 메시지와 끝 메시지를 static 형태로 수정

--- src/main/java/baseball/GamePrinter.java
+++ src/main/java/baseball/GamePrinter.java
@@ -1,7 +1,14 @@
 package baseball;
 
 public interface GamePrinter {
-    void printStartingMessage();
+    String STARTING_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    String ENDING_MESSAGE = "숫자 야구 게임을 종료합니다.";
 
-    void printEndingMessage();
+    static void printStartingMessage() {
+        System.out.println(STARTING_MESSAGE);
+    }
+
+    static void printEndingMessage() {
+        System.out.println(ENDING_MESSAGE);
+    }
 }
