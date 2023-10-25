0aee69f8574b7bf0e4715378874c6822e03a762c
mingeun0507 <tqdlfkg@naver.com>
2023-10-25 15:43:03 +0900

    feat: 출력, 에러에 사용할 상수 추가로 정의

--- src/main/java/baseball/constants/ErrorConst.java
+++ src/main/java/baseball/constants/ErrorConst.java
@@ -4,7 +4,7 @@ public enum ErrorConst {
     NULL_INPUT("입력값이 null입니다."),
     OUT_OF_BOUNDARY("입력값이 1~9 사이의 값이 아닙니다."),
     INVALID_LENGTH("입력값의 길이가 유효하지 않습니다."),
-    ;
+    INVALID_INPUT("입력값이 유효하지 않습니다.");
 
     private final String message;
 
