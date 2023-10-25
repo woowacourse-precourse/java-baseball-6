49ba6bd5bbd9ac6e5d08d214ac4e0d55971ea2ce
Hae-K <bhk4392@inha.edu>
2023-10-25 16:44:13 +0900

    feat(BaseballExecutor): 느슨한 결합을 하기 위한 인터페이스와 구현체 생성

---
+++ src/main/java/baseball/games/numberbaseball/core/BaseballExecutor.java
@@ -0,0 +1,10 @@
+package baseball.games.numberbaseball.core;
+
+import baseball.GamingConsole;
+
+public class BaseballExecutor implements GamingConsole {
+    @Override
+    public void createNewGame() {
+
+    }
+}
