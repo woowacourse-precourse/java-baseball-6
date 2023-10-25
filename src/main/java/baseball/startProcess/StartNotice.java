928be3eb7e50994009f4c68fe5cab29c9be179af
kang jihyeong <her4386@gmail.com>
2023-10-25 17:54:41 +0900

    StartNotice, StartProcess : 클래스명 변경, 게임 규칙 적용

--- src/main/java/baseball/startProcess/StartNotice.java
+++ src/main/java/baseball/startProcess/StartNotice.java
@@ -1,5 +1,5 @@
 package baseball.startProcess;
 
-public class startNotice {
+public class StartNotice {
     public static final String startPhrase = "숫자 야구 게임을 시작합니다.";
 }
---
+++ src/main/java/baseball/startProcess/StartProcess.java
@@ -0,0 +1,38 @@
+package baseball.startProcess;
+
+import baseball.GameRole;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StartProcess {
+    private List<Integer> computer;
+
+    public StartProcess(GameRole gameRole) {
+        initVariable();
+        printStartPhrase();
+        combineComputerNumber(gameRole);
+    }
+
+    private void initVariable() {
+        computer = new ArrayList<>();
+    }
+
+    private void printStartPhrase() {
+        System.out.println(StartNotice.startPhrase);
+    }
+
+    private void combineComputerNumber(GameRole gameRole) {
+        while (computer.size() < gameRole.GAME_NUMBER_CNT) {
+            int randomNumber = Randoms.pickNumberInRange(gameRole.GAME_NUMBER_RANGE_OVER, gameRole.GAME_NUMBER_RANGE_UNDER);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+    }
+
+    public List<Integer> getComputerNumber() {
+        return computer;
+    }
+}
