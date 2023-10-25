63a5065baee9a2aa0e316295c77c29029a7eec64
leejaehun <ldyjka@gmail.com>
2023-10-25 19:08:25 +0900

    modify: TODO 주석 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,14 +1,9 @@
 package baseball;
 
 import game.BaseballGameClass;
-import java.io.IOException;
 
 public class Application {
     public static void main(String[] args) throws IllegalArgumentException{
-        // TODO: 프로그램 구현
-        // TODO: baseball game class를 service로 바꾸기.
-        // test code
-        // Game Start
         System.out.println("숫자 야구 게임을 시작합니다.");
 
         BaseballGameClass baseballGameClass = new BaseballGameClass();
