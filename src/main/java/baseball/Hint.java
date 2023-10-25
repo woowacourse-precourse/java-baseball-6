d2969d4669eda634ea519a6c1abde4c60d2e8301
OhTaeHo-97 <57743934+OhTaeHo-97@users.noreply.github.com>
2023-10-25 17:56:52 +0900

    feat: 스트라이크, 볼 개수 체크 기능

--- src/main/java/baseball/Hint.java
+++ src/main/java/baseball/Hint.java
@@ -38,4 +38,31 @@ public class Hint {
         System.out.println(
                 this.ballCnt + GameMessage.BALL.getMessage() + " " + this.strikeCnt + GameMessage.STRIKE.getMessage());
     }
+
+    private void countStrikeCntAndBallCnt(List<Integer> playerNmbs, List<Integer> targetNmbs) {
+        for (int idx = 0; idx < Constant.BASEBALL_GAME_NUMBER_LENGTH.getValue(); idx++) {
+            validateStrikeAndBall(idx, playerNmbs, targetNmbs);
+        }
+    }
+
+    private void validateStrikeAndBall(int index, List<Integer> playerNmbs, List<Integer> targetNmbs) {
+        if (validateStrike(playerNmbs.get(index), targetNmbs.get(index))) {
+            return;
+        }
+        validateBall(playerNmbs.get(index), targetNmbs);
+    }
+
+    private boolean validateStrike(int playerNmb, int targetNmb) {
+        if (playerNmb == targetNmb) {
+            this.strikeCnt++;
+            return true;
+        }
+        return false;
+    }
+
+    private void validateBall(int playerNmb, List<Integer> targetNmbs) {
+        if (targetNmbs.contains(playerNmb)) {
+            this.ballCnt++;
+        }
+    }
 }
