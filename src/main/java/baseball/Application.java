71365bb3a5cc00cd4ba09fe33b9e02a51db16411
kite707 <12201680@inha.edu>
2023-10-25 21:15:51 +0900

    feat: userInput예외 처리
    사용자가 정답을 맞추려 시도할 때 입력한 값이 아래 조건을 만족하는지 검증합니다.
    
    1. 입력값의 길이는 2이다
    3. 입력값은 문자를 포함하지 않는다.
    
    위 조건에 부합하지 않을 시 IllegalArgumentException을 발생시킵니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -80,6 +80,18 @@ public class Application {
         return false;
     }
 
+    public static void validateUserInput(String userInput) {
+        if (userInput.length() != LENGTH) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < userInput.length(); i++) {
+            int curInput = userInput.charAt(i) - '0';
+            if (curInput < 0 || curInput > 9) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
         boolean isGameRestart = true;
@@ -89,6 +101,7 @@ public class Application {
             while (isIncorrect) {
                 System.out.print("숫자를 입력해주세요 : ");
                 String userInput = Console.readLine();
+                validateUserInput(userInput);
                 isIncorrect = evaluator(userInput, answer);
             }
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
