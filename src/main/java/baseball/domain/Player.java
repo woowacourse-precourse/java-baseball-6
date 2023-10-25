3f27902385dfe3416b76e1cf406ae96e08739e45
gundorit <packdev937@gmail.com>
2023-10-25 13:22:28 +0900

    feat (Player) : 사용자와 컴퓨터를 나타낼 도메인을 생성한다

---
+++ src/main/java/baseball/domain/Player.java
@@ -0,0 +1,23 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Player {
+
+    private List<Integer> numbers;
+
+    public Player() {
+    }
+
+    public Player(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public void setNumbers(List<Integer> newNumbers) {
+        numbers = newNumbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+}
