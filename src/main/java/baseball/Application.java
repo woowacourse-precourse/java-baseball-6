d96b32224be07c44b35a2a6664f7ecfa493015e3
Project-Longinus <Team.Project.Longinus@gmail.com>
2023-10-25 22:24:54 +0900

    맞겠지 뭐

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,21 @@
 package baseball;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        int[] randomNumber = new int[3];
+        for (int i = 0; i < 3; i++) {
+            randomNumber[i] = Randoms.pickNumberInRange(1, 9);
+        }
+
+        System.out.print("어디 한번 씨부려 보거라 : ");
+        System.out.println(Console.readLine());
+
+        System.out.println(Arrays.toString(randomNumber));
     }
 }
+
