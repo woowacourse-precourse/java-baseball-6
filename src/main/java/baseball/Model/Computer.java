fec26ff194cd103670a6b4f1b1630f2f7ce53352
ljh <dusk5588@naver.com>
2023-10-25 21:04:15 +0900

    [Feat] Computer 클래스 구현

--- src/main/java/baseball/Model/Computer.java
+++ src/main/java/baseball/Model/Computer.java
@@ -1,4 +1,25 @@
 package baseball.Model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Computer {
+
+    List<Integer> answer = new ArrayList<>();
+
+    public Computer() {
+        answer.clear();
+        while (answer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!answer.contains(randomNumber)) {
+                answer.add(randomNumber);
+            }
+        }
+    }
+
+    public List<Integer> getAnswer() {
+        return answer;
+    }
 }
