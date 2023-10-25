e4bc650e78ec3fb5122d73ab2c7db820650e0759
mikekks <thdalsrb79@naver.com>
2023-10-25 15:10:42 +0900

    [feat] client의 입력 받는 기능 및 예외 처리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,37 @@
 package baseball;
 
+import baseball.controller.BasicController;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int nextNum = 1;
+        BasicController controller = new BasicController();
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        while(nextNum != 2){
+            controller.createComputerNumber();
+
+            while(nextNum != 2){
+                System.out.print("숫자를 입력해주세요 : ");
+                String userNum = Console.readLine();
+
+                if(userNum.length() > 3)
+                    throw new IllegalArgumentException("입력이 올바르지 않습니다.");
+
+                if(controller.checkNumber(userNum)) {
+                    String nextStep = Console.readLine();
+                    nextNum = Integer.parseInt(nextStep);
+                    break;
+                }
+
+            }
+
+        }
     }
 }
