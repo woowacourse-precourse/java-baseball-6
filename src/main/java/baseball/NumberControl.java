b86c3084164314861750f6837077cb4ef9df467c
kijen723 <kij2646@gmail.com>
2023-10-25 18:44:19 +0900

    refactor: 랜덤, 사용자 숫자 관리를 위한 클래스 분류

--- src/main/java/baseball/NumberControl.java
+++ src/main/java/baseball/NumberControl.java
@@ -1,4 +1,30 @@
 package baseball;
 
-public class RandomNumber {
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberControl {
+    public static List<Integer> getRandomNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        return computer;
+    }
+
+    public static List<Integer> getUserNumber() {
+        String userNumber = UserInput.userNumber();
+        List<Integer> user = new ArrayList<>();
+        user.add(Character.getNumericValue(userNumber.charAt(0)));
+        user.add(Character.getNumericValue(userNumber.charAt(1)));
+        user.add(Character.getNumericValue(userNumber.charAt(2)));
+
+        return user;
+    }
 }
