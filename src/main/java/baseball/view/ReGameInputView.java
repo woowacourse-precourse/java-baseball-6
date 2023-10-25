d69e7fce3cfe42598925f8b99a245c45a75e381c
Chaeros <chpark6851@koreatech.ac.kr>
2023-10-25 19:58:08 +0900

    feat: 사용자 입력 && 검증 후 반환하는 메서드 추가
    사용자로부터 재시작/종료를 결정하는 입력을 받고, 검증 후 문제가 없으면
    반환해주는 메서드를 추가했습니다.

--- src/main/java/baseball/view/ReGameInputView.java
+++ src/main/java/baseball/view/ReGameInputView.java
@@ -1,8 +1,23 @@
 package baseball.view;
 
 import baseball.constant.errormessage.PlayerInputError;
+import camp.nextstep.edu.missionutils.Console;
 
 public class ReGameInputView {
+
+    private static final String INPUT_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    /**
+     * 게임 재시작 여부를 사용자로 부터 입력받고(1:재시작, 2:종료), 입력값을 검증한 뒤 문제없으면 반환
+     *
+     * @return : 사용자가 입력한 문자열 반환
+     */
+    public String inputReGameCommand() {
+        System.out.println(INPUT_MESSAGE);
+        String command = Console.readLine();
+        validateCorrectNumber(command);
+        return command;
+    }
 
     /**
      * 입력한 문자열이 1 또는 2가 아니면 예외 발생
