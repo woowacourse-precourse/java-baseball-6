4054c5d01b0f4c36f5788f28a60db939cbc8f6f6
Chaeros <chpark6851@koreatech.ac.kr>
2023-10-25 19:26:04 +0900

    const: 숫자 미입력시 예외 문구 상수 추가
    사용자로부터 받은 입력에 숫자가 아닌 값이 포함되었을 경우 출력할 문구를
    상수로 추가했습니다.

---
+++ src/main/java/baseball/constant/errormessage/PlayerInputError.java
@@ -0,0 +1,5 @@
+package baseball.constant.errormessage;
+
+public class PlayerInputError {
+    public static final String PLAYER_NUMBER_WRONG_INPUT_NOT_INTEGER_MESSAGE = "[ERROR] 1~9 사이 숫자가 아닌 다른 문자가 입력에 포함되어 있습니다.";
+}
