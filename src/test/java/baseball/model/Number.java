1d95bf5d27f315a23d73d2c08b86ffa7c91c96b4
Laniel Park <Laniel8177@gmail.com>
2023-10-25 19:12:52 +0900

    feat : Number Base Class
    숫자들을 저장할 정해진 길이의 List<Integer>를 가지는  Base Class.
    길이는 3으로 고정한다.

---
+++ src/test/java/baseball/model/Number.java
@@ -0,0 +1,21 @@
+package baseball.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class Number {
+    protected static final int length = 3;
+    private final List<Integer> digits;
+
+    public Number(List<Integer> digits) {
+        assert digits.size() == length;
+
+        this.digits = new ArrayList<>(digits);
+    }
+
+    public int getDigit(int index) {
+        assert index >= 0 && index < length;
+
+        return this.digits.get(index);
+    }
+}
