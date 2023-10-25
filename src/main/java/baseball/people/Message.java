de33ae5e848208565f76071b25b13fa612f6fce8
U-keun Song <sukeun319@gmail.com>
2023-10-25 17:11:13 +0900

    Feat : 볼과 스트라이크의 개수를 적절한 문자열로 변환해주는 메서드 구현
    전에 새로 만들었던 Message 열거형 클래스에서 볼과 스트라이크의 개수를 받아서 "볼"과 "스트라이크"를 붙인 문자열을 반환하도록 구현했다.

--- src/main/java/baseball/people/Message.java
+++ src/main/java/baseball/people/Message.java
@@ -3,8 +3,9 @@ package baseball.people;
 public enum Message {
     GAME_START("숫자 야구 게임을 시작합니다."),
     GAME_END("3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
-    BALL("%d볼 "),
+    BALL("%d볼"),
     STRIKE("%d스트라이크"),
+    BALL_STRIKE("%d볼 %d스트라이크"),
     NOTHING("낫싱"),
     DEMAND_INPUT("숫자를 입력해주세요 : "),
     ASK_TO_RESTART("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. "),
@@ -19,5 +20,21 @@ public enum Message {
 
     public String getMessage() {
         return message;
+    }
+
+    public static String getBallStrikeMessage(int ball, int strike) {
+        if (ball == 0 && strike == 0) {
+            return NOTHING.message;
+        }
+
+        if (ball == 0) {
+            return String.format(STRIKE.message, strike);
+        }
+
+        if (strike == 0) {
+            return String.format(BALL.message, ball);
+        }
+
+        return String.format(BALL_STRIKE.message, ball, strike);
     }
 }
