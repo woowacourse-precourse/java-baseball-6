735539bb9613fbb9b2a17da237103b7fc06ebebe
Somin Park <ps4708@naver.com>
2023-10-25 14:04:40 +0900

    refactor: 숫자 랜덤 생성 기능 코드 단순화

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -3,9 +3,11 @@ package baseball.model;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-public class ComputerNumber {
+public class Computer {
 
     private static final int RANDOM_MIN_VALUE = 1;
     private static final int RANDOM_MAX_VALUE = 9;
@@ -13,19 +15,17 @@ public class ComputerNumber {
 
     private final List<Integer> computerNumber;
 
-    public ComputerNumber() {
+    public Computer() {
         this.computerNumber = createRandomNumbers();
     }
 
     public List<Integer> createRandomNumbers() {
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < RANDOM_NUMBERS_SIZE) {
+        Set<Integer> numbers = new HashSet<>();
+        while (numbers.size() < RANDOM_NUMBERS_SIZE) {
             int randomNumber = Randoms.pickNumberInRange(RANDOM_MIN_VALUE, RANDOM_MAX_VALUE);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
+            numbers.add(randomNumber);
         }
-        return computer;
+        return new ArrayList<>(numbers);
     }
 
 }
