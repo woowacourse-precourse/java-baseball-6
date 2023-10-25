4bd7810f55a576100544b9353f7b942a2f7e2937
Han <yulmam1@naver.com>
2023-10-25 20:06:52 +0900

    fix(view) : InputView class에 printRetryMessage 메소드의 오탈자를 수정합니다

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -9,6 +9,6 @@ public class InputView {
     }
 
     public void printRetryMessage() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력해 주세요");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 }
