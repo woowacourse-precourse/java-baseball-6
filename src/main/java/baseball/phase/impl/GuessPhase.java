81e3b6d89750bc9e6c535d6eceecd5c04493fc69
shortboy7 <thdwldnd7@naver.com>
2023-10-25 10:41:03 +0900

    feat:extract and validate string

--- src/main/java/baseball/phase/impl/GuessPhase.java
+++ src/main/java/baseball/phase/impl/GuessPhase.java
@@ -3,6 +3,9 @@ package baseball.phase.impl;
 import baseball.game.BaseballGame;
 import baseball.phase.Phase;
 import baseball.phase.PhaseID;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GuessPhase implements Phase {
     private BaseballGame baseballGame;
@@ -17,11 +20,35 @@ public class GuessPhase implements Phase {
 
     @Override
     public void play() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+        List<Integer> guess = extractGuess(input);
 
     }
 
     @Override
     public void setGame(BaseballGame game) {
         this.baseballGame = game;
+    }
+
+    private List<Integer> extractGuess(String input) {
+        List<Integer> result = new ArrayList<>();
+        boolean[] visited = new boolean[10];
+        input = input.trim();
+        int inputLength = input.length();
+
+        for (int i = 0; i < inputLength; i++) {
+            char ch = input.charAt(i);
+            if ('1' <= ch && ch <= '9') {
+                if (visited[ch - '0']) {
+                    throw new IllegalArgumentException();
+                }
+                result.add(ch - '0');
+                visited[ch - '0'] = true;
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
+        return result;
     }
 }
