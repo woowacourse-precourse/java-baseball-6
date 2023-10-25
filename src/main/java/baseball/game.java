77015e884c4dd92e07d6756ebbcdb414da72ce17
JungJei <greteljds0251@gmail.com>
2023-10-25 13:40:08 +0900

    feat: compare computer number and user number

---
+++ src/main/java/baseball/game.java
@@ -0,0 +1,30 @@
+package baseball;
+
+import java.util.List;
+
+public class game {
+    private int ball = 0;
+    private int strike = 0;
+
+    public void numCompare(int computerNumber, int userNumber, int i, int j) {
+        if (computerNumber == userNumber) {
+            indexCompare(i, j);
+        }
+    }
+
+    public void indexCompare(int i, int j) {
+        if (i == j) {
+            strike += 1;
+        } else {
+            ball += 1;
+        }
+    }
+
+    public void listCompare(List<Integer> computerNum, List<Integer> userNum) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                numCompare(computerNum.get(i), userNum.get(j), i, j);
+            }
+        }
+    }
+}
