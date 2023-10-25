3c346341a786fe9de7cfe38d8a8808f383a6e0e7
edjjincode <138580800+edjjincode@users.noreply.github.com>
2023-10-25 10:10:45 +0900

    feat(apply): compare computer number and user number
    - Make compareNumber method to compare game numbers with user numbers
    - calculate the strike numbers and ball numbers

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -2,11 +2,26 @@ package baseball.service;
 
 import baseball.domain.Game;
 import baseball.domain.User;
+import baseball.utils.Validator;
+import baseball.utils.RandomUtils;
+import baseball.view.RequestInput;
+import baseball.view.SystemMessage;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class GameService {
 
+    int size;
+
     Game game;
     User user = new User();
+
+    Validator validator = new Validator();
+
+    SystemMessage systemMessage = new SystemMessage();
 
     public void playGame() {
         int strike = 0;
@@ -17,8 +32,31 @@ public class GameService {
             strike = game.getStrikeCount(); /**스트라이크 숫자 확인 **/
         }
     }
-    private void computeScore() {
-        /**생성된 무작위 게임 번호와 사용자가 입력한 번호와의 비교**/
+
+    public void setGame(int size, int start, int end) {
+        this.size = size;
+        game = new Game(RandomUtils.getRandomNumbers(size, start, end));
     }
+
+    private void computeScore() {
+        compareNumber(game.getGameNumbers(), user.getUserNumbers());
+    }
+
+    private void compareNumber(int[] gameNumber, int[] userNumber) {
+
+        for (int i = 0; i < gameNumber.length; i++){
+            Integer[] gameArray = Arrays.stream(gameNumber).boxed().toArray(Integer[]::new);
+            List<Integer> gameNumberList = Arrays.asList(gameArray);
+            if (gameNumberList.get(i) == userNumber[i]){
+                game.incStrikeCount();
+                continue;
+            }
+            if (gameNumberList.contains(userNumber[i])){
+                game.incBallCount();
+            }
+        }
+    }
+
+
 
 }
