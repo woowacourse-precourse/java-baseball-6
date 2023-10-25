db46e8203f9091af97efb16ac5fafc895a08759b
GH-00 <ghkim7319@gmail.com>
2023-10-25 13:08:12 +0900

    [feat] validate input

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,36 +4,50 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Application {
 
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        while (true) {
-            List<Integer> computer = generateComputerNumbers();
-            boolean isGameEnd = false;
+        boolean isGameEnd = false;
 
-            while (!isGameEnd) {
-                String input = Console.readLine();
-                if (input.equals("1")) {
-                    System.out.println("게임을 새로 시작합니다.");
-                    break;
-                }
-                if (input.equals("2")) {
-                    System.out.println("게임을 종료합니다.");
-                    return;
-                }
+        while (!isGameEnd) {
+            List<Integer> computer;
+            try {
+                computer = generateComputerNumbers();
+            } catch (IllegalStateException e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
+
+            boolean isRoundEnd = false;
+
+            while (!isRoundEnd) {
                 try {
-                    int userInput = Integer.parseInt(input);
-                    String result = checkUserInput(userInput, computer);
-                    System.out.println(result);
-                    if (result.equals("3스트라이크")) {
-                        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    System.out.print("숫자를 입력해주세요 : ");
+                    String input = Console.readLine();
+                    if (input.equals("1")) {
+                        System.out.println("게임을 새로 시작합니다.");
+                        isRoundEnd = true;
+                    } else if (input.equals("2")) {
+                        System.out.println("게임을 종료합니다.");
+                        isRoundEnd = true;
                         isGameEnd = true;
+                    } else {
+                        validateInput(input);
+                        int userInput = Integer.parseInt(input);
+                        String result = calculateResult(userInput, computer);
+                        System.out.println(result);
+                        if (result.equals("3스트라이크")) {
+                            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                            isRoundEnd = true;
+                        }
                     }
-                } catch (NumberFormatException e) {
-                    System.out.println("잘못된 값을 입력하셨습니다. 다시 입력해주세요.");
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
                 }
             }
         }
@@ -41,16 +55,34 @@ public class Application {
 
     private static List<Integer> generateComputerNumbers() {
         List<Integer> computer = new ArrayList<>();
+        Set<Integer> generatedNumbers = new HashSet<>();
+
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
+            if (generatedNumbers.add(randomNumber)) {
                 computer.add(randomNumber);
+            }
+            if (generatedNumbers.size() == 9) {
+                throw new IllegalStateException("컴퓨터 숫자 생성에 실패했습니다.");
             }
         }
         return computer;
     }
 
-    private static String checkUserInput(int userInput, List<Integer> computer) {
+    private static void validateInput(String input) {
+        if (input.length() != 3 || !input.matches("[1-9]+") || input.contains("0")) {
+            throw new IllegalArgumentException("잘못된 값을 입력하셨습니다. 다시 입력해주세요.");
+        }
+        Set<Character> set = new HashSet<>();
+        for (char ch : input.toCharArray()) {
+            set.add(ch);
+        }
+        if (set.size() != 3) {
+            throw new IllegalArgumentException("잘못된 값을 입력하셨습니다. 다시 입력해주세요.");
+        }
+    }
+
+    private static String calculateResult(int userInput, List<Integer> computer) {
         List<Integer> userDigits = convertToDigits(userInput);
         int strikeCount = 0;
         int ballCount = 0;
