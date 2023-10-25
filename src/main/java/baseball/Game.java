7cef8bdfda4a212511ec1858bd5a207818920c27
choibyungjune <bottle6c@yonsei.ac.kr>
2023-10-25 19:02:43 +0900

    refactor: parse 3 numbers from user

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -14,6 +14,20 @@ public class Game {
     computerNumbers = generateRandomNumbers(3, 1, 9);
   }
 
+  public void playGame() {
+    System.out.println("숫자 야구 게임을 시작합니다.");
+
+    while (true) {
+      System.out.print("숫자를 입력해주세요 : ");
+      String guessInput = Console.readLine();
+      List<Integer> guessNumbers = parseGuessNumber(guessInput);
+
+      if (guessNumbers.size() != 3) {
+        throw new IllegalArgumentException("잘못된 입력입니다.");
+      }
+    }
+  }
+
   private List<Integer> generateRandomNumbers(int count, int min, int max) {
     List<Integer> computer = new ArrayList<>();
     while (computer.size() < count) {
@@ -25,5 +39,17 @@ public class Game {
     return computer;
   }
 
-
+  private List<Integer> parseGuessNumber(String guessNumber) {
+    List<Integer> guessNumberList = new ArrayList<>();
+    for (int i = 0; i < guessNumber.length(); i++) {
+      char digitChar = guessNumber.charAt(i);
+      if (Character.isDigit(digitChar)){
+        int digit = Character.getNumericValue(digitChar);
+        guessNumberList.add(digit);
+      } else {
+        throw new IllegalArgumentException("입력이 숫자가 아닙니다.");
+      }
+    }
+    return guessNumberList;
+  }
 }
