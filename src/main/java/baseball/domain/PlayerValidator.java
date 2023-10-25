8914e79879ef3886560c9e358b3a925b14e60e2a
gundorit <packdev937@gmail.com>
2023-10-25 13:26:49 +0900

    feat (PlayerValidator) : 숫자가 서로 다른 수로 이루어졌는지 검증한다

--- src/main/java/baseball/domain/PlayerValidator.java
+++ src/main/java/baseball/domain/PlayerValidator.java
@@ -1,18 +1,30 @@
 package baseball.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class PlayerValidator {
 
     public static final String NUMBER_SIZE_EX_MESSAGE = "입력값은 세 자리의 숫자여야 합니다.\n";
+    public static final String NUMBER_NOT_DUPLICATED_EX_MESSAGE = "입력값은 중복되는 수가 하나라도 있으면 안됩니다.\n";
+
 
     public static void validate(List<Integer> numbers) {
         validateNumberSize(numbers);
+        validateNotDuplicated(numbers);
     }
 
     private static void validateNumberSize(List<Integer> numbers) {
         if (numbers.size() != 3) {
             throw new IllegalArgumentException(NUMBER_SIZE_EX_MESSAGE);
+        }
+    }
+
+    private static void validateNotDuplicated(List<Integer> numbers) {
+        Set<Integer> checkDuplicateNumbers = new HashSet<>(numbers);
+        if (checkDuplicateNumbers.size() != 3) {
+            throw new IllegalArgumentException(NUMBER_NOT_DUPLICATED_EX_MESSAGE);
         }
     }
 }
