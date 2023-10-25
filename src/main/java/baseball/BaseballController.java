3394c7841a15a350e41c4378fb94764b6bb97be7
jhsseonn <hs125bt@gmail.com>
2023-10-25 17:31:43 +0900

    style: 안 쓰는 패키지 임포트 삭제

--- src/main/java/baseball/BaseballController.java
+++ src/main/java/baseball/BaseballController.java
@@ -1,7 +1,5 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class BaseballController {
 
     private BaseballException baseballException = new BaseballException();
--- src/main/java/baseball/BaseballService.java
+++ src/main/java/baseball/BaseballService.java
@@ -2,10 +2,6 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
-
 public class BaseballService {
 
     private BaseballException baseballException = new BaseballException();
