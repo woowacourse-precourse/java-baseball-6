15f817ef2cbbdd20bb5fcd659ff548be20bd7667
KongJunWoo <joonoo3@naver.com>
2023-10-25 17:48:18 +0900

    feat : 전체 게임 제어
    - 게임을 먼저 생성
    - 게임을 실행
    - isRestart 반환 값을 통해 재시작 여부 결정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import baseball.controller.Game;
+
 public class Application {
     public static void main(String[] args) {
+
+        do {
+            Game game = Game.create(); //game 객체 생성
+            game.start(); //game 시작
+        }while(Game.isRestart());
+
         // TODO: 프로그램 구현
     }
 }
