b99f154029c75fd5cfeef4fbf24ec72cc05da5de
박채연 <parkchaeyeon@bagchaeyeon-ui-MacBookAir.local>
2023-10-25 23:24:16 +0900

    feat : 실행 클래스 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Game game = new Game();
+        Judgment judgment = new Judgment();
+
+        game.startGame();
+        List<Integer> computerNumbers = game.generateRandomNumber();
+
+        while (true) {
+            List<Integer> playerNumbers = game.inputPlayerNumbers();
+            int[] counts = judgment.countMatchingNumbers(computerNumbers, playerNumbers);
+            judgment.printHint(counts[0], counts[1]);
+
+            if (counts[0] == 3) {
+                if (!judgment.askForRestartOrExit()) {
+                    break;
+                }
+                computerNumbers = game.generateRandomNumber();
+            }
+        }
     }
 }
