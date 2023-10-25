1b0e79ad8b08f425a8ca85b42d7a5731ce549e1b
sojungpp <sophie2458@naver.com>
2023-10-25 09:47:44 +0900

    refactor: 불필요한 import문 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,12 +3,6 @@ package baseball;
 import baseball.controller.BaseballController;
 import baseball.view.InputView;
 import baseball.view.OutputView;
-import camp.nextstep.edu.missionutils.Randoms;
-import camp.nextstep.edu.missionutils.Console;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
 
