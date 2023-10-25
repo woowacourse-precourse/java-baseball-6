17417b2da863d94aabdcc95039c651048c0ec923
bokyung <bbookng@gmail.com>
2023-10-25 10:58:53 +0900

    [Feat] Result 구현 완료 - Result 생성자 구현 완료 - getMessage method 구현

--- src/main/java/baseball/model/Result.java
+++ src/main/java/baseball/model/Result.java
@@ -7,7 +7,20 @@ public class Result {
     private String message;
 
     public Result(int strikeCount, int ballCount) {
+        this.strikeCount = strikeCount;
+        this.ballCount = ballCount;
 
+        String message = "";
+
+        if (ballCount > 0) message += String.format("%d볼 ", this.ballCount);
+        if (strikeCount > 0) message += String.format("%d스트라이크", this.strikeCount);
+        if (message.isEmpty()) message += "낫싱";
+
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
     }
 
 }
