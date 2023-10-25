4aed89b3c2c131ba582ddb63422b243b694a6849
방준호 <yureka0920@naver.com>
2023-10-25 14:26:26 +0900

    readme 작성.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,11 +9,9 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
-        try {
+
             // TODO: 프로그램 구현
             boolean reGame = true;
-
-
             while(reGame) {
                 reGame = false;
                 System.out.println("숫자 야구 게임을 시작합니다.");
@@ -29,8 +27,7 @@ public class Application {
                     System.out.println("숫자를 입력해주세요 :");
                     String userInput = readLine();
                     if(userInput.length()>3){
-                        System.out.println("숫자는 3개만 입력해주세요");
-                        throw new IllegalArgumentException();
+                        throw new IllegalArgumentException("숫자는 3개만 입력해주세요");
                     }
                     int strikeCount = 0;
                     int ballCount = 0;
@@ -66,13 +63,12 @@ public class Application {
 
                     //맞으면
                     if (strikeCount >= 3) {
-
+                        System.out.println(strikeCount + "스트라이크");
                         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                         String askReGame = readLine();
                         if(askReGame.length()>1){
-                            System.out.println("1 또는 2를 입력해주세요");
-                            throw new IllegalArgumentException();
+                            throw new IllegalArgumentException("1 또는 2를 입력해주세요");
                         }
                         if (askReGame.equals("2")) {
                             System.out.println("종료");
@@ -82,19 +78,19 @@ public class Application {
                             reGame=true;
                             retry = false;
                         }
+                    }else if((strikeCount==0)&&(ballCount==0)){
+                        System.out.println("낫싱");
                     } else {
 
-                        System.out.println(strikeCount + "스트라이크");
-                        System.out.println(ballCount + "볼");
+                        System.out.println(ballCount + "볼 "+strikeCount + "스트라이크");
+
                     }
 
 
                 }
             }
 
-        }catch (IllegalArgumentException e){
 
-        }
 
 
 
