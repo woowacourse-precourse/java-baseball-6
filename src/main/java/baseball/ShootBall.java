714bda66350d68643dd6fc0410f5878ac0d623be
서창덕 <44630705+Garodden@users.noreply.github.com>
2023-10-25 17:14:08 +0900

    fix:ShootBall.java(5)번 기능 출력문 수정  23.10.25 -서창덕

--- src/main/java/baseball/ShootBall.java
+++ src/main/java/baseball/ShootBall.java
@@ -95,11 +95,12 @@ public class ShootBall {
             System.out.print(strike+"스트라이크");
         if(ball==0 && strike==0)
             System.out.print("낫싱");
+        System.out.print("\n");
         if(strike==3){
             result = true;
-            System.out.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n");
         }
-        System.out.print("\n");
+
 
         return result;
     }
