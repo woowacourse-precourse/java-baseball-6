1fff73de0489b963e5b824c20a21d187735d6b99
m2nseop <pms246@naver.com>
2023-10-25 16:48:45 +0900

    feat: 게임루프 기능(+종료 및 재시작)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,7 +3,7 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         BaseballGame baseballGame = new BaseballGame();
-        baseballGame.gameStart();
+        baseballGame.initGame(); // 최초 게임 실행
 
     }
 }
