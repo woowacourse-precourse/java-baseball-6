0cacff7452bd984c0d0fd395a4747ca16a5bc146
twoosky <lhjae3@naver.com>
2023-10-25 19:58:19 +0900

    test: computer 숫자 비교 메서드 테스트 구현

---
+++ src/test/java/baseball/ComputerTest.java
@@ -0,0 +1,33 @@
+package baseball;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import baseball.ComputerTestConfig.TestComputer;
+import baseball.domain.Computer;
+import baseball.domain.Player;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Computer 클래스")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class ComputerTest {
+
+    @Test
+    void countSameIndexNumber_메소드는_같은_인덱스의_Player_숫자값과_일치하는_개수를_반환한다() {
+        Player player = new Player("123");
+        Computer computer = new TestComputer(List.of(1, 2, 6));
+
+        assertEquals(computer.countSameIndexNumber(player), 2);
+    }
+
+    @Test
+    void countContainNumber_메소드는_포함하고_있는_Player_숫자_개수를_반환한다() {
+        Player player = new Player("123");
+        Computer computer = new TestComputer(List.of(3, 2, 4));
+
+        assertEquals(computer.countContainNumber(player), 2);
+    }
+}
---
+++ src/test/java/baseball/ComputerTestConfig.java
@@ -0,0 +1,26 @@
+package baseball;
+
+import baseball.domain.Computer;
+import baseball.utils.NumberGenerator;
+import java.util.List;
+
+public class ComputerTestConfig {
+    public static class TestNumberGenerator implements NumberGenerator {
+        private final List<Integer> numbers;
+
+        public TestNumberGenerator(List<Integer> numbers) {
+            this.numbers = numbers;
+        }
+
+        @Override
+        public List<Integer> generate() {
+            return numbers;
+        }
+    }
+
+    public static class TestComputer extends Computer {
+        public TestComputer(List<Integer> numbers) {
+            super(new TestNumberGenerator(numbers));
+        }
+    }
+}
