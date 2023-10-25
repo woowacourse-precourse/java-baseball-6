982a9558374ee949a30a0ea79cb67312e5e0572a
beomsic <qjatjr29@khu.ac.kr>
2023-10-25 13:42:57 +0900

    :fire: remove : 사용하지 않는 인터페이스 삭제

--- src/main/java/baseball/view/OutPut.java
+++ src/main/java/baseball/view/OutPut.java
@@ -5,7 +5,5 @@ import java.io.IOException;
 public interface OutPut {
     void gameStart() throws IOException;
     void print(String msg) throws IOException;
-    void printResult(int strikeCount, int ballCount) throws IOException;
-    void collect(int strikeCount) throws IOException;
     void close() throws IOException;
 }
