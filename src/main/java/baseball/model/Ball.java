418199ed4da5770a5dcfb3c604db793eb8a5f1bf
becooq81 <becooq81@gmail.com>
2023-10-25 23:34:37 +0900

    refactor(Ball): removed methods that became useless b/c they are added to UserBalls to handle input there

--- src/main/java/baseball/model/Ball.java
+++ src/main/java/baseball/model/Ball.java
@@ -7,14 +7,9 @@ import static baseball.util.AppConstants.OUT_OF_RANGE_MESSAGE;
 import java.util.Objects;
 
 public class Ball {
-    private static final char CHAR_MIN_NUMBER = (char)(INT_MIN_NUMBER + '0');
-    private static final char CHAR_MAX_NUMBER = (char)(INT_MAX_NUMBER + '0');
 
     private final Integer number;
 
-    public Ball(char charInput) {
-        this.number = validateIsNaturalNumber(charInput);
-    }
 
     public Ball(int intInput) {
         this.number = validateIsInRange(intInput);
@@ -22,13 +17,6 @@ public class Ball {
 
     public static boolean equals(Ball a, Ball b) {
         return Objects.equals(a.number, b.number);
-    }
-
-    private Integer validateIsNaturalNumber(char charInput) {
-        if (charInput < CHAR_MIN_NUMBER || charInput > CHAR_MAX_NUMBER) {
-            throw new IllegalArgumentException(OUT_OF_RANGE_MESSAGE);
-        }
-        return Character.getNumericValue(charInput);
     }
 
     private Integer validateIsInRange(int intInput) {
