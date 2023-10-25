ee0cf2487403131120e1578a75924014076d37ac
saebyeok2 <tomato8269@naver.com>
2023-10-25 20:59:42 +0900

    refactor: 메서드 이름 변경

--- src/main/java/baseball/domain/BaseballGame.java
+++ src/main/java/baseball/domain/BaseballGame.java
@@ -11,8 +11,8 @@ import camp.nextstep.edu.missionutils.Console;
 public class BaseballGame {
     private Computer computer;
     private Player player;
-    private int ballCount;
-    private int strikeCount;
+    private int ball;
+    private int strike;
 
     public BaseballGame() {
         this.computer = new Computer();
@@ -23,7 +23,7 @@ public class BaseballGame {
         System.out.println(START_MESSAGE);
         do {
             play();
-        } while (checkRestart());
+        } while (confirmRestartOrExit());
     }
 
     public void play() {
@@ -32,13 +32,13 @@ public class BaseballGame {
             player.inputNumbers();
             compareNumbers();
             printHint();
-        } while (!isAnswer());
+        } while (!isGameComplete());
     }
 
     // 플레이어가 입력한 수와 컴퓨터가 선택한 수를 비교하여 스트라이크 / 볼 판정
     public void compareNumbers() {
-        ballCount = 0;
-        strikeCount = 0;
+        ball = 0;
+        strike = 0;
 
         for (int i = 0; i < NUMBER_SIZE; i++) {
             int computerNumber = computer.getNumberOf(i);
@@ -46,10 +46,10 @@ public class BaseballGame {
 
             if (computerNumber == playerNumber) {
                 // 같은 숫자가 같은 위치에 있으면 스트라이크
-                strikeCount++;
+                strike++;
             } else if (computer.contains(playerNumber)) {
                 // 같은 숫자가 다른 위치에 있으면 볼
-                ballCount++;
+                ball++;
             }
         }
     }
@@ -58,11 +58,11 @@ public class BaseballGame {
     public void printHint() {
         String result = "";
 
-        if (ballCount != 0) {
-            result += ballCount + "볼";
+        if (ball != 0) {
+            result += ball + "볼";
         }
-        if (strikeCount != 0) {
-            result += (result.isEmpty() ? "" : " ") + strikeCount + "스트라이크";
+        if (strike != 0) {
+            result += (result.isEmpty() ? "" : " ") + strike + "스트라이크";
         }
         if (result.isEmpty()) {
             result = "낫싱";
@@ -72,8 +72,8 @@ public class BaseballGame {
     }
 
     // 정답 확인. 3개의 숫자를 모두 맞힌 경우 true 반환
-    public boolean isAnswer() {
-        if (strikeCount == 3) {
+    public boolean isGameComplete() {
+        if (strike == 3) {
             System.out.println(END_MESSAGE);
             return true;
         }
@@ -81,7 +81,7 @@ public class BaseballGame {
     }
 
     // 게임 종료 OR 재시작 확인
-    public boolean checkRestart() {
+    public boolean confirmRestartOrExit() {
         System.out.println(RESTART_CHECK_MESSAGE);
         String input = Console.readLine();
 
