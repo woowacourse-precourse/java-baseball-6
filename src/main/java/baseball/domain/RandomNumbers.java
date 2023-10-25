5853756a1c4b44edfdca7c1590a8c8216e5419d8
BaeDongHyeon <sasd730@gmail.com>
2023-10-25 22:16:29 +0900

    REFACTOR: generate 공개 범위 수정

--- src/main/java/baseball/domain/RandomNumbers.java
+++ src/main/java/baseball/domain/RandomNumbers.java
@@ -9,7 +9,7 @@ public class RandomNumbers {
 
     private final List<Integer> numbers = new ArrayList<>();
 
-    private List<Integer> generate() {
+    public List<Integer> generate() {
         while (numbers.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!numbers.contains(randomNumber)) {
