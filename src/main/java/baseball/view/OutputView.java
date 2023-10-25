0879c274a8066b65e51f7d4676526a6ead415f32
edgar6bf <edgar6bf@gmail.com>
2023-10-25 20:58:07 +0900

    feat: OutputView 게임 시작 & 게임 종료 메시지 출력 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,6 +4,10 @@ import baseball.dto.PlayResult;
 
 public class OutputView {
 
+    public static void printGameStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
     public static void printPlayResult(PlayResult result) {
         printNothing(result.isNothing());
         printBall(result.ballCount(), result.strikeCount());
@@ -33,5 +37,9 @@ public class OutputView {
         if (ballCount > 0 && strikeCount > 0) {
             System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
         }
+    }
+
+    public static void printGameEnd() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 }
