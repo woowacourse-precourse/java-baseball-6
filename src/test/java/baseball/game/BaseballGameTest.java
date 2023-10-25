2903bc7369e009ca6a57e194dbef4e441e7fd200
kwang <shinkwang@yonsei.ac.kr>
2023-10-25 22:34:15 +0900

    style: Delete white-space

--- src/test/java/baseball/game/BaseballGameTest.java
+++ src/test/java/baseball/game/BaseballGameTest.java
@@ -1,7 +1,6 @@
 package baseball.game;
 
 import baseball.error.ErrorCode;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
