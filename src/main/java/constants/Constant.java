f4a21afe8b1d80e77141eef308a08624e424f420
changhyeonnam <hj04143@gmail.com>
2023-10-25 16:51:23 +0900

    FEAT: 모든기능 구현

---
+++ src/main/java/constants/Constant.java
@@ -0,0 +1,27 @@
+package constants;
+
+public class Constant {
+
+    public static final int ANSWER_SIZE = 3;
+    public static final int INPUT_SIZE = 3;
+    public static final int START_RANGE_NUM = 1;
+    public static final int END_RANGE_NUM = 9;
+
+    public static final int MAX_STRIKE_NUM = 3;
+
+    public static final String RESTART_TRUE = "1";
+    public static final String RESTART_FALSE = "2";
+
+
+    public static final String START_GAME_CMD = "숫자 야구 게임을 시작합니다.";
+    public static final String INPUT_NUM_CMD = "숫자를 입력해주세요 : ";
+    public static final String END_GAME_CMD = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String RESTART_GAME_CMD = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public static final String STRIKE_CMD = "스트라이크";
+    public static final String BALL_CMD = "볼";
+    public static final String NOTHING_CMD = "낫싱";
+    public static final int ZERO_COUNT = 0;
+
+
+}
---
+++ src/main/java/constants/StringError.java
@@ -0,0 +1,8 @@
+package constants;
+
+public class StringError {
+    public static final String wrongInputError = "잘못된 입력입니다.";
+    public static final String wrongLengthError = "잚못된 길이의 입력입니다.";
+    public static final String wrongTypeError = "잘못된 타입의 입력입니다.";
+    public static final String wrongEndGame = "게임이 비정상적으로 진행되었습니다.";
+}
