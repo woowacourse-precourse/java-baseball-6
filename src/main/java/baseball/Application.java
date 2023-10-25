5da9e208edac322a1211f0ec3a3b3027d711ca17
moonyeju <moonyeju8510@gmail.com>
2023-10-25 11:08:23 +0900

    FEAT: 게임 종료 및 재시작 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,7 +7,19 @@ import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
-        startGame();
+        int choice=1;
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while(true){
+            if (choice == 2) {
+                System.out.println("게임을 완전히 종료합니다.");
+                return;
+            } else {
+                startGame();
+            }
+            System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요: ");
+
+            choice = Integer.parseInt(Console.readLine());
+        }
     }
 
     public static void startGame() {
