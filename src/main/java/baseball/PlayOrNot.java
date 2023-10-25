de11e16cf166c03cf997731d0bbb5e6010a6e7db
DESKTOP-0PS92PR\전서현 <tjgus5935@gmail.com>
2023-10-25 18:03:57 +0900

    refactor: 유효성 검증 코드 클래스 분리

--- src/main/java/baseball/PlayOrNot.java
+++ src/main/java/baseball/PlayOrNot.java
@@ -4,33 +4,17 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class PlayOrNot {
     private static final String PLAY_OR_NOT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
-    private static final String CHECK_NUMBER = "숫자만 입력해주세요.";
-    private static final String VALID_NUMBER = "1 혹은 2만 입력해주세요.";
 
     public static boolean inputNum(){
         System.out.print(PLAY_OR_NOT);
         String inputNumber = Console.readLine();
 
-        int number = checkNumber(inputNumber);
-        validNumber(number);
+        ValidationNumber.checkNumber(inputNumber);
+        int number = Integer.parseInt(inputNumber);
+        ValidationNumber.validNumber(number);
 
         if (number == 1) return true;
         else return false;
-    }
-
-    private static int checkNumber(String inputNumber){
-        try{
-            int number = Integer.parseInt(inputNumber);
-            return number;
-        } catch (NumberFormatException e){
-            throw new IllegalArgumentException(CHECK_NUMBER);
-        }
-    }
-
-    private static void validNumber(int number){
-        if (number != 1 && number != 2){
-            throw new IllegalArgumentException(VALID_NUMBER);
-        }
     }
 
 }
--- src/main/java/baseball/PlayerNumber.java
+++ src/main/java/baseball/PlayerNumber.java
@@ -6,10 +6,6 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class PlayerNumber {
     private static final String NUMBER_INPUT = "숫자를 입력해주세요 : ";
-    private static final String CHECK_NUMBER = "숫자만 입력해주세요.";
-    private static final String CHECK_LENGTH = "3자리 수를 입력해주세요.";
-    private static final String CHECK_ZERO = "1~9의 수만 입력해주세요.";
-    private static final String DUPLICATE = "서로 다른 3개의 수를 입력해주세요.";
 
     private List<Integer> player;
 
@@ -22,44 +18,17 @@ public class PlayerNumber {
         String inputNumber = Console.readLine();
 
         //숫자인지 확인
-        checkNumber(inputNumber);
+        ValidationNumber.checkNumber(inputNumber);
         //숫자 배열로 변환
         List<Integer> player = StringToList(inputNumber);
         //3자리 수인지 확인
-        checkLength(player);
+        ValidationNumber.checkLength(player);
         //1~9 숫자인지 확인
-        checkZero(player);
+        ValidationNumber.checkZero(player);
         //3자리 수 중에 같은 수가 있는지 확인
-        duplicateNumber(player);
+        ValidationNumber.duplicateNumber(player);
 
         return new PlayerNumber(player);
-    }
-
-    private static void checkNumber(String inputNumber){
-        try{
-            Integer.parseInt(inputNumber);
-        } catch (NumberFormatException e){
-            throw new IllegalArgumentException(CHECK_NUMBER);
-        }
-    }
-
-    private static void checkLength(List<Integer> player){
-        if (player.size() != 3){
-            throw new IllegalArgumentException(CHECK_LENGTH);
-        }
-    }
-
-    private static void checkZero(List<Integer> player){
-        if (player.contains(0)){
-            throw new IllegalArgumentException(CHECK_ZERO);
-        }
-    }
-
-    private static void duplicateNumber(List<Integer> player){
-        Set<Integer> set = new HashSet<Integer>(player);
-        if (set.size() != 3){
-            throw new IllegalArgumentException(DUPLICATE);
-        }
     }
 
     public static List<Integer> StringToList(String inputNumber){
