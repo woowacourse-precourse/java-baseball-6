4e99d2fd9a1cc8dbfe0a7c456a534c0848bfc82f
dradnats1012 <herjebi1012@gmail.com>
2023-10-25 16:20:59 +0900

    Feat : 볼 공백 추가

--- src/main/java/baseball/message/ConsoleMessage.java
+++ src/main/java/baseball/message/ConsoleMessage.java
@@ -5,7 +5,7 @@ public enum ConsoleMessage {
     START_GAME("숫자 야구 게임을 시작합니다."),
     INPUT_NUMBER("숫자를 입력해주세요 : "),
     STRIKE("스트라이크"),
-    BALL("볼"),
+    BALL("볼 "),
     NOTHING("낫싱"),
     RE_GAME("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
