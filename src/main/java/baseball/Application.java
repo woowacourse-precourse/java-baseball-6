a02125cd3f2559054430c3cec4cb1df7519e4bf2
youngseonseo <sue000731@naver.com>
2023-10-25 22:20:59 +0900

    Console 사용해서 입력값 받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,20 +1,18 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        List<Integer> computer = new ArrayList<>();
-
         while(true){
-            System.out.println("숫자 야구 게임을 시작합니다.");
 
+            System.out.println("숫자 야구 게임을 시작합니다.");
+            List<Integer> computer = new ArrayList<>();
             while (computer.size() < 3) {               // 임의의 컴퓨터 숫자 입력
                 int randomNumber = Randoms.pickNumberInRange(1, 9);
                 if (!computer.contains(randomNumber)) {
@@ -22,11 +20,12 @@ public class Application {
                 }
             }
 
+
             while(true){
 
                 System.out.print("숫자를 입력해주세요 : ");
                 try {
-                    int num = sc.nextInt();
+                    int num = Integer.parseInt(Console.readLine());
 
                     if((int) (Math.log10(num)+1) != 3)    // 숫자가 3개 입력이 맞는지 확인
                         throw new IllegalArgumentException("숫자는 3개여야 합니다.");
@@ -82,7 +81,7 @@ public class Application {
                     if(strike ==3){
                         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                        int input = sc.nextInt();
+                        int input = Integer.parseInt(Console.readLine());
                         if(input==1){
                             break;
                         }
