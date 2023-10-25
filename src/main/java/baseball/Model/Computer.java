3b7a8c5bcb120a014be5a0fe6309f98271516086
SSUHYUNKIM <yellosuhyun@naver.com>
2023-10-25 13:45:37 +0900

    feat: 컴퓨터 랜덤 숫자 생성 기능

---
+++ src/main/java/baseball/Model/Computer.java
@@ -0,0 +1,20 @@
+package baseball.Model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private final List<Integer> randomNumber = new ArrayList<>();
+
+    private List<Integer> generateRandomNumber() {
+        List<Integer> randomNum = new ArrayList<>();
+        while(randomNum.size() < 3) {
+            int randomNumberByComputer = Randoms.pickNumberInRange(1, 9);
+            if(!randomNum.contains(randomNumberByComputer)) {
+                randomNum.add(randomNumberByComputer);
+            }
+        }
+        return randomNum;
+    }
+}
