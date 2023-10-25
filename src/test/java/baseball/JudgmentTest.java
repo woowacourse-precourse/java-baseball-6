5121e816f05534fba536a2ba935205b3c7ff2756
jihyuk <kwlgur0416@gmail.com>
2023-10-25 19:51:56 +0900

    fix : 제출 시 빌드 실패로 인한 @Nested 삭제

--- src/test/java/baseball/JudgmentTest.java
+++ src/test/java/baseball/JudgmentTest.java
@@ -8,7 +8,6 @@ import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.List;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 
@@ -38,68 +37,64 @@ public class JudgmentTest {
         assertThat(result).isEqualTo(true);
     }
 
-    @Nested
-    class 추측값이_정답이_아니고_ {
+    @Test
+    void 추측값이_정답이_아니고_볼과_스트라이크가_모두_없는_경우() {
+        // given
+        List<Integer> guess = List.of(4, 5, 6);
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
 
-        @Test
-        void 볼과_스트라이크가_모두_없는_경우() {
-            // given
-            List<Integer> guess = List.of(4, 5, 6);
-            OutputStream out = new ByteArrayOutputStream();
-            System.setOut(new PrintStream(out));
+        // when
+        boolean result = judgment.judge(guess);
 
-            // when
-            boolean result = judgment.judge(guess);
+        //then
+        assertThat(out.toString()).contains("낫싱");
+        assertThat(result).isEqualTo(false);
+    }
 
-            //then
-            assertThat(out.toString()).contains("낫싱");
-            assertThat(result).isEqualTo(false);
-        }
+    @Test
+    void 볼과_스트라이크가_모두_존재하는_경우() {
+        // given
+        List<Integer> guess = List.of(2, 5, 3);
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
 
-        @Test
-        void 볼과_스트라이크가_모두_존재하는_경우() {
-            // given
-            List<Integer> guess = List.of(2, 5, 3);
-            OutputStream out = new ByteArrayOutputStream();
-            System.setOut(new PrintStream(out));
+        // when
+        boolean result = judgment.judge(guess);
 
-            // when
-            boolean result = judgment.judge(guess);
+        //then
+        assertThat(out.toString()).contains("1볼 1스트라이크");
+        assertThat(result).isEqualTo(false);
+    }
 
-            //then
-            assertThat(out.toString()).contains("1볼 1스트라이크");
-            assertThat(result).isEqualTo(false);
-        }
+    @Test
+    void 볼만_존재하는_경우() {
+        // given
+        List<Integer> guess = List.of(2, 9, 1);
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
 
-        @Test
-        void 볼만_존재하는_경우() {
-            // given
-            List<Integer> guess = List.of(2, 9, 1);
-            OutputStream out = new ByteArrayOutputStream();
-            System.setOut(new PrintStream(out));
+        // when
+        boolean result = judgment.judge(guess);
 
-            // when
-            boolean result = judgment.judge(guess);
+        //then
+        assertThat(out.toString()).contains("2볼");
+        assertThat(result).isEqualTo(false);
+    }
 
-            //then
-            assertThat(out.toString()).contains("2볼");
-            assertThat(result).isEqualTo(false);
-        }
+    @Test
+    void 스트라이크만_존재하는_경우() {
+        // given
+        List<Integer> guess = List.of(1, 2, 7);
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
 
-        @Test
-        void 스트라이크만_존재하는_경우() {
-            // given
-            List<Integer> guess = List.of(1, 2, 7);
-            OutputStream out = new ByteArrayOutputStream();
-            System.setOut(new PrintStream(out));
+        // when
+        boolean result = judgment.judge(guess);
 
-            // when
-            boolean result = judgment.judge(guess);
-
-            //then
-            assertThat(out.toString()).contains("2스트라이크");
-            assertThat(result).isEqualTo(false);
-        }
+        //then
+        assertThat(out.toString()).contains("2스트라이크");
+        assertThat(result).isEqualTo(false);
     }
 
 }
