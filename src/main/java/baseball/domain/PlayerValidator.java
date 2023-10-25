347a9775d34fb322847a9ef373ab29b648401728
gundorit <packdev937@gmail.com>
2023-10-25 13:38:05 +0900

    feat (PlayerValidator) : 숫자가 1~9 사이의 수로 이루어졌는지 검증한다

--- src/main/java/baseball/domain/PlayerValidator.java
+++ src/main/java/baseball/domain/PlayerValidator.java
@@ -8,11 +8,15 @@ public class PlayerValidator {
 
     public static final String NUMBER_SIZE_EX_MESSAGE = "입력값은 세 자리의 숫자여야 합니다.\n";
     public static final String NUMBER_NOT_DUPLICATED_EX_MESSAGE = "입력값은 중복되는 수가 하나라도 있으면 안됩니다.\n";
+    public static final String NUMBER_RANGE_EX_MESSAGE = "모든 숫자는 1에서 9 사이의 값을 가져야 합니다.\n";
+
 
 
     public static void validate(List<Integer> numbers) {
         validateNumberSize(numbers);
         validateNotDuplicated(numbers);
+        validateNumberRange(numbers);
+
     }
 
     private static void validateNumberSize(List<Integer> numbers) {
@@ -25,6 +29,14 @@ public class PlayerValidator {
         Set<Integer> checkDuplicateNumbers = new HashSet<>(numbers);
         if (checkDuplicateNumbers.size() != 3) {
             throw new IllegalArgumentException(NUMBER_NOT_DUPLICATED_EX_MESSAGE);
+        }
+    }
+
+    private static void validateNumberRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < 1 || number > 9) {
+                throw new IllegalArgumentException(NUMBER_RANGE_EX_MESSAGE);
+            }
         }
     }
 }
