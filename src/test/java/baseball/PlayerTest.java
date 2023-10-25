fe0cf560302cf68db13ec947083d1394dccffa15
twoosky <lhjae3@naver.com>
2023-10-25 19:44:05 +0900

    test: nested 제거

--- src/test/java/baseball/PlayerTest.java
+++ src/test/java/baseball/PlayerTest.java
@@ -5,25 +5,18 @@ import static baseball.utils.ErrorMessages.PLAYER_NUMBERS_INVALID_SIZE;
 import baseball.domain.Player;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("Player 클래스")
 public class PlayerTest {
 
-    @Nested
-    class 생성자는 {
-        @Nested
-        class 입력값의_길이가_3이_아니라면 {
-            @ParameterizedTest
-            @ValueSource(strings = {"1", "12", "123456789"})
-            void IllegalArgumentException_예외를_던진다(String input) {
-                Assertions.assertThatThrownBy(() -> new Player(input))
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessageContaining(PLAYER_NUMBERS_INVALID_SIZE);
-            }
-        }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "12", "123456789"})
+    void IllegalArgumentException_예외를_던진다(String input) {
+        Assertions.assertThatThrownBy(() -> new Player(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(PLAYER_NUMBERS_INVALID_SIZE);
     }
 }
