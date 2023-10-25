6d906aff40b9c343014e6b79bd72cca21bb7ec92
jjw0071 <jjw0071@naver.com>
2023-10-25 20:33:06 +0900

    feat : 플레이어 객체 생성

---
+++ src/main/java/baseball/Player.java
@@ -0,0 +1,21 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.HashSet;
+
+
+public class Player {
+
+    private int length;
+
+    Player(int length){
+        this.length = length;
+    }
+
+    private Player() {
+    }
+
+
+
+}
