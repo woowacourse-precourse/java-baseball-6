d29dcc82de807613e6865e57647210c096967569
peeaaccee <wlgp8958@naver.com>
2023-10-25 19:32:32 +0900

    feat(Game): Game객체의 필드와 생성자 추가

---
+++ src/main/java/baseball/domain/controller/Game.java
@@ -0,0 +1,10 @@
+package baseball.domain.controller;
+
+public class Game {
+    public static final String INIT_GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public static final String RESTART_SIGNAL = "1";
+    public static final String EXIT_SIGNAL = "2";
+
+    private Game() {
+    }
+}
