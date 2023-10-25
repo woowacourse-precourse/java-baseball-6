bd6f4c24562a60f3abef08720a7c0e79689bf317
SangHee Lee <lsh277604@gmail.com>
2023-10-25 21:20:25 +0900

    fix: pass test

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,12 +4,13 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+
         Human human = new Human();
-        Computer computer = new Computer();
+        Computer computer;
         boolean systemFlag = true;
 
         while (systemFlag) {
-//            computer = new Computer();
+            computer = new Computer();
             System.out.println("숫자 야구 게임을 시작합니다.");
             boolean gameFlag = true;
 
