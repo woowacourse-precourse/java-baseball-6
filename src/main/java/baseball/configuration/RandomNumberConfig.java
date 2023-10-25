780a93c3c92e4e18388b3affdc5e283b23c6ef25
Sung Hyobin <hellmir610@gmail.com>
2023-10-25 20:59:24 +0900

    feat: Add function of generating not duplicated random numbers

---
+++ src/main/java/baseball/configuration/RandomNumberConfig.java
@@ -0,0 +1,9 @@
+package baseball.configuration;
+
+public class RandomNumberConfig {
+
+    public static final int START_INCLUSIVE = 1;
+    public static final int END_INCLUSIVE = 9;
+    public static final int COUNT = 3;
+
+}
