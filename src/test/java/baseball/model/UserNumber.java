1dcebd946e2a427e9f939ef75839f2b08295bbd9
Laniel Park <Laniel8177@gmail.com>
2023-10-25 19:42:37 +0900

    chore : add public keyword & delete unused import

--- src/test/java/baseball/model/UserNumber.java
+++ src/test/java/baseball/model/UserNumber.java
@@ -1,11 +1,9 @@
 package baseball.model;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
-class UserNumber extends Number {
+public class UserNumber extends Number {
 
     public UserNumber(String input) {
         super(parseInput(input));
