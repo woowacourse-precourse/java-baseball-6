3519cd225feb0f839c0b841567c7879ea5f2efda
HyeJi Choi <choi@HyeJiui-MacBookAir.local>
2023-10-25 21:31:13 +0900

    1주 차 미션 제출_1

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,20 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        while(true){
+            if(Game.start() == 2){
+                break;
+            }
+            else {
+                /* continue */
+            }
+        }
     }
 }
