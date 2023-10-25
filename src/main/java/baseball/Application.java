66fb54ff83f9233b4e76dcd77bb36a339ac59853
wochae <officialwochae@gmail.com>
2023-10-25 13:06:22 +0900

    [FIX] : do while should not use

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,13 +5,13 @@ public class Application {
   public static void main(String[] args) {
     // TODO: 프로그램 구현
     int restartStatus = 0;
-    do {
+    while (restartStatus == 0) {
       try {
         Play play = new Play();
         restartStatus = play.gameSet();
       } catch (IllegalArgumentException e) {
         throw new IllegalArgumentException("입력 형식이 올바르지 않습니다.");
       }
-    } while (restartStatus == 1);
+    }
   }
 }
