b294c154192363ff84b29335fb5a5637826c4b62
LeeSungNo-ian <isnn0103@gmail.com>
2023-10-25 20:11:25 +0900

    feat: 정답을 맞췄을 시 재시작 혹은 종료 매서드 생성

--- src/main/java/controller/BaseballGameController.java
+++ src/main/java/controller/BaseballGameController.java
@@ -19,13 +19,21 @@ public class BaseballGameController {
 
     private boolean isFirstRound = true;
 
+    public static final int GAME_ONGOING = 0;
+    public static final int CONTINUE_GAME = 1;
+    public static final int FINISH_GAME = 2;
+
+    private int gameStatus = GAME_ONGOING;
+
     public void gameProgress() {
+        playGameInformationOutputView.printGameStart();
+        gameStatus = GAME_ONGOING;
+
         while (true) {
             Set<Integer> computerBalls = null;
             Set<Integer> playerBalls = null;
 
             if (isFirstRound) {
-                playGameInformationOutputView.printGameStart();
                 computer.generateBalls();
                 computerBalls = computer.getComputerNumber();
                 isFirstRound = true;
@@ -38,7 +46,31 @@ public class BaseballGameController {
             String gameResult = referee.judgeValue(computerBalls, playerBalls);
             System.out.println(gameResult);
 
-            playerGameRestartUtil.gameRestartQuestion();
+            if (gameResult.equals("3스트라이크")) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                decideAdditionalGame();
+            }
+
+            if (gameStatus == FINISH_GAME) {
+                break;
+            }
+
+            if (gameStatus == CONTINUE_GAME) {
+                computer.initComputerBall();
+                isFirstRound = true;
+                gameStatus = GAME_ONGOING;
+                System.out.println("게임 재시작");
+            }
+        }
+    }
+
+    private void decideAdditionalGame() {
+        String nextGameState = playerGameRestartUtil.gameRestartQuestion();
+        if (nextGameState.equals("1")) {
+            gameStatus = CONTINUE_GAME;
+        }
+        if (nextGameState.equals("2")) {
+            gameStatus = FINISH_GAME;
         }
     }
 }
--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -4,16 +4,17 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class Player {
-    private final Set<Integer> playerBall = new LinkedHashSet<>();
+    public final Set<Integer> playerBall = new LinkedHashSet<>();
 
     public void generateBalls(String balls) {
+        initPlayerBall();
         for (int ballIndex = 0; ballIndex < balls.length(); ballIndex++) {
             int uniqueNumber = balls.charAt(ballIndex) - '0';
             playerBall.add(uniqueNumber);
         }
     }
 
-    public void initPlayerBall() {
+    private void initPlayerBall() {
         playerBall.clear();
     }
 
