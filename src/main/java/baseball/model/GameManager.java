50b00447537cd03a5a8559bd37eeecdfd6cb20d8
Cycrypto <jh01love@naver.com>
2023-10-25 12:58:55 +0900

    refactor:컨벤션에 맞게 코드 수정

--- src/main/java/baseball/model/GameManager.java
+++ src/main/java/baseball/model/GameManager.java
@@ -4,28 +4,32 @@ import baseball.controller.GameUtils;
 import java.util.*;
 
 public class GameManager {
-    private GameCompetitor counterPlayer;
-    private List<Integer> computerNumber;
-    private GameUtils utility;
+    private final List<Integer> computerNumber;
+    private final GameUtils utility;
 
     public GameManager() {
-        this.counterPlayer = new GameCompetitor();
-        this.computerNumber = this.counterPlayer.generateRandomNumber();
+        GameCompetitor counterPlayer = new GameCompetitor();
+        this.computerNumber = counterPlayer.generateRandomNumber();
         this.utility = new GameUtils();
     }
 
     public GameStatusCode processTurn(String playerNumber) {
+        GameStatusCode result = null;
         if (utility.isValidInputForm(playerNumber)) {
             List<Integer> gameTurnResult = GameUtils.compareNumbers(playerNumber, computerNumber);
 
-            GameUtils.printGameTurnResult(gameTurnResult);
+            GameUtils.GameTurnResult(gameTurnResult);
             System.out.println();
+
             if (GameUtils.isGameOver(gameTurnResult)) {
-                return GameStatusCode.STOP;
+                result = GameStatusCode.STOP;
             }
         } else {
             throw new IllegalArgumentException("잘못된 입력입니다.");
         }
-        return GameStatusCode.PLAYING;
+        if (result == null) {
+            result = GameStatusCode.PLAYING;
+        }
+        return result;
     }
 }
