5ae7a9b7c6359896912a0918224c5c5200a7ca67
uhyunglee <uhyunglee@naver.com>
2023-10-25 20:37:37 +0900

    [DOCS] : ADD FUNCTION SPEC

--- src/main/java/baseball/domain/JudgeNumber.java
+++ src/main/java/baseball/domain/JudgeNumber.java
@@ -3,7 +3,7 @@ package baseball.domain;
 import java.util.List;
 
 public class JudgeNumber {
-    public int correctCount(List<Integer> computer, List<Integer> player){
+    public int count(List<Integer> computer, List<Integer> player){
         return 0;
     }
     public boolean isNumber(){
