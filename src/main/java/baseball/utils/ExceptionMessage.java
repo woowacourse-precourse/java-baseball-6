a009e19f80ac78df6445d34622d9c755ce46e55a
alxqkekrroqkfwk <fexxet@icloud.com>
2023-10-25 21:17:31 +0900

    [style] : code clean up

--- src/main/java/baseball/utils/ExceptionMessage.java
+++ src/main/java/baseball/utils/ExceptionMessage.java
@@ -2,11 +2,11 @@ package baseball.utils;
 
 public enum ExceptionMessage {
 
-    LENGTH("3자리 숫자를 입력해주세요.") ,
+    LENGTH("3자리 숫자를 입력해주세요."),
 
-    NUMBER("숫자만 입력해주세요.") ,
+    NUMBER("숫자만 입력해주세요."),
 
-    OVERLAP("서로 다른 임의의 수 3개를 입력해야 합니다.") ,
+    OVERLAP("서로 다른 임의의 수 3개를 입력해야 합니다."),
 
     INVALID("잘못된 값을 입력하셨습니다.");
 
