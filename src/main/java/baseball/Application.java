0d17923defb1da2fbc7ff1372862d0025814d41e
9gi_coach22 <9gi_coach22@ssafy.com>
2023-10-25 16:59:01 +0900

    Feat : 변수 수정 및 3볼 로직 오류 해결

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,12 +5,15 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
+
+    public static int ballCount = 3; // 해당 자릿수의 숫자야구 가능
+    public static int strikeCount = ballCount+1;
     
     public static HashMap<String, String> makeAnswer() {
 
         HashMap<String, String> answerIndexMap = new HashMap<>();
 
-        for (int i = 0; i < 3;) {
+        for (int i = 0; i < ballCount;) {
             String randomNumber = Integer.toString(Randoms.pickNumberInRange(1, 9));
             if (!answerIndexMap.containsKey(randomNumber)) {
                 answerIndexMap.put(randomNumber, Integer.toString(i)); // { answer : index }
@@ -33,7 +36,7 @@ public class Application {
             System.out.printf("숫자를 입력해주세요 : ");
             String inputString = Console.readLine();
 
-            if (inputString.length() != 3) {
+            if (inputString.length() != ballCount) {
                 throw new IllegalArgumentException();
             }
 
@@ -41,7 +44,7 @@ public class Application {
 
             HashMap<String, String> duplicatedCheckMap = new HashMap<>();
 
-            for (int i = 0; i < 3; i++) {
+            for (int i = 0; i < ballCount; i++) {
                 String index = Integer.toString(i);
                 String inputAnswer = Character.toString(inputString.charAt(i));
 
@@ -53,7 +56,7 @@ public class Application {
 
                 if (answerIndexMap.containsKey(inputAnswer)) { // answer가 key 값으로 존재할 경우,
                     if (answerIndexMap.get(inputAnswer).equals(index)) { // index까지 일치하는 경우
-                        score += 3;
+                        score += strikeCount;
                     } else {
                         score += 1;
                     }
@@ -64,24 +67,24 @@ public class Application {
 
             if (score == 0) {
                 printString = "낫싱";
-            } else if (score == 9) {
-                printString = "3스트라이크";
+            } else if (score == strikeCount * ballCount) {
+                printString = String.format("%d스트라이크", ballCount);
             } else {
-                int strike = score/3;
-                int ball = score%3;
+                int strike = score/strikeCount;
+                int ball = score%strikeCount;
                 if (ball == 0) {
                     printString = String.format("%d스트라이크", strike);
                 } else if (strike == 0) {
                     printString = String.format("%d볼", ball);
                 } else {
                     printString = String.format("%d볼 %d스트라이크", ball, strike);
-                };
+                }
             }
 
             System.out.println(printString);
 
-            if (score == 9) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            if (score == strikeCount * ballCount) {
+                System.out.printf("%d개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n", ballCount);
                 playingGame = Integer.parseInt(Console.readLine());
 
                 if (playingGame != 1 && playingGame != 2) {
