69500b3cac82650db7843ec423b3d93b4689843e
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-10-25 14:02:49 +0900

    fix: printResult(), add nothing case

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -70,6 +70,9 @@ public class Application {
         if(strike!=0){
             System.out.print(strike+"스트라이크 ");
         }
+        if(ball==0 && strike==0){
+            System.out.print("낫싱");
+        }
         System.out.println();
         if(strike==3){
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
