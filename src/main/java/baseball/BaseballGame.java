40abfa55845d68e87e811aefa59c502967b22ab1
plan11plan <tjtfy7028@gmail.com>
2023-10-25 18:33:38 +0900

    fix: 종료시점에 2를 눌렀을때 한번 더 숫자 물어보는 버그 수정

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -61,6 +61,7 @@ public class BaseballGame {
 
         if (strike == 3) {
             ending();
+            return;
         }
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
