3d8ed8df95f4a8386c85fd3c47f004829bc5191e
조은별 <joeunbyeol98@gmail.com>
2023-10-25 20:10:51 +0900

    feat(ErrorMessage): 게임 명령어 예외 메시지 추가

--- src/main/java/baseball/utils/ErrorMessage.java
+++ src/main/java/baseball/utils/ErrorMessage.java
@@ -7,4 +7,5 @@ public class ErrorMessage {
     public static String NUMBERS_SIZE_IS_INVALID = ERROR_TAG + "3개의 숫자만 가능합니다.\n";
     public static String NUMBERS_OUT_OF_RANGE = ERROR_TAG + "1~9까지 3개의 숫자를 공백 없이 나열해주세요.\n";
     public static String NUMBERS_DUPLICATED = ERROR_TAG + "서로 다른 숫자만 가능합니다.\n";
+    public static String COMMAND_CODE_IS_INVALID = ERROR_TAG + "1 혹은 2 숫자만 가능합니다.\n";
 }
