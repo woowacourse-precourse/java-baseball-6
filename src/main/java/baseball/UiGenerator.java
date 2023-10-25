7380426573858f3cf2e579ec8b511efcbe3d5d75
권태현 <kygkth2011@gmail.com>
2023-10-25 22:47:10 +0900

    feat(UiGenerator) : 볼, 스트라이크 출력 UI 구현

--- src/main/java/baseball/UiGenerator.java
+++ src/main/java/baseball/UiGenerator.java
@@ -2,22 +2,25 @@ package baseball;
 
 public class UiGenerator {
     public void printGameStart() {
-        System.out.println("게임을 시작합니다.");
+        System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
     public void printRequestUserPrediction() {
-        System.out.print("숫자를 입력해주세요.");
+        System.out.print("숫자를 입력해주세요 : ");
     }
 
     public void printEndCurrentGameAndAskNewGame() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
-    public void printStrikeBall(int strike, int ball) {
-        if (strike + ball == 0) {
+    public void printStrikeBall(GuessStatus guessStatus) {
+        if (guessStatus.isNoting()) {
             System.out.println("낫싱");
             return;
         }
+        
+        int strike = guessStatus.getStrike();
+        int ball = guessStatus.getBall();
         if (ball > 0) {
             System.out.print(ball + "볼 ");
         }
