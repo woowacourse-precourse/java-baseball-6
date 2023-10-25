3e8ff9bbf2f872263aad1e3748e986aa54f5917c
ykmxxi <ykmxxi97@gmail.com>
2023-10-25 15:45:54 +0900

    refactor(PlayerTest): `@DisplayName`을 통해 테스트 메소드의 의도를 드러내도록 변경

--- src/test/java/baseball/domain/PlayerTest.java
+++ src/test/java/baseball/domain/PlayerTest.java
@@ -3,6 +3,7 @@ package baseball.domain;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -10,12 +11,14 @@ class PlayerTest {
 
 	@ParameterizedTest
 	@ValueSource(strings = {"123", "456", "789"})
+	@DisplayName("플레이어 생성 성공")
 	void givenNumbers_whenCreatePlayer_thenNoException(String numbers) {
 		assertThatNoException().isThrownBy(() -> new Player(numbers));
 	}
 
 	@ParameterizedTest
 	@ValueSource(strings = {"12삼", "1@3", "12e"})
+	@DisplayName("플레이어 생성 실패: 잘못된 입력이 들어오면 예외 발생")
 	void givenInput_whenValidateNumber_thenThrowIllegalArgumentException(String input) {
 		assertThatThrownBy(() -> new Player(input))
 				.isInstanceOf(IllegalArgumentException.class)
