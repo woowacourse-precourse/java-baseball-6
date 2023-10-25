b313c525f14de8b34808fcdb6fdfd0e9913c5424
동원 <ai06021@gmail.com>
2023-10-25 18:28:11 +0900

    fix : input값이 안 받아지는것 같아서 수정

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -1,30 +1,35 @@
 package baseball;
 
-import java.util.ArrayList;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Input {
-    String input = Console.readLine();
+
     public List<Integer> numbers() {
+        String input = Console.readLine();
+
         List<Integer> inputNumber = new ArrayList<>();
         System.out.print("숫자를 입력해주세요 : ");
         for (int i = 0; i < input.length(); i++) {
             char num = input.charAt(i);
-            if (num < '0' || num > '9')
+            if (num < '0' || num > '9') {
                 break;
+            }
             int value = Integer.parseInt(String.valueOf(num));
             inputNumber.add(value);
         }
-        if (!inputCheck(inputNumber))
+        if (!inputCheck(inputNumber)) {
             inputNumber.clear();
+        }
         return inputNumber;
     }
 
     private boolean inputCheck(List<Integer> numbers) {
         try {
-            if (numbers.size() == 3)
+            if (numbers.size() == 3) {
                 throw new IllegalArgumentException();
+            }
         } catch (IllegalArgumentException e) {
             return false;
         }
@@ -33,9 +38,11 @@ public class Input {
 
     public int newGameCheck() {
         try {
+            String input = Console.readLine();
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
-            if (input.length() != 1)
+            if (input.length() != 1) {
                 throw new IllegalArgumentException();
+            }
             int newgame = Integer.parseInt(input);
             if (newgame == 1) {
                 return 1;
