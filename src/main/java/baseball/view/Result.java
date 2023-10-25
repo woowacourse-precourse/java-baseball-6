08b85ce931e109b896543cb191a0cb0ea1abd2bc
yj-melissa <melimelame@gmail.com>
2023-10-25 22:37:45 +0900

    fix: 문자열 출력 형식 수정

--- src/main/java/baseball/view/Result.java
+++ src/main/java/baseball/view/Result.java
@@ -3,7 +3,7 @@ package baseball.view;
 import baseball.model.Score;
 
 public class Result {
-    private static final String BALL_MESSAGE = "볼";
+    private static final String BALL_MESSAGE = "볼 ";
     private static final String STRIKE_MESSAGE = "스트라이크";
     private static final String NOTHING_MESSAGE = "낫싱";
 
