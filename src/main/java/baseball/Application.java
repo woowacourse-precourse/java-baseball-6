109fb4343e37994ffda122c90bb288eccab1f90b
sunwoong <sunwoong1122@gmail.com>
2023-10-25 21:03:42 +0900

    del: todo 주석 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,9 +5,8 @@ import baseball.controller.MainController;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         DependencyContainer dependencyContainer = new DependencyContainer();
         MainController mainController = dependencyContainer.getMainController();
-        mainController.startBaseballGame();
+        mainController.startGame();
     }
 }
