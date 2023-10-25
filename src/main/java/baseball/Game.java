d03a95016e820f6f2b7700109df50774e5a1d288
ziy00n <jiyun4188@gmail.com>
2023-10-25 15:37:15 +0900

    feat : 입력값에 대한 검증 기능 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -6,6 +6,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Game {
 
@@ -31,17 +32,49 @@ public class Game {
     }
 
     // 플레이어한테 숫자 입력받기
-    public List<Integer> InputNum() {
+    public List<Integer> inputNum() {
         System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
-        List<Integer> inputNum = new ArrayList<>();
+        ValidateInputNum(input);
+        List<Integer> inputNumber = new ArrayList<>();
         for(int i=0; i < input.length(); i++) {
             char ch = input.charAt(i);
             if (Character.isDigit(ch)) {
                 int num = Character.getNumericValue(ch);
-                inputNum.add(num);
+                inputNumber.add(num);
             }
         }
-        return inputNum;
+        return inputNumber;
+    }
+
+    // 입력받은 문자열이 숫자가 맞는지 검증
+    public boolean isValidNumber(String input) {
+        String pattern = "^[1-9]*$";
+        if (!Pattern.matches(pattern, input)) {
+            throw new IllegalArgumentException("입력값이 올바르지 않아 종료합니다.");
+        }
+        return true;
+    }
+
+    // 입력받은 문자열이 세자리인지 검증
+    public boolean isThreeDigit(String input) {
+        if(input.length() != 3) {
+            throw new IllegalArgumentException("입력값이 세자리가 아니므로 종료합니다.");
+        }
+        return true;
+    }
+
+    // 입력받은 문자열에 대해 중복 검증
+    public boolean isNotDuplicate(String input) {
+        if(input.charAt(0) == input.charAt(1) || input.charAt(1) == input.charAt(2) || input.charAt(0) == input.charAt(2)){
+            throw new IllegalArgumentException("입력값에 중복된 값이 존재하므로 종료합니다.");
+        }
+        return true;
+    }
+
+    public void ValidateInputNum(String input) {
+        if (!isValidNumber(input) || !isThreeDigit(input) || isNotDuplicate(input)) {
+            throw new IllegalArgumentException();
+        }
     }
 }
