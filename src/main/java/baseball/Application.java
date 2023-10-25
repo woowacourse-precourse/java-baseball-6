91e29ac69115e1387c063a49586a1a7a765837f3
SiHyun-Lee113 <82255633+SiHyun-Lee113@users.noreply.github.com>
2023-10-25 15:14:48 +0900

    [우테코] 2차 수정 - 입력 메서드 분리 - 출력 메서드 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,12 +2,13 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.sql.Array;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class Application {
     public static void main(String[] args) {
@@ -27,12 +28,13 @@ public class Application {
                 }
             }
 
-            while(true) {
+            while (true) {
                 int strikes = 0;
                 int balls = 0;
+
                 // 숫자야구 입력 로직
                 System.out.print("숫자를 입력해 주세요 : ");
-                String input = inputStringAndVerification("forInput");
+                String input = inputForGaming();
                 Map<Integer, Integer> map = new HashMap<>();
                 int i = 0;
                 for (String s : input.split("")) {
@@ -54,18 +56,32 @@ public class Application {
                 }
 
                 // 입력 결과
+                printResult(strikes, balls);
                 if (strikes == 3) {
-                    System.out.printf("%d스트라이크\n", strikes);
-                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                    String progressOrNot = inputStringAndVerification("forGame");
+                    String progressOrNot = inputForProgress();
                     gameCondition = progressOrNot.equals("2");
                     break;
-                } else if (strikes == 0 && balls == 0) {
+                }
+            }
+        }
+    }
+
+    public static void printResult(int strikes, int balls) {
+        switch (strikes) {
+            case 3 -> {
+                System.out.printf("%d스트라이크\n", strikes);
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            }
+            case 0 -> {
+                if (balls == 0) {
                     System.out.println("낫싱");
-                } else if (strikes == 0){
+                } else {
                     System.out.printf("%d볼\n", balls);
-                } else if (balls == 0) {
+                }
+            }
+            default -> {
+                if (balls == 0) {
                     System.out.printf("%d스트라이크\n", strikes);
                 } else {
                     System.out.printf("%d볼 %d스트라이크\n", balls, strikes);
@@ -74,30 +90,34 @@ public class Application {
         }
     }
 
-    public static String inputStringAndVerification(String purpose) {
-        String input = "";
-        // 중복 확인용
+    public static String inputForProgress() {
+        String input = Console.readLine();
+        if (!(input.equals("1") || input.equals("2"))) {
+            throwError();
+        }
+        return input;
+    }
 
-        input = Console.readLine();
+    public static String inputForGaming() {
+        String input = Console.readLine();
         HashSet<String> set = new HashSet<>(Arrays.asList(input.split("")));
-
-        int numbers = 0;
-        // 숫자가 아닌 입력 or 공백 포함한 수 제거
         try {
-            numbers = Integer.parseInt(input);
+            // 숫자가 아닌 입력 or 공백 포함한 수 제거
+            int numbers = Integer.parseInt(input);
+
+            // 음수 or 3자리가 아닌 입력
+            // 중복 확인 or 0 포함 수 제거
+            if (set.size() < 3 || set.contains("0") || numbers <= 100 || numbers > 999) {
+                throwError();
+            }
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("올바르지 않은 입력입니다. 게임 종료");
+            throwError();
         }
-
-        if (purpose.equals("forGame")) {
-            if (numbers == 1 || numbers == 2)
-                return input;
-        }
-
-        // 음수 or 3자리가 아닌 입력
-        // 중복 확인 or 0 포함 수 제거
-        if (set.size() < 3 || set.contains("0") || numbers <= 100 || numbers > 999) throw new IllegalArgumentException("올바르지 않은 입력입니다. 게임 종료");
 
         return input;
+    }
+
+    public static void throwError() {
+        throw new IllegalArgumentException("올바르지 않은 입력입니다. 게임 종료");
     }
 }
