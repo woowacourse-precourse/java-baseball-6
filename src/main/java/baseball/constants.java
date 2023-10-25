d4f87573ba8703524b79304a3b0b3435fffd06a0
yev <ye897@naver.com>
2023-10-25 20:10:38 +0900

    상수 작성

---
+++ src/main/java/baseball/constants.java
@@ -0,0 +1,9 @@
+package baseball;
+
+public final class constants {
+
+    public static final Integer MIN_NUMBER = 1;
+    public static final Integer MAX_NUMBER = 9;
+    public static final Integer RESTART_NUMBER = 1;
+    public static final Integer END_NUMBER = 2;
+}
