c76d1fe8d3c7ee40777360eeb1fcf04d1c528245
dongha922 <kdh4575@naver.com>
2023-10-25 23:08:52 +0900

    변수선언

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,37 +1,69 @@
 package baseball;
-//import camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
-//import camp.nextstep.edu.missionutils.Console.readLine;
+import java.util.Date;
+import java.util.List;
+
+import javax.naming.NamingException;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("숫자 야구를 시작합니다");
+        Game rightAnswer = new Game();
+        //Game.init(rightAnswer);
     }
 }
-public class Game{
-    private List<Integer> number;
-    public Game(){
-
+class Array {
+    static boolean checkArrayContains(final int[] arr, final int number) {
+        for (int value : arr) {
+            if (value == number) {
+                return true;
+            }
+        }
+        return false;
     }
-    public void run(){
-        try{
-            do {
-                play();
-            } while (reStrart() == true);
-        }catch (IllegalArgumentException e){
-            throw e;
+
+    static boolean checkArrayContains(final char[] arr, final char number) {
+        for (int value : arr) {
+            if (value == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static char[] getCharArrayFromString(String str) {
+        char[] charArray = new char[str.length()];
+
+        for (int i = 0; i < str.length(); i++) {
+            charArray[i] = str.charAt(i);
         }
 
+        return charArray;
     }
-    private void play(){
-        String input;
-        init();
-        do{
-            try{
-                input = getList();
-            }catch (IllegalArgumentException e){
-                throw e;
+
+    static int getIndexFromValue(final int[] arr, final int value) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == value) {
+                return i;
             }
-        }while(checkResult(input) == false);
+        }
+        return -1;
     }
 }
+class Game {
+    private static final int START_RANGE = 1;
+    private static final int END_RANGE = 9;
+    static final int NUMBER_COUNT = 3;
+    private static final int NEW_GAME = 1;
+    private static final int QUIT_GAME = 2;
+
+    private static final String GET_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String SUCCESS_MESSAGE = Game.NUMBER_COUNT + "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String NEW_GAME_CHECK_MESSAGE = "게임을 새로 시작하려면 " + Game.NEW_GAME + ", 종료하려면 " + Game.QUIT_GAME + "를 입력하세요.";
+
+    int[] number = new int[NUMBER_COUNT];
+
+}
+
+
