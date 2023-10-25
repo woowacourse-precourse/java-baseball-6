489b42ebbef4abecc5e2716f2277a68735d7d02e
swhyeon98 <smreo75@gmail.com>
2023-10-25 21:08:41 +0900

    feat: 스트라이크 수를 구하는 기능 추가

--- src/main/java/baseball/Umpire.java
+++ src/main/java/baseball/Umpire.java
@@ -1,6 +1,19 @@
 package baseball;
 
+import java.util.List;
+
 public class Umpire {
 
+    private static final int INIT_NUMBER = 0;
+    private static final int MAX_NUMBER_SIZE = 3;
 
+    public int getStrikeCount(List<Integer> computerNumber, List<Integer> userNumber) {
+        int strike = INIT_NUMBER;
+        for (int i = INIT_NUMBER; i < MAX_NUMBER_SIZE; i++) {
+            if (computerNumber.get(i) == userNumber.get(i)) {
+                strike++;
+            }
+        }
+        return strike;
+    }
 }
