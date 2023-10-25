c21fd45f125b67d2af0db0628bc49009dfbf4ac3
jh-jin <devjin0122@gmail.com>
2023-10-25 18:58:59 +0900

    feat: 메시지 출력 기능

---
+++ src/main/java/baseball/OutputMessage.java
@@ -0,0 +1,37 @@
+package baseball;
+
+public class OutputMessage {
+
+    public static void startGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void demandNumbers() {
+        System.out.println("숫자를 입력해주세요 : ");
+    }
+
+    public static void continueGame() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    public static void resultBallCount(int ballCount) {
+        System.out.println(ballCount+"볼");
+    }
+
+    public static void resultStrikeCount(int strikeCount) {
+        System.out.println(strikeCount+"+스트라이크");
+    }
+
+    public static void resultBallAndStrikeCount(int ballCount, int strikeCount) {
+        System.out.println(ballCount+"볼 "+strikeCount+"+스트라이크");
+    }
+
+    public static void resultNothing() {
+        System.out.println("낫싱");
+    }
+
+    public static void resultCorrect() {
+        resultStrikeCount(3);
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+}
