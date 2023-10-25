324e4bf1c71ddeffd60d35890f2f204795f1eb34
tsulocalize <tsulocalizing@gmail.com>
2023-10-25 19:22:23 +0900

    프로그램 작동 순서 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,10 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputManager inputManager = new InputManager();
+        JudgementManager judgementManager = new JudgementManager();
+        GameManager gameManager = new GameManager(inputManager, judgementManager);
+
+        gameManager.playGame();
     }
 }
