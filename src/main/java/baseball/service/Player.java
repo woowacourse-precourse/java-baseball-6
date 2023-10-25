35b1abbff0f141b9ae4b8e3eeeee91d6be40db15
strongcookdas <goodjav141592@gmail.com>
2023-10-25 19:27:49 +0900

    feat : 숫자야구 숫자 입력, 종료시 메뉴 숫자 입력 메소드 구현 - enterNumbers - enterMenuNumber

--- src/main/java/baseball/service/Player.java
+++ src/main/java/baseball/service/Player.java
@@ -1,6 +1,9 @@
 package baseball.service;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Player {
@@ -8,6 +11,31 @@ public class Player {
     private final String GAME_INPUT_REGEX = "[1-9]+";
     private final String GAME_INIT_REGEX = "[1-2]+";
 
+    public List<Integer> enterNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        String input = Console.readLine();
+        System.out.println(input);
+
+        if (!isRandomNumbers(input)) {
+            throw new IllegalArgumentException();
+        }
+
+        for (char c : input.toCharArray()) {
+            numbers.add(Character.getNumericValue(c));
+        }
+        return numbers;
+    }
+
+    public Integer enterMenuNumber() {
+        String input = Console.readLine();
+        System.out.println(input);
+
+        if (!isNumber(input)) {
+            throw new IllegalArgumentException();
+        }
+
+        return Integer.parseInt(input);
+    }
 
     public boolean isRandomNumbers(String input) {
 
