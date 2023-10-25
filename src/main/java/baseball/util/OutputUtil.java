be5dcd99eda042935caf85df4cfa50bc284dcb02
Jmmk <jmk525@gmail.com>
2023-10-25 22:03:48 +0900

    feat: 출력 시 사용되는 유틸 클래스 및 메서드 구현

---
+++ src/main/java/baseball/util/OutputUtil.java
@@ -0,0 +1,16 @@
+package baseball.util;
+
+public class OutputUtil {
+    public static void printStartMessage() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void printChooseOptionMessage() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    public static void printInsertInputMessage() {
+        System.out.print(("숫자를 입력해주세요 : "));
+    }
+
+}
