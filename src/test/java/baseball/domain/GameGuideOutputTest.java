399f5513422f0cebe0a363845db9ec88b9b05a32
robinjoon <robin980108@naver.com>
2023-10-25 17:54:25 +0900

    fix(게임 진행 가이드 출력) : 게임 진행 가이드 출력 기능 테스트코드 최적화

--- src/test/java/baseball/domain/GameGuideOutputTest.java
+++ src/test/java/baseball/domain/GameGuideOutputTest.java
@@ -11,12 +11,11 @@ import org.junit.jupiter.api.Test;
 
 class GameGuideOutputTest {
 
-    private PrintStream standardOut;
     private OutputStream captor;
 
     @BeforeEach
     protected final void init() {
-        standardOut = System.out;
+        PrintStream standardOut = System.out;
         captor = new ByteArrayOutputStream();
         System.setOut(new PrintStream(captor));
     }
@@ -24,19 +23,19 @@ class GameGuideOutputTest {
     @Test
     void startGame() {
         GameGuideOutput.startGame();
-        assertTrue(captor.toString().trim().equals("숫자 야구 게임을 시작합니다."));
+        assertEquals("숫자 야구 게임을 시작합니다.", captor.toString().trim());
     }
 
     @Test
     void inputBaseBall() {
         GameGuideOutput.inputBaseBall();
-        assertTrue(captor.toString().equals("숫자를 입력해주세요 : "));
+        assertEquals("숫자를 입력해주세요 : ", captor.toString());
     }
 
     @Test
     void gameEnd() {
         GameGuideOutput.gameEnd();
-        assertTrue(captor.toString().trim().equals("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"
-                + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."));
+        assertEquals("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"
+                + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.", captor.toString().trim());
     }
 }
