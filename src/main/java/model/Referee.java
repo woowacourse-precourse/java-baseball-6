796be967b105e87ca5e08fa8ea779313edac92cd
LeeSungNo-ian <isnn0103@gmail.com>
2023-10-25 19:56:32 +0900

    feat: strike 개수 메서드 생성

--- src/main/java/model/Referee.java
+++ src/main/java/model/Referee.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class Referee {
@@ -10,5 +12,18 @@ public class Referee {
             }
         }
         return true;
+    }
+
+    int countStrike(Set<Integer> computerBalls, Set<Integer> playerBalls) {
+        int strikeCount = 0;
+        for (int index = 0; index < computerBalls.size(); index++) {
+            List<Integer> computerBall = new ArrayList<Integer>(computerBalls);
+            List<Integer> playerBall = new ArrayList<Integer>(playerBalls);
+
+            if (computerBall.get(index) == playerBall.get(index)) {
+                strikeCount++;
+            }
+        }
+        return strikeCount;
     }
 }
