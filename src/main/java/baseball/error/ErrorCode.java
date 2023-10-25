20a9f7a457ff4e94aaf2def8d3566ff609cd66a7
kwang <shinkwang@yonsei.ac.kr>
2023-10-25 22:09:05 +0900

    feat: Make ErrorCode

--- src/main/java/baseball/error/ErrorCode.java
+++ src/main/java/baseball/error/ErrorCode.java
@@ -2,7 +2,7 @@ package baseball.error;
 
 public enum ErrorCode {
 
-    RANGE_ERROR("temp"),
+    RESTART_ERROR("Number must be 1 or 2"),
     DIGIT_ERROR("Number must be digit"),
     SIZE_ERROR("Number must be correct-digit size"),
     DUPLICATION_ERROR("Number must not be duplicated");
