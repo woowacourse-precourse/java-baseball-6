813a6f190f81efdea54ccf7384ebf7c4a464a20c
Hyoseop Song <crad_on25@naver.com>
2023-10-25 21:59:25 +0900

    feature(ScoreCalculator): 스트라이크, 볼 판별 스트림 기반 메소드 구현

--- src/main/java/baseball/domain/ScoreCalculator.java
+++ src/main/java/baseball/domain/ScoreCalculator.java
@@ -1,5 +1,8 @@
 package baseball.domain;
 
+import baseball.SYSTEM_VALUE.GameStatusValue;
+import java.util.stream.Stream;
+
 public class ScoreCalculator {
     public ScoreCalculator() {
     }
@@ -7,5 +10,20 @@ public class ScoreCalculator {
     public static Score createScore(BallNumber ballNumber1, BallNumber ballNumber2) {
         return new Score(calculateBall(ballNumber1, ballNumber2),
                 calculateStrike(ballNumber1, ballNumber2));
+    }
+
+    private static int calculateBall(BallNumber baseballNum1, BallNumber baseballNum2) {
+        return (int) Stream.iterate(0, i -> i + 1)
+                .limit(GameStatusValue.GAME_FINISH)
+                .filter(i -> baseballNum2.contains(baseballNum1.getNumberAt(i)))
+                .filter(i -> baseballNum1.getNumberAt(i) != baseballNum2.getNumberAt(i))
+                .count();
+    }
+
+    private static int calculateStrike(BallNumber baseballNum1, BallNumber baseballNum2) {
+        return (int) Stream.iterate(0, i -> i + 1)
+                .limit(GameStatusValue.GAME_FINISH)
+                .filter(i -> baseballNum1.getNumberAt(i) == baseballNum2.getNumberAt(i))
+                .count();
     }
 }
