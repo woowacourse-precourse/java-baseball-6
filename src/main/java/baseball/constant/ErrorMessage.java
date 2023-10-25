fd8b744c7c9f2146c9585d25705df13edeb2dbf4
JeongHunHui <deca3248@gmail.com>
2023-10-25 22:00:38 +0900

    refactor: 필요없는 예외 제거

--- src/main/java/baseball/constant/ErrorMessage.java
+++ src/main/java/baseball/constant/ErrorMessage.java
@@ -2,8 +2,7 @@ package baseball.constant;
 
 public enum ErrorMessage {
     NUMBER_INPUT_ERROR("유효하지 않은 숫자 입력입니다."),
-    GAME_DECISION_INPUT_ERROR("유효하지 않은 게임 결정 입력입니다."),
-    INVALID_INDEX_ERROR("유효하지 않은 인덱스입니다.");
+    GAME_DECISION_INPUT_ERROR("유효하지 않은 게임 결정 입력입니다.");
 
     private final String message;
 
