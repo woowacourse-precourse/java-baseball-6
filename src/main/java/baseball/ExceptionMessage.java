8a9e8509a9b9a5857faa6f00aa2ff2bc4c49934a
2021110806 <2021110806@dgu.ac.kr>
2023-10-25 22:26:21 +0900

    refactor(ExceptionMessage.java): 에러 메시지 변경

--- src/main/java/baseball/ExceptionMessage.java
+++ src/main/java/baseball/ExceptionMessage.java
@@ -1,10 +1,10 @@
 package baseball;
 
 public enum ExceptionMessage {
-    INVALID_ANSWER_LENGTH_MESSAGE("[ERROR] 3개의 숫자만을 입력해야 합니다."),
+    INVALID_ANSWER_LENGTH_MESSAGE("[ERROR] 입력값의 길이는 3이어야 합니다."),
     INVALID_ANSWER_RANGE_MESSAGE("[ERROR] 1부터 9까지의 숫자만을 입력해야 합니다."),
     INVALID_ANSWER_DUPLICATE_MESSAGE("[ERROR] 중복되지 않은 숫자들로 구성해야 합니다."),
-    INVALID_RETRY_INPUT_LENGTH_MESSAGE("[ERROR] 1개의 숫자만을 입력해야 합니다."),
+    INVALID_RETRY_INPUT_LENGTH_MESSAGE("[ERROR] 입력값의 길이는 1이어야 합니다."),
     INVALID_RETRY_INPUT_RANGE_MESSAGE("[ERROR] 1 혹은 2를 입력해야 합니다.");
     private String message;
 
