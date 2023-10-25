f4a0124be4712b3268d73989b04ac01914e04959
kg9585 <kg9585@naver.com>
2023-10-25 21:04:27 +0900

    refactor:(Result): 하드코드 제거

--- src/main/java/baseball/util/Constants.java
+++ src/main/java/baseball/util/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final int BALL_MIN = 1;
     public static final int BALL_MAX = 9;
     public static final int DEFAULT_VALUE = 0;
-    public static final int COUNT_PLUS = 1;
+    public static final int VALUE_ONE = 1;
     public static final String RESULT_SPACE = " ";
 
     private Constants() {
