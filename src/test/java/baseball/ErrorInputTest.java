ea1785b599a8928fc7a10a793e9a8148f12073e0
dokyeong99 <dlehrud451@gmail.com>
2023-10-25 23:11:06 +0900

    input_test_update

---
+++ src/test/java/baseball/ErrorInputTest.java
@@ -0,0 +1,60 @@
+package baseball;
+
+import baseball.service.InputErrorCheckService;
+import camp.nextstep.edu.missionutils.test.Assertions;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.*;
+
+
+public class ErrorInputTest extends NsTest {
+
+    private InputErrorCheckService inputErrorCheckService
+            = new InputErrorCheckService();
+
+
+    @Test
+    void 같은_숫자_입력_테스트(){
+        assertThat(inputErrorCheckService.sameNumCheck("122")).isEqualTo(false);
+    }
+
+    @Test
+    void 숫자만_입력_테스트(){
+        assertThat(inputErrorCheckService.IntegerCheck("31둘")).isEqualTo(false);
+    }
+
+
+
+
+    @Test
+    void 게임_재시작_입력_에러(){
+        assertThat(inputErrorCheckService.restartInputErrCheck("3")).isEqualTo(false);
+    }
+
+    @Test
+    void 게임_재시작_입력_정상(){
+        assertThat(inputErrorCheckService.restartInputErrCheck("1")).isEqualTo(true);
+    }
+
+    @Test
+    void 인풋_길이_테스트_1(){
+        assertThat(inputErrorCheckService.inputNumLengthCheck("123")).isEqualTo(true);
+    }
+
+    @Test
+    void 인풋_길이_테스트_2(){
+        assertThat(inputErrorCheckService.inputNumLengthCheck("1234")).isEqualTo(false);
+    }
+
+    @Test
+    void 인풋_길이_테스트_3(){
+        assertThat(inputErrorCheckService.inputNumLengthCheck("12")).isEqualTo(false);
+    }
+
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
