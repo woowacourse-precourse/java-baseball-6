2cbfe36b407b29876331fab9c251e1f2b78571bb
jaegyeongkim <dongji_11@naver.com>
2023-10-25 10:46:46 +0900

    refactor: 예외 발생 시 보여지는 예외메시지 추가

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -1,69 +1,77 @@
 package utils;
 
+import static baseball.Application.NUMBER_LENGTH;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import static baseball.Application.NUMBER_LENGTH;
-
 public class Utils {
-    public List<Integer> stringToIntList(String input){
+    public List<Integer> stringToIntList(String input) {
         List<Integer> intList = new ArrayList<>();
-        for(int i=0; i<input.length(); i++){
-            int split_number = input.charAt(i)-48;
+        for (int i = 0; i < input.length(); i++) {
+            int split_number = input.charAt(i) - 48;
             intList.add(split_number);
         }
         return intList;
     }
-    public String intListToString(List input){
+
+    public String intListToString(List input) {
         String string = "";
-        for(int i=0; i<input.size(); i++){
+        for (int i = 0; i < input.size(); i++) {
             String split_number = String.valueOf(input.get(i));
-            string= string + split_number;
+            string = string + split_number;
         }
         return string;
     }
+
     public void inputLengthIsZero(String input) {
-        if(input.equals("")){
-            throw new IllegalArgumentException();
+        if (input.equals("")) {
+            throw new IllegalArgumentException("입력의 길이가 0입니다.");
         }
     }
-    public void inputLengthIsOver(String input){
-        if(input.length()>NUMBER_LENGTH){
-            throw new IllegalArgumentException();
+
+    public void inputLengthIsOver(String input) {
+        if (input.length() > NUMBER_LENGTH) {
+            throw new IllegalArgumentException("입력된 숫자가 요구된 개수보다 많습니다.");
         }
     }
-    public void inputLengthIsUnder(String input){
-        if(input.length()<NUMBER_LENGTH){
-            throw  new IllegalArgumentException();
+
+    public void inputLengthIsUnder(String input) {
+        if (input.length() < NUMBER_LENGTH) {
+            throw new IllegalArgumentException("입력된 숫자가 요구된 개수보다 적습니다.");
         }
     }
-    public void inputIsNotNumber(String input){
-        for(int i=0; i<input.length();i++){
+
+    public void inputIsNotNumber(String input) {
+        for (int i = 0; i < input.length(); i++) {
             int ascii = input.charAt(i);
-            if(ascii<48 || ascii>58){
-                throw new IllegalArgumentException();
+            if (ascii < 48 || ascii > 58) {
+                throw new IllegalArgumentException("입력은 모두 숫자로 구성되어야 합니다.");
             }
         }
     }
-    public void duplicateNumber(String input){
+
+    public void duplicateNumber(String input) {
         Set<Character> set = new HashSet<>();
-        for(int i =0; i< input.length(); i++){
+        for (int i = 0; i < input.length(); i++) {
             set.add(input.charAt(i));
         }
-        if(set.size()<NUMBER_LENGTH){
-            throw new IllegalArgumentException();
+        if (set.size() < NUMBER_LENGTH) {
+            throw new IllegalArgumentException("입력된 숫자 중 중복된 것이 있습니다.");
         }
     }
-    public void userGameStartValidation(String input){
+
+    public void userGameStartValidation(String input) {
         inputLengthIsZero(input);
         inputIsNotNumber(input);
         inputLengthIsOver(input);
         inputLengthIsUnder(input);
         duplicateNumber(input);
     }
-    public void userGameRestartOrEndValidation(String input){
+
+    public void userGameRestartOrEndValidation(String input) {
         inputLengthIsZero(input);
         inputIsNotNumber(input);
     }
