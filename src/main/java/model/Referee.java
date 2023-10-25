eea6fc4569a0fb8856cf19fd07b91708fb500b00
LeeSungNo-ian <isnn0103@gmail.com>
2023-10-25 19:53:25 +0900

    feat: Referee 모델 생성 및 nothing 매서드 생성

---
+++ src/main/java/model/Referee.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.util.Set;
+
+public class Referee {
+    boolean isNothing(Set<Integer> computerBalls, Set<Integer> playerBalls) {
+        for (int playerBall: playerBalls) {
+            if (computerBalls.contains(playerBall)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
