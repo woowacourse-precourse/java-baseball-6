89becaa83b1d40de4691dd12ba534a207c68a5bf
MingyeomKim <mingyum119@naver.com>
2023-10-25 20:58:49 +0900

    refactor(DigitConstants): 정적 변수 사이의 개행 추가
    클린 코드를 적용하여 정적 변수 사이사이에 수직 공백을 추가한다.

--- src/main/java/baseball/constants/DigitConstants.java
+++ src/main/java/baseball/constants/DigitConstants.java
@@ -2,10 +2,16 @@ package baseball.constants;
 
 public final class DigitConstants {
     public static final int RESTART_ANSWER = 1;
+
     public static final int THREE_STRIKES = 3;
+
     public static final int INITIAL_INDEX = 0;
+
     public static final int LAST_INDEX = 3;
+
     public static final int NUMBER_SIZE = 3;
+
     public static final int NUMBER_RANGE_MIN = 1;
+
     public static final int NUMBER_RANGE_MAX = 9;
 }
