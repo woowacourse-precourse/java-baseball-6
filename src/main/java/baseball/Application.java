1aa424cbf3a03be4606ea7f638e4c65773e11d35
jiwon1217 <koagjw@gmail.com>
2023-10-27 03:38:37 +0900

    :recycle: refactor: startGame() 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,10 +8,7 @@ public class Application {
     public static void main(String[] args) {
         Computer computer = new Computer();
         Player player = new Player();
-        startBaseballGame(computer, player);
-    }
 
-    private static void startBaseballGame(Computer computer, Player player) {
         BaseballGame baseballGame = new BaseballGame(computer, player);
         baseballGame.start();
     }
