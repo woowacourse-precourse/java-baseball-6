5b8b29bb13f54fabda692b3129f2869d6716bee1
jiwon1217 <koagjw@gmail.com>
2023-10-27 04:16:45 +0900

    :recycle: refactor: ErrorMessage 삭제

--- src/main/java/baseball/util/Validator.java
+++ src/main/java/baseball/util/Validator.java
@@ -1,16 +1,17 @@
 package baseball.util;
 
-import baseball.constants.ErrorMessage;
 import java.util.regex.Pattern;
 
 public class Validator {
+    private static final String INVALID_INPUT = "유효하지 않은 입력입니다.";
+    private static final String INVALID_RESTART_OR_EXIT = "입력 값은 1 (재시작) 또는 2 (종료) 여야 합니다.";
     private static final Pattern THREE_DIGITS_REGEX = Pattern.compile("^^[1-9]{3}$");
     private static final Pattern DUPLICATE_DIGITS_REGEX = Pattern.compile("(\\d).*\\1");
     private static final Pattern RESTART_OR_EXIT_REGEX = Pattern.compile("[1-2]");
 
     public static void validateInputNumbers(String input) {
         if (!isValidThreeDigits(input) || hasDuplicateDigits(input)) {
-            throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT.toString());
+            throw new IllegalArgumentException(INVALID_INPUT);
         }
     }
 
@@ -24,7 +25,7 @@ public class Validator {
 
     public static void validateRestartOrExitNumber(String input) {
         if (!isValidRestartOrExit(input)) {
-            throw new IllegalArgumentException(ErrorMessage.INVALID_RESTART_OR_EXIT.toString());
+            throw new IllegalArgumentException(INVALID_RESTART_OR_EXIT);
         }
     }
 
