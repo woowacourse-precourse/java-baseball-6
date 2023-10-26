7990ba71c55592ea1fd866daf6dfda47a7f9bbb1
dgh06175 <dgh06175@gmail.com>
2023-10-26 17:07:22 +0900

    feat: 무작위 숫자 생성 함수

--- src/main/java/racingcar/domain/RandomNumberGenerator.java
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -1,4 +1,9 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RandomNumberGenerator {
+    public static int generate () {
+        return Randoms.pickNumberInRange(0,9);
+    }
 }
