6fa28d45541f8ee9f5cd7e0a9d1d4f21e436276e
dgh06175 <dgh06175@gmail.com>
2023-10-26 17:21:47 +0900

    feat: 자동차 별로 무작위로 전진

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,11 +1,33 @@
 package racingcar.controller;
 
+import static racingcar.ui.OutputView.printInputAttemptMessage;
+import static racingcar.ui.OutputView.printInputCarNamesMessage;
+
+import java.util.ArrayList;
 import java.util.List;
+import racingcar.domain.RacingCar;
 import racingcar.ui.InputView;
 
 public class RacingGameController {
-    public void start() {
+
+    List<RacingCar> racingCars = new ArrayList<>();
+    int attempt;
+
+    public RacingGameController() {
+        printInputCarNamesMessage();
         List<String> nameList = InputView.inputCarNames();
-        int attempt = InputView.inputAttempt();
+        printInputAttemptMessage();
+        this.attempt = InputView.inputAttempt();
+        for (String name: nameList) {
+            racingCars.add(new RacingCar(name));
+        }
+    }
+    public void start() {
+        while (attempt-- > 0) {
+            for(RacingCar racingCar: racingCars) {
+                racingCar.moveRandom();
+            }
+        }
+
     }
 }
--- src/main/java/racingcar/ui/OutputView.java
+++ src/main/java/racingcar/ui/OutputView.java
@@ -1,4 +1,20 @@
 package racingcar.ui;
 
 public class OutputView {
+    public static void printInputCarNamesMessage() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public static void printInputAttemptMessage() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public static void printAttemptResultMessage() {
+        System.out.println("실행 결과");
+    }
+
+
+    public static void printAttemptResult() {
+
+    }
 }
