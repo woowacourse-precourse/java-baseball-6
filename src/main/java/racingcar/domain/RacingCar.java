7b696d5b683043c8efdf6cfcd8d5441968494ed6
dgh06175 <dgh06175@gmail.com>
2023-10-26 17:07:31 +0900

    feat: 자동차 추상 클래스

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -1,4 +1,22 @@
 package racingcar.domain;
 
-public class RacingCar {
+public abstract class RacingCar {
+    String name;
+    int location;
+    RacingCar(String name) {
+        this.name = name;
+        location = 0;
+    }
+
+    public void moveRandom() {
+        if (RandomNumberGenerator.generate() >= 4) location += 1;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getLocation() {
+        return this.location;
+    }
 }
