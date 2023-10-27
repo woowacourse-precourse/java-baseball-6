6673c3968c6d389ceca9fac0c09005d5116f5dc2
Junho Lee <ghzm888@gmail.com>
2023-10-27 16:33:30 +0900

    Baseball game을 진행하는 service logic

---
+++ src/main/java/baseball/domain/service/BaseballService.java
@@ -0,0 +1,45 @@
+package baseball.domain.service;
+
+import baseball.domain.Client;
+import baseball.domain.Computer;
+import baseball.global.constant.OutputType;
+import baseball.global.utils.ConsoleUtil;
+
+import java.util.Objects;
+
+public class BaseballService {
+    public void startGame(Computer computer) {
+
+        while (true) {
+            ConsoleUtil.commonOutput(OutputType.INPUT_NUMBER.getComment());
+            Client client = new Client();
+            calculateBallAndStrikeCount(computer, client);
+            ConsoleUtil.ballAndStrikeCountOutput(client.getBall(), client.getStrike());
+            if (client.gameEnd()) {
+                break;
+            }
+        }
+    }
+
+    private void calculateBallAndStrikeCount(Computer computer, Client client) {
+        for (int i = 0; i < computer.getNumbers().size(); i++) {
+            increaseCount(computer, client, i);
+        }
+    }
+
+    private void increaseCount(Computer computer, Client client, Integer i) {
+        if (isStrike(computer, client, i)) {
+            client.increaseStrikeCount();
+        } else if (isBall(computer, client, i)) {
+            client.increaseBallCount();
+        }
+    }
+
+    private boolean isBall(Computer computer, Client client, int i) {
+        return computer.getNumbers().contains(client.getNumbers().get(i));
+    }
+
+    private boolean isStrike(Computer computer, Client client, int i) {
+        return Objects.equals(computer.getNumbers().get(i), client.getNumbers().get(i));
+    }
+}
