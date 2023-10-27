cbcf640864078325ea30083e36fd51a46137f7ba
Junho Lee <ghzm888@gmail.com>
2023-10-27 17:46:17 +0900

    컬렉션을 wrapping한 일급 컬렉션 객체 생성

---
+++ src/main/java/baseball/domain/RandomNumbers.java
@@ -0,0 +1,18 @@
+package baseball.domain;
+
+import java.util.Collections;
+import java.util.List;
+
+public class RandomNumbers {
+
+    private final List<Integer> numbers;
+
+    public RandomNumbers(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return Collections.unmodifiableList(numbers);
+    }
+
+}
