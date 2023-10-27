fdbb0f731078a2790fead11846adc4cba332a423
고동환\eurdr <eurdream98@naver.com>
2023-10-27 22:20:56 +0900

    2023-10-27

---
+++ java-racingcar-6/src/test/java/study/StringTest.java
@@ -0,0 +1,51 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+
+    @Test
+    void split_메서드로_주어진_값을_구분() {
+        String input = "1,2";
+        String[] result = input.split(",");
+
+        assertThat(result).contains("2", "1");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    void split_메서드_사용시_구분자가_포함되지_않은_경우_값을_그대로_반환() {
+        String input = "1";
+        String[] result = input.split(",");
+
+        assertThat(result).contains("1");
+    }
+
+    @Test
+    void substring_메서드로_특정_구간_값을_반환() {
+        String input = "(1,2)";
+        String result = input.substring(1, 4);
+
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    void charAt_메서드로_특정_위치의_문자_찾기() {
+        String input = "abc";
+        char charAtElement = input.charAt(0);
+        assertThat(charAtElement).isEqualTo('a');
+    }
+
+    @Test
+    void charAt_메서드_사용시_문자열의_길이보다_큰_숫자_위치의_문자를_찾을_때_예외_발생() {
+        String input = "abc";
+
+        assertThatThrownBy(() -> input.charAt(5))
+                .isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 5");
+    }
+
+}
