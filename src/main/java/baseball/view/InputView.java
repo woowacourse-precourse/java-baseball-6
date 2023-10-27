9f5ce2016fd38d00d15b2e1f205047ea93454a3b
Hugh <lee2317884@gmail.com>
2023-10-27 10:40:35 +0900

    refactor: InputView 클래스 명시적 private 생성자 사용 불 필요한 인스턴스 생성 방지

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -8,6 +8,9 @@ public class InputView {
     public static final String INPUT_USER_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
     public static final String INPUT_USER_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
+    private InputView() {
+    }
+
     public static String inputUserNumberMessage() {
         System.out.print(INPUT_USER_NUMBER_MESSAGE);
         return validateUserNumberInput();
