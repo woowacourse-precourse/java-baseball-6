6a54cfea6645c293b808daad86e297a1ffeb87c1
Donggeon Moon <nullsafer@naver.com>
2023-10-28 15:04:58 +0900

    fix: 검증 로직 중 빠진 조건 추가

--- src/main/java/baseball/util/ValidationUtils.java
+++ src/main/java/baseball/util/ValidationUtils.java
@@ -1,15 +1,22 @@
 package baseball.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ValidationUtils {
+    private static final Pattern pattern = Pattern.compile("^[1-9]{3}$");
     private ValidationUtils() {
     }
 
     public static void validateInput(String inputValue) {
-        if (!inputValue.matches("^[1-9]{3}$")) {
+        Matcher matcher = pattern.matcher(inputValue);
+        if (!(matcher.matches())) {
             throw new IllegalArgumentException("1~9 사이의 세 개의 숫자를 입력해주세요.");
         }
 
-        if (inputValue.charAt(0) == inputValue.charAt(1) || (inputValue.charAt(1) == inputValue.charAt(2))) {
+        if (inputValue.charAt(0) == inputValue.charAt(1)
+                || (inputValue.charAt(1) == inputValue.charAt(2))
+                || (inputValue.charAt(0) == inputValue.charAt(2))) {
             throw new IllegalArgumentException("서로 다른 3개의 숫자를 입력해주세요.");
         }
     }
