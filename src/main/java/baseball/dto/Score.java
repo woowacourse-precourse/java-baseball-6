61c53f10338d713efce576fe07f507220b3a48cb
Donggeon Moon <nullsafer@naver.com>
2023-10-28 18:55:50 +0900

    refactor: 점수 계산 책임 score 이동 및 메서드 분리
    추가로 null-safety 확보

--- src/main/java/baseball/dto/Score.java
+++ src/main/java/baseball/dto/Score.java
@@ -3,17 +3,53 @@ package baseball.dto;
 import static baseball.enums.Message.BALL;
 import static baseball.enums.Message.STRIKE;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
+
 public class Score {
     private int ballCount;
     private int strikeCount;
 
     private Score(ScoreBuilder builder) {
+        Objects.requireNonNull(builder);
+
         this.ballCount = builder.ballCount;
         this.strikeCount = builder.strikeCount;
     }
 
-    public static ScoreBuilder builder() {
+    static ScoreBuilder builder() {
         return new ScoreBuilder();
+    }
+
+    public static Score from(List<Integer> inputIntegers, List<Integer> randomIntegers) {
+        Objects.requireNonNull(inputIntegers);
+        Objects.requireNonNull(randomIntegers);
+
+        Score score = Score.builder().build();
+
+        IntStream.range(0, inputIntegers.size())
+                .forEach(i -> traverseInputValue(inputIntegers, i, randomIntegers, score));
+
+        return score;
+    }
+
+    private static void traverseInputValue(List<Integer> inputValue, int i, List<Integer> randomIntegers, Score score) {
+        Objects.requireNonNull(inputValue);
+        Objects.requireNonNull(randomIntegers);
+        Objects.requireNonNull(score);
+
+        for (int j = 0; j < randomIntegers.size(); j++) {
+
+            if (Objects.equals(inputValue.get(i), randomIntegers.get(j)) && i == j) {
+                score.increaseStrikeCount();
+                break;
+            }
+
+            if (Objects.equals(inputValue.get(i), randomIntegers.get(j))) {
+                score.increaseBallCount();
+            }
+        }
     }
 
     public int getBallCount() {
@@ -52,7 +88,7 @@ public class Score {
         return this.getStrikeCount() + this.getBallCount() == 0;
     }
 
-    public static class ScoreBuilder {
+    private static class ScoreBuilder {
         private int ballCount;
 
         private int strikeCount;
