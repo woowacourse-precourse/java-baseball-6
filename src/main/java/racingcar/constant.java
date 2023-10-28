6f26d3a32f1e5b732953283d59c88e76f3dd8e2f
dgh06175 <dgh06175@gmail.com>
2023-10-29 00:47:36 +0900

    remove: 처음부터 다시 구현하기 위해 모든 코드 삭제

--- src/main/java/racingcar/constant.java
+++
@@ -1,9 +0,0 @@
-package racingcar;
-
-public class constant {
-    public static final int CAR_INIT_LOCATION = 0;
-    public static final int CAR_MOVE_CONDITION_NUMBER = 4;
-    public static final int MAX_CAR_NAME_LENGTH = 5;
-    public static final int CAR_MOVE_DISTANCE = 1;
-    public static final String CAR_NAME_REGEX = "^[A-Za-z0-9,]*[A-Za-z0-9]$";
-}
--- src/main/java/racingcar/domain/RandomNumberGenerator.java
+++
@@ -1,9 +0,0 @@
-package racingcar.domain;
-
-import camp.nextstep.edu.missionutils.Randoms;
-
-public class RandomNumberGenerator {
-    public static int generate () {
-        return Randoms.pickNumberInRange(0,9);
-    }
-}
