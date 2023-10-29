3b67e93c7f8993a862d4fbdbc7fcdf90db682260
jexnjeux <zieun.na@gmail.com>
2023-10-30 00:34:03 +0900

    Test(baseball): 컴퓨터 수와 플레이어 수 비교 테스트

---
+++ src/test/java/baseball/domain/RefereeTest.java
@@ -0,0 +1,49 @@
+package baseball.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RefereeTest {
+
+    private Referee referee;
+
+    @BeforeEach
+    void setReferee() {
+        referee = new Referee();
+    }
+
+    @Test
+    void 스트라이크3() {
+        String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 3));
+        assertThat(result).isEqualTo("3 스트라이크");
+    }
+
+    @Test
+    void 낫싱() {
+        String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(4, 6, 8));
+        assertThat(result).isEqualTo("낫싱");
+    }
+
+    @Test
+    void 볼1() {
+        String result = referee.compare(Arrays.asList(2, 1, 3), Arrays.asList(1, 4, 5));
+        assertThat(result).isEqualTo("1 볼");
+    }
+
+    @Test
+    void 볼1_스트라이크1() {
+        String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(2, 5, 3));
+        assertThat(result).isEqualTo("1 볼 1 스트라이크");
+    }
+
+    @Test
+    void 볼3() {
+        String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(2, 3, 1));
+        assertThat(result).isEqualTo("3 볼");
+    }
+
+
+}
