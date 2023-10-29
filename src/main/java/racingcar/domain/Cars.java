c47ee3c8aab36b97cfa330ad6e4e84013751ded3
dgh06175 <dgh06175@gmail.com>
2023-10-29 18:43:06 +0900

    feat: Cars 클래스 단위테스트 통과

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,4 +1,33 @@
 package racingcar.domain;
 
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import racingcar.domain.numbergenerator.NumberGenerator;
+
 public class Cars {
+    private final String INPUT_NAMES_REGEX = "^[A-Za-z0-9,]*[A-Za-z0-9]$";
+    private final List<Car> cars;
+
+    public Cars(String inputNames, NumberGenerator numberGenerator) {
+        validateInputNames(inputNames);
+        String[] names = inputNames.split(",");
+        cars = Arrays.stream(names)
+                .map(name -> new Car(name, numberGenerator))
+                .collect(Collectors.toList());
+    }
+
+    public void allTryMove() {
+        cars.forEach(Car::tryMove);
+    }
+
+    public List<Car> getCars() {
+        return Collections.unmodifiableList(cars);
+    }
+
+    private void validateInputNames(String inputNames) {
+        if (!inputNames.matches(INPUT_NAMES_REGEX)) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
