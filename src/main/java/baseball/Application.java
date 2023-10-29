fee1e342df802f2f4eda391e33771303a8b7901c
jexnjeux <zieun.na@gmail.com>
2023-10-29 18:44:24 +0900

    Feat(baseball): 서로 다른 임의의 수 3개를 생성하는 기능 구현"

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import baseball.domain.NumberGenerator;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
@@ -10,35 +11,38 @@ import java.util.Set;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        List<Integer> computerNums = selectComputerNums();
-        List<Integer> userInput;
-        int[] result;
-        while (true) {
-            System.out.print("숫자를 입력해주세요 : ");
-            userInput = getUserInput();
-            result = compareNumber(computerNums, userInput);
-            if (result[0] == 0 && result[1] == 0) {
-                System.out.print("낫싱");
-            }
-            if (result[0] > 0) {
-                System.out.print(result[0] + "볼 ");
-            }
-            if (result[1] > 0) {
-                System.out.print(result[1] + "스트라이크");
-            }
-            System.out.println();
-            if (result[1] == 3) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                String numStr = Console.readLine();
-                if (numStr.equals("2")) {
-                    break;
-                } else {
-                    computerNums = selectComputerNums();
-                }
-            }
-        }
+        NumberGenerator generator = new NumberGenerator();
+        List<Integer> numbers = generator.createRandomNumbers();
+        System.out.println(numbers);
+//        System.out.println("숫자 야구 게임을 시작합니다.");
+//        List<Integer> computerNums = selectComputerNums();
+//        List<Integer> userInput;
+//        int[] result;
+//        while (true) {
+//            System.out.print("숫자를 입력해주세요 : ");
+//            userInput = getUserInput();
+//            result = compareNumber(computerNums, userInput);
+//            if (result[0] == 0 && result[1] == 0) {
+//                System.out.print("낫싱");
+//            }
+//            if (result[0] > 0) {
+//                System.out.print(result[0] + "볼 ");
+//            }
+//            if (result[1] > 0) {
+//                System.out.print(result[1] + "스트라이크");
+//            }
+//            System.out.println();
+//            if (result[1] == 3) {
+//                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+//                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+//                String numStr = Console.readLine();
+//                if (numStr.equals("2")) {
+//                    break;
+//                } else {
+//                    computerNums = selectComputerNums();
+//                }
+//            }
+//        }
     }
 
     public static List<Integer> selectComputerNums() {
--- src/main/java/baseball/domain/NumberGenerator.java
+++ src/main/java/baseball/domain/NumberGenerator.java
@@ -1,11 +1,24 @@
 package baseball.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class NumberGenerator {
 
+    /*
+    3개의 숫자가 담길 때까지 -> for 문으로 되지 않는다.
+    만약 이미 존재하는 숫자라면 담지 않는다.
+     */
     public List<Integer> createRandomNumbers() {
-        return null;
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < 3) {
+            int number = new Random().nextInt(9) + 1;
+            if (!numbers.contains(number)) {
+                numbers.add(number);
+            }
+        }
+        return numbers;
 
     }
 
