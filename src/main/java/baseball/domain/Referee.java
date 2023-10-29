76029081bb8e79d6b02070c527af3e07fe894078
jexnjeux <zieun.na@gmail.com>
2023-10-29 23:47:55 +0900

    Fix(baseball): 볼과 스트라이크인 경우 출력 문구 수정

--- src/main/java/baseball/domain/Referee.java
+++ src/main/java/baseball/domain/Referee.java
@@ -23,7 +23,7 @@ public class Referee {
         } else if (ball == 0) {
             return strike + " 스트라이크";
         }
-        return ball + " 볼 " + strike + "스트라이크";
+        return ball + " 볼 " + strike + " 스트라이크";
     }
 
 
