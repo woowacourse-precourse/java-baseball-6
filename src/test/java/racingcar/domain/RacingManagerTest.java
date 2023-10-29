b672cdfe0c02c390778af049b7327da04e984f4c
dgh06175 <dgh06175@gmail.com>
2023-10-29 19:04:41 +0900

    test: RacingManager 클래스 단위테스트 작성

---
+++ src/test/java/racingcar/domain/RacingManagerTest.java
@@ -0,0 +1,86 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.numbergenerator.NumberGenerator;
+import racingcar.domain.numbergenerator.SetNumberGenerator;
+
+class RacingManagerTest {
+    NumberGenerator successNumberGenerator = new SetNumberGenerator(5);
+    NumberGenerator failureNumberGenerator = new SetNumberGenerator(0);
+    String successInputName = "a,b,c,d";
+    @Test
+    void 횟수_입력_검증_테스트1() {
+        assertThatThrownBy(() -> {
+           new RacingManager(successInputName, 0, successNumberGenerator);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    void 횟수_입력_검증_테스트2() {
+        assertThatThrownBy(() -> {
+            new RacingManager(successInputName, -1, successNumberGenerator);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    void 경기_종료_테스트1() {
+        RacingManager racingManager = new RacingManager(successInputName, 2, successNumberGenerator);
+
+        racingManager.doAttempt();
+        racingManager.doAttempt();
+
+        assertThat(racingManager.isRaceEnd()).isTrue();
+    }
+    @Test
+    void 경기_종료_테스트2() {
+        RacingManager racingManager = new RacingManager(successInputName, 3, successNumberGenerator);
+
+        racingManager.doAttempt();
+        racingManager.doAttempt();
+
+        assertThat(racingManager.isRaceEnd()).isFalse();
+    }
+    @Test
+    void 차수_실행_테스트() {
+        RacingManager racingManager = new RacingManager(successInputName, 4, successNumberGenerator);
+
+        racingManager.doAttempt();
+        racingManager.doAttempt();
+        racingManager.doAttempt();
+        racingManager.doAttempt();
+
+        for(int i = 0; i < 4; i++) {
+            assertThat(racingManager.cars.getCars().get(i).getPosition()).isEqualTo(4);
+        }
+    }
+    @Test
+    void 차수_결과_테스트() {
+        RacingManager racingManager = new RacingManager(successInputName, 4, successNumberGenerator);
+
+        racingManager.doAttempt();
+
+        Map<String, Integer> attemptResult = new HashMap<>(0);
+        attemptResult.put("a", 1);
+        attemptResult.put("b", 1);
+        attemptResult.put("c", 1);
+        attemptResult.put("d", 1);
+
+        assertThat(racingManager.getAttemptResult()).isEqualTo(attemptResult);
+    }
+    @Test
+    void 게임_승자_테스트() {
+        RacingManager racingManager = new RacingManager(successInputName, 3, successNumberGenerator);
+
+        racingManager.doAttempt();
+        racingManager.doAttempt();
+        racingManager.doAttempt();
+
+        List<String> winner = new ArrayList<>(List.of(successInputName.split(",")));
+
+        assertThat(racingManager.getWinners()).isEqualTo(winner);
+    }
+}
