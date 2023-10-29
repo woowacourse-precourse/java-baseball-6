5707af400f02a67be0a58ee2b4b63b4270a166b2
jexnjeux <zieun.na@gmail.com>
2023-10-29 20:12:05 +0900

    Feat(baseball): 기능 완성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,68 +1,38 @@
 package baseball;
 
+import baseball.domain.NumberGenerator;
 import baseball.domain.Referee;
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.Scanner;
 
 public class Application {
 
     public static void main(String[] args) {
+        NumberGenerator generator = new NumberGenerator();
+        List<Integer> computer = generator.createRandomNumbers();
+
         Referee referee = new Referee();
-        String result = referee.compare(Arrays.asList(7, 8, 9), Arrays.asList(1, 2, 3));
-        System.out.println(result);
+        String result = "";
+        while (!result.equals("3 스트라이크")) {
+            result = referee.compare(computer, askNumbers());
+            System.out.println(result);
+        }
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
 
     }
 
+    public static List<Integer> askNumbers() {
+        System.out.print("숫자를 입력해 주세요: ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
 
-    public static List<Integer> selectComputerNums() {
-        Set<Integer> set = new HashSet<>();
-        while (set.size() != 3) {
-            int random = Randoms.pickNumberInRange(1, 9);
-            set.add(random);
+        List<Integer> numbers = new ArrayList<>();
+        for (String number : input.split("")) {
+            numbers.add(Integer.valueOf(number));
         }
-        return new ArrayList<>(set);
+        return numbers;
+
     }
-
-    public static List<Integer> getUserInput() {
-        String numStr = Console.readLine();
-        Set<Integer> set = new HashSet<>();
-        List<Integer> userInput = new ArrayList<>();
-
-        if (numStr.length() != 3) {
-            throw new IllegalArgumentException("invalid number");
-        }
-
-        for (int i = 0; i < 3; i++) {
-            if (1 <= numStr.charAt(i) - '0' && numStr.charAt(i) - '0' <= 9) {
-                set.add(numStr.charAt(i) - '0');
-            }
-        }
-        if (set.size() != 3) {
-            throw new IllegalArgumentException("invalid number");
-        }
-
-        for (int i = 0; i < 3; i++) {
-            userInput.add(numStr.charAt(i) - '0');
-        }
-        return userInput;
-    }
-
-    public static int[] compareNumber(List<Integer> computerNums, List<Integer> userInput) {
-        int[] result = new int[2];
-        for (int i = 0; i < 3; i++) {
-            if (computerNums.get(i) == userInput.get(i)) {
-                result[1]++;
-            } else if (computerNums.contains(userInput.get(i))) {
-                result[0]++;
-            }
-        }
-        return result;
-    }
-
-
 }
--- src/main/java/baseball/domain/Referee.java
+++ src/main/java/baseball/domain/Referee.java
@@ -21,7 +21,7 @@ public class Referee {
         } else if (strike == 0) {
             return ball + " 볼";
         } else if (ball == 0) {
-            return strike + "스트라이크";
+            return strike + " 스트라이크";
         }
         return ball + " 볼 " + strike + "스트라이크";
     }
