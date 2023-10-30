f560daeef148005750799abc856b99dc49c09adb
dgh06175 <dgh06175@gmail.com>
2023-10-31 01:15:29 +0900

    refactor: 상수에 static 키워드 추가

--- src/main/java/racingcar/domain/Engine.java
+++ src/main/java/racingcar/domain/Engine.java
@@ -3,8 +3,8 @@ package racingcar.domain;
 import racingcar.domain.numbergenerator.NumberGenerator;
 
 public class Engine {
-    private final int MOVE_CONDITION_MIN_NUMBER = 4;
-    private NumberGenerator numberGenerator;
+    private static final int MOVE_CONDITION_MIN_NUMBER = 4;
+    private final NumberGenerator numberGenerator;
     public Engine(NumberGenerator numberGenerator) {
         this.numberGenerator = numberGenerator;
     }
