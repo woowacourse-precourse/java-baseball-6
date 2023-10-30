0c8dce9ed8f983a1585701ee01d790f51869b0d4
doha <dohaahi@naver.com>
2023-10-30 21:35:32 +0900

    refactor: 매직 넘버 상수로 분리

--- src/main/java/baseball/util/RandomNumberGenerator.java
+++ src/main/java/baseball/util/RandomNumberGenerator.java
@@ -3,8 +3,12 @@ package baseball.util;
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class RandomNumberGenerator {
+
+    public static final int START_INCLUSIVE = 1;
+    public static final int END_INCLUSIVE = 9;
+
     public int generate() {
-        return pickNumberInRange(1, 9);
+        return pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
     }
 
 }
