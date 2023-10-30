3214babbfe56b7cabb83076cbdcb3486e2261afb
dgh06175 <dgh06175@gmail.com>
2023-10-30 15:57:59 +0900

    feat: InputView 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,19 +1,31 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
     public String inputCarNames() {
-        return "";
+        String carNames = Console.readLine();
+        validateCarNames(carNames);
+        return carNames;
     }
 
     public int inputAttempts() {
-        return 0;
+        String attempts = Console.readLine();
+        validateAttempts(attempts);
+        return Integer.parseInt(attempts);
     }
 
-    private void validateCarNames() {
-
+    void validateCarNames(String inputCarNames) throws IllegalArgumentException {
+        if (inputCarNames.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
     }
 
-    private void validateAttempts() {
-
+    void validateAttempts(String attempts) throws IllegalArgumentException {
+        try {
+            Integer.parseInt(attempts);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
     }
 }
