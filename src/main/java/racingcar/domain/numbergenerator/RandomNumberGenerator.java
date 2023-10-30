d9ce24a4db21dbb1aad71ad23a918e1f62d0e87e
dgh06175 <dgh06175@gmail.com>
2023-10-31 01:48:46 +0900

    form: @Override와 개행문자 추가

--- src/main/java/racingcar/domain/numbergenerator/RandomNumberGenerator.java
+++ src/main/java/racingcar/domain/numbergenerator/RandomNumberGenerator.java
@@ -5,6 +5,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class RandomNumberGenerator implements NumberGenerator {
     private static final int MIN_NUMBER = 0;
     private static final int MAX_NUMBER = 9;
+
+    @Override
     public int generate() {
         return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
     }
