4f2bdacad50da253f49753e4275ae928dc6825b7
jhva <junghun8158@naver.com>
2023-10-31 15:46:55 +0900

    :sparkles: feat(RaceService): 자동차 경주 상세 출력을 위한 메서드 추가

--- src/main/java/racingcar/service/RaceService.java
+++ src/main/java/racingcar/service/RaceService.java
@@ -1,5 +1,37 @@
 package racingcar.service;
 
+import java.util.Arrays;
+import racingcar.domain.Car;
+import racingcar.domain.RaceCar;
+import racingcar.view.InputView;
+
 public class RaceService {
 
+    private final RaceCar raceCar;
+
+    public RaceService(InputView inputView) {
+        this.raceCar = new RaceCar();
+        addRacingCarsFromInput(inputView.racingCarName(inputView.commonFromInput()));
+    }
+
+    public void start() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Car car : raceCar.getSavedCar()) {
+            String carName = car.getCarName();
+            String carValue = car.getCarValue();
+            stringBuilder.append(carName).append(" : ").append(carValue).append("\n");
+        }
+        System.out.println(stringBuilder.toString());
+    }
+
+    public void addRacingCarsFromInput(String inputName) {
+        Arrays.stream(inputName.split(","))
+                .forEach(name -> raceCar.addSaveCar(new Car(name)));
+    }
+
+    public void getWinner() {
+        System.out.println(
+                "최종 우승자 : " + raceCar.findWinner(raceCar.findWinnerNumber())
+        );
+    }
 }
