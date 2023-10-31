c2aa8f98fe7b9fecae233fd8fccb2299739e798b
jhva <junghun8158@naver.com>
2023-10-31 15:42:22 +0900

    :sparkles: feat(InputView): 사용자 입력 기능 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,5 +1,14 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 
+    public String commonFromInput() {
+        return Console.readLine();
+    }
+
+    public String racingCarName(String carName) {
+        return String.join(",", carName);
+    }
 }
