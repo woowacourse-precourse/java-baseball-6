1c242fb9845c94dd550b24e2c810d7e081133ecd
동원 <ai06021@gmail.com>
2023-10-31 21:10:49 +0900

    fix : input값 확인 play에서 하도록

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,10 +6,8 @@ public class Application {
     public static void main(String[] args) {
         while (true) {
             System.out.println("숫자 야구 게임을 시작합니다.");
-            String input = Console.readLine();
             Play play = new Play();
-            play.inputCheck(input);
-            play.game(input);
+            play.game();
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             String retry = Console.readLine();
             Newgame newgame = new Newgame();
