dd6279320ef179fadf1501f9e7a35946c739dc99
jhva <junghun8158@naver.com>
2023-10-31 15:44:24 +0900

    :sparkles: feat(OutView): 출력 기능 추가 및 Message Enum Class 추가

---
+++ src/main/java/racingcar/constants/MessageUtils.java
@@ -0,0 +1,17 @@
+package racingcar.constants;
+
+public enum MessageUtils {
+
+    GAME_START("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    GAME_CYCLE("시도할 회수는 몇회인가요?");
+
+    private final String message;
+
+    MessageUtils(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+}
--- src/main/java/racingcar/view/OutView.java
+++ src/main/java/racingcar/view/OutView.java
@@ -1,5 +1,19 @@
 package racingcar.view;
 
+import static racingcar.constants.MessageUtils.GAME_CYCLE;
+import static racingcar.constants.MessageUtils.GAME_START;
+
 public class OutView {
 
+    public OutView() {
+        this.racingStartMsg();
+    }
+
+    public void racingStartMsg() {
+        System.out.println(GAME_START.getMessage());
+    }
+
+    public void racingCycleMsg() {
+        System.out.println(GAME_CYCLE.getMessage());
+    }
 }
