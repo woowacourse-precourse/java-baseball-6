b070a0bd946af090cca746e735945b4e0281d3d8
jhva <junghun8158@naver.com>
2023-10-31 15:48:02 +0900

    :sparkles: feat(main): 자동차 게임 실행 메서드 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import racingcar.controller.RaceController;
+
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RaceController raceController = new RaceController();
+        raceController.run();
+        raceController.stop();
     }
 }
