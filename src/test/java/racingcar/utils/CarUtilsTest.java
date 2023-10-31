a0ea6867fe598ae10292c477b08728da5fca8839
jhva <junghun8158@naver.com>
2023-10-31 15:35:55 +0900

    :white_check_mark: test(CarUtils): 문자열 split 테스트 추가

--- src/test/java/racingcar/utils/CarUtilsTest.java
+++ src/test/java/racingcar/utils/CarUtilsTest.java
@@ -1,5 +1,6 @@
 package racingcar.utils;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import camp.nextstep.edu.missionutils.Randoms;
@@ -11,5 +12,16 @@ public class CarUtilsTest {
     public void 랜덤으로_0부터_9까지의_정수_하나를_반환한다() {
         int randomValue = Randoms.pickNumberInRange(0, 9);
         assertTrue(randomValue >= 0 && randomValue <= 9);
+    }
+
+    @Test
+    void 인풋으로받은_문자열을_split으로_쉼표기준으로_짤른이후_확인한다() {
+        String input = "pobi,woni,jun";
+        String[] expected = {"pobi", "woni", "jun"};
+
+        String[] result = input.split(",");
+
+        // 예상 결과와 실제 결과를 배열 요소별로 비교합니다.
+        assertArrayEquals(expected, result);
     }
 }
