2793756de9adc64a7e205fe024a356ee16196433
황성태 <tjdxo11933@naver.com>
2023-11-04 21:19:01 +0900

    코드 정리 및 주석

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,7 @@ import baseball.enums.GameFlag;
 import baseball.enums.Message;
 import baseball.enums.OrNot;
 import baseball.exception.WrongInputException;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.HashSet;
 import java.util.Random;
@@ -14,52 +15,58 @@ public class Application {
 
 
     public static void main(String[] args) throws WrongInputException {
+        // Game 시작 Setting
         GameFlag flag = GameFlag.START;
         PrintMessage.printlnMessage(Message.START);
+
         while (flag == GameFlag.START) {
-            // 랜덤한 값 서로 다른 수 3자리를 생성한다. TODO 함수로 뺄 예정
-            Random random = new Random();
-            random.setSeed(System.currentTimeMillis());
+
+            // 컴퓨터 수 생성
             Set<Integer> targetSet = new HashSet<>();
             while (targetSet.size() < 3) {
-                int randomNum = random.nextInt(9)+1;
-                targetSet.add(randomNum);
+                int randomNumber = Randoms.pickNumberInRange(1, 9);
+                targetSet.add(randomNumber);
             }
 
             GameInputValue gameValue = new GameInputValue(targetSet.stream()
                     .map(String::valueOf)
                     .reduce("", String::concat));
+
             flag = GameFlag.ING;
 
             // 맞출 때 까지 반복
-            while (flag == GameFlag.ING){
+            while (flag == GameFlag.ING) {
                 PrintMessage.printMessage(Message.INPUT_NUMBER);
                 Scanner sc = new Scanner(System.in);
                 gameValue.setUserInputValue(sc.nextLine());
+
+                // 입력값 검증
                 try {
                     gameValue.checkValidUserInput();
-                }catch (WrongInputException exception){
+                } catch (WrongInputException exception) {
                     PrintMessage.printlnMessage(exception.getMessage());
                     continue;
                 }
 
-                // 사용자 수와 컴퓨터 수 검증하기
+                // 사용자 수와 컴퓨터 수 비교
                 if (!gameValue.isEqaulsUserAndComputerValue()) {
                     Result result = new Result(gameValue.countSameNumber() - gameValue.countSamePositionAndNumber(), gameValue.countSamePositionAndNumber());
                     if (gameValue.isNothing()) {
                         PrintMessage.printlnMessage(Message.NOTHING);
-                    }else{
+                    } else {
                         PrintMessage.printlnMessage(Message.makeHintMessage(result));
                     }
                     continue;
                 }
 
+
+                // 리플레이 또는 종료
                 PrintMessage.printlnMessage(Message.PLAY_NEXT_GAME_OR_NOT);
                 Scanner sc2 = new Scanner(System.in);
                 String answer = sc2.nextLine();
+
                 if (answer.equals(OrNot.YES.getProcessCode())) {
                     flag = GameFlag.START;
-                    break;
                 }
                 if (answer.equals(OrNot.NO.getProcessCode())) {
                     flag = GameFlag.END;
