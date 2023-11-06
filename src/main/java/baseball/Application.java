9de23048c5276caeb9c97ad6b0ad896941a24a1f
junwoong2 <117076854+junwoong2@users.noreply.github.com>
2023-11-06 21:44:00 +0900

    .

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    	Random computer = new Random();
+    	ArrayList<Integer> random = new ArrayList<>();
+    	random = computer.Computer();
+    	
+    	System.out.println(random);
     }
+
 }
