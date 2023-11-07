91a03fc59b217defc2b19b269347ff7a7ab02702
황성태 <tjdxo11933@naver.com>
2023-11-07 16:47:59 +0900

    Answer생성자를 통해 각 도메인 별로 파라미터에 따라 다르게 생성하는 로직 구현

--- src/main/java/baseball_oop/domain/participant/player/Pitcher.java
+++ src/main/java/baseball_oop/domain/participant/player/Pitcher.java
@@ -1,10 +1,10 @@
 package baseball_oop.domain.participant.player;
 
 import baseball_oop.vo.Answer;
-
+import static camp.nextstep.edu.missionutils.Console.readLine;
 public class Pitcher implements Player {
     @Override
     public Answer generateAnswer() {
-        return null;
+        return new Answer(readLine());
     }
 }
--- src/main/java/baseball_oop/vo/Answer.java
+++ src/main/java/baseball_oop/vo/Answer.java
@@ -1,12 +1,31 @@
 package baseball_oop.vo;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class Answer implements Comparable<Answer> {
     List<Integer> values;
     private final int LENGTH_LIMIT = 3;
 
+    public Answer() {
+        Set<Integer> set = new HashSet<>();
+        while (set.size() < LENGTH_LIMIT) {
+            set.add(pickNumberInRange(1, 9));
+        }
+        this.values = set.stream().toList();
+    }
 
+
+    public Answer(String input) {
+        this.values = Arrays.stream(input.split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
 
     @Override
     public int compareTo(Answer o) {
