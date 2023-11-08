1d54302f15d8414f05a51868e99931aa20c2fbae
jiixon <jr0107380@gmail.com>
2023-11-08 18:16:02 +0900

    fix: randomNum 메서드 분리

--- src/main/java/baseball/Model/CreateRandomNum.java
+++ src/main/java/baseball/Model/CreateRandomNum.java
@@ -1,2 +1,22 @@
-package baseball.Model;public class CreateRandomNum {
+package baseball.Model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateRandomNum {
+    private List<Integer> computerNumbers;
+    public List<Integer> create(){
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size()<3){
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+        computerNumbers = numbers;
+        return computerNumbers;
+    }
+
 }
