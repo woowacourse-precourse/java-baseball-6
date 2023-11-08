71ad3ab0e31ac4d11dbcc83d3b3f921e06b27ad6
jiixon <jr0107380@gmail.com>
2023-11-08 18:17:16 +0900

    test: 랜덤숫자테스트코드 작성

--- src/test/java/baseball/RandomNumberTest.java
+++ src/test/java/baseball/RandomNumberTest.java
@@ -6,19 +6,13 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-public class GameTest {
+public class RandomNumberTest {
     @Test
     void 세_자리_수인지_확인(){
         CreateRandomNum createRandomNum = new CreateRandomNum();
         List<Integer> result = createRandomNum.create();
 
-        // 세 자리 수인지 확인
         Assertions.assertThat(result.size()).isEqualTo(3);
-
-        
     }
     @Test
     void 각_자리_숫자가_1부터_9까지_수인지_확인(){
@@ -27,7 +21,6 @@ public class GameTest {
 
         for (int number : result) {
             Assertions.assertThat(number >= 1 && number <= 9).isTrue();
-
         }
     }
     @Test
@@ -37,20 +30,9 @@ public class GameTest {
 
         for (int i = 0; i < result.size(); i++) {
             for (int j = i + 1; j < result.size(); j++) {
-                Assertions.assertThat(result.get(i)).isEqualTo(result.get(j));
+                Assertions.assertThat(result.get(i)).isNotEqualTo(result.get(j));
             }
         }
-    }
-
-    @Test
-    void 볼_판단_테스트(){
-
-    }
-
-    @Test
-    void 스트라이크_판단_테슽트(){
-
-
     }
 
 }
