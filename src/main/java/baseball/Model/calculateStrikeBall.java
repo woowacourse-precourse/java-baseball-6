c0b09338e5b6b76768c9c228b83a2c9864f781d7
jiixon <jr0107380@gmail.com>
2023-11-09 15:21:43 +0900

    fix: countStrike 메서드 추가

--- src/main/java/baseball/Model/calculateStrikeBall.java
+++ src/main/java/baseball/Model/calculateStrikeBall.java
@@ -2,7 +2,7 @@ package baseball.Model;
 
 import java.util.List;
 
-public class TotalStrikeBallNum {
+public class calculateStrikeBall {
     public int totalStrikeBall(List<Integer> computer,List<Integer> userInput){
         int result = 0;
         for(int i = 0; i<userInput.size(); i++){
@@ -13,4 +13,13 @@ public class TotalStrikeBallNum {
         return result;
     }
 
+    public int countStrike(List<Integer> computer,List<Integer> userInput){
+        int strike = 0;
+        for(int i = 0; i<userInput.size(); i++){
+            if(computer.get(i) == userInput.get(i)){
+                strike += 1;
+            }
+        }
+        return strike;
+    }
 }
