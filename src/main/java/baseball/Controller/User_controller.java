db560d415713fecf84814711554684908802d959
junwoong2 <117076854+junwoong2@users.noreply.github.com>
2023-11-09 14:55:51 +0900

    finish

--- src/main/java/baseball/Controller/User_controller.java
+++ src/main/java/baseball/Controller/User_controller.java
@@ -1,15 +1,30 @@
 package baseball.Controller;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class User_controller {
 
     public int[] UserInput(String input, int size) throws IllegalArgumentException {
         checkSize(input, size);
+        checkSame(input);
         return getNum(input, size);
     }
 
     private void checkSize(String input, int size) throws IllegalArgumentException {
         if (input.length() != size) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkSame(String input) throws IllegalArgumentException{
+        Set<Character> charSet = new HashSet<>();
+        for (int i = 0; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+            if (charSet.contains(currentChar)) {
+                throw new IllegalArgumentException();
+            }
+            charSet.add(currentChar);
         }
     }
 
