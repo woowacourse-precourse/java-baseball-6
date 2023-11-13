99293fb19d12239a3dd17f12448e3ea2ca64b21e
구동엽 <ehd_duq94@naver.com>
2023-11-14 03:02:40 +0900

    refactor : 코드 리펙토링

--- src/main/java/baseball/Controller/GameController.java
+++ src/main/java/baseball/Controller/GameController.java
@@ -1,78 +1,38 @@
 package baseball.Controller;
 
+import baseball.Service.CheckUserNumber;
 import baseball.Model.Game;
 import baseball.View.GameView;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class GameController {
+
     private final GameView gameView = new GameView();
 
-    GameController() {
+    public GameController(){
         gameView.showGameStart();
     }
 
-    public void start() {
+    public void GameStart(){
         Game game = new Game();
-        while (game.getGameStatus()) {
-            gameView.showInputNumbers();
-            String strInputNumbers = gameView.getInput();
-            validInputNumbers(strInputNumbers);
-            List<Integer> inputNumbers = stringToIntList(strInputNumbers);
+        while(game.gamestatus()){
+            gameView.showInputNumbers();  //숫자를 입력해 주세요:
+            String strInputNumbers = gameView.getInput(); //입력받아 string형에 저장
+            CheckUserNumber check = new CheckUserNumber(strInputNumbers); //오류 검사
+            List<Integer> UserNumbers = game.stringToIntList(strInputNumbers); //console로 입력 받으면 string형 이기 때문에 비교과 쉽게 List로 변환
 
-            String gameResult = game.makeGameResult(inputNumbers);
-            gameView.showString(gameResult);
+            String Gameresult = game.makeGameResult(UserNumbers);
+            gameView.showString(Gameresult);
         }
         gameView.showGameEnd();
 
         gameView.showInputRestart();
         String strInputRestart = gameView.getInput();
         if (game.isRestart(strInputRestart)) {
-            start();
+            GameStart();
         }
     }
 
-    private void validInputNumbers(String String) {
-        checkLength(String);
-        checkInt(String);
-        checkDuplicate(String);
-    }
 
-    private void checkLength(String String) { //입력값이 길이가 3인지 확인
-        if (String.length() != 3) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private void checkInt(String String) { //입력 값이 정수형인지 확인
-        try {
-            int inputInt = Integer.parseInt(String);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private void checkDuplicate(String String) { //입력값이 같은지 판단 ex)333 221 112
-        Set<Character> charSet = new HashSet<>();
-        for (int i = 0; i < String.length(); i++) {
-            char currentChar = String.charAt(i);
-            if (charSet.contains(currentChar)) {
-                throw new IllegalArgumentException();
-            }
-            charSet.add(currentChar);
-        }
-    }
-
-    private List<Integer> stringToIntList(String String) { //입력값을 List로 변경
-        List<Integer> intList = new ArrayList<>();
-        for (int i = 0; i < String.length(); i++) {
-            char digitChar = String.charAt(i);
-            int digitInt = Character.getNumericValue(digitChar);
-            intList.add(digitInt);
-        }
-        return intList;
-    }
 }
--- src/main/java/baseball/Model/GameResult.java
+++ src/main/java/baseball/Model/GameResult.java
@@ -3,41 +3,42 @@ package baseball.Model;
 import java.util.List;
 
 public class GameResult {
-    private int balls = 0;
-    private int strikes = 0;
 
-    GameResult(List<Integer> answerNumbers, List<Integer> inputNumbers) {
-        for (int i = 0; i < answerNumbers.size(); i++) {
-            int answerDigit = answerNumbers.get(i);
-            int inputDigit = inputNumbers.get(i);
+    private int ball = 0;
+    private int strike = 0;
 
-            if (answerDigit == inputDigit) {
-                this.strikes++;
-            } else if (answerNumbers.contains(inputDigit)) {
-                this.balls++;
+    GameResult(List<Integer> answerNumbers, List<Integer> UserNumbers ){
+        for(int i=0; i<answerNumbers.size(); i++){
+            int computer = answerNumbers.get(i);
+            int user = UserNumbers.get(i);
+
+            if(computer == user){
+                this.strike++;
+            }
+            else if(answerNumbers.contains(user)){
+                this.ball++;
             }
         }
     }
 
     public String resultToString() {
         String result = "";
-        if (this.balls != 0) {
-            result += this.balls + "볼";
+        if (this.ball != 0) {
+            result += this.ball + "볼";
         }
-        if (this.strikes != 0) {
-            if (this.balls != 0) {
+        if (this.strike != 0) {
+            if (this.ball != 0) {
                 result += " ";
             }
-            result += this.strikes + "스트라이크";
+            result += this.strike + "스트라이크";
         }
-        if (this.balls == 0 && this.strikes == 0) {
+        if (this.ball == 0 && this.strike == 0) {
             result = "낫싱";
         }
         return result;
     }
 
     public int getStrikes() {
-        return this.strikes;
+        return this.strike;
     }
-
 }
