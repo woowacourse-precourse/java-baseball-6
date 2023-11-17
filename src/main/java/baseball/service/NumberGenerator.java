4ae13dc70c16b934fbff7f1276aa3a49248f3bee
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-17 14:48:28 +0900

    [#1 Task2] feat(service.NumberGenerator): 정답 숫자 조합을 생성합니다
    - pickNumbersInRangeWithoutDuplicates() 메서드는 최솟값, 최댓값, 뽑을 숫자개수를 입력하면 해당 범위에 있는 숫자 중에 중복을 제외하고 입력된 개수만큼 추출해서 List로 반환하는 메서드 입니다.

--- src/main/java/baseball/service/NumberGenerator.java
+++ src/main/java/baseball/service/NumberGenerator.java
@@ -1,4 +1,23 @@
 package baseball.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class NumberGenerator {
+    public static List<Integer> pickNumbersInRangeWithoutDuplicates(int startRange, int endRange, int count) {
+        List<Integer> numbers = new ArrayList<>();
+
+        while (numbers.size() < count) {
+            int randomNumber = Randoms.pickNumberInRange(startRange, endRange);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+
+        numbers.sort(null);
+
+        return numbers;
+    }
 }
