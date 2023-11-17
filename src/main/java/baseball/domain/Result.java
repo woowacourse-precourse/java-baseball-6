9d15eeae4270e27df8927c5384c2206e333783d4
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-17 20:04:49 +0900

    [#1 Task5] test(GameResultCalculator): 정답 숫자 조합과 사용자가 입력한 숫자 조합을 비교해서 ball, strike, out 개수를 계산하고 Result 객체에 담아서 반환한다
    - 여러가지 상황에서 calculateResult() 메서드가 정확하게 동작하는지 확인하는 테스트 코드를 작성했습니다.

--- src/main/java/baseball/domain/Result.java
+++ src/main/java/baseball/domain/Result.java
@@ -1,5 +1,7 @@
 package baseball.domain;
 
+import java.util.Objects;
+
 public class Result {
     int ball;
     int strike;
@@ -9,5 +11,18 @@ public class Result {
         this.ball = ball;
         this.strike = strike;
         this.out = out;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Result result = (Result) o;
+        return ball == result.ball && strike == result.strike && out == result.out;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ball, strike, out);
     }
 }
--- src/test/java/baseball/service/GameResultCalculatorTest.java
+++ src/test/java/baseball/service/GameResultCalculatorTest.java
@@ -1,10 +1,12 @@
 package baseball.service;
 
+import baseball.domain.Result;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static baseball.service.GameResultCalculator.calculateResult;
 import static baseball.service.GameResultCalculator.calculateStrikeCount;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -66,6 +68,129 @@ class GameResultCalculatorTest {
     }
 
 
+    @Test
+    @DisplayName("0볼 3스트라이크 일 때, Result를 정확하게 생성한다.")
+    void calculateResultCorrectlyWhenZeroBallThreeStrike() {
+        // given
+        List<Integer> computerNumbers = List.of(1, 2, 3);
+        List<Integer> userNumbers = List.of(1, 2, 3);
 
+        // when
+        Result result = calculateResult(computerNumbers, userNumbers);
 
+        // then
+        assertThat(result).isEqualTo(new Result(0, 3, 0));
+    }
+
+    @Test
+    @DisplayName("0볼 2스트라이크 일 때, Result를 정확하게 생성한다.")
+    void calculateResultCorrectlyWhenZeroBallTwoStrike() {
+        // given
+        List<Integer> computerNumbers = List.of(1, 2, 3);
+        List<Integer> userNumbers = List.of(1, 2, 4);
+
+        // when
+        Result result = calculateResult(computerNumbers, userNumbers);
+
+        // then
+        assertThat(result).isEqualTo(new Result(0, 2, 1));
+    }
+
+    @Test
+    @DisplayName("0볼 1스트라이크 일 때, Result를 정확하게 생성한다.")
+    void calculateResultCorrectlyWhenZeroBallOneStrike() {
+        // given
+        List<Integer> computerNumbers = List.of(1, 2, 3);
+        List<Integer> userNumbers = List.of(1, 4, 5);
+
+        // when
+        Result result = calculateResult(computerNumbers, userNumbers);
+
+        // then
+        assertThat(result).isEqualTo(new Result(0, 1, 2));
+    }
+
+    @Test
+    @DisplayName("1볼 1스트라이크 일 때, Result를 정확하게 생성한다.")
+    void calculateResultCorrectlyWhenOneBallOneStrike() {
+        // given
+        List<Integer> computerNumbers = List.of(1, 2, 3);
+        List<Integer> userNumbers = List.of(1, 3, 4);
+
+        // when
+        Result result = calculateResult(computerNumbers, userNumbers);
+
+        // then
+        assertThat(result).isEqualTo(new Result(1, 1, 1));
+    }
+
+    @Test
+    @DisplayName("2볼 1스트라이크 일 때, Result를 정확하게 생성한다.")
+    void calculateResultCorrectlyWhenTwoBallOneStrike() {
+        // given
+        List<Integer> computerNumbers = List.of(1, 2, 3);
+        List<Integer> userNumbers = List.of(1, 3, 2);
+
+        // when
+        Result result = calculateResult(computerNumbers, userNumbers);
+
+        // then
+        assertThat(result).isEqualTo(new Result(2, 1, 0));
+    }
+
+    @Test
+    @DisplayName("1볼 0스트라이크 일 때, Result를 정확하게 생성한다.")
+    void calculateResultCorrectlyWhenOneBallZeroStrike() {
+        // given
+        List<Integer> computerNumbers = List.of(1, 2, 3);
+        List<Integer> userNumbers = List.of(2, 4, 5);
+
+        // when
+        Result result = calculateResult(computerNumbers, userNumbers);
+
+        // then
+        assertThat(result).isEqualTo(new Result(1, 0, 2));
+    }
+
+    @Test
+    @DisplayName("2볼 0스트라이크 일 때, Result를 정확하게 생성한다.")
+    void calculateResultCorrectlyWhenTwoBallZeroStrike() {
+        // given
+        List<Integer> computerNumbers = List.of(1, 2, 3);
+        List<Integer> userNumbers = List.of(2, 1, 4);
+
+        // when
+        Result result = calculateResult(computerNumbers, userNumbers);
+
+        // then
+        assertThat(result).isEqualTo(new Result(2, 0, 1));
+    }
+
+    @Test
+    @DisplayName("3볼 0스트라이크 일 때, Result를 정확하게 생성한다.")
+    void calculateResultCorrectlyWhenThreeBallZeroStrike() {
+        // given
+        List<Integer> computerNumbers = List.of(1, 2, 3);
+        List<Integer> userNumbers = List.of(2, 3, 1);
+
+        // when
+        Result result = calculateResult(computerNumbers, userNumbers);
+
+        // then
+        assertThat(result).isEqualTo(new Result(3, 0, 0));
+    }
+
+    @Test
+    @DisplayName("0볼 0스트라이크 일 때, Result를 정확하게 생성한다.")
+    void calculateResultCorrectlyWhenZeroBallZeroStrike() {
+        // given
+        List<Integer> computerNumbers = List.of(1, 2, 3);
+        List<Integer> userNumbers = List.of(4, 5, 6);
+
+        // when
+        Result result = calculateResult(computerNumbers, userNumbers);
+
+        // then
+        assertThat(result).isEqualTo(new Result(0, 0, 3));
+    }
 }
