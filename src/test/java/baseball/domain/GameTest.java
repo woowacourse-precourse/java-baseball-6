63b6a234c772179067f8ae21b23fbe7d55690c62
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-17 19:15:05 +0900

    [#1 Task3] test(domain.Game): Game 클래스가 생성될 때, 멤버변수에 정답 숫자조합이 만들어진다
    - Game 클래스가 생성될 때, 정답 숫자 조합이 정확하게 등록되는지 확인하는 테스트 코드를 작성했습니다.

---
+++ src/test/java/baseball/domain/GameTest.java
@@ -0,0 +1,26 @@
+package baseball.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+class GameTest {
+    @Test
+    @DisplayName("Game 클래스를 생성할 때 computerAnswer가 만들어 진다.")
+    void gameGeneratorShouldMakeComputerAnswerAndRegister() {
+        // given, when
+        Game game = new Game();
+        List<Integer> computerAnswer = game.getComputerAnswer();
+
+        // then
+        assertThat(computerAnswer)
+                .hasSize(3)
+                .allMatch(number -> number >= 1 && number <= 9)
+                .doesNotHaveDuplicates();
+    }
+}
