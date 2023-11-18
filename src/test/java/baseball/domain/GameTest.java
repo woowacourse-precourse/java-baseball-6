640d457437d5a670ae9489fe2ebaf036e65e0429
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-18 09:17:34 +0900

    [#1 Task8] test(domain.Game.makeGameResult): 사용자가 입력한 숫자 조합을 가지고 게임 결과를 계산한다
    - makeGameResult() 메서드가 사용자 입력값을 가지고 GameResult를 제대로 반환하는지 확인하는 테스트 코드를 작성했습니다.

--- src/test/java/baseball/domain/GameTest.java
+++ src/test/java/baseball/domain/GameTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 class GameTest {
@@ -21,5 +21,22 @@ class GameTest {
                 .hasSize(3)
                 .allMatch(number -> number >= 1 && number <= 9)
                 .doesNotHaveDuplicates();
+    }
+
+    @Test
+    @DisplayName("사용자 입력값을 가지고 게임 결과를 계산후 정확하게 반환한다.")
+    void makeGameResultCorrectly() {
+        // given
+        List<Integer> userNumbers = List.of(1, 2, 3);
+        List<String> answerCandidate =
+                List.of("낫싱", "1볼", "2볼", "1볼 1스트라이크", "1스트라이크", "2스트라이크", "3스트라이크");
+
+        // when, then
+        for (int i = 0; i < 1000; i++) {
+            Game game = new Game();
+            GameResult gameResult = game.makeGameResult(userNumbers);
+
+            assertThat(gameResult.toString()).isIn(answerCandidate);
+        }
     }
 }
