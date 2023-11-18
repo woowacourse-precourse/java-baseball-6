1a3113504dc036e2554539ba64d3d7f80bb51000
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-18 09:16:42 +0900

    [#1 Task8] feat(domain.Game.makeGameResult): 사용자가 입력한 숫자 조합을 가지고 게임 결과를 계산한다
    - makeGameResult() 메서드는 사용자가 입력한 숫자조합을 가지고 정답을 계산한 후 그 결과를 반환하는 메서드 입니다.
    - 만약 사용자가 정답을 맞추었다면, 멤버변수 isUserWin을 true로 변경합니다.

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -2,35 +2,33 @@ package baseball.domain;
 
 import java.util.List;
 
+import static baseball.service.GameResultCalculator.*;
 import static baseball.service.NumberGenerator.*;
 
 public class Game {
     private final List<Integer> computerNumbers;
-    private List<Integer> userNumbers;
-
     private boolean isUserWin = false;
 
     public Game() {
         this.computerNumbers = pickNumbersInRangeWithoutDuplicates(1,9, 3);
     }
 
-    public Result makeResult() {
-        return null;
+    public GameResult makeGameResult(List<Integer> userNumbers) {
+        GameResult gameResult = caculateGameResult(computerNumbers, userNumbers);
+
+        if (gameResult.equals(new GameResult(0, 3, 0))) {
+            isUserWin = true;
+        }
+
+        return gameResult;
     }
 
     public List<Integer> getComputerNumbers() {
         return computerNumbers;
     }
 
-    public List<Integer> getUserNumbers() {
-        return userNumbers;
-    }
-
     public boolean isUserWin() {
         return isUserWin;
     }
 
-    public void setUserNumbers(List<Integer> userNumbers) {
-        this.userNumbers = userNumbers;
-    }
 }
