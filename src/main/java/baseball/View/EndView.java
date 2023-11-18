d4038e4187a7614673262abf28bc42193e0cb1a1
Choihohee <lovelove4927@naver.com>
2023-11-18 15:07:38 +0900

    feat: GameService 오류와 구조 수정

--- src/main/java/baseball/View/EndView.java
+++
@@ -1,9 +0,0 @@
-package baseball.View;
-
-public class EndView {
-    public void main(String[] args) { }
-
-    public void endview() {
-        System.out.println("게임을 다시 시작하려면 1, 종료하려면 2를 입력하세요.");
-    }
-}
