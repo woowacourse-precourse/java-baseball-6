85ebca73662a54c14e8f160cc4d778922f978719
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-18 09:12:22 +0900

    [#1 Task7] feat(GameController): 사용자가 입력한 숫자조합을 검증 후 이상이 없으면 List<Integer> 형태로 변환한다
    - askAnswer() 메서드는 사용자로 부터 입력값을 받아와서 그 입력값에 이상이 없는지 검증하고 검증 후에 List<Integer> 형태로 반환하는 메서드 입니다.
    - validateUserAnswer() 메서드에서 사용자 입력값이 형식에 맞는지 검사하고 이상이 있는 경우 IllegalArgumentException 을 발생시킵니다.
    - userAnswerToUserNumbers() 메서드에서 사용자 입력값을 숫자형태의 리스트로 변환합니다
        예) "123" → {1, 2, 3}

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,4 +1,30 @@
 package baseball.controller;
 
+import baseball.domain.Game;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static baseball.view.InputView.readUserAnswer;
+
 public class GameController {
+    public void playGame() {
+        Game game = new Game();
+    }
+
+    public List<Integer> askAnswer() {
+        String userAnswer = readUserAnswer();
+        validateUserAnswer(userAnswer);
+        return userAnswerToUserNumbers(userAnswer);
+    }
+
+    private void validateUserAnswer(String userAnswer) {
+    }
+
+    private List<Integer> userAnswerToUserNumbers(String userInput) {
+        return userInput.chars()
+                .map(Character::getNumericValue)
+                .boxed()
+                .collect(Collectors.toList());
+    }
 }
