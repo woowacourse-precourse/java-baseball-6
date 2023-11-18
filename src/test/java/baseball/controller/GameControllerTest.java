c753be25d772005b6cfb8d508008b48832610820
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-18 11:19:21 +0900

    [#1 Task10] test( GameController): 사용자가 정답을 맞출 때 까지 계속 물어본다
    - askUntilCorrectAnswer() 메서드가 사용자가 정답을 입력할 때까지 계속해서 질문을 하는지 확인하는 테스트 코드를 작성하였습니다.

--- src/test/java/baseball/controller/GameControllerTest.java
+++ src/test/java/baseball/controller/GameControllerTest.java
@@ -1,5 +1,6 @@
 package baseball.controller;
 
+import baseball.domain.Game;
 import camp.nextstep.edu.missionutils.Console;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,12 +8,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class GameControllerTest {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
     private GameController gameController;
 
     InputStream createUserInput(String input) {
@@ -21,12 +27,14 @@ class GameControllerTest {
 
     @BeforeEach
     public void beforeEach() {
+        System.setOut(new PrintStream(outputStreamCaptor));
         gameController = new GameController();
     }
 
     @AfterEach
     public void afterEach() {
         Console.close();
+        System.setOut(standardOut);
     }
 
     @Test
@@ -42,5 +50,27 @@ class GameControllerTest {
         assertThat(result)
                 .hasSize(3)
                 .containsSubsequence(1, 2, 3);
+    }
+
+    @Test
+    @DisplayName("사용자가 정답을 맞출 때까지 계속 물어보는 지 확인한다.")
+    void askUntilCorrectAnswer() {
+        // given
+        System.setIn(createUserInput("123\n456\n789"));
+
+        List<Integer> computerAnswer = List.of(7, 8, 9);
+        Game game = new Game(computerAnswer);
+
+        // when
+        gameController.askUntilCorrectAnswer(game);
+        String result = outputStreamCaptor.toString();
+        int count = countSomeWord(result, "숫자를 입력해주세요 : ");
+
+        // then
+        assertThat(count).isEqualTo(3);
+    }
+
+    public int countSomeWord(String target, String someWord) {
+        return target.split(someWord).length - 1;
     }
 }
