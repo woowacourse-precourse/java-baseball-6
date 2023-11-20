9128a6f59ff8eae60cd2109ef43d11fc85498dbd
jisu-om <lifyji@naver.com>
2023-11-20 21:30:15 +0900

    Style: 불필요한 import 제거

--- src/main/java/baseball/controller/MainController.java
+++ src/main/java/baseball/controller/MainController.java
@@ -1,14 +1,5 @@
 package baseball.controller;
 
-import baseball.dto.RoundResultDto;
-import baseball.model.ComputerNumber;
-import baseball.model.UserNumber;
-import baseball.service.GameManager;
-import baseball.view.InputView;
-import baseball.view.OutputView;
-
-import java.util.List;
-
 public class MainController {
     public static MainController instance = new MainController(GameRestartController.getInstance(), GamePlayController.getInstance());
     private final GameRestartController gameRestartController;
