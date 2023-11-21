5b3cd90252f0033bfa3e6acad520fb0305b4b57f
dmswl6310 <dmswl6310@gmail.com>
2023-11-21 09:22:37 +0900

    feat(Game,Inputview): 게임 시작 문구 출력

--- src/main/java/baseball/controller/Game.java
+++ src/main/java/baseball/controller/Game.java
@@ -1,15 +1,22 @@
 package baseball.controller;
 
 import baseball.domain.Baseball;
+import baseball.view.InputView;
 
 public class Game {
     Baseball baseball;
 
     public void run() {
+        start();
         play();
+    }
+
+    private void start() {
+        InputView.printStart();
     }
 
     private void play() {
         baseball = new Baseball();
     }
+
 }
