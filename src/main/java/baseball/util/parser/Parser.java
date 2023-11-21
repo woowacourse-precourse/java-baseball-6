55b566e64fce987607def38466b2dd8703e52a89
kkonii <119216472+kkonii@users.noreply.github.com>
2023-11-21 14:25:34 +0900

    Refactor(parser) : 세 자리 숫자로 변환하는 `타입` 변경
    - List<Integer>로 변경

---
+++ src/main/java/baseball/util/parser/Parser.java
@@ -0,0 +1,19 @@
+package baseball.util.parser;
+
+import baseball.util.validator.Validator;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Parser {
+    private static final String INPUT_SEPARATOR = "";
+
+    public static List<Integer> parseToNumeric(String userInput) {
+        Validator.validateEmpty(userInput);
+        Validator.validateNumeric(userInput);
+
+        return Arrays.stream(userInput.split(INPUT_SEPARATOR))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+}
