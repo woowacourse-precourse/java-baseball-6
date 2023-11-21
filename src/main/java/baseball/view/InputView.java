69476af89b2c528af410affd19a982938df54288
dmswl6310 <dmswl6310@gmail.com>
2023-11-21 10:29:41 +0900

    feat(InputView): 사용자 입력에 대한 예외처리
    길이와 범위가 맞기않을때 예외 출력 및 재입력

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -3,13 +3,32 @@ package baseball.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static void readUserNum() {
+    public static int readUserNum() {
         try {
             String userNum = Console.readLine();
+            validateLength(userNum);
+//            validateIsUnique();
+            validateRange(userNum);
 
-            int userNumber = convertStringToInt(Console.readLine());
+            return convertStringToInt(userNum);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
+            return readUserNum();
+        }
+    }
+
+    public static void validateLength(String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("입력값은 세자리여야 합니다. 재입력 하세요.");
+        }
+    }
+
+    public static void validateRange(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            char num = input.charAt(i);
+            if (num < '1' || num > '9') {
+                throw new IllegalArgumentException("입력값은 1~9사이 숫자 여야합니다. 재입력 하세요.");
+            }
         }
     }
 
@@ -17,7 +36,7 @@ public class InputView {
         try {
             return Integer.parseInt(num);
         } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("유효하지 않은 입력입니다. 재입력 하세요.");
+            throw new IllegalArgumentException("입력값은 숫자여야 합니다. 재입력 하세요.");
         }
     }
 }
