6cd5f6c7635326a2711005015ee3ab3bd4fd7b77
dmswl6310 <dmswl6310@gmail.com>
2023-11-21 11:23:16 +0900

    feat(Baseball): strike와 ball 갯수 계산

--- src/main/java/baseball/domain/Baseball.java
+++ src/main/java/baseball/domain/Baseball.java
@@ -21,4 +21,42 @@ public class Baseball {
         }
         return computerNum;
     }
+
+    public String getResult(int inputNum) {
+        int[] result=countStrikeBall(inputNum);
+        int strike=result[1];
+        int ball=result[2];
+
+//        String result = resultToString(strike, ball);
+
+    }
+
+    private boolean isStrike(int index,String targetNum){
+        return targetNum.equals(computerNum.get(index));
+    }
+
+    private boolean isBall(String targetNum){
+        return computerNum.contains(targetNum);
+    }
+
+    private int[] countStrikeBall(int input){
+        int[] result=new int[2];
+
+        String userNum = Integer.toString(input);
+        for (int i = 0; i < 3; i++) {
+            String targetNum=userNum.substring(i,i+1);
+            if (isStrike(i,targetNum)) {
+                result[0]++;
+                continue;
+            }
+            if (isBall(targetNum)) {
+                result[1]++;
+            }
+        }
+        return result;
+    }
+
+    public(int index,int input){
+
+    }
 }
