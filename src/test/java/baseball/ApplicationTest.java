bd816797419125476d25c5a0f11f37ba26ba3c70
dmswl6310 <dmswl6310@gmail.com>
2023-11-21 14:39:17 +0900

    test(ApplicationTest): 실행 테스트 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -20,6 +20,28 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 게임한판_후_종료() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("246", "135", "2");
+                    assertThat(output()).contains("낫싱", "3스트라이크", "게임 종료");
+                },
+                1, 3, 5
+        );
+    }
+
+    @Test
+    void 낫싱_후_3볼_후_재시작() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("456", "789", "231", "123", "1", "145", "362", "897", "789", "2");
+                    assertThat(output()).contains("낫싱", "낫싱", "3볼", "3스트라이크", "낫싱", "낫싱", "3볼", "3스트라이크", "게임 종료");
+                },
+                1, 2, 3, 7, 8, 9
+        );
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() -> {
             runException("1234");
