b9f385a1c4061032c32216479c2fb870f15dbdd5
dmswl6310 <dmswl6310@gmail.com>
2023-11-21 09:52:46 +0900

    feat(InputView): 사용자 입력 숫자 받기

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,5 +1,24 @@
 package baseball.view;
 
-public class InputView {
+import camp.nextstep.edu.missionutils.Console;
 
+public class InputView {
+    public static void readUserNum() {
+        try {
+            String userNum = Console.readLine();
+
+            int userNumber = convertStringToInt(Console.readLine());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static int convertStringToInt(String num) {
+        try {
+            return Integer.parseInt(num);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("유효하지 않은 입력입니다. 재입력 하세요.");
+        }
+    }
 }
+
