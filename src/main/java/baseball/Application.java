bb4dcd031710a9c232bd449e12a130e2a8c61041
YoonseolLee <thisyoon97@gmail.com>
2023-11-21 11:25:14 +0900

    6

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,6 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("helloooo");
-        System.out.println("12345");
+        System.out.println("수정");
     }
 }
