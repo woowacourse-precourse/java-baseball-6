4ed8fd24e6845600a90554c112497bec2f3fd548
dmswl6310 <dmswl6310@gmail.com>
2023-11-21 12:18:28 +0900

    style(OutputView): 숫자 입력시 한줄띄우기는 부분 삭제

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -6,7 +6,7 @@ public class OutputView {
     }
 
     public static void printRequestNumber() {
-        System.out.println("숫자를 입력해주세요 : ");
+        System.out.print("숫자를 입력해주세요 : ");
     }
 
     public static void printResult(String result) {
