c8c008f71476065ad747ebd17d2c7ddfc3aa5e69
dmswl6310 <dmswl6310@gmail.com>
2023-11-21 12:02:02 +0900

    feat(Game): 3스트라이크 일 시, 게임 종료 문구 출력

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -12,4 +12,12 @@ public class OutputView {
     public static void printResult(String result) {
         System.out.println(result);
     }
+
+    public static void printEnding() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public static void printRequestRestart() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
