675f93814d33a6861ea920d4ae883752f2d333d2
anneu <b.imnuj@gmail.com>
2023-11-24 16:41:01 +0900

    refactor: 게임 재시작 여부 입력 요청 로직 추가

--- src/main/java/baseball/view/Input.java
+++ src/main/java/baseball/view/Input.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class Input {
 
     public String readCommand() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String input = read();
         validateNumberFormat(input);
         return input;
