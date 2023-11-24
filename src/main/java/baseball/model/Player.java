c4665686e01e516e9ff94129eebd44095a3e65af
anneu <b.imnuj@gmail.com>
2023-11-24 14:59:22 +0900

    feat: 플레이어 생성 기능 구현

---
+++ src/main/java/baseball/model/Player.java
@@ -0,0 +1,12 @@
+package baseball.model;
+
+import java.util.List;
+
+public class Player {
+
+    private final List<Integer> numbers;
+
+    public Player(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+}
