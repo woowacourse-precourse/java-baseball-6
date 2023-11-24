d2ca2e640937f2daa5080250e593a55821a9ccf1
anneu <b.imnuj@gmail.com>
2023-11-24 17:57:43 +0900

    refactor: 숫자 형식 예외 메세지 추가

--- src/main/java/baseball/view/InputValidator.java
+++ src/main/java/baseball/view/InputValidator.java
@@ -2,11 +2,13 @@ package baseball.view;
 
 public class InputValidator {
 
+    private static final String NUMBER_FORMAT_EX_MESSAGE = "숫자가 아닙니다.";
+
     public void validateNumberFormat(String input) {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NUMBER_FORMAT_EX_MESSAGE);
         }
     }
 }
