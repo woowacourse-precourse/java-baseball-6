8dc2a4be9cc6a90903bab825b146bb7b3aedee1b
anneu <b.imnuj@gmail.com>
2023-11-24 14:28:20 +0900

    feat: 서로 다른 3자리 랜덤 수 생성 기능 구현

---
+++ src/main/java/baseball/model/NumberMaker.java
@@ -0,0 +1,25 @@
+package baseball.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberMaker {
+    public List<Integer> makeComputerNumbers() {
+        List<Integer> computerNumbers = new ArrayList<>();
+
+        while (computerNumbers.size() <= 3) {
+            int randomNumber = pickRandomNumber();
+            if (!computerNumbers.contains(randomNumber)) {
+                computerNumbers.add(randomNumber);
+            }
+        }
+
+        return computerNumbers;
+    }
+
+    private int pickRandomNumber() {
+        return Randoms.pickNumberInRange(1, 9);
+    }
+}
