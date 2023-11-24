2308a42d1a5d7a2e58ae74e4eabe43ae995f2b7b
anneu <b.imnuj@gmail.com>
2023-11-24 17:08:16 +0900

    test: 게임 진행 상황을 확인하는 테스트 추가

--- src/test/java/baseball/model/JudgementTest.java
+++ src/test/java/baseball/model/JudgementTest.java
@@ -2,8 +2,12 @@ package baseball.model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,5 +27,28 @@ class JudgementTest {
         // then
         assertThat(judgement.getStrike()).isEqualTo(1);
         assertThat(judgement.getBall()).isEqualTo(2);
+    }
+
+    @DisplayName("게임 진행 상황을 확인한다.")
+    @MethodSource("generateData")
+    @ParameterizedTest
+    void isProgress(Player player, boolean expected) {
+        //given
+        Computer computer = new Computer(List.of(1, 2, 3));
+        Judgement judgement = new Judgement(computer);
+        judgement.judge(player);
+
+        //when
+        boolean result = judgement.isProgress();
+
+        //then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> generateData() {
+        return Stream.of(
+                Arguments.of(new Player(List.of(1, 2, 3)), false),
+                Arguments.of(new Player(List.of(1, 3, 2)), true)
+        );
     }
 }
