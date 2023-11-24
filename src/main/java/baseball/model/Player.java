19494d7c040e06302431d64a53b32275d29e8e23
anneu <b.imnuj@gmail.com>
2023-11-24 15:03:14 +0900

    feat: 플레이어의 수가 서로 다른지 검증하는 기능 구현

--- src/main/java/baseball/model/Player.java
+++ src/main/java/baseball/model/Player.java
@@ -7,6 +7,20 @@ public class Player {
     private final List<Integer> numbers;
 
     public Player(List<Integer> numbers) {
+        validate(numbers);
         this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        validateDuplicate(numbers);
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        int uniqueNumberCount = (int) numbers.stream()
+                .distinct()
+                .count();
+        if (numbers.size() != uniqueNumberCount) {
+            throw new IllegalArgumentException("서로 다른 숫자여야 합니다.");
+        }
     }
 }
