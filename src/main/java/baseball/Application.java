a2089b6bdbd5e01a405dbc08195163ff4d9a450c
SungHoCha <tjdgh3725@gmail.com>
2023-11-27 02:52:07 +0900

    test

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,27 +1,39 @@
 package baseball;
 
-import baseball.domain.Balls;
-import baseball.domain.BallsFactory;
-import baseball.domain.Referee;
+
 import baseball.view.InputView;
+import baseball.view.OutputView;
 
 import java.util.List;
+import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        int next = 1;
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        while (next == 1) {
-            Balls computerBalls = BallsFactory.createComputerBalls();
-            Referee referee = new Referee();
-            while (referee.getStrikeNumber() != 3) {
-                List<Integer> ballNumbers = InputView.takeInput();
-                Balls playerBalls = BallsFactory.createBalls(ballNumbers);
-                referee = computerBalls.play(playerBalls);
-                referee.judge();
-                next = referee.getNext();
-            }
+        LunchService lunchService = new LunchService();
+        CoachsDTO coachsDTO = takeInputName(lunchService);
+
+        for (String name : coachsDTO.getNames()) {
+            reportProhibitedFood(name, lunchService);
+        }
+        OutputView.printMenu(lunchService.recommendFoods());
+    }
+
+    private static CoachsDTO takeInputName(LunchService lunchService) {
+        try {
+            return lunchService.createCoachsDTO(InputView.readCoachName());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return takeInputName(lunchService);
+        }
+    }
+
+    private static void reportProhibitedFood(String name, LunchService lunchService) {
+        try {
+            lunchService.putProhibitedFoods(InputView.readProhibitedFood(name));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            reportProhibitedFood(name, lunchService);
         }
     }
 }
--- src/main/java/baseball/domain/BallStatus.java
+++
@@ -1,5 +0,0 @@
-package baseball.domain;
-
-public enum BallStatus {
-    STRIKE, BALL, NOTHING
-}
