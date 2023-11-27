720795d7b7c36d4d684df098eec1781b13cdb001
ejdrp1 <ejdrp1@naver.com>
2023-11-27 20:48:29 +0900

    feat(setRandomNum) : 무작위 숫자 3개를 정해주는 메서드

--- src/main/java/baseball/Controller/BaseBallController.java
+++ src/main/java/baseball/Controller/BaseBallController.java
@@ -1,6 +1,8 @@
 package baseball.Controller;
 
 import baseball.View.BaseBallView;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class BaseBallController {
 
@@ -12,6 +14,6 @@ public class BaseBallController {
 
     private void inputNumData() {
         BaseBallView.displayInputNumData();
-
+        String inputNum = Console.readLine();
     }
 }
--- src/main/java/baseball/Model/Computer.java
+++ src/main/java/baseball/Model/Computer.java
@@ -1,6 +1,20 @@
 package baseball.Model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Computer {
 
+    List<Integer> computer = new ArrayList<>();
 
+    private void setRandomNum() {
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+    }
 }
