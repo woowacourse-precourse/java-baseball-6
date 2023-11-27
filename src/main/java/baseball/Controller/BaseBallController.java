d514dc568eb942e832455270be2d12e3fd27f1a5
ejdrp1 <ejdrp1@naver.com>
2023-11-27 21:33:57 +0900

    chore(inputNumData) : 입력받을 데이터 어떤 자료형으로 받을지 수정

--- src/main/java/baseball/Controller/BaseBallController.java
+++ src/main/java/baseball/Controller/BaseBallController.java
@@ -1,31 +1,29 @@
 package baseball.Controller;
 
 import baseball.Model.Computer;
-import baseball.Model.User;
+import baseball.Model.Number;
 import baseball.View.BaseBallView;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class BaseBallController {
 
     Computer computer = new Computer();
-    User user = new User();
+    Number number = new Number();
 
 //    프로그램 시작
     public void startProgram() {
         BaseBallView.displayStartProgram(); // 게임 시작 msg
 //        무작위 숫자 생성 (3개)
         List<Integer> comNum = computer.setRandomNum(); // [a, b, c]
-        inputNumData();
+        inputNumData(comNum);
     }
 
-    private void inputNumData() {
+    private void inputNumData(List<Integer> comNum) {
         BaseBallView.displayInputNumData(); // 숫자 입력 msg
-//        사용자 숫자 입력
-        String[] arr = user.inputNumValue();
-
-
-
+//        사용자 숫자 입력 (어떤 자료형으로 받을건지 고민해보자)
+        String[] userNumArr = number.inputNumValue(); // [x, y, z]
     }
+
 }
--- src/main/java/baseball/Model/Number.java
+++ src/main/java/baseball/Model/Number.java
@@ -1,4 +1,11 @@
 package baseball.Model;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Number {
+
+    public String[] inputNumValue() {
+        String inputNum = Console.readLine();
+        return inputNum.split(",");
+    }
 }
