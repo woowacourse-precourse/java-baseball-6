abb64ffbc33f2e7477eed6ed195b4790dc47593e
kgy1008 <gayeon1008@g.skku.edu>
2023-11-29 00:16:51 +0900

    Finish

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,5 @@
 package baseball;
 
-import java.util.Arrays;
-
-import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
-import static camp.nextstep.edu.missionutils.Console.readLine;
 public class Application {
     public static void main(String[] args) {
         Game target = new Game();
