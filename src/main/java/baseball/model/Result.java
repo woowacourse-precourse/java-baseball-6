b97e94595b42c0f90b3c47b3f192a8c577918fab
gahyeon <1000ji1@naver.com>
2023-11-30 03:24:26 +0900

    feat : print result function

--- src/main/java/baseball/model/Result.java
+++ src/main/java/baseball/model/Result.java
@@ -1,6 +1,10 @@
 package baseball.model;
 
 public class Result {
+    private static final String NOTHING = "낫싱";
+    private static final String BALL = "볼";
+    private static final String STRIKE = "스트라이크";
+
     private int ball = 0;
     private int strike = 0;
 
@@ -19,5 +23,21 @@ public class Result {
     public void addStrike() {
         this.strike++;
     }
-    
+
+    public String print() {
+        String result = NOTHING;
+
+        if (ball == 0 && strike == 0) {
+            return NOTHING;
+        } else if (strike == 3 && ball == 0) {
+            return strike + STRIKE;
+        } else if (ball > 0) {
+            result = ball + BALL;
+            if (strike > 0) {
+                result += " " + strike + STRIKE;
+            }
+            return result;
+        }
+        return result;
+    }
 }
