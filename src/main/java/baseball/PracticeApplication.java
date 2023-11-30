fee1877d2b5a15b601c40c4d0e74f584a92aeb28
mango <esudatcom@gmail.com>
2023-11-30 16:54:15 +0900

    docs: 숫자 야구 리드미 작성

---
+++ src/main/java/baseball/PracticeApplication.java
@@ -0,0 +1,15 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.*;
+
+public class PracticeApplication {
+    public static void main(String[] args) {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+    }
+
+    public static int computerNumbers(){
+
+    }
+}
+
