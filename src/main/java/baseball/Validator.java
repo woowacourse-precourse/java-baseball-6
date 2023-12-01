39f9d8fd07845d35ec3aaf0417e8f05217f898fa
Jiwon Kwon <kjiwon411@gmail.com>
2023-12-01 15:16:17 +0900

    (refactor)baseball: separate methods from Application.java

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -1,2 +1,27 @@
-package baseball;public class Validator {
+package baseball;
+
+public class Validator {
+    public static void validateNumber(String player) {
+        try {
+            validateIntConvertable(player);
+            int number = Integer.parseInt(player);
+            validateRange(number);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    static void validateRange(int number) {
+        if (number < 100 || number > 999) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    static void validateIntConvertable(String player) {
+        try {
+            Integer.parseInt(player);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException();
+        }
+    }
 }
