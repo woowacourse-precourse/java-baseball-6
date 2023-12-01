da48a92eb4cb6e1657f3f7e671fe017051680565
mango <esudatcom@gmail.com>
2023-12-01 15:58:31 +0900

    feat: 볼 & 스트라이크 카운트 메서드 구현

--- src/main/java/baseball/PracticeApplication.java
+++ src/main/java/baseball/PracticeApplication.java
@@ -3,6 +3,7 @@ package baseball;
 import camp.nextstep.edu.missionutils.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PracticeApplication {
@@ -12,7 +13,12 @@ public class PracticeApplication {
 
         System.out.println(isNumbers("123"));
         System.out.println(isNotDuplication("133"));
-        System.out.println(userNumbers());
+//        System.out.println(userNumbers());
+
+        List<Integer> computer = Arrays.asList(1, 2, 3);
+        List<Integer> user = Arrays.asList(1, 2, 4);
+
+        System.out.println(ballCount(computer, user, 1));
 
     }
 
@@ -32,14 +38,14 @@ public class PracticeApplication {
     public static List<Integer> userNumbers() {
         System.out.print("숫자를 입력해주세요 : ");
         String inputNumbers = Console.readLine();
-        if(isValid(inputNumbers)){
+        if (isValid(inputNumbers)) {
             List<Integer> numbers = new ArrayList<>();
-            for (char ch: inputNumbers.toCharArray()) {
+            for (char ch : inputNumbers.toCharArray()) {
                 numbers.add(Character.getNumericValue(ch));
             }
             return numbers;
         } else {
-            throw  new IllegalArgumentException("중복되지 않은 3자리 수를 입력해주세요.");
+            throw new IllegalArgumentException("중복되지 않은 3자리 수를 입력해주세요.");
         }
     }
 
@@ -69,6 +75,32 @@ public class PracticeApplication {
             }
         }
         return true;
+    }
+
+    // 숫자 비교하여 결과를 판단하는 메서드
+//    public static String judgment(List<Integer> computerNumbers, List<Integer> userNumbers) {
+//        //
+//    }
+
+    // 스트라이크로 이미 카운트가 되었다면 볼에는 포함이 되면 안됨
+    private static int ballCount(List<Integer> computerNumbers, List<Integer> userNumbers, int strikeCount) {
+        int count = 0;
+        for (int userNumber : userNumbers()) {
+            if (computerNumbers.contains(userNumber)) {
+                count++;
+            }
+        }
+        return count - strikeCount;
+    }
+
+    private static int strikeCount(List<Integer> computerNumbers, List<Integer> userNumbers) {
+        int count = 0;
+        for (int i = 0; i < computerNumbers.size(); i++) {
+            if (computerNumbers.get(i).equals(userNumbers.get(i))) {
+                count++;
+            }
+        }
+        return count;
     }
 
 
