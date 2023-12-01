340b5e0ce0a8ea68eb3443788fbaeec6f03dbdcc
Jiwon Kwon <kjiwon411@gmail.com>
2023-12-01 12:59:19 +0900

    (test)baseball: test computer creates random numbers
    - 3 numbers
    - all different

---
+++ src/test/java/baseball/randomNumberGeneratorTest.java
@@ -0,0 +1,22 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+public class randomNumberGeneratorTest {
+
+    @Test
+    void createRandomNumbers() {
+        int[] testComputer = randomNumberGenerator.randomNumberGenerator();
+        assertThat(testComputer.length).isEqualTo(3);
+        for (int i=1; i<3; i++) {
+            assertThat(testComputer[i]).isNotEqualTo(testComputer[i-1]);
+        }
+    }
+}
