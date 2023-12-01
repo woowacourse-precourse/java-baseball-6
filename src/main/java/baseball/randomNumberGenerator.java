86af4ef545cd8fae3a77b1f667c98d37bb2a808a
Jiwon Kwon <kjiwon411@gmail.com>
2023-12-01 12:58:41 +0900

    (refactor)baseball: remove unnecessary codes

--- src/main/java/baseball/randomNumberGenerator.java
+++ src/main/java/baseball/randomNumberGenerator.java
@@ -1,2 +1,20 @@
-package baseball;public class randomNumberGenerator {
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+
+public class randomNumberGenerator {
+
+    public static int[] generateComputer() {
+        int[] computer = new int[3];
+        int num = Randoms.pickNumberInRange(1, 9);
+        computer[0] = num;
+        for (int i=1; i<3; i++) {
+            while (num == computer[i-1]) {
+                num = Randoms.pickNumberInRange(1, 9);
+            }
+            computer[i] = num;
+        }
+        return computer;
+    }
 }
