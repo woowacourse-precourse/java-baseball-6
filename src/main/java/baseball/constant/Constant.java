024245a133ad18307c53ca7326d20f2b2883ac89
박형균 <phk@baghyeong-gyun-ui-MacBookPro.local>
2023-12-02 16:10:58 +0900

    style: formatting

--- src/main/java/baseball/constant/Constant.java
+++ src/main/java/baseball/constant/Constant.java
@@ -1,6 +1,11 @@
 package baseball.constant;
 
 public class Constant {
+
+    private Constant() {
+
+    }
+
     public static final int RIGHT_NUMBER_LENGTH = 3;
     public static final int MAX_STRIKE = 3;
     public static final String GAME_RESTART = "1";
