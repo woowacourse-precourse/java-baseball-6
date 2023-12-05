c0ce317a2870fe46eecb4692fb052147e1e5d4b6
rakyun1 <dbskim234@naver.com>
2023-12-05 05:50:27 -0700

    Test: add InputTest.java Input.java를 테스트 하는 클라스

---
+++ src/test/java/baseball/InputTest.java
@@ -0,0 +1,25 @@
+package baseball;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class InputTest {
+	Input input = new Input();
+	@Test
+	void TestisNotDuplicateWhenUserInputIsDuplicate(){
+		String duplicateValue = "111";
+		assertThrows(IllegalArgumentException.class, () -> {
+			input.isNotDuplicate(duplicateValue);
+		});
+	}
+
+	@Test
+	void testIsNotDuplicateWhenUserInputIsNotDuplicate() {
+		String nonDuplicateValue = "123";
+		assertDoesNotThrow(() -> {
+			input.isNotDuplicate(nonDuplicateValue);
+		});
+	}
+}
