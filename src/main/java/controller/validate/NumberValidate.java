8111a2e5dc35c1463a8c6a4ad84e3c147de9652d
jwAhn1020 <takartin@naver.com>
2023-12-11 03:18:45 +0900

    12/11 feat : util 디렉토리 생성

--- src/main/java/controller/validate/NumberValidate.java
+++ src/main/java/controller/validate/NumberValidate.java
@@ -8,7 +8,7 @@ public class NumberValidate {
             throw new IllegalArgumentException("[ERROR] 입력한 수가 세자리 수가 아닙니다.");
         }
         if (!containsOnlyNumbers(numberToCheck)) {
-            throw new IllegalArgumentException("[ERROR] 숫자가 아닌것이 입력되어 있습니다.");
+            throw new IllegalArgumentException("[ERROR] 숫자가 아닌 것이 입력되어 있습니다.");
         }
     }
 
