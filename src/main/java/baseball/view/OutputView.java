df056dd1e95ad308b6abe24fd103be1c8eb09f1c
MingyeomKim <mingyum119@naver.com>
2023-12-14 04:10:40 +0900

    feat: 힌트를 출력하는 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,5 +1,6 @@
 package baseball.view;
 
+import baseball.domain.HintResult;
 import baseball.view.console.ConsoleWriter;
 
 public class OutputView {
@@ -12,4 +13,31 @@ public class OutputView {
         ConsoleWriter.printlnMessage("숫자 야구 게임을 시작합니다.");
     }
 
+    public void printHintResult(HintResult hintResult) {
+        int strike = hintResult.strike();
+        int ball = hintResult.ball();
+
+        ConsoleWriter.printlnMessage(generateResultMessage(strike, ball));
+    }
+
+    public String generateResultMessage(int strike, int ball) {
+        if (strike != 0 && ball != 0) {
+            return String.format(
+                    BOTH,
+                    String.format(BALL, ball),
+                    String.format(STRIKE, strike)
+            );
+        }
+        if (ball != 0) {
+            return String.format(BALL, ball);
+        }
+        if (strike != 0) {
+            return String.format(STRIKE, strike);
+        }
+        return NOTHING;
+    }
+
+    public void printGameOver() {
+        ConsoleWriter.printlnMessage("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
 }
