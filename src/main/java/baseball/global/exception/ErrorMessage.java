2ef10d1bcddc57b4ccb9a10d88ab651f95643725
MingyeomKim <mingyum119@naver.com>
2023-12-14 04:12:12 +0900

    feat: 에러 메시지를 저장하는 Enum 클래스 구현

--- src/main/java/baseball/global/exception/ErrorMessage.java
+++ src/main/java/baseball/global/exception/ErrorMessage.java
@@ -2,6 +2,9 @@ package baseball.global.exception;
 
 public enum ErrorMessage {
     BLANK_INPUT_ERROR("빈 문자열이 입력되었습니다."),
+    NOT_NUMBER_ERROR("숫자만을 입력해주세요."),
+    INVALID_NUMBER_ERROR("숫자를 다시 입력해주세요."),
+    INVALID_RANGE_ERROR("올바른 범위의 숫자를 입력해주세요.");
 
     private final String message;
 
