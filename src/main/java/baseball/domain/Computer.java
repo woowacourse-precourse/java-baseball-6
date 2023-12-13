bbb317b94a4edc4c1f188e6464152ed9a110fc36
MingyeomKim <mingyum119@naver.com>
2023-12-14 04:10:16 +0900

    feat: 힌트를 생성하는 기능 구현

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -10,4 +10,24 @@ public class Computer {
         this.numbers = RandomNumberGenerator.generateRandomNumber();
     }
 
+    public HintResult generateHintResult(List<Integer> givenNumbers) {
+        int strike = 0;
+        int ball = 0;
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) == givenNumbers.get(i)) {
+                strike++;
+            }
+            ball += isBallIndex(i, givenNumbers);
+        }
+        return new HintResult(strike, ball);
+    }
+
+    private int isBallIndex(int i, List<Integer> givenNumbers) {
+        for (int j = 0; j < givenNumbers.size(); j++) {
+            if (i != j && numbers.get(i) == givenNumbers.get(j)) {
+                return 1;
+            }
+        }
+        return 0;
+    }
 }
