bec5f7da3cd1a49d800627b3502cd5bb8a80f3b1
koomin1227 <koomin1227@naver.com>
2024-01-03 00:49:57 +0900

    feat: baseball 게임 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         Baseball baseball = new Baseball();
-        baseball.playGame();
+        baseball.run();
         // TODO: 프로그램 구현
     }
 
