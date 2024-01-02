a9e9240852d0fcce64651a7b17e74f19ed15a8d3
koomin1227 <koomin1227@naver.com>
2024-01-02 23:25:52 +0900

    feat: Input의 method를 static으로 변경

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -6,26 +6,26 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 public class Input {
-    public ArrayList<Integer> inputNumber() {
+    public static ArrayList<Integer> inputNumber() {
         String input = Console.readLine();
         return validate(input);
     }
 
-    private ArrayList<Integer> validate(String input) {
+    private static ArrayList<Integer> validate(String input) {
         if (input.length() != 3) {
             throw new IllegalArgumentException("3자리가 아님");
         }
         if (!isAllNumber(input)){
             throw new IllegalArgumentException("숫자가 아님");
         }
-        ArrayList<Integer> numbers = this.convertToNumbers(input);
+        ArrayList<Integer> numbers = convertToNumbers(input);
         if (!isAllDifferent(numbers)){
             throw new IllegalArgumentException("같은 숫자가 있음");
         }
         return numbers;
     }
 
-    private boolean isAllNumber(String input){
+    private static boolean isAllNumber(String input){
         for(int i = 0;i < input.length();i++){
             if (!Character.isDigit(input.charAt(i)) || input.charAt(i) == '0'){
                 return false;
@@ -34,7 +34,7 @@ public class Input {
         return true;
     }
 
-    private ArrayList<Integer> convertToNumbers(String input){
+    private static ArrayList<Integer> convertToNumbers(String input){
         ArrayList<Integer> numbers = new ArrayList<>(3);
         for(int i = 0;i < 3;i++){
             int digit = Character.getNumericValue(input.charAt(i));
@@ -42,7 +42,7 @@ public class Input {
         }
         return numbers;
     }
-    private boolean isAllDifferent(ArrayList<Integer> numbers){
+    private static boolean isAllDifferent(ArrayList<Integer> numbers){
         return !Objects.equals(numbers.get(0), numbers.get(1)) && !Objects.equals(numbers.get(1), numbers.get(2)) && !Objects.equals(numbers.get(0), numbers.get(2));
     }
 }
