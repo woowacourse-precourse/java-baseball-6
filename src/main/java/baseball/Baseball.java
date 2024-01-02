8152d8d60ccd8a649cf3a5a768fe91636ebb8eca
koomin1227 <koomin1227@naver.com>
2024-01-02 18:11:13 +0900

    feat: 게임 진행 클래스 생성

---
+++ src/main/java/baseball/Baseball.java
@@ -0,0 +1,12 @@
+package baseball;
+
+import java.util.Scanner;
+
+public class Baseball {
+    public void playGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+        }
+    }
+}
